d2438814acf8cbcf0996cee3f55225395576cd43
Irina Fedorova <irfedorova@google.com>
2024-02-19 11:04:40 +0000

    Reland "kiosk: Crash kiosk session if unable to close browser window"
    This is a reland of commit 753b156c6e00c0af03e6ca23fc3f1e8b17b3ba18
    
    Original change's description:
    > kiosk: Crash kiosk session if unable to close browser window
    >
    > If we find that an undesired browser window did not close as expected,
    > we crash the browser process. This will result in the kiosk session
    > restarting.
    >
    > Bug: b/325401325
    > Test: KioskBrowserSessionDeathTest
    > Change-Id: Ie59031e69c944bb649a8b318e8238f1316d01ff9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5291238
    > Reviewed-by: Sergii Bykov <sbykov@google.com>
    > Commit-Queue: Irina Fedorova <irfedorova@google.com>
    > Reviewed-by: Irina Fedorova <irfedorova@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1261556}
    
    Bug: b/325401325
    Change-Id: Ide8b6003df8a0659b136cea7a30e1fb86ffda4e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301389
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1262307}

--- chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.h
+++ chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.h
@@ -5,9 +5,12 @@
 #ifndef CHROME_BROWSER_CHROMEOS_APP_MODE_KIOSK_BROWSER_WINDOW_HANDLER_H_
 #define CHROME_BROWSER_CHROMEOS_APP_MODE_KIOSK_BROWSER_WINDOW_HANDLER_H_
 
+#include <map>
+
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_policies.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -63,6 +66,9 @@ class KioskBrowserWindowHandler : public BrowserListObserver {
   void HandleNewBrowserWindow(Browser* browser);
   void HandleNewSettingsWindow(Browser* browser, const std::string& url_string);
 
+  void CloseBrowserWindowsIf(base::FunctionRef<bool(const Browser&)> filter);
+  void CloseBrowserAndSetTimer(Browser* browser);
+  void OnCloseBrowserTimeout();
   void CloseAllUnexpectedBrowserWindows();
 
   // BrowserListObserver
@@ -106,6 +112,12 @@ class KioskBrowserWindowHandler : public BrowserListObserver {
 
   // Provides access to app session related policies.
   KioskPolicies kiosk_policies_;
+
+  // Map that keeps track of all unexpected browser windows until they are
+  // confirmed to be closed via `OnBrowserRemoved`. If they did not get closed
+  // before the timer fires, we will crash as we consider the kiosk session
+  // compromised.
+  std::map<Browser*, base::OneShotTimer> closing_browsers_;
 
   base::WeakPtrFactory<KioskBrowserWindowHandler> weak_ptr_factory_{this};
 };
