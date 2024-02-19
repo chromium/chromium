599b61c695917a7d9022a7e9a9fbede6d9bcc8d4
Tommy Martino <tmartino@google.com>
2024-02-19 10:35:06 +0000

    [iOS] Always use standard BrowserState's AffiliationService
    We have observed crashes where the AffiliationServiceFactory returns a
    nullptr in incognito. The main BrowserState's AffiliationService is safe
    to use in incognito; the corresponding Android/Desktop code already uses
    the main Profile for this purpose. This CL adopts that behavior on iOS.
    
    (cherry picked from commit 4769c7740da207ae745cd8cf9aa923a6c00d0738)
    
    Bug: 41494635
    Change-Id: Ifd361e85d574899a9047d51437aed128e080b846
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5301562
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1261674}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5303517
    Owners-Override: Erhu Akpobaro <eakpobaro@google.com>
    Reviewed-by: Erhu Akpobaro <eakpobaro@google.com>
    Cr-Commit-Position: refs/branch-heads/6261@{#867}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- ios/chrome/browser/passwords/model/ios_chrome_affiliation_service_factory.mm
+++ ios/chrome/browser/passwords/model/ios_chrome_affiliation_service_factory.mm
@@ -30,8 +30,16 @@ IOSChromeAffiliationServiceFactory::GetInstance() {
 password_manager::AffiliationService*
 IOSChromeAffiliationServiceFactory::GetForBrowserState(
     web::BrowserState* browser_state) {
+  CHECK(browser_state);
+
+  // Always use the original BrowserState, not incognito. AffiliationService is
+  // safe to use in incognito.
+  auto* original_browser_state =
+      ChromeBrowserState::FromBrowserState(browser_state)
+          ->GetOriginalChromeBrowserState();
+
   return static_cast<password_manager::AffiliationService*>(
-      GetInstance()->GetServiceForBrowserState(browser_state, true));
+      GetInstance()->GetServiceForBrowserState(original_browser_state, true));
 }
 
 IOSChromeAffiliationServiceFactory::IOSChromeAffiliationServiceFactory()
