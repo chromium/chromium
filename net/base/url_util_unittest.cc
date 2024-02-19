826a8158bd76bbe6ced7ef25dad6ee74deac8948
Hayato Ito <hayato@chromium.org>
2024-02-19 07:55:06 +0000

    [URL] Migrate net/base/url_util_unittest to flag-dependent tests
    Bug: 40063064
    Change-Id: I561037692c18d97f509de765f87243897be678b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5298419
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262269}

--- net/base/url_util_unittest.cc
+++ net/base/url_util_unittest.cc
@@ -9,9 +9,11 @@
 
 #include "base/format_macros.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 #include "url/scheme_host_port.h"
+#include "url/url_features.h"
 #include "url/url_util.h"
 
 using base::ASCIIToUTF16;
@@ -660,6 +662,28 @@ TEST(UrlUtilTest, IsLocalhost) {
   EXPECT_TRUE(IsLocalhost(localhost6));
 }
 
+class UrlUtilTypedTest : public ::testing::TestWithParam<bool> {
+ public:
+  UrlUtilTypedTest()
+      : use_standard_compliant_non_special_scheme_url_parsing_(GetParam()) {
+    if (use_standard_compliant_non_special_scheme_url_parsing_) {
+      scoped_feature_list_.InitAndEnableFeature(
+          url::kStandardCompliantNonSpecialSchemeURLParsing);
+    } else {
+      scoped_feature_list_.InitAndDisableFeature(
+          url::kStandardCompliantNonSpecialSchemeURLParsing);
+    }
+  }
+
+ protected:
+  bool use_standard_compliant_non_special_scheme_url_parsing_;
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+INSTANTIATE_TEST_SUITE_P(All, UrlUtilTypedTest, ::testing::Bool());
+
 TEST(UrlUtilTest, SimplifyUrlForRequest) {
   struct {
     const char* const input_url;
@@ -691,16 +715,37 @@ TEST(UrlUtilTest, SimplifyUrlForRequest) {
       "ftp://user:pass@google.com:80/sup?yo#X#X",
       "ftp://google.com:80/sup?yo",
     },
-    { // Try a nonstandard URL
-      "foobar://user:pass@google.com:80/sup?yo#X#X",
-      "foobar://user:pass@google.com:80/sup?yo",
-    },
   };
   for (const auto& test : tests) {
     SCOPED_TRACE(test.input_url);
     GURL input_url(GURL(test.input_url));
     GURL expected_url(GURL(test.expected_simplified_url));
     EXPECT_EQ(expected_url, SimplifyUrlForRequest(input_url));
+  }
+}
+
+TEST_P(UrlUtilTypedTest, SimplifyUrlForRequest) {
+  static constexpr struct {
+    const char* const input_url;
+    const char* const expected_when_compliant;
+    const char* const expected_when_non_compliant;
+  } tests[] = {
+      {
+          // Try a non-special URL
+          "foobar://user:pass@google.com:80/sup?yo#X#X",
+          "foobar://google.com:80/sup?yo",
+          "foobar://user:pass@google.com:80/sup?yo",
+      },
+  };
+
+  for (const auto& test : tests) {
+    SCOPED_TRACE(test.input_url);
+    GURL simplified = SimplifyUrlForRequest(GURL(test.input_url));
+    if (use_standard_compliant_non_special_scheme_url_parsing_) {
+      EXPECT_EQ(simplified, GURL(test.expected_when_compliant));
+    } else {
+      EXPECT_EQ(simplified, GURL(test.expected_when_non_compliant));
+    }
   }
 }
 
