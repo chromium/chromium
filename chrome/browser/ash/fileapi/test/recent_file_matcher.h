1f79e49988fcdc03df10e0eb854909dc8e9cc3be
Bo Majewski <majewski@chromium.org>
2024-02-20 05:10:43 +0000

    [File]: Fix reporting of the RecentFileMatcher.
    The recent file matcher was incorrectly set up, printing fixed text and
    a binary dump. This CL fixes both issues. By passing the empty string we
    get printout of parameter values printed as a tuple. By using the
    result_listener, we can explain what did not match correctly.
    
    Bug: b:307455066
    Change-Id: I90856e1d20b7daa52c5c576055c2fbddf24fcdee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5308559
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262542}

--- chrome/browser/ash/fileapi/test/recent_file_matcher.h
+++ chrome/browser/ash/fileapi/test/recent_file_matcher.h
@@ -12,14 +12,13 @@ namespace ash {
 // Custom matcher that compares path and modified time with the specified
 // values. The `path` must be base::FilePath. The `modified_time` must be
 // base::Time.
-MATCHER_P2(IsRecentFile,
-           path,
-           modified_time,
-           "Checks name and modification time of a RecentFile") {
+MATCHER_P2(IsRecentFile, path, modified_time, "") {
   if (arg.url().path() != path) {
+    *result_listener << arg.url().path() << " != " << path;
     return false;
   }
   if (arg.last_modified() != modified_time) {
+    *result_listener << arg.last_modified() << " != " << modified_time;
     return false;
   }
   return true;
