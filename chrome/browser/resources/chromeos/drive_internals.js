253ec89e377b6eaeab43d9015cb0b851211f0ff7
François Degros <fdegros@chromium.org>
2024-02-20 03:24:39 +0000

    drive_internals: Comments
    BUG=None
    
    Change-Id: I29a17ed29c466f4fdbbdb79d32cb36043b6e4f30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5307479
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1262523}

--- chrome/browser/resources/chromeos/drive_internals.js
+++ chrome/browser/resources/chromeos/drive_internals.js
@@ -261,7 +261,7 @@ function updateOtherServiceLogsUrl(url) {
  * Adds a new row to the syncing paths table upon successful completion.
  * @param {string} path The path that was synced.
  * @param {string} status The drive::FileError as a string without the
- *     FILE_ERROR_ prefix.
+ *     "FILE_ERROR_" prefix.
  */
 function onAddSyncPath(path, status) {
   $('mirroring-path-status').textContent = status;
@@ -293,7 +293,8 @@ function onAddSyncPath(path, status) {
 /**
  * Remove a path from the syncing table.
  * @param {string} path The path that was synced.
- * @param {string} status The drive::FileError as a string.
+ * @param {string} status The drive::FileError as a string without the
+ *     "FILE_ERROR_" prefix.
  */
 function onRemoveSyncPath(path, status) {
   if (status !== 'OK') {
