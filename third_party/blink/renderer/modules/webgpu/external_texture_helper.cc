26bef9b7392fdb1793bad740ac7c0dcb2f732d27
Sunny Sachanandani <sunnyps@chromium.org>
2024-02-21 19:34:28 +0000

    webgpu: Eliminate a copy from video importExternalTexture fallback
    Use PaintCanvasVideoRenderer::CopyVideoFrameToSharedImage instead of
    Paint to avoid an extra copy to PCVR's internal cache. As a result,
    use the WebGPU ExternalTexture to perform mirror/flip/rotation.
    
    Also use the specified src_color_space for the intermediate resource
    with the YUV-RGB conversion stripped out since that's handled by
    CopyVideoFrameToSharedImage.
    
    Bug: b/316324499
    Change-Id: If48ea7de5ce96324d9bda56acb525267d6dc30c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5247746
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263500}

--- third_party/blink/renderer/modules/webgpu/external_texture_helper.cc
+++ third_party/blink/renderer/modules/webgpu/external_texture_helper.cc
@@ -6,6 +6,7 @@
 
 #include "media/base/video_frame.h"
 #include "media/base/video_transformation.h"
+#include "media/renderers/paint_canvas_video_renderer.h"
 #include "third_party/blink/renderer/core/html/media/html_video_element.h"
 #include "third_party/blink/renderer/modules/webcodecs/video_frame.h"
 #include "third_party/blink/renderer/modules/webgpu/dawn_conversions.h"
@@ -119,41 +120,6 @@ bool IsSameGamutAndGamma(gfx::ColorSpace src_color_space,
   return false;
 }
 
-// Copy this helper function from media/renderers/paint_canvas_video_renderer.cc
-// to workaround issue crbug.com/1407112. We need to ensure no color space
-// conversion happens during all conversions. And leverage Dawn to do the color
-// space conversion
-// TODO(crbug.com/1407112): Remove this after fixing crbug.com/1407112
-gfx::ColorSpace GetVideoFrameRGBColorSpacePreferringSRGB(
-    const media::VideoFrame* frame) {
-  const auto rgb_color_space = frame->ColorSpace().GetAsFullRangeRGB();
-  auto primary_id = rgb_color_space.GetPrimaryID();
-  switch (primary_id) {
-    case gfx::ColorSpace::PrimaryID::CUSTOM:
-      return rgb_color_space;
-    case gfx::ColorSpace::PrimaryID::SMPTE170M:
-    case gfx::ColorSpace::PrimaryID::SMPTE240M:
-      primary_id = gfx::ColorSpace::PrimaryID::BT709;
-      break;
-    default:
-      break;
-  }
-  auto transfer_id = rgb_color_space.GetTransferID();
-  switch (transfer_id) {
-    case gfx::ColorSpace::TransferID::CUSTOM:
-    case gfx::ColorSpace::TransferID::CUSTOM_HDR:
-      return rgb_color_space;
-    case gfx::ColorSpace::TransferID::BT709_APPLE:
-    case gfx::ColorSpace::TransferID::SMPTE170M:
-    case gfx::ColorSpace::TransferID::SMPTE240M:
-      transfer_id = gfx::ColorSpace::TransferID::SRGB;
-      break;
-    default:
-      break;
-  }
-  return gfx::ColorSpace(primary_id, transfer_id);
-}
-
 ExternalTextureSource GetExternalTextureSourceFromVideoElement(
     HTMLVideoElement* video,
     ExceptionState& exception_state) {
@@ -255,6 +221,14 @@ ExternalTexture CreateExternalTexture(
       static_cast<uint32_t>(visible_rect.width()),
       static_cast<uint32_t>(visible_rect.height())};
 
+  // Set ExternalTexture rotation and mirrored state.
+  const media::VideoFrameMetadata& metadata = media_video_frame->metadata();
+  if (metadata.transformation) {
+    external_texture_desc.rotation =
+        FromVideoRotation(metadata.transformation->rotation);
+    external_texture_desc.mirrored = metadata.transformation->mirrored;
+  }
+
   const bool zero_copy =
       (media_video_frame->HasTextures() &&
        (media_video_frame->format() == media::PIXEL_FORMAT_NV12) &&
@@ -291,6 +265,8 @@ ExternalTexture CreateExternalTexture(
     external_texture_desc.plane1 = plane1;
 
     // Set color space transformation metas for ExternalTexture
+    // TODO(b/41486014): This doesn't handle color spaces which don't have a
+    // SkYUVColorSpace equivalent such as YCoCg/YCgCo.
     std::array<float, 12> yuvToRgbMatrix =
         GetYUVToRGBMatrix(src_color_space, media_video_frame->BitDepth());
     external_texture_desc.yuvToRgbConversionMatrix = yuvToRgbMatrix.data();
@@ -308,14 +284,6 @@ ExternalTexture CreateExternalTexture(
         color_space_conversion_constants.src_transfer_constants.data();
     external_texture_desc.dstTransferFunctionParameters =
         color_space_conversion_constants.dst_transfer_constants.data();
-
-    // Set ExternalTexture rotation and Y-axis flipY
-    const media::VideoFrameMetadata& metadata = media_video_frame->metadata();
-    if (metadata.transformation) {
-      external_texture_desc.rotation =
-          FromVideoRotation(metadata.transformation->rotation);
-      external_texture_desc.flipY = metadata.transformation->mirrored;
-    }
 
     external_texture.wgpu_external_texture =
         device->GetProcs().deviceCreateExternalTexture(device->GetHandle(),
@@ -345,30 +313,20 @@ ExternalTexture CreateExternalTexture(
   // - Reset origin of visible rect in ExternalTextureDesc and use internal
   // shader to
   //   handle visible rect.
-  const auto intrinsic_size =
-      gfx::Size(media_video_frame->visible_rect().width(),
-                media_video_frame->visible_rect().height());
-
   external_texture_desc.visibleOrigin = {};
 
-  // Try to workaround crbug.com/1407112 by keeping no color space conversion
-  // DrawVideoFrameIntoResourceProvider by setting the canvas resource's
-  // colorspace to the specific ones. However not all color space can be
-  // converted to SkColorSpace, in that case default to sRGB.
-  // TODO(crbug.com/1407112): set recyclable_canvas_resource_color_space to dest
-  // color space after fixing crbug.com/1407112.
-  gfx::ColorSpace recyclable_canvas_resource_color_space =
-      GetVideoFrameRGBColorSpacePreferringSRGB(media_video_frame.get());
-  if (!recyclable_canvas_resource_color_space.ToSkColorSpace()) {
-    recyclable_canvas_resource_color_space = gfx::ColorSpace::CreateSRGB();
-  }
-
   // Get a recyclable resource for producing WebGPU-compatible shared images.
+  // The recyclable resource's color space is the same as source color space
+  // with the YUV to RGB transform stripped out since that's handled by the
+  // PaintCanvasVideoRenderer.
+  // TODO(b/41486014): This doesn't handle color spaces which don't have a
+  // SkYUVColorSpace equivalent such as YCoCg/YCgCo.
+  gfx::ColorSpace resource_color_space = src_color_space.GetAsRGB();
   std::unique_ptr<RecyclableCanvasResource> recyclable_canvas_resource =
       device->GetDawnControlClient()->GetOrCreateCanvasResource(
-          SkImageInfo::MakeN32Premul(
-              intrinsic_size.width(), intrinsic_size.height(),
-              recyclable_canvas_resource_color_space.ToSkColorSpace()));
+          SkImageInfo::MakeN32Premul(visible_rect.width(),
+                                     visible_rect.height(),
+                                     resource_color_space.ToSkColorSpace()));
   if (!recyclable_canvas_resource) {
     return external_texture;
   }
@@ -381,25 +339,34 @@ ExternalTexture CreateExternalTexture(
   if (auto* context_provider = context_provider_wrapper->ContextProvider())
     raster_context_provider = context_provider->RasterContextProvider();
 
-  // TODO(crbug.com/1174809): This isn't efficient for VideoFrames which are
-  // already available as a shared image. A WebGPUMailboxTexture should be
-  // created directly from the VideoFrame instead.
-  // TODO(crbug.com/1174809): VideoFrame cannot extract video_renderer.
-  // DrawVideoFrameIntoResourceProvider() creates local_video_renderer always.
-  // This might affect performance, maybe a cache local_video_renderer could
-  // help.
-  const auto dest_rect = gfx::Rect(intrinsic_size);
-  if (!DrawVideoFrameIntoResourceProvider(
-          std::move(media_video_frame), resource_provider,
-          raster_context_provider, dest_rect, video_renderer)) {
-    return external_texture;
+  std::unique_ptr<media::PaintCanvasVideoRenderer> local_video_renderer;
+  if (!video_renderer) {
+    local_video_renderer = std::make_unique<media::PaintCanvasVideoRenderer>();
+    video_renderer = local_video_renderer.get();
   }
+
+  // We don't need to specify a sync token since both CanvasResourceProvider and
+  // PaintCanvasVideoRenderer use the SharedGpuContext.
+  gpu::MailboxHolder dst_mailbox(
+      resource_provider->GetBackingMailboxForOverwrite(
+          MailboxSyncMode::kUnverifiedSyncToken),
+      gpu::SyncToken(), resource_provider->GetBackingTextureTarget());
+
+  // The returned sync token is from the SharedGpuContext - it's ok to drop it
+  // here since WebGPUMailboxTexture::FromCanvasResource will generate a new
+  // sync token from the SharedContextState and wait on it anyway.
+  std::ignore = video_renderer->CopyVideoFrameToSharedImage(
+      raster_context_provider, std::move(media_video_frame), dst_mailbox,
+      /*use_visible_rect=*/true);
 
   scoped_refptr<WebGPUMailboxTexture> mailbox_texture =
       WebGPUMailboxTexture::FromCanvasResource(
           device->GetDawnControlClient(), device->GetHandle(),
           WGPUTextureUsage::WGPUTextureUsage_TextureBinding,
           std::move(recyclable_canvas_resource));
+  if (!mailbox_texture) {
+    return {};
+  }
 
   WGPUTextureViewDescriptor view_desc = {};
   view_desc.arrayLayerCount = WGPU_ARRAY_LAYER_COUNT_UNDEFINED;
@@ -411,16 +378,12 @@ ExternalTexture CreateExternalTexture(
   external_texture_desc.plane0 = plane0;
 
   // Decide whether color space conversion could be skipped.
-  // Try to workaround crbug.com/1407112 by using Dawn to do color space
-  // conversion.
-  // TODO(crbug.com/1407112): compare recyclable_canvas_resource_color_space
-  // instead of src_color_space after fixing crbug.com/1407112.
   external_texture_desc.doYuvToRgbConversionOnly =
-      IsSameGamutAndGamma(src_color_space, dst_color_space);
+      IsSameGamutAndGamma(resource_color_space, dst_color_space);
 
   // Set color space transformation metas for ExternalTexture
   ColorSpaceConversionConstants color_space_conversion_constants =
-      GetColorSpaceConversionConstants(src_color_space, dst_color_space);
+      GetColorSpaceConversionConstants(resource_color_space, dst_color_space);
 
   external_texture_desc.gamutConversionMatrix =
       color_space_conversion_constants.gamut_conversion_matrix.data();
