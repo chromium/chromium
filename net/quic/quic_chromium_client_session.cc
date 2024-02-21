de9ce1844e5024a8b9a822fa8321f59a05fb990c
Liza Burakova <liza@chromium.org>
2024-02-21 19:02:15 +0000

    Check if session is going away in Handle::RequestStream.
    This CL adds an extra check in the QuicChromiumClientSession
    handle's RequestSession to make sure the session is not
    marked as going away before creating a new StreamRequest.
    
    Bug: 41491379
    Change-Id: I687dfc23131871cdba345d3cf78dbbbd2e619ce9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5268864
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263483}

--- net/quic/quic_chromium_client_session.cc
+++ net/quic/quic_chromium_client_session.cc
@@ -471,7 +471,8 @@ int QuicChromiumClientSession::Handle::RequestStream(
     const NetworkTrafficAnnotationTag& traffic_annotation) {
   DCHECK(!stream_request_);
 
-  if (!session_) {
+  // TODO(crbug.com/41491379): Add a regression test.
+  if (!session_ || session_->going_away_) {
     return ERR_CONNECTION_CLOSED;
   }
 
