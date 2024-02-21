ec76a6e2a0b400e2283df7a290b166956cc24ac9
Adam Langley <agl@chromium.org>
2024-02-21 02:13:21 +0000

    cbor: make `DiagnosticWriter` a little smarter.
    The DiagnosticWriter converts CBOR structures into debugging strings. It
    approximately caps the size of the returned strings to avoid huge
    messages causing problems. But when a CBOR structure contains a huge
    bytestring, that can immediately hit the limit and cause all the
    rest of the structure to be ignored.
    
    This change causes any bytestring that, when hex-encoded, is > 87.5% of
    the max output length, or which pushes the debugging string over the
    limit, to be replaced by a placeholder: "(x bytes)".
    
    This isn't perfect, it's still a greedy algorithm, but it's a modest
    debugging improvement when there are large bytestrings in a message.
    
    Change-Id: I3a812d179a0f353336f86cab612ae517d9b572cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5310565
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263095}

--- components/cbor/diagnostic_writer_unittest.cc
+++ components/cbor/diagnostic_writer_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "components/cbor/diagnostic_writer.h"
 
+#include "base/strings/stringprintf.h"
 #include "components/cbor/reader.h"
 #include "components/cbor/values.h"
 
@@ -59,7 +60,46 @@ TEST(CBORDiagnosticWriterTest, SizeLimit) {
   EXPECT_LT(
       DiagnosticWriter::Write(cbor::Value(bytes), /*rough_max_output_bytes=*/0)
           .size(),
-      3u);
+      20u);
+}
+
+TEST(CBORDiagnosticWriterTest, LargeBytestrings) {
+  constexpr struct {
+    size_t length;
+    bool should_be_truncated;
+  } kTestCases[] = {
+      {0, false},
+      {1, false},
+      // Just under the 87.5% threshold.
+      {56, false},
+      // Just over the 87.5% threshold.
+      {57, true},
+      // 100% of the output limit.
+      {64, true},
+      // Over the output limit.
+      {65, true},
+  };
+
+  for (const auto& test : kTestCases) {
+    SCOPED_TRACE(test.length);
+
+    Value::ArrayValue array;
+    array.emplace_back(1);
+    array.emplace_back(std::vector<uint8_t>(test.length, 0));
+    array.emplace_back(3);
+
+    std::string expected;
+    if (test.should_be_truncated) {
+      expected = base::StringPrintf("[1, (%zu bytes), 3]", test.length);
+    } else {
+      expected = base::StringPrintf("[1, h'%s', 3]",
+                                    std::string(test.length * 2, '0').c_str());
+    }
+
+    EXPECT_EQ(expected,
+              DiagnosticWriter::Write(cbor::Value(array),
+                                      /*rough_max_output_bytes=*/128));
+  }
 }
 
 TEST(CBORDiagnosticWriterTest, InvalidUTF8) {
