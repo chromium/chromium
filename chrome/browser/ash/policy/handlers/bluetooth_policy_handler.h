b653766d42bbe5814fe1f22ca0543556825c932c
Yun-Hao Chung <howardchung@google.com>
2024-02-21 10:12:33 +0000

    [Bluetooth] Update policy setting after adapter is ready
    It is possible that when Chrome receives the policy update signal, the
    Bluetooth adapter is not ready. This would cause the policy setting to
    fail and never update to the right settings even after the adapter is
    ready.
    
    This patch adds observers to watch the state of the adapter to update
    the policy if necessary to fix the issue.
    
    BUG=b:308608134
    TEST=tested on marasov with the following steps
    1. rmmod btusb.
    2. update policy settings in admin.google.com.
    3. modprobe btusb.
    4. verify the policy is updated.
    
    Change-Id: I55c210e80f3fa1acec0ee8d95cc2d5ac6ad49b2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5237976
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Commit-Queue: Yun-hao Chung <howardchung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263230}

--- chrome/browser/ash/policy/handlers/bluetooth_policy_handler.h
+++ chrome/browser/ash/policy/handlers/bluetooth_policy_handler.h
@@ -16,30 +16,40 @@ namespace policy {
 // This class observes the device setting |DeviceAllowBluetooth|, and calls
 // BluetoothAdapter::Shutdown() appropriately based on the value of that
 // setting.
-class BluetoothPolicyHandler {
+class BluetoothPolicyHandler : public device::BluetoothAdapter::Observer {
  public:
   explicit BluetoothPolicyHandler(ash::CrosSettings* cros_settings);
 
   BluetoothPolicyHandler(const BluetoothPolicyHandler&) = delete;
   BluetoothPolicyHandler& operator=(const BluetoothPolicyHandler&) = delete;
 
-  ~BluetoothPolicyHandler();
+  ~BluetoothPolicyHandler() override;
+
+  // device::BluetoothAdapter::Observer:
+  void AdapterPresentChanged(device::BluetoothAdapter* adapter,
+                             bool present) override;
+  void AdapterPoweredChanged(device::BluetoothAdapter* adapter,
+                             bool powered) override;
 
  private:
+  // Saves a reference to the adapter (so it stays alive) and calls
+  // |SetBluetoothPolicy| to update the current policy to the adapter.
+  void InitializeOnAdapterReady(
+      scoped_refptr<device::BluetoothAdapter> adapter);
   // Once a trusted set of policies is established, this function calls
   // |Shutdown| with the trusted state of the |DeviceAllowBluetooth| policy
   // through helper function |SetBluetoothPolicy|.
   void OnBluetoothPolicyChanged();
 
-  // Helper function used in device::BluetoothAdapterFactory::GetAdapter
-  // callback. Saves a reference to the adapter (so it stays alive) and calls
-  // |Shutdown| on the Bluetooth stack in order to disable it.
-  void SetBluetoothPolicy(scoped_refptr<device::BluetoothAdapter> adapter);
+  // Set the current policies to the Bluetooth adapter if |adapter_| is ready
+  // and a new policy is found but not yet set to the adapter.
+  void SetBluetoothPolicy();
 
   raw_ptr<ash::CrosSettings, DanglingUntriaged> cros_settings_;
   base::CallbackListSubscription allow_bluetooth_subscription_;
   base::CallbackListSubscription allowed_services_subscription_;
   scoped_refptr<device::BluetoothAdapter> adapter_;
+  bool new_policy_update_pending_ = false;
   base::WeakPtrFactory<BluetoothPolicyHandler> weak_factory_{this};
 };
 
