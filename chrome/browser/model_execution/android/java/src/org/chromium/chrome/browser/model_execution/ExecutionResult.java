d617e1e11a07f0f1ecd91c3a0d952543b287023f
Salvador Guerrero <salg@google.com>
2024-02-22 01:37:13 +0000

    Added Java interface for model execution
    This CL adds an empty interface to expose model execution to Java code.
    
    Bug: b/324912283
    Change-Id: I8f89b41196d15ae97c5fec0abbca79ad82abc533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5293024
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263727}

---
+++ chrome/browser/model_execution/android/java/src/org/chromium/chrome/browser/model_execution/ExecutionResult.java
@@ -0,0 +1,90 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.model_execution;
+
+import androidx.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Optional;
+
+/**
+ * Represents a result from a model execution. The result may be complete, partial or contain an
+ * error code.
+ */
+public class ExecutionResult {
+
+    /** Possible failure modes of model execution. */
+    @IntDef({
+        ExecutionError.UNKNOWN,
+        ExecutionError.NOT_AVAILABLE,
+        ExecutionError.BUSY,
+        ExecutionError.FILTERED,
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface ExecutionError {
+        public static final int UNKNOWN = 0;
+        public static final int NOT_AVAILABLE = 1;
+        public static final int BUSY = 2;
+        public static final int FILTERED = 3;
+    }
+
+    /**
+     * Constructor for results with error codes.
+     *
+     * @param errorCode A value from {@code ExecutionError}
+     */
+    ExecutionResult(@ExecutionError int errorCode) {
+        mIsCompleteResult = false;
+        mErrorCode = Optional.of(errorCode);
+    }
+
+    /**
+     * Constructor for successful results.
+     *
+     * @param response A response string.
+     * @param isCompleteResult Whether {@code response} is a complete result or part of a stream.
+     */
+    ExecutionResult(String response, boolean isCompleteResult) {
+        mIsCompleteResult = isCompleteResult;
+        mResponse = response;
+    }
+
+    /**
+     * Returns the response text related to this execution result. If {@code isCompleteResult()} is
+     * true then it'll return the full string returned from the model. Otherwise it'll contain a
+     * partial substring meant to be concatenated to other streaming results to form a full
+     * response. If the model failed to execute then null is returned.
+     *
+     * @return A string representing a full or partial execution result.
+     */
+    public String getResponse() {
+        return mResponse;
+    }
+
+    /**
+     * Returns an error code if the model failed to execute, {@code Optional.empty()} otherwise.
+     *
+     * @return A value from {@link ExecutionError} if the model failed to execute, empty optional
+     *     otherwise.
+     */
+    public @ExecutionError Optional<Integer> getErrorCode() {
+        return mErrorCode;
+    }
+
+    /**
+     * Returns whether this is a complete response or part of a stream.
+     *
+     * @return
+     */
+    public boolean isCompleteResult() {
+        return mIsCompleteResult;
+    }
+
+    private String mResponse;
+
+    @ExecutionError private Optional<Integer> mErrorCode;
+    private boolean mIsCompleteResult;
+}
