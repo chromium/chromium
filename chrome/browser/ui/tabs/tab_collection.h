115f1b2b4b72dbfa2f7e8254aa1c1b6233cb685e
Shibalik Mohapatra <shibalik@chromium.org>
2024-02-22 03:52:27 +0000

    [TabStripRefactor] Implement TabCollection interface.
    Change-Id: I36f6f984f1f5c7d33bff7fee93f144eb91b3c0d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311073
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1263778}

---
+++ chrome/browser/ui/tabs/tab_collection.h
@@ -0,0 +1,63 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_TABS_TAB_COLLECTION_H_
+#define CHROME_BROWSER_UI_TABS_TAB_COLLECTION_H_
+
+#include <cstddef>
+#include <memory>
+#include <optional>
+#include "base/memory/raw_ptr.h"
+#include "base/types/pass_key.h"
+
+namespace tabs {
+
+class TabModel;
+
+class TabCollection {
+ public:
+  TabCollection();
+  virtual ~TabCollection() = default;
+  TabCollection(const TabCollection&) = delete;
+  TabCollection& operator=(const TabCollection&) = delete;
+
+  // Returns true if the tab collection tree contains the tab.
+  virtual bool ContainsTabRecursive(TabModel* tab_model) const = 0;
+  // Returns true is the tab collection contains the collection. This is a
+  // non-recursive check.
+  virtual bool ContainsCollection(TabCollection* collection) const = 0;
+  // Recursively get the index of the tab_model among all the leaf tab_models.
+  virtual std::optional<size_t> GetIndexOfTabRecursive(
+      TabModel* tab_model) const = 0;
+  // Non-recursively get the index of a collection.
+  virtual std::optional<size_t> GetIndexOfCollection(
+      TabCollection* collection) const = 0;
+
+  virtual size_t ChildCount() const = 0;
+  // Total number of tabs the collection contains.
+  virtual size_t TabCountRecursive() const = 0;
+
+  [[nodiscard]] virtual std::unique_ptr<TabModel> MaybeRemoveTab(
+      TabModel* tab) = 0;
+  [[nodiscard]] virtual std::unique_ptr<TabCollection> MaybeRemoveCollection(
+      TabCollection* collection) = 0;
+
+  TabCollection* GetParentCollection() const { return parent_; }
+  // This should be called either when this collection is added to another
+  // collection or it is removed from another collection. The child collection
+  // should not try to call this internally and set its parent.
+  void OnReparented(TabCollection* new_parent) { parent_ = new_parent; }
+
+ protected:
+  base::PassKey<TabCollection> GetPassKey() {
+    return base::PassKey<TabCollection>();
+  }
+
+ private:
+  raw_ptr<TabCollection> parent_ = nullptr;
+};
+
+}  // namespace tabs
+
+#endif  // CHROME_BROWSER_UI_TABS_TAB_COLLECTION_H_
