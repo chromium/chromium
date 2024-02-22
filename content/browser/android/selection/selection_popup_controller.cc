78597e6875d70b73e80703e6fed0fce632af4af2
Bo Liu <boliu@chromium.org>
2024-02-22 18:13:16 +0000

    m123: Add back base::Feature kAndroidSurfaceControlMagnifier
    Add as enabled-by-default so this CL is a no-op. Policy is to remove
    feature 2 milestones after launch, and turned out to be useful for this
    particular launch.
    
    (cherry picked from commit effe88aad6fc8c57c13380fe3414a98efff84a10)
    
    Bug: 40249766
    Change-Id: I8ff792603a6d3ab5c5a7cb107a02e7bc18a33e3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5314327
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Bo Liu <boliu@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1263595}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318310
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6312@{#53}
    Cr-Branched-From: 6711dcdae48edaf98cbc6964f90fac85b7d9986e-refs/heads/main@{#1262506}

--- content/browser/android/selection/selection_popup_controller.cc
+++ content/browser/android/selection/selection_popup_controller.cc
@@ -9,6 +9,7 @@
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
+#include "base/feature_list.h"
 #include "content/browser/android/selection/composited_touch_handle_drawable.h"
 #include "content/browser/gpu/gpu_data_manager_impl.h"
 #include "content/browser/renderer_host/render_widget_host_view_android.h"
@@ -51,12 +52,18 @@ bool IsOffsetAdjustValid(
          endOffset + result->extended_end_adjust <= surroundingTextLength;
 }
 
+BASE_FEATURE(kAndroidSurfaceControlMagnifier,
+             "AndroidSurfaceControlMagnifier",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace
 
 namespace {
 
 bool IsAndroidSurfaceControlMagnifierEnabled() {
-  static bool enabled = gfx::SurfaceControl::SupportsSurfacelessControl();
+  static bool enabled =
+      gfx::SurfaceControl::SupportsSurfacelessControl() &&
+      base::FeatureList::IsEnabled(kAndroidSurfaceControlMagnifier);
   return enabled;
 }
 
