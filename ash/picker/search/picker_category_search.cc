6ffacd04bbdce0def508630c2bccaa36f9099477
Darren Shen <shend@chromium.org>
2024-02-22 23:34:24 +0000

    Use case-insensitive substring matching for categories.
    Bug: b/325973235
    Change-Id: I5a07cc66eec24ede771768969839442e8c033b3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5320029
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264308}

--- ash/picker/search/picker_category_search.cc
+++ ash/picker/search/picker_category_search.cc
@@ -4,22 +4,36 @@
 
 #include "ash/picker/search/picker_category_search.h"
 
-#include <string>
+#include <string_view>
 #include <vector>
 
 #include "ash/picker/views/picker_strings.h"
 #include "ash/public/cpp/picker/picker_category.h"
 #include "ash/public/cpp/picker/picker_search_result.h"
+#include "base/check.h"
+#include "base/ranges/algorithm.h"
+#include "base/strings/string_util.h"
 
 namespace ash {
+namespace {
+
+bool ContainsCaseInsensitive(std::u16string_view haystack,
+                             std::u16string_view needle) {
+  return base::ranges::search(haystack, needle,
+                              base::CaseInsensitiveCompareASCII<char>()) !=
+         haystack.end();
+}
+
+}  // namespace
 
 std::vector<PickerSearchResult> PickerCategorySearch(
     base::span<const PickerCategory> categories,
     std::u16string_view query) {
-  // TODO: b/325973235 - Search using //chromeos/ash/components/string_matching
+  CHECK(!query.empty());
+
   std::vector<PickerSearchResult> matches;
   for (const PickerCategory category : categories) {
-    if (query == GetLabelForPickerCategory(category)) {
+    if (ContainsCaseInsensitive(GetLabelForPickerCategory(category), query)) {
       matches.push_back(PickerSearchResult::Category(category));
     }
   }
--- ash/picker/search/picker_category_search_unittest.cc
+++ ash/picker/search/picker_category_search_unittest.cc
@@ -4,6 +4,9 @@
 
 #include "ash/picker/search/picker_category_search.h"
 
+#include <string>
+#include <vector>
+
 #include "ash/public/cpp/picker/picker_category.h"
 #include "ash/public/cpp/picker/picker_search_result.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -12,28 +15,77 @@
 namespace ash {
 namespace {
 
+using ::testing::Eq;
 using ::testing::Field;
 using ::testing::IsEmpty;
+using ::testing::Pointwise;
 using ::testing::Property;
 using ::testing::VariantWith;
 
-TEST(PickerCategorySearchTest, UnavailableCategory) {
-  EXPECT_THAT(PickerCategorySearch({}, u"Emoji"), IsEmpty());
+MATCHER(ResultMatchesCategory, "") {
+  const auto& [result, category] = arg;
+  return ExplainMatchResult(
+      Property("data", &PickerSearchResult::data,
+               VariantWith<PickerSearchResult::CategoryData>(Field(
+                   "category", &PickerSearchResult::CategoryData::category,
+                   category))),
+      result, result_listener);
 }
 
-TEST(PickerCategorySearchTest, ExactMatch) {
-  EXPECT_THAT(PickerCategorySearch({{PickerCategory::kEmojis}}, u"Emojis"),
-              ElementsAre(Property(
-                  "data", &PickerSearchResult::data,
-                  VariantWith<PickerSearchResult::CategoryData>(Field(
-                      "category", &PickerSearchResult::CategoryData::category,
-                      PickerCategory::kEmojis)))));
+struct TestCase {
+  std::vector<PickerCategory> available_categories;
+  std::u16string query;
+  std::vector<PickerCategory> expected_categories;
+};
+
+class PickerCategorySearchTest : public testing::TestWithParam<TestCase> {};
+
+TEST_P(PickerCategorySearchTest, MatchesExpectedCategories) {
+  const auto& [available_categories, query, expected_categories] = GetParam();
+  EXPECT_THAT(PickerCategorySearch(available_categories, query),
+              Pointwise(ResultMatchesCategory(), expected_categories));
 }
 
-TEST(PickerCategorySearchTest, NoMatch) {
-  EXPECT_THAT(PickerCategorySearch({{PickerCategory::kEmojis}}, u"NotEmoji"),
-              IsEmpty());
-}
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    PickerCategorySearchTest,
+    testing::Values(
+        // Exact match
+        TestCase{
+            .available_categories = {PickerCategory::kEmojis},
+            .query = u"Emojis",
+            .expected_categories = {PickerCategory::kEmojis},
+        },
+        // Case-insensitive match
+        TestCase{
+            .available_categories = {PickerCategory::kEmojis},
+            .query = u"eMoJiS",
+            .expected_categories = {PickerCategory::kEmojis},
+        },
+        // Prefix match
+        TestCase{
+            .available_categories = {PickerCategory::kEmojis},
+            .query = u"e",
+            .expected_categories = {PickerCategory::kEmojis},
+        },
+        // Substring match
+        TestCase{
+            .available_categories = {PickerCategory::kEmojis},
+            .query = u"moj",
+            .expected_categories = {PickerCategory::kEmojis},
+        },
+        // Category unavailable
+        TestCase{
+            .available_categories = {PickerCategory::kBrowsingHistory},
+            .query = u"Emojis",
+            .expected_categories = {},
+        },
+        // Not matched
+        TestCase{
+            .available_categories = {PickerCategory::kEmojis},
+            .query = u"emoji1",
+            .expected_categories = {},
+        }));
 
 }  // namespace
 }  // namespace ash
