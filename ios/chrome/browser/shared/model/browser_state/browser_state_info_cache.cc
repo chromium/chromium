e2a247f5894a84471bb4f90e65cac9737c71ae72
Federica Germinario <fedegermi@google.com>
2024-02-23 12:42:45 +0000

    [ios] Correctly update key kBrowserStatesLastActive
    This key was never setup in code, this CL makes sure that the content
    is always updated.
    The key will be used in a follow up CL.
    
    Bug: 325923177
    Change-Id: Icc52dbd59817cdbc22fe52b5728f8780b35bd47f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318110
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264500}

--- ios/chrome/browser/shared/model/browser_state/browser_state_info_cache.cc
+++ ios/chrome/browser/shared/model/browser_state/browser_state_info_cache.cc
@@ -11,6 +11,7 @@
 
 #include "base/check_op.h"
 #include "base/i18n/case_conversion.h"
+#include "base/json/values_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
@@ -19,7 +20,7 @@
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/scoped_user_pref_update.h"
 #include "ios/chrome/browser/shared/model/browser_state/browser_state_info_cache_observer.h"
-#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
+#include "ios/chrome/browser/shared/model/prefs/pref_names.h"
 
 namespace {
 const char kGAIAIdKey[] = "gaia_id";
@@ -53,6 +54,12 @@ void BrowserStateInfoCache::AddBrowserState(
       prefs_->GetInteger(prefs::kBrowserStatesNumCreated);
   prefs_->SetInteger(prefs::kBrowserStatesNumCreated, browser_states_count + 1);
 
+  base::Value::List last_active_browser_states =
+      prefs_->GetList(prefs::kBrowserStatesLastActive).Clone();
+  last_active_browser_states.Append(browser_state_path.BaseName().value());
+  prefs_->SetList(prefs::kBrowserStatesLastActive,
+                  std::move(last_active_browser_states));
+
   base::Value::Dict info;
   info.Set(kGAIAIdKey, gaia_id);
   info.Set(kUserNameKey, user_name);
@@ -79,6 +86,14 @@ void BrowserStateInfoCache::RemoveBrowserState(
       prefs_->GetInteger(prefs::kBrowserStatesNumCreated);
   DCHECK_GE(browser_states_count, 1);
   prefs_->SetInteger(prefs::kBrowserStatesNumCreated, browser_states_count - 1);
+
+  base::Value::List last_active_browser_states =
+      prefs_->GetList(prefs::kBrowserStatesLastActive).Clone();
+  const base::Value browser_state_to_remove =
+      FilePathToValue(browser_state_path.BaseName());
+  last_active_browser_states.EraseValue(browser_state_to_remove);
+  prefs_->SetList(prefs::kBrowserStatesLastActive,
+                  std::move(last_active_browser_states));
 
   std::string key = CacheKeyFromBrowserStatePath(browser_state_path);
   cache.Remove(key);
