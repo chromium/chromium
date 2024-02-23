07f7b37a91f5b739520410d5c6f9612dd629cc0c
Ari Chivukula <arichiv@chromium.org>
2024-02-23 21:08:28 +0000

    [BREFs] Fix error when mixing meta/header tokens
    BREFs doesn't support header tokens, but this means we failed to test
    what happens when meta and header tokens are mixed. As it turns out,
    what happens it that the origin_trial_state_host_remote_ that's bound
    when processing header tokens expires and future calls fail. We can
    prevent this by checking if the remote isn't connected and resetting it
    if so.
    
    Bug: 326458800, 40243430
    Change-Id: I2fd0f51466cdbef0923db14ef71f534047c54546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318841
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264741}

---
+++ third_party/blink/web_tests/wpt_internal/cookies/dt-coexist.sp_meta.cp_header.sub.https.window.js
@@ -0,0 +1,40 @@
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+
+'use strict';
+
+// Here's the set-up for this test:
+// Step 1 (window) Set up listener for "SetData" and "HadData" message.
+// Step 2 (window) Open other window on cross-site origin.
+// Step 3 (other-window) Set cookies and local storage.
+// Step 4 (other-window) Send "SetData" message.
+// Step 5 (window) Embed iframe on cross-site origin.
+// Step 6 (iframe) Check for cookies and local storage.
+// Step 7 (iframe) Send "HadData" message.
+// Step 8 (window) Cleanup.
+
+async_test(t => {
+    const spMeta = document.createElement('meta');
+    spMeta.httpEquiv = 'origin-trial';
+    // The token below was generated via the following command:
+    // tools/origin_trials/generate_token.py https://web-platform.test:8444 DisableThirdPartyStoragePartitioning --expire-timestamp=2000000000
+    spMeta.content = 'A/ZoX2C4Y7GqRBvcNYxeXkxquUdlGfuEF0r03N4M7+RsJA1fINcuOjrLCTJ0QNK1nmzogxeZ3okd9c0kpdW0fAQAAAB1eyJvcmlnaW4iOiAiaHR0cHM6Ly93ZWItcGxhdGZvcm0udGVzdDo4NDQ0IiwgImZlYXR1cmUiOiAiRGlzYWJsZVRoaXJkUGFydHlTdG9yYWdlUGFydGl0aW9uaW5nIiwgImV4cGlyeSI6IDIwMDAwMDAwMDB9';
+    document.head.append(spMeta);
+
+    // Step 1
+    window.addEventListener("message", t.step_func(e => {
+        if (e.data.type == "SetData") {
+            // Step 5
+            let iframe = document.createElement("iframe");
+            iframe.src = "https://{{hosts[alt][]}}:{{ports[https][0]}}/wpt_internal/cookies/resources/iframe.html";
+            document.body.appendChild(iframe);
+        } else if (e.data.type == "HadData") {
+            // Step 8
+            assert_equals(e.data.message, "Cookie,LocalStorage,", "Unpartitioned data should be accessable");
+            t.done();
+        }
+    }));
+
+    // Step 2
+    window.open("https://{{hosts[alt][]}}:{{ports[https][0]}}/wpt_internal/cookies/resources/window.html");
+}, "Verify origin SP Meta + CP Header Deprecation Trials can coexist");
