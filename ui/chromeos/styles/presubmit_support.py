a6cceff5cac1b8495a4fa279683df6d48cd3f3ee
Sean Kau <skau@chromium.org>
2024-02-24 02:45:17 +0000

    Add tokens for GM3 Shadows in CSS
    Adds tokens for cros.sys.app-elevation{1,3,5} in CSS. Tokens appear
    as --cros-sys-app-elevation-{1,3,5}-shadow and are appropriate for use
    in box-shadow.
    
    Bug: b:318010393
    Change-Id: I5ba5029372717d49ed7c5843f10ab696f061c375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5155263
    Reviewed-by: Zain Afzal <zafzal@google.com>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Commit-Queue: Zain Afzal <zafzal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1264844}

--- ui/chromeos/styles/presubmit_support.py
+++ ui/chromeos/styles/presubmit_support.py
@@ -52,6 +52,7 @@ def _CheckSemanticColors(input_api, output_api):
         input_api.os_path.join(*style_root, 'cros_shadows.json5'),
         input_api.os_path.join(*style_root, 'cros_sys_colors.json5'),
         input_api.os_path.join(*style_root, 'cros_typography.json5'),
+        input_api.os_path.join(*style_root, 'cros_gm3_shadows.json5'),
     ]
 
     cros_prefixes, cros_variables = ExtractPrefixesAndVariables(cros_styles)
