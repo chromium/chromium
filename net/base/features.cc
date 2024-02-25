2714aae63da2f35b29e24ab2469faadf65e25c71
Patrick Meenan <pmeenan@chromium.org>
2024-02-25 20:11:03 +0000

    Enable the PriorityHeader feature by default
    The experiment has completed successfully and the feature has been
    approved to launch in Chrome 124:
    https://chromestatus.com/feature/5109106573049856
    
    The feature flag itself is still in place as a killswitch and will be
    removed at a later date (at which point the unit tests will also be
    updated to stop testing both with and without the header).
    
    Bug: 40252001
    Change-Id: Ib2324b8ad5648fa1ca9292a11e15189a9b4d307d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5318780
    Commit-Queue: Patrick Meenan <pmeenan@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1264979}

--- net/base/features.cc
+++ net/base/features.cc
@@ -489,7 +489,7 @@ BASE_FEATURE(kThirdPartyPartitionedStorageAllowedByDefault,
 
 BASE_FEATURE(kPriorityHeader,
              "PriorityHeader",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSpdyHeadersToHttpResponseUseBuilder,
              "SpdyHeadersToHttpResponseUseBuilder",
