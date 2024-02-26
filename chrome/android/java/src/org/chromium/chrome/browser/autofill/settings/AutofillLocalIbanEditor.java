d35e355274efba04b4fd281f1223524f3982b83f
Verina Armanyous <averina@google.com>
2024-02-26 19:05:00 +0000

    [IBAN][CLANK] Part 1: Allow users to add a new IBAN from settings page
    This CL:
    - Adds skeleton for AutofillLocalIbanEditor class
    - Creates an IBAN add view allowing users to add a new IBAN.
    
    A followup CL will incorporate the "Add IBAN" button and the TODOs
    in the AutofillLocalIbanEditor class
    
    design doc: go/autofill-iban-dd-clank
    
    After relation chain: https://screenshot.googleplex.com/7G7QwstNiW6BmeD.png
    Mock:
    https://screenshot.googleplex.com/6Qyvpw4MmSCirXp
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL This CL introduces
    skeleton class and the rest will follow in followup CLs.
    
    Bug: b/309163418
    Change-Id: Ie930e5003c88078b48c5e46949b8bc3879672e57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208071
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265336}

---
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillLocalIbanEditor.java
@@ -0,0 +1,87 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill.settings;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.chromium.build.annotations.UsedByReflection;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.autofill.AutofillEditorBase;
+
+public class AutofillLocalIbanEditor extends AutofillEditorBase {
+    // This class creates a view for adding a local IBAN. A local IBAN gets saved to the
+    // user's device only.
+    protected Button mDoneButton;
+    protected EditText mNickname;
+    protected TextInputLayout mNicknameLabel;
+
+    @UsedByReflection("AutofillPaymentMethodsFragment.java")
+    public AutofillLocalIbanEditor() {}
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO(b/309163678): Disable autofill for the fields.
+        View v = super.onCreateView(inflater, container, savedInstanceState);
+
+        mDoneButton = (Button) v.findViewById(R.id.button_primary);
+        mNickname = (EditText) v.findViewById(R.id.iban_nickname_edit);
+        mNicknameLabel = (TextInputLayout) v.findViewById(R.id.iban_nickname_label);
+
+        mNickname.setOnFocusChangeListener(
+                (view, hasFocus) -> mNicknameLabel.setCounterEnabled(hasFocus));
+
+        initializeButtons(v);
+        return v;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.autofill_local_iban_editor;
+    }
+
+    @Override
+    protected int getTitleResourceId(boolean isNewEntry) {
+        // TODO(b/309163678): Use isNewEntry to decide which title to display
+        // (i.e., autofill_add_local_iban or autofill_edit_local_iban).
+        return R.string.autofill_add_local_iban;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // TODO(b/309163678): Update this once the AutofillIbanEditor class is added.
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        updateSaveButtonEnabled();
+    }
+
+    @Override
+    protected boolean saveEntry() {
+        // TODO(b/320757907): Save IBAN from settings page.
+        return false;
+    }
+
+    @Override
+    protected void deleteEntry() {
+        // TODO(b/309163615): User can delete existing IBANs from settings page.
+    }
+
+    private void updateSaveButtonEnabled() {
+        // Enable save button if IBAN value is valid.
+        // TODO(b/321120072): Verify IBAN value and enable the save button appropriately.
+        mDoneButton.setEnabled(false);
+    }
+}
