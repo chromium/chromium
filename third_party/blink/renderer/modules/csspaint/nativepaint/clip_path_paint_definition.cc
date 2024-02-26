f75681bc2c06cfa202fe71e5829014bd82a7d281
Claire Chambers <clchambers@microsoft.com>
2024-02-26 21:33:41 +0000

    [CompositeClipPathAnimation] Fix crash for global values
    This CL fixes a bug where only 'fixed' falls back to main for composited clip path animation. This makes all global values fall back, to avoid crashing during paint.
    
    Bug: 41493093
    
    Change-Id: Ie510eb3ef2aa22a95192e3107d50af2c69b6e105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5226767
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265455}

--- third_party/blink/renderer/modules/csspaint/nativepaint/clip_path_paint_definition.cc
+++ third_party/blink/renderer/modules/csspaint/nativepaint/clip_path_paint_definition.cc
@@ -11,7 +11,12 @@
 #include "third_party/blink/renderer/core/animation/path_interpolation_functions.h"
 #include "third_party/blink/renderer/core/css/basic_shape_functions.h"
 #include "third_party/blink/renderer/core/css/css_identifier_value.h"
+#include "third_party/blink/renderer/core/css/css_inherited_value.h"
+#include "third_party/blink/renderer/core/css/css_initial_value.h"
+#include "third_party/blink/renderer/core/css/css_revert_layer_value.h"
+#include "third_party/blink/renderer/core/css/css_revert_value.h"
 #include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
+#include "third_party/blink/renderer/core/css/css_unset_value.h"
 #include "third_party/blink/renderer/core/css/css_value_list.h"
 #include "third_party/blink/renderer/core/css/cssom/paint_worklet_deferred_image.h"
 #include "third_party/blink/renderer/core/css/cssom/paint_worklet_input.h"
@@ -150,6 +155,14 @@ scoped_refptr<BasicShape> CreateBasicShape(
       interpolable_value, untyped_non_interpolable_value, conversion_data);
 }
 
+bool CanExtractShapeOrPath(const CSSValue* computed_value) {
+  // TODO(pdr): Support <geometry-box> (alone, or with a shape).
+  if (const auto* list = DynamicTo<CSSValueList>(computed_value)) {
+    return list->First().IsBasicShapeValue() || list->First().IsPathValue();
+  }
+  return false;
+}
+
 scoped_refptr<BasicShape> GetAnimatedShapeFromKeyframe(
     const PropertySpecificKeyframe* frame,
     const KeyframeEffectModelBase* model,
@@ -167,10 +180,9 @@ scoped_refptr<BasicShape> GetAnimatedShapeFromKeyframe(
                              *const_cast<Element*>(element));
 
     // TODO(pdr): Support <geometry-box> (alone, or with a shape).
-    if (const auto* list = DynamicTo<CSSValueList>(computed_value)) {
-      if (list->First().IsBasicShapeValue() || list->First().IsPathValue()) {
-        basic_shape = BasicShapeForValue(state, list->First());
-      }
+    if (CanExtractShapeOrPath(computed_value)) {
+      basic_shape = BasicShapeForValue(
+          state, DynamicTo<CSSValueList>(computed_value)->First());
     }
   } else {
     DCHECK(frame->IsTransitionPropertySpecificKeyframe());
@@ -198,17 +210,12 @@ bool ValidateClipPathValue(const Element* element,
                            const CSSValue* value,
                            const InterpolableValue* interpolable_value) {
   if (value) {
-    auto* identifier_value = DynamicTo<CSSIdentifierValue>(value);
-    // Don't try to composite animations with clip-path: none, as this is not
-    // compatible with the method used to paint composite clip path animations:
-    // A mask image would potentially clip content unless if it was the size of
-    // the entire viewport.
-    if (identifier_value &&
-        identifier_value->GetValueID() == CSSValueID::kNone) {
-      return false;
+    // Don't try to composite animations where we can't extract a shape or path
+    if (CanExtractShapeOrPath(value)) {
+      return true;
     }
 
-    return true;
+    return false;
   } else if (interpolable_value) {
     // There is no need to check for clip-path: none here, as transitions are
     // not defined for this non-interpolable value. See
