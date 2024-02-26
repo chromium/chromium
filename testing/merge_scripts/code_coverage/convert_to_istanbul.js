80bac52364f419b306071f142bcf089bce336cc4
Srinivas Hegde <srinivashegde@google.com>
2024-02-26 20:46:22 +0000

    js-coverage: Parallelize V8 to istanbul conversion.
    It takes ~35m for converting all the coverage directories. Parallelize
    this operation to make it faster.
    
    Bug: b:315181254
    Test: Ran both chromeos and linux rel try jobs to confirm
    Change-Id: If33d0cdb2a4d38cfde896002f14419fc47fad0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5322018
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: Srinivas Hegde <srinivashegde@google.com>
    Commit-Queue: Prakhar Asthana <pasthana@google.com>
    Reviewed-by: Prakhar Asthana <pasthana@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265418}

--- testing/merge_scripts/code_coverage/convert_to_istanbul.js
+++ testing/merge_scripts/code_coverage/convert_to_istanbul.js
@@ -11,155 +11,28 @@
 const NODE_MODULES = [
   '..', '..', '..', 'third_party', 'js_code_coverage', 'node_modules'];
 
-const {createHash} = require('crypto');
-const {join, dirname, normalize} = require('path');
-const {readdir, readFile, writeFile, mkdir, access} = require('fs').promises;
-const V8ToIstanbul = require(join(...NODE_MODULES, 'v8-to-istanbul'));
+const {Worker} = require('worker_threads');
+const {join} = require('path');
+const {readFile, mkdir} = require('fs').promises;
 const {ArgumentParser} = require(join(...NODE_MODULES, 'argparse'));
-const convertSourceMap = require(join(...NODE_MODULES, 'convert-source-map'));
-const sourceMap = require(join(...NODE_MODULES, 'source-map'));
 
-/**
- * Validate that the mapping in the sourcemaps is valid.
- * @param {Object} mapping Individual mapping to validate.
- * @param {Map} sourcesMap Map of the sources in the mappings to it's content.
- * @param {string} instrumentedFilePath Path to the instrumented file.
- * @returns {boolean} true if mapping is valid, false otherwise.
- */
-function validateMapping(mapping, sourcesMap, instrumentedFilePath) {
-  if (!mapping.generatedLine || !mapping.originalLine || !mapping.source) {
-    console.log(`Invalid mapping found for ${instrumentedFilePath}`);
-    return false;
-  }
-
-  // Verify that we have file contents.
-  if (!sourcesMap[mapping.source]) {
-    return false;
-  }
-
-  // Verify that the mapping line numbers refers to actual lines in source.
-  const origLine = sourcesMap[mapping.source][mapping.originalLine-1];
-  const genLine = sourcesMap[instrumentedFilePath][mapping.generatedLine-1];
-  if (origLine === undefined || genLine === undefined) {
-    return false;
-  }
-
-  // Verify that the mapping columns refers to actual column bounds in source.
-  if (mapping.generatedColumn > genLine.length ||
-      mapping.originalColumn > origLine.length) {
-    return false;
-  }
-
-  return true;
-}
-
-/**
- * Validate the sourcemap by looking at:
- * 1. Existence of the source files in sourcemap
- * 2. Verify original and generated lines are within bounds.
- * @param {string} instrumentedFilePath Path to the file with source map.
- * @returns {boolean} true if sourcemap is valid, false otherwise
- */
-async function validateSourceMaps(instrumentedFilePath) {
-  const rawSource = await readFile(instrumentedFilePath, 'utf8');
-  const rawSourceMap = convertSourceMap.fromSource(rawSource) ||
-      convertSourceMap.fromMapFileSource(
-        rawSource, dirname(instrumentedFilePath));
-
-  if (!rawSourceMap || rawSourceMap.sourcemap.sources.length < 1) {
-    console.log(`No valid source map found for ${instrumentedFilePath}`);
-    return false;
-  }
-
-  let sourcesMap = {};
-  sourcesMap[instrumentedFilePath] = rawSource.toString().split('\n');
-  for (let i = 0; i < rawSourceMap.sourcemap.sources.length; i++) {
-    const sourcePath = normalize(join(
-        rawSourceMap.sourcemap.sourceRoot, rawSourceMap.sourcemap.sources[i]));
-    try {
-      const content = await readFile(sourcePath, 'utf-8');
-      sourcesMap[sourcePath] = content.toString().split('\n');
-    } catch (error) {
-      if (error.code === 'ENOENT') {
-        console.error(`Original missing for ${sourcePath}`);
-        return false;
-      } else {
-        throw error;
-      }
-    }
-  }
-
-  let validMap = true;
-  const consumer =
-      await new sourceMap.SourceMapConsumer(rawSourceMap.sourcemap);
-  consumer.eachMapping(function(mapping) {
-    if (!validMap ||
-      !validateMapping(mapping, sourcesMap, instrumentedFilePath)) {
-      validMap = false;
-    }
+function createWorker(coverageDir, sourceDir, outputDir, urlToPathMap) {
+  return new Promise(function(resolve, reject) {
+    const worker = new Worker(join(__dirname, 'coverage_worker.js'), {
+      workerData: {
+        coverageDir: coverageDir,
+        sourceDir: sourceDir,
+        outputDir: outputDir,
+        urlToPathMap: urlToPathMap
+      },
+    });
+    worker.on('message', (data) => {
+      resolve(data);
+    });
+    worker.on('error', (msg) => {
+      reject(`An error ocurred: ${msg}`);
+    });
   });
-
-  // Destroy consumer as we dont need it anymore.
-  consumer.destroy();
-  return validMap;
-}
-
-/**
- * Extracts the raw coverage data from the v8 coverage reports and converts
- * them into IstanbulJS compliant reports.
- * @param {string} coverageDirectory Directory containing the raw v8 output.
- * @param {string} instrumentedDirectoryRoot Directory containing the source
- *    files where the coverage was instrumented from.
- * @param {string} outputDir Directory to store the istanbul coverage reports.
- * @param {!Map<string, string>} urlToPathMap A mapping of URL observed during
- *    test execution to the on-disk location created in previous steps.
- */
-async function extractCoverage(
-    coverageDirectory, instrumentedDirectoryRoot, outputDir, urlToPathMap) {
-  const coverages = await readdir(coverageDirectory);
-
-  for (const fileName of coverages) {
-    if (!fileName.endsWith('.cov.json'))
-      continue;
-
-    const filePath = join(coverageDirectory, fileName);
-    const contents = await readFile(filePath, 'utf-8');
-
-    const {result: scriptCoverages} = JSON.parse(contents);
-    if (!scriptCoverages)
-      throw new Error(`result key missing for file: ${filePath}`);
-
-    for (const coverage of scriptCoverages) {
-      if (!urlToPathMap[coverage.url])
-        continue;
-
-      const instrumentedFilePath =
-          join(instrumentedDirectoryRoot, urlToPathMap[coverage.url]);
-      const validSourceMap = await validateSourceMaps(instrumentedFilePath)
-      if (!validSourceMap) {
-        continue;
-      }
-
-      const converter = V8ToIstanbul(instrumentedFilePath);
-      await converter.load();
-      converter.applyCoverage(coverage.functions);
-      const convertedCoverage = converter.toIstanbul();
-
-      const jsonString = JSON.stringify(convertedCoverage);
-      await writeFile(
-          join(outputDir, createSHA1HashFromFileContents(jsonString) + '.json'),
-          jsonString);
-    }
-  }
-}
-
-/**
- * Helper function to provide a unique file name for resultant istanbul reports.
- * @param {string} str File contents
- * @return {string} A sha1 hash to be used as a file name.
- */
-function createSHA1HashFromFileContents(contents) {
-  return createHash('sha1').update(contents).digest('hex');
 }
 
 /**
@@ -171,13 +44,17 @@ async function main(args) {
   const urlToPathMapFile =
       await readFile(join(args.source_dir, 'parsed_scripts.json'));
   const urlToPathMap = JSON.parse(urlToPathMapFile.toString());
-
   const outputDir = join(args.output_dir, 'istanbul')
   await mkdir(outputDir, {recursive: true});
-  for (const coverageDir of args.raw_coverage_dirs)
-    await extractCoverage(
-        coverageDir, args.source_dir, outputDir, urlToPathMap);
 
+  const workerPromises = [];
+  for (const coverageDir of args.raw_coverage_dirs) {
+    workerPromises.push(
+        createWorker(coverageDir, args.source_dir, outputDir, urlToPathMap));
+  }
+
+  const results = await Promise.all(workerPromises);
+  console.log(`Result from workers: ${results}`)
   return outputDir;
 }
 
