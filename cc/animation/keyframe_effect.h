2d291d41530577dec04d5b893b7f4b4ef4dd560a
David Bokan <bokan@chromium.org>
2024-02-26 16:33:07 +0000

    [Merge M-123] animation: fix infinitely growing models list
    https://crrev.com/c/4605129 made it so that an updated animation in
    Blink resulting in a cancel/re-create of a compositor animation would
    reuse the compositor animation's id and group, causing it to push new
    properties to the existing compositor-thread animation, rather than
    creating a new animation. While this preserves the Animation and
    KeyframeEffect, it pushes new KeyframeModels - removing the old ones.
    
    However, grouped KeyframeModels will not be deleted until every model in
    the group is finished. Since the incoming KeyframeModels have the same
    group as the to-be-removed models, this doesn't happen until the new
    models complete. However, some animations may never complete, leading to
    unbounded growth.
    
    This CL fixes the issue by setting the replaced group_id on a
    KeyframeEffect performing this replacement. When the incoming keyframe
    models are activated, the completed ones, matching the KeyframeEffect's
    replaced group_id, have their group id removed. This allows them to be
    deleted immediately and also prevents the model from dispatching a
    finished event, since it's conceptually been "updated" rather than
    deleted.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:cc/animation/keyframe_effect.cc;l=1024;drc=07bd1fe6394d0575e95b8211c872d851784f1d66
    
    (cherry picked from commit 7e07e06cb1f8a579e64c19c7174e8532e1550eb6)
    
    Change-Id: I5783b92285997a3a90cbb6563412f768f8978774
    Bug: 323708859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5300130
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1264266}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323799
    Auto-Submit: David Bokan <bokan@chromium.org>
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6312@{#151}
    Cr-Branched-From: 6711dcdae48edaf98cbc6964f90fac85b7d9986e-refs/heads/main@{#1262506}

--- cc/animation/keyframe_effect.h
+++ cc/animation/keyframe_effect.h
@@ -166,6 +166,10 @@ class CC_ANIMATION_EXPORT KeyframeEffect : public gfx::KeyframeEffect {
 
   bool awaiting_deletion() { return awaiting_deletion_; }
 
+  void set_replaced_group(int replaced_group) {
+    replaced_group_ = replaced_group;
+  }
+
  protected:
   // We override this because we have additional bookkeeping (eg, noting if
   // we've aborted a scroll animation, updating ticking state, sending updates
@@ -198,6 +202,11 @@ class CC_ANIMATION_EXPORT KeyframeEffect : public gfx::KeyframeEffect {
 
   // element_animations_ is non-null if controller is attached to an element.
   scoped_refptr<ElementAnimations> element_animations_;
+
+  // When an animation is being replaced (see is_replaced_ in animation.h),
+  // this is set to the animation group being replaced. Set only on the
+  // impl-side KeyframeEffect.
+  std::optional<int> replaced_group_;
 
   // Only try to start KeyframeModels when new keyframe models are added or
   // when the previous attempt at starting KeyframeModels failed to start all
