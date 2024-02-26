e1f14074604434aea8e230772d7999f4ca4787f7
Wayne Jackson Jr. <wbjacksonjr@google.com>
2024-02-26 11:59:46 +0000

    [webview][autofill] Fix Autofill Action Menu Item Click Handling
    Two issues were present with the auto-fill action menu:
    
    1. The click handling didn't propagate.
    2. The menu didn't dismiss.
    
    1 was caused because the menu item click listener was being cleared
    out in `onPrepareActionMode()`.
    
    Both issues have now been fixed.
    
    Additional note: Auto-fill already worked as expected in the
    drop-down text selection menu.
    
    Bug: 307854957
    Change-Id: I9cf39f3fc3eaf9d3462eb8b3f2af5872f9f84c09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317882
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265141}

--- content/public/android/java/src/org/chromium/content/browser/selection/FloatingPastePopupMenu.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/FloatingPastePopupMenu.java
@@ -133,13 +133,13 @@ public class FloatingPastePopupMenu implements PastePopupMenu {
             SortedSet<SelectionMenuGroup> nonSelectionMenuItems =
                     SelectActionMenuHelper.getNonSelectionMenuItems(
                             mContext, actionMenuDelegate, mSelectionActionMenuDelegate);
+            mCustomMenuItemClickListeners.clear();
             SelectionPopupControllerImpl.initializeActionMenu(
                     mContext, nonSelectionMenuItems, menu, mCustomMenuItemClickListeners, null);
         }
 
         @Override
         public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-            mCustomMenuItemClickListeners.clear();
             return false;
         }
 
@@ -149,6 +149,7 @@ public class FloatingPastePopupMenu implements PastePopupMenu {
                     mCustomMenuItemClickListeners.get(item);
             if (customMenuItemClickListener != null) {
                 customMenuItemClickListener.onClick(mParent);
+                mode.finish();
             } else {
                 int id = item.getItemId();
                 if (id == R.id.select_action_menu_paste) {
