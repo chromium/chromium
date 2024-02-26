dba0dcbed238e5ecc3b788142147bf83a65e46cb
Toni Barzic <tbarzic@chromium.org>
2024-02-26 19:56:12 +0000

    launcher: Fix crash in TopIconAnimationView
    Don't update icon background layer color if layer was not created.
    Depending on whether the animation view animates an icon that's shown
    within the folder icon or not, the icon background may not have a layer.
    When animating a view that doesn't animate into the folder icon, the
    background color does not animate differently from the rest of the icon
    so no extra layer is needed, so the layer does not exist.
    
    BUG=b/325103208
    
    Change-Id: I735d0dddd8f98f6142caf43d5e89fb5ef1a0f367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324338
    Reviewed-by: Ana Salazar <anasalazar@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265382}

--- ash/app_list/views/top_icon_animation_view.cc
+++ ash/app_list/views/top_icon_animation_view.cc
@@ -231,7 +231,7 @@ gfx::Size TopIconAnimationView::CalculatePreferredSize() const {
 void TopIconAnimationView::OnThemeChanged() {
   views::View::OnThemeChanged();
 
-  if (icon_background_) {
+  if (icon_background_ && icon_background_->layer()) {
     icon_background_->layer()->SetColor(
         GetColorProvider()->GetColor(cros_tokens::kCrosSysSystemOnBaseOpaque));
   }
