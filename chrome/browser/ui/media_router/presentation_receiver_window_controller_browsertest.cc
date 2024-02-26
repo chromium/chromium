dfd979c87d4a78e35cfb2d6ecaf7ce9204c4fe83
Georg Neis <neis@chromium.org>
2024-02-26 02:17:17 +0000

    Reenable PresentationReceiverWindowControllerBrowserTest.CreatesWindow
    ... on Lacros, after replacing a RunUntilIdle use with a RunUntil use.
    With this change I'm no longer able to trigger a failure.
    
    Bug: 40732305
    Change-Id: Ifae147766b0d0a90695e0196ec64b63bb2a2b73d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5309018
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265019}

--- chrome/browser/ui/media_router/presentation_receiver_window_controller_browsertest.cc
+++ chrome/browser/ui/media_router/presentation_receiver_window_controller_browsertest.cc
@@ -11,6 +11,7 @@
 #include "base/functional/bind.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "base/test/run_until.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/timer/elapsed_timer.h"
 #include "chrome/browser/profiles/profile.h"
@@ -166,14 +167,8 @@ class PresentationReceiverWindowControllerBrowserTest
   }
 };
 
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-// TODO(crbug.com/1424970): Re-enable on lacros.
-#define MAYBE_CreatesWindow DISABLED_CreatesWindow
-#else
-#define MAYBE_CreatesWindow CreatesWindow
-#endif
 IN_PROC_BROWSER_TEST_F(PresentationReceiverWindowControllerBrowserTest,
-                       MAYBE_CreatesWindow) {
+                       CreatesWindow) {
   ReceiverWindowDestroyer destroyer;
   auto receiver_window =
       PresentationReceiverWindowController::CreateFromOriginalProfile(
@@ -182,9 +177,8 @@ IN_PROC_BROWSER_TEST_F(PresentationReceiverWindowControllerBrowserTest,
                          base::Unretained(&destroyer)),
           GetNoopTitleChangeCallback());
   receiver_window->Start(kPresentationId, GURL("about:blank"));
-  base::RunLoop().RunUntilIdle();
-
-  EXPECT_TRUE(IsWindowFullscreen(*receiver_window));
+  EXPECT_TRUE(base::test::RunUntil(
+      [&]() { return IsWindowFullscreen(*receiver_window); }));
 
   destroyer.AwaitTerminate(std::move(receiver_window));
 }
