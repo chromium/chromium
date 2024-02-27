bc623f34880443753eb2041c7569ec29e65745db
Brian Begnoche <bcb@chromium.org>
2024-02-27 17:46:19 +0000

    Disable flaky BackForwardCacheBrowserTest.FiresDidFirstVisuallyNonEmptyPaintWhenRestoredFromCache in content_browsertests
    Bug: 327195951
    Change-Id: If017710c8fd381faf593636ade2f7643739512c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5327730
    Auto-Submit: Brian Begnoche <bcb@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Owners-Override: Brian Begnoche <bcb@chromium.org>
    Commit-Queue: John Abd-El-Malek <jam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265888}

--- content/browser/back_forward_cache_browsertest.cc
+++ content/browser/back_forward_cache_browsertest.cc
@@ -1178,9 +1178,17 @@ IN_PROC_BROWSER_TEST_F(
 }
 
 // Make sure we fire DidFirstVisuallyNonEmptyPaint when restoring from bf-cache.
+// TODO(crbug.com/327195951): Re-enable this test
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+#define MAYBE_FiresDidFirstVisuallyNonEmptyPaintWhenRestoredFromCache \
+  DISABLED_FiresDidFirstVisuallyNonEmptyPaintWhenRestoredFromCache
+#else
+#define MAYBE_FiresDidFirstVisuallyNonEmptyPaintWhenRestoredFromCache \
+  FiresDidFirstVisuallyNonEmptyPaintWhenRestoredFromCache
+#endif
 IN_PROC_BROWSER_TEST_F(
     BackForwardCacheBrowserTest,
-    FiresDidFirstVisuallyNonEmptyPaintWhenRestoredFromCache) {
+    MAYBE_FiresDidFirstVisuallyNonEmptyPaintWhenRestoredFromCache) {
   ASSERT_TRUE(embedded_test_server()->Start());
   GURL url_a(embedded_test_server()->GetURL("a.com", "/title1.html"));
   GURL url_b(embedded_test_server()->GetURL("b.com", "/title1.html"));
