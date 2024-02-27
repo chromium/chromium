226f892863b5dbea2122cde937ecd398f540a4c7
Mark Schillaci <mschillaci@google.com>
2024-02-27 15:27:38 +0000

    [Page Zoom] Reset user preferences for zoom related Android settings
    This CL uses new strings for the keys of zoom related user settings on
    Android. This will reset all saved user preferences to start a rollout
    without any potential stale state.
    
    AX-Relnotes: N/A
    Bug: 326970610
    Change-Id: I76004b7c447b9622a3e3a5705082e32d08dd1b04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324271
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1265802}

--- components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/AccessibilityConstants.java
+++ components/browser_ui/accessibility/android/java/src/org/chromium/components/browser_ui/accessibility/AccessibilityConstants.java
@@ -7,11 +7,11 @@ package org.chromium.components.browser_ui.accessibility;
 /** Constants used for accessibility classes. */
 public final class AccessibilityConstants {
     /**
-     * Page Zoom feature preferences. Tracks if a user wants the menu item always visible, and
-     * what their default level of zoom should be.
+     * Page Zoom feature preferences. Tracks if a user wants the menu item always visible, and what
+     * their default level of zoom should be.
      */
     public static final String PAGE_ZOOM_ALWAYS_SHOW_MENU_ITEM =
-            "Chrome.PageZoom.AlwaysShowMenuItem";
+            "Chrome.PageZoom.AlwaysShowMenuItem.Refreshed";
 
     /** Tracks if a user wants Page Zoom to include an OS level adjustment for zoom level. */
     public static final String PAGE_ZOOM_INCLUDE_OS_ADJUSTMENT =
