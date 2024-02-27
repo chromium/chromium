a5daf162ba02d74befd4462f4f166fda80b74216
Alexander Bolodurin <alexbn@chromium.org>
2024-02-27 01:59:46 +0000

    [gardener] disable flaky WebHid test on ChromeOS (Ash)
    Bug: crbug.com/41494522
    Change-Id: I5ef6c8bc3f88e8a93147a8f4a7b5885e8cc8a83f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321373
    Reviewed-by: François Degros <fdegros@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265589}

--- chrome/browser/hid/hid_browsertest.cc
+++ chrome/browser/hid/hid_browsertest.cc
@@ -411,8 +411,8 @@ class WebHidExtensionFeatureDisabledBrowserTest
   }
 };
 
-// TODO(crbug.com/1521554): Re-enable on linux and lacros.
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+// TODO(crbug.com/1521554): Re-enable on linux and ChromeOS.
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_FeatureDisabled DISABLED_FeatureDisabled
 #else
 #define MAYBE_FeatureDisabled FeatureDisabled
