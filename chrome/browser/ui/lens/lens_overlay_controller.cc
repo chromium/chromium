cebfd477c70e7333049fd1867f865b1c8132e4ab
Erik Chen <erikchen@chromium.org>
2024-02-27 23:24:42 +0000

    Add more scaffolding for lens overlay.
    Change-Id: I2ae6eb1e4da9e088609a2ca3871a2a2bad140a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5326299
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266148}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -4,7 +4,34 @@
 
 #include "chrome/browser/ui/lens/lens_overlay_controller.h"
 
+#include "chrome/browser/ui/tabs/tab_model.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+
 LensOverlayController::LensOverlayController(tabs::TabModel* tab_model)
-    : tab_model_(tab_model) {}
+    : tab_model_(tab_model) {
+  tab_model_->owning_model()->AddObserver(this);
+}
 
 LensOverlayController::~LensOverlayController() = default;
+
+void LensOverlayController::OnTabStripModelChanged(
+    TabStripModel* tab_strip_model,
+    const TabStripModelChange& change,
+    const TabStripSelectionChange& selection) {
+  if (!selection.active_tab_changed()) {
+    return;
+  }
+
+  if (selection.new_contents == tab_model_->contents()) {
+    TabForegrounded();
+    return;
+  }
+
+  if (selection.old_contents == tab_model_->contents()) {
+    TabBackgrounded();
+  }
+}
+
+void LensOverlayController::TabForegrounded() {}
+
+void LensOverlayController::TabBackgrounded() {}
--- chrome/browser/ui/lens/lens_overlay_controller.h
+++ chrome/browser/ui/lens/lens_overlay_controller.h
@@ -7,17 +7,34 @@
 
 #include "base/memory/raw_ptr.h"
 
+#include "base/scoped_observation.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+
+class TabStripModel;
+
 namespace tabs {
 class TabModel;
 }  // namespace tabs
 
 // Manages all state associated with the lens overlay.
-class LensOverlayController {
+class LensOverlayController : public TabStripModelObserver {
  public:
   explicit LensOverlayController(tabs::TabModel* tab_model);
-  ~LensOverlayController();
+  ~LensOverlayController() override;
+
+  // Overridden from TabStripModelObserver:
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection) override;
 
  private:
+  // Called when the associated tab enters the foreground.
+  void TabForegrounded();
+
+  // Called when the associated tab enters the background.
+  void TabBackgrounded();
+
   // Owns this class.
   raw_ptr<tabs::TabModel> tab_model_;
 };
