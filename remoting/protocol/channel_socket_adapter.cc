a20fa69108f357195328cbb7b6232f20ee5fe0fa
Per K <perkj@chromium.org>
2024-02-27 05:57:35 +0000

    Update Chromoting to use rtc::PacketTransportInternal::RegisterReceivedPacketCallback
    Purpose is to remove use of raw pointers to payload and simplify adding metadata to received packets.
    Also to replace Sigslot for ReadPacket with webrtc::CallbackList.
    
    Bug: webrtc:15368, webrtc:11943
    Change-Id: Ic36c3c024ebc10cd245312fb95c1bbc61b0b15bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5317678
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Per Kjellander <perkj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1265646}

--- remoting/protocol/channel_socket_adapter.cc
+++ remoting/protocol/channel_socket_adapter.cc
@@ -18,8 +18,11 @@ TransportChannelSocketAdapter::TransportChannelSocketAdapter(
     : channel_(ice_transport) {
   DCHECK(channel_);
 
-  channel_->SignalReadPacket.connect(
-      this, &TransportChannelSocketAdapter::OnNewPacket);
+  channel_->RegisterReceivedPacketCallback(
+      this, [&](rtc::PacketTransportInternal* transport,
+                const rtc::ReceivedPacket& packet) {
+        OnNewPacket(transport, packet);
+      });
   channel_->SignalWritableState.connect(
       this, &TransportChannelSocketAdapter::OnWritableState);
   channel_->SignalDestroyed.connect(
@@ -30,6 +33,11 @@ TransportChannelSocketAdapter::~TransportChannelSocketAdapter() {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   if (destruction_callback_) {
     std::move(destruction_callback_).Run();
+  }
+  if (channel_) {
+    // Channel may still exist in unit tests. We must deregister packet callback
+    // in order to prevent callbacks after destruction.
+    channel_->DeregisterReceivedPacketCallback(this);
   }
 }
 
@@ -107,7 +115,7 @@ void TransportChannelSocketAdapter::Close(int error_code) {
 
   DCHECK(error_code != net::OK);
   closed_error_code_ = error_code;
-  channel_->SignalReadPacket.disconnect(this);
+  channel_->DeregisterReceivedPacketCallback(this);
   channel_->SignalDestroyed.disconnect(this);
   channel_ = nullptr;
 
@@ -128,23 +136,21 @@ void TransportChannelSocketAdapter::Close(int error_code) {
 
 void TransportChannelSocketAdapter::OnNewPacket(
     rtc::PacketTransportInternal* transport,
-    const char* data,
-    size_t data_size,
-    const int64_t& packet_time,
-    int flags) {
+    const rtc::ReceivedPacket& packet) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
   DCHECK_EQ(transport, channel_);
   if (!read_callback_.is_null()) {
     DCHECK(read_buffer_.get());
-    CHECK_LT(data_size, static_cast<size_t>(std::numeric_limits<int>::max()));
-
+    CHECK_LT(packet.payload().size(),
+             static_cast<size_t>(std::numeric_limits<int>::max()));
+    size_t data_size = packet.payload().size();
     if (read_buffer_size_ < static_cast<int>(data_size)) {
       LOG(WARNING) << "Data buffer is smaller than the received packet. "
                    << "Dropping the data that doesn't fit.";
       data_size = read_buffer_size_;
     }
 
-    memcpy(read_buffer_->data(), data, data_size);
+    memcpy(read_buffer_->data(), packet.payload().data(), data_size);
 
     net::CompletionRepeatingCallback callback = read_callback_;
     read_callback_.Reset();
