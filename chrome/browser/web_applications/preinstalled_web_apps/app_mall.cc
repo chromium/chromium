f3e3f718601cb04b5d921b0e8817b984e98ce369
Tim Sergeant <tsergeant@chromium.org>
2024-02-28 22:57:57 +0000

    Reland "[Mall] Add preload config for Mall app"
    This is a reland of commit 0f0b4c47e29d74c49a62a5c739c101679fdb18c3
    
    Relanding without using app icon that can't be accessed from all builds.
    
    Original change's description:
    > [Mall] Add preload config for Mall app
    >
    > Bug: b/326342444
    > Change-Id: I03c9a9bf1cb1138390185fe9b5ae9e5257b3d164
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319822
    > Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Sophia Lin <sophialin@google.com>
    > Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1266212}
    
    Bug: b/326342444
    Change-Id: Id92da8883acc0a020e083c7772fd5d1a2faae1b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5330427
    Reviewed-by: Sophia Lin <sophialin@google.com>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266697}

---
+++ chrome/browser/web_applications/preinstalled_web_apps/app_mall.cc
@@ -0,0 +1,47 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/web_applications/preinstalled_web_apps/app_mall.h"
+
+#include <memory>
+
+#include "base/functional/bind.h"
+#include "chrome/browser/web_applications/external_install_options.h"
+#include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
+#include "chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_app_definition_utils.h"
+#include "chrome/browser/web_applications/web_app_install_info.h"
+#include "chrome/grit/preinstalled_web_apps_resources.h"
+#include "chromeos/constants/chromeos_features.h"
+
+namespace web_app {
+
+ExternalInstallOptions GetConfigForAppMall() {
+  ExternalInstallOptions options(
+      /*install_url=*/GURL("https://discover.apps.chrome/"),
+      /*user_display_mode=*/mojom::UserDisplayMode::kStandalone,
+      /*install_source=*/ExternalInstallSource::kExternalDefault);
+
+  options.user_type_allowlist = {"unmanaged"};
+  options.gate_on_feature = chromeos::features::kCrosMall.name;
+
+  options.load_and_await_service_worker_registration = false;
+  options.only_use_app_info_factory = false;
+
+  // This App Info Factory is temporary, to help with prototyping.
+  // TODO(b/327080071): Remove.
+  options.app_info_factory = base::BindRepeating([]() {
+    auto info = std::make_unique<WebAppInstallInfo>();
+    info->title = u"Get Apps and Games";
+    info->start_url = GURL("https://discover.apps.chrome/");
+    info->manifest_id = GURL("https://discover.apps.chrome/");
+    info->display_mode = DisplayMode::kStandalone;
+    info->icon_bitmaps.any = LoadBundledIcons(
+        {IDR_PREINSTALLED_WEB_APPS_GOOGLE_SHEETS_ICON_192_PNG});
+    return info;
+  });
+
+  return options;
+}
+
+}  // namespace web_app
