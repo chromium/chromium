40c0b26d65356a549236d7b8b5c5cc64dad32828
Theresa Sullivan <twellington@chromium.org>
2024-02-28 20:31:47 +0000

    Add back GestureNavigation.Activated, GestureNavigation.Cancelled, GestureNavigation.Completed, GestureNavigation.Reversed
    This reverts commit 2d41a8a7402d484b1edc9f54c09fbe021a6f46ee.
    
    Reason for revert: Would like to have these metrics again.
    
    Original change's description:
    > [Expired Histograms] Remove GestureNavigation.Activated, GestureNavigation.Cancelled, GestureNavigation.Completed, GestureNavigation.Reversed
    >
    > Bug: 1447163
    > Change-Id: I4251073db6315eec020789911974d6ba46eb9c66
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4651239
    > Commit-Queue: Nan Lin <linnan@chromium.org>
    > Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1163682}
    
    OBSOLETE_HISTOGRAMS=Expired GestureNavigation.Type replaced by GestureNavigation.Type2
    
    (cherry picked from commit 1b33e41eeeb5c8d3b2de086e93291c5b6aa316b8)
    
    Bug: 325618728
    Change-Id: Ia42c04ad7905fbd30fb128058b7349ebe461cd78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5303995
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1261957}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332832
    Cr-Commit-Position: refs/branch-heads/6261@{#982}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- chrome/android/java/src/org/chromium/chrome/browser/gesturenav/SideSlideLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/gesturenav/SideSlideLayout.java
@@ -73,6 +73,15 @@ public class SideSlideLayout extends ViewGroup {
     private final int mMediumAnimationDuration;
     private final int mCircleWidth;
 
+    // Metrics
+    private static long sLastCompletedTime;
+    private static boolean sLastCompletedForward;
+
+    // Maximum amount of overscroll for a single side gesture action. An action is regarded
+    // as an attempt to navigate via a gesture ('activated') and used for UMA if the maximum
+    // overscroll is bigger than a certain threshold.
+    private float mMaxOverscroll;
+
     private OnNavigateListener mListener;
     private OnResetListener mResetListener;
 
@@ -274,6 +283,7 @@ public class SideSlideLayout extends ViewGroup {
     public boolean start() {
         if (!isEnabled() || mNavigating || mListener == null) return false;
         mTotalMotion = 0;
+        mMaxOverscroll = 0.f;
         mIsBeingDragged = true;
         mWillNavigate = false;
         initializeOffset();
@@ -303,6 +313,7 @@ public class SideSlideLayout extends ViewGroup {
 
         float overscroll = getOverscroll();
         float extraOs = overscroll - mTotalDragDistance;
+        if (overscroll > mMaxOverscroll) mMaxOverscroll = overscroll;
         float slingshotDist = mTotalDragDistance;
         float tensionSlingshotPercent =
                 Math.max(0, Math.min(extraOs, slingshotDist * 2) / slingshotDist);
@@ -369,9 +380,23 @@ public class SideSlideLayout extends ViewGroup {
         // See ACTION_UP handling in {@link #onTouchEvent(...)}.
         mIsBeingDragged = false;
 
+        boolean activated = mMaxOverscroll >= mArrowViewWidth / 3;
+        if (activated) {
+            GestureNavMetrics.recordHistogram("GestureNavigation.Activated2", mIsForward);
+        }
+
         if (isEnabled() && willNavigate()) {
             if (allowNav) {
                 setNavigating(true);
+                GestureNavMetrics.recordHistogram("GestureNavigation.Completed2", mIsForward);
+                long time = System.currentTimeMillis();
+                if (sLastCompletedTime > 0
+                        && time - sLastCompletedTime < NAVIGATION_REVERSAL_MS
+                        && mIsForward != sLastCompletedForward) {
+                    GestureNavMetrics.recordHistogram("GestureNavigation.Reversed2", mIsForward);
+                }
+                sLastCompletedTime = time;
+                sLastCompletedForward = mIsForward;
             } else {
                 // Show navigation instead of triggering navigation. Just hide the arrow
                 // by fading it away.
@@ -388,6 +413,9 @@ public class SideSlideLayout extends ViewGroup {
         mAnimateToStartPosition.setInterpolator(mDecelerateInterpolator);
         mArrowView.clearAnimation();
         mArrowView.startAnimation(mAnimateToStartPosition);
+        if (activated) {
+            GestureNavMetrics.recordHistogram("GestureNavigation.Cancelled2", mIsForward);
+        }
     }
 
     /** Reset the effect, clearing any active animations. */
