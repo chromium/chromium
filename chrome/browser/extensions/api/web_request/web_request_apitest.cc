beddcd95d51f0d2bbf7c4190d2c46c235a42f23b
Ming-Ying Chung <mych@chromium.org>
2024-02-28 19:46:44 +0000

    [gardener] Disable flaky ExtensionWebRequestApiWebTransportTest.Main
    (cherry picked from commit b565d938b2b538ff0b242493c4399f2491601388)
    
    Bug: b:326122304
    Change-Id: Ic8380a8aca4db8f3e3a4fe8e1b672976349e3224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311680
    Owners-Override: Ming-Ying Chung <mych@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1263170}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328686
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Owners-Override: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/branch-heads/6261@{#981}
    Cr-Branched-From: 9755d9d81e4a8cb5b4f76b23b761457479dbb06b-refs/heads/main@{#1250580}

--- chrome/browser/extensions/api/web_request/web_request_apitest.cc
+++ chrome/browser/extensions/api/web_request/web_request_apitest.cc
@@ -2001,7 +2001,8 @@ class ExtensionWebRequestApiWebTransportTest
 
 // Test that the webRequest events are dispatched for the WebTransport
 // handshake.
-IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiWebTransportTest, Main) {
+// TODO(crbug.com/326122304): Re-enable this test
+IN_PROC_BROWSER_TEST_F(ExtensionWebRequestApiWebTransportTest, DISABLED_Main) {
   ASSERT_TRUE(RunTest("test_webtransport.html")) << message_;
 }
 
