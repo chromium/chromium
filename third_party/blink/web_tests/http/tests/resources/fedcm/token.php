d37223ed9afc794d3085eb2fc1e7e4b4e23f99e9
Nicolás Peña <npm@chromium.org>
2024-02-28 22:56:57 +0000

    [FedCM] Add CORS support in WPTs as well as a no CORS test
    The no CORS test fails right now but will pass once the CORS flag is
    enabled by default
    
    Bug: 40284123
    Change-Id: Iaa864b2d94b4fff5c6258dfc1ba5aa78bce70180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5332937
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1266696}

--- third_party/blink/web_tests/http/tests/resources/fedcm/token.php
+++ third_party/blink/web_tests/http/tests/resources/fedcm/token.php
@@ -1,4 +1,10 @@
-<?php header("Content-Type: application/json") ?>
+<?php
+header("Content-Type: application/json");
+if (isset($_GET['origin'])) {
+  header("Access-Control-Allow-Origin: " . $_GET['origin']);
+  header("Access-Control-Allow-Credentials: true");
+}
+?>
 {
   "token": "<?php echo $_POST["account_id"]; ?>"
 }
