435ced8d52a520e59a592ebfdb85b74715f123b4
Leon Masopust <lmasopust@google.com>
2024-02-29 09:16:17 +0000

    Revert "AbortSignal: Use native async gc() for GC tests"
    This reverts commit 169fa04e8660200dd58fb95f2748688ad880581c.
    
    Reason for revert: suspect causing wpt_internal/scheduler/task-signal-any-memory-abort-% tests to consistently fail 
    
    Original change's description:
    > AbortSignal: Use native async gc() for GC tests
    >
    > Use native async gc() instead of postTask() + sync gc() in an attempt
    > to deflake abort signal memory tests, which might help since it's
    > stack-less.
    >
    > Bug: 326449175
    > Change-Id: Ic95cc657aae84c0d5b0c2918f2154411cf378694
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5327503
    > Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    > Commit-Queue: Scott Haseley <shaseley@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1266481}
    
    Bug: 326449175
    Change-Id: I6b40f0838b39160a275825eb4bc645b257feeaca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335096
    Commit-Queue: Leon Masopust <lmasopust@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1266881}

--- third_party/blink/web_tests/wpt_internal/dom/abort/resources/run-async-gc.js
+++ third_party/blink/web_tests/wpt_internal/dom/abort/resources/run-async-gc.js
@@ -1,9 +1,11 @@
-async function runAsyncGC() {
+async function runAsyncGC(args = {}) {
   // Run gc in a loop to ensure anything needing more than one cycle can be
   // collected, e.g. due to dependencies. Note this is similar to
   // ThreadState::CollectAllGarbageForTesting, but async and with 2 less
   // iterations.
   for (let i = 0; i < 3; i++) {
-    await gc({type: 'major', execution: 'async', flavor: 'last-resort'});
+    // crbug.com/1474629: invoking gc({execution: 'async'}) trips leak
+    // detection, so use postTask and run sync gc() to do async GC.
+    await scheduler.postTask(() => { gc(); }, args);
   }
 }
