345306ae8f80c1a527ffcc3ef05a73f9a36a35fd
John Stiles <johnstiles@google.com>
2024-02-29 19:56:22 +0000

    Enable Raster Pipeline Perlin Noise and rebaseline.
    Unlike the legacy implementation, this version properly handles
    rotation. It is also much faster thanks to SIMD.
    
    Bug: 40045243
    Change-Id: Iee769ad8462ff433c4bd3ae56cd655334c6bba5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5323882
    Reviewed-by: Brian Osman <brianosman@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Commit-Queue: John Stiles <johnstiles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267154}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -241,6 +241,10 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_DONT_PAD_LAYER_IMAGES
 
+// TODO: (skbug.com/40045243) - Remove legacy Perlin Noise implementation from
+// Skia, then remove this migration flag.
+#define SK_RASTER_PIPELINE_PERLIN_NOISE
+
 /* When --disable-skia-runtime-opts is set in Chrome (or when SkGraphics::Init
    is not called), Skia will prefer precision over performance when computing
    reciprocals and inverse-square roots.
