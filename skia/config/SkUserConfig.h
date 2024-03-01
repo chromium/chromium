c2d448c4009098dfdf49d092c480378affdbeed9
Brian Osman <brianosman@google.com>
2024-03-01 18:15:23 +0000

    Guard upcoming Skia fix for non-AA edge rounding
    See http://review.skia.org/820656 for details.
    
    Change-Id: I6689a1d46dc7041671f72f9f5298dc0d4800fc0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5336197
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Auto-Submit: Brian Osman <brianosman@google.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267446}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -229,6 +229,8 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_USE_LEGACY_MIPMAP_BUILDER
 
+#define SK_LEGACY_EDGE_AA_ROUNDING
+
 #define SK_SUPPORT_LEGACY_CONIC_CHOP
 
 #define SK_ENABLE_SKSL_IN_RASTER_PIPELINE
