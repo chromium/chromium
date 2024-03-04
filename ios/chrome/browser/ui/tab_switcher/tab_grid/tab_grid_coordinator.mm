0b10ba3678c05254a7fba4abe15d102daf99b602
Mike Dougherty <michaeldo@chromium.org>
2024-03-04 18:52:01 +0000

    Revert "[ios] Fixes a 0.5s delay when entering or exiting the tab grid."
    This reverts commit 116f4b845f378ca5567c0143fb475317eff59cac.
    
    Reason for revert: Causes status bar content to stay light in light mode when on BVC after crrev.com/c/5226613.
    
    Original change's description:
    > [ios] Fixes a 0.5s delay when entering or exiting the tab grid.
    >
    > On iOS 15, snapshotting views with afterScreenUpdates:YES waits 0.5s for
    > the status bar style to update. Work around that delay by taking the
    > snapshot first and then triggering the tab grid animation afterwards.
    >
    > This bug was visible in scenarios where entering or exiting the tab grid
    > required a status bar style change -- specifically, light mode
    > non-incognito BVCs.
    >
    > BUG=1236260
    >
    > Change-Id: I7c0bcde3ba0c008d6d8c7dce99a0a61c5d654fa9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3069680
    > Reviewed-by: edchin <edchin@chromium.org>
    > Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#908525}
    
    Bug: 1236260
    Change-Id: Idde7228b00d91c0a23d945868a10a163dff70dbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5336158
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Reviewed-by: edchin <edchin@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1267971}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
@@ -430,6 +430,8 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
       return;
     }
 
+    strongSelf.baseViewController.childViewControllerForStatusBarStyle = nil;
+
     if (IsNewTabGridTransitionsEnabled()) {
       [self
           performBrowserToTabGridTransitionWithAnimationEnabled:animated
@@ -443,12 +445,6 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
                                                      completion:
                                                          transitionCompletionBlock];
     }
-    // On iOS 15+, snapshotting views with afterScreenUpdates:YES waits 0.5s
-    // for the status bar style to update. Work around that delay by taking
-    // the snapshot first (during
-    // `transitionFromBrowser:toTabGrid:activePage:withCompletion`) and then
-    // updating the status bar style afterwards.
-    strongSelf.baseViewController.childViewControllerForStatusBarStyle = nil;
   };
 
   // If a BVC is currently being presented, dismiss it.  This will trigger any
@@ -540,6 +536,9 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
     self.firstPresentation = NO;
   };
 
+  self.baseViewController.childViewControllerForStatusBarStyle =
+      self.bvcContainer.currentBVC;
+
   [self.baseViewController contentWillDisappearAnimated:animated];
 
   if (IsNewTabGridTransitionsEnabled()) {
@@ -553,14 +552,6 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
                                                      completion:
                                                          extendedCompletion];
   }
-
-  // On iOS 15+, snapshotting views with afterScreenUpdates:YES waits 0.5s for
-  // the status bar style to update. Work around that delay by taking the
-  // snapshot first (during
-  // `transitionFromTabGrid:toBrowser:activePage:withCompletion`) and then
-  // updating the status bar style afterwards.
-  self.baseViewController.childViewControllerForStatusBarStyle =
-      self.bvcContainer.currentBVC;
 }
 
 #pragma mark - Private
