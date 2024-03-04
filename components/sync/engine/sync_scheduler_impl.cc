a41a6fac5f221cf8cbe8e7085026671b34874938
Marc Treib <treib@chromium.org>
2024-03-04 10:40:28 +0000

    Sync: Clean up kSyncPollIfTimerNotRunning
    This was a killswitch. The feature has rolled out in M122 and not caused
    any issues, so time to clean it up!
    
    Bug: 40939309
    Change-Id: I87c8809a39d55ac3cf6dea5cefe24bff0f905aca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5335219
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1267755}

--- components/sync/engine/sync_scheduler_impl.cc
+++ components/sync/engine/sync_scheduler_impl.cc
@@ -28,10 +28,6 @@ namespace syncer {
 
 namespace {
 
-BASE_FEATURE(kSyncPollIfTimerNotRunning,
-             "SyncPollIfTimerNotRunning",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 constexpr base::TimeDelta kLocalChangeNudgeDelayForTest = base::Milliseconds(1);
 
 bool IsConfigRelatedUpdateOriginValue(
@@ -672,21 +668,13 @@ void SyncSchedulerImpl::TrySyncCycleJobImpl(
       SDVLOG(2) << "Found pending nudge job";
       DoNudgeSyncCycleJob();
     } else {
-      bool should_poll = false;
-      if (base::FeatureList::IsEnabled(kSyncPollIfTimerNotRunning)) {
-        // If the poll timer isn't running, that means a poll is pending.
-        // Most likely this was called from PollTimerCallback(), but it's also
-        // possible that the poll was triggered earlier while the scheduler was
-        // in an error state, and now it has exited the error state.
-        // Note: At most one of the two poll timers can be running (which one
-        // depends on the state of kSyncSchedulerUseWallClockTimer).
-        should_poll =
-            !poll_timer_ticks_.IsRunning() && !poll_timer_wall_.IsRunning();
-      } else {
-        should_poll =
-            (TimeTicks::Now() - last_poll_reset_ticks_) >= GetPollInterval();
-      }
-      if (should_poll) {
+      // If the poll timer isn't running, that means a poll is pending.
+      // Most likely this was called from PollTimerCallback(), but it's also
+      // possible that the poll was triggered earlier while the scheduler was
+      // in an error state, and now it has exited the error state.
+      // Note: At most one of the two poll timers can be running (which one
+      // depends on the state of kSyncSchedulerUseWallClockTimer).
+      if (!poll_timer_ticks_.IsRunning() && !poll_timer_wall_.IsRunning()) {
         SDVLOG(2) << "Found pending poll";
         DoPollSyncCycleJob();
       }
