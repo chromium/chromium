fdc04e7e3380643faca5726ea735dfdc06a39919
Kuan Huang <kuanhuang@chromium.org>
2024-03-04 20:20:05 +0000

    Disable StartupBrowserWithWebAppTest.LastUsedProfilesWithWebApp on win
    The test is flaky and failed on 35 builds on windows builders:
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FStartupBrowserWithWebAppTest.LastUsedProfilesWithWebApp?q=VHASH%3A2876d8e0ab8a0546
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FStartupBrowserWithWebAppTest.LastUsedProfilesWithWebApp?q=VHASH%3Aff28206f63c46326
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FStartupBrowserWithWebAppTest.LastUsedProfilesWithWebApp?q=VHASH%3A6912dad9a26925c1
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FStartupBrowserWithWebAppTest.LastUsedProfilesWithWebApp?q=VHASH%3A98c7d59277f191fa
    
    Bug: 327256043, 325415943
    Change-Id: I831a941cf90bd1d4c69cc7100b4d9b80fd59f7c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331982
    Commit-Queue: Kuan Huang <kuanhuang@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268040}

--- chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
+++ chrome/browser/ui/startup/startup_browser_creator_browsertest.cc
@@ -2281,8 +2281,14 @@ IN_PROC_BROWSER_TEST_F(StartupBrowserWithWebAppTest,
   CloseBrowserAsynchronously(browser());
 }
 
+// TODO(crbug.com/327256043): Flaky on win
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_LastUsedProfilesWithWebApp DISABLED_LastUsedProfilesWithWebApp
+#else
+#define MAYBE_LastUsedProfilesWithWebApp LastUsedProfilesWithWebApp
+#endif
 IN_PROC_BROWSER_TEST_F(StartupBrowserWithWebAppTest,
-                       LastUsedProfilesWithWebApp) {
+                       MAYBE_LastUsedProfilesWithWebApp) {
   ProfileManager* profile_manager = g_browser_process->profile_manager();
 
   base::FilePath dest_path = profile_manager->user_data_dir();
