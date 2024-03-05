875ec45bba10cebb3cb76c8666a7a92e3c9c08f5
Ben Pastene <bpastene@chromium.org>
2024-03-05 20:40:19 +0000

    utr: Add a __main__.py as an alternative entry point for the script
    This is just for convenience so that people can invoke the UTR via
    `python3 tools/utr ...` instead of `./tools/utr/run.py ...`.
    
    The latter is prob still preferred since it sets vpython3 to be the
    interpreter. But the UTR is pretty basic, and standard python3 should
    be fine too.
    
    Bug: 41492688
    Change-Id: I39afb6f37e512a1094b2c0c5c596083ffdb87a5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5346363
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268664}

---
+++ tools/utr/__main__.py
@@ -0,0 +1,12 @@
+#!/bin/env vpython3
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Alternative entry point to invoking the UTR.
+
+Allows the UTR to be invoked like `python3 ./tools/utr ...`
+"""
+
+import run
+
+run.main()
