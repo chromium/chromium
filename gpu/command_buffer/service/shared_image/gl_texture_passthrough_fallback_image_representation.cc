7fca241eddf3be8e917abbd713506380ecd8153d
Colin Blundell <blundell@chromium.org>
2024-03-05 12:32:09 +0000

    graphite: Do not use glTexStorage2D for GL fallback representation
    With Graphite on Swiftshader, we use the GL fallback representation
    which performs interop with the Graphite backing by reading from it and
    uploading to a GL texture using glTexSubImage2D. However, it uses
    glTexStorage2D if available when creating the texture which precludes
    being able to redefine the texture with glTexSubImage2D. So simply use
    glTexImage2D on creation which is fine since this is test-only code.
    
    Bug: 41495039
    Change-Id: Icff6cffec02005fce6ba7c0e21b3d73a2ecaa13d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5341941
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1268384}

--- gpu/command_buffer/service/shared_image/gl_texture_passthrough_fallback_image_representation.cc
+++ gpu/command_buffer/service/shared_image/gl_texture_passthrough_fallback_image_representation.cc
@@ -51,16 +51,11 @@ scoped_refptr<gles2::TexturePassthrough> CreateGLTexture(
 
   gl::ScopedProgressReporter scoped_progress_reporter(progress_reporter);
 
-  if (IsTexStorage2DAvailable()) {
-    const GLenum internal_format = format_desc.storage_internal_format;
-    api->glTexStorage2DEXTFn(target, /*levels=*/1, internal_format,
-                             size.width(), size.height());
-  } else {
-    const GLenum internal_format = format_desc.image_internal_format;
-    api->glTexImage2DFn(target, 0, internal_format, size.width(), size.height(),
-                        0, format_desc.data_format, format_desc.data_type,
-                        nullptr);
-  }
+  // Use glTexImage2D instead of glTexStorage2D so that we can later use
+  // glTexSubImage2D in GLTextureHolder::UploadFromMemory.
+  api->glTexImage2DFn(target, 0, format_desc.image_internal_format,
+                      size.width(), size.height(), 0, format_desc.data_format,
+                      format_desc.data_type, nullptr);
 
   return base::MakeRefCounted<gles2::TexturePassthrough>(service_id, target);
 }
