d5d44a0eeaece7a522b0f372325f2b41178428da
Duncan Mercer <mercerd@google.com>
2024-03-06 19:59:27 +0000

    Add browser proxy mojo wrapper to Lens overlay typescript
    Bug: b/328087485
    Change-Id: Ibe0e521b354bdeb27af6407a5fcae62f0b7d0141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349048
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269183}

---
+++ chrome/browser/resources/lens/overlay/browser_proxy.ts
@@ -0,0 +1,32 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {LensPageCallbackRouter, LensPageHandlerFactory, LensPageHandlerRemote} from './lens.mojom-webui.js';
+
+let instance: BrowserProxy|null = null;
+
+export interface BrowserProxy {
+  callbackRouter: LensPageCallbackRouter;
+  handler: LensPageHandlerRemote;
+}
+
+export class BrowserProxyImpl implements BrowserProxy {
+  callbackRouter: LensPageCallbackRouter = new LensPageCallbackRouter();
+  handler: LensPageHandlerRemote = new LensPageHandlerRemote();
+
+  constructor() {
+    const factory = LensPageHandlerFactory.getRemote();
+    factory.createLensPageHandler(
+        this.handler.$.bindNewPipeAndPassReceiver(),
+        this.callbackRouter.$.bindNewPipeAndPassRemote());
+  }
+
+  static getInstance(): BrowserProxy {
+    return instance || (instance = new BrowserProxyImpl());
+  }
+
+  static setInstance(obj: BrowserProxy) {
+    instance = obj;
+  }
+}
