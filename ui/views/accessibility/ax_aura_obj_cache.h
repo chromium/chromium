26555cd0a510fdb6fab0740cfcc512735a5fc798
Josiah Krutz <josiahk@chromium.org>
2024-03-06 02:29:06 +0000

    [Cleanup] Fix accessiblity -> accessibility typos
    AX-Relnotes: N/A.
    Bug: None.
    Change-Id: I5a266a5d1f8df617e7fa6a563e911fc61d2776c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5241670
    Auto-Submit: Josiah Krutz <josiahk@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1268851}

--- ui/views/accessibility/ax_aura_obj_cache.h
+++ ui/views/accessibility/ax_aura_obj_cache.h
@@ -142,7 +142,7 @@ class VIEWS_EXPORT AXAuraObjCache : public aura::client::FocusChangeObserver {
                       std::map<AuraView*, ui::AXNodeID>* aura_view_to_id_map);
 
   // The window that should take a11y focus. This is for a window that needs to
-  // work with accessiblity features, but cannot take real focus. Gets set to
+  // work with accessibility features, but cannot take real focus. Gets set to
   // null if the window is destroyed.
   raw_ptr<aura::Window> a11y_override_window_ = nullptr;
 
