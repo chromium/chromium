0169834e1bea71504ef61fdb1a83a8c0e15ac399
Louis Romero <lpromero@google.com>
2024-03-06 21:50:34 +0000

    Enable TabGridCompositionalLayout by default
    Change-Id: I2091c0231ca94e0bd7a057c51233da1037935051
    Fixed: 41488079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349096
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269243}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -290,7 +290,7 @@ BASE_FEATURE(kTabGridAlwaysBounce,
 
 BASE_FEATURE(kTabGridCompositionalLayout,
              "TabGridCompositionalLayout",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsTabGridCompositionalLayoutEnabled() {
   return base::FeatureList::IsEnabled(kTabGridCompositionalLayout);
