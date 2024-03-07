6d3f7b1ff53d70b80bce608c66fa31509c29016d
Eliot Courtney <edcourtney@google.com>
2024-03-07 07:09:28 +0000

    Reland "Sync bounds for mirror layers."
    This is a reland of commit bfd714210e762ddbf54b4261ed514feb879da572
    
    Previously, this broke because tile occlusion was not enabled and
    old content was being shown with wrong bounds, so syncing to those
    bounds made everything transparent.
    
    With tile occlusion enabled, windows know when they are shown and will
    produce new content, so sync bounds should get content with proper
    bounds it is safe to sync to. Without this, old bounds from snapshots
    will be positioned incorrectly and show some transparent gap.
    
    Original change's description:
    > Sync bounds for mirror layers.
    >
    > Bug: 40223673
    > Test: CQ + local testing
    > Change-Id: I457f4cb7f58d105ab3cac7c0031b58735864a222
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5273759
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1258845}
    
    Bug: 40223673
    Change-Id: I734dcd755daeea405ad01254b37c39a816de1eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5334320
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269431}

--- ash/wm/window_mirror_view.h
+++ ash/wm/window_mirror_view.h
@@ -33,7 +33,7 @@ class ASH_EXPORT WindowMirrorView : public views::View,
  public:
   explicit WindowMirrorView(aura::Window* source,
                             bool show_non_client_view = false,
-                            bool sync_bounds = false);
+                            bool sync_bounds = true);
 
   WindowMirrorView(const WindowMirrorView&) = delete;
   WindowMirrorView& operator=(const WindowMirrorView&) = delete;
