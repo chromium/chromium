95eeeefb18a0ace844611481df11f30e8263111b
Wei-Luan Wang <weiluanwang@google.com>
2024-03-07 06:43:29 +0000

    [Telemetry] Restrict manufacturer fallback to certain models
    We plan to deprecate the fallback logic for reading manufacturer from
    DMI. Restrict the fallback to certain models in order to limit the
    impact and prevent new models from relying on DMI.
    
          telemetry extension APIs are both accessible or both not
          accessible before and after the changes.
          1. model=eldrid, with cros-config filled
          2. model=drawcia, with cros-config not filled but with DMI filled
          3. model=lillipup, with both cros-config and DMI not filled
    
    Bug: b:325560417, b:300015436
    Test: Manual test on real devices with different models to verify the
    Change-Id: I8b862875edde57e44f51f32b256014bb9a26384c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5348558
    Reviewed-by: Ethan Cheng <yycheng@google.com>
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Denny Huang <dennyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1269419}

--- chrome/browser/chromeos/extensions/telemetry/api/common/hardware_info_delegate.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/common/hardware_info_delegate.cc
@@ -6,8 +6,10 @@
 
 #include <memory>
 #include <string>
+#include <string_view>
 #include <utility>
 
+#include "base/containers/contains.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
@@ -23,6 +25,15 @@
 namespace chromeos {
 
 namespace {
+
+// It is generally not recommended to bahave differently based on model names.
+// However, to keep backward compatibility during the deprecation, use an
+// allowlist to restrict the impact to old models. See b/300015436.
+constexpr std::string_view kManufacturerFallbackEnabledModelList[] = {
+    "drawcia", "drawlat", "drawman",  "drawper", "landia",    "landrid",
+    "lantis",  "madoo",   "barla",    "careena", "dragonair", "dratini",
+    "jinlon",  "sona",    "syndra",   "dooly",   "noibat",    "snappy",
+    "soraka",  "berknip", "dirinboz", "gumboz"};
 
 // Returns manufacturer read from sys_vendor file. Runs in the separate thread
 // pool which supports blocking sys calls.
@@ -43,7 +54,14 @@ std::string GetManufacturerFromSysfsSync() {
   return manufacturer;
 }
 
-void GetManufacturerFromSysfs(base::OnceCallback<void(std::string)> callback) {
+// Callback from SysInfo::GetHardwareInfo().
+void OnGetHardwareInfo(base::OnceCallback<void(std::string)> callback,
+                       base::SysInfo::HardwareInfo hardware_info) {
+  if (!base::Contains(kManufacturerFallbackEnabledModelList,
+                      hardware_info.model)) {
+    std::move(callback).Run("");
+    return;
+  }
   base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, {base::MayBlock()},
       base::BindOnce(&GetManufacturerFromSysfsSync), std::move(callback));
@@ -90,7 +108,12 @@ HardwareInfoDelegate::~HardwareInfoDelegate() = default;
 
 // GetManufacturer tries to get the manufacturer (or OEM name) from
 // ProbeServiceAsh[1] first. If no (or empty) information is returned,
-// GetManufacturer falls back to SysInfo[2].
+// GetManufacturer falls back to SysInfo[2]. Notice that the fallback [2] is
+// going to be deprecated so the fallback is enabled only on certain models
+// that have not been migrated to cros_config.
+// TODO(b/300015436): Remove [2] after cros_config is backfilled on the listed
+// models.
+//
 // [1] ProbeServiceAsh fetches the OEM name from cros_config.
 // [2] SysInfo fetches the manufacturer information from the
 // "/sys/devices/virtual/dmi/id/sys_vendor" system file.
@@ -114,7 +137,8 @@ void HardwareInfoDelegate::FallbackHandler(ManufacturerCallback done_cb,
     return;
   }
 
-  GetManufacturerFromSysfs(std::move(done_cb));
+  base::SysInfo::GetHardwareInfo(
+      base::BindOnce(&OnGetHardwareInfo, std::move(done_cb)));
 }
 
 }  // namespace chromeos
