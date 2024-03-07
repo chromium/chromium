22be19a7fed2574884715f88f17103345c733bfe
Ella Ge <eirage@chromium.org>
2024-03-07 15:52:17 +0000

    Enable SmartPWAInstallPromptAndroid on ToT by default
    Change-Id: Ia939b855fe43e8afb3702235269622326832def9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349981
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269596}

--- components/webapps/browser/features.cc
+++ components/webapps/browser/features.cc
@@ -21,7 +21,7 @@ BASE_FEATURE(kAmbientBadgeSuppressFirstVisit,
 // Enables or disables the installable ambient badge message.
 BASE_FEATURE(kInstallPromptGlobalGuardrails,
              "InstallPromptGlobalGuardrails",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 extern const base::FeatureParam<int>
     kInstallPromptGlobalGuardrails_DismissCount{&kInstallPromptGlobalGuardrails,
                                                 "dismiss_count", 3};
@@ -55,7 +55,7 @@ BASE_FEATURE(kCreateShortcutIgnoresManifest,
 // Use segmentation to decide whether install prompt should be shown.
 BASE_FEATURE(kInstallPromptSegmentation,
              "InstallPromptSegmentation",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Keys to use when querying the variations params.
 BASE_FEATURE(kAppBannerTriggering,
