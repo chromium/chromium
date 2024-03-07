d06de970bd0736b3f2d4344d9ad3f3d0d9a4f3e1
Shibalik Mohapatra <shibalik@chromium.org>
2024-03-07 19:44:49 +0000

    Implementation of GroupedTabCollection class.
    This change includes -
    
    1. GroupedTabCollection implementation which represents all the tabs in a group.
    
    2. Unittests for the operations.
    
    3. Addition of the new files to BUILD.gn
    
    Change-Id: I1885dc41cf57809aed8e3c7b070f80dc9f9394b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349089
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269757}

--- chrome/browser/ui/tabs/tab_collection_unittest.cc
+++ chrome/browser/ui/tabs/tab_collection_unittest.cc
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/tabs/tab_collection.h"
 #include <memory>
+
 #include "chrome/browser/ui/tabs/pinned_tab_collection.h"
+#include "chrome/browser/ui/tabs/tab_collection.h"
+#include "chrome/browser/ui/tabs/tab_group_tab_collection.h"
 #include "chrome/browser/ui/tabs/tab_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/test_tab_strip_model_delegate.h"
@@ -13,16 +15,34 @@
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-class PinnedTabCollectionTest : public ::testing::Test {
+class TabCollectionBaseTest : public ::testing::Test {
  public:
-  PinnedTabCollectionTest() {
+  TabCollectionBaseTest() {
     scoped_feature_list_.InitWithFeatures(
         {features::kTabStripCollectionStorage}, {});
-    pinned_collection_ = std::make_unique<tabs::PinnedTabCollection>();
     testing_profile_ = std::make_unique<TestingProfile>();
     tab_strip_model_delegate_ = std::make_unique<TestTabStripModelDelegate>();
     tab_strip_model_ = std::make_unique<TabStripModel>(
         tab_strip_model_delegate_.get(), testing_profile_.get());
+  }
+  TabCollectionBaseTest(const TabCollectionBaseTest&) = delete;
+  TabCollectionBaseTest& operator=(const TabCollectionBaseTest&) = delete;
+  ~TabCollectionBaseTest() override = default;
+
+  TabStripModel* GetTabStripModel() { return tab_strip_model_.get(); }
+
+ private:
+  content::BrowserTaskEnvironment task_environment_;
+  base::test::ScopedFeatureList scoped_feature_list_;
+  std::unique_ptr<TabStripModel> tab_strip_model_;
+  std::unique_ptr<Profile> testing_profile_;
+  std::unique_ptr<TestTabStripModelDelegate> tab_strip_model_delegate_;
+};
+
+class PinnedTabCollectionTest : public TabCollectionBaseTest {
+ public:
+  PinnedTabCollectionTest() {
+    pinned_collection_ = std::make_unique<tabs::PinnedTabCollection>();
   }
   PinnedTabCollectionTest(const PinnedTabCollectionTest&) = delete;
   PinnedTabCollectionTest& operator=(const PinnedTabCollectionTest&) = delete;
@@ -31,8 +51,6 @@ class PinnedTabCollectionTest : public ::testing::Test {
   tabs::PinnedTabCollection* GetPinnedCollection() {
     return pinned_collection_.get();
   }
-
-  TabStripModel* GetTabStripModel() { return tab_strip_model_.get(); }
 
   void AddTabs(int num) {
     for (int i = 0; i < num; i++) {
@@ -48,12 +66,7 @@ class PinnedTabCollectionTest : public ::testing::Test {
  private:
   // TODO(shibalik): Create a parent collection after implementation of
   // TabStripCollection.
-  content::BrowserTaskEnvironment task_environment_;
-  base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<tabs::PinnedTabCollection> pinned_collection_;
-  std::unique_ptr<TabStripModel> tab_strip_model_;
-  std::unique_ptr<Profile> testing_profile_;
-  std::unique_ptr<TestTabStripModelDelegate> tab_strip_model_delegate_;
 };
 
 TEST_F(PinnedTabCollectionTest, AddOperation) {
@@ -130,4 +143,109 @@ TEST_F(PinnedTabCollectionTest, MoveOperation) {
   pinned_collection->MoveTab(tab_model_one_ptr, 4ul);
   EXPECT_EQ(pinned_collection->ChildCount(), 5ul);
   EXPECT_EQ(pinned_collection->GetIndexOfTabRecursive(tab_model_one_ptr), 4ul);
+}
+
+class TabGroupTabCollectionTest : public TabCollectionBaseTest {
+ public:
+  TabGroupTabCollectionTest() {
+    grouped_collection_ = std::make_unique<tabs::TabGroupTabCollection>(
+        tab_groups::TabGroupId::GenerateNew());
+  }
+  TabGroupTabCollectionTest(const TabGroupTabCollectionTest&) = delete;
+  TabGroupTabCollectionTest& operator=(const TabGroupTabCollectionTest&) =
+      delete;
+  ~TabGroupTabCollectionTest() override { grouped_collection_.reset(); }
+
+  tabs::TabGroupTabCollection* GetCollection() {
+    return grouped_collection_.get();
+  }
+
+  void AddTabs(int num) {
+    for (int i = 0; i < num; i++) {
+      std::unique_ptr<tabs::TabModel> tab_model =
+          std::make_unique<tabs::TabModel>(nullptr, GetTabStripModel());
+      tabs::TabModel* tab_model_ptr = tab_model.get();
+      GetCollection()->AppendTab(std::move(tab_model));
+      EXPECT_EQ(GetCollection()->GetIndexOfTabRecursive(tab_model_ptr),
+                GetCollection()->ChildCount() - 1);
+    }
+  }
+
+ private:
+  std::unique_ptr<tabs::TabGroupTabCollection> grouped_collection_;
+};
+
+TEST_F(TabGroupTabCollectionTest, AddOperation) {
+  auto tab_model_one =
+      std::make_unique<tabs::TabModel>(nullptr, GetTabStripModel());
+  auto tab_model_two =
+      std::make_unique<tabs::TabModel>(nullptr, GetTabStripModel());
+
+  tabs::TabModel* tab_model_one_ptr = tab_model_one.get();
+  tabs::TabModel* tab_model_two_ptr = tab_model_two.get();
+
+  EXPECT_FALSE(tab_model_one_ptr->GetParentCollectionForTesting());
+  tabs::TabGroupTabCollection* grouped_collection = GetCollection();
+
+  grouped_collection->AppendTab(std::move(tab_model_one));
+  EXPECT_EQ(tab_model_one_ptr->group(), grouped_collection->GetTabGroupId());
+  EXPECT_EQ(tab_model_one_ptr->GetParentCollectionForTesting(),
+            grouped_collection);
+  EXPECT_TRUE(grouped_collection->ContainsTabRecursive(tab_model_one_ptr));
+
+  AddTabs(4);
+
+  EXPECT_EQ(grouped_collection->ChildCount(), 5ul);
+  EXPECT_EQ(grouped_collection->TabCountRecursive(), 5ul);
+
+  grouped_collection->AddTab(std::move(tab_model_two), 3ul);
+  EXPECT_EQ(grouped_collection->GetIndexOfTabRecursive(tab_model_two_ptr), 3ul);
+}
+
+TEST_F(TabGroupTabCollectionTest, RemoveOperation) {
+  auto tab_model_one =
+      std::make_unique<tabs::TabModel>(nullptr, GetTabStripModel());
+  tabs::TabModel* tab_model_one_ptr = tab_model_one.get();
+
+  tabs::TabGroupTabCollection* grouped_collection = GetCollection();
+
+  // Add four tabs
+  AddTabs(4);
+
+  // Add `tab_model_one` to index 3.
+  grouped_collection->AddTab(std::move(tab_model_one), 3ul);
+  EXPECT_EQ(grouped_collection->GetIndexOfTabRecursive(tab_model_one_ptr), 3ul);
+  EXPECT_EQ(grouped_collection->ChildCount(), 5ul);
+
+  EXPECT_EQ(tab_model_one_ptr->group(), grouped_collection->GetTabGroupId());
+  auto removed_tab_model =
+      grouped_collection->MaybeRemoveTab(tab_model_one_ptr);
+  EXPECT_FALSE(removed_tab_model->group().has_value());
+  EXPECT_FALSE(removed_tab_model->GetParentCollectionForTesting());
+  EXPECT_EQ(grouped_collection->ChildCount(), 4ul);
+  EXPECT_EQ(removed_tab_model.get(), tab_model_one_ptr);
+}
+
+TEST_F(TabGroupTabCollectionTest, MoveOperation) {
+  auto tab_model_one =
+      std::make_unique<tabs::TabModel>(nullptr, GetTabStripModel());
+  tabs::TabModel* tab_model_one_ptr = tab_model_one.get();
+
+  tabs::TabGroupTabCollection* grouped_collection = GetCollection();
+
+  // Add four tabs
+  AddTabs(4);
+
+  // Add `tab_model_one` to index 3.
+  grouped_collection->AddTab(std::move(tab_model_one), 3ul);
+  EXPECT_EQ(grouped_collection->GetIndexOfTabRecursive(tab_model_one_ptr), 3ul);
+  EXPECT_EQ(grouped_collection->ChildCount(), 5ul);
+
+  grouped_collection->MoveTab(tab_model_one_ptr, 1ul);
+  EXPECT_EQ(grouped_collection->ChildCount(), 5ul);
+  EXPECT_EQ(grouped_collection->GetIndexOfTabRecursive(tab_model_one_ptr), 1ul);
+
+  grouped_collection->MoveTab(tab_model_one_ptr, 4ul);
+  EXPECT_EQ(grouped_collection->ChildCount(), 5ul);
+  EXPECT_EQ(grouped_collection->GetIndexOfTabRecursive(tab_model_one_ptr), 4ul);
 }
---
+++ chrome/browser/ui/tabs/tab_group_tab_collection.cc
@@ -0,0 +1,81 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <optional>
+
+#include "chrome/browser/ui/tabs/tab_collection_storage.h"
+#include "chrome/browser/ui/tabs/tab_group_tab_collection.h"
+#include "chrome/browser/ui/tabs/tab_model.h"
+#include "components/tab_groups/tab_group_id.h"
+
+namespace tabs {
+
+TabGroupTabCollection::TabGroupTabCollection(tab_groups::TabGroupId group_id)
+    : group_id_(group_id),
+      impl_(std::make_unique<TabCollectionStorage>(*this)) {}
+
+TabGroupTabCollection::~TabGroupTabCollection() = default;
+
+void TabGroupTabCollection::AddTab(std::unique_ptr<TabModel> tab_model,
+                                   size_t index) {
+  TabModel* inserted_tab_model = impl_->AddTab(std::move(tab_model), index);
+  inserted_tab_model->set_group(/*group=*/group_id_);
+  inserted_tab_model->OnReparented(this, GetPassKey());
+}
+
+void TabGroupTabCollection::AppendTab(std::unique_ptr<TabModel> tab_model) {
+  AddTab(std::move(tab_model), ChildCount());
+}
+
+void TabGroupTabCollection::MoveTab(TabModel* tab_model, size_t index) {
+  impl_->MoveTab(tab_model, index);
+}
+
+void TabGroupTabCollection::CloseTab(TabModel* tab_model) {
+  impl_->CloseTab(tab_model);
+}
+
+bool TabGroupTabCollection::ContainsTabRecursive(TabModel* tab_model) const {
+  return impl_->ContainsTab(tab_model);
+}
+
+bool TabGroupTabCollection::ContainsCollection(
+    TabCollection* collection) const {
+  return false;
+}
+
+std::optional<size_t> TabGroupTabCollection::GetIndexOfTabRecursive(
+    TabModel* tab_model) const {
+  return impl_->GetIndexOfTab(tab_model);
+}
+
+std::optional<size_t> TabGroupTabCollection::GetIndexOfCollection(
+    TabCollection* collection) const {
+  return std::nullopt;
+}
+
+std::unique_ptr<TabModel> TabGroupTabCollection::MaybeRemoveTab(
+    TabModel* tab_model) {
+  std::unique_ptr<TabModel> removed_tab_model = impl_->RemoveTab(tab_model);
+  removed_tab_model->set_group(/*group=*/std::nullopt);
+  removed_tab_model->OnReparented(nullptr, GetPassKey());
+  return removed_tab_model;
+}
+
+size_t TabGroupTabCollection::ChildCount() const {
+  return impl_->GetChildrenCount();
+}
+
+size_t TabGroupTabCollection::TabCountRecursive() const {
+  // Same as total number of children since there are no child collections.
+  return ChildCount();
+}
+
+std::unique_ptr<TabCollection> TabGroupTabCollection::MaybeRemoveCollection(
+    TabCollection* collection) {
+  return nullptr;
+}
+
+}  // namespace tabs
