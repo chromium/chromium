0b759c682f34f9718a9e81889e7572da695ed5bb
Siyu An <siyua@chromium.org>
2024-03-08 00:17:35 +0000

    [VCN] Add mediator and view controller for the error dialog
    Add both in one CL since we use the UIAlertController which has not
    consumer for it. Figured merging them in one CL will make it easier to
    review.
    
    Next CL will add handling dismissing events:
    1. When model controller calls Dismiss
    2. When cancel button of the alert view is tapped.
    
    Bug: b/324613263, b/324612991
    Change-Id: Ia7498e21821b190680e5c612ecfbc631907a80a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5297147
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1269920}

--- ios/chrome/browser/ui/autofill/error_dialog/autofill_error_dialog_coordinator.mm
+++ ios/chrome/browser/ui/autofill/error_dialog/autofill_error_dialog_coordinator.mm
@@ -10,11 +10,19 @@
 #import "components/autofill/core/browser/ui/payments/autofill_error_dialog_controller_impl.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/public/commands/autofill_commands.h"
+#import "ios/chrome/browser/ui/autofill/error_dialog/autofill_error_dialog_mediator.h"
+#import "ios/chrome/browser/ui/autofill/error_dialog/autofill_error_dialog_mediator_delegate.h"
 
 @implementation AutofillErrorDialogCoordinator {
   // The model layer controller. This model controller provide access to model
   // data and also handles interactions.
   std::unique_ptr<autofill::AutofillErrorDialogControllerImpl> _modelController;
+
+  // The C++ mediator class that connects the model controller and the IOS view
+  // implementation.
+  std::unique_ptr<AutofillErrorDialogMediator> _mediator;
+
+  __weak UIAlertController* _alertController;
 }
 
 - (instancetype)initWithBaseViewController:(UIViewController*)viewController
@@ -27,8 +35,49 @@
     _modelController =
         std::make_unique<autofill::AutofillErrorDialogControllerImpl>(
             std::move(errorContext));
+    _mediator = std::make_unique<AutofillErrorDialogMediator>(
+        _modelController->GetWeakPtr(), self);
   }
   return self;
+}
+
+#pragma mark - ChromeCoordinator
+
+- (void)start {
+  // Displays the error dialog. This is routed through the model-layer
+  // controller, which then invokes the mediator. This is done to allow the
+  // model to have a handle on the mediator, which implements a required
+  // model-layer interface.
+  // base::Unretained here is safe since the callback is invoked immediately
+  // after being passed to the `_modelController`.
+  _modelController->Show(base::BindOnce(&AutofillErrorDialogMediator::Show,
+                                        base::Unretained(_mediator.get())));
+}
+
+- (void)stop {
+  [_alertController dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - AutofillErrorDialogMediatorDelegate
+
+- (void)showErrorDialog:(NSString*)title
+                message:(NSString*)message
+            buttonLabel:(NSString*)buttonLabel {
+  UIAlertController* alertController =
+      [UIAlertController alertControllerWithTitle:title
+                                          message:message
+                                   preferredStyle:UIAlertControllerStyleAlert];
+  // TODO(b/324613367): For button action handler, call mediator OnDismissed.
+  UIAlertAction* buttonAction =
+      [UIAlertAction actionWithTitle:buttonLabel
+                               style:UIAlertActionStyleCancel
+                             handler:nil];
+  [alertController addAction:buttonAction];
+  alertController.modalPresentationStyle = UIModalPresentationOverFullScreen;
+  [self.baseViewController presentViewController:alertController
+                                        animated:YES
+                                      completion:nil];
+  _alertController = alertController;
 }
 
 @end
