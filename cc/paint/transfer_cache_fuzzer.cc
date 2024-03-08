90747f3c781df60312f3e4720e5036f791babd11
Brendon Tiszka <tiszka@chromium.org>
2024-03-08 21:50:01 +0000

    Improve TransferCacheEntry fuzzer
    * Disable logging.
    * Support unaligned memory on asan/release fuzzer builds
      to increase the odss of discovering bugs like
      crbug.com/327183408.
    
    Bug: 327254040
    Change-Id: I3708d811996d44a68ed53cabbee1748d94905e93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5328099
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270393}

--- cc/paint/transfer_cache_fuzzer.cc
+++ cc/paint/transfer_cache_fuzzer.cc
@@ -1,4 +1,4 @@
-// Copyright 2018 The Chromium Authors
+// Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,12 +12,16 @@
 #include "third_party/skia/include/core/SkSurface.h"
 #include "third_party/skia/include/gpu/GrDirectContext.h"
 
+struct Environment {
+  Environment() { logging::SetMinLogLevel(logging::LOGGING_FATAL); }
+};
+
 // TODO(crbug.com/1442381): Implement fuzzer with Skia Graphite backend.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  // Align data. ImageTransferCacheEntry requires 16-byte.
-  const uint8_t* aligned_data = base::bits::AlignUp(data, 16);
-  size_t alignment_gap = aligned_data - data;
-  if (size < alignment_gap + 4) {
+  static Environment env;
+
+  // Size required for fuzzing metadata.
+  if (size < 2) {
     return 0;
   }
 
@@ -33,7 +37,28 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     return 0;
   }
 
+#if DCHECK_IS_ON()
+  // Align data on debug builds. ImageTransferCacheEntry requires 16-byte
+  // alignment on debug builds. Note: Consume one byte becauase the first
+  // byte was used for `TransferCacheEntryType`
+  const uint8_t* aligned_data = base::bits::AlignUp(&data[1], 16);
+  size_t alignment_gap = aligned_data - data;
+  if (size < alignment_gap) {
+    return 0;
+  }
   base::span<const uint8_t> span(aligned_data, size - alignment_gap);
+#else
+  // Support memory backing to discover bugs in release builds that require
+  // unaligned memory.
+  size_t offset = data[1] % 16;
+  const uint8_t* unaligned_data = &data[2] + offset;
+  size_t unaligned_gap = unaligned_data - data;
+  if (size < unaligned_gap) {
+    return 0;
+  }
+  base::span<const uint8_t> span(unaligned_data, size - unaligned_gap);
+#endif
+
   if (!entry->Deserialize(context_provider->GrContext(),
                           /*graphite_recorder=*/nullptr, span)) {
     return 0;
