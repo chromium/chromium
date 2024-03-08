fde1df29f5cc5b304cc88f5bd81f5a8d3bfaf79a
Emily Shack <emshack@chromium.org>
2024-03-08 23:16:18 +0000

    Tab Organization: Fix TabOrganizationSession UAF
    Bug: b/328736351
    Change-Id: I3a7e798a50f36654cff1e3ad2feeac4279e49d52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5356957
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270468}

--- chrome/browser/ui/tabs/organization/tab_organization_session.cc
+++ chrome/browser/ui/tabs/organization/tab_organization_session.cc
@@ -109,6 +109,9 @@ TabOrganizationSession::~TabOrganizationSession() {
 
   if (request_) {
     request_->LogResults(this);
+    // The request may contain a callback which should happen before the
+    // destructor goes out of scope
+    request_.reset();
   }
 }
 
