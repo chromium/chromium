a1244c0f6cc60be5e6444670a59e07f0db5f748d
Lingqi Chi <lingqi@chromium.org>
2024-03-08 08:46:59 +0000

    gardener: disable AutoPictureInPictureWithVideoPlaybackBrowserTest.AllowOncePersistsUntilNavigation on win asan
    Bug: 328717201
    Change-Id: Ieac66863b5be9795edb47539d48aa63f8608466c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5355192
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270078}

--- chrome/browser/picture_in_picture/auto_picture_in_picture_tab_helper_browsertest.cc
+++ chrome/browser/picture_in_picture/auto_picture_in_picture_tab_helper_browsertest.cc
@@ -1063,8 +1063,15 @@ IN_PROC_BROWSER_TEST_F(AutoPictureInPictureTabHelperBrowserTest,
   tab_helper->set_auto_blocker_for_testing(nullptr);
 }
 
+// TODO(crbug.com/328717201): Test is flaky on win asan.
+#if BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)
+#define MAYBE_AllowOncePersistsUntilNavigation \
+  DISABLED_AllowOncePersistsUntilNavigation
+#else
+#define MAYBE_AllowOncePersistsUntilNavigation AllowOncePersistsUntilNavigation
+#endif
 IN_PROC_BROWSER_TEST_F(AutoPictureInPictureWithVideoPlaybackBrowserTest,
-                       AllowOncePersistsUntilNavigation) {
+                       MAYBE_AllowOncePersistsUntilNavigation) {
   LoadAutoVideoPipPage(browser());
   auto* web_contents = browser()->tab_strip_model()->GetActiveWebContents();
   PlayVideo(web_contents);
