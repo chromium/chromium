ee5fa93613adf032c588d6ed9d68936a15c678bf
Ian Barkley-Yeung <iby@chromium.org>
2024-03-09 00:12:45 +0000

    Add more information to DumpWithoutCrashing
    We're seen some crashes where the stack generated by a
    DumpWithoutCrashing is corrupted. Record the location in a crash
    key so that we can debug these corrupted crashes.
    
    BUG=b:324771555
    
    Change-Id: Ia8c5a4d0b31654c72f83202888c218d19a61ddaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5336524
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1270494}

--- base/debug/dump_without_crashing.cc
+++ base/debug/dump_without_crashing.cc
@@ -4,11 +4,16 @@
 
 #include "base/debug/dump_without_crashing.h"
 
+#include <map>
+#include <utility>
+
 #include "base/check.h"
+#include "base/debug/crash_logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
 #include "base/synchronization/lock.h"
 #include "base/trace_event/base_tracing.h"
+#include "build/buildflag.h"
 
 namespace {
 
@@ -91,6 +96,15 @@ bool DumpWithoutCrashing(const base::Location& location,
   TRACE_EVENT0("base", "DumpWithoutCrashing");
   if (dump_without_crashing_function_ &&
       ShouldDumpWithoutCrashWithLocation(location, time_between_dumps)) {
+#if !BUILDFLAG(IS_NACL)
+    // Record the location file and line so that in the case of corrupt stacks
+    // we're still getting accurate file/line information. See
+    // crbug.com/324771555.
+    SCOPED_CRASH_KEY_STRING256("DumpWithoutCrashing", "file",
+                               location.file_name());
+    SCOPED_CRASH_KEY_NUMBER("DumpWithoutCrashing", "line",
+                            location.line_number());
+#endif
     (*dump_without_crashing_function_)();
     base::UmaHistogramEnumeration("Stability.DumpWithoutCrashingStatus",
                                   DumpWithoutCrashingStatus::kUploaded);
