6cdbb075e42a55d812095655d0ac0d06487088b7
stluong <stluong@chromium.org>
2024-03-11 17:26:29 +0000

    Adjust hover card title and domain spacing
    The hover card contents spacing was adjusted for ChromeRefresh2023 but
    the spacing between the card title and domain was overlooked. This CL
    adds the additional spacing between the title and domain specified for
    ChromeRefresh2023
    
    Before fix: https://screenshot.googleplex.com/6XeFmfGnj2bLodF.png
    After fix: https://screenshot.googleplex.com/5svjbhYBabJ2ZuM.png
    
    Fixed: 326303277
    Change-Id: Ia6a4cfcdba42bb5a394b49696bc89a80d8965a13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5357496
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271038}

--- chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_bubble_view.cc
@@ -71,6 +71,7 @@ constexpr int kHoverCardTitleMaxLines = 2;
 
 constexpr int kHorizontalMargin = 18;
 constexpr int kVerticalMargin = 10;
+constexpr int kTitleDomainSpacing = 4;
 constexpr int kFootnoteVerticalMargin = 8;
 constexpr auto kTitleMargins =
     gfx::Insets::VH(kVerticalMargin, kHorizontalMargin);
@@ -431,8 +432,9 @@ TabHoverCardBubbleView::TabHoverCardBubbleView(Tab* tab)
       features::IsChromeRefresh2023() ? kTextAreaRefreshMargins : kTitleMargins;
   domain_label_->SetVisible(show_domain);
   if (show_domain) {
-    const gfx::Insets domain_margins = gfx::Insets::TLBR(
-        0, title_margins.left(), title_margins.bottom(), title_margins.right());
+    gfx::Insets domain_margins = title_margins;
+    domain_margins.set_top(features::IsChromeRefresh2023() ? kTitleDomainSpacing
+                                                           : 0);
     domain_label_->SetProperty(views::kMarginsKey, domain_margins);
     title_margins.set_bottom(0);
   }
