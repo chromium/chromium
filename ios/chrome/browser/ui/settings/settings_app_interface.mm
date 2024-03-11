5eef003060941d1a59e233e57e293deccdad2511
Federica Germinario <fedegermi@google.com>
2024-03-11 13:49:05 +0000

    [ios] Make SettingsAppInterface independent from mainBrowserState
    This CL changes SettingsAppInterface to take the viewController
    from the sceneState instead of taking it from the mainController.
    
    Fixed: 327328907
    Change-Id: I210d931650a4cfaaecb49b86ad46b67c130484fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5360855
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1270926}

--- ios/chrome/browser/ui/settings/settings_app_interface.mm
+++ ios/chrome/browser/ui/settings/settings_app_interface.mm
@@ -13,9 +13,9 @@
 #import "components/prefs/pref_service.h"
 #import "components/search_engines/prepopulated_engines.h"
 #import "components/search_engines/template_url_service.h"
-#import "ios/chrome/app/main_controller.h"
 #import "ios/chrome/browser/content_settings/model/host_content_settings_map_factory.h"
 #import "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider_interface.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
@@ -80,11 +80,9 @@ bool HostToLocalHostRewrite(GURL* url, web::BrowserState* browser_state) {
 }
 
 + (BOOL)settingsRegisteredKeyboardCommands {
-  // TODO(crbug.com/327328907): Avoid using
-  // mainController.browserProviderInterface.
+  SceneState* sceneState = chrome_test_util::GetForegroundActiveScene();
   UIViewController* viewController =
-      chrome_test_util::GetMainController()
-          .browserProviderInterface.mainBrowserProvider.viewController;
+      sceneState.browserProviderInterface.mainBrowserProvider.viewController;
   return viewController.presentedViewController.keyCommands != nil;
 }
 
