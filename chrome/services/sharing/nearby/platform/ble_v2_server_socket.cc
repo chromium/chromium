6c20b59c58ec18b42dd398c90f5f2b6416aa21e9
Daniel Classon <dclasson@google.com>
2024-03-12 17:47:39 +0000

    [Nearby Presence] Add stub implementation of Input/Output Stream
    Adds stubbed implementation for Input/Output stream in BLE v2.
    
    Test: Tested manually with BLE V2 flag, unit tests.
    
    Bug: b/320554697
    Change-Id: I8106de648cf7fb17c39a5c4f0d2bea62689daa19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5324116
    Reviewed-by: Jack Shira <jackshira@google.com>
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271667}

--- chrome/services/sharing/nearby/platform/ble_v2_server_socket.cc
+++ chrome/services/sharing/nearby/platform/ble_v2_server_socket.cc
@@ -4,31 +4,51 @@
 
 #include "chrome/services/sharing/nearby/platform/ble_v2_server_socket.h"
 
-#include "base/logging.h"
 #include "chrome/services/sharing/nearby/platform/ble_v2_remote_peripheral.h"
-
-#include "chromeos/ash/services/nearby/public/mojom/nearby_connections_types.mojom.h"
-#include "device/bluetooth/public/mojom/adapter.mojom.h"
 
 namespace nearby::chrome {
 
+// =================BleV2InputStream=================
+ExceptionOr<ByteArray> BleV2InputStream::Read(std::int64_t size) {
+  // Left deliberately unimplemented. Requires BLE Weave implementation.
+  return ExceptionOr<ByteArray>(Exception::kIo);
+}
+
+Exception BleV2InputStream::Close() {
+  // Left deliberately unimplemented. Requires BLE Weave implementation.
+  return {Exception::kSuccess};
+}
+
+// =================BleV2OutputStream=================
+Exception BleV2OutputStream::Write(const ByteArray& data) {
+  // Left deliberately unimplemented. Requires BLE Weave implementation.
+  return {Exception::kIo};
+}
+
+Exception BleV2OutputStream::Flush() {
+  // Left deliberately unimplemented. Requires BLE Weave implementation.
+  return {Exception::kSuccess};
+}
+
+Exception BleV2OutputStream::Close() {
+  // Left deliberately unimplemented. Requires BLE Weave implementation.
+  return {Exception::kSuccess};
+}
+
 // =================BleV2Socket=================
-BleV2Socket::BleV2Socket() {}
+BleV2Socket::BleV2Socket() {
+  input_stream_ = std::make_unique<BleV2InputStream>();
+  output_stream_ = std::make_unique<BleV2OutputStream>();
+}
 BleV2Socket::~BleV2Socket() {}
 
 InputStream& BleV2Socket::GetInputStream() {
   // Left deliberately unimplemented.
-  mojo::ScopedDataPipeConsumerHandle input_handle;
-  input_stream_ = std::make_unique<InputStreamImpl>(
-      connections::mojom::Medium::kBluetooth, nullptr, std::move(input_handle));
   return *input_stream_;
 }
 
 OutputStream& BleV2Socket::GetOutputStream() {
   // Left deliberately unimplemented.
-  mojo::ScopedDataPipeProducerHandle handle;
-  output_stream_ = std::make_unique<OutputStreamImpl>(
-      connections::mojom::Medium::kBluetooth, nullptr, std::move(handle));
   return *output_stream_;
 }
 
@@ -50,12 +70,43 @@ BleV2ServerSocket::BleV2ServerSocket() {}
 BleV2ServerSocket::~BleV2ServerSocket() {}
 
 std::unique_ptr<::nearby::api::ble_v2::BleSocket> BleV2ServerSocket::Accept() {
-  // Left deliberately unimplemented.
-  return std::make_unique<BleV2Socket>();
+  // This implementation of Accept intentionally blocks on the calling thread
+  // until Close is called, thus effectively never returning a `BleSocket`
+  // instance. This is done intentionally because the Chrome platform is not
+  // expected to implement the deprecated `BleSocket` interface, but Nearby
+  // Connections calls this Accept method on all platforms.
+  //
+  // The calling thread is a dedicated worker thread -- blocking here will not
+  // block the main thread.
+  //
+  // In the future, the Chrome platform will implement GattServer and its
+  // associated primitives, to allow Nearby Connections to compose its own
+  // BleV2ServerSocket via Weave. At that time this method can be revisited to
+  // be deleted.
+  //
+  // Calls to Accept post-Close will not block.
+  mutex_.Lock();
+
+  // Sleep forever until Close is called.
+  while (!closed_) {
+    condition_variable_.Wait();
+  }
+
+  mutex_.Unlock();
+  return nullptr;
 }
 
 Exception BleV2ServerSocket::Close() {
-  // Left deliberately unimplemented.
+  // This implementation of Close signals all blocked Accept threads to wake
+  // and exit. It also sets closed_ to true so that future calls to Accept do
+  // not block. Calling Close a second time is expected and is a no-op.
+  mutex_.Lock();
+
+  closed_ = true;
+
+  condition_variable_.Notify();
+
+  mutex_.Unlock();
   return {Exception::kSuccess};
 }
 
