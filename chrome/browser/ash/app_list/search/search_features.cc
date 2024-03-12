132807672f52b5a31625807345cb70971e997b05
Lauren Commeignes <laurencom@chromium.org>
2024-03-12 04:18:24 +0000

    Put System Info Answer Cards on TOT by default
    Bug: b/324478253
    Change-Id: I8a74d1d9bd62d6a79b39c52610cdbc1fa3552f4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5363993
    Reviewed-by: Dmitry Grebenyuk <dgrebenyuk@google.com>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1271362}

--- chrome/browser/ash/app_list/search/search_features.cc
+++ chrome/browser/ash/app_list/search/search_features.cc
@@ -47,7 +47,7 @@ BASE_FEATURE(kLauncherImageSearchOcr,
 
 BASE_FEATURE(kLauncherSystemInfoAnswerCards,
              "LauncherSystemInfoAnswerCards",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLauncherManateeForKeyboardShortcuts,
              "LauncherManateeForKeyboardShortcuts",
