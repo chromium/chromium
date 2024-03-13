1058f4b634445fe223986616c431671d9f615b03
Rubin Deliallisi <rubindl@chromium.org>
2024-03-13 17:04:08 +0000

    [Cake] Add content to Personalize Google Services page
    - Implement the VC view.
    - Add missing strings.
    
    Bug: 324091979
    Change-Id: I48760188cd7e6fc9afb8f1b00b5bfb2aff17ec94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5361593
    Auto-Submit: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272230}

--- ios/chrome/browser/ui/settings/google_services/personalize_google_services_view_controller.mm
+++ ios/chrome/browser/ui/settings/google_services/personalize_google_services_view_controller.mm
@@ -4,9 +4,31 @@
 
 #import "ios/chrome/browser/ui/settings/google_services/personalize_google_services_view_controller.h"
 
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/shared/ui/list_model/list_model.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_image_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_link_header_footer_item.h"
 #import "ios/chrome/browser/ui/settings/google_services/manage_sync_settings_constants.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
-#import "ui/base/l10n/l10n_util_mac.h"
+#import "ui/base/l10n/l10n_util.h"
+
+namespace {
+
+enum SectionIdentifier {
+  kSectionIdentifierLinkouts = kSectionIdentifierEnumZero
+};
+
+enum ItemType {
+  kItemTypeHeader = kItemTypeEnumZero,
+  kItemTypeWebAndAppActivity,
+  kItemTypeLinkedGoogleServices,
+};
+
+}  // namespace
 
 @implementation PersonalizeGoogleServicesViewController
 
@@ -17,6 +39,8 @@
   self.title = l10n_util::GetNSString(
       IDS_IOS_MANAGE_SYNC_PERSONALIZE_GOOGLE_SERVICES_TITLE);
   self.view.accessibilityIdentifier = kPersonalizeGoogleServicesViewIdentifier;
+
+  [self loadModel];
 }
 
 - (void)didMoveToParentViewController:(UIViewController*)parent {
@@ -25,6 +49,57 @@
     [self.presentationDelegate
         personalizeGoogleServicesViewcontrollerDidRemove:self];
   }
+}
+
+#pragma mark - SettingsRootTableViewController
+
+- (void)loadModel {
+  [super loadModel];
+  TableViewModel* model = self.tableViewModel;
+
+  // Linkouts section.
+  [model addSectionWithIdentifier:kSectionIdentifierLinkouts];
+
+  // Header item.
+  TableViewLinkHeaderFooterItem* headerItem =
+      [[TableViewLinkHeaderFooterItem alloc] initWithType:kItemTypeHeader];
+  headerItem.text = l10n_util::GetNSStringF(
+      IDS_IOS_PERSONALIZE_GOOGLE_SERVICES_HEADER,
+      l10n_util::GetStringUTF16(IDS_IOS_PERSONALIZE_GOOGLE_SERVICES_WAA_TITLE),
+      l10n_util::GetStringUTF16(
+          IDS_IOS_PERSONALIZE_GOOGLE_SERVICES_LINKED_SERVICES_TITLE));
+  [model setHeader:headerItem
+      forSectionWithIdentifier:kSectionIdentifierLinkouts];
+
+  // Web and App Activity item.
+  TableViewImageItem* webAndAppActivityItem =
+      [[TableViewImageItem alloc] initWithType:kItemTypeWebAndAppActivity];
+  webAndAppActivityItem.accessoryView = [[UIImageView alloc]
+      initWithImage:DefaultAccessorySymbolConfigurationWithRegularWeight(
+                        kExternalLinkSymbol)];
+  webAndAppActivityItem.accessoryView.tintColor =
+      [UIColor colorNamed:kTextQuaternaryColor];
+  webAndAppActivityItem.title =
+      l10n_util::GetNSString(IDS_IOS_PERSONALIZE_GOOGLE_SERVICES_WAA_TITLE);
+  webAndAppActivityItem.accessibilityTraits |= UIAccessibilityTraitButton;
+  [model addItem:webAndAppActivityItem
+      toSectionWithIdentifier:kSectionIdentifierLinkouts];
+
+  // Linked Google Services item.
+  TableViewImageItem* linkedGoogleServicesItem =
+      [[TableViewImageItem alloc] initWithType:kItemTypeLinkedGoogleServices];
+  linkedGoogleServicesItem.accessoryView = [[UIImageView alloc]
+      initWithImage:DefaultAccessorySymbolConfigurationWithRegularWeight(
+                        kExternalLinkSymbol)];
+  linkedGoogleServicesItem.accessoryView.tintColor =
+      [UIColor colorNamed:kTextQuaternaryColor];
+  linkedGoogleServicesItem.title = l10n_util::GetNSString(
+      IDS_IOS_PERSONALIZE_GOOGLE_SERVICES_LINKED_SERVICES_TITLE);
+  linkedGoogleServicesItem.detailText = l10n_util::GetNSString(
+      IDS_IOS_PERSONALIZE_GOOGLE_SERVICES_LINKED_SERVICES_DESCRIPTION);
+  linkedGoogleServicesItem.accessibilityTraits |= UIAccessibilityTraitButton;
+  [model addItem:linkedGoogleServicesItem
+      toSectionWithIdentifier:kSectionIdentifierLinkouts];
 }
 
 @end
