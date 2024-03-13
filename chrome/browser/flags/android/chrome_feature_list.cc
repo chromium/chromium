903fe4144876ae949a55ab442d6938b43586f04c
Gang Wu <gangwu@chromium.org>
2024-03-13 22:48:19 +0000

    Enable RelatedSearchesAllLanguage by default
    Bug: b:329086276
    Change-Id: Ifa963ffb94752779122b0f93b62915c36fed4f26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368289
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1272476}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -805,7 +805,7 @@ BASE_FEATURE(kReengagementNotification,
 
 BASE_FEATURE(kRelatedSearchesAllLanguage,
              "RelatedSearchesAllLanguage",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kReportParentalControlSitesChild,
              "ReportParentalControlSitesChild",
