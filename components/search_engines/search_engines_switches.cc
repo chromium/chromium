226d319f8d0366a84e1c191b848f8399c8f275a5
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-03-13 13:34:41 +0000

    🧇 Annotate feature flag as component export
    This is intended to fix Android component builds crashing on startup:
    
    ```
    [FATAL:feature_list.cc(743)] Check failed: CheckFeatureIdentity(feature). SearchEngineChoiceTrigger has multiple
    definitions. Either it is defined more than once in code or (for
    component builds) the code is built into multiple components (shared
    libraries) without a corresponding export statement
    ```
    
    Bug: None
    Change-Id: Iff0efe031eb47943c675eeafccc53fd117f12fe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5240839
    Reviewed-by: Menghan Yang <myuu@google.com>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272110}

--- components/search_engines/search_engines_switches.cc
+++ components/search_engines/search_engines_switches.cc
@@ -8,18 +8,22 @@ namespace switches {
 
 // Additional query params to insert in the search and instant URLs.  Useful for
 // testing.
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 const char kExtraSearchQueryParams[] = "extra-search-query-params";
 
 // Override the country used for search engine choice region checks.
 // Intended for testing. Expects 2-letter country codes.
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 const char kSearchEngineChoiceCountry[] = "search-engine-choice-country";
 
 // Disable the search engine choice screen for testing / autmation.
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 const char kDisableSearchEngineChoiceScreen[] =
     "disable-search-engine-choice-screen";
 
 // Force-enable showing the search engine choice screen for testing regardless
 // of region or choice already having been made.
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 const char kForceSearchEngineChoiceScreen[] =
     "force-search-engine-choice-screen";
 
@@ -28,6 +32,7 @@ const char kForceSearchEngineChoiceScreen[] =
 // The default feature state is split by platform to ease potential merges
 // that could be needed if we need to change the state while waterfalling this
 // feature.
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 BASE_FEATURE(kSearchEngineChoiceTrigger,
              "SearchEngineChoiceTrigger",
 #if BUILDFLAG(IS_ANDROID)
@@ -44,17 +49,21 @@ BASE_FEATURE(kSearchEngineChoiceTrigger,
 
 );
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 const base::FeatureParam<bool> kSearchEngineChoiceTriggerForTaggedProfilesOnly{
     &kSearchEngineChoiceTrigger, /*name=*/"for_tagged_profiles_only", true};
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 const base::FeatureParam<bool> kSearchEngineChoiceTriggerWithForceEeaCountry{
     &kSearchEngineChoiceTrigger, /*name=*/"with_force_eea_country", false};
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 const base::FeatureParam<std::string> kSearchEngineChoiceTriggerRepromptParams{
     &kSearchEngineChoiceTrigger,
     /*name=*/"reprompt",
     /*default_value=*/"{}"};
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 const base::FeatureParam<bool> kSearchEngineChoiceTriggerSkipFor3p{
     &kSearchEngineChoiceTrigger,
     /*name=*/"skip_for_3p",
--- components/search_engines/search_engines_switches.h
+++ components/search_engines/search_engines_switches.h
@@ -7,25 +7,34 @@
 
 #include <string>
 
+#include "base/component_export.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 
 namespace switches {
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 extern const char kExtraSearchQueryParams[];
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 extern const char kSearchEngineChoiceCountry[];
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 extern const char kDisableSearchEngineChoiceScreen[];
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 extern const char kForceSearchEngineChoiceScreen[];
 
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 BASE_DECLARE_FEATURE(kSearchEngineChoiceTrigger);
+
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 extern const base::FeatureParam<bool>
     kSearchEngineChoiceTriggerForTaggedProfilesOnly;
 
 // Forces the search engine choice country to Belgium. Used for testing
 // purposes.
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 extern const base::FeatureParam<bool>
     kSearchEngineChoiceTriggerWithForceEeaCountry;
 
@@ -40,11 +49,13 @@ extern const base::FeatureParam<bool>
 // This reprompts users in Belgium who made the choice strictly before version
 // 5, and users in all other countries who made the choice strictly before
 // version 2.
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 extern const base::FeatureParam<std::string>
     kSearchEngineChoiceTriggerRepromptParams;
 
 // Whether the search engine choice screen should be suppressed when the
 // default search engine is not Google.
+COMPONENT_EXPORT(SEARCH_ENGINES_SWITCHES)
 extern const base::FeatureParam<bool> kSearchEngineChoiceTriggerSkipFor3p;
 }  // namespace switches
 
