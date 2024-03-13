0959c5f0c8f358ca0f38b048390359bb4273ce08
David Yeung <dayeung@chromium.org>
2024-03-13 21:04:44 +0000

    Revert "Reland "Record WebUI NotifyScriptLoaded initialization time.""
    This reverts commit 247d60cd077eef87e7b71b24022c3885bde648a0.
    
    Reason for revert: Spike in crashes in AndroidWebView https://g-issues.chromium.org/issues/328348303. Investigation is also pretty much concluded for NotifyScriptLoaded so there is no need to keep this in the code.
    
    OBSOLETE_HISTOGRAM[Blink.NotifyScriptLoaded.TabSearch]=Investigation completed, no longer needed.
    
    Bug: 328348303, 41492614
    
    Original change's description:
    > Reland "Record WebUI NotifyScriptLoaded initialization time."
    >
    > This is a reland of commit 837bd7343ed45f95816a0e6728216bc22f6cd219
    > Revert was caused by a crash where fetching url led to a seg fault.
    >
    > Changes have been made to validate that we are not in a detached state and that the Url is available.
    >
    > Original change's description:
    > > Record WebUI NotifyScriptLoaded initialization time.
    > >
    > > Purpose of the metric is to investigate WebUI start up time in P99 scenarios. This CL specifically targets the time the renderer takes in `HTMLDocumentParser::NotifyScriptLoaded` when launching a TabSearch WebUI. This is meant to be a temporary change and will be removed after investigation is done.
    > >
    > > Proposal:
    > > https://docs.google.com/document/d/1Y86ieKaGDpVcIaaQyOvpkNEmErCDHgZdt1vyBPliQLc/edit?usp=sharing
    > >
    > > Bug: 1519190
    > > Change-Id: Ib5ecfb40f7613bedc47bdbbe41c79868aabe6345
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5204702
    > > Commit-Queue: David Yeung <dayeung@chromium.org>
    > > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > > Reviewed-by: Ian Clelland <iclelland@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1251650}
    >
    > Bug: 1519190
    > Change-Id: Iea39487e19b6eb8d0ae05914cefb599775dad769
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239214
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Commit-Queue: David Yeung <dayeung@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1253670}
    
    Bug: 1519190
    Change-Id: Iddbc9769ff703bf313ccf9893fafb45b7dbe9478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5362796
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1272411}

--- third_party/blink/renderer/core/html/parser/html_document_parser_loading_test.cc
+++ third_party/blink/renderer/core/html/parser/html_document_parser_loading_test.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/test/metrics/histogram_tester.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/html/parser/html_document_parser.h"
@@ -410,25 +409,6 @@ TEST_P(HTMLDocumentParserLoadingTest,
 
   css_async_resource.Complete("");
   platform_->RunUntilIdle();
-}
-
-TEST_P(HTMLDocumentParserLoadingTest, TestWithWebUIUrl) {
-  const char* histogram_name = "Blink.NotifyScriptLoaded.TabSearch";
-  base::HistogramTester histogram_tester;
-  SimRequest main_resource("chrome://tab-search.top-chrome/", "text/html");
-  SimRequest::Params params;
-  params.response_http_status = 200;
-  SimSubresourceRequest js("chrome://tab-search.top-chrome/non-existent.js",
-                           "application/javascript", params);
-  LoadURL("chrome://tab-search.top-chrome/");
-  main_resource.Complete(R"HTML(
-    <script src="non-existent.js"></script>
-  )HTML");
-  platform_->RunUntilIdle();
-  js.Complete("");
-  platform_->RunUntilIdle();
-
-  histogram_tester.ExpectTotalCount(histogram_name, 1);
 }
 
 }  // namespace blink
