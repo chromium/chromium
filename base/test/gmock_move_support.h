43b6e8e3728d58885b56f5ea62b044d82aac4c29
Daniel Cheng <dcheng@chromium.org>
2024-03-15 14:36:13 +0000

    Add documentation about mocking methods with multiple move-only args.
    Bug: 326047091
    Change-Id: I0acbdb6da7919bed46ca0f81d34707331fa004ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5368248
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273382}

--- base/test/gmock_move_support.h
+++ base/test/gmock_move_support.h
@@ -9,8 +9,37 @@
 #include <tuple>
 #include <utility>
 
-// A similar action as testing::SaveArg, but it does an assignment with
-// std::move() instead of always performing a copy.
+// Moves the `I`th argument to `out`. Analogous to `testing::SaveArg()`, which
+// copies instead.
+//
+// Example:
+//
+//   std::unique_ptr<int> result;
+//   EXPECT_CALL(mocked_object, Method).WillOnce(MoveArg<0>(&result));
+//   mocked_object.Method(std::make_unique<int>(123));
+//   EXPECT_THAT(result, Pointee(Eq(123)));
+//
+// Important: it is not possible to use multiple `MoveArg()` actions in a single
+// `DoAll()`: per the googlemock documentation, all but the last action receive
+// a read-only view of the arguments. Allowing an intermediate action to consume
+// the arguments would leave the original arguments in an unspecified state for
+// invoking subsequent actions, which is dubious.
+//
+// A simple workaround is to use `Invoke()` with a lambda instead, e.g.
+//
+//   std::unique_ptr<int> int_result;
+//   std::unique_ptr<double> double_result;
+//   EXPECT_CALL(mocked_object, Method).WillOnce(Invoke(
+//       [&] (auto&& arg1, auto&& arg2) {
+//         int_result = std::move(arg1);
+//         double_result = std::move(arg2);
+//         return 42;
+//       }));
+//   EXPECT_EQ(42, mocked_object.Method(std::make_unique<int>(123),
+//                                      std::make_unique<double>(0.5)));
+//   EXPECT_THAT(int_result, Pointee(Eq(123)));
+//   EXPECT_THAT(double_result, Pointee(DoubleEq(0.5)));
+
 template <size_t I = 0, typename T>
 auto MoveArg(T* out) {
   return [out](auto&&... args) {
@@ -19,22 +48,25 @@ auto MoveArg(T* out) {
 }
 
 // Moves the `I`th argument to `*out` and returns `return_value`.
-// This addresses that `DoAll(MoveArg(), Return())` does not work for moveable
-// types passed by value, since the first action only receives a read-only view
-// of its arguments.
+//
+// This is a convenience helper for code that wants to write the following:
+//
+//   DoAll(MoveArg<N>(&saved_arg), Return(value))
+//
+// The above is not actually possible to write because:
+// - `DoAll()` requires that its final action has a return type that matches the
+//   mocked call's return type. So `Return()` must be last.
+// - But any actions before the last receive a read-only view of the arguments.
+//   So `MoveArg()` receives a read-only view and cannot move out of it.
 //
 // Example:
 //
-// using MoveOnly = std::unique<int>;
-// struct MockFoo {
-//   MOCK_METHOD(bool, Fun, (MoveOnly), ());
-// };
+//   std::unique_ptr<int> result;
+//   EXPECT_CALL(mocked_object, Method).WillOnce(
+//       MoveArgAndReturn<0>(&result, true));
+//   EXPECT_TRUE(mocked_object.Method(std::make_unique<int>(123)));
+//   EXPECT_THAT(int_result, Pointee(Eq(123)));
 //
-// MoveOnly result;
-// MockFoo foo;
-// EXPECT_CALL(foo, Fun).WillOnce(MoveArgAndReturn(&result, true));
-// EXPECT_TRUE(foo.Fun(std::make_unique<int>(123)));
-// EXPECT_THAT(result, testing::Pointee(123));
 template <size_t I = 0, typename T1, typename T2>
 auto MoveArgAndReturn(T1* out, T2&& return_value) {
   return [out, value = std::forward<T2>(return_value)](auto&&... args) mutable {
