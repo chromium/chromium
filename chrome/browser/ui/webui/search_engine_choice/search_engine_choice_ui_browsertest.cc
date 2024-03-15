d4c8bfb3695205ee7a11741152794bee0d8426c2
Jack Yammine <jyammine@google.com>
2024-03-15 13:47:40 +0000

    [waffle]Fix icon and radio button position when snippet is expanded
    The search engine icon and radio button should keep their initial
    position when the snippet is expanded.
    
    Screenshot: http://b/329414835#comment2
    Fixed: b:329414835
    Change-Id: Ia1f111b26020480c2e1429922b51d09b5807ffda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5365879
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1273360}

--- chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui_browsertest.cc
+++ chrome/browser/ui/webui/search_engine_choice/search_engine_choice_ui_browsertest.cc
@@ -28,6 +28,7 @@
 #include "components/search_engines/template_url_service.h"
 #include "components/signin/public/base/signin_switches.h"
 #include "content/public/browser/host_zoom_map.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -93,6 +94,8 @@ struct TestParam {
   std::string test_suffix;
   bool use_dark_theme = false;
   bool use_right_to_left_language = false;
+  bool select_first_search_engine = false;
+  bool first_snippet_text_larger = false;
   gfx::Size dialog_dimensions = gfx::Size(988, 900);
 };
 
@@ -113,12 +116,53 @@ const TestParam kTestParams[] = {
     {.test_suffix = "NarrowSize", .dialog_dimensions = gfx::Size(300, 900)},
     {.test_suffix = "ShortAndNarrowSize",
      .dialog_dimensions = gfx::Size(500, 500)},
+    {.test_suffix = "LargerFirstEngineSnippet",
+     .first_snippet_text_larger = true},
+    {.test_suffix = "FirstEngineSelectedWithLargerSnippet",
+     .select_first_search_engine = true,
+     .first_snippet_text_larger = true},
 #endif
     // We enable the test on platforms other than Windows with the smallest
     // height due to a small maximum window height set by the operating system.
     // The test will crash if we exceed that height.
     {.test_suffix = "ShortSize", .dialog_dimensions = gfx::Size(988, 376)},
 };
+
+class SearchEngineChoiceNavigationObserver
+    : public content::TestNavigationObserver {
+ public:
+  explicit SearchEngineChoiceNavigationObserver(GURL url)
+      : content::TestNavigationObserver(url) {}
+
+  void NavigationOfInterestDidFinish(
+      content::NavigationHandle* navigation_handle) override {
+    web_contents_ = navigation_handle->GetWebContents();
+  }
+
+  content::WebContents* web_contents() const { return web_contents_; }
+
+ private:
+  raw_ptr<content::WebContents> web_contents_;
+};
+
+const char kSelectFirstSearchEngineJsString[] =
+    "(() => {"
+    "  const app = document.querySelector('search-engine-choice-app');"
+    "  const searchEngineList = app.shadowRoot.querySelectorAll("
+    "      'cr-radio-button');"
+    "  searchEngineList[0].click();"
+    "  return true;"
+    "})();";
+
+const char kMakeFirstSnippetLargerJsString[] =
+    "(() => {"
+    "const app = document.querySelector('search-engine-choice-app');"
+    "const marketingSnippet = "
+    "app.shadowRoot.querySelectorAll('.marketing-snippet');"
+    "marketingSnippet[0].textContent = "
+    "marketingSnippet[0].textContent.repeat(2);"
+    "return true;"
+    "})();";
 }  // namespace
 
 class SearchEngineChoiceUIPixelTest
@@ -159,7 +203,7 @@ class SearchEngineChoiceUIPixelTest
         /*dialog_disabled=*/false);
 
     GURL url = GURL(chrome::kChromeUISearchEngineChoiceURL);
-    content::TestNavigationObserver observer(url);
+    SearchEngineChoiceNavigationObserver observer(url);
     observer.StartWatchingNewWebContents();
 
     views::NamedWidgetShownWaiter widget_waiter(
@@ -185,6 +229,19 @@ class SearchEngineChoiceUIPixelTest
     ShowSearchEngineChoiceDialog(
         *browser(), gfx::Size(dialog_width, dialog_height), zoom_factor);
     widget_waiter.WaitIfNeededAndGet();
+
+    content::WebContents* web_contents = observer.web_contents();
+    CHECK(web_contents);
+
+    if (GetParam().select_first_search_engine) {
+      EXPECT_EQ(true, content::EvalJs(web_contents,
+                                      kSelectFirstSearchEngineJsString));
+    }
+
+    if (GetParam().first_snippet_text_larger) {
+      EXPECT_EQ(true,
+                content::EvalJs(web_contents, kMakeFirstSnippetLargerJsString));
+    }
     observer.Wait();
   }
 
