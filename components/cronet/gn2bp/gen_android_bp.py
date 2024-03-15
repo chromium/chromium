c4bf8686b021c9da533527d466162ce94faacd0c
Mohannad Farrag <aymanm@google.com>
2024-03-15 17:19:45 +0000

    Use AOSP version of android.test.rules in gn2bp
    Change-Id: I467b2e00b2e61f11bbd37a955179c813f0197efa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374293
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1273503}

--- components/cronet/gn2bp/gen_android_bp.py
+++ components/cronet/gn2bp/gen_android_bp.py
@@ -302,6 +302,9 @@ def add_androidx_junit_java_deps(module, arch):
 def add_androidx_test_runner_java_deps(module, arch):
     module.static_libs.add("androidx.test.runner")
 
+def add_androidx_test_rules_java_deps(module, arch):
+    module.static_libs.add("androidx.test.rules")
+
 
 def add_android_test_base_java_deps(module, arch):
     module.libs.add("android.test.base")
@@ -413,6 +416,8 @@ _builtin_deps = {
     add_androidx_activity_activity,
     '//third_party/androidx:androidx_fragment_fragment_java':
     add_androidx_fragment_fragment,
+    '//third_party/androidx:androidx_test_rules_java':
+    add_androidx_test_rules_java_deps,
 }
 builtin_deps = {
     "{}{}".format(key, suffix): value
