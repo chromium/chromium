0a9fd9d4e4db03b384ca18865f12d0119404e510
Daniel Cheng <dcheng@chromium.org>
2024-03-18 16:25:45 +0000

    Make kUnsafeSchemes a constexpr variable.
    This was actually being initialized at runtime, since the initializer
    was not a constant expression. base::MakeFixedFlatSet is becoming
    stricter and requiring consteval, so fixing this consists of two parts:
    - marking `kUnsafeSchemes` itself as constexpr
    - making all the URL constants constexpr
    
    This also fixes IWYU for url_constants.h in url_utils.cc.
    
    Bug: 41486252
    Change-Id: I54c0da71d65dc0f8aec3015ce10c3e2b23b56b16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372940
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274204}

--- url/url_constants.h
+++ url/url_constants.h
@@ -7,63 +7,60 @@
 
 #include <stddef.h>
 
-#include "base/component_export.h"
-
 namespace url {
 
-COMPONENT_EXPORT(URL) extern const char kAboutBlankURL[];
-COMPONENT_EXPORT(URL) extern const char16_t kAboutBlankURL16[];
-COMPONENT_EXPORT(URL) extern const char kAboutSrcdocURL[];
-COMPONENT_EXPORT(URL) extern const char16_t kAboutSrcdocURL16[];
+inline constexpr char kAboutBlankURL[] = "about:blank";
+inline constexpr char16_t kAboutBlankURL16[] = u"about:blank";
+inline constexpr char kAboutSrcdocURL[] = "about:srcdoc";
+inline constexpr char16_t kAboutSrcdocURL16[] = u"about:srcdoc";
 
-COMPONENT_EXPORT(URL) extern const char kAboutBlankPath[];
-COMPONENT_EXPORT(URL) extern const char16_t kAboutBlankPath16[];
-COMPONENT_EXPORT(URL) extern const char kAboutSrcdocPath[];
-COMPONENT_EXPORT(URL) extern const char16_t kAboutSrcdocPath16[];
+inline constexpr char kAboutBlankPath[] = "blank";
+inline constexpr char16_t kAboutBlankPath16[] = u"blank";
+inline constexpr char kAboutSrcdocPath[] = "srcdoc";
+inline constexpr char16_t kAboutSrcdocPath16[] = u"srcdoc";
 
-COMPONENT_EXPORT(URL) extern const char kAboutScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kAboutScheme16[];
-COMPONENT_EXPORT(URL) extern const char kBlobScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kBlobScheme16[];
-// The content scheme is specific to Android for identifying a stored file.
-COMPONENT_EXPORT(URL) extern const char kContentScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kContentScheme16[];
-COMPONENT_EXPORT(URL) extern const char kContentIDScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kContentIDScheme16[];
-COMPONENT_EXPORT(URL) extern const char kDataScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kDataScheme16[];
-COMPONENT_EXPORT(URL) extern const char kFileScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kFileScheme16[];
-COMPONENT_EXPORT(URL) extern const char kFileSystemScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kFileSystemScheme16[];
-COMPONENT_EXPORT(URL) extern const char kFtpScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kFtpScheme16[];
-COMPONENT_EXPORT(URL) extern const char kHttpScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kHttpScheme16[];
-COMPONENT_EXPORT(URL) extern const char kHttpsScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kHttpsScheme16[];
-COMPONENT_EXPORT(URL) extern const char kJavaScriptScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kJavaScriptScheme16[];
-COMPONENT_EXPORT(URL) extern const char kMailToScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kMailToScheme16[];
-COMPONENT_EXPORT(URL) extern const char kTelScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kTelScheme16[];
-COMPONENT_EXPORT(URL) extern const char kUrnScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kUrnScheme16[];
-COMPONENT_EXPORT(URL) extern const char kUuidInPackageScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kUuidInPackageScheme16[];
-COMPONENT_EXPORT(URL) extern const char kWebcalScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kWebcalScheme16[];
-COMPONENT_EXPORT(URL) extern const char kWsScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kWsScheme16[];
-COMPONENT_EXPORT(URL) extern const char kWssScheme[];
-COMPONENT_EXPORT(URL) extern const char16_t kWssScheme16[];
+inline constexpr char kAboutScheme[] = "about";
+inline constexpr char16_t kAboutScheme16[] = u"about";
+inline constexpr char kBlobScheme[] = "blob";
+inline constexpr char16_t kBlobScheme16[] = u"blob";
+inline constexpr char kContentScheme[] = "content";
+inline constexpr char16_t kContentScheme16[] = u"content";
+inline constexpr char kContentIDScheme[] = "cid";
+inline constexpr char16_t kContentIDScheme16[] = u"cid";
+inline constexpr char kDataScheme[] = "data";
+inline constexpr char16_t kDataScheme16[] = u"data";
+inline constexpr char kFileScheme[] = "file";
+inline constexpr char16_t kFileScheme16[] = u"file";
+inline constexpr char kFileSystemScheme[] = "filesystem";
+inline constexpr char16_t kFileSystemScheme16[] = u"filesystem";
+inline constexpr char kFtpScheme[] = "ftp";
+inline constexpr char16_t kFtpScheme16[] = u"ftp";
+inline constexpr char kHttpScheme[] = "http";
+inline constexpr char16_t kHttpScheme16[] = u"http";
+inline constexpr char kHttpsScheme[] = "https";
+inline constexpr char16_t kHttpsScheme16[] = u"https";
+inline constexpr char kJavaScriptScheme[] = "javascript";
+inline constexpr char16_t kJavaScriptScheme16[] = u"javascript";
+inline constexpr char kMailToScheme[] = "mailto";
+inline constexpr char16_t kMailToScheme16[] = u"mailto";
+inline constexpr char kTelScheme[] = "tel";
+inline constexpr char16_t kTelScheme16[] = u"tel";
+inline constexpr char kUrnScheme[] = "urn";
+inline constexpr char16_t kUrnScheme16[] = u"urn";
+inline constexpr char kUuidInPackageScheme[] = "uuid-in-package";
+inline constexpr char16_t kUuidInPackageScheme16[] = u"uuid-in-package";
+inline constexpr char kWebcalScheme[] = "webcal";
+inline constexpr char16_t kWebcalScheme16[] = u"webcal";
+inline constexpr char kWsScheme[] = "ws";
+inline constexpr char16_t kWsScheme16[] = u"ws";
+inline constexpr char kWssScheme[] = "wss";
+inline constexpr char16_t kWssScheme16[] = u"wss";
 
 // Used to separate a standard scheme and the hostname: "://".
-COMPONENT_EXPORT(URL) extern const char kStandardSchemeSeparator[];
-COMPONENT_EXPORT(URL) extern const char16_t kStandardSchemeSeparator16[];
+inline constexpr char kStandardSchemeSeparator[] = "://";
+inline constexpr char16_t kStandardSchemeSeparator16[] = u"://";
 
-COMPONENT_EXPORT(URL) extern const size_t kMaxURLChars;
+inline constexpr size_t kMaxURLChars = 2 * 1024 * 1024;
 
 }  // namespace url
 
