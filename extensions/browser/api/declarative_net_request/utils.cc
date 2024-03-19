7bc0347c9a623a5e13e1a63065dbbdbaf5a4bc4e
Kelvin Jiang <kelvinjiang@chromium.org>
2024-03-19 18:05:00 +0000

    [Extensions][DNR] Fix prefs deletion for delayed installs
    In PopulateExtensionInfoPrefs, certain prefs are supposed to be removed
    from the extension_dict. This works fine if extension_dict points
    directly to the extension's own prefs, but these prefs are not
    removed if it points to a dict for delayed install prefs that are
    meant to be applied to the extension's own prefs later (since delayed
    install prefs are created from scratch).
    
    To fix this, add a removed_prefs list which stores pref keys that
    will actually be removed when a delayed installation is completed.
    
    This was found to be the root cause of the DNR bug found below, and
    a regression test was added.
    
    Note: While this fix prevents this issue from popping up again, it
    cannot fix corrupted/stale prefs with values from a previous version
    of the extension that were not cleaned up for a delayed install or
    update.
    
    For the kDNRStaticRulesetPref, this was fixed by incrementing the
    ruleset format version which forces a re-index which will write the
    correct values to prefs.
    
    The extension just needs to call the
    declarativeNetRequest.UpdateEnabledStaticRulesets method to correct
    the value of the kDNREnabledStaticRulesetIDs pref.
    
    Bug: 40285683
    Change-Id: I8b985625118e1ea1ac32990041660b5339e1e2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371000
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275066}

--- extensions/browser/api/declarative_net_request/utils.cc
+++ extensions/browser/api/declarative_net_request/utils.cc
@@ -48,7 +48,7 @@ namespace flat_rule = url_pattern_index::flat;
 // url_pattern_index.fbs. Whenever an extension with an indexed ruleset format
 // version different from the one currently used by Chrome is loaded, the
 // extension ruleset will be reindexed.
-constexpr int kIndexedRulesetFormatVersion = 31;
+constexpr int kIndexedRulesetFormatVersion = 32;
 
 // This static assert is meant to catch cases where
 // url_pattern_index::kUrlPatternIndexFormatVersion is incremented without
