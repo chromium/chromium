1d5d119861371054f850fc10905fea9d82778eba
Dustin J. Mitchell <djmitche@chromium.org>
2024-03-19 13:59:42 +0000

    Print MessageFrame data, too
    Now that we are using DATAGRAM (Message) frames, the debugging output
    should include that data.
    
    Change-Id: I5fd693bd5448da8e56172cc8fdcd66a3f7ab41bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370892
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274878}

--- net/quic/quic_test_packet_printer.cc
+++ net/quic/quic_test_packet_printer.cc
@@ -187,6 +187,10 @@ class QuicPacketPrinter : public QuicFramerVisitorInterface {
   }
   bool OnMessageFrame(const QuicMessageFrame& frame) override {
     *output_ << "OnMessageFrame: " << frame;
+    // In a test context, `frame.data` should always be set.
+    CHECK(frame.data);
+    *output_ << "         data: { "
+             << base::HexEncode(frame.data, frame.message_length) << " }\n";
     return true;
   }
   bool OnHandshakeDoneFrame(const QuicHandshakeDoneFrame& frame) override {
