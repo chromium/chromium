0e19889ded699e1f8c8c3d14be986803428d466b
Mikel Astiz <mastiz@chromium.org>
2024-03-19 16:44:07 +0000

    [bookmarks] Fix batch upload deleting bookmarks
    In particular, there is one calling site to
    BookmarkModelView::RemoveAllSyncableNodes() from
    components/browser_sync/sync_client_utils.cc that can be exercised on
    iOS and is problematic if feature
    `syncer::kEnableBookmarkFoldersForAccountStorage` is enabled (or via
    chrome://flag#unified-bookmark-model).
    
    Before this patch,
    BookmarkModelViewUsingLocalOrSyncableNodes::RemoveAllSyncableNodes()
    assumed there is a dedicated BookmarkModel instance for local bookmarks,
    but this doesn't hold true when the feature is enabled.
    
    The solution is to avoid BookmarkModel::RemoveAllUserBookmarks() and
    instead achieve the same effect, but more selectively, by iterating
    through syncable permanent folders and removing all their children.
    
    Change-Id: I949e3902c1868ff83db15ff5c1beff53d8373f99
    Fixed: 330327396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372854
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1274989}

--- components/sync_bookmarks/bookmark_model_view.cc
+++ components/sync_bookmarks/bookmark_model_view.cc
@@ -182,9 +182,22 @@ void BookmarkModelViewUsingLocalOrSyncableNodes::EnsurePermanentNodesExist() {
 }
 
 void BookmarkModelViewUsingLocalOrSyncableNodes::RemoveAllSyncableNodes() {
-  // Relevant on iOS only, to delete all account bookmarks in a dedicated
-  // BookmarkModel instance.
-  underlying_model()->RemoveAllUserBookmarks();
+  underlying_model()->BeginExtensiveChanges();
+
+  for (const auto& permanent_node : root_node()->children()) {
+    if (!IsNodeSyncable(permanent_node.get())) {
+      continue;
+    }
+
+    for (int i = static_cast<int>(permanent_node->children().size() - 1);
+         i >= 0; --i) {
+      underlying_model()->Remove(
+          permanent_node->children()[i].get(),
+          bookmarks::metrics::BookmarkEditSource::kOther);
+    }
+  }
+
+  underlying_model()->EndExtensiveChanges();
 }
 
 const bookmarks::BookmarkNode*
