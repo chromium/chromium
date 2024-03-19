2aeb006d83d5cf0e3edfd0ad8884065c3f713a96
Rasika Navarange <rasikan@google.com>
2024-03-19 21:33:40 +0000

    Run StdlibSmoke diff tests in perfetto_diff_tests target
    Run smoke tests for the entire stdlib in Chromium as well to prevent
    changes that break the Perfetto diff test suite when we roll the
    Chromium stdlib.
    
    Change-Id: I9e8fca5538db143ea65a870a779d02799f47bb92
    Bug: 315943893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372058
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Rasika Navarange <rasikan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1275199}

--- base/tracing/test/trace_processor/diff_tests/include_index.py
+++ base/tracing/test/trace_processor/diff_tests/include_index.py
@@ -4,6 +4,7 @@
 # found in the LICENSE file.
 
 import os
+import sys
 from typing import List
 
 from python.generators.diff_tests import testing
@@ -11,9 +12,19 @@ from chrome.tests import ChromeStdlib
 from chrome.tests_scroll_jank import ChromeScrollJankStdlib
 from chrome.tests_chrome_interactions import ChromeInteractions
 
+# Import diff tests from third_party/perfetto/test/trace_processor
+PERFETTO_DIFF_TEST_DIR = os.path.abspath(os.path.join(os.path.dirname(
+                    __file__),
+                    '..', '..', '..', '..', '..',
+                    'third_party', 'perfetto',
+                    'test', 'trace_processor'))
+sys.path.append(PERFETTO_DIFF_TEST_DIR)
+from diff_tests.stdlib.tests import StdlibSmoke
+
 def fetch_all_diff_tests(index_path: str) -> List['testing.TestCase']:
   test_data_path = os.path.abspath(os.path.join(__file__, '../../../data'))
   return [
+      *StdlibSmoke(index_path, 'stdlib', 'StdlibSmoke').fetch(),
       *ChromeScrollJankStdlib(index_path, 'chrome', 'ChromeScrollJankStdlib',
                               test_data_path).fetch(),
       *ChromeStdlib(index_path, 'chrome', 'ChromeStdlib',
