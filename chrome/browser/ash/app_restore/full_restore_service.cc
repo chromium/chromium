76f54e87b8b3f16f9b1aeb9ebe21469955815f0b
Sammie Quon <sammiequon@chromium.org>
2024-03-20 19:24:58 +0000

    pine: Pass correct tab count data to ash
    Test: manual
    Bug: none
    Change-Id: Ib416e5f3edcb3c088fb0bfa22dc3a2b3a4d7fcf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373796
    Reviewed-by: Min Chen <minch@chromium.org>
    Auto-Submit: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1275769}

--- chrome/browser/ash/app_restore/full_restore_service.cc
+++ chrome/browser/ash/app_restore/full_restore_service.cc
@@ -772,7 +772,7 @@ std::unique_ptr<PineContentsData> FullRestoreService::CreatePineContentsData(
         }
       }
       pine_contents_data->apps_infos.emplace_back(app_id, tab_title, tab_urls,
-                                                  tab_urls.size());
+                                                  tabs.size());
     }
   }
   return pine_contents_data;
