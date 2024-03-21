d79148eacfa2f92f9b880201b177c0e767741e18
Junji Watanabe <jwata@google.com>
2024-03-21 05:47:57 +0000

    [siso] Add starlark config format check to PRESUBMIT.py
    This CL adds CheckSisoConfigFormat presubmit check which runs
    `lucicfg fmt -dry-run` command against modified Siso config files.
    
    The error message looks like this.
    
    ❯ git cl presubmit -f --parallel
    ** Presubmit ERRORS: 1 **
    Validate /usr/local/google/home/jwata/chromium2/src/build/config/siso/v8.star
    lucicfg fmt -dry-run -log-level warning /usr/local/google/home/jwata/chromium2/src/build/config/siso/v8.star (0.10s) failed
    /usr/local/google/home/jwata/chromium2/src/build/config/siso/v8.star: needs formatting
    Error: Some files need formatting
    
    Bug: 330636153
    Fixed: 330636153
    Change-Id: I7e48a561e1669008c4d46722fb9e64acb412e45d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5383868
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Auto-Submit: Junji Watanabe <jwata@google.com>
    Commit-Queue: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276060}

--- build/config/siso/PRESUBMIT.py
+++ build/config/siso/PRESUBMIT.py
@@ -19,3 +19,18 @@ def CheckTryjobFooters(input_api, output_api):
       "Cq-Include-Trybots: luci.chromium.try:linux_chromium_tsan_siso_rel_ng\n"
   )
   return [output_api.PresubmitPromptWarning(message)]
+
+
+def CheckSisoConfigFormat(input_api, output_api):
+  """Check if build/config/siso/*.star files are formatted correctly."""
+  repo_root = input_api.change.RepositoryRoot()
+  log_level = 'debug' if input_api.verbose else 'warning'
+  commands = []
+  for f in input_api.AffectedFiles():
+    filepath = f.AbsoluteLocalPath()
+    if not filepath.endswith('.star'):
+      continue
+    name = 'Validate ' + filepath
+    cmd = ['lucicfg', 'fmt', '-dry-run', '-log-level', log_level, filepath]
+    commands.append(input_api.Command(name, cmd, {}, output_api.PresubmitError))
+  return input_api.RunTests(commands)
