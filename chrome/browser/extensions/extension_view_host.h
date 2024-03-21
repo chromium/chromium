d483c533511735b687994f6d9120fa4bf99f628b
EmiliaPaz <emiliapaz@chromium.org>
2024-03-21 17:37:26 +0000

    [Extensions] Move UnhandledKeyboardEvent() to private method
    UnhandledKeyboardEvent() is only called by a method in the same class.
    Thus we can drop 'virtual' and move to private.
    
    Bug: n/a
    Change-Id: Iaa1730d8427c2c42c17fd805b461bf4a12c9b004
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384907
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276345}

--- chrome/browser/extensions/extension_view_host.h
+++ chrome/browser/extensions/extension_view_host.h
@@ -54,13 +54,6 @@ class ExtensionViewHost
   // Returns the browser associated with this ExtensionViewHost.
   virtual Browser* GetBrowser();
 
-  // Handles keyboard events that were not handled by HandleKeyboardEvent().
-  // Platform specific implementation may override this method to handle the
-  // event in platform specific way. Returns whether the events are handled.
-  virtual bool UnhandledKeyboardEvent(
-      content::WebContents* source,
-      const content::NativeWebKeyboardEvent& event);
-
   // ExtensionHost
   void OnDidStopFirstLoad() override;
   void LoadInitialURL() override;
@@ -117,6 +110,12 @@ class ExtensionViewHost
   // Returns whether the provided event is a raw escape keypress in a
   // mojom::ViewType::kExtensionPopup.
   bool IsEscapeInPopup(const content::NativeWebKeyboardEvent& event) const;
+
+  // Handles keyboard events that were not handled by HandleKeyboardEvent().
+  // Platform specific implementation may override this method to handle the
+  // event in platform specific way. Returns whether the events are handled.
+  bool UnhandledKeyboardEvent(content::WebContents* source,
+                              const content::NativeWebKeyboardEvent& event);
 
   // The browser associated with the ExtensionView, if any. Note: since this
   // ExtensionViewHost could be associated with a browser even if `browser_` is
