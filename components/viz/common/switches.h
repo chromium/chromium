d1959ba139b3226d29d5c34ca2f532298ddd2cc2
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-03-21 20:56:00 +0000

    Add switch `draw-quad-split-limit`
    This switch specifies the maximum number of quad splits during
    occlusion culling.
    
    Bug: b/330617490
    Change-Id: Ia3d6eecffb27b0dc11df1e7df5b09ddcfb05f5d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5385931
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276487}

--- components/viz/common/switches.h
+++ components/viz/common/switches.h
@@ -19,6 +19,7 @@ VIZ_COMMON_EXPORT extern const char kDeadlineToSynchronizeSurfaces[];
 VIZ_COMMON_EXPORT extern const char kDisableAdpf[];
 VIZ_COMMON_EXPORT extern const char kDisableFrameRateLimit[];
 VIZ_COMMON_EXPORT extern const char kDoubleBufferCompositing[];
+VIZ_COMMON_EXPORT extern const char kDrawQuadSplitLimit[];
 VIZ_COMMON_EXPORT extern const char kEnableHardwareOverlays[];
 VIZ_COMMON_EXPORT extern const char kRunAllCompositorStagesBeforeDraw[];
 VIZ_COMMON_EXPORT extern const char kShowAggregatedDamage[];
