e1eb9b261dabda2c18c3429fd6d59f645903da36
rudranshd <rudranshd@google.com>
2024-03-21 18:49:53 +0000

    Wrap Quick Settings error message text
    Wraps the Quick Settings error message text so that multi-line messages
    are supported.
    
    Screenshot(before/unwrapped):
    https://screenshot.googleplex.com/9GNMsh8VgwmUZYT.png
    Screenshot(after/wrapped):
    https://screenshot.googleplex.com/9mGA6286tZcWRvW.png
    
    Bug: b/307783129
    Change-Id: Id5e410fe12fbd208c3184c97e62b299fc47919cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5257757
    Reviewed-by: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Rudransh Dikshit <rudranshd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276406}

--- ash/system/tray/tray_info_label.cc
+++ ash/system/tray/tray_info_label.cc
@@ -6,16 +6,23 @@
 
 #include "ash/style/ash_color_id.h"
 #include "ash/style/typography.h"
+#include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_utils.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/views/layout/fill_layout.h"
+#include "ui/views/layout/flex_layout.h"
 
 namespace ash {
 
 TrayInfoLabel::TrayInfoLabel(int message_id)
     : label_(TrayPopupUtils::CreateDefaultLabel()) {
-  SetLayoutManager(std::make_unique<views::FillLayout>());
+  SetLayoutManager(std::make_unique<views::FlexLayout>())
+      ->SetOrientation(views::LayoutOrientation::kVertical);
+  // TODO(b/330548312): Wrap label properly without workaround
+  label_->SetMultiLine(true);
+  label_->SetMaximumWidth(kTrayMenuWidth - kMenuExtraMarginFromLeftEdge -
+                          kWideTrayPopupItemMinStartWidth -
+                          kTrayPopupPaddingHorizontal);
 
   TriView* tri_view = TrayPopupUtils::CreateDefaultRowView(
       /*use_wide_layout=*/false);
