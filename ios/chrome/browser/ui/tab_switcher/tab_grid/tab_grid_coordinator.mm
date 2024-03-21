f0aa1483c9123e5f533ca8968e81322f0c4a4e86
Gauthier Ambard <gambard@chromium.org>
2024-03-21 15:26:27 +0000

    [iOS] Only trigger BVC's fullscreen update when the size class changes
    Currently any change to the traitCollection triggers an update of the
    elements BVC. It seems that they only need to listen to the size class
    so only trigger the update if the size class changes.
    
    The issue here is that the fullscreen model's offset is only updated
    when the BVC becomes visible. However, the trait collection is updated
    before it is visible. So traitCollectionDidChange is called while the
    fullscreen model still has the offset of the previous tab (which was
    scrolled to the top). At the end of the trait collection updates, the
    model makes sure that the WebView is still scrolled to the top. But
    the WebView has changed before trait collection was called, so the
    model is scrolling to the top the new WebView, which shouldn't be
    scrolled to the top.
    
    This CL also uses the SceneState's window to override the user
    interface style as it is more clear about the purpose.
    
    Another solution would have been to fix fullscreen and change the way
    it is listening to content offset updates.
    
    Bug: 328766487
    Change-Id: I7c7b3aadb513c657fe9630100e30dd14f668c0e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5378323
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1276236}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_coordinator.mm
@@ -407,6 +407,7 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
   }
 
   __weak __typeof(self) weakSelf = self;
+  __weak UIWindow* sceneWindow = sceneState.window;
 
   ProceduralBlock transitionCompletionBlock = ^{
     __typeof(self) strongSelf = weakSelf;
@@ -421,8 +422,7 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
       [strongSelf displayBringAndroidTabsPrompt];
     }
 
-    strongSelf.baseViewController.view.window.overrideUserInterfaceStyle =
-        UIUserInterfaceStyleDark;
+    sceneWindow.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
   };
 
   ProceduralBlock transitionBlock = ^{
@@ -505,7 +505,8 @@ bool FindNavigatorShouldBePresentedInBrowser(Browser* browser) {
     self.tabGridEnterTime = base::TimeTicks();
   }
 
-  self.baseViewController.view.window.overrideUserInterfaceStyle =
+  SceneState* sceneState = self.regularBrowser->GetSceneState();
+  sceneState.window.overrideUserInterfaceStyle =
       UIUserInterfaceStyleUnspecified;
 
   // If another BVC is already being presented, swap this one into the
