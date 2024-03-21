f43d7794be02bc8ab9fe00f9d6ffa26e686e937e
Adam Rice <ricea@chromium.org>
2024-03-21 05:43:39 +0000

    Revert "mahi: binds the screen ai content extraction."
    This reverts commit 83fb08c27bb8d559eea9f4f8753dce00a4e052e3.
    
    Reason for revert: Suspected of causing msan bot failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/41993/overview
    
    Original change's description:
    > mahi: binds the screen ai content extraction.
    >
    > Bug: b/318565573
    > Change-Id: I77780e5c32943c3ac128777e6cd0b829c8b5a00b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373702
    > Commit-Queue: CJ Huang <chenjih@google.com>
    > Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1275959}
    
    Bug: b/318565573
    Change-Id: I37c22e99c7a1a0225198d478dd8c93c02257370e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384867
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Adam Rice <ricea@google.com>
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276058}

--- chromeos/components/mahi/ax_tree_extractor.h
+++ chromeos/components/mahi/ax_tree_extractor.h
@@ -5,7 +5,6 @@
 #ifndef CHROMEOS_COMPONENTS_MAHI_AX_TREE_EXTRACTOR_H_
 #define CHROMEOS_COMPONENTS_MAHI_AX_TREE_EXTRACTOR_H_
 
-#include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chromeos/components/mahi/public/mojom/content_extraction.mojom.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -18,8 +17,6 @@ using ExtractContentCallback =
     mojom::ContentExtractionService::ExtractContentCallback;
 using GetContentSizeCallback =
     mojom::ContentExtractionService::GetContentSizeCallback;
-using OnAxTreeDistilledCallback =
-    base::OnceCallback<void(const std::vector<ui::AXNodeID>&)>;
 
 // `AXTreeExtractor` is a class that distills an AXTreeUpdate, and then either
 // extracts the contents or gets the size of the contents.
@@ -45,22 +42,17 @@ class AXTreeExtractor {
                       GetContentSizeCallback callback);
 
  private:
-  void DistillViaAlgorithm(const ui::AXTree* tree,
+  void DistillViaAlgorithm(const ui::AXTree& tree,
                            std::vector<ui::AXNodeID>* content_node_ids);
 
-  void OnGetScreen2xResult(
-      std::vector<ui::AXNodeID> content_node_ids_algorithm,
-      OnAxTreeDistilledCallback on_ax_tree_distilled_callback,
-      const std::vector<ui::AXNodeID>& content_node_ids_screen2x);
-
   void OnDistilledForContentExtraction(
-      std::unique_ptr<ui::AXTree> tree,
+      const ui::AXTree& tree,
       ExtractContentCallback callback,
       mojom::ResponseStatus status,
       const std::vector<ui::AXNodeID>& content_node_ids);
 
   void OnDistilledForContentSize(
-      std::unique_ptr<ui::AXTree> tree,
+      const ui::AXTree& tree,
       GetContentSizeCallback callback,
       mojom::ResponseStatus status,
       const std::vector<ui::AXNodeID>& content_node_ids);
