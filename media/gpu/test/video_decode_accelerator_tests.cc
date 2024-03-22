889e953de1b4a86daa5e042fdac64c6900e8cb20
Bijan Choobineh <bchoobineh@chromium.org>
2024-03-22 02:45:50 +0000

    media/gpu: Update v_d_a tests to use Test Suites
    Previous to this CL, the |video_decode_accelerator_tests| were set up to
    use the GTest framework. This framework ran tests but did not have test
    report generation features, and as such could not be integrated with
    ChromiumCQ.
    
    Updates the |video_decode_accelerator_tests| to use Test Suites instead
    through the |video_decode_accelerator_test_suite| class. Based on how
    Test Suites are run, all of the test input parsing logic was moved from
    the v_d_a test main to the test suite class.
    
    Bug: b:326097522
    Test: ran v_d_a and v_e_a tests on cherry, brya, and corsola.
    Change-Id: Ic9449667c1cec10dadd88f3429601fe0db5ae9d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384796
    Reviewed-by: Steve Cho <stevecho@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Auto-Submit: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Commit-Queue: Bijan Choobineh‎ <bchoobineh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276665}

--- media/gpu/test/video_decode_accelerator_tests.cc
+++ media/gpu/test/video_decode_accelerator_tests.cc
@@ -16,6 +16,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/test/bind.h"
+#include "base/test/launcher/unit_test_launcher.h"
 #include "build/build_config.h"
 #include "media/base/decoder_buffer.h"
 #include "media/base/encryption_scheme.h"
@@ -27,6 +28,7 @@
 #include "media/filters/dav1d_video_decoder.h"
 #include "media/gpu/chromeos/platform_video_frame_pool.h"
 #include "media/gpu/test/video_bitstream.h"
+#include "media/gpu/test/video_decode_accelerator_test_suite.h"
 #include "media/gpu/test/video_frame_file_writer.h"
 #include "media/gpu/test/video_frame_validator.h"
 #include "media/gpu/test/video_player/decoder_listener.h"
@@ -46,73 +48,7 @@ namespace test {
 
 namespace {
 
-// Video decoder tests usage message. Make sure to also update the documentation
-// under docs/media/gpu/video_decoder_test_usage.md when making changes here.
-constexpr const char* usage_msg =
-    R"(usage: video_decode_accelerator_tests
-           [-v=<level>] [--vmodule=<config>]
-           [--validator_type=(none|md5|ssim)]
-           [--output_frames=(all|corrupt)] [--output_format=(png|yuv)]
-           [--output_limit=<number>] [--output_folder=<folder>]
-           [--linear_output] ([--use-legacy]|[--use_vd_vda])
-           [--use-gl=<backend>] [--gtest_help] [--help]
-           [<video path>] [<video metadata path>]
-)";
-
-// Video decoder tests help message.
-const std::string help_msg =
-    std::string(
-        R"""(Run the video decode accelerator tests on the video specified by
-<video path>. If no <video path> is given the default
-"test-25fps.h264" video will be used.
-
-The <video metadata path> should specify the location of a json file
-containing the video's metadata, such as frame checksums. By default
-<video path>.json will be used.
-
-The following arguments are supported:
-   -v                   enable verbose mode, e.g. -v=2.
-  --vmodule             enable verbose mode for the specified module,
-                        e.g. --vmodule=*media/gpu*=2.
-
-  --validator_type      validate decoded frames, possible values are
-                        md5 (default, compare against md5hash of expected
-                        frames), ssim (compute SSIM against expected
-                        frames, currently allowed for AV1 streams only)
-                        and none (disable frame validation).
-  --use-legacy          use the legacy VDA-based video decoders.
-  --use_vd_vda          use the new VD-based video decoders with a
-                        wrapper that translates to the VDA interface,
-                        used to test interaction with older components
-  --linear_output       use linear buffers as the final output of the
-                        decoder which may require the use of an image
-                        processor internally. This flag only works in
-                        conjunction with --use_vd_vda.
-                        Disabled by default.
-  --output_frames       write the selected video frames to disk, possible
-                        values are "all|corrupt".
-  --output_format       set the format of frames saved to disk, supported
-                        formats are "png" (default) and "yuv".
-  --output_limit        limit the number of frames saved to disk.
-  --output_folder       set the folder used to store frames, defaults to
-                        "<testname>".
-  --use-gl              specify which GPU backend to use, possible values
-                        include desktop (GLX), egl (GLES w/ ANGLE), and
-                        swiftshader (software rendering)""") +
-#if defined(ARCH_CPU_ARM_FAMILY)
-    R"""(
-  --disable-libyuv      use hw format conversion instead of libYUV.
-                        libYUV will be used by default, unless the
-                        video decoder format is not supported;
-                        in that case the code will try to use the
-                        v4l2 image processor.)""" +
-#endif  // defined(ARCH_CPU_ARM_FAMILY)
-    R"""(
-  --gtest_help          display the gtest help and exit.
-  --help                display this help and exit.
-)""";
-
-media::test::VideoPlayerTestEnvironment* g_env;
+media::test::VideoDecodeAcceleratorTestSuite* g_env;
 
 // Video decode test class. Performs setup and teardown for each single test.
 class VideoDecoderTest : public ::testing::Test {
@@ -710,175 +646,11 @@ TEST_F(VideoDecoderTest, InitializeWithNonSupportedConfig) {
 }  // namespace media
 
 int main(int argc, char** argv) {
-  // Set the default test data path.
-  media::test::VideoBitstream::SetTestDataPath(media::GetTestDataPath());
+  media::test::g_env =
+      media::test::VideoDecodeAcceleratorTestSuite::Create(argc, argv);
 
-  // Print the help message if requested. This needs to be done before
-  // initializing gtest, to overwrite the default gtest help message.
-  base::CommandLine::Init(argc, argv);
-  base::CommandLine* cmd_line = base::CommandLine::ForCurrentProcess();
-  LOG_ASSERT(cmd_line);
-  if (cmd_line->HasSwitch("help")) {
-    std::cout << media::test::usage_msg << "\n" << media::test::help_msg;
-    return 0;
-  }
-
-  // Check if a video was specified on the command line.
-  base::CommandLine::StringVector args = cmd_line->GetArgs();
-  base::FilePath video_path =
-      (args.size() >= 1) ? base::FilePath(args[0]) : base::FilePath();
-  base::FilePath video_metadata_path =
-      (args.size() >= 2) ? base::FilePath(args[1]) : base::FilePath();
-
-  // Parse command line arguments.
-  auto validator_type =
-      media::test::VideoPlayerTestEnvironment::ValidatorType::kMD5;
-  media::test::FrameOutputConfig frame_output_config;
-  base::FilePath::StringType output_folder = base::FilePath::kCurrentDirectory;
-  bool use_legacy = false;
-  bool use_vd_vda = false;
-  bool linear_output = false;
-  std::vector<base::test::FeatureRef> disabled_features;
-  std::vector<base::test::FeatureRef> enabled_features;
-
-  media::test::DecoderImplementation implementation =
-      media::test::DecoderImplementation::kVD;
-  base::CommandLine::SwitchMap switches = cmd_line->GetSwitches();
-  for (base::CommandLine::SwitchMap::const_iterator it = switches.begin();
-       it != switches.end(); ++it) {
-    if (it->first.find("gtest_") == 0 ||  // Handled by GoogleTest
-                                          // Options below are handled by Chrome
-        it->first == "use-gl" || it->first == "v" || it->first == "vmodule" ||
-        it->first == "enable-features" || it->first == "disable-features" ||
-        it->first == "test-launcher-shard-index" ||
-        it->first == "test-launcher-summary-output" ||
-        it->first == "test-launcher-total-shards") {
-      continue;
-    }
-
-    if (it->first == "validator_type") {
-      if (it->second == "none") {
-        validator_type =
-            media::test::VideoPlayerTestEnvironment::ValidatorType::kNone;
-      } else if (it->second == "md5") {
-        validator_type =
-            media::test::VideoPlayerTestEnvironment::ValidatorType::kMD5;
-      } else if (it->second == "ssim") {
-        validator_type =
-            media::test::VideoPlayerTestEnvironment::ValidatorType::kSSIM;
-      } else {
-        std::cout << "unknown validator type \"" << it->second
-                  << "\", possible values are \"none|md5|ssim\"\n";
-        return EXIT_FAILURE;
-      }
-    } else if (it->first == "output_frames") {
-      if (it->second == "all") {
-        frame_output_config.output_mode = media::test::FrameOutputMode::kAll;
-      } else if (it->second == "corrupt") {
-        frame_output_config.output_mode =
-            media::test::FrameOutputMode::kCorrupt;
-      } else {
-        std::cout << "unknown frame output mode \"" << it->second
-                  << "\", possible values are \"all|corrupt\"\n";
-        return EXIT_FAILURE;
-      }
-    } else if (it->first == "output_format") {
-      if (it->second == "png") {
-        frame_output_config.output_format =
-            media::test::VideoFrameFileWriter::OutputFormat::kPNG;
-      } else if (it->second == "yuv") {
-        frame_output_config.output_format =
-            media::test::VideoFrameFileWriter::OutputFormat::kYUV;
-      } else {
-        std::cout << "unknown frame output format \"" << it->second
-                  << "\", possible values are \"png|yuv\"\n";
-        return EXIT_FAILURE;
-      }
-    } else if (it->first == "output_limit") {
-      if (!base::StringToUint64(it->second,
-                                &frame_output_config.output_limit)) {
-        std::cout << "invalid number \"" << it->second << "\n";
-        return EXIT_FAILURE;
-      }
-    } else if (it->first == "output_folder") {
-      output_folder = it->second;
-    } else if (it->first == "use-legacy") {
-      use_legacy = true;
-      implementation = media::test::DecoderImplementation::kVDA;
-    } else if (it->first == "use_vd_vda") {
-      use_vd_vda = true;
-      implementation = media::test::DecoderImplementation::kVDVDA;
-    } else if (it->first == "linear_output") {
-      linear_output = true;
-#if defined(ARCH_CPU_ARM_FAMILY)
-    } else if (it->first == "disable-libyuv") {
-      enabled_features.clear();
-#endif  // defined(ARCH_CPU_ARM_FAMILY)
-    } else {
-      std::cout << "unknown option: --" << it->first << "\n"
-                << media::test::usage_msg;
-      return EXIT_FAILURE;
-    }
-  }
-
-  disabled_features.push_back(media::kGlobalVaapiLock);
-
-  if (use_legacy && use_vd_vda) {
-    std::cout << "--use-legacy and --use_vd_vda cannot be enabled together.\n"
-              << media::test::usage_msg;
-    return EXIT_FAILURE;
-  }
-  if (linear_output && !use_vd_vda) {
-    std::cout << "--linear_output must be used with the VDVDA (--use_vd_vda)\n"
-                 "implementation.\n"
-              << media::test::usage_msg;
-    return EXIT_FAILURE;
-  }
-
-  testing::InitGoogleTest(&argc, argv);
-
-  // Add the command line flag for HEVC testing which will be checked by the
-  // video decoder to allow clear HEVC decoding.
-  cmd_line->AppendSwitch("enable-clear-hevc-for-testing");
-
-#if defined(ARCH_CPU_ARM_FAMILY) && BUILDFLAG(IS_CHROMEOS)
-  // On some platforms bandwidth compression is fully opaque and can not be read
-  // by the cpu. This prevents MD5 computation as that is done by the CPU. This
-  // is currently only needed for Trogdor/Strongbad to disable UBWC compression.
-  setenv("MINIGBM_DEBUG", "nocompression", 1);
-#endif
-
-#if BUILDFLAG(USE_V4L2_CODEC)
-  // For V4L2 testing with VISL, dumb driver is used with vkms for minigbm
-  // backend. In this case, the primary node needs to be used instead of the
-  // render node.
-  cmd_line->AppendSwitch(switches::kEnablePrimaryNodeAccessForVkmsTesting);
-
-  std::unique_ptr<base::FeatureList> feature_list =
-      std::make_unique<base::FeatureList>();
-  feature_list->InitFromCommandLine(
-      cmd_line->GetSwitchValueASCII(switches::kEnableFeatures),
-      cmd_line->GetSwitchValueASCII(switches::kDisableFeatures));
-  if (feature_list->IsFeatureOverridden("V4L2FlatStatefulVideoDecoder")) {
-    enabled_features.push_back(media::kV4L2FlatStatefulVideoDecoder);
-  }
-  if (feature_list->IsFeatureOverridden("V4L2FlatVideoDecoder")) {
-    enabled_features.push_back(media::kV4L2FlatVideoDecoder);
-    enabled_features.push_back(media::kV4L2FlatStatefulVideoDecoder);
-  }
-#endif
-
-  // Set up our test environment.
-  media::test::VideoPlayerTestEnvironment* test_environment =
-      media::test::VideoPlayerTestEnvironment::Create(
-          video_path, video_metadata_path, validator_type, implementation,
-          linear_output, base::FilePath(output_folder), frame_output_config,
-          enabled_features, disabled_features);
-  if (!test_environment)
-    return EXIT_FAILURE;
-
-  media::test::g_env = static_cast<media::test::VideoPlayerTestEnvironment*>(
-      testing::AddGlobalTestEnvironment(test_environment));
-
-  return RUN_ALL_TESTS();
+  return base::LaunchUnitTests(
+      argc, argv,
+      base::BindOnce(&media::test::VideoDecodeAcceleratorTestSuite::Run,
+                     base::Unretained(media::test::g_env)));
 }
