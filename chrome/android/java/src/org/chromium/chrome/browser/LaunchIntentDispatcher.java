41495d05b108fa7bda85d1b915ecb38afeb8e952
Jinsuk Kim <jinsukkim@chromium.org>
2024-03-22 16:14:00 +0000

    [ASH] Use the package name passed by identity sharing API only
    With this CL, ASH now relies on the host package name passed by identity
    sharing API only, not by service connection or startActivityForResult.
    Also:
    
    - records a boolean histogram indicating whether CustomTabs/Browser
      are launched with shared identity by hosts.
    - simplifies Activity.getCallingActivity -> Activity.getPackageName
      with a single call Activity.getCallingPackage. This becomes
      possible as the latter works in reliable fashion on N+.
    
    Bug: 330118241
    Change-Id: I95f37d063ed35334070f435fcb8c61e8ec20fbb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374029
    Reviewed-by: Kevin Grosu <kgrosu@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276919}

--- chrome/android/java/src/org/chromium/chrome/browser/LaunchIntentDispatcher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/LaunchIntentDispatcher.java
@@ -9,7 +9,6 @@ import android.app.Activity;
 import android.app.ActivityManager.RecentTaskInfo;
 import android.app.Notification;
 import android.app.SearchManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -18,7 +17,6 @@ import android.provider.MediaStore;
 import android.text.TextUtils;
 
 import androidx.annotation.IntDef;
-import androidx.annotation.OptIn;
 import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.browser.customtabs.CustomTabsSessionToken;
 import androidx.browser.customtabs.TrustedWebUtils;
@@ -362,13 +360,14 @@ public class LaunchIntentDispatcher {
         IntentUtils.safeRemoveExtra(mIntent, IntentHandler.EXTRA_CALLING_ACTIVITY_PACKAGE);
 
         Intent intent = new Intent(mIntent);
-        ComponentName callingActivity = mActivity.getCallingActivity();
-        if (callingActivity != null) {
-            intent.putExtra(
-                    IntentHandler.EXTRA_CALLING_ACTIVITY_PACKAGE, callingActivity.getPackageName());
+        String packageName = mActivity.getCallingPackage();
+        boolean identityShared = false;
+        if (packageName != null) {
+            intent.putExtra(IntentHandler.EXTRA_CALLING_ACTIVITY_PACKAGE, packageName);
         } else {
-            String packageName = getClientPackageNameFromIdentitySharing();
+            packageName = getCallingPackageIdentitySharing(mActivity);
             if (packageName != null) {
+                identityShared = true;
                 intent.putExtra(IntentHandler.EXTRA_CALLING_ACTIVITY_PACKAGE, packageName);
             }
         }
@@ -387,17 +386,18 @@ public class LaunchIntentDispatcher {
             }
 
             mActivity.startActivity(launchIntent, null);
+            RecordHistogram.recordBooleanHistogram("CustomTabs.IdentityShared", identityShared);
             return true;
         }
     }
 
     /**
-     * @return Client package name obtained from {@link Activity#getLaunchedFromPackage()}.
-     *         {@code null} if the underlying OS doesn't support the feature.
+     * @param activity The current {@link Activity} object.
+     * @return Client package name obtained from {@link Activity#getLaunchedFromPackage()}. {@code
+     *     null} if the underlying OS doesn't support the feature.
      */
-    @OptIn(markerClass = androidx.core.os.BuildCompat.PrereleaseSdkCheck.class)
-    private String getClientPackageNameFromIdentitySharing() {
-        return BuildCompat.isAtLeastU() ? mActivity.getLaunchedFromPackage() : null;
+    public static String getCallingPackageIdentitySharing(Activity activity) {
+        return BuildCompat.isAtLeastU() ? activity.getLaunchedFromPackage() : null;
     }
 
     /** Handles launching a {@link ChromeTabbedActivity}. */
@@ -421,6 +421,8 @@ public class LaunchIntentDispatcher {
             }
             RecordHistogram.recordBooleanHistogram(
                     "Android.Intent.HasNonSpoofablePackageName", hasNonSpoofablePackageName());
+            boolean identityShared = getCallingPackageIdentitySharing(mActivity) != null;
+            RecordHistogram.recordBooleanHistogram("Android.Intent.IdentityShared", identityShared);
         }
 
         if (mActivity instanceof ChromeLauncherActivity) {
@@ -499,13 +501,8 @@ public class LaunchIntentDispatcher {
     }
 
     private boolean hasNonSpoofablePackageName() {
-        ComponentName callingActivity = mActivity.getCallingActivity();
-        String packageName = "";
-        if (callingActivity != null) {
-            packageName = callingActivity.getPackageName();
-        }
-        return !TextUtils.isEmpty(packageName)
-                || !TextUtils.isEmpty(getClientPackageNameFromIdentitySharing());
+        return !TextUtils.isEmpty(mActivity.getCallingPackage())
+                || !TextUtils.isEmpty(getCallingPackageIdentitySharing(mActivity));
     }
 
     private static boolean clearTopIntentsForCustomTabsEnabled(Intent intent) {
