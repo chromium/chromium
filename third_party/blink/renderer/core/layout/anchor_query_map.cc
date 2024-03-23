eb3483f0f00c8f5024db060b1ab5d61f00f29966
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-03-23 16:44:46 +0000

    [anchor] Remove a unused LogicalAnchorQueryMap constructor.
    There should be no behaviour change.
    
    Change-Id: I90f2c52f0cc77e00fb87d59372aec737e0c2ea18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388973
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277292}

--- third_party/blink/renderer/core/layout/anchor_query_map.cc
+++ third_party/blink/renderer/core/layout/anchor_query_map.cc
@@ -363,18 +363,6 @@ struct StitchedAnchorQueries {
 LogicalAnchorQueryMap::LogicalAnchorQueryMap(
     const LayoutBox& root_box,
     const LogicalFragmentLinkVector& children,
-    const FragmentItemsBuilder::ItemWithOffsetList* items,
-    const WritingModeConverter& converter)
-    : root_box_(root_box),
-      converter_(converter),
-      writing_direction_(converter.GetWritingDirection()) {
-  DCHECK(&root_box);
-  SetChildren(children, items);
-}
-
-LogicalAnchorQueryMap::LogicalAnchorQueryMap(
-    const LayoutBox& root_box,
-    const LogicalFragmentLinkVector& children,
     WritingDirectionMode writing_direction)
     : root_box_(root_box), writing_direction_(writing_direction) {
   DCHECK(&root_box);
@@ -382,10 +370,8 @@ LogicalAnchorQueryMap::LogicalAnchorQueryMap(
 }
 
 void LogicalAnchorQueryMap::SetChildren(
-    const LogicalFragmentLinkVector& children,
-    const FragmentItemsBuilder::ItemWithOffsetList* items) {
+    const LogicalFragmentLinkVector& children) {
   children_ = &children;
-  items_ = items;
 
   // Invalidate the cache when children may have changed.
   computed_for_ = nullptr;
