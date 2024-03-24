d06b92d8235ceed6e2db461a9ed73475e4b92e9b
Gang Wu <gangwu@chromium.org>
2024-03-24 01:57:22 +0000

    clean up the flag in OmniboxResourceProvider
    UX revamp phrase 2 launched, so this CL clean up the params
    modernize_visual_update_active_color_on_omnibox for the
    OMNIBOX_MODERNIZE_VISUAL_UPDATE.
    
    Change-Id: Ie20348f54a2b442f044549ae00d3534d279f6639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388429
    Auto-Submit: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1277338}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/styles/OmniboxResourceProvider.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/styles/OmniboxResourceProvider.java
@@ -476,10 +476,7 @@ public class OmniboxResourceProvider {
         }
 
         return context.getResources()
-                .getDimensionPixelSize(
-                        OmniboxFeatures.shouldShowActiveColorOnOmnibox()
-                                ? R.dimen.toolbar_url_focus_height_increase_active_color
-                                : R.dimen.toolbar_url_focus_height_increase_no_active_color);
+                .getDimensionPixelSize(R.dimen.toolbar_url_focus_height_increase_active_color);
     }
 
     /** Returns the amount of pixels for the toolbar's side padding when the omnibox is focused. */
