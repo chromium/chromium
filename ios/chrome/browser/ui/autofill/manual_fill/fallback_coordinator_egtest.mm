fe9a2f8965ae61440e61ed334fa37e8ce7688bd0
Alexis Hétu <sugoi@chromium.org>
2024-03-24 23:15:54 +0000

    Revert "[iOS] Reenable testInputAccessoryBarIsPresentAfterPickers on iPhone simulator"
    This reverts commit e142cf54446535e009c9f4d7c428eee33a771c2f.
    
    Reason for revert: Test failure: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-noncq/43110/blamelist
    
    Original change's description:
    > [iOS] Reenable testInputAccessoryBarIsPresentAfterPickers on iPhone simulator
    >
    > Reduced the number of calls to [GetFirstResponder() reloadInputViews] in didRegisterFormActivity, because this was firing every time we were switching between username and password fields, for instance. Also re-enabled testInputAccessoryBarIsPresentAfterPickers, which tests this case, on iPhone simulator, for now (will try reenabling on device also if this is successful).
    >
    > Bug: 325597035
    > Change-Id: Ifdbe4eb82f484b6555babccb0799b48ee9fd5d26
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380764
    > Reviewed-by: Tommy Martino <tmartino@chromium.org>
    > Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1277011}
    
    Bug: 325597035
    Change-Id: I8b1280904297b402b4e5af103d904544862d5c91
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388139
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Alexis Hétu <sugoi@chromium.org>
    Owners-Override: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277417}

--- ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator_egtest.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/fallback_coordinator_egtest.mm
@@ -163,12 +163,10 @@ id<GREYMatcher> ProfileTableViewButtonMatcher() {
 // Tests that the input accessory view continues working after a picker is
 // present.
 - (void)testInputAccessoryBarIsPresentAfterPickers {
-  // TODO(crbug.com/1488889): Test fails on iPhone device.
-#if !TARGET_OS_SIMULATOR
+  // TODO(crbug.com/1488889): Test fails on iPhone.
   if (![ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_DISABLED(@"Fails on iPhone.");
   }
-#endif
 
   // Add the profile to be used.
   [AutofillAppInterface saveExampleProfile];
@@ -196,13 +194,13 @@ id<GREYMatcher> ProfileTableViewButtonMatcher() {
   [[EarlGrey selectElementWithMatcher:ManualFallbackProfilesTableViewMatcher()]
       assertWithMatcher:grey_notVisible()];
 
-  // Tap in the web view so the keyboard or popover dismisses.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
-      performAction:grey_tapAtPoint(CGPointMake(0, 0))];
-
   // On iPad the picker is a table view in a popover, we need to
   // dismiss that first.
   if ([ChromeEarlGrey isIPadIdiom]) {
+    // Tap in the web view so the popover dismisses.
+    [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
+        performAction:grey_tapAtPoint(CGPointMake(0, 0))];
+
     // Verify the table view is not visible.
     [[EarlGrey
         selectElementWithMatcher:grey_allOf(
