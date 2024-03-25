84f86697a4d9cf5177fd4ab067b8ab4566d81987
Florian Zubeil <zubeil@google.com>
2024-03-25 09:54:17 +0000

    Kiosk heartbeats: Set initial delay to zero
    Remove the initial delay for kiosk heartbeats so they are send as soon as possible.
    
    Bug: b/330500746
    Change-Id: I11fb9c03f0a2d05727a48c11e4d53927932f800f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372858
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Commit-Queue: Florian Zubeil <zubeil@google.com>
    Reviewed-by: Steffen Seckler <sseckler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1277530}

--- chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager_unittest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager_unittest.cc
@@ -1103,17 +1103,16 @@ class KioskHeartbeatTelemetryTest : public MetricReportingManagerTest {
     // Mock app service unavailability to eliminate noise.
     ON_CALL(*mock_delegate_ptr, IsAppServiceAvailableForProfile)
         .WillByDefault(Return(false));
-    ON_CALL(
-        *mock_delegate_ptr,
-        CreatePeriodicCollector(
-            /*sampler=*/_,
-            /*queue=*/heartbeat_queue_ptr_.get(),
-            /*report_settings=*/_,
-            /*enable_setting_path=*/::ash::kHeartbeatEnabled,
-            /*setting_enabled_default_value=*/
-            metrics::kHeartbeatTelemetryDefaultValue,
-            /*rate_setting_path=*/::ash::kHeartbeatFrequency, _, 1,
-            /*init_delay=*/metrics::kDefaultHeartbeatTelemetryCollectionRate))
+    ON_CALL(*mock_delegate_ptr,
+            CreatePeriodicCollector(
+                /*sampler=*/_,
+                /*queue=*/heartbeat_queue_ptr_.get(),
+                /*report_settings=*/_,
+                /*enable_setting_path=*/::ash::kHeartbeatEnabled,
+                /*setting_enabled_default_value=*/
+                metrics::kHeartbeatTelemetryDefaultValue,
+                /*rate_setting_path=*/::ash::kHeartbeatFrequency, _, 1,
+                /*init_delay=*/base::TimeDelta()))
         .WillByDefault([&]() {
           return std::make_unique<FakeCollector>(&collector_count_);
         });
@@ -1182,17 +1181,16 @@ TEST_F(KioskHeartbeatTelemetryTest, Init) {
                               _, _, _, _, _))
       .Times(AnyNumber());
   // PeriodicCollector should be created here.
-  EXPECT_CALL(
-      *mock_delegate_ptr,
-      CreatePeriodicCollector(
-          /*sampler=*/_,
-          /*queue=*/heartbeat_queue_ptr_.get(),
-          /*report_settings=*/_,
-          /*enable_setting_path=*/::ash::kHeartbeatEnabled,
-          /*setting_enabled_default_value=*/
-          metrics::kHeartbeatTelemetryDefaultValue,
-          /*rate_setting_path=*/::ash::kHeartbeatFrequency, _, 1,
-          /*init_delay=*/metrics::kDefaultHeartbeatTelemetryCollectionRate))
+  EXPECT_CALL(*mock_delegate_ptr,
+              CreatePeriodicCollector(
+                  /*sampler=*/_,
+                  /*queue=*/heartbeat_queue_ptr_.get(),
+                  /*report_settings=*/_,
+                  /*enable_setting_path=*/::ash::kHeartbeatEnabled,
+                  /*setting_enabled_default_value=*/
+                  metrics::kHeartbeatTelemetryDefaultValue,
+                  /*rate_setting_path=*/::ash::kHeartbeatFrequency, _, 1,
+                  /*init_delay=*/base::TimeDelta()))
       .Times(1);
 
   metric_reporting_manager->OnLogin(profile());
