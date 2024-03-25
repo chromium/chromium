0dfedaae72a9974976b6722293d86bf2061a40ca
Matt Menke <mmenke@chromium.org>
2024-03-25 16:30:25 +0000

    Fix dangling pointers in CoalescingCertVerifier::Request.
    Null out the `job_` pointer just before a call that may delete the
    underlying object, rather than just after.
    
    Also null out the `verify_result_` raw pointer after writing to it.
    
    Bug: 40212619
    Change-Id: Iace18cb2f6ad4bc292fe3b82e48840088a8b21ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5390666
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277700}

--- net/cert/coalescing_cert_verifier.cc
+++ net/cert/coalescing_cert_verifier.cc
@@ -173,9 +173,9 @@ class CoalescingCertVerifier::Request
   void OnJobAbort();
 
  private:
-  raw_ptr<CoalescingCertVerifier::Job, DanglingUntriaged> job_;
+  raw_ptr<CoalescingCertVerifier::Job> job_;
 
-  raw_ptr<CertVerifyResult, DanglingUntriaged> verify_result_;
+  raw_ptr<CertVerifyResult> verify_result_;
   CompletionOnceCallback callback_;
   const NetLogWithSource net_log_;
 };
@@ -342,10 +342,14 @@ CoalescingCertVerifier::Request::~Request() {
     net_log_.AddEvent(NetLogEventType::CANCELLED);
     net_log_.EndEvent(NetLogEventType::CERT_VERIFIER_REQUEST);
 
+    // Need to null out `job_` before aborting the request to avoid a dangling
+    // pointer warning, as aborting the request may delete `job_`.
+    auto* job = job_.get();
+    job_ = nullptr;
+
     // If the Request is deleted before the Job, then detach from the Job.
     // Note: This may cause |job_| to be deleted.
-    job_->AbortRequest(this);
-    job_ = nullptr;
+    job->AbortRequest(this);
   }
 }
 
@@ -358,6 +362,11 @@ void CoalescingCertVerifier::Request::Complete(int result) {
   // similarly, break the association here so that when the Request is
   // deleted, it does not try to abort the (now-completed) Job.
   job_ = nullptr;
+
+  // Also need to break the association with `verify_result_`, so that
+  // dangling pointer checks the result and the Request be destroyed
+  // in any order.
+  verify_result_ = nullptr;
 
   net_log_.EndEvent(NetLogEventType::CERT_VERIFIER_REQUEST);
 
