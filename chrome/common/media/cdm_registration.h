3ec80b2b35407e54a8ece4fc55c05ab3dc2af032
John Rummell <jrummell@chromium.org>
2024-03-25 19:24:59 +0000

    Reapply "Enable Widevine CDM rollback on Linux/ChromeOS"
    This reverts commit b1646bfdf2c9f2bbe91c5124ea20deb92793c38d.
    
    This change was reverted due to problems with LaCros. Reapplying with a
    flag so that it doesn't by default apply to LaCros. If the flag is
    disabled then the previous behavior is maintained (choose the Component
    Updated version only if it is greater than the bundled version.)
    
    Original change's description:
    > Enable Widevine CDM rollback on Linux/ChromeOS
    >
    > Component Update allows for downgrading components to an earlier
    > version. However, on Linux and ChromeOS, the Widevine CDM needs
    > to be loaded before the zygote is locked, and thus uses a hint
    > file to determine which version is supplied by Component Update.
    > Current checking in cdm_registration uses the larger of the bundled
    > version or the Component Updated version (if both exist), so if the
    > downgrade is to a version prior to the current bundled version, the
    > rollback doesn't work.
    >
    > To fix this, the hint file created when a new component is downloaded
    > is updated to keep track of the bundled version at the time it was
    > downloaded, as Component Update will check the bundled version when
    > determining if a new version is required. If the bundled version
    > matches this saved version, then the bundled version is skipped when
    > checking what should be loaded into the zygote (thus always loading
    > the Component Updated version).
    
    Bug: 41491028
    Test: manual test plus updated unit_tests pass
    Change-Id: I94d941e251f271653c7efad1009293c0bd597d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381848
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277842}

--- chrome/common/media/cdm_registration.h
+++ chrome/common/media/cdm_registration.h
@@ -7,9 +7,16 @@
 
 #include <vector>
 
+#include "build/build_config.h"
 #include "content/public/common/cdm_info.h"
+#include "third_party/widevine/cdm/buildflags.h"
 
 // Register CdmInfo for Content Decryption Modules (CDM) supported.
 void RegisterCdmInfo(std::vector<content::CdmInfo>* cdms);
+
+#if BUILDFLAG(ENABLE_WIDEVINE) && BUILDFLAG(IS_LINUX)
+// Returns the software secure Widevine CDM.
+std::vector<content::CdmInfo> GetSoftwareSecureWidevineForTesting();
+#endif
 
 #endif  // CHROME_COMMON_MEDIA_CDM_REGISTRATION_H_
--- chrome/common/media/component_widevine_cdm_hint_file_linux.h
+++ chrome/common/media/component_widevine_cdm_hint_file_linux.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_COMMON_MEDIA_COMPONENT_WIDEVINE_CDM_HINT_FILE_LINUX_H_
 #define CHROME_COMMON_MEDIA_COMPONENT_WIDEVINE_CDM_HINT_FILE_LINUX_H_
 
+#include <optional>
+
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "third_party/widevine/cdm/buildflags.h"
@@ -21,19 +23,28 @@
 
 namespace base {
 class FilePath;
+class Version;
 }  // namespace base
 
 // The APIs here wrap the component updated Widevine hint file, which lives
 // inside the WidevineCdm folder of the user-data-dir, so that the Linux zygote
-// process can preload the latest version of Widevine.
+// process can preload the latest version of Widevine. This file is updated
+// whenever Component Update for the Widevine CDM runs and selects a different
+// Widevine CDM that should be used. As the Widevine CDM on Linux can only be
+// loaded at Chrome startup, the CDM can't be changed immediately. This file is
+// used to help decide which version of the CDM should be loaded the next time
+// Chrome starts.
 //
-// The hint file will be a dictionary with one key:
+// The hint file will be a dictionary with two keys:
 // {
 //     "Path": $path_to_WidevineCdm_directory
+//     "LastBundledVersion": version of the bundled Widevine CDM
 // }
 //
-// $path_to_WidevineCdm_directory will point to a directory structure
-// containing:
+// $path_to_WidevineCdm_directory will be a directory containing the Widevine
+// CDM. It could refer to the bundled CDM or a CDM downloaded by Component
+// Update, depending on what version Component Update determines is current.
+// It will point to a directory structure containing:
 //     LICENSE
 //     manifest.json
 //     _platform_specific/
@@ -41,18 +52,33 @@ class FilePath;
 //             libwidevinecdm.so
 // The actual executable (and directory containing it) will be platform
 // specific. There may be additional files as well as the ones listed above.
+//
+// "LastBundledVersion" will only be set if there was a bundled Widevine CDM
+// available when the hint file is updated (by Component Update selecting a new
+// Widevine CDM version). It is used to determine if the bundled Widevine CDM
+// changed due to a Chrome update next time Chrome starts.
 
 // Records a new Widevine path into the hint file, replacing the current
 // contents if any. |cdm_base_path| is the directory containing the new
-// instance. Returns true if the hint file has been successfully updated,
-// otherwise false.
+// instance. |bundled_version| is the version of the current bundled CDM, if
+// any. Returns true if the hint file has been successfully updated, otherwise
+// false.
 [[nodiscard]] bool UpdateWidevineCdmHintFile(
-    const base::FilePath& cdm_base_path);
+    const base::FilePath& cdm_base_path,
+    std::optional<base::Version> bundled_version);
 
-// Returns the latest component updated Widevine CDM directory. If the hint file
-// exists and is valid, returns the CDM base_path with the value loaded from the
-// file. Otherwise returns empty base::FilePath(). This function does not verify
-// that the path returned exists or not.
-[[nodiscard]] base::FilePath GetLatestComponentUpdatedWidevineCdmDirectory();
+// Returns the directory containing a Widevine CDM selected by Component
+// Update the last time it ran. If there are no CDM updates, it may be the
+// bundled CDM directory (if it exists). If the hint file exists and is valid,
+// returns the CDM base_path with the value loaded from the file. Otherwise
+// returns empty base::FilePath(). This function does not verify that the path
+// returned exists or not.
+[[nodiscard]] base::FilePath GetHintedWidevineCdmDirectory();
+
+// Returns the version of the bundled Widevine CDM that existed when the hint
+// file was last updated (by Component Update the last time it ran). May return
+// nullopt if there was no bundled Widevine CDM at the time when the hint file
+// was updated or is otherwise not set.
+std::optional<base::Version> GetBundledVersionDuringLastComponentUpdate();
 
 #endif  // CHROME_COMMON_MEDIA_COMPONENT_WIDEVINE_CDM_HINT_FILE_LINUX_H_
