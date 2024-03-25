c813d2ee6fe7f2b57fdde92e7729f4633a5944e0
Joe Downing <joedow@google.com>
2024-03-25 21:39:19 +0000

    Add a new base class for start_host impls
    This CL adds a base class which will be used for start_host
    specializations (Corp, Cloud, OAuth). I will do the actual
    refactoring in a follow-up.
    
    Bug: b/320745119, b/330404222
    Change-Id: Id8544c4ae43192dfa2b228935ea3702836c56b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388963
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1277961}

---
+++ remoting/host/setup/host_starter_base.cc
@@ -0,0 +1,258 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/setup/host_starter_base.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "base/functional/bind.h"
+#include "base/functional/callback.h"
+#include "base/location.h"
+#include "base/logging.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/strings/stringprintf.h"
+#include "base/threading/platform_thread.h"
+#include "base/values.h"
+#include "google_apis/gaia/gaia_oauth_client.h"
+#include "google_apis/google_api_keys.h"
+#include "remoting/base/hostname.h"
+#include "remoting/base/rsa_key_pair.h"
+#include "remoting/host/host_config.h"
+#include "remoting/host/pin_hash.h"
+#include "remoting/host/setup/daemon_controller.h"
+#include "remoting/host/setup/host_starter.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+
+namespace remoting {
+
+HostStarterBase::HostStarterBase(
+    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory)
+    : url_loader_factory_(url_loader_factory) {
+  weak_ptr_ = weak_ptr_factory_.GetWeakPtr();
+}
+
+HostStarterBase::~HostStarterBase() = default;
+
+void HostStarterBase::StartHost(Params params, CompletionCallback on_done) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DCHECK(!on_done_);
+
+  start_host_params_ = std::move(params);
+  if (start_host_params_.name.empty()) {
+    // Use the FQDN if a name was not provided via the command line.
+    start_host_params_.name = GetHostname();
+  }
+  // |auth_code| and |redirect_url| must match and either be populated or empty.
+  DCHECK(start_host_params_.auth_code.empty() ==
+         start_host_params_.redirect_url.empty());
+
+  on_done_ = std::move(on_done);
+  key_pair_ = RsaKeyPair::Generate();
+
+  // Check to see if there is an existing host instance on this machine which
+  // needs to be cleaned up before we can create and start a new host instance.
+  daemon_controller_->GetConfig(
+      base::BindOnce(&HostStarterBase::OnExistingConfigLoaded, weak_ptr_));
+}
+
+void HostStarterBase::OnExistingConfigLoaded(
+    std::optional<base::Value::Dict> config) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (config.has_value()) {
+    std::string* host_id = config->FindString(kHostIdConfigPath);
+    if (host_id) {
+      existing_host_id_.emplace(*host_id);
+      // Formatted to make start_host output more readable.
+      LOG(INFO) << "\n  Found existing host: `" << *existing_host_id_ << "`.\n"
+                << "  This instance will be deleted from the Directory before "
+                << "starting the new host instance.";
+    }
+  }
+
+  if (!start_host_params_.auth_code.empty()) {
+    oauth_helper_.emplace(url_loader_factory_)
+        .FetchTokens(
+            start_host_params_.owner_email, start_host_params_.auth_code,
+            {
+                .client_id = google_apis::GetOAuth2ClientID(
+                    google_apis::CLIENT_REMOTING),
+                .client_secret = google_apis::GetOAuth2ClientSecret(
+                    google_apis::CLIENT_REMOTING),
+                .redirect_uri = start_host_params_.redirect_url,
+            },
+            base::BindOnce(&HostStarterBase::OnUserTokensRetrieved, weak_ptr_),
+            base::BindOnce(&HostStarterBase::HandleError, weak_ptr_));
+  } else {
+    RegisterNewHost(std::string());
+  }
+}
+
+void HostStarterBase::OnUserTokensRetrieved(const std::string& access_token,
+                                            const std::string& refresh_token) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  // We don't need a `refresh_token` for the user so ignore it even if the
+  // authorization_code was created with the offline param.
+  RegisterNewHost(access_token);
+}
+
+void HostStarterBase::OnNewHostRegistered(
+    const std::string& directory_id,
+    const std::string& owner_account_email,
+    const std::string& service_account_email,
+    const std::string& authorization_code) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (directory_id.empty()) {
+    HandleError("No host id returned by the Directory.", REGISTRATION_ERROR);
+    return;
+  }
+  if (!start_host_params_.id.empty() &&
+      !base::EqualsCaseInsensitiveASCII(start_host_params_.id, directory_id)) {
+    HandleError(base::StringPrintf(
+                    "Host id (%s) returned from the service does not match "
+                    "the host id provided (%s)",
+                    directory_id.c_str(), start_host_params_.id.c_str()),
+                REGISTRATION_ERROR);
+    return;
+  }
+  start_host_params_.id = base::ToLowerASCII(directory_id);
+
+  if (owner_account_email.empty()) {
+    HandleError("No owner account email provided in callback.",
+                REGISTRATION_ERROR);
+    return;
+  }
+  start_host_params_.owner_email = base::ToLowerASCII(owner_account_email);
+
+  if (service_account_email.empty()) {
+    HandleError("No service account email provided in callback.",
+                REGISTRATION_ERROR);
+    return;
+  }
+  service_account_email_ = base::ToLowerASCII(service_account_email);
+
+  if (authorization_code.empty()) {
+    HandleError("No authorization code returned by the Directory.",
+                REGISTRATION_ERROR);
+    return;
+  }
+
+  oauth_helper_.emplace(url_loader_factory_)
+      .FetchTokens(
+          start_host_params_.owner_email, start_host_params_.auth_code,
+          {
+              .client_id = google_apis::GetOAuth2ClientID(
+                  google_apis::CLIENT_REMOTING_HOST),
+              .client_secret = google_apis::GetOAuth2ClientSecret(
+                  google_apis::CLIENT_REMOTING_HOST),
+              .redirect_uri = start_host_params_.redirect_url,
+          },
+          base::BindOnce(&HostStarterBase::OnServiceAccountTokensRetrieved,
+                         weak_ptr_),
+          base::BindOnce(&HostStarterBase::HandleError, weak_ptr_));
+}
+
+void HostStarterBase::OnServiceAccountTokensRetrieved(
+    const std::string& access_token,
+    const std::string& refresh_token) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  service_account_refresh_token_ = refresh_token;
+  RemoveOldHostFromDirectory(
+      base::BindOnce(&HostStarterBase::StopOldHost, weak_ptr_));
+}
+
+void HostStarterBase::StopOldHost() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (existing_host_id_.has_value()) {
+    daemon_controller_->Stop(
+        base::BindOnce(&HostStarterBase::OnOldHostStopped, weak_ptr_));
+  } else {
+    GenerateConfigFile();
+  }
+}
+
+void HostStarterBase::OnOldHostStopped(DaemonController::AsyncResult result) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  bool stopped = false;
+  for (auto i = 0; !stopped && i < 10; i++) {
+    stopped =
+        (daemon_controller_->GetState() == DaemonController::STATE_STOPPED);
+    if (!stopped) {
+      base::PlatformThread::Sleep(base::Seconds(1));
+    }
+  }
+  if (!stopped) {
+    LOG(WARNING) << "Unable to stop existing host process. Setup will "
+                 << "continue, but you may need to reboot to complete it.";
+  }
+
+  GenerateConfigFile();
+}
+
+void HostStarterBase::GenerateConfigFile() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  base::Value::Dict config;
+  // These 5 values are required for the host to start up properly. Other values
+  // are optional depending on the use case.
+  config.Set(kHostOwnerConfigPath, start_host_params_.owner_email);
+  config.Set(kServiceAccountConfigPath, service_account_email_);
+  config.Set(kOAuthRefreshTokenConfigPath, service_account_refresh_token_);
+  config.Set(kHostIdConfigPath, start_host_params_.id);
+  config.Set(kPrivateKeyConfigPath, key_pair_->ToString());
+
+  if (!start_host_params_.name.empty()) {
+    config.Set(kHostNameConfigPath, start_host_params_.name);
+  }
+  if (!start_host_params_.pin.empty()) {
+    std::string host_secret_hash = remoting::MakeHostPinHash(
+        start_host_params_.id, start_host_params_.pin);
+    config.Set(kHostSecretHashConfigPath, host_secret_hash);
+  }
+
+  daemon_controller_->SetConfigAndStart(
+      std::move(config), start_host_params_.enable_crash_reporting,
+      base::BindOnce(&HostStarterBase::OnNewHostStarted,
+                     base::Unretained(this)));
+}
+
+void HostStarterBase::OnNewHostStarted(DaemonController::AsyncResult result) {
+  if (result != DaemonController::RESULT_OK) {
+    HandleError(base::StringPrintf("Failed to start host: %d",
+                                   static_cast<int>(result)),
+                START_ERROR);
+  } else {
+    std::move(on_done_).Run(START_COMPLETE);
+  }
+}
+
+void HostStarterBase::HandleError(const std::string& error_message,
+                                  Result error_result) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  ReportError(error_message, base::BindOnce(std::move(on_done_), error_result));
+}
+
+void HostStarterBase::ReportError(const std::string& message,
+                                  base::OnceClosure on_error_reported) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  LOG(ERROR) << message;
+  std::move(on_error_reported).Run();
+}
+
+void HostStarterBase::SetDaemonControllerForTest(
+    scoped_refptr<remoting::DaemonController> daemon_controller) {
+  daemon_controller_ = daemon_controller;
+}
+
+}  // namespace remoting
---
+++ remoting/host/setup/host_starter_base_unittest.cc
@@ -0,0 +1,476 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/setup/host_starter_base.h"
+
+#include "base/run_loop.h"
+#include "base/test/task_environment.h"
+#include "google_apis/gaia/gaia_urls.h"
+#include "net/http/http_status_code.h"
+#include "remoting/base/auto_thread_task_runner.h"
+#include "remoting/host/host_config.h"
+#include "remoting/host/setup/daemon_controller.h"
+#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
+#include "services/network/test/test_shared_url_loader_factory.h"
+#include "services/network/test/test_url_loader_factory.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace remoting {
+
+namespace {
+
+// Response for spoofing Gaia calls.
+constexpr char kAccessToken[] = "LEGIT_ACCESS_TOKEN";
+constexpr char kRefreshToken[] = "LEGIT_REFRESH_TOKEN";
+constexpr char kGetTokensResponse[] = R"({
+            "refresh_token": "LEGIT_REFRESH_TOKEN",
+            "access_token": "LEGIT_ACCESS_TOKEN",
+            "expires_in": 3600,
+            "token_type": "Bearer"
+         })";
+constexpr char kGetUserEmailResponse[] = R"({"email": "user@test.com"})";
+
+// Known values for testing config file generation.
+constexpr char kTestUserEmail[] = "user@test.com";
+constexpr char kTestRobotEmail[] = "robot@chromoting.com";
+constexpr char kTestRobotAuthCode[] = "robot_auth_code";
+constexpr char kTestDirectoryId[] = "test_directory_id";
+constexpr char kTestMachineName[] = "test_machine_name";
+
+class TestDaemonControllerDelegate : public DaemonController::Delegate {
+ public:
+  TestDaemonControllerDelegate();
+
+  TestDaemonControllerDelegate(const TestDaemonControllerDelegate&) = delete;
+  TestDaemonControllerDelegate& operator=(const TestDaemonControllerDelegate&) =
+      delete;
+
+  ~TestDaemonControllerDelegate() override;
+
+  // DaemonController::Delegate interface.
+  DaemonController::State GetState() override;
+  std::optional<base::Value::Dict> GetConfig() override;
+  void CheckPermission(bool it2me,
+                       DaemonController::BoolCallback callback) override;
+  void SetConfigAndStart(base::Value::Dict config,
+                         bool consent,
+                         DaemonController::CompletionCallback done) override;
+  void UpdateConfig(base::Value::Dict config,
+                    DaemonController::CompletionCallback done) override;
+  void Stop(DaemonController::CompletionCallback done) override;
+  DaemonController::UsageStatsConsent GetUsageStatsConsent() override;
+
+  // Methods used for controlling behavior of the fake instance.
+  void set_result_for_config_and_start(DaemonController::AsyncResult result) {
+    result_for_config_and_start_ = result;
+  }
+  void set_initial_config(base::Value::Dict config) {
+    config_ = std::move(config);
+  }
+
+  bool stop_called() { return stop_called_; }
+
+ private:
+  bool stop_called_ = false;
+  DaemonController::State state_ = DaemonController::STATE_STOPPED;
+  DaemonController::AsyncResult result_for_config_and_start_ =
+      DaemonController::RESULT_OK;
+  base::Value::Dict config_;
+};
+
+TestDaemonControllerDelegate::TestDaemonControllerDelegate() = default;
+
+TestDaemonControllerDelegate::~TestDaemonControllerDelegate() = default;
+
+DaemonController::State TestDaemonControllerDelegate::GetState() {
+  return state_;
+}
+
+std::optional<base::Value::Dict> TestDaemonControllerDelegate::GetConfig() {
+  return config_.Clone();
+}
+
+void TestDaemonControllerDelegate::CheckPermission(
+    bool it2me,
+    DaemonController::BoolCallback callback) {
+  ADD_FAILURE() << "Unexpected call to CheckPermission()";
+}
+
+void TestDaemonControllerDelegate::SetConfigAndStart(
+    base::Value::Dict config,
+    bool consent,
+    DaemonController::CompletionCallback done) {
+  config_ = std::move(config);
+  state_ = DaemonController::STATE_STARTED;
+  std::move(done).Run(result_for_config_and_start_);
+}
+
+void TestDaemonControllerDelegate::UpdateConfig(
+    base::Value::Dict config,
+    DaemonController::CompletionCallback done) {
+  ADD_FAILURE() << "Unexpected call to UpdateConfig()";
+}
+
+void TestDaemonControllerDelegate::Stop(
+    DaemonController::CompletionCallback done) {
+  stop_called_ = true;
+  state_ = DaemonController::STATE_STOPPED;
+  std::move(done).Run(DaemonController::RESULT_OK);
+}
+
+DaemonController::UsageStatsConsent
+TestDaemonControllerDelegate::GetUsageStatsConsent() {
+  ADD_FAILURE() << "Unexpected call to GetUsageStatsConsent()";
+  return DaemonController::UsageStatsConsent();
+}
+
+class TestHostStarter : public HostStarterBase {
+ public:
+  TestHostStarter(
+      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+      scoped_refptr<DaemonController> daemon_controller);
+
+  TestHostStarter(const TestHostStarter&) = delete;
+  TestHostStarter& operator=(const TestHostStarter&) = delete;
+
+  ~TestHostStarter() override;
+
+  // HostStarterBase implementation.
+  void RegisterNewHost(const std::string& access_token) override;
+  void RemoveOldHostFromDirectory(base::OnceClosure on_removed) override;
+  void ReportError(const std::string& error_message,
+                   base::OnceClosure on_done) override;
+
+  std::string& user_access_token() { return user_access_token_; }
+  std::string& error_message() { return error_message_; }
+
+  void clear_directory_id() { directory_id_.clear(); }
+
+  void clear_owner_account_email() { owner_account_email_.clear(); }
+
+  void clear_service_account_email() { service_account_email_.clear(); }
+
+  void clear_robot_authorization_code() { robot_authorization_code_.clear(); }
+
+ private:
+  std::string user_access_token_;
+  std::string error_message_;
+
+  std::string directory_id_{kTestDirectoryId};
+  std::string owner_account_email_{kTestUserEmail};
+  std::string service_account_email_{kTestRobotEmail};
+  std::string robot_authorization_code_{kTestRobotAuthCode};
+};
+
+TestHostStarter::TestHostStarter(
+    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
+    scoped_refptr<DaemonController> daemon_controller)
+    : HostStarterBase(url_loader_factory) {
+  SetDaemonControllerForTest(daemon_controller);
+}
+
+TestHostStarter::~TestHostStarter() = default;
+
+void TestHostStarter::RegisterNewHost(const std::string& access_token) {
+  user_access_token_ = access_token;
+  OnNewHostRegistered(directory_id_, owner_account_email_,
+                      service_account_email_, robot_authorization_code_);
+}
+
+void TestHostStarter::RemoveOldHostFromDirectory(base::OnceClosure on_removed) {
+  std::move(on_removed).Run();
+}
+
+void TestHostStarter::ReportError(const std::string& error_message,
+                                  base::OnceClosure on_done) {
+  error_message_ = error_message;
+  LOG(ERROR) << error_message_;
+  std::move(on_done).Run();
+}
+
+}  // namespace
+
+class HostStarterBaseTest : public testing::Test {
+ public:
+  HostStarterBaseTest();
+  ~HostStarterBaseTest() override;
+
+  void SetUp() override;
+  void TearDown() override;
+
+  HostStarter::CompletionCallback GetCompletionCallback() {
+    return base::BindOnce(&HostStarterBaseTest::CompletionHandler,
+                          base::Unretained(this));
+  }
+
+ protected:
+  void RunUntilQuit();
+  void CompletionHandler(HostStarter::Result result);
+
+  TestHostStarter& test_host_starter() { return *test_host_starter_; }
+  std::optional<HostStarter::Result>& start_result() { return start_result_; }
+  TestDaemonControllerDelegate& test_daemon_controller_delegate() {
+    return *test_daemon_controller_delegate_;
+  }
+
+ private:
+  base::RepeatingClosure quit_closure_;
+
+  base::test::TaskEnvironment task_environment_{
+      base::test::TaskEnvironment::MainThreadType::IO};
+
+  network::TestURLLoaderFactory test_url_loader_factory_;
+  scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
+
+  // Reference to the DaemonControllerDelegate used for testing, which is owned
+  // by |test_host_starter_|.
+  raw_ptr<TestDaemonControllerDelegate> test_daemon_controller_delegate_;
+
+  std::optional<HostStarter::Result> start_result_;
+  std::unique_ptr<TestHostStarter> test_host_starter_;
+};
+
+HostStarterBaseTest::HostStarterBaseTest() = default;
+
+HostStarterBaseTest::~HostStarterBaseTest() = default;
+
+void HostStarterBaseTest::SetUp() {
+  test_url_loader_factory_.AddResponse(
+      GaiaUrls::GetInstance()->oauth2_token_url().spec(), kGetTokensResponse);
+  test_url_loader_factory_.AddResponse(
+      GaiaUrls::GetInstance()->oauth_user_info_url().spec(),
+      kGetUserEmailResponse);
+  shared_url_loader_factory_ = test_url_loader_factory_.GetSafeWeakWrapper();
+  test_daemon_controller_delegate_ = new TestDaemonControllerDelegate();
+  scoped_refptr<DaemonController> daemon_controller(new DaemonController(
+      base::WrapUnique(test_daemon_controller_delegate_.get())));
+  test_host_starter_ = std::make_unique<TestHostStarter>(
+      shared_url_loader_factory_, daemon_controller);
+  quit_closure_ = task_environment_.QuitClosure();
+}
+
+void HostStarterBaseTest::TearDown() {
+  // Clear the raw pointer so it doesn't appear to be hanging.
+  test_daemon_controller_delegate_ = nullptr;
+  // Clear the test instance and allow the threads to shut down so ASAN doesn't
+  // detect it as leaked.
+  test_host_starter_.reset();
+  task_environment_.RunUntilIdle();
+}
+
+void HostStarterBaseTest::RunUntilQuit() {
+  task_environment_.RunUntilQuit();
+}
+
+void HostStarterBaseTest::CompletionHandler(HostStarter::Result result) {
+  start_result_ = result;
+  quit_closure_.Run();
+}
+
+TEST_F(HostStarterBaseTest, StartHostUsingOAuth) {
+  HostStarter::Params params;
+  params.pin = "123456";
+  params.name = kTestMachineName;
+  params.auth_code = "auth_me_dude";
+  params.redirect_url = "/redirect";
+
+  test_host_starter().StartHost(std::move(params), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation completed successfully.
+  EXPECT_EQ(start_result(), HostStarter::START_COMPLETE);
+
+  // Verify the user access token was provided to the subclass and no errors
+  // were reported.
+  EXPECT_EQ(test_host_starter().user_access_token(), kAccessToken);
+  EXPECT_EQ(test_host_starter().error_message(), std::string());
+
+  // Verify the configuration dict has the expected fields populated.
+  auto config = test_daemon_controller_delegate().GetConfig();
+  ASSERT_TRUE(config.has_value());
+  const std::string* value = config->FindString(kHostOwnerConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kTestUserEmail);
+  value = config->FindString(kServiceAccountConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kTestRobotEmail);
+  value = config->FindString(kOAuthRefreshTokenConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kRefreshToken);
+  value = config->FindString(kHostIdConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kTestDirectoryId);
+  value = config->FindString(kHostNameConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kTestMachineName);
+  // Just check for existence here.
+  EXPECT_TRUE(config->FindString(kPrivateKeyConfigPath));
+  EXPECT_TRUE(config->FindString(kHostSecretHashConfigPath));
+
+  // Verify Stop() was not called.
+  EXPECT_FALSE(test_daemon_controller_delegate().stop_called());
+}
+
+TEST_F(HostStarterBaseTest, CorpCodePath) {
+  HostStarter::Params params;
+  params.owner_email = kTestUserEmail;
+
+  test_host_starter().StartHost(std::move(params), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation completed successfully.
+  EXPECT_EQ(start_result(), HostStarter::START_COMPLETE);
+
+  // Verify no user access token was provided and no errors were reported.
+  EXPECT_EQ(test_host_starter().user_access_token(), std::string());
+  EXPECT_EQ(test_host_starter().error_message(), std::string());
+
+  // Verify the configuration dict has the expected fields populated.
+  auto config = test_daemon_controller_delegate().GetConfig();
+  ASSERT_TRUE(config.has_value());
+  const std::string* value = config->FindString(kHostOwnerConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kTestUserEmail);
+  value = config->FindString(kServiceAccountConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kTestRobotEmail);
+  value = config->FindString(kOAuthRefreshTokenConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kRefreshToken);
+  value = config->FindString(kHostIdConfigPath);
+  ASSERT_NE(value, nullptr);
+  EXPECT_EQ(*value, kTestDirectoryId);
+  // We use the value from GetHostname() if no name is provided.
+  EXPECT_TRUE(config->FindString(kHostNameConfigPath));
+  // Just check for existence here.
+  EXPECT_TRUE(config->FindString(kPrivateKeyConfigPath));
+  // Ensure we did not write a PIN hash value since no PIN was provided.
+  EXPECT_FALSE(config->FindString(kHostSecretHashConfigPath));
+
+  // Verify Stop() was not called.
+  EXPECT_FALSE(test_daemon_controller_delegate().stop_called());
+}
+
+TEST_F(HostStarterBaseTest, ExistingHostIsStopped) {
+  HostStarter::Params params;
+  params.owner_email = kTestUserEmail;
+
+  base::Value::Dict initial_config;
+  initial_config.Set(kHostIdConfigPath, "old_crusty_host_id");
+  test_daemon_controller_delegate().set_initial_config(
+      std::move(initial_config));
+
+  test_host_starter().StartHost(std::move(params), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation completed successfully.
+  EXPECT_EQ(start_result(), HostStarter::START_COMPLETE);
+
+  // Verify no errors were reported.
+  EXPECT_TRUE(test_host_starter().error_message().empty());
+
+  // Verify Stop() was called.
+  EXPECT_TRUE(test_daemon_controller_delegate().stop_called());
+}
+
+TEST_F(HostStarterBaseTest, MismatchedOwnerEmail) {
+  HostStarter::Params params;
+  params.owner_email = "not-the-person-who-generated-the-auth-code@fraud.com";
+  params.auth_code = "auth_me_dude";
+  params.redirect_url = "/redirect";
+
+  test_host_starter().StartHost(std::move(params), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation failed for the expected reason.
+  EXPECT_EQ(start_result(), HostStarter::PERMISSION_DENIED);
+
+  // Verify no user access token was provided and an error was reported.
+  EXPECT_EQ(test_host_starter().user_access_token(), std::string());
+  EXPECT_FALSE(test_host_starter().error_message().empty());
+}
+
+TEST_F(HostStarterBaseTest, RegisterNewHostCallbackDoesNotProvideId) {
+  test_host_starter().clear_directory_id();
+
+  test_host_starter().StartHost(HostStarter::Params(), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation failed for the expected reason.
+  EXPECT_EQ(start_result(), HostStarter::REGISTRATION_ERROR);
+
+  // Verify an error was reported.
+  EXPECT_NE(test_host_starter().error_message(), std::string());
+}
+
+TEST_F(HostStarterBaseTest, RegisterNewHostCallbackProvideMismatchedId) {
+  HostStarter::Params params;
+  params.id = "my-guid";
+
+  test_host_starter().StartHost(std::move(params), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation failed for the expected reason.
+  EXPECT_EQ(start_result(), HostStarter::REGISTRATION_ERROR);
+
+  // Verify an error was reported.
+  EXPECT_NE(test_host_starter().error_message(), std::string());
+}
+
+TEST_F(HostStarterBaseTest, RegisterNewHostCallbackDoesNotProvideAuthCode) {
+  test_host_starter().clear_robot_authorization_code();
+
+  test_host_starter().StartHost(HostStarter::Params(), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation failed for the expected reason.
+  EXPECT_EQ(start_result(), HostStarter::REGISTRATION_ERROR);
+
+  // Verify an error was reported.
+  EXPECT_NE(test_host_starter().error_message(), std::string());
+}
+
+TEST_F(HostStarterBaseTest, RegisterNewHostCallbackDoesNotProvideOwnerEmail) {
+  test_host_starter().clear_owner_account_email();
+
+  test_host_starter().StartHost(HostStarter::Params(), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation failed for the expected reason.
+  EXPECT_EQ(start_result(), HostStarter::REGISTRATION_ERROR);
+
+  // Verify an error was reported.
+  EXPECT_NE(test_host_starter().error_message(), std::string());
+}
+
+TEST_F(HostStarterBaseTest,
+       RegisterNewHostCallbackDoesNotProvideServiceAccount) {
+  test_host_starter().clear_service_account_email();
+
+  test_host_starter().StartHost(HostStarter::Params(), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation failed for the expected reason.
+  EXPECT_EQ(start_result(), HostStarter::REGISTRATION_ERROR);
+
+  // Verify an error was reported.
+  EXPECT_NE(test_host_starter().error_message(), std::string());
+}
+
+TEST_F(HostStarterBaseTest, NewHostFailsToStart) {
+  test_daemon_controller_delegate().set_result_for_config_and_start(
+      DaemonController::RESULT_FAILED);
+
+  test_host_starter().StartHost(HostStarter::Params(), GetCompletionCallback());
+  RunUntilQuit();
+
+  // Make sure the operation failed for the expected reason.
+  EXPECT_EQ(start_result(), HostStarter::START_ERROR);
+
+  // Verify an error was reported.
+  EXPECT_NE(test_host_starter().error_message(), std::string());
+}
+
+}  // namespace remoting
