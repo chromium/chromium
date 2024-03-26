d8fade3988d773522d5e436e4574081d35fe33ca
Colin Blundell <blundell@chromium.org>
2024-03-26 10:21:02 +0000

    [//gpu] Have WebGPUDecoderImpl check for STM support on Android
    Chromium is using SharedTextureMemoryAHardwareBuffer rather than the old
    ExternalImage API on Android for importing AHardwareBuffers. This CL
    updates WebGPUDecoderImpl's check for support correspondingly.
    
    Bug: 327111284
    Change-Id: I0adc95cf1930d7147f2bbfc465d1efaa50c9b13c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5392681
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278219}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -1720,19 +1720,20 @@ wgpu::Adapter WebGPUDecoderImpl::CreatePreferredAdapter(
     // upload/readback to/from shared images.
     bool supports_external_textures = false;
 #if BUILDFLAG(IS_APPLE)
-    // On Apple, Chromium uses SharedTextureMemory to import IOSurfaces.
     wgpu::Adapter adapter_obj(adapter.Get());
     supports_external_textures =
         adapter_obj.HasFeature(wgpu::FeatureName::SharedTextureMemoryIOSurface);
+#elif BUILDFLAG(IS_ANDROID)
+    wgpu::Adapter adapter_obj(adapter.Get());
+    supports_external_textures = adapter_obj.HasFeature(
+        wgpu::FeatureName::SharedTextureMemoryAHardwareBuffer);
 #else
-    // On all other platforms, Chromium currently uses the platform-specific
-    // ExternalImage API surfaces.
-    // NOTE: These platforms should be switched to the corresponding
+    // Chromium is in the midst of being transitioned to SharedTextureMemory
+    // platform by platform. On platforms that have not yet been transitioned,
+    // Chromium uses the platform-specific ExternalImage API surfaces.  NOTE:
+    // These platforms should be switched to the corresponding
     // SharedTextureMemory feature check as they are converted to using
     // SharedTextureMemory.
-    // TODO(crbug.com/327111284): Change this to check for
-    // SharedTextureMemoryAHardwareBuffer on Android-Vulkan once we've made the
-    // switch there.
     supports_external_textures = adapter.SupportsExternalImages();
 #endif
     if (!(supports_external_textures || is_swiftshader)) {
