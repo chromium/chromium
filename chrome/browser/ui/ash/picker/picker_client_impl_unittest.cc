d2d1786640ce5e8a0ae4f6780b48b1242b3e0164
Darren Shen <shend@chromium.org>
2024-03-26 23:27:38 +0000

    Change PickerClientImpl to use BrowserWithTestWindowTest.
    It has all the setup we're doing manually without sacrificing
    readability. We'll need it to create browser tabs in future CLs.
    
    Bug: b/331312566
    Change-Id: I0216956e24e034329d10a4b7d0a513b26aff765f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398239
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278681}

--- chrome/browser/ui/ash/picker/picker_client_impl_unittest.cc
+++ chrome/browser/ui/ash/picker/picker_client_impl_unittest.cc
@@ -14,6 +14,7 @@
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
@@ -23,10 +24,7 @@
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/test/test_history_database.h"
 #include "components/user_manager/fake_user_manager.h"
-#include "components/user_manager/scoped_user_manager.h"
-#include "content/public/test/browser_task_environment.h"
-#include "net/test/embedded_test_server/embedded_test_server.h"
-#include "services/network/test/test_shared_url_loader_factory.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -75,91 +73,55 @@ void AddBookmarks(TestingProfile* profile,
                          std::u16string(title), url);
 }
 
-class PickerClientImplTest : public testing::Test {
+class PickerClientImplTest : public BrowserWithTestWindowTest {
  public:
-  PickerClientImplTest()
-      : task_environment_(content::BrowserTaskEnvironment::IO_MAINLOOP),
-        test_shared_url_loader_factory_(
-            base::MakeRefCounted<network::TestSharedURLLoaderFactory>()),
-        fake_user_manager_(std::make_unique<user_manager::FakeUserManager>()),
-        testing_profile_manager_(TestingBrowserProcess::GetGlobal()) {}
+  PickerClientImplTest() = default;
 
   void SetUp() override {
-    ASSERT_TRUE(testing_profile_manager_.SetUp());
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    BrowserWithTestWindowTest::SetUp();
   }
-  void TearDown() override {
-    for (const user_manager::User* user : fake_user_manager_->GetUsers()) {
-      fake_user_manager_->OnUserProfileWillBeDestroyed(user->GetAccountId());
-    }
-  }
-
-  struct LoginState {
-    raw_ptr<user_manager::UserManager> user_manager;
-    raw_ptr<TestingProfile> profile;
-  };
-
-  // Returns the user manager and profile used in this test, logged into a fake
-  // user.
-  LoginState LogInAsFakeUser() {
-    AccountId account_id = AccountId::FromUserEmail("test@test");
-
-    const user_manager::User* user = fake_user_manager_->AddUser(account_id);
-    fake_user_manager_->UserLoggedIn(account_id, user->username_hash(),
-                                     /*browser_restart=*/false,
-                                     /*is_child=*/false);
-    TestingProfile* profile = CreateTestingProfileForAccount(account_id);
-    fake_user_manager_->OnUserProfileCreated(account_id, profile->GetPrefs());
-
-    return {
-        .user_manager = fake_user_manager_.Get(),
-        .profile = profile,
-    };
-  }
-
+  void TearDown() override { BrowserWithTestWindowTest::TearDown(); }
   scoped_refptr<network::SharedURLLoaderFactory> GetSharedURLLoaderFactory() {
     return test_shared_url_loader_factory_;
   }
 
- private:
-  TestingProfile* CreateTestingProfileForAccount(const AccountId& account_id) {
-    return testing_profile_manager_.CreateTestingProfile(
-        account_id.GetUserEmail(),
-        {{HistoryServiceFactory::GetInstance(),
-          base::BindRepeating(&BuildTestHistoryService, temp_dir_.GetPath())},
-         {BookmarkModelFactory::GetInstance(),
-          BookmarkModelFactory::GetDefaultFactory()},
-         {TemplateURLServiceFactory::GetInstance(),
-          base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor)}},
-        /*is_main_profile=*/false, test_shared_url_loader_factory_);
+  TestingProfile* CreateProfile(const std::string& profile_name) override {
+    auto* profile = profile_manager()->CreateTestingProfile(
+        profile_name, GetTestingFactories(), /*is_main_profile=*/false,
+        test_shared_url_loader_factory_);
+    OnUserProfileCreated(profile_name, profile);
+    return profile;
   }
 
+  TestingProfile::TestingFactories GetTestingFactories() override {
+    return {
+        {HistoryServiceFactory::GetInstance(),
+         base::BindRepeating(&BuildTestHistoryService, temp_dir_.GetPath())},
+        {BookmarkModelFactory::GetInstance(),
+         BookmarkModelFactory::GetDefaultFactory()},
+        {TemplateURLServiceFactory::GetInstance(),
+         base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor)}};
+  }
+
+ private:
   base::ScopedTempDir temp_dir_;
-  content::BrowserTaskEnvironment task_environment_;
   scoped_refptr<network::SharedURLLoaderFactory>
       test_shared_url_loader_factory_;
-  // Keep `fake_user_manager_` before `testing_profile_manager_` to match
-  // destruction order in production:
-  // https://crsrc.org/c/chrome/browser/ash/chrome_browser_main_parts_ash.cc;l=1668;drc=c7da8fba0e20c71d61e5c78ecd6a3872c4c56e6c
-  // https://crsrc.org/c/chrome/browser/ash/chrome_browser_main_parts_ash.cc;l=1719;drc=c7da8fba0e20c71d61e5c78ecd6a3872c4c56e6c
-  user_manager::TypedScopedUserManager<user_manager::FakeUserManager>
-      fake_user_manager_;
-  TestingProfileManager testing_profile_manager_;
 };
 
 TEST_F(PickerClientImplTest, GetsSharedURLLoaderFactory) {
   ash::PickerController controller;
-  PickerClientImpl client(&controller, LogInAsFakeUser().user_manager);
+  PickerClientImpl client(&controller, user_manager());
 
   EXPECT_EQ(client.GetSharedURLLoaderFactory(), GetSharedURLLoaderFactory());
 }
 
 TEST_F(PickerClientImplTest, StartCrosSearch) {
   ash::PickerController controller;
-  LoginState login = LogInAsFakeUser();
-  PickerClientImpl client(&controller, login.user_manager);
-  AddSearchToHistory(login.profile, GURL("http://foo.com/history"));
-  AddBookmarks(login.profile, u"Foobaz", GURL("http://foo.com/bookmarks"));
+  PickerClientImpl client(&controller, user_manager());
+  AddSearchToHistory(profile(), GURL("http://foo.com/history"));
+  AddBookmarks(profile(), u"Foobaz", GURL("http://foo.com/bookmarks"));
   base::test::TestFuture<void> test_done;
 
   NiceMock<MockSearchResultsCallback> mock_search_callback;
@@ -198,10 +160,9 @@ TEST_F(PickerClientImplTest, StartCrosSearch) {
 
 TEST_F(PickerClientImplTest, StartCrosSearchFilteredByBrowsingHistory) {
   ash::PickerController controller;
-  LoginState login = LogInAsFakeUser();
-  PickerClientImpl client(&controller, login.user_manager);
-  AddSearchToHistory(login.profile, GURL("http://foo.com/history"));
-  AddBookmarks(login.profile, u"Foobaz", GURL("http://foo.com/bookmarks"));
+  PickerClientImpl client(&controller, user_manager());
+  AddSearchToHistory(profile(), GURL("http://foo.com/history"));
+  AddBookmarks(profile(), u"Foobaz", GURL("http://foo.com/bookmarks"));
   base::test::TestFuture<void> test_done;
 
   NiceMock<MockSearchResultsCallback> mock_search_callback;
@@ -226,10 +187,9 @@ TEST_F(PickerClientImplTest, StartCrosSearchFilteredByBrowsingHistory) {
 
 TEST_F(PickerClientImplTest, StartCrosSearchFilteredByBookmarks) {
   ash::PickerController controller;
-  LoginState login = LogInAsFakeUser();
-  PickerClientImpl client(&controller, login.user_manager);
-  AddSearchToHistory(login.profile, GURL("http://foo.com/history"));
-  AddBookmarks(login.profile, u"Foobaz", GURL("http://foo.com/bookmarks"));
+  PickerClientImpl client(&controller, user_manager());
+  AddSearchToHistory(profile(), GURL("http://foo.com/history"));
+  AddBookmarks(profile(), u"Foobaz", GURL("http://foo.com/bookmarks"));
   base::test::TestFuture<void> test_done;
 
   NiceMock<MockSearchResultsCallback> mock_search_callback;
