8a06620e1eed830aa3814dae4228e784eaecda54
Avi Drissman <avi@chromium.org>
2024-03-26 20:01:24 +0000

    Move mojo/ to use new base::Pickle functionality
    This moves to using the new base::Pickle creation factories.
    
    Bug: 330028190
    Change-Id: I86983ae7aada7e0459878156a50ec01308ab9fdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384448
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278550}

--- mojo/public/cpp/bindings/lib/native_enum_serialization.h
+++ mojo/public/cpp/bindings/lib/native_enum_serialization.h
@@ -47,8 +47,8 @@ struct NativeEnumSerializerImpl {
 
   static bool Deserialize(int32_t input, UserType* output) {
     PickleData data = {sizeof(int32_t), input};
-    base::Pickle pickle_view(reinterpret_cast<const char*>(&data),
-                             sizeof(PickleData));
+    base::Pickle pickle_view =
+        base::Pickle::WithUnownedBuffer(base::byte_span_from_ref(data));
     base::PickleIterator iter(pickle_view);
     return Traits::Read(&pickle_view, &iter, output);
   }
