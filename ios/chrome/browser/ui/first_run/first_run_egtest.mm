01bd2e2e9151dd13bea82029367afd6ade30800a
Jérôme Lebel <jlebel@chromium.org>
2024-03-26 15:35:04 +0000

    [M124][iOS] Add a preferred size for the DSE choice screen on iPad
    This cherry-pick includes also:
    + crrev.com/c/5389314
    + crrev.com/c/5391319
    
    This CL sets a preferred size of (540, 820) for the preferred size
    on iPad for the Default Search Engine Choice view.
    
    It follows the documentation here:
    https://developer.apple.com/documentation/uikit/uimodalpresentationstyle/uimodalpresentationformsheet?language=objc
    
    Screenshots (Google-internal):
    https://docs.google.com/document/d/1QI4kOf1GRCE9koCjNQS9MYzN9Q1ATtroHFovA8Wn0YY/edit?usp=sharing
    
    Fixed: 331195422
    
    (cherry picked from commit 7f37e895de8f9b97296050291792c980df84210f)
    
    Change-Id: I614a75f4e99a1d1333140d238f48880bb5d588b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388186
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1277705}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5393062
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6367@{#266}
    Cr-Branched-From: d158c6dc6e3604e6f899041972edf26087a49740-refs/heads/main@{#1274542}

--- ios/chrome/browser/ui/first_run/first_run_egtest.mm
+++ ios/chrome/browser/ui/first_run/first_run_egtest.mm
@@ -1782,15 +1782,8 @@ void DismissDefaultBrowserAndOmniboxPositionSelectionScreens() {
 // Tests that the Search Engine Choice screen is displayed, that the primary
 // button is correctly updated when the user selects a search engine then
 // scrolls down and that it correctly sets the default search engine.
-// TODO(crbug.com/329210226): Re-enable the test on device.
-#if !TARGET_OS_SIMULATOR
-#define MAYBE_testSearchEngineChoiceScreenSelectThenScroll \
-  DISABLED_testSearchEngineChoiceScreenSelectThenScroll
-#else
-#define MAYBE_testSearchEngineChoiceScreenSelectThenScroll \
-  testSearchEngineChoiceScreenSelectThenScroll
-#endif
-- (void)MAYBE_testSearchEngineChoiceScreenSelectThenScroll {
+// TODO(crbug.com/329210226): Re-enable the test.
+- (void)DISABLED_testSearchEngineChoiceScreenSelectThenScroll {
   // Skips sign-in.
   [[self elementInteractionWithGreyMatcher:
              chrome_test_util::PromoStyleSecondaryActionButtonMatcher()
@@ -1827,15 +1820,8 @@ void DismissDefaultBrowserAndOmniboxPositionSelectionScreens() {
 // Tests that the Search Engine Choice screen is displayed, that the
 // primary button is correctly updated when the user scrolls down then selects a
 // search engine and that it correctly sets the default search engine.
-// TODO(crbug.com/329210226): Re-enable the test on device.
-#if !TARGET_OS_SIMULATOR
-#define MAYBE_testSearchEngineChoiceScreenScrollThenSelect \
-  DISABLED_testSearchEngineChoiceScreenScrollThenSelect
-#else
-#define MAYBE_testSearchEngineChoiceScreenScrollThenSelect \
-  testSearchEngineChoiceScreenScrollThenSelect
-#endif
-- (void)MAYBE_testSearchEngineChoiceScreenScrollThenSelect {
+// TODO(crbug.com/329210226): Re-enable the test.
+- (void)DISABLED_testSearchEngineChoiceScreenScrollThenSelect {
   // Skips sign-in.
   [[self elementInteractionWithGreyMatcher:
              chrome_test_util::PromoStyleSecondaryActionButtonMatcher()
