28113a612cab3df4c209864efe7d7f3ac4a25c9c
Kramer Ge <fangzhoug@chromium.org>
2024-03-26 16:21:45 +0000

    [exo]Skip GetWidgetHitTestMask if root_surface is null
    When root_surface is destroyed ShellSurfaceBase::OnSurfaceDestroying()
    calls SetRootSurface() first, then calls widget_->Hide().
    widget_->Hide() can lead to DispatchMouseExitToHidingWindow() which will
    perform hitttes on the ShellSurfaceBase again, so we need to perform a
    nullptr check in GetWidgetHitTestMask().
    
    Bug: b/327689672
    Change-Id: I0822966a932ef8999799849d6b1c107d4583c58b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5385986
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278366}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -1404,6 +1404,9 @@ void ShellSurfaceBase::GetWidgetHitTestMask(SkPath* mask) const {
     return;
   }
 
+  if (!HasHitTestRegion()) {
+    return;
+  }
   GetHitTestMask(mask);
 
   const float scale = GetScale();
