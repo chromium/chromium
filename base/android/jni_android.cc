7d4a99957ee114c9cfc1e64d548433299d32eeb3
Kyle Farnung <kfarnung@microsoft.com>
2024-03-26 21:15:16 +0000

    [base] Fix missing Robolectric deps
    The Robolectric flavor of both jni_android.cc and
    library_loader_hooks.cc were incorrectly referencing the non-Robolectric
    versions of the JniAndroid_jni.h and LibraryLoader_jni.h headers,
    respectively. This causes Ninja's missingdeps check to flag these
    objects as having missing dependencies.
    
    Following the pattern of other files, update the objects to check for
    the IS_ROBOLECTRIC build flag to include the correct headers.
    
    Bug: 331246724
    Change-Id: Ib9135f4182df200defe36782607312866f8bfb5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5395189
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Fortiner <samfort@microsoft.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Fortiner <samfort@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1278600}

--- base/android/jni_android.cc
+++ base/android/jni_android.cc
@@ -11,7 +11,6 @@
 #include "base/android/jni_string.h"
 #include "base/android/jni_utils.h"
 #include "base/android_runtime_jni_headers/Throwable_jni.h"
-#include "base/base_jni/JniAndroid_jni.h"
 #include "base/debug/debugging_buildflags.h"
 #include "base/feature_list.h"
 #include "base/logging.h"
@@ -19,6 +18,12 @@
 #include "build/robolectric_buildflags.h"
 #include "third_party/abseil-cpp/absl/base/attributes.h"
 #include "third_party/jni_zero/jni_zero.h"
+
+#if BUILDFLAG(IS_ROBOLECTRIC)
+#include "base/base_robolectric_jni/JniAndroid_jni.h"  // nogncheck
+#else
+#include "base/base_jni/JniAndroid_jni.h"
+#endif
 
 namespace base {
 namespace android {
