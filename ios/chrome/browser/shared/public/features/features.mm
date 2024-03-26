c40f523037d679651e045ca9db8b62d21c1bc5b0
Jood Hajeer <jood@google.com>
2024-03-26 19:05:28 +0000

    [iOS] UNOp2 fast-follows: Enable features by default
    This includes the features:
    kHistoryOptInForRestoreShortyAndReSignin
    kEnableBatchUploadFromBookmarksManager
    kEnableReviewAccountSettingsPromo
    kLinkAccountSettingsToPrivacyFooter
    
    Bug: 1487524
    Change-Id: I98face06f785e2a7b6b5769c05b70031396708ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398117
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278519}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -331,19 +331,19 @@ BASE_FEATURE(kEnableUIEditMenuInteraction,
 
 BASE_FEATURE(kHistoryOptInForRestoreShortyAndReSignin,
              "HistoryOptInForRestoreShortyAndReSignin",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableBatchUploadFromBookmarksManager,
              "EnableBatchUploadFromBookmarksManager",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableReviewAccountSettingsPromo,
              "EnableReviewAccountSettingsPromo",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLinkAccountSettingsToPrivacyFooter,
              "LinkAccountSettingsToPrivacyFooter",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableWebChannels,
              "EnableWebChannels",
