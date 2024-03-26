4516d846c6bbc71d61995b56bb4942b8308261ec
Yulun Zeng <yulunz@chromium.org>
2024-03-26 16:31:00 +0000

    Enable limited entropy mode in beta.
    Bug: 41484361
    Change-Id: I349c5099b8de5f1b7322c47a7e708db91bb85824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5393244
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Yulun Zeng <yulunz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278375}

--- components/variations/limited_entropy_mode_gate.cc
+++ components/variations/limited_entropy_mode_gate.cc
@@ -21,10 +21,8 @@ bool IsLimitedEntropyModeEnabled(version_info::Channel channel) {
   if (g_forced_state_for_testing != ForcedState::UNSET) {
     return g_forced_state_for_testing == ForcedState::ENABLED;
   }
-  // TODO(crbug.com/1511779): Enable limited entropy mode in more channels.
-  return channel == version_info::Channel::CANARY ||
-         channel == version_info::Channel::DEV ||
-         channel == version_info::Channel::UNKNOWN;
+  // TODO(crbug.com/1511779): Enable limited entropy mode in stable.
+  return channel != version_info::Channel::STABLE;
 }
 
 void EnableLimitedEntropyModeForTesting() {
