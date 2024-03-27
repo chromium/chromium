9fb12f9db066c3e73fc85bb642d8b00c843023d6
Rahul Kumar Adudodla <adudodla@google.com>
2024-03-27 19:01:25 +0000

    Dismiss Apple ID Verification from EG tests
    Bug: 329680174
    Change-Id: I66f3852829eb418230cc3de70459a50cbfc0191f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5400201
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Yue She <yueshe@google.com>
    Reviewed-by: Yue She <yueshe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1279143}

--- ios/testing/system_alert_handler.mm
+++ ios/testing/system_alert_handler.mm
@@ -61,6 +61,7 @@ NSDictionary<NSString*, NSArray<NSString*>*>* TextToButtonsOfKnownSystemAlerts(
       @"Unable to activate Touch ID on this iPhone." : @[ @"OK" ],
       @"Like to Access the Microphone" : @[ @"OK" ],
       @"Edit Home Screen" : @[ @"Dismiss" ],
+      @"Apple ID Verification" : @[ @"Not Now" ],
     };
   });
   return text_to_buttons;
