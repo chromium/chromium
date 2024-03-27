5aea85df0ede4bc8e8ed7ceb0073f2f23c0c2336
Cheick Cisse <cheickcisse@google.com>
2024-03-27 13:22:44 +0000

    [PriceInsights][iOS] Add price tracking UI
    This CL adds the following:
    - The main content view stack, which will hold the views of the different components of Price Insights
    - The first component of Price Insights, Price Tracking UI. This component also displays the Price Range if the data is available together with price tracking.
    
    Screenshots:
    No variant price tracking and price range: https://screenshot.googleplex.com/AQKpcbPY8w8wTkK.png
    Variant price tracking and price range: https://screenshot.googleplex.com/4qKkazvYm4GTHJV.png
    Only price tracking: https://screenshot.googleplex.com/4zHAPtKB4FdNyb7.png
    Only price range when price history is available
    https://screenshot.googleplex.com/5Fr6fSBmarvPer5.png
    
    This CL is missing the refactoring of PriceNotificationTrackButton to allow toggling between track and untrack.
    
    Bug: b/326449938
    Change-Id: I5367d9973f9a63753d8fa20c1d68ffcf89843365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5370306
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1278905}

--- ios/chrome/browser/price_insights/ui/price_insights_cell.mm
+++ ios/chrome/browser/price_insights/ui/price_insights_cell.mm
@@ -4,10 +4,191 @@
 
 #import "ios/chrome/browser/price_insights/ui/price_insights_cell.h"
 
-@implementation PriceInsightsCell
+#import "base/strings/sys_string_conversions.h"
+#import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/ui/price_notifications/cells/price_notifications_track_button.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ui/base/l10n/l10n_util_mac.h"
+
+namespace {
+
+// The horizontal inset for the content within the contentStackView.
+const CGFloat kContentHorizontalInset = 20.0f;
+
+// The vertical inset for the content within the contentStackView.
+const CGFloat kContentVerticalInset = 16.0f;
+
+// The horizontal inset between contentStackView and contentView.
+const CGFloat kHorizontalInset = 16.0f;
+
+// The spacing between the content stack views.
+const CGFloat kContentStackViewSpacing = 4.0f;
+
+// The spacing between price tracking vertical stack views.
+const CGFloat kPriceTrackingVerticalStackViewSpacing = 2.0f;
+
+// The spacing between price tracking stack views.
+const CGFloat kPriceTrackingStackViewSpacing = 20.0f;
+
+// The corner radius of this container.
+const float kCornerRadius = 24;
+
+}  // namespace
+
+@interface PriceInsightsCell ()
+
+// Object with data related to price insights.
+@property(nonatomic, strong) PriceInsightsItem* item;
+
+@end
+
+@implementation PriceInsightsCell {
+  PriceNotificationsTrackButton* _trackButton;
+  UIStackView* _priceTrackingStackView;
+  UIStackView* _contentStackView;
+}
 
 #pragma mark - Public
 
-- (void)configureWithItem:(PriceInsightsItem*)item {
+- (instancetype)initWithFrame:(CGRect)frame {
+  self = [super initWithFrame:frame];
+  if (self) {
+    _contentStackView = [[UIStackView alloc] init];
+    _contentStackView.translatesAutoresizingMaskIntoConstraints = NO;
+    _contentStackView.axis = UILayoutConstraintAxisVertical;
+    _contentStackView.spacing = kContentStackViewSpacing;
+    _contentStackView.distribution = UIStackViewDistributionFill;
+    _contentStackView.alignment = UIStackViewAlignmentFill;
+    _contentStackView.clipsToBounds = YES;
+    _contentStackView.layer.cornerRadius = kCornerRadius;
+
+    [self.contentView addSubview:_contentStackView];
+    AddSameConstraintsWithInsets(
+        _contentStackView, self.contentView,
+        NSDirectionalEdgeInsetsMake(0, kHorizontalInset, 0, -kHorizontalInset));
+  }
+  return self;
 }
+
+- (void)configureWithItem:(PriceInsightsItem*)item {
+  self.item = item;
+  if (self.item.canPriceTrack ||
+      ([self hasPriceRange] && [self hasPriceHistory])) {
+    [self configurePriceTrackingAndRange];
+    [_contentStackView addArrangedSubview:_priceTrackingStackView];
+  }
+}
+
+#pragma mark - Private
+
+// Returns whether or not price range is available.
+- (BOOL)hasPriceRange {
+  return self.item.lowPrice.length > 0 && self.item.highPrice.length > 0;
+}
+
+// Returns whether or not price has one typical price. If there is only one
+// typical price, the high and low price are equal.
+- (BOOL)hasPriceOneTypicalPrice {
+  return [self hasPriceRange]
+             ? [self.item.highPrice isEqualToString:self.item.lowPrice]
+             : NO;
+}
+
+// Returns whether or not there are any variants.
+- (BOOL)hasVariants {
+  return self.item.variants.length > 0;
+}
+
+// Returns whether or not price history is available.
+- (BOOL)hasPriceHistory {
+  return NO;
+}
+
+// Method that creates a view for both price tracking and price range, or solely
+// for price tracking or price range when price history is also available.
+- (void)configurePriceTrackingAndRange {
+  UILabel* priceTrackingTitle = [[UILabel alloc] init];
+  priceTrackingTitle.numberOfLines = 1;
+  priceTrackingTitle.textAlignment = NSTextAlignmentLeft;
+  priceTrackingTitle.font =
+      CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightSemibold);
+  priceTrackingTitle.adjustsFontForContentSizeCategory = YES;
+  priceTrackingTitle.adjustsFontSizeToFitWidth = NO;
+  priceTrackingTitle.translatesAutoresizingMaskIntoConstraints = NO;
+  priceTrackingTitle.lineBreakMode = NSLineBreakByTruncatingTail;
+  priceTrackingTitle.textColor = [UIColor colorNamed:kTextPrimaryColor];
+  priceTrackingTitle.text = self.item.title;
+
+  UILabel* priceTrackingSubtitle = [[UILabel alloc] init];
+  priceTrackingSubtitle.textAlignment = NSTextAlignmentLeft;
+  priceTrackingSubtitle.font =
+      CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightRegular);
+  priceTrackingSubtitle.adjustsFontForContentSizeCategory = YES;
+  priceTrackingSubtitle.adjustsFontSizeToFitWidth = NO;
+  priceTrackingSubtitle.lineBreakMode = NSLineBreakByTruncatingTail;
+  priceTrackingSubtitle.translatesAutoresizingMaskIntoConstraints = NO;
+  priceTrackingSubtitle.textColor = [UIColor colorNamed:kTextSecondaryColor];
+  if ([self hasPriceRange] && [self hasPriceHistory]) {
+    priceTrackingSubtitle.numberOfLines = 1;
+    priceTrackingSubtitle.text =
+        [self hasVariants]
+            ? ([self hasPriceOneTypicalPrice]
+                   ? l10n_util::GetNSStringF(
+                         IDS_PRICE_RANGE_ALL_OPTIONS_ONE_TYPICAL_PRICE,
+                         base::SysNSStringToUTF16(self.item.lowPrice))
+                   : l10n_util::GetNSStringF(
+                         IDS_PRICE_RANGE_ALL_OPTIONS,
+                         base::SysNSStringToUTF16(self.item.lowPrice),
+                         base::SysNSStringToUTF16(self.item.highPrice)))
+            : ([self hasPriceOneTypicalPrice]
+                   ? l10n_util::GetNSStringF(
+                         IDS_PRICE_RANGE_SINGLE_OPTION_ONE_TYPICAL_PRICE,
+                         base::SysNSStringToUTF16(self.item.lowPrice))
+                   : l10n_util::GetNSStringF(
+                         IDS_PRICE_RANGE_SINGLE_OPTION,
+                         base::SysNSStringToUTF16(self.item.lowPrice),
+                         base::SysNSStringToUTF16(self.item.highPrice)));
+  } else {
+    priceTrackingSubtitle.numberOfLines = 2;
+    priceTrackingSubtitle.text =
+        l10n_util::GetNSString(IDS_PRICE_TRACKING_DESCRIPTION);
+  }
+
+  UIStackView* verticalStack = [[UIStackView alloc]
+      initWithArrangedSubviews:@[ priceTrackingTitle, priceTrackingSubtitle ]];
+  verticalStack.axis = UILayoutConstraintAxisVertical;
+  verticalStack.distribution = UIStackViewDistributionFill;
+  verticalStack.alignment = UIStackViewAlignmentLeading;
+  verticalStack.spacing = kPriceTrackingVerticalStackViewSpacing;
+
+  _priceTrackingStackView = [[UIStackView alloc] init];
+  [_priceTrackingStackView addArrangedSubview:verticalStack];
+
+  if (self.item.canPriceTrack) {
+    _trackButton = [[PriceNotificationsTrackButton alloc] init];
+    [_trackButton addTarget:self
+                     action:@selector(trackButtonToggled)
+           forControlEvents:UIControlEventTouchUpInside];
+    [_priceTrackingStackView addArrangedSubview:_trackButton];
+  }
+
+  _priceTrackingStackView.axis = UILayoutConstraintAxisHorizontal;
+  _priceTrackingStackView.spacing = kPriceTrackingStackViewSpacing;
+  _priceTrackingStackView.distribution = UIStackViewDistributionFill;
+  _priceTrackingStackView.alignment = UIStackViewAlignmentCenter;
+  _priceTrackingStackView.translatesAutoresizingMaskIntoConstraints = NO;
+  _priceTrackingStackView.backgroundColor =
+      [UIColor colorNamed:kBackgroundColor];
+  _priceTrackingStackView.layoutMarginsRelativeArrangement = YES;
+  _priceTrackingStackView.layoutMargins =
+      UIEdgeInsets(kContentVerticalInset, kContentHorizontalInset,
+                   kContentVerticalInset, kContentHorizontalInset);
+}
+
+#pragma mark - Actions
+
+- (void)trackButtonToggled {
+}
+
 @end
--- ios/chrome/browser/price_insights/ui/price_insights_item.h
+++ ios/chrome/browser/price_insights/ui/price_insights_item.h
@@ -11,6 +11,19 @@
 // the UICollectionViewCell.
 @interface PriceInsightsItem : NSObject
 
+// The product title.
+@property(nonatomic, copy) NSString* title;
+// The product variants.
+@property(nonatomic, copy) NSString* variants;
+// The product typically low price.
+@property(nonatomic, copy) NSString* lowPrice;
+// The product typically high price.
+@property(nonatomic, copy) NSString* highPrice;
+// Whether or not the price can be tracked.
+@property(nonatomic, assign) BOOL canPriceTrack;
+// Whether or not the price is already being tracked.
+@property(nonatomic, assign) BOOL isPriceTracked;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_PRICE_INSIGHTS_UI_PRICE_INSIGHTS_ITEM_H_
