cfdc9117e08d9f050cc0c1ec65d59a4f69985379
Bastian Kersting <bkersting@google.com>
2024-03-28 12:46:38 +0000

    Revert "Automatic Fullscreen: Wait for popup document settings propagation"
    This reverts commit fea8c772e1493dca209d84375b7b9eced2cc5a6f.
    
    Reason for revert: This is failing on LSAN bots, e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/57013/test-results?sortby=&groupby=
    
    Original change's description:
    > Automatic Fullscreen: Wait for popup document settings propagation
    >
    > Retry fullscreen on load for async document settings propagation.
    > Settings are needed for renderer-side automatic fullscreen checks.
    > Use a timeout to prevent requests after a potential cooldown.
    >
    > Bug: 40941384, b:315859364
    > Change-Id: I2a656ff18c3f0a75874afce23098ed32ef0d78c6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5381806
    > Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
    > Auto-Submit: Mike Wasserman <msw@chromium.org>
    > Commit-Queue: Mike Wasserman <msw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1279175}
    
    Bug: 40941384, b:315859364
    Change-Id: I4d06b419626279551bb6e06b50bddd5f583caf66
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401700
    Commit-Queue: Bastian Kersting <bkersting@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Bastian Kersting <bkersting@google.com>
    Cr-Commit-Position: refs/heads/main@{#1279580}

--- chrome/browser/ui/exclusive_access/fullscreen_controller_interactive_browsertest.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_controller_interactive_browsertest.cc
@@ -873,14 +873,10 @@ class AutomaticFullscreenTest : public FullscreenControllerInteractiveTest,
         let w = open(location.href, '', 'popup');
         return new Promise(resolve => {
           w.onload = () => {
-            w.document.onfullscreenchange = () => {
-              clearInterval(w.int);
-              resolve(!!w.document.fullscreenElement);
-            };
-            // Wait for document settings to reach the renderer after load :-/
-            w.int = setInterval(() => w.document.body.requestFullscreen(), 500);
-            // Stop retrying before the automatic fullscreen cooldown period.
-            setTimeout(w.document.onfullscreenchange, 3000);
+            setTimeout(async () => {
+                try { await w.document.body.requestFullscreen(); } catch {}
+                resolve(!!w.document.fullscreenElement);
+            }, 300);  // Wait for document settings to propagate :-/
           };
         });
       })();
