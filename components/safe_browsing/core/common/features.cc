7b62d8278046d7f2b6c93d3ff6df5648b7001ec8
Yoshiki Iguchi <yoshiki@chromium.org>
2024-03-29 14:05:54 +0000

    Revert "Enable SafeBrowsingHashPrefixRealTimeLookups by default on Desktop + iOS"
    This reverts commit 64d1fe3ff099e706d13548b1ebbca9b7034c7eac.
    
    Reason for revert: Breaking `ChromePingManagerFactoryTest.*` in `unit_tests` (b/331903205)
    
    Original change's description:
    > Enable SafeBrowsingHashPrefixRealTimeLookups by default on Desktop + iOS
    >
    > This is done a few days early so we can validate whether it causes any
    > builder issues ahead of time.
    >
    > Per these docs[1], we can check just the IS_CHROMEOS build flag to span
    > both lacros and ash.
    >
    > [1]: https://source.chromium.org/chromium/chromium/src/+/main:docs/lacros.md;l=77-79;drc=f86cdf272a8d2a0427d7e05e996811a19cb611e9
    >
    > Bug: 40280359
    > Change-Id: I22d6401a6b24e4b3931bb4147f7adc2808e2bd7b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402467
    > Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    > Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    > Auto-Submit: thefrog <thefrog@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1279900}
    
    Bug: 40280359
    Bug: b/331903205
    Change-Id: Icfd57dc196eb2eb4572b9208eea66d1108112f53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5404658
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280156}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -128,13 +128,7 @@ BASE_FEATURE(kFriendlierSafeBrowsingSettingsStandardProtection,
 
 BASE_FEATURE(kHashPrefixRealTimeLookups,
              "SafeBrowsingHashPrefixRealTimeLookups",
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_IOS)
-             base::FEATURE_ENABLED_BY_DEFAULT
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 constexpr base::FeatureParam<std::string> kHashPrefixRealTimeLookupsRelayUrl{
     &kHashPrefixRealTimeLookups,
