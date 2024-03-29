1b0a8848730d764579a7b71e7b91ac07736f4d79
David Padlipsky <dpad@google.com>
2024-03-29 23:05:54 +0000

    settings: Remove filtering of floss virtual device
    In crrev.com/c/5407789, the floss virtual device is being filtered out
    before it reaches the DeviceDataManager. Filtering in the notifier is no
    longer needed as it will no longer be injested here.
    
    Fixed: b/331976511
    Change-Id: I09c1f351cb9c5cd747ab14b6af08906e727aff4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5406868
    Reviewed-by: Danny Wang <wangdanny@google.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1280403}

--- ash/system/input_device_settings/input_device_notifier.cc
+++ ash/system/input_device_settings/input_device_notifier.cc
@@ -24,7 +24,6 @@
 #include "components/prefs/pref_service.h"
 #include "device/bluetooth/bluetooth_common.h"
 #include "device/bluetooth/bluetooth_device.h"
-#include "device/bluetooth/floss/floss_features.h"
 #include "ui/events/devices/device_data_manager.h"
 #include "ui/events/devices/input_device.h"
 #include "ui/events/devices/keyboard_device.h"
@@ -35,11 +34,6 @@ namespace ash {
 namespace {
 
 using DeviceId = InputDeviceSettingsController::DeviceId;
-
-// The floss bluetooth handler adds a fake mouse device to the system with the
-// following properties. It is filted out based on the name and vid/pid.
-const char kFlossExtraMouseName[] = "VIRTUAL_SUSPEND_UHID";
-constexpr VendorProductId kFlossExtraMouseVidPid = {0x0000, 0x0000};
 
 bool AreOnLoginScreen() {
   auto status = Shell::Get()->session_controller()->login_status();
@@ -374,14 +368,6 @@ std::vector<ui::InputDevice>
 InputDeviceNotifier<mojom::MousePtr, ui::InputDevice>::GetUpdatedDeviceList() {
   auto mice = ui::DeviceDataManager::GetInstance()->GetMouseDevices();
   std::erase_if(mice, [](const auto& mouse) {
-    if (floss::features::IsFlossEnabled()) {
-      if (kFlossExtraMouseVidPid ==
-              VendorProductId{mouse.vendor_id, mouse.product_id} &&
-          mouse.name == kFlossExtraMouseName) {
-        return true;
-      }
-    }
-
     // Some I2C touchpads falsely claim to be mice, see b/205272718
     // By filtering out internal mice, i2c touchpads are prevented from being in
     // the "mouse" category in settings.
