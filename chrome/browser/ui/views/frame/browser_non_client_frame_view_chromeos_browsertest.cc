0ba590b2e7ecdb630bd03e169d01a23a1541db4d
Georg Neis <neis@chromium.org>
2024-04-01 06:49:38 +0000

    Port a test to Lacros
    BrowserNonClientFrameViewChromeOSTouchTestWithWebUiTabStrip's
    TabletSplitViewSwipeDownFromEdgeOpensWebUiTabStrip test sent a touch
    event to a point outside the browser window. In the case of Lacros,
    that would require using ui_controls (and making the test run as part
    of interactive_ui_tests). Fortunately, we can just as well use a point
    inside the browser window for the purpose of the test.
    
    Bug: 1494785
    Change-Id: Ic6475e5167b7ce84a6dcbb2342e70689bcab8d8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5409792
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280670}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos_browsertest.cc
@@ -246,18 +246,9 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_EQ(HTCLIENT, frame_view->NonClientHitTest(top_point));
 }
 
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-// TODO(crbug.com/1494785): Find out why the test fails and fix it. May require
-// ui_controls for global touch events.
-#define MAYBE_TabletSplitViewSwipeDownFromEdgeOpensWebUiTabStrip \
-  DISABLED_TabletSplitViewSwipeDownFromEdgeOpensWebUiTabStrip
-#else
-#define MAYBE_TabletSplitViewSwipeDownFromEdgeOpensWebUiTabStrip \
-  TabletSplitViewSwipeDownFromEdgeOpensWebUiTabStrip
-#endif
 IN_PROC_BROWSER_TEST_F(
     BrowserNonClientFrameViewChromeOSTouchTestWithWebUiTabStrip,
-    MAYBE_TabletSplitViewSwipeDownFromEdgeOpensWebUiTabStrip) {
+    TabletSplitViewSwipeDownFromEdgeOpensWebUiTabStrip) {
   if (!IsSnapWindowSupported()) {
     GTEST_SKIP() << "Ash is too old.";
   }
@@ -273,9 +264,10 @@ IN_PROC_BROWSER_TEST_F(
   EnterTabletMode();
   SnapWindow(widget->GetNativeWindow(), crosapi::mojom::SnapPosition::kPrimary);
 
-  // A point above the window, but not in the center horizontally, as a swipe
-  // down from the top center will show the chromeos tablet mode multitask menu.
-  gfx::Point edge_point(100, -1);
+  // A point at the top of the window, but not in the center horizontally, as a
+  // swipe down from the top center will show the chromeos tablet mode multitask
+  // menu.
+  gfx::Point edge_point(100, 0);
 
   ASSERT_FALSE(browser_view->webui_tab_strip()->GetVisible());
   aura::Window* window = widget->GetNativeWindow();
