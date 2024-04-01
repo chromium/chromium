b66aa771cfc2af4513ec1e76cd799f04f1c9a197
Colin Blundell <blundell@chromium.org>
2024-04-01 10:26:11 +0000

    [//gpu] Add unittest of ClientSI creation from SharedImageInterface
    This CL adds a unittest of ClientSharedImage creation from
    SharedImageInterface. To do so, we add a TestSharedImageInterface
    implementation in client_shared_image_unittest.cc (the only existing one
    is in //components/viz and is much heavierweight). We stub everything
    out except for the one function needed to create a ClientSI in the
    initial unittest. We will fill it out as the tests expand over time.
    
    Note: While it might seem odd to create ClientSharedImage from
    SharedImageInterface in the ClientSharedImage unittest, this is the
    only and intended supported model of creating ClientSharedImages beyond
    ImportUnowned() (which is already tested). Unless going through
    ImportUnowned(), a ClientSharedImage instance *must* be supplied a
    SharedImageInterfaceHolder, and that can *only* be given by a subclass
    of SharedImageInterface.
    
    Bug: 40286368
    Change-Id: Ia47ed9250c7be46a1508e8310b32d53c37245857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5407653
    Reviewed-by: Quyen Le <lehoangquyen@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1280691}

--- gpu/command_buffer/client/client_shared_image_unittest.cc
+++ gpu/command_buffer/client/client_shared_image_unittest.cc
@@ -6,13 +6,124 @@
 
 #include <GLES2/gl2.h>
 
+#include "gpu/command_buffer/client/shared_image_interface.h"
+#include "gpu/command_buffer/common/shared_image_capabilities.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/gpu_fence.h"
 
 namespace gpu {
 
-// TODO(blundell): Set up SharedImageInterface in these tests and test creation
-// paths that pass in SIIHolder.
+namespace {
+// NOTE: If this test implementation starts to grow heavy, consider pulling the
+// viz::TestSharedImageInterface implementation down into //gpu, unifying that
+// with this one, and exposing that as a general-purpose test utility.
+class TestSharedImageInterface : public SharedImageInterface {
+ public:
+  TestSharedImageInterface() = default;
+
+  // SharedImageInterface:
+  scoped_refptr<ClientSharedImage> CreateSharedImage(
+      const SharedImageInfo& si_info,
+      SurfaceHandle surface_handle) override {
+    mailbox_for_most_recently_created_shared_image_ =
+        gpu::Mailbox::GenerateForSharedImage();
+    return base::MakeRefCounted<gpu::ClientSharedImage>(
+        mailbox_for_most_recently_created_shared_image_, si_info.meta,
+        SyncToken(), holder_, gfx::EMPTY_BUFFER);
+  }
+  scoped_refptr<ClientSharedImage> CreateSharedImage(
+      const SharedImageInfo& si_info,
+      base::span<const uint8_t> pixel_data) override {
+    return nullptr;
+  }
+  scoped_refptr<ClientSharedImage> CreateSharedImage(
+      const SharedImageInfo& si_info,
+      SurfaceHandle surface_handle,
+      gfx::BufferUsage buffer_usage) override {
+    return nullptr;
+  }
+  scoped_refptr<ClientSharedImage> CreateSharedImage(
+      const SharedImageInfo& si_info,
+      SurfaceHandle surface_handle,
+      gfx::BufferUsage buffer_usage,
+      gfx::GpuMemoryBufferHandle buffer_handle) override {
+    return nullptr;
+  }
+  scoped_refptr<ClientSharedImage> CreateSharedImage(
+      const SharedImageInfo& si_info,
+      gfx::GpuMemoryBufferHandle buffer_handle) override {
+    return nullptr;
+  }
+  SharedImageInterface::SharedImageMapping CreateSharedImage(
+      const SharedImageInfo& si_info) override {
+    return {nullptr, base::WritableSharedMemoryMapping()};
+  }
+  scoped_refptr<ClientSharedImage> CreateSharedImage(
+      gfx::GpuMemoryBuffer* gpu_memory_buffer,
+      GpuMemoryBufferManager* gpu_memory_buffer_manager,
+      gfx::BufferPlane plane,
+      const SharedImageInfo& si_info) override {
+    return nullptr;
+  }
+  void UpdateSharedImage(const SyncToken& sync_token,
+                         const Mailbox& mailbox) override {}
+  void UpdateSharedImage(const SyncToken& sync_token,
+                         std::unique_ptr<gfx::GpuFence> acquire_fence,
+                         const Mailbox& mailbox) override {}
+  scoped_refptr<ClientSharedImage> ImportSharedImage(
+      const ExportedSharedImage& exported_shared_image) override {
+    return nullptr;
+  }
+  void DestroySharedImage(const SyncToken& sync_token,
+                          const Mailbox& mailbox) override {}
+  void DestroySharedImage(
+      const SyncToken& sync_token,
+      scoped_refptr<ClientSharedImage> client_shared_image) override {}
+  SwapChainSharedImages CreateSwapChain(viz::SharedImageFormat format,
+                                        const gfx::Size& size,
+                                        const gfx::ColorSpace& color_space,
+                                        GrSurfaceOrigin surface_origin,
+                                        SkAlphaType alpha_type,
+                                        uint32_t usage) override {
+    return {nullptr, nullptr};
+  }
+  void PresentSwapChain(const SyncToken& sync_token,
+                        const Mailbox& mailbox) override {}
+#if BUILDFLAG(IS_FUCHSIA)
+  void RegisterSysmemBufferCollection(zx::eventpair service_handle,
+                                      zx::channel sysmem_token,
+                                      gfx::BufferFormat format,
+                                      gfx::BufferUsage usage,
+                                      bool register_with_image_pipe) override {}
+#endif  // BUILDFLAG(IS_FUCHSIA)
+  SyncToken GenVerifiedSyncToken() override { return SyncToken(); }
+  SyncToken GenUnverifiedSyncToken() override { return SyncToken(); }
+  void VerifySyncToken(SyncToken& sync_token) override {}
+  void WaitSyncToken(const SyncToken& sync_token) override {}
+  void Flush() override {}
+  scoped_refptr<gfx::NativePixmap> GetNativePixmap(
+      const Mailbox& mailbox) override {
+    return nullptr;
+  }
+  const SharedImageCapabilities& GetCapabilities() override {
+    return shared_image_capabilities_;
+  }
+  void SetCapabilities(const SharedImageCapabilities& caps) {}
+
+  const Mailbox& GetMailboxForMostRecentlyCreatedSharedImage() {
+    return mailbox_for_most_recently_created_shared_image_;
+  }
+
+ private:
+  ~TestSharedImageInterface() override = default;
+
+  SharedImageCapabilities shared_image_capabilities_;
+  Mailbox mailbox_for_most_recently_created_shared_image_;
+};
+
+}  // namespace
+
 TEST(ClientSharedImageTest, ImportUnowned) {
   auto mailbox = Mailbox::GenerateForSharedImage();
   const auto kFormat = viz::SinglePlaneFormat::kRGBA_8888;
@@ -37,6 +148,39 @@ TEST(ClientSharedImageTest, ImportUnowned) {
   EXPECT_EQ(client_si->GetTextureTarget(),
             static_cast<uint32_t>(GL_TEXTURE_2D));
   EXPECT_FALSE(client_si->HasHolder());
+}
+
+TEST(ClientSharedImageTest, CreateViaSharedImageInterface) {
+  auto sii = base::MakeRefCounted<TestSharedImageInterface>();
+
+  const auto kFormat = viz::SinglePlaneFormat::kRGBA_8888;
+  const gfx::Size kSize(256, 256);
+  const uint32_t kUsage =
+      SHARED_IMAGE_USAGE_RASTER_WRITE | SHARED_IMAGE_USAGE_DISPLAY_READ;
+  SharedImageInfo si_info{kFormat,
+                          kSize,
+                          gfx::ColorSpace(),
+                          kTopLeft_GrSurfaceOrigin,
+                          kOpaque_SkAlphaType,
+                          kUsage,
+                          ""};
+
+  auto client_si = sii->CreateSharedImage(si_info, kNullSurfaceHandle);
+
+  EXPECT_TRUE(client_si->HasHolder());
+
+  // Check that the ClientSI's state matches the input parameters.
+  EXPECT_TRUE(client_si->mailbox() ==
+              sii->GetMailboxForMostRecentlyCreatedSharedImage());
+  EXPECT_EQ(client_si->format(), kFormat);
+  EXPECT_EQ(client_si->size(), kSize);
+  EXPECT_EQ(client_si->usage(), kUsage);
+
+  // With no scanout or WebGPU usage, external sampling not configured, and no
+  // client-side native buffer handle passed, the SharedImage should be using
+  // the default texture target on all platforms.
+  EXPECT_EQ(client_si->GetTextureTarget(),
+            static_cast<uint32_t>(GL_TEXTURE_2D));
 }
 
 }  // namespace gpu
