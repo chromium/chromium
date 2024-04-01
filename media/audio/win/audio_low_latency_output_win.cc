chromium/chromium

commit f97f0d76be0b54403ef09d45f130c7aeec3798f9

Audio offload for video streaming behind flag.
This enables audio offload behind flag only for streams with latency tag set to kPlayback. For other latency types, audio offload will always be disabled.
Use --enable-features=AudioOffload to enable it. By default the buffer time is 50ms. You can specify buffer time(in ms) by adding extra param for the audio offload feature instead(for example):
--enable-features=AudioOffload:buffer_time_ms/70

Audio offload requires audio service to be at low integrity level, so please make sure audio service is sandboxed when turning on this feature, otherwise the audio output will be automatically switched to non-offload low latency mode.

Bug: chromium:41472482
Change-Id: I7c415274d8378dded76a3c6bb211c84f4c9124f4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5235522
Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
Reviewed-by: Sam McNally <sammc@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1274728}

media/audio/win/audio_low_latency_output_win.cc
@@ -126,51 +126,9 @@ WASAPIAudioOutputStream::WASAPIAudioOutputStream(
   if (!avrt_init)
     SendLogMessage("%s => (WARNING: failed to load Avrt.dll)", __func__);
 
+  // The param passed in may not be for audio offload, and we need to force
+  // disable audio offload if the param is not preferred for it.
   audio_bus_ = AudioBus::Create(params);
-
-  // Set up the desired render format specified by the client. We use the
-  // WAVE_FORMAT_EXTENSIBLE structure to ensure that multiple channel ordering
-  // and high precision data can be supported.
-
-  // Begin with the WAVEFORMATEX structure that specifies the basic format.
-  WAVEFORMATEX* format = &format_.Format;
-  format->wFormatTag = WAVE_FORMAT_EXTENSIBLE;
-  format->nChannels = params.channels();
-  format->nSamplesPerSec = params.sample_rate();
-  format->wBitsPerSample = sizeof(float) * 8;
-  format->nBlockAlign = (format->wBitsPerSample / 8) * format->nChannels;
-  format->nAvgBytesPerSec = format->nSamplesPerSec * format->nBlockAlign;
-  format->cbSize = sizeof(WAVEFORMATEXTENSIBLE) - sizeof(WAVEFORMATEX);
-
-  // Add the parts which are unique to WAVE_FORMAT_EXTENSIBLE.
-  format_.Samples.wValidBitsPerSample = format->wBitsPerSample;
-  format_.dwChannelMask = CoreAudioUtil::GetChannelConfig(device_id, eRender);
-  format_.SubFormat = KSDATAFORMAT_SUBTYPE_IEEE_FLOAT;
-
-  // Store size (in different units) of audio packets which we expect to
-  // get from the audio endpoint device in each render event.
-  packet_size_frames_ = params.frames_per_buffer();
-  packet_size_bytes_ = params.GetBytesPerBuffer(kSampleFormatF32);
-
-#if BUILDFLAG(ENABLE_PLATFORM_DTS_AUDIO)
-  if (params.format() == AudioParameters::AUDIO_BITSTREAM_DTS) {
-    format_.SubFormat = KSDATAFORMAT_SUBTYPE_IEC61937_DTS;
-    format->wBitsPerSample = 16;
-    format->nChannels = 2;
-    format_.dwChannelMask = KSAUDIO_SPEAKER_STEREO;
-    format_.Samples.wValidBitsPerSample = format->wBitsPerSample;
-    format->nBlockAlign = (format->wBitsPerSample / 8) * format->nChannels;
-    format->nAvgBytesPerSec = format->nSamplesPerSec * format->nBlockAlign;
-    packet_size_frames_ = 512;
-    packet_size_bytes_ = params.GetBytesPerBuffer(kSampleFormatS16);
-  }
-#endif  // BUILDFLAG(ENABLE_PLATFORM_DTS_AUDIO)
-  SendLogMessage("%s => (audio engine format=[%s])", __func__,
-                 CoreAudioUtil::WaveFormatToString(&format_).c_str());
-
-  SendLogMessage("%s => (packet size=[%zu bytes/%zu audio frames/%.3f ms])",
-                 __func__, packet_size_bytes_, packet_size_frames_,
-                 params.GetBufferDuration().InMillisecondsF());
 
   AudioParameters::HardwareCapabilities hardware_capabilities =
       params.hardware_capabilities().value_or(
@@ -222,6 +180,23 @@ bool WASAPIAudioOutputStream::Open() {
     return false;
   }
 
+  HRESULT hr = S_FALSE;
+  enable_audio_offload_ = params_.RequireOffload();
+
+  if (share_mode_ == AUDCLNT_SHAREMODE_SHARED && enable_audio_offload_) {
+    enable_audio_offload_ =
+        CoreAudioUtil::EnableOffloadForClient(audio_client.Get());
+    if (!enable_audio_offload_) {
+      SendLogMessage("%s => (INFO: Not enrolling into audio offload.",
+                     __func__);
+      // Return here to allow falling back to non-offload mode.
+      return false;
+    }
+  }
+
+  // Setup wave format after possible audio offload enabling.
+  SetupWaveFormat();
+
   // Extra sanity to ensure that the provided device format is still valid.
   if (!CoreAudioUtil::IsFormatSupported(audio_client.Get(), share_mode_,
                                         &format_)) {
@@ -230,18 +205,22 @@ bool WASAPIAudioOutputStream::Open() {
     return false;
   }
 
-  HRESULT hr = S_FALSE;
   if (share_mode_ == AUDCLNT_SHAREMODE_SHARED) {
     // Initialize the audio stream between the client and the device in shared
     // mode and using event-driven buffer handling.
     hr = CoreAudioUtil::SharedModeInitialize(
         audio_client.Get(), &format_, audio_samples_render_event_.Get(),
         requested_iaudioclient3_buffer_size_, &endpoint_buffer_size_frames_,
-        communications_device ? &kCommunicationsSessionId : nullptr);
+        communications_device ? &kCommunicationsSessionId : nullptr,
+        enable_audio_offload_);
     if (FAILED(hr)) {
       RecordAudioFailure(kOpenFailureHistogram, hr);
       SendLogMessage("%s => (ERROR: IAudioClient::SharedModeInitialize=[%s])",
                      __func__, ErrorToString(hr).c_str());
+      // With audio offload requested, initialization may fail if resource for
+      // audio offload is limited. For low latency output, audio output
+      // resampler will fallback to non-offload mode first; If still fails to
+      // initialize, will then fallback to linear PCM.
       return false;
     }
 
@@ -252,11 +231,27 @@ bool WASAPIAudioOutputStream::Open() {
       return false;
     }
 
-    const int preferred_frames_per_buffer = static_cast<int>(
-        format_.Format.nSamplesPerSec *
-            CoreAudioUtil::ReferenceTimeToTimeDelta(device_period)
-                .InSecondsF() +
-        0.5);
+    UINT32 preferred_frames_per_buffer = 0;
+    if (enable_audio_offload_) {
+      audio_client->GetBufferSize(&preferred_frames_per_buffer);
+
+      // if the granted buffer size is not the same as requested buffer size
+      // re-initializing audio bus.
+      if (packet_size_frames_ != preferred_frames_per_buffer) {
+        AudioParameters params = AudioParameters(params_);
+        params.set_frames_per_buffer(preferred_frames_per_buffer);
+        audio_bus_ = AudioBus::Create(params);
+        // For Offload case GetBuffer API requires the preferred buffer size to
+        // be used or it will fail.
+        packet_size_frames_ = preferred_frames_per_buffer;
+        packet_size_bytes_ = params.GetBytesPerBuffer(kSampleFormatS16);
+      }
+    } else {
+      preferred_frames_per_buffer = AudioTimestampHelper::TimeToFrames(
+          CoreAudioUtil::ReferenceTimeToTimeDelta(device_period),
+          format_.Format.nSamplesPerSec);
+    }
+
     SendLogMessage("%s => (preferred_frames_per_buffer=[%d audio frames])",
                    __func__, preferred_frames_per_buffer);
 
@@ -880,9 +875,14 @@ bool WASAPIAudioOutputStream::RenderAudioFromSource(UINT64 device_frequency) {
     DCHECK_LE(num_filled_bytes, packet_size_bytes_);
     audio_bus_->Scale(volume_);
 
-    // We skip clipping since that occurs at the shared memory boundary.
-    audio_bus_->ToInterleaved<Float32SampleTypeTraitsNoClip>(
-        frames_filled, reinterpret_cast<float*>(audio_data));
+    if (enable_audio_offload_) {
+      audio_bus_->ToInterleaved<SignedInt16SampleTypeTraits>(
+          frames_filled, reinterpret_cast<short*>(audio_data));
+    } else {
+      // We skip clipping since that occurs at the shared memory boundary.
+      audio_bus_->ToInterleaved<Float32SampleTypeTraitsNoClip>(
+          frames_filled, reinterpret_cast<float*>(audio_data));
+    }
 
     peak_detector_->FindPeak(audio_bus_.get());
 
@@ -1060,6 +1060,58 @@ void WASAPIAudioOutputStream::OnDeviceChanged() {
   device_changed_ = true;
   if (source_)
     source_->OnError(AudioSourceCallback::ErrorType::kDeviceChange);
+}
+
+void WASAPIAudioOutputStream::SetupWaveFormat() {
+  // We use the WAVE_FORMAT_EXTENSIBLE structure to ensure that multiple
+  // channel ordering
+  // and high precision data can be supported.
+  // Begin with the WAVEFORMATEX structure that specifies the basic format.
+  WAVEFORMATEX* format = &format_.Format;
+  // Override for audio offload.
+  if (enable_audio_offload_) {
+    format_.SubFormat = KSDATAFORMAT_SUBTYPE_PCM;
+    format->wBitsPerSample = 16;
+    packet_size_bytes_ = params_.GetBytesPerBuffer(kSampleFormatS16);
+  } else {
+    format->wBitsPerSample = sizeof(float) * 8;
+    format_.SubFormat = KSDATAFORMAT_SUBTYPE_IEEE_FLOAT;
+    packet_size_bytes_ = params_.GetBytesPerBuffer(kSampleFormatF32);
+  }
+  format->wFormatTag = WAVE_FORMAT_EXTENSIBLE;
+  format->nChannels = params_.channels();
+  format->nSamplesPerSec = params_.sample_rate();
+  format->nBlockAlign = (format->wBitsPerSample / 8) * format->nChannels;
+  format->nAvgBytesPerSec = format->nSamplesPerSec * format->nBlockAlign;
+  format->cbSize = sizeof(WAVEFORMATEXTENSIBLE) - sizeof(WAVEFORMATEX);
+
+  // Add the parts which are unique to WAVE_FORMAT_EXTENSIBLE.
+  format_.Samples.wValidBitsPerSample = format->wBitsPerSample;
+  format_.dwChannelMask = CoreAudioUtil::GetChannelConfig(device_id_, eRender);
+
+  // Store size (in different units) of audio packets which we expect to
+  // get from the audio endpoint device in each render event.
+  packet_size_frames_ = params_.frames_per_buffer();
+
+#if BUILDFLAG(ENABLE_PLATFORM_DTS_AUDIO)
+  if (params_.format() == AudioParameters::AUDIO_BITSTREAM_DTS) {
+    format_.SubFormat = KSDATAFORMAT_SUBTYPE_IEC61937_DTS;
+    format->wBitsPerSample = 16;
+    format->nChannels = 2;
+    format_.dwChannelMask = KSAUDIO_SPEAKER_STEREO;
+    format_.Samples.wValidBitsPerSample = format->wBitsPerSample;
+    format->nBlockAlign = (format->wBitsPerSample / 8) * format->nChannels;
+    format->nAvgBytesPerSec = format->nSamplesPerSec * format->nBlockAlign;
+    packet_size_frames_ = 512;
+    packet_size_bytes_ = params.GetBytesPerBuffer(kSampleFormatS16);
+  }
+#endif  // BUILDFLAG(ENABLE_PLATFORM_DTS_AUDIO)
+  SendLogMessage("%s => (audio engine format=[%s])", __func__,
+                 CoreAudioUtil::WaveFormatToString(&format_).c_str());
+
+  SendLogMessage("%s => (packet size=[%zu bytes/%zu audio frames/%.3f ms])",
+                 __func__, packet_size_bytes_, packet_size_frames_,
+                 params_.GetBufferDuration().InMillisecondsF());
 }
 
 }  // namespace media

media/audio/audio_manager_base.cc
@@ -452,9 +452,12 @@ AudioOutputStream* AudioManagerBase::MakeAudioOutputStreamProxy(
         //    existing dispatcher are the same as the requested dispatcher.
         // 2) Unified IO is used, input_params and output_params of the existing
         //    dispatcher are the same as the request dispatcher.
+        bool same_offload_mode = output_params.RequireOffload() ==
+                                 dispatcher->output_params.RequireOffload();
         return params.Equals(dispatcher->input_params) &&
                output_params.Equals(dispatcher->output_params) &&
-               output_device_id == dispatcher->output_device_id;
+               output_device_id == dispatcher->output_device_id &&
+               same_offload_mode;
       });
   if (it != output_dispatchers_.end())
     return (*it)->dispatcher->CreateStreamProxy();

media/audio/audio_output_resampler.cc
@@ -24,6 +24,9 @@
 #include "media/audio/audio_manager.h"
 #include "media/audio/audio_output_dispatcher_impl.h"
 #include "media/audio/audio_output_proxy.h"
+#if BUILDFLAG(IS_WIN)
+#include "media/audio/win/core_audio_util_win.h"
+#endif  // BUILDFLAG(IS_WIN)
 #include "media/base/audio_converter.h"
 #include "media/base/audio_timestamp_helper.h"
 #include "media/base/limits.h"
@@ -119,7 +122,7 @@ static void RecordStats(const AudioParameters& output_params) {
 #if BUILDFLAG(IS_WIN)
 // Converts low latency based |output_params| into high latency appropriate
 // output parameters in error situations.
-AudioParameters GetFallbackOutputParams(
+AudioParameters GetFallbackHighLatencyOutputParams(
     const AudioParameters& original_output_params) {
   DCHECK_EQ(original_output_params.format(),
             AudioParameters::AUDIO_PCM_LOW_LATENCY);
@@ -134,6 +137,31 @@ AudioParameters GetFallbackOutputParams(
   fallback_params.set_format(AudioParameters::AUDIO_PCM_LINEAR);
   fallback_params.set_frames_per_buffer(frames_per_buffer);
   return fallback_params;
+}
+
+// Get output parameter for low latency non-offload output, which is used for
+// falling back from offload mode.
+AudioParameters GetFallbackLowLatencyOutputParams(
+    const std::string& device_id,
+    const AudioParameters& original_output_params) {
+  AudioParameters output_params;
+
+  HRESULT hr = CoreAudioUtil::GetPreferredAudioParameters(
+      device_id,
+      /*is_output_device=*/true, &output_params, /*is_offload_stream=*/false);
+  if (SUCCEEDED(hr)) {
+    // Retain the channel layout and effects of the original output parameters.
+    int effects = output_params.effects();
+    effects |= original_output_params.effects();
+    output_params.set_effects(effects);
+    output_params.SetChannelLayoutConfig(
+        original_output_params.channel_layout(),
+        original_output_params.channels());
+    return output_params;
+  }
+  // If we fail to get preferred audio parameters, return empty(invalid)
+  // parameters so that fallbacking to linear mode will be attempted then.
+  return AudioParameters();
 }
 #endif
 
@@ -261,6 +289,8 @@ bool AudioOutputResampler::OpenStream() {
       "Media.FallbackToHighLatencyAudioPath";
   constexpr char kOpenLowLatencyHistogramName[] =
       "Media.AudioOutputResampler.OpenLowLatencyStream";
+  constexpr char kOpenLowLatencyOffloadHistogramName[] =
+      "Media.AudioOutputResampler.OpenLowLatencyStream.Offload";
 
   if (dispatcher_->OpenStream()) {
     // Only record the UMA statistic if we didn't fallback during construction
@@ -288,6 +318,12 @@ bool AudioOutputResampler::OpenStream() {
   // If we have successfully opened a stream previously, there's nothing more to
   // be done.
   if (!first_stream) {
+    if (original_output_params_.RequireOffload()) {
+      base::UmaHistogramEnumeration(
+          kOpenLowLatencyOffloadHistogramName,
+          GetSubsequentStreamCreationResultBucket(output_params_, false));
+      return false;
+    }
     base::UmaHistogramEnumeration(
         kOpenLowLatencyHistogramName,
         GetSubsequentStreamCreationResultBucket(output_params_, false));
@@ -297,12 +333,34 @@ bool AudioOutputResampler::OpenStream() {
   base::UmaHistogramBoolean(kFallbackHistogramName, true);
 
   // Only Windows has a high latency output driver that is not the same as the
-  // low latency path.
+  // low latency path; or it may originally be attempted to be initialized in
+  // offload mode while rejected later due to resource limitation.
 #if BUILDFLAG(IS_WIN)
+  // If Open() fails with offload mode, first try to fallback to non-offload
+  // mode.
+  if (original_output_params_.RequireOffload() &&
+      output_params_.RequireOffload()) {
+    DLOG(ERROR)
+        << "Unable to open device in offload mode. Attempt to fallback to "
+        << "non-offloaded low latency mode.";
+    output_params_ = GetFallbackLowLatencyOutputParams(
+        device_id_.empty() ? CoreAudioUtil::GetDefaultOutputDeviceID()
+                           : device_id_,
+        output_params_);
+    if (output_params_.IsValid()) {
+      dispatcher_ = MakeDispatcher(device_id_, output_params_);
+      if (dispatcher_->OpenStream()) {
+        base::UmaHistogramEnumeration(kOpenLowLatencyHistogramName,
+                                      OpenStreamResult::kFallbackToLinear);
+        return true;
+      }
+    }
+  }
+
   DLOG(ERROR) << "Unable to open audio device in low latency mode.  Falling "
               << "back to high latency audio output.";
 
-  output_params_ = GetFallbackOutputParams(original_output_params_);
+  output_params_ = GetFallbackHighLatencyOutputParams(original_output_params_);
   const std::string fallback_device_id = "";
   dispatcher_ = MakeDispatcher(fallback_device_id, output_params_);
   if (dispatcher_->OpenStream()) {

media/audio/win/audio_low_latency_output_win.h
@@ -197,6 +197,9 @@ class MEDIA_EXPORT WASAPIAudioOutputStream
   // Called by AudioSessionEventListener() when a device change occurs.
   void OnDeviceChanged();
 
+  // Set up the desired render format specified by the client.
+  void SetupWaveFormat();
+
   // Contains the thread ID of the creating thread.
   const base::PlatformThreadId creating_thread_id_;
 
@@ -289,6 +292,8 @@ class MEDIA_EXPORT WASAPIAudioOutputStream
   Microsoft::WRL::ComPtr<IAudioClock> audio_clock_;
 
   bool device_changed_ = false;
+
+  bool enable_audio_offload_ = false;
 
   // Generates Windows audio session events for `session_listener_` to handle.
   Microsoft::WRL::ComPtr<IAudioSessionControl> audio_session_control_;

media/audio/win/audio_low_latency_output_win_unittest.cc
@@ -189,6 +189,20 @@ class AudioOutputStreamWrapper {
     return CreateOutputStream();
   }
 
+  // Creates AudioOutputStream object using non-default parameters where the
+  // sample rate, frame size and audio offload are modified.
+  AudioOutputStream* Create(int sample_rate,
+                            int samples_per_packet,
+                            bool audio_offload) {
+    sample_rate_ = sample_rate;
+    samples_per_packet_ = samples_per_packet;
+    AudioParameters::HardwareCapabilities hardware_cap(0, true);
+    hardware_cap.require_audio_offload = true;
+    hardware_capabilities_ = hardware_cap;
+
+    return CreateOutputStream();
+  }
+
   AudioParameters::Format format() const { return format_; }
   int channels() const { return channels_; }
   int sample_rate() const { return sample_rate_; }
@@ -198,6 +212,9 @@ class AudioOutputStreamWrapper {
   AudioOutputStream* CreateOutputStream() {
     AudioParameters params(format_, {channel_layout_, channels_}, sample_rate_,
                            samples_per_packet_);
+    if (hardware_capabilities_) {
+      params.set_hardware_capabilities(hardware_capabilities_.value());
+    }
     DVLOG(1) << params.AsHumanReadableString();
     AudioOutputStream* aos = audio_man_->MakeAudioOutputStream(
         params, std::string(), AudioManager::LogCallback());
@@ -211,6 +228,7 @@ class AudioOutputStreamWrapper {
   ChannelLayout channel_layout_;
   int sample_rate_;
   int samples_per_packet_;
+  std::optional<AudioParameters::HardwareCapabilities> hardware_capabilities_;
 };
 
 // Convenience method which creates a default AudioOutputStream object.
@@ -376,6 +394,23 @@ TEST_F(WASAPIAudioOutputStreamTest, ValidPacketSize) {
       FROM_HERE, loop.QuitWhenIdleClosure(), TestTimeouts::action_timeout());
   loop.Run();
   aos->Stop();
+  aos->Close();
+}
+
+// Verify that we are not allowed to open the output stream with audio offload
+// enabled in exclusive mode.
+TEST_F(WASAPIAudioOutputStreamTest, ExclusiveModeWithAudioOffload) {
+  ABORT_AUDIO_TEST_IF_NOT(HasCoreAudioAndOutputDevices(audio_manager_.get()) &&
+                          ExclusiveModeIsEnabled());
+
+  // Create exclusive-mode WASAPI output stream which plays out in stereo
+  // using the minimum buffer size at 48kHz sample rate.
+  AudioOutputStreamWrapper aosw(audio_manager_.get());
+
+  // Open should fail with offload stream in exclusive mode.
+  AudioOutputStream* aos = aosw.Create(48000, 160, true);
+  EXPECT_FALSE(aos->Open());
+
   aos->Close();
 }
 

media/audio/win/audio_manager_win.cc
@@ -334,6 +334,7 @@ AudioParameters AudioManagerWin::GetPreferredOutputStreamParameters(
   int effects = AudioParameters::NO_EFFECTS;
   int min_buffer_size = 0;
   int max_buffer_size = 0;
+  bool attempt_audio_offload = CoreAudioUtil::IsAudioOffloadSupported(nullptr);
 
   if (cmd_line->HasSwitch(switches::kEnableExclusiveAudio)) {
     // TODO(rtoy): tune these values for best possible WebAudio
@@ -347,10 +348,11 @@ AudioParameters AudioManagerWin::GetPreferredOutputStreamParameters(
       channel_layout_config = input_params.channel_layout_config();
   } else {
     AudioParameters params;
+
     HRESULT hr = CoreAudioUtil::GetPreferredAudioParameters(
         output_device_id.empty() ? GetDefaultOutputDeviceID()
                                  : output_device_id,
-        true, &params);
+        true, &params, attempt_audio_offload);
     if (FAILED(hr)) {
       // This can happen when CoreAudio isn't supported or available
       // (e.g. certain installations of Windows Server 2008 R2).
@@ -420,8 +422,8 @@ AudioParameters AudioManagerWin::GetPreferredOutputStreamParameters(
   if (user_buffer_size)
     buffer_size = user_buffer_size;
 
-  AudioParameters::HardwareCapabilities hardware_capabilities(min_buffer_size,
-                                                              max_buffer_size);
+  AudioParameters::HardwareCapabilities hardware_capabilities(
+      min_buffer_size, max_buffer_size, attempt_audio_offload);
 #if BUILDFLAG(ENABLE_PASSTHROUGH_AUDIO_CODECS)
   hardware_capabilities.bitstream_formats = 0;
   hardware_capabilities.require_encapsulation = false;

commit 974d478e9fa53e3bd381927612100586b3205527

[Audio] Fix undeclared identifier error
When passing `enable_platform_dts_audio = true`
in the args parameter, an undeclared identifier
error will appear.

The error should be introduced by commit f97f0d7.

Fixed: 331841867,41472482
Change-Id: I81ad92249ddb1ab3eec990880424d44ab6c7c070
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401314
Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
Reviewed-by: Jianlin Qiu <jianlin.qiu@intel.com>
Cr-Commit-Position: refs/heads/main@{#1280843}

media/audio/win/audio_low_latency_output_win.cc
@@ -1103,7 +1103,7 @@ void WASAPIAudioOutputStream::SetupWaveFormat() {
     format->nBlockAlign = (format->wBitsPerSample / 8) * format->nChannels;
     format->nAvgBytesPerSec = format->nSamplesPerSec * format->nBlockAlign;
     packet_size_frames_ = 512;
-    packet_size_bytes_ = params.GetBytesPerBuffer(kSampleFormatS16);
+    packet_size_bytes_ = params_.GetBytesPerBuffer(kSampleFormatS16);
   }
 #endif  // BUILDFLAG(ENABLE_PLATFORM_DTS_AUDIO)
   SendLogMessage("%s => (audio engine format=[%s])", __func__,