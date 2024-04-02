b953235f65b9d59ad0a5cea863f7dd462f41ed62
Aya El Gendy <ayag@chromium.org>
2024-04-02 23:15:44 +0000

    [CodeHealth] Remove TODO comment which has been fixed
    Bug fixed in b/40805389
    
    Bug: b/332406584
    Change-Id: I52088ed7e45ca4c428fa3318cabfa8d2be739b81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403889
    Reviewed-by: Amr Aboelkher <amraboelkher@chromium.org>
    Commit-Queue: Aya Elgendy‎ <ayag@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281531}

--- chrome/browser/ash/login/enrollment/enrollment_launcher.cc
+++ chrome/browser/ash/login/enrollment/enrollment_launcher.cc
@@ -267,8 +267,8 @@ void EnrollmentLauncherImpl::DoEnroll(policy::DMAuth auth_data) {
   DCHECK(enrollment_config_.is_mode_attestation() ||
          oauth_status_ == OAUTH_STARTED_WITH_AUTH_CODE ||
          oauth_status_ == OAUTH_STARTED_WITH_TOKEN);
-  // TODO(crbug.com/1271134): Logging as "WARNING" to make sure it's preserved
-  // in the logs.
+
+  // Logging as "WARNING" to make sure it's preserved in the logs.
   LOG(WARNING) << "Enroll with token type: "
                << static_cast<int>(auth_data.token_type());
   auth_data_ = std::move(auth_data);
@@ -415,8 +415,7 @@ void EnrollmentLauncherImpl::OnEnrollmentFinished(
   enrollment_handler_.reset();
   attestation_flow_.reset();
 
-  // TODO(crbug.com/1271134): Logging as "WARNING" to make sure it's preserved
-  // in the logs.
+  // Logging as "WARNING" to make sure it's preserved in the logs.
   LOG(WARNING) << "Enrollment finished, code: " << status.enrollment_code();
   ReportEnrollmentStatus(status);
   if (oauth_status_ != OAUTH_NOT_STARTED) {
