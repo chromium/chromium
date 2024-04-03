c9f4a3fa203693881b0e97a32520fd0968762c6d
Benjamin Williams <bwwilliams@google.com>
2024-04-03 15:57:45 +0000

    [iOS] Prevent Docking Promo deregistration via killswitch.
    This change introduces an enabled-by-default killswitch preventing the
    Docking Promo from being deregistered in the Promos Manager.
    
    The Docking Promo might never be displayed even if a user once qualified
    for seeing it. This happens when an eligible user registers for the
    promo, but is not shown the promo on that same app launch.
    
    To make the promo easier to see, it shouldn't remove itself once it's
    been activated for a user.
    
    The Docking Promo is only ever shown once per app installation, so this
    will not spam eligible users.
    
    (cherry picked from commit 86df2211725f8e763ab5ecc760dc1c32f609f2d4)
    
    Change-Id: I00425edbae6a0f2fc61a3355dbddb4e97b205021
    Bug: 332318589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5410491
    Reviewed-by: Huiting Yu <huitingyu@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1281363}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5419163
    Cr-Commit-Position: refs/branch-heads/6367@{#520}
    Cr-Branched-From: d158c6dc6e3604e6f899041972edf26087a49740-refs/heads/main@{#1274542}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -65,8 +65,6 @@ const char kIOSDockingPromoNewUserInactiveThresholdHours[] =
     "IOSDockingPromoNewUserInactiveThresholdHours";
 const char kIOSDockingPromoOldUserInactiveThresholdHours[] =
     "IOSDockingPromoOldUserInactiveThresholdHours";
-const char kIOSDockingPromoUsesStartUtilities[] =
-    "IOSDockingPromoUsesStartUtilities";
 
 BASE_FEATURE(kIOSDockingPromo,
              "IOSDockingPromo",
@@ -74,6 +72,10 @@ BASE_FEATURE(kIOSDockingPromo,
 
 BASE_FEATURE(kIOSDockingPromoFixedTriggerLogicKillswitch,
              "IOSDockingPromoFixedTriggerLogicKillswitch",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kIOSDockingPromoPreventDeregistrationKillswitch,
+             "IOSDockingPromoPreventDeregistrationKillswitch",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNonModalDefaultBrowserPromoCooldownRefactor,
@@ -450,12 +452,6 @@ int HoursInactiveForOldUsersUntilShowingDockingPromo() {
   return base::GetFieldTrialParamByFeatureAsInt(
       kIOSDockingPromo, kIOSDockingPromoOldUserInactiveThresholdHours,
       /*default_value=*/72);
-}
-
-bool IsDockingPromoUsingStartUtilities() {
-  return base::GetFieldTrialParamByFeatureAsBool(
-      kIOSDockingPromo, kIOSDockingPromoUsesStartUtilities,
-      /*default=*/false);
 }
 
 bool IsWebChannelsEnabled() {
