0e3c668f8d3ec1d00fe72d501accba879bceba6a
Lukasz Anforowicz <lukasza@chromium.org>
2024-04-03 19:33:53 +0000

    Trim `cargo vet` exemptions by removing the unused `memoffset` crate.
    This CL removes the unused `//third_party/rust/memoffset` crate and its
    dependencies (the `autocfg` crate).  Additional notes:
    
    * `memoffset` crate:
    
        - It was imported into Chromium when working on experimental
          integration of https://github.com/google/crubit into Chromium.
          Since then 1) we decided to proceed with `cxx` for now, and 2)
          Crubit has removed their dependency on `memoffset`:
          https://github.com/google/crubit/commit/dea0567ef8070759ac4f2614bf3e6da5927e734c
        - So far the crate has been covered by `cargo vet` exemption.
          Auditing is problematic, because IIUC the crate has known UB in
          older `rustc` versions (see
          https://github.com/Gilnaa/memoffset/blob/153fc3d50f03755be53148bc3eb97390f9011e45/src/offset_of.rs#L42-L44)
          and `cargo vet` doesn't support conditional certification ("safe,
          but only in new `rustc`").
        - It is unlikely that `memoffset` will be needed in the future.
          Instead, the code should use the new Rust builtins (see Crubit's
          commit above).
    
    * `autocfg` crate:
    
        - It was imported into Chromium in https://crrev.com/c/3576923, as a
          dependency of `autocxx`.
        - It may be needed in the future. For example,
          go/image-crate-chromium-security-review points out that `autocfg`
          is a dependency of `num-rational` and `num-traits` (there are
          transitive dependencies of the `png` crate).
        - The crate has been covered by a real audit.  We are retaining this
          audit - this will hopefully help if/when reimporting this crate
          in the future.
    
    Bug: 41494083
    Change-Id: Id3af3629ecc6a05717d25642d15ba2a2ff8222c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420783
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282051}

--- third_party/rust/chromium_crates_io/vendor/autocfg-1.2.0/examples/versions.rs
+++
@@ -1,9 +0,0 @@
-extern crate autocfg;
-
-fn main() {
-    // Normally, cargo will set `OUT_DIR` for build scripts.
-    let ac = autocfg::AutoCfg::with_dir("target").unwrap();
-    for i in 0..100 {
-        ac.emit_rustc_version(1, i);
-    }
-}
