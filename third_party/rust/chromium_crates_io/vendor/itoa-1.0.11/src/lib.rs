dc1f24c499e9bc7d682fdd9014da0cd1273e882e
Lukasz Anforowicz <lukasza@chromium.org>
2024-04-03 00:16:44 +0000

    Roll itoa: 1.0.10 => 1.0.11 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * itoa: 1.0.10 => 1.0.11
    
    The following `cargo vet` audit criteria are required:
    
    * itoa@1.0.11: does-not-implement-crypto, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: I385a9c65a6123fc9ab2d88b597022e40e5619bf9
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5410360
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281565}

--- third_party/rust/chromium_crates_io/vendor/itoa-1.0.11/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/itoa-1.0.11/src/lib.rs
@@ -30,7 +30,7 @@
 //!
 //! ![performance](https://raw.githubusercontent.com/dtolnay/itoa/master/performance.png)
 
-#![doc(html_root_url = "https://docs.rs/itoa/1.0.10")]
+#![doc(html_root_url = "https://docs.rs/itoa/1.0.11")]
 #![no_std]
 #![allow(
     clippy::cast_lossless,
@@ -137,52 +137,60 @@ macro_rules! impl_Integer {
                 let mut n = if is_nonnegative {
                     self as $conv_fn
                 } else {
-                    // convert the negative num to positive by summing 1 to it's 2 complement
+                    // Convert negative number to positive by summing 1 to its two's complement.
                     (!(self as $conv_fn)).wrapping_add(1)
                 };
                 let mut curr = buf.len() as isize;
                 let buf_ptr = buf.as_mut_ptr() as *mut u8;
                 let lut_ptr = DEC_DIGITS_LUT.as_ptr();
 
-                unsafe {
-                    // need at least 16 bits for the 4-characters-at-a-time to work.
-                    if mem::size_of::<$t>() >= 2 {
-                        // eagerly decode 4 characters at a time
-                        while n >= 10000 {
-                            let rem = (n % 10000) as isize;
-                            n /= 10000;
+                // Need at least 16 bits for the 4-digits-at-a-time to work.
+                if mem::size_of::<$t>() >= 2 {
+                    // Eagerly decode 4 digits at a time.
+                    while n >= 10000 {
+                        let rem = (n % 10000) as isize;
+                        n /= 10000;
 
-                            let d1 = (rem / 100) << 1;
-                            let d2 = (rem % 100) << 1;
-                            curr -= 4;
+                        let d1 = (rem / 100) << 1;
+                        let d2 = (rem % 100) << 1;
+                        curr -= 4;
+                        unsafe {
                             ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
                             ptr::copy_nonoverlapping(lut_ptr.offset(d2), buf_ptr.offset(curr + 2), 2);
                         }
                     }
+                }
 
-                    // if we reach here numbers are <= 9999, so at most 4 chars long
-                    let mut n = n as isize; // possibly reduce 64bit math
+                // If we reach here, numbers are <=9999 so at most 4 digits long.
+                let mut n = n as isize; // Possibly reduce 64-bit math.
 
-                    // decode 2 more chars, if > 2 chars
-                    if n >= 100 {
-                        let d1 = (n % 100) << 1;
-                        n /= 100;
-                        curr -= 2;
+                // Decode 2 more digits, if >2 digits.
+                if n >= 100 {
+                    let d1 = (n % 100) << 1;
+                    n /= 100;
+                    curr -= 2;
+                    unsafe {
                         ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
                     }
+                }
 
-                    // decode last 1 or 2 chars
-                    if n < 10 {
-                        curr -= 1;
+                // Decode last 1 or 2 digits.
+                if n < 10 {
+                    curr -= 1;
+                    unsafe {
                         *buf_ptr.offset(curr) = (n as u8) + b'0';
-                    } else {
-                        let d1 = n << 1;
-                        curr -= 2;
+                    }
+                } else {
+                    let d1 = n << 1;
+                    curr -= 2;
+                    unsafe {
                         ptr::copy_nonoverlapping(lut_ptr.offset(d1), buf_ptr.offset(curr), 2);
                     }
+                }
 
-                    if !is_nonnegative {
-                        curr -= 1;
+                if !is_nonnegative {
+                    curr -= 1;
+                    unsafe {
                         *buf_ptr.offset(curr) = b'-';
                     }
                 }
@@ -239,51 +247,57 @@ macro_rules! impl_Integer128 {
                 let n = if is_nonnegative {
                     self as u128
                 } else {
-                    // convert the negative num to positive by summing 1 to it's 2 complement
+                    // Convert negative number to positive by summing 1 to its two's complement.
                     (!(self as u128)).wrapping_add(1)
                 };
                 let mut curr = buf.len() as isize;
                 let buf_ptr = buf.as_mut_ptr() as *mut u8;
 
-                unsafe {
-                    // Divide by 10^19 which is the highest power less than 2^64.
+                // Divide by 10^19 which is the highest power less than 2^64.
+                let (n, rem) = udiv128::udivmod_1e19(n);
+                let buf1 = unsafe { buf_ptr.offset(curr - U64_MAX_LEN as isize) as *mut [MaybeUninit<u8>; U64_MAX_LEN] };
+                curr -= rem.write(unsafe { &mut *buf1 }).len() as isize;
+
+                if n != 0 {
+                    // Memset the base10 leading zeros of rem.
+                    let target = buf.len() as isize - 19;
+                    unsafe {
+                        ptr::write_bytes(buf_ptr.offset(target), b'0', (curr - target) as usize);
+                    }
+                    curr = target;
+
+                    // Divide by 10^19 again.
                     let (n, rem) = udiv128::udivmod_1e19(n);
-                    let buf1 = buf_ptr.offset(curr - U64_MAX_LEN as isize) as *mut [MaybeUninit<u8>; U64_MAX_LEN];
-                    curr -= rem.write(&mut *buf1).len() as isize;
+                    let buf2 = unsafe { buf_ptr.offset(curr - U64_MAX_LEN as isize) as *mut [MaybeUninit<u8>; U64_MAX_LEN] };
+                    curr -= rem.write(unsafe { &mut *buf2 }).len() as isize;
 
                     if n != 0 {
-                        // Memset the base10 leading zeros of rem.
-                        let target = buf.len() as isize - 19;
-                        ptr::write_bytes(buf_ptr.offset(target), b'0', (curr - target) as usize);
+                        // Memset the leading zeros.
+                        let target = buf.len() as isize - 38;
+                        unsafe {
+                            ptr::write_bytes(buf_ptr.offset(target), b'0', (curr - target) as usize);
+                        }
                         curr = target;
 
-                        // Divide by 10^19 again.
-                        let (n, rem) = udiv128::udivmod_1e19(n);
-                        let buf2 = buf_ptr.offset(curr - U64_MAX_LEN as isize) as *mut [MaybeUninit<u8>; U64_MAX_LEN];
-                        curr -= rem.write(&mut *buf2).len() as isize;
-
-                        if n != 0 {
-                            // Memset the leading zeros.
-                            let target = buf.len() as isize - 38;
-                            ptr::write_bytes(buf_ptr.offset(target), b'0', (curr - target) as usize);
-                            curr = target;
-
-                            // There is at most one digit left
-                            // because u128::max / 10^19 / 10^19 is 3.
-                            curr -= 1;
+                        // There is at most one digit left
+                        // because u128::MAX / 10^19 / 10^19 is 3.
+                        curr -= 1;
+                        unsafe {
                             *buf_ptr.offset(curr) = (n as u8) + b'0';
                         }
                     }
+                }
 
-                    if !is_nonnegative {
-                        curr -= 1;
+                if !is_nonnegative {
+                    curr -= 1;
+                    unsafe {
                         *buf_ptr.offset(curr) = b'-';
                     }
-
-                    let len = buf.len() - curr as usize;
-                    let bytes = slice::from_raw_parts(buf_ptr.offset(curr), len);
-                    str::from_utf8_unchecked(bytes)
                 }
+
+                let len = buf.len() - curr as usize;
+                let bytes = unsafe { slice::from_raw_parts(buf_ptr.offset(curr), len) };
+                unsafe { str::from_utf8_unchecked(bytes) }
             }
         }
     )*};
