40370e3c56502052e62f799ca06f92abf446432c
Gayane Petrosyan <gayane@google.com>
2024-04-03 18:31:22 +0000

    [iOS] Record default browser tailored promo display and actions
    Record to UMA and user defaults when tailored promos are displayed or
    user interacts with the promo.
    
    Continue recording to user defaults as that is used by non-modal
    promos.
    
    Bug: b/331797165
    Change-Id: I77b23a31e51faa4a96743a6fb5e6ff04cccdd968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405416
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282000}

--- ios/chrome/browser/ui/default_promo/base_default_browser_promo_view_provider.mm
+++ ios/chrome/browser/ui/default_promo/base_default_browser_promo_view_provider.mm
@@ -4,6 +4,9 @@
 
 #import "ios/chrome/browser/ui/default_promo/base_default_browser_promo_view_provider.h"
 
+#import "base/metrics/histogram_functions.h"
+#import "base/metrics/user_metrics.h"
+#import "base/metrics/user_metrics_action.h"
 #import "base/notreached.h"
 #import "ios/chrome/browser/default_browser/model/utils.h"
 #import "ios/chrome/browser/promos_manager/model/promo_config.h"
@@ -13,6 +16,8 @@
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util_mac.h"
 
+using base::RecordAction;
+using base::UserMetricsAction;
 using l10n_util::GetNSString;
 
 @interface BaseDefaultBrowserPromoViewProvider ()
@@ -56,36 +61,51 @@ using l10n_util::GetNSString;
 }
 
 - (void)promoWasDisplayed {
-  // TODO: Record metrics.
+  [self recordDefaultBrowserPromoShown];
 }
 
 #pragma mark - StandardPromoActionHandler
 
 // The "Primary Action" was touched.
 - (void)standardPromoPrimaryAction {
+  RecordAction(
+      UserMetricsAction("IOS.DefaultBrowserPromo.TailoredFullscreen.Accepted"));
+  LogDefaultBrowserPromoHistogramForAction(
+      self.defaultBrowserPromoType,
+      IOSDefaultBrowserPromoAction::kActionButton);
+  LogUserInteractionWithTailoredFullscreenPromo();
+
   [self openSettings];
   [self dissmissPromo];
-
-  // TODO: Record metrics.
 }
 
 // The "Secondary Action" was touched.
 - (void)standardPromoSecondaryAction {
-  [self dissmissPromo];
+  RecordAction(
+      UserMetricsAction("IOS.DefaultBrowserPromo.TailoredFullscreen.Cancel"));
+  LogDefaultBrowserPromoHistogramForAction(
+      self.defaultBrowserPromoType, IOSDefaultBrowserPromoAction::kCancel);
+  LogUserInteractionWithTailoredFullscreenPromo();
 
-  // TODO: Record metrics.
+  [self dissmissPromo];
 }
 
 // The "Learn More" button was touched.
 - (void)standardPromoLearnMoreAction {
-  [self showLearnMoreView];
+  base::RecordAction(base::UserMetricsAction(
+      "IOS.DefaultBrowserPromo.TailoredFullscreen.MoreInfoTapped"));
+  LogUserInteractionWithTailoredFullscreenPromo();
 
-  // TODO: Record metrics.
+  [self showLearnMoreView];
 }
 
 // Gesture-based actions.
 - (void)standardPromoDismissSwipe {
-  // TODO: Record metrics.
+  RecordAction(
+      UserMetricsAction("IOS.DefaultBrowserPromo.TailoredFullscreen.Dismiss"));
+  LogDefaultBrowserPromoHistogramForAction(
+      self.defaultBrowserPromoType, IOSDefaultBrowserPromoAction::kDismiss);
+  LogUserInteractionWithTailoredFullscreenPromo();
 }
 
 #pragma mark - StandardPromoViewProvider
@@ -151,4 +171,18 @@ using l10n_util::GetNSString;
   self.promoViewController.secondaryActionString =
       GetNSString(IDS_IOS_DEFAULT_BROWSER_SECONDARY_BUTTON_TEXT);
 }
+
+// Records that a default browser promo has been shown.
+- (void)recordDefaultBrowserPromoShown {
+  // Record the current state before updating the local storage.
+  RecordPromoDisplayStatsToUMA();
+
+  RecordAction(
+      UserMetricsAction("IOS.DefaultBrowserPromo.TailoredFullscreen.Appear"));
+  base::UmaHistogramEnumeration(
+      "IOS.DefaultBrowserPromo.Shown",
+      DefaultPromoTypeForUMA(self.defaultBrowserPromoType));
+  LogFullscreenDefaultBrowserPromoDisplayed();
+}
+
 @end
---
+++ ios/chrome/browser/ui/default_promo/base_default_browser_promo_view_provider_unittest.mm
@@ -0,0 +1,147 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/default_promo/base_default_browser_promo_view_provider.h"
+
+#import "base/test/metrics/histogram_tester.h"
+#import "base/test/metrics/user_action_tester.h"
+#import "ios/chrome/browser/default_browser/model/utils.h"
+#import "ios/chrome/browser/default_browser/model/utils_test_support.h"
+#import "ios/chrome/browser/ui/default_promo/all_tabs_default_browser_promo_view_provider.h"
+#import "testing/gtest_mac.h"
+#import "testing/platform_test.h"
+
+#pragma mark - Fixture.
+
+// Fixture to test BaseDefaultBrowserPromoViewProvider.
+class BaseDefaultBrowserPromoViewProviderTest : public PlatformTest {
+ protected:
+  void SetUp() override {
+    PlatformTest::SetUp();
+
+    all_tabs_view_provider_ =
+        [[AllTabsDefaultBrowserPromoViewProvider alloc] init];
+
+    ClearDefaultBrowserPromoData();
+  }
+
+  AllTabsDefaultBrowserPromoViewProvider* all_tabs_view_provider_;
+};
+
+#pragma mark - Tests.
+
+// Tests that all expected metrics and logs are recorded on promo display.
+TEST_F(BaseDefaultBrowserPromoViewProviderTest, TestRecordMetricsOnDisplay) {
+  base::UserActionTester user_action_tester;
+  base::HistogramTester histogram_tester;
+
+  EXPECT_EQ(0, DisplayedFullscreenPromoCount());
+
+  // Notify the view provider that promo was displayed.
+  [all_tabs_view_provider_ promoWasDisplayed];
+
+  // Check that all expected UMA histograms are recorded.
+  histogram_tester.ExpectTotalCount(
+      "IOS.DefaultBrowserPromo.DaysSinceLastPromoInteraction", 1);
+  histogram_tester.ExpectTotalCount(
+      "IOS.DefaultBrowserPromo.GenericPromoDisplayCount", 1);
+  histogram_tester.ExpectTotalCount(
+      "IOS.DefaultBrowserPromo.TailoredPromoDisplayCount", 1);
+  histogram_tester.ExpectTotalCount("IOS.DefaultBrowserPromo.Shown", 1);
+  histogram_tester.ExpectBucketCount("IOS.DefaultBrowserPromo.Shown", 3, 1);
+
+  EXPECT_EQ(1, user_action_tester.GetActionCount(
+                   "IOS.DefaultBrowserPromo.TailoredFullscreen.Appear"));
+
+  // Check that user defaults are updated.
+  EXPECT_EQ(1, DisplayedFullscreenPromoCount());
+}
+
+// Tests that all expected metrics and logs are recorded on primary action.
+TEST_F(BaseDefaultBrowserPromoViewProviderTest,
+       TestRecordMetricsOnPrimaryAction) {
+  base::UserActionTester user_action_tester;
+  base::HistogramTester histogram_tester;
+
+  EXPECT_FALSE(HasUserInteractedWithTailoredFullscreenPromoBefore());
+
+  // Notify the view provider that primary button was tapped.
+  [all_tabs_view_provider_ standardPromoPrimaryAction];
+
+  // Check that all expected UMA histograms are recorded.
+  histogram_tester.ExpectTotalCount(
+      "IOS.DefaultBrowserFullscreenTailoredPromoAllTabs", 1);
+  histogram_tester.ExpectBucketCount(
+      "IOS.DefaultBrowserFullscreenTailoredPromoAllTabs", 0, 1);
+
+  EXPECT_EQ(1, user_action_tester.GetActionCount(
+                   "IOS.DefaultBrowserPromo.TailoredFullscreen.Accepted"));
+
+  // Check that user defaults are updated.
+  EXPECT_TRUE(HasUserInteractedWithTailoredFullscreenPromoBefore());
+}
+
+// Tests that all expected metrics and logs are recorded on secondary action.
+TEST_F(BaseDefaultBrowserPromoViewProviderTest,
+       TestRecordMetricsOnSecondaryAction) {
+  base::UserActionTester user_action_tester;
+  base::HistogramTester histogram_tester;
+
+  EXPECT_FALSE(HasUserInteractedWithTailoredFullscreenPromoBefore());
+
+  // Notify the view provider that secondary button was tapped.
+  [all_tabs_view_provider_ standardPromoSecondaryAction];
+
+  // Check that all expected UMA histograms are recorded.
+  histogram_tester.ExpectTotalCount(
+      "IOS.DefaultBrowserFullscreenTailoredPromoAllTabs", 1);
+  histogram_tester.ExpectBucketCount(
+      "IOS.DefaultBrowserFullscreenTailoredPromoAllTabs", 1, 1);
+
+  EXPECT_EQ(1, user_action_tester.GetActionCount(
+                   "IOS.DefaultBrowserPromo.TailoredFullscreen.Cancel"));
+
+  // Check that user defaults are updated.
+  EXPECT_TRUE(HasUserInteractedWithTailoredFullscreenPromoBefore());
+}
+
+// Tests that all expected metrics and logs are recorded on learn more.
+TEST_F(BaseDefaultBrowserPromoViewProviderTest, TestRecordMetricsOnLearnMore) {
+  base::UserActionTester user_action_tester;
+
+  EXPECT_FALSE(HasUserInteractedWithTailoredFullscreenPromoBefore());
+
+  // Notify the view provider that learn more was tapped.
+  [all_tabs_view_provider_ standardPromoLearnMoreAction];
+
+  // Check that all expected UMA histograms are recorded.
+  EXPECT_EQ(1,
+            user_action_tester.GetActionCount(
+                "IOS.DefaultBrowserPromo.TailoredFullscreen.MoreInfoTapped"));
+
+  // Check that user defaults are updated.
+  EXPECT_TRUE(HasUserInteractedWithTailoredFullscreenPromoBefore());
+}
+
+// Tests that all expected metrics and logs are recorded on dismiss.
+TEST_F(BaseDefaultBrowserPromoViewProviderTest, TestRecordMetricsOnDismiss) {
+  base::UserActionTester user_action_tester;
+  base::HistogramTester histogram_tester;
+
+  EXPECT_FALSE(HasUserInteractedWithTailoredFullscreenPromoBefore());
+
+  // Notify the view provider that promo was dismissed.
+  [all_tabs_view_provider_ standardPromoDismissSwipe];
+
+  // Check that all expected UMA histograms are recorded.
+  histogram_tester.ExpectTotalCount(
+      "IOS.DefaultBrowserFullscreenTailoredPromoAllTabs", 1);
+  histogram_tester.ExpectBucketCount(
+      "IOS.DefaultBrowserFullscreenTailoredPromoAllTabs", 3, 1);
+  EXPECT_EQ(1, user_action_tester.GetActionCount(
+                   "IOS.DefaultBrowserPromo.TailoredFullscreen.Dismiss"));
+
+  // Check that user defaults are updated.
+  EXPECT_TRUE(HasUserInteractedWithTailoredFullscreenPromoBefore());
+}
