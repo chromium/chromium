a71defb31ecc6e4ee11c81bf0c620bf5d07cb1de
Marco Paniconi <marpan@google.com>
2024-04-04 21:27:07 +0000

    Roll src/third_party/libvpx/source/libvpx/ d790001fd..6445da1b4 (14 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/d790001fd56e..6445da1b40da
    
    $ git log d790001fd..6445da1b4 --date=short --no-merges --format='%ad %ae %s'
    2024-04-02 wtc Fix GCC -Wmissing-braces warnings
    2024-04-03 casey.smalley Add missing configuration includes
    2024-01-23 casey.smalley Unit test config changes for Chromium
    2024-04-02 wtc Compare ctx->pending_cx_data with NULL
    2024-04-03 hiroh vp9 rc: Fix GetSegmentationData() crash in aq_mode=0
    2024-04-02 wtc Set priv->cx_data_sz to 0 if cx_data alloc fails
    2024-03-28 wtc encoder_encode: Assert pending_cx_data_sz is valid
    2024-03-28 wtc Dont use VPX_CODEC_CORRUPT_FRAME in set_frame_size
    2024-03-19 gerdazsejke.more Add SVE2 implementation of vpx_highbd_convolve8_avg
    2024-03-17 gerdazsejke.more Merge horiz. and vert. passes in HBD SVE2 2D 4tap convolution
    2024-03-29 jingning Update yv12_mb initialization
    2024-03-28 jianj Add invalid value to gop decision enums
    2024-03-28 wtc Assert the return value of read_tx_mode() is < 5
    2024-03-29 jingning Initialize yv12_mb array
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=jzern@google.com
    
    Bug: b:308446709, b:259487065
    Change-Id: I4ea5b4ce56d8ea628db8134a7e584f091ff48321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426046
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: James Zern <jzern@google.com>
    Reviewed-by: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282735}

--- DEPS
+++ DEPS
@@ -1526,7 +1526,7 @@ deps = {
   },
 
   'src/third_party/libvpx/source/libvpx':
-    Var('chromium_git') + '/webm/libvpx.git' + '@' +  'd790001fd56edbc39482d596868ed83ee7355368',
+    Var('chromium_git') + '/webm/libvpx.git' + '@' +  '6445da1b40da7967ccaee23d1ac46d5c3981b89c',
 
   'src/third_party/libwebm/source':
     Var('chromium_git') + '/webm/libwebm.git' + '@' + 'e4fbea0c9751ae8aa86629b197a28d8276a2b0da',
--- third_party/libvpx/source/config/linux/arm64-highbd/vpx_dsp_rtcd.h
+++ third_party/libvpx/source/config/linux/arm64-highbd/vpx_dsp_rtcd.h
@@ -3793,7 +3793,30 @@ void vpx_highbd_convolve8_avg_neon(const uint16_t* src,
                                    int w,
                                    int h,
                                    int bd);
-#define vpx_highbd_convolve8_avg vpx_highbd_convolve8_avg_neon
+void vpx_highbd_convolve8_avg_sve2(const uint16_t* src,
+                                   ptrdiff_t src_stride,
+                                   uint16_t* dst,
+                                   ptrdiff_t dst_stride,
+                                   const InterpKernel* filter,
+                                   int x0_q4,
+                                   int x_step_q4,
+                                   int y0_q4,
+                                   int y_step_q4,
+                                   int w,
+                                   int h,
+                                   int bd);
+RTCD_EXTERN void (*vpx_highbd_convolve8_avg)(const uint16_t* src,
+                                             ptrdiff_t src_stride,
+                                             uint16_t* dst,
+                                             ptrdiff_t dst_stride,
+                                             const InterpKernel* filter,
+                                             int x0_q4,
+                                             int x_step_q4,
+                                             int y0_q4,
+                                             int y_step_q4,
+                                             int w,
+                                             int h,
+                                             int bd);
 
 void vpx_highbd_convolve8_avg_horiz_c(const uint16_t* src,
                                       ptrdiff_t src_stride,
@@ -8651,6 +8674,10 @@ static void setup_rtcd_internal(void) {
   vpx_highbd_convolve8 = vpx_highbd_convolve8_neon;
   if (flags & HAS_SVE2) {
     vpx_highbd_convolve8 = vpx_highbd_convolve8_sve2;
+  }
+  vpx_highbd_convolve8_avg = vpx_highbd_convolve8_avg_neon;
+  if (flags & HAS_SVE2) {
+    vpx_highbd_convolve8_avg = vpx_highbd_convolve8_avg_sve2;
   }
   vpx_highbd_convolve8_avg_horiz = vpx_highbd_convolve8_avg_horiz_neon;
   if (flags & HAS_SVE) {
