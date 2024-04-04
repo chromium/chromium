d7cdc8102d1effe596e04598c1eee7299cee94a9
Siyu An <siyua@chromium.org>
2024-04-04 22:57:31 +0000

    [VCN] Remove manual_fill full_card_requester
    Remove full_card_requester class from the manual_fill flow and use
    CreditCardAccessManager instead. This is needed as we introduce virtual
    cards to iOS. full_card_requester supports only CVC auth for masked
    server cards while CreditCardAccessManager supports both masked server
    card and virtual card, and can decide the auth methods used internally.
    
    full_card_requester is no longer needed hence deleted.
    
    This CL is a no-op change for now (before virtual cards are supported),
    but one implicit change is that we fix the auth reason here[1]. Should
    be Autofill not PaymentsRequest. (This auth reason is actually not
    needed either, but it will be cleaned up by following clean-up CLs).
    
    Screen recordings to verify this is no-op:
    success: https://drive.google.com/file/d/1RULfzN5oawENafC0ojXPpEFCqlNJowc8/view?usp=sharing
    failure: https://drive.google.com/file/d/1jbT28k353N06Z8CaLHfkSexDxEjkrDg9/view?usp=sharing
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/autofill/manual_fill/full_card_requester.mm;l=33;drc=8ea9893d6cdfe28d02336afed0d5c859b82c1809
    
    Bug: 324612840
    Change-Id: I0cdf8aa3038ea513cee99ac5e6702b442b9b3170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5422039
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282807}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_full_card_requester.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_full_card_requester.mm
@@ -8,11 +8,11 @@
 
 #import "components/autofill/core/browser/browser_autofill_manager.h"
 #import "components/autofill/core/browser/data_model/credit_card.h"
+#import "components/autofill/core/browser/payments/credit_card_access_manager.h"
 #import "components/autofill/ios/browser/autofill_driver_ios.h"
 #import "components/autofill/ios/browser/autofill_java_script_feature.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/ui/autofill/manual_fill/full_card_request_result_delegate_bridge.h"
-#import "ios/chrome/browser/ui/autofill/manual_fill/full_card_requester.h"
 #import "ios/web/public/js_messaging/web_frame.h"
 #import "ios/web/public/js_messaging/web_frames_manager.h"
 #import "ios/web/public/web_state.h"
@@ -33,10 +33,9 @@ class CreditCard;
 @end
 
 @implementation ManualFillFullCardRequester {
-  std::unique_ptr<FullCardRequester> _fullCardRequester;
   // Obj-C delegate to receive the success or failure result, when
   // asking credit card unlocking.
-  std::unique_ptr<FullCardRequestResultDelegateBridge> _cardAssistant;
+  __weak id<FullCardRequestResultDelegateObserving> _delegate;
 }
 
 - (instancetype)initWithBrowserState:(ChromeBrowserState*)browserState
@@ -47,8 +46,7 @@ class CreditCard;
   if (self) {
     _browserState = browserState;
     _webStateList = webStateList;
-    _cardAssistant =
-        std::make_unique<FullCardRequestResultDelegateBridge>(delegate);
+    _delegate = delegate;
   }
   return self;
 }
@@ -67,12 +65,32 @@ class CreditCard;
   autofill::BrowserAutofillManager& autofillManager =
       autofill::AutofillDriverIOS::FromWebStateAndWebFrame(webState, mainFrame)
           ->GetAutofillManager();
-  _fullCardRequester =
-      std::make_unique<FullCardRequester>(viewController, self.browserState);
-  _fullCardRequester->GetFullCard(card, &autofillManager,
-                                  _cardAssistant->GetWeakPtr());
+
+  autofill::CreditCardAccessManager& creditCardAccessManager =
+      autofillManager.GetCreditCardAccessManager();
+  __weak __typeof(self) weakSelf = self;
+  creditCardAccessManager.FetchCreditCard(
+      &card, base::BindOnce(^(autofill::CreditCardFetchResult result,
+                              const autofill::CreditCard* fetchedCard) {
+        [weakSelf onCreditCardFetched:result fetchedCard:fetchedCard];
+      }));
+
   // TODO(crbug.com/845472): closing CVC requester doesn't restore icon bar
   // above keyboard.
+}
+
+#pragma mark - Private methods
+
+// Callback invoked when the card retrieval is finished. It notifies the
+// delegate the result of the card retrieval process and provides the card if
+// the process succeeded.
+- (void)onCreditCardFetched:(autofill::CreditCardFetchResult)result
+                fetchedCard:(const autofill::CreditCard*)fetchedCard {
+  if (result == autofill::CreditCardFetchResult::kSuccess && fetchedCard) {
+    [_delegate onFullCardRequestSucceeded:*fetchedCard];
+  } else {
+    [_delegate onFullCardRequestFailed];
+  }
 }
 
 @end
