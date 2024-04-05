e58c73bd1dbbf72a1506681881631602d8fa027d
Yuki Shiino <yukishiino@chromium.org>
2024-04-05 00:49:30 +0000

    Revert "[webauthn] Polish Touch ID UI"
    This reverts commit 3e9b1bb614ed93e729aa553af32454fc81944e4f.
    
    Reason for revert: Suspicious about causing test failures of
    GPMPasskeysAuthenticatorDialogTest.InvokeUi_touchid
    at
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/26633/overview
    
    Original change's description:
    > [webauthn] Polish Touch ID UI
    >
    > Polish the Touch ID GPM UI:
    > * Move the UI to a sheet.
    > * Wait for the animation to finish before proceeding.
    > * Add text displaying the account details (for get assertion & make
    >   credential)
    > * Catch Touch ID being locked and defer to a password if that happens.
    > * Remove old bubble code.
    >
    > Bug: 40274370
    > Change-Id: I73cb9470b1a0a4d2531e11cfce96ab99632f1921
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403928
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1282777}
    
    Bug: 40274370
    Change-Id: Ibb80e45d6a58e7cd3b3d283719eb9bea0f71f967
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5427371
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1282874}

--- chrome/browser/webauthn/authenticator_request_dialog_model.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -406,6 +406,7 @@ StepUIType step_ui_type(AuthenticatorRequestDialogModel::Step step) {
       return StepUIType::WINDOW;
 
     case AuthenticatorRequestDialogModel::Step::kGPMCreatePasskey:
+    case AuthenticatorRequestDialogModel::Step::kGPMTouchID:
     case AuthenticatorRequestDialogModel::Step::kGPMOnboarding:
     case AuthenticatorRequestDialogModel::Step::kGPMPasskeySaved:
       return StepUIType::BUBBLE;
@@ -1475,7 +1476,6 @@ void AuthenticatorRequestDialogController::OnAccountPreselected(
   DCHECK(account_preselected_callback_);
   account_preselected_callback_.Run(*cred);
   model_->creds.clear();
-  model_->preselected_cred = *cred;
 
   if (source != device::AuthenticatorType::kPhone &&
       source != device::AuthenticatorType::kEnclave) {
@@ -1655,9 +1655,6 @@ void AuthenticatorRequestDialogController::set_account_state(
       // The account was recovered but now we need to prompt for an existing
       // GPM PIN.
       PromptForGPMPin();
-    } else if (state == AccountState::kReadyWithBiometrics) {
-      // The account was recovered, prompt for biometrics.
-      SetCurrentStep(Step::kGPMTouchID);
     }
   }
 }
