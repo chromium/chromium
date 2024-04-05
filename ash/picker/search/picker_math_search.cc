956586e274749ca72892eff1e9ca5c5d8052a798
Jing Wang <jiwan@chromium.org>
2024-04-05 02:43:48 +0000

    picker: call fend api for math results
    Bug: b:329174747
    Change-Id: Ib3c2d0e1a4bad8ae4810d47909f98294503e0774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5422828
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282922}

--- ash/picker/search/picker_math_search.cc
+++ ash/picker/search/picker_math_search.cc
@@ -8,12 +8,16 @@
 #include <string_view>
 
 #include "ash/public/cpp/picker/picker_search_result.h"
+#include "base/strings/utf_string_conversions.h"
+#include "third_party/rust/fend_core/v1/wrapper/fend_core.h"
 
 namespace ash {
 
 std::optional<PickerSearchResult> PickerMathSearch(std::u16string_view query) {
-  if (query == u"1 + 1") {
-    return PickerSearchResult::Text(u"2");
+  std::optional<std::string> result =
+      fend_core::evaluate(base::UTF16ToUTF8(query));
+  if (result.has_value()) {
+    return PickerSearchResult::Text(base::UTF8ToUTF16(*result));
   }
   return std::nullopt;
 }
