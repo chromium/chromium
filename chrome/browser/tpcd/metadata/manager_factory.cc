b7d58530e119f6114f7bea1741e5691d56cc7ddd
Jonathan Njeunje <njeunje@chromium.org>
2024-04-08 13:46:12 +0000

    Impl syncing of the content setting to the network process
    This CL also creates the Manager factory logic.
    
    Details: https://docs.google.com/document/d/1l5C0fL-gSf1sSN7WhOR9A1jqXMgYvjv9j6wxAaoCY9o/edit?usp=sharing
    
    Follow up:
    * Hook the manager to the cookie setting objects.
    
    bug: b/315186237
    Change-Id: I046783702def102a6bb1837b93ee024496a961b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425923
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Commit-Queue: Jonathan Njeunje <njeunje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283855}

---
+++ chrome/browser/tpcd/metadata/manager_factory.cc
@@ -0,0 +1,35 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/tpcd/metadata/manager_factory.h"
+
+#include "base/functional/bind.h"
+#include "components/content_settings/core/common/content_settings.h"
+#include "components/tpcd/metadata/manager.h"
+#include "components/tpcd/metadata/parser.h"
+#include "content/public/browser/network_service_instance.h"
+#include "net/base/features.h"
+#include "services/network/public/mojom/network_service.mojom.h"
+
+namespace tpcd::metadata {
+
+// static
+Manager* ManagerFactory::GetForProfile(Profile* profile) {
+  if (!base::FeatureList::IsEnabled(net::features::kTpcdMetadataGrants)) {
+    return nullptr;
+  }
+
+  if (!(profile->IsRegularProfile() || profile->IsGuestSession())) {
+    return nullptr;
+  }
+
+  auto sync_network_service = [](const ContentSettingsForOneType& grants) {
+    content::GetNetworkService()->SetTpcdMetadataGrants(grants);
+  };
+
+  return tpcd::metadata::Manager::GetInstance(
+      Parser::GetInstance(), base::BindRepeating(sync_network_service));
+}
+
+}  // namespace tpcd::metadata
