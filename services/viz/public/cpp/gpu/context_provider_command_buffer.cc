4236d43fa8d272a63c5c210e7831329150f9cf19
Saifuddin Hitawala <hitawala@chromium.org>
2024-04-08 19:12:30 +0000

    [viz] Check usages of RasterImplGLES on Android
    The last few usages of RasterImplementationGLES are removed from
    Android with switching to use RasterInterface in VideoResourceUpdater.
    Thus, Android should never need a RasterImplementationGLES through
    ContextProviderCommandBuffer. This change adds a DUMP_WILL_BE_CHECK
    which helps validate it.
    
    Bug: None
    Change-Id: Ib94b6cd6b63f5f69b70b110187e3cd1cdfd0fcb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426160
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284022}

--- services/viz/public/cpp/gpu/context_provider_command_buffer.cc
+++ services/viz/public/cpp/gpu/context_provider_command_buffer.cc
@@ -376,15 +376,25 @@ gpu::raster::RasterInterface* ContextProviderCommandBuffer::RasterInterface() {
   DCHECK_EQ(bind_result_, gpu::ContextResult::kSuccess);
   CheckValidSequenceOrLockAcquired();
 
-  if (raster_interface_)
+  if (raster_interface_) {
     return raster_interface_.get();
+  }
 
   if (!attributes_.enable_raster_interface) {
     return nullptr;
   }
 
-  if (!gles2_impl_.get())
+#if BUILDFLAG(IS_ANDROID)
+  // The last few usages of RasterImplementationGLES are removed from Android
+  // with switching to use RasterInterface in VideoResourceUpdater. Thus,
+  // Android should never need a RasterImplementationGLES through
+  // ContextProviderCommandBuffer. This DUMP_WILL_BE_CHECK helps validate it.
+  DUMP_WILL_BE_CHECK(false);
+#endif
+
+  if (!gles2_impl_.get()) {
     return nullptr;
+  }
 
   raster_interface_ = std::make_unique<gpu::raster::RasterImplementationGLES>(
       gles2_impl_.get(), gles2_impl_.get(), ContextCapabilities());
