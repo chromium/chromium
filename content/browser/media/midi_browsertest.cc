cd399baf8d63d17667e67ee0a4513129c14e66d2
Sina Firoozabadi <sinafirooz@google.com>
2024-04-09 22:55:07 +0000

    Re-enable MIDI browser tests on ChromeOS
    Change-Id: I30a5fe52995b8f1bb5d8ad01cb3ce58acec2f1d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4771258
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Commit-Queue: Sina Firoozabadi <sinafirooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284758}

--- content/browser/media/midi_browsertest.cc
+++ content/browser/media/midi_browsertest.cc
@@ -38,7 +38,7 @@ class MidiBrowserTest : public ContentBrowserTest {
     EXPECT_TRUE(NavigateToURL(shell(), https_test_server_->GetURL(path)));
 
     const std::u16string result = watcher.WaitAndGetTitle();
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_LINUX)
     // Try does not allow accessing /dev/snd/seq, and it results in a platform
     // specific initialization error. See http://crbug.com/371230.
     // Also, Chromecast does not support the feature and results in
@@ -88,7 +88,7 @@ class MidiBrowserTestBlockMidiByDefault : public ContentBrowserTest {
     EXPECT_TRUE(NavigateToURL(shell(), https_test_server_->GetURL(path)));
 
     const std::u16string result = watcher.WaitAndGetTitle();
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_LINUX)
     // Try does not allow accessing /dev/snd/seq, and it results in a platform
     // specific initialization error. See http://crbug.com/371230.
     // Also, Chromecast does not support the feature and results in
