1ecf931d6e070003ed0e23b4867b903476f92181
Simon Ziegltrum <ziegltrum@google.com>
2024-04-10 10:58:43 +0000

    [OOBE] Enable AiIntro screen by default
    Bug: b/330136560 b/330135232
    Change-Id: Ib46ce4ae11e0b97cd871fc87276a1daaa7c09de8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439552
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285031}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2118,7 +2118,7 @@ BASE_FEATURE(kOobeLazyLoading,
 // Enables OOBE ai intro feature.
 BASE_FEATURE(kOobeAiIntro,
              "OobeAiIntro",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFeatureManagementOobeAiIntro,
              "FeatureManagementOobeAiIntro",
