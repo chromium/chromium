e450c331304cf07a279d99659974114a86bd90e9
Leszek Swirski <leszeks@google.com>
2024-04-10 14:07:22 +0000

    [v8] Add field trial for V8SynchronousSparkplug
    Bug: 333670348
    Change-Id: Ifabb0437347fabed857c0ee0307b0f3cbd0b3cfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5443013
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285098}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -817,6 +817,7 @@ public final class ProductionSupportedFlagList {
         Flag.baseFeature("V8SingleThreadedGCInBackground"),
         Flag.baseFeature("V8MemoryReducer"),
         Flag.baseFeature("V8MinorMS"),
+        Flag.baseFeature("V8SynchronousSparkplug"),
         Flag.baseFeature("WebAssemblyMoreAggressiveCodeCaching"),
         Flag.baseFeature("WebAssemblyTurboshaft"),
         Flag.baseFeature("WebAssemblyTurboshaftInstructionSelection"),
