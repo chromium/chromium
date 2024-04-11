008ff684ea780934f4044a390b4e0b8193faefc4
Akihiro Ota <akihiroota@chromium.org>
2024-04-11 15:50:29 +0000

    FaceGaze: Add class to capture mouse and keyboard events
    This change adds the MockEventHandler class to FaceGaze C++ browser
    tests. This allows the tests to verify the number and types of events
    that are being triggered by FaceGaze.
    
    Bug: b/309121742
    Change-Id: I7dd8049d3634abc3cd8c37734d4421a84788bdc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445007
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Tzarial Kuznia <zork@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285864}

--- chrome/browser/ash/accessibility/facegaze_browsertest.cc
+++ chrome/browser/ash/accessibility/facegaze_browsertest.cc
@@ -13,7 +13,10 @@
 #include "ui/accessibility/accessibility_features.h"
 #include "ui/display/screen.h"
 #include "ui/display/test/display_manager_test_api.h"
+#include "ui/events/event.h"
+#include "ui/events/event_handler.h"
 #include "ui/events/test/event_generator.h"
+#include "ui/events/types/event_type.h"
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/point_f.h"
 
@@ -24,7 +27,48 @@ using MockFaceLandmarkerResult = FaceGazeTestUtils::MockFaceLandmarkerResult;
 
 namespace {
 
+const int kMouseDeviceId = 1;
 const char* kDefaultDisplaySize = "1200x800";
+
+aura::Window* GetRootWindow() {
+  auto* root_window = Shell::GetRootWindowForNewWindows();
+  if (!root_window) {
+    root_window = Shell::GetPrimaryRootWindow();
+  }
+
+  return root_window;
+}
+
+// A class that records mouse and key events.
+class MockEventHandler : public ui::EventHandler {
+ public:
+  MockEventHandler() = default;
+  ~MockEventHandler() override = default;
+  MockEventHandler(const MockEventHandler&) = delete;
+  MockEventHandler& operator=(const MockEventHandler&) = delete;
+
+  void OnKeyEvent(ui::KeyEvent* event) override {
+    key_events_.push_back(*event);
+  }
+
+  void OnMouseEvent(ui::MouseEvent* event) override {
+    mouse_events_.push_back(*event);
+  }
+
+  void ClearEvents() {
+    key_events_.clear();
+    mouse_events_.clear();
+  }
+
+  const std::vector<ui::KeyEvent>& key_events() const { return key_events_; }
+  const std::vector<ui::MouseEvent>& mouse_events() const {
+    return mouse_events_;
+  }
+
+ private:
+  std::vector<ui::KeyEvent> key_events_;
+  std::vector<ui::MouseEvent> mouse_events_;
+};
 
 // A class that helps initialize FaceGaze with a configuration.
 class Config {
@@ -125,6 +169,7 @@ class FaceGazeIntegrationTest : public AccessibilityFeatureBrowserTest {
 
   void SetUpOnMainThread() override {
     InProcessBrowserTest::SetUpOnMainThread();
+    GetRootWindow()->AddPreTargetHandler(&event_handler_);
     event_generator_ = std::make_unique<ui::test::EventGenerator>(
         Shell::Get()->GetPrimaryRootWindow());
     display::test::DisplayManagerTestApi(Shell::Get()->display_manager())
@@ -133,6 +178,11 @@ class FaceGazeIntegrationTest : public AccessibilityFeatureBrowserTest {
     // Initialize FaceGaze.
     utils_->EnableFaceGaze();
     utils_->CreateFaceLandmarker();
+  }
+
+  void TearDownOnMainThread() override {
+    GetRootWindow()->RemovePreTargetHandler(&event_handler_);
+    InProcessBrowserTest::TearDownOnMainThread();
   }
 
   void ConfigureFaceGaze(const Config& config) {
@@ -154,7 +204,7 @@ class FaceGazeIntegrationTest : public AccessibilityFeatureBrowserTest {
     // By default the cursor is placed at the center of the screen. To
     // initialize FaceGaze, move the cursor somewhere, then move it to the
     // location specified by the config.
-    event_generator_->set_mouse_source_device_id(1);
+    event_generator_->set_mouse_source_device_id(kMouseDeviceId);
     MoveMouseTo(gfx::Point(0, 0));
     AssertCursorAt(gfx::Point(0, 0));
     MoveMouseTo(config.cursor_location());
@@ -179,22 +229,36 @@ class FaceGazeIntegrationTest : public AccessibilityFeatureBrowserTest {
     ASSERT_EQ(location, display::Screen::GetScreen()->GetCursorScreenPoint());
   }
 
+  MockEventHandler& event_handler() { return event_handler_; }
   FaceGazeTestUtils* utils() { return utils_.get(); }
 
  private:
   std::unique_ptr<FaceGazeTestUtils> utils_;
   std::unique_ptr<ui::test::EventGenerator> event_generator_;
+  MockEventHandler event_handler_;
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, UpdateCursorLocation) {
   ConfigureFaceGaze(Config().AsDefault());
+  event_handler().ClearEvents();
 
-  // Move cursor using forehead.
   utils()->ProcessFaceLandmarkerResult(
       MockFaceLandmarkerResult().WithNormalizedForeheadLocation(0.11, 0.21));
   utils()->TriggerMouseControllerInterval();
   AssertCursorAt(gfx::Point(360, 560));
+
+  // We expect two mouse move events to be received because the FaceGaze
+  // extension calls two APIs to update the cursor position.
+  const std::vector<ui::MouseEvent> mouse_events =
+      event_handler().mouse_events();
+  ASSERT_EQ(2u, mouse_events.size());
+  ASSERT_EQ(ui::ET_MOUSE_MOVED, mouse_events[0].type());
+  ASSERT_EQ(gfx::Point(360, 560), mouse_events[0].root_location());
+  ASSERT_TRUE(mouse_events[0].IsSynthesized());
+  ASSERT_EQ(ui::ET_MOUSE_MOVED, mouse_events[1].type());
+  ASSERT_EQ(gfx::Point(360, 560), mouse_events[1].root_location());
+  ASSERT_TRUE(mouse_events[1].IsSynthesized());
 }
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, ResetCursor) {
@@ -211,10 +275,21 @@ IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, ResetCursor) {
   utils()->TriggerMouseControllerInterval();
   AssertCursorAt(gfx::Point(360, 560));
 
+  event_handler().ClearEvents();
+
   // Reset the cursor to the center of the screen using a gesture.
   utils()->ProcessFaceLandmarkerResult(
       MockFaceLandmarkerResult().WithGesture("jawOpen", 90));
   AssertCursorAt(gfx::Point(600, 400));
+
+  // We expect one mouse move event to be received because the FaceGaze
+  // extension only calls one API to reset the cursor position.
+  const std::vector<ui::MouseEvent> mouse_events =
+      event_handler().mouse_events();
+  ASSERT_EQ(1u, mouse_events.size());
+  ASSERT_EQ(ui::ET_MOUSE_MOVED, mouse_events[0].type());
+  ASSERT_EQ(gfx::Point(600, 400), mouse_events[0].root_location());
+  ASSERT_TRUE(mouse_events[0].IsSynthesized());
 }
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest,
@@ -235,9 +310,11 @@ IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest,
   // Attempt to reset the cursor to the center of the screen using a gesture.
   // This gesture will be ignored because the gesture doesn't have high enough
   // confidence.
+  event_handler().ClearEvents();
   utils()->ProcessFaceLandmarkerResult(
       MockFaceLandmarkerResult().WithGesture("jawOpen", 90));
   AssertCursorAt(gfx::Point(360, 560));
+  ASSERT_EQ(0u, event_handler().mouse_events().size());
 }
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest,
