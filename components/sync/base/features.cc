8d64238eb6a65c0a5e677dd88740ac03106ba0c0
Ankush Singh <ankushkush@google.com>
2024-04-11 08:27:57 +0000

    [android] Enable SyncShowIdentityErrorsForSignedInUsers by default
    The experiment is approved for launch.
    
    Bug: 40944114
    Change-Id: I96de5ec5b9e691f89d8656074b185088f9904152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5442878
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285668}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -211,7 +211,7 @@ BASE_FEATURE(kSyncSchedulerUseWallClockTimer,
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kSyncShowIdentityErrorsForSignedInUsers,
              "SyncShowIdentityErrorsForSignedInUsers",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 BASE_FEATURE(kSyncRememberCustomPassphraseAfterSignout,
