f763f41dd3f035e0b1b34416e428c392c3e1ae73
Xi Han <hanxi@google.com>
2024-04-12 14:01:55 +0000

    [MagicStack] Update metrics in module ranker.
    This is a follow up CL of https://crrev.com/c/5443638 which renames the
    TopImpression metric.
    
    Bug: 41485537
    Change-Id: I30e8e960cc07ee5237f5cc1d60f298fa8275e2ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5443777
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286455}

--- components/segmentation_platform/embedder/default_model/android_home_module_ranker.cc
+++ components/segmentation_platform/embedder/default_model/android_home_module_ranker.cc
@@ -21,7 +21,7 @@ using proto::SegmentId;
 // Default parameters for AndroidHomeModuleRanker model.
 constexpr SegmentId kSegmentId =
     SegmentId::OPTIMIZATION_TARGET_SEGMENTATION_ANDROID_HOME_MODULE_RANKER;
-constexpr int64_t kModelVersion = 2;
+constexpr int64_t kModelVersion = 3;
 // Store 28 buckets of input data (28 days).
 constexpr int64_t kSignalStorageLength = 28;
 // Wait until we have 0 days of data.
@@ -35,7 +35,7 @@ constexpr std::array<const char*, 3> kAndroidHomeModuleLabels = {
 // InputFeatures.
 
 // Enum values for the MagicStack.Clank.NewTabPage|StartSurface.Module.Click and
-// MagicStack.Clank.NewTabPage|StartSurface.Module.TopImpression histograms.
+// MagicStack.Clank.NewTabPage|StartSurface.Module.TopImpressionV2 histograms.
 constexpr std::array<int32_t, 1> kEnumValueForSingleTab{/*SingleTab=*/0};
 
 constexpr std::array<int32_t, 1> kEnumValueForPriceChange{/*PriceChange=*/1};
@@ -60,13 +60,13 @@ constexpr std::array<MetadataWriter::UMAFeature, 24> kUMAFeatures = {
         kEnumValueForSingleTab.size()),
     // 2
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.NewTabPage.Module.TopImpression",
+        "MagicStack.Clank.NewTabPage.Module.TopImpressionV2",
         7,
         kEnumValueForSingleTab.data(),
         kEnumValueForSingleTab.size()),
     // 3
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.StartSurface.Module.TopImpression",
+        "MagicStack.Clank.StartSurface.Module.TopImpressionV2",
         7,
         kEnumValueForSingleTab.data(),
         kEnumValueForSingleTab.size()),
@@ -84,13 +84,13 @@ constexpr std::array<MetadataWriter::UMAFeature, 24> kUMAFeatures = {
         kEnumValueForSingleTab.size()),
     // 6
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.NewTabPage.Module.TopImpression",
+        "MagicStack.Clank.NewTabPage.Module.TopImpressionV2",
         28,
         kEnumValueForSingleTab.data(),
         kEnumValueForSingleTab.size()),
     // 7
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.StartSurface.Module.TopImpression",
+        "MagicStack.Clank.StartSurface.Module.TopImpressionV2",
         28,
         kEnumValueForSingleTab.data(),
         kEnumValueForSingleTab.size()),
@@ -109,13 +109,13 @@ constexpr std::array<MetadataWriter::UMAFeature, 24> kUMAFeatures = {
         kEnumValueForPriceChange.size()),
     // 10
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.NewTabPage.Module.TopImpression",
+        "MagicStack.Clank.NewTabPage.Module.TopImpressionV2",
         7,
         kEnumValueForPriceChange.data(),
         kEnumValueForPriceChange.size()),
     // 11
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.StartSurface.Module.TopImpression",
+        "MagicStack.Clank.StartSurface.Module.TopImpressionV2",
         7,
         kEnumValueForPriceChange.data(),
         kEnumValueForPriceChange.size()),
@@ -133,13 +133,13 @@ constexpr std::array<MetadataWriter::UMAFeature, 24> kUMAFeatures = {
         kEnumValueForPriceChange.size()),
     // 14
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.NewTabPage.Module.TopImpression",
+        "MagicStack.Clank.NewTabPage.Module.TopImpressionV2",
         28,
         kEnumValueForPriceChange.data(),
         kEnumValueForPriceChange.size()),
     // 15
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.StartSurface.Module.TopImpression",
+        "MagicStack.Clank.StartSurface.Module.TopImpressionV2",
         28,
         kEnumValueForPriceChange.data(),
         kEnumValueForPriceChange.size()),
@@ -158,13 +158,13 @@ constexpr std::array<MetadataWriter::UMAFeature, 24> kUMAFeatures = {
         kEnumValueForTabResumption.size()),
     // 18
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.NewTabPage.Module.TopImpression",
+        "MagicStack.Clank.NewTabPage.Module.TopImpressionV2",
         7,
         kEnumValueForTabResumption.data(),
         kEnumValueForTabResumption.size()),
     // 19
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.StartSurface.Module.TopImpression",
+        "MagicStack.Clank.StartSurface.Module.TopImpressionV2",
         7,
         kEnumValueForTabResumption.data(),
         kEnumValueForTabResumption.size()),
@@ -182,13 +182,13 @@ constexpr std::array<MetadataWriter::UMAFeature, 24> kUMAFeatures = {
         kEnumValueForTabResumption.size()),
     // 22
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.NewTabPage.Module.TopImpression",
+        "MagicStack.Clank.NewTabPage.Module.TopImpressionV2",
         28,
         kEnumValueForTabResumption.data(),
         kEnumValueForTabResumption.size()),
     // 23
     MetadataWriter::UMAFeature::FromEnumHistogram(
-        "MagicStack.Clank.StartSurface.Module.TopImpression",
+        "MagicStack.Clank.StartSurface.Module.TopImpressionV2",
         28,
         kEnumValueForTabResumption.data(),
         kEnumValueForTabResumption.size()),
