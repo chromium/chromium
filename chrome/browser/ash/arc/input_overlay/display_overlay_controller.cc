27c8ee44e9ab2607e138dd80677c97a65ea7129c
Cici Ruan <cuicuiruan@google.com>
2024-04-14 03:21:59 +0000

    arc/gio: Remove the ax tree updates
    This CL removes the ax tree updates since it still can navigate by tab
    key for now. The further fix for CVox navigation between widgets will
    be uploaded later.
    
    Bug: b/334219554
    Test: None
    Change-Id: I232dc2763d4028336ac9eb11d994d0d1acbe098b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450329
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287031}

--- chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
+++ chrome/browser/ash/arc/input_overlay/display_overlay_controller.cc
@@ -58,7 +58,6 @@ namespace arc::input_overlay {
 namespace {
 
 using ash::game_dashboard_utils::GetNextWidgetToFocus;
-using ash::game_dashboard_utils::UpdateAccessibilityTree;
 
 // UI specs.
 constexpr int kMenuEntrySideMargin = 24;
@@ -152,7 +151,6 @@ class DisplayOverlayController::FocusCycler {
     if (auto it = std::find(widget_list_.begin(), widget_list_.end(), widget);
         it == widget_list_.end()) {
       widget_list_.emplace_back(widget);
-      UpdateAccessibilityTree(widget_list_);
     }
   }
 
@@ -160,7 +158,6 @@ class DisplayOverlayController::FocusCycler {
     if (auto it = std::find(widget_list_.begin(), widget_list_.end(), widget);
         it != widget_list_.end()) {
       widget_list_.erase(it);
-      UpdateAccessibilityTree(widget_list_);
     }
   }
 
