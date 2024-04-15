de33a75fbc552b7fcde416578154557a9456b7ce
Simon Ziegltrum <ziegltrum@google.com>
2024-04-15 16:05:00 +0000

    [OOBE] Fix cbx device name in string for AiIntro/Tuna
    New: https://screenshot.googleplex.com/A3ytsjJDco9itkb
    New: https://screenshot.googleplex.com/BihRGt7GeDNjTrn
    
    Bug: b/334148488 b/330135232
    Change-Id: I309b591d612aee1659aa0956ea6da4ef757a5bfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445638
    Auto-Submit: Simon Ziegltrum <ziegltrum@google.com>
    Commit-Queue: Simon Ziegltrum <ziegltrum@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287387}

--- chrome/browser/ui/webui/ash/login/ai_intro_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/ai_intro_screen_handler.cc
@@ -4,9 +4,14 @@
 
 #include "chrome/browser/ui/webui/ash/login/ai_intro_screen_handler.h"
 
+#include "ash/constants/ash_features.h"
+#include "base/strings/utf_string_conversions.h"
+#include "build/branding_buildflags.h"
 #include "chrome/browser/ui/webui/ash/login/base_screen_handler.h"
+#include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/login/localized_values_builder.h"
+#include "ui/base/resource/resource_bundle.h"
 #include "ui/chromeos/devicetype_utils.h"
 
 namespace ash {
@@ -19,8 +24,22 @@ AiIntroScreenHandler::~AiIntroScreenHandler() = default;
 void AiIntroScreenHandler::DeclareLocalizedValues(
     ::login::LocalizedValuesBuilder* builder) {
   builder->Add("aiIntroScreenTitle", IDS_AI_INTRO_TITLE);
-  builder->AddF("aiIntroScreenSubtitle", IDS_AI_INTRO_SUBTITLE,
-                ui::GetChromeOSDeviceName());
+  // TODO(b/334819371) Cleanup strings
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  if (base::FeatureList::IsEnabled(features::kFeatureManagementOobeAiIntro)) {
+    auto product_name =
+        ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
+            IDR_CROS_OOBE_PRODUCT_NAME);
+    // Trim trailing space
+    product_name = product_name.substr(0, product_name.length() - 1);
+    builder->AddF("aiIntroScreenSubtitle", IDS_AI_INTRO_SUBTITLE,
+                  base::UTF8ToUTF16(product_name));
+  } else
+#endif
+  {
+    builder->AddF("aiIntroScreenSubtitle", IDS_AI_INTRO_SUBTITLE,
+                  ui::GetChromeOSDeviceName());
+  }
   builder->Add("slideWriteTitle", IDS_AI_INTRO_SLIDE_WRITE_TITLE);
   builder->Add("slideWriteText", IDS_AI_INTRO_SLIDE_WRITE_TEXT);
   builder->Add("slideWallpaperTitle", IDS_AI_INTRO_SLIDE_WALLPAPER_TITLE);
--- chrome/browser/ui/webui/ash/login/tuna_screen_handler.cc
+++ chrome/browser/ui/webui/ash/login/tuna_screen_handler.cc
@@ -4,9 +4,14 @@
 
 #include "chrome/browser/ui/webui/ash/login/tuna_screen_handler.h"
 
+#include "ash/constants/ash_features.h"
+#include "base/strings/utf_string_conversions.h"
+#include "build/branding_buildflags.h"
 #include "chrome/browser/ui/webui/ash/login/base_screen_handler.h"
+#include "chrome/grit/chrome_unscaled_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/login/localized_values_builder.h"
+#include "ui/base/resource/resource_bundle.h"
 #include "ui/chromeos/devicetype_utils.h"
 
 namespace ash {
@@ -18,7 +23,22 @@ TunaScreenHandler::~TunaScreenHandler() = default;
 // Add localized values that you want to propagate to the JS side here.
 void TunaScreenHandler::DeclareLocalizedValues(
     ::login::LocalizedValuesBuilder* builder) {
-  builder->AddF("tunaScreenTitle", IDS_TUNA_TITLE, ui::GetChromeOSDeviceName());
+  // TODO(b/334819371) Cleanup strings
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  if (base::FeatureList::IsEnabled(features::kFeatureManagementOobeTuna)) {
+    auto product_name =
+        ui::ResourceBundle::GetSharedInstance().GetRawDataResource(
+            IDR_CROS_OOBE_PRODUCT_NAME);
+    // Trim trailing space
+    product_name = product_name.substr(0, product_name.length() - 1);
+    builder->AddF("tunaScreenTitle", IDS_TUNA_TITLE,
+                  base::UTF8ToUTF16(product_name));
+  } else
+#endif
+  {
+    builder->AddF("tunaScreenTitle", IDS_TUNA_TITLE,
+                  ui::GetChromeOSDeviceName());
+  }
   builder->Add("tunaScreenSubtitle", IDS_TUNA_SUBTITLE);
 }
 
