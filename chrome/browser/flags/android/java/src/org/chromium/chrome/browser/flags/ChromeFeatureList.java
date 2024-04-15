e92f4ba4e3d3725b72eac3decee4b193619eac83
Anna Tsvirchkova <atsvirchkova@google.com>
2024-04-15 10:41:28 +0000

    Revert "[UPM] Enable OmniboxPedalsTest#testManagePasswords test"
    This reverts commit 015ab335dc0bd74716268621f3262a3bb1840c81.
    
    Reason for revert: OmniboxPedalsTest test fails on android-12l-x64-dbg-tests bot
    
    Original change's description:
    > [UPM] Enable OmniboxPedalsTest#testManagePasswords test
    >
    > The test checks that Chrome password settings open when the omnibox
    > pedal is clicked. The test is relevant only for no-UPM flow because
    > Chrome should open GMS Core UI when UPM is enabled.
    >
    > Bug: 333905073
    > Change-Id: I2708624d1b9599844f078bd95701fd6ecb1be293
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447542
    > Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    > Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1287182}
    
    Bug: 333905073
    Change-Id: Ie05f9728ab1f9eb73c3af6881b227bc1a033d1c2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444437
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287247}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -495,8 +495,6 @@ public abstract class ChromeFeatureList {
     public static final String SHARED_PASSWORD_NOTIFICATION_UI = "SharedPasswordNotificationUI";
     public static final String UNIFIED_PASSWORD_MANAGER_LOCAL_PASSWORDS_ANDROID_NO_MIGRATION =
             "UnifiedPasswordManagerLocalPasswordsAndroidNoMigration";
-    public static final String UNIFIED_PASSWORD_MANAGER_LOCAL_PASSWORDS_ANDROID_WITH_MIGRATION =
-            "UnifiedPasswordManagerLocalPasswordsAndroidWithMigration";
     public static final String UNIFIED_PASSWORD_MANAGER_LOCAL_PWD_MIGRATION_WARNING =
             "UnifiedPasswordManagerLocalPasswordsMigrationWarning";
     public static final String USE_CHIME_ANDROID_SDK = "UseChimeAndroidSdk";
