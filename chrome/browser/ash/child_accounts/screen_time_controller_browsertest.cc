4dca44c90abc7cd86014652209bc48c4cde970fd
Sebastien Lalancette <seblalancette@chromium.org>
2024-04-15 15:20:11 +0000

    Disable Flaky ScreenTimeControllerTest tests
    The tests are flaky on ci/linux-chromeos-rel. e.g.:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-rel/75868
    
    Bug: 334319436,334304756,334160972
    Change-Id: I6746a862cd96cc69144b6e0f9ddb40a10c4aaebd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454591
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Owners-Override: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287359}

--- chrome/browser/ash/child_accounts/screen_time_controller_browsertest.cc
+++ chrome/browser/ash/child_accounts/screen_time_controller_browsertest.cc
@@ -553,7 +553,9 @@ IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest, DefaultDailyLimit) {
 }
 
 // Tests that the bedtime locks an active session when it is reached.
-IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest, ActiveSessionBedtime) {
+// TODO(crbug.com/334319436): Flaky test.
+IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest,
+                       DISABLED_ActiveSessionBedtime) {
   LogInChildAndSetupClockWithTime("1 Jan 2018 10:00:00 PST");
 
   system::TimezoneSettings::GetInstance()->SetTimezoneFromID(u"PST");
@@ -584,7 +586,9 @@ IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest, ActiveSessionBedtime) {
 }
 
 // Tests that the daily limit locks the device when it is reached.
-IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest, ActiveSessionDailyLimit) {
+// TODO(crbug.com/334304756): Flaky on CrOS.
+IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest,
+                       DISABLED_ActiveSessionDailyLimit) {
   LogInChildAndSetupClockWithTime("1 Jan 2018 10:00:00 PST");
 
   system::TimezoneSettings::GetInstance()->SetTimezoneFromID(u"PST");
@@ -717,7 +721,8 @@ IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest,
 }
 
 // Tests if call the observers for usage time limit warning.
-IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest, CallObservers) {
+// TODO(crbug.com/334160972): Flaky on CrOS.
+IN_PROC_BROWSER_TEST_F(ScreenTimeControllerTest, DISABLED_CallObservers) {
   LogInChildAndSetupClockWithTime("1 Jan 2018 10:00:00 PST");
 
   system::TimezoneSettings::GetInstance()->SetTimezoneFromID(u"PST");
