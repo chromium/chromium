4ee0951e12fb301b6e914ebf33c70926b3f07e6c
Xinghui Lu <xinghuilu@chromium.org>
2024-04-15 21:19:37 +0000

    Add DownloadReportWithoutUserDecision to #download-warning-improvements flag
    Bug: 329471668
    Change-Id: I6e8f71dafd6437bede96cf6ca62d67f67794cf14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5452176
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287624}

--- chrome/browser/metrics/chrome_feature_list_creator.cc
+++ chrome/browser/metrics/chrome_feature_list_creator.cc
@@ -101,6 +101,9 @@ GetSwitchDependentFeatureOverrides(const base::CommandLine& command_line) {
       {switches::kEnableDownloadWarningImprovements,
        std::cref(safe_browsing::kDeepScanningPromptRemoval),
        base::FeatureList::OVERRIDE_ENABLE_FEATURE},
+      {switches::kEnableDownloadWarningImprovements,
+       std::cref(safe_browsing::kDownloadReportWithoutUserDecision),
+       base::FeatureList::OVERRIDE_ENABLE_FEATURE},
 
       // Override for --privacy-sandbox-ads-apis.
       {switches::kEnablePrivacySandboxAdsApis,
