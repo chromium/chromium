b9d5e1251ad0ca0949f2db0f8b34f5437699dfbc
Ben Pastene <bpastene@chromium.org>
2024-04-15 23:43:51 +0000

    utr: Don't colorize the cipd init command
    Similar to the `cipd install` command, the rich logger will colorize
    random parts in the `cipd init` command. So this applies the same fix:
    using the 'basic_logger' to print the cmd.
    
    Bug: 41492688
    Change-Id: I80d66d108cf5ab162ca4a361ad2205245211e234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454429
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287724}

--- tools/utr/cipd.py
+++ tools/utr/cipd.py
@@ -29,7 +29,9 @@ def fetch_recipe_bundle(is_verbose):
     # our own root for the bundle.
     cmd = [exe, 'init', '-force', str(_CIPD_ROOT_DIR)]
     logging.info('Initializing cipd root for bundle:')
-    logging.info(' '.join(cmd))
+    # Use the "basic_logger" here (and below) to avoid rich from coloring random
+    # bits of the printed command.
+    logging.getLogger('basic_logger').info(' '.join(cmd))
     subprocess.check_call(cmd)
 
   cmd = [
@@ -43,8 +45,6 @@ def fetch_recipe_bundle(is_verbose):
       'debug' if is_verbose else 'warning',
   ]
   logging.info('[cyan]Running bundle install command:[/]')
-  # Use the "basic_logger" to avoid rich from coloring random bits of the
-  # printed command.
   logging.getLogger('basic_logger').info(' '.join(cmd))
   # The stdout of `cipd install` seems noisy, and all useful logs appear to go
   # to stderr anyway.
