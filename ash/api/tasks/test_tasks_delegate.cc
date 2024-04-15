17d637d20cba25836a6b0ef79eee794edd3923be
Alexander Bolodurin <alexbn@chromium.org>
2024-04-15 01:23:03 +0000

    Revert "Tasks-Api: Fill in TestTasksDelegate"
    This reverts commit 96e14ab43b4458774c8ead1c724842a57e750e62.
    
    Reason for revert: failures on multiple bots for multiple tests.
    
    DoNotDisturbNotificationControllerWithFocusModeTest.*
    FocusModeDetailedViewTest.*
    FocusModeChipCarouselTest.*
    FocusModeControllerMultiUserTest.*
    FocusModeCountdownViewTest.*
    FocusModeFeaturePodControllerTest.*
    
    Bug: b/334223570
    
    Original change's description:
    > Tasks-Api: Fill in TestTasksDelegate
    >
    > This CL fills in the body of TestTasksDelegate so that we can use it for
    > testing in Focus Mode.
    >
    > Bug: b/319307346
    > Change-Id: I7e7985d082227caf410087ad626fb60ae3f072d7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319140
    > Reviewed-by: Hongyu Long <hongyulong@chromium.org>
    > Reviewed-by: Richard Chui <richui@chromium.org>
    > Reviewed-by: Sean Kau <skau@chromium.org>
    > Commit-Queue: Daniel Andersson <dandersson@google.com>
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1286608}
    
    Bug: b/319307346
    Change-Id: I21c54e0bc9aade565e46eef117470ddca6049668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446778
    Owners-Override: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287122}

--- ash/api/tasks/test_tasks_delegate.cc
+++ ash/api/tasks/test_tasks_delegate.cc
@@ -4,55 +4,37 @@
 
 #include "ash/api/tasks/test_tasks_delegate.h"
 
-#include <memory>
-
-#include "ash/api/tasks/fake_tasks_client.h"
-#include "ash/api/tasks/tasks_types.h"
-#include "base/logging.h"
+#include "ash/api/tasks/tasks_client.h"
+#include "base/notreached.h"
 #include "components/account_id/account_id.h"
 
 namespace ash::api {
 
-TestTasksDelegate::TestTasksDelegate() {
-  AccountId account_id = EmptyAccountId();
-  AddFakeTasksClient(account_id, std::make_unique<FakeTasksClient>());
-  SetActiveAccount(account_id);
-}
+TestTasksDelegate::TestTasksDelegate() = default;
 
 TestTasksDelegate::~TestTasksDelegate() = default;
 
 void TestTasksDelegate::UpdateClientForProfileSwitch(
     const AccountId& account_id) {
-  if (clients_.find(account_id) == clients_.end()) {
-    LOG(WARNING) << "No FakeTasksClient exists for active account";
-  }
-
-  if (active_client_) {
-    active_client_->InvalidateCache();
-  }
-
-  SetActiveAccount(account_id);
+  NOTIMPLEMENTED();
 }
 
 void TestTasksDelegate::GetTaskLists(
     bool force_fetch,
     TasksClient::GetTaskListsCallback callback) {
-  CHECK(active_client_);
-  active_client_->GetTaskLists(force_fetch, std::move(callback));
+  NOTIMPLEMENTED();
 }
 
 void TestTasksDelegate::GetTasks(const std::string& task_list_id,
                                  bool force_fetch,
                                  TasksClient::GetTasksCallback callback) {
-  CHECK(active_client_);
-  active_client_->GetTasks(task_list_id, force_fetch, std::move(callback));
+  NOTIMPLEMENTED();
 }
 
 void TestTasksDelegate::AddTask(const std::string& task_list_id,
                                 const std::string& title,
                                 TasksClient::OnTaskSavedCallback callback) {
-  CHECK(active_client_);
-  active_client_->AddTask(task_list_id, title, std::move(callback));
+  NOTIMPLEMENTED();
 }
 
 void TestTasksDelegate::UpdateTask(const std::string& task_list_id,
@@ -60,21 +42,7 @@ void TestTasksDelegate::UpdateTask(const std::string& task_list_id,
                                    const std::string& title,
                                    bool completed,
                                    TasksClient::OnTaskSavedCallback callback) {
-  CHECK(active_client_);
-  active_client_->UpdateTask(task_list_id, task_id, title, completed,
-                             std::move(callback));
-}
-
-void TestTasksDelegate::AddFakeTasksClient(
-    const AccountId& account_id,
-    std::unique_ptr<FakeTasksClient> tasks_client) {
-  clients_.insert_or_assign(account_id, std::move(tasks_client));
-}
-
-void TestTasksDelegate::SetActiveAccount(const AccountId& account_id) {
-  const auto iter = clients_.find(account_id);
-  CHECK(iter != clients_.end());
-  active_client_ = iter->second.get();
+  NOTIMPLEMENTED();
 }
 
 }  // namespace ash::api
