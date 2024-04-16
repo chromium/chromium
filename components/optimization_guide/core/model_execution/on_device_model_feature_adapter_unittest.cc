49c5ef95028b66744a5d8ae0ed14ea692946c05f
Steven Holte <holte@google.com>
2024-04-16 19:46:18 +0000

    Move some adapter specific tests.
    Bug: 335015454
    
    Change-Id: Ie2ab1dea8408f7099b2e0d16de8e602a960fc5a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455999
    Commit-Queue: Steven Holte <holte@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288249}

--- components/optimization_guide/core/model_execution/on_device_model_feature_adapter_unittest.cc
+++ components/optimization_guide/core/model_execution/on_device_model_feature_adapter_unittest.cc
@@ -4,6 +4,8 @@
 
 #include "components/optimization_guide/core/model_execution/on_device_model_feature_adapter.h"
 
+#include "base/test/test.pb.h"
+#include "components/optimization_guide/core/optimization_guide_util.h"
 #include "components/optimization_guide/proto/features/compose.pb.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -67,6 +69,132 @@ TEST(OnDeviceModelFeatureAdapterTest,
   proto::ComposeRequest request;
   request.mutable_page_metadata()->set_page_url("url");
   EXPECT_EQ(std::nullopt, adapter->ConstructTextSafetyRequest(request, "text"));
+}
+
+TEST(OnDeviceModelFeatureAdapterTest, ConstructInputString_NoInputConfig) {
+  proto::OnDeviceModelExecutionFeatureConfig config;
+  auto adapter =
+      base::MakeRefCounted<OnDeviceModelFeatureAdapter>(std::move(config));
+
+  base::test::TestMessage test;
+  test.set_test("some test");
+  auto result =
+      adapter->ConstructInputString(test, /*want_input_context=*/false);
+
+  EXPECT_FALSE(result);
+}
+
+TEST(OnDeviceModelFeatureAdapterTest, ConstructInputString_MismatchRequest) {
+  proto::OnDeviceModelExecutionFeatureConfig config;
+  auto* input_config = config.mutable_input_config();
+  input_config->set_request_base_name("wrong name");
+  auto adapter =
+      base::MakeRefCounted<OnDeviceModelFeatureAdapter>(std::move(config));
+
+  base::test::TestMessage test;
+  test.set_test("some test");
+  auto result =
+      adapter->ConstructInputString(test, /*want_input_context=*/false);
+
+  EXPECT_FALSE(result);
+}
+
+TEST(OnDeviceModelFeatureAdapterTest, ConstructInputString_ForInputContext) {
+  proto::OnDeviceModelExecutionFeatureConfig config;
+  auto* input_config = config.mutable_input_config();
+  input_config->set_request_base_name("base.test.TestMessage");
+  auto* ctx_substitution = input_config->add_input_context_substitutions();
+  ctx_substitution->set_string_template("hello this is %s");
+  ctx_substitution->add_substitutions()->add_candidates()->set_raw_string(
+      "input context");
+  auto* execute_substitution = input_config->add_execute_substitutions();
+  execute_substitution->set_string_template("this should be ignored");
+  auto adapter =
+      base::MakeRefCounted<OnDeviceModelFeatureAdapter>(std::move(config));
+
+  base::test::TestMessage test;
+  test.set_test("some test");
+  auto result =
+      adapter->ConstructInputString(test, /*want_input_context=*/true);
+
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(result->input_string, "hello this is input context");
+}
+
+TEST(OnDeviceModelFeatureAdapterTest, ConstructInputString_ForExecution) {
+  proto::OnDeviceModelExecutionFeatureConfig config;
+  auto* input_config = config.mutable_input_config();
+  input_config->set_request_base_name("base.test.TestMessage");
+  auto* ctx_substitution = input_config->add_input_context_substitutions();
+  ctx_substitution->set_string_template("this should be ignored");
+  auto* execute_substitution = input_config->add_execute_substitutions();
+  execute_substitution->set_string_template("hello this is %s");
+  execute_substitution->add_substitutions()->add_candidates()->set_raw_string(
+      "execution");
+  auto adapter =
+      base::MakeRefCounted<OnDeviceModelFeatureAdapter>(std::move(config));
+
+  base::test::TestMessage test;
+  test.set_test("some test");
+  auto result =
+      adapter->ConstructInputString(test, /*want_input_context=*/false);
+
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(result->input_string, "hello this is execution");
+}
+
+TEST(OnDeviceModelFeatureAdapterTest, ConstructOutputMetadata_NoOutputConfig) {
+  proto::OnDeviceModelExecutionFeatureConfig config;
+
+  auto adapter =
+      base::MakeRefCounted<OnDeviceModelFeatureAdapter>(std::move(config));
+
+  auto maybe_metadata = adapter->ConstructOutputMetadata("output");
+
+  EXPECT_FALSE(maybe_metadata.has_value());
+}
+
+TEST(OnDeviceModelFeatureAdapterTest, ConstructOutputMetadata_BadProto) {
+  proto::OnDeviceModelExecutionFeatureConfig config;
+  auto* oc = config.mutable_output_config();
+  oc->set_proto_type("garbage type");
+  oc->mutable_proto_field()->add_proto_descriptors()->set_tag_number(1);
+  auto adapter =
+      base::MakeRefCounted<OnDeviceModelFeatureAdapter>(std::move(config));
+
+  auto maybe_metadata = adapter->ConstructOutputMetadata("output");
+
+  EXPECT_FALSE(maybe_metadata.has_value());
+}
+
+TEST(OnDeviceModelFeatureAdapterTest,
+     ConstructOutputMetadata_DescriptorSpecifiedNotStringValue) {
+  proto::OnDeviceModelExecutionFeatureConfig config;
+  auto* oc = config.mutable_output_config();
+  oc->set_proto_type("optimization_guide.proto.ComposeRequest");
+  oc->mutable_proto_field()->add_proto_descriptors()->set_tag_number(7);
+  auto adapter =
+      base::MakeRefCounted<OnDeviceModelFeatureAdapter>(std::move(config));
+
+  auto maybe_metadata = adapter->ConstructOutputMetadata("output");
+
+  EXPECT_FALSE(maybe_metadata.has_value());
+}
+
+TEST(OnDeviceModelFeatureAdapterTest, ConstructOutputMetadata_DescriptorValid) {
+  proto::OnDeviceModelExecutionFeatureConfig config;
+  auto* oc = config.mutable_output_config();
+  oc->set_proto_type("optimization_guide.proto.ComposeResponse");
+  oc->mutable_proto_field()->add_proto_descriptors()->set_tag_number(1);
+  auto adapter =
+      base::MakeRefCounted<OnDeviceModelFeatureAdapter>(std::move(config));
+
+  auto maybe_metadata = adapter->ConstructOutputMetadata("output");
+
+  ASSERT_TRUE(maybe_metadata.has_value());
+  EXPECT_EQ(
+      "output",
+      ParsedAnyMetadata<proto::ComposeResponse>(*maybe_metadata)->output());
 }
 
 }  // namespace optimization_guide
