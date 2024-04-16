bda54dafd534e2aa4cbb32079379d9ba2b458f8b
Quentin Pubert <qpubert@google.com>
2024-04-16 17:45:50 +0000

    [iOS] Enable Save to Photos feature by default
    Change-Id: I565fda82b0d4d7bb5d1de0b4137ed7a63ff6e00a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458174
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288166}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -321,7 +321,7 @@ BASE_FEATURE(kIOSSaveToDrive,
 
 BASE_FEATURE(kIOSSaveToPhotos,
              "IOSSaveToPhotos",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableUIEditMenuInteraction,
              "EnableUIEditMenuInteraction",
