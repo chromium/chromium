df77329741e7aa30193df243e669a04dbf808d74
Joemer Ramos <joemerramos@chromium.org>
2024-04-17 20:24:34 +0000

    [iOS] Enable HPRT for iOS
    Enable HPRT for iOS by default.
    
    Bug: 40280359
    Change-Id: I191b6fc2d8cfb278b9461a126fec7df02e13c804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5462099
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288943}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -133,7 +133,7 @@ BASE_FEATURE(kFriendlierSafeBrowsingSettingsStandardProtection,
 BASE_FEATURE(kHashPrefixRealTimeLookups,
              "SafeBrowsingHashPrefixRealTimeLookups",
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_CHROMEOS)
+    BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_IOS)
              base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
