881fb1b44ef273496730019a5748ef2af17de972
Nidhi Jaju <nidhijaju@chromium.org>
2024-04-18 05:58:12 +0000

    Revert "Add displayName to animation trace event"
    This reverts commit 7e1f73fef48baa28dd62ec73857ea25964e50457.
    
    Reason for revert: http/tests/inspector-protocol/tracing/animation.js is flakily failing.
    i.e. https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/26910/overview
    
    Original change's description:
    > Add displayName to animation trace event
    >
    > We use this property for auditing in Lighthouse by using a cdp call.
    > We want to avoid making cdp calls in our transition to RPP,
    > so adding this information in through the trace
    >
    > Bug: 331429737,335215001
    > Change-Id: I08533ae28c2738fddc5c0f5c14a29e505fa3f5c3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448403
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    > Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1288539}
    
    Bug: 331429737,335215001
    Change-Id: I0588e958c570f6e248e0eea499c90e1b03abfded
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464704
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1289159}

--- third_party/blink/renderer/core/inspector/inspector_animation_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_animation_agent.h
@@ -68,7 +68,6 @@ class CORE_EXPORT InspectorAnimationAgent final
   protocol::Response AssertAnimation(const String& id,
                                      blink::Animation*& result);
 
-  static String AnimationDisplayName(const Animation& animation);
   void Trace(Visitor*) const override;
 
  private:
