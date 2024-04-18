495e030e4952cbf06250842f0db21a9754386281
Solomon Kinard <solomonkinard@chromium.org>
2024-04-18 20:05:39 +0000

    Extensions: Collapse namespaces where possible and move comments to .h
    crrev.org/r/19678004 introduced an alphabetized .h and a commented .cc.
    However, the alphabetization seems to not be a real dependency. Comments
    were moved to the header file to match the style of extension_features.h
    and etc.
    
    Bug: None
    Change-Id: I68a6a06043c236c19676d323c3b6796bb997c308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464500
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289526}

--- extensions/common/switches.cc
+++ extensions/common/switches.cc
@@ -6,97 +6,41 @@
 
 #include "build/chromeos_buildflags.h"
 
-namespace extensions {
+namespace extensions::switches {
 
-namespace switches {
-
-// Allows non-https URL for background_page for hosted apps.
 const char kAllowHTTPBackgroundPage[] = "allow-http-background-page";
-
-// Allows the browser to load extensions that lack a modern manifest when that
-// would otherwise be forbidden.
 const char kAllowLegacyExtensionManifests[] =
     "allow-legacy-extension-manifests";
-
-// Adds the given extension ID to all the permission allowlists.
 const char kAllowlistedExtensionID[] = "allowlisted-extension-id";
-
-// Enables extension options to be embedded in chrome://extensions rather than
-// a new tab.
 const char kEmbeddedExtensionOptions[] = "embedded-extension-options";
-
-// Enable BLE Advertising in apps.
 const char kEnableBLEAdvertising[] = "enable-ble-advertising-in-apps";
-
-// Enables extension APIs that are in development.
 const char kEnableExperimentalExtensionApis[] =
     "enable-experimental-extension-apis";
-
-// Disable checking for user opt-in for extensions that want to inject script
-// into file URLs (ie, always allow it). This is used during automated testing.
 const char kDisableExtensionsFileAccessCheck[] =
     "disable-extensions-file-access-check";
-
-// Disable the net::URLRequestThrottlerManager functionality for
-// requests originating from extensions.
 const char kDisableExtensionsHttpThrottling[] =
     "disable-extensions-http-throttling";
-
-// Marks a renderer as extension process.
 const char kExtensionProcess[] = "extension-process";
-
-// Enables extensions running scripts on chrome:// URLs.
-// Extensions still need to explicitly request access to chrome:// URLs in the
-// manifest.
 const char kExtensionsOnChromeURLs[] = "extensions-on-chrome-urls";
-
-// Whether to force developer mode extensions highlighting.
 const char kForceDevModeHighlighting[] = "force-dev-mode-highlighting";
-
-// Whether to disable app content verification when testing changes locally on
-// Chromebox for Meetings hardware.
 const char kDisableAppContentVerification[] =
     "disable-app-content-verification";
-
-// Comma-separated list of paths to apps to load at startup. The first app in
-// the list will be launched.
 const char kLoadApps[] = "load-apps";
-
-// Comma-separated list of paths to extensions to load at startup.
 const char kLoadExtension[] = "load-extension";
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-// Path to the unpacked test extension to load into the signin profile. The ID
-// extension loaded must match kTestSigninProfileExtensionId.
 const char kLoadSigninProfileTestExtension[] =
     "load-signin-profile-test-extension";
-
-// Path to the unpacked test extension to load into guest mode. The extension ID
-// must match kGuestModeTestExtensionId.
 const char kLoadGuestModeTestExtension[] = "load-guest-mode-test-extension";
 #endif
 
-// Allows the use of the `testing` reason in offscreen documents.
 const char kOffscreenDocumentTesting[] = "offscreen-document-testing";
-
-// Set the parameters for ExtensionURLLoaderThrottleBrowserTest.
 const char kSetExtensionThrottleTestParams[] =
     "set-extension-throttle-test-params";
-
-// Makes component extensions appear in chrome://settings/extensions.
 const char kShowComponentExtensionOptions[] =
     "show-component-extension-options";
-
-// Pass launch source to platform apps.
 const char kTraceAppSource[] = "enable-trace-app-source";
-
-// Enable package hash check: the .crx file sha256 hash sum should be equal to
-// the one received from update manifest.
 const char kEnableCrxHashCheck[] = "enable-crx-hash-check";
-
-// Mute extension errors while working with new manifest version.
 const char kAllowFutureManifestVersion[] = "allow-future-manifest-version";
 
-}  // namespace switches
-
-}  // namespace extensions
+}  // namespace extensions::switches
--- extensions/common/switches.h
+++ extensions/common/switches.h
@@ -7,39 +7,87 @@
 
 #include "build/chromeos_buildflags.h"
 
-// All switches in alphabetical order. The switches should be documented
-// alongside the definition of their values in the .cc file.
-namespace extensions {
+namespace extensions::switches {
 
-namespace switches {
-
+// Allows non-https URL for background_page for hosted apps.
 extern const char kAllowHTTPBackgroundPage[];
+
+// Allows the browser to load extensions that lack a modern manifest when that
+// would otherwise be forbidden.
 extern const char kAllowLegacyExtensionManifests[];
+
+// Adds the given extension ID to all the permission allowlists.
 extern const char kAllowlistedExtensionID[];
+
+// Whether to disable app content verification when testing changes locally on
+// Chromebox for Meetings hardware.
 extern const char kDisableAppContentVerification[];
+
+// Disable checking for user opt-in for extensions that want to inject script
+// into file URLs (ie, always allow it). This is used during automated testing.
 extern const char kDisableExtensionsFileAccessCheck[];
+
+// Disable the net::URLRequestThrottlerManager functionality for
+// requests originating from extensions.
 extern const char kDisableExtensionsHttpThrottling[];
+
+// Enables extension options to be embedded in chrome://extensions rather than
+// a new tab.
 extern const char kEmbeddedExtensionOptions[];
+
+// Enables extension APIs that are in development.
 extern const char kEnableExperimentalExtensionApis[];
+
+// Enable BLE Advertising in apps.
 extern const char kEnableBLEAdvertising[];
+
+// Marks a renderer as extension process.
 extern const char kExtensionProcess[];
+
+// Enables extensions running scripts on chrome:// URLs.
+// Extensions still need to explicitly request access to chrome:// URLs in the
+// manifest.
 extern const char kExtensionsOnChromeURLs[];
+
+// Whether to force developer mode extensions highlighting.
 extern const char kForceDevModeHighlighting[];
+
+// Comma-separated list of paths to apps to load at startup. The first app in
+// the list will be launched.
 extern const char kLoadApps[];
+
+// Comma-separated list of paths to extensions to load at startup.
 extern const char kLoadExtension[];
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+// Path to the unpacked test extension to load into the signin profile. The ID
+// extension loaded must match kTestSigninProfileExtensionId.
 extern const char kLoadSigninProfileTestExtension[];
+
+// Path to the unpacked test extension to load into guest mode. The extension ID
+// must match kGuestModeTestExtensionId.
 extern const char kLoadGuestModeTestExtension[];
 #endif
+
+// Allows the use of the `testing` reason in offscreen documents.
 extern const char kOffscreenDocumentTesting[];
+
+// Set the parameters for ExtensionURLLoaderThrottleBrowserTest.
 extern const char kSetExtensionThrottleTestParams[];
+
+// Makes component extensions appear in chrome://settings/extensions.
 extern const char kShowComponentExtensionOptions[];
+
+// Pass launch source to platform apps.
 extern const char kTraceAppSource[];
+
+// Enable package hash check: the .crx file sha256 hash sum should be equal to
+// the one received from update manifest.
 extern const char kEnableCrxHashCheck[];
+
+// Mute extension errors while working with new manifest version.
 extern const char kAllowFutureManifestVersion[];
 
-}  // namespace switches
-
-}  // namespace extensions
+}  // namespace extensions::switches
 
 #endif  // EXTENSIONS_COMMON_SWITCHES_H_
