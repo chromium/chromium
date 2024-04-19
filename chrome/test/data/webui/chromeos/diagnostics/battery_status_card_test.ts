4e1749c47f811f33c91d3ffc7452ed72547316ba
Michael Checo <michaelcheco@google.com>
2024-04-19 00:18:19 +0000

    diagnostics: Convert battery_status_card_test to TS
    Bug: b/245336251
    Change-Id: I689e9312786dc431332029b48190a104038fb9a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5451050
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289688}

--- chrome/test/data/webui/chromeos/diagnostics/battery_status_card_test.ts
+++ chrome/test/data/webui/chromeos/diagnostics/battery_status_card_test.ts
@@ -10,54 +10,44 @@ import {BatteryStatusCardElement} from 'chrome://diagnostics/battery_status_card
 import {getDiagnosticsIcon} from 'chrome://diagnostics/diagnostics_utils.js';
 import {fakeBatteryChargeStatus, fakeBatteryChargeStatus2, fakeBatteryChargeStatus3, fakeBatteryHealth, fakeBatteryHealth2, fakeBatteryHealth3, fakeBatteryInfo} from 'chrome://diagnostics/fake_data.js';
 import {FakeSystemDataProvider} from 'chrome://diagnostics/fake_system_data_provider.js';
-import {getSystemDataProvider, setSystemDataProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
+import {setSystemDataProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
 import {RoutineSectionElement} from 'chrome://diagnostics/routine_section.js';
 import {BatteryChargeStatus, BatteryHealth, BatteryInfo, ExternalPowerSource} from 'chrome://diagnostics/system_data_provider.mojom-webui.js';
 import {RoutineType} from 'chrome://diagnostics/system_routine_controller.mojom-webui.js';
-import {TextBadgeElement} from 'chrome://diagnostics/text_badge.js';
+import {CrButtonElement} from 'chrome://resources/ash/common/cr_elements/cr_button/cr_button.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
+import {strictQuery} from 'chrome://resources/ash/common/typescript_utils/strict_query.js';
+import {assert} from 'chrome://resources/js/assert.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
-
-import {isChildVisible, isVisible} from '../test_util.js';
+import {isChildVisible, isVisible} from 'chrome://webui-test/test_util.js';
 
 import * as dx_utils from './diagnostics_test_utils.js';
 
 const BATTERY_ICON_PREFIX = 'battery-';
 
 suite('batteryStatusCardTestSuite', function() {
-  /** @type {?BatteryStatusCardElement} */
-  let batteryStatusElement = null;
+  let batteryStatusElement: BatteryStatusCardElement|null = null;
 
-  /** @type {?FakeSystemDataProvider} */
-  let provider = null;
+  const provider: FakeSystemDataProvider = new FakeSystemDataProvider();
 
   suiteSetup(() => {
-    provider = new FakeSystemDataProvider();
     setSystemDataProviderForTesting(provider);
   });
 
   setup(() => {
-    document.body.innerHTML = window.trustedTypes.emptyHTML;
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
   });
 
   teardown(() => {
-    if (batteryStatusElement) {
-      batteryStatusElement.remove();
-    }
+    batteryStatusElement?.remove();
     batteryStatusElement = null;
-
-    provider.reset();
+    provider?.reset();
   });
 
-  /**
-   * @param {!BatteryInfo} batteryInfo
-   * @param {!Array<!BatteryChargeStatus>} batteryChargeStatus
-   * @param {!Array<!BatteryHealth>} batteryHealth
-   * @return {!Promise}
-   */
   function initializeBatteryStatusCard(
-      batteryInfo, batteryChargeStatus, batteryHealth) {
+      batteryInfo: BatteryInfo, batteryChargeStatus: BatteryChargeStatus[],
+      batteryHealth: BatteryHealth[]): Promise<void> {
     assertFalse(!!batteryStatusElement);
 
     // Initialize the fake data.
@@ -66,9 +56,8 @@ suite('batteryStatusCardTestSuite', function() {
     provider.setFakeBatteryInfo(batteryInfo);
 
     // Add the battery status card to the DOM.
-    batteryStatusElement = /** @type {!BatteryStatusCardElement} */ (
-        document.createElement('battery-status-card'));
-    assertTrue(!!batteryStatusElement);
+    batteryStatusElement = document.createElement('battery-status-card');
+    assert(batteryStatusElement);
     document.body.appendChild(batteryStatusElement);
 
     return flushTasks();
@@ -76,66 +65,35 @@ suite('batteryStatusCardTestSuite', function() {
 
   /**
    * Returns the routine-section from the card.
-   * @return {!RoutineSectionElement}
    */
-  function getRoutineSection() {
-    const routineSection = /** @type {!RoutineSectionElement} */ (
-        batteryStatusElement.shadowRoot.querySelector('routine-section'));
-    assertTrue(!!routineSection);
-    return routineSection;
-  }
-
-  /**
-   * Returns the status badge.
-   * @return {!TextBadgeElement}
-   */
-  function getStatusBadge() {
-    const routineSectionElement = getRoutineSection();
-
-    return /** @type {!TextBadgeElement} */ (
-        routineSectionElement.shadowRoot.querySelector('#testStatusBadge'));
-  }
-
-  /**
-   * Returns the status text.
-   * @return {!HTMLElement}
-   */
-  function getStatusTextElement() {
-    const routineSectionElement = getRoutineSection();
-
-    const statusText =
-        /** @type {!HTMLElement} */ (
-            routineSectionElement.shadowRoot.querySelector('#testStatusText'));
-    assertTrue(!!statusText);
-    return statusText;
+  function getRoutineSection(): RoutineSectionElement {
+    assert(batteryStatusElement);
+    return strictQuery(
+        'routine-section', batteryStatusElement.shadowRoot,
+        RoutineSectionElement);
   }
 
   /**
    * Returns the Run Tests button from inside the routine-section.
-   * @return {!CrButtonElement}
    */
-  function getRunTestsButton() {
+  function getRunTestsButton(): CrButtonElement {
     const button = dx_utils.getRunTestsButtonFromSection(getRoutineSection());
-    assertTrue(!!button);
+    assert(button);
     return button;
   }
 
   /**
    * Returns whether the run tests button is disabled.
-   * @return {boolean}
    */
-  function isRunTestsButtonDisabled() {
+  function isRunTestsButtonDisabled(): boolean {
     return getRunTestsButton().disabled;
   }
 
-  /**
-   * Get batteryChargeStatus.powerAdapterStatus private member for testing.
-   * @suppress {visibility} // access private member
-   * @return {!ExternalPowerSource}
-   */
-  function getPowerAdapterStatus() {
-    assertTrue(!!batteryStatusElement);
-    return batteryStatusElement.batteryChargeStatus.powerAdapterStatus;
+
+  function getPowerAdapterStatus(): ExternalPowerSource {
+    assert(batteryStatusElement);
+    return batteryStatusElement.getBatteryChargeStatusForTesting()
+        .powerAdapterStatus;
   }
 
   test('BatteryStatusCardPopulated', () => {
@@ -145,35 +103,35 @@ suite('batteryStatusCardTestSuite', function() {
           dx_utils.assertTextContains(
               dx_utils.getDataPointValue(
                   batteryStatusElement, '#batteryHealth'),
-              `${fakeBatteryHealth[0].batteryWearPercentage}`);
+              `${fakeBatteryHealth[0]!.batteryWearPercentage}`);
           dx_utils.assertTextContains(
               dx_utils.getDataPoint(batteryStatusElement, '#batteryHealth')
                   .tooltipText,
               loadTimeData.getString('batteryHealthTooltipText'));
           dx_utils.assertTextContains(
               dx_utils.getDataPointValue(batteryStatusElement, '#cycleCount'),
-              `${fakeBatteryHealth[0].cycleCount}`);
+              `${fakeBatteryHealth[0]!.cycleCount}`);
           dx_utils.assertTextContains(
               dx_utils.getDataPoint(batteryStatusElement, '#cycleCount')
                   .tooltipText,
               loadTimeData.getString('cycleCountTooltipText'));
           dx_utils.assertTextContains(
               dx_utils.getDataPointValue(batteryStatusElement, '#currentNow'),
-              `${fakeBatteryChargeStatus[0].currentNowMilliamps}`);
+              `${fakeBatteryChargeStatus[0]!.currentNowMilliamps}`);
           dx_utils.assertTextContains(
               dx_utils.getDataPoint(batteryStatusElement, '#currentNow')
                   .tooltipText,
               loadTimeData.getString('currentNowTooltipText'));
           dx_utils.assertElementContainsText(
-              batteryStatusElement.shadowRoot.querySelector(
+              batteryStatusElement!.shadowRoot!.querySelector(
                   '#batteryStatusChipInfo'),
-              `${fakeBatteryHealth[0].chargeFullDesignMilliampHours}`);
+              `${fakeBatteryHealth[0]!.chargeFullDesignMilliampHours}`);
           const barChart =
               dx_utils.getPercentBarChartElement(batteryStatusElement);
           assertEquals(
-              fakeBatteryHealth[0].chargeFullNowMilliampHours, barChart.max);
+              fakeBatteryHealth[0]!.chargeFullNowMilliampHours, barChart.max);
           assertEquals(
-              fakeBatteryChargeStatus[0].chargeNowMilliampHours,
+              fakeBatteryChargeStatus[0]!.chargeNowMilliampHours,
               barChart.value);
 
           // Verify that the data points container is visible.
@@ -198,8 +156,8 @@ suite('batteryStatusCardTestSuite', function() {
           assertEquals(
               routineSectionElement.routines[0], RoutineType.kBatteryCharge);
 
-          batteryStatusElement.onBatteryChargeStatusUpdated(
-              fakeBatteryChargeStatus[2]);
+          batteryStatusElement!.onBatteryChargeStatusUpdated(
+              (fakeBatteryChargeStatus[2] as BatteryChargeStatus));
           return flushTasks();
         })
         .then(() => {
@@ -209,8 +167,8 @@ suite('batteryStatusCardTestSuite', function() {
           assertEquals(
               routineSectionElement.routines[0], RoutineType.kBatteryDischarge);
 
-          batteryStatusElement.onBatteryChargeStatusUpdated(
-              fakeBatteryChargeStatus[3]);
+          batteryStatusElement!.onBatteryChargeStatusUpdated(
+              (fakeBatteryChargeStatus[3] as BatteryChargeStatus));
           return flushTasks();
         })
         .then(() => {
@@ -220,8 +178,8 @@ suite('batteryStatusCardTestSuite', function() {
               routineSectionElement.additionalMessage,
               loadTimeData.getString('batteryChargeTestFullMessage'));
           assertTrue(isRunTestsButtonDisabled());
-          assertTrue(isVisible(/** @type {!HTMLElement} */ (
-              routineSectionElement.shadowRoot.querySelector('#messageIcon'))));
+          assertTrue(isVisible(
+              routineSectionElement.shadowRoot!.querySelector('#messageIcon')));
         });
   });
 
@@ -232,7 +190,7 @@ suite('batteryStatusCardTestSuite', function() {
                fakeBatteryInfo, fakeBatteryChargeStatus, fakeBatteryHealth)
         .then(() => {
           assertEquals(ExternalPowerSource.kAc, getPowerAdapterStatus());
-          assertEquals(expectedBatteryIcon, batteryStatusElement.batteryIcon);
+          assertEquals(expectedBatteryIcon, batteryStatusElement!.batteryIcon);
         });
   });
 
@@ -246,7 +204,7 @@ suite('batteryStatusCardTestSuite', function() {
           const expectedIconRange = '71-77';
           assertEquals(
               getDiagnosticsIcon(BATTERY_ICON_PREFIX + expectedIconRange),
-              batteryStatusElement.batteryIcon);
+              batteryStatusElement!.batteryIcon);
         });
   });
 
@@ -260,7 +218,7 @@ suite('batteryStatusCardTestSuite', function() {
           const expectedIconRange = 'outline';
           assertEquals(
               getDiagnosticsIcon(BATTERY_ICON_PREFIX + expectedIconRange),
-              batteryStatusElement.batteryIcon);
+              batteryStatusElement!.batteryIcon);
         });
   });
 });
