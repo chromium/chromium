b8350817deb4c1c2b8b0b693efd5ad21be035539
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-04-19 11:29:58 +0000

    [iOS] Fix `testContextMenuOpenInGroup` on iPad.
    Bug: 335584000
    Change-Id: I804f900d42426dd2371e4eb766ecff56479b88a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463146
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289862}

--- ios/chrome/browser/ui/context_menu/context_menu_egtest.mm
+++ ios/chrome/browser/ui/context_menu/context_menu_egtest.mm
@@ -270,6 +270,7 @@ void RelaunchAppWithInactiveTabs2WeeksEnabled() {
   AppLaunchConfiguration config;
   config.features_enabled.push_back(kTabGroupsInGrid);
   config.features_enabled.push_back(kTabGroupsIPad);
+  config.features_enabled.push_back(kModernTabStrip);
   config.features_disabled.push_back(web::features::kSmoothScrollingDefault);
   return config;
 }
@@ -682,10 +683,6 @@ void RelaunchAppWithInactiveTabs2WeeksEnabled() {
 // exists in the tab grid, the option `Open in group` will become a submenu,
 // tapping it will result in listing all the existing tab groups.
 - (void)testContextMenuOpenInGroup {
-  // TODO(crbug.com/335584000): Test fails in some iPads.
-  if ([ChromeEarlGrey isIPadIdiom]) {
-    EARL_GREY_TEST_SKIPPED(@"Test disabled on iPad.");
-  }
   const GURL initialURL = self.testServer->GetURL(kInitialPageUrl);
   [ChromeEarlGrey loadURL:initialURL];
   [ChromeEarlGrey
