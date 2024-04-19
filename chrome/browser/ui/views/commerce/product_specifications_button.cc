826be436d227d31093df287f353e7d8e1332f5d0
Yue Zhang <yuezhanggg@chromium.org>
2024-04-19 17:06:40 +0000

    [Product specifications] Add UI components for tab strip entry point
    Bug: b/325661685
    Change-Id: I8186b3d0e92cb2a1d9e59c6f89cc7018a66008fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450752
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290011}

---
+++ chrome/browser/ui/views/commerce/product_specifications_button.cc
@@ -0,0 +1,281 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/commerce/product_specifications_button.h"
+
+#include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/color/chrome_color_id.h"
+#include "chrome/browser/ui/tabs/organization/tab_organization_service.h"
+#include "chrome/browser/ui/views/tabs/tab_strip_controller.h"
+#include "chrome/grit/generated_resources.h"
+#include "components/strings/grit/components_strings.h"
+#include "components/vector_icons/vector_icons.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/compositor/layer.h"
+#include "ui/views/animation/ink_drop.h"
+#include "ui/views/controls/highlight_path_generator.h"
+#include "ui/views/layout/box_layout.h"
+#include "ui/views/mouse_watcher_view_host.h"
+#include "ui/views/view_class_properties.h"
+
+namespace {
+// TODO(b/335015993): These constants are shared with TabSearchContainer, and
+// should be moved into one place. Similar for the LockedExpansionMode enum used
+// in this file.
+constexpr int kProductSpecificationsButtonCornerRadius = 10;
+constexpr int kProductSpecificationsButtonLabelMargin = 10;
+constexpr int kProductSpecificationsCloseButtonMargin = 8;
+constexpr int kProductSpecificationsCloseButtonSize = 16;
+
+constexpr base::TimeDelta kExpansionInDuration = base::Milliseconds(500);
+constexpr base::TimeDelta kExpansionOutDuration = base::Milliseconds(250);
+constexpr base::TimeDelta kOpacityInDuration = base::Milliseconds(300);
+constexpr base::TimeDelta kOpacityOutDuration = base::Milliseconds(100);
+constexpr base::TimeDelta kOpacityDelay = base::Milliseconds(100);
+constexpr base::TimeDelta kShowDuration = base::Seconds(20);
+
+}  // namespace
+
+ProductSpecificationsButton::ProductSpecificationsButton(
+    TabStripController* tab_strip_controller,
+    bool before_tab_strip,
+    View* locked_expansion_view)
+    : TabStripControlButton(
+          tab_strip_controller,
+          base::BindRepeating(&ProductSpecificationsButton::OnClicked,
+                              base::Unretained(this)),
+          l10n_util::GetStringUTF16(
+              IDS_PRODUCT_SPECIFICATIONS_ENTRY_POINT_DEFAULT),
+          Edge::kNone),
+      locked_expansion_view_(locked_expansion_view) {
+  mouse_watcher_ = std::make_unique<views::MouseWatcher>(
+      std::make_unique<views::MouseWatcherViewHost>(locked_expansion_view,
+                                                    gfx::Insets()),
+      this);
+  auto* const layout_manager =
+      SetLayoutManager(std::make_unique<views::BoxLayout>());
+  layout_manager->set_main_axis_alignment(
+      views::BoxLayout::MainAxisAlignment::kEnd);
+
+  SetProperty(views::kElementIdentifierKey,
+              kProductSpecificationsButtonElementId);
+
+  SetTooltipText(l10n_util::GetStringUTF16(
+      IDS_PRODUCT_SPECIFICATIONS_ENTRY_POINT_DEFAULT));
+  // TODO(b/325661685): Set accessibility name of the button.
+  SetLabelStyle(views::style::STYLE_BODY_3_EMPHASIS);
+  label()->SetElideBehavior(gfx::ElideBehavior::NO_ELIDE);
+
+  const gfx::Insets label_margin =
+      gfx::Insets().set_left(kProductSpecificationsButtonLabelMargin);
+  label()->SetProperty(views::kMarginsKey, label_margin);
+
+  SetForegroundFrameActiveColorId(kColorTabSearchButtonCRForegroundFrameActive);
+  SetForegroundFrameInactiveColorId(
+      kColorTabSearchButtonCRForegroundFrameInactive);
+  SetBackgroundFrameActiveColorId(kColorNewTabButtonCRBackgroundFrameActive);
+  SetBackgroundFrameInactiveColorId(
+      kColorNewTabButtonCRBackgroundFrameInactive);
+  SetCloseButton(base::BindRepeating(&ProductSpecificationsButton::OnDismissed,
+                                     base::Unretained(this)));
+
+  const int space_between_buttons = 2;
+  gfx::Insets margin = gfx::Insets();
+  if (before_tab_strip) {
+    margin.set_left(space_between_buttons);
+  } else {
+    margin.set_right(space_between_buttons);
+  }
+  SetProperty(views::kMarginsKey, margin);
+  SetOpacity(0);
+
+  expansion_animation_.SetTweenType(gfx::Tween::Type::ACCEL_20_DECEL_100);
+  opacity_animation_.SetTweenType(gfx::Tween::Type::LINEAR);
+
+  set_paint_transparent_for_custom_image_theme(false);
+
+  layout_manager->SetFlexForView(close_button_, 1);
+
+  UpdateColors();
+}
+
+ProductSpecificationsButton::~ProductSpecificationsButton() = default;
+
+gfx::Size ProductSpecificationsButton::CalculatePreferredSize() const {
+  const int full_width = GetLayoutManager()->GetPreferredSize(this).width();
+  const int width = full_width * width_factor_;
+  const int height = TabStripControlButton::CalculatePreferredSize().height();
+  return gfx::Size(width, height);
+}
+
+void ProductSpecificationsButton::MouseMovedOutOfHost() {
+  SetLockedExpansionMode(LockedExpansionMode::kNone);
+}
+
+void ProductSpecificationsButton::AnimationCanceled(
+    const gfx::Animation* animation) {
+  ApplyAnimationValue(animation);
+}
+
+void ProductSpecificationsButton::AnimationEnded(
+    const gfx::Animation* animation) {
+  ApplyAnimationValue(animation);
+}
+
+void ProductSpecificationsButton::AnimationProgressed(
+    const gfx::Animation* animation) {
+  ApplyAnimationValue(animation);
+}
+
+void ProductSpecificationsButton::Show() {
+  if (locked_expansion_view_->IsMouseHovered()) {
+    SetLockedExpansionMode(LockedExpansionMode::kWillShow);
+  }
+  if (locked_expansion_mode_ == LockedExpansionMode::kNone) {
+    ExecuteShow();
+  }
+}
+
+void ProductSpecificationsButton::Hide() {
+  if (locked_expansion_view_->IsMouseHovered()) {
+    SetLockedExpansionMode(LockedExpansionMode::kWillHide);
+  }
+  if (locked_expansion_mode_ == LockedExpansionMode::kNone) {
+    ExecuteHide();
+  }
+}
+
+void ProductSpecificationsButton::SetOpacity(float factor) {
+  label()->layer()->SetOpacity(factor);
+  close_button_->layer()->SetOpacity(factor);
+}
+
+int ProductSpecificationsButton::GetCornerRadius() const {
+  return kProductSpecificationsButtonCornerRadius;
+}
+
+void ProductSpecificationsButton::ApplyAnimationValue(
+    const gfx::Animation* animation) {
+  float value = animation->GetCurrentValue();
+  if (animation == &expansion_animation_) {
+    SetWidthFactor(value);
+  } else if (animation == &opacity_animation_) {
+    SetOpacity(value);
+  }
+}
+
+void ProductSpecificationsButton::ExecuteShow() {
+  expansion_animation_.SetSlideDuration(
+      GetAnimationDuration(kExpansionInDuration));
+  opacity_animation_.SetSlideDuration(GetAnimationDuration(kOpacityInDuration));
+  const base::TimeDelta delay = GetAnimationDuration(kOpacityDelay);
+  opacity_animation_delay_timer_.Start(
+      FROM_HERE, delay, this,
+      &ProductSpecificationsButton::ShowOpacityAnimation);
+
+  expansion_animation_.Show();
+
+  hide_button_timer_.Start(FROM_HERE, kShowDuration, this,
+                           &ProductSpecificationsButton::OnTimeout);
+}
+
+void ProductSpecificationsButton::ExecuteHide() {
+  expansion_animation_.SetSlideDuration(
+      GetAnimationDuration(kExpansionOutDuration));
+  expansion_animation_.Hide();
+
+  opacity_animation_.SetSlideDuration(
+      GetAnimationDuration(kOpacityOutDuration));
+  opacity_animation_.Hide();
+}
+
+void ProductSpecificationsButton::OnClicked() {
+  // TODO(b/325661685): Add implementation for this method.
+}
+
+void ProductSpecificationsButton::OnDismissed() {
+  ExecuteHide();
+}
+
+void ProductSpecificationsButton::OnTimeout() {
+  Hide();
+}
+
+void ProductSpecificationsButton::SetCloseButton(
+    views::LabelButton::PressedCallback pressed_callback) {
+  auto close_button =
+      std::make_unique<views::LabelButton>(std::move(pressed_callback));
+  close_button->SetTooltipText(l10n_util::GetStringUTF16(
+      IDS_TOOLTIP_PRODUCT_SPECIFICATIONS_ENTRY_POINT_CLOSE));
+
+  const ui::ImageModel icon_image_model = ui::ImageModel::FromVectorIcon(
+      vector_icons::kCloseChromeRefreshIcon,
+      kColorTabSearchButtonCRForegroundFrameActive,
+      kProductSpecificationsCloseButtonSize);
+
+  close_button->SetImageModel(views::Button::STATE_NORMAL, icon_image_model);
+  close_button->SetImageModel(views::Button::STATE_HOVERED, icon_image_model);
+  close_button->SetImageModel(views::Button::STATE_PRESSED, icon_image_model);
+
+  close_button->SetPaintToLayer();
+  close_button->layer()->SetFillsBoundsOpaquely(false);
+
+  views::InkDrop::Get(close_button.get())
+      ->SetMode(views::InkDropHost::InkDropMode::ON);
+  views::InkDrop::Get(close_button.get())->SetHighlightOpacity(0.16f);
+  views::InkDrop::Get(close_button.get())->SetVisibleOpacity(0.14f);
+  views::InkDrop::Get(close_button.get())
+      ->SetBaseColorId(kColorTabSearchButtonCRForegroundFrameActive);
+
+  auto ink_drop_highlight_path =
+      std::make_unique<views::CircleHighlightPathGenerator>(gfx::Insets());
+  views::HighlightPathGenerator::Install(close_button.get(),
+                                         std::move(ink_drop_highlight_path));
+
+  close_button->SetPreferredSize(
+      gfx::Size(kProductSpecificationsCloseButtonSize,
+                kProductSpecificationsCloseButtonSize));
+  close_button->SetBorder(nullptr);
+
+  const gfx::Insets margin =
+      gfx::Insets().set_left_right(kProductSpecificationsCloseButtonMargin,
+                                   kProductSpecificationsCloseButtonMargin);
+  close_button->SetProperty(views::kMarginsKey, margin);
+
+  close_button_ = AddChildView(std::move(close_button));
+}
+
+void ProductSpecificationsButton::SetLockedExpansionMode(
+    LockedExpansionMode mode) {
+  if (mode == LockedExpansionMode::kNone) {
+    if (locked_expansion_mode_ == LockedExpansionMode::kWillShow) {
+      ExecuteShow();
+    } else if (locked_expansion_mode_ == LockedExpansionMode::kWillHide) {
+      ExecuteHide();
+    }
+  } else {
+    mouse_watcher_->Start(GetWidget()->GetNativeWindow());
+  }
+  locked_expansion_mode_ = mode;
+}
+
+void ProductSpecificationsButton::SetWidthFactor(float factor) {
+  width_factor_ = factor;
+  PreferredSizeChanged();
+}
+
+void ProductSpecificationsButton::ShowOpacityAnimation() {
+  opacity_animation_.Show();
+}
+
+base::TimeDelta ProductSpecificationsButton::GetAnimationDuration(
+    base::TimeDelta duration) {
+  return gfx::Animation::ShouldRenderRichAnimation() ? duration
+                                                     : base::TimeDelta();
+}
+
+BEGIN_METADATA(ProductSpecificationsButton)
+END_METADATA
