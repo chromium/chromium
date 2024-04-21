bb18afe94a6c62f1378a2b8a6ab6a574233f05c5
Michael Checo <michaelcheco@google.com>
2024-04-21 01:44:49 +0000

    diagnostics: Convert cpu_card_test to TS
    Bug: b/245336251
    Change-Id: I2aa553fa761afa9a04330a695692b6ca0e8a2467
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450663
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290391}

--- chrome/test/data/webui/chromeos/diagnostics/cpu_card_test.ts
+++ chrome/test/data/webui/chromeos/diagnostics/cpu_card_test.ts
@@ -8,60 +8,48 @@ import 'chrome://webui-test/chromeos/mojo_webui_test_support.js';
 import {CpuCardElement} from 'chrome://diagnostics/cpu_card.js';
 import {fakeCpuUsage, fakeMemoryUsage, fakeSystemInfo} from 'chrome://diagnostics/fake_data.js';
 import {FakeSystemDataProvider} from 'chrome://diagnostics/fake_system_data_provider.js';
-import {getSystemDataProvider, setSystemDataProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
+import {setSystemDataProviderForTesting} from 'chrome://diagnostics/mojo_interface_provider.js';
 import {RoutineSectionElement} from 'chrome://diagnostics/routine_section.js';
-import {CpuUsage, SystemDataProviderInterface, SystemInfo} from 'chrome://diagnostics/system_data_provider.mojom-webui.js';
-import {RoutineType} from 'chrome://diagnostics/system_routine_controller.mojom-webui.js';
+import {CpuUsage, MemoryUsage, SystemInfo} from 'chrome://diagnostics/system_data_provider.mojom-webui.js';
 import {loadTimeData} from 'chrome://resources/ash/common/load_time_data.m.js';
+import {strictQuery} from 'chrome://resources/ash/common/typescript_utils/strict_query.js';
+import {assert} from 'chrome://resources/js/assert.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
-
-import {isChildVisible, isVisible} from '../test_util.js';
+import {isChildVisible, isVisible} from 'chrome://webui-test/test_util.js';
 
 import * as dx_utils from './diagnostics_test_utils.js';
 
 suite('cpuCardTestSuite', function() {
-  /** @type {?CpuCardElement} */
-  let cpuElement = null;
+  let cpuElement: CpuCardElement|null = null;
 
-  /** @type {?FakeSystemDataProvider} */
-  let provider = null;
+  const provider = new FakeSystemDataProvider();
 
   suiteSetup(() => {
-    provider = new FakeSystemDataProvider();
     setSystemDataProviderForTesting(provider);
   });
 
   setup(() => {
-    document.body.innerHTML = window.trustedTypes.emptyHTML;
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
   });
 
   teardown(() => {
-    if (cpuElement) {
-      cpuElement.remove();
-    }
+    cpuElement?.remove();
     cpuElement = null;
     provider.reset();
   });
 
-  /**
-   * @param {!Array<!CpuUsage>} cpuUsage
-   * @param {!SystemInfo} systemInfo
-   * @param {!Array<MemoryUsage>} memoryUsage
-   * @return {!Promise}
-   */
-  function initializeCpuCard(cpuUsage, systemInfo, memoryUsage) {
-    assertFalse(!!cpuElement);
-
+  function initializeCpuCard(
+      cpuUsage: CpuUsage[], systemInfo: SystemInfo,
+      memoryUsage: MemoryUsage[]): Promise<void> {
     // Initialize the fake data.
     provider.setFakeCpuUsage(cpuUsage);
     provider.setFakeSystemInfo(systemInfo);
     provider.setFakeMemoryUsage(memoryUsage);
 
     // Add the CPU card to the DOM.
-    cpuElement =
-        /** @type {!CpuCardElement} */ (document.createElement('cpu-card'));
-    assertTrue(!!cpuElement);
+    cpuElement = document.createElement('cpu-card');
+    assert(cpuElement);
     document.body.appendChild(cpuElement);
 
     return flushTasks();
@@ -69,31 +57,27 @@ suite('cpuCardTestSuite', function() {
 
   /**
    * Returns the routine-section from the card.
-   * @return {!RoutineSectionElement}
    */
-  function getRoutineSection() {
-    const routineSection =
-        /** @type {!RoutineSectionElement} */ (
-            cpuElement.shadowRoot.querySelector('routine-section'));
-    assertTrue(!!routineSection);
+  function getRoutineSection(): RoutineSectionElement {
+    const routineSection = strictQuery(
+        'routine-section', cpuElement!.shadowRoot, RoutineSectionElement);
+    assert(routineSection);
     return routineSection;
   }
 
   /**
    * Returns the Run Tests button from inside the routine-section.
-   * @return {!HTMLElement}
    */
-  function getRunTestsButton() {
+  function getRunTestsButton(): HTMLButtonElement {
     const button = dx_utils.getRunTestsButtonFromSection(getRoutineSection());
-    assertTrue(!!button);
+    assert(button);
     return button;
   }
 
   /**
    * Returns whether the run tests button is disabled.
-   * @return {boolean}
    */
-  function isRunTestsButtonDisabled() {
+  function isRunTestsButtonDisabled(): boolean {
     return getRunTestsButton().disabled;
   }
 
@@ -108,32 +92,35 @@ suite('cpuCardTestSuite', function() {
           dx_utils.assertTextContains(
               dx_utils.getDataPointValue(cpuElement, '#cpuUsageUser'),
               `${
-                  fakeCpuUsage[0].percentUsageUser +
-                  fakeCpuUsage[0].percentUsageSystem}`);
+                  fakeCpuUsage[0]!.percentUsageUser +
+                  fakeCpuUsage[0]!.percentUsageSystem}`);
           dx_utils.assertTextContains(
               dx_utils.getDataPoint(cpuElement, '#cpuUsageUser').tooltipText,
               loadTimeData.getStringF('cpuUsageTooltipText', 4));
           dx_utils.assertTextContains(
               dx_utils.getDataPointValue(cpuElement, '#cpuTemp'),
-              `${fakeCpuUsage[0].averageCpuTempCelsius}`);
+              `${fakeCpuUsage[0]!.averageCpuTempCelsius}`);
 
-          const convertkhzToGhz = (num) => parseFloat(num / 1000000).toFixed(2);
+          const convertkhzToGhz = (num: number) =>
+              parseFloat(`${num / 1000000}`).toFixed(2);
           dx_utils.assertTextContains(
               dx_utils.getDataPointValue(cpuElement, '#cpuSpeed'),
-              `${convertkhzToGhz(fakeCpuUsage[0].scalingCurrentFrequencyKhz)}`);
+              `${
+                  convertkhzToGhz(
+                      fakeCpuUsage[0]!.scalingCurrentFrequencyKhz)}`);
           dx_utils.assertElementContainsText(
-              cpuElement.shadowRoot.querySelector('#cpuChipInfo'),
+              cpuElement!.shadowRoot!.querySelector('#cpuChipInfo'),
               `${fakeSystemInfo.cpuModelName}`);
           dx_utils.assertElementContainsText(
-              cpuElement.shadowRoot.querySelector('#cpuChipInfo'),
+              cpuElement!.shadowRoot!.querySelector('#cpuChipInfo'),
               `${fakeSystemInfo.cpuThreadsCount}`);
           dx_utils.assertElementContainsText(
-              cpuElement.shadowRoot.querySelector('#cpuChipInfo'),
+              cpuElement!.shadowRoot!.querySelector('#cpuChipInfo'),
               `${fakeSystemInfo.cpuMaxClockSpeedKhz}`);
 
           const cpuChart = dx_utils.getRealtimeCpuChartElement(cpuElement);
-          assertEquals(fakeCpuUsage[0].percentUsageUser, cpuChart.user);
-          assertEquals(fakeCpuUsage[0].percentUsageSystem, cpuChart.system);
+          assertEquals(fakeCpuUsage[0]!.percentUsageUser, cpuChart.user);
+          assertEquals(fakeCpuUsage[0]!.percentUsageSystem, cpuChart.system);
 
           // Verify the routine section is in the page.
           assertTrue(!!getRoutineSection());
@@ -171,8 +158,8 @@ suite('cpuCardTestSuite', function() {
               routineSectionElement.additionalMessage,
               loadTimeData.getString('notEnoughAvailableMemoryCpuMessage'));
           assertTrue(isRunTestsButtonDisabled());
-          assertTrue(isVisible(/** @type {!HTMLElement} */ (
-              routineSectionElement.shadowRoot.querySelector('#messageIcon'))));
+          assertTrue(isVisible(
+              routineSectionElement.shadowRoot!.querySelector('#messageIcon')));
         });
   });
 });
