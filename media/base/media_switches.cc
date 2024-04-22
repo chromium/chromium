8ea3193f40fd0be3b577f9b3cf611f9d53f86abe
Saifuddin Hitawala <hitawala@chromium.org>
2024-04-22 17:59:28 +0000

    Multiplanar: Launch software decode on Linux, ChromeOS
    Launch multiplanar SI for software video decode on ChromeOS, UMA for
    50% stable looks good [1].
    
    Also launch multiplanar SI for software video decode on Linux, UMA
    for 50% stable looks good [2].
    
    [1] https://uma.googleplex.com/p/chrome/variations?sid=fb9629484f5dae1ab4018076820d37d5
    [2] https://uma.googleplex.com/p/chrome/variations?sid=9f0ef2298bed9e9cfe003f5092e07db5
    
    Bug: 40239769
    Change-Id: I75de9767561bd231079873e61ad1c318fb1a54ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473085
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290761}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -597,7 +597,7 @@ BASE_FEATURE(kUseMultiPlaneFormatForHardwareVideo,
 BASE_FEATURE(kUseMultiPlaneFormatForSoftwareVideo,
              "UseMultiPlaneFormatForSoftwareVideo",
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_FUCHSIA) || \
-    BUILDFLAG(IS_WIN)
+    BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
              base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
