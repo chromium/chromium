6e5638155ef0a7d616f89780ea4c08509c562fca
Joe Downing <joedow@chromium.org>
2024-04-22 17:24:54 +0000

    Don't advertise multistream for ChromeOS
    It looks like there is a crash when selecting source for ChromeOS with multistream is enabled. Since M125 just branched, I think we should make a change so the capability is not advertised and then fix the issue in M126.
    
    (cherry picked from commit b831795bf5e54a69b244c8d10ca0684402fe5494)
    
    Change-Id: I910bc232dbb9992c8f2b3daa0c313e85e4239d95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464329
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1290105}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473523
    Cr-Commit-Position: refs/branch-heads/6422@{#218}
    Cr-Branched-From: 9012208d0ce02e0cf0adb9b62558627c356f3278-refs/heads/main@{#1287751}

--- remoting/host/it2me_desktop_environment.cc
+++ remoting/host/it2me_desktop_environment.cc
@@ -105,8 +105,11 @@ std::string It2MeDesktopEnvironment::GetCapabilities() const {
 
   // TODO: joedow - Move MultiStream capability to a shared base
   // class once all platforms and connection modes support it.
+  // TODO: b/335873769 - Fix crash in ChromeOS when multi-stream is enabled.
+#if !BUILDFLAG(IS_CHROMEOS)
   capabilities += " ";
   capabilities += protocol::kMultiStreamCapability;
+#endif
 
   return capabilities;
 }
