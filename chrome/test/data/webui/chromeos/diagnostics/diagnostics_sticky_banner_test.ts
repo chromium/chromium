874a962cbc6b719de052e44a5ccea79b714196e8
Michael Checo <michaelcheco@google.com>
2024-04-23 22:52:29 +0000

    diagnostics: Convert diagnostics_sticky_banner_test to TS
    Bug: 245336251
    Change-Id: Ib9ce937a982c50085a23900765fd49e0a1993886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455309
    Reviewed-by: Camden Bickel <cambickel@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291587}

--- chrome/test/data/webui/chromeos/diagnostics/diagnostics_sticky_banner_test.ts
+++ chrome/test/data/webui/chromeos/diagnostics/diagnostics_sticky_banner_test.ts
@@ -3,80 +3,64 @@
 // found in the LICENSE file.
 
 import 'chrome://webui-test/chromeos/mojo_webui_test_support.js';
+import 'chrome://diagnostics/diagnostics_sticky_banner.js';
 
 import {DiagnosticsStickyBannerElement} from 'chrome://diagnostics/diagnostics_sticky_banner.js';
+import {strictQuery} from 'chrome://resources/ash/common/typescript_utils/strict_query.js';
+import {assert} from 'chrome://resources/js/assert.js';
 import {assertEquals, assertFalse, assertNotEquals, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
-
-import {isVisible} from '../test_util.js';
+import {isVisible} from 'chrome://webui-test/test_util.js';
 
 import * as dx_utils from './diagnostics_test_utils.js';
 
 suite('diagnosticsStickyBannerTestSuite', function() {
-  /** @type {?DiagnosticsStickyBannerElement} */
-  let diagnosticsStickyBannerElement = null;
+  let diagnosticsStickyBannerElement: DiagnosticsStickyBannerElement|null =
+      null;
 
   setup(() => {
-    document.body.innerHTML = window.trustedTypes.emptyHTML;
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
   });
 
   teardown(() => {
-    diagnosticsStickyBannerElement.remove();
+    diagnosticsStickyBannerElement?.remove();
     diagnosticsStickyBannerElement = null;
   });
 
-  /** @return {!Promise} */
-  function initializeDiagnosticsStickyBanner() {
-    assertFalse(!!diagnosticsStickyBannerElement);
-
+  function initializeDiagnosticsStickyBanner(): Promise<void> {
     // Add the sticky banner to the DOM.
     diagnosticsStickyBannerElement =
-        /** @type {!DiagnosticsStickyBannerElement} */ (
-            document.createElement('diagnostics-sticky-banner'));
-    assertTrue(!!diagnosticsStickyBannerElement);
+        document.createElement('diagnostics-sticky-banner');
+    assert(diagnosticsStickyBannerElement);
     document.body.appendChild(diagnosticsStickyBannerElement);
 
     return flushTasks();
   }
 
-  /** @return {!Element} */
-  function getBanner() {
-    assertTrue(!!diagnosticsStickyBannerElement);
-
-    return /** @type {!Element} */ (
-        diagnosticsStickyBannerElement.shadowRoot.querySelector('#banner'));
+  function getBanner(): Element {
+    assert(diagnosticsStickyBannerElement);
+    return strictQuery(
+        '#banner', diagnosticsStickyBannerElement.shadowRoot, Element);
   }
 
-  /** @return {!Element} */
   function getBannerMsg() {
-    assertTrue(!!diagnosticsStickyBannerElement);
-
-    return /** @type {!Element} */ (
-        diagnosticsStickyBannerElement.shadowRoot.querySelector('#bannerMsg'));
+    assert(diagnosticsStickyBannerElement);
+    return strictQuery(
+        '#bannerMsg', diagnosticsStickyBannerElement.shadowRoot, Element);
   }
 
-  /**
-   * @suppress {visibility}
-   * @return {string}
-   */
-  function getScrollClass_() {
-    return diagnosticsStickyBannerElement.scrollingClass;
+  function getScrollClass(): string {
+    assert(diagnosticsStickyBannerElement);
+    return diagnosticsStickyBannerElement.getScrollingClassForTesting();
   }
 
-  /**
-   * @suppress {visibility}
-   * @return {number}
-   */
-  function getScrollTimerId_() {
-    return diagnosticsStickyBannerElement.scrollTimerId;
+  function getScrollTimerId(): number {
+    assert(diagnosticsStickyBannerElement);
+    return diagnosticsStickyBannerElement.getScrollTimerIdForTesting();
   }
 
-  /**
-   * @param {string} message
-   * @return {!Promise}
-   */
-  function setBannerMessage(message) {
-    assertTrue(!!diagnosticsStickyBannerElement);
+  function setBannerMessage(message: string): Promise<void> {
+    assert(diagnosticsStickyBannerElement);
     diagnosticsStickyBannerElement.bannerMessage = message;
 
     return flushTasks();
@@ -84,9 +68,8 @@ suite('diagnosticsStickyBannerTestSuite', function() {
 
   /**
    * Triggers 'dismiss-caution-banner' custom event.
-   * @return {!Promise}
    */
-  function triggerDismissBannerEvent() {
+  function triggerDismissBannerEvent(): Promise<void> {
     window.dispatchEvent(new CustomEvent('dismiss-caution-banner', {
       bubbles: true,
       composed: true,
@@ -97,9 +80,8 @@ suite('diagnosticsStickyBannerTestSuite', function() {
 
   /**
    * Triggers 'scroll' event.
-   * @return {!Promise}
    */
-  function triggerScrollEvent() {
+  function triggerScrollEvent(): Promise<void> {
     window.dispatchEvent(new CustomEvent('scroll', {
       bubbles: true,
       composed: true,
@@ -111,10 +93,8 @@ suite('diagnosticsStickyBannerTestSuite', function() {
   /**
    * Triggers 'show-caution-banner' custom event with correctly configured event
    * detail object based on provided message.
-   * @param {string} message
-   * @return {!Promise}
    */
-  function triggerShowBannerEvent(message) {
+  function triggerShowBannerEvent(message: string): Promise<void> {
     window.dispatchEvent(new CustomEvent('show-caution-banner', {
       bubbles: true,
       composed: true,
@@ -151,6 +131,7 @@ suite('diagnosticsStickyBannerTestSuite', function() {
     return initializeDiagnosticsStickyBanner()
         .then(() => triggerShowBannerEvent(bannerText1))
         .then(() => {
+          assert(diagnosticsStickyBannerElement);
           assertEquals(
               bannerText1, diagnosticsStickyBannerElement.bannerMessage);
           assertTrue(isVisible(getBanner()));
@@ -158,6 +139,7 @@ suite('diagnosticsStickyBannerTestSuite', function() {
         })
         .then(() => triggerShowBannerEvent(bannerText2))
         .then(() => {
+          assert(diagnosticsStickyBannerElement);
           assertEquals(
               bannerText2, diagnosticsStickyBannerElement.bannerMessage);
           assertTrue(isVisible(getBanner()));
@@ -174,6 +156,7 @@ suite('diagnosticsStickyBannerTestSuite', function() {
         })
         .then(() => triggerDismissBannerEvent())
         .then(() => {
+          assert(diagnosticsStickyBannerElement);
           assertEquals('', diagnosticsStickyBannerElement.bannerMessage);
           assertFalse(isVisible(getBanner()));
           dx_utils.assertElementDoesNotContainText(getBannerMsg(), testMessage);
@@ -183,27 +166,27 @@ suite('diagnosticsStickyBannerTestSuite', function() {
   test('BannerHandlesScrollEvent', () => {
     return initializeDiagnosticsStickyBanner()
         .then(() => {
-          dx_utils.assertTextDoesNotContain(getScrollClass_(), 'elevation-2');
-          assertEquals(-1, getScrollTimerId_());
+          dx_utils.assertTextDoesNotContain(getScrollClass(), 'elevation-2');
+          assertEquals(-1, getScrollTimerId());
         })
         .then(() => triggerScrollEvent())
         // Do not update if no banner message is set.
         .then(() => {
-          dx_utils.assertTextDoesNotContain(getScrollClass_(), 'elevation-2');
-          assertEquals(-1, getScrollTimerId_());
+          dx_utils.assertTextDoesNotContain(getScrollClass(), 'elevation-2');
+          assertEquals(-1, getScrollTimerId());
         })
         .then(() => setBannerMessage('Test Message'))
         .then(() => triggerScrollEvent())
         // First scroll initializes but does not update class.
         .then(() => {
-          dx_utils.assertTextDoesNotContain(getScrollClass_(), 'elevation-2');
-          assertNotEquals(-1, getScrollTimerId_());
+          dx_utils.assertTextDoesNotContain(getScrollClass(), 'elevation-2');
+          assertNotEquals(-1, getScrollTimerId());
         })
         // Subsequent scrolls ensure class name is set.
         .then(() => triggerScrollEvent())
         .then(() => {
-          dx_utils.assertTextContains(getScrollClass_(), 'elevation-2');
-          assertNotEquals(-1, getScrollTimerId_());
+          dx_utils.assertTextContains(getScrollClass(), 'elevation-2');
+          assertNotEquals(-1, getScrollTimerId());
         });
   });
 });
