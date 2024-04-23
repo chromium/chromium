2cc1bc0c15e1699a3a7f180e19dad21fd9c802b5
Colin Blundell <blundell@chromium.org>
2024-04-23 13:44:47 +0000

    [//gpu] Eliminate RasterImplementationGLES handling legacy mailboxes
    The only non-SharedImage mailboxes that can still be created are
    exclusively for SharedBitmaps, which are used only with the software
    compositor.
    
    Note: If RasterImplementationGLES::CopySharedImage() or
    CreateAndConsumeForGpuRaster() are passed an invalid mailbox, a
    service-side error will be generated. This preserves the high-level
    current behavior in this case, since there is no way to produce a
    texture for a non-SharedImage mailbox and hence
    CreateAndConsumeTextureCHROMIUM() will raise an error.
    
    Bug: 40057997
    Change-Id: Ibaaa2d0a8a0b2e43fd52b87e297be17f74a7d01a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5471633
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291232}

--- gpu/command_buffer/client/raster_implementation_gles.cc
+++ gpu/command_buffer/client/raster_implementation_gles.cc
@@ -169,14 +169,11 @@ void RasterImplementationGLES::CopySharedImage(
     GLsizei height,
     GLboolean unpack_flip_y,
     GLboolean unpack_premultiply_alpha) {
-  // CopySharedImage does not support legacy mailboxes so fallback to
-  // CopySubTexture.
   // We don't know if this would require rgb to yuv or yuv to rgb conversion, so
   // we check for both flags, but in reality validating command decoder doesn't
   // support either and passthrough command decoder always supports both.
   if (capabilities_.supports_yuv_to_rgb_conversion &&
-      capabilities_.supports_rgb_to_yuv_conversion &&
-      source_mailbox.IsSharedImage() && dest_mailbox.IsSharedImage()) {
+      capabilities_.supports_rgb_to_yuv_conversion) {
     if (width < 0) {
       LOG(ERROR) << "GL_INVALID_VALUE, glCopySharedImage, width < 0";
       return;
@@ -554,9 +551,7 @@ bool RasterImplementationGLES::ReadbackImagePixels(
 
 GLuint RasterImplementationGLES::CreateAndConsumeForGpuRaster(
     const gpu::Mailbox& mailbox) {
-  return mailbox.IsSharedImage()
-             ? gl_->CreateAndTexStorage2DSharedImageCHROMIUM(mailbox.name)
-             : gl_->CreateAndConsumeTextureCHROMIUM(mailbox.name);
+  return gl_->CreateAndTexStorage2DSharedImageCHROMIUM(mailbox.name);
 }
 
 GLuint RasterImplementationGLES::CreateAndConsumeForGpuRaster(
