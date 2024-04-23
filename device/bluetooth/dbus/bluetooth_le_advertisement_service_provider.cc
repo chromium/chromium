33ac4ba5516076c43543c30fc1a87a34eb5405da
Brittany Hartmire <bhartmire@google.com>
2024-04-23 16:04:26 +0000

    Revert "Add default secondary channel for Ext Adv on BlueZ"
    This reverts commit 0f7a78c5843c9a68114e891675027945b0dd0364.
    
    Reason for revert: This change breaks NearbyConnections advertising
    for Quick Start. See b/336354178.
    
    Original change's description:
    > Add default secondary channel for Ext Adv on BlueZ
    >
    > BlueZ API requires Secondary Channel to be provided in Extended
    > Advertisement. Otherwise, legacy advertisement setting will be used.
    >
    > in advertisement object via dbus-monitor.
    >
    > Bug: 332570629
    > Test: Enable ble nearby v2 and verify chrome includes SecondaryChannel
    > Test: Disable ble nearby v2 and verify nearby share works
    > Change-Id: I5c45d893ba33eb9bcddc03fef60312c7a5fcc11c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446391
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Ying Hsu <yinghsu@chromium.org>
    > Commit-Queue: Ying Hsu <yinghsu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1289798}
    
    Bug: 332570629
    Change-Id: If568bbdcf2ad36a177781502154346d531523961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5474490
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Ying Hsu <yinghsu@chromium.org>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291320}

--- device/bluetooth/dbus/bluetooth_le_advertisement_service_provider.cc
+++ device/bluetooth/dbus/bluetooth_le_advertisement_service_provider.cc
@@ -178,10 +178,6 @@ class BluetoothAdvertisementServiceProviderImpl
                scan_response_data_) {
       writer.OpenVariant("o", &variant_writer);
       AppendScanResponseDataVariant(&variant_writer);
-    } else if ((property_name ==
-                bluetooth_advertisement::kSecondaryChannelProperty)) {
-      writer.OpenVariant("s", &variant_writer);
-      variant_writer.AppendString(bluetooth_advertisement::kPhy1M);
     } else {
       std::unique_ptr<dbus::ErrorResponse> error_response =
           dbus::ErrorResponse::FromMethodCall(
@@ -251,7 +247,6 @@ class BluetoothAdvertisementServiceProviderImpl
     writer.OpenArray("{sv}", &array_writer);
 
     AppendType(&array_writer);
-    AppendSecondaryChannel(&array_writer);
     AppendServiceUUIDs(&array_writer);
     AppendManufacturerData(&array_writer);
     AppendSolicitUUIDs(&array_writer);
@@ -297,15 +292,6 @@ class BluetoothAdvertisementServiceProviderImpl
     } else {
       dict_entry_writer.AppendVariantOfString("peripheral");
     }
-    array_writer->CloseContainer(&dict_entry_writer);
-  }
-
-  void AppendSecondaryChannel(dbus::MessageWriter* array_writer) {
-    dbus::MessageWriter dict_entry_writer(NULL);
-    array_writer->OpenDictEntry(&dict_entry_writer);
-    dict_entry_writer.AppendString(
-        bluetooth_advertisement::kSecondaryChannelProperty);
-    dict_entry_writer.AppendVariantOfString(bluetooth_advertisement::kPhy1M);
     array_writer->CloseContainer(&dict_entry_writer);
   }
 
