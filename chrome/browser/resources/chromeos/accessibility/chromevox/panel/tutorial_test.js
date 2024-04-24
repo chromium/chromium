3627bb236eae749053665e26bf4d63c47efe0215
Hayato Ito <hayato@chromium.org>
2024-04-24 08:13:11 +0000

    Gardening: Disable a flaky test
    Disable ChromeVoxTutorialTest.ResourcesTest.
    
    Bug: 336702956
    Change-Id: I78b759710721d8a76e5d1efc5d9c26a08503d82f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5481610
    Auto-Submit: Hayato Ito <hayato@chromium.org>
    Owners-Override: Hayato Ito <hayato@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291750}

--- chrome/browser/resources/chromeos/accessibility/chromevox/panel/tutorial_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/panel/tutorial_test.js
@@ -567,7 +567,9 @@ AX_TEST_F('ChromeVoxTutorialTest', 'RestartNudges', async function() {
 });
 
 // Tests that the tutorial closes and ChromeVox navigates to a resource link.
-AX_TEST_F('ChromeVoxTutorialTest', 'ResourcesTest', async function() {
+//
+// Flaky. See crbug.com/336702956.
+AX_TEST_F('ChromeVoxTutorialTest', 'DISABLED_ResourcesTest', async function() {
   const mockFeedback = this.createMockFeedback();
   const root = await this.runWithLoadedTree(this.simpleDoc);
   await this.launchAndWaitForTutorial();
