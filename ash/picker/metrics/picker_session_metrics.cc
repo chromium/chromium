a44812c4e96868b4542a1da861eafd82b378fa53
Jihad Hanna <jihadghanna@google.com>
2024-04-24 12:45:25 +0000

    Revert "picker: add metrics for picker start session event"
    This reverts commit 38779eb72240033519ac50acf8ac0c7976254450.
    
    Reason for revert:
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/36291/overview 
    
    Original change's description:
    > picker: add metrics for picker start session event
    >
    > Bug: b:322927522
    > Change-Id: Idcdedaf53c19c0dd9c11161119a120bd0d04c487
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464640
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Jong Ahn <jongahn@chromium.org>
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Jing Wang <jiwan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1291618}
    
    Bug: b:322927522
    Change-Id: Ib14cd23e75beb5fb57aa144fd3b24da58b0e8dd0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5481573
    Owners-Override: Jihad Hanna <jihadghanna@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291827}

--- ash/picker/metrics/picker_session_metrics.cc
+++ ash/picker/metrics/picker_session_metrics.cc
@@ -6,70 +6,8 @@
 
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
-#include "components/metrics/structured/structured_events.h"
-#include "components/metrics/structured/structured_metrics_client.h"
-#include "ui/base/ime/text_input_client.h"
 
 namespace ash {
-namespace {
-
-namespace ms = metrics::structured;
-namespace cros_events = metrics::structured::events::v2::cr_os_events;
-
-cros_events::PickerInputFieldType GetInputFieldType(
-    ui::TextInputClient* client) {
-  if (client == nullptr) {
-    return cros_events::PickerInputFieldType::NONE;
-  }
-  switch (client->GetTextInputType()) {
-    case ui::TEXT_INPUT_TYPE_NONE:
-      return cros_events::PickerInputFieldType::NONE;
-    case ui::TEXT_INPUT_TYPE_TEXT:
-    case ui::TEXT_INPUT_TYPE_TEXT_AREA:
-      if (client->CanInsertImage()) {
-        return cros_events::PickerInputFieldType::RICH_TEXT;
-      } else {
-        return cros_events::PickerInputFieldType::PLAIN_TEXT;
-      }
-    case ui::TEXT_INPUT_TYPE_PASSWORD:
-      return cros_events::PickerInputFieldType::PASSWORD;
-    case ui::TEXT_INPUT_TYPE_SEARCH:
-      return cros_events::PickerInputFieldType::SEARCH;
-    case ui::TEXT_INPUT_TYPE_EMAIL:
-      return cros_events::PickerInputFieldType::EMAIL;
-    case ui::TEXT_INPUT_TYPE_NUMBER:
-      return cros_events::PickerInputFieldType::NUMBER;
-    case ui::TEXT_INPUT_TYPE_TELEPHONE:
-      return cros_events::PickerInputFieldType::TELEPHONE;
-    case ui::TEXT_INPUT_TYPE_URL:
-      return cros_events::PickerInputFieldType::URL;
-    case ui::TEXT_INPUT_TYPE_DATE:
-    case ui::TEXT_INPUT_TYPE_DATE_TIME:
-    case ui::TEXT_INPUT_TYPE_DATE_TIME_LOCAL:
-    case ui::TEXT_INPUT_TYPE_MONTH:
-    case ui::TEXT_INPUT_TYPE_TIME:
-    case ui::TEXT_INPUT_TYPE_WEEK:
-    case ui::TEXT_INPUT_TYPE_DATE_TIME_FIELD:
-      return cros_events::PickerInputFieldType::DATE_TIME;
-    case ui::TEXT_INPUT_TYPE_CONTENT_EDITABLE:
-    case ui::TEXT_INPUT_TYPE_NULL:
-      return cros_events::PickerInputFieldType::OTHER;
-  }
-}
-
-int GetSelectionLength(ui::TextInputClient* client) {
-  if (!client) {
-    return 0;
-  }
-  gfx::Range selection_range;
-  client->GetEditableSelectionRange(&selection_range);
-  if (selection_range.IsValid() && !selection_range.is_empty()) {
-    return selection_range.length();
-  }
-  return 0;
-}
-
-}  // namespace
 
 PickerSessionMetrics::PickerSessionMetrics() = default;
 
@@ -88,13 +26,6 @@ void PickerSessionMetrics::RecordOutcome(SessionOutcome outcome) {
 
   base::UmaHistogramEnumeration("Ash.Picker.Session.Outcome", outcome);
   recorded_outcome_ = true;
-}
-
-void PickerSessionMetrics::OnStartSession(ui::TextInputClient* client) {
-  ms::StructuredMetricsClient::Record(
-      cros_events::Picker_StartSession()
-          .SetInputFieldType(GetInputFieldType(client))
-          .SetSelectionLength(GetSelectionLength(client)));
 }
 
 }  // namespace ash
--- ash/picker/metrics/picker_session_metrics.h
+++ ash/picker/metrics/picker_session_metrics.h
@@ -9,10 +9,6 @@
 
 #include "ash/ash_export.h"
 
-namespace ui {
-class TextInputClient;
-}  // namespace ui
-
 namespace ash {
 
 // Records metrics for a session of using Picker.
@@ -34,9 +30,6 @@ class ASH_EXPORT PickerSessionMetrics {
   ~PickerSessionMetrics();
 
   void RecordOutcome(SessionOutcome outcome);
-
-  // Records CrOS event metrics when a picker session starts.
-  void OnStartSession(ui::TextInputClient* client);
 
  private:
   // Whether the outcome of this session has been recorded.
