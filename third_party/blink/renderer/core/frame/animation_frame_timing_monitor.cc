2931c641ca38226a1881fd6e52cbc328a9225cab
Aoyuan Zuo <zuoaoyuan@chromium.org>
2024-04-24 18:49:02 +0000

    3P Script Detector Share Between Iframes
    * Previous undesired outcome: Script window can be null and dereference
      nullptr results in crash.
    * Root cause: We suspect script from iframe could have their dom window
      detached by the time animation_frame_timing_monitor doing 3p script
      detection after BeginMainFrame.
    * This CL address it by: use local root frame's window instead to
      retrieve 3p script detector; also added a check to early exit just in
      case.
    
    Bug: 336375355, 336503423, 336517147
    Change-Id: I4e21e0b133fae2e52f50b42c7f42d21848e17eb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5479330
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Aoyuan Zuo <zuoaoyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292012}

--- third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
+++ third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
@@ -71,7 +71,8 @@ void AnimationFrameTimingMonitor::WillPerformStyleAndLayoutCalculation() {
       base::TimeTicks::Now());
 }
 
-void AnimationFrameTimingMonitor::DidBeginMainFrame() {
+void AnimationFrameTimingMonitor::DidBeginMainFrame(
+    LocalDOMWindow& local_root_window) {
   // This can happen if the AnimationFrameTimingMonitor instance is created
   // in the middle of a frame.
   if (!current_frame_timing_info_) {
@@ -114,7 +115,8 @@ void AnimationFrameTimingMonitor::DidBeginMainFrame() {
 
     client_.ReportLongAnimationFrameTiming(current_frame_timing_info_);
   }
-  RecordLongAnimationFrameUKMAndTrace(*current_frame_timing_info_);
+  RecordLongAnimationFrameUKMAndTrace(*current_frame_timing_info_,
+                                      local_root_window);
 
   first_ui_event_timestamp_ = base::TimeTicks();
   current_frame_timing_info_.Clear();
@@ -235,10 +237,7 @@ void AnimationFrameTimingMonitor::OnTaskCompleted(
     DOMWindowPerformance::performance(*frame->DomWindow())
         ->ReportLongAnimationFrameTiming(timing_info);
   }
-
-  if (frame->IsMainFrame()) {
-    RecordLongAnimationFrameUKMAndTrace(*timing_info);
-  }
+  RecordLongAnimationFrameUKMAndTrace(*timing_info, *frame->DomWindow());
 }
 
 namespace {
@@ -279,7 +278,8 @@ void RecordLongAnimationFrameTrace(const AnimationFrameTimingInfo& info,
 }  // namespace
 
 void AnimationFrameTimingMonitor::RecordLongAnimationFrameUKMAndTrace(
-    const AnimationFrameTimingInfo& info) {
+    const AnimationFrameTimingInfo& info,
+    LocalDOMWindow& window) {
   // Record all animation frames to traces, but only long ones to UKM.
   RecordLongAnimationFrameTrace(info, this);
   if (info.Duration() < kLongAnimationFrameDuration) {
@@ -316,8 +316,8 @@ void AnimationFrameTimingMonitor::RecordLongAnimationFrameUKMAndTrace(
     total_forced_style_and_layout_duration += script->StyleDuration();
     total_forced_style_and_layout_duration += script->LayoutDuration();
     ThirdPartyScriptDetector::Technology third_party_technology =
-        ThirdPartyScriptDetector::From(*(script->Window()))
-            .Detect(script->GetSourceLocation().url);
+        ThirdPartyScriptDetector::From(window).Detect(
+            script->GetSourceLocation().url);
     switch (script->GetInvokerType()) {
       case ScriptTimingInfo::InvokerType::kClassicScript:
       case ScriptTimingInfo::InvokerType::kModuleScript:
