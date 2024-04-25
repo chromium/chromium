8d9cf58c28fe61ab2ccf130156e9ce27f6c580e6
Alvin Ji <alvinji@chromium.org>
2024-04-25 21:04:29 +0000

    geolocation: Clean unused feature flag "ReducedWifiPollingInterval"
    Change-Id: I832f1f273e8ebf28b805a6fc24f60d0c7d440349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5486698
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292693}

--- services/device/public/cpp/device_features.cc
+++ services/device/public/cpp/device_features.cc
@@ -27,11 +27,6 @@ BASE_FEATURE(kWinrtGeolocationImplementation,
 BASE_FEATURE(kMacCoreLocationBackend,
              "MacCoreLocationBackend",
              base::FEATURE_DISABLED_BY_DEFAULT);
-// Reduces the interval between WiFi polls to fetch new WiFi data sooner. This
-// can be useful in situations where new WiFi data is needed frequently.
-BASE_FEATURE(kCrOSGeolocationReducedWifiPollingInterval,
-             "ReducedWifiPollingInterval",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 // Enable serial communication for SPP devices.
 BASE_FEATURE(kEnableBluetoothSerialPortProfileInSerialApi,
              "EnableBluetoothSerialPortProfileInSerialApi",
