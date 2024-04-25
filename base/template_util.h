e31366e869011db454a3d35f363367955d198b33
Lei Zhang <thestig@chromium.org>
2024-04-25 16:23:16 +0000

    Move base::is_scoped_enum to its own header
    Put this code in base/types/cxx23_is_scoped_enum.h. When Chromium
    requires C++23, it will be more obvious that this code can be deleted in
    favor of the std:: version.
    
    Change-Id: Ia5b47c67277157e6683c4485c28a4e72e86f85b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484305
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292496}

--- base/template_util.h
+++ base/template_util.h
@@ -41,25 +41,6 @@ struct priority_tag<0> {};
 
 }  // namespace internal
 
-namespace internal {
-
-// The indirection with std::is_enum<T> is required, because instantiating
-// std::underlying_type_t<T> when T is not an enum is UB prior to C++20.
-template <typename T, bool = std::is_enum_v<T>>
-struct IsScopedEnumImpl : std::false_type {};
-
-template <typename T>
-struct IsScopedEnumImpl<T, /*std::is_enum_v<T>=*/true>
-    : std::negation<std::is_convertible<T, std::underlying_type_t<T>>> {};
-
-}  // namespace internal
-
-// Implementation of C++23's std::is_scoped_enum
-//
-// Reference: https://en.cppreference.com/w/cpp/types/is_scoped_enum
-template <typename T>
-struct is_scoped_enum : internal::IsScopedEnumImpl<T> {};
-
 }  // namespace base
 
 #endif  // BASE_TEMPLATE_UTIL_H_
--- base/types/cxx23_is_scoped_enum_unittest.cc
+++ base/types/cxx23_is_scoped_enum_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/template_util.h"
+#include "base/types/cxx23_is_scoped_enum.h"
 
 #include <stdint.h>
 
