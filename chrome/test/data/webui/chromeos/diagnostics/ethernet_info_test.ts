7d28e02dab0e903092db64ff313d166608978c7e
Michael Checo <michaelcheco@google.com>
2024-04-25 21:52:09 +0000

    diagnostics: Convert ethernet_info_test to TS
    Bug: b/245336251
    Change-Id: Ida7ff15133d365e78370d455b364da3930ccaec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5456059
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Camden Bickel <cambickel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292717}

--- chrome/test/data/webui/chromeos/diagnostics/ethernet_info_test.ts
+++ chrome/test/data/webui/chromeos/diagnostics/ethernet_info_test.ts
@@ -5,48 +5,41 @@
 import 'chrome://diagnostics/ethernet_info.js';
 import 'chrome://webui-test/chromeos/mojo_webui_test_support.js';
 
+import {EthernetNetwork} from 'chrome://diagnostics/diagnostics_types.js';
+import {EthernetInfoElement} from 'chrome://diagnostics/ethernet_info.js';
 import {fakeEthernetNetwork} from 'chrome://diagnostics/fake_data.js';
 import {AuthenticationType, EthernetStateProperties, Network} from 'chrome://diagnostics/network_health_provider.mojom-webui.js';
-import {assertFalse, assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
+import {assert} from 'chrome://resources/js/assert.js';
+import {assertFalse} from 'chrome://webui-test/chromeos/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
-
-import {assertDataPointHasExpectedHeaderAndValue, assertTextContains, getDataPointValue} from './diagnostics_test_utils.js';
+import {assertDataPointHasExpectedHeaderAndValue} from './diagnostics_test_utils.js';
 
 suite('ethernetInfoTestSuite', function() {
-  /** @type {?EthernetInfoElement} */
-  let ethernetInfoElement = null;
+  let ethernetInfoElement: EthernetInfoElement|null = null;
 
   setup(() => {
-    document.body.innerHTML = window.trustedTypes.emptyHTML;
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
   });
 
   teardown(() => {
-    ethernetInfoElement.remove();
+    ethernetInfoElement?.remove();
     ethernetInfoElement = null;
   });
 
-  /**
-   * @param {!Network} network
-   */
-  function initializeEthernetInfo(network) {
+  function initializeEthernetInfo(network: EthernetNetwork): Promise<void> {
     assertFalse(!!ethernetInfoElement);
 
     // Add the ethernet info to the DOM.
-    ethernetInfoElement =
-        /** @type {!EthernetInfoElement} */ (
-            document.createElement('ethernet-info'));
-    assertTrue(!!ethernetInfoElement);
-    ethernetInfoElement.network = network;
+    ethernetInfoElement = document.createElement('ethernet-info');
+    assert(ethernetInfoElement);
+    ethernetInfoElement.network = network as Network;
     document.body.appendChild(ethernetInfoElement);
 
     return flushTasks();
   }
 
-  /**
-   * @param {!EthernetStateProperties} typeProps
-   * @return {!Network}
-   */
-  function getEthernetNetworkWithTypeProperties(typeProps) {
+  function getEthernetNetworkWithTypeProperties(
+      typeProps: EthernetStateProperties): Network {
     const baseProperties = {
       authentication: AuthenticationType.kNone,
     };
@@ -54,21 +47,27 @@ suite('ethernetInfoTestSuite', function() {
     return Object.assign({}, fakeEthernetNetwork, {
       typeProperties: {
         ethernet: ethernetTypeProperies,
+        cellular: undefined,
+        wifi: undefined,
       },
+      ipConfig: {},
     });
   }
 
   test('EthernetInfoIpAddressBasedOnNetwork', () => {
     return initializeEthernetInfo(fakeEthernetNetwork).then(() => {
+      assert(ethernetInfoElement);
       const expectedHeader = ethernetInfoElement.i18n('networkIpAddressLabel');
+      assert(fakeEthernetNetwork && fakeEthernetNetwork.ipConfig && fakeEthernetNetwork.ipConfig.ipAddress);
+      const ipAddress = fakeEthernetNetwork.ipConfig.ipAddress;
       assertDataPointHasExpectedHeaderAndValue(
-          ethernetInfoElement, '#ipAddress', expectedHeader,
-          fakeEthernetNetwork.ipConfig.ipAddress);
+          ethernetInfoElement, '#ipAddress', expectedHeader, ipAddress);
     });
   });
 
   test('EthernetInfoAuthenticationBasedOnNetwork', () => {
     return initializeEthernetInfo(fakeEthernetNetwork).then(() => {
+      assert(ethernetInfoElement);
       const expectedHeader =
           ethernetInfoElement.i18n('networkAuthenticationLabel');
       const expectedValue =
@@ -82,9 +81,9 @@ suite('ethernetInfoTestSuite', function() {
   test('EthernetInfoAuthenticationWithAuthentication', () => {
     return initializeEthernetInfo(
                getEthernetNetworkWithTypeProperties(
-                   /** @type {EthernetStateProperties} */
-                   ({authentication: AuthenticationType.kNone})))
+                   {authentication: AuthenticationType.kNone}))
         .then(() => {
+          assert(ethernetInfoElement);
           const expectedHeader =
               ethernetInfoElement.i18n('networkAuthenticationLabel');
           const expectedValue = ethernetInfoElement.i18n(
