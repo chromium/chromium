cec52b4596caffbbfe53d727679bc671283e1471
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-04-27 06:18:57 +0000

    Import wpt@4fecc4e54012ccfce5a63252bfce505cf7c237f4
    https://github.com/web-platform-tests/wpt/compare/6ae7875d4...4fecc4e54
    
    Using wpt-import in Chromium b3ff8f22f6e1a6c0ac98b9037af3ef02af693afc.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ib605943c05dc4f37224d820d0a2fd2d06262dee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490994
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1293373}

--- third_party/blink/web_tests/external/wpt/webcodecs/video-encoder-flush.https.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/video-encoder-flush.https.any.js
@@ -22,12 +22,16 @@ const H264_AVC_CONFIG = {
   avc: {format: 'avc'},
 };
 
-promise_test(async t => {
-  let codecInit = getDefaultCodecInit(t);
-  const encoderConfig = {
+let CONFIG = null;
+promise_setup(async () => {
+  CONFIG = {
     '?vp8': VP8_CONFIG,
     '?h264_avc': H264_AVC_CONFIG,
   }[location.search];
+});
+
+promise_test(async t => {
+  let codecInit = getDefaultCodecInit(t);
 
   let outputs = 0;
   let firstOutput = new Promise(resolve => {
@@ -40,7 +44,7 @@ promise_test(async t => {
   });
 
   let encoder = new VideoEncoder(codecInit);
-  encoder.configure(encoderConfig);
+  encoder.configure(CONFIG);
 
   let frame1 = createFrame(640, 480, 0);
   let frame2 = createFrame(640, 480, 33333);
@@ -61,3 +65,48 @@ promise_test(async t => {
 
   assert_equals(outputs, 1, 'outputs');
 }, 'Test reset during flush');
+
+promise_test(async t => {
+  let frame1 = createFrame(640, 480, 0);
+  let frame2 = createFrame(640, 480, 33333);
+  t.add_cleanup(() => {
+    frame1.close();
+    frame2.close();
+  });
+
+  const callbacks = {};
+  const encoder = createVideoEncoder(t, callbacks);
+
+  let flushInCallbackDone;
+  let outputs = 0;
+  let firstOutput = new Promise(resolve => {
+    callbacks.output = (chunk, metadata) => {
+      encoder.reset();
+
+      callbacks.output = (chunk, metadata) => {
+        outputs++;
+      };
+
+      encoder.configure(CONFIG);
+      encoder.encode(frame2);
+      flushInCallbackDone = encoder.flush();
+
+      resolve();
+    };
+  });
+
+  encoder.configure(CONFIG);
+  encoder.encode(frame1);
+  const flushDone = encoder.flush();
+
+  // Wait for the first output, then reset.
+  await firstOutput;
+
+  // Flush should have been synchronously rejected.
+  await promise_rejects_dom(t, 'AbortError', flushDone);
+
+  // Wait for the second flush and check the output count.
+  await flushInCallbackDone;
+
+  assert_equals(outputs, 1, 'outputs');
+}, 'Test new flush after reset in a flush callback');
