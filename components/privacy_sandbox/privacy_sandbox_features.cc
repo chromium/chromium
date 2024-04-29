186537224d489f0ee83304a6cecca2f657b854d2
Xiaochen Zhou <xiaochenzh@chromium.org>
2024-04-29 16:37:38 +0000

    [Privacy Sandbox] Enable USER BLOCKING component task priority by default.
    After this CL lands, the finch config for the feature will set to 100%.
    
    Bug: 1500723, 1448875
    Change-Id: Ie16d9b0b7c1ee132e34da71f151660c199884885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490492
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293742}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -116,7 +116,7 @@ BASE_FEATURE(kPrivacySandboxAttestationsHigherComponentRegistrationPriority,
 
 BASE_FEATURE(kPrivacySandboxAttestationsUserBlockingPriority,
              "PrivacySandboxAttestationsUserBlockingPriority",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPrivacySandboxProactiveTopicsBlocking,
              "PrivacySandboxProactiveTopicsBlocking",
