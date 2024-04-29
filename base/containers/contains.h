78134647e2c1286477a692ef4d02efa71b17e880
Daniel Cheng <dcheng@chromium.org>
2024-04-29 17:46:28 +0000

    Simplify base::Contains() with concepts and if constexpr
    The helper trait can be replaced with a concept, and the complicated
    base::priority_tag dispatch mechanism can be replaced with if constexpr.
    
    Bug: 337356049
    Change-Id: I8d7a9a7ee0a985ec90508bae1620292cffff65ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494984
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293803}

--- base/containers/contains.h
+++ base/containers/contains.h
@@ -17,59 +17,8 @@ namespace internal {
 
 // Small helper to detect whether a given type has a nested `key_type` typedef.
 // Used below to catch misuses of the API for associative containers.
-template <typename T, typename SFINAE = void>
-struct HasKeyType : std::false_type {};
-
 template <typename T>
-struct HasKeyType<T, std::void_t<typename T::key_type>> : std::true_type {};
-
-// Probe whether a `contains` member function exists and return the result of
-// `container.contains(value)` if this is a valid expression. This is the
-// highest priority option.
-template <typename Container, typename Value>
-constexpr auto ContainsImpl(const Container& container,
-                            const Value& value,
-                            priority_tag<2>)
-    -> decltype(container.contains(value)) {
-  return container.contains(value);
-}
-
-// Probe whether a `find` member function exists and whether its return value
-// can be compared with `container.end()`. Intended for STL style maps and sets
-// that lack a `contains` member function.
-template <typename Container, typename Value>
-constexpr auto ContainsImpl(const Container& container,
-                            const Value& value,
-                            priority_tag<1>)
-    -> decltype(container.find(value) != container.end()) {
-  return container.find(value) != container.end();
-}
-
-// Probe whether a `find` member function exists and whether its return value
-// can be compared with `Container::npos`. Intended for STL style strings that
-// lack a `contains` member function.
-template <typename Container, typename Value>
-constexpr auto ContainsImpl(const Container& container,
-                            const Value& value,
-                            priority_tag<1>)
-    -> decltype(container.find(value) != Container::npos) {
-  return container.find(value) != Container::npos;
-}
-
-// Generic fallback option, using a linear search over `container` to find
-// `value`. Has the lowest priority. This will not compile for associative
-// containers, as this likely is a performance bug.
-template <typename Container, typename Value>
-constexpr bool ContainsImpl(const Container& container,
-                            const Value& value,
-                            priority_tag<0>) {
-  static_assert(
-      !HasKeyType<Container>::value,
-      "Error: About to perform linear search on an associative container. "
-      "Either use a more generic comparator (e.g. std::less<>) or, if a linear "
-      "search is desired, provide an explicit projection parameter.");
-  return ranges::find(container, value) != ranges::end(container);
-}
+concept HasKeyType = requires { typename T::key_type; };
 
 }  // namespace internal
 
@@ -78,7 +27,22 @@ constexpr bool ContainsImpl(const Container& container,
 // exists, and fall back to a generic linear search over `container`.
 template <typename Container, typename Value>
 constexpr bool Contains(const Container& container, const Value& value) {
-  return internal::ContainsImpl(container, value, internal::priority_tag<2>());
+  if constexpr (requires {
+                  { container.contains(value) } -> std::same_as<bool>;
+                }) {
+    return container.contains(value);
+  } else if constexpr (requires { container.find(value) != Container::npos; }) {
+    return container.find(value) != Container::npos;
+  } else if constexpr (requires { container.find(value) != container.end(); }) {
+    return container.find(value) != container.end();
+  } else {
+    static_assert(
+        !internal::HasKeyType<Container>,
+        "Error: About to perform linear search on an associative container. "
+        "Either use a more generic comparator (e.g. std::less<>) or, if a "
+        "linear search is desired, provide an explicit projection parameter.");
+    return ranges::find(container, value) != ranges::end(container);
+  }
 }
 
 // Overload that allows to provide an additional projection invocable. This
