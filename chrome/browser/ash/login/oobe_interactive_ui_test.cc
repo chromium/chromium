be58d09f1414631d118ddf98c94be6a325c4f792
Marc Treib <treib@chromium.org>
2024-04-29 09:10:23 +0000

    Disable flaky EphemeralUserOobeTest.RegularEphemeralUser
    It was already disabled on ASan, but it's (now) flaky in regular builds
    too.
    
    Bug: 40935123
    Change-Id: I1852f3974e1fa004b1c2d14d339c1d9fd5507564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491083
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293576}

--- chrome/browser/ash/login/oobe_interactive_ui_test.cc
+++ chrome/browser/ash/login/oobe_interactive_ui_test.cc
@@ -1145,16 +1145,10 @@ class EphemeralUserOobeTest : public OobeBaseTest,
       &mixin_host_, DeviceStateMixin::State::OOBE_COMPLETED_CLOUD_ENROLLED};
 };
 
-// TODO(crbug.com/40882667): Flaky on Linux Chrome OS ASan LSan and dbg.
-// Re-enable this test.
-#if BUILDFLAG(IS_CHROMEOS) && (defined(ADDRESS_SANITIZER) || !defined(NDEBUG))
-#define MAYBE_RegularEphemeralUser DISABLED_RegularEphemeralUser
-#else
-#define MAYBE_RegularEphemeralUser RegularEphemeralUser
-#endif
+// TODO(crbug.com/40882667): Flaky. Re-enable this test.
 // In this test we login as a regular user, which means it is not affilated
 // with the domain of the device. Thus we still need a consent from user.
-IN_PROC_BROWSER_TEST_P(EphemeralUserOobeTest, MAYBE_RegularEphemeralUser) {
+IN_PROC_BROWSER_TEST_P(EphemeralUserOobeTest, DISABLED_RegularEphemeralUser) {
   LoginDisplayHost::default_host()->GetWizardContext()->is_branded_build = true;
 
   WaitForGaiaSignInScreen();
