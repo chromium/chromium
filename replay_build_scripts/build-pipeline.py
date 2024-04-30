ff654a0b5836d64b02038059e043e1c4c06a2e99
Dan Miller <dan@replay.io>
2024-04-29 20:25:12 -0400

    scripts: generate dynamic deploy pipeline for driver/linker/chromium (#1204)

---
+++ replay_build_scripts/build-pipeline.py
@@ -0,0 +1,44 @@
+import json
+
+
+def read_commit_hash(file_path):
+    """Reads the commit hash from the given file."""
+    with open(file_path, "r") as file:
+        return file.read().strip()
+
+
+def generate_buildkite_pipeline(commit_hash):
+    """Generates a Buildkite pipeline with a dynamic commit hash in JSON format."""
+
+    # driver_revision is the first 12 characters of the commit hash.
+    driver_revision = commit_hash.split()[0][:12]
+    pipeline = {
+        "steps": [
+            {
+                "trigger": "build-driver-linker",
+                "key": "build-driver-linker",
+                "build": {
+                    "commit": commit_hash,
+                    "message": "Triggered from chromium: ${BUILDKITE_MESSAGE}",
+                },
+            },
+            {
+                "trigger": "chromium-build",
+                "build": {
+                    "env": {"DRIVER_REVISION": driver_revision},
+                },
+                "depends_on": ["build-driver-linker"],
+            },
+        ]
+    }
+    return json.dumps(pipeline, indent=4)
+
+
+def main():
+    commit_hash = read_commit_hash("REPLAY_BACKEND_REV")
+    pipeline_json = generate_buildkite_pipeline(commit_hash)
+    print(pipeline_json)
+
+
+if __name__ == "__main__":
+    main()
