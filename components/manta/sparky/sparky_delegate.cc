68c82bd54fc2e7c0acd15ec637971cb5a20bad33
Lauren Commeignes <laurencom@chromium.org>
2024-05-01 01:55:16 +0000

    Add in experimental sparky delegate.
    Bug: 336703051
    Change-Id: I0465c7738f5fc18be8a3ef6bd808bb5037790db4
    Test: Tests will be created once the Sparky Provider is created.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5500158
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294721}

---
+++ components/manta/sparky/sparky_delegate.cc
@@ -0,0 +1,28 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/manta/sparky/sparky_delegate.h"
+
+#include <memory>
+#include <optional>
+
+#include "base/values.h"
+
+namespace manta {
+
+SettingsData::SettingsData(const std::string& pref_name,
+                           const PrefType& pref_type,
+                           std::optional<base::Value> value)
+    : pref_name(pref_name), value(std::move(value)) {}
+
+SettingsData::~SettingsData() = default;
+
+void SettingsData::UpdateValue(std::optional<base::Value> new_value) {
+  value = std::move(new_value);
+}
+
+SparkyDelegate::SparkyDelegate() = default;
+SparkyDelegate::~SparkyDelegate() = default;
+
+}  // namespace manta
---
+++ components/manta/sparky/sparky_delegate.h
@@ -0,0 +1,65 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MANTA_SPARKY_SPARKY_DELEGATE_H_
+#define COMPONENTS_MANTA_SPARKY_SPARKY_DELEGATE_H_
+
+#include <map>
+#include <memory>
+#include <optional>
+#include <string>
+
+#include "base/component_export.h"
+#include "base/values.h"
+
+namespace manta {
+
+enum class COMPONENT_EXPORT(MANTA) PrefType {
+  kNone = 0,
+  kBoolean,
+  kNumber,
+  kString,
+  kUrl,
+  kList,
+  kDictionary,
+  kMaxValue = kDictionary,
+};
+
+// Stores the setting data for the current or wanted state of a Pref.
+struct COMPONENT_EXPORT(MANTA) SettingsData {
+  std::string pref_name;
+  PrefType pref_type;
+  std::optional<base::Value> value;
+
+  SettingsData(const std::string& pref_name,
+               const PrefType& pref_type,
+               std::optional<base::Value> value);
+
+  ~SettingsData();
+
+  SettingsData(const SettingsData&) = delete;
+  SettingsData& operator=(const SettingsData&) = delete;
+
+  void UpdateValue(std::optional<base::Value> new_value);
+};
+
+// Virtual class to handle the information requests and actions taken within
+// Sparky Provider which have a Chrome dependency.
+class COMPONENT_EXPORT(MANTA) SparkyDelegate {
+ public:
+  using SettingsDataList = std::map<std::string, std::unique_ptr<SettingsData>>;
+  SparkyDelegate();
+  SparkyDelegate(const SparkyDelegate&) = delete;
+  SparkyDelegate& operator=(const SparkyDelegate&) = delete;
+
+  virtual ~SparkyDelegate();
+
+  virtual bool SetSettings(std::unique_ptr<SettingsData> settings_data) = 0;
+  virtual void UpdateSettingsList() = 0;
+  virtual SettingsDataList* GetSettingsList() = 0;
+};
+
+}  // namespace manta
+
+#endif  // COMPONENTS_MANTA_SPARKY_SPARKY_DELEGATE_H_
