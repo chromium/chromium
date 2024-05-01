b5fcdadee6cc970dd6da840afaeecceebe0d6943
Kristi Saney <kristislee@google.com>
2024-05-01 19:30:29 +0000

    Reland "Add tests for voice and language prefs"
    This is a reland of commit eaf191b2e8d1ee194f2cc8a5f1c1cf87721a3a05
    
    This fixes the imports to use bundled read_anything.js.
    
    Original change's description:
    > Add tests for voice and language prefs
    >
    > Fixed: 337910273
    > Change-Id: I674a87b63e9823bdee136674da82464daaca928c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5503425
    > Auto-Submit: Kristi Saney <kristislee@google.com>
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Commit-Queue: Lauren Winston <lwinston@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1294761}
    
    Change-Id: Id6a6fcdff42a2ac48489befd7c8a82069bb77ace
    Fixed: 337910273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5504012
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1295013}

---
+++ chrome/test/data/webui/side_panel/read_anything/prefs_test.ts
@@ -0,0 +1,163 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {flush} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {BrowserProxy} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import type {ReadAnythingElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {assertArrayEquals, assertEquals} from 'chrome-untrusted://webui-test/chai_assert.js';
+
+import {suppressInnocuousErrors} from './common.js';
+import {FakeReadingMode} from './fake_reading_mode.js';
+import {TestColorUpdaterBrowserProxy} from './test_color_updater_browser_proxy.js';
+
+// TODO: b/40927698 - Add more tests.
+suite('PrefsTest', () => {
+  let app: ReadAnythingElement;
+  let testBrowserProxy: TestColorUpdaterBrowserProxy;
+
+  setup(() => {
+    suppressInnocuousErrors();
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    testBrowserProxy = new TestColorUpdaterBrowserProxy();
+    BrowserProxy.setInstance(testBrowserProxy);
+    const readingMode = new FakeReadingMode();
+    chrome.readingMode = readingMode as unknown as typeof chrome.readingMode;
+    chrome.readingMode.isReadAloudEnabled = true;
+    app = document.createElement('read-anything-app');
+    document.body.appendChild(app);
+  });
+
+  suite('on restore settings from prefs', () => {
+    setup(() => {
+      // We are not testing the toolbar for this suite.
+      app.$.toolbar.restoreSettingsFromPrefs = () => {};
+    });
+
+    suite('populates enabled languages', () => {
+      const langs = ['si', 'km', 'th'];
+
+      function enabledLanguagesInPref(): string[] {
+        // Bypass Typescript compiler to allow us to get a private property.
+        // @ts-ignore
+        return app.enabledLanguagesInPref;
+      }
+
+      setup(() => {
+        // @ts-ignore
+        app.availableVoices = [
+          {lang: langs[0]!, name: ''},
+          {lang: langs[1]!, name: ''},
+          {lang: langs[2]!, name: ''},
+        ];
+        // @ts-ignore
+        app.availableLangs = langs;
+      });
+
+      test('with langs stored in prefs', () => {
+        chrome.readingMode.getLanguagesEnabledInPref = () => langs;
+
+        app.restoreSettingsFromPrefs();
+
+        assertArrayEquals(enabledLanguagesInPref(), langs);
+      });
+
+      test('with browser lang', () => {
+        chrome.readingMode.baseLanguageForSpeech = langs[1]!;
+
+        app.restoreSettingsFromPrefs();
+
+        assertArrayEquals(enabledLanguagesInPref(), [langs[1]!]);
+      });
+    });
+
+    suite('initializes voice', () => {
+      const langForDefaultVoice = 'en';
+      const lang1 = 'zh';
+      const lang2 = 'tr';
+      const langWithNoVoices = 'elvish';
+
+      const defaultVoice = {
+        lang: langForDefaultVoice,
+        name: 'Kristi',
+        default: true,
+      } as SpeechSynthesisVoice;
+      const firstVoiceWithLang1 = {lang: lang1, name: 'Lauren'} as
+          SpeechSynthesisVoice;
+      const defaultVoiceWithLang1 = {
+        lang: lang1,
+        name: 'Eitan',
+        default: true,
+      } as SpeechSynthesisVoice;
+      const firstVoiceWithLang2 = {lang: lang2, name: 'Yu'} as
+          SpeechSynthesisVoice;
+      const secondVoiceWithLang2 = {lang: lang2, name: 'Xiang'} as
+          SpeechSynthesisVoice;
+      const otherVoice = {lang: 'it', name: 'Shari'} as SpeechSynthesisVoice;
+      const voices = [
+        defaultVoice,
+        firstVoiceWithLang1,
+        defaultVoiceWithLang1,
+        otherVoice,
+        firstVoiceWithLang2,
+        secondVoiceWithLang2,
+      ];
+
+      function selectedVoice(): SpeechSynthesisVoice {
+        // Bypass Typescript compiler to allow us to set a private property
+        // @ts-ignore
+        return app.selectedVoice;
+      }
+
+      setup(() => {
+        // @ts-ignore
+        app.availableVoices = voices;
+        flush();
+      });
+
+      test('to the stored voice for this language if there is one', () => {
+        chrome.readingMode.getStoredVoice = () => otherVoice.name;
+        app.restoreSettingsFromPrefs();
+        assertEquals(selectedVoice(), otherVoice);
+      });
+
+      suite('when there is no stored voice for this language', () => {
+        setup(() => {
+          chrome.readingMode.getStoredVoice = () => '';
+        });
+
+        suite('and no voices at all for this language', () => {
+          setup(() => {
+            app.speechSynthesisLanguage = langWithNoVoices;
+          });
+
+          test('to the current voice if there is one', () => {
+            // @ts-ignore
+            app.selectedVoice = otherVoice;
+            app.restoreSettingsFromPrefs();
+            assertEquals(selectedVoice(), otherVoice);
+          });
+
+          test('to the device default if there\'s no current voice', () => {
+            app.restoreSettingsFromPrefs();
+            assertEquals(selectedVoice(), defaultVoice);
+          });
+        });
+
+        test('to the default voice for this language', () => {
+          app.speechSynthesisLanguage = lang1;
+          app.restoreSettingsFromPrefs();
+          assertEquals(selectedVoice(), defaultVoiceWithLang1);
+        });
+
+        test(
+            'to the first listed voice for this language if there\'s no default',
+            () => {
+              app.speechSynthesisLanguage = lang2;
+              app.restoreSettingsFromPrefs();
+              assertEquals(selectedVoice(), firstVoiceWithLang2);
+            });
+      });
+    });
+  });
+});
