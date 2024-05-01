7eea4909dd12c3ad39fe86473b547ab7bbaad7a1
Yuki Shiino <yukishiino@chromium.org>
2024-05-01 05:29:36 +0000

    LUD/ELUD: Fix a reentrancy issue of SamplingState (ThreadLocalState)
    glibc's TLS implementation may call free(), and it causes a reentrancy
    issue with LUD and ELUD. Other gwp-asan shouldn't hit the issue because
    they do not use a SamplingState on the call paths of free(). LUD and
    ELUD do call SamplingState::Sample() on the call paths of free(), so
    they have this reentrancy issue.
    
    pthread library doesn't have this issue, so this patch uses pthread
    APIs until we find a better solution.
    
    Bug: 332234169
    Change-Id: Ib3c010da8d69af7b60b80b8845168a0e8cb55c34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5456559
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Max Ihlenfeldt <max@igalia.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Sergei Glazunov <glazunov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294777}

--- components/gwp_asan/client/thread_local_state.h
+++ components/gwp_asan/client/thread_local_state.h
@@ -10,17 +10,35 @@
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_APPLE) || BUILDFLAG(IS_ANDROID)
+// On macOS and Android (before Q), the first use of a `thread_local` variable
+// on a new thread will cause an allocation, leading to infinite recursion.
+// Also, `thread_local` goes through `emutls` on Android, which is slower than
+// `pthread_getspecific`.
 #define USE_PTHREAD_TLS
+#elif BUILDFLAG(IS_POSIX) && defined(COMPONENT_BUILD)
+// On POSIX platforms when built as component build, use of a `thread_local`
+// variable may or may not cause a call to `free()` depending on an
+// implementation of TLS. At least in case of glibc/glibc/elf/dl-tls.c,
+// `_dl_update_slotinfo()` calls `free()` (as of Apr 2024).
+//
+// This is a problem for LUD (Lightweight UaF Detector) and ELUD (Extreme LUD)
+// as they call `SamplingState::Sample()` inside `free()`, which leads to
+// infinite recursion. (See also https://crbug.com/332234169)
+//
+// Fortunately, statically-linked executables (non-component builds) do not hit
+// the problem because they use the "local-exec" model of TLS (i.e. do not call
+// any library function).
+#define USE_PTHREAD_TLS
+#endif
+
+#if defined(USE_PTHREAD_TLS)
 #include <pthread.h>
 #endif
 
 namespace gwp_asan::internal {
 
 // Class that provides the optimal thread-local storage implementation depending
-// on the platform. On macOS and Android (before Q), the first use of a
-// `thread_local` variable on a new thread will cause an allocation, leading to
-// infinite recursion. Also, `thread_local` goes through `emutls` on Android,
-// which is slower than `pthread_getspecific`.
+// on the platform.
 template <typename T>
 class ThreadLocalState {
  protected:
@@ -37,7 +55,8 @@ class ThreadLocalState {
   ALWAYS_INLINE static void SetState(uintptr_t value) { state_ = value; }
 
  private:
-  static thread_local uintptr_t state_;
+  static thread_local uintptr_t state_
+      __attribute__((tls_model("initial-exec")));
 #else
   ALWAYS_INLINE static uintptr_t GetState() {
     DCHECK(tls_key_);
