de9ac5f9564b1cc7595acbc89b5f12c7472c0de1
Chloe Pelling <cpelling@google.com>
2024-05-01 23:42:10 +0000

    exo: Fix memory leaks in WaylandProtocolLoggerTest.
    Should now run cleanly under ASAN/LSAN.
    
    BUG=b:336495168
    
    Change-Id: I8132ef2e4b0dbe365cbe40f5955f30e0a3c3634e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5481467
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Chloe Pelling <cpelling@google.com>
    Commit-Queue: Chloe Pelling <cpelling@google.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295180}

--- components/exo/wayland/wayland_protocol_logger_unittest.cc
+++ components/exo/wayland/wayland_protocol_logger_unittest.cc
@@ -12,14 +12,12 @@
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "components/exo/wayland/test/client_util.h"
+#include "components/exo/wayland/test/resource_key.h"
 #include "components/exo/wayland/test/server_util.h"
 #include "components/exo/wayland/test/wayland_server_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-// TODO(b/336495168): Test suite consistently failing on ChromeOS ASAN and LSAN.
-#if !(BUILDFLAG(IS_CHROMEOS) && \
-      (defined(ADDRESS_SANITIZER) || defined(LEAK_SANITIZER)))
 namespace exo::wayland {
 namespace {
 
@@ -56,6 +54,26 @@ class WaylandProtocolLoggerTest : public test::WaylandServerTest {
 
 std::vector<std::vector<std::string>> WaylandProtocolLoggerTest::messages_ = {};
 
+class ClientData : public test::TestClient::CustomData {
+ public:
+  ClientData()
+      : callback(nullptr, &wl_callback_destroy),
+        data_source(nullptr, &wl_data_source_destroy),
+        pointer(nullptr, &wl_pointer_destroy),
+        surface(nullptr, &wl_surface_destroy),
+        xdg_surface(nullptr, &xdg_surface_destroy),
+        xdg_toplevel(nullptr, &xdg_toplevel_destroy) {}
+  ~ClientData() override = default;
+
+  std::unique_ptr<wl_callback, decltype(&wl_callback_destroy)> callback;
+  std::unique_ptr<wl_data_source, decltype(&wl_data_source_destroy)>
+      data_source;
+  std::unique_ptr<wl_pointer, decltype(&wl_pointer_destroy)> pointer;
+  std::unique_ptr<wl_surface, decltype(&wl_surface_destroy)> surface;
+  std::unique_ptr<xdg_surface, decltype(&xdg_surface_destroy)> xdg_surface;
+  std::unique_ptr<xdg_toplevel, decltype(&xdg_toplevel_destroy)> xdg_toplevel;
+};
+
 std::string ProxyId(void* proxy) {
   return base::NumberToString(
       wl_proxy_get_id(reinterpret_cast<wl_proxy*>(proxy)));
@@ -73,9 +91,12 @@ void AddState(wl_array* states, xdg_toplevel_state state) {
 TEST_F(WaylandProtocolLoggerTest, LogsBasicRequest) {
   std::string surface_id;
   std::string compositor_id;
+
   PostToClientAndWait([&](test::TestClient* client) {
+    auto* data = client->set_data(std::make_unique<ClientData>());
+    data->surface.reset(wl_compositor_create_surface(client->compositor()));
     compositor_id = ProxyId(client->compositor());
-    surface_id = ProxyId(wl_compositor_create_surface(client->compositor()));
+    surface_id = ProxyId(data->surface.get());
   });
 
   EXPECT_THAT(messages_,
@@ -91,7 +112,9 @@ TEST_F(WaylandProtocolLoggerTest, LogsBasicEvent) {
   EXPECT_THAT(messages_, ::testing::IsEmpty());
 
   PostToClientAndWait([&](test::TestClient* client) {
-    callback_id = ProxyId(wl_display_sync(client->display()));
+    auto* data = client->set_data(std::make_unique<ClientData>());
+    data->callback.reset(wl_display_sync(client->display()));
+    callback_id = ProxyId(data->callback.get());
   });
 
   EXPECT_THAT(messages_,
@@ -101,14 +124,17 @@ TEST_F(WaylandProtocolLoggerTest, LogsBasicEvent) {
 
 TEST_F(WaylandProtocolLoggerTest, LogsObjects) {
   std::string xdg_wm_base_id;
-  std::string surface_id;
   std::string xdg_surface_id;
+  std::string surface_id;
+
   PostToClientAndWait([&](test::TestClient* client) {
-    auto* surface = wl_compositor_create_surface(client->compositor());
+    auto* data = client->set_data(std::make_unique<ClientData>());
     xdg_wm_base_id = ProxyId(client->xdg_wm_base());
-    surface_id = ProxyId(surface);
-    xdg_surface_id =
-        ProxyId(xdg_wm_base_get_xdg_surface(client->xdg_wm_base(), surface));
+    data->surface.reset(wl_compositor_create_surface(client->compositor()));
+    data->xdg_surface.reset(xdg_wm_base_get_xdg_surface(client->xdg_wm_base(),
+                                                        data->surface.get()));
+    surface_id = ProxyId(data->surface.get());
+    xdg_surface_id = ProxyId(data->xdg_surface.get());
   });
 
   EXPECT_THAT(messages_,
@@ -121,18 +147,16 @@ TEST_F(WaylandProtocolLoggerTest, LogsObjects) {
 
 TEST_F(WaylandProtocolLoggerTest, LogsStrings) {
   constexpr const char* window_title = "🦄🌈 I ♥️ UTF-8 😋";
-  std::string xdg_wm_base_id;
-  std::string surface_id;
-  std::string xdg_surface_id;
   std::string xdg_toplevel_id;
 
   PostToClientAndWait([&](test::TestClient* client) {
-    auto* surface = wl_compositor_create_surface(client->compositor());
-    auto* xdg_surface =
-        xdg_wm_base_get_xdg_surface(client->xdg_wm_base(), surface);
-    auto* xdg_toplevel = xdg_surface_get_toplevel(xdg_surface);
-    xdg_toplevel_set_title(xdg_toplevel, window_title);
-    xdg_toplevel_id = ProxyId(xdg_toplevel);
+    auto* data = client->set_data(std::make_unique<ClientData>());
+    data->surface.reset(wl_compositor_create_surface(client->compositor()));
+    data->xdg_surface.reset(xdg_wm_base_get_xdg_surface(client->xdg_wm_base(),
+                                                        data->surface.get()));
+    data->xdg_toplevel.reset(xdg_surface_get_toplevel(data->xdg_surface.get()));
+    xdg_toplevel_set_title(data->xdg_toplevel.get(), window_title);
+    xdg_toplevel_id = ProxyId(data->xdg_toplevel.get());
   });
 
   EXPECT_THAT(messages_, ::testing::Contains(::testing::ElementsAre(
@@ -166,15 +190,19 @@ TEST_F(WaylandProtocolLoggerTest, DISABLED_LogsObjectsAndStrings) {
 
 TEST_F(WaylandProtocolLoggerTest, LogsFileDescriptors) {
   std::string data_source_id;
-  wl_data_source* data_source;
+  test::ResourceKey data_source_resource_key;
+
   PostToClientAndWait([&](test::TestClient* client) {
-    data_source = wl_data_device_manager_create_data_source(
-        client->data_device_manager());
-    data_source_id = ProxyId(data_source);
+    auto* data = client->set_data(std::make_unique<ClientData>());
+    data->data_source.reset(wl_data_device_manager_create_data_source(
+        client->data_device_manager()));
+    data_source_id = ProxyId(data->data_source.get());
+    data_source_resource_key =
+        test::client_util::GetResourceKey(data->data_source.get());
   });
 
   wl_resource* data_source_resource = test::server_util::LookUpResource(
-      server_.get(), test::client_util::GetResourceKey(data_source));
+      server_.get(), data_source_resource_key);
   EXPECT_NE(data_source_resource, nullptr);
 
   // Actually sending a wl_data_source::send event requires a valid file
@@ -203,21 +231,23 @@ TEST_F(WaylandProtocolLoggerTest, LogsFileDescriptors) {
 TEST_F(WaylandProtocolLoggerTest, LogsFixedPointNumbers) {
   // Arrange: Create a wl_pointer client object, wait for server to receive.
   std::string pointer_id;
-  wl_pointer* pointer;
+  test::ResourceKey pointer_resource_key;
+
   PostToClientAndWait([&](test::TestClient* client) {
-    pointer = wl_seat_get_pointer(client->seat());
-    pointer_id = ProxyId(pointer);
+    auto* data = client->set_data(std::make_unique<ClientData>());
+    data->pointer.reset(wl_seat_get_pointer(client->seat()));
+    pointer_id = ProxyId(data->pointer.get());
+    pointer_resource_key =
+        test::client_util::GetResourceKey(data->pointer.get());
   });
 
   // Act: Send a motion event
-  wl_resource* pointer_resource = test::server_util::LookUpResource(
-      server_.get(), test::client_util::GetResourceKey(pointer));
+  wl_resource* pointer_resource =
+      test::server_util::LookUpResource(server_.get(), pointer_resource_key);
   EXPECT_NE(pointer_resource, nullptr);
 
-  PostToClientAndWait([&](test::TestClient* client) {
-    wl_pointer_send_motion(pointer_resource, 123, wl_fixed_from_double(1234.5),
-                           wl_fixed_from_double(0));
-  });
+  wl_pointer_send_motion(pointer_resource, 123, wl_fixed_from_double(1234.5),
+                         wl_fixed_from_double(0));
 
   // Assert: Motion event correctly logged
   EXPECT_THAT(
@@ -228,30 +258,34 @@ TEST_F(WaylandProtocolLoggerTest, LogsFixedPointNumbers) {
 }
 
 TEST_F(WaylandProtocolLoggerTest, LogsArrays) {
-  xdg_toplevel* toplevel = nullptr;
   std::string xdg_toplevel_id;
+  test::ResourceKey xdg_toplevel_resource_key;
 
   PostToClientAndWait([&](test::TestClient* client) {
-    auto* surface = wl_compositor_create_surface(client->compositor());
-    auto* xdg_surface =
-        xdg_wm_base_get_xdg_surface(client->xdg_wm_base(), surface);
-    toplevel = xdg_surface_get_toplevel(xdg_surface);
-    xdg_toplevel_id = ProxyId(toplevel);
+    auto* data = client->set_data(std::make_unique<ClientData>());
+    data->surface.reset(wl_compositor_create_surface(client->compositor()));
+    data->xdg_surface.reset(xdg_wm_base_get_xdg_surface(client->xdg_wm_base(),
+                                                        data->surface.get()));
+    data->xdg_toplevel.reset(xdg_surface_get_toplevel(data->xdg_surface.get()));
+    xdg_toplevel_id = ProxyId(data->xdg_toplevel.get());
+    xdg_toplevel_resource_key =
+        test::client_util::GetResourceKey(data->xdg_toplevel.get());
   });
 
   wl_resource* xdg_toplevel_resource = test::server_util::LookUpResource(
-      server_.get(), test::client_util::GetResourceKey(toplevel));
+      server_.get(), xdg_toplevel_resource_key);
   EXPECT_NE(xdg_toplevel_resource, nullptr);
 
   // Act: Send a typical configure event.
-  messages_.clear();
-  PostToClientAndWait([&](test::TestClient* client) {
+  {
+    messages_.clear();
     wl_array states;
     wl_array_init(&states);
     AddState(&states, XDG_TOPLEVEL_STATE_ACTIVATED);
     AddState(&states, XDG_TOPLEVEL_STATE_FULLSCREEN);
     xdg_toplevel_send_configure(xdg_toplevel_resource, 1024, 768, &states);
-  });
+    wl_array_release(&states);
+  }
 
   // Assert: Short arrays print correctly.
   EXPECT_THAT(messages_,
@@ -261,15 +295,16 @@ TEST_F(WaylandProtocolLoggerTest, LogsArrays) {
                   "1024", "768", "array[8 bytes]{0400000002000000}")));
 
   // Act: Send a very long array.
-  messages_.clear();
-  PostToClientAndWait([&](test::TestClient* client) {
+  {
+    messages_.clear();
     wl_array states;
     wl_array_init(&states);
     for (int i = 0; i < 14; i++) {
       AddState(&states, XDG_TOPLEVEL_STATE_ACTIVATED);
     }
     xdg_toplevel_send_configure(xdg_toplevel_resource, 1024, 768, &states);
-  });
+    wl_array_release(&states);
+  }
 
   // Assert: Long arrays truncate as expected.
   EXPECT_THAT(messages_,
@@ -283,4 +318,3 @@ TEST_F(WaylandProtocolLoggerTest, LogsArrays) {
 }
 
 }  // namespace exo::wayland
-#endif
