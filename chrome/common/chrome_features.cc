f5b1fc7e78b7afe97aa1b61c576faf98adc9fe73
Dibyajyoti Pal <dibyapal@google.com>
2024-05-02 17:37:07 +0000

    [PWA] Enable universal install by default
    This CL enables universal install by default on ToT, which is the
    step to launch as per go/finch-best-practices.
    Launch approval is in launch/4312170
    
    Bug: 329442899, 326611791
    Change-Id: I7bc9cfd65fa367e2631b794d6f7a47154394013b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506699
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295581}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -1555,7 +1555,12 @@ BASE_FEATURE(kWebAppSyncGeneratedIconUpdateFix,
 
 BASE_FEATURE(kWebAppUniversalInstall,
              "WebAppUniversalInstall",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+#if BUILDFLAG(IS_CHROMEOS)
+             base::FEATURE_DISABLED_BY_DEFAULT
+#else
+             base::FEATURE_ENABLED_BY_DEFAULT
+#endif  // BUILDFLAG(IS_CHROMEOS)
+);
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 BASE_FEATURE(kWebAppManifestPolicyAppIdentityUpdate,
