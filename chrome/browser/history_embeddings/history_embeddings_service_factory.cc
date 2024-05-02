2d674f95cce0d40952f3933ff7a9802938f158c1
Tommy C. Li <tommycli@chromium.org>
2024-05-02 22:38:25 +0000

    Add HistoryEmbeddings to fieldtrial_testing_config.json
    Needed to support any Variations configuration.
    
    This CL also fixes a tab helper crash that shows up when the flag is
    default enabled in all tests.
    
    This also avoids constructing the service for ephemeral and kiosk mode
    profiles.
    
    Bug: b/338245900
    Change-Id: Ie6a0f923bc5432762cdae3675a55cdccf725d028
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506718
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295804}

--- chrome/browser/history_embeddings/history_embeddings_service_factory.cc
+++ chrome/browser/history_embeddings/history_embeddings_service_factory.cc
@@ -4,15 +4,43 @@
 
 #include "chrome/browser/history_embeddings/history_embeddings_service_factory.h"
 
+#include "chrome/browser/app_mode/app_mode_utils.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/history_embeddings/chrome_passage_embeddings_service_controller.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/page_content_annotations/page_content_annotations_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_attributes_entry.h"
+#include "chrome/browser/profiles/profile_attributes_storage.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history_embeddings/history_embeddings_service.h"
 #include "components/keyed_service/core/service_access_type.h"
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "chrome/browser/ash/profiles/profile_helper.h"
+#endif
+
+namespace {
+
+bool IsEphemeralProfile(Profile* profile) {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (ash::ProfileHelper::IsEphemeralUserProfile(profile)) {
+    return true;
+  }
+#endif
+
+  // Catch additional logic that may not be caught by the existing Ash check.
+  ProfileAttributesStorage& storage =
+      g_browser_process->profile_manager()->GetProfileAttributesStorage();
+  ProfileAttributesEntry* entry =
+      storage.GetProfileAttributesWithPath(profile->GetPath());
+  return entry && entry->IsEphemeral();
+}
+
+}  // namespace
 
 // static
 history_embeddings::HistoryEmbeddingsService*
@@ -42,6 +70,13 @@ std::unique_ptr<KeyedService>
 HistoryEmbeddingsServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   auto* profile = Profile::FromBrowserContext(context);
+
+  // Embeddings don't last long enough to help users in kiosk or ephemeral
+  // profile mode, so simply never construct the service for those users.
+  if (chrome::IsRunningInAppMode() || IsEphemeralProfile(profile)) {
+    return nullptr;
+  }
+
   auto* history_service = HistoryServiceFactory::GetForProfile(
       profile, ServiceAccessType::EXPLICIT_ACCESS);
   // The history service is never null; even unit tests build and use one.
--- chrome/test/data/webui/history/history_routing_test.ts
+++ chrome/test/data/webui/history/history_routing_test.ts
@@ -31,6 +31,9 @@ import {navigateTo} from './test_util.js';
         enableHistoryClusters: 'Enable',
         isHistoryClustersEnabled,
         isHistoryClustersVisible: true,
+        /* For this test suite we want to keep testing the old cr-tabs which
+           is the default Stable configuration. */
+        enableHistoryEmbeddings: false,
       });
     });
 
