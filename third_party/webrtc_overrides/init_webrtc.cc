724bab29a659524bfd616310502904e46a558a74
Evan Shrubsole <eshr@google.com>
2024-05-02 12:46:49 +0000

    Remove use of legacy WebRTC tracing API
    Perfetto is always used in Chrome and this may now be removed.
    
    Bug: webrtc:15917
    Change-Id: I551be2ad473b4a1dea35618357925cb12e717ba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5508498
    Commit-Queue: Evan Shrubsole <eshr@google.com>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295402}

--- third_party/webrtc_overrides/init_webrtc.cc
+++ third_party/webrtc_overrides/init_webrtc.cc
@@ -4,41 +4,8 @@
 
 #include "init_webrtc.h"
 
-#include "base/command_line.h"
-#include "base/files/file_path.h"
-#include "base/files/file_util.h"
-#include "base/native_library.h"
-#include "base/path_service.h"
-#include "base/trace_event/trace_event.h"
 #include "third_party/webrtc/rtc_base/event_tracer.h"
 #include "third_party/webrtc/system_wrappers/include/cpu_info.h"
-#include "third_party/webrtc_overrides/rtc_base/logging.h"
-
-namespace {
-
-#if !defined(RTC_USE_PERFETTO)
-const unsigned char* GetCategoryGroupEnabled(const char* category_group) {
-  return TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(category_group);
-}
-
-void AddTraceEvent(char phase,
-                   const unsigned char* category_group_enabled,
-                   const char* name,
-                   unsigned long long id,
-                   int num_args,
-                   const char** arg_names,
-                   const unsigned char* arg_types,
-                   const unsigned long long* arg_values,
-                   unsigned char flags) {
-  base::trace_event::TraceArguments args(num_args, arg_names, arg_types,
-                                         arg_values);
-  TRACE_EVENT_API_ADD_TRACE_EVENT(phase, category_group_enabled, name,
-                                  trace_event_internal::kGlobalScope, id, &args,
-                                  flags);
-}
-#endif
-
-}  // namespace
 
 bool InitializeWebRtcModuleBeforeSandbox() {
   // Workaround for crbug.com/176522
@@ -49,9 +16,5 @@ bool InitializeWebRtcModuleBeforeSandbox() {
 }
 
 void InitializeWebRtcModule() {
-#if defined(RTC_USE_PERFETTO)
   webrtc::RegisterPerfettoTrackEvents();
-#else
-  webrtc::SetupEventTracer(&GetCategoryGroupEnabled, &AddTraceEvent);
-#endif  // defined(RTC_USE_PERFETTO)
 }
