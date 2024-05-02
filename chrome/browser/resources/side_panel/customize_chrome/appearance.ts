1c9d2b3a3c6517a7456b2621e3c0c00cad0b5085
dpapad <dpapad@chromium.org>
2024-05-02 23:36:37 +0000

    SidePanel: Migrate c/b/r/side_panel/customize_chrome to Lit, part 6.
    In this part migrating appearance.ts
    
    Bug: 40943652
    Change-Id: I2aae722a78a9e7bd9ff91b41d9036f4729162ddc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5508014
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295848}

--- chrome/browser/resources/side_panel/customize_chrome/appearance.ts
+++ chrome/browser/resources/side_panel/customize_chrome/appearance.ts
@@ -9,17 +9,17 @@ import 'chrome://resources/cr_components/customize_color_scheme_mode/customize_c
 import 'chrome://resources/cr_components/theme_color_picker/theme_color_picker.js';
 import 'chrome://resources/cr_components/managed_dialog/managed_dialog.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
-import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
-import 'chrome://resources/cr_elements/cr_icons.css.js';
 import 'chrome://resources/cr_elements/cr_toggle/cr_toggle.js';
 
 import type {CrToggleElement} from 'chrome://resources/cr_elements/cr_toggle/cr_toggle.js';
-import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
+import {I18nMixinLit} from 'chrome://resources/cr_elements/i18n_mixin_lit.js';
 import {assert} from 'chrome://resources/js/assert.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+import type {PropertyValues} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
-import {getTemplate} from './appearance.html.js';
+import {getCss} from './appearance.css.js';
+import {getHtml} from './appearance.html.js';
 import {CustomizeChromeAction, recordCustomizeChromeAction} from './common.js';
 import type {CustomizeChromePageCallbackRouter, CustomizeChromePageHandlerInterface, Theme} from './customize_chrome.mojom-webui.js';
 import {CustomizeChromeApiProxy} from './customize_chrome_api_proxy.js';
@@ -38,111 +38,70 @@ export interface AppearanceElement {
   };
 }
 
-export class AppearanceElement extends I18nMixin
-(PolymerElement) {
+const AppearanceElementBase = I18nMixinLit(CrLitElement);
+
+export class AppearanceElement extends AppearanceElementBase {
   static get is() {
     return 'customize-chrome-appearance';
   }
 
-  static get template() {
-    return getTemplate();
+  static override get styles() {
+    return getCss();
   }
 
-  static get properties() {
-    return {
-      theme_: Object,
+  override render() {
+    return getHtml.bind(this)();
+  }
 
-      editThemeButtonText_: {
-        type: String,
-        computed: 'computeEditThemeButtonText_(wallpaperSearchButtonEnabled_)',
-      },
+  static override get properties() {
+    return {
+      theme_: {type: Object},
+      editThemeButtonText_: {type: String},
 
       thirdPartyThemeId_: {
         type: String,
-        computed: 'computeThirdPartyThemeId_(theme_)',
-        reflectToAttribute: true,
+        reflect: true,
       },
 
       thirdPartyThemeName_: {
         type: String,
-        computed: 'computeThirdPartyThemeName_(theme_)',
-        reflectToAttribute: true,
+        reflect: true,
       },
 
-      showBottomDivider_: {
-        type: Boolean,
-        value: false,
-        computed:
-            'computeShowBottomDivider_(showClassicChromeButton_, showDeviceThemeToggle_)',
-      },
-
-      showClassicChromeButton_: {
-        type: Boolean,
-        value: false,
-        computed: 'computeShowClassicChromeButton_(theme_)',
-      },
-
-      showColorPicker_: {
-        type: Boolean,
-        value: false,
-        computed: 'computeShowColorPicker_(theme_)',
-      },
-
-      showDeviceThemeToggle_: {
-        type: Boolean,
-        value: false,
-        computed: 'computeShowDeviceThemeToggle_(theme_)',
-      },
-
-      showThemeSnapshot_: {
-        type: Boolean,
-        value: false,
-        computed: 'computeShowThemeSnapshot_(theme_)',
-      },
-
-      showUploadedImageButton_: {
-        type: Boolean,
-        value: false,
-        computed: 'computeShowUploadedImageButton_(theme_)',
-      },
-
-      showSearchedImageButton_: {
-        type: Boolean,
-        value: false,
-        computed: 'computeShowSearchedImageButton_(theme_)',
-      },
-
-      showManagedDialog_: Boolean,
+      showBottomDivider_: {type: Boolean},
+      showClassicChromeButton_: {type: Boolean},
+      showColorPicker_: {type: Boolean},
+      showDeviceThemeToggle_: {type: Boolean},
+      showThemeSnapshot_: {type: Boolean},
+      showUploadedImageButton_: {type: Boolean},
+      showSearchedImageButton_: {type: Boolean},
+      showManagedDialog_: {type: Boolean},
 
       wallpaperSearchButtonEnabled_: {
         type: Boolean,
-        value: () => loadTimeData.getBoolean('wallpaperSearchButtonEnabled'),
-        reflectToAttribute: true,
+        reflect: true,
       },
 
-      wallpaperSearchEnabled_: {
-        type: Boolean,
-        value: () => loadTimeData.getBoolean('wallpaperSearchEnabled'),
-      },
+      wallpaperSearchEnabled_: {type: Boolean},
     };
   }
 
-  private theme_: Theme|undefined = undefined;
-  private editThemeButtonText_:
-    string;
-  private thirdPartyThemeId_: string|null = null;
-  private thirdPartyThemeName_: string|null = null;
-  private hasUploadedImage_: boolean;
-  private showBottomDivider_: boolean;
-  private showClassicChromeButton_: boolean;
-  private showColorPicker_: boolean;
-  private showDeviceThemeToggle_: boolean;
-  private showThemeSnapshot: boolean;
-  private showManagedDialog_: boolean;
-  private wallpaperSearchButtonEnabled_:
-    boolean;
-  private wallpaperSearchEnabled_:
-    boolean;
+  protected theme_?: Theme;
+  protected editThemeButtonText_: string;
+  protected thirdPartyThemeId_: string|null = null;
+  protected thirdPartyThemeName_: string|null = null;
+  protected showBottomDivider_: boolean = false;
+  protected showClassicChromeButton_: boolean = false;
+  protected showColorPicker_: boolean = false;
+  protected showDeviceThemeToggle_: boolean = false;
+  protected showThemeSnapshot_: boolean = false;
+  protected showUploadedImageButton_: boolean = false;
+  protected showSearchedImageButton_: boolean = false;
+  protected showManagedDialog_: boolean;
+  protected wallpaperSearchButtonEnabled_: boolean =
+      loadTimeData.getBoolean('wallpaperSearchButtonEnabled');
+  private wallpaperSearchEnabled_: boolean =
+      loadTimeData.getBoolean('wallpaperSearchEnabled');
 
   private setThemeListenerId_: number|null = null;
 
@@ -171,16 +130,36 @@ export class AppearanceElement extends I18nMixin
     this.callbackRouter_.removeListener(this.setThemeListenerId_);
   }
 
+  override willUpdate(changedProperties: PropertyValues<this>) {
+    super.willUpdate(changedProperties);
+
+    const changedPrivateProperties =
+        changedProperties as Map<PropertyKey, unknown>;
+
+    this.editThemeButtonText_ = this.computeEditThemeButtonText_();
+
+    if (changedPrivateProperties.has('theme_')) {
+      this.thirdPartyThemeId_ = this.computeThirdPartyThemeId_();
+      this.thirdPartyThemeName_ = this.computeThirdPartyThemeName_();
+      this.showClassicChromeButton_ = this.computeShowClassicChromeButton_();
+      this.showColorPicker_ = this.computeShowColorPicker_();
+      this.showDeviceThemeToggle_ = this.computeShowDeviceThemeToggle_();
+      this.showThemeSnapshot_ = this.computeShowThemeSnapshot_();
+      this.showUploadedImageButton_ = this.computeShowUploadedImageButton_();
+      this.showSearchedImageButton_ = this.computeShowSearchedImageButton_();
+    }
+
+    this.showBottomDivider_ = this.computeShowBottomDivider_();
+  }
+
   focusOnThemeButton() {
     this.$.editThemeButton.focus();
   }
 
   private computeEditThemeButtonText_(): string {
-    if (!this.wallpaperSearchButtonEnabled_) {
-      return this.i18n('changeTheme');
-    } else {
-      return this.i18n('categoriesHeader');
-    }
+    return this.i18n(
+        this.wallpaperSearchButtonEnabled_ ? 'categoriesHeader' :
+                                             'changeTheme');
   }
 
   private computeThirdPartyThemeId_(): string|null {
@@ -237,7 +216,7 @@ export class AppearanceElement extends I18nMixin
         this.theme_.backgroundImage.localBackgroundId);
   }
 
-  private onEditThemeClicked_() {
+  protected onEditThemeClicked_() {
     recordCustomizeChromeAction(CustomizeChromeAction.EDIT_THEME_CLICKED);
     if (this.handleClickForManagedThemes_()) {
       return;
@@ -245,7 +224,7 @@ export class AppearanceElement extends I18nMixin
     this.dispatchEvent(new Event('edit-theme-click'));
   }
 
-  private onWallpaperSearchClicked_() {
+  protected onWallpaperSearchClicked_() {
     recordCustomizeChromeAction(
         CustomizeChromeAction.WALLPAPER_SEARCH_APPEARANCE_BUTTON_CLICKED);
     if (this.handleClickForManagedThemes_()) {
@@ -254,17 +233,17 @@ export class AppearanceElement extends I18nMixin
     this.dispatchEvent(new Event('wallpaper-search-click'));
   }
 
-  private onThirdPartyLinkButtonClick_() {
+  protected onThirdPartyLinkButtonClick_() {
     if (this.thirdPartyThemeId_) {
       this.pageHandler_.openThirdPartyThemePage(this.thirdPartyThemeId_);
     }
   }
 
-  private onUploadedImageButtonClick_() {
+  protected onUploadedImageButtonClick_() {
     this.pageHandler_.chooseLocalCustomBackground();
   }
 
-  private onSearchedImageButtonClick_() {
+  protected onSearchedImageButtonClick_() {
     if (this.wallpaperSearchEnabled_) {
       this.dispatchEvent(new CustomEvent('wallpaper-search-click'));
     } else {
@@ -272,7 +251,7 @@ export class AppearanceElement extends I18nMixin
     }
   }
 
-  private onSetClassicChromeClicked_() {
+  protected onSetClassicChromeClicked_() {
     if (this.handleClickForManagedThemes_()) {
       return;
     }
@@ -282,11 +261,11 @@ export class AppearanceElement extends I18nMixin
         CustomizeChromeAction.SET_CLASSIC_CHROME_THEME_CLICKED);
   }
 
-  private onFollowThemeToggleChange_(e: CustomEvent<boolean>) {
+  protected onFollowThemeToggleChange_(e: CustomEvent<boolean>) {
     this.pageHandler_.setFollowDeviceTheme(e.detail);
   }
 
-  private onManagedDialogClosed_() {
+  protected onManagedDialogClosed_() {
     this.showManagedDialog_ = false;
   }
 
