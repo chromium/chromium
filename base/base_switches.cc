7c111cd3b3b6cde08c2d47b30a7482e066687982
Alex Gough <ajgo@chromium.org>
2024-05-02 22:23:16 +0000

    Add tiny doc for field-trial-handle switch
    On Windows this command line parameter has an additional
    argument indicating how the child should obtain the handle.
    This is more completely documented in
    SerializeSharedMemoryRegionMetadata() but a sentence here
    should reduce surprises for people reading the code.
    
    Change-Id: Ib2a84975d02216b79306b67c65b65f6430df297f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5211873
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295795}

--- base/base_switches.cc
+++ base/base_switches.cc
@@ -37,12 +37,13 @@ const char kEnableLowEndDeviceMode[]        = "enable-low-end-device-mode";
 const char kEnableBackgroundThreadPool[] = "enable-background-thread-pool";
 
 // Handle to the shared memory segment containing field trial state that is to
-// be shared between processes. The argument to this switch is made of 4
-// segments, separated by commas:
-// 1. The platform-specific handle id for the shared memory as a string.
-// 2. The high 64 bits of the shared memory block GUID.
-// 3. The low 64 bits of the shared memory block GUID.
-// 4. The size of the shared memory segment as a string.
+// be shared between processes. The argument to this switch is made of segments
+// separated by commas:
+// - The platform-specific handle id for the shared memory as a string.
+// - (Windows only) i=inherited by duplication or p=child must open parent.
+// - The high 64 bits of the shared memory block GUID.
+// - The low 64 bits of the shared memory block GUID.
+// - The size of the shared memory segment as a string.
 const char kFieldTrialHandle[] = "field-trial-handle";
 
 // This option can be used to force field trials when testing changes locally.
