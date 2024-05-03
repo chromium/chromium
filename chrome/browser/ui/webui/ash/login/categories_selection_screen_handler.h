a9a0d38ff4d026892bd5ecf80d435b458f6f95aa
David Bertoni <dbertoni@chromium.org>
2024-05-03 20:14:03 +0000

    [Code Health] Remove uses of base::SupportsWeakPtr
    (/chrome/browser/ui/webui/ash/login)
    
    This CL removes uses of this deprecated base class by adding a
    base::WeakPtrFactory data member.
    
    In some cases, the base class was no longer being used, so it's
    simply removed.
    
    This CL was uploaded by git cl split.
    
    R=xiyuan@chromium.org
    
    Bug: 647430
    Change-Id: If9a80cc1b16c83cb19816ddbcb4a9551e3d51c03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513273
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296307}

--- chrome/browser/ui/webui/ash/login/categories_selection_screen_handler.h
+++ chrome/browser/ui/webui/ash/login/categories_selection_screen_handler.h
@@ -5,7 +5,6 @@
 #ifndef CHROME_BROWSER_UI_WEBUI_ASH_LOGIN_CATEGORIES_SELECTION_SCREEN_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_ASH_LOGIN_CATEGORIES_SELECTION_SCREEN_HANDLER_H_
 
-#include "base/memory/weak_ptr.h"
 #include "base/values.h"
 #include "chrome/browser/ui/webui/ash/login/base_screen_handler.h"
 
@@ -13,8 +12,7 @@ namespace ash {
 
 // Interface for dependency injection between CategoriesSelectionScreen and its
 // WebUI representation.
-class CategoriesSelectionScreenView
-    : public base::SupportsWeakPtr<CategoriesSelectionScreenView> {
+class CategoriesSelectionScreenView {
  public:
   inline constexpr static StaticOobeScreenId kScreenId{
       "categories-selection", "CategoriesSelectionScreen"};
