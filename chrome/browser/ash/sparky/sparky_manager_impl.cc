25497fe449c8f685ac887c076242ec68ea26db85
Lauren Commeignes <laurencom@chromium.org>
2024-05-03 07:22:07 +0000

    Add in manager for experimental feature Sparky.
    This manager derives from the Mahi Manager, which connects to the
    front end for this feature.
    
    Test: Tests will be created once the sparky provider is made.
    Bug: b/333479467
    Change-Id: Ia7d66af27558d3b93fc0c50b790bd08f4067a44a
    Low-Coverage-Reason: EXPERIMENTAL_CODE Skeleton implementation
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5437414
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295966}

---
+++ chrome/browser/ash/sparky/sparky_manager_impl.cc
@@ -0,0 +1,232 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/sparky/sparky_manager_impl.h"
+
+#include <stdint.h>
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+#include "ash/constants/ash_pref_names.h"
+#include "ash/shell.h"
+#include "ash/system/mahi/mahi_panel_widget.h"
+#include "base/functional/callback.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/values.h"
+#include "chrome/browser/ash/crosapi/crosapi_ash.h"
+#include "chrome/browser/ash/crosapi/crosapi_manager.h"
+#include "chrome/browser/ash/mahi/mahi_browser_delegate_ash.h"
+#include "chromeos/components/mahi/public/cpp/mahi_manager.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
+#include "components/manta/features.h"
+#include "components/manta/manta_service.h"
+#include "components/prefs/pref_service.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/views/widget/unique_widget_ptr.h"
+
+namespace {
+
+using chromeos::MahiResponseStatus;
+using crosapi::mojom::MahiContextMenuActionType;
+
+ash::MahiBrowserDelegateAsh* GetMahiBrowserDelgateAsh() {
+  auto* mahi_browser_delegate_ash = crosapi::CrosapiManager::Get()
+                                        ->crosapi_ash()
+                                        ->mahi_browser_delegate_ash();
+  CHECK(mahi_browser_delegate_ash);
+  return mahi_browser_delegate_ash;
+}
+
+}  // namespace
+namespace ash {
+
+SparkyManagerImpl::SparkyManagerImpl(Profile* profile,
+                                     manta::MantaService* manta_service)
+    : profile_(profile), mahi_provider_(manta_service->CreateMahiProvider()) {
+  CHECK(manta::features::IsMantaServiceEnabled());
+}
+
+SparkyManagerImpl::~SparkyManagerImpl() = default;
+
+void SparkyManagerImpl::OpenMahiPanel(int64_t display_id) {
+  if (!IsEnabled()) {
+    return;
+  }
+
+  mahi_panel_widget_ = MahiPanelWidget::CreatePanelWidget(display_id);
+  mahi_panel_widget_->Show();
+}
+
+std::u16string SparkyManagerImpl::GetContentTitle() {
+  return u"";
+}
+
+gfx::ImageSkia SparkyManagerImpl::GetContentIcon() {
+  return gfx::ImageSkia();
+}
+
+GURL SparkyManagerImpl::GetContentUrl() {
+  return current_page_info_->url;
+}
+
+void SparkyManagerImpl::GetSummary(MahiSummaryCallback callback) {
+  GetMahiBrowserDelgateAsh()->GetContentFromClient(
+      current_page_info_->client_id, current_page_info_->page_id,
+      base::BindOnce(&SparkyManagerImpl::OnGetPageContentForSummary,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+}
+
+void SparkyManagerImpl::GetOutlines(MahiOutlinesCallback callback) {
+  std::vector<chromeos::MahiOutline> outlines;
+  std::move(callback).Run(outlines, MahiResponseStatus::kUnknownError);
+}
+
+void SparkyManagerImpl::GoToOutlineContent(int outline_id) {}
+
+void SparkyManagerImpl::AnswerQuestion(const std::u16string& question,
+                                       bool current_panel_content,
+                                       MahiAnswerQuestionCallback callback) {
+  if (current_panel_content) {
+    mahi_provider_->QuestionAndAnswer(
+        base::UTF16ToUTF8(current_panel_content_->page_content),
+        current_panel_qa_, base::UTF16ToUTF8(question),
+        base::BindOnce(&SparkyManagerImpl::OnSparkyProviderQAResponse,
+                       weak_ptr_factory_.GetWeakPtr(), question,
+                       std::move(callback)));
+    return;
+  }
+
+  GetMahiBrowserDelgateAsh()->GetContentFromClient(
+      current_page_info_->client_id, current_page_info_->page_id,
+      base::BindOnce(&SparkyManagerImpl::OnGetPageContentForQA,
+                     weak_ptr_factory_.GetWeakPtr(), question,
+                     std::move(callback)));
+}
+
+void SparkyManagerImpl::GetSuggestedQuestion(
+    MahiGetSuggestedQuestionCallback callback) {}
+
+void SparkyManagerImpl::SetCurrentFocusedPageInfo(
+    crosapi::mojom::MahiPageInfoPtr info) {
+  GURL url_before_update = current_page_info_->url;
+  current_page_info_ = std::move(info);
+  bool did_url_change =
+      !url_before_update.EqualsIgnoringRef(current_page_info_->url);
+
+  bool available =
+      current_page_info_->IsDistillable.value_or(false) && did_url_change;
+  NotifyRefreshAvailability(/*available=*/available);
+}
+
+void SparkyManagerImpl::OnContextMenuClicked(
+    crosapi::mojom::MahiContextMenuRequestPtr context_menu_request) {
+  switch (context_menu_request->action_type) {
+    case MahiContextMenuActionType::kSummary:
+    case MahiContextMenuActionType::kOutline:
+      // TODO(b/318565610): Update the behaviour of kOutline.
+      OpenMahiPanel(context_menu_request->display_id);
+      return;
+    case MahiContextMenuActionType::kQA:
+      OpenMahiPanel(context_menu_request->display_id);
+
+      // Ask question.
+      // TODO(b/331837721): `SparkyManagerImpl` should own an instance of
+      // `MahiUiController` and use it to answer question here. This
+      // functionality shouldn't need to be routed through the widget. We also
+      // need to add unit test logic for this after the refactor.
+      if (!context_menu_request->question) {
+        return;
+      }
+
+      // When the user sends a question from the context menu, we treat it as
+      // the start of a new journey, so we set `current_panel_content` false.
+      static_cast<MahiPanelWidget*>(mahi_panel_widget_.get())
+          ->SendQuestion(context_menu_request->question.value(),
+                         /*current_panel_content=*/false);
+      return;
+    case MahiContextMenuActionType::kSettings:
+      // TODO(b/318565610): Update the behaviour of kSettings
+      return;
+    case MahiContextMenuActionType::kNone:
+      return;
+  }
+}
+
+bool SparkyManagerImpl::IsEnabled() {
+  // TODO (b/333479467): Update with new pref for this feature.
+  return IsSupportedWithCorrectFeatureKey() &&
+         Shell::Get()->session_controller()->GetActivePrefService()->GetBoolean(
+             ash::prefs::kMahiEnabled);
+}
+
+void SparkyManagerImpl::NotifyRefreshAvailability(bool available) {
+  auto* mahi_widget = static_cast<MahiPanelWidget*>(mahi_panel_widget_.get());
+  if (mahi_widget) {
+    mahi_widget->NotifyRefreshAvailabilityChanged(available);
+  }
+}
+
+void SparkyManagerImpl::OnGetPageContentForSummary(
+    MahiSummaryCallback callback,
+    crosapi::mojom::MahiPageContentPtr mahi_content_ptr) {
+  if (!mahi_content_ptr) {
+    std::move(callback).Run(u"summary text",
+                            MahiResponseStatus::kContentExtractionError);
+    return;
+  }
+
+  // Assign current panel content and clear the current panel QA
+  current_panel_content_ = std::move(mahi_content_ptr);
+  current_panel_qa_.clear();
+
+  latest_response_status_ = MahiResponseStatus::kUnknownError;
+  std::move(callback).Run(u"Couldn't get summary", latest_response_status_);
+  return;
+}
+
+void SparkyManagerImpl::OnSparkyProviderQAResponse(
+    const std::u16string& question,
+    MahiAnswerQuestionCallback callback,
+    base::Value::Dict dict,
+    manta::MantaStatus status) {
+  if (status.status_code != manta::MantaStatusCode::kOk) {
+    latest_response_status_ = MahiResponseStatus::kUnknownError;
+    current_panel_qa_.emplace_back(base::UTF16ToUTF8(question), "");
+    std::move(callback).Run(std::nullopt, latest_response_status_);
+    return;
+  }
+
+  // TODO (b/333479467): Handle response returned once new provider is built.
+}
+
+void SparkyManagerImpl::OnGetPageContentForQA(
+    const std::u16string& question,
+    MahiAnswerQuestionCallback callback,
+    crosapi::mojom::MahiPageContentPtr mahi_content_ptr) {
+  if (!mahi_content_ptr) {
+    std::move(callback).Run(std::nullopt,
+                            MahiResponseStatus::kContentExtractionError);
+    return;
+  }
+
+  // Assign current panel content and clear the current panel QA
+  current_panel_content_ = std::move(mahi_content_ptr);
+  current_panel_qa_.clear();
+
+  mahi_provider_->QuestionAndAnswer(
+      base::UTF16ToUTF8(current_panel_content_->page_content),
+      current_panel_qa_, base::UTF16ToUTF8(question),
+      base::BindOnce(&SparkyManagerImpl::OnSparkyProviderQAResponse,
+                     weak_ptr_factory_.GetWeakPtr(), question,
+                     std::move(callback)));
+}
+
+void SparkyManagerImpl::OpenFeedbackDialog() {}
+
+}  // namespace ash
---
+++ chrome/browser/ash/sparky/sparky_manager_impl.h
@@ -0,0 +1,99 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_SPARKY_SPARKY_MANAGER_IMPL_H_
+#define CHROME_BROWSER_ASH_SPARKY_SPARKY_MANAGER_IMPL_H_
+
+#include "base/memory/raw_ptr.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chromeos/components/mahi/public/cpp/mahi_manager.h"
+#include "components/manta/mahi_provider.h"
+#include "components/manta/manta_service.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/views/widget/unique_widget_ptr.h"
+namespace ash {
+
+// The Mahi UI will be temporarily re-used for this feature which is an
+// experimental prototype. For this reason, the SparkyManagerImpl derives from
+// MahiManger, which contains the controls for the Mahi Ui.
+class SparkyManagerImpl : public chromeos::MahiManager, public KeyedService {
+ public:
+  SparkyManagerImpl(Profile* profile, manta::MantaService* manta_service);
+
+  SparkyManagerImpl(const SparkyManagerImpl&) = delete;
+  SparkyManagerImpl& operator=(const SparkyManagerImpl&) = delete;
+
+  ~SparkyManagerImpl() override;
+
+  // chromeos::MahiManager:
+  void OpenMahiPanel(int64_t display_id) override;
+  std::u16string GetContentTitle() override;
+  gfx::ImageSkia GetContentIcon() override;
+  GURL GetContentUrl() override;
+  void GetSummary(MahiSummaryCallback callback) override;
+  void GetOutlines(MahiOutlinesCallback callback) override;
+  void GoToOutlineContent(int outline_id) override;
+  void AnswerQuestion(const std::u16string& question,
+                      bool current_panel_content,
+                      MahiAnswerQuestionCallback callback) override;
+  void GetSuggestedQuestion(MahiGetSuggestedQuestionCallback callback) override;
+  void SetCurrentFocusedPageInfo(crosapi::mojom::MahiPageInfoPtr info) override;
+  void OnContextMenuClicked(
+      crosapi::mojom::MahiContextMenuRequestPtr context_menu_request) override;
+  void OpenFeedbackDialog() override;
+  bool IsEnabled() override;
+
+  // Notifies the panel that refresh is available or not for the corresponding
+  // surface.
+  void NotifyRefreshAvailability(bool available);
+
+ private:
+  friend class SparkyManagerImplTest;
+  friend class SparkyManagerImplFeatureKeyTest;
+
+  // Initialize required provider if it is not initialized yet.
+  void MaybeInitialize();
+
+  void OnGetPageContentForSummary(
+      MahiSummaryCallback callback,
+      crosapi::mojom::MahiPageContentPtr mahi_content_ptr);
+
+  void OnGetPageContentForQA(
+      const std::u16string& question,
+      MahiAnswerQuestionCallback callback,
+      crosapi::mojom::MahiPageContentPtr mahi_content_ptr);
+
+  void OnSparkyProviderQAResponse(const std::u16string& question,
+                                  MahiAnswerQuestionCallback callback,
+                                  base::Value::Dict dict,
+                                  manta::MantaStatus status);
+
+  crosapi::mojom::MahiPageInfoPtr current_page_info_ =
+      crosapi::mojom::MahiPageInfo::New();
+
+  crosapi::mojom::MahiPageContentPtr current_panel_content_ =
+      crosapi::mojom::MahiPageContent::New();
+
+  raw_ptr<Profile> profile_;
+
+  // Pair of question and their corresponding answer for the current panel
+  // content.
+  std::vector<std::pair<std::string, std::string>> current_panel_qa_;
+
+  // TODO (b/333479467): replace this with the Sparky provider once created.
+  std::unique_ptr<manta::MahiProvider> mahi_provider_;
+
+  // Keeps track of the latest result and code, used for feedback.
+  std::u16string latest_summary_;
+  chromeos::MahiResponseStatus latest_response_status_;
+
+  // The widget contains the Mahi main panel.
+  views::UniqueWidgetPtr mahi_panel_widget_;
+
+  base::WeakPtrFactory<SparkyManagerImpl> weak_ptr_factory_{this};
+};
+
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_SPARKY_SPARKY_MANAGER_IMPL_H_
