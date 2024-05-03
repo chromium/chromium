98354a640e074da24444b00e8382989b80aa25a7
danakj <danakj@chromium.org>
2024-05-03 17:41:58 +0000

    Use bounds-safe constructs for os_compat_android.
    The API of the functions here are fixed as they are standing in for
    libc functions. But we can use safer constructs internally to make
    them pass compilation with -Wunsafe-buffer-usage.
    
    futimes() takes a pointer that must have 2 elements. Internally, use a
    span to refer to the pointer, and a std::array instead of a native
    array.
    
    R=agrieve@chromium.org
    
    Bug: 40284755
    Change-Id: Ic978383876a91335ef552abc4fe9c219d70f8cc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512067
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296192}

--- base/os_compat_android.cc
+++ base/os_compat_android.cc
@@ -2,26 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "base/os_compat_android.h"
+
+#include <array>
 
 #include <asm/unistd.h>
 #include <errno.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
+#include "base/containers/span.h"
 #include "base/numerics/safe_conversions.h"
 
 extern "C" {
 #if __ANDROID_API__ < 26
-int futimes(int fd, const struct timeval tv[2]) {
-  if (tv == nullptr)
+int futimes(int fd, const struct timeval tv_ptr[2]) {
+  if (tv_ptr == nullptr) {
     return base::checked_cast<int>(syscall(__NR_utimensat, fd, NULL, NULL, 0));
+  }
 
+  // SAFETY: The caller is required to give an array of two elements.
+  auto tv = UNSAFE_BUFFERS(base::make_span<2u>(tv_ptr, 2u));
   if (tv[0].tv_usec < 0 || tv[0].tv_usec >= 1000000 ||
       tv[1].tv_usec < 0 || tv[1].tv_usec >= 1000000) {
     errno = EINVAL;
@@ -29,7 +30,7 @@ int futimes(int fd, const struct timeval tv[2]) {
   }
 
   // Convert timeval to timespec.
-  struct timespec ts[2];
+  std::array<struct timespec, 2> ts;
   ts[0].tv_sec = tv[0].tv_sec;
   ts[0].tv_nsec = tv[0].tv_usec * 1000;
   ts[1].tv_sec = tv[1].tv_sec;
