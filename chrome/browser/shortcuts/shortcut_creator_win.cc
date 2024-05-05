9044a5d388f5a6c01eba8cc57caf128852a63c82
David Bienvenu <davidbienvenu@chromium.org>
2024-05-05 15:09:16 +0000

    win: add Win PWA shortcut creator
    Add Windows version of PWA shortcut creator, and unit tests.
    
    Bug: 331614403, 333024272
    Change-Id: If6ad92a808a9ae4a853cd22649d2b26f4e3f39ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5503132
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296634}

---
+++ chrome/browser/shortcuts/shortcut_creator_win.cc
@@ -0,0 +1,85 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "chrome/browser/shortcuts/shortcut_creator.h"
+
+#include <windows.h>
+
+#include "base/base_paths.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/hash/hash.h"
+#include "base/path_service.h"
+#include "base/strings/strcat.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/win/shortcut.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/shortcuts/platform_util_win.h"
+#include "chrome/common/chrome_switches.h"
+#include "ui/gfx/icon_util.h"
+#include "ui/gfx/image/image_family.h"
+#include "url/gurl.h"
+
+namespace shortcuts {
+namespace {
+
+base::FilePath CreateIconFileFromBitmap(const base::FilePath& icon_path,
+                                        const gfx::ImageFamily& icon_images) {
+  if (!base::CreateDirectory(icon_path)) {
+    return base::FilePath();
+  }
+  const base::FilePath icon_file = icon_path.Append(L"shortcut.ico");
+
+  // Write the .ico file containing this new bitmap.
+  if (!IconUtil::CreateIconFileFromImageFamily(icon_images, icon_file)) {
+    // This can happen if the profile directory is deleted between the
+    // beginning of this function and here.
+    return base::FilePath();
+  }
+  return icon_file;
+}
+
+}  // namespace
+
+void CreateShortcutOnUserDesktop(const std::string& shortcut_name,
+                                 const GURL& shortcut_url,
+                                 gfx::ImageFamily icon_images,
+                                 const base::FilePath& profile_path,
+                                 ShortcutCreatorCallback complete) {
+  base::FilePath chrome_proxy_path = GetChromeProxyPath();
+  // Create a .ico file from the icon images, and put it in
+  // <profile dir>/shortcuts/resources/<Url Hash>/icons.
+  std::string url_hash =
+      base::NumberToString(base::PersistentHash(shortcut_url.spec()));
+  base::FilePath target_path =
+      profile_path.Append(kWebShortcutsIconDirName).AppendASCII(url_hash);
+  base::FilePath icon_path = CreateIconFileFromBitmap(target_path, icon_images);
+
+  // Create a desktop shortcut
+  base::FilePath desktop;
+  if (!base::PathService::Get(base::DIR_USER_DESKTOP, &desktop) ||
+      desktop.empty()) {
+    std::move(complete).Run(ShortcutCreatorResult::kError);
+    return;
+  }
+  base::FilePath shortcut_path =
+      desktop.Append(base::StrCat({base::ASCIIToWide(shortcut_name), L".lnk"}));
+
+  base::win::ShortcutProperties target_and_args_properties;
+  target_and_args_properties.set_target(chrome_proxy_path);
+  target_and_args_properties.set_arguments(base::StrCat(
+      {L"--", base::ASCIIToWide(switches::kProfileDirectory), L"=\"",
+       profile_path.BaseName().value(), L"\" --",
+       base::ASCIIToWide(switches::kIgnoreProfileDirectoryIfNotExists), L" ",
+       base::ASCIIToWide(shortcut_url.spec())}));
+  target_and_args_properties.set_icon(icon_path, /*icon_index_in=*/0);
+
+  bool res =
+      CreateOrUpdateShortcutLink(shortcut_path, target_and_args_properties,
+                                 base::win::ShortcutOperation::kCreateAlways);
+  std::move(complete).Run(res ? ShortcutCreatorResult::kSuccess
+                              : ShortcutCreatorResult::kError);
+}
+
+}  // namespace shortcuts
---
+++ chrome/browser/shortcuts/shortcut_creator_win_unittest.cc
@@ -0,0 +1,111 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/shortcuts/shortcut_creator.h"
+
+#include <algorithm>
+#include <functional>
+
+#include "base/base_paths.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/hash/hash.h"
+#include "base/path_service.h"
+#include "base/strings/strcat.h"
+#include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/test/scoped_path_override.h"
+#include "base/test/test_future.h"
+#include "base/test/test_shortcut_win.h"
+#include "base/win/scoped_com_initializer.h"
+#include "chrome/browser/profiles/profile_shortcut_manager_win.h"
+#include "chrome/browser/shortcuts/platform_util_win.h"
+#include "chrome/common/chrome_switches.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/gfx/image/image.h"
+#include "ui/gfx/image/image_family.h"
+#include "ui/gfx/image/image_unittest_util.h"
+#include "url/gurl.h"
+
+namespace shortcuts {
+
+namespace {
+
+constexpr char kShortcutFileName[] = "Test Name.lnk";
+constexpr char kShortcutName[] = "Test Name";
+
+struct ImageDesc {
+  int size;
+  SkColor color;
+};
+
+gfx::ImageFamily CreateImageFamily(std::vector<ImageDesc> images) {
+  gfx::ImageFamily image_family;
+  for (const ImageDesc& image_desc : images) {
+    image_family.Add(gfx::test::CreateImage(image_desc.size, image_desc.color));
+  }
+  return image_family;
+}
+
+base::FilePath GetUserDesktopPath() {
+  return base::PathService::CheckedGet(base::DIR_USER_DESKTOP);
+}
+
+class ShortcutCreatorWinTest : public testing::Test {
+ protected:
+  const GURL kUrl = GURL("https://example.com/test?query=1s&basdf");
+  void SetUp() override {
+    ASSERT_TRUE(default_profile_path_.CreateUniqueTempDir());
+  }
+  void VerifyShortcut() const;
+  const base::FilePath& default_profile_path() const {
+    return default_profile_path_.GetPath();
+  }
+
+ private:
+  base::ScopedTempDir default_profile_path_;
+  base::ScopedPathOverride desktop_override_{base::DIR_USER_DESKTOP};
+  base::win::ScopedCOMInitializer com_initializer_;
+};
+
+void ShortcutCreatorWinTest::VerifyShortcut() const {
+  base::win::ShortcutProperties properties;
+  properties.set_target(GetChromeProxyPath());
+  std::string url_hash =
+      base::NumberToString(base::PersistentHash(kUrl.spec()));
+  base::FilePath target_path = default_profile_path()
+                                   .Append(kWebShortcutsIconDirName)
+                                   .AppendASCII(url_hash)
+                                   .Append(L"shortcut.ico");
+  properties.set_icon(target_path, 0);
+  properties.set_arguments(base::StrCat(
+      {profiles::internal::CreateProfileShortcutFlags(default_profile_path(),
+                                                      /*incognito=*/false),
+       L" --", base::ASCIIToWide(switches::kIgnoreProfileDirectoryIfNotExists),
+       L" ", base::ASCIIToWide(kUrl.spec())}));
+  const base::FilePath shortcut_path =
+      GetUserDesktopPath().AppendASCII(kShortcutFileName);
+  ASSERT_TRUE(base::PathExists(shortcut_path));
+  base::win::ValidateShortcut(shortcut_path, properties);
+  // TODO(b/333024272): Verify images in .ico file are correct.
+}
+
+TEST_F(ShortcutCreatorWinTest, ShortcutCreated) {
+  gfx::ImageFamily images =
+      CreateImageFamily({{.size = 16, .color = SK_ColorCYAN},
+                         {.size = 32, .color = SK_ColorBLUE},
+                         {.size = 256, .color = SK_ColorGREEN}});
+
+  base::test::TestFuture<ShortcutCreatorResult> future;
+  CreateShortcutOnUserDesktop(kShortcutName, kUrl, std::move(images),
+                              default_profile_path(), future.GetCallback());
+  ASSERT_TRUE(future.Wait());
+  EXPECT_EQ(ShortcutCreatorResult::kSuccess, future.Get());
+
+  VerifyShortcut();
+}
+
+}  // namespace
+
+}  // namespace shortcuts
