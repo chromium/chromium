008c6be3128105d1f4a5484f72c334898eba31e1
Ivana Žužić <izuzic@google.com>
2024-05-06 13:34:32 +0000

    Reland "[Fixit] Test launching credential manager from settings"
    This is a reland of commit 4161083a85d03bb94c975d3e8e96f9de65bba6e7
    
    The reason why the test failed is that there was an outdated GMS Core on
    the Pixel 2 device that the bot uses. In this case, instead of calling
    the credential manager to display passwords, a message that encourages
    updating GMS Core is displayed. The test was waiting for a call that
    would never happen so it was failing. It's not possible to make the test
    work for this device so a restriction is added.
    
    Original change's description:
    > [Fixit] Test launching credential manager from settings
    >
    > This test verifies that the credential manager fakes are called when
    > password manager is accessed from Chrome settings.
    > This is the first integration test that verifies such a use case.
    >
    > Bug: b/329432495
    > Change-Id: Ib87f2b2366a3fd51dd9beb4e3afd1c8cc8f1bb4e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5509633
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Peter Conn <peconn@chromium.org>
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1296058}
    
    Bug: b/329432495
    Change-Id: I287c7d8a6a61eb990e85ff586a8d890daf57d006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513456
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296812}

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/password_manager/settings/CredentialManagerIntegrationTest.java
@@ -0,0 +1,95 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.password_manager.settings;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.filters.MediumTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+
+import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.PayloadCallbackHelper;
+import org.chromium.base.test.util.Restriction;
+import org.chromium.chrome.browser.flags.ChromeSwitches;
+import org.chromium.chrome.browser.password_manager.CredentialManagerLauncherFactory;
+import org.chromium.chrome.browser.password_manager.FakeCredentialManagerLauncherFactoryImpl;
+import org.chromium.chrome.browser.settings.MainSettings;
+import org.chromium.chrome.browser.settings.SettingsActivityTestRule;
+import org.chromium.chrome.browser.sync.SyncTestRule;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.R;
+import org.chromium.ui.test.util.DeviceRestriction;
+import org.chromium.ui.test.util.GmsCoreVersionRestriction;
+
+/** Integration test for accessing credential manager. */
+@RunWith(ChromeJUnit4ClassRunner.class)
+@Batch(Batch.PER_CLASS)
+@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE, "show-autofill-signatures"})
+public class CredentialManagerIntegrationTest {
+    @Rule public SyncTestRule mSyncTestRule = new SyncTestRule();
+
+    @Rule
+    public SettingsActivityTestRule<MainSettings> mSettingsActivityTestRule =
+            new SettingsActivityTestRule<>(MainSettings.class);
+
+    private FakeCredentialManagerLauncherFactoryImpl mFakeLauncherFactory =
+            new FakeCredentialManagerLauncherFactoryImpl();
+
+    final PayloadCallbackHelper<PendingIntent> mSuccessCallbackHelper =
+            new PayloadCallbackHelper<>();
+    final PayloadCallbackHelper<Exception> mFailureCallbackHelper = new PayloadCallbackHelper<>();
+
+    @Before
+    public void setup() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        CredentialManagerLauncherFactory.setFactoryForTesting(mFakeLauncherFactory);
+        mFakeLauncherFactory.setSuccessCallback(mSuccessCallbackHelper::notifyCalled);
+        mFakeLauncherFactory.setFailureCallback(mFailureCallbackHelper::notifyCalled);
+
+        Context context = ApplicationProvider.getApplicationContext();
+        // The Intent for testing leads to MainSettings because it needed the Activity.
+        // The production code would show the passwords in GMSCore.
+        mFakeLauncherFactory.setIntent(
+                PendingIntent.getActivity(
+                        context,
+                        123,
+                        new Intent(context, MainSettings.class),
+                        PendingIntent.FLAG_IMMUTABLE));
+
+        mSyncTestRule.setUpAccountAndEnableSyncForTesting();
+    }
+
+    @Test
+    @MediumTest
+    @Restriction({
+        DeviceRestriction.RESTRICTION_TYPE_NON_AUTO,
+        GmsCoreVersionRestriction.RESTRICTION_TYPE_VERSION_GE_22W30
+    })
+    public void testUseCredentialManagerFromChromeSettings() {
+        mSettingsActivityTestRule.startSettingsActivity();
+        onView(withText(R.string.password_manager_settings_title)).perform(click());
+
+        // Verify that success callback was called.
+        assertNotNull(mSuccessCallbackHelper.getOnlyPayloadBlocking());
+        // Verify that failure callback was not called.
+        assertEquals(0, mFailureCallbackHelper.getCallCount());
+    }
+}
--- chrome/browser/password_manager/android/test_support/java/src/org/chromium/chrome/browser/password_manager/FakeCredentialManagerLauncherFactoryImpl.java
+++ chrome/browser/password_manager/android/test_support/java/src/org/chromium/chrome/browser/password_manager/FakeCredentialManagerLauncherFactoryImpl.java
@@ -4,18 +4,40 @@
 
 package org.chromium.chrome.browser.password_manager;
 
+import android.app.PendingIntent;
+
+import org.chromium.base.Callback;
+
 /**
  * The factory for creating a fake {@link CredentialManagerLauncher} to be used in integration
  * tests.
  */
 public class FakeCredentialManagerLauncherFactoryImpl extends CredentialManagerLauncherFactory {
-    private CredentialManagerLauncher mLauncher;
+    private FakeCredentialManagerLauncher mLauncher;
+    private PendingIntent mPendingIntent;
+    private Callback<PendingIntent> mSuccessCallback;
+    private Callback<Exception> mFailureCallback;
+
+    public void setSuccessCallback(Callback<PendingIntent> successCallback) {
+        mSuccessCallback = successCallback;
+    }
+
+    public void setFailureCallback(Callback<Exception> failureCallback) {
+        mFailureCallback = failureCallback;
+    }
+
+    public void setIntent(PendingIntent pendingIntent) {
+        mPendingIntent = pendingIntent;
+    }
 
     /** Returns the fake implementation of {@link CredentialManagerLauncher} used for tests. */
     @Override
     public CredentialManagerLauncher createLauncher() {
         if (mLauncher == null) {
             mLauncher = new FakeCredentialManagerLauncher();
+            mLauncher.setSuccessCallback(mSuccessCallback);
+            mLauncher.setFailureCallback(mFailureCallback);
+            mLauncher.setIntent(mPendingIntent);
         }
         return mLauncher;
     }
