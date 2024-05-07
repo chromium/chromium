0b01699d4095d594105f313d0763d49a29f82606
Rouslan Solomakhin <rouslan@chromium.org>
2024-05-07 18:15:51 +0000

    [Iban] Split GetOrderedIbansToSuggest() test into two tests
    Before this patch, the PayemntsDataManagerTest.GetOrderedIbansToSuggest
    was responsible for testing both deduplication and sorting logic. This
    test was flaky, however.
    
    This patch splits the large test case into two smaller test case:
    1) Verify deduplication works correctly.
    2) Verify ordering works correctly.
    
    After this patch, it should be easier to isolate the source of the flake
    in the GetOrderedIbansToSuggest() tests.
    
    Bug: 337499028
    Change-Id: I40b63a898f26385eeaf3bdde2760578ea4bf8404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515739
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297648}

--- components/autofill/core/browser/payments_data_manager_unittest.cc
+++ components/autofill/core/browser/payments_data_manager_unittest.cc
@@ -240,6 +240,9 @@ class PaymentsDataManagerHelper : public PersonalDataManagerTestBase {
   }
 
   void AddLocalIban(Iban& iban) {
+    // AddAsLocalIban() expects only newly extracted IBANs in Iban::kUnknown
+    // state to be saved.
+    iban.set_record_type(Iban::kUnknown);
     iban.set_identifier(
         Iban::Guid(payments_data_manager().AddAsLocalIban(iban)));
     WaitForOnPaymentsDataChanged();
@@ -330,40 +333,60 @@ TEST_F(PaymentsDataManagerTest, GetIbans) {
   ExpectSameElements(all_ibans, payments_data_manager().GetIbans());
 }
 
-// Test that deduplication works correctly when a local IBAN has a matching
-// prefix and suffix (either equal or starting with) and the same length as the
-// server IBANs.
-TEST_F(PaymentsDataManagerTest, GetIbansToSuggest) {
+// Test that a local IBAN is removed from suggestions when it has a matching
+// prefix and suffix (either equal or starting with) and the same length as a
+// server IBAN.
+TEST_F(PaymentsDataManagerTest,
+       GetIbansToSuggestRemovesLocalIbanThatMatchesServerIban) {
   payments_data_manager().SetSyncingForTest(true);
 
-  // Create two IBANs, and two server IBANs.
-  // `local_iban1` and `server_iban1` have the same prefix, suffix and length.
-  Iban local_iban1;
-  local_iban1.set_value(u"FR76 3000 6000 0112 3456 7890 189");
-  local_iban1.set_use_date(AutofillClock::Now() - base::Days(4));
-  Iban local_iban2;
-  local_iban2.set_value(u"CH56 0483 5012 3456 7800 9");
-  local_iban2.set_use_date(AutofillClock::Now() - base::Days(3));
+  // `local_iban` and `server_iban` have the same prefix, suffix and length.
+  Iban local_iban;
+  local_iban.set_value(u"FR76 3000 6000 0112 3456 7890 189");
+  local_iban.set_use_date(AutofillClock::Now() - base::Days(4));
 
-  Iban server_iban1(Iban::InstrumentId(1234567));
-  server_iban1.set_prefix(u"FR76");
-  server_iban1.set_suffix(u"0189");
-  server_iban1.set_length(27);
-  server_iban1.set_use_date(AutofillClock::Now() - base::Days(2));
+  Iban server_iban(Iban::InstrumentId(1234567));
+  server_iban.set_prefix(u"FR76");
+  server_iban.set_suffix(u"0189");
+  server_iban.set_length(27);
+  server_iban.set_use_date(AutofillClock::Now() - base::Days(2));
 
-  Iban server_iban2 = test::GetServerIban2();
-  server_iban2.set_length(34);
-  server_iban2.set_use_date(AutofillClock::Now() - base::Days(1));
+  AddLocalIban(local_iban);
 
-  AddLocalIban(local_iban1);
-  AddLocalIban(local_iban2);
-
-  GetServerDataTable()->SetServerIbansForTesting({server_iban1, server_iban2});
+  GetServerDataTable()->SetServerIbansForTesting({server_iban});
   payments_data_manager().Refresh();
   WaitForOnPaymentsDataChanged();
 
   EXPECT_THAT(payments_data_manager().GetOrderedIbansToSuggest(),
-              testing::ElementsAre(server_iban2, server_iban1, local_iban2));
+              testing::ElementsAre(server_iban));
+}
+
+// Test that IBANs are ordered according to the frecency rating. All of the
+// IBANs in this test case have the use count = 1.
+TEST_F(PaymentsDataManagerTest, GetIbansToSuggestOrdersByFrecency) {
+  payments_data_manager().SetSyncingForTest(true);
+
+  Iban local_iban1 = test::GetLocalIban();
+  local_iban1.set_use_date(AutofillClock::Now() - base::Days(4));
+  AddLocalIban(local_iban1);
+
+  Iban local_iban2 = test::GetLocalIban2();
+  local_iban2.set_use_date(AutofillClock::Now() - base::Days(3));
+  AddLocalIban(local_iban2);
+
+  Iban server_iban2 = test::GetServerIban2();
+  server_iban2.set_use_date(AutofillClock::Now() - base::Days(2));
+
+  Iban server_iban3 = test::GetServerIban3();
+  server_iban3.set_use_date(AutofillClock::Now() - base::Days(1));
+
+  GetServerDataTable()->SetServerIbansForTesting({server_iban2, server_iban3});
+  payments_data_manager().Refresh();
+  WaitForOnPaymentsDataChanged();
+
+  EXPECT_THAT(payments_data_manager().GetOrderedIbansToSuggest(),
+              testing::ElementsAre(server_iban3, server_iban2, local_iban2,
+                                   local_iban1));
 }
 
 TEST_F(PaymentsDataManagerTest, AddLocalIbans) {
