c4ad7dcafffb19a320faaaf0b3b0e69120e80ab1
Joe Mason <joenotcharles@google.com>
2024-05-07 17:45:57 +0000

    Handle periods of 0 CPU in SystemMetricsTest.MeasureChildCpuUsage
    R=wez
    
    Bug: 338429160
    Change-Id: I2ab1834145ce1f9b8f11cf1e014a0a60755f6639
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513123
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297620}

--- base/process/process_metrics_unittest.cc
+++ base/process/process_metrics_unittest.cc
@@ -695,9 +695,17 @@ TEST_F(SystemMetricsTest, MeasureChildCpuUsage) {
       child_launcher.CreateChildProcessMetrics();
 
   const TimeDelta cpu_usage1 = TestCumulativeCPU(metrics.get(), TimeDelta());
-  PlatformThread::Sleep(TestTimeouts::tiny_timeout());
 
-  const TimeDelta cpu_usage2 = TestCumulativeCPU(metrics.get(), cpu_usage1);
+  // The child thread does busy work, so it should get some CPU usage. There's a
+  // small chance it won't be scheduled during the delay so loop several times.
+  const auto abort_time =
+      base::TimeTicks::Now() + TestTimeouts::action_max_timeout();
+  TimeDelta cpu_usage2;
+  while (cpu_usage2.is_zero() && !HasFailure() &&
+         base::TimeTicks::Now() < abort_time) {
+    PlatformThread::Sleep(TestTimeouts::tiny_timeout());
+    cpu_usage2 = TestCumulativeCPU(metrics.get(), cpu_usage1);
+  }
   EXPECT_TRUE(cpu_usage2.is_positive());
 
   ASSERT_TRUE(child_launcher.TerminateChildProcess());
