c9c97a2db66ede61e81cff7fdf3ac7bb84a793c0
Ashley Newson <ashleynewson@chromium.org>
2024-05-07 09:27:17 +0000

    Revert "Reland "Split app_service/webapk into its own BUILD.gn file.""
    This reverts commit 1a8bd8b2ac78ef93d2cf26381c82e521a3d0be91.
    
    Reason for revert: Still causing (perhaps slightly different) build failures. https://ci.chromium.org/ui/p/chrome/builders/ci/android-x86_64/18935/overview
    
    Original change's description:
    > Reland "Split app_service/webapk into its own BUILD.gn file."
    >
    > This is a reland of commit 306478860b1f3f23e10b6c4ad54436863bbce0c4
    >
    > This creates a new BUILD.gn file for app_service/webapk which is
    > unidirectionally depended on by the App Service. As part of this,
    > webapk_manager.{h,cc} is moved to the app_service directory as it
    > depends on App Service, avoiding a dependency cycle.
    >
    > It's a bit unclear why this CL was reverted for a Chrome on Android
    > compile failure as all the changes are within is_chromeos and
    > !is_android blocks. The one change to the CL is to move the dependency
    > on the webapk target to within an is_chromeos block to see if that
    > helps.
    >
    > The original webapk/BUILD.gn file was landed as part of a series of
    > reverts, so it is not included in this CL. It is unchanged in the
    > reland.
    >
    > Bug: b/336172688
    > Change-Id: I61d8a855b12aa31d21b46a84495498f68079b48d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515684
    > Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    > Commit-Queue: Dominick Ng <dominickn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1297331}
    
    Bug: b/336172688
    Bug: b/338220073
    Change-Id: I86d2a9042d5112d7de3922dfab29942bb90552d1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5518692
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ashley Newson <ashleynewson@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297377}

--- chrome/browser/apps/app_service/webapk/webapk_manager_unittest.cc
+++ chrome/browser/apps/app_service/webapk/webapk_manager_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/apps/app_service/webapk_manager.h"
+#include "chrome/browser/apps/app_service/webapk/webapk_manager.h"
 
 #include <memory>
 #include <vector>
