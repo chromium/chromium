d425e75edb78408dc1ac63561f598e2fc1fa5e27
Sunggook Chue <sunggch@microsoft.com>
2024-05-07 23:46:10 +0000

    Allow no key first frame for VP9 code in the Mac OS
    There is a flaky test failure on the MP4 Muxer test
    for VP9 encoding in the Mac OS. There is a known issue
    in the Linux where it allows it.
    
    The change is extend the existing skipping CHECK to the
    all platforms.
    
    Bug: 339068029
    Change-Id: Iab17b8ee88eb74901a46182cb0f1687663aa94de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5523777
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Markus Handell <handellm@google.com>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1297855}

--- media/muxers/mp4_muxer_delegate.cc
+++ media/muxers/mp4_muxer_delegate.cc
@@ -126,14 +126,12 @@ void Mp4MuxerDelegate::AddVideoFrame(
   DVLOG(1) << __func__ << ", " << params.AsHumanReadableString();
 
   if (!video_track_index_.has_value()) {
-#if BUILDFLAG(IS_LINUX)
     // TODO(crbug.com/333614631): Potential issue for the first frame.
     if (!is_key_frame) {
       CHECK_EQ(params.codec, VideoCodec::kVP9);
       DVLOG(1) << __func__ << ", not a key frame";
       return;
     }
-#endif
 
     CHECK(codec_description.has_value() || (params.codec == VideoCodec::kVP9));
     CHECK(is_key_frame);
