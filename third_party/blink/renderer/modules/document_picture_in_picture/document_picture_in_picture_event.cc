d0c69e72e0a22bf3b78ea09de6af17120f2b496e
Tommy Steimel <steimel@chromium.org>
2024-05-07 18:56:27 +0000

    [M120-LTS][document pip] Don't assume the enter event window is a LocalDOMWindow
    This CL changes DocumentPictureInPictureEvent to store a DOMWindow
    instead of a LocalDOMWindow to prevent crashes when the window it gets
    is actually a RemoteDOMWindow.
    
    (cherry picked from commit 2314741cdf2c4a6e11234dda7006ec0dd9005bbb)
    
    (cherry picked from commit 98bcf9ef5cdd3a19f8b739f2cc6b2afdb01b7694)
    
    Bug: 335003891
    Change-Id: I86a0ec5a89b51a26d5dd89559f86e6e4d6c3e8fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5467978
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Original-Original-Commit-Position: refs/heads/main@{#1290122}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5477908
    Auto-Submit: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Original-Commit-Position: refs/branch-heads/6367@{#974}
    Cr-Original-Branched-From: d158c6dc6e3604e6f899041972edf26087a49740-refs/heads/main@{#1274542}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5481473
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Owners-Override: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6099@{#2012}
    Cr-Branched-From: e6ee4500f7d6549a9ac1354f8d056da49ef406be-refs/heads/main@{#1217362}

--- third_party/blink/renderer/modules/document_picture_in_picture/document_picture_in_picture_event.cc
+++ third_party/blink/renderer/modules/document_picture_in_picture/document_picture_in_picture_event.cc
@@ -8,7 +8,7 @@ namespace blink {
 
 DocumentPictureInPictureEvent* DocumentPictureInPictureEvent::Create(
     const AtomicString& type,
-    LocalDOMWindow* document_picture_in_picture_window) {
+    DOMWindow* document_picture_in_picture_window) {
   return MakeGarbageCollected<DocumentPictureInPictureEvent>(
       type, document_picture_in_picture_window);
 }
@@ -19,13 +19,13 @@ DocumentPictureInPictureEvent* DocumentPictureInPictureEvent::Create(
   return MakeGarbageCollected<DocumentPictureInPictureEvent>(type, initializer);
 }
 
-LocalDOMWindow* DocumentPictureInPictureEvent::window() const {
+DOMWindow* DocumentPictureInPictureEvent::window() const {
   return document_picture_in_picture_window_.Get();
 }
 
 DocumentPictureInPictureEvent::DocumentPictureInPictureEvent(
     AtomicString const& type,
-    LocalDOMWindow* document_picture_in_picture_window)
+    DOMWindow* document_picture_in_picture_window)
     : Event(type, Bubbles::kYes, Cancelable::kNo),
       document_picture_in_picture_window_(document_picture_in_picture_window) {}
 
@@ -33,8 +33,7 @@ DocumentPictureInPictureEvent::DocumentPictureInPictureEvent(
     AtomicString const& type,
     const DocumentPictureInPictureEventInit* initializer)
     : Event(type, initializer),
-      document_picture_in_picture_window_(
-          static_cast<LocalDOMWindow*>(initializer->window())) {}
+      document_picture_in_picture_window_(initializer->window()) {}
 
 void DocumentPictureInPictureEvent::Trace(Visitor* visitor) const {
   visitor->Trace(document_picture_in_picture_window_);
