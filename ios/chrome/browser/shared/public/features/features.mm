e3dfe9b725c86cb18fcb885674edfe24de50ac84
Gayane Petrosyan <gayane@google.com>
2024-05-07 16:58:18 +0000

    [iOS] Enable Default browser video instructions in settings by default
    Bug: b/315306154
    Change-Id: I8118e9045f87b1e7c66decb76338b904834d9949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515626
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297578}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -299,7 +299,7 @@ BASE_FEATURE(kOnlyAccessClipboardAsync,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kDefaultBrowserVideoInSettings,
              "DefaultBrowserVideoInSettings",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kThemeColorInTopToolbar,
              "ThemeColorInTopToolbar",
