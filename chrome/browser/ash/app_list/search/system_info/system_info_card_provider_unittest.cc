349edb45625a4bc4573f9fdec0dcfcae2884a85c
Sparik Hayrapetyan <ust@google.com>
2024-05-08 09:14:52 +0000

    Revert "Refactor system info test utils to chromeos/ash"
    This reverts commit b17e25ad005d5362d194433fcd20f5a44376a4e2.
    
    Reason for revert: suspecting to be causing consistent test failures, please see b/339366904
    
    Original change's description:
    > Refactor system info test utils to chromeos/ash
    >
    > Change-Id: I324bfa0351615d2524d03ddfb44a865d6a7d4e43
    > Bug: b:338876367
    > Test: Unit Test SystemInfoCardProvider.
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513891
    > Reviewed-by: Paul Moy <pmoy@chromium.org>
    > Reviewed-by: CJ Huang <chenjih@google.com>
    > Reviewed-by: David Padlipsky <dpad@google.com>
    > Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1297760}
    
    Bug: b:338876367
    Change-Id: I0421baaf0037fe3b9fb5514ece12257d921bf63c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522401
    Commit-Queue: Sparik Hayrapetyan <ust@google.com>
    Owners-Override: Sparik Hayrapetyan <ust@google.com>
    Auto-Submit: Sparik Hayrapetyan <ust@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1298004}

--- chrome/browser/ash/app_list/search/system_info/system_info_card_provider_unittest.cc
+++ chrome/browser/ash/app_list/search/system_info/system_info_card_provider_unittest.cc
@@ -32,9 +32,9 @@
 #include "chromeos/ash/components/mojo_service_manager/fake_mojo_service_manager.h"
 #include "chromeos/ash/components/system_info/cpu_data.h"
 #include "chromeos/ash/components/system_info/cpu_usage_data.h"
-#include "chromeos/ash/components/system_info/system_info_test_util.h"
 #include "chromeos/ash/components/system_info/system_info_util.h"
 #include "chromeos/ash/services/cros_healthd/public/cpp/fake_cros_healthd.h"
+#include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_probe.mojom-forward.h"
 #include "chromeos/ash/services/cros_healthd/public/mojom/cros_healthd_probe.mojom.h"
 #include "chromeos/dbus/power/fake_power_manager_client.h"
 #include "chromeos/dbus/power_manager/power_supply_properties.pb.h"
@@ -60,6 +60,159 @@ constexpr char kProbeErrorCpuInfo[] =
     "Apps.AppList.SystemInfoProvider.CrosHealthdProbeError.CpuInfo";
 constexpr char kProbeErrorMemoryInfo[] =
     "Apps.AppList.SystemInfoProvider.CrosHealthdProbeError.MemoryInfo";
+
+void SetProbeTelemetryInfoResponse(healthd_mojom::BatteryInfoPtr battery_info,
+                                   healthd_mojom::CpuInfoPtr cpu_info,
+                                   healthd_mojom::MemoryInfoPtr memory_info) {
+  auto info = healthd_mojom::TelemetryInfo::New();
+  if (battery_info) {
+    info->battery_result =
+        healthd_mojom::BatteryResult::NewBatteryInfo(std::move(battery_info));
+  }
+  if (memory_info) {
+    info->memory_result =
+        healthd_mojom::MemoryResult::NewMemoryInfo(std::move(memory_info));
+  }
+  if (cpu_info) {
+    info->cpu_result =
+        healthd_mojom::CpuResult::NewCpuInfo(std::move(cpu_info));
+  }
+
+  ash::cros_healthd::FakeCrosHealthd::Get()
+      ->SetProbeTelemetryInfoResponseForTesting(info);
+}
+
+void SetCrosHealthdCpuResponse(
+    const std::vector<system_info::CpuUsageData>& usage_data,
+    const std::vector<int32_t>& cpu_temps,
+    const std::vector<uint32_t>& scaled_cpu_clock_speed) {
+  auto cpu_info_ptr = healthd_mojom::CpuInfo::New();
+  auto physical_cpu_info_ptr = healthd_mojom::PhysicalCpuInfo::New();
+
+  DCHECK_EQ(usage_data.size(), scaled_cpu_clock_speed.size());
+  for (size_t i = 0; i < usage_data.size(); ++i) {
+    const auto& data = usage_data[i];
+    auto logical_cpu_info_ptr = healthd_mojom::LogicalCpuInfo::New();
+
+    logical_cpu_info_ptr->user_time_user_hz = data.GetUserTime();
+    logical_cpu_info_ptr->system_time_user_hz = data.GetSystemTime();
+    logical_cpu_info_ptr->idle_time_user_hz = data.GetIdleTime();
+
+    logical_cpu_info_ptr->scaling_current_frequency_khz =
+        scaled_cpu_clock_speed[i];
+
+    physical_cpu_info_ptr->logical_cpus.emplace_back(
+        std::move(logical_cpu_info_ptr));
+  }
+
+  cpu_info_ptr->physical_cpus.push_back(std::move(physical_cpu_info_ptr));
+  for (const auto& cpu_temp : cpu_temps) {
+    auto cpu_temp_channel_ptr = healthd_mojom::CpuTemperatureChannel::New();
+    cpu_temp_channel_ptr->temperature_celsius = cpu_temp;
+    cpu_info_ptr->temperature_channels.emplace_back(
+        std::move(cpu_temp_channel_ptr));
+  }
+
+  SetProbeTelemetryInfoResponse(/*battery_info=*/nullptr,
+                                std::move(cpu_info_ptr),
+                                /*memory_info=*/nullptr);
+}
+
+void SetCrosHealthdMemoryUsageResponse(uint32_t total_memory_kib,
+                                       uint32_t free_memory_kib,
+                                       uint32_t available_memory_kib) {
+  healthd_mojom::MemoryInfoPtr memory_info = healthd_mojom::MemoryInfo::New(
+      total_memory_kib, free_memory_kib, available_memory_kib,
+      /*page_faults_since_last_boot=*/0);
+  SetProbeTelemetryInfoResponse(/*battery_info=*/nullptr, /*cpu_info=*/nullptr,
+                                /*memory_info=*/std::move(memory_info));
+}
+
+// Constructs a BatteryInfoPtr.
+healthd_mojom::BatteryInfoPtr CreateCrosHealthdBatteryHealthResponse(
+    double charge_full_now,
+    double charge_full_design,
+    int32_t cycle_count) {
+  healthd_mojom::NullableUint64Ptr temp_value_ptr(
+      healthd_mojom::NullableUint64::New());
+  auto battery_info = healthd_mojom::BatteryInfo::New(
+      /*cycle_count=*/cycle_count, /*voltage_now=*/0,
+      /*vendor=*/"",
+      /*serial_number=*/"", /*charge_full_design=*/charge_full_design,
+      /*charge_full=*/charge_full_now,
+      /*voltage_min_design=*/0,
+      /*model_name=*/"",
+      /*charge_now=*/0,
+      /*current_now=*/0,
+      /*technology=*/"",
+      /*status=*/"",
+      /*manufacture_date=*/std::nullopt, std::move(temp_value_ptr));
+  return battery_info;
+}
+
+void SetCrosHealthdBatteryHealthResponse(double charge_full_now,
+                                         double charge_full_design,
+                                         int32_t cycle_count) {
+  healthd_mojom::BatteryInfoPtr battery_info =
+      CreateCrosHealthdBatteryHealthResponse(charge_full_now,
+                                             charge_full_design, cycle_count);
+  SetProbeTelemetryInfoResponse(std::move(battery_info),
+                                /*cpu_info=*/nullptr,
+                                /*memory_info=*/nullptr);
+}
+
+bool AreValidPowerTimes(int64_t time_to_full, int64_t time_to_empty) {
+  // Exactly one of `time_to_full` or `time_to_empty` must be zero. The other
+  // can be a positive integer to represent the time to charge/discharge or -1
+  // to represent that the time is being calculated.
+  return (time_to_empty == 0 && (time_to_full > 0 || time_to_full == -1)) ||
+         (time_to_full == 0 && (time_to_empty > 0 || time_to_empty == -1));
+}
+
+power_manager::PowerSupplyProperties ConstructPowerSupplyProperties(
+    power_manager::PowerSupplyProperties::ExternalPower power_source,
+    power_manager::PowerSupplyProperties::BatteryState battery_state,
+    bool is_calculating_battery_time,
+    int64_t time_to_full,
+    int64_t time_to_empty,
+    double battery_percent) {
+  power_manager::PowerSupplyProperties props;
+  props.set_external_power(power_source);
+  props.set_battery_state(battery_state);
+
+  if (battery_state ==
+      power_manager::PowerSupplyProperties_BatteryState_NOT_PRESENT) {
+    // Leave `time_to_full` and `time_to_empty` unset.
+    return props;
+  }
+
+  DCHECK(AreValidPowerTimes(time_to_full, time_to_empty));
+
+  props.set_is_calculating_battery_time(is_calculating_battery_time);
+  props.set_battery_time_to_full_sec(time_to_full);
+  props.set_battery_time_to_empty_sec(time_to_empty);
+  props.set_battery_percent(battery_percent);
+
+  return props;
+}
+
+// Sets the PowerSupplyProperties on FakePowerManagerClient. Calling this
+// method immediately notifies PowerManagerClient observers. One of
+// `time_to_full` or `time_to_empty` must be either -1 or a positive number.
+// The other must be 0. If `battery_state` is NOT_PRESENT, both `time_to_full`
+// and `time_to_empty` will be left unset.
+void SetPowerManagerProperties(
+    power_manager::PowerSupplyProperties::ExternalPower power_source,
+    power_manager::PowerSupplyProperties::BatteryState battery_state,
+    bool is_calculating_battery_time,
+    int64_t time_to_full,
+    int64_t time_to_empty,
+    double battery_percent) {
+  power_manager::PowerSupplyProperties props = ConstructPowerSupplyProperties(
+      power_source, battery_state, is_calculating_battery_time, time_to_full,
+      time_to_empty, battery_percent);
+  chromeos::FakePowerManagerClient::Get()->UpdatePowerProperties(props);
+}
 
 // Get the path to file manager's test data directory.
 base::FilePath GetTestDataFilePath(const std::string& file_name) {
@@ -91,6 +244,14 @@ void AddFile(const std::string& file_name,
   const int res = base::File::Lstat(target_path, &stat);
   ASSERT_FALSE(res < 0) << "Couldn't stat" << target_path.value();
   ASSERT_EQ(expected_size, stat.st_size);
+}
+
+healthd_mojom::ProbeErrorPtr CreateProbeError(
+    healthd_mojom::ErrorType error_type) {
+  auto probe_error = healthd_mojom::ProbeError::New();
+  probe_error->type = error_type;
+  probe_error->msg = "probe error";
+  return probe_error;
 }
 
 void VerifyProbeErrorBucketCounts(const base::HistogramTester& tester,
@@ -141,7 +302,8 @@ class SystemInfoCardProviderTest : public testing::Test {
 
  protected:
   void SetUp() override {
-    system_info::SetUpTest();
+    chromeos::PowerManagerClient::InitializeFake();
+    ash::cros_healthd::FakeCrosHealthd::Initialize();
 
     // Initialize fake DBus clients.
     ash::ConciergeClient::InitializeFake(/*fake_cicerone_client=*/nullptr);
@@ -178,7 +340,8 @@ class SystemInfoCardProviderTest : public testing::Test {
     search_controller_.reset();
     profile_.reset();
     arc_service_manager_.reset();
-    system_info::TestDownTest();
+    ash::cros_healthd::FakeCrosHealthd::Shutdown();
+    chromeos::PowerManagerClient::Shutdown();
     ash::disks::DiskMountManager::Shutdown();
     storage::ExternalMountPoints::GetSystemInstance()->RevokeAllFileSystems();
     ash::SpacedClient::Shutdown();
@@ -253,8 +416,8 @@ TEST_F(SystemInfoCardProviderTest, PreventTriggeringOfTooShortQueries) {
   system_info::CpuUsageData core_1(1000, 1000, 1000);
   system_info::CpuUsageData core_2(2000, 2000, 2000);
 
-  system_info::SetCrosHealthdCpuResponse(
-      {core_1, core_2}, {temp_1, temp_2, temp_3}, {core_1_speed, core_2_speed});
+  SetCrosHealthdCpuResponse({core_1, core_2}, {temp_1, temp_2, temp_3},
+                            {core_1_speed, core_2_speed});
   StartSearch(u"cp");
   Wait();
   ASSERT_TRUE(results().empty());
@@ -282,8 +445,8 @@ TEST_F(SystemInfoCardProviderTest, Cpu) {
   system_info::CpuUsageData core_1(1000, 1000, 1000);
   system_info::CpuUsageData core_2(2000, 2000, 2000);
 
-  system_info::SetCrosHealthdCpuResponse(
-      {core_1, core_2}, {temp_1, temp_2, temp_3}, {core_1_speed, core_2_speed});
+  SetCrosHealthdCpuResponse({core_1, core_2}, {temp_1, temp_2, temp_3},
+                            {core_1_speed, core_2_speed});
 
   StartSearch(u"cpu");
   Wait();
@@ -319,9 +482,9 @@ TEST_F(SystemInfoCardProviderTest, Cpu) {
   system_info::CpuUsageData core_1_delta(3000, 2500, 4500);
   system_info::CpuUsageData core_2_delta(1000, 5500, 3500);
 
-  system_info::SetCrosHealthdCpuResponse(
-      {core_1 + core_1_delta, core_2 + core_2_delta},
-      {new_temp_1, new_temp_2, new_temp_3}, {core_1_speed, core_2_speed});
+  SetCrosHealthdCpuResponse({core_1 + core_1_delta, core_2 + core_2_delta},
+                            {new_temp_1, new_temp_2, new_temp_3},
+                            {core_1_speed, core_2_speed});
 
   timer_ptr->Fire();
   Wait();
@@ -329,10 +492,10 @@ TEST_F(SystemInfoCardProviderTest, Cpu) {
   EXPECT_EQ(title.GetText(), u"CPU usage snapshot: 60%");
   EXPECT_EQ(details.GetText(), u"Temperature: 20°C - Current speed: 5.5GHz");
 
-  system_info::SetCrosHealthdCpuResponse({core_1 + core_1_delta + core_1_delta,
-                                          core_2 + core_2_delta + core_2_delta},
-                                         {new_temp_1, new_temp_2, new_temp_3},
-                                         {core_1_speed, core_2_speed});
+  SetCrosHealthdCpuResponse({core_1 + core_1_delta + core_1_delta,
+                             core_2 + core_2_delta + core_2_delta},
+                            {new_temp_1, new_temp_2, new_temp_3},
+                            {core_1_speed, core_2_speed});
 
   StartSearch(u"cpu");
   Wait();
@@ -350,7 +513,7 @@ TEST_F(SystemInfoCardProviderTest, CpuProbeError) {
   base::HistogramTester histogram_tester;
 
   auto cpu_result = healthd_mojom::CpuResult::NewError(
-      system_info::CreateProbeError(healthd_mojom::ErrorType::kFileReadError));
+      CreateProbeError(healthd_mojom::ErrorType::kFileReadError));
   info->cpu_result = std::move(cpu_result);
   ash::cros_healthd::FakeCrosHealthd::Get()
       ->SetProbeTelemetryInfoResponseForTesting(info);
@@ -377,8 +540,8 @@ TEST_F(SystemInfoCardProviderTest, Memory) {
   const uint32_t free_memory_kib = 2000000;
   const uint32_t available_memory_kib = 4000000;
 
-  system_info::SetCrosHealthdMemoryUsageResponse(
-      total_memory_kib, free_memory_kib, available_memory_kib);
+  SetCrosHealthdMemoryUsageResponse(total_memory_kib, free_memory_kib,
+                                    available_memory_kib);
 
   StartSearch(u"memory");
   Wait();
@@ -411,8 +574,8 @@ TEST_F(SystemInfoCardProviderTest, Memory) {
   const uint32_t free_memory_kib_2 = 2000000;
   const uint32_t available_memory_kib_2 = 2000000;
 
-  system_info::SetCrosHealthdMemoryUsageResponse(
-      total_memory_kib_2, free_memory_kib_2, available_memory_kib_2);
+  SetCrosHealthdMemoryUsageResponse(total_memory_kib_2, free_memory_kib_2,
+                                    available_memory_kib_2);
 
   timer_ptr->Fire();
   Wait();
@@ -441,9 +604,8 @@ TEST_F(SystemInfoCardProviderTest, MemoryProbeError) {
   auto info = healthd_mojom::TelemetryInfo::New();
   base::HistogramTester histogram_tester;
 
-  auto memory_result =
-      healthd_mojom::MemoryResult::NewError(system_info::CreateProbeError(
-          healthd_mojom::ErrorType::kSystemUtilityError));
+  auto memory_result = healthd_mojom::MemoryResult::NewError(
+      CreateProbeError(healthd_mojom::ErrorType::kSystemUtilityError));
   info->memory_result = std::move(memory_result);
   ash::cros_healthd::FakeCrosHealthd::Get()
       ->SetProbeTelemetryInfoResponseForTesting(info);
@@ -465,8 +627,8 @@ TEST_F(SystemInfoCardProviderTest, Battery) {
   const double charge_full_design = 26;
   const int32_t cycle_count = 500;
 
-  system_info::SetCrosHealthdBatteryHealthResponse(
-      charge_full_now, charge_full_design, cycle_count);
+  SetCrosHealthdBatteryHealthResponse(charge_full_now, charge_full_design,
+                                      cycle_count);
 
   const auto power_source =
       power_manager::PowerSupplyProperties_ExternalPower_AC;
@@ -477,9 +639,9 @@ TEST_F(SystemInfoCardProviderTest, Battery) {
   const int64_t time_to_empty_secs = 0;
   const double battery_percent = 94.0;
 
-  system_info::SetPowerManagerProperties(
-      power_source, battery_state, is_calculating_battery_time,
-      time_to_full_secs, time_to_empty_secs, battery_percent);
+  SetPowerManagerProperties(power_source, battery_state,
+                            is_calculating_battery_time, time_to_full_secs,
+                            time_to_empty_secs, battery_percent);
   Wait();
 
   StartSearch(u"battery");
@@ -515,9 +677,9 @@ TEST_F(SystemInfoCardProviderTest, Battery) {
   const int64_t new_time_to_full_secs = time_to_full_secs - 100;
   const double new_battery_percent = 96.0;
 
-  system_info::SetPowerManagerProperties(
-      power_source, battery_state, is_calculating_battery_time,
-      new_time_to_full_secs, time_to_empty_secs, new_battery_percent);
+  SetPowerManagerProperties(power_source, battery_state,
+                            is_calculating_battery_time, new_time_to_full_secs,
+                            time_to_empty_secs, new_battery_percent);
   Wait();
 
   EXPECT_EQ(results()[0]->system_info_answer_card_data()->bar_chart_percentage,
@@ -540,8 +702,8 @@ TEST_F(SystemInfoCardProviderTest, BatteryWhileCalculating) {
   const double charge_full_design = 26;
   const int32_t cycle_count = 500;
 
-  system_info::SetCrosHealthdBatteryHealthResponse(
-      charge_full_now, charge_full_design, cycle_count);
+  SetCrosHealthdBatteryHealthResponse(charge_full_now, charge_full_design,
+                                      cycle_count);
 
   const auto power_source =
       power_manager::PowerSupplyProperties_ExternalPower_AC;
@@ -552,9 +714,9 @@ TEST_F(SystemInfoCardProviderTest, BatteryWhileCalculating) {
   const int64_t time_to_empty_secs = 0;
   const double battery_percent = 94.0;
 
-  system_info::SetPowerManagerProperties(
-      power_source, battery_state, is_calculating_battery_time,
-      time_to_full_secs, time_to_empty_secs, battery_percent);
+  SetPowerManagerProperties(power_source, battery_state,
+                            is_calculating_battery_time, time_to_full_secs,
+                            time_to_empty_secs, battery_percent);
   StartSearch(u"battery");
   Wait();
 
@@ -580,7 +742,7 @@ TEST_F(SystemInfoCardProviderTest, BatteryProbeError) {
   base::HistogramTester histogram_tester;
 
   auto battery_result = healthd_mojom::BatteryResult::NewError(
-      system_info::CreateProbeError(healthd_mojom::ErrorType::kParseError));
+      CreateProbeError(healthd_mojom::ErrorType::kParseError));
   info->battery_result = std::move(battery_result);
   ash::cros_healthd::FakeCrosHealthd::Get()
       ->SetProbeTelemetryInfoResponseForTesting(info);
@@ -594,9 +756,9 @@ TEST_F(SystemInfoCardProviderTest, BatteryProbeError) {
   const int64_t time_to_empty_secs = 0;
   const double battery_percent = 94.0;
 
-  system_info::SetPowerManagerProperties(
-      power_source, battery_state, is_calculating_battery_time,
-      time_to_full_secs, time_to_empty_secs, battery_percent);
+  SetPowerManagerProperties(power_source, battery_state,
+                            is_calculating_battery_time, time_to_full_secs,
+                            time_to_empty_secs, battery_percent);
 
   StartSearch(u"battery");
   Wait();
@@ -614,7 +776,7 @@ TEST_F(SystemInfoCardProviderTest, BatteryProbeDataError) {
   auto info = healthd_mojom::TelemetryInfo::New();
   base::HistogramTester histogram_tester;
 
-  system_info::SetCrosHealthdBatteryHealthResponse(0, 0, 0);
+  SetCrosHealthdBatteryHealthResponse(0, 0, 0);
 
   const auto power_source =
       power_manager::PowerSupplyProperties_ExternalPower_AC;
@@ -625,9 +787,9 @@ TEST_F(SystemInfoCardProviderTest, BatteryProbeDataError) {
   const int64_t time_to_empty_secs = 0;
   const double battery_percent = 94.0;
 
-  system_info::SetPowerManagerProperties(
-      power_source, battery_state, is_calculating_battery_time,
-      time_to_full_secs, time_to_empty_secs, battery_percent);
+  SetPowerManagerProperties(power_source, battery_state,
+                            is_calculating_battery_time, time_to_full_secs,
+                            time_to_empty_secs, battery_percent);
 
   StartSearch(u"battery");
   Wait();
@@ -647,8 +809,8 @@ TEST_F(SystemInfoCardProviderTest, BatteryPowerManagerError) {
   const double charge_full_design = 26;
   const int32_t cycle_count = 500;
 
-  system_info::SetCrosHealthdBatteryHealthResponse(
-      charge_full_now, charge_full_design, cycle_count);
+  SetCrosHealthdBatteryHealthResponse(charge_full_now, charge_full_design,
+                                      cycle_count);
 
   std::nullopt_t props = std::nullopt;
   chromeos::FakePowerManagerClient::Get()->UpdatePowerProperties(props);
