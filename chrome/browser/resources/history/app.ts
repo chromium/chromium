ee392b42115b802156529501d8a54285816b5e86
Mingyu Lei <leimy@chromium.org>
2024-05-08 02:34:34 +0000

    Revert "Reland 2: Add HistoryEmbeddings to fieldtrial_testing_config.json"
    This reverts commit 3d2d4c04dbdc0f4aca2f3eb57fc39e875a914f94.
    
    Reason for revert: it seems causing some crashes in the tests (e.g. https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/47538/test-results and https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/27498/test-results), the weakptr of embeddings_service is invalid when it's being used.
    
    Original change's description:
    > Reland 2: Add HistoryEmbeddings to fieldtrial_testing_config.json
    >
    > Reland 2 of this CL:
    > https://chromium-review.googlesource.com/c/chromium/src/+/5515642
    >
    > Now I've finally figured out what's wrong. The Windows 10 Debug bot
    > requires both browser_tests and browser_tests_no_field_trial to pass,
    > so I need to make the tests pass with and without the field trial.
    >
    > Patchset 1 is the original patch unchanged.
    >
    > Further patchsets are the changes required to make them pass with and
    > without the field trial.
    >
    > Bug: b/338245900
    > Change-Id: I25c187514814534d1a258016043ed7a3d2b6c02a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5518587
    > Reviewed-by: manuk hovanesian <manukh@chromium.org>
    > Auto-Submit: Tommy Li <tommycli@chromium.org>
    > Commit-Queue: Tommy Li <tommycli@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1297521}
    
    Bug: b/338245900
    Change-Id: I369625bafdf82a6059a0300243ef7f7b1c3741a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5524295
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297910}

--- chrome/browser/resources/history/app.ts
+++ chrome/browser/resources/history/app.ts
@@ -280,8 +280,6 @@ export class HistoryAppElement extends HistoryAppElementBase {
   private queryState_: QueryState;
   private selectedPage_: string;
   private selectedTab_: number;
-  private lastRecordedSelectedPageHistogramValue_: HistoryPageViewHistogram =
-      HistoryPageViewHistogram.END;
   private showHistoryClusters_: boolean;
   private tabsIcons_: string[];
   private tabsNames_: string[];
@@ -696,12 +694,6 @@ export class HistoryAppElement extends HistoryAppElementBase {
         histogramValue = HistoryPageViewHistogram.HISTORY;
         break;
     }
-
-    // Avoid double-recording the same page consecutively.
-    if (histogramValue === this.lastRecordedSelectedPageHistogramValue_) {
-      return;
-    }
-    this.lastRecordedSelectedPageHistogramValue_ = histogramValue;
 
     this.browserService_!.recordHistogram(
         'History.HistoryPageView', histogramValue,
