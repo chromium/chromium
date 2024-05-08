0df87f3bc8589229f7b59740bf236d82f7c209a2
André Kempe <andre.kempe@arm.com>
2024-05-08 08:27:01 +0000

    Remove iFuncs to fix error at runtime link-time
    When using iFuncs for x64, the runtime linker crashes due to a
    segmentation fault which is probably caused by handling the iFuncs
    in conjunction with other compiler flags.
    
    Since iFuncs are currently only required when compiling for Arm64
    with Memory Tagging Extension or Branch Target Identification
    enabled, this CL removes the iFuncs for other cases.
    
    Bug: 338071862
    Cq-Include-Trybots: luci.chromium.try:linux-centipede-asan-rel
    Change-Id: I88e9813892891ca794824032b0736bffe818efc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514287
    Commit-Queue: Andre Kempe <andre.kempe@arm.com>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297997}

--- base/allocator/partition_allocator/src/partition_alloc/page_allocator_internals_posix.cc
+++ base/allocator/partition_allocator/src/partition_alloc/page_allocator_internals_posix.cc
@@ -1,14 +1,21 @@
 // Copyright 2021 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#include <stdint.h>
 #include <sys/mman.h>
 
-#include "partition_alloc/aarch64_support.h"
 #include "partition_alloc/build_config.h"
 #include "partition_alloc/page_allocator.h"
-#include "partition_alloc/partition_alloc_base/cpu.h"
-#include "partition_alloc/partition_alloc_base/notreached.h"
 #include "partition_alloc/partition_alloc_buildflags.h"
+
+#if BUILDFLAG(HAS_MEMORY_TAGGING) || \
+    (defined(__ARM_FEATURE_BTI_DEFAULT) && (__ARM_FEATURE_BTI_DEFAULT == 1))
+struct __ifunc_arg_t;
+
+#include "partition_alloc/aarch64_support.h"
+
+#define NEEDS_HANDLING_OF_HW_CAPABILITIES
+#endif
 
 // PA_PROT_BTI requests a page that supports BTI landing pads.
 #define PA_PROT_BTI 0x10
@@ -51,6 +58,7 @@ int GetAccessFlags(PageAccessibilityConfiguration accessibility) {
 
 }  // namespace
 
+#if defined(NEEDS_HANDLING_OF_HW_CAPABILITIES)
 using GetAccessFlagsInternalFn = int(PageAccessibilityConfiguration);
 
 extern "C" GetAccessFlagsInternalFn* ResolveGetAccessFlags(
@@ -70,9 +78,16 @@ extern "C" GetAccessFlagsInternalFn* ResolveGetAccessFlags(
     }
   }
 }
+#endif
 
 // Resolve the implementation for GetAccessFlags using an iFunc.
 int GetAccessFlags(PageAccessibilityConfiguration accessibility)
+#if defined(NEEDS_HANDLING_OF_HW_CAPABILITIES)
     __attribute__((ifunc("ResolveGetAccessFlags")));
+#else
+{
+  return GetAccessFlags<false, false>(accessibility);
+}
+#endif
 
 }  // namespace partition_alloc::internal
