d8f6bff336a2ab2b858b5bcbf7acfe73d1f5ec62
Katie Dektar <katie@chromium.org>
2024-05-08 23:24:51 +0000

    [a11y] Update caret blink rate when rate changes
    When a user is using the touchscreen, they can slide the
    caret blink rate setting slider while the caret is shown.
    This changes makes the caret blink rate update immediately
    in that case by checking the current blink rate against
    the blink rate used last time blinking was started.
    
    Bug: b/339258778
    Test: Manual
    Change-Id: I2fdb9adc923b2050f9ae3856ba43e6f60647c4fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526530
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298419}

--- third_party/blink/renderer/core/editing/frame_caret.cc
+++ third_party/blink/renderer/core/editing/frame_caret.cc
@@ -140,13 +140,21 @@ void FrameCaret::StopCaretBlinkTimer() {
 
 void FrameCaret::StartBlinkCaret() {
   // Start blinking with a black caret. Be sure not to restart if we're
-  // already blinking in the right location.
-  if (caret_blink_timer_.IsActive())
-    return;
-
+  // already blinking in the right location at the right rate.
   base::TimeDelta blink_interval = LayoutTheme::GetTheme().CaretBlinkInterval();
-  if (!blink_interval.is_zero())
+  if (caret_blink_timer_.IsActive()) {
+    if (blink_interval == caret_blink_timer_.RepeatInterval()) {
+      // Already blinking at the right rate.
+      return;
+    }
+
+    // If it was active but we are changing the blink rate, reset state.
+    StopCaretBlinkTimer();
+  }
+
+  if (!blink_interval.is_zero()) {
     caret_blink_timer_.StartRepeating(blink_interval, FROM_HERE);
+  }
 
   display_item_client_->SetActive(true);
   SetVisibleIfActive(true);
