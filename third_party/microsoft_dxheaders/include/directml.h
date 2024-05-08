37a8609bbbe5200fa034718304b3c3a04296240b
Ningxin Hu <ningxin.hu@intel.com>
2024-05-08 23:09:10 +0000

    Fix the incorrect casing issue of including DirectML.h
    This change fixes the "Linux to Windows cross compile" build failure of
    previous CL
    https://chromium-review.googlesource.com/c/chromium/src/+/5514432
    
    Bug: 338434855
    Change-Id: Ic4db70e85b65996e2dd48fe0e99b92967eada3dc
    Cq-Include-Trybots: luci.chromium.try:linux-win-cross-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5527424
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298410}

--- third_party/microsoft_dxheaders/include/directml.h
+++ third_party/microsoft_dxheaders/include/directml.h
@@ -6,7 +6,7 @@
 #define THIRD_PARTY_MICROSOFT_DXHEADERS_INCLUDE_DIRECTML_H_
 
 #define DML_TARGET_VERSION_USE_LATEST
-#include "third_party/microsoft_dxheaders/src/include/directx/directml.h"
+#include "third_party/microsoft_dxheaders/src/include/directx/DirectML.h"
 #undef DML_TARGET_VERSION_USE_LATEST
 
 #endif  // THIRD_PARTY_MICROSOFT_DXHEADERS_INCLUDE_DIRECTML_H_
