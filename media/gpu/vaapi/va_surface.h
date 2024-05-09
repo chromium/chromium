4dffaf071a6038dd0ae147305bac12f9af9adfa7
Miguel Casas-Sanchez <mcasas@chromium.org>
2024-05-09 20:33:00 +0000

    media/gpu/vaapi: Fix VASurface comments
    Removing most of the comment since it talks about
    deprecated classes and uses.
    
    Bug: 339518553
    Change-Id: I069f324ad73bc2a25794e9744abf527dfbecf407
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5525701
    Reviewed-by: Hang Nguyen <hnt@chromium.org>
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298846}

--- media/gpu/vaapi/va_surface.h
+++ media/gpu/vaapi/va_surface.h
@@ -16,72 +16,9 @@
 
 namespace media {
 
-// A VA-API-specific decode surface used by VaapiH264Decoder to decode into
-// and use as reference for decoding other surfaces. It is also handed by the
-// decoder to VaapiVideoDecodeAccelerator when the contents of the surface are
-// ready and should be displayed. VAVDA converts the surface contents into an
-// X/Drm Pixmap bound to a texture for display and releases its reference to it.
-// Decoder releases its references to the surface when it's done decoding and
-// using it as reference. Note that a surface may still be used for reference
-// after it's been sent to output and also after it is no longer used by VAVDA.
-// Thus, the surface can be in use by both VAVDA and the Decoder at the same
-// time, or by either of them, with the restriction that VAVDA will never get
-// the surface until the contents are ready, and it is guaranteed that the
-// contents will not change after that.
-// When both the decoder and VAVDA release their references to the surface,
-// it is freed and the release callback is executed to put the surface back
-// into available surfaces pool, which is managed externally.
-//
-//   VASurfaceID is allocated in VaapiWrapper.
-//        |
-// +----->|
-// |      v
-// | VASurfaceID is put onto VaapiVideoDecodeAccelerator::available_va_surfaces_
-// |      |      list.
-// |      v
-// | VASurfaceID is taken off of the VVDA:available_va_surfaces_ when
-// |      |      VaapiH264Decoder requests more output surfaces, is wrapped into
-// |      |      a VASurface and passed to VaapiH264Decoder.
-// |      v
-// | VASurface is put onto VaapiH264Decoder::available_va_surfaces_, keeping
-// |      |      the only reference to it until it's needed for decoding.
-// |      v
-// | VaapiH264Decoder starts decoding a new frame. It takes a VASurface off of
-// |      |      VHD::available_va_surfaces_ and assigns it to a DecodeSurface,
-// |      |      which now keeps the only reference.
-// |      v
-// | DecodeSurface is used for decoding, putting data into associated VASurface.
-// |      |
-// |      |--------------------------------------------------+
-// |      |                                                  |
-// |      v                                                  v
-// | DecodeSurface is to be output.              VaapiH264Decoder uses the
-// | VaapiH264Decoder passes the associated      DecodeSurface and associated
-// | VASurface to VaapiVideoDecodeAccelerator,   VASurface as reference for
-// | which stores it (taking a ref) on           decoding more frames.
-// | pending_output_cbs_ queue until an output               |
-// | VaapiPicture becomes available.                         v
-// |                 |                           Once the DecodeSurface is not
-// |                 |                           needed as reference anymore,
-// |                 v                           it is released, releasing the
-// | A VaapiPicture becomes available after      associated VASurface reference.
-// | the client of VVDA returns                              |
-// | a PictureBuffer associated with it. VVDA                |
-// | puts the contents of the VASurface into                 |
-// | it and releases the reference to VASurface.             |
-// |                 |                                       |
-// |                 '---------------------------------------'
-// |                                     |
-// |                                     v
-// | Neither VVDA nor VHD hold a reference to VASurface. VASurface is released,
-// | ReleaseCB gets called in its destructor, which puts the associated
-// | VASurfaceID back onto VVDA::available_va_surfaces_.
-// |                                     |
-// '-------------------------------------|
-//                                       |
-//                                       v
-//                       VaapiWrapper frees VASurfaceID.
-//
+// A VA-API-specific VASurfaceID and metadata holder.
+// TODO(339518553): Remove this class in favour of its not ref-counted
+// ScopedVASurface sibling.
 class VASurface : public base::RefCountedThreadSafe<VASurface> {
  public:
   using ReleaseCB = base::OnceCallback<void(VASurfaceID)>;
