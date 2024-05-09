215caffd4cdb0866174d7817480234dc4ec4a166
EmiliaPaz <emiliapaz@chromium.org>
2024-05-09 00:47:43 +0000

    [Extensions] Add extension rows to mv2 deprecation panel
    Adds an extension row with icon, name and buttons for each extension
    in the element.
    
    Screenshot: https://drive.google.com/file/d/1EYcJHlJhOwoaqtljvR1MHvJx8GjVJIHF/view?usp=sharing
    
    Bug: 337191307
    Change-Id: I27fd52916f97a265884aaaec41ad4b7080644833
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5523868
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298453}

--- chrome/test/data/webui/extensions/mv2_deprecation_panel_test.ts
+++ chrome/test/data/webui/extensions/mv2_deprecation_panel_test.ts
@@ -6,7 +6,8 @@
 import 'chrome://extensions/extensions.js';
 
 import type {ExtensionsMv2DeprecationPanelElement} from 'chrome://extensions/extensions.js';
-import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 import {isVisible} from 'chrome://webui-test/test_util.js';
 
 import {createExtensionInfo} from './test_util.js';
@@ -16,8 +17,13 @@ suite('ExtensionsMV2DeprecationPanel', function() {
 
   setup(function() {
     panelElement = document.createElement('extensions-mv2-deprecation-panel');
-    panelElement.extensions = [createExtensionInfo()];
+    panelElement.extensions = [createExtensionInfo({
+      name: 'Extension A',
+      isAffectedByMV2Deprecation: true,
+    })];
     document.body.appendChild(panelElement);
+
+    return flushTasks();
   });
 
   test('header content is always visible', function() {
@@ -25,5 +31,32 @@ suite('ExtensionsMV2DeprecationPanel', function() {
         isVisible(panelElement.shadowRoot!.querySelector('.header-text')));
     assertTrue(
         isVisible(panelElement.shadowRoot!.querySelector('.header-button')));
+  });
+
+  test('correct number of extension rows', async function() {
+    // Verify there is one extension row for the extension added at setup.
+    let extensionRows =
+        panelElement.shadowRoot!.querySelectorAll('.extension-row');
+    assertEquals(extensionRows.length, 1);
+    assertEquals(
+        extensionRows[0]?.querySelector('.extension-name')?.textContent?.trim(),
+        'Extension A');
+
+    // Add a new extension to the panel.
+    panelElement.push('extensions', createExtensionInfo({
+                        name: 'Extension B',
+                        isAffectedByMV2Deprecation: true,
+                      }));
+    await flushTasks();
+
+    // Verify there are two extension rows.
+    extensionRows = panelElement.shadowRoot!.querySelectorAll('.extension-row');
+    assertEquals(extensionRows.length, 2);
+    assertEquals(
+        extensionRows[0]?.querySelector('.extension-name')?.textContent?.trim(),
+        'Extension A');
+    assertEquals(
+        extensionRows[1]?.querySelector('.extension-name')?.textContent?.trim(),
+        'Extension B');
   });
 });
