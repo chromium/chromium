0bf35b00aade6855fc8df8e2f6b1c594da02d45a
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-05-10 20:47:18 +0000

    [Extensions] Remove unused ctor on WebstoreInstallWithPrompt
    WebstoreInstallWithPrompt had an unused ctor. Remove it.
    
    Bug: None
    Change-Id: I76d0471ec1b38f0f85f8f759645999d91a398167
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526026
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299498}

--- chrome/browser/extensions/webstore_install_with_prompt.cc
+++ chrome/browser/extensions/webstore_install_with_prompt.cc
@@ -17,21 +17,6 @@ namespace extensions {
 WebstoreInstallWithPrompt::WebstoreInstallWithPrompt(
     const std::string& webstore_item_id,
     Profile* profile,
-    Callback callback)
-    : WebstoreStandaloneInstaller(webstore_item_id,
-                                  profile,
-                                  std::move(callback)),
-      show_post_install_ui_(true),
-      dummy_web_contents_(
-          WebContents::Create(WebContents::CreateParams(profile))),
-      parent_window_(nullptr) {
-  dummy_web_contents_->SetOwnerLocationForDebug(FROM_HERE);
-  set_install_source(WebstoreInstaller::INSTALL_SOURCE_OTHER);
-}
-
-WebstoreInstallWithPrompt::WebstoreInstallWithPrompt(
-    const std::string& webstore_item_id,
-    Profile* profile,
     gfx::NativeWindow parent_window,
     Callback callback)
     : WebstoreStandaloneInstaller(webstore_item_id,
