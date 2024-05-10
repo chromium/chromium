e5f922579a6fd941e146f29c41245a255d53eb20
Kristi Saney <kristislee@google.com>
2024-05-10 00:06:40 +0000

    Re-enable AppReceivesToolbarChanges on Windows
    crrev.com/c/5528102 seemed to fix the Mac errors, and looking at the
    old Windows errors, those seemed to have the same issue, so we should be
    able to re-enable safely.
    
    Bug: none
    Change-Id: I7e0f5663fc14dc8cfa061be09328ca804a878b9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530550
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298993}

--- chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
+++ chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
@@ -91,13 +91,7 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, UpdateContent) {
                    "mocha.run()");
 }
 
-// TODO(crbug.com/337936370): Flaky on Windows.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_AppReceivesToolbarChanges DISABLED_AppReceivesToolbarChanges
-#else
-#define MAYBE_AppReceivesToolbarChanges AppReceivesToolbarChanges
-#endif
-IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, MAYBE_AppReceivesToolbarChanges) {
+IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, AppReceivesToolbarChanges) {
   RunSidePanelTest(
       "side_panel/read_anything/app_receives_toolbar_changes_test.js",
       "mocha.run()");
