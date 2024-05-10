e6bc05bcc572979a23c025b9f2ee5c8d52bc7c37
Alan Cutter <alancutter@chromium.org>
2024-05-10 04:43:15 +0000

    Revert "oak: show shelf background when exit Overview"
    This reverts commit fdcaed5c78fa886fa067cdd523577147ee192686.
    
    Reason for revert: UAF crash on SAN bots. Needs to listen for OverviewController destruction and unobserve it.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/43113/test-results?sortby=&groupby=
    
    Original change's description:
    > oak: show shelf background when exit Overview
    >
    > The shelf background should be shown when start to exit Overview,
    > but currently it shows after fully exiting the Overview. The problem
    > is because we forgot to observe OverviewController such that the
    > background does not update when exiting the Overview.
    >
    > Bug: b:339683597
    > Change-Id: Iccd7a339d9e2c8a8b24c2777e16d122f89b279cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530225
    > Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1298992}
    
    Bug: b:339683597
    Change-Id: I2ed08d4331571e3398e8d7e010ed76a365ece7b1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528346
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alan Cutter <alancutter@google.com>
    Auto-Submit: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299074}

--- ash/shelf/shelf_widget.h
+++ ash/shelf/shelf_widget.h
@@ -32,7 +32,6 @@ class DragHandle;
 class FocusCycler;
 class HotseatWidget;
 class LoginShelfView;
-class OverviewController;
 class Shelf;
 class ShelfLayoutManager;
 class ShelfNavigationWidget;
@@ -229,9 +228,6 @@ class ASH_EXPORT ShelfWidget : public SessionObserver,
   raw_ptr<LoginShelfView> login_shelf_view_;
 
   ScopedSessionObserver scoped_session_observer_;
-
-  base::ScopedObservation<OverviewController, OverviewObserver>
-      overview_observation_{this};
 
   size_t force_show_hotseat_count_ = 0;
 
