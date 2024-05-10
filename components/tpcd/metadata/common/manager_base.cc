3a7f616b310bf3033b88af8e10c97dd1129804da
Joshua Pawlicki <waffles@chromium.org>
2024-05-10 19:46:45 +0000

    Revert "Move the grace period opt out logic to the tpcd:metadata component"
    This reverts commit 51ad2b231b95bdcca98b252a8bf601161ef5f61b.
    
    Reason for revert: ios compile failure @ https://ci.chromium.org/ui/p/chromium/builders/ci/ios-device/359152/overview
    
    Original change's description:
    > Move the grace period opt out logic to the tpcd:metadata component
    >
    > This CL moves the logic to ignore certain metadata grants to the
    > tpcd::metadata component to keep things tighty and avoid future possible
    > bugs, as at this point, it's more of an internal behavior to tpcd
    > metadata than an external one.
    >
    > Bug: b:339851693
    > Change-Id: I64a8b4ae9c169c17cede3005256e5ee9345128ee
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530773
    > Reviewed-by: Anton Maliev <amaliev@chromium.org>
    > Reviewed-by: Dylan Cutler <dylancutler@google.com>
    > Auto-Submit: Jonathan Njeunje <njeunje@chromium.org>
    > Commit-Queue: Dylan Cutler <dylancutler@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1299441}
    
    Bug: b:339851693
    Change-Id: Ic3fa229af0c133d6d3762ad92026891525fb895e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531111
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Owners-Override: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299445}

--- components/tpcd/metadata/common/manager_base.cc
+++ components/tpcd/metadata/common/manager_base.cc
@@ -4,7 +4,6 @@
 
 #include "components/tpcd/metadata/common/manager_base.h"
 
-#include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_rules.h"
 #include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/content_settings/core/common/host_indexed_content_settings.h"
@@ -13,22 +12,6 @@
 namespace tpcd::metadata::common {
 ManagerBase::ManagerBase() = default;
 ManagerBase::~ManagerBase() = default;
-
-// Whether to bypass any available grants from the Third Party Cookie
-// Deprecation TPCD Metadata.
-bool IgnoreTpcdDtGracePeriodMetadataGrant(
-    const content_settings::SettingInfo* info) {
-  switch (info->metadata.tpcd_metadata_cohort()) {
-    case content_settings::mojom::TpcdMetadataCohort::GRACE_PERIOD_FORCED_OFF:
-      return true;
-    case content_settings::mojom::TpcdMetadataCohort::DEFAULT:
-    case content_settings::mojom::TpcdMetadataCohort::GRACE_PERIOD_FORCED_ON:
-      return false;
-  }
-
-  NOTREACHED_NORETURN() << "Invalid enum value: "
-                        << info->metadata.tpcd_metadata_cohort();
-}
 
 ContentSetting ManagerBase::GetContentSetting(
     const Grants& grants,
@@ -63,21 +46,11 @@ ContentSetting ManagerBase::GetContentSetting(
     }
   }
 
-  // The `first_party_url` and `third_party_url` wasn't granted access by any of
-  // the available metadata entries.
   if (out_info && result == CONTENT_SETTING_BLOCK) {
     out_info->primary_pattern = ContentSettingsPattern::Wildcard();
     out_info->secondary_pattern = ContentSettingsPattern::Wildcard();
     out_info->metadata = {};
   }
-
-  // The `first_party_url` and `third_party_url` was granted access by at least
-  // one of the available metadata entries, but shouldn't be considered as its
-  // grace period is forced off.
-  else if (out_info && IgnoreTpcdDtGracePeriodMetadataGrant(out_info)) {
-    result = CONTENT_SETTING_BLOCK;
-  }
-
   return result;
 }
 
--- components/tpcd/metadata/common/manager_base_unittest.cc
+++ components/tpcd/metadata/common/manager_base_unittest.cc
@@ -8,10 +8,8 @@
 #include <memory>
 #include <vector>
 
-#include "base/notreached.h"
 #include "base/test/scoped_feature_list.h"
 #include "components/content_settings/core/common/content_settings.h"
-#include "components/content_settings/core/common/content_settings_enums.mojom-shared.h"
 #include "components/content_settings/core/common/host_indexed_content_settings.h"
 #include "net/base/features.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -19,10 +17,14 @@
 
 namespace tpcd::metadata::common {
 
-class ManagerBaseTest : public testing::Test {
+class ManagerBaseTest
+    : public testing::Test,
+      public testing::WithParamInterface</*kTpcdMetadataGrants*/ bool> {
  public:
   ManagerBaseTest() = default;
   ~ManagerBaseTest() override = default;
+
+  bool IsTpcdMetadataGrantsEnabled() { return GetParam(); }
 
   ManagerBase* GetManagerBase() {
     if (!manager_base_) {
@@ -30,19 +32,6 @@ class ManagerBaseTest : public testing::Test {
     }
     return manager_base_.get();
   }
-
- private:
-  std::unique_ptr<ManagerBase> manager_base_;
-};
-
-class ManagerBaseFeatureTest
-    : public ManagerBaseTest,
-      public testing::WithParamInterface</*kTpcdMetadataGrants*/ bool> {
- public:
-  ManagerBaseFeatureTest() = default;
-  ~ManagerBaseFeatureTest() override = default;
-
-  bool IsTpcdMetadataGrantsEnabled() { return GetParam(); }
 
  protected:
   void SetUp() override {
@@ -60,13 +49,12 @@ class ManagerBaseFeatureTest
 
  private:
   base::test::ScopedFeatureList scoped_list_;
+  std::unique_ptr<ManagerBase> manager_base_;
 };
 
-INSTANTIATE_TEST_SUITE_P(/* no label */,
-                         ManagerBaseFeatureTest,
-                         testing::Bool());
+INSTANTIATE_TEST_SUITE_P(/* no label */, ManagerBaseTest, testing::Bool());
 
-TEST_P(ManagerBaseFeatureTest, GetContentSetting) {
+TEST_P(ManagerBaseTest, GetContentSetting) {
   const std::string primary_pattern_spec = "https://www.der.com";
   const std::string secondary_pattern_spec = "https://www.foo.com";
 
@@ -124,100 +112,4 @@ TEST_P(ManagerBaseFeatureTest, GetContentSetting) {
   }
 }
 
-class ManagerBaseCohortTest
-    : public ManagerBaseTest,
-      public testing::WithParamInterface<
-          /*content_settings::mojom::TpcdMetadataCohort:*/ int32_t> {
- public:
-  ManagerBaseCohortTest() = default;
-  ~ManagerBaseCohortTest() override = default;
-
-  content_settings::mojom::TpcdMetadataCohort GetCohortBeingTested() {
-    return static_cast<content_settings::mojom::TpcdMetadataCohort>(GetParam());
-  }
-
-  ContentSetting ExpectedContentSetting(
-      const content_settings::mojom::TpcdMetadataCohort cohort) {
-    switch (cohort) {
-      case content_settings::mojom::TpcdMetadataCohort::GRACE_PERIOD_FORCED_OFF:
-        return CONTENT_SETTING_BLOCK;
-      case content_settings::mojom::TpcdMetadataCohort::DEFAULT:
-      case content_settings::mojom::TpcdMetadataCohort::GRACE_PERIOD_FORCED_ON:
-        return CONTENT_SETTING_ALLOW;
-    }
-
-    NOTREACHED_NORETURN() << "Invalid enum value: " << GetCohortBeingTested();
-  }
-
- protected:
-  void SetUp() override {
-    std::vector<base::test::FeatureRef> enabled_features;
-    std::vector<base::test::FeatureRef> disabled_features;
-
-    enabled_features.push_back(net::features::kTpcdMetadataGrants);
-
-    scoped_list_.InitWithFeatures(enabled_features, disabled_features);
-  }
-
- private:
-  base::test::ScopedFeatureList scoped_list_;
-  std::unique_ptr<ManagerBase> manager_base_;
-};
-
-INSTANTIATE_TEST_SUITE_P(
-    /* no label */,
-    ManagerBaseCohortTest,
-    testing::Range<int32_t>(
-        static_cast<int32_t>(
-            content_settings::mojom::TpcdMetadataCohort::kMinValue),
-        static_cast<int32_t>(
-            content_settings::mojom::TpcdMetadataCohort::kMaxValue) +
-            1,
-        /*step=*/1));
-TEST_P(ManagerBaseCohortTest, GetContentSetting) {
-  const std::string primary_pattern_spec = "https://www.der.com";
-  const std::string secondary_pattern_spec = "https://www.foo.com";
-
-  GURL first_party_url = GURL(secondary_pattern_spec);
-  GURL third_party_url = GURL(primary_pattern_spec);
-  GURL third_party_url_no_grants = GURL("https://www.bar.com");
-
-  base::Value value(ContentSetting::CONTENT_SETTING_ALLOW);
-  content_settings::RuleMetaData rule_metadata;
-  rule_metadata.set_tpcd_metadata_rule_source(
-      content_settings::mojom::TpcdMetadataRuleSource::SOURCE_TEST);
-  rule_metadata.set_tpcd_metadata_cohort(GetCohortBeingTested());
-
-  ContentSettingsForOneType grants;
-  grants.emplace_back(
-      ContentSettingsPattern::FromString(primary_pattern_spec),
-      ContentSettingsPattern::FromString(secondary_pattern_spec),
-      std::move(value),
-      /*source=*/std::string(), /*incognito=*/false, rule_metadata);
-
-  {
-    content_settings::SettingInfo out_info;
-    EXPECT_EQ(GetManagerBase()->GetContentSetting(grants, third_party_url,
-                                                  first_party_url, &out_info),
-              ExpectedContentSetting(GetCohortBeingTested()));
-    EXPECT_EQ(out_info.primary_pattern.ToString(), primary_pattern_spec);
-    EXPECT_EQ(out_info.secondary_pattern.ToString(), secondary_pattern_spec);
-    EXPECT_EQ(out_info.metadata.tpcd_metadata_rule_source(),
-              content_settings::mojom::TpcdMetadataRuleSource::SOURCE_TEST);
-    EXPECT_EQ(out_info.metadata.tpcd_metadata_cohort(), GetCohortBeingTested());
-  }
-
-  {
-    content_settings::SettingInfo out_info;
-    EXPECT_EQ(
-        GetManagerBase()->GetContentSetting(grants, third_party_url_no_grants,
-                                            first_party_url, &out_info),
-        CONTENT_SETTING_BLOCK);
-    EXPECT_EQ(out_info.primary_pattern.ToString(), "*");
-    EXPECT_EQ(out_info.secondary_pattern.ToString(), "*");
-    EXPECT_EQ(
-        out_info.metadata.tpcd_metadata_rule_source(),
-        content_settings::mojom::TpcdMetadataRuleSource::SOURCE_UNSPECIFIED);
-  }
-}
 }  // namespace tpcd::metadata::common
