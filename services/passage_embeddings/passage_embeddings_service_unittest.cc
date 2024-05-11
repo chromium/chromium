2fa23610d30ac35e304a62926edf5c95f89be57e
Zekun Jiang <zekunjiang@google.com>
2024-05-11 02:27:23 +0000

    Add unit tests to PassageEmbeddingsService.
    Bug: b/339501711
    Change-Id: Ic1bb9a72d5351b7262cf441d13d96bcffc856058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530098
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299660}

--- services/passage_embeddings/passage_embeddings_service_unittest.cc
+++ services/passage_embeddings/passage_embeddings_service_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "services/passage_embeddings/passage_embeddings_service.h"
 
+#include "base/path_service.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -13,34 +14,136 @@
 namespace passage_embeddings {
 namespace {
 
+constexpr uint32_t kInputWindowSize = 256;
+constexpr size_t kEmbeddingsOutputSize = 768;
+
 class PassageEmbeddingsServiceTest : public testing::Test {
  public:
   PassageEmbeddingsServiceTest()
-      : service_impl_(service_.BindNewPipeAndPassReceiver()),
-        embedder_impl_(embedder_.BindNewPipeAndPassReceiver()) {}
+      : service_impl_(service_.BindNewPipeAndPassReceiver()) {}
 
   mojo::Remote<mojom::PassageEmbeddingsService>& service() { return service_; }
-  mojo::Remote<mojom::PassageEmbedder>& embedder() { return embedder_; }
+
+  void SetUp() override {
+    base::FilePath test_data_dir;
+    base::PathService::Get(base::DIR_SRC_TEST_DATA_ROOT, &test_data_dir);
+    test_data_dir = test_data_dir.AppendASCII("services")
+                        .AppendASCII("test")
+                        .AppendASCII("data")
+                        .AppendASCII("passage_embeddings");
+    embeddings_path_ =
+        test_data_dir.AppendASCII("dummy_embeddings_model.tflite");
+    sp_path_ = test_data_dir.AppendASCII("sentencepiece.model");
+  }
+
+  mojom::PassageEmbeddingsLoadModelsParamsPtr MakeParams(
+      base::FilePath embeddings_path,
+      base::FilePath sp_path,
+      uint32_t input_window_size) {
+    auto params = mojom::PassageEmbeddingsLoadModelsParams::New();
+    params->embeddings_model = base::File(
+        embeddings_path, base::File::FLAG_OPEN | base::File::FLAG_READ);
+    params->sp_model =
+        base::File(sp_path, base::File::FLAG_OPEN | base::File::FLAG_READ);
+    params->input_window_size = input_window_size;
+    return params;
+  }
+
+ protected:
+  base::FilePath embeddings_path_;
+  base::FilePath sp_path_;
 
  private:
   base::test::TaskEnvironment task_environment_;
   mojo::Remote<mojom::PassageEmbeddingsService> service_;
   PassageEmbeddingsService service_impl_;
-  mojo::Remote<mojom::PassageEmbedder> embedder_;
-  PassageEmbedder embedder_impl_;
 };
 
-TEST_F(PassageEmbeddingsServiceTest, TestStub) {
+TEST_F(PassageEmbeddingsServiceTest, LoadValidModels) {
   base::RunLoop run_loop;
+  mojom::PassageEmbeddingsLoadModelsParamsPtr params =
+      MakeParams(embeddings_path_, sp_path_, kInputWindowSize);
+  mojo::Remote<mojom::PassageEmbedder> embedder_remote;
+  service()->LoadModels(
+      std::move(params), embedder_remote.BindNewPipeAndPassReceiver(),
+      base::BindLambdaForTesting([&](bool load_model_success) {
+        EXPECT_TRUE(load_model_success);
+        run_loop.Quit();
+      }));
+  run_loop.Run();
+}
 
-  embedder()->GenerateEmbeddings(
+TEST_F(PassageEmbeddingsServiceTest, LoadModelsWithInvalidEmbeddingsModel) {
+  base::RunLoop run_loop;
+  mojom::PassageEmbeddingsLoadModelsParamsPtr params =
+      MakeParams(sp_path_, sp_path_, kInputWindowSize);
+  mojo::Remote<mojom::PassageEmbedder> embedder_remote;
+  service()->LoadModels(
+      std::move(params), embedder_remote.BindNewPipeAndPassReceiver(),
+      base::BindLambdaForTesting([&](bool load_model_success) {
+        EXPECT_FALSE(load_model_success);
+        run_loop.Quit();
+      }));
+  run_loop.Run();
+}
+
+TEST_F(PassageEmbeddingsServiceTest, LoadModelsWithInvalidSpModel) {
+  base::RunLoop run_loop;
+  mojom::PassageEmbeddingsLoadModelsParamsPtr params =
+      MakeParams(embeddings_path_, embeddings_path_, kInputWindowSize);
+  mojo::Remote<mojom::PassageEmbedder> embedder_remote;
+  service()->LoadModels(
+      std::move(params), embedder_remote.BindNewPipeAndPassReceiver(),
+      base::BindLambdaForTesting([&](bool load_model_success) {
+        EXPECT_FALSE(load_model_success);
+        run_loop.Quit();
+      }));
+  run_loop.Run();
+}
+
+TEST_F(PassageEmbeddingsServiceTest, LoadModelsWithInvalidInputWindowSize) {
+  base::RunLoop run_loop;
+  mojom::PassageEmbeddingsLoadModelsParamsPtr params =
+      MakeParams(embeddings_path_, sp_path_, 0u);
+  mojo::Remote<mojom::PassageEmbedder> embedder_remote;
+  service()->LoadModels(
+      std::move(params), embedder_remote.BindNewPipeAndPassReceiver(),
+      base::BindLambdaForTesting([&](bool load_model_success) {
+        EXPECT_FALSE(load_model_success);
+        run_loop.Quit();
+      }));
+  run_loop.Run();
+}
+
+TEST_F(PassageEmbeddingsServiceTest, RespondsWithEmbeddings) {
+  base::RunLoop load_models_run_loop;
+  mojom::PassageEmbeddingsLoadModelsParamsPtr params =
+      MakeParams(embeddings_path_, sp_path_, kInputWindowSize);
+  mojo::Remote<mojom::PassageEmbedder> embedder_remote;
+  service()->LoadModels(
+      std::move(params), embedder_remote.BindNewPipeAndPassReceiver(),
+      base::BindLambdaForTesting([&](bool load_model_success) {
+        EXPECT_TRUE(load_model_success);
+        load_models_run_loop.Quit();
+      }));
+  load_models_run_loop.Run();
+
+  base::RunLoop generate_embeddings_run_loop;
+  embedder_remote->GenerateEmbeddings(
       {"hello", "world"},
       base::BindLambdaForTesting(
           [&](const std::vector<mojom::PassageEmbeddingsResultPtr> results) {
-            EXPECT_EQ(results.size(), 0u);
-            run_loop.Quit();
+            EXPECT_EQ(results.size(), 2u);
+            EXPECT_EQ(results[0]->passage, "hello");
+            EXPECT_EQ(results[1]->passage, "world");
+
+            for (const auto& result : results) {
+              EXPECT_EQ(result->embeddings.size(), kEmbeddingsOutputSize);
+            }
+
+            generate_embeddings_run_loop.Quit();
           }));
-  run_loop.Run();
+  generate_embeddings_run_loop.Run();
 }
 
 }  // namespace
