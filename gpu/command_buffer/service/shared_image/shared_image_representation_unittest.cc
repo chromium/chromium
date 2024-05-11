ffcc6511ef6d1523bfbda84af508355af68b4693
Jiawei Shao <jiawei.shao@intel.com>
2024-05-11 06:28:20 +0000

    Remove textureReference() in shared_image_representation_unittest
    Bug: dawn:2234
    Change-Id: I21c2bf5a8d3db66b6eff2678224b2e1ea9413fcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5532905
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1299691}

--- gpu/command_buffer/service/shared_image/shared_image_representation_unittest.cc
+++ gpu/command_buffer/service/shared_image/shared_image_representation_unittest.cc
@@ -190,11 +190,7 @@ TEST_F(SharedImageRepresentationTest, DawnClearing) {
   // wgpu::Texture(reinterpret_cast<WGPUTexture>(203)), so we have to override
   // the texture reference/release procs to avoid crashing.
   DawnProcTable procs = {};
-#if defined(WGPU_BREAKING_REFERENCE_ADDREF)
   procs.textureAddRef = [](WGPUTexture) {};
-# else
-  procs.textureReference = [](WGPUTexture) {};
-#endif
   procs.textureRelease = [](WGPUTexture) {};
   dawnProcSetProcs(&procs);
 
