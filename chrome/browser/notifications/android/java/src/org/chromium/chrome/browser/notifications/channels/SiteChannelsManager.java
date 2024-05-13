47ed18d1c67b7a02dc5a353e635fd0d356833f40
Min Qin <qinmin@chromium.org>
2024-05-13 16:55:13 +0000

    Convert the logic of reusing an old channel to an assert when creating a site channel
    For site channel creation, there are 2 callers:
    NotificationSettingsBridge and NotificationChannelPreserver.
    1. For NotificationSettingBridge, createChannel() is called
    by native NotificationChannelProviderAndroid. in the native code,
    CreateChannel() is always called right after a getSiteChannels() call,
    so there is no need to call getSiteChannelForOrigin() again inside createSiteChannel() on the java side.
    2. For NotificationChannelPreserver, createSiteChannel() is only
    called after the channel has been previously deleted, so there
    is also no need to check getSiteChannelForOrigin().
    
    In addition to the above reasoning, using an old channel might
    be problematic. For example, if Chrome wants to disable a previously
    enabled siteChannel, calling this method will not work when setting
    enabled = false.
    
    Since getSiteChannelForOrigin() can be a very expensive call due
    to the fact that NotificationManager lives in another process, so
    this CL turns the call into an assert for now to ensure
    the above logic is correct.
    
    Bug: b:242038130, b:242028843
    Change-Id: I2b8a4a4413a430e92ed145d6d1d7881090393215
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519597
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300095}

--- chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/channels/SiteChannelsManager.java
+++ chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/channels/SiteChannelsManager.java
@@ -48,9 +48,12 @@ public class SiteChannelsManager {
     }
 
     /**
-     * Creates a channel for the given origin, unless a channel for this origin already exists. The
-     * channel will appear within the Sites channel group, with default importance, or no importance
-     * if created as blocked.
+     * Creates a channel for the given origin. Don't call this if the channel for the origin already
+     * exists, as the returned SiteChannel might not have the same description or importance as
+     * expected. See:
+     * https://developer.android.com/reference/android/app/NotificationManager#createNotificationChannel(android.app.NotificationChannel).
+     * The newly created channel will appear within the Sites channel group, with default
+     * importance, or no importance if created as blocked.
      *
      * @param origin The site origin, to be used as the channel's user-visible name.
      * @param creationTime A string representing the time of channel creation.
@@ -58,10 +61,8 @@ public class SiteChannelsManager {
      * @return The channel created for the given origin.
      */
     public SiteChannel createSiteChannel(String origin, long creationTime, boolean enabled) {
-        SiteChannel preexistingChannel = getSiteChannelForOrigin(origin);
-        if (preexistingChannel != null) {
-            return preexistingChannel;
-        }
+        assert getSiteChannelForOrigin(origin) == null;
+
         // Channel group must be created before the channel.
         NotificationChannelGroup channelGroup =
                 ChromeChannelDefinitions.getInstance()
