4f528b54f816ac7071a3063fe6719fb074058e08
David Pennington <dpenning@chromium.org>
2024-05-13 20:35:07 +0000

    [STGV2] Add localized strings for deletion dialog
    All strings in the deletion dialog need updated text for localization.
    Added here, and made modifications to the text names based on whether
    its synced or not. a future CL will add non synced.
    
    Bug: b/340279545
    Change-Id: I641ca0b4a3d81cea05564114e6f61134c87fd4a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5533482
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300265}

--- chrome/browser/ui/tabs/tab_group_deletion_dialog_controller.cc
+++ chrome/browser/ui/tabs/tab_group_deletion_dialog_controller.cc
@@ -14,7 +14,9 @@
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/grit/generated_resources.h"
 #include "components/prefs/pref_service.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/dialog_model.h"
 
 namespace tab_groups {
@@ -27,35 +29,34 @@ namespace {
 // TODO(b/331254038) replace these hardcoded strings with IDS strings.
 
 // The text that shows on the checkbox.
-constexpr char16_t kDontAsk[] = u"Don't ask again";
+constexpr int kDontAskId = IDS_TAB_GROUP_DELETION_DIALOG_DONT_ASK;
 
 // For deletion, the text that shows on the dialog
-constexpr char16_t kDeleteTitle[] = u"Delete tab group?";
-constexpr char16_t kDeleteBody[] =
-    u"Deleting the group will remove it from this device and other devices "
-    u"using the same Google Account";
-constexpr char16_t kDeleteOkText[] = u"Delete";
+constexpr int kDeleteTitleId = IDS_TAB_GROUP_DELETION_DIALOG_TITLE_DELETE;
+constexpr int kDeleteBodyId = IDS_TAB_GROUP_DELETION_DIALOG_BODY_SYNCED_DELETE;
+constexpr int kDeleteOkTextId = IDS_TAB_GROUP_DELETION_DIALOG_OK_TEXT_DELETE;
 
 // For ungrouping, the text that shows on the dialog.
-constexpr char16_t kUngroupTitle[] = u"Are you sure you want to ungroup?";
-constexpr char16_t kUngroupBody[] =
-    u"Ungrouping will leave the tabs open on this device but delete the group "
-    u"on this device and other devices using the same Google Account";
-constexpr char16_t kUngroupOkText[] = u"Ungroup";
+constexpr int kUngroupTitleId = IDS_TAB_GROUP_DELETION_DIALOG_TITLE_UNGROUP;
+constexpr int kUngroupBodyId =
+    IDS_TAB_GROUP_DELETION_DIALOG_BODY_SYNCED_UNGROUP;
+constexpr int kUngroupOkTextId = IDS_TAB_GROUP_DELETION_DIALOG_OK_TEXT_UNGROUP;
 
 // For closing the last tab, the text that shows on the dialog.
-constexpr char16_t kCloseTabAndDeleteTitle[] = u"Close tab and delete group?";
-constexpr char16_t kCloseTabAndDeleteBody[] =
-    u"Closing the last tab will also delete the group from this device and "
-    u"other devices using the same Google Account";
-constexpr char16_t kCloseTabAndDeleteOkText[] = u"Close and delete group";
+constexpr int kCloseTabAndDeleteTitleId =
+    IDS_TAB_GROUP_DELETION_DIALOG_TITLE_CLOSE_TAB_AND_DELETE;
+constexpr int kCloseTabAndDeleteBodyId =
+    IDS_TAB_GROUP_DELETION_DIALOG_BODY_SYNCED_DELETE;
+constexpr int kCloseTabAndDeleteOkTextId =
+    IDS_TAB_GROUP_DELETION_DIALOG_OK_TEXT_DELETE;
 
 // For removing the last tab, the text that shows on the dialog.
-constexpr char16_t kRemoveTabAndDeleteTitle[] = u"Remove tab and delete group?";
-constexpr char16_t kRemoveTabAndDeleteBody[] =
-    u"Removing the last tab will also delete the group from this device and "
-    u"other devices using the same Google Account";
-constexpr char16_t kRemoveTabAndDeleteOkText[] = u"Remove and delete group";
+constexpr int kRemoveTabAndDeleteTitleId =
+    IDS_TAB_GROUP_DELETION_DIALOG_TITLE_REMOVE_TAB_AND_DELETE;
+constexpr int kRemoveTabAndDeleteBodyId =
+    IDS_TAB_GROUP_DELETION_DIALOG_BODY_SYNCED_DELETE;
+constexpr int kRemoveTabAndDeleteOkTextId =
+    IDS_TAB_GROUP_DELETION_DIALOG_OK_TEXT_DELETE;
 
 struct DialogText {
   const std::u16string title;
@@ -67,18 +68,24 @@ struct DialogText {
 DialogText GetDialogText(DeletionDialogController::DialogType type) {
   switch (type) {
     case DeletionDialogController::DialogType::DeleteSingle: {
-      return DialogText{kDeleteTitle, kDeleteBody, kDeleteOkText};
+      return DialogText{l10n_util::GetStringUTF16(kDeleteTitleId),
+                        l10n_util::GetStringUTF16(kDeleteBodyId),
+                        l10n_util::GetStringUTF16(kDeleteOkTextId)};
     }
     case DeletionDialogController::DialogType::UngroupSingle: {
-      return DialogText{kUngroupTitle, kUngroupBody, kUngroupOkText};
+      return DialogText{l10n_util::GetStringUTF16(kUngroupTitleId),
+                        l10n_util::GetStringUTF16(kUngroupBodyId),
+                        l10n_util::GetStringUTF16(kUngroupOkTextId)};
     }
     case DeletionDialogController::DialogType::RemoveTabAndDelete: {
-      return DialogText{kRemoveTabAndDeleteTitle, kRemoveTabAndDeleteBody,
-                        kRemoveTabAndDeleteOkText};
+      return DialogText{l10n_util::GetStringUTF16(kRemoveTabAndDeleteTitleId),
+                        l10n_util::GetStringUTF16(kRemoveTabAndDeleteBodyId),
+                        l10n_util::GetStringUTF16(kRemoveTabAndDeleteOkTextId)};
     }
     case DeletionDialogController::DialogType::CloseTabAndDelete: {
-      return DialogText{kCloseTabAndDeleteTitle, kCloseTabAndDeleteBody,
-                        kCloseTabAndDeleteOkText};
+      return DialogText{l10n_util::GetStringUTF16(kCloseTabAndDeleteTitleId),
+                        l10n_util::GetStringUTF16(kCloseTabAndDeleteBodyId),
+                        l10n_util::GetStringUTF16(kCloseTabAndDeleteOkTextId)};
     }
   }
 }
@@ -209,7 +216,7 @@ std::unique_ptr<ui::DialogModel> DeletionDialogController::BuildDialogModel(
       .SetTitle(strings.title)
       .AddParagraph(ui::DialogModelLabel(strings.body))
       .AddCheckbox(kDeletionDialogDontAskCheckboxId,
-                   ui::DialogModelLabel(kDontAsk))
+                   ui::DialogModelLabel(l10n_util::GetStringUTF16(kDontAskId)))
       .AddCancelButton(base::BindOnce(&DeletionDialogController::OnDialogCancel,
                                       base::Unretained(this)),
                        ui::DialogModel::Button::Params().SetEnabled(true).SetId(
