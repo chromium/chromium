ab39efb97a0107c5c6a57514bac6f6fa1a9d4e91
Benoît Lizé <lizeb@chromium.org>
2024-05-14 07:38:12 +0000

    [canvas] Disable canvas hibernation on all platforms
    Canvas hibernation and its various recent fixes exposes at least two
    issues that occur in stable:
    - PDF previews disappearing
    - Canvas corruption with ANGLE Metal on ARM64 macOS
    
    While these are being fixed, disable canvas hibernation on all platforms.
    This is expected to regress memory usage.
    
    (cherry picked from commit 80b674bbac6fd9b41c59fc4b2c20b7e22863a245)
    
    Bug: 328755781
    Change-Id: Ib7aceab4f305eef3e4ceec0a89c67f06b9d08feb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514288
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1296731}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536794
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Reviewed-by: Prudhvikumar Bommana <pbommana@google.com>
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/6422@{#991}
    Cr-Branched-From: 9012208d0ce02e0cf0adb9b62558627c356f3278-refs/heads/main@{#1287751}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -512,11 +512,13 @@ BASE_FEATURE(kCacheStorageCodeCacheHintHeader,
 const base::FeatureParam<std::string> kCacheStorageCodeCacheHintHeaderName{
     &kCacheStorageCodeCacheHintHeader, "name", "x-CacheStorageCodeCacheHint"};
 
-// Enabled 03/2024 on macOS (and much earlier elsewhere).
-// Keeping the feature to perform a holdback experiment.
+// Temporarily disabled due to issues:
+// - PDF blank previews
+// - Canvas corruption on ARM64 macOS
+// See https://g-issues.chromium.org/issues/328755781
 BASE_FEATURE(kCanvas2DHibernation,
              "Canvas2DHibernation",
-             base::FeatureState::FEATURE_ENABLED_BY_DEFAULT);
+             base::FeatureState::FEATURE_DISABLED_BY_DEFAULT);
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_FUCHSIA)
 // Enables camera preview in permission bubble and site settings.
