42fc562599d784a8a646703ce3b7c158ce1a8466
Jose Dapena Paz <jdapena@igalia.com>
2024-05-15 14:01:03 +0000

    IWYU: missing include for usage of std::optional in run_segmenter.h
    Bug: 41455655
    Change-Id: If28c13730926447b1d362ecfe08ae0bc44112876
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539811
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301267}

--- third_party/blink/renderer/platform/fonts/shaping/run_segmenter.h
+++ third_party/blink/renderer/platform/fonts/shaping/run_segmenter.h
@@ -6,7 +6,10 @@
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_SHAPING_RUN_SEGMENTER_H_
 
 #include <unicode/uscript.h>
+
 #include <memory>
+#include <optional>
+
 #include "third_party/blink/renderer/platform/fonts/font_orientation.h"
 #include "third_party/blink/renderer/platform/fonts/orientation_iterator.h"
 #include "third_party/blink/renderer/platform/fonts/script_run_iterator.h"
