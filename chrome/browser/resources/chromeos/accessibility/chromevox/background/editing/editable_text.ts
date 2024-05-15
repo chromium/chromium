bc97f6cd2bcb178b2cef20ae18e3e56853782918
Anastasia Helfinstein <anastasi@google.com>
2024-05-15 20:48:45 +0000

    [ChromeVox] Move describeTextChanged to EditableText
    As part of the effort to make the ChromeVox codebase more streamlined
    and easier to navigate, we are moving towards removing redundant layers
    of abstraction such as ChromeVoxEditableTextBase. This change moves one
    function from that class to its only child, AutomationEditableText.
    
    AX-Relnotes: n/a.
    Bug: b:265481751
    Change-Id: I153324584974ff41c79f83c564d8b576011071c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499561
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301557}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/editable_text.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/editing/editable_text.ts
@@ -2,13 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {TestImportManager} from '/common/testing/test_import_manager.js';
 import {LocalStorage} from '/common/local_storage.js';
+import {StringUtil} from '/common/string_util.js';
+import {TestImportManager} from '/common/testing/test_import_manager.js';
 
 import {NavBraille} from '../../common/braille/nav_braille.js';
 import {Msgs} from '../../common/msgs.js';
 import {Spannable} from '../../common/spannable.js';
-import {TtsSpeechProperties} from '../../common/tts_types.js';
+import {Personality, TtsSpeechProperties} from '../../common/tts_types.js';
 import {ValueSelectionSpan, ValueSpan} from '../braille/spans.js';
 import {ChromeVox} from '../chromevox.js';
 import {OutputNodeSpan} from '../output/output_types.js';
@@ -60,8 +61,9 @@ export class AutomationEditableText extends ChromeVoxEditableTextBase {
     if (evt.value === this.value) {
       AutomationEditableText.prototype.describeSelectionChanged.call(this, evt);
     } else {
-      this.describeTextChanged(
-          new TextChangeEvent(this.value, this.start, this.end, true), evt);
+      AutomationEditableText.prototype.describeTextChanged.call(
+          this, new TextChangeEvent(this.value, this.start, this.end, true),
+          evt);
     }
     this.lastChangeDescribed = true;
 
@@ -169,6 +171,159 @@ export class AutomationEditableText extends ChromeVoxEditableTextBase {
         this.speak(Msgs.getMsg('selected'));
       }
     }
+  }
+
+  /** Describe a change where the text changes. */
+  describeTextChanged(prev: TextChangeEvent, evt: TextChangeEvent): void {
+    let personality = new TtsSpeechProperties();
+    if (evt.value.length < (prev.value.length - 1)) {
+      personality = Personality.DELETED;
+    }
+    if (this.isPassword) {
+      this.speak(
+          Msgs.getMsg('password_char'), evt.triggeredByUser, personality);
+      return;
+    }
+
+    // First, see if there's a selection at the end that might have been
+    // added by autocomplete. If so, replace the event information with it.
+    const origEvt = evt;
+    let autocompleteSuffix = '';
+    if (evt.start < evt.end && evt.end === evt.value.length) {
+      autocompleteSuffix = evt.value.slice(evt.start);
+      evt = new TextChangeEvent(
+          evt.value.slice(0, evt.start), evt.start, evt.start,
+          evt.triggeredByUser);
+    }
+
+    // Precompute the length of prefix and suffix of values.
+    const commonPrefixLen =
+        StringUtil.longestCommonPrefixLength(evt.value, prev.value);
+    const commonSuffixLen =
+        StringUtil.longestCommonSuffixLength(evt.value, prev.value);
+
+    // Now see if the previous selection (if any) was deleted
+    // and any new text was inserted at that character position.
+    // This would handle pasting and entering text by typing, both from
+    // a cursor and from a selection.
+    let prefixLen = prev.start;
+    let suffixLen = prev.value.length - prev.end;
+    if (evt.value.length >= prefixLen + suffixLen + (evt.end - evt.start) &&
+        commonPrefixLen >= prefixLen && commonSuffixLen >= suffixLen) {
+      this.describeTextChangedHelper(
+          prev, origEvt, prefixLen, suffixLen, autocompleteSuffix, personality);
+      return;
+    }
+
+    // Next, see if one or more characters were deleted from the previous
+    // cursor position and the new cursor is in the expected place. This
+    // handles backspace, forward-delete, and similar shortcuts that delete
+    // a word or line.
+    prefixLen = evt.start;
+    suffixLen = evt.value.length - evt.end;
+    if (prev.start === prev.end && evt.start === evt.end &&
+        commonPrefixLen >= prefixLen && commonSuffixLen >= suffixLen) {
+      // Forward deletions causes reading of the character immediately to the
+      // right of the caret.
+      if (prev.start === evt.start && prev.end === evt.end) {
+        this.speak(evt.value[evt.start]!, evt.triggeredByUser);
+      } else {
+        this.describeTextChangedHelper(
+            prev, origEvt, prefixLen, suffixLen, autocompleteSuffix,
+            personality);
+      }
+      return;
+    }
+
+    // See if the change is related to IME's complex operation.
+    if (this.describeTextChangedByIME(
+            prev, evt, commonPrefixLen, commonSuffixLen)) {
+      return;
+    }
+
+    // If all above fails, we assume the change was not the result of a normal
+    // user editing operation, so we'll have to speak feedback based only
+    // on the changes to the text, not the cursor position / selection.
+    // First, restore the event.
+    evt = origEvt;
+
+    // Try to do a diff between the new and the old text. If it is a one
+    // character insertion/deletion at the start or at the end, just speak that
+    // character.
+    if ((evt.value.length === (prev.value.length + 1)) ||
+        ((evt.value.length + 1) === prev.value.length)) {
+      // The user added text either to the beginning or the end.
+      if (evt.value.length > prev.value.length) {
+        if (commonPrefixLen === prev.value.length) {
+          this.speak(
+              evt.value[evt.value.length - 1]!, evt.triggeredByUser,
+              personality);
+          return;
+        } else if (commonSuffixLen === prev.value.length) {
+          this.speak(evt.value[0]!, evt.triggeredByUser, personality);
+          return;
+        }
+      }
+      // The user deleted text either from the beginning or the end.
+      if (evt.value.length < prev.value.length) {
+        if (commonPrefixLen === evt.value.length) {
+          this.speak(
+              prev.value[prev.value.length - 1]!, evt.triggeredByUser,
+              personality);
+          return;
+        } else if (commonSuffixLen === evt.value.length) {
+          this.speak(prev.value[0]!, evt.triggeredByUser, personality);
+          return;
+        }
+      }
+    }
+
+    if (this.multiline) {
+      // The below is a somewhat loose way to deal with non-standard
+      // insertions/deletions. Intentionally skip for multiline since deletion
+      // announcements are covered above and insertions are non-standard
+      // (possibly due to auto complete). Since content editable's often refresh
+      // content by removing and inserting entire chunks of text, this type of
+      // logic often results in unintended consequences such as reading all text
+      // when only one character has been entered.
+      return;
+    }
+
+    // If the text is short, just speak the whole thing.
+    if (evt.value.length <= ChromeVoxEditableTextBase.maxShortPhraseLen) {
+      this.describeTextChangedHelper(prev, evt, 0, 0, '', personality);
+      return;
+    }
+
+    // Otherwise, look for the common prefix and suffix, but back up so
+    // that we can speak complete words, to be minimally confusing.
+    prefixLen = commonPrefixLen;
+    while (prefixLen < prev.value.length && prefixLen < evt.value.length &&
+           prev.value[prefixLen] === evt.value[prefixLen]) {
+      prefixLen++;
+    }
+    while (prefixLen > 0 &&
+           !StringUtil.isWordBreakChar(prev.value[prefixLen - 1]!)) {
+      prefixLen--;
+    }
+
+    // For suffix, commonSuffixLen is not used because suffix here won't overlap
+    // with prefix, and also we need to consider |autocompleteSuffix|.
+    suffixLen = 0;
+    while (suffixLen < (prev.value.length - prefixLen) &&
+           suffixLen < (evt.value.length - prefixLen) &&
+           prev.value[prev.value.length - suffixLen - 1] ===
+               evt.value[evt.value.length - suffixLen - 1]) {
+      suffixLen++;
+    }
+    while (suffixLen > 0 &&
+           !StringUtil.isWordBreakChar(
+               prev.value[prev.value.length - suffixLen]!)) {
+      suffixLen--;
+    }
+
+    this.describeTextChangedHelper(
+        prev, evt, prefixLen, suffixLen, '', personality);
   }
 
   /** Called when the text field has been updated. */
