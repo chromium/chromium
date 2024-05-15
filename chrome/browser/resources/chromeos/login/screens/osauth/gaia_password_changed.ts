26a815123aa3014fe2888cfa1261ce9a5b54ae34
Danila Kuzmin <dkuzmin@google.com>
2024-05-15 20:06:21 +0000

    [oobe] Determine orientation using media queries in screens
    Instead of explicitly setting the orientation from the browser side
    we can determine the orientation using a media query. That simplifies
    the code and reduces additional dependencies for other WebUIs that
    reuse OOBE components and styles.
    
    Bug: b:336507936
    Change-Id: Ie88c652a3c3d3677cec009783dbb526b955783d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473625
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301521}

--- chrome/browser/resources/chromeos/login/screens/osauth/gaia_password_changed.ts
+++ chrome/browser/resources/chromeos/login/screens/osauth/gaia_password_changed.ts
@@ -8,7 +8,6 @@
 
 import '//resources/ash/common/cr_elements/cros_color_overrides.css.js';
 import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
-import '//resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import '../../components/oobe_icons.html.js';
 import '../../components/buttons/oobe_next_button.js';
 import '../../components/common_styles/oobe_common_styles.css.js';
--- chrome/browser/resources/chromeos/login/screens/osauth/local_data_loss_warning.ts
+++ chrome/browser/resources/chromeos/login/screens/osauth/local_data_loss_warning.ts
@@ -5,7 +5,6 @@
 import '//resources/ash/common/cr_elements/cros_color_overrides.css.js';
 import '//resources/ash/common/cr_elements/cr_input/cr_input.js';
 import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
-import '//resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import '../../components/oobe_icons.html.js';
 import '../../components/buttons/oobe_next_button.js';
 import '../../components/common_styles/oobe_common_styles.css.js';
