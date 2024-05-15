6781dade1d3bacae9cee1bdbe8a3ccbd4a55d1c7
Daniel Cheng <dcheng@chromium.org>
2024-05-15 04:58:34 +0000

    Use absl::Cleanup to run done callback in ClickToCallMessageHandler
    absl::Cleanup is a helper to run arbitrary code when the absl::Cleanup
    object goes out of scope, which is useful for executing cleanup code or
    ensuring something always runs. The current //base equivalent is
    base::ScopedClosureRunner, which executes a base::OnceClosure when the
    runner object goes out of scope.
    
    Compared to base::ScopedClosureRunner, there are several benefits to
    using absl::Cleanup:
    - works with capturing lambdas, which are often much more concise than
      base::BindOnce()
    - requires no heap allocations
    - less impact on binary size since absl::Cleanup instantiates fewer
      templates
    
    This CL is part of a project-wide cleanup to migrate to absl::Cleanup
    where appropriate. The general criteria for migrating usages of
    base::ScopedClosureRunner:
    - The cleanup scoper must not escape block scope, e.g. it is not
      returned from the function, passed to another function, or bound into
      a callback.
    - The cleanup scoper's type does not need to be named, e.g. the scoper
      construction can use CTAD:
        absl::Cleanup run_at_exit = [] { RestoreSettings(original); };
      Note: having to write absl::Cleanup<decltype(lambda)> as a type is
      often a sign that absl::Cleanup is not a good fit for how the code is
      currently structured.
    - The cleanup scoper is not simply running a base::OnceClosure.
    
    Bug: 339492604
    Change-Id: Ia8f32e079c6b8fc94e0fe1fd8f504bfdc7954522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536277
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301103}

--- chrome/browser/sharing/click_to_call/click_to_call_message_handler_android.cc
+++ chrome/browser/sharing/click_to_call/click_to_call_message_handler_android.cc
@@ -6,7 +6,6 @@
 
 #include "base/android/jni_string.h"
 #include "base/check.h"
-#include "base/functional/callback_helpers.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/strcat.h"
 #include "base/trace_event/trace_event.h"
@@ -14,6 +13,7 @@
 #include "chrome/browser/sharing/click_to_call/click_to_call_utils.h"
 #include "chrome/browser/sharing/proto/click_to_call_message.pb.h"
 #include "chrome/browser/sharing/proto/sharing_message.pb.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 #include "url/gurl.h"
 
 ClickToCallMessageHandler::ClickToCallMessageHandler() = default;
@@ -25,8 +25,9 @@ void ClickToCallMessageHandler::OnMessage(
     SharingMessageHandler::DoneCallback done_callback) {
   DCHECK(message.has_click_to_call_message());
   TRACE_EVENT0("sharing", "ClickToCallMessageHandler::OnMessage");
-  base::ScopedClosureRunner runner(
-      base::BindOnce(std::move(done_callback), /*response=*/nullptr));
+  absl::Cleanup response_runner = [&done_callback] {
+    std::move(done_callback).Run(/*response=*/nullptr);
+  };
 
   std::string phone_number = message.click_to_call_message().phone_number();
   GURL phone_url(base::StrCat({"tel:", phone_number}));
