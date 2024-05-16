53d51092d906ce70ff83baf4ffc75810b7f94747
Nate Fischer <ntfschr@chromium.org>
2024-05-16 18:33:17 +0000

    AW: do not inline the WebViewEnableCrash code
    This adds an extra function call for the WebViewEnableCrash feature,
    using the NOINLINE and NOOPT annotations to force this function to
    remain in the final build. Without this change, CrashInNative() is
    renamed to some un-symbolizable name.
    
    ThisFunctionWillCrash() will still have its symbols stripped in the
    final build, however the stack frame will remain in the crash output so
    we can rely on this function name to appear once the crash is
    symbolized.
    
    Fixed: 340963660
    Test: compile an official build, enable the flag, and symbolize stack
    Test: out/Official/bin/trichrome_webview_google_apk stack crash.txt
    Change-Id: I7cc5f17795eded8374e1156d14606baa227104c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5543129
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302108}

--- android_webview/browser/aw_crashy_class_utils.cc
+++ android_webview/browser/aw_crashy_class_utils.cc
@@ -5,12 +5,21 @@
 #include <ostream>
 
 #include "android_webview/browser_jni_headers/AwCrashyClassUtils_jni.h"
-#include "base/check.h"
+#include "base/compiler_specific.h"
+#include "base/logging.h"
 
 namespace android_webview {
 
+// Do not inline this function. This is meant to simulate a crash like we would
+// see in a real incident. Real crashes will generally have at least 1
+// non-inlined stack frame, so we force this to happen in this case using both
+// the NOINLINE and NOOPT annotations (for some reason, NOINLINE is not enough).
+NOINLINE NOOPT void ThisFunctionWillCrash() {
+  LOG(FATAL) << "WebView Forced Native Crash for WebView Browser Process";
+}
+
 static void JNI_AwCrashyClassUtils_CrashInNative(JNIEnv* env) {
-  CHECK(false) << "WebView Forced Native Crash for WebView Browser Process";
+  ThisFunctionWillCrash();
 }
 
 }  // namespace android_webview
