4910e17c633968e9b5b9ef192ae16cee2479041b
danakj <danakj@chromium.org>
2024-05-17 19:42:12 +0000

    Wrap span(ptr, size) with UNSAFE_BUFFERS in FieldTrial::FieldTrialEntry
    The construction here is not obviously sound, leave a TODO that we
    should push the span construction closer to where we can guarantee
    correctness (or construct from a container directly).
    
    R=lukasza@chromium.org
    
    Bug: 341324165
    Change-Id: Ia601e799780f9faa3ebb93715392eb6df1523939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546788
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302803}

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -226,7 +226,10 @@ bool FieldTrial::FieldTrialEntry::GetParams(
 
 PickleIterator FieldTrial::FieldTrialEntry::GetPickleIterator() const {
   Pickle pickle = Pickle::WithUnownedBuffer(
-      span(GetPickledDataPtr(), checked_cast<size_t>(pickle_size)));
+      // TODO(crbug.com/40284755): FieldTrialEntry should be constructed with a
+      // span over the pickle memory.
+      UNSAFE_BUFFERS(
+          span(GetPickledDataPtr(), checked_cast<size_t>(pickle_size))));
   return PickleIterator(pickle);
 }
 
