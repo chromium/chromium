75b41cdffc57c7e623cd629c1a7276854930743a
Sunggook Chue <sunggch@microsoft.com>
2024-05-17 20:41:34 +0000

    Add keyframe check for the first video frame in WebM muxer.
    MP4Muxer already check missing keyframe for VP9 encoding, which
    might be misleading as the issue is MP4Muxer.
    However, the issue repro on the WebM as well so I add the same
    validation and bug on the WebM muxer side.
    
    Bug:339841680
    
    Change-Id: Ia7539352a9348eea6d044fe5b1bc6c6ede2febcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529348
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1302832}

--- media/muxers/mp4_muxer_delegate.cc
+++ media/muxers/mp4_muxer_delegate.cc
@@ -126,7 +126,7 @@ void Mp4MuxerDelegate::AddVideoFrame(
   DVLOG(1) << __func__ << ", " << params.AsHumanReadableString();
 
   if (!video_track_index_.has_value()) {
-    // TODO(crbug.com/333614631): Potential issue for the first frame.
+    // TODO(crbug.com/339841680): Potential issue for the first frame.
     if (!is_key_frame) {
       CHECK_EQ(params.codec, VideoCodec::kVP9);
       DVLOG(1) << __func__ << ", not a key frame";
