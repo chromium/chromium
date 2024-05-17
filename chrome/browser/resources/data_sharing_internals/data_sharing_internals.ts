932728fd1701bdcabf754db406ad5605b77d355d
Min Qin <qinmin@chromium.org>
2024-05-17 01:12:33 +0000

    Display group information on data sharing internal page
    Bug: 328265318
    Change-Id: Icc8623d8f846fc3f5b64ea10debac1645e180ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5532275
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302355}

--- chrome/browser/resources/data_sharing_internals/data_sharing_internals.ts
+++ chrome/browser/resources/data_sharing_internals/data_sharing_internals.ts
@@ -1,3 +1,41 @@
 // Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+import {getRequiredElement} from 'chrome://resources/js/util.js';
+
+import type {GroupData} from './data_sharing_internals.mojom-webui.js';
+import {DataSharingInternalsBrowserProxy} from './data_sharing_internals_browser_proxy.js';
+
+function getProxy(): DataSharingInternalsBrowserProxy {
+  return DataSharingInternalsBrowserProxy.getInstance();
+}
+
+function appendTextChildToList(textToShow: string, element: HTMLUListElement) {
+  const textElement = document.createElement('pre');
+  textElement.textContent = textToShow;
+  element.appendChild(textElement);
+}
+
+/**
+ * Show all groups information.
+ */
+function displayGroups(isSuccess: boolean, groupData: GroupData[]) {
+  getRequiredElement('get-all-groups-status').textContent =
+      isSuccess ? 'success' : 'failed';
+  const groupList = getRequiredElement('group-list');
+  groupData.forEach((group) => {
+    const listItem = document.createElement('ul');
+    appendTextChildToList(group.groupId, listItem);
+    appendTextChildToList(group.name, listItem);
+    groupList.appendChild(listItem);
+    // TODO (qinmin): display member information for the group.
+  });
+}
+
+function initialize() {
+  getProxy().getAllGroups().then(
+      response => displayGroups(response.isSuccess, response.data));
+}
+
+document.addEventListener('DOMContentLoaded', initialize);
