8054a28f22400a90c0c2045d919ab1f7383b937a
Jack Shira <jackshira@google.com>
2024-05-20 23:59:01 +0000

    [Nearby Share] Add WifiDirectServerSocket
    Adds the scaffolding for WifiDirectServerSocket.
    
    Bug: b/341389768
    Tests: N/A
    Change-Id: I73ce8682a6691f5b77e2e5b42dd2422a6ed30caf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548928
    Reviewed-by: Brando Socarras <brandosocarras@google.com>
    Commit-Queue: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1303522}

---
+++ chrome/services/sharing/nearby/platform/wifi_direct_server_socket.cc
@@ -0,0 +1,32 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/sharing/nearby/platform/wifi_direct_server_socket.h"
+
+namespace nearby::chrome {
+
+WifiDirectServerSocket::WifiDirectServerSocket() = default;
+WifiDirectServerSocket::~WifiDirectServerSocket() = default;
+
+// api::WifiDirectServerSocket
+std::string WifiDirectServerSocket::GetIPAddress() const {
+  NOTIMPLEMENTED();
+  return std::string();
+}
+
+int WifiDirectServerSocket::GetPort() const {
+  NOTIMPLEMENTED();
+  return -1;
+}
+
+std::unique_ptr<api::WifiDirectSocket> WifiDirectServerSocket::Accept() {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+Exception WifiDirectServerSocket::Close() {
+  NOTIMPLEMENTED();
+  return {Exception::kSuccess};
+}
+}  // namespace nearby::chrome
