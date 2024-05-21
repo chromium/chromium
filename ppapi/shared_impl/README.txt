f57b6dacd5e5f9673a83c5e32a4c259ad2e7c934
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-21 17:37:36 +0200

    Delete ppapi directory

--- ppapi/shared_impl/README.txt
+++
@@ -1,2 +0,0 @@
-This directory contains implementation code for PPAPI that needs to be shared
-between the backend implementation in the renderer and in the proxy.
--- ppapi/tests/extensions/packaged_app/main.js
+++
@@ -1,7 +0,0 @@
-// Copyright 2014 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {width: 500, height: 500});
-});
