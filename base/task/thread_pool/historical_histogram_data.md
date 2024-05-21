f904fb55d0ed8364c9d84411e4f4977f1d9cca4c
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-21 17:29:47 +0200

    Delete base directory

--- base/task/thread_pool/historical_histogram_data.md
+++
@@ -1,4 +0,0 @@
-# Historical Histogram Data
-
-Data from deprecated histograms is available in this
-[doc](https://docs.google.com/document/d/1YYEBlpkg3aulPZwIb_CrXZ3lScIFbJ2HPalC-WB0l8s/edit?usp=sharing).
--- base/test/android/java/src/org/chromium/base/MultiprocessTestClientService2.java
+++
@@ -1,8 +0,0 @@
-// Copyright 2016 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-package org.chromium.base;
-
-/** A subclass used only to differentiate different test client service process instances. */
-public class MultiprocessTestClientService2 extends MultiprocessTestClientService {}
