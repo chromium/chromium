d6537d035e66007d4d82a4ba872819f3078da44d
danakj <danakj@chromium.org>
2024-05-21 21:32:57 +0000

    Mark span(ptr, size) ctor as UNSAFE_BUFFERS in the base64 fuzzer
    And use base::as_string_view() which didn't exist when the code was
    converted to span.
    
    R=lukasza@chromium.org
    
    Bug: 341324165, 40284755
    Change-Id: I9d001b152b0664856cdf1ecd131c78eaa056089d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553941
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304057}

--- base/base64_encode_fuzzer.cc
+++ base/base64_encode_fuzzer.cc
@@ -9,18 +9,19 @@
 #include "base/check_op.h"
 
 // Encode some random data, and then decode it.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  base::span<const uint8_t> data_span(data, size);
-  std::string_view data_piece(reinterpret_cast<const char*>(data), size);
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data_ptr, size_t size) {
+  // SAFETY: libfuzzer provides a valid pointer and size pair.
+  auto data = UNSAFE_BUFFERS(base::span(data_ptr, size));
+  std::string_view data_string = base::as_string_view(data);
 
-  const std::string encode_output = base::Base64Encode(data_span);
+  const std::string encode_output = base::Base64Encode(data);
   std::string decode_output;
   CHECK(base::Base64Decode(encode_output, &decode_output));
-  CHECK_EQ(data_piece, decode_output);
+  CHECK_EQ(data_string, decode_output);
 
   // Also run the std::string_view variant and check that it gives the same
   // results.
-  CHECK_EQ(encode_output, base::Base64Encode(data_piece));
+  CHECK_EQ(encode_output, base::Base64Encode(data_string));
 
   return 0;
 }
