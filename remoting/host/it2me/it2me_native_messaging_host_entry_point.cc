1a394fc3ee0c735a3e6f329c47dae0573c5d26f3
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-21 17:37:59 +0200

    Delete remoting directory

--- remoting/host/it2me/it2me_native_messaging_host_entry_point.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2014 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "remoting/host/it2me/it2me_native_messaging_host_main.h"
-
-int main(int argc, char** argv) {
-  return remoting::It2MeNativeMessagingHostMain(argc, argv);
-}
--- remoting/host/setup/me2me_native_messaging_host_entry_point.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2014 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "remoting/host/setup/me2me_native_messaging_host_main.h"
-
-int main(int argc, char** argv) {
-  return remoting::Me2MeNativeMessagingHostMain(argc, argv);
-}
