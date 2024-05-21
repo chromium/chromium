80aa60888363bdcbf3bd9fa06485fdcad6db285c
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-21 17:30:02 +0200

    Delete build directory

--- build/3pp_common/README.md
+++
@@ -1,5 +0,0 @@
-# 3pp_util
-
-Contains helper scripts for 3pp configs.
-
-See [`//docs/docs/cipd_and_3pp.md`](/docs/cipd_and_3pp.md)
--- build/install-build-deps.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# Copyright 2012 The Chromium Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-exec "$(cd $(dirname $0) && pwd)/install-build-deps.py" "$@"
