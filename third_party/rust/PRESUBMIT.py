445eb1ea874e2f61d4aca772a62ad8b7ddbf9869
Lukasz Anforowicz <lukasza@chromium.org>
2024-05-21 16:40:25 +0000

    gnrt: Require `crypto-safe` instead of `does-not-implement-crypto`.
    Note that the new requirement can be met by *either* a `crypto-safe`
    certification *or* a `does-not-implement-crypto` certification.  If a
    crate does not implement crypto, use `does-not-implement-crypto`, which
    implies `crypto-safe`, but does not require expert review in order to
    audit for.
    
    Requiring `crypto-safe` instead of `does-not-implement-crypto` is
    desirable because:
    
    * It allows `rand_chacha` to pass the `cargo vet` policy after
      it got certified as `crypto-safe` in https://crrev.com/c/5548657.
    * `crypto-safe` is a stricter requirement than `does-not-implement-crypto`
    * `does-not-implement-crypto` implies `crypto-safe` as declared in
      `third_party/rust/chromium_crates_io/supply-chain/audits.toml`:
    
        ```
        [criteria.does-not-implement-crypto]
        description = ...
        implies = "crypto-safe"
        ```
    
    This CL also tweaks `third_party/rust/PRESUBMIT.py` so that
    `CheckVetConfigTomlAndItsTemplateAreEditedTogether` passes even though
    this CL doesn't edit `vet_config.toml.hbs`.  (This is okay, because this
    CL didn't manually modify `cargo.toml`, but instead this CL edited
    `gnrt`'s sources and regenerated `config.toml` by running
    `tools/crates/run_gnrt.py vendor`.)
    
    Bug: 41494083
    Change-Id: I64f0a75d6f9a36334efc5f3cc366470cf26c6b7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5550757
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303835}

--- third_party/rust/PRESUBMIT.py
+++ third_party/rust/PRESUBMIT.py
@@ -35,12 +35,24 @@ def CheckVetConfigTomlAndItsTemplateAreEditedTogether(input_api, output_api):
     toml_path = input_api.os_path.join(
         chromium_crates_io, 'supply-chain', 'config.toml')
     hbs_path = input_api.os_path.join(chromium_crates_io, 'vet_config.toml.hbs')
+    gnrt_path = input_api.os_path.join('tools', 'crates', 'gnrt')
 
-    affected_files = input_api.LocalPaths()
+    # We don't use `input_api.AffectedFiles()` nor `input_api.LocalPaths()`,
+    # because (unlike `input_api.change.AffectedFiles()`) they only cover files
+    # underneath `//third_party/rust` (and our checks also care about
+    # `//tools/crates/gnrt` aka `gnrt_path`).
+    affected_files = set([f.LocalPath() for f in input_api.change.AffectedFiles()])
+
     is_toml_edited = toml_path in affected_files
     is_hbs_edited = hbs_path in affected_files
-
     if is_toml_edited != is_hbs_edited:
+        # Allow editing `config.toml` without editing `vet_config.toml.hbs` when
+        # `gnrt` sources change - assume in this case that the `gnrt` changes
+        # affect how `config.toml` is generated.
+        is_gnrt_edited = any([gnrt_path in f for f in affected_files])
+        if is_toml_edited and is_gnrt_edited:
+            return []
+
         return [output_api.PresubmitError(
             f"ERROR: `{hbs_path}` and `{toml_path}` are not modified together",
             long_text=\
