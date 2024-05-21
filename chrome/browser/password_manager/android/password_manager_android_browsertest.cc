407645a1a02e0f92137d47cb0705f7f630e59812
Anna Tsvirchkova <atsvirchkova@google.com>
2024-05-21 14:46:33 +0000

    [Gardener] Disable flaky PasswordManagerAndroidBrowserTest.TriggerFormSubmission
    Bug: 40275666
    Change-Id: Ibcc8597a146184e1329c54e3d76e9eabaf4a43f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553954
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Auto-Submit: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1303740}

--- chrome/browser/password_manager/android/password_manager_android_browsertest.cc
+++ chrome/browser/password_manager/android/password_manager_android_browsertest.cc
@@ -73,8 +73,9 @@ class PasswordManagerAndroidBrowserTest
   net::EmbeddedTestServer https_server_;
 };
 
+// Disabled due to flakiness, see crbug.com/40275666.
 IN_PROC_BROWSER_TEST_P(PasswordManagerAndroidBrowserTest,
-                       TriggerFormSubmission) {
+                       DISABLED_TriggerFormSubmission) {
   base::HistogramTester uma_recorder;
   bool has_form_tag = GetParam();
   NavigateToFile(has_form_tag ? "/password/simple_password.html"
