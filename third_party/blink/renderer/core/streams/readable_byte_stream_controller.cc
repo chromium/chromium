cd405492789ec4bc6ecd598754154c527ff60e95
Nidhi Jaju <nidhijaju@chromium.org>
2024-05-21 07:07:52 +0000

    Streams: Check if buffer is detached when filling pull-into descriptor
    The pull-into descriptor can become out-of-sync with the array buffer
    when the buffer is detached. This CL adds a check to see if the buffer
    is detached before trying to fill it.
    
    Bug: 339877167
    Change-Id: I17d98416a746a5ade24308ef332cba829933d7de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553232
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303628}

--- third_party/blink/renderer/core/streams/readable_byte_stream_controller.cc
+++ third_party/blink/renderer/core/streams/readable_byte_stream_controller.cc
@@ -494,7 +494,8 @@ void ReadableByteStreamController::ProcessPullIntoDescriptorsUsingQueue(
         controller->pending_pull_intos_[0];
     //   c. If ! ReadableByteStreamControllerFillPullIntoDescriptorFromQueue(
     //   controller, pullIntoDescriptor) is true,
-    if (FillPullIntoDescriptorFromQueue(controller, pull_into_descriptor)) {
+    if (FillPullIntoDescriptorFromQueue(controller, pull_into_descriptor,
+                                        exception_state)) {
       //     i. Perform !
       //     ReadableByteStreamControllerShiftPendingPullInto(controller).
       ShiftPendingPullInto(controller);
@@ -504,6 +505,15 @@ void ReadableByteStreamController::ProcessPullIntoDescriptorsUsingQueue(
                                controller->controlled_readable_stream_,
                                pull_into_descriptor, exception_state);
       DCHECK(!exception_state.HadException());
+    }
+    if (exception_state.HadException()) {
+      // Instead of returning a rejection, which is inconvenient here,
+      // call ControllerError(). The only difference this makes is that it
+      // happens synchronously, but that should not be observable.
+      ReadableByteStreamController::Error(script_state, controller,
+                                          exception_state.GetException());
+      exception_state.ClearException();
+      return;
     }
   }
 }
@@ -989,7 +999,12 @@ void ReadableByteStreamController::FillHeadPullIntoDescriptor(
 
 bool ReadableByteStreamController::FillPullIntoDescriptorFromQueue(
     ReadableByteStreamController* controller,
-    PullIntoDescriptor* pull_into_descriptor) {
+    PullIntoDescriptor* pull_into_descriptor,
+    ExceptionState& exception_state) {
+  if (pull_into_descriptor->buffer->IsDetached()) {
+    exception_state.ThrowTypeError("buffer is detached");
+    return false;
+  }
   // https://streams.spec.whatwg.org/#readable-byte-stream-controller-fill-pull-into-descriptor-from-queue
   // 1. Let elementSize be pullIntoDescriptor.[[elementSize]].
   const size_t element_size = pull_into_descriptor->element_size;
@@ -1240,7 +1255,8 @@ void ReadableByteStreamController::PullInto(
     //   a. If !
     //   ReadableByteStreamControllerFillPullIntoDescriptorFromQueue(controller,
     //   pullIntoDescriptor) is true,
-    if (FillPullIntoDescriptorFromQueue(controller, pull_into_descriptor)) {
+    if (FillPullIntoDescriptorFromQueue(controller, pull_into_descriptor,
+                                        exception_state)) {
       //     i. Let filledView be !
       //     ReadableByteStreamControllerConvertPullIntoDescriptor(pullIntoDescriptor).
       DOMArrayBufferView* filled_view = ConvertPullIntoDescriptor(
@@ -1252,6 +1268,15 @@ void ReadableByteStreamController::PullInto(
       //     iii. Perform readIntoRequest’s chunk steps, given filledView.
       read_into_request->ChunkSteps(script_state, filled_view, exception_state);
       //     iv. Return.
+      return;
+    }
+    if (exception_state.HadException()) {
+      // Instead of returning a rejection, which is inconvenient here,
+      // call ControllerError(). The only difference this makes is that it
+      // happens synchronously, but that should not be observable.
+      ReadableByteStreamController::Error(script_state, controller,
+                                          exception_state.GetException());
+      exception_state.ClearException();
       return;
     }
     //   b. If controller.[[closeRequested]] is true,
