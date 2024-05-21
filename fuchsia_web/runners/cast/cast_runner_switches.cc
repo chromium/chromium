bf15d5435f1278fa6dbcd9643fa097e916b5ad86
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2024-05-21 17:34:26 +0200

    Delete fuchsia_web directory

--- fuchsia_web/runners/cast/cast_runner_switches.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "fuchsia_web/runners/cast/cast_runner_switches.h"
-
-const char kDisableVulkanForTestsSwitch[] = "disable-vulkan-for-tests";
-
-const char kForceHeadlessForTestsSwitch[] = "force-headless-for-tests";
