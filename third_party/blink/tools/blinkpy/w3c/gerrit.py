2cd81d1076901245bf55581073eca3e08f0020ff
Weizhong Xia <weizhong@google.com>
2024-05-21 02:15:31 +0000

    Revert "Export chromium CLs only after all owners have LGTM-ed"
    This reverts commit 95d4c92abc7c8d963c91a393edf19efc4e7ea5b4.
    
    Reason for revert: <The exporter is failing with error message: Bad Request>
    
    Original change's description:
    > Export chromium CLs only after all owners have LGTM-ed
    >
    > This will likely reduce number of tests runs at WPT upstream and save
    > the resources needed at upstream. It will also reduce the number of
    > orphaned PRs at upstream when Chromium CL is only intended for
    > experimenting.
    >
    > The path for Chromium commits will work as usual.
    >
    > Bug: 338231967
    > Change-Id: I68928a407a76f9763b355f2f18dd0cc1c4db8a36
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5551171
    > Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    > Commit-Queue: Weizhong Xia <weizhong@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1303365}
    
    Bug: 338231967
    Change-Id: Id3b69f17ef15a7a2685ff0eb9ecae4062cb76522
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5550596
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1303563}

--- third_party/blink/tools/blinkpy/w3c/gerrit.py
+++ third_party/blink/tools/blinkpy/w3c/gerrit.py
@@ -168,7 +168,7 @@ class GerritAPI:
         query = ' '.join([
             f'project:"{self.project_config.gerrit_project}"',
             f'branch:{self.project_config.gerrit_branch}',
-            'is:submittable',
+            'is:open',
             '-is:wip',
         ])
         open_cls = self.query_cls(query, limit, output_options)
