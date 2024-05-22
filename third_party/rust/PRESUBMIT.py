0f20598431fde2acb20db06d2c29e2c8504c0073
Dustin J. Mitchell <djmitche@chromium.org>
2024-05-22 16:09:32 +0000

    Temporarily disable a presubmit with false positives
    As described in https://crbug.com/342093065, this presubmit is
    incorrectly flagging auto-generated CLs.
    
    Bug: 342093065
    Change-Id: Ibe81eab008f588245166f18dd88ca83ee441404d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5558923
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304473}

--- third_party/rust/PRESUBMIT.py
+++ third_party/rust/PRESUBMIT.py
@@ -28,37 +28,3 @@ def CheckCargoVet(input_api, output_api):
     if input_api.verbose:
         print('Running ' + cmd_name)
     return input_api.RunTests([test_cmd])
-
-def CheckVetConfigTomlAndItsTemplateAreEditedTogether(input_api, output_api):
-    chromium_crates_io = input_api.os_path.join(
-        'third_party', 'rust', 'chromium_crates_io')
-    toml_path = input_api.os_path.join(
-        chromium_crates_io, 'supply-chain', 'config.toml')
-    hbs_path = input_api.os_path.join(chromium_crates_io, 'vet_config.toml.hbs')
-    gnrt_path = input_api.os_path.join('tools', 'crates', 'gnrt')
-
-    # We don't use `input_api.AffectedFiles()` nor `input_api.LocalPaths()`,
-    # because (unlike `input_api.change.AffectedFiles()`) they only cover files
-    # underneath `//third_party/rust` (and our checks also care about
-    # `//tools/crates/gnrt` aka `gnrt_path`).
-    affected_files = set([f.LocalPath() for f in input_api.change.AffectedFiles()])
-
-    is_toml_edited = toml_path in affected_files
-    is_hbs_edited = hbs_path in affected_files
-    if is_toml_edited != is_hbs_edited:
-        # Allow editing `config.toml` without editing `vet_config.toml.hbs` when
-        # `gnrt` sources change - assume in this case that the `gnrt` changes
-        # affect how `config.toml` is generated.
-        is_gnrt_edited = any([gnrt_path in f for f in affected_files])
-        if is_toml_edited and is_gnrt_edited:
-            return []
-
-        return [output_api.PresubmitError(
-            f"ERROR: `{hbs_path}` and `{toml_path}` are not modified together",
-            long_text=\
-                f"Please edit `{hbs_path}` " + \
-                f"and then regenerate `{toml_path}` " + \
-                f"by running `tools/crates/run_gnrt.py vendor`" \
-        )]
-
-    return []
