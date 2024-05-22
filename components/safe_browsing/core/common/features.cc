de7c2915d98c185feb817029d2aef531671ced4e
Daniel Rubery <drubery@chromium.org>
2024-05-22 15:32:52 +0000

    Enable SafeBrowsingEncryptedArchivesMetadata by default
    We have launch approval so launch it!
    
    Bug: 40074456
    Change-Id: I943fa0c0442a42ad09202f5d700125a099268ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5556538
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304452}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -102,7 +102,7 @@ const base::FeatureParam<int> kDownloadWarningSurveyIgnoreDelaySeconds{
 
 BASE_FEATURE(kEncryptedArchivesMetadata,
              "SafeBrowsingEncryptedArchivesMetadata",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtendedReportingRemovePrefDependency,
              "ExtendedReportingRemovePrefDependency",
