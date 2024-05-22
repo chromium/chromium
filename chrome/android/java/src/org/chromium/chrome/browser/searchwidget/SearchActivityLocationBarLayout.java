2b230a2cf7b261113c9dd8de97652699c52e24f0
Tomasz Wiszkowski <ender@google.com>
2024-05-22 21:33:41 +0000

    Always request Omnibox focus on SearchActivity.
    When Omnibox is inflated, it requests itself to be initially disabled
    to prevent Chrome from starting up with focused Omnibox. This behavior
    impacts SearchActivity as well, where we don't explicitly request
    Omnibox focus if the Voice input was successful.
    
    This change alters the behavior to ensure the Omnibox focus is
    always requested, regardless of intended search type, or outcome of
    an interaction with other 1p apps.
    
    Change-Id: I6caa64f96e47ea2c625aef12be57c43147fac3ff
    Fixed: 342067835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5559128
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1304699}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java
@@ -148,21 +148,14 @@ public class SearchActivityLocationBarLayout extends LocationBarLayout {
             runVoiceSearch();
         } else if (searchType == SearchType.LENS) {
             runGoogleLens();
-        } else {
-            focusTextBox();
         }
+        focusTextBox();
         mInteractionFromWidget = false;
     }
 
     /** Begins a new Voice query. */
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     void runVoiceSearch() {
-        // Run Voice before focusing the Omnibox. Voice search may trigger omnibox focus as part of
-        // its own flow in the event where the input is ambiguous. Focusing the Omnibox early may
-        // affect this flow.
-        //
-        // Note that the Voice search will call us back in the event of any failure via
-        // notifyVoiceRecognitionCanceled() call, giving us the opportunity to focus the Omnibox.
         View micButton = findViewById(R.id.mic_button);
         if (!micButton.performClick()) {
             // Voice recognition is not available. Fall back to regular text search.
@@ -171,20 +164,11 @@ public class SearchActivityLocationBarLayout extends LocationBarLayout {
                             R.string.quick_action_search_widget_message_no_voice_search,
                             Toast.LENGTH_LONG)
                     .show();
-            focusTextBox();
         }
     }
 
     /** Begins a new Lens query. */
     private void runGoogleLens() {
-        // Preemptively focus the Search box to handle fallback to text search for every case where
-        // Lens search could not be performed, including events where Lens is started and canceled
-        // by the User.
-        // Unlike Voice, Lens gives us no feedback about completion and does not interact with the
-        // Omnibox at any point. Focus is relevant here, because otherwise canceled Lens intent
-        // lands the User on a white, unfocused activity with no keyboard and single, empty text
-        // field on top.
-        focusTextBox();
         View lensButton = findViewById(R.id.lens_camera_button);
         if (!lensButton.performClick()) {
             Toast.makeText(
