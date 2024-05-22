6189a2dd474b4bd6a88bc12a3deadf3feaeef96e
Peter Kotwicz <pkotwicz@chromium.org>
2024-05-22 22:24:58 +0000

    Move DigitalIdentityProvider to //chrome part 3/4
    This CL deletes the IdentityCredentialsDelegate alias now that the
    internal Chrome build no longer uses it.
    
    BUG=332562244
    
    Change-Id: Id69f157c8d3517f6c945806f9f6c86a60d900cff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5559943
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304723}

--- content/public/android/java/src/org/chromium/content/browser/webid/IdentityCredentialsDelegate.java
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-package org.chromium.content.browser.webid;
-
-/** TODO(crbug.com/332562244): Delete once all implementers are ported to content_public version. */
-public interface IdentityCredentialsDelegate
-        extends org.chromium.content_public.browser.webid.IdentityCredentialsDelegate {}
