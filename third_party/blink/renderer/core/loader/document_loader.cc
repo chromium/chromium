df44ea98d38258a8b30a565c47656a10a8a892af
W. James MacLean <wjmaclean@chromium.org>
2024-05-22 14:29:24 +0000

    Remove srcdoc else-if block in CalculateOrigin()
    This is a cleanup CL to remove an else-if block in
    DocumentLoader::CalculateOrigin that is now redundant with the adoption
    of new rules around about:srcdoc navigations.
    
    Bug: 328279696
    Change-Id: I624967dea4935ebf212ffb55f4bd38ce391df64e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538771
    Commit-Queue: W. James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304421}

--- third_party/blink/renderer/core/loader/document_loader.cc
+++ third_party/blink/renderer/core/loader/document_loader.cc
@@ -2278,28 +2278,6 @@ scoped_refptr<SecurityOrigin> DocumentLoader::CalculateOrigin(
     debug_info_builder.Append(", url=");
     debug_info_builder.Append(owner_document->Url().BaseAsString());
     debug_info_builder.Append(")");
-  } else if (url_.IsAboutSrcdocURL()) {
-    // If there's no owner_document and this is a sandboxed srcdoc load, then
-    // get the origin from the remote parent. In general, a srcdoc navigation
-    // with no owner_document can only currently happen  if the iframe is
-    // sandboxed and isolated sandboxed frames is enabled, in which case, copy
-    // the origin from the remote parent here (though this origin will only be
-    // used to derive the actual opaque origin later),
-    // TODO(https://crbug.com/328279696): this block can be removed once the
-    // about:srcdoc navigation blocking finishes rolling out, as then the
-    // initiator can never be cross-origin to the parent.
-    bool is_sandboxed = (policy_container_->GetPolicies().sandbox_flags &
-                         network::mojom::blink::WebSandboxFlags::kOrigin) !=
-                        network::mojom::blink::WebSandboxFlags::kNone;
-    CHECK(is_sandboxed);
-    debug_info_builder.Append("about_srcdoc_with_remote_parent[origin=");
-    origin = frame_->Tree()
-                 .Parent()
-                 ->GetSecurityContext()
-                 ->GetSecurityOrigin()
-                 ->IsolatedCopy();
-    debug_info_builder.Append(origin->ToString());
-    debug_info_builder.Append("]");
   } else {
     debug_info_builder.Append("use_url_with_precursor");
     // Otherwise, create an origin that propagates precursor information
