c72bcc04ce52702acbe6be6ec66ff90e683a2c75
Maks Orlovich <morlovich@chromium.org>
2024-05-22 16:41:36 +0000

    CodeHealth: Avoid discouraged unique_ptr<T[]> in remoting.
    In one case, replace it with the now preferred because HeapArray
    which keeps track of the length, which array unique ptr doesn't (see
    https://crbug.com/40285824).
    
    In other, use a base:: helper for a config read to avoid needing to
    manage buffers in the first place.
    
    Fixed: 326459387
    Fixed: 326459178
    Change-Id: I5e2cb20ad3f5c0152a8c6370c11711d7ade5af5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5552876
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304503}

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -616,12 +616,7 @@ bool HostProcess::InitWithCommandLine(const base::CommandLine* cmd_line) {
 
     // Read config from stdin if necessary.
     if (host_config_path_ == base::FilePath(kStdinConfigPath)) {
-      const size_t kBufferSize = 4096;
-      std::unique_ptr<char[]> buf(new char[kBufferSize]);
-      size_t len;
-      while ((len = fread(buf.get(), 1, kBufferSize, stdin)) > 0) {
-        host_config_.append(buf.get(), len);
-      }
+      base::ReadStreamToString(stdin, &host_config_);
     }
   } else {
     base::FilePath default_config_dir = remoting::GetConfigDir();
