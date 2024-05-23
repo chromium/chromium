476cae03acf1eea3439eb700a354f6846e431725
Danil Somsikov <dsv@chromium.org>
2024-05-23 08:51:53 +0000

    Log an error if a binding argument can't be parsed.
    Bug: b:295119680
    Change-Id: I86ac8c102d88b915b2bc9a5ba0bd28d17971e5bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5277385
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304926}

--- chrome/browser/devtools/devtools_embedder_message_dispatcher.cc
+++ chrome/browser/devtools/devtools_embedder_message_dispatcher.cc
@@ -281,11 +281,15 @@ struct ParamTuple<T, Ts...> {
 
 template <typename... As>
 bool ParseAndHandle(const base::RepeatingCallback<void(As...)>& handler,
+                    const std::string& method,
                     DispatchCallback callback,
                     const base::Value::List& list) {
   ParamTuple<As...> tuple;
-  if (!tuple.Parse(list, list.begin()))
+  if (!tuple.Parse(list, list.begin())) {
+    LOG(ERROR) << "Failed to parse arguments for " << method
+               << " call: " << list.DebugString();
     return false;
+  }
   tuple.Apply(handler);
   return true;
 }
@@ -293,11 +297,15 @@ bool ParseAndHandle(const base::RepeatingCallback<void(As...)>& handler,
 template <typename... As>
 bool ParseAndHandleWithCallback(
     const base::RepeatingCallback<void(DispatchCallback, As...)>& handler,
+    const std::string& method,
     DispatchCallback callback,
     const base::Value::List& list) {
   ParamTuple<As...> tuple;
-  if (!tuple.Parse(list, list.begin()))
+  if (!tuple.Parse(list, list.begin())) {
+    LOG(ERROR) << "Failed to parse arguments for " << method
+               << " call: " << list.DebugString();
     return false;
+  }
   tuple.Apply(handler, std::move(callback));
   return true;
 }
@@ -329,7 +337,7 @@ class DispatcherImpl : public DevToolsEmbedderMessageDispatcher {
                        Delegate* delegate) {
     handlers_[method] = base::BindRepeating(
         &ParseAndHandle<As...>,
-        base::BindRepeating(handler, base::Unretained(delegate)));
+        base::BindRepeating(handler, base::Unretained(delegate)), method);
   }
 
   template <typename... As>
@@ -339,7 +347,7 @@ class DispatcherImpl : public DevToolsEmbedderMessageDispatcher {
                                    Delegate* delegate) {
     handlers_[method] = base::BindRepeating(
         &ParseAndHandleWithCallback<As...>,
-        base::BindRepeating(handler, base::Unretained(delegate)));
+        base::BindRepeating(handler, base::Unretained(delegate)), method);
   }
 
  private:
