46d1bb24df6ec211984d058e0016cef15b6e4619
Colin Blundell <blundell@chromium.org>
2024-05-23 18:14:42 +0000

    [//gpu] Fix RawDraw
    [1] inadvertently broke RawDraw, as RawDrawImageBackingFactory checks
    that the usages exactly match what it expects [2][3].
    RASTER_READ | RASTER_WRITE came in due to the expansion of RASTER.
    
    This CL removes RASTER_READ from the usages that
    RawDrawImageBackingFactory expects.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5250514
    [2] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/service/shared_image/raw_draw_image_backing_factory.cc;l=15-18;drc=c0265133106c7647e90f9aaa4377d28190b1a6a9;bpv=1;bpt=1
    [3] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/service/shared_image/raw_draw_image_backing_factory.cc;l=90-94;drc=c0265133106c7647e90f9aaa4377d28190b1a6a9;bpv=1;bpt=1
    
    Bug: 341801008
    Change-Id: If8bdad88d14fd9be0b52bc55e37bf4323d2b94b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563209
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305227}

--- gpu/command_buffer/service/shared_image/raw_draw_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/raw_draw_image_backing_factory.cc
@@ -13,9 +13,8 @@ namespace gpu {
 // works only with OOP-raster (since it must store PaintOps for playback during
 // compositing).
 constexpr uint32_t kSupportedUsage =
-    SHARED_IMAGE_USAGE_DISPLAY_READ | SHARED_IMAGE_USAGE_RASTER_READ |
-    SHARED_IMAGE_USAGE_RASTER_WRITE | SHARED_IMAGE_USAGE_OOP_RASTERIZATION |
-    SHARED_IMAGE_USAGE_RAW_DRAW;
+    SHARED_IMAGE_USAGE_DISPLAY_READ | SHARED_IMAGE_USAGE_RASTER_WRITE |
+    SHARED_IMAGE_USAGE_OOP_RASTERIZATION | SHARED_IMAGE_USAGE_RAW_DRAW;
 
 RawDrawImageBackingFactory::RawDrawImageBackingFactory()
     : SharedImageBackingFactory(kSupportedUsage) {}
