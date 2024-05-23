98e873aa3602e72ae9c42d8d0017ddec5f0ab493
Lingyun Cai <lingyun.cai@intel.com>
2024-05-23 01:29:50 +0000

    Always have move event listener logic in MoveNodeToNewDocument()
    This CL removes the check on moving event listeners from the fast
    adoption code, and adds it back to the common path.
    
    Bug: 341104769
    
    Change-Id: Ic9738d9af97fecdcff6c8c45eea0634b6dc3b9dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546684
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304811}

--- third_party/blink/renderer/core/dom/tree_scope_adopter.cc
+++ third_party/blink/renderer/core/dom/tree_scope_adopter.cc
@@ -225,20 +225,19 @@ inline void TreeScopeAdopter::MoveNodeToNewDocument(
             element, new_document);
       }
     }
-
-    if (old_document.HasAnyListenerTypes()) {
-      node.MoveEventListenersToNewDocument(old_document, new_document);
-    }
   } else {
     // DCHECK all the fast adoption conditions
     DCHECK(!old_document.HasNodeIterators());
     DCHECK(!old_document.HasRanges());
-    DCHECK(!old_document.HasAnyListenerTypes());
     DCHECK(!old_document.HasMutationObservers());
     DCHECK(!old_document.ShouldInvalidateNodeListCaches());
     DCHECK(!old_document.HasExplicitlySetAttrElements());
     DCHECK(!old_document.HasCachedAttrAssociatedElements());
   }
+
+  // TODO(crbug.com/341104769): revisit to see if the logic of moving event
+  // listeners could be optimized for fast adoption.
+  node.MoveEventListenersToNewDocument(old_document, new_document);
 
   if (node.GetCustomElementState() == CustomElementState::kCustom) {
     CustomElement::EnqueueAdoptedCallback(To<Element>(node), old_document,
@@ -259,7 +258,6 @@ inline void TreeScopeAdopter::MoveNodeToNewDocument(
 inline bool TreeScopeAdopter::IsDocumentEligibleForFastAdoption(
     Document& old_document) const {
   return !old_document.HasNodeIterators() && !old_document.HasRanges() &&
-         !old_document.HasAnyListenerTypes() &&
          !old_document.HasMutationObservers() &&
          !old_document.ShouldInvalidateNodeListCaches() &&
          !old_document.HasExplicitlySetAttrElements() &&
