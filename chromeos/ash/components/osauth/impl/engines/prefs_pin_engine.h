affda98b7bdfbcf5b6bb86205f99cfe5e15dcc56
John Admanski <jadmanski@google.com>
2024-05-23 17:20:24 +0000

    Add unit tests for the prefs PIN engine.
    This adds some basic tests: success, failure, and a bunch of failures
    leading to a timeout.
    
    There's some duplication between tests and probably would be even more
    inter-module duplication if we added tests for the other engines, but I
    didn't want to over-abstract with so little examples to work from at the
    start.
    
    BUG=b:271263584
    
    Change-Id: I997d43b684bc0f1695b9d55fffaca8bfc368b89b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555931
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: John Admanski <jadmanski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305179}

--- chromeos/ash/components/osauth/impl/engines/prefs_pin_engine.h
+++ chromeos/ash/components/osauth/impl/engines/prefs_pin_engine.h
@@ -19,8 +19,12 @@
 
 namespace ash {
 
-class PrefsPinEngine : public AuthFactorEngine, public CryptohomeCore::Client {
+class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_OSAUTH) PrefsPinEngine
+    : public AuthFactorEngine,
+      public CryptohomeCore::Client {
  public:
+  static constexpr int kMaximumUnlockAttempts = 5;
+
   PrefsPinEngine(CryptohomeCore& core, PrefService& pref_service);
 
   ~PrefsPinEngine() override;
@@ -29,8 +33,6 @@ class PrefsPinEngine : public AuthFactorEngine, public CryptohomeCore::Client {
   void PerformPinAttempt(const std::string& raw_pin);
 
  private:
-  static constexpr int kMaximumUnlockAttempts = 5;
-
   // Functions to implement AuthFactorEngine.
   AshAuthFactor GetFactor() const override;
   void InitializeCommon(CommonInitCallback callback) override;
