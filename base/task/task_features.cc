d6f0096fac7b950926cc75393e96c7293ed6b31d
Sean Maher <spvw@chromium.org>
2024-05-24 20:03:35 +0000

    base task: Revert accidental enabling of feature in previous CL
    This accidentally enabled by default UIPumpImprovementsWin, which is
    associated with a bunch of tricky nested loop problems and hangs!
    
    Bug: 1269738907
    Change-Id: I29695c0a8189ce909e1f5bab45aef43338e3442a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569177
    Commit-Queue: Sean Maher <spvw@google.com>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305887}

--- base/task/task_features.cc
+++ base/task/task_features.cc
@@ -55,7 +55,7 @@ BASE_FEATURE(kExplicitHighResolutionTimerWin,
 
 BASE_FEATURE(kUIPumpImprovementsWin,
              "UIPumpImprovementsWin",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPumpFastToSleepAndroid,
              "PumpFastToSleepAndroid",
