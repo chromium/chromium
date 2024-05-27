830c508c1e83d4264e0c841d38af67c322521b9e
Hans Wennborg <hans@chromium.org>
2024-05-27 15:00:55 +0000

    Suppress unsafe buffers warnings from the latest version of Clang
    Bug: 341324165
    Change-Id: Icfcfef88ac4bd9b0b043e69083b1ff40358d4566
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5573391
    Owners-Override: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306430}

--- net/disk_cache/simple/simple_index_file.cc
+++ net/disk_cache/simple/simple_index_file.cc
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/341324165): Fix and remove.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "net/disk_cache/simple/simple_index_file.h"
 
 #include <utility>
