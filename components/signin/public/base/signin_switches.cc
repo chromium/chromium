ea4240acb4b1605abaec10d49ddff05b9038bb01
Samar <samarchehade@google.com>
2024-05-27 14:53:20 +0000

    [Signin][Android] Enable SeedAccountsRevamp by default.
    Bug: b/40284908
    Change-Id: Ibcd206e71051d6955943002d2407583666569ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569682
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306426}

--- components/signin/public/base/signin_switches.cc
+++ components/signin/public/base/signin_switches.cc
@@ -16,7 +16,7 @@ namespace switches {
 // Feature to refactor how and when accounts are seeded on Android.
 BASE_FEATURE(kSeedAccountsRevamp,
              "SeedAccountsRevamp",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Feature to apply enterprise policies on signin regardless of sync status.
 BASE_FEATURE(kEnterprisePolicyOnSignin,
