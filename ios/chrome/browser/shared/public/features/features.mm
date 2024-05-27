453710ad93f7209d2ffe1872a243ae07c61454b0
Stepan Khapugin <stkhapugin@chromium.org>
2024-05-27 12:37:13 +0000

    [iOS][Omnibox] Enable experimental textfield by default.
    Fixed: 41402635
    Change-Id: I453ad0db1867de2fa445ae6fa57becb5bf30eb29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569400
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306378}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -143,7 +143,7 @@ BASE_FEATURE(kIOSEditMenuHideSearchWeb,
 
 BASE_FEATURE(kIOSNewOmniboxImplementation,
              "kIOSNewOmniboxImplementation",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableColorLensAndVoiceIconsInHomeScreenWidget,
              "kEnableColorLensAndVoiceIconsInHomeScreenWidget",
