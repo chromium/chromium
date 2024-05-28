6e4d4b8fbada32c4a4cc23dd848c45e386666b64
Morten Stenshorne <mstensho@chromium.org>
2024-05-28 22:36:04 +0000

    Need to include non-auto margins when resolving auto margins.
    Make sure that an auto margin doesn't receive the space actually taken
    up by the non-auto margin on the other side of the box. Didn't get this
    right for @page auto margins.
    
    Bug: 40286153
    Change-Id: Iff877c6f0ad9ecee594f51ef7b4ca5f71eb3be26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5573406
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307087}

--- third_party/blink/renderer/core/layout/pagination_utils.cc
+++ third_party/blink/renderer/core/layout/pagination_utils.cc
@@ -166,12 +166,14 @@ void ResolvePageBoxGeometry(const BlockNode& page_box,
         }
       };
   LayoutUnit additional_inline_space =
-      space.AvailableSize().inline_size - geometry->border_box_size.inline_size;
+      space.AvailableSize().inline_size -
+      (geometry->border_box_size.inline_size + margins->InlineSum());
   ResolveAutoMargin(style.MarginInlineStartUsing(style),
                     style.MarginInlineEndUsing(style), additional_inline_space,
                     &margins->inline_start, &margins->inline_end);
   LayoutUnit additional_block_space =
-      space.AvailableSize().block_size - geometry->border_box_size.block_size;
+      space.AvailableSize().block_size -
+      (geometry->border_box_size.block_size + margins->BlockSum());
   ResolveAutoMargin(style.MarginBlockStartUsing(style),
                     style.MarginBlockEndUsing(style), additional_block_space,
                     &margins->block_start, &margins->block_end);
