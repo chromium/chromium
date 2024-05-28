551d606ec1151acf86b07881640bd0aa88c3b9a8
Anastasia Helfinstein <anastasi@google.com>
2024-05-28 19:59:34 +0000

    [a11y extension] Fix high contrast webstore extension
    Takes the functioning mv2 version of the files from the webstore, and
    re-migrates to mv3 from scratch.
    
    AX-Relnotes: n/a.
    Bug: b/260590502, b/342407844
    Change-Id: Iac551d4e0c66b3308909ccf9ef505a1843ce4613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577857
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1306975}

--- ui/accessibility/extensions/highcontrast/background.js
+++ ui/accessibility/extensions/highcontrast/background.js
@@ -1,133 +1,59 @@
-// Copyright 2014 The Chromium Authors
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-class Background {
-  constructor() {
-    this.readyTabs_ = new Set();
-    this.init_();
-  }
+importScripts("common.js");
 
-  /**
-   * @param {function(chrome.tabs.Tab)} tabCallback A function that performs
-   *     some action on each tab.
-   * @private
-   */
-  forAllTabs_(tabCallback) {
-    chrome.windows.getAll({'populate': true}, function(windows) {
-      for (const window of windows) {
-        for (const tab of window.tabs) {
-          if (isDisallowedUrl(tab.url)) {
-            continue;
-          }
-          tabCallback(tab);
+function injectContentScripts() {
+  chrome.windows.getAll({'populate': true}, windows => {
+    for (const w of windows) {
+      for (const tab of w.tabs) {
+        var url = tab.url;
+        if (url.indexOf('chrome') == 0 || url.indexOf('about') == 0) {
+          continue;
         }
-      }
-    });
-  }
-
-  /** @public */
-  injectContentScripts() {
-    this.forAllTabs_(tab => {
-      try {
         chrome.scripting.executeScript({
-          target: {tabId: tab.id, allFrames: true},
-          files: ['highcontrast.js'],
+            target: { tabId: tab.id, allFrames: true },
+            files: [ 'common.js', 'highcontrast.js' ],
+            injectImmediately: true,
         });
-      } catch (err) {
-        // Convert the error to a warning to prevent service worker crash.
-        console.warn(err);
       }
-    });
-  }
-
-  /** @private */
-  updateTabs_() {
-    this.forAllTabs_(tab => {
-      const msg = {
-        'enabled': Storage.enabled,
-        'scheme': Storage.getSiteScheme(siteFromUrl(tab.url)),
-      };
-      if (this.readyTabs_.has(tab.id)) {
-        chrome.tabs.sendMessage(tab.id, msg);
-      }
-    });
-  }
-
-  /** @private */
-  toggleEnabled_() {
-    Storage.enabled = !Storage.enabled;
-    this.updateTabs_();
-  }
-
-  /**
-   * @param {string} url
-   * @private
-   */
-  toggleSite_(url) {
-    const site = siteFromUrl(url);
-    let scheme = Storage.getSiteScheme(site);
-    if (scheme > 0) {
-      scheme = 0;
-    } else if (Storage.scheme > 0) {
-      scheme = Storage.scheme;
-    } else {
-      scheme = Storage.SCHEME.defaultValue;
     }
-    Storage.setSiteScheme(site, scheme);
-    this.updateTabs_();
-  }
+  });
+}
 
-  /**
-   * @param {*} message
-   * @param {chrome.runtime.MessageSender} sender
-   * @param {function} sendResponse
-   * @private
-   */
-  handleRequest_(message, sender, sendResponse) {
-    if (message['updateTabs']) {
-      this.updateTabs_();
-    }
+function toggleEnabled() {
+  setEnabled(!getEnabled());
+}
+
+function toggleSite(url) {
+  var site = siteFromUrl(url);
+  var scheme = getSiteScheme(site);
+  if (scheme > 0) {
+    scheme = 0;
+  } else if (getDefaultScheme() > 0) {
+    scheme = getDefaultScheme();
+  } else {
+    scheme = DEFAULT_SCHEME;
+  }
+  setSiteScheme(site, scheme);
+}
+
+async function init() {
+  injectContentScripts();
+
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message['toggle_global']) {
-      this.toggleEnabled_();
+      toggleEnabled();
     }
     if (message['toggle_site']) {
-      this.toggleSite_(sender.tab ? sender.tab.url : 'www.example.com');
+      toggleSite(sender.tab ? sender.tab.url : 'www.example.com');
     }
-    if (message['init']) {
-      let scheme = Storage.scheme;
-      if (sender.tab) {
-        scheme = Storage.getSiteScheme(siteFromUrl(sender.tab.url));
-        this.readyTabs_.add(sender.tab.id);
-      } else {
-        console.warn('No tab for init message from', JSON.stringify(sender));
-      }
-      const msg = {
-        'enabled': Storage.enabled,
-        'scheme': scheme,
-      };
-      sendResponse(msg);
-    }
-  }
+  });
 
-  /** @private */
-  init_() {
-    this.injectContentScripts();
-    this.updateTabs_();
-
-    chrome.runtime.onMessage.addListener(this.handleRequest_.bind(this));
-
-    chrome.storage.onChanged.addListener(this.updateTabs_.bind(this));
-
-    if (navigator.userAgentData.platform.indexOf('Mac') != -1) {
-      chrome.action.setTitle({'title': 'High Contrast (Cmd+Shift+F11)'});
-    }
+  if (navigator.appVersion.indexOf('Mac') != -1) {
+    chrome.action.setTitle({'title': 'High Contrast (Cmd+Shift+F11)'});
   }
 }
 
-self.addEventListener('install', () => {
-  importScripts('common.js');
-  importScripts('storage.js');
-  Storage.initialize();
-  const background = new Background();
-});
+self.addEventListener('install', init);
