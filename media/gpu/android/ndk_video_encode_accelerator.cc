dfd59ac6ee065a4549046acf57b612fdc9d044c5
Dale Curtis <dalecurtis@chromium.org>
2024-05-28 23:04:40 +0000

    [canary] Disable profile selection and tests for Android encoding.
    Implementation is incomplete, level needs to be set as well per
    the documentation.
    
    R=eugene
    
    (cherry picked from commit a657de31943f1094917a1e51bc2636f45b3360b5)
    
    Bug: 343199623, 329691946
    Change-Id: I70f7425b57cc28f634455a658d7e93dbb2fc5514
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578194
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1307095}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577870
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6507@{#7}
    Cr-Branched-From: 5ee4b4ac657169264e2139c955fe2beeb71062b5-refs/heads/main@{#1306718}

--- media/gpu/android/ndk_video_encode_accelerator.cc
+++ media/gpu/android/ndk_video_encode_accelerator.cc
@@ -43,6 +43,16 @@ enum PixelFormat {
   COLOR_FORMAT_YUV420_SEMIPLANAR = 21,  // Same as NV12
 };
 
+struct AMediaFormatDeleter {
+  inline void operator()(AMediaFormat* ptr) const {
+    if (ptr) {
+      AMediaFormat_delete(ptr);
+    }
+  }
+};
+
+// TODO(crbug.com/343199623): Uncomment once we also set level.
+#if 0
 enum class CodecProfileLevel {
   // Subset of MediaCodecInfo.CodecProfileLevel
   AVCProfileBaseline = 0x01,
@@ -77,12 +87,6 @@ enum class CodecProfileLevel {
   HEVCProfileMain10HDR10 = 0x1000,
   HEVCProfileMain10HDR10Plus = 0x2000,
   Unknown = 0xFFFFFF,
-};
-struct AMediaFormatDeleter {
-  inline void operator()(AMediaFormat* ptr) const {
-    if (ptr)
-      AMediaFormat_delete(ptr);
-  }
 };
 
 CodecProfileLevel GetAndroidVideoProfile(VideoCodecProfile profile,
@@ -126,6 +130,7 @@ CodecProfileLevel GetAndroidVideoProfile(VideoCodecProfile profile,
       return CodecProfileLevel::Unknown;
   }
 }
+#endif
 
 bool GetAndroidColorValues(const gfx::ColorSpace& cs,
                            int* standard,
@@ -203,13 +208,14 @@ MediaFormatPtr CreateVideoFormat(const VideoEncodeAccelerator::Config& config,
                                  int num_temporal_layers,
                                  PixelFormat format) {
   int iframe_interval = config.gop_length.value_or(kDefaultGOPLength);
-  int profile = static_cast<int>(GetAndroidVideoProfile(
-      config.output_profile, config.is_constrained_h264));
   auto mime = MediaCodecUtil::CodecToAndroidMimeType(
       VideoCodecProfileToVideoCodec(config.output_profile));
   MediaFormatPtr result(AMediaFormat_new());
   AMediaFormat_setString(result.get(), AMEDIAFORMAT_KEY_MIME, mime.c_str());
-  AMediaFormat_setInt32(result.get(), AMEDIAFORMAT_KEY_PROFILE, profile);
+  // TODO(crbug.com/343199623): Uncomment once we also set level.
+  // int profile = static_cast<int>(GetAndroidVideoProfile(
+  //     config.output_profile, config.is_constrained_h264));
+  // AMediaFormat_setInt32(result.get(), AMEDIAFORMAT_KEY_PROFILE, profile);
   AMediaFormat_setInt32(result.get(), AMEDIAFORMAT_KEY_WIDTH,
                         frame_size.width());
   AMediaFormat_setInt32(result.get(), AMEDIAFORMAT_KEY_HEIGHT,
