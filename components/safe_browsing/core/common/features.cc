98b8fd7038c4b4afd6b39a95c9dad9064bcc1f08
Sarah Krakowiak Criel <skrakowi@chromium.org>
2024-05-29 14:08:45 +0000

    CreateWarningShownClientSafeBrowsingReports enable on TOT
    Bug: b/327838835
    Change-Id: I6d2c1e1b9a0bc2cb43111412e665036ca467f149
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577338
    Reviewed-by: Nwokedi Idika <nwokedi@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307392}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -56,7 +56,7 @@ BASE_FEATURE(kCreateNotificationsAcceptedClientSafeBrowsingReports,
 
 BASE_FEATURE(kCreateWarningShownClientSafeBrowsingReports,
              "CreateWarningShownClientSafeBrowsingReports",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDeepScanningPromptRemoval,
              "SafeBrowsingDeepScanningPromptRemoval",
