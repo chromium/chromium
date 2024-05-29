a206a26a6b06d08d25b70ef5d981e31d57bd3f95
Ashutosh Singhal <macinashutosh@google.com>
2024-05-29 15:23:09 +0000

    [Cleanup]  download_db_conversions_unittest
    * remove template arguments from std::make_pair usage
    https://clang.llvm.org/extra/clang-tidy/checks/modernize/use-emplace.html
    
    * replace push_back with emplace_back
    https://clang.llvm.org/extra/clang-tidy/checks/modernize/use-emplace.html
    
    Change-Id: Id70507d2517e84cdc7a71aef933ad8efa719faa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5573790
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1307441}

--- components/download/database/download_db_conversions_unittest.cc
+++ components/download/database/download_db_conversions_unittest.cc
@@ -46,10 +46,8 @@ InProgressInfo CreateInProgressInfo() {
   info.bytes_wasted = 1234;
   info.auto_resume_count = 3;
   info.fetch_error_body = true;
-  info.request_headers.emplace_back(
-      std::make_pair<std::string, std::string>("123", "456"));
-  info.request_headers.emplace_back(
-      std::make_pair<std::string, std::string>("ABC", "def"));
+  info.request_headers.emplace_back("123", "456");
+  info.request_headers.emplace_back("ABC", "def");
   info.credentials_mode = ::network::mojom::CredentialsMode::kOmit;
   return info;
 }
@@ -82,10 +80,8 @@ TEST_F(DownloadDBConversionsTest, DownloadEntry) {
   entry.ukm_download_id = 123;
   entry.bytes_wasted = 1234;
   entry.fetch_error_body = true;
-  entry.request_headers.emplace_back(
-      std::make_pair<std::string, std::string>("123", "456"));
-  entry.request_headers.emplace_back(
-      std::make_pair<std::string, std::string>("ABC", "def"));
+  entry.request_headers.emplace_back("123", "456");
+  entry.request_headers.emplace_back("ABC", "def");
   EXPECT_EQ(entry, DownloadEntryFromProto(DownloadEntryToProto(entry)));
 }
 
@@ -96,17 +92,14 @@ TEST_F(DownloadDBConversionsTest, DownloadEntries) {
 
   // Entries vector with one entry.
   DownloadUrlParameters::RequestHeadersType request_headers;
-  entries.push_back(DownloadEntry("guid", "request origin",
-                                  DownloadSource::UNKNOWN, false,
-                                  request_headers, 123));
+  entries.emplace_back("guid", "request origin", DownloadSource::UNKNOWN, false,
+                       request_headers, 123);
   EXPECT_EQ(entries, DownloadEntriesFromProto(DownloadEntriesToProto(entries)));
 
   // Entries vector with multiple entries.
-  request_headers.emplace_back(
-      DownloadUrlParameters::RequestHeadersNameValuePair("key", "value"));
-  entries.push_back(DownloadEntry("guid2", "request origin",
-                                  DownloadSource::UNKNOWN, true,
-                                  request_headers, 456));
+  request_headers.emplace_back("key", "value");
+  entries.emplace_back("guid2", "request origin", DownloadSource::UNKNOWN, true,
+                       request_headers, 456);
   EXPECT_EQ(entries, DownloadEntriesFromProto(DownloadEntriesToProto(entries)));
 }
 
