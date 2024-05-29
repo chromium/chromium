532c32b0f9b4fb5a82bffeb31085e237132e6b43
Tomasz Wiszkowski <ender@google.com>
2024-05-29 23:17:55 +0000

    Eliminate blink effect while animating suggestions list.
    Change-Id: I8444181d775246df8779c46212ed2e63c7dc231e
    Fixed: 341377411
    Bug: 40253396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582507
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307762}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionListViewBinder.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionListViewBinder.java
@@ -42,6 +42,7 @@ class SuggestionListViewBinder {
         } else if (SuggestionListProperties.EMBEDDER.equals(propertyKey)) {
             view.dropdown.setEmbedder(model.get(SuggestionListProperties.EMBEDDER));
         } else if (SuggestionListProperties.OMNIBOX_SESSION_ACTIVE.equals(propertyKey)) {
+            updateContainerVisibility(model, view);
             view.dropdown.onOmniboxSessionStateChange(
                     model.get(SuggestionListProperties.OMNIBOX_SESSION_ACTIVE));
         } else if (SuggestionListProperties.GESTURE_OBSERVER.equals(propertyKey)) {
@@ -108,7 +109,9 @@ class SuggestionListViewBinder {
     private static void updateContainerVisibility(
             PropertyModel model, SuggestionListViewHolder holder) {
         ModelList listItems = model.get(SuggestionListProperties.SUGGESTION_MODELS);
-        int visibility = listItems.size() == 0 ? View.GONE : View.VISIBLE;
+        boolean shouldBeVisible =
+                model.get(SuggestionListProperties.OMNIBOX_SESSION_ACTIVE) && listItems.size() > 0;
+        int visibility = shouldBeVisible ? View.VISIBLE : View.GONE;
         holder.container.setVisibility(visibility);
         holder.dropdown.setVisibility(visibility);
     }
