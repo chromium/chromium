18462ebe1ed0cb31a992d70b4fc4d12011efd14a
Lauren Winston <lwinston@google.com>
2024-05-30 19:15:39 +0000

    [Reading Mode] Re-enable delayed side panel loading.
    This is based on side panel guidance.
    
    The Reading Mode panel will now be shown after UI finishes loading.
    This prevents empty loading artifacts from showing.
    
    Fonts are loaded asynchronously, so they won't block the side panel
    loading.
    
    Fixed: 327507135
    Change-Id: Iddca3de7f090f44cfb006ce3ed3123e747557e14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5571131
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308188}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -318,14 +318,6 @@ bool IsReadAnythingOmniboxIconEnabled() {
   return base::FeatureList::IsEnabled(::features::kReadAnythingOmniboxIcon);
 }
 
-BASE_FEATURE(kReadAnythingDelaySidePanelLoad,
-             "ReadAnythingDelaySidePanelLoad",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-bool IsReadAnythingDelaySidePanelLoadEnabled() {
-  return base::FeatureList::IsEnabled(
-      ::features::kReadAnythingDelaySidePanelLoad);
-}
-
 BASE_FEATURE(kReadAnythingReadAloud,
              "ReadAnythingReadAloud",
              base::FEATURE_DISABLED_BY_DEFAULT);
