9495fe837f609910257d4034e69666af9ebac2c5
Min Qin <qinmin@chromium.org>
2024-05-30 00:10:51 +0000

    Add group member to datasharing internal UI
    Bug: 328265318
    Change-Id: I3bf9f362084b1ba85054cd173a90bc2615e78ffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5583116
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307791}

--- chrome/browser/resources/data_sharing_internals/data_sharing_internals.ts
+++ chrome/browser/resources/data_sharing_internals/data_sharing_internals.ts
@@ -4,7 +4,8 @@
 
 import {getRequiredElement} from 'chrome://resources/js/util.js';
 
-import type {GroupData} from './data_sharing_internals.mojom-webui.js';
+import type {GroupData, GroupMember} from './data_sharing_internals.mojom-webui.js';
+import {RoleType} from './data_sharing_internals.mojom-webui.js';
 import {DataSharingInternalsBrowserProxy} from './data_sharing_internals_browser_proxy.js';
 
 function getProxy(): DataSharingInternalsBrowserProxy {
@@ -12,10 +13,35 @@ function getProxy(): DataSharingInternalsBrowserProxy {
 }
 
 function appendTextChildToList(textToShow: string, element: HTMLUListElement) {
-  const textElement = document.createElement('pre');
+  const textElement = document.createElement('li');
   textElement.textContent = textToShow;
   element.appendChild(textElement);
 }
+
+function roleTypeToString(role: RoleType): string {
+  switch (role) {
+    case RoleType.UNKNOWN:
+      return 'Unknown';
+    case RoleType.OWNER:
+      return 'Owner';
+    case RoleType.MEMBER:
+      return 'Member';
+    case RoleType.INVITEE:
+      return 'Invitee';
+  }
+}
+
+function addMemberToGroup(member: GroupMember, group: HTMLUListElement) {
+  const memberlistItem = document.createElement('li');
+  const memberItem = document.createElement('ul');
+  appendTextChildToList(member.displayName, memberItem);
+  appendTextChildToList(member.email, memberItem);
+  appendTextChildToList(roleTypeToString(member.role), memberItem);
+  appendTextChildToList(member.avatarUrl.url, memberItem);
+  memberlistItem.appendChild(memberItem);
+  group.appendChild(memberlistItem);
+}
+
 
 /**
  * Show all groups information.
@@ -25,11 +51,15 @@ function displayGroups(isSuccess: boolean, groupData: GroupData[]) {
       isSuccess ? 'success' : 'failed';
   const groupList = getRequiredElement('group-list');
   groupData.forEach((group) => {
-    const listItem = document.createElement('ul');
-    appendTextChildToList(group.groupId, listItem);
-    appendTextChildToList(group.name, listItem);
+    const listItem = document.createElement('li');
+    const groupItem = document.createElement('ul');
+    appendTextChildToList(group.groupId, groupItem);
+    appendTextChildToList(group.name, groupItem);
+    group.members.forEach((member) => {
+      addMemberToGroup(member, groupItem);
+    });
+    listItem.appendChild(groupItem);
     groupList.appendChild(listItem);
-    // TODO (qinmin): display member information for the group.
   });
 }
 
