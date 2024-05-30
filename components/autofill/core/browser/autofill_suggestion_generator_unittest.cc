a404b284dd9e8fd974be1fed8c2c482520d57577
Christoph Schwering <schwering@google.com>
2024-05-30 16:43:38 +0000

    Fix crash bug in label creation.
    The absence of labels for an autofill::Suggestion is represented as
    Suggestion::labels = {}; In particular, (keyboard accessory) UI code
    expects that !Suggestion::labels.empty() means that there must be
    suggestions - i.e. Suggestion::labels[0] should not be empty.
    
    This CL is a hotfix to avoid that Suggestion::labels is set to {{}}
    during address suggestion generation.
    
    In the mid-term, we should revisit (and clearly document) which label
    configurations are permissible on which platform.
    
    Bug: 343433571
    Change-Id: I07bca57dadf9f8a7ae57cd10790282c5bdd040e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5580874
    Owners-Override: Erhu Akpobaro <eakpobaro@google.com>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308066}

--- components/autofill/core/browser/autofill_suggestion_generator_unittest.cc
+++ components/autofill/core/browser/autofill_suggestion_generator_unittest.cc
@@ -1419,7 +1419,7 @@ TEST_F(
 
   ASSERT_EQ(suggestions.size(), 1u);
   EXPECT_EQ(suggestions[0].labels,
-            std::vector<std::vector<Suggestion::Text>>({{}}));
+            std::vector<std::vector<Suggestion::Text>>({}));
 }
 
 TEST_F(AutofillChildrenSuggestionGeneratorTest,
@@ -1608,7 +1608,7 @@ TEST_F(
               EqualsFieldByFieldFillingSuggestion(
                   SuggestionType::kAddressFieldByFieldFilling,
                   profile().GetInfo(NAME_FIRST, app_locale()), NAME_FIRST,
-                  Suggestion::Guid(profile().guid()), {{}}));
+                  Suggestion::Guid(profile().guid()), {}));
 }
 
 TEST_F(AutofillChildrenSuggestionGeneratorTest,
