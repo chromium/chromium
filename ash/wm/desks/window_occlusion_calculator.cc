06c86a95f5f84708d06ae80a8fb5a7a1eadce410
Eric Sum <esum@google.com>
2024-05-31 23:11:40 +0000

    Add a short-lived utility class to track desk window occlusion state.
    Makes the DeskPreviewView code a little cleaner by adding this utility
    to take care of the window occlusion tracking.
    
    Bug: b:294094124
    Change-Id: I36b94f3be819d1389ee9784b39fd06810c659699
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578734
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308956}

---
+++ ash/wm/desks/window_occlusion_calculator.cc
@@ -0,0 +1,234 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/wm/desks/window_occlusion_calculator.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/check.h"
+#include "base/logging.h"
+#include "base/notreached.h"
+#include "ui/aura/window_occlusion_change_builder.h"
+#include "ui/aura/window_tracker.h"
+
+namespace ash {
+
+namespace {
+
+void NotifyObserversOfOcclusionChange(
+    base::ObserverList<WindowOcclusionCalculator::Observer>& observers,
+    aura::Window* window) {
+  for (WindowOcclusionCalculator::Observer& obs : observers) {
+    obs.OnWindowOcclusionChanged(window);
+  }
+}
+
+}  // namespace
+
+// Called by the `aura::WindowOcclusionTracker` whenever an occlusion state is
+// calculated for a window. This simply relays that occlusion state back to the
+// `WindowOcclusionCalculator`.
+class WindowOcclusionCalculator::WindowOcclusionChangeBuilderImpl
+    : public aura::WindowOcclusionChangeBuilder {
+ public:
+  explicit WindowOcclusionChangeBuilderImpl(
+      WindowOcclusionCalculator* occlusion_reader)
+      : occlusion_reader_(occlusion_reader) {
+    CHECK(occlusion_reader_);
+  }
+  WindowOcclusionChangeBuilderImpl(const WindowOcclusionChangeBuilderImpl&) =
+      delete;
+  WindowOcclusionChangeBuilderImpl& operator=(
+      const WindowOcclusionChangeBuilderImpl&) = delete;
+  ~WindowOcclusionChangeBuilderImpl() override {
+    // `aura::WindowOcclusionChangeBuilder` is a short-lived class.
+    // `aura::WindowOcclusionTracker` creates a new one whenever it recomputes
+    // occlusion state, and then destroys it when the computation is done.
+    //
+    // During the computation, it's technically possible for
+    // `aura::WindowOcclusionTracker` to set a window's occlusion state
+    // incrementally during the calculation with transient values (i.e. `Add()`
+    // can get called multiple times for the same window). Therefore, it's more
+    // efficient to cache each window's most recent occlusion state within
+    // `Add()` and flush them all here in the destructor with the occlusion
+    // computation's final values. This prevents transient occlusion states made
+    // during the computation from unnecessarily getting propagated.
+    for (const auto& [window, occlusion_state] : occlusion_changes_) {
+      // Corner case: Window might be destroyed from the time it was `Add()`ed
+      // until now. If so, don't set occlusion state for a dead window.
+      if (window_tracker_.Contains(window)) {
+        occlusion_reader_->SetOcclusionState(window, occlusion_state);
+      }
+    }
+  }
+
+  // aura::WindowOcclusionChangeBuilder:
+  void Add(aura::Window* window,
+           aura::Window::OcclusionState occlusion_state,
+           SkRegion occluded_region) override {
+    CHECK_NE(occlusion_state, aura::Window::OcclusionState::UNKNOWN);
+    occlusion_changes_[window] = occlusion_state;
+    window_tracker_.Add(window);
+  }
+
+ private:
+  const raw_ptr<WindowOcclusionCalculator> occlusion_reader_;
+  WindowOcclusionCalculator::WindowOcclusionMap occlusion_changes_;
+  aura::WindowTracker window_tracker_;
+};
+
+// One per parent window being observed.
+class WindowOcclusionCalculator::ObservationState {
+ public:
+  using Observer = WindowOcclusionCalculator::Observer;
+
+  ObservationState(aura::Window* parent_window,
+                   aura::WindowOcclusionTracker* occlusion_tracker)
+      : forced_visibility_(parent_window, occlusion_tracker) {}
+  ObservationState(const ObservationState&) = delete;
+  ObservationState& operator=(const ObservationState&) = delete;
+  ~ObservationState() = default;
+
+  base::ObserverList<Observer>& observer_list() { return observer_list_; }
+
+ private:
+  base::ObserverList<Observer> observer_list_;
+  // All parent windows being observed are forced visible. This requirement is
+  // specific to desk bar usage. The parent windows (and all their descendants)
+  // for inactive desks are `HIDDEN` by default, which is not useful for this
+  // performance optimization. Forcing each desk's parent window to be visible
+  // makes the occlusion state of that desk's windows available.
+  aura::WindowOcclusionTracker::ScopedForceVisible forced_visibility_;
+};
+
+WindowOcclusionCalculator::WindowOcclusionCalculator() {
+  occlusion_tracker_.set_occlusion_change_builder_factory(base::BindRepeating(
+      [](WindowOcclusionCalculator* occlusion_reader)
+          -> std::unique_ptr<aura::WindowOcclusionChangeBuilder> {
+        return std::make_unique<WindowOcclusionChangeBuilderImpl>(
+            std::move(occlusion_reader));
+      },
+      // Use-after-free is not a concern in this case because the
+      // `occlusion_tracker_` is owned by `WindowOcclusionCalculator` and
+      // hence, does not outlive `this`.
+      base::Unretained(this)));
+}
+
+WindowOcclusionCalculator::~WindowOcclusionCalculator() = default;
+
+aura::Window::OcclusionState WindowOcclusionCalculator::GetOcclusionState(
+    aura::Window* window) const {
+  auto iter = occlusion_map_.find(window);
+  return iter == occlusion_map_.end() ? aura::Window::OcclusionState::UNKNOWN
+                                      : iter->second;
+}
+
+void WindowOcclusionCalculator::AddObserver(
+    const aura::Window::Windows& parent_windows_to_track,
+    Observer* observer) {
+  // The `ScopedPause` is an optimization. Each call to
+  // `aura::WindowOcclusionTracker::Track()` triggers a traversal of the entire
+  // window tree to recompute the occlusion, and
+  // `aura::WindowOcclusionTracker::Track()` must be called for all
+  // `parent_windows_to_track` plus each of their descendants (which can be a
+  // lot of computation). Therefore, pause the `occlusion_tracker_` first so
+  // that each added window does not trigger a recalculation. Then, when the
+  // `ScopedPause` is destroyed, one occlusion calculation is triggered for all
+  // tracked windows. This yields the same result but with only one window tree
+  // traversal.
+  {
+    aura::WindowOcclusionTracker::ScopedPause occlusion_calculation_barrier(
+        &occlusion_tracker_);
+    for (const auto& parent_window : parent_windows_to_track) {
+      // Adding an entry to `occlusion_change_observers_` at this point sets up
+      // the forced visibility for the `parent_window`.
+      occlusion_change_observers_.try_emplace(
+          parent_window.get(), std::make_unique<ObservationState>(
+                                   parent_window, &occlusion_tracker_));
+      if (!tracked_window_observations_.IsObservingSource(parent_window)) {
+        TrackOcclusionChangesForAllDescendants(parent_window.get());
+      }
+    }
+  }
+  // Add observer after the initial occlusion calculation is done. That ensures
+  // the `observer` is not notified immediately of the initial occlusion state
+  // (which is not necessary for the desk bar's use case).
+  for (const auto& [parent_window, observation_state] :
+       occlusion_change_observers_) {
+    // The `observer` should only be notified of occlusion changes for the
+    // windows it cares about (those in `parent_windows_to_track`).
+    if (base::Contains(parent_windows_to_track, parent_window)) {
+      observation_state->observer_list().AddObserver(observer);
+    }
+  }
+}
+
+void WindowOcclusionCalculator::RemoveObserver(Observer* observer) {
+  aura::WindowOcclusionTracker::ScopedPause occlusion_calculation_barrier(
+      &occlusion_tracker_);
+  auto iter = occlusion_change_observers_.begin();
+  while (iter != occlusion_change_observers_.end()) {
+    ObservationState& observation_state = *iter->second;
+    observation_state.observer_list().RemoveObserver(observer);
+    if (observation_state.observer_list().empty()) {
+      iter = occlusion_change_observers_.erase(iter);
+    } else {
+      ++iter;
+    }
+  }
+}
+
+// Note `aura::WindowOcclusionTracker` automatically updates all of its
+// bookkeeping internally when a tracked window gets destroyed. So this only
+// has to do bookkeeping for `WindowOcclusionCalculator` specific things.
+void WindowOcclusionCalculator::OnWindowDestroyed(aura::Window* window) {
+  tracked_window_observations_.RemoveObservation(window);
+  // Erasure makes `GetOcclusionState()` return `UNKNOWN` if the caller happens
+  // to request the occlusion state of a window that was being tracked
+  // but got destroyed.
+  occlusion_map_.erase(window);
+  // Prevents use-after-free in `SetOcclusionState()` when iterating through
+  // `occlusion_change_observers_`.
+  occlusion_change_observers_.erase(window);
+}
+
+void WindowOcclusionCalculator::SetOcclusionState(
+    aura::Window* window,
+    aura::Window::OcclusionState occlusion_state) {
+  // Update the `occlusion_map_` before notifying observers of the change in
+  // case the observer immediately calls `GetOcclusionState()` for the changed
+  // window.
+  auto [iter, inserted] = occlusion_map_.try_emplace(window, occlusion_state);
+  if (!inserted && iter->second == occlusion_state) {
+    return;
+  }
+  iter->second = occlusion_state;
+  DVLOG(4) << __func__ << " window=" << window->GetName() << " occlusion="
+           << aura::Window::OcclusionStateToString(occlusion_state);
+
+  // Note the API allows for multiple observers for the same occlusion change,
+  // so iterate completely through `occlusion_change_observers_` without fast
+  // returning or breaking.
+  for (const auto& [parent_window, observation_state] :
+       occlusion_change_observers_) {
+    if (parent_window->Contains(window)) {
+      NotifyObserversOfOcclusionChange(observation_state->observer_list(),
+                                       window);
+    }
+  }
+}
+
+void WindowOcclusionCalculator::TrackOcclusionChangesForAllDescendants(
+    aura::Window* window) {
+  CHECK(!tracked_window_observations_.IsObservingSource(window));
+  tracked_window_observations_.AddObservation(window);
+  occlusion_tracker_.Track(window);
+  // Order in which children are tracked does not matter here.
+  for (const auto& child : window->children()) {
+    TrackOcclusionChangesForAllDescendants(child.get());
+  }
+}
+
+}  // namespace ash
---
+++ ash/wm/desks/window_occlusion_calculator.h
@@ -0,0 +1,133 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WM_DESKS_WINDOW_OCCLUSION_CALCULATOR_H_
+#define ASH_WM_DESKS_WINDOW_OCCLUSION_CALCULATOR_H_
+
+#include <memory>
+#include <vector>
+
+#include "ash/ash_export.h"
+#include "base/containers/flat_map.h"
+#include "base/memory/raw_ptr.h"
+#include "base/observer_list.h"
+#include "base/observer_list_types.h"
+#include "base/scoped_multi_source_observation.h"
+#include "ui/aura/window.h"
+#include "ui/aura/window_observer.h"
+#include "ui/aura/window_occlusion_tracker.h"
+
+namespace ash {
+
+// Calculates occlusion state for a set of `aura::Window`s and notifies
+// observers whenever the occlusion changes.
+//
+// Context:
+// When rendering the desk bar, we only want to mirror the windows in each desk
+// that are actually visible for performance reasons. Thus, each desk's window's
+// occlusion state is required.
+//
+// There are 2 issues with using the global `aura::WindowOcclusionTracker`
+// inside `aura::Env` (i.e. `aura::Window::GetOcclusionState()` to get this
+// information):
+// 1) Window occlusion tracking is paused when opening overview mode for a
+//    separate performance reason. This prevents this particular optimization
+//    from getting each window's occlusion state.
+// 2) If we turn on a window's occlusion tracking for this optimization, it's on
+//    permanently for the rest of the ChromeOS session (resulting in possibly
+//    unnecessary calculations for an indefinite amount of time). We really only
+//    need the occlusion state for the few seconds that the desk bar is open.
+//
+// To solve this, a separate `aura::WindowOcclusionTracker` instance is created
+// within `WindowOcclusionCalculator` to compute windows' occlusion state and is
+// only alive for the duration of the desk bar. The key here is that this
+// transient tracker uses a different `aura::WindowOcclusionChangeBuilder`
+// implementation that does not call `aura::Window::SetOcclusionInfo()`. Rather,
+// it passively observes the occlusion state calculated for each window and
+// makes this information available to the caller. The global
+// `aura::WindowOcclusionTracker` inside `aura::Env` is still the only one in
+// the system mutating the `aura::Window` occlusion state.
+class ASH_EXPORT WindowOcclusionCalculator : public aura::WindowObserver {
+ public:
+  class Observer : public base::CheckedObserver {
+   public:
+    // Invoked whenever the occlusion state of a tracked window changes. The
+    // caller may synchronously call `GetOcclusionState()` to retrieve the
+    // `window`'s new state.
+    virtual void OnWindowOcclusionChanged(aura::Window* window) = 0;
+  };
+
+  WindowOcclusionCalculator();
+  WindowOcclusionCalculator(const WindowOcclusionCalculator&) = delete;
+  WindowOcclusionCalculator& operator=(const WindowOcclusionCalculator&) =
+      delete;
+  ~WindowOcclusionCalculator() override;
+
+  // Returns the current occlusion state of the given `window`. The occlusion
+  // state is only `UNKNOWN` in one of these cases:
+  // * The `window` is not currently being tracked via `AddObserver()`.
+  // * The `window` was being tracked and has since been destroyed.
+  // * The `window` is being tracked but was just added to the window tree and
+  //   its occlusion cannot be calculated yet. It should be available
+  //   imminently, at which time an `OnWindowOcclusionChanged()` notification
+  //   will fire.
+  aura::Window::OcclusionState GetOcclusionState(aura::Window* window) const;
+
+  // Starts tracking the occlusion state of all windows in
+  // `parent_windows_to_track` and their descendants. The `observer' is notified
+  // afterwards if the occlusion state of any of the aforementioned windows
+  // changes. Each window in `parent_windows_to_track` is forced to be visible
+  // before calculating the occlusion of them and their descendants.
+  //
+  // Multiple observers may be registered for the same parent window. It is also
+  // OK if one of `parent_windows_to_track` is a descendant of a parent window
+  // that is already being tracked. No restrictions here.
+  void AddObserver(const aura::Window::Windows& parent_windows_to_track,
+                   Observer* observer);
+
+  // Removes `observer`; this is a no-op if `observer` has not been added.
+  void RemoveObserver(Observer* observer);
+
+ private:
+  class ObservationState;
+  class WindowOcclusionChangeBuilderImpl;
+
+  using WindowOcclusionMap =
+      base::flat_map<raw_ptr<aura::Window>, aura::Window::OcclusionState>;
+
+  // aura::WindowObserver:
+  void OnWindowDestroyed(aura::Window* window) override;
+
+  void SetOcclusionState(aura::Window* window,
+                         aura::Window::OcclusionState occlusion_state);
+  void TrackOcclusionChangesForAllDescendants(aura::Window* window);
+
+  // Holds the current occlusion state for all tracked windows. This includes
+  // parent windows being observed and their descendants.
+  //
+  // Should outlive `occlusion_tracker_` since `occlusion_tracker_` writes to
+  // this map whenever a window's occlusion state changes.
+  WindowOcclusionMap occlusion_map_;
+
+  aura::WindowOcclusionTracker occlusion_tracker_;
+
+  // Map from parent window to the observers that should be notified when the
+  // parent window's occlusion changes or any of its descendants' occlusion
+  // changes.
+  //
+  // Should be destroyed before the `occlusion_tracker_` since the
+  // `ObservationState` holds a raw pointer to the `occlusion_tracker_` via its
+  // `forced_visibility_` member.
+  base::flat_map<raw_ptr<aura::Window>, std::unique_ptr<ObservationState>>
+      occlusion_change_observers_;
+
+  // Only for monitoring when a tracked `aura::Window` gets destroyed. See
+  // `OnWindowDestroyed()` implementation.
+  base::ScopedMultiSourceObservation<aura::Window, aura::WindowObserver>
+      tracked_window_observations_{this};
+};
+
+}  // namespace ash
+
+#endif  // ASH_WM_DESKS_WINDOW_OCCLUSION_CALCULATOR_H_
