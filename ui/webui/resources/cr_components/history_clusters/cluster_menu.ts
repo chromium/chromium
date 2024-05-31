ee72d82ce7c225ea03a804ca35e4adb9d113822b
rbpotter <rbpotter@chromium.org>
2024-05-31 19:06:59 +0000

    History Clusters: Migrate to Lit, part 4
    In this CL migrating the cluster-menu element.
    
    Bug: 40943652
    Change-Id: I26f320113396e0facc028f1f0ed78f0a167ddfea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582871
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308812}

--- ui/webui/resources/cr_components/history_clusters/cluster_menu.ts
+++ ui/webui/resources/cr_components/history_clusters/cluster_menu.ts
@@ -2,18 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './history_clusters_shared_style.css.js';
 import '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
-import '//resources/cr_elements/cr_lazy_render/cr_lazy_render.js';
 
-import type {CrActionMenuElement} from '//resources/cr_elements/cr_action_menu/cr_action_menu.js';
-import type {CrLazyRenderElement} from '//resources/cr_elements/cr_lazy_render/cr_lazy_render.js';
-import {I18nMixin} from '//resources/cr_elements/i18n_mixin.js';
+import {I18nMixinLit} from '//resources/cr_elements/i18n_mixin_lit.js';
+import {assert} from '//resources/js/assert.js';
 import {loadTimeData} from '//resources/js/load_time_data.js';
-import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
-import {getTemplate} from './cluster_menu.html.js';
+import {getCss} from './cluster_menu.css.js';
+import {getHtml} from './cluster_menu.html.js';
 
 /**
  * @fileoverview This file provides a custom element displaying an action menu.
@@ -27,43 +25,44 @@ declare global {
   }
 }
 
-const ClusterMenuElementBase = I18nMixin(PolymerElement);
+const ClusterMenuElementBase = I18nMixinLit(CrLitElement);
 
-interface ClusterMenuElement {
+export interface ClusterMenuElement {
   $: {
-    actionMenu: CrLazyRenderElement<CrActionMenuElement>,
     actionMenuButton: HTMLElement,
   };
 }
 
-class ClusterMenuElement extends ClusterMenuElementBase {
+export class ClusterMenuElement extends ClusterMenuElementBase {
   static get is() {
     return 'cluster-menu';
   }
 
-  static get template() {
-    return getTemplate();
+  static override get styles() {
+    return getCss();
   }
 
-  static get properties() {
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
     return {
       /**
        * Usually this is true, but this can be false if deleting history is
        * prohibited by Enterprise policy.
        */
-      allowDeletingHistory_: {
-        type: Boolean,
-        value: () => loadTimeData.getBoolean('allowDeletingHistory'),
-      },
+      allowDeletingHistory_: {type: Boolean},
 
       /**
        * Whether the cluster is in the side panel.
        */
       inSidePanel_: {
         type: Boolean,
-        value: () => loadTimeData.getBoolean('inSidePanel'),
-        reflectToAttribute: true,
+        reflect: true,
       },
+
+      renderActionMenu_: {type: Boolean},
     };
   }
 
@@ -71,18 +70,27 @@ class ClusterMenuElement extends ClusterMenuElementBase {
   // Properties
   //============================================================================
 
-  private allowDeletingHistory_: boolean;
+  protected allowDeletingHistory_: boolean =
+      loadTimeData.getBoolean('allowDeletingHistory');
+  protected inSidePanel_: boolean = loadTimeData.getBoolean('inSidePanel');
+  protected renderActionMenu_: boolean = false;
 
   //============================================================================
   // Event handlers
   //============================================================================
 
-  private onActionMenuButtonClick_(event: Event) {
-    this.$.actionMenu.get().showAt(this.$.actionMenuButton);
+  protected async onActionMenuButtonClick_(event: Event) {
     event.preventDefault();  // Prevent default browser action (navigation).
+    if (!this.renderActionMenu_) {
+      this.renderActionMenu_ = true;
+      await this.updateComplete;
+    }
+    const menu = this.shadowRoot!.querySelector('cr-action-menu');
+    assert(menu);
+    menu.showAt(this.$.actionMenuButton);
   }
 
-  private onOpenAllButtonClick_(event: Event) {
+  protected onOpenAllButtonClick_(event: Event) {
     event.preventDefault();  // Prevent default browser action (navigation).
 
     this.dispatchEvent(new CustomEvent('open-all-visits', {
@@ -90,10 +98,10 @@ class ClusterMenuElement extends ClusterMenuElementBase {
       composed: true,
     }));
 
-    this.$.actionMenu.get().close();
+    this.closeActionMenu_();
   }
 
-  private onHideAllButtonClick_(event: Event) {
+  protected onHideAllButtonClick_(event: Event) {
     event.preventDefault();  // Prevent default browser action (navigation).
 
     this.dispatchEvent(new CustomEvent('hide-all-visits', {
@@ -101,10 +109,10 @@ class ClusterMenuElement extends ClusterMenuElementBase {
       composed: true,
     }));
 
-    this.$.actionMenu.get().close();
+    this.closeActionMenu_();
   }
 
-  private onRemoveAllButtonClick_(event: Event) {
+  protected onRemoveAllButtonClick_(event: Event) {
     event.preventDefault();  // Prevent default browser action (navigation).
 
     this.dispatchEvent(new CustomEvent('remove-all-visits', {
@@ -112,7 +120,17 @@ class ClusterMenuElement extends ClusterMenuElementBase {
       composed: true,
     }));
 
-    this.$.actionMenu.get().close();
+    this.closeActionMenu_();
+  }
+
+  //============================================================================
+  // Helper methods
+  //============================================================================
+
+  private closeActionMenu_() {
+    const menu = this.shadowRoot!.querySelector('cr-action-menu');
+    assert(menu);
+    menu.close();
   }
 }
 
