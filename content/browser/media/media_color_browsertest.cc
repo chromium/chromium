f3318307fd3001d81a4fcf7979b537bcffdf8e43
Yuki Shiino <yukishiino@chromium.org>
2024-05-31 00:16:14 +0000

    Revert "Video: Add Pixel color test for GBR encoded video"
    This reverts commit 603c86a8c1e71350dc596cb28e898e24e05a6560.
    
    Reason for revert: Suspicious about causing test failures at:
    https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-arm64-cast-receiver-rel/14811/overview
    MediaColorTest.GbrpAv1
    https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-arm64-cast-receiver-rel/14811/test-results?q=ExactID%3Aninja%3A%2F%2Fcontent%2Ftest%3Acontent_browsertests%2FMediaColorTest.GbrpAv1+VHash%3A480dfd54bffe054f
    
    Original change's description:
    > Video: Add Pixel color test for GBR encoded video
    >
    > Add the basic pixel color test for VP9/H264/AV1, and leave a TODO
    > for H265. After resolving the colorspace full range issue on macOS,
    > and validate HEVC 4:4:4 + GBR video on Windows is working as expected,
    > we can then enable the test.
    >
    > Bug: 343014700
    > Change-Id: Id5857e81db75ceb98966af3c82ff8dbce67bfb23
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584419
    > Reviewed-by: Dan Sanders <sandersd@chromium.org>
    > Commit-Queue: Sida Zhu <zhusida@bytedance.com>
    > Cr-Commit-Position: refs/heads/main@{#1308321}
    
    Bug: 343014700
    Change-Id: I5eaddc2c43e6d82d925231d53f2473b57ae9dd60
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586696
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308374}

--- content/browser/media/media_color_browsertest.cc
+++ content/browser/media/media_color_browsertest.cc
@@ -21,9 +21,8 @@ class MediaColorTest : public MediaBrowserTest {
     ASSERT_TRUE(embedded_test_server()->Start());
   }
 
-  void RunColorTest(const std::string& video_file,
-                    const std::string& html_path) {
-    GURL base_url = embedded_test_server()->GetURL(html_path);
+  void RunColorTest(const std::string& video_file) {
+    GURL base_url = embedded_test_server()->GetURL("/blackwhite.html");
 
     GURL::Replacements replacements;
     replacements.SetQueryStr(video_file);
@@ -32,12 +31,6 @@ class MediaColorTest : public MediaBrowserTest {
     std::string final_title = RunTest(test_url, media::kEndedTitle);
     EXPECT_EQ(media::kEndedTitle, final_title);
   }
-  void RunBlackWhiteTest(const std::string& video_file) {
-    RunColorTest(video_file, "/blackwhite.html");
-  }
-  void RunGBRPTest(const std::string& video_file) {
-    RunColorTest(video_file, "/gbrp.html");
-  }
   void SetUp() override {
     EnablePixelOutput();
     MediaBrowserTest::SetUp();
@@ -45,19 +38,11 @@ class MediaColorTest : public MediaBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv420pVp8) {
-  RunBlackWhiteTest("yuv420p.webm");
+  RunColorTest("yuv420p.webm");
 }
 
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv444pVp9) {
-  RunBlackWhiteTest("yuv444p.webm");
-}
-
-IN_PROC_BROWSER_TEST_F(MediaColorTest, GbrpVp9) {
-  RunGBRPTest("vp9.mp4");
-}
-
-IN_PROC_BROWSER_TEST_F(MediaColorTest, GbrpAv1) {
-  RunGBRPTest("av1.mp4");
+  RunColorTest("yuv444p.webm");
 }
 
 #if BUILDFLAG(USE_PROPRIETARY_CODECS)
@@ -70,7 +55,7 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, GbrpAv1) {
 #define MAYBE_Yuv420pH264 Yuv420pH264
 #endif
 IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pH264) {
-  RunBlackWhiteTest("yuv420p.mp4");
+  RunColorTest("yuv420p.mp4");
 }
 
 // This test fails on Android: http://crbug.com/647818
@@ -80,7 +65,7 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pH264) {
 #define MAYBE_Yuvj420pH264 Yuvj420pH264
 #endif
 IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuvj420pH264) {
-  RunBlackWhiteTest("yuvj420p.mp4");
+  RunColorTest("yuvj420p.mp4");
 }
 
 // This fails on ChromeOS: http://crbug.com/647400,
@@ -91,7 +76,7 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuvj420pH264) {
 #define MAYBE_Yuv420pRec709H264 Yuv420pRec709H264
 #endif
 IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pRec709H264) {
-  RunBlackWhiteTest("yuv420p_rec709.mp4");
+  RunColorTest("yuv420p_rec709.mp4");
 }
 
 // Android doesn't support 10bpc.
@@ -102,24 +87,17 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pRec709H264) {
 #define MAYBE_Yuv420pHighBitDepth Yuv420pHighBitDepth
 #endif
 IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pHighBitDepth) {
-  RunBlackWhiteTest("yuv420p_hi10p.mp4");
+  RunColorTest("yuv420p_hi10p.mp4");
 }
 
 // Android devices usually only support baseline, main and high.
 #if !BUILDFLAG(IS_ANDROID)
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv422pH264) {
-  RunBlackWhiteTest("yuv422p.mp4");
+  RunColorTest("yuv422p.mp4");
 }
 
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv444pH264) {
-  RunBlackWhiteTest("yuv444p.mp4");
-}
-
-// TODO(crbug.com/343014700): Add GbrpH265 test for H265 after resolving
-// color space full range issue on macOS, and validate HEVC 4:4:4 + GBR
-// video on Windows is working as expected.
-IN_PROC_BROWSER_TEST_F(MediaColorTest, GbrpH264) {
-  RunGBRPTest("h264.mp4");
+  RunColorTest("yuv444p.mp4");
 }
 #endif  // !BUILDFLAG(IS_ANDROID)
 #endif  // BUILDFLAG(USE_PROPRIETARY_CODECS)
