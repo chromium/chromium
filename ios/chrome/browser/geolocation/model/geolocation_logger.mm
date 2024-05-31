c11bcd0ba4cf936c5fb4c52f930be9ad98920672
Ali Juma <ajuma@chromium.org>
2024-05-31 15:58:09 +0000

    [iOS] Add metrics for geolocation permission
    The existing metric for geolocation permission had expired a couple
    years ago, and the logic in GeolocationLogger dates back to when
    Chrome would proactively prompt the user for location permission.
    
    This CL simplifies the logic in GeolocationLogger, removes the old
    broken metric, and adds two new metrics, one that logs the permission
    state on startup, and another that logs changes in permission state.
    
    OBSOLETE_HISTOGRAM[Geolocation.AuthorizationActionExistingUser]=Replaced by Geolocation.IOS.InitialAuthorizationState and Geolocation.IOS.ChangedAuthorizationState
    
    Bug: 339078943
    Change-Id: I3d8a6cc613ee7e499ebc9496269cc9e763ddfce1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570964
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308673}

--- ios/chrome/browser/geolocation/model/geolocation_logger.mm
+++ ios/chrome/browser/geolocation/model/geolocation_logger.mm
@@ -7,53 +7,61 @@
 #import <CoreLocation/CoreLocation.h>
 
 #import "base/metrics/histogram_macros.h"
-#import "components/google/core/common/google_util.h"
-#import "ios/chrome/app/tests_hook.h"
-#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
-#import "ios/web/public/navigation/navigation_item.h"
-#import "ios/web/public/navigation/navigation_manager.h"
-#import "ios/web/public/web_state.h"
 
 namespace {
 
-// Values for the histograms that record the user's action when prompted to
-// authorize the use of location by Chrome. These match the definition of
-// GeolocationAuthorizationAction in Chromium
-// src-internal/tools/histograms/histograms.xml.
-typedef enum {
-  // The user authorized use of location.
-  kAuthorizationActionAuthorized = 0,
-  // The user permanently denied use of location (Don't Allow).
-  kAuthorizationActionPermanentlyDenied,
-  // The user denied use of location at this prompt (Not Now).
-  kAuthorizationActionDenied,
-  // The number of possible AuthorizationAction values to report.
-  kAuthorizationActionCount,
-} AuthorizationAction;
-
-// Name of the histogram recording AuthorizationAction for an existing user.
-const char* const kGeolocationAuthorizationActionExistingUser =
-    "Geolocation.AuthorizationActionExistingUser";
-
-enum class PermissionStatus {
-  // Status unknown, usually because the system is slow to respond.
-  kPermissionUnknown = 0,
-  // User has not made a permission choice.
-  kPermissionNotDetermined = 1,
-  // User has made a permission choice, be it allowed, denied, etc.
-  kPermissionDetermined = 2,
-  kMaxValue = kPermissionDetermined,
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+//
+// LINT.IfChange(AuthorizationStatus)
+enum class AuthorizationStatus {
+  // The user has not chosen whether to allow location access.
+  kNotDetermined = 0,
+  // The user cannot allow location access.
+  kRestricted = 1,
+  // The user denied location access either for the app or globally.
+  kDenied = 2,
+  // The user granted location access at all times.
+  kAuthorizedAlways = 3,
+  // The user granted location access only while the app is in use.
+  kAuthorizedWhenInUse = 4,
+  kMaxValue = kAuthorizedWhenInUse
 };
+// LINT.ThenChange(/tools/metrics/histograms/metadata/geolocation/enums.xml)
+
+// Name of the histogram recording initial geolocation authorization state.
+constexpr char kGeolocationInitialAuthorizationStateHistogram[] =
+    "Geolocation.IOS.InitialAuthorizationState";
+
+// Name of the histogram recording a change in geolocation authorization state.
+constexpr char kGeolocationAuthorizationStateChangedHistogram[] =
+    "Geolocation.IOS.ChangedAuthorizationState";
+
+AuthorizationStatus ToAuthorizationStatus(
+    CLAuthorizationStatus authorization_status) {
+  switch (authorization_status) {
+    case kCLAuthorizationStatusNotDetermined:
+      return AuthorizationStatus::kNotDetermined;
+    case kCLAuthorizationStatusRestricted:
+      return AuthorizationStatus::kRestricted;
+    case kCLAuthorizationStatusDenied:
+      return AuthorizationStatus::kDenied;
+    case kCLAuthorizationStatusAuthorizedAlways:
+      return AuthorizationStatus::kAuthorizedAlways;
+    case kCLAuthorizationStatusAuthorizedWhenInUse:
+      return AuthorizationStatus::kAuthorizedWhenInUse;
+    default:
+      // Since CLAuthorizationStatus is an iOS-provided enum, safely handle new
+      // values by falling back to `kNotDetermined`.
+      return AuthorizationStatus::kNotDetermined;
+  }
+}
 
 }  // anonymous namespace
 
 @interface GeolocationLogger () <CLLocationManagerDelegate>
 
 @property(nonatomic, strong) CLLocationManager* locationManager;
-
-// Whether the permission was undefined or not. Used to choose whether to log
-// the permission or not.
-@property(atomic, assign) PermissionStatus permissionStatus;
 
 @end
 
@@ -69,61 +77,28 @@ enum class PermissionStatus {
   if (self) {
     _locationManager = [[CLLocationManager alloc] init];
     [_locationManager setDelegate:self];
-    dispatch_queue_t priorityQueue =
-        dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0ul);
-    _permissionStatus = PermissionStatus::kPermissionUnknown;
-    dispatch_async(priorityQueue, ^{
-      self.permissionStatus = self.locationManager.authorizationStatus ==
-                                      kCLAuthorizationStatusNotDetermined
-                                  ? PermissionStatus::kPermissionNotDetermined
-                                  : PermissionStatus::kPermissionDetermined;
-    });
   }
   return self;
-}
-
-#pragma mark - Private
-
-- (void)recordAuthorizationAction:(AuthorizationAction)authorizationAction {
-  self.permissionStatus = PermissionStatus::kPermissionDetermined;
-  UMA_HISTOGRAM_ENUMERATION(kGeolocationAuthorizationActionExistingUser,
-                            authorizationAction, kAuthorizationActionCount);
-}
-
-// Boolean value indicating whether location services are enabled on the
-// device.
-- (BOOL)locationServicesEnabled {
-  return !tests_hook::DisableGeolocation() &&
-         [CLLocationManager locationServicesEnabled];
 }
 
 #pragma mark - CLLocationManagerDelegate
 
 - (void)locationManagerDidChangeAuthorization:
     (CLLocationManager*)locationManager {
-  if (self.permissionStatus == PermissionStatus::kPermissionUnknown)
+  // The initial call to this method represents the initial value of geolocation
+  // authorization status rather than a change.
+  static BOOL initialCall = YES;
+  if (initialCall) {
+    initialCall = NO;
+    UMA_HISTOGRAM_ENUMERATION(
+        kGeolocationInitialAuthorizationStateHistogram,
+        ToAuthorizationStatus(self.locationManager.authorizationStatus));
     return;
-
-  if (self.permissionStatus == PermissionStatus::kPermissionNotDetermined) {
-    switch (locationManager.authorizationStatus) {
-      case kCLAuthorizationStatusNotDetermined:
-        // We may get a spurious notification about a transition to
-        // `kCLAuthorizationStatusNotDetermined` when we first start location
-        // services. Ignore it and don't reset `systemPrompt_` until we get a
-        // real change.
-        break;
-
-      case kCLAuthorizationStatusRestricted:
-      case kCLAuthorizationStatusDenied:
-        [self recordAuthorizationAction:kAuthorizationActionPermanentlyDenied];
-        break;
-
-      case kCLAuthorizationStatusAuthorizedAlways:
-      case kCLAuthorizationStatusAuthorizedWhenInUse:
-        [self recordAuthorizationAction:kAuthorizationActionAuthorized];
-        break;
-    }
   }
+
+  UMA_HISTOGRAM_ENUMERATION(
+      kGeolocationAuthorizationStateChangedHistogram,
+      ToAuthorizationStatus(self.locationManager.authorizationStatus));
 }
 
 @end
