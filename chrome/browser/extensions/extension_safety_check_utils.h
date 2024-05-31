95b12b2d17d8f5c9257a630950e809d1adce8b6d
Adam Psarouthakis <psarouthakis@google.com>
2024-05-31 22:07:14 +0000

    Extension Safety Hub Refactor
    The Extension Safety Hub has two main classes that require information
    on extensions that trigger a review. Until now code has been duplicated
    across these two touchpoints and owned by two different teams. This CL
    creates a class that can be used by both touch points, reducing the
    amount of code duplication, and consolidating the owners of the
    Extensions Safety Hub.
    
    Bug: 335639105
    Change-Id: Id0d09ba3f28578934c4c7783a460499d1659906e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545709
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Adam Psarouthakis <psarouthakis@google.com>
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308926}

---
+++ chrome/browser/extensions/extension_safety_check_utils.h
@@ -0,0 +1,42 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_EXTENSION_SAFETY_CHECK_UTILS_H_
+#define CHROME_BROWSER_EXTENSIONS_EXTENSION_SAFETY_CHECK_UTILS_H_
+
+#include "base/gtest_prod_util.h"
+#include "chrome/common/extensions/api/developer_private.h"
+#include "extensions/browser/blocklist_state.h"
+
+class Profile;
+
+namespace extensions {
+
+class CWSInfoServiceInterface;
+class Extension;
+
+// These functions are used as a utility functions for the Extension
+// Safety Check.
+namespace ExtensionSafetyCheckUtils {
+// Returns the Safety Hub warning reason for an extension.
+api::developer_private::SafetyCheckWarningReason GetSafetyCheckWarningReason(
+    const Extension& extension,
+    Profile* profile);
+
+// A helper function to `GetSafetyCheckWarningReason` to simplify testing.
+api::developer_private::SafetyCheckWarningReason
+GetSafetyCheckWarningReasonHelper(CWSInfoServiceInterface* cws_info_service,
+                                  BitMapBlocklistState blocklist_state,
+                                  Profile* profile,
+                                  const Extension& extension);
+
+// Returns the display strings related to a Safety Hub Warning reason.
+api::developer_private::SafetyCheckStrings GetSafetyCheckWarningStrings(
+    api::developer_private::SafetyCheckWarningReason warning_reason,
+    api::developer_private::ExtensionState state);
+
+}  // namespace ExtensionSafetyCheckUtils
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_SAFETY_CHECK_UTILS_H_
