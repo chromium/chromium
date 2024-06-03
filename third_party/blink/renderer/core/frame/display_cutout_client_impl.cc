8da040ad0865e003d29a97c5a306cc133cc32c2d
Wenyu Fu <wenyufu@chromium.org>
2024-06-03 16:45:02 +0000

    [E2E] Dynamic safe-area-inset* as browser control scrolls
    Dynamically change safe-area-inset* css attribute as browser control scrolls.
    
    This CL implements that the safe-area-inset-bottom will increase as the    browser control scrolls below the display cutout area, keeping the bottom attached item remains in the touchable area. When browser controls scroll back to the screen, the safe-area-inset* will decrease as the viewport is taken over by the browser control.
    
    Recording and more design details please see the attached bug.
    
    Bug: 324436581
    Change-Id: I039da021d150939513f3da1465b8f88fe95f4842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5543639
    Reviewed-by: Charles Hager <clhager@google.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309385}

--- third_party/blink/renderer/core/frame/display_cutout_client_impl.cc
+++ third_party/blink/renderer/core/frame/display_cutout_client_impl.cc
@@ -7,26 +7,84 @@
 #include "third_party/blink/renderer/core/css/document_style_environment_variables.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
+#include "third_party/blink/renderer/core/frame/visual_viewport.h"
+#include "third_party/blink/renderer/core/page/page.h"
+#include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace blink {
 
-DisplayCutoutClientImpl::DisplayCutoutClientImpl(
-    LocalFrame* frame,
-    mojo::PendingAssociatedReceiver<mojom::blink::DisplayCutoutClient> receiver)
-    : frame_(frame), receiver_(this, std::move(receiver)) {}
+// static
+const char DisplayCutoutClientImpl::kSupplementName[] =
+    "DisplayCutoutClientImpl";
 
+DisplayCutoutClientImpl::DisplayCutoutClientImpl(
+    LocalFrame& frame,
+    mojo::PendingAssociatedReceiver<mojom::blink::DisplayCutoutClient> receiver)
+    : Supplement<LocalFrame>(frame), receiver_(this, std::move(receiver)) {}
+
+// static
 void DisplayCutoutClientImpl::BindMojoReceiver(
     LocalFrame* frame,
     mojo::PendingAssociatedReceiver<mojom::blink::DisplayCutoutClient>
         receiver) {
   if (!frame)
     return;
-  MakeGarbageCollected<DisplayCutoutClientImpl>(frame, std::move(receiver));
+  frame->ProvideSupplement(MakeGarbageCollected<DisplayCutoutClientImpl>(
+      *frame, std::move(receiver)));
+}
+
+// static
+DisplayCutoutClientImpl* DisplayCutoutClientImpl::From(LocalFrame* frame) {
+  return Supplement<LocalFrame>::From<DisplayCutoutClientImpl>(frame);
+}
+
+void DisplayCutoutClientImpl::UpdateSafeAreaInsetWithBrowserControls(
+    Document* document,
+    const BrowserControls& browser_controls,
+    bool force_update) {
+  if (!RuntimeEnabledFeatures::DynamicSafeAreaInsetsEnabled()) {
+    return;
+  }
+
+  DCHECK(document);
+
+  // Adjust the top / left / right is not needed, since they are set when
+  // display insets was received at |SetSafeArea()|.
+  int inset_bottom = safe_area_insets_.bottom();
+  int bottom_controls_full_height = browser_controls.BottomHeight();
+  float control_ratio = browser_controls.BottomShownRatio();
+  float dip_scale = document->GetPage()->GetVisualViewport().ScaleFromDIP();
+
+  // As control_ratio decrease, safe_area_inset_bottom will be added to the web
+  // page to keep the bottom element out from the display cutout area.
+  float safe_area_inset_bottom =
+      std::max(0.f, inset_bottom - control_ratio * bottom_controls_full_height /
+                                       dip_scale);
+  if (force_update ||
+      safe_area_inset_bottom != last_set_safe_are_bottom_insets_) {
+    last_set_safe_are_bottom_insets_ = safe_area_inset_bottom;
+    DocumentStyleEnvironmentVariables& vars =
+        document->GetStyleEngine().EnsureEnvironmentVariables();
+    vars.SetVariable(
+        UADefinedVariable::kSafeAreaInsetBottom,
+        StyleEnvironmentVariables::FormatPx(safe_area_inset_bottom));
+  }
 }
 
 void DisplayCutoutClientImpl::SetSafeArea(const gfx::Insets& safe_area) {
-  DocumentStyleEnvironmentVariables& vars =
-      frame_->GetDocument()->GetStyleEngine().EnsureEnvironmentVariables();
+  if (safe_area_insets_ == safe_area) {
+    return;
+  }
+
+  safe_area_insets_ = safe_area;
+  last_set_safe_are_bottom_insets_ = safe_area_insets_.bottom();
+
+  // Apply the safe area only if the insets has changed. Safe area can be
+  // override by browser controls positions in the page.
+  DocumentStyleEnvironmentVariables& vars = GetSupplementable()
+                                                ->GetDocument()
+                                                ->GetStyleEngine()
+                                                .EnsureEnvironmentVariables();
   vars.SetVariable(UADefinedVariable::kSafeAreaInsetTop,
                    StyleEnvironmentVariables::FormatPx(safe_area.top()));
   vars.SetVariable(UADefinedVariable::kSafeAreaInsetLeft,
@@ -35,10 +93,6 @@ void DisplayCutoutClientImpl::SetSafeArea(const gfx::Insets& safe_area) {
                    StyleEnvironmentVariables::FormatPx(safe_area.bottom()));
   vars.SetVariable(UADefinedVariable::kSafeAreaInsetRight,
                    StyleEnvironmentVariables::FormatPx(safe_area.right()));
-}
-
-void DisplayCutoutClientImpl::Trace(Visitor* visitor) const {
-  visitor->Trace(frame_);
 }
 
 }  // namespace blink
