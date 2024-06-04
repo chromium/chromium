a575657e0fe7624d0a2ec87bc5005c67033af9a6
Elly <ellyjones@chromium.org>
2024-06-04 21:28:24 +0000

    views: enable unsafe buffer checking
    This change enables -Wunsafe-buffers across views by marking every file
    with violations using the unsafe buffers pragma. Followup changes will
    remove all the instances of this pragma and leave all of Views protected
    by -Wunsafe-buffers.
    
    AX-Relnotes: n/a
    Bug: 344639839
    Change-Id: Icc89c9d1900985ef2609b5a2f1685781cc93400b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595116
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310204}

--- ui/views/color_chooser/color_chooser_view.h
+++ ui/views/color_chooser/color_chooser_view.h
@@ -2,6 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#if defined(UNSAFE_BUFFERS_BUILD)
+// TODO(https://crbug.com/344639839): fix the unsafe buffer errors in this file,
+// then remove this pragma.
+#pragma allow_unsafe_buffers
+#endif
+
 #ifndef UI_VIEWS_COLOR_CHOOSER_COLOR_CHOOSER_VIEW_H_
 #define UI_VIEWS_COLOR_CHOOSER_COLOR_CHOOSER_VIEW_H_
 
--- ui/views/examples/flex_layout_example.cc
+++ ui/views/examples/flex_layout_example.cc
@@ -2,6 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#if defined(UNSAFE_BUFFERS_BUILD)
+// TODO(https://crbug.com/344639839): fix the unsafe buffer errors in this file,
+// then remove this pragma.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "ui/views/examples/flex_layout_example.h"
 
 #include <memory>
