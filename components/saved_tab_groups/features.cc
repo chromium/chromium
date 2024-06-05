641dd198f28232ee56369814521dee9750276e9c
Shakti Sahu <shaktisahu@chromium.org>
2024-06-05 16:32:02 +0000

    [Tab Group Sync] Enable Java shared prefs migration
    This CL enables the Java shared prefs migration that was
    introduced in
    https://chromium-review.googlesource.com/c/chromium/src/+/5583203
    
    Bug: None
    Change-Id: Iaff2590b49e84354c86ad6bba5a9fae7d2b826b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597931
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310673}

--- components/saved_tab_groups/features.cc
+++ components/saved_tab_groups/features.cc
@@ -44,7 +44,7 @@ BASE_FEATURE(kTabGroupSyncUno,
 // ModelTypeStore.
 BASE_FEATURE(kMigrationFromJavaSharedPrefs,
              "MigrationFromJavaSharedPrefs",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsTabGroupsSaveV2Enabled() {
   return base::FeatureList::IsEnabled(kTabGroupsSaveV2);
