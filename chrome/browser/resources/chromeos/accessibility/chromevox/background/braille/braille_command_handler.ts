5689f2f69d97892121c863bed3b21dde3eca41f9
Anastasia Helfinstein <anastasi@google.com>
2024-06-05 19:30:29 +0000

    [ChromeVox] Convert BrailleCommandHandler to TypeScript
    AX-Relnotes: n/a.
    Bug: b/267329383
    Change-Id: I07fcf193dc92696cee64fe858884c981fae2e7fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5558694
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310816}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/braille/braille_command_handler.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/braille/braille_command_handler.ts
@@ -14,7 +14,6 @@ import {TestImportManager} from '/common/testing/test_import_manager.js';
 import {BrailleCommandData} from '../../common/braille/braille_command_data.js';
 import {BrailleKeyCommand, BrailleKeyEvent} from '../../common/braille/braille_key_types.js';
 import {NavBraille} from '../../common/braille/nav_braille.js';
-import {BridgeConstants} from '../../common/bridge_constants.js';
 import {Command} from '../../common/command.js';
 import {EventSourceType} from '../../common/event_source_type.js';
 import {Spannable} from '../../common/spannable.js';
@@ -27,17 +26,19 @@ import {Output} from '../output/output.js';
 import {OutputNodeSpan, OutputSelectionSpan} from '../output/output_types.js';
 import {ChromeVoxPrefs} from '../prefs.js';
 
+const IntentCommandType = chrome.automation.IntentCommandType;
+const IntentMoveDirectionType = chrome.automation.IntentMoveDirectionType;
+const IntentTextBoundaryType = chrome.automation.IntentTextBoundaryType;
 const RoleType = chrome.automation.RoleType;
 const StateType = chrome.automation.StateType;
 
 export class BrailleCommandHandler {
-  /** @private */
-  constructor() {
-    /** @private {boolean} */
-    this.bypassed_ = false;
-  }
+  static instance: BrailleCommandHandler;
+  private bypassed_ = false;
 
-  static init() {
+  private constructor() {}
+
+  static init(): void {
     if (BrailleCommandHandler.instance) {
       throw new Error(
           'BrailleCommandHandler cannot be instantiated more than once.');
@@ -50,19 +51,16 @@ export class BrailleCommandHandler {
    *
    * Used by LearnMode to capture the events and prevent the standard behavior,
    * in favor of reporting what that behavior would be.
-   * @param {boolean} state
    */
-  static setBypass(state) {
+  static setBypass(state: boolean): void {
     BrailleCommandHandler.instance.bypassed_ = state;
   }
 
   /**
    * Handles a braille command.
-   * @param {!BrailleKeyEvent} evt
-   * @param {!NavBraille} content
-   * @return {boolean} True if evt was processed.
+   * @return True if evt was processed.
    */
-  static onBrailleKeyEvent(evt, content) {
+  static onBrailleKeyEvent(evt: BrailleKeyEvent, content: NavBraille): boolean {
     if (BrailleCommandHandler.instance.bypassed_) {
       // Prevent any handling of the event when bypassed.
       return true;
@@ -95,16 +93,18 @@ export class BrailleCommandHandler {
         CommandHandlerInterface.instance.onCommand(Command.JUMP_TO_BOTTOM);
         break;
       case BrailleKeyCommand.ROUTING:
+        // TODO(b/314203187): Not null asserted, check that this is correct.
         const textEditHandler =
-            DesktopAutomationInterface.instance.textEditHandler;
+            DesktopAutomationInterface.instance!.textEditHandler;
         textEditHandler?.injectInferredIntents([{
-          command: chrome.automation.IntentCommandType.MOVE_SELECTION,
-          textBoundary: chrome.automation.IntentTextBoundaryType.CHARACTER,
+          command: IntentCommandType.MOVE_SELECTION,
+          textBoundary: IntentTextBoundaryType.CHARACTER,
+          moveDirection: IntentMoveDirectionType.FORWARD,
         }]);
         BrailleCommandHandler.onRoutingCommand_(
             content.text,
             // Cast ok since displayPosition is always defined in this case.
-            /** @type {number} */ (evt.displayPosition));
+            evt.displayPosition as number);
         break;
       case BrailleKeyCommand.CHORD:
         if (!evt.brailleDots) {
@@ -124,12 +124,7 @@ export class BrailleCommandHandler {
     return true;
   }
 
-  /**
-   * @param {!Spannable} text
-   * @param {number} position
-   * @private
-   */
-  static onRoutingCommand_(text, position) {
+  private static onRoutingCommand_(text: Spannable, position: number): void {
     let actionNodeSpan = null;
     let selectionSpan = null;
     const selSpans = text.getSpansInstanceOf(OutputSelectionSpan);
@@ -192,19 +187,19 @@ export class BrailleCommandHandler {
   /**
    * Customizes ChromeVox commands when issued from a braille display while
    * within editable text.
-   * @param {string} command
-   * @return {boolean} True if the command should propagate.
-   * @private
+   * @return True if the command should propagate.
    */
-  static onEditCommand_(command) {
+  private static onEditCommand_(command: string): boolean {
     const current = ChromeVoxRange.current;
-    if (ChromeVoxPrefs.isStickyModeOn() ||
-        !current?.start?.node?.state[StateType.EDITABLE]) {
+    const state = current?.start?.node?.state ?? {};
+    if (ChromeVoxPrefs.isStickyModeOn() || !state[StateType.EDITABLE]) {
       return true;
     }
 
-    const textEditHandler = DesktopAutomationInterface.instance.textEditHandler;
-    const editable = AutomationUtil.getEditableRoot(current.start.node);
+    // TODO(b/314203187): Not null asserted, check that this is correct.
+    const textEditHandler =
+        DesktopAutomationInterface.instance!.textEditHandler!;
+    const editable = AutomationUtil.getEditableRoot(current!.start.node);
     if (!editable || editable !== textEditHandler?.node) {
       return true;
     }
@@ -258,8 +253,5 @@ export class BrailleCommandHandler {
     return false;
   }
 }
-
-/** @type {BrailleCommandHandler} */
-BrailleCommandHandler.instance;
 
 TestImportManager.exportForTesting(BrailleCommandHandler);
