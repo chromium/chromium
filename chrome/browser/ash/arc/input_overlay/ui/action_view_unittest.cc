ed0f07e1458072715dbf9e98a54d4ec44e031c06
pjlee <pjlee@google.com>
2024-06-05 18:26:15 +0000

    arc/gio: Migrate action view with game dashboard flag
    This CL migrates the action view test to use the game dashboard flag by
    changing the base test class from `ViewTestBase` to
    `OverlayViewTestBase`. Additionally, remove key binding change tests
    and tweak various vector values to accommodate new window size.
    
    Bug: b/286152619
    Test: ./out/Default/unit_tests --gtest_filter=*ActionViewTest*
    Change-Id: I4d6d0688b50d2c950f32bf7a933df93d6bee72f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597400
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Commit-Queue: Joseph Pang <pjlee@google.com>
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310766}

--- chrome/browser/ash/arc/input_overlay/ui/action_view_unittest.cc
+++ chrome/browser/ash/arc/input_overlay/ui/action_view_unittest.cc
@@ -7,7 +7,7 @@
 #include "chrome/browser/ash/arc/input_overlay/actions/action.h"
 #include "chrome/browser/ash/arc/input_overlay/constants.h"
 #include "chrome/browser/ash/arc/input_overlay/display_overlay_controller.h"
-#include "chrome/browser/ash/arc/input_overlay/test/view_test_base.h"
+#include "chrome/browser/ash/arc/input_overlay/test/overlay_view_test_base.h"
 #include "chrome/browser/ash/arc/input_overlay/touch_injector.h"
 #include "chrome/browser/ash/arc/input_overlay/ui/action_label.h"
 #include "chrome/browser/ash/arc/input_overlay/ui/input_mapping_view.h"
@@ -62,14 +62,10 @@ void CheckActionTapLabelPosition(TapLabelPosition label_position,
 
 }  // namespace
 
-class ActionViewTest : public ViewTestBase {
+class ActionViewTest : public OverlayViewTestBase {
  public:
   ActionViewTest() = default;
-
-  void SetRepositionController() {
-    move_action_view_->SetRepositionController();
-    tap_action_view_->SetRepositionController();
-  }
+  ~ActionViewTest() override = default;
 
  protected:
   void PressLeftMouseAtActionView(ActionView* action_view) {
@@ -143,21 +139,19 @@ class ActionViewTest : public ViewTestBase {
     return input_mapping_view_->GetIndexOf(view);
   }
 
- private:
-  void SetUp() override {
-    ViewTestBase::SetUp();
-    SetRepositionController();
-  }
+  gfx::Point local_location_;
+  gfx::Point root_location_;
 };
 
 TEST_F(ActionViewTest, TestDragMoveActionMove) {
   SetDisplayMode(DisplayMode::kEdit);
+  auto* move_action_view = move_action_->action_view();
   // Drag move by mouse.
   auto updated_pos = move_action_->touch_down_positions()[0];
-  PressLeftMouseAtActionView(move_action_view_);
+  PressLeftMouseAtActionView(move_action_view);
   auto origin_mouse_pos = root_location_;
-  MouseDragActionViewBy(move_action_view_, gfx::Vector2d(50, 60));
-  ReleaseLeftMouse(move_action_view_);
+  MouseDragActionViewBy(move_action_view, gfx::Vector2d(10, 10));
+  ReleaseLeftMouse(move_action_view);
   // Save the change.
   move_action_->BindPending();
   auto mouse_moved = root_location_ - origin_mouse_pos;
@@ -168,10 +162,10 @@ TEST_F(ActionViewTest, TestDragMoveActionMove) {
 
   // Drag move by touch.
   updated_pos = move_action_->touch_down_positions()[0];
-  TouchPressAtActionView(move_action_view_);
+  TouchPressAtActionView(move_action_view);
   auto origin_touch_pos = root_location_;
-  TouchMoveAtActionViewBy(move_action_view_, gfx::Vector2d(-10, -15));
-  TouchReleaseAtActionView(move_action_view_);
+  TouchMoveAtActionViewBy(move_action_view, gfx::Vector2d(-10, -15));
+  TouchReleaseAtActionView(move_action_view);
   // Save the change.
   move_action_->BindPending();
   auto touch_moved = root_location_ - origin_touch_pos;
@@ -181,55 +175,19 @@ TEST_F(ActionViewTest, TestDragMoveActionMove) {
                      kTolerance);
 }
 
-TEST_F(ActionViewTest, TestDisplayModeChange) {
-  // In view mode.
-  auto* label = tap_action_view_->labels()[0].get();
-  EXPECT_EQ(label->size(), tap_action_view_->size());
-  const auto expected_touch_center = tap_action_view_->GetTouchCenterInWindow();
-  // In edit mode.
-  SetDisplayMode(DisplayMode::kEdit);
-  auto* touch_point = tap_action_view_->touch_point();
-  EXPECT_TRUE(touch_point);
-  auto touch_point_in_window = touch_point->bounds().CenterPoint();
-  touch_point_in_window.Offset(tap_action_view_->origin().x(),
-                               tap_action_view_->origin().y());
-  EXPECT_EQ(expected_touch_center, touch_point_in_window);
-  auto tap_view_bounds = tap_action_view_->bounds();
-  EXPECT_TRUE(tap_view_bounds.Contains(touch_point_in_window));
-  // Change key binding.
-  ui::KeyEvent event_a(ui::ET_KEY_PRESSED, ui::VKEY_A, 0);
-  label->OnKeyPressed(event_a);
-  EXPECT_EQ(tap_view_bounds, tap_action_view_->bounds());
-  touch_point_in_window = touch_point->bounds().CenterPoint();
-  touch_point_in_window.Offset(tap_action_view_->origin().x(),
-                               tap_action_view_->origin().y());
-  EXPECT_EQ(expected_touch_center, touch_point_in_window);
-  tap_view_bounds = tap_action_view_->bounds();
-  EXPECT_TRUE(tap_view_bounds.Contains(touch_point_in_window));
-  auto label_position = label->origin();
-  label_position.Offset(tap_action_view_->origin().x(),
-                        tap_action_view_->origin().y());
-  tap_action_->BindPending();
-  // In view mode.
-  SetDisplayMode(DisplayMode::kView);
-  auto label_position_new = label->origin();
-  label_position_new.Offset(tap_action_view_->origin().x(),
-                            tap_action_view_->origin().y());
-  EXPECT_EQ(label_position, label_position_new);
-}
-
 TEST_F(ActionViewTest, TestDragMoveActionTap) {
   SetDisplayMode(DisplayMode::kEdit);
-  const auto* touch_point = tap_action_view_->touch_point();
-  const auto* label = tap_action_view_->labels()[0].get();
+  auto* tap_action_view = tap_action_->action_view();
+  const auto* touch_point = tap_action_view->touch_point();
+  const auto* label = tap_action_view->labels()[0].get();
   // Check initial position.
   CheckActionTapLabelPosition(TapLabelPosition::kTopLeft, touch_point, label);
   // Drag move by mouse.
   auto updated_pos = tap_action_->touch_down_positions()[0];
-  PressLeftMouseAtActionView(tap_action_view_);
+  PressLeftMouseAtActionView(tap_action_view);
   auto origin_mouse_pos = root_location_;
-  MouseDragActionViewBy(tap_action_view_, gfx::Vector2d(-10, 0));
-  ReleaseLeftMouse(tap_action_view_);
+  MouseDragActionViewBy(tap_action_view, gfx::Vector2d(-10, 0));
+  ReleaseLeftMouse(tap_action_view);
   // Save the change.
   tap_action_->BindPending();
   auto mouse_moved = root_location_ - origin_mouse_pos;
@@ -241,10 +199,10 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
 
   // Drag move by touch.
   updated_pos = tap_action_->touch_down_positions()[0];
-  TouchPressAtActionView(tap_action_view_);
+  TouchPressAtActionView(tap_action_view);
   auto origin_touch_pos = root_location_;
-  TouchMoveAtActionViewBy(tap_action_view_, gfx::Vector2d(20, 0));
-  TouchReleaseAtActionView(tap_action_view_);
+  TouchMoveAtActionViewBy(tap_action_view, gfx::Vector2d(20, 0));
+  TouchReleaseAtActionView(tap_action_view);
   // Save the change.
   tap_action_->BindPending();
   auto touch_moved = root_location_ - origin_touch_pos;
@@ -256,7 +214,7 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
 
   // The label position has different label offset positions depending on the
   // current position.
-  // Mouse drag to the right edge. T represents the `tap_action_view_` position.
+  // Mouse drag to the right edge. T represents the `tap_action_view` position.
   // From
   //  |----------------|
   //  |                |
@@ -271,14 +229,14 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |  |          | T|
   //  |  |          |  |
   //  |__|__________|__|
-  const auto& available_size = tap_action_view_->parent()->size();
-  PressLeftMouseAtActionView(tap_action_view_);
+  const auto& available_size = tap_action_view->parent()->size();
+  PressLeftMouseAtActionView(tap_action_view);
   auto touch_point_in_window = touch_point->origin();
-  touch_point_in_window.Offset(tap_action_view_->origin().x(),
-                               tap_action_view_->origin().y());
-  MouseDragActionViewBy(tap_action_view_,
+  touch_point_in_window.Offset(tap_action_view->origin().x(),
+                               tap_action_view->origin().y());
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(available_size.width(), 0));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kTopLeft, touch_point, label);
 
   // Mouse drag to the left edge.
@@ -288,10 +246,10 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |T |          |  |
   //  |  |          |  |
   //  |__|__________|__|
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_,
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(-available_size.width(), 0));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kTopRight, touch_point, label);
 
   // Mouse drag to the middle-left.
@@ -301,10 +259,10 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |  |  T       |  |
   //  |  |          |  |
   //  |__|__________|__|
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_,
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(available_size.width() / 3, 0));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kTopLeft, touch_point, label);
 
   // Mouse drag to the middle-right.
@@ -314,10 +272,10 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |  |       T  |  |
   //  |  |          |  |
   //  |__|__________|__|
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_,
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(available_size.width() / 3, 0));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kTopRight, touch_point, label);
 
   // Mouse drag to the top edge.
@@ -327,10 +285,10 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |  |          |  |
   //  |  |          |  |
   //  |__|__________|__|
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_,
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(0, -available_size.height()));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kBottomRight, touch_point,
                               label);
 
@@ -341,10 +299,10 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |  |          |  |
   //  |  |          |  |
   //  |__|__________|__|
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_,
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(available_size.width(), 0));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kBottomLeft, touch_point,
                               label);
 
@@ -355,10 +313,10 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |  |          |  |
   //  |  |          |  |
   //  |__|__________|__|
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_,
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(-2 * available_size.width() / 3, 0));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kBottomLeft, touch_point,
                               label);
 
@@ -369,10 +327,10 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |  |          |  |
   //  |  |          |  |
   //  |__|__________|__|
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_,
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(-available_size.width(), 0));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kBottomRight, touch_point,
                               label);
 
@@ -383,20 +341,21 @@ TEST_F(ActionViewTest, TestDragMoveActionTap) {
   //  |  |          |  |
   //  |  |          |  |
   //  |T_|__________|__|
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_,
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view,
                         gfx::Vector2d(0, available_size.height()));
-  ReleaseLeftMouse(tap_action_view_);
+  ReleaseLeftMouse(tap_action_view);
   CheckActionTapLabelPosition(TapLabelPosition::kTopRight, touch_point, label);
 }
 
 TEST_F(ActionViewTest, TestArrowKeyMove) {
   SetDisplayMode(DisplayMode::kEdit);
+  auto* move_action_view = move_action_->action_view();
   // Arrow key left single press & release.
   auto updated_pos = move_action_->touch_down_positions()[0];
-  move_action_view_->touch_point()->OnKeyPressed(
+  move_action_view->touch_point()->OnKeyPressed(
       ui::KeyEvent(ui::ET_KEY_PRESSED, ui::VKEY_LEFT, ui::EF_NONE));
-  move_action_view_->touch_point()->OnKeyReleased(
+  move_action_view->touch_point()->OnKeyReleased(
       ui::KeyEvent(ui::ET_KEY_RELEASED, ui::VKEY_LEFT, ui::EF_NONE));
   move_action_->BindPending();
   auto move_left = gfx::Vector2d(-kArrowKeyMoveDistance, 0);
@@ -406,9 +365,9 @@ TEST_F(ActionViewTest, TestArrowKeyMove) {
 
   // Arrow key down single press & release.
   updated_pos = move_action_->touch_down_positions()[0];
-  move_action_view_->touch_point()->OnKeyPressed(
+  move_action_view->touch_point()->OnKeyPressed(
       ui::KeyEvent(ui::ET_KEY_PRESSED, ui::VKEY_DOWN, ui::EF_NONE));
-  move_action_view_->touch_point()->OnKeyReleased(
+  move_action_view->touch_point()->OnKeyReleased(
       ui::KeyEvent(ui::ET_KEY_RELEASED, ui::VKEY_DOWN, ui::EF_NONE));
   move_action_->BindPending();
   auto move_down = gfx::Vector2d(0, kArrowKeyMoveDistance);
@@ -421,11 +380,11 @@ TEST_F(ActionViewTest, TestArrowKeyMove) {
   int key_press_times = 5;
   auto move_right = gfx::Vector2d(kArrowKeyMoveDistance, 0);
   for (int i = 0; i < key_press_times; i++) {
-    move_action_view_->touch_point()->OnKeyPressed(
+    move_action_view->touch_point()->OnKeyPressed(
         ui::KeyEvent(ui::ET_KEY_PRESSED, ui::VKEY_RIGHT, ui::EF_NONE));
     updated_pos += move_right;
   }
-  move_action_view_->touch_point()->OnKeyReleased(
+  move_action_view->touch_point()->OnKeyReleased(
       ui::KeyEvent(ui::ET_KEY_RELEASED, ui::VKEY_RIGHT, ui::EF_NONE));
   move_action_->BindPending();
   EXPECT_POINTF_NEAR(updated_pos, move_action_->touch_down_positions()[0],
@@ -434,42 +393,44 @@ TEST_F(ActionViewTest, TestArrowKeyMove) {
 
 TEST_F(ActionViewTest, TestActionViewReorder) {
   SetDisplayMode(DisplayMode::kEdit);
-  // Move `move_action_view_` to the right. `tap_action_view_` is sorted in
+  auto* move_action_view = move_action_->action_view();
+  auto* tap_action_view = tap_action_->action_view();
+  // Move `tap_action_view` to the right of `move_action_view`.
+  // `move_action_view` is sorted in front.
+  TouchPressAtActionView(tap_action_view);
+  TouchMoveAtActionViewBy(tap_action_view, gfx::Vector2d(10, 0));
+  TouchReleaseAtActionView(tap_action_view);
+  SetDisplayMode(DisplayMode::kView);
+  SetDisplayMode(DisplayMode::kEdit);
+  EXPECT_EQ(1u, *GetIndexOf(tap_action_view));
+  EXPECT_EQ(0u, *GetIndexOf(move_action_view));
+  // Move `move_action_view` to the right. `tap_action_view` is sorted in
   // front.
-  TouchPressAtActionView(move_action_view_);
-  TouchMoveAtActionViewBy(move_action_view_, gfx::Vector2d(20, 0));
-  TouchReleaseAtActionView(move_action_view_);
+  TouchPressAtActionView(move_action_view);
+  TouchMoveAtActionViewBy(move_action_view, gfx::Vector2d(20, 0));
+  TouchReleaseAtActionView(move_action_view);
   SetDisplayMode(DisplayMode::kView);
   SetDisplayMode(DisplayMode::kEdit);
-  EXPECT_EQ(0u, *GetIndexOf(tap_action_view_));
-  EXPECT_EQ(1u, *GetIndexOf(move_action_view_));
-  // Move `tap_action_view_` to the right of `move_action_view_`.
-  // `move_action_view_` is sorted in front.
-  TouchPressAtActionView(tap_action_view_);
-  TouchMoveAtActionViewBy(tap_action_view_, gfx::Vector2d(30, 0));
-  TouchReleaseAtActionView(tap_action_view_);
+  EXPECT_EQ(0u, *GetIndexOf(tap_action_view));
+  EXPECT_EQ(1u, *GetIndexOf(move_action_view));
+  // Move `tap_action_view` to the top of `move_action_view`.
+  // `tap_action_view` is sorted in front.
+  PressLeftMouseAtActionView(tap_action_view);
+  MouseDragActionViewBy(tap_action_view, gfx::Vector2d(0, -10));
+  ReleaseLeftMouse(tap_action_view);
   SetDisplayMode(DisplayMode::kView);
   SetDisplayMode(DisplayMode::kEdit);
-  EXPECT_EQ(1u, *GetIndexOf(tap_action_view_));
-  EXPECT_EQ(0u, *GetIndexOf(move_action_view_));
-  // Move `tap_action_view_` to the top of `move_action_view_`.
-  // `tap_action_view_` is sorted in front.
-  PressLeftMouseAtActionView(tap_action_view_);
-  MouseDragActionViewBy(tap_action_view_, gfx::Vector2d(0, -10));
-  ReleaseLeftMouse(tap_action_view_);
+  EXPECT_EQ(0u, *GetIndexOf(tap_action_view));
+  EXPECT_EQ(1u, *GetIndexOf(move_action_view));
+  // Move `move_action_view` to the left side of the window.
+  // `move_action_view` is sorted in front.
+  TouchPressAtActionView(move_action_view);
+  TouchMoveAtActionViewBy(move_action_view, gfx::Vector2d(-30, 0));
+  TouchReleaseAtActionView(move_action_view);
   SetDisplayMode(DisplayMode::kView);
   SetDisplayMode(DisplayMode::kEdit);
-  EXPECT_EQ(0u, *GetIndexOf(tap_action_view_));
-  EXPECT_EQ(1u, *GetIndexOf(move_action_view_));
-  // Move `move_action_view_` to the left side of the window.
-  // `move_action_view_` is sorted in front.
-  TouchPressAtActionView(move_action_view_);
-  TouchMoveAtActionViewBy(move_action_view_, gfx::Vector2d(-30, 0));
-  TouchReleaseAtActionView(move_action_view_);
-  SetDisplayMode(DisplayMode::kView);
-  SetDisplayMode(DisplayMode::kEdit);
-  EXPECT_EQ(1u, *GetIndexOf(tap_action_view_));
-  EXPECT_EQ(0u, *GetIndexOf(move_action_view_));
+  EXPECT_EQ(1u, *GetIndexOf(tap_action_view));
+  EXPECT_EQ(0u, *GetIndexOf(move_action_view));
 }
 
 }  // namespace arc::input_overlay
