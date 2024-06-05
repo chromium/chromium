4e0570c3c0ad3bea0fc75ff804375d89eb04ca57
Anastasia Helfinstein <anastasi@google.com>
2024-06-05 20:20:28 +0000

    [ChromeVox] Convert OutputLogger to TypeScript
    AX-Relnotes: n/a.
    Bug: b/267329383
    Change-Id: Ieeaef02acc6563235bd2ea20e1f5ec5b9cea06b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598386
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310845}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/output/output_logger.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/output/output_logger.ts
@@ -13,44 +13,36 @@ import {LogStore} from '../logging/log_store.js';
 import {OutputRuleSpecifier} from './output_rules.js';
 
 export class OutputFormatLogger {
-  /**
-   * @param {string} enableKey The key to enable logging in LocalStorage
-   * @param {!LogType} type
-   */
-  constructor(enableKey, type) {
-    /** @private {string} */
-    this.str_ = '';
-    /** @private {string} */
+  private str_: string = '';
+  private storageEnabledKey_: string;
+  private logType_: LogType;
+
+
+  /** @param enableKey The key to enable logging in LocalStorage. */
+  constructor(enableKey: string, type: LogType) {
     this.storageEnabledKey_ = enableKey;
-    /** @private {!LogType} */
     this.logType_ = type;
   }
 
-  /** @return {boolean} */
-  get loggingDisabled() {
+  get loggingDisabled(): boolean {
     return !LocalStorage.get(this.storageEnabledKey_);
   }
 
   /** Sends the queued logs to the LogStore. */
-  commitLogs() {
+  commitLogs(): void {
     if (this.str_) {
       LogStore.instance.writeTextLog(this.str_, this.logType_);
     }
   }
 
-  /** @param {string} str */
-  write(str) {
+  write(str: string): void {
     if (this.loggingDisabled) {
       return;
     }
     this.str_ += str;
   }
 
-  /**
-   * @param {string} token
-   * @param {string|undefined} value
-   */
-  writeTokenWithValue(token, value) {
+  writeTokenWithValue(token: string, value?: string): void {
     if (this.loggingDisabled) {
       return;
     }
@@ -63,18 +55,14 @@ export class OutputFormatLogger {
     this.str_ += '\n';
   }
 
-  /** @param {string} token */
-  writeToken(token) {
+  writeToken(token: string): void {
     if (this.loggingDisabled) {
       return;
     }
     this.str_ += '$' + token + ': ';
   }
 
-  /**
-   * @param {OutputRuleSpecifier} rule
-   */
-  writeRule(rule) {
+  writeRule(rule: OutputRuleSpecifier): void {
     if (this.loggingDisabled) {
       return;
     }
@@ -89,15 +77,14 @@ export class OutputFormatLogger {
     this.str_ += '\n';
   }
 
-  bufferClear() {
+  bufferClear(): void {
     if (this.loggingDisabled) {
       return;
     }
     this.str_ += '\nBuffer is cleared.\n';
   }
 
-  /** @param {string} errorMsg */
-  writeError(errorMsg) {
+  writeError(errorMsg: string): void {
     if (this.loggingDisabled) {
       return;
     }
