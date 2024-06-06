0c9d3c0f121071aad0be47ea844127f19aa616f6
Sakib Shabir Tantray <s1.tantray@samsung.com>
2024-06-06 05:36:27 +0000

    Reland "Keyboard shortcuts are ignored in the menu."
    This is a reland of commit 514565500183d77846ea6963f99956b51a6f4dd9
    
    Failure caused by calling a method on a null weak pointer. Fully
    exiting the menu destroys its object, we were calling
    MenuController::ShouldStopEventPropagation after exiting the menu
    fully, which leads to invalid vptr.
    Check for key events before exiting the menu fully.
    
    Original change's description:
    > Keyboard shortcuts are ignored in the menu.
    >
    > Keyboard Shortcuts: https://support.google.com/chrome/answer/157179
    >
    > Modifiers are mistakenly ignored for all menu hotkeys. Keyboard
    > shortcuts need to be handled in the app menu, when any keyboard
    > shortcut is pressed, close the app menu, and after that, it should
    > perform the supposed task.
    >
    > This CL will handle keyboard shortcuts in the app menu on Windows
      and Linux. Currently we are handling Shift-Esc only. Adding Ctrl-J
      shortcut with this CL, as it is supposed to open the downloads page.
      In follow-up patches, other shortcuts will be included.
    >
    > Bug: None
    >
    > Change-Id: I906818922206d2d48dd22a9a67212fabe571de46
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584445
    > Reviewed-by: Shubham Gupta <shubham.gupta@chromium.org>
    > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    > Commit-Queue: Sakib Shabir <s1.tantray@samsung.com>
    > Cr-Commit-Position: refs/heads/main@{#1308472}
    
    Bug: None
    
    Change-Id: Ia9e045cddbdd21fac89a1816b7b69ff253cca51e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588476
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Shubham Gupta <shubham.gupta@chromium.org>
    Commit-Queue: Sakib Shabir <s1.tantray@samsung.com>
    Cr-Commit-Position: refs/heads/main@{#1311085}

--- ui/views/controls/menu/menu_pre_target_handler_aura.cc
+++ ui/views/controls/menu/menu_pre_target_handler_aura.cc
@@ -58,7 +58,36 @@ void MenuPreTargetHandlerAura::OnCancelMode(ui::CancelModeEvent* event) {
 }
 
 void MenuPreTargetHandlerAura::OnKeyEvent(ui::KeyEvent* event) {
+  // Fully exit the menu, if the key event is supposed to perform some task.
+  if (ShouldCancelMenuForEvent(*event)) {
+    controller_->Cancel(MenuController::ExitType::kAll);
+    return;
+  }
   controller_->OnWillDispatchKeyEvent(event);
+}
+
+bool MenuPreTargetHandlerAura::ShouldCancelMenuForEvent(
+    const ui::KeyEvent& event) {
+  const ui::KeyboardCode key_code = event.key_code();
+  switch (key_code) {
+    case ui::VKEY_ESCAPE:
+      // Fully exit the menu when Shift-Esc is pressed because it is
+      // supposed to open the Task Manager on Windows and Linux
+      if (event.IsShiftDown()) {
+        return true;
+      }
+      break;
+    case ui::VKEY_J:
+      // Fully exit the menu when Ctrl+J is pressed because it is supposed
+      // to open the downloads page on Windows and Linux.
+      if (event.IsControlDown()) {
+        return true;
+      }
+      break;
+    default:
+      break;
+  }
+  return false;
 }
 
 void MenuPreTargetHandlerAura::Cleanup() {
