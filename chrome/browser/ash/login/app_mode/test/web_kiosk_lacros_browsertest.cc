f76a2120f290a93274564e39442cac85182da924
Michael Ershov <miersh@google.com>
2024-06-06 09:32:16 +0000

    Revert "Disable another Lacros Kiosk browser test"
    This reverts commit b03a4e3921fb3e8d16cb52f88eba63023120a57c.
    
    Reason for revert: 
    Flaky test was fixed by https://crrev.com/c/5545188
    
    Original change's description:
    > Disable another Lacros Kiosk browser test
    >
    > A bunch of similar tests were recently disabled in crrev.com/c/5560545
    > but this one was missed.
    >
    > Bug: 342108781
    > Change-Id: I648d13ed31a73308cf1f4649460f13a4c4923e06
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560986
    > Commit-Queue: Marc Treib <treib@chromium.org>
    > Owners-Override: Marc Treib <treib@chromium.org>
    > Auto-Submit: Marc Treib <treib@chromium.org>
    > Reviewed-by: Victor Vianna <victorvianna@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1304946}
    
    Bug: 342108781
    Change-Id: Ie7df10d41acfde3a1a517dd2ede29e5a4bb6ae93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600611
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Auto-Submit: Michael Ershov <miersh@google.com>
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311139}

--- chrome/browser/ash/login/app_mode/test/web_kiosk_lacros_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/web_kiosk_lacros_browsertest.cc
@@ -86,14 +86,7 @@ IN_PROC_BROWSER_TEST_F(WebKioskLacrosTest, ShouldCloseNewAshBrowserWindow) {
   EXPECT_FALSE(session().is_shutting_down());
 }
 
-// TODO(b/342108781): Disable the test on ci/linux-chromeos-chrome-with-lacros
-// since it fails.
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-#define MAYBE_ShouldAllowSettingsWindow DISABLED_ShouldAllowSettingsWindow
-#else
-#define MAYBE_ShouldAllowSettingsWindow ShouldAllowSettingsWindow
-#endif
-IN_PROC_BROWSER_TEST_F(WebKioskLacrosTest, MAYBE_ShouldAllowSettingsWindow) {
+IN_PROC_BROWSER_TEST_F(WebKioskLacrosTest, ShouldAllowSettingsWindow) {
   if (!kiosk_ash_starter_.HasLacrosArgument()) {
     return;
   }
