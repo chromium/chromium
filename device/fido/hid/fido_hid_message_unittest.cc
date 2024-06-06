bd5ef8b7028c07a6009d4e949281f3df3988e122
Peter Boström <pbos@chromium.org>
2024-06-06 10:58:42 +0000

    Upgrade NOTREACHED to CHECK in FidoHidMessage
    This invariant failure should be unreachable (filtered before it reaches
    this point).
    
    Bug: 40580068
    Change-Id: I589aaf97619b04e24f2a6b7f23d852eb7406b066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600525
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311166}

--- device/fido/hid/fido_hid_message_unittest.cc
+++ device/fido/hid/fido_hid_message_unittest.cc
@@ -166,23 +166,17 @@ TEST(FidoHidMessageTest, TestMessagePartitoning) {
 TEST(FidoHidMessageTest, TooLarge) {
   std::vector<uint8_t> data;
 
-#if DCHECK_IS_ON()
-#define EXPECT_SIZE_FAILURE(x) EXPECT_DEATH_IF_SUPPORTED(x, "")
-#else
-#define EXPECT_SIZE_FAILURE(x) EXPECT_FALSE(x.has_value())
-#endif
-
   // kHidInitPacketHeaderSize is too small a report size to be valid.
-  EXPECT_SIZE_FAILURE(FidoHidMessage::Create(kHidBroadcastChannel,
-                                             FidoHidDeviceCommand::kPing,
-                                             kHidInitPacketHeaderSize, data));
+  EXPECT_DEATH_IF_SUPPORTED(
+      FidoHidMessage::Create(kHidBroadcastChannel, FidoHidDeviceCommand::kPing,
+                             kHidInitPacketHeaderSize, data),
+      "");
 
   // kHidMaxPacketSize + 1 is too large a report size.
-  EXPECT_SIZE_FAILURE(FidoHidMessage::Create(kHidBroadcastChannel,
-                                             FidoHidDeviceCommand::kPing,
-                                             kHidMaxPacketSize + 1, data));
-
-#undef EXPECT_SIZE_FAILURE
+  EXPECT_DEATH_IF_SUPPORTED(
+      FidoHidMessage::Create(kHidBroadcastChannel, FidoHidDeviceCommand::kPing,
+                             kHidMaxPacketSize + 1, data),
+      "");
 }
 
 TEST(FidoHidMessageTest, TestMaxSize) {
