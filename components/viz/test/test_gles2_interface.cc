012113fee62020261f1a317c867741fd500feafd
Colin Blundell <blundell@chromium.org>
2024-06-06 13:24:40 +0000

    [Viz] Remove dead setters from TestGLES2Interface
    Noticed while plumbing around this code for other purposes.
    
    Change-Id: Id9c0732dfcd89d4e3876a9d7b73bf77a8ae7782b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600305
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311211}

--- components/viz/test/test_gles2_interface.cc
+++ components/viz/test/test_gles2_interface.cc
@@ -428,14 +428,6 @@ void TestGLES2Interface::ReadPixels(GLint x,
   memset(pixels, 0, pixels_size);
 }
 
-void TestGLES2Interface::set_support_texture_format_bgra8888(bool support) {
-  test_capabilities_.texture_format_bgra8888 = support;
-}
-
-void TestGLES2Interface::set_support_sync_query(bool support) {
-  test_capabilities_.sync_query = support;
-}
-
 void TestGLES2Interface::set_support_texture_half_float_linear(bool support) {
   test_capabilities_.texture_half_float_linear = support;
 }
@@ -444,16 +436,8 @@ void TestGLES2Interface::set_support_texture_norm16(bool support) {
   test_capabilities_.texture_norm16 = support;
 }
 
-void TestGLES2Interface::set_msaa_is_slow(bool msaa_is_slow) {
-  test_capabilities_.msaa_is_slow = msaa_is_slow;
-}
-
 void TestGLES2Interface::set_gpu_rasterization(bool gpu_rasterization) {
   test_capabilities_.gpu_rasterization = gpu_rasterization;
-}
-
-void TestGLES2Interface::set_avoid_stencil_buffers(bool avoid_stencil_buffers) {
-  test_capabilities_.avoid_stencil_buffers = avoid_stencil_buffers;
 }
 
 void TestGLES2Interface::set_max_texture_size(int size) {
