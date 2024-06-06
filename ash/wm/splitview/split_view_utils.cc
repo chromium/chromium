0fa07d813412543c9f28a8ea67f114576222dc00
Sophie Wen <sophiewen@google.com>
2024-06-06 02:05:38 +0000

    [merge to 126] splitview: Fix kFasterSplitScreenSetup disabled
    This fixes the `CanStartSplitViewOverviewSessionInClamshell()` check
    to *not* start split view overview session on a window
    snapped outside of overview if kFasterSplitScreenSetup is disabled.
    
    (cherry picked from commit 4d10af2fbbd9643a4cb803c7e24cb51520b47e4b)
    
    Bug: b/344629673
    Test: added, verified it fails w/o fix in CL:5594196
    Change-Id: I90ee0eeb0860ffc483cec4c4adec4524eaa75d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593595
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1310350}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600058
    Cr-Commit-Position: refs/branch-heads/6478@{#1211}
    Cr-Branched-From: e6143acc03189c5e52959545b110d6d17ecd5286-refs/heads/main@{#1300313}

--- ash/wm/splitview/split_view_utils.cc
+++ ash/wm/splitview/split_view_utils.cc
@@ -19,6 +19,7 @@
 #include "ash/system/toast/toast_manager_impl.h"
 #include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/overview/overview_controller.h"
+#include "ash/wm/overview/overview_session.h"
 #include "ash/wm/overview/overview_utils.h"
 #include "ash/wm/screen_pinning_controller.h"
 #include "ash/wm/snap_group/snap_group.h"
@@ -26,6 +27,7 @@
 #include "ash/wm/splitview/layout_divider_controller.h"
 #include "ash/wm/splitview/split_view_constants.h"
 #include "ash/wm/splitview/split_view_controller.h"
+#include "ash/wm/splitview/split_view_overview_session.h"
 #include "ash/wm/splitview/split_view_types.h"
 #include "ash/wm/window_positioning_utils.h"
 #include "ash/wm/window_restore/window_restore_controller.h"
@@ -867,6 +869,13 @@ aura::Window* GetOppositeVisibleSnappedWindow(aura::Window* window) {
       continue;
     }
 
+    if (IsInOverviewSession() &&
+        GetOverviewSession()->IsWindowInOverview(top_window)) {
+      // Skip any windows that are in overview, since they are visually not
+      // snapped to the user.
+      continue;
+    }
+
     const auto* top_window_state = WindowState::Get(top_window);
     const gfx::Rect top_window_bounds = top_window->GetBoundsInScreen();
     if (top_window_state->GetStateType() == opposite_snap_type) {
@@ -917,14 +926,30 @@ bool ShouldConsiderWindowForFasterSplitView(
 bool CanStartSplitViewOverviewSessionInClamshell(
     aura::Window* window,
     WindowSnapActionSource snap_action_source) {
+  // If kFasterSplitScreenSetup is disabled, only allow split view overview if
+  // the window is being dragged to snap while in overview. Note this is
+  // different from `IsFasterSplitScreenOrSnapGroupEnabledInClamshell()` which
+  // checks if kFasterSplitScreenSetup *or* kSnapGroup is enabled.
+  const bool is_overview_drag_to_snap =
+      snap_action_source ==
+      WindowSnapActionSource::kDragOrSelectOverviewWindowToSnap;
+  if (!Shell::Get()->IsInTabletMode() &&
+      !features::IsFasterSplitScreenSetupEnabled()) {
+    // TODO(b/344958499): Temporary fix. Investigate why `OnWindowSnapped()` is
+    // called multiple times.
+    return is_overview_drag_to_snap && !GetOppositeVisibleSnappedWindow(window);
+  }
+
   if (IsInOverviewSession() && WindowState::Get(window)->IsSnapped()) {
     return !RootWindowController::ForWindow(window)
                 ->split_view_overview_session();
   }
 
   // Skip starting `SplitViewOverviewSession` if a fully visible window snapped
-  // on the opposite side.
-  if (GetOppositeVisibleSnappedWindow(window)) {
+  // on the opposite side. Exception: If dragging in Overview, skip checking
+  // opposite-side snapped windows, as they're not visually snapped for the
+  // user in Overview.
+  if (!is_overview_drag_to_snap && GetOppositeVisibleSnappedWindow(window)) {
     return false;
   }
 
@@ -932,7 +957,9 @@ bool CanStartSplitViewOverviewSessionInClamshell(
 }
 
 bool IsSnapGroupEnabledInClamshellMode() {
-  return SnapGroupController::Get() &&
+  // `SnapGroupController` is only created if `kSnapGroup` is enabled in
+  // `Shell::Init()`.
+  return features::IsSnapGroupEnabled() && SnapGroupController::Get() &&
          !display::Screen::GetScreen()->InTabletMode();
 }
 
