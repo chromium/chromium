419835a38edb655ac04f49a8ee16ebc31eb10229
Scott Haseley <shaseley@chromium.org>
2024-06-06 18:45:17 +0000

    Enable BlinkSchedulerPrioritizeNavigationIPCs by default
    The experiment resulted in small FCP and LCP improvements, and the
    prioritization prevents high priority work from blocking navigations,
    which unblocks scheduler.yield().
    
    Experiment results:
    https://docs.google.com/document/d/1qQHNPIVN0ctJjxgkydwLg5vTNFQxJ4EeQK-g7u_pz7E/edit?usp=sharing
    
    Bug: 40114705
    Change-Id: Ibbedd98a732a2d7fdd4510857ed07264aa1779b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5585346
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311441}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2422,7 +2422,7 @@ BASE_FEATURE(kEmulateLoadStartedForInspectorOncePerResource,
 
 BASE_FEATURE(kBlinkSchedulerPrioritizeNavigationIPCs,
              "BlinkSchedulerPrioritizeNavigationIPCs",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableFileBackedBlobFactory,
              "EnableFileBackedBlobFactory",
