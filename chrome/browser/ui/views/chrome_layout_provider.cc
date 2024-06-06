8f13b1fbfdbfc4dbaf469389ff874688f230e271
Dana Fried <dfried@chromium.org>
2024-06-06 18:15:56 +0000

    [GM2 cleanup] chrome_layout_provider.cc
    Bug: 338287039
    Change-Id: I2c2b83cb98d51fb0886c91c25a05d0a7aa02f3f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605214
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311417}

--- chrome/browser/ui/views/chrome_layout_provider.cc
+++ chrome/browser/ui/views/chrome_layout_provider.cc
@@ -7,19 +7,12 @@
 #include <algorithm>
 
 #include "base/feature_list.h"
-#include "base/logging.h"
 #include "chrome/browser/ui/views/chrome_typography.h"
 #include "components/omnibox/common/omnibox_features.h"
 #include "ui/base/pointer/touch_ui_controller.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/gfx/shadow_value.h"
 
 namespace {
-
-// TODO(pbos): Inline kHarmonyLayoutUnit calculations below as it's not really
-// respected (there's 3 * unit / 4 in use to express 12).
-// The Harmony layout unit. All distances are in terms of this unit.
-constexpr int kHarmonyLayoutUnit = 16;
 
 ChromeLayoutProvider* g_chrome_layout_provider = nullptr;
 
@@ -54,12 +47,10 @@ gfx::Insets ChromeLayoutProvider::GetInsetsMetric(int metric) const {
   switch (metric) {
     case views::INSETS_DIALOG:
     case views::INSETS_DIALOG_SUBSECTION: {
-      return features::IsChromeRefresh2023() ? gfx::Insets::VH(20, 20)
-                                             : gfx::Insets(kHarmonyLayoutUnit);
+      return gfx::Insets::VH(20, 20);
     }
     case views::INSETS_DIALOG_FOOTNOTE: {
-      return features::IsChromeRefresh2023() ? gfx::Insets::TLBR(10, 20, 15, 20)
-                                             : gfx::Insets(kHarmonyLayoutUnit);
+      return gfx::Insets::TLBR(10, 20, 15, 20);
     }
     case views::INSETS_CHECKBOX_RADIO_BUTTON: {
       gfx::Insets insets = LayoutProvider::GetInsetsMetric(metric);
@@ -68,30 +59,24 @@ gfx::Insets ChromeLayoutProvider::GetInsetsMetric(int metric) const {
                                insets.right());
     }
     case views::INSETS_VECTOR_IMAGE_BUTTON:
-      return gfx::Insets(kHarmonyLayoutUnit / 4);
+      return gfx::Insets(4);
     case views::InsetsMetric::INSETS_LABEL_BUTTON:
-      return touch_ui ? gfx::Insets::VH(kHarmonyLayoutUnit / 2,
-                                        kHarmonyLayoutUnit / 2)
+      return touch_ui ? gfx::Insets::VH(8, 8)
                       : LayoutProvider::GetInsetsMetric(metric);
     case INSETS_BOOKMARKS_BAR_BUTTON:
       return touch_ui ? gfx::Insets::VH(8, 10) : gfx::Insets(6);
     case INSETS_TOAST:
-      return gfx::Insets::VH(0, kHarmonyLayoutUnit);
+      return gfx::Insets::VH(0, 16);
     case INSETS_OMNIBOX_PILL_BUTTON:
       if (!touch_ui) {
         return gfx::Insets::VH(4, 8);
       } else {
-        return touch_ui
-                   ? gfx::Insets::VH(kHarmonyLayoutUnit / 2, kHarmonyLayoutUnit)
-                   : gfx::Insets::VH(5, 12);
+        return touch_ui ? gfx::Insets::VH(8, 16) : gfx::Insets::VH(5, 12);
       }
     case INSETS_PAGE_INFO_HOVER_BUTTON: {
       const gfx::Insets insets =
           LayoutProvider::GetInsetsMetric(views::INSETS_LABEL_BUTTON);
-      const int horizontal_padding =
-          features::IsChromeRefresh2023()
-              ? 20
-              : GetDistanceMetric(views::DISTANCE_BUTTON_HORIZONTAL_PADDING);
+      const int horizontal_padding = 20;
       // Hover button in page info requires double the height compared to the
       // label button because it behaves like a menu control.
       return gfx::Insets::VH(insets.height(), horizontal_padding);
@@ -110,11 +95,11 @@ int ChromeLayoutProvider::GetDistanceMetric(int metric) const {
 
   switch (static_cast<ChromeDistanceMetric>(metric)) {
     case DISTANCE_CONTENT_LIST_VERTICAL_SINGLE:
-      return kHarmonyLayoutUnit / 4;
+      return 4;
     case DISTANCE_CONTENT_LIST_VERTICAL_MULTI:
-      return kHarmonyLayoutUnit / 2;
+      return 8;
     case DISTANCE_CONTROL_LIST_VERTICAL:
-      return kHarmonyLayoutUnit * 3 / 4;
+      return 12;
     case DISTANCE_DROPDOWN_BUTTON_LABEL_ARROW_SPACING:
       return 8;
     case DISTANCE_DROPDOWN_BUTTON_RIGHT_MARGIN:
@@ -122,7 +107,7 @@ int ChromeLayoutProvider::GetDistanceMetric(int metric) const {
     case DISTANCE_EXTENSIONS_MENU_WIDTH:
       return kMediumDialogWidth;
     case DISTANCE_EXTENSIONS_MENU_BUTTON_ICON_SIZE:
-      return features::IsChromeRefresh2023() ? 20 : 16;
+      return 20;
     case DISTANCE_EXTENSIONS_MENU_BUTTON_ICON_SMALL_SIZE:
       return 16;
     case DISTANCE_EXTENSIONS_MENU_EXTENSION_ICON_SIZE:
@@ -134,13 +119,13 @@ int ChromeLayoutProvider::GetDistanceMetric(int metric) const {
     case DISTANCE_EXTENSIONS_MENU_BUTTON_MARGIN:
       return GetDistanceMetric(DISTANCE_CONTROL_LIST_VERTICAL);
     case DISTANCE_RELATED_CONTROL_HORIZONTAL_SMALL:
-      return kHarmonyLayoutUnit;
+      return 16;
     case DISTANCE_RELATED_CONTROL_VERTICAL_SMALL:
-      return kHarmonyLayoutUnit / 2;
+      return 8;
     case DISTANCE_BUTTON_MINIMUM_WIDTH:
       return GetDistanceMetric(views::DISTANCE_DIALOG_BUTTON_MINIMUM_WIDTH);
     case DISTANCE_RELATED_LABEL_HORIZONTAL_LIST:
-      return kHarmonyLayoutUnit / 2;
+      return 8;
     case DISTANCE_SUBSECTION_HORIZONTAL_INDENT:
       return 0;
     case DISTANCE_TOAST_CONTROL_VERTICAL:
@@ -148,9 +133,9 @@ int ChromeLayoutProvider::GetDistanceMetric(int metric) const {
     case DISTANCE_TOAST_LABEL_VERTICAL:
       return 12;
     case DISTANCE_UNRELATED_CONTROL_HORIZONTAL_LARGE:
-      return kHarmonyLayoutUnit;
+      return 16;
     case DISTANCE_UNRELATED_CONTROL_VERTICAL_LARGE:
-      return kHarmonyLayoutUnit;
+      return 16;
     case DISTANCE_BUBBLE_HEADER_VECTOR_ICON_SIZE:
       return 20;
     case DISTANCE_STANDALONE_BUBBLE_PREFERRED_WIDTH:
@@ -170,19 +155,17 @@ int ChromeLayoutProvider::GetDistanceMetric(int metric) const {
     case DISTANCE_SIDE_PANEL_HEADER_INTERIOR_MARGIN_HORIZONTAL:
       return 4;
     case DISTANCE_HORIZONTAL_SEPARATOR_PADDING_PAGE_INFO_VIEW:
-      return features::IsChromeRefresh2023() ? 20 : 0;
+      return 20;
     case DISTANCE_INFOBAR_HORIZONTAL_ICON_LABEL_PADDING:
-      return features::IsChromeRefresh2023() ? 16 : 12;
+      return 16;
     case DISTANCE_INFOBAR_HEIGHT:
       // Spec says height of button should be 36dp, vertical padding on both
       // top and bottom should be 8dp.
       return 36 + 2 * 8;
     case DISTANCE_PERMISSION_PROMPT_HORIZONTAL_ICON_LABEL_PADDING:
-      return features::IsChromeRefresh2023()
-                 ? 8
-                 : GetDistanceMetric(views::DISTANCE_RELATED_LABEL_HORIZONTAL);
+      return 8;
     case DISTANCE_RICH_HOVER_BUTTON_ICON_HORIZONTAL:
-      return features::IsChromeRefresh2023() ? 8 : 12;
+      return 8;
   }
   NOTREACHED_NORETURN();
 }
@@ -194,8 +177,7 @@ int ChromeLayoutProvider::GetSnappedDialogWidth(int min_width) const {
       return snap_point;
   }
 
-  return ((min_width + kHarmonyLayoutUnit - 1) / kHarmonyLayoutUnit) *
-         kHarmonyLayoutUnit;
+  return ((min_width + 15) / 16) * 16;
 }
 
 const views::TypographyProvider& ChromeLayoutProvider::GetTypographyProvider()
