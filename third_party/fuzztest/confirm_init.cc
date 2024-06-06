74bf662f977cc695278964ba61c47ff70db938eb
Adrian Taylor <adetaylor@chromium.org>
2024-06-06 12:39:07 +0000

    Exclude fuzztests from Android etc.
    There are some platforms where FuzzTests can technically build and run,
    but we don't want them to. That's because on such platforms, FuzzTests
    would run on CI but not on ClusterFuzz, and that would impose an undue
    burden on gardeners to triage any problems found. We want to restrict CI
    runs of FuzzTests to those platforms where the FuzzTests will also run
    on ClusterFuzz, such that security shepherds (and ClusterFuzz's more
    general automation) will arrange to triage any problems found.
    
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel,win-libfuzzer-asan-rel,linux-centipede-asan-rel
    Bug: 40286211
    Change-Id: I14910ce71c0ffdf7c0eecbfdedefb5891e3a56b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5547979
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311192}

--- third_party/fuzztest/confirm_init.cc
+++ third_party/fuzztest/confirm_init.cc
@@ -2,12 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "third_party/fuzztest/init_fuzztest_buildflags.h"
 #include "third_party/fuzztest/init_helper.h"
 #include "third_party/fuzztest/src/fuzztest/init_fuzztest.h"
 
 static void RealInitFunction(int* argc, char*** argv) {
   fuzztest::ParseAbslFlags(*argc, *argv);
+#if BUILDFLAG(REGISTER_FUZZTESTS_IN_TEST_SUITES)
   fuzztest::InitFuzzTest(argc, argv);
+#endif
 }
 
 // base/test:test_support is used in test suites containing fuzztests
