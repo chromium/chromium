80489ad92bf88bf227ad182f92eed2219f8d1a1a
Jocelyn Tran <jocelyntran@google.com>
2024-06-06 19:24:52 +0000

    [Merge M126] Revert "[A11y] Process update only when options bounds are ready"
    This reverts commit 7b5a09f8d44399bdf4573f5194752ff0c7caa71f.
    
    Reason for revert: Tast tests with drop-downs failing.
    
    Original change's description:
    > [A11y] Process update only when options bounds are ready
    >
    > When there are more than 1000 options in a select, the first 1000 are
    > included in the first layout and the rest are included later. However,
    > only the bounds of the options from the first layout are sent to the
    > accessibility tree, causing an index out of bounds error when trying to
    > access the bounds of the 1001st option element.
    >
    > This change sends updated bounds to the tree and also prevents the
    > processing of the bounds before all of them are ready.
    >
    > Fixed: 338646632
    > Change-Id: I2e6e8eaf473e3030aef9e6425201fba3672f733f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5532661
    > Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    > Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    > Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1302226}
    
    (cherry picked from commit 070aabe90ad9f480c82a674dfc1902ff2244e62e)
    
    Bug: 341802417
    Change-Id: I973e4571a186a02f70b098e5cb05b742fc80fb38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595050
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1309988}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604782
    Cr-Commit-Position: refs/branch-heads/6478@{#1240}
    Cr-Branched-From: e6143acc03189c5e52959545b110d6d17ecd5286-refs/heads/main@{#1300313}

--- third_party/blink/renderer/core/page/page_popup_controller.h
+++ third_party/blink/renderer/core/page/page_popup_controller.h
@@ -80,7 +80,6 @@ class PagePopupController : public ScriptWrappable, public Supplement<Page> {
 
  private:
   PagePopup& popup_;
-  std::optional<gfx::Point> popup_origin_;
 
   WTF::Vector<gfx::Rect> options_bounds_;
 
