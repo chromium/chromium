3ee5132badcd6bbbe01a6a1e7766f209da8383b3
danakj <danakj@chromium.org>
2024-06-06 17:22:31 +0000

    Allow Rust allocation to fail on large allocations
    Instead of just crashing, like we do in C++, allow the allocation to
    fail and be handled gracefully if the caller was using fallible
    allocation methods (such as Vec::try_reserve).
    
    If the alignment is large, the allocation will still crash if it's
    too large as PartitionAlloc does not yet provide a way to do an
    aligned allocation without crashing on failure/OOM. This will require
    adding a new API to PartitionAlloc.
    
    Bug: b/342251590
    Change-Id: I6d787243168b02d27628073ad9bff500556b687d
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577834
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311375}

--- build/rust/tests/test_cpp_including_rust/shared_unittests.cc
+++ build/rust/tests/test_cpp_including_rust/shared_unittests.cc
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stddef.h>
 #include <stdint.h>
 
 #include <memory>
 
 #include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_buildflags.h"
-#include "build/buildflag.h"
 #include "build/rust/tests/test_rust_shared_library/src/lib.rs.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -34,4 +34,39 @@ TEST(RustSharedTest, RustComponentUsesPartitionAlloc) {
 
 TEST(RustSharedTest, AllocAligned) {
   alloc_aligned();
+}
+
+// TODO(crbug.com/343218479): A rust_shared_library target does not use our
+// allocation shim so it is not properly redirected to PartitionAlloc (though is
+// happens to use PA on Linux since the std allocator calls malloc). To enable
+// this, we need to insert the allocation shim into `rust_shared_library`,
+// probably by having the template produce a Rust static library and having
+// clang do the linking step.
+TEST(RustSharedTest, DISABLED_RustLargeAllocationFailure) {
+  // If allocation succeeds, we get true back.
+  EXPECT_TRUE(allocate_huge_via_rust(100u, 1u));
+
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+  // We only do these tests when using PA, as the system allocator will not fail
+  // on large allocations (unless it is really OOM).
+
+  // PartitionAlloc currently limits all allocations to no more than i32::MAX
+  // elements, so the allocation will fail. If done through normal malloc(),
+  // PA will crash when an allocation fails rather than return null, but Rust
+  // can be trusted to handle failure without introducing null derefs so this
+  // should fail gracefully.
+  size_t max_size = partition_alloc::internal::MaxDirectMapped();
+  EXPECT_FALSE(allocate_huge_via_rust(max_size + 1u, 4u));
+
+  // Same as above but with an alignment larger than PartitionAlloc's default
+  // alignment, which goes down a different path.
+  size_t big_alignment = alignof(std::max_align_t) * 2u;
+  EXPECT_FALSE(allocate_huge_via_rust(max_size + 1u, big_alignment));
+
+  // PartitionAlloc will crash if given an alignment larger than this. The
+  // allocation hooks handle it gracefully.
+  size_t max_alignment = partition_alloc::internal::kMaxSupportedAlignment;
+  EXPECT_FALSE(allocate_huge_via_rust(100u, max_alignment * 2u));
+
+#endif
 }
