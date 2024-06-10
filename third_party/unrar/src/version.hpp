9ca41d71ec57c60df3974b5efed106f9f01cf607
Daniel Rubery <drubery@chromium.org>
2024-06-10 22:45:13 +0000

    Roll unrar to 7.0.9
    This update only changed some version numbers, so we are not waiting the
    typical amount of time for fuzzing.
    
    Fixed: 346148108
    Change-Id: I3d7d6e3391053cd49346a4a5db2a84ebc192fcf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5618235
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313101}

--- third_party/unrar/src/version.hpp
+++ third_party/unrar/src/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     7
 #define RARVER_MINOR     1
-#define RARVER_BETA      1
-#define RARVER_DAY      25
-#define RARVER_MONTH     4
+#define RARVER_BETA      0
+#define RARVER_DAY      12
+#define RARVER_MONTH     5
 #define RARVER_YEAR   2024
