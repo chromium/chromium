3e1a2a52ba6cb6d18b376653ec10be13abb1b3d5
Tali Gorokhovsky <taligoro@google.com>
2024-06-10 20:56:43 +0000

    Add request/response logic for fetching a list of students.
    Bug: b:343731449
    Change-Id: I5895d36691cf99f1c041ff8051dee40c33f598b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608973
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Tali Gorokhovsky <taligoro@google.com>
    Reviewed-by: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313039}

---
+++ google_apis/classroom/classroom_api_list_students_request.cc
@@ -0,0 +1,111 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "google_apis/classroom/classroom_api_list_students_request.h"
+
+#include <memory>
+#include <string>
+
+#include "base/check.h"
+#include "base/files/file_path.h"
+#include "base/functional/bind.h"
+#include "base/location.h"
+#include "base/types/expected.h"
+#include "base/values.h"
+#include "google_apis/classroom/classroom_api_students_response_types.h"
+#include "google_apis/common/api_error_codes.h"
+#include "google_apis/common/base_requests.h"
+#include "google_apis/common/request_sender.h"
+#include "google_apis/gaia/gaia_urls.h"
+#include "net/base/url_util.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
+#include "url/gurl.h"
+
+namespace google_apis::classroom {
+namespace {
+
+constexpr char kListStudentsUrlTemplate[] = "/v1/courses/$1/students";
+
+constexpr char kFieldsParameterName[] = "fields";
+constexpr char kRequestedFields[] =
+    "students(profile(id,name(fullName),emailAddress)),nextPageToken";
+
+constexpr char kPageTokenParameterName[] = "pageToken";
+
+std::unique_ptr<Students> ParseResponse(std::string json) {
+  std::unique_ptr<base::Value> raw_value = ParseJson(json);
+  return raw_value ? Students::CreateFrom(*raw_value) : nullptr;
+}
+
+}  // namespace
+
+ListStudentsRequest::ListStudentsRequest(RequestSender* sender,
+                                         const std::string& course_id,
+                                         const std::string& page_token,
+                                         Callback callback)
+    : UrlFetchRequestBase(sender, ProgressCallback(), ProgressCallback()),
+      course_id_(course_id),
+      page_token_(page_token),
+      callback_(std::move(callback)) {
+  CHECK(!course_id_.empty());
+  CHECK(!callback_.is_null());
+}
+
+ListStudentsRequest::~ListStudentsRequest() = default;
+
+GURL ListStudentsRequest::GetURL() const {
+  auto url = GaiaUrls::GetInstance()->classroom_api_origin_url().Resolve(
+      base::ReplaceStringPlaceholders(kListStudentsUrlTemplate, {course_id_},
+                                      nullptr));
+  url = net::AppendOrReplaceQueryParameter(url, kFieldsParameterName,
+                                           kRequestedFields);
+  if (!page_token_.empty()) {
+    url = net::AppendOrReplaceQueryParameter(url, kPageTokenParameterName,
+                                             page_token_);
+  }
+  return url;
+}
+
+ApiErrorCode ListStudentsRequest::MapReasonToError(ApiErrorCode code,
+                                                   const std::string& reason) {
+  return code;
+}
+
+bool ListStudentsRequest::IsSuccessfulErrorCode(ApiErrorCode error) {
+  return error == HTTP_SUCCESS;
+}
+
+void ListStudentsRequest::ProcessURLFetchResults(
+    const network::mojom::URLResponseHead* response_head,
+    base::FilePath response_file,
+    std::string response_body) {
+  ApiErrorCode error = GetErrorCode();
+  switch (error) {
+    case HTTP_SUCCESS:
+      blocking_task_runner()->PostTaskAndReplyWithResult(
+          FROM_HERE, base::BindOnce(&ParseResponse, std::move(response_body)),
+          base::BindOnce(&ListStudentsRequest::OnDataParsed,
+                         weak_ptr_factory_.GetWeakPtr()));
+      break;
+    default:
+      RunCallbackOnPrematureFailure(error);
+      OnProcessURLFetchResultsComplete();
+      break;
+  }
+}
+
+void ListStudentsRequest::RunCallbackOnPrematureFailure(ApiErrorCode error) {
+  std::move(callback_).Run(base::unexpected(error));
+}
+
+void ListStudentsRequest::OnDataParsed(std::unique_ptr<Students> students) {
+  if (!students) {
+    std::move(callback_).Run(base::unexpected(PARSE_ERROR));
+  } else {
+    std::move(callback_).Run(std::move(students));
+  }
+  OnProcessURLFetchResultsComplete();
+}
+
+}  // namespace google_apis::classroom
---
+++ google_apis/classroom/classroom_api_students_response_types.cc
@@ -0,0 +1,79 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "google_apis/classroom/classroom_api_students_response_types.h"
+
+#include <memory>
+#include <string>
+#include <string_view>
+
+#include "base/json/json_value_converter.h"
+#include "base/notreached.h"
+#include "base/values.h"
+#include "google_apis/common/parser_util.h"
+
+namespace google_apis::classroom {
+namespace {
+
+constexpr char kApiResponseProfileKey[] = "profile";
+constexpr char kApiResponseStudentsKey[] = "students";
+constexpr char kApiResponseFullNameKey[] = "fullName";
+constexpr char kApiResponseEmailAddressKey[] = "emailAddress";
+constexpr char kApiResponseNameKey[] = "name";
+
+}  // namespace
+
+// ----- Name -----
+
+// static
+void Name::RegisterJSONConverter(base::JSONValueConverter<Name>* converter) {
+  converter->RegisterStringField(kApiResponseFullNameKey, &Name::full_name_);
+}
+
+// ----- UserProfile -----
+
+// static
+void UserProfile::RegisterJSONConverter(
+    base::JSONValueConverter<UserProfile>* converter) {
+  converter->RegisterStringField(kApiResponseIdKey, &UserProfile::id_);
+  converter->RegisterNestedField<Name>(kApiResponseNameKey,
+                                       &UserProfile::name_);
+  converter->RegisterStringField(kApiResponseEmailAddressKey,
+                                 &UserProfile::email_address_);
+}
+
+// ----- Student -----
+
+// static
+void Student::RegisterJSONConverter(
+    base::JSONValueConverter<Student>* converter) {
+  converter->RegisterNestedField(kApiResponseProfileKey, &Student::profile_);
+}
+
+// ----- Students -----
+
+Students::Students() = default;
+
+Students::~Students() = default;
+
+// static
+void Students::RegisterJSONConverter(
+    base::JSONValueConverter<Students>* converter) {
+  converter->RegisterRepeatedMessage<Student>(kApiResponseStudentsKey,
+                                              &Students::items_);
+  converter->RegisterStringField(kApiResponseNextPageTokenKey,
+                                 &Students::next_page_token_);
+}
+
+// static
+std::unique_ptr<Students> Students::CreateFrom(const base::Value& value) {
+  auto students = std::make_unique<Students>();
+  base::JSONValueConverter<Students> converter;
+  if (!converter.Convert(value, students.get())) {
+    return nullptr;
+  }
+  return students;
+}
+
+}  // namespace google_apis::classroom
