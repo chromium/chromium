e110335bc05e607505afd5d7041b3ea881ba0101
Sven Zheng <svenzheng@google.com>
2024-06-10 20:34:38 +0000

    Use is_set() to replace deprecated isSet()
    To remove the warnings when using the tool.
    ./tools/bisect-builds.py:1052: DeprecationWarning: isSet() is deprecated, use is_set() instead
      if quit_event and quit_event.isSet():
    ./tools/bisect-builds.py:1054: DeprecationWarning: isSet() is deprecated, use is_set() instead
      if progress_event and progress_event.isSet():
    
    Bug: 344921695
    Test: Tested locally.
    Change-Id: I4cbb730521525bb52727854cef43b803e37f42dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5617990
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Auto-Submit: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313019}

--- tools/bisect-builds.py
+++ tools/bisect-builds.py
@@ -1049,9 +1049,9 @@ def FetchRevision(context, rev, filename, quit_event=None, progress_event=None):
                     displayed.
   """
   def ReportHook(blocknum, blocksize, totalsize):
-    if quit_event and quit_event.isSet():
+    if quit_event and quit_event.is_set():
       raise RuntimeError('Aborting download of revision %s' % str(rev))
-    if progress_event and progress_event.isSet():
+    if progress_event and progress_event.is_set():
       size = blocknum * blocksize
       if totalsize == -1:  # Total size not known.
         progress = 'Received %d bytes' % size
@@ -1068,7 +1068,7 @@ def FetchRevision(context, rev, filename, quit_event=None, progress_event=None):
       gsutil_download(download_url, filename)
     else:
       urllib.request.urlretrieve(download_url, filename, ReportHook)
-      if progress_event and progress_event.isSet():
+      if progress_event and progress_event.is_set():
         print()
   except RuntimeError:
     pass
