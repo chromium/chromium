chromium/chromium

commit cd845984ec250e76a0bd5106244ad869ae01fa12

[SurfacePolish] Fix bug about toolbar flips to white when navigating from NTP to webpage
When navigating from the NTP to a webpage, the toolbar color initially
changes to the default toolbar color during the loading phase. It will
change to the browser tab's color or stay unchanged after the loading
phase. The loading process occurs directly on the NTP. However, with the
Surface Polish enabled, the NTP's toolbar has a different color
than the default toolbar, and this results in a flip of white when
navigating from NTP to webpage, as shown in the
image:
https://drive.google.com/file/d/1ywogmzelW1OQwGdPCvrd0EndFkAbvCoM/
view?usp=sharing&resourcekey=0-XV6pRmumU3L4vy36HRSJ4w.
This CL aims to fix this problem.

When navigating from the NTP to a webpage, the ToolbarPhone#mVisualState
will immediately turn from VisualState.NEW_TAB_NORMAL or
VisualState.NEW_TAB_SEARCH_ENGINE_NO_LOGO to VisualState.NORMAL called
by function ToolbarPhone#onTabOrModelChanged. This is the starting point
of the loading phase. This method has edge cases related to Start
Surface and tab switcher, so we need to check the starting point is not
turned on when the current page is Start Surface or tab switcher.

The ending point of the loading phase is the time we know if we have a
website theme color. Due to the limitation that webpages lacking a
"theme-color" meta element will not display a theme color, subsequent
functions intended to change the theme color will not activate.
Moreover, we currently lack a reliable method to determine when all
metadata tags have finished processing. As a workaround, we update the
toolbar and location bar to the default color in the function
ToolbarPhone#onDidFirstVisuallyNonEmptyPaint, prior to the potential
activation of ToolbarPhone#onPrimaryColorChanged. This method has its
drawbacks, especially when handling webpages that has a brand color, as
it may result in a few frames displaying incorrect colors(default color)

chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -293,6 +293,10 @@ public class ToolbarPhone extends ToolbarLayout
     // page is Start Surface / NTP, to indicate that the appearance of the real search box changed.
     private boolean mIsStartOrNtpWithSurfacePolish;
 
+    // Added due to https://crbug.com/323888159 to mark the loading phase while navigating from NTP
+    // to webpages.
+    private boolean mIsInLoadingPhaseFromNtpToWebpage;
+
     // The following are some properties used during animation.  We use explicit property classes
     // to avoid the cost of reflection for each animation setup.
 
@@ -469,7 +473,7 @@ public class ToolbarPhone extends ToolbarLayout
      * @return The location bar color.
      */
     private @ColorInt int getLocationBarColorForToolbarColor(@ColorInt int toolbarColor) {
-        if (isLocationBarShownInGeneralNtpOrStartSurface()) {
+        if (isLocationBarShownInGeneralNtpOrStartSurface() || mIsInLoadingPhaseFromNtpToWebpage) {
             assert mHomeSurfaceLocationBarBackgroundColor != 0;
             return mHomeSurfaceLocationBarBackgroundColor;
         }
@@ -885,6 +889,9 @@ public class ToolbarPhone extends ToolbarLayout
             case VisualState.NEW_TAB_SEARCH_ENGINE_NO_LOGO:
                 return mHomeSurfaceToolbarBackgroundColor;
             case VisualState.NORMAL:
+                if (mIsInLoadingPhaseFromNtpToWebpage) {
+                    return mHomeSurfaceToolbarBackgroundColor;
+                }
                 return ChromeColors.getDefaultThemeColor(getContext(), false);
             case VisualState.INCOGNITO:
                 return ChromeColors.getDefaultThemeColor(getContext(), true);
@@ -1096,8 +1103,8 @@ public class ToolbarPhone extends ToolbarLayout
     }
 
     /**
-     * Updates the location bar layout, as the result of either a focus change or scrolling the
-     * New Tab Page.
+     * Updates the location bar layout, as the result of either a focus change or scrolling the New
+     * Tab Page.
      */
     private void updateLocationBarLayoutForExpansionAnimation() {
         TraceEvent.begin("ToolbarPhone.updateLocationBarLayoutForExpansionAnimation");
@@ -1202,7 +1209,9 @@ public class ToolbarPhone extends ToolbarLayout
             // change.
             if ((mLocationBar.getPhoneCoordinator().hasFocus() || !isLocationBarShownInNtp)
                     && mTabSwitcherState == STATIC_TAB) {
-                boolean usePolishedLocationBar = isLocationBarShownInGeneralNtpOrStartSurface();
+                boolean usePolishedLocationBar =
+                        isLocationBarShownInGeneralNtpOrStartSurface()
+                                || mIsInLoadingPhaseFromNtpToWebpage;
                 // Add a special case for general NTP and Start Surface to the defaultColor to
                 // ensure that the color is right and changes smoothly during the un-focus
                 // animation.
@@ -1732,8 +1741,8 @@ public class ToolbarPhone extends ToolbarLayout
     }
 
     /**
-     * @return Whether the location bar background should be drawn in
-     *         {@link #drawLocationBar(Canvas, long)}.
+     * @return Whether the location bar background should be drawn in {@link
+     *     #drawLocationBar(Canvas, long)}.
      */
     private boolean shouldDrawLocationBarBackground() {
         return (mLocationBar.getPhoneCoordinator().getAlpha() > 0
@@ -2099,6 +2108,11 @@ public class ToolbarPhone extends ToolbarLayout
         updateProgressBarVisibility();
         updateShadowVisibility();
         updateTabSwitcherButtonRipple();
+        if (mIsShowingStartSurfaceHomepage || mIsShowingStartSurfaceTabSwitcher) {
+            // Makes sure the loading phase from NTP to webpage is not turned on when showing Start
+            // Surface homepage or tab switcher.
+            mIsInLoadingPhaseFromNtpToWebpage = false;
+        }
         // Url bar should be focusable. This will be set in UrlBar#onDraw but there's a delay which
         // may cause focus to fail, so set here too. Only set to true if the GTS is NOT showing,
         // such as during the exit tab switcher event.
@@ -2425,6 +2439,23 @@ public class ToolbarPhone extends ToolbarLayout
     }
 
     @Override
+    public void onDidFirstVisuallyNonEmptyPaint() {
+        super.onDidFirstVisuallyNonEmptyPaint();
+        if (mIsInLoadingPhaseFromNtpToWebpage) {
+            // Updates toolbar and location bar's color to the default color when transitioning from
+            // NTP to other webpages before {@link ToolbarPhone#onPrimaryColorChanged(boolean)}
+            // is triggered. TODO(crbug.com/323888159): This code was created to deal with the
+            // case when the navigating webpage does not have the "theme-color" meta element or does
+            // not have its own brand color. In this situation, the function and {@link
+            // ToolbarPhone#onPrimaryColorChanged(boolean)} won't be triggered. This solution has
+            // limitations when dealing with webpages that use a brand color, as there may be a few
+            // frames with incorrect colors before the correct brand color is loaded. This code will
+            // be removed once a better solution is developed.
+            onPrimaryColorChanged(/* shouldAnimate= */ true);
+        }
+    }
+
+    @Override
     public void onTabOrModelChanged() {
         super.onTabOrModelChanged();
         updateNtpAnimationState();
@@ -2451,6 +2482,12 @@ public class ToolbarPhone extends ToolbarLayout
 
         final @ColorInt int initialLocationBarColor =
                 getLocationBarColorForToolbarColor(initialColor);
+
+        // When the webpage finishes loading during the NTP phase, the process should halt at this
+        // point because the tab's color is updated, and the initial color of the location bar is
+        // established for the upcoming navigation animation.
+        mIsInLoadingPhaseFromNtpToWebpage = false;
+
         final @ColorInt int finalLocationBarColor = getLocationBarColorForToolbarColor(finalColor);
 
         // Ignore theme color changes while the omnibox is focused, since we want a standard,
@@ -2749,6 +2786,8 @@ public class ToolbarPhone extends ToolbarLayout
             }
         }
 
+        startLoadingPhaseFromNtpToWebpage(newVisualState);
+
         mVisualState = newVisualState;
 
         // Refresh the toolbar texture.
@@ -2803,6 +2842,28 @@ public class ToolbarPhone extends ToolbarLayout
 
         getMenuButtonCoordinator().setVisibility(true);
         TraceEvent.end("ToolbarPhone.updateVisualsForLocationBarState");
+    }
+
+    /**
+     * Initiates the loading phase when transitioning from the NTP to a webpage. When the old visual
+     * state is either VisualState.NEW_TAB_NORMAL or VisualState.NEW_TAB_SEARCH_ENGINE_NO_LOGO, and
+     * the new visual state updates to VisualState.NORMAL, and the current page is neither the Start
+     * Surface nor the tab switcher, this indicates the beginning of the process of navigating from
+     * the New Tab Page to other webpages. This condition is unique because in other scenarios, the
+     * visual state does not change in this manner.
+     */
+    private void startLoadingPhaseFromNtpToWebpage(@VisualState int newVisualState) {
+        boolean isShowingStartSurfaceOrTabSwitcher =
+                mIsShowingStartSurfaceHomepage || mIsShowingStartSurfaceTabSwitcher;
+        boolean isStartLoadingPhaseFromNtpToWebpage =
+                (mVisualState == VisualState.NEW_TAB_NORMAL
+                                || mVisualState == VisualState.NEW_TAB_SEARCH_ENGINE_NO_LOGO)
+                        && newVisualState == VisualState.NORMAL;
+        if (ChromeFeatureList.sSurfacePolishForToolbarKillSwitch.isEnabled()
+                && isStartLoadingPhaseFromNtpToWebpage
+                && !isShowingStartSurfaceOrTabSwitcher) {
+            mIsInLoadingPhaseFromNtpToWebpage = true;
+        }
     }
 
     @Override

chrome/android/java/src/org/chromium/chrome/browser/compositor/layouts/LayoutManagerChrome.java
@@ -51,8 +51,8 @@ import java.util.List;
 import java.util.concurrent.Callable;
 
 /**
- * A {@link Layout} controller for the more complicated Chrome browser.  This is currently a
- * superset of {@link LayoutManagerImpl}.
+ * A {@link Layout} controller for the more complicated Chrome browser. This is currently a superset
+ * of {@link LayoutManagerImpl}.
  */
 public class LayoutManagerChrome extends LayoutManagerImpl
         implements ChromeAccessibilityUtil.Observer {
@@ -75,7 +75,7 @@ public class LayoutManagerChrome extends LayoutManagerImpl
     // Event Filter Handlers
     private final SwipeHandler mToolbarSwipeHandler;
 
-    /** Whether or not animations are enabled.  This can disable certain layouts or effects. */
+    /** Whether or not animations are enabled. This can disable certain layouts or effects. */
     private boolean mEnableAnimations = true;
 
     private LayoutStateObserver mTabSwitcherFocusLayoutStateObserver;
@@ -188,9 +188,10 @@ public class LayoutManagerChrome extends LayoutManagerImpl
 
     /**
      * Creates {@link org.chromium.chrome.features.tasks.tab_management.TabSwitcherLayout}
+     *
      * @param tabSwitcher An interface to talk to the Grid Tab Switcher.
      * @param browserControlsStateProvider The {@link BrowserControlsStateProvider} for top
-     *         controls.
+     *     controls.
      * @param scrimCoordinator scrim coordinator for GTS
      * @param tabSwitcherScrimAnchor scrim anchor view for GTS
      */
@@ -389,13 +390,13 @@ public class LayoutManagerChrome extends LayoutManagerImpl
         try {
             final ViewGroup containerView = mCreateTabSwitcherOrStartSurfaceCallable.call();
 
-                final TabSwitcher tabSwitcher = mTabSwitcherSupplier.get();
-                assert tabSwitcher != null;
-                createTabSwitcherLayout(
-                        tabSwitcher,
-                        mHost.getBrowserControlsManager(),
-                        mScrimCoordinator,
-                        containerView);
+            final TabSwitcher tabSwitcher = mTabSwitcherSupplier.get();
+            assert tabSwitcher != null;
+            createTabSwitcherLayout(
+                    tabSwitcher,
+                    mHost.getBrowserControlsManager(),
+                    mScrimCoordinator,
+                    containerView);
 
             if (DeviceFormFactor.isNonMultiDisplayContextOnTablet(mHost.getContext())) {
                 mThemeColorObserver =
@@ -533,7 +534,7 @@ public class LayoutManagerChrome extends LayoutManagerImpl
 
     /**
      * @param enabled Whether or not to allow model-reactive animations (tab creation, closing,
-     *                etc.).
+     *     etc.).
      */
     public void setEnableAnimations(boolean enabled) {
         mEnableAnimations = enabled;
@@ -554,6 +555,7 @@ public class LayoutManagerChrome extends LayoutManagerImpl
 
     /** A {@link SwipeHandler} meant to respond to edge events for the toolbar. */
     protected class ToolbarSwipeHandler implements SwipeHandler {
+
         /** The scroll direction of the current gesture. */
         private @ScrollDirection int mScrollDirection;
 
@@ -631,6 +633,7 @@ public class LayoutManagerChrome extends LayoutManagerImpl
 
         /**
          * Compute the direction of the scroll.
+         *
          * @param dx The distance traveled on the X axis.
          * @param dy The distance traveled on the Y axis.
          * @return The direction of the scroll.
@@ -669,7 +672,7 @@ public class LayoutManagerChrome extends LayoutManagerImpl
 
     /**
      * @param id The id of the {@link Tab} to search for.
-     * @return   A {@link Tab} instance or {@code null} if it could be found.
+     * @return A {@link Tab} instance or {@code null} if it could be found.
      */
     protected Tab getTabById(int id) {
         TabModelSelector selector = getTabModelSelector();

chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -201,7 +201,7 @@ import org.chromium.url.GURL;
 import java.util.List;
 
 /**
- * Contains logic for managing the toolbar visual component.  This class manages the interactions
+ * Contains logic for managing the toolbar visual component. This class manages the interactions
  * with the rest of the application to ensure the toolbar is always visually up to date.
  */
 public class ToolbarManager
@@ -1131,6 +1131,11 @@ public class ToolbarManager
                     public void onNavigationStateChanged() {
                         onBackPressStateChanged();
                     }
+
+                    @Override
+                    public void didFirstVisuallyNonEmptyPaint(Tab tab) {
+                        mToolbar.onDidFirstVisuallyNonEmptyPaint();
+                    }
                 };
 
         mTabModelSelectorObserver =
@@ -1358,6 +1363,7 @@ public class ToolbarManager
 
     /**
      * Set container view on which GTS toolbar needs to inflate.
+     *
      * @param containerView view containing GTS fullscreen toolbar.
      */
     public void setTabSwitcherFullScreenView(ViewGroup containerView) {
@@ -1368,6 +1374,7 @@ public class ToolbarManager
 
     /**
      * Handle a layout change event.
+     *
      * @param layoutType The layout being switched to.
      */
     private void updateForLayout(@LayoutType int layoutType) {
@@ -1874,6 +1881,7 @@ public class ToolbarManager
 
     /**
      * Adds a custom action button to the {@link Toolbar}, if it is supported.
+     *
      * @param drawable The {@link Drawable} to use as the background for the button.
      * @param description The content description for the custom action button.
      * @param listener The {@link OnClickListener} to use for clicks to the button.
@@ -2648,6 +2656,7 @@ public class ToolbarManager
 
     /**
      * Sets the top margin for the control container.
+     *
      * @param margin The margin in pixels.
      */
     private void setControlContainerTopMargin(int margin) {

chrome/browser/about_flags.cc
@@ -7056,6 +7056,12 @@ const FeatureEntry kFeatureEntries[] = {
                                     kSurfacePolishVariations,
                                     "SurfacePolish")},
 
+    {"enable-surface-polish-for-toolbar-kill-switch",
+     flag_descriptions::kSurfacePolishForToolbarKillSwitchName,
+     flag_descriptions::kSurfacePolishForToolbarKillSwitchDescription,
+     kOsAndroid,
+     FEATURE_VALUE_TYPE(chrome::android::kSurfacePolishForToolbarKillSwitch)},
+
     {"enable-magic-stack-android", flag_descriptions::kMagicStackAndroidName,
      flag_descriptions::kMagicStackAndroidDescription, kOsAndroid,
      FEATURE_WITH_PARAMS_VALUE_TYPE(chrome::android::kMagicStackAndroid,

chrome/browser/flag_descriptions.cc
@@ -4494,6 +4494,11 @@ const char kSurfacePolishName[] = "Surface Polish";
 const char kSurfacePolishDescription[] =
     "Enable clank home surface polish for Start surface and NTP.";
 
+const char kSurfacePolishForToolbarKillSwitchName[] =
+    "Surface Polish For Toolbar Kill Switch";
+const char kSurfacePolishForToolbarKillSwitchDescription[] =
+    "Enable clank home surface polish for toolbar, this is a kill switch.";
+
 const char kTabletToolbarReorderingAndroidName[] =
     "Tablet Toolbar Reordering Android.";
 const char kTabletToolbarReorderingAndroidDescription[] =

chrome/browser/flag_descriptions.h
@@ -2662,6 +2662,9 @@ extern const char kStrictSiteIsolationDescription[];
 extern const char kSurfacePolishName[];
 extern const char kSurfacePolishDescription[];
 
+extern const char kSurfacePolishForToolbarKillSwitchName[];
+extern const char kSurfacePolishForToolbarKillSwitchDescription[];
+
 extern const char kTabGroupPaneAndroidName[];
 extern const char kTabGroupPaneAndroidDescription[];
 

chrome/browser/flags/android/chrome_feature_list.cc
@@ -292,6 +292,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kStartSurfaceReturnTime,
     &kAccountReauthenticationRecentTimeWindow,
     &kSurfacePolish,
+    &kSurfacePolishForToolbarKillSwitch,
     &kUmaBackgroundSessions,
     &kUseLibunwindstackNativeUnwinderAndroid,
     &kVerticalAutomotiveBackButtonToolbar,
@@ -946,6 +947,10 @@ BASE_FEATURE(kAccountReauthenticationRecentTimeWindow,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSurfacePolish, "SurfacePolish", base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kSurfacePolishForToolbarKillSwitch,
+             "SurfacePolishForToolbarKillSwitch",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTabResumptionModuleAndroid,
              "TabResumptionModuleAndroid",

chrome/browser/flags/android/chrome_feature_list.h
@@ -170,6 +170,7 @@ BASE_DECLARE_FEATURE(kStartSurfaceAndroid);
 BASE_DECLARE_FEATURE(kStartSurfaceReturnTime);
 BASE_DECLARE_FEATURE(kAccountReauthenticationRecentTimeWindow);
 BASE_DECLARE_FEATURE(kSurfacePolish);
+BASE_DECLARE_FEATURE(kSurfacePolishForToolbarKillSwitch);
 BASE_DECLARE_FEATURE(kTabResumptionModuleAndroid);
 BASE_DECLARE_FEATURE(kUmaBackgroundSessions);
 BASE_DECLARE_FEATURE(kUseLibunwindstackNativeUnwinderAndroid);

commit 3a71266293216a208dfe3ebceb5954351d4f23fb

[SurfacePolish] Fix incorrect toolbar color when navigating from single tab card to tabs without brand color.
This CL addresses a issue introduced by CL 5506213
(https://chromium-review.googlesource.com/c/chromium/src/+/5506213)
where the toolbar color failed to update when navigating from a
single tab card to tabs without brand color (see screenshot:
https://drive.google.com/file/d/1EdGTVQ0CKylSW2EGtUW-aVHNK6Gqd7tn/view?
usp=sharing&resourcekey=0-Uqnsiq6KA4AJK36JOQy4cQ).
The toolbar color incorrectly persisted as the NTP background color
because the visual state of the last visited tab is
VisualState.NORMAL, causing the mIsInLoadingPhaseFromNtpToWebpage
variable to be incorrectly set to true.

This CL implements a verification to ensure navigation occurs within the NTP. The expected behavior after this change is demonstrated in the following screenshot:
https://drive.google.com/file/d/1sEAR_URdeUqTk5jKcW22bJi-IsrUJW-0/view?
usp=sharing&resourcekey=0-dunJFlw1XOVpwNnB7_T6JQ.

Bug: 323888159
Change-Id: Idd7ec7d2c0603613fb7dc89ec631ec6a806eadb4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597981
Reviewed-by: Sky Malice <skym@chromium.org>
Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
Reviewed-by: Theresa Sullivan <twellington@chromium.org>
Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1313174}

chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -2108,9 +2108,9 @@ public class ToolbarPhone extends ToolbarLayout
         updateProgressBarVisibility();
         updateShadowVisibility();
         updateTabSwitcherButtonRipple();
-        if (mIsShowingStartSurfaceHomepage || mIsShowingStartSurfaceTabSwitcher) {
+        if (mIsShowingStartSurfaceHomepage) {
             // Makes sure the loading phase from NTP to webpage is not turned on when showing Start
-            // Surface homepage or tab switcher.
+            // Surface homepage.
             mIsInLoadingPhaseFromNtpToWebpage = false;
         }
         // Url bar should be focusable. This will be set in UrlBar#onDraw but there's a delay which
@@ -2864,21 +2864,21 @@ public class ToolbarPhone extends ToolbarLayout
     /**
      * Initiates the loading phase when transitioning from the NTP to a webpage. When the old visual
      * state is either VisualState.NEW_TAB_NORMAL or VisualState.NEW_TAB_SEARCH_ENGINE_NO_LOGO, and
-     * the new visual state updates to VisualState.NORMAL, and the current page is neither the Start
-     * Surface nor the tab switcher, this indicates the beginning of the process of navigating from
-     * the New Tab Page to other webpages. This condition is unique because in other scenarios, the
-     * visual state does not change in this manner.
+     * the new visual state updates to VisualState.NORMAL, and the current page is NTP instead of
+     * Start Surface, this indicates the beginning of the process of navigating from the New Tab
+     * Page to other webpages. This condition is unique because in other scenarios, the visual state
+     * does not change in this manner.
      */
     private void startLoadingPhaseFromNtpToWebpage(@VisualState int newVisualState) {
-        boolean isShowingStartSurfaceOrTabSwitcher =
-                mIsShowingStartSurfaceHomepage || mIsShowingStartSurfaceTabSwitcher;
         boolean isStartLoadingPhaseFromNtpToWebpage =
                 (mVisualState == VisualState.NEW_TAB_NORMAL
                                 || mVisualState == VisualState.NEW_TAB_SEARCH_ENGINE_NO_LOGO)
                         && newVisualState == VisualState.NORMAL;
+        boolean hasVisibleNtp = getToolbarDataProvider().getNewTabPageDelegate().wasShowingNtp();
         if (ChromeFeatureList.sSurfacePolishForToolbarKillSwitch.isEnabled()
                 && isStartLoadingPhaseFromNtpToWebpage
-                && !isShowingStartSurfaceOrTabSwitcher) {
+                && !mIsShowingStartSurfaceHomepage
+                && hasVisibleNtp) {
             mIsInLoadingPhaseFromNtpToWebpage = true;
         }
     }