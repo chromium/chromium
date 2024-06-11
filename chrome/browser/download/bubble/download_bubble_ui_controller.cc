64848f934ef7d9d2623c3af8c8184031bf67885f
Daniel Rubery <drubery@chromium.org>
2024-06-11 18:04:07 +0000

    Expire Download.Bubble.PartialToFullViewLatency
    This was added for the initial launch of the download bubble, but does
    not seem generally useful long-term.
    
    OBSOLETE_HISTOGRAMS=Removed due to lack of use.
    Fixed: 346342558
    
    Change-Id: If403be4ae1556076a6d5c8e646ecd43fd3cd10b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622930
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313548}

--- chrome/browser/download/bubble/download_bubble_ui_controller.cc
+++ chrome/browser/download/bubble/download_bubble_ui_controller.cc
@@ -220,12 +220,7 @@ std::vector<DownloadUIModelPtr> DownloadBubbleUIController::GetDownloadUIModels(
 }
 
 std::vector<DownloadUIModelPtr> DownloadBubbleUIController::GetMainView() {
-  if (last_partial_view_shown_time_.has_value()) {
-    base::UmaHistogramLongTimes(
-        "Download.Bubble.PartialToFullViewLatency",
-        base::Time::Now() - (*last_partial_view_shown_time_));
-    last_partial_view_shown_time_ = std::nullopt;
-  }
+  last_partial_view_shown_time_ = std::nullopt;
   last_primary_view_was_partial_ = false;
   std::vector<DownloadUIModelPtr> list =
       GetDownloadUIModels(/*is_main_view=*/true);
