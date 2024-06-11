293368b87cefca7e314a38969a4d655ee03d1242
Gabriel Tellez Ornelas <gtellezornelas@google.com>
2024-06-11 13:01:16 +0000

    Configure lit and implement rudimentary element
    This configures lit for WebUI development and adds a rudimentary element to expand on in later changes
    
    Bug: 330877132
    Change-Id: I4da78c2d410acf9ad85e5a8ef376b4b13bd3f7d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604904
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Gabriel Tellez Ornelas <gtellezornelas@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313353}

---
+++ chrome/browser/resources/privacy_sandbox/internals/related_website_sets/app.ts
@@ -0,0 +1,51 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '//resources/cr_elements/cr_input/cr_input.js';
+
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+
+import {getCss} from './app.css.js';
+import {getHtml} from './app.html.js';
+
+export class RelatedWebsiteSetsAppElement extends CrLitElement {
+  static get is() {
+    return 'related-website-sets-app';
+  }
+
+  static override get styles() {
+    return getCss();
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
+    return {
+      disabled: {
+        type: Boolean,
+        reflect: true,
+      },
+      myValue: {type: String},
+    };
+  }
+
+  disabled: boolean = false;
+  myValue: string = 'This element is made in lit!';
+
+  // Referenced from the template, so must be protected (not private).
+  protected onInputValueChanged_(e: CustomEvent<{value: string}>) {
+    this.myValue = e.detail.value;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'related-website-sets-app': RelatedWebsiteSetsAppElement;
+  }
+}
+
+customElements.define(
+    RelatedWebsiteSetsAppElement.is, RelatedWebsiteSetsAppElement);
