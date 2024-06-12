e049586515e000258d6ad560b9ba35e0c4273d8b
Evan Stade <estade@chromium.org>
2024-06-12 17:08:35 +0000

    IDB: add debug code to help trace down source of missing large value
    Bug: 342779913
    Change-Id: I53b4708c5441847bfc024eeab9f03f6478a0b3a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622697
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314084}

--- third_party/blink/renderer/modules/indexeddb/idb_request_loader.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_request_loader.cc
@@ -6,6 +6,9 @@
 
 #include <algorithm>
 
+#include "base/debug/dump_without_crashing.h"
+#include "base/debug/stack_trace.h"
+#include "components/crash/core/common/crash_key.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -136,6 +139,17 @@ void IDBRequestLoader::DidFail(FileErrorCode) {
   DCHECK(file_reader_loading_);
   file_reader_loading_ = false;
 #endif  // DCHECK_IS_ON()
+
+  // TODO(https://crbug.com/3342779913): fix bug and remove this debug code.
+  {
+    static crash_reporter::CrashKeyString<1024> trace_key(
+        "crbug/3342779913/stack");
+    crash_reporter::SetCrashKeyStringToStackTrace(&trace_key,
+                                                  base::debug::StackTrace());
+    base::debug::DumpWithoutCrashing();
+    trace_key.Clear();
+  }
+
   OnLoadComplete(/*error=*/true);
 }
 
