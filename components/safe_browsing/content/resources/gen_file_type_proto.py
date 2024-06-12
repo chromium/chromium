c8fc09d4aa374bb8b9fed980eecf3e0d9d601e9a
Daniel Rubery <drubery@chromium.org>
2024-06-12 22:16:29 +0000

    Explicitly specify output files for make_all_file_types_protobuf
    Right now, we specify an output directory in GN. This causes problems
    with cleandead if this target becomes unused (see dependent
    CL). cleandead expects the outputs to be a list of files. By adding
    version computation to push_file_type_proto.py, we can copy files to the
    appropriate directory in the GCS bucket while also having the known
    paths on disk.
    
    NO_IFTTT=Newly added block, tracked by IFTTT
    
    Bug: 340848922, 40222986
    Change-Id: I9423182f0cfad6c2bac6d779e5cc0a5e0e0bafc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5620894
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314298}

--- components/safe_browsing/content/resources/gen_file_type_proto.py
+++ components/safe_browsing/content/resources/gen_file_type_proto.py
@@ -31,6 +31,7 @@ from binary_proto_generator import BinaryProtoGenerator
 #   value: proto-platform_type (int)
 def PlatformTypes():
     return {
+        # LINT.IfChange(PlatformTypes)
         "android":
         download_file_types_pb2.DownloadFileType.PLATFORM_TYPE_ANDROID,
         "chromeos":
@@ -40,6 +41,7 @@ def PlatformTypes():
         "linux": download_file_types_pb2.DownloadFileType.PLATFORM_TYPE_LINUX,
         "mac": download_file_types_pb2.DownloadFileType.PLATFORM_TYPE_MAC,
         "win": download_file_types_pb2.DownloadFileType.PLATFORM_TYPE_WINDOWS,
+        # LINT.ThenChange(BUILD.gn:PlatformTypes)
     }
 
 
@@ -163,8 +165,8 @@ class DownloadFileTypeProtoGenerator(BinaryProtoGenerator):
         else:
             # Make a separate file for each platform
             for platform_type, platform_enum in PlatformTypes().items():
-                # e.g. .../all/77/chromeos/download_file_types.pb
-                outfile = os.path.join(opts.outdir, str(pb.version_id),
+                # e.g. .../all/chromeos/download_file_types.pb
+                outfile = os.path.join(opts.outdir,
                                        platform_type, opts.outbasename)
                 MakeSubDirs(outfile)
                 FilterForPlatformAndWrite(pb, platform_enum, outfile)
@@ -174,9 +176,7 @@ class DownloadFileTypeProtoGenerator(BinaryProtoGenerator):
                           '--all',
                           action="store_true",
                           default=False,
-                          help='Write a separate file for every platform. '
-                          'Outfile must have a %d for version and %s for '
-                          'platform.')
+                          help='Write a separate file for every platform.')
         parser.add_option(
             '-t',
             '--type',
