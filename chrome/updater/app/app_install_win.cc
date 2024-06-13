b09029d6b3c7d518cf2e09e4b836f5ae5c0bdf5f
Sorin Jianu <sorin@chromium.org>
2024-06-13 04:35:29 +0000

    updater: alias stack variables in AppInstallControllerImpl::StateChange
    This is intended to facilitate debug the failing CHECK.
    
    Additional logging can't be added because this function gets called
    a lot.
    
    This code is covered by the end-to-end integration tests for
    Windows.
    
    Bug: 345250525
    Change-Id: I74fa1f76648bb8cb2c4e7a49a1e2cfd37b5ceac8
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626925
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314430}

--- chrome/updater/app/app_install_win.cc
+++ chrome/updater/app/app_install_win.cc
@@ -21,6 +21,7 @@
 
 #include "base/check.h"
 #include "base/check_op.h"
+#include "base/debug/alias.h"
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "base/json/json_string_value_serializer.h"
@@ -712,8 +713,16 @@ void AppInstallControllerImpl::InstallComplete(UpdateService::Result result) {
 void AppInstallControllerImpl::StateChange(
     const UpdateService::UpdateState& update_state) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  AppInstallProgressIPC* dbg_ipc = install_progress_observer_ipc_.get();
+  base::debug::Alias(&dbg_ipc);
   CHECK(install_progress_observer_ipc_);
 
+  // crbug.com/345250525 - understand why the check fails.
+  UpdateService::UpdateState::State state = update_state.state;
+  base::debug::Alias(&state);
+  DEBUG_ALIAS_FOR_CSTR(dbg_app_id1, app_id_.c_str(), 64);
+  DEBUG_ALIAS_FOR_CSTR(dbg_app_id2, update_state.app_id.c_str(), 64);
   CHECK_EQ(app_id_, update_state.app_id);
 
   switch (update_state.state) {
