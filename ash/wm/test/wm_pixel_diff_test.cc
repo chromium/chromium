92e3a3c2d6d0d0cf762a20a6c8944a874899d8c6
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-06-13 23:19:56 +0000

    Do not snap rounded corners bounds to pixel boundary
    In crrev.com/c/4726545, to fix pixel alignment of content and
    non-content areas of Lacros, rounded corner bounds were snapped to pixel
    boundary using `ToEnclosingRect()`.
    
    However, it caused 1px clipping of certain Ash UI elements based for
    various device scale factors. It is inconsistent to snap the rounded
    rect in cc but keep the sub pixel alignment in other places. [1][2][3]
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/viz/service/display/skia_renderer.cc;l=1926;drc=07e391877cb258f178c27de5308dd3d09fca725e
    [2] https://source.chromium.org/chromium/chromium/src/+/main:components/viz/service/display/surface_aggregator.cc;l=64;drc=07e391877cb258f178c27de5308dd3d09fca725e
    [3] https://source.chromium.org/chromium/chromium/src/+/main:components/viz/service/display/skia_renderer.cc;l=4038;drc=07e391877cb258f178c27de5308dd3d09fca725e
    
    If lacros requires us to snap its rounded corners bounds, it can be done
    in exo. (Will be done in followup CL)
    
    Bug: b/329864176, b/40267343
    Change-Id: I58d52e885ebe0aa22a62de5fdc0eceecf5b47c8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606007
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314948}

--- ash/wm/test/wm_pixel_diff_test.cc
+++ ash/wm/test/wm_pixel_diff_test.cc
@@ -74,7 +74,7 @@ TEST_F(WmPixelDiffTest, OverviewAndDesksBarBasic) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "overview_and_desks_bar_basic",
-      /*revision_number=*/13, desk_widget, overview_widget1, overview_widget2,
+      /*revision_number=*/14, desk_widget, overview_widget1, overview_widget2,
       overview_widget3));
 }
 
--- cc/trees/draw_property_utils.cc
+++ cc/trees/draw_property_utils.cc
@@ -815,14 +815,10 @@ std::pair<gfx::MaskFilterInfo, bool> GetMaskFilterInfoPair(
   if (!property_trees->GetToTarget(node->transform_id, target_id, &to_target))
     return kEmptyMaskFilterInfoPair;
 
-  auto mfi = node->mask_filter_info;
-  mfi.ApplyTransform(to_target);
+  auto result =
+      std::make_pair(node->mask_filter_info, node->is_fast_rounded_corner);
+  result.first.ApplyTransform(to_target);
 
-  auto rrect_f = gfx::RRectF::ToEnclosingRRectF(mfi.rounded_corner_bounds());
-  auto result = std::make_pair(
-      gfx::MaskFilterInfo(rrect_f,
-                          mfi.gradient_mask().value_or(gfx::LinearGradient())),
-      node->is_fast_rounded_corner);
   if (result.first.IsEmpty()) {
     return kEmptyMaskFilterInfoPair;
   }
