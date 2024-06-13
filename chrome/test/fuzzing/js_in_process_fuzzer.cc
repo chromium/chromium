7da6c45184f1442845695bbf8e5832d6ea78f1da
Paul Semel <paulsemel@chromium.org>
2024-06-13 07:54:39 +0000

    fuzzing: integrate fuzzilli into chrome
    This adds support for fuzzing Chrome with Fuzzilli. As for now, this is
    still experimental.
    
    Bug: 346608861
    Change-Id: I5eb2cf37835ae79df4ee75b6ecf9183c3f3a2cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605371
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Auto-Submit: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314477}

--- chrome/test/fuzzing/js_in_process_fuzzer.cc
+++ chrome/test/fuzzing/js_in_process_fuzzer.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <optional>
 #include <string_view>
 
 #include "base/strings/escape.h"
@@ -11,6 +12,7 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chrome/test/fuzzing/in_process_fuzzer.h"
+#include "chrome/test/fuzzing/in_process_fuzzer_buildflags.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/browser_test_utils.h"
 
@@ -24,8 +26,9 @@
 
 class JsInProcessFuzzer : public InProcessFuzzer {
  public:
-  JsInProcessFuzzer() = default;
+  JsInProcessFuzzer();
   void SetUpOnMainThread() override;
+  base::CommandLine::StringVector GetChromiumCommandLineArguments() override;
 
   int Fuzz(const uint8_t* data, size_t size) override;
 };
@@ -38,12 +41,30 @@ namespace {
 // fuzzer. Empirically, valid JS cases complete locally well within 2
 // seconds so allow 8 seconds to account for slowness on fuzzing
 // infrastructure.
-constexpr base::TimeDelta kJsExecutionTimeout = base::Seconds(8);
+
+#if BUILDFLAG(IS_FUZZILLI)
+// Fuzzilli handles timeouts by itself so that it detects when there are
+// infinite loops.
+constexpr std::optional<base::TimeDelta> kJsExecutionTimeout = std::nullopt;
+constexpr RunLoopTimeoutBehavior kJsRunLoopTimeoutBehavior =
+    RunLoopTimeoutBehavior::kDefault;
+#else
+constexpr std::optional<base::TimeDelta> kJsExecutionTimeout = base::Seconds(8);
+constexpr RunLoopTimeoutBehavior kJsRunLoopTimeoutBehavior =
+    RunLoopTimeoutBehavior::kDeclareInfiniteLoop;
+#endif
+
 constexpr std::string_view kBlankHtmlPage =
     "<html><head><title>Test page</title></head>"
     "<body><p>Test text.</p></body></html>";
 
 }  // namespace
+
+JsInProcessFuzzer::JsInProcessFuzzer()
+    : InProcessFuzzer(InProcessFuzzerOptions{
+          .run_loop_timeout_behavior = kJsRunLoopTimeoutBehavior,
+          .run_loop_timeout = kJsExecutionTimeout,
+      }) {}
 
 void JsInProcessFuzzer::SetUpOnMainThread() {
   InProcessFuzzer::SetUpOnMainThread();
@@ -51,55 +72,42 @@ void JsInProcessFuzzer::SetUpOnMainThread() {
   const bool kUsePlus = false;
   url_string.append(base::EscapeQueryParamValue(kBlankHtmlPage, kUsePlus));
   CHECK(ui_test_utils::NavigateToURL(browser(), GURL(url_string)));
+#if BUILDFLAG(IS_FUZZILLI)
+  // Fuzzilli needs to see this. Unfortunately, we install a signal handler at
+  // //content/public/test/browser_test_base.cc that exits when one of those
+  // signals occur. Disabling it allows for Fuzzilli to see them.
+  signal(SIGSEGV, SIG_DFL);
+  signal(SIGTERM, SIG_DFL);
+#endif
+}
+
+base::CommandLine::StringVector
+JsInProcessFuzzer::GetChromiumCommandLineArguments() {
+  return {
+      FILE_PATH_LITERAL("--js-flags='--jit-fuzzing --allow-natives-syntax "
+                        "--expose-gc --fuzzing --future --harmony'")};
 }
 
 int JsInProcessFuzzer::Fuzz(const uint8_t* data, size_t size) {
   std::string_view js_str(reinterpret_cast<const char*>(data), size);
-  base::RunLoop run_loop;
-  base::OneShotTimer timer;
-  bool valid_input = true;
-  base::RepeatingCallback<void()> run_fuzz_case_lambda =
-      base::BindLambdaForTesting([&]() {
-        std::u16string js_str16 = base::UTF8ToUTF16(std::string_view(js_str));
-        // End the run loop either when the JS finishes or 2 seconds expires
-        timer.Start(FROM_HERE, kJsExecutionTimeout,
-                    base::BindLambdaForTesting([&]() {
-                      // If we hit a timeout executing this test case, it's
-                      // probably a JavaScript infinite loop. If we leave it for
-                      // 30+ seconds then the RunLoop will crash, and the fuzzer
-                      // engine will regard that as a failure. Instead, we exit
-                      // the entire browser. Centipede will assume that the
-                      // problem is that the shared memory isn't big enough.
-                      // This is obviously a little inefficient, but it's
-                      // unclear that there's a better way to do this. If we
-                      // didn't run these fuzzers in --single-process mode then
-                      // we could Shutdown() the RenderProcessHost, but that's
-                      // not an option right now.
-                      LOG(INFO) << "Timeout hit, exiting the whole browser on "
-                                   "next iteration.";
-                      DeclareInfiniteLoop();
-                      valid_input = false;
-                      run_loop.Quit();
-                    }));
-        content::WebContents* contents =
-            browser()->tab_strip_model()->GetActiveWebContents();
-        content::RenderFrameHost* rfh = contents->GetPrimaryMainFrame();
-        // Execute JS within an existing page - that's the essence of this
-        // fuzzer and why it's much quicker than html_in_process_fuzzer
-        // or page_load_in_process_fuzzer.
-        // We use this asynchronous function rather than (for instance)
-        // content::ExecJs so that we can apply the timeout above.
-        // We use the "WithUserGesture" version so that this can
-        // in theory explore APIs which are gated behind that restriction
-        // (subject to future developments with dictionaries, corpora, etc.)
-        rfh->ExecuteJavaScriptWithUserGestureForTests(
-            js_str16, base::BindLambdaForTesting([&](base::Value) {
-              timer.Stop();
-              run_loop.Quit();
-            }));
-      });
-  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
-      FROM_HERE, run_fuzz_case_lambda);
-  run_loop.Run();
-  return valid_input ? 0 : -1;
+  content::WebContents* contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  content::RenderFrameHost* rfh = contents->GetPrimaryMainFrame();
+  // Execute JS within an existing page - that's the essence of this
+  // fuzzer and why it's much quicker than html_in_process_fuzzer
+  // or page_load_in_process_fuzzer.
+  // We use a synchronous function because we set the InProcessFuzzer's
+  // kDeclareInfiniteLoop behaviour in case of infinite loops.
+  // We invoke ExecJs with default options, which turns on user gestures in JS
+  // so that this can in theory explore APIs which are gated behind that
+  // restriction (subject to future developments with dictionaries, corpora,
+  // etc.)
+  testing::AssertionResult res = content::ExecJs(rfh, js_str);
+#if BUILDFLAG(IS_FUZZILLI)
+  // Fuzzilli needs to know when an exception was uncaught.
+  if (!res) {
+    return -1;
+  }
+#endif
+  return 0;
 }
