d158e26c30ba10d2520b984ba3e068ab0485e242
Gabriel Tellez Ornelas <gtellezornelas@google.com>
2024-06-14 11:54:38 +0000

    Develop Toolbar component for Related Website Sets Dev UI
    This sets up the toolbar component using webUi gallery and lit
    
    Light mode:
    https://screenshot.googleplex.com/4AxC6ZEeoTmbpAE.png
    https://screenshot.googleplex.com/3wx4mesJEwEWTmf.png
    
    Dark mode:
    
    https: //screenshot.googleplex.com/9z2h88vf9UrWQFE.png
    https: //screenshot.googleplex.com/4rBNt42txtPgowP.png
    Bug: 330877132
    Change-Id: Ib15af9fcb1778cab36d783a3bcacd8dbfba39b4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5617710
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Gabriel Tellez Ornelas <gtellezornelas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1315143}

--- chrome/browser/resources/privacy_sandbox/internals/related_website_sets/app.html.ts
+++ chrome/browser/resources/privacy_sandbox/internals/related_website_sets/app.html.ts
@@ -8,9 +8,9 @@ import type {RelatedWebsiteSetsAppElement} from './app.js';
 
 export function getHtml(this: RelatedWebsiteSetsAppElement) {
   return html`
-<div>Input something</div>
-<cr-input id="input" .value="${this.myValue}"
-    ?disabled="${this.disabled}"
-    @value-changed="${this.onInputValueChanged_}">
-</cr-input>`;
+<related-website-sets-toolbar
+  id="toolbar"
+  .pageName="${this.pageTitle_}"
+  .narrow="${this.narrow_}">
+</related-website-sets-toolbar>`;
 }
