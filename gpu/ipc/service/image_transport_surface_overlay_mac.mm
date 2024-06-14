39dd592ae4ca58ea98263802a177c88c86f0d519
Maggie Chen <magchen@chromium.org>
2024-06-14 18:26:13 +0000

    Enable NewPresentationFeedbackTimeStamps by default
    In preparation for 100% stable launch, enable the feature by default.
    
    Bug: 324458376,b/338297503
    Change-Id: I4f99928af371223bbb7e78121e9362d68023cfdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626667
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Auto-Submit: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315350}

--- gpu/ipc/service/image_transport_surface_overlay_mac.mm
+++ gpu/ipc/service/image_transport_surface_overlay_mac.mm
@@ -46,7 +46,7 @@ BASE_FEATURE(kAVFoundationOverlays,
 // Use CVDisplayLink timing for PresentationFeedback timestamps.
 BASE_FEATURE(kNewPresentationFeedbackTimeStamps,
              "NewPresentationFeedbackTimeStamps",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_MAC)
 }  // namespace
 
