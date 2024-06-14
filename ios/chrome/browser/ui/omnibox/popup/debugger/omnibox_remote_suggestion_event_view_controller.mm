db0951ac9307049e8982fa2f82577ff5b8b1a61f
Christian Xu <christianxu@google.com>
2024-06-14 12:43:31 +0000

    [iOS][Omnibox][Debugger] Add copy button for response body
    Add a copy button for remote suggestion service response body.
    
    Bug: None
    Change-Id: I5dfc08b5857e421c028d3c0276b35f7ab9998e8d
    Low-Coverage-Reason: EXPERIMENTAL_CODE Debugger code
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630397
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315153}

--- ios/chrome/browser/ui/omnibox/popup/debugger/omnibox_remote_suggestion_event_view_controller.mm
+++ ios/chrome/browser/ui/omnibox/popup/debugger/omnibox_remote_suggestion_event_view_controller.mm
@@ -4,6 +4,7 @@
 
 #import "ios/chrome/browser/ui/omnibox/popup/debugger/omnibox_remote_suggestion_event_view_controller.h"
 
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/ui/omnibox/popup/debugger/omnibox_remote_suggestion_event.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 
@@ -12,6 +13,14 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.view.backgroundColor = UIColor.systemBackgroundColor;
+
+  UIButton* copyButton = [UIButton
+      systemButtonWithImage:DefaultSymbolWithPointSize(kCopyActionSymbol,
+                                                       kSymbolActionPointSize)
+                     target:self
+                     action:@selector(didTapCopyButton)];
+  copyButton.translatesAutoresizingMaskIntoConstraints = NO;
+  [copyButton setTitle:@"Copy" forState:UIControlStateNormal];
 
   UILabel* requestLabel = [[UILabel alloc] init];
   requestLabel.translatesAutoresizingMaskIntoConstraints = NO;
@@ -24,7 +33,7 @@
   responseLabel.numberOfLines = 0;
 
   UIStackView* stackView = [[UIStackView alloc]
-      initWithArrangedSubviews:@[ requestLabel, responseLabel ]];
+      initWithArrangedSubviews:@[ copyButton, requestLabel, responseLabel ]];
   stackView.translatesAutoresizingMaskIntoConstraints = NO;
   stackView.axis = UILayoutConstraintAxisVertical;
 
@@ -40,6 +49,11 @@
   [NSLayoutConstraint activateConstraints:@[
     [stackView.widthAnchor constraintEqualToAnchor:scrollView.widthAnchor]
   ]];
+}
+
+- (void)didTapCopyButton {
+  UIPasteboard.generalPasteboard.string =
+      [self prettifyJsonString:self.event.responseBody];
 }
 
 - (NSString*)prettifyJsonString:(NSString*)jsonString {
