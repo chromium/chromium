9e8c0601e356d72797e5c719618d91472383cda0
Shakti Sahu <shaktisahu@chromium.org>
2024-06-14 16:58:06 +0000

    [Tab Group Sync] Add support for GetDeletedGroupIds using prefs
    This CL adds back the support for caching deleted group IDs which
    was removed during migration of local IDs from Shared Prefs based
    store to native ModelTypeStore. In general, when the client
    receives a group deletion from sync, the UI may or may not be
    available to process the deletion and close out the group from
    local tab model. Hence some sort of storage is required to
    store the deleted group IDs. In this CL, we are using PrefService
    to store the deleted group IDs. Note that the group entry is
    gone from sync, and ModelTypeStore, only available in the pref
    to be cleared out either immediately if the UI is available to
    close the group, or on next restart.
    
    Bug: 346962799
    Change-Id: I16a52842e8fac1d2ef1e0dd5fae4bea0fc4e8351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5632396
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315293}

--- components/saved_tab_groups/pref_names.h
+++ components/saved_tab_groups/pref_names.h
@@ -38,6 +38,15 @@ inline constexpr char kAutoPinNewTabGroups[] = "auto_pin_new_tab_groups";
 inline constexpr char kSavedTabGroupSpecificsToDataMigration[] =
     "saved_tab_groups.specifics_to_data_migration";
 
+// The pref that stores the deleted group IDs that needs to be closed in the
+// local tab model. Stores a dictionary of local ID -> sync ID. Local ID is the
+// main thing that the tab model needs. On startup, UI queries the deleted IDs
+// and closes them up. Sync ID isn't really needed since it's already removed
+// from sync and the model, but kept for use in case we find a future use for
+// it. When the group is closed, the ID will be deleted from this pref.
+inline constexpr char kDeletedTabGroupIds[] =
+    "saved_tab_groups.deleted_group_ids";
+
 // Registers the Clear Browsing Data UI prefs.
 void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
 
