5127468a6b5bbfd8556e90f3fa630018d474bd18
Yingying Ma <yingying.ma@intel.com>
2024-06-14 07:30:13 +0000

    Reland "Pack sequence header OBU and frame OBU for AV1"
    This is a reland of commit 21b70a08871847a67c0041560e59f266159bd943
    
    Original change's description:
    > Pack sequence header OBU and frame OBU for AV1
    >
    > Implement BuildSequenceHeaderOBU() and BuildFrameHeaderOBU() for
    > AV1 and replace VA-API code with them.
    >
    > TEST: build media_unittests, then execute it with --gtest_filter=AV1BuilderTest.*
    >
    > Bug: 40275246
    >
    > Change-Id: Iea03725a33e503777d682716393f56743a292d63
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5556899
    > Reviewed-by: Justin Green <greenjustin@google.com>
    > Commit-Queue: Yingying Ma <yingying.ma@intel.com>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1307181}
    
    Bug: 40275246
    Change-Id: I6bfc747961724efd1283807adb3811c821b5d7ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5629183
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Justin Green <greenjustin@google.com>
    Commit-Queue: Yingying Ma <yingying.ma@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1315067}

--- media/gpu/av1_builder.h
+++ media/gpu/av1_builder.h
@@ -17,9 +17,19 @@ namespace media {
 // Helper class for AV1 to write packed bitstream data.
 class MEDIA_GPU_EXPORT AV1BitstreamBuilder {
  public:
+  struct SequenceHeader;
+  struct FrameHeader;
+
   AV1BitstreamBuilder();
   ~AV1BitstreamBuilder();
   AV1BitstreamBuilder(AV1BitstreamBuilder&&);
+
+  // Pack sequence header OBU. Spec 5.5.
+  static AV1BitstreamBuilder BuildSequenceHeaderOBU(
+      const SequenceHeader& seq_hdr);
+  // Pack frame OBU. Spec 5.9.
+  static AV1BitstreamBuilder BuildFrameHeaderOBU(const SequenceHeader& seq_hdr,
+                                                 const FrameHeader& pic_hdr);
 
   void Write(uint64_t val, int num_bits);
   void WriteBool(bool val);
@@ -41,6 +51,62 @@ class MEDIA_GPU_EXPORT AV1BitstreamBuilder {
  private:
   std::vector<std::pair<uint64_t, int>> queued_writes_;
   size_t total_outstanding_bits_ = 0;
+};
+
+// Parameters used to build OBUs.
+struct AV1BitstreamBuilder::SequenceHeader {
+  uint32_t profile;
+  uint32_t level;
+  uint32_t tier;
+  uint32_t frame_width_bits_minus_1;
+  uint32_t frame_height_bits_minus_1;
+  uint32_t width;
+  uint32_t height;
+  bool use_128x128_superblock;
+  bool enable_filter_intra;
+  bool enable_intra_edge_filter;
+  bool enable_interintra_compound;
+  bool enable_masked_compound;
+  bool enable_warped_motion;
+  bool enable_dual_filter;
+  bool enable_order_hint;
+  bool enable_jnt_comp;
+  bool enable_ref_frame_mvs;
+  uint32_t order_hint_bits_minus_1;
+  bool enable_superres;
+  bool enable_cdef;
+  bool enable_restoration;
+};
+
+struct AV1BitstreamBuilder::FrameHeader {
+  libgav1::FrameType frame_type;
+  bool error_resilient_mode;
+  bool disable_cdf_update;
+  bool disable_frame_end_update_cdf;
+  uint32_t base_qindex;
+  uint8_t order_hint;
+  uint32_t filter_level[2];
+  uint32_t filter_level_u;
+  uint32_t filter_level_v;
+  uint32_t sharpness_level;
+  bool loop_filter_delta_enabled;
+  uint8_t primary_ref_frame;
+  uint8_t ref_frame_idx[7];
+  uint8_t refresh_frame_flags;
+  uint32_t ref_order_hint[8];
+  uint8_t cdef_y_pri_strength[8];
+  uint8_t cdef_y_sec_strength[8];
+  uint8_t cdef_uv_pri_strength[8];
+  uint8_t cdef_uv_sec_strength[8];
+  bool reduced_tx_set;
+  bool segmentation_enabled;
+  bool segmentation_update_map;
+  bool segmentation_temporal_update;
+  bool segmentation_update_data;
+  uint32_t segment_number;
+  uint32_t feature_mask[8 /*libgav1::kMaxSegments*/];
+  uint32_t feature_data[8 /*libgav1::kMaxSegments*/]
+                       [8 /*libgav1::kSegmentFeatureMax*/];
 };
 
 }  // namespace media
