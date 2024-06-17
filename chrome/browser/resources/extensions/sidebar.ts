29c6029722542b51cf89ed2609cbb81b171fa272
Kelvin Jiang <kelvinjiang@chromium.org>
2024-06-17 22:48:37 +0000

    [Extensions WebUI] Restore discover more text styling
    Re-introduce cr-shared-style to fix the #discover-more-text styling.
    The difference was quite hard to notice on dark mode.
    
    This won't break the cr-menu-selector style since the selectable
    attribute is no longer needed when all elements inside the menu (links)
    are now selectable.
    
    Screenshot: https://imgur.com/a/wiF8Vrr
    
    Bug: 346898511,41493489
    Change-Id: Id62d9e266ecb36cfd32bad4252d967763861088e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5629972
    Auto-Submit: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316132}

--- chrome/browser/resources/extensions/sidebar.ts
+++ chrome/browser/resources/extensions/sidebar.ts
@@ -6,6 +6,7 @@ import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
 import 'chrome://resources/cr_elements/cr_menu_selector/cr_menu_selector.js';
 import 'chrome://resources/cr_elements/cr_nav_menu_item_style.css.js';
 import 'chrome://resources/cr_elements/cr_ripple/cr_ripple.js';
+import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/polymer/v3_0/iron-selector/iron-selector.js';
 import './icons.html.js';
 import './shared_vars.css.js';
