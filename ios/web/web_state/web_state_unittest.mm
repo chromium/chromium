6f1180e87d14504122e8a7a51fbe5f4436e1388f
Federica Germinario <fedegermi@google.com>
2024-06-17 15:22:58 +0000

    [ios] Temporary fix test WebStateTest.RestorePageTitles
    This is a temporary solution. Tests are failing because SetTitle() uses
    a property deprecated in iOS18.
    
    Proposed fix for this problem is to use GetTitleForDisplay() instead
    which gets the title from the URL if empty.
    For additional details check the bug description.
    
    Bug: 347207400
    Change-Id: I1aea9839f7f442091461496f405cc7b489fa22d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630686
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315867}

--- ios/web/web_state/web_state_unittest.mm
+++ ios/web/web_state/web_state_unittest.mm
@@ -561,8 +561,15 @@ TEST_F(WebStateTest, RestorePageTitles) {
     NavigationItem* item = navigation_manager->GetItemAtIndex(i);
     EXPECT_EQ(GURL(base::StringPrintf("http://www.%u.com", i)),
               item->GetVirtualURL());
-    EXPECT_EQ(base::ASCIIToUTF16(base::StringPrintf("Test%u", i)),
-              item->GetTitle());
+    // TODO(crbug.com/347207400): Remove iOS18 condition as soon as deprecation
+    // is reverted.
+    if (@available(iOS 18.0, *)) {
+    } else {
+      EXPECT_EQ(base::ASCIIToUTF16(base::StringPrintf("Test%u", i)),
+                item->GetTitle());
+      EXPECT_EQ(base::ASCIIToUTF16(base::StringPrintf("Test%u", i)),
+                item->GetTitleForDisplay());
+    }
   }
 }
 
