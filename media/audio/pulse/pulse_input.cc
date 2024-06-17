e7bb4b29525af58508f2d3317428a8468e7f9a80
AlexiaBojian <bojianalexia4@gmail.com>
2024-06-17 23:25:06 +0000

    Deleted sleep 5ms call because we have a ring buffer and FIFO on the actual shared memory
    Change-Id: I5158dfe4962ee394e702fa712ae82f5e69cc3dbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631351
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Alexia Bojian <bojianalexia4@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1316155}

--- media/audio/pulse/pulse_input.cc
+++ media/audio/pulse/pulse_input.cc
@@ -380,12 +380,6 @@ void PulseAudioInputStream::ReadData() {
 
     callback_->OnData(audio_bus, capture_time, normalized_volume, {});
 
-    // Sleep 5ms to wait until render consumes the data in order to avoid
-    // back to back OnData() method.
-    // TODO(dalecurtis): Delete all this. It shouldn't be necessary now that we
-    // have a ring buffer and FIFO on the actual shared memory.,
-    if (fifo_.available_blocks())
-      base::PlatformThread::Sleep(base::Milliseconds(5));
   }
 
   pa_threaded_mainloop_signal(pa_mainloop_, 0);
