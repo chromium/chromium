0cc05419b992fac5e6553a80ff52483cf38f0317
Vincent Boisselle <vincb@google.com>
2024-06-18 15:17:42 +0000

    [ios] Update the fill data upon filling cred the way it used to be
    This reverts the way the field data manager is updated to what it
    used to be before crrev.com/c/5494354,
    
    Before crrev.com/c/5494354, the fill data for would be updated for
    both fields upon receiving "success" regardless of whether or not
    filling really happened. The success status is returned when there are
    field elements in the renderer that correspond to the fill data
    but doesn't look beyond that.
    
    The goal of crrev.com/c/5494354 was to only update the fill data
    if filling did really happen to reflect the reality, but that has
    a non-negligible effect on the PasswordManager.FillingAssistance
    metric with a notable transfer from
    ":-) Credential fields filled manually..." to
    "%-) Neither filling nor user input" (more context
    in crbug.com/346378062).
    
    It is hard to rationalize which approach is the best, so we propose
    as a first step to just go back to what it used to be (status quo),
    then from there we can figure out what we should do.
    
    We still keep the "did really fill" condition for calling
    UpdateStateOnUserInput which really needs to fit reality.
    
    Change-Id: If11daff87bac2f5ef0a55b02d3785cc3ebeb5b9e
    Bug: 346378062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5637082
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316457}

--- components/password_manager/ios/password_form_helper_unittest.mm
+++ components/password_manager/ios/password_form_helper_unittest.mm
@@ -426,12 +426,13 @@ TEST_F(PasswordFormHelperTest, FillPasswordFormWithFillData_Success_NoFill) {
   id result = ExecuteJavaScript(kInputFieldValueVerificationScript);
   EXPECT_NSEQ(@"u1=test-username;p1=test-password;", result);
 
-  // Check that username and password fields were not updated as they were not
-  // filled.
+  // Check that username and password fields were still updated even if they
+  // were not filled. This odd behavior is kept to not skew metrics downstream
+  // (e.g. PasswordManager.FillingAssistance).
   autofill::FieldDataManager* fieldDataManager =
       autofill::FieldDataManagerFactoryIOS::FromWebFrame(GetMainFrame());
-  EXPECT_FALSE(fieldDataManager->WasAutofilledOnUserTrigger(username_field_id));
-  EXPECT_FALSE(fieldDataManager->WasAutofilledOnUserTrigger(password_field_id));
+  EXPECT_TRUE(fieldDataManager->WasAutofilledOnUserTrigger(username_field_id));
+  EXPECT_TRUE(fieldDataManager->WasAutofilledOnUserTrigger(password_field_id));
 
   // Verify that the fill operation was recorded as a success.
   histogram_tester.ExpectUniqueSample("PasswordManager.FillingSuccessIOS", true,
@@ -573,7 +574,7 @@ TEST_F(PasswordFormHelperTest,
   // FieldDataManager but not the username as it wasn't filled.
   autofill::FieldDataManager* fieldDataManager =
       autofill::FieldDataManagerFactoryIOS::FromWebFrame(GetMainFrame());
-  EXPECT_FALSE(fieldDataManager->WasAutofilledOnUserTrigger(username_field_id));
+  EXPECT_TRUE(fieldDataManager->WasAutofilledOnUserTrigger(username_field_id));
   EXPECT_TRUE(fieldDataManager->WasAutofilledOnUserTrigger(password_field_id));
 
   // Verify that the password was filled.
