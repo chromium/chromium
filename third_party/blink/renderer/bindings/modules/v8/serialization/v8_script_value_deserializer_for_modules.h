9201852fa37437ea76b38e59be2ab8e1f9e4e8e9
Jeremy Roman <jbroman@chromium.org>
2024-06-18 20:00:03 +0000

    Remove an old MSVC 2015 workaround.
    Change-Id: I5555c61380fd3d3dab930d0dba36e6e241791292
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639795
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316645}

--- third_party/blink/renderer/bindings/modules/v8/serialization/v8_script_value_deserializer_for_modules.h
+++ third_party/blink/renderer/bindings/modules/v8/serialization/v8_script_value_deserializer_for_modules.h
@@ -26,19 +26,7 @@ class VideoFrame;
 class MODULES_EXPORT V8ScriptValueDeserializerForModules final
     : public V8ScriptValueDeserializer {
  public:
-  // TODO(jbroman): This should just be:
-  // using V8ScriptValueDeserializer::V8ScriptValueDeserializer;
-  // Unfortunately, MSVC 2015 emits C2248, claiming that it cannot access its
-  // own private members. Until it's gone, we write the constructors by hand.
-  V8ScriptValueDeserializerForModules(ScriptState* script_state,
-                                      UnpackedSerializedScriptValue* unpacked,
-                                      const Options& options = Options())
-      : V8ScriptValueDeserializer(std::move(script_state), unpacked, options) {}
-  V8ScriptValueDeserializerForModules(
-      ScriptState* script_state,
-      scoped_refptr<SerializedScriptValue> value,
-      const Options& options = Options())
-      : V8ScriptValueDeserializer(script_state, std::move(value), options) {}
+  using V8ScriptValueDeserializer::V8ScriptValueDeserializer;
 
   static bool ExecutionContextExposesInterface(ExecutionContext*,
                                                SerializationTag interface_tag);
