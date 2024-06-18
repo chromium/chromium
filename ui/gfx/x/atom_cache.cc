cb58e6c2871351b16150ca617173dfabdfc66af6
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-06-18 14:33:34 +0000

    Add chromium/x-source-url to AtomCache
    This new clipboard MIME type was added in crrev.com/c/5598457, but due
    to it not also being added to AtomCache it triggered the error log in
    AtomCache::GetAtom.
    
    Bug: b/345214239
    Change-Id: I40794d65f4441f0153f5a190a9fcea52cc39d65e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5636366
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316426}

--- ui/gfx/x/atom_cache.cc
+++ ui/gfx/x/atom_cache.cc
@@ -160,6 +160,7 @@ constexpr const char* kAtomsToCache[] = {
     "chromium/x-file-system-files",
     "chromium/x-pepper-custom-data",
     "chromium/x-renderer-taint",
+    "chromium/x-source-url",
     "chromium/x-web-custom-data",
     "chromium/x-webkit-paste",
     "image/png",
