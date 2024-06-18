e9b6b20cee414c9a070bcb9e891f90e556ddc97a
Joe Mason <joenotcharles@google.com>
2024-06-18 15:41:51 +0000

    Remove MemoryPressureMetrics, which does nothing
    Looks like the contents of this class was removed in https://crrev.com/c/4905381, but it was never deleted.
    
    R=anthonyvd
    
    Bug: 1384535
    Change-Id: I92abcd60d31d55cd15fdb1501a95072091abfeaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631803
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316480}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -16,7 +16,6 @@
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/performance_manager/decorators/helpers/page_live_state_decorator_helper.h"
-#include "chrome/browser/performance_manager/metrics/memory_pressure_metrics.h"
 #include "chrome/browser/performance_manager/metrics/metrics_provider_desktop.h"
 #include "chrome/browser/performance_manager/metrics/page_resource_monitor.h"
 #include "chrome/browser/performance_manager/observers/page_load_metrics_observer.h"
@@ -203,8 +202,6 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
       std::make_unique<performance_manager::policies::MemorySaverModePolicy>());
 #endif  // !BUILDFLAG(IS_ANDROID)
 
-  graph->PassToGraph(
-      std::make_unique<performance_manager::metrics::MemoryPressureMetrics>());
   graph->PassToGraph(
       std::make_unique<performance_manager::metrics::PageResourceMonitor>());
 
