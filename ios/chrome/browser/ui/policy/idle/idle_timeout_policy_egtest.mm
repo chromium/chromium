bf9565894e5017d655049a5677b7bd105ea1557b
Sylvain Defresne <sdefresne@chromium.org>
2024-06-19 14:35:16 +0000

    [gardener][ios] Fix flaky compilation issue
    The file ios/chrome/browser/ui/policy/idle/idle_timeout_policy_egtest.mm
    had an unnecessary import of MaterialComponents/MaterialComponents.h but
    no dependency on the corresponding target.
    
    This caused the build to flakily fail. Additionally the import was not
    allowed since the test module is not allowed to depend on MDC. Remove
    the import to fix the compilation and fix the layering violation.
    
    Bug: none
    Change-Id: I57bb7cbfb36ed487013627d15feba52a6b97c3f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639687
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317023}

--- ios/chrome/browser/ui/policy/idle/idle_timeout_policy_egtest.mm
+++ ios/chrome/browser/ui/policy/idle/idle_timeout_policy_egtest.mm
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <MaterialComponents/MaterialSnackbar.h>
 #import <XCTest/XCTest.h>
 
 #import "base/ios/ios_util.h"
