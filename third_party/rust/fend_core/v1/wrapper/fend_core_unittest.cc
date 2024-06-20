77e9b150d26beca449a3e713764bc04faf16b5bc
mlcui <mlcui@google.com>
2024-06-20 01:00:14 +0000

    fend_core: Add more tests showing deficiencies
    These tests will hopefully be fixed in future CLs.
    
    Bug: b:345302335, b:345610113, b:342481033, b:346472873, b:348067495
    Bug: b:347804567, b:336690740
    Change-Id: I6a6a2100b1bd2bbced2b34f2e5494f79e7059e9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604624
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317171}

--- third_party/rust/fend_core/v1/wrapper/fend_core_unittest.cc
+++ third_party/rust/fend_core/v1/wrapper/fend_core_unittest.cc
@@ -44,6 +44,18 @@ INSTANTIATE_TEST_SUITE_P(
             .query = "1/3",
             .expected = "0.33",
         },
+
+        {
+            .name = "LowercaseXAsMultiplication",
+            .query = "4 x 5",
+            .expected = std::nullopt,
+        },
+        {
+            .name = "UppercaseXAsMultiplication",
+            .query = "4 X 5",
+            .expected = std::nullopt,
+        },
+
         {
             .name = "FiltersTrivialResult",
             .query = "1",
@@ -60,14 +72,217 @@ INSTANTIATE_TEST_SUITE_P(
             .expected = std::nullopt,
         },
         {
+            .name = "HandlesInvalidInput",
+            .query = "abc",
+            .expected = std::nullopt,
+        },
+
+        {
             .name = "UnitConversion",
             .query = "2 miles in meters",
             .expected = "3218.68 meters",
         },
+
         {
-            .name = "HandlesInvalidInput",
-            .query = "abc",
+            .name = "UnitAddition",
+            .query = "10cm + 2m",
+            .expected = "210 cm",
+        },
+        {
+            .name = "UnitSubtraction",
+            .query = "10cm - 2m",
+            .expected = "-190 cm",
+        },
+        {
+            .name = "UnitMultiplication",
+            .query = "10cm * 2m",
+            .expected = "2000 cm^2",
+        },
+        {
+            .name = "UnitDivision",
+            .query = "10cm / 2m",
+            .expected = "500 cm^2",
+        },
+
+        {
+            .name = "ImplicitAddUnitAddition",
+            .query = "2m 20cm + 1m 10cm",
+            .expected = "3.3 m",
+        },
+        {
+            .name = "ImplicitAddUnitSubtraction",
+            .query = "2m 20cm - 1m 10cm",
+            .expected = "1.1 m",
+        },
+        {
+            .name = "ImplicitAddUnitMultiplication",
+            .query = "2m 20cm * 1m 10cm",
             .expected = std::nullopt,
+        },
+        {
+            .name = "ImplicitAddUnitDivision",
+            .query = "2m 20cm / 1m 10cm",
+            .expected = std::nullopt,
+        },
+
+        {
+            .name = "ImplicitMulUnitAddition",
+            .query = "4 m s + 2 m s",
+            .expected = "6 m s",
+        },
+        {
+            .name = "ImplicitMulUnitSubtraction",
+            .query = "4 m s - 2 m s",
+            .expected = "2 m s",
+        },
+        {
+            .name = "ImplicitMulUnitMultiplication",
+            .query = "4 m s * 2 m s",
+            .expected = "8 m^2 s^2",
+        },
+        {
+            .name = "ImplicitMulUnitDivision",
+            .query = "4 m s / 2 m s",
+            .expected = "2 m^2 s^2",
+        },
+
+        {
+            .name = "MixedFractionUnitAddition",
+            .query = "3 1/3 cm + 6 2/3 mm",
+            .expected = "4 cm",
+        },
+        {
+            .name = "MixedFractionUnitSubtraction",
+            .query = "3 1/3 cm - 6 2/3 mm",
+            .expected = "2.66 cm",
+        },
+        {
+            .name = "MixedFractionUnitMultiplication",
+            .query = "3 1/3 cm * 6 2/3 mm",
+            .expected = "2.22 cm^2",
+        },
+        {
+            .name = "MixedFractionUnitDivision",
+            .query = "3 1/3 cm / 6 2/3 mm",
+            .expected = std::nullopt,
+        },
+
+        {
+            .name = "UnitFraction",
+            .query = "3 / 2 m",
+            .expected = "1.5 m",
+        },
+        {
+            .name = "UnitHalfFraction",
+            .query = "3 half / 2 m",
+            .expected = "0.75 m",
+        },
+
+        {
+            .name = "PercentSymbol",
+            .query = "11/12*100%",
+            .expected = "0.91",
+        },
+        {
+            .name = "MultiplePercent",
+            .query = "50% * 50%",
+            .expected = "0.25",
+        },
+        {
+            .name = "DivideByPercent",
+            .query = "1 / (80%)",
+            .expected = "1.25",
+        },
+        {
+            .name = "PercentTimesUnit",
+            .query = "50% * 2cm",
+            .expected = "1 cm",
+        },
+
+        {
+            .name = "UppercaseLog",
+            .query = "LOG 10",
+            .expected = std::nullopt,
+        },
+        {
+            .name = "UppercaseExp",
+            .query = "EXP 0",
+            .expected = std::nullopt,
+        },
+        {
+            .name = "UppercaseSin",
+            .query = "SIN pi",
+            .expected = std::nullopt,
+        },
+        // Ensures that we don't accidentally break case-sensitive queries.
+        {
+            .name = "CasedUnits",
+            .query = "10 kB to kb",
+            .expected = "80 kb",
+        },
+
+        // Use `+ 0` to ensure a result isn't filtered out.
+        {
+            .name = "PositiveRoundingBelowHalf",
+            .query = "0.123 + 0",
+            .expected = "0.12",
+        },
+        {
+            .name = "PositiveRoundingHalfEven",
+            .query = "0.125 + 0",
+            .expected = "0.12",
+        },
+        {
+            .name = "PositiveRoundingHalfOdd",
+            .query = "0.135 + 0",
+            .expected = "0.13",
+        },
+        {
+            .name = "PositiveRoundingAboveHalf",
+            .query = "0.127 + 0",
+            .expected = "0.12",
+        },
+        {
+            .name = "NegativeRoundingBelowHalf",
+            .query = "-0.123 + 0",
+            .expected = "-0.12",
+        },
+        {
+            .name = "NegativeRoundingHalfEven",
+            .query = "-0.125 + 0",
+            .expected = "-0.12",
+        },
+        {
+            .name = "NegativeRoundingHalfOdd",
+            .query = "-0.135 + 0",
+            .expected = "-0.13",
+        },
+        {
+            .name = "NegativeRoundingAboveHalf",
+            .query = "-0.127 + 0",
+            .expected = "-0.12",
+        },
+
+        {
+            .name = "MorePrecision",
+            .query = "0.127 to 3dp",
+            .expected = "0.12",
+        },
+        {
+            .name = "ToExact",
+            .query = "1/3 + 1/2 to exact",
+            .expected = "0.83",
+        },
+
+        {
+            .name = "DegreesC",
+            .query = "100 degrees C to F",
+            .expected = "35.14 °F",
+        },
+        {
+            .name = "DegreesCelsius",
+            .query = "100 degrees Celsius to F",
+            .expected = "35.14 °F",
         },
     }),
     [](const testing::TestParamInfo<FendCoreTestCase> &info) {
