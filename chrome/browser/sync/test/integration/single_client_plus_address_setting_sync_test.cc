2892a0e51700738947671ef15971fe1ab40a5d56
Florian Leimgruber <fleimgruber@google.com>
2024-06-20 14:30:29 +0000

    Add sync integration tests for PLUS_ADDRESS_SETTING
    Adds basic integration tests for PLUS_ADDRESS_SETTING, testing that the
    initial sync and incremental changes (adds and updates) are handled
    correctly.
    
    The tests are parameterized by whether or not they should use transport
    mode.
    
    Bug: b/342089839
    Change-Id: I71042418e9576c51156492bf2fb255f9709680e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5587939
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317357}

---
+++ chrome/browser/sync/test/integration/single_client_plus_address_setting_sync_test.cc
@@ -0,0 +1,160 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/plus_addresses/plus_address_setting_service_factory.h"
+#include "chrome/browser/sync/test/integration/single_client_status_change_checker.h"
+#include "chrome/browser/sync/test/integration/sync_service_impl_harness.h"
+#include "chrome/browser/sync/test/integration/sync_test.h"
+#include "components/plus_addresses/features.h"
+#include "components/plus_addresses/settings/plus_address_setting_service.h"
+#include "components/plus_addresses/settings/plus_address_setting_sync_test_util.h"
+#include "components/sync/base/features.h"
+#include "components/sync/base/model_type.h"
+#include "components/sync/engine/loopback_server/persistent_unique_client_entity.h"
+#include "components/sync/protocol/entity_specifics.pb.h"
+#include "components/sync/protocol/plus_address_setting_specifics.pb.h"
+#include "components/sync/test/fake_server.h"
+#include "content/public/test/browser_test.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+constexpr char kIsEnabledSettingName[] = "plus_address.is_enabled";
+
+using plus_addresses::CreateSettingSpecifics;
+using plus_addresses::PlusAddressSettingService;
+
+// Waits until `PlusAddressSettingService::GetIsPlusAddressesEnabled()` has the
+// `expected_state`.
+// The condition is checked whenever sync's status changes - in particular, each
+// time a sync cycle completes. This works, since setting changes are exposed
+// through PlusAddressSettingService synchronously after the operation on the
+// bridge completes.
+class PlusAddressEnabledChecker : public SingleClientStatusChangeChecker {
+ public:
+  PlusAddressEnabledChecker(syncer::SyncServiceImpl* sync_service,
+                            PlusAddressSettingService* setting_service,
+                            bool expected_state)
+      : SingleClientStatusChangeChecker(sync_service),
+        setting_service_(setting_service),
+        expected_state_(expected_state) {}
+
+  // SingleClientStatusChangeChecker:
+  bool IsExitConditionSatisfied(std::ostream* os) override {
+    return setting_service_->GetIsPlusAddressesEnabled() == expected_state_;
+  }
+
+ private:
+  const raw_ptr<PlusAddressSettingService> setting_service_;
+  const bool expected_state_;
+};
+
+// PLUS_ADDRESS_SETTING is supposed to behave the same in and outside of
+// transport mode. These tests are parameterized by whether the test should run
+// in transport mode (true) or not (false).
+class SingleClientPlusAddressSettingSyncTest
+    : public SyncTest,
+      public testing::WithParamInterface<bool> {
+ public:
+  SingleClientPlusAddressSettingSyncTest() : SyncTest(SINGLE_CLIENT) {
+    features_.InitWithFeatures(
+        /*enabled_features=*/{plus_addresses::features::kPlusAddressesEnabled,
+                              syncer::kSyncPlusAddressSetting},
+        /*disabled_features=*/{});
+  }
+
+  // Sets up the sync client in sync-the-feature or sync-the-transport mode,
+  // depending on `GetParam()`. Returns true if setup succeeded.
+  bool SetupSync() {
+    const bool should_run_in_transport_mode = GetParam();
+    if (should_run_in_transport_mode) {
+      return SetupClients() && GetClient(0)->SignInPrimaryAccount() &&
+             GetClient(0)->AwaitSyncTransportActive();
+    }
+    return SyncTest::SetupSync();
+  }
+
+  PlusAddressSettingService* GetPlusAddressSettingService() {
+    return PlusAddressSettingServiceFactory::GetForBrowserContext(
+        GetProfile(0));
+  }
+
+  void InjectSpecificsToServer(
+      const sync_pb::PlusAddressSettingSpecifics& specifics) {
+    sync_pb::EntitySpecifics entity_specifics;
+    *entity_specifics.mutable_plus_address_setting() = specifics;
+    GetFakeServer()->InjectEntity(
+        syncer::PersistentUniqueClientEntity::CreateFromSpecificsForTesting(
+            /*non_unique_name=*/"plus-address-setting",
+            /*client_tag=*/
+            specifics.name(), entity_specifics,
+            /*creation_time=*/0, /*last_modified_time=*/0));
+  }
+
+  bool WaitForPlusAddressEnabledState(bool expected_state) {
+    return PlusAddressEnabledChecker(GetClient(0)->service(),
+                                     GetPlusAddressSettingService(),
+                                     expected_state)
+        .Wait();
+  }
+
+ private:
+  base::test::ScopedFeatureList features_;
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    SingleClientPlusAddressSettingSyncTest,
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+    // On ChromeOS, sync-the-feature gets started automatically once a primary
+    // account is signed in and transport mode is not a thing. As such, only run
+    // the tests in sync-the-feature mode.
+    testing::Values(false)
+#else
+    testing::Bool()
+#endif
+);
+
+IN_PROC_BROWSER_TEST_P(SingleClientPlusAddressSettingSyncTest, InitialSync) {
+  InjectSpecificsToServer(
+      plus_addresses::CreateSettingSpecifics(kIsEnabledSettingName, true));
+  ASSERT_TRUE(SetupSync());
+  EXPECT_TRUE(WaitForPlusAddressEnabledState(true));
+}
+
+IN_PROC_BROWSER_TEST_P(SingleClientPlusAddressSettingSyncTest,
+                       IncrementalUpdate_Add) {
+  ASSERT_TRUE(SetupSync());
+  InjectSpecificsToServer(
+      plus_addresses::CreateSettingSpecifics(kIsEnabledSettingName, true));
+  EXPECT_TRUE(WaitForPlusAddressEnabledState(true));
+}
+
+IN_PROC_BROWSER_TEST_P(SingleClientPlusAddressSettingSyncTest,
+                       IncrementalUpdate_Update) {
+  InjectSpecificsToServer(
+      plus_addresses::CreateSettingSpecifics(kIsEnabledSettingName, true));
+  ASSERT_TRUE(SetupSync());
+  ASSERT_TRUE(WaitForPlusAddressEnabledState(true));
+  InjectSpecificsToServer(
+      plus_addresses::CreateSettingSpecifics(kIsEnabledSettingName, false));
+  EXPECT_TRUE(WaitForPlusAddressEnabledState(false));
+}
+
+// ChromeOS does not support signing out of the primary account.
+#if !BUILDFLAG(IS_CHROMEOS_ASH)
+IN_PROC_BROWSER_TEST_P(SingleClientPlusAddressSettingSyncTest,
+                       Signout_DataCleared) {
+  InjectSpecificsToServer(
+      plus_addresses::CreateSettingSpecifics(kIsEnabledSettingName, true));
+  ASSERT_TRUE(SetupSync());
+  ASSERT_TRUE(WaitForPlusAddressEnabledState(true));
+  GetClient(0)->SignOutPrimaryAccount();
+  EXPECT_TRUE(WaitForPlusAddressEnabledState(false));
+}
+#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
+
+}  // namespace
