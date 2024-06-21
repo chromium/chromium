dad231d469024d06c967f0ff1782a0ff8b8d2d2d
Ioana Pandele <ioanap@chromium.org>
2024-06-21 10:41:25 +0000

    [Passwords] Extend UPM histograms and deprecate an unused one
    OBSOLETE_HISTOGRAM[PasswordManager.CredentialEditError]=This is a histogram for the old management UI on Android which has been largely replaced by UPM.
    
    Fixed: 344592099, 346342270, 343044350
    Change-Id: Ib1174993db1c3c583ba04111a9bc4c70117013ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5644947
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317858}

--- chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditControllerTest.java
+++ chrome/browser/password_entry_edit/android/internal/java/src/org/chromium/chrome/browser/password_entry_edit/CredentialEditControllerTest.java
@@ -21,9 +21,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import static org.chromium.chrome.browser.password_entry_edit.CredentialEditMediator.BLOCKED_CREDENTIAL_ACTION_HISTOGRAM;
-import static org.chromium.chrome.browser.password_entry_edit.CredentialEditMediator.CredentialEditError.DUPLICATE_USERNAME;
-import static org.chromium.chrome.browser.password_entry_edit.CredentialEditMediator.CredentialEditError.EMPTY_PASSWORD;
-import static org.chromium.chrome.browser.password_entry_edit.CredentialEditMediator.EDIT_ERROR_HISTOGRAM;
 import static org.chromium.chrome.browser.password_entry_edit.CredentialEditMediator.FEDERATED_CREDENTIAL_ACTION_HISTOGRAM;
 import static org.chromium.chrome.browser.password_entry_edit.CredentialEditMediator.SAVED_PASSWORD_ACTION_HISTOGRAM;
 import static org.chromium.chrome.browser.password_entry_edit.CredentialEditProperties.ALL_KEYS;
@@ -250,10 +247,6 @@ public class CredentialEditControllerTest {
         mMediator.setCredential(TEST_USERNAME, TEST_PASSWORD, false);
         mMediator.onPasswordTextChanged("");
         assertTrue(mModel.get(EMPTY_PASSWORD_ERROR));
-        assertThat(
-                RecordHistogram.getHistogramValueCountForTesting(
-                        EDIT_ERROR_HISTOGRAM, EMPTY_PASSWORD),
-                is(1));
 
         mMediator.onPasswordTextChanged(TEST_PASSWORD);
         assertFalse(mModel.get(EMPTY_PASSWORD_ERROR));
@@ -266,10 +259,6 @@ public class CredentialEditControllerTest {
 
         mMediator.onUsernameTextChanged(NEW_TEST_USERNAME);
         assertTrue(mModel.get(DUPLICATE_USERNAME_ERROR));
-        assertThat(
-                RecordHistogram.getHistogramValueCountForTesting(
-                        EDIT_ERROR_HISTOGRAM, DUPLICATE_USERNAME),
-                is(1));
 
         mMediator.onUsernameTextChanged(TEST_USERNAME);
         assertFalse(mModel.get(DUPLICATE_USERNAME_ERROR));
