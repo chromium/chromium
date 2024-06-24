6a7dd4fb391a40bff28db3daf84a2d6b6efa458e
Chris Lu <thegreenfrog@chromium.org>
2024-06-24 19:09:31 +0000

    [ios] ContentSuggesitonsVCAudience implement MagicStackModuleContainer
    This removed redundant protocol definitions.
    
    Change-Id: Ibea14fd53144f1ec839fa1ead39733332536c090
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5646009
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318713}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller_audience.h
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller_audience.h
@@ -5,39 +5,18 @@
 #ifndef IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_CONTENT_SUGGESTIONS_VIEW_CONTROLLER_AUDIENCE_H_
 #define IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_CONTENT_SUGGESTIONS_VIEW_CONTROLLER_AUDIENCE_H_
 
+#import "ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_module_container_delegate.h"
+
 enum class ContentSuggestionsModuleType;
 enum class SafetyCheckItemType;
 @class SetUpListItemView;
 
 // Audience for the ContentSuggestions, getting information from it.
-@protocol ContentSuggestionsViewControllerAudience
+@protocol
+    ContentSuggestionsViewControllerAudience <MagicStackModuleContainerDelegate>
 
 // Notifies the audience of the UIKit viewWillDisappear: callback.
 - (void)viewWillDisappear;
-
-// Notifies the audience to present the Set Up List Show More Menu.
-- (void)showSetUpListSeeMoreMenu;
-
-// Notifies the audience that the module of `type` should be never shown
-// anymore.
-- (void)neverShowModuleType:(ContentSuggestionsModuleType)type;
-
-// Indicates that the user has tapped the context menu item to enable
-// notifications for a module.
-- (void)enableNotifications:(ContentSuggestionsModuleType)type;
-
-// Indicates that the user has tapped the context menu item to disable
-// notifications.
-- (void)disableNotifications:(ContentSuggestionsModuleType)type;
-
-// Notifies the audience that the Magic Stack edit button was tapped.
-- (void)didTapMagicStackEditButton;
-
-// Notifies the audience to show the list of parcels.
-- (void)showMagicStackParcelList;
-
-// Notifies the audience to show the recent tabs panel.
-- (void)showMagicStackRecentTabs;
 
 // Called when a Safety Check item is selected by the user.
 - (void)didSelectSafetyCheckItem:(SafetyCheckItemType)type;
