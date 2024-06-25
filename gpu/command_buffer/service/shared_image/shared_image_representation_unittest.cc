cc692d18fc2dfed6dfba62a6372eb6e3ed387c94
Jan Keitel <jkeitel@google.com>
2024-06-25 14:02:02 +0000

    Add more SharedImage usages to SharedImageRepresentation test.
    Bug: 349290188
    Change-Id: If71cb412aa955745acce8f2274bea947a3b4c50e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5650772
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319118}

--- gpu/command_buffer/service/shared_image/shared_image_representation_unittest.cc
+++ gpu/command_buffer/service/shared_image/shared_image_representation_unittest.cc
@@ -27,9 +27,11 @@ class SharedImageRepresentationTest : public ::testing::Test {
     auto color_space = gfx::ColorSpace::CreateSRGB();
     auto surface_origin = kTopLeft_GrSurfaceOrigin;
     auto alpha_type = kPremul_SkAlphaType;
-    uint32_t usage =
-        SHARED_IMAGE_USAGE_GLES2_READ | SHARED_IMAGE_USAGE_GLES2_WRITE;
-
+    // Add the usages that the tests in this file require.
+    SharedImageUsageSet usage =
+        SHARED_IMAGE_USAGE_GLES2_READ | SHARED_IMAGE_USAGE_GLES2_WRITE |
+        SHARED_IMAGE_USAGE_RASTER_READ | SHARED_IMAGE_USAGE_RASTER_WRITE |
+        SHARED_IMAGE_USAGE_WEBGPU_WRITE;
     auto backing = std::make_unique<TestImageBacking>(
         mailbox_, format, size, color_space, surface_origin, alpha_type, usage,
         /*estimated_size=*/0);
