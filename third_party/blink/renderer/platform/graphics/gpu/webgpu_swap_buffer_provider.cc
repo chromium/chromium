3e4a6046589778f5c8e8e99c9e2a3715472ed7d0
Colin Blundell <blundell@chromium.org>
2024-06-26 09:15:13 +0000

    [Blink] Minor code simplification
    Addresses the excellent comment left here [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5648061/comment/4ceb7954_3d18532f/
    
    Bug: 339171225
    Change-Id: Ia393678bc6ae32b826e4d9c0086b90811c6f645d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5657111
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319640}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_swap_buffer_provider.cc
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_swap_buffer_provider.cc
@@ -288,13 +288,11 @@ WebGPUSwapBufferProvider::GetLastWebGPUMailboxTextureAndSize() const {
 
   wgpu::DawnTextureInternalUsageDescriptor internal_usage;
   internal_usage.internalUsage = internal_usage_;
-  wgpu::Extent3D size = {};
-  size.width = latest_swap_buffer->size.width();
-  size.height = latest_swap_buffer->size.height();
   wgpu::TextureDescriptor desc = {
       .nextInChain = &internal_usage,
       .usage = usage_,
-      .size = size,
+      .size = {static_cast<uint32_t>(latest_swap_buffer->size.width()),
+               static_cast<uint32_t>(latest_swap_buffer->size.height())},
       .format = format_,
   };
 
