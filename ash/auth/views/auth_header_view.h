09355de65b33ca6151400e96bd29fe707efa2e6e
Istvan Nagy <iscsi@google.com>
2024-06-27 12:49:38 +0000

    [High5] Implement header for the authentication dialog.
    Bug: b:349265481, b:348326316
    Change-Id: I358ba72353cc87f88768a3fb8dc74a7db1345dae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5650946
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1320295}

---
+++ ash/auth/views/auth_header_view.h
@@ -0,0 +1,61 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_AUTH_VIEWS_AUTH_HEADER_VIEW_H_
+#define ASH_AUTH_VIEWS_AUTH_HEADER_VIEW_H_
+
+#include <string>
+
+#include "ash/ash_export.h"
+#include "ash/login/ui/animated_rounded_image_view.h"
+#include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/observer_list_types.h"
+#include "components/account_id/account_id.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/controls/label.h"
+#include "ui/views/view.h"
+
+namespace ash {
+
+// Contains the user avatar, title and description.
+// The title can display error message.
+// Provide function to restore the original title.
+class ASH_EXPORT AuthHeaderView : public views::View {
+  METADATA_HEADER(AuthHeaderView, views::View)
+
+ public:
+  AuthHeaderView(const AccountId& account_id,
+                 const std::u16string& title,
+                 const std::u16string& description);
+
+  AuthHeaderView(const AuthHeaderView&) = delete;
+  AuthHeaderView& operator=(const AuthHeaderView&) = delete;
+
+  ~AuthHeaderView() override;
+
+  // views::View:
+  gfx::Size CalculatePreferredSize(
+      const views::SizeBounds& available_size) const override;
+
+  base::WeakPtr<AuthHeaderView> AsWeakPtr() {
+    return weak_ptr_factory_.GetWeakPtr();
+  }
+
+  void SetErrorTitle(const std::u16string& error_str);
+  void RestoreTitle();
+
+ private:
+  raw_ptr<AnimatedRoundedImageView> avatar_view_ = nullptr;
+  raw_ptr<views::Label> title_label_ = nullptr;
+  raw_ptr<views::Label> description_label_ = nullptr;
+
+  const std::u16string title_str_;
+
+  base::WeakPtrFactory<AuthHeaderView> weak_ptr_factory_{this};
+};
+
+}  // namespace ash
+
+#endif  // ASH_AUTH_VIEWS_AUTH_HEADER_VIEW_H_
