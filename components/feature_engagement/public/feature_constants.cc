5cc1a9ba18850f35de94128982177014df420fec
Timofey Chudakov <tchudakov@google.com>
2024-06-29 21:13:26 +0000

    Disable IPH_PlusAddressCreateSuggestion by default.
    Bug: 346474398
    Change-Id: I53ade6aac4cbed88c74cde41214e9ab6e60dcddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5666496
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1321385}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -649,7 +649,7 @@ BASE_FEATURE(kIPHCookieControlsFeature,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHPlusAddressCreateSuggestionFeature,
              "IPH_PlusAddressCreateSuggestion",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE) ||
         // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) ||
         // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_FUCHSIA)
