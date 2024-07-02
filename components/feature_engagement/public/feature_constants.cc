0498d9f10a63634fcca419f536c79eb1acdd23a7
Alvin Lam <alvingo@google.com>
2024-07-02 17:23:03 +0000

    [3PCD Reminder] Disable the reminder IPH feature
    Disabling the IPH feature for now to ensure that the reminder will not
    show in prod.
    
    Change-Id: I8e713edf9a8fca91e90c73bf153d0b54f46b1301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5669693
    Auto-Submit: Alvin Lam <alvingo@google.com>
    Commit-Queue: Alvin Lam <alvingo@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322273}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -184,7 +184,7 @@ BASE_FEATURE(kIPHTrackingProtectionFullOnboardingFeature,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHTrackingProtectionReminderFeature,
              "IPH_TrackingProtectionReminder",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHWebUITabStripFeature,
              "IPH_WebUITabStrip",
              base::FEATURE_DISABLED_BY_DEFAULT);
