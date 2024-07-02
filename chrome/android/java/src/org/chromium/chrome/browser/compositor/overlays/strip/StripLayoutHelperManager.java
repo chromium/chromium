bbdd0e2e278aab40daceb68ee4a2eec587f2f52a
Sirisha Kavuluru <skavuluru@google.com>
2024-07-02 16:48:09 +0000

    [Tab Strip] Added metric for model selector button click
    Bug: 350568238
    Change-Id: Ib474dfa104bb69ae199a136f41dfb45af0e43cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5665760
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322245}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelperManager.java
@@ -30,6 +30,7 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.content.res.AppCompatResources;
 
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
@@ -680,6 +681,7 @@ public class StripLayoutHelperManager
         getActiveStripLayoutHelper().finishAnimationsAndPushTabUpdates();
         if (!mModelSelectorButton.isVisible()) return;
         mTabModelSelector.selectModel(!mTabModelSelector.isIncognitoSelected());
+        RecordUserAction.record("MobileToolbarModelSelected");
     }
 
     @VisibleForTesting
