ae8b4ff70be9656abc2bf59de1202c749ee69d21
Joe DeBlasio <jdeblasio@chromium.org>
2024-07-02 16:57:31 +0000

    Harmlessly re-order a enums.xml entry
    This CL fixes a ordering issue in an enum used in a histogram. See the
    bug for why I think this is OK to do.
    
    Fixed: 349972040
    Change-Id: I5e1e71190f030a7b5754d4643df4b8f9bac32f5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5668593
    Auto-Submit: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322251}

--- chrome/browser/ssl/https_first_mode_settings_tracker.h
+++ chrome/browser/ssl/https_first_mode_settings_tracker.h
@@ -33,8 +33,8 @@ class StatefulSSLHostStateDelegate;
 // The set of valid states of the user-controllable HTTPS-First Mode setting.
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
-// Must be kept in sync with the HttpsFirstModeSetting enum located in
-// chrome/browser/resources/settings/privacy_page/security_page.js.
+// Must be kept in sync with the HttpsFirstModeSetting enums located in
+// chrome/browser/resources/settings/privacy_page/security_page.ts and enums.xml
 enum class HttpsFirstModeSetting {
   kDisabled = 0,
   kEnabledIncognito = 1,
