29d02afc2d77b8a632d7ffa2726dac132558ab1f
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2024-07-02 14:20:32 +0000

    [Interactive test] Add check esim name interactive test
    Adds check for eSIM profile name interactive test.
    
    Video: https://drive.google.com/file/d/1Q3fRkOjk3wbJzTuP_jRmatXrm2fpaB2y/view?usp=sharing
    
    
    Fixed: b/344673024
    Test: Ran CQ, added interactive test.
    Change-Id: Ie1cac266d69f7674f12103c905567e68c4904196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5648226
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1322159}

---
+++ chrome/test/base/ash/interactive/cellular/esim_ui_elements_interactive_uitest.cc
@@ -0,0 +1,93 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "chrome/test/base/ash/interactive/cellular/esim_util.h"
+#include "chrome/test/base/ash/interactive/interactive_ash_test.h"
+#include "chrome/test/base/ash/interactive/settings/interactive_uitest_elements.h"
+#include "chromeos/ash/components/dbus/hermes/hermes_euicc_client.h"
+#include "chromeos/ash/components/dbus/hermes/hermes_manager_client.h"
+#include "chromeos/ash/components/dbus/shill/shill_manager_client.h"
+#include "dbus/object_path.h"
+#include "ui/base/interaction/element_identifier.h"
+
+namespace ash {
+namespace {
+
+class EsimUiElementsUiTest : public InteractiveAshTest {
+ protected:
+  // InteractiveAshTest:
+  void SetUpOnMainThread() override {
+    InteractiveAshTest::SetUpOnMainThread();
+
+    // Set up context for element tracking for InteractiveBrowserTest.
+    SetupContextWidget();
+
+    // Ensure the OS Settings app is installed.
+    InstallSystemApps();
+
+    auto* hermes_manager_client =
+        HermesManagerClient::Get()->GetTestInterface();
+    ASSERT_TRUE(hermes_manager_client);
+
+    hermes_manager_client->ClearEuiccs();
+
+    hermes_manager_client->AddEuicc(dbus::ObjectPath(euicc_info_.path()),
+                                    euicc_info_.eid(),
+                                    /*is_active=*/true,
+                                    /*physical_slot=*/0);
+
+    auto* hermes_euicc_client = HermesEuiccClient::Get()->GetTestInterface();
+    ASSERT_TRUE(hermes_euicc_client);
+
+    hermes_euicc_client->AddCarrierProfile(
+        dbus::ObjectPath(esim_info_.profile_path()),
+        dbus::ObjectPath(euicc_info_.path()), esim_info_.iccid(),
+        esim_info_.name(), esim_info_.nickname(), esim_info_.service_provider(),
+        hermes_euicc_client->GenerateFakeActivationCode(),
+        /*network_service_path=*/esim_info_.service_path(),
+        /*state=*/hermes::profile::State::kActive,
+        /*profile_class=*/hermes::profile::ProfileClass::kOperational,
+        /*add_carrier_profile_behavior=*/
+        HermesEuiccClient::TestInterface::AddCarrierProfileBehavior::
+            kAddProfileWithService);
+
+    ShillServiceClient::Get()->Connect(
+        dbus::ObjectPath(esim_info_.service_path()), base::DoNothing(),
+        base::DoNothing());
+  }
+
+  const EsimInfo& esim_info() { return esim_info_; }
+
+ private:
+  const EuiccInfo euicc_info_{/*id=*/0};
+  const EsimInfo esim_info_{/*id=*/0};
+};
+
+IN_PROC_BROWSER_TEST_F(EsimUiElementsUiTest, OsSettingsDetailsPage) {
+  DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kOSSettingsId);
+
+  ui::ElementContext context =
+      LaunchSystemWebApp(SystemWebAppType::SETTINGS, kOSSettingsId);
+
+  // Run the following steps with the OS Settings context set as the default.
+  RunTestSequenceInContext(
+      context,
+
+      Log("Navigating to the details page for the eSIM network"),
+
+      NavigateToInternetDetailsPage(kOSSettingsId,
+                                    NetworkTypePattern::Cellular(),
+                                    esim_info().nickname()),
+
+      WaitForElementTextContains(
+          kOSSettingsId, settings::cellular::CellularDetailsSubpageTitle(),
+          /*text=*/esim_info().nickname()),
+
+      Log("Test complete"));
+}
+
+}  // namespace
+}  // namespace ash
