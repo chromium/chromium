8502059444500fc57290fd00a9d8c6849c1181be
Timothy Pearson <tpearson@raptorcs.com>
2024-07-02 01:05:38 +0000

    partition_alloc: Autodetect page size on ppc64 systems
    Similar to arm64, ppc64 systems are able to run in either 4k
    or 64k page mode depending on the kernel build.  Autodetect
    the page size in use on any particular ppc64 system vs. hard
    coding for 64k pages.
    
    Change-Id: I00fe4f644ee9322878035c87c6fc131166a6f32a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5650969
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321947}

--- base/allocator/partition_allocator/src/partition_alloc/page_allocator_constants.h
+++ base/allocator/partition_allocator/src/partition_alloc/page_allocator_constants.h
@@ -26,7 +26,8 @@
 #define PAGE_ALLOCATOR_CONSTANTS_DECLARE_CONSTEXPR __attribute__((const))
 
 #elif (PA_BUILDFLAG(IS_ANDROID) && PA_BUILDFLAG(PA_ARCH_CPU_64_BITS)) || \
-    (PA_BUILDFLAG(IS_LINUX) && PA_BUILDFLAG(PA_ARCH_CPU_ARM64))
+    (PA_BUILDFLAG(IS_LINUX) && PA_BUILDFLAG(PA_ARCH_CPU_ARM64)) || \
+    (PA_BUILDFLAG(IS_LINUX) && PA_BUILDFLAG(PA_ARCH_CPU_PPC64))
 // This should work for all POSIX (if needed), but currently all other
 // supported OS/architecture combinations use either hard-coded values
 // (such as x86) or have means to determine these values without needing
