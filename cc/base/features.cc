ea35278c5823ab9d2ff75f4c18815e96eb2ced89
Jonathan Ross <jonross@chromium.org>
2024-07-03 20:56:27 +0000

    Launch MetricsTracingCalculationReduction
    Moving to launch. While there was no significant performance gains at
    scale. Only doing this excess work for tracing when we are tracing is
    still correct.
    
    Bug: 41486236
    Change-Id: I5427251c652a37a408165a6d121fa1b12bcb0c2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676673
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323014}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -183,7 +183,7 @@ BASE_FEATURE(kClearCanvasResourcesInBackground,
 
 BASE_FEATURE(kMetricsTracingCalculationReduction,
              "MetricsTracingCalculationReduction",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPaintWithGainmapShader,
              "PaintWithGainmapShader",
