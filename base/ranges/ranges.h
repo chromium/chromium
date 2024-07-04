d4253cd5854c1f10600a735c8f5654fc90c9053d
Daniel Cheng <dcheng@chromium.org>
2024-07-04 15:18:19 +0000

    Remove `base::ranges::begin()` and `base::ranges::end()`
    Bug: 337356049
    Change-Id: I8a882fb7ce847ccccb60fa7c5a913055eeb0a50f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494911
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323365}

--- base/ranges/ranges.h
+++ base/ranges/ranges.h
@@ -15,92 +15,16 @@
 #include <type_traits>
 #include <utility>
 
+// TODO(dcheng): Remove this in a followup.
 #include "base/template_util.h"
 
-namespace base {
-
-namespace internal {
-
-// Overload for C array.
-template <typename T, size_t N>
-constexpr T* begin(T (&array)[N], priority_tag<2>) {
-  return array;
-}
-
-// Generic container overload.
-template <typename Range>
-constexpr auto begin(Range&& range, priority_tag<1>)
-    -> decltype(std::forward<Range>(range).begin()) {
-  return std::forward<Range>(range).begin();
-}
-
-// Overload for free begin() function.
-template <typename Range>
-constexpr auto begin(Range&& range, priority_tag<0>)
-    -> decltype(begin(std::forward<Range>(range))) {
-  return begin(std::forward<Range>(range));
-}
-
-// Overload for C array.
-template <typename T, size_t N>
-constexpr T* end(T (&array)[N], priority_tag<2>) {
-  return array + N;
-}
-
-// Generic container overload.
-template <typename Range>
-constexpr auto end(Range&& range, priority_tag<1>)
-    -> decltype(std::forward<Range>(range).end()) {
-  return std::forward<Range>(range).end();
-}
-
-// Overload for free end() function.
-template <typename Range>
-constexpr auto end(Range&& range, priority_tag<0>)
-    -> decltype(end(std::forward<Range>(range))) {
-  return end(std::forward<Range>(range));
-}
-
-}  // namespace internal
-
-namespace ranges {
-
-// Simplified implementation of C++20's std::ranges::begin.
-// As opposed to std::ranges::begin, this implementation does does not check
-// whether begin() returns an iterator and does not inhibit ADL.
-//
-// The trailing return type and dispatch to the internal implementation is
-// necessary to be SFINAE friendly.
-//
-// Reference: https://wg21.link/range.access.begin
-template <typename Range>
-constexpr auto begin(Range&& range) noexcept
-    -> decltype(internal::begin(std::forward<Range>(range),
-                                internal::priority_tag<2>())) {
-  return internal::begin(std::forward<Range>(range),
-                         internal::priority_tag<2>());
-}
-
-// Simplified implementation of C++20's std::ranges::end.
-// As opposed to std::ranges::end, this implementation does does not check
-// whether end() returns an iterator and does not inhibit ADL.
-//
-// The trailing return type and dispatch to the internal implementation is
-// necessary to be SFINAE friendly.
-//
-// Reference: - https://wg21.link/range.access.end
-template <typename Range>
-constexpr auto end(Range&& range) noexcept
-    -> decltype(internal::end(std::forward<Range>(range),
-                              internal::priority_tag<2>())) {
-  return internal::end(std::forward<Range>(range), internal::priority_tag<2>());
-}
+namespace base::ranges {
 
 // Implementation of C++20's std::ranges::iterator_t.
 //
 // Reference: https://wg21.link/ranges.syn#:~:text=iterator_t
 template <typename Range>
-using iterator_t = decltype(ranges::begin(std::declval<Range&>()));
+using iterator_t = decltype(std::begin(std::declval<Range&>()));
 
 // Implementation of C++20's std::ranges::range_value_t.
 //
@@ -108,8 +32,6 @@ using iterator_t = decltype(ranges::begin(std::declval<Range&>()));
 template <typename Range>
 using range_value_t = std::iter_value_t<iterator_t<Range>>;
 
-}  // namespace ranges
-
-}  // namespace base
+}  // namespace base::ranges
 
 #endif  // BASE_RANGES_RANGES_H_
