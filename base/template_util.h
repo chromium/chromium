aebd527ee25753581f0b969d8f32730740dc22c5
Daniel Cheng <dcheng@chromium.org>
2024-07-05 16:51:16 +0000

    Remove base::internal::priority_tag
    There are no longer any uses of this in Chrome.
    
    Bug: 337356049
    Change-Id: I2a39cfeb55c784e3e3119f6d07dc6bd49c6d4f64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5677883
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323743}

--- base/template_util.h
+++ base/template_util.h
@@ -26,15 +26,6 @@ struct is_iterator<
     std::void_t<typename std::iterator_traits<T>::iterator_category>>
     : std::true_type {};
 
-// Helper to express preferences in an overload set. If more than one overload
-// are available for a given set of parameters the overload with the higher
-// priority will be chosen.
-template <size_t I>
-struct priority_tag : priority_tag<I - 1> {};
-
-template <>
-struct priority_tag<0> {};
-
 }  // namespace internal
 
 }  // namespace base
