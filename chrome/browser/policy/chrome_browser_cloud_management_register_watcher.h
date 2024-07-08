249ee09c2a8d5e8e1647859ed3fa1e1636ce9dc8
Daniel Cheng <dcheng@chromium.org>
2024-07-08 15:16:42 +0000

    [MSan] Fix use-after-dtor in ChromeBrowserCloudManagementRegisterWatcher
    Ensure fields used by the dialog closed callback outlive the dialog.
    
    Bug: 40222690
    Change-Id: I41b780f91563d5c9785863dde4ba6af742b666e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5680832
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324235}

--- chrome/browser/policy/chrome_browser_cloud_management_register_watcher.h
+++ chrome/browser/policy/chrome_browser_cloud_management_register_watcher.h
@@ -112,15 +112,15 @@ class ChromeBrowserCloudManagementRegisterWatcher
 
   const raw_ptr<ChromeBrowserCloudManagementController> controller_;
 
+  std::optional<bool> register_result_;
+  base::Time visible_start_time_;
+
   base::RunLoop run_loop_;
   std::unique_ptr<EnterpriseStartupDialog> dialog_;
 
   bool is_restart_needed_ = false;
-  std::optional<bool> register_result_;
 
   DialogCreationCallback test_create_dialog_callback_;
-
-  base::Time visible_start_time_;
 };
 
 }  // namespace policy
