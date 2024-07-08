c742e122518ee276061af67dcafb5566b9e275dc
Jian Li <jianli@chromium.org>
2024-07-08 19:36:29 +0000

    M126: Check language for web feed
    (cherry picked from commit 6673a3cfc6a7521343b495a526505b7ecc78ae41)
    
    Bug: 349424259
    Change-Id: I89d4ddcb1440ba68c9798dd9b24b264cd6ada7c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5653584
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1319476}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5684513
    Reviewed-by: Sky Malice <skym@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6478@{#1724}
    Cr-Branched-From: e6143acc03189c5e52959545b110d6d17ecd5286-refs/heads/main@{#1300313}

--- components/feed/core/v2/feed_stream.cc
+++ components/feed/core/v2/feed_stream.cc
@@ -67,6 +67,7 @@
 #include "components/offline_pages/task/closure_task.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace feed {
 namespace {
@@ -631,7 +632,8 @@ bool FeedStream::IsFeedEnabledByDse() {
 }
 
 bool FeedStream::IsWebFeedEnabled() {
-  return feed::IsWebFeedEnabledForLocale(delegate_->GetCountry()) &&
+  return l10n_util::GetLanguage(delegate_->GetLanguageTag()) == "en" &&
+         feed::IsWebFeedEnabledForLocale(delegate_->GetCountry()) &&
          !delegate_->IsSupervisedAccount();
 }
 
