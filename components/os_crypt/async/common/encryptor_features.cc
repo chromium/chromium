eb2d3b108f6aae712160a0e471730c8a6728a5cc
Will Harris <wfh@chromium.org>
2024-07-08 21:59:25 +0000

    Enable ProtectEncryptionKey feature by default
    This feature will launch shortly so this CL enables it in the code
    by default.
    
    BUG=335630159
    
    Change-Id: I51fdd7f9f4f33ca2d7f097f482244d5e2acb2f80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685432
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324521}

--- components/os_crypt/async/common/encryptor_features.cc
+++ components/os_crypt/async/common/encryptor_features.cc
@@ -12,7 +12,7 @@ namespace os_crypt_async::features {
 #if BUILDFLAG(IS_WIN)
 BASE_FEATURE(kProtectEncryptionKey,
              "ProtectEncryptionKey",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_WIN)
 
 }  // namespace os_crypt_async::features
