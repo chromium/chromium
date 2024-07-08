443041e66abba73c61d8dfd1b626b2a5c5584b89
Henrique Nakashima <hnakashima@chromium.org>
2024-07-08 21:50:38 +0000

    [Android] Re-enable testExternalNavigationMessage in Android 12 and 13
    Likely fixed along with other versions by
    https://chromium-review.googlesource.com/c/chromium/src/+/5630143
    
    Bug: 339502391
    Change-Id: I5d2aa321972ea8e5aea5d794b20bdee4222b8974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5659026
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324517}

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -22,8 +22,6 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
-import android.os.Build;
-import android.os.Build.VERSION_CODES;
 import android.os.PatternMatcher;
 import android.os.SystemClock;
 import android.text.TextUtils;
@@ -64,7 +62,6 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -1630,10 +1627,6 @@ public class UrlOverridingTest {
 
     @Test
     @LargeTest
-    @DisableIf.Build(
-            sdk_is_greater_than = Build.VERSION_CODES.R,
-            sdk_is_less_than = VERSION_CODES.UPSIDE_DOWN_CAKE,
-            message = "crbug.com/339502391")
     public void testExternalNavigationMessage() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
 
