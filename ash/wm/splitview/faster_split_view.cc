5d1061984957725f6c3802d82879eda0629d7c63
Michele Fan <michelefan@chromium.org>
2024-07-08 21:32:25 +0000

    pe: Add shadow to the settings button
    Demo: http://b/349888239#comment2
    Fixed: b/349888239
    Test: Manually
    Change-Id: Ic067cb0a63deb2d4d7ab29c114b00ee1923db740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5674040
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324507}

--- ash/wm/splitview/faster_split_view.cc
+++ ash/wm/splitview/faster_split_view.cc
@@ -9,10 +9,12 @@
 #include "ash/style/ash_color_id.h"
 #include "ash/style/icon_button.h"
 #include "ash/style/style_util.h"
+#include "ash/style/system_shadow.h"
 #include "ash/system/toast/system_toast_view.h"
 #include "ash/wm/wm_constants.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/compositor/layer.h"
 #include "ui/views/controls/button/label_button.h"
 #include "ui/views/highlight_border.h"
 
@@ -24,7 +26,68 @@ namespace {
 // settings button.
 constexpr int kSettingsButtonSpacingDp = 8;
 
+// -----------------------------------------------------------------------------
+// FasterSplitViewSettingsButton:
+
+// Settings button in the faster split screen setup that can deep link to the
+// window snap preference section upon clicking.
+class FasterSplitViewSettingsButton : public IconButton {
+  METADATA_HEADER(FasterSplitViewSettingsButton, IconButton)
+
+ public:
+  explicit FasterSplitViewSettingsButton(
+      base::RepeatingClosure settings_callback)
+      : IconButton(std::move(settings_callback),
+                   IconButton::Type::kLarge,
+                   &kOverviewSettingsIcon,
+                   IDS_ASH_OVERVIEW_SETTINGS_BUTTON_LABEL),
+        shadow_(SystemShadow::CreateShadowOnTextureLayer(
+            SystemShadow::Type::kElevation4)) {
+    SetPaintToLayer();
+    layer()->SetFillsBoundsOpaquely(false);
+    SetBackgroundColor(kColorAshShieldAndBase80);
+
+    const float settings_button_corner_radius =
+        GetPreferredSize().height() / 2.0f;
+    SetBorder(std::make_unique<views::HighlightBorder>(
+        settings_button_corner_radius,
+        views::HighlightBorder::Type::kHighlightBorderOnShadow));
+    shadow_->SetRoundedCornerRadius(settings_button_corner_radius);
+
+    StyleUtil::SetUpFocusRingForView(this, kWindowMiniViewFocusRingHaloInset);
+  }
+
+  FasterSplitViewSettingsButton(const FasterSplitViewSettingsButton&) = delete;
+  FasterSplitViewSettingsButton& operator=(
+      const FasterSplitViewSettingsButton&) = delete;
+  ~FasterSplitViewSettingsButton() override = default;
+
+  // views::View:
+  void AddedToWidget() override {
+    // Since the layer of the shadow has to be added as a sibling to this view's
+    // layer, we need to wait until the view is added to the widget.
+    auto* parent = layer()->parent();
+    parent->Add(shadow_->GetLayer());
+    parent->StackAtBottom(shadow_->GetLayer());
+  }
+
+  void OnBoundsChanged(const gfx::Rect& previous_bounds) override {
+    // The shadow layer is a sibling of this view's layer, whose contents bounds
+    // should be the same as the view's bounds.
+    shadow_->SetContentBounds(layer()->bounds());
+  }
+
+ private:
+  std::unique_ptr<SystemShadow> shadow_;
+};
+
+BEGIN_METADATA(FasterSplitViewSettingsButton)
+END_METADATA
+
 }  // namespace
+
+// -----------------------------------------------------------------------------
+// FasterSplitView:
 
 FasterSplitView::FasterSplitView(base::RepeatingClosure skip_callback,
                                  base::RepeatingClosure settings_callback) {
@@ -38,26 +101,15 @@ FasterSplitView::FasterSplitView(base::RepeatingClosure skip_callback,
       l10n_util::GetStringUTF16(IDS_ASH_OVERVIEW_FASTER_SPLITSCREEN_TOAST_SKIP),
       /*dismiss_callback=*/std::move(skip_callback),
       /*leading_icon=*/&gfx::kNoneIcon, /*use_custom_focus=*/false));
-  toast->dismiss_button()->SetTooltipText(l10n_util::GetStringUTF16(
+  auto* dismiss_button = toast->dismiss_button();
+  dismiss_button->SetTooltipText(l10n_util::GetStringUTF16(
       IDS_ASH_OVERVIEW_FASTER_SPLITSCREEN_TOAST_DISMISS_WINDOW_SUGGESTIONS));
-  toast->dismiss_button()->SetID(kDismissButtonIDForTest);
+  dismiss_button->SetID(kDismissButtonIDForTest);
 
-  auto* settings_button = AddChildView(std::make_unique<IconButton>(
-      std::move(settings_callback), IconButton::Type::kLarge,
-      &kOverviewSettingsIcon, IDS_ASH_OVERVIEW_SETTINGS_BUTTON_LABEL));
-  settings_button->SetBackgroundColor(kColorAshShieldAndBase80);
+  auto* settings_button =
+      AddChildView(std::make_unique<FasterSplitViewSettingsButton>(
+          std::move(settings_callback)));
   settings_button->SetID(kSettingsButtonIDForTest);
-
-  views::FocusRing* focus_ring = StyleUtil::SetUpFocusRingForView(
-      settings_button, kWindowMiniViewFocusRingHaloInset);
-  focus_ring->SetOutsetFocusRingDisabled(true);
-  focus_ring->SetColorId(ui::kColorAshFocusRing);
-
-  const int toast_height = settings_button->GetPreferredSize().height();
-  const float toast_corner_radius = toast_height / 2.0f;
-  settings_button->SetBorder(std::make_unique<views::HighlightBorder>(
-      toast_corner_radius,
-      views::HighlightBorder::Type::kHighlightBorderOnShadow));
 }
 
 FasterSplitView::~FasterSplitView() = default;
