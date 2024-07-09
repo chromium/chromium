96807d774ac8463b95dee2aea92f3e629612d642
Sergey Ulanov <sergeyu@chromium.org>
2024-07-09 16:48:24 +0000

    [M127] Reland "[WebRTC][Fuchsia] Ignore private (ULA) local addresses"
    This is a reland of commit 2644dca494c61a6e1a701ebeec077aace31210d7
    
    Original change was reverted because it broke debug builds.
    Update the code to call rtc::IPIsPrivate() instead of
    rtc::IPIsPrivateNetwork(). IPIsPrivate is marked as RTC_EXPORT, so
    it's accessible in component builds. That doesn't change the logic
    as link-local and loopback addresses are already filtered.
    Also removed the `iface_addr.family() == AF_INET6` check - it's
    redundant because this branch is executed only for IPv6 addresses.
    
    Original change's description:
    > [WebRTC][Fuchsia] Ignore private (ULA) local addresses
    >
    > After https://crrev.com/1256859 WebRTC now allows ULA addresses to be
    > used as P2P candidates. This broke some applications on Fuchsia.
    > Temporarily work around this issue by skipping these addresses on
    > Fuchsia.
    >
    > Bug: 350111561
    > Bug: 341820248
    > Bug: 323820219
    > Change-Id: I3e17486205b04f2fbd050e5384738a2332a48c8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5668879
    > Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    > Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1322400}
    
    (cherry picked from commit ba6887034587e01ee3c166c0d9aa902537dfd1ed)
    
    Bug: 350111561
    Bug: 341820248
    Bug: 323820219
    Bug: 350799277
    Change-Id: I2ab9be71a608abc248f839ae6e7111dc5cabf545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675471
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1322938}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676995
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6533@{#1229}
    Cr-Branched-From: 7e0b87ec6b8cb5cb2969e1479fc25776e582721d-refs/heads/main@{#1313161}

--- third_party/blink/renderer/platform/p2p/ipc_network_manager.cc
+++ third_party/blink/renderer/platform/p2p/ipc_network_manager.cc
@@ -154,6 +154,15 @@ void IpcNetworkManager::OnNetworkListChanged(
         continue;
       }
 
+      // On Fuchsia skip private IPv6 addresses as they break some application.
+      // TODO(b/350111561): Remove once the applications are updated to handle
+      // ULA addresses properly.
+#if BUILDFLAG(IS_FUCHSIA)
+      if (rtc::IPIsPrivate(iface_addr)) {
+        continue;
+      }
+#endif  // BUILDFLAG(IS_FUCHSIA)
+
       use_default_ipv6_address |= (default_ipv6_local_address == it->address);
     }
     network->AddIP(iface_addr);
