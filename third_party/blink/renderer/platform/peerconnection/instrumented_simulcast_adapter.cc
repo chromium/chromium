0d9173168bbb9687eb3d141e0a27ebddb3cb4722
Hirokazu Honda <hiroh@chromium.org>
2024-07-09 03:35:40 +0000

    InstrumentedSimulcastAdapter: Add sequence checker
    This CL adds sequence checkers to
    InstrumentedSimulcastAdapter::EncoderFactoryAdapter.
    
    Bug: b:320555128
    Test: blink_platform_unittests
    Test: Google Meet call with DCHECK
    Change-Id: Id58c1c12c56834a733b708b7254f550fffc5ef38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679904
    Reviewed-by: Erik Språng <sprang@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324649}

--- third_party/blink/renderer/platform/peerconnection/instrumented_simulcast_adapter.cc
+++ third_party/blink/renderer/platform/peerconnection/instrumented_simulcast_adapter.cc
@@ -20,25 +20,36 @@ class InstrumentedSimulcastAdapter::EncoderFactoryAdapter
                         bool is_primary)
       : encoder_factory_(encoder_factory),
         state_callback_(state_callback),
-        is_primary_(is_primary) {}
+        is_primary_(is_primary) {
+    // The constructor is performed in the webrtc worker thread, not webrtc
+    // encoder sequence.
+    DETACH_FROM_SEQUENCE(encoder_sequence_);
+  }
+
+  ~EncoderFactoryAdapter() override {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
+  }
 
   // webrtc::VideoEncoderFactory implementations.
   std::vector<webrtc::SdpVideoFormat> GetSupportedFormats() const override {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
     return encoder_factory_->GetSupportedFormats();
   }
 
   std::vector<webrtc::SdpVideoFormat> GetImplementations() const override {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
     return encoder_factory_->GetImplementations();
   }
   CodecSupport QueryCodecSupport(
       const webrtc::SdpVideoFormat& format,
       std::optional<std::string> scalability_mode) const override {
+    DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
     return encoder_factory_->QueryCodecSupport(format, scalability_mode);
   }
   std::unique_ptr<webrtc::VideoEncoder> Create(
       const webrtc::Environment& env,
       const webrtc::SdpVideoFormat& format) override {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+    DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
     std::unique_ptr<webrtc::VideoEncoder> encoder =
         encoder_factory_->Create(env, format);
     next_encoder_id_ += is_primary_ ? 1 : -1;
@@ -55,8 +66,10 @@ class InstrumentedSimulcastAdapter::EncoderFactoryAdapter
   const raw_ptr<EncoderStateObserver> state_callback_;
   const bool is_primary_;
 
-  int next_encoder_id_ GUARDED_BY_CONTEXT(sequence_checker_);
-  SEQUENCE_CHECKER(sequence_checker_);
+  int next_encoder_id_ GUARDED_BY_CONTEXT(encoder_sequence_);
+
+  // WebRTC encoder sequence.
+  SEQUENCE_CHECKER(encoder_sequence_);
 };
 
 std::unique_ptr<InstrumentedSimulcastAdapter>
@@ -66,6 +79,9 @@ InstrumentedSimulcastAdapter::Create(
     webrtc::VideoEncoderFactory* secondary_encoder_factory,
     std::unique_ptr<EncoderStateObserver> encoder_state_observer,
     const webrtc::SdpVideoFormat& format) {
+  // InstrumentedSimulcastAdapter is created on the webrtc worker sequence.
+  // The operations (e.g. InitEncode() and Encode()) are performed in the
+  // encoder sequence.
   std::unique_ptr<EncoderFactoryAdapter> primary_factory_adapter;
   std::unique_ptr<EncoderFactoryAdapter> secondary_factory_adapter;
   if (primary_encoder_factory) {
@@ -86,6 +102,9 @@ InstrumentedSimulcastAdapter::Create(
 }
 
 InstrumentedSimulcastAdapter::~InstrumentedSimulcastAdapter() {
+  // The destructor is executed in the encoder sequence. This is checked by
+  // the sequence checker in EncoderFactoryAdapter.
+
   // EncoderStateObserver must outlive encoders.
   DestroyStoredEncoders();
 }
