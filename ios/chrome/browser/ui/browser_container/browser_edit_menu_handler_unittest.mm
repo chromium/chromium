df40b9f1a3fa094ef092ff20a603d61be0592c1e
Federica Germinario <fedegermi@google.com>
2024-07-10 12:35:43 +0000

    [iOS] Fix tests BrowserEditMenuHandlerTest.* for iOS18
    On iOS18 a new selector was added:
     - 1:c:_startWritingTools:
    This CL updates tests to check the correct expected menu on iOS18
    
    Fixed: 351291228
    Change-Id: I5099c49393eea0c8a246522cee95f9f0be453af1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678899
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325418}

--- ios/chrome/browser/ui/browser_container/browser_edit_menu_handler_unittest.mm
+++ ios/chrome/browser/ui/browser_container/browser_edit_menu_handler_unittest.mm
@@ -70,7 +70,47 @@ NSString* kPageHTML = @"<html>"
 
 // Return the base menu depending on the environment.
 NSMutableArray* GetExpectedMenu() {
-  if (@available(iOS 17.4, *)) {
+  if (@available(iOS 18, *)) {
+    return [NSMutableArray arrayWithArray:@[
+      @"0:m:com.apple.menu.standard-edit",
+      @"1:c:cut:",
+      @"1:c:copy:",
+      @"1:c:paste:",
+      @"1:c:delete:",
+      @"1:c:select:",
+      @"1:c:selectAll:",
+      @"0:m:com.apple.menu.replace",
+      @"1:c:promptForReplace:",
+      @"1:c:transliterateChinese:",
+      @"1:c:_insertDrawing:",
+      @"1:m:com.apple.menu.autofill",
+      @"2:m:com.apple.menu.insert-from-external-sources",
+      @"2:c:captureTextFromCamera:",
+      @"1:c:_startWritingTools:",
+      @"0:m:com.apple.menu.open",
+      @"0:m:com.apple.menu.format",
+      @"1:m:com.apple.menu.text-style",
+      @"2:c:toggleBoldface:",
+      @"2:c:toggleItalics:",
+      @"2:c:toggleUnderline:",
+      @"1:m:com.apple.menu.writing-direction",
+      @"2:c:makeTextWritingDirectionRightToLeft:",
+      @"2:c:makeTextWritingDirectionLeftToRight:",
+      @"1:c:_showTextFormattingOptions:",
+      @"0:m:com.apple.menu.lookup",
+      @"1:c:findSelected:",
+      @"1:c:_define:",
+      @"1:c:_translate:",
+      @"0:m:com.apple.menu.learn",
+      @"1:c:addShortcut:",
+      @"0:m:com.apple.command.speech",
+      @"1:c:_accessibilitySpeak:",
+      @"1:c:_accessibilitySpeakLanguageSelection:",
+      @"1:c:_accessibilityPauseSpeaking:",
+      @"0:m:com.apple.menu.share",
+      @"1:c:share:"
+    ]];
+  } else if (@available(iOS 17.4, *)) {
     return [NSMutableArray arrayWithArray:@[
       @"0:m:com.apple.menu.standard-edit",
       @"1:c:cut:",
