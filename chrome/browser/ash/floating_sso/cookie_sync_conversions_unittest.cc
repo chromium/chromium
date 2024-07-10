fd3f6f2f0ee53a44df48a74f0bc2a67585ad435c
Andrey Davydov <andreydav@google.com>
2024-07-10 11:03:00 +0000

    Reland "Read from sync store on FloatingSsoSyncBridge creation"
    This is a reland of commit 68a0ee7c19267500fdb6ab7f607cc7315d2f23d3.
    
    Revert was due to a circular dependency between Sync and Floating SSO
    targets, we fix it here by making Floating SSO depend only on
    //chrome/browser/sync:factories target. While at it, apply the same fix
    to targets which were mentioned under allow_circular_includes_from in
    chrome/browser/sync/BUILD.gn.
    
    Original change's description:
    > Read from sync store on FloatingSsoSyncBridge creation
    >
    > Reading the data from disk and saving it in memory.
    > Corresponding sync docs:
    > https://www.chromium.org/developers/design-documents/sync/model-api/#initialization
    >
    > Bug: b:346354110
    > Test: unit test FloatingSsoSyncBridgeTest.InitialEntities
    > Change-Id: Icf4793a7be9a0037cdb51e8726de75e45b04bd29
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675968
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Reviewed-by: Simon Hangl <simonha@google.com>
    > Commit-Queue: Andrey Davydov <andreydav@google.com>
    > Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1324212}
    
    Bug: b:346354110, 40251079
    Change-Id: I7d3dc88269c80f47ed1941a320a720133fce42d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5682996
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1325394}

--- chrome/browser/ash/floating_sso/cookie_sync_conversions_unittest.cc
+++ chrome/browser/ash/floating_sso/cookie_sync_conversions_unittest.cc
@@ -13,6 +13,7 @@
 #include "base/strings/strcat.h"
 #include "base/test/protobuf_matchers.h"
 #include "base/time/time.h"
+#include "chrome/browser/ash/floating_sso/cookie_sync_test_util.h"
 #include "components/sync/protocol/cookie_specifics.pb.h"
 #include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_constants.h"
@@ -22,18 +23,6 @@
 #include "url/gurl.h"
 
 namespace ash::floating_sso {
-
-namespace {
-
-constexpr char kNameForTests[] = "TestName";
-constexpr char kValueForTests[] = "TestValue";
-constexpr char kDomainForTests[] = "www.example.com";
-constexpr char kPathForTests[] = "/baz";
-constexpr char kTopLevelSiteForTesting[] = "https://toplevelsite.com";
-constexpr char kUrlForTesting[] = "https://www.example.com/test/foo.html";
-constexpr int kPortForTests = 19;
-
-}  // namespace
 
 // Verify that a cookie can be written to proto and then restored from it
 // without loosing any data.
@@ -74,38 +63,13 @@ TEST(CookieSyncConversionsTest, CookieToProtoAndBack) {
 // Verify that reading a cookie from Sync proto and then writing it back
 // without changes results in the same proto.
 TEST(CookieSyncConversionsTest, ProtoToCookieAndBack) {
-  sync_pb::CookieSpecifics sync_specifics;
-  sync_specifics.set_unique_key(
-      "https://toplevelsite.comtrueTestNamewww.example.com/baz219");
-  sync_specifics.set_name(kNameForTests);
-  sync_specifics.set_value(kValueForTests);
-  sync_specifics.set_domain(kDomainForTests);
-  sync_specifics.set_path(kPathForTests);
-  sync_specifics.set_creation_time_windows_epoch_micros(13357418862798591);
-  sync_specifics.set_expiry_time_windows_epoch_micros(0);
-  sync_specifics.set_last_access_time_windows_epoch_micros(13357418862798591);
-  sync_specifics.set_last_update_time_windows_epoch_micros(13357418862799017);
-  sync_specifics.set_secure(true);
-  sync_specifics.set_httponly(false);
-  sync_specifics.set_site_restrictions(
-      sync_pb::CookieSpecifics_CookieSameSite_UNSPECIFIED);
-  sync_specifics.set_priority(sync_pb::CookieSpecifics_CookiePriority_MEDIUM);
-  sync_specifics.set_source_scheme(
-      sync_pb::CookieSpecifics_CookieSourceScheme_SECURE);
-  sync_specifics.mutable_partition_key()->set_top_level_site(
-      kTopLevelSiteForTesting);
-  sync_specifics.mutable_partition_key()->set_has_cross_site_ancestor(true);
-  sync_specifics.set_source_port(kPortForTests);
-  sync_specifics.set_source_type(
-      sync_pb::CookieSpecifics_CookieSourceType_HTTP);
+  sync_pb::CookieSpecifics sync_specifics = DefaultCookieSpecificsForTest();
 
   std::unique_ptr<net::CanonicalCookie> cookie = FromSyncProto(sync_specifics);
-
   ASSERT_TRUE(cookie);
 
   std::optional<sync_pb::CookieSpecifics> restored_specifics =
       ToSyncProto(*cookie);
-
   ASSERT_TRUE(restored_specifics);
 
   EXPECT_THAT(sync_specifics, base::test::EqualsProto(*restored_specifics));
@@ -140,33 +104,12 @@ TEST(CookieSyncConversionsTest, PartitionKeyShouldBeSerializable) {
 // Verify that we don't build a CanonicalCookie object if the cookie
 // saved in Sync proto is not canonical.
 TEST(CookieSyncConversionsTest, NonCanonicalCookieInProto) {
-  sync_pb::CookieSpecifics sync_specifics;
-  sync_specifics.set_unique_key(
-      "https://toplevelsite.comtrueTestNamewww.example.com/baz219");
-  sync_specifics.set_name(kNameForTests);
-  sync_specifics.set_value(kValueForTests);
-  sync_specifics.set_domain(kDomainForTests);
-  sync_specifics.set_path(kPathForTests);
-  // Set creation time to null and last_access time to some valid value
-  // - this should result in a failure when trying to build a
+  sync_pb::CookieSpecifics sync_specifics = DefaultCookieSpecificsForTest();
+  // Overwrite creation time to be null while keeping last_access time a valid
+  // value. This should result in a failure when trying to build a
   // CanonicalCookie object.
   sync_specifics.set_creation_time_windows_epoch_micros(0);
-  sync_specifics.set_expiry_time_windows_epoch_micros(0);
-  sync_specifics.set_last_access_time_windows_epoch_micros(13357418862798591);
-  sync_specifics.set_last_update_time_windows_epoch_micros(13357418862799017);
-  sync_specifics.set_secure(true);
-  sync_specifics.set_httponly(false);
-  sync_specifics.set_site_restrictions(
-      sync_pb::CookieSpecifics_CookieSameSite_UNSPECIFIED);
-  sync_specifics.set_priority(sync_pb::CookieSpecifics_CookiePriority_MEDIUM);
-  sync_specifics.set_source_scheme(
-      sync_pb::CookieSpecifics_CookieSourceScheme_SECURE);
-  sync_specifics.mutable_partition_key()->set_top_level_site(
-      kTopLevelSiteForTesting);
-  sync_specifics.mutable_partition_key()->set_has_cross_site_ancestor(true);
-  sync_specifics.set_source_port(kPortForTests);
-  sync_specifics.set_source_type(
-      sync_pb::CookieSpecifics_CookieSourceType_HTTP);
+  ASSERT_GT(sync_specifics.last_access_time_windows_epoch_micros(), 0);
 
   std::unique_ptr<net::CanonicalCookie> cookie = FromSyncProto(sync_specifics);
 
--- chrome/browser/ash/floating_sso/floating_sso_service.h
+++ chrome/browser/ash/floating_sso/floating_sso_service.h
@@ -11,6 +11,7 @@
 #include "chrome/browser/ash/floating_sso/floating_sso_sync_bridge.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
+#include "components/sync/model/model_type_store.h"
 
 namespace syncer {
 class ModelTypeChangeProcessor;
@@ -25,7 +26,8 @@ class FloatingSsoService : public KeyedService {
  public:
   FloatingSsoService(
       PrefService* prefs,
-      std::unique_ptr<syncer::ModelTypeChangeProcessor> change_processor);
+      std::unique_ptr<syncer::ModelTypeChangeProcessor> change_processor,
+      syncer::OnceModelTypeStoreFactory create_store_callback);
   FloatingSsoService(const FloatingSsoService&) = delete;
   FloatingSsoService& operator=(const FloatingSsoService&) = delete;
 
