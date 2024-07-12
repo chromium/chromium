14b2f81bf8044d965faf16da914d265cdd800264
James Cook <jamescook@chromium.org>
2024-07-12 00:08:01 +0000

    birch: Localize strings in suggestion type context menu
    Localize the strings for the primary birch right-click menu.
    
    Bug: b:342667577
    Test: manually verified strings are correct in UI
    Change-Id: Idbbb6a68c84d715086e2475699e4261238f705d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5698007
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326476}

--- ash/wm/overview/birch/birch_bar_context_menu_model.cc
+++ ash/wm/overview/birch/birch_bar_context_menu_model.cc
@@ -5,9 +5,11 @@
 #include "ash/wm/overview/birch/birch_bar_context_menu_model.h"
 
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/wm/overview/overview_utils.h"
 #include "base/types/cxx23_to_underlying.h"
 #include "chromeos/ash/components/geolocation/simple_geolocation_provider.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/menu_separator_types.h"
 #include "ui/views/controls/menu/menu_types.h"
 
@@ -29,33 +31,38 @@ BirchBarContextMenuModel::BirchBarContextMenuModel(
     : ui::SimpleMenuModel(delegate) {
   // Show suggestions option is in both expanded and collapsed menu.
   AddItem(base::to_underlying(CommandId::kShowSuggestions),
-          u"Show suggestions");
+          l10n_util::GetStringUTF16(IDS_ASH_BIRCH_MENU_SHOW_SUGGESTIONS));
 
   // Expanded menu also has customizing suggestions options.
   if (type == Type::kExpandedBarMenu) {
     AddSeparator(ui::MenuSeparatorType::NORMAL_SEPARATOR);
 
     bool enabled = IsWeatherAllowedByGeolocation();
-    AddItem(base::to_underlying(CommandId::kWeatherSuggestions),
-            enabled ? u"Weather" : u"Weather (not available)");
+    std::u16string weather_label =
+        enabled ? l10n_util::GetStringUTF16(IDS_ASH_BIRCH_MENU_WEATHER)
+                : l10n_util::GetStringUTF16(
+                      IDS_ASH_BIRCH_MENU_WEATHER_NOT_AVAILABLE);
+    AddItem(base::to_underlying(CommandId::kWeatherSuggestions), weather_label);
     auto weather_index = GetIndexOfCommandId(
         base::to_underlying(CommandId::kWeatherSuggestions));
     SetEnabledAt(weather_index.value(), enabled);
     if (!enabled) {
-      // TODO(b/328486578): Localize string.
       SetMinorText(weather_index.value(),
-                   u"Weather isn't available because location access is "
-                   u"turned off. You can change this in settings.");
+                   l10n_util::GetStringUTF16(
+                       IDS_ASH_BIRCH_MENU_WEATHER_NOT_AVAILABLE_TOOLTIP));
     }
 
     AddItem(base::to_underlying(CommandId::kCalendarSuggestions),
-            u"Google Calendar");
-    AddItem(base::to_underlying(CommandId::kDriveSuggestions), u"Google Drive");
+            l10n_util::GetStringUTF16(IDS_ASH_BIRCH_MENU_CALENDAR));
+    AddItem(base::to_underlying(CommandId::kDriveSuggestions),
+            l10n_util::GetStringUTF16(IDS_ASH_BIRCH_MENU_DRIVE));
     AddItem(base::to_underlying(CommandId::kChromeTabSuggestions),
-            u"Chrome browser");
-    AddItem(base::to_underlying(CommandId::kMediaSuggestions), u"Media");
+            l10n_util::GetStringUTF16(IDS_ASH_BIRCH_MENU_CHROME_BROWSER));
+    AddItem(base::to_underlying(CommandId::kMediaSuggestions),
+            l10n_util::GetStringUTF16(IDS_ASH_BIRCH_MENU_MEDIA));
     AddSeparator(ui::MenuSeparatorType::NORMAL_SEPARATOR);
-    AddItemWithIcon(base::to_underlying(CommandId::kReset), u"Reset",
+    AddItemWithIcon(base::to_underlying(CommandId::kReset),
+                    l10n_util::GetStringUTF16(IDS_ASH_BIRCH_MENU_RESET),
                     CreateIconForMenuItem(kResetIcon));
   }
 }
