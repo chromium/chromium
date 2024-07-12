38b0ba06d47f8d5f6bab4368de1d3b0ca70d9662
Ziad Youssef <ziadyoussef@google.com>
2024-07-12 14:19:36 +0000

    Support removing preinstalled webview on devices using Trichrome
    Bug: 40240461
    Test: remove_preinstalled_webview.py --verbose
    Change-Id: I0cf040a247abe6b49435ed236215eea9a6eade3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5682998
    Reviewed-by: Peter Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Ziad Youssef <ziadyoussef@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326749}

--- android_webview/tools/remove_preinstalled_webview.py
+++ android_webview/tools/remove_preinstalled_webview.py
@@ -33,23 +33,90 @@ import logging
 import os
 import sys
 
-
 sys.path.append(os.path.join(
     os.path.dirname(__file__), os.pardir, os.pardir, 'build', 'android'))
 # pylint: disable=wrong-import-position,import-error
 import devil_chromium
 from devil.android import device_errors
 from devil.android import device_utils
+from devil.android.sdk import version_codes
 from devil.android.tools import script_common
 from devil.android.tools import system_app
 from devil.utils import logging_common
 
 WEBVIEW_PACKAGES = ['com.android.webview', 'com.google.android.webview']
 
+TRICHROME_WEBVIEW_PACKAGE = 'com.google.android.webview'
+TRICHROME_CHROME_PACKAGE = 'com.android.chrome'
+TRICHROME_LIBRARY_PACKAGE = 'com.google.android.trichromelibrary'
+
+ALREADY_UNINSTALLED_ERROR_MESSAGE = "DELETE_FAILED_INTERNAL_ERROR"
+
+
+def FindFilePath(device, file_name):
+  paths = device.RunShellCommand(['find', '/product', '-iname', file_name],
+                                 check_return=True)
+  assert len(paths) <= 1, ('Found multiple paths %s for %s' %
+                           (str(paths), file_name))
+  return paths
+
+
+def FindSystemAPKFiles(device, apk_name):
+  """The expected structure of WebViewGoogle system APK is one of the following:
+    On most Q+ devices and emulators:
+    /product/app/WebViewGoogle/WebViewGoogle.apk.gz
+    /product/app/WebViewGoogle-Stub/WebViewGoogle-Stub.apk
+    On Q and R emulators:
+    /product/app/WebViewGoogle/WebViewGoogle.apk
+
+    Others Trichrome system APKs follow a similar structure.
+  """
+  paths = []
+  paths.extend(FindFilePath(device, apk_name + '.apk.gz'))
+  paths.extend(FindFilePath(device, apk_name + '-Stub.apk'))
+  paths.extend(FindFilePath(device, apk_name + '.apk'))
+  if len(paths) == 0:
+    logging.info('%s system APK not found or already removed', apk_name)
+  return paths
+
+
+def RemoveTrichromeSystemAPKs(device):
+  """Removes Trichrome system APKs."""
+  logging.info('Removing Trichrome system APKs from %s...', device.serial)
+  paths = []
+  with system_app.EnableSystemAppModification(device):
+    for apk in ['WebViewGoogle', 'Chrome', 'TrichromeLibrary']:
+      paths.extend(FindSystemAPKFiles(device, apk))
+    device.RemovePath(paths, force=True, recursive=True)
+
+
+def UninstallTrichromePackages(device):
+  """Uninstalls Trichrome packages."""
+  logging.info('Uninstalling Trichrome packages from %s...', device.serial)
+  device.Uninstall(TRICHROME_WEBVIEW_PACKAGE)
+  device.Uninstall(TRICHROME_CHROME_PACKAGE)
+  # Keep uninstalling TRICHROME_LIBRARY_PACKAGE until we get
+  # device_errors.AdbCommandFailedError as multiple versions maybe installed.
+  # device.Uninstall doesn't work on shared libraries. We need to call Uninstall
+  # on AdbWrapper directly.
+  is_trichrome_library_installed = True
+  try:
+    # Limiting uninstalling to 10 times as a precaution.
+    for _ in range(10):
+      device.adb.Uninstall(TRICHROME_LIBRARY_PACKAGE)
+  except device_errors.AdbCommandFailedError as e:
+    if e.output and ALREADY_UNINSTALLED_ERROR_MESSAGE in e.output:
+      # Trichrome library is already uninstalled.
+      is_trichrome_library_installed = False
+  if is_trichrome_library_installed:
+    raise device_errors.CommandFailedError(
+        '{} is still installed on the device'.format(TRICHROME_LIBRARY_PACKAGE),
+        device)
+
 
 def UninstallWebViewSystemImages(device):
   """Uninstalls system images for known WebView packages."""
-  print('Removing system images from %s...' % device.serial)
+  logging.info('Removing system images from %s...', device.serial)
   system_app.RemoveSystemApps(device, WEBVIEW_PACKAGES)
   # Removing system apps will reboot the device, so we try to unlock the device
   # once that's done.
@@ -58,7 +125,7 @@ def UninstallWebViewSystemImages(device):
 
 def UninstallWebViewUpdates(device):
   """Uninstalls updates for WebView packages, if updates exist."""
-  print('Uninstalling updates from %s...' % device.serial)
+  logging.info('Uninstalling updates from %s...', device.serial)
   for webview_package in WEBVIEW_PACKAGES:
     try:
       device.Uninstall(webview_package)
@@ -80,9 +147,15 @@ def CheckWebViewIsUninstalled(device):
 
 def RemovePreinstalledWebViews(device):
   device.EnableRoot()
-  UninstallWebViewUpdates(device)
   try:
-    UninstallWebViewSystemImages(device)
+    if device.build_version_sdk >= version_codes.Q:
+      logging.warning('This is a Q+ device, so both WebView and Chrome will be '
+                      'removed.')
+      RemoveTrichromeSystemAPKs(device)
+      UninstallTrichromePackages(device)
+    else:
+      UninstallWebViewUpdates(device)
+      UninstallWebViewSystemImages(device)
     CheckWebViewIsUninstalled(device)
   except device_errors.CommandFailedError:
     if device.is_emulator:
@@ -95,7 +168,6 @@ def RemovePreinstalledWebViews(device):
                     '\n')
     raise
   device.SetWebViewFallbackLogic(False)  # Allow standalone WebView on N+
-
 
 def main():
   parser = argparse.ArgumentParser(description="""
