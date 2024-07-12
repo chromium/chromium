162df7bb423918ce9cda0a2e33ef0aabc03cdf10
Darren Shen <shend@chromium.org>
2024-07-12 02:57:16 +0000

    Revert "[A11y] Improvements to date/time picker accessibility"
    This reverts commit ef915b42e42d6f163706c9dd66250de277d03328.
    
    Reason for revert: Appears to cause Fuschia tests to fail: https://ci.chromium.org/ui/p/chromium/builders/ci/fuchsia-arm64-cast-receiver-rel/15789/test-results?sortby=&groupby=
    
    Original change's description:
    > [A11y] Improvements to date/time picker accessibility
    >
    > 1. Support tab navigation in the dropdown button whether or not the
    >    a11y tree is turned on. More about this below.
    > 2. Set all a11y properties whether a datalist is being used or not,
    >    including the role and that haspopup propery.
    > 3. Reuse code
    > 4. Needed to pass new a11y DCHECK in CL:5671013
    >
    > More about enabling tab navigation on the drop down:
    >
    > Before this CL, tab navigation to the dropdown button was conditional
    > based on whether the a11y tree is buing build. This code
    > was landed in WebKit in 2014, in
    > https://codereview.chromium.org/577553002.
    >
    > At that time, VoiceOver was the only screen reader supported, and
    > Apple did not have full keyboard access on by default. It was
    > common to condition full keyboard access on the presence of
    > VoiceOver. The original assumptions leading to this no longer hold.
    > Specifically, uUse of the a11y tree does not equate to using a11y features.
    > * Users need to be able to use full keyboard navigation even if
    >   they are not using the a11y tree, because the need for keyboard
    >   navigation is independent of the use of the a11y tree or an
    >   assistive technology.
    > * Presence of the a11y tree does not mean an assistive technology is
    >   being used. The a11y tree is often turned on by third party
    >   utilities completely unrelated to assistive technologies, such
    >   as password managers or window management tools, and OS features.
    >
    > Therefore, it does not make sense to use condition tabbing behavior
    > on the presence of the a11y cache.
    >
    > Bug: none
    > Change-Id: Ia3fba6b1f2fa891ad828aa9d6078da32c07a7996
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5692480
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1326453}
    
    Bug: none
    Change-Id: I1a2b5bdae5809f1d536d22d605b35a27984544ea
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5699547
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Owners-Override: Darren Shen <shend@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1326525}

--- third_party/blink/web_tests/fast/forms/resources/multiple-fields-blur-and-focus-events.js
+++ third_party/blink/web_tests/fast/forms/resources/multiple-fields-blur-and-focus-events.js
@@ -35,26 +35,24 @@ function startTestFor(typeName)
     shouldBeEqualToString('testInput.blur(); state()', 'blur=1 focus=1');
 
     if (window.eventSender) {
-        // Note: the number of tab stops is the number of fields + 1 for the
-        // drop down button.
-        var numberOfTabStops;
+        var numberOfFields;
         switch (typeName) {
         case 'week':
         case 'month':
-            numberOfTabStops = 3;
+            numberOfFields = 2;
             break;
         case 'datetime':
         case 'datetime-local':
-            numberOfTabStops = 7;
+            numberOfFields = 6;
             break;
         default:
-            numberOfTabStops = 4;
+            numberOfFields = 3;
             break;
         }
 
         beginTestCase('focus and Tab key to blur');
         document.getElementById("before").focus();
-        for (var i = 0; i < numberOfTabStops; i++)
+        for (var i = 0; i < numberOfFields; i++)
             shouldBeEqualToString('keyDown("\t"); state()', 'blur=0 focus=1');
         shouldBeEqualToString('keyDown("\t"); state()', 'blur=1 focus=1');
     } else {
