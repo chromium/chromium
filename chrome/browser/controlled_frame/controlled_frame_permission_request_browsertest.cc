b8948ba798246c54e80998a8fa1c0a010c56e110
Zelin Liu <zelin@chromium.org>
2024-07-15 18:04:17 +0000

    controlledframe: Add download permissions browser test
    Bug: b/349854056 b/40191772
    Change-Id: I8483228882b84b7360843f996690826ed3a8b1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704507
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327641}

--- chrome/browser/controlled_frame/controlled_frame_permission_request_browsertest.cc
+++ chrome/browser/controlled_frame/controlled_frame_permission_request_browsertest.cc
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+#include <optional>
 #include <set>
 #include <string>
 #include <vector>
 
+#include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/chrome_content_browser_client.h"
@@ -14,17 +17,22 @@
 #include "chrome/browser/hid/chrome_hid_delegate.h"
 #include "chrome/browser/hid/hid_chooser_context_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/hid/hid_chooser_controller.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h"
 #include "chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_builder.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/download/public/common/download_item.h"
 #include "components/permissions/mock_chooser_controller_view.h"
+#include "content/public/browser/download_manager.h"
 #include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "content/public/test/download_test_observer.h"
 #include "extensions/browser/event_router.h"
 #include "extensions/browser/guest_view/web_view/web_view_guest.h"
 #include "extensions/common/extension_features.h"
@@ -35,7 +43,9 @@
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom-forward.h"
 #include "url/origin.h"
 
+using testing::Contains;
 using testing::EndsWith;
+using testing::NotNull;
 using testing::StartsWith;
 
 namespace controlled_frame {
@@ -68,7 +78,7 @@ struct PermissionRequestTestParam {
   bool calls_allow;
   EmbedderPolicy embedder_policy;
   bool has_embedder_content_setting;
-  std::string expected_result;
+  bool expected_success;
 };
 
 const std::vector<PermissionRequestTestParam> kTestParams{
@@ -76,32 +86,31 @@ const std::vector<PermissionRequestTestParam> kTestParams{
      .calls_allow = true,
      .embedder_policy = EmbedderPolicy::kBothEmbedderAndRequestingOrigin,
      .has_embedder_content_setting = true,
-     .expected_result = "SUCCESS"},
+     .expected_success = true},
     {.name = "FailsBecauseNotAllow",
      .calls_allow = false,
      .embedder_policy = EmbedderPolicy::kBothEmbedderAndRequestingOrigin,
      .has_embedder_content_setting = true,
-     .expected_result = "FAIL"},
+     .expected_success = false},
     {.name = "FailsBecauseEmbedderDoesNotHavePermissionsPolicy",
      .calls_allow = true,
      .embedder_policy = EmbedderPolicy::kNoPolicy,
      .has_embedder_content_setting = true,
-     .expected_result = "FAIL"},
+     .expected_success = false},
     {.name = "FailsBecauseEmbedderPermissionsPolicyMissingEmbedderOrigin",
      .calls_allow = true,
      .embedder_policy = EmbedderPolicy::kNoEmbedderOrigin,
-     .has_embedder_content_setting = true,
-     .expected_result = "FAIL"},
+     .has_embedder_content_setting = true},
     {.name = "FailsBecauseEmbedderPermissionsPolicyMissingRequestingOrigin",
      .calls_allow = true,
      .embedder_policy = EmbedderPolicy::kNoRequestingOrigin,
      .has_embedder_content_setting = true,
-     .expected_result = "FAIL"},
+     .expected_success = false},
     {.name = "FailsBecauseNoEmbedderContentSettings",
      .calls_allow = true,
      .embedder_policy = EmbedderPolicy::kBothEmbedderAndRequestingOrigin,
      .has_embedder_content_setting = false,
-     .expected_result = "FAIL"},
+     .expected_success = false},
 };
 
 class PermissionRequestEventObserver
@@ -163,10 +172,13 @@ class ControlledFramePermissionRequestTest
   }
 
   void RunTestAndVerify(const PermissionRequestTestCase& test_case,
-                        const PermissionRequestTestParam& test_param) {
+                        const PermissionRequestTestParam& test_param,
+                        std::optional<base::OnceCallback<std::string(bool)>>
+                            get_expected_result_callback = std::nullopt) {
     // If the permission has no dependent permissions policy feature, then skip
     // the true negative permissions policy test cases.
-    if (test_param.embedder_policy == EmbedderPolicy::kNoPolicy &&
+    if (test_param.embedder_policy !=
+            EmbedderPolicy::kBothEmbedderAndRequestingOrigin &&
         test_case.policy_features.empty()) {
       return;
     }
@@ -225,9 +237,18 @@ class ControlledFramePermissionRequestTest
     extensions::EventRouter::Get(profile())->AddObserverForTesting(
         &event_observer);
 
-    EXPECT_THAT(content::EvalJs(controlled_frame, test_case.test_script)
-                    .ExtractString(),
-                StartsWith(test_param.expected_result));
+    base::OnceCallback<std::string(bool)> callback =
+        get_expected_result_callback.has_value()
+            ? std::move(get_expected_result_callback.value())
+            : base::BindLambdaForTesting(
+                  [](bool should_success) -> std::string {
+                    return should_success ? "SUCCESS" : "FAIL";
+                  });
+
+    EXPECT_THAT(
+        content::EvalJs(controlled_frame, test_case.test_script)
+            .ExtractString(),
+        StartsWith(std::move(callback).Run(test_param.expected_success)));
 
     // TODO(b/349841268): Make permissions policy check happen before extensions
     // event for media permissions.
@@ -332,6 +353,84 @@ IN_PROC_BROWSER_TEST_P(ControlledFramePermissionRequestTest, Geolocation) {
 
   PermissionRequestTestParam test_param = GetParam();
   RunTestAndVerify(test_case, test_param);
+}
+
+class TestDownloadManagerObserver : public content::DownloadManager::Observer {
+ public:
+  void OnDownloadCreated(content::DownloadManager* manager,
+                         download::DownloadItem* item) override {
+    CHECK(item);
+    downloads_.push_back(item->GetSuggestedFilename());
+  }
+
+  const std::vector<std::string>& Downloads() const { return downloads_; }
+
+ private:
+  std::vector<std::string> downloads_;
+};
+
+IN_PROC_BROWSER_TEST_P(ControlledFramePermissionRequestTest, Download) {
+  const std::string download_script = R"(
+    (function() {
+      try {
+          const link = document.createElement("a");
+          link.download = $1;
+          link.href = $1;
+          link.click();
+          return 'SUCCESS';
+      } catch (err) {
+        return 'FAIL: ' + err.name + ': ' + err.message;
+      }
+    })();
+  )";
+
+  PermissionRequestTestCase test_case;
+  test_case.test_script =
+      content::JsReplace(download_script, "download_test.zip");
+  test_case.permission_name = "download";
+
+  PermissionRequestTestParam test_param = GetParam();
+
+  content::DownloadTestObserverTerminal completion_observer(
+      profile()->GetDownloadManager(), test_param.expected_success ? 2 : 1,
+      content::DownloadTestObserver::ON_DANGEROUS_DOWNLOAD_FAIL);
+
+  TestDownloadManagerObserver download_observer;
+  profile()->GetDownloadManager()->AddObserver(&download_observer);
+
+  RunTestAndVerify(
+      test_case, test_param,
+      base::BindLambdaForTesting(
+          [](bool should_success) -> std::string { return "SUCCESS"; }));
+
+  // If |completion_observer| is expecting 0 downloads, then it will not wait
+  // for unexpected downloads. To avoid this, We execute another download in a
+  // normal tab, so at least one download will be waited on.
+  {
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+
+    ASSERT_TRUE(content::NavigateToURL(
+        web_contents, embedded_https_test_server().base_url()));
+
+    ASSERT_THAT(content::EvalJs(web_contents->GetPrimaryMainFrame(),
+                                content::JsReplace(download_script,
+                                                   "download_baseline.txt"))
+                    .ExtractString(),
+                StartsWith("SUCCESS"));
+  }
+
+  completion_observer.WaitForFinished();
+
+  EXPECT_EQ(download_observer.Downloads().size(),
+            test_param.expected_success ? 2ul : 1ul);
+
+  EXPECT_THAT(download_observer.Downloads(), Contains("download_baseline.txt"));
+  if (download_observer.Downloads().size() == 2ul) {
+    EXPECT_THAT(download_observer.Downloads(), Contains("download_test.zip"));
+  }
+
+  profile()->GetDownloadManager()->RemoveObserver(&download_observer);
 }
 
 INSTANTIATE_TEST_SUITE_P(/*no prefix*/
