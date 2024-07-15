859171ed107c16964348075b4136a677e40d467d
Peter Wen <wnwen@chromium.org>
2024-07-15 16:41:34 +0000

    pgo: Refactor generate_profile.py
    This prepares us for splitting benchmarks into individual stories on
    mobile.
    
    No functionality change.
    
    Bug: 352333007
    Change-Id: Id1fc7062354b28acc6d5888f2dfeb90ca62fc6dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5698545
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327561}

--- tools/pgo/generate_profile.py
+++ tools/pgo/generate_profile.py
@@ -44,19 +44,16 @@ LLVM_DIR = f'{ROOT_DIR}/third_party/llvm-build/Release+Asserts'
 PROFDATA = f'{LLVM_DIR}/bin/llvm-profdata' + exe_ext
 
 
-def main():
-    if not os.path.exists(PROFDATA):
-        print(f'{PROFDATA} does not exist, downloading it')
-        subprocess.run([sys.executable, UPDATE_PY, '--package=coverage_tools'],
-                       check=True)
-
+def parse_args():
     parser = argparse.ArgumentParser(
         epilog=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-C',
-                        help='Path to build directory',
-                        required=True,
-                        metavar='builddir',
-                        dest='builddir')
+                        '--builddir',
+                        help='Path to build directory.',
+                        required=True)
+    parser.add_argument('--profiledir',
+                        help='Path to store temporary profiles, default is '
+                        'builddir/profile.')
     parser.add_argument('--keep-temps',
                         action='store_true',
                         help='Whether to keep temp files')
@@ -86,7 +83,7 @@ def main():
         help='Add flags necessary for temporal PGO (experimental).')
     parser.add_argument(
         '-r',
-        '--repeat',
+        '--repeats',
         type=int,
         default=5,
         help='Number of times to attempt each benchmark if it fails, default 5.'
@@ -96,6 +93,7 @@ def main():
                         action='count',
                         default=0,
                         help='Increase verbosity level (repeat as needed)')
+
     args = parser.parse_args()
 
     # This is load-bearing:
@@ -104,151 +102,181 @@ def main():
     #   be an absolute path
     # Both are based off this variable.
     # See also https://crbug.com/1478279
-    builddir = os.path.realpath(args.builddir)
-    if sys.platform == 'darwin':
-        chrome_path = f'{builddir}/Chromium.app/Contents/MacOS/Chromium'
-    elif args.android_browser:
-        chrome_path = None
-        if not args.android_device_path:
-            for settings in ANDROID_SETTINGS:
-                if settings.browser_type == args.android_browser:
-                    package = settings.package
-                    break
-            else:
-                assert False, f'Unable to find {args.android_browser} settings.'
-            args.android_device_path = (
-                f'/data/data/{package}/cache/pgo_profiles')
+    args.builddir = os.path.realpath(args.builddir)
+
+    if args.android_browser and not args.android_device_path:
+        for settings in ANDROID_SETTINGS:
+            if settings.browser_type == args.android_browser:
+                package = settings.package
+                break
+        else:
+            assert False, f'Unable to find {args.android_browser} settings.'
+        args.android_device_path = (f'/data/data/{package}/cache/pgo_profiles')
+
+    if not args.profiledir:
+        args.profiledir = f'{args.builddir}/profile'
+
+    return args
+
+
+def run_benchmark(benchmark_args: list[str], args):
+    '''Puts profdata in {profiledir}/{args[0]}.profdata'''
+    name = benchmark_args[0]
+
+    # Clean up intermediate files from previous runs.
+    profraw_path = f'{args.profiledir}/{name}/raw'
+    if os.path.exists(profraw_path):
+        shutil.rmtree(profraw_path)
+    os.makedirs(profraw_path, exist_ok=True)
+    profdata_path = f'{args.profiledir}/{name}.profdata'
+    if os.path.exists(profdata_path):
+        os.remove(profdata_path)
+
+    env = os.environ.copy()
+    if args.android_browser:
+        env['CHROME_PGO_PROFILING'] = '1'
     else:
-        chrome_path = f'{builddir}/chrome' + exe_ext
-    profiledir = f'{builddir}/profile'
+        env['LLVM_PROFILE_FILE'] = f'{profraw_path}/default-%2m.profraw'
 
-    def run_benchmark(benchmark_args):
-        '''Puts profdata in {profiledir}/{args[0]}.profdata'''
-        name = benchmark_args[0]
+    cmd = ['vpython3', 'tools/perf/run_benchmark'] + benchmark_args + [
+        '--assert-gpu-compositing',
+        # Abort immediately when any story fails, since a failed story fails
+        # to produce valid profdata. Fail fast and rely on repeats to get a
+        # valid profdata.
+        '--max-failures=0',
+    ] + ['-v'] * args.verbose
 
-        # Clean up intermediate files from previous runs.
-        profraw_path = f'{profiledir}/{name}/raw'
-        if os.path.exists(profraw_path):
-            shutil.rmtree(profraw_path)
-        os.makedirs(profraw_path, exist_ok=True)
-        profdata_path = f'{profiledir}/{name}.profdata'
-        if os.path.exists(profdata_path):
-            os.remove(profdata_path)
-
-        env = os.environ.copy()
-        if args.android_browser:
-            env['CHROME_PGO_PROFILING'] = '1'
+    if args.android_browser:
+        cmd += [
+            f'--browser={args.android_browser}',
+            '--fetch-device-data',
+            '--fetch-device-data-platform=android',
+            f'--fetch-data-path-device={args.android_device_path}',
+            f'--fetch-data-path-local={profraw_path}',
+        ]
+    else:
+        if sys.platform == 'darwin':
+            exe_path = f'{args.builddir}/Chromium.app/Contents/MacOS/Chromium'
         else:
-            env['LLVM_PROFILE_FILE'] = f'{profraw_path}/default-%2m.profraw'
+            exe_path = f'{args.builddir}/chrome' + exe_ext
+        cmd += [
+            '--browser=exact',
+            f'--browser-executable={exe_path}',
+        ]
 
-        cmd = ['vpython3', 'tools/perf/run_benchmark'] + benchmark_args + [
-            '--assert-gpu-compositing',
-            # Abort immediately when any story fails, since a failed story fails
-            # to produce valid profdata. Fail fast and rely on repeats to get a
-            # valid profdata.
-            '--max-failures=0',
-        ] + ['-v'] * args.verbose
+    subprocess.run(cmd,
+                   check=True,
+                   shell=sys.platform == 'win32',
+                   env=env,
+                   cwd=ROOT_DIR)
 
-        if args.android_browser:
-            cmd += [
-                f'--browser={args.android_browser}',
-                '--fetch-device-data',
-                '--fetch-device-data-platform=android',
-                f'--fetch-data-path-device={args.android_device_path}',
-                f'--fetch-data-path-local={profraw_path}',
-            ]
-        else:
-            cmd += [
-                '--browser=exact',
-                f'--browser-executable={chrome_path}',
-            ]
+    if not args.skip_profdata:
+        # Android's `adb pull` does not allow * globbing (i.e. pulling
+        # pgo_profiles/*). Since the naming of profraw files can vary, pull
+        # the directory and check subdirectories recursively for .profraw
+        # files.
+        profraw_files = glob.glob(f'{profraw_path}/**/*.profraw',
+                                  recursive=True)
+        if not profraw_files:
+            raise RuntimeError(f'No profraw files found in {profraw_path}')
+        subprocess.run([PROFDATA, 'merge', '-o', profdata_path] +
+                       profraw_files,
+                       check=True)
 
-        subprocess.run(cmd,
-                       check=True,
-                       shell=sys.platform == 'win32',
-                       env=env,
-                       cwd=ROOT_DIR)
 
-        if not args.skip_profdata:
-            # Android's `adb pull` does not allow * globbing (i.e. pulling
-            # pgo_profiles/*). Since the naming of profraw files can vary, pull
-            # the directory and check subdirectories recursively for .profraw
-            # files.
-            subprocess.run(
-                [PROFDATA, 'merge', '-o', profdata_path] +
-                glob.glob(f'{profraw_path}/**/*.profraw', recursive=True),
-                check=True)
-
-    def run_benchmark_with_repeats(benchmark_args, repeats=args.repeat):
-        assert repeats > 0, 'repeats must be at least 1'
-        for idx in range(repeats):
-            try:
-                if idx > 0:
-                    print(f'Running attempt {idx} for {benchmark_args}')
-                run_benchmark(benchmark_args)
-            except subprocess.CalledProcessError as e:
-                if idx < repeats - 1:
-                    print(e)
-                    print(f'Retrying... ')
-                else:
-                    print(f'Failed {repeats} times')
-                    raise e
+def run_benchmark_with_repeats(benchmark_args: list[str], args):
+    assert args.repeats > 0, 'repeats must be at least 1'
+    for idx in range(args.repeats):
+        try:
+            print(f'Running attempt {idx} for {benchmark_args}')
+            run_benchmark(benchmark_args, args)
+            # Succeeded!
+            return
+        except subprocess.CalledProcessError as e:
+            if idx < args.repeats - 1:
+                print(e)
+                print(f'Retrying... ')
             else:
-                # Succeeded!
-                return
+                print(f'Failed {args.repeats} times')
+                raise e
 
-    if os.path.exists(profiledir):
-        shutil.rmtree(profiledir)
+
+def run_benchmarks(benchmarks: list[list[str]], args):
+    for benchmark_args in benchmarks:
+        run_benchmark_with_repeats(benchmark_args, args)
+
+
+def merge_profdata(args):
+    merge_cmd = [PROFDATA, 'merge']
+    if args.temporal_trace_length:
+        merge_cmd += [
+            '--temporal-profile-max-trace-length',
+            str(args.temporal_trace_length)
+        ]
+    profile_output_path = f'{args.builddir}/profile.profdata'
+    merge_cmd += ['-o', profile_output_path]
+    profdata_files = glob.glob(f'{args.profiledir}/*.profdata')
+    if not profdata_files:
+        print(f'No profdata files found in {args.profiledir}')
+    subprocess.run(merge_cmd + profdata_files, check=True)
+
+    if args.temporal_trace_length:
+        orderfile_cmd = [
+            PROFDATA, 'order', profile_output_path, '-o',
+            f'{args.builddir}/orderfile.txt'
+        ]
+        subprocess.run(orderfile_cmd, check=True)
+
+
+def main():
+    args = parse_args()
+
+    if not os.path.exists(PROFDATA):
+        print(f'{PROFDATA} does not exist, downloading it')
+        subprocess.run([sys.executable, UPDATE_PY, '--package=coverage_tools'],
+                       check=True)
+    assert os.path.exists(PROFDATA), f'{PROFDATA} does not exist'
+
+    if os.path.exists(args.profiledir):
+        print(f'Removing {args.profiledir}')
+        shutil.rmtree(args.profiledir)
 
     # Run the shortest benchmarks first to fail early if anything is wrong.
-    run_benchmark_with_repeats(['speedometer3'])
-    run_benchmark_with_repeats(['jetstream2'])
+    benchmarks: list[list[str]] = [
+        ['speedometer3'],
+        ['jetstream2'],
+    ]
 
     # These benchmarks require special access permissions:
     # https://www.chromium.org/developers/telemetry/upload_to_cloud_storage/#request-access-for-google-partners
     if not args.run_public_benchmarks_only:
-        run_benchmark_with_repeats([
-            'system_health.common_mobile'
-            if args.android_browser else 'system_health.common_desktop',
+        platform = 'mobile' if args.android_browser else 'desktop'
+        benchmarks.append([
+            f'system_health.common_{platform}',
             '--run-abridged-story-set',
         ])
-        run_benchmark_with_repeats([
-            'rendering.mobile'
-            if args.android_browser else 'rendering.desktop',
+        benchmarks.append([
+            f'rendering.{platform}',
             '--also-run-disabled-tests',
             '--story-tag-filter=motionmark_fixed_2_seconds',
             '--story-filter-exclude=motionmark_fixed_2_seconds_images',
         ])
         if sys.platform == 'darwin':
-            run_benchmark_with_repeats([
-                'rendering.desktop',
+            benchmarks.append([
+                f'rendering.{platform}',
                 '--also-run-disabled-tests',
                 '--story-tag-filter=motionmark_fixed_2_seconds',
                 '--extra-browser-args=--enable-features=SkiaGraphite',
             ])
 
+    run_benchmarks(benchmarks, args)
+
     if not args.skip_profdata:
-        merge_cmd = [PROFDATA, 'merge']
-        if args.temporal_trace_length:
-            merge_cmd += [
-                '--temporal-profile-max-trace-length',
-                str(args.temporal_trace_length)
-            ]
-        profile_output_path = f'{builddir}/profile.profdata'
-        merge_cmd += ['-o', profile_output_path]
-        subprocess.run(merge_cmd + glob.glob(f'{profiledir}/*.profdata'),
-                       check=True)
-
-        if args.temporal_trace_length:
-            orderfile_cmd = [
-                PROFDATA, 'order', profile_output_path, '-o',
-                f'{builddir}/orderfile.txt'
-            ]
-            subprocess.run(orderfile_cmd, check=True)
-
+        merge_profdata(args)
 
     if not args.keep_temps:
-        shutil.rmtree(profiledir)
+        print(f'Cleaning up {args.profiledir}, use --keep-temps to keep it.')
+        shutil.rmtree(args.profiledir, ignore_errors=True)
 
 
 if __name__ == '__main__':
