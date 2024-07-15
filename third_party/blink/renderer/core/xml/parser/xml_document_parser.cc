3e704b325ccd7f42e67b3e5914cfb96759354bee
Joey Arhar <jarhar@chromium.org>
2024-07-15 16:16:11 +0000

    Migrate off of deprecated libxml methods
    In the next roll of libxml I am going to make, a bunch of struct members
    will be marked as deprecated. This patch migrates off of them as
    recommended here: https://gitlab.gnome.org/GNOME/libxml2/-/issues/767
    
    I am doing this in a separate patch from the roll in case anything
    breaks.
    
    The old code is kept behind an #if to support people who build chrome
    without our included copy of libxml.
    
    Co-authored-by: Nick Wellnhofer <wellnhofer@aevum.de>
    Change-Id: Idd2fe9b9a5e54d5af1a382037314f9def64395cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703800
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327545}

--- third_party/blink/renderer/core/xml/parser/xml_document_parser.cc
+++ third_party/blink/renderer/core/xml/parser/xml_document_parser.cc
@@ -538,8 +538,9 @@ static inline void SetAttributes(
 
 static void SwitchEncoding(xmlParserCtxtPtr ctxt, bool is_8bit) {
   // Make sure we don't call xmlSwitchEncoding in an error state.
-  if ((ctxt->errNo != XML_ERR_OK) && (ctxt->disableSAX == 1))
+  if (ctxt->errNo != XML_ERR_OK) {
     return;
+  }
 
   if (is_8bit) {
     xmlSwitchEncoding(ctxt, XML_CHAR_ENCODING_8859_1);
@@ -717,9 +718,8 @@ scoped_refptr<XMLParserContext> XMLParserContext::CreateStringParser(
   InitializeLibXMLIfNecessary();
   xmlParserCtxtPtr parser =
       xmlCreatePushParserCtxt(handlers, nullptr, nullptr, 0, nullptr);
-  xmlCtxtUseOptions(parser, XML_PARSE_HUGE);
+  xmlCtxtUseOptions(parser, XML_PARSE_HUGE | XML_PARSE_NOENT);
   parser->_private = user_data;
-  parser->replaceEntities = true;
   return base::AdoptRef(new XMLParserContext(parser));
 }
 
@@ -747,13 +747,16 @@ scoped_refptr<XMLParserContext> XMLParserContext::CreateMemoryParser(
   xmlCtxtUseOptions(parser,
                     XML_PARSE_NODICT | XML_PARSE_NOENT | XML_PARSE_HUGE);
 
-  // Internal initialization
+#if LIBXML_VERSION < 21300
+  // Internal initialization required before libxml2 2.13.
+  // Fixed with https://gitlab.gnome.org/GNOME/libxml2/-/commit/8c5848bd
   parser->sax2 = 1;
   parser->instate = XML_PARSER_CONTENT;  // We are parsing a CONTENT
   parser->depth = 0;
   parser->str_xml = xmlDictLookup(parser->dict, BAD_CAST "xml", 3);
   parser->str_xmlns = xmlDictLookup(parser->dict, BAD_CAST "xmlns", 5);
   parser->str_xml_ns = xmlDictLookup(parser->dict, XML_XML_NAMESPACE, 36);
+#endif
   parser->_private = user_data;
 
   return base::AdoptRef(new XMLParserContext(parser));
