164c4373713d49036a5b9f07c5244a5f479b2df2
Noah Rose Ledesma <noahrose@google.com>
2024-07-16 22:48:01 +0000

    Create installer for Linux
    Create a Linux installer and define an interface for all platforms.
    
    Bug: 344878963
    Change-Id: I5b0c4797b3b0061b58bf69b70bafda3e058e5607
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5714654
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328495}

---
+++ chrome/enterprise_companion/installer.h
@@ -0,0 +1,15 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_ENTERPRISE_COMPANION_INSTALLER_H_
+#define CHROME_ENTERPRISE_COMPANION_INSTALLER_H_
+
+namespace enterprise_companion {
+
+// Install the Chrome Enterprise Companion App.
+bool Install();
+
+}  // namespace enterprise_companion
+
+#endif  // CHROME_ENTERPRISE_COMPANION_INSTALLER_H_
---
+++ chrome/enterprise_companion/installer_mac.mm
@@ -0,0 +1,14 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/enterprise_companion/installer.h"
+
+namespace enterprise_companion {
+
+bool Install() {
+  // TODO(crbug.com/344878963): Implement the installer.
+  return false;
+}
+
+}  // namespace enterprise_companion
