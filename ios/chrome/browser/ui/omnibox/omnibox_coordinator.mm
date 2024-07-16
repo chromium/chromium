5481e09a4865925a8d1412191695a27ad9fcbd3c
Joemer Ramos <joemerramos@chromium.org>
2024-07-16 13:33:49 +0000

    [iOS] Clean Up Startup Improvements Feature Flag in Omnibox
    Clean up startup improvements feature flag related code.
    
    Bug: 352833570
    Change-Id: Ifbf47aa36e9e2f8e705c2973097670d925939dca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704222
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328115}

--- ios/chrome/browser/ui/omnibox/omnibox_coordinator.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_coordinator.mm
@@ -181,12 +181,6 @@
   _keyboardMediator.omniboxTextField = self.textField;
   _keyboardMediator.delegate = self;
 
-  if (!base::FeatureList::IsEnabled(kEnableStartupImprovements)) {
-    self.keyboardAccessoryView = ConfigureAssistiveKeyboardViews(
-        self.textField, kDotComTLD, _keyboardMediator, templateURLService,
-        self.bubblePresenter);
-  }
-
   if (base::FeatureList::IsEnabled(omnibox::kZeroSuggestPrefetching)) {
     self.zeroSuggestPrefetchHelper = [[ZeroSuggestPrefetchHelper alloc]
         initWithWebStateList:self.browser->GetWebStateList()
@@ -229,15 +223,13 @@
 }
 
 - (void)focusOmnibox {
-  if (base::FeatureList::IsEnabled(kEnableStartupImprovements)) {
-    if (!self.keyboardAccessoryView) {
-      TemplateURLService* templateURLService =
-          ios::TemplateURLServiceFactory::GetForBrowserState(
-              self.browser->GetBrowserState());
-      self.keyboardAccessoryView = ConfigureAssistiveKeyboardViews(
-          self.textField, kDotComTLD, _keyboardMediator, templateURLService,
-          self.bubblePresenter);
-    }
+  if (!self.keyboardAccessoryView) {
+    TemplateURLService* templateURLService =
+        ios::TemplateURLServiceFactory::GetForBrowserState(
+            self.browser->GetBrowserState());
+    self.keyboardAccessoryView = ConfigureAssistiveKeyboardViews(
+        self.textField, kDotComTLD, _keyboardMediator, templateURLService,
+        self.bubblePresenter);
   }
 
   if (![self.textField isFirstResponder]) {
