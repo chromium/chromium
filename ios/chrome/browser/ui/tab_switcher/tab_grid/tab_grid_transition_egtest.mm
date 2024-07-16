84370d74ba652da29b093155e71d69b6ec66a1d9
Federica Germinario <fedegermi@google.com>
2024-07-16 14:07:37 +0000

    [ios] Fix test testLeaveSwitcherAfterEnteringAndExittingInactiveTabs
    This CL adds a workaround to fix test in
    TabSwitcherTransitionTestCase when run on iOS18 devices.
    
    Bug: 349816094
    Change-Id: I97c98787fc53c2abab43a0dfd27334ff6cf2dbe4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713310
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1328141}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_transition_egtest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_transition_egtest.mm
@@ -709,10 +709,17 @@ void ExpectIdleHistogramBucketCount(const char* histogram,
   ExpectIdleHistogramCount(kUMATabSwitcherIdleIncognitoTabGridPageHistogram, 0);
 
   [ChromeEarlGrey showTabSwitcher];
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(
-                                   kInactiveTabsButtonAccessibilityIdentifier)]
-      performAction:grey_tap()];
+  if (@available(iOS 18, *)) {
+    XCUIApplication* app = [[XCUIApplication alloc] init];
+    if ([app.buttons[kInactiveTabsButtonAccessibilityIdentifier] exists]) {
+      [app.buttons[kInactiveTabsButtonAccessibilityIdentifier] tap];
+    }
+  } else {
+    [[EarlGrey
+        selectElementWithMatcher:
+            grey_accessibilityID(kInactiveTabsButtonAccessibilityIdentifier)]
+        performAction:grey_tap()];
+  }
   [[EarlGrey selectElementWithMatcher:testing::NavigationBarBackButton()]
       performAction:grey_tap()];
   ShowTabViewController();
