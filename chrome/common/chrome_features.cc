9ebd36b234c4ff0d67f203dae0311ccd539f8378
Dibyajyoti Pal <dibyapal@google.com>
2024-07-17 22:04:33 +0000

    [PWA] Enable universal install on CrOS
    Bug: 343216389
    Change-Id: I49224a3bb50de03266593e89ca8abc4a2f902179
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717858
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329158}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -1599,11 +1599,7 @@ BASE_FEATURE(kWebAppSyncGeneratedIconUpdateFix,
 
 BASE_FEATURE(kWebAppUniversalInstall,
              "WebAppUniversalInstall",
-#if BUILDFLAG(IS_CHROMEOS)
-             base::FEATURE_DISABLED_BY_DEFAULT
-#else
              base::FEATURE_ENABLED_BY_DEFAULT
-#endif  // BUILDFLAG(IS_CHROMEOS)
 );
 #endif  // !BUILDFLAG(IS_ANDROID)
 
