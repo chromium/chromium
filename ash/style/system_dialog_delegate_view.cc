c122a701ff901f9ab0e06ed8df23d57fa1572bb8
Xiaodan Zhu <zxdan@chromium.org>
2024-07-17 16:38:57 +0000

    Fix the accessible role of dialog heading
    This cl fixed the accessible role of system dialog heading.
    
    Bug: b:351036570
    Change-Id: I7166ea0f796d10dd1687a75c4e4c06b7ab369aaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713533
    Auto-Submit: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328928}

--- ash/style/system_dialog_delegate_view.cc
+++ ash/style/system_dialog_delegate_view.cc
@@ -250,7 +250,7 @@ SystemDialogDelegateView::SystemDialogDelegateView() {
   title_->SetAutoColorReadabilityEnabled(false);
   title_->SetEnabledColorId(kTitleColorId);
   title_->SetVisible(false);
-  title_->GetViewAccessibility().SetRole(ax::mojom::Role::kHeader);
+  title_->GetViewAccessibility().SetRole(ax::mojom::Role::kHeading);
   title_->SetProperty(views::kElementIdentifierKey, kTitleTextIdForTesting);
 
   description_ = AddChildView(std::make_unique<views::Label>());
