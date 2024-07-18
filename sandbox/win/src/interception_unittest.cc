65d45ef1536f6add3612ea96e812c1250434d448
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-07-18 18:37:31 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to sandbox/*
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    Note that in patchset2, change to:
    /build/config/unsafe_buffers_paths.txt
    was reverted. Indeed, running too many (~3) CQ run touching this file is
    making the builder cache much slower. I will bundle every change to this
    file in a subsequent CL. I will limit myself to 1-2 CQ run per day.
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 351564777
    Change-Id: I817a47d5ac73b416627fcf67490649ee7b7c43dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717754
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329709}

--- sandbox/win/src/interception_unittest.cc
+++ sandbox/win/src/interception_unittest.cc
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 // This file contains unit tests for InterceptionManager.
 // The tests require private information so the whole interception.cc file is
 // included from this file.
