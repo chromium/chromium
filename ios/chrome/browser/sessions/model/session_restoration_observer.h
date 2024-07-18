bf483c3c587329272d131b48071ed47856522892
Sylvain Defresne <sdefresne@chromium.org>
2024-07-18 08:10:08 +0000

    [ios] Preparation for moving //ios/chrome/browser/sessions files
    Add forwarding headers and target to prepare for moving the files
    from //ios/chrome/browser/{sessions => sessions/model} without
    breaking the internal builders.
    
    This is part of a multi-CLs change:
     => https://crrev.com/c/5717949
     -  https://crrev.com/i/7498390
     -  https://crrev.com/c/5717871
    
    Bug: 40281350
    Change-Id: I27e46bc827b50b4e5c6d50140685e1fad19774a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717949
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329376}

---
+++ ios/chrome/browser/sessions/model/session_restoration_observer.h
@@ -0,0 +1,13 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SESSIONS_MODEL_SESSION_RESTORATION_OBSERVER_H_
+#define IOS_CHROME_BROWSER_SESSIONS_MODEL_SESSION_RESTORATION_OBSERVER_H_
+
+// TODO(crbug.com/40281350): remove when all internal code has been fixed
+// to use the new path to the file.
+
+#include "ios/chrome/browser/sessions/session_restoration_observer.h"
+
+#endif  // IOS_CHROME_BROWSER_SESSIONS_MODEL_SESSION_RESTORATION_OBSERVER_H_
---
+++ ios/chrome/browser/sessions/model/session_restoration_service_factory.h
@@ -0,0 +1,13 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SESSIONS_MODEL_SESSION_RESTORATION_SERVICE_FACTORY_H_
+#define IOS_CHROME_BROWSER_SESSIONS_MODEL_SESSION_RESTORATION_SERVICE_FACTORY_H_
+
+// TODO(crbug.com/40281350): remove when all internal code has been fixed
+// to use the new path to the file.
+
+#include "ios/chrome/browser/sessions/session_restoration_service_factory.h"
+
+#endif  // IOS_CHROME_BROWSER_SESSIONS_MODEL_SESSION_RESTORATION_SERVICE_FACTORY_H_
