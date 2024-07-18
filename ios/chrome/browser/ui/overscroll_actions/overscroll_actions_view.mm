fa60d333dd98afc6c960bfb49c158f703d6b0622
Chris Lu <thegreenfrog@chromium.org>
2024-07-18 15:29:38 +0000

    [ios] Move overscroll action image sizeToFit after adding to superview
    According to documentation, sizeToFit called on a view that has not been
    added to the view hierarchy may resize to the bounds of the screen. This
    may be require a alot of memory to size to such a large bounds and
    then convert back to the desired size.
    
    Low-Coverage-Reason: EXPERIMENTAL_CODE
    Change-Id: Ib2f9e7a73e58b773fdd9ed0f03cac006ea52428d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719128
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329565}

--- ios/chrome/browser/ui/overscroll_actions/overscroll_actions_view.mm
+++ ios/chrome/browser/ui/overscroll_actions/overscroll_actions_view.mm
@@ -255,22 +255,37 @@ const CGFloat kActionViewBackgroundColorBrightnessIncognito = 80.0 / 256.0;
     _addTabActionImageView.image = DefaultSymbolTemplateWithPointSize(
         kPlusSymbol, kOverScrollSymbolPointSize);
     _addTabActionImageView.tintColor = [UIColor colorNamed:kTextPrimaryColor];
-    [_addTabActionImageView sizeToFit];
+    if (!IsHomeMemoryImprovementsEnabled()) {
+      [_addTabActionImageView sizeToFit];
+    }
     [self addSubview:_addTabActionImageView];
+    if (IsHomeMemoryImprovementsEnabled()) {
+      [_addTabActionImageView sizeToFit];
+    }
 
     _reloadActionImageView = [[UIImageView alloc] init];
     _reloadActionImageView.image = CustomSymbolTemplateWithPointSize(
         kArrowClockWiseSymbol, kOverScrollSymbolPointSize);
     _reloadActionImageView.tintColor = [UIColor colorNamed:kTextPrimaryColor];
-    [_reloadActionImageView sizeToFit];
+    if (!IsHomeMemoryImprovementsEnabled()) {
+      [_reloadActionImageView sizeToFit];
+    }
     [self addSubview:_reloadActionImageView];
+    if (IsHomeMemoryImprovementsEnabled()) {
+      [_reloadActionImageView sizeToFit];
+    }
 
     _closeTabActionImageView = [[UIImageView alloc] init];
     _closeTabActionImageView.image = DefaultSymbolTemplateWithPointSize(
         kXMarkSymbol, kOverScrollSymbolPointSize);
     _closeTabActionImageView.tintColor = [UIColor colorNamed:kTextPrimaryColor];
-    [_closeTabActionImageView sizeToFit];
+    if (!IsHomeMemoryImprovementsEnabled()) {
+      [_closeTabActionImageView sizeToFit];
+    }
     [self addSubview:_closeTabActionImageView];
+    if (IsHomeMemoryImprovementsEnabled()) {
+      [_closeTabActionImageView sizeToFit];
+    }
 
     _addTabLabel = [[UILabel alloc] init];
     _addTabLabel.numberOfLines = 0;
