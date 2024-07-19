3ea69ba983504186e0ab4d491e9ce3a0e9aed31b
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-07-19 21:43:20 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to storage/*
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    Note that in patchset2, change to:
    /build/config/unsafe_buffers_paths.txt
    was reverted. Indeed, running too many (~3) CQ run touching this file is
    making the builder cache much slower. I will bundle every change to this
    file in a subsequent CL. I will limit myself to 1-2 CQ run per day.
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 40285824
    Change-Id: I5d5e25d1df3003cf4cf8a04895626f30b64923a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717693
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330528}

--- storage/browser/blob/blob_data_builder.h
+++ storage/browser/blob/blob_data_builder.h
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #ifndef STORAGE_BROWSER_BLOB_BLOB_DATA_BUILDER_H_
 #define STORAGE_BROWSER_BLOB_BLOB_DATA_BUILDER_H_
 
