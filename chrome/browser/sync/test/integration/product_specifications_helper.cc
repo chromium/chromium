fa88c1125c9dd73b016b178491619c0d421ef660
David Maunder <davidjm@google.com>
2024-07-22 15:46:33 +0000

    Update ProductSpecifications sync integration tests for multi specifics
    The product specifications sync integration was re-written to store
    a ProductSpecificationsSet across multiple specifics to reduce the
    risk of hitting the 8K limit. That work is almost finished and
    is behind a flag which will be enabled by default soon. This CL
    updates the integration tests in preparation for enabling by default.
    Note, the single specifics tests have not been preserved as we
    have no intention in continuing with this approach.
    
    bug: 354617072
    Change-Id: I20169c4e6a10257e5f0c9b75d129c240b09c6e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5728654
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331077}

--- chrome/browser/sync/test/integration/product_specifications_helper.cc
+++ chrome/browser/sync/test/integration/product_specifications_helper.cc
@@ -6,6 +6,7 @@
 
 #include <sstream>
 
+#include "base/containers/to_vector.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
 #include "components/commerce/core/product_specifications/product_specifications_service.h"
@@ -15,51 +16,37 @@
 namespace commerce {
 
 ProductSpecificationsChecker::ProductSpecificationsChecker(
-    commerce::ProductSpecificationsService* service,
-    const sync_pb::ProductComparisonSpecifics& product_comparison_specifics)
-    : product_comparison_specifics_(product_comparison_specifics),
-      service_(service) {}
+    ProductSpecificationsService* service,
+    ProductSpecificationsSet* expected_set)
+    : expected_set_(expected_set), service_(service) {}
 
 ProductSpecificationsChecker::~ProductSpecificationsChecker() = default;
 
 bool ProductSpecificationsChecker::IsExitConditionSatisfied(std::ostream* os) {
-  *os << "Waiting for CompareSpecifics:\n";
-  *os << "    uuid: " << product_comparison_specifics_->uuid() << "\n";
-  *os << "    name: " << product_comparison_specifics_->name() << "\n";
-  *os << "    creation_time: "
-      << product_comparison_specifics_->creation_time_unix_epoch_millis()
+  *os << "Waiting for ProductSpeificationsSet:\n";
+  *os << "    uuid: " << expected_set_->uuid() << "\n";
+  *os << "    name: " << expected_set_->name() << "\n";
+  *os << "    creation_time: " << expected_set_->creation_time() << "\n";
+  *os << "    update_time: " << expected_set_->update_time() << "\n";
+  *os << "    urls: "
+      << base::JoinString(base::ToVector(expected_set_->urls(), &GURL::spec),
+                          ", ")
       << "\n";
-  *os << "    update_time: "
-      << product_comparison_specifics_->update_time_unix_epoch_millis() << "\n";
-  std::vector<std::string> urls;
-  for (const sync_pb::ComparisonData& comparison_data :
-       product_comparison_specifics_->data()) {
-    urls.push_back(comparison_data.url());
-  }
-  *os << "    urls: " << base::JoinString(urls, ", ");
   return IsSpecificsAvailableAndEqual();
 }
 
 bool ProductSpecificationsChecker::IsSpecificsAvailableAndEqual() {
   for (const ProductSpecificationsSet& product_specifications_set :
        service_->GetAllProductSpecifications()) {
-    std::vector<GURL> specifics_urls;
-    for (sync_pb::ComparisonData data : product_comparison_specifics_->data()) {
-      specifics_urls.emplace_back(data.url());
-    }
-    if (product_specifications_set.uuid().AsLowercaseString() ==
-            product_comparison_specifics_->uuid() &&
-        product_specifications_set.name() ==
-            product_comparison_specifics_->name() &&
+    // TODO(crbug.com/354689571) create == operator on
+    // ProductSpecificationsSet.
+    if (product_specifications_set.uuid() == expected_set_->uuid() &&
+        product_specifications_set.name() == expected_set_->name() &&
         product_specifications_set.creation_time() ==
-            base::Time::FromMillisecondsSinceUnixEpoch(
-                product_comparison_specifics_
-                    ->creation_time_unix_epoch_millis()) &&
+            expected_set_->creation_time() &&
         product_specifications_set.update_time() ==
-            base::Time::FromMillisecondsSinceUnixEpoch(
-                product_comparison_specifics_
-                    ->update_time_unix_epoch_millis()) &&
-        product_specifications_set.urls() == specifics_urls) {
+            expected_set_->update_time() &&
+        product_specifications_set.urls() == expected_set_->urls()) {
       return true;
     }
   }
