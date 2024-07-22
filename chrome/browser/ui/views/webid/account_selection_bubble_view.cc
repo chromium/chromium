97ca590afa10e0f4926ad72440d2db1adadfb9b1
Nicolás Peña <npm@chromium.org>
2024-07-22 16:57:22 +0000

    [FedCM] Decrease multi IDP bottom margin
    While we had increased this margin before, it does not look good in
    various cases. We had increased it to avoid a tiny jag which may be
    apparent with a smaller margin. To avoid overcomplicating the logic,
    we set a slightly smaller margin in all cases.
    
    Bug: 354702333
    Change-Id: I384d339b93c893037ec73ac331a9a822351f02b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730933
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331136}

--- chrome/browser/ui/views/webid/account_selection_bubble_view.cc
+++ chrome/browser/ui/views/webid/account_selection_bubble_view.cc
@@ -246,7 +246,10 @@ AccountSelectionBubbleView::AccountSelectionBubbleView(
       rp_context_(rp_context) {
   SetButtons(ui::DIALOG_BUTTON_NONE);
   set_fixed_width(kBubbleWidth);
-  set_margins(gfx::Insets::VH(kTopBottomPadding + kVerticalSpacing, 0));
+  set_margins(idp_title.has_value()
+                  ? gfx::Insets::VH(kTopBottomPadding + kVerticalSpacing, 0)
+                  : gfx::Insets::TLBR(kTopBottomPadding + kVerticalSpacing, 0,
+                                      kVerticalSpacing, 0));
   // TODO(crbug.com/40224637): we are currently using a custom header because
   // the icon, title, and close buttons from a bubble are not customizable
   // enough to satisfy the UI requirements. However, this adds complexity to the
