efb1eeeae7f8976fd2a371efa1fc7cc446ec08ae
Hzj_jie <zijiehe@google.com>
2024-07-22 23:45:03 +0000

    [Fuchsia] Ensure the Dummy can be used by 'with' statement
    Bug: 346625286
    Change-Id: I9616005fc9cf9d502fc8392cd425f78e01d938fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5732062
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331435}

--- build/fuchsia/test/monitors_unittests.py
+++ build/fuchsia/test/monitors_unittests.py
@@ -14,33 +14,45 @@ import unittest.mock as mock
 import monitors
 
 
+def dump() -> bool:
+    """Tries to dump the metrics into a temporary file and returns if the
+    file exits."""
+    with tempfile.TemporaryDirectory() as tmpdir:
+        monitors.dump(tmpdir)
+        return os.path.isfile(
+            os.path.join(tmpdir, 'test_script_metrics.jsonpb'))
+
+
 class MonitorsRealTest(unittest.TestCase):
     """Test real implementation of monitors.py."""
 
     def test_run_real_implementation(self) -> None:
-        """Ensure the real version of the monitors is loaded."""
+        """Ensures the real version of the monitors is loaded."""
         importlib.reload(monitors)
         ave = monitors.average('test', 'run', 'real', 'implementation')
         ave.record(1)
         ave.record(2)
-        with tempfile.TemporaryDirectory() as tmpdir:
-            monitors.dump(tmpdir)
-            self.assertTrue(
-                os.path.isfile(
-                    os.path.join(tmpdir, 'test_script_metrics.jsonpb')))
+        self.assertTrue(dump())
 
     @mock.patch('os.path.isdir', side_effect=[False, True])
     def test_run_dummy_implementation(self, *_) -> None:
-        """Ensure the dummy version of the monitors is loaded."""
+        """Ensures the dummy version of the monitors is loaded."""
         importlib.reload(monitors)
-        ave = monitors.average('test', 'run', 'real', 'implementation')
+        ave = monitors.average('test', 'run', 'dummy', 'implementation')
         ave.record(1)
         ave.record(2)
-        with tempfile.TemporaryDirectory() as tmpdir:
-            monitors.dump(tmpdir)
-            self.assertFalse(
-                os.path.isfile(
-                    os.path.join(tmpdir, 'test_script_metrics.jsonpb')))
+        self.assertFalse(dump())
+
+    @mock.patch('os.path.isdir', side_effect=[False, True])
+    def test_with_dummy_implementation(self, *_) -> None:
+        """Ensures the dummy version of the monitors can be used by 'with'
+        statement."""
+        importlib.reload(monitors)
+        executed = False
+        with monitors.time_consumption('test', 'with', 'dummy'):
+            executed = True
+        self.assertTrue(executed)
+        self.assertFalse(dump())
 
 
 if __name__ == '__main__':
