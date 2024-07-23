60b3996af2dea122de6ca00475bc4e81ed95ee76
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-07-23 05:42:26 +0000

    Refactor code in c/u/f/highlight_border_overlay.cc
    * Introduce HighlightBorderProperties struct.
    * Move image creation code into a separate function.
    * Update comments.
    * Decompose complicated boolean conditions.
    
    Change-Id: I323438cae8fb91c8e30b67fc725fb15eef4e3b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5728414
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331560}

--- chromeos/ui/frame/highlight_border_overlay.cc
+++ chromeos/ui/frame/highlight_border_overlay.cc
@@ -11,18 +11,18 @@
 #include "chromeos/ui/base/window_properties.h"
 #include "chromeos/ui/base/window_state_type.h"
 #include "chromeos/ui/frame/frame_utils.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
 #include "ui/display/screen.h"
+#include "ui/gfx/geometry/size.h"
 #include "ui/gfx/image/canvas_image_source.h"
+#include "ui/gfx/image/image_skia.h"
 #include "ui/views/highlight_border.h"
 #include "ui/views/widget/widget.h"
 
 namespace {
 
-// A highlight border overlay is featured by its highlight color, border color,
-// and rounded corner radius.
-using HighlightBorderFeatureKey = std::tuple<SkColor, SkColor, int>;
 // Currently, each dark and light mode has only one set of highlight and border
 // colors. The windows that are using HighlightBorderOverlay have three
 // different rounded corner radius. There should be 6 different types of image
@@ -32,33 +32,69 @@ constexpr size_t kMaxImageSourceNum = 6;
 constexpr views::HighlightBorder::Type kBorderType =
     views::HighlightBorder::Type::kHighlightBorderOnShadow;
 
-// `ImageSource` generates an image painted with a highlight border.
-class ImageSource : public gfx::CanvasImageSource {
+// A highlight border overlay is featured by its highlight color, border color,
+// and rounded corner radius.
+struct HighlightBorderProperties {
+  bool operator<(const HighlightBorderProperties& other) const {
+    return std::tie(highlight_color, border_color, corner_radius) <
+           std::tie(other.highlight_color, other.border_color,
+                    other.corner_radius);
+  }
+
+  SkColor highlight_color = SK_ColorTRANSPARENT;
+  SkColor border_color = SK_ColorTRANSPARENT;
+  int corner_radius = 0;
+};
+
+// Generates an image painted with a highlight border.
+class HighlightBorderImageSource : public gfx::CanvasImageSource {
  public:
-  ImageSource(SkColor highlight_color,
-              SkColor border_color,
-              int corner_radius,
-              const gfx::Size& size)
-      : gfx::CanvasImageSource(size),
-        highlight_color_(highlight_color),
-        border_color_(border_color),
-        corner_radius_(corner_radius) {}
-  ImageSource(const ImageSource&) = delete;
-  ImageSource& operator=(const ImageSource&) = delete;
-  ~ImageSource() override = default;
+  HighlightBorderImageSource(const gfx::Size& size,
+                             const HighlightBorderProperties& properties)
+      : gfx::CanvasImageSource(size), properties_(properties) {}
+  HighlightBorderImageSource(const HighlightBorderImageSource&) = delete;
+  HighlightBorderImageSource& operator=(const HighlightBorderImageSource&) =
+      delete;
+  ~HighlightBorderImageSource() override = default;
 
   // gfx::CanvasImageSource:
   void Draw(gfx::Canvas* canvas) override {
     views::HighlightBorder::PaintBorderToCanvas(
-        canvas, highlight_color_, border_color_, gfx::Rect(size()),
-        gfx::RoundedCornersF(corner_radius_), kBorderType);
+        canvas, properties_.highlight_color, properties_.border_color,
+        gfx::Rect(size()), gfx::RoundedCornersF(properties_.corner_radius),
+        kBorderType);
   }
 
  private:
-  const SkColor highlight_color_;
-  const SkColor border_color_;
-  const int corner_radius_;
+  const HighlightBorderProperties properties_;
 };
+
+gfx::ImageSkia GetHighlightBorderImageMatching(
+    const gfx::Size& size,
+    const HighlightBorderProperties& properties) {
+  static base::NoDestructor<std::map<HighlightBorderProperties, gfx::ImageSkia>>
+      image_source_cache;
+
+  auto iter = image_source_cache->find(properties);
+  if (iter != image_source_cache->end()) {
+    return iter->second;
+  }
+
+  // Evict the images that have no owners.
+  std::erase_if(*image_source_cache, [](auto& key_and_image_source) {
+    return key_and_image_source.second.IsUniquelyOwned();
+  });
+
+  // Create and store a new image.
+  const auto& [iterator, inserted] = image_source_cache->emplace(
+      properties, gfx::ImageSkia(std::make_unique<HighlightBorderImageSource>(
+                                     size, properties),
+                                 /*scale=*/1.0f));
+  DCHECK(inserted);
+  DCHECK_LE(image_source_cache->size(), kMaxImageSourceNum);
+
+  return iterator->second;
+}
 
 }  // namespace
 
@@ -85,7 +121,7 @@ HighlightBorderOverlay::~HighlightBorderOverlay() {
 }
 
 gfx::Size HighlightBorderOverlay::CalculateImageSourceSize() const {
-  // Initialize the image source bounds with 1 px of center patch size.
+  // Initialize the image source bounds with 1 dp of center patch size.
   gfx::Rect image_source_bounds(1, 1);
 
   // Outset the bounds with border region.
@@ -147,25 +183,34 @@ void HighlightBorderOverlay::UpdateLayerVisibilityAndBounds() {
   // Outset the bounds by one border thickness for outer border.
   layer_bounds.Inset(-gfx::Insets(views::kHighlightBorderThickness));
 
+  // TabletState might be nullptr in some tests.
+  const bool in_tablet_mode = display::Screen::GetScreen()->InTabletMode();
+  const auto window_state_type =
+      window_->GetProperty(chromeos::kWindowStateTypeKey);
+
+  const bool show_border_in_tablet_mode =
+      in_tablet_mode &&
+      window_state_type == chromeos::WindowStateType::kFloated &&
+      window_state_type == chromeos::WindowStateType::kPip;
+  const bool show_border_in_clamshell_mode =
+      !in_tablet_mode &&
+      window_state_type != chromeos::WindowStateType::kFullscreen;
+
   const gfx::Insets border_region = CalculateBorderRegion();
+  const bool fits_layer_bounds =
+      border_region.width() <= layer_bounds.width() &&
+      border_region.height() <= layer_bounds.height();
+
   // Hide the border if it's in tablet mode and the window is not float nor
   // pip or if it's in clamshell mode and the window is in fullscreen state.
   // Also hide the border if the border region is wider or higher than the
   // window since border is in layer space. It cannot exceed the bounds of the
   // layer.
-  const auto window_state_type =
-      window_->GetProperty(chromeos::kWindowStateTypeKey);
+  const bool show_border =
+      fits_layer_bounds &&
+      (show_border_in_clamshell_mode || show_border_in_tablet_mode);
 
-  // TabletState might be nullptr in some tests.
-  const bool in_tablet_mode = display::Screen::GetScreen()->InTabletMode();
-
-  if ((in_tablet_mode &&
-       window_state_type != chromeos::WindowStateType::kFloated &&
-       window_state_type != chromeos::WindowStateType::kPip) ||
-      (!in_tablet_mode &&
-       window_state_type == chromeos::WindowStateType::kFullscreen) ||
-      border_region.width() > layer_bounds.width() ||
-      border_region.height() > layer_bounds.height()) {
+  if (!show_border) {
     layer_.SetVisible(false);
     return;
   }
@@ -176,38 +221,19 @@ void HighlightBorderOverlay::UpdateLayerVisibilityAndBounds() {
 }
 
 void HighlightBorderOverlay::UpdateNinePatchLayer() {
+  const gfx::Size image_source_size = CalculateImageSourceSize();
+
   // Get the highlight border features.
   const views::View& view = *(widget_->GetContentsView());
   SkColor highlight_color =
       views::HighlightBorder::GetHighlightColor(view, kBorderType);
   SkColor border_color =
       views::HighlightBorder::GetBorderColor(view, kBorderType);
-  HighlightBorderFeatureKey key(highlight_color, border_color,
-                                rounded_corner_radius_);
+  const HighlightBorderProperties properties(highlight_color, border_color,
+                                             rounded_corner_radius_);
 
-  gfx::Size image_source_size = CalculateImageSourceSize();
-
-  static base::NoDestructor<std::map<HighlightBorderFeatureKey, gfx::ImageSkia>>
-      image_source_map;
-  auto iter = image_source_map->find(key);
-  if (iter == image_source_map->end()) {
-    // Evict the image source which has no owners.
-    std::erase_if(*image_source_map, [](auto& key_and_image_source) {
-      return key_and_image_source.second.IsUniquelyOwned();
-    });
-    // Create a new image.
-    auto insertion = image_source_map->emplace(
-        key, gfx::ImageSkia(std::make_unique<ImageSource>(
-                                highlight_color, border_color,
-                                rounded_corner_radius_, image_source_size),
-                            image_source_size));
-    DCHECK(insertion.second);
-    // Add a dcheck here to notice the cache size change.
-    DCHECK_LE(image_source_map->size(), kMaxImageSourceNum);
-    iter = insertion.first;
-  }
-
-  layer_.UpdateNinePatchLayerImage(iter->second);
+  layer_.UpdateNinePatchLayerImage(
+      GetHighlightBorderImageMatching(image_source_size, properties));
 
   gfx::Rect aperture(image_source_size);
   gfx::Insets border_region = CalculateBorderRegion();
