85d420e74019539a11846fd924e34ad3464ef205
Calder Kitagawa <ckitagawa@chromium.org>
2024-07-23 16:23:41 +0000

    [Tab Switcher] Re-enable three always passing tests on reviver
    These tests have been disabled for in some cases years and are
    currently passing according to test reviver.
    
    To gardeners if this re-introduces a flaky tests please just disable the
    ones that are still flaky.
    
    Fixed: 40716237, 40822626, 337061378
    Change-Id: I1b87d19ff224005b7c45c3c241cf6b083646eb6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5731934
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331784}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutTest.java
@@ -223,7 +223,6 @@ public class TabSwitcherLayoutTest {
 
     @Test
     @LargeTest
-    @DisabledTest(message = "https://crbug.com/1122657")
     public void testThumbnailAspectRatio_default() {
         prepareTabs(2, 0, "about:blank");
         enterTabSwitcher(mActivityTestRule.getActivity());
@@ -267,7 +266,6 @@ public class TabSwitcherLayoutTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1297930")
     public void testRecycling_defaultAspectRatio() {
         prepareTabs(10, 0, mUrl);
         ChromeTabUtils.switchTabInCurrentTabModel(mActivityTestRule.getActivity(), 0);
@@ -429,7 +427,6 @@ public class TabSwitcherLayoutTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "b/337061378")
     public void verifyTabGroupStateAfterReparenting() throws Exception {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         assertTrue(
