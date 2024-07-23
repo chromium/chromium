31a0336ce02e89a58085d32cd91479d9bc05e038
Ian Struiksma <ianstruiksma@google.com>
2024-07-23 17:07:43 +0000

    [Autofill][Captured Sites] Add parsing pattern override in tests
    Add flag "--parsing_pattern" to captured sites tests, which allows to
    run with different parsing patterns when necessary.
    
    Keep sites running on bot running with 'experimental'.
    
    Bug: 40166753
    Change-Id: I8af29876138fb3cfd4594ecbc6fc12519a56969c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5721927
    Commit-Queue: Ian Struiksma <ianstruiksma@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331818}

--- chrome/browser/autofill/autofill_captured_sites_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_captured_sites_interactive_uitest.cc
@@ -72,6 +72,10 @@ namespace autofill {
 
 namespace {
 
+// This flag allows a caller to specify a different parsing pattern for the
+// test run. When not given, will use "experimental".
+constexpr char kParsingPatternFlag[] = "parsing_pattern";
+
 // The timeout for actions like bringing up the Autofill popup or showing the
 // preview of suggestions.
 constexpr base::TimeDelta kAutofillWaitForActionInterval = base::Seconds(5);
@@ -98,6 +102,15 @@ base::FilePath GetReplayFilesRootDirectory() {
 autofill::ElementExpr GetElementByXpath(const std::string& xpath) {
   return autofill::ElementExpr(base::StringPrintf(
       "automation_helper.getElementByXpath(`%s`)", xpath.c_str()));
+}
+
+std::string GetParsingPatternProvider() {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  // The command-line flag to specify the parsing pattern.
+  if (command_line && command_line->HasSwitch(kParsingPatternFlag)) {
+    return command_line->GetSwitchValueASCII(kParsingPatternFlag);
+  }
+  return "experimental";
 }
 
 // Implements the `kAutofillCapturedSiteTestsMetricsScraper` testing feature.
@@ -333,6 +346,9 @@ class AutofillCapturedSitesInteractiveTest
   }
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
+    std::string prediction_source = GetParsingPatternProvider();
+    VLOG(1) << "Using '" << prediction_source << "' Parsing Pattern Provider.";
+
     // Enable the autofill show typed prediction feature. When active this
     // feature forces input elements on a form to display their autofill type
     // prediction. Test will check this attribute on all the relevant input
@@ -343,7 +359,7 @@ class AutofillCapturedSitesInteractiveTest
                               {features::test::kAutofillShowTypePredictions,
                                {}},
                               {features::kAutofillParsingPatternProvider,
-                               {{"prediction_source", "experimental"}}},
+                               {{"prediction_source", prediction_source}}},
                               {features::test::
                                    kAutofillCapturedSiteTestsUseAutofillFlow,
                                {}}},
