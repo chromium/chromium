8de1f5d06fe9e6ffbf210a1294f7f432536a47bb
Siye Liu <siliu@microsoft.com>
2024-07-23 05:09:42 +0000

    Create helper for inserting text of inputType `insertReplacementText`
    Both `SpellChecker::ReplaceMisspelledRange` and
    `TextSuggestionController::ReplaceRangeWithText` need to insert text of
    inputType `insertReplacementText`. We can put the common code in one
    helper method `InsertTextAndSendInputEventsOfTypeInsertReplacementText`
    in `editing_utilities.h/cc` files. The helper does the following:
      1. dispatch beforeinput event of inputType `insertReplacementText`.
      2. insert text into DOM.
      3. dispatch input event of inputType `insertReplacementText`.
    
    Bug: 40946143
    Change-Id: I2ef09520af3d9a88994dc62b86d4049135cc4633
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5711291
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1331554}

--- third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.cc
+++ third_party/blink/renderer/core/editing/suggestion/text_suggestion_controller.cc
@@ -6,8 +6,6 @@
 
 #include "base/ranges/algorithm.h"
 #include "third_party/blink/public/platform/browser_interface_broker_proxy.h"
-#include "third_party/blink/renderer/core/clipboard/data_transfer.h"
-#include "third_party/blink/renderer/core/clipboard/data_transfer_access_policy.h"
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/editor.h"
 #include "third_party/blink/renderer/core/editing/ephemeral_range.h"
@@ -622,42 +620,8 @@ void TextSuggestionController::ReplaceRangeWithText(const EphemeralRange& range,
   GetFrame().Selection().SetSelectionAndEndTyping(
       SelectionInDOMTree::Builder().SetBaseAndExtent(range).Build());
 
-  // TODO(editing-dev): We should check whether |TextSuggestionController| is
-  // available or not.
-  // TODO(editing-dev): The use of UpdateStyleAndLayout
-  // needs to be audited.  See http://crbug.com/590369 for more details.
-  GetFrame().GetDocument()->UpdateStyleAndLayout(
-      DocumentUpdateReason::kSpellCheck);
-
-  // Dispatch 'beforeinput'.
-  Element* const target = FindEventTargetFrom(
-      GetFrame(), GetFrame().Selection().ComputeVisibleSelectionInDOMTree());
-
-  DataTransfer* const data_transfer = DataTransfer::Create(
-      DataTransfer::DataTransferType::kInsertReplacementText,
-      DataTransferAccessPolicy::kReadable,
-      DataObject::CreateFromString(replacement));
-
-  const bool is_canceled =
-      DispatchBeforeInputDataTransfer(
-          target, InputEvent::InputType::kInsertReplacementText,
-          data_transfer) != DispatchEventResult::kNotCanceled;
-
-  // 'beforeinput' event handler may destroy target frame.
-  if (!IsAvailable())
-    return;
-
-  // TODO(editing-dev): The use of UpdateStyleAndLayout
-  // needs to be audited.  See http://crbug.com/590369 for more details.
-  GetFrame().GetDocument()->UpdateStyleAndLayout(
-      DocumentUpdateReason::kSpellCheck);
-
-  if (is_canceled)
-    return;
-
-  GetFrame().GetEditor().InsertTextWithoutSendingTextEvent(
-      replacement, false, nullptr,
-      InputEvent::InputType::kInsertReplacementText);
+  InsertTextAndSendInputEventsOfTypeInsertReplacementText(GetFrame(),
+                                                          replacement);
 }
 
 }  // namespace blink
