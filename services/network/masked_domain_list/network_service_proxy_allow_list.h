5eaf6d4edcec65f624011189e31814d225cbc90b
Ron Sailer <rsailer@chromium.org>
2024-07-23 17:36:39 +0000

    Add public suffix list based rules to the MDL.
    Owners might own certain resources but not what is hosted on them.
    In those cases, the owners might list them in the PSL under the private domains section.
    Requests from an owner's property to a resource owned by the same owner should be proxied if the resource is listed in the PSL.
    
    Bug: 328788380
    Change-Id: I113ec9ab8212281829c6f73e0d9b73e6a3a2edfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5668249
    Reviewed-by: Alex Kallam <aakallam@chromium.org>
    Commit-Queue: Ron Sailer <rsailer@chromium.org>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331842}

--- services/network/masked_domain_list/network_service_proxy_allow_list.h
+++ services/network/masked_domain_list/network_service_proxy_allow_list.h
@@ -7,6 +7,7 @@
 
 #include "components/privacy_sandbox/masked_domain_list/masked_domain_list.pb.h"
 #include "net/base/network_anonymization_key.h"
+#include "net/base/scheme_host_port_matcher.h"
 #include "services/network/masked_domain_list/url_matcher_with_bypass.h"
 #include "services/network/public/mojom/network_context.mojom.h"
 #include "services/network/public/mojom/network_service.mojom-forward.h"
@@ -25,7 +26,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyAllowList {
   NetworkServiceProxyAllowList(const NetworkServiceProxyAllowList&);
 
   static NetworkServiceProxyAllowList CreateForTesting(
-      std::map<std::string, std::set<std::string>> first_party_map);
+      const std::map<std::string, std::set<std::string>>& first_party_map);
 
   // Estimates dynamic memory usage.
   // See base/trace_event/memory_usage_estimator.h for more info.
@@ -33,24 +34,27 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyAllowList {
 
   // Returns true if the allow list is eligible to be used but does not indicate
   // that the allow list is currently populated.
-  bool IsEnabled();
+  bool IsEnabled() const;
 
   // Returns true if there are entries in the allow list and it is possible to
   // match on them. If false, `Matches` will always return false.
-  bool IsPopulated();
+  bool IsPopulated() const;
 
   // Determines if the request is eligible for the proxy by determining if the
   // request_url is for an eligible domain and if the NAK supports eligibility.
   // If the top_frame_origin of the NAK does not have the same owner as the
   // request_url and the request_url is in the allow list, the request is
   // eligible for the proxy.
-  bool Matches(const GURL& request_url,
-               const net::NetworkAnonymizationKey& network_anonymization_key);
+  // TODO(crbug.com/354649091): Public Suffix List domains and subdomains
+  // proxy 1st party requests because no same-origin check is performed.
+  bool Matches(
+      const GURL& request_url,
+      const net::NetworkAnonymizationKey& network_anonymization_key) const;
 
   // Use the Masked Domain List and exclusion list to generate the allow list
   // and the 1P bypass rules.
   void UseMaskedDomainList(const masked_domain_list::MaskedDomainList& mdl,
-                           const std::vector<std::string> exclusion_list);
+                           const std::vector<std::string>& exclusion_list);
 
  private:
   // Policy that determines which domains are bypassed from IP Protection.
@@ -59,12 +63,22 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkServiceProxyAllowList {
   // Contains match rules from the Masked Domain List.
   UrlMatcherWithBypass url_matcher_with_bypass_;
 
+  // Matcher which matches against the public suffix list domains.
+  net::SchemeHostPortMatcher public_suffix_list_matcher_;
+
   // Removes domains from the MDL which are either part of the exclusion list
   // themselves or are subdomains of an entry in the exclusion list.
   // Returns MDL after removing such domains.
   std::set<std::string> ExcludeDomainsFromMDL(
       const std::set<std::string>& mdl_domains,
       const std::set<std::string>& excluded_domains);
+
+  // Determines whether or not the resource URL matches any URL listed in the
+  // public suffix list.
+  bool MatchesPublicSuffixList(const GURL& resource_url) const;
+
+  // Add domains to the `public_suffix_list_matcher_`.
+  void AddPublicSuffixListRules(const std::set<std::string>& domains);
 
   FRIEND_TEST_ALL_PREFIXES(NetworkServiceProxyAllowListBaseTest,
                            ExclusionSetDomainsRemovedFromMDL);
