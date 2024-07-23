e891607844b3437ecabdeb999ef9f132de3f1ad4
danakj <danakj@chromium.org>
2024-07-23 21:08:11 +0000

    Enable unsafe-buffer-usage in the base64 implementation
    Use spans instead of pointer arithmetic to construct pointers to pass
    to the C library.
    
    R=dcheng@chromium.org
    
    Bug: 40284755
    Change-Id: Ie8d132d924bae10546984516b48547941dd923b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735574
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331995}

--- base/base64.cc
+++ base/base64.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "base/base64.h"
 
 #include <stddef.h>
@@ -44,13 +39,20 @@ void Base64EncodeAppend(span<const uint8_t> input, std::string* output) {
   CHECK_LE(input.size(), MODP_B64_MAX_INPUT_LEN);
   size_t encode_data_len = modp_b64_encode_data_len(input.size());
 
-  size_t prefix_len = output->size();
-  output->resize(base::CheckAdd(encode_data_len, prefix_len).ValueOrDie());
+  const size_t after_size =
+      base::CheckAdd(encode_data_len, output->size()).ValueOrDie();
+  output->resize(after_size);
 
-  const size_t output_size = modp_b64_encode_data(
-      output->data() + prefix_len, reinterpret_cast<const char*>(input.data()),
-      input.size());
-  CHECK_EQ(output->size(), prefix_len + output_size);
+  span<const char> read = base::as_chars(input);
+  span<char> write = base::span(*output).last(encode_data_len);
+
+  const size_t written_size = modp_b64_encode_data(
+      write.data(),  // This must point to `encode_data_len` many chars.
+      read.data(), read.size());
+  // If this failed it would indicate we wrote OOB or left bytes uninitialized.
+  // It's possible for this to be elided by the compiler, since writing OOB is
+  // UB.
+  CHECK_EQ(written_size, write.size());
 }
 
 std::string Base64Encode(std::string_view input) {
@@ -60,13 +62,12 @@ std::string Base64Encode(std::string_view input) {
 bool Base64Decode(std::string_view input,
                   std::string* output,
                   Base64DecodePolicy policy) {
-  std::string temp;
-  temp.resize(modp_b64_decode_len(input.size()));
+  std::string decode_buf;
+  decode_buf.resize(modp_b64_decode_len(input.size()));
 
-  // does not null terminate result since result is binary data!
-  size_t input_size = input.size();
-  size_t output_size = modp_b64_decode(&(temp[0]), input.data(), input_size,
-                                       GetModpPolicy(policy));
+  // Does not NUL-terminate result since result is binary data!
+  size_t written_size = modp_b64_decode(decode_buf.data(), input.data(),
+                                        input.size(), GetModpPolicy(policy));
 
   // Forgiving mode requires whitespace to be stripped prior to decoding.
   // We don't do that in the above code to ensure that the "happy path" of
@@ -74,37 +75,52 @@ bool Base64Decode(std::string_view input,
   // will always cause `modp_b64_decode` to fail, just handle whitespace
   // stripping on failure. This is not much slower than just scanning for
   // whitespace first, even for input with whitespace.
-  if (output_size == MODP_B64_ERROR &&
+  if (written_size == MODP_B64_ERROR &&
       policy == Base64DecodePolicy::kForgiving) {
     // We could use `output` here to avoid an allocation when decoding is done
     // in-place, but it violates the API contract that `output` is only modified
     // on success.
     std::string input_without_whitespace;
     RemoveChars(input, kInfraAsciiWhitespace, &input_without_whitespace);
-    output_size =
-        modp_b64_decode(&(temp[0]), input_without_whitespace.data(),
+    // This means that the required size to decode is at most what was needed
+    // above, which means `decode_buf` will fit the decoded bytes at its current
+    // size and we don't need to call `modp_b64_decode_len()` again.
+    CHECK_LE(input_without_whitespace.size(), input.size());
+    written_size =
+        modp_b64_decode(decode_buf.data(), input_without_whitespace.data(),
                         input_without_whitespace.size(), GetModpPolicy(policy));
   }
 
-  if (output_size == MODP_B64_ERROR)
+  if (written_size == MODP_B64_ERROR) {
     return false;
+  }
 
-  temp.resize(output_size);
-  output->swap(temp);
+  // If this failed it would indicate we wrote OOB. It's possible for this to be
+  // elided by the compiler, since writing OOB is UB.
+  CHECK_LE(written_size, decode_buf.size());
+
+  // Shrinks the buffer and makes it NUL-terminated.
+  decode_buf.resize(written_size);
+  *output = std::move(decode_buf);
   return true;
 }
 
 std::optional<std::vector<uint8_t>> Base64Decode(std::string_view input) {
-  std::vector<uint8_t> ret(modp_b64_decode_len(input.size()));
+  std::vector<uint8_t> write_buf(modp_b64_decode_len(input.size()));
+  span<char> write = base::as_writable_chars(base::span(write_buf));
 
-  size_t input_size = input.size();
-  size_t output_size = modp_b64_decode(reinterpret_cast<char*>(ret.data()),
-                                       input.data(), input_size);
-  if (output_size == MODP_B64_ERROR)
+  size_t written_size =
+      modp_b64_decode(write.data(), input.data(), input.size());
+  if (written_size == MODP_B64_ERROR) {
     return std::nullopt;
+  }
 
-  ret.resize(output_size);
-  return ret;
+  // If this failed it would indicate we wrote OOB. It's possible for this to be
+  // elided by the compiler, since writing OOB is UB.
+  CHECK_LE(written_size, write.size());
+
+  write_buf.resize(written_size);
+  return write_buf;
 }
 
 }  // namespace base
