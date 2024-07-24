970be2aec7448c8c6eeaa3ba5eac68a80376647f
Sam Maier <smaier@chromium.org>
2024-07-24 20:18:59 +0000

    Reland "jni_zero: add a hash to ensure multiplexing lines up"
    This reverts commit aa2445185c97ecbf9afb646e9f2d8cd44e0c37f6.
    
    Reason for revert: Monochrome shares the J/N.java, so we also need
    to check if Java's priority targets match C++'s all targets. PS#1->2
    
    Original change's description:
    > Revert "jni_zero: add a hash to ensure multiplexing lines up"
    >
    > This reverts commit 10c89dc6adc3dabdb1101b63f1414a6a5121d85a.
    >
    > Reason for revert: crbug.com/354025384
    >
    > Original change's description:
    > > jni_zero: add a hash to ensure multiplexing lines up
    > >
    > > Bug: 348183775
    > > Change-Id: I91c0dbeca74a38159bd13cdc8381aab485a225be
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5680442
    > > Reviewed-by: Mohannad Farrag <aymanm@google.com>
    > > Commit-Queue: Sam Maier <smaier@chromium.org>
    > > Auto-Submit: Sam Maier <smaier@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1329481}
    >
    > Bug: 348183775
    > Change-Id: Ia0a2ae11c3cacc0c333e9860c76bd92beb7368e7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5731035
    > Commit-Queue: Krishna Govind <govind@chromium.org>
    > Owners-Override: Krishna Govind <govind@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Sam Maier <smaier@chromium.org>
    > Reviewed-by: Krishna Govind <govind@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1331338}
    
    Bug: 348183775
    Change-Id: Id0c0f5c20701b2e32b8c674f94d1616d4f5b317e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735732
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332564}

--- third_party/jni_zero/java/src/org/jni_zero/JniInit.java
+++ third_party/jni_zero/java/src/org/jni_zero/JniInit.java
@@ -16,4 +16,34 @@ public class JniInit {
         // RegisterNatives(), but simpler to return an array than to make Java->Native work.
         return new Object[] {Collections.EMPTY_LIST, Collections.EMPTY_MAP};
     }
+
+    @CalledByNative
+    private static void crashIfMultiplexingMisaligned(long wholeHash, long priorityHash) {
+        try {
+            // Reflection is required because we cannot reference the J/N class at compile time -
+            // it gets inserted at the very end of the build process as a srcjar_dep.
+            long javaWholeHash = Class.forName("J.N").getField("WHOLE_HASH").getLong(null);
+            long javaPriorityHash = Class.forName("J.N").getField("PRIORITY_HASH").getLong(null);
+            // We only compare the "priority" to the "whole" - we need the entirety of at least one
+            // to always be compared.
+            if (javaWholeHash != wholeHash
+                    && javaWholeHash != priorityHash
+                    && javaPriorityHash != wholeHash) {
+                throw new RuntimeException(
+                        "JNI Zero multiplexing hashes do not align. Native: "
+                                + wholeHash
+                                + " or "
+                                + priorityHash
+                                + " Java: "
+                                + javaWholeHash
+                                + " or "
+                                + javaPriorityHash);
+            }
+        } catch (ReflectiveOperationException e) {
+            // This check is just a backup. If we fail to actually do the check, we assert so that
+            // we get some notice on debug/assert enabled builds, but don't want to crash everyone
+            // since it's likely fine.
+            assert false : "JNI multiplexing hash lookup failed with " + e.getMessage();
+        }
+    }
 }
