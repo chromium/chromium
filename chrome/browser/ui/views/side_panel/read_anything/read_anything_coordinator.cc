439953631565d7a76eac3faf33801ee48c237eff
Tzarial Kuznia <zork@google.com>
2024-07-24 13:08:48 +0000

    Update reading mode gdoc helper to load from resource files.
    Change-Id: I3f4bb9a9511a57807be98af31350a3834adc9c82
    Bug: b:324143642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5688110
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Tzarial Kuznia <zork@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332283}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator.cc
@@ -8,6 +8,7 @@
 #include <string>
 #include <vector>
 
+#include "base/check_is_test.h"
 #include "base/functional/bind.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
@@ -15,6 +16,8 @@
 #include "build/chromeos_buildflags.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/accessibility/embedded_a11y_extension_loader.h"
+#include "chrome/browser/extensions/component_loader.h"
+#include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
@@ -29,9 +32,11 @@
 #include "chrome/browser/ui/webui/side_panel/read_anything/read_anything_prefs.h"
 #include "chrome/browser/ui/webui/side_panel/read_anything/read_anything_untrusted_ui.h"
 #include "chrome/common/extensions/extension_constants.h"
+#include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/accessibility/reading/distillable_pages.h"
 #include "components/feature_engagement/public/feature_constants.h"
+#include "extensions/browser/extension_system.h"
 #include "ui/accessibility/accessibility_features.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -312,6 +317,7 @@ void ReadAnythingCoordinator::ActivePageDistillable() {
 }
 
 void ReadAnythingCoordinator::InstallGDocsHelperExtension() {
+#if BUILDFLAG(IS_CHROMEOS)
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   EmbeddedA11yManagerLacros::GetInstance()->SetReadingModeEnabled(true);
 #else
@@ -321,15 +327,45 @@ void ReadAnythingCoordinator::InstallGDocsHelperExtension() {
       extension_misc::kReadingModeGDocsHelperManifestFilename,
       /*should_localize=*/false);
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+#else
+  extensions::ExtensionService* service =
+      extensions::ExtensionSystem::Get(GetBrowser().profile())
+          ->extension_service();
+  if (!service) {
+    // In tests, the service might not be created.
+    CHECK_IS_TEST();
+    return;
+  }
+  extensions::ComponentLoader* component_loader = service->component_loader();
+  if (!component_loader->Exists(
+          extension_misc::kReadingModeGDocsHelperExtensionId)) {
+    component_loader->Add(
+        IDR_READING_MODE_GDOCS_HELPER_MANIFEST,
+        base::FilePath(FILE_PATH_LITERAL("reading_mode_gdocs_helper")));
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
 void ReadAnythingCoordinator::RemoveGDocsHelperExtension() {
+#if BUILDFLAG(IS_CHROMEOS)
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   EmbeddedA11yManagerLacros::GetInstance()->SetReadingModeEnabled(false);
 #else
   EmbeddedA11yExtensionLoader::GetInstance()->RemoveExtensionWithId(
       extension_misc::kReadingModeGDocsHelperExtensionId);
 #endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+#else
+  extensions::ExtensionService* service =
+      extensions::ExtensionSystem::Get(GetBrowser().profile())
+          ->extension_service();
+  if (!service) {
+    // In tests, the service might not be created.
+    CHECK_IS_TEST();
+    return;
+  }
+  service->component_loader()->Remove(
+      extension_misc::kReadingModeGDocsHelperExtensionId);
+#endif  // BUILDFLAG(IS_CHROMEOS)
 }
 
 void ReadAnythingCoordinator::ActivePageNotDistillableForTesting() {
