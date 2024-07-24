e53da5e3b0d743c6fd6569679b1d950804319ccf
Joel Hockey <joelhockey@chromium.org>
2024-07-24 23:57:04 +0000

    Enable DragDropFiles for M129
    Bug: b/350860794
    Change-Id: Ifbe80b122e70cfd45f4fa3a652de131b749c314d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5722410
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332683}

--- ui/base/ui_base_features.cc
+++ ui/base/ui_base_features.cc
@@ -273,9 +273,7 @@ BASE_FEATURE(kClipboardFiles,
              "ClipboardFiles",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kDragDropFiles,
-             "DragDropFiles",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kDragDropFiles, "DragDropFiles", base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN)
