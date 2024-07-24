a4afe80bdb19cfd78126844ceb2504d2a3f58ca6
Peter McNeeley <petermcneeley@chromium.org>
2024-07-24 00:04:13 +0000

    Finch null hypothesis testing using meta finch study
    This feature does nothing but print if it is enabled or disable on
    the start of the viz thread.
    
    Bug: 354724066
    Change-Id: Ic8766b3f2f1a83d358cd3b6ba5079435a375b131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730477
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332049}

--- components/viz/common/features.h
+++ components/viz/common/features.h
@@ -92,6 +92,7 @@ VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kBatchMainThreadReleaseCallbacks);
 VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kColorConversionInRenderer);
 VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kLastVSyncArgsKillswitch);
 VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kBlitRequestsForViewTransition);
+VIZ_COMMON_EXPORT BASE_DECLARE_FEATURE(kVizNullHypothesis);
 
 VIZ_COMMON_EXPORT extern const char kDraw1Point12Ms[];
 VIZ_COMMON_EXPORT extern const char kDraw2Points6Ms[];
