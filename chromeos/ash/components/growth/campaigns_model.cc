a3ccbdf4549849a8a8f3fe3e10105b3943fdc88d
Li Lin <llin@chromium.org>
2024-07-24 02:08:54 +0000

    [Perk Discovery]: Add G1 notification image to the Growth Framework.
    Bug: b/353771356
    Change-Id: Id8b8a782333702513cb0ca78df6883cf17420759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726016
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332104}

--- chromeos/ash/components/growth/campaigns_model.cc
+++ chromeos/ash/components/growth/campaigns_model.cc
@@ -204,6 +204,8 @@ std::optional<int> GetBuiltInImageResourceId(
       return IDR_GROWTH_FRAMEWORK_SPARK_1P_APP_PNG;
     case BuiltInImage::kSparkV2:
       return IDR_GROWTH_FRAMEWORK_SPARK_V2_PNG;
+    case BuiltInImage::kG1Notification:
+      return IDR_GROWTH_FRAMEWORK_G1_NOTIFICATION_PNG;
   }
 }
 
