1c3da31a617b20954f71a568e4d7f623506b8797
Takuto Ikuta <tikuta@chromium.org>
2024-07-24 02:04:25 +0000

    content/public/renderer: fix missing deps
    This is to fix build error like
    https://ci.chromium.org/ui/p/chromium/builders/build/android-build-perf-ninja/1663/overview
    
    This is follow up CL of https://crrev.com/c/5718751.
    
    Bug: b/352472971
    Bug: 40253918
    Change-Id: I27e61cadd36bb300bbdf1278a5a929bbe9554910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730453
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332102}

--- content/public/renderer/render_frame_media_playback_options.h
+++ content/public/renderer/render_frame_media_playback_options.h
@@ -5,8 +5,7 @@
 #ifndef CONTENT_PUBLIC_RENDERER_RENDER_FRAME_MEDIA_PLAYBACK_OPTIONS_H_
 #define CONTENT_PUBLIC_RENDERER_RENDER_FRAME_MEDIA_PLAYBACK_OPTIONS_H_
 
-#include "build/android_buildflags.h"
-#include "build/build_config.h"
+#include "content/common/content_export.h"
 #include "content/public/common/media_playback_renderer_type.mojom.h"
 
 namespace content {
@@ -14,11 +13,7 @@ namespace content {
 // Default value for is_background_suspend_enabled is determined statically in
 // Chromium, but some content embedders (e.g. Cast) may need to change it at
 // runtime.
-#if BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_DESKTOP_ANDROID)
-const bool kIsBackgroundMediaSuspendEnabled = true;
-#else
-const bool kIsBackgroundMediaSuspendEnabled = false;
-#endif
+CONTENT_EXPORT extern const bool kIsBackgroundMediaSuspendEnabled;
 
 struct RenderFrameMediaPlaybackOptions {
   // Whether the renderer should automatically suspend media playback on
