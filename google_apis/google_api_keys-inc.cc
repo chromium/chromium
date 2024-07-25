32fb53e6a4f76da5f040a87afa31f60eda5075f4
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-07-25 13:18:02 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to google_apis/*
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    Note that in patchset2, change to:
    /build/config/unsafe_buffers_paths.txt
    was reverted. Indeed, running too many (~3) CQ run touching this file is
    making the builder cache much slower. I will bundle every change to this
    file in a subsequent CL. I will limit myself to 1-2 CQ run per day.
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 40285824
    Change-Id: I6c49b30cb64b4598caa6ecb554f0ff1b1c1599a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717991
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332903}

--- google_apis/google_api_keys-inc.cc
+++ google_apis/google_api_keys-inc.cc
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "google_apis/google_api_keys.h"
 
 // If you add more includes to this list, you also need to add them to
