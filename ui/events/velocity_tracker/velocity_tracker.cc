9057e2bfde973f7e44a5b203618c38c994e04d83
Devon Loehr <dloehr@google.com>
2024-07-25 14:00:59 +0000

    Remove uses of variable-length arrays
    Variable-length arrays are not part of the C++ standard, and are
    dangerous to use because they allocate a potentially-unbounded
    amount of data on the stack. In addition, they trigger a clang warning
    (-Wvla-extension) which is currently suppressed in Chromium builds.
    
    There are some uses of VLAs in velocity_tracker.cc that were already
    disabled for MSCV. This simply extends that to disable them
    unconditionally.
    
    Bug: 349656479
    Change-Id: Ibae540ba9980fc280532e1fff7ec32cfb9a17b18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5738896
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332915}

--- ui/events/velocity_tracker/velocity_tracker.cc
+++ ui/events/velocity_tracker/velocity_tracker.cc
@@ -490,18 +490,11 @@ static bool SolveLeastSquares(const float* x,
                               uint32_t n,
                               float* out_b,
                               float* out_det) {
-  // MSVC does not support variable-length arrays (used by the original Android
-  // implementation of this function).
-#if defined(COMPILER_MSVC)
-  const uint32_t M_ARRAY_LENGTH =
+  constexpr uint32_t M_ARRAY_LENGTH =
       LeastSquaresVelocityTrackerStrategy::kHistorySize;
-  const uint32_t N_ARRAY_LENGTH = Estimator::kMaxDegree;
+  constexpr uint32_t N_ARRAY_LENGTH = Estimator::kMaxDegree;
   DCHECK_LE(m, M_ARRAY_LENGTH);
   DCHECK_LE(n, N_ARRAY_LENGTH);
-#else
-  const uint32_t M_ARRAY_LENGTH = m;
-  const uint32_t N_ARRAY_LENGTH = n;
-#endif
 
   // Expand the X vector to a matrix A, pre-multiplied by the weights.
   float a[N_ARRAY_LENGTH][M_ARRAY_LENGTH];  // column-major order
