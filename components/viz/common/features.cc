dbbf1b0e7d2e6574de3c7187efddbfad0d161df0
Robert Flack <flackr@chromium.org>
2024-07-25 15:21:39 +0000

    Enable BackdropFilterMirrorEdgeMode by default.
    This enables using the mirror edge mode for oversampling by
    backdrop-filter. Mirroring at the edge instead of duplicating
    produces more visually accurate blurs and reduces flickering
    when new content is introduced at the edges.
    
    PSA: https://groups.google.com/a/chromium.org/g/blink-dev/c/ZtMnFCHZhMQ/m/ewdpvCq_AQAJ
    
    Bug: 40040614
    Change-Id: Ie1b579f28b43c326b3542cdd6c5b7b7dd0a67b60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741893
    Commit-Queue: Robert Flack <flackr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332969}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -36,7 +36,7 @@ BASE_FEATURE(kAndroidBrowserControlsInViz,
 
 BASE_FEATURE(kBackdropFilterMirrorEdgeMode,
              "BackdropFilterMirrorEdgeMode",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUseDrmBlackFullscreenOptimization,
              "UseDrmBlackFullscreenOptimization",
--- third_party/blink/web_tests/virtual/backdrop-filter-mirror-edge/external/wpt/css/filter-effects/README.txt
+++
@@ -1,2 +0,0 @@
-This directory contains tests for the BackdropFilterMirrorEdgeMode
-feature, which is tracked by https://crbug.com/40040614.
