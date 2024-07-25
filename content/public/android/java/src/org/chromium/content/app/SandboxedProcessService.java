afb00af19589dad0d16d7f69aeb9d7589f262e18
Andrew Grieve <agrieve@chromium.org>
2024-07-25 18:32:21 +0000

    Android: Enable R8 for content_browsertests
    Should make the tests run a bit faster and also reduce logspam about dex
    verification failures.
    
    Also simplifies tools:node="remove" nodes for
    androidx.startup.InitializationProvider to list only required
    attributes.
    
    Include-Ci-Only-Tests: true
    Bug: 40277467
    Change-Id: I23df73f47456c4b7db6ef2bfd31d789da2a672fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5734577
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333111}

--- content/public/android/java/src/org/chromium/content/app/SandboxedProcessService.java
+++ content/public/android/java/src/org/chromium/content/app/SandboxedProcessService.java
@@ -4,8 +4,11 @@
 
 package org.chromium.content.app;
 
+import org.chromium.build.annotations.UsedByReflection;
+
 /**
- * Sandboxed Services inherit from this class. We enforce the privileged/sandboxed
- * distinction by type-checking objects against this parent class.
+ * Sandboxed Services inherit from this class. We enforce the privileged/sandboxed distinction by
+ * type-checking objects against this parent class.
  */
+@UsedByReflection("Subclass names constructed by appending a number to this class's name")
 public class SandboxedProcessService extends ContentChildProcessService {}
