099f1e1dffa6dc0cc73e38af003d782c209067ec
Lingqi Chi <lingqi@chromium.org>
2024-07-25 06:00:03 +0000

    Precog: tweak SearchPrefetchEligibilityReason's IFTTT
    we do not record SearchPrefetchEligibilityReason in precog dashboard.
    Instead, it is using PreloadingEligibility or ChromePreloadingEligibility.
    
    Change-Id: Ie6bb7109734f6f34bad82d5db0621d09bbe35be2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735819
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332799}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.h
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_service.h
@@ -46,11 +46,9 @@ struct ResourceRequest;
 // numeric values should never be reused.
 //
 // Any updates to this class need to be propagated to enums.xml.
-//
-// If you change this, please follow the process in
-// go/preloading-dashboard-updates to update the mapping reflected in dashboard,
-// or if you are not a Googler, please file an FYI bug on https://crbug.new with
-// component Internals>Preload.
+// This enum is used for UMA only. For recording a UKM PreloadingAttempt record,
+// use or introduce a corresponding enum to content::PreloadingEligibility or
+// ChromePreloadingEligibility.
 //
 // LINT.IfChange
 enum class SearchPrefetchEligibilityReason {
@@ -77,7 +75,7 @@ enum class SearchPrefetchEligibilityReason {
   kSlowNetwork = 9,
   kMaxValue = kSlowNetwork,
 };
-// LINT.ThenChange()
+// LINT.ThenChange(/tools/metrics/histograms/enums.xml:SearchPrefetchEligibilityReason)
 
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
