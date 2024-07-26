0494cf01d4c6e51808d0c1ef7611d7807888185f
Jan Keitel <jkeitel@google.com>
2024-07-26 21:29:20 +0000

    Remove unsafe buffer pragmas from DiscardableSharedMemoryManager.
    Bots are happy and discardable_memory is part of the unsafe buffer
    checks. It appears that these annotations were not necessary.
    
    Change-Id: I276ae10a273adab6e08ea93b1b7d40336914ab69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725456
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333783}

--- components/discardable_memory/service/discardable_shared_memory_manager.cc
+++ components/discardable_memory/service/discardable_shared_memory_manager.cc
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-#pragma check_unsafe_buffers
-#endif
-
 #include "components/discardable_memory/service/discardable_shared_memory_manager.h"
 
 #include <algorithm>
