96aacd05c1f8837fbf73dd0bff0aba8af44732c6
John Admanski <jadmanski@google.com>
2024-07-26 17:05:50 +0000

    Drop use of the AuthScanResult signal.
    This signal is subsumed into the PrepareAuthFactorProgress signal, which
    contains a superset of the information. Nothing else appears to be
    depending directly on this specific signal.
    
    BUG=b:347752239
    
    Change-Id: I33a44e178db972af2c20041de181944e570c5c4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5736075
    Commit-Queue: John Admanski <jadmanski@chromium.org>
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333634}

--- chromeos/ash/components/dbus/userdataauth/userdataauth_client.cc
+++ chromeos/ash/components/dbus/userdataauth/userdataauth_client.cc
@@ -425,25 +425,12 @@ class UserDataAuthClientImpl : public UserDataAuthClient {
     }
   }
 
-  void OnAuthScanResult(dbus::Signal* signal) {
-    dbus::MessageReader reader(signal);
-    ::user_data_auth::AuthScanResult proto;
-    if (!reader.PopArrayOfBytesAsProto(&proto)) {
-      LOG(ERROR)
-          << "Failed to parse AuthScanResult protobuf from UserDataAuth signal";
-      return;
-    }
-    for (auto& observer : fp_observer_list_) {
-      observer.OnFingerprintScan(proto.fingerprint_result());
-    }
-  }
-
   void OnAuthEnrollmentProgress(dbus::Signal* signal) {
     dbus::MessageReader reader(signal);
     ::user_data_auth::AuthEnrollmentProgress proto;
     if (!reader.PopArrayOfBytesAsProto(&proto)) {
-      LOG(ERROR)
-          << "Failed to parse AuthScanResult protobuf from UserDataAuth signal";
+      LOG(ERROR) << "Failed to parse AuthEnrollmentProgress protobuf from "
+                    "UserDataAuth signal";
       return;
     }
     for (auto& observer : fp_observer_list_) {
@@ -476,6 +463,16 @@ class UserDataAuthClientImpl : public UserDataAuthClient {
         observer.OnFingerprintAuthScan(
             proto.auth_progress().biometrics_progress());
       }
+    } else if (proto.purpose() ==
+                   ::user_data_auth::PURPOSE_AUTHENTICATE_AUTH_FACTOR &&
+               proto.auth_progress().auth_factor_type() ==
+                   ::user_data_auth::AUTH_FACTOR_TYPE_LEGACY_FINGERPRINT) {
+      for (auto& observer : fp_observer_list_) {
+        observer.OnFingerprintScan(proto.auth_progress()
+                                       .biometrics_progress()
+                                       .scan_result()
+                                       .fingerprint_result());
+      }
     } else {
       LOG(ERROR) << "Received a unrecognized PrepareAuthFactorProgress signal";
       return;
@@ -495,12 +492,6 @@ class UserDataAuthClientImpl : public UserDataAuthClient {
         ::user_data_auth::kUserDataAuthInterface,
         ::user_data_auth::kLowDiskSpace,
         base::BindRepeating(&UserDataAuthClientImpl::OnLowDiskSpace,
-                            weak_factory_.GetWeakPtr()),
-        base::BindOnce(&OnSignalConnected));
-    proxy_->ConnectToSignal(
-        ::user_data_auth::kUserDataAuthInterface,
-        ::user_data_auth::kAuthScanResultSignal,
-        base::BindRepeating(&UserDataAuthClientImpl::OnAuthScanResult,
                             weak_factory_.GetWeakPtr()),
         base::BindOnce(&OnSignalConnected));
     proxy_->ConnectToSignal(
