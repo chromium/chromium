d4c207dd041e95cff50fceb7b923c44ad4b817be
George <geoada@meta.com>
2024-07-26 20:49:11 +0000

    Remove Unused Variable in DownloadForegroundServiceImpl
    This private variable is never referenced.
    
    Change-Id: Ib9b6e25115326ac2c2825b28e5966b6b7569fcb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742898
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333765}

--- chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundServiceImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/DownloadForegroundServiceImpl.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.download;
 import static org.chromium.chrome.browser.download.DownloadSnackbarController.INVALID_NOTIFICATION_ID;
 
 import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -21,7 +20,6 @@ import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 import androidx.core.app.ServiceCompat;
 
-import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.components.browser_ui.notifications.ForegroundServiceUtils;
 
@@ -33,23 +31,11 @@ public class DownloadForegroundServiceImpl extends DownloadForegroundService.Imp
     private static final String TAG = "DownloadFg";
     private final IBinder mBinder = new LocalBinder();
 
-    private NotificationManager mNotificationManager;
-
     @IntDef({StopForegroundNotification.KILL, StopForegroundNotification.DETACH})
     @Retention(RetentionPolicy.SOURCE)
     public @interface StopForegroundNotification {
         int KILL = 0; // Kill notification regardless of ability to detach.
         int DETACH = 1; // Try to detach, otherwise kill and relaunch.
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        mNotificationManager =
-                (NotificationManager)
-                        ContextUtils.getApplicationContext()
-                                .getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     /**
