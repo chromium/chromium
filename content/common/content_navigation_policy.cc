98ed74e8c59ca122d56cd0165d079c48355ff356
Rakina Zata Amni <rakina@chromium.org>
2024-07-26 14:36:16 +0000

    [M128] Stop enabling RD subframe by default
    We're turning back to only enable it on 1% of Stable to avoid exposing
    the linked bug to too many users. The bug itself has a workaround so
    we can continue experimenting to get exposure of more interesting cases
    and also continue experiment the main frame version.
    
    (cherry picked from commit a51e308785f768ffb974b87be4306d32c7db0dea)
    
    Bug: 352689348
    Change-Id: I284796929634ef9f656af38db38f956216b62f6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737695
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1332342}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743140
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6613@{#148}
    Cr-Branched-From: 03c1799e6f9c7239802827eab5e935b9e14fceae-refs/heads/main@{#1331488}

--- content/common/content_navigation_policy.cc
+++ content/common/content_navigation_policy.cc
@@ -114,7 +114,7 @@ constexpr base::FeatureParam<RenderDocumentLevel>::Option
         {RenderDocumentLevel::kAllFrames, "all-frames"}};
 const base::FeatureParam<RenderDocumentLevel> render_document_level{
     &features::kRenderDocument, kRenderDocumentLevelParameterName,
-    RenderDocumentLevel::kSubframe, &render_document_levels};
+    RenderDocumentLevel::kCrashedFrame, &render_document_levels};
 
 RenderDocumentLevel GetRenderDocumentLevel() {
   if (base::FeatureList::IsEnabled(features::kRenderDocument))
