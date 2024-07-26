3a0e070ba2732e0664f64de691d0af941a5b1ed0
Timofey Chudakov <tchudakov@google.com>
2024-07-26 13:01:55 +0000

    [Android] Rename address sheet keyboard accessory actions.
    Bug: 327838324
    Change-Id: I96d8476e8f0872dfb8d7e5ca1c9d6cbda59c3ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741546
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333505}

--- chrome/browser/keyboard_accessory/android/manual_filling_controller_impl.cc
+++ chrome/browser/keyboard_accessory/android/manual_filling_controller_impl.cc
@@ -398,8 +398,8 @@ AccessoryController* ManualFillingControllerImpl::GetControllerForAction(
     case AccessoryAction::CROSS_DEVICE_PASSKEY:
       return pwd_controller_.get();
     case AccessoryAction::MANAGE_ADDRESSES:
-    case AccessoryAction::CREATE_PLUS_ADDRESS:
-    case AccessoryAction::SELECT_PLUS_ADDRESS:
+    case AccessoryAction::CREATE_PLUS_ADDRESS_FROM_ADDRESS_SHEET:
+    case AccessoryAction::SELECT_PLUS_ADDRESS_FROM_ADDRESS_SHEET:
       return address_controller_.get();
     case AccessoryAction::MANAGE_CREDIT_CARDS:
       return payment_method_controller_.get();
