71f2e397a1cb6626eb8dee9474280da9b367329e
Gabriel Viera <gviera@google.com>
2024-07-28 23:39:19 +0000

    [Webauthn] Add Passkey Deleted Confirmation View
    * Adds views for a bubble that will appear to notify the user
      that an invalid passkey was deleted and that they need to sign in to
      create a new one.
    
    https://screenshot.googleplex.com/VcaTs3d4mSFTdVf.png
    
    Bug: 347727501
    Change-Id: I7fd01d4aea8631d741b1e4ba5aa159a28d23ba63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742098
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Gabriel Viera <gviera@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334051}

---
+++ chrome/browser/ui/views/webauthn/passkey_deleted_confirmation_view.h
@@ -0,0 +1,31 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_WEBAUTHN_PASSKEY_DELETED_CONFIRMATION_VIEW_H_
+#define CHROME_BROWSER_UI_VIEWS_WEBAUTHN_PASSKEY_DELETED_CONFIRMATION_VIEW_H_
+
+#include "chrome/browser/ui/views/passwords/password_bubble_view_base.h"
+#include "chrome/browser/ui/webauthn/passkey_deleted_confirmation_controller.h"
+
+// A view informing the user that their passkey was deleted.
+class PasskeyDeletedConfirmationView : public PasswordBubbleViewBase {
+ public:
+  PasskeyDeletedConfirmationView(content::WebContents* web_contents,
+                                 views::View* anchor_view,
+                                 DisplayReason display_reason);
+  ~PasskeyDeletedConfirmationView() override;
+
+ private:
+  // PasswordBubbleViewBase:
+  PasskeyDeletedConfirmationController* GetController() override;
+  const PasskeyDeletedConfirmationController* GetController() const override;
+  ui::ImageModel GetWindowIcon() override;
+
+  // Notifies the `controller_` to open password manager and closes the bubble.
+  void OnManagePasskeysButtonClicked();
+
+  PasskeyDeletedConfirmationController controller_;
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_WEBAUTHN_PASSKEY_DELETED_CONFIRMATION_VIEW_H_
