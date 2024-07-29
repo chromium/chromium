8807a1b0f01cf1a22cc913ac68bf9ba832cc6158
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-07-29 23:30:44 +0000

    [M128][sync] Expose LocalDataDescription's domain display text to Java
    No behavior change, but exposes this logic [1] to Java. There are
    no new strings, a single string is moved from ios_strings.grd to
    sync_ui_strings.grdp so it can reused by Android.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:ios/chrome/browser/ui/settings/google_services/bulk_upload/bulk_upload_mediator.mm;l=193;drc=fc0de1c030cdf94435d3791012ccbf00b1b26738
    
    (cherry picked from commit 9383548c4493a92160313a8ecac5365530d93048)
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL the lines that are not tested are just plumbing from C++ to Java and will be tested by integration tests in future CLs.
    Bug: 326040498
    Change-Id: Ife5b954fc3699efa455e57de3b8fea9799353858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5733878
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1332465}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741831
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/branch-heads/6613@{#297}
    Cr-Branched-From: 03c1799e6f9c7239802827eab5e935b9e14fceae-refs/heads/main@{#1331488}

--- components/sync/android/java/src/org/chromium/components/sync/LocalDataDescription.java
+++ components/sync/android/java/src/org/chromium/components/sync/LocalDataDescription.java
@@ -7,8 +7,12 @@ package org.chromium.components.sync;
 import androidx.annotation.VisibleForTesting;
 
 import org.jni_zero.CalledByNative;
+import org.jni_zero.JNINamespace;
+import org.jni_zero.JniType;
+import org.jni_zero.NativeMethods;
 
 /** Java version of the native LocalDataDescription struct. */
+@JNINamespace("syncer")
 public class LocalDataDescription {
     private final int mItemCount;
     private final String[] mDomains;
@@ -22,8 +26,30 @@ public class LocalDataDescription {
         mDomainCount = domainCount;
     }
 
+    /**
+     * Returns a string that summarizes the domain content of the description, meant to be consumed
+     * by the UI. Must not be called if itemCount() is 0.
+     *
+     * @return The display text.
+     */
+    public String getDomainsDisplayText() {
+        assert mItemCount > 0;
+        assert mDomains.length > 0;
+        return LocalDataDescriptionJni.get()
+                .getDomainsDisplayText(mItemCount, mDomains, mDomainCount);
+    }
+
     @VisibleForTesting
     public int itemCount() {
         return mItemCount;
+    }
+
+    @NativeMethods
+    interface Natives {
+        @JniType("std::u16string")
+        String getDomainsDisplayText(
+                int itemCount,
+                @JniType("std::vector<std::string>") String[] domains,
+                int domainCount);
     }
 }
