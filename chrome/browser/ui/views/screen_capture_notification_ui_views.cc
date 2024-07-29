da84fa1ed63f3802bf788d1f1876b1e2972e6be5
Allen Bauer <kylixrd@chromium.org>
2024-07-29 16:50:36 +0000

    Modernize the ScreenCaptureNotificationUI code.
    o Use new CLIENT_OWNS_WIDGET ownership mode.
    o Remove use of set_owned_by_client() - was deprecated.
    o Separate implementation of ScreenCaptureNotificationUI from widget delegate view.
    o Use std::unique_ptr<Widget> for lifetime management of Widget.
    o Remove DanglingUntriaged from the raw_ptrs
    
    Change-Id: Ifce50eec9d9b95d2f1f1f7429eb91d492d139fbe
    Bug: 338254375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744035
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334291}

--- chrome/browser/ui/views/screen_capture_notification_ui_views.cc
+++ chrome/browser/ui/views/screen_capture_notification_ui_views.cc
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/screen_capture_notification_ui.h"
 
 #include <memory>
 
 #include "base/functional/bind.h"
+#include "base/memory/raw_ptr.h"
 #include "base/scoped_multi_source_observation.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -24,6 +24,7 @@
 #include "ui/color/color_provider.h"
 #include "ui/display/display.h"
 #include "ui/display/screen.h"
+#include "ui/views/background.h"
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/md_text_button.h"
@@ -96,27 +97,21 @@ BEGIN_METADATA(NotificationBarClientView)
 ADD_PROPERTY_METADATA(gfx::Rect, ClientRect)
 END_METADATA
 
-// ScreenCaptureNotificationUI implementation using Views.
-class ScreenCaptureNotificationUIViews : public ScreenCaptureNotificationUI,
-                                         public views::WidgetDelegateView,
+class ScreenCaptureNotificationUIViews : public views::WidgetDelegateView,
                                          public views::ViewObserver {
   METADATA_HEADER(ScreenCaptureNotificationUIViews, views::WidgetDelegateView)
 
  public:
   ScreenCaptureNotificationUIViews(
       const std::u16string& text,
-      content::WebContents* capturing_web_contents);
+      content::WebContents* capturing_web_contents,
+      base::OnceClosure stop_callback,
+      content::MediaStreamUI::SourceCallback source_callback);
   ScreenCaptureNotificationUIViews(const ScreenCaptureNotificationUIViews&) =
       delete;
   ScreenCaptureNotificationUIViews& operator=(
       const ScreenCaptureNotificationUIViews&) = delete;
   ~ScreenCaptureNotificationUIViews() override;
-
-  // ScreenCaptureNotificationUI:
-  gfx::NativeViewId OnStarted(
-      base::OnceClosure stop_callback,
-      content::MediaStreamUI::SourceCallback source_callback,
-      const std::vector<content::DesktopMediaID>& media_ids) override;
 
   // views::WidgetDelegateView:
   views::ClientView* CreateClientView(views::Widget* widget) override;
@@ -124,42 +119,68 @@ class ScreenCaptureNotificationUIViews : public ScreenCaptureNotificationUI,
       views::Widget* widget) override;
 
   // views::ViewObserver:
-  void OnViewBoundsChanged(View* observed_view) override;
+  void OnViewBoundsChanged(views::View* observed_view) override;
+  void OnViewIsDeleting(views::View* observed_view) override;
 
  private:
   // Helper to call |stop_callback_|.
   void NotifyStopped();
   // Helper to call |source_callback_|.
   void NotifySourceChange();
+
+  const base::WeakPtr<content::WebContents> capturing_web_contents_;
+  base::OnceClosure stop_callback_;
+  content::MediaStreamUI::SourceCallback source_callback_;
+  base::ScopedMultiSourceObservation<views::View, views::ViewObserver>
+      view_observations_{this};
+  raw_ptr<NotificationBarClientView> client_view_ = nullptr;
+  raw_ptr<views::View> source_button_ = nullptr;
+  raw_ptr<views::View> stop_button_ = nullptr;
+  raw_ptr<views::View> hide_link_ = nullptr;
+};
+
+// ScreenCaptureNotificationUI implementation using Views.
+class ScreenCaptureNotificationUIImpl : public ScreenCaptureNotificationUI {
+ public:
+  ScreenCaptureNotificationUIImpl(const std::u16string& text,
+                                  content::WebContents* capturing_web_contents);
+  ScreenCaptureNotificationUIImpl(const ScreenCaptureNotificationUIImpl&) =
+      delete;
+  ScreenCaptureNotificationUIImpl& operator=(
+      const ScreenCaptureNotificationUIImpl&) = delete;
+  ~ScreenCaptureNotificationUIImpl() override = default;
+
+  // ScreenCaptureNotificationUI override:
+  gfx::NativeViewId OnStarted(
+      base::OnceClosure stop_callback,
+      content::MediaStreamUI::SourceCallback source_callback,
+      const std::vector<content::DesktopMediaID>& media_ids) override;
+
+ private:
   // Helper to set window id to parent browser window id for task bar grouping.
 #if BUILDFLAG(IS_WIN)
   void SetWindowsAppId(views::Widget* widget);
 #endif
 
-  base::OnceClosure stop_callback_;
-  content::MediaStreamUI::SourceCallback source_callback_;
-  base::ScopedMultiSourceObservation<views::View, views::ViewObserver>
-      bounds_observations_{this};
-  raw_ptr<NotificationBarClientView, DanglingUntriaged> client_view_ = nullptr;
-  raw_ptr<views::View, DanglingUntriaged> source_button_ = nullptr;
-  raw_ptr<views::View, DanglingUntriaged> stop_button_ = nullptr;
-  raw_ptr<views::View, DanglingUntriaged> hide_link_ = nullptr;
-  const base::WeakPtr<content::WebContents> capturing_web_contents_;
+  std::u16string text_;
+  base::WeakPtr<content::WebContents> capturing_web_contents_;
+  std::unique_ptr<views::Widget> widget_;
 };
 
 ScreenCaptureNotificationUIViews::ScreenCaptureNotificationUIViews(
     const std::u16string& text,
-    content::WebContents* capturing_web_contents)
+    content::WebContents* capturing_web_contents,
+    base::OnceClosure stop_callback,
+    content::MediaStreamUI::SourceCallback source_callback)
     : capturing_web_contents_(capturing_web_contents
                                   ? capturing_web_contents->GetWeakPtr()
-                                  : nullptr) {
+                                  : nullptr),
+      stop_callback_(std::move(stop_callback)),
+      source_callback_(std::move(source_callback)) {
   SetShowCloseButton(false);
   SetShowTitle(false);
   SetTitle(text);
 
-  // TODO(pbos): Investigate if this can be SetOwnedByWidget(true) and get rid
-  // of `delete GetWidget();` in the destructor.
-  set_owned_by_client();
   SetOwnedByWidget(false);
   RegisterDeleteDelegateCallback(
       base::BindOnce(&ScreenCaptureNotificationUIViews::NotifyStopped,
@@ -186,6 +207,10 @@ ScreenCaptureNotificationUIViews::ScreenCaptureNotificationUIViews(
                           base::Unretained(this)),
       source_text));
 
+  if (source_callback_.is_null()) {
+    source_button_->SetVisible(false);
+  }
+
   std::u16string stop_text =
       l10n_util::GetStringUTF16(IDS_MEDIA_SCREEN_CAPTURE_NOTIFICATION_STOP);
   auto stop_button = std::make_unique<views::MdTextButton>(
@@ -206,90 +231,24 @@ ScreenCaptureNotificationUIViews::ScreenCaptureNotificationUIViews(
 
   // The client rect for NotificationBarClientView uses the bounds for the
   // following views.
-  bounds_observations_.AddObservation(source_button_.get());
-  bounds_observations_.AddObservation(stop_button_.get());
-  bounds_observations_.AddObservation(hide_link_.get());
+  view_observations_.AddObservation(source_button_.get());
+  view_observations_.AddObservation(stop_button_.get());
+  view_observations_.AddObservation(hide_link_.get());
+
+  SetBackground(views::CreateThemedSolidBackground(ui::kColorDialogBackground));
 }
 
 ScreenCaptureNotificationUIViews::~ScreenCaptureNotificationUIViews() {
   source_callback_.Reset();
   stop_callback_.Reset();
-  delete GetWidget();
-}
-
-gfx::NativeViewId ScreenCaptureNotificationUIViews::OnStarted(
-    base::OnceClosure stop_callback,
-    content::MediaStreamUI::SourceCallback source_callback,
-    const std::vector<content::DesktopMediaID>& media_ids) {
-  if (GetWidget())
-    return 0;
-
-  stop_callback_ = std::move(stop_callback);
-  source_callback_ = std::move(source_callback);
-
-  if (source_callback_.is_null())
-    source_button_->SetVisible(false);
-
-  views::Widget* widget = new views::Widget;
-
-  views::Widget::InitParams params(
-      views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET,
-      views::Widget::InitParams::TYPE_WINDOW);
-  params.delegate = this;
-  params.opacity = views::Widget::InitParams::WindowOpacity::kTranslucent;
-  params.remove_standard_frame = true;
-  params.z_order = ui::ZOrderLevel::kFloatingUIElement;
-  params.name = "ScreenCaptureNotificationUIViews";
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  // TODO(sergeyu): The notification bar must be shown on the monitor that's
-  // being captured. Make sure it's always the case. Currently we always capture
-  // the primary monitor.
-  params.context = ash::Shell::GetPrimaryRootWindow();
-#endif
-
-  widget->set_frame_type(views::Widget::FrameType::kForceCustom);
-  widget->Init(std::move(params));
-
-  SetBackground(views::CreateSolidBackground(
-      GetColorProvider()->GetColor(ui::kColorDialogBackground)));
-
-  display::Screen* screen = display::Screen::GetScreen();
-  // TODO(sergeyu): Move the notification to the display being captured when
-  // per-display screen capture is supported.
-  gfx::Rect work_area = screen->GetPrimaryDisplay().work_area();
-
-  // Place the bar in the center of the bottom of the display.
-  gfx::Size size = widget->non_client_view()->GetPreferredSize();
-  gfx::Rect bounds(
-      work_area.x() + work_area.width() / 2 - size.width() / 2,
-      work_area.y() + work_area.height() - size.height(),
-      size.width(), size.height());
-  widget->SetBounds(bounds);
-
-#if BUILDFLAG(IS_WIN)
-  SetWindowsAppId(widget);
-#endif
-
-  if (media_ids.empty() ||
-      media_ids.front().type == content::DesktopMediaID::Type::TYPE_SCREEN) {
-    // Focus the notification widget if sharing a screen.
-    widget->Show();
-  } else {
-    // Do not focus the notification widget if sharing a window.
-    widget->ShowInactive();
-  }
-  // This has to be called after Show() to have effect.
-  widget->SetOpacity(kWindowAlphaValue);
-  widget->SetVisibleOnAllWorkspaces(true);
-
-  return 0;
 }
 
 views::ClientView* ScreenCaptureNotificationUIViews::CreateClientView(
     views::Widget* widget) {
   DCHECK(!client_view_);
   client_view_ = new NotificationBarClientView(widget, this);
+  // To prevent client_view_ from dangling, we observe it for deletion.
+  view_observations_.AddObservation(client_view_.get());
   return client_view_;
 }
 
@@ -305,29 +264,116 @@ ScreenCaptureNotificationUIViews::CreateNonClientFrameView(
 }
 
 void ScreenCaptureNotificationUIViews::OnViewBoundsChanged(
-    View* observed_view) {
+    views::View* observed_view) {
+  if (observed_view == client_view_.get()) {
+    return;
+  }
   gfx::Rect client_rect = source_button_->bounds();
   client_rect.Union(stop_button_->bounds());
   client_rect.Union(hide_link_->bounds());
   client_view_->SetClientRect(client_rect);
 }
 
+void ScreenCaptureNotificationUIViews::OnViewIsDeleting(
+    views::View* observed_view) {
+  if (observed_view == client_view_.get()) {
+    client_view_ = nullptr;
+  }
+}
+
 void ScreenCaptureNotificationUIViews::NotifySourceChange() {
-  if (!source_callback_.is_null())
+  if (!source_callback_.is_null()) {
     // CSC is only supported for tab-capture, so setting it to `false` is the
     // correct behavior so long as we don't support cross-surface-type
     // switching.
     source_callback_.Run(content::DesktopMediaID(),
                          /*captured_surface_control_active=*/false);
+  }
 }
 
 void ScreenCaptureNotificationUIViews::NotifyStopped() {
-  if (!stop_callback_.is_null())
+  if (!stop_callback_.is_null()) {
     std::move(stop_callback_).Run();
+  }
+}
+
+BEGIN_METADATA(ScreenCaptureNotificationUIViews)
+END_METADATA
+
+ScreenCaptureNotificationUIImpl::ScreenCaptureNotificationUIImpl(
+    const std::u16string& text,
+    content::WebContents* capturing_web_contents)
+    : text_(text),
+      capturing_web_contents_(capturing_web_contents
+                                  ? capturing_web_contents->GetWeakPtr()
+                                  : nullptr) {}
+
+gfx::NativeViewId ScreenCaptureNotificationUIImpl::OnStarted(
+    base::OnceClosure stop_callback,
+    content::MediaStreamUI::SourceCallback source_callback,
+    const std::vector<content::DesktopMediaID>& media_ids) {
+  if (widget_) {
+    return 0;
+  }
+
+  widget_ = std::make_unique<views::Widget>();
+  auto screen_capture_notification_ui_views =
+      std::make_unique<ScreenCaptureNotificationUIViews>(
+          text_, capturing_web_contents_.get(), std::move(stop_callback),
+          std::move(source_callback));
+
+  views::Widget::InitParams params(
+      views::Widget::InitParams::CLIENT_OWNS_WIDGET,
+      views::Widget::InitParams::TYPE_WINDOW);
+  params.delegate = screen_capture_notification_ui_views.release();
+  params.opacity = views::Widget::InitParams::WindowOpacity::kTranslucent;
+  params.remove_standard_frame = true;
+  params.z_order = ui::ZOrderLevel::kFloatingUIElement;
+  params.name = "ScreenCaptureNotificationUIViews";
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // TODO(sergeyu): The notification bar must be shown on the monitor that's
+  // being captured. Make sure it's always the case. Currently we always capture
+  // the primary monitor.
+  params.context = ash::Shell::GetPrimaryRootWindow();
+#endif
+
+  widget_->set_frame_type(views::Widget::FrameType::kForceCustom);
+  widget_->Init(std::move(params));
+
+  display::Screen* screen = display::Screen::GetScreen();
+  // TODO(sergeyu): Move the notification to the display being captured when
+  // per-display screen capture is supported.
+  gfx::Rect work_area = screen->GetPrimaryDisplay().work_area();
+
+  // Place the bar in the center of the bottom of the display.
+  gfx::Size size = widget_->non_client_view()->GetPreferredSize();
+  gfx::Rect bounds(work_area.x() + work_area.width() / 2 - size.width() / 2,
+                   work_area.y() + work_area.height() - size.height(),
+                   size.width(), size.height());
+  widget_->SetBounds(bounds);
+
+#if BUILDFLAG(IS_WIN)
+  SetWindowsAppId(widget_.get());
+#endif
+
+  if (media_ids.empty() ||
+      media_ids.front().type == content::DesktopMediaID::Type::TYPE_SCREEN) {
+    // Focus the notification widget if sharing a screen.
+    widget_->Show();
+  } else {
+    // Do not focus the notification widget if sharing a window.
+    widget_->ShowInactive();
+  }
+  // This has to be called after Show() to have effect.
+  widget_->SetOpacity(kWindowAlphaValue);
+  widget_->SetVisibleOnAllWorkspaces(true);
+
+  return 0;
 }
 
 #if BUILDFLAG(IS_WIN)
-void ScreenCaptureNotificationUIViews::SetWindowsAppId(views::Widget* widget) {
+void ScreenCaptureNotificationUIImpl::SetWindowsAppId(views::Widget* widget) {
   if (!capturing_web_contents_) {
     return;
   }
@@ -348,15 +394,12 @@ void ScreenCaptureNotificationUIViews::SetWindowsAppId(views::Widget* widget) {
 }
 #endif  // BUILDFLAG(IS_WIN)
 
-BEGIN_METADATA(ScreenCaptureNotificationUIViews)
-END_METADATA
-
 }  // namespace
 
 std::unique_ptr<ScreenCaptureNotificationUI>
 ScreenCaptureNotificationUI::Create(
     const std::u16string& text,
     content::WebContents* capturing_web_contents) {
-  return std::make_unique<ScreenCaptureNotificationUIViews>(
+  return std::make_unique<ScreenCaptureNotificationUIImpl>(
       text, capturing_web_contents);
 }
