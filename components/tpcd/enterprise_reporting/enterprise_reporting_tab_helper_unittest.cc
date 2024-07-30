f7fddc18dbb19eb60d105710d8f54176a6132c8e
Erin Stewart <erinstewart@google.com>
2024-07-30 13:10:33 +0000

    Use mocking in EnterpriseReportingTabHelper unit tests
    The EnterpriseReportingTabHelper unit tests are not correctly implemented, as they do not test whether QueueEnterpriseReport was called. Unit tests now use mocking to resolve this.
    
    go/3pcd-enterprise-reporting (Googlers-only)
    
    Bug: 350966712
    Change-Id: Ibd44640eb32d5999480afd2f6b5d3913aa95a1f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744037
    Commit-Queue: Aaron Selya <selya@google.com>
    Reviewed-by: Aaron Selya <selya@google.com>
    Auto-Submit: Erin Stewart <erinstewart@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334780}

--- components/tpcd/enterprise_reporting/enterprise_reporting_tab_helper_unittest.cc
+++ components/tpcd/enterprise_reporting/enterprise_reporting_tab_helper_unittest.cc
@@ -5,50 +5,36 @@
 #include "components/tpcd/enterprise_reporting/enterprise_reporting_tab_helper.h"
 
 #include "base/test/scoped_feature_list.h"
-#include "base/test/test_future.h"
+#include "base/test/values_test_util.h"
+#include "base/values.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
-#include "content/public/test/test_browser_context.h"
 #include "content/public/test/test_renderer_host.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "net/base/features.h"
 #include "net/cookies/cookie_inclusion_status.h"
 #include "services/network/test/test_network_context.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+namespace tpcd::enterprise_reporting {
 namespace {
 
 class MockNetworkContext : public network::TestNetworkContext {
  public:
-  explicit MockNetworkContext(content::BrowserContext* browser_context) {
-    browser_context->GetDefaultStoragePartition()->SetNetworkContextForTesting(
-        receiver_.BindNewPipeAndPassRemote());
-  }
+  explicit MockNetworkContext(
+      mojo::PendingReceiver<network::mojom::NetworkContext> receiver)
+      : receiver_(this, std::move(receiver)) {}
 
-  MockNetworkContext(const MockNetworkContext&) = delete;
-  MockNetworkContext& operator=(const MockNetworkContext&) = delete;
-
-  ~MockNetworkContext() override = default;
-
-  bool WaitAndGetInvokedQueueEnterpriseReport() {
-    return invoked_queue_enterprise_report_.Get();
-  }
-
-  bool InvokedQueueEnterpriseReportSet() {
-    return invoked_queue_enterprise_report_.IsReady();
-  }
-
-  void QueueEnterpriseReport(const std::string& type,
-                             const std::string& group,
-                             const GURL& url,
-                             base::Value::Dict body) override {
-    invoked_queue_enterprise_report_.SetValue(true);
-  }
+  MOCK_METHOD(
+      void,
+      QueueEnterpriseReport,
+      (const std::string&, const std::string&, const GURL&, base::Value::Dict),
+      (override));
 
  private:
-  mojo::Receiver<network::mojom::NetworkContext> receiver_{this};
-  base::test::TestFuture<bool> invoked_queue_enterprise_report_;
+  mojo::Receiver<network::mojom::NetworkContext> receiver_;
 };
 
 class EnterpriseReportingTabHelperTest
@@ -58,144 +44,177 @@ class EnterpriseReportingTabHelperTest
     scoped_feature_list_.InitAndEnableFeature(
         net::features::kReportingApiEnableEnterpriseCookieIssues);
     content::RenderViewHostTestHarness::SetUp();
+
+    mojo::PendingRemote<network::mojom::NetworkContext> network_context_remote;
+    mock_network_context_ = std::make_unique<MockNetworkContext>(
+        network_context_remote.InitWithNewPipeAndPassReceiver());
+    main_rfh()->GetStoragePartition()->SetNetworkContextForTesting(
+        std::move(network_context_remote));
+
+    cookie_ = *net::CanonicalCookie::CreateForTesting(kUrl_, "A=1",
+                                                      base::Time::Now());
   }
 
+  ~EnterpriseReportingTabHelperTest() override = default;
+
  protected:
+  MockNetworkContext* mock_network_context() {
+    return mock_network_context_.get();
+  }
+
   base::test::ScopedFeatureList scoped_feature_list_;
+  const GURL kUrl_ = GURL("http://www.google.com");
+  net::CanonicalCookie cookie_;
+
+ private:
+  std::unique_ptr<MockNetworkContext> mock_network_context_;
 };
 
 TEST_F(EnterpriseReportingTabHelperTest, ReportingFeatureDisabled) {
+  // QueueEnterpriseReport() shouldn't be called
+  EXPECT_CALL(*mock_network_context(), QueueEnterpriseReport).Times(0);
+
   // Disable the reporting feature.
   scoped_feature_list_.Reset();
   scoped_feature_list_.InitAndDisableFeature(
       net::features::kReportingApiEnableEnterpriseCookieIssues);
 
-  auto network_context =
-      std::make_unique<MockNetworkContext>(browser_context());
-
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper::
       CreateForWebContents(web_contents());
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper* tab_helper =
       tpcd::enterprise_reporting::EnterpriseReportingTabHelper::FromWebContents(
           web_contents());
-  const GURL url("http://www.google.com");
 
-  std::unique_ptr<net::CanonicalCookie> cookie(
-      net::CanonicalCookie::CreateForTesting(url, "A=1", base::Time::Now()));
-
-  // Create a status that would cause a report to be queued if the feature
-  // was enabled, but the report won't be queued since the feature is disabled.
+  // Create a status that would cause a report to be queued if the
+  // feature was enabled, but the report won't be queued since the
+  // feature is disabled.
   net::CookieInclusionStatus status;
   status.AddWarningReason(
       net::CookieInclusionStatus::WARN_THIRD_PARTY_PHASEOUT);
-
   const content::CookieAccessDetails& details = content::CookieAccessDetails(
-      content::CookieAccessDetails::Type::kChange, url, url,
-      {{*cookie, net::CookieAccessResult(status)}}, 1u);
+      content::CookieAccessDetails::Type::kChange, kUrl_, kUrl_,
+      {{cookie_, net::CookieAccessResult(status)}}, 1u);
+
   tab_helper->OnCookiesAccessed(main_rfh(), details);
-  // QueueEnterpriseReport() shouldn't have been called
-  EXPECT_FALSE(network_context->InvokedQueueEnterpriseReportSet());
 }
 
 TEST_F(EnterpriseReportingTabHelperTest,
        OnCookiesAccessedWithExcludeThirdPartyBlockedWithinFirstPartySetError) {
-  auto network_context =
-      std::make_unique<MockNetworkContext>(browser_context());
+  // TODO(crbug.com/352737473): Update group parameter to use endpoint from
+  // subscription.
+  EXPECT_CALL(
+      *mock_network_context(),
+      QueueEnterpriseReport("enterprise-third-party-cookie-access-error",
+                            "enterprise-third-party-cookie-access-error",
+                            GURL(""), base::test::IsJson(R"json({
+            "frameUrl": "",
+             "accessUrl": "http://www.google.com/",
+             "name": "A",
+             "domain": "www.google.com",
+             "path": "/",
+             "accessOperation": "write"
+          })json")));
 
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper::
       CreateForWebContents(web_contents());
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper* tab_helper =
       tpcd::enterprise_reporting::EnterpriseReportingTabHelper::FromWebContents(
           web_contents());
-  const GURL url("http://www.google.com");
-
-  std::unique_ptr<net::CanonicalCookie> cookie(
-      net::CanonicalCookie::CreateForTesting(url, "A=1", base::Time::Now()));
 
   net::CookieInclusionStatus status;
   status.AddExclusionReason(
       net::CookieInclusionStatus::
           EXCLUDE_THIRD_PARTY_BLOCKED_WITHIN_FIRST_PARTY_SET);
-
   const content::CookieAccessDetails& details = content::CookieAccessDetails(
-      content::CookieAccessDetails::Type::kChange, url, url,
-      {{*cookie, net::CookieAccessResult(status)}}, 1u);
+      content::CookieAccessDetails::Type::kChange, kUrl_, kUrl_,
+      {{cookie_, net::CookieAccessResult(status)}}, 1u);
+
   tab_helper->OnCookiesAccessed(main_rfh(), details);
-  EXPECT_TRUE(network_context->WaitAndGetInvokedQueueEnterpriseReport());
 }
 
 TEST_F(EnterpriseReportingTabHelperTest,
        OnCookiesAccessedWithExcludeThirdPartyPhaseoutError) {
-  auto network_context =
-      std::make_unique<MockNetworkContext>(browser_context());
+  // TODO(crbug.com/352737473): Update group parameter to use endpoint from
+  // subscription.
+  EXPECT_CALL(
+      *mock_network_context(),
+      QueueEnterpriseReport("enterprise-third-party-cookie-access-error",
+                            "enterprise-third-party-cookie-access-error",
+                            GURL(""), base::test::IsJson(R"json({
+            "frameUrl": "",
+             "accessUrl": "http://www.google.com/",
+             "name": "A",
+             "domain": "www.google.com",
+             "path": "/",
+             "accessOperation": "write"
+          })json")));
 
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper::
       CreateForWebContents(web_contents());
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper* tab_helper =
       tpcd::enterprise_reporting::EnterpriseReportingTabHelper::FromWebContents(
           web_contents());
-  const GURL url("http://www.google.com");
-
-  std::unique_ptr<net::CanonicalCookie> cookie(
-      net::CanonicalCookie::CreateForTesting(url, "A=1", base::Time::Now()));
 
   net::CookieInclusionStatus status;
   status.AddExclusionReason(
       net::CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT);
-
   const content::CookieAccessDetails& details = content::CookieAccessDetails(
-      content::CookieAccessDetails::Type::kChange, url, url,
-      {{*cookie, net::CookieAccessResult(status)}}, 1u);
+      content::CookieAccessDetails::Type::kChange, kUrl_, kUrl_,
+      {{cookie_, net::CookieAccessResult(status)}}, 1u);
+
   tab_helper->OnCookiesAccessed(main_rfh(), details);
-  EXPECT_TRUE(network_context->WaitAndGetInvokedQueueEnterpriseReport());
 }
 
 TEST_F(EnterpriseReportingTabHelperTest,
        OnCookiesAccessedWithWarnThirdPartyPhaseoutWarning) {
-  auto network_context =
-      std::make_unique<MockNetworkContext>(browser_context());
+  // TODO(crbug.com/352737473): Update group parameter to use endpoint from
+  // subscription.
+  EXPECT_CALL(
+      *mock_network_context(),
+      QueueEnterpriseReport("enterprise-third-party-cookie-access-warning",
+                            "enterprise-third-party-cookie-access-warning",
+                            GURL(""), base::test::IsJson(R"json({
+            "frameUrl": "",
+             "accessUrl": "http://www.google.com/",
+             "name": "A",
+             "domain": "www.google.com",
+             "path": "/",
+             "accessOperation": "read"
+          })json")));
 
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper::
       CreateForWebContents(web_contents());
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper* tab_helper =
       tpcd::enterprise_reporting::EnterpriseReportingTabHelper::FromWebContents(
           web_contents());
-  const GURL url("http://www.google.com");
-
-  std::unique_ptr<net::CanonicalCookie> cookie(
-      net::CanonicalCookie::CreateForTesting(url, "A=1", base::Time::Now()));
 
   net::CookieInclusionStatus status;
   status.AddWarningReason(
       net::CookieInclusionStatus::WARN_THIRD_PARTY_PHASEOUT);
-
   const content::CookieAccessDetails& details = content::CookieAccessDetails(
-      content::CookieAccessDetails::Type::kChange, url, url,
-      {{*cookie, net::CookieAccessResult(status)}}, 1u);
+      content::CookieAccessDetails::Type::kRead, kUrl_, kUrl_,
+      {{cookie_, net::CookieAccessResult(status)}}, 1u);
+
   tab_helper->OnCookiesAccessed(main_rfh(), details);
-  EXPECT_TRUE(network_context->WaitAndGetInvokedQueueEnterpriseReport());
 }
 
 TEST_F(EnterpriseReportingTabHelperTest,
        OnCookiesAccessedWithoutQueueingReport) {
-  auto network_context =
-      std::make_unique<MockNetworkContext>(browser_context());
+  // QueueEnterpriseReport() shouldn't be called
+  EXPECT_CALL(*mock_network_context(), QueueEnterpriseReport).Times(0);
 
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper::
       CreateForWebContents(web_contents());
   tpcd::enterprise_reporting::EnterpriseReportingTabHelper* tab_helper =
       tpcd::enterprise_reporting::EnterpriseReportingTabHelper::FromWebContents(
           web_contents());
-  const GURL url("http://www.google.com");
 
-  std::unique_ptr<net::CanonicalCookie> cookie(
-      net::CanonicalCookie::CreateForTesting(url, "A=1", base::Time::Now()));
+  const content::CookieAccessDetails& details =
+      content::CookieAccessDetails(content::CookieAccessDetails::Type::kChange,
+                                   kUrl_, kUrl_, {{cookie_}}, 1u);
 
-  const content::CookieAccessDetails& details = content::CookieAccessDetails(
-      content::CookieAccessDetails::Type::kChange, url, url, {{*cookie}}, 1u);
   tab_helper->OnCookiesAccessed(main_rfh(), details);
-  // QueueEnterpriseReport() shouldn't have been called
-  EXPECT_FALSE(network_context->InvokedQueueEnterpriseReportSet());
 }
 
 }  // namespace
+}  // namespace tpcd::enterprise_reporting
