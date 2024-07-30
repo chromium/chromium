d69547121a2fca2b60d0864d93778e3277ee9b35
EmiliaPaz <emiliapaz@chromium.org>
2024-07-30 23:54:31 +0000

    [Extensions] Add icons to MV2 deprecation disabled dialog
    Chrome has announced that MV2 extensions will gradually no longer be
    supported in the browser. None of these changes will take effect on any
    Chrome channel before June, 2024. See more information at
    https://developer.chrome.com/blog/resuming-the-transition-to-mv3.
    
    The second stage of the deprecation shows a dialog listing the
    disabled extensions. This CL adds the extension icon(s) to the dialog.
    Since retrieving the icon is an async operation, we have to wait
    for all icons to be retrieved before showing the dialog (and check
    the extensions are still affected, since they could have been
    updated externally).
    
    Screenshots
    - single extension:
    https://drive.google.com/file/d/1J8K1nkUxBf80Hjpa9aWBvHiao008JIDC/view?usp=sharing
    - multiple extensions:
    https://drive.google.com/file/d/1mfiGDq8epQpFGrI12K3BzrWAA5SXOdXJ/view?usp=sharing
    
    Bug: 355218108
    Change-Id: Idd3e47b22692f16f068bf419bab2a2b9169da8a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741763
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335169}

--- chrome/browser/ui/views/extensions/mv2_deprecation_disabled_dialog_browsertest.cc
+++ chrome/browser/ui/views/extensions/mv2_deprecation_disabled_dialog_browsertest.cc
@@ -9,6 +9,7 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/extensions/extensions_dialogs.h"
+#include "chrome/browser/ui/extensions/mv2_disabled_dialog_controller.h"
 #include "chrome/browser/ui/views/extensions/extensions_dialogs_browsertest.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/disable_reason.h"
@@ -34,21 +35,36 @@ class Mv2DeprecationDisabledDialogBrowserTest
   // DialogBrowserTest:
   void ShowUi(const std::string& name) override {
     scoped_refptr<const extensions::Extension> extension_A =
-        AddMV2ExtensionAndDisable(u"Extension A");
+        AddMV2ExtensionAndDisable("Extension A");
+    extensions::Mv2DisabledDialogController::ExtensionInfo extension_info_A;
+    extension_info_A.id = extension_A->id();
+    extension_info_A.name = extension_A->name();
+    extension_info_A.icon = gfx::Image();
+
     scoped_refptr<const extensions::Extension> extension_B =
-        AddMV2ExtensionAndDisable(u"Extension A");
+        AddMV2ExtensionAndDisable("Extension B");
+    extensions::Mv2DisabledDialogController::ExtensionInfo extension_info_B;
+    extension_info_B.id = extension_B->id();
+    extension_info_B.name = extension_B->name();
+    extension_info_B.icon = gfx::Image();
+
+    std::vector<extensions::Mv2DisabledDialogController::ExtensionInfo>
+        extensions_info;
+    extensions_info.push_back(extension_info_A);
+    extensions_info.push_back(extension_info_B);
+
     extensions::ShowMv2DeprecationDisabledDialog(
         browser()->profile(), browser()->window()->GetNativeWindow(),
-        {extension_A->id(), extension_B->id()},
+        extensions_info,
         /*remove_callback=*/base::DoNothing(),
         /*manage_callback=*/base::DoNothing(),
         /*close_callback=*/base::DoNothing());
   }
 
   scoped_refptr<const extensions::Extension> AddMV2ExtensionAndDisable(
-      const std::u16string extension_name) {
+      const std::string extension_name) {
     scoped_refptr<const extensions::Extension> extension =
-        extensions::ExtensionBuilder("MV2 Extension")
+        extensions::ExtensionBuilder(extension_name)
             .SetManifestVersion(2)
             .SetLocation(extensions::mojom::ManifestLocation::kInternal)
             .Build();
