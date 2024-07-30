c1000e10498134c51f2be2fe3418be7f86207b0f
Nathan Memmott <memmott@chromium.org>
2024-07-30 17:08:23 +0000

    Change Observer: Add WPTs for writable file streams
    Adds expectations for all writable file stream methods.
    
    Test failure expectations are added for closed events reporting as
    "appeared" instead of "modified" due to bug 340584120.
    
    Bug: 341136316
    Change-Id: Id01f9ab849437793a6d69b7281aea394c03c76c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743270
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334934}

---
+++ third_party/blink/web_tests/external/wpt/fs/FileSystemObserver-writable-file-stream.https.tentative.any.js
@@ -0,0 +1,4 @@
+// META: script=resources/sandboxed-fs-test-helpers.js
+// META: script=resources/test-helpers.js
+// META: script=resources/collecting-file-system-observer.js
+// META: script=script-tests/FileSystemObserver-writable-file-stream.js
