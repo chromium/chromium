1553afebbba2083722b4c5df1d29bead05eeb41c
HuanPo Lin <robertlin@chromium.org>
2024-07-31 07:09:42 +0000

    gardener: Disable testMoveToBackgroundAndToForeground
    Bug: 356534232
    Change-Id: I1b816ce5af223edaf8dc44a2d94fbdeee5bd0938
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5750223
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Robert Lin <robertlin@google.com>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335278}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_egtest.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_egtest.mm
@@ -61,7 +61,8 @@
 
 // Tests that the search engine choice dialog is always visible when the app
 // goes to background and foreground.
-- (void)testMoveToBackgroundAndToForeground {
+// TODO(crbug.com/356534232): Re-enable after fixing flakiness.
+- (void)DISABLED_testMoveToBackgroundAndToForeground {
   [SearchEngineChoiceEarlGreyUI verifySearchEngineChoiceScreenIsDisplayed];
   [[AppLaunchManager sharedManager] backgroundAndForegroundApp];
   [SearchEngineChoiceEarlGreyUI verifySearchEngineChoiceScreenIsDisplayed];
