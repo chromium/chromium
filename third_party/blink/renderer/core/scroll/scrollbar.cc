9406c556aefde29838e8d3a3211fb2f1a462a18e
Xianzhu Wang <wangxianzhu@chromium.org>
2024-07-31 23:21:45 +0000

    Null check scrollable_area_ in Scrollbar functions
    - if (!scrollable_area_) in Maximum() and UsedColorScheme().
    - CHECK(scrollable_area_) in functions called only from
      PaintLayerScrollableArea or functions that have checked
      scrollable_area_.
    
    Bug: 356423440
    Change-Id: Ia3afb35ced7125a72d83ed99c445069b70015241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5754568
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335757}

--- third_party/blink/renderer/core/scroll/scrollbar.cc
+++ third_party/blink/renderer/core/scroll/scrollbar.cc
@@ -142,6 +142,9 @@ bool Scrollbar::IsLeftSideVerticalScrollbar() const {
 }
 
 int Scrollbar::Maximum() const {
+  if (!scrollable_area_) {
+    return 0;
+  }
   gfx::Vector2d max_offset = scrollable_area_->MaximumScrollOffsetInt() -
                              scrollable_area_->MinimumScrollOffsetInt();
   return orientation_ == kHorizontalScrollbar ? max_offset.x() : max_offset.y();
@@ -705,6 +708,7 @@ void Scrollbar::InjectScrollGesture(WebInputEvent::Type gesture_type,
 }
 
 bool Scrollbar::DeltaWillScroll(ScrollOffset delta) const {
+  CHECK(scrollable_area_);
   ScrollOffset current_offset = scrollable_area_->GetScrollOffset();
   ScrollOffset target_offset = current_offset + delta;
   ScrollOffset clamped_offset =
@@ -770,6 +774,7 @@ bool Scrollbar::UsesNinePatchTrackAndCanSkipRepaint(
 }
 
 bool Scrollbar::ShouldParticipateInHitTesting() {
+  CHECK(scrollable_area_);
   // Non-overlay scrollbars should always participate in hit testing.
   if (!IsOverlayScrollbar())
     return true;
@@ -937,6 +942,9 @@ bool Scrollbar::IsOpaque() const {
 }
 
 mojom::blink::ColorScheme Scrollbar::UsedColorScheme() const {
+  if (!scrollable_area_) {
+    return mojom::blink::ColorScheme::kLight;
+  }
   return IsOverlayScrollbar()
              ? scrollable_area_->GetOverlayScrollbarColorScheme()
              : scrollable_area_->UsedColorSchemeScrollbars();
