c92567081f5627cbcce093c1d0d11e2cced171d3
David Padlipsky <dpad@google.com>
2024-07-31 00:15:44 +0000

    Add support for System Accessibility Binding
    A new HID keycode for a system accessibility binding was added in this
    proposal:
    www.usb.org/sites/default/files/hutrr116-systemaccessbilitybinding_2.pdf
    
    This maps the HID code correctly throughout Chrome.
    
    Bug: b/355696040
    Change-Id: I29b90f740c1e335c20f25f30669710a7fbb7322b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744058
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335179}

--- ui/events/keycodes/dom/keycode_converter_unittest.cc
+++ ui/events/keycodes/dom/keycode_converter_unittest.cc
@@ -29,8 +29,8 @@ namespace {
 // These are in the same order as the columns in dom_code_data.inc
 // as reflected in the DOM_CODE() macro below.
 const size_t expected_mapped_key_count[] = {
-    222,  // evdev
-    222,  // xkb
+    223,  // evdev
+    223,  // xkb
     157,  // windows
     119,  // mac
 };
--- ui/events/keycodes/keyboard_codes_posix.h
+++ ui/events/keycodes/keyboard_codes_posix.h
@@ -260,8 +260,12 @@ enum KeyboardCode : unsigned short {
 #if BUILDFLAG(IS_CHROMEOS)
   VKEY_FUNCTION = 0xFF,
   VKEY_RIGHT_ALT = 0x100,
+#endif
+
+  // System Accessibility Binding.
   VKEY_ACCESSIBILITY = 0x101,
 
+#if BUILDFLAG(IS_CHROMEOS)
   // The following values are used to be able to recognize button events within
   // ChromeOS. They have no functionality by default.
   VKEY_BUTTON_0 = 0xFF00,
