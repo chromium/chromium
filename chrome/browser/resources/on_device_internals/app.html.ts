70321bf7d2e2aa0b8ac2c367620e9a7ef783859c
rajendrant <rajendrant@chromium.org>
2024-08-01 19:36:55 +0000

    Add tabbox to on-device internals page
    Adds a tabbox and moves the current UI to "Tools" tab.
    This will enable adding new tabs to show event logs and state in subsequent CLs.
    
    Mocks:
    http://screen/7FMowYSdyoyB2mF
    http://screen/BxhWUW6QQUEQ8QC
    
    Bug: b:346491542
    Change-Id: I1373f613489c2423928525e2aeaf44dc98bda208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5750886
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336195}

---
+++ chrome/browser/resources/on_device_internals/app.html.ts
@@ -0,0 +1,22 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {OnDeviceInternalsAppElement} from './app.js';
+
+export function getHtml(this: OnDeviceInternalsAppElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
+<h1>On-Device Internals</h1>
+<cr-tab-box id="tabbox" hidden
+    @selected-index-change="${this.onSelectedIndexChange_}">
+  <div slot="tab">Tools</div>
+  <div slot="panel" id="tools">
+    <on-device-internals-tools></on-device-internals-tools>
+  </div>
+</cr-tab-box>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
