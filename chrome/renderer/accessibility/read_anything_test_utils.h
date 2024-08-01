fa38e4dd8c319b0769bcbd094583c6b4d2d58f97
Lauren Winston <lwinston@google.com>
2024-08-01 21:07:00 +0000

    Reland "[Read Anything] Create test utils for read anything tests."
    This is a reland of commit 0abf425e80c9a915256b81087499c0aba0d50598
    
    Original change's description:
    > [Read Anything] Create test utils for read anything tests.
    >
    > Bug: none
    > Change-Id: I8385dd8db82349bb021d953c430c4e1b8596ec8a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753373
    > Reviewed-by: Kristi Saney <kristislee@google.com>
    > Commit-Queue: Lauren Winston <lwinston@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1335741}
    
    Bug: none
    Change-Id: I5770d9cf8549c7f76b3d3c56b508bf654a2aed38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757291
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336257}

---
+++ chrome/renderer/accessibility/read_anything_test_utils.h
@@ -0,0 +1,18 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_RENDERER_ACCESSIBILITY_READ_ANYTHING_TEST_UTILS_H_
+#define CHROME_RENDERER_ACCESSIBILITY_READ_ANYTHING_TEST_UTILS_H_
+
+#include "ui/accessibility/ax_node.h"
+#include "ui/accessibility/ax_node_position.h"
+
+namespace test {
+
+void SetUpdateTreeID(ui::AXTreeUpdate* update, ui::AXTreeID tree_id);
+std::unique_ptr<ui::AXTreeUpdate> CreateInitialUpdate();
+
+}  // namespace test
+
+#endif  // CHROME_RENDERER_ACCESSIBILITY_READ_ANYTHING_TEST_UTILS_H_
