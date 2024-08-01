8db8278e2e92389fe7b1b50129964329165eb155
Claire Chambers <clchambers@microsoft.com>
2024-08-01 17:57:13 +0000

    [CompositeClipPathAnimation] Remove HasCurrentClipPathAnimation
    This CL fixes a CHECK failure that occurred on multiple websites
    resulting from the HasCurrentClipPathAnimation flag being unexpectedly
    unset when kNeedsRepaint was set following an animation being set
    compositor pending. This situation arises when a transition completes, and also in certain other scenarios that are difficult to debug. In these cases, a compositing decision should still be made, however the flag is not set due to logic in
    CSSAnimations::UpdateAnimationFlags. As such, the issue lies with the
    CHECK - its failure in this scenario is not indicative of a code fault.
    This check was added to ensure that kNeedsRepaint wouldn't persist
    beyond the lifetime of an animation, however the effect of delays and
    suppressed animations were not considered.
    
    The HasCurrentClipPathAnimation flag was initially added to avoid heavy logic in HasCompositeClipPathAnimation being run every repaint.
    However, this was recently ameliorated in favor of using the new
    kNoAnimation state for the composited paint status, so this flag is now unused except as a CHECK.
    
    Rather than attempting to align these two values, this CL simply removes HasCompositeClipPathAnimation.
    
    Bug: 355108923
    Change-Id: Iaa57ad84212ac1fdcd8fb231685697cafb41ea91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737832
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Claire Chambers <clchambers@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1336131}

--- third_party/blink/renderer/core/animation/css/css_animations.cc
+++ third_party/blink/renderer/core/animation/css/css_animations.cc
@@ -1872,10 +1872,6 @@ bool AffectsBackgroundColor(const AnimationEffect& effect) {
   return effect.Affects(PropertyHandle(GetCSSPropertyBackgroundColor()));
 }
 
-bool AffectsClipPath(const AnimationEffect& effect) {
-  return effect.Affects(PropertyHandle(GetCSSPropertyClipPath()));
-}
-
 void UpdateAnimationFlagsForEffect(const AnimationEffect& effect,
                                    ComputedStyleBuilder& builder) {
   if (effect.Affects(PropertyHandle(GetCSSPropertyOpacity())))
@@ -1894,9 +1890,6 @@ void UpdateAnimationFlagsForEffect(const AnimationEffect& effect,
     builder.SetHasCurrentBackdropFilterAnimation(true);
   if (AffectsBackgroundColor(effect))
     builder.SetHasCurrentBackgroundColorAnimation(true);
-  if (AffectsClipPath(effect)) {
-    builder.SetHasCurrentClipPathAnimation(true);
-  }
 }
 
 // Called for animations that are newly created or updated.
