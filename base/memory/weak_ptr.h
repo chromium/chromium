ce088ac05439b7d8037287e1d2e3b1445e17c1ea
Dave Tapuska <dtapuska@chromium.org>
2024-08-03 00:25:23 +0000

    [SupportsWeakPtr] Remove unused friend classes
    The classes have been changed so clean up dangling class definitions.
    
    Bug: 40485134
    Change-Id: I9ec210eb99b4c028e4bad9d410b6145825a0f919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5760730
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336875}

--- base/memory/weak_ptr.h
+++ base/memory/weak_ptr.h
@@ -95,19 +95,10 @@ class WorkerNodeImpl;
 // TODO(crbug.com/40485134): This is a hack to prevent new uses of
 // SupportsWeakPtr. The remaining uses are troublesome to fix, so we declare
 // these classes as friends so they have access to the constructor.
-namespace gl {
-class GLContext;
-class GLSurface;
-}  // namespace gl
-
 namespace ui {
 class MenuModel;
 class TextInputClient;
 }  // namespace ui
-
-namespace views {
-class WidgetDelegate;
-}  // namespace views
 
 namespace base {
 
@@ -514,11 +505,8 @@ class SupportsWeakPtr : public internal::SupportsWeakPtrBase {
   friend struct MultiplyDerivedProducer;
   friend struct Producer;
   friend struct weak_ptr_unittest::Target;
-  friend class gl::GLContext;
-  friend class gl::GLSurface;
   friend class ui::MenuModel;
   friend class ui::TextInputClient;
-  friend class views::WidgetDelegate;
 
   // The constructor is private so only the declared friends can construct
   // instances.
