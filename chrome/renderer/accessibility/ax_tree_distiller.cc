8479502b58fbf758df8c90f16ec91c06017b8147
Ramin Halavati <rhalavati@google.com>
2024-08-04 14:16:44 +0000

    Set ScreenAI connection in AxMainNodeAnnotator to autodisconnect on idle.
    The connection ScreenAI service in AxMainNodeAnnotator is reset if it's
    not used for 5 minutes and it will be reconnected if needed again.
    This helps the ScreenAI service release unused resources.
    
    AX-Relnotes: n/a
    Bug: 353718857
    Change-Id: I4a885a898d544be8f0559e6ff5db099815daa4da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741712
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337052}

--- chrome/renderer/accessibility/ax_tree_distiller.cc
+++ chrome/renderer/accessibility/ax_tree_distiller.cc
@@ -27,6 +27,8 @@
 namespace {
 
 // Time after which an idle connection to Screen AI service is disconnected.
+// TODO(b/353718857): Remove this when ScreenAI service is set to auto shut down
+// on idle.
 constexpr base::TimeDelta kScreenAIIdleDisconnectDelay = base::Minutes(5);
 
 // TODO: Consider moving this to AXNodeProperties.
