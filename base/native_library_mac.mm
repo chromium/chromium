af9eb84faa337eb3bd8293ea3f5a7291f2c99bc4
Nico Weber <thakis@chromium.org>
2024-08-05 21:05:02 +0000

    mac: Stop being careful about dlclose()ing libraries
    Back in macOS 10.8, one had to be careful to not unload shared libraries
    that contained Objective-C.
    
    In dyld-421.1, dyld learned to not unload such shared libraries when
    dlclose() called, see:
    
    https://github.com/apple-oss-distributions/dyld/commit/e6bd50b5cafc2
    
    Look for `fRetainForObjC` in src/ImageLoaderMachO.cpp in that commit.
    
    dyld-421.1 shipped in macOS 10.12, released in 2016, see
    https://github.com/apple-oss-distributions/distribution-macOS/tree/bee204afdfe063
    
    (Look for "dyld", see that it's pinned at e6bd50b5cafc2.)
    
    Chrome requires macOS 10.15, so we no longer need to have code for
    this ourselves. This reverts most of
    https://chromiumcodereview.appspot.com/12793004/
    
    No expected behavior change.
    
    Bug: 40299954,351867703
    Change-Id: Ieb1364b36104daadf85b91d6c2e935fe48c69bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758316
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337480}

--- base/native_library_mac.mm
+++ base/native_library_mac.mm
@@ -21,30 +21,6 @@
 
 namespace base {
 
-static NativeLibraryObjCStatus GetObjCStatusForImage(
-    const void* function_pointer) {
-  Dl_info info;
-  if (!dladdr(function_pointer, &info))
-    return OBJC_UNKNOWN;
-
-  // See if the image contains an "ObjC image info" segment. This method
-  // of testing is used in _CFBundleGrokObjcImageInfoFromFile in
-  // CF-1153.18/CFBundle_Grok.c, around line 349.
-  //
-  // In 64-bit images, ObjC can be recognized in __DATA,__objc_imageinfo.
-  const auto* header =
-      reinterpret_cast<const struct mach_header_64*>(info.dli_fbase);
-  unsigned long size = 0;
-  getsectiondata(header, SEG_DATA, "__objc_imageinfo", &size);
-  if (size > 0) {
-    return OBJC_PRESENT;
-  }
-  // ....except when "SharedRegionEncodingV2" is on, it's in
-  // __DATA_CONST,__objc_image_info (see https://crbug.com/1220459#c16)
-  getsectiondata(header, "__DATA_CONST", "__objc_imageinfo", &size);
-  return size > 0 ? OBJC_PRESENT : OBJC_NOT_PRESENT;
-}
-
 std::string NativeLibraryLoadError::ToString() const {
   return message;
 }
@@ -63,7 +39,6 @@ NativeLibrary LoadNativeLibraryWithOptions(const FilePath& library_path,
     NativeLibrary native_lib = new NativeLibraryStruct();
     native_lib->type = DYNAMIC_LIB;
     native_lib->dylib = dylib;
-    native_lib->objc_status = OBJC_UNKNOWN;
     return native_lib;
   }
   apple::ScopedCFTypeRef<CFURLRef> url(CFURLCreateFromFileSystemRepresentation(
@@ -78,47 +53,29 @@ NativeLibrary LoadNativeLibraryWithOptions(const FilePath& library_path,
   NativeLibrary native_lib = new NativeLibraryStruct();
   native_lib->type = BUNDLE;
   native_lib->bundle = bundle;
-  native_lib->objc_status = OBJC_UNKNOWN;
   return native_lib;
 }
 
 void UnloadNativeLibrary(NativeLibrary library) {
-  if (library->objc_status == OBJC_NOT_PRESENT) {
-    if (library->type == BUNDLE) {
-      CFRelease(library->bundle);
-    } else {
-      dlclose(library->dylib);
-    }
+  if (library->type == BUNDLE) {
+    CFRelease(library->bundle);
   } else {
-    VLOG(2) << "Not unloading NativeLibrary because it may contain an ObjC "
-               "segment. library->objc_status = " << library->objc_status;
-    // Deliberately do not CFRelease the bundle or dlclose the dylib because
-    // doing so can corrupt the ObjC runtime method caches. See
-    // http://crbug.com/172319 for details.
+    dlclose(library->dylib);
   }
   delete library;
 }
 
 void* GetFunctionPointerFromNativeLibrary(NativeLibrary library,
                                           const char* name) {
-  void* function_pointer = nullptr;
-
   // Get the function pointer using the right API for the type.
   if (library->type == BUNDLE) {
     apple::ScopedCFTypeRef<CFStringRef> symbol_name =
         SysUTF8ToCFStringRef(name);
-    function_pointer =
-        CFBundleGetFunctionPointerForName(library->bundle, symbol_name.get());
-  } else {
-    function_pointer = dlsym(library->dylib, name);
+    return CFBundleGetFunctionPointerForName(library->bundle,
+                                             symbol_name.get());
   }
 
-  // If this library hasn't been tested for having ObjC, use the function
-  // pointer to look up the section information for the library.
-  if (function_pointer && library->objc_status == OBJC_UNKNOWN)
-    library->objc_status = GetObjCStatusForImage(function_pointer);
-
-  return function_pointer;
+  return dlsym(library->dylib, name);
 }
 
 std::string GetNativeLibraryName(std::string_view name) {
