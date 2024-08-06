55e6c7319e28843f17e6cde4ea00d244a8bd40df
Marijn Kruisselbrink <mek@chromium.org>
2024-08-06 17:31:58 +0000

    Revert "controlledframe: Re-enable Kiosk tests"
    This reverts commit 20c0fd2538908e055e5702440515bdd96fcf45ca.
    
    Reason for revert: Tests are still failing in https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/37160/overview
    
    Original change's description:
    > controlledframe: Re-enable Kiosk tests
    >
    > Kiosk availability has been fixed with a previous commit and these
    > tests now reliably pass. Re-enable them.
    >
    > Bug: b/354264993, b/40191772, b/355290700, b/355529251
    > Change-Id: I6bcb49e8b7ae927318f16805b03be2a8bec8032f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5754823
    > Reviewed-by: Sergii Bykov <sbykov@google.com>
    > Auto-Submit: Chase Phillips <cmp@chromium.org>
    > Commit-Queue: Sergii Bykov <sbykov@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1337745}
    
    Bug: b/354264993, b/40191772, b/355290700, b/355529251
    Change-Id: Id8e6b44a03f6978000af439a19661877fe21cdb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766134
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Marijn Kruisselbrink <mek@chromium.org>
    Owners-Override: Marijn Kruisselbrink <mek@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1337966}

--- chrome/browser/ash/login/app_mode/test/web_kiosk_controlled_frame_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/web_kiosk_controlled_frame_browsertest.cc
@@ -148,7 +148,8 @@ class WebKioskControlledFrameHttpTest
             /*https=*/GetParam()) {}
 };
 
-IN_PROC_BROWSER_TEST_P(WebKioskControlledFrameHttpTest, ApiAvailability) {
+IN_PROC_BROWSER_TEST_P(WebKioskControlledFrameHttpTest,
+                       DISABLED_ApiAvailability) {
   content::WebContents* web_contents = TestSetup();
   ASSERT_NE(web_contents, nullptr);
 
@@ -160,6 +161,7 @@ IN_PROC_BROWSER_TEST_P(WebKioskControlledFrameHttpTest, ApiAvailability) {
   bool is_api_available = CreateControlledFrame(
       web_contents->GetPrimaryMainFrame(), web_app_server().base_url());
   if (feature_enabled_ && UseHttpsUrl()) {
+    // TODO: crbug.com/355529251 - Fix expectation.
     EXPECT_TRUE(is_api_available);
   } else {
     EXPECT_FALSE(is_api_available);
@@ -180,7 +182,9 @@ class WebKioskControlledFrameChannelTest
             /*https=*/true) {}
 };
 
-IN_PROC_BROWSER_TEST_P(WebKioskControlledFrameChannelTest, ApiAvailability) {
+// TODO(crbug.com/355290700): Re-enable this test
+IN_PROC_BROWSER_TEST_P(WebKioskControlledFrameChannelTest,
+                       DISABLED_ApiAvailability) {
   content::WebContents* web_contents = TestSetup();
   ASSERT_NE(web_contents, nullptr);
 
