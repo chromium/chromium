94d66463c41bf040c3b163fe287b88b49a1cf535
Sylvain Defresne <sdefresne@chromium.org>
2024-08-06 11:18:58 +0000

    [ios] Stop complaining if "NewTabPageLocation" policy is unset
    Other policy handler do not complain if the policy is not found in
    the PolicyMap, so change NewTabPageLocationPolicyHandler to return
    with success if the "NewTabPageLocation" policy is unset.
    
    Removes the following log spam on each application start:
    
        [45946:259:WARNING:configuration_policy_pref_store.cc(31)]
        Policy NewTabPageLocation: Invalid URL.
    
    Fixed: 357780476
    Change-Id: I53e16f859b6efc2879b84c93a4b4ff2ef894214d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5764424
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337778}

--- ios/chrome/browser/policy/model/new_tab_page_location_policy_handler.cc
+++ ios/chrome/browser/policy/model/new_tab_page_location_policy_handler.cc
@@ -58,6 +58,9 @@ bool NewTabPageLocationPolicyHandler::CheckPolicySettings(
   // `GetValueUnsafe` is used to differentiate between the policy value being
   // unset vs being set with an incorrect type.
   const base::Value* value = policies.GetValueUnsafe(policy_name());
+  if (!value) {
+    return true;
+  }
   if (NewTabPageLocationPolicyHandler::ValidateNewTabPageLocationURL(value)) {
     return true;
   }
