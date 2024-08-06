ff29eb6516a6e11e410ce83b5ecf5eb06452f492
Ivana Žužić <izuzic@google.com>
2024-08-06 09:59:59 +0000

    [UPMAccessLossWarning] Trigger the access loss warning after filling
    This CL introduces the bridge to trigger showing the access loss warning
    from cpp and uses it to trigger showing the sheet after filling
    credentials from the touch to fill sheet.
    
    The decision to show or not to show the sheet will be common for all the
    trigger points and it will be defined in a follow-up CL, for now
    the only criteria is the feature flag.
    
    The decision about the content on the sheet will be done on the Java
    side as a follow-up.
    
    Bug: 356627466
    Change-Id: I13663966d7570c4d074205064fa602e293e22b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752350
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337759}

---
+++ chrome/browser/password_manager/android/access_loss/mock_password_access_loss_warning_bridge.cc
@@ -0,0 +1,10 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/password_manager/android/access_loss/mock_password_access_loss_warning_bridge.h"
+
+MockPasswordAccessLossWarningBridge::MockPasswordAccessLossWarningBridge() =
+    default;
+MockPasswordAccessLossWarningBridge::~MockPasswordAccessLossWarningBridge() =
+    default;
