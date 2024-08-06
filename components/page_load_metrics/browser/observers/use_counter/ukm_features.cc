a3b4484b362855f7c8d52e52bae407fb3cd35937
Ruth Itua <itua@google.com>
2024-08-06 20:21:50 +0000

    Protected Audience: UKM opt-in for kV8Navigator_JoinAdInterestGroup_Method
    New UKM collection review: https://docs.google.com/document/d/13SVkosr1l3iN2cCf1k48GwqQUxgsR1LnMhxnAKSafsI/edit?tab=t.0
    
    Bug: 357620897
    Change-Id: I0cda26bc734f82ce73886f2aafb2d532f130dec2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5761880
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Commit-Queue: Ruth Itua <itua@google.com>
    Reviewed-by: Orr Bernstein <orrb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338065}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -406,6 +406,7 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kIdentityDigitalCredentialsDeepLink,
           WebFeature::kV8FileSystemObserver_Observe_Method,
           WebFeature::kGamepadHapticActuatorType,
+          WebFeature::kV8Navigator_JoinAdInterestGroup_Method,
       }));
   return *opt_in_features;
 }
