2061181a4b93cd2e4e2f4ce0601c77bf9c8ac145
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-08-06 11:45:44 +0000

    spanification tool: preliminary cleanup.
    Minor modifications:
    - Add some comments and refactoring that improve my own understanding of
      the script.
    - Remove the parts about "==== EDITS BEGIN ====", because it isn't used
      and I would like to ensure there no change appended, before exploring
      the tree.
    - Add TODO about failed expectations.
    - Run formatting, following Google coding style (4 spaces indentations).
    
    Bug: 356643982
    Change-Id: Ic306c730fa80c102773bc56f7e259a0f2d7fbc9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757573
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Ali Hijazi <ahijazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337787}

--- tools/clang/spanify/extract_edits.py
+++ tools/clang/spanify/extract_edits.py
@@ -39,7 +39,7 @@ extract_edits.py would then emit the following output:
     <edit2>
     <edit3>
     ...
-Where the edit is either a replacemnt or an include directive.
+Where the edit is either a replacement or an include directive.
 
 For more details about how the tool works, see the doc here:
 https://docs.google.com/document/d/1hUPe21CDdbT6_YFHl03KWlcZqhNIPBAfC-5N5DDY2OE/
@@ -47,210 +47,237 @@ https://docs.google.com/document/d/1hUPe21CDdbT6_YFHl03KWlcZqhNIPBAfC-5N5DDY2OE/
 
 import sys
 
-
 class Node:
-  is_buffer = '0'
-  replacement = ''
-  include_directive = ''
-  size_info_available = '0'
-  # We need to also rewrite deref expressions of the form:
-  # |*buf = something;| into |buf[0] = something;|
-  # for that, create a link between buf and the deref expression.
-  is_deref_node = '0'
-  is_data_change = '0'
+    is_buffer = '0'
+    replacement = ''
+    include_directive = ''
+    size_info_available = '0'
+    # We need to also rewrite deref expressions of the form:
+    # |*buf = something;| into |buf[0] = something;|
+    # for that, create a link between buf and the deref expression.
+    is_deref_node = '0'
+    is_data_change = '0'
 
-  def __init__(self, is_buffer, replacement, include_directive,
-               size_info_available, is_deref_node, is_data_change) -> None:
-    self.is_buffer = is_buffer
-    self.replacement = replacement
-    self.include_directive = include_directive
-    self.size_info_available = size_info_available
-    self.is_deref_node = is_deref_node
-    self.is_data_change = is_data_change
+    def __init__(self, is_buffer, replacement, include_directive,
+                 size_info_available, is_deref_node, is_data_change) -> None:
+        self.is_buffer = is_buffer
+        self.replacement = replacement
+        self.include_directive = include_directive
+        self.size_info_available = size_info_available
+        self.is_deref_node = is_deref_node
+        self.is_data_change = is_data_change
 
-  def __eq__(self, other):
-    if isinstance(other, Node):
-      return self.replacement == other.replacement
-    return False
+    def __eq__(self, other):
+        if isinstance(other, Node):
+            return self.replacement == other.replacement
+        return False
 
-  def __hash__(self) -> int:
-    return hash((self.replacement, self.include_directive))
-
+    def __hash__(self) -> int:
+        return hash((self.replacement, self.include_directive))
 
 # Function to parse the string representation of a node and create a Node
 # object.
-def GetNode(txt: str):
-  # Skipping the first and last character that correspond to the curly braces
-  # denoting the start and end of a serialized node.
-  x = txt[1:-1].split('\\,')
-  # Expect exactly 6 elements that correspond to the following node attributes:
-  # is_buffer, replacement, include_directive, size_info_available,
-  # is_deref_node, is_data_change
-  assert len(x) == 6
-  return Node(*x)
+def ParseNode(txt: str):
+    # Skipping the first and last character that correspond to the curly braces
+    # denoting the start and end of a serialized node.
+    x = txt[1:-1].split('\\,')
+    # Expect exactly 6 elements that correspond to the following node
+    # attributes:
+    # - is_buffer
+    # - replacement
+    # - include_directive
+    # - size_info_available
+    # - is_deref_node
+    # - is_data_change
+    assert len(x) == 6
+    return Node(*x)
 
 
 def DFS(visited: set, graph: dict, key: str, key_to_node: dict, changes: set):
-  """
-  Explore the graph in depth-first search from the given key. Identify edits to
-  apply.
+    """
+    Explore the graph in depth-first search from the given key. Identify edits
+    to apply.
 
-  Args:
-      visited: A set to track visited nodes. This avoids processing the same
-               node multiple times when there are cycles.
-      graph: The graph representing the dependencies between nodes.
-      key: The current key being processed.
-      key_to_node: A mapping of keys to nodes.
-      changes: A set to collect the identified changes.
-  """
-  if key not in visited:
+    Args:
+        visited: A set to track visited nodes. This avoids processing the same
+                 node multiple times when there are cycles.
+        graph: The graph representing the dependencies between nodes.
+        key: The current key being processed.
+        key_to_node: A mapping of keys to nodes.
+        changes: A set to collect the identified changes.
+    """
+    # Only visit nodes once:
+    if key in visited:
+        return
+    visited.add(key)
+
     node = key_to_node[key]
     if not node.replacement.endswith('<empty>'):
-      changes.add(node.replacement)
-      changes.add(node.include_directive)
-    visited.add(key)
+        changes.add(node.replacement)
+        changes.add(node.include_directive)
+
     for neighbour in graph[key]:
-      DFS(visited, graph, neighbour.replacement, key_to_node, changes)
+        DFS(visited, graph, neighbour.replacement, key_to_node, changes)
 
 
 def SizeInfoAvailable(visited: dict, graph: dict, key: str, key_to_node: dict):
-  """
-  Determines whether size information is available for a buffer node and its
-  neighbors. Updates the node's size_info_available attribute.
+    """
+    Determines whether size information is available for a buffer node and its
+    neighbors. Updates the node's size_info_available attribute.
 
-  Args:
-      visited: Property of Nodes(None, 'visiting', or 'visited').
-      graph: The adjacency graph.
-      key: The current node's key being processed.
-      key_to_node: A key to Node mapping.
+    Args:
+        visited: Property of Nodes(None, 'visiting', or 'visited').
+        graph: The adjacency graph.
+        key: The current node's key being processed.
+        key_to_node: A key to Node mapping.
 
-  Returns:
-      None if a cycle is detected,
-      '1' if size information is available for the node and its neighbors,
-      '0' otherwise,
-  """
-  n = key_to_node[key]
-  # If we reached a node that contains size info, just return that.
-  if n.size_info_available == '1':
-    return '1'
-  # Cycle detection: If the node is currently being visited, there's a cycle.
-  # We can't determine the size info for this node.
-  if visited.get(key) == 'visiting':
-    return None
+    Returns:
+        None if a cycle is detected,
+        '1' if size information is available for the node and its neighbors,
+        '0' otherwise,
+    """
+    n = key_to_node[key]
 
-  # Memoization: If the node has already been visited, return the result
-  # immediately.
-  if visited.get(key) == 'visited':
-    return n.size_info_available
+    # If we reached a node that contains size info, just return that.
+    if n.size_info_available == '1':
+        return '1'
 
-  visited[key] = 'visiting'
-  size_info_available = '0'
-  # Check neighbors. If any neighbor doesn't have size info or there's a
-  # cycle, the current node also doesn't.
-  for neighbour in graph[key]:
-    # Break as soon as we encounter a neighbor for which size info is not
-    # available.
-    if SizeInfoAvailable(visited, graph, neighbour.replacement,
-                         key_to_node) == '0':
-      size_info_available = '0'
-      break
-    size_info_available = '1'
-  n.size_info_available = size_info_available
-  visited[key] = 'visited'
-  key_to_node[key] = n
-  return size_info_available
+    # Cycle detection: If the node is currently being visited, there's a cycle.
+    # We can't determine the size info for this node.
+    if visited.get(key) == 'visiting':
+        return None
+
+    # Memoization: If the node has already been visited, return the result
+    # immediately.
+    if visited.get(key) == 'visited':
+        return n.size_info_available
+
+    visited[key] = 'visiting'
+    size_info_available = '0'
+    # Check neighbors. If any neighbor doesn't have size info or there's a
+    # cycle, the current node also doesn't.
+    for neighbour in graph[key]:
+        # Break as soon as we encounter a neighbor for which size info is not
+        # available.
+        if SizeInfoAvailable(visited, graph, neighbour.replacement,
+                             key_to_node) == '0':
+            size_info_available = '0'
+            break
+        size_info_available = '1'
+
+    n.size_info_available = size_info_available
+    visited[key] = 'visited'
+    key_to_node[key] = n
+    return size_info_available
 
 
 def main():
-  graph = dict()
-  key_to_node = dict()  # Since we cannot use nodes as dict keys, use this to
-  # map a key (the replacement directive) to a node.
+    # Since we cannot use nodes as dict keys, use this to map a key (the
+    # replacement directive) to a node.
+    key_to_node = dict()
+    graph = dict()
 
-  inside_marker_lines = False
-  changes = set()
-  for line in sys.stdin:
-    line = line.rstrip('\n\r')
-    if line == '==== BEGIN EDITS ====':
-      inside_marker_lines = True
-      continue
-    if line == '==== END EDITS ====':
-      inside_marker_lines = False
-      continue
-    if inside_marker_lines:
-      changes.add(line)
-      continue
+    # Collect from every compile units the nodes and edges of the graph:
+    for line in sys.stdin:
+        line = line.rstrip('\n\r')
+        nodes = line.split(';')
 
-    nodes = line.split(';')
-    # These are buffer usage nodes
-    if len(nodes) == 1:
-      lhs = GetNode(nodes[0])
-      key_to_node[lhs.replacement] = lhs
-      if lhs.replacement not in graph:
-        graph[lhs.replacement] = set()
-      continue
+        # Parse buffer usage nodes:
+        if len(nodes) == 1:
+            lhs = ParseNode(nodes[0])
+            key_to_node[lhs.replacement] = lhs
+            if lhs.replacement not in graph:
+                graph[lhs.replacement] = set()
+            continue
 
-    lhs = GetNode(nodes[0])
-    rhs = GetNode(nodes[1])
+        # Else, parse the edge between two nodes:
+        assert len(nodes) == 2
+        lhs = ParseNode(nodes[0])
+        rhs = ParseNode(nodes[1])
 
-    # We might have seen this node before determining it's a buffer that needs
-    # to be rewritten. Make sure this info is properly stored and not
-    # overwritten.
-    for node in (lhs, rhs):
-      if node.replacement in key_to_node:
-        node.is_buffer = '1' if node.is_buffer == '1' or key_to_node[
-            node.replacement].is_buffer == '1' else '0'
-      key_to_node[node.replacement] = node
-      if node.replacement not in graph:
-        graph[node.replacement] = set()
+        # We might have seen this node before determining it's a buffer that
+        # needs to be rewritten. Make sure this info is properly stored and not
+        # overwritten.
+        for node in (lhs, rhs):
+            if node.is_buffer == '0' and \
+                    node.replacement in key_to_node and \
+                    key_to_node[node.replacement].is_buffer == '1':
+                node.is_buffer = '1'
 
-    graph[lhs.replacement].add(rhs)
+            key_to_node[node.replacement] = node
+            if node.replacement not in graph:
+                graph[node.replacement] = set()
 
-  for key in graph:
-    node = key_to_node[key]
-    visited = dict()
-    if node.is_buffer == '1':
-      SizeInfoAvailable(visited, graph, key, key_to_node)
+        graph[lhs.replacement].add(rhs)
 
-  visited = set()
-  for key in graph:
-    node = key_to_node[key]
-    if (node.is_buffer == '1' and key not in visited
-        and node.size_info_available == '1'):
-      DFS(visited, graph, key, key_to_node, changes)
+    # Determine whether size information is available for each buffer node:
+    for key in graph:
+        node = key_to_node[key]
+        visited = dict()
 
-  # Iterate over the deref_nodes and then check if their only neighbor was
-  # visited. Visited nodes here are nodes who's type was rewritten to span.
-  # In that case, the deref expression needs to be adapted(rewritten)
-  for key in graph:
-    node = key_to_node[key]
-    if node.is_deref_node == '1':
-      neighbor = list(graph[key])[0]
-      if neighbor.replacement in visited:
-        changes.add(node.replacement)
+        if node.is_buffer == '1':
+            SizeInfoAvailable(visited, graph, key, key_to_node)
 
-  for key in graph:
-    node = key_to_node[key]
-    if node.is_data_change != '1':
-      continue
-    # A data change needs to be added if lhs was not rewritten and rhs was.
-    # The lhs key is stored in the data_change_node's include_directive.
-    # Check if the lhs key is in the set of visited nodes (i.e lhs was
-    # rewritten). If lhs was rewritten, we don't need to add `.data()`
-    if node.include_directive in visited:
-      continue
+    # Collect the changes to apply. We start from buffer nodes whose size
+    # info is available and explore the graph in depth-first search.
+    changes = set()
+    visited = set()
+    for key in graph:
+        node = key_to_node[key]
 
-    neighbor = list(graph[key])[0]  # expect to only have 1 neighbor
-    # If the rhs node was visited (i.e rewritten), then we need to apply the
-    # data change.
-    if neighbor.replacement in visited:
-      # In this case, rhs was rewritten, and lhs was not, we need to add
-      # the corresponding `.data()`
-      changes.add(node.replacement)
+        # We only want to rewrite components connected to a buffer node.
+        if node.is_buffer != '1':
+            continue
 
-  for text in changes:
-    print(text)
-  return 0
+        # Some buffers might not have their size info available. We can't
+        # rewrite those.
+        if node.size_info_available != '1':
+            continue
 
+        DFS(visited, graph, key, key_to_node, changes)
+
+    # Iterate over the deref_nodes and then check if their only neighbor was
+    # visited. Visited nodes here are nodes who's type was rewritten to span.
+    # In that case, the deref expression needs to be adapted(rewritten)
+    for key in graph:
+        node = key_to_node[key]
+
+        if node.is_deref_node == '1':
+            neighbor = list(graph[key])[0]
+            if neighbor.replacement in visited:
+                changes.add(node.replacement)
+
+    # At the edge in between rewritten and non-rewritten nodes, we need
+    # to add a call to `.data()` to access the pointer from the span:
+    for key in graph:
+        node = key_to_node[key]
+
+        if node.is_data_change != '1':
+            continue
+
+        # A data change needs to be added if lhs was not rewritten and rhs was.
+        # The lhs key is stored in the data_change_node's include_directive.
+        # Check if the lhs key is in the set of visited nodes (i.e lhs was
+        # rewritten). If lhs was rewritten, we don't need to add `.data()`
+        if node.include_directive in visited:
+            continue
+
+        # Expect a single neighbor.
+        # TODO(357433195): In practice, this is not always the case. Investigate
+        # why and add the assertion back:
+        # assert (len(graph[key]) == 1)
+        neighbor = list(graph[key])[0]
+
+        # If the rhs node was visited (i.e rewritten), then we need to apply the
+        # data change.
+        if neighbor.replacement in visited:
+            # In this case, rhs was rewritten, and lhs was not, we need to add
+            # the corresponding `.data()`
+            changes.add(node.replacement)
+
+    for text in changes:
+        print(text)
+    return 0
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
