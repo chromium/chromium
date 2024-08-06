c8a542706d72453042eb690f9e02feeab7053528
Gang Wu <gangwu@chromium.org>
2024-08-06 23:57:30 +0000

    Only donate recent 7 days tabs
    According to the privacy review, we need to update this to only donate
    recent 7 days tabs.
    
    Bug: b/357437231
    Change-Id: I2f7277d3b0c4c6820153e40df4141426889f8170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5761180
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338186}

--- chrome/android/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchProviderTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchProviderTest.java
@@ -6,6 +6,8 @@ package org.chromium.chrome.browser.auxiliary_search;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
@@ -25,6 +27,8 @@ import org.mockito.junit.MockitoRule;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
+import org.chromium.base.FeatureList;
+import org.chromium.base.FeatureList.TestValues;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.JniMocker;
@@ -41,6 +45,7 @@ import org.chromium.url.GURL;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 
 /** Unit tests for {@link AuxiliarySearchProvider} */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -62,12 +67,49 @@ public class AuxiliarySearchProviderTest {
     private @Mock TabModelSelector mTabModelSelector;
 
     private AuxiliarySearchProvider mAuxiliarySearchProvider;
+    private MockTabModel mMockNormalTabModel;
 
     @Before
     public void setUp() {
         mJniMocker.mock(AuxiliarySearchBridgeJni.TEST_HOOKS, mMockAuxiliarySearchBridgeJni);
         doReturn(FAKE_NATIVE_PROVIDER).when(mMockAuxiliarySearchBridgeJni).getForProfile(mProfile);
         mAuxiliarySearchProvider = new AuxiliarySearchProvider(mProfile, mTabModelSelector);
+        mMockNormalTabModel = new MockTabModel(mProfile, null);
+        doReturn(mMockNormalTabModel).when(mTabModelSelector).getModel(false);
+    }
+
+    private Tab createTab(int index, long timestamp) {
+        MockTab tab = mMockNormalTabModel.addTab(index);
+        tab.setGurlOverrideForTesting(new GURL(TAB_URL + Integer.toString(index)));
+        tab.setTitle(TAB_TITLE + Integer.toString(index));
+        tab.setTimestampMillis(timestamp);
+
+        return tab;
+    }
+
+    private void compareTabs(List<Tab> expectedTabs, List<Tab> returnedTabs) {
+        HashSet<Integer> returnedTabsNumbers = new HashSet<Integer>();
+        for (Tab returnedTab : returnedTabs) {
+            int returnedNumber =
+                    Integer.valueOf(returnedTab.getUrl().getSpec().substring(TAB_URL.length()));
+
+            Tab expectedTab = null;
+            for (Tab tab : expectedTabs) {
+                int expectedNumber =
+                        Integer.valueOf(tab.getUrl().getSpec().substring(TAB_URL.length()));
+                if (expectedNumber == returnedNumber) {
+                    expectedTab = tab;
+                    break;
+                }
+            }
+            assertNotNull(expectedTab);
+            assertEquals(returnedTab.getTitle(), expectedTab.getTitle());
+            assertEquals(returnedTab.getTitle(), expectedTab.getTitle());
+            assertEquals(returnedTab.getTimestampMillis(), expectedTab.getTimestampMillis());
+
+            returnedTabsNumbers.add(returnedNumber);
+        }
+        assertEquals(expectedTabs.size(), returnedTabsNumbers.size());
     }
 
     @Test
@@ -112,15 +154,15 @@ public class AuxiliarySearchProviderTest {
     @Test
     @SmallTest
     public void testGetTabsSearchableDataProtoAsync() {
-        MockTabModel mockTabModel = new MockTabModel(mProfile, null);
         ArrayList<Tab> tabList = new ArrayList<>();
+        long now = System.currentTimeMillis();
         // Create 200 tabs with different timestamps(from 0 to 199), and only the newest 100 tabs
         // should be returned from 'getTabsSearchableDataProtoAsync'.
         for (int i = 0; i < 200; i++) {
-            MockTab tab = mockTabModel.addTab(i);
+            MockTab tab = mMockNormalTabModel.addTab(i);
             tab.setGurlOverrideForTesting(new GURL(TAB_URL + Integer.toString(i)));
             tab.setTitle(TAB_TITLE + Integer.toString(i));
-            tab.setTimestampMillis(i);
+            tab.setTimestampMillis(now + i);
             if (i >= 100) {
                 tabList.add(tab);
             }
@@ -128,7 +170,6 @@ public class AuxiliarySearchProviderTest {
 
         Object[] tabObject = new Object[tabList.size()];
         tabList.toArray(tabObject);
-        doReturn(mockTabModel).when(mTabModelSelector).getModel(false);
         doAnswer(
                         invocation -> {
                             invocation.<Callback<Object[]>>getArgument(2).onResult(tabObject);
@@ -155,7 +196,7 @@ public class AuxiliarySearchProviderTest {
                             assertTrue(
                                     "Only the newest 100 tabs should be received",
                                     number >= 100 && number <= 199);
-                            assertEquals(number, (int) tab.getLastAccessTimestamp());
+                            assertEquals(now + (long) number, tab.getLastAccessTimestamp());
                             returnedTabsNumbers.add(number);
                         }
                         assertEquals(returnedTabsNumbers.size(), 100);
@@ -165,7 +206,63 @@ public class AuxiliarySearchProviderTest {
 
     @Test
     @SmallTest
+    public void testGetTabsByMinimalAccessTime() {
+        long now = System.currentTimeMillis();
+        List<Tab> tabList =
+                List.of(
+                        createTab(1, now - 50),
+                        createTab(2, now - 100),
+                        createTab(3, now - 150),
+                        createTab(4, now - 200),
+                        createTab(5, now - 250));
+
+        var tabs = mAuxiliarySearchProvider.getTabsByMinimalAccessTime(now - 150);
+        assertEquals(3, tabs.size());
+        compareTabs(tabList.subList(0, 3), tabs);
+
+        tabs = mAuxiliarySearchProvider.getTabsByMinimalAccessTime(now);
+        assertEquals(0, tabs.size());
+
+        tabs = mAuxiliarySearchProvider.getTabsByMinimalAccessTime(0);
+        assertEquals(5, tabs.size());
+        compareTabs(tabList, tabs);
+    }
+
+    @Test
+    @SmallTest
     public void testTabToAuxiliarySearchEntry_nullTab() {
         assertNull(AuxiliarySearchProvider.tabToAuxiliarySearchEntry(null));
+    }
+
+    @Test
+    @SmallTest
+    public void configuredTabsAgeCannotBeZero() {
+        var config = new TestValues();
+        config.addFieldTrialParamOverride(
+                ChromeFeatureList.ANDROID_APP_INTEGRATION,
+                AuxiliarySearchProvider.TAB_AGE_HOURS_PARAM,
+                "0");
+        FeatureList.setTestValues(config);
+        // Recreate provider to update the finch parameter.
+        mAuxiliarySearchProvider = new AuxiliarySearchProvider(mProfile, mTabModelSelector);
+
+        assertNotEquals(0L, mAuxiliarySearchProvider.getTabsMaxAgeMs());
+        assertEquals(
+                AuxiliarySearchProvider.DEFAULT_TAB_AGE_HOURS * 60 * 60 * 1000,
+                mAuxiliarySearchProvider.getTabsMaxAgeMs());
+    }
+
+    @Test
+    @SmallTest
+    public void configuredTabsAge() {
+        var config = new TestValues();
+        config.addFieldTrialParamOverride(
+                ChromeFeatureList.ANDROID_APP_INTEGRATION,
+                AuxiliarySearchProvider.TAB_AGE_HOURS_PARAM,
+                "10");
+        FeatureList.setTestValues(config);
+        // Recreate provider to update the finch parameter.
+        mAuxiliarySearchProvider = new AuxiliarySearchProvider(mProfile, mTabModelSelector);
+        assertEquals(10 * 60 * 60 * 1000, mAuxiliarySearchProvider.getTabsMaxAgeMs());
     }
 }
