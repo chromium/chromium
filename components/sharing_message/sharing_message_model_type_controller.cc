55dd126b977c9831fcb44abe72986a3eae61d102
Hira Mahmood <hiramahmood@google.com>
2024-08-07 21:50:24 +0000

    Rename SharingMessageDataTypeController
    SharingMessageModelTypeController was renamed to
    SharingMessageDataTypeController in this CL: crrev.com/c/5754864
    
    This CL renames it back to ModelType so that crrev.com/c/5718879 can be
    cleanly reverted.
    
    Bug: 356649891, 356879192
    Change-Id: Ifc0a4d6525b061e1f69f74c1725b38ec133171f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5769621
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338695}

--- components/sharing_message/sharing_message_model_type_controller.cc
+++ components/sharing_message/sharing_message_model_type_controller.cc
@@ -2,23 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/sharing_message/sharing_message_data_type_controller.h"
+#include "components/sharing_message/sharing_message_model_type_controller.h"
 
 #include <utility>
 
-SharingMessageDataTypeController::SharingMessageDataTypeController(
+SharingMessageModelTypeController::SharingMessageModelTypeController(
     std::unique_ptr<syncer::DataTypeControllerDelegate>
         delegate_for_full_sync_mode,
     std::unique_ptr<syncer::DataTypeControllerDelegate>
         delegate_for_transport_mode)
     : syncer::DataTypeController(syncer::SHARING_MESSAGE,
-                                  std::move(delegate_for_full_sync_mode),
-                                  std::move(delegate_for_transport_mode)) {}
+                                 std::move(delegate_for_full_sync_mode),
+                                 std::move(delegate_for_transport_mode)) {}
 
-SharingMessageDataTypeController::~SharingMessageDataTypeController() =
+SharingMessageModelTypeController::~SharingMessageModelTypeController() =
     default;
 
-void SharingMessageDataTypeController::Stop(syncer::SyncStopMetadataFate fate,
+void SharingMessageModelTypeController::Stop(syncer::SyncStopMetadataFate fate,
                                              StopCallback callback) {
   DCHECK(CalledOnValidThread());
   // Clear sync metadata regardless of incoming fate even when sync gets paused
@@ -27,5 +27,5 @@ void SharingMessageDataTypeController::Stop(syncer::SyncStopMetadataFate fate,
   // determine whether sharing messages can be sent (they can't if sync is
   // paused).
   DataTypeController::Stop(syncer::SyncStopMetadataFate::CLEAR_METADATA,
-                            std::move(callback));
+                           std::move(callback));
 }
--- components/sharing_message/sharing_message_model_type_controller.h
+++ components/sharing_message/sharing_message_model_type_controller.h
@@ -2,31 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_SHARING_MESSAGE_SHARING_MESSAGE_DATA_TYPE_CONTROLLER_H_
-#define COMPONENTS_SHARING_MESSAGE_SHARING_MESSAGE_DATA_TYPE_CONTROLLER_H_
+#ifndef COMPONENTS_SHARING_MESSAGE_SHARING_MESSAGE_MODEL_TYPE_CONTROLLER_H_
+#define COMPONENTS_SHARING_MESSAGE_SHARING_MESSAGE_MODEL_TYPE_CONTROLLER_H_
 
 #include <memory>
 
 #include "components/sync/service/data_type_controller.h"
 
 // Controls syncing of SHARING_MESSAGE.
-class SharingMessageDataTypeController : public syncer::DataTypeController {
+class SharingMessageModelTypeController : public syncer::DataTypeController {
  public:
   // |delegate_for_full_sync_mode| and |delegate_for_transport_mode| must not be
   // null.
-  SharingMessageDataTypeController(
+  SharingMessageModelTypeController(
       std::unique_ptr<syncer::DataTypeControllerDelegate>
           delegate_for_full_sync_mode,
       std::unique_ptr<syncer::DataTypeControllerDelegate>
           delegate_for_transport_mode);
-  ~SharingMessageDataTypeController() override;
-  SharingMessageDataTypeController(const SharingMessageDataTypeController&) =
+  ~SharingMessageModelTypeController() override;
+  SharingMessageModelTypeController(const SharingMessageModelTypeController&) =
       delete;
-  SharingMessageDataTypeController& operator=(
-      const SharingMessageDataTypeController&) = delete;
+  SharingMessageModelTypeController& operator=(
+      const SharingMessageModelTypeController&) = delete;
 
   // DataTypeController overrides.
   void Stop(syncer::SyncStopMetadataFate fate, StopCallback callback) override;
 };
 
-#endif  // COMPONENTS_SHARING_MESSAGE_SHARING_MESSAGE_DATA_TYPE_CONTROLLER_H_
+#endif  // COMPONENTS_SHARING_MESSAGE_SHARING_MESSAGE_MODEL_TYPE_CONTROLLER_H_
