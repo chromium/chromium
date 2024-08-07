f29928ec4aab74fa6914ffb7b80410077a24a715
David Bertoni <dbertoni@chromium.org>
2024-08-07 18:18:07 +0000

    [Extensions] Create a BUILD.gn for the permissions API.
    Bug: 40593486
    Change-Id: I1bcdd3eb9df3486b3bc58d0c9d7c4b1d2ea4da54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767321
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338604}

--- chrome/browser/extensions/permissions/permissions_helpers.cc
+++ chrome/browser/extensions/permissions/permissions_helpers.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/api/permissions/permissions_api_helpers.h"
+#include "chrome/browser/extensions/permissions/permissions_helpers.h"
 
 #include <stddef.h>
 
