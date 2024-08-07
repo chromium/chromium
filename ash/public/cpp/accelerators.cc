c8aaecdbf1cee34907fd81a3034480f85558d964
David Padlipsky <dpad@google.com>
2024-08-07 22:24:48 +0000

    Unlock Search + F shortcut as only right alt is meant to be locked
    Bug: b/352813358
    Change-Id: Id79840d131260c2f0b70a5d1d56fbf2cc8c973d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767852
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Auto-Submit: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338721}

--- ash/public/cpp/accelerators.cc
+++ ash/public/cpp/accelerators.cc
@@ -410,8 +410,7 @@ const size_t kToggleGameDashboardAcceleratorDataLength =
 const AcceleratorData kTogglePickerAcceleratorData[] = {
     {false, ui::VKEY_RIGHT_ALT, ui::EF_NONE, AcceleratorAction::kTogglePicker,
      true},
-    {true, ui::VKEY_F, ui::EF_COMMAND_DOWN, AcceleratorAction::kTogglePicker,
-     true},
+    {true, ui::VKEY_F, ui::EF_COMMAND_DOWN, AcceleratorAction::kTogglePicker},
 };
 
 const size_t kTogglePickerAcceleratorDataLength =
