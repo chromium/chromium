7d3d709a816ed5b583354589647e1a929b0e68ec
Ian Clelland <iclelland@chromium.org>
2024-08-07 14:20:06 +0000

    Fix PageLoadTimingStatus histogram
    The histogram description is supposed to be kept in sync with the C++
    enum definitions, but two commits (https://crrev.com/c/1949861 and
    https://crrev.com/c/2177653) 4 years ago broke that sync, and the
    dashboards have been misinterpreting the histogram buckets since that
    time.
    
    This corrects the XML description of the enum values, assigns specific
    integers to the symbols in code, and re-emphasises that the two need to
    remain in sync (including IFTTT annotations which should catch any
    future changes in presubmit.)
    
    Change-Id: I52ae87bd86c790ccb27ea7d9a20d5a48d3c63bf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765693
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338458}

--- components/page_load_metrics/browser/page_load_metrics_update_dispatcher.h
+++ components/page_load_metrics/browser/page_load_metrics_update_dispatcher.h
@@ -32,72 +32,77 @@ enum class PageLoadTrackerPageType;
 
 // Used to track the status of PageLoadTimings received from the render process.
 //
+// These values are recorded in histograms. Entries should not be renumbered
+// and numeric values should never be reused.
+//
 // If you add elements to this enum, make sure you update the enum value in
 // histograms.xml. Only add elements to the end to prevent inconsistencies
 // between versions.
+// LINT.IfChange(PageLoadTimingStatus)
 enum PageLoadTimingStatus {
   // The PageLoadTiming is valid (all data within the PageLoadTiming is
   // consistent with expectations).
-  VALID,
+  VALID = 0,
 
   // All remaining status codes are for invalid PageLoadTimings.
 
   // The PageLoadTiming was empty.
-  INVALID_EMPTY_TIMING,
+  INVALID_EMPTY_TIMING = 1,
 
   // The PageLoadTiming had a null navigation_start.
-  INVALID_NULL_NAVIGATION_START,
+  INVALID_NULL_NAVIGATION_START = 2,
 
   // Script load or execution durations in the PageLoadTiming were too long.
-  INVALID_SCRIPT_LOAD_LONGER_THAN_PARSE,
-  INVALID_SCRIPT_EXEC_LONGER_THAN_PARSE,
-  INVALID_SCRIPT_LOAD_DOC_WRITE_LONGER_THAN_SCRIPT_LOAD,
-  INVALID_SCRIPT_EXEC_DOC_WRITE_LONGER_THAN_SCRIPT_EXEC,
+  INVALID_SCRIPT_LOAD_LONGER_THAN_PARSE = 3,
+  INVALID_SCRIPT_EXEC_LONGER_THAN_PARSE = 4,
+  INVALID_SCRIPT_LOAD_DOC_WRITE_LONGER_THAN_SCRIPT_LOAD = 5,
+  INVALID_SCRIPT_EXEC_DOC_WRITE_LONGER_THAN_SCRIPT_EXEC = 6,
 
   // The order of two events in the PageLoadTiming was invalid. Either the first
   // wasn't present when the second was present, or the second was reported as
   // happening before the first.
-  INVALID_ORDER_RESPONSE_START_PARSE_START,
-  INVALID_ORDER_PARSE_START_PARSE_STOP,
-  INVALID_ORDER_PARSE_STOP_DOM_CONTENT_LOADED,
-  INVALID_ORDER_DOM_CONTENT_LOADED_LOAD,
-  INVALID_ORDER_PARSE_START_FIRST_PAINT,
+  INVALID_ORDER_RESPONSE_START_PARSE_START = 7,
+  INVALID_ORDER_PARSE_START_PARSE_STOP = 8,
+  INVALID_ORDER_PARSE_STOP_DOM_CONTENT_LOADED = 9,
+  INVALID_ORDER_DOM_CONTENT_LOADED_LOAD = 10,
+  INVALID_ORDER_PARSE_START_FIRST_PAINT = 11,
   // Deprecated but not removing because it would affect histogram enumeration.
-  INVALID_ORDER_FIRST_PAINT_FIRST_TEXT_PAINT,
-  INVALID_ORDER_FIRST_PAINT_FIRST_IMAGE_PAINT,
-  INVALID_ORDER_FIRST_PAINT_FIRST_CONTENTFUL_PAINT,
-  INVALID_ORDER_FIRST_PAINT_FIRST_MEANINGFUL_PAINT,
+  INVALID_ORDER_FIRST_PAINT_FIRST_TEXT_PAINT = 12,
+  INVALID_ORDER_FIRST_PAINT_FIRST_IMAGE_PAINT = 13,
+  INVALID_ORDER_FIRST_PAINT_FIRST_CONTENTFUL_PAINT = 14,
+  INVALID_ORDER_FIRST_PAINT_FIRST_MEANINGFUL_PAINT = 15,
   // Deprecated but not removing because it would affect histogram enumeration.
-  INVALID_ORDER_FIRST_MEANINGFUL_PAINT_PAGE_INTERACTIVE,
+  INVALID_ORDER_FIRST_MEANINGFUL_PAINT_PAGE_INTERACTIVE = 16,
 
   // We received a first input delay without a first input timestamp.
-  INVALID_NULL_FIRST_INPUT_TIMESTAMP,
+  INVALID_NULL_FIRST_INPUT_TIMESTAMP = 17,
   // We received a first input timestamp without a first input delay.
-  INVALID_NULL_FIRST_INPUT_DELAY,
+  INVALID_NULL_FIRST_INPUT_DELAY = 18,
 
   // We received a longest input delay without a longest input timestamp.
-  INVALID_NULL_LONGEST_INPUT_TIMESTAMP,
+  INVALID_NULL_LONGEST_INPUT_TIMESTAMP = 19,
   // We received a longest input timestamp without a longest input delay.
-  INVALID_NULL_LONGEST_INPUT_DELAY,
+  INVALID_NULL_LONGEST_INPUT_DELAY = 20,
 
   // We received a first scroll delay without a first scroll timestamp.
-  INVALID_NULL_FIRST_SCROLL_TIMESTAMP,
+  INVALID_NULL_FIRST_SCROLL_TIMESTAMP = 21,
   // We received a first scroll timestamp without a first scroll delay.
-  INVALID_NULL_FIRST_SCROLL_DELAY,
+  INVALID_NULL_FIRST_SCROLL_DELAY = 22,
 
   // Longest input delay cannot happen before first input delay.
-  INVALID_LONGEST_INPUT_TIMESTAMP_LESS_THAN_FIRST_INPUT_TIMESTAMP,
+  INVALID_LONGEST_INPUT_TIMESTAMP_LESS_THAN_FIRST_INPUT_TIMESTAMP = 23,
 
   // Longest input delay cannot be less than first input delay.
-  INVALID_LONGEST_INPUT_DELAY_LESS_THAN_FIRST_INPUT_DELAY,
+  INVALID_LONGEST_INPUT_DELAY_LESS_THAN_FIRST_INPUT_DELAY = 24,
 
   // Deprecated but not removing because it would affect histogram enumeration.
-  INVALID_ORDER_PARSE_START_ACTIVATION_START,
-  INVALID_ORDER_ACTIVATION_START_FIRST_PAINT,
+  INVALID_ORDER_PARSE_START_ACTIVATION_START = 25,
+  INVALID_ORDER_ACTIVATION_START_FIRST_PAINT = 26,
 
   // New values should be added before this final entry.
   LAST_PAGE_LOAD_TIMING_STATUS,
 };
+// LINT.ThenChange(//tools/metrics/histograms/metadata/page/enums.xml:PageLoadTimingStatus)
 
 extern const char kPageLoadTimingStatus[];
 
