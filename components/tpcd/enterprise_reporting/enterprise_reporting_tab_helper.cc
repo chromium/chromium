4a793ba968cef48e3da5a5e53d6c755f5601bfe0
Erin Stewart <erinstewart@google.com>
2024-08-08 19:41:43 +0000

    Subscribe to cookie events when a NavigationHandle is available
    Use WebContentsObserver to subscribe to cookie events on navigation.
    This will be used for enterprise reporting.
    
    go/3pcd-enterprise-reporting (Googlers-only)
    
    Bug: 350966712
    Change-Id: Ic30172c21321403a72db1146e946afc2f2664791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689791
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Erin Stewart <erinstewart@google.com>
    Reviewed-by: Aaron Selya <selya@google.com>
    Commit-Queue: Aaron Selya <selya@google.com>
    Commit-Queue: Erin Stewart <erinstewart@google.com>
    Cr-Commit-Position: refs/heads/main@{#1339232}

--- components/tpcd/enterprise_reporting/enterprise_reporting_tab_helper.cc
+++ components/tpcd/enterprise_reporting/enterprise_reporting_tab_helper.cc
@@ -4,6 +4,8 @@
 
 #include "components/tpcd/enterprise_reporting/enterprise_reporting_tab_helper.h"
 
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/storage_partition.h"
 #include "services/network/public/mojom/network_context.mojom.h"
@@ -39,6 +41,30 @@ base::Value::Dict CreateReportBody(const ReportDetails& report_details) {
   return body;
 }
 
+bool ShouldReport(const net::CookieInclusionStatus& status) {
+  return status.HasExclusionReason(
+             net::CookieInclusionStatus::
+                 EXCLUDE_THIRD_PARTY_BLOCKED_WITHIN_FIRST_PARTY_SET) ||
+         status.HasExclusionReason(
+             net::CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT) ||
+         status.HasWarningReason(
+             net::CookieInclusionStatus::WARN_THIRD_PARTY_PHASEOUT);
+}
+
+std::string GetReportType(const net::CookieInclusionStatus& status) {
+  if (status.HasExclusionReason(
+          net::CookieInclusionStatus::
+              EXCLUDE_THIRD_PARTY_BLOCKED_WITHIN_FIRST_PARTY_SET) ||
+      status.HasExclusionReason(
+          net::CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT)) {
+    return kEnterpriseErrorReportType;
+  } else {
+    DCHECK(status.HasWarningReason(
+        net::CookieInclusionStatus::WARN_THIRD_PARTY_PHASEOUT));
+    return kEnterpriseWarningReportType;
+  }
+}
+
 }  // namespace
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(EnterpriseReportingTabHelper);
@@ -68,24 +94,15 @@ void EnterpriseReportingTabHelper::OnCookiesAccessed(
   for (const net::CookieWithAccessResult& cookie :
        details.cookie_access_result_list) {
     const net::CookieInclusionStatus& status = cookie.access_result.status;
-    std::string report_type;
-    if (status.HasExclusionReason(
-            net::CookieInclusionStatus::
-                EXCLUDE_THIRD_PARTY_BLOCKED_WITHIN_FIRST_PARTY_SET) ||
-        status.HasExclusionReason(
-            net::CookieInclusionStatus::EXCLUDE_THIRD_PARTY_PHASEOUT)) {
-      report_type = kEnterpriseErrorReportType;
-    } else if (status.HasWarningReason(
-                   net::CookieInclusionStatus::WARN_THIRD_PARTY_PHASEOUT)) {
-      report_type = kEnterpriseWarningReportType;
-    } else {
+    if (!ShouldReport(status)) {
       continue;
     }
+    std::string report_type = GetReportType(status);
 
     ReportDetails report_details(
-        render_frame_host->GetLastCommittedURL().spec(), details.url.spec(),
-        cookie.cookie.Name(), cookie.cookie.Domain(), cookie.cookie.Path(),
-        access_operation);
+        /*frame_url=*/render_frame_host->GetLastCommittedURL().spec(),
+        /*access_url=*/details.url.spec(), cookie.cookie.Name(),
+        cookie.cookie.Domain(), cookie.cookie.Path(), access_operation);
 
     // TODO(crbug.com/352737473): Update group parameter to use endpoint from
     // subscription.
@@ -93,6 +110,52 @@ void EnterpriseReportingTabHelper::OnCookiesAccessed(
         report_type, report_type,
         render_frame_host->GetOutermostMainFrameOrEmbedder()
             ->GetLastCommittedURL(),
+        CreateReportBody(report_details));
+  }
+}
+
+void EnterpriseReportingTabHelper::OnCookiesAccessed(
+    content::NavigationHandle* navigation_handle,
+    const content::CookieAccessDetails& details) {
+  if (!base::FeatureList::IsEnabled(
+          net::features::kReportingApiEnableEnterpriseCookieIssues)) {
+    return;
+  }
+  content::BrowserContext* browser_context =
+      web_contents()->GetBrowserContext();
+  content::StoragePartition* storage_partition =
+      browser_context->GetStoragePartition(
+          navigation_handle->GetStartingSiteInstance(),
+          /*can_create=*/true);
+  network::mojom::NetworkContext* network_context =
+      storage_partition->GetNetworkContext();
+
+  std::string access_operation =
+      details.type == network::mojom::CookieAccessDetails_Type::kRead ? "read"
+                                                                      : "write";
+
+  for (const net::CookieWithAccessResult& cookie :
+       details.cookie_access_result_list) {
+    const net::CookieInclusionStatus& status = cookie.access_result.status;
+    if (!ShouldReport(status)) {
+      continue;
+    }
+    std::string report_type = GetReportType(status);
+
+    ReportDetails report_details(/*frame_url=*/details.url.spec(),
+                                 /*access_url=*/details.url.spec(),
+                                 cookie.cookie.Name(), cookie.cookie.Domain(),
+                                 cookie.cookie.Path(), access_operation);
+
+    // TODO(crbug.com/352737473): Update group parameter to use endpoint from
+    // subscription.
+    network_context->QueueEnterpriseReport(
+        report_type, report_type,
+        navigation_handle->IsInOutermostMainFrame()
+            ? details.url
+            : navigation_handle->GetParentFrameOrOuterDocument()
+                  ->GetOutermostMainFrame()
+                  ->GetLastCommittedURL(),
         CreateReportBody(report_details));
   }
 }
