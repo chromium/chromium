54b3bb62aa7b426bee6cdbc3d96b874f7184ddb3
Kamila Hasanbega <hkamila@google.com>
2024-08-08 08:12:01 +0000

    Code health: Replace lock icon to tune icon naming
    As the lock icon on the omnibox was replaced with the tune icon, this
    cl changes how we refer to this icon in two console messages.
    
    Change-Id: I8db476989fcab9082e9eae95f7495306cf0702ff
    Fixed: 40925521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766692
    Commit-Queue: Kamila Hasanbega <hkamila@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338920}

--- components/permissions/permission_context_base.cc
+++ components/permissions/permission_context_base.cc
@@ -73,14 +73,14 @@ const char kPermissionBlockedRepeatedIgnoresMessage[] =
 const char kPermissionBlockedRepeatedDismissalsMessage[] =
     "%s permission has been blocked as the user has dismissed the permission "
     "prompt several times. This can be reset in Page Info which can be "
-    "accessed by clicking the lock icon next to the URL. See "
+    "accessed by clicking the tune icon next to the URL. See "
     "https://www.chromestatus.com/feature/6443143280984064 for more "
     "information.";
 
 const char kPermissionBlockedRepeatedIgnoresMessage[] =
     "%s permission has been blocked as the user has ignored the permission "
     "prompt several times. This can be reset in Page Info which can be "
-    "accessed by clicking the lock icon next to the URL. See "
+    "accessed by clicking the tune icon next to the URL. See "
     "https://www.chromestatus.com/feature/6443143280984064 for more "
     "information.";
 #endif
