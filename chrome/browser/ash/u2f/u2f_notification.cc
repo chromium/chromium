c4c9b6677ebaf54502c4f645ca3dde1cb792e349
Eriko Kurimoto <elkurin@chromium.org>
2024-08-08 12:20:11 +0000

    ash: Create //chrome/browser/ash/u2f
    Bug: b:357064309
    Change-Id: I591bc912c260ecfbb5ba6f152b9898660cd07187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767059
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339000}

--- chrome/browser/ash/u2f/u2f_notification.cc
+++ chrome/browser/ash/u2f/u2f_notification.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/u2f_notification.h"
+#include "chrome/browser/ash/u2f/u2f_notification.h"
 
 #include <memory>
 #include <string>
