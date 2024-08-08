51f825df3bbb4acd31e7171982fe01fade8216b7
Greg Thompson <grt@chromium.org>
2024-08-08 21:26:52 +0000

    Remove use of variable-length array in AccessibilityWinBrowserTest
    Variable-length arrays are not part of the C++ standard, and are
    dangerous to use because they allocate a potentially-unbounded
    amount of data on the stack. In addition, they trigger a clang warning
    (-Wvla-extension) which is currently suppressed in Chromium builds.
    
    Bug: 349656479
    Change-Id: I007b61c38dec6e83fbee5f053bba4bc4379b8768
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771412
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339311}

--- content/browser/accessibility/accessibility_win_browsertest.cc
+++ content/browser/accessibility/accessibility_win_browsertest.cc
@@ -4395,11 +4395,12 @@ IN_PROC_BROWSER_TEST_F(AccessibilityWinBrowserTest, TestIAccessibleAction) {
       image_action->get_localizedName(2, localized_name.Receive()));
   EXPECT_EQ(nullptr, localized_name.Get());
 
-  LONG n_key_bindings = -1;
-  BSTR* key_bindings[n_actions];
+  LONG n_key_bindings = 0;
+  BSTR* key_bindings = nullptr;
   EXPECT_HRESULT_SUCCEEDED(
-      image_action->get_keyBinding(0, 100, key_bindings, &n_key_bindings));
+      image_action->get_keyBinding(0, 100, &key_bindings, &n_key_bindings));
   EXPECT_EQ(0, n_key_bindings);
+  EXPECT_EQ(nullptr, key_bindings);
 
   base::win::ScopedVariant childid_self(CHILDID_SELF);
   base::win::ScopedBstr image_name;
