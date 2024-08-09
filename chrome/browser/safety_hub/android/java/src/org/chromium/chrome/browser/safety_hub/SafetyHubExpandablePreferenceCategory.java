d852f9328edacc4b6c9507f15378b56cc05ac531
Zaina Al-Mashni <zalmashni@google.com>
2024-08-09 16:09:22 +0000

    [SHoA] Increase top padding for Safety Tips Module
    Screenshot: https://screenshot.googleplex.com/95AH7LSYd6FcVc8
    Fixed: 354738293
    Change-Id: Iebf208f2c9eb7e97309307e58dba50dd39fc852d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777202
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1339680}

--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubExpandablePreferenceCategory.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubExpandablePreferenceCategory.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.safety_hub;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.preference.PreferenceViewHolder;
@@ -20,6 +21,15 @@ public class SafetyHubExpandablePreferenceCategory extends ExpandablePreferenceG
     @Override
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
+
+        // Set the top padding for the preference.
+        View container = holder.itemView;
+        container.setPadding(
+                container.getPaddingStart(),
+                32,
+                container.getPaddingEnd(),
+                container.getPaddingBottom());
+
         TextView titleView = (TextView) holder.findViewById(android.R.id.title);
         assert titleView != null;
         titleView.setTextAppearance(R.style.TextAppearance_TextMediumThick_Accent1);
