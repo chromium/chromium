b8267b3628c3c2899fbbf1eb077a12d1a9c7fce9
Kenichi Ishibashi <bashi@chromium.org>
2024-08-10 01:54:25 +0000

    Make HostResolver::AllProtocolEndpointsHaveEch() generic
    So that we can call the function for both HostResolverEndpointResult
    and ServiceEndpoint. This is a preparation for supporting ECH in
    HttpStreamPool.
    
    No behavior changes.
    
    Bug: 346835898
    Change-Id: I31be2c8a2057c8a78690454c0d9fd5c152da3db1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5775902
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339996}

--- net/dns/host_resolver.cc
+++ net/dns/host_resolver.cc
@@ -516,23 +516,6 @@ AddressList HostResolver::EndpointResultToAddressList(
 }
 
 // static
-bool HostResolver::AllProtocolEndpointsHaveEch(
-    base::span<const HostResolverEndpointResult> endpoints) {
-  bool has_svcb = false;
-  for (const auto& endpoint : endpoints) {
-    if (!endpoint.metadata.supported_protocol_alpns.empty()) {
-      has_svcb = true;
-      if (endpoint.metadata.ech_config_list.empty()) {
-        return false;  // There is a non-ECH SVCB/HTTPS route.
-      }
-    }
-  }
-  // Either there were no SVCB/HTTPS records (should be SVCB-optional), or there
-  // were and all supported ECH (should be SVCB-reliant).
-  return has_svcb;
-}
-
-// static
 bool HostResolver::MayUseNAT64ForIPv4Literal(HostResolverFlags flags,
                                              HostResolverSource source,
                                              const IPAddress& ip_address) {
