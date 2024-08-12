5e7eef1fb35eddeb21458aa9ff5feb049293915b
Liza Bipin <mlbipin@google.com>
2024-08-12 15:07:33 +0000

    Remove references to expired metric ManagedUsers.BlockedFrameDepth
    Change-Id: Ib4a641438b166bb0148526a5b80b12d7e6a266d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5768935
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340392}

--- chrome/browser/supervised_user/supervised_user_navigation_observer.cc
+++ chrome/browser/supervised_user/supervised_user_navigation_observer.cc
@@ -191,11 +191,6 @@ void SupervisedUserNavigationObserver::DidFinishLoad(
     RecordPageLoadUKM(render_frame_host);
   }
 
-  if (base::Contains(supervised_user_interstitials_,
-                     render_frame_host->GetFrameTreeNodeId())) {
-    UMA_HISTOGRAM_COUNTS_1000("ManagedUsers.BlockedFrameDepth",
-                              render_frame_host->GetFrameDepth());
-  }
 }
 
 void SupervisedUserNavigationObserver::RecordPageLoadUKM(
