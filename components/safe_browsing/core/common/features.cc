f0c4a5ff299884ccd5488089856d82cbdfc39abd
Daniel Rubery <drubery@chromium.org>
2024-08-12 18:44:58 +0000

    Enable kGooglePlayProtectPrompt by default
    This is approved for a full launch, so we can enable it by default and
    allow it to release as Chrome rolls out.
    
    Change-Id: Ibe2e02ac1451b07d06676bf1ddfacda904b03b24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783994
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340531}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -169,7 +169,7 @@ BASE_FEATURE(kExtensionTelemetryDisableOffstoreExtensions,
 
 BASE_FEATURE(kGooglePlayProtectPrompt,
              "SafeBrowsingGooglePlayProtectPrompt",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGooglePlayProtectInApkTelemetry,
              "SafeBrowsingGooglePlayProtectInApkTelemetry",
