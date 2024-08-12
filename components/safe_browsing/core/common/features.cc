f197786ed15120617b7d27d2166945f2864e88cf
Richard Chen <richche@chromium.org>
2024-08-12 17:04:34 +0000

    Launch ExtensionTelemetryDeclarativeNetRequestActionSignal
    We collected enough data in Stable 10%, ready to launch.
    
    Bug: 338063661
    Change-Id: I75db60974fbcac08baf1479eda1799b89a956428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783718
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340453}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -119,7 +119,7 @@ BASE_FEATURE(kExtensionTelemetryConfiguration,
 
 BASE_FEATURE(kExtensionTelemetryDeclarativeNetRequestActionSignal,
              "SafeBrowsingExtensionTelemetryDeclarativeNetRequestActionSignal",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kExtensionTelemetryFileDataForCommandLineExtensions,
              "SafeBrowsingExtensionTelemetryFileDataForCommandLineExtensions",
