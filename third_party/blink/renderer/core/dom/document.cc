641c2acc87d05f9db1f10eeaa3d771fb0fc1a5a3
Noam Rosenthal <nrosenthal@chromium.org>
2024-08-12 09:43:43 +0000

    Keep text selection during moveBefore
    Bug: 353441316
    Change-Id: Id988191a7deca179e5ac196ef58d2a9129b75845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725934
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340280}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -5851,10 +5851,13 @@ void Document::NodeWillBeRemoved(Node& n) {
   for (NodeIterator* ni : node_iterators_)
     ni->NodeWillBeRemoved(n);
 
-  for (Range* range : ranges_) {
-    range->NodeWillBeRemoved(n);
-    if (range == sequential_focus_navigation_starting_point_)
-      range->FixupRemovedNodeAcrossShadowBoundary(n);
+  if (!StatePreservingAtomicMoveInProgress()) {
+    for (Range* range : ranges_) {
+      range->NodeWillBeRemoved(n);
+      if (range == sequential_focus_navigation_starting_point_) {
+        range->FixupRemovedNodeAcrossShadowBoundary(n);
+      }
+    }
   }
 
   synchronous_mutation_observer_set_.ForEachObserver(
