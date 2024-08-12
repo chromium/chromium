fd02f7bf81ed03a0b4ee0d3c493efaa5116178bd
Dana Fried <dfried@chromium.org>
2024-08-12 18:39:17 +0000

    [User Education] Move NTP promos to auto-configuration.
    This removes hard-coded configuration from desktop NTP customization
    promos and switches to using "promo feature used" calls.
    
    The old events are added to the promo specifications to respect users
    who already used the feature under the old configuration, but are
    otherwise no longer used.
    
    Change-Id: Iae9d4823012e6fe8f12b0d222db275ae118637a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773736
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340524}

--- chrome/browser/new_tab_page/feature_promo_helper/new_tab_page_feature_promo_helper_unittest.cc
+++ chrome/browser/new_tab_page/feature_promo_helper/new_tab_page_feature_promo_helper_unittest.cc
@@ -5,6 +5,9 @@
 #include "chrome/browser/new_tab_page/feature_promo_helper/new_tab_page_feature_promo_helper.h"
 
 #include "chrome/browser/feature_engagement/tracker_factory.h"
+#include "chrome/browser/ui/views/user_education/browser_user_education_service.h"
+#include "chrome/browser/user_education/user_education_service.h"
+#include "chrome/browser/user_education/user_education_service_factory.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/test_browser_window.h"
 #include "components/feature_engagement/public/event_constants.h"
@@ -34,6 +37,11 @@ class NewTabPageFeaturePromoHelperTest : public BrowserWithTestWindowTest {
         static_cast<testing::NiceMock<feature_engagement::test::MockTracker>*>(
             feature_engagement::TrackerFactory::GetForBrowserContext(
                 tab_->GetBrowserContext()));
+
+    MaybeRegisterChromeFeaturePromos(
+        UserEducationServiceFactory::GetForBrowserContext(
+            tab_->GetBrowserContext())
+            ->feature_promo_registry());
   }
 
   NewTabPageFeaturePromoHelper* helper() { return helper_.get(); }
@@ -88,10 +96,11 @@ class NewTabPageFeaturePromoHelperTest : public BrowserWithTestWindowTest {
 
 TEST_F(NewTabPageFeaturePromoHelperTest, RecordFeatureUsage_CustomizeChrome) {
   EXPECT_CALL(*mock_tracker(),
-              NotifyEvent(feature_engagement::events::kCustomizeChromeOpened))
+              NotifyUsedEvent(testing::Ref(
+                  feature_engagement::kIPHDesktopCustomizeChromeFeature)))
       .Times(1);
-  helper()->RecordFeatureUsage(
-      feature_engagement::events::kCustomizeChromeOpened, tab());
+  helper()->RecordPromoFeatureUsage(
+      feature_engagement::kIPHDesktopCustomizeChromeFeature, tab());
 }
 
 TEST_F(NewTabPageFeaturePromoHelperTest,
