d9afad6634cfe35ad9bb4ddeba16ee7b6428013f
Xiaochen Zhou <xiaochenzh@chromium.org>
2024-08-13 14:56:16 +0000

    Protected Audience: Enable the feature for renderSize field to browserSignals of scoreAd by default.
    Enable feature kRenderSizeInScoreAdBrowserSignals by default. Remove
    corresponding field trial entry.
    
    This feature (fix) will be launched via finch. See crrev.com/c/5447954.
    
    Bug: 333628467
    Change-Id: I022233aff2c6609c296e47790cf64d1cea40860a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757351
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340981}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2652,7 +2652,7 @@ BASE_FEATURE(kNoThrottlingVisibleAgent,
 
 BASE_FEATURE(kRenderSizeInScoreAdBrowserSignals,
              "RenderSizeInScoreAdBrowserSignals",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOptimizeLoadingDataUrls,
              "OptimizeLoadingDataUrls",
