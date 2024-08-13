bf51fd493abcfbea5440c455f23ed1730ac28bbe
Ali Juma <ajuma@chromium.org>
2024-08-13 21:22:50 +0000

    [iOS] Add a provider API for memory experimentation
    This defines a memory experimentation provider API.
    
    This is part of a sequence of CLs:
    1) https://crrev.com/c/5756193 (this CL)
    2) https://crrev.com/i/7549887
    3) https://crrev.com/c/5756911
    
    Change-Id: I1d93f31fcc2f1549b9cdd003c475a1253192dc82
    Bug: 359252988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756193
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341272}

---
+++ ios/chrome/browser/providers/memory_experimenter/chromium_memory_experimenter.mm
@@ -0,0 +1,11 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/memory_experimenter/memory_experimenter_api.h"
+
+namespace ios::provider {
+
+void BeginMemoryExperimentation() {}
+
+}  // namespace ios::provider
