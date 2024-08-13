e060295655a2405e76e6c9c38e85e4bee0063089
JunHo Seo <junho0924.seo@lge.com>
2024-08-13 04:54:08 +0000

    Snav: Ensure initial focused element on test
    This CL ensures explicit starting element on spatial navigation tests.
    
    In addition, likely unclear information can be provided
    when there is no found activeElement on evaluation step.
    (e.g. Cannot read properties of null (reading 'activeElement'))
    This CL makes more robust in the case so that we can check
    step number and expected element.
    
    Bug: 351244340
    Change-Id: Id2460bdca350115cc3a27ef1a38ab1dbc51ff786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782429
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: JunHo Seo <junho0924.seo@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1340795}

--- third_party/blink/web_tests/fast/spatial-navigation/resources/snav-testharness.js
+++ third_party/blink/web_tests/fast/spatial-navigation/resources/snav-testharness.js
@@ -65,6 +65,13 @@
     return null;
   }
 
+  function focusedElement() {
+    let focused_document = focusedDocument();
+    if (focused_document)
+      return focused_document.activeElement;
+    return null;
+  }
+
   // Allows us to query element ids also in iframes' documents.
   function findElement(searchString) {
     let searchPath = searchString.split(",");
@@ -100,7 +107,7 @@
     let receivingDoc = wanted.ownerDocument;
     let verifyAndAdvance = gAsyncTest.step_func(function() {
       clearTimeout(failureTimer);
-      let focused = focusedDocument().activeElement;
+      let focused = focusedElement();
       assert_equals(focused, wanted,
                     'step ' + step + ' ' + JSON.stringify(move) + ':');
 
@@ -117,7 +124,7 @@
     // Start a timer to catch the failure of missing keyup event.
     failureTimer = setTimeout(gAsyncTest.step_func(function() {
       assert_unreached('step ' + step + ': timeout when waiting for focus on ' + expectedId +
-                       ', actual focus on ' + focusedDocument().activeElement.id);
+                       ', actual focus on ' + (focusedElement() ? focusedElement().id : '<null>'));
       gAsyncTest.done();
     }), 1000);
     triggerMove(direction);
@@ -149,6 +156,10 @@
 
       // All iframes must be loaded before trying to navigate to them.
       window.addEventListener('load', gAsyncTest.step_func(() => {
+        // Some test pages give focus to arbitrary element as start point.
+        // Otherwise, ensure root document as start point.
+        if (!focusedElement())
+          document.focus();
         stepAndAssertMoves(expectedMoves);
       }));
     }
