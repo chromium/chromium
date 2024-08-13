f4502bf5b3f61eb56bf9935692721f73c10137a6
Jean-Philippe Gravel <jpgravel@chromium.org>
2024-08-13 20:06:17 +0000

    Remove redundant ClearRect in HTMLCanvasElement::Reset
    For 2d contexts, `HTMLCanvasElement::Reset` calls `context->Reset()`,
    which clears the frame buffer. This makes the following call to
    `context_->ClearRect()` redundant.
    
    Bug: 354124879
    Change-Id: I481a7f4e2b800346edb047ded65cd6fa5f502851
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5749521
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341209}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -761,14 +761,11 @@ void HTMLCanvasElement::Reset() {
   gfx::Size old_size = Size();
   gfx::Size new_size(w, h);
 
-  // If the size of an existing buffer matches, we can just clear it instead of
-  // reallocating.  This optimization is only done for 2D canvases for now.
-  if (ResourceProvider() != nullptr && old_size == new_size &&
-      IsRenderingContext2D()) {
-    if (!canvas_is_clear_) {
-      canvas_is_clear_ = true;
-      context_->ClearRect(0, 0, width(), height());
-    }
+  // If the size of an existing buffer matches, we can reuse that buffer.
+  // This optimization is only done for 2D canvases for now.
+  if (IsRenderingContext2D() && ResourceProvider() != nullptr &&
+      old_size == new_size) {
+    canvas_is_clear_ = true;
     return;
   }
 
