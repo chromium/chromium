d03eb7f7b7aa635b5e5131640f60bf38e1bd0eee
Nasser Al-shawwa <alshawwa@chromium.org>
2024-08-13 13:23:50 +0000

    Add boilerplate for watermark print enhancements to `PrintCompositor`.
    Change-Id: Iea941d484a28102e808f7797b5df4b9c654d785b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783535
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340936}

--- components/services/print_compositor/print_compositor_impl.cc
+++ components/services/print_compositor/print_compositor_impl.cc
@@ -16,6 +16,7 @@
 #include "build/build_config.h"
 #include "components/crash/core/common/crash_key.h"
 #include "components/discardable_memory/client/client_discardable_shared_memory_manager.h"
+#include "components/enterprise/buildflags/buildflags.h"
 #include "components/services/print_compositor/public/cpp/print_service_mojo_types.h"
 #include "content/public/utility/utility_thread.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -39,6 +40,10 @@
 #include "third_party/skia/include/core/SkFontMgr.h"
 #elif BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_ANDROID)
 #include "third_party/blink/public/platform/platform.h"
+#endif
+
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
+#include "components/enterprise/watermarking/features.h"
 #endif
 
 using MojoDiscardableSharedMemoryManager =
@@ -67,9 +72,21 @@ sk_sp<SkDocument> MakeDocument(
       generate_document_outline, &stream);
 }
 
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
+void DrawEnterpriseWatermark(SkCanvas* canvas, SkSize size) {
+  if (!base::FeatureList::IsEnabled(
+          enterprise_watermark::features::kEnablePrintWatermark)) {
+    return;
+  }
+}
+#endif
+
 void DrawPage(SkDocument* doc, const SkDocumentPage& page) {
   SkCanvas* canvas = doc->beginPage(page.fSize.width(), page.fSize.height());
   canvas->drawPicture(page.fPicture);
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
+  DrawEnterpriseWatermark(canvas, page.fSize);
+#endif
   doc->endPage();
 }
 
