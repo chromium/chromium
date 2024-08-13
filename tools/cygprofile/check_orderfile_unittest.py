b0b71ceb2f4b62c5ac7090a69b6f1558fc9bdd18
Egor Pasko <pasko@chromium.org>
2024-08-13 17:25:11 +0000

    orderfile: Ignore same-offset symbols in the orderfile
    A few symbols can be deduplicated at link time by Identical Code Folding
    (ICF) in LLD (and other linkers). When it happens, in the final binary a
    number of symbols point at the same address.
    
    When building a binary with orderfile instrumentation, we disable ICF to
    get more precision in the data. As a consequence the Orderfile Generator
    has no idea which symbols are going to be aliased this way by ICF in the
    final release official build (without instrumentation).
    
    A dilemma appears when symbol ordering wants to put two same-offset
    symbols in different parts of the binary (to minimize iTLB misses, for
    example). We never explored the matter as far as to find a strategy that
    would bring benefits from selectively disabling ICF in such a way. In
    other words, it is probably okay to put the corresponding section in any
    of the positions selected by the Orderfile Generator.
    
    Fortunately, the LLD is careful to predictably put all of the
    same-address symbols as soon as the first symbol from the group is
    encountered in the --symbol-ordering-file.
    
    Let's consider such behavior as 'good enough' for our optimization
    goals. In the orderfile verification step we would track symbols grouped
    by the same offset instead of tracking symbols individually. If a
    group's representative appears once in the orderfile, its order is
    checked, but then all remaining appearances of this group in the
    orderfile are ignored (=allowed without limits).
    
    Symbols from the orderfile may be missing in the binary. Here I propose
    to print a warning about them, but not introduce a hard limit yet. This
    will allow us to see what happens with stale orderfiles (by running
    check_orderfile on various revisions that the orderfile bot has not
    seen).
    
    This is the ~last prerequisite of removing the orderfile patching.
    
    Bug: 340534475
    Change-Id: I6f4d44a6df95db7ac9a158bf9398cb0ab800a044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783099
    Reviewed-by: Rasika Navarange <rasikan@google.com>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341075}

--- tools/cygprofile/check_orderfile_unittest.py
+++ tools/cygprofile/check_orderfile_unittest.py
@@ -13,9 +13,14 @@ class TestCheckOrderFile(unittest.TestCase):
   _SYMBOL_INFOS = [
       symbol_extractor.SymbolInfo('first', 0x1, 0, ''),
       symbol_extractor.SymbolInfo('second', 0x2, 0, ''),
+      symbol_extractor.SymbolInfo('secondSameOffsetA', 0x2, 0, ''),
+      symbol_extractor.SymbolInfo('secondSameOffsetB', 0x2, 0, ''),
       symbol_extractor.SymbolInfo('notProfiled', 0x4, 0, ''),
       symbol_extractor.SymbolInfo('third', 0x3, 0, ''),
-      symbol_extractor.SymbolInfo('foo.llvm.123', 0x4, 0, ''),
+      symbol_extractor.SymbolInfo('foo.llvm.123', 0x5, 0, ''),
+      symbol_extractor.SymbolInfo('atTwoAddresses', 0x6, 0, ''),
+      symbol_extractor.SymbolInfo('between', 0x6, 0, ''),
+      symbol_extractor.SymbolInfo('atTwoAddresses', 0x8, 0, ''),
   ]
 
   def testVerifySymbolOrder(self):
@@ -37,6 +42,32 @@ class TestCheckOrderFile(unittest.TestCase):
             ['first', 'eighth', 'third', 'foo.llvm.123'], self._SYMBOL_INFOS,
             0))
 
+  def testVerifySameOffset(self):
+    self.assertTrue(
+        check_orderfile._VerifySymbolOrder(
+            ['first', 'second', 'third', 'secondSameOffsetA'],
+            self._SYMBOL_INFOS, 0))
+    self.assertTrue(
+        check_orderfile._VerifySymbolOrder([
+            'first', 'second', 'third', 'secondSameOffsetA', 'foo.llvm.123',
+            'secondSameOffsetB'
+        ], self._SYMBOL_INFOS, 0))
+    self.assertTrue(
+        check_orderfile._VerifySymbolOrder(
+            ['first', 'secondSameOffsetB', 'third', 'second'],
+            self._SYMBOL_INFOS, 0))
+    self.assertFalse(
+        check_orderfile._VerifySymbolOrder(
+            ['first', 'third', 'second', 'secondSameOffsetA'],
+            self._SYMBOL_INFOS, 0))
+
+  def testSymbolAtMultipleAddressesIsIgnored(self):
+    self.assertTrue(
+        check_orderfile._VerifySymbolOrder(['between', 'atTwoAddresses'],
+                                           self._SYMBOL_INFOS, 0))
+    self.assertTrue(
+        check_orderfile._VerifySymbolOrder(['atTwoAddresses', 'between'],
+                                           self._SYMBOL_INFOS, 0))
 
 if __name__ == '__main__':
   unittest.main()
