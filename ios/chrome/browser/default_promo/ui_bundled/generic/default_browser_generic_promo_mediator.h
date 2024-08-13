1a3e1d7b636d5c44e671724bcd24780d18159a48
Eric Ekey <ericekey@google.com>
2024-08-13 13:16:30 +0000

    [iOS] Segmented Default Browser Video Promo
    Adds personalized messaging to the Default Browser video promo,
    using user segmentation info. Subtle differences in UI strings
    can influence user engagement metrics. This change intends to
    increase default browser adoption and user retention.
    
    Low-Coverage-Reason: OTHER This change does not change the public interface.
    Fixed: 355243689
    Change-Id: I0099dfdba6cbc8e22dc6d6cbfce4f1e5dc2009b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5745174
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340931}

--- ios/chrome/browser/default_promo/ui_bundled/generic/default_browser_generic_promo_mediator.h
+++ ios/chrome/browser/default_promo/ui_bundled/generic/default_browser_generic_promo_mediator.h
@@ -7,11 +7,39 @@
 
 #import <UIKit/UIKit.h>
 
-// The mediator for the generic default browser promo.
+#import "base/ios/block_types.h"
+
+namespace segmentation_platform {
+class DeviceSwitcherResultDispatcher;
+class SegmentationPlatformService;
+}  // namespace segmentation_platform
+
+@protocol DefaultBrowserGenericPromoConsumer;
+
+// The mediator for the generic default browser promo, with personalized
+// messaging for users identified by the Segmentation Platform as being in a
+// targeted segment.
 @interface DefaultBrowserGenericPromoMediator : NSObject
 
-// Handles user tap on primary action.
+// Main consumer for this mediator.
+@property(nonatomic, weak) id<DefaultBrowserGenericPromoConsumer> consumer;
+
+// Initializer with `segmentationService` and `deviceSwitcherResultDispatcher`.
+- (instancetype)initWithSegmentationService:
+                    (segmentation_platform::SegmentationPlatformService*)
+                        segmentationService
+             deviceSwitcherResultDispatcher:
+                 (segmentation_platform::DeviceSwitcherResultDispatcher*)
+                     dispatcher;
+
+// Handles user tap on primary action. Allows the user to open the iOS settings.
 - (void)didTapPrimaryActionButton;
+
+// Sets the user segment retrieved from the Segmentation Platform.
+- (void)retrieveUserSegmentWithCompletion:(ProceduralBlock)completion;
+
+// Disconnects this mediator.
+- (void)disconnect;
 
 @end
 
