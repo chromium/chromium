05a28d6510268d0b8aefff3d7e668e01779e33e2
Aliona DANGLA <alionadangla@google.com>
2024-08-13 16:03:05 +0000

    [iOS] Replace chrome_browser_state_manager.h imports
    This CL update chrome_browser_state_manager.h imports to use
    profile_manager_ios.h header instead.
    
    This CL was uploaded by git cl split.
    
    R=rohitrao@chromium.org
    
    Bug: 358072368
    Change-Id: I9a4eb716bce5104d3e40e536446a7a7d7cad9600
    Fixed: 359176329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782740
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341023}

--- ios/chrome/browser/policy/model/reporting/profile_report_generator_ios.mm
+++ ios/chrome/browser/policy/model/reporting/profile_report_generator_ios.mm
@@ -11,7 +11,7 @@
 #import "ios/chrome/browser/policy/model/policy_conversions_client_ios.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
-#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state_manager.h"
+#import "ios/chrome/browser/shared/model/profile/profile_manager_ios.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/system_identity.h"
