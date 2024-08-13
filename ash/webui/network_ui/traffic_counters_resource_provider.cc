3b7c72273d0eb4645007c084c1421000ac921bce
Kartik Hegde <khegde@google.com>
2024-08-13 20:56:54 +0000

    traffic_counters: Add screen reader feedback for reset button pressed
    BUG=b:357085402
    TEST=Ensured that the deployed changes are tested on a DUT (the reset
    button message is announced via screen reader feedback)
    
    Change-Id: Id95782209f44d5718c527b8380b629d545a38bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785259
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Kartik Hegde <khegde@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341249}

--- ash/webui/network_ui/traffic_counters_resource_provider.cc
+++ ash/webui/network_ui/traffic_counters_resource_provider.cc
@@ -58,6 +58,8 @@ constexpr webui::LocalizedString kLocalizedStrings[] = {
      IDS_TRAFFIC_COUNTERS_DATA_USAGE_DROPDOWN_LABEL},
     {"TrafficCountersDataUsageResetDayTooltipA11yLabel",
      IDS_TRAFFIC_COUNTERS_DATA_USAGE_RESET_DAY_TOOLTIP_A11Y_LABEL},
+    {"TrafficCountersDataUsageResetButtonPressedA11yMessage",
+     IDS_TRAFFIC_COUNTERS_DATA_USAGE_RESET_BUTTON_PRESSED_A11Y_MESSAGE},
 };
 
 }  // namespace
