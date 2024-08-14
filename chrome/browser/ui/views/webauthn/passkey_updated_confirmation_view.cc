84ca0bb876182f6d7275a4e333306937b69c5989
Gabriel Viera <gviera@google.com>
2024-08-14 18:15:19 +0000

    [Webauthn] Add Passkey Updated Confirmation View
    * Adds views for a bubble that will appear to notify the user
      that a passkey was updated with the current user details.
    
    https://screenshot.googleplex.com/VHmiYskBjRZUiPo.png
    
    Bug: 356907668
    Change-Id: Ia3403b50d7c7b77fb4911ebbd94c1c40c689400f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783703
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Gabriel Viera <gviera@google.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341813}

---
+++ chrome/browser/ui/views/webauthn/passkey_updated_confirmation_view.cc
@@ -0,0 +1,74 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/webauthn/passkey_updated_confirmation_view.h"
+
+#include "base/functional/bind.h"
+#include "chrome/browser/ui/passwords/passwords_model_delegate.h"
+#include "chrome/browser/ui/passwords/ui_utils.h"
+#include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "chrome/grit/generated_resources.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "ui/views/controls/styled_label.h"
+#include "ui/views/layout/fill_layout.h"
+
+PasskeyUpdatedConfirmationView::PasskeyUpdatedConfirmationView(
+    content::WebContents* web_contents,
+    views::View* anchor_view,
+    DisplayReason display_reason)
+    : PasswordBubbleViewBase(web_contents,
+                             anchor_view,
+                             /*easily_dismissable=*/true),
+      controller_(PasswordsModelDelegateFromWebContents(web_contents),
+                  display_reason == AUTOMATIC
+                      ? password_manager::metrics_util::
+                            AUTOMATIC_PASSKEY_UPDATED_CONFIRMATION
+                      : password_manager::metrics_util::
+                            MANUAL_PASSKEY_UPDATED_CONFIRMATION) {
+  SetButtons(ui::DIALOG_BUTTON_NONE);
+  SetShowIcon(true);
+  SetTitle(controller_.GetTitle());
+  SetLayoutManager(std::make_unique<views::FillLayout>());
+
+  size_t offset;
+  const std::u16string link =
+      l10n_util::GetStringUTF16(IDS_WEBAUTHN_GPM_PASSKEY_UPDATED_LINK);
+  std::u16string text = l10n_util::GetStringFUTF16(
+      IDS_WEBAUTHN_GPM_PASSKEY_UPDATED_LABEL, link, &offset);
+
+  auto label = std::make_unique<views::StyledLabel>();
+  label->SetText(text);
+  label->SetTextContext(views::style::CONTEXT_BUBBLE_FOOTER);
+  label->SetDefaultTextStyle(views::style::STYLE_SECONDARY);
+  label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  label->AddStyleRange(
+      gfx::Range(offset, offset + link.length()),
+      views::StyledLabel::RangeStyleInfo::CreateForLink(base::BindRepeating(
+          &PasskeyUpdatedConfirmationView::OnGooglePasswordManagerLinkClicked,
+          base::Unretained(this))));
+  AddChildView(std::move(label));
+}
+
+PasskeyUpdatedConfirmationView::~PasskeyUpdatedConfirmationView() = default;
+
+PasskeyUpdatedConfirmationController*
+PasskeyUpdatedConfirmationView::GetController() {
+  return &controller_;
+}
+
+const PasskeyUpdatedConfirmationController*
+PasskeyUpdatedConfirmationView::GetController() const {
+  return &controller_;
+}
+
+ui::ImageModel PasskeyUpdatedConfirmationView::GetWindowIcon() {
+  return ui::ImageModel::FromVectorIcon(GooglePasswordManagerVectorIcon(),
+                                        ui::kColorIcon);
+}
+
+void PasskeyUpdatedConfirmationView::OnGooglePasswordManagerLinkClicked() {
+  controller_.OnGooglePasswordManagerLinkClicked();
+  CloseBubble();
+}
