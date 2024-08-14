5533aa28bfe84fc19fb0f8d0766870be65368480
Eriko Kurimoto <elkurin@chromium.org>
2024-08-14 01:24:22 +0000

    ash: Move shortcut_mapping_pref_service to //c/b/ash/events
    Bug: b:357060745
    Change-Id: I73bf716b51b0cdc988e7856241a156e1a41784b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783600
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341400}

--- chrome/browser/ash/events/shortcut_mapping_pref_service.cc
+++ chrome/browser/ash/events/shortcut_mapping_pref_service.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/shortcut_mapping_pref_service.h"
+#include "chrome/browser/ash/events/shortcut_mapping_pref_service.h"
 
 #include "ash/constants/ash_pref_names.h"
 #include "chrome/browser/browser_process.h"
--- chrome/browser/ash/events/shortcut_mapping_pref_service.h
+++ chrome/browser/ash/events/shortcut_mapping_pref_service.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_SHORTCUT_MAPPING_PREF_SERVICE_H_
-#define CHROME_BROWSER_ASH_SHORTCUT_MAPPING_PREF_SERVICE_H_
+#ifndef CHROME_BROWSER_ASH_EVENTS_SHORTCUT_MAPPING_PREF_SERVICE_H_
+#define CHROME_BROWSER_ASH_EVENTS_SHORTCUT_MAPPING_PREF_SERVICE_H_
 
 #include "ui/base/shortcut_mapping_pref_delegate.h"
 
@@ -26,4 +26,4 @@ class ShortcutMappingPrefService : public ui::ShortcutMappingPrefDelegate {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_SHORTCUT_MAPPING_PREF_SERVICE_H_
+#endif  // CHROME_BROWSER_ASH_EVENTS_SHORTCUT_MAPPING_PREF_SERVICE_H_
