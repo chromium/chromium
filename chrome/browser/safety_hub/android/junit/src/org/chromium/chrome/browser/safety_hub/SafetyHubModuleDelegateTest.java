bac468e24bf8d52131e4cb2a211eebea40c538ed
Zaina Al-Mashni <zalmashni@google.com>
2024-08-14 14:55:50 +0000

    [SHoA] Add a test rule for Safety Hub
    Add a test rule to avoid duplication of the SH lengthy test set up and
    migrate all relevant tests to use it.
    
    Bug: 324562205
    Change-Id: I8c8e287866c08c12199266e4a2deca314c5dc528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782923
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341647}

--- chrome/browser/safety_hub/android/junit/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleDelegateTest.java
+++ chrome/browser/safety_hub/android/junit/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleDelegateTest.java
@@ -19,7 +19,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
@@ -28,84 +27,38 @@ import org.chromium.base.supplier.Supplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Features;
-import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
-import org.chromium.chrome.browser.password_manager.FakePasswordCheckupClientHelper;
-import org.chromium.chrome.browser.password_manager.FakePasswordCheckupClientHelperFactoryImpl;
-import org.chromium.chrome.browser.password_manager.FakePasswordManagerBackendSupportHelper;
-import org.chromium.chrome.browser.password_manager.PasswordCheckupClientHelperFactory;
-import org.chromium.chrome.browser.password_manager.PasswordManagerBackendSupportHelper;
-import org.chromium.chrome.browser.password_manager.PasswordManagerHelper;
-import org.chromium.chrome.browser.password_manager.PasswordManagerHelperJni;
-import org.chromium.chrome.browser.password_manager.PasswordManagerUtilBridge;
-import org.chromium.chrome.browser.password_manager.PasswordManagerUtilBridgeJni;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.profiles.ProfileManager;
-import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
-import org.chromium.chrome.browser.signin.services.SigninManager;
-import org.chromium.chrome.browser.sync.SyncServiceFactory;
 import org.chromium.chrome.browser.ui.signin.SigninAndHistorySyncActivityLauncher;
 import org.chromium.chrome.browser.ui.signin.SigninAndHistorySyncCoordinator;
 import org.chromium.chrome.browser.ui.signin.SyncConsentActivityLauncher;
-import org.chromium.components.prefs.PrefService;
-import org.chromium.components.signin.base.CoreAccountInfo;
-import org.chromium.components.signin.identitymanager.ConsentLevel;
-import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
-import org.chromium.components.sync.SyncService;
-import org.chromium.components.user_prefs.UserPrefs;
-import org.chromium.components.user_prefs.UserPrefsJni;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 
 /** Tests {@link SafetyHubModuleDelegate} */
 @RunWith(BaseRobolectricTestRunner.class)
 @Batch(Batch.PER_CLASS)
 public class SafetyHubModuleDelegateTest {
-    private static final String TEST_EMAIL_ADDRESS = "test@email.com";
 
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
+    @Rule public SafetyHubTestRule mSafetyHubTestRule = new SafetyHubTestRule();
 
-    @Rule public JniMocker mJniMocker = new JniMocker();
-
-    @Mock private PendingIntent mPasswordCheckIntentForAccountCheckup;
-    @Mock private SyncService mSyncService;
-    @Mock private SigninManager mSigninManager;
-    @Mock private IdentityServicesProvider mIdentityServicesProvider;
-    @Mock private IdentityManager mIdentityManager;
-    @Mock private Profile mProfile;
-    @Mock private PrefService mPrefService;
-    @Mock private UserPrefs.Natives mUserPrefsNatives;
-    @Mock private PasswordManagerUtilBridge.Natives mPasswordManagerUtilBridgeNatives;
-    @Mock private PasswordManagerHelper.Natives mPasswordManagerHelperNativeMock;
     @Mock private Supplier<ModalDialogManager> mModalDialogManagerSupplier;
     @Mock private SigninAndHistorySyncActivityLauncher mSigninLauncher;
     @Mock private SyncConsentActivityLauncher mSyncLauncher;
 
     private ModalDialogManager mModalDialogManager;
-
     private Context mContext;
-
     private SafetyHubModuleDelegate mSafetyHubModuleDelegate;
+    private Profile mProfile;
+    private PendingIntent mPasswordCheckIntentForAccountCheckup;
 
     @Before
     public void setUp() {
         mContext = ContextUtils.getApplicationContext();
-        MockitoAnnotations.initMocks(this);
-
-        mJniMocker.mock(UserPrefsJni.TEST_HOOKS, mUserPrefsNatives);
-        mJniMocker.mock(PasswordManagerUtilBridgeJni.TEST_HOOKS, mPasswordManagerUtilBridgeNatives);
-        mJniMocker.mock(PasswordManagerHelperJni.TEST_HOOKS, mPasswordManagerHelperNativeMock);
-
-        ProfileManager.setLastUsedProfileForTesting(mProfile);
-        when(mProfile.getOriginalProfile()).thenReturn(mProfile);
-        when(mUserPrefsNatives.get(mProfile)).thenReturn(mPrefService);
-        when(mIdentityServicesProvider.getSigninManager(mProfile)).thenReturn(mSigninManager);
-        when(mSigninManager.getIdentityManager()).thenReturn(mIdentityManager);
-        when(mIdentityServicesProvider.getIdentityManager(mProfile)).thenReturn(mIdentityManager);
-
-        IdentityServicesProvider.setInstanceForTests(mIdentityServicesProvider);
-        SyncServiceFactory.setInstanceForTesting(mSyncService);
-        setUpPasswordManagerBackendForTesting();
+        mProfile = mSafetyHubTestRule.getProfile();
+        mPasswordCheckIntentForAccountCheckup =
+                mSafetyHubTestRule.getIntentForAccountPasswordCheckup();
 
         mModalDialogManager =
                 new ModalDialogManager(
@@ -118,45 +71,10 @@ public class SafetyHubModuleDelegateTest {
                         mProfile, mModalDialogManagerSupplier, mSigninLauncher, mSyncLauncher);
     }
 
-    private void setSignedInState(boolean signedIn) {
-        when(mIdentityManager.hasPrimaryAccount(ConsentLevel.SIGNIN)).thenReturn(signedIn);
-        when(mIdentityManager.getPrimaryAccountInfo(ConsentLevel.SIGNIN))
-                .thenReturn(
-                        signedIn
-                                ? CoreAccountInfo.createFromEmailAndGaiaId(TEST_EMAIL_ADDRESS, "0")
-                                : null);
-    }
-
-    private void setUPMStatus(boolean isUPMEnabled) {
-        when(mPasswordManagerUtilBridgeNatives.shouldUseUpmWiring(mSyncService, mPrefService))
-                .thenReturn(isUPMEnabled);
-        when(mPasswordManagerUtilBridgeNatives.areMinUpmRequirementsMet()).thenReturn(true);
-    }
-
-    private void setUpPasswordManagerBackendForTesting() {
-        FakePasswordManagerBackendSupportHelper helper =
-                new FakePasswordManagerBackendSupportHelper();
-        helper.setBackendPresent(true);
-        PasswordManagerBackendSupportHelper.setInstanceForTesting(helper);
-
-        setUpFakePasswordCheckupClientHelper();
-    }
-
-    private void setUpFakePasswordCheckupClientHelper() {
-        FakePasswordCheckupClientHelperFactoryImpl passwordCheckupClientHelperFactory =
-                new FakePasswordCheckupClientHelperFactoryImpl();
-        PasswordCheckupClientHelperFactory.setFactoryForTesting(passwordCheckupClientHelperFactory);
-        FakePasswordCheckupClientHelper fakePasswordCheckupClientHelper =
-                (FakePasswordCheckupClientHelper) passwordCheckupClientHelperFactory.createHelper();
-
-        fakePasswordCheckupClientHelper.setIntentForAccountCheckup(
-                mPasswordCheckIntentForAccountCheckup);
-    }
-
     @Test
     public void testOpenPasswordCheckUI() throws PendingIntent.CanceledException {
-        setSignedInState(true);
-        setUPMStatus(true);
+        mSafetyHubTestRule.setSignedInState(true);
+        mSafetyHubTestRule.setUPMStatus(true);
 
         mSafetyHubModuleDelegate.showPasswordCheckUI(mContext);
         verify(mPasswordCheckIntentForAccountCheckup, times(1)).send();
@@ -165,7 +83,7 @@ public class SafetyHubModuleDelegateTest {
     @Test
     @Features.EnableFeatures(ChromeFeatureList.REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS)
     public void testLaunchSigninPromo() {
-        setSignedInState(false);
+        mSafetyHubTestRule.setSignedInState(false);
         mSafetyHubModuleDelegate.launchSyncOrSigninPromo(mContext);
         verify(mSigninLauncher)
                 .launchActivityIfAllowed(
@@ -183,7 +101,7 @@ public class SafetyHubModuleDelegateTest {
     @Test
     @Features.DisableFeatures(ChromeFeatureList.REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS)
     public void testLaunchSyncPromo() {
-        setSignedInState(false);
+        mSafetyHubTestRule.setSignedInState(false);
         mSafetyHubModuleDelegate.launchSyncOrSigninPromo(mContext);
         verify(mSyncLauncher)
                 .launchActivityIfAllowed(eq(mContext), eq(SigninAccessPoint.SAFETY_CHECK));
