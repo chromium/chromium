c818629da2a9a5d5e1dbe0431d69777d4f0ea3b2
Aoyuan Zuo <zuoaoyuan@chromium.org>
2024-08-14 19:52:38 +0000

    Enable ThirdPartyScriptDetection by Default
    The feature enables third party script detection and UKM reporting in
    Long Animation Frame Timing with a predefined list of enumerated third
    parties.
    
    With 21 days data on 1% stable, we observed no significant metrics
    movement on all platforms.
    
    Bug: 40266658
    Change-Id: Iccc23f6823f3c55566f40430672a8edb02dda9e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766017
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Aoyuan Zuo <zuoaoyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341884}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2327,7 +2327,7 @@ BASE_FEATURE(kStylusRichGestures,
 // Enables third party script regex matching for detecting technologies.
 BASE_FEATURE(kThirdPartyScriptDetection,
              "ThirdPartyScriptDetection",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kThreadedBodyLoader,
              "ThreadedBodyLoader",
