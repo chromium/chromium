5fc4e82171a06f10fcabee725e1a8fb089c1e9b8
Chris Fredrickson <cfredric@chromium.org>
2024-08-14 15:44:01 +0000

    Migrate to optional-based GetHeader overload in ['components/embedder_support/android', 'content/browser/android']
    HttpRequestHeaders::GetHeader has a new overload which returns an
    optional instead of using an out-parameter. I'm migrating callers and
    will eventually delete the old version of the method.
    
    This CL was uploaded by git cl split.
    
    R=jinsukkim@chromium.org
    
    Bug: 355451174
    Change-Id: I6f6d4f900190d2153ef05ce91edf8d2523a56387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5788841
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341689}

--- content/browser/android/content_url_loader_factory.cc
+++ content/browser/android/content_url_loader_factory.cc
@@ -56,12 +56,12 @@ bool GetRequestedByteRange(const network::ResourceRequest& request,
   *first_byte_to_send = 0;
   *total_bytes_to_send = content_size;
 
-  std::string range_header;
+  std::optional<std::string> range_header =
+      request.headers.GetHeader(net::HttpRequestHeaders::kRange);
   std::vector<net::HttpByteRange> ranges;
 
-  if (!request.headers.GetHeader(net::HttpRequestHeaders::kRange,
-                                 &range_header) ||
-      !net::HttpUtil::ParseRangeHeader(range_header, &ranges)) {
+  if (!range_header ||
+      !net::HttpUtil::ParseRangeHeader(*range_header, &ranges)) {
     return true;
   }
 
@@ -85,11 +85,13 @@ void GetMimeType(const network::ResourceRequest& request,
                  std::string* out_mime_type) {
   out_mime_type->clear();
 
-  std::string intent_type_header;
-  if ((request.resource_type ==
-       static_cast<int>(blink::mojom::ResourceType::kMainFrame)) &&
-      request.headers.GetHeader("X-Chrome-intent-type", &intent_type_header)) {
-    *out_mime_type = intent_type_header;
+  if (request.resource_type ==
+      static_cast<int>(blink::mojom::ResourceType::kMainFrame)) {
+    std::optional<std::string> intent_type_header =
+        request.headers.GetHeader("X-Chrome-intent-type");
+    if (intent_type_header) {
+      *out_mime_type = std::move(intent_type_header).value();
+    }
   }
 
   if (out_mime_type->empty())
