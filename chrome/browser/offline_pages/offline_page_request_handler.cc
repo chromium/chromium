fd0b35c80e12624289c033f611ae4a5608faaec6
Chris Fredrickson <cfredric@chromium.org>
2024-08-14 17:22:56 +0000

    Migrate to optional-based GetHeader overload in chrome/browser/offline_pages
    HttpRequestHeaders::GetHeader has a new overload which returns an
    optional instead of using an out-parameter. I'm migrating callers and
    will eventually delete the old version of the method.
    
    This CL was uploaded by git cl split.
    
    R=dewittj@chromium.org
    
    Bug: 355451174
    Change-Id: I9b6fe3e09d180b72b8908ddae8c4a516f6d4d59b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783168
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341771}

--- chrome/browser/offline_pages/offline_page_request_handler.cc
+++ chrome/browser/offline_pages/offline_page_request_handler.cc
@@ -302,8 +302,9 @@ OfflinePageRequestHandler::OfflinePageRequestHandler(
       network_state_(NetworkState::CONNECTED_NETWORK),
       candidate_index_(0) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  std::string offline_header_value;
-  extra_request_headers.GetHeader(kOfflinePageHeader, &offline_header_value);
+  std::string offline_header_value =
+      extra_request_headers.GetHeader(kOfflinePageHeader)
+          .value_or(std::string());
   // Note that |offline_header| will be empty if parsing from the header value
   // fails.
   offline_header_ = OfflinePageHeader(offline_header_value);
