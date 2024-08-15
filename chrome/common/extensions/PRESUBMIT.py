c6deea440117d1aefdc415623051468eaaa4d94e
Tim Judkins <tjudkins@chromium.org>
2024-08-15 17:04:33 +0000

    [Extensions] Update extensions PRESUMBIT files to PRESUBMIT_VERSION 2
    The version 2 format requires less boilerplate, repetition and is a bit
    nicer to read. Basic rundown of 2.0.0 format is that all functions
    beginning with 'Check' will automatically be run as presubmit checks and
    ending the function with 'Upload' or 'Commit' will restrict it to only
    be run for those cases (otherwise they are run for both). For more
    details see: https://www.chromium.org/developers/how-tos/depottools/presubmit-scripts/
    
    `git cl presubmit -v -v` and `git cl presubmit --upload -v -v` were run
    locally to ensure all the checks were still being run as expected.
    
    Also runs a formatting pass over the files for improved readability
    and consistency.
    
    Bug: N/A
    Change-Id: Ib4a1216e597d66008c607b9791613b36c0b8c3fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789479
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342327}

--- chrome/common/extensions/PRESUBMIT.py
+++ chrome/common/extensions/PRESUBMIT.py
@@ -13,11 +13,14 @@ import fnmatch
 import os
 import re
 
+PRESUBMIT_VERSION = '2.0.0'
+
 EXTENSIONS_PATH = os.path.join('chrome', 'common', 'extensions')
 DOCS_PATH = os.path.join(EXTENSIONS_PATH, 'docs')
 TEMPLATES_PATH = os.path.join(DOCS_PATH, 'templates')
 INTROS_PATH = os.path.join(TEMPLATES_PATH, 'intros')
 ARTICLES_PATH = os.path.join(TEMPLATES_PATH, 'articles')
+
 
 def _CheckHeadingIDs(input_api):
   ids_re = re.compile('<h[23].*id=.*?>')
@@ -26,25 +29,21 @@ def _CheckHeadingIDs(input_api):
   for name in input_api.AbsoluteLocalPaths():
     if not os.path.exists(name):
       continue
-    if (fnmatch.fnmatch(name, '*%s*' % INTROS_PATH) or
-        fnmatch.fnmatch(name, '*%s*' % ARTICLES_PATH)):
+    if fnmatch.fnmatch(name, '*%s*' % INTROS_PATH) or fnmatch.fnmatch(
+        name, '*%s*' % ARTICLES_PATH
+    ):
       contents = input_api.ReadFile(name)
-      if (len(re.findall(headings_re, contents)) !=
-          len(re.findall(ids_re, contents))):
+      if len(re.findall(headings_re, contents)) != len(
+          re.findall(ids_re, contents)
+      ):
         bad_files.append(name)
   return bad_files
 
-def _CheckChange(input_api, output_api):
+
+def CheckChange(input_api, output_api):
   results = [
       output_api.PresubmitError('File %s needs an id for each heading.' % name)
-      for name in _CheckHeadingIDs(input_api)]
+      for name in _CheckHeadingIDs(input_api)
+  ]
 
   return results
-
-def CheckChangeOnUpload(input_api, output_api):
-  results = []
-  results += _CheckChange(input_api, output_api)
-  return results
-
-def CheckChangeOnCommit(input_api, output_api):
-  return _CheckChange(input_api, output_api)
