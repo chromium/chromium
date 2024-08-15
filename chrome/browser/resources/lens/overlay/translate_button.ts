c2801c054218f00c1fafcd5111c25ecbc09f9f94
Juan Mojica <juanmojica@google.com>
2024-08-15 20:40:40 +0000

    [lensoverlay] Limit languages to those translatable by the server.
    Change-Id: I731aad1cee68ed9a93de9234d93ee42d0226831d
    Bug: b:354275185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791665
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342468}

--- chrome/browser/resources/lens/overlay/translate_button.ts
+++ chrome/browser/resources/lens/overlay/translate_button.ts
@@ -14,6 +14,20 @@ import {LanguageBrowserProxyImpl} from './language_browser_proxy.js';
 import type {LanguageBrowserProxy} from './language_browser_proxy.js';
 import {getTemplate} from './translate_button.html.js';
 
+// The language codes that are supported to be translated by the server.
+const SUPPORTED_TRANSLATION_LANGUAGES = new Set([
+  'af',  'sq',  'am',    'ar',    'hy', 'az', 'eu', 'be', 'bn', 'bs', 'bg',
+  'ca',  'ceb', 'zh-CN', 'zh-TW', 'co', 'hr', 'cs', 'da', 'nl', 'en', 'eo',
+  'et',  'fi',  'fr',    'fy',    'gl', 'ka', 'de', 'el', 'gu', 'ht', 'ha',
+  'haw', 'hi',  'hmn',   'hu',    'is', 'ig', 'id', 'ga', 'it', 'iw', 'ja',
+  'jv',  'kn',  'kk',    'km',    'rw', 'ko', 'ku', 'ky', 'lo', 'la', 'lv',
+  'lt',  'lb',  'mk',    'mg',    'ms', 'ml', 'mt', 'mi', 'mr', 'mn', 'my',
+  'ne',  'no',  'ny',    'or',    'ps', 'fa', 'pl', 'pt', 'pa', 'ro', 'ru',
+  'sm',  'gd',  'sr',    'st',    'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es',
+  'su',  'sw',  'sv',    'tl',    'tg', 'ta', 'tt', 'te', 'th', 'tr', 'tk',
+  'uk',  'ur',  'ug',    'uz',    'vi', 'cy', 'xh', 'yi', 'yo', 'zu',
+]);
+
 export interface TranslateButtonElement {
   $: {
     languagePicker: HTMLDivElement,
@@ -87,7 +101,9 @@ export class TranslateButtonElement extends PolymerElement {
 
   private onLanguageListRetrieved(
       languageList: chrome.languageSettingsPrivate.Language[]) {
-    this.translateLanguageList = languageList;
+    this.translateLanguageList = languageList.filter((language) => {
+      return SUPPORTED_TRANSLATION_LANGUAGES.has(language.code);
+    });
 
     // After receiving the language list, get the default translate target
     // language. This needs to happen after fetching the language list so we can
