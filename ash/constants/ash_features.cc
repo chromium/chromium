89944c4b718f8ffb8135b264651b5b7daa477529
Jun Ishiguro <junis@google.com>
2024-08-15 12:15:41 +0000

    Revert "Saved Desks: Enable UI revamp"
    This reverts commit 755d431901225aa268ad416c047202dd5ca014b2.
    
    Reason for revert: (gardener) tast.wmp.DesksTemplate* and
    tast.wmp.SavedDeskDelete are failing and this blocks chromeos-chrome
    uprevs.
    
    Original change's description:
    > Saved Desks: Enable UI revamp
    >
    > Enable the Saved Desk UI feature by default, and change the criteria so
    > Forest must also be enabled.
    >
    > Bug: b/327639285
    > Change-Id: I1c54657dc6a9c603ba9b1d526ad14a4407dd6d37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789532
    > Auto-Submit: Elijah Hewer <hewer@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1342088}
    
    Bug: b/360073927, b/327639285
    Change-Id: I36598839cd93bf2bde9b45d5e3d3377321ddaf12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5786287
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1342220}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2623,7 +2623,7 @@ BASE_FEATURE(kSmartReader, "SmartReader", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSavedDeskUiRevamp,
              "SavedDeskUiRevamp",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kQuickAppAccessTestUI,
              "QuickAppAccessTestUI",
@@ -4557,11 +4557,8 @@ bool IsSamlNotificationOnPasswordChangeSuccessEnabled() {
 }
 
 bool IsSavedDeskUiRevampEnabled() {
-  if (!base::FeatureList::IsEnabled(kForestFeature)) {
-    return false;
-  }
-
-  return base::FeatureList::IsEnabled(kSavedDeskUiRevamp);
+  return base::FeatureList::IsEnabled(kForestFeature) &&
+         base::FeatureList::IsEnabled(kSavedDeskUiRevamp);
 }
 
 bool IsScalableIphEnabled() {
