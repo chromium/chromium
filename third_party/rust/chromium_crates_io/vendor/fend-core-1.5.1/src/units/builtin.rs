3c603ad2ff4c19c17fb7f0ed78f44e9727184fca
Lukasz Anforowicz <lukasza@chromium.org>
2024-08-15 20:28:06 +0000

    Roll fend-core: 1.5.0 => 1.5.1 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * fend-core: 1.5.0 => 1.5.1
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * fend-core@1.5.1: crypto-safe, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: If56d742c78b2c9f5d8952809974c9565df91904d
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789785
    Reviewed-by: Adrian Taylor <adetaylor@google.com>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342461}

--- third_party/rust/chromium_crates_io/vendor/fend-core-1.5.1/src/units/builtin.rs
+++ third_party/rust/chromium_crates_io/vendor/fend-core-1.5.1/src/units/builtin.rs
@@ -510,6 +510,13 @@ const COMMON_PHYSICAL_UNITS: &[UnitTuple] = &[
 	("sqm", "", "=m^2", ""),
 	("sqmm", "", "=mm^2", ""),
 	("gongjin", "", "l@1 kilogram", ""),
+	(
+		"toe",
+		"toe",
+		"s@41.868 gigajoules",
+		"tonne of oil equivalent",
+	),
+	("kgoe", "kgoe", "1/1000 toe", "kilogram of oil equivalent"),
 	// TODO remove these compatibility units
 	("lightyear", "lightyears", "light_year", ""),
 	("light", "", "c", ""),
