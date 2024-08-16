dac52215f060922b4b8e9cfe2117ffa11edaabb4
Florian Leimgruber <fleimgruber@google.com>
2024-08-16 12:34:52 +0000

    Enable AutofillSilentlyRemoveQuasiDuplicates by default
    As decided in today's feature sync.
    
    Bug: 325450676
    Change-Id: I81fdd151166206c0cc661743688a5f909e2bca27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789126
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342760}

--- components/autofill/core/common/autofill_features.cc
+++ components/autofill/core/common/autofill_features.cc
@@ -816,7 +816,7 @@ BASE_FEATURE(kAutofillLogDeduplicationMetrics,
 // TODO(crbug.com/325450676): Remove when launched.
 BASE_FEATURE(kAutofillSilentlyRemoveQuasiDuplicates,
              "AutofillSilentlyRemoveQuasiDuplicates",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Currently, the importing logic offers new profile creation if the observed
 // profile is non-mergeable with any existing profile. With this feature, low-
