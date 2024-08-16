3687323bd7b79acdbca9e904f61b1fb50cb9e692
Lei Zhang <thestig@chromium.org>
2024-08-16 20:45:01 +0000

    Round test expectation values in a PDF PluginController test
    Locally, the window.devicePixelRatio value is 1.0625. This causes the
    expectations for bitmap dimensions to be non-integer values, which does
    not make sense. Round down to match the test behavior.
    
    Change-Id: Id421ebcf3749f38da82acd39a42eb823a966ef15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784198
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343022}

--- chrome/test/data/pdf/plugin_controller_test.ts
+++ chrome/test/data/pdf/plugin_controller_test.ts
@@ -8,8 +8,8 @@ const tests = [
   async function testRequestThumbnail() {
     const data = await PluginController.getInstance().requestThumbnail(0);
 
-    const expectedWidth = 108 * window.devicePixelRatio;
-    const expectedHeight = 140 * window.devicePixelRatio;
+    const expectedWidth = Math.floor(108 * window.devicePixelRatio);
+    const expectedHeight = Math.floor(140 * window.devicePixelRatio);
     chrome.test.assertEq(expectedWidth, data.width);
     chrome.test.assertEq(expectedHeight, data.height);
 
