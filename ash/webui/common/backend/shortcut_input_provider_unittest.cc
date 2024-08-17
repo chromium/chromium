144ff96a8262603af62c90d06a34cd3fd2500069
Danny Wang <wangdanny@google.com>
2024-08-17 00:36:20 +0000

    shortcut: Properly support fn key in shortcut input
    Fn key is discarded generally because it's meant to be invisible to the
    overall system but we want to be able to display it in certain parts of
    the UI (such as shortcut input).
    
    Bug: b/359984305
    Test: ash_webui_unittests, manually verified in dut
    Change-Id: I1788774781e082486dd939cc4dfe1db939bc4f65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759105
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343133}

--- ash/webui/common/backend/shortcut_input_provider_unittest.cc
+++ ash/webui/common/backend/shortcut_input_provider_unittest.cc
@@ -56,6 +56,12 @@ ui::KeyEvent CreateKeyEvent(bool pressed) {
       ui::VKEY_A, ui::EF_NONE);
 }
 
+ui::KeyEvent CreateFnKeyEvent(bool pressed) {
+  return ui::KeyEvent(
+      pressed ? ui::EventType::kKeyPressed : ui::EventType::kKeyReleased,
+      ui::VKEY_FUNCTION, ui::EF_NONE);
+}
+
 }  // namespace
 
 class ShortcutInputProviderTest : public AshTestBase {
@@ -153,6 +159,31 @@ TEST_F(ShortcutInputProviderTest, PrewrittenEventOnlyDoesNotTriggerObserver) {
   EXPECT_TRUE(Shell::Get()
                   ->accelerator_controller()
                   ->ShouldPreventProcessingAccelerators());
+}
+
+TEST_F(ShortcutInputProviderTest, SimpleEventFixFnInShortCutApp) {
+  ui::KeyEvent prerewritten_pressed_event = CreateFnKeyEvent(/*pressed=*/true);
+  ui::KeyEvent prerewritten_released_event =
+      CreateFnKeyEvent(/*pressed=*/false);
+  ui::KeyEvent pressed_event = CreateFnKeyEvent(/*pressed=*/true);
+  ui::KeyEvent released_event = CreateFnKeyEvent(/*pressed=*/false);
+  shortcut_input_provider_->TieProviderToWidget(widget_.get());
+
+  shortcut_input_handler_->OnPrerewriteKeyInputEvent(
+      prerewritten_pressed_event);
+  shortcut_input_handler_->OnKeyEvent(&pressed_event);
+  shortcut_input_handler_->OnPrerewriteKeyInputEvent(
+      prerewritten_released_event);
+  shortcut_input_handler_->OnKeyEvent(&released_event);
+  shortcut_input_provider_->FlushMojoForTesting();
+
+  EXPECT_EQ(1, observer_->num_input_events_pressed());
+  EXPECT_EQ(1, observer_->num_input_events_released());
+  EXPECT_TRUE(Shell::Get()
+                  ->accelerator_controller()
+                  ->ShouldPreventProcessingAccelerators());
+  EXPECT_TRUE(
+      Shell::Get()->shortcut_input_handler()->should_consume_key_events());
 }
 
 TEST_F(ShortcutInputProviderTest, SimpleEvent) {
