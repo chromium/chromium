3936340c8aa8059622e211f45a5927cb1eb5725f
Mark Cogan <marq@google.com>
2024-08-19 16:08:12 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ios/chrome/browser/download/model.
    
    This CL was uploaded by git cl split.
    
    R=xinghuilu@chromium.org
    
    Bug: 359819865
    Change-Id: I2c65e7bbeebefcb0a15f91ed14809acb1f24c13a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796884
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343541}

--- ios/chrome/browser/download/model/background_service/background_download_service_factory.h
+++ ios/chrome/browser/download/model/background_service/background_download_service_factory.h
@@ -5,13 +5,12 @@
 #ifndef IOS_CHROME_BROWSER_DOWNLOAD_MODEL_BACKGROUND_SERVICE_BACKGROUND_DOWNLOAD_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_DOWNLOAD_MODEL_BACKGROUND_SERVICE_BACKGROUND_DOWNLOAD_SERVICE_FACTORY_H_
 
-#include <memory>
+#import <memory>
 
-#include "base/no_destructor.h"
-#include "components/download/public/background_service/clients.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ChromeBrowserState;
+#import "base/no_destructor.h"
+#import "components/download/public/background_service/clients.h"
+#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 namespace download {
 class BackgroundDownloadService;
