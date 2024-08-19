df7445df2714c5182d4677cf9793e4d4cbc6c90a
Mark Cogan <marq@google.com>
2024-08-19 14:57:28 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ios/chrome/browser/gcm/model.
    
    This CL was uploaded by git cl split.
    
    R=fgorski@chromium.org
    
    Bug: 359819865
    Change-Id: I04e10acbf79efaa9403e43c1284790c0710449f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797263
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343505}

--- ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.h
+++ ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.h
@@ -5,13 +5,12 @@
 #ifndef IOS_CHROME_BROWSER_GCM_MODEL_IOS_CHROME_GCM_PROFILE_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_GCM_MODEL_IOS_CHROME_GCM_PROFILE_SERVICE_FACTORY_H_
 
-#include <memory>
-#include <string>
+#import <memory>
+#import <string>
 
-#include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ChromeBrowserState;
+#import "base/no_destructor.h"
+#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 namespace gcm {
 class GCMProfileService;
