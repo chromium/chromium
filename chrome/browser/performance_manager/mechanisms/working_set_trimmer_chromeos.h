d97f0978c849c0f9d0df20ebae164ea25e2ffc82
Joe Mason <joenotcharles@google.com>
2024-08-19 23:36:07 +0000

    [PM] Trim the ChromeOS working set on the threadpool when PM is on the main thread
    Bug: 40755583
    Change-Id: If0badc57f005effda61048084e971c4d84e71a64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797444
    Commit-Queue: François Degros <fdegros@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343844}

--- chrome/browser/performance_manager/mechanisms/working_set_trimmer_chromeos.h
+++ chrome/browser/performance_manager/mechanisms/working_set_trimmer_chromeos.h
@@ -11,6 +11,7 @@
 #include "base/memory/weak_ptr.h"
 #include "base/no_destructor.h"
 #include "base/process/process_handle.h"
+#include "base/sequence_checker.h"
 #include "base/timer/elapsed_timer.h"
 #include "chrome/browser/performance_manager/mechanisms/working_set_trimmer.h"
 
@@ -71,6 +72,8 @@ class WorkingSetTrimmerChromeOS : public WorkingSetTrimmer {
   // directly, it should always be retrieved via
   // WorkingSetTrimmer::GetInstance().
   WorkingSetTrimmerChromeOS();
+
+  SEQUENCE_CHECKER(sequence_checker_);
 
   raw_ptr<content::BrowserContext> context_for_testing_ = nullptr;
 
