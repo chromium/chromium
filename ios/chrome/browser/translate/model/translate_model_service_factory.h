650d6c43b4557dc4e9eeab5a1ba9e446c76bda72
Mark Cogan <marq@google.com>
2024-08-19 15:17:47 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ios/chrome/browser/translate/model.
    
    This CL was uploaded by git cl split.
    
    R=cuianthony@chromium.org
    
    Bug: 359819865
    Change-Id: I95d8f7a594925aca133d3d375bc71aeb1abd6b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797286
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343517}

--- ios/chrome/browser/translate/model/translate_model_service_factory.h
+++ ios/chrome/browser/translate/model/translate_model_service_factory.h
@@ -5,12 +5,11 @@
 #ifndef IOS_CHROME_BROWSER_TRANSLATE_MODEL_TRANSLATE_MODEL_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_TRANSLATE_MODEL_TRANSLATE_MODEL_SERVICE_FACTORY_H_
 
-#include <memory>
+#import <memory>
 
-#include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ChromeBrowserState;
+#import "base/no_destructor.h"
+#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 namespace translate {
 class TranslateModelService;
