339f63719b9debb59ab3c9d0ac7d5d8abcb7ece4
Alex Moshchuk <alexmos@chromium.org>
2024-08-19 17:39:17 +0000

    Fix CanAccessDataForOrigin checks in WebViewTest.Shim_TestLoadDataAPI
    This test was using CanAccessDataForOrigin() to check an origin that
    wasn't actually committed in a WebView guest, since the committed
    origin had the embedded_test_server()'s port and the origin passed to
    CADFO didn't. This was already fixed for the
    AreOriginKeyedProcessesEnabledByDefault mode in
    https://crrev.com/5669692, but it will soon matter even without that
    mode for upcoming CADFO enforcements based on a list of committed
    origins. Hence, this CL fixes the origin used in CADFO to always match
    the committed one.
    
    Bug: 40148776
    Change-Id: I9b1b6b979258a346bf29fa6ff7868e1c997447a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798024
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343590}

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -4131,19 +4131,14 @@ IN_PROC_BROWSER_TEST_F(WebViewTest, Shim_TestLoadDataAPI) {
 
   auto* security_policy = content::ChildProcessSecurityPolicy::GetInstance();
   url::Origin base_origin =
-      content::SiteIsolationPolicy::AreOriginKeyedProcessesEnabledByDefault()
-          ? url::Origin::Create(
-                embedded_test_server()->GetURL("localhost", "/"))
-          : url::Origin::Create(GURL("http://localhost"));
+      url::Origin::Create(embedded_test_server()->GetURL("localhost", "/"));
   EXPECT_TRUE(security_policy->CanAccessDataForOrigin(
       guest_main_frame->GetProcess()->GetID(), base_origin));
 
   // Ensure the process doesn't have access to some other origin. This
   // verifies that site isolation is enforced.
   url::Origin another_origin =
-      content::SiteIsolationPolicy::AreOriginKeyedProcessesEnabledByDefault()
-          ? url::Origin::Create(embedded_test_server()->GetURL("foo.com", "/"))
-          : url::Origin::Create(GURL("http://foo.com"));
+      url::Origin::Create(embedded_test_server()->GetURL("foo.com", "/"));
   EXPECT_FALSE(security_policy->CanAccessDataForOrigin(
       guest_main_frame->GetProcess()->GetID(), another_origin));
 }
