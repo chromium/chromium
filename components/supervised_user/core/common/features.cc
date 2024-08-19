e2b0ba3bb926279b34b939d6486a92785a354b86
Nohemi Fernandez <fernandex@chromium.org>
2024-08-19 10:54:58 +0000

    [iOS] Disable account capability migration.
    The behavior for the two affected UIs (new tab page feed and incognito
    mode) requires additional testing and metrics to verify the correctness.
    Disable the feature while this is in progress.
    
    Bug: b:360787816
    Change-Id: I5e84ff340e8a41f14403f9e3b364eeb43b3b70c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783812
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343415}

--- components/supervised_user/core/common/features.cc
+++ components/supervised_user/core/common/features.cc
@@ -155,10 +155,10 @@ BASE_FEATURE(kWaitUntilAccessTokenAvailableForClassifyUrl,
 #if BUILDFLAG(IS_IOS)
 BASE_FEATURE(kReplaceSupervisionPrefsWithAccountCapabilitiesOnIOS,
              "ReplaceSupervisionPrefsWithAccountCapabilitiesOnIOS",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kReplaceSupervisionSystemCapabilitiesWithAccountCapabilitiesOnIOS,
              "ReplaceSupervisionSystemCapabilitiesWithAccountCapabilitiesOnIOS",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kFetchListFamilyMembersWithCapability,
