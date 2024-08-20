1417422d769cfc0ec12c8eb7290b7812a40c5f45
Mark Cogan <marq@google.com>
2024-08-20 05:55:16 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ios/chrome/browser/contextual_panel.
    
    This CL was uploaded by git cl split.
    
    R=rkgibson@google.com
    
    Bug: 359819865
    Change-Id: I02569d1121dd7da914ef00d52f56b2b38b3adfbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797223
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343942}

--- ios/chrome/browser/contextual_panel/model/contextual_panel_model_service_factory.h
+++ ios/chrome/browser/contextual_panel/model/contextual_panel_model_service_factory.h
@@ -5,10 +5,10 @@
 #ifndef IOS_CHROME_BROWSER_CONTEXTUAL_PANEL_MODEL_CONTEXTUAL_PANEL_MODEL_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_CONTEXTUAL_PANEL_MODEL_CONTEXTUAL_PANEL_MODEL_SERVICE_FACTORY_H_
 
-#include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "base/no_destructor.h"
+#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
-class ChromeBrowserState;
 class ContextualPanelModelService;
 
 // Singleton that owns all ContextualPanelModelServices and associates them with
--- ios/chrome/browser/contextual_panel/sample/model/sample_panel_model_factory.h
+++ ios/chrome/browser/contextual_panel/sample/model/sample_panel_model_factory.h
@@ -5,10 +5,10 @@
 #ifndef IOS_CHROME_BROWSER_CONTEXTUAL_PANEL_SAMPLE_MODEL_SAMPLE_PANEL_MODEL_FACTORY_H_
 #define IOS_CHROME_BROWSER_CONTEXTUAL_PANEL_SAMPLE_MODEL_SAMPLE_PANEL_MODEL_FACTORY_H_
 
-#include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "base/no_destructor.h"
+#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
-class ChromeBrowserState;
 class SamplePanelModel;
 
 // Singleton that owns all SamplePanelModels and associates them with
