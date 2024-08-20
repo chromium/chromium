232be2fe462724e441d417922e0318c253e78ccb
rbpotter <rbpotter@chromium.org>
2024-08-20 20:54:50 +0000

    [Reland] PDF Viewer: Migrate toolbar and sidenav elements to Lit
    Bug: 40943652
    Change-Id: I423af8abfa869c2d5ef4740d5dbee6e9c7085ea5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798567
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344375}

--- chrome/test/data/pdf/ink2_test.ts
+++ chrome/test/data/pdf/ink2_test.ts
@@ -4,8 +4,7 @@
 
 import {assert} from 'chrome://resources/js/assert.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
-import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
-import {isVisible} from 'chrome://webui-test/test_util.js';
+import {isVisible, microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 const viewer = document.body.querySelector('pdf-viewer')!;
 const viewerToolbar = viewer.$.toolbar;
@@ -37,7 +36,7 @@ chrome.test.runTests([
     chrome.test.assertFalse(viewerToolbar.annotationMode);
 
     viewerToolbar.toggleAnnotation();
-    await waitAfterNextRender(viewerToolbar);
+    await microtasksFinished();
     const sidePanel = viewer.shadowRoot!.querySelector('viewer-side-panel');
     assert(sidePanel);
 
@@ -46,7 +45,7 @@ chrome.test.runTests([
     chrome.test.assertTrue(isVisible(sidePanel));
 
     viewerToolbar.toggleAnnotation();
-    await waitAfterNextRender(viewerToolbar);
+    await microtasksFinished();
 
     // The side panel should be hidden when annotation mode is disabled.
     chrome.test.assertFalse(viewerToolbar.annotationMode);
