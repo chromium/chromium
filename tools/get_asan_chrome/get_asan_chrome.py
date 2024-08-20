bf9741015cccbc2b0622a5d2b682b0a50adb141d
Paul Semel <paulsemel@chromium.org>
2024-08-20 12:32:46 +0000

    Fix oses choices in get_asan_chrome.py
    We can now pick the available debug versions of the OSES.
    
    Change-Id: I8cd4bdb43095cd588a8c907cac03b4c5f2ddf449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783291
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344081}

--- tools/get_asan_chrome/get_asan_chrome.py
+++ tools/get_asan_chrome/get_asan_chrome.py
@@ -67,14 +67,25 @@ def get_release_metadata_by_version(version):
 
 
 def get_release_metadata_by_channel(release_info):
+    os_to_platform = {
+        'linux': 'linux',
+        'linux_debug': 'linux',
+        'mac': 'mac',
+        'win64': 'win64',
+        'lacros': 'lacros',
+    }
+    platform = os_to_platform[release_info.os]
     uri = (f'https://chromiumdash.appspot.com/fetch_releases'
-           f'?platform={release_info.os}'
+           f'?platform={platform}'
            f'&channel={release_info.channel}'
            f'&num=1&offset=0')
-    json_response = fetch_json(uri)[0]
-    release_info.branch_position = json_response[
-        'chromium_main_branch_position']
-    release_info.version = json_response['version']
+    json_response = fetch_json(uri)
+    if not json_response:
+        fail(f'No releases found for platform "{platform}" '
+             f'and channel "{release_info.channel}"')
+    release = json_response[0]
+    release_info.branch_position = release['chromium_main_branch_position']
+    release_info.version = release['version']
 
 
 def get_release_metadata(release_info):
@@ -119,7 +130,7 @@ def download_asan_chrome(release_info, download_dir, quiet, retries=100):
         'linux': 'linux-release/asan-linux-release',
         'linux_debug': 'linux-debug/asan-linux-debug',
         'mac': 'mac-release/asan-mac-release',
-        'mac_debug': 'mac-release/asan-mac-debug',
+        # 'mac_debug': 'mac-debug/asan-mac-debug',
         # 'ios': 'ios-release/asan-ios-release', # unsupported
         'lacros': 'linux-release-chromeos/asan-linux-release',
         # android is currently unsupported
@@ -178,7 +189,13 @@ if __name__ == '__main__':
                        help='Chromium channel, e.g. canary.')
     parser.add_argument(
         '--os',
-        choices=['linux', 'mac', 'win64', 'lacros'],
+        choices=[
+            'win64',
+            'linux',
+            'linux_debug',
+            'mac',
+            'lacros',
+        ],
         help='Operating system type as defined by Chromium Dash.')
     parser.add_argument(
         '--download_directory',
