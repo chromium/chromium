06898097606382abef1a4312f778003dfca5760f
Mark Cogan <marq@google.com>
2024-08-20 06:32:32 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ['ios/chrome/browser/optimization_guide/model', 'ios/chrome/browser/page_content_annotations/model', 'ios/chrome/browser/text_selection/model'].
    
    This CL was uploaded by git cl split.
    
    R=robertogden@chromium.org
    
    Bug: 359819865
    Change-Id: Icdd497781a300259b509aa3d2d69ef0884c4e343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797265
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343961}

--- ios/chrome/browser/text_selection/model/text_classifier_model_service_factory.h
+++ ios/chrome/browser/text_selection/model/text_classifier_model_service_factory.h
@@ -5,12 +5,12 @@
 #ifndef IOS_CHROME_BROWSER_TEXT_SELECTION_MODEL_TEXT_CLASSIFIER_MODEL_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_TEXT_SELECTION_MODEL_TEXT_CLASSIFIER_MODEL_SERVICE_FACTORY_H_
 
-#include <memory>
+#import <memory>
 
-#include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "base/no_destructor.h"
+#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
-class ChromeBrowserState;
 class TextClassifierModelService;
 
 // Singleton that owns all TextClassifierModelService(s) and associates them
