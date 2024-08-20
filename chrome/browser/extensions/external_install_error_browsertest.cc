07db9f42cf36ff352e7bfb9bef8acb7479ca58c2
Kelvin Jiang <kelvinjiang@chromium.org>
2024-08-20 17:22:11 +0000

    [Extensions] External install: only call webstore API for CWS extensions
    Chrome prompts users for installing extensions listed from a
    preferences file. However, this involves making a call to a CWS API
    to (attempt to) fetch the extension's metadata before showing the
    prompt.
    
    This API fetch is done for non-CWS extensions as well, which is not
    ideal since we shouldn't send any non-CWS extension IDs to a CWS API.
    
    This CL fixes that by just showing the install prompt instead of
    starting an API call for a non-CWS extension.
    
    Bug: 325314721
    Change-Id: Ibc13cefd79f9df47239d8726ce3c9a2934e1dce9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797252
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344243}

--- chrome/browser/extensions/external_install_error_browsertest.cc
+++ chrome/browser/extensions/external_install_error_browsertest.cc
@@ -52,6 +52,41 @@ class ExternalInstallErrorTest : public ExtensionBrowserTest {
         extensions_features::kUseItemSnippetsAPI);
   }
 
+ protected:
+  void InstallExternalExtension(const char* provided_extension_id,
+                                const std::string& version,
+                                const std::string& crx_path) {
+    ExtensionRegistry* registry = ExtensionRegistry::Get(profile());
+
+    {
+      // Wait for an external extension to be installed and a global error about
+      // it added.
+      test::GlobalErrorWaiter waiter(profile());
+      TestExtensionRegistryObserver observer(registry);
+
+      auto provider = std::make_unique<MockExternalProvider>(
+          extension_service(), mojom::ManifestLocation::kExternalPref);
+      provider->UpdateOrAddExtension(provided_extension_id, version,
+                                     test_data_dir_.AppendASCII(crx_path));
+      extension_service()->AddProviderForTesting(std::move(provider));
+      extension_service()->CheckForExternalUpdates();
+
+      auto extension = observer.WaitForExtensionInstalled();
+      EXPECT_EQ(extension->id(), provided_extension_id);
+
+      waiter.Wait();
+    }
+
+    // Verify the extension is in the expected state (disabled for being
+    // unacknowledged).
+    EXPECT_FALSE(
+        registry->enabled_extensions().Contains(provided_extension_id));
+    ExtensionPrefs* prefs = ExtensionPrefs::Get(profile());
+    EXPECT_FALSE(prefs->IsExternalExtensionAcknowledged(provided_extension_id));
+    EXPECT_EQ(disable_reason::DISABLE_EXTERNAL_EXTENSION,
+              prefs->GetDisableReasons(provided_extension_id));
+  }
+
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
 };
@@ -64,35 +99,8 @@ IN_PROC_BROWSER_TEST_F(ExternalInstallErrorTest, TestShutdown) {
   FeatureSwitch::ScopedOverride feature_override(
       FeatureSwitch::prompt_for_external_extensions(), true);
 
-  ExtensionRegistry* registry = ExtensionRegistry::Get(profile());
-
-  const char kId[] = "ldnnhddmnhbkjipkidpdiheffobcpfmf";
-  {
-    // Wait for an external extension to be installed and a global error about
-    // it added.
-    test::GlobalErrorWaiter waiter(profile());
-    TestExtensionRegistryObserver observer(registry);
-
-    auto provider = std::make_unique<MockExternalProvider>(
-        extension_service(), mojom::ManifestLocation::kExternalPref);
-    provider->UpdateOrAddExtension(kId, "1.0.0.0",
-                                   test_data_dir_.AppendASCII("good.crx"));
-    extension_service()->AddProviderForTesting(std::move(provider));
-    extension_service()->CheckForExternalUpdates();
-
-    auto extension = observer.WaitForExtensionInstalled();
-    EXPECT_EQ(extension->id(), kId);
-
-    waiter.Wait();
-  }
-
-  // Verify the extension is in the expected state (disabled for being
-  // unacknowledged).
-  EXPECT_FALSE(registry->enabled_extensions().Contains(kId));
-  ExtensionPrefs* prefs = ExtensionPrefs::Get(profile());
-  EXPECT_FALSE(prefs->IsExternalExtensionAcknowledged(kId));
-  EXPECT_EQ(disable_reason::DISABLE_EXTERNAL_EXTENSION,
-            prefs->GetDisableReasons(kId));
+  const char kId[] = "akjooamlhcgeopfifcmlggaebeocgokj";
+  InstallExternalExtension(kId, "1", "update_from_webstore.crx");
 
   // Verify the external error.
   ExternalInstallManager* manager =
@@ -119,41 +127,17 @@ class ExternalInstallErrorItemSnippetsTest : public ExternalInstallErrorTest {
 // Similar to the ExternalInstallErrorTest version of this test except
 // webstore data is fetched from the item snippets API (API response is mocked)
 // for this test.
-IN_PROC_BROWSER_TEST_F(ExternalInstallErrorItemSnippetsTest, TestShutdown) {
+IN_PROC_BROWSER_TEST_F(ExternalInstallErrorItemSnippetsTest,
+                       TestShutdownWithWebstoreExtension) {
   // This relies on prompting for external extensions.
   FeatureSwitch::ScopedOverride feature_override(
       FeatureSwitch::prompt_for_external_extensions(), true);
 
-  const char kId[] = "ldnnhddmnhbkjipkidpdiheffobcpfmf";
+  const char kId[] = "akjooamlhcgeopfifcmlggaebeocgokj";
   auto mock_response = CreateMockResponse(kId);
   WebstoreDataFetcher::SetMockItemSnippetReponseForTesting(mock_response.get());
 
-  ExtensionRegistry* registry = ExtensionRegistry::Get(profile());
-  {
-    // Wait for an external extension to be installed and a global error about
-    // it added.
-    test::GlobalErrorWaiter waiter(profile());
-    TestExtensionRegistryObserver observer(registry);
-
-    auto provider = std::make_unique<MockExternalProvider>(
-        extension_service(), mojom::ManifestLocation::kExternalPref);
-    provider->UpdateOrAddExtension(kId, "1.0.0.0",
-                                   test_data_dir_.AppendASCII("good.crx"));
-    extension_service()->AddProviderForTesting(std::move(provider));
-    extension_service()->CheckForExternalUpdates();
-
-    auto extension = observer.WaitForExtensionInstalled();
-    EXPECT_EQ(extension->id(), kId);
-    waiter.Wait();
-  }
-
-  // Verify the extension is in the expected state (disabled for being
-  // unacknowledged).
-  EXPECT_FALSE(registry->enabled_extensions().Contains(kId));
-  ExtensionPrefs* prefs = ExtensionPrefs::Get(profile());
-  EXPECT_FALSE(prefs->IsExternalExtensionAcknowledged(kId));
-  EXPECT_EQ(disable_reason::DISABLE_EXTERNAL_EXTENSION,
-            prefs->GetDisableReasons(kId));
+  InstallExternalExtension(kId, "1", "update_from_webstore.crx");
 
   // Verify the external error.
   ExternalInstallManager* manager =
@@ -172,6 +156,36 @@ IN_PROC_BROWSER_TEST_F(ExternalInstallErrorItemSnippetsTest, TestShutdown) {
   EXPECT_EQ(kMockRatingCountString, alert_prompt->localized_rating_count());
   EXPECT_EQ(kMockRatingCount, alert_prompt->rating_count());
   EXPECT_EQ(kMockAverageRating, alert_prompt->average_rating());
+}
+
+// Same as the above test except the extension does not update from the
+// webstore, so the prompt should not display any webstore data.
+IN_PROC_BROWSER_TEST_F(ExternalInstallErrorItemSnippetsTest,
+                       TestShutdownWithNonWebstoreExtension) {
+  // This relies on prompting for external extensions.
+  FeatureSwitch::ScopedOverride feature_override(
+      FeatureSwitch::prompt_for_external_extensions(), true);
+
+  const char kId[] = "ldnnhddmnhbkjipkidpdiheffobcpfmf";
+  auto mock_response = CreateMockResponse(kId);
+  WebstoreDataFetcher::SetMockItemSnippetReponseForTesting(mock_response.get());
+
+  InstallExternalExtension(kId, "1.0.0.0", "good.crx");
+
+  // Verify the external error.
+  ExternalInstallManager* manager =
+      extension_service()->external_install_manager();
+  std::vector<ExternalInstallError*> errors = manager->GetErrorsForTesting();
+  ASSERT_EQ(1u, errors.size());
+  EXPECT_EQ(kId, errors[0]->extension_id());
+
+  ExtensionInstallPrompt::Prompt* alert_prompt =
+      errors[0]->GetPromptForTesting();
+  ASSERT_TRUE(alert_prompt);
+
+  // Validate that the alert prompt's data corresponds to what is returned from
+  // the item snippets API.
+  EXPECT_FALSE(alert_prompt->has_webstore_data());
 }
 
 }  // namespace extensions
