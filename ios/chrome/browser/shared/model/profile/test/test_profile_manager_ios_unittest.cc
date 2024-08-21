dbdc08ad64ad7a4037ecd6bde0450ca53f514c8c
Mark Cogan <marq@google.com>
2024-08-21 17:29:46 +0000

    Rename and move TestChromeBrowserStateManagerTest
    Renames and moves the test, adds the new unit test target.
    
    
    Bug: 358061475, 358062318
    Change-Id: I6ec8476f79166d82af00065cf97eaa1bed3671c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797220
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344904}

--- ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios_unittest.cc
+++ ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios_unittest.cc
@@ -1,35 +1,31 @@
-// Copyright 2017 The Chromium Authors
+// Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state_manager.h"
+#include "ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios.h"
 
-#include <memory>
-
-#include "base/files/file_path.h"
 #include "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
 #include "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
 #include "ios/web/public/test/web_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
-using TestChromeBrowserStateManagerTest = PlatformTest;
+using TestProfileManagerIOSTest = PlatformTest;
 
 // Tests that the list of loaded browser states is empty after construction.
-TEST_F(TestChromeBrowserStateManagerTest, Constructor) {
+TEST_F(TestProfileManagerIOSTest, Constructor) {
   web::WebTaskEnvironment task_environment;
   IOSChromeScopedTestingLocalState scoped_testing_local_state;
-  TestChromeBrowserStateManager browser_state_manager;
-  EXPECT_EQ(0U, browser_state_manager.GetLoadedBrowserStates().size());
+  TestProfileManagerIOS profile_manager;
+  EXPECT_EQ(0U, profile_manager.GetLoadedBrowserStates().size());
 }
 
 // Tests that the list of loaded browser states has one element after calling
 // AddBrowserStateWithBuilder(...).
-TEST_F(TestChromeBrowserStateManagerTest, AddBrowserStateWithBuilder) {
+TEST_F(TestProfileManagerIOSTest, AddBrowserStateWithBuilder) {
   web::WebTaskEnvironment task_environment;
   IOSChromeScopedTestingLocalState scoped_testing_local_state;
-  TestChromeBrowserStateManager browser_state_manager;
-  browser_state_manager.AddBrowserStateWithBuilder(
-      TestChromeBrowserState::Builder());
-  EXPECT_EQ(1U, browser_state_manager.GetLoadedBrowserStates().size());
+  TestProfileManagerIOS profile_manager;
+  profile_manager.AddBrowserStateWithBuilder(TestChromeBrowserState::Builder());
+  EXPECT_EQ(1U, profile_manager.GetLoadedBrowserStates().size());
 }
