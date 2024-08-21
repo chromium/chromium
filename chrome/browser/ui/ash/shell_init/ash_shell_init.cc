58037dbc2bfaf3349bdeed71865c557388188811
Eriko Kurimoto <elkurin@chromium.org>
2024-08-21 03:08:54 +0000

    ash: Create //chrome/browser/ui/ash/shell_init
    Bug: b:358243034
    Change-Id: Id5f7159f37771330f5af1869ec0d922e39c7dd87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796324
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344566}

--- chrome/browser/ui/ash/shell_init/ash_shell_init.cc
+++ chrome/browser/ui/ash/shell_init/ash_shell_init.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/ash/ash_shell_init.h"
+#include "chrome/browser/ui/ash/shell_init/ash_shell_init.h"
 
 #include <utility>
 
--- chrome/browser/ui/ash/shell_init/ash_shell_init.h
+++ chrome/browser/ui/ash/shell_init/ash_shell_init.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_ASH_SHELL_INIT_H_
-#define CHROME_BROWSER_UI_ASH_ASH_SHELL_INIT_H_
+#ifndef CHROME_BROWSER_UI_ASH_SHELL_INIT_ASH_SHELL_INIT_H_
+#define CHROME_BROWSER_UI_ASH_SHELL_INIT_ASH_SHELL_INIT_H_
 
 // Initializes and destroys the ash::Shell when Ash is running in process.
 class AshShellInit {
@@ -16,4 +16,4 @@ class AshShellInit {
   ~AshShellInit();
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_ASH_SHELL_INIT_H_
+#endif  // CHROME_BROWSER_UI_ASH_SHELL_INIT_ASH_SHELL_INIT_H_
