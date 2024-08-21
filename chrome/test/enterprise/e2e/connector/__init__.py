f6a3ec701ec63416f5c129d59c77d6331563d58f
Xiang Ji <jxiang@google.com>
2024-08-21 21:43:05 +0000

    Disable Splunk connector test until the license is renewed
    Bug: b:361382502
    Change-Id: Ib4b7b63d04c5ff6f34055cec1269109fd8401e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803697
    Reviewed-by: Kuan Huang <kuanhuang@chromium.org>
    Commit-Queue: Xiang Ji <jxiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345073}

--- chrome/test/enterprise/e2e/connector/__init__.py
+++ chrome/test/enterprise/e2e/connector/__init__.py
@@ -16,4 +16,5 @@ from .reporting_connector_combined.reporting_connector_combined_test import *
 from .reporting_connector_crowdstrike.reporting_connector_crowdstrike_test import *
 from .reporting_connector_pan.reporting_connector_pan_test import *
 from .reporting_connector_pubsub.reporting_connector_pubsub_test import *
-from .reporting_connector_splunk.reporting_connector_splunk_test import *
+# TODO(b/361382502): re-enable once the splunk license is renewed
+# from .reporting_connector_splunk.reporting_connector_splunk_test import *
