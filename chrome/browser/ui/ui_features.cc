2e468fa74a29e8a44022516538a3824be9dde19d
Muyao Xu <muyaoxu@google.com>
2024-08-22 19:36:20 +0000

    [FS Shortcut] Enable the feature on ToT
    Bug: 330597325
    Change-Id: I1a77ed2b06bb056959e76a9515f2bcbc84373002
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807318
    Reviewed-by: Ahmed Moussa <ahmedmoussa@google.com>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345589}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -209,7 +209,7 @@ const base::FeatureParam<bool> kPreloadTopChromeWebUIDelayPreload{
 #if !BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kPressAndHoldEscToExitBrowserFullscreen,
              "PressAndHoldEscToExitBrowserFullscreen",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 // Enable responsive toolbar. Toolbar buttons overflow to a chevron button when
