d69a7ac7124b9e91aaf130899caa6ad282970d0c
Takumi Fujimoto <takumif@chromium.org>
2024-08-26 13:20:33 +0000

    Disable CaptionButtonHitTest for Windows ASAN
    It looks like the previous CL to disable it meant to disable it for
    ASAN builds but did it for debug builds instead.
    
    Bug: 361780162
    Change-Id: Id15c6643d26031f49b8865cc9fdcde2249333f9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803892
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346687}

--- chrome/browser/ui/views/frame/browser_frame_view_browsertest_win.cc
+++ chrome/browser/ui/views/frame/browser_frame_view_browsertest_win.cc
@@ -417,8 +417,8 @@ IN_PROC_BROWSER_TEST_F(WebAppBrowserFrameViewWinWindowControlsOverlayTest,
   EXPECT_EQ(close_button->GetTooltipText(), u"");
 }
 
-#if BUILDFLAG(IS_WIN) && !defined(NDEBUG)
 // TODO(crbug.com/361780162): This test has been flaky on Windows ASan testers.
+#if BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)
 #define MAYBE_CaptionButtonHitTest DISABLED_CaptionButtonHitTest
 #else
 #define MAYBE_CaptionButtonHitTest CaptionButtonHitTest
