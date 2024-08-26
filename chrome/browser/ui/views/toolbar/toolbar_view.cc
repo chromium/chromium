f4309ce06d76172bb944f270b3750d3f9657224e
Joseph Park <josephjoopark@chromium.org>
2024-08-26 19:16:44 +0000

    [M128][toolbarpinning] Return correct anchor view for page actions on minimal toolbar browsers
    There exist browsers with minimal toolbars in which the pinned actions
    container does not exist. If it does not exist, we return the default
    anchor (location bar).
    
    (cherry picked from commit e4b97efc02f20289604698f6b00a80e89fcefd5c)
    
    Bug: 358542671
    Change-Id: Ic0eae816999f82005e0ff33e14dba26ce7d83328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787019
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Auto-Submit: Joseph Park <josephjoopark@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1341661}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807212
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6613@{#1408}
    Cr-Branched-From: 03c1799e6f9c7239802827eab5e935b9e14fceae-refs/heads/main@{#1331488}

--- chrome/browser/ui/views/toolbar/toolbar_view.cc
+++ chrome/browser/ui/views/toolbar/toolbar_view.cc
@@ -1148,7 +1148,7 @@ views::AccessiblePaneView* ToolbarView::GetAsAccessiblePaneView() {
 views::View* ToolbarView::GetAnchorView(
     std::optional<PageActionIconType> type) {
   if (features::IsToolbarPinningEnabled()) {
-    if (type.has_value()) {
+    if (pinned_toolbar_actions_container_ && type.has_value()) {
       const std::optional<actions::ActionId> action_id =
           GetPageActionIconView(type.value())->action_id();
       if (action_id.has_value() &&
