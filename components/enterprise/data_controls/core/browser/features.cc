b2358477f93a7f523fd9c2827531e37cc23a8e72
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-08-26 18:18:07 +0000

    Make kEnableDesktopDataControls enabled by default
    Change-Id: Ibcc3b6a98abf227d19783e97ac2a73a7d371b3fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5811706
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1346853}

--- components/enterprise/data_controls/core/browser/features.cc
+++ components/enterprise/data_controls/core/browser/features.cc
@@ -8,7 +8,7 @@ namespace data_controls {
 
 BASE_FEATURE(kEnableDesktopDataControls,
              "EnableDesktopDataControls",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableScreenshotProtection,
              "EnableScreenshotProtection",
