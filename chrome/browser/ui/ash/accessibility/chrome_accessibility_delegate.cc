acf0185867d3b8c44be8da2ee6ad7c910edbb3d3
Georg Neis <neis@chromium.org>
2024-08-26 03:08:42 +0000

    ash: Move c/b/ui/ash/chrome_accessibility_delegate*
    ... to c/b/ui/ash/accessibility/.
    
    AX-Relnotes: n/a.
    Bug: b:358244102
    Change-Id: I76080835b9fbbd687a60cd8a5a250eaee0a50f52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803382
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346512}

--- chrome/browser/ui/ash/accessibility/chrome_accessibility_delegate.cc
+++ chrome/browser/ui/ash/accessibility/chrome_accessibility_delegate.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/ash/chrome_accessibility_delegate.h"
+#include "chrome/browser/ui/ash/accessibility/chrome_accessibility_delegate.h"
 
 #include <limits>
 
--- chrome/browser/ui/ash/accessibility/chrome_accessibility_delegate.h
+++ chrome/browser/ui/ash/accessibility/chrome_accessibility_delegate.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_CHROME_ACCESSIBILITY_DELEGATE_H_
-#define CHROME_BROWSER_UI_ASH_CHROME_ACCESSIBILITY_DELEGATE_H_
+#ifndef CHROME_BROWSER_UI_ASH_ACCESSIBILITY_CHROME_ACCESSIBILITY_DELEGATE_H_
+#define CHROME_BROWSER_UI_ASH_ACCESSIBILITY_CHROME_ACCESSIBILITY_DELEGATE_H_
 
 #include "ash/accessibility/accessibility_delegate.h"
 
@@ -26,4 +26,4 @@ class ChromeAccessibilityDelegate : public ash::AccessibilityDelegate {
   double GetSavedScreenMagnifierScale() override;
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_CHROME_ACCESSIBILITY_DELEGATE_H_
+#endif  // CHROME_BROWSER_UI_ASH_ACCESSIBILITY_CHROME_ACCESSIBILITY_DELEGATE_H_
