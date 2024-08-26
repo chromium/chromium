5a6420522280a36f0717c09d2e59714ed8490bc7
Zelin Liu <zelin@chromium.org>
2024-08-26 21:58:11 +0000

    controlledframe: Add test for postMessage with newwindow API
    Change-Id: Id4df6b6ffc43c66110d55be536111c024c96d158
    Bug: b/349853760 b/40191772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796474
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346994}

--- chrome/browser/controlled_frame/controlled_frame_new_window_browsertest.cc
+++ chrome/browser/controlled_frame/controlled_frame_new_window_browsertest.cc
@@ -62,7 +62,7 @@ IN_PROC_BROWSER_TEST_F(ControlledFrameNewWindowBrowserTest, AttachSucceeds) {
     });
 
     const frames = document.getElementsByTagName('controlledframe');
-    if (frames.length != 2) {
+    if (frames.length !== 2) {
       return [
         'FAIL: expected 2 <controlledframe> elements, found ' + frames.length
       ];
@@ -121,7 +121,7 @@ IN_PROC_BROWSER_TEST_F(ControlledFrameNewWindowBrowserTest, DiscardSucceeds) {
       });
       frame.executeScript({code: 'window.open($1);'});
     });
-  } catch (err){
+  } catch (err) {
     return "FAIL: " + err.message;
   }
 })();
@@ -133,6 +133,113 @@ IN_PROC_BROWSER_TEST_F(ControlledFrameNewWindowBrowserTest, DiscardSucceeds) {
   EXPECT_EQ(1, content::EvalJs(
                    app_frame,
                    "document.getElementsByTagName('controlledframe').length;"));
+}
+
+IN_PROC_BROWSER_TEST_F(ControlledFrameNewWindowBrowserTest,
+                       PostMessageAfterAttachSucceeds) {
+  auto [app_frame, controlled_frame] =
+      InstallAndOpenIwaThenCreateControlledFrame(
+          /*controlled_frame_host_name=*/std::nullopt,
+          "/controlled_frame.html");
+
+  auto test_script = content::JsReplace(
+      R"(
+async function executeScriptOnFrame(frame, script) {
+  return await new Promise((resolve) => {
+    frame.executeScript({code: script}, (result) => {
+      if (!result) {
+        throw new Error('executeScript returned no result');
+      }
+      if (result[0] !== 'SUCCESS') {
+        throw new Error('expected SUCCESS but got ' + result[0]);
+      }
+      resolve('SUCCESS');
+    });
+  });
+};
+
+(async function() {
+  try {
+    await new Promise((resolve, reject) => {
+      const frame = document.getElementsByTagName('controlledframe')[0];
+      if (!frame) {
+        reject('Could not find a controlledframe element.');
+      }
+
+      frame.addEventListener('newwindow', (e) => {
+        if (!e.window.attach) {
+          reject('window.attach does not exist ');
+        }
+        const newcontrolledframe = document.createElement('controlledframe');
+        // Attach the new window to the new <controlledframe>.
+        try {
+          newcontrolledframe.addEventListener('loadstop', resolve);
+          newcontrolledframe.addEventListener('loadabort', reject);
+          e.window.attach(newcontrolledframe);
+          document.body.appendChild(newcontrolledframe);
+        } catch (err) {
+          reject(err.message);
+        }
+      });
+      frame.executeScript({code: 'document.openedWindow = window.open($1);'});
+    });
+
+    const listenscript = `
+      (function() {
+        window.addEventListener('message', (e) => {document.lastMessage = e;});
+        return 'SUCCESS';
+      })();
+    `;
+    const sendscript = `
+      (function() {
+        const target = document.openedWindow || window.opener;
+        if (target === null) {
+          return 'missing postMessage target';
+        }
+        target.postMessage('hello test');
+        return 'SUCCESS';
+      })();
+    `;
+    const verifyscript = `
+      (function() {
+        if (!document.lastMessage) {
+          return 'no message received';
+        }
+        if (document.lastMessage.data !== 'hello test') {
+          return 'unexpected lastMessage\\nexpected: hello test\\nactual: ' +
+              document.lastMessage.data;
+        }
+        return 'SUCCESS';
+      })();
+    `;
+
+    const frames = Array.from(document.getElementsByTagName('controlledframe'));
+    if (frames.length !== 2) {
+      throw new Error(
+          'expected 2 <controlledframe> elements, found ' + frames.length);
+    }
+    for (const frame of frames) {
+      await executeScriptOnFrame(frame, listenscript);
+    }
+    for (const frame of frames) {
+      await executeScriptOnFrame(frame, sendscript);
+    }
+    // Trigger resolve() in 0.1s after sending out messages.
+    await new Promise((resolve) => {
+      setTimeout(resolve, 100);
+    });
+    for (const frame of frames) {
+      await executeScriptOnFrame(frame, verifyscript);
+    }
+    return 'SUCCESS';
+  } catch (e) {
+    return 'FAIL: ' + e.message;
+  }
+})();
+    )",
+      embedded_https_test_server().GetURL("/index.html"));
+
+  EXPECT_EQ("SUCCESS", content::EvalJs(app_frame, test_script));
 }
 
 }  // namespace controlled_frame
