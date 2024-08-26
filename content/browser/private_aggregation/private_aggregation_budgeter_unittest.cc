5b1284becb825bac9909590e009c2a21e5f69a5f
Dan McArdle <dmcardle@chromium.org>
2024-08-26 15:09:20 +0000

    content/browser/private_aggregation: Fix typoed arg comments
    This CL fixes typoes of the form /*arg-*/ that should have been /*arg=*/
    under the assumption that they were trying to name an argument.
    
    This CL was uploaded by git cl split.
    
    R=alexmt@chromium.org
    
    Bug: 362092187
    Change-Id: I926951805e50df94b39428798b572a192eca0ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5813182
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346734}

--- content/browser/private_aggregation/private_aggregation_budgeter_unittest.cc
+++ content/browser/private_aggregation/private_aggregation_budgeter_unittest.cc
@@ -208,7 +208,7 @@ class PrivateAggregationBudgeterTest : public testing::Test {
     return PrivateAggregationBudgetKey::CreateForTesting(
         /*origin=*/url::Origin::Create(GURL("https://a.example/")),
         /*api_invocation_time=*/kExampleTime,
-        /*api-*/ PrivateAggregationBudgetKey::Api::kProtectedAudience);
+        /*api=*/PrivateAggregationBudgetKey::Api::kProtectedAudience);
   }
 
   base::FilePath db_path() const {
@@ -2091,7 +2091,7 @@ TEST_F(PrivateAggregationBudgeterTest, StaleDataClearedAfterConsumeBudget) {
       PrivateAggregationBudgetKey::CreateForTesting(
           /*origin=*/url::Origin::Create(GURL("https://b.example/")),
           /*api_invocation_time=*/kExampleTime,
-          /*api-*/ PrivateAggregationBudgetKey::Api::kProtectedAudience);
+          /*api=*/PrivateAggregationBudgetKey::Api::kProtectedAudience);
   base::RunLoop run_loop;
   budgeter()->ConsumeBudget(
       /*budget=*/1, non_stale_key,
