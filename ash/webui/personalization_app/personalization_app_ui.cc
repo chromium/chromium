32ff90c103468f2822ea3222ff411efaccc8d3ee
Thuong Phan <thuongphan@chromium.org>
2024-08-27 19:12:59 +0000

    p13n: update managed icon description for Sea Pen tile
    Update the description for the managed icon of Sea Pen tile to "This
    feature is managed by your administrators".
    
    Bug: b/361819116
    Test: http://screen/BHc5CtDvbPhC6NY
    Change-Id: Ieda45975afecdf19f8cf86216e6c3832f524e85e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5813466
    Reviewed-by: Erica Lee <ericamlee@google.com>
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347533}

--- ash/webui/personalization_app/personalization_app_ui.cc
+++ ash/webui/personalization_app/personalization_app_ui.cc
@@ -126,6 +126,7 @@ void AddStrings(content::WebUIDataSource* source) {
       {"zeroImages", IDS_PERSONALIZATION_APP_NO_IMAGES},
       {"oneImage", IDS_PERSONALIZATION_APP_ONE_IMAGE},
       {"multipleImages", IDS_PERSONALIZATION_APP_MULTIPLE_IMAGES},
+      {"managedFeature", IDS_PERSONALIZATION_APP_MANAGED_FEATURE},
       {"managedSetting", IDS_PERSONALIZATION_APP_MANAGED_SETTING},
       {"ariaLabelChangeWallpaper",
        IDS_PERSONALIZATION_APP_ARIA_LABEL_CHANGE_WALLPAPER},
