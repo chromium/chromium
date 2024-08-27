f57950dbffe0b274d106ac92735539e505a2f0c2
Anthony Cui <cuianthony@google.com>
2024-08-27 16:42:00 +0000

    [Compose] Enable ComposeUiRefinement feature by default
    Bug: b/333550933
    Change-Id: Iffde8f340d3e45cc24e70f96f168692493f45049
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812174
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347432}

--- components/compose/core/browser/compose_features.cc
+++ components/compose/core/browser/compose_features.cc
@@ -82,7 +82,7 @@ BASE_FEATURE(kComposeRequestLatencyTimeout,
 
 BASE_FEATURE(kComposeUiRefinement,
              "ComposeUiRefinement",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableNudgeForUnspecifiedHint,
              "ComposeEnableNudgeForUnspecifiedHint",
