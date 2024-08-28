869872fcabba99e99dce5138f34d96531005c98c
Seung Jae Lim <andysjlim@chromium.org>
2024-08-28 21:06:25 +0000

    Enable the study kClientSideDetectionVibrationApi by default
    Bug: 331230508
    Change-Id: Ia0c0e5628923f8892a9da23737dd9574d9f5ef3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823857
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348272}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -49,7 +49,7 @@ BASE_FEATURE(kClientSideDetectionSamplePing,
 
 BASE_FEATURE(kClientSideDetectionVibrationApi,
              "ClientSideDetectionVibrationApi",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCreateNotificationsAcceptedClientSafeBrowsingReports,
              "CreateNotificationsAcceptedClientSafeBrowsingReports",
