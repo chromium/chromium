e12e9e58d596c812048a6a097956b8d663baddb5
Irem Uguz <iremuguz@google.com>
2024-08-28 09:26:20 +0000

    Reenable SupportToolTest.UrlGeneratorTest.'token generation success'
    The test was disabled due to flakiness in the attached bug. The possible
    cause of the flakiness is that the test is checking the clipboard
    contents before the token is copied. Resolve the flakiness by polling
    for the toast message to open for 3 seconds. Will observe the test
    stability after this fix.
    
    Bug: b:349562679
    Change-Id: Iecd519d0c1601e0cfdce213e836d3dee2375188a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5821054
    Reviewed-by: Andrei Nedoluzhko <nedol@google.com>
    Commit-Queue: Irem Uguz <iremuguz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347898}

--- chrome/test/data/webui/support_tool/support_tool_test.ts
+++ chrome/test/data/webui/support_tool/support_tool_test.ts
@@ -13,6 +13,7 @@ import 'chrome://support-tool/url_generator.js';
 import type {CrButtonElement} from 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import type {CrCheckboxElement} from 'chrome://resources/cr_elements/cr_checkbox/cr_checkbox.js';
 import type {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
+import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
@@ -177,6 +178,37 @@ class TestSupportToolBrowserProxy extends TestBrowserProxy implements
   }
 }
 
+// Waits until `toast` is opened. Waits for 3 seconds and polls every second by
+// default. Resolves to `true` if toast is opened. Returns error otherwise.
+async function waitForToastOpened(
+    toast: CrToastElement, intervalMs: number = 100,
+    timeoutMs: number = 301): Promise<NonNullable<boolean>> {
+  let rejectTimer: number|null = null;
+  let pollTimer: number|null = null;
+
+  function cleanup() {
+    if (rejectTimer) {
+      window.clearTimeout(rejectTimer);
+    }
+    if (pollTimer) {
+      window.clearInterval(pollTimer);
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    rejectTimer = window.setTimeout(() => {
+      cleanup();
+      reject(new Error('Toast element did not get opened on time'));
+    }, timeoutMs);
+
+    pollTimer = window.setInterval(() => {
+      if (toast.open) {
+        cleanup();
+        resolve(true);
+      }
+    }, intervalMs);
+  });
+}
 
 suite('SupportToolTest', function() {
   let supportTool: SupportToolElement;
@@ -428,6 +460,8 @@ suite('UrlGeneratorTest', function() {
     // Click the button to generate URL and copy to clipboard.
     copyLinkButton.click();
     await browserProxy.whenCalled('generateCustomizedUrl');
+    // A toast message needs to be opened when the copy link button is clicked.
+    assertTrue(await waitForToastOpened(urlGenerator.$.copyToast));
     // Check the URL value copied to clipboard if it's as expected.
     const copiedToken = await navigator.clipboard.readText();
     assertEquals(copiedToken, expectedToken);
@@ -450,11 +484,10 @@ suite('UrlGeneratorTest', function() {
     copyLinkButton!.click();
     await browserProxy.whenCalled('generateCustomizedUrl');
     // Check that there's an error message shown to user.
-    assertTrue(urlGenerator.$.errorMessageToast.open);
+    assertTrue(await waitForToastOpened(urlGenerator.$.errorMessageToast));
   });
 
-  // TODO(crbug.com/349562679): Re-enable test.
-  test.skip('token generation success', async () => {
+  test('token generation success', async () => {
     // Ensure the button is disabled when we open the page.
     const copyTokenButton = urlGenerator.shadowRoot!.getElementById(
                                 'copyTokenButton')! as CrButtonElement;
@@ -478,6 +511,8 @@ suite('UrlGeneratorTest', function() {
     // Click the button to generate URL and copy to clipboard.
     copyTokenButton.click();
     await browserProxy.whenCalled('generateSupportToken');
+    // A toast message needs to be opened when the copy token button is clicked.
+    assertTrue(await waitForToastOpened(urlGenerator.$.copyToast));
     // Check the token value copied to clipboard if it's as expected.
     const copiedToken = await navigator.clipboard.readText();
     assertEquals(copiedToken, expectedToken);
