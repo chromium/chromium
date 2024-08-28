f5690c1d01f2d0c1ca061dd23c49c6c4b07aa3d5
Danil Chapovalov <danilchap@chromium.org>
2024-08-28 10:57:49 +0000

    Roll abseil_revision 5a01d0f77e..40a975ff1e
    Change Log:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+log/5a01d0f77e..40a975ff1e
    Full diff:
    https://chromium.googlesource.com/external/github.com/abseil/abseil-cpp/+/5a01d0f77e..40a975ff1e
    
    Bug: None
    Change-Id: Ib48dd489ef6672c9d99c9e284e201e0573f1f41c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5821931
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Danil Chapovalov <danilchap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347926}

--- third_party/abseil-cpp/absl/base/attributes.h
+++ third_party/abseil-cpp/absl/base/attributes.h
@@ -844,15 +844,20 @@
 //     return s;  // warning: address of stack memory returned
 //   }
 //
+// We disable this on Clang versions < 13 because of the following
+// false-positive:
+//
+//   absl::string_view f(absl::optional<absl::string_view> sv) { return *sv; }
+//
 // See the following links for details:
 // https://reviews.llvm.org/D64448
 // https://lists.llvm.org/pipermail/cfe-dev/2018-November/060355.html
-#if ABSL_HAVE_CPP_ATTRIBUTE(gsl::Pointer)
+#if ABSL_HAVE_CPP_ATTRIBUTE(gsl::Pointer) && \
+    (!defined(__clang_major__) || __clang_major__ >= 13)
 #define ABSL_ATTRIBUTE_VIEW [[gsl::Pointer]]
 #else
 #define ABSL_ATTRIBUTE_VIEW
 #endif
-#define ABSL_INTERNAL_ATTRIBUTE_VIEW ABSL_ATTRIBUTE_VIEW  // Deprecated
 
 // ABSL_ATTRIBUTE_OWNER indicates that a type is a container, smart pointer, or
 // similar class that owns all the data that it points to.
@@ -870,15 +875,20 @@
 //     return s;  // warning: address of stack memory returned
 //   }
 //
+// We disable this on Clang versions < 13 because of the following
+// false-positive:
+//
+//   absl::string_view f(absl::optional<absl::string_view> sv) { return *sv; }
+//
 // See the following links for details:
 // https://reviews.llvm.org/D64448
 // https://lists.llvm.org/pipermail/cfe-dev/2018-November/060355.html
-#if ABSL_HAVE_CPP_ATTRIBUTE(gsl::Owner)
+#if ABSL_HAVE_CPP_ATTRIBUTE(gsl::Owner) && \
+    (!defined(__clang_major__) || __clang_major__ >= 13)
 #define ABSL_ATTRIBUTE_OWNER [[gsl::Owner]]
 #else
 #define ABSL_ATTRIBUTE_OWNER
 #endif
-#define ABSL_INTERNAL_ATTRIBUTE_OWNER ABSL_ATTRIBUTE_OWNER  // Deprecated
 
 // ABSL_ATTRIBUTE_TRIVIAL_ABI
 // Indicates that a type is "trivially relocatable" -- meaning it can be
