9f1637d7bb755f08ca4e75f784faf1a590350b1a
Andrew Grieve <agrieve@chromium.org>
2024-08-29 14:37:58 +0000

    android_deps: Fix DEPS generation of jakarta_inject_jakarta_inject_api
    It was resolving to 2.0.1.MR, but gradle resolved to 2.0.1
    
    Bug: 362735859
    Change-Id: I62eb9816fd09dfdc1397199e89d7a0882ca2aee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5825928
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348621}

--- third_party/android_deps/libs/jakarta_inject_jakarta_inject_api/3pp/fetch.py
+++ third_party/android_deps/libs/jakarta_inject_jakarta_inject_api/3pp/fetch.py
@@ -20,7 +20,7 @@ SPEC = fetch_common.Spec(repo_url=_REPO_URL,
                          file_ext='jar',
                          patch_version='cr1',
                          version_override=None,
-                         version_filter=None)
+                         version_filter=r'\d+\.\d+\.\d+$')
 
 
 if __name__ == '__main__':
