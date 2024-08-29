f0f4981095acd0153c48c1d6ee7a136412a5d5d7
Kene Okoye <kenok@google.com>
2024-08-29 16:04:12 +0000

    [PS] Privacy Guide: Conditionally show Ad Topics description on completion screen
    Bug: b:361816872
    Change-Id: I9f38cce73a2e7070d34bcd8d05f77aac054e0591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805153
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Kene Okoye <kenok@google.com>
    Cr-Commit-Position: refs/heads/main@{#1348669}

--- chrome/browser/privacy_guide/android/junit/src/org/chromium/chrome/browser/privacy_guide/DoneFragmentTest.java
+++ chrome/browser/privacy_guide/android/junit/src/org/chromium/chrome/browser/privacy_guide/DoneFragmentTest.java
@@ -4,17 +4,20 @@
 
 package org.chromium.chrome.browser.privacy_guide;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentFactory;
 import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.core.app.ApplicationProvider;
 
 import org.junit.After;
 import org.junit.Before;
@@ -26,7 +29,10 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Features.DisableFeatures;
+import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.privacy_sandbox.PrivacySandboxBridge;
 import org.chromium.chrome.browser.privacy_sandbox.PrivacySandboxBridgeJni;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -36,6 +42,7 @@ import org.chromium.components.signin.identitymanager.IdentityManager;
 
 /** Tests for {@link DoneFragment} */
 @RunWith(BaseRobolectricTestRunner.class)
+@EnableFeatures({ChromeFeatureList.PRIVACY_SANDBOX_PRIVACY_GUIDE_AD_TOPICS})
 public class DoneFragmentTest {
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
     @Rule public JniMocker mMocker = new JniMocker();
@@ -49,6 +56,7 @@ public class DoneFragmentTest {
     private DoneFragment mFragment;
     private View mPsButton;
     private View mWaaButton;
+    private String mPrivacySandboxDescriptionText;
 
     private void initFragment() {
         mScenario =
@@ -73,6 +81,10 @@ public class DoneFragmentTest {
                     mFragment = (DoneFragment) fragment;
                     mPsButton = fragment.getView().findViewById(R.id.ps_button);
                     mWaaButton = fragment.getView().findViewById(R.id.waa_button);
+                    mPrivacySandboxDescriptionText =
+                            ((TextView) fragment.getView().findViewById(R.id.ps_description))
+                                    .getText()
+                                    .toString();
                 });
     }
 
@@ -146,5 +158,30 @@ public class DoneFragmentTest {
         initFragment();
 
         assertFalse(mWaaButton.isShown());
+    }
+
+    @Test
+    public void testPrivacySandboxDescriptionForAdTopicsIsDisplayedWhenAdTopicsIsEnabled() {
+        setPrivacySandboxState(false, false);
+        initFragment();
+
+        String privacySandboxDescriptionAdTopicsString =
+                ApplicationProvider.getApplicationContext()
+                        .getResources()
+                        .getString(R.string.privacy_guide_privacy_sandbox_description_ad_topics);
+        assertEquals(mPrivacySandboxDescriptionText, privacySandboxDescriptionAdTopicsString);
+    }
+
+    @Test
+    @DisableFeatures({ChromeFeatureList.PRIVACY_SANDBOX_PRIVACY_GUIDE_AD_TOPICS})
+    public void testPrivacySandboxDescriptionIsDisplayedWhenAdTopicsIsDisabled() {
+        setPrivacySandboxState(false, false);
+        initFragment();
+
+        String privacySandboxDescriptionString =
+                ApplicationProvider.getApplicationContext()
+                        .getResources()
+                        .getString(R.string.privacy_guide_privacy_sandbox_description);
+        assertEquals(mPrivacySandboxDescriptionText, privacySandboxDescriptionString);
     }
 }
