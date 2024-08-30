81a082348b6e2d7d04007f6bdfcc463475e49119
vikas soni <vikassoni@chromium.org>
2024-08-30 19:44:37 +0000

    [SIPool] SharedImagePool apis, implementation and unittests.
    1. Add and implement SharedImagePool apis.
    2. Add unittests to test it.
    
    Bug: 361128461
    Change-Id: Icc5616ef57ecf25824b51ab2952042645fc98686
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824265
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349379}

---
+++ gpu/command_buffer/client/shared_image_pool.cc
@@ -0,0 +1,122 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/command_buffer/client/shared_image_pool.h"
+
+#include "gpu/command_buffer/client/shared_image_interface.h"
+
+namespace {
+
+gpu::ImageInfo GetImageInfo(scoped_refptr<gpu::ClientImage> image) {
+  auto shared_image = image->GetSharedImage();
+  return gpu::ImageInfo(shared_image->size(), shared_image->format(),
+                        shared_image->usage());
+}
+
+}  // namespace
+
+namespace gpu {
+
+// Implementation of the ClientImage class.
+ClientImage::ClientImage(scoped_refptr<ClientSharedImage> shared_image)
+    : shared_image_(std::move(shared_image)),
+      sync_token_(shared_image_->creation_sync_token()) {}
+
+ClientImage::~ClientImage() = default;
+
+scoped_refptr<ClientSharedImage> ClientImage::GetSharedImage() const {
+  return shared_image_;
+}
+
+const SyncToken& ClientImage::GetSyncToken() const {
+  return sync_token_;
+}
+
+SharedImagePoolBase::SharedImagePoolBase(
+    const ImageInfo& image_info,
+    const scoped_refptr<SharedImageInterface> sii,
+    std::optional<uint8_t> max_pool_size)
+    : image_info_(image_info),
+      sii_(std::move(sii)),
+      max_pool_size_(std::move(max_pool_size)) {}
+
+SharedImagePoolBase::~SharedImagePoolBase() {
+  ClearInternal();
+}
+
+size_t SharedImagePoolBase::GetPoolSizeForTesting() const {
+  return image_pool_.size();
+}
+
+scoped_refptr<ClientSharedImage>
+SharedImagePoolBase::CreateSharedImageInternal() {
+  CHECK(sii_);
+  return sii_->CreateSharedImage(
+      {image_info_.format, image_info_.size, image_info_.color_space,
+       image_info_.surface_origin, image_info_.alpha_type, image_info_.usage,
+       "SharedImagePoolBase"},
+      gpu::kNullSurfaceHandle);
+}
+
+scoped_refptr<ClientImage> SharedImagePoolBase::GetImageFromPoolInternal() {
+  if (!image_pool_.empty()) {
+    auto image = image_pool_.back();
+    image_pool_.pop_back();
+    return image;
+  }
+  return nullptr;
+}
+
+void SharedImagePoolBase::ReleaseImageInternal(scoped_refptr<ClientImage> image,
+                                               SyncToken release_sync_token) {
+  if (!image || (GetImageInfo(image) != image_info_)) {
+    return;
+  }
+  // Ensure that there is only one reference which the current |image| and
+  // clients are not accidentally keeping more references alive while releasing
+  // this |image|.
+  CHECK(image->HasOneRef());
+
+  // Ensure proper cleanup of the image if the pool is full.
+  if (max_pool_size_.has_value() &&
+      image_pool_.size() >= max_pool_size_.value()) {
+    // Pool is now full, so delete the image and its underlying shared image.
+    // We only need to update the shared image with destruction sync token.
+    auto shared_image = image->GetSharedImage();
+    CHECK(shared_image);
+    shared_image->UpdateDestructionSyncToken(std::move(release_sync_token));
+  } else {
+    // If the image is being recycled for reuse, we should ensure that previous
+    // user has finished using it before it is re-used. This is done by setting
+    // the |image->sync_token| to |release_sync_token| as
+    // the new user waits on the |image->sync_token| before using the
+    // |image|.
+    image->sync_token_ = std::move(release_sync_token);
+    image_pool_.push_back(std::move(image));
+  }
+}
+
+void SharedImagePoolBase::ClearInternal() {
+  // Explicitly clear the pool and delete all images.
+  for (auto& image : image_pool_) {
+    auto shared_image = image->GetSharedImage();
+    CHECK(shared_image);
+
+    // Note that |sync_token_| has to be waited upon before the image
+    // is re-used or deleted to ensure that previous user has finished using it.
+    shared_image->UpdateDestructionSyncToken(image->sync_token_);
+  }
+  image_pool_.clear();
+}
+
+void SharedImagePoolBase::ReconfigureInternal(const ImageInfo& image_info) {
+  if (image_info_ == image_info) {
+    return;
+  }
+  // If ImageInfo does not matches, we clear the existing images.
+  ClearInternal();
+  image_info_ = image_info;
+}
+
+}  // namespace gpu
