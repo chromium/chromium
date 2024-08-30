44c51eb4baeb64e476300911eec0cf7844370848
Eriko Kurimoto <elkurin@chromium.org>
2024-08-30 08:27:45 +0000

    ash: Move //chrome/browser/ui/ash/image_downloader to non-UI
    Bug: b:362392737
    Change-Id: Ia796f634ba535b21ead8e44b022f448a38b9ad42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5821334
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349083}

--- chrome/browser/ash/image_downloader/image_downloader_impl.cc
+++ chrome/browser/ash/image_downloader/image_downloader_impl.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/ash/image_downloader/image_downloader_impl.h"
+#include "chrome/browser/ash/image_downloader/image_downloader_impl.h"
 
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/bitmap_fetcher/bitmap_fetcher.h"
--- chrome/browser/ash/image_downloader/image_downloader_impl.h
+++ chrome/browser/ash/image_downloader/image_downloader_impl.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_IMAGE_DOWNLOADER_IMAGE_DOWNLOADER_IMPL_H_
-#define CHROME_BROWSER_UI_ASH_IMAGE_DOWNLOADER_IMAGE_DOWNLOADER_IMPL_H_
+#ifndef CHROME_BROWSER_ASH_IMAGE_DOWNLOADER_IMAGE_DOWNLOADER_IMPL_H_
+#define CHROME_BROWSER_ASH_IMAGE_DOWNLOADER_IMAGE_DOWNLOADER_IMPL_H_
 
 #include "ash/public/cpp/image_downloader.h"
 
@@ -38,4 +38,4 @@ class ImageDownloaderImpl : public ash::ImageDownloader {
                 ash::ImageDownloader::DownloadCallback callback) override;
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_IMAGE_DOWNLOADER_IMAGE_DOWNLOADER_IMPL_H_
+#endif  // CHROME_BROWSER_ASH_IMAGE_DOWNLOADER_IMAGE_DOWNLOADER_IMPL_H_
