752b2e3da7e691489244c776d63a68943281f19c
rbpotter <rbpotter@chromium.org>
2024-08-30 20:27:20 +0000

    PDF: Fix page selector regression
    Also adds a regression test.
    
    Bug: 363052591
    Change-Id: Ieabd196268374d117e471e15c9e313ada0f0d71e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827795
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349393}

--- chrome/browser/resources/pdf/elements/viewer_page_selector.html.ts
+++ chrome/browser/resources/pdf/elements/viewer_page_selector.html.ts
@@ -9,7 +9,7 @@ import type {ViewerPageSelectorElement} from './viewer_page_selector.js';
 export function getHtml(this: ViewerPageSelectorElement) {
   return html`<!--_html_template_start_-->
 <div id="content">
-  <input part="input" type="text" id="pageSelector" value="${this.pageNo}"
+  <input part="input" type="text" id="pageSelector" .value="${this.pageNo}"
       @pointerup="${this.select}" @input="${this.onInput_}"
       @change="${this.pageNoCommitted}" aria-label="$i18n{labelPageNumber}">
   <span id="divider">/</span>
