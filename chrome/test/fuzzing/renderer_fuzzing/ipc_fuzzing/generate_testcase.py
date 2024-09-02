d1563699376318df966471d9695fcb189200e15f
Paul Semel <paulsemel@chromium.org>
2024-09-02 16:25:00 +0000

    fuzzing: re-enable checking for errors in ipc-interfaces-dumper
    We are currently having issues with the Centipede CQ that sporadically
    fails in the generate_testcase.py script. This might help debug what's
    going on.
    
    Change-Id: I240ce4ce233c3f250d35ded5b3b58f430e53ef45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832002
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Auto-Submit: Paul Semel <paulsemel@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349887}

--- chrome/test/fuzzing/renderer_fuzzing/ipc_fuzzing/generate_testcase.py
+++ chrome/test/fuzzing/renderer_fuzzing/ipc_fuzzing/generate_testcase.py
@@ -246,10 +246,9 @@ def run_ipc_dumper(dumper_path: str, out_file: str):
   # this tool.
   env["ASAN_OPTIONS"] = 'detect_odr_violation=0'
   args = [XVFB_PATH, os.path.abspath(dumper_path)]
-  # TODO(349980051): crbug.com/349980051: re-enable checking for the output of
-  # the script once we understand why UBSan is making browsertests so slow with
-  # DCHECKs on.
-  subprocess.run(args, capture_output=True, env=env, check=False)
+  # TODO(349980051): crbug.com/349980051: when ubsan is enabled by default in
+  # ASAN enabled builds, we had timeout issues running this binary.
+  subprocess.run(args, capture_output=True, env=env, check=True)
 
 
 def generate_interfaces(ipc_interfaces_dumper: str,
