d6c24f76a3f9747e59cc70135f0f956e7fc93150
HAIFAN WANG <wanghaifan@google.com>
2024-09-03 22:12:37 +0000

    [M129] Demo Mode: Add a feature flag IsDemoModeAppLandscapeLockedEnabled
    A previous CL (crrev/c/5548737) locks the demo mode app orientation in
    landscape. But it did not consider the case when the device is rotated
    180 degrees, and used kLandscapePrimary (locked to 0 degree). Thus,
    the device would look upside-down in the demo mode app when the device
    is rotated 180 degrees. Please refer to the videos in b/362820428.
    
    Therefore, we decided to add a feature flag disabled by default to
    block this landscape lock. When the issue is resolved (by a CL like
    this crrev/c/5823860), we will enable the flag.
    
    Since the browser test was already added, we enable this flag in the
    test.
    
    TEST=browser test
    DemoModeAppIntegrationTest.LaunchWebAppFromComponentContent*
    
    (cherry picked from commit ff7cac829cdaf30555e1851e3a76f7d3af9dafca)
    
    Bug: b/363040623
    Change-Id: Ief05d371f5554529c90851d0570c1c7a88cc688c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5825930
    Reviewed-by: Xiqi Ruan <xiqiruan@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Auto-Submit: Haifan Wang <wanghaifan@google.com>
    Commit-Queue: Haifan Wang <wanghaifan@google.com>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1349440}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834254
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6668@{#814}
    Cr-Branched-From: 05bc664984ca075216b7f2198c88b9725bfa1b9b-refs/heads/main@{#1343869}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2914,6 +2914,11 @@ BASE_FEATURE(kDemoModeGMSCoreWindowCloser,
              "DemoModeGMSCoreWindowCloser",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Disable the demo mode app orientation locked in landscape.
+BASE_FEATURE(kDemoModeAppLandscapeLocked,
+             "DemoModeAppLandscapeLocked",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 // Moves toasts to the bottom-side corner where the status area is instead of
 // the center when enabled.
 BASE_FEATURE(kSideAlignedToasts,
@@ -3712,6 +3717,10 @@ bool IsAudioSourceFetcherResamplingEnabled() {
 
 bool IsDemoModeGMSCoreWindowCloserEnabled() {
   return base::FeatureList::IsEnabled(kDemoModeGMSCoreWindowCloser);
+}
+
+bool IsDemoModeAppLandscapeLockedEnabled() {
+  return base::FeatureList::IsEnabled(kDemoModeAppLandscapeLocked);
 }
 
 bool IsEcheSWAEnabled() {
