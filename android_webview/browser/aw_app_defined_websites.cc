201790d55ef3f4689cb52d0d8d34ec0068ab2e32
Peter Birk Pakkenberg <pbirk@chromium.org>
2024-09-03 10:22:33 +0000

    Make all AppDefinedDomain read async and cached
    The AppDefinedDomains are read from a system service, which may have a
    significant delay. On the other hand, since these are values from the
    apps manifest, they are not expected to change during the runtime of the
    app.
    
    This CL ensures that the information is always read on a background
    thread, as it was already done by the
    masked_domain_list_component_loader.
    It also caches results in memory to speed up subsequent calls.
    
    It is possible that GetAppDefinedDomains may be called multiple times
    for the same criteria before the first callback returns with a result
    and caches it. In this case the following calls will overwrite the
    cached value. This is not a problem per se, but slightly inefficient,
    and will be fixed in a follow-up CL.
    
    Bug: 353556455
    Change-Id: Ie9c2e0cdc35fbcb7e149cfb9d4158a3dd4eed52e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823049
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350067}

--- android_webview/browser/aw_app_defined_websites.cc
+++ android_webview/browser/aw_app_defined_websites.cc
@@ -4,11 +4,17 @@
 
 #include "android_webview/browser/aw_app_defined_websites.h"
 
+#include <memory>
+
 #include "base/android/jni_android.h"
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
+#include "base/functional/bind.h"
+#include "base/memory/ptr_util.h"
 #include "base/no_destructor.h"
+#include "base/sequence_checker.h"
+#include "base/task/thread_pool.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
@@ -16,7 +22,9 @@
 
 namespace android_webview {
 
-std::vector<std::string> GetAppDefinedDomains(
+namespace {
+
+std::vector<std::string> GetAppDefinedDomainsFromManifest(
     AppDefinedDomainCriteria criteria) {
   JNIEnv* env = base::android::AttachCurrentThread();
   base::android::ScopedJavaLocalRef<jobjectArray> jobject_domains;
@@ -51,6 +59,45 @@ std::vector<std::string> GetAppDefinedDomains(
   base::android::AppendJavaStringArrayToStringVector(env, jobject_domains,
                                                      &domains);
   return domains;
+}
+
+}  // namespace
+
+// static
+AppDefinedWebsites* AppDefinedWebsites::GetInstance() {
+  static base::NoDestructor<AppDefinedWebsites> instance(
+      base::BindRepeating(&GetAppDefinedDomainsFromManifest));
+  return instance.get();
+}
+
+AppDefinedWebsites::AppDefinedWebsites(AppDomainProvider provider)
+    : provider_(std::move(provider)) {}
+AppDefinedWebsites::~AppDefinedWebsites() = default;
+
+void AppDefinedWebsites::GetAppDefinedDomains(AppDefinedDomainCriteria criteria,
+                                              AppDomainCallback callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  auto find_it = domains_cache_.find(criteria);
+  if (find_it != domains_cache_.end()) {
+    std::move(callback).Run(*find_it->second);
+    return;
+  }
+
+  base::ThreadPool::PostTaskAndReplyWithResult(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
+      base::BindOnce(provider_, criteria),
+      base::BindOnce(&AppDefinedWebsites::DomainsReturnedFromManifest,
+                     weak_ptr_factory_.GetWeakPtr(), criteria,
+                     std::move(callback)));
+}
+
+void AppDefinedWebsites::DomainsReturnedFromManifest(
+    AppDefinedDomainCriteria criteria,
+    AppDomainCallback callback,
+    const std::vector<std::string>& data) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  domains_cache_[criteria] = std::make_unique<std::vector<std::string>>(data);
+  std::move(callback).Run(*domains_cache_[criteria]);
 }
 
 }  // namespace android_webview
--- android_webview/browser/aw_app_defined_websites.h
+++ android_webview/browser/aw_app_defined_websites.h
@@ -8,10 +8,16 @@
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_map.h"
+#include "base/functional/callback.h"
+#include "base/functional/callback_forward.h"
+#include "base/memory/weak_ptr.h"
+#include "base/no_destructor.h"
+#include "base/sequence_checker.h"
+
 namespace android_webview {
 
-// Used to determine which sources to retrieve related
-// websites from.
+// Used to determine which sources to retrieve related websites from.
 // This enum is used to set the values for feature parameter
 // `features::kWebViewIpProtectionExclusionCriteria`.
 // Do not modify/reorder the enum without ensuring that the above mentioned
@@ -35,7 +41,46 @@ enum class AppDefinedDomainCriteria {
   kAndroidAssetStatementsAndWebLinks = 4,
 };
 
-std::vector<std::string> GetAppDefinedDomains(AppDefinedDomainCriteria policy);
+// AppDefinedWebsites provides access to domain lists defined in the embedding
+// app's manifest file. As the domain lists are static, they will be cached for
+// future access once they have been loaded once.
+// This class should only be accessed on the UI thread.
+class AppDefinedWebsites {
+ public:
+  using AppDomainCallback =
+      base::OnceCallback<void(const std::vector<std::string>&)>;
+
+  // Get the global instance of AppDefinedWebsites.
+  static AppDefinedWebsites* GetInstance();
+
+  // Get the specified list of domains from the app manifest.
+  // The list will be fetched on a background thread if it is not already
+  // cached. The `callback` will be executed on the calling sequence.
+  void GetAppDefinedDomains(AppDefinedDomainCriteria criteria,
+                            AppDomainCallback callback);
+
+ private:
+  friend class base::NoDestructor<AppDefinedWebsites>;
+  friend class AppDefinedWebsitesTest;
+
+  using AppDomainProvider = base::RepeatingCallback<std::vector<std::string>(
+      AppDefinedDomainCriteria)>;
+
+  explicit AppDefinedWebsites(AppDomainProvider provider);
+  ~AppDefinedWebsites();
+
+  void DomainsReturnedFromManifest(AppDefinedDomainCriteria criteria,
+                                   AppDomainCallback callback,
+                                   const std::vector<std::string>& data);
+
+  SEQUENCE_CHECKER(sequence_checker_);
+  AppDomainProvider provider_;
+  base::flat_map<AppDefinedDomainCriteria,
+                 std::unique_ptr<std::vector<std::string>>>
+      domains_cache_;
+
+  base::WeakPtrFactory<AppDefinedWebsites> weak_ptr_factory_{this};
+};
 
 }  // namespace android_webview
 
