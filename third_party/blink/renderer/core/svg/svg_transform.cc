f8f278aadc2d05c42f0606654dfed22adb030d90
Fredrik Söderquist <fs@opera.com>
2024-09-03 16:25:17 +0000

    spanification: //third_party/blink/renderer/core/svg/svg_transform.cc
    Bug: 356658478
    Change-Id: Id4050d379300ed22f07a64ed83c3539aec9617b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830356
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1350209}

--- third_party/blink/renderer/core/svg/svg_transform.cc
+++ third_party/blink/renderer/core/svg/svg_transform.cc
@@ -18,11 +18,6 @@
  * Boston, MA 02110-1301, USA.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/svg/svg_transform.h"
 
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
@@ -168,7 +163,7 @@ gfx::PointF DecomposeRotationCenter(const AffineTransform& matrix,
 }  // namespace
 
 String SVGTransform::ValueAsString() const {
-  double arguments[6];
+  std::array<double, 6> arguments;
   size_t argument_count = 0;
   switch (transform_type_) {
     case SVGTransformType::kUnknown:
