41e27c09fcdf3727c587f4e0c61c9086aa1082aa
WhaleChang <whalechang@google.com>
2024-09-04 03:15:18 +0000

    launch BluetoothFlossTelephony on ToT
    Since Finch is already 10% on stable. Base on the best practices, enable
    the feature flag on ToT.
    
    BUG=b:342090697
    TEST=CQ
    
    Change-Id: I9d93eca6dac9ac503758936b25d62f4d300bd73d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5828693
    Reviewed-by: Hsinyu Chao <hychao@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Ching Yun Chang <whalechang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350573}

--- device/bluetooth/chromeos_platform_features.cc
+++ device/bluetooth/chromeos_platform_features.cc
@@ -16,7 +16,7 @@ BASE_FEATURE(kBluetoothFlossCoredump,
 
 BASE_FEATURE(kBluetoothFlossTelephony,
              "BluetoothFlossTelephony",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kBluetoothBtsnoopInternals,
              "BluetoothBtsnoopInternals",
