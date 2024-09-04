8c6bb54555a11cfed1128d82210b5eb684c164fd
Daniel Rubery <drubery@chromium.org>
2024-09-04 16:11:59 +0000

    Enable SafeBrowsingDeepScanningPromptRemoval by default
    This is approved for full launch so enable by default.
    
    Change-Id: I4d76f5b993e41b24dd1bc26052ae528bb0e6be88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834645
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350841}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -65,7 +65,7 @@ BASE_FEATURE(kDangerousDownloadInterstitial,
 
 BASE_FEATURE(kDeepScanningPromptRemoval,
              "SafeBrowsingDeepScanningPromptRemoval",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDeepScanningCriteria,
              "SafeBrowsingDeepScanningCriteria",
