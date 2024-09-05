735b0796c3335998587c231c71c08b1982ee167e
Lei Zhang <thestig@chromium.org>
2024-09-05 18:52:47 +0000

    PDF Viewer: Refactor viewer_thumbnail_test.ts
    - Consolidate common code like <canvas> creation and various checks into
      their own helper functions.
    - Separate code that sets up <viewer-thumbnail> for testing from code
      that checks the properties of the element.
    
    Change-Id: Ib65b84e566e75ef14d1e74ab3481dde119d7fbb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838411
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351601}

--- chrome/test/data/pdf/viewer_thumbnail_test.ts
+++ chrome/test/data/pdf/viewer_thumbnail_test.ts
@@ -12,39 +12,44 @@ function createThumbnail() {
   return thumbnail;
 }
 
+function createPdfCanvas(
+    thumbnail: ViewerThumbnailElement, imageSize: number[]) {
+  thumbnail.image = new ImageData(imageSize[0]!, imageSize[1]!);
+  return thumbnail.shadowRoot!.querySelector('canvas')!;
+}
+
+function checkThumbnailAncestorDivSize(
+    canvas: HTMLCanvasElement, divSize: number[]) {
+  const div = canvas.parentElement!;
+  chrome.test.assertEq(divSize[0], div.offsetWidth);
+  chrome.test.assertEq(divSize[1], div.offsetHeight);
+}
+
+function checkThumbnailSize(canvas: HTMLCanvasElement, canvasSize: number[]) {
+  chrome.test.assertEq(`${canvasSize[0]}px`, canvas.style.width);
+  chrome.test.assertEq(`${canvasSize[1]}px`, canvas.style.height);
+}
+
 function testThumbnailSize(
     thumbnail: ViewerThumbnailElement, imageSize: number[],
     canvasSize: number[]) {
-  const imageData = new ImageData(imageSize[0]!, imageSize[1]!);
-  thumbnail.image = imageData;
+  const canvas = createPdfCanvas(thumbnail, imageSize);
+  checkThumbnailSize(canvas, canvasSize);
 
-  const canvas = thumbnail.shadowRoot!.querySelector('canvas')!;
-  chrome.test.assertEq(`${canvasSize[0]}px`, canvas.style.width);
-  chrome.test.assertEq(`${canvasSize[1]}px`, canvas.style.height);
-
-  // The div containing the canvas should be resized to fit.
-  const div = canvas.parentElement!;
-  chrome.test.assertEq(canvasSize[0], div.offsetWidth);
-  chrome.test.assertEq(canvasSize[1], div.offsetHeight);
+  // The thumbnail div ancestor containing the canvas should be resized to fit.
+  checkThumbnailAncestorDivSize(canvas, canvasSize);
 }
 
-async function testThumbnailRotation(
-    thumbnail: ViewerThumbnailElement, clockwiseRotations: number,
-    divSize: number[]) {
-  thumbnail.clockwiseRotations = clockwiseRotations;
-  await microtasksFinished();
-
-  const canvas = thumbnail.shadowRoot!.querySelector('canvas')!;
+function checkRotatedThumbnailSizeAndTransform(
+    canvas: HTMLCanvasElement, clockwiseRotations: number, divSize: number[]) {
   const halfTurn = clockwiseRotations % 2 === 0;
   chrome.test.assertEq(
       `${halfTurn ? divSize[0] : divSize[1]}px`, canvas.style.width);
   chrome.test.assertEq(
       `${halfTurn ? divSize[1] : divSize[0]}px`, canvas.style.height);
 
-  // The div containing the rotated canvas should be resized to fit.
-  const div = canvas.parentElement!;
-  chrome.test.assertEq(divSize[0], div.offsetWidth);
-  chrome.test.assertEq(divSize[1], div.offsetHeight);
+  // The thumbnail div ancestor containing the canvas should be resized to fit.
+  checkThumbnailAncestorDivSize(canvas, divSize);
 
   chrome.test.assertEq(
       `rotate(${clockwiseRotations * 90}deg)`, canvas.style.transform);
@@ -53,13 +58,14 @@ async function testThumbnailRotation(
 async function testThumbnailRotations(
     imageSize: number[], rotatedDivSizes: number[][]) {
   const thumbnail = createThumbnail();
-  const imageData = new ImageData(imageSize[0]!, imageSize[1]!);
-  thumbnail.image = imageData;
+  const canvas = createPdfCanvas(thumbnail, imageSize);
 
   chrome.test.assertEq(4, rotatedDivSizes.length);
   for (let rotations = 0; rotations < rotatedDivSizes.length; rotations++) {
-    await testThumbnailRotation(
-        thumbnail, rotations, rotatedDivSizes[rotations]!);
+    thumbnail.clockwiseRotations = rotations;
+    await microtasksFinished();
+    checkRotatedThumbnailSizeAndTransform(
+        canvas, rotations, rotatedDivSizes[rotations]!);
   }
 }
 
@@ -180,9 +186,7 @@ const tests = [
   },
   async function testContextMenuDisabled() {
     // Set some image data so a canvas is created inside the thumbnail.
-    const thumbnail = createThumbnail();
-    thumbnail.image = new ImageData(108, 140);
-    const canvas = thumbnail.shadowRoot!.querySelector('canvas')!;
+    const canvas = createPdfCanvas(createThumbnail(), [108, 140]);
 
     const whenContextMenu = eventToPromise('contextmenu', canvas);
     canvas.dispatchEvent(new CustomEvent('contextmenu', {cancelable: true}));
