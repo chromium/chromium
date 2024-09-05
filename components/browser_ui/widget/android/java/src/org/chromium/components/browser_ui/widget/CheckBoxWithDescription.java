61b785018f5f3a639712c4f820d430724d87323b
Joel Tan-Aristy <jtanaristy@google.com>
2024-09-05 21:50:19 +0000

    Fixes CheckBoxWithDescription to have centered checkbox.
    Before the checkbox had uneven whitespace above and below it:
    https://screenshot.googleplex.com/9dV9R7A8jmmbqUF.png
    After the checkbox has 8dp whitespace above and below it
    as intended:
    https://screenshot.googleplex.com/9FNpeJRfhi6ueCX.png
    Whenever there is no description, appears as intended:
    https://screenshot.googleplex.com/DAAiEtcyze9FMod.png
    
    Bug: 41486095
    Change-Id: I8c65e8522e756a532078d83210e365403646ea1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818355
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351706}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/CheckBoxWithDescription.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/CheckBoxWithDescription.java
@@ -11,10 +11,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.CheckBox;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.VisibleForTesting;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 /**
  * A CheckBox with a primary and descriptive text to the right.
@@ -22,7 +22,7 @@ import androidx.annotation.VisibleForTesting;
  * TODO(crbug.com/40862238): Add CompoundButtonWithDescription to avoid duplicate code with
  * RadioButtonWithDescription.
  */
-public class CheckBoxWithDescription extends RelativeLayout implements OnClickListener {
+public class CheckBoxWithDescription extends ConstraintLayout implements OnClickListener {
     private CheckBox mCheckBox;
     private TextView mPrimary;
     private TextView mDescription;
@@ -84,10 +84,8 @@ public class CheckBoxWithDescription extends RelativeLayout implements OnClickLi
         mDescription.setText(text);
 
         if (TextUtils.isEmpty(text)) {
-            ((LayoutParams) mPrimary.getLayoutParams()).addRule(RelativeLayout.CENTER_VERTICAL);
             mDescription.setVisibility(View.GONE);
         } else {
-            ((LayoutParams) mPrimary.getLayoutParams()).removeRule(RelativeLayout.CENTER_VERTICAL);
             mDescription.setVisibility(View.VISIBLE);
         }
     }
