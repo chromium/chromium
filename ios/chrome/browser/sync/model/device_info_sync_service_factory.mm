44b221b35ea3e9630cfa1b75ab90d2a0b279c187
Hira Mahmood <hiramahmood@google.com>
2024-09-05 17:01:34 +0000

    [iOS] Check if send tab notification permissions are enabled
    This CL checks if Send Tab push notification permissions are enabled
    on the receiving device before storing the receiving type in the Device
    Info.
    
    DeviceInfo bridge notified when the setting is updated in this CL:
    crrev.com/c/5833942.
    
    Bug: 343495515
    Change-Id: Ief6c97802f8f998c859c5583fda9647f8fc88f44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834235
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351512}

--- ios/chrome/browser/sync/model/device_info_sync_service_factory.mm
+++ ios/chrome/browser/sync/model/device_info_sync_service_factory.mm
@@ -7,6 +7,7 @@
 #import <optional>
 #import <utility>
 
+#import "base/feature_list.h"
 #import "base/functional/bind.h"
 #import "base/memory/raw_ptr.h"
 #import "base/memory/singleton.h"
@@ -24,7 +25,9 @@
 #import "components/sync_device_info/device_info_sync_client.h"
 #import "components/sync_device_info/device_info_sync_service_impl.h"
 #import "components/sync_device_info/local_device_info_provider_impl.h"
+#import "ios/chrome/browser/push_notification/model/push_notification_client_id.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_service.h"
+#import "ios/chrome/browser/push_notification/model/push_notification_settings_util.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/profile/profile_manager_ios.h"
@@ -61,14 +64,20 @@ class DeviceInfoSyncClient : public syncer::DeviceInfoSyncClient {
   // syncer::DeviceInfoSyncClient:
   sync_pb::SyncEnums_SendTabReceivingType GetSendTabToSelfReceivingType()
       const override {
-    // TODO(crbug.com/343495515): Check if push notifications are enabled on
-    // device.
-    return base::FeatureList::IsEnabled(
-               send_tab_to_self::kSendTabToSelfIOSPushNotifications)
-               ? sync_pb::
-                     SyncEnums_SendTabReceivingType_SEND_TAB_RECEIVING_TYPE_CHROME_AND_PUSH_NOTIFICATION
-               : sync_pb::
-                     SyncEnums_SendTabReceivingType_SEND_TAB_RECEIVING_TYPE_CHROME_OR_UNSPECIFIED;
+    std::string gaia_id =
+        identity_manager_->GetPrimaryAccountInfo(signin::ConsentLevel::kSignin)
+            .gaia;
+    bool send_tab_notifications_enabled = push_notification_settings::
+        GetMobileNotificationPermissionStatusForClient(
+            PushNotificationClientId::kSendTab, gaia_id);
+    if (base::FeatureList::IsEnabled(
+            send_tab_to_self::kSendTabToSelfIOSPushNotifications) &&
+        send_tab_notifications_enabled) {
+      return sync_pb::
+          SyncEnums_SendTabReceivingType_SEND_TAB_RECEIVING_TYPE_CHROME_AND_PUSH_NOTIFICATION;
+    }
+    return sync_pb::
+        SyncEnums_SendTabReceivingType_SEND_TAB_RECEIVING_TYPE_CHROME_OR_UNSPECIFIED;
   }
 
   // syncer::DeviceInfoSyncClient:
