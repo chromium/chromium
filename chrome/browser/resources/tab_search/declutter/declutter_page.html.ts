ca99f5beb5574835b9566a27a48c657ef34f7a84
Emily Shack <emshack@chromium.org>
2024-09-05 18:03:18 +0000

    Declutter: Add CSS styling to declutter page
    Brings the general shape of the declutter page up to spec.
    Does not include:
    * Translateable strings (still using some placeholders)
    * Updated per-item subheading text
    * Updated close icon
    
    Other than that, should be looking much better!
    
    Bug: 364383619
    Change-Id: I555fe8e86f509f8921be51573a35bf98833b87a1
    Screenshot: https://screenshot.googleplex.com/cakfpWfKWnvW8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834062
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351565}

--- chrome/browser/resources/tab_search/declutter/declutter_page.html.ts
+++ chrome/browser/resources/tab_search/declutter/declutter_page.html.ts
@@ -8,21 +8,26 @@ import type {DeclutterPageElement} from './declutter_page.js';
 
 export function getHtml(this: DeclutterPageElement) {
   return html`<!--_html_template_start_-->
-  <div id="header">
-    <cr-icon-button iron-icon="cr:arrow-back" @click="${this.onBackClick_}">
-    </cr-icon-button>
-    $i18n{declutterTitle}
-  </div>
-  <div id="tabList">
-    ${this.staleTabDatas_.map((item, index) => html`
-        <tab-search-item class="mwb-list-item" .data="${item}"
-            role="option"
-            data-index="${index}"
-            @close="${this.onTabRemove_}">
-        </tab-search-item>
-    `)}
-  </div>
-  <cr-button class="action-button" @click="${this.onCloseTabsClick_}">
-    Close tabs
-  </cr-button><!--_html_template_end_-->`;
+  <div id="declutterPage">
+    <div id="header">
+      <cr-icon-button iron-icon="cr:arrow-back" @click="${this.onBackClick_}">
+      </cr-icon-button>
+      <div id="headerText">
+        <div class="title">$i18n{declutterTitle}</div>
+        <div class="subheading">Tabs not used for 7 days or more</div>
+      </div>
+    </div>
+    <div id="tabList">
+      ${this.staleTabDatas_.map((item, index) => html`
+          <tab-search-item class="mwb-list-item" .data="${item}"
+              role="option"
+              data-index="${index}"
+              @close="${this.onTabRemove_}">
+          </tab-search-item>
+      `)}
+    </div>
+    <cr-button class="action-button" @click="${this.onCloseTabsClick_}">
+      Close tabs
+    </cr-button>
+  </div><!--_html_template_end_-->`;
 }
