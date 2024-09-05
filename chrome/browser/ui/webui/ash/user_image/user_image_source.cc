16b2758f638305dadd39a94b485cc26955c70923
Eriko Kurimoto <elkurin@chromium.org>
2024-09-05 02:38:21 +0000

    ash: Create //chrome/browser/ui/webui/ash/user_image
    Bug: b:364173601
    Change-Id: Id81921686e5ba6b2fd0e3c6adf72423c80947157
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823463
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351188}

--- chrome/browser/ui/webui/ash/user_image/user_image_source.cc
+++ chrome/browser/ui/webui/ash/user_image/user_image_source.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/ash/user_image_source.h"
+#include "chrome/browser/ui/webui/ash/user_image/user_image_source.h"
 
 #include "ash/constants/ash_features.h"
 #include "base/memory/ref_counted_memory.h"
--- chrome/browser/ui/webui/ash/user_image/user_image_source.h
+++ chrome/browser/ui/webui/ash/user_image/user_image_source.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_ASH_USER_IMAGE_SOURCE_H_
-#define CHROME_BROWSER_UI_WEBUI_ASH_USER_IMAGE_SOURCE_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_USER_IMAGE_USER_IMAGE_SOURCE_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_USER_IMAGE_USER_IMAGE_SOURCE_H_
 
 #include <string>
 #include <vector>
@@ -47,4 +47,4 @@ class UserImageSource : public content::URLDataSource {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_UI_WEBUI_ASH_USER_IMAGE_SOURCE_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_USER_IMAGE_USER_IMAGE_SOURCE_H_
