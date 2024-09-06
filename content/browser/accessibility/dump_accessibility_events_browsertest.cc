9c985f552bcacc9d6ee1e4f6e51cb99f4ce4b089
Aaron leventhal <aleventhal@google.com>
2024-09-06 20:02:37 +0000

    [A11y] Reenable EventsAriaMenuItemFocus test
    Bug: 338217303,365027678
    Change-Id: I62dd4e1321dbcef01454a6b91c251eca7f159eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838807
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1352233}

--- content/browser/accessibility/dump_accessibility_events_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -298,16 +298,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
   RunEventTest(FILE_PATH_LITERAL("aria-live-changed.html"));
 }
 
-// TODO(crbug.com/338217303): Very flaky on Mac.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_AccessibilityEventsAriaMenuItemFocus \
-  DISABLED_AccessibilityEventsAriaMenuItemFocus
-#else
-#define MAYBE_AccessibilityEventsAriaMenuItemFocus \
-  AccessibilityEventsAriaMenuItemFocus
-#endif
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
-                       MAYBE_AccessibilityEventsAriaMenuItemFocus) {
+                       AccessibilityEventsAriaMenuItemFocus) {
   RunEventTest(FILE_PATH_LITERAL("aria-menuitem-focus.html"));
 }
 
