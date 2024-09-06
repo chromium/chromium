191025e5990cafdf4c164e146f99e503d0be2103
Colin Blundell <blundell@chromium.org>
2024-09-06 12:47:22 +0000

    [Canvas] Remove call to OffscreenCanvas::GetOrCreateResourceProvider()
    OffscreenCanvas::SetFilterQualityInResource() does the following:
    
    if (ResourceProvider())
        GetOrCreateResourceProvider()->SetFilterQuality(filter_quality);
    
    GetOrCreateResourceProvider() has the following code at its entry [1]:
    
     if (ResourceProvider() && !restoring_gpu_context_) {
        return ResourceProvider();
      }
    
    `restoring_gpu_context_` is initialized to false [2] and set only via
    SetRestoreGpuContext(), which itself is called only in
    TryRestoreContextEvent() to "wrap" a separate call to
    GetOrCreateResourceProvider() (i.e., it's set to true immediately
    before the call and back to false immediately after the call) that's
    made via calling
    OffscreenCanvasRC2D::GetOrCreateCanvasResourceProvider() [3].
    SetFilterQualityInResource() is not invoked within the context of
    OffscreenCanvasRenderingContext2D::GetOrCreateCanvasResourceProvider().
    Hence, `restoring_gpu_context_` cannot be true when
    GetOrCreateResourceProvider() is called by
    SetFilterQualityInResource().
    
    This CL replaces the call to GetOrCreateResourceProvider() with a
    call to ResourceProvider().
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc;l=525-527?q=OffscreenCanvas::GetORCreat&ss=chromium
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.h;l=291;drc=82dff63dbf9db05e9274e11d9128af7b9f51ceaa;bpv=1;bpt=1
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc;l=432-434;drc=82dff63dbf9db05e9274e11d9128af7b9f51ceaa;bpv=1;bpt=1
    
    Bug: 348445884
    Change-Id: I9f5abf31c26d372b54df9070252ad24979a4ad68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827536
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351985}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
@@ -624,7 +624,7 @@ void OffscreenCanvas::SetFilterQualityInResource(
 
   SetFilterQuality(filter_quality);
   if (ResourceProvider())
-    GetOrCreateResourceProvider()->SetFilterQuality(filter_quality);
+    ResourceProvider()->SetFilterQuality(filter_quality);
   if (context_ && (IsWebGL() || IsWebGPU())) {
     context_->SetFilterQuality(filter_quality);
   }
