274befc18475e50f8fb0c385e1b6c29711db6a32
Ivan Šandrk <isandrk@chromium.org>
2024-09-06 10:34:16 +0000

    [Bedtime Hours] Add logic to the controller
    This CL adds the main business logic to the controller, stubs out API
    calls using a delegate, and adds a unittest to verify the logic.
    
    Bug: b:345186543
    Change-Id: Ib5b3e2525694b6689b34bf1c5ac5746ac22e1af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837057
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Emmanuel Arias Soto <eariassoto@google.com>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Fahad Mansoor <fahadmansoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351937}

--- chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller.cc
+++ chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller.cc
@@ -4,19 +4,47 @@
 
 #include "chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller.h"
 
+#include <algorithm>
+#include <optional>
+#include <vector>
+
 #include "base/functional/bind.h"
+#include "base/location.h"
+#include "base/memory/raw_ref.h"
+#include "base/time/time.h"
+#include "base/timer/wall_clock_timer.h"
+#include "base/values.h"
+#include "chromeos/ash/components/policy/weekly_time/checked_util.h"
+#include "chromeos/ash/components/policy/weekly_time/weekly_time_checked.h"
+#include "chromeos/ash/components/policy/weekly_time/weekly_time_interval_checked.h"
 #include "chromeos/constants/pref_names.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_registry_simple.h"
+#include "components/prefs/pref_service.h"
 
 namespace policy {
 
+namespace {
+
+using ::policy::weekly_time::ExtractIntervalsFromList;
+using ::policy::weekly_time::GetDurationToNextEvent;
+using ::policy::weekly_time::IntervalsContainTime;
+
+}  // namespace
+
 DeviceRestrictionScheduleController::DeviceRestrictionScheduleController(
-    PrefService& pref_service) {
-  registrar_.Init(&pref_service);
+    Delegate& delegate,
+    PrefService& local_state)
+    : delegate_(delegate) {
+  registrar_.Init(&local_state);
+  // `base::Unretained` is safe here because `this` outlives `registrar_` which
+  // unregisters the observer when it is destroyed.
   registrar_.Add(
       chromeos::prefs::kDeviceRestrictionSchedule,
       base::BindRepeating(&DeviceRestrictionScheduleController::OnPolicyUpdated,
-                          weak_factory_.GetWeakPtr()));
+                          base::Unretained(this)));
+
+  OnPolicyUpdated();
 }
 
 DeviceRestrictionScheduleController::~DeviceRestrictionScheduleController() =
@@ -32,7 +60,85 @@ void DeviceRestrictionScheduleController::RegisterLocalStatePrefs(
 }
 
 void DeviceRestrictionScheduleController::OnPolicyUpdated() {
-  // TODO(b/355636599): Implement function.
+  const base::Value::List& policy_value =
+      registrar_.prefs()->GetList(chromeos::prefs::kDeviceRestrictionSchedule);
+
+  if (!UpdateIntervalsIfChanged(policy_value)) {
+    return;
+  }
+
+  Run();
+}
+
+void DeviceRestrictionScheduleController::Run() {
+  // Reset any potentially running timers.
+  run_timer_.Stop();
+
+  // Update state.
+  const base::Time current_time = base::Time::Now();
+  const std::optional<base::Time> next_run_time = GetNextRunTime(current_time);
+  const State state = GetCurrentState(current_time);
+
+  // Set up timers if there's a schedule (`intervals_` isn't empty).
+  if (next_run_time.has_value()) {
+    // Set up next run of the function.
+    StartRunTimer(next_run_time.value());
+  }
+
+  // Block or unblock login. This needs to be the last statement since it could
+  // cause a restart to the login-screen.
+  delegate_->BlockLogin(state == State::kRestricted);
+}
+
+std::optional<base::Time> DeviceRestrictionScheduleController::GetNextRunTime(
+    base::Time current_time) const {
+  const WeeklyTimeChecked current_weekly_time_checked =
+      WeeklyTimeChecked::FromTimeAsLocalTime(current_time);
+  std::optional<base::TimeDelta> time_to_next_run =
+      GetDurationToNextEvent(intervals_, current_weekly_time_checked);
+
+  if (!time_to_next_run.has_value()) {
+    // `intervals_` is empty.
+    return std::nullopt;
+  }
+
+  return current_time + time_to_next_run.value();
+}
+
+DeviceRestrictionScheduleController::State
+DeviceRestrictionScheduleController::GetCurrentState(
+    base::Time current_time) const {
+  auto current_weekly_time_checked =
+      WeeklyTimeChecked::FromTimeAsLocalTime(current_time);
+  return IntervalsContainTime(intervals_, current_weekly_time_checked)
+             ? State::kRestricted
+             : State::kRegular;
+}
+
+bool DeviceRestrictionScheduleController::UpdateIntervalsIfChanged(
+    const base::Value::List& policy_value) {
+  std::vector<WeeklyTimeIntervalChecked> new_intervals;
+  auto intervals_opt = ExtractIntervalsFromList(policy_value);
+  if (intervals_opt.has_value()) {
+    new_intervals = std::move(intervals_opt.value());
+  } else {
+    // Intervals parsing error. Use empty intervals.
+    new_intervals = {};
+  }
+
+  if (new_intervals == intervals_) {
+    return false;
+  }
+  intervals_ = std::move(new_intervals);
+  return true;
+}
+
+void DeviceRestrictionScheduleController::StartRunTimer(
+    base::Time next_run_time) {
+  // `this` outlives `run_timer_`.
+  run_timer_.Start(FROM_HERE, next_run_time,
+                   base::BindOnce(&DeviceRestrictionScheduleController::Run,
+                                  base::Unretained(this)));
 }
 
 }  // namespace policy
