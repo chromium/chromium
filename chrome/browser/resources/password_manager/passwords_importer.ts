4e43cdea8c77fde0f8ce7bf64fd4ee53ac3a5331
dpapad <dpapad@chromium.org>
2024-09-06 09:35:04 +0000

    WebUI: Bulk migrate iron-icon to cr-icon, part 3.
    In this part updating c/b/r/password_manager.
    
    This is in preparation of removing iron-icon from Desktop builds.
    
    Bug: 40943652
    Change-Id: Ib10512377c6a526081e8b9921bbaa3eafe476f39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836123
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351926}

--- chrome/browser/resources/password_manager/passwords_importer.ts
+++ chrome/browser/resources/password_manager/passwords_importer.ts
@@ -5,7 +5,7 @@
 import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/md_select.css.js';
-import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import 'chrome://resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
 import './site_favicon.js';
 import './dialogs/password_preview_item.js';
