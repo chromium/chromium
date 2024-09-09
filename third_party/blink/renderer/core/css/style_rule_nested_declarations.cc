4b38c7c215f7cf5ebfcd1d4d48cf59515a507f5f
Anders Hartvoll Ruud <andruud@chromium.org>
2024-09-09 15:53:42 +0000

    Avoid unsafe buffer usage in style_nested_declarations.cc
    Use implicit conversion from HeapVector<CSSSelector> to base::span
    instead of begin()/size().
    
    Bug: 351564777
    Change-Id: Ie1be2955f9f4a601b23c602827781b7dd6e28627
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834017
    Auto-Submit: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352774}

--- third_party/blink/renderer/core/css/style_rule_nested_declarations.cc
+++ third_party/blink/renderer/core/css/style_rule_nested_declarations.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/css/style_rule_nested_declarations.h"
 
 namespace blink {
@@ -15,8 +10,7 @@ void StyleRuleNestedDeclarations::ReplaceSelectorList(
     const CSSSelector* selector_list) {
   HeapVector<CSSSelector> selectors = CSSSelectorList::Copy(selector_list);
   style_rule_ = StyleRule::Create(
-      base::span<CSSSelector>{selectors.begin(), selectors.size()},
-      style_rule_->Properties().ImmutableCopyIfNeeded());
+      selectors, style_rule_->Properties().ImmutableCopyIfNeeded());
 }
 
 }  // namespace blink
