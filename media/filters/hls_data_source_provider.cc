dbf80e1965ea4b2c678adae93463173b0bb9d81c
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-09-09 22:18:21 +0000

    Revert "spanification: enforce -Wunsafe-buffer-usage in //media"
    This reverts commit 83c94e6fa26d93d6df1e4dba4edf4573c9f8f486.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8737236874803091649
    
    Sample failed build: https://ci.chromium.org/b/8737236874803091649
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5816876&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8737236874803091649&type=BUG
    
    Original change's description:
    > spanification: enforce -Wunsafe-buffer-usage in //media
    >
    > Add `#pragma allow_unsafe_buffers` to //media and enforce
    > -Wunsafe-buffer-usage
    >
    > This is a preparation to fix each files.
    > This CL has no behavior changes.
    >
    > Bug: 40285824
    > Change-Id: Iec07b35c5887d2e3cfe7823b8da0f85356a6f7bc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5816876
    > Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    > Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1353009}
    >
    
    Bug: 40285824
    Change-Id: I18c3ed1503f880bc2cc5de5c9c681481d308530f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5847591
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1353024}

--- media/filters/hls_data_source_provider.cc
+++ media/filters/hls_data_source_provider.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "media/filters/hls_data_source_provider.h"
 #include "base/trace_event/trace_event.h"
 
