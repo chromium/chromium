7e95c43e5e8f3f6b64f38aae702e7a6dc3b5d269
Daniel White <danieltwhite@google.com>
2024-09-09 16:37:24 +0000

    [iOS] Deprecate didTraitCollectionChange in GestureInProductHelpView
    Since didTraitCollectionChange is deprecated in iOS 17,
    GestureInProductHelpView was updated to use the new alternative system,
    registerForTraitChanges.
    
    Fixed: 349390722
    Change-Id: I1411f6ed0645ee036591d60cb202a044ca992121
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5835816
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1352803}

--- ios/chrome/browser/bubble/ui_bundled/gesture_iph/gesture_in_product_help_view.mm
+++ ios/chrome/browser/bubble/ui_bundled/gesture_iph/gesture_in_product_help_view.mm
@@ -342,6 +342,17 @@ UIButton* CreateDismissButton(UIAction* primaryAction) {
     self.alpha = 0;
     self.isAccessibilityElement = YES;
     self.accessibilityViewIsModal = YES;
+
+    if (@available(iOS 17, *)) {
+      __weak __typeof(self) weakSelf = self;
+      NSArray<UITrait>* traits =
+          (@[ UITraitHorizontalSizeClass.self, UITraitVerticalSizeClass.self ]);
+      UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
+                                       UITraitCollection* previousCollection) {
+        [weakSelf pauseAnimationOnTraitChange:previousCollection];
+      };
+      [self registerForTraitChanges:traits withHandler:handler];
+    }
   }
   return self;
 }
@@ -402,14 +413,16 @@ UIButton* CreateDismissButton(UIAction* primaryAction) {
                     MAX(min_height, targetSize.height));
 }
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
-  if (ShouldGestureIndicatorOffsetFromCenter(previousTraitCollection) !=
-      ShouldGestureIndicatorOffsetFromCenter(self.traitCollection)) {
-    [_animator pauseAnimation];
-    _needsRepositionBubbleAndGestureIndicator = YES;
+  if (@available(iOS 17, *)) {
+    return;
   }
+
+  [self pauseAnimationOnTraitChange:previousTraitCollection];
 }
+#endif
 
 - (void)layoutSubviews {
   [super layoutSubviews];
@@ -728,6 +741,17 @@ UIButton* CreateDismissButton(UIAction* primaryAction) {
           completionHandler();
         }
       }];
+}
+
+// Pauses the animation if there is a change to the gesture indicator's offset
+// position after a change to the view's trait collection.
+- (void)pauseAnimationOnTraitChange:
+    (UITraitCollection*)previousTraitCollection {
+  if (ShouldGestureIndicatorOffsetFromCenter(previousTraitCollection) !=
+      ShouldGestureIndicatorOffsetFromCenter(self.traitCollection)) {
+    [_animator pauseAnimation];
+    _needsRepositionBubbleAndGestureIndicator = YES;
+  }
 }
 
 @end
