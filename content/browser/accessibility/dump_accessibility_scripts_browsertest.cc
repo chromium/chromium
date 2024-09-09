525bf6052421ebaece621e9cbfecd96ea9db35cf
Aaron Leventhal <aleventhal@google.com>
2024-09-09 12:14:47 +0000

    [A11y] Reenable Mac AX API line number test
    Bug: 40930250, 365027678
    Change-Id: I45bb00c77da9a8d7f9c23b15c3eafc1ddc04358f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840859
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352677}

--- content/browser/accessibility/dump_accessibility_scripts_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_scripts_browsertest.cc
@@ -304,14 +304,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityScriptTest, AXHighestEditableAncestor) {
   RunTypedTest<kMacAttributes>("ax-highest-editable-ancestor.html");
 }
 
-// TODO(crbug.com/40930250): Flaky
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_AXInsertionPointLineNumber DISABLED_AXInsertionPointLineNumber
-#else
-#define MAYBE_AXInsertionPointLineNumber AXInsertionPointLineNumber
-#endif
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityScriptTest,
-                       MAYBE_AXInsertionPointLineNumber) {
+                       AXInsertionPointLineNumber) {
   RunTypedTest<kMacAttributes>("ax-insertion-point-line-number.html");
 }
 
