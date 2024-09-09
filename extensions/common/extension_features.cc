a2865a465a92e1b6548ee0f4aaedee565ee79d81
Anunoy Ghosh <anunoy@chromium.org>
2024-09-09 19:19:55 +0000

    Enable IncludeJSCallStackInExtensionApiRequest feature on ToT
    Enable feature on ToT in  preparation for 100% launch.
    Also remove fieldtrial_testing config entry.
    
    Bug: 339401873
    Change-Id: Ie5b152fb575c4b35902ae6c9db6b930c407739a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5835996
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Anunoy Ghosh <anunoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352894}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -184,7 +184,7 @@ BASE_FEATURE(kDeclarativeNetRequestResponseHeaderMatching,
 
 BASE_FEATURE(kIncludeJSCallStackInExtensionApiRequest,
              "IncludeJSCallStackInExtensionApiRequest",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUseItemSnippetsAPI,
              "UseItemSnippetsAPI",
