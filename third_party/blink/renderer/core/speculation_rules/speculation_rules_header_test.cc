7a6a60c8a9102ae7556bc5b3abda9a820e05150c
Adithya Srinivasan <adithyas@chromium.org>
2024-09-10 20:38:07 +0000

    SpeculationRules: Handle detached document in SpeculationRuleLoader::NotifyFinished
    Ordinarily we cancel resource loads when the document is detached,
    but there's a small window of time where the document could detach
    after the load completes but before the notification is sent to
    SpeculationRuleLoader.
    
    (cherry picked from commit 98c51a71df1e64740e29c3bf9dc3b8477f7606a9)
    
    Bug: 356767669
    Change-Id: I8885ee597c425139ae3088e7042739cf128a4796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756187
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1336747}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5852273
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6613_141@{#3}
    Cr-Branched-From: 7a1e7be2d0a6b3064a32b9b84d13944871e705a8-refs/branch-heads/6613@{#1715}
    Cr-Branched-From: 03c1799e6f9c7239802827eab5e935b9e14fceae-refs/heads/main@{#1331488}

--- third_party/blink/renderer/core/speculation_rules/speculation_rules_header_test.cc
+++ third_party/blink/renderer/core/speculation_rules/speculation_rules_header_test.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/blink/renderer/core/speculation_rules/speculation_rules_header.h"
 
+#include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "net/base/net_errors.h"
 #include "net/http/http_status_code.h"
@@ -58,6 +59,11 @@ class ScopedRegisterMockedURLLoads {
              "single_url_prefetch_relative.json"),
         test::CoreTestDataPath(
             "speculation_rules/single_url_prefetch_relative.json"),
+        "application/speculationrules+json");
+
+    url_test_helpers::RegisterMockedURLLoad(
+        KURL("https://speculationrules.test/document_rule_prefetch.json"),
+        test::CoreTestDataPath("speculation_rules/document_rule_prefetch.json"),
         "application/speculationrules+json");
 
     KURL redirect_url(
@@ -399,5 +405,44 @@ TEST(SpeculationRulesHeaderTest, NetError) {
       DocumentSpeculationRules::From(page_holder.GetDocument()).rule_sets(),
       ::testing::IsEmpty());
 }
+
+// Regression test for crbug.com/356767669.
+// Order of events:
+// 1) The load of the speculation rules header completes
+// 2) The document detaches
+// 3) SpeculationRuleLoader::NotifyFinished is called
+TEST(SpeculationRulesHeaderTest, DocumentDetached) {
+  test::TaskEnvironment task_environment;
+  base::HistogramTester histogram_tester;
+  auto* chrome_client = MakeGarbageCollected<ConsoleCapturingChromeClient>();
+  DummyPageHolder page_holder(/*initial_view_size=*/{}, chrome_client);
+  ScopedRegisterMockedURLLoads mock_url_loads;
+
+  ResourceResponse document_response(KURL("https://speculation-rules.test/"));
+  document_response.SetHttpStatusCode(200);
+  document_response.SetMimeType(AtomicString("text/html"));
+  document_response.SetTextEncodingName(AtomicString("UTF-8"));
+  document_response.AddHttpHeaderField(
+      http_names::kSpeculationRules,
+      AtomicString("\"https://speculationrules.test/"
+                   "document_rule_prefetch.json\""));
+  SpeculationRulesHeader::ProcessHeadersForDocumentResponse(
+      document_response, *page_holder.GetFrame().DomWindow());
+
+  page_holder.GetDocument()
+      .GetTaskRunner(TaskType::kDOMManipulation)
+      ->PostTask(FROM_HERE, base::BindLambdaForTesting([&]() {
+                   page_holder.GetDocument().Shutdown();
+                 }));
+  url_test_helpers::ServeAsynchronousRequests();
+
+  histogram_tester.ExpectUniqueSample("Blink.SpeculationRules.LoadOutcome",
+                                      SpeculationRulesLoadOutcome::kSuccess, 0);
+  histogram_tester.ExpectTotalCount("Blink.SpeculationRules.FetchTime", 1);
+  EXPECT_THAT(chrome_client->ConsoleMessages(),
+              Not(Contains(ResultOf([](const auto& m) { return m.Utf8(); },
+                                    HasSubstr("Speculation-Rules")))));
+}
+
 }  // namespace
 }  // namespace blink
