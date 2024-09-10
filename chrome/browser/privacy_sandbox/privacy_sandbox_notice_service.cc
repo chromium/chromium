277ddb534fd7467b6d33cbbe623121fb21c4fe4f
Christine Ying <chrstne@google.com>
2024-09-10 16:00:06 +0000

    [N&C] Add migration code for other notices (PA, 3Ads, Restricted)
    This is to migrate the old prefs that have already been set before
    the notice and consent work is done.
    
    Change-Id: Ic750fa722f4bc3e473328bca1619421126d58e04
    Bug: 333407991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839714
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353387}

--- chrome/browser/privacy_sandbox/privacy_sandbox_notice_service.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_notice_service.cc
@@ -22,6 +22,21 @@ base::Time GetTimeFromPref(PrefService* prefs, const std::string& pref_name) {
   return base::ValueToTime(prefs->GetValue(pref_name)).value_or(base::Time());
 }
 
+// If prefs from any surface are previously set, we should not set new prefs
+// from the migration.
+bool ShouldSetMigrationPrefs(
+    PrefService* prefs,
+    PrivacySandboxNoticeStorage* notice_storage,
+    const std::vector<std::string_view>& notice_names) {
+  for (const auto& notice_name : notice_names) {
+    auto notice_data = notice_storage->ReadNoticeData(prefs, notice_name);
+    if (notice_data && notice_data.has_value()) {
+      return false;
+    }
+  }
+  return true;
+}
+
 }  // namespace
 
 PrivacySandboxNoticeService::PrivacySandboxNoticeService(
@@ -48,55 +63,128 @@ PrivacySandboxNoticeStorage* PrivacySandboxNoticeService::GetNoticeStorage() {
 }
 
 // TODO(crbug.com/333406690): Remove this once the old privacy sandbox prefs are
-// migrated to the new data model.
+// migrated to the new data model. Old prefs don't differentiate between CCT and
+// BrApp, so all Clank related prefs will be classified as BrApp.
 void PrivacySandboxNoticeService::MigratePrivacySandboxPrefsToDataModel() {
   // TopicsConsentModal
   {
-// TODO(crbug.com/352577199): Once CCTs are refactored using the storage
-// service, change this migration code to take into account CCTs.
 #if BUILDFLAG(IS_ANDROID)
     std::string topics_notice_name = kTopicsConsentModalClankBrApp;
 #else
     std::string topics_notice_name = kTopicsConsentModal;
 #endif  // BUILDFLAG(IS_ANDROID)
 
-    const auto consent_update_time = GetTimeFromPref(
-        pref_service_, prefs::kPrivacySandboxTopicsConsentLastUpdateTime);
-    // Topics can be updated through the settings (kSettings) page or a notice
-    // (kConfirmation).
-    const auto* update_reason = pref_service_->GetUserPrefValue(
-        prefs::kPrivacySandboxTopicsConsentLastUpdateReason);
+    if (ShouldSetMigrationPrefs(
+            pref_service_, notice_storage_.get(),
+            {kTopicsConsentModal, kTopicsConsentModalClankBrApp,
+             kTopicsConsentModalClankCCT})) {
+      const auto consent_update_time = GetTimeFromPref(
+          pref_service_, prefs::kPrivacySandboxTopicsConsentLastUpdateTime);
+      // Topics can be updated through the settings (kSettings) page or a notice
+      // (kConfirmation).
+      const auto* update_reason = pref_service_->GetUserPrefValue(
+          prefs::kPrivacySandboxTopicsConsentLastUpdateReason);
 
-    // Only prefs set from updating a notice are migrated. If the
-    // `update_reason` isn't set at all we will leave the pref settings to their
-    // default values.
-    if (update_reason &&
-        static_cast<TopicsConsentUpdateSource>(update_reason->GetInt()) ==
-            TopicsConsentUpdateSource::kConfirmation) {
-      // We need to use kPrivacySandboxTopicsConsentGiven as it stores the
-      // consent status of the user ignoring overrides.
-      auto* consent_decision_given = pref_service_->GetUserPrefValue(
-          prefs::kPrivacySandboxTopicsConsentGiven);
-      if (consent_decision_given) {
+      // Only prefs set from updating a notice are migrated. If the
+      // `update_reason` isn't set at all we will leave the pref settings to
+      // their default values.
+      if (update_reason &&
+          static_cast<TopicsConsentUpdateSource>(update_reason->GetInt()) ==
+              TopicsConsentUpdateSource::kConfirmation) {
+        // We need to use kPrivacySandboxTopicsConsentGiven as it stores the
+        // consent status of the user ignoring overrides.
+        auto* consent_decision_given = pref_service_->GetUserPrefValue(
+            prefs::kPrivacySandboxTopicsConsentGiven);
+        if (consent_decision_given) {
+          PrivacySandboxNoticeData notice_data;
+          notice_data.notice_action_taken = consent_decision_given->GetBool()
+                                                ? NoticeActionTaken::kOptIn
+                                                : NoticeActionTaken::kOptOut;
+          notice_data.notice_action_taken_time = consent_update_time;
+          notice_storage_->MigratePrivacySandboxNoticeData(
+              pref_service_, notice_data, topics_notice_name);
+        }
+      } else if (update_reason && static_cast<TopicsConsentUpdateSource>(
+                                      update_reason->GetInt()) ==
+                                      TopicsConsentUpdateSource::kSettings) {
+        // Prefs set by settings are mapped to 'kUnknownActionPreMigration'
+        // since it's unknown what action the user took on a notice, if any
         PrivacySandboxNoticeData notice_data;
-        notice_data.notice_action_taken = consent_decision_given->GetBool()
-                                              ? NoticeActionTaken::kOptIn
-                                              : NoticeActionTaken::kOptOut;
-        notice_data.notice_action_taken_time = consent_update_time;
+        notice_data.notice_action_taken =
+            NoticeActionTaken::kUnknownActionPreMigration;
+        notice_data.notice_action_taken_time = base::Time();
         notice_storage_->MigratePrivacySandboxNoticeData(
             pref_service_, notice_data, topics_notice_name);
       }
-    } else if (update_reason && static_cast<TopicsConsentUpdateSource>(
-                                    update_reason->GetInt()) ==
-                                    TopicsConsentUpdateSource::kSettings) {
-      // Prefs set by settings are mapped to 'kUnknownActionPreMigration'
-      // since it's unknown what action the user took on a notice, if any
-      PrivacySandboxNoticeData notice_data;
-      notice_data.notice_action_taken =
-          NoticeActionTaken::kUnknownActionPreMigration;
-      notice_data.notice_action_taken_time = base::Time();
-      notice_storage_->MigratePrivacySandboxNoticeData(
-          pref_service_, notice_data, topics_notice_name);
+    }
+  }
+
+  // ProtectedAudienceMeasurementNoticeModal
+  {
+#if BUILDFLAG(IS_ANDROID)
+    std::string pam_notice_name =
+        kProtectedAudienceMeasurementNoticeModalClankBrApp;
+#else
+    std::string pam_notice_name = kProtectedAudienceMeasurementNoticeModal;
+#endif  // BUILDFLAG(IS_ANDROID)
+
+    if (ShouldSetMigrationPrefs(
+            pref_service_, notice_storage_.get(),
+            {kProtectedAudienceMeasurementNoticeModal,
+             kProtectedAudienceMeasurementNoticeModalClankBrApp,
+             kProtectedAudienceMeasurementNoticeModalClankCCT})) {
+      auto* notice_acknowledged = pref_service_->GetUserPrefValue(
+          prefs::kPrivacySandboxM1EEANoticeAcknowledged);
+      if (notice_acknowledged && notice_acknowledged->GetBool()) {
+        PrivacySandboxNoticeData notice_data;
+        notice_data.notice_action_taken = NoticeActionTaken::kAck;
+        notice_storage_->MigratePrivacySandboxNoticeData(
+            pref_service_, notice_data, pam_notice_name);
+      }
+    }
+  }
+
+  // ThreeAdsAPIsNoticeModal
+  {
+#if BUILDFLAG(IS_ANDROID)
+    std::string three_ads_apis_notice_name = kThreeAdsAPIsNoticeModalClankBrApp;
+#else
+    std::string three_ads_apis_notice_name = kThreeAdsAPIsNoticeModal;
+#endif  // BUILDFLAG(IS_ANDROID)
+    if (ShouldSetMigrationPrefs(
+            pref_service_, notice_storage_.get(),
+            {kThreeAdsAPIsNoticeModal, kThreeAdsAPIsNoticeModalClankBrApp,
+             kThreeAdsAPIsNoticeModalClankCCT})) {
+      auto* notice_acknowledged = pref_service_->GetUserPrefValue(
+          prefs::kPrivacySandboxM1RowNoticeAcknowledged);
+      if (notice_acknowledged && notice_acknowledged->GetBool()) {
+        PrivacySandboxNoticeData notice_data;
+        notice_data.notice_action_taken = NoticeActionTaken::kAck;
+        notice_storage_->MigratePrivacySandboxNoticeData(
+            pref_service_, notice_data, three_ads_apis_notice_name);
+      }
+    }
+  }
+
+  // MeasurementNoticeModal
+  {
+#if BUILDFLAG(IS_ANDROID)
+    std::string measurement_notice_name = kMeasurementNoticeModalClankBrApp;
+#else
+    std::string measurement_notice_name = kMeasurementNoticeModal;
+#endif  // BUILDFLAG(IS_ANDROID)
+    if (ShouldSetMigrationPrefs(
+            pref_service_, notice_storage_.get(),
+            {kMeasurementNoticeModal, kMeasurementNoticeModalClankBrApp,
+             kMeasurementNoticeModalClankCCT})) {
+      auto* notice_acknowledged = pref_service_->GetUserPrefValue(
+          prefs::kPrivacySandboxM1RestrictedNoticeAcknowledged);
+      if (notice_acknowledged && notice_acknowledged->GetBool()) {
+        PrivacySandboxNoticeData notice_data;
+        notice_data.notice_action_taken = NoticeActionTaken::kAck;
+        notice_storage_->MigratePrivacySandboxNoticeData(
+            pref_service_, notice_data, measurement_notice_name);
+      }
     }
   }
 }
