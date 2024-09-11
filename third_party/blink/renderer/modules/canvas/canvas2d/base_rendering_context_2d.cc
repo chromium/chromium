b751f7dffa5009ed34a561ad4c7dee050630c3a7
Aaron Krajeski <aaronhk@chromium.org>
2024-09-11 16:00:16 +0000

    [M126-LTS] Use path2d cache flag for clip
    All other methods that use Path2D use path2d_use_paint_cache_ except
    for clip() which was always enabled. This could allow for situations
    where a DCHECK was triggered in the paint op buffer. See attached
    bug.
    
    (cherry picked from commit 2658f9415fc9656a645df543e782d393236fdd38)
    
    Bug: 349903568
    Change-Id: I179037bdd15d474cb2ef182e041f2b47b0ee868c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5716900
    Commit-Queue: Aaron Krajeski <aaronhk@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1329058}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834804
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Owners-Override: Artem Sumaneev <asumaneev@google.com>
    Auto-Submit: Roger Felipe Zanoni da Silva (xWF) <rzanoni@google.com>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Roger Felipe Zanoni da Silva (xWF) <rzanoni@google.com>
    Cr-Commit-Position: refs/branch-heads/6478@{#1957}
    Cr-Branched-From: e6143acc03189c5e52959545b110d6d17ecd5286-refs/heads/main@{#1300313}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -1567,7 +1567,7 @@ void BaseRenderingContext2D::clip(Path2D* dom_path,
         IdentifiabilitySensitiveStringToken(winding_rule_string));
   }
   ClipInternal(dom_path->GetPath(), winding_rule_string,
-               UsePaintCache::kEnabled);
+               path2d_use_paint_cache_);
 }
 
 bool BaseRenderingContext2D::isPointInPath(const double x,
