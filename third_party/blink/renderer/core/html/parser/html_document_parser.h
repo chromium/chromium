4b5aee8dff01bb8ecbd51fa989342f779d68eee7
Alex N. Jose <alexnj@chromium.org>
2024-09-11 22:58:01 +0000

    Implement Document-Policy: expect-no-embedded-resources
    The expect-no-linked-resources configuration point in Document Policy allows a document to hint to the user agent to better optimize its loading sequence, such as not using the default speculative parsing behavior. The current implementation on this CL skips attaching PreloadScanner if the policy is supplied.
    
    Bug: 365632977
    Change-Id: Ia4b307a4eac2051cca77d6ad1fe8354aa74a6790
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840722
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354246}

--- third_party/blink/renderer/core/html/parser/html_document_parser.h
+++ third_party/blink/renderer/core/html/parser/html_document_parser.h
@@ -254,6 +254,8 @@ class CORE_EXPORT HTMLDocumentParser : public ScriptableDocumentParser,
                              int newly_consumed_characters,
                              int tokens_parsed) const;
 
+  bool ShouldSkipPreloadScan();
+
   HTMLInputStream input_;
   const HTMLParserOptions options_;
   Member<HTMLParserReentryPermit> reentry_permit_ =
@@ -292,6 +294,9 @@ class CORE_EXPORT HTMLDocumentParser : public ScriptableDocumentParser,
 
   // Set to true if PumpTokenizer() was called at least once.
   bool did_pump_tokenizer_ = false;
+
+  // Cached result of ShouldSkipPreloadScan()
+  bool should_skip_preload_scan_ = false;
 };
 
 }  // namespace blink
---
+++ third_party/blink/web_tests/virtual/expect-no-embedded-resources/README.md
@@ -0,0 +1,4 @@
+# ExpectNoEmbeddedResources
+This suite runs the tests in http/expect-no-embedded-resources/.
+
+See crbug.com/365632977.
