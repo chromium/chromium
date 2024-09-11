70741de6a5fd5e903887867c5c8b7b3dbce11def
Maks Orlovich <morlovich@chromium.org>
2024-09-11 11:58:42 +0000

    FLEDGE: Add WPT for high-level problems with serverResponse payload
    In particular error injection at various layers and failing to
    find/authorize the response.
    
    Change-Id: Id6d77d7866ab7acf4d1c2fef5cb637c7f0a8065a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850614
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353884}

--- third_party/blink/web_tests/external/wpt/fledge/tentative/server-response.https.window.js
+++ third_party/blink/web_tests/external/wpt/fledge/tentative/server-response.https.window.js
@@ -6,6 +6,8 @@
 // META: script=/common/subset-tests.js
 // META: timeout=long
 // META: variant=?1-4
+// META: variant=?5-8
+// META: variant=?9-12
 
 // These tests focus on the serverResponse field in AuctinConfig, e.g.
 // auctions involving bidding and auction services.
@@ -97,3 +99,179 @@ subsetTest(promise_test, async test => {
   createAndNavigateFencedFrame(test, auctionResult);
   await waitForObservedRequests(uuid, [adB, trackBuyer, trackSeller]);
 }, 'Basic B&A auction with reporting URLs');
+
+async function runFaultInjectTest(test, fault) {
+  const uuid = generateUuid(test);
+  const adA = 'https://example.org/a';
+  const adB = 'https://example.org/b';
+  const adsArray =
+      [{renderURL: adA, adRenderId: 'a'}, {renderURL: adB, adRenderId: 'b'}];
+  await joinInterestGroup(test, uuid, {ads: adsArray});
+
+  const result = await navigator.getInterestGroupAdAuctionData(
+      {seller: window.location.origin});
+  assert_true(result.requestId !== null);
+  assert_true(result.request.length > 0);
+
+  let decoded = await BA.decodeInterestGroupData(result.request);
+
+  let serverResponseMsg = {
+    'biddingGroups': {},
+    'adRenderURL': adsArray[0].renderURL,
+    'interestGroupName': DEFAULT_INTEREST_GROUP_NAME,
+    'interestGroupOwner': window.location.origin,
+  };
+  serverResponseMsg.biddingGroups[window.location.origin] = [0];
+
+  let serverResponse =
+      await BA.encodeServerResponse(serverResponseMsg, decoded, fault);
+
+  let hashString = await BA.payloadHash(serverResponse);
+  await BA.authorizeServerResponseHashes([hashString]);
+
+  let auctionResult = await navigator.runAdAuction({
+    'seller': window.location.origin,
+    'interestGroupBuyers': [window.location.origin],
+    'requestId': result.requestId,
+    'serverResponse': serverResponse,
+    'resolveToConfig': true,
+  });
+  expectNoWinner(auctionResult);
+}
+
+subsetTest(promise_test, async test => {
+  return runFaultInjectTest(test, BA.injectCborFault);
+}, 'Basic B&A auction - fault inject at CBOR');
+
+subsetTest(promise_test, async test => {
+  return runFaultInjectTest(test, BA.injectGzipFault);
+}, 'Basic B&A auction - fault inject at gzip');
+
+subsetTest(promise_test, async test => {
+  return runFaultInjectTest(test, BA.injectFrameFault);
+}, 'Basic B&A auction - fault inject at framing');
+
+subsetTest(promise_test, async test => {
+  return runFaultInjectTest(test, BA.injectEncryptFault);
+}, 'Basic B&A auction - fault inject at encryption');
+
+subsetTest(promise_test, async test => {
+  const uuid = generateUuid(test);
+  const adA = 'https://example.org/a';
+  const adB = 'https://example.org/b';
+  const adsArray =
+      [{renderURL: adA, adRenderId: 'a'}, {renderURL: adB, adRenderId: 'b'}];
+  await joinInterestGroup(test, uuid, {ads: adsArray});
+
+  const result = await navigator.getInterestGroupAdAuctionData(
+      {seller: window.location.origin});
+  assert_true(result.requestId !== null);
+  assert_true(result.request.length > 0);
+
+  let decoded = await BA.decodeInterestGroupData(result.request);
+
+  let serverResponseMsg = {
+    'biddingGroups': {},
+    'adRenderURL': adsArray[0].renderURL,
+    'interestGroupName': DEFAULT_INTEREST_GROUP_NAME,
+    'interestGroupOwner': window.location.origin,
+  };
+  serverResponseMsg.biddingGroups[window.location.origin] = [0];
+
+  let serverResponse =
+      await BA.encodeServerResponse(serverResponseMsg, decoded);
+
+  // Mess up the array for a bit before computing hash to get the wrong hash,
+  // then undo.
+  serverResponse[0] ^= 0xBE;
+  let hashString = await BA.payloadHash(serverResponse);
+  await BA.authorizeServerResponseHashes([hashString]);
+  serverResponse[0] ^= 0xBE;
+
+  let auctionResult = await navigator.runAdAuction({
+    'seller': window.location.origin,
+    'interestGroupBuyers': [window.location.origin],
+    'requestId': result.requestId,
+    'serverResponse': serverResponse,
+    'resolveToConfig': true,
+  });
+  expectNoWinner(auctionResult);
+}, 'Basic B&A auction - Wrong authorization');
+
+subsetTest(promise_test, async test => {
+  const uuid = generateUuid(test);
+  const adA = 'https://example.org/a';
+  const adB = 'https://example.org/b';
+  const adsArray =
+      [{renderURL: adA, adRenderId: 'a'}, {renderURL: adB, adRenderId: 'b'}];
+  await joinInterestGroup(test, uuid, {ads: adsArray});
+
+  const result =
+      await navigator.getInterestGroupAdAuctionData({seller: OTHER_ORIGIN1});
+  assert_true(result.requestId !== null);
+  assert_true(result.request.length > 0);
+
+  let decoded = await BA.decodeInterestGroupData(result.request);
+
+  let serverResponseMsg = {
+    'biddingGroups': {},
+    'adRenderURL': adsArray[0].renderURL,
+    'interestGroupName': DEFAULT_INTEREST_GROUP_NAME,
+    'interestGroupOwner': window.location.origin,
+  };
+  serverResponseMsg.biddingGroups[window.location.origin] = [0];
+
+  let serverResponse =
+      await BA.encodeServerResponse(serverResponseMsg, decoded);
+
+  let hashString = await BA.payloadHash(serverResponse);
+  await BA.authorizeServerResponseHashes([hashString]);
+
+  let auctionResult = await navigator.runAdAuction({
+    'seller': window.location.origin,
+    'interestGroupBuyers': [window.location.origin],
+    'requestId': result.requestId,
+    'serverResponse': serverResponse,
+    'resolveToConfig': true,
+  });
+  expectNoWinner(auctionResult);
+}, 'Basic B&A auction - Wrong seller');
+
+subsetTest(promise_test, async test => {
+  const uuid = generateUuid(test);
+  const adA = 'https://example.org/a';
+  const adB = 'https://example.org/b';
+  const adsArray =
+      [{renderURL: adA, adRenderId: 'a'}, {renderURL: adB, adRenderId: 'b'}];
+  await joinInterestGroup(test, uuid, {ads: adsArray});
+
+  const result = await navigator.getInterestGroupAdAuctionData(
+      {seller: window.location.origin});
+  assert_true(result.requestId !== null);
+  assert_true(result.request.length > 0);
+
+  let decoded = await BA.decodeInterestGroupData(result.request);
+
+  let serverResponseMsg = {
+    'biddingGroups': {},
+    'adRenderURL': adsArray[0].renderURL,
+    'interestGroupName': DEFAULT_INTEREST_GROUP_NAME,
+    'interestGroupOwner': window.location.origin,
+  };
+  serverResponseMsg.biddingGroups[window.location.origin] = [0];
+
+  let serverResponse =
+      await BA.encodeServerResponse(serverResponseMsg, decoded);
+
+  let hashString = await BA.payloadHash(serverResponse);
+  await BA.authorizeServerResponseHashes([hashString]);
+
+  let auctionResult = await navigator.runAdAuction({
+    'seller': window.location.origin,
+    'interestGroupBuyers': [window.location.origin],
+    'requestId': token(),
+    'serverResponse': serverResponse,
+    'resolveToConfig': true,
+  });
+  expectNoWinner(auctionResult);
+}, 'Basic B&A auction - Wrong request Id');
