05b196425a534775111763e2bc3308b1e89c2796
Ameur Hosni <ameurhosni@google.com>
2024-09-12 18:13:07 +0000

    [IOS][LO] Add user education video in the consent dialog
    This CL adds a lottie animation view in the lens overlay consent view.
    
    Bug: 359781007
    Change-Id: I0547e3484ecb4d1dd70f0899eceafab10485a385
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5849793
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354700}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_consent_view_controller.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_consent_view_controller.mm
@@ -5,6 +5,8 @@
 #import "ios/chrome/browser/lens_overlay/ui/lens_overlay_consent_view_controller.h"
 
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/public/provider/chrome/browser/lottie/lottie_animation_api.h"
+#import "ios/public/provider/chrome/browser/lottie/lottie_animation_configuration.h"
 
 namespace {
 
@@ -12,9 +14,15 @@ const CGFloat kButtonHeight = 50.0f;
 
 const NSDirectionalEdgeInsets dialogInsets = {20.0f, 20.0f, 20.0f, 20.0f};
 
+NSString* const kLensUserEducationLightMode = @"lens_usered_lightmode";
+
+NSString* const kLensUserEducationDarkMode = @"lens_usered_darkmode";
+
 }  // namespace
 
-@implementation LensOverlayConsentViewController
+@implementation LensOverlayConsentViewController {
+  id<LottieAnimation> _animationViewWrapper;
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
@@ -48,13 +56,19 @@ const NSDirectionalEdgeInsets dialogInsets = {20.0f, 20.0f, 20.0f, 20.0f};
       @"Wanna use Lens Overlay? Please give your consent [todo:localize]";
   bodyLabel.numberOfLines = 0;
 
-  UIView* videoView = [[UIView alloc] init];
-  videoView.layer.cornerRadius = 15.0f;
-  videoView.backgroundColor = [UIColor lightGrayColor];
-  videoView.translatesAutoresizingMaskIntoConstraints = NO;
+  _animationViewWrapper =
+      self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark
+          ? [self createAnimation:kLensUserEducationDarkMode]
+          : [self createAnimation:kLensUserEducationLightMode];
+
+  _animationViewWrapper.animationView
+      .translatesAutoresizingMaskIntoConstraints = NO;
+  _animationViewWrapper.animationView.contentMode =
+      UIViewContentModeScaleAspectFit;
 
   UIStackView* verticalStack = [[UIStackView alloc] initWithArrangedSubviews:@[
-    titleLabel, videoView, bodyLabel, acceptButton, denyButton
+    titleLabel, _animationViewWrapper.animationView, bodyLabel, acceptButton,
+    denyButton
   ]];
   verticalStack.axis = UILayoutConstraintAxisVertical;
   verticalStack.alignment = UIStackViewAlignmentFill;
@@ -74,6 +88,11 @@ const NSDirectionalEdgeInsets dialogInsets = {20.0f, 20.0f, 20.0f, 20.0f};
   AddSameConstraintsWithInsets(verticalStack, self.view, dialogInsets);
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  [_animationViewWrapper play];
+}
+
 - (UIButton*)newButtonWithTitle:(NSString*)title
                   actionHandler:(UIActionHandler)handler {
   UIButtonConfiguration* buttonConfiguration =
@@ -87,6 +106,17 @@ const NSDirectionalEdgeInsets dialogInsets = {20.0f, 20.0f, 20.0f, 20.0f};
   button.translatesAutoresizingMaskIntoConstraints = NO;
 
   return button;
+}
+
+#pragma mark - Private
+
+// Creates and returns the LottieAnimation view for the `animationAssetName`.
+- (id<LottieAnimation>)createAnimation:(NSString*)animationAssetName {
+  LottieAnimationConfiguration* config =
+      [[LottieAnimationConfiguration alloc] init];
+  config.animationName = animationAssetName;
+  config.loopAnimationCount = -1;  // Always loop.
+  return ios::provider::GenerateLottieAnimation(config);
 }
 
 @end
