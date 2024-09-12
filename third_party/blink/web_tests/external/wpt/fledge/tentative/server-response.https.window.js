6942809aa6594f7b1820168b2a1dfe631e785300
Maks Orlovich <morlovich@chromium.org>
2024-09-12 20:42:42 +0000

    FLEDGE: basic WPT for hybrid B&A auctions.
    ...where a B&A auction is a component in a client-side auction.
    
    (And also remove some unused fields from pure B&A ones)
    
    Change-Id: Ic8e14a087d432af0a0f93880ed2c2278f74069cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5858038
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354798}

--- third_party/blink/web_tests/external/wpt/fledge/tentative/server-response.https.window.js
+++ third_party/blink/web_tests/external/wpt/fledge/tentative/server-response.https.window.js
@@ -43,7 +43,6 @@ subsetTest(promise_test, async test => {
 
   let auctionResult = await navigator.runAdAuction({
     'seller': window.location.origin,
-    'interestGroupBuyers': [window.location.origin],
     'requestId': result.requestId,
     'serverResponse': serverResponse,
     'resolveToConfig': true,
@@ -90,7 +89,6 @@ subsetTest(promise_test, async test => {
 
   let auctionResult = await navigator.runAdAuction({
     'seller': window.location.origin,
-    'interestGroupBuyers': [window.location.origin],
     'requestId': result.requestId,
     'serverResponse': serverResponse,
     'resolveToConfig': true,
@@ -99,6 +97,139 @@ subsetTest(promise_test, async test => {
   createAndNavigateFencedFrame(test, auctionResult);
   await waitForObservedRequests(uuid, [adB, trackBuyer, trackSeller]);
 }, 'Basic B&A auction with reporting URLs');
+
+subsetTest(promise_test, async test => {
+  const uuid = generateUuid(test);
+  const adA = createTrackerURL(window.location.origin, uuid, 'track_get', 'a');
+  const adB = createTrackerURL(window.location.origin, uuid, 'track_get', 'b');
+  const adsArray =
+      [{renderURL: adA, adRenderId: 'a'}, {renderURL: adB, adRenderId: 'b'}];
+  await joinInterestGroup(test, uuid, {
+    ads: adsArray,
+    biddingLogicURL: createBiddingScriptURL({allowComponentAuction: true})
+  });
+
+  const result = await navigator.getInterestGroupAdAuctionData(
+      {seller: window.location.origin});
+  assert_true(result.requestId !== null);
+  assert_true(result.request.length > 0);
+
+  let decoded = await BA.decodeInterestGroupData(result.request);
+
+  // The server-side auction uses a bid of 10, for second ad, so it should
+  // win over the client-side component auctions bid of 9.
+  let serverResponseMsg = {
+    'biddingGroups': {},
+    'adRenderURL': adsArray[1].renderURL,
+    'interestGroupName': DEFAULT_INTEREST_GROUP_NAME,
+    'interestGroupOwner': window.location.origin,
+    'topLevelSeller': window.location.origin,
+    'bid': 10,
+  };
+  serverResponseMsg.biddingGroups[window.location.origin] = [0];
+
+  let serverResponse =
+      await BA.encodeServerResponse(serverResponseMsg, decoded);
+
+  let hashString = await BA.payloadHash(serverResponse);
+  await BA.authorizeServerResponseHashes([hashString]);
+
+  let auctionConfig = {
+    seller: window.location.origin,
+    decisionLogicURL: createDecisionScriptURL(uuid),
+    interestGroupBuyers: [],
+    resolveToConfig: true,
+    componentAuctions: [
+      {
+        seller: window.location.origin,
+        decisionLogicURL: createDecisionScriptURL(uuid),
+        interestGroupBuyers: [window.location.origin],
+      },
+      {
+        seller: window.location.origin,
+        requestId: result.requestId,
+        serverResponse: serverResponse,
+      }
+    ]
+  };
+
+  let auctionResult = await navigator.runAdAuction(auctionConfig);
+  expectSuccess(auctionResult);
+  createAndNavigateFencedFrame(test, auctionResult);
+  await waitForObservedRequests(uuid, [adB]);
+}, 'Hybrid B&A auction');
+
+subsetTest(promise_test, async test => {
+  const uuid = generateUuid(test);
+  const adA = createTrackerURL(window.location.origin, uuid, 'track_get', 'a');
+  const adB = createTrackerURL(window.location.origin, uuid, 'track_get', 'b');
+  const adsArray =
+      [{renderURL: adA, adRenderId: 'a'}, {renderURL: adB, adRenderId: 'b'}];
+  await joinInterestGroup(test, uuid, {
+    ads: adsArray,
+    biddingLogicURL: createBiddingScriptURL({allowComponentAuction: true})
+  });
+
+  const result = await navigator.getInterestGroupAdAuctionData(
+      {seller: window.location.origin});
+  assert_true(result.requestId !== null);
+  assert_true(result.request.length > 0);
+
+  let decoded = await BA.decodeInterestGroupData(result.request);
+
+  // The server-side auction uses a bid of 10, for second ad, so it should
+  // win over the client-side component auctions bid of 9.
+  const trackServerSeller = createSellerReportURL(uuid);
+  const trackBuyer = createBidderReportURL(uuid);
+  let serverResponseMsg = {
+    'biddingGroups': {},
+    'adRenderURL': adsArray[1].renderURL,
+    'interestGroupName': DEFAULT_INTEREST_GROUP_NAME,
+    'interestGroupOwner': window.location.origin,
+    'topLevelSeller': window.location.origin,
+    'bid': 10,
+    'winReportingURLs': {
+      'buyerReportingURLs': {'reportingURL': trackBuyer},
+      'componentSellerReportingURLs': {'reportingURL': trackServerSeller}
+    }
+  };
+  serverResponseMsg.biddingGroups[window.location.origin] = [0];
+
+  let serverResponse =
+      await BA.encodeServerResponse(serverResponseMsg, decoded);
+
+  let hashString = await BA.payloadHash(serverResponse);
+  await BA.authorizeServerResponseHashes([hashString]);
+
+  let trackTopSeller = createSellerReportURL(uuid, 'top');
+  let trackClientSeller = createSellerReportURL(uuid, 'client');
+  let auctionConfig = {
+    seller: window.location.origin,
+    decisionLogicURL: createDecisionScriptURL(
+        uuid, {reportResult: `sendReportTo("${trackTopSeller}")`}),
+    interestGroupBuyers: [],
+    resolveToConfig: true,
+    componentAuctions: [
+      {
+        seller: window.location.origin,
+        decisionLogicURL: createDecisionScriptURL(
+            uuid, {reportResult: `sendReportTo("${trackClientSeller}")`}),
+        interestGroupBuyers: [window.location.origin],
+      },
+      {
+        seller: window.location.origin,
+        requestId: result.requestId,
+        serverResponse: serverResponse,
+      }
+    ]
+  };
+
+  let auctionResult = await navigator.runAdAuction(auctionConfig);
+  expectSuccess(auctionResult);
+  createAndNavigateFencedFrame(test, auctionResult);
+  await waitForObservedRequests(
+      uuid, [adB, trackBuyer, trackServerSeller, trackTopSeller]);
+}, 'Hybrid B&A auction with reporting URLs');
 
 async function runFaultInjectTest(test, fault) {
   const uuid = generateUuid(test);
@@ -131,7 +262,6 @@ async function runFaultInjectTest(test, fault) {
 
   let auctionResult = await navigator.runAdAuction({
     'seller': window.location.origin,
-    'interestGroupBuyers': [window.location.origin],
     'requestId': result.requestId,
     'serverResponse': serverResponse,
     'resolveToConfig': true,
@@ -190,7 +320,6 @@ subsetTest(promise_test, async test => {
 
   let auctionResult = await navigator.runAdAuction({
     'seller': window.location.origin,
-    'interestGroupBuyers': [window.location.origin],
     'requestId': result.requestId,
     'serverResponse': serverResponse,
     'resolveToConfig': true,
@@ -229,7 +358,6 @@ subsetTest(promise_test, async test => {
 
   let auctionResult = await navigator.runAdAuction({
     'seller': window.location.origin,
-    'interestGroupBuyers': [window.location.origin],
     'requestId': result.requestId,
     'serverResponse': serverResponse,
     'resolveToConfig': true,
@@ -268,7 +396,6 @@ subsetTest(promise_test, async test => {
 
   let auctionResult = await navigator.runAdAuction({
     'seller': window.location.origin,
-    'interestGroupBuyers': [window.location.origin],
     'requestId': token(),
     'serverResponse': serverResponse,
     'resolveToConfig': true,
