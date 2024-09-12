cf2cca652b2596b32866896953b574d2ffbeeb1f
Rushan Suleymanov <rushans@google.com>
2024-09-12 14:53:32 +0000

    [Sync] Add tests for syncing shared tab positions
    Bug: 351357559
    Change-Id: I8c7a868e86cabef0ee639629697f284a744715cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839525
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354538}

--- chrome/browser/sync/test/integration/two_client_shared_tab_group_data_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_shared_tab_group_data_sync_test.cc
@@ -13,6 +13,7 @@
 #include "components/data_sharing/public/features.h"
 #include "components/saved_tab_groups/saved_tab_group.h"
 #include "components/saved_tab_groups/saved_tab_group_model.h"
+#include "components/saved_tab_groups/saved_tab_group_tab.h"
 #include "components/saved_tab_groups/saved_tab_group_test_utils.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -21,6 +22,7 @@ namespace tab_groups {
 namespace {
 
 using testing::ElementsAre;
+using testing::SizeIs;
 
 constexpr char kCollaborationId[] = "collaboration";
 
@@ -55,6 +57,21 @@ class TwoClientSharedTabGroupDataSyncTest : public SyncTest {
     GetSavedTabGroupModel(profile_index)->Add(std::move(group));
   }
 
+  void MoveTab(int profile_index,
+               const SavedTabGroup& group,
+               const SavedTabGroupTab& tab,
+               size_t new_index) {
+    GetSavedTabGroupModel(profile_index)
+        ->MoveTabInGroupTo(group.saved_guid(), tab.saved_tab_guid(), new_index);
+  }
+
+  // Blocks the caller until both profiles have the same shared tab groups.
+  bool WaitForMatchingModels() {
+    return SharedTabGroupsMatchChecker(*GetSavedTabGroupModel(0),
+                                       *GetSavedTabGroupModel(1))
+        .Wait();
+  }
+
  private:
   base::test::ScopedFeatureList feature_overrides_;
 };
@@ -74,9 +91,7 @@ IN_PROC_BROWSER_TEST_F(TwoClientSharedTabGroupDataSyncTest,
   group.AddTabLocally(tab_2);
   AddTabGroup(0, group);
 
-  ASSERT_TRUE(SharedTabGroupsMatchChecker(*GetSavedTabGroupModel(0),
-                                          *GetSavedTabGroupModel(1))
-                  .Wait());
+  ASSERT_TRUE(WaitForMatchingModels());
 
   ASSERT_THAT(GetAllTabGroups(1),
               ElementsAre(HasSharedGroupMetadata(
@@ -84,6 +99,61 @@ IN_PROC_BROWSER_TEST_F(TwoClientSharedTabGroupDataSyncTest,
   EXPECT_THAT(GetAllTabGroups(1).front().saved_tabs(),
               ElementsAre(HasTabMetadata("tab 1", "http://google.com/1"),
                           HasTabMetadata("tab 2", "http://google.com/2")));
+}
+
+IN_PROC_BROWSER_TEST_F(TwoClientSharedTabGroupDataSyncTest,
+                       ShouldSyncTabPositions) {
+  ASSERT_TRUE(SetupSync());
+
+  SavedTabGroup group(u"title", TabGroupColorId::kBlue,
+                      /*urls=*/{}, /*position=*/std::nullopt);
+  group.SetCollaborationId(kCollaborationId);
+  SavedTabGroupTab tab_1(GURL("http://google.com/1"), u"tab 1",
+                         group.saved_guid(), /*position=*/std::nullopt);
+  SavedTabGroupTab tab_2(GURL("http://google.com/2"), u"tab 2",
+                         group.saved_guid(), /*position=*/std::nullopt);
+  SavedTabGroupTab tab_3(GURL("http://google.com/3"), u"tab 3",
+                         group.saved_guid(), /*position=*/std::nullopt);
+  group.AddTabLocally(tab_1);
+  group.AddTabLocally(tab_2);
+  group.AddTabLocally(tab_3);
+  AddTabGroup(0, group);
+
+  ASSERT_TRUE(WaitForMatchingModels());
+  ASSERT_THAT(GetAllTabGroups(1),
+              ElementsAre(HasSharedGroupMetadata(
+                  "title", TabGroupColorId::kBlue, kCollaborationId)));
+  ASSERT_THAT(GetAllTabGroups(1).front().saved_tabs(),
+              ElementsAre(HasTabMetadata("tab 1", "http://google.com/1"),
+                          HasTabMetadata("tab 2", "http://google.com/2"),
+                          HasTabMetadata("tab 3", "http://google.com/3")));
+
+  // Move tab to the end.
+  MoveTab(0, group, tab_1, /*new_index=*/2);
+  ASSERT_TRUE(WaitForMatchingModels());
+  ASSERT_THAT(GetAllTabGroups(1), SizeIs(1));
+  EXPECT_THAT(GetAllTabGroups(1).front().saved_tabs(),
+              ElementsAre(HasTabMetadata("tab 2", "http://google.com/2"),
+                          HasTabMetadata("tab 3", "http://google.com/3"),
+                          HasTabMetadata("tab 1", "http://google.com/1")));
+
+  // Move tab in the middle.
+  MoveTab(0, group, tab_1, /*new_index=*/1);
+  ASSERT_TRUE(WaitForMatchingModels());
+  ASSERT_THAT(GetAllTabGroups(1), SizeIs(1));
+  EXPECT_THAT(GetAllTabGroups(1).front().saved_tabs(),
+              ElementsAre(HasTabMetadata("tab 2", "http://google.com/2"),
+                          HasTabMetadata("tab 1", "http://google.com/1"),
+                          HasTabMetadata("tab 3", "http://google.com/3")));
+
+  // Move tab to the beginning.
+  MoveTab(0, group, tab_1, /*new_index=*/0);
+  WaitForMatchingModels();
+  ASSERT_THAT(GetAllTabGroups(1), SizeIs(1));
+  EXPECT_THAT(GetAllTabGroups(1).front().saved_tabs(),
+              ElementsAre(HasTabMetadata("tab 1", "http://google.com/1"),
+                          HasTabMetadata("tab 2", "http://google.com/2"),
+                          HasTabMetadata("tab 3", "http://google.com/3")));
 }
 
 }  // namespace
