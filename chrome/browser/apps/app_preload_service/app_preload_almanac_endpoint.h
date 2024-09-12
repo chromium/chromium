2c6702f8f775fdce516bacd4caf3e7561d24b1e0
Tim Sergeant <tsergeant@chromium.org>
2024-09-12 03:14:06 +0000

    Almanac: Migrate Preloads to use automatic Context in Almanac requests
    Bug: b:363900327
    Change-Id: Ife33b72ad37e7a293319f493b7f0153912ec4427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856340
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354334}

--- chrome/browser/apps/app_preload_service/app_preload_almanac_endpoint.h
+++ chrome/browser/apps/app_preload_service/app_preload_almanac_endpoint.h
@@ -13,16 +13,9 @@
 #include "chrome/browser/apps/app_preload_service/preload_app_definition.h"
 
 class GURL;
+class Profile;
 
-namespace network {
-class SharedURLLoaderFactory;
-}  // namespace network
-
-namespace apps {
-
-struct DeviceInfo;
-
-namespace app_preload_almanac_endpoint {
+namespace apps::app_preload_almanac_endpoint {
 
 using GetInitialAppsCallback =
     base::OnceCallback<void(std::optional<std::vector<PreloadAppDefinition>>,
@@ -33,15 +26,11 @@ using GetInitialAppsCallback =
 // the App Provisioning Service API. `callback` will be called with a list of
 // (possibly zero) apps, or `std::nullopt` if an error occurred while
 // fetching apps.
-void GetAppsForFirstLogin(
-    const DeviceInfo& device_info,
-    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
-    GetInitialAppsCallback callback);
+void GetAppsForFirstLogin(Profile* profile, GetInitialAppsCallback callback);
 
 // Gets the URL for the App Provisioning Service endpoint. Exposed for tests.
 GURL GetServerUrl();
 
-}  // namespace app_preload_almanac_endpoint
-}  // namespace apps
+}  // namespace apps::app_preload_almanac_endpoint
 
 #endif  // CHROME_BROWSER_APPS_APP_PRELOAD_SERVICE_APP_PRELOAD_ALMANAC_ENDPOINT_H_
