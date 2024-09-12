fe1ab045bb44a840669dc00bcc964eaee29f574e
Will Harris <wfh@chromium.org>
2024-09-12 03:30:01 +0000

    OSCrypt: Add further support for Encryptor options into test_utils
    With this CL, the test Encryptors vended by the test_support
    library are more like production ones. They contain two keys
    both enabled for encryption, but only one is marked as OSCrypt
    Sync compatible.
    
    This means that test code can now call `GetInstance` with
    different options - `kNone` or `kEncryptSyncCompat` - and do
    more realistic testing of key upgrade scenarios.
    
    BUG=40241934
    
    Change-Id: I16a3d06a5b16610aa4ff408163847beb04255ece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856112
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354343}

--- components/os_crypt/async/browser/test_utils.h
+++ components/os_crypt/async/browser/test_utils.h
@@ -20,8 +20,10 @@ std::unique_ptr<OSCryptAsync> GetTestOSCryptAsyncForTesting(
 // Obtain a test Encryptor. This Encryptor will perform encryption using a
 // random key. The key for test Encryptors is different each time this function
 // is called, and different from the ones vended from the test OSCryptAsync
-// above.
-Encryptor GetTestEncryptorForTesting();
+// above. An `option` can be specified in the same way as calling `GetInstance`
+// on `OSCryptAsync`.
+Encryptor GetTestEncryptorForTesting(
+    Encryptor::Option option = Encryptor::Option::kNone);
 
 }  // namespace os_crypt_async
 
