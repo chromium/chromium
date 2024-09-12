f2bdb86b1c58bde70c0888474baf5f778082848e
Ramin Halavati <rhalavati@google.com>
2024-09-12 16:08:48 +0000

    Add crash key for ScreenAI file load.
    chrome_screen_ai.dll crashes in rare cases when loading a model file. To
    find out which model file is causing the crash, a crash key is added.
    
    AX-Relnotes: n/a
    Bug: 361733242
    Change-Id: I9a1050a958e9d08e46bad7a8980d9ba258fe9902
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857677
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354598}

--- services/screen_ai/screen_ai_service_impl.cc
+++ services/screen_ai/screen_ai_service_impl.cc
@@ -145,6 +145,15 @@ class ModelDataHolder {
     int64_t length = model_file->GetLength();
     CHECK_GE(buffer_size, length);
     CHECK_EQ(UNSAFE_TODO(model_file->Read(0, buffer, length)), length);
+
+// TODO(crbug.com/361733242): Remove after the crash is fixed.
+#if BUILDFLAG(IS_WIN)
+    // `relative_file_path` is from `files_list_main_content_extraction.txt`
+    // or `files_list_ocr.txt` and under 100 characters long.
+    static crash_reporter::CrashKeyString<100> crash_info(
+        "last_loaded_screen_ai_file");
+    crash_info.Set(relative_file_path);
+#endif
   }
 
   void AddModelFiles(base::flat_map<base::FilePath, base::File> model_files) {
