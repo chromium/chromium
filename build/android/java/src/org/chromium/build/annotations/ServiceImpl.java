cbf2dac7393c8a8cfc9b1d62e53419b4805efd6d
Andrew Grieve <agrieve@chromium.org>
2024-09-13 13:58:13 +0000

    Android: Make ServiceImpl have SOURCE retention rather than CLASS
    Has no impact, but might as well since CLASS is not necessary.
    
    Bug: 40901855
    Change-Id: Ic3de5575bdbc80e1fab8ad965c79d82d628a15bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857275
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355169}

--- build/android/java/src/org/chromium/build/annotations/ServiceImpl.java
+++ build/android/java/src/org/chromium/build/annotations/ServiceImpl.java
@@ -16,7 +16,7 @@ import java.lang.annotation.Target;
  * require an annotation processor).
  */
 @Target({ElementType.TYPE})
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.SOURCE)
 public @interface ServiceImpl {
     /** The service implemented by this class. */
     Class<?> value();
