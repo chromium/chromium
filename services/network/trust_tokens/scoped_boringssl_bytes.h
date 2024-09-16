8d83ccd9e6d549c47330c400eaa172371b9b5dc9
John Kim <johnykim@google.com>
2024-09-16 19:13:38 +0000

    [Code Health] Remove RAW_PTR_EXCLUSION for 'network::ScopedBoringsslBytes' and replaced with raw_ptr. Updated referenced function calls accordingly.
    Bug: 346693761
    Change-Id: I574b83aa090b146e1e3185c541b93c4c65413efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854588
    Reviewed-by: Aykut Bulut <aykutb@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: John Kim <johnykim@google.com>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356055}

--- services/network/trust_tokens/scoped_boringssl_bytes.h
+++ services/network/trust_tokens/scoped_boringssl_bytes.h
@@ -10,9 +10,8 @@
 #ifndef SERVICES_NETWORK_TRUST_TOKENS_SCOPED_BORINGSSL_BYTES_H_
 #define SERVICES_NETWORK_TRUST_TOKENS_SCOPED_BORINGSSL_BYTES_H_
 
-#include "base/check.h"
 #include "base/containers/span.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "third_party/boringssl/src/include/openssl/mem.h"
 
 namespace network {
@@ -37,21 +36,20 @@ namespace network {
 class ScopedBoringsslBytes {
  public:
   ScopedBoringsslBytes() = default;
-  ~ScopedBoringsslBytes() { OPENSSL_free(ptr_); }
+  ~ScopedBoringsslBytes() { OPENSSL_free(ptr_.ExtractAsDangling()); }
 
   bool is_valid() { return ptr_; }
-  uint8_t** mutable_ptr() { return &ptr_; }
+  raw_ptr<uint8_t>* mutable_ptr() { return &ptr_; }
   size_t* mutable_len() { return &len_; }
 
   base::span<const uint8_t> as_span() const {
     CHECK(ptr_);
-    return base::make_span(ptr_, len_);
+    return base::make_span(ptr_.get(), len_);
   }
 
  private:
   size_t len_ = 0;
-  // RAW_PTR_EXCLUSION: #addr-of
-  RAW_PTR_EXCLUSION uint8_t* ptr_ = nullptr;
+  raw_ptr<uint8_t> ptr_ = nullptr;
 };
 
 }  // namespace network
