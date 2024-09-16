4b5c12fdb7570b3f29e15f5169afcde510395167
Henrique Nakashima <hnakashima@chromium.org>
2024-09-16 18:52:34 +0000

    [Android] Replace .filter disable with @DisableIf in TabbedAppMenuTest
    This makes the tests run on Test Reviver so we can see the failures.
    Also prevent the filter to have to be added to new bots that are
    Android 14+.
    
    Bug: 354278364
    Change-Id: Id9b4b003dee5752b34b986e6bc87d6b54e038eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867369
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356019}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/TabbedAppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/TabbedAppMenuTest.java
@@ -9,6 +9,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import android.content.res.Configuration;
+import android.os.Build;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ListView;
@@ -33,6 +34,7 @@ import org.chromium.base.task.TaskTraits;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -79,6 +81,9 @@ import java.util.concurrent.TimeoutException;
 /** Tests tabbed mode app menu popup. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@DisableIf.Build(
+        sdk_is_greater_than = Build.VERSION_CODES.TIRAMISU,
+        message = "crbug.com/354278364")
 public class TabbedAppMenuTest {
     private static final int RENDER_TEST_REVISION = 2;
 
