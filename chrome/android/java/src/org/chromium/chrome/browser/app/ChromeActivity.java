dc533d6abac3f7bfb2be1dbc4eb618c413a78341
Nafis Abedin <nafisabedin@google.com>
2024-09-17 15:34:54 +0000

    [Clank Startup Metrics] Fix Overcounting Clank Cold Start Metric
    ColdStartTracker.wasColdOnFirstActivityCreationOrNow() should be paired
    with SimpleStartupForegroundSessionDetector to avoid overcounting
    when continuing across foreground -> background and back.
    
    (cherry picked from commit cfeda78a596b733281b210366c059ce6b8068c31)
    
    Bug: 365532007
    Change-Id: Id515c8eb9f0644b47d142872e7732e8013c850fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5846168
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1353254}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5868030
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Cr-Commit-Position: refs/branch-heads/6668@{#1343}
    Cr-Branched-From: 05bc664984ca075216b7f2198c88b9725bfa1b9b-refs/heads/main@{#1343869}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -125,6 +125,7 @@ import org.chromium.chrome.browser.layouts.LayoutManagerAppUtils;
 import org.chromium.chrome.browser.media.FullscreenVideoPictureInPictureController;
 import org.chromium.chrome.browser.metrics.LaunchMetrics;
 import org.chromium.chrome.browser.metrics.LegacyTabStartupMetricsTracker;
+import org.chromium.chrome.browser.metrics.SimpleStartupForegroundSessionDetector;
 import org.chromium.chrome.browser.metrics.StartupMetricsTracker;
 import org.chromium.chrome.browser.metrics.UmaActivityObserver;
 import org.chromium.chrome.browser.modaldialog.TabModalLifetimeHandler;
@@ -1196,7 +1197,9 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         if (isMainIntentLaunch) {
             RecordHistogram.recordBooleanHistogram(
                     "Startup.Android.MainIntentIsColdStart",
-                    ColdStartTracker.wasColdOnFirstActivityCreationOrNow());
+                    ColdStartTracker.wasColdOnFirstActivityCreationOrNow()
+                            && SimpleStartupForegroundSessionDetector
+                                    .runningCleanForegroundSession());
         }
 
         Tab tab = getActivityTab();
