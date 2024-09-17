d1152bd121b9db737036aac430cd989a107e0f6e
Tom Burgin <bur@google.com>
2024-09-17 15:31:07 +0000

    mac: update kGrossPadding
    current x86 slice size: 261488
    current kGrossPadding: 76k
    size without padding: 261488 - 76k - (32 × (76k ÷ 4k)) = 183056
    target in file: 288k - (16k / 2) = 280k
    slice target size: 280k - 16k offset = 264k
    new kGrossPadding: 264k - 183056 = 87280
    round kGrossPadding to closest 4k alignment: 84k
    
    16k (start of x86_64 slice) + 183056 (size without current padding) +
    84k (padding) +  (32 × (84k ÷ 4k)) (padding code signing) = 286128 or
    592 over target, perfect!
    
    
    Change-Id: I5530c3745c7b29dd4e7d20fb3dafda7d97344e04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870464
    Auto-Submit: Tom Burgin <bur@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356484}

--- chrome/app/chrome_exe_main_mac.cc
+++ chrome/app/chrome_exe_main_mac.cc
@@ -129,7 +129,7 @@ typedef int (*ChromeMainPtr)(int, char**);
 // revised. Hopefully a more elegant solution will become apparent before that's
 // required.
 #if !defined(DCHECK_ALWAYS_ON)
-__attribute__((used)) const char kGrossPaddingForCrbug1300598[76 * 1024] = {};
+__attribute__((used)) const char kGrossPaddingForCrbug1300598[84 * 1024] = {};
 #else
 // DCHECK builds are larger and therefore require less padding. See
 // https://crbug.com/1394196 for the calculations, and
