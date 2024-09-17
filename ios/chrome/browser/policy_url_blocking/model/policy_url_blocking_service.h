050cbbf79cc6bf755aa0eac02634648f0481ce65
Sylvain Defresne <sdefresne@chromium.org>
2024-09-17 12:47:48 +0000

    Remove unnecessary forward-declaration
    Bug: none
    Change-Id: I0667e44668de334543b3ebc1693f86517eae697b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5865477
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356423}

--- ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_service.h
+++ ios/chrome/browser/policy_url_blocking/model/policy_url_blocking_service.h
@@ -9,11 +9,7 @@
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
 #include "components/policy/core/browser/url_blocklist_manager.h"
-#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
-
-namespace web {
-class BrowserState;
-}  // namespace web
+#include "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 // Associates a policy::URLBlocklistManager instance with a BrowserState.
 class PolicyBlocklistService : public KeyedService {
