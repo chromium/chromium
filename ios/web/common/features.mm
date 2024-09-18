3fbfa80672c3e496e2ec296dd79f0761bd884be0
Janice Liu <janiceliu@chromium.org>
2024-09-18 21:37:31 +0000

    Enable UserAgentBugFixVersion Feature
    Launch ticket: https://chromestatus.com/feature/5084885065400320?gate=5845347679862784
    
    Bug: 350426233
    Change-Id: If7bb970e1e3f3c642930f79a139fea15188a81d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873283
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Auto-Submit: Janice Liu <janiceliu@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357328}

--- ios/web/common/features.mm
+++ ios/web/common/features.mm
@@ -112,7 +112,7 @@ BASE_FEATURE(kDisableRaccoon,
 
 BASE_FEATURE(kUserAgentBugFixVersion,
              "UserAgentBugFixVersion",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLogJavaScriptErrors,
              "LogJavaScriptErrors",
