5bf0dcaa19bebb8a63e27e05434d8b18fd9badb4
Lei Zhang <thestig@chromium.org>
2024-09-18 15:11:28 +0000

    [PDF Ink Signatures] Set tool type for StrokeInputs
    Right now, the tool type field in StrokeInputs is unspecified and
    defaults to `kUnknown`. Set it to `kMouse` instead, and leave TODOs to
    adjust it appropriately.
    
    Bug: 353942909
    Change-Id: Ideb6b1f79890fbcb105ca5f00d3fae870eb7cddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871261
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357100}

--- pdf/pdf_ink_module.cc
+++ pdf/pdf_ink_module.cc
@@ -337,8 +337,10 @@ bool PdfInkModule::StartStroke(const gfx::PointF& position) {
   state.page_index = page_index;
 
   // Start of the first segment of a stroke.
+  // TODO(crbug.com/353942909): Set `tool_type` appropriately.
   StrokeInputSegment segment;
   segment.push_back({
+      .tool_type = ink::StrokeInput::ToolType::kMouse,
       .position = {page_position.x(), page_position.y()},
   });
   state.inputs.push_back(std::move(segment));
@@ -695,7 +697,9 @@ void PdfInkModule::RecordStrokePosition(const gfx::PointF& position) {
   gfx::PointF canonical_position =
       ConvertEventPositionToCanonicalPosition(position, state.page_index);
   base::TimeDelta time_diff = base::Time::Now() - state.start_time.value();
+  // TODO(crbug.com/353942909): Set `tool_type` appropriately.
   state.inputs.back().push_back({
+      .tool_type = ink::StrokeInput::ToolType::kMouse,
       .position = {canonical_position.x(), canonical_position.y()},
       .elapsed_time =
           ink::Duration32::Seconds(static_cast<float>(time_diff.InSecondsF())),
