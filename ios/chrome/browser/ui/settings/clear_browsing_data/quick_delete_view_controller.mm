3eecc18ecd769c7e738ab03943dc74fa84b4993d
Cristian Ciacu <cristianciacu@google.com>
2024-09-18 21:16:46 +0000

    [iOS][QD] Handle dismissal of QD when 'esc' is pressed
    This CL treats the case when QD is dismissed by pressing the 'esc' key,
    because in the current implementation, the underlying page becomes
    irresponsive to the user. To solve that,
    `quick_delete_view_controller.mm` now implements UIResponder, such that
    when 'esc' is pressed, QD is dismissed properly.
    
    Bug: 367217284, 335387869
    Change-Id: Ibc139a0e997c2d172549df89d8ed2e6f85dbe608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5864293
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357310}

--- ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_view_controller.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_view_controller.mm
@@ -10,6 +10,7 @@
 #import "base/metrics/histogram_functions.h"
 #import "base/metrics/user_metrics.h"
 #import "components/browsing_data/core/browsing_data_utils.h"
+#import "ios/chrome/browser/keyboard/ui_bundled/UIKeyCommand+Chrome.h"
 #import "ios/chrome/browser/net/model/crurl.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/ui/bottom_sheet/table_view_bottom_sheet_view_controller+subclassing.h"
@@ -687,6 +688,24 @@ typedef NS_ENUM(NSInteger, ItemIdentifier) {
       NSDirectionalEdgeInsetsMake(kTrashIconContainerViewTopPadding, 0, 0, 0));
 
   return outerView;
+}
+
+#pragma mark - UIResponder
+
+// To always be able to register key commands via -keyCommands, the VC must be
+// able to become first responder.
+- (BOOL)canBecomeFirstResponder {
+  return YES;
+}
+
+- (NSArray*)keyCommands {
+  return @[ UIKeyCommand.cr_close ];
+}
+
+#pragma mark - KeyCommandActions
+
+- (void)keyCommand_close {
+  [self dismissQuickDelete];
 }
 
 @end
