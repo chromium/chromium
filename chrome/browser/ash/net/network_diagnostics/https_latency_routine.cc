b11a2210792f6111ba64e360d50cadbd062ef9e2
Jason Jeremy Iman <jasongustaman@google.com>
2024-09-18 05:25:39 +0000

    Allow to use system DNS client for ChromeOS DNS diagnostic
    The implementation was using "HostResolverSource::DNS" source to create
    the DNS resolving task. Using the source would mean to resolve only
    using Chrome built-in DNS client.
    
    When BuiltInDnsClientEnabled policy is set to false, the previous
    resolving code always fails as the result of the resolver source.
    
    This patch updates the resolver source to "HostResolverSource::ANY"
    (default behavior the source is unset such that Chrome will decide which
    resolver to use (i.e. Chrome built-in DNS client when possible,
    otherwise use the system DNS client).
    
    Bug: b/353448388
    Change-Id: I460c71d32e083ec25bf4ae7e3cfcefe28c1ee427
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5859477
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Jason Iman <jasongustaman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356913}

--- chrome/browser/ash/net/network_diagnostics/https_latency_routine.cc
+++ chrome/browser/ash/net/network_diagnostics/https_latency_routine.cc
@@ -131,10 +131,11 @@ void HttpsLatencyRoutine::AttemptNextResolution() {
   GURL url = hostnames_to_query_dns_.back();
   hostnames_to_query_dns_.pop_back();
 
+  // Resolver host parameter source must be unset or set to ANY in order for DNS
+  // queries with BuiltInDnsClientEnabled policy disabled to work (b/353448388).
   network::mojom::ResolveHostParametersPtr parameters =
       network::mojom::ResolveHostParameters::New();
   parameters->dns_query_type = net::DnsQueryType::A;
-  parameters->source = net::HostResolverSource::DNS;
   parameters->cache_usage =
       network::mojom::ResolveHostParameters::CacheUsage::DISALLOWED;
 
