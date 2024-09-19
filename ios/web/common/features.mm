6039f1f5946dd39cd01f5713bb5003cf9c03e569
Mike Dougherty <michaeldo@chromium.org>
2024-09-19 17:34:11 +0000

    Enable RemoveOldWebStateRestoration by default in M131
    Bug: 40276021
    Change-Id: If44be6e8637cc51c3daf1af453a936a06bab9b64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5602167
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357705}

--- ios/web/common/features.mm
+++ ios/web/common/features.mm
@@ -85,7 +85,7 @@ BASE_FEATURE(kForceSynthesizedRestoreSession,
 
 BASE_FEATURE(kRemoveOldWebStateRestoration,
              "RemoveOldWebStateRestoration",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDetectDestroyedNavigationContexts,
              "DetectDestroyedNavigationContexts",
