a320db5004177792c6d989d2ad1f5e5972eab4dc
Gyuyoung Kim <gyuyoung@igalia.com>
2024-09-19 07:56:37 +0000

    [CodeHealth][domain_reliability] Add curly braces as per the latest style guide
    Add a curly brace when the body is only one line in if-else statement
    because the exception doesn't apply to multi-keyword statements like
    if-else or do-while.
    
    Guideline: https://google.github.io/styleguide/cppguide.html#Formatting_Looping_Branching
    
    Bug: 346952289
    Change-Id: Ib5cf106363d271a26604d601956e12014088914d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839972
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357496}

--- components/domain_reliability/util.cc
+++ components/domain_reliability/util.cc
@@ -88,10 +88,11 @@ bool GetDomainReliabilityBeaconStatus(
     int http_response_code,
     std::string* beacon_status_out) {
   if (net_error == net::OK) {
-    if (http_response_code >= 400 && http_response_code < 600)
+    if (http_response_code >= 400 && http_response_code < 600) {
       *beacon_status_out = "http.error";
-    else
+    } else {
       *beacon_status_out = "ok";
+    }
     return true;
   }
 
