811078c4cf232b15805a2edba21f90655d387af2
Christine Ying <chrstne@google.com>
2024-09-19 16:25:02 +0000

    [PrivacyPolicy] Add dark mode support to privacy policy page
    dark mode screencast: https://screencast.googleplex.com/cast/NjY0OTk2NTA1MTkwNDAwMHw0ODExNTQ3MS03Zg
    light mode screencast: https://screencast.googleplex.com/cast/NTIzMzYxNjc0MDE1NTM5MnxmNmNkNjk5Ni1kNg
    
    Change-Id: Ife38de38796a988fdfa334a8ef9335c621673c42
    Bug: 365867510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874222
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357645}

--- chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_dialog_untrusted_ui.cc
+++ chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_dialog_untrusted_ui.cc
@@ -5,6 +5,9 @@
 #include "chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_dialog_untrusted_ui.h"
 
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/themes/theme_service.h"
+#include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
@@ -14,6 +17,7 @@
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/common/url_constants.h"
+#include "ui/native_theme/native_theme.h"
 #include "url/gurl.h"
 
 PrivacySandboxDialogUntrustedUIConfig::PrivacySandboxDialogUntrustedUIConfig()
@@ -41,13 +45,28 @@ PrivacySandboxDialogUntrustedUI::PrivacySandboxDialogUntrustedUI(
   untrusted_source->OverrideContentSecurityPolicy(
       network::mojom::CSPDirectiveName::ScriptSrc,
       "script-src chrome-untrusted://resources 'self' 'unsafe-inline';");
+  untrusted_source->OverrideContentSecurityPolicy(
+      network::mojom::CSPDirectiveName::StyleSrc,
+      "style-src 'self' chrome-untrusted://resources chrome-untrusted://theme "
+      "'unsafe-inline';");
 
   untrusted_source->AddResourcePath(
       chrome::kChromeUIUntrustedPrivacySandboxDialogPrivacyPolicyPath,
       IDR_PRIVACY_SANDBOX_PRIVACY_SANDBOX_PRIVACY_POLICY_HTML);
 
+  // Dark mode support.
+  ThemeService::BrowserColorScheme color_scheme =
+      ThemeServiceFactory::GetForProfile(Profile::FromWebUI(web_ui))
+          ->GetBrowserColorScheme();
+  bool is_dark_mode =
+      (color_scheme == ThemeService::BrowserColorScheme::kSystem)
+          ? ui::NativeTheme::GetInstanceForNativeUi()->ShouldUseDarkColors()
+          : color_scheme == ThemeService::BrowserColorScheme::kDark;
+
   untrusted_source->AddString("privacyPolicyURL",
-                              chrome::kPrivacyPolicyOnlineURLPath);
+                              is_dark_mode
+                                  ? chrome::kPrivacyPolicyOnlineDarkModeURLPath
+                                  : chrome::kPrivacyPolicyOnlineURLPath);
 
   untrusted_source->AddFrameAncestor(
       GURL(chrome::kChromeUIPrivacySandboxDialogURL));
--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -588,6 +588,10 @@ inline constexpr char16_t kCwsEnhancedSafeBrowsingLearnMoreURL[] =
 inline constexpr char kPrivacyPolicyOnlineURLPath[] =
     "https://policies.google.com/privacy/embedded";
 
+// The URL path to online privacy policy dark mode.
+inline constexpr char kPrivacyPolicyOnlineDarkModeURLPath[] =
+    "https://policies.google.com/privacy/embedded?color_scheme=dark";
+
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS_ASH)
 // "Learn more" URL for the enhanced playback notification dialog.
 inline constexpr char kEnhancedPlaybackNotificationLearnMoreURL[] =
