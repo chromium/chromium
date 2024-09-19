9954a39f07650e6788593651584868bca3e818d7
Nasser Al-shawwa <alshawwa@chromium.org>
2024-09-19 21:39:08 +0000

    Add command line switches for watermark opacity.
    This change does several things: it changes the default opacity
    to 4% for the fill and 6% for the border. It also allows users
    to set the value using the `--watermark-fill-opacity` and
    `--watermark-outline-opacity` command line switches.
    
    Bug: b/367371474
    Change-Id: Ia8ad025cd279529e46abeb6392ed9781a5547152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867387
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Zequan Wu <zequanwu@google.com>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357869}

--- chrome/browser/enterprise/watermark/watermark_view.cc
+++ chrome/browser/enterprise/watermark/watermark_view.cc
@@ -9,8 +9,12 @@
 #include <algorithm>
 #include <string>
 
+#include "base/command_line.h"
 #include "cc/paint/paint_canvas.h"
+#include "chrome/common/channel_info.h"
 #include "components/enterprise/watermarking/watermark.h"
+#include "components/version_info/version_info.h"
+#include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/canvas.h"
@@ -21,7 +25,50 @@
 namespace {
 constexpr float kTextSize = 24.0f;
 constexpr int kWatermarkBlockWidth = 350;
+
+// The following values are calculated from the required percentages, 4% for
+// fill opacity and 6% for outline opacity.
+constexpr int kDefaultFillOpacity = 0xb;
+constexpr int kDefaultOutlineOpacity = 0x11;
+
+// Default color values for outline and fill.
+constexpr SkColor kFillColor =
+    SkColorSetARGB(kDefaultFillOpacity, 0x00, 0x00, 0x00);
+constexpr SkColor kOutlineColor =
+    SkColorSetARGB(kDefaultOutlineOpacity, 0xff, 0xff, 0xff);
+
+// Command line switches that allow users to set fill and outline opacity values
+// as a percentage between 1 and 99, inclusive.
+constexpr char kWatermarkFillOpacityPercentFlag[] = "watermark-fill-opacity";
+constexpr char kWatermarkOutlineOpacityPercentFlag[] =
+    "watermark-outline-opacity";
+
+int GetClampedOpacity(int default_value, const char* cmd_opacity_percent_flag) {
+  base::CommandLine* cmd = base::CommandLine::ForCurrentProcess();
+  if (cmd->HasSwitch(cmd_opacity_percent_flag) &&
+      (chrome::GetChannel() != version_info::Channel::STABLE ||
+       chrome::GetChannel() != version_info::Channel::BETA)) {
+    int opacity = default_value;
+    if (base::StringToInt(cmd->GetSwitchValueASCII(cmd_opacity_percent_flag),
+                          &opacity)) {
+      return std::clamp(opacity, 1, 99) * 256 / 100;
+    }
+  }
+  return default_value;
 }
+
+SkColor GetFillColor() {
+  int opacity =
+      GetClampedOpacity(kDefaultFillOpacity, kWatermarkFillOpacityPercentFlag);
+  return SkColorSetA(kFillColor, opacity);
+}
+
+SkColor GetOutlineColor() {
+  int opacity = GetClampedOpacity(kDefaultOutlineOpacity,
+                                  kWatermarkOutlineOpacityPercentFlag);
+  return SkColorSetA(kOutlineColor, opacity);
+}
+}  // namespace
 
 namespace enterprise_watermark {
 
@@ -50,8 +97,9 @@ void WatermarkView::SetString(const std::string& text) {
     // The coordinates here do not matter as the display rect will change for
     // each drawn block.
     gfx::Rect display_rect(0, 0, kWatermarkBlockWidth, 0);
-    text_fill_ = CreateFillRenderText(display_rect, utf16_text);
-    text_outline_ = CreateOutlineRenderText(display_rect, utf16_text);
+    text_fill_ = CreateFillRenderText(display_rect, utf16_text, GetFillColor());
+    text_outline_ =
+        CreateOutlineRenderText(display_rect, utf16_text, GetOutlineColor());
 
     // `block_height_` is going to be the max required height for a single line
     // times the number of line.
