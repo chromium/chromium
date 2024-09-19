fc3407751b109614c38e1a3962ab0cabdfc4506c
Michael Wilson <mjwilson@chromium.org>
2024-09-19 18:44:00 +0000

    Prepare SetSinkIdResolver for consolidation into AudioContext
    In order to better control the SetSinkId behavior, I want to move the
    SetSinkIdResolver functionality directly into AudioContext.
    
    Before attempting to do that, do minor cleanup that was discovered
    during the feasibility investigation:
    - Call OnSetSinkIdComplete directly instead of as a callback for error
      states
    - Collapse NotifySetSinkIdIsDone()
    - Hide implementation of Resolver()
    - Minor whitespace, ordering, and naming adjustments
    - Mark the class as final
    
    This should cause no functional changes.
    
    Change-Id: I8f69aebbcb614f08d0aaf53fa34813d49b3677fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870926
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357756}

--- third_party/blink/renderer/modules/webaudio/setsinkid_resolver.cc
+++ third_party/blink/renderer/modules/webaudio/setsinkid_resolver.cc
@@ -21,6 +21,7 @@ SetSinkIdResolver::SetSinkIdResolver(
       resolver_(MakeGarbageCollected<ScriptPromiseResolver<IDLUndefined>>(
           script_state)) {
   DCHECK(IsMainThread());
+
   // Currently the only available AudioSinkOptions is a type of a silent sink,
   // which can be specified by an empty descriptor constructor.
   auto& frame_token = To<LocalDOMWindow>(audio_context_->GetExecutionContext())
@@ -38,40 +39,43 @@ SetSinkIdResolver::SetSinkIdResolver(
                audio_utilities::GetSinkIdForTracing(sink_descriptor_));
 }
 
+void SetSinkIdResolver::Trace(Visitor* visitor) const {
+  visitor->Trace(audio_context_);
+  visitor->Trace(resolver_);
+}
+
 void SetSinkIdResolver::Start() {
   TRACE_EVENT1("webaudio", "SetSinkIdResolver::Start", "sink_id",
                audio_utilities::GetSinkIdForTracing(sink_descriptor_));
   DCHECK(IsMainThread());
 
-  ExecutionContext* context = resolver_->GetExecutionContext();
-  if (!context || !audio_context_ || audio_context_->IsContextCleared()) {
+  auto* execution_context = resolver_->GetExecutionContext();
+  if (!execution_context || !audio_context_ ||
+      audio_context_->IsContextCleared()) {
     // No point in rejecting promise, as it will bail out upon detached
     // context anyway.
     return;
   }
 
-  auto set_sink_id_completion_callback = WTF::BindOnce(
-      &SetSinkIdResolver::OnSetSinkIdComplete, WrapPersistent(this));
-
   // Refer to
   // https://webaudio.github.io/web-audio-api/#validating-sink-identifier for
   // sink_id/sink_descriptor validation steps.
   if (sink_descriptor_ == audio_context_->GetSinkDescriptor()) {
-    std::move(set_sink_id_completion_callback)
-        .Run(media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_OK);
+    OnSetSinkIdComplete(media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_OK);
   } else if (!audio_context_->IsValidSinkDescriptor(sink_descriptor_)) {
-    std::move(set_sink_id_completion_callback)
-        .Run(media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_ERROR_NOT_FOUND);
+    OnSetSinkIdComplete(
+        media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_ERROR_NOT_FOUND);
   } else {
     auto* audio_destination = audio_context_->destination();
-
     // A sanity check to make sure we have valid audio_destination node from
     // `audio_context_`.
     if (!audio_destination) {
-      std::move(set_sink_id_completion_callback)
-          .Run(media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_ERROR_INTERNAL);
+      OnSetSinkIdComplete(
+          media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_ERROR_INTERNAL);
     } else {
       audio_context_->NotifySetSinkIdBegins();
+      auto set_sink_id_completion_callback = WTF::BindOnce(
+          &SetSinkIdResolver::OnSetSinkIdComplete, WrapPersistent(this));
       auto set_sink_descriptor_callback = WTF::BindOnce(
           &RealtimeAudioDestinationNode::SetSinkDescriptor,
           WrapWeakPersistent(
@@ -82,6 +86,10 @@ void SetSinkIdResolver::Start() {
           ->PostTask(FROM_HERE, std::move(set_sink_descriptor_callback));
     }
   }
+}
+
+ScriptPromiseResolver<IDLUndefined>* SetSinkIdResolver::Resolver() {
+  return resolver_;
 }
 
 void SetSinkIdResolver::OnSetSinkIdComplete(media::OutputDeviceStatus status) {
@@ -96,11 +104,12 @@ void SetSinkIdResolver::OnSetSinkIdComplete(media::OutputDeviceStatus status) {
 
   ScriptState* script_state = resolver_->GetScriptState();
   ScriptState::Scope scope(script_state);
-
   switch (status) {
     case media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_OK:
-      // Update AudioContext's sink ID and fire the 'onsinkchange' event
-      NotifySetSinkIdIsDone();
+      if (audio_context_ && !audio_context_->IsContextCleared()) {
+        // Update AudioContext's sink ID and fire the 'onsinkchange' event
+        audio_context_->NotifySetSinkIdIsDone(sink_descriptor_);
+      }
       resolver_->Resolve();
       break;
     case media::OutputDeviceStatus::OUTPUT_DEVICE_STATUS_ERROR_NOT_FOUND:
@@ -126,9 +135,7 @@ void SetSinkIdResolver::OnSetSinkIdComplete(media::OutputDeviceStatus status) {
   }
 
   auto& resolvers = audio_context_->GetSetSinkIdResolver();
-
   resolvers.pop_front();
-
   if (!resolvers.empty()) {
     // Prevent potential stack overflow under heavy load by scheduling the next
     // resolver start asynchronously instead of invoking it directly.
@@ -138,21 +145,6 @@ void SetSinkIdResolver::OnSetSinkIdComplete(media::OutputDeviceStatus status) {
         ->GetTaskRunner(TaskType::kInternalMediaRealTime)
         ->PostTask(FROM_HERE, std::move(next_start_task));
   }
-}
-
-void SetSinkIdResolver::NotifySetSinkIdIsDone() {
-  DCHECK(IsMainThread());
-
-  if (!audio_context_ || audio_context_->IsContextCleared()) {
-    return;
-  }
-
-  audio_context_->NotifySetSinkIdIsDone(sink_descriptor_);
-}
-
-void SetSinkIdResolver::Trace(Visitor* visitor) const {
-  visitor->Trace(audio_context_);
-  visitor->Trace(resolver_);
 }
 
 }  // namespace blink
