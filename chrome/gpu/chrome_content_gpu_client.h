7943e28883c27ee3824284adadc4efed0805f596
Jose Dapena Paz <jdapena@igalia.com>
2024-09-20 06:53:49 +0000

    libstdc++: avoid requiring ThreadProfiler include
    Fix compilation error with libstdc++ because of setting nullptr as the
    default value of ChromeContentGpuClient::main_thread_profiler_
    unique_ptr.
    
    In libstdc++ implementation, assigning a default value to a unique_ptr
    requires the type declaration. But nullptr is actually the default
    value already, so it is not needed.
    
    Bug: 41455655
    Change-Id: Ifd3f2fd312186d76e1884f36e7077b7af27eafd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873245
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1358024}

--- chrome/gpu/chrome_content_gpu_client.h
+++ chrome/gpu/chrome_content_gpu_client.h
@@ -49,8 +49,7 @@ class ChromeContentGpuClient : public content::ContentGpuClient {
 
  private:
   // Used to profile main thread startup.
-  std::unique_ptr<sampling_profiler::ThreadProfiler> main_thread_profiler_ =
-      nullptr;
+  std::unique_ptr<sampling_profiler::ThreadProfiler> main_thread_profiler_;
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   scoped_refptr<arc::ProtectedBufferManager> protected_buffer_manager_;
