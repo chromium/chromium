4576d43e2f05b2ca4a2ed0fe2990ae4daaf67eec
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-09-20 09:14:06 +0000

    Explicitly declare ProfileStatistic keyed service dependencies
    KeyedServices should use DependsOn() for their dependencies to
    make sure that destruction order is correct.
    
    Bug: 368297674
    Change-Id: Ifa7b76ca370bffb0c533dab5eab055d042e334bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872532
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358066}

--- chrome/browser/profiles/profile_statistics_unittest.cc
+++ chrome/browser/profiles/profile_statistics_unittest.cc
@@ -4,56 +4,46 @@
 
 #include "chrome/browser/profiles/profile_statistics.h"
 
-#include <map>
 #include <memory>
-#include <set>
 #include <utility>
-#include <vector>
 
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
-#include "base/run_loop.h"
-#include "chrome/browser/bookmarks/bookmark_model_factory.h"
-#include "chrome/browser/bookmarks/chrome_bookmark_client.h"
-#include "chrome/browser/bookmarks/managed_bookmark_service_factory.h"
-#include "chrome/browser/history/history_service_factory.h"
-#include "chrome/browser/password_manager/profile_password_store_factory.h"
+#include "base/test/test_file_util.h"
 #include "chrome/browser/profiles/profile_statistics_aggregator.h"
-#include "chrome/browser/profiles/profile_statistics_common.h"
-#include "chrome/browser/profiles/profile_statistics_factory.h"
-#include "chrome/browser/sync/account_bookmark_sync_service_factory.h"
-#include "chrome/browser/sync/local_or_syncable_bookmark_sync_service_factory.h"
-#include "chrome/browser/undo/bookmark_undo_service_factory.h"
-#include "chrome/browser/webdata_services/web_data_service_factory.h"
-#include "chrome/common/pref_names.h"
-#include "chrome/test/base/testing_browser_process.h"
-#include "chrome/test/base/testing_profile.h"
-#include "chrome/test/base/testing_profile_manager.h"
+#include "components/autofill/core/browser/test_personal_data_manager.h"
+#include "components/autofill/core/browser/webdata/autocomplete/autocomplete_table.h"
+#include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/bookmarks/browser/bookmark_model.h"
-#include "components/password_manager/core/browser/password_manager_test_utils.h"
+#include "components/bookmarks/test/test_bookmark_client.h"
+#include "components/history/core/browser/history_database_params.h"
+#include "components/history/core/browser/history_service.h"
+#include "components/os_crypt/async/browser/test_utils.h"
 #include "components/password_manager/core/browser/password_store/test_password_store.h"
-#include "components/prefs/pref_service.h"
-#include "components/sync_bookmarks/bookmark_sync_service.h"
-#include "content/public/browser/browser_task_traits.h"
-#include "content/public/browser/browser_thread.h"
+#include "components/prefs/testing_pref_service.h"
+#include "components/webdata/common/web_database.h"
+#include "components/webdata/common/web_database_service.h"
 #include "content/public/test/browser_task_environment.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
 
-std::unique_ptr<KeyedService> BuildBookmarkModelWithoutLoad(
-    content::BrowserContext* context) {
-  Profile* profile = Profile::FromBrowserContext(context);
-  return std::make_unique<bookmarks::BookmarkModel>(
-      std::make_unique<ChromeBookmarkClient>(
-          profile, ManagedBookmarkServiceFactory::GetForProfile(profile),
-          LocalOrSyncableBookmarkSyncServiceFactory::GetForProfile(profile),
-          AccountBookmarkSyncServiceFactory::GetForProfile(profile),
-          BookmarkUndoServiceFactory::GetForProfile(profile)));
-}
-
-void LoadBookmarkModel(Profile* profile) {
-  BookmarkModelFactory::GetForBrowserContext(profile)->Load(profile->GetPath());
+scoped_refptr<autofill::AutofillWebDataService> BuildFakeAutofillWebDataService(
+    scoped_refptr<base::SequencedTaskRunner> task_runner,
+    os_crypt_async::OSCryptAsync* os_crypt) {
+  scoped_refptr<WebDatabaseService> web_database_service =
+      base::MakeRefCounted<WebDatabaseService>(
+          base::FilePath(WebDatabase::kInMemoryPath),
+          /*ui_task_runner=*/task_runner,
+          /*db_task_runner=*/task_runner);
+  // The 1 table needed by the *Autofill*WebDataService is the autocomplete one.
+  web_database_service->AddTable(
+      std::make_unique<autofill::AutocompleteTable>());
+  web_database_service->LoadDatabase(os_crypt);
+  return base::MakeRefCounted<autofill::AutofillWebDataService>(
+      std::move(web_database_service),
+      /*ui_task_runner=*/task_runner);
 }
 
 class BookmarkStatHelper {
@@ -72,53 +62,57 @@ class BookmarkStatHelper {
 
 class ProfileStatisticsTest : public testing::Test {
  public:
-  ProfileStatisticsTest() : manager_(TestingBrowserProcess::GetGlobal()) {}
-  ~ProfileStatisticsTest() override = default;
-
- protected:
-  void SetUp() override {
-    ASSERT_TRUE(manager_.SetUp());
+  ProfileStatisticsTest() {
+    history_service_.Init(history::HistoryDatabaseParams(
+        base::CreateUniqueTempDirectoryScopedToTest(),
+        /*download_interrupt_reason_none=*/0,
+        /*download_interrupt_reason_crash=*/0, version_info::Channel::UNKNOWN));
+    profile_password_store_->Init(&pref_service_,
+                                  /*affiliated_match_helper=*/nullptr);
+    autofill_web_data_service_->Init(base::DoNothing());
   }
 
-  void TearDown() override {
+  ~ProfileStatisticsTest() override {
+    profile_password_store_->ShutdownOnUIThread();
   }
 
-  TestingProfileManager* manager() { return &manager_; }
+  std::unique_ptr<ProfileStatisticsAggregator> CreateAggregator(
+      base::OnceClosure done_callback) {
+    return std::make_unique<ProfileStatisticsAggregator>(
+        autofill_web_data_service_, &personal_data_manager_, &bookmark_model_,
+        &history_service_, profile_password_store_, &pref_service_,
+        /*platform_credential_store=*/nullptr, std::move(done_callback));
+  }
+
+  bookmarks::BookmarkModel* bookmark_model() { return &bookmark_model_; }
 
  private:
   content::BrowserTaskEnvironment task_environment_;
-  TestingProfileManager manager_;
+  TestingPrefServiceSimple pref_service_;
+  autofill::TestPersonalDataManager personal_data_manager_;
+  bookmarks::BookmarkModel bookmark_model_{
+      std::make_unique<bookmarks::TestBookmarkClient>()};
+  history::HistoryService history_service_;
+  const scoped_refptr<password_manager::TestPasswordStore>
+      profile_password_store_ =
+          base::MakeRefCounted<password_manager::TestPasswordStore>();
+  const std::unique_ptr<os_crypt_async::OSCryptAsync> os_crypt_ =
+      os_crypt_async::GetTestOSCryptAsyncForTesting(
+          /*is_sync_for_unittests=*/true);
+  const scoped_refptr<autofill::AutofillWebDataService>
+      autofill_web_data_service_ = BuildFakeAutofillWebDataService(
+          task_environment_.GetMainThreadTaskRunner(),
+          os_crypt_.get());
 };
 
 TEST_F(ProfileStatisticsTest, WaitOrCountBookmarks) {
-  // We need history, autofill and password services for the test to succeed.
-  TestingProfile* profile = manager()->CreateTestingProfile(
-      "Test 1",
-      /*testing_factories=*/
-      {TestingProfile::TestingFactory{
-           BookmarkModelFactory::GetInstance(),
-           base::BindRepeating(&BuildBookmarkModelWithoutLoad)},
-       TestingProfile::TestingFactory{
-           HistoryServiceFactory::GetInstance(),
-           HistoryServiceFactory::GetDefaultFactory()},
-       TestingProfile::TestingFactory{
-           WebDataServiceFactory::GetInstance(),
-           WebDataServiceFactory::GetDefaultFactory()},
-       TestingProfile::TestingFactory{
-           ProfilePasswordStoreFactory::GetInstance(),
-           base::BindRepeating(&password_manager::BuildPasswordStore<
-                               content::BrowserContext,
-                               password_manager::TestPasswordStore>)}});
-
-  ASSERT_TRUE(profile);
-
   // Run ProfileStatisticsAggregator::WaitOrCountBookmarks.
   BookmarkStatHelper bookmark_stat_helper;
   base::RunLoop run_loop_aggregator_done;
 
-  ProfileStatisticsAggregator aggregator(
-      profile, run_loop_aggregator_done.QuitClosure());
-  aggregator.AddCallbackAndStartAggregator(
+  std::unique_ptr<ProfileStatisticsAggregator> aggregator =
+      CreateAggregator(run_loop_aggregator_done.QuitClosure());
+  aggregator->AddCallbackAndStartAggregator(
       base::BindRepeating(&BookmarkStatHelper::StatsCallback,
                           base::Unretained(&bookmark_stat_helper)));
 
@@ -128,7 +122,7 @@ TEST_F(ProfileStatisticsTest, WaitOrCountBookmarks) {
   EXPECT_EQ(0, bookmark_stat_helper.GetNumOfTimesCalled());
 
   // Run ProfileStatisticsAggregator::WaitOrCountBookmarks again.
-  aggregator.AddCallbackAndStartAggregator(
+  aggregator->AddCallbackAndStartAggregator(
       profiles::ProfileStatisticsCallback());
   // Wait until ProfileStatisticsAggregator::WaitOrCountBookmarks is run.
   base::RunLoop run_loop2;
@@ -137,7 +131,7 @@ TEST_F(ProfileStatisticsTest, WaitOrCountBookmarks) {
 
   // Load the bookmark model. When the model is loaded (asynchronously), the
   // observer added by WaitOrCountBookmarks is run.
-  LoadBookmarkModel(profile);
+  bookmark_model()->LoadEmptyForTest();
 
   run_loop_aggregator_done.Run();
   EXPECT_EQ(1, bookmark_stat_helper.GetNumOfTimesCalled());
