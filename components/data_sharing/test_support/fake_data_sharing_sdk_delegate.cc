20f4a2d76245c066b150e720edbad77df32375a6
Maksim Moskvitin <mmoskvitin@google.com>
2024-09-20 09:29:37 +0000

    [DataSharing] Move FakeDataSharingSDKDelegate to test_support
    It seems to belong there.
    
    Bug: 301390275
    Change-Id: Ic6048b41df0a3e001fc1821e87e1481df310edf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5875960
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358071}

--- components/data_sharing/test_support/fake_data_sharing_sdk_delegate.cc
+++ components/data_sharing/test_support/fake_data_sharing_sdk_delegate.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/data_sharing/internal/fake_data_sharing_sdk_delegate.h"
+#include "components/data_sharing/test_support/fake_data_sharing_sdk_delegate.h"
 
 #include "base/strings/string_number_conversions.h"
 #include "base/task/single_thread_task_runner.h"
