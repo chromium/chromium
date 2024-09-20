f12ff7dcc1be661fc4a408459b3f2eb6319766d0
Cristian Ciacu <cristianciacu@google.com>
2024-09-20 10:43:10 +0000

    [M130][iOS][QD] Handle dismissal of QD when 'esc' is pressed
    This CL treats the case when QD is dismissed by pressing the 'esc' key,
    because in the current implementation, the underlying page becomes
    irresponsive to the user. To solve that,
    `quick_delete_view_controller.mm` now implements UIResponder, such that
    when 'esc' is pressed, QD is dismissed properly.
    
    (cherry picked from commit 3eecc18ecd769c7e738ab03943dc74fa84b4993d)
    
    Bug: 367217284, 335387869
    Change-Id: Ibc139a0e997c2d172549df89d8ed2e6f85dbe608
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5864293
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1357310}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873270
    Auto-Submit: Filipa Senra <fsenra@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6723@{#172}
    Cr-Branched-From: 985f2961df230630f9cbd75bd6fe463009855a11-refs/heads/main@{#1356013}

--- ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_view_controller.h
+++ ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_view_controller.h
@@ -5,6 +5,7 @@
 #ifndef IOS_CHROME_BROWSER_UI_SETTINGS_CLEAR_BROWSING_DATA_QUICK_DELETE_VIEW_CONTROLLER_H_
 #define IOS_CHROME_BROWSER_UI_SETTINGS_CLEAR_BROWSING_DATA_QUICK_DELETE_VIEW_CONTROLLER_H_
 
+#import "ios/chrome/browser/keyboard/ui_bundled/key_command_actions.h"
 #import "ios/chrome/browser/shared/ui/bottom_sheet/table_view_bottom_sheet_view_controller.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_consumer.h"
 
@@ -14,7 +15,7 @@
 // View controller for Quick Delete, the new vesion of Clear/Delete Browsing
 // Data.
 @interface QuickDeleteViewController
-    : BottomSheetViewController <QuickDeleteConsumer>
+    : BottomSheetViewController <QuickDeleteConsumer, KeyCommandActions>
 
 // Local dispatcher for this `QuickDeleteViewController`.
 @property(nonatomic, weak) id<QuickDeletePresentationCommands>
