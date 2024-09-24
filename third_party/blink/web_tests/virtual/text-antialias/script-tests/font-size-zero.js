cc49538580f514e9afc75c70cfecb71d97412ec4
Kent Tamura <tkent@chromium.org>
2024-09-24 07:30:44 +0000

    Remove web_tests/virtual/text-antialias/script-tests/
    The "script-tests" framework is obsoleted. This CL folds JavaScript
    files to HTML files, and convert them for testharness.js.
    
    This CL has no behavior changes.
    
    Change-Id: Idddd985a71525dc1446f98dce26b0345d765aaba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5884915
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359210}

--- third_party/blink/web_tests/virtual/text-antialias/script-tests/font-size-zero.js
+++
@@ -1,9 +0,0 @@
-var s = document.createElement('span');
-s.style.fontSize = 0;
-s.innerHTML = 'Text';
-document.body.appendChild(s);
-
-shouldBe("s.getBoundingClientRect().height", "0");
-shouldBe("s.getBoundingClientRect().width", "0");
-
-document.body.removeChild(s);
