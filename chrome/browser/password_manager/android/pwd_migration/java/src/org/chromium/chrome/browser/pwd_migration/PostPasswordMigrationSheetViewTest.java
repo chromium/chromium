59a0e258a4095cdcb7d303b82a148766a4ebcfd2
Sinan Sahin <sinansahin@google.com>
2024-09-24 17:15:51 +0000

    🌱 Disable PostPasswordMigrationSheetViewTest#sheetSetsTheTitleAndSubtitleAboutLocalPasswords
    Bug: 369371078
    Change-Id: I73c6faa3bd069eafeaa944832c7027746c9a4746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5888064
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Owners-Override: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1359438}

--- chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PostPasswordMigrationSheetViewTest.java
+++ chrome/browser/password_manager/android/pwd_migration/java/src/org/chromium/chrome/browser/pwd_migration/PostPasswordMigrationSheetViewTest.java
@@ -39,6 +39,7 @@ import org.mockito.quality.Strictness;
 import org.chromium.base.Callback;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
@@ -193,6 +194,7 @@ public class PostPasswordMigrationSheetViewTest {
     @Test
     @MediumTest
     @EnableFeatures(UNIFIED_PASSWORD_MANAGER_LOCAL_PASSWORDS_ANDROID_ACCESS_LOSS_WARNING)
+    @DisabledTest(message = "crbug.com/369371078")
     public void sheetSetsTheTitleAndSubtitleAboutLocalPasswords() {
         // The sheet is shown.
         runOnUiThreadBlocking(() -> mModel.set(VISIBLE, true));
