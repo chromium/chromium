ab34a7731f0a55052e3dc26f2de4622d98c8d6b8
Yuki Awano <yawano@google.com>
2024-09-25 16:18:14 +0000

    assistant: update libassistant sandbox policy
    - Internal Linux-ChromeOS libassistant binary requires additional
      syscalls to work. Add them to the sandbox policy.
    
    Bug: b:362834495
    Fixed: b:362834495
    Test: manually tested as described in the bug
    Change-Id: If2530d63f46a91cf5261f6584c9ba3f46257ab21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5876922
    Commit-Queue: Yuki Awano <yawano@google.com>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359980}

--- sandbox/policy/linux/bpf_libassistant_policy_linux.cc
+++ sandbox/policy/linux/bpf_libassistant_policy_linux.cc
@@ -4,10 +4,13 @@
 
 #include "sandbox/policy/linux/bpf_libassistant_policy_linux.h"
 
+#include <linux/membarrier.h>
 #include <netinet/tcp.h>
 #include <sys/socket.h>
 
+#include "base/system/sys_info.h"
 #include "sandbox/linux/bpf_dsl/bpf_dsl.h"
+#include "sandbox/linux/seccomp-bpf-helpers/sigsys_handlers.h"
 #include "sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.h"
 #include "sandbox/linux/seccomp-bpf-helpers/syscall_sets.h"
 #include "sandbox/linux/syscall_broker/broker_process.h"
@@ -18,12 +21,44 @@ using sandbox::bpf_dsl::Allow;
 using sandbox::bpf_dsl::Arg;
 using sandbox::bpf_dsl::Error;
 using sandbox::bpf_dsl::If;
+using sandbox::bpf_dsl::Kill;
 using sandbox::bpf_dsl::ResultExpr;
 using sandbox::bpf_dsl::Trap;
 using sandbox::syscall_broker::BrokerProcess;
 
 namespace sandbox {
 namespace policy {
+namespace {
+
+// Those are additional syscalls required for internal Linux-ChromeOS build:
+// b/362834495.
+bool IsInternalLinuxCrosSyscalls(int sysno) {
+  if (base::SysInfo::IsRunningOnChromeOS()) {
+    return false;
+  }
+
+  return sysno == __NR_membarrier || sysno == __NR_epoll_pwait2;
+}
+
+ResultExpr RestrictInternalLinuxCrosSyscalls(int sysno) {
+  switch (sysno) {
+    case __NR_membarrier: {
+      const Arg<int> command(0);
+      return If(AnyOf(command == MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED_RSEQ,
+                      command == MEMBARRIER_CMD_PRIVATE_EXPEDITED_RSEQ),
+                Allow())
+          .Else(CrashSIGSYS());
+    }
+    case __NR_epoll_pwait2:
+      return Allow();
+  }
+
+  CHECK(false) << "RestrictInternalLinuxCrosSyscalls must be called after "
+                  "IsInternalLinuxCrosSyscalls";
+
+  return Kill();
+}
+}  // namespace
 
 LibassistantProcessPolicy::LibassistantProcessPolicy() = default;
 LibassistantProcessPolicy::~LibassistantProcessPolicy() = default;
@@ -66,6 +101,10 @@ ResultExpr LibassistantProcessPolicy::EvaluateSyscall(int sysno) const {
     case __NR_sync:
       return Allow();
     default:
+      if (IsInternalLinuxCrosSyscalls(sysno)) {
+        return RestrictInternalLinuxCrosSyscalls(sysno);
+      }
+
       if (SyscallSets::IsGoogle3Threading(sysno)) {
         return RestrictGoogle3Threading(sysno);
       }
