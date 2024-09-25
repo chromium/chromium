95b6bda307839eb5f6148a003c162ba0e9c3759c
Ioana Pandele <ioanap@chromium.org>
2024-09-25 14:18:29 +0000

    [Gardener] Disable flaky  PrivacySandboxDialogTest on arm builds
    PrivacySandboxDialogTest#testRenderEEAConsentPrivacyPolicyLink
    
    Bug: 369540483
    Change-Id: Iea14b8cb2643c2db832031fdc3235357c458282c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889832
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359917}

--- chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogTest.java
+++ chrome/browser/privacy_sandbox/android/javatests/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogTest.java
@@ -44,6 +44,7 @@ import org.mockito.MockitoAnnotations;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -213,6 +214,9 @@ public final class PrivacySandboxDialogTest {
     @SmallTest
     @Feature({"RenderTest"})
     @EnableFeatures(ChromeFeatureList.PRIVACY_SANDBOX_PRIVACY_POLICY)
+    // TODO(crbug.com/369540483): fix and re-enable on ARM devices.
+    @DisableIf.Build(supported_abis_includes = "armeabi-v7a")
+    @DisableIf.Build(supported_abis_includes = "arm64-v8a")
     public void testRenderEEAConsentPrivacyPolicyLink() throws IOException {
         mFakePrivacySandboxBridge.setRequiredPromptType(PromptType.M1_CONSENT);
         launchDialog();
