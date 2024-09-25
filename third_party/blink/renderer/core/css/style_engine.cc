807e5132356b2dc1017e743d4dc7eb058f283410
Rune Lillesveen <futhark@chromium.org>
2024-09-25 15:47:25 +0000

    Safe arrays in StyleEngine/ActiveStyleSheets
    Bug: 351564777
    Change-Id: Ia60e811a16af46fc8f5372151a4730342fc3eb7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890866
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359966}

--- third_party/blink/renderer/core/css/style_engine.cc
+++ third_party/blink/renderer/core/css/style_engine.cc
@@ -27,11 +27,6 @@
  * Boston, MA 02110-1301, USA.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/css/style_engine.h"
 
 #include "base/auto_reset.h"
