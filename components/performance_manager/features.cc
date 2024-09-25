4e36fb6b7bef14dfbd55a717a1ca2bf533b75256
Kameron Lutes <kalutes@google.com>
2024-09-25 01:01:58 +0000

    ChromeOS: Enable UnthrottledTabProcessReporting by default
    UnthrottledTabProcessReporting has been at 1% stable for several weeks
    and shown no regressions. It is now safe to enable by default.
    
    Bug: b:326859669
    Change-Id: I936be50b2634243c4923e6d9958fb83ed4e64777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5881699
    Commit-Queue: Kameron Lutes <kalutes@chromium.org>
    Reviewed-by: David Stevens <stevensd@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359696}

--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -113,7 +113,7 @@ const base::FeatureParam<int> kMemoryFreeBytesThreshold{
 #if BUILDFLAG(IS_CHROMEOS)
 BASE_FEATURE(kUnthrottledTabProcessReporting,
              "UnthrottledTabProcessReporting",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 #endif
