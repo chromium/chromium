0d930717b9ca423a693e2aef23f9b24d88ca1d4b
dljames <dljames@chromium.org>
2024-09-26 16:44:32 +0000

    [TabGroups] Fix crash when restoring grouped tab in app like browsers
    The crash happens when a grouped tab is restored into a browser that
    does not support tab groups. In this scenario, we will touch multiple
    different kinds of CHECKS / nullptr dereferences which will crash the
    browser.
    
    Changes:
    - CHECK TabGroupModel, TabStripModel, and TabGroup have values before
    use in BrowserLiveTabContext::SetVisualDataForGroup and
    BrowserLiveTabContext::GetVisualDataForGroup
    - Restore the grouped tab as an ungrouped tab if there is no
    TabGroupModel for the browser
    - Adds a regression test for this specific scenario
    
    Change-Id: I0230b7a7c86c31c77740488ce8959de77bd20865
    Bug: 368139715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5888488
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360585}

--- chrome/browser/ui/browser_live_tab_context.cc
+++ chrome/browser/ui/browser_live_tab_context.cc
@@ -154,10 +154,13 @@ std::optional<tab_groups::TabGroupId> BrowserLiveTabContext::GetTabGroupForTab(
 const tab_groups::TabGroupVisualData*
 BrowserLiveTabContext::GetVisualDataForGroup(
     const tab_groups::TabGroupId& group) const {
-  return browser_->tab_strip_model()
-      ->group_model()
-      ->GetTabGroup(group)
-      ->visual_data();
+  TabStripModel* tab_strip_model = browser_->tab_strip_model();
+  CHECK(tab_strip_model);
+  TabGroupModel* group_model = tab_strip_model->group_model();
+  CHECK(group_model);
+  TabGroup* tab_group = group_model->GetTabGroup(group);
+  CHECK(tab_group);
+  return tab_group->visual_data();
 }
 
 const std::optional<base::Uuid>
@@ -186,8 +189,13 @@ bool BrowserLiveTabContext::IsTabPinned(int index) const {
 void BrowserLiveTabContext::SetVisualDataForGroup(
     const tab_groups::TabGroupId& group,
     const tab_groups::TabGroupVisualData& visual_data) {
-  browser_->tab_strip_model()->group_model()->GetTabGroup(group)->SetVisualData(
-      std::move(visual_data));
+  TabStripModel* tab_strip_model = browser_->tab_strip_model();
+  CHECK(tab_strip_model);
+  TabGroupModel* group_model = tab_strip_model->group_model();
+  CHECK(group_model);
+  TabGroup* tab_group = group_model->GetTabGroup(group);
+  CHECK(tab_group);
+  tab_group->SetVisualData(std::move(visual_data));
 }
 
 const gfx::Rect BrowserLiveTabContext::GetRestoredBounds() const {
@@ -218,7 +226,11 @@ sessions::LiveTab* BrowserLiveTabContext::AddRestoredTab(
                 ->session_storage_namespace()
           : nullptr;
 
-  std::optional<tab_groups::TabGroupId> group_id = tab.group;
+  // If the browser does not support tabs groups, restore the grouped tab as a
+  // normal tab instead. See crbug.com/368139715.
+  std::optional<tab_groups::TabGroupId> group_id =
+      browser_->tab_strip_model()->SupportsTabGroups() ? tab.group
+                                                       : std::nullopt;
   std::optional<base::Uuid> saved_group_id = tab.saved_group_id;
   content::WebContents* web_contents = nullptr;
 
