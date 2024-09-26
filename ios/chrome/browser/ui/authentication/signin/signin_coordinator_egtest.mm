662dda0b8a811dd5eeae62b9d318520b85712c0b
Vincent Boisselle <vincb@google.com>
2024-09-26 13:07:33 +0000

    [ios] Disable SigninCoordinatorTestCase/testSignInDisconnectFromChromeManaged_ClearDataFeatureDisabled
    Because it consistently fails on of the CI bots.
    
    Change-Id: Ib7123a1cbd217b839308df5cd22f828d829f4141
    Bug: 369617405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889876
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Owners-Override: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360482}

--- ios/chrome/browser/ui/authentication/signin/signin_coordinator_egtest.mm
+++ ios/chrome/browser/ui/authentication/signin/signin_coordinator_egtest.mm
@@ -147,13 +147,8 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config = [super appConfigurationForTestCase];
 
-  if ([self isRunningTest:@selector
-            (testSignInDisconnectFromChromeManaged_ClearDataFeatureDisabled)]) {
-    config.features_disabled.push_back(
-        kClearDeviceDataOnSignOutForManagedUsers);
-  } else {
-    config.features_enabled.push_back(kClearDeviceDataOnSignOutForManagedUsers);
-  }
+  config.features_enabled.push_back(kClearDeviceDataOnSignOutForManagedUsers);
+
   if ([self isRunningTest:@selector(testOpenManageSyncSettingsFromNTP)] ||
       [self isRunningTest:@selector
             (FLAKY_testAccessiblityStringForSignedInUserWithoutName)]) {
@@ -293,7 +288,8 @@ void SetSigninEnterprisePolicyValue(BrowserSigninMode signinMode) {
 
 // Tests that signing out of a managed account from the Settings works
 // correctly.
-- (void)testSignInDisconnectFromChromeManaged_ClearDataFeatureDisabled {
+// TODO(crbug.com/369617405): Disabled due to flakiness.
+- (void)DISABLED_testSignInDisconnectFromChromeManaged_ClearDataFeatureDisabled {
   // Sign-in with a managed account.
   FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeManagedIdentity];
   [SigninEarlGreyUI signinWithFakeIdentity:fakeIdentity];
