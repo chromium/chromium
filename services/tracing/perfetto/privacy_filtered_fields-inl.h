1d49203ef9ea02944d534b8ff6c46cf539db74df
Tushar Agarwal <agarwaltushar@google.com>
2024-09-26 10:53:35 +0000

    Update `privacy_filtered_fields-inl.h`.
    For more context, please refer to the attached bugs.
    
    Bug: 369604559,b/355589236,b/356584965,b/356587463
    Change-Id: I5322cdc324a22ec9149e18cb272ae02aa318d6e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890653
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Tushar Agarwal <agarwaltushar@google.com>
    Cr-Commit-Position: refs/branch-heads/6723@{#499}
    Cr-Branched-From: 985f2961df230630f9cbd75bd6fe463009855a11-refs/heads/main@{#1356013}

--- services/tracing/perfetto/privacy_filtered_fields-inl.h
+++ services/tracing/perfetto/privacy_filtered_fields-inl.h
@@ -154,17 +154,6 @@ constexpr int kChromeHistogramSampleIndices[] = {1, 3, -1};
 constexpr MessageInfo kChromeHistogramSample = {kChromeHistogramSampleIndices,
                                                 nullptr};
 
-// Proto Message: ComponentInfo
-constexpr int kComponentInfoIndices[] = {1, 2, -1};
-constexpr MessageInfo kComponentInfo = {kComponentInfoIndices, nullptr};
-
-// Proto Message: ChromeLatencyInfo
-constexpr int kChromeLatencyInfoIndices[] = {1, 2, 3, 4, 5, 6, -1};
-constexpr MessageInfo const* kChromeLatencyInfoComplexMessages[] = {
-    nullptr, nullptr, nullptr, &kComponentInfo, nullptr, nullptr};
-constexpr MessageInfo kChromeLatencyInfo = {kChromeLatencyInfoIndices,
-                                            kChromeLatencyInfoComplexMessages};
-
 // Proto Message: ChromeFrameReporter
 constexpr int kChromeFrameReporterIndices[] = {1, 2, 3, 4,  5,  6,
                                                7, 8, 9, 10, 11, -1};
@@ -431,9 +420,10 @@ constexpr MessageInfo kLocalSurfaceId = {kLocalSurfaceIdIndices,
                                          kLocalSurfaceIdComplexMessages};
 
 // Proto Message: ChromeGraphicsPipeline
-constexpr int kChromeGraphicsPipelineIndices[] = {1, 2, 3, 4, 5, 6, -1};
+constexpr int kChromeGraphicsPipelineIndices[] = {1, 2, 3, 4, 5, 6, 8, -1};
 constexpr MessageInfo const* kChromeGraphicsPipelineComplexMessages[] = {
-    nullptr, &kFrameSinkId, nullptr, &kLocalSurfaceId, nullptr, nullptr};
+    nullptr, &kFrameSinkId, nullptr, &kLocalSurfaceId,
+    nullptr, nullptr,       nullptr};
 constexpr MessageInfo kChromeGraphicsPipeline = {
     kChromeGraphicsPipelineIndices, kChromeGraphicsPipelineComplexMessages};
 
@@ -494,16 +484,33 @@ constexpr int kAnimationFrameScriptTimingInfoIndices[] = {1, 2, 3, 9, 10, -1};
 constexpr MessageInfo kAnimationFrameScriptTimingInfo = {
     kAnimationFrameScriptTimingInfoIndices, nullptr};
 
+// Proto Message: ComponentInfo
+constexpr int kComponentInfoIndices[] = {1, 2, -1};
+constexpr MessageInfo kComponentInfo = {kComponentInfoIndices, nullptr};
+
+// Proto Message: ChromeLatencyInfo2
+constexpr int kChromeLatencyInfo2Indices[] = {1, 2, 3, 4,  5, 6,
+                                              7, 8, 9, 10, -1};
+constexpr MessageInfo const* kChromeLatencyInfo2ComplexMessages[] = {
+    nullptr, nullptr, nullptr, &kComponentInfo, nullptr,
+    nullptr, nullptr, nullptr, nullptr,         nullptr};
+constexpr MessageInfo kChromeLatencyInfo2 = {
+    kChromeLatencyInfo2Indices, kChromeLatencyInfo2ComplexMessages};
+
+// Proto Message: EventTiming
+constexpr int kEventTimingIndices[] = {1, 3, 4, 5, 6, 7, 8, 9, -1};
+constexpr MessageInfo kEventTiming = {kEventTimingIndices, nullptr};
+
 // Proto Message: TrackEvent
 constexpr int kTrackEventIndices[] = {
     1,    2,    3,    5,    6,    9,    10,   11,   12,   16,   17,   22,
-    23,   24,   25,   26,   27,   28,   29,   30,   31,   32,   33,   34,
-    35,   36,   38,   39,   40,   41,   42,   43,   47,   48,   1001, 1002,
-    1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014,
-    1015, 1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025, 1028,
-    1031, 1032, 1033, 1034, 1036, 1038, 1039, 1040, 1041, 1042, 1046, 1047,
-    1048, 1049, 1050, 1051, 1052, 1053, 1054, 1055, 1056, 1057, 1058, 1059,
-    1060, 1061, 1064, 1065, -1};
+    23,   24,   25,   26,   27,   28,   30,   31,   32,   33,   34,   35,
+    36,   38,   39,   40,   41,   42,   43,   47,   48,   1001, 1002, 1003,
+    1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1015,
+    1016, 1017, 1018, 1019, 1020, 1021, 1022, 1023, 1024, 1025, 1028, 1031,
+    1032, 1033, 1034, 1036, 1038, 1039, 1040, 1041, 1042, 1046, 1047, 1048,
+    1049, 1050, 1051, 1052, 1053, 1054, 1055, 1056, 1057, 1058, 1059, 1060,
+    1061, 1064, 1065, 1068, 1069, -1};
 constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     nullptr,
     nullptr,
@@ -523,7 +530,6 @@ constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     &kChromeKeyedService,
     &kChromeLegacyIpc,
     &kChromeHistogramSample,
-    &kChromeLatencyInfo,
     nullptr,
     nullptr,
     &kChromeFrameReporter,
@@ -592,7 +598,9 @@ constexpr MessageInfo const* kTrackEventComplexMessages[] = {
     &kTouchDispositionGestureFilter,
     &kViewClassName,
     &kAnimationFrameTimingInfo,
-    &kAnimationFrameScriptTimingInfo};
+    &kAnimationFrameScriptTimingInfo,
+    &kChromeLatencyInfo2,
+    &kEventTiming};
 constexpr MessageInfo kTrackEvent = {kTrackEventIndices,
                                      kTrackEventComplexMessages};
 
