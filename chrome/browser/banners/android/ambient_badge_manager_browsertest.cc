4a26b7f1a219b916e3ba8243cc3a27c2dd4ed751
Shu Yang <shuyng@google.com>
2024-09-27 00:14:49 +0000

    🌱 Disable AmbientBadgeManagerBrowserTest#ShowAmbientBadge
    Bug: 369913977
    Change-Id: Ice13c9f7481f57168ccc2021def2785538702887
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893694
    Owners-Override: Shu Yang <shuyng@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360872}

--- chrome/browser/banners/android/ambient_badge_manager_browsertest.cc
+++ chrome/browser/banners/android/ambient_badge_manager_browsertest.cc
@@ -222,7 +222,9 @@ class AmbientBadgeManagerBrowserTest : public AndroidBrowserTest {
       mock_segmentation_service_;
 };
 
-IN_PROC_BROWSER_TEST_F(AmbientBadgeManagerBrowserTest, ShowAmbientBadge) {
+// TODO(crbug.com/369913977): Flaky
+IN_PROC_BROWSER_TEST_F(AmbientBadgeManagerBrowserTest,
+                       DISABLED_ShowAmbientBadge) {
   SetSegmentationResult(MLInstallabilityPromoter::kShowInstallPromptLabel);
 
   RunTest(embedded_test_server()->GetURL("/banners/manifest_test_page.html"),
