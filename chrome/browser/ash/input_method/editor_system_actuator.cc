9c412a7e4e453558620e1530d85025c28bb70754
Chuong Ho <hdchuong@google.com>
2024-09-27 00:49:31 +0000

    [cherrypick to M130] editor: whitelist systemPreferences settings
    Needs to allowlist systemPreferences so that the clicking the cog icon
    can direct the system preferences link
    
    (cherry picked from commit d330cea4332644cdda0c2f032d8464f0f44ef536)
    
    Bug: b:362651418
    Change-Id: Ib742130502378c10f15d1eb805d3216d4c89d1d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889167
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1359722}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892393
    Cr-Commit-Position: refs/branch-heads/6723@{#548}
    Cr-Branched-From: 985f2961df230630f9cbd75bd6fe463009855a11-refs/heads/main@{#1356013}

--- chrome/browser/ash/input_method/editor_system_actuator.cc
+++ chrome/browser/ash/input_method/editor_system_actuator.cc
@@ -33,7 +33,8 @@ constexpr std::string_view
 
 bool IsUrlAllowed(const GURL& url) {
   return url.SchemeIs(url::kHttpsScheme) ||
-         url.spec().starts_with("chrome://os-settings/osLanguages/input");
+         url.spec().starts_with("chrome://os-settings/osLanguages/input") ||
+         url.spec().starts_with("chrome://os-settings/systemPreferences");
 }
 
 EditorTextInsertion::InsertionStrategy GetInsertionStrategy(const GURL& url) {
