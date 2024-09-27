f1c7b10eb20317fbc142ff74716fd4c51f8e75d1
April Zhou <aprilzhou@google.com>
2024-09-27 01:55:48 +0000

    boca: Add photo url into classroom list roster payload.
    This API is not used by other products in chromeOS yet so won't impact
    existing quota.
    
    Bug: b:356247314
    Test: unit tested.
    Change-Id: Ia029f7e462c856f4b01fe6ffe8185f3d6779ddb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882131
    Reviewed-by: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360905}

--- google_apis/classroom/classroom_api_students_response_types_unittest.cc
+++ google_apis/classroom/classroom_api_students_response_types_unittest.cc
@@ -8,6 +8,7 @@
 
 #include "base/json/json_reader.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
 
 namespace google_apis::classroom {
 
@@ -33,7 +34,8 @@ TEST(ClassroomApiStudentsResponseTypesTest, ConvertsStudents) {
                   "name":{
                      "fullName":"Student1 full"
                   },
-                  "emailAddress":"student1@foo.com"
+                  "emailAddress":"student1@foo.com",
+                  "photoUrl":"//s1"
                }
             },
             {
@@ -42,7 +44,8 @@ TEST(ClassroomApiStudentsResponseTypesTest, ConvertsStudents) {
                   "name":{
                      "fullName":"Student2 full"
                   },
-                  "emailAddress":"student2@foo.com"
+                  "emailAddress":"student2@foo.com",
+                  "photoUrl":"//s2"
                }
             }
          ]
@@ -59,12 +62,14 @@ TEST(ClassroomApiStudentsResponseTypesTest, ConvertsStudents) {
             "Student1 full");
   EXPECT_EQ(students->items().at(0)->profile().email_address(),
             "student1@foo.com");
+  EXPECT_EQ(students->items().at(0)->profile().photo_url(), GURL("https://s1"));
 
   EXPECT_EQ(students->items().at(1)->profile().id(), "student-2");
   EXPECT_EQ(students->items().at(1)->profile().name().full_name(),
             "Student2 full");
   EXPECT_EQ(students->items().at(1)->profile().email_address(),
             "student2@foo.com");
+  EXPECT_EQ(students->items().at(1)->profile().photo_url(), GURL("https://s2"));
 }
 
 TEST(ClassroomApiStudentsResponseTypesTest, ConvertsNextPageToken) {
