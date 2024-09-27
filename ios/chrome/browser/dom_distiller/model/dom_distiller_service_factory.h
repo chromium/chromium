8fc7a30c285af2bc7b84a67fb782709a8c0b882b
Federica Germinario <fedegermi@google.com>
2024-09-27 12:24:46 +0000

    [ios] Update files in i/c/b/dom_distiller/*
    This CL replaces:
    * ChromeBrowserState => ProfileIOS
    * GetForBrowserState => GetForProfile at call sites.
    * GetBrowserState => GetProfile
    * browser_state, browserState => profile
    
    web::BrowserState code is not updated.
    
    Fixed: 365527268
    Change-Id: Id0e0ad5d8cbb54f87b489e76608e0f8afec23328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890616
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361075}

--- ios/chrome/browser/dom_distiller/model/dom_distiller_service_factory.h
+++ ios/chrome/browser/dom_distiller/model/dom_distiller_service_factory.h
@@ -19,9 +19,6 @@ namespace dom_distiller {
 
 class DomDistillerServiceFactory : public BrowserStateKeyedServiceFactory {
  public:
-  // TODO(crbug.com/358301380): remove this method.
-  static DomDistillerService* GetForBrowserState(ProfileIOS* profile);
-
   static DomDistillerService* GetForProfile(ProfileIOS* profile);
   static DomDistillerServiceFactory* GetInstance();
 
