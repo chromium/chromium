a7f4fd4de51379a1072d931a106da92433356216
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-09-28 03:24:21 +0000

    Import wpt@dbf4b1c0813c0ff6bb522dd93becc44b2cd2a379
    https://github.com/web-platform-tests/wpt/compare/fd25aeb94...dbf4b1c08
    
    Using wpt-import in Chromium 5483fa2824fd5cb98dcf1333f91c9436be82f218.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I86575d3c78c69452f2b081e48b7aae2d81813c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891345
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1361470}

--- third_party/blink/web_tests/external/wpt/cookie-store/cookieStore_event_delete.https.window.js
+++ third_party/blink/web_tests/external/wpt/cookie-store/cookieStore_event_delete.https.window.js
@@ -37,3 +37,39 @@ promise_test(async testCase => {
     assert_equals(event.changed.length, 0);
   }
 }, 'cookieStore fires change event for cookie deleted by cookieStore.delete()');
+
+promise_test(async testCase => {
+  testCase.add_cleanup(async () => {
+    await cookieStore.delete('cookie-name');
+  });
+
+  const eventPromise = new Promise((resolve) => {
+    const events = [];
+    cookieStore.onchange = event => {
+      events.push(event);
+      if (event.type === 'change' &&
+          event.deleted.length === 1 &&
+          event.deleted[0].name === 'cookie-name') {
+         resolve(events);
+       }
+    }
+  });
+
+  await cookieStore.delete('cookie-unknown');
+  await cookieStore.set('cookie-name', 'cookie-value');
+  await cookieStore.delete('cookie-another-unknown');
+  await cookieStore.delete('cookie-name');
+
+  const events = await eventPromise;
+
+  assert_equals(events.length, 2);
+  assert_true(events[0] instanceof CookieChangeEvent);
+  assert_equals(events[0].type, 'change');
+  assert_equals(events[0].changed.length, 1);
+  assert_equals(events[0].changed[0].name, 'cookie-name');
+
+  assert_true(events[1] instanceof CookieChangeEvent);
+  assert_equals(events[1].type, 'change');
+  assert_equals(events[1].deleted.length, 1);
+  assert_equals(events[1].deleted[0].name, 'cookie-name');
+}, 'cookieStore does not fire change events for non-existing expired cookies');
