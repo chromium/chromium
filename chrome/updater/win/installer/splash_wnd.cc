667d31dee14b01bb6b5b65bcfb22c1de9fd0cd7b
S. Ganesh <ganesh@chromium.org>
2024-09-29 19:20:58 +0000

    updater: logo splash screen
    Bug: 40234383
    Change-Id: Ib4dbe95a58a30023c125a8a67957a0c4935f18d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892530
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361614}

--- chrome/updater/win/installer/splash_wnd.cc
+++ chrome/updater/win/installer/splash_wnd.cc
@@ -7,8 +7,6 @@
 #include "base/win/scoped_gdi_object.h"
 #include "chrome/updater/win/installer/installer_resource.h"
 #include "chrome/updater/win/ui/l10n_util.h"
-#include "chrome/updater/win/ui/ui.h"
-#include "chrome/updater/win/ui/ui_constants.h"
 #include "chrome/updater/win/ui/ui_util.h"
 
 namespace updater::ui {
@@ -21,17 +19,10 @@ LRESULT SplashWnd::OnInitDialog(UINT /*msg*/,
                                 LPARAM /*lparam*/,
                                 BOOL& handled) {
   SetWindowText(GetInstallerDisplayName({}).c_str());
-  SetDlgItemText(IDC_SPLASH_TEXT, GetLocalizedSplashScreenString().c_str());
 
   CenterWindow();
   SetWindowIcon(m_hWnd, IDI_MINI_INSTALLER,
                 base::win::ScopedGDIObject<HICON>::Receiver(hicon_).get());
-
-  CreateOwnerDrawTitleBar(m_hWnd, GetDlgItem(IDC_SPLASH_TITLE_BAR_SPACER),
-                          kBkColor);
-  SetCustomDlgColors(kTextColor, kBkColor);
-
-  EnableFlatButtons(m_hWnd);
 
   handled = true;
   return 0;
--- chrome/updater/win/installer/splash_wnd.h
+++ chrome/updater/win/installer/splash_wnd.h
@@ -7,15 +7,13 @@
 
 #include <windows.h>
 
+#include "base/win/atl.h"
 #include "base/win/scoped_gdi_object.h"
 #include "chrome/updater/win/installer/installer_resource.h"
-#include "chrome/updater/win/ui/owner_draw_controls.h"
 
 namespace updater::ui {
 
-class SplashWnd : public CDialogImpl<SplashWnd>,
-                  public OwnerDrawTitleBar,
-                  public CustomDlgColors {
+class SplashWnd : public CDialogImpl<SplashWnd> {
  public:
   static constexpr int IDD = IDD_SPLASH;
 
@@ -26,8 +24,6 @@ class SplashWnd : public CDialogImpl<SplashWnd>,
     MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
     MESSAGE_HANDLER(WM_CLOSE, OnClose)
     MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
-    CHAIN_MSG_MAP(OwnerDrawTitleBar)
-    CHAIN_MSG_MAP(CustomDlgColors)
   END_MSG_MAP()
 
   LRESULT OnInitDialog(UINT /*msg*/,
