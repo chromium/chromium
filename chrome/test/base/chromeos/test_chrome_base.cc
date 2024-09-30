90fa6badb4e06ea5f31d21eb46a35aa6a250e167
Georg Neis <neis@chromium.org>
2024-09-30 23:46:54 +0000

    Remove test_lacros_chrome and StandaloneTestController
    Change-Id: I39bbe195d39aa41b3ea341044e9f8a9f62ef97f4
    Bug: b:369519665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894199
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362089}

--- chrome/test/base/chromeos/test_chrome_base.cc
+++ chrome/test/base/chromeos/test_chrome_base.cc
@@ -10,15 +10,10 @@
 #include "base/functional/bind.h"
 #include "base/test/allow_check_is_test_for_testing.h"
 #include "chrome/browser/chrome_browser_main.h"
+#include "chrome/test/base/chromeos/test_ash_chrome_browser_main_extra_parts.h"
 #include "content/public/browser/browser_main_parts.h"
 #include "headless/public/headless_shell.h"
 #include "ui/gfx/switches.h"
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/test/base/chromeos/test_ash_chrome_browser_main_extra_parts.h"
-#else
-#include "chrome/test/base/chromeos/test_lacros_chrome_browser_main_extra_parts.h"
-#endif
 
 namespace test {
 
@@ -50,13 +45,8 @@ void TestChromeBase::CreatedBrowserMainPartsImpl(
     content::BrowserMainParts* browser_main_parts) {
   browser_main_parts_ =
       static_cast<ChromeBrowserMainParts*>(browser_main_parts);
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   browser_main_parts_->AddParts(
       std::make_unique<test::TestAshChromeBrowserMainExtraParts>());
-#else
-  browser_main_parts_->AddParts(
-      std::make_unique<test::TestLacrosChromeBrowserMainExtraParts>());
-#endif
 }
 
 }  // namespace test
