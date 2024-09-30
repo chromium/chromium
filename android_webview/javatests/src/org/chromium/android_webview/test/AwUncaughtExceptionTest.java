c857dfe35ee7a6735adb856fc78ea220989ecce0
Duong Dac <ddac@chromium.org>
2024-09-30 11:37:58 +0000

    Disable AwUncaughtExceptionTest.testShouldOverrideUrlLoading
    This test is failing on android-12-x64-rel.
    
    Bug: 370449449
    Change-Id: Ia833229fa8ff2278163e79ce7c7d94bf9cff1bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898652
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Owners-Override: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361737}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwUncaughtExceptionTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwUncaughtExceptionTest.java
@@ -29,6 +29,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.base.test.util.DoNotBatch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 
 import java.lang.reflect.Field;
@@ -280,6 +281,7 @@ public class AwUncaughtExceptionTest extends AwParameterizedTest {
     @Test
     @MediumTest
     @Feature({"AndroidWebView"})
+    @DisabledTest(message = "https://crbug.com/370449449")
     public void testShouldOverrideUrlLoading() throws InterruptedException {
         final CountDownLatch latch = new CountDownLatch(1);
         final String msg = "dies.";
