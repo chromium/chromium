db5c6353ec79a287c0495a8c0c66dd8a18b80cf0
Tom Sepez <tsepez@chromium.org>
2024-10-01 20:50:11 +0000

    Replace const-value arguments where const-ref intended.
    This is of concern when passing vectors and strings, as unintended
    (and often expensive) copies occur under the covers.
    
    -- Make non-const and move in callee where appropriate.
    -- Convert to string_view where appropriate.
    -- Avoid stray const in a few places
    
    This CL was uploaded by git cl split.
    
    R=estade@chromium.org, nhiroki@chromium.org
    
    Bug: 369952985
    Change-Id: I6e887449bbabc34d91ea20a0f5632100ad7f1386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5901447
    Reviewed-by: Evan Stade <estade@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362657}

--- storage/browser/file_system/file_system_request_info.cc
+++ storage/browser/file_system/file_system_request_info.cc
@@ -3,16 +3,17 @@
 // found in the LICENSE file.
 
 #include "storage/browser/file_system/file_system_request_info.h"
+
 #include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "url/gurl.h"
 
 namespace storage {
 
 FileSystemRequestInfo::FileSystemRequestInfo(
-    const GURL url,
-    const std::string storage_domain,
+    const GURL& url,
+    const std::string& storage_domain,
     int content_id,
-    const blink::StorageKey storage_key)
+    const blink::StorageKey& storage_key)
     : url(url),
       storage_domain(storage_domain),
       content_id(content_id),
