8331962ababcea7a658d06e43ee5836fe2d6ab18
Wenyu Fu <wenyufu@chromium.org>
2024-10-01 21:27:46 +0000

    [E2E] Observe Configuration change and force layout
    Force an drawToEdge update when configuration changes to enforce correct
    padding for the root view. This is a speculative fix for issues that
    Chrome sometimes draws into the system insets when switching between
    portrait and landscape mode.
    
    Bug: 369887909
    Change-Id: Ia24e0bfc6ee183c59e7f9157ebae16fb5bb14211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898280
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362685}

--- chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeControllerImpl.java
+++ chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeControllerImpl.java
@@ -5,6 +5,8 @@
 package org.chromium.chrome.browser.ui.edge_to_edge;
 
 import android.app.Activity;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
 import android.graphics.Rect;
 import android.os.Build.VERSION_CODES;
 import android.view.View;
@@ -65,6 +67,7 @@ public class EdgeToEdgeControllerImpl
     private final Callback<LayoutManager> mOnLayoutManagerCallback =
             new ValueChangedCallback<>(this::updateLayoutStateProvider);
     private final FullscreenManager mFullscreenManager;
+    private final ComponentCallbacks mComponentCallback;
 
     // Cached rects used for adding under fullscreen.
     private final Rect mCachedWindowVisibleRect = new Rect();
@@ -168,6 +171,20 @@ public class EdgeToEdgeControllerImpl
 
         mWindowInsetsConsumer = this::handleWindowInsets;
         mInsetObserver.addInsetsConsumer(mWindowInsetsConsumer);
+
+        mComponentCallback =
+                new ComponentCallbacks() {
+                    @Override
+                    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+                        // When configuration changed, force an padding update.
+                        // See https://crbug.com/369887909
+                        drawToEdge(mIsPageOptedIntoEdgeToEdge, /* changedWindowState= */ true);
+                    }
+
+                    @Override
+                    public void onLowMemory() {}
+                };
+        mActivity.registerComponentCallbacks(mComponentCallback);
 
         assert mInsetObserver.getLastRawWindowInsets() != null
                 : "The inset observer should have non-null insets by the time the"
@@ -471,6 +488,11 @@ public class EdgeToEdgeControllerImpl
             topPadding = Math.max(0, mCachedWindowVisibleRect.top - mCachedContentVisibleRect.top);
             bottomPadding =
                     Math.max(0, mCachedContentVisibleRect.bottom - mCachedWindowVisibleRect.bottom);
+        } else if (topPadding == 0) {
+            // In odd cases, we see Chrome has set a 0 as top padding observed in
+            // crbug.com/369887909. In those cases, fix the padding based on the visible area.
+            Log.w(TAG, "topPadding = 0 when not in fullscreen mode.");
+            topPadding = Math.abs(mCachedWindowVisibleRect.top - mCachedContentVisibleRect.top);
         }
 
         // Use Insets to store the paddings as it is immutable.
@@ -499,6 +521,7 @@ public class EdgeToEdgeControllerImpl
     @CallSuper
     @Override
     public void destroy() {
+        mActivity.unregisterComponentCallbacks(mComponentCallback);
         if (mWebContentsObserver != null) {
             mWebContentsObserver.destroy();
             mWebContentsObserver = null;
