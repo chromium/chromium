47e9480f27b9358cb50956f764bac3e7e528820b
Fredrik Söderquist <fs@opera.com>
2024-10-01 19:40:42 +0000

    spanification: //third_party/blink/common/messaging/string_message_codec.cc
    Bug: 351564777
    Change-Id: Ic50932d2848951f96250a0492cd1230beb724d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898953
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362611}

--- third_party/blink/common/messaging/string_message_codec.cc
+++ third_party/blink/common/messaging/string_message_codec.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/public/common/messaging/string_message_codec.h"
 
 #include <memory>
@@ -48,15 +43,18 @@ class VectorArrayBuffer : public WebMessageArrayBufferPayload {
 
   std::optional<base::span<const uint8_t>> GetAsSpanIfPossible()
       const override {
-    return base::make_span(data_).subspan(position_, length_);
+    return AsSpan();
   }
 
   void CopyInto(base::span<uint8_t> dest) const override {
-    CHECK_GE(dest.size(), length_);
-    memcpy(dest.data(), data_.data() + position_, length_);
+    dest.copy_from(AsSpan());
   }
 
  private:
+  base::span<const uint8_t> AsSpan() const {
+    return base::span(data_).subspan(position_, length_);
+  }
+
   std::vector<uint8_t> data_;
   size_t position_;
   size_t length_;
@@ -87,8 +85,7 @@ class BigBufferArrayBuffer : public WebMessageArrayBufferPayload {
   }
 
   void CopyInto(base::span<uint8_t> dest) const override {
-    CHECK(dest.size() >= data_.size());
-    memcpy(dest.data(), data_.data(), data_.size());
+    dest.copy_from(base::make_span(data_));
   }
 
  private:
@@ -134,10 +131,8 @@ void WriteUint32(uint32_t value, std::vector<uint8_t>* buffer) {
   }
 }
 
-void WriteBytes(const char* bytes,
-                size_t num_bytes,
-                std::vector<uint8_t>* buffer) {
-  buffer->insert(buffer->end(), bytes, bytes + num_bytes);
+void WriteBytes(base::span<const uint8_t> bytes, std::vector<uint8_t>* buffer) {
+  buffer->insert(buffer->end(), bytes.begin(), bytes.end());
 }
 
 bool ReadUint8(base::BufferIterator<const uint8_t>& iter, uint8_t* value) {
@@ -199,15 +194,17 @@ TransferableMessage EncodeWebMessagePayload(const WebMessagePayload& payload) {
               std::string data_latin1(str.cbegin(), str.cend());
               WriteUint8(kOneByteStringTag, &buffer);
               WriteUint32(data_latin1.size(), &buffer);
-              WriteBytes(data_latin1.c_str(), data_latin1.size(), &buffer);
+              WriteBytes(base::as_byte_span(data_latin1), &buffer);
             } else {
-              size_t num_bytes = str.size() * sizeof(char16_t);
-              if ((buffer.size() + 1 + BytesNeededForUint32(num_bytes)) & 1)
+              auto str_as_bytes = base::as_byte_span(str);
+              if ((buffer.size() + 1 +
+                   BytesNeededForUint32(str_as_bytes.size())) &
+                  1) {
                 WriteUint8(kPaddingTag, &buffer);
+              }
               WriteUint8(kTwoByteStringTag, &buffer);
-              WriteUint32(num_bytes, &buffer);
-              WriteBytes(reinterpret_cast<const char*>(str.data()), num_bytes,
-                         &buffer);
+              WriteUint32(str_as_bytes.size(), &buffer);
+              WriteBytes(str_as_bytes, &buffer);
             }
           },
           [&](const std::unique_ptr<WebMessageArrayBufferPayload>&
