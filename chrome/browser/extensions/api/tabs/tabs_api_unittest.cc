0203d05bde3d3d95b59ef2596e216f6314f3bbe5
dljames <dljames@chromium.org>
2024-10-01 00:49:41 +0000

    [TabGroupSync] Re-enable tests on ChromeOS when migration flag enabled
    Re-enables previously disabled tests in TabsApiUnitTest:
    - All affected functions in TabsApiUnitTest
    - The entire SavedTabGroupBarUnitTest suite
    - The entire STGEverythingMenuUnitTest suite
    
    When the migration flag is enabled, the TabGroupSyncService attempts to
    initialize which involves listening to any open saved groups in the
    browser.
    
    On ChromeOS we hit a CHECK(browser) because we did not properly
    associate a saved tab group created locally to a browser. So when we try
    to find the browser a group belongs to we can't find it. CHECK.
    
    This only happens when the migration flag is turned on and the V2 flag
    is disabled. This combination puts us in a state where we are using the
    new service which is expecting the V2 flag to be turned on which
    introduces the auto saving functionality for us.
    
    To mitigate the issue, we need to manually observe the newly created
    groups so when the ChromeOS browser starts we can find the browser a
    group belongs to. This change also makes it possible to observe the changes in the group correctly without needing to have separate code paths in the test.
    
    Everything should be consolidated now.
    
    Change-Id: I530b9e269968672498f745bb33453333cda5ec2e
    Bug: 368188068, 368189478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874722
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362124}

--- chrome/browser/extensions/api/tabs/tabs_api_unittest.cc
+++ chrome/browser/extensions/api/tabs/tabs_api_unittest.cc
@@ -36,7 +36,9 @@
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/test_browser_window.h"
 #include "components/saved_tab_groups/features.h"
+#include "components/saved_tab_groups/saved_tab_group.h"
 #include "components/saved_tab_groups/tab_group_sync_service.h"
+#include "components/saved_tab_groups/types.h"
 #include "components/sessions/content/session_tab_helper.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "content/public/browser/navigation_entry.h"
@@ -128,6 +130,8 @@ class TabsApiUnitTest : public ExtensionServiceTestBase {
         browser()->profile());
   }
 
+  void MaybeSaveLocalTabGroup(const tab_groups::LocalTabGroupID& local_id);
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   aura::Window* root_window() { return test_helper_.GetContext(); }
 #endif
@@ -183,6 +187,28 @@ void TabsApiUnitTest::TearDown() {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   test_helper_.TearDown();
 #endif
+}
+
+void TabsApiUnitTest::MaybeSaveLocalTabGroup(
+    const tab_groups::LocalTabGroupID& local_id) {
+  if (tab_groups::IsTabGroupsSaveV2Enabled()) {
+    // In V2, all tab groups are automatically saved by default. For this
+    // reason, there is no need to manually save the group again.
+    return;
+  }
+
+  tab_groups::SavedTabGroup saved_group =
+      tab_groups::SavedTabGroupUtils::CreateSavedTabGroupFromLocalId(local_id);
+  const base::Uuid sync_id = saved_group.saved_guid();
+  sync_service()->AddGroup(std::move(saved_group));
+  if (tab_groups::IsTabGroupSyncServiceDesktopMigrationEnabled()) {
+    // Manually observe the group for V1 code paths. These code paths were
+    // introduced before autosaving which is why this is required.
+    sync_service()->ConnectLocalTabGroup(sync_id, local_id,
+                                         tab_groups::OpeningSource::kUnknown);
+  }
+
+  ASSERT_TRUE(sync_service()->GetGroup(local_id));
 }
 
 bool TabsApiUnitTest::CommitPendingLoadForController(
@@ -576,12 +602,7 @@ TEST_F(TabsApiUnitTest, TabsUpdateSavedTabGroupTab) {
       ->GetTabGroup(group)
       ->SetVisualData(visual_data);
 
-  if (!tab_groups::IsTabGroupsSaveV2Enabled()) {
-    // The group is not saved by default if we enter here. Manually save it.
-    saved_service->AddGroup(
-        tab_groups::SavedTabGroupUtils::CreateSavedTabGroupFromLocalId(group));
-    ASSERT_TRUE(saved_service->GetGroup(group));
-  }
+  MaybeSaveLocalTabGroup(group);
 
   EXPECT_TRUE(
       ExtensionTabUtil::TabIsInSavedTabGroup(raw_contents, GetTabStripModel()));
@@ -712,15 +733,7 @@ TEST_F(TabsApiUnitTest, TabsUpdateSavedTabGroupTab) {
         function.get(), args, profile(), api_test_utils::FunctionMode::kNone));
   }
 
-  if (!tab_groups::IsTabGroupsSaveV2Enabled() &&
-      tab_groups::IsTabGroupSyncServiceDesktopMigrationEnabled()) {
-    // Listener layer for the migration flag is reliant on v2 being enabled. For
-    // this reason we do not observe the group when the tab is removed which is
-    // why the group still exists when the last tab is removed.
-    ASSERT_TRUE(saved_service->GetGroup(group));
-  } else {
-    ASSERT_FALSE(saved_service->GetGroup(group));
-  }
+  ASSERT_FALSE(saved_service->GetGroup(group));
 }
 
 // Tests that calling chrome.tabs.update with a JavaScript URL results
@@ -910,10 +923,7 @@ TEST_F(TabsApiUnitTest, TabsMoveSavedTabGroupTabAllowed) {
       ->GetTabGroup(group)
       ->SetVisualData(visual_data);
 
-  if (!tab_groups::IsTabGroupsSaveV2Enabled()) {
-    saved_service->AddGroup(
-        tab_groups::SavedTabGroupUtils::CreateSavedTabGroupFromLocalId(group));
-  }
+  MaybeSaveLocalTabGroup(group);
 
   // Use the TabsUpdateFunction to navigate to chromium.org
   int tab_extension_id = sessions::SessionTabHelper::IdForTab(
@@ -1176,16 +1186,7 @@ TEST_F(TabsApiUnitTest, TabsGroupForSavedTabGroupTab) {
 
   // group the first tab. make sure its saved.
   tab_groups::TabGroupId old_group = GetTabStripModel()->AddToNewGroup({0});
-  if (!tab_groups::IsTabGroupsSaveV2Enabled()) {
-    tab_groups::TabGroupSyncService* saved_service =
-        tab_groups::SavedTabGroupUtils::GetServiceForProfile(
-            browser()->profile());
-    ASSERT_TRUE(saved_service);
-
-    saved_service->AddGroup(
-        tab_groups::SavedTabGroupUtils::CreateSavedTabGroupFromLocalId(
-            old_group));
-  }
+  MaybeSaveLocalTabGroup(old_group);
 
   // with extensions group the 2 tabs into a new group.
   auto function = base::MakeRefCounted<TabsGroupFunction>();
@@ -1283,10 +1284,7 @@ TEST_F(TabsApiUnitTest, TabsUngroupSingleGroupForSavedTabGroup) {
       ->GetTabGroup(group)
       ->SetVisualData(visual_data);
 
-  if (!tab_groups::IsTabGroupsSaveV2Enabled()) {
-    saved_service->AddGroup(
-        tab_groups::SavedTabGroupUtils::CreateSavedTabGroupFromLocalId(group));
-  }
+  MaybeSaveLocalTabGroup(group);
 
   auto function = base::MakeRefCounted<TabsUngroupFunction>();
   function->set_extension(extension);
@@ -1454,10 +1452,7 @@ TEST_F(TabsApiUnitTest, TabsGoForwardAndBackSavedTabGroupTab) {
       ->GetTabGroup(group)
       ->SetVisualData(visual_data);
 
-  if (!tab_groups::IsTabGroupsSaveV2Enabled()) {
-    saved_service->AddGroup(
-        tab_groups::SavedTabGroupUtils::CreateSavedTabGroupFromLocalId(group));
-  }
+  MaybeSaveLocalTabGroup(group);
 
   {
     auto goback_function = base::MakeRefCounted<TabsGoBackFunction>();
@@ -1755,10 +1750,7 @@ TEST_F(TabsApiUnitTest, TabsDiscardSavedTabGroupTabNotAllowed) {
       ->GetTabGroup(group)
       ->SetVisualData(visual_data);
 
-  if (!tab_groups::IsTabGroupsSaveV2Enabled()) {
-    saved_service->AddGroup(
-        tab_groups::SavedTabGroupUtils::CreateSavedTabGroupFromLocalId(group));
-  }
+  MaybeSaveLocalTabGroup(group);
 
   // The tab discard function should fail.
   auto function = base::MakeRefCounted<TabsDiscardFunction>();
@@ -1811,10 +1803,7 @@ TEST_F(TabsApiUnitTest,
       ->GetTabGroup(group)
       ->SetVisualData(visual_data);
 
-  if (!tab_groups::IsTabGroupsSaveV2Enabled()) {
-    saved_service->AddGroup(
-        tab_groups::SavedTabGroupUtils::CreateSavedTabGroupFromLocalId(group));
-  }
+  MaybeSaveLocalTabGroup(group);
 
   // The tab discard function should not fail.
   auto function = base::MakeRefCounted<TabsDiscardFunction>();
