54dfb8b51b039e1780d1b31b686265e92af8d051
James Cook <jamescook@chromium.org>
2024-10-01 21:50:33 +0000

    birch: Use prod backdrop server for weather requests
    Remove the parameter that allows using the alpha backdrop server. The
    internal code will then default to prod. This CL can land once we
    confirm the prod backdrop server supports an external weather client ID
    ("external-weather-client-id").
    
    The internal cleanup CL that can land after this one is:
    https://chrome-internal-review.googlesource.com/c/chrome/assistant/+/7551922
    
    Bug: b:353759771
    Change-Id: Ic329da3de1ee7b1d395715633b1d3b65f172609f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757864
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362693}

--- ash/ambient/backdrop/ambient_backend_controller_impl.cc
+++ ash/ambient/backdrop/ambient_backend_controller_impl.cc
@@ -446,7 +446,6 @@ void AmbientBackendControllerImpl::FetchPersonalAlbums(
 
 void AmbientBackendControllerImpl::FetchWeather(
     std::optional<std::string> weather_client_id,
-    bool prefer_alpha_endpoint,
     FetchWeatherCallback callback) {
   auto response_handler =
       [](FetchWeatherCallback callback,
@@ -478,8 +477,7 @@ void AmbientBackendControllerImpl::FetchWeather(
   DCHECK(user->HasGaiaAccount());
   BackdropClientConfig::Request request =
       backdrop_client_config_.CreateFetchWeatherInfoRequest(
-          user->GetAccountId().GetGaiaId(), GetClientId(), weather_client_id,
-          prefer_alpha_endpoint);
+          user->GetAccountId().GetGaiaId(), GetClientId(), weather_client_id);
   std::unique_ptr<network::ResourceRequest> resource_request =
       CreateResourceRequest(request);
   auto backdrop_url_loader = std::make_unique<BackdropURLLoader>();
--- ash/birch/birch_weather_provider.cc
+++ ash/birch/birch_weather_provider.cc
@@ -100,15 +100,10 @@ void BirchWeatherProvider::RequestBirchDataFetch() {
 }
 
 void BirchWeatherProvider::FetchWeather() {
-  // Use the prod endpoint by default. This results in the alpha server being
-  // used for canary/dev channel and the prod server being used for beta/stable.
-  const bool prefer_prod_endpoint = base::GetFieldTrialParamByFeatureAsBool(
-      features::kBirchWeather, "prod_weather_endpoint", true);
   Shell::Get()
       ->ambient_controller()
       ->ambient_backend_controller()
       ->FetchWeather("chromeos-system-ui",
-                     /*prefer_alpha_endpoint=*/!prefer_prod_endpoint,
                      base::BindOnce(&BirchWeatherProvider::OnWeatherInfoFetched,
                                     weak_factory_.GetWeakPtr()));
 }
