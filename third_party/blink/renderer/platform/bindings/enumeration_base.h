3a09658d25f070e477798dc8cabceaceb2308862
Dave Tapuska <dtapuska@chromium.org>
2024-10-03 21:25:54 +0000

    [bindings] Make enumeration -> String creation explicit
    To ensure we don't regress the fixes in the bindings code to use
    an enumeration where possible make the conversion to String explicit.
    
    Bug: 366411876, 370425456
    Change-Id: Ia39ed4bca0788e00b66db7c87fe3d415b6edaa58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905745
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363868}

--- third_party/blink/renderer/platform/bindings/enumeration_base.h
+++ third_party/blink/renderer/platform/bindings/enumeration_base.h
@@ -36,7 +36,7 @@ class PLATFORM_EXPORT EnumerationBase {
 
   // Migration adapter
   operator AtomicString() const { return AtomicString(string_literal_); }
-  operator String() const { return string_literal_; }
+  explicit operator String() const { return string_literal_; }
 
   // Returns true if the value is invalid.  The instance in this state must be
   // created only when an exception is thrown.
