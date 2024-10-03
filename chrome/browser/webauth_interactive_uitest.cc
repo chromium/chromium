9093525f78943856dd27cf29c731305b916b319c
Liam Brady <lbrady@google.com>
2024-10-03 00:06:45 +0000

    [Code health] Refactor WebAuth tests to use Kombucha.
    The WebAuth focus test has been disabled since 2021 due to flakiness.
    This CL refactors the tests to use the Kombucha test framework to
    hopefully eliminate any flakes. The old test was a single test that was
    checking multiple scenarios, so this CL splits that into a separate
    test for each scenario.
    
    Bug: 40774271
    Change-Id: Ibd9c470d0fec27dae0ec0a76fd94d2d735e6ecec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5886402
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363399}

--- chrome/browser/webauth_interactive_uitest.cc
+++ chrome/browser/webauth_interactive_uitest.cc
@@ -2,35 +2,52 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/bind.h"
-#include "build/build_config.h"
 #include "chrome/browser/devtools/devtools_window_testing.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/webauthn/authenticator_request_scheduler.h"
 #include "chrome/browser/webauthn/chrome_authenticator_request_delegate.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "content/public/browser/scoped_authenticator_environment_for_testing.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "device/fido/virtual_fido_device_factory.h"
 #include "net/dns/mock_host_resolver.h"
-#include "net/test/embedded_test_server/embedded_test_server.h"
-#include "testing/gmock/include/gmock/gmock.h"
 
 namespace {
 
-class WebAuthFocusTest : public InProcessBrowserTest,
+DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kFirstTabId);
+DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kSecondTabId);
+
+constexpr char kRegisterTemplate[] =
+    "() => {"
+    "  return navigator.credentials.create({publicKey: {"
+    "    rp: {name: 't'},"
+    "    user: {id: new Uint8Array([1]), name: 't', displayName: 't'},"
+    "    challenge: new Uint8Array([1,2,3,4]),"
+    "    timeout: 10000,"
+    "    attestation: '$1',"
+    "    pubKeyCredParams: [{type: 'public-key', alg: -7}]"
+    "  }}).then(c => {return 'OK';},"
+    "           e => {return e.toString();})"
+    "}";
+
+constexpr char kOkText[] = "OK";
+constexpr char kNotAllowedText[] =
+    "NotAllowedError: The operation is not allowed at this time because the "
+    "page does not have focus.";
+
+class WebAuthFocusTest : public InteractiveBrowserTest,
                          public AuthenticatorRequestDialogModel::Observer {
- protected:
-  WebAuthFocusTest()
-      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
+ public:
+  WebAuthFocusTest() : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
 
   WebAuthFocusTest(const WebAuthFocusTest&) = delete;
   WebAuthFocusTest& operator=(const WebAuthFocusTest&) = delete;
@@ -39,6 +56,15 @@ class WebAuthFocusTest : public InProcessBrowserTest,
     host_resolver()->AddRule("*", "127.0.0.1");
     https_server_.ServeFilesFromSourceDirectory("content/test/data");
     ASSERT_TRUE(https_server_.Start());
+    InteractiveBrowserTest::SetUpOnMainThread();
+    ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
+
+    auto virtual_device_factory =
+        std::make_unique<device::test::VirtualFidoDeviceFactory>();
+    virtual_device_factory_ = virtual_device_factory.get();
+    auth_env_ =
+        std::make_unique<content::ScopedAuthenticatorEnvironmentForTesting>(
+            std::move(virtual_device_factory));
   }
 
   GURL GetHttpsURL(const std::string& hostname,
@@ -46,7 +72,9 @@ class WebAuthFocusTest : public InProcessBrowserTest,
     return https_server_.GetURL(hostname, relative_url);
   }
 
-  raw_ptr<AuthenticatorRequestDialogModel> dialog_model_;
+  device::test::VirtualFidoDeviceFactory* virtual_device_factory() {
+    return virtual_device_factory_;
+  }
 
  private:
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -55,117 +83,111 @@ class WebAuthFocusTest : public InProcessBrowserTest,
 
   void OnModelDestroyed(AuthenticatorRequestDialogModel* model) override {}
 
+  std::unique_ptr<content::ScopedAuthenticatorEnvironmentForTesting> auth_env_;
+  raw_ptr<device::test::VirtualFidoDeviceFactory> virtual_device_factory_;
+
   net::EmbeddedTestServer https_server_;
 };
 
-// TODO(crbug.com/40774271): Disabled for being flaky.
-IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, DISABLED_Focus) {
+IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, SucceedWhenTabIsFocused) {
+  RunTestSequence(
+      InstrumentTab(kFirstTabId),
+      NavigateWebContents(kFirstTabId,
+                          GetHttpsURL("www.example.com", "/title1.html")),
+      CheckJsResult(
+          kFirstTabId,
+          base::ReplaceStringPlaceholders(
+              kRegisterTemplate, std::vector<std::string>{"none"}, nullptr),
+          kOkText));
+}
+
+IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, FailWhenTabIsNotFocused) {
   // Web Authentication requests will often trigger machine-wide indications,
   // such as a Security Key flashing for a touch. If background tabs were able
   // to trigger this, there would be a risk of user confusion since the user
   // would not know which tab they would be interacting with if they touched a
   // Security Key. Because of that, some Web Authentication APIs require that
   // the frame be in the foreground in a focused window.
+  RunTestSequence(
+      InstrumentTab(kFirstTabId),
+      NavigateWebContents(kFirstTabId,
+                          GetHttpsURL("www.example.com", "/title1.html")),
+      AddInstrumentedTab(kSecondTabId,
+                         GetHttpsURL("www.example.com", "/title1.html")),
+      CheckJsResult(
+          kFirstTabId,
+          base::ReplaceStringPlaceholders(
+              kRegisterTemplate, std::vector<std::string>{"none"}, nullptr),
+          kNotAllowedText),
+      CheckJsResult(
+          kSecondTabId,
+          base::ReplaceStringPlaceholders(
+              kRegisterTemplate, std::vector<std::string>{"none"}, nullptr),
+          kOkText));
+}
 
-  ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), GetHttpsURL("www.example.com", "/title1.html")));
-
-  auto owned_virtual_device_factory =
-      std::make_unique<device::test::VirtualFidoDeviceFactory>();
-  auto* virtual_device_factory = owned_virtual_device_factory.get();
-  content::ScopedAuthenticatorEnvironmentForTesting auth_env(
-      std::move(owned_virtual_device_factory));
-
-  constexpr char kRegisterTemplate[] =
-      "navigator.credentials.create({publicKey: {"
-      "  rp: {name: 't'},"
-      "  user: {id: new Uint8Array([1]), name: 't', displayName: 't'},"
-      "  challenge: new Uint8Array([1,2,3,4]),"
-      "  timeout: 10000,"
-      "  attestation: '$1',"
-      "  pubKeyCredParams: [{type: 'public-key', alg: -7}]"
-      "}}).then(c => 'OK',"
-      "         e => e.toString());";
-  const std::string register_script = base::ReplaceStringPlaceholders(
-      kRegisterTemplate, std::vector<std::string>{"none"}, nullptr);
-
-  content::WebContents* const initial_web_contents =
-      browser()->tab_strip_model()->GetActiveWebContents();
-
-  // When operating in the foreground, the operation should succeed.
-  EXPECT_EQ("OK", content::EvalJs(initial_web_contents, register_script));
-
-  // Open a new tab to put the previous page in the background.
-  chrome::NewTab(browser());
-
-  // When in the background, the same request should result in a focus error.
-  std::string result =
-      content::EvalJs(initial_web_contents, register_script).ExtractString();
-  constexpr char kFocusErrorSubstring[] = "the page does not have focus";
-  EXPECT_THAT(result, ::testing::HasSubstr(kFocusErrorSubstring));
-
-  // Close the tab and the action should succeed again.
-  chrome::CloseTab(browser());
-  EXPECT_EQ("OK", content::EvalJs(initial_web_contents, register_script));
-
+IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, FailWhenTabFocusChangesWhileRunning) {
   // Start the request in the foreground and open a new tab between starting and
   // finishing the request. This should fail because we don't want foreground
   // pages to be able to start a request, open a trusted site in a new
   // tab/window, and have the user believe that they are interacting with that
   // trusted site.
-  virtual_device_factory->mutable_state()->simulate_press_callback =
+  virtual_device_factory()->mutable_state()->simulate_press_callback =
       base::BindRepeating(
           [](Browser* browser, device::VirtualFidoDevice* device) {
             chrome::NewTab(browser);
             return true;
           },
           browser());
-  result =
-      content::EvalJs(initial_web_contents, register_script).ExtractString();
-  EXPECT_THAT(result, ::testing::HasSubstr(kFocusErrorSubstring));
+  RunTestSequence(
+      InstrumentTab(kFirstTabId),
+      NavigateWebContents(kFirstTabId,
+                          GetHttpsURL("www.example.com", "/title1.html")),
+      CheckJsResult(
+          kFirstTabId,
+          base::ReplaceStringPlaceholders(
+              kRegisterTemplate, std::vector<std::string>{"none"}, nullptr),
+          kNotAllowedText));
+}
 
-  // Close the tab and the action should succeed again.
-  chrome::CloseTab(browser());
-  virtual_device_factory->mutable_state()->simulate_press_callback.Reset();
-  EXPECT_EQ("OK", content::EvalJs(initial_web_contents, register_script));
+IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, SucceedWithDevtoolsOpen) {
+  RunTestSequence(
+      InstrumentTab(kFirstTabId),
+      NavigateWebContents(kFirstTabId,
+                          GetHttpsURL("www.example.com", "/title1.html")),
+      FocusWebContents(kFirstTabId),
+      CheckJsResult(kFirstTabId, "() => document.hasFocus()", true),
+      Do([this]() {
+        DevToolsWindowTesting::OpenDevToolsWindowSync(
+            browser()->tab_strip_model()->GetActiveWebContents(),
+            true /* docked, not a separate window */);
+      }),
+      CheckJsResult(kFirstTabId, "() => document.hasFocus()", false),
+      CheckJsResult(
+          kFirstTabId,
+          base::ReplaceStringPlaceholders(
+              kRegisterTemplate, std::vector<std::string>{"none"}, nullptr),
+          kOkText));
+}
 
-  // Open dev tools and check that operations still succeed.
-  DevToolsWindow* dev_tools_window =
-      DevToolsWindowTesting::OpenDevToolsWindowSync(
-          initial_web_contents, true /* docked, not a separate window */);
-  EXPECT_EQ("OK", content::EvalJs(initial_web_contents, register_script));
-  DevToolsWindowTesting::CloseDevToolsWindowSync(dev_tools_window);
-
-  // Open a second browser window.
-  chrome::NewWindow(browser());
-  Browser* new_window = BrowserList::GetInstance()->GetLastActive();
-  ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(new_window));
-
-  // Operations in the (now unfocused) window should still succeed, as the
-  // calling tab is still the active tab in that window.
-  EXPECT_EQ("OK", content::EvalJs(initial_web_contents, register_script));
-
-  // Check that closing the window brings things back to a focused state.
-  chrome::CloseWindow(new_window);
-  ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
-  EXPECT_EQ("OK", content::EvalJs(initial_web_contents, register_script));
-
-  // Try with "direct" attestation.
-  virtual_device_factory->mutable_state()->simulate_press_callback =
-      base::BindLambdaForTesting([&](device::VirtualFidoDevice* device) {
-        dialog_model_ = AuthenticatorRequestScheduler::GetRequestDelegate(
-                            initial_web_contents)
-                            ->dialog_model();
-        dialog_model_->observers.AddObserver(this);
-        return true;
-      });
-
-  const std::string get_assertion_with_attestation_script =
-      base::ReplaceStringPlaceholders(
-          kRegisterTemplate, std::vector<std::string>{"direct"}, nullptr);
-  EXPECT_EQ("OK", content::EvalJs(initial_web_contents,
-                                  get_assertion_with_attestation_script));
+IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, SucceedWithNewWindowOpen) {
+  virtual_device_factory()->mutable_state()->simulate_press_callback =
+      base::BindRepeating(
+          [](Browser* browser, device::VirtualFidoDevice* device) {
+            chrome::NewWindow(browser);
+            Browser* new_window = BrowserList::GetInstance()->GetLastActive();
+            return ui_test_utils::BringBrowserWindowToFront(new_window);
+          },
+          browser());
+  RunTestSequence(
+      InstrumentTab(kFirstTabId),
+      NavigateWebContents(kFirstTabId,
+                          GetHttpsURL("www.example.com", "/title1.html")),
+      CheckJsResult(
+          kFirstTabId,
+          base::ReplaceStringPlaceholders(
+              kRegisterTemplate, std::vector<std::string>{"none"}, nullptr),
+          kOkText));
 }
 
 }  // anonymous namespace
