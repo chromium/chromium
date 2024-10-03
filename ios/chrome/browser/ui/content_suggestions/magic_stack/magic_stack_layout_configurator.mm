1b0ef41b64fd257f4b01e52d24b7862c5d29e4e6
Chris Lu <thegreenfrog@chromium.org>
2024-10-03 14:51:41 +0000

    [ios] Fix one Magic Stack card layout
    UX would like the Magic Stack card to match the width of the MVT card
    when there is only one (e.g. no peeking card).
    
    Video: https://drive.google.com/file/d/1utgacd9AichLLnbTQa6FAwojkkpTHZfi/view?usp=sharing
    
    Bug: 370949874
    Change-Id: If2f13d63d49bce288d26c0febbe23949c88656eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905843
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363632}

--- ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_layout_configurator.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_layout_configurator.mm
@@ -49,15 +49,17 @@
       [NSCollectionLayoutItem itemWithLayoutSize:item_size];
 
   CGSize size = layoutEnvironment.container.contentSize;
+  CGFloat peekingInset = ModuleNarrowerWidthToAllowPeekingForTraitCollection(
+      layoutEnvironment.traitCollection);
+  if ([self.dataSource.snapshot
+          numberOfItemsInSection:kMagicStackSectionIdentifier] == 1) {
+    peekingInset = 0;
+  }
   // Group size of fixed width for a module and height matching that of the
   // CollectionView.
   NSCollectionLayoutSize* group_size = [NSCollectionLayoutSize
-      sizeWithWidthDimension:
-          [NSCollectionLayoutDimension
-              absoluteDimension:
-                  size.width -
-                  ModuleNarrowerWidthToAllowPeekingForTraitCollection(
-                      layoutEnvironment.traitCollection)]
+      sizeWithWidthDimension:[NSCollectionLayoutDimension
+                                 absoluteDimension:size.width - peekingInset]
              heightDimension:[NSCollectionLayoutDimension
                                  fractionalHeightDimension:1.]];
   if (sectionIndex ==
