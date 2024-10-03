c4f66b7cfdcefc020a2ab40f7293da6273232618
Kent Tamura <tkent@chromium.org>
2024-10-03 08:37:20 +0000

    Spanify inline_node_test.cc
    Use a safe array.
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: I6609f949f7953b95028da4f81a310079088ba4a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904319
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1363524}

--- third_party/blink/renderer/core/layout/inline/inline_node_test.cc
+++ third_party/blink/renderer/core/layout/inline/inline_node_test.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/layout/inline/inline_node.h"
 
 #include "testing/gmock/include/gmock/gmock.h"
@@ -504,7 +499,7 @@ TEST_F(InlineNodeTest, SegmentBidiIsolate) {
 
 struct MinMaxData {
   const char* content;
-  int min_max[2];
+  std::array<int, 2> min_max;
   const char* target_style = "";
   const char* style = "";
   const char* lang = nullptr;
