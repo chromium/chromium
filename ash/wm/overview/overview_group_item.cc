82e1637a39c425c301ccc76d3e820928e8d5fff3
Michele Fan <michelefan@chromium.org>
2024-10-04 21:18:17 +0000

    snap-group: Clean up `OverviewGroupContainerView`
    The `OverviewGroupContainerView` was initially implemented to
    facilitate the first iteration of Overview design when the group
    container view was responsible for handling events. However,
    this approach is no longer needed.
    
    Fixed: b/371222701
    Test: Manual + run existing
    Change-Id: Ica2f03d1eab20b8b8857ffc5965e1d0fd3898e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908594
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364446}

--- ash/wm/overview/overview_group_item.cc
+++ ash/wm/overview/overview_group_item.cc
@@ -12,7 +12,6 @@
 #include "ash/wm/overview/overview_constants.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_grid.h"
-#include "ash/wm/overview/overview_group_container_view.h"
 #include "ash/wm/overview/overview_item.h"
 #include "ash/wm/overview/overview_item_base.h"
 #include "ash/wm/overview/overview_item_view.h"
@@ -34,6 +33,7 @@
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
+#include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
@@ -566,8 +566,8 @@ void OverviewGroupItem::CreateItemWidget() {
 
   CreateShadow();
 
-  overview_group_container_view_ = item_widget_->SetContentsView(
-      std::make_unique<OverviewGroupContainerView>(this));
+  overview_group_container_view_ =
+      item_widget_->SetContentsView(std::make_unique<views::View>());
   item_widget_->Show();
   item_widget_->GetLayer()->SetMasksToBounds(/*masks_to_bounds=*/false);
 }
