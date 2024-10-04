eecf97d1c30ec9a23d5b545f726df528b5610086
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-10-04 20:13:48 +0000

    Handle clicks & extra states in ShowBookmarkAccountStorageMoveDialog()
    No behavior change because the function currently isn't called. This CL:
    
    1) Have the dialog appearance depend on whether the moved BookmarkNode
    is a bookmark or a folder, and on the destination of the move (account
    or device). There are 4 states in total, screenshots are in
    https://crbug.com/#comment10. This is covered by pixel tests.
    
    2) Adds click handling to the dialog buttons. If the ok button is
    clicked, the code moves the bookmark and then calls back.
    If the cancel button is clicked, it just calls back. This is covered by
    interactive tests.
    
    Fixed: 354896249
    Change-Id: Ifdac30eef4e4170b36d3b9638769cafc8ffcb1c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5895535
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364412}

--- chrome/browser/ui/views/bookmarks/bookmark_account_storage_move_dialog.h
+++ chrome/browser/ui/views/bookmarks/bookmark_account_storage_move_dialog.h
@@ -5,12 +5,33 @@
 #ifndef CHROME_BROWSER_UI_VIEWS_BOOKMARKS_BOOKMARK_ACCOUNT_STORAGE_MOVE_DIALOG_H_
 #define CHROME_BROWSER_UI_VIEWS_BOOKMARKS_BOOKMARK_ACCOUNT_STORAGE_MOVE_DIALOG_H_
 
+#include "base/functional/callback_forward.h"
+#include "ui/base/interaction/element_identifier.h"
+
 class Browser;
 
-// Shows a modal dialog in `browser`'s window that offers to move bookmark(s)
-// from local-only storage to the signed-in account, or the other way around.
-// Must only be called if there is a signed-in account.
-// TODO(crbug.com/354896249): Take a dismissal callback as argument.
-void ShowBookmarkAccountStorageMoveDialog(Browser* browser);
+namespace bookmarks {
+class BookmarkNode;
+}  // namespace bookmarks
+
+// IDs for the button dialogs, to allow pressing them in tests.
+DECLARE_ELEMENT_IDENTIFIER_VALUE(kBookmarkAccountStorageMoveDialogOkButton);
+DECLARE_ELEMENT_IDENTIFIER_VALUE(kBookmarkAccountStorageMoveDialogCancelButton);
+
+// Shows a modal dialog in `browser`'s window that offers to move `node` to a
+// `target_folder` from a different bookmark storage. That is, either `node` is
+// local-only and `target_folder` is in the signed-in account, or the other way
+// around. `closed_callback` will be invoked once the dialog is closed, either
+// because the user accepted the move or canceled it (current callers don't need
+// to distinguish the 2 cases). If the user accepts, `node` will become the
+// `index`-th child of `target_folder`.
+// Must only be called if there is a signed-in account, and if `node` and
+// `target_folder` have different storages, otherwise the call will crash.
+void ShowBookmarkAccountStorageMoveDialog(
+    Browser* browser,
+    const bookmarks::BookmarkNode* node,
+    const bookmarks::BookmarkNode* target_folder,
+    size_t index,
+    base::OnceClosure closed_callback);
 
 #endif  // CHROME_BROWSER_UI_VIEWS_BOOKMARKS_BOOKMARK_ACCOUNT_STORAGE_MOVE_DIALOG_H_
