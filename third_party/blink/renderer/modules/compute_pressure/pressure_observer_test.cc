826efb4a6b147d1b08d95b1a92b78c8d508f7ad0
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2024-10-04 18:44:22 +0000

    compute pressure: Rewrite rate obfuscation web tests as unit tests
    These tests have been flaky since they have been rewritten to use
    WebDriver (bug 347031400). https://crrev.com/c/5898989 added them to
    SlowTests, but that did not work: contrary to what I expected, marking a
    test as long in WPT's metadata and adding it to SlowTests are
    idempotent, not cumulative operations (i.e. I thought adding a test to
    SlowTests would increase the timeout even more, but it's just a no-op if
    the test is already marked as long).
    
    However, after investigating it further I believe this is not just a
    timeout problem: these tests cannot work anymore by definition. The idea
    of these tests is to send a number of updates and verify that:
    
    * Sending a number of updates smaller than the spec-defined threshold to
      trigger the mitigations causes the difference between
      PressureRecord.time in subsequent updates to be smaller than the
      penalty time.
    
    * Sending a number of updates greater than the spec-defined threshold
      does trigger the mitigation and cause update N within a spec-defined
      range to be sent with an implementation-defined delay.
    
    In all cases, we need to send an update to WebDriver and verify that
    PressureObserver has received it before sending the next one.
    Consequently, the time it takes between sending and receiving updates is
    actually outside the test's control: busy bots (such as the mac-rel and
    win-rel ones) can simply cause the web test to WebDriver or the
    WebDriver to browser process communication to take arbitrarily long,
    leading to:
    
    * Measured time between updates that should not trigger a penalty still
      taking longer than allowed because the system took too long to deliver
      the update_virtual_pressure_source() call. This causes the "not
      triggered" test to fail and leads to false positives in the
      "triggered" test.
    
    * Calls to update_virtual_pressure_source() taking too long to be on the
      browser side so that and/or too few updates being sent from the
      browser side while waiting under penalty so that the difference
      between the pre-penalty update's timestamp and the
      update-under-penalty's timestamp ends up being less than the minimum
      penalty time. This makes the "triggered" test never flip the
      |gotPenalty| flag to true.
    
    This was not a problem in the past because the MojoJS-based tests were
    restricted to Blink.
    
    The only way to have full control over the timing is to use unit tests
    where we can have a mock time source that we move forward as we see fit.
    
    Bug: 370726578, 347031400
    Change-Id: I9b21513c7b8e83177e7d7b091607100d0cd21c87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5909373
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1364368}

--- third_party/blink/renderer/modules/compute_pressure/pressure_observer_test.cc
+++ third_party/blink/renderer/modules/compute_pressure/pressure_observer_test.cc
@@ -8,16 +8,24 @@
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/bindings/core/v8/native_value_traits.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_function.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_pressure_observer_options.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_pressure_record.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_pressure_state.h"
 #include "third_party/blink/renderer/modules/compute_pressure/pressure_observer_test_utils.h"
+#include "third_party/blink/renderer/modules/compute_pressure/pressure_record.h"
+#include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
+#include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
 #include "v8/include/v8.h"
+
+using device::mojom::blink::PressureState;
 
 namespace blink {
 
@@ -46,6 +54,44 @@ class ClosureRunnerCallable final : public ScriptFunction::Callable {
   base::OnceClosure callback_;
 };
 
+// Helper class expected to be used as a PressureObserver callback (i.e.
+// PressureUpdateCallback). When invoked, it takes the |changes| array passed as
+// a first argument to the callback and stores its elements, which can later be
+// retrieved by the pressure_records() method.
+//
+// This is similar to the following in JS:
+//
+// let pressure_records = [];
+// const PressureRecordAccumulator = (changes) => {
+//   pressure_records = pressure_records.concat(changes);
+// }
+// /* Later on */
+// const observer = new PressureObserver(PressureRecordAccumulator);
+class PressureRecordAccumulator final : public ScriptFunction::Callable {
+ public:
+  ScriptValue Call(ScriptState*, ScriptValue script_value) override {
+    {
+      NonThrowableExceptionState exception_state;
+      const auto& updates =
+          NativeValueTraits<IDLSequence<PressureRecord>>::NativeValue(
+              script_value.GetIsolate(), script_value.V8Value(),
+              exception_state);
+      pressure_records_.AppendVector(updates);
+    }
+    return ScriptValue();
+  }
+
+  const auto& pressure_records() const { return pressure_records_; }
+
+  void Trace(Visitor* visitor) const override {
+    visitor->Trace(pressure_records_);
+    ScriptFunction::Callable::Trace(visitor);
+  }
+
+ private:
+  HeapVector<Member<PressureRecord>> pressure_records_;
+};
+
 void WaitForPromiseFulfillment(ScriptState* script_state,
                                ScriptPromiseUntyped promise) {
   base::RunLoop run_loop;
@@ -60,6 +106,150 @@ void WaitForPromiseFulfillment(ScriptState* script_state,
 }
 
 }  // namespace
+
+TEST(PressureObserverTest, RateObfuscationMitigation) {
+  test::TaskEnvironment task_environment(
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME);
+
+  FakePressureService pressure_service;
+  ComputePressureTestingContext scope(&pressure_service);
+
+  auto* pressure_record_accumulator =
+      MakeGarbageCollected<PressureRecordAccumulator>();
+  auto* callback = V8PressureUpdateCallback::Create(
+      MakeGarbageCollected<ScriptFunction>(scope.GetScriptState(),
+                                           pressure_record_accumulator)
+          ->V8Function());
+
+  constexpr size_t kNumPressureStates =
+      static_cast<size_t>(PressureState::kMaxValue) + 1U;
+  constexpr std::array<device::mojom::blink::PressureState, kNumPressureStates>
+      kPressureStates = {
+          PressureState::kNominal,
+          PressureState::kFair,
+          PressureState::kSerious,
+          PressureState::kCritical,
+      };
+  constexpr base::TimeDelta kSamplingInterval = base::Milliseconds(200);
+  constexpr base::TimeDelta kSmallInterval = base::Milliseconds(100);
+
+  auto* observer = PressureObserver::Create(callback);
+  // Add 1 to kNumPressureStats because we want to initially send kNumPressure
+  // updates without triggering the rate obfuscation mitigations.
+  observer->change_rate_monitor_for_testing()
+      .set_change_count_threshold_for_testing(kNumPressureStates + 1U);
+  observer->change_rate_monitor_for_testing().set_penalty_duration_for_testing(
+      kPenaltyDuration);
+
+  auto* options = PressureObserverOptions::Create();
+  options->setSampleInterval(kSamplingInterval.InMilliseconds());
+  auto promise = observer->observe(
+      scope.GetScriptState(), V8PressureSource(V8PressureSource::Enum::kCpu),
+      options, scope.GetExceptionState());
+  WaitForPromiseFulfillment(scope.GetScriptState(), promise);
+
+  // Fast-forward by any positive amount of time just so that any
+  // base::TimeTicks::Now() invocation differs from the original value recorded
+  // by ChangeRateMonitor when it was created by PressureObserver.
+  task_environment.FastForwardBy(kSmallInterval);
+
+  const auto& pressure_records =
+      pressure_record_accumulator->pressure_records();
+
+  // First test sending updates without triggering the rate obfuscation
+  // mitigation. We send kNumPressureStates updates and verify that they were
+  // sent without delay.
+  {
+    for (wtf_size_t i = 0; i < kPressureStates.size(); ++i) {
+      // None of these updates trigger the rate obfuscation mitigations because
+      // of the value we passed to set_change_count_threshold_for_testing(), so
+      // they are all dispatched immediately even if we do not advance time.
+      EXPECT_EQ(pressure_records.size(), i);
+      const auto& state = kPressureStates[i];
+      pressure_service.SendUpdate(device::mojom::blink::PressureUpdate::New(
+          device::mojom::blink::PressureSource::kCpu, state,
+          base::TimeTicks::Now()));
+      task_environment.FastForwardBy(base::Milliseconds(0));
+      EXPECT_EQ(pressure_records.size(), i + 1);
+
+      // Advance time nonetheless so that the next update is sent with a more
+      // recent timestamp.
+      task_environment.FastForwardBy(kSamplingInterval);
+    }
+    ASSERT_EQ(pressure_records.size(), kNumPressureStates);
+
+    // While here, check that PressureRecord.time is recorded properly for each
+    // update. The difference between each timestamp should be
+    // kSamplingInterval, which is how much we fast-forwarded by in the loop
+    // above.
+    for (wtf_size_t i = 0; i < (pressure_records.size() - 1U); ++i) {
+      EXPECT_EQ(pressure_records[i + 1]->time() - pressure_records[i]->time(),
+                kSamplingInterval.InMilliseconds());
+    }
+  }
+
+  // Test the rate obfuscation mitigation. At this point, we have sent
+  // kNumPressureStates updates and therefore activated the rate obfuscation
+  // mitigation for future updates.
+  {
+    const wtf_size_t original_callback_count = pressure_records.size();
+
+    // This update will not be sent immediately and will be queued by the rate
+    // obfuscation code instead.
+    pressure_service.SendUpdate(device::mojom::blink::PressureUpdate::New(
+        device::mojom::blink::PressureSource::kCpu, PressureState::kNominal,
+        base::TimeTicks::Now()));
+    task_environment.FastForwardBy(base::Milliseconds(0));
+    EXPECT_EQ(pressure_records.size(), original_callback_count);
+
+    // Advancing by a delta smaller than kPenaltyDuration (4000ms) also does
+    // not send any updates.
+    task_environment.FastForwardBy(kSamplingInterval);
+    EXPECT_EQ(pressure_records.size(), original_callback_count);
+
+    // Test the what happens when an update is sent while we are already under
+    // penalty: the new update must replace the previously queued one.
+    pressure_service.SendUpdate(device::mojom::blink::PressureUpdate::New(
+        device::mojom::blink::PressureSource::kCpu, PressureState::kFair,
+        base::TimeTicks::Now()));
+    // If we advance another 200ms, we are still 3600s short of the penalty
+    // duration, after which we will finally send an update.
+    task_environment.FastForwardBy(kSamplingInterval);
+    EXPECT_EQ(pressure_records.size(), original_callback_count);
+    // Advance the remaining 3600s to get out of the penalty. This is
+    // kPenaltyDuration minus the two FastForwardBy(kSamplingInterval) calls we
+    // have made.
+    task_environment.FastForwardBy(kPenaltyDuration - kSamplingInterval * 2);
+    const wtf_size_t new_callback_count = original_callback_count + 1U;
+    ASSERT_EQ(pressure_records.size(), new_callback_count);
+
+    // Verify that the update sent is the second one, not the first.
+    // We compare strings to make the output easier to compare in case of error.
+    EXPECT_EQ(pressure_records.back()->state().AsString(),
+              V8PressureState(V8PressureState::Enum::kFair).AsString());
+    // This update was sent after fast-forwarding by 100ms once and by 200ms 5
+    // times.
+    EXPECT_EQ(pressure_records.back()->time(),
+              (kSmallInterval + (5 * kSamplingInterval)).InMilliseconds());
+  }
+
+  // Check that the rate obfuscation mitigation has been reset and not in place
+  // anymore.
+  {
+    const wtf_size_t original_callback_count = pressure_records.size();
+
+    // Send an update and verify it has been delivered with no delay again.
+    pressure_service.SendUpdate(device::mojom::blink::PressureUpdate::New(
+        device::mojom::blink::PressureSource::kCpu, PressureState::kSerious,
+        base::TimeTicks::Now()));
+    task_environment.FastForwardBy(base::Milliseconds(0));
+    EXPECT_EQ(pressure_records.size(), original_callback_count + 1U);
+    EXPECT_EQ(pressure_records.back()->state().AsString(),
+              V8PressureState(V8PressureState::Enum::kSerious).AsString());
+  }
+
+  observer->disconnect();
+}
 
 TEST(PressureObserverTest, PressureObserverDisconnectBeforePenaltyEnd) {
   test::TaskEnvironment task_environment(
@@ -92,16 +282,16 @@ TEST(PressureObserverTest, PressureObserverDisconnectBeforePenaltyEnd) {
   // First update.
   task_environment.FastForwardBy(kDelayTime);
   pressure_service.SendUpdate(device::mojom::blink::PressureUpdate::New(
-      device::mojom::blink::PressureSource::kCpu,
-      device::mojom::blink::PressureState::kCritical, base::TimeTicks::Now()));
+      device::mojom::blink::PressureSource::kCpu, PressureState::kCritical,
+      base::TimeTicks::Now()));
 
   callback_run_loop.Run();
 
   // Second update triggering the penalty.
   task_environment.FastForwardBy(kDelayTime);
   pressure_service.SendUpdate(device::mojom::blink::PressureUpdate::New(
-      device::mojom::blink::PressureSource::kCpu,
-      device::mojom::blink::PressureState::kNominal, base::TimeTicks::Now()));
+      device::mojom::blink::PressureSource::kCpu, PressureState::kNominal,
+      base::TimeTicks::Now()));
   // The number of seconds here should not exceed the penalty time, we just
   // want to run some code like OnUpdate() but not the pending delayed task
   // that it should have created.
@@ -145,16 +335,16 @@ TEST(PressureObserverTest, PressureObserverUnobserveBeforePenaltyEnd) {
   // First update.
   task_environment.FastForwardBy(kDelayTime);
   pressure_service.SendUpdate(device::mojom::blink::PressureUpdate::New(
-      device::mojom::blink::PressureSource::kCpu,
-      device::mojom::blink::PressureState::kNominal, base::TimeTicks::Now()));
+      device::mojom::blink::PressureSource::kCpu, PressureState::kNominal,
+      base::TimeTicks::Now()));
 
   callback_run_loop.Run();
 
   // Second update triggering the penalty.
   task_environment.FastForwardBy(kDelayTime);
   pressure_service.SendUpdate(device::mojom::blink::PressureUpdate::New(
-      device::mojom::blink::PressureSource::kCpu,
-      device::mojom::blink::PressureState::kCritical, base::TimeTicks::Now()));
+      device::mojom::blink::PressureSource::kCpu, PressureState::kCritical,
+      base::TimeTicks::Now()));
   // The number of seconds here should not exceed the penalty time, we just
   // want to run some code like OnUpdate() but not the pending delayed task
   // that it should have created.
