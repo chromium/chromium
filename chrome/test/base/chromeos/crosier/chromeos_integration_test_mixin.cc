535ef8019c146e33365604402ff92f8a2fb7f385
Georg Neis <neis@chromium.org>
2024-10-05 00:32:24 +0000

    Remove obsolete Lacros support from Crosier
    Bug: b:365741912, b:365742770
    AX-Relnotes: n/a.
    Change-Id: I03b9eb30ffb9be6e0b5e905cd6eb1815cb844ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905025
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364519}

--- chrome/test/base/chromeos/crosier/chromeos_integration_test_mixin.cc
+++ chrome/test/base/chromeos/crosier/chromeos_integration_test_mixin.cc
@@ -5,17 +5,11 @@
 #include "chrome/test/base/chromeos/crosier/chromeos_integration_test_mixin.h"
 
 #include "base/command_line.h"
-#include "build/chromeos_buildflags.h"
-#include "ui/compositor/compositor_switches.h"
-#include "ui/gl/gl_switches.h"
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "ash/constants/ash_switches.h"
-#include "base/files/file_util.h"
 #include "base/path_service.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ui/compositor/compositor_switches.h"
+#include "ui/gl/gl_switches.h"
 
 ChromeOSIntegrationTestMixin::ChromeOSIntegrationTestMixin(
     InProcessBrowserTestMixinHost* host)
@@ -31,28 +25,9 @@ void ChromeOSIntegrationTestMixin::SetUpCommandLine(
   // and generating screenshots during test failures.
   command_line->AppendSwitch(switches::kEnablePixelOutputInTests);
   command_line->AppendSwitch(switches::kUseGpuInTests);
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  // For Ash testing, which Lacros version to use is different for Browser
-  // and OS side. The strategy need to comply with the production version skew
-  // guidelines.
-  // On browser side, all Ash builders need to compile Lacros using an
-  // alternate toolchain. Compiled Lacros is under //out/Default/lacros_clang.
-  // So there is no version skew between Ash and Lacros.
-  // On OS side, Ash tests should use the RootFS version packed in the
-  // OS image.
-  base::FilePath path = command_line->GetProgram();
-  path = path.DirName().Append("lacros_clang").Append("chrome");
-  if (base::PathExists(path)) {
-    command_line->AppendSwitchPath(ash::switches::kLacrosChromePath, path);
-    LOG(INFO) << "Testing with locally compiled Lacros.";
-  } else {
-    LOG(INFO) << "Testing with RootFS Lacros.";
-  }
-#endif
 }
 
 bool ChromeOSIntegrationTestMixin::SetUpUserDataDirectory() {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   // Always have --user-data-dir present in commandline arguments.
   // Without the argument, there are some permission issues. Here the logic
   // is: a temporary dir is created in base framework and set in PathService.
@@ -63,6 +38,5 @@ bool ChromeOSIntegrationTestMixin::SetUpUserDataDirectory() {
   if (!cmdline->HasSwitch(switches::kUserDataDir)) {
     cmdline->AppendSwitchPath(switches::kUserDataDir, user_data_dir);
   }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   return InProcessBrowserTestMixin::SetUpUserDataDirectory();
 }
--- chrome/test/base/chromeos/crosier/chromeos_test_suite.cc
+++ chrome/test/base/chromeos/crosier/chromeos_test_suite.cc
@@ -4,23 +4,14 @@
 
 #include "chrome/test/base/chromeos/crosier/chromeos_test_suite.h"
 
-#include "build/chromeos_buildflags.h"
-#include "ui/events/test/event_generator.h"
-#include "ui/ozone/platform/drm/test/ui_controls_system_input_injector.h"
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/test/ui_controls_ash.h"
 #include "base/check.h"
 #include "base/command_line.h"
-#include "base/time/time.h"
 #include "chrome/test/base/chromeos/crosier/helper/switches.h"
 #include "chrome/test/base/chromeos/crosier/helper/test_sudo_helper_client.h"
 #include "content/public/common/content_switches.h"
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "base/check.h"
-#include "base/files/file_util.h"
-#include "chrome/common/chrome_paths_lacros.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+#include "ui/events/test/event_generator.h"
+#include "ui/ozone/platform/drm/test/ui_controls_system_input_injector.h"
 
 ChromeOSTestSuite::ChromeOSTestSuite(int argc, char** argv)
     : content::ContentTestSuiteBase(argc, argv) {}
@@ -33,7 +24,6 @@ void ChromeOSTestSuite::Initialize() {
   // chromeos_integration_tests must use functions in ui_controls.h.
   ui::test::EventGenerator::BanEventGenerator();
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   ui::test::EnableUIControlsSystemInputInjector();
 
   base::CommandLine* cmdline = base::CommandLine::ForCurrentProcess();
@@ -45,23 +35,4 @@ void ChromeOSTestSuite::Initialize() {
   }
 
   cmdline->AppendSwitch(switches::kDisableMojoBroker);
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
-  // The lacros binary receives certain paths from ash very early in startup.
-  // Simulate that behavior here. See chrome_paths_lacros.cc for details. The
-  // specific path doesn't matter as long as it exists.
-  CHECK(scoped_temp_dir_.CreateUniqueTempDir());
-  base::FilePath temp_path = scoped_temp_dir_.GetPath();
-  chrome::SetLacrosDefaultPaths(
-      /*documents_dir=*/temp_path,
-      /*downloads_dir=*/temp_path,
-      /*drivefs=*/base::FilePath(),
-      /*onedrive=*/base::FilePath(),
-      /*removable_media_dir=*/base::FilePath(),
-      /*android_files_dir=*/base::FilePath(),
-      /*linux_files_dir=*/base::FilePath(),
-      /*ash_resources_dir=*/base::FilePath(),
-      /*share_cache_dir=*/temp_path,
-      /*preinstalled_web_app_config_dir=*/base::FilePath(),
-      /*preinstalled_web_app_extra_config_dir=*/base::FilePath());
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 }
