aec274ddaf739e60e09c6c0ff4dd84f9ec82e893
Chase Hartsell <chazzy@chromium.org>
2024-10-07 23:02:22 +0000

    [130][Compare] Add feature for product specifications cache
    This patch adds a feature for the product specifications cache to allow
    enabling and disabling caching.
    
    (cherry picked from commit 4a709d418f7c67db1a037da4558fff542b9396d1)
    
    Bug: 371047963
    Change-Id: Ic744b2bfb1d92d357fb7c0ff767460b89f11a688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906501
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1363761}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5907274
    Cr-Commit-Position: refs/branch-heads/6723@{#1069}
    Cr-Branched-From: 985f2961df230630f9cbd75bd6fe463009855a11-refs/heads/main@{#1356013}

--- components/commerce/core/commerce_feature_list.h
+++ components/commerce/core/commerce_feature_list.h
@@ -84,6 +84,7 @@ BASE_DECLARE_FEATURE(kProductSpecifications);
 BASE_DECLARE_FEATURE(kProductSpecificationsClearMetadataOnNewlySupportedFields);
 BASE_DECLARE_FEATURE(kProductSpecificationsMultiSpecifics);
 BASE_DECLARE_FEATURE(kProductSpecificationsSyncTitle);
+BASE_DECLARE_FEATURE(kProductSpecificationsCache);
 
 BASE_DECLARE_FEATURE(kShoppingIconColorVariant);
 BASE_DECLARE_FEATURE(kShoppingList);
