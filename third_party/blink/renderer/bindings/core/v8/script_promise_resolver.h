f083fbd51af10091356b5f6f245fa6830691597e
Tom Sepez <tsepez@chromium.org>
2024-10-07 23:16:26 +0000

    Avoid some unintentional WTF::Vector copies.
    Applies one or more of the following:
    -- std::move(argument) where possible.
    -- add 'const' where helpful.
    -- remove stray 'const' where not helpful.
    -- pass by ref where helpful.
    -- introduce Take*() methods for zero-copy from buffer (just prior
       to destruction anyways).
    
    This CL was uploaded by git cl split.
    
    R=kouhei@chromium.org
    
    Bug: 369952985
    Change-Id: I14f26f042d1f7e052ead720a20e37481027d00c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905938
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365215}

--- third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h
+++ third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h
@@ -89,7 +89,7 @@ class CORE_EXPORT ScriptPromiseResolverBase
     if (!PrepareToResolveOrReject<kRejecting>()) {
       return;
     }
-    ResolveOrReject<IDLType, BlinkType>(value);
+    ResolveOrReject<IDLType, BlinkType>(std::move(value));
   }
 
   // These are shorthand helpers for rejecting the promise with a common type.
@@ -187,7 +187,8 @@ class CORE_EXPORT ScriptPromiseResolverBase
       v8::MicrotasksScope microtasks_scope(
           isolate, ToMicrotaskQueue(script_state_.Get()),
           v8::MicrotasksScope::kDoNotRunMicrotasks);
-      value_.Reset(isolate, ToV8Traits<IDLType>::ToV8(script_state_, value));
+      value_.Reset(isolate,
+                   ToV8Traits<IDLType>::ToV8(script_state_, std::move(value)));
     }
     NotifyResolveOrReject();
   }
@@ -251,7 +252,7 @@ class ScriptPromiseResolver final : public ScriptPromiseResolverBase {
     if (!PrepareToResolveOrReject<kResolving>()) {
       return;
     }
-    ResolveOrReject<IDLResolvedType, BlinkType>(value);
+    ResolveOrReject<IDLResolvedType, BlinkType>(std::move(value));
   }
 
   // This Resolve() variant completely ignores the ScriptState given in the
@@ -263,7 +264,7 @@ class ScriptPromiseResolver final : public ScriptPromiseResolverBase {
     if (!PrepareToResolveOrReject<kResolving>()) {
       return;
     }
-    ResolveOrReject<IDLResolvedType, BlinkType>(value);
+    ResolveOrReject<IDLResolvedType, BlinkType>(std::move(value));
   }
 
   // This Resolve() method allows a Promise expecting to be resolved with a
