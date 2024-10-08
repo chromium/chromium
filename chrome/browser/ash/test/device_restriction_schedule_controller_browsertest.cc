0fa3e3ec22e71354adb9c58e02a90d8bd9b868e5
Ivan Šandrk <isandrk@chromium.org>
2024-10-08 11:35:12 +0000

    [Bedtime Hours] Add missing browsertests
    Add missing browsertests:
    * That the upcoming logout notification shows
    * That the post-logout notification shows
    
    Bug: b:345186543
    Change-Id: Ib0e7090ec6937965b92ce1a7c5b2a48fe2c7b47b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913017
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365469}

--- chrome/browser/ash/test/device_restriction_schedule_controller_browsertest.cc
+++ chrome/browser/ash/test/device_restriction_schedule_controller_browsertest.cc
@@ -4,8 +4,11 @@
 
 #include "chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller.h"
 
+#include <utility>
+
 #include "ash/constants/ash_switches.h"
 #include "base/json/json_string_value_serializer.h"
+#include "base/scoped_observation.h"
 #include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/login/login_manager_test.h"
@@ -15,9 +18,12 @@
 #include "chrome/browser/ash/login/test/scoped_policy_update.h"
 #include "chrome/browser/lifetime/termination_notification.h"
 #include "chrome/browser/ui/webui/ash/login/device_disabled_screen_handler.h"
+#include "chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller_delegate_impl.h"
 #include "chromeos/ash/components/policy/weekly_time/test_support.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
 #include "content/public/test/browser_test.h"
+#include "ui/message_center/message_center.h"
+#include "ui/message_center/message_center_observer.h"
 
 namespace policy {
 
@@ -57,6 +63,71 @@ class DeviceRestrictionScheduleControllerTest : public ash::LoginManagerTest {
       ash::DeviceStateMixin::State::OOBE_COMPLETED_CLOUD_ENROLLED};
   ash::LoginManagerMixin login_mixin_{&mixin_host_};
 };
+
+class CaptureNotificationWaiter : public message_center::MessageCenterObserver {
+ public:
+  CaptureNotificationWaiter(base::OnceClosure on_notification_added,
+                            const std::string& match_notification_id)
+      : on_notification_added_(std::move(on_notification_added)),
+        match_notification_id_(match_notification_id) {
+    auto* message_center = message_center::MessageCenter::Get();
+    if (message_center->FindNotificationById(match_notification_id_)) {
+      std::move(on_notification_added_).Run();
+      return;
+    }
+    observation_.Observe(message_center);
+  }
+
+  // message_center::MessageCenterObserver:
+  void OnNotificationAdded(const std::string& notification_id) override {
+    if (notification_id == match_notification_id_) {
+      std::move(on_notification_added_).Run();
+    }
+  }
+
+ private:
+  base::OnceClosure on_notification_added_;
+  std::string match_notification_id_;
+  base::ScopedObservation<message_center::MessageCenter,
+                          message_center::MessageCenterObserver>
+      observation_{this};
+};
+
+IN_PROC_BROWSER_TEST_F(DeviceRestrictionScheduleControllerTest,
+                       UpcomingLogoutNotificationShows) {
+  LoginUser(login_mixin_.users()[0].account_id);
+
+  // Restriction schedule starts in 20 minutes and lasts for 2 hours.
+  SetRestrictionSchedule(base::Minutes(20), base::Hours(2));
+
+  // Verify that upcoming session end notification shows.
+  base::test::TestFuture<void> future;
+  CaptureNotificationWaiter waiter(
+      future.GetCallback(), DeviceRestrictionScheduleControllerDelegateImpl::
+                                kUpcomingLogoutNotificationId);
+  ASSERT_TRUE(future.Wait());
+}
+
+IN_PROC_BROWSER_TEST_F(DeviceRestrictionScheduleControllerTest,
+                       PRE_PostLogoutNotificationShows) {
+  LoginUser(login_mixin_.users()[0].account_id);
+
+  // Restriction schedule started 20 minutes ago and lasts for 2 hours.
+  SetRestrictionSchedule(-base::Minutes(20), base::Hours(2));
+
+  // Logout happens here (Chrome shuts down), and then we start again on the
+  // login screen in the next part of the test.
+}
+
+IN_PROC_BROWSER_TEST_F(DeviceRestrictionScheduleControllerTest,
+                       PostLogoutNotificationShows) {
+  // Verify that post-logout notification shows.
+  base::test::TestFuture<void> future;
+  CaptureNotificationWaiter waiter(
+      future.GetCallback(), DeviceRestrictionScheduleControllerDelegateImpl::
+                                kPostLogoutNotificationId);
+  ASSERT_TRUE(future.Wait());
+}
 
 IN_PROC_BROWSER_TEST_F(DeviceRestrictionScheduleControllerTest,
                        LogoutOnEnteringRestrictedSchedule) {
