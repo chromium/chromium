ce58e9bfdd0a6569e57249dc14ae3b7f49ecc81a
Nigel Tao <nigeltao@chromium.org>
2024-10-08 10:53:57 +0000

    Image Loader: add MV3 TypeScript files
    This partially rolls forward the recent "Image Loader: roll back to MV2"
    commit: https://crrev.com/c/5859075
    
    It re-adds two *.ts files that were rolled back, but these *.ts files
    are not yet shipped in the image_loader extension. There is no change
    yet to //ui/file_manager/file_names.gni or
    //ui/file_manager/image_loader/BUILD.gn.
    
    This commit just reincarnates these files in the source code repository
    so that we can make further changes to them, with those further changes
    being clearly separate from undoing a rollback.
    
    Bug: b:319190834
    Test: Manual
    Change-Id: Iea6e4188830eabd9353ba2d3d1ce5e3629e166e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912638
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365450}

---
+++ ui/file_manager/image_loader/service_worker.ts
@@ -0,0 +1,143 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Entry point for the Image Loader's service worker.
+ */
+
+import {assert} from 'chrome://resources/js/assert.js';
+
+import type {LoadImageRequest, LoadImageResponse} from './load_image_request.js';
+import type {PrivateApi} from './sw_od_messages.js';
+
+const EXTENSION_ID = 'pmfjbimdmchhbnneeidfognadeopoehp';
+
+const ALLOW_LISTED_FILE_MANAGER_SWA = 'chrome://file-manager';
+const ALLOW_LISTED_NATIVE = 'com.google.ash_thumbnail_loader';
+
+// setupOffscreenDocument is based on
+// https://developer.chrome.com/docs/extensions/reference/api/offscreen#maintain_the_lifecycle_of_an_offscreen_document
+let creatingOffscreenDocument: Promise<void>|undefined;
+async function setupOffscreenDocument() {
+  const url = chrome.runtime.getURL('offscreen.html');
+  const existingContexts = await chrome.runtime.getContexts({
+    contextTypes: [chrome.runtime.ContextType.OFFSCREEN_DOCUMENT],
+    documentUrls: [url],
+  });
+  if (existingContexts.length > 0) {
+    return;
+  }
+
+  if (creatingOffscreenDocument) {
+    await creatingOffscreenDocument;
+    return;
+  }
+
+  creatingOffscreenDocument = chrome.offscreen.createDocument({
+    url: url,
+    // 'USER_MEDIA' isn't quite right, since we never call getUserMedia, but
+    // it's the closest listed reason from
+    // https://developer.chrome.com/docs/extensions/reference/api/offscreen#type-Reason
+    reasons: [chrome.offscreen.Reason.USER_MEDIA],
+    justification: 'Lets us use <img>, <canvas> and XMLHttpRequest',
+  });
+  await creatingOffscreenDocument;
+  creatingOffscreenDocument = undefined;
+}
+
+// Forwards a message from the service worker to the offscreen document.
+//
+// sendResponse will not be called if msg.cancel is truthy. See the
+// ImageLoader.handle method.
+async function sendToOffscreenDocument(
+    msg: LoadImageRequest, senderOrigin: string,
+    sendResponse: (r: LoadImageResponse) => void) {
+  assert(msg);
+  await setupOffscreenDocument();
+  msg.imageLoaderRequestId = senderOrigin + ':' + msg.taskId;
+  chrome.runtime.sendMessage(
+      null, msg, undefined, (response: LoadImageResponse) => {
+        sendResponse(response);
+      });
+}
+
+// Handle imageLoaderPrivate calls on behalf of the offscreen document.
+chrome.runtime.onMessage.addListener(
+    (msg: PrivateApi, sender: chrome.runtime.MessageSender,
+     sendResponse: (thumbnailDataUrl: string) => void) => {
+      if (sender.id !== EXTENSION_ID) {
+        return false;
+      } else if (msg.apiMethod === 'getArcDocumentsProviderThumbnail') {
+        chrome.imageLoaderPrivate.getArcDocumentsProviderThumbnail(
+            msg.params.url,
+            msg.params.widthHint,
+            msg.params.heightHint,
+            sendResponse,
+        );
+        return true;
+      } else if (msg.apiMethod === 'getDriveThumbnail') {
+        chrome.imageLoaderPrivate.getDriveThumbnail(
+            msg.params.url,
+            msg.params.cropToSquare,
+            sendResponse,
+        );
+        return true;
+      } else if (msg.apiMethod === 'getPdfThumbnail') {
+        chrome.imageLoaderPrivate.getPdfThumbnail(
+            msg.params.url,
+            msg.params.width,
+            msg.params.height,
+            sendResponse,
+        );
+        return true;
+      }
+      return false;
+    });
+
+// Listen to messages from the Files app SWA.
+chrome.runtime.onMessageExternal.addListener(
+    (msg: LoadImageRequest, sender: chrome.runtime.MessageSender,
+     sendResponse: (r: LoadImageResponse) => void) => {
+      if (!msg || (sender.origin !== ALLOW_LISTED_FILE_MANAGER_SWA)) {
+        return false;
+      }
+      sendToOffscreenDocument(msg, ALLOW_LISTED_FILE_MANAGER_SWA, sendResponse);
+      // Return true (or false) if the runtime should keep sendResponse
+      // valid-to-call (or invalid) after this closure returns.
+      //
+      // If msg.cancel is truthy then sendToOffscreenDocument will not call
+      // sendResponse, so we can return false.
+      //
+      // If msg.cancel is falsy then sendToOffscreenDocument will call it, but
+      // also asynchronously, so return true.
+      return !msg.cancel;
+    });
+
+// Listen to messages from ash-chrome itself, e.g. thumbnails in the "tote"
+// (also known as the "holding space"), after taking a screenshot.
+//
+// Each native connection is expected to send a single request only, so we
+// disconnect after handling a message.
+chrome.runtime.onConnectNative.addListener((port: chrome.runtime.Port) => {
+  assert(port.sender);
+  if (port.sender.nativeApplication !== ALLOW_LISTED_NATIVE) {
+    port.disconnect();
+    return;
+  }
+
+  port.onMessage.addListener((msg: LoadImageRequest) => {
+    if (!msg) {
+      port.disconnect();
+      return;
+    } else if (msg.cancel) {
+      port.disconnect();
+    }
+    sendToOffscreenDocument(
+        msg, ALLOW_LISTED_NATIVE, (response: LoadImageResponse) => {
+          assert(!msg.cancel);
+          port.postMessage(response);
+          port.disconnect();
+        });
+  });
+});
