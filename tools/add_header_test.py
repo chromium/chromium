b9d8d6aeeef9feacf6eb4838cdccca4c2da2a0eb
Peter Kasting <pkasting@chromium.org>
2024-10-08 18:33:10 +0000

    Tweak Windows header-sorting in .clang-format and add_header.py.
    * Add <winternl.h> to the list of must-be-early Windows headers.
    * Add "windows_h_disallowed.h" as "must be last".
    * Minor comment/regex typo fixes.
    * Alphabetize .clang-format list (cosmetic change; that was already the
      actual behavior).
    * Alphabetize add_header.py change (behavior change; this order is
      the sort key). This matches the .clang-format behavior so they
      don't fight each other.
    
    Bug: 364987728
    Change-Id: Ic8560ab1cf476918c8dd9454dc7ef5ccb5aecd8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913362
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365712}

--- tools/add_header_test.py
+++ tools/add_header_test.py
@@ -376,27 +376,27 @@ class SerializeIncludesTest(unittest.TestCase):
     primary_header = add_header.Include('"cow.h"', 'include', [], None)
     primary_header.is_primary_header = True
     includes.append(primary_header)
+    includes.append(add_header.Include('<objbase.h>', 'include', [], None))
+    includes.append(add_header.Include('<atlbase.h>', 'include', [], None))
+    includes.append(add_header.Include('<ole2.h>', 'include', [], None))
+    includes.append(add_header.Include('<shobjidl.h>', 'include', [], None))
+    includes.append(add_header.Include('<tchar.h>', 'include', [], None))
+    includes.append(add_header.Include('<unknwn.h>', 'include', [], None))
     includes.append(add_header.Include('<winsock2.h>', 'include', [], None))
     includes.append(add_header.Include('<windows.h>', 'include', [], None))
     includes.append(add_header.Include('<ws2tcpip.h>', 'include', [], None))
-    includes.append(add_header.Include('<shobjidl.h>', 'include', [], None))
-    includes.append(add_header.Include('<atlbase.h>', 'include', [], None))
-    includes.append(add_header.Include('<ole2.h>', 'include', [], None))
-    includes.append(add_header.Include('<unknwn.h>', 'include', [], None))
-    includes.append(add_header.Include('<objbase.h>', 'include', [], None))
-    includes.append(add_header.Include('<tchar.h>', 'include', [], None))
-    includes.append(add_header.Include('<string.h>', 'include', [], None))
     includes.append(add_header.Include('<stddef.h>', 'include', [], None))
     includes.append(add_header.Include('<stdio.h>', 'include', [], None))
+    includes.append(add_header.Include('<string.h>', 'include', [], None))
     includes.append(add_header.Include('"moo.h"', 'include', [], None))
     random.shuffle(includes)
     source = add_header.SerializeIncludes(includes)
     self.assertEqual(source, [
-        '#include "cow.h"', '', '#include <winsock2.h>', '#include <windows.h>',
-        '#include <ws2tcpip.h>', '#include <shobjidl.h>',
-        '#include <atlbase.h>', '#include <ole2.h>', '#include <unknwn.h>',
-        '#include <objbase.h>', '#include <tchar.h>', '#include <stddef.h>',
-        '#include <stdio.h>', '#include <string.h>', '', '#include "moo.h"'
+        '#include "cow.h"', '', '#include <objbase.h>', '#include <atlbase.h>',
+        '#include <ole2.h>', '#include <shobjidl.h>', '#include <tchar.h>',
+        '#include <unknwn.h>', '#include <windows.h>', '#include <winsock2.h>',
+        '#include <ws2tcpip.h>', '#include <stddef.h>', '#include <stdio.h>',
+        '#include <string.h>', '', '#include "moo.h"'
     ])
 
 
