c274ebbe3a89d7fe6c82211d980d3059e4401671
Amanda Lin Dietz <aldietz@google.com>
2024-10-09 23:54:00 +0000

    [FaceGaze] Remove long click logic from MouseClickMacro
    Bug: 371052538
    Change-Id: Ia48941fccb43fb9963237fbc1382f826aaad36f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917169
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366535}

--- chrome/browser/resources/chromeos/accessibility/common/action_fulfillment/macros/mouse_click_macro.ts
+++ chrome/browser/resources/chromeos/accessibility/common/action_fulfillment/macros/mouse_click_macro.ts
@@ -11,36 +11,19 @@ import ScreenPoint = chrome.accessibilityPrivate.ScreenPoint;
 import SyntheticMouseEventButton = chrome.accessibilityPrivate.SyntheticMouseEventButton;
 
 /**
- * Class that implements a macro to send a synthetic mouse click.
+ * Class that implements a macro to send a single synthetic mouse click.
  */
 export class MouseClickMacro extends Macro {
   private leftClick_: boolean;
   private location_: ScreenPoint|undefined;
-  private runCount_ = 0;
-  private clickImmediately_: boolean;
 
   /**
-   * Pass the location in density-independent pixels. Defaults to left click and
-   * clicking immediately.
+   * Pass the location in density-independent pixels. Defaults to left click.
    */
-  constructor(
-      location: ScreenPoint|undefined, leftClick = true,
-      clickImmediately = true) {
-    super(
-        leftClick ? (clickImmediately ? MacroName.MOUSE_CLICK_LEFT :
-                                        MacroName.MOUSE_LONG_CLICK_LEFT) :
-                    MacroName.MOUSE_CLICK_RIGHT);
+  constructor(location: ScreenPoint|undefined, leftClick = true) {
+    super(leftClick ? MacroName.MOUSE_CLICK_LEFT : MacroName.MOUSE_CLICK_RIGHT);
     this.leftClick_ = leftClick;
     this.location_ = location;
-    this.clickImmediately_ = clickImmediately;
-  }
-
-  /**
-   * The mouse click macro should be run twice, once when the click begins and
-   * again when it ends.
-   */
-  override triggersAtActionStartAndEnd(): boolean {
-    return !this.clickImmediately_;
   }
 
   /** Invalid context if location isn't set. */
@@ -48,13 +31,8 @@ export class MouseClickMacro extends Macro {
     if (!this.location_) {
       return this.createFailureCheckContextResult_(MacroError.BAD_CONTEXT);
     }
-    if ((this.clickImmediately_ && this.runCount_ > 0) ||
-        (!this.clickImmediately_ && this.runCount_ > 2)) {
-      return this.createFailureCheckContextResult_(
-          MacroError.INVALID_USER_INTENT);
-    } else {
-      return this.createSuccessCheckContextResult_();
-    }
+
+    return this.createSuccessCheckContextResult_();
   }
 
   updateLocation(location?: ScreenPoint): void {
@@ -62,26 +40,15 @@ export class MouseClickMacro extends Macro {
   }
 
   override run(): RunMacroResult {
-    if (!this.location_ || this.runCount_ > 2) {
+    if (!this.location_) {
       return this.createRunMacroResult_(/*isSuccess=*/ false);
     }
     const mouseButton = this.leftClick_ ? SyntheticMouseEventButton.LEFT :
                                           SyntheticMouseEventButton.RIGHT;
 
-    if (this.runCount_ === 0) {
-      EventGenerator.sendMousePress(
-          this.location_.x, this.location_.y, mouseButton);
-      if (this.clickImmediately_) {
-        // Increment run count so we perform the click release.
-        this.runCount_++;
-      }
-    }
-
-    if (this.runCount_ === 1) {
-      EventGenerator.sendMouseRelease(this.location_.x, this.location_.y);
-    }
-
-    this.runCount_++;
+    EventGenerator.sendMousePress(
+        this.location_.x, this.location_.y, mouseButton);
+    EventGenerator.sendMouseRelease(this.location_.x, this.location_.y);
     return this.createRunMacroResult_(/*isSuccess=*/ true);
   }
 }
