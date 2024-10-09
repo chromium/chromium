e9f53c87eebc42c54a819797ccfc9aefbd3ca65b
dpapad <dpapad@chromium.org>
2024-10-09 23:41:13 +0000

    Fix CheckEsLintConfigChanges PRESUBMIT check after ESLint v9 migration.
    This presubmit check was informing developers when an ESLint config file
    changed about manually running a command to ensure no ESLint violations
    were introduced, and it was broken after the replacement of per-folder
    .eslintrc.js files with a global tools/web_dev_style/eslint.config.mjs
    file during the update to ESLint v9.
    
    Porting the check to the new single config world, to print the following
    when needed:
    
    tools/web_dev_style/eslint.config.mjs modified. Consider running 'git cl presubmit --files "*.js;*.ts"' in order to check and fix the affected files before landing this change.
    
    Bug: 365027672
    Change-Id: I5377140b82973b709854676031d7b58565596a07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922516
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366526}

--- tools/web_dev_style/PRESUBMIT.py
+++ tools/web_dev_style/PRESUBMIT.py
@@ -3,6 +3,9 @@
 # found in the LICENSE file.
 
 
+PRESUBMIT_VERSION = '2.0.0'
+
+
 def CheckChangeOnUpload(*args):
   return _CommonChecks(*args)
 
@@ -15,3 +18,22 @@ def _CommonChecks(input_api, output_api):
   tests = ['test_suite.py']
 
   return input_api.canned_checks.RunUnitTests(input_api, output_api, tests)
+
+
+def CheckEsLintConfigChanges(input_api, output_api):
+  """Suggest using "git cl presubmit --files" when the global configuration
+    file eslint.config.mjs file is modified. This is important because
+    modifications to this file can trigger ESLint errors in any .js or .ts
+    files in the repository, leading to hidden presubmit errors."""
+  results = []
+
+  eslint_filter = lambda f: input_api.FilterSourceFile(
+      f, files_to_check=[r'tools/web_dev_style/eslint.config.mjs$'])
+  for f in input_api.AffectedFiles(include_deletes=False,
+                                   file_filter=eslint_filter):
+    results.append(
+        output_api.PresubmitNotifyResult(
+            '%(file)s modified. Consider running \'git cl presubmit '
+            '--files "*.js;*.ts"\' in order to check and fix the affected '
+            'files before landing this change.' % {'file': f.LocalPath()}))
+  return results
