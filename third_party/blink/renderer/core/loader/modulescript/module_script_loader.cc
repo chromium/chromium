2c501634c1191be1e509720103f06d51b94e6311
Justin Lulejian <jlulejian@chromium.org>
2024-10-09 00:57:41 +0000

    [Extensions][ServiceWorker] Skip worker for isolated world module fetch
    Before this change, an isolated world (e.g. extension content script,
    but also others) could dynamically import a script from an accessible
    resource (for extensions this is possible with web accessible
    resources and a matching site). When this occurs a web service worker
    could intercept that request and respond with arbitrary content.
    
    After this change, isolated world module requests skip triggering the
    worker fetch handler. This includes extension content scripts, but also
    includes any other scripts that execute in the isolated world context.
    
    Bug: 371011220
    Change-Id: I37eda47324b6933a93d2a44792a06ff91399981f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917013
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365918}

--- third_party/blink/renderer/core/loader/modulescript/module_script_loader.cc
+++ third_party/blink/renderer/core/loader/modulescript/module_script_loader.cc
@@ -153,12 +153,20 @@ void ModuleScriptLoader::FetchInternal(
   url_ = module_request.Url();
 #endif
 
+  DOMWrapperWorld& request_world = modulator_->GetScriptState()->World();
+
+  // Prevents web service workers from intercepting isolated world dynamic
+  // script imports requests and responding with different contents.
+  // TODO(crbug.com/1296102): Link to documentation that describes the criteria
+  // where module imports are handled by service worker fetch handler.
+  resource_request.SetSkipServiceWorker(request_world.IsIsolatedWorld());
+
   // <spec step="9">Set request 's destination to the result of running the
   // fetch destination from module type steps given destination and
   // moduleType.</spec>
   SetFetchDestinationFromModuleType(resource_request, module_request);
 
-  ResourceLoaderOptions options(&modulator_->GetScriptState()->World());
+  ResourceLoaderOptions options(&request_world);
 
   // <spec step="11">Set request's initiator type to "script".</spec>
   options.initiator_info.name = fetch_initiator_type_names::kScript;
