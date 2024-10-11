897480e33b029759869c87dcd4e2e48e87124e5e
Theo Cristea <theocristea@google.com>
2024-10-11 09:23:30 +0000

    [Gardening] Disable failing AddressFieldParserTest on Android
    Bug: 372842861
    Change-Id: I5486e9e007eb948121c76d25798db8cc4d7c013f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5923984
    Owners-Override: Theo Cristea <theocristea@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367376}

--- components/autofill/core/browser/form_parsing/address_field_parser_unittest.cc
+++ components/autofill/core/browser/form_parsing/address_field_parser_unittest.cc
@@ -112,7 +112,15 @@ TEST_F(AddressFieldParserTest, ParseStreetNameAndHouseNumbertPL) {
 // Tests that fields are classified as |ADDRESS_HOME_STREET_NAME|,
 // |ADDRESS_HOME_HOUSE_NUMBER| and |ADDRESS_HOME_APT_NUM|  when they are labeled
 // accordingly and both are present.
-TEST_F(AddressFieldParserTest, ParseStreetNameHouseNumbertAndAptNumPL) {
+// TODO(crbug.com/372842861): Re-enable test.
+#if BUILDFLAG(IS_ANDROID)
+#define MAYBE_ParseStreetNameHouseNumbertAndAptNumPL \
+  DISABLED_ParseStreetNameHouseNumbertAndAptNumPL
+#else
+#define MAYBE_ParseStreetNameHouseNumbertAndAptNumPL \
+  ParseStreetNameHouseNumbertAndAptNumPL
+#endif
+TEST_F(AddressFieldParserTest, MAYBE_ParseStreetNameHouseNumbertAndAptNumPL) {
   AddTextFormFieldData("street", "ulica", ADDRESS_HOME_STREET_NAME);
   AddTextFormFieldData("house-number", "Nr domu", ADDRESS_HOME_HOUSE_NUMBER);
   AddTextFormFieldData("house-number", "Nr lokalu", ADDRESS_HOME_APT_NUM);
