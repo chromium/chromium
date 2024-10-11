95be79c5e7f531cc44df2274341745f8cca83da0
Yang Wang <yangwangyw@google.com>
2024-10-11 17:38:57 +0000

    Add code coverage for cct handler.
    Bug: 372874694
    Change-Id: I93a39ba9c5b36a2c8f7291c6fe4097dfbf6f0eb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924150
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Yang Wang <yangwangyw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367605}

--- chrome/android/junit/src/org/chromium/chrome/browser/privacy_sandbox/CctHandlerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/privacy_sandbox/CctHandlerTest.java
@@ -6,40 +6,79 @@ package org.chromium.chrome.browser.privacy_sandbox;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.doReturn;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.Browser;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.robolectric.annotation.Config;
 
+import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class CctHandlerTest {
+    @Rule public MockitoRule rule = MockitoJUnit.rule();
+
+    @Mock private Context mContext;
+
+    private CctHandler mCctHandler;
+
+    private static final String TEST_URL = "https://www.example.com";
+
+    @Before
+    public void setup() {
+        mCctHandler = new CctHandler(mContext);
+    }
+
     @Test
-    public void testOpenUrlInCct() {
-        Context context = mock(Context.class);
+    public void testPrepareIntentSuccess() {
+        Intent expectedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(TEST_URL));
+        expectedIntent.setPackage(mContext.getPackageName());
+        expectedIntent.putExtra(Browser.EXTRA_APPLICATION_ID, mContext.getPackageName());
 
-        String testUrl = "https://www.example.com";
-        Intent expectedIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(testUrl));
-        expectedIntent.setPackage(context.getPackageName());
-        expectedIntent.putExtra(Browser.EXTRA_APPLICATION_ID, context.getPackageName());
+        Intent actualIntent = mCctHandler.prepareIntent(TEST_URL).getIntent();
 
-        // 2. Call the Method
-        Intent actualIntent = new CctHandler(context).prepareIntent(testUrl).getIntent();
-
-        // 3. Assertions
         assertNotNull(actualIntent);
         assertEquals(Intent.ACTION_VIEW, actualIntent.getAction());
-        assertEquals(Uri.parse(testUrl), actualIntent.getData());
-        assertEquals(context.getPackageName(), actualIntent.getPackage());
+        assertEquals(Uri.parse(TEST_URL), actualIntent.getData());
+        assertEquals(mContext.getPackageName(), actualIntent.getPackage());
         assertTrue(actualIntent.hasExtra(Browser.EXTRA_APPLICATION_ID));
+    }
+
+    @Test
+    public void testOpenUrlInCctFailure_nullIntent() {
+        assertThrows(AssertionError.class, () -> mCctHandler.openUrlInCct());
+    }
+
+    @Test
+    public void testOpenUrlInCctFailure_untrustedPackage() {
+        doReturn("untrusted_package").when(mContext).getPackageName();
+
+        assertThrows(
+                AssertionError.class, () -> mCctHandler.prepareIntent(TEST_URL).openUrlInCct());
+    }
+
+    @Test
+    public void testOpenUrlInCctSuccess() {
+        doReturn(ContextUtils.getApplicationContext().getPackageName())
+                .when(mContext)
+                .getPackageName();
+
+        CctHandler testCctHandler = mCctHandler.prepareIntent(TEST_URL).openUrlInCct();
+
+        assertNotNull(testCctHandler);
     }
 }
