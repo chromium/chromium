c9fec160cec3b3fde6305ba8bf1079bd3ded3b2d
Salvador Guerrero <salg@google.com>
2024-10-11 20:03:37 +0000

    Split Trex class from offline dino game
    Change-Id: Id978628e5c9f63f38eab974bc3da35fef41190c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914148
    Auto-Submit: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Edward Jung <edwardjung@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367718}

--- components/neterror/resources/dino_game/utils.js
+++ components/neterror/resources/dino_game/utils.js
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {IS_IOS} from './constants.js';
 
 /**
  * Get random number.
@@ -10,4 +11,12 @@
  */
 export function getRandomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Return the current timestamp.
+ * @return {number}
+ */
+export function getTimeStamp() {
+  return IS_IOS ? new Date().getTime() : performance.now();
 }
