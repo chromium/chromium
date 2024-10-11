a0e227fcbfa4d16aedd4534fbe8d78f7a4da24d4
Renato Silva <rrsilva@google.com>
2024-10-11 22:31:53 +0000

    [High5] Fix CryptohomeRecoverySetupScreenTest
    CryptohomeRecoverySetupScreenTest was previously doing too many hacks in
    order to properly show the CryptohomeRecoverySetupScreen. It would go
    through the whole OOBE flow once and force it to stop after the last
    screen so that it could show the CryptohomeRecoverySetupScreen. Thus the
    screen was actually being shown twice. With this change, the OOBE flow
    will be stopped exactly where this screen needs to be tested, without
    needing extra steps such as copying the original UserContext. The fact
    that the screen was being shown twice was causing some new logic in
    WizardController to trigger a CHECK in a codepath for the new PIN-only
    OOBE that is being developed.
    
    Bug: b/365059362, b/372853725, b/348326316
    Change-Id: I3b5eebbe8778aa4bced242c35442bc27818a51a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5923501
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>
    Auto-Submit: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367827}

--- chrome/browser/ash/login/screens/osauth/cryptohome_recovery_setup_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/osauth/cryptohome_recovery_setup_screen_browsertest.cc
@@ -13,8 +13,10 @@
 #include "base/run_loop.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_utils.h"
+#include "chrome/browser/ash/login/screens/sync_consent_screen.h"
 #include "chrome/browser/ash/login/test/cryptohome_mixin.h"
 #include "chrome/browser/ash/login/test/login_manager_mixin.h"
 #include "chrome/browser/ash/login/test/oobe_base_test.h"
@@ -24,6 +26,7 @@
 #include "chrome/browser/ash/login/wizard_controller.h"
 #include "chrome/browser/ui/ash/login/login_display_host.h"
 #include "chrome/browser/ui/webui/ash/login/cryptohome_recovery_setup_screen_handler.h"
+#include "chrome/browser/ui/webui/ash/login/sync_consent_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/user_creation_screen_handler.h"
 #include "chromeos/ash/components/cryptohome/constants.h"
 #include "chromeos/ash/components/osauth/public/auth_session_storage.h"
@@ -44,54 +47,62 @@ class CryptohomeRecoverySetupScreenTest : public OobeBaseTest {
         &CryptohomeRecoverySetupScreenTest::HandleScreenExit,
         base::Unretained(this)));
 
+    // SyncConsentScreen is immediately before CryptohomeRecoverySetupScreen. We
+    // use this callback to carefully stop the OOBE flow just before it so that
+    // we can prepare the opt-in value.
+    sync_consent_callback_ =
+        GetSyncConsentScreen()->get_exit_callback_for_testing();
+    GetSyncConsentScreen()->set_exit_callback_for_testing(
+        sync_consent_result_waiter_.GetRepeatingCallback());
+
     OobeBaseTest::SetUpOnMainThread();
   }
 
-  void TearDownOnMainThread() override {
-    OobeBaseTest::TearDownOnMainThread();
-    result_ = std::nullopt;
+  void SimulateRecoveryFactorOptInValue(bool opted_in) {
+    LoginDisplayHost::default_host()
+        ->GetWizardContextForTesting()
+        ->recovery_setup.ask_about_recovery_consent = true;
+    LoginDisplayHost::default_host()
+        ->GetWizardContextForTesting()
+        ->recovery_setup.recovery_factor_opted_in = opted_in;
   }
 
-  void LoginAsRegularUser() {
-    // Login, and skip the post login screens.
+  void LoginAndWaitForSyncConsentScreenExit() {
+    // Login, and skip the post login screens until the SyncConsentScreen.
     auto* context =
         LoginDisplayHost::default_host()->GetWizardContextForTesting();
     context->skip_post_login_screens_for_tests = true;
-    context->defer_oobe_flow_finished_for_tests = true;
     login_manager_mixin_.LoginAsNewRegularUser();
-    WizardControllerExitWaiter(UserCreationView::kScreenId).Wait();
-    // Wait for the recovery screen and copy the user context before it is
-    // cleared.
-    WaitForScreenExit();
-    std::unique_ptr<UserContext> user_context =
-        ash::AuthSessionStorage::Get()->BorrowForTests(
-            FROM_HERE, context->extra_factors_token.value());
-    context->extra_factors_token = std::nullopt;
-    cryptohome_.MarkUserAsExisting(user_context->GetAccountId());
-    ContinueScreenExit();
-    // Wait until the OOBE flow finishes before we set new values on the wizard
-    // context.
-    OobeScreenExitWaiter(UserCreationView::kScreenId).Wait();
 
-    // Set the values on the wizard context: the `extra_factors_token`
-    // is available after the previous screens have run regularly, and it holds
-    // an authenticated auth session.
-    user_context->ResetAuthSessionIds();
-    auto session_ids = cryptohome_.AddSession(user_context->GetAccountId(),
-                                              /*authenticated=*/true);
-    user_context->SetAuthSessionIds(session_ids.first, session_ids.second);
-    user_context->SetSessionLifetime(base::Time::Now() +
-                                     cryptohome::kAuthsessionInitialLifetime);
-    context->extra_factors_token =
-        ash::AuthSessionStorage::Get()->Store(std::move(user_context));
+    // Wait for the SyncConsentScreen to exit and set the system to not skip any
+    // more screens. `CryptohomeRecoverySetup` will be shown next.
+    ASSERT_TRUE(sync_consent_result_waiter_.Wait());
     context->skip_post_login_screens_for_tests = false;
-    // Clear the test state.
-    result_ = std::nullopt;
+  }
+
+  void ProceedAndWaitForScreenToExit() {
+    // Trigger the continuation of the SyncConsentScreen which will lead to the
+    // CryptohomeRecoverySetupScreen and wait for it to exit.
+    sync_consent_callback_.Run(sync_consent_result_waiter_.Take());
+    WaitForScreenExit();
+  }
+
+  void ExpectScreenExitMetrics(bool opted_in) {
+    histogram_tester_.ExpectTotalCount(
+        "OOBE.StepCompletionTimeByExitReason.Cryptohome-recovery-setup.Done",
+        opted_in ? 1 : 0);
+    histogram_tester_.ExpectTotalCount(
+        "OOBE.StepCompletionTime.Cryptohome-recovery-setup", opted_in ? 1 : 0);
   }
 
   CryptohomeRecoverySetupScreen* GetScreen() {
     return WizardController::default_controller()
         ->GetScreen<CryptohomeRecoverySetupScreen>();
+  }
+
+  SyncConsentScreen* GetSyncConsentScreen() {
+    return WizardController::default_controller()
+        ->GetScreen<SyncConsentScreen>();
   }
 
   void WaitForScreenExit() {
@@ -103,18 +114,6 @@ class CryptohomeRecoverySetupScreenTest : public OobeBaseTest {
     run_loop.Run();
   }
 
-  void ContinueScreenExit() {
-    original_callback_.Run(result_.value());
-    if (screen_exit_callback_)
-      std::move(screen_exit_callback_).Run();
-  }
-
-  void ShowScreen() {
-    LoginDisplayHost::default_host()->StartWizard(
-        CryptohomeRecoverySetupScreenView::kScreenId);
-    WaitForScreenExit();
-  }
-
   LoginManagerMixin login_manager_mixin_{&mixin_host_};
   CryptohomeMixin cryptohome_{&mixin_host_};
   std::optional<CryptohomeRecoverySetupScreen::Result> result_;
@@ -122,61 +121,50 @@ class CryptohomeRecoverySetupScreenTest : public OobeBaseTest {
  private:
   void HandleScreenExit(CryptohomeRecoverySetupScreen::Result result) {
     result_ = result;
+    original_callback_.Run(result);
     if (screen_exit_callback_)
       std::move(screen_exit_callback_).Run();
   }
 
   base::test::ScopedFeatureList feature_list_;
+  base::HistogramTester histogram_tester_;
+
   CryptohomeRecoverySetupScreen::ScreenExitCallback original_callback_;
   base::RepeatingClosure screen_exit_callback_;
+
+  SyncConsentScreen::ScreenExitCallback sync_consent_callback_;
+  base::test::TestFuture<SyncConsentScreen::Result> sync_consent_result_waiter_;
 };
 
 // If user opts out from recovery, the screen should be skipped.
 IN_PROC_BROWSER_TEST_F(CryptohomeRecoverySetupScreenTest, SkippedOnOptOut) {
-  LoginAsRegularUser();
-  LoginDisplayHost::default_host()
-      ->GetWizardContextForTesting()
-      ->recovery_setup.ask_about_recovery_consent = true;
-  LoginDisplayHost::default_host()
-      ->GetWizardContextForTesting()
-      ->recovery_setup.recovery_factor_opted_in = false;
-  base::HistogramTester histogram_tester;
+  LoginAndWaitForSyncConsentScreenExit();
 
-  ShowScreen();
+  SimulateRecoveryFactorOptInValue(/*opted_in=*/false);
 
-  ContinueScreenExit();
+  ProceedAndWaitForScreenToExit();
+
   EXPECT_EQ(result_.value(),
             CryptohomeRecoverySetupScreen::Result::NOT_APPLICABLE);
-  histogram_tester.ExpectTotalCount(
-      "OOBE.StepCompletionTimeByExitReason.Cryptohome-recovery-setup.Done", 0);
-  histogram_tester.ExpectTotalCount(
-      "OOBE.StepCompletionTime.Cryptohome-recovery-setup", 0);
+  ExpectScreenExitMetrics(/*opted_in=*/false);
 }
 
 // If user opts in to recovery, the screen should be shown. In this case
 // auth session should not be cleared.
 IN_PROC_BROWSER_TEST_F(CryptohomeRecoverySetupScreenTest,
                        ShowDoesntClearAuthSession) {
-  LoginAsRegularUser();
-  LoginDisplayHost::default_host()
-      ->GetWizardContextForTesting()
-      ->recovery_setup.ask_about_recovery_consent = true;
-  LoginDisplayHost::default_host()
-      ->GetWizardContextForTesting()
-      ->recovery_setup.recovery_factor_opted_in = true;
-  base::HistogramTester histogram_tester;
+  LoginAndWaitForSyncConsentScreenExit();
 
-  ShowScreen();
+  SimulateRecoveryFactorOptInValue(/*opted_in=*/true);
+
+  ProceedAndWaitForScreenToExit();
+
   EXPECT_TRUE(LoginDisplayHost::default_host()
                   ->GetWizardContextForTesting()
                   ->extra_factors_token.has_value());
 
-  ContinueScreenExit();
   EXPECT_EQ(result_.value(), CryptohomeRecoverySetupScreen::Result::DONE);
-  histogram_tester.ExpectTotalCount(
-      "OOBE.StepCompletionTimeByExitReason.Cryptohome-recovery-setup.Done", 1);
-  histogram_tester.ExpectTotalCount(
-      "OOBE.StepCompletionTime.Cryptohome-recovery-setup", 1);
+  ExpectScreenExitMetrics(/*opted_in=*/true);
 }
 
 }  // namespace ash
