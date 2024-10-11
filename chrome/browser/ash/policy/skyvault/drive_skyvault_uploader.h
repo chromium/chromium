58f3c2f6a0433f679f3d940e0850457f2f36d742
Steven Luong <stluong@chromium.org>
2024-10-11 17:35:19 +0000

    Revert "[skyvault] Add DriveSkyvaultUploader::Cancel() method"
    This reverts commit 97107ab42ef9e601505efbd29ff6954d69593960.
    
    Reason for revert: Use of initialized value and fails on Linux ChromiumOS MSAN bot.
    
    Original change's description:
    > [skyvault] Add DriveSkyvaultUploader::Cancel() method
    >
    > Adds an option to cancel an upload to Google Drive.
    >
    > R=poromov@chromium.org
    >
    > Bug: 349097807
    > Change-Id: I15e49864aaa0ba60e02e0cc5d0032ffed91d24b9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920193
    > Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    > Reviewed-by: Sergey Poromov <poromov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1367525}
    
    Bug: 349097807
    Change-Id: Ieb84be8b471fe516dd66fcc811d92c84ac93224b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924846
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Owners-Override: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367603}

--- chrome/browser/ash/policy/skyvault/drive_skyvault_uploader.h
+++ chrome/browser/ash/policy/skyvault/drive_skyvault_uploader.h
@@ -54,9 +54,6 @@ class DriveSkyvaultUploader
   // Initiated by the `Upload` static method.
   void Run();
 
-  // Cancels the upload, if possible.
-  void Cancel();
-
   DriveSkyvaultUploader(const DriveSkyvaultUploader&) = delete;
   DriveSkyvaultUploader& operator=(const DriveSkyvaultUploader&) = delete;
 
@@ -127,10 +124,6 @@ class DriveSkyvaultUploader
 
   // Whether `EndCopy()` was called.
   bool copy_ended_ = false;
-
-  // Set to `true` if upload is explicitly cancelled by owner. Forces every step
-  // to exit early.
-  bool cancelled_ = false;
 
   // Stores the first encountered error, if any.
   std::optional<MigrationUploadError> error_;
