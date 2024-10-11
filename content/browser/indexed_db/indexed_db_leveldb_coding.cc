008798e9994f28152982e6f7f87d0d7459f63c85
Evan Stade <estade@chromium.org>
2024-10-11 00:11:07 +0000

    IDB: adjust error handling in DecodeIDBKeyPath().
    Instead of DCHECK/NOTREACHED, just return false when the input doesn't
    parse in an expected format.
    
    This data is not trusted since it's coming from disk (or in this case,
    from a fuzzer PRNG).
    
    Bug: 371248259
    Change-Id: Ib36be2b43ab0f183dce90cfca988f95a8be68fb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5909682
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367217}

--- content/browser/indexed_db/indexed_db_leveldb_coding.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding.cc
@@ -769,14 +769,16 @@ bool DecodeIDBKeyPath(std::string_view* slice, IndexedDBKeyPath* value) {
 
   switch (type) {
     case blink::mojom::IDBKeyPathType::Null:
-      DCHECK(slice->empty());
+      if (!slice->empty()) {
+        return false;
+      }
       *value = IndexedDBKeyPath();
       return true;
     case blink::mojom::IDBKeyPathType::String: {
       std::u16string string;
-      if (!DecodeStringWithLength(slice, &string))
+      if (!DecodeStringWithLength(slice, &string) || !slice->empty()) {
         return false;
-      DCHECK(slice->empty());
+      }
       *value = IndexedDBKeyPath(string);
       return true;
     }
@@ -791,12 +793,14 @@ bool DecodeIDBKeyPath(std::string_view* slice, IndexedDBKeyPath* value) {
           return false;
         array.push_back(string);
       }
-      DCHECK(slice->empty());
+      if (!slice->empty()) {
+        return false;
+      }
       *value = IndexedDBKeyPath(array);
       return true;
     }
   }
-  NOTREACHED_IN_MIGRATION();
+
   return false;
 }
 
