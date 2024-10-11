4b2de6933ae10595832fd6415fbf96fed6a9ae89
Dorian Brandon <dorianbrandon@google.com>
2024-10-11 19:18:46 +0000

    Extract Live translate call to translation service into its own class.
    To prepare for Boca to be a future consumer of the Live Translate code, this CL extracts the actual call the the Translation API into its own file. Boca and Live Translate will be able to provide the needed API key as part of initialization of the class.
    
    Bug: 359931233
    Change-Id: I01cb2f50901cdde75b7c6aeff7269539ea8052cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925057
    Commit-Queue: Dorian Brandon <dorianbrandon@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Aiden Chiavatti <chiav@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367685}

--- components/live_caption/live_translate_controller.h
+++ components/live_caption/live_translate_controller.h
@@ -9,13 +9,10 @@
 #include <string>
 
 #include "base/memory/raw_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/soda/constants.h"
+#include "components/live_caption/translation_dispatcher.h"
 #include "media/mojo/mojom/speech_recognition_result.h"
 #include "mojo/public/cpp/bindings/remote.h"
-#include "services/data_decoder/public/cpp/data_decoder.h"
-#include "services/network/public/mojom/url_loader_factory.mojom.h"
 
 class PrefChangeRegistrar;
 class PrefService;
@@ -27,10 +24,6 @@ class BrowserContext;
 namespace user_prefs {
 class PrefRegistrySyncable;
 }  // namespace user_prefs
-
-namespace network {
-class SimpleURLLoader;
-}  // namespace network
 
 namespace captions {
 
@@ -59,28 +52,10 @@ class LiveTranslateController : public KeyedService {
                               OnTranslateEventCallback callback);
 
  private:
-  void ResetURLLoaderFactory();
-  void OnURLLoadComplete(OnTranslateEventCallback callback,
-                         std::unique_ptr<std::string> response_body);
-
-  // Called when the data decoder service provides parsed JSON data for a server
-  // response.
-  void OnResponseJsonParsed(OnTranslateEventCallback callback,
-                            data_decoder::DataDecoder::ValueOrError result);
-
   void OnLiveTranslateEnabledChanged();
-
-  raw_ptr<content::BrowserContext> browser_context_;
   raw_ptr<PrefService> profile_prefs_;
-
-  mojo::Remote<network::mojom::URLLoaderFactory> url_loader_factory_;
-  std::unique_ptr<network::SimpleURLLoader> url_loader_;
-
-  data_decoder::DataDecoder data_decoder_;
-
   const std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
-
-  base::WeakPtrFactory<LiveTranslateController> weak_factory_{this};
+  const std::unique_ptr<TranslationDispatcher> translation_dispatcher_;
 };
 
 }  // namespace captions
