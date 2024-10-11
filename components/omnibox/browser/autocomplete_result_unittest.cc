e8d5114b003d821292bba402233dd4ef3f07dd49
Tomasz Wiszkowski <ender@google.com>
2024-10-11 16:47:13 +0000

    Revert "[Gardening] Disable flaky AutocompleteResultTest"
    This reverts commit 816141d4d114c2980ff384a4e5509677d8737be0.
    
    Reason for revert: Invalid culprit. It looks like death tests are broken in general and related instrumentation should be fixed.
    
    This CL isn't flaky. Infrastructure is broken. http://b/372886573
    
    Original change's description:
    > [Gardening] Disable flaky AutocompleteResultTest
    >
    > Bug: 372886010
    > Change-Id: Iec1ae802fe3b1ce54ccaa0629610ba87525b10d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5923991
    > Owners-Override: Theo Cristea <theocristea@google.com>
    > Auto-Submit: Theo Cristea <theocristea@google.com>
    > Commit-Queue: Theo Cristea <theocristea@google.com>
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Commit-Queue: Jan Keitel <jkeitel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1367542}
    
    Bug: 372886010
    Change-Id: Ie642ec93f4cbf601a54bf9e253f824e9556fd35b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925083
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1367571}

--- components/omnibox/browser/autocomplete_result_unittest.cc
+++ components/omnibox/browser/autocomplete_result_unittest.cc
@@ -1421,16 +1421,7 @@ TEST_F(AutocompleteResultTest, SortAndCullReorderForDefaultMatch) {
 
 // Note: DCHECKs not firing on Cast.
 #if DCHECK_IS_ON()
-// TODO(crbug.com/372886010): Re-enable test.
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_SortAndCullFailsWithIncorrectDefaultScheme \
-  DISABLED_SortAndCullFailsWithIncorrectDefaultScheme
-#else
-#define MAYBE_SortAndCullFailsWithIncorrectDefaultScheme \
-  SortAndCullFailsWithIncorrectDefaultScheme
-#endif
-TEST_F(AutocompleteResultTest,
-       MAYBE_SortAndCullFailsWithIncorrectDefaultScheme) {
+TEST_F(AutocompleteResultTest, SortAndCullFailsWithIncorrectDefaultScheme) {
   // Scenario:
   // - User navigates to a webpage whose URL looks like a scheme,
   //   e.g. "chrome:123" (note the colon).
