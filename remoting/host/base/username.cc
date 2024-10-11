c77cf46e632c3faf6158650e536be966b07169f0
Lambros Lambrou <lambroslambrou@chromium.org>
2024-10-11 21:31:32 +0000

    [remoting] Add extra logging around GetUsername().
    Some users have reported occasional failures of the getpwuid_r() system
    call. This updates GetUsername(), used by the PAM local-login check, to
    log more information for any failed system calls.
    
    Bug: 371981596
    Change-Id: I76eb1adb4ec2464580e09244b2dd72e59824ea3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926509
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367787}

--- remoting/host/base/username.cc
+++ remoting/host/base/username.cc
@@ -6,11 +6,13 @@
 
 #include <vector>
 
+#include "base/logging.h"
 #include "base/notreached.h"
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_POSIX)
 #include <pwd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
 #endif  // BUILDFLAG(IS_POSIX)
@@ -21,14 +23,24 @@ std::string GetUsername() {
 #if BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_ANDROID)
   long buf_size = sysconf(_SC_GETPW_R_SIZE_MAX);
   if (buf_size <= 0) {
+    PLOG(ERROR) << "sysconf(_SC_GETPW_R_SIZE_MAX) failed";
     return std::string();
   }
 
   std::vector<char> buf(buf_size);
   struct passwd passwd;
   struct passwd* passwd_result = nullptr;
-  getpwuid_r(getuid(), &passwd, &(buf[0]), buf_size, &passwd_result);
-  return passwd_result ? passwd_result->pw_name : std::string();
+  uid_t user_id = getuid();
+  getpwuid_r(user_id, &passwd, &(buf[0]), buf_size, &passwd_result);
+  if (!passwd_result) {
+    PLOG(ERROR) << "getpwuid_r() failed for user " << user_id;
+    return std::string();
+  }
+  std::string result(passwd_result->pw_name);
+  if (result.empty()) {
+    LOG(ERROR) << "getpwuid_r() returned empty pw_name for user " << user_id;
+  }
+  return result;
 #else
   NOTIMPLEMENTED();
   return std::string();
