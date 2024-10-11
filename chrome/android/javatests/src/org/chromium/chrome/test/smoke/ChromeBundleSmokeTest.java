ebe7b7215a5f6a367f9e386c6a27ea53424dfa2d
Theo Cristea <theocristea@google.com>
2024-10-11 08:49:51 +0000

    [Gardening] Disable failing ChromeBundleSmokeTest
    Bug: 372840602
    Change-Id: I382328b37688c23eef069ecf3c64fc7c0573c315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922708
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367361}

--- chrome/android/javatests/src/org/chromium/chrome/test/smoke/ChromeBundleSmokeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/test/smoke/ChromeBundleSmokeTest.java
@@ -21,6 +21,7 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.test.pagecontroller.rules.ChromeUiApplicationTestRule;
 import org.chromium.chrome.test.pagecontroller.rules.ChromeUiAutomatorTestRule;
 import org.chromium.chrome.test.pagecontroller.utils.IUi2Locator;
@@ -32,6 +33,7 @@ import org.chromium.chrome.test.pagecontroller.utils.UiLocatorHelper;
 /** Smoke Test for Chrome bundles. */
 @LargeTest
 @RunWith(BaseJUnit4ClassRunner.class)
+@DisabledTest(message = "crbug.com/372840602")
 public class ChromeBundleSmokeTest {
     private static final String TARGET_ACTIVITY =
             "org.chromium.chrome.browser.test_dummy.TestDummyActivity";
