35e7dc290c0ad16db790d5d481e9504d510b01e7
Etienne Dechamps <edechamps@google.com>
2024-10-11 12:05:27 +0000

    Fix Cronet tests on Android V / 15
    The Chromium Android V / 15 bots bundle an HttpEngine that is newer
    than the U / 14 bots. Account for the differences in the tests.
    
    Bug: b:371973616
    Change-Id: I304d82cb5c571a389ff09bd6016c2009767704f9
    Cq-Include-Trybots: luci.chromium.try:android-cronet-x64-dbg-15-tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921700
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367444}

--- components/cronet/android/test/javatests/src/org/chromium/net/BidirectionalStreamTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/BidirectionalStreamTest.java
@@ -922,10 +922,13 @@ public class BidirectionalStreamTest {
         builder.addHeader("goodheader2", "headervalue");
         IllegalArgumentException e =
                 assertThrows(IllegalArgumentException.class, () -> builder.build().start());
+        var oldMessage = "Invalid header header:name=headervalue";
+        var newMessage = "Invalid header with headername: header:name";
         if (mTestRule.implementationUnderTest() == CronetImplementation.AOSP_PLATFORM
                 && !mTestRule.isRunningInAOSP()) {
-            // TODO(b/307234565): Remove check once chromium Android 14 emulator has latest changes.
-            assertThat(e).hasMessageThat().isEqualTo("Invalid header header:name=headervalue");
+            // We may be running against an HttpEngine backed by an old version of Cronet, so accept
+            // both the old and new variants of the message.
+            assertThat(e).hasMessageThat().isAnyOf(oldMessage, newMessage);
         } else {
             assertThat(e).hasMessageThat().isEqualTo("Invalid header with headername: header:name");
         }
@@ -941,14 +944,15 @@ public class BidirectionalStreamTest {
         builder.addHeader("headername", "bad header\r\nvalue");
         IllegalArgumentException e =
                 assertThrows(IllegalArgumentException.class, () -> builder.build().start());
+        var oldMessage = "Invalid header headername=bad header\r\nvalue";
+        var newMessage = "Invalid header with headername: headername";
         if (mTestRule.implementationUnderTest() == CronetImplementation.AOSP_PLATFORM
                 && !mTestRule.isRunningInAOSP()) {
-            // TODO(b/307234565): Remove check once chromium Android 14 emulator has latest changes.
-            assertThat(e)
-                    .hasMessageThat()
-                    .isEqualTo("Invalid header headername=bad header\r\nvalue");
+            // We may be running against an HttpEngine backed by an old version of Cronet, so accept
+            // both the old and new variants of the message.
+            assertThat(e).hasMessageThat().isAnyOf(oldMessage, newMessage);
         } else {
-            assertThat(e).hasMessageThat().isEqualTo("Invalid header with headername: headername");
+            assertThat(e).hasMessageThat().isEqualTo(newMessage);
         }
     }
 
