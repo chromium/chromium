6117c06f2256e906d06b89d5448de8a04bdc8817
Yuki Awano <yawano@google.com>
2024-10-12 00:13:48 +0000

    quick-answers: add corner radius
    Bug: b:372476349
    Test: manually tested
    Change-Id: I88038d0d75951bb6514f1b264bc6c69a7acec4a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922470
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367867}

--- chrome/browser/ui/chromeos/read_write_cards/read_write_cards_view.cc
+++ chrome/browser/ui/chromeos/read_write_cards/read_write_cards_view.cc
@@ -7,7 +7,10 @@
 #include "base/memory/raw_ref.h"
 #include "chrome/browser/ui/chromeos/read_write_cards/read_write_cards_ui_controller.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/compositor/layer.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/geometry/rounded_corners_f.h"
+#include "ui/views/layout/layout_provider.h"
 #include "ui/views/view.h"
 #include "ui/views/view_shadow.h"
 
@@ -18,6 +21,17 @@ ReadWriteCardsView::ReadWriteCardsView(
     : view_shadow_(std::make_unique<views::ViewShadow>(this, /*elevation=*/2)),
       read_write_cards_ui_controller_(read_write_cards_ui_controller) {
   context_menu_bounds_ = read_write_cards_ui_controller_->context_menu_bounds();
+
+  view_shadow_->SetRoundedCornerRadius(
+      GetLayoutProvider()->GetCornerRadiusMetric(
+          views::ShapeContextTokens::kMenuRadius));
+
+  CHECK(layer()) << "A layer should be created by the constructor of "
+                    "ViewShadow with SetPaintToLayer()";
+  layer()->SetRoundedCornerRadius(
+      gfx::RoundedCornersF(views::LayoutProvider::Get()->GetCornerRadiusMetric(
+          views::ShapeContextTokens::kMenuRadius)));
+  layer()->SetIsFastRoundedCorner(true);
 }
 
 ReadWriteCardsView::~ReadWriteCardsView() = default;
