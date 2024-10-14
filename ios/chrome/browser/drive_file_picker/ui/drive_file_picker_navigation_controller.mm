10bfd5fcf7f7ba0aa75fdcbaf0ed0de8a7ab1587
Quentin Pubert <qpubert@google.com>
2024-10-14 15:15:10 +0000

    [iOS] Update Drive file picker safe area insets so toolbar stays visible
    This CL adds keyboard frame change observation in the Drive file picker
    so that a change in keyboard frame leads to an update in the view's
    safe area insets so it remains unobscured.
    
    Bug: 371206301
    Change-Id: I8a92c915801373e9ab210ab0205388cad4fbb816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925359
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368258}

--- ios/chrome/browser/drive_file_picker/ui/drive_file_picker_navigation_controller.mm
+++ ios/chrome/browser/drive_file_picker/ui/drive_file_picker_navigation_controller.mm
@@ -4,18 +4,72 @@
 
 #import "ios/chrome/browser/drive_file_picker/ui/drive_file_picker_navigation_controller.h"
 
-#import "base/apple/foundation_util.h"
+#import "base/task/sequenced_task_runner.h"
 #import "ios/chrome/browser/drive_file_picker/ui/drive_file_picker_constants.h"
-#import "ios/chrome/common/ui/util/constraints_ui_util.h"
-#import "ios/chrome/grit/ios_strings.h"
-#import "ui/base/l10n/l10n_util_mac.h"
 
 @implementation DriveFilePickerNavigationController
 
 #pragma mark - UIViewController
 
 - (void)viewDidLoad {
+  [super viewDidLoad];
   self.view.accessibilityIdentifier = kDriveFilePickerAccessibilityIdentifier;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+  [super viewDidAppear:animated];
+  // Observe keyboard frame changes to update safe area insets accordingly.
+  [[NSNotificationCenter defaultCenter]
+      addObserver:self
+         selector:@selector(keyboardWillChangeFrame:)
+             name:UIKeyboardWillChangeFrameNotification
+           object:nil];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+  [super viewDidDisappear:animated];
+  [[NSNotificationCenter defaultCenter]
+      removeObserver:self
+                name:UIKeyboardWillChangeFrameNotification
+              object:nil];
+}
+
+#pragma mark - Private
+
+// Called when a `UIKeyboardWillChangeFrameNotification` is received.
+- (void)keyboardWillChangeFrame:(NSNotification*)notification {
+  // Update the additional safe area inset asynchronously to ensure the view has
+  // already moved to its new position, as it might move in reaction to the
+  // change of keyboard frame.
+  __weak __typeof(self) weakSelf = self;
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE,
+      base::BindOnce(
+          [](DriveFilePickerNavigationController* navigationController,
+             const CGRect& keyboardFrameInWindow) {
+            [navigationController
+                ensureViewIsUnobscuredByKeyboardFrame:keyboardFrameInWindow];
+          },
+          weakSelf,
+          [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue]));
+}
+
+// Update the additional safe area insets to ensure the view is unobscured.
+- (void)ensureViewIsUnobscuredByKeyboardFrame:
+    (const CGRect&)keyboardFrameInWindow {
+  CGRect safeAreaLayoutFrameInWindow =
+      [self.view convertRect:self.view.safeAreaLayoutGuide.layoutFrame
+                      toView:self.view.window];
+  // Adjust the additional safe area bottom inset so the bottom of the safe
+  // area's frame matches the top of the keyboard's frame.
+  UIEdgeInsets additionalSafeAreaInsets = self.additionalSafeAreaInsets;
+  additionalSafeAreaInsets.bottom +=
+      CGRectGetMaxY(safeAreaLayoutFrameInWindow) -
+      CGRectGetMinY(keyboardFrameInWindow);
+  // Ensure the additional inset is always positive.
+  additionalSafeAreaInsets.bottom =
+      std::max(0.0, additionalSafeAreaInsets.bottom);
+  self.additionalSafeAreaInsets = additionalSafeAreaInsets;
 }
 
 @end
