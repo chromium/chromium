772e6e1e0667f094214c3ac09eab05ac2608e52a
Nate Chapin <japhet@chromium.org>
2024-10-14 23:50:47 +0000

    Remove a usage of ExceptionState::GetException in miscellaneeous_operations_test.cc.
    I missed this in https://chromium-review.googlesource.com/c/chromium/src/+/5901093,
    it's the last usage of ExceptionState::GetException outside of the bindings.
    GetException() will be removed in https://chromium-review.googlesource.com/c/chromium/src/+/5882467
    
    Change-Id: Ie3df8e55a8b3564a152d2f44a1706cd156f5af06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922465
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368512}

--- third_party/blink/renderer/core/streams/miscellaneous_operations_test.cc
+++ third_party/blink/renderer/core/streams/miscellaneous_operations_test.cc
@@ -327,14 +327,13 @@ TEST(MiscellaneousOperationsTest, CallOrNoop1ThrowingMethod) {
   ASSERT_TRUE(underlying_value.IsObject());
   auto underlying_object = underlying_value.V8Value().As<v8::Object>();
   v8::Local<v8::Value> arg0 = v8::Number::New(scope.GetIsolate(), 17);
-  ExceptionState exception_state(scope.GetIsolate(),
-                                 v8::ExceptionContext::kUnknown, "", "");
-  auto maybe_result =
-      CallOrNoop1(scope.GetScriptState(), underlying_object, "transform",
-                  "transformer.transform", arg0, exception_state);
-  ASSERT_TRUE(exception_state.HadException());
+  v8::TryCatch try_catch(scope.GetIsolate());
+  auto maybe_result = CallOrNoop1(scope.GetScriptState(), underlying_object,
+                                  "transform", "transformer.transform", arg0,
+                                  PassThroughException(scope.GetIsolate()));
+  ASSERT_TRUE(try_catch.HasCaught());
   EXPECT_TRUE(maybe_result.IsEmpty());
-  EXPECT_TRUE(exception_state.GetException()->IsBoolean());
+  EXPECT_TRUE(try_catch.Exception()->IsBoolean());
 }
 
 v8::Local<v8::Promise> PromiseCallFromText(V8TestingScope* scope,
