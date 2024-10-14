960fd28ddcb233b8b0de9cdc63551dd075ca1530
Md Hasibul Hasan <hasibulhasan873@gmail.com>
2024-10-14 19:47:24 +0000

    [UI] Use mojo enum for `MenuSourceType` in c/b/u/ash/
    This CL replaces the uses of `MenuSourceType` with the mojo version
    in c/b/u/ash/*.
    
    Bug: 355460976
    Change-Id: I311e372d4858ffd7278230630ff455c5daff0cf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924333
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Md Hasibul Hasan <hasibulhasan873@gmail.com>
    Auto-Submit: Md Hasibul Hasan <hasibulhasan873@gmail.com>
    Reviewed-by: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368393}

--- chrome/browser/ui/ash/clipboard/clipboard_history_browsertest.cc
+++ chrome/browser/ui/ash/clipboard/clipboard_history_browsertest.cc
@@ -66,6 +66,7 @@
 #include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
 #include "ui/base/data_transfer_policy/data_transfer_policy_controller.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/mojom/menu_source_type.mojom.h"
 #include "ui/events/event_constants.h"
 #include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/events/test/event_generator.h"
@@ -1959,7 +1960,7 @@ IN_PROC_BROWSER_TEST_F(ClipboardHistoryLongpressEnabledBrowserTest,
   // Show the clipboard history menu via the Ctrl+V long-press shortcut so that
   // the menu's educational footer shows.
   EXPECT_TRUE(GetClipboardHistoryController()->ShowMenu(
-      gfx::Rect(), ui::MENU_SOURCE_NONE,
+      gfx::Rect(), ui::mojom::MenuSourceType::kNone,
       crosapi::mojom::ClipboardHistoryControllerShowSource::
           kControlVLongpress));
   EXPECT_TRUE(GetClipboardHistoryController()->IsMenuShowing());
