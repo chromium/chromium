95187d55ec74dba3dcf7885c6ac6ffab835b0f3e
Justin Lulejian <jlulejian@chromium.org>
2024-10-14 20:53:26 +0000

    Reland "[Extensions] Create message delivery tracker class."
    This is a reland of commit ce4d63c12d16f33cead60852bc256a8e5fdebed5
    
    This reland has changes, I took advantage of the revert to make a
    couple improvements and save another review commit:
    
    1) Refactored histogram types/names. Removed some and made them
      specific to each background type. This'll make it easier to determine
      how well each background type performs.
    2) Moved the class and unittest to //extensions/browser since I realized
      that when attempting to use MessageTracker it caused some circular
      dependencies
    3) Added a unit test to exercise each background type.
    
    Original change's description:
    > [Extensions] Create message delivery tracker class.
    >
    > Message delivery appears to be failing in some stages of the message
    > process because sometimes the caller never gets a response from the
    > background context (specifically service workers). To scope down where
    > in the process the issue might be occurring I created this class to
    > help coordinate the tracking of the message.
    >
    > The tracker will monitor message progress through key stages and emits
    > success/failure metrics. This will help pinpoint potential issues in
    > the message delivery process.
    >
    > It allows a caller (in the browser) to call this singleton as the
    > message is observed to pass through each stage.
    >
    > It is not specific to workers to allow for comparison between
    > background types.
    >
    > The general flow of a call is:
    >   1) NotifyStartTrackingMessageDelivery() to start tracking a message
    >      to its background context
    >   2) NotifyUpdateMessageDelivery() each time when the next stage in
    >      message delivery has been reached
    >   3) NotifyStopTrackingMessageDelivery() to stop tracking the message
    >      when it has been deemed as *successfully delivered to the
    >      background context (which emits success metrics)
    >
    > Starting and updating message delivery will result in a delayed task
    > (NotifyStaleMessage()) that is called 30 seconds later. 30 seconds was
    > determined arbitrarily. If the message is still being tracked at this
    > point it is considered stale/unsuccessful and failure metrics will be
    > emitted.
    >
    > *Successful can be defined differently depending on if the caller
    > expects a response or not since expecting a response would track the
    > later response stages back to the caller, whereas not expecting a
    > response wouldn't need to track those later stages.
    >
    > Bug: 371011217
    > Change-Id: I1d02837fb6dcccec3a56fb4cc6b1a5974087a180
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906729
    > Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    > Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    > Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1366516}
    
    Bug: 371011217
    Change-Id: Icd8dd7b765c9e9a87d5b761c04aee4795c4a2393
    Cq-Include-Trybots: luci.chromium.try:android-desktop-x64-compile-rel,android-desktop-14-x64-rel,android-desktop-arm64-compile-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924035
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368420}

---
+++ extensions/browser/message_tracker.cc
@@ -0,0 +1,247 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/browser/message_tracker.h"
+
+#include <map>
+
+#include "base/containers/contains.h"
+#include "base/functional/callback_helpers.h"
+#include "base/memory/singleton.h"
+#include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/task/single_thread_task_runner.h"
+#include "base/time/time.h"
+#include "base/unguessable_token.h"
+#include "components/keyed_service/content/browser_context_dependency_manager.h"
+#include "content/public/browser/browser_context.h"
+#include "extensions/browser/extensions_browser_client.h"
+
+namespace extensions {
+
+namespace {
+
+MessageTracker::TestObserver* g_test_observer = nullptr;
+
+std::string GetBackgroundStringForBackgroundType(
+    MessageTracker::MessageDestinationBackgroundType& type) {
+  switch (type) {
+    case MessageTracker::MessageDestinationBackgroundType::kNone:
+      return "None";
+    case MessageTracker::MessageDestinationBackgroundType::kPersistentPage:
+      return "PersistentBackgroundPage";
+    case MessageTracker::MessageDestinationBackgroundType::kEventPage:
+      return "EventPage";
+    case MessageTracker::MessageDestinationBackgroundType::kServiceWorker:
+      return "ServiceWorker";
+  }
+}
+
+class MessageTrackerFactory : public BrowserContextKeyedServiceFactory {
+ public:
+  MessageTrackerFactory();
+  MessageTrackerFactory(const MessageTrackerFactory&) = delete;
+  MessageTrackerFactory& operator=(const MessageTrackerFactory&) = delete;
+  ~MessageTrackerFactory() override = default;
+
+  MessageTracker* GetForBrowserContext(content::BrowserContext* context);
+
+ private:
+  // BrowserContextKeyedServiceFactory:
+  content::BrowserContext* GetBrowserContextToUse(
+      content::BrowserContext* context) const override;
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
+      content::BrowserContext* context) const override;
+};
+
+MessageTrackerFactory::MessageTrackerFactory()
+    : BrowserContextKeyedServiceFactory(
+          "MessageTracker",
+          BrowserContextDependencyManager::GetInstance()) {}
+
+MessageTracker* MessageTrackerFactory::GetForBrowserContext(
+    content::BrowserContext* browser_context) {
+  return static_cast<MessageTracker*>(
+      GetServiceForBrowserContext(browser_context, /*create=*/true));
+}
+
+content::BrowserContext* MessageTrackerFactory::GetBrowserContextToUse(
+    content::BrowserContext* context) const {
+  // One instance will exist across incognito and regular contexts.
+  return ExtensionsBrowserClient::Get()->GetContextRedirectedToOriginal(
+      context, /*force_guest_profile=*/true);
+}
+
+std::unique_ptr<KeyedService>
+MessageTrackerFactory::BuildServiceInstanceForBrowserContext(
+    content::BrowserContext* context) const {
+  return std::make_unique<MessageTracker>(context);
+}
+
+}  // namespace
+
+MessageTracker::TrackedMessage::TrackedMessage(
+    const MessageDeliveryStage status,
+    const MessageDestinationBackgroundType destination_background_type)
+    : stage_(status),
+      destination_background_type_(destination_background_type) {
+  start_time_ = base::Time::Now();
+}
+
+void MessageTracker::TrackedMessage::ResetTimeout() {
+  start_time_ = base::Time::Now();
+}
+
+MessageTracker::MessageDeliveryStage& MessageTracker::TrackedMessage::stage() {
+  return stage_;
+}
+
+MessageTracker::MessageDestinationBackgroundType&
+MessageTracker::TrackedMessage::destination_background_type() {
+  return destination_background_type_;
+}
+
+MessageTracker::MessageTracker(content::BrowserContext* context)
+    : context_(context) {}
+
+MessageTracker::~MessageTracker() {
+  tracked_messages_.clear();
+}
+
+// static
+MessageTracker* MessageTracker::Get(content::BrowserContext* browser_context) {
+  return static_cast<MessageTrackerFactory*>(GetFactory())
+      ->GetForBrowserContext(browser_context);
+}
+
+// static
+BrowserContextKeyedServiceFactory* MessageTracker::GetFactory() {
+  static base::NoDestructor<MessageTrackerFactory> g_factory;
+  return g_factory.get();
+}
+
+void MessageTracker::NotifyStartTrackingMessageDelivery(
+    const base::UnguessableToken& message_id,
+    const MessageDeliveryStage stage,
+    const MessageDestinationBackgroundType destination_background_type) {
+  CHECK(!base::Contains(tracked_messages_, message_id));
+  tracked_messages_.emplace(message_id,
+                            TrackedMessage(stage, destination_background_type));
+
+  // Eventually emits metrics on whether the message sat in this stage past the
+  // timeout.
+  base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&MessageTracker::NotifyStaleMessage,
+                     weak_factory_.GetWeakPtr(), message_id, stage),
+      /*delay=*/message_stale_timeout_);
+}
+
+void MessageTracker::NotifyUpdateMessageDelivery(
+    const base::UnguessableToken& message_id,
+    const MessageDeliveryStage new_stage) {
+  TrackedMessage* tracked_message = GetTrackedMessage(message_id);
+
+  // A message might've become stale and then later we try updating its stage,
+  // but we've removed its tracking due to stale.
+  if (!tracked_message) {
+    return;
+  }
+
+  tracked_message->ResetTimeout();
+  // A message should only move "forward" in the messaging stages.
+  CHECK(new_stage > tracked_message->stage());
+  tracked_message->stage() = new_stage;
+
+  // Eventually emits metrics on whether the message sat in this stage past the
+  // timeout.
+  base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&MessageTracker::NotifyStaleMessage,
+                     weak_factory_.GetWeakPtr(), message_id, new_stage),
+      /*delay=*/message_stale_timeout_);
+}
+
+void MessageTracker::NotifyStopTrackingMessageDelivery(
+    const base::UnguessableToken& message_id) {
+  TrackedMessage* tracked_message = GetTrackedMessage(message_id);
+  // A message might've become stale and then later we try to stop tracking it
+  // or two paths might try to stop tracking and one of them finished first.
+  if (!tracked_message) {
+    return;
+  }
+
+  std::string background_suffix = GetBackgroundStringForBackgroundType(
+      tracked_message->destination_background_type());
+  base::UmaHistogramBoolean(
+      base::StrCat({"Extensions.MessagePipeline.MessageCompleted", ".",
+                    background_suffix}),
+      true);
+  base::UmaHistogramCustomTimes(
+      base::StrCat({"Extensions.MessagePipeline.MessageCompletedTime", ".",
+                    background_suffix}),
+      base::Time::Now() - tracked_message->start_time(), base::Microseconds(1),
+      base::Seconds(30), /*buckets=*/50);
+
+  tracked_messages_.erase(message_id);
+}
+
+MessageTracker::TestObserver::TestObserver() = default;
+MessageTracker::TestObserver::~TestObserver() = default;
+
+// static
+void MessageTracker::SetObserverForTest(
+    MessageTracker::TestObserver* observer) {
+  g_test_observer = observer;
+}
+
+MessageTracker::TrackedMessage* MessageTracker::GetTrackedMessage(
+    const base::UnguessableToken& message_id) {
+  auto it = tracked_messages_.find(message_id);
+  return it == tracked_messages_.end() ? nullptr : &it->second;
+}
+
+void MessageTracker::NotifyStaleMessage(
+    const base::UnguessableToken message_id,
+    const MessageDeliveryStage previous_stage) {
+  // Ensure the test observer is notified before we exit the method, but
+  // after we do any work related to handling the registration.
+  base::ScopedClosureRunner notify_test_observer(base::BindOnce(
+      [](const base::UnguessableToken& message_id) {
+        if (g_test_observer) {
+          g_test_observer->OnTrackingStale(message_id);
+        }
+      },
+      message_id));
+
+  TrackedMessage* tracked_message = GetTrackedMessage(message_id);
+
+  // The message is no longer being tracked (e.g. completed process
+  // successfully).
+  if (!tracked_message) {
+    return;
+  }
+
+  // Message moved onto the next stage so return early. Another
+  // NotifyStaleMessage() will check the new status.
+  if (tracked_message->stage() != previous_stage) {
+    return;
+  }
+
+  // Message is stale so emit fail metrics and cleanup from tracking.
+  std::string background_suffix = GetBackgroundStringForBackgroundType(
+      tracked_message->destination_background_type());
+  base::UmaHistogramBoolean(
+      base::StrCat({"Extensions.MessagePipeline.MessageCompleted", ".",
+                    background_suffix}),
+      false);
+  base::UmaHistogramEnumeration(
+      base::StrCat({"Extensions.MessagePipeline.MessageStaleAtStage", ".",
+                    background_suffix}),
+      tracked_message->stage());
+
+  tracked_messages_.erase(message_id);
+}
+
+}  // namespace extensions
---
+++ extensions/browser/message_tracker_unittest.cc
@@ -0,0 +1,295 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/browser/message_tracker.h"
+
+#include "base/strings/strcat.h"
+#include "base/test/metrics/histogram_tester.h"
+#include "base/test/task_environment.h"
+#include "base/time/time.h"
+#include "content/public/browser/browser_context.h"
+#include "content/public/test/browser_task_environment.h"
+#include "content/public/test/test_browser_context.h"
+#include "extensions/browser/extensions_test.h"
+#include "extensions/browser/test_extensions_browser_client.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace extensions {
+
+namespace {
+
+class MessageTrackerTestObserver : public MessageTracker::TestObserver {
+ public:
+  explicit MessageTrackerTestObserver(const base::UnguessableToken message_id)
+      : observed_message_id_(message_id) {
+    MessageTracker::SetObserverForTest(this);
+  }
+
+  ~MessageTrackerTestObserver() override {
+    MessageTracker::SetObserverForTest(nullptr);
+  }
+
+  void WaitForTrackingStale() { on_tracking_stale_runloop.Run(); }
+
+ private:
+  void OnTrackingStale(const base::UnguessableToken& message_id) override {
+    if (observed_message_id_ == message_id) {
+      on_tracking_stale_runloop.Quit();
+    }
+  }
+
+ private:
+  base::UnguessableToken observed_message_id_;
+  base::RunLoop on_tracking_stale_runloop;
+};
+
+}  // namespace
+
+class MessageTrackerUnitTest : public ExtensionsTest {
+ protected:
+  void SetUp() override {
+    ExtensionsTest::SetUp();
+    message_tracker_ = MessageTracker::Get(browser_context());
+  }
+
+  void TearDown() override {
+    message_tracker_ = nullptr;
+    ExtensionsTest::TearDown();
+  }
+
+  MessageTracker* message_tracker() { return message_tracker_; }
+  base::HistogramTester& histogram_tester() { return histogram_tester_; }
+
+ private:
+  raw_ptr<MessageTracker> message_tracker_;
+  base::HistogramTester histogram_tester_;
+};
+
+// Tests that the tracker correctly records and keeps track of an
+// extension message as it progresses through its delivery stages.
+TEST_F(MessageTrackerUnitTest, NotifyStartTrackingMessageDelivery) {
+  base::UnguessableToken message_id = base::UnguessableToken::Create();
+  message_tracker()->NotifyStartTrackingMessageDelivery(
+      message_id, MessageTracker::MessageDeliveryStage::kUnknown,
+      MessageTracker::MessageDestinationBackgroundType::kServiceWorker);
+
+  message_tracker()->NotifyUpdateMessageDelivery(
+      message_id,
+      MessageTracker::MessageDeliveryStage::kOpenChannelRequestReceived);
+
+  // Tracker continues to track messages as they move through each messaging
+  // stage.
+  EXPECT_EQ(message_tracker()->GetNumberOfTrackedMessagesForTest(), 1u);
+}
+
+class MessageTrackerMultiBackgroundDestinationUnitTest
+    : public MessageTrackerUnitTest,
+      public testing::WithParamInterface<
+          MessageTracker::MessageDestinationBackgroundType> {};
+
+// Tests that the tracker correctly records and reports metrics when an
+// extension message finishes its delivery and stops being tracked.
+TEST_P(MessageTrackerMultiBackgroundDestinationUnitTest,
+       NotifyStopTrackingMessageDelivery_ForDestinationType) {
+  base::UnguessableToken message_id = base::UnguessableToken::Create();
+  MessageTrackerTestObserver observer(message_id);
+  message_tracker()->SetMessageStaleTimeoutForTest(base::Seconds(1));
+  MessageTracker::MessageDestinationBackgroundType destination_background_type =
+      GetParam();
+  message_tracker()->NotifyStartTrackingMessageDelivery(
+      message_id, MessageTracker::MessageDeliveryStage::kUnknown,
+      destination_background_type);
+  message_tracker()->NotifyStopTrackingMessageDelivery(message_id);
+
+  {
+    SCOPED_TRACE(
+        "waiting for stale check to run after stopping new message tracking");
+    observer.WaitForTrackingStale();
+  }
+
+  // Tracker stops tracking messages when they complete the process.
+  EXPECT_EQ(message_tracker()->GetNumberOfTrackedMessagesForTest(), 0u);
+
+  switch (destination_background_type) {
+    case MessageTracker::MessageDestinationBackgroundType::kNone:
+      histogram_tester().ExpectBucketCount(
+          "Extensions.MessagePipeline.MessageCompleted.None", /*sample=*/true,
+          /*expected_count=*/1);
+      histogram_tester().ExpectTotalCount(
+          "Extensions.MessagePipeline.MessageCompletedTime.None",
+          /*expected_count=*/1);
+      histogram_tester().ExpectTotalCount(
+          "Extensions.MessagePipeline.MessageStaleAtStage.None",
+          /*expected_count=*/0);
+      break;
+    case MessageTracker::MessageDestinationBackgroundType::kPersistentPage:
+      histogram_tester().ExpectBucketCount(
+          "Extensions.MessagePipeline.MessageCompleted."
+          "PersistentBackgroundPage",
+          /*sample=*/true, /*expected_count=*/1);
+      histogram_tester().ExpectTotalCount(
+          "Extensions.MessagePipeline.MessageCompletedTime."
+          "PersistentBackgroundPage",
+          /*expected_count=*/1);
+      histogram_tester().ExpectTotalCount(
+          "Extensions.MessagePipeline.MessageStaleAtStage."
+          "PersistentBackgroundPage",
+          /*expected_count=*/0);
+      break;
+    case MessageTracker::MessageDestinationBackgroundType::kEventPage:
+      histogram_tester().ExpectBucketCount(
+          "Extensions.MessagePipeline.MessageCompleted.EventPage",
+          /*sample=*//*sample=*/true, /*expected_count=*/1);
+      histogram_tester().ExpectTotalCount(
+          "Extensions.MessagePipeline.MessageCompletedTime.EventPage",
+          /*expected_count=*/1);
+      histogram_tester().ExpectTotalCount(
+          "Extensions.MessagePipeline.MessageStaleAtStage.EventPage",
+          /*expected_count=*/0);
+      break;
+    case MessageTracker::MessageDestinationBackgroundType::kServiceWorker:
+      histogram_tester().ExpectBucketCount(
+          "Extensions.MessagePipeline.MessageCompleted.ServiceWorker",
+          /*sample=*/true, /*expected_count=*/1);
+      histogram_tester().ExpectTotalCount(
+          "Extensions.MessagePipeline.MessageCompletedTime.ServiceWorker",
+          /*expected_count=*/1);
+      histogram_tester().ExpectTotalCount(
+          "Extensions.MessagePipeline.MessageStaleAtStage.ServiceWorker",
+          /*expected_count=*/0);
+      break;
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    BackgroundDestination,
+    MessageTrackerMultiBackgroundDestinationUnitTest,
+    testing::Values(
+        MessageTracker::MessageDestinationBackgroundType::kNone,
+        MessageTracker::MessageDestinationBackgroundType::kPersistentPage,
+        MessageTracker::MessageDestinationBackgroundType::kEventPage,
+        MessageTracker::MessageDestinationBackgroundType::kServiceWorker));
+
+// Tests that the tracker correctly records and reports metrics when an
+// extension message becomes stale.
+TEST_F(MessageTrackerUnitTest, NotifyStaleMessage) {
+  base::UnguessableToken message_id = base::UnguessableToken::Create();
+  MessageTrackerTestObserver observer(message_id);
+  message_tracker()->SetMessageStaleTimeoutForTest(base::Microseconds(1));
+  message_tracker()->NotifyStartTrackingMessageDelivery(
+      message_id, MessageTracker::MessageDeliveryStage::kUnknown,
+      MessageTracker::MessageDestinationBackgroundType::kServiceWorker);
+
+  {
+    SCOPED_TRACE(
+        "waiting for stale check to run after starting new message tracking");
+    observer.WaitForTrackingStale();
+  }
+
+  histogram_tester().ExpectBucketCount(
+      "Extensions.MessagePipeline.MessageCompleted.ServiceWorker",
+      /*sample=*/false,
+      /*expected_count=*/1);
+  histogram_tester().ExpectBucketCount(
+      "Extensions.MessagePipeline.MessageStaleAtStage.ServiceWorker",
+      MessageTracker::MessageDeliveryStage::kUnknown, /*expected_count=*/1);
+}
+
+// Tests that the tracker correctly records and reports metrics when an
+// extension message stale check occurs *after* an update call occurs which
+// means the message is not stale.
+TEST_F(MessageTrackerUnitTest, NotifyStaleMessageAfterUpdateToNewStage) {
+  base::UnguessableToken message_id = base::UnguessableToken::Create();
+  MessageTrackerTestObserver observer(message_id);
+  message_tracker()->SetMessageStaleTimeoutForTest(base::Seconds(1));
+  message_tracker()->NotifyStartTrackingMessageDelivery(
+      message_id, MessageTracker::MessageDeliveryStage::kUnknown,
+      MessageTracker::MessageDestinationBackgroundType::kServiceWorker);
+  message_tracker()->NotifyUpdateMessageDelivery(
+      message_id,
+      MessageTracker::MessageDeliveryStage::kOpenChannelRequestReceived);
+
+  // This will wait for the stale check created by
+  // NotifyStartTrackingMessageDelivery(), then proceed.
+  {
+    SCOPED_TRACE(
+        "waiting for stale check to run after starting and updating new "
+        "message tracking");
+    observer.WaitForTrackingStale();
+  }
+
+  // This will prevent the stale check that runs from
+  // NotifyUpdateMessageDelivery() to prevent emitting stale metrics.
+  message_tracker()->NotifyStopTrackingMessageDelivery(message_id);
+
+  histogram_tester().ExpectBucketCount(
+      "Extensions.MessagePipeline.MessageCompleted.ServiceWorker",
+      /*sample=*/true, /*expected_count=*/1);
+  histogram_tester().ExpectTotalCount(
+      "Extensions.MessagePipeline.MessageCompletedTime.ServiceWorker",
+      /*expected_count=*/1);
+  histogram_tester().ExpectTotalCount(
+      "Extensions.MessagePipeline.MessageStaleAtStage.ServiceWorker",
+      /*expected_count=*/0);
+}
+
+// Tests that the tracker correctly records and reports metrics when an
+// extension message stale check occurs *before* an update call occurs which
+// means the message is stale.
+TEST_F(MessageTrackerUnitTest, NotifyStaleMessageBeforeUpdateToNewStage) {
+  base::UnguessableToken message_id = base::UnguessableToken::Create();
+  MessageTrackerTestObserver observer(message_id);
+  message_tracker()->SetMessageStaleTimeoutForTest(base::Microseconds(1));
+  message_tracker()->NotifyStartTrackingMessageDelivery(
+      message_id, MessageTracker::MessageDeliveryStage::kUnknown,
+      MessageTracker::MessageDestinationBackgroundType::kServiceWorker);
+
+  {
+    SCOPED_TRACE(
+        "waiting for stale check to run after starting new message tracking");
+    observer.WaitForTrackingStale();
+  }
+
+  message_tracker()->NotifyUpdateMessageDelivery(
+      message_id,
+      MessageTracker::MessageDeliveryStage::kOpenChannelRequestReceived);
+
+  histogram_tester().ExpectBucketCount(
+      "Extensions.MessagePipeline.MessageCompleted.ServiceWorker",
+      /*sample=*/false,
+      /*expected_count=*/1);
+  histogram_tester().ExpectBucketCount(
+      "Extensions.MessagePipeline.MessageStaleAtStage.ServiceWorker",
+      MessageTracker::MessageDeliveryStage::kUnknown, /*expected_count=*/1);
+}
+
+// Tests that the tracker correctly records and reports metrics when an
+// extension message becomes stale *before* a late subsequent stop tracking call
+// occurs.
+TEST_F(MessageTrackerUnitTest, NotifyStaleMessageWithLateStop) {
+  base::UnguessableToken message_id = base::UnguessableToken::Create();
+  MessageTrackerTestObserver observer(message_id);
+  message_tracker()->SetMessageStaleTimeoutForTest(base::Microseconds(1));
+  message_tracker()->NotifyStartTrackingMessageDelivery(
+      message_id, MessageTracker::MessageDeliveryStage::kUnknown,
+      MessageTracker::MessageDestinationBackgroundType::kServiceWorker);
+
+  {
+    SCOPED_TRACE(
+        "waiting for stale check to run after starting new message tracking");
+    observer.WaitForTrackingStale();
+  }
+
+  message_tracker()->NotifyStopTrackingMessageDelivery(message_id);
+
+  histogram_tester().ExpectBucketCount(
+      "Extensions.MessagePipeline.MessageCompleted.ServiceWorker",
+      /*sample=*/false,
+      /*expected_count=*/1);
+  histogram_tester().ExpectBucketCount(
+      "Extensions.MessagePipeline.MessageStaleAtStage.ServiceWorker",
+      MessageTracker::MessageDeliveryStage::kUnknown, /*expected_count=*/1);
+}
+
+}  // namespace extensions
