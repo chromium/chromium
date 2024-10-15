7e9fb03494380104f6c8a387951cbc9167a30a94
dpapad <dpapad@chromium.org>
2024-10-15 17:08:57 +0000

    FlagsUI: Add more tests for a few previously uncovered codepaths.
    Specifically:
     - Test tab/blur event handlers on the last changed <select> and
       'restart' button, to check that the 'restart' button is inserted in
       the tab order after a change.
     - Test rendering of experiments with non-default value.
     - Test that the slash "/" keystroke focuses the search box.
    
    Also rename some Mocha test case names for consistency.
    
    Bug: 40246381
    Change-Id: I08f47eaa58de18947a9eed6904334be877865b9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932323
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368841}

--- chrome/test/data/webui/flags/app_test.ts
+++ chrome/test/data/webui/flags/app_test.ts
@@ -82,11 +82,8 @@ suite('FlagsAppTest', function() {
   let resetAllButton: HTMLButtonElement;
   let browserProxy: TestFlagsBrowserProxy;
 
-  setup(async function() {
-    document.body.innerHTML = window.trustedTypes!.emptyHTML;
-    browserProxy = new TestFlagsBrowserProxy();
-    browserProxy.setFeatureData(experimentalFeaturesData);
-    FlagsBrowserProxyImpl.setInstance(browserProxy);
+  async function setupApp(data: ExperimentalFeaturesData) {
+    browserProxy.setFeatureData(data);
     app = document.createElement('flags-app');
     document.body.appendChild(app);
     app.setAnnounceStatusDelayMsForTesting(0);
@@ -96,6 +93,13 @@ suite('FlagsAppTest', function() {
     clearSearch = app.getRequiredElement<HTMLInputElement>('.clear-search');
     resetAllButton =
         app.getRequiredElement<HTMLButtonElement>('#experiment-reset-all');
+  }
+
+  setup(function() {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    browserProxy = new TestFlagsBrowserProxy();
+    FlagsBrowserProxyImpl.setInstance(browserProxy);
+    return setupApp(experimentalFeaturesData);
   });
 
   function searchBoxInput(text: string) {
@@ -129,37 +133,104 @@ suite('FlagsAppTest', function() {
     // </if>
   });
 
-  test(
-      'check available/unavailable tabs are rendered properly',
-      async function() {
-        const crTabs = app.getRequiredElement('cr-tabs');
-        assertTrue(isVisible(crTabs));
-        assertEquals(2, crTabs.tabNames.length);
-        assertEquals(0, crTabs.selected);
+  test('AvailableUnavailableTabsRendered', async function() {
+    const crTabs = app.getRequiredElement('cr-tabs');
+    assertTrue(isVisible(crTabs));
+    assertEquals(2, crTabs.tabNames.length);
+    assertEquals(0, crTabs.selected);
 
-        const defaultAvailableExperimentsContainer =
-            app.getRequiredElement('#default-experiments');
-        const nonDefaultAvailableExperimentsContainer =
-            app.getRequiredElement('#non-default-experiments');
-        const unavailableExperimentsContainer =
-            app.getRequiredElement('#unavailable-experiments');
-        assertFalse(isVisible(nonDefaultAvailableExperimentsContainer));
-        assertTrue(isVisible(defaultAvailableExperimentsContainer));
-        assertFalse(isVisible(unavailableExperimentsContainer));
+    const defaultAvailableExperimentsContainer =
+        app.getRequiredElement('#default-experiments');
+    const nonDefaultAvailableExperimentsContainer =
+        app.getRequiredElement('#non-default-experiments');
+    const unavailableExperimentsContainer =
+        app.getRequiredElement('#unavailable-experiments');
+    assertFalse(isVisible(nonDefaultAvailableExperimentsContainer));
+    assertTrue(isVisible(defaultAvailableExperimentsContainer));
+    assertFalse(isVisible(unavailableExperimentsContainer));
 
-        // Toggle unavailable tab and the unavailable experiments container
-        // becomes visible.
-        const tabs = crTabs.shadowRoot!.querySelectorAll<HTMLElement>('.tab');
-        assertEquals(2, tabs.length);
-        tabs[1]!.click();
-        await microtasksFinished();
-        assertEquals(1, crTabs.selected);
-        assertFalse(isVisible(nonDefaultAvailableExperimentsContainer));
-        assertFalse(isVisible(defaultAvailableExperimentsContainer));
-        assertTrue(isVisible(unavailableExperimentsContainer));
-      });
+    // Toggle unavailable tab and the unavailable experiments container
+    // becomes visible.
+    const tabs = crTabs.shadowRoot!.querySelectorAll<HTMLElement>('.tab');
+    assertEquals(2, tabs.length);
+    tabs[1]!.click();
+    await microtasksFinished();
+    assertEquals(1, crTabs.selected);
+    assertFalse(isVisible(nonDefaultAvailableExperimentsContainer));
+    assertFalse(isVisible(defaultAvailableExperimentsContainer));
+    assertTrue(isVisible(unavailableExperimentsContainer));
+  });
 
-  test('clear search button shown/hidden', async function() {
+  test('DefaultNonDefaultExperimentsRendered', async function() {
+    function getDefaultEntries() {
+      return app.shadowRoot!.querySelectorAll(
+          '#default-experiments flags-experiment');
+    }
+
+    function getNonDefaultEntries() {
+      return app.shadowRoot!.querySelectorAll(
+          '#non-default-experiments flags-experiment');
+    }
+
+    // Check that the dummy data has two experiments in their default state.
+    assertEquals(2, experimentalFeaturesData.supportedFeatures.length);
+    assertTrue(experimentalFeaturesData.supportedFeatures[0]!.is_default);
+    assertTrue(experimentalFeaturesData.supportedFeatures[1]!.is_default);
+
+    // Check that they are rendered under the corresponding sections on startup.
+    assertEquals(2, getDefaultEntries().length);
+    assertEquals(0, getNonDefaultEntries().length);
+
+    // Simulate case where one experiment is in default state, and one isn't.
+    const data: ExperimentalFeaturesData =
+        structuredClone(experimentalFeaturesData);
+    data.supportedFeatures[1]!.is_default = false;
+    await setupApp(data);
+
+    // Check that they are rendered under the corresponding sections on startup.
+    assertEquals(1, getDefaultEntries().length);
+    assertEquals(1, getNonDefaultEntries().length);
+  });
+
+  test('RestartButtonTabOrder', async function() {
+    const experiments = app.getRequiredElement('#default-experiments')
+                            .querySelectorAll('flags-experiment');
+    assertEquals(2, experiments.length);
+
+    // Focus the first experiment's <select> and simulate a change.
+    const select = experiments[0]!.shadowRoot!.querySelector('select');
+    assertTrue(!!select);
+    select.focus();
+    assertEquals(select, getDeepActiveElement());
+    select.selectedIndex = 1;
+    select.dispatchEvent(new Event('change'));
+    await microtasksFinished();
+
+    // Simulate a 'Tab' keystroke, and check that the 'restart' button is
+    // focused, instead of the next experiment.
+    select.dispatchEvent(new KeyboardEvent('keydown', {key: 'Tab'}));
+    const restartButton =
+        app.getRequiredElement<HTMLButtonElement>('#experiment-restart-button');
+    assertEquals(restartButton, getDeepActiveElement());
+
+    // Simulate 'Shift+Tab' keystroke on the 'restart' button. The previously
+    // changed <select> should be focused.
+    restartButton.dispatchEvent(
+        new KeyboardEvent('keydown', {key: 'Tab', shiftKey: true}));
+    assertEquals(select, getDeepActiveElement());
+
+    // Simulate another 'Tab' keystroke. This time the next experiment should be
+    // focused and not the 'restart' button.
+    select.dispatchEvent(new KeyboardEvent('keydown', {key: 'Tab'}));
+    // Normally the next experiment's permalink should be focused, but
+    // simulating a 'Tab' event from JS does not trigger the same actions as a
+    // real user-gesture. For the purposes of this test checking that the focus
+    // does not go back to the 'restart' button is sufficient (guarantees that
+    // the select's 'blur' handler did its job).
+    assertNotEquals(restartButton, getDeepActiveElement());
+  });
+
+  test('ClearSearchButtonVisibility', async function() {
     // The clear search button is hidden initially.
     assertFalse(isVisible(clearSearch));
 
@@ -216,7 +287,7 @@ suite('FlagsAppTest', function() {
     return browserProxy.whenCalled('restartBrowser');
   });
 
-  test('search and found match', function() {
+  test('SearchMatchFound', function() {
     const promise = eventToPromise('search-finished-for-testing', app);
     searchBoxInput('available');
     return promise.then(() => {
@@ -240,7 +311,7 @@ suite('FlagsAppTest', function() {
     });
   });
 
-  test('search and match not found', function() {
+  test('SearchMatchNotFound', function() {
     const promise = eventToPromise('search-finished-for-testing', app);
     searchBoxInput('none');
     return promise.then(() => {
@@ -268,8 +339,17 @@ suite('FlagsAppTest', function() {
     // Search field is focused on page load.
     assertEquals(searchTextArea, getDeepActiveElement());
 
-    // Remove focus on search field.
+    // Dispatch 'Escape' keyboard event and check that search is blurred.
+    window.dispatchEvent(new KeyboardEvent('keyup', {key: 'Escape'}));
+    assertEquals(document.body, getDeepActiveElement());
+
+    // Dispatch '/' keyboard event and check that search is focused.
+    window.dispatchEvent(new KeyboardEvent('keyup', {key: '/'}));
+    assertEquals(searchTextArea, getDeepActiveElement());
+
+    // Remove focus from search field.
     searchTextArea.blur();
+    assertEquals(document.body, getDeepActiveElement());
 
     // Clear search.
     searchBoxInput('test');
@@ -277,15 +357,5 @@ suite('FlagsAppTest', function() {
 
     // Search field is focused after search is cleared.
     assertEquals(searchTextArea, getDeepActiveElement());
-
-    // Dispatch 'Enter' keyboard event and check that search remains focused.
-    searchBoxInput('test');
-    window.dispatchEvent(new KeyboardEvent('keyup', {key: 'Enter'}));
-    assertEquals(searchTextArea, getDeepActiveElement());
-
-    // Dispatch 'Escape' keyboard event and check that search is cleard and not
-    // focused.
-    window.dispatchEvent(new KeyboardEvent('keyup', {key: 'Escape'}));
-    assertNotEquals(searchTextArea, getDeepActiveElement());
   });
 });
