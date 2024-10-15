1f9a4db9f8f0d8b1561a6e264d2d88f064f19fbc
Jose Dapena Paz <jdapena@igalia.com>
2024-10-15 21:28:47 +0000

    IWYU: missing include for usage of std::variant in bookmark_merged_surface_service.h
    Bug: 41455655
    Change-Id: I00b251c7dc4a35a9bf7fd023fd35a9051c7a715c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5935116
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1369030}

--- chrome/browser/bookmarks/bookmark_merged_surface_service.h
+++ chrome/browser/bookmarks/bookmark_merged_surface_service.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_BOOKMARKS_BOOKMARK_MERGED_SURFACE_SERVICE_H_
 #define CHROME_BROWSER_BOOKMARKS_BOOKMARK_MERGED_SURFACE_SERVICE_H_
 
+#include <variant>
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
