e8bff4aef87552104706b23de449d037e5e55d8c
Henrique Nakashima <hnakashima@chromium.org>
2024-10-15 22:40:41 +0000

    [Android] Do not directly enable features from FieldTrials
    FeatureList.setTestFeaturesNoResetForTesting() should be doing this,
    but it was not cleaning up arguments like "FeatureA<Trial1", so
    even though native did, Java-side mechanisms like FeatureList.java
    and CachedFlag.java did not interpret that.
    
    Get Features#reset() to do this clean up before putting the value in
    FeatureList#setTestFeaturesNoResetForTesting() and we don't have to
    amend the test feature config with FeatureList.mergeTestValue() in FieldTrials#applyFieldTrials().
    
    Bug: 372717700
    Change-Id: I4a58b2c94b9902403cc90d6c140ea8cc93f3246a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920784
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369071}

--- base/test/android/javatests/src/org/chromium/base/test/util/Features.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/Features.java
@@ -13,6 +13,7 @@ import org.chromium.base.cached_flags.ValuesReturned;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -57,8 +58,32 @@ public class Features {
     public static void reset(Map<String, Boolean> flagStates) {
         // TODO(agrieve): Use ScopedFeatureList to update native feature states even after
         //     native feature list has been initialized.
-        FeatureList.setTestFeaturesNoResetForTesting(flagStates);
+        Map<String, Boolean> cleanFlagStates = cleanUpFlagStates(flagStates);
+        FeatureList.setTestFeaturesNoResetForTesting(cleanFlagStates);
         // Apply "--force-fieldtrials" passed by @CommandLineFlags.
         FieldTrials.getInstance().applyFieldTrials(CommandLine.getInstance());
+    }
+
+    /**
+     * Removes field trials from the keys of |flagsStates|.
+     *
+     * <p>E.g.: {"FeatureA<Trial1": true} becomes {"FeatureA": true}.
+     */
+    private static Map<String, Boolean> cleanUpFlagStates(Map<String, Boolean> flagStates) {
+        Map<String, Boolean> cleanFlagStates = new HashMap<>();
+        for (Map.Entry<String, Boolean> entry : flagStates.entrySet()) {
+            String rawFeatureName = entry.getKey();
+            Boolean featureFlagValue = entry.getValue();
+            if (rawFeatureName.contains("<")) {
+                assert featureFlagValue
+                        : String.format(
+                                "--disable-features=%s should not have a field trial",
+                                rawFeatureName);
+                cleanFlagStates.put(rawFeatureName.split("<")[0], featureFlagValue);
+            } else {
+                cleanFlagStates.put(rawFeatureName, featureFlagValue);
+            }
+        }
+        return cleanFlagStates;
     }
 }
