53258f0c45f7ff85d898c018f4fe892e64d2e6cf
Kaan Icer <icer@google.com>
2024-10-15 00:56:35 +0000

    Enable Loading & Confirmation by default on Android
    launch/4302656
    
    Feature Sync approval:
    https://docs.google.com/document/d/1oqCRE3_dlBiL8gkhMT86d9iEimYFcJ_-HvNFkhPAtqc/edit?tab=t.0#bookmark=id.dmzw9h69o4qi
    
    Bug: 40287758
    
    Change-Id: Id9f3ad07ee49f655b37caf9cc0a47bdf687c94fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921377
    Reviewed-by: Kavita Soni <kavitasoni@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368542}

--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -177,21 +177,13 @@ BASE_FEATURE(kAutofillEnablePrefetchingRiskDataForRetrieval,
 // result when completed.
 BASE_FEATURE(kAutofillEnableSaveCardLoadingAndConfirmation,
              "AutofillEnableSaveCardLoadingAndConfirmation",
-#if BUILDFLAG(IS_ANDROID)
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#else
              base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 
 // When enabled, save card will fallback to a local save if the server upload of
 // a card encounters a failure.
 BASE_FEATURE(kAutofillEnableSaveCardLocalSaveFallback,
              "AutofillEnableSaveCardLocalSaveFallback",
-#if BUILDFLAG(IS_ANDROID)
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#else
              base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 
 // When enabled, Autofill will attempt to offer upload save for IBANs
 // (International Bank Account Numbers) and autofill server-based IBANs.
@@ -219,11 +211,7 @@ BASE_FEATURE(kAutofillEnableVcn3dsAuthentication,
 // result when completed.
 BASE_FEATURE(kAutofillEnableVcnEnrollLoadingAndConfirmation,
              "AutofillEnableVcnEnrollLoadingAndConfirmation",
-#if BUILDFLAG(IS_ANDROID)
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#else
              base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 
 // When enabled, Chrome will display grayed out virtual card suggestions on
 // merchant websites where the merchant has opted-out of virtual cards.
