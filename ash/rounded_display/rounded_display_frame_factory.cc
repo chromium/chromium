539721c2e30ef45396473ff892c2a8528d5ddd33
Colin Blundell <blundell@chromium.org>
2024-10-15 16:50:03 +0000

    [//ash] Spanify remaining call of ScopedMapping::Memory()
    Moves the code to use GetSkPixmapForPlane(). This variant of the API
    wraps GetMemoryForPlane() [1]. SkBitmap::readPixels(SkPixmap) then
    unpacks the passed-in SkPixmap [2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/client/client_shared_image.cc;l=182-187?q=GetSkPixmapForPla&ss=chromium
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/src/core/SkBitmap.cpp;l=497-499?q=SkBitmap::readP&ss=chromium
    
    Bug: 369213516
    Change-Id: I463c171bfe8631fce18e39489e1c152a60016de0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5928672
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368832}

--- ash/rounded_display/rounded_display_frame_factory.cc
+++ ash/rounded_display/rounded_display_frame_factory.cc
@@ -278,13 +278,11 @@ void RoundedDisplayFrameFactory::Paint(
     return;
   }
 
-  uint8_t* data = static_cast<uint8_t*>(mapping->Memory(0));
-  int stride = mapping->Stride(0);
-
   canvas.GetBitmap().readPixels(
-      SkImageInfo::MakeN32Premul(mapping->Size().width(),
-                                 mapping->Size().height()),
-      data, stride, 0, 0);
+      mapping->GetSkPixmapForPlane(
+          0, SkImageInfo::MakeN32Premul(mapping->Size().width(),
+                                        mapping->Size().height())),
+      0, 0);
 }
 
 void RoundedDisplayFrameFactory::AppendQuad(
