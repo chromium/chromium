7e8be05a99b4be7d9af633a9d66223b391f1f994
Wolfgang Billenstein <bwolfgang@google.com>
2024-10-15 10:46:24 +0000

    Provide static API::IsUserEligible() function to be used in settings
    Fixed: 373550852
    Change-Id: Ic2d5c24869e5bbe6f9a79a7e144ebed836d97b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5934364
    Reviewed-by: Andrii Natiahlyi <natiahlyi@google.com>
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368694}

---
+++ chrome/browser/autofill_prediction_improvements/autofill_prediction_improvements_util.h
@@ -0,0 +1,16 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_AUTOFILL_PREDICTION_IMPROVEMENTS_AUTOFILL_PREDICTION_IMPROVEMENTS_UTIL_H_
+#define CHROME_BROWSER_AUTOFILL_PREDICTION_IMPROVEMENTS_AUTOFILL_PREDICTION_IMPROVEMENTS_UTIL_H_
+
+class Profile;
+
+namespace autofill_prediction_improvements {
+
+bool IsUserEligible(Profile* profile);
+
+}
+
+#endif  // CHROME_BROWSER_AUTOFILL_PREDICTION_IMPROVEMENTS_AUTOFILL_PREDICTION_IMPROVEMENTS_UTIL_H_
