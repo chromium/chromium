41050c0bd22fd284b6c4cfa03d5ef968195c76c4
Eriko Kurimoto <elkurin@chromium.org>
2024-10-16 11:21:57 +0000

    pinned_apps: Move //c/b/ui/ash/default_pinned_apps to //chromeos
    Bug: b:366380528
    Change-Id: Ia099ef50d680913929bbaef1f320db9e18a9773b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920025
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369299}

--- chromeos/ash/components/default_pinned_apps/default_pinned_apps.h
+++ chromeos/ash/components/default_pinned_apps/default_pinned_apps.h
@@ -2,16 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_DEFAULT_PINNED_APPS_DEFAULT_PINNED_APPS_H_
-#define CHROME_BROWSER_UI_ASH_DEFAULT_PINNED_APPS_DEFAULT_PINNED_APPS_H_
+#ifndef CHROMEOS_ASH_COMPONENTS_DEFAULT_PINNED_APPS_DEFAULT_PINNED_APPS_H_
+#define CHROMEOS_ASH_COMPONENTS_DEFAULT_PINNED_APPS_DEFAULT_PINNED_APPS_H_
 
 #include <vector>
 
+#include "base/component_export.h"
 #include "content/public/browser/browser_context.h"
 
 using StaticAppId = const char*;
 
+COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_DEFAULT_PINNED_APPS)
 std::vector<StaticAppId> GetDefaultPinnedAppsForFormFactor(
     content::BrowserContext* browser_context);
 
-#endif  // CHROME_BROWSER_UI_ASH_DEFAULT_PINNED_APPS_DEFAULT_PINNED_APPS_H_
+#endif  // CHROMEOS_ASH_COMPONENTS_DEFAULT_PINNED_APPS_DEFAULT_PINNED_APPS_H_
