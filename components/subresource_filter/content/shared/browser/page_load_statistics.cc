ab850bcfc82ea2a446e05d0633743b14c7fcdf0b
Alex Turner <alexmt@chromium.org>
2024-10-16 15:16:03 +0000

    Remove PageLoadStatistics CHECKs that cause crashes
    These can be triggered in rare navigation situations and are not
    necessary for correctness/security. See bugs below for discussion about
    a longer term fix, in particular crbug.com/341798380#comment29.
    
    Bug: 341798380, 372883698, 370252763
    Change-Id: I4936328d8591d81aaf7e56b5eeefaf6506dcc0ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5923440
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369410}

--- components/subresource_filter/content/shared/browser/page_load_statistics.cc
+++ components/subresource_filter/content/shared/browser/page_load_statistics.cc
@@ -7,32 +7,14 @@
 #include <string_view>
 
 #include "base/check.h"
-#include "base/debug/crash_logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/not_fatal_until.h"
 #include "base/strings/strcat.h"
 #include "components/subresource_filter/core/common/time_measurements.h"
 #include "components/subresource_filter/core/mojom/subresource_filter.mojom.h"
-#include "content/public/browser/navigation_handle.h"
-#include "content/public/browser/render_frame_host.h"
-#include "net/base/net_errors.h"
-#include "url/gurl.h"
 
 namespace subresource_filter {
-
-PageLoadStatistics::PageLoadStatistics(
-    const mojom::ActivationState& state,
-    std::string_view uma_filter_tag,
-    content::NavigationHandle* navigation_handle,
-    content::RenderFrameHost* frame_host)
-    : activation_state_(state),
-      uma_filter_tag_(uma_filter_tag),
-      details_for_crash_keys_(DetailsForCrashKeys{
-          .navigation_url = navigation_handle->GetURL(),
-          .navigation_error_code = navigation_handle->GetNetErrorCode(),
-          .has_navigation_committed = navigation_handle->HasCommitted(),
-          .frame_last_commited_url = frame_host->GetLastCommittedURL()}) {}
 
 PageLoadStatistics::PageLoadStatistics(const mojom::ActivationState& state,
                                        std::string_view uma_filter_tag)
@@ -51,33 +33,6 @@ void PageLoadStatistics::OnDocumentLoadStatistics(
   aggregated_document_statistics_.num_loads_disallowed +=
       statistics.num_loads_disallowed;
 
-  // TODO(crbug.com/341798380): These CHECKs and crash keys are temporary for
-  // narrowing down an issue. Remove once fixed.
-  if (!activation_state_.measure_performance && details_for_crash_keys_) {
-    SCOPED_CRASH_KEY_STRING1024(
-        "bug341798380", "navigation-url",
-        details_for_crash_keys_->navigation_url.possibly_invalid_spec());
-    SCOPED_CRASH_KEY_STRING256(
-        "bug341798380", "navigation-error-code",
-        net::ErrorToString(details_for_crash_keys_->navigation_error_code));
-    SCOPED_CRASH_KEY_BOOL("bug341798380", "navigation-has-committed",
-                          details_for_crash_keys_->has_navigation_committed);
-    SCOPED_CRASH_KEY_STRING1024("bug341798380", "last-committed-url",
-                                details_for_crash_keys_->frame_last_commited_url
-                                    .possibly_invalid_spec());
-    SCOPED_CRASH_KEY_NUMBER(
-        "bug341798380", "activation-level",
-        static_cast<int>(activation_state_.activation_level));
-    SCOPED_CRASH_KEY_BOOL("bug341798380", "filtering-disabled",
-                          activation_state_.filtering_disabled_for_document);
-    SCOPED_CRASH_KEY_BOOL("bug341798380", "generic-rules-disabled",
-                          activation_state_.generic_blocking_rules_disabled);
-
-    CHECK(statistics.evaluation_total_wall_duration.is_zero(),
-          base::NotFatalUntil::M132);
-    CHECK(statistics.evaluation_total_cpu_duration.is_zero(),
-          base::NotFatalUntil::M132);
-  }
   aggregated_document_statistics_.evaluation_total_wall_duration +=
       statistics.evaluation_total_wall_duration;
   aggregated_document_statistics_.evaluation_total_cpu_duration +=
@@ -118,14 +73,10 @@ void PageLoadStatistics::OnDidFinishLoad() {
                       ".PageLoad.SubresourceEvaluation.TotalCPUDuration"}),
         aggregated_document_statistics_.evaluation_total_cpu_duration,
         base::Microseconds(1), base::Seconds(10), 50);
-  } else {
-    CHECK(aggregated_document_statistics_.evaluation_total_wall_duration
-              .is_zero(),
-          base::NotFatalUntil::M132);
-    CHECK(
-        aggregated_document_statistics_.evaluation_total_cpu_duration.is_zero(),
-        base::NotFatalUntil::M132);
   }
+  // Theoretically, we should be able to add an else case that CHECK()s that the
+  // evaluation durations are zero. However, this causes crashes as the renderer
+  // and browser appear to sometimes get out of sync. See crbug.com/372883698.
 }
 
 }  // namespace subresource_filter
