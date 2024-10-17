f7bf57c4059306bc76d69b317708112bafc43604
Zaina Al-Mashni <zalmashni@google.com>
2024-10-17 19:14:34 +0000

    Reland "[Synapse] Add experimental features page metrics"
    This is a reland of commit 5bd0b061b3f0727693cc6381848c8265fe39487f
    
    Reason for reland: The feature flag in the settings_menu_test was misspelled which caused the tests to fail on some bots where the feature is disabled. This has now been fixed.
    
    Original change's description:
    > [Synapse] Add experimental features page metrics
    >
    > Bug: 362225975
    > Change-Id: I91b0e061108a4b908483a0335a8d4bb4e4231c3f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5930665
    > Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    > Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    > Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1369917}
    
    Bug: 362225975
    Change-Id: I56df6a391be121a3540cddff07857a9b09c9eeda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937952
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370190}

--- chrome/browser/resources/settings/settings.ts
+++ chrome/browser/resources/settings/settings.ts
@@ -62,7 +62,7 @@ export {SettingsDefaultBrowserPageElement} from './default_browser_page/default_
 // </if>
 export {HatsBrowserProxy, HatsBrowserProxyImpl, SafeBrowsingSetting, SecurityPageInteraction, TrustSafetyInteraction} from './hats_browser_proxy.js';
 export {loadTimeData} from './i18n_setup.js';
-export {CardBenefitsUserAction, CvcDeletionUserAction, DeleteBrowsingDataAction, MetricsBrowserProxy, MetricsBrowserProxyImpl, PrivacyElementInteractions, PrivacyGuideInteractions, PrivacyGuideSettingsStates, PrivacyGuideStepsEligibleAndReached, SafeBrowsingInteractions, SafetyCheckInteractions, SafetyCheckNotificationsModuleInteractions, SafetyCheckUnusedSitePermissionsModuleInteractions, SafetyHubCardState, SafetyHubEntryPoint, SafetyHubModuleType, SafetyHubSurfaces} from './metrics_browser_proxy.js';
+export {AiPageInteractions, CardBenefitsUserAction, CvcDeletionUserAction, DeleteBrowsingDataAction, MetricsBrowserProxy, MetricsBrowserProxyImpl, PrivacyElementInteractions, PrivacyGuideInteractions, PrivacyGuideSettingsStates, PrivacyGuideStepsEligibleAndReached, SafeBrowsingInteractions, SafetyCheckInteractions, SafetyCheckNotificationsModuleInteractions, SafetyCheckUnusedSitePermissionsModuleInteractions, SafetyHubCardState, SafetyHubEntryPoint, SafetyHubModuleType, SafetyHubSurfaces} from './metrics_browser_proxy.js';
 export {NtpExtension, OnStartupBrowserProxy, OnStartupBrowserProxyImpl} from './on_startup_page/on_startup_browser_proxy.js';
 export {SettingsOnStartupPageElement} from './on_startup_page/on_startup_page.js';
 export {SettingsStartupUrlDialogElement} from './on_startup_page/startup_url_dialog.js';
