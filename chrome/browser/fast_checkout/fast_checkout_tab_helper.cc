978e8e135343c52e463e88ee17e4362d732bdbcc
Minoru Chikamune <chikamune@chromium.org>
2024-10-17 07:55:02 +0000

    Run FastCheckoutClient later
    We observed that FastCheckoutTabHelper::DidStartNavigation delays
    navigation on the navigation critical path on Android. The first
    navigation is especially delayed around 3 ms. This CL mitigate the delay
    by posting a heavy task to process it later.
    
    Before this CL (delays 3.6ms):
    https://ui.perfetto.dev/#!/?s=3ce6f2c879d4ca17ff436c49005bf72dcb97c581
    
    After this CL (delays 6us):
    https://ui.perfetto.dev/#!/?s=7717cf3e813d0080a5f7efb027c9b81da809c8ee
    
    Bug: 373908102
    Change-Id: I505e38d9d2bc024281b3b80097db7f8cc09c9b45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939555
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369840}

--- chrome/browser/fast_checkout/fast_checkout_tab_helper.cc
+++ chrome/browser/fast_checkout/fast_checkout_tab_helper.cc
@@ -11,16 +11,24 @@
 #include "chrome/browser/profiles/profile.h"
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/commerce/core/heuristics/commerce_heuristics_provider.h"
+#include "components/google/core/common/google_util.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 namespace {
+
+BASE_FEATURE(kAsyncFastCheckout,
+             "AsyncFastCheckout",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsCartOrCheckoutUrl(const GURL& url) {
   return commerce_heuristics::IsVisitCheckout(url) ||
          commerce_heuristics::IsVisitCart(url);
 }
+
 }  // namespace
 
 FastCheckoutTabHelper::FastCheckoutTabHelper(content::WebContents* web_contents)
@@ -45,6 +53,25 @@ void FastCheckoutTabHelper::DidStartNavigation(
     return;
   }
 
+  if (base::FeatureList::IsEnabled(kAsyncFastCheckout)) {
+    // DidStartNavigation() locates on the navigation critical path. To avoid
+    // blocking navigation, we run DidStartNavigationImpl asynchronously.
+    //
+    // GoogleSearch is not the target of the Fast Checkout.
+    if (!google_util::IsGoogleSearchUrl(url)) {
+      content::GetUIThreadTaskRunner({base::TaskPriority::BEST_EFFORT})
+          ->PostTask(
+              FROM_HERE,
+              base::BindOnce(&FastCheckoutTabHelper::DidStartNavigationImpl,
+                             weak_ptr_factory_.GetWeakPtr(), std::move(url)));
+    }
+  } else {
+    DidStartNavigationImpl(url);
+  }
+}
+
+void FastCheckoutTabHelper::DidStartNavigationImpl(const GURL& url) {
+  TRACE_EVENT("navigation", "FastCheckoutTabHelper::DidStartNavigationImpl");
   FetchCapabilities(url);
   if (autofill::ContentAutofillClient* client =
           autofill::ContentAutofillClient::FromWebContents(web_contents())) {
@@ -55,6 +82,7 @@ void FastCheckoutTabHelper::DidStartNavigation(
 }
 
 void FastCheckoutTabHelper::FetchCapabilities(const GURL& url) {
+  TRACE_EVENT("navigation", "FastCheckoutTabHelper::FetchCapabilities");
   // Check for both checkout and cart URLs because some websites use cart URLs
   // throughout their whole checkout funnel.
   if (IsCartOrCheckoutUrl(url)) {
