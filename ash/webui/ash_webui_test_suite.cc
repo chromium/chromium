cd9e96131474ce38e4d331558831eeb16b8db988
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2024-10-17 01:22:46 +0000

    Reland "[views-ax] Making Widget an AXModeObserver"
    This is a reland of commit a45292dec78890c13ac6e063930bd3cd8dfe9671
    
    It was reverted after a bot that does not run on the CQ started
    failing. After looking into it the issue was that
    chromecast/graphics tests did not have an AXPlatform instance
    initialized. As such, I added it and updated the deps/build files.
    
    I manually added the bot to run and it now passes.
    
    Original change's description:
    > [views-ax] Making Widget an AXModeObserver
    >
    > This CL makes Widget an AXModeObserver, this is so we can listen
    > to when accessibility gets turned on with the kNativeAPIs AXMode,
    > at which point we can complete the cache initialization for all
    > the descendant views of that Widget.
    >
    > This allows Views authors the ability to offload the computation
    > of expensive accessibility attributes until we know for sure
    > that accessibility is "on". In order for Views authors to make
    > use of this, the View in question needs to override
    > `OnAccessibilityInitializing`. See the comment on view.h for the
    > `OnAccessibilityInitializing` function for more detail on how to
    > do this.
    >
    > This required changes to test files and their deps, this is because
    > not all test suites initialize an AXPlatform. This is now required
    > since now we will be checking for the AXMode (checking if a11y is
    > enabled) anytime we add a View as a child. This is done through
    > the AXPlatform singleton. There were some test suites that then
    > were crashing since we were trying to access the AXPlatform
    > singleton and since none was initialized for those test suites
    > they would crash. As a result, changes to these test suites were
    > needed so they could initialize an AXPlatform for tests.
    >
    > This CL is part of the ViewsAX project:
    > https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    >
    > Bug: 325137417
    > Change-Id: I66e6affdcca5def5d8a31d5f681a257b05cf3343
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5886957
    > Reviewed-by: Jacques Newman <janewman@microsoft.com>
    > Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    > Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1367799}
    
    Bug: 325137417
    Change-Id: I9d6144b5be56f414460e024d66610d144d0b1ed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5935144
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Antonio Rivera <antoniori@google.com>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1369755}

--- ash/webui/ash_webui_test_suite.cc
+++ ash/webui/ash_webui_test_suite.cc
@@ -9,6 +9,7 @@
 #include "base/i18n/rtl.h"
 #include "base/path_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/accessibility/platform/provide_ax_platform_for_tests.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/gl/test/gl_surface_test_support.h"
@@ -44,6 +45,9 @@ AshWebUITestSuite::~AshWebUITestSuite() = default;
 
 void AshWebUITestSuite::Initialize() {
   base::TestSuite::Initialize();
+
+  testing::UnitTest::GetInstance()->listeners().Append(
+      new ui::ProvideAXPlatformForTests());
 
   gl::GLSurfaceTestSupport::InitializeOneOff();
 
