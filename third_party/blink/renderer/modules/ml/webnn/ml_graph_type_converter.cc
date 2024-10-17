1d33271e936275098e0988e53f42d6d1d3adef13
Peter Kasting <pkasting@chromium.org>
2024-10-17 06:36:08 +0000

    Fix unsafe usage of spans to out-of-lifetime initializer lists.
    The existing code returned and stored spans by value that were created
    directly from initializer lists. Those initializer lists are only alive
    until the end of the expression, and the underlying data is not
    guaranteed by the spec to be present in the binary or in any other way
    alive and safe-to-access. It's possible this has been working in
    practice because the compiler decided to put these values in .rodata or
    something, but it doesn't have to. (I had a long email conversation with
    Arthur O'Dwyer a.k.a. quuxplusone about this topic where he provided
    multiple examples to demonstrate.)
    
    Instead, use an owning type (in this case, std::array), so the lifetimes
    are clear and managed.
    
    code as part of fixing errors uncovered by span API changes)
    
    Bug: 364987728
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR (changing type in existing
    Change-Id: Ic3e31ff03ddf3fbb5e98ad498537edcf4939aa40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937579
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369809}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_type_converter.cc
@@ -428,7 +428,7 @@ std::optional<base::span<const uint32_t>> GetOutputOperandPermutation(
   }
 }
 
-std::optional<base::span<const uint32_t>> GetConv2DFilterPermutation(
+std::optional<std::array<uint32_t, 4>> GetConv2DFilterPermutation(
     webnn::InputOperandLayout input_layout,
     bool depthwise,
     blink::V8MLConv2dFilterOperandLayout filter_layout) {
@@ -439,11 +439,11 @@ std::optional<base::span<const uint32_t>> GetConv2DFilterPermutation(
         case blink::V8MLConv2dFilterOperandLayout::Enum::kOihw:
           return std::nullopt;
         case blink::V8MLConv2dFilterOperandLayout::Enum::kHwio:
-          return base::span({3u, 2u, 0u, 1u});
+          return std::to_array<uint32_t>({3u, 2u, 0u, 1u});
         case blink::V8MLConv2dFilterOperandLayout::Enum::kOhwi:
-          return base::span({0u, 3u, 1u, 2u});
+          return std::to_array<uint32_t>({0u, 3u, 1u, 2u});
         case blink::V8MLConv2dFilterOperandLayout::Enum::kIhwo:
-          return base::span({3u, 0u, 1u, 2u});
+          return std::to_array<uint32_t>({3u, 0u, 1u, 2u});
       }
       break;
     case webnn::InputOperandLayout::kNhwc:
@@ -451,11 +451,11 @@ std::optional<base::span<const uint32_t>> GetConv2DFilterPermutation(
         // Mojo expects the IHWO layout.
         switch (filter_layout.AsEnum()) {
           case blink::V8MLConv2dFilterOperandLayout::Enum::kOihw:
-            return base::span({1u, 2u, 3u, 0u});
+            return std::to_array<uint32_t>({1u, 2u, 3u, 0u});
           case blink::V8MLConv2dFilterOperandLayout::Enum::kHwio:
-            return base::span({2u, 0u, 1u, 3u});
+            return std::to_array<uint32_t>({2u, 0u, 1u, 3u});
           case blink::V8MLConv2dFilterOperandLayout::Enum::kOhwi:
-            return base::span({3u, 1u, 2u, 0u});
+            return std::to_array<uint32_t>({3u, 1u, 2u, 0u});
           case blink::V8MLConv2dFilterOperandLayout::Enum::kIhwo:
             return std::nullopt;
         }
@@ -463,20 +463,20 @@ std::optional<base::span<const uint32_t>> GetConv2DFilterPermutation(
         switch (filter_layout.AsEnum()) {
           // Mojo expects the OHWI layout.
           case blink::V8MLConv2dFilterOperandLayout::Enum::kOihw:
-            return base::span({0u, 2u, 3u, 1u});
+            return std::to_array<uint32_t>({0u, 2u, 3u, 1u});
           case blink::V8MLConv2dFilterOperandLayout::Enum::kHwio:
-            return base::span({3u, 0u, 1u, 2u});
+            return std::to_array<uint32_t>({3u, 0u, 1u, 2u});
           case blink::V8MLConv2dFilterOperandLayout::Enum::kOhwi:
             return std::nullopt;
           case blink::V8MLConv2dFilterOperandLayout::Enum::kIhwo:
-            return base::span({3u, 1u, 2u, 0u});
+            return std::to_array<uint32_t>({3u, 1u, 2u, 0u});
         }
       }
       break;
   }
 }
 
-std::optional<base::span<const uint32_t>> GetConvTranspose2DFilterPermutation(
+std::optional<std::array<uint32_t, 4>> GetConvTranspose2DFilterPermutation(
     webnn::InputOperandLayout input_layout,
     blink::V8MLConvTranspose2dFilterOperandLayout filter_layout) {
   switch (input_layout) {
@@ -486,18 +486,18 @@ std::optional<base::span<const uint32_t>> GetConvTranspose2DFilterPermutation(
         case blink::V8MLConvTranspose2dFilterOperandLayout::Enum::kIohw:
           return std::nullopt;
         case blink::V8MLConvTranspose2dFilterOperandLayout::Enum::kHwoi:
-          return base::span({3u, 2u, 0u, 1u});
+          return std::to_array<uint32_t>({3, 2, 0, 1});
         case blink::V8MLConvTranspose2dFilterOperandLayout::Enum::kOhwi:
-          return base::span({3u, 0u, 1u, 2u});
+          return std::to_array<uint32_t>({3u, 0u, 1u, 2u});
       }
       break;
     case webnn::InputOperandLayout::kNhwc:
       // Mojo expects OHWI layout.
       switch (filter_layout.AsEnum()) {
         case blink::V8MLConvTranspose2dFilterOperandLayout::Enum::kIohw:
-          return base::span({1u, 2u, 3u, 0u});
+          return std::to_array<uint32_t>({1u, 2u, 3u, 0u});
         case blink::V8MLConvTranspose2dFilterOperandLayout::Enum::kHwoi:
-          return base::span({2u, 0u, 1u, 3u});
+          return std::to_array<uint32_t>({2u, 0u, 1u, 3u});
         case blink::V8MLConvTranspose2dFilterOperandLayout::Enum::kOhwi:
           return std::nullopt;
       }
@@ -702,7 +702,7 @@ std::optional<String> SerializeConv2dOperation(
   conv2d_mojo->output_operand_id = output_operand_id;
 
   const MLOperand* filter_operand = conv2d->Inputs()[1];
-  std::optional<base::span<const uint32_t>> filter_permutation;
+  std::optional<std::array<uint32_t, 4>> filter_permutation;
 
   if constexpr (std::is_same<MLConv2dOptionsType, MLConv2dOptions>::value) {
     conv2d_mojo->kind = blink_mojom::Conv2d::Kind::kDirect;
