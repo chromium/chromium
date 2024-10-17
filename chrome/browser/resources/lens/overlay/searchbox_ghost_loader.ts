d3d026892b86812f6864a49267958ce868568419
dpapad <dpapad@chromium.org>
2024-10-17 20:19:22 +0000

    Lens: Replace paper-spinner-lite with non-Polymer alternative.
    This is part of the effort to remove paper-spinner-lite from Desktop
    builds.
    
    Bug: 40943652
    Change-Id: I8faf6be8953664252f1f786cec721ff3231ab587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938254
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370220}

--- chrome/browser/resources/lens/overlay/searchbox_ghost_loader.ts
+++ chrome/browser/resources/lens/overlay/searchbox_ghost_loader.ts
@@ -2,29 +2,21 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_shared_style.css.js';
-import '//resources/polymer/v3_0/paper-spinner/paper-spinner-lite.js';
+import '//resources/cr_elements/cr_spinner_style.css.js';
 import '/lens/shared/searchbox_shared_style.css.js';
 
-import {I18nMixin} from '//resources/cr_elements/i18n_mixin.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './searchbox_ghost_loader.html.js';
 
-const SearchboxGhostLoaderElementBase = I18nMixin(PolymerElement);
-
 // Displays a loading preview while waiting on autocomplete to return matches.
-class SearchboxGhostLoaderElement extends SearchboxGhostLoaderElementBase {
+class SearchboxGhostLoaderElement extends PolymerElement {
   static get is() {
     return 'cr-searchbox-ghost-loader';
   }
 
   static get template() {
     return getTemplate();
-  }
-
-  static get properties() {
-    return {};
   }
 }
 
