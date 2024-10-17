accb40c49569b75b897c3ffdae91b115fbaa1df9
Daniel White <danieltwhite@google.com>
2024-10-17 18:38:06 +0000

    [Code Health][iOS] Move i/c/b/ui/print to coordinator
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/coordinator/ to showcase that they are model files.
    
    Bug: 40223315
    Change-Id: I9c63d1482eab749983cbfd32c02e33e9e0c7924b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905190
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370161}

--- ios/chrome/browser/print/coordinator/print_coordinator.mm
+++ ios/chrome/browser/print/coordinator/print_coordinator.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/print/print_coordinator.h"
+#import "ios/chrome/browser/print/coordinator/print_coordinator.h"
 
 #import "base/logging.h"
 #import "base/metrics/user_metrics.h"
