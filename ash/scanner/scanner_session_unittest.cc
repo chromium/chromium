d73db78b01e10771555607b0e4950d2b5601584b
mlcui <mlcui@google.com>
2024-10-17 06:41:40 +0000

    scanner: Add session integration tests for running actions
    Bug: b:363101106
    Change-Id: I6a6a2100c3af07f3258603930cf14230599160de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933491
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369813}

--- ash/scanner/scanner_session_unittest.cc
+++ ash/scanner/scanner_session_unittest.cc
@@ -8,6 +8,7 @@
 #include <utility>
 #include <vector>
 
+#include "ash/public/cpp/test/test_new_window_delegate.h"
 #include "ash/scanner/fake_scanner_profile_scoped_delegate.h"
 #include "ash/scanner/scanner_action_view_model.h"
 #include "base/test/test_future.h"
@@ -15,12 +16,23 @@
 #include "components/manta/proto/scanner.pb.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
 
 namespace ash {
 namespace {
 
+using ::testing::_;
 using ::testing::IsEmpty;
+using ::testing::Property;
 using ::testing::SizeIs;
+
+class MockNewWindowDelegate : public TestNewWindowDelegate {
+ public:
+  MOCK_METHOD(void,
+              OpenUrl,
+              (const GURL& url, OpenUrlFrom from, Disposition disposition),
+              (override));
+};
 
 TEST(ScannerSessionTest, FetchActionsForImageReturnsEmptyWhenDelegateErrors) {
   FakeScannerProfileScopedDelegate delegate;
@@ -68,6 +80,81 @@ TEST(ScannerSessionTest,
       manta::MantaStatus{.status_code = manta::MantaStatusCode::kOk});
 
   EXPECT_THAT(future.Take(), SizeIs(3));
+}
+
+TEST(ScannerSessionTest, RunningNewEventActionOpensUrl) {
+  MockNewWindowDelegate new_window_delegate;
+  EXPECT_CALL(new_window_delegate,
+              OpenUrl(Property("spec", &GURL::spec,
+                               "https://calendar.google.com/calendar/render"
+                               "?action=TEMPLATE"
+                               "&text=%F0%9F%8C%8F"
+                               "&details=formerly+%22Geo+Sync%22"
+                               "&dates=20241014T160000%2F20241014T161500"
+                               "&location=Wonderland"),
+                      _, _))
+      .Times(1);
+  FakeScannerProfileScopedDelegate delegate;
+  ScannerSession session(&delegate);
+  manta::proto::NewEventAction event_action;
+  event_action.set_title("🌏");
+  event_action.set_description("formerly \"Geo Sync\"");
+  event_action.set_dates("20241014T160000/20241014T161500");
+  event_action.set_location("Wonderland");
+
+  base::test::TestFuture<std::vector<ScannerActionViewModel>> future;
+  session.FetchActionsForImage(nullptr, future.GetCallback());
+  auto output = std::make_unique<manta::proto::ScannerOutput>();
+  *output->add_objects()->add_actions()->mutable_new_event() =
+      std::move(event_action);
+  delegate.SendFakeActionsResponse(
+      std::move(output),
+      manta::MantaStatus{.status_code = manta::MantaStatusCode::kOk});
+  std::vector<ScannerActionViewModel> actions = future.Take();
+  ASSERT_THAT(actions, SizeIs(1));
+  base::test::TestFuture<bool> action_finished_future;
+  std::move(actions.front())
+      .ToCallback(action_finished_future.GetCallback())
+      .Run();
+
+  EXPECT_TRUE(action_finished_future.Get());
+}
+
+TEST(ScannerSessionTest, RunningNewContactActionOpensUrl) {
+  MockNewWindowDelegate new_window_delegate;
+  EXPECT_CALL(new_window_delegate,
+              OpenUrl(Property("spec", &GURL::spec,
+                               "https://contacts.google.com/new"
+                               "?givenname=Andr%C3%A9"
+                               "&familyname=Fran%C3%A7ois"
+                               "&email=afrancois%40example.com"
+                               "&phone=%2B61400000000"),
+                      _, _))
+      .Times(1);
+  FakeScannerProfileScopedDelegate delegate;
+  ScannerSession session(&delegate);
+  manta::proto::NewContactAction contact_action;
+  contact_action.set_given_name("André");
+  contact_action.set_family_name("François");
+  contact_action.set_email("afrancois@example.com");
+  contact_action.set_phone("+61400000000");
+
+  base::test::TestFuture<std::vector<ScannerActionViewModel>> future;
+  session.FetchActionsForImage(nullptr, future.GetCallback());
+  auto output = std::make_unique<manta::proto::ScannerOutput>();
+  *output->add_objects()->add_actions()->mutable_new_contact() =
+      std::move(contact_action);
+  delegate.SendFakeActionsResponse(
+      std::move(output),
+      manta::MantaStatus{.status_code = manta::MantaStatusCode::kOk});
+  std::vector<ScannerActionViewModel> actions = future.Take();
+  ASSERT_THAT(actions, SizeIs(1));
+  base::test::TestFuture<bool> action_finished_future;
+  std::move(actions.front())
+      .ToCallback(action_finished_future.GetCallback())
+      .Run();
+
+  EXPECT_TRUE(action_finished_future.Get());
 }
 
 }  // namespace
