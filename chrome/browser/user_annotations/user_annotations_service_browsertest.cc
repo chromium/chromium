ba1e63dadaafd1beecd6c1e13cb21123e3c3c548
Sophie Chang <sophiechang@chromium.org>
2024-10-18 21:50:20 +0000

    Disable flaky user annotations service browser tests
    Bug: 367201367
    Change-Id: Ie976227d3dbb935c2f4a01948ca43da47d5b775d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943562
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370846}

--- chrome/browser/user_annotations/user_annotations_service_browsertest.cc
+++ chrome/browser/user_annotations/user_annotations_service_browsertest.cc
@@ -201,13 +201,8 @@ IN_PROC_BROWSER_TEST_F(UserAnnotationsServiceBrowserTest,
 
 // TODO(crbug.com/367201367):  Re-enable once flakiness is resolved for Windows
 // ASAN. Also flaky on Mac.
-#if (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)) || BUILDFLAG(IS_MAC)
-#define MAYBE_FormSubmissionFlow DISABLED_FormSubmissionFlow
-#else
-#define MAYBE_FormSubmissionFlow FormSubmissionFlow
-#endif
 IN_PROC_BROWSER_TEST_F(UserAnnotationsServiceBrowserTest,
-                       MAYBE_FormSubmissionFlow) {
+                       DISABLED_FormSubmissionFlow) {
   EnableSignin();
 
   base::HistogramTester histogram_tester;
@@ -283,8 +278,10 @@ IN_PROC_BROWSER_TEST_F(UserAnnotationsServiceExplicitAllowlistBrowserTest,
                                     0);
 }
 
+// TODO(crbug.com/367201367):  Re-enable once flakiness is resolved for Windows
+// ASAN. Also flaky on Mac.
 IN_PROC_BROWSER_TEST_F(UserAnnotationsServiceExplicitAllowlistBrowserTest,
-                       OnAllowlist) {
+                       DISABLED_OnAllowlist) {
   EnableSignin();
 
   base::HistogramTester histogram_tester;
