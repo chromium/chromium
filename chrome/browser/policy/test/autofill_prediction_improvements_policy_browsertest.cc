18bac6aa51d6600a020cc0340769197cdff63676
Sophie Chang <sophiechang@chromium.org>
2024-10-19 17:36:47 +0000

    Enable sign in on the autofill prediction improvements enterprise policy browsertest
    Bug: 373532716
    Change-Id: I46512d9be3a4fb3c302ddc4432e602d12016f6d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5944062
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Anqing Zhao <anqing@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371023}

--- chrome/browser/policy/test/autofill_prediction_improvements_policy_browsertest.cc
+++ chrome/browser/policy/test/autofill_prediction_improvements_policy_browsertest.cc
@@ -10,6 +10,7 @@
 #include "chrome/browser/autofill_prediction_improvements/chrome_autofill_prediction_improvements_client.h"
 #include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -19,6 +20,8 @@
 #include "components/password_manager/core/browser/password_autofill_manager.h"
 #include "components/policy/core/common/policy_map.h"
 #include "components/policy/policy_constants.h"
+#include "components/signin/public/base/signin_switches.h"
+#include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
@@ -48,7 +51,13 @@ class AutofillPredictionImprovementsPolicyTest
   bool disabled_by_policy() const { return policy_value() == kDisable; }
 
   void SetUpOnMainThread() override {
-    InProcessBrowserTest::SetUpOnMainThread();
+    PolicyTest::SetUpOnMainThread();
+
+    identity_test_env_adaptor_ =
+        std::make_unique<IdentityTestEnvironmentProfileAdaptor>(
+            browser()->profile());
+
+    EnableSignin();
 
     PolicyMap policies;
     SetPolicy(&policies, key::kAutofillPredictionSettings,
@@ -65,9 +74,43 @@ class AutofillPredictionImprovementsPolicyTest
     return browser()->tab_strip_model()->GetActiveWebContents();
   }
 
+  void SetUpInProcessBrowserTestFixture() override {
+    PolicyTest::SetUpInProcessBrowserTestFixture();
+
+    create_services_subscription_ =
+        BrowserContextDependencyManager::GetInstance()
+            ->RegisterCreateServicesCallbackForTesting(
+                base::BindRepeating(&AutofillPredictionImprovementsPolicyTest::
+                                        OnWillCreateBrowserContextServices,
+                                    base::Unretained(this)));
+  }
+
  private:
+  void EnableSignin() {
+    auto account_info =
+        identity_test_env_adaptor_->identity_test_env()
+            ->MakePrimaryAccountAvailable("user@gmail.com",
+                                          signin::ConsentLevel::kSignin);
+    AccountCapabilitiesTestMutator mutator(&account_info.capabilities);
+    mutator.set_can_use_model_execution_features(true);
+    identity_test_env_adaptor_->identity_test_env()
+        ->UpdateAccountInfoForAccount(account_info);
+    identity_test_env_adaptor_->identity_test_env()
+        ->SetAutomaticIssueOfAccessTokens(true);
+  }
+
+  void OnWillCreateBrowserContextServices(content::BrowserContext* context) {
+    IdentityTestEnvironmentProfileAdaptor::
+        SetIdentityTestEnvironmentFactoriesOnBrowserContext(context);
+  }
+
   base::test::ScopedFeatureList scoped_feature_list_{
       autofill_prediction_improvements::kAutofillPredictionImprovements};
+
+  // Identity test support.
+  std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
+      identity_test_env_adaptor_;
+  base::CallbackListSubscription create_services_subscription_;
 };
 
 INSTANTIATE_TEST_SUITE_P(,
