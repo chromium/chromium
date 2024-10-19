a3322d537f6b4d28d407a31506021eafffd8eb2d
Khushal Sagar <khushalsagar@chromium.org>
2024-10-19 00:14:57 +0000

    chrome: Fix TabOnBackGestureHandler lifetime
    The handler shouldn't outlive its tab but we currently miss resetting it
    if the tab changes mid gesture. This change clears it if the tab changes
    and dispatches a synthetic onBackCancelled to ensure the handler sees
    the expected event stream in case the previous tab is restored.
    
    We were also not cancelling the gesture for SwipeRefreshHandler reset
    case.
    
    Bug: 373617224
    Change-Id: I4e38c2f27d086610449e7ba21c09d60720d7dc11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943065
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370908}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -277,6 +277,7 @@ public class ToolbarManager
     private final TabObscuringHandler mTabObscuringHandler;
     private ToolbarDragDropCoordinator mToolbarDragDropCoordinator;
     private OnAttachStateChangeListener mAttachStateChangeListener;
+    private final BackPressHandler mBackPressHandler;
     private final BackPressManager mBackPressManager;
     private final UserEducationHelper mUserEducationHelper;
     private final ToolbarLongPressMenuHandler mToolbarLongPressMenuHandler;
@@ -708,10 +709,11 @@ public class ToolbarManager
                         this::updateButtonStatus,
                         mActivityTabProvider);
         if (backPressManager != null && BackPressManager.isEnabled()) {
-            OnBackPressHandler handler = new OnBackPressHandler();
-            backPressManager.addHandler(handler, BackPressHandler.Type.TAB_HISTORY);
+            mBackPressHandler = new OnBackPressHandler();
+            backPressManager.addHandler(mBackPressHandler, BackPressHandler.Type.TAB_HISTORY);
             mBackPressManager = backPressManager;
         } else {
+            mBackPressHandler = null;
             mBackPressManager = null;
         }
 
@@ -2339,6 +2341,8 @@ public class ToolbarManager
         assert profile != null
                 : "Failed to get Profile when offTheRecord = "
                         + mTabModelSelector.isOffTheRecordModelSelected();
+
+        if (mBackPressHandler != null) mBackPressHandler.handleOnBackCancelled();
 
         mLocationBarModel.setTab(tab, profile);
         updateTabLoadingState(true);
