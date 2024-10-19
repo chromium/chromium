9a0b86ea08952f0cd278e394925ed54a8ab07642
Sophie Chang <sophiechang@chromium.org>
2024-10-19 18:31:19 +0000

    Reland "Reland "Reland Switch off of user annotations feature flag in favor of shared autofill prediction improvements flag""
    This is a reland of commit c719b7bbafa2102e9c493c284e645e3553f9bf34
    
    Original change's description:
    > Reland "Reland Switch off of user annotations feature flag in favor of shared autofill prediction improvements flag"
    >
    > This is a reland of commit 1367bb3882cb79d154c7cbb2041fefbe734343ac
    >
    > Original change's description:
    > > Reland Switch off of user annotations feature flag in favor of shared autofill prediction improvements flag
    > >
    > > Bug: 373532716
    > > Change-Id: Id7b955952560858ed0cae4532963cd9a0c98d70e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941358
    > > Reviewed-by: Raj T <rajendrant@chromium.org>
    > > Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1370275}
    >
    > Bug: 373532716
    > Change-Id: If0cd52198ed4d3ca570bfb7502915833c0cb6018
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941574
    > Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Wolfgang Billenstein <bwolfgang@google.com>
    > Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    > Reviewed-by: Raj T <rajendrant@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1370732}
    
    Bug: 373532716
    Change-Id: Iac17c813c75ae85dcc0b945bf676ba34be7a7fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943668
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371027}

--- components/user_annotations/user_annotations_features_unittest.cc
+++ components/user_annotations/user_annotations_features_unittest.cc
@@ -5,6 +5,7 @@
 #include "components/user_annotations/user_annotations_features.h"
 
 #include "base/test/scoped_feature_list.h"
+#include "components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_features.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -17,7 +18,7 @@ using ::testing::UnorderedElementsAre;
 TEST(UserAnnotationsFeaturesTest, GetAllowedHostsForFormsAnnotations) {
   base::test::ScopedFeatureList scoped_feature_list;
   scoped_feature_list.InitAndEnableFeatureWithParameters(
-      kUserAnnotations,
+      autofill_prediction_improvements::kAutofillPredictionImprovements,
       {{"allowed_hosts_for_form_submissions", "example.com,otherhost.com"}});
   EXPECT_THAT(GetAllowedHostsForFormsAnnotations(),
               UnorderedElementsAre("example.com", "otherhost.com"));
