295b81f83a7e2c7d7dcb7ce9d19b2079b30e83b2
James Cook <jamescook@chromium.org>
2024-10-21 21:11:31 +0000

    extensions: Introduce ExtensionPlatformBrowserTest
    It supports tests on desktop Android. This is a step towards running
    extensions API tests on desktop Android.
    
    Bug: 373434594
    Test: compiles
    Change-Id: Ifcbf318a8e141354328118c24f80be348fb369df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943660
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371652}

---
+++ chrome/browser/extensions/extension_platform_browsertest.cc
@@ -0,0 +1,56 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/extension_platform_browsertest.h"
+
+#include "base/path_service.h"
+#include "chrome/browser/extensions/platform_test_extension_loader.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_paths.h"
+#include "chrome/test/base/chrome_test_utils.h"
+
+namespace extensions {
+
+ExtensionPlatformBrowserTest::ExtensionPlatformBrowserTest(
+    ContextType context_type)
+    : context_type_(context_type) {}
+
+ExtensionPlatformBrowserTest::~ExtensionPlatformBrowserTest() = default;
+
+void ExtensionPlatformBrowserTest::SetUpCommandLine(
+    base::CommandLine* command_line) {
+  // TODO(crbug.com/373434594): Move this to SetUpOnMainThread()?
+  base::PathService::Get(chrome::DIR_TEST_DATA, &test_data_dir_);
+  test_data_dir_ = test_data_dir_.AppendASCII("extensions");
+}
+
+const Extension* ExtensionPlatformBrowserTest::LoadExtension(
+    const base::FilePath& path,
+    const LoadOptions& options) {
+  base::ScopedAllowBlockingForTesting scoped_allow_blocking;
+  base::FilePath test_dir;
+  base::PathService::Get(chrome::DIR_TEST_DATA, &test_dir);
+  base::FilePath extension_path = test_dir.Append(path);
+
+  Profile* profile = chrome_test_utils::GetProfile(this);
+  PlatformTestExtensionLoader loader(profile);
+  loader.set_allow_incognito_access(options.allow_in_incognito);
+  loader.set_allow_file_access(options.allow_file_access);
+  loader.set_ignore_manifest_warnings(options.ignore_manifest_warnings);
+  loader.set_wait_for_renderers(options.wait_for_renderers);
+
+  if (options.install_param != nullptr) {
+    loader.set_install_param(options.install_param);
+  }
+
+  // TODO(crbug.com/373434594): Support TestServiceWorkerContextObserver for the
+  // wait_for_registration_stored option.
+  CHECK(!options.wait_for_registration_stored);
+
+  scoped_refptr<const Extension> extension =
+      loader.LoadExtension(extension_path);
+  return extension.get();
+}
+
+}  // namespace extensions
