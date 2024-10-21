1820513529a37e7cd86e9488174cead8f8cf760a
François Doray <fdoray@chromium.org>
2024-10-21 15:09:31 +0000

    Re-enable PageDiscardingHelperBrowserTest.DiscardSpecificPage.
    The test was disabled due to flakiness, but the CL that caused the
    flakiness has been reverted, so the test can be re-enabled (see bug).
    
    Bug: 348648417
    Change-Id: I83a641e74b43776b927bc0986b8387f7f7b425da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926055
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371385}

--- chrome/browser/performance_manager/policies/page_discarding_helper_browsertest.cc
+++ chrome/browser/performance_manager/policies/page_discarding_helper_browsertest.cc
@@ -160,8 +160,7 @@ class PageDiscardingHelperBrowserTest : public InProcessBrowserTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(PageDiscardingHelperBrowserTest,
-                       DISABLED_DiscardSpecificPage) {
+IN_PROC_BROWSER_TEST_F(PageDiscardingHelperBrowserTest, DiscardSpecificPage) {
   // Test urgent and proactive discards in a loop to avoid the overhead of
   // starting a new browser every time.
   // TODO(crbug.com/40899366): Add tests for all the other heuristics in
