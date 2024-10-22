08bd17e0488425adbd1392fa44075f94d61f2f0c
Mohannad Farrag <aymanm@google.com>
2024-10-22 12:19:55 +0000

    Add gen_android_bp tests
    Those are some tests that were added to gn2bp in AOSP, this CL simply
    upstream the AOSP version. [aosp/3284296]
    
    Enabled PRESUBMIT to run on commit to make sure that the code does not
    rot.
    
    Bug: 349074194
    Fixed: 349074194
    Change-Id: I09d905da81f145073c5e6856835721e0ffae29b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5934567
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1371971}

--- components/cronet/gn2bp/PRESUBMIT.py
+++ components/cronet/gn2bp/PRESUBMIT.py
@@ -10,24 +10,15 @@ details on the presubmit API built into depot_tools.
 # See third_party/depot_tools/presubmit_support.py
 PRESUBMIT_VERSION = '2.0.0'
 
-# TODO(b/349074194): Set to True once rust has been enabled for Cronet.
-_ENABLE_PRESUBMIT = False
-
 
 def CheckGenerateBuildScriptsOutput(input_api, output_api):
     presubmit_script_path = input_api.PresubmitLocalPath()
-    if _ENABLE_PRESUBMIT:
-        return input_api.RunTests(
-            input_api.canned_checks.GetUnitTests(
-                input_api,
-                output_api,
-                unit_tests=[
-                    input_api.os_path.join(
-                        presubmit_script_path, 'tests',
-                        'generate_build_scripts_output_test.py'),
-                    input_api.os_path.join(presubmit_script_path, 'tests',
-                                           'gen_android_bp_test.py')
-                ],
-            ))
-    else:
-        return ()
+    return input_api.RunTests(
+        input_api.canned_checks.GetUnitTests(
+            input_api,
+            output_api,
+            unit_tests=[
+                input_api.os_path.join(presubmit_script_path, 'tests',
+                                       'gen_android_bp_test.py')
+            ],
+        ))
