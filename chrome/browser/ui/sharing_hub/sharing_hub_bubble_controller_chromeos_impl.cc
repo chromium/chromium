a97b9cd6430302594df422f369ff9cd7a190e64f
Antonio Gomes <tonikitoo@igalia.com>
2024-10-22 18:20:30 +0000

    Clean lacros-specific code in chrome/browser/ui/sharing_hub
    This CL deletes the lacros-only code in c/b/ui/sharing_hub/
    directory, now that the project has been terminated.
    
    Particularly, the removals in this directory happens in two files,
    sharing_hub_bubble_controller_chromeos_impl.cc|h, and do:
    - deletion of code guarded by BUILDFLAG(IS_CHROMEOS_LACROS).
    - deletion of the remaining BUILDFLAG(IS_CHROMEOS_ASH) guards
      but not its block code inside.
    - removal of inclusions of `build/chromeos_buildflags.h`.
    
    R=hidehiko@chromium.org, neis@chromium.org
    
    Bug: 374890766, 373971535
    Change-Id: Ieb823d3918387f29f9b63da1c6e593fbdb4e2706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5952190
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1372187}

--- chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller_chromeos_impl.cc
+++ chrome/browser/ui/sharing_hub/sharing_hub_bubble_controller_chromeos_impl.cc
@@ -8,10 +8,11 @@
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/user_metrics.h"
 #include "base/strings/utf_string_conversions.h"
-#include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/share/share_metrics.h"
 #include "chrome/browser/sharesheet/sharesheet_metrics.h"
+#include "chrome/browser/sharesheet/sharesheet_service.h"
+#include "chrome/browser/sharesheet/sharesheet_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -20,38 +21,7 @@
 #include "content/public/browser/web_contents.h"
 #include "ui/views/controls/button/button.h"
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/sharesheet/sharesheet_service.h"
-#include "chrome/browser/sharesheet/sharesheet_service_factory.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chrome/browser/ui/lacros/window_utility.h"
-#include "chromeos/crosapi/mojom/app_service_types.mojom.h"
-#include "chromeos/crosapi/mojom/sharesheet.mojom.h"
-#include "chromeos/crosapi/mojom/sharesheet_mojom_traits.h"
-#include "chromeos/lacros/lacros_service.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
-
 namespace sharing_hub {
-
-namespace {
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-crosapi::mojom::IntentPtr CreateCrosapiShareIntent(
-    const std::string& share_text,
-    const std::string& share_title) {
-  crosapi::mojom::IntentPtr intent = crosapi::mojom::Intent::New();
-  intent->action = apps_util::kIntentActionSend;
-  intent->mime_type = "text/plain";
-  intent->share_text = share_text;
-  if (!share_title.empty())
-    intent->share_title = share_title;
-  return intent;
-}
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
-
-}  // namespace
 
 // static
 SharingHubBubbleController*
@@ -125,11 +95,7 @@ void SharingHubBubbleControllerChromeOsImpl::ShowSharesheet(
   DCHECK(highlighted_button);
   highlighted_button_tracker_.SetView(highlighted_button);
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   ShowSharesheetAsh();
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
-  ShowSharesheetLacros();
-#endif
 
   // Save the window in order to close the Sharesheet if the tab is closed. This
   // will return the incorrect window if called later.
@@ -139,11 +105,7 @@ void SharingHubBubbleControllerChromeOsImpl::ShowSharesheet(
 
 void SharingHubBubbleControllerChromeOsImpl::CloseSharesheet() {
   if (parent_window_ && !parent_window_tracker_->WasNativeWindowDestroyed()) {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
     CloseSharesheetAsh();
-#elif BUILDFLAG(IS_CHROMEOS_LACROS)
-    CloseSharesheetLacros();
-#endif
   }
 
   // OnSharesheetClosed() is not guaranteed to be called by the
@@ -153,7 +115,6 @@ void SharingHubBubbleControllerChromeOsImpl::CloseSharesheet() {
   parent_window_tracker_ = nullptr;
 }
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
 sharesheet::SharesheetService*
 SharingHubBubbleControllerChromeOsImpl::GetSharesheetService() {
   Profile* const profile =
@@ -193,40 +154,6 @@ void SharingHubBubbleControllerChromeOsImpl::CloseSharesheetAsh() {
 
   sharesheet_controller->CloseBubble(sharesheet::SharesheetResult::kCancel);
 }
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-void SharingHubBubbleControllerChromeOsImpl::ShowSharesheetLacros() {
-  auto* const service = chromeos::LacrosService::Get();
-  if (!service || !service->IsAvailable<crosapi::mojom::Sharesheet>())
-    return;
-
-  crosapi::mojom::IntentPtr intent =
-      CreateCrosapiShareIntent(GetWebContents().GetLastCommittedURL().spec(),
-                               base::UTF16ToUTF8(GetWebContents().GetTitle()));
-
-  service->GetRemote<crosapi::mojom::Sharesheet>()->ShowBubbleWithOnClosed(
-      lacros_window_utility::GetRootWindowUniqueId(
-          GetWebContents().GetTopLevelNativeWindow()),
-      sharesheet::LaunchSource::kOmniboxShare, std::move(intent),
-      base::BindOnce(
-          &SharingHubBubbleControllerChromeOsImpl::OnSharesheetClosedLacros,
-          weak_ptr_factory_.GetWeakPtr()));
-}
-
-void SharingHubBubbleControllerChromeOsImpl::CloseSharesheetLacros() {
-  auto* const service = chromeos::LacrosService::Get();
-  if (!service || !service->IsAvailable<crosapi::mojom::Sharesheet>())
-    return;
-
-  service->GetRemote<crosapi::mojom::Sharesheet>()->CloseBubble(
-      lacros_window_utility::GetRootWindowUniqueId(parent_window_));
-}
-
-void SharingHubBubbleControllerChromeOsImpl::OnSharesheetClosedLacros() {
-  OnSharesheetClosed(views::Widget::ClosedReason::kUnspecified);
-}
-#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
 
 void SharingHubBubbleControllerChromeOsImpl::OnSharesheetClosed(
     views::Widget::ClosedReason reason) {
