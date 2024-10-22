9af72732bc3b5c63f2d815ac152ff68079988dce
Kelvin Jiang <kelvinjiang@chromium.org>
2024-10-22 20:20:52 +0000

    [Extensions] Enable item snippets API by default
    Enable calling the item snippets API for fetching Chrome Web Store item
    metadata by default. This will then allow the turndown of the legacy
    item JSON API endpoint.
    
    Bug: 325314721
    Change-Id: I548fb5fb2bc5e7d96723712ad24432385d73756c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954712
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372278}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -177,7 +177,7 @@ BASE_FEATURE(kIncludeJSCallStackInExtensionApiRequest,
 
 BASE_FEATURE(kUseItemSnippetsAPI,
              "UseItemSnippetsAPI",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUseNewServiceWorkerTaskQueue,
              "UseNewServiceWorkerTaskQueue",
