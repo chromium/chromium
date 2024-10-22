0c499e24f088f85a5608ff73ca080252fd0648dc
Joe Mason <joenotcharles@google.com>
2024-10-22 21:25:06 +0000

    Reland "Update ScopedThreadPriority docs and add test banning realtime priority"
    This reverts commit b2971863c4f3f6b335ef756898492230d43444ca.
    
    Reason for revert: Fixed failing test.
    
    Changed the new test to use EXPECT_DCHECK_DEATH since the assert
    it's testing has NotFatalUntil, so is still a DCHECK on some
    builders.
    
    Original change's description:
    > Revert "Update ScopedThreadPriority docs and add test banning realtime priority"
    >
    > This reverts commit 36d698d4c1568bb88d1f5ae0c22014edf7499763.
    >
    > Reason for revert: https://crbug.com/374853850
    >
    > Original change's description:
    > > Update ScopedThreadPriority docs and add test banning realtime priority
    > >
    > > Change-Id: I68b47518c2f128c2a04b16c003c790c3b9b056e6
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5949944
    > > Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    > > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > > Auto-Submit: Joe Mason <joenotcharles@google.com>
    > > Commit-Queue: Joe Mason <joenotcharles@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1371631}
    >
    > Bug: 374853850
    > Change-Id: I1c91c618123c3935e9edbff7c5b43b25ed8b31eb
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5951423
    > Owners-Override: Mikihito Matsuura <mikt@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Mikihito Matsuura <mikt@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1371786}
    
    Bug: 374853850
    Change-Id: I59caab83f8afda85d88dd21b34c79c2e97bc8397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954785
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372328}

--- base/threading/scoped_thread_priority.cc
+++ base/threading/scoped_thread_priority.cc
@@ -4,6 +4,7 @@
 
 #include "base/threading/scoped_thread_priority.h"
 
+#include "base/check_op.h"
 #include "base/location.h"
 #include "base/threading/platform_thread.h"
 #include "base/trace_event/base_tracing.h"
@@ -12,7 +13,7 @@
 namespace base {
 
 ScopedBoostPriority::ScopedBoostPriority(ThreadType target_thread_type) {
-  DCHECK_LT(target_thread_type, ThreadType::kRealtimeAudio);
+  CHECK_LT(target_thread_type, ThreadType::kRealtimeAudio, NotFatalUntil::M133);
   const ThreadType original_thread_type =
       PlatformThread::GetCurrentThreadType();
   const bool should_boost = original_thread_type < target_thread_type &&
