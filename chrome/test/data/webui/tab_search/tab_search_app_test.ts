0a57b67a62f7cdead994768a9113da737f39c0d5
Shibalik Mohapatra <shibalik@chromium.org>
2024-10-23 17:26:50 +0000

    [TabDeclutter] Add feature to field trial config.
    Change-Id: I3489a11210dfb4fc0c0554f19b5ae1bce8e183dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5928064
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372814}

--- chrome/test/data/webui/tab_search/tab_search_app_test.ts
+++ chrome/test/data/webui/tab_search/tab_search_app_test.ts
@@ -5,8 +5,8 @@
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import type {TabSearchAppElement} from 'chrome://tab-search.top-chrome/tab_search.js';
 import {TabSearchApiProxyImpl, TabSearchSection} from 'chrome://tab-search.top-chrome/tab_search.js';
-import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {isVisible, microtasksFinished} from 'chrome://webui-test/test_util.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {TestTabSearchApiProxy} from './test_tab_search_api_proxy.js';
 
@@ -59,16 +59,5 @@ suite('TabOrganizationPageTest', () => {
     await microtasksFinished();
 
     assertEquals(1, crTabs.selected);
-  });
-
-  test('Disabling tab organization from callback router', async () => {
-    const crTabs = tabSearchApp.shadowRoot!.querySelector('cr-tabs');
-    assertTrue(!!crTabs);
-    assertTrue(isVisible(crTabs));
-
-    testProxy.getCallbackRouterRemote().tabOrganizationEnabledChanged(false);
-    await microtasksFinished();
-
-    assertFalse(isVisible(crTabs));
   });
 });
