25cb1f94e7c6d0ea7ed674001272a16bae244c5f
Mustafa Emre Acer <meacer@chromium.org>
2024-10-23 21:38:38 +0000

    [CT] Re-enable Static CT API policy enforcement
    The enforcement logic now ignores logs with unset log types among other
    changes.
    
    Bug: 370724580
    Change-Id: Icb7eb887a5891e351bb3dbf84cb17050bb0f277d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5957874
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372943}

--- net/base/features.cc
+++ net/base/features.cc
@@ -606,7 +606,7 @@ BASE_FEATURE(kEncryptedAndPlaintextValuesAreInvalid,
 
 BASE_FEATURE(kEnableStaticCTAPIEnforcement,
              "EnableStaticCTAPIEnforcement",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDiskCacheBackendExperiment,
              "DiskCacheBackendExperiment",
