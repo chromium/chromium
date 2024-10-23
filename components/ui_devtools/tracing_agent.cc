fa6ca7ae46d5497e0bb03889c02409c850050015
Jan Keitel <jkeitel@google.com>
2024-10-23 16:57:33 +0000

    Use safe buffer constructs in tracing_agent.cc
    Bug: 40285824
    Change-Id: I1c4d784c2a5ba7137ce59b91c0c5f4a809a8c05c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5956396
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372786}

--- components/ui_devtools/tracing_agent.cc
+++ components/ui_devtools/tracing_agent.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/ui_devtools/tracing_agent.h"
 
 #include <algorithm>
@@ -19,6 +14,7 @@
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/process/process.h"
+#include "base/strings/cstring_view.h"
 #include "base/timer/timer.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/traced_value.h"
@@ -333,8 +329,9 @@ void TracingAgent::OnTraceDataCollected(
   const size_t messageSuffixSize = 10;
   message.reserve(message.size() + valid_trace_fragment.size() +
                   messageSuffixSize - trace_data_buffer_state_.offset);
-  message.append(valid_trace_fragment.c_str() +
-                 trace_data_buffer_state_.offset);
+  message.append(base::cstring_view(valid_trace_fragment)
+                     .substr(trace_data_buffer_state_.offset)
+                     .data());
   message += "] } }";
   frontend()->sendRawNotification(
       std::make_unique<TracingNotification>(std::move(message)));
