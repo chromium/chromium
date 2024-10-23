e5022d470faac2f31487980152302723dcc04c15
dpapad <dpapad@chromium.org>
2024-10-23 23:17:16 +0000

    CrOS: Remove disallowed dependency from chrome://dlp-internals.
    CrOS-only UIs shouldn't depend on `//ui/webui/resources/cr_elements/`.
    Instead, should be using the CrOS cr_elements fork at
    `ash/webui/common/resources/cr_elements/`.
    
    Fixed: 375231022
    Change-Id: I54f723120426eff3133e5d11134ce68ecb154a61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5959087
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373015}

--- chrome/browser/resources/dlp_internals/dlp_internals_ui.ts
+++ chrome/browser/resources/dlp_internals/dlp_internals_ui.ts
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_elements/cr_page_selector/cr_page_selector.js';
-import 'chrome://resources/cr_elements/cr_tabs/cr_tabs.js';
-import '//resources/cr_elements/cr_collapse/cr_collapse.js';
-import '//resources/cr_elements/cr_expand_button/cr_expand_button.js';
+import '//resources/ash/common/cr_elements/cr_expand_button/cr_expand_button.js';
+import '//resources/ash/common/cr_elements/cr_tabs/cr_tabs.js';
+import '//resources/polymer/v3_0/iron-collapse/iron-collapse.js';
+import '//resources/polymer/v3_0/iron-pages/iron-pages.js';
 import './strings.m.js';
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
