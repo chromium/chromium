5f0f8fc2eb9c555b171a0d076680d7cc6a41e236
Dave Tapuska <dtapuska@chromium.org>
2024-10-24 13:54:51 +0000

    [Extensions] Report bad message for invalid worker thread id
    If the renderer sends up a worker thread id that matches the main
    thread tell it to get lost.
    
    Bug: 374658914
    Change-Id: I96c0febfa29c391d5b7893af0bbaaf3d76dc030c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954283
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373295}

--- extensions/browser/bad_message.h
+++ extensions/browser/bad_message.h
@@ -71,10 +71,11 @@ enum BadMessageReason {
   EFH_NO_BACKGROUND_HOST_FOR_FRAME = 31,
   LEGACY_IPC_MISMATCH = 32,
   ER_SW_INVALID_LAZY_BACKGROUND_PARAM = 33,
+  SWH_BAD_WORKER_THREAD_ID = 34,
   // Please add new elements here. The naming convention is abbreviated class
   // name (e.g. ExtensionHost becomes EH) plus a unique description of the
   // reason. After making changes, you MUST update histograms.xml by running:
-  // "python tools/metrics/histograms/update_bad_message_reasons.py"
+  // "vpython3 tools/metrics/histograms/update_bad_message_reasons.py"
   BAD_MESSAGE_MAX
 };
 
