cc1554be6b0b43e29c72d6b35da64a03342d3e5e
Ari Chivukula <arichiv@chromium.org>
2024-10-24 18:48:51 +0000

    [Partitioned Popins] Simplify RSA tests
    These are getting out of hand, let's consolidate the read paths in
    JavaScript at least.
    
    Explainer: https://explainers-by-googlers.github.io/partitioned-popins/
    I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/ApU_zUmpQ2g/
    
    Bug: 340606651
    Change-Id: I43ae4fcd338611c4d0cd918b3fdad6b59098fd22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962496
    Reviewed-by: Sandor «Alex» Major <sandormajor@google.com>
    Commit-Queue: Sandor «Alex» Major <sandormajor@google.com>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373485}

--- third_party/blink/web_tests/external/wpt/partitioned-popins/resources/partitioned-popins.cookies-popin.py
+++ third_party/blink/web_tests/external/wpt/partitioned-popins/resources/partitioned-popins.cookies-popin.py
@@ -5,24 +5,14 @@ def main(request, response):
     headers[0] = (b"Content-Type", b"text/html")
     headers.append((b'Popin-Policy', b"partitioned=*"))
     cookies = readCookies(request)
-    message = b"ReadOnLoad:"
-    if cookies.get(b"first-party-strict") == id:
-        message += b"FirstPartyStrict-"
-    if cookies.get(b"first-party-lax") == id:
-        message += b"FirstPartyLax-"
-    if cookies.get(b"first-party-none") == id:
-        message += b"FirstPartyNone-"
-    if cookies.get(b"third-party-strict") == id:
-        message += b"ThirdPartyStrict-"
-    if cookies.get(b"third-party-lax") == id:
-        message += b"ThirdPartyLax-"
-    if cookies.get(b"third-party-none") == id:
-        message += b"ThirdPartyNone-"
+    decoded_cookies = [key + b"=" + val for key, val in cookies.items()]
+    cookie_string = b";".join(decoded_cookies)
     document = b"""
 <!doctype html>
 <meta charset="utf-8">
 <script src="/resources/testdriver.js"></script>
 <script src="/resources/testdriver-vendor.js"></script>
+<script src="/partitioned-popins/resources/cookie-helpers.js"></script>
 <script>
 (async function() {
   test_driver.set_test_context(window.opener);
@@ -30,209 +20,52 @@ def main(request, response):
   // Step 7 (partitioned-popins/partitioned-popins.cookies.tentative.sub.https.window.js)
   const id = (new URLSearchParams(window.location.search)).get("id");
   test_driver.set_test_context(window.top);
-  document.cookie = "first-party-strict-popin=" + id + "; SameSite=Strict; Secure";
-  document.cookie = "first-party-lax-popin=" + id + "; SameSite=Lax; Secure";
-  document.cookie = "first-party-none-popin=" + id + "; SameSite=None; Secure";
-  document.cookie = "third-party-strict-popin=" + id + "; Partitioned; SameSite=Strict; Secure";
-  document.cookie = "third-party-lax-popin=" + id + "; Partitioned; SameSite=Lax; Secure";
-  document.cookie = "third-party-none-popin=" + id + "; Partitioned; SameSite=None; Secure";
+  let cookie_string_on_load = \"""" + cookie_string + b"""\";
+  let message = "ReadOnLoad:";
+  message += getCookieMessage(cookie_string_on_load, "FirstParty", "", id);
+  message += getCookieMessage(cookie_string_on_load, "ThirdParty", "", id);
+  document.cookie = "FirstPartyStrictPopin=" + id + "; SameSite=Strict; Secure";
+  document.cookie = "FirstPartyLaxPopin=" + id + "; SameSite=Lax; Secure";
+  document.cookie = "FirstPartyNonePopin=" + id + "; SameSite=None; Secure";
+  document.cookie = "ThirdPartyStrictPopin=" + id + "; Partitioned; SameSite=Strict; Secure";
+  document.cookie = "ThirdPartyLaxPopin=" + id + "; Partitioned; SameSite=Lax; Secure";
+  document.cookie = "ThirdPartyNonePopin=" + id + "; Partitioned; SameSite=None; Secure";
   let resp = await fetch("/partitioned-popins/resources/get_cookies.py", {credentials: 'include'});
-  let resp_json = await resp.json();
-  let message = \"""" + message + b""",ReadOnFetch:";
-  // TODO(crbug.com/340606651): Consolidate and refactor
-  if (resp_json["first-party-strict"] == id) {
-    message += "FirstPartyStrict-";
-  }
-  if (resp_json["first-party-lax"] == id) {
-    message += "FirstPartyLax-";
-  }
-  if (resp_json["first-party-none"] == id) {
-    message += "FirstPartyNone-";
-  }
-  if (resp_json["third-party-strict"] == id) {
-    message += "ThirdPartyStrict-";
-  }
-  if (resp_json["third-party-lax"] == id) {
-    message += "ThirdPartyLax-";
-  }
-  if (resp_json["third-party-none"] == id) {
-    message += "ThirdPartyNone-";
-  }
-  if (resp_json["first-party-strict-popin"] == id) {
-    message += "FirstPartyStrictPopin-";
-  }
-  if (resp_json["first-party-lax-popin"] == id) {
-    message += "FirstPartyLaxPopin-";
-  }
-  if (resp_json["first-party-none-popin"] == id) {
-    message += "FirstPartyNonePopin-";
-  }
-  if (resp_json["third-party-strict-popin"] == id) {
-    message += "ThirdPartyStrictPopin-";
-  }
-  if (resp_json["third-party-lax-popin"] == id) {
-    message += "ThirdPartyLaxPopin-";
-  }
-  if (resp_json["third-party-none-popin"] == id) {
-    message += "ThirdPartyNonePopin-";
-  }
+  let resp_text = await resp.text();
+  message += ",ReadOnFetch:";
+  message += getCookieMessage(resp_text, "FirstParty", "", id);
+  message += getCookieMessage(resp_text, "ThirdParty", "", id);
+  message += getCookieMessage(resp_text, "FirstParty", "Popin", id);
+  message += getCookieMessage(resp_text, "ThirdParty", "Popin", id);
   message += ",ReadOnDocument:";
-  if (document.cookie.includes("first-party-strict="+id)) {
-    message += "FirstPartyStrict-";
-  }
-  if (document.cookie.includes("first-party-lax="+id)) {
-    message += "FirstPartyLax-";
-  }
-  if (document.cookie.includes("first-party-none="+id)) {
-    message += "FirstPartyNone-";
-  }
-  if (document.cookie.includes("third-party-strict="+id)) {
-    message += "ThirdPartyStrict-";
-  }
-  if (document.cookie.includes("third-party-lax="+id)) {
-    message += "ThirdPartyLax-";
-  }
-  if (document.cookie.includes("third-party-none="+id)) {
-    message += "ThirdPartyNone-";
-  }
-  if (document.cookie.includes("first-party-strict-popin="+id)) {
-    message += "FirstPartyStrictPopin-";
-  }
-  if (document.cookie.includes("first-party-lax-popin="+id)) {
-    message += "FirstPartyLaxPopin-";
-  }
-  if (document.cookie.includes("first-party-none-popin="+id)) {
-    message += "FirstPartyNonePopin-";
-  }
-  if (document.cookie.includes("third-party-strict-popin="+id)) {
-    message += "ThirdPartyStrictPopin-";
-  }
-  if (document.cookie.includes("third-party-lax-popin="+id)) {
-    message += "ThirdPartyLaxPopin-";
-  }
-  if (document.cookie.includes("third-party-none-popin="+id)) {
-    message += "ThirdPartyNonePopin-";
-  }
+  message += getCookieMessage(document.cookie, "FirstParty", "", id);
+  message += getCookieMessage(document.cookie, "ThirdParty", "", id);
+  message += getCookieMessage(document.cookie, "FirstParty", "Popin", id);
+  message += getCookieMessage(document.cookie, "ThirdParty", "Popin", id);
   await test_driver.set_permission({ name: 'storage-access' }, 'granted');
   await test_driver.bless("fake user interaction", () => document.requestStorageAccess());
-  document.cookie = "first-party-strict-popin-after-rsa=" + id + "; SameSite=Strict; Secure";
-  document.cookie = "first-party-lax-popin-after-rsa=" + id + "; SameSite=Lax; Secure";
-  document.cookie = "first-party-none-popin-after-rsa=" + id + "; SameSite=None; Secure";
-  document.cookie = "third-party-strict-popin-after-rsa=" + id + "; Partitioned; SameSite=Strict; Secure";
-  document.cookie = "third-party-lax-popin-after-rsa=" + id + "; Partitioned; SameSite=Lax; Secure";
-  document.cookie = "third-party-none-popin-after-rsa=" + id + "; Partitioned; SameSite=None; Secure";
+  document.cookie = "FirstPartyStrictPopinAfterRSA=" + id + "; SameSite=Strict; Secure";
+  document.cookie = "FirstPartyLaxPopinAfterRSA=" + id + "; SameSite=Lax; Secure";
+  document.cookie = "FirstPartyNonePopinAfterRSA=" + id + "; SameSite=None; Secure";
+  document.cookie = "ThirdPartyStrictPopinAfterRSA=" + id + "; Partitioned; SameSite=Strict; Secure";
+  document.cookie = "ThirdPartyLaxPopinAfterRSA=" + id + "; Partitioned; SameSite=Lax; Secure";
+  document.cookie = "ThirdPartyNonePopinAfterRSA=" + id + "; Partitioned; SameSite=None; Secure";
   resp = await fetch("/partitioned-popins/resources/get_cookies.py", {credentials: 'include'});
-  resp_json = await resp.json();
+  resp_text = await resp.text();
   message += ",ReadOnFetchAfterRSA:";
-  if (resp_json["first-party-strict"] == id) {
-    message += "FirstPartyStrict-";
-  }
-  if (resp_json["first-party-lax"] == id) {
-    message += "FirstPartyLax-";
-  }
-  if (resp_json["first-party-none"] == id) {
-    message += "FirstPartyNone-";
-  }
-  if (resp_json["third-party-strict"] == id) {
-    message += "ThirdPartyStrict-";
-  }
-  if (resp_json["third-party-lax"] == id) {
-    message += "ThirdPartyLax-";
-  }
-  if (resp_json["third-party-none"] == id) {
-    message += "ThirdPartyNone-";
-  }
-  if (resp_json["first-party-strict-popin"] == id) {
-    message += "FirstPartyStrictPopin-";
-  }
-  if (resp_json["first-party-lax-popin"] == id) {
-    message += "FirstPartyLaxPopin-";
-  }
-  if (resp_json["first-party-none-popin"] == id) {
-    message += "FirstPartyNonePopin-";
-  }
-  if (resp_json["third-party-strict-popin"] == id) {
-    message += "ThirdPartyStrictPopin-";
-  }
-  if (resp_json["third-party-lax-popin"] == id) {
-    message += "ThirdPartyLaxPopin-";
-  }
-  if (resp_json["third-party-none-popin"] == id) {
-    message += "ThirdPartyNonePopin-";
-  }
-  if (resp_json["first-party-strict-popin-after-rsa"] == id) {
-    message += "FirstPartyStrictPopinAfterRSA-";
-  }
-  if (resp_json["first-party-lax-popin-after-rsa"] == id) {
-    message += "FirstPartyLaxPopinAfterRSA-";
-  }
-  if (resp_json["first-party-none-popin-after-rsa"] == id) {
-    message += "FirstPartyNonePopinAfterRSA-";
-  }
-  if (resp_json["third-party-strict-popin-after-rsa"] == id) {
-    message += "ThirdPartyStrictPopinAfterRSA-";
-  }
-  if (resp_json["third-party-lax-popin-after-rsa"] == id) {
-    message += "ThirdPartyLaxPopinAfterRSA-";
-  }
-  if (resp_json["third-party-none-popin-after-rsa"] == id) {
-    message += "ThirdPartyNonePopinAfterRSA-";
-  }
+  message += getCookieMessage(resp_text, "FirstParty", "", id);
+  message += getCookieMessage(resp_text, "ThirdParty", "", id);
+  message += getCookieMessage(resp_text, "FirstParty", "Popin", id);
+  message += getCookieMessage(resp_text, "ThirdParty", "Popin", id);
+  message += getCookieMessage(resp_text, "FirstParty", "PopinAfterRSA", id);
+  message += getCookieMessage(resp_text, "ThirdParty", "PopinAfterRSA", id);
   message += ",ReadOnDocumentAfterRSA:";
-  if (document.cookie.includes("first-party-strict="+id)) {
-    message += "FirstPartyStrict-";
-  }
-  if (document.cookie.includes("first-party-lax="+id)) {
-    message += "FirstPartyLax-";
-  }
-  if (document.cookie.includes("first-party-none="+id)) {
-    message += "FirstPartyNone-";
-  }
-  if (document.cookie.includes("third-party-strict="+id)) {
-    message += "ThirdPartyStrict-";
-  }
-  if (document.cookie.includes("third-party-lax="+id)) {
-    message += "ThirdPartyLax-";
-  }
-  if (document.cookie.includes("third-party-none="+id)) {
-    message += "ThirdPartyNone-";
-  }
-  if (document.cookie.includes("first-party-strict-popin="+id)) {
-    message += "FirstPartyStrictPopin-";
-  }
-  if (document.cookie.includes("first-party-lax-popin="+id)) {
-    message += "FirstPartyLaxPopin-";
-  }
-  if (document.cookie.includes("first-party-none-popin="+id)) {
-    message += "FirstPartyNonePopin-";
-  }
-  if (document.cookie.includes("third-party-strict-popin="+id)) {
-    message += "ThirdPartyStrictPopin-";
-  }
-  if (document.cookie.includes("third-party-lax-popin="+id)) {
-    message += "ThirdPartyLaxPopin-";
-  }
-  if (document.cookie.includes("third-party-none-popin="+id)) {
-    message += "ThirdPartyNonePopin-";
-  }
-  if (document.cookie.includes("first-party-strict-popin-after-rsa="+id)) {
-    message += "FirstPartyStrictPopinAfterRSA-";
-  }
-  if (document.cookie.includes("first-party-lax-popin-after-rsa="+id)) {
-    message += "FirstPartyLaxPopinAfterRSA-";
-  }
-  if (document.cookie.includes("first-party-none-popin-after-rsa="+id)) {
-    message += "FirstPartyNonePopinAfterRSA-";
-  }
-  if (document.cookie.includes("third-party-strict-popin-after-rsa="+id)) {
-    message += "ThirdPartyStrictPopinAfterRSA-";
-  }
-  if (document.cookie.includes("third-party-lax-popin-after-rsa="+id)) {
-    message += "ThirdPartyLaxPopinAfterRSA-";
-  }
-  if (document.cookie.includes("third-party-none-popin-after-rsa="+id)) {
-    message += "ThirdPartyNonePopinAfterRSA-";
-  }
+  message += getCookieMessage(document.cookie, "FirstParty", "", id);
+  message += getCookieMessage(document.cookie, "ThirdParty", "", id);
+  message += getCookieMessage(document.cookie, "FirstParty", "Popin", id);
+  message += getCookieMessage(document.cookie, "ThirdParty", "Popin", id);
+  message += getCookieMessage(document.cookie, "FirstParty", "PopinAfterRSA", id);
+  message += getCookieMessage(document.cookie, "ThirdParty", "PopinAfterRSA", id);
   window.opener.postMessage({type: "popin-read", message: message}, "*");
   window.close();
 })();
