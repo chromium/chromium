bfd7c71b0db7ff43a17537cd9b7f97626ce5acad
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-10-25 17:18:06 +0000

    Create iOS implementation of `ConnectorsManager`
    There are currently no pure virtual methods in `ConnectorsManagerBase`,
    but making an iOS-specific override anyway will allow for easier support
    of such methods in the future simplifies testing on iOS test targets.
    
    Bug: 370466578, 344578617
    Change-Id: Ifff652a07e7171523244916d36ec1dc8c7974cb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962317
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1373993}

---
+++ ios/chrome/browser/enterprise/connectors/connectors_manager_unittest.mm
@@ -0,0 +1,113 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/enterprise/connectors/connectors_manager.h"
+
+#import "base/json/json_reader.h"
+#import "components/enterprise/connectors/core/connectors_prefs.h"
+#import "components/enterprise/connectors/core/reporting_constants.h"
+#import "components/enterprise/connectors/core/service_provider_config.h"
+#import "components/prefs/testing_pref_service.h"
+#import "testing/gtest/include/gtest/gtest.h"
+#import "testing/platform_test.h"
+
+namespace enterprise_connectors {
+
+namespace {
+
+class ConnectorsManagerTest : public PlatformTest {
+ public:
+  TestingPrefServiceSimple* prefs() { return &pref_service_; }
+
+  void SetUp() override {
+    // Setup required to register connectors prefs with `pref_service_`.
+    RegisterProfilePrefs(prefs()->registry());
+  }
+
+ private:
+  TestingPrefServiceSimple pref_service_;
+};
+
+}  // namespace
+
+TEST_F(ConnectorsManagerTest, ReportingSettings) {
+  // All reporting settings tests are done in the same test so as to validate
+  // that `ConnectorsManager` is able to properly observe the pref as it
+  // changes.
+
+  ConnectorsManager manager(prefs(), GetServiceProviderConfig());
+
+  EXPECT_FALSE(
+      manager.IsReportingConnectorEnabled(ReportingConnector::SECURITY_EVENT));
+  EXPECT_FALSE(
+      manager.GetReportingSettings(ReportingConnector::SECURITY_EVENT));
+  EXPECT_TRUE(
+      manager
+          .GetReportingServiceProviderNames(ReportingConnector::SECURITY_EVENT)
+          .empty());
+
+  prefs()->Set(kOnSecurityEventPref, *base::JSONReader::Read(
+                                         R"([
+                                              {
+                                                "service_provider": "google"
+                                              }
+                                            ])",
+                                         base::JSON_ALLOW_TRAILING_COMMAS));
+
+  EXPECT_TRUE(
+      manager.IsReportingConnectorEnabled(ReportingConnector::SECURITY_EVENT));
+  auto settings =
+      manager.GetReportingSettings(ReportingConnector::SECURITY_EVENT);
+  EXPECT_TRUE(settings.has_value());
+  EXPECT_EQ(settings->enabled_event_names,
+            std::set<std::string>(kAllReportingEvents.begin(),
+                                  kAllReportingEvents.end()));
+  EXPECT_TRUE(settings->enabled_opt_in_events.empty());
+  auto provider_names = manager.GetReportingServiceProviderNames(
+      ReportingConnector::SECURITY_EVENT);
+  EXPECT_EQ(provider_names, std::vector<std::string>({"google"}));
+
+  prefs()->Set(kOnSecurityEventPref, *base::JSONReader::Read(
+                                         R"([{
+                                              "service_provider": "google",
+                                              "enabled_event_names": [
+                                                "passwordReuseEvent",
+                                                "interstitialEvent"
+                                              ],
+                                              "enabled_opt_in_events": [
+                                                {
+                                                  "name": "loginEvent",
+                                                  "url_patterns": [
+                                                    "foo.com",
+                                                    "bar.com"
+                                                  ]
+                                                },
+                                                {
+                                                  "name": "passwordBreachEvent",
+                                                  "url_patterns": [
+                                                    "baz.com"
+                                                  ]
+                                                }
+                                              ]
+                                              }])",
+                                         base::JSON_ALLOW_TRAILING_COMMAS));
+
+  EXPECT_TRUE(
+      manager.IsReportingConnectorEnabled(ReportingConnector::SECURITY_EVENT));
+  settings = manager.GetReportingSettings(ReportingConnector::SECURITY_EVENT);
+  EXPECT_TRUE(settings.has_value());
+  EXPECT_EQ(settings->enabled_event_names,
+            std::set<std::string>({"passwordReuseEvent", "interstitialEvent"}));
+  EXPECT_TRUE(settings->enabled_opt_in_events.count("loginEvent"));
+  EXPECT_EQ(settings->enabled_opt_in_events["loginEvent"],
+            std::vector<std::string>({"foo.com", "bar.com"}));
+  EXPECT_TRUE(settings->enabled_opt_in_events.count("passwordBreachEvent"));
+  EXPECT_EQ(settings->enabled_opt_in_events["passwordBreachEvent"],
+            std::vector<std::string>({"baz.com"}));
+  provider_names = manager.GetReportingServiceProviderNames(
+      ReportingConnector::SECURITY_EVENT);
+  EXPECT_EQ(provider_names, std::vector<std::string>({"google"}));
+}
+
+}  // namespace enterprise_connectors
