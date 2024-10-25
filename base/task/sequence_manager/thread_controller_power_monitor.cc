353ef9d1248cbd62fdd7280d89750ac7d65f68bc
Jie Jiang <jiejiang@chromium.org>
2024-10-25 15:38:45 +0000

    Revert "Clean up UsePowerMonitorWithThreadController"
    This reverts commit f66896faa0eae6b4b3fa65eb4fe70bc8a7b76e47.
    
    Reason for revert: b/374173161. This breaks the suspend behavior on
    CrOS.
    
    Original change's description:
    > Clean up UsePowerMonitorWithThreadController
    >
    > The feature has been enabled by default for a long time, removing it
    > should be a no-op.
    >
    > Bug: 356234726
    > Change-Id: I85739a627364102ec97b00c99fc46616e97825e1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832196
    > Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    > Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    > Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1359479}
    
    Bug: 356234726, 374173161
    Change-Id: Icd591c4e6dd4881ce93e48254b28a4cd6bc869f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960956
    Commit-Queue: Jie Jiang <jiejiang@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373928}

--- base/task/sequence_manager/thread_controller_power_monitor.cc
+++ base/task/sequence_manager/thread_controller_power_monitor.cc
@@ -12,6 +12,19 @@ namespace base {
 namespace sequence_manager {
 namespace internal {
 
+namespace {
+
+// Activate the power management events that affect task scheduling.
+BASE_FEATURE(kUsePowerMonitorWithThreadController,
+             "UsePowerMonitorWithThreadController",
+             FEATURE_ENABLED_BY_DEFAULT);
+
+// TODO(crbug.com/40127966): Remove this when the experiment becomes the
+// default.
+bool g_use_thread_controller_power_monitor_ = false;
+
+}  // namespace
+
 ThreadControllerPowerMonitor::ThreadControllerPowerMonitor() = default;
 
 ThreadControllerPowerMonitor::~ThreadControllerPowerMonitor() {
@@ -35,7 +48,27 @@ bool ThreadControllerPowerMonitor::IsProcessInPowerSuspendState() {
   return is_power_suspended_;
 }
 
+// static
+void ThreadControllerPowerMonitor::InitializeFeatures() {
+  DCHECK(!g_use_thread_controller_power_monitor_);
+  g_use_thread_controller_power_monitor_ =
+      FeatureList::IsEnabled(kUsePowerMonitorWithThreadController);
+}
+
+// static
+void ThreadControllerPowerMonitor::OverrideUsePowerMonitorForTesting(
+    bool use_power_monitor) {
+  g_use_thread_controller_power_monitor_ = use_power_monitor;
+}
+
+// static
+void ThreadControllerPowerMonitor::ResetForTesting() {
+  g_use_thread_controller_power_monitor_ = false;
+}
+
 void ThreadControllerPowerMonitor::OnSuspend() {
+  if (!g_use_thread_controller_power_monitor_)
+    return;
   DCHECK(!is_power_suspended_);
 
   TRACE_EVENT_BEGIN("base", "ThreadController::Suspended",
@@ -45,6 +78,9 @@ void ThreadControllerPowerMonitor::OnSuspend() {
 }
 
 void ThreadControllerPowerMonitor::OnResume() {
+  if (!g_use_thread_controller_power_monitor_)
+    return;
+
   // It is possible a suspend was already happening before the observer was
   // added to the power monitor. Ignoring the resume notification in that case.
   if (is_power_suspended_) {
