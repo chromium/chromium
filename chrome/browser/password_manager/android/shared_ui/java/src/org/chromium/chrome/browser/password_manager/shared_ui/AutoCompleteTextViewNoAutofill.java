ff714eb6f5c90cecee5b0d6fc4e656edc2347df8
Anna Tsvirchkova <atsvirchkova@google.com>
2024-10-25 15:19:40 +0000

    Fix AwG suggesting to save password for the Chrome App
    After showing add username dialog (it's shown when user saves a password
    with empty username), AwG pops up proposing to save the password. This
    happens because of using TextInputEditText ui element, which provides
    autofill structure to AwG. This CL uses the ui element, which doesn't
    provide autofill structure (and moves this element to a more shared
    place).
    
    Bug: 372415388
    Change-Id: I8a53f801c6ab847662fa7a1c52c01b437b723e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5965062
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373911}

--- chrome/browser/password_manager/android/shared_ui/java/src/org/chromium/chrome/browser/password_manager/shared_ui/AutoCompleteTextViewNoAutofill.java
+++ chrome/browser/password_manager/android/shared_ui/java/src/org/chromium/chrome/browser/password_manager/shared_ui/AutoCompleteTextViewNoAutofill.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.password_edit_dialog;
+package org.chromium.chrome.browser.password_manager.shared_ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
