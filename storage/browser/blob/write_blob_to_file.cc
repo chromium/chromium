chromium/chromium

commit f9659c2f1b20b149c5daa0a8cd4ee15006fe7a19

Use safer forms of base::File::Read*() and Write*() methods.
Prefer the span form over the discrete ptr/length ones, since the
latter will likely be marked UNSAFE_BUFFER_USAGE in a future Cl. At
which point using those forms will require special annotations.

-- Update to use optional<> return value in callers.
-- Use ...AndCheck() methods in place of length comparisons.
-- Convert some literals from strings to string_views.

Change-Id: I178fb720d94436afc8b862521a6a2dbd47724d04
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773711
Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
Commit-Queue: Nico Weber <thakis@chromium.org>
Owners-Override: Nico Weber <thakis@chromium.org>
Reviewed-by: Nico Weber <thakis@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1340078}

storage/browser/blob/write_blob_to_file.cc
@@ -12,6 +12,7 @@
 #include <utility>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
@@ -102,28 +103,30 @@ bool CopyFileContentsWithOffsetAndSize(base::File* infile,
     size_t bytes_to_read =
         std::min(buffer.size(),
                  base::checked_cast<size_t>(checked_max_size.ValueOrDie()));
-    int bytes_read = infile->ReadAtCurrentPos(buffer.data(), bytes_to_read);
-    if (bytes_read < 0)
+    std::optional<size_t> bytes_read = infile->ReadAtCurrentPos(
+        base::as_writable_byte_span(buffer).first(bytes_to_read));
+    if (!bytes_read.has_value()) {
       return false;
-    if (bytes_read == 0)
+    }
+    if (bytes_read.value() == 0) {
       return true;
-    checked_max_size -= bytes_read;
-    if (!checked_max_size.IsValid())
+    }
+    checked_max_size -= bytes_read.value();
+    if (!checked_max_size.IsValid()) {
       return false;
-
+    }
     // Allow for partial writes
-    int bytes_written_per_read = 0;
-    do {
-      int bytes_written_partial = outfile->WriteAtCurrentPos(
-          &buffer[bytes_written_per_read], bytes_read - bytes_written_per_read);
-      if (bytes_written_partial < 0)
+    auto span_to_write = base::as_byte_span(buffer).first(bytes_read.value());
+    while (!span_to_write.empty()) {
+      std::optional<size_t> bytes_written_partial =
+          outfile->WriteAtCurrentPos(span_to_write);
+      if (!bytes_written_partial.has_value()) {
         return false;
-
-      bytes_written_per_read += bytes_written_partial;
-      *bytes_copied += bytes_written_partial;
-    } while (bytes_written_per_read < bytes_read);
-    if (checked_max_size.ValueOrDie() == 0)
-      return true;
+      }
+      span_to_write = span_to_write.subspan(bytes_written_partial.value());
+      *bytes_copied += bytes_written_partial.value();
+    }
+    return true;
   }
 
   NOTREACHED_IN_MIGRATION();

chrome/browser/safe_browsing/cloud_content_scanning/connector_data_pipe_getter_unittest.cc
@@ -15,6 +15,7 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace safe_browsing {
+
 class ConnectorDataPipeGetterTest : public testing::Test {
  public:
   std::optional<base::File> CreateFile(const std::string& content) {
@@ -22,12 +23,12 @@ class ConnectorDataPipeGetterTest : public testing::Test {
     base::FilePath path = temp_dir_.GetPath().AppendASCII("test.txt");
     base::File file(path, base::File::FLAG_CREATE | base::File::FLAG_READ |
                               base::File::FLAG_WRITE);
-    if (!file.IsValid())
+    if (!file.IsValid()) {
       return std::nullopt;
-
-    if (file.WriteAtCurrentPos(content.data(), content.size()) < 0)
+    }
+    if (!file.WriteAtCurrentPosAndCheck(base::as_byte_span(content))) {
       return std::nullopt;
-
+    }
     return file;
   }
 

chrome/browser/safe_browsing/cloud_content_scanning/file_analysis_request.cc
@@ -6,6 +6,7 @@
 
 #include <string_view>
 
+#include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "base/files/memory_mapped_file.h"
 #include "base/ranges/algorithm.h"
@@ -94,13 +95,12 @@ GetFileDataBlocking(const base::FilePath& path, bool detect_mime_type) {
   std::unique_ptr<crypto::SecureHash> secure_hash =
       crypto::SecureHash::Create(crypto::SecureHash::SHA256);
   size_t bytes_read = 0;
-  std::vector<char> buf;
-  buf.resize(kReadFileChunkSize);
+  std::vector<char> buf(kReadFileChunkSize);
 
   while (bytes_read < file_data.size) {
-    int64_t bytes_currently_read =
-        file.ReadAtCurrentPos(&buf[0], kReadFileChunkSize);
-    if (bytes_currently_read == -1) {
+    std::optional<size_t> bytes_currently_read =
+        file.ReadAtCurrentPos(base::as_writable_byte_span(buf));
+    if (!bytes_currently_read.has_value()) {
       // Reset the size to zero since some code assumes an UNKNOWN result is
       // matched with a zero size.
       file_data.size = 0;
@@ -108,13 +108,13 @@ GetFileDataBlocking(const base::FilePath& path, bool detect_mime_type) {
     }
 
     // Use the first read chunk to get the mimetype as necessary.
-    if (detect_mime_type && (bytes_read == 0)) {
+    if (detect_mime_type && bytes_read == 0) {
       file_data.mime_type = GetFileMimeType(
-          path, std::string_view(buf.data(), bytes_currently_read));
+          path, std::string_view(buf.data(), bytes_currently_read.value()));
     }
 
-    secure_hash->Update(buf.data(), bytes_currently_read);
-    bytes_read += bytes_currently_read;
+    secure_hash->Update(buf.data(), bytes_currently_read.value());
+    bytes_read += bytes_currently_read.value();
   }
 
   file_data.hash.resize(crypto::kSHA256Length);

chrome/browser/safe_browsing/cloud_content_scanning/file_analysis_request_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/safe_browsing/cloud_content_scanning/file_analysis_request.h"
 
+#include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/callback_helpers.h"
@@ -277,7 +278,7 @@ TEST_F(FileAnalysisRequestTest, PopulatesDigest) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   auto request = MakeRequest(file_path, file_path.BaseName(),
                              /*delay_opening_file*/ false);
@@ -302,7 +303,7 @@ TEST_F(FileAnalysisRequestTest, PopulatesFilename) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   auto request = MakeRequest(file_path, file_path.BaseName(),
                              /*delay_opening_file*/ false);
@@ -386,7 +387,7 @@ TEST_F(FileAnalysisRequestTest, DelayedFileOpening) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   auto request =
       MakeRequest(file_path, file_path.BaseName(), /*delay_opening_file*/ true);

chrome/browser/safe_browsing/cloud_content_scanning/file_opening_job_unittest.cc
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "base/command_line.h"
+#include "base/containers/span.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/weak_ptr.h"
 #include "base/run_loop.h"
@@ -49,7 +50,7 @@ class FileOpeningJobTest : public testing::Test {
           base::StringPrintf("foo%d.txt", next_file_id_));
       ++next_file_id_;
       base::File file(path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-      file.WriteAtCurrentPos(/*data*/ "foo", /*size*/ 3);
+      file.WriteAtCurrentPos(base::byte_span_from_cstring("foo"));
 
       auto request = std::make_unique<FileAnalysisRequest>(
           enterprise_connectors::AnalysisSettings(), path, path.BaseName(),

chrome/browser/safe_browsing/cloud_content_scanning/multipart_uploader_unittest.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/functional/callback_helpers.h"
@@ -42,7 +43,7 @@ class MultipartUploadRequestTest : public testing::Test {
     base::FilePath path = temp_dir_.GetPath().AppendASCII(file_name);
     base::File file(path, base::File::FLAG_CREATE_ALWAYS |
                               base::File::FLAG_READ | base::File::FLAG_WRITE);
-    file.WriteAtCurrentPos(content.data(), content.size());
+    file.WriteAtCurrentPos(base::as_byte_span(content));
     return path;
   }
 

chrome/browser/safe_browsing/cloud_content_scanning/resumable_uploader_unittest.cc
@@ -6,6 +6,7 @@
 
 #include <memory>
 
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/memory/read_only_shared_memory_region.h"
@@ -75,7 +76,7 @@ class ResumableUploadRequestTest : public testing::Test {
     base::FilePath path = temp_dir_.GetPath().AppendASCII(file_name);
     base::File file(path, base::File::FLAG_CREATE_ALWAYS |
                               base::File::FLAG_READ | base::File::FLAG_WRITE);
-    file.WriteAtCurrentPos(content.data(), content.size());
+    file.WriteAtCurrentPos(base::as_byte_span(content));
     return path;
   }
 

chrome/browser/safe_browsing/download_protection/download_protection_service_unittest.cc
@@ -15,6 +15,7 @@
 #include "base/base_paths.h"
 #include "base/command_line.h"
 #include "base/containers/flat_map.h"
+#include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -3573,7 +3574,7 @@ TEST_F(DeepScanningDownloadTest, LargeFileBlockedByPreference) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   NiceMockDownloadItem item;
   PrepareBasicDownloadItemWithFullPaths(
@@ -4453,7 +4454,7 @@ TEST_F(DeepScanningDownloadTest, PolicyEnabled) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   NiceMockDownloadItem item;
   PrepareBasicDownloadItemWithFullPaths(
@@ -4517,7 +4518,7 @@ TEST_F(DeepScanningDownloadTest, PolicyDisabled) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   NiceMockDownloadItem item;
   PrepareBasicDownloadItemWithFullPaths(
@@ -4583,7 +4584,7 @@ TEST_F(DeepScanningDownloadTest, SafeVerdictPrecedence) {
     // Create the file.
     base::File file(file_path,
                     base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-    file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+    file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
     NiceMockDownloadItem item;
     PrepareBasicDownloadItemWithFullPaths(
@@ -4995,7 +4996,7 @@ TEST_F(EnterpriseCsdDownloadTest, SkipsConsumerCsdWhenEnabled) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   NiceMockDownloadItem item;
   PrepareBasicDownloadItemWithFullPaths(
@@ -5043,7 +5044,7 @@ TEST_F(EnterpriseCsdDownloadTest, PopulatesCsdFieldWhenEnabled) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   NiceMockDownloadItem item;
   PrepareBasicDownloadItemWithFullPaths(
@@ -5091,7 +5092,7 @@ TEST_F(EnterpriseCsdDownloadTest, StillDoesMetadataCheckForLargeFile) {
 
   // Create the file.
   base::File file(file_path, base::File::FLAG_CREATE | base::File::FLAG_WRITE);
-  file.WriteAtCurrentPos(file_contents.data(), file_contents.size());
+  file.WriteAtCurrentPos(base::as_byte_span(file_contents));
 
   NiceMockDownloadItem item;
   PrepareBasicDownloadItemWithFullPaths(

commit c0a5b5c2c0504b99bcc6552537e5a82c567348e9

Blobs: fix error when partial-copying large file blobs.
Regressed in f9659c2f1b20b149c5daa0a8cd4ee15006fe7a19
Tests in follow-up.

Bug: 369670458
Change-Id: Idcdf512ccfc3e521ca83a4f5f5d5008e13fa38bd
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963322
Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1373896}

storage/browser/blob/write_blob_to_file.cc
@@ -137,7 +137,9 @@ bool CopyFileContentsWithOffsetAndSize(base::File* infile,
       span_to_write = span_to_write.subspan(bytes_written_partial.value());
       *bytes_copied += bytes_written_partial.value();
     }
-    return true;
+    if (checked_max_size.ValueOrDie() == 0) {
+      return true;
+    }
   }
 
   NOTREACHED();