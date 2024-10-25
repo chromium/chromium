78e72e74de5c1c241cd10a5178bde94260f5f822
James Zern <jzern@chromium.org>
2024-10-25 17:44:04 +0000

    Roll src/third_party/libvpx/source/libvpx/ c21ed8d7a..f7ccdd15c (1 commit)
    https://chromium.googlesource.com/webm/libvpx.git/+log/c21ed8d7a675..f7ccdd15c0ea
    
    $ git log c21ed8d7a..f7ccdd15c --date=short --no-merges --format='%ad %ae %s'
    2024-10-22 jzern args.c: make arg_init() static
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    
    Bug: b:308446709
    Change-Id: If5ba5ad1c61438a0a6a2fcc646fd4438d902f36b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5967033
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Auto-Submit: James Zern <jzern@google.com>
    Commit-Queue: James Zern <jzern@google.com>
    Cr-Commit-Position: refs/heads/main@{#1374017}

--- third_party/libvpx/source/config/vpx_version.h
+++ third_party/libvpx/source/config/vpx_version.h
@@ -4,9 +4,9 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 14
 #define VERSION_PATCH 1
-#define VERSION_EXTRA "383-gc21ed8d7a"
+#define VERSION_EXTRA "384-gf7ccdd15c"
 #define VERSION_PACKED \
   ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | (VERSION_PATCH))
-#define VERSION_STRING_NOSP "v1.14.1-383-gc21ed8d7a"
-#define VERSION_STRING " v1.14.1-383-gc21ed8d7a"
+#define VERSION_STRING_NOSP "v1.14.1-384-gf7ccdd15c"
+#define VERSION_STRING " v1.14.1-384-gf7ccdd15c"
 #endif  // VPX_VERSION_H_
