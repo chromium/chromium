75a123f64678e3327c5ccc452494617400728299
Thomas Lukaszewicz <tluk@chromium.org>
2024-10-27 19:54:19 +0000

    [webui] Introduce utilities to manage BrowserWindowInterface
    WebUI clients will be able to retrieve and listen for changes to
    their embedding BrowserWindowInterface.
    
    It is the responsibility of embedders (typically the WebUI
    embedding frameworks such as the WebUI Bubble and Side Panel
    infrastructure) to call SetBrowserWindowInterface() when the
    browser associated with a WebContents they manage changes.
    
    This CL only adds the utilities and relevant tests. See
    downstream CLs for an example of how this will be used practically.
    
    See go/webui-embedding-context for details.
    
    Bug: 360724768
    Change-Id: I8f5f8859907084093f93b1d965c907b612e87a37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960779
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374418}

---
+++ chrome/browser/ui/webui/webui_embedding_context.cc
@@ -0,0 +1,102 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/webui_embedding_context.h"
+
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_user_data.h"
+
+namespace webui {
+namespace {
+
+// Responsible for holding the BrowserWindowInterface for the embedded
+// WebContents and notifing downstream clients of context changes.
+class EmbedderContextData
+    : public content::WebContentsUserData<EmbedderContextData> {
+ public:
+  EmbedderContextData(const EmbedderContextData&) = delete;
+  EmbedderContextData& operator=(const EmbedderContextData&) = delete;
+  ~EmbedderContextData() override = default;
+
+  static EmbedderContextData* GetOrCreate(content::WebContents* web_contents) {
+    EmbedderContextData::CreateForWebContents(web_contents);
+    return EmbedderContextData::FromWebContents(web_contents);
+  }
+
+  // Updates `browser_window_interface_` and registers/notifies listeners if
+  // appropriate.
+  void SetBrowserWindowInterface(
+      BrowserWindowInterface* browser_window_interface) {
+    if (browser_window_interface_ == browser_window_interface) {
+      return;
+    }
+    browser_did_close_subscription_.reset();
+    browser_window_interface_ = browser_window_interface;
+
+    if (browser_window_interface_) {
+      browser_did_close_subscription_ =
+          browser_window_interface_->RegisterBrowserDidClose(
+              base::BindRepeating(&EmbedderContextData::OnBrowserDidClose,
+                                  base::Unretained(this)));
+    }
+    context_change_callbacks_.Notify();
+  }
+
+  BrowserWindowInterface* browser_window_interface() {
+    return browser_window_interface_;
+  }
+
+  base::CallbackListSubscription RegisterBrowserWindowInterfaceChanged(
+      base::RepeatingClosure context_changed_cb) {
+    return context_change_callbacks_.Add(std::move(context_changed_cb));
+  }
+
+ private:
+  friend class content::WebContentsUserData<EmbedderContextData>;
+
+  explicit EmbedderContextData(content::WebContents* web_contents)
+      : WebContentsUserData<EmbedderContextData>(*web_contents) {}
+
+  void OnBrowserDidClose(BrowserWindowInterface* browser_window_interface) {
+    CHECK_EQ(browser_window_interface_, browser_window_interface);
+    SetBrowserWindowInterface(nullptr);
+  }
+
+  raw_ptr<BrowserWindowInterface> browser_window_interface_ = nullptr;
+
+  // Notifies this when `browser_window_interface_` has closed.
+  std::optional<base::CallbackListSubscription> browser_did_close_subscription_;
+
+  // Client registrations for changes to `browser_window_interface_`.
+  base::RepeatingCallbackList<void()> context_change_callbacks_;
+
+  WEB_CONTENTS_USER_DATA_KEY_DECL();
+};
+
+WEB_CONTENTS_USER_DATA_KEY_IMPL(EmbedderContextData);
+
+}  // namespace
+
+void SetBrowserWindowInterface(
+    content::WebContents* host_contents,
+    BrowserWindowInterface* browser_window_interface) {
+  EmbedderContextData::GetOrCreate(host_contents)
+      ->SetBrowserWindowInterface(browser_window_interface);
+}
+
+BrowserWindowInterface* GetBrowserWindowInterface(
+    content::WebContents* host_contents) {
+  return EmbedderContextData::GetOrCreate(host_contents)
+      ->browser_window_interface();
+}
+
+base::CallbackListSubscription RegisterBrowserWindowInterfaceChanged(
+    content::WebContents* host_contents,
+    base::RepeatingClosure context_changed_cb) {
+  return EmbedderContextData::GetOrCreate(host_contents)
+      ->RegisterBrowserWindowInterfaceChanged(std::move(context_changed_cb));
+}
+
+}  // namespace webui
---
+++ chrome/browser/ui/webui/webui_embedding_context.h
@@ -0,0 +1,44 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_WEBUI_EMBEDDING_CONTEXT_H_
+#define CHROME_BROWSER_UI_WEBUI_WEBUI_EMBEDDING_CONTEXT_H_
+
+#include "base/callback_list.h"
+#include "base/functional/callback_forward.h"
+#include "base/memory/raw_ptr.h"
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+class BrowserWindowInterface;
+
+// These utilities help manage embedding context relevant to a `host_contents`
+// for WebUI clients. `host_contents` is the WebContents object managed by
+// embedders that owns the WebUI.
+
+namespace webui {
+
+// Called by embedders to set the browser for `host_contents`.
+void SetBrowserWindowInterface(
+    content::WebContents* host_contents,
+    BrowserWindowInterface* browser_window_interface);
+
+// Gets the interface for the embedding browser. Clients should not persist this
+// reference. May be null.
+BrowserWindowInterface* GetBrowserWindowInterface(
+    content::WebContents* host_contents);
+
+// The associated browser window interface can be updated or nullified as the
+// embedding context changes (e.g. a WebContents hosting a WebUI is added to,
+// removed from or transferred to a browser window). Clients should register a
+// context-changed subscription and respond to these events appropriately.
+base::CallbackListSubscription RegisterBrowserWindowInterfaceChanged(
+    content::WebContents* host_contents,
+    base::RepeatingClosure context_changed_cb);
+
+}  // namespace webui
+
+#endif  // CHROME_BROWSER_UI_WEBUI_WEBUI_EMBEDDING_CONTEXT_H_
