3448a68ec0a44945bc004aa2a212c64e43035376
Sammie Quon <sammiequon@chromium.org>
2024-10-28 19:02:40 +0000

    coral: Use the app restore data to launch apps
    This allows apps to launch in their old window states, bounds, etc.
    Apps can have multiple windows, so currently this just takes the first
    window, as multi instance apps are quite rare. The window id should
    be passed to the coral backend in a follow up and be returned with the
    app id, as it also an identifier.
    
    Bug: 365839465
    Change-Id: I3c62a386135f0216fad965cf055efd4524802d61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941179
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374775}

--- chrome/browser/ui/ash/wm/coral_browsertest.cc
+++ chrome/browser/ui/ash/wm/coral_browsertest.cc
@@ -35,6 +35,9 @@
 #include "chrome/test/base/ash/util/ash_test_util.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chromeos/ash/services/coral/public/mojom/coral_service.mojom.h"
+#include "components/app_restore/full_restore_read_handler.h"
+#include "components/app_restore/full_restore_save_handler.h"
+#include "components/app_restore/restore_data.h"
 #include "content/public/test/browser_test.h"
 #include "gmock/gmock.h"
 
@@ -105,17 +108,45 @@ class CoralBrowserTest : public InProcessBrowserTest {
   base::test::ScopedFeatureList scoped_feature_list_{features::kCoralFeature};
 };
 
-IN_PROC_BROWSER_TEST_F(CoralBrowserTest, PRE_PostLoginBrowser) {
-  // Ensure that post login overview shows up by having at least one app open
-  // and immediate saving to bypass the 2.5 second throttle.
-  CreateBrowser(ProfileManager::GetActiveUserProfile());
+IN_PROC_BROWSER_TEST_F(CoralBrowserTest, PRE_PostLoginLaunch) {
+  Profile* profile = ProfileManager::GetActiveUserProfile();
+  test::InstallSystemAppsForTesting(profile);
+
+  // Launch some SWA's. We will confirm their window bounds and state in the
+  // real test. This will also ensure that post login overview shows up by
+  // having at least one app open.
+  test::CreateSystemWebApp(profile, SystemWebAppType::FILE_MANAGER);
+  aura::Window* files_window =
+      BrowserList::GetInstance()->GetLastActive()->window()->GetNativeWindow();
+  files_window->SetBounds(gfx::Rect(600, 600));
+
+  test::CreateSystemWebApp(profile, SystemWebAppType::SETTINGS);
+  aura::Window* settings_window =
+      BrowserList::GetInstance()->GetLastActive()->window()->GetNativeWindow();
+  WindowState::Get(settings_window)->Maximize();
+
+  // Immediate save to full restore file to bypass the 2.5 second throttle.
   AppLaunchInfoSaveWaiter::Wait();
 }
 
 // Launches a browser with the expected tabs when the post login coral chip is
 // clicked.
-IN_PROC_BROWSER_TEST_F(CoralBrowserTest, PostLoginBrowser) {
-  test::InstallSystemAppsForTesting(ProfileManager::GetActiveUserProfile());
+IN_PROC_BROWSER_TEST_F(CoralBrowserTest, PostLoginLaunch) {
+  ASSERT_TRUE(BrowserList::GetInstance()->empty());
+
+  Profile* profile = ProfileManager::GetActiveUserProfile();
+
+  // `InstallSystemAppsForTesting()` will uninstall and reinstall SWA's. This
+  // will invalidate full restore data in the read handler (see
+  // `FullRestoreDataHandler::OnAppUpdate()`). We manually clone the data and
+  // add it back, SWA's won't be uninstalled in production code like this.
+  auto* full_restore_read_handler =
+      ::full_restore::FullRestoreReadHandler::GetInstance();
+  auto restore_data =
+      full_restore_read_handler->GetRestoreDataForTesting(profile->GetPath());
+  test::InstallSystemAppsForTesting(profile);
+  full_restore_read_handler->SetRestoreDataForTesting(profile->GetPath(),
+                                                      std::move(restore_data));
 
   // Wait until the chip is visible, it may not be visible while data fetch is
   // underway or the overview animation is still running.
@@ -134,8 +165,9 @@ IN_PROC_BROWSER_TEST_F(CoralBrowserTest, PostLoginBrowser) {
   // TODO(sammiequon): These tabs and apps are currently hardcoded in ash for
   // `switches::kForceBirchFakeCoral`. Update to use a test coral provider
   // instead.
+  BrowserList* browsers = BrowserList::GetInstance();
   EXPECT_TRUE(
-      base::ranges::any_of(*BrowserList::GetInstance(), [](Browser* browser) {
+      base::ranges::any_of(*browsers, [](Browser* browser) {
         TabStripModel* tab_strip_model = browser->tab_strip_model();
         return tab_strip_model->count() == 3 &&
                tab_strip_model->GetWebContentsAt(0)->GetVisibleURL() ==
@@ -145,15 +177,22 @@ IN_PROC_BROWSER_TEST_F(CoralBrowserTest, PostLoginBrowser) {
                tab_strip_model->GetWebContentsAt(2)->GetVisibleURL() ==
                    GURL("https://www.notion.so/");
       }));
-  EXPECT_TRUE(
-      base::ranges::any_of(*BrowserList::GetInstance(), [](Browser* browser) {
-        return IsBrowserForSystemWebApp(browser, SystemWebAppType::SETTINGS);
-      }));
-  EXPECT_TRUE(
-      base::ranges::any_of(*BrowserList::GetInstance(), [](Browser* browser) {
-        return IsBrowserForSystemWebApp(browser,
-                                        SystemWebAppType::FILE_MANAGER);
-      }));
+
+  // Tests that the files and settings SWAs are launched and have their previous
+  // session window bounds.
+  auto files_it = base::ranges::find_if(*browsers, [](Browser* browser) {
+    return IsBrowserForSystemWebApp(browser, SystemWebAppType::FILE_MANAGER);
+  });
+  ASSERT_NE(browsers->end(), files_it);
+  auto settings_it = base::ranges::find_if(*browsers, [](Browser* browser) {
+    return IsBrowserForSystemWebApp(browser, SystemWebAppType::SETTINGS);
+  });
+  ASSERT_NE(browsers->end(), settings_it);
+
+  aura::Window* files_window = (*files_it)->window()->GetNativeWindow();
+  EXPECT_EQ(gfx::Rect(600, 600), files_window->GetBoundsInScreen());
+  aura::Window* settings_window = (*settings_it)->window()->GetNativeWindow();
+  EXPECT_TRUE(WindowState::Get(settings_window)->IsMaximized());
 }
 
 // Tests that clicking the in session coral button opens and activates a new
