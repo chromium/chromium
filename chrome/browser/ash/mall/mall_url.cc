e205fc3f4de20d236aff09249cd712b482781be3
Tim Sergeant <tsergeant@chromium.org>
2024-10-28 00:11:30 +0000

    Mall: Add an Almanac feature flag configured from Finch
    This allows us to change the behavior of the Almanac server based on a
    Finch experiment.
    
    Bug: b:370382529
    Change-Id: Idb077dc98c98c84278ed43ab5b84c3f13e279444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5955810
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374436}

--- chrome/browser/ash/mall/mall_url.cc
+++ chrome/browser/ash/mall/mall_url.cc
@@ -10,16 +10,29 @@
 #include "base/strings/strcat.h"
 #include "chrome/browser/apps/almanac_api_client/device_info_manager.h"
 #include "chrome/browser/apps/almanac_api_client/proto/client_context.pb.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "chromeos/constants/url_constants.h"
 #include "net/base/url_util.h"
 #include "url/gurl.h"
 
 namespace ash {
 
+BASE_FEATURE_PARAM(int,
+                   kMallAlmanacFeatureFlag,
+                   &chromeos::features::kCrosMall,
+                   "almanac_flag_id",
+                   0);
+
 GURL GetMallLaunchUrl(const apps::DeviceInfo& info, std::string_view path) {
   apps::proto::ClientContext context;
   *context.mutable_device_context() = info.ToDeviceContext();
   *context.mutable_user_context() = info.ToUserContext();
+
+  // Mall experiments may define an Almanac feature flag which we attach as part
+  // of the client context, so that it is included in requests to Almanac.
+  if (kMallAlmanacFeatureFlag.Get() != 0) {
+    context.mutable_user_context()->add_flag_ids(kMallAlmanacFeatureFlag.Get());
+  }
 
   std::string encoded_context = base::Base64Encode(context.SerializeAsString());
 
