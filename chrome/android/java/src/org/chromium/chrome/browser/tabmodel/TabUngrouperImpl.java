fce6b81beb8d803c26672ebb5642b7a5517a1a40
Calder Kitagawa <ckitagawa@chromium.org>
2024-10-29 12:36:48 +0000

    [Tab Model] Implement TabUngrouper#ungroupTabs
    This finishes the implementation of TabUngrouperImpl#ungroupTabs to
    protect synced and collaboration tab groups from accidental deletion.
    
    With this CL all the implementations are roughly finished.
    
    Follow ups are required to:
    1) Update text in ActionConfirmationManager and deduplicate down to a
       single dialog message per action.
    2) Add metrics.
    3) Migrate all existing callers to the new API.
    
    Bug: 345854441
    Change-Id: I557042c219b99a9edd1bb0f9f0a330402bbffef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5966881
    Reviewed-by: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375163}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabUngrouperImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabUngrouperImpl.java
@@ -10,12 +10,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.Callback;
 import org.chromium.base.supplier.Supplier;
+import org.chromium.chrome.browser.data_sharing.DataSharingTabGroupUtils;
+import org.chromium.chrome.browser.data_sharing.DataSharingTabGroupUtils.GroupsPendingDestroy;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tabmodel.TabModelRemover.TabModelRemoverFlowHandler;
+import org.chromium.chrome.browser.tasks.tab_management.ActionConfirmationManager;
 import org.chromium.components.browser_ui.widget.ActionConfirmationResult;
+import org.chromium.components.data_sharing.member_role.MemberRole;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /** Implementation of the {@link TabUngrouper} interface. */
 public class TabUngrouperImpl implements TabUngrouper {
@@ -44,12 +52,99 @@ public class TabUngrouperImpl implements TabUngrouper {
             boolean trailing,
             boolean allowDialog,
             @Nullable TabModelActionListener listener) {
-        // TODO(crbug.com/347981662, crbug.com/347981397, crbug.com/345854441): Show a dialog and
-        // create NTPs if needed.
-        PassthroughTabUngrouper.doUngroupTabs(
-                mTabModelRemover.getTabGroupModelFilter(), tabs, trailing);
-        if (listener != null) {
-            listener.onConfirmationDialogResult(ActionConfirmationResult.IMMEDIATE_CONTINUE);
+        UngroupTabsHandler ungroupTabsHandler =
+                new UngroupTabsHandler(
+                        mTabModelRemover.getTabGroupModelFilter(),
+                        mTabModelRemover.getActionConfirmationManager(),
+                        tabs,
+                        trailing,
+                        listener);
+        mTabModelRemover.doTabRemovalFlow(ungroupTabsHandler, allowDialog);
+    }
+
+    private static class UngroupTabsHandler implements TabModelRemoverFlowHandler {
+        private final TabGroupModelFilter mTabGroupModelFilter;
+        private final ActionConfirmationManager mActionConfirmationManager;
+        private final List<Tab> mTabsToUngroup;
+        private final boolean mTrailing;
+        private @Nullable TabModelActionListener mListener;
+
+        UngroupTabsHandler(
+                @NonNull TabGroupModelFilter tabGroupModelFilter,
+                @NonNull ActionConfirmationManager actionConfirmationManager,
+                @NonNull List<Tab> tabsToUngroup,
+                boolean trailing,
+                @Nullable TabModelActionListener listener) {
+            mTabGroupModelFilter = tabGroupModelFilter;
+            mActionConfirmationManager = actionConfirmationManager;
+            mTabsToUngroup = tabsToUngroup;
+            mTrailing = trailing;
+            mListener = listener;
         }
+
+        @Override
+        public @NonNull GroupsPendingDestroy computeGroupsPendingDestroy() {
+            return DataSharingTabGroupUtils.getSyncedGroupsDestroyedByTabRemoval(
+                    mTabGroupModelFilter.getTabModel(), mTabsToUngroup);
+        }
+
+        @Override
+        public void onPlaceholderTabsCreated(@NonNull List<Tab> placeholderTabs) {
+            // Intentional no-op as there is no possibility to undo this operation so the tabs do
+            // not need to be tracked.
+        }
+
+        @Override
+        public void showTabGroupDeletionConfirmationDialog(@NonNull Callback<Integer> onResult) {
+            mActionConfirmationManager.processUngroupAttempt(
+                    adaptOnResultCallback(onResult, takeListener()));
+        }
+
+        @Override
+        public void showCollaborationKeepDialog(
+                @MemberRole int memberRole, @NonNull String title, Callback<Integer> onResult) {
+            if (memberRole == MemberRole.OWNER) {
+                mActionConfirmationManager.processCollaborationOwnerRemoveLastTab(
+                        title, adaptOnResultCallback(onResult, takeListener()));
+            } else if (memberRole == MemberRole.MEMBER) {
+                mActionConfirmationManager.processCollaborationMemberRemoveLastTab(
+                        title, adaptOnResultCallback(onResult, takeListener()));
+            } else {
+                assert false : "Not reached";
+            }
+        }
+
+        @Override
+        public void performAction() {
+            TabGroupModelFilter filter = mTabGroupModelFilter;
+            TabModel tabModel = filter.getTabModel();
+            List<Tab> newTabsToUngroup =
+                    mTabsToUngroup.stream()
+                            .map(tab -> tabModel.getTabById(tab.getId()))
+                            .filter(Objects::nonNull)
+                            .filter(tab -> !tab.isClosing() && filter.isTabInTabGroup(tab))
+                            .collect(Collectors.toList());
+
+            PassthroughTabUngrouper.doUngroupTabs(filter, newTabsToUngroup, mTrailing);
+            if (mListener != null) {
+                mListener.onConfirmationDialogResult(ActionConfirmationResult.IMMEDIATE_CONTINUE);
+            }
+        }
+
+        private @Nullable TabModelActionListener takeListener() {
+            TabModelActionListener listener = mListener;
+            mListener = null;
+            return listener;
+        }
+    }
+
+    private static @NonNull Callback<Integer> adaptOnResultCallback(
+            @NonNull Callback<Integer> callback, @Nullable TabModelActionListener listener) {
+        return (result) -> {
+            callback.onResult(result);
+            if (listener != null) {
+                listener.onConfirmationDialogResult(result);
+            }
+        };
     }
 }
