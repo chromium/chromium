6e85fe5304ca224fecef92eb23546604bd210cad
Giovanni Pezzino <giovax@google.com>
2024-10-29 10:22:30 +0000

    Disable NaclAllow feature by default.
    As part of the NaCl deprecation process, starting from M132 NaCl will be disabled by default also on ChromeOS.
    
    ChromeOS admins have a policy to re-enable NaCl until its final removal that will happen in M139.
    
    BUG: 362424621
    Change-Id: I50c56a5beb6082087b8a68f35dbe4a0397e62134
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953283
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Giovanni Pezzino <giovax@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375122}

--- chrome/browser/chrome_browser_main_extra_parts_nacl_deprecation.cc
+++ chrome/browser/chrome_browser_main_extra_parts_nacl_deprecation.cc
@@ -31,14 +31,7 @@ bool ShouldNaClBeAllowed() {
 
 }  // namespace
 
-BASE_FEATURE(kNaclAllow,
-             "NaclAllow",
-#if BUILDFLAG(IS_CHROMEOS)
-             base::FEATURE_ENABLED_BY_DEFAULT
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-);
+BASE_FEATURE(kNaclAllow, "NaclAllow", base::FEATURE_DISABLED_BY_DEFAULT);
 
 void ChromeBrowserMainExtraPartsNaclDeprecation::PostEarlyInitialization() {
 #if BUILDFLAG(ENABLE_NACL)
