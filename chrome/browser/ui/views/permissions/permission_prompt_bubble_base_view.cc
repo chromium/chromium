b5525744efecb0cb4c19f995870a21511cd768a4
Elias Klim <elklm@chromium.org>
2024-10-29 11:53:07 +0000

    Fix permission chip crash caused by an empty widget
    (cherry picked from commit d50df6573f9bc7e75a72da40a104be6238d4842e)
    
    Bug: 370155429
    Change-Id: Iea4cb396810957dbeff5109809940f53c4decabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953400
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Elias Klim <elklm@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1372757}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975453
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6778@{#1163}
    Cr-Branched-From: b21671ca172dcfd1566d41a770b2808e7fa7cd88-refs/heads/main@{#1368529}

--- chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.cc
+++ chrome/browser/ui/views/permissions/permission_prompt_bubble_base_view.cc
@@ -242,7 +242,10 @@ void PermissionPromptBubbleBaseView::RunButtonCallback(int button_id) {
   if (browser_view && browser_view->GetLocationBarView()->GetChipController() &&
       browser_view->GetLocationBarView()
           ->GetChipController()
-          ->IsPermissionPromptChipVisible()) {
+          ->IsPermissionPromptChipVisible() &&
+      browser_view->GetLocationBarView()
+          ->GetChipController()
+          ->IsBubbleShowing()) {
     ChipController* chip_controller =
         browser_view->GetLocationBarView()->GetChipController();
     switch (button) {
