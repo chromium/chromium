89d07fbf119461281c2fe635e9878b394e120feb
Mohannad Farrag <aymanm@google.com>
2024-10-30 12:53:30 +0000

    Revert "Disable proc_macro2 for host builds"
    This reverts commit d84a13b958d0e6f33c13b7be4ff9d0e1d76784f0.
    
    Reason for revert: Does not compile on AOSP :(
    
    Original change's description:
    > Disable proc_macro2 for host builds
    >
    > libproc_macro2 is not available on linux_musl_arm64. To avoid breaking
    > things for that HostCross target, disable host cross compilation for
    > proc_macro2 as recommended.
    >
    > Bug: b:374842582
    > Change-Id: I88e571e7f3d2ae3f4746c708c370debb34fff15d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953281
    > Reviewed-by: Mohannad Farrag <aymanm@google.com>
    > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1372721}
    
    Bug: b:374842582
    Change-Id: I476e4902f1a0b65b420c1f4bf699868674e0e158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975669
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1375796}

--- components/cronet/gn2bp/gen_android_bp.py
+++ components/cronet/gn2bp/gen_android_bp.py
@@ -248,9 +248,11 @@ additional_args = {
     'cronet_aml_third_party_netty_tcnative_netty_tcnative_so__testing': [
         ('cflags', {"-Wno-error=pointer-bool-conversion"})
     ],
-    'cronet_aml_third_party_apache_portable_runtime_apr__testing': [('cflags', {
-        "-Wno-incompatible-pointer-types-discards-qualifiers",
-    })],
+    'cronet_aml_third_party_apache_portable_runtime_apr__testing': [
+        ('cflags', {
+            "-Wno-incompatible-pointer-types-discards-qualifiers",
+        })
+    ],
     # TODO(b/324872305): Remove when gn desc expands public_configs and update code to propagate the
     # include_dir from the public_configs
     # We had to add the export_include_dirs for each target because soong generates each header
@@ -301,15 +303,8 @@ additional_args = {
         ('errorprone', ('javacflags', {
             "-Xep:ReturnValueIgnored:WARN",
         }))
-    ],
-    # See http://crbug.com/374842582 and http://crbug.com/353739440.
-    'libproc_macro2_cronet_aml_third_party_rust_proc_macro2_v1_lib': [
-        ('host_cross_supported', False)
-    ],
-    # See http://crbug.com/374842582 and http://crbug.com/353739440.
-    'libproc_macro2_cronet_aml_third_party_rust_proc_macro2_v1_lib__testing': [
-        ('host_cross_supported', False)
-    ],
+    ]
+    # end export_include_dir.
 }
 
 _FEATURE_REGEX = "feature=\\\"(.+)\\\""
@@ -732,7 +727,6 @@ class Module(object):
     self.edition = None
     self.rustlibs = set()
     self.proc_macros = set()
-    self.host_cross_supported = None
 
   def to_string(self, output):
     if self.comment:
@@ -797,7 +791,6 @@ class Module(object):
     self._output_field(output, 'crate_root')
     self._output_field(output, 'rustlibs')
     self._output_field(output, 'proc_macros')
-    self._output_field(output, 'host_cross_supported')
     if self.rtti:
       self._output_field(output, 'rtti')
 
