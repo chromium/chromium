3340079d63ae3b4c0fb698705247e10cfab9afb8
Stephen McGruer <smcgruer@chromium.org>
2024-10-30 01:48:42 +0000

    [Payment Request] Clip ViewStack to have properly rounded corners
    Before this CL, Payment Request dialogs were masked to have rounded
    corners, but then drew a rectangular view over it that escaped from the
    masking and 'broke through' the corners. The problem is that ViewStack
    paints to a layer, and there is a known issue (crbug.com/358379367)
    where layers do not clip to their parents correctly.
    
    This CL works around that issue, by explicitly setting rounded corners
    on the ViewStack layer (with a matching radius to the parent).
    
    Note that there is a separate issue where WebViews don't respect
    masks/clips from their parents. As such, Payment Handler dialogs will
    now have the correct clipping on-top but still 'break through' on the
    bottom corners (as the WebView draws over the clip). A follow-up CL will
    also add a workaround for that.
    
    Credit to kerenzhu@ for the fix!
    
    Before: https://screenshot.googleplex.com/8MtfpmJi4KWWAif.png
    After: https://screenshot.googleplex.com/6KCuUoHZxBUEAqJ.png
    
    Bug: 40929358, 358379367
    Change-Id: Ic41d04c88cb28d8fcebdf4e9a20d7d1fa18f5b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973071
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375631}

--- chrome/browser/ui/views/payments/payment_request_dialog_view.cc
+++ chrome/browser/ui/views/payments/payment_request_dialog_view.cc
@@ -35,6 +35,7 @@
 #include "ui/base/mojom/dialog_button.mojom.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/color/color_id.h"
+#include "ui/compositor/layer.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/label.h"
@@ -461,6 +462,13 @@ PaymentRequestDialogView::PaymentRequestDialogView(
   SetLayoutManager(std::make_unique<views::FillLayout>());
 
   view_stack_ = AddChildView(std::make_unique<ViewStack>());
+  // ViewStack paints to a layer, and currently layers don't clip to the bounds
+  // of the window opaque layer. Until this is fixed, we have to set rounded
+  // corners directly here.
+  //
+  // TODO(crbug.com/358379367): Remove once layers obey the clip by default.
+  view_stack_->layer()->SetRoundedCornerRadius(
+      gfx::RoundedCornersF(GetCornerRadius()));
 
   SetupSpinnerOverlay();
 
