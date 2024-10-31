bf01934449fedd1451f10de3b4f40f9b9f1e6baf
mlcui <mlcui@google.com>
2024-10-31 07:12:30 +0000

    scanner: Add unpopulated action class
    This will be used to store actions from the initial response from the
    Scanner service, which are expected to be "unpopulated". As we expect
    unpopulated actions to be quite small, `PopulateToVariant` intentionally
    _copies_ the unpopulated action and does not require an rvalue
    reference.
    
    This will allow `ScannerActionViewModel::ToCallback` to be a repeating
    callback instead of a once callback after this currently unused class is
    integrated in a following CL.
    
    Bug: b:376165802
    Change-Id: I6a6a2100f8be126888b2cf0de2e32349154cf75c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981409
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376138}

---
+++ ash/scanner/scanner_unpopulated_action.cc
@@ -0,0 +1,105 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/scanner/scanner_unpopulated_action.h"
+
+#include <optional>
+#include <utility>
+
+#include "ash/public/cpp/scanner/scanner_action.h"
+#include "base/functional/bind.h"
+#include "base/notreached.h"
+#include "components/manta/proto/scanner.pb.h"
+
+namespace ash {
+
+namespace {
+
+// Converts a proto action - a oneof - into the equivalent variant type in
+// Scanner code.
+// Returns nullopt if the action is unsupported.
+// TODO: b/376165802 - This is duplicated from `scanner_session.cc`. Remove the
+// equivalent code from that file after migrating to `ScannerUnpopulatedAction`.
+std::optional<ScannerAction> ProtoActionToVariant(
+    manta::proto::ScannerAction proto_action) {
+  switch (proto_action.action_case()) {
+    case manta::proto::ScannerAction::kNewEvent:
+      return std::move(*proto_action.mutable_new_event());
+
+    case manta::proto::ScannerAction::kNewContact:
+      return std::move(*proto_action.mutable_new_contact());
+
+    case manta::proto::ScannerAction::kNewGoogleDoc:
+      return std::move(*proto_action.mutable_new_google_doc());
+
+    case manta::proto::ScannerAction::kNewGoogleSheet:
+      return std::move(*proto_action.mutable_new_google_sheet());
+
+    case manta::proto::ScannerAction::kCopyToClipboard:
+      return std::move(*proto_action.mutable_copy_to_clipboard());
+
+    case manta::proto::ScannerAction::ACTION_NOT_SET:
+      return std::nullopt;
+  }
+
+  // This should never be reached, as `action_case()` should always return a
+  // valid enum value. If the oneof field is set to something which is not
+  // recognised by this client, that is indistinguishable from an unknown field,
+  // and the above case should be `ACTION_NOT_SET`.
+  NOTREACHED();
+}
+
+// Called when `PopulateToVariant`'s call to the provided
+// `PopulateToProtoCallback` succeeds.
+void OnPopulatedToProto(
+    ScannerUnpopulatedAction::PopulateToVariantCallback callback,
+    std::optional<manta::proto::ScannerAction> populated_action) {
+  if (!populated_action.has_value()) {
+    std::move(callback).Run(std::nullopt);
+    return;
+  }
+
+  std::move(callback).Run(ProtoActionToVariant(std::move(*populated_action)));
+}
+
+}  // namespace
+
+ScannerUnpopulatedAction::ScannerUnpopulatedAction(
+    manta::proto::ScannerAction unpopulated_action,
+    PopulateToProtoCallback populate_to_proto_callback)
+    : unpopulated_action_(std::move(unpopulated_action)),
+      populate_to_proto_callback_(std::move(populate_to_proto_callback)) {}
+
+std::optional<ScannerUnpopulatedAction> ScannerUnpopulatedAction::FromProto(
+    manta::proto::ScannerAction unpopulated_action,
+    PopulateToProtoCallback populate_to_proto_callback) {
+  if (unpopulated_action.action_case() ==
+      manta::proto::ScannerAction::ACTION_NOT_SET) {
+    return std::nullopt;
+  }
+
+  return ScannerUnpopulatedAction(std::move(unpopulated_action),
+                                  std::move(populate_to_proto_callback));
+}
+
+ScannerUnpopulatedAction::ScannerUnpopulatedAction(
+    const ScannerUnpopulatedAction&) = default;
+ScannerUnpopulatedAction& ScannerUnpopulatedAction::operator=(
+    const ScannerUnpopulatedAction&) = default;
+ScannerUnpopulatedAction::ScannerUnpopulatedAction(ScannerUnpopulatedAction&&) =
+    default;
+ScannerUnpopulatedAction& ScannerUnpopulatedAction::operator=(
+    ScannerUnpopulatedAction&&) = default;
+
+ScannerUnpopulatedAction::~ScannerUnpopulatedAction() = default;
+
+void ScannerUnpopulatedAction::PopulateToVariant(
+    PopulateToVariantCallback callback) const& {
+  // This causes a copy of the unpopulated action to be made.
+  populate_to_proto_callback_.Run(
+      unpopulated_action_,
+      base::BindOnce(&OnPopulatedToProto, std::move(callback)));
+}
+
+}  // namespace ash
