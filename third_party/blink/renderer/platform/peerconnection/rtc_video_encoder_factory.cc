bfb8add3239ec3a7027691575a15871c0d637646
Zhaoliang Ma <zhaoliang.ma@intel.com>
2024-10-31 05:11:53 +0000

    Enable constrained baseline profile for H264 HW coding on ChromeOS
    Bug: None
    Test: choose H264 42e01f in https://orphis.github.io/webrtc-sandbox/simulcast-playground.html
    Change-Id: I832e951799bbf5e611dd1072bbb7e699c753e143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5955373
    Reviewed-by: Jianlin Qiu <jianlin.qiu@intel.com>
    Commit-Queue: Zhaoliang Ma <zhaoliang.ma@intel.com>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376108}

--- third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_factory.cc
+++ third_party/blink/renderer/platform/peerconnection/rtc_video_encoder_factory.cc
@@ -307,12 +307,12 @@ SupportedFormats GetSupportedFormatsInternal(
       supported_formats.profiles.push_back(profile.profile);
       supported_formats.sdp_formats.push_back(std::move(*format));
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 #if BUILDFLAG(IS_WIN)
       const bool kShouldAddH264Cbp =
           base::FeatureList::IsEnabled(kMediaFoundationH264CbpEncoding) &&
           profile.profile == media::VideoCodecProfile::H264PROFILE_BASELINE;
-#elif BUILDFLAG(IS_LINUX)
+#elif BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
       const bool kShouldAddH264Cbp =
           profile.profile == media::VideoCodecProfile::H264PROFILE_BASELINE;
 #endif
