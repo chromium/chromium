fd5b994e86b4ffbbc2f0cfea95e557aacad846d4
Daniel White <danieltwhite@google.com>
2024-11-01 06:43:45 +0000

    [iOS] Deprecate didTraitCollectionChange in OverscrollActionsView
    Since didTraitCollectionChange is deprecated in iOS 17,
    OverscrollActionsView was updated to use the new alternative system,
    registerForTraitChanges.
    
    Fixed: 349391888
    Change-Id: I99bec2d010439dd177e55e925509f6ac25e565e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867497
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376767}

--- ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_view.mm
+++ ios/chrome/browser/overscroll_actions/ui_bundled/overscroll_actions_view.mm
@@ -343,6 +343,13 @@ const CGFloat kActionViewBackgroundColorBrightnessIncognito = 80.0 / 256.0;
                                                 action:@selector(tapGesture:)];
     [_tapGesture setDelegate:self];
     [self addGestureRecognizer:_tapGesture];
+
+    if (@available(iOS 17, *)) {
+      NSArray<UITrait>* traits =
+          TraitCollectionSetForTraits(@[ UITraitUserInterfaceStyle.class ]);
+      [self registerForTraitChanges:traits
+                         withAction:@selector(updateLayerColors)];
+    }
   }
   return self;
 }
@@ -480,10 +487,16 @@ const CGFloat kActionViewBackgroundColorBrightnessIncognito = 80.0 / 256.0;
     [CATransaction commit];
 }
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
+  if (@available(iOS 17, *)) {
+    return;
+  }
+
   [self updateLayerColors];
 }
+#endif
 
 #pragma mark - Private
 
