747a32ba346d6b44064f35faeab781cf2b2995ab
Daniel White <danieltwhite@google.com>
2024-11-01 05:59:18 +0000

    [iOS] Deprecate didTraitCollectionChange in TabGridVC
    Since didTraitCollectionChange is deprecated in iOS 17, TabGridVC was
    updated to use the new alternative system, registerForTraitChanges.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR iOS 16 Deprecation.
    Fixed: 349391710
    Change-Id: I5f891903430dfcce8a935786d4f36a4ba99f4244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855906
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376755}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -239,6 +239,12 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   // Hide the toolbars and the floating button, so they can fade in the first
   // time there's a transition into this view controller.
   [self hideToolbars];
+
+  if (@available(iOS 17, *)) {
+    NSArray<UITrait>* traits = TraitCollectionSetForTraits(nil);
+    [self registerForTraitChanges:traits
+                       withAction:@selector(updateConstraitsOnTraitChange)];
+  }
 }
 
 - (void)viewDidLayoutSubviews {
@@ -273,23 +279,16 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
   return UIStatusBarStyleLightContent;
 }
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
-  if (IsPinnedTabsEnabled()) {
-    [self updatePinnedTabsViewControllerConstraints];
+  if (@available(iOS 17, *)) {
+    return;
   }
-  if ([self.swipeToIncognitoIPH superview] == self.view) {
-    self.swipeToIncognitoIPHBottomConstraint.active = NO;
-    self.swipeToIncognitoIPHBottomConstraint =
-        [self.swipeToIncognitoIPH.bottomAnchor
-            constraintEqualToAnchor:[self shouldUseCompactLayout]
-                                        ? self.bottomToolbar.topAnchor
-                                        : self.regularTabsViewController.view
-                                              .bottomAnchor];
 
-    self.swipeToIncognitoIPHBottomConstraint.active = YES;
-  }
+  [self updateConstraitsOnTraitChange];
 }
+#endif
 
 #pragma mark - UIScrollViewDelegate
 
@@ -1404,6 +1403,23 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
 
   // Actions on both bars should be disabled during dragging.
   self.topToolbar.pageControl.userInteractionEnabled = NO;
+}
+
+- (void)updateConstraitsOnTraitChange {
+  if (IsPinnedTabsEnabled()) {
+    [self updatePinnedTabsViewControllerConstraints];
+  }
+  if ([self.swipeToIncognitoIPH superview] == self.view) {
+    self.swipeToIncognitoIPHBottomConstraint.active = NO;
+    self.swipeToIncognitoIPHBottomConstraint =
+        [self.swipeToIncognitoIPH.bottomAnchor
+            constraintEqualToAnchor:[self shouldUseCompactLayout]
+                                        ? self.bottomToolbar.topAnchor
+                                        : self.regularTabsViewController.view
+                                              .bottomAnchor];
+
+    self.swipeToIncognitoIPHBottomConstraint.active = YES;
+  }
 }
 
 #pragma mark - UIGestureRecognizerDelegate
