f46d2a2ee34fee82906b98f26364245531896597
Tanushree Bisht <tanub@google.com>
2024-11-01 20:35:58 +0000

    Move WebDocumentSubresourceFilter to content/renderer/
    This was previously in content/shared/renderer for usage for the
    Fingerprinting Protection Filter. However, it is no longer needed,
    so it is being moved out of shared.
    
    Bug: 376864424
    Change-Id: I5e8041cb22b8acb6d4077bae3b53aed6760d75a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981922
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Tanushree Bisht <tanub@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377065}

--- components/subresource_filter/content/renderer/web_document_subresource_filter_impl.cc
+++ components/subresource_filter/content/renderer/web_document_subresource_filter_impl.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/subresource_filter/content/shared/renderer/web_document_subresource_filter_impl.h"
+#include "components/subresource_filter/content/renderer/web_document_subresource_filter_impl.h"
 
 #include <memory>
 #include <utility>
