d0d1aa0e680dcdfe8220a158e9d1662597b6bed2
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-04 19:33:24 +0000

    Implement the non-deprecated builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 40249337
    Change-Id: I94c4fc1d55848453715d8073905ad0638ce51a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799817
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377808}

--- chrome/browser/background_sync/background_sync_controller_factory.h
+++ chrome/browser/background_sync/background_sync_controller_factory.h
@@ -28,7 +28,7 @@ class BackgroundSyncControllerFactory : public ProfileKeyedServiceFactory {
   ~BackgroundSyncControllerFactory() override;
 
   // BrowserContextKeyedServiceFactory methods:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
