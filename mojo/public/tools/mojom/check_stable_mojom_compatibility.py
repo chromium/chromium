ae778a8267dfe7b2c38f3ad8af63564d927de60c
Jason Chase <chasej@chromium.org>
2024-11-04 20:04:43 +0000

    Remove obsolete generated file from mojom presubmit
    Follow-up to crrev.com/c/5938988.
    
    The old location of the generated file had to be kept, so that CL
    would pass the presubmit.
    
    Change-Id: Ie4a2f27ec0126c00c616e5a37bd27c34503f7bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942909
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377834}

--- mojo/public/tools/mojom/check_stable_mojom_compatibility.py
+++ mojo/public/tools/mojom/check_stable_mojom_compatibility.py
@@ -83,10 +83,6 @@ def _ValidateDelta(root, delta):
     generated_files_to_skip = {
         ('third_party/blink/public/mojom/runtime_feature_state/'
          'runtime_feature.mojom'),
-        # TODO(chasej@): Remove this entry after CL lands to move to
-        # origin_trials/
-        ('third_party/blink/public/mojom/origin_trial_feature/'
-         'origin_trial_feature.mojom'),
         ('third_party/blink/public/mojom/origin_trials/'
          'origin_trial_feature.mojom'),
     }
