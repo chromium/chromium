6a68ae2f79856a18e50e0f41dc598d3b0546fda3
Titouan Rigoudy <titouan@chromium.org>
2024-11-05 09:37:25 +0000

    [Gardener] Disable testCloseGroupInTabGrid on simulators.
    Bug: 376964632
    Change-Id: I9479feb51c35e9f12be3b60003e5e129cb8943e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993491
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Owners-Override: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Pâris Meuleman <pmeuleman@chromium.org>
    Commit-Queue: Pâris Meuleman <pmeuleman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378184}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_sync_egtest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_sync_egtest.mm
@@ -301,9 +301,15 @@ void CloseGroupAtIndex(int group_cell_index) {
                   @"The number of saved tab groups should be 0.");
 }
 
+// TODO(crbug.com/376964632): Deflake test on simulator.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testCloseGroupInTabGrid DISABLED_testCloseGroupInTabGrid
+#else
+#define MAYBE_testCloseGroupInTabGrid testCloseGroupInTabGrid
+#endif
 // Tests that closing a group in the tab grid reflects the change in the
 // Tab Groups panel.
-- (void)testCloseGroupInTabGrid {
+- (void)MAYBE_testCloseGroupInTabGrid {
   [ChromeEarlGreyUI openTabGrid];
 
   // Creates a tab group with an item at 0.
