360b8599a024f674ac2d8b78e8316571d766cb0c
Zhe Li <zheliooo@google.com>
2024-11-05 00:11:28 +0000

    [TabGroupContextMenu] enable by default
    Bug: 377321806
    Change-Id: I5a9d88daabed5bd21fb03022088af4779f4488e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5987296
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378010}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -965,7 +965,7 @@ BASE_FEATURE(kTabStripGroupCollapseAndroid,
 
 BASE_FEATURE(kTabStripGroupContextMenuAndroid,
              "TabStripGroupContextMenuAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTabStripLayoutOptimization,
              "TabStripLayoutOptimization",
