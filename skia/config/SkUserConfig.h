d07d137371d8e4ae5f4095061af3e623cdf96f77
Jim Van Verth <jvanverth@google.com>
2024-11-06 18:19:31 +0000

    [graphite] Enable new arc renderer in Chrome
    Bug: skia:366426193
    Change-Id: I1cb93321ba7537e08edcb2e9df51a8d35c95b370
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979914
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Commit-Queue: Jim Van Verth <jvanverth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379112}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -231,8 +231,6 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_LEGACY_INITWITHPREV_LAYER_SIZING
 
-#define SK_USE_LEGACY_ARCS_GRAPHITE
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
