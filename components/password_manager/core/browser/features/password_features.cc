4a77da02fc9c6ff9002ece7b96192d7935eed341
Adem Derinel <derinel@google.com>
2024-11-06 15:55:41 +0000

    Update comment for WebAuthnUsePasskeyFromAnotherDeviceInContextMenu
    The feature also depends on PasswordManualFallbackAvailable. Make sure
    the comment captures it.
    
    Change-Id: Ib63f95568b0bc309549e1f9fcf139e236d8fd70c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000072
    Auto-Submit: Adem Derinel <derinel@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379027}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -18,7 +18,8 @@ BASE_FEATURE(kAutoApproveSharedPasswordUpdatesFromSameSender,
 BASE_FEATURE(kAutofillPasswordUserPerceptionSurvey,
              "AutofillPasswordUserPerceptionSurvey",
              base::FEATURE_DISABLED_BY_DEFAULT);
-// Default enabled in M131. Remove in or after M134.
+// Default enabled in M131. Remove in or after M134 and after
+// PasswordManualFallbackAvailable feature is cleaned up.
 BASE_FEATURE(kWebAuthnUsePasskeyFromAnotherDeviceInContextMenu,
              "WebAuthnUsePasskeyFromAnotherDeviceInContextMenu",
              base::FEATURE_ENABLED_BY_DEFAULT);
