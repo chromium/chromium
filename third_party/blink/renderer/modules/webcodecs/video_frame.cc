ba847f53800e348b95a9f106738ff276fda59ef7
Dan Sanders <sandersd@chromium.org>
2024-11-06 23:51:26 +0000

    [webcodecs] Add rotation and flip init options on VideoFrame
    This change implements `rotation` and `flip` options in `VideoFrameInit`
    and `VideoFrameBufferInit`, as described in
    https://github.com/w3c/webcodecs/issues/351#issuecomment-2359389479.
    
    Bug: 40243431
    Change-Id: I9eae4f4f101df7a285abd6575f7271b7589a512c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939863
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379353}

--- third_party/blink/renderer/modules/webcodecs/video_frame.cc
+++ third_party/blink/renderer/modules/webcodecs/video_frame.cc
@@ -669,6 +669,13 @@ VideoFrame* VideoFrame::Create(ScriptState* script_state,
     return nullptr;
   }
 
+  media::VideoTransformation transformation = media::kNoTransformation;
+  bool transformed = false;
+  if (RuntimeEnabledFeatures::WebCodecsOrientationEnabled()) {
+    transformation = media::VideoTransformation(init->rotation(), init->flip());
+    transformed = transformation != media::kNoTransformation;
+  }
+
   constexpr char kAlphaDiscard[] = "discard";
 
   // Special case <video> and VideoFrame to directly use the underlying frame.
@@ -710,7 +717,7 @@ VideoFrame* VideoFrame::Create(ScriptState* script_state,
     // We can't modify frame metadata directly since there may be other owners
     // accessing these fields concurrently.
     if (init->hasTimestamp() || init->hasDuration() || force_opaque ||
-        init->hasVisibleRect() || init->hasDisplayWidth()) {
+        init->hasVisibleRect() || transformed || init->hasDisplayWidth()) {
       auto wrapped_frame = media::VideoFrame::WrapVideoFrame(
           source_frame, wrapped_format, parsed_init.visible_rect,
           parsed_init.display_size);
@@ -735,6 +742,12 @@ VideoFrame* VideoFrame::Create(ScriptState* script_state,
         wrapped_frame->metadata().frame_duration =
             base::Microseconds(init->duration());
       }
+      if (transformed) {
+        wrapped_frame->metadata().transformation =
+            wrapped_frame->metadata()
+                .transformation.value_or(media::kNoTransformation)
+                .add(transformation);
+      }
       source_frame = std::move(wrapped_frame);
     }
 
@@ -747,8 +760,7 @@ VideoFrame* VideoFrame::Create(ScriptState* script_state,
       // Note: It's possible for another realm (Worker) to destroy our handle if
       // this frame was transferred via BroadcastChannel to multiple realms.
       if (local_handle && local_handle->sk_image() && !force_opaque &&
-          !init->hasVisibleRect() && !init->hasDisplayWidth() &&
-          !init->hasDisplayHeight()) {
+          !init->hasVisibleRect() && !transformed && !init->hasDisplayWidth()) {
         sk_image = local_handle->sk_image();
       }
     }
@@ -899,10 +911,8 @@ VideoFrame* VideoFrame::Create(ScriptState* script_state,
   if (init->hasDuration()) {
     frame->metadata().frame_duration = base::Microseconds(init->duration());
   }
-  if (orientation != ImageOrientationEnum::kDefault) {
-    frame->metadata().transformation =
-        ImageOrientationToVideoTransformation(orientation);
-  }
+  frame->metadata().transformation =
+      ImageOrientationToVideoTransformation(orientation).add(transformation);
   return MakeGarbageCollected<VideoFrame>(
       base::MakeRefCounted<VideoFrameHandle>(
           std::move(frame), std::move(sk_image),
@@ -1063,6 +1073,11 @@ VideoFrame* VideoFrame::Create(ScriptState* script_state,
 
   if (init->hasDuration()) {
     frame->metadata().frame_duration = base::Microseconds(init->duration());
+  }
+
+  if (RuntimeEnabledFeatures::WebCodecsOrientationEnabled()) {
+    frame->metadata().transformation =
+        media::VideoTransformation(init->rotation(), init->flip());
   }
 
   return MakeGarbageCollected<VideoFrame>(std::move(frame),
