5322e5d3e5276527e2ac5ee27da20d6d4d2ebf83
Sunggook Chue <sunggch@microsoft.com>
2024-11-07 19:35:21 +0000

    AudioWorkletGlobalScope has valid currentFrame during construction.
    AudioWorkletGlobalScope is created with 0 currentFrame count,
    which make currentTime as 0 even though audio context has
    non zero currentTime.
    
    The fix is to pass the currentFrame info during creation through
    AudioWorkletObjectProxy.
    
    Bug: 372866278
    Change-Id: I717744005588ebc571862367662b0ac2cdb947ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5958713
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379851}

--- third_party/blink/renderer/modules/webaudio/audio_worklet_messaging_proxy.cc
+++ third_party/blink/renderer/modules/webaudio/audio_worklet_messaging_proxy.cc
@@ -99,7 +99,8 @@ AudioWorkletMessagingProxy::CreateObjectProxy(
   return std::make_unique<AudioWorkletObjectProxy>(
       static_cast<AudioWorkletMessagingProxy*>(messaging_proxy),
       parent_execution_context_task_runners,
-      worklet_->GetBaseAudioContext()->sampleRate());
+      worklet_->GetBaseAudioContext()->sampleRate(),
+      worklet_->GetBaseAudioContext()->CurrentSampleFrame());
 }
 
 std::unique_ptr<WorkerThread> AudioWorkletMessagingProxy::CreateWorkerThread() {
--- third_party/blink/renderer/modules/webaudio/audio_worklet_object_proxy.h
+++ third_party/blink/renderer/modules/webaudio/audio_worklet_object_proxy.h
@@ -17,7 +17,8 @@ class AudioWorkletObjectProxy final : public ThreadedWorkletObjectProxy {
  public:
   AudioWorkletObjectProxy(AudioWorkletMessagingProxy*,
                           ParentExecutionContextTaskRunners*,
-                          float context_sample_rate);
+                          float context_sample_rate,
+                          uint64_t context_sample_frame_at_construction);
 
   // Implements WorkerReportingProxy.
   void DidCreateWorkerGlobalScope(WorkerOrWorkletGlobalScope*) override;
@@ -32,6 +33,10 @@ class AudioWorkletObjectProxy final : public ThreadedWorkletObjectProxy {
   CrossThreadPersistent<AudioWorkletGlobalScope> global_scope_;
 
   float context_sample_rate_;
+
+  // The variable gets sets on construction time and it won't be
+  // changed over the course of the AWGS's lifetime.
+  const uint64_t context_sample_frame_at_construction_;
 };
 
 }  // namespace blink
