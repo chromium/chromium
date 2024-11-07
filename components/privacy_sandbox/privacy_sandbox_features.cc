899f67eda469eaba9ddfa3310fc02653f06e73c4
Alex Turner <alexmt@chromium.org>
2024-11-07 15:28:08 +0000

    Enable PrivateAggregationDebugReportingIgnoreSiteExceptions by default
    Currently, Private Aggregation's debug mode availability is tied to a
    third-party cookie settings check. This includes allowing debug mode if
    a top-level site has an exception. This cl enables a feature for
    ignoring that site-level exception; this protects cross-site data that
    may have been written from other sites without an exception.
    
    Bug: 364318217
    Change-Id: I1da1ddfc44eaaa93b47b20c2dd59d288d65a9d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993394
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379700}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -117,7 +117,7 @@ BASE_FEATURE(kPrivateAggregationDebugReportingCookieDeprecationTesting,
 
 BASE_FEATURE(kPrivateAggregationDebugReportingIgnoreSiteExceptions,
              "PrivateAggregationDebugReportingIgnoreSiteExceptions",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPrivacySandboxInternalsDevUI,
              "PrivacySandboxInternalsDevUI",
