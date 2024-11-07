6d0f2a395238f131814cbb97d05e33407758ef44
Chuong Ho <hdchuong@google.com>
2024-11-07 05:17:02 +0000

    picker: show lobster entry below editor entry points
    In picker zero state with some text selection, lobster is displayed
    above editor entry points, which isn't expected.
    
    This CL moves the lobster entry from primary section view into an new
    unnamed section view for lobster. This unnamed section view is
    positioned below primary section view where editor entries live.
    
    Bug: b:377637338
    Change-Id: I974e7d2d50bdaee4f3a2d1b2b5b726f20ff22c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999030
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379464}

--- ash/quick_insert/views/quick_insert_zero_state_view_unittest.cc
+++ ash/quick_insert/views/quick_insert_zero_state_view_unittest.cc
@@ -502,7 +502,7 @@ TEST_F(QuickInsertZeroStateViewTest,
                            kPickerWidth, &asset_fetcher_, &submenu_controller_,
                            &preview_controller_);
 
-  EXPECT_THAT(view.primary_section_view_for_testing(), IsNull());
+  EXPECT_THAT(view.category_section_views_for_testing(), IsEmpty());
 }
 
 TEST_F(QuickInsertZeroStateViewTest, ShowLobsterCategoryAsItemWithSubMenu) {
@@ -519,20 +519,16 @@ TEST_F(QuickInsertZeroStateViewTest, ShowLobsterCategoryAsItemWithSubMenu) {
                            &preview_controller_);
 
   EXPECT_THAT(
-      view.primary_section_view_for_testing(),
-      Pointee(AllOf(
-          Property("GetVisible", &views::View::GetVisible, true),
-          Property(
-              "item_views_for_testing",
-              &QuickInsertSectionView::item_views_for_testing,
-              ElementsAre(AsView<QuickInsertListItemView>(Property(
-                  &QuickInsertListItemView::GetPrimaryTextForTesting,
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-                  l10n_util::GetStringUTF16(IDS_PICKER_LOBSTER_SELECTION_LABEL)
-#else
-                  u""
-#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
-                      )))))));
+      view.category_section_views_for_testing(),
+      ElementsAre(Pair(
+          QuickInsertCategoryType::kLobster,
+          Pointee(AllOf(
+              Property("GetVisible", &views::View::GetVisible, true),
+              Property("item_views_for_testing",
+                       &QuickInsertSectionView::item_views_for_testing,
+                       ElementsAre(AsView<QuickInsertListItemView>(Property(
+                           &QuickInsertListItemView::GetPrimaryTextForTesting,
+                           u"")))))))));
 }
 
 TEST_F(QuickInsertZeroStateViewTest, ShowsCaseTransformationBehindSubmenu) {
