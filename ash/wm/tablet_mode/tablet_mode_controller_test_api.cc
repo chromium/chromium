22c4bacc08e7786b78fea7c6c0d01a356563e13d
Vincent Chiang <vincentchiang@chromium.org>
2024-11-07 18:09:31 +0000

    Notify bluetooth device changes on device added
    The BluetoothDevicesObserver notifies its observers when a connected
    device's configuration changed or when the adapter is powered on/off.
    This misses the case when a device is added to the adapter, causing
    folded back ChromeOS devices to boot into tablet mode despite having a
    bluetooth mouse connected.
    
    Added unit tests for TabletModeController to make sure the observer
    call correctly trigger leaving tablet mode and starting in laptop mode
    when folded back.
    
    Bug: b:362082543
    Change-Id: I65356244a1d69dde2391668a7ffce3936bf2c150
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980092
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Vincent Chiang <vincentchiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379798}

--- ash/wm/tablet_mode/tablet_mode_controller_test_api.cc
+++ ash/wm/tablet_mode/tablet_mode_controller_test_api.cc
@@ -4,12 +4,16 @@
 
 #include "ash/wm/tablet_mode/tablet_mode_controller_test_api.h"
 
+#include <vector>
+
 #include "ash/constants/ash_switches.h"
 #include "ash/shell.h"
 #include "base/command_line.h"
 #include "base/numerics/math_constants.h"
 #include "base/run_loop.h"
 #include "base/time/default_tick_clock.h"
+#include "device/bluetooth/test/mock_bluetooth_adapter.h"
+#include "device/bluetooth/test/mock_bluetooth_device.h"
 #include "ui/events/devices/device_data_manager_test_api.h"
 #include "ui/events/devices/input_device.h"
 #include "ui/events/devices/touchpad_device.h"
@@ -17,6 +21,8 @@
 namespace ash {
 
 namespace {
+
+constexpr char kBluetoothDevicePublicAddress[] = "01:23:45:67:89:AB";
 
 bool IsTabletModeControllerInitialized() {
   return base::CommandLine::ForCurrentProcess()->HasSwitch(
@@ -63,6 +69,42 @@ void TabletModeControllerTestApi::AttachExternalTouchpad() {
   if (!IsTabletModeControllerInitialized()) {
     tablet_mode_controller_->OnInputDeviceConfigurationChanged(
         ui::InputDeviceEventObserver::kTouchpad);
+  }
+}
+
+void TabletModeControllerTestApi::AttachBluetoothMouse(
+    device::MockBluetoothAdapter* bluetooth_adapter) {
+  uint32_t test_vendor_id = 0x1111;
+  uint32_t test_product_id = 0x1112;
+  const char test_device_name[] = "bluetooth mouse";
+
+  // Need to set device to DeviceDataManager since that's the source of device
+  // look up. This will not trigger tablet mode since it is not yet connected to
+  // bt adapter.
+  ui::DeviceDataManagerTestApi().SetMouseDevices({ui::InputDevice(
+      5, ui::InputDeviceType::INPUT_DEVICE_BLUETOOTH, test_device_name, "",
+      base::FilePath(), test_vendor_id, test_product_id, 0)});
+
+  std::unique_ptr<device::MockBluetoothDevice> mock_device =
+      std::make_unique<testing::NiceMock<device::MockBluetoothDevice>>(
+          bluetooth_adapter, /*bluetooth_class=*/0, test_device_name,
+          kBluetoothDevicePublicAddress,
+          /*initially_paired=*/true, /*connected=*/true);
+  ON_CALL(*mock_device, GetDeviceType)
+      .WillByDefault(testing::Return(device::BluetoothDeviceType::MOUSE));
+  ON_CALL(*mock_device, GetVendorID)
+      .WillByDefault(testing::Return(test_vendor_id));
+  ON_CALL(*mock_device, GetProductID)
+      .WillByDefault(testing::Return(test_product_id));
+
+  std::vector<raw_ptr<const device::BluetoothDevice, VectorExperimental>>
+      devices;
+  devices.push_back(mock_device.get());
+  ON_CALL(*bluetooth_adapter, GetDevices)
+      .WillByDefault(testing::Return(devices));
+
+  for (auto& observer : bluetooth_adapter->GetObservers()) {
+    observer.DeviceAdded(bluetooth_adapter, mock_device.get());
   }
 }
 
