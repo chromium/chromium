c02df3773fb43a01a20763517422b7ac6773e8d1
Jihad Hanna <jihadghanna@google.com>
2024-11-07 15:57:23 +0000

    Move GetSkipFieldFillLogMessage to the FieldFillingSkipReason header
    It makes more sense for such a util function to exist in the header.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE This CL only moves code.
    Bug: 40100455
    Change-Id: I3e29d2efa2c644a840dceb10ebe00ae48515be4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993439
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379719}

--- components/autofill/core/browser/form_filler.cc
+++ components/autofill/core/browser/form_filler.cc
@@ -47,49 +47,6 @@ namespace {
 // This is used for sites that change multiple things consecutively.
 constexpr base::TimeDelta kWaitTimeForDynamicForms = base::Milliseconds(200);
 
-std::string_view GetSkipFieldFillLogMessage(
-    FieldFillingSkipReason skip_reason) {
-  switch (skip_reason) {
-    case FieldFillingSkipReason::kNotInFilledSection:
-      return "Skipped: Not part of filled section";
-    case FieldFillingSkipReason::kNotFocused:
-      return "Skipped: Only fill when focused";
-    case FieldFillingSkipReason::kUnrecognizedAutocompleteAttribute:
-      return "Skipped: Unrecognized autocomplete attribute";
-    case FieldFillingSkipReason::kFormChanged:
-      return "Skipped: Form has changed";
-    case FieldFillingSkipReason::kInvisibleField:
-      return "Skipped: Invisible field";
-    case FieldFillingSkipReason::kValuePrefilled:
-      return "Skipped: Value is prefilled";
-    case FieldFillingSkipReason::kUserFilledFields:
-      return "Skipped: User filled the field";
-    case FieldFillingSkipReason::kAlreadyAutofilled:
-      return "Skipped: Field is already autofilled.";
-    case FieldFillingSkipReason::kNoFillableGroup:
-      return "Skipped: Field type has no fillable group";
-    case FieldFillingSkipReason::kRefillNotInInitialFill:
-      return "Skipped: Refill field group different from initial filling group";
-    case FieldFillingSkipReason::kExpiredCards:
-      return "Skipped: Expired expiration date for credit card";
-    case FieldFillingSkipReason::kFillingLimitReachedType:
-      return "Skipped: Field type filling limit reached";
-    case FieldFillingSkipReason::kFieldDoesNotMatchTargetFieldsSet:
-      return "Skipped: The field type does not match the targeted fields.";
-    case FieldFillingSkipReason::kFieldTypeUnrelated:
-      return "Skipped: The field type is not related to the data used for "
-             "filling.";
-    case FieldFillingSkipReason::kNoValueToFill:
-      return "Skipped: No value to fill.";
-    case FieldFillingSkipReason::kAutofilledValueDidNotChange:
-      return "Skipped: Field already autofilled with same value.";
-    case FieldFillingSkipReason::kNotSkipped:
-      return "Fillable";
-    case FieldFillingSkipReason::kUnknown:
-      NOTREACHED();
-  }
-}
-
 // Returns how many fields with type |field_type| may be filled in a form at
 // maximum.
 size_t TypeValueFormFillingLimit(FieldType field_type) {
@@ -421,17 +378,17 @@ FillingProduct FormFiller::UndoAutofill(
           });
   // Remove the fields to be skipped so that we only pass fields to be modified
   // by the renderer.
-  std::erase_if(fields, [this, &operation,
-                         &cached_fields](const FormFieldData& field) {
-    // Skip not-autofilled fields as undo only acts on autofilled fields.
-    return !field.is_autofilled() ||
-           // Skip fields whose last autofill operations is different than
-           // the one of the trigger field.
-           form_autofill_history_.GetLastFillingOperationForField(
-               field.global_id()) != operation ||
-           // Skip fields that are not cached to avoid unexpected outcomes.
-           !cached_fields.contains(field.global_id());
-  });
+  std::erase_if(
+      fields, [this, &operation, &cached_fields](const FormFieldData& field) {
+        // Skip not-autofilled fields as undo only acts on autofilled fields.
+        return !field.is_autofilled() ||
+               // Skip fields whose last autofill operations is different than
+               // the one of the trigger field.
+               form_autofill_history_.GetLastFillingOperationForField(
+                   field.global_id()) != operation ||
+               // Skip fields that are not cached to avoid unexpected outcomes.
+               !cached_fields.contains(field.global_id());
+      });
 
   for (FormFieldData& field : fields) {
     AutofillField& autofill_field =
