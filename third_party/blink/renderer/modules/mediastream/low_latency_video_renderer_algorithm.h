39ef4acef5d7547473f916a6f37ae90ef7c949cc
Jesse McKenna <jessemckenna@google.com>
2024-11-07 20:59:14 +0000

    Remove LowLatencyVideoRendererAlgorithm feature
    This feature is enabled by default, has existed since at least M108, and
    has no associated Finch experiment.
    
    Because the feature no longer changes max_post_decode_queue_size et al,
    those member variables can be removed and the constants used directly.
    This also means that kMaxConsecutiveFramesToDrop is always zero, so the
    non-zero code path can be removed.
    
    Finally, I noticed that the comments refer to kMaxQueueSize, which was
    later replaced with max_post_decode_queue_size (which can now be the
    constant kMaxPostDecodeQueueSize)
    
    This CL is part of go/code-health-rotation.
    
    Fixed: 356625406
    Change-Id: I3415da6e71f656625f4c3b90400aa7ae14fea925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5996150
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379915}

--- third_party/blink/renderer/modules/mediastream/low_latency_video_renderer_algorithm.h
+++ third_party/blink/renderer/modules/mediastream/low_latency_video_renderer_algorithm.h
@@ -117,15 +117,6 @@ class MODULES_EXPORT LowLatencyVideoRendererAlgorithm {
   Stats stats_;
   std::optional<base::TimeTicks> last_deadline_min_stats_recorded_;
   void RecordAndResetStats();
-  // Maximum post decode queue size which should trigger a max queue size
-  // reduction.
-  uint16_t max_post_decode_queue_size_;
-  // Maximum number of frames to drop when there is a max queue size reduction.
-  // A size of 0 indicates dropping all the frames in the queue.
-  uint16_t max_consecutive_frames_to_drop_;
-  // Count of consecutive rendered frames with a newer frame in the queue which
-  // should force a steady state reduction of one frame.
-  uint16_t reduce_steady_state_queue_size_threshold_;
 };
 
 }  // namespace blink
