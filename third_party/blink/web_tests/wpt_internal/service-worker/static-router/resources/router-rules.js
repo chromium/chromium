e4ac5529356c17e412989c1fb335318910653314
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2024-11-08 00:58:56 +0000

    Move Chromium implementation specific WPT.
    The specification does not define the maximum depth/width.
    However, the current WPT limit them.  Let me move such WPTs to Chromium
    specific locations.
    
    In the coming CL, I will also write the WPT to ask the minimum
    depth/width as a response to:
    https://github.com/w3c/ServiceWorker/pull/1714
    
    Change-Id: I722667fbdc69aed32d8fa66d3e17050bf33c480a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999015
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380071}

--- third_party/blink/web_tests/wpt_internal/service-worker/static-router/resources/router-rules.js
+++ third_party/blink/web_tests/wpt_internal/service-worker/static-router/resources/router-rules.js
@@ -1,3 +1,7 @@
+// The value is coming from:
+// https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/public/common/service_worker/service_worker_router_rule.h;l=28;drc=6f3f85b321146cfc0f9eb81a74c7c2257821461e
+const CONDITION_MAX_RECURSION_DEPTH = 10;
+
 const routerRules = {
   'condition-compatible-regex-pattern': [{
     condition: {urlPattern: new URLPattern({pathname: '/**/direct.txt'})},
@@ -7,6 +11,34 @@ const routerRules = {
     condition: {urlPattern: {search: ':s' }},
     source: 'network'
   }],
+  'condition-invalid-or-condition-depth': (() => {
+    const addOrCondition = (depth) => {
+      if (depth > CONDITION_MAX_RECURSION_DEPTH) {
+        return {urlPattern: '/foo'};
+      }
+      return {
+        or: [addOrCondition(depth + 1)]
+      };
+    };
+    return {condition: addOrCondition(1), source: 'network'};
+  })(),
+  'condition-invalid-not-condition-depth': (() => {
+    const generateNotCondition = (depth) => {
+      if (depth > CONDITION_MAX_RECURSION_DEPTH) {
+        return {
+          urlPattern: '/**/example.txt',
+        };
+      }
+      return {not: generateNotCondition(depth + 1)};
+    };
+    return {condition: generateNotCondition(1), source: 'network'};
+  })(),
+  'condition-invalid-router-size': [...Array(512)].map((val, i) => {
+    return {
+      condition: {urlPattern: `/foo-${i}`},
+      source: 'network'
+    };
+  }),
 };
 
 export {routerRules};
