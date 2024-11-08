135946fe90e321645e1ce9d6a8faf9081ca41900
Yuwei Huang <yuweih@chromium.org>
2024-11-08 23:19:04 +0000

    [mac host] Unconditionally load broker service plist
    Currently the broker service plist is only loaded when it's a fresh
    install, or it's a keystone-triggered update while there is at least an
    active host. This means if someone has CRD installed but hasn't set up
    a host, then they won't be able to do so after a keystone update, since
    the host process is not able to start or connect to the agent broker
    process.
    
    This CL fixes this by making the postflight script unconditionally load
    the broker service plist. The broker service won't be started until a
    host process attempts to connect the IPC, since it's on-demand.
    
    Bug: 378143243
    Change-Id: I6bda34d8a0bac206c9ef7ac1636c0a5610fee282
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6006656
    Reviewed-by: Jamie Walch <jamiewalch@chromium.org>
    Auto-Submit: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380685}

--- remoting/host/installer/mac/Scripts/remoting_postflight.sh
+++ remoting/host/installer/mac/Scripts/remoting_postflight.sh
@@ -122,21 +122,26 @@ fi
 rm -rf "$HELPERTOOLS/$HOST_LEGACY_BUNDLE_NAME"
 ln -s "$HELPERTOOLS/$HOST_BUNDLE_NAME" "$HELPERTOOLS/$HOST_LEGACY_BUNDLE_NAME"
 
-# Load the service for each user for whom the service was unloaded in the
+# Load the broker service. It must be loaded unconditionally, since the ME2ME
+# native messaging host won't load it. The service is on-demand and won't be
+# started until a host process connects to
+# chromoting.agent_process_broker_mojo_ipc.
+logger Loading broker service
+launchctl load -w $BROKER_PLIST
+
+# Load the host service for each user for whom the service was unloaded in the
 # preflight script (this includes the root user, in case only the login screen
 # is being remoted and this is a Keystone-triggered update).
-# Also, in case this is a fresh install, load the service for the user running
+# Also, in case this is a fresh install (where $USER is not "root", but the
+# script still has root privileges), load the host service for the user running
 # the installer, so they don't have to log out and back in again.
 if [[ -n "$USER" && "$USER" != "root" ]]; then
   id -u "$USER" >> "$USERS_TMP_FILE"
 fi
 
 if [[ -r "$USERS_TMP_FILE" ]]; then
-  logger Starting broker service
-  launchctl load -w $BROKER_PLIST
-
   for uid in $(sort "$USERS_TMP_FILE" | uniq); do
-    logger Starting service for user "$uid".
+    logger Starting host service for user "$uid".
 
     load="launchctl load -w -S Aqua $PLIST"
     start="launchctl start $SERVICE_NAME"
