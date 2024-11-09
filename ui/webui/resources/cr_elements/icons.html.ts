de0f124e5b8979ad4e8f4b11b0854aa39455c663
Emily Shack <emshack@chromium.org>
2024-11-09 00:46:08 +0000

    Declutter: Update arrow-back icon
    Bug: 378124080
    Change-Id: I299dfe87a68acf41872893c329c5eb1562d6d735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6006221
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380735}

--- ui/webui/resources/cr_elements/icons.html.ts
+++ ui/webui/resources/cr_elements/icons.html.ts
@@ -90,7 +90,7 @@ div.innerHTML = getTrustedHTML`
       </g>
       <g id="arrow-back">
         <path
-          d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z">
+          d="m7.824 13 5.602 5.602L12 20l-8-8 8-8 1.426 1.398L7.824 11H20v2Zm0 0">
         </path>
       </g>
       <g id="arrow-drop-up">
