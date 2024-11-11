11a7d812eace0f8678abe2c04c9b4b54afff45b7
Alex Mitra <alexmitra@chromium.org>
2024-11-11 18:06:08 +0000

    Disable testNavigationHistoryNavigationToEvictedPageBFCacheEnabled on P
    This test is causing cascading build failures on android-pie-arm64-rel.
    Example: https://ci.chromium.org/ui/p/chromium/builders/ci/android-pie-arm64-rel/26049
    
    Bug: 378479369
    Change-Id: I49854b63fc5f25880633634d359f6d2c427541d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012362
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Owners-Override: Alex Mitra <alexmitra@chromium.org>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381275}

--- android_webview/javatests/src/org/chromium/android_webview/test/NavigationListenerTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/NavigationListenerTest.java
@@ -4,6 +4,7 @@
 
 package org.chromium.android_webview.test;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.test.InstrumentationRegistry;
@@ -28,6 +29,7 @@ import org.chromium.android_webview.common.AwFeatures;
 import org.chromium.android_webview.test.TestAwContentsClient.OnReceivedTitleHelper;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.components.embedder_support.util.WebResourceResponseInfo;
@@ -948,6 +950,9 @@ public class NavigationListenerTest extends AwParameterizedTest {
     @LargeTest
     @Feature({"AndroidWebView", "NavigationListener"})
     @CommandLineFlags.Add({"enable-features=EnableNavigationListener"})
+    @DisableIf.Build(
+            sdk_equals = Build.VERSION_CODES.P,
+            message = "https://crbug.com/378479369")
     public void testNavigationHistoryNavigationToEvictedPageBFCacheEnabled() throws Throwable {
         mAwContents.getSettings().setBackForwardCacheEnabled(true);
         // Navigation #1: Set up the listener and navigate to `url`. This will create a new page and
