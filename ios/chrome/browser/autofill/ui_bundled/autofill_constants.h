b43d773412366106c4a46103f0692a57eec9ff6a
Vidhan Jain <vidhanj@google.com>
2024-11-11 17:30:40 +0000

    [iOS] Combine constants file in i/c/b/autofill/ui_bundled/*
    Change-Id: I73bacc81825f58d88f38da8a44208f39b4fd6180
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6005006
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381248}

--- ios/chrome/browser/autofill/ui_bundled/autofill_constants.h
+++ ios/chrome/browser/autofill/ui_bundled/autofill_constants.h
@@ -7,6 +7,8 @@
 
 #import <Foundation/Foundation.h>
 
+#import "ios/chrome/browser/shared/ui/list_model/list_model.h"
+
 // Accessibility identifier for the country selection view.
 extern NSString* const kAutofillCountrySelectionTableViewId;
 extern NSString* const kAutofillCountrySelectionSearchScrimId;
@@ -19,6 +21,32 @@ enum class AutofillSaveProfilePromptMode {
   kUpdateProfile,
   // The prompt is for migrating a profile to the account.
   kMigrateProfile
+};
+
+// `AutofillProfileEditTableViewController` and
+// `AutofillSettingsProfileEditTableViewController` both create the same view
+// using the below section identifiers and item types. Identifier for section
+// for autofill profile edit views.
+typedef NS_ENUM(NSInteger, AutofillProfileDetailsSectionIdentifier) {
+  AutofillProfileDetailsSectionIdentifierFields = kSectionIdentifierEnumZero,
+  AutofillProfileDetailsSectionIdentifierName,
+  AutofillProfileDetailsSectionIdentifierAddress,
+  AutofillProfileDetailsSectionIdentifierPhoneEmail,
+  AutofillProfileDetailsSectionIdentifierButton,
+  AutofillProfileDetailsSectionIdentifierMigrationButton,
+  AutofillProfileDetailsSectionIdentifierErrorFooter,
+  AutofillProfileDetailsSectionIdentifierFooter,
+};
+
+// Identifier for item types for autofill profile edit views.
+typedef NS_ENUM(NSInteger, AutofillProfileDetailsItemType) {
+  AutofillProfileDetailsItemTypeTextField = kItemTypeEnumZero,
+  AutofillProfileDetailsItemTypeCountrySelectionField,
+  AutofillProfileDetailsItemTypeError,
+  AutofillProfileDetailsItemTypeFooter,
+  AutofillProfileDetailsItemTypeSaveButton,
+  AutofillProfileDetailsItemTypeMigrateToAccountRecommendation,
+  AutofillProfileDetailsItemTypeMigrateToAccountButton
 };
 
 #endif  // IOS_CHROME_BROWSER_AUTOFILL_UI_BUNDLED_AUTOFILL_CONSTANTS_H_
