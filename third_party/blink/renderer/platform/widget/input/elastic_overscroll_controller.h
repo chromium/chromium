a6c40a7466fce93533a4b82bc79c9ac1d1082deb
Gastón Rodríguez <gastonr@microsoft.com>
2024-11-12 19:28:43 +0000

    Fix single-axis overscroll fling jitter
    This CL is a follow up to CL:5894444. It fixes a different scenario that
    causes jittering when overscrolling.
    
    To repro (in Chrome Canary):
    1. Enable elastic overscroll (chrome://flags/#elastic-overscroll)
    2. Open a webpage. For example, lipsum.com
    3. Do a drag gesture downwards and the sidewards, but don't release your
       finger from the screen.
    4. Right before releasing, fling your finger exactly in one axis such
       that the animation is not diagonal.
    5. The Overscroll controller will "blink" in the different axis you
       flinged (if you flinged sidewards, the content will blink vertically)
       and then execute the rest of the bounce back animation. The correct
       behavior would be to just bounce back into place.
    
    A video showcasing the jitter vs the fix can be found in the linked
    crbug.
    
    The root cause for this issue is that the forward bounce animation has
    no velocity in one of the axis and the stretch function returns zero
    when this is the case, instead of the correct stretched amount.
    
    This CL:
    - Fixes this problem by removing the velocity limitation.
    - Fixes an incorrect time delta subtraction that caused bounce back
      animations to be shorter than they should.
    - Fixes the test implemented in CL:5894444 which was not working
      properly.
    
    Bug: 369653186
    Change-Id: I1be7588a817e2fffa18326a028e6ca7a10b37c13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980379
    Reviewed-by: Rahul Arakeri <arakeri@microsoft.com>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381914}

--- third_party/blink/renderer/platform/widget/input/elastic_overscroll_controller.h
+++ third_party/blink/renderer/platform/widget/input/elastic_overscroll_controller.h
@@ -119,7 +119,9 @@ class PLATFORM_EXPORT ElasticOverscrollController {
   FRIEND_TEST_ALL_PREFIXES(ElasticOverscrollControllerBezierTest,
                            NoSyntheticEventsOverscroll);
   FRIEND_TEST_ALL_PREFIXES(ElasticOverscrollControllerBezierTest,
-                           VerifyTwoAxisForwardAnimation);
+                           VerifyDifferentDurationForwardAnimations);
+  FRIEND_TEST_ALL_PREFIXES(ElasticOverscrollControllerBezierTest,
+                           VerifyOneAxisForwardAnimation);
 
   enum State {
     // The initial state, during which the overscroll amount is zero and
