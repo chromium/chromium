2959fed06592a13248f8a8ed5f20e94b581885c0
Kalvin Lee <kdlee@chromium.org>
2024-11-12 12:42:12 +0000

    spanify: Use ranged for in `AbstractTrustworthinessTest`
    `spanify` wants to rewrite the `inputs` array but appears to have
    trouble finding the array size. This CL is a no-op and simply removes
    the impetus for `spanify` to act by using a ranged (rather than indexed)
    for loop.
    
    Bug: 364338808
    Change-Id: I746fcff9ce0c26accf9ff60f91dec2357b64132c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012379
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381709}

--- services/network/public/cpp/is_potentially_trustworthy_unittest.h
+++ services/network/public/cpp/is_potentially_trustworthy_unittest.h
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef SERVICES_NETWORK_PUBLIC_CPP_IS_POTENTIALLY_TRUSTWORTHY_UNITTEST_H_
 #define SERVICES_NETWORK_PUBLIC_CPP_IS_POTENTIALLY_TRUSTWORTHY_UNITTEST_H_
 
@@ -268,7 +263,7 @@ TYPED_TEST_P(AbstractTrustworthinessTest, TestcasesInheritedFromBlink) {
     const char* url;
   };
 
-  TestCase inputs[] = {
+  static constexpr TestCase inputs[] = {
       // Access is granted to webservers running on localhost.
       {true, true, "http://localhost"},
       {true, true, "http://localhost."},
@@ -360,20 +355,19 @@ TYPED_TEST_P(AbstractTrustworthinessTest, TestcasesInheritedFromBlink) {
       {false, false, "data:text/html,Hello"},
   };
 
-  for (size_t i = 0; i < std::size(inputs); ++i) {
-    SCOPED_TRACE(inputs[i].url);
-    auto origin = this->CreateOriginFromString(inputs[i].url);
-    EXPECT_EQ(inputs[i].is_potentially_trustworthy,
+  for (const auto& [is_potentially_trustworthy, is_localhost, url] : inputs) {
+    SCOPED_TRACE(url);
+    auto origin = this->CreateOriginFromString(url);
+    EXPECT_EQ(is_potentially_trustworthy,
               this->IsOriginPotentiallyTrustworthy(origin));
-    EXPECT_EQ(inputs[i].is_localhost, this->IsOriginOfLocalhost(origin));
+    EXPECT_EQ(is_localhost, this->IsOriginOfLocalhost(origin));
 
-    GURL test_gurl(inputs[i].url);
+    GURL test_gurl(url);
     if (!(test_gurl.SchemeIsBlob() || test_gurl.SchemeIsFileSystem())) {
       // Check that the origin's notion of localhost matches //net's notion of
       // localhost. This is skipped for blob: and filesystem: URLs since
       // blink::SecurityOrigin uses their inner URL's origin.
-      EXPECT_EQ(net::IsLocalhost(GURL(inputs[i].url)),
-                this->IsOriginOfLocalhost(origin));
+      EXPECT_EQ(net::IsLocalhost(GURL(url)), this->IsOriginOfLocalhost(origin));
     }
   }
 }
