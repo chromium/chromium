90aa07dc400d2d41ad6f77fe4a564a9bd0b46adb
Fr <beaufort.francois@gmail.com>
2024-11-12 17:27:48 +0000

    Revert "WebGPU: Use Adapter GetFeatures() instead of EnumerateFeatures()"
    This reverts commit e4c4e4d8d3b15b15bd41ebc393f296186a0c0d33.
    
    Reason for revert: Both https://dawn-review.googlesource.com/c/dawn/+/214114 
    and this CL should have been reverted.
    
    Original change's description:
    > WebGPU: Use Adapter GetFeatures() instead of EnumerateFeatures()
    >
    > Bug: 368672123
    > Change-Id: I63ce2353b01d4a871031eb6488f514dbf81628e0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5958567
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Commit-Queue: Fr <beaufort.francois@gmail.com>
    > Reviewed-by: Maggie Chen <magchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1378476}
    
    Bug: 368672123
    Change-Id: I8890f15d7c43074aaa70d96227df17871ab1f8e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011801
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381840}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -375,10 +375,6 @@ class WebGPUDecoderImpl final : public WebGPUDecoder {
   WGPUBool AdapterHasFeatureImpl(WGPUAdapter adapter, WGPUFeatureName feature);
   size_t AdapterEnumerateFeaturesImpl(WGPUAdapter adapter,
                                       WGPUFeatureName* features);
-  void AdapterGetFeaturesImpl(WGPUAdapter adapter,
-                              WGPUSupportedFeatures* features_out);
-  void SupportedFeaturesFreeMembersImpl(
-      WGPUSupportedFeatures supported_features);
   template <typename CallbackInfo>
   WGPUFuture RequestDeviceImpl(WGPUAdapter adapter,
                                const WGPUDeviceDescriptor* descriptor,
@@ -1193,16 +1189,6 @@ WebGPUDecoderImpl::WebGPUDecoderImpl(
     return parent_decoder->AdapterEnumerateFeaturesImpl(
         std::forward<decltype(args)>(args)...);
   };
-  wire_procs.adapterGetFeatures = [](auto... args) {
-    DCHECK(parent_decoder);
-    return parent_decoder->AdapterGetFeaturesImpl(
-        std::forward<decltype(args)>(args)...);
-  };
-  wire_procs.supportedFeaturesFreeMembers = [](auto... args) {
-    DCHECK(parent_decoder);
-    return parent_decoder->SupportedFeaturesFreeMembersImpl(
-        std::forward<decltype(args)>(args)...);
-  };
   wire_procs.adapterRequestDevice2 = [](auto... args) {
     DCHECK(parent_decoder);
     return parent_decoder->RequestDeviceImpl(
@@ -1415,35 +1401,6 @@ size_t WebGPUDecoderImpl::AdapterEnumerateFeaturesImpl(
     memcpy(features_out, features.data(), sizeof(wgpu::FeatureName) * count);
   }
   return count;
-}
-
-void WebGPUDecoderImpl::AdapterGetFeaturesImpl(
-    WGPUAdapter adapter,
-    WGPUSupportedFeatures* features_out) {
-  wgpu::Adapter adapter_obj(adapter);
-  wgpu::SupportedFeatures supported_features;
-  adapter_obj.GetFeatures(&supported_features);
-
-  std::vector<wgpu::FeatureName> exposed_features;
-  for (uint32_t i = 0; i < supported_features.featureCount; ++i) {
-    wgpu::FeatureName feature = supported_features.features[i];
-    if (IsFeatureExposed(feature)) {
-      exposed_features.push_back(feature);
-    };
-  }
-  const size_t count = exposed_features.size();
-  WGPUFeatureName* features = new WGPUFeatureName[count];
-  uint32_t index = 0;
-  for (wgpu::FeatureName feature : exposed_features) {
-    features[index++] = static_cast<WGPUFeatureName>(feature);
-  }
-  features_out->featureCount = count;
-  features_out->features = features;
-}
-
-void WebGPUDecoderImpl::SupportedFeaturesFreeMembersImpl(
-    WGPUSupportedFeatures supported_features) {
-  delete[] supported_features.features;
 }
 
 template <typename CallbackInfo>
