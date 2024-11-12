d0484047e6dccc610c5297e03694b9fcf42e5c9c
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-11-12 16:05:58 +0000

    Revert "[iOS][QD] Deflake SettingsTestCase/TestClearCookies"
    This reverts commit ad39666766082ddf145d40bcdf58fe53455a0cc6.
    
    Reason for revert: This fix increased flakiness.
    
    Original change's description:
    > [iOS][QD] Deflake SettingsTestCase/TestClearCookies
    >
    > The test is flaky since sometimes it fails to find the element it should
    > wait for. There was a WaitForAppToIdle before the waiting for the
    > element to disappear. Meaning, when WaitForAppToIdle returned, QD was no
    > longer in view so the element is no longer in the tree hierarchy. This
    > CL fixed it my moving the WaitForAppToIdle after the wait for the
    > element to disappear.
    >
    > Fixed: 378085824
    > Change-Id: Ie4cc8dde375278fa4d1fadb4748c547f87d148f6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011127
    > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > Commit-Queue: Sergio Collazos <sczs@chromium.org>
    > Auto-Submit: Filipa Senra <fsenra@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1381256}
    
    Change-Id: Ia976e1f0e819ae99d888cbb943a16ff467204a89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6010899
    Auto-Submit: Elmehdi Rahmaoui <erahmaoui@google.com>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381781}

--- ios/chrome/browser/ui/settings/settings_egtest.mm
+++ ios/chrome/browser/ui/settings/settings_egtest.mm
@@ -115,12 +115,11 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 // scheduled for removal.
 - (void)clearBrowsingData {
   [ChromeEarlGreyUI tapClearBrowsingDataMenuButton:ClearBrowsingDataButton()];
+  [ChromeEarlGreyUI waitForAppToIdle];
 
   // Wait for the browsing data button to disappear.
   [ChromeEarlGrey
       waitForUIElementToDisappearWithMatcher:BrowsingDataButtonMatcher()];
-
-  [ChromeEarlGreyUI waitForAppToIdle];
 }
 
 // From the NTP, clears the cookies and site data via the UI.
@@ -276,7 +275,8 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 // Tests that clearing the cookies through the UI does clear all of them. Use a
 // local server to navigate to a page that sets then tests a cookie, and then
 // clears the cookie and tests it is not set.
-- (void)testClearCookies {
+// TODO(crbug.com/378085824): Reenable the test.
+- (void)DISABLED_testClearCookies {
   // Set pref to the last hour.
   [ChromeEarlGrey
       setIntegerValue:static_cast<int>(browsing_data::TimePeriod::LAST_HOUR)
