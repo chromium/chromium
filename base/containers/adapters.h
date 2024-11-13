130e1dd694768dc034914959debbae98c32b5fdd
Daniel Cheng <dcheng@chromium.org>
2024-11-13 10:12:39 +0000

    Make base::Reversed() play more nicely with generic ranges
    - Satisfy the `std::ranges::sized_range()` concept if the input type
      satisfies the `std::ranges::sized_range()` concept.
    - Don't require the begin and end iterator to be the same type, since
      the `std::ranges::range` concept does not require this.
    
    Bug: 378623811
    Change-Id: I44b9fe40d442118f381fc7ac839a2d0e3877fbcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015245
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382227}

--- base/containers/adapters.h
+++ base/containers/adapters.h
@@ -8,6 +8,7 @@
 #include <stddef.h>
 
 #include <iterator>
+#include <ranges>
 #include <utility>
 
 #include "base/memory/raw_ptr_exclusion.h"
@@ -17,17 +18,29 @@ namespace base {
 namespace internal {
 
 // Internal adapter class for implementing base::Reversed.
+// TODO(crbug.com/378623811): Parts of this (e.g. the `size()` helper) should be
+// extracted to a base template that can be shared/reused. In addition, this
+// should be constrained to Ts that satisfy the std::ranges::range concept.
 template <typename T>
 class ReversedAdapter {
  public:
-  using Iterator = decltype(std::rbegin(std::declval<T&>()));
-
   explicit ReversedAdapter(T& t) : t_(t) {}
   ReversedAdapter(const ReversedAdapter& ra) : t_(ra.t_) {}
   ReversedAdapter& operator=(const ReversedAdapter&) = delete;
 
-  Iterator begin() const { return std::rbegin(t_); }
-  Iterator end() const { return std::rend(t_); }
+  auto begin() { return std::rbegin(t_); }
+  auto begin() const { return std::rbegin(t_); }
+  auto cbegin() const { return std::crbegin(t_); }
+
+  auto end() { return std::rend(t_); }
+  auto end() const { return std::rend(t_); }
+  auto cend() const { return std::crend(t_); }
+
+  auto size() const
+    requires std::ranges::sized_range<T>
+  {
+    return std::ranges::size(t_);
+  }
 
  private:
   // RAW_PTR_EXCLUSION: References a STACK_ALLOCATED class. It is only used
--- base/containers/adapters_unittest.cc
+++ base/containers/adapters_unittest.cc
@@ -4,17 +4,81 @@
 
 #include "base/containers/adapters.h"
 
+#include <ranges>
+#include <utility>
 #include <vector>
 
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
 
+class UnsizedVector {
+ public:
+  UnsizedVector() = default;
+  UnsizedVector(std::initializer_list<int> il) : v_(il) {}
+
+  // Sentinel iterator type that wraps `end()` to ensure this type doesn't
+  // satisfy the `sized_range` concept.
+  template <typename Iterator>
+  class Sentinel {
+   public:
+    Sentinel() = default;
+    explicit Sentinel(Iterator it) : wrapped_it_(it) {}
+
+    bool operator==(Iterator it) const { return wrapped_it_ == it; }
+
+   private:
+    Iterator wrapped_it_;
+  };
+
+  auto begin() const { return v_.begin(); }
+  auto end() const { return Sentinel(v_.end()); }
+  auto rbegin() const { return v_.rbegin(); }
+  auto rend() const { return Sentinel(v_.rend()); }
+  auto begin() { return v_.begin(); }
+  auto end() { return Sentinel(v_.end()); }
+  auto rbegin() { return v_.rbegin(); }
+  auto rend() { return Sentinel(v_.rend()); }
+
+  auto operator[](size_t pos) const { return v_[pos]; }
+  auto operator[](size_t pos) { return v_[pos]; }
+
+ private:
+  std::vector<int> v_;
+};
+
+[[maybe_unused]] void StaticAsserts() {
+  {
+    // A named local variable is more readable than std::declval<T>().
+    [[maybe_unused]] std::vector<int> v;
+    static_assert(std::ranges::range<decltype(base::Reversed(v))>);
+    static_assert(std::ranges::sized_range<decltype(base::Reversed(v))>);
+  }
+
+  {
+    // A named local variable is more readable than std::declval<T>().
+    [[maybe_unused]] UnsizedVector v;
+    static_assert(std::ranges::range<decltype(v)>);
+    static_assert(!std::ranges::sized_range<decltype(v)>);
+    static_assert(std::ranges::range<decltype(base::Reversed(v))>);
+    static_assert(!std::ranges::sized_range<decltype(base::Reversed(v))>);
+  }
+}
+
 TEST(AdaptersTest, Reversed) {
-  std::vector<int> v;
-  v.push_back(3);
-  v.push_back(2);
-  v.push_back(1);
+  std::vector<int> v = {3, 2, 1};
+  int j = 0;
+  for (int& i : base::Reversed(v)) {
+    EXPECT_EQ(++j, i);
+    i += 100;
+  }
+  EXPECT_EQ(103, v[0]);
+  EXPECT_EQ(102, v[1]);
+  EXPECT_EQ(101, v[2]);
+}
+
+TEST(AdaptersTest, ReversedUnsized) {
+  UnsizedVector v = {3, 2, 1};
   int j = 0;
   for (int& i : base::Reversed(v)) {
     EXPECT_EQ(++j, i);
@@ -38,10 +102,7 @@ TEST(AdaptersTest, ReversedArray) {
 }
 
 TEST(AdaptersTest, ReversedConst) {
-  std::vector<int> v;
-  v.push_back(3);
-  v.push_back(2);
-  v.push_back(1);
+  std::vector<int> v = {3, 2, 1};
   const std::vector<int>& cv = v;
   int j = 0;
   for (int i : base::Reversed(cv)) {
