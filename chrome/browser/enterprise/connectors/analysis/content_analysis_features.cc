6088c0dc7d75f7996ccfdcc39f5f621cfbdf3a14
Nancy Xiao <nancylanxiao@google.com>
2024-11-13 16:08:44 +0000

    Create StopRegisterFcm feature flag
    Bug: b/360113709
    Change-Id: Ib2e7fe015dcbfae54e1694938cbaf96834f500e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6017067
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Auto-Submit: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1382359}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_features.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_features.cc
@@ -10,8 +10,16 @@ BASE_FEATURE(kResumableUploadEnabled,
              "ResumableUploadEnabled",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kStopRegisterFcmEnabled,
+             "StopRegisterFcmEnabled",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsResumableUploadEnabled() {
   return base::FeatureList::IsEnabled(kResumableUploadEnabled);
+}
+
+bool IsStopRegisterFcmEnabled() {
+  return base::FeatureList::IsEnabled(kStopRegisterFcmEnabled);
 }
 
 }  // namespace enterprise_connectors
