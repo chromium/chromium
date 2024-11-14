cd1a01a976687779ef1fb699c34ac9d07f4861f8
Dave Tapuska <dtapuska@chromium.org>
2024-11-14 20:46:18 +0000

    [ios] Fix failure building content_shell
    Update constructor for NativeFileInfo to pass an
    additional argument in.
    
    Change-Id: Idb47835665af8bb34b28f345fa8d05ebee615a2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6019826
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383200}

--- content/shell/browser/shell_file_select_helper.cc
+++ content/shell/browser/shell_file_select_helper.cc
@@ -230,7 +230,8 @@ void ShellFileSelectHelper::OnListDone(int error) {
   std::vector<blink::mojom::FileChooserFileInfoPtr> chooser_files;
   for (const auto& file_path : entry->results_) {
     chooser_files.push_back(blink::mojom::FileChooserFileInfo::NewNativeFile(
-        blink::mojom::NativeFileInfo::New(file_path, std::u16string())));
+        blink::mojom::NativeFileInfo::New(file_path, std::u16string(),
+                                          std::vector<std::u16string>())));
   }
 
   listener_->FileSelected(std::move(chooser_files), base_dir_,
@@ -251,8 +252,8 @@ void ShellFileSelectHelper::ConvertToFileChooserFileInfoList(
   for (const auto& file : files) {
     chooser_files.push_back(blink::mojom::FileChooserFileInfo::NewNativeFile(
         blink::mojom::NativeFileInfo::New(
-            file.local_path,
-            base::FilePath(file.display_name).AsUTF16Unsafe())));
+            file.local_path, base::FilePath(file.display_name).AsUTF16Unsafe(),
+            std::vector<std::u16string>())));
   }
 
   listener_->FileSelected(std::move(chooser_files), base::FilePath(),
