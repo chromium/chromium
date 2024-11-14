e8283d52a7ed99a8f0ad531581c2b86bb68181f3
Christopher Cameron <ccameron@chromium.org>
2024-11-14 08:22:11 +0000

    Unify SkNamedTransferFnExt::kSRGB and SkNamedTransferFn::kSRGB
    These two constants historically had slightly different values. Inline
    the value for SkNamedTransferFnExt::kSRGB into gfx::ColorSpace. A
    follow-on change will unify that (and update tests).
    
    Bug: 376758571
    Change-Id: Ie10645b4306fe50d39cc6f9efbbb3ed6603b4e5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011217
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382856}

--- ui/gfx/color_space.cc
+++ ui/gfx/color_space.cc
@@ -679,7 +679,11 @@ sk_sp<SkColorSpace> ColorSpace::ToSkColorSpace(
       return SkColorSpace::MakeSRGBLinear();
   }
 
-  skcms_TransferFunction transfer_fn = SkNamedTransferFnExt::kSRGB;
+  // This is almost equal to SkNamedTransferFunction::kSRGB, but has some slight
+  // rounding differences that some tests depend on. These tests should be
+  // updated.
+  skcms_TransferFunction transfer_fn = {2.4f, 0.947867345704f, 0.052132654296f,
+                                        0.077399380805f, 0.040449937172f};
   switch (transfer_) {
     case TransferID::SRGB:
       break;
