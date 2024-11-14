a5b3dd0eb859952e6a6694f452225da6fbbd4cf6
EmiliaPaz <emiliapaz@chromium.org>
2024-11-14 23:35:40 +0000

    [Extensions] Enable site access requests API by default
    Enable calling the permissions.add|removeSiteAccessRequests API by
    default. This will allow extensions to show site access requests in
    the toolbar. Request will only be shown when a separate flag is
    enabled.
    
    Bug: 330588494
    Change-Id: Iba67cc45db4f05a3eefefa1072c3a1105e7cf8ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023993
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383321}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -25,7 +25,7 @@ BASE_FEATURE(kApiEnterpriseKioskInput,
 
 BASE_FEATURE(kApiPermissionsSiteAccessRequests,
              "ApiPermissionsSiteAccessRequests",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kApiUserScriptsMultipleWorlds,
              "ApiUserScriptsMultipleWorlds",
