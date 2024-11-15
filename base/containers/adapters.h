ed4bf5d5b6bc71202f1921af9628b69b603c0e77
Daniel Cheng <dcheng@chromium.org>
2024-11-15 22:27:51 +0000

    Split implementation details of range adapters into separate header
    Bug: 378623811
    Change-Id: I37e518ff05db6645542e2218f8590741ef88c5d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022068
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383841}

--- base/containers/adapters.h
+++ base/containers/adapters.h
@@ -5,51 +5,9 @@
 #ifndef BASE_CONTAINERS_ADAPTERS_H_
 #define BASE_CONTAINERS_ADAPTERS_H_
 
-#include <stddef.h>
-
-#include <iterator>
-#include <ranges>
-#include <utility>
-
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/containers/adapters_internal.h"
 
 namespace base {
-
-namespace internal {
-
-// Internal adapter class for implementing base::Reversed.
-// TODO(crbug.com/378623811): Parts of this (e.g. the `size()` helper) should be
-// extracted to a base template that can be shared/reused. In addition, this
-// should be constrained to Ts that satisfy the std::ranges::range concept.
-template <typename T>
-class ReversedAdapter {
- public:
-  explicit ReversedAdapter(T& t) : t_(t) {}
-  ReversedAdapter(const ReversedAdapter& ra) : t_(ra.t_) {}
-  ReversedAdapter& operator=(const ReversedAdapter&) = delete;
-
-  auto begin() { return std::rbegin(t_); }
-  auto begin() const { return std::rbegin(t_); }
-  auto cbegin() const { return std::crbegin(t_); }
-
-  auto end() { return std::rend(t_); }
-  auto end() const { return std::rend(t_); }
-  auto cend() const { return std::crend(t_); }
-
-  auto size() const
-    requires std::ranges::sized_range<T>
-  {
-    return std::ranges::size(t_);
-  }
-
- private:
-  // RAW_PTR_EXCLUSION: References a STACK_ALLOCATED class. It is only used
-  // inside for loops. Ideally, the container being iterated over should be the
-  // one held via a raw_ref/raw_ptrs.
-  RAW_PTR_EXCLUSION T& t_;
-};
-
-}  // namespace internal
 
 // Reversed returns a container adapter usable in a range-based "for" statement
 // for iterating a reversible container in reverse order.
