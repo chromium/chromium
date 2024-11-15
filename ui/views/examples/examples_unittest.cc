39cf1d364ca7a3e8ff8b265584ab15fb1ac17b1c
Robert Liao <robliao@chromium.org>
2024-11-15 22:00:58 +0000

    Convert Examples Tests to Use EXPECT_THAT(conditions) For Log Diagnostics
    Before if a test failed, the log would contain:
    
    ..\..\ui\views\examples\examples_unittest.cc(33):
        error: Value of: (exit_code == ExamplesExitCode::kSucceeded) ||
                (exit_code == ExamplesExitCode::kNone)
      Actual: false
    Expected: true
    
    It is not possible to determine what the exit_code was from this output.
    
    Now with this change, it contains:
    
    ..\..\ui\views\examples\examples_unittest.cc(34):
        error: Value of: exit_code
    Expected: (is equal to 4-byte object <00-00 00-00>) or
              (is equal to 4-byte object <03-00 00-00>)
      Actual: 4-byte object <02-00 00-00>, isn't equal to
              4-byte object <00-00 00-00>, and isn't equal to
              4-byte object <03-00 00-00>
    
    This allows for log based diagnostics.
    
    Change-Id: I7358d228fa94393d0f3173d7fe27554e905d2805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6024418
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383831}

--- ui/views/examples/examples_unittest.cc
+++ ui/views/examples/examples_unittest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "build/build_config.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/views/examples/ax_example.h"
 #include "ui/views/examples/examples_exit_code.h"
@@ -19,8 +20,8 @@ namespace views::examples {
 TEST(ExamplesTest, MAYBE_TestViewsExamplesLaunches) {
   const ExamplesExitCode exit_code = ExamplesMainProc(/*under_test=*/true);
   // Check the status of the Skia Gold comparison.
-  EXPECT_TRUE((exit_code == ExamplesExitCode::kSucceeded) ||
-              (exit_code == ExamplesExitCode::kNone));
+  EXPECT_THAT(exit_code, testing::AnyOf(ExamplesExitCode::kSucceeded,
+                                        ExamplesExitCode::kNone));
 }
 
 TEST(ExamplesTest, TestViewsExamplesLaunchesWithArgs) {
@@ -29,8 +30,8 @@ TEST(ExamplesTest, TestViewsExamplesLaunchesWithArgs) {
   const ExamplesExitCode exit_code =
       ExamplesMainProc(/*under_test=*/true, std::move(examples));
   // Check the status of the Skia Gold comparison.
-  EXPECT_TRUE((exit_code == ExamplesExitCode::kSucceeded) ||
-              (exit_code == ExamplesExitCode::kNone));
+  EXPECT_THAT(exit_code, testing::AnyOf(ExamplesExitCode::kSucceeded,
+                                        ExamplesExitCode::kNone));
 }
 
 }  // namespace views::examples
