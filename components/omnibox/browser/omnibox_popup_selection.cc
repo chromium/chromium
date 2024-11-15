dd359c5bec0455c63dce812a6ceb9497021ceb0c
Orin Jaworski <orinj@google.com>
2024-11-15 17:27:43 +0000

    [omnibox] Make IPH Chrome Tip links accessible by selecting with tab key
    Screenshots:
    - [IPH link not selected](http://screen/BVbLuBpBgpY3j4q)
    - [IPH link selected, focus ring around clickable area](http://screen/628oUQocJedaAEa)
    
    (cherry picked from commit cc3acee2a74d1b9c6d924e64e47e900cd96f8323)
    
    Bug: 376545774
    Change-Id: I6f6425c772162839a411e0df19ed1460365a34f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6014231
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1382652}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6024693
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6778@{#2250}
    Cr-Branched-From: b21671ca172dcfd1566d41a770b2808e7fa7cd88-refs/heads/main@{#1368529}

--- components/omnibox/browser/omnibox_popup_selection.cc
+++ components/omnibox/browser/omnibox_popup_selection.cc
@@ -4,12 +4,13 @@
 
 #include "components/omnibox/browser/omnibox_popup_selection.h"
 
+#include <algorithm>
+
 #include "build/build_config.h"
 #include "components/omnibox/browser/actions/omnibox_action.h"
+#include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/autocomplete_result.h"
 #include "components/search_engines/template_url_service.h"
-
-#include <algorithm>
 
 constexpr bool kIsDesktop = !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS);
 
@@ -99,6 +100,8 @@ bool OmniboxPopupSelection::IsControlPresentOnMatch(
       return match.type == AutocompleteMatchType::HISTORY_EMBEDDINGS;
     case FOCUSED_BUTTON_REMOVE_SUGGESTION:
       return match.SupportsDeletion();
+    case FOCUSED_IPH_LINK:
+      return match.IsIPHSuggestion() && !match.iph_link_url.is_empty();
     default:
       break;
   }
@@ -205,6 +208,7 @@ OmniboxPopupSelection::GetAllAvailableSelectionsSorted(
     all_states.push_back(FOCUSED_BUTTON_THUMBS_UP);
     all_states.push_back(FOCUSED_BUTTON_THUMBS_DOWN);
     all_states.push_back(FOCUSED_BUTTON_REMOVE_SUGGESTION);
+    all_states.push_back(FOCUSED_IPH_LINK);
   }
   DCHECK(std::is_sorted(all_states.begin(), all_states.end()))
       << "This algorithm depends on a sorted list of line states.";
