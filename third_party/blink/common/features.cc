866da06f823897ff0b3a8c1d9a634d2b5214d38d
Camillia Smith Barnes <cammie@chromium.org>
2024-11-15 22:49:05 +0000

    [Merge to M132] Shared Storage: Enable SharedStorageSelectURLSavedQueries by default
    Intent to Ship:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/rvYQWbjy-gE/m/lysCiQRkBgAJ
    
    (cherry picked from commit b1a33c0e3dec12891af430a10f8bb655ef6765e4)
    
    Bug: 367440966
    Change-Id: If349ed36252d23ad630c85ff7013f75034aebe13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6019644
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Cammie Smith Barnes <cammie@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1382594}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026430
    Cr-Commit-Position: refs/branch-heads/6834@{#249}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2472,7 +2472,7 @@ BASE_FEATURE(kSharedStorageCreateWorkletCustomDataOrigin,
 
 BASE_FEATURE(kSharedStorageSelectURLSavedQueries,
              "SharedStorageSelectURLSavedQueries",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSharedStorageAPIEnableWALForDatabase,
              "SharedStorageAPIEnableWALForDatabase",
