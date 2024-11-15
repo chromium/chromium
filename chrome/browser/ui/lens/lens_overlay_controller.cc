2ca9ecaacbdabd62092f280f4ff55cb9a43172af
Bryan Nguyen <nguyenbryan@google.com>
2024-11-15 00:13:52 +0000

    [lensoverlay] Fix menu button on Windows.
    The menu button was previously only working on Mac and Linux due to
    crbug.com/378566071. The preselection bubble should be active for the
    menu button to work.
    
    Also fixes a pre-existing issue where closing the bubble using the
    escape key leads to a crash when reshowing the bubble.
    
    Bug: b:374051721, 378565178
    Change-Id: I1ff68c8a7e3539d612653c6d3afa3dc8a28efb3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6013706
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383339}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -2015,6 +2015,7 @@ void LensOverlayController::OnViewBoundsChanged(views::View* observed_view) {
 
 void LensOverlayController::OnWidgetDestroying(views::Widget* widget) {
   preselection_widget_ = nullptr;
+  preselection_widget_observer_.Reset();
 }
 
 void LensOverlayController::OnOmniboxFocusChanged(
@@ -2488,10 +2489,16 @@ void LensOverlayController::ShowPreselectionBubble() {
             weak_factory_.GetWeakPtr(),
             tab_->GetBrowserWindowInterface()->TopContainer(),
             net::NetworkChangeNotifier::IsOffline(),
-            base::BindRepeating(&LensOverlayController::CloseUIAsync,
-                                weak_factory_.GetWeakPtr(),
-                                lens::LensOverlayDismissalSource::
-                                    kPreselectionToastExitButton)));
+            /*exit_clicked_callback=*/
+            base::BindRepeating(
+                &LensOverlayController::CloseUIAsync,
+                weak_factory_.GetWeakPtr(),
+                lens::LensOverlayDismissalSource::kPreselectionToastExitButton),
+            /*on_cancel_callback=*/
+            base::BindOnce(&LensOverlayController::CloseUIAsync,
+                           weak_factory_.GetWeakPtr(),
+                           lens::LensOverlayDismissalSource::
+                               kPreselectionToastEscapeKeyPress)));
     preselection_widget_->SetNativeWindowProperty(
         views::kWidgetIdentifierKey,
         const_cast<void*>(kLensOverlayPreselectionWidgetIdentifier));
