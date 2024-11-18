f7a4885c64111287515db34742279c98838880ed
Emily Shack <emshack@chromium.org>
2024-11-18 20:13:32 +0000

    [M132] Declutter: Fix close tab tooltip
    (cherry picked from commit efe899f3d15c564376d03e1ae8c017abb3a20e6e)
    
    Bug: 378403139
    Change-Id: Id7dc41f607ab482f5058b727352c2c1e6a2a9749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011929
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1381969}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032895
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6834@{#445}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- chrome/browser/resources/tab_search/declutter/declutter_page.ts
+++ chrome/browser/resources/tab_search/declutter/declutter_page.ts
@@ -158,6 +158,10 @@ export class DeclutterPageElement extends CrLitElement {
         'declutterCloseTabAriaLabel', tabData.tab.title);
   }
 
+  protected getCloseButtonTooltip_(): string {
+    return loadTimeData.getString('declutterCloseTabTooltip');
+  }
+
   protected onBackClick_() {
     this.fire('back-click');
   }
