371a5b80f3d3715ea1ba57b6751b28f8d83c27af
Maksim Moskvitin <mmoskvitin@google.com>
2024-11-18 17:43:10 +0000

    [Code Health][iOS] Prepare to move ios/c/b/ui/infobar
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    This is part 1 of 3-sided patch to ensure that downstream dependency
    won't break.
    
    Bug: 324232673
    Change-Id: I4f8a8264effac88e191c63647e67efb20b4c4cf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032549
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1384423}

---
+++ ios/chrome/browser/infobars/ui_bundled/infobar_constants.h
@@ -0,0 +1,10 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INFOBARS_UI_BUNDLED_INFOBAR_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_INFOBARS_UI_BUNDLED_INFOBAR_CONSTANTS_H_
+
+#import "ios/chrome/browser/ui/infobars/infobar_constants.h"
+
+#endif  // IOS_CHROME_BROWSER_INFOBARS_UI_BUNDLED_INFOBAR_CONSTANTS_H_
