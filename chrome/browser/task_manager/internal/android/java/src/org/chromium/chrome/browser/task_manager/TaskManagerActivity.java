e03096d1bd8b835ae3f74a3921d5c638db1e578e
Ritika Gupta <ritikagup@google.com>
2024-11-18 23:42:54 +0000

    Revert "[Task manager] Get memory footprints and show them"
    This reverts commit b39303dcd5132fc179ee0e89932face8aefcc09e.
    
    Reason for revert: Few bots failing.
    
    Original change's description:
    > [Task manager] Get memory footprints and show them
    >
    > Include C++ task manager logic in Android's chrome and use it to
    > retrieve task information (just memory footprints for now) and show them
    > on the task manager activity. Currently only BrowserProcessTaskProvider
    > is enabled on Android as a TaskProvider, leaving the TODO to enable
    > other task providers in task_manager_impl.cc.
    >
    > This change adds several C++ files anew to chrome on android,
    > unavoidably increasing the binary size.
    >
    > Low-Coverage-Reason: EXPERIMENTAL_CODE The UI is temporary and the bridge is just JNI bind
    > Bug: 353596679
    > Binary-Size: Size increase is unavoidable (see above).
    > Change-Id: Ic943c055036ae14121674b3911472ea06cebc689
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012138
    > Commit-Queue: Keigo Oka <oka@chromium.org>
    > Auto-Submit: Keigo Oka <oka@chromium.org>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: Michael Wojcicka <mwoj@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1384585}
    
    Bug: 379746458
    Change-Id: I4d7e9bac0718379f9d64597d084b5cb82029ab6c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032016
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Owners-Override: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1384648}

--- chrome/browser/task_manager/internal/android/java/src/org/chromium/chrome/browser/task_manager/TaskManagerActivity.java
+++ chrome/browser/task_manager/internal/android/java/src/org/chromium/chrome/browser/task_manager/TaskManagerActivity.java
@@ -9,75 +9,24 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Map;
-import java.util.TreeMap;
+import org.chromium.base.Log;
 
 public class TaskManagerActivity extends AppCompatActivity {
     private static final String TAG = "TaskManager";
-    private static final int REFRESH_TIME_MS = 1000;
-
-    private TaskManagerServiceBridge mBridge = new TaskManagerServiceBridge();
-    private TaskManagerServiceBridge.ObserverHandle mObserverHandle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Log.i(TAG, "onCreate");
+
+        // Feel free to remove the following code on implementing the real task manager UI.
         setContentView(R.layout.task_manager_activity);
-
-        TaskManagerObserver observer = createObserver();
-
-        mObserverHandle =
-                mBridge.addObserver(observer, REFRESH_TIME_MS, RefreshType.MEMORY_FOOTPRINT);
-    }
-
-    private TaskManagerObserver createObserver() {
-        // Feel free to update the following code on implementing the real task manager UI.
-
         TextView textView = findViewById(R.id.five);
 
-        Map<Long, Long> mem = new TreeMap<Long, Long>();
-        return new TaskManagerObserver() {
-            @Override
-            public void onTaskAdded(long id) {}
+        TaskManagerServiceBridge bridge = new TaskManagerServiceBridge();
+        int five = bridge.getFive();
 
-            @Override
-            public void onTaskToBeRemoved(long id) {}
-
-            @Override
-            public void onTasksRefreshed(long[] taskIds) {
-                for (long taskId : taskIds) {
-                    mem.put(taskId, mBridge.getMemoryFootprintUsage(taskId));
-                }
-                StringBuilder txt = new StringBuilder();
-                for (Map.Entry<Long, Long> e : mem.entrySet()) {
-                    txt.append("Task ");
-                    txt.append(e.getKey());
-                    txt.append("'s memory footprint usage = ");
-                    txt.append(e.getValue());
-                    txt.append("\n");
-                }
-                textView.setText(txt.toString());
-            }
-
-            @Override
-            public void onTasksRefreshedWithBackgroundCalculations(long[] taskIds) {}
-
-            @Override
-            public void onTaskUnresponsive(long id) {}
-
-            @Override
-            public void onActiveTaskFetched(long id) {}
-        };
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        if (mObserverHandle != null) {
-            mBridge.removeObserver(mObserverHandle);
-            mObserverHandle = null;
-        }
+        textView.setText(String.valueOf(five));
     }
 }
--- chrome/browser/task_manager/internal/android/java/src/org/chromium/chrome/browser/task_manager/TaskManagerServiceBridge.java
+++ chrome/browser/task_manager/internal/android/java/src/org/chromium/chrome/browser/task_manager/TaskManagerServiceBridge.java
@@ -6,60 +6,15 @@ package org.chromium.chrome.browser.task_manager;
 
 import org.jni_zero.NativeMethods;
 
-/**
- * This class acts as a bridge between Java and the C++ task manager, providing the methods for
- * receiving updates and fetching specific task information (e.g., memory usage) from the C++ task
- * manager logic.
- */
 class TaskManagerServiceBridge {
-    /**
-     * Adds the observer.
-     *
-     * @param refreshTimeMillis Specifies how often the observer should be notified. See
-     *     TaskManagerObserver in task_manager_observer.h for specifics.
-     * @param refreshType Specifies the types of resources that the observer should receive. See
-     *     RefreshType in task_manager_observer.h for details.
-     * @return The handle of the observer, which should be used on calling removeObserver.
-     */
-    ObserverHandle addObserver(
-            TaskManagerObserver observer, int refreshTimeMillis, @RefreshType int refreshType) {
-        long pointer =
-                TaskManagerServiceBridgeJni.get()
-                        .addObserver(observer, refreshTimeMillis, refreshType);
-        return new ObserverHandle(pointer);
-    }
-
-    /** Removes the observer. */
-    void removeObserver(ObserverHandle handle) {
-        TaskManagerServiceBridgeJni.get().removeObserver(handle.getPointer());
-    }
-
-    // Following methods for fetching task information mirror the functions in
-    // task_manager_interface.h. See the file for details.
-
-    long getMemoryFootprintUsage(long taskId) {
-        return TaskManagerServiceBridgeJni.get().getMemoryFootprintUsage(taskId);
-    }
-
-    static class ObserverHandle {
-        private long mPointer;
-
-        ObserverHandle(long pointer) {
-            mPointer = pointer;
-        }
-
-        long getPointer() {
-            return mPointer;
-        }
-    }
 
     @NativeMethods
     interface Natives {
-        long addObserver(
-                TaskManagerObserver observer, int refreshTimeMillis, @RefreshType int refreshType);
+        int five();
+    }
 
-        void removeObserver(long pointer);
-
-        long getMemoryFootprintUsage(long taskId);
+    int getFive() {
+        Natives jni = TaskManagerServiceBridgeJni.get();
+        return jni.five();
     }
 }
