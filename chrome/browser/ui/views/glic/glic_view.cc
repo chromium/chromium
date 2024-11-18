6c8347461c0696be76a01adac52fda4a5e2eb68a
Anthony Cui <cuianthony@google.com>
2024-11-18 22:47:34 +0000

    Add initial view for glic UI
    Bug: 377583620
    Change-Id: Ice85c43466eea242ed6811c6b2ccdb44e17e5884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026683
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384612}

---
+++ chrome/browser/ui/views/glic/glic_view.cc
@@ -0,0 +1,44 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/glic/glic_view.h"
+
+#include "base/command_line.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_switches.h"
+#include "content/public/browser/web_contents.h"
+#include "ui/views/controls/webview/webview.h"
+#include "ui/views/widget/widget.h"
+
+namespace glic {
+
+GlicView::GlicView(Profile* profile) {
+  auto web_view = std::make_unique<views::WebView>(profile);
+  web_view->LoadInitialURL(GURL("chrome://glic"));
+  web_view->GetWebContents()->SetPageBaseBackgroundColor(SK_ColorTRANSPARENT);
+
+  AddChildView(std::move(web_view));
+}
+
+GlicView::~GlicView() = default;
+
+// static
+views::UniqueWidgetPtr GlicView::CreateWidget(Profile* profile,
+                                              const gfx::Rect& initial_bounds) {
+  views::Widget::InitParams params(
+      views::Widget::InitParams::CLIENT_OWNS_WIDGET,
+      views::Widget::InitParams::TYPE_WINDOW_FRAMELESS);
+  params.opacity = views::Widget::InitParams::WindowOpacity::kTranslucent;
+  params.remove_standard_frame = true;
+  params.shadow_type = views::Widget::InitParams::ShadowType::kNone;
+  params.bounds = initial_bounds;
+
+  views::UniqueWidgetPtr widget =
+      std::make_unique<views::Widget>(std::move(params));
+
+  widget->SetContentsView(std::make_unique<GlicView>(profile));
+
+  return widget;
+}
+}  // namespace glic
---
+++ chrome/browser/ui/views/glic/glic_view.h
@@ -0,0 +1,34 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_GLIC_GLIC_VIEW_H_
+#define CHROME_BROWSER_UI_VIEWS_GLIC_GLIC_VIEW_H_
+
+#include "ui/views/view.h"
+#include "ui/views/widget/unique_widget_ptr.h"
+
+namespace gfx {
+class Rect;
+}  // namespace gfx
+
+class Profile;
+
+namespace glic {
+
+class GlicView : public views::View {
+ public:
+  explicit GlicView(Profile* profile);
+  GlicView(const GlicView&) = delete;
+  GlicView& operator=(const GlicView&) = delete;
+  ~GlicView() override;
+
+  // Creates a menu widget that contains a `GlicView`, configured with the
+  // given `initial_bounds`.
+  static views::UniqueWidgetPtr CreateWidget(Profile* profile,
+                                             const gfx::Rect& initial_bounds);
+};
+
+}  // namespace glic
+
+#endif  // CHROME_BROWSER_UI_VIEWS_GLIC_GLIC_VIEW_H_
