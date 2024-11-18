08d6565542987592e84b2ef4d7e1529d6316cc68
Hidehiko Abe <hidehiko@chromium.org>
2024-11-18 04:19:17 +0000

    boca: Do not depend on global active user.
    The Boca webui is tied to a BrowserContext instance already, so the
    User corresponding to the BrowserContext should be used,
    instead of depending on the global active User.
    
    Along with the fix, now WebUI needs to be instantiated for unittests.
    So, switches the TestSuite to use content::UnitTestTestRunner to
    use global code injection for WebUI instances.
    
    BUG=278643115
    TEST=Tryjob
    
    Change-Id: I6b3aecfd2b9785f75162ec58f2639e150a0978e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022046
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1384158}

--- ash/webui/boca_ui/boca_app_page_handler.cc
+++ ash/webui/boca_ui/boca_app_page_handler.cc
@@ -33,6 +33,7 @@
 #include "chromeos/ash/components/boca/session_api/remove_student_request.h"
 #include "chromeos/ash/components/boca/session_api/session_client_impl.h"
 #include "chromeos/ash/components/boca/session_api/update_session_request.h"
+#include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ui/frame/multitask_menu/float_controller_base.h"
 #include "chromeos/ui/wm/constants.h"
 #include "content/public/browser/web_contents.h"
@@ -161,7 +162,8 @@ BocaAppHandler::BocaAppHandler(
       remote_(std::move(remote)),
       session_client_impl_(session_client_impl),
       web_ui_(web_ui) {
-  auto* user = user_manager::UserManager::Get()->GetActiveUser();
+  auto* user = ash::BrowserContextHelper::Get()->GetUserByBrowserContext(
+      web_ui->GetWebContents()->GetBrowserContext());
   user_identity_.set_email(user->GetAccountId().GetUserEmail());
   user_identity_.set_gaia_id(user->GetAccountId().GetGaiaId());
   user_identity_.set_full_name(base::UTF16ToUTF8(user->GetDisplayName()));
