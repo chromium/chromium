ed9dd9543af9022452a98b677cd23275645e91af
Eli Hayoon <elius@google.com>
2024-11-18 19:49:49 +0000

    Revert "Log 3rd party cookie metrics even if the experiment is disabled"
    This reverts commit dbb8a9a9cb479fb5209de6fe696788e107fa8a58.
    
    Reason for revert: Messes up some dashboards
    
    Original change's description:
    > Log 3rd party cookie metrics even if the experiment is disabled
    >
    > Bug: 367351039
    > Change-Id: I1869229d66c6df80a69a8375b59590d71fa1a8af
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5895159
    > Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    > Commit-Queue: Eli Hayoon <elius@google.com>
    > Reviewed-by: Anton Maliev <amaliev@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1362483}
    
    Bug: 367351039
    Change-Id: Iadc42e9f15979c46b682c38eb04c7e229e856af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026352
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Eli Hayoon <elius@google.com>
    Cr-Commit-Position: refs/heads/main@{#1384516}

--- chrome/browser/page_load_metrics/observers/third_party_cookie_deprecation_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/third_party_cookie_deprecation_metrics_observer_browsertest.cc
@@ -99,6 +99,8 @@ const Allow3PCMechanismBrowserTestCase kAllowMechanismTestCases[] = {
         .allow_by_global_setting = true,
         .expected_allow_mechanism_histogram_sample =
             ThirdPartyCookieAllowMechanism::kAllowByGlobalSetting,
+        .expected_web_feature_histogram_sample =
+            WebFeature::kThirdPartyCookieDeprecation_AllowByGlobalSetting,
     },
     {
         .allow_by_3pcd_1p_trial_token = true,
@@ -174,6 +176,8 @@ const Allow3PCMechanismBrowserTestCase kAllowMechanismTestCases[] = {
         .allow_by_explicit_setting = true,
         .expected_allow_mechanism_histogram_sample =
             ThirdPartyCookieAllowMechanism::kAllowByExplicitSetting,
+        .expected_web_feature_histogram_sample =
+            WebFeature::kThirdPartyCookieDeprecation_AllowByExplicitSetting,
     },
     // Precedence testing test cases:
     {
@@ -181,6 +185,8 @@ const Allow3PCMechanismBrowserTestCase kAllowMechanismTestCases[] = {
         .allow_by_3pcd_1p_trial_token = true,
         .expected_allow_mechanism_histogram_sample =
             ThirdPartyCookieAllowMechanism::kAllowByGlobalSetting,
+        .expected_web_feature_histogram_sample =
+            WebFeature::kThirdPartyCookieDeprecation_AllowByGlobalSetting,
     },
     {
         .allow_by_3pcd_1p_trial_token = true,
@@ -1278,9 +1284,11 @@ IN_PROC_BROWSER_TEST_P(ThirdPartyCookieDeprecationObserverSSABrowserTest,
 
   histogram_tester.ExpectUniqueSample(kThirdPartyCookieAllowMechanismHistogram,
                                       /*kAllowByStorageAccess*/ 6, 2);
+  // Only record blink usage when tracking protection is onboard.
   histogram_tester.ExpectBucketCount(
       kWebFeatureHistogram,
-      WebFeature::kThirdPartyCookieAccessBlockByExperiment, GetParam() ? 1 : 0);
+      WebFeature::kThirdPartyCookieDeprecation_AllowByStorageAccess,
+      GetParam() ? 1 : 0);
 }
 
 class ThirdPartyCookieDeprecationObserverCookieReadBrowserTest
