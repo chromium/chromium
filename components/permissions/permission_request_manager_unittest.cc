9fcd7818f1a255573ddc0d86b718bfe95275d671
Jagadesh P <jagadeshjai1999@gmail.com>
2024-11-19 16:07:24 +0000

    Fix dangling pointer in |PermissionRequestManagerTest|.
    Bug: 40212619
    Change-Id: I55f66d57274660e39bd7f2b9db2791362608e3d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6024729
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Jagadesh Jai <jagadeshjai1999@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1384993}

--- components/permissions/permission_request_manager_unittest.cc
+++ components/permissions/permission_request_manager_unittest.cc
@@ -79,6 +79,7 @@ class PermissionRequestManagerTest : public content::RenderViewHostTestHarness {
 
   void TearDown() override {
     prompt_factory_ = nullptr;
+    manager_ = nullptr;
     content::RenderViewHostTestHarness::TearDown();
   }
 
@@ -182,7 +183,7 @@ class PermissionRequestManagerTest : public content::RenderViewHostTestHarness {
   MockPermissionRequest iframe_request_other_domain_;
   MockPermissionRequest iframe_request_camera_other_domain_;
   MockPermissionRequest iframe_request_mic_other_domain_;
-  raw_ptr<PermissionRequestManager, DanglingUntriaged> manager_;
+  raw_ptr<PermissionRequestManager> manager_;
   std::unique_ptr<MockPermissionPromptFactory> prompt_factory_;
   TestPermissionsClient client_;
   base::test::ScopedFeatureList feature_list_;
