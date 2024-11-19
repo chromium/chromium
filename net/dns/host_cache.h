c1d78704f9c65b42666ffeb8fbc7fd383c0f2aa6
Keita Suzuki <suzukikeita@chromium.org>
2024-11-19 03:04:07 +0000

    Re-enable HostCache-related metrics
    This CL re-enables HostCache-related metrics, which was deleted in
    crbug.com/41428232. It re-enables parts of the metrics that we would like
    to use, and makes adjustments so that it fits the modern coding
    convention.
    
    Bug: 378566923
    Change-Id: I5ceedbf57a503b98e47cf70785485c4b96078c7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011050
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384741}

--- net/dns/host_cache.h
+++ net/dns/host_cache.h
@@ -480,8 +480,35 @@ class NET_EXPORT HostCache {
   FRIEND_TEST_ALL_PREFIXES(HostCacheTest, NoCache);
 
   enum SetOutcome : int;
-  enum LookupOutcome : int;
-  enum EraseReason : int;
+
+  // The result of cache lookup.
+  //
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused.
+  //
+  // LINT.IfChange(LookupOutcome)
+  enum class LookupOutcome {
+    kLookupMissAbsent = 0,
+    kLookupMissStale = 1,
+    kLookupHitValid = 2,
+    kLookupHitStale = 3,
+    kMaxValue = kLookupHitStale
+  };
+  // LINT.ThenChange(//tools/metrics/histograms/metadata/net/enums.xml:LookupOutcome)
+
+  // The reason why an entry was erased.
+  //
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused.
+  //
+  // LINT.IfChange(EraseReason)
+  enum class EraseReason {
+    kEraseEvict = 0,
+    kEraseClear = 1,
+    kEraseDestruct = 2,
+    kMaxValue = kEraseDestruct
+  };
+  // LINT.ThenChange(//tools/metrics/histograms/metadata/net/enums.xml:EraseReason)
 
   // Returns the result that is least stale, based on the number of network
   // changes since the result was cached. If the results are equally stale,
@@ -503,6 +530,21 @@ class NET_EXPORT HostCache {
   // Returns matching key and entry from cache and nullptr if no match. An exact
   // match for |key| is required.
   std::pair<const Key, Entry>* LookupInternal(const Key& key);
+
+  // Record cache lookup metrics for the `entry`.
+  void RecordLookup(LookupOutcome outcome,
+                    base::TimeTicks now,
+                    const Key& key,
+                    const Entry* entry);
+
+  // Record cache erase metrics for the `entry`.
+  void RecordErase(EraseReason reason,
+                   base::TimeTicks now,
+                   const Key& key,
+                   const Entry& entry);
+
+  // Record cache erase metrics for all entries.
+  void RecordEraseAll(EraseReason reason, base::TimeTicks now);
 
   // Returns true if this HostCache can contain no entries.
   bool caching_is_disabled() const { return max_entries_ == 0; }
