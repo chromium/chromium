c4350890f281358c8ba9229954f0983ce25fc6d9
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-19 09:59:35 +0000

    [Private network device]Migrate to BuildServiceInstanceForBrowserContext
    `BuildServiceInstanceFor` is deprecated. This CL ensures that the new
    method `BuildServiceInstanceForBrowserContext` is used instead.
    
    In particular it ensures that services are created with a unique_ptr.
    
    This CL was uploaded by git cl split.
    
    R=lyf@chromium.org
    
    Bug: 40249337
    Change-Id: I16cc175cefd5176dc71004f063bc06470a32bb3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021381
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384841}

--- chrome/browser/private_network_access/private_network_device_permission_context_factory.h
+++ chrome/browser/private_network_access/private_network_device_permission_context_factory.h
@@ -43,7 +43,7 @@ class PrivateNetworkDevicePermissionContextFactory
   ~PrivateNetworkDevicePermissionContextFactory() override;
 
   // BrowserContextKeyedServiceFactory methods:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* profile) const override;
 };
 
