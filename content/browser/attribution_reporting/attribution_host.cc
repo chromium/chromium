76a0d3289e7b2e2cc021af9a92d41b5309a3cca8
Nan Lin <linnan@chromium.org>
2024-11-19 16:05:45 +0000

    [Merge M132] Add metric for attribution reporting registrations for client bounce
    We consider a non-user-initiated navigation to be a client bounce
    if it navigates the page away within a short period of time without
    any user interaction.
    
    Note that we only measure non-navigation tied attribution data hosts
    as navigation-tied registrations are validated and therefore not a
    concern.
    
    (cherry picked from commit a58f9aa5fea1d646259d3f3a2b3e553fe92b1e1a)
    
    Bug: 378650101
    Change-Id: I48a4a29198454c9d0a018f45a05c048d9ca1282d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012217
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1383740}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032871
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6834@{#519}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- content/browser/attribution_reporting/attribution_host.cc
+++ content/browser/attribution_reporting/attribution_host.cc
@@ -17,6 +17,7 @@
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
 #include "components/attribution_reporting/data_host.mojom.h"
@@ -133,16 +134,60 @@ AttributionInputEvent AttributionHost::GetMostRecentNavigationInputEvent()
 }
 
 void AttributionHost::DidStartNavigation(NavigationHandle* navigation_handle) {
+  // Impression navigations need to navigate the primary main frame to be valid.
+  // Impressions should never be attached to same-document navigations but can
+  // be the result of a bad renderer.
+
+  // A navigation is considered client bounce if it navigates the current page
+  // away within a short period of time without any user interaction with the
+  // page, and the navigation is not user initiated. Client bounce detection
+  // only cares about primary main frame and non-same-document navigations.
+
+  if (!navigation_handle->IsInPrimaryMainFrame() ||
+      navigation_handle->IsSameDocument()) {
+    return;
+  }
+
+  if (primary_main_frame_data_.has_value()) {
+    // Note that `NavigationHandle::HasUserGesture()` does not capture
+    // browser-initiated navigations. The negation of
+    // `NavigationHandle::IsRendererInitiated()` tells us whether the navigation
+    // is browser-initiated.
+    bool has_user_gesture = navigation_handle->HasUserGesture() ||
+                            !navigation_handle->IsRendererInitiated();
+
+    // A user gesture indicates no client-redirect. And, we don't consider a
+    // client-redirect to be a bounce if there was user interaction on the page
+    // or if we timed out on the client bounce detection timers.
+    if (!has_user_gesture && !primary_main_frame_data_->has_user_interaction &&
+        primary_main_frame_data_->num_data_hosts_registered > 0) {
+      CHECK(last_navigation_time_.has_value());
+      base::TimeDelta time_since_last_navigation =
+          base::Time::Now() - *last_navigation_time_;
+
+      if (time_since_last_navigation < base::Seconds(1)) {
+        base::UmaHistogramCounts100(
+            "Conversions.NumDataHostsRegisteredOnClientBounce.1s",
+            primary_main_frame_data_->num_data_hosts_registered);
+      }
+      if (time_since_last_navigation < base::Seconds(5)) {
+        base::UmaHistogramCounts100(
+            "Conversions.NumDataHostsRegisteredOnClientBounce.5s",
+            primary_main_frame_data_->num_data_hosts_registered);
+      }
+      if (time_since_last_navigation < base::Seconds(10)) {
+        base::UmaHistogramCounts100(
+            "Conversions.NumDataHostsRegisteredOnClientBounce.10s",
+            primary_main_frame_data_->num_data_hosts_registered);
+      }
+    }
+  }
+
   const auto& impression = navigation_handle->GetImpression();
 
   // TODO(crbug.com/40262156): Consider checking for navigations taking place in
   // a prerendered main frame.
-
-  // Impression navigations need to navigate the primary main frame to be valid.
-  // Impressions should never be attached to same-document navigations but can
-  // be the result of a bad renderer.
-  if (!impression || !navigation_handle->IsInPrimaryMainFrame() ||
-      navigation_handle->IsSameDocument()) {
+  if (!impression) {
     return;
   }
   RenderFrameHostImpl* initiator_frame_host =
@@ -193,6 +238,21 @@ void AttributionHost::DidRedirectNavigation(
 }
 
 void AttributionHost::DidFinishNavigation(NavigationHandle* navigation_handle) {
+  if (navigation_handle->IsInPrimaryMainFrame() &&
+      !navigation_handle->IsSameDocument()) {
+    if (primary_main_frame_data_.has_value()) {
+      // Resets for further client redirects.
+      primary_main_frame_data_->num_data_hosts_registered = 0;
+    }
+
+    // Sets current time to detect further client redirects.
+    last_navigation_time_ = base::Time::Now();
+
+    if (navigation_handle->HasCommitted()) {
+      primary_main_frame_data_ = PrimaryMainFrameData();
+    }
+  }
+
   const auto& impression = navigation_handle->GetImpression();
   if (!impression.has_value()) {
     return;
@@ -209,6 +269,14 @@ void AttributionHost::DidFinishNavigation(NavigationHandle* navigation_handle) {
 
   ongoing_registration_eligible_navigations_.erase(
       navigation_handle->GetNavigationId());
+}
+
+void AttributionHost::FrameReceivedUserActivation(
+    RenderFrameHost* render_frame_host) {
+  // We consider user activation from all frames in the page.
+  if (primary_main_frame_data_.has_value()) {
+    primary_main_frame_data_->has_user_interaction = true;
+  }
 }
 
 void AttributionHost::NotifyNavigationRegistrationData(
@@ -280,6 +348,10 @@ void AttributionHost::RegisterDataHost(
       static_cast<RenderFrameHostImpl*>(receivers_.GetCurrentTargetFrame()));
   if (!suitable_context.has_value()) {
     return;
+  }
+
+  if (primary_main_frame_data_.has_value()) {
+    primary_main_frame_data_->num_data_hosts_registered++;
   }
 
   AttributionDataHostManager* manager = suitable_context->data_host_manager();
--- content/browser/attribution_reporting/attribution_host.h
+++ content/browser/attribution_reporting/attribution_host.h
@@ -8,9 +8,11 @@
 #include <stdint.h>
 
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "base/containers/flat_set.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "build/buildflag.h"
 #include "components/attribution_reporting/data_host.mojom-forward.h"
@@ -63,6 +65,11 @@ class CONTENT_EXPORT AttributionHost
   friend class AttributionHostTestPeer;
   friend class WebContentsUserData<AttributionHost>;
 
+  struct PrimaryMainFrameData {
+    int num_data_hosts_registered = 0;
+    bool has_user_interaction = false;
+  };
+
   // blink::mojom::AttributionHost:
   void NotifyNavigationWithBackgroundRegistrationsWillStart(
       const blink::AttributionSrcToken& attribution_src_token,
@@ -79,6 +86,7 @@ class CONTENT_EXPORT AttributionHost
   void DidStartNavigation(NavigationHandle* navigation_handle) override;
   void DidRedirectNavigation(NavigationHandle* navigation_handle) override;
   void DidFinishNavigation(NavigationHandle* navigation_handle) override;
+  void FrameReceivedUserActivation(RenderFrameHost* render_frame_host) override;
 
   void NotifyNavigationRegistrationData(NavigationHandle* navigation_handle);
 
@@ -96,6 +104,9 @@ class CONTENT_EXPORT AttributionHost
   std::unique_ptr<AttributionInputEventTrackerAndroid>
       input_event_tracker_android_;
 #endif
+
+  std::optional<base::Time> last_navigation_time_;
+  std::optional<PrimaryMainFrameData> primary_main_frame_data_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
