01a4d39d10c77cbad081a617bebc8d4c5eb4e373
Maksim Moskvitin <mmoskvitin@google.com>
2024-11-20 09:49:03 +0000

    [Code Health][iOS] Prepare to move ios/c/b/ui/browser_container
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    This is part 1 of 3-sided patch to ensure that downstream dependency
    won't break.
    
    Bug: 324232508
    Change-Id: Ic9af59fbadd1e74fdd8e70ff80d019be118e2f81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6034890
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385525}

---
+++ ios/chrome/browser/browser_container/ui_bundled/edit_menu_app_interface.h
@@ -0,0 +1,10 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_BROWSER_CONTAINER_UI_BUNDLED_EDIT_MENU_APP_INTERFACE_H_
+#define IOS_CHROME_BROWSER_BROWSER_CONTAINER_UI_BUNDLED_EDIT_MENU_APP_INTERFACE_H_
+
+#import "ios/chrome/browser/ui/browser_container/edit_menu_app_interface.h"
+
+#endif  // IOS_CHROME_BROWSER_BROWSER_CONTAINER_UI_BUNDLED_EDIT_MENU_APP_INTERFACE_H_
