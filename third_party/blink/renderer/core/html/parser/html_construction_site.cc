94d20faa163464fdca6768bda57286215792334f
Mason Freed <masonf@chromium.org>
2024-11-20 00:02:28 +0000

    Remove CHECK that can get hit due to foster parenting
    In this case:
    
    <table>
      <template>
        <h1 parseparts>{{#}}</h1>
      </template>
    </table>
    
    the prior CHECK could get hit. Since we're just popping the part
    root, this CL just removes the CHECK.
    
    Note that the prior CL that added the test for this inadvertently
    made the test not work, due to the name. See crbug.com/379764806.
    
    Bug: 40271855
    Bug: 379764806
    Change-Id: Ib723cf166ecbe0f2a7da855c27d56e84e2957fba
    Fixed: 372994343
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033772
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385341}

--- third_party/blink/renderer/core/html/parser/html_construction_site.cc
+++ third_party/blink/renderer/core/html/parser/html_construction_site.cc
@@ -951,6 +951,7 @@ void HTMLConstructionSite::InsertHTMLTemplateElement(
     DocumentFragment* template_content = template_element->content();
     if (pending_dom_parts_ && template_content &&
         !RuntimeEnabledFeatures::DOMPartsAPIMinimalEnabled()) {
+      DCHECK(RuntimeEnabledFeatures::DOMPartsAPIEnabled());
       pending_dom_parts_->PushPartRoot(&template_content->getPartRoot());
     }
   }
@@ -1408,10 +1409,9 @@ void HTMLConstructionSite::FinishedTemplateElement(
   if (!pending_dom_parts_) {
     return;
   }
-  if (RuntimeEnabledFeatures::DOMPartsAPIEnabled() &&
-      !RuntimeEnabledFeatures::DOMPartsAPIMinimalEnabled()) {
-    PartRoot* last_root = pending_dom_parts_->PopPartRoot();
-    CHECK_EQ(&content_fragment->getPartRoot(), last_root);
+  DCHECK(RuntimeEnabledFeatures::DOMPartsAPIEnabled());
+  if (!RuntimeEnabledFeatures::DOMPartsAPIMinimalEnabled()) {
+    pending_dom_parts_->PopPartRoot();
   }
 }
 
@@ -1493,6 +1493,7 @@ PartRoot* HTMLConstructionSite::PendingDOMParts::CurrentPartRoot() const {
 void HTMLConstructionSite::PendingDOMParts::PushPartRoot(PartRoot* root) {
   DCHECK(RuntimeEnabledFeatures::DOMPartsAPIEnabled());
   DCHECK(!RuntimeEnabledFeatures::DOMPartsAPIMinimalEnabled());
+  DCHECK(root);
   return part_root_stack_.push_back(root);
 }
 
