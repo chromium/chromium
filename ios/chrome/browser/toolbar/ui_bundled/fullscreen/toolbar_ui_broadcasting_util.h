226630e4ef62336b090b4c1930323d83e24b8fe3
Christian Xu <christianxu@google.com>
2024-11-20 09:52:51 +0000

    [CodeHealth][iOS] Prepare for ui/toolbar move to toolbar/ui_bundled
    Create headers to prepare for toolbar move to https://crrev.com/i/7834388.
    
    Bug: 324233041
    Change-Id: Ia968b74e072fe26350f36b658265b585fa5cf062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035119
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385528}

---
+++ ios/chrome/browser/toolbar/ui_bundled/fullscreen/toolbar_ui_broadcasting_util.h
@@ -0,0 +1,10 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_TOOLBAR_UI_BUNDLED_FULLSCREEN_TOOLBAR_UI_BROADCASTING_UTIL_H_
+#define IOS_CHROME_BROWSER_TOOLBAR_UI_BUNDLED_FULLSCREEN_TOOLBAR_UI_BROADCASTING_UTIL_H_
+
+#import "ios/chrome/browser/toolbar/ui_bundled/fullscreen/toolbar_ui_broadcasting_util.h"
+
+#endif  // IOS_CHROME_BROWSER_TOOLBAR_UI_BUNDLED_FULLSCREEN_TOOLBAR_UI_BROADCASTING_UTIL_H_
---
+++ ios/chrome/browser/toolbar/ui_bundled/public/toolbar_constants.h
@@ -0,0 +1,10 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_TOOLBAR_UI_BUNDLED_PUBLIC_TOOLBAR_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_TOOLBAR_UI_BUNDLED_PUBLIC_TOOLBAR_CONSTANTS_H_
+
+#import "ios/chrome/browser/toolbar/ui_bundled/public/toolbar_constants.h"
+
+#endif  // IOS_CHROME_BROWSER_TOOLBAR_UI_BUNDLED_PUBLIC_TOOLBAR_CONSTANTS_H_
