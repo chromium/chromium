2574d4215380e1e1820ffe87f2f4cce38bbc387a
Noémie St-Onge <noemies@google.com>
2024-11-20 16:35:57 +0000

    [iOS] Enable Keyboard Accessory Upgrade by default
    Fixed: 326414026
    Change-Id: I4f5ed73675bff495c7688c9f4fe7eea66cf1ac83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033511
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1385702}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -84,7 +84,7 @@ SegmentedDefaultBrowserExperimentTypeEnabled() {
 
 BASE_FEATURE(kIOSKeyboardAccessoryUpgrade,
              "IOSKeyboardAccessoryUpgrade",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIOSKeyboardAccessoryUpgradeShortManualFillMenu,
              "IOSKeyboardAccessoryUpgradeShortManualFillMenu",
