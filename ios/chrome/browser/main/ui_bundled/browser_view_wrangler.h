085e84c08b277964c84b21a8200973e6170da59b
Maksim Moskvitin <mmoskvitin@google.com>
2024-11-20 14:56:25 +0000

    [Code Health][iOS] Prepare to move ios/c/b/ui/main
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    This is part 1 of 3-sided patch to ensure that downstream dependency
    won't break.
    
    Bug: 324233258
    Change-Id: I0b3154984b48404b98ad6d7179a32167de270589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038772
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385649}

---
+++ ios/chrome/browser/main/ui_bundled/browser_view_wrangler.h
@@ -0,0 +1,10 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_MAIN_UI_BUNDLED_BROWSER_VIEW_WRANGLER_H_
+#define IOS_CHROME_BROWSER_MAIN_UI_BUNDLED_BROWSER_VIEW_WRANGLER_H_
+
+#import "ios/chrome/browser/ui/main/browser_view_wrangler.h"
+
+#endif  // IOS_CHROME_BROWSER_MAIN_UI_BUNDLED_BROWSER_VIEW_WRANGLER_H_
