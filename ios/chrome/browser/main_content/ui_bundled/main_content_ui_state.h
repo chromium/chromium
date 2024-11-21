7cf7be649fd70a2a268eeb43821fdbd8b0fa11d5
Maksim Moskvitin <mmoskvitin@google.com>
2024-11-21 13:22:26 +0000

    [Code Health][iOS] Prepare to move ios/c/b/ui/main_content
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    This is part 1 of 3-sided patch to ensure that downstream dependency
    won't break.
    
    Bug: 324233238
    Change-Id: I5c8a84ea76c45b1c08bdb4e7ee53fe2b2e056010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6037621
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386191}

---
+++ ios/chrome/browser/main_content/ui_bundled/main_content_ui_state.h
@@ -0,0 +1,10 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_MAIN_CONTENT_UI_BUNDLED_MAIN_CONTENT_UI_STATE_H_
+#define IOS_CHROME_BROWSER_MAIN_CONTENT_UI_BUNDLED_MAIN_CONTENT_UI_STATE_H_
+
+#import "ios/chrome/browser/ui/main_content/main_content_ui_state.h"
+
+#endif  // IOS_CHROME_BROWSER_MAIN_CONTENT_UI_BUNDLED_MAIN_CONTENT_UI_STATE_H_
