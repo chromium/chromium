3a9a46093a4a0f275cdb6d420341d3d424452803
Christian Xu <christianxu@google.com>
2024-11-21 13:21:38 +0000

    [CodeHealth][iOS] Prepare ui/tab_switcher move to ui_bundled
    Bug: 324233299
    Change-Id: I572a79395a0833a68513b41918b0d19b356d1ead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039895
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1386190}

---
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_grid_constants.h
@@ -0,0 +1,10 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_TAB_SWITCHER_UI_BUNDLED_TAB_GRID_TAB_GRID_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_TAB_SWITCHER_UI_BUNDLED_TAB_GRID_TAB_GRID_CONSTANTS_H_
+
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_constants.h"
+
+#endif  // IOS_CHROME_BROWSER_TAB_SWITCHER_UI_BUNDLED_TAB_GRID_TAB_GRID_CONSTANTS_H_
