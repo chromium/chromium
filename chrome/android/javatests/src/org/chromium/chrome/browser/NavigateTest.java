51777af5feff5956d5008296500ed242ebaac644
Ioana Pandele <ioanap@chromium.org>
2024-11-21 14:33:08 +0000

    Revert "[Gardener] Disable flaky NavigateTest#testNavigateMany"
    This reverts commit f10ba660dbf524e4d54b7cdfbe011afeddc4481f.
    
    Reason for revert: The restriction doesn't seem to work and automotive is also proving flaky.
    
    Original change's description:
    > [Gardener] Disable flaky NavigateTest#testNavigateMany
    >
    > Bug: 380238040
    > Change-Id: Ib429ed834b21408374b99f5c78f02f258ce18424
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039235
    > Owners-Override: Ioana Pandele <ioanap@chromium.org>
    > Commit-Queue: Adem Derinel <derinel@google.com>
    > Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    > Reviewed-by: Adem Derinel <derinel@google.com>
    > Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1386214}
    
    Bug: 380238040
    Change-Id: I2f509cd131342f0a53d464d973350d5e33ed3dbe
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6039236
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1386217}

--- chrome/android/javatests/src/org/chromium/chrome/browser/NavigateTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/NavigateTest.java
@@ -68,7 +68,6 @@ import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.net.test.ServerCertificate;
 import org.chromium.net.test.util.TestWebServer;
 import org.chromium.ui.base.DeviceFormFactor;
-import org.chromium.ui.test.util.DeviceRestriction;
 import org.chromium.url.GURL;
 import org.chromium.url.Origin;
 
@@ -163,7 +162,6 @@ public class NavigateTest {
     @Restriction(DeviceFormFactor.TABLET)
     @MediumTest
     @Feature({"Navigation"})
-    @DisableIf.Device(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO)
     public void testNavigateMany() throws Exception {
         final String[] urls =
                 mTestServer.getURLs(
