cf6a06587288608359cedc282a0dc035b3bfa3e2
Etienne Dechamps <edechamps@google.com>
2024-11-21 09:34:03 +0000

    Add trace events to CronetHttpURLConnection
    Example trace: https://ui.perfetto.dev/#!/?s=7da5e42a086776752002fb94d905592f514303fb
    
    Bug: b:346770535
    Change-Id: If4af2a4ce3c259f554163209341a627b8c2a79bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023570
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Auto-Submit: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1386120}

--- components/cronet/android/java/src/org/chromium/net/urlconnection/CronetChunkedOutputStream.java
+++ components/cronet/android/java/src/org/chromium/net/urlconnection/CronetChunkedOutputStream.java
@@ -4,6 +4,7 @@
 
 package org.chromium.net.urlconnection;
 
+import org.chromium.base.metrics.ScopedSysTraceEvent;
 import org.chromium.net.UploadDataProvider;
 import org.chromium.net.UploadDataSink;
 
@@ -100,20 +101,24 @@ final class CronetChunkedOutputStream extends CronetOutputStream {
 
         @Override
         public void read(final UploadDataSink uploadDataSink, final ByteBuffer byteBuffer) {
-            if (byteBuffer.remaining() >= mBuffer.remaining()) {
-                byteBuffer.put(mBuffer);
-                mBuffer.clear();
-                uploadDataSink.onReadSucceeded(mLastChunk);
-                if (!mLastChunk) {
-                    // Quit message loop so embedder can write more data.
-                    mMessageLoop.quit();
+            try (var traceEvent =
+                    ScopedSysTraceEvent.scoped(
+                            "CronetChunkedOutputStream.UploadDataProviderImpl#read")) {
+                if (byteBuffer.remaining() >= mBuffer.remaining()) {
+                    byteBuffer.put(mBuffer);
+                    mBuffer.clear();
+                    uploadDataSink.onReadSucceeded(mLastChunk);
+                    if (!mLastChunk) {
+                        // Quit message loop so embedder can write more data.
+                        mMessageLoop.quit();
+                    }
+                } else {
+                    int oldLimit = mBuffer.limit();
+                    mBuffer.limit(mBuffer.position() + byteBuffer.remaining());
+                    byteBuffer.put(mBuffer);
+                    mBuffer.limit(oldLimit);
+                    uploadDataSink.onReadSucceeded(false);
                 }
-            } else {
-                int oldLimit = mBuffer.limit();
-                mBuffer.limit(mBuffer.position() + byteBuffer.remaining());
-                byteBuffer.put(mBuffer);
-                mBuffer.limit(oldLimit);
-                uploadDataSink.onReadSucceeded(false);
             }
         }
 
@@ -135,14 +140,16 @@ final class CronetChunkedOutputStream extends CronetOutputStream {
     }
 
     /**
-     * Helper function to upload {@code mBuffer} to the native stack. This
-     * function blocks until {@code mBuffer} is consumed and there is space to
-     * write more data.
+     * Helper function to upload {@code mBuffer} to the native stack. This function blocks until
+     * {@code mBuffer} is consumed and there is space to write more data.
      */
     private void uploadBufferInternal() throws IOException {
-        checkNotClosed();
-        mBuffer.flip();
-        mMessageLoop.loop();
-        checkNoException();
+        try (var traceEvent =
+                ScopedSysTraceEvent.scoped("CronetChunkedOutputStream#uploadBufferInternal")) {
+            checkNotClosed();
+            mBuffer.flip();
+            mMessageLoop.loop();
+            checkNoException();
+        }
     }
 }
