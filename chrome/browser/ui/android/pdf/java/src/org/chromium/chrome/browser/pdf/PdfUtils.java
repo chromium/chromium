c359cfc4862a2b41fbf54535cc008faf2ecaede2
Shu Yang <shuyng@google.com>
2024-11-22 22:44:29 +0000

    [M132] Record pdf load time for the first successful load
    There might be multiple onLoadDocumentSuccess for each explicit pdf
    load.
    Since we start measuring load time when we explicitly load pdf, only the
    interval till the first onLoadDocumentSuccess is meaningful load time.
    
    (cherry picked from commit 9b87c7d4340be0a756ddd0d5f1253906032142af)
    
    Bug: 380130062
    Change-Id: Iefea8d9a24e5776cb496c375d78757bc0490c99f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6040186
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1386055}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6040387
    Cr-Commit-Position: refs/branch-heads/6834@{#807}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfUtils.java
+++ chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfUtils.java
@@ -355,6 +355,10 @@ public class PdfUtils {
         RecordHistogram.recordTimesHistogram("Android.Pdf.DocumentLoadTime.Paired", duration);
     }
 
+    static void recordPdfLoadTimeFirstPaired(long duration) {
+        RecordHistogram.recordTimesHistogram("Android.Pdf.DocumentLoadTime.FirstPaired", duration);
+    }
+
     static void recordFindInPage(int findInPageCounts) {
         RecordHistogram.recordExactLinearHistogram(
                 "Android.Pdf.FindInPageCounts", findInPageCounts, /* max= */ 9);
