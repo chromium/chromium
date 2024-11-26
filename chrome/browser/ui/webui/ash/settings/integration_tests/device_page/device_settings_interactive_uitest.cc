887ae325716ede7889131f3b7a680cd6b652b709
YuhanYang <yyhyyh@google.com>
2024-11-26 19:23:58 +0000

    Fix broken DeviceSettingsInteractiveUiTest.MouseButtonRenaming
    The test fails when trying to:
    1. Click icon element to enter customize mouse buttons page
    2. flaky when entering input name after popping out the renaming dialog
       without waiting for elements to load
    3. closing the current window and opening another to assert name change.
    
    Test: interactive_ui_test
    Bug: 325543031
    Change-Id: Ic96f8a07c17842e44ac61ebae688b8740c37c621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050198
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388387}

--- chrome/browser/ui/webui/ash/settings/integration_tests/device_page/device_settings_interactive_uitest.cc
+++ chrome/browser/ui/webui/ash/settings/integration_tests/device_page/device_settings_interactive_uitest.cc
@@ -31,6 +31,7 @@
 #include "ui/events/devices/input_device.h"
 #include "ui/events/devices/keyboard_device.h"
 #include "ui/events/devices/touchpad_device.h"
+#include "ui/events/keycodes/keyboard_codes_posix.h"
 #include "ui/events/test/event_generator.h"
 
 namespace ash {
@@ -796,9 +797,7 @@ IN_PROC_BROWSER_TEST_F(DeviceSettingsInteractiveUiTest,
       Log("Calendar closed with mouse button"));
 }
 
-// Disabled for crbug.com/325543031.
-IN_PROC_BROWSER_TEST_F(DeviceSettingsInteractiveUiTest,
-                       DISABLED_MouseButtonRenaming) {
+IN_PROC_BROWSER_TEST_F(DeviceSettingsInteractiveUiTest, MouseButtonRenaming) {
   const DeepQuery kCustomizeMouseButtonsRowQuery{
       "os-settings-ui",
       "os-settings-main",
@@ -807,7 +806,15 @@ IN_PROC_BROWSER_TEST_F(DeviceSettingsInteractiveUiTest,
       "settings-per-device-mouse",
       "settings-per-device-mouse-subsection",
       "#customizeMouseButtons",
-      "#icon",
+  };
+
+  const DeepQuery kCustomizeMouseButtonsHelpSectionQuery{
+      "os-settings-ui",
+      "os-settings-main",
+      "main-page-container",
+      "settings-device-page",
+      "settings-customize-mouse-buttons-subpage",
+      "#helpSection",
   };
 
   const DeepQuery kMiddleButtonEditButtonQuery =
@@ -823,40 +830,35 @@ IN_PROC_BROWSER_TEST_F(DeviceSettingsInteractiveUiTest,
       "div > customize-button-row:nth-child(1)" + "#buttonLabel";
 
   SetMouseDevices({kFiveKeyMouse});
-  // Used to relaunch the settings app after the customizable mouse button
-  // has been edited.
-  DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kNewSettingsAppWebContentsId);
 
   RunTestSequence(
       SetupInternalKeyboard(),
       LaunchSettingsApp(webcontents_id_,
                         chromeos::settings::mojom::kPerDeviceMouseSubpagePath),
       Log("Clicking customize mouse buttons row"),
+      ScrollIntoView(webcontents_id_, kCustomizeMouseButtonsRowQuery),
       ClickElement(webcontents_id_, kCustomizeMouseButtonsRowQuery),
+      WaitForElementExists(webcontents_id_,
+                           kCustomizeMouseButtonsHelpSectionQuery),
+      Log("Registering a new button for the mouse"), Do([&]() {
+        ui::test::EventGenerator generator(Shell::GetPrimaryRootWindow());
+        generator.PressAndReleaseKey(ui::VKEY_A, ui::EF_NONE, kFiveKeyMouse.id);
+      }),
       Log("Clicking edit icon for mouse 'Middle Button'"),
       ClickElement(webcontents_id_, kMiddleButtonEditButtonQuery),
-      Log("Clearing existing mouse button name"),
-      SendKeyPressEvent(ui::KeyboardCode::VKEY_A, ui::EF_CONTROL_DOWN),
-      SendKeyPressEvent(ui::KeyboardCode::VKEY_BACK),
+      WaitForElementExists(webcontents_id_, kSaveButtonQuery),
+      Log("Clearing existing mouse button name"), Do([&]() {
+        ui::test::EventGenerator generator(Shell::GetPrimaryRootWindow());
+        generator.PressAndReleaseKey(ui::VKEY_A, ui::EF_CONTROL_DOWN,
+                                     kDeviceId1);
+        generator.PressAndReleaseKey(ui::VKEY_BACK, ui::EF_NONE, kDeviceId1);
+      }),
       Log("Renaming mouse button to 'custom'"), EnterLowerCaseText("custom"),
       ClickElement(webcontents_id_, kSaveButtonQuery),
       Log("Verifying that the custom mouse button has been renamed to "
           "'custom'"),
       WaitForElementTextContains(webcontents_id_, kCustomizeableButtonNameQuery,
-                                 "custom"),
-      Log("Closing the Settings app"),
-      SendAccelerator(webcontents_id_,
-                      {ui::VKEY_W, ui::EF_CONTROL_DOWN | ui::EF_SHIFT_DOWN}),
-      WaitForHide(webcontents_id_, /*transition_only_on_event=*/true),
-      LaunchSettingsApp(kNewSettingsAppWebContentsId,
-                        chromeos::settings::mojom::kPerDeviceMouseSubpagePath),
-      ClickElement(kNewSettingsAppWebContentsId,
-                   kCustomizeMouseButtonsRowQuery),
-      Log("Confirming the updated mouse button name is saved correctly"),
-      WaitForElementTextContains(kNewSettingsAppWebContentsId,
-                                 kCustomizeableButtonNameQuery, "custom")
-
-  );
+                                 "custom"));
 }
 
 IN_PROC_BROWSER_TEST_F(DeviceSettingsInteractiveUiTest,
