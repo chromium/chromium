e9e256d86da9a635a4d22717fe908306a8437c8d
Adem Derinel <derinel@google.com>
2024-11-27 07:29:25 +0000

    Enable unsafe buffer warnings and IWYU in crashes_ui.cc
    Bug: 40285824
    Change-Id: I7837256940ad16a400d432fc70761d0cda2c23ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6049197
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1388663}

--- chrome/browser/ui/webui/crashes_ui.cc
+++ chrome/browser/ui/webui/crashes_ui.cc
@@ -2,25 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/browser/ui/webui/crashes_ui.h"
 
 #include <stddef.h>
 
 #include <memory>
 #include <string>
-#include <vector>
+#include <utility>
 
 #include "base/functional/bind.h"
-#include "base/functional/callback_helpers.h"
 #include "base/memory/ref_counted_memory.h"
-#include "base/strings/utf_string_conversions.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/system/sys_info.h"
 #include "base/values.h"
+#include "base/version_info/version_info.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/crash_upload_list/crash_upload_list.h"
@@ -28,19 +23,20 @@
 #include "chrome/browser/metrics/metrics_reporting_state.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
-#include "chrome/common/url_constants.h"
+#include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/branded_strings.h"
 #include "components/crash/core/browser/crashes_ui_util.h"
 #include "components/grit/components_scaled_resources.h"
 #include "components/grit/dev_ui_components_resources.h"
+#include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
-#include "components/version_info/version_info.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/browser/web_ui_message_handler.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/base/resource/resource_scale_factor.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chromeos/ash/components/dbus/debug_daemon/debug_daemon_client.h"
