014283d7192b1fe0b94c58163c17214bdc240f12
Antonio Gomes <tonikitoo@igalia.com>
2024-11-27 13:19:38 +0000

    Remove use of crosapi::browser_util::IsLacrosEnabled from chrome_saved_desk_delegate.cc
    ... now that the Lacros project is terminated.
    
    The condition does not ever resolve to `true`, hence it is no-op.
    
    R=hidehiko@chromium.org, neis@chromium.org
    
    Bug: 373971535, 381109480
    Change-Id: I25193066902601bf56b819ba677d39ba969fc923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6048809
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1388782}

--- chrome/browser/ui/ash/desks/chrome_saved_desk_delegate.cc
+++ chrome/browser/ui/ash/desks/chrome_saved_desk_delegate.cc
@@ -18,11 +18,6 @@
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/apps/icon_standardizer.h"
-#include "chrome/browser/ash/crosapi/browser_manager.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
-#include "chrome/browser/ash/crosapi/crosapi_ash.h"
-#include "chrome/browser/ash/crosapi/crosapi_manager.h"
-#include "chrome/browser/ash/crosapi/desk_template_ash.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -391,15 +386,6 @@ void ChromeSavedDeskDelegate::GetFaviconForUrl(
     base::OnceCallback<void(const gfx::ImageSkia&)> callback,
     base::CancelableTaskTracker* tracker) const {
   TRACE_EVENT0("ui", "ChromeSavedDeskDelegate::GetFaviconForUrl");
-  // Get the icons from lacros favicon service.
-  if (crosapi::browser_util::IsLacrosEnabled()) {
-    crosapi::CrosapiManager::Get()
-        ->crosapi_ash()
-        ->desk_template_ash()
-        ->GetFaviconImage(GURL(page_url), lacros_profile_id,
-                          std::move(callback));
-    return;
-  }
 
   favicon::FaviconService* favicon_service =
       FaviconServiceFactory::GetForProfile(
