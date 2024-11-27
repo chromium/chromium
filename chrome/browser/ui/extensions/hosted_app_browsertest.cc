d285eb55f287bf5e7228edb67ccacc8abcc4b9b7
Finnur Thorarinsson <finnur@chromium.org>
2024-11-27 23:13:12 +0000

    [NavCapture]: WebAppRegistrar migration part 2a.
    This change is for directory/directories:
    chrome/browser/ui/extensions
    
    In the first (previous) part, a few functions from WebAppRegistrar
    were migrated over to IsInstallState, in an effort to flush out
    incorrect uses. This CL changes some of those call sites (mostly
    tests) to GetInstallSite, which is a more appropriate API
    (especially for tests).
    
    This CL was uploaded by git cl split.
    
    R=emiliapaz@chromium.org
    
    DD: https://docs.google.com/document/d/14JPgYUVX8ECAMdXlp6GwXP-ucXnIIPXjTfxWt-WINLo/edit?usp=sharing
    Bug: 379827962
    Change-Id: I40bbf4d1d7d43e482d64ca2fd45b23031766c806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055899
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389101}

--- chrome/browser/ui/extensions/hosted_app_browsertest.cc
+++ chrome/browser/ui/extensions/hosted_app_browsertest.cc
@@ -552,10 +552,7 @@ using HostedAppTest = HostedOrWebAppTest;
 // Tests that hosted apps are not web apps.
 IN_PROC_BROWSER_TEST_P(HostedAppTest, NotWebApp) {
   SetupApp("app");
-  EXPECT_FALSE(registrar().IsInstallState(
-      app_id_, {web_app::proto::InstallState::SUGGESTED_FROM_ANOTHER_DEVICE,
-                web_app::proto::InstallState::INSTALLED_WITHOUT_OS_INTEGRATION,
-                web_app::proto::InstallState::INSTALLED_WITH_OS_INTEGRATION}));
+  EXPECT_TRUE(registrar().IsNotInRegistrar(app_id_));
   const Extension* app =
       ExtensionRegistry::Get(profile())->enabled_extensions().GetByID(app_id_);
   EXPECT_TRUE(app->is_hosted_app());
@@ -1491,10 +1488,7 @@ IN_PROC_BROWSER_TEST_P(HostedAppProcessModelTest, MAYBE_FromOutsideHostedApp) {
 IN_PROC_BROWSER_TEST_P(HostedAppProcessModelTest,
                        AppRegistrarExcludesPackaged) {
   SetupApp("https_app");
-  EXPECT_FALSE(registrar().IsInstallState(
-      app_id_, {web_app::proto::InstallState::SUGGESTED_FROM_ANOTHER_DEVICE,
-                web_app::proto::InstallState::INSTALLED_WITHOUT_OS_INTEGRATION,
-                web_app::proto::InstallState::INSTALLED_WITH_OS_INTEGRATION}));
+  EXPECT_TRUE(registrar().IsNotInRegistrar(app_id_));
 }
 
 // Check that we can successfully complete a navigation to an app URL with a
