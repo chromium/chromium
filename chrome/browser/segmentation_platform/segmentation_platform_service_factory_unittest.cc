3d856939687adb00c9ef03cc35de50c5d894c99b
Xi Han <hanxi@google.com>
2024-11-28 00:09:07 +0000

    [AppIntegration] Rank Auxiliary search module in magic stack.
    Use the biggest weight for the auxiliary search module card to make it
    shown as the first module if possible.
    
    Bug: 380086456
    Change-Id: I065196f85ab375c490b22b063695085c206e5685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6056386
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389124}

--- chrome/browser/segmentation_platform/segmentation_platform_service_factory_unittest.cc
+++ chrome/browser/segmentation_platform/segmentation_platform_service_factory_unittest.cc
@@ -639,9 +639,13 @@ TEST_F(SegmentationPlatformServiceFactoryTest, TestAndroidHomeModuleRanker) {
   input_context->metadata_args.emplace(
       segmentation_platform::kSafetyHubFreshness,
       segmentation_platform::processing::ProcessedValue::FromFloat(-1));
+  input_context->metadata_args.emplace(
+      segmentation_platform::kAuxiliarySearchFreshness,
+      segmentation_platform::processing::ProcessedValue::FromFloat(-1));
 
   std::vector<std::string> result = {kPriceChange, kSingleTab,
-                                     kTabResumptionForAndroidHome, kSafetyHub};
+                                     kTabResumptionForAndroidHome, kSafetyHub,
+                                     kAuxiliarySearch};
   ExpectGetClassificationResult(
       segmentation_platform::kAndroidHomeModuleRankerKey, prediction_options,
       input_context,
