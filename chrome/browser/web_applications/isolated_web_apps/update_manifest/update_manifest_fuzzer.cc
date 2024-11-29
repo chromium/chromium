cc1f4df508030376087ee9c5bd7f5ccba81ec3cc
Peter Boström <pbos@chromium.org>
2024-11-29 00:48:29 +0000

    Initialize ICU in web_application_fuzztests
    Bug: 381129857
    Change-Id: I55e3a805ef4b1b47f63de490624fcb6049753cac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055360
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1389587}

--- chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fuzzer.cc
+++ chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fuzzer.cc
@@ -4,7 +4,10 @@
 
 #include "chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest.h"
 
+#include "base/at_exit.h"
+#include "base/i18n/icu_util.h"
 #include "base/json/json_reader.h"
+#include "base/logging.h"
 #include "base/test/fuzztest_support.h"
 #include "base/values.h"
 #include "third_party/fuzztest/src/fuzztest/fuzztest.h"
@@ -15,6 +18,24 @@ void UpdateManifestCanSuccessfullyParseAnyString(const base::Value& json) {
   auto result = UpdateManifest::CreateFromJson(
       json, GURL("https://example.com/manifest.json"));
 }
+
+class Environment {
+ public:
+  Environment() {
+    // Sets min log level to FATAL for performance.
+    logging::SetMinLogLevel(logging::LOGGING_FATAL);
+    // ICU is as of 2024-11-28 reachable from here so we need to initialize ICU
+    // to avoid hitting NOTREACHED()s. See https://crbug.com/381129857.
+    CHECK(base::i18n::InitializeICU());
+  }
+  // Required by ICU integration according to several other fuzzer environments.
+  // TODO(pbos): Consider breaking out this fuzzer environment to something
+  // shared among most fuzzers. See net/base/fuzzer_test_support.cc and consider
+  // moving something like that into the commons.
+  base::AtExitManager at_exit_manager;
+};
+
+Environment* const environment = new Environment();
 
 FUZZ_TEST(UpdateManifestFuzzTest, UpdateManifestCanSuccessfullyParseAnyString)
     .WithSeeds({*base::JSONReader::Read("{}"), *base::JSONReader::Read(R"({
