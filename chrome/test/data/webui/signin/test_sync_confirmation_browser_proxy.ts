8fc269da3b417a3bd9308ed1644ee86360ad1831
Tomasz Jurkiewicz <tju@google.com>
2024-12-03 10:47:19 +0000

    Revert "Remove latency and button tracking from the launched"
    This reverts commit 6cd4004e0553a809d868653be7b1f3761a3da6f1.
    
    Reason for revert: Metrics are needed to evaluate server-side rollout part of the feature
    
    Bug: 381815676, 318349361
    
    Original change's description:
    > Remove latency and button tracking from the launched
    > MinorModeRestrictionsForHistorySyncOptInDesktopStudy feature.
    >
    > NO_IFTTT=moved file
    >
    > Bug: 318349361
    > Change-Id: I1ae887f27ba95d18c5cd3720115c0967e71f2806
    > Low-Coverage-Reason: TRIVIAL_CHANGE This CL only removes code
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5947745
    > Reviewed-by: Alex Ilin <alexilin@chromium.org>
    > Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1373284}
    
    (cherry picked from commit d6a7c98e7dec8f1ae22edba48efe19a419eaff93)
    
    Bug: 318349361
    Change-Id: I7cf985bd6d02f22a8dc33b7b0c5b271b33fd1c64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6055166
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1389700}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062697
    Cr-Commit-Position: refs/branch-heads/6834@{#1470}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- chrome/test/data/webui/signin/test_sync_confirmation_browser_proxy.ts
+++ chrome/test/data/webui/signin/test_sync_confirmation_browser_proxy.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import type {SyncConfirmationBrowserProxy} from 'chrome://sync-confirmation/sync_confirmation_browser_proxy.js';
+import type {ScreenMode, SyncConfirmationBrowserProxy} from 'chrome://sync-confirmation/sync_confirmation_browser_proxy.js';
 import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
 
 export class TestSyncConfirmationBrowserProxy extends TestBrowserProxy
@@ -18,16 +18,17 @@ export class TestSyncConfirmationBrowserProxy extends TestBrowserProxy
     ]);
   }
 
-  confirm(description: string[], confirmation: string) {
-    this.methodCalled('confirm', [description, confirmation]);
+  confirm(description: string[], confirmation: string, screenMode: ScreenMode) {
+    this.methodCalled('confirm', [description, confirmation, screenMode]);
   }
 
-  undo() {
-    this.methodCalled('undo');
+  undo(screenMode: ScreenMode) {
+    this.methodCalled('undo', [screenMode]);
   }
 
-  goToSettings(description: string[], confirmation: string) {
-    this.methodCalled('goToSettings', [description, confirmation]);
+  goToSettings(
+      description: string[], confirmation: string, screenMode: ScreenMode) {
+    this.methodCalled('goToSettings', [description, confirmation, screenMode]);
   }
 
   openDeviceSyncSettings() {
