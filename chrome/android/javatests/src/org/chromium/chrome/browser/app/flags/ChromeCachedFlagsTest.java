969f666c31eaa3adf2aeca0d81d2f3816ac42198
Rubin Deliallisi <rubindl@chromium.org>
2024-12-03 16:21:14 +0000

    [CodeHealth] Fix sSafetyHubMagicStack default value
    - The default value of the cached flags now matches the default value
    of the flag on the native side.
    - The flag is only used as a kill switch for a part of a feature that is
    already rolling out.
    
    Bug: 380113169
    Change-Id: Icc03c51d55a1e64195a5d84573a2cc8238ea3dde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062760
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391034}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlagsTest.java
@@ -65,7 +65,6 @@ public class ChromeCachedFlagsTest {
                     ChromeFeatureList.sNewTabPageAndroidTriggerForPrerender2,
                     ChromeFeatureList.sPostGetMyMemoryStateToBackground,
                     ChromeFeatureList.sPrefetchBrowserInitiatedTriggers,
-                    ChromeFeatureList.sSafetyHubMagicStack,
                     ChromeFeatureList.sSearchInCCT,
                     ChromeFeatureList.sTabGroupPaneAndroid,
                     ChromeFeatureList.sTabResumptionModuleAndroid,
