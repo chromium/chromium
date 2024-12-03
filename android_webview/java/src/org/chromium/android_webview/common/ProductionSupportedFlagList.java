5b8a11d97fb3bd6fda875d7a996a34f0986d5238
Daniel Lehmann <dlehmann@chromium.org>
2024-12-03 09:55:14 +0000

    [v8][wasm] Prepare WebView and field trial for Finch of Wasm deopt
    ...and speculative call_indirect inlining.
    
    There were already gin feature flags present for both V8 features, as
    well as two field trials. But the two disparate field trials didn't test
    Wasm deopt and call_indirect inlining enabled at the same time.
    Testing WebView needed an additional change as well.
    
    Bug: 42204618
    Change-Id: If1c765ebb5c001103fd994108d99a4181c03ce36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6062960
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Daniel Lehmann <dlehmann@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1390881}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -750,7 +750,8 @@ public final class ProductionSupportedFlagList {
         Flag.baseFeature("V8GCSpeedUsesCounters"),
         Flag.baseFeature("WebAssemblyTurboshaft"),
         Flag.baseFeature("WebAssemblyTurboshaftInstructionSelection"),
-        Flag.baseFeature("WebAssemblyInlining"),
+        Flag.baseFeature("WebAssemblyDeopt"),
+        Flag.baseFeature("WebAssemblyInliningCallIndirect"),
         Flag.baseFeature(
                 AwFeatures.WEBVIEW_MEDIA_INTEGRITY_API_BLINK_EXTENSION,
                 "Enable the WebView Media Integrity API as a Blink extension. Only works if"
