9627bc2609e8a458d07b49b6b88264e214f5eedb
Juan Mojica <juanmojica@google.com>
2024-12-03 00:57:43 +0000

    [lensoverlay] Fix coachmark anchor on Mac.
    The coachmark was anchoring incorrectly due to negative coordinates
    on MacOS. This modifies the min Y value to only be 8 if the anchor
    height > 0 (e.g. the anchor view is showing). This also makes sure
    that crbug.com/353978648 remains fixed.
    
    Bug: b:380097162
    Change-Id: I3f27e47835a75b9ef60f4d8479c1f7d2c4e52cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6064499
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390716}

--- chrome/browser/ui/lens/lens_preselection_bubble.cc
+++ chrome/browser/ui/lens/lens_preselection_bubble.cc
@@ -160,9 +160,14 @@ gfx::Rect LensPreselectionBubble::GetBubbleBounds() {
         anchor_bounds.x() + (anchor_bounds.width() - bubble_size.width()) / 2;
     // Take bubble out of its original bounds to cross "line of death". However,
     // if there is no line of death, we set the bubble to below the top of the
-    // screen.
-    const int y = std::max(kPreselectionBubbleMinY,
-                           anchor_bounds.bottom() - bubble_size.height() / 2);
+    // screen. On Mac, the |bottom| of |anchor_bounds| can be negative so set
+    // the appropriate minY based on whether the top container is showing (has a
+    // height > 0).
+    const int minY = anchor_bounds.height() > 0
+                         ? anchor_bounds.bottom() - bubble_size.height() / 2
+                         : kPreselectionBubbleMinY;
+    const int y =
+        std::max(minY, anchor_bounds.bottom() - bubble_size.height() / 2);
     return gfx::Rect(x, y, bubble_size.width(), bubble_size.height());
   }
   return gfx::Rect();
