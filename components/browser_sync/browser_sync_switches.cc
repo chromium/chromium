926456864c15a06cbda6aaf44fac594964593fef
Marc Treib <treib@chromium.org>
2024-12-03 13:48:06 +0000

    [iOS] Enable ForceMigrateSyncingUserToSignedIn by default
    Bug: 373263970
    Change-Id: Iec56b0f5b274e5d18c0e29e52c0901bb9ce662b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063580
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1390952}

--- components/browser_sync/browser_sync_switches.cc
+++ components/browser_sync/browser_sync_switches.cc
@@ -29,6 +29,10 @@ BASE_FEATURE(kUndoMigrationOfSyncingUserToSignedIn,
 
 BASE_FEATURE(kForceMigrateSyncingUserToSignedIn,
              "ForceMigrateSyncingUserToSignedIn",
+#if BUILDFLAG(IS_IOS)
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#else
              base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
 
 }  // namespace switches
