c6e518b54d74c4f674f84627c11ebff85f4d7564
Brandon Wylie <wylieb@chromium.org>
2024-12-04 19:33:21 +0000

    [M132][bookmarks] Use the original profile when accessing the IdentityManager
    (cherry picked from commit 09c0c5c120f158e9d626f64ea81ae686d92cf515)
    
    Bug: 379623585,382258215
    Change-Id: Ia55164bf1ad51d0e5f003662a3a93bd13aaab626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6050594
    Reviewed-by: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1390747}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6071305
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6834@{#1582}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerCoordinator.java
@@ -182,7 +182,8 @@ public class BookmarkManagerCoordinator
                         context,
                         mBookmarkModel,
                         snackbarManager,
-                        IdentityServicesProvider.get().getIdentityManager(profile));
+                        IdentityServicesProvider.get()
+                                .getIdentityManager(profile.getOriginalProfile()));
 
         // Using OneshotSupplier as an alternative to a 2-step initialization process.
         OneshotSupplierImpl<BookmarkDelegate> bookmarkDelegateSupplier =
