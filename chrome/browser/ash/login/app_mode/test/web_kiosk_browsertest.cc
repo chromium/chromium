83a1b60b41d9df584e9fb94bde1aca727ee5db6c
Edman Anjos <edmanp@google.com>
2024-12-04 11:03:25 +0000

    kiosk: Extract KioskWebAppOfflineEnabledTest from WebKioskTest
    Bug: 379246208
    Test: testing/xvfb.py tools/autotest.py -C out/Default chrome/browser/ash/login/app_mode/test/web_kiosk_browsertest.cc chrome/browser/ash/app_mode/test/kiosk_web_app_offline_enabled_browsertest.cc
    Change-Id: Ice6efe79abe87b86a4a526d91e0933fecfaa95b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054035
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391579}

--- chrome/browser/ash/login/app_mode/test/web_kiosk_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/web_kiosk_browsertest.cc
@@ -10,63 +10,36 @@
 #include "ash/public/cpp/keyboard/keyboard_controller.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
-#include "base/auto_reset.h"
 #include "base/check_deref.h"
 #include "base/check_op.h"
-#include "base/test/gtest_tags.h"
 #include "chrome/browser/ash/app_mode/kiosk_app.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_types.h"
 #include "chrome/browser/ash/app_mode/kiosk_controller.h"
-#include "chrome/browser/ash/app_mode/kiosk_test_helper.h"
 #include "chrome/browser/ash/app_mode/load_profile.h"
 #include "chrome/browser/ash/app_mode/test/kiosk_mixin.h"
 #include "chrome/browser/ash/app_mode/test/network_state_mixin.h"
 #include "chrome/browser/ash/app_mode/web_app/web_kiosk_app_manager.h"
 #include "chrome/browser/ash/login/app_mode/test/kiosk_base_test.h"
-#include "chrome/browser/ash/login/screens/error_screen.h"
-#include "chrome/browser/ash/login/test/js_checker.h"
-#include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/ash/ownership/fake_owner_settings_service.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_web_app_install_util.h"
-#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/ash/login/login_display_host.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/test/test_browser_closed_waiter.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/webui/ash/login/error_screen_handler.h"
 #include "chrome/browser/web_applications/externally_managed_app_manager.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
-#include "chromeos/crosapi/mojom/web_kiosk_service.mojom-shared.h"
 #include "components/policy/core/browser/browser_policy_connector.h"
-#include "components/policy/core/common/mock_configuration_policy_provider.h"
-#include "components/policy/core/common/policy_map.h"
-#include "components/policy/core/common/policy_types.h"
-#include "components/policy/policy_constants.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/app_window/app_window.h"
-#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
 
 namespace {
-
-const test::UIPath kNetworkConfigureScreenContinueButton = {"error-message",
-                                                            "continueButton"};
-
-Profile& CurrentProfile() {
-  return CHECK_DEREF(ProfileManager::GetPrimaryUserProfile());
-}
-
-bool IsWebAppInstalled(const KioskApp& app) {
-  auto& profile = CurrentProfile();
-  auto [state, __] =
-      chromeos::GetKioskWebAppInstallState(profile, app.url().value());
-  return crosapi::mojom::WebKioskInstallState::kInstalled == state;
-}
 
 Browser::CreateParams CreateNewBrowserParams(Browser* initial_kiosk_browser,
                                              bool is_popup_browser) {
@@ -87,19 +60,6 @@ Browser* OpenNewBrowser(Browser* initial_kiosk_browser, bool is_popup_browser) {
   Browser* new_browser = Browser::Create(params);
   new_browser->window()->Show();
   return new_browser;
-}
-
-void WaitNetworkScreen() {
-  OobeScreenWaiter(ErrorScreenView::kScreenId).Wait();
-}
-
-void ExpectNetworkScreenContinueButtonShown(bool is_shown) {
-  test::OobeJS().ExpectPathDisplayed(is_shown,
-                                     kNetworkConfigureScreenContinueButton);
-}
-
-[[nodiscard]] std::optional<base::AutoReset<bool>> BlockKioskLaunch() {
-  return {KioskTestHelper::BlockAppLaunch()};
 }
 
 // Returns the web app configured in Kiosk.
@@ -232,66 +192,5 @@ IN_PROC_BROWSER_TEST_F(WebKioskTest,
   ASSERT_TRUE(browser_closed_waiter.WaitUntilClosed());
   EXPECT_EQ(BrowserList::GetInstance()->size(), 1u);
 }
-
-class WebKioskOfflineEnabledTest : public WebKioskTest,
-                                   public testing::WithParamInterface<bool> {
- public:
-  WebKioskOfflineEnabledTest() = default;
-
-  WebKioskOfflineEnabledTest(const WebKioskOfflineEnabledTest&) = delete;
-  WebKioskOfflineEnabledTest& operator=(const WebKioskOfflineEnabledTest&) =
-      delete;
-
-  ~WebKioskOfflineEnabledTest() override = default;
-
-  void SetUpInProcessBrowserTestFixture() override {
-    WebKioskTest::SetUpInProcessBrowserTestFixture();
-    provider_.SetDefaultReturns(
-        /*is_initialization_complete_return=*/true,
-        /*is_first_policy_load_complete_return=*/true);
-
-    policy::BrowserPolicyConnector::SetPolicyProviderForTesting(&provider_);
-
-    policy::PolicyMap values;
-    values.Set(policy::key::kKioskWebAppOfflineEnabled,
-               policy::POLICY_LEVEL_MANDATORY, policy::POLICY_SCOPE_USER,
-               policy::POLICY_SOURCE_CLOUD, base::Value(IsAppOfflineEnabled()),
-               nullptr);
-    provider_.UpdateChromePolicy(values);
-  }
-
-  bool IsAppOfflineEnabled() { return GetParam(); }
-
- private:
-  testing::NiceMock<policy::MockConfigurationPolicyProvider> provider_;
-};
-
-IN_PROC_BROWSER_TEST_P(WebKioskOfflineEnabledTest,
-                       PRE_AlreadyInstalledOffline) {
-  network_state_.SimulateOnline();
-  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskWebApp()));
-  ASSERT_TRUE(kiosk_.WaitSessionLaunched());
-  ASSERT_TRUE(IsWebAppInstalled(TheKioskWebApp()));
-}
-
-IN_PROC_BROWSER_TEST_P(WebKioskOfflineEnabledTest, AlreadyInstalledOffline) {
-  base::AddFeatureIdTagToTestResult(
-      "screenplay-35e430a3-04b3-46a7-aa0a-207a368b8cba");
-
-  network_state_.SimulateOffline();
-  ASSERT_TRUE(kiosk_.LaunchManually(TheKioskWebApp()));
-
-  if (!IsAppOfflineEnabled()) {
-    auto block_launch_override = BlockKioskLaunch();
-    WaitNetworkScreen();
-    ExpectNetworkScreenContinueButtonShown(/*is_shown=*/false);
-    block_launch_override.reset();
-    network_state_.SimulateOnline();
-  }
-
-  ASSERT_TRUE(kiosk_.WaitSessionLaunched());
-}
-
-INSTANTIATE_TEST_SUITE_P(All, WebKioskOfflineEnabledTest, testing::Bool());
 
 }  // namespace ash
