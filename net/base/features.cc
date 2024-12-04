ed1a8b05afa0cd16e9aa7e7c9b64041f8575f463
David Benjamin <davidben@chromium.org>
2024-12-04 01:03:05 +0000

    Enable PostQuantumKyber (i.e. ML-KEM) on all platforms by default
    Previously it was only on for desktop by default. This picks up mobile
    as well.
    
    Note: the feature is named "Kyber" but it actually controls ML-KEM
    because the UseMLKEM feature has already been toggled on.
    
    Bug: 40910498
    Change-Id: Ie9a082623cc39fa46990944ee74864bbbed40c9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6064451
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391386}

--- net/base/features.cc
+++ net/base/features.cc
@@ -154,11 +154,7 @@ BASE_FEATURE(kPartitionConnectionsByNetworkIsolationKey,
 
 BASE_FEATURE(kPostQuantumKyber,
              "PostQuantumKyber",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#else
              base::FEATURE_ENABLED_BY_DEFAULT);
-#endif
 
 BASE_FEATURE(kUseMLKEM, "UseMLKEM", base::FEATURE_ENABLED_BY_DEFAULT);
 
