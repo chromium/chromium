e603bc88a2e35eb09081e491baef28e0ef83cf97
Federica Germinario <fedegermi@google.com>
2024-12-04 15:25:09 +0000

    Use ProfileKeyedServiceFactoryIOS in AutocompleteScoringModelServiceFactory
    Convert AutocompleteScoringModelServiceFactory to use
    ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215799
    Change-Id: I322a5a5dab8a0c304012bc21b6600ea2b90c5896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063808
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1391665}

--- ios/chrome/browser/autocomplete/model/autocomplete_scoring_model_service_factory.h
+++ ios/chrome/browser/autocomplete/model/autocomplete_scoring_model_service_factory.h
@@ -5,34 +5,22 @@
 #ifndef IOS_CHROME_BROWSER_AUTOCOMPLETE_MODEL_AUTOCOMPLETE_SCORING_MODEL_SERVICE_FACTORY_H_
 #define IOS_CHROME_BROWSER_AUTOCOMPLETE_MODEL_AUTOCOMPLETE_SCORING_MODEL_SERVICE_FACTORY_H_
 
-#import <memory>
-
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class AutocompleteScoringModelService;
 class KeyedService;
 class ProfileIOS;
-
-namespace content {
-class BrowserState;
-}  // namespace content
 
 namespace ios {
 
 // A factory to create a unique `AutocompleteScoringModelService` per
 // profile. Has a dependency on `OptimizationGuideKeyedServiceFactory`.
 class AutocompleteScoringModelServiceFactory
-    : public BrowserStateKeyedServiceFactory {
+    : public ProfileKeyedServiceFactoryIOS {
  public:
   static AutocompleteScoringModelService* GetForProfile(ProfileIOS* profile);
   static AutocompleteScoringModelServiceFactory* GetInstance();
-
-  // Disallow copy/assign.
-  AutocompleteScoringModelServiceFactory(
-      const AutocompleteScoringModelServiceFactory&) = delete;
-  AutocompleteScoringModelServiceFactory& operator=(
-      const AutocompleteScoringModelServiceFactory&) = delete;
 
  private:
   friend class base::NoDestructor<AutocompleteScoringModelServiceFactory>;
@@ -40,14 +28,10 @@ class AutocompleteScoringModelServiceFactory
   AutocompleteScoringModelServiceFactory();
   ~AutocompleteScoringModelServiceFactory() override;
 
-  // `BrowserContextKeyedServiceFactory` overrides.
-  //
+  // BrowserStateKeyedServiceFactory:
   // Returns nullptr if `OptimizationGuideKeyedService` is null.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
-      web::BrowserState* context) const override;
-  bool ServiceIsNULLWhileTesting() const override;
 };
 
 }  // namespace ios
