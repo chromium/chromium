fcb24546f922b1d87ed01f27b93731a56570c21d
Jeffrey Young <cowmoo@google.com>
2024-12-05 17:55:50 +0000

    ash: rm kJelly feature from fullscreen_pixeltest
    kJelly is fully launched and should be torn down.
    
    Bug: 291622042
    Test: ash_pixeltests --gtest_filter=*FullscreenPixel*
    Change-Id: If87456a387c27d83525abc319e2620ed7c80c394
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6071446
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392376}

--- ash/fullscreen_pixeltest.cc
+++ ash/fullscreen_pixeltest.cc
@@ -6,23 +6,18 @@
 #include "ash/test/ash_test_base.h"
 #include "ash/test/pixel/ash_pixel_differ.h"
 #include "ash/test/pixel/ash_pixel_test_init_params.h"
-#include "base/test/scoped_feature_list.h"
-#include "chromeos/constants/chromeos_features.h"
 
 namespace ash {
 
 class FullscreenPixelTest : public AshTestBase {
  public:
-  FullscreenPixelTest() : scoped_features_(chromeos::features::kJelly) {}
+  FullscreenPixelTest() = default;
 
   // AshTestBase:
   std::optional<pixel_test::InitParams> CreatePixelTestInitParams()
       const override {
     return pixel_test::InitParams();
   }
-
- private:
-  base::test::ScopedFeatureList scoped_features_;
 };
 
 // Verifies the primary fullscreen of an active user session.
