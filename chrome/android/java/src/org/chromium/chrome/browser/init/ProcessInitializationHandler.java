462db54088c1f5a3fbb71495f941f4752351ea10
Mark Schillaci <mschillaci@google.com>
2024-12-05 15:41:12 +0000

    [Page Zoom] Add metric to track overall feature usage
    This CL adds a histogram that is recorded on start-up that will give
    a signal of the overall usage of the Accessibility Page Zoom feature,
    measured as any user that has set a zoom level on a site, or set a
    default value in the accessibility settings page.
    
    AX-Relnotes: N/A
    Bug: 380466815
    Change-Id: I502257b4f9da470c81b2d7cdb525e5e040311cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6063829
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1392293}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -103,6 +103,7 @@ import org.chromium.chrome.browser.webapps.ChromeWebApkHost;
 import org.chromium.chrome.browser.webapps.WebApkUninstallTracker;
 import org.chromium.chrome.browser.webapps.WebappRegistry;
 import org.chromium.components.background_task_scheduler.BackgroundTaskSchedulerFactory;
+import org.chromium.components.browser_ui.accessibility.PageZoomUtils;
 import org.chromium.components.browser_ui.contacts_picker.ContactsPickerDialog;
 import org.chromium.components.browser_ui.photo_picker.DecoderServiceHost;
 import org.chromium.components.browser_ui.photo_picker.PhotoPickerDelegateBase;
@@ -522,6 +523,7 @@ public class ProcessInitializationHandler {
                 .addObserver(
                         new ContentCaptureHistoryDeletionObserver(
                                 () -> PlatformContentCaptureController.getInstance()));
+        PageZoomUtils.recordFeatureUsage(profile);
     }
 
     /**
