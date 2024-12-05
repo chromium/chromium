687d6a24fbb9282699c2c58d03603a36c6fc4572
Sylvain Defresne <sdefresne@chromium.org>
2024-12-05 09:41:41 +0000

    [ios] Convert TemplateURLServiceFactory to PKSFIOS
    Convert TemplateURLServiceFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363216382
    Change-Id: I366852c96db2e417ce6f23942628f19a40c68468
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070258
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392143}

--- ios/chrome/browser/search_engines/model/template_url_service_factory.mm
+++ ios/chrome/browser/search_engines/model/template_url_service_factory.mm
@@ -9,14 +9,12 @@
 #import "base/functional/callback.h"
 #import "base/no_destructor.h"
 #import "components/keyed_service/core/service_access_type.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "components/search_engines/default_search_manager.h"
 #import "components/search_engines/template_url_service.h"
 #import "ios/chrome/browser/history/model/history_service_factory.h"
 #import "ios/chrome/browser/search_engines/model/search_engine_choice_service_factory.h"
 #import "ios/chrome/browser/search_engines/model/template_url_service_client_impl.h"
 #import "ios/chrome/browser/search_engines/model/ui_thread_search_terms_data.h"
-#import "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/webdata_services/model/web_data_service_factory.h"
 #import "rlz/buildflags/buildflags.h"
@@ -59,8 +57,8 @@ std::unique_ptr<KeyedService> BuildTemplateURLService(
 // static
 TemplateURLService* TemplateURLServiceFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<TemplateURLService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<TemplateURLService>(
+      profile, /*create=*/true);
 }
 
 // static
@@ -76,9 +74,9 @@ TemplateURLServiceFactory::GetDefaultFactory() {
 }
 
 TemplateURLServiceFactory::TemplateURLServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "TemplateURLService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("TemplateURLService",
+                                    ProfileSelection::kRedirectedInIncognito,
+                                    TestingCreation::kNoServiceForTests) {
   DependsOn(ios::HistoryServiceFactory::GetInstance());
   DependsOn(ios::WebDataServiceFactory::GetInstance());
   DependsOn(ios::SearchEngineChoiceServiceFactory::GetInstance());
@@ -96,15 +94,6 @@ std::unique_ptr<KeyedService>
 TemplateURLServiceFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
   return BuildTemplateURLService(context);
-}
-
-web::BrowserState* TemplateURLServiceFactory::GetBrowserStateToUse(
-    web::BrowserState* context) const {
-  return GetBrowserStateRedirectedInIncognito(context);
-}
-
-bool TemplateURLServiceFactory::ServiceIsNULLWhileTesting() const {
-  return true;
 }
 
 }  // namespace ios
