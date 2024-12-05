ee9452c421dcf20d02fe7d4863f82e87b79cff2b
Vincent Boisselle <vincb@google.com>
2024-12-05 16:25:39 +0000

    Revert "[iOS] Updates test_HTTPWithSlowHTTPS_ShouldFallBack for rich inline"
    This reverts commit 5875ff225e2a9d1ffc9f7a4291b5dea21cd0ade5.
    
    Reason for revert: TypedNavigationUpgradeTestCase/test_HTTPWithSlowHTTPS_ShouldFallBack started failing
    
    Original change's description:
    > [iOS] Updates test_HTTPWithSlowHTTPS_ShouldFallBack for rich inline
    >
    > Before the rich inline autocomplete feature, the omnibox would load a
    > history suggestion that loads the HTTP URL directly.
    >
    > With rich inline autocomplete, the omnibox loads a shortcut suggestion
    > which retries the HTTPS URL.
    >
    > This CL update the test to reflect this behavior. Also update typing
    > text in the omnibox to check the text without the additional text of
    > rich inline autocomplete.
    >
    > Bug: 335821156, 40165447
    > Change-Id: I778fdf0edac57e2ed436a5ed84a8908da6c88247
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070030
    > Commit-Queue: Christian Xu <christianxu@chromium.org>
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1392122}
    
    Bug: 335821156, 40165447
    Change-Id: Ibdfc988cf94a0bc62ae4d959d6ecb830c87ce777
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075262
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Owners-Override: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1392315}

--- ios/chrome/test/earl_grey/chrome_matchers_app_interface.mm
+++ ios/chrome/test/earl_grey/chrome_matchers_app_interface.mm
@@ -471,7 +471,7 @@ UIWindow* WindowWithAccessibilityIdentifier(NSString* accessibility_id) {
       matcherWithMatchesBlock:^BOOL(id element) {
         OmniboxTextFieldIOS* omnibox =
             base::apple::ObjCCast<OmniboxTextFieldIOS>(element);
-        return [omnibox.displayedText isEqualToString:text];
+        return [omnibox.text isEqualToString:text];
       }
       descriptionBlock:^void(id<GREYDescription> description) {
         [description
