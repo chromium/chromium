729715b327e4af8105dec17d54fc009e52412537
Federica Germinario <fedegermi@google.com>
2024-12-05 17:07:14 +0000

    Use ProfileKeyedServiceFactoryIOS in DeviceSharingManagerFactory
    Convert DeviceSharingManagerFactory to use
    ProfileKeyedServiceFactoryIOS.
    This improvement removes boilerplate code from the factory.
    
    Fixed: 363215625
    Change-Id: Ic68baab46174b663ae00ebde67ae5758be32ea63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074638
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392341}

--- ios/chrome/browser/device_sharing/model/device_sharing_manager_factory.h
+++ ios/chrome/browser/device_sharing/model/device_sharing_manager_factory.h
@@ -6,14 +6,14 @@
 #define IOS_CHROME_BROWSER_DEVICE_SHARING_MODEL_DEVICE_SHARING_MANAGER_FACTORY_H_
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class DeviceSharingManager;
 class ProfileIOS;
 
 // Keyed service factory for BrowserList.
 // This factory returns the same instance for regular and OTR profiles.
-class DeviceSharingManagerFactory : public BrowserStateKeyedServiceFactory {
+class DeviceSharingManagerFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static DeviceSharingManager* GetForProfile(ProfileIOS* profile);
   static DeviceSharingManagerFactory* GetInstance();
@@ -22,11 +22,6 @@ class DeviceSharingManagerFactory : public BrowserStateKeyedServiceFactory {
   // registered with SetTestingFactory to use real instances during testing.
   static TestingFactory GetDefaultFactory();
 
-  // Not copyable or moveable.
-  DeviceSharingManagerFactory(const DeviceSharingManagerFactory&) = delete;
-  DeviceSharingManagerFactory& operator=(const DeviceSharingManagerFactory&) =
-      delete;
-
  private:
   friend class base::NoDestructor<DeviceSharingManagerFactory>;
 
@@ -34,8 +29,6 @@ class DeviceSharingManagerFactory : public BrowserStateKeyedServiceFactory {
 
   // BrowserStateKeyedServiceFactory:
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 };
 
