cf1baa459f60bfb45ca3ab485304244459ae37bf
Arthur Milchior <arthurmilchior@chromium.org>
2024-12-05 16:25:00 +0000

    [iOS][Signin]Move InterruptibleChromeCoordinator
    This class should not have been part of the first_run folder. It is
    used by every SigninCoordinators.
    
    This will be even more important while I try to remove the
    InterruptibleCoordinator, because I’ll need to introduce a feature
    flag, and I want to define it with the InterruptibleChromeCoordinator,
    but don’t want the code to seems to indicate that some classes
    dependds on the first_run experience when it does not.
    
    Bug: 381444097
    Change-Id: Idfe013f7f4a4931644abd004ce578c271ac4506d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068481
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392314}

--- ios/chrome/browser/ui/authentication/signin/interruptible_chrome_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/interruptible_chrome_coordinator.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/first_run/ui_bundled/interruptible_chrome_coordinator.h"
+#import "ios/chrome/browser/ui/authentication/signin/interruptible_chrome_coordinator.h"
 
 @implementation InterruptibleChromeCoordinator
 
