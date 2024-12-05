b88f6cdb398b324febe412dc34bfeaaf537a74a2
Longsheng Yin <longsheng@google.com>
2024-12-05 23:02:02 +0000

    [Amount Extraction] Create stub implementation of Amount Extraction Heuristic Regexes Installer
    * This installer is used to integrate with Component Updater.
    * Loading and populating logic will be implemented in a subsequent CL.
    
    Bug: 378147177
    Change-Id: Ib2808a91da426dcf25d21d85256faa57aa43d009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068808
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Longsheng Yin <longsheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1392603}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -100,6 +100,12 @@
 #include "chrome/browser/component_updater/widevine_cdm_component_installer.h"
 #endif  // BUILDFLAG(ENABLE_WIDEVINE_CDM_COMPONENT)
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS)
+#include "components/component_updater/installer_policies/amount_extraction_heuristic_regexes_component_installer.h"
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
+        // BUILDFLAG(IS_CHROMEOS)
+
 #if defined(USE_AURA)
 #include "ui/aura/env.h"
 #endif
@@ -243,6 +249,12 @@ void RegisterComponentsForUpdate() {
   RegisterOpenCookieDatabaseComponent(cus);
 
   RegisterCookieReadinessListComponent(cus);
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS)
+  RegisterAmountExtractionHeuristicRegexesComponent(cus);
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
+        // BUILDFLAG(IS_CHROMEOS)
 }
 
 }  // namespace component_updater
