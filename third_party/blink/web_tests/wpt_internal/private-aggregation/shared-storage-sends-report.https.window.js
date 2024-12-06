aa52462ac98ccb1a4134a7aa72086ba178e6776b
Dan McArdle <dmcardle@chromium.org>
2024-12-06 18:56:04 +0000

    PrivateAggregation: Increase polling timeout in flaky Shared Storage WPT
    Failures are spiking today for shared-storage-sends-report [0]. Here's a
    representative error message:
    
      [FAIL] run() that calls Private Aggregation with mergeable contributions
        assert_equals: Unexpected number of reports. expected 1 but got 0
    
    [0]: https://luci-analysis.appspot.com/p/chromium/clusters/testname-v4/cbefd7e53a9c59053a133ad09e52e1de?selectedMetrics=human-cls-failed-presubmit%2Ccritical-failures-exonerated%2Cfailures%2Cbuilds-failed-due-to-flaky-tests&historyTimeRange=30d&annotated=false&tab=overview
    
    It's plausible that https://crrev.com/c/6068376 is responsible because
    it reduced the polling timeout in all of Private Aggregation's
    shared-storage-* WPTs. I'm not sure why shared-storage-sends-report,
    specifically "run() that calls Private Aggregation with mergeable
    contributions" seems to be the primarily affected test. I'm guessing it
    may be because it might be because the previous test runs *two*
    worklets, which might mean it takes a little longer before the browser
    sends the expected reports.
    
    This CL speculatively increases the value of `fullTimeoutMs`. Hopefully,
    this should enable the failing test to wait long enough for the expected
    report to be received.
    
    Comparison of the individual test times (in milliseconds):
    
      .virtual
        .private-aggregation-developer-mode
          .wpt_internal
            .private-aggregation
              .fenced-frame-disallowed-fields.sub.https.html
                1075 -> 1110 (103.26%)
              .fenced-frame-network-revocation.sub.https.html
                5596 -> 5471 (97.77%)
              .protected-audience-aggregation-coordinator.https.window.html
                24694 -> 24831 (100.55%)
              .protected-audience-auction-report-buyers-debug-mode-disabled.https.window.html
                25910 -> 25973 (100.24%)
              .protected-audience-auction-report-buyers-debug-mode-enabled.https.window.html
                11191 -> 11212 (100.19%)
              .protected-audience-filtering-id-sends-report.https.window.html
                22323 -> 22289 (99.85%)
              .protected-audience-sends-report.https.window.html
                17776 -> 18202 (102.40%)
              .shared-storage-aggregation-coordinator.https.window.html
                2007 -> 2019 (100.60%)
              .shared-storage-context-id-sends-report.https.window.html
                1669 -> 1647 (98.68%)
              .shared-storage-filtering-id-sends-null-report.https.window.html
                3233 -> 2616 (80.92%)
              .shared-storage-filtering-id-sends-report.https.window.html
                4821 -> 4702 (97.53%)
              .shared-storage-sends-report.https.window.html
                4855 -> 8146 (167.79%)
    
    Bug: 362901607, 382564805
    Change-Id: I49e9b2f15f49811a96b1202b329fca3ff93c36b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6077585
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Auto-Submit: Dan McArdle <dmcardle@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393041}

--- third_party/blink/web_tests/wpt_internal/private-aggregation/shared-storage-sends-report.https.window.js
+++ third_party/blink/web_tests/wpt_internal/private-aggregation/shared-storage-sends-report.https.window.js
@@ -9,7 +9,7 @@
 const reportPoller = new ReportPoller(
     '/.well-known/private-aggregation/report-shared-storage',
     '/.well-known/private-aggregation/debug/report-shared-storage',
-    /*fullTimeoutMs=*/ 500,
+    /*fullTimeoutMs=*/ 1500,
 );
 
 private_aggregation_promise_test(async () => {
