d547e68c2be460a5f165e8c6b64ba02fad9fbac5
Sylvain Defresne <sdefresne@chromium.org>
2024-12-06 09:37:58 +0000

    [ios] Convert SafeBrowsingClientFactory to PKSFIOS
    Convert SafeBrowsingClientFactory to be a ProfileKeydServiceFactoryIOS.
    
    Fixed: 363217787
    Change-Id: Ifcd9c21cfb8fae55f6e803101f4ea37a16aedb88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6070032
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1392769}

--- ios/chrome/browser/safe_browsing/model/safe_browsing_client_factory.h
+++ ios/chrome/browser/safe_browsing/model/safe_browsing_client_factory.h
@@ -8,21 +8,17 @@
 #include <memory>
 
 #include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 class ProfileIOS;
 class SafeBrowsingClient;
 
 // Singleton that owns all SafeBrowsingClients and associates them with
 // a profile.
-class SafeBrowsingClientFactory : public BrowserStateKeyedServiceFactory {
+class SafeBrowsingClientFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static SafeBrowsingClient* GetForProfile(ProfileIOS* profile);
   static SafeBrowsingClientFactory* GetInstance();
-
-  SafeBrowsingClientFactory(const SafeBrowsingClientFactory&) = delete;
-  SafeBrowsingClientFactory& operator=(const SafeBrowsingClientFactory&) =
-      delete;
 
  private:
   friend class base::NoDestructor<SafeBrowsingClientFactory>;
@@ -32,8 +28,6 @@ class SafeBrowsingClientFactory : public BrowserStateKeyedServiceFactory {
 
   // BrowserStateKeyedServiceFactory implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
-      web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
       web::BrowserState* context) const override;
 };
 
