0a3c0b08c163b50b3721cf6b36e06f5ba17c4203
Dale Curtis <dalecurtis@chromium.org>
2024-12-06 21:38:31 +0000

    Refactor code for setting up MediaCodec CryptoInfo.
    This prepare the code for reuse with LinearBlock secure buffers.
    
    R=jrummell
    
    Bug: 327625558
    Change-Id: I7346577c321622bef2527d2f495eb4d94a757e5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6075701
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393159}

--- media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
@@ -791,6 +791,39 @@ class MediaCodecBridge {
         }
     }
 
+    private int validateCryptoInfo(int cipherMode, int patternEncrypt, int patternSkip) {
+        if (cipherMode == MEDIA_CODEC_UNKNOWN_CIPHER_MODE) {
+            return MediaCodecStatus.UNKNOWN_CIPHER_MODE;
+        }
+        if (cipherMode != MediaCodec.CRYPTO_MODE_AES_CBC
+                && patternEncrypt != 0
+                && patternSkip != 0) {
+            Log.e(TAG, "Pattern encryption only supported for 'cbcs' scheme (CBC mode).");
+            return MediaCodecStatus.PATTERN_ENCRYPTION_NOT_SUPPORTED;
+        }
+        return MediaCodecStatus.OK;
+    }
+
+    private CryptoInfo generateCryptoInfo(
+            byte[] iv,
+            byte[] keyId,
+            int[] numBytesOfClearData,
+            int[] numBytesOfEncryptedData,
+            int numSubSamples,
+            int cipherMode,
+            int patternEncrypt,
+            int patternSkip) {
+        var cryptoInfo = new CryptoInfo();
+        cryptoInfo.set(
+                numSubSamples, numBytesOfClearData, numBytesOfEncryptedData, keyId, iv, cipherMode);
+        if (cipherMode == MediaCodec.CRYPTO_MODE_AES_CBC
+                && patternEncrypt != 0
+                && patternSkip != 0) {
+            cryptoInfo.setPattern(new CryptoInfo.Pattern(patternEncrypt, patternSkip));
+        }
+        return cryptoInfo;
+    }
+
     @SuppressLint("WrongConstant") // False positive on logging statement.
     @CalledByNative
     private int queueSecureInputBuffer(
@@ -807,27 +840,23 @@ class MediaCodecBridge {
             long presentationTimeUs) {
         try {
             cipherMode = translateEncryptionSchemeValue(cipherMode);
-            if (cipherMode == MEDIA_CODEC_UNKNOWN_CIPHER_MODE) {
-                return MediaCodecStatus.UNKNOWN_CIPHER_MODE;
+
+            var status = validateCryptoInfo(cipherMode, patternEncrypt, patternSkip);
+            if (status != MediaCodecStatus.OK) {
+                return status;
             }
-            boolean usesCbcs = cipherMode == MediaCodec.CRYPTO_MODE_AES_CBC;
-            CryptoInfo cryptoInfo = new CryptoInfo();
-            cryptoInfo.set(
-                    numSubSamples,
-                    numBytesOfClearData,
-                    numBytesOfEncryptedData,
-                    keyId,
-                    iv,
-                    cipherMode);
-            if (patternEncrypt != 0 && patternSkip != 0) {
-                if (usesCbcs) {
-                    // Above platform check ensured that setting the pattern is indeed supported.
-                    MediaCodecUtil.setPatternIfSupported(cryptoInfo, patternEncrypt, patternSkip);
-                } else {
-                    Log.e(TAG, "Pattern encryption only supported for 'cbcs' scheme (CBC mode).");
-                    return MediaCodecStatus.PATTERN_ENCRYPTION_NOT_SUPPORTED;
-                }
-            }
+
+            var cryptoInfo =
+                    generateCryptoInfo(
+                            iv,
+                            keyId,
+                            numBytesOfClearData,
+                            numBytesOfEncryptedData,
+                            numSubSamples,
+                            cipherMode,
+                            patternEncrypt,
+                            patternSkip);
+            assert cryptoInfo != null;
             mMediaCodec.queueSecureInputBuffer(index, offset, cryptoInfo, presentationTimeUs, 0);
         } catch (MediaCodec.CryptoException e) {
             if (e.getErrorCode() == MediaCodec.CryptoException.ERROR_NO_KEY) {
