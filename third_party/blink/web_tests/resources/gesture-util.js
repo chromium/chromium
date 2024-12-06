2d181dfe726e1cbff7b6183cf65f8b5fa4eac1ea
Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
2024-12-06 18:14:18 +0000

    Remove scroll_by_percentage param from SmoothScrollByXY()
    The CL removes scroll_by_percentage parameter from
    GpuBenchmarking::SmoothScrollByXY() and its remaining usages in the
    codebase. The param will be still present in deprecated
    GpuBenchmarking::SmoothScrollBy() due to the dependency of
    third_party/catapult/ and tools/perf/ code on the method. This will
    be fixed later. See more details: crbug.com/382108343.
    
    Bug: 359747082
    Change-Id: I7cbd20a8eeebdb001fb11db8cc95d2e308df0994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6042872
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Commit-Queue: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1393013}

--- third_party/blink/web_tests/resources/gesture-util.js
+++ third_party/blink/web_tests/resources/gesture-util.js
@@ -300,8 +300,7 @@ const MIN_FRACTION_TO_STEP_WHEN_PAGING = 0.875;
 // This will be replaced by smoothScrollWithXY.
 function smoothScroll(pixels_to_scroll, start_x, start_y, gesture_source_type,
                       direction, speed_in_pixels_s, precise_scrolling_deltas,
-                      scroll_by_page, cursor_visible, scroll_by_percentage,
-                      modifier_keys) {
+                      scroll_by_page, cursor_visible, modifier_keys) {
   let pixels_to_scroll_x = 0;
   let pixels_to_scroll_y = 0;
   if (direction == "down") {
@@ -328,7 +327,7 @@ function smoothScroll(pixels_to_scroll, start_x, start_y, gesture_source_type,
   return smoothScrollWithXY(pixels_to_scroll_x, pixels_to_scroll_y, start_x,
                             start_y, gesture_source_type, speed_in_pixels_s,
                             precise_scrolling_deltas, scroll_by_page,
-                            cursor_visible, scroll_by_percentage, modifier_keys);
+                            cursor_visible, modifier_keys);
 }
 
 // modifier_keys means the keys pressed while doing the mouse wheel scroll, it
@@ -340,8 +339,7 @@ function smoothScroll(pixels_to_scroll, start_x, start_y, gesture_source_type,
 function smoothScrollWithXY(pixels_to_scroll_x, pixels_to_scroll_y, start_x,
                             start_y, gesture_source_type, speed_in_pixels_s,
                             precise_scrolling_deltas, scroll_by_page,
-                            cursor_visible, scroll_by_percentage, modifier_keys,
-                            modifier_buttons) {
+                            cursor_visible, modifier_keys, modifier_buttons) {
   return new Promise((resolve, reject) => {
     if (window.chrome && chrome.gpuBenchmarking) {
       chrome.gpuBenchmarking.smoothScrollByXY(pixels_to_scroll_x,
@@ -354,7 +352,6 @@ function smoothScrollWithXY(pixels_to_scroll_x, pixels_to_scroll_y, start_x,
                                               precise_scrolling_deltas,
                                               scroll_by_page,
                                               cursor_visible,
-                                              scroll_by_percentage,
                                               modifier_keys,
                                               modifier_buttons);
     } else {
@@ -379,8 +376,7 @@ function wheelTick(scroll_tick_x, scroll_tick_y, center, speed_in_pixels_s,
                             center.x, center.y, GestureSourceType.MOUSE_INPUT,
                             speed_in_pixels_s, false /* precise_scrolling_deltas */,
                             false /* scroll_by_page */, true /* cursor_visible */,
-                            false /* scroll_by_percentage */, modifier_keys,
-                            modifier_buttons);
+                            modifier_keys, modifier_buttons);
 }
 
 const LEGACY_MOUSE_WHEEL_TICK_MULTIPLIER = 120;
