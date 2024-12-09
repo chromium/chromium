f02a1891391c56cbb2951650b39aac32feb78c96
Sergey Poromov <poromov@chromium.org>
2024-12-09 14:22:25 +0000

    [gardener] Revert "HTML attribute fuzzer"
    This reverts commit 217a1ffac201748d3985ea1fdd4ca89625cb00a9.
    
    Reason for revert: Consistent Linux ChromiumOS MSan Tests failures.
    First failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/48286/blamelist
    
    Original change's description:
    > HTML attribute fuzzer
    >
    > This is a first step toward a fuzzer for all Blink's HTML attributes,
    > which is a notable gap in our coverage-guided fuzzer report (for
    > instance
    > https://analysis.chromium.org/coverage/p/chromium/file?host=chromium.googlesource.com&project=chromium/src&ref=refs/heads/main&revision=e3c6b61f35330afb377a2086ffadc3d7e2321d8c&path=//third_party/blink/renderer/core/html/html_anchor_element.cc&platform=fuzz&test_suite_type=any&modifier_id=0#348
    > ).
    >
    > To run it:
    > ASAN_OPTIONS=detect_odr_violation=0 out/libfuzzer/blink_unittests_BlinkAttributeFuzzTestFixture_NoElementAttributeCrashes_fuzzer
    >
    > This may be slightly duplicative of some other fuzzers (e.g. blackbox
    > fuzzers, and the DomatoLPM fuzzer under development) but as a simple
    > coverage guided fuzzer in this area it's likely to explore this code
    > more rapidly than most other approaches.
    >
    > Cq-Include-Trybots: luci.chromium.try:linux-centipede-asan-rel
    > Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel
    > Cq-Include-Trybots: luci.chromium.try:win-libfuzzer-asan-rel
    > Bug: 40947696, 40286211
    > Change-Id: Id2f61ab4d9d9c59b877e3760620ed059d2a4386f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388376
    > Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1393546}
    
    Bug: 40947696, 40286211
    Change-Id: I5ed0176fa55854c6e8bdd638f30eb36d14ff7460
    Cq-Include-Trybots: luci.chromium.try:linux-centipede-asan-rel
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel
    Cq-Include-Trybots: luci.chromium.try:win-libfuzzer-asan-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080516
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Owners-Override: Sergey Poromov <poromov@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393631}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -2517,8 +2517,7 @@ def check(path, contents):
             or basename.endswith('_test') or basename.endswith('_test_helpers')
             or basename.endswith('_test_utils')
             or basename.endswith('_unittest') or basename.endswith('_fuzzer')
-            or basename.endswith('_perftest')
-            or basename.endswith('_fuzztest')):
+            or basename.endswith('_perftest')):
         return results
     entries = _find_matching_entries(path)
     if not entries:
