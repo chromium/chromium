b1e00124b2ec1fe4dd41451de9ab23c4923888b6
Munira Tursunova <moonira@google.com>
2024-12-09 13:34:23 +0000

    Fix wrong emoji presentation selection on Android
    Currently we rely on the assumption that the only color font installed
    on Android platform is Noto Color Emoji when retrieving platform
    fallback font for character.
    So currently if the monochromatic glyph presentation was requested and
    the found font is "Noto Color Emoji" font, we override it with system
    monochromatic font. Although if the monochromatic presentation of the
    glyph was requested and the found font was colored font, but it's
    postscript name is not "NotoColorEmoji", we use that colored font, even
    when there is a font with monochromatic presentation of the searching
    glyph in the system.
    To fix that we check whether the font is colored or not based on the
    presence of color table in the font.
    
    (cherry picked from commit 8ecd68a72fe5c67c55a69c5e68ed2fd9c27d43d0)
    
    Bug: 379709899
    Change-Id: I89a16a8bbe43b8ed6d2468a585452f204047ab2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038776
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1388344}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080514
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Auto-Submit: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/branch-heads/6778@{#2746}
    Cr-Branched-From: b21671ca172dcfd1566d41a770b2808e7fa7cd88-refs/heads/main@{#1368529}

---
+++ third_party/blink/renderer/platform/fonts/opentype/color_table_lookup.cc
@@ -0,0 +1,65 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/fonts/opentype/color_table_lookup.h"
+
+namespace blink {
+
+namespace {
+SkFontTableTag kCpalTag = SkSetFourByteTag('C', 'P', 'A', 'L');
+SkFontTableTag kColrTag = SkSetFourByteTag('C', 'O', 'L', 'R');
+SkFontTableTag kSbixTag = SkSetFourByteTag('s', 'b', 'i', 'x');
+SkFontTableTag kCbdtTag = SkSetFourByteTag('C', 'B', 'D', 'T');
+SkFontTableTag kCblcTag = SkSetFourByteTag('C', 'B', 'L', 'C');
+}  // namespace
+
+bool ColorTableLookup::TypefaceHasAnySupportedColorTable(
+    const SkTypeface* typeface) {
+  if (!typeface) {
+    return false;
+  }
+  const int num_tags = typeface->countTables();
+  if (!num_tags) {
+    return false;
+  }
+  std::unique_ptr<SkFontTableTag[]> tags(new SkFontTableTag[num_tags]);
+  const int returned_tags = typeface->getTableTags(tags.get());
+  if (!returned_tags) {
+    return false;
+  }
+  bool has_cpal = false;
+  bool has_colr = false;
+  bool has_cbdt = false;
+  bool has_cblc = false;
+  for (int i = 0; i < returned_tags; i++) {
+    SkFontTableTag tag = tags[i];
+    if (tag == kSbixTag) {
+      return true;
+    }
+    if (tag == kCpalTag) {
+      if (has_colr) {
+        return true;
+      }
+      has_cpal = true;
+    } else if (tag == kColrTag) {
+      if (has_cpal) {
+        return true;
+      }
+      has_colr = true;
+    } else if (tag == kCbdtTag) {
+      if (has_cblc) {
+        return true;
+      }
+      has_cbdt = true;
+    } else if (tag == kCblcTag) {
+      if (has_cbdt) {
+        return true;
+      }
+      has_cblc = true;
+    }
+  }
+  return false;
+}
+
+}  // namespace blink
