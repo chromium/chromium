cfc7692a080cf02418abaed071e14ebc2bec65a1
Sylvain Defresne <sdefresne@chromium.org>
2024-12-09 17:20:45 +0000

    [ios] Convert SamplePanelModelFactory to PKSFIOS
    Convert SamplePanelModelFactory to be a ProfileKeydServiceFactoryIOS.
    
    Fixed: 363218193
    Change-Id: Ic6ab35a41c49f2f154b412d137c68d37ca5e6546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080536
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393721}

--- ios/chrome/browser/contextual_panel/sample/model/sample_panel_model_factory.mm
+++ ios/chrome/browser/contextual_panel/sample/model/sample_panel_model_factory.mm
@@ -5,14 +5,13 @@
 #import "ios/chrome/browser/contextual_panel/sample/model/sample_panel_model_factory.h"
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "ios/chrome/browser/contextual_panel/sample/model/sample_panel_model.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 
 // static
 SamplePanelModel* SamplePanelModelFactory::GetForProfile(ProfileIOS* profile) {
-  return static_cast<SamplePanelModel*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<SamplePanelModel>(
+      profile, /*create=*/true);
 }
 
 // static
@@ -22,9 +21,7 @@ SamplePanelModelFactory* SamplePanelModelFactory::GetInstance() {
 }
 
 SamplePanelModelFactory::SamplePanelModelFactory()
-    : BrowserStateKeyedServiceFactory(
-          "SamplePanelModel",
-          BrowserStateDependencyManager::GetInstance()) {}
+    : ProfileKeyedServiceFactoryIOS("SamplePanelModel") {}
 
 SamplePanelModelFactory::~SamplePanelModelFactory() {}
 
