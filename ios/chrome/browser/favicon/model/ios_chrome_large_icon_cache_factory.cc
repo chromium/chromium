9bfcce41ac136e7e9cabe39f91ce77e9cfa3c522
Sylvain Defresne <sdefresne@chromium.org>
2024-12-09 11:25:59 +0000

    [ios] Convert IOSChromeLargeIconCacheFactory to PKSFIOS
    Convert IOSChromeLargeIconCacheFactory to be a
    ProfileKeydServiceFactoryIOS.
    
    Fixed: 363215812
    Change-Id: I2a9f71324689456e66ab99f9bfd1e0115c6c8584
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6074722
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1393584}

--- ios/chrome/browser/favicon/model/ios_chrome_large_icon_cache_factory.cc
+++ ios/chrome/browser/favicon/model/ios_chrome_large_icon_cache_factory.cc
@@ -4,18 +4,15 @@
 
 #include "ios/chrome/browser/favicon/model/ios_chrome_large_icon_cache_factory.h"
 
-#include "base/memory/ptr_util.h"
 #include "base/no_destructor.h"
-#include "components/keyed_service/ios/browser_state_dependency_manager.h"
 #include "ios/chrome/browser/favicon/model/large_icon_cache.h"
-#include "ios/chrome/browser/shared/model/browser_state/browser_state_otr_helper.h"
 #include "ios/chrome/browser/shared/model/profile/profile_ios.h"
 
 // static
 LargeIconCache* IOSChromeLargeIconCacheFactory::GetForProfile(
     ProfileIOS* profile) {
-  return static_cast<LargeIconCache*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
+  return GetInstance()->GetServiceForProfileAs<LargeIconCache>(profile,
+                                                               /*create=*/true);
 }
 
 // static
@@ -25,19 +22,14 @@ IOSChromeLargeIconCacheFactory* IOSChromeLargeIconCacheFactory::GetInstance() {
 }
 
 IOSChromeLargeIconCacheFactory::IOSChromeLargeIconCacheFactory()
-    : BrowserStateKeyedServiceFactory(
-          "LargeIconCache",
-          BrowserStateDependencyManager::GetInstance()) {}
+    : ProfileKeyedServiceFactoryIOS("LargeIconCache",
+                                    ProfileSelection::kOwnInstanceInIncognito) {
+}
 
-IOSChromeLargeIconCacheFactory::~IOSChromeLargeIconCacheFactory() {}
+IOSChromeLargeIconCacheFactory::~IOSChromeLargeIconCacheFactory() = default;
 
 std::unique_ptr<KeyedService>
 IOSChromeLargeIconCacheFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
-  return base::WrapUnique(new LargeIconCache);
-}
-
-web::BrowserState* IOSChromeLargeIconCacheFactory::GetBrowserStateToUse(
-    web::BrowserState* context) const {
-  return GetBrowserStateOwnInstanceInIncognito(context);
+  return std::make_unique<LargeIconCache>();
 }
