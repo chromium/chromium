1fd61a82d90be6e36956b2f6439dc041905f8c54
Sylvain Defresne <sdefresne@chromium.org>
2024-12-09 18:07:24 +0000

    [ios] Convert LanguageModelManagerFactory to PKSFIOS
    Convert LanguageModelManagerFactory to be a ProfileKeydServiceFactoryIOS.
    
    Fixed: 363217487
    Change-Id: I96a1a4bf0707e57cea30cb143ae0a86bbc01b6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080344
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1393753}

--- ios/chrome/browser/language/model/language_model_manager_factory.h
+++ ios/chrome/browser/language/model/language_model_manager_factory.h
@@ -8,10 +8,7 @@
 #import <memory>
 
 #import "base/no_destructor.h"
-#import "components/keyed_service/core/keyed_service.h"
-#import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ProfileIOS;
+#import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
 
 namespace language {
 class LanguageModelManager;
@@ -19,14 +16,10 @@ class LanguageModelManager;
 
 // Manages the language model for each profile. The particular language model
 // provided depends on feature flags.
-class LanguageModelManagerFactory : public BrowserStateKeyedServiceFactory {
+class LanguageModelManagerFactory : public ProfileKeyedServiceFactoryIOS {
  public:
   static language::LanguageModelManager* GetForProfile(ProfileIOS* profile);
   static LanguageModelManagerFactory* GetInstance();
-
-  LanguageModelManagerFactory(const LanguageModelManagerFactory&) = delete;
-  LanguageModelManagerFactory& operator=(const LanguageModelManagerFactory&) =
-      delete;
 
  private:
   friend class base::NoDestructor<LanguageModelManagerFactory>;
@@ -37,8 +30,6 @@ class LanguageModelManagerFactory : public BrowserStateKeyedServiceFactory {
   // BrowserStateKeyedServiceFactory implementation.
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       web::BrowserState* context) const override;
-  web::BrowserState* GetBrowserStateToUse(
-      web::BrowserState* state) const override;
 };
 
 #endif  // IOS_CHROME_BROWSER_LANGUAGE_MODEL_LANGUAGE_MODEL_MANAGER_FACTORY_H_
