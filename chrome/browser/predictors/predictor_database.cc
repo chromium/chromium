dbebab641dca0daf4d7a182176077550f9602d07
Florian Leimgruber <fleimgruber@google.com>
2024-12-10 15:19:03 +0000

    Dangling pointer in TableManager
    Three classes derive from TableManager - and for two of them,
    AutocompleteActionPredictorTable and ResourcePrefetchPredictorTables,
    the TableManager::db_ member becomes dangling. This CL fixes both and
    removes the AcrossTasksDanglingUntriaged annotation from db_.
    
    For both classes, the db_ members points to the
    PredictorDatabaseInternal::db_ unique_ptr. PredictorDatabaseInternal
    further holds scoped_refptrs AutocompleteActionPredictorTable
    autocomplete_table_ and ResourcePrefetchPredictorTables
    resource_prefetch_tables_. The class is their primary owner, who
    initializes the tables with PredictorDatabaseInternal::db_.
    
    In ~PredictorDatabaseInternal(), right before
    PredictorDatabaseInternal::db_ is about to be destroyed, the code
    currently schedules a DeleteSoon() task on the DB sequence*, that will
    asynchronously delete it.
    The code probably expected that by the time this happens,
    autocomplete_table_ and resource_prefetch_tables_ will have been
    destroyed. But since they are scoped_refptrs, this is not necessarily
    the case.
    
    To fix the dangling pointers, this CL posts two more tasks on the DB
    sequence prior to the DeleteSoon(), which reset their TableManager::db_
    using TableManager::ResetDB(). Since the DB sequence is a sequence (see
    http://shortn/_yHccWYJOyX), this guarantees that by the time the
    PredictorDatabaseInternal::db_ is destroyed, the TableManager::db_ won't
    dangle.
    
    * Operations on the TableManager are generally performed on a separate
      DB sequence, http://shortn/_PmndDfUNUy.
    
    Fixed: 41485270
    Change-Id: I64086fafb3b6b47c8605bf890c88c1be2b3e20d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6054349
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394288}

--- chrome/browser/predictors/predictor_database.cc
+++ chrome/browser/predictors/predictor_database.cc
@@ -117,7 +117,13 @@ PredictorDatabaseInternal::~PredictorDatabaseInternal() {
   }
   // The connection pointer needs to be deleted on the DB sequence since there
   // might be a task in progress on the DB sequence which uses this connection.
-  db_task_runner_->DeleteSoon(FROM_HERE, db_.release());
+  db_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(&AutocompleteActionPredictorTable::ResetDB,
+                                autocomplete_table_));
+  db_task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(&ResourcePrefetchPredictorTables::ResetDB,
+                                resource_prefetch_tables_));
+  db_task_runner_->DeleteSoon(FROM_HERE, std::move(db_));
 }
 
 void PredictorDatabaseInternal::Initialize() {
