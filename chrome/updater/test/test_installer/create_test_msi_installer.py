1bf24644919e5ae3354bf5fe1a41c2077782927b
S. Ganesh <ganesh@chromium.org>
2024-12-10 17:56:50 +0000

    updater: Fix IntegrationInstallerResultsTestCases for Google-branded
    Fixed: 383176071
    Change-Id: Ic4f897315ddfcbb4ca2c6027ed83f682067b682e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6083166
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394387}

--- chrome/updater/test/test_installer/create_test_msi_installer.py
+++ chrome/updater/test/test_installer/create_test_msi_installer.py
@@ -66,9 +66,7 @@ class TestInstaller:
         target_wxs = msi_base_file_path + '.wxs'
 
         if sys.platform == 'win32' and os.path.isfile(
-                self._candle_path) and os.path.isfile(self._light_path) and (
-                    not os.path.isfile(target_wxs) or not filecmp.cmp(
-                        self._msi_template_path, target_wxs, shallow=False)):
+                self._candle_path) and os.path.isfile(self._light_path):
             checked_in_dir = os.path.dirname(self._checked_in_msi)
             if not os.path.exists(checked_in_dir):
                 os.makedirs(checked_in_dir)
