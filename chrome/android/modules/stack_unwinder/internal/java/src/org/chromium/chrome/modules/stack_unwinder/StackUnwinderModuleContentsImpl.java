209b457bdf2a3a904453a59c6994507b1cfe37df
Mathijs Affourtit <maffourtit@google.com>
2024-12-11 11:13:08 -0800

    Revert "Reland "Move Libunwindstack initialization out of DFM""
    This reverts commit 85a6a02aca408915d944aea0ec8674c4cc2a0fee.
    
    Reason for revert: Crashes on Canary, b/383487704
    
    Original change's description:
    > Reland "Move Libunwindstack initialization out of DFM"
    >
    > This is a reland of commit d51b5329e39f84aa48e7c62c4635822b855b5622
    >
    > Original change's description:
    > > Move Libunwindstack initialization out of DFM
    > >
    > > Previously Libunwindstack was loaded through the stack_unwinder DFM. However Libunwindstack is sufficiently small that we can load it directly. This enables using Libunwindstack without loading the DFM, which facilitates enabling SSM on Android stable for arm64.
    > >
    > > Change-Id: I0989e3581eb858b6bce15100236f66f3f4b71200
    > > Bug: 375463618
    > > Binary-Size: Expected size increase, due to moving libunwindstack out of DFM into main APK.
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937826
    > > Commit-Queue: Mathijs Affourtit <maffourtit@google.com>
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1393052}
    >
    > Bug: 375463618
    > Binary-Size: Expected size increase, due to moving libunwindstack out of DFM into main APK.
    > Change-Id: Ia23176fac7a273fe1c872dfa3f0a6e78f27fd497
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6080247
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Mathijs Affourtit <maffourtit@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1394445}
    
    Bug: 375463618, 383487704
    Change-Id: If14717bfa0776d849c5fc12447abefd9d22fc11d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089365
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Auto-Submit: Mathijs Affourtit <maffourtit@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1394988}

--- chrome/android/modules/stack_unwinder/internal/java/src/org/chromium/chrome/modules/stack_unwinder/StackUnwinderModuleContentsImpl.java
+++ chrome/android/modules/stack_unwinder/internal/java/src/org/chromium/chrome/modules/stack_unwinder/StackUnwinderModuleContentsImpl.java
@@ -10,14 +10,39 @@ import org.jni_zero.NativeMethods;
  * Provides access to the stack unwinder native code functions within the dynamic feature module.
  */
 public class StackUnwinderModuleContentsImpl implements StackUnwinderModuleContents {
-
+    /**
+     * Returns the pointer to the CreateMemoryRegionsMap native function within the module, encoded
+     * as a long.
+     */
     @Override
-    public long getDoNothingFunction() {
-        return StackUnwinderModuleContentsImplJni.get().getDoNothingFunction();
+    public long getCreateMemoryRegionsMapFunction() {
+        return StackUnwinderModuleContentsImplJni.get().getCreateMemoryRegionsMapFunction();
+    }
+
+    /**
+     * Returns the pointer to the CreateNativeUnwinder native function within the module, encoded as
+     * a long.
+     */
+    @Override
+    public long getCreateNativeUnwinderFunction() {
+        return StackUnwinderModuleContentsImplJni.get().getCreateNativeUnwinderFunction();
+    }
+
+    /**
+     * Returns the pointer to the CreateLibunwindstackUnwinder native function within the module,
+     * encoded as a long.
+     */
+    @Override
+    public long getCreateLibunwindstackUnwinderFunction() {
+        return StackUnwinderModuleContentsImplJni.get().getCreateLibunwindstackUnwinderFunction();
     }
 
     @NativeMethods("stack_unwinder")
     interface Natives {
-        long getDoNothingFunction();
+        long getCreateMemoryRegionsMapFunction();
+
+        long getCreateNativeUnwinderFunction();
+
+        long getCreateLibunwindstackUnwinderFunction();
     }
 }
--- chrome/android/modules/stack_unwinder/internal/stack_unwinder_module_contents_impl.cc
+++ chrome/android/modules/stack_unwinder/internal/stack_unwinder_module_contents_impl.cc
@@ -6,21 +6,60 @@
 #include <type_traits>
 #include <utility>
 
+#include "base/profiler/libunwindstack_unwinder_android.h"
+#include "base/profiler/native_unwinder_android.h"
+#include "base/profiler/native_unwinder_android_memory_regions_map.h"
 #include "chrome/android/features/stack_unwinder/public/function_types.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/android/modules/stack_unwinder/internal/jni_headers/StackUnwinderModuleContentsImpl_jni.h"
 
-void DoNothing() {
-  return;
+std::unique_ptr<base::NativeUnwinderAndroidMemoryRegionsMap>
+CreateMemoryRegionsMap() {
+  return base::NativeUnwinderAndroid::CreateMemoryRegionsMap();
 }
 
-static_assert(std::is_same<stack_unwinder::DoNothingFunction,
-                           decltype(&DoNothing)>::value,
-              "DoNothingFunction typedef must match the declared function "
-              "type");
+static_assert(std::is_same<stack_unwinder::CreateMemoryRegionsMapFunction,
+                           decltype(&CreateMemoryRegionsMap)>::value,
+              "CreateMemoryRegionsMapFunction typedef must match the declared "
+              "function type");
 
-static jlong JNI_StackUnwinderModuleContentsImpl_GetDoNothingFunction(
+std::unique_ptr<base::Unwinder> CreateNativeUnwinder(
+    base::NativeUnwinderAndroidMapDelegate* map_delegate,
+    uintptr_t exclude_module_with_base_address) {
+  return std::make_unique<base::NativeUnwinderAndroid>(
+      exclude_module_with_base_address, map_delegate);
+}
+
+static_assert(std::is_same<stack_unwinder::CreateNativeUnwinderFunction,
+                           decltype(&CreateNativeUnwinder)>::value,
+              "CreateNativeUnwinderFunction typedef must match the declared "
+              "function type");
+
+std::unique_ptr<base::Unwinder> CreateLibunwindstackUnwinder() {
+  return std::make_unique<base::LibunwindstackUnwinderAndroid>();
+}
+
+static_assert(
+    std::is_same<stack_unwinder::CreateLibunwindstackUnwinderFunction,
+                 decltype(&CreateLibunwindstackUnwinder)>::value,
+    "CreateLibunwindstackUnwinderFunction typedef must match the declared "
+    "function type");
+
+static jlong
+JNI_StackUnwinderModuleContentsImpl_GetCreateMemoryRegionsMapFunction(
     JNIEnv* env) {
-  return reinterpret_cast<jlong>(&DoNothing);
+  return reinterpret_cast<jlong>(&CreateMemoryRegionsMap);
+}
+
+static jlong
+JNI_StackUnwinderModuleContentsImpl_GetCreateNativeUnwinderFunction(
+    JNIEnv* env) {
+  return reinterpret_cast<jlong>(&CreateNativeUnwinder);
+}
+
+static jlong
+JNI_StackUnwinderModuleContentsImpl_GetCreateLibunwindstackUnwinderFunction(
+    JNIEnv* env) {
+  return reinterpret_cast<jlong>(&CreateLibunwindstackUnwinder);
 }
