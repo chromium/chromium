2f94ac783daed641b8fdd31e6160f725705c8f75
Nate Fischer <ntfschr@chromium.org>
2024-12-11 08:37:14 +0000

    Gardener: disable PaymentRequest*.testPay
    This disables PaymentRequestNameTest.testPay() and
    PaymentRequestEmailAndPhoneTest.testPay(). This appears to be causing
    some of the later tests in the same batch to fail. I'm going to
    tentatively try disabling only this one test case and hope this improves
    the reliability of the other test cases in the same batch.
    
    Bug: 383399707
    Test: None
    Change-Id: Ifc0d22be943645b58dccdd9cc3f6defa6b461198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6086539
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Owners-Override: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394713}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestEmailAndPhoneTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestEmailAndPhoneTest.java
@@ -4,6 +4,8 @@
 
 package org.chromium.chrome.browser.payments;
 
+import android.os.Build;
+
 import androidx.test.filters.MediumTest;
 
 import org.junit.Assert;
@@ -14,6 +16,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.chrome.browser.autofill.AutofillTestHelper;
@@ -120,6 +123,8 @@ public class PaymentRequestEmailAndPhoneTest {
     @Test
     @MediumTest
     @Feature({"Payments"})
+    @DisableIf.Build(
+            sdk_equals = Build.VERSION_CODES.UPSIDE_DOWN_CAKE) // https://crbug.com/383399707
     public void testPay() throws TimeoutException {
         mPaymentRequestTestRule.runJavaScriptAndWaitForUiEvent(
                 "buy();", mPaymentRequestTestRule.getReadyToPay());
