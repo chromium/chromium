46b45520f27233ba0cb2711a2eba230e93b9ef1d
vikas soni <vikassoni@chromium.org>
2024-12-11 13:13:28 -0800

    [SIPool] Implement unguessable shared image pool id.
    Implement a new class SharedImagePoolId which is an UnguessableToken.
    This is required since pool id will be sent and used across different
    process including renderers, browser and gpu process.
    
    Follow up Cls will convert SharedImagePool to use this new
    SharedImagePoolId.
    
    Bug: 383466770
    Change-Id: Ia748745a59212b8dc68a81b03e834d7b0c8abb71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6089020
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1395061}

---
+++ gpu/command_buffer/common/shared_image_pool_id.cc
@@ -0,0 +1,21 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/command_buffer/common/shared_image_pool_id.h"
+
+namespace gpu {
+
+PoolId::PoolId() = default;
+
+PoolId::PoolId(const base::UnguessableToken& token) : token_(token) {}
+
+PoolId PoolId::Create() {
+  return PoolId(base::UnguessableToken::Create());
+}
+
+std::string PoolId::ToDebugString() const {
+  return token_.ToString();
+}
+
+}  // namespace gpu
