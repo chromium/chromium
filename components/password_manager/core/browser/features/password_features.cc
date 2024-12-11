db2cb24bbc93471751a548418da8d102751049e6
Din Nezametdinov <shaikhitdin@google.com>
2024-12-11 09:13:35 -0800

    [Passwords] Enable new UFF server predictions by default
    Leads agreed with the feature launch.
    Implement step 7 from go/finch-best-practices.
    
    Bug: 40925827
    Change-Id: I76b481a2c6d431747452185dea70e987d6dfdb18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088219
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Din Nezametdinov <shaikhitdin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1394897}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -162,7 +162,7 @@ BASE_FEATURE(kUsernameFirstFlowFallbackCrowdsourcing,
 
 BASE_FEATURE(kUsernameFirstFlowWithIntermediateValuesPredictions,
              "UsernameFirstFlowWithIntermediateValuesPredictions",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUsernameFirstFlowWithIntermediateValuesVoting,
              "UsernameFirstFlowWithIntermediateValuesVoting",
