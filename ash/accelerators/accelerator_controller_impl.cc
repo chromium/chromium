f3ebe7b3c8149c750f9698b344cdb2b01af75ac8
Andy Phan <andyphan@chromium.org>
2024-12-11 01:33:59 +0000

    [CodeHealth] Remove allow_unsafe_buffers from accelerator files
    After https://crrev.com/c/6075565 and https://crrev.com/c/6075544 have
    replaced the C-style arrays with std::array for Ash accelerators, the
    allow_unsafe_buffers pragma can be removed from related files. Remove
    them.
    
    Bug: 40285824
    Change-Id: I2457437c71c48681b153805bb307550ca9f6c2d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6082222
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1394623}

--- ash/accelerators/accelerator_controller_impl.cc
+++ ash/accelerators/accelerator_controller_impl.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "ash/accelerators/accelerator_controller_impl.h"
 
 #include <string>
