c75742beff32cf677d1b36c3d996e2189afc353a
Lauren Winston <lwinston@google.com>
2024-12-11 16:55:51 -0800

    [Read aloud] Use the version from version.json
    Bug: 353972727
    Change-Id: If8ef2c5feb99965cc5572693836a62dfa37cb778
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090093
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Xiang Xiao <xiangxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395188}

--- chrome/browser/resources/tts_engine/background.ts
+++ chrome/browser/resources/tts_engine/background.ts
@@ -10,10 +10,28 @@
 // engine.
 console.info('TTS engine extension installed!');
 
+const DOWNLOAD_BASE_URL = 'https://dl.google.com/android/tts/wasm/';
+
 // This should be incremented each time there's a new upload of the TTS engine.
-const CURRENT_VERSION = 'v1';
-const DOWNLOAD_URL =
-    'https://dl.google.com/android/tts/wasm/' + CURRENT_VERSION;
+let CURRENT_VERSION = 'v1';
+
+fetch(DOWNLOAD_BASE_URL + 'version.json')
+    .then(response => response.json())
+    .then(data => {
+      const version = data.version;
+      if (typeof version === 'string') {
+        // Regex to match a version string like 'v1' or 'v2'.
+        const pattern = /^v\d+$/;
+        if (pattern.test(version)) {
+          // If the version is valid, update the download version.
+          CURRENT_VERSION = version;
+        } else {
+          console.info('Found invalid version in version.json: ' + version);
+        }
+      }
+    });
+
+const DOWNLOAD_URL = DOWNLOAD_BASE_URL + CURRENT_VERSION;
 
 
 // TODO(crbug.com/353972727): Use of remotely fetched code is a temporary
