179b0a8af7ff429d6228731a1907dfdb355a2fbe
François Doray <fdoray@chromium.org>
2024-12-13 12:52:57 -0800

    [perfcombined2024] Enable RunTasksByBatches on Windows by default.
    go/perf-combined-2024-summary
    
    Bug: 40287434
    Change-Id: I99f3b1fc6a453b3e76db2ecd3b88a577044b5fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6093967
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396151}

--- base/task/task_features.cc
+++ base/task/task_features.cc
@@ -59,7 +59,7 @@ BASE_FEATURE(kPumpFastToSleepAndroid,
 
 BASE_FEATURE(kRunTasksByBatches,
              "RunTasksByBatches",
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
              base::FEATURE_ENABLED_BY_DEFAULT);
 #else
              base::FEATURE_DISABLED_BY_DEFAULT);
