d7226ab5330d1dc5f2c0b6af30a9c88c332faace
Alexander Timin <altimin@chromium.org>
2024-12-13 09:57:42 -0800

    [tracing] Optimise `chrome_touch_move_to_scroll_update`.
    Computing chrome_touch_move_to_scroll_update on my local machine and
    trace attached in the bug takes 19s. This patch brings it down to 44ms.
    
    There are two main reasons for bad performance here:
    1. Using ancestor_slice for computing parents is slow (expected).
    2. FULL JOIN is unreasonably slow in sqlite (unexpected, it seems that
       the SQLite ignores the constraint and just executes N^2 join).
    
    This patch fixes both:
    1. Now we leverage _interval_intersect to efficiently intersect two sets
       of intervals and check if one encloses the other.
    2. FULL JOIN is replaced with a UNION + two LEFT JOINs.
    
    Bug: 381882186
    Change-Id: I50c5caba8ef756cbc992b0ee7483cc2808480aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6094111
    Reviewed-by: Petr Cermak <petrcermak@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396031}

--- base/tracing/stdlib/chrome/input.sql
+++ base/tracing/stdlib/chrome/input.sql
@@ -2,8 +2,9 @@
 -- Use of this source code is governed by a BSD-style license that can be
 -- found in the LICENSE file.
 
-INCLUDE PERFETTO MODULE slices.with_context;
 INCLUDE PERFETTO MODULE chrome.android_input;
+INCLUDE PERFETTO MODULE intervals.intersect;
+INCLUDE PERFETTO MODULE slices.with_context;
 
 -- Processing steps of the Chrome input pipeline.
 CREATE PERFETTO TABLE _chrome_input_pipeline_steps_no_input_type(
@@ -122,7 +123,7 @@ CREATE PERFETTO TABLE chrome_touch_move_to_scroll_update(
   scroll_update_latency_id LONG
 ) AS
 WITH
-scroll_update_steps AS (
+scroll_update_steps AS MATERIALIZED (
   SELECT *
   FROM chrome_input_pipeline_steps
   WHERE step = 'STEP_SEND_INPUT_EVENT_UI'
@@ -130,30 +131,69 @@ scroll_update_steps AS (
 ),
 -- By default, we map a scroll update event to an ancestor touch move event with
 -- STEP_TOUCH_EVENT_HANDLED.
-default_mapping AS (
+default_mapping AS MATERIALIZED (
   SELECT
-    touch_move_step.latency_id AS touch_move_latency_id,
-    scroll_update_steps.latency_id AS scroll_update_latency_id
-  FROM scroll_update_steps
-  JOIN ancestor_slice(scroll_update_steps.slice_id) AS ancestor
-  JOIN chrome_input_pipeline_steps AS touch_move_step
-    ON ancestor.id = touch_move_step.slice_id
-  WHERE touch_move_step.step = 'STEP_TOUCH_EVENT_HANDLED'
+    touch_move_handled_step.latency_id AS touch_move_latency_id,
+    scroll_update_step.latency_id AS scroll_update_latency_id
+  -- Performance optimization: we are interested in finding all of the
+  -- scroll_update_steps which have a "touch_move_handled" parent: to do this,
+  -- we intersect them and find all of the intersections which match
+  -- a scroll_update_step.
+  -- We are using `slice_id` as `id` as it's unique and 32-bit (unlike latency_id,
+  -- which can be 64-bit).
+  FROM _interval_intersect!(
+    (
+      (
+        SELECT slice_id AS id, *
+        FROM scroll_update_steps),
+      (
+        SELECT slice_id AS id, *
+        FROM chrome_input_pipeline_steps step
+        WHERE step = 'STEP_TOUCH_EVENT_HANDLED')
+    ),
+    (utid)
+  ) AS ii
+  JOIN scroll_update_steps AS scroll_update_step
+    ON ii.id_0 = scroll_update_step.slice_id
+  JOIN chrome_input_pipeline_steps AS touch_move_handled_step
+    ON ii.id_1 = touch_move_handled_step.slice_id
+  -- If intersection matches the `scroll_update_step`, then it means that
+  -- `touch_move_handled_step` is an ancestor of `scroll_update_step`.
+  WHERE ii.ts = scroll_update_step.ts AND ii.dur = scroll_update_step.dur
 ),
 -- In the rare case where there are no touch handlers in the renderer, there's
 -- no ancestor touch move event with STEP_TOUCH_EVENT_HANDLED. In that case, we
 -- try to fall back to an ancestor touch move event with
 -- STEP_SEND_INPUT_EVENT_UI instead.
-fallback_mapping AS (
+fallback_mapping AS MATERIALIZED (
   SELECT
-    touch_move_step.latency_id AS touch_move_latency_id,
-    scroll_update_steps.latency_id AS scroll_update_latency_id
-  FROM scroll_update_steps
-  JOIN ancestor_slice(scroll_update_steps.slice_id) AS ancestor
-  JOIN chrome_input_pipeline_steps AS touch_move_step
-    ON ancestor.id = touch_move_step.slice_id
-  WHERE touch_move_step.step = 'STEP_SEND_INPUT_EVENT_UI'
-  AND touch_move_step.input_type = 'TOUCH_MOVE_EVENT'
+    send_touch_move_step.latency_id AS touch_move_latency_id,
+    scroll_update_step.latency_id AS scroll_update_latency_id
+  -- See the comment in the default_mapping CTE for an explanation what's going on here.
+  FROM _interval_intersect!(
+    (
+      (
+        SELECT slice_id AS id, *
+        FROM scroll_update_steps),
+      (
+        SELECT slice_id AS id, *
+        FROM chrome_input_pipeline_steps step
+        WHERE step = 'STEP_SEND_INPUT_EVENT_UI' AND input_type = 'TOUCH_MOVE_EVENT')
+    ),
+    (utid)
+  ) AS ii
+  JOIN scroll_update_steps AS scroll_update_step
+    ON ii.id_0 = scroll_update_step.slice_id
+  JOIN chrome_input_pipeline_steps AS send_touch_move_step
+    ON ii.id_1 = send_touch_move_step.slice_id
+  WHERE ii.ts = scroll_update_step.ts AND ii.dur = scroll_update_step.dur
+),
+-- We ideally would want to do a FULL JOIN here, but it is very slow in SQLite,
+-- so instead we are doing UNION + two LEFT JOINs.
+scroll_update_latency_ids AS (
+  SELECT scroll_update_latency_id FROM default_mapping
+  UNION
+  SELECT scroll_update_latency_id FROM fallback_mapping
 )
 SELECT
   COALESCE(
@@ -161,8 +201,9 @@ SELECT
     fallback_mapping.touch_move_latency_id
   ) AS touch_move_latency_id,
   scroll_update_latency_id
-FROM default_mapping
-FULL OUTER JOIN fallback_mapping USING (scroll_update_latency_id);
+FROM scroll_update_latency_ids
+LEFT JOIN default_mapping USING (scroll_update_latency_id)
+LEFT JOIN fallback_mapping USING (scroll_update_latency_id);
 
 -- Matches Android input id to the corresponding touch move event.
 CREATE PERFETTO TABLE chrome_dispatch_android_input_event_to_touch_move(
