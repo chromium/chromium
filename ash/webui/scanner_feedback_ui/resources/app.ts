829d7fc7f0677fa809d40f186dda9ddd075a57b5
mlcui <mlcui@google.com>
2024-12-13 00:18:03 -0800

    scanner: Add <mako-orca-feedback> skeleton to feedback WebUI
    This currently has no strings, but successfully displays without any CSP
    or import errors.
    
    Bug: b:379974095
    Change-Id: I6a6a21003c4d7e0f0ab775bc5c044d5b052d09d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6073206
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1395806}

--- ash/webui/scanner_feedback_ui/resources/app.ts
+++ ash/webui/scanner_feedback_ui/resources/app.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '//resources/cros_components/orca_feedback/orca-feedback.js';
+
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './app.html.js';
