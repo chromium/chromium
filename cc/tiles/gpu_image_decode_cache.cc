72e3ee416f51703a649229ef753b55459d9036e8
Stefan Zager <szager@chromium.org>
2024-12-14 11:25:51 -0800

    [M131] Prevent ImageData from being reclaimed while in use
    Cherry-picked from:
      https://chromium-review.googlesource.com/c/chromium/src/+/5990752
    
    Bug: chromium:368222741
    Change-Id: Idcef7864100ba53143c3c809ae16b5988b8a3dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6094731
    Reviewed-by: Prudhvikumar Bommana <pbommana@google.com>
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/branch-heads/6778@{#3025}
    Cr-Branched-From: b21671ca172dcfd1566d41a770b2808e7fa7cd88-refs/heads/main@{#1368529}

--- cc/tiles/gpu_image_decode_cache.cc
+++ cc/tiles/gpu_image_decode_cache.cc
@@ -2398,6 +2398,9 @@ void GpuImageDecodeCache::DecodeImageIfNecessary(
 
   image_data->decode.ResetData();
 
+  // Prevent image_data from being deleted while lock is not held.
+  scoped_refptr<ImageData> image_data_holder(image_data);
+
   // Decode the image into `aux_image_data` while the lock is not held.
   DecodedAuxImageData aux_image_data[kAuxImageCount];
   {
@@ -2725,6 +2728,9 @@ void GpuImageDecodeCache::UploadImageIfNecessary_GpuCpu_YUVA(
   sk_sp<SkImage> uploaded_v_image =
       image_data->decode.image(2, AuxImage::kDefault);
 
+  // Prevent image_data from being deleted while lock is not held.
+  scoped_refptr<ImageData> image_data_holder(image_data);
+
   // For kGpu, we upload and color convert (if necessary).
   if (image_data->mode == DecodedDataMode::kGpu) {
     DCHECK(!use_transfer_cache_);
@@ -2811,6 +2817,9 @@ void GpuImageDecodeCache::UploadImageIfNecessary_GpuCpu_RGBA(
     sk_sp<SkColorSpace> color_space) {
   DCHECK(!use_transfer_cache_);
   DCHECK(!image_data->info.yuva.has_value());
+
+  // Prevent image_data from being deleted while lock is not held.
+  scoped_refptr<ImageData> image_data_holder(image_data);
 
   // RGBX decoding is below.
   // For kGpu, we upload and color convert (if necessary).
