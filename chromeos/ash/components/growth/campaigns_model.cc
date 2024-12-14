dd06ac1cc118d79cdcfa749479ecbd9ceddc265b
Yuki Awano <yawano@google.com>
2024-12-13 16:33:37 -0800

    growth: add mall app icon
    - This CL changes the function to return a nullopt with an error log for
      an invalid enum value. Not having a return statement after switch
      statement without default case can lead to an undefined behavior.
    
    Bug: b:378757116
    Fixed: b:378757116
    Test: na
    Change-Id: I2a784be928b7aeda896079bac4eee3853e899693
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6094503
    Commit-Queue: Yuki Awano <yawano@google.com>
    Reviewed-by: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396266}

--- chromeos/ash/components/growth/campaigns_model.cc
+++ chromeos/ash/components/growth/campaigns_model.cc
@@ -14,11 +14,13 @@
 
 #include "ash/constants/ash_features.h"
 #include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/webui/grit/ash_mall_cros_app_resources.h"
 #include "ash/webui/grit/ash_print_management_resources.h"
 #include "base/logging.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
+#include "base/strings/to_string.h"
 #include "base/time/time.h"
 #include "base/version.h"
 #include "build/branding_buildflags.h"
@@ -246,7 +248,11 @@ std::optional<int> GetBuiltInImageResourceId(
     case BuiltInImage::kPlayStoreIcon:
       return IDR_PRODUCT_LOGO_128;
 #endif  // BUILDFLAG(ENABLE_CROS_SCALABLE_IPH)
+    case growth::BuiltInImage::kMallAppIcon:
+      return IDR_ASH_MALL_CROS_APP_IMAGES_MALL_ICON_192_PNG;
   }
+
+  return std::nullopt;
 }
 
 std::optional<BuiltInImage> GetBuiltInImageType(
