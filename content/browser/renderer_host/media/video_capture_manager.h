3524ce528548d1d743a6aa6e339ecb5a186c22bc
Guido Urdaneta <guidou@chromium.org>
2024-12-13 18:17:54 -0800

    [VideoCaptureManager] Replace raw pointers with scoped_refptr
    VCM used VideoCaptureController raw pointers in a number of places,
    including as a field in VCM::CaptureDeviceStartRequest.
    
    This CL replaces the field and some other usages with a scoped_refptr
    to prevent dangling pointers.
    
    Bug: 382135228
    Change-Id: I1bd5f95bdf57631227034beb8bb076f258606378
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088073
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396301}

--- content/browser/renderer_host/media/video_capture_manager.h
+++ content/browser/renderer_host/media/video_capture_manager.h
@@ -308,7 +308,7 @@ class CONTENT_EXPORT VideoCaptureManager
   // Finds a VideoCaptureController for the indicated |capture_session_id|,
   // creating a fresh one if necessary. Returns nullptr if said
   // |capture_session_id| is invalid.
-  VideoCaptureController* GetOrCreateController(
+  scoped_refptr<VideoCaptureController> GetOrCreateController(
       const media::VideoCaptureSessionId& capture_session_id,
       const media::VideoCaptureParams& params);
 
@@ -320,7 +320,7 @@ class CONTENT_EXPORT VideoCaptureManager
   // another request pending start.
   void QueueStartDevice(
       const media::VideoCaptureSessionId& session_id,
-      VideoCaptureController* controller,
+      scoped_refptr<VideoCaptureController> controller,
       const media::VideoCaptureParams& params,
       mojo::PendingRemote<video_effects::mojom::VideoEffectsProcessor>
           video_effects_processor);
