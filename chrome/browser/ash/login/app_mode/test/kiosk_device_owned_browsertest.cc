bd825f8b0368ad8c86bc2a0ebb700b8ede70e84f
Edman Anjos <edmanp@google.com>
2024-12-16 08:19:04 -0800

    kiosk: Extract KioskSettingsTest from KioskDeviceOwnedTest
    Here are the tests extracted:
    
    KioskDeviceOwnedTest, SettingsWindow
      ==> KioskSettingsTest, CanNavigateToSettingsUrl
      ==> KioskSettingsTest, CanNavigateToSettingsSubUrl
      ==> KioskSettingsTest, CannotNavigateToNonSettingsUrl
      ==> KioskSettingsTest, CannotNavigateToDisallowedSubUrl
      ==> KioskSettingsTest, DoesNotOpenTwoSettingsBrowsers
      ==> KioskSettingsTest,
          NavigatingToSettingsInAppCreatesNewBrowser
      ==> KioskSettingsTest,
          SettingsBrowserIsNullWhenSettingsIsClosed
    
    KioskDeviceOwnedTest, SettingsWindowShouldBeActive
      ==> KioskSettingsTest, CanRefocusSettings
    
    KioskDeviceOwnedTest, SettingsWindowRemainsOpen
    KioskDeviceOwnedTest, CloseSettingsWindow
      ==> deleted in favor of pre-existing tests:
          KioskTest, ExitsIfOnlySettingsWindowRemainsOpen
          KioskTest, DoesNotExitWhenSettingsWindowCloses
    
    Bug: 379246208
    Test: testing/xvfb.py tools/autotest.py -C out/Default chrome/browser/ash/app_mode/test/kiosk_settings_browsertest.cc chrome/browser/ash/login/app_mode/test/kiosk_device_owned_browsertest.cc
    Change-Id: I9fd5212298268700906d372cd7204144fd19bcef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088910
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396768}

--- chrome/browser/ash/login/app_mode/test/kiosk_device_owned_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_device_owned_browsertest.cc
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 #include <string>
-#include <vector>
 
 #include "base/check_deref.h"
 #include "base/functional/bind.h"
@@ -12,7 +11,6 @@
 #include "chrome/browser/ash/accessibility/speech_monitor.h"
 #include "chrome/browser/ash/app_mode/kiosk_chrome_app_manager.h"
 #include "chrome/browser/ash/app_mode/kiosk_controller.h"
-#include "chrome/browser/ash/app_mode/kiosk_system_session.h"
 #include "chrome/browser/ash/login/app_mode/network_ui_controller.h"
 #include "chrome/browser/ash/login/app_mode/test/kiosk_base_test.h"
 #include "chrome/browser/ash/login/test/device_state_mixin.h"
@@ -21,18 +19,15 @@
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
 #include "chrome/browser/ash/login/test/test_predicate_waiter.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
-#include "chrome/browser/chromeos/app_mode/kiosk_settings_navigation_throttle.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/ash/login/login_display_host.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
-#include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
-#include "chrome/browser/ui/test/test_browser_closed_waiter.h"
 #include "chrome/browser/ui/webui/ash/login/app_launch_splash_screen_handler.h"
 #include "chrome/grit/generated_resources.h"
 #include "chromeos/ash/components/settings/cros_settings_names.h"
@@ -42,7 +37,6 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
-#include "content/public/test/test_navigation_observer.h"
 #include "extensions/browser/app_window/app_window.h"
 #include "extensions/browser/app_window/app_window_registry.h"
 #include "extensions/browser/browsertest_util.h"
@@ -54,8 +48,6 @@
 #include "extensions/test/result_catcher.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/page_transition_types.h"
-#include "ui/base/window_open_disposition.h"
 
 namespace ash {
 
@@ -63,24 +55,6 @@ namespace {
 
 const test::UIPath kSplashScreenLaunchText = {"app-launch-splash",
                                               "launchText"};
-
-constexpr char kSettingsPage1[] = "chrome://os-settings/manageAccessibility";
-constexpr char kSettingsPage2[] =
-    "chrome-extension://mndnfokpggljbaajbnioimlmbfngpief/chromevox/options/"
-    "options.html";
-
-NavigateParams OpenBrowserWithUrl(
-    const std::string& url,
-    WindowOpenDisposition window_type = WindowOpenDisposition::CURRENT_TAB) {
-  Profile* profile = ProfileManager::GetPrimaryUserProfile();
-
-  NavigateParams params(profile, GURL(url), ui::PAGE_TRANSITION_AUTO_BOOKMARK);
-  params.disposition = window_type;
-  params.window_action = NavigateParams::SHOW_WINDOW;
-  Navigate(&params);
-
-  return params;
-}
 
 void WaitForNetworkTimeoutMessage() {
   test::TestPredicateWaiter(base::BindRepeating([]() {
@@ -90,21 +64,6 @@ void WaitForNetworkTimeoutMessage() {
            l10n_util::GetStringUTF8(IDS_APP_START_NETWORK_WAIT_TIMEOUT_MESSAGE);
   })).Wait();
 }
-
-// Helper class to replace settings urls for KioskSettingsNavigationThrottle.
-class ScopedSettingsPages {
- public:
-  explicit ScopedSettingsPages(
-      std::vector<chromeos::KioskSettingsNavigationThrottle::SettingsPage>*
-          pages) {
-    chromeos::KioskSettingsNavigationThrottle::SetSettingPagesForTesting(pages);
-  }
-
-  ~ScopedSettingsPages() {
-    chromeos::KioskSettingsNavigationThrottle::SetSettingPagesForTesting(
-        nullptr);
-  }
-};
 
 // Helper class to count how many times an extension has been loaded.
 class ExtensionReadyObserver : public extensions::ExtensionRegistryObserver {
@@ -183,185 +142,6 @@ IN_PROC_BROWSER_TEST_F(KioskDeviceOwnedTest,
   // Network becomes online and app launch is resumed.
   SimulateNetworkOnline();
   WaitForAppLaunchSuccess();
-}
-
-IN_PROC_BROWSER_TEST_F(KioskDeviceOwnedTest, SettingsWindow) {
-  StartAppLaunchFromLoginScreen(NetworkStatus::kOnline);
-  WaitForAppLaunchWithOptions(/*check_launch_data=*/true,
-                              /*terminate_app=*/false,
-                              /*keep_app_open=*/true);
-
-  // At this moment, app session should be initialized.
-  std::vector<chromeos::KioskSettingsNavigationThrottle::SettingsPage>
-      settings_pages = {
-          {"https://page1.com/", /*allow_subpages*/ true},
-          {"https://page2.com/", /*allow_subpages*/ false},
-      };
-
-  const GURL page1("https://page1.com/");
-  const GURL page1_sub("https://page1.com/sub");
-  const GURL page2("https://page2.com/");
-  const GURL page2_sub("https://page2.com/sub");
-  const GURL page3("https://page3.com/");
-
-  // Replace the settings allowlist with `settings_pages`.
-  ScopedSettingsPages pages(&settings_pages);
-  KioskSystemSession* system_session =
-      KioskController::Get().GetKioskSystemSession();
-
-  // App session should be initialized.
-  ASSERT_TRUE(system_session);
-  ASSERT_FALSE(system_session->GetSettingsBrowserForTesting());
-
-  OpenBrowserWithUrl(page1.spec(), WindowOpenDisposition::NEW_POPUP);
-  EXPECT_FALSE(DidSessionCloseNewWindow(system_session));
-
-  Browser* settings_browser = system_session->GetSettingsBrowserForTesting();
-  ASSERT_TRUE(settings_browser);
-
-  content::WebContents* web_contents =
-      settings_browser->tab_strip_model()->GetActiveWebContents();
-  // Try navigating to an allowed subpage.
-  NavigateToURLBlockUntilNavigationsComplete(web_contents, page1_sub, 1);
-  EXPECT_EQ(web_contents->GetLastCommittedURL(), page1_sub);
-
-  // Open another browser with url page2.
-  // Also, expect navigation inside of the old window to page2.
-  content::TestNavigationObserver settings_navigation_observer(web_contents, 1);
-  OpenBrowserWithUrl(page2.spec(), WindowOpenDisposition::NEW_POPUP);
-  EXPECT_FALSE(DidSessionCloseNewWindow(system_session));
-  // Also wait for navigaiton to finish.
-  settings_navigation_observer.Wait();
-
-  // The settings browser should not have changed.
-  ASSERT_EQ(settings_browser, system_session->GetSettingsBrowserForTesting());
-  EXPECT_EQ(web_contents->GetLastCommittedURL(), page2);
-
-  // Try navigating to a disallowed subpage (this won't commit the navigation).
-  NavigateToURLBlockUntilNavigationsComplete(
-      web_contents, page2_sub, 1,
-      /*ignore_uncommitted_navigations=*/false);
-  EXPECT_EQ(web_contents->GetLastCommittedURL(), page2);
-
-  // Try navigating to a disallowed page (this won't commit the navigation).
-  NavigateToURLBlockUntilNavigationsComplete(
-      web_contents, page3, 1,
-      /*ignore_uncommitted_navigations=*/false);
-  EXPECT_EQ(web_contents->GetLastCommittedURL(), page2);
-
-  // Close settings browser, expect the value to be cleared.
-  CloseBrowserSynchronously(settings_browser);
-  EXPECT_FALSE(system_session->GetSettingsBrowserForTesting());
-
-  // Open another browser with url page2, but now of the default type.
-  // This should create a new browser of app type, and close the non-app one.
-  NavigateParams params = OpenBrowserWithUrl(page2.spec());
-  // Wait for two browser handlings -- for non-app and app browser.
-  EXPECT_FALSE(DidSessionCloseNewWindow(system_session));
-  EXPECT_FALSE(DidSessionCloseNewWindow(system_session));
-
-  // One browser should be created.
-  settings_browser = system_session->GetSettingsBrowserForTesting();
-  ASSERT_TRUE(settings_browser);
-  EXPECT_FALSE(params.browser == settings_browser);
-}
-
-// This test covers b/245088137: after opening the settings browser and moving
-// focus to the main kiosk app, the settings browser could not be opened again.
-IN_PROC_BROWSER_TEST_F(KioskDeviceOwnedTest, SettingsWindowShouldBeActive) {
-  StartAppLaunchFromLoginScreen(NetworkStatus::kOnline);
-  WaitForAppLaunchWithOptions(/*check_launch_data=*/true,
-                              /*terminate_app=*/false,
-                              /*keep_app_open=*/true);
-  KioskSystemSession* system_session =
-      KioskController::Get().GetKioskSystemSession();
-
-  // App session should be initialized.
-  ASSERT_TRUE(system_session);
-  ASSERT_FALSE(system_session->GetSettingsBrowserForTesting());
-
-  OpenBrowserWithUrl(kSettingsPage1, WindowOpenDisposition::NEW_POPUP);
-  EXPECT_FALSE(DidSessionCloseNewWindow(system_session));
-
-  Browser* settings_browser = system_session->GetSettingsBrowserForTesting();
-
-  // Make sure the settings browser was opened, and it is focused.
-  ASSERT_TRUE(settings_browser);
-  EXPECT_TRUE(settings_browser->window()->IsActive());
-
-  // Emulate focus switching.
-  settings_browser->window()->Deactivate();
-  EXPECT_FALSE(settings_browser->window()->IsActive());
-
-  content::WebContents* web_contents =
-      settings_browser->tab_strip_model()->GetActiveWebContents();
-
-  // Open another settings browser.
-  // Also, expect navigation inside of the old window to kSettingsPage2.
-  content::TestNavigationObserver settings_navigation_observer(web_contents, 1);
-  OpenBrowserWithUrl(kSettingsPage2, WindowOpenDisposition::NEW_POPUP);
-  EXPECT_FALSE(DidSessionCloseNewWindow(system_session));
-  // Also wait for navigaiton to finish.
-  settings_navigation_observer.Wait();
-
-  // The settings browser should not have changed.
-  ASSERT_EQ(settings_browser, system_session->GetSettingsBrowserForTesting());
-  EXPECT_EQ(web_contents->GetLastCommittedURL(), GURL(kSettingsPage2));
-
-  // The settings browser should be focused again.
-  EXPECT_TRUE(settings_browser->window()->IsActive());
-}
-
-// If only the a11y settings window remains open, it should not be automatically
-// closed in the chrome app kiosk session.
-IN_PROC_BROWSER_TEST_F(KioskDeviceOwnedTest, SettingsWindowRemainsOpen) {
-  StartAppLaunchFromLoginScreen(NetworkStatus::kOnline);
-  WaitForAppLaunchWithOptions(/*check_launch_data=*/true,
-                              /*terminate_app=*/false,
-                              /*keep_app_open=*/true);
-  KioskSystemSession* system_session =
-      KioskController::Get().GetKioskSystemSession();
-  // App session should be initialized.
-  ASSERT_NE(system_session, nullptr);
-
-  OpenA11ySettingsBrowser(system_session);
-  Browser* settings_browser = system_session->GetSettingsBrowserForTesting();
-  ASSERT_NE(settings_browser, nullptr);
-
-  // Only the settings browser is opened.
-  EXPECT_EQ(BrowserList::GetInstance()->size(), 1u);
-  EXPECT_FALSE(system_session->is_shutting_down());
-}
-
-// Closing the a11y settings window should not exit the chrome app kiosk
-// session.
-IN_PROC_BROWSER_TEST_F(KioskDeviceOwnedTest, CloseSettingsWindow) {
-  StartAppLaunchFromLoginScreen(NetworkStatus::kOnline);
-  WaitForAppLaunchWithOptions(/*check_launch_data=*/true,
-                              /*terminate_app=*/false,
-                              /*keep_app_open=*/true);
-  KioskSystemSession* system_session =
-      KioskController::Get().GetKioskSystemSession();
-  // App session should be initialized.
-  ASSERT_NE(system_session, nullptr);
-
-  OpenA11ySettingsBrowser(system_session);
-  Browser* settings_browser = system_session->GetSettingsBrowserForTesting();
-  ASSERT_NE(settings_browser, nullptr);
-  ASSERT_NE(settings_browser->window(), nullptr);
-
-  // Only the settings browser is opened.
-  EXPECT_EQ(BrowserList::GetInstance()->size(), 1u);
-  EXPECT_FALSE(system_session->is_shutting_down());
-
-  settings_browser->window()->Close();
-  // Ensure `settings_browser` is closed.
-  TestBrowserClosedWaiter browser_closed_waiter{settings_browser};
-  ASSERT_TRUE(browser_closed_waiter.WaitUntilClosed());
-
-  // No browsers are opened in the chrome app kiosk session.
-  EXPECT_EQ(BrowserList::GetInstance()->size(), 0u);
-  EXPECT_FALSE(system_session->is_shutting_down());
 }
 
 // Verifies that an enterprise device does not auto-launch kiosk mode when cros
