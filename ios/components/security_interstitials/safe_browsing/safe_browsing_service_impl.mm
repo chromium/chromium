9c43c2b681d7b52fbfbcf49f0433002df7f02062
Daniel Rubery <drubery@chromium.org>
2024-12-16 11:48:18 -0800

    Re-enable logging of SafeBrowsing.AuthenticatedCookieResetEndpoint on iOS
    This makes use of the trusted_params, which should be acceptable because
    the Safe Browsing network context is only used for browser-generated
    requests to Safe Browsing. But since is_trusted was not set in the
    network context creation, this logging was causing network requests to
    fail.
    
    Bug: 382303498
    Change-Id: I66de28903e8042c625fabec7cfe78e3dfb908bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6092195
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396891}

--- ios/components/security_interstitials/safe_browsing/safe_browsing_service_impl.mm
+++ ios/components/security_interstitials/safe_browsing/safe_browsing_service_impl.mm
@@ -81,6 +81,7 @@ void SafeBrowsingServiceImpl::Initialize(const base::FilePath& user_data_path) {
       network::mojom::URLLoaderFactoryParams::New();
   url_loader_factory_params->process_id = network::mojom::kBrowserProcessId;
   url_loader_factory_params->is_orb_enabled = false;
+  url_loader_factory_params->is_trusted = true;
   network_context_client_->CreateURLLoaderFactory(
       std::move(url_loader_factory_pending_receiver_),
       std::move(url_loader_factory_params));
