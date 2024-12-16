0c3594dc869c5e55168f6693615370b545906afa
Benoît Lizé <lizeb@chromium.org>
2024-12-16 10:41:04 -0800

    [android] Add a feature to lower thread priority in ETC1 compression
    ETC1 compression can be very expensive, and typically runs at default
    priority, potentially starving other, more important threads. This CL
    adds a feature to lower the thread priority during compression, and uses
    it at its callsites.
    
    Since whether to use it or not depends on the callsite, the feature is
    added in common code, but used at the callsites. The mechanism is in
    ui_resource_provider.cc to avoid code duplication.
    
    In local testing, this results in the big core to no longer be tied by
    the compression task, which then takes longer, showing that higher
    priority activities were competing with it.
    
    Change-Id: I8097a263d31c9b0a9d6c697702204fbdeecd7f24
    Bug: 369986099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6094143
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1396839}

--- chrome/browser/thumbnail/cc/etc1_thumbnail_helper.cc
+++ chrome/browser/thumbnail/cc/etc1_thumbnail_helper.cc
@@ -12,6 +12,7 @@
 #include <array>
 
 #include "base/containers/span.h"
+#include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/numerics/byte_conversions.h"
 #include "base/numerics/safe_conversions.h"
@@ -251,8 +252,16 @@ void CompressTask(SkBitmap raw_data,
                   bool supports_etc_non_power_of_two,
                   base::OnceCallback<void(sk_sp<SkPixelRef>, const gfx::Size&)>
                       post_compression_task) {
-  sk_sp<SkPixelRef> compressed_data = ui::UIResourceProvider::CompressBitmap(
-      raw_data, supports_etc_non_power_of_two);
+  sk_sp<SkPixelRef> compressed_data = nullptr;
+
+  if (base::FeatureList::IsEnabled(ui::kCompressBitmapAtBackgroundPriority)) {
+    compressed_data =
+        ui::UIResourceProvider::CompressBitmapAtBackgroundPriority(
+            raw_data, supports_etc_non_power_of_two);
+  } else {
+    compressed_data = ui::UIResourceProvider::CompressBitmap(
+        raw_data, supports_etc_non_power_of_two);
+  }
   gfx::Size content_size = compressed_data
                                ? gfx::Size(raw_data.width(), raw_data.height())
                                : gfx::Size();
