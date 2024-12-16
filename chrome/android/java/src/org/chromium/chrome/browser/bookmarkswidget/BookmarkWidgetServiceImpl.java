2a2f4636789f48a2e9be80ba26d8e591e8317094
Brandon Wylie <wylieb@chromium.org>
2024-12-16 15:07:40 -0800

    [bookmarks] Supply the PartnerBookmarkIterator to bookmark widget
    Bug: 384197258
    Change-Id: I1f6485d12189b00ee841d12eb5224380cec51033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6098319
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1397031}

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarkswidget/BookmarkWidgetServiceImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarkswidget/BookmarkWidgetServiceImpl.java
@@ -29,6 +29,7 @@ import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.bookmarks.BookmarkModel;
 import org.chromium.chrome.browser.bookmarks.BookmarkModelObserver;
@@ -181,6 +182,8 @@ public class BookmarkWidgetServiceImpl extends BookmarkWidgetService.Impl {
             mRemainingTaskCount = 1;
             mBookmarkModel =
                     BookmarkModel.getForProfile(ProfileManager.getLastUsedRegularProfile());
+            mBookmarkModel.setPartnerBookmarkIteratorSupplier(
+                    () -> AppHooks.get().getPartnerBookmarkIterator());
             mBookmarkModel.finishLoadingBookmarkModel(
                     new Runnable() {
                         @Override
