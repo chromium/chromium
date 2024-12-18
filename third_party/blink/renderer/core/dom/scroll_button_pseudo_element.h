52658a3e5092c402f62ef524b1831155fe317e4f
Daniil Sakhapov <sakhapov@chromium.org>
2024-12-18 11:49:08 -0800

    [carousel] Improve scroll buttons scrolling code
    This CL fixes the problem that zoom affects the scroll amount by reusing
    scrolling logic from scroll manager code that is also used for page up
    and page down buttons handling.
    
    Fixed: 383552806
    Change-Id: I581c91b99acf78159728b03e7f3173c6d1686a50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6097334
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Auto-Submit: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398125}

--- third_party/blink/renderer/core/dom/scroll_button_pseudo_element.h
+++ third_party/blink/renderer/core/dom/scroll_button_pseudo_element.h
@@ -6,6 +6,7 @@
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_SCROLL_BUTTON_PSEUDO_ELEMENT_H_
 
 #include "third_party/blink/renderer/core/dom/pseudo_element.h"
+#include "third_party/blink/renderer/core/input/scroll_manager.h"
 #include "third_party/blink/renderer/core/scroll/scroll_snapshot_client.h"
 
 namespace blink {
@@ -36,9 +37,7 @@ class ScrollButtonPseudoElement : public PseudoElement,
   bool UpdateSnapshotInternal();
 
   bool enabled_ = true;
-  // As per https://drafts.csswg.org/css-overflow-5/#scroll-buttons
-  // defines a "page" size to be 85% of the scrollport size.
-  static constexpr double PageSizePercent = 0.85;
+  Member<ScrollManager> scroll_manager_;
 };
 
 template <>
