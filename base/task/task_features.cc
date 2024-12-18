49a2b6809390b691cd46cd4f9e95aca1bd40b043
François Doray <fdoray@chromium.org>
2024-12-18 11:44:03 -0800

    [perfcombined2024] Enable PumpFastToSleepAndroid by default.
    go/perf-combined-2024-summary
    
    Bug: 40287434
    Change-Id: I99ba5519fe62c67442de499a47166f1b770cf2f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6090909
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398118}

--- base/task/task_features.cc
+++ base/task/task_features.cc
@@ -55,7 +55,7 @@ BASE_FEATURE(kUIPumpImprovementsWin,
 
 BASE_FEATURE(kPumpFastToSleepAndroid,
              "PumpFastToSleepAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kRunTasksByBatches,
              "RunTasksByBatches",
