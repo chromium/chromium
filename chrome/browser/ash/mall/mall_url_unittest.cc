1025bfa2a6b5fa742982937e99f4e38e021d9d6f
Alan Cutter <alancutter@chromium.org>
2024-12-17 21:47:10 -0800

    Reland "Add origin parameter to chrome://mall iframe"
    This is a reland of commit ba6bfc4f538e3b1f5a965580ac0b062ca1856064
    
    Fixed incorrect expectations and attempted to fix test flakiness that
    caused the original CL to have its try failures exonerated:
    https://chromium-review.googlesource.com/c/chromium/src/+/6099098/1..2
    
    Original change's description:
    > Add origin parameter to chrome://mall iframe
    >
    > This CL passes the the parent origin chrome://mall to its iframe via
    > an origin URL param.
    > This enables the site to allow iframing for chrome://mall.
    >
    > Bug: b:384363138
    > Change-Id: Ifba96fb81a95b0b04ea578005fdb2a92aeec64df
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099090
    > Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    > Commit-Queue: Alan Cutter <alancutter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1397635}
    
    Bug: b:384363138
    Change-Id: I83c0b089cbce50f09be0c26e4d543055a6623105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099098
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1397736}

--- chrome/browser/ash/mall/mall_url_unittest.cc
+++ chrome/browser/ash/mall/mall_url_unittest.cc
@@ -45,12 +45,14 @@ TEST_F(MallUrlTest, GetMallLaunchUrl) {
   ASSERT_EQ(launch_url.host(), GURL(chromeos::kAppMallBaseUrl).host());
 
   std::vector<std::string> query_parts = base::SplitString(
-      launch_url.query(), "=", base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL);
+      launch_url.query(), "=&", base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL);
 
-  ASSERT_EQ(query_parts[0], "context");
+  ASSERT_EQ(query_parts[0], "origin");
+  ASSERT_EQ(query_parts[1], "chrome%3A%2F%2Fmall");
+  ASSERT_EQ(query_parts[2], "context");
 
   std::string proto_string;
-  ASSERT_TRUE(base::Base64Decode(net::UnescapePercentEncodedUrl(query_parts[1]),
+  ASSERT_TRUE(base::Base64Decode(net::UnescapePercentEncodedUrl(query_parts[3]),
                                  &proto_string,
                                  base::Base64DecodePolicy::kStrict));
 
@@ -61,14 +63,16 @@ TEST_F(MallUrlTest, GetMallLaunchUrl) {
 }
 
 TEST_F(MallUrlTest, GetMallLaunchUrl_BasePath) {
-  EXPECT_THAT(GetMallLaunchUrl(apps::DeviceInfo(), "/").spec(),
-              testing::StartsWith("https://discover.apps.chrome/?context="));
+  EXPECT_THAT(
+      GetMallLaunchUrl(apps::DeviceInfo(), "/").spec(),
+      testing::StartsWith(
+          "https://discover.apps.chrome/?origin=chrome%3A%2F%2Fmall&context="));
 }
 
 TEST_F(MallUrlTest, GetMallLaunchUrl_SimplePath) {
-  EXPECT_THAT(
-      GetMallLaunchUrl(apps::DeviceInfo(), "/apps/").spec(),
-      testing::StartsWith("https://discover.apps.chrome/apps/?context="));
+  EXPECT_THAT(GetMallLaunchUrl(apps::DeviceInfo(), "/apps/").spec(),
+              testing::StartsWith("https://discover.apps.chrome/apps/"
+                                  "?origin=chrome%3A%2F%2Fmall&context="));
 }
 
 TEST_F(MallUrlTest, GetMallLaunchUrl_PathWithQuery) {
@@ -76,19 +80,20 @@ TEST_F(MallUrlTest, GetMallLaunchUrl_PathWithQuery) {
   // query parameters.
   EXPECT_THAT(GetMallLaunchUrl(apps::DeviceInfo(), "/search?q=netflix").spec(),
               testing::StartsWith(
-                  "https://discover.apps.chrome/search%3Fq=netflix?context="));
+                  "https://discover.apps.chrome/"
+                  "search%3Fq=netflix?origin=chrome%3A%2F%2Fmall&context="));
 }
 
 TEST_F(MallUrlTest, GetMallLaunchUrl_RelativePath) {
-  EXPECT_THAT(
-      GetMallLaunchUrl(apps::DeviceInfo(), "../apps/").spec(),
-      testing::StartsWith("https://discover.apps.chrome/apps/?context="));
+  EXPECT_THAT(GetMallLaunchUrl(apps::DeviceInfo(), "../apps/").spec(),
+              testing::StartsWith("https://discover.apps.chrome/apps/"
+                                  "?origin=chrome%3A%2F%2Fmall&context="));
 }
 
 TEST_F(MallUrlTest, GetMallLaunchUrl_RelativePathHost) {
   EXPECT_THAT(GetMallLaunchUrl(apps::DeviceInfo(), "//example.com/").spec(),
-              testing::StartsWith(
-                  "https://discover.apps.chrome//example.com/?context="));
+              testing::StartsWith("https://discover.apps.chrome//example.com/"
+                                  "?origin=chrome%3A%2F%2Fmall&context="));
 }
 
 }  // namespace ash
