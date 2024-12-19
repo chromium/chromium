3cf439db3048cd26b6aae2e2130a52a1236713f8
Mingyu Lei <leimy@chromium.org>
2024-12-18 17:46:21 -0800

    Prompt API: fix non-streaming prompt API
    crrev.com/c/6074052 only fixed the streaming API after updating the
    streaming behavior, this CL fixes the non-streaming one as well.
    
    Bug: 353869537
    Change-Id: Ia90330559b2bbae1413420fc576a5709ef081cec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6104114
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398357}

--- third_party/blink/renderer/modules/ai/model_execution_responder.cc
+++ third_party/blink/renderer/modules/ai/model_execution_responder.cc
@@ -82,7 +82,7 @@ class Responder final : public GarbageCollected<Responder>,
         mojom::blink::ModelStreamingResponseStatus::kOngoing);
     response_callback_count_++;
     // Update the response with the latest value.
-    response_ = text;
+    response_ = response_ + text;
   }
 
   void OnCompletion(
