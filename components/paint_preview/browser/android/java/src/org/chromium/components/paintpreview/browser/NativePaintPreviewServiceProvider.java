5fa2ede0b91ce1fba1aebb4df87c45b7a787ced3
Sam Maier <smaier@chromium.org>
2024-12-19 10:00:37 -0800

    Android: adding nullability annotations to paint_preview and zoom
    Bug: 40657579
    Change-Id: I2281b67c345af4214afa23276feb60a06d6ceec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6110194
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398679}

--- components/paint_preview/browser/android/java/src/org/chromium/components/paintpreview/browser/NativePaintPreviewServiceProvider.java
+++ components/paint_preview/browser/android/java/src/org/chromium/components/paintpreview/browser/NativePaintPreviewServiceProvider.java
@@ -4,10 +4,13 @@
 
 package org.chromium.components.paintpreview.browser;
 
+import org.chromium.build.annotations.NullMarked;
+
 /**
  * The Java-side implementations of paint_preview_base_service.cc should implement this interface.
  * Provides a method for accessing the native PaintPreviewBaseService.
  */
+@NullMarked
 public interface NativePaintPreviewServiceProvider {
     long getNativeBaseService();
 }
