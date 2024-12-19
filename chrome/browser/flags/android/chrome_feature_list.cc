1b1d201b8b5d2d3573ee81fe6b80cfec36b99ce2
Salvador Guerrero <salg@google.com>
2024-12-19 13:31:56 -0800

    Enabled android page content provider
    Bug: 382124748
    Change-Id: Ib95f5a0c77adc50a792f7c84eafed8b579d64444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6109796
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398833}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -877,7 +877,7 @@ BASE_FEATURE(kPageAnnotationsService,
 
 BASE_FEATURE(kPageContentProvider,
              "PageContentProvider",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPowerSavingModeBroadcastReceiverInBackground,
              "PowerSavingModeBroadcastReceiverInBackground",
