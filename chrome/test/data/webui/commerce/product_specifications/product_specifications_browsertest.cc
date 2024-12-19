2edf228fc2616528e444ee516c4af9b2e8496aeb
Tsuyoshi Horo <horo@chromium.org>
2024-12-18 20:47:15 -0800

    Disable ProductSpecificationsTest.App
    Bug: 385024070
    Change-Id: Ib5a81eb6b5acc6feccadd90f6bda21fd428a2095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6109611
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398406}

--- chrome/test/data/webui/commerce/product_specifications/product_specifications_browsertest.cc
+++ chrome/test/data/webui/commerce/product_specifications/product_specifications_browsertest.cc
@@ -69,7 +69,8 @@ class ProductSpecificationsTest : public WebUIMochaBrowserTest {
   base::WeakPtrFactory<ProductSpecificationsTest> weak_ptr_factory_{this};
 };
 
-IN_PROC_BROWSER_TEST_F(ProductSpecificationsTest, App) {
+// TODO(crbug.com/385024070): Flaky on all platforms.
+IN_PROC_BROWSER_TEST_F(ProductSpecificationsTest, DISABLED_App) {
   RunTest("commerce/product_specifications/app_test.js", "mocha.run()");
 }
 
