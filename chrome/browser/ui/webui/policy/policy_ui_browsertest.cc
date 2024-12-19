e97408c501358cd50b482facb672433568a0ee8f
James Cook <jamescook@chromium.org>
2024-12-18 16:19:50 -0800

    Disable PolicyUITest.SendPolicyValues on desktop-android
    The test is failing consistently and making our bot red. See bug for
    details.
    
    Bug: 384989795
    Change-Id: I5f0351a47311c18f21b5b4fa205384de68fb36c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6107758
    Reviewed-by: Xiqi Ruan <xiqiruan@chromium.org>
    Owners-Override: Xiqi Ruan <xiqiruan@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398324}

--- chrome/browser/ui/webui/policy/policy_ui_browsertest.cc
+++ chrome/browser/ui/webui/policy/policy_ui_browsertest.cc
@@ -26,6 +26,7 @@
 #include "base/time/clock.h"
 #include "base/time/time.h"
 #include "base/values.h"
+#include "build/android_buildflags.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
@@ -548,7 +549,13 @@ IN_PROC_BROWSER_TEST_F(PolicyUITest, SendPolicyNames) {
   VerifyPolicies(expected_policies);
 }
 
-IN_PROC_BROWSER_TEST_F(PolicyUITest, SendPolicyValues) {
+// TODO(crbug.com/384989795): Fails on desktop android, see bug.
+#if BUILDFLAG(IS_DESKTOP_ANDROID)
+#define MAYBE_SendPolicyValues DISABLED_SendPolicyValues
+#else
+#define MAYBE_SendPolicyValues SendPolicyValues
+#endif
+IN_PROC_BROWSER_TEST_F(PolicyUITest, MAYBE_SendPolicyValues) {
   // Verifies that policy values are sent to the UI and processed there
   // correctly by setting the values of four known and one unknown policy and
   // checking that the policy table contains the policy names, values and
