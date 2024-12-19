d30007e732c9de275b154d0363c4ea09bfb17409
Christian Xu <christianxu@google.com>
2024-12-19 09:30:50 -0800

    [iOS][LO] Add lens overlay price insights counterfactual
    Lens overlay's entrypoint in the location bar is trumped by price
    insights. To measure the impact of price insights on the feature, there
    is a feature flag to disable price insights
    "kLensOverlayDisablePriceInsights" used in experimentation.
    
    This CL adds another flag that enables the lens overlay entrypoint only
    when the price insights entrypoint would have been presented. It will be
    used as a counterfactual arm during experimentation.
    - When the price insights entrypoint should be presented, the lens
    overlay entrypoint is shown instead.
    
    Bug: 383902019
    Change-Id: Idf83e1c91970bdfa590130a4d20b3a7df583f95d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6099213
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398649}

--- ios/chrome/browser/price_insights/model/price_insights_feature.mm
+++ ios/chrome/browser/price_insights/model/price_insights_feature.mm
@@ -36,7 +36,8 @@ bool IsPriceInsightsEnabled(ProfileIOS* profile) {
   // Allow Lens overlay to disable price insights because the price insights
   // entrypoint trumps lens overlay in the location bar. This is only used for
   // experimentation in coordination with the price insight owner.
-  if (base::FeatureList::IsEnabled(kLensOverlayDisablePriceInsights)) {
+  if (base::FeatureList::IsEnabled(kLensOverlayDisablePriceInsights) &&
+      !base::FeatureList::IsEnabled(kLensOverlayPriceInsightsCounterfactual)) {
     return false;
   }
 
