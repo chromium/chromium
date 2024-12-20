1e541302a1a83a9488cb9003412d58c2cc660a96
Juan Mojica <juanmojica@google.com>
2024-12-20 11:13:24 -0800

    Revert "Use SolidColorLayer as a background for ContentsWebView"
    This reverts commit 9419fccc5dd796b7f581e154add788643ddf9b93.
    
    Reason for revert: The CL is breaking event propagation in PWAs
    b:385257216
    
    Original change's description:
    > Use SolidColorLayer as a background for ContentsWebView
    >
    > Currently, ContentsWebView paints a background. This background will
    > be repainted when ever browser window repaints. (It is quite a
    > frequent operation)
    >
    > Using a SolidColorLayer is more efficient since we do not have to
    > repaint the background.
    >
    > Additionally on platforms using aura, this SolidColorLayer will not
    > be drawn due to occlusion culling by viz. ContentsWebView is always
    > occluded by the WebContentsViewAura layer. Therefore, the
    > ContentsWebView background will be only drawn when the web contents are
    > not visible.
    >
    > Bug: b:383408762
    > Change-Id: I3567fcfb1abe736a72e54b50f5681aaa714984bb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6093289
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1396874}
    
    Bug: b:383408762, b:385257216
    Change-Id: I56a4cde6cbc7482e7e9206d2997866aba1ad3c16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6112082
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399287}

--- chrome/browser/ui/views/frame/contents_web_view.cc
+++ chrome/browser/ui/views/frame/contents_web_view.cc
@@ -4,7 +4,6 @@
 
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
 
-#include "base/check.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/views/status_bubble_views.h"
 #include "content/public/browser/render_widget_host_view.h"
@@ -14,7 +13,6 @@
 #include "ui/color/color_provider.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_tree_owner.h"
-#include "ui/compositor/layer_type.h"
 #include "ui/views/background.h"
 #include "ui/views/view_class_properties.h"
 
@@ -33,8 +31,6 @@ DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(ContentsWebView,
 ContentsWebView::ContentsWebView(content::BrowserContext* browser_context)
     : views::WebView(browser_context),
       status_bubble_(nullptr) {
-  // Draws the ContentsWebView background.
-  SetPaintToLayer(ui::LAYER_SOLID_COLOR);
   SetProperty(views::kElementIdentifierKey, kContentsWebViewElementId);
 #if BUILDFLAG(ENABLE_GLIC)
   glic_border_ = AddChildView(std::make_unique<glic::BorderView>());
@@ -70,24 +66,19 @@ StatusBubbleViews* ContentsWebView::GetStatusBubble() const {
 
 void ContentsWebView::SetBackgroundVisible(bool background_visible) {
   background_visible_ = background_visible;
-  if (GetWidget()) {
+  if (GetWidget())
     UpdateBackgroundColor();
-  }
-}
-
-const gfx::RoundedCornersF& ContentsWebView::GetBackgroundRadii() const {
-  const ui::Layer* background_layer = layer();
-
-  CHECK(background_layer);
-  return background_layer->rounded_corner_radii();
 }
 
 void ContentsWebView::SetBackgroundRadii(const gfx::RoundedCornersF& radii) {
-  ui::Layer* background_layer = layer();
+  if (background_radii_ == radii) {
+    return;
+  }
 
-  CHECK(background_layer);
-  background_layer->SetRoundedCornerRadius(radii);
-  background_layer->SetIsFastRoundedCorner(true);
+  background_radii_ = radii;
+  if (GetWidget()) {
+    UpdateBackgroundColor();
+  }
 }
 
 bool ContentsWebView::GetNeedsNotificationWhenVisibleBoundsChange() const {
@@ -110,12 +101,12 @@ void ContentsWebView::OnLetterboxingChanged() {
 }
 
 void ContentsWebView::UpdateBackgroundColor() {
-  const SkColor color = GetColorProvider()->GetColor(
+  SkColor color = GetColorProvider()->GetColor(
       is_letterboxing() ? kColorWebContentsBackgroundLetterboxing
                         : kColorWebContentsBackground);
-
-  ui::Layer* background_layer = layer();
-  background_layer->SetColor(background_visible_ ? color : SK_ColorTRANSPARENT);
+  SetBackground(background_visible_ ? views::CreateRoundedRectBackground(
+                                          color, background_radii_)
+                                    : nullptr);
 
   if (web_contents()) {
     content::RenderWidgetHostView* rwhv =
@@ -159,7 +150,9 @@ void ContentsWebView::CloneWebContentsLayer() {
     return;
   }
 
-  // The cloned layer is in a different coordinate system than our layer (which
+  SetPaintToLayer();
+
+  // The cloned layer is in a different coordinate system them our layer (which
   // is now the new parent of the cloned layer). Convert coordinates so that the
   // cloned layer appears at the right location.
   gfx::PointF origin;
@@ -173,6 +166,7 @@ void ContentsWebView::CloneWebContentsLayer() {
 
 void ContentsWebView::DestroyClonedLayer() {
   cloned_layer_tree_.reset();
+  DestroyLayer();
 }
 
 void ContentsWebView::RenderViewReady() {
