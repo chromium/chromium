6791c9c09db04c2014308fd6fbaa3da0a75a62bb
Gavin Williams <gavinwill@chromium.org>
2024-12-19 17:11:02 -0800

    Connecitvity: Convert network_config_input to ts
    Fixed: 367735415
    Change-Id: I5d36f6bb4b1df33812a2a67c1a392e51352449aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6108012
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1398963}

--- ash/webui/common/resources/network/network_config_input.ts
+++ ash/webui/common/resources/network/network_config_input.ts
@@ -10,18 +10,13 @@ import '//resources/ash/common/cr_elements/cr_shared_vars.css.js';
 import './cr_policy_network_indicator_mojo.js';
 import './network_shared.css.js';
 
+import {assert} from '//resources/js/assert.js';
 import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {CrPolicyNetworkBehaviorMojo, CrPolicyNetworkBehaviorMojoInterface} from './cr_policy_network_behavior_mojo.js';
 import {NetworkConfigElementBehavior, NetworkConfigElementBehaviorInterface} from './network_config_element_behavior.js';
 import {getTemplate} from './network_config_input.html.js';
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {CrPolicyNetworkBehaviorMojoInterface}
- * @implements {NetworkConfigElementBehaviorInterface}
- */
 const NetworkConfigInputElementBase = mixinBehaviors(
     [
       CrPolicyNetworkBehaviorMojo,
@@ -29,10 +24,9 @@ const NetworkConfigInputElementBase = mixinBehaviors(
     ],
     PolymerElement);
 
-/** @polymer */
-class NetworkConfigInputElement extends NetworkConfigInputElementBase {
+export class NetworkConfigInputElement extends NetworkConfigInputElementBase {
   static get is() {
-    return 'network-config-input';
+    return 'network-config-input' as const;
   }
 
   static get template() {
@@ -52,24 +46,41 @@ class NetworkConfigInputElement extends NetworkConfigInputElementBase {
         type: Boolean,
         value: false,
       },
+
+      readonly: {
+        type: Boolean,
+        value: false,
+      },
+
+      value: String,
     };
   }
 
-  focus() {
-    this.shadowRoot.querySelector('cr-input').focus();
+  label: string;
+  override hidden: boolean;
+  invalid: boolean;
+  readonly: boolean;
+  value: string;
+
+  override focus() {
+    const input = this.shadowRoot!.querySelector('cr-input')
+    assert(input);
+    input.focus();
   }
 
-  /**
-   * @param {!Event} event
-   * @private
-   */
-  onKeypress_(event) {
+  private onKeypress_(event: KeyboardEvent): void {
     if (event.key !== 'Enter') {
       return;
     }
     event.stopPropagation();
     this.dispatchEvent(
         new CustomEvent('enter', {bubbles: true, composed: true}));
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [NetworkConfigInputElement.is]: NetworkConfigInputElement;
   }
 }
 
--- chrome/test/data/webui/chromeos/network/network_config_input_test.ts
+++ chrome/test/data/webui/chromeos/network/network_config_input_test.ts
@@ -4,12 +4,13 @@
 
 import 'chrome://resources/ash/common/network/network_config_input.js';
 
+import {NetworkConfigInputElement} from 'chrome://resources/ash/common/network/network_config_input.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {keyEventOn} from 'chrome://webui-test/keyboard_mock_interactions.js';
 
 suite('NetworkConfigInputTest', function() {
-  /** @type {!NetworkConfigInput|undefined} */
-  let configInput;
+  let configInput: NetworkConfigInputElement;
 
   setup(function() {
     configInput = document.createElement('network-config-input');
@@ -17,14 +18,15 @@ suite('NetworkConfigInputTest', function() {
     flush();
   });
 
-  test('Enter key propagates up enter event', function(done) {
+  test('Enter key propagates up enter event', (done) => {
     // Set up a listener to assert the 'enter' event fired.
     configInput.addEventListener('enter', function() {
       done();
     });
 
     // Simulate pressing enter on the input element.
-    const inputEl = configInput.$$('cr-input');
+    const inputEl = configInput.shadowRoot!.querySelector('cr-input');
+    assertTrue(!!inputEl);
     keyEventOn(
         inputEl, 'keypress', /* keyCode */ 13, /* modifiers */ undefined,
         'Enter');
