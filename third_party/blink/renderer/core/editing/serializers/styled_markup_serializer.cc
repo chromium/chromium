b88ffb57dd44f480cea7b796d12d73fca964a5a3
Tanu Jain <tanujain@microsoft.com>
2024-12-23 22:36:39 -0800

    [Editing][Serialize] Append list element tag to copied HTML of list with extended selection
    When the selection is extended beyond the list, the list is
    incorrectly copied. Specifically, if there is a <br> tag before the
    list and the entire content is copied, the list
    element tag (<ul>, <ol>) is getting lost. Blink only considers the
    list item tag (<li>), rather than the list element tag.
    
    In the file styled_markup_serializer.cc, change the
    should_append_parent_tag to true when last_closed_ is a list item
    tag. This ensures that the list element tag is retained during the
    copy operation, preserving the structure of the list.
    
    Bug: 40658772
    Change-Id: Ia54c2bdd23b677b25ef307d11944fb8b5c2098f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6057128
    Reviewed-by: Ashish Kumar <ashishkum@microsoft.com>
    Commit-Queue: Tanu Jain <tanujain@microsoft.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1399992}

--- third_party/blink/renderer/core/editing/serializers/styled_markup_serializer.cc
+++ third_party/blink/renderer/core/editing/serializers/styled_markup_serializer.cc
@@ -46,9 +46,12 @@
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/html/forms/html_text_area_element.h"
 #include "third_party/blink/renderer/core/html/html_body_element.h"
+#include "third_party/blink/renderer/core/html/html_dlist_element.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/html/html_iframe_element.h"
+#include "third_party/blink/renderer/core/html/html_olist_element.h"
 #include "third_party/blink/renderer/core/html/html_table_element.h"
+#include "third_party/blink/renderer/core/html/html_ulist_element.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
 namespace blink {
@@ -82,6 +85,17 @@ bool HandleSelectionBoundary<EditingInFlatTreeStrategy>(const Node& node) {
 }
 
 }  // namespace
+
+template <typename... ElementTypes>
+inline Node* FirstAncestorOfTypes(const Node& current) {
+  for (ContainerNode* ancestor = current.parentNode(); ancestor;
+       ancestor = ancestor->parentNode()) {
+    if (((DynamicTo<ElementTypes>(*ancestor)) || ...)) {
+      return ancestor;
+    }
+  }
+  return nullptr;
+}
 
 template <typename Strategy>
 class StyledMarkupTraverser {
@@ -207,25 +221,8 @@ String StyledMarkupSerializer<Strategy>::CreateMarkup() {
   // be #text when its parent is a formatting tag. In this case, #text is
   // wrapped by <span> tag, but this text should be wrapped by the formatting
   // tag. See http://crbug.com/634482
-  bool should_append_parent_tag = false;
-  if (!last_closed_) {
-    last_closed_ =
-        StyledMarkupTraverser<Strategy>().Traverse(first_node, past_end);
-    if (last_closed_ && last_closed_->IsTextNode() &&
-        IsPresentationalHTMLElement(last_closed_->parentNode())) {
-      last_closed_ = last_closed_->parentElement();
-      should_append_parent_tag = true;
-    }
-    if (RuntimeEnabledFeatures::IncludeTableTagInExtendedSelectionEnabled()) {
-      if (last_closed_ && IsTablePartElement(last_closed_)) {
-        if (auto* first_ancestor_table_traversal =
-                Traversal<HTMLTableElement>::FirstAncestor(*last_closed_)) {
-          last_closed_ = first_ancestor_table_traversal;
-          should_append_parent_tag = true;
-        }
-      }
-    }
-  }
+  bool should_append_parent_tag =
+      DetermineParentTagAndUpdateLastClosed(first_node, past_end);
 
   StyledMarkupTraverser<Strategy> traverser(&markup_accumulator, last_closed_);
   Node* last_closed = traverser.Traverse(first_node, past_end);
@@ -318,6 +315,42 @@ String StyledMarkupSerializer<Strategy>::CreateMarkup() {
     markup_accumulator.AppendInterchangeNewline();
 
   return markup_accumulator.TakeResults();
+}
+
+template <typename Strategy>
+bool StyledMarkupSerializer<Strategy>::DetermineParentTagAndUpdateLastClosed(
+    Node* first_node,
+    Node* past_end) {
+  if (last_closed_) {
+    return false;
+  }
+  last_closed_ =
+      StyledMarkupTraverser<Strategy>().Traverse(first_node, past_end);
+  if (last_closed_ && last_closed_->IsTextNode() &&
+      IsPresentationalHTMLElement(last_closed_->parentNode())) {
+    last_closed_ = last_closed_->parentElement();
+    return true;
+  }
+  if (RuntimeEnabledFeatures::IncludeTableTagInExtendedSelectionEnabled()) {
+    if (last_closed_ && IsTablePartElement(last_closed_)) {
+      if (auto* first_ancestor_table_traversal =
+              Traversal<HTMLTableElement>::FirstAncestor(*last_closed_)) {
+        last_closed_ = first_ancestor_table_traversal;
+        return true;
+      }
+    }
+  }
+  if (RuntimeEnabledFeatures::
+          IncludeListElementTagInExtendedSelectionEnabled() &&
+      last_closed_ && IsListItemTag(last_closed_)) {
+    if (Node* ancestor =
+            FirstAncestorOfTypes<HTMLUListElement, HTMLOListElement,
+                                 HTMLDListElement>(*last_closed_)) {
+      last_closed_ = ancestor;
+      return true;
+    }
+  }
+  return false;
 }
 
 template <typename Strategy>
