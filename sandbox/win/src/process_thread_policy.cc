6a5265cab1f288b8ec9801201d1e1a1378161e60
Alex Gough <ajgo@chromium.org>
2024-12-30 14:40:34 -0800

    Bypass DuplicateHandle checks in sandbox IPC
    This code needs to pass threads to children. Rather than
    allowing all thread handles this CL instead bypasses
    the hook in process_thread_dispatcher.cc directly. This
    means that other Chrome code accidentally sending thread
    handles to children should still be caught by the checks.
    
    Bug: 40408399
    Change-Id: Ide865ac3b42ef4ee3bd8272af7e9ac2f88d9ac41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6127931
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401013}

--- sandbox/win/src/process_thread_policy.cc
+++ sandbox/win/src/process_thread_policy.cc
@@ -12,6 +12,7 @@
 
 #include "base/memory/free_deleter.h"
 #include "base/win/scoped_handle.h"
+#include "build/build_config.h"
 #include "sandbox/win/src/ipc_tags.h"
 #include "sandbox/win/src/nt_internals.h"
 #include "sandbox/win/src/policy_engine_opcodes.h"
@@ -21,6 +22,30 @@
 #include "sandbox/win/src/win_utils.h"
 
 namespace sandbox {
+namespace {
+BOOL CallDuplicateHandle(HANDLE hSourceProcessHandle,
+                         HANDLE hSourceHandle,
+                         HANDLE hTargetProcessHandle,
+                         LPHANDLE lpTargetHandle,
+                         DWORD dwDesiredAccess,
+                         BOOL bInheritHandle,
+                         DWORD dwOptions) {
+#if !defined(OFFICIAL_BUILD) && !defined(COMPONENT_BUILD)
+  // In tests this bypasses the //sandbox/policy hooks for ::DuplicateHandle.
+  using DuplicateHandleFunctionPtr = decltype(::DuplicateHandle)*;
+  static DuplicateHandleFunctionPtr duplicatehandle_fn =
+      reinterpret_cast<DuplicateHandleFunctionPtr>(::GetProcAddress(
+          ::GetModuleHandle(L"kernel32.dll"), "DuplicateHandle"));
+  return duplicatehandle_fn(hSourceProcessHandle, hSourceHandle,
+                            hTargetProcessHandle, lpTargetHandle,
+                            dwDesiredAccess, bInheritHandle, dwOptions);
+#else
+  return ::DuplicateHandle(hSourceProcessHandle, hSourceHandle,
+                           hTargetProcessHandle, lpTargetHandle,
+                           dwDesiredAccess, bInheritHandle, dwOptions);
+#endif
+}
+}  // namespace
 
 NTSTATUS ProcessPolicy::OpenThreadAction(const ClientInfo& client_info,
                                          uint32_t desired_access,
@@ -39,9 +64,9 @@ NTSTATUS ProcessPolicy::OpenThreadAction(const ClientInfo& client_info,
   NTSTATUS status = GetNtExports()->OpenThread(&local_handle, desired_access,
                                                &attributes, &client_id);
   if (NT_SUCCESS(status)) {
-    if (!::DuplicateHandle(::GetCurrentProcess(), local_handle,
-                           client_info.process, handle, 0, false,
-                           DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS)) {
+    if (!CallDuplicateHandle(::GetCurrentProcess(), local_handle,
+                             client_info.process, handle, 0, false,
+                             DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS)) {
       return STATUS_ACCESS_DENIED;
     }
   }
@@ -55,16 +80,17 @@ NTSTATUS ProcessPolicy::OpenProcessTokenExAction(const ClientInfo& client_info,
                                                  uint32_t attributes,
                                                  HANDLE* handle) {
   *handle = nullptr;
-  if (CURRENT_PROCESS != process)
+  if (CURRENT_PROCESS != process) {
     return STATUS_ACCESS_DENIED;
+  }
 
   HANDLE local_handle = nullptr;
   NTSTATUS status = GetNtExports()->OpenProcessTokenEx(
       client_info.process, desired_access, attributes, &local_handle);
   if (NT_SUCCESS(status)) {
-    if (!::DuplicateHandle(::GetCurrentProcess(), local_handle,
-                           client_info.process, handle, 0, false,
-                           DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS)) {
+    if (!CallDuplicateHandle(::GetCurrentProcess(), local_handle,
+                             client_info.process, handle, 0, false,
+                             DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS)) {
       return STATUS_ACCESS_DENIED;
     }
   }
@@ -85,9 +111,9 @@ DWORD ProcessPolicy::CreateThreadAction(
   if (!local_handle.is_valid()) {
     return ::GetLastError();
   }
-  if (!::DuplicateHandle(::GetCurrentProcess(), local_handle.get(),
-                         client_info.process, handle, 0, FALSE,
-                         DUPLICATE_SAME_ACCESS)) {
+  if (!CallDuplicateHandle(::GetCurrentProcess(), local_handle.get(),
+                           client_info.process, handle, 0, FALSE,
+                           DUPLICATE_SAME_ACCESS)) {
     return ERROR_ACCESS_DENIED;
   }
   return ERROR_SUCCESS;
