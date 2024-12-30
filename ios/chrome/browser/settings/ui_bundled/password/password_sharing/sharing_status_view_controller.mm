8eea0a46c09178910218f4ec2af9dba4aa88ca79
Daniel White <danieltwhite@google.com>
2024-12-30 10:29:26 -0800

    [iOS] Deprecate UITextItemInteraction in SharingStatusViewController
    Since UITextItemInteraction is deprecated in iOS 17, was updated to use
    the new alternative system,
    textView:primaryActionForTextItem:defaultAction.
    
    Fixed: 349400180
    Change-Id: I40d89a2c687a60358813f2fa6bcbf2bcc1a22222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088947
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1400953}

--- ios/chrome/browser/settings/ui_bundled/password/password_sharing/sharing_status_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_sharing/sharing_status_view_controller.mm
@@ -244,12 +244,23 @@ NSString* const kSharingStatusFooterId = @"SharingStatusViewFooter";
 
 #pragma mark - UITextViewDelegate
 
+#if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (BOOL)textView:(UITextView*)textView
     shouldInteractWithURL:(NSURL*)URL
                   inRange:(NSRange)characterRange
               interaction:(UITextItemInteraction)interaction {
   [self.delegate changePasswordLinkWasTapped];
   return NO;
+}
+#endif
+
+- (UIAction*)textView:(UITextView*)textView
+    primaryActionForTextItem:(UITextItem*)textItem
+               defaultAction:(UIAction*)defaultAction API_AVAILABLE(ios(17.0)) {
+  __weak __typeof(self) weakSelf = self;
+  return [UIAction actionWithHandler:^(UIAction* action) {
+    [weakSelf.delegate changePasswordLinkWasTapped];
+  }];
 }
 
 #pragma mark - Private
