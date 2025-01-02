f81e64b4b3db62e518026829bd9b95cd8678866e
Andy Phan <andyphan@chromium.org>
2025-01-02 09:07:02 -0800

    [CodeHealth] Migrate user actions UI to TypeScript
    Fixed: 385317090
    Change-Id: I531f8fd8cb72e770aa0c35f6be1d997dc3ee2124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6111518
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1401460}

--- components/user_actions_ui/resources/user_actions.ts
+++ components/user_actions_ui/resources/user_actions.ts
@@ -3,31 +3,28 @@
 // found in the LICENSE file.
 
 /**
- * Javascript for user_actions.html, served from chrome://user-actions/
+ * Typescript for user_actions.html, served from chrome://user-actions/
  * This is used to debug user actions recording. It displays a live
  * stream of all user action events that occur in chromium while the
  * chrome://user-actions/ page is open.
- *
- * The simple object defined in this javascript file listens for
- * callbacks from the C++ code saying that a new user action was seen.
  */
 
-import {addWebUiListener} from 'chrome://resources/js/cr.js';
-import {$} from 'chrome://resources/js/util.js';
+import {addWebUiListener} from '//resources/js/cr.js';
+import {getRequiredElement} from '//resources/js/util.js';
 
 /**
  * Appends a row to the output table listing the user action observed
  * and the current timestamp.
- * @param {string} userAction the name of the user action observed.
+ * @param userAction the name of the user action observed.
  */
-function observeUserAction(userAction) {
-  const table = $('user-actions-table');
+function observeUserAction(userAction: string): void {
+  const table = getRequiredElement('user-actions-table');
   const tr = document.createElement('tr');
   let td = document.createElement('td');
   td.textContent = userAction;
   tr.appendChild(td);
   td = document.createElement('td');
-  td.textContent = Date.now() / 1000;  // in seconds since epoch
+  td.textContent = (Date.now() / 1000).toString();  // in seconds since epoch
   tr.appendChild(td);
   table.appendChild(tr);
 }
