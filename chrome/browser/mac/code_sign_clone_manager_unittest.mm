1a3aeebce4848b92bdadc2b9fef85df4efa921e3
Avi Drissman <avi@chromium.org>
2025-01-02 11:22:40 -0800

    [M132] Fix loss of default browser via code sign cloning
    Code sign cloning creates a clone of Chromium upon launch in order to
    solve code sign issues. However, because Launch Services tracks
    applications, the existence of a second copy of the Chromium app
    is troublesome and in the worst case can cause the loss of default
    browser. Therefore, clone the app with the extension ".bundle". This
    avoids Launch Services issues, but still addresses the problem of
    dynamic reference code validation.
    
    (cherry picked from commit cd1341f18bf05bb4aa9e16b2126e41f3b59beed4)
    
    Bug: 381199182
    Change-Id: Ie8f65373113da7ee2bc87476ff4593d3439c7e52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6102575
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1397999}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6138690
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6834@{#3111}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- chrome/browser/mac/code_sign_clone_manager_unittest.mm
+++ chrome/browser/mac/code_sign_clone_manager_unittest.mm
@@ -70,8 +70,8 @@ namespace code_sign_clone_manager {
 TEST(CodeSignCloneManagerTest, Clone) {
   content::BrowserTaskEnvironment task_environment;
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      code_sign_clone_manager::kMacAppCodeSignClone);
+  feature_list.InitWithFeatures(
+      {kMacAppCodeSignClone, kMacAppCodeSignCloneRenameAsBundle}, {});
 
   // Create the test app,
   base::FilePath test_app = CreateTestApp();
@@ -95,6 +95,13 @@ TEST(CodeSignCloneManagerTest, Clone) {
   run_loop.Run();
 
   ASSERT_NE(tmp_app_path, base::FilePath());
+
+  // Make sure the tmp app path has a ".bundle" extension.
+  //
+  // (`base::FilePath::Extension()` only returns a double extension for common
+  // extensions that occur in double-extensions (e.g. `.bz`, `.gz`, `.z`) and
+  // `.bundle` is not on that list, so it doesn't return `.app.bundle`.)
+  EXPECT_EQ(tmp_app_path.Extension(), ".bundle");
 
   // Make sure the tmp app path has the expected files.
   for (base::FilePath file : test_files) {
@@ -123,8 +130,7 @@ TEST(CodeSignCloneManagerTest, Clone) {
 TEST(CodeSignCloneManagerTest, InvalidDirhelperPath) {
   content::BrowserTaskEnvironment task_environment;
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      code_sign_clone_manager::kMacAppCodeSignClone);
+  feature_list.InitAndEnableFeature(kMacAppCodeSignClone);
 
   base::FilePath test_app = CreateTestApp();
   base::ScopedPathOverride scoped_path_override(content::CHILD_PROCESS_EXE);
@@ -155,8 +161,7 @@ TEST(CodeSignCloneManagerTest, InvalidDirhelperPath) {
 TEST(CodeSignCloneManagerTest, FailedHardLink) {
   content::BrowserTaskEnvironment task_environment;
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      code_sign_clone_manager::kMacAppCodeSignClone);
+  feature_list.InitAndEnableFeature(kMacAppCodeSignClone);
 
   base::FilePath test_app = CreateTestApp();
   base::ScopedPathOverride scoped_path_override(content::CHILD_PROCESS_EXE);
@@ -182,8 +187,7 @@ TEST(CodeSignCloneManagerTest, FailedHardLink) {
 TEST(CodeSignCloneManagerTest, FailedClone) {
   base::test::TaskEnvironment task_environment;
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      code_sign_clone_manager::kMacAppCodeSignClone);
+  feature_list.InitAndEnableFeature(kMacAppCodeSignClone);
 
   // Starting in macOS 10.15 the system volume is a separate read-only volume.
   // Cloning files from the system volume to a non-system volume will fail with
@@ -211,8 +215,7 @@ TEST(CodeSignCloneManagerTest, FailedClone) {
 
 TEST(CodeSignCloneManagerTest, ChromeCodeSignCloneCleanupMain) {
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(
-      code_sign_clone_manager::kMacAppCodeSignClone);
+  feature_list.InitAndEnableFeature(kMacAppCodeSignClone);
 
   struct TestCase {
     std::string name;
@@ -332,7 +335,7 @@ TEST(CodeSignCloneManagerTest, IsFileOpenMoreThanOnceSeparateProcess) {
     // `stdin_writer` is closed, causing `tee` to exit.
     class TeeChildProcess {
      public:
-      TeeChildProcess(const base::FilePath& temp_file_path) {
+      explicit TeeChildProcess(const base::FilePath& temp_file_path) {
         Init(temp_file_path);
         if (testing::Test::HasFatalFailure()) {
           return;
