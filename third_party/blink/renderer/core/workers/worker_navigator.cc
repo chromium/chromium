f3300abe2fcd0164794d7a782cc221d10c17f322
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-01-06 05:34:49 -0800

    [M130] Make AcceptLanguagesWatcher a weak persistent object
    DedicatedWorkerOrSharedWorkerFetchContext keeps on having a pointer
    to the AcceptLanguagesWatcher as a raw_ptr.  Even if the implementing
    object gets garbage-collected, the pointer is kept.  Therefore,
    DedicatedWorkerOrSharedWorkerFetchContext may use it after the GC.
    
    This CL revised the raw_ptr to AcceptLanguagesWatcher to use
    WeakPersistent so that the users of the object won't access it
    after it gets GCed.
    
    (cherry picked from commit 9e7b77da977401361ef3bc9ad0f43e7563f49657)
    
    Bug: 379869752
    Change-Id: Ic3997fcf17aa93f45507241e0fc18a486efc1932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038980
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1390151}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148168
    Reviewed-by: Erhu Akpobaro <eakpobaro@google.com>
    Commit-Queue: Daniel Yip <danielyip@google.com>
    Auto-Submit: Daniel Yip <danielyip@google.com>
    Owners-Override: Daniel Yip <danielyip@google.com>
    Cr-Commit-Position: refs/branch-heads/6723@{#2761}
    Cr-Branched-From: 985f2961df230630f9cbd75bd6fe463009855a11-refs/heads/main@{#1356013}

--- third_party/blink/renderer/core/workers/worker_navigator.cc
+++ third_party/blink/renderer/core/workers/worker_navigator.cc
@@ -61,4 +61,9 @@ void WorkerNavigator::NotifyUpdate() {
       *Event::Create(event_type_names::kLanguagechange));
 }
 
+void WorkerNavigator::Trace(Visitor* visitor) const {
+  NavigatorBase::Trace(visitor);
+  AcceptLanguagesWatcher::Trace(visitor);
+}
+
 }  // namespace blink
