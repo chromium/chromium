91adb99c745fd4b2457bca0480e9eb66c115e781
Valerie Young <spectranaut@igalia.com>
2025-01-06 14:20:05 -0800

    [SwitchAccess] Convert modal_dialog_node.js to TypeScript
    Bug: 378856317
    Change-Id: I39fd19bd650f10f6a6ea5293248259c74100ccdd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6115117
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1402651}

--- chrome/browser/resources/chromeos/accessibility/switch_access/nodes/modal_dialog_node.ts
+++ chrome/browser/resources/chromeos/accessibility/switch_access/nodes/modal_dialog_node.ts
@@ -2,29 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {EventGenerator} from '../../common/event_generator.js';
-import {KeyCode} from '../../common/key_code.js';
+import {EventGenerator} from '/common/event_generator.js';
+import {KeyCode} from '/common/key_code.js';
 
 import {BasicNode, BasicRootNode} from './basic_node.js';
 
-const AutomationNode = chrome.automation.AutomationNode;
+type AutomationNode = chrome.automation.AutomationNode;
 
 /** This class represents the group rooted at a modal dialog. */
 export class ModalDialogRootNode extends BasicRootNode {
-  /** @override */
-  onExit() {
+  override onExit(): void {
     // To close a modal dialog, we need to send an escape key event.
     EventGenerator.sendKeyPress(KeyCode.ESCAPE);
   }
 
   /**
    * Creates the tree structure for a modal dialog.
-   * @param {!AutomationNode} dialogNode
-   * @return {!ModalDialogRootNode}
    */
-  static buildTree(dialogNode) {
+  static override buildTree(dialogNode: AutomationNode): ModalDialogRootNode {
     const root = new ModalDialogRootNode(dialogNode);
-    const childConstructor = node => BasicNode.create(node, root);
+    const childConstructor = (node: AutomationNode): BasicNode =>
+        BasicNode.create(node, root);
 
     BasicRootNode.findAndSetChildren(root, childConstructor);
     return root;
