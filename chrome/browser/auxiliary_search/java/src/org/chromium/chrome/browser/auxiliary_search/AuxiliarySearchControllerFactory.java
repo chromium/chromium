875ad42e3ae220a7607d20990297d890bc65c222
Xi Han <hanxi@google.com>
2025-01-06 14:31:59 -0800

    [AppIntegration] Add a method for testing.
    This is a follow up of CL: https://crrev.com/c/6148609. This CL adds a
    missing change required by https://crrev.com/i/7934008.
    
    Bug: 385358372
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Change-Id: I246e1ffadabfd528a895b4cba1dc8e8449162161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6150608
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1402661}

--- chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchControllerFactory.java
+++ chrome/browser/auxiliary_search/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchControllerFactory.java
@@ -112,4 +112,8 @@ public class AuxiliarySearchControllerFactory {
         mHooksForTesting = instanceForTesting;
         ResettersForTesting.register(() -> mHooksForTesting = null);
     }
+
+    public void resetIsTabletForTesting() {
+        mIsTablet = null;
+    }
 }
