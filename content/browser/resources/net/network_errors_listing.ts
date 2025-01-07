8fe8c210fcc723e493e380525bdc361812f4cd83
dpapad <dpapad@chromium.org>
2025-01-07 09:49:02 -0800

    WebUI: Leverage build_webui() in chrome://network-errors, migrate to TS.
    This is in preparation of removing a JS minification step happening
    within Grit on Android builds, as Grit should not be responsible for
    performing minification.
    
    Bug: 340278433
    Change-Id: Idcfeb3511f2451b8a37f843321bc25ff5d88785f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6149108
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403061}

--- content/browser/resources/net/network_errors_listing.ts
+++ content/browser/resources/net/network_errors_listing.ts
@@ -2,38 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {$} from 'chrome://resources/js/util.js';
+import {getRequiredElement} from 'chrome://resources/js/util.js';
 
 /**
  * Generate the page content.
- * @param {Array<Object>} errorCodes Error codes array consisting of a
- *    numerical error ID and error code string.
+ * @param errorCodes Error codes array consisting of a numerical error ID and
+ *     error code string.
  */
-function listErrorCodes(errorCodes) {
+function listErrorCodes(errorCodes: NetworkError[]) {
   const errorPageUrl = 'chrome://network-error/';
   const errorCodesList = document.createElement('ul');
-  for (let i = 0; i < errorCodes.length; i++) {
+  for (const error of errorCodes) {
     const listEl = document.createElement('li');
     const errorCodeLinkEl = document.createElement('a');
-    errorCodeLinkEl.href = errorPageUrl + errorCodes[i].errorId;
-    errorCodeLinkEl.textContent =
-        errorCodes[i].errorCode + ' (' + errorCodes[i].errorId + ')';
+    errorCodeLinkEl.href = errorPageUrl + error.errorId;
+    errorCodeLinkEl.textContent = error.errorCode + ' (' + error.errorId + ')';
     listEl.appendChild(errorCodeLinkEl);
     errorCodesList.appendChild(listEl);
   }
-  $('pages').appendChild(errorCodesList);
+  getRequiredElement('pages').appendChild(errorCodesList);
+}
+
+interface NetworkError {
+  errorCode: string;
+  errorId: number;
 }
 
 function initialize() {
   const xhr = new XMLHttpRequest();
   xhr.open('GET', 'network-error-data.json');
-  xhr.addEventListener('load', function(e) {
+  xhr.addEventListener('load', function(_e) {
     if (xhr.status === 200) {
       try {
-        const data = JSON.parse(xhr.responseText);
+        const data =
+            JSON.parse(xhr.responseText) as {errorCodes: NetworkError[]};
         listErrorCodes(data['errorCodes']);
       } catch (e) {
-        $('pages').innerText = 'Could not parse the error codes data. ' +
+        getRequiredElement('pages').textContent =
+            'Could not parse the error codes data. ' +
             'Try reloading the page.';
       }
     }
