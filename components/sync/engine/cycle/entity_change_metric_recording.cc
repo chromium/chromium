ee975582254aa145bacfd91e0c846746fff70fd0
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-01-07 05:05:14 -0800

    [sync] Delete legacy ModelType histograms
    Unblocked by http://cl/709887990.
    
    OBSOLETE_HISTOGRAMS=ModelTypeCount4 & ModelTypeEntityChange3 were respectively replaced by DataTypeCount and DataTypeEntityChange
    
    Fixed: 358120886
    Change-Id: If99d8918bd8165103fd4713ad404f81eefd56e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6010750
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1402915}

--- components/sync/engine/cycle/entity_change_metric_recording.cc
+++ components/sync/engine/cycle/entity_change_metric_recording.cc
@@ -11,8 +11,6 @@ namespace syncer {
 namespace {
 
 const char kEntityChangeHistogramPrefix[] = "Sync.DataTypeEntityChange.";
-const char kLegacyEntityChangeHistogramPrefix[] =
-    "Sync.ModelTypeEntityChange3.";
 
 }  // namespace
 
@@ -20,14 +18,6 @@ void RecordEntityChangeMetrics(DataType type, DataTypeEntityChange change) {
   std::string histogram_name = std::string(kEntityChangeHistogramPrefix) +
                                DataTypeToHistogramSuffix(type);
   base::UmaHistogramEnumeration(histogram_name, change);
-
-  // Legacy equivalent, before the metric was renamed.
-  // TODO(crbug.com/358120886): Stop recording once alerts are switched to use
-  // Sync.DataTypeEntityChange.
-  std::string legacy_histogram_name =
-      std::string(kLegacyEntityChangeHistogramPrefix) +
-      DataTypeToHistogramSuffix(type);
-  base::UmaHistogramEnumeration(legacy_histogram_name, change);
 }
 
 std::string GetEntityChangeHistogramNameForTest(DataType type) {
