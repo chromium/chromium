c628c87401d0b41613e9d81125d123292327ccbe
zhoupeng <zhoupeng.1996@bytedance.com>
2025-01-06 18:59:47 -0800

    InsertText should not change the sibling element's DOM structure
    Consider this test case:
    <div contenteditable>
      <span style="color: red;">red</span><span>black</span>
      <span style="color: red;">^red</span><span>bla|ck</span>
      <span style="color: red;">red</span><span>black</span>
    </div>
    The result of executing `execCommand('insertText', false, 'x')`:
    <div contenteditable>
      <span style="color: red;">red</span>black
      <font color="#ff0000">x</font>ck
      <span style="color: red;">red</span>black
    </div>
    In this test case, `SplitTextAtStart` does not create a dummy span,
    yet it still performs a cleanup. This is the primary reason for
    the issue[1], cleanup all `<span>black</span>` as dummy spans.
    
    [1]: https://issues.chromium.org/issues/379186294
    
    Bug: 379186294
    Change-Id: If499f05db8cc874bf600f2ab4a92dec650f6891b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6147627
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Peng Zhou <zhoupeng.1996@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1402770}

--- third_party/blink/renderer/core/editing/commands/apply_style_command.cc
+++ third_party/blink/renderer/core/editing/commands/apply_style_command.cc
@@ -61,6 +61,7 @@
 #include "third_party/blink/renderer/core/layout/layout_text.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
@@ -707,30 +708,42 @@ void ApplyStyleCommand::ApplyInlineStyle(EditingStyle* style,
   // of it
   bool split_start = IsValidCaretPositionInTextNode(start);
   if (split_start) {
-    if (ShouldSplitTextElement(start.AnchorNode()->parentElement(), style))
+    bool should_split_text_element =
+        ShouldSplitTextElement(start.AnchorNode()->parentElement(), style);
+    if (should_split_text_element) {
       SplitTextElementAtStart(start, end);
-    else
+    } else {
       SplitTextAtStart(start, end);
+    }
     start = StartPosition();
     end = EndPosition();
     if (start.IsNull() || end.IsNull())
       return;
-    start_dummy_span_ancestor = DummySpanAncestorForNode(start.AnchorNode());
+    if (!RuntimeEnabledFeatures::SplitTextNotCleanupDummySpansEnabled() ||
+        should_split_text_element) {
+      start_dummy_span_ancestor = DummySpanAncestorForNode(start.AnchorNode());
+    }
   }
 
   // split the end node and containing element if the selection ends inside of
   // it
   bool split_end = IsValidCaretPositionInTextNode(end);
   if (split_end) {
-    if (ShouldSplitTextElement(end.AnchorNode()->parentElement(), style))
+    bool should_split_text_element =
+        ShouldSplitTextElement(end.AnchorNode()->parentElement(), style);
+    if (should_split_text_element) {
       SplitTextElementAtEnd(start, end);
-    else
+    } else {
       SplitTextAtEnd(start, end);
+    }
     start = StartPosition();
     end = EndPosition();
     if (start.IsNull() || end.IsNull())
       return;
-    end_dummy_span_ancestor = DummySpanAncestorForNode(end.AnchorNode());
+    if (!RuntimeEnabledFeatures::SplitTextNotCleanupDummySpansEnabled() ||
+        should_split_text_element) {
+      end_dummy_span_ancestor = DummySpanAncestorForNode(end.AnchorNode());
+    }
   }
 
   // Remove style from the selection.
