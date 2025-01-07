3bd5cd81b8245823666d1776d16689b5076ac269
Tommy Nyquist <nyquist@chromium.org>
2025-01-07 13:21:55 -0800

    [messaging] Enable messaging framework by default.
    This enables the messaging framework by default on mobile platforms.
    Desktop is blocked on crbug.com/387533186
    
    Bug: 345856704
    Change-Id: Ied897f09189911853de8e0f0349b45ddda9c54d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6104217
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403218}

--- components/collaboration/public/features.cc
+++ components/collaboration/public/features.cc
@@ -5,12 +5,17 @@
 #include "components/collaboration/public/features.h"
 
 #include "base/feature_list.h"
+#include "build/build_config.h"
 
 namespace collaboration::features {
 
 BASE_FEATURE(kCollaborationMessaging,
              "CollaborationMessaging",
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#else
              base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
 
 BASE_FEATURE(kCollaborationFlowAndroid,
              "CollaborationFlowAndroid",
