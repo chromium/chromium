5d7c796aac55a6d44cc1e03f652f28873ad72061
Achuith Bhandarkar <achuith@chromium.org>
2025-01-08 09:44:04 -0800

    Replace interactive_test_utils with EventGenerator
    BUG=40268116
    
    Change-Id: I36460469e53f537c47250793db676981f829b555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6144887
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1403654}

--- chrome/browser/ash/login/password_change_browsertest.cc
+++ chrome/browser/ash/login/password_change_browsertest.cc
@@ -9,6 +9,7 @@
 
 #include "ash/public/cpp/login_screen_test_api.h"
 #include "ash/public/cpp/reauth_reason.h"
+#include "ash/shell.h"
 #include "base/auto_reset.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -51,6 +52,7 @@
 #include "content/public/test/test_launcher.h"
 #include "google_apis/gaia/gaia_id.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
+#include "ui/events/test/event_generator.h"
 
 namespace ash {
 
@@ -193,9 +195,9 @@ IN_PROC_BROWSER_TEST_F(PasswordChangeTest, SubmitOnEnterKeyPressed) {
   // Fill out and submit the old password, using "ENTER" key.
   test::PasswordChangedTypeOldPassword(
       test_user_info_.auth_config.online_password);
-  ASSERT_TRUE(ui_test_utils::SendKeyPressToWindowSync(
-      nullptr, ui::VKEY_RETURN, false /* control */, false /* shift */,
-      false /* alt */, false /* command */));
+
+  ui::test::EventGenerator generator(ash::Shell::GetPrimaryRootWindow());
+  generator.PressKey(ui::VKEY_RETURN, 0);
 
   test::CreatePasswordUpdateNoticePageWaiter()->Wait();
   test::PasswordUpdateNoticeExpectDone();
