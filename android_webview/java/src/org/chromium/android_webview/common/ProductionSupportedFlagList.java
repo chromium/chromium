3072829ae0a904ee3cb998e73fd73022007f3e31
Jihad Hanna <jihadghanna@google.com>
2025-01-08 03:56:19 -0800

    Cleanup AutofillDecoupleAutofillCountFromCache
    Bug: 40100455
    Change-Id: If3f3d668a2b9dba0a1ace03c2702ea59a212ea58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148136
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1403494}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -265,10 +265,6 @@ public final class ProductionSupportedFlagList {
                 AutofillFeatures.AUTOFILL_INFER_LABEL_FROM_DEFAULT_SELECT_TEXT,
                 "Considers the text of a <select> element's first <option> as a potential label"),
         Flag.baseFeature(
-                AutofillFeatures.AUTOFILL_DECOUPLE_AUTOFILL_COUNT_FROM_CACHE,
-                "Makes AutofillManager::GetCachedFormAndField return a form even if"
-                        + " form->autofill_count() == 0"),
-        Flag.baseFeature(
                 AutofillFeatures.AUTOFILL_DETECT_REMOVED_FORM_CONTROLS,
                 "Enables Autofill to detect if form controls are removed from the DOM"),
         Flag.baseFeature(
