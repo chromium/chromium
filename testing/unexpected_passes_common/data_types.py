0839685d4d5185a0c58d36c62cc6b0191b87dea2
Brian Sheedy <bsheedy@chromium.org>
2025-01-09 10:50:47 -0800

    Add UPF string registries
    Adds and uses registries for sharing string data between results and
    expectations. This results in significant memory usage improvements
    since each object only stores an int for each shared field instead of
    a copy of the string, which can be up to several hundred characters of
    Unicode in some cases.
    
    This CL only touches cases that can be transparently migrated using
    properties. There are a number of other potential places where this
    can be used, but those will be handled separately due to being more
    invasive.
    
    For the WebGPU use case, this appears to reduce the peak memory usage
    from ~72 GB to ~23 GB.
    
    Bug: 388307196
    Change-Id: Ia6d2fdf2ab6eba6702c2166f276cb2e7c85ad03c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6159984
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1404296}

--- testing/unexpected_passes_common/data_types.py
+++ testing/unexpected_passes_common/data_types.py
@@ -17,6 +17,9 @@ import six
 # //third_party/catapult/third_party/typ imports.
 from typ import expectations_parser
 
+# //testing imports.
+from unexpected_passes_common import registry
+
 FULL_PASS = 1
 NEVER_PASS = 2
 PARTIAL_PASS = 3
@@ -76,13 +79,15 @@ class BaseExpectation():
                tags: Iterable[str],
                expected_results: Union[str, Iterable[str]],
                bug: Optional[str] = None):
-    self.test = test
-    self.tags = frozenset(tags)
-    self.bug = bug or ''
+    self._test_id = registry.RegisterTestName(test)
+    self._tags_id = registry.RegisterTagSet(frozenset(tags))
+    self._bug_id = registry.RegisterBug(bug or '')
     if isinstance(expected_results, str):
-      self.expected_results = frozenset([expected_results])
+      expected_results = frozenset([expected_results])
     else:
-      self.expected_results = frozenset(expected_results)
+      expected_results = frozenset(expected_results)
+    self._expected_results_id = registry.RegisterExpectedResults(
+        expected_results)
 
     # We're going to be making a lot of comparisons, and fnmatch is *much*
     # slower (~40x from rough testing) than a straight comparison, so only use
@@ -102,7 +107,28 @@ class BaseExpectation():
     return not self.__eq__(other)
 
   def __hash__(self) -> int:
-    return hash((self.test, self.tags, self.expected_results, self.bug))
+    return hash(
+        (self._test_id, self._tags_id, self._expected_results_id, self._bug_id))
+
+  @property
+  def test(self) -> str:
+    return registry.RetrieveTestName(self._test_id)
+
+  @property
+  def tags(self) -> FrozenSet[str]:
+    return registry.RetrieveTagSet(self._tags_id)
+
+  @tags.setter
+  def tags(self, new_tags: FrozenSet[str]):
+    self._tags_id = registry.RegisterTagSet(new_tags)
+
+  @property
+  def bug(self) -> str:
+    return registry.RetrieveBug(self._bug_id)
+
+  @property
+  def expected_results(self) -> FrozenSet[str]:
+    return registry.RetrieveExpectedResults(self._expected_results_id)
 
   def _IsWildcard(self) -> bool:
     # This logic is the same as typ's expectation parser.
@@ -192,10 +218,11 @@ class BaseResult():
       build_id: A string containing the Buildbucket ID for the build this result
           came from.
     """
-    self.test = test
-    self.tags = frozenset(tags)
-    self.actual_result = actual_result
-    self.step = step
+    self._test_id = registry.RegisterTestName(test)
+    self._tags_id = registry.RegisterTagSet(frozenset(tags))
+    self._actual_result_id = registry.RegisterActualResult(actual_result)
+    self._step_id = registry.RegisterStep(step)
+    # TODO(crbug.com/388307196): Switch to using ints instead of strings.
     self.build_id = build_id
 
   def __eq__(self, other: Any) -> bool:
@@ -208,8 +235,24 @@ class BaseResult():
     return not self.__eq__(other)
 
   def __hash__(self) -> int:
-    return hash(
-        (self.test, self.tags, self.actual_result, self.step, self.build_id))
+    return hash((self._test_id, self._tags_id, self._actual_result_id,
+                 self._step_id, self.build_id))
+
+  @property
+  def test(self) -> str:
+    return registry.RetrieveTestName(self._test_id)
+
+  @property
+  def tags(self) -> FrozenSet[str]:
+    return registry.RetrieveTagSet(self._tags_id)
+
+  @property
+  def actual_result(self) -> str:
+    return registry.RetrieveActualResult(self._actual_result_id)
+
+  @property
+  def step(self) -> str:
+    return registry.RetrieveStep(self._step_id)
 
 
 class BaseBuildStats():
