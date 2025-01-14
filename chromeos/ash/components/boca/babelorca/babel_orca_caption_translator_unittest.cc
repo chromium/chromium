002f0b517251139dec869c485c147491fce76edf
Avynn Donaghe <avynn@google.com>
2025-01-14 08:31:47 -0800

    [BabelOrca] Ensure Caption Translation Ordering
    ensures that caption translations are dispatched to the caption
    bubble in the order that they were dispatched to the translation
    service.
    
    AX-Relnotes: N/A
    Bug: 377544551
    Change-Id: Ib12c236115ecb136b6d7d8dfe54d63e120cefe2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091636
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Reviewed-by: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406116}

--- chromeos/ash/components/boca/babelorca/babel_orca_caption_translator_unittest.cc
+++ chromeos/ash/components/boca/babelorca/babel_orca_caption_translator_unittest.cc
@@ -14,6 +14,7 @@
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/metrics_hashes.h"
 #include "base/run_loop.h"
+#include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/metrics/user_action_tester.h"
 #include "base/test/task_environment.h"
@@ -36,6 +37,11 @@ constexpr char kTranscriptionPartial[] =
     "The red fox jumped over the lazy brown dog.";
 constexpr char kTranscriptionFullFinal[] =
     "The red fox jumped over the lazy brown dog. Hello there! ";
+constexpr char kTranscriptionPartialWithMore[] =
+    "the red fox jumped over the lazy brown dog. Hello";
+constexpr char kOtherTranscription[] = "This is a second transcript!";
+constexpr char kOtherTranscriptionPartial[] = "This is a";
+constexpr char kOtherTranscriptionPartialMore[] = "this is a second";
 }  // namespace
 
 class BabelOrcaCaptionTranslatorTest : public testing::Test {
@@ -270,6 +276,146 @@ TEST_F(BabelOrcaCaptionTranslatorTest,
   EXPECT_EQ(
       actions.GetActionCount(boca::kBocaBabelorcaActionOfStudentSwitchLanguage),
       0);
+}
+
+// Tests that dispatches are done in order.
+TEST_F(BabelOrcaCaptionTranslatorTest, DispatchTranslationsInOrder) {
+  bool first_dispatch_handled = false;
+  bool second_dispatch_handled = false;
+  bool third_dispatch_handled = false;
+  bool fourth_dispatch_handled = false;
+  bool fifth_dispatch_handled = false;
+  bool sixth_dispatch_handled = false;
+  bool seventh_dispatch_handled = false;
+  base::OnceCallback<void()> first_bound_dispatch;
+  base::OnceCallback<void()> second_bound_dispatch;
+  base::OnceCallback<void()> third_bound_dispatch;
+  base::RunLoop first_run_loop;
+  base::RunLoop second_run_loop;
+  base::RunLoop third_run_loop;
+
+  // These first two Translate calls ensure that if a segment we're
+  // currently working on is updated the Translator will immediately
+  // dispatch the updated segment without enqueueing.
+  translation_dispatcher_->SetDispatchHandler(base::BindLambdaForTesting(
+      [&first_bound_dispatch](base::OnceCallback<void()> callback) {
+        first_bound_dispatch = std::move(callback);
+      }));
+  caption_translator_->Translate(
+      media::SpeechRecognitionResult(kOtherTranscriptionPartial,
+                                     /*is_final=*/false),
+      base::BindLambdaForTesting(
+          [&first_dispatch_handled](const media::SpeechRecognitionResult&) {
+            first_dispatch_handled = true;
+          }),
+      kDefaultSourceName, kIdeographicLanguage);
+
+  translation_dispatcher_->SetDispatchHandler(base::BindLambdaForTesting(
+      [&second_bound_dispatch](base::OnceCallback<void()> callback) {
+        second_bound_dispatch = std::move(callback);
+      }));
+  caption_translator_->Translate(
+      media::SpeechRecognitionResult(kOtherTranscriptionPartialMore,
+                                     /*is_final=*/false),
+      base::BindLambdaForTesting(
+          [&second_dispatch_handled](const media::SpeechRecognitionResult&) {
+            second_dispatch_handled = true;
+          }),
+      kDefaultSourceName, kIdeographicLanguage);
+
+  // We expect that these dispatches are bound, as they should both
+  // have skipped the queue since they're int he current segment.
+  // These will never be invoked however as the actual dispatcher
+  // cancels pending requests if a new one is passed to it.
+  ASSERT_FALSE(first_bound_dispatch.is_null());
+  ASSERT_FALSE(second_bound_dispatch.is_null());
+
+  translation_dispatcher_->SetDispatchHandler(base::BindLambdaForTesting(
+      [&third_bound_dispatch](base::OnceCallback<void()> callback) {
+        third_bound_dispatch = std::move(callback);
+      }));
+  caption_translator_->Translate(
+      media::SpeechRecognitionResult(kOtherTranscription, /*is_final=*/true),
+      base::BindLambdaForTesting([&third_dispatch_handled, &first_run_loop](
+                                     const media::SpeechRecognitionResult&) {
+        third_dispatch_handled = true;
+        first_run_loop.Quit();
+      }),
+      kDefaultSourceName, kIdeographicLanguage);
+
+  // we expect the third dispatch to be bound immediately.
+  ASSERT_FALSE(third_bound_dispatch.is_null());
+
+  // We should not affect the behavior of the subsequent calls to the dispatcher
+  // as we will pretend that the the latency between the server and the client
+  // has improved since the delayed dispatch of the first translate.
+  translation_dispatcher_->UnsetDispatchHandler();
+
+  // The subsequent calls to translate start work on a new segment.  The first
+  // segment will "complete" its request when we invoke the second bound
+  // dispatch below. Since the first two calls to translate contains a partial
+  // transcription of the final call we expect that they will be skipped and
+  // only the last call to translate will actually be dispatched.
+  caption_translator_->Translate(
+      media::SpeechRecognitionResult(kTranscriptionPartial, /*is_final=*/false),
+      base::BindLambdaForTesting(
+          [&fourth_dispatch_handled](const media::SpeechRecognitionResult&) {
+            fourth_dispatch_handled = true;
+          }),
+      kDefaultSourceName, kIdeographicLanguage);
+
+  caption_translator_->Translate(
+      media::SpeechRecognitionResult(kTranscriptionPartialWithMore,
+                                     /*is_final=*/false),
+      base::BindLambdaForTesting(
+          [&fifth_dispatch_handled](const media::SpeechRecognitionResult&) {
+            // The first dispatch should have completed before this one was
+            // handled.
+            fifth_dispatch_handled = true;
+          }),
+      kDefaultSourceName, kIdeographicLanguage);
+
+  // Note here that is_final is false, even though it is we still expect that
+  // this one will be handled as it is the most up to date segment that the
+  // translator has by the time it flushes the queue.
+  caption_translator_->Translate(
+      media::SpeechRecognitionResult(kTranscriptionFull, /*is_final=*/false),
+      base::BindLambdaForTesting([&sixth_dispatch_handled, &second_run_loop](
+                                     const media::SpeechRecognitionResult&) {
+        sixth_dispatch_handled = true;
+        second_run_loop.Quit();
+      }),
+      kDefaultSourceName, kIdeographicLanguage);
+
+  // At this point the first request is still pending, so we expect that none of
+  // the translations have been handled. When we invoke the third_bound_dispatch
+  // we then should expect that only the third and fifth callbacks were invoked.
+  // The rest were skipped because a more final transcription has come through.
+  std::move(third_bound_dispatch).Run();
+  first_run_loop.Run();
+  second_run_loop.Run();
+
+  // We only expect that the final dispatch in the first segment and the last
+  // segment in the queue to be dispatched.
+  EXPECT_FALSE(first_dispatch_handled);
+  EXPECT_FALSE(second_dispatch_handled);
+  EXPECT_TRUE(third_dispatch_handled);
+  EXPECT_FALSE(fourth_dispatch_handled);
+  EXPECT_FALSE(fifth_dispatch_handled);
+  EXPECT_TRUE(sixth_dispatch_handled);
+
+  // Ensure that the translator still dispatches if the last completed request
+  // was not final and the queue was empty.
+  caption_translator_->Translate(
+      media::SpeechRecognitionResult(kOtherTranscription, /*is_final=*/true),
+      base::BindLambdaForTesting([&seventh_dispatch_handled, &third_run_loop](
+                                     const media::SpeechRecognitionResult&) {
+        seventh_dispatch_handled = true;
+        third_run_loop.Quit();
+      }),
+      kDefaultSourceName, kIdeographicLanguage);
+  third_run_loop.Run();
+  EXPECT_TRUE(seventh_dispatch_handled);
 }
 
 }  // namespace ash::babelorca
--- chromeos/ash/components/boca/babelorca/fakes/fake_translation_dispatcher.cc
+++ chromeos/ash/components/boca/babelorca/fakes/fake_translation_dispatcher.cc
@@ -4,9 +4,20 @@
 
 #include "chromeos/ash/components/boca/babelorca/fakes/fake_translation_dispatcher.h"
 
+#include "base/functional/callback.h"
+#include "base/location.h"
 #include "components/live_caption/translation_util.h"
 
 namespace ash::babelorca {
+
+namespace {
+
+void InvokeFakeDispatch(captions::TranslateEventCallback callback,
+                        const std::string& result) {
+  std::move(callback).Run(base::ok(result));
+}
+
+}  // namespace
 
 FakeBabelOrcaTranslationDispatcher::FakeBabelOrcaTranslationDispatcher() =
     default;
@@ -19,13 +30,15 @@ void FakeBabelOrcaTranslationDispatcher::GetTranslation(
     const std::string& target_language,
     captions::TranslateEventCallback callback) {
   ++num_translation_calls_;
+  if (!dispatch_handler_.is_null()) {
+    dispatch_handler_.Run(base::BindOnce(&InvokeFakeDispatch,
+                                         std::move(callback),
+                                         injected_result_.value_or(result)));
+    return;
+  }
+
   std::move(callback).Run(
       captions::TranslateEvent(injected_result_.value_or(result)));
-}
-
-void FakeBabelOrcaTranslationDispatcher::InjectTranslationResult(
-    const std::string& translation) {
-  injected_result_ = translation;
 }
 
 }  // namespace ash::babelorca
