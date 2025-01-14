929b66b743b3eb9b759052f1209713147162d663
Vinny Persky <vinnypersky@google.com>
2025-01-14 15:03:20 -0800

    Add BNPL Init and Reset functionality, as well as refactoring
    This CL adds an init and reset functionality in BNPL manager, moves
    public methods that should be private into private methods, adds a new
    BnplManagerTestApi, and moves certain test functionality into the test
    API.
    
    Bug: 356443046, 378519110
    Change-Id: I2f99caf37013989a3484a7af9364d724ee2c197f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6143175
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1406391}

--- components/autofill/core/browser/payments/bnpl_manager.h
+++ components/autofill/core/browser/payments/bnpl_manager.h
@@ -22,10 +22,22 @@ struct BnplFetchVcnResponseDetails;
 // transaction.
 class BnplManager {
  public:
+  using OnBnplVcnFetchedCallback = base::OnceCallback<void(const CreditCard&)>;
+
   explicit BnplManager(PaymentsAutofillClient* payments_autofill_client);
   BnplManager(const BnplManager& other) = delete;
   BnplManager& operator=(const BnplManager& other) = delete;
   ~BnplManager();
+
+  // Initializes the BNPL flow, which includes UI shown to the user to select an
+  // issuer, a possible ToS dialog, and redirecting to the selected issuer's
+  // website before filling the form, if the flow succeeds.
+  // `final_checkout_amount` is the checkout amount extracted from the page (in
+  // micros). `on_bnpl_vcn_fetched_callback` is the callback that should be run
+  // if the flow is completed successfully, to fill the form with the VCN that
+  // will facilitate the BNPL transaction.
+  void InitBnplFlow(uint64_t final_checkout_amount,
+                    OnBnplVcnFetchedCallback on_bnpl_vcn_fetched_callback);
 
   // This function attempts to convert a string representation of a monetary
   // value in dollars into a uint64_t by parsing it as a double and multiplying
@@ -38,6 +50,43 @@ class BnplManager {
   std::optional<uint64_t> MaybeParseAmountToMonetaryMicroUnits(
       const std::string& amount);
 
+ private:
+  friend class BnplManagerTestApi;
+
+  // A collection of information that represents the state of an ongoing BNPL
+  // flow.
+  struct OngoingFlowState {
+    OngoingFlowState();
+    OngoingFlowState(const OngoingFlowState&) = delete;
+    OngoingFlowState& operator=(const OngoingFlowState&) = delete;
+    ~OngoingFlowState();
+
+    // Billing customer number for the user's Google Payments account.
+    std::string billing_customer_number;
+
+    // Risk data contains the fingerprint data for the user and the device.
+    std::string risk_data;
+
+    // BNPL Issuer Data - Populated when user selects a BNPL issuer
+    // Instrument ID used by the server to identify a specific BNPL issuer. This
+    // is selected by the user.
+    std::string instrument_id;
+
+    // Context token shared between client and Payments server.
+    std::string context_token;
+
+    // URL that the the partner redirected the user to after finishing the BNPL
+    // flow on the partner website.
+    GURL redirect_url;
+
+    // The final checkout amount on the page (in micros), used for the ongoing
+    // BNPL flow.
+    uint64_t final_checkout_amount;
+
+    // The callback that will fill the fetched BNPL VCN into the form.
+    OnBnplVcnFetchedCallback on_bnpl_vcn_fetched_callback;
+  };
+
   // This function makes the appropriate call to the payments server to fetch
   // the VCN details for the BNPL issuer selected in the BNPL manager.
   void FetchVcnDetails();
@@ -47,26 +96,12 @@ class BnplManager {
   void OnVcnDetailsFetched(PaymentsAutofillClient::PaymentsRpcResult result,
                            const BnplFetchVcnResponseDetails& response_details);
 
- private:
-  friend class BnplManagerTest;
-
   // The associated payments autofill client.
   const raw_ref<PaymentsAutofillClient> payments_autofill_client_;
 
-  // Billing customer number for the user's Google Payments account.
-  std::string billing_customer_number_;
-  // Risk data contains the fingerprint data for the user and the device.
-  std::string risk_data_;
-
-  // BNPL Issuer Data - Populated when user selects a BNPL issuer
-  // Instrument ID used by the server to identify a specific BNPL issuer. This
-  // is selected by the user.
-  std::string instrument_id_;
-  // Context token shared between client and Payments server.
-  std::string context_token_;
-  // URL that the the partner redirected the user to after finishing the BNPL
-  // flow on the partner website.
-  GURL redirect_url_;
+  // The state for the ongoing flow. Only present if there is a flow currently
+  // ongoing. Set when a flow is initiated, and reset upon flow completion.
+  std::unique_ptr<OngoingFlowState> ongoing_flow_state_;
 
   base::WeakPtrFactory<BnplManager> weak_factory_{this};
 };
