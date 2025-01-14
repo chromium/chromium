5811c417255b5467acc30385ebd6a97cdca168bc
Andreas Haas <ahaas@chromium.org>
2025-01-14 10:05:14 -0800

    [bindings] Remove special support for sequences in NoAllocDirectCall
    In V8, sequences (Local<Array>) are handled the same as Local<Value>
    parameters, so there is no reason to keep the special handling.
    
    R=caseq@chromium.org
    
    Bug: 41492790
    Change-Id: I4d9678201df72a196fe1bdd1166c55e3d2d874b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6174919
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andreas Haas <ahaas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406189}

--- third_party/blink/renderer/bindings/scripts/bind_gen/interface.py
+++ third_party/blink/renderer/bindings/scripts/bind_gen/interface.py
@@ -2013,17 +2013,10 @@ def nadc_parameter_v8_type_and_symbol_node(cg_context, argument, v8_arg_name,
         argument, web_idl.Attribute)
 
     unwrapped_idl_type = argument.idl_type.unwrap()
-    if "PassAsSpan" in argument.idl_type.effective_annotations:
+    if ("PassAsSpan" in argument.idl_type.effective_annotations
+            or unwrapped_idl_type.is_interface
+            or unwrapped_idl_type.is_sequence):
         return ("v8::Local<v8::Value>",
-                make_v8_to_blink_value(blink_arg_name,
-                                       "${{{}}}".format(v8_arg_name),
-                                       argument.idl_type,
-                                       argument=argument,
-                                       error_exit_return_statement="return;",
-                                       cg_context=cg_context))
-    if unwrapped_idl_type.is_interface or unwrapped_idl_type.is_sequence:
-        return ("v8::Local<v8::Value>"
-                if unwrapped_idl_type.is_interface else "v8::Local<v8::Array>",
                 make_v8_to_blink_value(blink_arg_name,
                                        "${{{}}}".format(v8_arg_name),
                                        argument.idl_type,
