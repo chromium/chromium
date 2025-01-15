667ae5eb4d4123bdd53eb9fe862eac7ac2107061
Julian Pastarmov <pastarmovj@google.com>
2025-01-15 03:34:46 -0800

    Clean up adjustments for alt versions
    Bug: none
    Change-Id: Ibe381a87d534afd47f90bbf2b26d8d1efd77a889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6163882
    Reviewed-by: Edward Jung <edwardjung@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406620}

--- components/error_page/common/alt_game_images.cc
+++ components/error_page/common/alt_game_images.cc
@@ -19,104 +19,21 @@
 #include "base/strings/string_util.h"
 #include "components/error_page/common/alt_game_image_data.h"
 #include "components/error_page/common/error_page_switches.h"
-#include "crypto/encryptor.h"
-#include "crypto/symmetric_key.h"
 
 namespace error_page {
-namespace {
-
-std::string DecryptImage(const std::string& image_ciphertext,
-                         crypto::SymmetricKey* key) {
-  constexpr int kBlockSize = 16;
-  crypto::Encryptor encryptor;
-  // *Never* use this pattern for encrypting anything that matters! There are
-  // many problems with this, but it's just obfuscation so it doesn't matter.
-  if (!encryptor.Init(key, crypto::Encryptor::Mode::CBC,
-                      /*iv=*/key->key().substr(0, kBlockSize))) {
-    DLOG(ERROR) << "Failed to initialize encryptor.";
-    return std::string();
-  }
-
-  std::string image_plaintext;
-  if (!encryptor.Decrypt(image_ciphertext, &image_plaintext)) {
-    DLOG(ERROR) << "Failed to decrypt image.";
-    return std::string();
-  }
-  return image_plaintext;
-}
-
-std::unique_ptr<crypto::SymmetricKey> GetKey() {
-  std::string b64_key = kNetErrorAltGameModeKey.Get();
-  if (b64_key.empty()) {
-    DLOG(ERROR) << "No image encryption key present.";
-    return nullptr;
-  }
-
-  // Note: key is base64url-encoded because the default base64 format includes
-  // the '/' character which interferes with setting the param from the command
-  // line for testing.
-  std::string raw_key;
-  if (!base::Base64UrlDecode(
-          b64_key, base::Base64UrlDecodePolicy::IGNORE_PADDING, &raw_key)) {
-    DLOG(ERROR) << "Failed to base64-decode image encryption key.";
-    return nullptr;
-  }
-
-  std::unique_ptr<crypto::SymmetricKey> key = crypto::SymmetricKey::Import(
-      crypto::SymmetricKey::Algorithm::AES, raw_key);
-  if (!key) {
-    DLOG(ERROR) << "Invalid image encryption key: wrong length?";
-    return nullptr;
-  }
-  return key;
-}
-
-}  // namespace
-
-BASE_FEATURE(kNetErrorAltGameMode,
-             "NetErrorAltGameMode",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-const base::FeatureParam<std::string> kNetErrorAltGameModeKey{
-    &kNetErrorAltGameMode, "Key", ""};
 
 bool EnableAltGameMode() {
-  return base::FeatureList::IsEnabled(kNetErrorAltGameMode) &&
-         base::CommandLine::ForCurrentProcess()->HasSwitch(
-             error_page::switches::kEnableDinosaurEasterEggAltGameImages);
+  return false;
 }
 
 std::string GetAltGameImage(int image_id, int scale) {
-  std::string encrypted_image;
-  if (!::error_page::LookupObfuscatedImage(image_id, scale, &encrypted_image)) {
-    DLOG(ERROR) << "Couldn't find alt game image with ID=" << image_id
-                << " and scale=" << scale << "x.";
-    return std::string();
-  }
-
-  std::unique_ptr<crypto::SymmetricKey> key = GetKey();
-  if (!key) {
-    DLOG(ERROR) << "Failed to load alt game image encryption key.";
-    return std::string();
-  }
-
-  std::string image = DecryptImage(encrypted_image, key.get());
-  if (image.empty()) {
-    DLOG(ERROR) << "Failed to decrypt alt game image " << image_id << " at "
-                << scale << "x scale.";
-    return std::string();
-  }
-
-  if (!base::StartsWith(image, "data:image/png;base64,")) {
-    DLOG(ERROR) << "Decrypted data doesn't look like an image data URL.";
-    return std::string();
-  }
-
-  return image;
+  NOTIMPLEMENTED();
+  return std::string();
 }
 
 int ChooseAltGame() {
-  // Image 0 should be "common".
-  return base::RandInt(1, CountAlternateImages() - 1);
+  NOTIMPLEMENTED();
+  return 0;
 }
 
 }  // namespace error_page
