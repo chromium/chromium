053e33cd47a8b6db780c0c9b325ccbe2c501be5b
Viplav Kadam <viplavkadam@google.com>
2025-01-14 17:41:25 -0800

    [Benefits][Chrome] Enable kAutofillEnableCardBenefitsForAmericanExpress
    This CL enables kAutofillEnableCardBenefitsForAmericanExpress by
    default for the desktop launch.
    
    Bug: 388535273
    Change-Id: Idc074987292a176dd3ad6d4c4b1cd417eef42ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6157864
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406460}

--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -41,7 +41,11 @@ BASE_FEATURE(kAutofillEnableBuyNowPayLaterSyncing,
 // Payments Autofill UI.
 BASE_FEATURE(kAutofillEnableCardBenefitsForAmericanExpress,
              "AutofillEnableCardBenefitsForAmericanExpress",
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
              base::FEATURE_DISABLED_BY_DEFAULT);
+#else
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
 
 // When enabled, card benefits offered by BMO will be shown in Payments Autofill
 // UI.
