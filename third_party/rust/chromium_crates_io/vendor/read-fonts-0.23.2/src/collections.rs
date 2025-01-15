73cd7c0aef2a9ed247c57541be1c79ef226e3a28
Dominik Röttsches <drott@chromium.org>
2025-01-15 04:36:30 -0800

    [Cherry-pick] Roll Fontations, Skrifa to 0.26.3
    Addresses issues in processing hdmx advance width information,
    see issue chromium:387516734.
    
    Updated crates:
    
    * font-types: 0.7.3 => 0.8.2
    * read-fonts: 0.23.2 => 0.25.3
    * skrifa: 0.24.1 => 0.26.3
    
    read-fonts and skrifa vetted for ub-risk-0, does-not-implement-cryto, safe-to-deploy.
    font-types vetted for ub-risk-1, does-not-implement-cryto, safe-to-deploy.
    
    (cherry picked from commit 602da45ed0c9beff880aecf9b72519349cc2d984)
    
    Bug: chromium:40045339, chromium:387516734, chromium:389016643
    Change-Id: Ia2390f0a79210a10eebd574a8276c573e63aa400
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6162416
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1404167}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176739
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6943@{#245}
    Cr-Branched-From: 72dd0b377c099e1e0230cc7345d5a5125b46ae7d-refs/heads/main@{#1402768}

--- third_party/rust/chromium_crates_io/vendor/read-fonts-0.23.2/src/collections.rs
+++
@@ -1,4 +0,0 @@
-//! Data structures useful for font work.
-
-pub mod int_set;
-pub use int_set::IntSet;
---
+++ third_party/rust/chromium_crates_io/vendor/read-fonts-0.25.3/src/collections.rs
@@ -0,0 +1,7 @@
+//! Data structures useful for font work.
+
+pub mod int_set;
+pub use int_set::IntSet;
+
+mod range_set;
+pub use range_set::RangeSet;
