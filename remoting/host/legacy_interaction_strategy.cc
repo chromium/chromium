6bb75629507a10a902a961734936990a15a71149
Erik Jensen <rkjnsn@google.com>
2025-01-15 13:39:54 -0800

    Create LegacyInteractionStrategy copying existing logic.
    This creates a LegacyInteractionStrategy implementation of
    DesktopInteractionStrategy that copies the existing logic from
    *DesktopEnvironment (including #ifs and calls to static Create()
    methods), and thus supports existing platforms.
    
    In the future, the hope is to replace this class and the static Create()
    methods with a dedicated implementation per platform.
    
    Bug: 378932951
    Change-Id: I2a1f632abab1af20dc746732370885358ebd4880
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6085578
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1406955}

---
+++ remoting/host/legacy_interaction_strategy.cc
@@ -0,0 +1,215 @@
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/legacy_interaction_strategy.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/check.h"
+#include "base/functional/bind.h"
+#include "base/functional/callback.h"
+#include "base/location.h"
+#include "base/memory/ptr_util.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/task/sequenced_task_runner.h"
+#include "base/task/single_thread_task_runner_thread_mode.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
+#include "build/buildflag.h"
+#include "remoting/host/action_executor.h"
+#include "remoting/host/active_display_monitor.h"
+#include "remoting/host/audio_capturer.h"
+#include "remoting/host/base/desktop_environment_options.h"
+#include "remoting/host/desktop_and_cursor_conditional_composer.h"
+#include "remoting/host/desktop_capturer_proxy.h"
+#include "remoting/host/desktop_capturer_wrapper.h"
+#include "remoting/host/desktop_display_info_monitor.h"
+#include "remoting/host/desktop_interaction_strategy.h"
+#include "remoting/host/desktop_resizer.h"
+#include "remoting/host/input_injector.h"
+#include "remoting/host/input_monitor/local_input_monitor.h"
+#include "remoting/host/keyboard_layout_monitor.h"
+#include "remoting/host/mouse_cursor_monitor_proxy.h"
+#include "remoting/protocol/desktop_capturer.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capture_options.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capture_types.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capturer.h"
+#include "third_party/webrtc/modules/desktop_capture/mouse_cursor_monitor.h"
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "remoting/host/chromeos/frame_sink_desktop_capturer.h"
+#include "remoting/host/chromeos/mouse_cursor_monitor_aura.h"
+#endif
+
+#if BUILDFLAG(IS_LINUX)
+#include "remoting/host/linux/wayland_utils.h"
+#endif
+
+namespace remoting {
+
+LegacyInteractionStrategy::~LegacyInteractionStrategy() {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+}
+
+std::unique_ptr<ActionExecutor>
+LegacyInteractionStrategy::CreateActionExecutor() {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  return ActionExecutor::Create();
+}
+
+std::unique_ptr<AudioCapturer>
+LegacyInteractionStrategy::CreateAudioCapturer() {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  return AudioCapturer::Create();
+}
+
+std::unique_ptr<InputInjector>
+LegacyInteractionStrategy::CreateInputInjector() {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  return InputInjector::Create(input_task_runner_, ui_task_runner_);
+}
+
+std::unique_ptr<DesktopResizer>
+LegacyInteractionStrategy::CreateDesktopResizer() {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  return DesktopResizer::Create();
+}
+
+std::unique_ptr<DesktopCapturer> LegacyInteractionStrategy::CreateVideoCapturer(
+    webrtc::ScreenId id) {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  scoped_refptr<base::SingleThreadTaskRunner> capture_task_runner;
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  capture_task_runner = ui_task_runner_;
+#elif BUILDFLAG(IS_LINUX)
+  if (IsRunningWayland()) {
+    // Each capturer instance should get its own thread so the capturers don't
+    // compete with each other in multistream mode.
+    capture_task_runner = base::ThreadPool::CreateSingleThreadTaskRunner(
+        {base::TaskPriority::HIGHEST},
+        base::SingleThreadTaskRunnerThreadMode::DEDICATED);
+  } else {
+    capture_task_runner = video_capture_task_runner_;
+  }
+#else
+  // The mouse cursor monitor runs on the |video_capture_task_runner_| so the
+  // desktop capturer also needs to run on that task_runner for certain
+  // platforms. For example, if we run the desktop capturer on a different
+  // thread on Windows, the cursor shape won't be captured when in GDI mode.
+  capture_task_runner = video_capture_task_runner_;
+#endif  // !BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_LINUX)
+
+#if defined(REMOTING_USE_X11)
+  if (!IsRunningWayland()) {
+    // Workaround for http://crbug.com/1361502: Run each capturer (and
+    // mouse-cursor-monitor) on a separate X11 Display.
+    auto new_options = webrtc::DesktopCaptureOptions::CreateDefault();
+    options_.desktop_capture_options()->set_x_display(
+        std::move(new_options.x_display()));
+    options_.desktop_capture_options()->x_display()->IgnoreXServerGrabs();
+  }
+#endif  // REMOTING_USE_X11
+
+  std::unique_ptr<DesktopCapturer> desktop_capturer;
+  if (options_.capture_video_on_dedicated_thread()) {
+    auto desktop_capturer_wrapper = std::make_unique<DesktopCapturerWrapper>();
+    desktop_capturer_wrapper->CreateCapturer(
+        *options_.desktop_capture_options(), id);
+    desktop_capturer = std::move(desktop_capturer_wrapper);
+  } else {
+    auto desktop_capturer_proxy =
+        std::make_unique<DesktopCapturerProxy>(std::move(capture_task_runner));
+    desktop_capturer_proxy->CreateCapturer(*options_.desktop_capture_options(),
+                                           id);
+    desktop_capturer = std::move(desktop_capturer_proxy);
+  }
+
+#if BUILDFLAG(IS_APPLE)
+  // Mac includes the mouse cursor in the captured image in curtain mode.
+  if (options_.enable_curtaining()) {
+    return desktop_capturer;
+  }
+#endif
+  return std::make_unique<DesktopAndCursorConditionalComposer>(
+      std::move(desktop_capturer));
+}
+
+std::unique_ptr<DesktopDisplayInfoMonitor>
+LegacyInteractionStrategy::CreateDisplayInfoMonitor() {
+  return std::make_unique<DesktopDisplayInfoMonitor>(ui_task_runner_);
+}
+
+std::unique_ptr<webrtc::MouseCursorMonitor>
+LegacyInteractionStrategy::CreateMouseCursorMonitor() {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  return std::make_unique<MouseCursorMonitorProxy>(
+      video_capture_task_runner_, *options_.desktop_capture_options());
+}
+
+std::unique_ptr<KeyboardLayoutMonitor>
+LegacyInteractionStrategy::CreateKeyboardLayoutMonitor(
+    base::RepeatingCallback<void(const protocol::KeyboardLayout&)> callback) {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  return KeyboardLayoutMonitor::Create(std::move(callback), input_task_runner_);
+}
+
+std::unique_ptr<ActiveDisplayMonitor>
+LegacyInteractionStrategy::CreateActiveDisplayMonitor(
+    base::RepeatingCallback<void(webrtc::ScreenId)> callback) {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  return ActiveDisplayMonitor::Create(ui_task_runner_, std::move(callback));
+}
+
+std::unique_ptr<LocalInputMonitor>
+LegacyInteractionStrategy::CreateLocalInputMonitor() {
+  DCHECK(caller_task_runner_->BelongsToCurrentThread());
+
+  return LocalInputMonitor::Create(caller_task_runner_, input_task_runner_,
+                                   ui_task_runner_);
+}
+
+LegacyInteractionStrategy::LegacyInteractionStrategy(
+    const DesktopEnvironmentOptions& options,
+    scoped_refptr<base::SingleThreadTaskRunner> caller_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> video_capture_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> input_task_runner)
+    : options_(options),
+      caller_task_runner_(std::move(caller_task_runner)),
+      ui_task_runner_(std::move(ui_task_runner)),
+      video_capture_task_runner_(std::move(video_capture_task_runner)),
+      input_task_runner_(std::move(input_task_runner)) {}
+
+LegacyInteractionStrategyFactory::LegacyInteractionStrategyFactory(
+    scoped_refptr<base::SingleThreadTaskRunner> caller_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> video_capture_task_runner,
+    scoped_refptr<base::SingleThreadTaskRunner> input_task_runner)
+    : caller_task_runner_(std::move(caller_task_runner)),
+      ui_task_runner_(std::move(ui_task_runner)),
+      video_capture_task_runner_(std::move(video_capture_task_runner)),
+      input_task_runner_(std::move(input_task_runner)) {}
+
+LegacyInteractionStrategyFactory::~LegacyInteractionStrategyFactory() = default;
+
+void LegacyInteractionStrategyFactory::Create(
+    const DesktopEnvironmentOptions& options,
+    CreateCallback callback) {
+  auto session = base::WrapUnique(new LegacyInteractionStrategy(
+      options, caller_task_runner_, ui_task_runner_, video_capture_task_runner_,
+      input_task_runner_));
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), std::move(session)));
+}
+
+}  // namespace remoting
