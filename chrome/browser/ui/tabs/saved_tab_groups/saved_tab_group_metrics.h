4e61e9df9ee1568cc7b20ab429c50fc5e24d3c07
Mickey Burks <mickeyburks@chromium.org>
2025-01-15 12:05:55 -0800

    [TabGroupSync] Emit metric when Recent Activity is opened
    Bug: 390146151
    Change-Id: I459b2bca652001da964fd7aba7dc6328ec9181cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176614
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1406888}

--- chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_metrics.h
+++ chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_metrics.h
@@ -30,7 +30,8 @@ enum class SharedTabGroupManageTypeDesktop {
   kShareGroup = 0,
   kManageGroup = 1,
   kDeleteGroup = 2,
-  kMaxValue = kDeleteGroup
+  kRecentActivity = 3,
+  kMaxValue = kRecentActivity
 };
 // LINT.ThenChange(//tools/metrics/histograms/metadata/tab/enums.xml:SharedTabGroupManageTypeDesktop)
 
