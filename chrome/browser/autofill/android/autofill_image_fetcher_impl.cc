238f4efe973b6ca3ed87dbcb277573d61727a389
Vishwas Uppoor <vishwasuppoor@google.com>
2025-01-14 19:59:17 -0800

    [PIX] AutofillImageFetcher refactor - Phase 1
    Introduce Pix specific API. It's a no-op.
    go/autofill-image-fetcher-refactor
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR: Tests will be added when the native API for Pix is added.
    Bug: 388216550, 388217006
    Change-Id: Ia45fe64844e92b759b1a42d21070e3697881027d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6151616
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1406500}

--- chrome/browser/autofill/android/autofill_image_fetcher_impl.cc
+++ chrome/browser/autofill/android/autofill_image_fetcher_impl.cc
@@ -30,6 +30,16 @@ void AutofillImageFetcherImpl::FetchImagesForURLs(
   }
 
   JNIEnv* env = base::android::AttachCurrentThread();
+
+  // For Pix, use the new API.
+  if (image_sizes.size() == 1 &&
+      image_sizes[0] == AutofillImageFetcherBase::ImageSize::kSquare) {
+    Java_AutofillImageFetcher_prefetchPixAccountImages(
+        env, GetOrCreateJavaImageFetcher(), image_urls);
+    return;
+  }
+
+  // TODO(crbug.com/388217006): Move all image fetch calls to the new API.
   std::vector<int> image_sizes_vector;
   std::transform(image_sizes.begin(), image_sizes.end(),
                  std::back_inserter(image_sizes_vector),
