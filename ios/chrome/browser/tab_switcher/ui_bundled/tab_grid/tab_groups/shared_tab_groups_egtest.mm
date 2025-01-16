8129f7fad27c48cda0da443814559fa760721446
Gauthier Ambard <gambard@chromium.org>
2025-01-16 06:06:22 -0800

    [iOS] Reenable Shared Tab Group join tests
    This CL adds a way to add a mock response to the PreviewServerProxy
    when the data sharing service is requesting preview of a group.
    
    Bug: 380113830
    Change-Id: I1dde6b57b1e50e86a5aeeb03f4c21db4a12759d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176738
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407270}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_egtest.mm
@@ -15,6 +15,7 @@
 #import "ios/chrome/browser/share_kit/model/test_constants.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
+#import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_group_sync_earl_grey_app_interface.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_eg_utils.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/test/query_title_server_util.h"
@@ -307,8 +308,8 @@ void ShareGroupAtIndex(int index) {
 }
 
 // Checks that the user with JoinOnly can trigger the Join flow.
-// TODO(crbug.com/380113830): Re-enable this test.
-- (void)DISABLED_testJoinGroup {
+- (void)testJoinGroup {
+  [TabGroupSyncEarlGreyAppInterface mockSharedEntitiesPreview];
   GURL joinGroupURL = data_sharing::GetDataSharingUrl(data_sharing::GroupToken(
       data_sharing::GroupId("resources%2F3be"), "CggHBicxA_slvx"));
   [ChromeEarlGrey loadURL:joinGroupURL waitForCompletion:NO];
@@ -409,8 +410,8 @@ void ShareGroupAtIndex(int index) {
 }
 
 // Checks that the user with JoinOnly can trigger the Join flow.
-// TODO(crbug.com/380113830): Re-enable this test.
-- (void)DISABLED_testJoinGroup {
+- (void)testJoinGroup {
+  [TabGroupSyncEarlGreyAppInterface mockSharedEntitiesPreview];
   GURL joinGroupURL = data_sharing::GetDataSharingUrl(data_sharing::GroupToken(
       data_sharing::GroupId("resources%2F3be"), "CggHBicxA_slvx"));
   [ChromeEarlGrey loadURL:joinGroupURL waitForCompletion:NO];
