1e8cc29beca8ec3d105cc99d6ccc1b9f546b5065
Jérôme Lebel <jlebel@chromium.org>
2025-01-16 06:50:50 -0800

    [iOS] Disable SeparateProfilesFRETestCase/testSignInWithManagedAccount
    This test fails on simulator.
    
    Bug: 390383517
    Change-Id: Id4ba7f06e87b30b25b3df29aa61fb624d8a34373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176979
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407290}

--- ios/chrome/browser/authentication/ui_bundled/separate_profiles_egtest.mm
+++ ios/chrome/browser/authentication/ui_bundled/separate_profiles_egtest.mm
@@ -242,6 +242,12 @@ id<GREYMatcher> PromoDeclineButtonMatcher() {
 
 // Tests signing in with a managed account during the FRE. This should convert
 // the existing profile to a managed profile.
+// TODO(crbug.com/390383517): Fails on simulator.
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_testSignInWithManagedAccount FLAKY_testSignInWithManagedAccount
+#else
+#define MAYBE_testSignInWithManagedAccount testSignInWithManagedAccount
+#endif
 - (void)testSignInWithManagedAccount {
   FakeSystemIdentity* const managedIdentity =
       [FakeSystemIdentity fakeManagedIdentity];
