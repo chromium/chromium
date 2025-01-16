5f82af5a07572051f71bf0d66d1c5a877ce2454d
Kristi Saney <kristislee@google.com>
2025-01-15 18:01:17 -0800

    [Read anything] Fix and add tests for the images toggle
    The initial count was what was causing the flakiness before.
    
    Fixed: 378139912
    Change-Id: I3f8db88ea6694968d6d255f130bc9b15648a82dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177969
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1407071}

--- chrome/browser/resources/side_panel/read_anything/read_anything.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything.ts
@@ -24,7 +24,7 @@ export type {SimpleActionMenu} from './menus/simple_action_menu.js';
 export {MetricsBrowserProxy, MetricsBrowserProxyImpl, ReadAloudHighlightState, ReadAloudSettingsChange, ReadAnythingNewPage, ReadAnythingSettingsChange, ReadAnythingSpeechError, ReadAnythingVoiceType} from './metrics_browser_proxy.js';
 export {ReadAnythingLogger, SpeechControls, TimeFrom, TimeTo} from './read_anything_logger.js';
 export type {ReadAnythingToolbarElement} from './read_anything_toolbar.js';
-export {IMAGES_TOGGLE_BUTTON_ID,LINK_TOGGLE_BUTTON_ID, LINKS_DISABLED_ICON, LINKS_ENABLED_ICON, moreOptionsClass} from './read_anything_toolbar.js';
+export {IMAGES_DISABLED_ICON, IMAGES_ENABLED_ICON, IMAGES_TOGGLE_BUTTON_ID, LINK_TOGGLE_BUTTON_ID, LINKS_DISABLED_ICON, LINKS_ENABLED_ICON, moreOptionsClass} from './read_anything_toolbar.js';
 export {AVAILABLE_GOOGLE_TTS_LOCALES, convertLangOrLocaleForVoicePackManager, convertLangOrLocaleToExactVoicePackLocale, convertLangToAnAvailableLangIfPresent, createInitialListOfEnabledLanguages, getFilteredVoiceList, getNotification, mojoVoicePackStatusToVoicePackStatusEnum, NotificationType, PACK_MANAGER_SUPPORTED_LANGS_AND_LOCALES, VoiceClientSideStatusCode, VoicePackServerStatusErrorCode, VoicePackServerStatusSuccessCode} from './voice_language_util.js';
 export {VoiceNotificationListener, VoiceNotificationManager} from './voice_notification_manager.js';
 export type {VoiceSelectionMenuElement} from './voice_selection_menu.js';
