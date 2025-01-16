c659aee3c166ecbf47ff73470afa5e896ace1615
Olivier ROBIN <olivierrobin@google.com>
2025-01-16 03:09:20 -0800

    Enable kShareInWebContextMenuIOS by default
    The launch was approved
    
    Change-Id: I19c77fb70d3ae0fa335fa6ffc1a5778ae894ce1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175173
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1407220}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -171,7 +171,7 @@ BASE_FEATURE(kSharedHighlightingIOS,
 
 BASE_FEATURE(kShareInWebContextMenuIOS,
              "ShareInWebContextMenuIOS",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kModernTabStrip,
              "ModernTabStrip",
