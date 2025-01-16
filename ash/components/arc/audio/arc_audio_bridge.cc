d6e254cc0cd1af0648eba8ca9e3884cba95306c4
Pattara Teerapong <pteerapong@chromium.org>
2025-01-15 20:30:18 -0800

    [M133] arc: Do not send audio switch state on boot
    This revert the previous change in CL:6109881 because it is causing CTS
    failure.
    
    Note that the CTS failure still occurs if ARC detects that the headset
    is plugged in, but we previously didn't sync this status on boot. This
    is a temporary fix while we investigate the real root cause.
    
          headset is plugged in.
    (cherry picked from commit 6362ef1da400d75328ab5570eb7fa87dd10f9d7b)
    
    Bug: 387837224
    Test: locally built, deploy to dut, test that the CTS pass if 3.5mm
    Change-Id: Ida286f8206d3b0b077eda6352c85704286817f6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6158505
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Auto-Submit: Pattara Teerapong <pteerapong@chromium.org>
    Commit-Queue: Pattara Teerapong <pteerapong@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1404097}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6176618
    Cr-Commit-Position: refs/branch-heads/6943@{#293}
    Cr-Branched-From: 72dd0b377c099e1e0230cc7345d5a5125b46ae7d-refs/heads/main@{#1402768}

--- ash/components/arc/audio/arc_audio_bridge.cc
+++ ash/components/arc/audio/arc_audio_bridge.cc
@@ -73,7 +73,6 @@ void ArcAudioBridge::OnConnectionReady() {
   // TODO(hidehiko): Replace with ConnectionHolder::IsConnected().
   available_ = true;
 
-  SendAudioNodesState();
   SendSpatialAudioState();
 }
 
