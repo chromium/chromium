c40daced4081bc3a1dfcd35995b3b544591df741
Emily Shack <emshack@chromium.org>
2025-01-16 12:27:59 -0800

    Listen for visual changes while editing tab group
    Bug: 334965868
    Change-Id: I60633e0c2aa03d2245302ce5df17ca13b4ac1529
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6178700
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1407511}

--- chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h
+++ chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.h
@@ -10,6 +10,7 @@
 #include <vector>
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/tabs/tab_group_header.h"
 #include "components/tab_groups/tab_group_color.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -36,7 +37,8 @@ class ColorPickerView;
 class TabGroupHeader;
 
 // A dialog for changing a tab group's visual parameters.
-class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView {
+class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView,
+                                 public TabStripModelObserver {
   METADATA_HEADER(TabGroupEditorBubbleView, views::BubbleDialogDelegateView)
 
  public:
@@ -79,6 +81,9 @@ class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView {
                            std::optional<gfx::Rect> anchor_rect,
                            bool stop_context_menu_propagation);
   ~TabGroupEditorBubbleView() override;
+
+  // TabStripModelObserver:
+  void OnTabGroupChanged(const TabGroupChange& change) override;
 
   void UpdateGroup();
   const std::u16string GetTextForCloseButton() const;
@@ -187,6 +192,7 @@ class TabGroupEditorBubbleView : public views::BubbleDialogDelegateView {
     bool stop_context_menu_propagation_;
   };
   std::unique_ptr<TitleField> BuildTitleField(const std::u16string& title);
+  std::u16string GetGroupTitle();
 
   class Footer : public views::View {
     METADATA_HEADER(Footer, views::View)
