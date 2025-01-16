bb04eed0981302fd97cbf20e7913ac52bb276f3d
Sky Malice <skym@chromium.org>
2025-01-15 17:25:45 -0800

    Remove level-list indirection from recent tabs bg.
    There seems to be some reused Drawable bug somewhere that sometimes
    causes the background of the icon for rows to not show. The old drawable
    list_item_icon_modern_bg was a level-list, allowing easy switching
    between different drawables. However this use case only ever used the
    first/default one, oval_surface_1. By removing this indirection and just
    directly using oval_surface_1, this bug seems to go away.
    
    While making this change, moved the logic from the java file to the
    layout file.
    
    Change-Id: Idb218b82ff6ba22d2ca192a8f04c60262f916c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6177564
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1407057}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/RecentTabsRowAdapter.java
@@ -184,7 +184,6 @@ public class RecentTabsRowAdapter extends BaseExpandableListAdapter {
                 viewHolder.textView = childView.findViewById(R.id.title_row);
                 viewHolder.domainView = childView.findViewById(R.id.domain_row);
                 viewHolder.imageView = childView.findViewById(R.id.recent_tabs_favicon);
-                viewHolder.imageView.setBackgroundResource(R.drawable.list_item_icon_modern_bg);
                 viewHolder.itemLayout = childView.findViewById(R.id.recent_tabs_list_item_layout);
                 childView.setTag(viewHolder);
             }
