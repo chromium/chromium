1e04097a429fdaed4de3cbfb5bec86fcb19aa121
Anuj Kumar <anujbh@google.com>
2025-01-16 18:44:45 -0800

    Fix ChromeWebView crash on dereferencing nullptr
    Root Cause: The PaymentsAutofillClient for CWV is explicitly setting VirtualCardManager to nullptr (see code reference). Later in the card save flow, the code attempts to access VirtualCardManager, leading to a crash when it tries to dereference this null pointer.
    
    VirtualCardManager is set to nullptr for CWV clients here: https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/core/browser/payments/payments_autofill_client.cc;l=171?q=PaymentsAutofillClient::GetVirtualCardEnrollmentManager()%20%7B&ss=chromium%2Fchromium%2Fsrc
    
    Impact: This is a critical issue (P0 crash) as it completely blocks users on CWV from saving cards.
    More details: https://docs.google.com/document/d/1_lUTProdmt9J0lxuIS3_eSN43TpKKthzuBba4v8G0hQ/edit?tab=t.0
    
    This is reproducible in the CWV shell app too.
    
    (cherry picked from commit 6e337c7013f4d7c636adea91b0786ec2a1aa1797)
    
    Bug: 390458335
    Change-Id: I860a8c2e25729ecec6f6e96349cc08684aff1b9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6173766
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Anuj Kumar <anujbh@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1406963}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179639
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6834@{#3866}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- components/autofill/core/browser/payments/credit_card_save_manager.cc
+++ components/autofill/core/browser/payments/credit_card_save_manager.cc
@@ -534,11 +534,13 @@ void CreditCardSaveManager::InitVirtualCardEnroll(
   // Hides save card confirmation dialog if still showing.
   client_->GetPaymentsAutofillClient()->HideSaveCardPrompt();
 
-  client_->GetPaymentsAutofillClient()
-      ->GetVirtualCardEnrollmentManager()
+  if (auto* virtual_card_enrollment_manager =
+      client_->GetPaymentsAutofillClient()->GetVirtualCardEnrollmentManager()) {
+    virtual_card_enrollment_manager
       ->InitVirtualCardEnroll(
-          credit_card, VirtualCardEnrollmentSource::kUpstream,
-          std::move(get_details_for_enrollment_response_details));
+        credit_card, VirtualCardEnrollmentSource::kUpstream,
+        std::move(get_details_for_enrollment_response_details));
+  }
 }
 
 CreditCardSaveStrikeDatabase*
@@ -1239,9 +1241,12 @@ void CreditCardSaveManager::OnUserDidAcceptUploadHelper(
         user_provided_card_details.expiration_date_year, app_locale_);
   }
 
-  client_->GetPaymentsAutofillClient()
-      ->GetVirtualCardEnrollmentManager()
+  // Virtual card enrollment manager may not be set of CWV clients.
+  if (auto* virtual_card_enrollment_manager =
+      client_->GetPaymentsAutofillClient()->GetVirtualCardEnrollmentManager()) {
+    virtual_card_enrollment_manager
       ->SetSaveCardBubbleAcceptedTimestamp(AutofillClock::Now());
+  }
 
   // Populating risk data and offering upload occur asynchronously.
   // If |risk_data| has already been loaded, send the upload card request.
