a195a756688327cc887b09a40cf3e920ac8f6fc2
Darren Shen <shend@chromium.org>
2025-01-19 15:11:49 -0800

    Enable PickerGifs by default.
    Bug: b:368442959
    Change-Id: I8fb47caf8c19a384834fc586094851232bbe14c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6166261
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1408472}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2307,7 +2307,7 @@ BASE_FEATURE(kPhoneHubShortQuickActionPodsTitles,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables GIF search in Picker.
-BASE_FEATURE(kPickerGifs, "PickerGifs", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kPickerGifs, "PickerGifs", base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPipDoubleTapToResize,
              "PipDoubleTapToResize",
