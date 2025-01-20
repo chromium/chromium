df75cc8417ef7a89b3b937854efa6eba0b24646e
Theo Cristea <theocristea@google.com>
2025-01-20 03:01:53 -0800

    Revert "[Tab Groups] Shrink Animation for Final Incognito Tab"
    This reverts commit 10682037b35f1c2cfe16252dc6d5580de7130e45.
    
    Reason for revert: This CL is a possible culprit for the consistent failure of "org.chromium.chrome.browser.tasks.tab_management.TabSwitcherTabletTest#testEmptyStateView_ToggleIncognito", starting with this build: https://ci.chromium.org/ui/p/chromium/builders/ci/android-14-tablet-landscape-arm64-rel/865/overview. I will run the bot on the revert CL. If the bot passes, I'll submit the revert.
    
    Original change's description:
    > [Tab Groups] Shrink Animation for Final Incognito Tab
    >
    > - Ensures the final shrink animation for the incognito tab switcher
    >         runs when closing the tab switcher
    > - Track animation completion using a Supplier
    > - Animation Recording: https://drive.google.com/file/d/1G-bdbIcQI9wY6HQ_Kx3KuUrfsN9GIz4S/view?usp=sharing&resourcekey=0-RLXdKuJIVe57phjl-binLQ
    >
    > Bug: 362329064
    > Change-Id: Ib72d054e64089fe0e0abf67b704cfa4f44636a75
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6114888
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1408085}
    
    Bug: 362329064
    Change-Id: I19e74a868f1da2e0eeb74fe9030d22a4075967a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6181814
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1408574}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/IncognitoTabSwitcherPaneUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/IncognitoTabSwitcherPaneUnitTest.java
@@ -4,7 +4,6 @@
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -88,8 +87,6 @@ public class IncognitoTabSwitcherPaneUnitTest {
             mIncognitoReauthControllerSupplier = new OneshotSupplierImpl<>();
     private final ObservableSupplierImpl<EdgeToEdgeController> mEdgeToEdgeSupplier =
             new ObservableSupplierImpl<>();
-    private final ObservableSupplierImpl<Boolean> mIsRecyclerViewAnimatorRunningSupplier =
-            new ObservableSupplierImpl<>(false);
 
     private Context mContext;
     private IncognitoTabSwitcherPane mIncognitoTabSwitcherPane;
@@ -118,8 +115,6 @@ public class IncognitoTabSwitcherPaneUnitTest {
 
         when(mTabGroupModelFilter.getTabModel()).thenReturn(mIncognitoTabModel);
         when(mTabGroupModelFilter.isTabModelRestored()).thenReturn(true);
-        when(mTabSwitcherPaneCoordinator.getIsRecyclerViewAnimatorRunning())
-                .thenReturn(mIsRecyclerViewAnimatorRunningSupplier);
 
         mIncognitoTabSwitcherPane =
                 new IncognitoTabSwitcherPane(
@@ -405,10 +400,6 @@ public class IncognitoTabSwitcherPaneUnitTest {
         IncognitoTabModelObserver observer = mIncognitoTabModelObserverCaptor.getValue();
 
         observer.didBecomeEmpty();
-        mIsRecyclerViewAnimatorRunningSupplier.set(true);
-        mIsRecyclerViewAnimatorRunningSupplier.set(false);
-        ShadowLooper.runUiThreadTasks();
-
         assertNull(mIncognitoTabSwitcherPane.getReferenceButtonDataSupplier().get());
         verify(mPaneHubController).focusPane(PaneId.TAB_SWITCHER);
         assertNull(mIncognitoTabSwitcherPane.getTabSwitcherPaneCoordinator());
@@ -425,12 +416,7 @@ public class IncognitoTabSwitcherPaneUnitTest {
                 buttonData.resolveContentDescription(mContext));
         assertNotNull(buttonData.resolveIcon(mContext));
 
-        mIncognitoTabSwitcherPane.createTabSwitcherPaneCoordinator();
         observer.didBecomeEmpty();
-        mIsRecyclerViewAnimatorRunningSupplier.set(true);
-        mIsRecyclerViewAnimatorRunningSupplier.set(false);
-        ShadowLooper.runUiThreadTasks();
-
         assertNull(mIncognitoTabSwitcherPane.getReferenceButtonDataSupplier().get());
         verify(mPaneHubController, times(2)).focusPane(PaneId.TAB_SWITCHER);
         assertNull(mIncognitoTabSwitcherPane.getTabSwitcherPaneCoordinator());
