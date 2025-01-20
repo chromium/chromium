e471a7858c9050cb66f4a7398a5a4464e7be1f51
Stephen White <senorblanco@chromium.org>
2025-01-20 07:26:11 -0800

    Revert "WebGPU: improve Dawn/GLES backend availability on Android."
    This reverts commit 12d7772ea4d823a6e09ecb23ab6a59342fd5e110.
    
    Reason for revert: causing failure of WebGPU content when Vulkan is disabled 
    
    Original change's description:
    > WebGPU: improve Dawn/GLES backend availability on Android.
    >
    > Change EGLImageBackingFactory to allow WebGPU usages on a
    > Skia GL context, rather than requiring the --use-webgpu-adapter=opengles
    > flag.
    >
    > Bug: 389876644
    > Change-Id: Icd7110a7caf8d2871908e9f404909af8010ef69e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180104
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Commit-Queue: Stephen White <senorblanco@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1408122}
    
    Bug: 389876644
    Change-Id: Ib0d7cfeb90d64e476b20dcb868f2446175e08154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6185664
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1408668}

--- gpu/command_buffer/service/shared_image/egl_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/egl_image_backing_factory.cc
@@ -116,9 +116,7 @@ bool EGLImageBackingFactory::IsSupported(SharedImageUsageSet usage,
 
   if ((usage.HasAny(SHARED_IMAGE_USAGE_WEBGPU_READ |
                     SHARED_IMAGE_USAGE_WEBGPU_WRITE)) &&
-      (gr_context_type != GrContextType::kGL ||
-       gl::GetGLImplementation() != gl::kGLImplementationEGLANGLE ||
-       gl::GetANGLEImplementation() != gl::ANGLEImplementation::kOpenGL)) {
+      (use_webgpu_adapter_ != WebGPUAdapterName::kOpenGLES)) {
     return false;
   }
 
