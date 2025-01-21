6dc114043cf39afe94e8f96c718a5e0c07c53dc5
Arthur Milchior <arthurmilchior@chromium.org>
2025-01-21 03:03:50 -0800

    [Signin]Rename AccessPoint enum entries
    See go/access_point_shorter to see how those patches were generated.
    
    This allows to remove at least 13 characters per line and avoid the
    redundant double "access point" when the entry is used.
    
    This CL was uploaded by git cl split.
    
    R=kazinova@google.com
    
    R: droger
    Bug: 389192722
    Change-Id: I2e7ab6484eb90639e99a06c040466cdb15cd6cee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187063
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1408936}

--- components/password_manager/core/browser/password_reuse_manager_signin_notifier_impl_unittest.cc
+++ components/password_manager/core/browser/password_reuse_manager_signin_notifier_impl_unittest.cc
@@ -79,7 +79,7 @@ TEST_F(PasswordReuseManagerSigninNotifierImplTest, SignOutContentArea) {
       /*email=*/"username2",
       /*refresh_token=*/"refresh_token",
       /*is_under_advanced_protection=*/false,
-      signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN,
+      signin_metrics::AccessPoint::kUnknown,
       signin_metrics::SourceForRefreshTokenOperation::kUnknown);
   // This call is necessary to ensure that the account removal is fully
   // processed in this testing context.
