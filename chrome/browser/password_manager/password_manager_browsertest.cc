8c2f93a79c576ade9ff9bf89c5ce2a557edaa861
Arthur Milchior <arthurmilchior@chromium.org>
2025-01-21 08:12:59 -0800

    [Signin]Rename AccessPoint enum entries
    See go/access_point_shorter to see how those patches were generated.
    
    This allows to remove at least 13 characters per line and avoid the
    redundant double "access point" when the entry is used.
    
    This CL was uploaded by git cl split.
    
    R=ioanap@chromium.org
    
    R: droger
    Bug: 389192722
    Change-Id: I16369be9cb81b3ddadf36aca153f52cf04ce9f3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6179831
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409033}

--- chrome/browser/password_manager/password_manager_browsertest.cc
+++ chrome/browser/password_manager/password_manager_browsertest.cc
@@ -4146,8 +4146,7 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerBrowserTestWithSigninInterception,
   DiceWebSigninInterceptor* signin_interceptor =
       helper_.GetSigninInterceptor(profile);
   signin_interceptor->MaybeInterceptWebSignin(
-      WebContents(), account_id,
-      signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN,
+      WebContents(), account_id, signin_metrics::AccessPoint::kUnknown,
       /*is_new_account=*/true,
       /*is_sync_signin=*/false);
   EXPECT_FALSE(signin_interceptor->is_interception_in_progress());
@@ -4177,8 +4176,7 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerBrowserTestWithSigninInterception,
   DiceWebSigninInterceptor* signin_interceptor =
       helper_.GetSigninInterceptor(profile);
   signin_interceptor->MaybeInterceptWebSignin(
-      WebContents(), account_id,
-      signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN,
+      WebContents(), account_id, signin_metrics::AccessPoint::kUnknown,
       /*is_new_account=*/true,
       /*is_sync_signin=*/false);
   EXPECT_FALSE(signin_interceptor->is_interception_in_progress());
@@ -4210,8 +4208,7 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerBrowserTestWithSigninInterception,
   DiceWebSigninInterceptor* signin_interceptor =
       helper_.GetSigninInterceptor(profile);
   signin_interceptor->MaybeInterceptWebSignin(
-      WebContents(), account_id,
-      signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN,
+      WebContents(), account_id, signin_metrics::AccessPoint::kUnknown,
       /*is_new_account=*/true,
       /*is_sync_signin=*/false);
   EXPECT_TRUE(signin_interceptor->is_interception_in_progress());
@@ -4235,8 +4232,7 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerBrowserTestWithSigninInterception,
   DiceWebSigninInterceptor* signin_interceptor =
       helper_.GetSigninInterceptor(profile);
   signin_interceptor->MaybeInterceptWebSignin(
-      WebContents(), account_id,
-      signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN,
+      WebContents(), account_id, signin_metrics::AccessPoint::kUnknown,
       /*is_new_account=*/true,
       /*is_sync_signin=*/false);
   EXPECT_TRUE(signin_interceptor->is_interception_in_progress());
