88fe396e7fde845fac75f659b56159c7dd082f4e
Aldo Culquicondor <acondor@chromium.org>
2025-01-21 08:04:26 -0800

    Reland "Update support for forward transition animations via onResume"
    This is a reland of commit be2fd01a3178f177080f569974c76e7d1ebe6d73
    
    NavigationTransitionsTest now avoid screenshots during backward navigations
    while in gesture mode. Tests needed to be adapted.
    
    The tests are skipped on emulators.
    Tested in
    https://ci.chromium.org/ui/inv/u-acondor-2025-01-20-15-18-00-8070fed3a83c063d/test-results?sortby=&groupby=&q=NavigationTransitionsTest
    
    Original change's description:
    > Update support for forward transition animations via onResume
    >
    > Instead of querying the java web contents every time there is a
    > navigation, react to events to update the capability in the WebContents.
    >
    > This will allow to communicate the navigation mode to the renderers only
    > when the mode changes.
    >
    > Bug: 384026829
    > Change-Id: Ia4a417887f275da28051cbb2ae4ee70d04905eda
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6172592
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1408089}
    
    Bug: 384026829
    Change-Id: I51393c64e7ec72d8b8a5bbab19f99f59ec280f82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6184552
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409025}

--- content/browser/renderer_host/navigation_transitions/navigation_entry_screenshot_browsertest.cc
+++ content/browser/renderer_host/navigation_transitions/navigation_entry_screenshot_browsertest.cc
@@ -216,11 +216,6 @@ class HostGetterCrossOrigin : public HostGetter {
   int index_ = 1;
 };
 
-class NoSupportForwardTransitionDelegate : public WebContentsDelegate {
- public:
-  bool SupportsForwardTransitionAnimation() override { return false; }
-};
-
 }  // namespace
 
 class NavigationEntryScreenshotBrowserTestBase : public ContentBrowserTest {
@@ -2026,8 +2021,7 @@ IN_PROC_BROWSER_TEST_P(NavigationEntryScreenshotCacheHitOrMissReasonBrowserTest,
 
 IN_PROC_BROWSER_TEST_P(NavigationEntryScreenshotCacheHitOrMissReasonBrowserTest,
                        BasicNavigationsNoSupportForForwardTransition) {
-  NoSupportForwardTransitionDelegate delegate;
-  web_contents()->SetDelegate(&delegate);
+  web_contents()->SetSupportsForwardTransitionAnimation(false);
   // Max of three screenshots per Profile (BrowserContext).
   const size_t page_size = GetUncompressedScreenshotSizeInBytes();
   const size_t memory_budget = 3 * page_size;
--- content/browser/web_contents/web_contents_android.h
+++ content/browser/web_contents/web_contents_android.h
@@ -245,6 +245,8 @@ class CONTENT_EXPORT WebContentsAndroid {
 
   void SetLongPressLinkSelectText(JNIEnv* env, jboolean enabled);
 
+  void SetSupportsForwardTransitionAnimation(JNIEnv* env, jboolean enabled);
+
   // Adds a crash report, like DumpWithoutCrashing(), including the Java stack
   // trace from which `web_contents` was created. This is meant to help debug
   // cases where BrowserContext is destroyed before its WebContents.
