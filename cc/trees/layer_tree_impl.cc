0c255754e62b5261525a5851dc7b437c54287149
Dirk Pranke <dpranke@google.com>
2025-01-22 09:53:50 -0800

    Remove ....LayerTreeImpl.CalculateDrawPropertiesUs metric.
    There seems to be a consensus that the Compositing.Browser
    .LayerTreeImpl.CalculateDrawPropertiesUs isn't really used or
    needed at this point, so this CL removes it. We can always
    add it back if we need it in the future.
    
    Bug: 40891358
    Change-Id: I16096e2f3d908bf594680b5df5ad977f0d9edc40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6183107
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1409763}

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -1663,12 +1663,7 @@ bool LayerTreeImpl::UpdateDrawProperties(
     draw_property_utils::CalculateDrawProperties(
         this, &render_surface_list_, output_update_layer_list_for_testing);
 
-    if (settings().single_thread_proxy_scheduler) {
-      // This metric is only recorded for the Browser.
-      UMA_HISTOGRAM_COUNTS_1M(
-          "Compositing.Browser.LayerTreeImpl.CalculateDrawPropertiesUs",
-          timer.Elapsed().InMicroseconds());
-    } else {
+    if (!settings().single_thread_proxy_scheduler) {
       // This metric is only recorded for the Renderer.
       UMA_HISTOGRAM_COUNTS_100(
           "Compositing.Renderer.NumRenderSurfaces",
