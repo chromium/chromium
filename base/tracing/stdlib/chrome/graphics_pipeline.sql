ca81ee95a9a7a8f550295bdd32eca1c53a5c1770
Alexander Timin <altimin@chromium.org>
2025-01-22 04:50:57 -0800

    Fix mapping from surface_frame_trace_id to display_trace_id
    A bunch of downstream logic relies on mapping from
    surface_frame_trace_id to display_trace_id being unique.
    
    This is currently not the case as the submitted surface frame can be
    aggregated multiple times.
    
    Bug: 389087471, 384724813
    Change-Id: I0efea96834477d65efd5d4ee8b85bebe7e2dc4b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180312
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Petr Cermak <petrcermak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1409589}

--- base/tracing/stdlib/chrome/graphics_pipeline.sql
+++ base/tracing/stdlib/chrome/graphics_pipeline.sql
@@ -98,27 +98,37 @@ FROM thread_slice
 WHERE name = 'Graphics.Pipeline' AND display_trace_id IS NOT NULL;
 
 -- Links surface frames (`chrome_graphics_pipeline_surface_frame_steps`) to the
--- display frame (`chrome_graphics_pipeline_display_frame_steps`) into which
--- they are merged. In other words, in general, multiple
--- `surface_frame_trace_id`s will correspond to one `display_trace_id`.
-CREATE PERFETTO TABLE chrome_graphics_pipeline_aggregated_frames(
+-- the first display frame (`chrome_graphics_pipeline_display_frame_steps`) into
+-- which it was included. As an display frame usually aggregates frames from
+-- multiple surfaces, multiple `surface_frame_trace_id`s will correspond to one
+-- `display_trace_id`.
+CREATE PERFETTO TABLE chrome_surface_frame_id_to_first_display_id(
   -- Id of the graphics pipeline, pre-surface aggregation.
   surface_frame_trace_id LONG,
   -- Id of the graphics pipeline, post-surface aggregation.
   display_trace_id LONG)
 AS
+WITH aggregations AS (
+  SELECT
+    args.int_value AS surface_frame_trace_id,
+    display_trace_id,
+    slice.ts
+  FROM chrome_graphics_pipeline_display_frame_steps step
+  JOIN slice USING (id)
+  JOIN args USING (arg_set_id)
+  WHERE
+    step.step = 'STEP_SURFACE_AGGREGATION'
+    AND args.flat_key
+      = 'chrome_graphics_pipeline.aggregated_surface_frame_trace_ids'
+)
 SELECT
-  args.int_value AS surface_frame_trace_id,
-  display_trace_id
-FROM chrome_graphics_pipeline_display_frame_steps step
-JOIN slice
-  USING (id)
-JOIN args
-  USING (arg_set_id)
-WHERE
-  step.step = 'STEP_SURFACE_AGGREGATION'
-  AND args.flat_key
-    = 'chrome_graphics_pipeline.aggregated_surface_frame_trace_ids';
+  surface_frame_trace_id,
+  FIRST_VALUE(display_trace_id) OVER (
+    PARTITION BY surface_frame_trace_id
+    ORDER BY ts
+  ) AS display_trace_id
+FROM aggregations
+GROUP BY surface_frame_trace_id;
 
 -- Links inputs (`chrome_input_pipeline_steps.latency_id`) to the surface frame
 -- (`chrome_graphics_pipeline_surface_frame_steps`) to which they correspond.
