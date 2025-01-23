4de38671036a9d406988116e5da5dec00282f294
Michelle Chen <michellegc@google.com>
2025-01-22 20:51:21 -0800

    scanner: Add ErrorView to show in the action button container.
    Add an ErrorView to be shown in the action button container if actions
    are not available.
    
    Followups will handle the logic for showing and hiding the ErrorView.
    
    Bug: b:378582420
    Change-Id: I87471fa9465ee3f53dcab39ad96a91950e6552ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6189039
    Reviewed-by: Elijah Hewer <hewer@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410114}

--- ash/capture_mode/action_button_container_view.h
+++ ash/capture_mode/action_button_container_view.h
@@ -5,6 +5,7 @@
 #ifndef ASH_CAPTURE_MODE_ACTION_BUTTON_CONTAINER_VIEW_H_
 #define ASH_CAPTURE_MODE_ACTION_BUTTON_CONTAINER_VIEW_H_
 
+#include <memory>
 #include <string>
 
 #include "ash/ash_export.h"
@@ -13,21 +14,53 @@
 #include "base/memory/weak_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/button/button.h"
+#include "ui/views/layout/box_layout_view.h"
 #include "ui/views/view.h"
 
 namespace gfx {
 struct VectorIcon;
 }  // namespace gfx
 
+namespace views {
+class Label;
+}
+
 namespace ash {
 
 class ActionButtonView;
+class SystemShadow;
 
-// A view that displays a row of action buttons near the capture region.
+// A view that displays a row of action buttons near the capture region. It may
+// display an error message if actions are not available.
 class ASH_EXPORT ActionButtonContainerView : public views::View {
   METADATA_HEADER(ActionButtonContainerView, views::View)
 
  public:
+  // A view that displays an error message and icon.
+  class ASH_EXPORT ErrorView : public views::BoxLayoutView {
+    METADATA_HEADER(ErrorView, views::BoxLayoutView)
+
+   public:
+    ErrorView();
+    ErrorView(const ErrorView&) = delete;
+    ErrorView& operator=(const ErrorView&) = delete;
+    ~ErrorView() override;
+
+    // views::BoxLayoutView:
+    void AddedToWidget() override;
+    void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
+
+    // Sets the error message to show on the error view.
+    void SetErrorMessage(const std::u16string& error_message);
+
+    const std::u16string& GetErrorMessageForTesting() const;
+
+   private:
+    std::unique_ptr<SystemShadow> shadow_;
+
+    raw_ptr<views::Label> error_label_ = nullptr;
+  };
+
   ActionButtonContainerView();
   ActionButtonContainerView(const ActionButtonContainerView&) = delete;
   ActionButtonContainerView& operator=(const ActionButtonContainerView&) =
@@ -50,11 +83,19 @@ class ASH_EXPORT ActionButtonContainerView : public views::View {
   // Returns the action buttons in this container.
   const views::View::Views& GetActionButtons() const;
 
+  // Shows an error view with the given `error_message`.
+  void ShowErrorView(const std::u16string& error_message);
+
+  // Hides the error view.
+  void HideErrorView();
+
   // Starts performing the button transition triggered after pressing the smart
   // actions button. This will fade out existing action buttons, remove the
   // smart actions button, then animate in new icon buttons to replace the old
   // copy text and search buttons.
   void StartSmartActionsButtonTransition();
+
+  const ErrorView* error_view_for_testing() const { return error_view_; }
 
  private:
   // Called when the smart actions button has faded out, to start the transition
@@ -66,6 +107,9 @@ class ASH_EXPORT ActionButtonContainerView : public views::View {
 
   // Contains the row of action buttons.
   raw_ptr<views::View> action_button_row_ = nullptr;
+
+  // Used to show an error message.
+  raw_ptr<ErrorView> error_view_ = nullptr;
 
   base::WeakPtrFactory<ActionButtonContainerView> weak_ptr_factory_{this};
 };
