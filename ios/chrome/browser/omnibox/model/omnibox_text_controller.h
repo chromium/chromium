97e31cf54db9bf2391e64c7da70a954dbf9d06b7
Christian Xu <christianxu@google.com>
2025-01-23 06:10:52 -0800

    [iOS][Omnibox] Add new controller classes for future CLs
    Add omnibox controller classes used in later CLs
    
    Bug: 390409677
    Change-Id: Ia1778b281140a2460fa66345e74b23feed376ec9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6187138
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410287}

---
+++ ios/chrome/browser/omnibox/model/omnibox_text_controller.h
@@ -0,0 +1,8 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OMNIBOX_MODEL_OMNIBOX_TEXT_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_OMNIBOX_MODEL_OMNIBOX_TEXT_CONTROLLER_H_
+
+#endif  // IOS_CHROME_BROWSER_OMNIBOX_MODEL_OMNIBOX_TEXT_CONTROLLER_H_
