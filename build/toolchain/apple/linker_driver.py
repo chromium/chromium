8c7989879959548cebdc170f4eb9f1aca0b67222
Fumitoshi Ukai <ukai@google.com>
2025-01-23 15:36:35 -0800

    apple/linker_driver.py supports solink step
    Redo of https://crrev.com/c/6056986
    Fix linker_driver.py instead of introducing solink_driver.py.
    
    Bug: 380769398
    Change-Id: I1069fe67ee9d3c447f4e4efea89d3a2e1f1671d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6068871
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Fumitoshi Ukai <ukai@google.com>
    Auto-Submit: Fumitoshi Ukai <ukai@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410604}

--- build/toolchain/apple/linker_driver.py
+++ build/toolchain/apple/linker_driver.py
@@ -20,9 +20,6 @@ import whole_archive
 # Prefix for all custom linker driver arguments.
 LINKER_DRIVER_ARG_PREFIX = '-Wcrl,'
 LINKER_DRIVER_COMPILER_ARG_PREFIX = '-Wcrl,driver,'
-# Linker action to create a directory and pass it to the linker as
-# `-object_path_lto`. Special-cased since it has to run before the link.
-OBJECT_PATH_LTO = 'object_path_lto'
 
 # The linker_driver.py is responsible for forwarding a linker invocation to
 # the compiler driver, while processing special arguments itself.
@@ -77,7 +74,16 @@ OBJECT_PATH_LTO = 'object_path_lto'
 #    `xcrun` is not used to invoke it.
 # -Wcrl,object_path_lto
 #    Creates temporary directory for LTO object files.
-
+#
+# -Wcrl,otoolpath,<otool path>
+#    Sets the path to the otool for solink process.
+# -Wcrl,nmpath,<nm path>
+#    Sets the path to the nm for solink process.
+#
+# -Wcrl.tocname,<tocname>
+#    Output TOC for solink.
+#    It would be processed both before the linker (to check reexport
+#    in old module) and after the linker (to produce TOC if needed).
 
 class LinkerDriver(object):
     def __init__(self, args):
@@ -88,31 +94,47 @@ class LinkerDriver(object):
         """
         self._args = args
 
+        # List of linker driver pre-actions that need to run before the link.
+        # **The sort order of this list affects the order in which
+        # the actions are invoked.**
+        # The first item in the tuple is the argument's -Wcrl,<sub_argument>
+        # and the second is the function to invoke.
+        self._pre_actions = [
+            ('object_path_lto', self.prepare_object_path_lto),
+            ('installnametoolpath,', self.set_install_name_tool_path),
+            ('dsymutilpath,', self.set_dsymutil_path),
+            ('strippath,', self.set_strip_path),
+            ('otoolpath,', self.set_otool_path),
+            ('nmpath,', self.set_nm_path),
+            ('tocname,', self.check_reexport_in_old_module),
+        ]
+
         # List of linker driver actions. **The sort order of this list affects
         # the order in which the actions are invoked.**
         # The first item in the tuple is the argument's -Wcrl,<sub_argument>
         # and the second is the function to invoke.
         self._actions = [
-            ('installnametoolpath,', self.set_install_name_tool_path),
             ('installnametool,', self.run_install_name_tool),
-            ('dsymutilpath,', self.set_dsymutil_path),
             ('dsym,', self.run_dsymutil),
             ('unstripped,', self.run_save_unstripped),
-            ('strippath,', self.set_strip_path),
             ('strip,', self.run_strip),
+            ('tocname,', self.output_toc),
         ]
 
         # Linker driver actions can modify the these values.
         self._driver_path = None  # Must be specified on the command line.
+        self._otool_cmd = ['xcrun', 'otool']
+        self._nm_cmd = ['xcrun', 'nm']
         self._install_name_tool_cmd = ['xcrun', 'install_name_tool']
         self._dsymutil_cmd = ['xcrun', 'dsymutil']
         self._strip_cmd = ['xcrun', 'strip']
 
         # The linker output file, lazily computed in self._get_linker_output().
         self._linker_output = None
-        # The temporary directory for intermediate LTO object files. If it
-        # exists, it will clean itself up on script exit.
-        self._object_path_lto = None
+
+        # may not need to reexport unless LC_REEXPORT_DYLIB is used.
+        self._reexport_in_old_module = False
+
 
     def run(self):
         """Runs the linker driver, separating out the main compiler driver's
@@ -122,8 +144,9 @@ class LinkerDriver(object):
         """
         # Collect arguments to the linker driver (this script) and remove them
         # from the arguments being passed to the compiler driver.
-        linker_driver_actions = {}
-        compiler_driver_args = []
+        self._linker_driver_actions = {}
+        self._linker_driver_pre_actions = {}
+        self._compiler_driver_args = []
         for index, arg in enumerate(self._args[1:]):
             if arg.startswith(LINKER_DRIVER_COMPILER_ARG_PREFIX):
                 assert not self._driver_path
@@ -131,9 +154,7 @@ class LinkerDriver(object):
                                             ):]
             elif arg.startswith(LINKER_DRIVER_ARG_PREFIX):
                 # Convert driver actions into a map of name => lambda to invoke.
-                driver_action = self._process_driver_arg(arg)
-                assert driver_action[0] not in linker_driver_actions
-                linker_driver_actions[driver_action[0]] = driver_action[1]
+                self._process_driver_arg(arg)
             else:
                 # TODO(crbug.com/40268754): On Apple, the linker command line
                 # produced by rustc for LTO includes these arguments, but the
@@ -141,16 +162,13 @@ class LinkerDriver(object):
                 # Upstream bug: https://github.com/rust-lang/rust/issues/60059
                 BAD_RUSTC_ARGS = '-Wl,-plugin-opt=O[0-9],-plugin-opt=mcpu=.*'
                 if not re.match(BAD_RUSTC_ARGS, arg):
-                    compiler_driver_args.append(arg)
+                    self._compiler_driver_args.append(arg)
 
         if not self._driver_path:
             raise RuntimeError(
                 "Usage: linker_driver.py -Wcrl,driver,<compiler-driver> "
                 "[linker-args]...")
 
-        if self._object_path_lto is not None:
-            compiler_driver_args.append('-Wl,-object_path_lto,{}'.format(
-                os.path.relpath(self._object_path_lto.name)))
         if self._get_linker_output() is None:
             raise ValueError(
                 'Could not find path to linker output (-o or --output)')
@@ -158,8 +176,8 @@ class LinkerDriver(object):
         # We want to link rlibs as --whole-archive if they are part of a unit
         # test target. This is determined by switch
         # `-LinkWrapper,add-whole-archive`.
-        compiler_driver_args = whole_archive.wrap_with_whole_archive(
-            compiler_driver_args, is_apple=True)
+        self._compiler_driver_args = whole_archive.wrap_with_whole_archive(
+            self._compiler_driver_args, is_apple=True)
 
         linker_driver_outputs = [self._get_linker_output()]
 
@@ -168,16 +186,26 @@ class LinkerDriver(object):
             # https://crbug.com/330262.
             env = os.environ.copy()
             env['ZERO_AR_DATE'] = '1'
+
+            # Run the driver pre-actions, in the order specified by the
+            # actions list.
+            for action in self._pre_actions:
+                name = action[0]
+                if name in self._linker_driver_pre_actions:
+                    self._linker_driver_pre_actions[name]()
+
             # Run the linker by invoking the compiler driver.
-            subprocess.check_call([self._driver_path] + compiler_driver_args,
+            subprocess.check_call([self._driver_path] +
+                                  self._compiler_driver_args,
                                   env=env)
 
             # Run the linker driver actions, in the order specified by the
             # actions list.
             for action in self._actions:
                 name = action[0]
-                if name in linker_driver_actions:
-                    linker_driver_outputs += linker_driver_actions[name]()
+                if name in self._linker_driver_actions:
+                    linker_driver_outputs += self._linker_driver_actions[name](
+                    )
         except:
             # If a linker driver action failed, remove all the outputs to make
             # the build step atomic.
@@ -212,23 +240,65 @@ class LinkerDriver(object):
             raise ValueError('%s is not a linker driver argument' % (arg, ))
 
         sub_arg = arg[len(LINKER_DRIVER_ARG_PREFIX):]
-        # Special-cased, since it needs to run before the link.
-        # TODO(lgrey): Remove if/when we start running `dsymutil`
-        # through the clang driver. See https://crbug.com/1324104
-        if sub_arg == OBJECT_PATH_LTO:
-            self._object_path_lto = tempfile.TemporaryDirectory(
-                dir=os.getcwd())
-            return (OBJECT_PATH_LTO, lambda: [])
+
+        found = False
+        for driver_action in self._pre_actions:
+            (pre_name, pre_action) = driver_action
+            if sub_arg.startswith(pre_name):
+                assert pre_name not in self._linker_driver_pre_actions
+                self._linker_driver_pre_actions[pre_name] = \
+                    lambda: pre_action(sub_arg[len(pre_name):])
+                # same sub_arg may be used in actions.
+                found = True
+                break
 
         for driver_action in self._actions:
             (name, action) = driver_action
             if sub_arg.startswith(name):
-                return (name, lambda: action(sub_arg[len(name):]))
+                assert name not in self._linker_driver_actions
+                self._linker_driver_actions[name] = \
+                        lambda: action(sub_arg[len(name):])
+                return
 
-        raise ValueError('Unknown linker driver argument: %s' % (arg, ))
+        if not found:
+            raise ValueError('Unknown linker driver argument: %s' % (arg, ))
+
+    def prepare_object_path_lto(self, arg):
+        """Linker driver pre-action for -Wcrl,object_path_lto.
+
+        Prepare object_path_lto path in temp directory.
+        """
+        # TODO(lgrey): Remove if/when we start running `dsymutil`
+        # through the clang driver. See https://crbug.com/1324104
+        # The temporary directory for intermediate LTO object files. If it
+        # exists, it will clean itself up on script exit.
+        object_path_lto = tempfile.TemporaryDirectory(dir=os.getcwd())
+        self._compiler_driver_args.append('-Wl,-object_path_lto,{}'.format(
+            os.path.relpath(object_path_lto.name)))
+
+    def check_reexport_in_old_module(self, tocname):
+        """Linker driver pre-action for -Wcrl,tocname,<path>.
+
+        Check whether it contains LC_REEXPORT_DYLIB in old module, so that
+        needs to ouptupt TOC file for solink even if the same TOC.
+
+        Returns:
+           True if old module have LC_REEXPORT_DYLIB
+        """
+        if not os.path.exists(tocname):
+            return
+        dylib = self._get_linker_output()
+        if not os.path.exists(dylib):
+            return
+        p = subprocess.run(self._otool_cmd + ['-l', dylib],
+                           capture_output=True)
+        if p.returncode != 0:
+            return
+        if re.match(b'\s+cmd LC_REEXPORT_DYLIB$', p.stdout, re.MULTILINE):
+            self._reexport_in_old_module = True
 
     def set_install_name_tool_path(self, install_name_tool_path):
-        """Linker driver action for -Wcrl,installnametoolpath,<path>.
+        """Linker driver pre-action for -Wcrl,installnametoolpath,<path>.
 
         Sets the invocation command for install_name_tool, which allows the
         caller to specify an alternate path. This action is always
@@ -237,12 +307,8 @@ class LinkerDriver(object):
         Args:
             install_name_tool_path: string, The path to the install_name_tool
                 binary to run
-
-        Returns:
-            No output - this step is run purely for its side-effect.
         """
         self._install_name_tool_cmd = [install_name_tool_path]
-        return []
 
     def run_install_name_tool(self, args_string):
         """Linker driver action for -Wcrl,installnametool,<args>. Invokes
@@ -294,7 +360,7 @@ class LinkerDriver(object):
         return [dsym_out]
 
     def set_dsymutil_path(self, dsymutil_path):
-        """Linker driver action for -Wcrl,dsymutilpath,<dsymutil_path>.
+        """Linker driver pre-action for -Wcrl,dsymutilpath,<dsymutil_path>.
 
         Sets the invocation command for dsymutil, which allows the caller to
         specify an alternate dsymutil. This action is always processed before
@@ -302,12 +368,8 @@ class LinkerDriver(object):
 
         Args:
             dsymutil_path: string, The path to the dsymutil binary to run
-
-        Returns:
-            No output - this step is run purely for its side-effect.
         """
         self._dsymutil_cmd = [dsymutil_path]
-        return []
 
     def run_save_unstripped(self, unstripped_path_prefix):
         """Linker driver action for -Wcrl,unstripped,<unstripped_path_prefix>.
@@ -335,19 +397,15 @@ class LinkerDriver(object):
 
         Args:
             strip_args_string: string, Comma-separated arguments for `strip`.
-
-        Returns:
-            list of string, Build step outputs.
         """
         strip_command = list(self._strip_cmd)
         if len(strip_args_string) > 0:
             strip_command += strip_args_string.split(',')
         strip_command.append(self._get_linker_output())
         subprocess.check_call(strip_command)
-        return []
 
     def set_strip_path(self, strip_path):
-        """Linker driver action for -Wcrl,strippath,<strip_path>.
+        """Linker driver pre-action for -Wcrl,strippath,<strip_path>.
 
         Sets the invocation command for strip, which allows the caller to
         specify an alternate strip. This action is always processed before the
@@ -355,12 +413,105 @@ class LinkerDriver(object):
 
         Args:
             strip_path: string, The path to the strip binary to run
-
-        Returns:
-            No output - this step is run purely for its side-effect.
         """
         self._strip_cmd = [strip_path]
-        return []
+
+    def set_otool_path(self, otool_path):
+        """Linker driver pre-action for -Wcrl,otoolpath,<otool_path>.
+
+        Sets the invocation command for otool.
+
+        Args:
+           otool_path: string. The path to the otool binary to run
+
+        """
+        self._otool_cmd = [otool_path]
+
+    def set_nm_path(self, nm_path):
+        """Linker driver pre-action for -Wcrl,nmpath,<nm_path>.
+
+        Sets the invocation command for nm.
+
+        Args:
+           nm_path: string. The path to the nm binary to run
+
+        Returns:
+           No output - this step is run purely for its side-effect.
+        """
+        self._nm_cmd = [nm_path]
+
+    def output_toc(self, tocname):
+        """Linker driver action for -Wcrl,tocname,<path>.
+
+        Produce *.TOC from linker output.
+
+        TODO(ukai): recursively collect symbols from all 'LC_REEXPORT_DYLIB'-
+        exported modules and present them all in the TOC, and
+        drop self._reexport_in_old_module.
+
+        Args:
+           tocname: string, The path to *.TOC file.
+        Returns:
+           list of string, TOC file as output.
+        """
+        new_toc = self._extract_toc()
+        old_toc = None
+        if not self._reexport_in_old_module:
+            try:
+                with open(tocname, 'rb') as f:
+                    old_toc = f.read()
+            except OSError:
+                pass
+
+        if self._reexport_in_old_module or new_toc != old_toc:
+            # TODO: use delete_on_close in python 3.12 or later.
+            with tempfile.NamedTemporaryFile(prefix=tocname + '.',
+                                             dir='.',
+                                             delete=False) as f:
+                f.write(new_toc)
+                f.close()
+                os.rename(f.name, tocname)
+        return [tocname]
+
+    def _extract_toc(self):
+        """Extract TOC from linker output.
+
+        Returns:
+           output contents in bytes.
+        """
+        toc = b''
+        dylib = self._get_linker_output()
+        out = subprocess.check_output(self._otool_cmd + ['-l', dylib])
+        lines = out.split(b'\n')
+        found_id = False
+        for i, line in enumerate(lines):
+            # Too many LC_ID_DYLIBs? We didn’t understand something about
+            # the otool output. Raise an exception and die, rather than
+            # proceeding.
+
+            # Not any LC_ID_DYLIBs? Probably not an MH_DYLIB. Probably fine, we
+            # can proceed with ID-less TOC generation.
+            if line == b'      cmd LC_ID_DYLIB':
+                if found_id:
+                    raise ValueError('Too many LC_ID_DYLIBs in %s' % dylib)
+                toc += line + b'\n'
+                for j in range(5):
+                    toc += lines[i + 1 + j] + b'\n'
+                found_id = True
+
+        # -U ignores undefined symbols
+        # -g display only global (external) symbols
+        # -p unsorted https://crrev.com/c/2173969
+        out = subprocess.check_output(self._nm_cmd + ['-Ugp', dylib])
+        lines = out.split(b'\n')
+        for line in lines:
+            fields = line.split(b' ', 2)
+            if len(fields) < 3:
+                continue
+            # fields = (value, type, name)
+            # emit [type, name]
+            toc += b' '.join(fields[1:3]) + b'\n'
+        return toc
 
 
 def _find_tools_paths(full_args):
