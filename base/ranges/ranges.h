7be06d9af3db3b022636d5f597353d9d89207427
Peter Kasting <pkasting@chromium.org>
2025-01-23 12:33:52 -0800

    Make base::ranges equivalent to std::ranges.
    This prevents backsliding while rewriting the codebase to explicitly
    spell the latter.
    
    Bug: 386918226
    Binary-Size: Unlikely actionable, OKed by agrieve@
    Change-Id: I1c5dd4e46802d3f1ae880ca28898de5d9b11287e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6169820
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410505}

--- base/ranges/ranges.h
+++ base/ranges/ranges.h
@@ -2,32 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef BASE_RANGES_RANGES_H_
 #define BASE_RANGES_RANGES_H_
 
-#include <array>
-#include <iterator>
-#include <type_traits>
-#include <utility>
+#include <ranges>
 
 namespace base::ranges {
 
-// Implementation of C++20's std::ranges::iterator_t.
-//
-// Reference: https://wg21.link/ranges.syn#:~:text=iterator_t
 template <typename Range>
-using iterator_t = decltype(std::begin(std::declval<Range&>()));
-
-// Implementation of C++20's std::ranges::range_value_t.
-//
-// Reference: https://wg21.link/ranges.syn#:~:text=range_value_t
+using iterator_t = std::ranges::iterator_t<Range>;
 template <typename Range>
-using range_value_t = std::iter_value_t<iterator_t<Range>>;
+using range_value_t = std::ranges::range_value_t<Range>;
 
 }  // namespace base::ranges
 
