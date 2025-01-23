a6a397a84dd38ff0cee7379b7c5b8432c543c396
Tanu Jain <tanujain@microsoft.com>
2025-01-22 21:33:55 -0800

    Remove RootElementWithPlaceHolderAfterDeletingSelection flag
    This was shipped in M130, and can now be removed.
    
    Bug: 40702380
    Change-Id: Iec7d8fb23b315c91e413698c722c204ce41e7282
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6171034
    Reviewed-by: Siye Liu <siliu@microsoft.com>
    Commit-Queue: Tanu Jain <tanujain@microsoft.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410119}

--- third_party/blink/renderer/core/editing/commands/delete_selection_command.cc
+++ third_party/blink/renderer/core/editing/commands/delete_selection_command.cc
@@ -1236,27 +1236,19 @@ void DeleteSelectionCommand::DoApply(EditingState* editing_state) {
   const Node* downstream_container_node = downstream_end.ComputeContainerNode();
   const Element* downstream_container_root_element =
       RootEditableElement(*downstream_container_node);
-  bool root_will_stay_open_without_placeholder =
-      downstream_container_node == downstream_container_root_element;
 
   // Check to determine if the root will stay open without a placeholder.
   // This is done by checking if the downstream end is within a root editable
   // element that has an inline layout object, or if the downstream end's
   // container node is within a shadow host that is a text control.
-  if (RuntimeEnabledFeatures::
-          RootElementWithPlaceHolderAfterDeletingSelectionEnabled()) {
-    root_will_stay_open_without_placeholder |=
-        (downstream_container_root_element &&
-         downstream_container_root_element->GetLayoutObject() &&
-         downstream_container_root_element->GetLayoutObject()->IsInline()) ||
-        (downstream_container_node->OwnerShadowHost() &&
-         downstream_container_node->OwnerShadowHost()->IsTextControl());
-  } else {
-    root_will_stay_open_without_placeholder |=
-        (downstream_container_node->IsTextNode() &&
-         downstream_container_node->parentNode() ==
-             downstream_container_root_element);
-  }
+  bool root_will_stay_open_without_placeholder =
+      (downstream_container_node == downstream_container_root_element) ||
+      (downstream_container_root_element &&
+       downstream_container_root_element->GetLayoutObject() &&
+       downstream_container_root_element->GetLayoutObject()->IsInline()) ||
+      (downstream_container_node->OwnerShadowHost() &&
+       downstream_container_node->OwnerShadowHost()->IsTextControl());
+
   VisiblePosition visible_start = CreateVisiblePosition(
       selection_to_delete_.Start(),
       selection_to_delete_.IsRange() ? TextAffinity::kDownstream : affinity);
