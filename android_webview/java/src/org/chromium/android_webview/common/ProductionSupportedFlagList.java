2950b87aaefbf463d1bca8768526116ec06775e9
Florian Leimgruber <fleimgruber@google.com>
2025-01-24 01:12:47 -0800

    Add AutofillBetterLocalHeuristicPlaceholderSupport to field trial
    Bug: 320965828
    Change-Id: I0f95cf18f26664205d3116837037e90b654a227c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6190544
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1410776}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -237,6 +237,9 @@ public final class ProductionSupportedFlagList {
                 AutofillFeatures.AUTOFILL_ACCEPT_DOM_MUTATION_AFTER_AUTOFILL_SUBMISSION,
                 "Accepts DOM_MUTATION_AFTER_AUTOFILL submissions detected on password forms."),
         Flag.baseFeature(
+                AutofillFeatures.AUTOFILL_BETTER_LOCAL_HEURISTIC_PLACEHOLDER_SUPPORT,
+                "Treats placeholders as a separate signal for Autofill local heuristics"),
+        Flag.baseFeature(
                 AutofillFeatures.AUTOFILL_ENABLE_EXPIRATION_DATE_IMPROVEMENTS,
                 "Enables various improvements to handling expiration dates."),
         Flag.baseFeature(
