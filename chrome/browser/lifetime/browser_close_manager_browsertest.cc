893a1fbedf3cc9412130f91af822b60a99643d83
Etienne Bergeron <etienneb@chromium.org>
2025-01-24 08:25:24 -0800

    Re-enable shutdown tests previously flaky on win7
    Re-enabling an unittests that was flaky.
    We suspect it is fixed by:
      https://chromium-review.googlesource.com/c/chromium/src/+/6179704
    
    This CL has no behavior changes.
    
    If still flaky, revert this CL.
    
    Bug: 751081
    Change-Id: I4e6e81d7ba7700c21115c712afabf3e8ddffeb9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180976
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410953}

--- chrome/browser/lifetime/browser_close_manager_browsertest.cc
+++ chrome/browser/lifetime/browser_close_manager_browsertest.cc
@@ -621,16 +621,9 @@ IN_PROC_BROWSER_TEST_F(BrowserCloseManagerBrowserTest,
   EXPECT_TRUE(BrowserList::GetInstance()->empty());
 }
 
-// Flaky on Windows 7 (dbg) trybot, see https://crbug.com/751081.
-#if BUILDFLAG(IS_WIN) && !defined(NDEBUG)
-#define MAYBE_TestAddWindowDuringShutdown DISABLED_TestAddWindowDuringShutdown
-#else
-#define MAYBE_TestAddWindowDuringShutdown TestAddWindowDuringShutdown
-#endif
-
 // Test that a window created during shutdown is closed.
 IN_PROC_BROWSER_TEST_F(BrowserCloseManagerBrowserTest,
-                       MAYBE_TestAddWindowDuringShutdown) {
+                       TestAddWindowDuringShutdown) {
   ASSERT_NO_FATAL_FAILURE(ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browsers_[0], embedded_test_server()->GetURL("/beforeunload.html"))));
   PrepareForDialog(browsers_[0]);
