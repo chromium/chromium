7fa2b5cb16760ca3f87d9bc5be988659b82833cf
Ioana Pandele <ioanap@chromium.org>
2025-01-24 08:35:24 -0800

    [Passwords] Make the PasswordManagerUtilBridge mockable
    Utils like isBackendPresent call into Java and depend on the exact
    chrome build being run by the bot. To make tests deterministic, these
    need to be mocked.
    
    Bug: 392027556
    
    Change-Id: I9b026624bcdfff89cf34b1ee2051f2d2491bfa84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6194122
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1410960}

---
+++ chrome/browser/password_manager/android/mock_password_manager_util_bridge.cc
@@ -0,0 +1,8 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/password_manager/android/mock_password_manager_util_bridge.h"
+
+MockPasswordManagerUtilBridge::MockPasswordManagerUtilBridge() = default;
+MockPasswordManagerUtilBridge::~MockPasswordManagerUtilBridge() = default;
