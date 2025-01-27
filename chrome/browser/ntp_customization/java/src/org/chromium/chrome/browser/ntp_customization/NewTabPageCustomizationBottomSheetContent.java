c0dd9eabf58059d9bdd3acea01fd8743342c3aff
Yanling <yanlinghu@google.com>
2025-01-27 14:10:49 -0800

    [NTP Customization] Create the first level bottom sheet
    This CL creates the xml for the first level bottom sheet of the NTP customization feature.
    
    The bottom sheet UI created in this CL is shown in the following screenshots: https://docs.google.com/document/d/1bMRVyyqOd0-cAmyTbL9IhKNU9uSvDdpdCngSzSXIHnk/edit?tab=t.0#bookmark=id.3fgvpk54q4r6
    
    The added strings are shown in the following screenshot: https://drive.google.com/file/d/1Q1ONi73ZQc4CGDjzvCRsktBlZuGkrvpP/view?usp=sharing&resourcekey=0-tgzpw0ai4IGjMY5ApS7hGA
    
    Low-Coverage-Reason: HARD_TO_TEST
    Bug: 390490415
    Change-Id: I657328a1433bf32f3efe9da981d78f146a7b7bc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168579
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Yanling Hu <yanlinghu@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411896}

---
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/NewTabPageCustomizationBottomSheetContent.java
@@ -0,0 +1,82 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ntp_customization;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
+
+/** Bottom sheet content of the new tab page customization. */
+public class NewTabPageCustomizationBottomSheetContent implements BottomSheetContent {
+    private final View mContentView;
+
+    public NewTabPageCustomizationBottomSheetContent(@NonNull View view) {
+        mContentView = view;
+    }
+
+    @Override
+    public View getContentView() {
+        return mContentView;
+    }
+
+    @Nullable
+    @Override
+    public View getToolbarView() {
+        return null;
+    }
+
+    @Override
+    public int getVerticalScrollOffset() {
+        return 0;
+    }
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public int getPriority() {
+        return BottomSheetContent.ContentPriority.HIGH;
+    }
+
+    @Override
+    public boolean swipeToDismissEnabled() {
+        return false;
+    }
+
+    @Override
+    public int getPeekHeight() {
+        return BottomSheetContent.HeightMode.DISABLED;
+    }
+
+    @Override
+    public float getFullHeightRatio() {
+        return BottomSheetContent.HeightMode.WRAP_CONTENT;
+    }
+
+    @NonNull
+    @Override
+    public String getSheetContentDescription(Context context) {
+        return "";
+    }
+
+    @Override
+    public int getSheetHalfHeightAccessibilityStringId() {
+        return 0;
+    }
+
+    @Override
+    public int getSheetFullHeightAccessibilityStringId() {
+        return 0;
+    }
+
+    @Override
+    public int getSheetClosedAccessibilityStringId() {
+        return 0;
+    }
+}
