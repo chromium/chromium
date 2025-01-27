bbcab271e9c839254320e39b248f59fde45edac8
Jun Ishiguro <junis@google.com>
2025-01-27 00:27:40 -0800

    Move //chrome/browser/ash/scheduler_config to //chromeos/ash/components/
    Bug: b:392511777
    Change-Id: Ia260239d11f13a6163d0176b2874a56c56354c92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6200285
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1411502}

--- chromeos/ash/components/scheduler_config/scheduler_configuration_manager.cc
+++ chromeos/ash/components/scheduler_config/scheduler_configuration_manager.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/scheduler_config/scheduler_configuration_manager.h"
+#include "chromeos/ash/components/scheduler_config/scheduler_configuration_manager.h"
 
 #include <string>
 
