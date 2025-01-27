8996c9629641510834cfe28642470aa7fa9e7a6f
Michael Thiessen <mthiesse@chromium.org>
2025-01-27 07:49:28 -0800

    Reland "Fix NPEs when a navigation happens while waiting for async interception"
    Since we've made async navigation interception async in Java, we have
    to handle various events like getting backgrounded clearing the
    RedirectHandler, or the user switching tabs.
    
    In situations like this, it's probably not a good idea to launch an
    app, so we should just cancel the check and have the navigation
    proceed.
    
    Bug: 381535042, 391842690, 392131251
    Change-Id: Id202684bab2a23d103d2b9d6cc04619476437009
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6202081
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1411624}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabsTest.java
@@ -19,7 +19,6 @@ import org.mockito.quality.Strictness;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -119,7 +118,6 @@ public class ArchivedTabsTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/391842690")
     public void testCloseAllTabsAndClickUndo() {
         ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
