c749bad4893f31686b0544692699909ace0fc89a
Norge Vizcay <vizcay@google.com>
2025-01-28 10:33:06 -0800

    Default enabled PlusAddressUserOnboardingEnabled.
    The CL enables the feature by default and fixes the necessary tests.
    
    Change-Id: Ia83843da4990a58163bc8d63425b607b2627a1f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6175030
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1412408}

--- chrome/browser/ui/android/plus_addresses/plus_address_creation_controller_android_unittest.cc
+++ chrome/browser/ui/android/plus_addresses/plus_address_creation_controller_android_unittest.cc
@@ -104,6 +104,9 @@ class PlusAddressCreationControllerAndroidEnabledTest
         }));
 
     PlusAddressCreationControllerAndroid::CreateForWebContents(web_contents());
+
+    ON_CALL(plus_address_setting_service(), GetHasAcceptedNotice)
+        .WillByDefault(Return(true));
   }
 
   void TearDown() override { ChromeRenderViewHostTestHarness::TearDown(); }
@@ -148,8 +151,6 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest, AcceptCreation) {
   features.InitWithFeatures({features::kPlusAddressUserOnboardingEnabled}, {});
   base::UserActionTester user_action_tester;
 
-  ON_CALL(plus_address_setting_service(), GetHasAcceptedNotice)
-      .WillByDefault(Return(true));
   // The setting service is not called if the notice is already accepted.
   EXPECT_CALL(plus_address_setting_service(), SetHasAcceptedNotice).Times(0);
 
@@ -187,39 +188,6 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest, AcceptCreation) {
   EXPECT_EQ(user_action_tester.GetActionCount(
                 "PlusAddresses.OfferedPlusAddressAccepted"),
             1);
-}
-
-// Tests that no notice is shown if the onboarding feature is disabled.
-TEST_F(PlusAddressCreationControllerAndroidEnabledTest,
-       NoNoticeIfOnboardingFeatureIsDisabled) {
-  std::unique_ptr<content::WebContents> web_contents =
-      ChromeRenderViewHostTestHarness::CreateTestWebContents();
-
-  // Simulate that the user has created 3 plus profiles. This is only relevant
-  // for the HaTS survey testing.
-  plus_address_service().add_plus_profile(
-      test::CreatePlusProfile("example1@gmail.com", /*is_confirmed=*/true));
-  plus_address_service().add_plus_profile(
-      test::CreatePlusProfile("example2@gmail.com", /*is_confirmed=*/true));
-  plus_address_service().add_plus_profile(
-      test::CreatePlusProfile("example3@gmail.com", /*is_confirmed=*/true));
-
-  ON_CALL(plus_address_setting_service(), GetHasAcceptedNotice)
-      .WillByDefault(Return(false));
-  EXPECT_CALL(plus_address_setting_service(), SetHasAcceptedNotice).Times(0);
-
-  controller().set_suppress_ui_for_testing(true);
-  base::test::TestFuture<const std::string&> future;
-  controller().OfferCreation(
-      url::Origin::Create(GURL("https://mattwashere.example")),
-      /*is_manual_fallback=*/false, future.GetCallback());
-  FastForwardBy(kDuration);
-  EXPECT_CALL(
-      autofill_client(),
-      TriggerPlusAddressUserPerceptionSurvey(
-          plus_addresses::hats::SurveyType::kCreatedMultiplePlusAddresses));
-  controller().OnConfirmed();
-  EXPECT_TRUE(future.IsReady());
 }
 
 // Tests that the notice is shown and its acceptance registered if the
@@ -391,7 +359,6 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest, RefreshPlusAddress) {
 
 TEST_F(PlusAddressCreationControllerAndroidEnabledTest, OnConfirmedError) {
   base::UserActionTester user_action_tester;
-
   controller().set_suppress_ui_for_testing(true);
   base::test::TestFuture<const std::string&> future;
   controller().OfferCreation(
@@ -434,7 +401,6 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest, OnConfirmedError) {
 
 TEST_F(PlusAddressCreationControllerAndroidEnabledTest, OnReservedError) {
   base::UserActionTester user_action_tester;
-
   controller().set_suppress_ui_for_testing(true);
   base::test::TestFuture<const std::string&> future;
 
@@ -485,9 +451,6 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest, TriesAgainToReserve) {
 TEST_F(PlusAddressCreationControllerAndroidEnabledTest,
        CancelAfterAffiliationError) {
   base::UserActionTester user_action_tester;
-  ON_CALL(plus_address_setting_service(), GetHasAcceptedNotice)
-      .WillByDefault(Return(true));
-
   controller().set_suppress_ui_for_testing(true);
 
   base::test::TestFuture<const std::string&> future;
@@ -514,9 +477,6 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest,
 TEST_F(PlusAddressCreationControllerAndroidEnabledTest,
        AcceptedAfterAffiliationError) {
   base::UserActionTester user_action_tester;
-  ON_CALL(plus_address_setting_service(), GetHasAcceptedNotice)
-      .WillByDefault(Return(true));
-
   controller().set_suppress_ui_for_testing(true);
 
   base::test::TestFuture<const std::string&> future;
@@ -543,9 +503,6 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest,
 TEST_F(PlusAddressCreationControllerAndroidEnabledTest,
        AcceptedAfterQuotaError) {
   base::UserActionTester user_action_tester;
-  ON_CALL(plus_address_setting_service(), GetHasAcceptedNotice)
-      .WillByDefault(Return(true));
-
   controller().set_suppress_ui_for_testing(true);
 
   base::test::TestFuture<const std::string&> future;
