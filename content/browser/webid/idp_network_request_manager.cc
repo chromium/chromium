e20c336ca02bc94487be9f7a5511f99f35b63b71
Christian Biesinger <cbiesinger@chromium.org>
2025-01-28 14:27:12 -0800

    [FedCM] Send outcome=success|failure to the metrics endpoint
    This is a cleaner way to distinguish success from failure.
    
    R=npm@chromium.org
    
    Fixed: 390453018
    Change-Id: I09bb63616aa584cf8f6f5d415c63ff8d7fb1578b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6209508
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412569}

--- content/browser/webid/idp_network_request_manager.cc
+++ content/browser/webid/idp_network_request_manager.cc
@@ -1114,7 +1114,8 @@ void IdpNetworkRequestManager::SendSuccessfulTokenRequestMetrics(
     base::TimeDelta account_selected_to_token_response_time,
     base::TimeDelta api_call_to_token_response_time) {
   std::string url_encoded_post_data = base::StringPrintf(
-      "time_to_show_ui=%d"
+      "outcome=success"
+      "&time_to_show_ui=%d"
       "&time_to_continue=%d"
       "&time_to_receive_token=%d"
       "&turnaround_time=%d",
@@ -1142,8 +1143,8 @@ void IdpNetworkRequestManager::SendFailedTokenRequestMetrics(
     bool did_show_ui,
     MetricsEndpointErrorCode error_code) {
   std::string url_encoded_post_data = base::StringPrintf(
-      "error_code=%d&did_show_ui=%s", static_cast<int>(error_code),
-      did_show_ui ? "true" : "false");
+      "outcome=failure&error_code=%d&did_show_ui=%s",
+      static_cast<int>(error_code), did_show_ui ? "true" : "false");
   std::unique_ptr<network::ResourceRequest> resource_request =
       CreateUncredentialedResourceRequest(metrics_endpoint_url,
                                           /*send_origin=*/false);
