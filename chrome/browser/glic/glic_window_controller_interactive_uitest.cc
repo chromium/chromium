861ba14ebc1da6c8303e5fa27d7360e26be55d3e
Ian Wells <iwells@chromium.org>
2025-01-29 15:21:28 -0800

    Activate glic after showing
    This causes glic to activate when shown on Mac without also bringing a
    browser window forward.
    
    Bug: 393137540
    Change-Id: Ia2fa2fffa94c9128d986925d3b5560853cf1b900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6215555
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413211}

--- chrome/browser/glic/glic_window_controller_interactive_uitest.cc
+++ chrome/browser/glic/glic_window_controller_interactive_uitest.cc
@@ -82,6 +82,24 @@ IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest, DoNotCrashOnBrowserClose) {
   ui_test_utils::WaitForBrowserToClose();
 }
 
+IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest, AttachedWidgetHasFocus) {
+  RunTestSequence(
+      OpenGlicWindow(GlicWindowMode::kAttached), CheckControllerHasWidget(true),
+      CheckControllerWidgetMode(GlicWindowMode::kAttached),
+      CheckResult(
+          [this] { return window_controller().GetGlicWidget()->IsActive(); },
+          true, "Glic is active"));
+}
+
+IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest, DetachedWidgetHasFocus) {
+  RunTestSequence(
+      OpenGlicWindow(GlicWindowMode::kDetached), CheckControllerHasWidget(true),
+      CheckControllerWidgetMode(GlicWindowMode::kDetached),
+      CheckResult(
+          [this] { return window_controller().GetGlicWidget()->IsActive(); },
+          true, "Glic is active"));
+}
+
 IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest, DoNotCrashWhenReopening) {
   RunTestSequence(OpenGlicWindow(GlicWindowMode::kAttached), CloseGlicWindow(),
                   OpenGlicWindow(GlicWindowMode::kAttached));
@@ -182,16 +200,13 @@ IN_PROC_BROWSER_TEST_F(GlicWindowControllerUiTest,
 
   RunTestSequence(ObserveState(views::test::kCurrentWidgetFocus),
                   OpenGlicWindow(GlicWindowMode::kDetached),
-                  // Note: it is possible that this will re-activate the browser
-                  // window on some platforms, which will cause an error. If
-                  // this flakes, feel free to disable the test.
-                  //
-                  // (This is likely one reason the test flakes on Linux; see
-                  // above.)
                   Do([this, &glic_native_view] {
                     auto* const widget = window_controller().GetGlicWidget();
                     glic_native_view = widget->GetNativeView();
-                    widget->Deactivate();
+                    // Activating the browser should deactivate glic. Calling
+                    // Deactivate() on the glic widget is unreliable on some
+                    // platforms and has no effect on Mac.
+                    browser()->GetBrowserView().Activate();
                   }),
                   SimulateGlicHotkey(),
                   WaitForState(views::test::kCurrentWidgetFocus,
