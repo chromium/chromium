d4f51015d99d33a38d1997ca356ec0943c05c08b
Salvador Guerrero <salg@google.com>
2025-01-29 11:29:28 -0800

    [Summarization] Added support for PDF
    This CL modifies AiAssistantService to use the analyze attachment
    functionality when viewing a PDF page
    
    Bug: 384089472
    Change-Id: Iee3d27b5f5501e296129564b77d4ac52ad270bc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180615
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413060}

--- chrome/android/junit/src/org/chromium/chrome/browser/ai/AiAssistantServiceUnitTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ai/AiAssistantServiceUnitTest.java
@@ -4,15 +4,20 @@
 
 package org.chromium.chrome.browser.ai;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 import com.google.common.util.concurrent.Futures;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -29,8 +34,11 @@ import org.robolectric.annotation.Config;
 import org.chromium.base.ServiceLoaderUtil;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.ai.proto.SystemAiProviderService.AvailabilityResponse;
+import org.chromium.chrome.browser.ai.proto.SystemAiProviderService.Capability;
+import org.chromium.chrome.browser.ai.proto.SystemAiProviderService.LaunchRequest;
 import org.chromium.chrome.browser.ai.proto.SystemAiProviderService.ServiceAvailable;
 import org.chromium.chrome.browser.ai.proto.SystemAiProviderService.ServiceNotAvailable;
+import org.chromium.chrome.browser.pdf.PdfPage;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.url.JUnitTestGURLs;
 
@@ -45,6 +53,7 @@ public class AiAssistantServiceUnitTest {
     @Mock private Tab mTab;
     @Mock SystemAiProvider mSystemAiProvider;
     @Captor private ArgumentCaptor<Intent> mIntentCaptor;
+    @Captor private ArgumentCaptor<LaunchRequest> mLaunchRequestCaptor;
 
     @Before
     public void setUp() throws Exception {
@@ -54,42 +63,87 @@ public class AiAssistantServiceUnitTest {
     @Test
     public void showAi_fallsBackToIntentWhenNoDownstreamImpl() {
         ServiceLoaderUtil.setInstanceForTesting(SystemAiProvider.class, null);
+        when(mTab.getUrl()).thenReturn(JUnitTestGURLs.URL_1);
 
         var service = new AiAssistantService();
 
         service.showAi(mContext, mTab);
 
-        assertVoiceActivityStarted();
+        var launchRequest = assertVoiceActivityStartedWithLaunchRequest();
+        assertLaunchRequestIsForSummarizeUrl(launchRequest, JUnitTestGURLs.URL_1.getSpec());
     }
 
     @Test
     public void showAi_fallsBackToIntentWhenDownstreamImplNotAvailable() {
         ServiceLoaderUtil.setInstanceForTesting(SystemAiProvider.class, mSystemAiProvider);
         setSystemAiProviderAsUnavailable();
+        when(mTab.getUrl()).thenReturn(JUnitTestGURLs.GOOGLE_URL_DOG);
 
         var service = new AiAssistantService();
         service.showAi(mContext, mTab);
 
-        assertVoiceActivityStarted();
+        var launchRequest = assertVoiceActivityStartedWithLaunchRequest();
+        assertLaunchRequestIsForSummarizeUrl(
+                launchRequest, JUnitTestGURLs.GOOGLE_URL_DOG.getSpec());
+    }
+
+    @Test
+    public void showAi_fallsBackToIntentWhenDownstreamImplNotAvailable_pdfPage() {
+        ServiceLoaderUtil.setInstanceForTesting(SystemAiProvider.class, mSystemAiProvider);
+        setSystemAiProviderAsUnavailable();
+        var pdfUri = JUnitTestGURLs.URL_1_WITH_PDF_PATH.getSpec();
+        var pdfPage = mock(PdfPage.class);
+        when(pdfPage.getUri()).thenReturn(Uri.parse(pdfUri));
+        when(pdfPage.getTitle()).thenReturn("file.pdf");
+        when(mTab.getNativePage()).thenReturn(pdfPage);
+
+        var service = new AiAssistantService();
+        service.showAi(mContext, mTab);
+
+        var launchRequest = assertVoiceActivityStartedWithLaunchRequest();
+        assertLaunchRequestIsForAnalyzeAttachment(launchRequest, pdfUri);
     }
 
     @Test
     public void showAi_usesDownstreamImplWhenAvailable() {
         ServiceLoaderUtil.setInstanceForTesting(SystemAiProvider.class, mSystemAiProvider);
         setSystemAiProviderAsAvailable();
+        when(mTab.getUrl()).thenReturn(JUnitTestGURLs.URL_2);
 
         var service = new AiAssistantService();
 
         service.showAi(mContext, mTab);
 
-        verify(mSystemAiProvider).launch(eq(mContext), any());
+        verify(mSystemAiProvider).launch(eq(mContext), mLaunchRequestCaptor.capture());
+        assertLaunchRequestIsForSummarizeUrl(
+                mLaunchRequestCaptor.getValue(), JUnitTestGURLs.URL_2.getSpec());
+    }
+
+    @Test
+    public void showAi_usesAnalyzeDocumentForPdfs() {
+        ServiceLoaderUtil.setInstanceForTesting(SystemAiProvider.class, mSystemAiProvider);
+        var pdfUri = "https://google.com/file.pdf";
+        var pdfPage = mock(PdfPage.class);
+        when(pdfPage.getUri()).thenReturn(Uri.parse(pdfUri));
+        when(pdfPage.getTitle()).thenReturn("file.pdf");
+        when(mTab.getNativePage()).thenReturn(pdfPage);
+        setSystemAiProviderAsAvailable();
+
+        var service = new AiAssistantService();
+
+        service.showAi(mContext, mTab);
+
+        verify(mSystemAiProvider).launch(eq(mContext), mLaunchRequestCaptor.capture());
+        assertLaunchRequestIsForAnalyzeAttachment(mLaunchRequestCaptor.getValue(), pdfUri);
     }
 
     private void setSystemAiProviderAsAvailable() {
+        var serviceAvailableBuilder =
+                ServiceAvailable.newBuilder()
+                        .addSupportedCapabilities(Capability.ANALYZE_ATTACHMENT_CAPABILITY)
+                        .addSupportedCapabilities(Capability.SUMMARIZE_URL_CAPABILITY);
         var availabilityResponse =
-                AvailabilityResponse.newBuilder()
-                        .setAvailable(ServiceAvailable.getDefaultInstance())
-                        .build();
+                AvailabilityResponse.newBuilder().setAvailable(serviceAvailableBuilder).build();
         when(mSystemAiProvider.isAvailable(eq(mContext), any()))
                 .thenReturn(Futures.immediateFuture(availabilityResponse));
     }
@@ -103,11 +157,33 @@ public class AiAssistantServiceUnitTest {
                 .thenReturn(Futures.immediateFuture(availabilityResponse));
     }
 
-    private void assertVoiceActivityStarted() {
+    private void assertLaunchRequestIsForSummarizeUrl(
+            LaunchRequest launchRequest, String expectedUrl) {
+        Assert.assertTrue(launchRequest.hasSummarizeUrl());
+        Assert.assertTrue(launchRequest.getSummarizeUrl().hasUrlContext());
+        Assert.assertEquals(expectedUrl, launchRequest.getSummarizeUrl().getUrlContext().getUrl());
+    }
+
+    private void assertLaunchRequestIsForAnalyzeAttachment(
+            LaunchRequest launchRequest, String expectedUri) {
+        Assert.assertTrue(launchRequest.hasAnalyzeAttachment());
+        assertThat(launchRequest.getAnalyzeAttachment().getFilesCount(), greaterThanOrEqualTo(1));
+        Assert.assertEquals(expectedUri, launchRequest.getAnalyzeAttachment().getFiles(0).getUri());
+    }
+
+    private LaunchRequest assertVoiceActivityStartedWithLaunchRequest() {
         verify(mContext).startActivity(mIntentCaptor.capture());
 
         var startedIntent = mIntentCaptor.getValue();
-
+        var launchRequestBytes =
+                startedIntent.getByteArrayExtra(AiAssistantService.EXTRA_LAUNCH_REQUEST);
         Assert.assertEquals(Intent.ACTION_VOICE_COMMAND, startedIntent.getAction());
+
+        try {
+            return LaunchRequest.parseFrom(launchRequestBytes);
+        } catch (InvalidProtocolBufferException ex) {
+            Assert.fail("Exception while parsing proto from intent");
+            return null;
+        }
     }
 }
