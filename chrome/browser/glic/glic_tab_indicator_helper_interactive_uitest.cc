84f3a1644d46586eef6e9022d55c48c056960039
Dana Fried <dfried@chromium.org>
2025-01-28 17:41:26 -0800

    Add tests for tab indicators
    Adds interactive/regression tests for the glic tab context tab indicator
    using simulated input into the mock glic window.
    
    This is roughly equivalent to doing a manual test using the mock.
    
    Change-Id: Id3c4559bcbc7f760a40e97fdf318db7b30b56605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6198934
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412659}

---
+++ chrome/browser/glic/glic_tab_indicator_helper_interactive_uitest.cc
@@ -0,0 +1,157 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/functional/bind.h"
+#include "build/build_config.h"
+#include "chrome/browser/glic/glic_tab_indicator_helper.h"
+#include "chrome/browser/glic/interactive_glic_test.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/tabs/tab_enums.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/tabs/tab_strip.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
+#include "content/public/test/browser_test.h"
+#include "ui/base/interaction/element_identifier.h"
+#include "ui/base/interaction/element_tracker.h"
+#include "ui/base/interaction/state_observer.h"
+#include "ui/views/interaction/element_tracker_views.h"
+#include "ui/views/interaction/interaction_test_util_views.h"
+#include "ui/views/interaction/polling_view_observer.h"
+#include "url/gurl.h"
+
+namespace glic {
+
+namespace {
+
+class TabAlertStateObserver
+    : public ui::test::PollingStateObserver<std::vector<TabAlertState>> {
+ public:
+  TabAlertStateObserver(Browser* browser, int tab_index)
+      : PollingStateObserver([browser, tab_index]() {
+          auto* const browser_view =
+              BrowserView::GetBrowserViewForBrowser(browser);
+          TabStrip* const tab_strip = browser_view->tabstrip();
+          if (tab_index < tab_strip->GetModelCount()) {
+            auto* const tab = tab_strip->tab_at(tab_index);
+            return tab->data().alert_state;
+          }
+          return std::vector<TabAlertState>();
+        }) {}
+  ~TabAlertStateObserver() override = default;
+};
+
+DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(TabAlertStateObserver, kTab1AlertState);
+DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(TabAlertStateObserver, kTab2AlertState);
+DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(TabAlertStateObserver, kTab3AlertState);
+
+DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kFirstTabId);
+DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kSecondTabId);
+
+}  // namespace
+
+class GlicTabIndicatorHelperUiTest : public test::InteractiveGlicTest {
+ public:
+  GlicTabIndicatorHelperUiTest() = default;
+  ~GlicTabIndicatorHelperUiTest() override = default;
+
+  static auto IsAccessing() {
+    return testing::Matcher<std::vector<TabAlertState>>(
+        testing::Contains(TabAlertState::GLIC_ACCESSING));
+  }
+  static auto IsNotAccessing() {
+    return testing::Matcher<std::vector<TabAlertState>>(
+        testing::Not(testing::Contains(TabAlertState::GLIC_ACCESSING)));
+  }
+
+  GURL GetTestUrl() const {
+    return embedded_test_server()->GetURL("/links.html");
+  }
+
+  auto LoadStartingPage() {
+    return Steps(InstrumentTab(kFirstTabId),
+                 NavigateWebContents(kFirstTabId, GetTestUrl()));
+  }
+
+  auto AddNewCandidateTab() {
+    return AddInstrumentedTab(kSecondTabId, GetTestUrl());
+  }
+
+ protected:
+  const InteractiveBrowserTest::DeepQuery kMockGlicContextAccessButton = {
+      "#contextAccessIndicator"};
+};
+
+IN_PROC_BROWSER_TEST_F(GlicTabIndicatorHelperUiTest, TabNotAlerted) {
+  RunTestSequence(LoadStartingPage(),
+                  ObserveState(kTab1AlertState, browser(), 0),
+                  OpenGlicWindow(GlicWindowMode::kAttached),
+                  WaitForState(kTab1AlertState, IsNotAccessing()));
+}
+
+IN_PROC_BROWSER_TEST_F(GlicTabIndicatorHelperUiTest, TabAlerted) {
+  RunTestSequence(LoadStartingPage(),
+                  ObserveState(kTab1AlertState, browser(), 0),
+                  OpenGlicWindow(GlicWindowMode::kAttached),
+                  ClickMockGlicElement(kMockGlicContextAccessButton),
+                  WaitForState(kTab1AlertState, IsAccessing()));
+}
+
+IN_PROC_BROWSER_TEST_F(GlicTabIndicatorHelperUiTest, TabAlertTurnsOff) {
+  RunTestSequence(LoadStartingPage(),
+                  ObserveState(kTab1AlertState, browser(), 0),
+                  OpenGlicWindow(GlicWindowMode::kAttached),
+                  ClickMockGlicElement(kMockGlicContextAccessButton),
+                  WaitForState(kTab1AlertState, IsAccessing()),
+                  ClickMockGlicElement(kMockGlicContextAccessButton),
+                  WaitForState(kTab1AlertState, IsNotAccessing()));
+}
+
+IN_PROC_BROWSER_TEST_F(GlicTabIndicatorHelperUiTest, SecondTabAlerted) {
+  RunTestSequence(LoadStartingPage(),
+                  ObserveState(kTab1AlertState, browser(), 0),
+                  ObserveState(kTab2AlertState, browser(), 1),
+                  AddNewCandidateTab(), SelectTab(kTabStripElementId, 1),
+                  OpenGlicWindow(GlicWindowMode::kAttached),
+                  ClickMockGlicElement(kMockGlicContextAccessButton),
+                  WaitForState(kTab2AlertState, IsAccessing()),
+                  WaitForState(kTab1AlertState, IsNotAccessing()));
+}
+
+IN_PROC_BROWSER_TEST_F(GlicTabIndicatorHelperUiTest, SwitchAlertedTabs) {
+  RunTestSequence(
+      LoadStartingPage(), ObserveState(kTab1AlertState, browser(), 0),
+      ObserveState(kTab2AlertState, browser(), 1), AddNewCandidateTab(),
+      OpenGlicWindow(GlicWindowMode::kAttached),
+      SelectTab(kTabStripElementId, 0),
+      ClickMockGlicElement(kMockGlicContextAccessButton),
+      WaitForState(kTab1AlertState, IsAccessing()),
+      SelectTab(kTabStripElementId, 1),
+      WaitForState(kTab2AlertState, IsAccessing()),
+      WaitForState(kTab1AlertState, IsNotAccessing()));
+}
+
+IN_PROC_BROWSER_TEST_F(GlicTabIndicatorHelperUiTest, ActiveBrowserAlerted) {
+#if BUILDFLAG(IS_LINUX)
+  if (views::test::InteractionTestUtilSimulatorViews::IsWayland()) {
+    GTEST_SKIP()
+        << "Programmatic window activation is not supported in the Weston "
+           "reference implementation of Wayland used by test bots.";
+  }
+#endif
+
+  Browser* const browser2 = CreateBrowser(browser()->profile());
+  Browser* const browser3 = CreateBrowser(browser()->profile());
+  RunTestSequence(LoadStartingPage(), OpenGlicWindow(GlicWindowMode::kDetached),
+                  InContext(browser2->window()->GetElementContext(),
+                            ActivateSurface(kBrowserViewElementId)),
+                  ObserveState(kTab1AlertState, browser(), 0),
+                  ObserveState(kTab2AlertState, browser2, 0),
+                  ObserveState(kTab3AlertState, browser3, 0),
+                  ClickMockGlicElement(kMockGlicContextAccessButton),
+                  WaitForState(kTab1AlertState, IsNotAccessing()),
+                  WaitForState(kTab2AlertState, IsAccessing()),
+                  WaitForState(kTab3AlertState, IsNotAccessing()));
+}
+
+}  // namespace glic
