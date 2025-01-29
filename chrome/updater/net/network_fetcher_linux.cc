9dceaddd528be9b89f89f6a81b0b2e5d734258c9
Sorin Jianu <sorin@chromium.org>
2025-01-28 16:44:50 -0800

    updater: resolve unsafe buffers issue in network_fetcher_linux.cc
    Change-Id: Ie1ca8569b92e6313c947fcbe2f0ca6b2cdc9d5ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6199705
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1412634}

--- chrome/updater/net/network_fetcher_linux.cc
+++ chrome/updater/net/network_fetcher_linux.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <curl/curl.h>
 #include <curl/system.h>
 #include <dlfcn.h>
@@ -17,7 +12,9 @@
 #include <string>
 #include <utility>
 
+#include "base/compiler_specific.h"
 #include "base/containers/flat_map.h"
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
@@ -352,15 +349,20 @@ size_t LibcurlNetworkFetcherImpl::CurlWriteFileCallback(void* data,
                                                         size_t member_size,
                                                         size_t num_members,
                                                         void* userp) {
-  base::CheckedNumeric<size_t> write_size =
+  // SAFETY: libcurl guarantees that `member_size` and `num_members` describe a
+  // valid readable portion of `data`. `userp` is a pointer to a stack
+  // allocated `base::File` guaranteed to be valid throughout the libcurl
+  // operation by `DownloadToFile`.
+  const base::CheckedNumeric<size_t> write_size =
       base::CheckedNumeric<size_t>(member_size) *
       base::CheckedNumeric<size_t>(num_members);
-  base::File* file = static_cast<base::File*>(userp);
-
-  int bytes_written = file->WriteAtCurrentPos(
-      static_cast<const char*>(data), write_size.Cast<int>().ValueOrDefault(0));
-
-  return bytes_written > 0 ? bytes_written : 0;
+  CHECK_LE(size_t{write_size.ValueOrDie()}, size_t{CURL_MAX_WRITE_SIZE});
+  CHECK(data);
+  const auto data_span = UNSAFE_BUFFERS(base::span<uint8_t>(
+      static_cast<uint8_t*>(data), write_size.ValueOrDie()));
+  auto* file = static_cast<base::File*>(userp);
+  const std::optional<int> bytes_written = file->WriteAtCurrentPos(data_span);
+  return bytes_written.value_or(0);
 }
 
 int LibcurlNetworkFetcherImpl::CurlTransferCallback(void* userp,
