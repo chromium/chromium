77b083641cffdb20a2d437b512a9296938fc61fe
Luke Warlow <lwarlow@igalia.com>
2025-01-30 02:35:02 -0800

    Update console logs that reference invokeaction
    These now reference command instead.
    
    Bug: 40284894
    Change-Id: I84c2f4a1222b5a3432f8147c1c40dfd53ac71dfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6214271
    Reviewed-by: Keith Cirkel <chromium@keithcirkel.co.uk>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Luke <lwarlow@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1413385}

--- third_party/blink/renderer/core/html/html_dialog_element.cc
+++ third_party/blink/renderer/core/html/html_dialog_element.cc
@@ -359,7 +359,7 @@ bool HTMLDialogElement::HandleCommandInternal(HTMLElement& invoker,
   if (HasPopoverAttribute() && popoverOpen()) {
     AddConsoleMessage(mojom::blink::ConsoleMessageSource::kOther,
                       mojom::blink::ConsoleMessageLevel::kError,
-                      "Dialog invokeactions are ignored on open popovers.");
+                      "Dialog commands are ignored on open popovers.");
     return false;
   }
 
@@ -373,7 +373,7 @@ bool HTMLDialogElement::HandleCommandInternal(HTMLElement& invoker,
       AddConsoleMessage(
           mojom::blink::ConsoleMessageSource::kOther,
           mojom::blink::ConsoleMessageLevel::kWarning,
-          "A closing invokeaction attempted to close an already closed Dialog");
+          "A command attempted to close an already closed Dialog");
     }
   } else if (command == CommandEventType::kShowModal) {
     if (isConnected() && !open) {
@@ -383,7 +383,7 @@ bool HTMLDialogElement::HandleCommandInternal(HTMLElement& invoker,
       AddConsoleMessage(
           mojom::blink::ConsoleMessageSource::kOther,
           mojom::blink::ConsoleMessageLevel::kWarning,
-          "An invokeaction attempted to open an already open Dialog as modal");
+          "A command attempted to open an already open Dialog as a modal");
     }
   }
 
