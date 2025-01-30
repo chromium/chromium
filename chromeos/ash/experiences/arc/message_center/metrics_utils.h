7375a1590a99e7e650e9519c53d01e393ae53726
Hidehiko Abe <hidehiko@chromium.org>
2025-01-29 20:59:57 -0800

    Move ash/components/arc to chromeos/ash/experiences/arc Part 9.
    This CL moves ash/public/cpp/external_arc/message_center/* files.
    Now, external_arc is effectively empty, and removed meta files there.
    
    BUG=386148857
    TEST=Tryjob
    
    Change-Id: I7eeb201c1cafc08f1aeae8556d3d36764866aa4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6210221
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413310}

---
+++ chromeos/ash/experiences/arc/message_center/metrics_utils.h
@@ -0,0 +1,26 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_EXPERIENCES_ARC_MESSAGE_CENTER_METRICS_UTILS_H_
+#define CHROMEOS_ASH_EXPERIENCES_ARC_MESSAGE_CENTER_METRICS_UTILS_H_
+
+#include "chromeos/ash/experiences/arc/mojom/notifications.mojom.h"
+
+namespace ash::metrics_utils {
+
+// Logs if action button is enabled for Arc notification.
+void LogArcNotificationActionEnabled(bool action_enabled);
+
+// Logs if inline reply is enabled for Arc notification.
+void LogArcNotificationInlineReplyEnabled(bool inline_reply_enabled);
+
+// Logs the style of Arc rich notification.
+void LogArcNotificationStyle(arc::mojom::ArcNotificationStyle style);
+
+// Logs if the notification is custom notification.
+void LogArcNotificationIsCustomNotification(bool is_custom_notification);
+
+}  // namespace ash::metrics_utils
+
+#endif  // CHROMEOS_ASH_EXPERIENCES_ARC_MESSAGE_CENTER_METRICS_UTILS_H_
