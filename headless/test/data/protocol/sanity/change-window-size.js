b947a8ebff0d5c3344db0199f15df9b9da2ae094
Peter Kvitek <kvitekp@chromium.org>
2025-01-31 10:16:00 -0800

    [headless] Fix Browser.getWindowBounds() window origin on Macs
    This CL fixes a problem with incorrect window origin returned by
    Browser.getWindowBounds() and Browser.getWindowForTarget() on Macs.
    
    Bug: 378531862
    Change-Id: Ia6bbc7c51b05d3e62637e62873e97bd5e8c27cb9
    Cq-Include-Trybots: luci.chromium.try:mac12-tests,mac13-tests,mac14-tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6220206
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414235}

--- headless/test/data/protocol/sanity/change-window-size.js
+++ headless/test/data/protocol/sanity/change-window-size.js
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 (async function(testRunner) {
-  const {session, dp} = await testRunner.startBlank('Tests window ' +
-      'bounds are properly adjusted upon Browser.setWindowSize.');
+  const {dp} = await testRunner.startBlank(
+      'Tests window bounds are properly adjusted upon Browser.setWindowSize.');
 
   const {windowId} = (await dp.Browser.getWindowForTarget()).result;
   dp.Browser.setWindowBounds({
@@ -16,13 +16,7 @@
   });
 
   const bounds = (await dp.Browser.getWindowBounds({windowId})).result;
-
-  // TODO(378531862): fix top/left on MacOS.
-  if (navigator.platform.startsWith('Mac')) {
-    bounds.bounds.left = 100;
-    bounds.bounds.top = 200;
-  }
-
   testRunner.log(bounds, 'Window bounds: ');
+
   testRunner.completeTest();
 })
