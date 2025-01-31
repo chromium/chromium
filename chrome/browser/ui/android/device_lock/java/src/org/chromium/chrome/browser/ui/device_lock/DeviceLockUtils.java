70c98c67f89bb4b16b10822b45c3be4a294a5d7f
Menghan Yang <myuu@google.com>
2025-01-31 02:12:17 -0800

    [Android][Signin] Update profile lock dialogs margin and buttons layout
    This CL makes the new profile lock dialog use stacked buttons instead of
    a one line button bar, to improve consistency with the other layouts in
    the sign-in flow.
    
    It also makes the dialogs layout use horizontal margins only if
    UNO_FOR_AUTO is enabled, and only for the lower subviews of the dialogs,
    instead of setting a padding on the whole layout without flag guarding.
    This fixes the margin for the progress bar under the illustration which
    should take the full width of the dialog.
    
    See https://crbug.com/367329904#comment14 for screenshots
    
    Bug: 367329904
    Change-Id: Ib13885de60720f6d7da4e2544d090ef7e28f8a6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6212623
    Commit-Queue: Menghan Yang <myuu@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1413982}

--- chrome/browser/ui/android/device_lock/java/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockUtils.java
+++ chrome/browser/ui/android/device_lock/java/src/org/chromium/chrome/browser/ui/device_lock/DeviceLockUtils.java
@@ -7,6 +7,8 @@ import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
+import android.view.View;
+import android.view.ViewGroup.MarginLayoutParams;
 
 public class DeviceLockUtils {
     static boolean isDeviceLockCreationIntentSupported(Context context) {
@@ -21,5 +23,21 @@ public class DeviceLockUtils {
 
     static Intent createDeviceLockThroughOSSettingsIntent() {
         return new Intent(Settings.ACTION_SECURITY_SETTINGS);
+    }
+
+    // TODO(crbug.com/352735671): Remove this once UNO_FOR_AUTO is launched, amd move the layout
+    // changes to the corresponding XMLs.
+    static void updateDialogSubviewMargins(View view) {
+        int marginPx =
+                view.getContext()
+                        .getResources()
+                        .getDimensionPixelSize(R.dimen.device_lock_dialog_horizontal_margin);
+        MarginLayoutParams layoutParams = (MarginLayoutParams) view.getLayoutParams();
+        layoutParams.setMargins(
+                layoutParams.leftMargin + marginPx,
+                layoutParams.topMargin,
+                layoutParams.rightMargin + marginPx,
+                layoutParams.bottomMargin);
+        view.setLayoutParams(layoutParams);
     }
 }
