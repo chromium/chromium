572b7e6dea308a2a1af3b31a0b228b860fafb88f
Anna Tsvirchkova <atsvirchkova@google.com>
2025-02-03 05:24:42 -0800

    Add metrics for password change success bubble
    Bug: 379654003
    Change-Id: Idc45ea0356a64f3bcba3961ea372dffa0be2a355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218376
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1414858}

---
+++ chrome/browser/ui/passwords/bubble_controllers/password_change/successful_password_change_bubble_controller_unittest.cc
@@ -0,0 +1,68 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/passwords/bubble_controllers/password_change/successful_password_change_bubble_controller.h"
+
+#include <memory>
+
+#include "base/test/metrics/histogram_tester.h"
+#include "chrome/browser/password_manager/password_change_delegate_mock.h"
+#include "chrome/browser/ui/passwords/passwords_model_delegate_mock.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using testing::Return;
+namespace metrics_util = password_manager::metrics_util;
+
+class SuccessfulPasswordChangeBubbleControllerTest : public ::testing::Test {
+ public:
+  void CreateController() {
+    EXPECT_CALL(mock_delegate_, OnBubbleShown());
+    ON_CALL(mock_delegate_, GetPasswordChangeDelegate)
+        .WillByDefault(Return(&password_change_delegate_));
+    controller_ = std::make_unique<SuccessfulPasswordChangeBubbleController>(
+        mock_delegate_.AsWeakPtr());
+  }
+
+ protected:
+  PasswordsModelDelegateMock mock_delegate_;
+  std::unique_ptr<SuccessfulPasswordChangeBubbleController> controller_;
+  PasswordChangeDelegateMock password_change_delegate_;
+};
+
+TEST_F(SuccessfulPasswordChangeBubbleControllerTest,
+       MetricsReportedForFinishPasswordChange) {
+  base::HistogramTester histogram_tester;
+  CreateController();
+
+  controller_->FinishPasswordChange();
+  controller_.reset();
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.PasswordChange.SuccessBubble",
+      metrics_util::CLICKED_ACCEPT, 1);
+}
+
+TEST_F(SuccessfulPasswordChangeBubbleControllerTest,
+       MetricsReportedForOpenPasswordManager) {
+  base::HistogramTester histogram_tester;
+  CreateController();
+
+  controller_->OpenPasswordManager();
+  controller_.reset();
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.PasswordChange.SuccessBubble",
+      metrics_util::CLICKED_MANAGE_PASSWORD, 1);
+}
+
+TEST_F(SuccessfulPasswordChangeBubbleControllerTest,
+       MetricsReportedForAboutPasswordChangeLink) {
+  base::HistogramTester histogram_tester;
+  CreateController();
+
+  controller_->NavigateToPasswordChangeSettings();
+  controller_.reset();
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.PasswordChange.SuccessBubble",
+      metrics_util::CLICKED_ABOUT_PASSWORD_CHANGE, 1);
+}
