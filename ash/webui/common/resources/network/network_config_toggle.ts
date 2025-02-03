9c1bca264ba826143529e6a6d9ac51f61ab95eb8
xiangdong kong <xiangdongkong@google.com>
2025-02-03 10:17:40 -0800

    typescript: Convert network_config_toggle to ts
    Bug: 367734817
    Change-Id: Ic1bc459bd786cf2c387be0d9b99eca4f1d82f709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6168516
    Auto-Submit: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1415027}

--- ash/webui/common/resources/network/network_config_toggle.ts
+++ ash/webui/common/resources/network/network_config_toggle.ts
@@ -10,29 +10,26 @@ import '//resources/ash/common/cr_elements/cr_shared_vars.css.js';
 import './cr_policy_network_indicator_mojo.js';
 import './network_shared.css.js';
 
+import {assert} from '//resources/js/assert.js';
 import {mixinBehaviors, PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {CrPolicyNetworkBehaviorMojo, CrPolicyNetworkBehaviorMojoInterface} from './cr_policy_network_behavior_mojo.js';
 import {NetworkConfigElementBehavior, NetworkConfigElementBehaviorInterface} from './network_config_element_behavior.js';
 import {getTemplate} from './network_config_toggle.html.js';
 
-/**
- * @constructor
- * @extends {PolymerElement}
- * @implements {CrPolicyNetworkBehaviorMojoInterface}
- * @implements {NetworkConfigElementBehaviorInterface}
- */
 const NetworkConfigToggleElementBase = mixinBehaviors(
-    [
-      CrPolicyNetworkBehaviorMojo,
-      NetworkConfigElementBehavior,
-    ],
-    PolymerElement);
+                                           [
+                                             CrPolicyNetworkBehaviorMojo,
+                                             NetworkConfigElementBehavior,
+                                           ],
+                                           PolymerElement) as {
+  new (): PolymerElement & CrPolicyNetworkBehaviorMojoInterface &
+  NetworkConfigElementBehaviorInterface
+};
 
-/** @polymer */
-class NetworkConfigToggleElement extends NetworkConfigToggleElementBase {
+export class NetworkConfigToggleElement extends NetworkConfigToggleElementBase {
   static get is() {
-    return 'network-config-toggle';
+    return 'network-config-toggle' as const;
   }
 
   static get template() {
@@ -63,25 +60,28 @@ class NetworkConfigToggleElement extends NetworkConfigToggleElementBase {
     };
   }
 
-  /** @override */
-  ready() {
+  checked: boolean;
+  label: string;
+  policyOnLeft: boolean;
+  subLabel: string;
+
+  override ready() {
     super.ready();
 
     this.addEventListener('click', this.onHostTap_.bind(this));
   }
 
-  /** @override */
-  focus() {
-    this.shadowRoot.querySelector('cr-toggle').focus();
+  override focus() {
+    const toggleElement = this.shadowRoot!.querySelector('cr-toggle');
+    assert(!!toggleElement);
+    toggleElement.focus();
   }
 
   /**
    * Handles non cr-toggle button clicks (cr-toggle handles its own click events
    * which don't bubble).
-   * @param {!Event} e
-   * @private
    */
-  onHostTap_(e) {
+  private onHostTap_(e: Event) {
     e.stopPropagation();
     if (this.getDisabled_(this.disabled, this.property)) {
       return;
@@ -89,6 +89,12 @@ class NetworkConfigToggleElement extends NetworkConfigToggleElementBase {
     this.checked = !this.checked;
     this.dispatchEvent(
         new CustomEvent('change', {bubbles: true, composed: true}));
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [NetworkConfigToggleElement.is]: NetworkConfigToggleElement;
   }
 }
 
