0553bf58948b2f1b02d2f18dd422ffd8715eae38
Ahmed Nasr <anasr@google.com>
2025-02-04 10:59:53 -0800

    Extract base class from LiveCaptionController to be used by babelorca.
    Bug: b:386835456
    Change-Id: Ic98cb051797b745c1b24bc01383ee68e51a01e9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227845
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1415689}

--- components/live_caption/live_caption_controller.h
+++ components/live_caption/live_caption_controller.h
@@ -12,14 +12,13 @@
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/live_caption/caption_controller_base.h"
 #include "components/live_caption/views/caption_bubble.h"
 #include "components/soda/constants.h"
 #include "components/soda/soda_installer.h"
 #include "media/mojo/mojom/speech_recognition.mojom.h"
 #include "ui/native_theme/caption_style.h"
 #include "ui/native_theme/native_theme_observer.h"
-
-class PrefChangeRegistrar;
 
 namespace content {
 class BrowserContext;
@@ -46,15 +45,14 @@ class LiveCaptionBubbleSettings;
 //
 class LiveCaptionController : public KeyedService,
                               public speech::SodaInstaller::Observer,
-                              public ui::NativeThemeObserver {
+                              public CaptionControllerBase {
  public:
   LiveCaptionController(
       PrefService* profile_prefs,
       PrefService* global_prefs,
       const std::string& application_locale,
       content::BrowserContext* browser_context,
-      base::RepeatingCallback<void()> create_ui_callback_for_testing_ =
-          base::RepeatingCallback<void()>());
+      std::unique_ptr<CaptionControllerBase::Delegate> delegate = nullptr);
   ~LiveCaptionController() override;
   LiveCaptionController(const LiveCaptionController&) = delete;
   LiveCaptionController& operator=(const LiveCaptionController&) = delete;
@@ -93,10 +91,13 @@ class LiveCaptionController : public KeyedService,
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
   CaptionBubbleController* caption_bubble_controller_for_testing() {
-    return caption_bubble_controller_.get();
+    return caption_bubble_controller();
   }
 
  private:
+  // CaptionControllerBase:
+  CaptionBubbleSettings* caption_bubble_settings() override;
+
   // SodaInstaller::Observer:
   void OnSodaInstalled(speech::LanguageCode language_code) override;
   void OnSodaProgress(speech::LanguageCode language_code,
@@ -104,30 +105,18 @@ class LiveCaptionController : public KeyedService,
   void OnSodaInstallError(speech::LanguageCode language_code,
                           speech::SodaInstaller::ErrorCode error_code) override;
 
-  // ui::NativeThemeObserver:
-  void OnCaptionStyleUpdated() override;
-
   void OnLiveCaptionEnabledChanged();
   void OnLiveCaptionLanguageChanged();
   bool IsLiveCaptionEnabled();
   void StartLiveCaption();
   void StopLiveCaption();
-  void CreateUI();
-  void DestroyUI();
   const std::string GetLanguageCode() const;
 
   void MaybeSetLiveCaptionLanguage();
 
-  raw_ptr<PrefService> profile_prefs_;
   raw_ptr<PrefService> global_prefs_;
   raw_ptr<content::BrowserContext> browser_context_;
-  std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
   const std::unique_ptr<LiveCaptionBubbleSettings> caption_bubble_settings_;
-  std::unique_ptr<CaptionBubbleController> caption_bubble_controller_;
-  std::optional<ui::CaptionStyle> caption_style_;
-  base::RepeatingCallback<void()> create_ui_callback_for_testing_;
-
-  const std::string application_locale_;
 
 #if BUILDFLAG(IS_CHROMEOS)
   // Tracks whether or not Live Caption has been enabled for babel orca.
@@ -136,11 +125,6 @@ class LiveCaptionController : public KeyedService,
 
   // Whether Live Caption is enabled.
   bool enabled_ = false;
-
-  // Whether the UI has been created. The UI is created asynchronously from the
-  // feature being enabled--we wait for SODA to download first. This flag
-  // ensures that the UI is not constructed or deconstructed twice.
-  bool is_ui_constructed_ = false;
 };
 
 }  // namespace captions
