805c3aad07a169c4f17bac28d41ac39627687f76
vikas soni <vikassoni@chromium.org>
2025-02-04 01:45:26 -0800

    [MapableSI] Remove unused GMB method from CameraBufferFactory.
    After CameraBufferFactory was converted to MappableSI,
    CreateGpuMemoryBuffer() is no longer used.
    
    This CL removes that method as well as any member variables which
    only that method was using.
    
    Bug: 40263579
    Change-Id: I2a5561a3bbc80d47ce8559d37132833d06797fbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227219
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415441}

--- media/capture/video/chromeos/camera_buffer_factory.cc
+++ media/capture/video/chromeos/camera_buffer_factory.cc
@@ -16,43 +16,6 @@ CameraBufferFactory::CameraBufferFactory() = default;
 
 CameraBufferFactory::~CameraBufferFactory() = default;
 
-std::unique_ptr<gfx::GpuMemoryBuffer>
-CameraBufferFactory::CreateGpuMemoryBuffer(const gfx::Size& size,
-                                           gfx::BufferFormat format,
-                                           gfx::BufferUsage usage) {
-  // TODO(b/363936240): Check if we can set and use GpuChannelHost in the
-  // browser process to create buffers.
-  // GpuChannelHost is able to be reset with new channel host to establish the
-  // new connection to GPU process via viz::Gpu APIs when GPU process crashes.
-  // Therefore, we use GpuChannelHost to create buffers in the utility process.
-  // |gpu_channel_host| is a nullptr in the browser process.
-  scoped_refptr<gpu::GpuChannelHost> gpu_channel_host =
-      VideoCaptureDeviceFactoryChromeOS::GetGpuChannelHost();
-  if (gpu_channel_host) {
-    gfx::GpuMemoryBufferHandle gmb_handle;
-    gpu_channel_host->CreateGpuMemoryBuffer(
-        size, viz::GetSharedImageFormat(format), usage, &gmb_handle);
-    if (gmb_handle.is_null()) {
-      LOG(ERROR)
-          << "GpuChannelHost doesn't work. Probably the gpu channel lost.";
-      return nullptr;
-    }
-    return gpu_memory_buffer_support_.CreateGpuMemoryBufferImplFromHandle(
-        std::move(gmb_handle), size, format, usage, base::NullCallback());
-  }
-  // GpuMemoryBufferManagerSingleton is only available in the browser process.
-  // |buf_manager| is a nullptr in the utility process.
-  gpu::GpuMemoryBufferManager* buf_manager =
-      VideoCaptureDeviceFactoryChromeOS::GetBufferManager();
-  if (buf_manager) {
-    return buf_manager->CreateGpuMemoryBuffer(size, format, usage,
-                                              gpu::kNullSurfaceHandle, nullptr);
-  }
-  LOG(ERROR)
-      << "Both of GpuChannelHost and GpuMemoryBufferManager are not set.";
-  return nullptr;
-}
-
 scoped_refptr<gpu::ClientSharedImage> CameraBufferFactory::CreateSharedImage(
     const gfx::Size& size,
     gfx::BufferFormat format,
