2a1e636f9625305c44ba6b86e882481dc843e459
Emily Shack <emshack@chromium.org>
2025-02-03 17:48:20 -0800

    [SxS] Add basic MultiContentsView
    Adds a view for displaying two web contents side by side. This is the
    last CL in the chain to get a basic end-to-end side by side prototype up
    and running.
    
    Bug: 391894786
    Change-Id: I9985486fc65c1065b02453f8ece375e7aee7d3ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217543
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415324}

---
+++ chrome/browser/ui/views/frame/multi_contents_view.cc
@@ -0,0 +1,45 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/frame/multi_contents_view.h"
+
+#include "base/feature_list.h"
+#include "chrome/browser/ui/ui_features.h"
+#include "chrome/browser/ui/views/frame/contents_web_view.h"
+#include "content/public/browser/web_contents.h"
+#include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/views/layout/flex_layout.h"
+#include "ui/views/view_class_properties.h"
+
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(MultiContentsView,
+                                      kMultiContentsViewElementId);
+
+MultiContentsView::MultiContentsView(content::BrowserContext* browser_context) {
+  active_contents_view_ =
+      AddChildView(std::make_unique<ContentsWebView>(browser_context));
+  inactive_contents_view_ =
+      AddChildView(std::make_unique<ContentsWebView>(browser_context));
+  inactive_contents_view_->SetVisible(false);
+
+  SetProperty(views::kElementIdentifierKey, kMultiContentsViewElementId);
+  SetLayoutManager(std::make_unique<views::FlexLayout>())
+      ->SetOrientation(views::LayoutOrientation::kHorizontal)
+      .SetDefault(
+          views::kFlexBehaviorKey,
+          views::FlexSpecification(views::MinimumFlexSizeRule::kPreferred,
+                                   views::MaximumFlexSizeRule::kUnbounded));
+}
+
+MultiContentsView::~MultiContentsView() = default;
+
+void MultiContentsView::SetWebContents(content::WebContents* web_contents,
+                                       bool active) {
+  ContentsWebView* contents_view =
+      active ? active_contents_view_ : inactive_contents_view_;
+  contents_view->SetWebContents(web_contents);
+  contents_view->SetVisible(web_contents != nullptr);
+}
+
+BEGIN_METADATA(MultiContentsView)
+END_METADATA
