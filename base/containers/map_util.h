f89c8f52e243624ba86d51d4bc80eadc4c804e54
Roger McFarlane <rogerm@chromium.org>
2025-02-04 08:19:28 -0800

    Use emplace_hint instead of operator[] to avoid unnecessary string construction.
    Change-Id: I5b25b2e09544c02cca020220eecfdb599dde2218
    Bug: 378650087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018368
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Roger McFarlane <rogerm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1415579}

--- base/containers/map_util.h
+++ base/containers/map_util.h
@@ -66,6 +66,40 @@ constexpr MappedElementType* FindPtrOrNull(Map& map, const Key& key) {
   return it != map.end() ? base::to_address(it->second) : nullptr;
 }
 
+// Insert or assign into a map type. This has semantics very similar to the
+// following statements:
+//
+//    map[key] = Element(params...)
+//    map[key] = element;
+//    map[key] = std::move(element);
+//    map.insert_or_assign(key, element);
+//
+// Where key can be any heterogeneous comparable overload of the key_type for
+// map. In particular, there is no need to construct a temporary `key` of the
+// type stored in the map as long as `key` is comparable with the
+// `Map::key_type`.
+//
+// The `element` is copied or moved into the map, depending on whether it is
+// passed by lvalue or rvalue reference.
+//
+// TODO(crbug.com/376532871): This can be removed once map::operator[] and
+// map::insert_or_assign support heterogenous key overloads, in C++26.
+template <typename Map,
+          typename Key,
+          typename MappedElementType =
+              std::pointer_traits<internal::MappedType<Map>>::element_type>
+Map::iterator InsertOrAssign(Map& map,
+                             const Key& key,
+                             MappedElementType&& element) {
+  auto it = map.lower_bound(key);
+  if (it == map.end() || it->first != key) {
+    it = map.emplace_hint(it, key, std::forward<MappedElementType>(element));
+  } else {
+    it->second = std::forward<MappedElementType>(element);
+  }
+  return it;
+}
+
 }  // namespace base
 
 #endif  // BASE_CONTAINERS_MAP_UTIL_H_
