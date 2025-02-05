4869ebcc17fb044914c4eda8d3c80d362dd6fc19
Lily Chen <chlily@chromium.org>
2025-02-05 10:51:44 -0800

    Enable SafeBrowsingDeepScanningCriteria feature by default
    This was approved to launch.
    
    Bug: 337315121
    Change-Id: I17a21997b76078ed2660425c0dcb030ae9a835e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6180120
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416313}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -102,7 +102,7 @@ BASE_FEATURE(kCreateWarningShownClientSafeBrowsingReports,
 
 BASE_FEATURE(kDeepScanningCriteria,
              "SafeBrowsingDeepScanningCriteria",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDelayedWarnings,
              "SafeBrowsingDelayedWarnings",
