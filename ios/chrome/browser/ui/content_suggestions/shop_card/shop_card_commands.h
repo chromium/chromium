75c7069b13a89139cedb555c15f976b2dcd1ac1b
Gloria Fang <gloriafang@google.com>
2025-02-06 11:55:30 -0800

    Add mediator and view for ShopCard
    Bug: 374170663
    Change-Id: Ied62c1e99cf07800118f3f8f1a281be59ffbb211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6232968
    Auto-Submit: Gloria Fang <gloriafang@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416945}

---
+++ ios/chrome/browser/ui/content_suggestions/shop_card/shop_card_commands.h
@@ -0,0 +1,11 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SHOP_CARD_SHOP_CARD_COMMANDS_H_
+#define IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SHOP_CARD_SHOP_CARD_COMMANDS_H_
+
+// Command protocol for events of ShopCard module.
+@protocol ShopCardCommands
+@end
+#endif  // IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_SHOP_CARD_SHOP_CARD_COMMANDS_H_
