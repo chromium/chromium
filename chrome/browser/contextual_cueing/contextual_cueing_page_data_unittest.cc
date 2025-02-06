b760360d0d3a12a4cc017841101e9ae22f339722
rajendrant <rajendrant@chromium.org>
2025-02-06 11:24:36 -0800

    [M134] Implement PDF page count signal for contextual cueing
    (cherry picked from commit a40bfd76d1d5f12e220db75111a61ec565ae84ca)
    
    Change-Id: Iea3689885283853f7f62d5c0da0b2829222472bf
    Bug: 389750286
    Fixed: 394614679
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6219781
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1415359}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6240214
    Auto-Submit: Raj T <rajendrant@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6998@{#128}
    Cr-Branched-From: de9c6fafd8ae5c6ea0438764076ca7d04a0b165d-refs/heads/main@{#1415337}

---
+++ chrome/browser/contextual_cueing/contextual_cueing_page_data_unittest.cc
@@ -0,0 +1,151 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/contextual_cueing/contextual_cueing_page_data.h"
+
+#include "base/test/test_future.h"
+#include "chrome/test/base/testing_profile.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/test_renderer_host.h"
+#include "content/public/test/test_utils.h"
+#include "content/public/test/web_contents_tester.h"
+
+namespace contextual_cueing {
+
+class ContextualCueingPageDataTest : public content::RenderViewHostTestHarness {
+ public:
+  void SetUp() override {
+    content::RenderViewHostTestHarness::SetUp();
+    web_contents_ = CreateTestWebContents();
+  }
+
+  void TearDown() override {
+    web_contents_.reset();
+    content::RenderViewHostTestHarness::TearDown();
+  }
+
+  std::unique_ptr<content::BrowserContext> CreateBrowserContext() override {
+    return std::make_unique<TestingProfile>();
+  }
+
+  void InvokePdfPageCountReceived(size_t page_count) {
+    auto* page_data =
+        ContextualCueingPageData::GetForPage(web_contents_->GetPrimaryPage());
+    page_data->OnPdfPageCountReceived(
+        pdf::mojom::PdfListener::GetPdfBytesStatus::kSuccess, {}, page_count);
+  }
+
+ protected:
+  std::unique_ptr<content::WebContents> web_contents_;
+};
+
+TEST_F(ContextualCueingPageDataTest, Basic) {
+  base::test::TestFuture<const std::string&> future;
+  optimization_guide::proto::GlicContextualCueingMetadata metadata;
+  auto* config = metadata.add_cueing_configurations();
+  config->set_cue_label("basic label");
+
+  ContextualCueingPageData::CreateForPage(web_contents_->GetPrimaryPage(),
+                                          std::move(metadata),
+                                          future.GetCallback());
+  ASSERT_TRUE(future.Wait());
+  EXPECT_EQ("basic label", future.Get());
+}
+
+TEST_F(ContextualCueingPageDataTest, NonPdfPageFails) {
+  base::test::TestFuture<const std::string&> future;
+  optimization_guide::proto::GlicContextualCueingMetadata metadata;
+  auto* config = metadata.add_cueing_configurations();
+  config->set_cue_label("basic label");
+  auto* pdf_condition = config->add_conditions();
+  pdf_condition->set_cueing_operator(
+      optimization_guide::proto::ContextualCueingOperator::
+          CONTEXTUAL_CUEING_OPERATOR_GREATER_THAN_OR_EQUAL_TO);
+  pdf_condition->set_int64_threshold(2);
+
+  ContextualCueingPageData::CreateForPage(web_contents_->GetPrimaryPage(),
+                                          std::move(metadata),
+                                          future.GetCallback());
+  ASSERT_TRUE(future.Wait());
+  EXPECT_TRUE(future.Get().empty());
+}
+
+TEST_F(ContextualCueingPageDataTest, PdfPageCountFails) {
+  content::WebContentsTester::For(web_contents_.get())
+      ->SetMainFrameMimeType(pdf::kPDFMimeType);
+
+  base::test::TestFuture<const std::string&> future;
+  optimization_guide::proto::GlicContextualCueingMetadata metadata;
+  auto* config = metadata.add_cueing_configurations();
+  config->set_cue_label("pdf label");
+  auto* pdf_condition = config->add_conditions();
+  pdf_condition->set_signal(optimization_guide::proto::
+                                CONTEXTUAL_CUEING_CLIENT_SIGNAL_PDF_PAGE_COUNT);
+  pdf_condition->set_cueing_operator(
+      optimization_guide::proto::ContextualCueingOperator::
+          CONTEXTUAL_CUEING_OPERATOR_GREATER_THAN_OR_EQUAL_TO);
+  pdf_condition->set_int64_threshold(2);
+
+  ContextualCueingPageData::CreateForPage(web_contents_->GetPrimaryPage(),
+                                          std::move(metadata),
+                                          future.GetCallback());
+  InvokePdfPageCountReceived(1);
+
+  ASSERT_TRUE(future.Wait());
+  EXPECT_TRUE(future.Get().empty());
+}
+
+TEST_F(ContextualCueingPageDataTest, PdfPageCountPasses) {
+  content::WebContentsTester::For(web_contents_.get())
+      ->SetMainFrameMimeType(pdf::kPDFMimeType);
+
+  base::test::TestFuture<const std::string&> future;
+  optimization_guide::proto::GlicContextualCueingMetadata metadata;
+  auto* config = metadata.add_cueing_configurations();
+  config->set_cue_label("pdf label");
+  auto* pdf_condition = config->add_conditions();
+  pdf_condition->set_signal(optimization_guide::proto::
+                                CONTEXTUAL_CUEING_CLIENT_SIGNAL_PDF_PAGE_COUNT);
+  pdf_condition->set_cueing_operator(
+      optimization_guide::proto::ContextualCueingOperator::
+          CONTEXTUAL_CUEING_OPERATOR_GREATER_THAN_OR_EQUAL_TO);
+  pdf_condition->set_int64_threshold(2);
+
+  ContextualCueingPageData::CreateForPage(web_contents_->GetPrimaryPage(),
+                                          std::move(metadata),
+                                          future.GetCallback());
+  InvokePdfPageCountReceived(4);
+
+  ASSERT_TRUE(future.Wait());
+  EXPECT_EQ("pdf label", future.Get());
+}
+
+TEST_F(ContextualCueingPageDataTest, BasicAndPdfPageCountCondition) {
+  content::WebContentsTester::For(web_contents_.get())
+      ->SetMainFrameMimeType(pdf::kPDFMimeType);
+
+  base::test::TestFuture<const std::string&> future;
+  optimization_guide::proto::GlicContextualCueingMetadata metadata;
+  auto* config = metadata.add_cueing_configurations();
+  config->set_cue_label("pdf label");
+  auto* pdf_condition = config->add_conditions();
+  pdf_condition->set_signal(optimization_guide::proto::
+                                CONTEXTUAL_CUEING_CLIENT_SIGNAL_PDF_PAGE_COUNT);
+  pdf_condition->set_cueing_operator(
+      optimization_guide::proto::ContextualCueingOperator::
+          CONTEXTUAL_CUEING_OPERATOR_GREATER_THAN_OR_EQUAL_TO);
+  pdf_condition->set_int64_threshold(2);
+
+  // Second basic condition should get picked.
+  config = metadata.add_cueing_configurations();
+  config->set_cue_label("basic label");
+
+  ContextualCueingPageData::CreateForPage(web_contents_->GetPrimaryPage(),
+                                          std::move(metadata),
+                                          future.GetCallback());
+  ASSERT_TRUE(future.Wait());
+  EXPECT_EQ("basic label", future.Get());
+}
+
+}  // namespace contextual_cueing
