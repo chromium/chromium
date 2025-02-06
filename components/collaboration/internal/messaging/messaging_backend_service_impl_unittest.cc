8b778351e2e9b700c896db9999cde4f5dde3366d
Shakti Sahu <shaktisahu@chromium.org>
2025-02-06 10:54:28 -0800

    [Messaging] Updates to focused tab should not result in dirty dot
    Currently a tab update results in both a dirty dot and a chip
    message. However, if the tab is the currently selected tab, the
    user shouldn't see another dirty dot. Additionally it results in
    a dirty dot for the tab group and the tab switcher.
    This CL fixes this behavior.
    Note that for desktop, it should still show a chip if the active
    tab is updated from remote.
    
    Bug: 394739325
    Change-Id: I4d468fa84b841e7ea9503d75800a1e738db6bde7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237570
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1416915}

--- components/collaboration/internal/messaging/messaging_backend_service_impl_unittest.cc
+++ components/collaboration/internal/messaging/messaging_backend_service_impl_unittest.cc
@@ -1552,6 +1552,7 @@ TEST_F(MessagingBackendServiceImplTest, TestGetMessagesForTab) {
 TEST_F(MessagingBackendServiceImplTest, TestSelectedTabGetsUpdated) {
   CreateAndInitializeService();
   SetupInstantMessageDelegate();
+  AddPersistentMessageObserver();
 
   data_sharing::GroupId collaboration_group_id =
       data_sharing::GroupId("my group id");
@@ -1582,6 +1583,12 @@ TEST_F(MessagingBackendServiceImplTest, TestSelectedTabGetsUpdated) {
       .WillRepeatedly(
           DoAll(SaveArg<0>(&message), MoveArg<1>(&success_callback)));
 
+  // Save the last invocation of DisplayPersistentMessage.
+  PersistentMessage last_persistent_message;
+  EXPECT_CALL(mock_persistent_message_observer_, DisplayPersistentMessage(_))
+      .Times(1)
+      .WillOnce(SaveArg<0>(&last_persistent_message));
+
   // Updating the currently selected tab should inform the delegate.
   tg_notifier_observer_->OnTabUpdated(*tab1, tab_groups::TriggerSource::REMOTE);
 
@@ -1590,6 +1597,15 @@ TEST_F(MessagingBackendServiceImplTest, TestSelectedTabGetsUpdated) {
   base::Uuid db_message_id = base::Uuid::ParseLowercase(db_message.uuid());
   EXPECT_EQ(db_message_id, message.attribution.id);
 
+  // Verify that the dirty bit is chip only and no dot.
+  EXPECT_FALSE(static_cast<int>(DirtyType::kDot) & db_message.dirty());
+  EXPECT_TRUE(static_cast<int>(DirtyType::kChip) & db_message.dirty());
+
+  // Verify persistent notification. There should be only a chip notification
+  // and no tab group notification.
+  EXPECT_EQ(PersistentNotificationType::CHIP, last_persistent_message.type);
+
+  // Verify instant message.
   EXPECT_EQ(CollaborationEvent::TAB_UPDATED, message.collaboration_event);
   EXPECT_EQ(InstantNotificationType::UNDEFINED, message.type);
 
