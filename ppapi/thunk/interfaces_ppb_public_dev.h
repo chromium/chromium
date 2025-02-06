b8c2f26a0a47b8926f288673e5d7cde2bd958750
Tom Sepez <tsepez@chromium.org>
2025-02-06 14:09:00 -0800

    Suppress unsafe libc calls warnings in ppapi/ headers.
    Rather than suppressing the entire ppapi/ directory in the
    unsafe_buffers_paths.txt file, add a few per-file suppressions
    as was previously done for the unsafe_buffer_unsage warnings.
    
    Hopefully this code will all be deleted later this year.
    
    Bug: 390223051
    Change-Id: I2fd2191d53f19be794b4dd7d0ea5abd41be40ce4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6235117
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417015}

--- ppapi/thunk/interfaces_ppb_public_dev.h
+++ ppapi/thunk/interfaces_ppb_public_dev.h
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/390223051): Remove C-library calls to fix the errors.
+#pragma allow_unsafe_libc_calls
+#endif
+
 // Please see inteface_ppb_public_stable for the documentation on the format of
 // this file.
 
