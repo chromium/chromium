5427caf459cc0da4e777f38da330c8d1f9a1a92a
Min Qin <qinmin@chromium.org>
2025-02-05 20:05:03 -0800

    [M134] Fix an issue that parsing preview server response causing OOM
    The customCompressedV1 field in UniquePosition proto is bytes type.
    When returning as a json string, grpc will encode the bytes using
    base64. As a result, Chrome needs to do a base64 decode before
    uncompressing the unique position
    
    (cherry picked from commit 413ca270e0c12098b747e52273fc9a06d49f734a)
    
    Bug: 394157935,394641010
    Change-Id: Ia6813cc8e6cdf5ddf46644f72f07784a56c8fbb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6229295
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1415757}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6237978
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6998@{#83}
    Cr-Branched-From: de9c6fafd8ae5c6ea0438764076ca7d04a0b165d-refs/heads/main@{#1415337}

--- components/data_sharing/internal/preview_server_proxy.cc
+++ components/data_sharing/internal/preview_server_proxy.cc
@@ -8,6 +8,7 @@
 #include <optional>
 #include <utility>
 
+#include "base/base64.h"
 #include "base/base64url.h"
 #include "base/command_line.h"
 #include "base/json/json_reader.h"
@@ -171,8 +172,9 @@ std::optional<sync_pb::SharedTab> ParseSharedTab(
     shared_tab->set_favicon_url(*favicon_url);
   }
   if (custom_compressed) {
-    shared_tab->mutable_unique_position()->set_custom_compressed_v1(
-        custom_compressed.value());
+    std::string decoded;
+    base::Base64Decode(custom_compressed.value(), &decoded);
+    shared_tab->mutable_unique_position()->set_custom_compressed_v1(decoded);
   }
   return shared_tab;
 }
