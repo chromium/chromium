98735104da0e5d15675477982e034d14cc1d90ea
Hidehiko Abe <hidehiko@chromium.org>
2025-02-06 15:52:33 -0800

    Setting the LOGIN_PRIMARY state before starting session creation.
    In the test, to fake the production behavior, LOGIN_PRIMARY state
    should be set before starting session creation.
    
    BUG=None
    TEST=Tryjob
    
    Change-Id: I77a16be211c4b8333ff9fa333189bb1ad62b25ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6238282
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417070}

--- chrome/browser/ash/child_accounts/event_based_status_reporting_service_unittest.cc
+++ chrome/browser/ash/child_accounts/event_based_status_reporting_service_unittest.cc
@@ -105,6 +105,9 @@ class EventBasedStatusReportingServiceTest : public testing::Test {
     chromeos::PowerManagerClient::InitializeFake();
     SystemClockClient::InitializeFake();
 
+    session_manager_.SetSessionState(
+        session_manager::SessionState::LOGIN_PRIMARY);
+
     profile_ = std::make_unique<TestingProfile>();
     profile_->SetIsSupervisedProfile();
     // TODO(hidehiko): we should set up kChild account from the beginning,
@@ -116,8 +119,6 @@ class EventBasedStatusReportingServiceTest : public testing::Test {
         user_manager::FakeUserManager::GetFakeUsernameHash(account_id()),
         /*new_user=*/false,
         /*has_active_session=*/false);
-    session_manager_.SetSessionState(
-        session_manager::SessionState::LOGIN_PRIMARY);
 
     ChildStatusReportingServiceFactory::GetInstance()->SetTestingFactory(
         profile(),
