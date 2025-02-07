370fc9a0a5cb76fdf3bf10df16b7b24a4ee363f7
Ritika Gupta <ritikagup@google.com>
2025-02-07 15:24:43 -0800

    [Data Sharing] Cleaning up unused methods and PeopleKit code.
    Now that these APIs are unified, these files are unused and can be deleted.
    
    Bug: 392956431
    Change-Id: I08064ec235336310f959badc08c5bd3f6784687b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6230302
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417611}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/DataSharingUiDelegateAndroid.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/DataSharingUiDelegateAndroid.java
@@ -4,32 +4,24 @@
 
 package org.chromium.chrome.browser.data_sharing;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.jni_zero.CalledByNative;
 
-import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.ServiceLoaderUtil;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.data_sharing.DataSharingUIDelegate;
-import org.chromium.components.data_sharing.configs.AvatarConfig;
 import org.chromium.components.data_sharing.configs.DataSharingAvatarBitmapConfig;
 import org.chromium.components.data_sharing.configs.DataSharingCreateUiConfig;
 import org.chromium.components.data_sharing.configs.DataSharingJoinUiConfig;
 import org.chromium.components.data_sharing.configs.DataSharingManageUiConfig;
 import org.chromium.components.data_sharing.configs.DataSharingRuntimeDataConfig;
-import org.chromium.components.data_sharing.configs.MemberPickerConfig;
 import org.chromium.url.GURL;
-
-import java.util.List;
 
 /**
  * Implementation of {@link DataSharingUIDelegate} that implements some methods while delegating
@@ -52,40 +44,6 @@ class DataSharingUiDelegateAndroid implements DataSharingUIDelegate {
     @CalledByNative
     private static DataSharingUiDelegateAndroid create(Profile profile) {
         return new DataSharingUiDelegateAndroid(profile);
-    }
-
-    @Override
-    public void showMemberPicker(
-            @NonNull Activity activity,
-            @NonNull ViewGroup view,
-            MemberPickerListener memberResult,
-            MemberPickerConfig config) {
-        if (mInternalDelegate != null) {
-            mInternalDelegate.showMemberPicker(activity, view, memberResult, config);
-        }
-    }
-
-    @Override
-    public void showFullPicker(
-            @NonNull Activity activity,
-            @NonNull ViewGroup view,
-            MemberPickerListener memberResult,
-            MemberPickerConfig config) {
-        if (mInternalDelegate != null) {
-            mInternalDelegate.showFullPicker(activity, view, memberResult, config);
-        }
-    }
-
-    @Override
-    public void showAvatars(
-            @NonNull Context context,
-            List<ViewGroup> views,
-            List<String> emails,
-            Callback<Boolean> success,
-            AvatarConfig config) {
-        if (mInternalDelegate != null) {
-            mInternalDelegate.showAvatars(context, views, emails, success, config);
-        }
     }
 
     @Override
@@ -116,13 +74,6 @@ class DataSharingUiDelegateAndroid implements DataSharingUIDelegate {
     public void updateRuntimeData(String sessionId, DataSharingRuntimeDataConfig runtimeData) {
         if (mInternalDelegate != null) {
             mInternalDelegate.updateRuntimeData(sessionId, runtimeData);
-        }
-    }
-
-    @Override
-    public void showAvatarsInTile(AvatarConfig avatarConfig) {
-        if (mInternalDelegate != null) {
-            mInternalDelegate.showAvatarsInTile(avatarConfig);
         }
     }
 
