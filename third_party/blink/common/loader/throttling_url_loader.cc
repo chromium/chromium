37a3d3a63f9806d98ab66fa104be52c7849778b9
Hiroshige Hayashizaki <hiroshige@chromium.org>
2025-02-07 10:29:37 -0800

    Remove ThrottlingURLLoader::RestartWithFactory()
    https://chromium-review.googlesource.com/c/chromium/src/+/5274112
    removed the last use of `RestartWithFactory()`.
    
    Bug: 40251638, 40947546
    Change-Id: Ie1b0115a11ff362e808f6f9424b9b56264045f07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6027113
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417441}

--- third_party/blink/common/loader/throttling_url_loader.cc
+++ third_party/blink/common/loader/throttling_url_loader.cc
@@ -232,9 +232,8 @@ ThrottlingURLLoader::StartInfo::StartInfo(
       request_id(in_request_id),
       options(in_options),
       url_request(*in_url_request),
-      task_runner(std::move(in_task_runner)) {
-  cors_exempt_header_list = std::move(in_cors_exempt_header_list);
-}
+      task_runner(std::move(in_task_runner)),
+      cors_exempt_header_list(std::move(in_cors_exempt_header_list)) {}
 
 ThrottlingURLLoader::StartInfo::~StartInfo() = default;
 
@@ -326,20 +325,6 @@ void ThrottlingURLLoader::ResetForFollowRedirect(
   url_loader_.ResetWithReason(
       network::mojom::URLLoader::kClientDisconnectReason,
       kFollowRedirectReason);
-}
-
-void ThrottlingURLLoader::RestartWithFactory(
-    scoped_refptr<network::SharedURLLoaderFactory> factory,
-    uint32_t url_loader_options) {
-  DCHECK_EQ(DEFERRED_NONE, deferred_stage_);
-  DCHECK(!loader_completed_);
-  url_loader_.reset();
-  client_receiver_.reset();
-  start_info_->url_loader_factory = std::move(factory);
-  start_info_->options = url_loader_options;
-  body_.reset();
-  cached_metadata_.reset();
-  StartNow();
 }
 
 void ThrottlingURLLoader::FollowRedirect(
