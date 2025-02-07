66b78afb5c9d791858b69b4014b8f355a55ee725
Alex Gough <ajgo@chromium.org>
2025-02-07 11:35:29 -0800

    Hook dwrite.__imp_GetUserDefaultLCID for Csrss Lockdown
    Dwrite tracing functions call GetUserDefaultLCID() which breaks
    when Csrss Lockdown is enabled. This CL adds an import table (IAT)
    hook to dwrite during renderer warmup when Csrss lockdown is
    enabled.
    
    As dwrite imports GetUserDefaultLCID from an apiset we must
    attempt to hook all versions of the apiset. So far two of the
    three published versions of the apiset have been observed in
    dwrite.dll in the wild.
    
    This CL adds tests of the warmup infrastructure, both this
    hook, and randomness warmup, to ensure they continue to work
    in future versions of Windows.
    
    Bug: 384336650
    Change-Id: Ia015117fc5c51d16f4cfdcc1daade408c257226f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226669
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417488}

--- sandbox/policy/win/sandbox_warmup.cc
+++ sandbox/policy/win/sandbox_warmup.cc
@@ -7,6 +7,8 @@
 #include <windows.h>
 
 #include "base/check_op.h"
+#include "base/no_destructor.h"
+#include "sandbox/policy/win/hook_util/hook_util.h"
 
 // Prototype for ProcessPrng.
 // See: https://learn.microsoft.com/en-us/windows/win32/seccng/processprng
@@ -17,6 +19,14 @@ BOOL WINAPI ProcessPrng(PBYTE pbData, SIZE_T cbData);
 namespace sandbox::policy {
 
 namespace {
+
+// Stores the default lcid.
+LCID g_user_default_lcid = 0;
+
+// Returns the lcid seen at startup.
+LCID HookedGetUserDefaultLCID() {
+  return g_user_default_lcid;
+}
 
 // Import bcryptprimitives!ProcessPrng rather than cryptbase!RtlGenRandom to
 // avoid opening a handle to \\Device\KsecDD in the renderer.
@@ -39,6 +49,38 @@ void WarmupRandomnessInfrastructure() {
   BOOL success = process_prng_fn(data, sizeof(data));
   // ProcessPrng is documented to always return TRUE.
   CHECK(success);
+}
+
+bool HookDwriteGetUserDefaultLCID() {
+  // Should not be called twice.
+  static bool first_call = true;
+  CHECK(first_call);
+  first_call = false;
+
+  // In Chrome dwrite.dll should be loaded as it is an import of chrome.dll.
+  HMODULE h_dwrite = ::GetModuleHandleW(L"dwrite.dll");
+  if (!h_dwrite) {
+    return false;
+  }
+
+  // Store this for our hook to return if it is called.
+  g_user_default_lcid = GetUserDefaultLCID();
+
+  // We never call the original version of GetUserDefaultLCID and never unhook.
+  static base::NoDestructor<sandbox::policy::IATHook> dwrite_hook;
+
+  // dwrite.dll imports 1-2-1 in Windows 10 10240 & 1-2-0 in Windows 11 22631.
+  const char* apisets[] = {"api-ms-win-core-localization-l1-2-0.dll",
+                           "api-ms-win-core-localization-l1-2-1.dll",
+                           "api-ms-win-core-localization-l1-2-2.dll"};
+  for (const char* apiset : apisets) {
+    if (NO_ERROR ==
+        dwrite_hook->Hook(h_dwrite, apiset, "GetUserDefaultLCID",
+                          reinterpret_cast<void*>(HookedGetUserDefaultLCID))) {
+      return true;
+    }
+  }
+  return false;
 }
 
 }  // namespace sandbox::policy
