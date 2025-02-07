8b4c0ad0ebb98df1c04da06a42bcc47bf128b7b2
Andrey Kosyakov <caseq@chromium.org>
2025-02-07 11:26:00 -0800

    Revert "[devtools] Always use remote URL for DevTools frontend resources"
    This reverts commit 12d80f3b96758a10615ae399e2b9b3d56068fcad.
    
    Reason for revert: causes b/395045924
    
    Original change's description:
    > [devtools] Always use remote URL for DevTools frontend resources
    >
    > Historically, there were two ways used to retrieve the DevTools
    > frontend itself for remote debugging: Either fetch it from an appspot
    > service, or ask the "inspected" Chrome instance to provide it itself.
    >
    > The latter only worked when DevTools was included in the binary, which
    > is not the case for Android and iOS.
    >
    > Somewhere over the course of 15 years, using the bundled DevTools of
    > the "inspected" target to remote debug, broke. The problem is, that
    > we still attempt to use this approach when clicking "Inspect" button
    > in "chrome://inspect". Since attempting to use this approach fails,
    > we'll use the DevTools of the host, rather then fetching the
    > appropriate version.
    >
    > This CL fixes this by always using the remote appspot server,
    > regardless if the built Chrome bundles DevTools itself or not. This
    > fixes remote debugging of Chrome binaries that bundle DevTools itself.
    >
    > Note: We'll leave the HTTP serving of the DevTools resources in-place.
    > Even though Chrome itself hasn't used that in years, it might still be
    > in use by third parties.
    >
    > R=dsv@chromium.org
    >
    > Fixed: 391279334
    > Change-Id: I3a64efd5e4e37c00263970332211fa1a78009c02
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6217788
    > Reviewed-by: Danil Somsikov <dsv@chromium.org>
    > Commit-Queue: Simon Zünd <szuend@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1415431}
    
    Change-Id: I25f24a05a6feb2c0e46b23b2d0007c11cbc11af9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244102
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417482}

--- content/browser/devtools/devtools_http_handler.cc
+++ content/browser/devtools/devtools_http_handler.cc
@@ -521,12 +521,17 @@ std::string DevToolsHttpHandler::GetFrontendURLInternal(
     scoped_refptr<DevToolsAgentHost> agent_host,
     const std::string& id,
     const std::string& host) {
-  std::string type = agent_host->GetType();
-  bool is_worker = type == DevToolsAgentHost::kTypeServiceWorker ||
-                   type == DevToolsAgentHost::kTypeSharedWorker;
-  std::string frontend_url =
-      base::StringPrintf(kRemoteUrlPattern, GetChromiumGitRevision().c_str(),
-                         is_worker ? "worker_app" : "inspector");
+  std::string frontend_url;
+  if (delegate_->HasBundledFrontendResources()) {
+    frontend_url = "/devtools/inspector.html";
+  } else {
+    std::string type = agent_host->GetType();
+    bool is_worker = type == DevToolsAgentHost::kTypeServiceWorker ||
+                     type == DevToolsAgentHost::kTypeSharedWorker;
+    frontend_url =
+        base::StringPrintf(kRemoteUrlPattern, GetChromiumGitRevision().c_str(),
+                           is_worker ? "worker_app" : "inspector");
+  }
   return base::StringPrintf("%s?ws=%s%s%s", frontend_url.c_str(), host.c_str(),
                             kPageUrlPrefix, id.c_str());
 }
