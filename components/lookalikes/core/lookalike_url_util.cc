7f4dd84f8272fc3ee5f98c5c833a1b42d684c756
Mustafa Emre Acer <meacer@chromium.org>
2025-02-07 16:03:16 -0800

    [Merge M132][Lookalikes] Disallow hostnames containing certain ligatures
    On some platforms, system fonts contain ligatures that render the
    Google logo. This can be used to spoof hostnames.
    
    This CL blocks hostnames containing unsafe ligatures with an
    interstitial.
    
    (cherry picked from commit 8831e558be2ff9567efce5f1e9ea54e47d41490a)
    
    Bug: 391788835
    Change-Id: Ida694f42b2540ec61b78c7f9f6d0732a768c16c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227546
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1415328}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6244108
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6834@{#4994}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- components/lookalikes/core/lookalike_url_util.cc
+++ components/lookalikes/core/lookalike_url_util.cc
@@ -744,6 +744,27 @@ bool IsComboSquatting(
   return false;
 }
 
+// Hostnames containing these strings are considered unsafe due to ligature
+// rendering in some fonts.
+const char* kUnsafeLigatures[] = {
+    "g_logo", "o_logo", "l_logo", "e_logo",
+    // google_logo is also unsafe, but e_logo is its substring.
+    // super_g_logo is also unsafe, but g_logo is its substring.
+    "google_g", "glogoligature", "ologoligature", "llogoligature",
+    "elogoligature",
+    // googlelogoligature is also unsafe, but elogoligature is its
+    // substring
+};
+
+bool IsUnsafeLigature(const DomainInfo& domain) {
+  for (const char* unsafe_ligature : kUnsafeLigatures) {
+    if (domain.hostname.find(unsafe_ligature) != std::string::npos) {
+      return true;
+    }
+  }
+  return false;
+}
+
 }  // namespace
 
 namespace lookalikes {
@@ -1274,6 +1295,10 @@ bool IsPunycodeInterstitialCandidate(const DomainInfo& domain) {
 }
 
 bool ShouldBlockBySpoofCheckResult(const DomainInfo& navigated_domain) {
+  if (IsUnsafeLigature(navigated_domain)) {
+    return true;
+  }
+
   // Here, only a subset of spoof checks that cause an IDN to fallback to
   // punycode are configured to show an interstitial.
   switch (navigated_domain.idn_result.spoof_check_result) {
