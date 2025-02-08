916a6d3b8a6e8df3e8f7c7b0486b95bbf1ba9c0f
dpapad <dpapad@chromium.org>
2025-02-07 16:08:55 -0800

    WebUI: Support ESLint type aware checks in build_webui().
    Type-aware ESLint checks (see [1], [2]) can do more sophisticated checks
    compared to non type-aware checks which run during presubmit.
    
    - Implement a new `eslint_ts()` GN rule along with corresponding
      tests.
    - Hook it up to `build_webui()`.
    - Guard it with a new `enable_type_aware_eslint_checks` flag that
      defaults to `false`.
    
    The new flag will be enabled on a target-by-target basis or as a whole
    once existing violations are fixed. Initially applying only
    @typescript-eslint/require-await when the flag is enabled. More checks
    will be added eventually.
    
    [1] https://typescript-eslint.io/rules/?=typeInformation
    [2] https://typescript-eslint.io/getting-started/typed-linting/
    
    Bug: 394634491
    Change-Id: Id2255656786fc144f95801f399bc34520c910cca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5252273
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417633}

---
+++ ui/webui/resources/tools/eslint_ts.config_base.mjs
@@ -0,0 +1,31 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import typescriptEslint from '../../../../third_party/node/node_modules/@typescript-eslint/eslint-plugin/dist/index.js';
+import tsParser from '../../../../third_party/node/node_modules/@typescript-eslint/parser/dist/index.js';
+
+export default {
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    parser: tsParser,
+
+    // The following field should be specified by client code. as follows:
+    //
+    // parserOptions: {
+    //   project: [path.join(import.meta.dirname, './tsconfig_build_ts.json')],
+    // },
+  },
+
+  plugins: {
+    '@typescript-eslint': typescriptEslint,
+  },
+
+  files: ['**/*.ts'],
+
+  rules: {
+    'require-await': 'off',
+    '@typescript-eslint/require-await' : 'error',
+  },
+};
