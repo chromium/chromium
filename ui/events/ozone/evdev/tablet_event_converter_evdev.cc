53f2f8df7b83bc023717f681368ae82c01da4180
Mitsuru Oshima <oshima@chromium.org>
2025-02-07 16:03:26 -0800

    Don't round tablet event location to int
    Bug: 326401407
    Change-Id: Ie9b1ad6e6f689c8ca3155b47c77a781b7d81c070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6245369
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Kenneth Albanowski <kenalba@google.com>
    Reviewed-by: Vincent Chiang <vincentchiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1417626}

--- ui/events/ozone/evdev/tablet_event_converter_evdev.cc
+++ ui/events/ozone/evdev/tablet_event_converter_evdev.cc
@@ -200,7 +200,7 @@ void TabletEventConverterEvdev::ConvertAbsEvent(const input_event& input) {
       abs_value_dirty_ = true;
       break;
     case ABS_PRESSURE:
-      pressure_ = (float)input.value / pressure_max_;
+      pressure_ = static_cast<float>(input.value) / pressure_max_;
       abs_value_dirty_ = true;
       break;
   }
@@ -208,11 +208,10 @@ void TabletEventConverterEvdev::ConvertAbsEvent(const input_event& input) {
 
 void TabletEventConverterEvdev::UpdateCursor() {
   gfx::Rect confined_bounds = cursor_->GetCursorConfinedBounds();
-
-  int x =
-      ((x_abs_location_ - x_abs_min_) * confined_bounds.width()) / x_abs_range_;
-  int y = ((y_abs_location_ - y_abs_min_) * confined_bounds.height()) /
-          y_abs_range_;
+  float x = ((x_abs_location_ - x_abs_min_) * confined_bounds.width()) /
+            static_cast<float>(x_abs_range_);
+  float y = ((y_abs_location_ - y_abs_min_) * confined_bounds.height()) /
+            static_cast<float>(y_abs_range_);
 
   x += confined_bounds.x();
   y += confined_bounds.y();
