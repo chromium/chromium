c50b945e8ccacc4eb9c25bbdd16fc1933edb512a
Joel Hockey <joelhockey@chromium.org>
2025-02-09 17:32:36 -0800

    [Merge 133] Disable FileSystemAccessDirectoryIterationBlocklistCheck for webview only
    (cherry picked from commit 26fb97c86e24cb547baedcb719b6020356a39bb5)
    
    Bug: 393606977
    Change-Id: Iedb3e9a03c201463d923d763175918f661eb8678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6240766
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1417109}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6242313
    Cr-Commit-Position: refs/branch-heads/6943@{#1497}
    Cr-Branched-From: 72dd0b377c099e1e0230cc7345d5a5125b46ae7d-refs/heads/main@{#1402768}

--- android_webview/browser/aw_field_trials.cc
+++ android_webview/browser/aw_field_trials.cc
@@ -31,6 +31,12 @@
 
 namespace internal {
 
+// Duplicated from content/browser/file_system_access/features.cc to allow
+// WebView-only override.
+BASE_FEATURE(kFileSystemAccessDirectoryIterationBlocklistCheck,
+             "FileSystemAccessDirectoryIterationBlocklistCheck",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 AwFeatureOverrides::AwFeatureOverrides(base::FeatureList& feature_list)
     : feature_list_(feature_list) {}
 
@@ -292,4 +298,9 @@ void AwFieldTrials::RegisterFeatureOverrides(base::FeatureList* feature_list) {
 
   // Disable Topics on WebView.
   aw_feature_overrides.DisableFeature(blink::features::kBrowsingTopics);
+
+  // Temporarily turn off kFileSystemAccessDirectoryIterationBlocklistCheck for
+  // a kill switch. https://crbug.com/393606977
+  aw_feature_overrides.DisableFeature(
+      internal::kFileSystemAccessDirectoryIterationBlocklistCheck);
 }
