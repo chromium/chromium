f934999d30eaee4e7761ae4871d2d6b94148942e
Christoph Schwering <schwering@google.com>
2025-02-10 07:41:02 -0800

    [Autofill] Make EntityType::field_type() constexpr
    Bug: 388590912
    Change-Id: I05c10ee00746f6228340ede524d8b06ee467c748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249577
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1418143}

--- components/autofill/core/browser/data_model/entity_type.cc
+++ components/autofill/core/browser/data_model/entity_type.cc
@@ -11,63 +11,24 @@
 
 namespace autofill {
 
-namespace {
-
-constexpr FieldType AttributeTypeNameToFieldType(AttributeTypeName a) {
-  switch (a) {
-    case AttributeTypeName::kPassportName:
-      return PASSPORT_NAME_TAG;
-    case AttributeTypeName::kPassportNumber:
-      return PASSPORT_NUMBER;
-    case AttributeTypeName::kPassportCountry:
-      return PASSPORT_ISSUING_COUNTRY_TAG;
-    case AttributeTypeName::kPassportExpiryDate:
-      return PASSPORT_EXPIRATION_DATE_TAG;
-    case AttributeTypeName::kPassportIssueDate:
-      return PASSPORT_ISSUE_DATE_TAG;
-    case AttributeTypeName::kLoyaltyCardProgram:
-      return LOYALTY_MEMBERSHIP_PROGRAM;
-    case AttributeTypeName::kLoyaltyCardProvider:
-      return LOYALTY_MEMBERSHIP_PROVIDER;
-    case AttributeTypeName::kLoyaltyCardMemberId:
-      return LOYALTY_MEMBERSHIP_ID;
-    case AttributeTypeName::kCarOwner:
-    case AttributeTypeName::kCarLicensePlate:
-    case AttributeTypeName::kCarRegistration:
-    case AttributeTypeName::kCarMake:
-    case AttributeTypeName::kCarModel:
-      return UNKNOWN_TYPE;
-    case AttributeTypeName::kDriversLicenseName:
-    case AttributeTypeName::kDriversLicenseRegion:
-    case AttributeTypeName::kDriversLicenseNumber:
-    case AttributeTypeName::kDriversLicenseExpirationDate:
-    case AttributeTypeName::kDriversLicenseIssueDate:
-      return UNKNOWN_TYPE;
-  }
-  NOTREACHED();
-}
-
-// AttributeTypeNameToFieldType must be injective: distinct AttributeTypeNames
-// must be mapped to distinct FieldTypes or to UNKNOWN_TYPE.
+// AttributeType::field_type() must be injective: distinct AttributeTypes must
+// be mapped to distinct FieldTypes or to UNKNOWN_TYPE.
 static_assert(
     std::ranges::all_of(DenseSet<AttributeType>::all(), [](AttributeType a) {
       return std::ranges::all_of(
           DenseSet<AttributeType>::all(), [&a](AttributeType b) {
-            FieldType fta = AttributeTypeNameToFieldType(a.name());
-            FieldType ftb = AttributeTypeNameToFieldType(b.name());
-            return a == b || fta == UNKNOWN_TYPE || fta != ftb;
+            return a == b || a.field_type() == UNKNOWN_TYPE ||
+                   a.field_type() != b.field_type();
           });
     }));
 
-}  // namespace
-
 // static
 std::optional<AttributeType> AttributeType::FromFieldType(FieldType type) {
-  // This lookup table is the inverse of AttributeTypeNameToFieldType().
+  // This lookup table is the inverse of AttributeType::field_type().
   static constexpr auto kTable = []() {
     std::array<std::optional<AttributeType>, MAX_VALID_FIELD_TYPE> arr{};
     for (AttributeType at : DenseSet<AttributeType>::all()) {
-      FieldType ft = AttributeTypeNameToFieldType(at.name());
+      FieldType ft = at.field_type();
       CHECK(ft == UNKNOWN_TYPE || !arr[ft]);
       arr[ft] = ft != UNKNOWN_TYPE ? std::optional(at) : std::nullopt;
     }
@@ -93,10 +54,6 @@ bool EntityType::ImportOrder(const EntityType& lhs, const EntityType& rhs) {
     }
   };
   return rank(lhs) < rank(rhs);
-}
-
-FieldType AttributeType::field_type() const {
-  return AttributeTypeNameToFieldType(name_);
 }
 
 std::ostream& operator<<(std::ostream& os, AttributeType a) {
