7393dc7537795143cd44a2b9d42fed5adf6aa162
Rune Lillesveen <futhark@chromium.org>
2025-02-11 04:35:07 -0800

    Stop applying -internal-overflow with overflow-inline/block supported
    Start using logical overflow longhands in UA stylesheets for the HTML
    select element. Adding the @supports rule makes it easier to not forget
    we can remove the support for the -internal-overflow properties when the
    runtime flag is removed.
    
    Bug: 41489999
    Change-Id: I264edde5e79ed51ae39809c83d41270638d5b756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249382
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418576}

--- third_party/blink/renderer/core/html/forms/html_select_element_test.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element_test.cc
@@ -7,6 +7,8 @@
 #include <memory>
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/core/css/css_default_style_sheets.h"
+#include "third_party/blink/renderer/core/css/resolver/style_resolver.h"
 #include "third_party/blink/renderer/core/css/style_engine.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
@@ -25,6 +27,7 @@
 #include "third_party/blink/renderer/core/testing/sim/sim_request.h"
 #include "third_party/blink/renderer/core/testing/sim/sim_test.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
+#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
 
 namespace blink {
@@ -1069,5 +1072,30 @@ TEST_F(HTMLSelectElementSimTest, DialogModeBaseSelectNestedButton) {
 
   ASSERT_FALSE(select->IsInDialogMode());
 }
+
+class HTMLSelectElementUATest : public HTMLSelectElementTest,
+                                public testing::WithParamInterface<bool>,
+                                public ScopedCSSLogicalOverflowForTest {
+ public:
+  HTMLSelectElementUATest() : ScopedCSSLogicalOverflowForTest(GetParam()) {}
+};
+
+TEST_P(HTMLSelectElementUATest, OverflowStyle) {
+  CSSDefaultStyleSheets::Instance().PrepareForLeakDetection();
+  SetHtmlInnerHTML(R"HTML(
+    <select id="target" multiple></select>
+  )HTML");
+
+  HTMLSelectElement* select = To<HTMLSelectElement>(GetElementById("target"));
+  if (select->UsesMenuList()) {
+    // The UA styles won't apply when the menu list rendering is delegated.
+    return;
+  }
+  GetDocument().UpdateStyleAndLayoutTree();
+  EXPECT_EQ(select->ComputedStyleRef().OverflowX(), EOverflow::kHidden);
+  EXPECT_EQ(select->ComputedStyleRef().OverflowY(), EOverflow::kScroll);
+}
+
+INSTANTIATE_TEST_SUITE_P(All, HTMLSelectElementUATest, ::testing::Bool());
 
 }  // namespace blink
