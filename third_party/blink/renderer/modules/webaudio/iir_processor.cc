2679cb8b155569731c0c596c368239836a344b4f
Michael Wilson <mjwilson@chromium.org>
2025-02-12 12:39:09 -0800

    Move IIRDSPKernel into iir_processor.cc
    IIRFilterHandler refactor 1/6
    
    This CL should cause no functional changes.
    
    This is the first in a series of CLs to refactor the IIRFilterHandler.
    
    The goal is to eventually remove the AudioDSPKernel and Processor
    class hierarchy.
    
    In this CL, we move IIRDSPKernel verbatim into iir_processor.cc.  This
    will make it easier to collapse entirely in follow-up changes.
    
    Change-Id: Icf99d3ce04660fdab8dc8bcfa6f429a32569b6ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6240939
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419480}

--- third_party/blink/renderer/modules/webaudio/iir_processor.cc
+++ third_party/blink/renderer/modules/webaudio/iir_processor.cc
@@ -2,13 +2,103 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "third_party/blink/renderer/modules/webaudio/iir_processor.h"
 
 #include <memory>
 
-#include "third_party/blink/renderer/modules/webaudio/iir_dsp_kernel.h"
+#include "third_party/blink/renderer/platform/audio/audio_dsp_kernel.h"
+#include "third_party/blink/renderer/platform/audio/iir_filter.h"
+#include "third_party/blink/renderer/platform/wtf/math_extras.h"
 
 namespace blink {
+
+class IIRProcessor;
+
+class IIRDSPKernel final : public AudioDSPKernel {
+ public:
+  explicit IIRDSPKernel(IIRProcessor*);
+
+  // AudioDSPKernel
+  void Process(const float* source,
+               float* dest,
+               uint32_t frames_to_process) override;
+  void Reset() override { iir_.Reset(); }
+
+  // Get the magnitude and phase response of the filter at the given
+  // set of frequencies (in Hz). The phase response is in radians.
+  void GetFrequencyResponse(int n_frequencies,
+                            const float* frequency_hz,
+                            float* mag_response,
+                            float* phase_response);
+
+  double TailTime() const override;
+  double LatencyTime() const override;
+  bool RequiresTailProcessing() const final;
+
+ protected:
+  IIRFilter iir_;
+
+ private:
+  double tail_time_;
+};
+
+IIRDSPKernel::IIRDSPKernel(IIRProcessor* processor)
+    : AudioDSPKernel(processor),
+      iir_(processor->Feedforward(), processor->Feedback()) {
+  tail_time_ =
+      iir_.TailTime(processor->SampleRate(), processor->IsFilterStable(),
+                    processor->RenderQuantumFrames());
+}
+
+void IIRDSPKernel::Process(const float* source,
+                           float* destination,
+                           uint32_t frames_to_process) {
+  DCHECK(source);
+  DCHECK(destination);
+
+  iir_.Process(source, destination, frames_to_process);
+}
+
+void IIRDSPKernel::GetFrequencyResponse(int n_frequencies,
+                                        const float* frequency_hz,
+                                        float* mag_response,
+                                        float* phase_response) {
+  DCHECK_GE(n_frequencies, 0);
+  DCHECK(frequency_hz);
+  DCHECK(mag_response);
+  DCHECK(phase_response);
+
+  Vector<float> frequency(n_frequencies);
+
+  double nyquist = Nyquist();
+
+  // Convert from frequency in Hz to normalized frequency (0 -> 1),
+  // with 1 equal to the Nyquist frequency.
+  for (int k = 0; k < n_frequencies; ++k) {
+    frequency[k] = frequency_hz[k] / nyquist;
+  }
+
+  iir_.GetFrequencyResponse(n_frequencies, frequency.data(), mag_response,
+                            phase_response);
+}
+
+bool IIRDSPKernel::RequiresTailProcessing() const {
+  // Always return true even if the tail time and latency might both be zero.
+  return true;
+}
+
+double IIRDSPKernel::TailTime() const {
+  return tail_time_;
+}
+
+double IIRDSPKernel::LatencyTime() const {
+  return 0;
+}
 
 IIRProcessor::IIRProcessor(float sample_rate,
                            uint32_t number_of_channels,
