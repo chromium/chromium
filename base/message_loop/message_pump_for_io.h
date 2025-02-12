7ef83f9fc7ad476684fde6ffd750ee6a4469503a
Dave Tapuska <dtapuska@chromium.org>
2025-02-12 15:02:32 -0800

    [ios blink] Add a NSRunLoop IO message loop with libdispatch events
    Create a NSRunLoop based IO thread that uses the DispatchSource
    objects to monitor mach ports and file descriptors.
    
    Since kqueue and CFFileDescriptorRunLoopSource are blocked in the
    BrowserEngineKit extension sandboxes the only option to watch for
    IO events is to use libdispatch. Chromium is designed
    around specific threads and thread local storage so using libdispatch
    generally for task scheduling is not possible. To mitigate this we
    implement an IO thread that will use DispatchSource objects which
    get notified in a dispatch block (run on an arbitrary thread chosen by libdispatch) which then posts messages to the IO thread that the event occurred. This incurs a performance overhead of an additional thread
    hop for every IO event. We have asked Apple to support kqueue natively
    so we hope this workaround will not be long lasted.
    
    Bug: 40254930
    Change-Id: I381ee66b9c1a831c5ed6c0143e9ee9b35c76fbcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6249458
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419575}

--- base/message_loop/message_pump_for_io.h
+++ base/message_loop/message_pump_for_io.h
@@ -9,12 +9,15 @@
 // types representing MessagePumpForIO.
 
 #include "base/message_loop/ios_cronet_buildflags.h"
+#include "build/blink_buildflags.h"
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "base/message_loop/message_pump_win.h"
 #elif BUILDFLAG(IS_IOS) && BUILDFLAG(CRONET_BUILD)
 #include "base/message_loop/message_pump_io_ios.h"
+#elif BUILDFLAG(IS_IOS) && BUILDFLAG(USE_BLINK)
+#include "base/message_loop/message_pump_io_ios_libdispatch.h"
 #elif BUILDFLAG(IS_APPLE)
 #include "base/message_loop/message_pump_kqueue.h"
 #elif BUILDFLAG(IS_NACL)
@@ -32,6 +35,8 @@ namespace base {
 using MessagePumpForIO = MessagePumpForIO;
 #elif BUILDFLAG(IS_IOS) && BUILDFLAG(CRONET_BUILD)
 using MessagePumpForIO = MessagePumpIOSForIO;
+#elif BUILDFLAG(IS_IOS) && BUILDFLAG(USE_BLINK)
+using MessagePumpForIO = MessagePumpIOSForIOLibdispatch;
 #elif BUILDFLAG(IS_APPLE)
 using MessagePumpForIO = MessagePumpKqueue;
 #elif BUILDFLAG(IS_NACL)
