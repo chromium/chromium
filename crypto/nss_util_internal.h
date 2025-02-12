ce10cdcbcad86be8700019c847334480b9a78987
Georg Neis <neis@chromium.org>
2025-02-11 16:31:33 -0800

    Remove Lacros leftovers from crypto/
    Lacros is gone.
    
    - Replace IS_CHROMEOS_ASH uses with IS_CHROMEOS.
    - Replace is_chromeos uses with is_chromeos.
    - Remove IS_CHROMEOS_LACROS uses.
    - (There were no is_chromeos_lacros uses.)
    - Where appropriate, remove includes of build/chromeos_buildflags.h or
      replace with build/build_config.h.
    - Remove imports of build/config/chromeos/ui_mode.gni.
    
    Bug: b:354842935
    Change-Id: I61e0e51b06c7f89739754f1ad8f087ce781eecb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6242733
    Auto-Submit: Georg Neis <neis@chromium.org>
    Commit-Queue: Bob Beck <bbe@chromium.org>
    Reviewed-by: Bob Beck <bbe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1418996}

--- crypto/nss_util_internal.h
+++ crypto/nss_util_internal.h
@@ -11,7 +11,7 @@
 
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
-#include "build/chromeos_buildflags.h"
+#include "build/build_config.h"
 #include "components/nacl/common/buildflags.h"
 #include "crypto/crypto_export.h"
 #include "crypto/scoped_nss_types.h"
@@ -56,7 +56,7 @@ class CRYPTO_EXPORT AutoSECMODListReadLock {
   raw_ptr<SECMODListLock> lock_;
 };
 
-#if BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
+#if BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
 // Returns path to the NSS database file in the provided profile
 // directory.
 CRYPTO_EXPORT base::FilePath GetSoftwareNSSDBPath(
@@ -148,7 +148,7 @@ CRYPTO_EXPORT void CloseChromeOSUserForTesting(
 CRYPTO_EXPORT void SetPrivateSoftwareSlotForChromeOSUserForTesting(
     ScopedPK11Slot slot);
 
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
+#endif  // BUILDFLAG(IS_CHROMEOS) && !BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
 
 // Loads the given module for this NSS session.
 SECMODModule* LoadNSSModule(const char* name,
