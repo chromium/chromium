837158ca8af6268b5c96036118b6411549492d32
Joe Downing <joedow@google.com>
2025-02-11 20:25:32 -0800

    Removing pre-alpha Cloud provisioning path
    I am going to implement an updated Cloud provisioning path which will
    use the default service account associated with the Compute Engine
    Instance however before I start work I wanted to clean up the pre-
    alpha code which was used last year as that will greatly reduce the
    risk of regressions in the new codepath. The pre-alpha path can
    be removed as the only consumer of it has indicated they can pin
    the M134 host package if needed.
    
    Bug: 388886766
    Change-Id: Icc60229ddab12f798b90f8c1e9c205bf910cc217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6254843
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419081}

--- remoting/base/cloud_service_client.h
+++ remoting/base/cloud_service_client.h
@@ -34,10 +34,6 @@ struct NetworkTrafficAnnotationTag;
 
 namespace remoting {
 
-namespace apis::v1 {
-class ProvisionGceInstanceResponse;
-}  // namespace apis::v1
-
 class ProtobufHttpStatus;
 
 // A service client that communicates with the directory service.
@@ -51,9 +47,6 @@ class CloudServiceClient {
       const ProtobufHttpStatus&,
       std::unique_ptr<::google::internal::remoting::cloud::v1alpha::
                           GenerateIceConfigResponse>)>;
-  using LegacyProvisionGceInstanceCallback = base::OnceCallback<void(
-      const ProtobufHttpStatus&,
-      std::unique_ptr<apis::v1::ProvisionGceInstanceResponse>)>;
   using ProvisionGceInstanceCallback = base::OnceCallback<void(
       const ProtobufHttpStatus&,
       std::unique_ptr<
@@ -74,10 +67,6 @@ class CloudServiceClient {
       std::unique_ptr<::google::internal::remoting::cloud::v1alpha::
                           VerifySessionTokenResponse>)>;
 
-  // TODO: joedow - Remove the single param c'tor when we no longer support the
-  // legacy provisioning path.
-  explicit CloudServiceClient(
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
   // Used for creating a service client to call the Remoting Cloud API using
   // the |api_key| provided.
   CloudServiceClient(
@@ -93,14 +82,6 @@ class CloudServiceClient {
 
   CloudServiceClient(const CloudServiceClient&) = delete;
   CloudServiceClient& operator=(const CloudServiceClient&) = delete;
-
-  // TODO: joedow - Remove the legacy codepath once the new flow is working.
-  void LegacyProvisionGceInstance(
-      const std::string& owner_email,
-      const std::string& display_name,
-      const std::string& public_key,
-      const std::optional<std::string>& existing_directory_id,
-      LegacyProvisionGceInstanceCallback callback);
 
   void ProvisionGceInstance(
       const std::string& owner_email,
--- remoting/host/setup/cloud_host_starter.cc
+++ remoting/host/setup/cloud_host_starter.cc
@@ -16,27 +16,21 @@
 #include "remoting/base/cloud_service_client.h"
 #include "remoting/base/protobuf_http_status.h"
 #include "remoting/host/host_config.h"
-#include "remoting/host/pin_hash.h"
 #include "remoting/host/setup/host_starter.h"
 #include "remoting/host/setup/host_starter_base.h"
 #include "remoting/proto/google/remoting/cloud/v1/provisioning_service.pb.h"
-#include "remoting/proto/remoting/v1/cloud_messages.pb.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace remoting {
 
 namespace {
 
-using LegacyProvisionGceInstanceResponse =
-    apis::v1::ProvisionGceInstanceResponse;
 using ProvisionGceInstanceResponse =
     ::google::remoting::cloud::v1::ProvisionGceInstanceResponse;
 
 // A helper class which provisions a cloud machine for Chrome Remote Desktop.
 class CloudHostStarter : public HostStarterBase {
  public:
-  explicit CloudHostStarter(
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
   CloudHostStarter(
       const std::string& api_key,
       scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
@@ -56,9 +50,6 @@ class CloudHostStarter : public HostStarterBase {
   void OnProvisionGceInstanceResponse(
       const ProtobufHttpStatus& status,
       std::unique_ptr<ProvisionGceInstanceResponse> response);
-  void OnLegacyProvisionGceInstanceResponse(
-      const ProtobufHttpStatus& status,
-      std::unique_ptr<LegacyProvisionGceInstanceResponse> response);
 
  private:
   std::unique_ptr<CloudServiceClient> cloud_service_client_;
@@ -67,12 +58,6 @@ class CloudHostStarter : public HostStarterBase {
 
   base::WeakPtrFactory<CloudHostStarter> weak_ptr_factory_{this};
 };
-
-CloudHostStarter::CloudHostStarter(
-    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory)
-    : HostStarterBase(url_loader_factory),
-      cloud_service_client_(
-          std::make_unique<CloudServiceClient>(url_loader_factory)) {}
 
 CloudHostStarter::CloudHostStarter(
     const std::string& api_key,
@@ -94,40 +79,15 @@ void CloudHostStarter::RegisterNewHost(
   DCHECK(!public_key.empty());
   DCHECK(!access_token.has_value());
 
-  // TODO: joedow - Remove the legacy code path once the new flow is supported.
-  if (!params().api_key.empty()) {
-    cloud_service_client_->ProvisionGceInstance(
-        params().owner_email, params().name, public_key, existing_host_id(),
-        base::BindOnce(&CloudHostStarter::OnProvisionGceInstanceResponse,
-                       weak_ptr_factory_.GetWeakPtr()));
-  } else {
-    cloud_service_client_->LegacyProvisionGceInstance(
-        params().owner_email, params().name, public_key, existing_host_id(),
-        base::BindOnce(&CloudHostStarter::OnLegacyProvisionGceInstanceResponse,
-                       weak_ptr_factory_.GetWeakPtr()));
-  }
+  cloud_service_client_->ProvisionGceInstance(
+      params().owner_email, params().name, public_key, existing_host_id(),
+      base::BindOnce(&CloudHostStarter::OnProvisionGceInstanceResponse,
+                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 void CloudHostStarter::OnProvisionGceInstanceResponse(
     const ProtobufHttpStatus& status,
     std::unique_ptr<ProvisionGceInstanceResponse> response) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  if (!status.ok()) {
-    HandleHttpStatusError(status);
-    return;
-  }
-
-  OnNewHostRegistered(
-      base::ToLowerASCII(response->directory_id()),
-      /*owner_account_email=*/std::string(),
-      base::ToLowerASCII(response->service_account_info().email()),
-      response->service_account_info().authorization_code());
-}
-
-void CloudHostStarter::OnLegacyProvisionGceInstanceResponse(
-    const ProtobufHttpStatus& status,
-    std::unique_ptr<LegacyProvisionGceInstanceResponse> response) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (!status.ok()) {
@@ -154,19 +114,8 @@ void CloudHostStarter::RemoveOldHostFromDirectory(
 void CloudHostStarter::ApplyConfigValues(base::Value::Dict& config) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  // The legacy code is taken when an API_KEY is not provided. In that case, the
-  // host acts like a Me2Me host rather than a Cloud host. Note that is kept for
-  // compatibility reasons but is not going to be supported long-term.
-  // TODO: joedow - Remove this when all Cloud hosts are required to use the
-  // Cloud API.
-  if (params().api_key.empty()) {
-    config.Set(kHostTypeHintPath, kMe2MeHostTypeHint);
-    config.Set(kHostSecretHashConfigPath,
-               MakeHostPinHash(params().id, params().pin));
-  } else {
-    config.Set(kHostTypeHintPath, kCloudHostTypeHint);
-    config.Set(kRequireSessionAuthorizationPath, true);
-  }
+  config.Set(kHostTypeHintPath, kCloudHostTypeHint);
+  config.Set(kRequireSessionAuthorizationPath, true);
 }
 
 }  // namespace
@@ -174,12 +123,6 @@ void CloudHostStarter::ApplyConfigValues(base::Value::Dict& config) {
 std::unique_ptr<HostStarter> ProvisionCloudInstance(
     const std::string& api_key,
     scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory) {
-  // TODO: joedow - Remove this bit when we no longer support the legacy
-  // provisioning code path.
-  if (api_key.empty()) {
-    return std::make_unique<CloudHostStarter>(url_loader_factory);
-  }
-
   return std::make_unique<CloudHostStarter>(api_key, url_loader_factory);
 }
 
