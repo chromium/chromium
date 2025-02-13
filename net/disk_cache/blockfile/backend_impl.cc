ecf2f26f2990fbed2114f400df5f9d258dacefa1
Patrick Meenan <pmeenan@chromium.org>
2025-02-13 06:34:43 -0800

    Switched blockfile dump to DLOG for a failed hash lookup
    This removes the dump when the blockfile cache fails to resolve a hash
    collision and changes it to a DLOG(WARNING) instead. The behavior
    in unchanged and it returns a failure in creating the entry (safe)
    and there aren't any plans to improve the blockfile cache.
    
    Bug: 395482242
    Change-Id: I08a23a6355a00be592ae20e7a2ac455d20428238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6258507
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Patrick Meenan <pmeenan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1419862}

--- net/disk_cache/blockfile/backend_impl.cc
+++ net/disk_cache/blockfile/backend_impl.cc
@@ -23,6 +23,7 @@
 #include "base/hash/hash.h"
 #include "base/lazy_instance.h"
 #include "base/location.h"
+#include "base/logging.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/histogram_functions.h"
@@ -544,7 +545,7 @@ scoped_refptr<EntryImpl> BackendImpl::CreateEntryImpl(const std::string& key) {
     DCHECK(!error);
     if (!parent && data_->table[hash & mask_]) {
       // We should have corrected the problem.
-      DUMP_WILL_BE_NOTREACHED();
+      DLOG(WARNING) << "Unable to correct hash collision";
       return nullptr;
     }
   }
