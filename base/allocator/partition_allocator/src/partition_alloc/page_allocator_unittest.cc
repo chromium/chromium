45b8db21d92c56866615a98b8973ffc841f72a33
Dave Tapuska <dtapuska@chromium.org>
2025-02-13 15:07:26 -0800

    [ios blink] Set MAP_JIT pages to RWX by default
    mprotect always fails with MAP_JIT'd pages so we have
    to set the correct permissions at creation time. Always allocate
    the pages as RWX. This permits v8 to actually create executable
    memory and run javascript on an iPhone. Some pages fail
    (mainly those with workers) because calling MAP_JIT more
    than once will fail. Pages such as workers will create an
    isolate which then creates a new v8 heap where code pages
    are stored for that thread.
    
    Bug: 40254930
    Change-Id: Ic8119b9d3a3e30dc2edfa5fcf173d6db0e228c0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6259474
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420181}

--- base/allocator/partition_allocator/src/partition_alloc/page_allocator_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/page_allocator_unittest.cc
@@ -634,15 +634,16 @@ TEST(PartitionAllocPageAllocatorTest, MappedPagesAccounting) {
   }
 }
 
-#if PA_BUILDFLAG(IS_IOS) && !TARGET_IPHONE_SIMULATOR
+#if PA_BUILDFLAG(IS_IOS)
 // MAP_JIT is not supported without the com.apple.developer.cs.allow-jit
-// entitlement which unittests do not have. The entitlement is not checked in
-// the simulator so allow the test to run there.
+// entitlement which unittests do not have. To toggle W^X of pages
+// BrowserEngineKit library is needed which is not supported
+// until the 17.4 SDK.
 #define MAYBE_AllocInaccessibleWillJitLater \
   DISABLED_AllocInaccessibleWillJitLater
 #else
 #define MAYBE_AllocInaccessibleWillJitLater AllocInaccessibleWillJitLater
-#endif  // PA_BUILDFLAG(IS_IOS) && !TARGET_IPHONE_SIMULATOR
+#endif  // PA_BUILDFLAG(IS_IOS)
 TEST(PartitionAllocPageAllocatorTest, MAYBE_AllocInaccessibleWillJitLater) {
   // Verify that kInaccessibleWillJitLater allows read/write, and read/execute
   // permissions to be set.
