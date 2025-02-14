4b9c8d6ddb87ac8845f2f0ec6d307d208651d48e
Zack Han <zackhan@chromium.org>
2025-02-14 10:52:29 -0800

    [Code health] Clean up stale base::Feature "ExoLinuxDmabufV3
    Bug: 356234242
    Change-Id: Ia2115a970210a41851bbb323229216a0b224de9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6258915
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1420607}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -788,13 +788,6 @@ BASE_FEATURE(kExoHapticFeedbackSupport,
              "ExoHapticFeedbackSupport",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-// Enables version 3 of the zwp_linux_dmabuf_v1 Wayland protocol.
-// This version adds support for DRM modifiers and is required by Mesas Vulkan
-// WSI, which otherwise falls back to software rendering.
-BASE_FEATURE(kExoLinuxDmabufV3,
-             "ExoLinuxDmabufV3",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // Enables version 4 of the zwp_linux_dmabuf_v1 Wayland protocol.
 // This version adds support for dynamic feedback, allowing the compositor to
 // give clients hints about more optimal DRM formats and modifiers depending on
