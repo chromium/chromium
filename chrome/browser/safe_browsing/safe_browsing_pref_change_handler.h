57d86207ab9dc32f0712a64595562b7249be6c30
Zack Han <zackhan@chromium.org>
2025-02-14 11:33:47 -0800

    [M134][Sync ESB]Integrate retry mechanisms into pref change handler
    This CL integrates the `MessageRetryHandler` class into the
    `SafeBrowsingPrefChangeHandler` to provide robust retry mechanisms for
    displaying the enhanced protection setting change notification.
    
    The following changes were made:
    
    * Integrated the `MessageRetryHandler` to handle retry logic based on
      preferences, ensuring the notification is displayed after a delay if
      conditions are not initially met.
    * Implemented an observer-based retry mechanism to monitor tab creation
      and attempt displaying the notification again when a new tab is
      available.
    * Added a preference value to prevent displaying the notification
      multiple times, improving the user experience.
    
    Bug: 394088849
    Fixed: 396656183
    
    (cherry picked from commit 7d849913def6ab9b3db84b18554dca60088739f9)
    
    Change-Id: I7f5a51e41b6694156156281dcd6d2d526b9d2618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6227279
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1419571}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6270317
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6998@{#741}
    Cr-Branched-From: de9c6fafd8ae5c6ea0438764076ca7d04a0b165d-refs/heads/main@{#1415337}

--- chrome/browser/safe_browsing/safe_browsing_pref_change_handler.h
+++ chrome/browser/safe_browsing/safe_browsing_pref_change_handler.h
@@ -14,6 +14,8 @@
 #include "chrome/browser/safe_browsing/tailored_security/chrome_tailored_security_service.h"
 #include "chrome/browser/safe_browsing/tailored_security/consented_message_android.h"
 #include "chrome/browser/safe_browsing/tailored_security/unconsented_message_android.h"
+#include "chrome/browser/ui/android/tab_model/tab_model_list_observer.h"
+#include "chrome/browser/ui/android/tab_model/tab_model_observer.h"
 #endif
 
 class Profile;
@@ -22,10 +24,32 @@ namespace safe_browsing {
 
 // Handles showing the appropriate toast or modal when the Safe Browsing
 // protection setting changes. This class is not thread-safe.
+#if BUILDFLAG(IS_ANDROID)
+class SafeBrowsingPrefChangeHandler : public TabModelObserver,
+                                      public TabModelListObserver {
+#else
 class SafeBrowsingPrefChangeHandler {
+#endif
  public:
   SafeBrowsingPrefChangeHandler();
+#if BUILDFLAG(IS_ANDROID)
+  ~SafeBrowsingPrefChangeHandler() override;
+#else
   virtual ~SafeBrowsingPrefChangeHandler();
+#endif
+
+  // The amount of time to wait after construction before checking if a retry is
+  // needed.
+  static constexpr const base::TimeDelta kRetryAttemptStartupDelay =
+      base::Minutes(2);
+
+  // The amount of time to wait between retry attempts.
+  static constexpr const base::TimeDelta kRetryNextAttemptDelay = base::Days(1);
+
+  // Length of time that the retry mechanism will wait before running. This
+  // delay is used for the case where the safe browsing pref change handler
+  // can't tell if it succeeded in the past.
+  static constexpr const base::TimeDelta kWaitingPeriodInterval = base::Days(2);
 
   // Handles notifying the user when necessary. The type of notification shown
   // depends on the platform and whether the user is currently on the security
@@ -38,8 +62,62 @@ class SafeBrowsingPrefChangeHandler {
   // Called when the consented modal is dismissed.
   void ConsentedMessageDismissed();
 
+  friend class SafeBrowsingPrefChangeHandlerAndroidTest;
+
+  FRIEND_TEST_ALL_PREFIXES(SafeBrowsingPrefChangeHandlerAndroidTest,
+                           AddAndRemoveTabModelListObserver);
+  FRIEND_TEST_ALL_PREFIXES(SafeBrowsingPrefChangeHandlerAndroidTest,
+                           AddAndRemoveTabModelObserver);
+  FRIEND_TEST_ALL_PREFIXES(SafeBrowsingPrefChangeHandlerAndroidTest,
+                           AddTabModelObserver_NoMatchingProfile);
+  FRIEND_TEST_ALL_PREFIXES(SafeBrowsingPrefChangeHandlerAndroidTest,
+                           RegisterObserver);
+  FRIEND_TEST_ALL_PREFIXES(SafeBrowsingPrefChangeHandlerAndroidTest, DidAddTab);
+  FRIEND_TEST_ALL_PREFIXES(SafeBrowsingPrefChangeHandlerAndroidTest,
+                           DidAddTab_NullTab);
+  FRIEND_TEST_ALL_PREFIXES(SafeBrowsingPrefChangeHandlerAndroidTest,
+                           OnTabModelAddedAndRemoved);
+
+  // Functions used for testing.
+  // Sets the TabModel for testing purposes.
+  void SetTabModelForTesting(TabModel* tab_model);
+
+  // Checks if the handler is currently observing the TabModelList.
+  bool IsObservingTabModelListForTesting() const;
+
+  // Checks if the handler is currently observing a TabModel.
+  bool IsObservingTabModelForTesting() const;
+
+  // Updates the retry state and tris to show notification when needed.
+  void RetryStateCallback(Profile* profile);
+
+  // Registers this as an observer on the TabModelList and, if possible, on a
+  // TabModel.
+  void RegisterObserver(Profile* profile);
+  void AddTabModelListObserver();
+  void AddTabModelObserver(Profile* profile);
+  void RemoveTabModelListObserver();
+  void RemoveTabModelObserver();
+
+  // TabModelObserver::
+  void DidAddTab(TabAndroid* tab, TabModel::TabLaunchType type) override;
+  // TabModelListObserver::
+  void OnTabModelAdded() override;
+  void OnTabModelRemoved() override;
+
+  // This tab model is used for the observer based retry mechanism.
+  // We can't depend on this being set as a tab can be deleted at
+  // any time.
+  raw_ptr<TabModel> observed_tab_model_ = nullptr;
+  bool observing_tab_model_list_ = false;
+
+  // The retry handler used to manage retry logic.
+  std::unique_ptr<MessageRetryHandler> retry_handler_;
+
   // The modal that is shown to the user.
   std::unique_ptr<TailoredSecurityConsentedModalAndroid> message_;
+
+  base::WeakPtrFactory<SafeBrowsingPrefChangeHandler> weak_ptr_factory_{this};
 #endif
 };
 
