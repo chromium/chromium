32f7cb889cc64e3a9e37923772de5ec79c1a0515
Kenneth Russell <kbr@chromium.org>
2025-02-14 16:26:58 -0800

    Allow larger buffers in multi-draw extensions.
    Reuse the Int32List and Uint32List typedefs from
    WebGLRenderingContextBase, which fixed many other entry points in the
    same way, using PassAsSpan and BufferSourceTypeNoSizeLimit.
    
    This also allowed the MakeSpan helpers and Impl functions to be
    removed from the multi-draw extensions, simplifying the code.
    
    Tested with forthcoming WebGL conformance test in
    https://github.com/KhronosGroup/WebGL/pull/3716 .
    
    Fixed: 395670641
    Change-Id: If3d528888a2348755c7445b0f9a1bc2075c63abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6270738
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Shrek Shao <shrekshao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1420801}

--- third_party/blink/renderer/modules/webgl/webgl_multi_draw.h
+++ third_party/blink/renderer/modules/webgl/webgl_multi_draw.h
@@ -21,94 +21,39 @@ class WebGLMultiDraw final : public WebGLExtension,
   explicit WebGLMultiDraw(WebGLRenderingContextBase*);
   WebGLExtensionName GetName() const override;
 
-  void multiDrawArraysWEBGL(
-      GLenum mode,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* firstsList,
-      GLuint firstsOffset,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* countsList,
-      GLuint countsOffset,
-      GLsizei drawcount) {
-    multiDrawArraysImpl(mode, MakeSpan(firstsList), firstsOffset,
-                        MakeSpan(countsList), countsOffset, drawcount);
-  }
+  void multiDrawArraysWEBGL(GLenum mode,
+                            base::span<const int32_t> firstsList,
+                            GLuint firstsOffset,
+                            base::span<const int32_t> countsList,
+                            GLuint countsOffset,
+                            GLsizei drawcount);
 
-  void multiDrawElementsWEBGL(
-      GLenum mode,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* countsList,
-      GLuint countsOffset,
-      GLenum type,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* offsetsList,
-      GLuint offsetsOffset,
-      GLsizei drawcount) {
-    multiDrawElementsImpl(mode, MakeSpan(countsList), countsOffset, type,
-                          MakeSpan(offsetsList), offsetsOffset, drawcount);
-  }
+  void multiDrawElementsWEBGL(GLenum mode,
+                              base::span<const int32_t> countsList,
+                              GLuint countsOffset,
+                              GLenum type,
+                              base::span<const int32_t> offsetsList,
+                              GLuint offsetsOffset,
+                              GLsizei drawcount);
 
   void multiDrawArraysInstancedWEBGL(
       GLenum mode,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* firstsList,
+      base::span<const int32_t> firstsList,
       GLuint firstsOffset,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* countsList,
+      base::span<const int32_t> countsList,
       GLuint countsOffset,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* instanceCountsList,
-      GLuint instanceCountsOffset,
-      GLsizei drawcount) {
-    multiDrawArraysInstancedImpl(mode, MakeSpan(firstsList), firstsOffset,
-                                 MakeSpan(countsList), countsOffset,
-                                 MakeSpan(instanceCountsList),
-                                 instanceCountsOffset, drawcount);
-  }
-
-  void multiDrawElementsInstancedWEBGL(
-      GLenum mode,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* countsList,
-      GLuint countsOffset,
-      GLenum type,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* offsetsList,
-      GLuint offsetsOffset,
-      const V8UnionInt32ArrayAllowSharedOrLongSequence* instanceCountsList,
-      GLuint instanceCountsOffset,
-      GLsizei drawcount) {
-    multiDrawElementsInstancedImpl(mode, MakeSpan(countsList), countsOffset,
-                                   type, MakeSpan(offsetsList), offsetsOffset,
-                                   MakeSpan(instanceCountsList),
-                                   instanceCountsOffset, drawcount);
-  }
-
- private:
-  void multiDrawArraysImpl(GLenum mode,
-                           const base::span<const int32_t>& firsts,
-                           GLuint firstsOffset,
-                           const base::span<const int32_t>& counts,
-                           GLuint countsOffset,
-                           GLsizei drawcount);
-
-  void multiDrawElementsImpl(GLenum mode,
-                             const base::span<const int32_t>& counts,
-                             GLuint countsOffset,
-                             GLenum type,
-                             const base::span<const int32_t>& offsets,
-                             GLuint offsetsOffset,
-                             GLsizei drawcount);
-
-  void multiDrawArraysInstancedImpl(
-      GLenum mode,
-      const base::span<const int32_t>& firsts,
-      GLuint firstsOffset,
-      const base::span<const int32_t>& counts,
-      GLuint countsOffset,
-      const base::span<const int32_t>& instanceCounts,
+      base::span<const int32_t> instanceCountsList,
       GLuint instanceCountsOffset,
       GLsizei drawcount);
 
-  void multiDrawElementsInstancedImpl(
+  void multiDrawElementsInstancedWEBGL(
       GLenum mode,
-      const base::span<const int32_t>& counts,
+      base::span<const int32_t> countsList,
       GLuint countsOffset,
       GLenum type,
-      const base::span<const int32_t>& offsets,
+      base::span<const int32_t> offsetsList,
       GLuint offsetsOffset,
-      const base::span<const int32_t>& instanceCounts,
+      base::span<const int32_t> instanceCountsList,
       GLuint instanceCountsOffset,
       GLsizei drawcount);
 };
