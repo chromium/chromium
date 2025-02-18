6ce3266c7b879292424d501210dc925d7d34c91c
Jonathan Hao <phao@chromium.org>
2025-02-18 12:20:00 -0800

    Roll prost-derive: 0.13.4 => 0.13.5 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * prost-derive: 0.13.4 => 0.13.5
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * prost-derive@0.13.5: crypto-safe, safe-to-run
    
    R=lukasza@chromium.org
    
    Bug: None
    Change-Id: I8304e9c18d2b7511d2953cbf9f2e9fb37b36f9ef
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276395
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Jonathan Hao <phao@chromium.org>
    Auto-Submit: Jonathan Hao <phao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421579}

--- third_party/rust/chromium_crates_io/vendor/prost-derive-0.13.5/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/prost-derive-0.13.5/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/prost-derive/0.13.4")]
+#![doc(html_root_url = "https://docs.rs/prost-derive/0.13.5")]
 // The `quote!` macro requires deep recursion.
 #![recursion_limit = "4096"]
 
