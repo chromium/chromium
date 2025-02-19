61ccd5989171052e64c7cf4f305b71a20f15d3a4
Maria Kazinova <kazinova@google.com>
2025-02-19 09:04:39 -0800

    Initialize camelCase splitting regexes statically.
    https://chromium-review.googlesource.com/c/chromium/src/+/6275290
    improved the regexes for splitting strings on camel-case, but it
    also made them heavier. This CL changes the code, so they are
    initialized statically, and thus parsed once and not on every string
    tokenization.
    
    Bug: 397535331, 397151724
    Change-Id: Iec6df2d2c29dab97ccd4e791abe6bbf4d6990744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6281688
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1421999}

--- components/autofill/core/browser/ml_model/field_classification_model_encoder.cc
+++ components/autofill/core/browser/ml_model/field_classification_model_encoder.cc
@@ -12,6 +12,7 @@
 
 #include "base/containers/flat_map.h"
 #include "base/containers/to_vector.h"
+#include "base/no_destructor.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -184,10 +185,15 @@ std::u16string FieldClassificationModelEncoder::StandardizeString(
   std::u16string standardized_input(input);
   if (encoding_parameters_.split_on_camel_case()) {
     std::string utf8_input = base::UTF16ToUTF8(standardized_input);
-    re2::RE2::GlobalReplace(&utf8_input, re2::RE2("(\\p{Ll})(\\p{Lu})"),
+
+    static base::NoDestructor<re2::RE2> kCamelCaseLowerUpperSplitRe(
+        "(\\p{Ll})(\\p{Lu})");
+    static base::NoDestructor<re2::RE2> kCamelCaseAcronymSplitRe(
+        "(\\p{Lu})(\\p{Lu}\\p{Ll})");
+    re2::RE2::GlobalReplace(&utf8_input, *kCamelCaseLowerUpperSplitRe,
                             "\\1 \\2");
-    re2::RE2::GlobalReplace(&utf8_input, re2::RE2("(\\p{Lu})(\\p{Lu}\\p{Ll})"),
-                            "\\1 \\2");
+    re2::RE2::GlobalReplace(&utf8_input, *kCamelCaseAcronymSplitRe, "\\1 \\2");
+
     standardized_input = base::UTF8ToUTF16(utf8_input);
   }
 
