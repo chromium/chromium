4856eb27578cd33fba82b644cd8651ad7170eccc
Tatsuyuki Ishi <ishitatsuyuki@google.com>
2025-02-19 15:58:17 -0800

    Add a Rust-based ETC1 texture compressor
    This is an ETC1 (GPU texture) compressor to replace the current ETC1
    compressor sourced from AOSP, which is slow and can disrupt foreground
    jobs. The new texture compressor is written in Rust, with a focus on
    performance, safety and portability.
    
    This compressor will be gated behind a build flag that can be turned off
    if a toolchain roll breaks build due to nightly features.
    
    Design docs (internal only): go/clank-rust-etc1-encoder
    Bug: 369986099
    
    Change-Id: I038769145dd5a6109a0273f8b2200b7ff30b9a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6218420
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Tatsuyuki Ishi <ishitatsuyuki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422262}

---
+++ ui/android/texture_compressor/selectors_tests.rs
@@ -0,0 +1,90 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+use rust_gtest_interop::prelude::*;
+use std::simd::prelude::*;
+
+chromium::import! {
+    "//ui/android:texture_compressor";
+}
+
+use texture_compressor::selectors::{
+    flip_pixels, flip_selectors, search_table_and_selectors_subblock, TABLES,
+};
+
+#[gtest(TextureCompressorTest, FlipPixels)]
+fn test_flip_pixels() {
+    #[rustfmt::skip]
+    let input = [
+        [ 0,  1,  2,  3],
+        [ 4,  5,  6,  7],
+        [ 8,  9, 10, 11],
+        [12, 13, 14, 15],
+    ];
+    let input = input.map(|row| row.map(|x| [Simd::splat(x); 3]));
+    #[rustfmt::skip]
+    let expected = [
+        [ 0,  1,  8,  9],
+        [ 4,  5, 12, 13],
+        [ 2,  3, 10, 11],
+        [ 6,  7, 14, 15],
+    ];
+    let expected = expected.map(|row| row.map(|x| [Simd::splat(x); 3]));
+    expect_eq!(flip_pixels(&input, Mask::splat(true)), expected);
+}
+
+#[gtest(TextureCompressorTest, FlipSelectors)]
+fn test_flip_selectors() {
+    #[rustfmt::skip]
+    let input = [
+         0,  1,  2,  3,
+         4,  5,  6,  7,
+         8,  9, 10, 11,
+        12, 13, 14, 15,
+    ];
+    let input = input.map(|x| Simd::splat(1u16 << x));
+    #[rustfmt::skip]
+    let expected = [
+         0,  1,  8,  9,
+         4,  5, 12, 13,
+         2,  3, 10, 11,
+         6,  7, 14, 15,
+    ];
+    let expected = expected.map(|x| Simd::splat(1u16 << x));
+    for (input, expected) in input.into_iter().zip(expected) {
+        expect_eq!(flip_selectors(input, Mask::splat(true)), expected);
+    }
+}
+
+#[gtest(TextureCompressorTest, SearchTableAndSelectors)]
+fn test_search_table_and_selectors() {
+    for (table_idx, table) in TABLES.iter().enumerate() {
+        let (sm, lg) = (table[0], table[1]);
+        let input = [[sm, lg, -sm, -lg], [-lg, -sm, lg, sm]]
+            .map(|row| row.map(|x| [Simd::splat(128 + x); 3]));
+        let base_color = [Simd::splat(128); 3];
+        let result = search_table_and_selectors_subblock(&input, base_color);
+        // The bits are arranged as col3_col2_col1_col0, with top 2 bits of each column
+        // being zeroed
+        expect_eq!(result.selector_hi, Simd::splat(0b0001_0001_0010_0010));
+        expect_eq!(result.selector_lo, Simd::splat(0b0001_0010_0001_0010));
+        expect_eq!(result.table_idx, Simd::splat(table_idx as u16));
+    }
+}
+
+#[gtest(TextureCompressorTest, SearchTableAndSelectorsMax)]
+fn test_search_table_and_selectors_max() {
+    // Test for overflow handling of the error function.
+    // The base color is set to an arbitrarily far value rather than being the
+    // average. In this case, table 7 has the smallest but non-zero error value.
+    // This test can catch overflows if it causes smaller tables to achieve a
+    // smaller error value than table 7.
+    let input = [[[Simd::splat(255); 3]; 4]; 2];
+    let base_color = [Simd::splat(0); 3];
+    let result = search_table_and_selectors_subblock(&input, base_color);
+    // See test_search_table_and_selectors for notes on bit arrangement.
+    expect_eq!(result.selector_hi, Simd::splat(0)); // All positive
+    expect_eq!(result.selector_lo, Simd::splat(0b0011_0011_0011_0011)); // All large
+    expect_eq!(result.table_idx, Simd::splat(7)); // Largest variance table
+}
