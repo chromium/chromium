93caa94cda601507422efab32ba3364f915b8c54
Koji Ishii <kojii@chromium.org>
2025-02-20 01:43:12 -0800

    [memory] Remove the pointer to `FragmentItem` in `FinalizeAfterLayout`
    This patch replaces the pointer to `FragmenntItem` to a
    pointer to `PhysicalBoxFragment` and an index of the
    `FragmentItem`.
    
    Because `FragmentItem` is a traceable DISALLOW_NEW object,
    this change avoids the risk when the backing buffer of
    `HeapHashMap` is moved.
    
    This patch doesn't have visible impacts to the Speedometer3
    score.
    
    Bug: 389707047
    Change-Id: I1fb77bbf30b0debea789048e70d9c8da2543d425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6275509
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422416}

--- third_party/blink/renderer/core/layout/inline/fragment_items.cc
+++ third_party/blink/renderer/core/layout/inline/fragment_items.cc
@@ -74,16 +74,50 @@ bool FragmentItems::IsSubSpan(const Span& span) const {
 void FragmentItems::FinalizeAfterLayout(
     const HeapVector<Member<const LayoutResult>, 1>& results,
     LayoutBlockFlow& container) {
+  // This class keeps the parent GC objecct and index in case the backing buffer
+  // is moved, because `FragmentItem` is a traceable DISALLOW_NEW objects,
+  class FragmentItemPtr {
+    DISALLOW_NEW();
+
+   public:
+    FragmentItemPtr() = default;
+    FragmentItemPtr(const FragmentItem& item,
+                    const PhysicalBoxFragment& fragment,
+                    const Span& items)
+        : fragment_(fragment), index_(&item - items.data()) {
+#if EXPENSIVE_DCHECKS_ARE_ON()
+      DCHECK_EQ(fragment.Items()->Items().data(), items.data());
+#endif  // EXPENSIVE_DCHECKS_ARE_ON()
+    }
+
+    const FragmentItem* Get() const {
+      return fragment_ ? &(*fragment_->Items())[index_] : nullptr;
+    }
+    const FragmentItem& operator*() const { return *Get(); }
+
+    void Trace(Visitor* visitor) const { visitor->Trace(fragment_); }
+
+   private:
+    Member<const PhysicalBoxFragment> fragment_;
+    wtf_size_t index_ = 0;
+  };
+
+  // This struct keeps the last `FragmentItem` and other properties for a
+  // `LayoutObject`. Their lifecycles are only within this function.
   struct LastItem {
-    GC_PLUGIN_IGNORE("GC API violation: https://crbug.com/389707047")
-    const FragmentItem* item;
+    DISALLOW_NEW();
+
+   public:
+    void Trace(Visitor* visitor) const { visitor->Trace(item); }
+
+    FragmentItemPtr item;
     wtf_size_t fragment_id;
-    wtf_size_t item_index;
+    wtf_size_t total_item_index;
   };
   HeapHashMap<Member<const LayoutObject>, LastItem> last_items;
   ClearCollectionScope<HeapHashMap<Member<const LayoutObject>, LastItem>>
       clear_scope(&last_items);
-  wtf_size_t item_index = 0;
+  wtf_size_t total_item_index = 0;
   wtf_size_t line_fragment_id = FragmentItem::kInitialLineFragmentId;
 
   // If there are container fragments that don't have fragment items, or if
@@ -104,10 +138,10 @@ void FragmentItems::FinalizeAfterLayout(
     }
 
     bool found_inflow_content = false;
-    fragment_items->size_of_earlier_fragments_ = item_index;
+    fragment_items->size_of_earlier_fragments_ = total_item_index;
     const Span items = fragment_items->Items();
     for (const FragmentItem& item : items) {
-      ++item_index;
+      ++total_item_index;
       if (item.Type() == FragmentItem::kLine) {
         DCHECK_EQ(item.DeltaToNextForSameLayoutObject(), 0u);
         item.SetFragmentId(line_fragment_id++);
@@ -156,23 +190,26 @@ void FragmentItems::FinalizeAfterLayout(
 
       // If this is the first fragment, associate with |layout_object|.
       const auto last_item_result =
-          last_items.insert(layout_object, LastItem{&item, 0, item_index});
+          last_items.insert(layout_object, LastItem{{item, fragment, items},
+                                                    /*fragment_id*/ 0,
+                                                    total_item_index});
       const bool is_first = last_item_result.is_new_entry;
       if (is_first) {
         item.SetFragmentId(0);
-        layout_object->SetFirstInlineFragmentItemIndex(item_index);
+        layout_object->SetFirstInlineFragmentItemIndex(total_item_index);
         continue;
       }
 
       // Update the last item for |layout_object|.
       LastItem* last = &last_item_result.stored_value->value;
-      const FragmentItem* last_item = last->item;
+      const FragmentItem* last_item = last->item.Get();
       DCHECK_EQ(last_item->DeltaToNextForSameLayoutObject(), 0u);
-      const wtf_size_t last_index = last->item_index;
+      const wtf_size_t last_index = last->total_item_index;
       DCHECK_GT(last_index, 0u);
       DCHECK_LT(last_index, fragment_items->EndItemIndex());
-      DCHECK_LT(last_index, item_index);
-      last_item->SetDeltaToNextForSameLayoutObject(item_index - last_index);
+      DCHECK_LT(last_index, total_item_index);
+      last_item->SetDeltaToNextForSameLayoutObject(total_item_index -
+                                                   last_index);
       // Because we found a following fragment, reset |IsLastForNode| for the
       // last item except:
       // a. |IsLastForNode| is computed from break token. The last item already
@@ -187,8 +224,8 @@ void FragmentItems::FinalizeAfterLayout(
 
       // Update this item.
       item.SetFragmentId(++last->fragment_id);
-      last->item = &item;
-      last->item_index = item_index;
+      last->item = {item, fragment, items};
+      last->total_item_index = total_item_index;
     }
 
     if (!found_inflow_content) {
