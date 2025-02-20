9928270d02e1e327b57db875ee6c56691f1d507f
Patrick Monette <pmonette@chromium.org>
2025-02-20 15:31:13 -0800

    [PM] Migrate uses of CallOnGraph/RunInGraph
    The performance manager now lives on the UI thread, making
    these functions obsolete.
    
    The graph can be directly accessed through the
    PerformanceManager::GetGraph() function.
    
    This CL was uploaded by git cl split.
    
    R=joenotcharles@google.com
    
    Bug: 40755583
    Change-Id: I97400c4d0f796fe1eb265f66cda84b24619b770f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6284705
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422845}

--- components/performance_manager/performance_manager_browsertest.cc
+++ components/performance_manager/performance_manager_browsertest.cc
@@ -52,27 +52,8 @@ IN_PROC_BROWSER_TEST_F(PerformanceManagerBrowserTest,
   base::WeakPtr<FrameNode> frame_node =
       PerformanceManager::GetFrameNodeForRenderFrameHost(rfh);
 
-  // Post a task to the Graph and make it call a function on the UI thread that
-  // will ensure that |frame_node| is really associated with |rfh|.
-
-  base::RunLoop run_loop;
-  auto check_rfh_on_main_thread =
-      base::BindLambdaForTesting([&](const RenderFrameHostProxy& rfh_proxy) {
-        EXPECT_EQ(rfh, rfh_proxy.Get());
-        run_loop.Quit();
-      });
-
-  auto call_on_graph_cb = base::BindLambdaForTesting([&]() {
-    EXPECT_TRUE(frame_node.get());
-    content::GetUIThreadTaskRunner({})->PostTask(
-        FROM_HERE, base::BindOnce(std::move(check_rfh_on_main_thread),
-                                  frame_node->GetRenderFrameHostProxy()));
-  });
-
-  PerformanceManager::CallOnGraph(FROM_HERE, call_on_graph_cb);
-
-  // Wait for |check_rfh_on_main_thread| to be called.
-  run_loop.Run();
+  const RenderFrameHostProxy& rfh_proxy = frame_node->GetRenderFrameHostProxy();
+  EXPECT_EQ(rfh, rfh_proxy.Get());
 
   // This closes the window, and destroys the underlying WebContents.
   shell()->Close();
@@ -80,15 +61,7 @@ IN_PROC_BROWSER_TEST_F(PerformanceManagerBrowserTest,
 
   // After deleting |contents| the corresponding FrameNode WeakPtr should be
   // invalid.
-  base::RunLoop run_loop_after_contents_reset;
-  auto quit_closure = run_loop_after_contents_reset.QuitClosure();
-  auto call_on_graph_cb_2 = base::BindLambdaForTesting([&]() {
-    EXPECT_FALSE(frame_node.get());
-    std::move(quit_closure).Run();
-  });
-
-  PerformanceManager::CallOnGraph(FROM_HERE, call_on_graph_cb_2);
-  run_loop_after_contents_reset.Run();
+  EXPECT_FALSE(frame_node);
 }
 
 IN_PROC_BROWSER_TEST_F(PerformanceManagerBrowserTest, OpenerTrackingWorks) {
