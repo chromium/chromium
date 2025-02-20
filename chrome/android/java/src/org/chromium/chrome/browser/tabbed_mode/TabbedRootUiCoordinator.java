ed6a6c1c295f3f447d398d754737f87d7dbab0c1
Eleanor Lee <eleanorlee@google.com>
2025-02-20 14:15:21 -0800

    Reland "Reland "Add instrumentation tests for nav bar color animations""
    This is a reland of commit 5167d3ae18589512817b6e1b25689880e27a3563
    
    Original change's description:
    > Reland "Add instrumentation tests for nav bar color animations"
    >
    > This is a reland of commit 360cc531f6c67f37a6f4af09c407667f5d9a632d
    >
    > Original change's description:
    > > Add instrumentation tests for nav bar color animations
    > >
    > > Bug: 394443224
    > > Change-Id: I5b5cd3b27e30b641ee5b7eb278354b5e4052f512
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6239556
    > > Reviewed-by: Charles Hager <clhager@google.com>
    > > Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    > > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1421662}
    >
    > Bug: 394443224
    > Change-Id: Ie1f4f5e10f9b1346934cc7460f823acfe34bfd9b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6278808
    > Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    > Reviewed-by: Charles Hager <clhager@google.com>
    > Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1422120}
    
    Bug: 394443224
    Change-Id: I8d4d4be8614ab8cf38a5fce49e8d338b3f175a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6285365
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1422800}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -1455,6 +1455,10 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
         return mRtlGestureNavIphController;
     }
 
+    public TabbedSystemUiCoordinator getTabbedSystemUiCoordinatorForTesting() {
+        return mSystemUiCoordinator;
+    }
+
     /** Called when a link is copied through context menu. */
     public void onContextMenuCopyLink() {
         // TODO(crbug.com/40732234): Find a better way of passing event for IPH.
