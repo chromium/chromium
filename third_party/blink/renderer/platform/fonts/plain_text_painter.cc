3a041207d28ae4fde3cf9539755c2733375312fb
Kent Tamura <tkent@chromium.org>
2025-02-19 21:28:31 -0800

    CanvasTextNG: Add blink::PlainTextPainter
    This CL adds a skeleton of PlainTextPainter, and update some callsites.
    PlainTextPainter methods are going to be implemented later.
    
    This CL has no behavior changes because of a runtime flag.
    
    Bug: 389726691
    Change-Id: Iae7aabbf853c90cb69a698f33fca766019ba1264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6284045
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422355}

---
+++ third_party/blink/renderer/platform/fonts/plain_text_painter.cc
@@ -0,0 +1,91 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/fonts/plain_text_painter.h"
+
+#include "third_party/blink/renderer/platform/fonts/text_run_paint_info.h"
+
+namespace blink {
+
+PlainTextPainter::PlainTextPainter(PlainTextPainter::Mode mode) : mode_(mode) {}
+
+void PlainTextPainter::Trace(Visitor* visitor) const {}
+
+PlainTextPainter& PlainTextPainter::Shared() {
+  DCHECK(RuntimeEnabledFeatures::PlainTextPainterEnabled());
+  DCHECK(IsMainThread());
+  DEFINE_STATIC_LOCAL(Persistent<PlainTextPainter>, shared_instance,
+                      (MakeGarbageCollected<PlainTextPainter>(kShared)));
+  return *shared_instance;
+}
+
+void PlainTextPainter::Draw(const TextRun& run,
+                            const Font& font,
+                            cc::PaintCanvas& canvas,
+                            const gfx::PointF& location,
+                            const cc::PaintFlags& flags,
+                            Font::DrawType draw_type) {
+  // TODO(crbug.com/389726691): Implement this without Font::DrawText().
+  font.DrawText(&canvas, run, location, flags, draw_type);
+}
+
+bool PlainTextPainter::DrawWithBidiReorder(
+    const TextRun& run,
+    unsigned from_index,
+    unsigned to_index,
+    const Font& font,
+    Font::CustomFontNotReadyAction action,
+    cc::PaintCanvas& canvas,
+    const gfx::PointF& location,
+    const cc::PaintFlags& flags,
+    Font::DrawType draw_type) {
+  // TODO(crbug.com/389726691): Implement this without Font::DrawText().
+  TextRunPaintInfo run_info(run);
+  run_info.from = from_index;
+  run_info.to = to_index;
+  return font.DrawBidiText(&canvas, run_info, location, action, flags,
+                           draw_type);
+}
+
+float PlainTextPainter::ComputeInlineSize(const TextRun& run,
+                                          const Font& font,
+                                          gfx::RectF* glyph_bounds) {
+  // TODO(crbug.com/389726691): Implement this without Font::Width().
+  return font.Width(run, glyph_bounds);
+}
+
+float PlainTextPainter::ComputeSubInlineSize(const TextRun& run,
+                                             unsigned from_index,
+                                             unsigned to_index,
+                                             const Font& font,
+                                             gfx::RectF* glyph_bounds) {
+  // TODO(crbug.com/389726691): Implement this without Font::SubRunWidth().
+  return font.SubRunWidth(run, from_index, to_index, glyph_bounds);
+}
+
+int PlainTextPainter::OffsetForPosition(
+    const TextRun& run,
+    const Font& font,
+    float position,
+    IncludePartialGlyphsOption partial_option,
+    BreakGlyphsOption break_option) const {
+  // TODO(crbug.com/389726691): Implement this without
+  // Font::OffsetForPosition().
+  return font.OffsetForPosition(run, position, partial_option, break_option);
+}
+
+gfx::RectF PlainTextPainter::SelectionRectForText(
+    const TextRun& run,
+    unsigned from_index,
+    unsigned to_index,
+    const Font& font,
+    const gfx::PointF& left_baseline,
+    float height) const {
+  // TODO(crbug.com/389726691): Implement this without
+  // Font::SelectionRectForText().
+  return font.SelectionRectForText(run, left_baseline, height, from_index,
+                                   to_index);
+}
+
+}  // namespace blink
--- third_party/blink/renderer/platform/fonts/string_truncator.cc
+++ third_party/blink/renderer/platform/fonts/string_truncator.cc
@@ -29,6 +29,7 @@
 #include "third_party/blink/renderer/platform/fonts/string_truncator.h"
 
 #include "third_party/blink/renderer/platform/fonts/font.h"
+#include "third_party/blink/renderer/platform/fonts/plain_text_painter.h"
 #include "third_party/blink/renderer/platform/text/text_break_iterator.h"
 #include "third_party/blink/renderer/platform/text/text_run.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
@@ -101,6 +102,10 @@ static base::span<const UChar> RightTruncateToBuffer(const String& string,
 
 static float StringWidth(const Font& renderer,
                          base::span<const UChar> characters) {
+  if (RuntimeEnabledFeatures::PlainTextPainterEnabled()) {
+    return PlainTextPainter::Shared().ComputeInlineSize(TextRun(characters),
+                                                        renderer);
+  }
   return renderer.Width(TextRun(characters));
 }
 
