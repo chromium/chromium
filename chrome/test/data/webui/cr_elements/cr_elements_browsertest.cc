0b8d7129ea6b701955ae8f12422edf73dafc3587
John Lee <johntlee@chromium.org>
2025-02-20 09:52:13 -0800

    WebUI: Add cr-scrollable class, indicating that an element is scrollable
    This CL uses offers a native CSS-only solution for indicating that an
    element is scrollable in either vertical direction.
    
    Bug: 395936384
    Change-Id: I071b5b4a59d455f28fc57337eba3cd7c6144ed5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6243271
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1422645}

--- chrome/test/data/webui/cr_elements/cr_elements_browsertest.cc
+++ chrome/test/data/webui/cr_elements/cr_elements_browsertest.cc
@@ -57,6 +57,10 @@ IN_PROC_BROWSER_TEST_F(CrElementsTest, CrDialog) {
   RunTest("cr_elements/cr_dialog_test.js", "mocha.run()");
 }
 
+IN_PROC_BROWSER_TEST_F(CrElementsTest, CrScrollable) {
+  RunTest("cr_elements/cr_scrollable_test.js", "mocha.run()");
+}
+
 // https://crbug.com/1008122 - Flaky on Mac.
 #if BUILDFLAG(IS_MAC)
 #define MAYBE_CrDrawer DISABLED_CrDrawer
