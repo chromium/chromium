7b270f250de77c487bb8fc4b3c1881fb0a6027a8
Chidera Olibie <colibie@google.com>
2025-02-21 04:29:40 -0800

    [wv] test to detect deadlock on webview startup
    This cl adds a small test that initializes webview via a threadsafe
    API. This is basically to help detect if we introduce a deadlock
    in the Chromium WebView startup code.
    
    It's not comprehensive since we call just a single API, but it
    would help detect if there's a problem in the general setup.
    
    I used getDefaultUserAgent because it's the more popular API for
    starting webview in the background.
    
    Bug: 396390037
    Change-Id: I17eeeb9777b8f983632e6ef444f854aa61fe2177
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6278686
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1423047}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewThreadTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewThreadTest.java
@@ -6,8 +6,10 @@ package org.chromium.webview_shell.test;
 
 import static org.chromium.base.test.util.ScalableTimeout.scaleTimeout;
 
+import android.os.Process;
 import android.webkit.CookieManager;
 import android.webkit.GeolocationPermissions;
+import android.webkit.WebSettings;
 import android.webkit.WebStorage;
 
 import androidx.test.filters.SmallTest;
@@ -22,6 +24,9 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.webview_shell.WebViewThreadTestActivity;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /** Tests running WebView on different threads. */
 @RunWith(BaseJUnit4ClassRunner.class)
@@ -136,6 +141,51 @@ public class WebViewThreadTest {
         Assert.assertTrue(mActivity.createWebViewOnUiThread(TIMEOUT));
         WebStorage.getInstance();
         Assert.assertTrue(loadDataWebViewInUiThread(DATA));
+    }
+
+    /**
+     * Initialize webview via a threadsafe API concurrently on both background and UI thread. This
+     * test attempts to catch any deadlocks that may be introduced in the Chromium WebView startup
+     * code when initializing WebView via a threadsafe API. Note: If this test starts timing out,
+     * there may be a deadlock somewhere.
+     */
+    @Test
+    @SmallTest
+    public void testConcurrentChromiumWebViewStartup() throws InterruptedException {
+        // There's a couple of tricks here to try to make the background thread's
+        // getDefaultUserAgent call run first.
+
+        // Trick: Trigger part of webview initialization that doesn't start chromium.
+        CookieManager.getInstance();
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        Thread bg =
+                new Thread(
+                        () -> {
+                            Process.setThreadPriority(Process.THREAD_PRIORITY_DISPLAY); // Trick
+                            // Trick: Posting to the UI thread within this background thread to try
+                            // to ensure that the background task starts executing first.
+                            mActivity.runOnUiThread(
+                                    () -> {
+                                        try {
+                                            // Wait for a portion of the test duration to allow the
+                                            // bg thread to execute getDefaultUserAgent first.
+                                            // Dividing by 4 so as not to timeout due to waiting.
+                                            Thread.sleep(TIMEOUT / 4);
+                                        } catch (InterruptedException e) {
+                                            throw new RuntimeException(
+                                                    "Thread.sleep() was interrupted, so we can't"
+                                                            + " trust the result of this test.",
+                                                    e);
+                                        }
+                                        WebSettings.getDefaultUserAgent(mActivity);
+                                        latch.countDown();
+                                    });
+                            WebSettings.getDefaultUserAgent(mActivity);
+                        });
+        bg.start();
+        Assert.assertTrue(latch.await(TIMEOUT, TimeUnit.MILLISECONDS));
+        bg.join();
     }
 
     /** LoadData for webview created in non-ui thread */
