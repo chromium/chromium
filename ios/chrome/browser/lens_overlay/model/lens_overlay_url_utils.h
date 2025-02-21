acbd81d861fb0c1c874e47c2fc7547366a767755
Christian Xu <christianxu@google.com>
2025-02-21 06:28:53 -0800

    [iOS][LO] Allow navigation redirection from Google domains
    Searches in some countries relies on redirection. ex: from
    www.google.com.hk to www.google.com.
    
    This CL allows these navigation in the bottom sheet.
    
    Fixed: 397536947
    Change-Id: Ib8b5226c2fdce863340c6adc780bdda21b5a0f22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276256
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1423090}

--- ios/chrome/browser/lens_overlay/model/lens_overlay_url_utils.h
+++ ios/chrome/browser/lens_overlay/model/lens_overlay_url_utils.h
@@ -5,15 +5,23 @@
 #ifndef IOS_CHROME_BROWSER_LENS_OVERLAY_MODEL_LENS_OVERLAY_URL_UTILS_H_
 #define IOS_CHROME_BROWSER_LENS_OVERLAY_MODEL_LENS_OVERLAY_URL_UTILS_H_
 
+#import "ios/web/public/navigation/web_state_policy_decider.h"
 #import "url/gurl.h"
 
 namespace lens {
+
+/// Whether the `url` is from a google domain.
+bool IsGoogleHostURL(GURL url);
 
 /// Whether the `url` is a lens overlay SRP.
 bool IsLensOverlaySRP(GURL url);
 
 /// Returns the search tearm of the lens overlay SRP.
 std::string ExtractQueryFromLensOverlaySRP(GURL url);
+
+/// Whether the `url` is a redirection initiated from a google URL.
+bool IsGoogleRedirection(GURL url,
+                         web::WebStatePolicyDecider::RequestInfo request_info);
 
 }  // namespace lens
 
