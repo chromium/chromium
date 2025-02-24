e22212bc5d38ec8e0fb8edaebdff740c3eb1f02d
Emily Andrews <emiled@microsoft.com>
2025-02-24 10:38:19 -0800

    Move ServiceProcessTracker and UtilityProcessClient into separate files
    It's easier to test classes when they are separated into their own
    files.
    
    When Utility Process Reuse logic is implemented, ServiceProcessTracker
    will need to be unit tested.
    
    No functional changes
    
    Bug: 41493609
    Change-Id: Iaccf430fe5fd75e8914ee66a1107c99feb376b24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6088677
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Emily Andrews <emiled@microsoft.com>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424007}

--- content/browser/service_host/service_process_host_impl.cc
+++ content/browser/service_host/service_process_host_impl.cc
@@ -7,15 +7,10 @@
 #include <utility>
 
 #include "base/functional/bind.h"
-#include "base/memory/weak_ptr.h"
-#include "base/no_destructor.h"
-#include "base/not_fatal_until.h"
-#include "base/observer_list.h"
-#include "base/process/process.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/synchronization/lock.h"
+#include "content/browser/service_host/service_process_tracker.h"
+#include "content/browser/service_host/utility_process_client.h"
 #include "content/browser/service_host/utility_process_host.h"
-#include "content/common/child_process.mojom.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
@@ -40,147 +35,6 @@ bool ShouldEnableSandbox(sandbox::mojom::Sandbox sandbox) {
   }
   return true;
 }
-
-// Internal helper to track running service processes.
-class ServiceProcessTracker {
- public:
-  ServiceProcessTracker() = default;
-
-  ServiceProcessTracker(const ServiceProcessTracker&) = delete;
-  ServiceProcessTracker& operator=(const ServiceProcessTracker&) = delete;
-
-  ~ServiceProcessTracker() = default;
-
-  ServiceProcessInfo AddProcess(base::Process process,
-                                const std::optional<GURL>& site,
-                                const std::string& service_interface_name) {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    auto id = GenerateNextId();
-    ServiceProcessInfo info(service_interface_name, site, id,
-                            std::move(process));
-    auto info_dup = info.Duplicate();
-    processes_.insert({id, std::move(info)});
-    for (auto& observer : observers_) {
-      observer.OnServiceProcessLaunched(info_dup);
-    }
-    return info_dup;
-  }
-
-  void NotifyTerminated(ServiceProcessId id) {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    auto iter = processes_.find(id);
-    CHECK(iter != processes_.end(), base::NotFatalUntil::M130);
-
-    for (auto& observer : observers_) {
-      observer.OnServiceProcessTerminatedNormally(iter->second.Duplicate());
-    }
-    processes_.erase(iter);
-  }
-
-  void NotifyCrashed(ServiceProcessId id) {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    auto iter = processes_.find(id);
-    CHECK(iter != processes_.end(), base::NotFatalUntil::M130);
-    for (auto& observer : observers_) {
-      observer.OnServiceProcessCrashed(iter->second.Duplicate());
-    }
-    processes_.erase(iter);
-  }
-
-  void AddObserver(ServiceProcessHost::Observer* observer) {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    observers_.AddObserver(observer);
-  }
-
-  void RemoveObserver(ServiceProcessHost::Observer* observer) {
-    // NOTE: Some tests may remove observers after BrowserThreads are shut down.
-    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI) ||
-           !BrowserThread::IsThreadInitialized(BrowserThread::UI));
-    observers_.RemoveObserver(observer);
-  }
-
-  std::vector<ServiceProcessInfo> GetProcesses() {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    std::vector<ServiceProcessInfo> processes;
-    for (const auto& entry : processes_) {
-      processes.push_back(entry.second.Duplicate());
-    }
-    return processes;
-  }
-
- private:
-  ServiceProcessId GenerateNextId() {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    return service_process_id_generator_.GenerateNextId();
-  }
-
-  ServiceProcessId::Generator service_process_id_generator_;
-
-  std::map<ServiceProcessId, ServiceProcessInfo> processes_;
-
-  // Observers are owned and used exclusively on the UI thread.
-  base::ObserverList<ServiceProcessHost::Observer> observers_;
-};
-
-ServiceProcessTracker& GetServiceProcessTracker() {
-  static base::NoDestructor<ServiceProcessTracker> tracker;
-  return *tracker;
-}
-
-// Helper to bridge UtilityProcessHost IO thread events to the
-// ServiceProcessTracker. Every UtilityProcessHost created for a service process
-// has a unique instance of this class associated with it.
-class UtilityProcessClient : public UtilityProcessHost::Client {
- public:
-  UtilityProcessClient(
-      const std::string& service_interface_name,
-      const std::optional<GURL>& site,
-      base::OnceCallback<void(const base::Process&)> process_callback)
-      : service_interface_name_(service_interface_name),
-        site_(std::move(site)),
-        process_callback_(std::move(process_callback)) {}
-
-  UtilityProcessClient(const UtilityProcessClient&) = delete;
-  UtilityProcessClient& operator=(const UtilityProcessClient&) = delete;
-
-  ~UtilityProcessClient() override = default;
-
-  // UtilityProcessHost::Client:
-  void OnProcessLaunched(const base::Process& process) override {
-    DCHECK_CURRENTLY_ON(BrowserThread::UI);
-    process_info_.emplace(GetServiceProcessTracker().AddProcess(
-        process.Duplicate(), site_, service_interface_name_));
-    if (process_callback_) {
-      std::move(process_callback_).Run(process);
-    }
-  }
-
-  void OnProcessTerminatedNormally() override {
-    GetServiceProcessTracker().NotifyTerminated(
-        process_info_->service_process_id());
-  }
-
-  void OnProcessCrashed() override {
-    // TODO(crbug.com/40654042): It is unclear how we can observe
-    // |OnProcessCrashed()| without observing |OnProcessLaunched()| first, but
-    // it can happen on Android. Ignore the notification in this case.
-    if (!process_info_) {
-      return;
-    }
-
-    GetServiceProcessTracker().NotifyCrashed(
-        process_info_->service_process_id());
-  }
-
- private:
-  const std::string service_interface_name_;
-
-  // Optional site GURL for per-site utility processes.
-  const std::optional<GURL> site_;
-
-  base::OnceCallback<void(const base::Process&)> process_callback_;
-  std::optional<ServiceProcessInfo> process_info_;
-};
 
 // TODO(crbug.com/40633267): Once UtilityProcessHost is used only by service
 // processes, its logic can be inlined here.
---
+++ content/browser/service_host/service_process_tracker.h
@@ -0,0 +1,60 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_SERVICE_HOST_SERVICE_PROCESS_TRACKER_H_
+#define CONTENT_BROWSER_SERVICE_HOST_SERVICE_PROCESS_TRACKER_H_
+
+#include <map>
+#include <optional>
+#include <string>
+
+#include "base/observer_list.h"
+#include "base/process/process.h"
+#include "content/public/browser/service_process_host.h"
+#include "content/public/browser/service_process_info.h"
+#include "url/gurl.h"
+
+namespace content {
+
+// This class keeps track of all the UtilityProcessHosts and coordinates
+// the notification system to the ServiceHost observers.
+class ServiceProcessTracker {
+ public:
+  ServiceProcessTracker();
+
+  ServiceProcessTracker(const ServiceProcessTracker&) = delete;
+  ServiceProcessTracker& operator=(const ServiceProcessTracker&) = delete;
+
+  ~ServiceProcessTracker();
+
+  ServiceProcessInfo AddProcess(base::Process process,
+                                const std::optional<GURL>& site,
+                                const std::string& service_interface_name);
+
+  void NotifyTerminated(ServiceProcessId id);
+
+  void NotifyCrashed(ServiceProcessId id);
+
+  void AddObserver(ServiceProcessHost::Observer* observer);
+
+  void RemoveObserver(ServiceProcessHost::Observer* observer);
+
+  std::vector<ServiceProcessInfo> GetProcesses();
+
+ private:
+  ServiceProcessId GenerateNextId();
+
+  ServiceProcessId::Generator service_process_id_generator_;
+
+  std::map<ServiceProcessId, ServiceProcessInfo> processes_;
+
+  // Observers are owned and used exclusively on the UI thread.
+  base::ObserverList<ServiceProcessHost::Observer> observers_;
+};
+
+ServiceProcessTracker& GetServiceProcessTracker();
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_SERVICE_HOST_SERVICE_PROCESS_TRACKER_H_
