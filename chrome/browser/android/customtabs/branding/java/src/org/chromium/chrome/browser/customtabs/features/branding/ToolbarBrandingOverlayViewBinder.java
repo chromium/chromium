a0f020a932ec2fcd232a7256937160618d558c20
Sinan Sahin <sinansahin@google.com>
2025-02-24 11:33:10 -0800

    [NestedIcon] Fix and polish branding animation
    This CL fixes the text clipping and adjusts the Chrome icon placement to
    better align with the security icon. This adjustment also required a
    change to how the rotation animation is done.
    
    Bug: 331672974
    Change-Id: I941e378a237eadcbd88ed202f5b3c966f1a943d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6287608
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1424047}

--- chrome/browser/android/customtabs/branding/java/src/org/chromium/chrome/browser/customtabs/features/branding/ToolbarBrandingOverlayViewBinder.java
+++ chrome/browser/android/customtabs/branding/java/src/org/chromium/chrome/browser/customtabs/features/branding/ToolbarBrandingOverlayViewBinder.java
@@ -20,7 +20,7 @@ import org.chromium.ui.modelutil.PropertyModel;
 
 /** View binder for the toolbar branding overlay. */
 public class ToolbarBrandingOverlayViewBinder {
-    private static final int ICON_ROTATE_DEGREES = 180;
+    private static final int MAX_DRAWABLE_LEVEL = 10_000;
 
     public static void bind(PropertyModel model, View view, PropertyKey key) {
         if (key == COLOR_DATA) {
@@ -39,21 +39,7 @@ public class ToolbarBrandingOverlayViewBinder {
             float progress = model.get(HIDING_PROGRESS);
             view.setAlpha(1.f - progress);
             ImageView icon = view.findViewById(R.id.branding_icon);
-            // TODO(sinansahin): Replace with View#isPivotSet() once min API is 28.
-            if (!isPivotSet(icon)) {
-                centerIconPivot(icon);
-            }
-            icon.setRotation(progress * ICON_ROTATE_DEGREES);
+            icon.getDrawable().setLevel(Math.round(progress * MAX_DRAWABLE_LEVEL));
         }
-    }
-
-    private static boolean isPivotSet(View view) {
-        return view.getTag(R.id.branding_icon_pivot_set_key) != null;
-    }
-
-    private static void centerIconPivot(ImageView icon) {
-        icon.setPivotX(icon.getPaddingStart() + (float) icon.getDrawable().getIntrinsicWidth() / 2);
-        icon.setPivotY((float) icon.getDrawable().getIntrinsicHeight() / 2);
-        icon.setTag(R.id.branding_icon_pivot_set_key);
     }
 }
