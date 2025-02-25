53d584dd1fd3e4ac379ee74992cf8570df837b6b
Zhenyao Mo <zmo@chromium.org>
2025-02-25 15:11:40 -0800

    Use union for different types of LayerImpl's params in mojo.
    This is necessary to extend to other LayerImpl types.
    
    TEST=TreesInViz bot
    
    Bug: 389301965
    Change-Id: I6cb8637c7738c57ae79c3afaa2f18433f16ed42a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6302409
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424823}

--- cc/mojo_embedder/viz_layer_context.cc
+++ cc/mojo_embedder/viz_layer_context.cc
@@ -465,11 +465,14 @@ void SerializeLayer(LayerImpl& layer,
   wire.effect_tree_index = layer.effect_tree_index();
   wire.scroll_tree_index = layer.scroll_tree_index();
   switch (layer.GetLayerType()) {
-    case mojom::LayerType::kSurface:
-      wire.surface_layer_extra = viz::mojom::SurfaceLayerExtra::New();
+    case mojom::LayerType::kSurface: {
+      auto surface_layer_extra = viz::mojom::SurfaceLayerExtra::New();
       SerializeSurfaceLayerExtra(static_cast<SurfaceLayerImpl&>(layer),
-                                 wire.surface_layer_extra);
+                                 surface_layer_extra);
+      wire.layer_extra = viz::mojom::LayerExtra::NewSurfaceLayerExtra(
+          std::move(surface_layer_extra));
       break;
+    }
     case mojom::LayerType::kPicture: {
       PictureLayerImpl& picture_layer = static_cast<PictureLayerImpl&>(layer);
       if (picture_layer.GetRasterSource()->IsSolidColor()) {
