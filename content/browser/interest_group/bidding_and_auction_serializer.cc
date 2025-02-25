928898ae1fe8f96ad0c3dedfb59c9377c8851e5b
Russ Hamilton <behamilton@google.com>
2025-02-25 11:48:52 -0800

    Fix check-failure in Bidding and Auction Serializer
    This failure occurs when a buyer's interest group information
    serializes to the exact same size in bytes as the ceiling of
    the average space remaining for each remaining buyer.
    
    Fixed: 394896168
    Change-Id: I67065d443c3ec9e72ac12661619c0f5adddae46d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6299356
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Russ Hamilton <behamilton@google.com>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424700}

--- content/browser/interest_group/bidding_and_auction_serializer.cc
+++ content/browser/interest_group/bidding_and_auction_serializer.cc
@@ -864,8 +864,12 @@ void BiddingAndAuctionSerializer::TargetSizeEstimator::UpdateUnsizedGroupSizes(
           remaining_unallocated_unsized_buyers_ - 1) /
          remaining_unallocated_unsized_buyers_)
             .ValueOrDie<size_t>();
-    if (equal_size_allocation == previous_size_allocation) {
+    if (equal_size_allocation <= previous_size_allocation) {
       // No changes mean no more buyers can be removed, so we're done.
+      // We have to use <= because we took the ceiling when calculating the
+      // equal size allocation above, so an assignment of this size may
+      // reduce the allocation by up to
+      // `remaining_unallocated_unsized_buyers_ - 1`.
       break;
     }
     unsized_buyer_size_ = equal_size_allocation;
