d09b77604e296bfc71eac1526ec692e02bce2eae
Celeste Pan <celestepan@microsoft.com>
2025-02-25 14:42:56 -0800

    [Masonry] Collect masonry items and resolve their span sizes.
    Before we begin implementing the Masonry layout algorithm, we need to
    first collect all of the masonry items and resolve their span sizes.
    
    The tests ensure that we are correctly retrieving all masonry items and
    correctly resolving their span sizes, although we cannot know the exact
    start and end lines of the items since we do not yet calculate the
    translation of the spans.
    
    Bug: 343257585
    Change-Id: I73294a0318ae4021b344275076c9626cb9186eef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6245767
    Reviewed-by: Ethan Jimenez <ethavar@microsoft.com>
    Commit-Queue: Celeste Pan <celestepan@microsoft.com>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1424810}

--- third_party/blink/renderer/core/layout/masonry/masonry_node.cc
+++ third_party/blink/renderer/core/layout/masonry/masonry_node.cc
@@ -4,6 +4,7 @@
 
 #include "third_party/blink/renderer/core/layout/masonry/masonry_node.h"
 
+#include "third_party/blink/renderer/core/layout/grid/grid_item.h"
 #include "third_party/blink/renderer/core/layout/grid/grid_line_resolver.h"
 
 namespace blink {
@@ -42,6 +43,46 @@ MasonryItemGroups MasonryNode::CollectItemGroups(
     }
   }
   return item_groups;
+}
+
+GridItems MasonryNode::ConstructMasonryItems(
+    const GridLineResolver& line_resolver,
+    wtf_size_t start_offset) const {
+  GridItems masonry_items;
+
+  {
+    bool should_sort_masonry_items_by_order_property = false;
+    const int initial_order = ComputedStyleInitialValues::InitialOrder();
+    const auto grid_axis_direction = Style().MasonryTrackSizingDirection();
+
+    // This collects all our children, and orders them by their order property.
+    for (auto child = FirstChild(); child; child = child.NextSibling()) {
+      auto masonry_item = std::make_unique<GridItemData>(
+          To<BlockNode>(child), /*parent_style=*/Style());
+
+      // We'll need to sort when we encounter a non-initial order property.
+      should_sort_masonry_items_by_order_property |=
+          child.Style().Order() != initial_order;
+
+      // Resolve the positions of the items based on style. We can only resolve
+      // the number of spans for each item based on the grid-axis.
+      GridSpan item_span = line_resolver.ResolveGridPositionsFromStyle(
+          masonry_item->node.Style(), grid_axis_direction);
+      if (item_span.IsUntranslatedDefinite()) {
+        item_span.Translate(start_offset);
+      }
+      masonry_item->resolved_position.SetSpan(item_span, grid_axis_direction);
+
+      masonry_items.Append(std::move(masonry_item));
+    }
+
+    // Sort items by order property if needed.
+    if (should_sort_masonry_items_by_order_property) {
+      masonry_items.SortByOrderProperty();
+    }
+  }
+
+  return masonry_items;
 }
 
 }  // namespace blink
