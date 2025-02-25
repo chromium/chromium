f33b7036458ec80b065ba83c57182a18ff153645
Kalvin Lee <kdlee@chromium.org>
2025-02-25 08:05:31 -0800

    arrayify: Prefer `constexpr`
    This CL is intended to be trivial. It marks the return from `std::to_array` as
    `constexpr`. This CL is a split of a naive textual search-and-replace under:
    
    media/audio
    
    This CL was uploaded by git cl split.
    
    Change-Id: I2312c7ca6c3fa52dcde995f71022a400a6a3a082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6301508
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424555}

--- media/audio/alsa/audio_manager_alsa.cc
+++ media/audio/alsa/audio_manager_alsa.cc
@@ -43,7 +43,7 @@ static const int kDefaultSampleRate = 48000;
 // real devices, we remove them from the list to avoiding duplicate counting.
 // In addition, note that we support no more than 2 channels for recording,
 // hence surround devices are not stored in the list.
-const auto kInvalidAudioInputDevices = std::to_array<const char*>({
+constexpr auto kInvalidAudioInputDevices = std::to_array<const char*>({
     "default",
     "dmix",
     "null",
