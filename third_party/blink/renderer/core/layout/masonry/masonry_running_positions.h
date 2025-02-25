e5b36a4263742f153ddcd44a9812b4d80aad9990
Celeste Pan <celestepan@microsoft.com>
2025-02-24 16:54:57 -0800

    [Masonry] Skeleton for Keeping Track of Running Positions
    Add skeleton code to keep track of and update the running positions of
    each track on the grid-axis. The comments also include basic steps of
    finding the next span to place an item. Future CL's will actually
    implement the methods.
    
    The layout algorithm reference is here:
    https://drafts.csswg.org/css-grid-3/#masonry-layout-algorithm
    
    Bug: 343257585
    Change-Id: I63b5e9dbd1f4b02411218360389330d6157da754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6277649
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Celeste Pan <celestepan@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1424233}

---
+++ third_party/blink/renderer/core/layout/masonry/masonry_running_positions.h
@@ -0,0 +1,50 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_MASONRY_MASONRY_RUNNING_POSITIONS_H_
+#define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_MASONRY_MASONRY_RUNNING_POSITIONS_H_
+
+#include "third_party/blink/renderer/platform/geometry/layout_unit.h"
+#include "third_party/blink/renderer/platform/wtf/vector.h"
+#include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
+
+namespace blink {
+
+// This class holds a list of running positions for each track. This will be
+// used to calculate the next position that an item should be placed.
+class MasonryRunningPositions {
+ public:
+  // Return the first span with the minimum max-position that comes after
+  // the auto-placement cursor in masonry's flow.
+  GridSpan DetermineMinMaxPositionSpan(wtf_size_t span_size) const;
+
+  // Update all the running positions for the tracks within the given lines to
+  // have the inputted `running_position`.
+  void UpdateRunningPositionsForSpan(GridSpan span,
+                                     LayoutUnit running_position);
+
+ private:
+  // Struct to keep track of a span of tracks' start lines and their
+  // max-positions, where the max-position of a span represents the maximum
+  // running position of all tracks in a span. This will always be used in
+  // conjunction with a span size, so we can calculate the ending line using
+  // `start_line` and a given span size.
+  struct MaxPositionSpan {
+    wtf_size_t start_line;
+    LayoutUnit max_pos;
+  };
+
+  // Given span size, returns a list of spans that have a max-position within
+  // the tie-threshold of the minimum max-position.
+  Vector<MaxPositionSpan> GetAllMaxPositionSpans(wtf_size_t span_size) const;
+
+  // The index of the `running_positions_` vector corresponds to the track
+  // number, while the value of the vector item corresponds to the current
+  // running position of the track. Note that the tracks are 0-indexed.
+  Vector<LayoutUnit> running_positions_;
+};
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_MASONRY_MASONRY_RUNNING_POSITIONS_H_
