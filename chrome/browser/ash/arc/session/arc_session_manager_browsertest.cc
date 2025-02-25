f7dbe56a83ad4e86f7b295cf730e8714e6156c15
Hidehiko Abe <hidehiko@chromium.org>
2025-02-24 18:54:25 -0800

    Remove UserManager stacking from browser_tests part 7/n
    This fixes the unexpected usage of UserManager in
    ArcSessionManagerTest.
    
    BUG=396238168
    TEST=Tryjob
    
    Change-Id: I3e829ae7d05e6ce47af02c7e36a342f09d77de06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6290880
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424283}

--- chrome/browser/ash/arc/session/arc_session_manager_browsertest.cc
+++ chrome/browser/ash/arc/session/arc_session_manager_browsertest.cc
@@ -21,9 +21,8 @@
 #include "chrome/browser/ash/arc/session/arc_session_manager_observer.h"
 #include "chrome/browser/ash/arc/test/arc_data_removed_waiter.h"
 #include "chrome/browser/ash/arc/test/test_arc_session_manager.h"
-#include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/ash/policy/test_support/embedded_policy_test_server_mixin.h"
-#include "chrome/browser/ash/profiles/profile_helper.h"
+#include "chrome/browser/ash/test/regular_logged_in_browser_test_mixin.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/certificate_provider/certificate_provider_service.h"
 #include "chrome/browser/certificate_provider/certificate_provider_service_factory.h"
@@ -35,6 +34,7 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/testing_profile.h"
+#include "chromeos/ash/components/browser_context_helper/browser_context_types.h"
 #include "chromeos/ash/experiences/arc/arc_prefs.h"
 #include "chromeos/ash/experiences/arc/session/arc_service_manager.h"
 #include "chromeos/ash/experiences/arc/session/arc_session_runner.h"
@@ -50,7 +50,6 @@
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
-#include "components/user_manager/scoped_user_manager.h"
 #include "components/user_manager/user_manager.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/browser_test.h"
@@ -109,7 +108,8 @@ class ArcPlayStoreDisabledWaiter : public ArcSessionManagerObserver {
 
 class ArcSessionManagerTest : public MixinBasedInProcessBrowserTest {
  protected:
-  ArcSessionManagerTest() = default;
+  explicit ArcSessionManagerTest(std::string_view user_email = kFakeUserName)
+      : account_id_(AccountId::FromUserEmailGaiaId(user_email, kFakeGaiaId)) {}
 
   ArcSessionManagerTest(const ArcSessionManagerTest&) = delete;
   ArcSessionManagerTest& operator=(const ArcSessionManagerTest&) = delete;
@@ -122,83 +122,47 @@ class ArcSessionManagerTest : public MixinBasedInProcessBrowserTest {
     arc::SetArcAvailableCommandLineForTesting(command_line);
   }
 
-  void SetUpOnMainThread() override {
-    MixinBasedInProcessBrowserTest::SetUpOnMainThread();
-    fake_user_manager_.Reset(std::make_unique<ash::FakeChromeUserManager>());
+  void SetUpInProcessBrowserTestFixture() override {
+    MixinBasedInProcessBrowserTest::SetUpInProcessBrowserTestFixture();
+
     // Init ArcSessionManager for testing.
-    ArcServiceLauncher::Get()->ResetForTesting();
     ArcSessionManager::SetUiEnabledForTesting(false);
     ArcSessionManager::EnableCheckAndroidManagementForTesting(true);
-    ArcSessionManager::Get()->SetArcSessionRunnerForTesting(
+    ArcServiceLauncher::SetArcSessionRunnerForTesting(
         std::make_unique<ArcSessionRunner>(
             base::BindRepeating(FakeArcSession::Create)));
+  }
 
-    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
-    ExpandPropertyFilesForTesting(ArcSessionManager::Get());
+  void SetUpBrowserContextKeyedServices(
+      content::BrowserContext* context) override {
+    MixinBasedInProcessBrowserTest::SetUpBrowserContextKeyedServices(context);
 
-    ash::ProfileHelper::SetAlwaysReturnPrimaryUserForTesting(true);
+    // Inject only for a user Profile.
+    if (ash::IsSigninBrowserContext(context)) {
+      return;
+    }
+    IdentityTestEnvironmentProfileAdaptor::
+        SetIdentityTestEnvironmentFactoriesOnBrowserContext(context);
+    chromeos::CertificateProviderServiceFactory::GetInstance()
+        ->SetTestingFactory(
+            context, base::BindRepeating(&CreateCertificateProviderService));
+  }
 
-    const AccountId account_id(
-        AccountId::FromUserEmailGaiaId(kFakeUserName, kFakeGaiaId));
-    fake_user_manager_->AddUser(account_id);
-    fake_user_manager_->LoginUser(account_id);
-
-    // Create test profile.
-    TestingProfile::Builder profile_builder;
-    profile_builder.SetPath(temp_dir_.GetPath().AppendASCII("TestArcProfile"));
-    profile_builder.SetProfileName(kFakeUserName);
-    profile_ = IdentityTestEnvironmentProfileAdaptor::
-        CreateProfileForIdentityTestEnvironment(profile_builder);
+  void SetUpOnMainThread() override {
+    MixinBasedInProcessBrowserTest::SetUpOnMainThread();
 
     identity_test_environment_adaptor_ =
-        std::make_unique<IdentityTestEnvironmentProfileAdaptor>(profile_.get());
-    ash::ProfileHelper::Get()->SetUserToProfileMappingForTesting(
-        fake_user_manager_->GetPrimaryUser(), profile_.get());
-
+        std::make_unique<IdentityTestEnvironmentProfileAdaptor>(profile());
     // Seed account info properly.
     identity_test_env()->MakePrimaryAccountAvailable(
-        kFakeUserName, signin::ConsentLevel::kSignin);
+        account_id_.GetUserEmail(), signin::ConsentLevel::kSignin);
 
     profile()->GetPrefs()->SetBoolean(prefs::kArcSignedIn, true);
     profile()->GetPrefs()->SetBoolean(prefs::kArcTermsAccepted, true);
-
-    // TestingProfile is not interpreted as a primary profile. Inject factory so
-    // that the instance of CertificateProviderService for the profile can be
-    // created.
-    chromeos::CertificateProviderServiceFactory::GetInstance()
-        ->SetTestingFactory(
-            profile(), base::BindRepeating(&CreateCertificateProviderService));
-
-    // Set up ARC for test profile.
-    // Currently, ArcSessionManager is singleton and set up with the original
-    // Profile instance. This re-initializes the ArcServiceLauncher by
-    // overwriting Profile with profile().
-    // TODO(hidehiko): This way several ArcService instances created with
-    // the original Profile instance on Browser creatuion are kept in the
-    // ArcServiceManager. For proper overwriting, those should be removed.
-    ArcServiceLauncher::Get()->OnPrimaryUserProfilePrepared(profile());
   }
 
   void TearDownOnMainThread() override {
-    // Explicitly removing the user is required; otherwise ProfileHelper keeps
-    // a dangling pointer to the User.
-    // TODO(nya): Consider removing all users from ProfileHelper in the
-    // destructor of ash::FakeChromeUserManager.
-    const AccountId account_id(
-        AccountId::FromUserEmailGaiaId(kFakeUserName, kFakeGaiaId));
-    fake_user_manager_->RemoveUserFromList(account_id);
-    // Since ArcServiceLauncher is (re-)set up with profile() in
-    // SetUpOnMainThread() it is necessary to Shutdown() before the profile()
-    // is destroyed. ArcServiceLauncher::Shutdown() will be called again on
-    // fixture destruction (because it is initialized with the original Profile
-    // instance in fixture, once), but it should be no op.
-    // TODO(hidehiko): Think about a way to test the code cleanly.
-    ArcServiceLauncher::Get()->Shutdown();
     identity_test_environment_adaptor_.reset();
-    profile_.reset();
-    base::RunLoop().RunUntilIdle();
-    ash::ProfileHelper::SetAlwaysReturnPrimaryUserForTesting(false);
-    fake_user_manager_.Reset();
     MixinBasedInProcessBrowserTest::TearDownOnMainThread();
   }
 
@@ -208,11 +172,7 @@ class ArcSessionManagerTest : public MixinBasedInProcessBrowserTest {
     base::RunLoop().RunUntilIdle();
   }
 
-  void set_profile_name(const std::string& username) {
-    profile_->set_profile_name(username);
-  }
-
-  Profile* profile() { return profile_.get(); }
+  Profile* profile() { return browser()->profile(); }
 
   signin::IdentityTestEnvironment* identity_test_env() {
     return identity_test_environment_adaptor_->identity_test_env();
@@ -223,13 +183,12 @@ class ArcSessionManagerTest : public MixinBasedInProcessBrowserTest {
   }
 
  private:
+  const AccountId account_id_;
+  ash::RegularLoggedInBrowserTestMixin logged_in_mixin_{&mixin_host_,
+                                                        account_id_};
   ash::EmbeddedPolicyTestServerMixin policy_test_server_mixin_{&mixin_host_};
-  user_manager::TypedScopedUserManager<ash::FakeChromeUserManager>
-      fake_user_manager_;
   std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
       identity_test_environment_adaptor_;
-  base::ScopedTempDir temp_dir_;
-  std::unique_ptr<TestingProfile> profile_;
 };
 
 IN_PROC_BROWSER_TEST_F(ArcSessionManagerTest, ConsumerAccount) {
@@ -241,8 +200,15 @@ IN_PROC_BROWSER_TEST_F(ArcSessionManagerTest, ConsumerAccount) {
             ArcSessionManager::Get()->state());
 }
 
-IN_PROC_BROWSER_TEST_F(ArcSessionManagerTest, WellKnownConsumerAccount) {
-  set_profile_name(kWellKnownConsumerName);
+class ArcSessionManagerWellKnownConsumerNameTest
+    : public ArcSessionManagerTest {
+ public:
+  ArcSessionManagerWellKnownConsumerNameTest()
+      : ArcSessionManagerTest(kWellKnownConsumerName) {}
+};
+
+IN_PROC_BROWSER_TEST_F(ArcSessionManagerWellKnownConsumerNameTest,
+                       WellKnownConsumerAccount) {
   EnableArc();
   ASSERT_EQ(ArcSessionManager::State::ACTIVE,
             ArcSessionManager::Get()->state());
