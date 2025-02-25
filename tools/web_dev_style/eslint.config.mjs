5c828832a6e539fb734b0205f731546f7a4c14d6
dpapad <dpapad@chromium.org>
2025-02-25 11:47:12 -0800

    [Reland #2] WebUI: Turn on ESLint JS presubmit checks for ios/ folder.
    Reland notes #2: The reason the previous reland was reverted was dealt
    in a precursor CL (see crbug.com/398274433) and is no longer part of
    this reland.
    
    Bug: 397907153
    Change-Id: I2ee1871ad10f3d607634085a31eb5c37e61e8dd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6293728
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424698}

--- tools/web_dev_style/eslint.config.mjs
+++ tools/web_dev_style/eslint.config.mjs
@@ -19,6 +19,12 @@ export default [
       // No point linting auto-generated files.
       'tools/typescript/definitions/**/*',
 
+      // Ignore generated checked-in JS file.
+      'ios/tools/documents_statistics_viewer/tsc/viewer.js',
+
+      // TODO(crbug.com/397907153): Fix violations and remove from ignore list.
+      'ios/**/*.ts',
+
       // ESLint is disabled for camera_app_ui and recorder_app_ui as they used
       // a custom eslint plugin that does not work with the latest eslint, and
       // they had complex eslint rc files that have not been updated to the
