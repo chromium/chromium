eade54b8a27b6c5cfb0fe9debd20fd431f57fdf5
Victor Hugo Vianna Silva <victorvianna@google.com>
2025-02-26 08:52:25 -0800

    Create redirect grpc header
    gRPC code will be moved from third_party/grpc/src to
    third_party/grpc/source in crrev.com/c/6269394, but there is 1
    downstream dependency on
    third_party/grpc/src/include/grpcpp/grpcpp.h.
    
    This CL introduces a corresponding header in third_party/grpc/source,
    so that downstream can switch to it in crrev.com/i/8037328. The new
    header only delegates to the old one.
    
    Bug: 40285204
    Change-Id: I784032f0aa207ce1a282d06b65f050c36d9e9305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6276492
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Vigen Issahhanjan <vigeni@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425188}

---
+++ third_party/grpc/source/include/grpcpp/grpcpp.h
@@ -0,0 +1,7 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(crbug.com/40285204): Fork all of grpc in third_party/grpc/source and
+// delete this.
+#include "third_party/grpc/src/include/grpcpp/grpcpp.h"
