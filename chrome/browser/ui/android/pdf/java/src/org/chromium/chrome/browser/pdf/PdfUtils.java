74017be6abc2b0593d86667a8ce21449e61c5bb3
Shu Yang <shuyng@google.com>
2025-02-26 10:13:47 -0800

    Allow data scheme for pdf pages
    screenshot: https://screenshot.googleplex.com/AKoq2Lg5KAtUWM3.png
    Caveat: inline PDF rendering for iframes is not supported yet.
    
    Bug: 399227212
    Change-Id: I920bb55c0283fe6d6913376601d397c0c8f4be43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6303009
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425243}

--- chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfUtils.java
+++ chrome/browser/ui/android/pdf/java/src/org/chromium/chrome/browser/pdf/PdfUtils.java
@@ -31,7 +31,6 @@ import java.io.File;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.net.URLEncoder;
-import java.util.HashSet;
 import java.util.Set;
 
 /** Utilities for inline pdf support. */
@@ -72,14 +71,15 @@ public class PdfUtils {
     private static final String PARAM_ANDROID_INLINE_PDF_IN_INCOGNITO = "inline_pdf_in_incognito";
     private static final String PARAM_ANDROID_INLINE_PDF_BACKPORT_IN_INCOGNITO =
             "inline_pdf_backport_in_incognito";
+    private static final Set<String> TRANSIENT_PDF_SCHEMES =
+            Set.of(
+                    UrlConstants.HTTP_SCHEME,
+                    UrlConstants.HTTPS_SCHEME,
+                    UrlConstants.BLOB_SCHEME,
+                    UrlConstants.DATA_SCHEME);
+    private static final Set<String> PERMANENT_PDF_SCHEMES =
+            Set.of(UrlConstants.CONTENT_SCHEME, UrlConstants.FILE_SCHEME);
     private static boolean sShouldOpenPdfInlineForTesting;
-    private static final Set<String> TRANSIENT_PDF_SCHEMES = new HashSet<>();
-
-    static {
-        TRANSIENT_PDF_SCHEMES.add(UrlConstants.HTTP_SCHEME);
-        TRANSIENT_PDF_SCHEMES.add(UrlConstants.HTTPS_SCHEME);
-        TRANSIENT_PDF_SCHEMES.add(UrlConstants.BLOB_SCHEME);
-    }
 
     /**
      * Determines whether the navigation is to a pdf file.
@@ -94,7 +94,7 @@ public class PdfUtils {
             return false;
         }
 
-        if (scheme.equals(UrlConstants.FILE_SCHEME) || scheme.equals(UrlConstants.CONTENT_SCHEME)) {
+        if (PERMANENT_PDF_SCHEMES.contains(scheme)) {
             return true;
         }
         if (TRANSIENT_PDF_SCHEMES.contains(scheme)) {
@@ -115,8 +115,7 @@ public class PdfUtils {
             return false;
         }
 
-        return scheme.equals(UrlConstants.FILE_SCHEME)
-                || scheme.equals(UrlConstants.CONTENT_SCHEME);
+        return PERMANENT_PDF_SCHEMES.contains(scheme);
     }
 
     private static @Nullable String getDecodedScheme(String url) {
@@ -191,11 +190,7 @@ public class PdfUtils {
         Uri uri = Uri.parse(url);
         String scheme = uri.getScheme();
         assert scheme != null;
-        assert scheme.equals(UrlConstants.HTTP_SCHEME)
-                || scheme.equals(UrlConstants.HTTPS_SCHEME)
-                || scheme.equals(UrlConstants.BLOB_SCHEME)
-                || scheme.equals(UrlConstants.CONTENT_SCHEME)
-                || scheme.equals(UrlConstants.FILE_SCHEME);
+        assert TRANSIENT_PDF_SCHEMES.contains(scheme) || PERMANENT_PDF_SCHEMES.contains(scheme);
         String fileName = defaultTitle;
         if (scheme.equals(UrlConstants.CONTENT_SCHEME)) {
             String displayName = ContentUriUtils.maybeGetDisplayName(url);
@@ -248,7 +243,7 @@ public class PdfUtils {
         if (TRANSIENT_PDF_SCHEMES.contains(scheme)) {
             return isDownloadSafe ? PdfPageType.TRANSIENT_SECURE : PdfPageType.TRANSIENT_INSECURE;
         }
-        if (scheme.equals(UrlConstants.CONTENT_SCHEME) || scheme.equals(UrlConstants.FILE_SCHEME)) {
+        if (PERMANENT_PDF_SCHEMES.contains(scheme)) {
             return PdfPageType.LOCAL;
         }
         return PdfPageType.NONE;
