d8c6a40062770aab5199a02b4afd3ac875d165a3
Colin Blundell <blundell@chromium.org>
2025-02-26 00:05:29 -0800

    [Blink] Port CreateFromCanvasSI() call to new variant
    Has it pass the components individually rather than passing in
    SkImageInfo.
    
    Bug: 371227617
    Change-Id: I5f2df5053fe8e0bb4025dab6f42a32bc72044749
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6300636
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424997}

--- third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image_test.cc
+++ third_party/blink/renderer/platform/graphics/accelerated_static_bitmap_image_test.cc
@@ -57,8 +57,8 @@ scoped_refptr<StaticBitmapImage> CreateBitmap() {
   auto client_si = gpu::ClientSharedImage::CreateForTesting();
 
   return AcceleratedStaticBitmapImage::CreateFromCanvasSharedImage(
-      std::move(client_si), GenTestSyncToken(100), 0,
-      SkImageInfo::MakeN32Premul(100, 100),
+      std::move(client_si), GenTestSyncToken(100), 0, gfx::Size(100, 100),
+      kN32_SkColorType, kPremul_SkAlphaType, nullptr,
       SharedGpuContext::ContextProviderWrapper(),
       base::PlatformThread::CurrentRef(),
       base::MakeRefCounted<base::NullTaskRunner>(), base::DoNothing());
