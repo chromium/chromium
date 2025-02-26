29b1dd1ae5f4f3d02c08235281b83646fa7c6ebf
Maks Orlovich <morlovich@chromium.org>
2025-02-26 07:41:45 -0800

    PrivateAggregationApiProtectedAudienceAdditionalExtensions on by default
    ...to prep for launch.
    
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/F0GAisJlomc/m/aoD_b_CXAAAJ
    
    Bug: 361262468
    Change-Id: Ida4377407ad2858a6ef46bbb71e633d3337c0c26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6278765
    Reviewed-by: Orr Bernstein <orrb@google.com>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425145}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2130,7 +2130,7 @@ BASE_FEATURE_PARAM(bool,
 // kPrivateAggregationApiProtectedAudienceExtensionsEnabled.
 BASE_FEATURE(kPrivateAggregationApiProtectedAudienceAdditionalExtensions,
              "PrivateAggregationApiProtectedAudienceAdditionalExtensions",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kProcessHtmlDataImmediately,
              "ProcessHtmlDataImmediately",
