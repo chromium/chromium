862406430437392d507e82a77b2e8ea0c2d48244
Fiaz Muhammad <mfiaz@google.com>
2025-02-26 12:19:30 -0800

    [Tab Group Parity] Add Views, Layouts & New Tab Group Row
    - New ViewBinder class for the New Tab Group Row of the Bottom Sheet
    - New Strings
    - New Dimensions
    - Add Layout for Tab Group Parity Bottom Sheet and the 'New Tab Group'
            Row
    
    Bug: 391387522
    Change-Id: I3e335212c0ed1b58e6616191695448097f5378fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304824
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425336}

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetView.java
@@ -0,0 +1,113 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.R;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
+import org.chromium.ui.modelutil.SimpleRecyclerViewAdapter;
+
+/** A view for the Tab Group List Bottom Sheet. Allows the user to add tabs to a tab group. */
+@NullMarked
+public class TabGroupListBottomSheetView extends LinearLayout implements BottomSheetContent {
+    private final RecyclerView mRecyclerView;
+    private final ViewGroup mContentView;
+
+    TabGroupListBottomSheetView(Context context) {
+        super(context);
+        mContentView =
+                (ViewGroup)
+                        LayoutInflater.from(context)
+                                .inflate(R.layout.tab_group_list_bottom_sheet, /* root= */ null);
+        mRecyclerView = mContentView.findViewById(R.id.tab_group_parity_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
+
+    void setRecyclerViewAdapter(SimpleRecyclerViewAdapter adapter) {
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    // BottomSheetContent implementation follows:
+    @Override
+    public View getContentView() {
+        return mContentView;
+    }
+
+    @Override
+    @Nullable
+    public View getToolbarView() {
+        return null;
+    }
+
+    @Override
+    public int getVerticalScrollOffset() {
+        return mRecyclerView.getScrollY();
+    }
+
+    @Override
+    public void destroy() {}
+
+    @Override
+    public boolean swipeToDismissEnabled() {
+        return true;
+    }
+
+    @Override
+    public int getPriority() {
+        return ContentPriority.LOW;
+    }
+
+    @Override
+    public float getFullHeightRatio() {
+        return HeightMode.WRAP_CONTENT;
+    }
+
+    @Override
+    public int getPeekHeight() {
+        return HeightMode.DISABLED;
+    }
+
+    @Override
+    public boolean hideOnScroll() {
+        return true;
+    }
+
+    // TODO: Update strings.
+    @Override
+    public @NonNull String getSheetContentDescription(Context context) {
+        return context.getString(R.string.tab_group_share_notice_bottom_sheet_content_description);
+    }
+
+    @Override
+    public int getSheetHalfHeightAccessibilityStringId() {
+        return R.string.tab_group_share_notice_bottom_sheet_full_height;
+    }
+
+    @Override
+    public int getSheetFullHeightAccessibilityStringId() {
+        return R.string.tab_group_share_notice_bottom_sheet_half_height;
+    }
+
+    @Override
+    public int getSheetClosedAccessibilityStringId() {
+        return R.string.tab_group_share_notice_bottom_sheet_closed;
+    }
+}
