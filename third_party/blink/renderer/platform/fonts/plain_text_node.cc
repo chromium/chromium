9508df52985483e4206677e6f9469f6037af69b3
Kent Tamura <tkent@chromium.org>
2025-02-25 21:35:46 -0800

    CanvasTextNG: Add PlainTextPainter::SegmentAndShape()
    It will be helpful to get ShapeResult/ShapeResultView from a TextRun.
    This CL adds PlainTextNode and PlainTextItem classes. They represent
    the result of SegmentAndShape().
    
    This CL has no behavior changes because of disabled runtime flags.
    
    Bug: 389726691
    Change-Id: If3e9de7828d7997dc749eb68fdef2b2884ebb9e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6290105
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1424964}

---
+++ third_party/blink/renderer/platform/fonts/plain_text_node.cc
@@ -0,0 +1,41 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/platform/fonts/plain_text_node.h"
+
+#include "third_party/blink/renderer/platform/fonts/font.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/shape_result.h"
+#include "third_party/blink/renderer/platform/fonts/shaping/shape_result_view.h"
+#include "third_party/blink/renderer/platform/text/text_run.h"
+
+namespace blink {
+
+void PlainTextItem::Trace(Visitor* visitor) const {
+  visitor->Trace(shape_result_);
+  visitor->Trace(shape_result_view_);
+}
+
+const ShapeResultView* PlainTextItem::EnsureView() const {
+  if (shape_result_ && !shape_result_view_) {
+    shape_result_view_ = ShapeResultView::Create(shape_result_);
+  }
+  return shape_result_view_;
+}
+
+// ================================================================
+
+PlainTextNode::PlainTextNode(const TextRun& run,
+                             bool normalize_space,
+                             bool bidi_overridden,
+                             const Font& font,
+                             bool supports_bidi)
+    : base_direction_(run.Direction()) {
+  // TODO(crbug.com/389726691): Implement segmentation and shaping.
+}
+
+void PlainTextNode::Trace(Visitor* visitor) const {
+  visitor->Trace(item_list_);
+}
+
+}  // namespace blink
