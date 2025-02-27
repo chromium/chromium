09e957a03e2a7af908f37ce05430707738e3cbf8
Quentin Pubert <qpubert@google.com>
2025-02-27 01:34:01 -0800

    [iOS] Enable Choose from Drive flag by default
    This CL enables the Choose from Drive feature flag by default to launch
    the associated feature. The field trial testing config for Choose from
    Drive is also removed.
    
    Change-Id: Ie72d20218726c54aafb4aed216ba720105179cfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6304301
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425632}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -437,7 +437,7 @@ TipsSafeBrowsingExperimentType TipsSafeBrowsingExperimentTypeEnabled() {
 
 BASE_FEATURE(kIOSChooseFromDrive,
              "IOSChooseFromDrive",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIOSDownloadNoUIUpdateInBackground,
              "IOSDownloadNoUIUpdateInBackground",
