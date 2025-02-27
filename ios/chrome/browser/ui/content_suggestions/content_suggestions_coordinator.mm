83b100dba00a8b643d62926c612021bec3e1cbfa
David Maunder <davidjm@google.com>
2025-02-27 06:03:37 -0800

    Add 'See more' integration for Price Tracking card
    This CL makes 'See more' visible for the Price Tracking card and
    integrates it with the price tracking menu. There is more to be done -
    for example, the menu will need to be customized from this entry point
    (not 'current page' for example). But this CL lays the foundation.
    
    Bug: 392972945
    Change-Id: Ie5f6f80f5b28ed8eede2ea680fc30016e7308bc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6303021
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425700}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -102,6 +102,7 @@
 #import "ios/chrome/browser/shared/public/commands/omnibox_commands.h"
 #import "ios/chrome/browser/shared/public/commands/open_lens_input_selection_command.h"
 #import "ios/chrome/browser/shared/public/commands/open_new_tab_command.h"
+#import "ios/chrome/browser/shared/public/commands/price_notifications_commands.h"
 #import "ios/chrome/browser/shared/public/commands/search_image_with_lens_command.h"
 #import "ios/chrome/browser/shared/public/commands/settings_commands.h"
 #import "ios/chrome/browser/shared/public/commands/show_signin_command.h"
@@ -869,6 +870,14 @@ using segmentation_platform::TipIdentifier;
       break;
     case ContentSuggestionsModuleType::kTabResumption:
       [self showMagicStackRecentTabs];
+      break;
+    case ContentSuggestionsModuleType::kShopCard:
+      if (commerce::kShopCardVariation.Get() == commerce::kShopCardArm1) {
+        id<PriceNotificationsCommands> priceNotificationsCommands =
+            HandlerForProtocol(self.browser->GetCommandDispatcher(),
+                               PriceNotificationsCommands);
+        [priceNotificationsCommands showPriceNotifications];
+      }
       break;
     default:
       break;
