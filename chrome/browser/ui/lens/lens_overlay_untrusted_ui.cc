20906db61b2bad95373faa6b19738a88e8c44710
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-02-27 05:54:15 -0800

    Revert "[lensoverlay] Add param to auto-focus the contextual searchbox."
    This reverts commit 5122eaebeae7b11c0050640a35b6e529f88a2835.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/4528951767597056
    
    Sample build with failed test: https://ci.chromium.org/b/8721818020600726225
    Affected test(s):
    [ninja://chrome/test:interactive_ui_tests/LensOverlayControllerCUJTest.CopyKeyCommandCopies](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FLensOverlayControllerCUJTest.CopyKeyCommandCopies?q=VHash%3Aeb4962cfe6250e4a)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F4528951767597056&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6307422&type=BUG
    
    Original change's description:
    > [lensoverlay] Add param to auto-focus the contextual searchbox.
    >
    > Screencast:
    > https://screencast.googleplex.com/cast/NjQ2ODIzNTEwMzgzMDAxNnwxNjc1YTUxNi0zYw
    >
    > Bug: b:399441527
    > Change-Id: I76dd62dc1453b99b8ee2babf7682f4f4ed317499
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6307422
    > Reviewed-by: Duncan Mercer <mercerd@google.com>
    > Reviewed-by: Ali Stanfield <stanfield@google.com>
    > Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1425502}
    >
    
    Bug: b:399441527
    Change-Id: Iea868bf938ee70fd225ee2b87ad910cd7243f5db
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6308173
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425698}

--- chrome/browser/ui/lens/lens_overlay_untrusted_ui.cc
+++ chrome/browser/ui/lens/lens_overlay_untrusted_ui.cc
@@ -210,8 +210,6 @@ LensOverlayUntrustedUI::LensOverlayUntrustedUI(content::WebUI* web_ui)
       lens::features::GetLensOverlayTranslateRecentLanguagesAmount());
   html_source->AddBoolean("simplifiedSelectionEnabled",
                           lens::features::IsSimplifiedSelectionEnabled());
-  html_source->AddBoolean("autoFocusSearchbox",
-                          lens::features::ShouldAutoFocusSearchbox());
 
   LensOverlayController& controller = GetLensOverlayController();
   html_source->AddDouble("invocationTime",
