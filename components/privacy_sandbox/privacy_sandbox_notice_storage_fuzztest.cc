8788ab1942e8a58fa1083df1d3d017dd17765eec
Christine Ying <chrstne@google.com>
2025-02-27 07:03:04 -0800

    [N&C] Add NoticeEvents List in N&C Storage data model
    migration following go/koilos:notice-consent-storage-schema-2-update-onepager
    
    Change-Id: Ie6f5cdd9040e7695f8bab995bc515d7ff80004cb
    Bug: 392088228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6226841
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1425724}

---
+++ components/privacy_sandbox/privacy_sandbox_notice_storage_fuzztest.cc
@@ -0,0 +1,54 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/privacy_sandbox/privacy_sandbox_notice_storage.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/fuzztest/src/fuzztest/fuzztest.h"
+
+namespace privacy_sandbox {
+
+void CheckConvertsV1SchemaSuccessfully(int notice_action_taken,
+                                       base::Time notice_taken_time,
+                                       base::Time notice_last_shown) {
+  V1MigrationData data_v1;
+  data_v1.notice_action_taken =
+      static_cast<NoticeActionTaken>(notice_action_taken);
+  data_v1.notice_action_taken_time = notice_taken_time;
+  data_v1.notice_last_shown = notice_last_shown;
+  PrivacySandboxNoticeData data_v2 =
+      PrivacySandboxNoticeStorage::ConvertV1SchemaToV2Schema(data_v1);
+  EXPECT_EQ(data_v2.schema_version, 2);
+  if (notice_action_taken) {
+    auto notice_event = PrivacySandboxNoticeStorage::NoticeActionToNoticeEvent(
+        static_cast<NoticeActionTaken>(notice_action_taken));
+    if (notice_event.has_value()) {
+      EXPECT_THAT(data_v2.notice_events,
+                  testing::IsSupersetOf(
+                      {std::make_pair(*notice_event, notice_taken_time)}));
+    }
+  }
+  if (notice_last_shown != base::Time()) {
+    EXPECT_THAT(data_v2.notice_events,
+                testing::IsSupersetOf(
+                    {std::make_pair(NoticeEvent::kShown, notice_last_shown)}));
+  }
+}
+
+fuzztest::Domain<base::Time> AnyTime() {
+  return fuzztest::Map(
+      [](int64_t micros) {
+        return base::Time::FromDeltaSinceWindowsEpoch(
+            base::Microseconds(micros));
+      },
+      fuzztest::Arbitrary<int64_t>());
+}
+
+FUZZ_TEST(PrivacySandboxNoticeStorageFuzzTest,
+          CheckConvertsV1SchemaSuccessfully)
+    .WithDomains(/*notice_action_taken:*/ fuzztest::InRange(0, 9),
+                 /*notice_taken_time:*/ AnyTime(),
+                 /*notice_last_shown:*/ AnyTime());
+
+}  // namespace privacy_sandbox
