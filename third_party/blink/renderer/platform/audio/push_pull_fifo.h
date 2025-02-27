63f40fc302e68cda4142b11eaa547bd8f3860dc0
Michael Wilson <mjwilson@chromium.org>
2025-02-27 15:41:06 -0800

    Remove unused kMaxFIFOLength
    I missed the declaration in https://crrev.com/c/4990165, remove this
    from the header file too.
    
    Change-Id: I62cee5e6ab35e928e7900e40eee2e93edaa3a7fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310438
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Auto-Submit: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426023}

--- third_party/blink/renderer/platform/audio/push_pull_fifo.h
+++ third_party/blink/renderer/platform/audio/push_pull_fifo.h
@@ -41,15 +41,11 @@ class PLATFORM_EXPORT PushPullFIFO final {
   USING_FAST_MALLOC(PushPullFIFO);
 
  public:
-  // Maximum FIFO length. (512 render quanta)
-  static const uint32_t kMaxFIFOLength;
-
   struct PullResult {
     uint32_t frames_provided = 0;
     size_t frames_to_render = 0;
   };
 
-  // |fifo_length| cannot exceed |kMaxFIFOLength|. Otherwise it crashes.
   // ||render_quantum_frames| is the render size used by the audio graph.  It
   // |defaults to 128, the original and default render size.
   explicit PushPullFIFO(unsigned number_of_channels,
