37d5cfa6bf7170434e72c8524bbfd7921b7d6b5a
Keigo Oka <oka@google.com>
2025-02-26 17:33:32 -0800

    extensions: Reorganize developer_private_event_router
    So that DeveloperPrivateEventRouter can be used on desktop android.
    
    Bug: b/392777363
    Change-Id: I694b96bc944f4fbffc6407a4a6cb4c2f59dc7847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6303838
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425495}

--- chrome/browser/extensions/api/developer_private/developer_private_event_router_desktop.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_event_router_desktop.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/api/developer_private/developer_private_event_router.h"
+#include "chrome/browser/extensions/api/developer_private/developer_private_event_router_desktop.h"
 
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_sync_util.h"
