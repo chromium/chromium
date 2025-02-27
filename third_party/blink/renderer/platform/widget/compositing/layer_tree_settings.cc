a5237a321287cb35c8d0523f20ff63c32055ad86
Saifuddin Hitawala <hitawala@chromium.org>
2025-02-27 12:19:20 -0800

    [blink] Don't set use_rgba_4444 for graphite
    Windows currently can use RGBA4444 formats for tiles when the device
    has a low memory policy. In such cases SkiaRenderer can end up
    drawing a TileDrawQuad which in turn creates promise textures with
    RGBA4444 formats. Such formats are not supported by Dawn.
    
    Thus, we should avoid using this format when Skia's Graphite-Dawn
    backend is being used. This change checks for the feature before
    updating the setting which is used for setting the format.
    
    Bug: 398868042
    Change-Id: I80299866e488ca54bfbdb18fc2328e083d1d84f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310795
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Quyen Le <lehoangquyen@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425908}

--- third_party/blink/renderer/platform/widget/compositing/layer_tree_settings.cc
+++ third_party/blink/renderer/platform/widget/compositing/layer_tree_settings.cc
@@ -536,10 +536,14 @@ cc::LayerTreeSettings GenerateLayerTreeSettings(
     //  - If we are not running in a WebView, where 4444 isn't supported.
     //  - If we are not using vulkan, since some GPU drivers don't support
     //    using RGBA4444 as color buffer.
+    //  - If we are not using Skia's Graphite-Dawn backend, since dawn does not
+    //  support RGBA_4444 formats.
     // TODO(penghuang): query supported formats from GPU process.
     if (!cmd.HasSwitch(switches::kDisableRGBA4444Textures) &&
         base::SysInfo::AmountOfPhysicalMemoryMB() <= 512 &&
-        !::features::IsUsingVulkan()) {
+        !::features::IsUsingVulkan() &&
+        !::features::IsSkiaGraphiteEnabled(
+            base::CommandLine::ForCurrentProcess())) {
       settings.use_rgba_4444 = true;
 
       // If we are going to unpremultiply and dither these tiles, we need to
