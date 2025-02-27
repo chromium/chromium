6de2fd644f7ba3a46ca8863f7a2e6c02f9d26427
Noam Rosenthal <nrosenthal@chromium.org>
2025-02-27 01:16:10 -0800

    corner-shape: render all curved superellipse values
    This uses a formula that approximates a superellipse using
    bezier curves, written by fserb@.
    
    The superellipse value creates 5 control points, that are used
    to draw two bezier curves per corner.
    
    Notch/bevel are special-cased, so the formula doesn't have to
    deal with infinites.
    
    Note that the WPT uses the same formula, but those were manually tested
    to demonstrate that they produce results very close to a superellipse,
    in a manner that works today on all browsers (using canvas).
    
    Bug: 394059604
    
    R=fs@opera.com
    R=fserb@chromium.org
    R=pdr@chromium.org
    
    Change-Id: Ia4496c3e410351dc050acfbc07f77c92653c63b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6286466
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1425626}

--- third_party/blink/renderer/platform/geometry/path.cc
+++ third_party/blink/renderer/platform/geometry/path.cc
@@ -31,15 +31,19 @@
 
 #include <math.h>
 
+#include <algorithm>
+
 #include "third_party/blink/renderer/platform/geometry/skia_geometry_utils.h"
 #include "third_party/blink/renderer/platform/geometry/stroke_data.h"
 #include "third_party/blink/renderer/platform/transforms/affine_transform.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/skia/include/pathops/SkPathOps.h"
+#include "ui/gfx/geometry/point.h"
 #include "ui/gfx/geometry/point_f.h"
 #include "ui/gfx/geometry/quad_f.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/skia_conversions.h"
+#include "ui/gfx/geometry/vector2d_f.h"
 
 namespace blink {
 
@@ -59,6 +63,47 @@ bool PathQuadIntersection(const SkPath& path, const gfx::QuadF& quad) {
 }
 
 enum class Corner { kTopLeft, kTopRight, kBottomRight, kBottomLeft };
+
+gfx::Vector2dF SuperellipseAt(float s, float t) {
+  float n = std::pow(2, s);
+  float x = std::pow(t, 1 / n);
+  float y = std::pow(1 - t, 1 / n);
+  return gfx::Vector2dF(x, y);
+}
+
+// TODO(fserb) document this.
+std::array<gfx::Vector2dF, 5> ApproximateSuperellipseAsBezierCurvePair(
+    float curvature) {
+  static constexpr std::array<double, 7> p = {
+      1.2430920942724248, 2.010479023614843,  0.32922901179443753,
+      0.2823023142212073, 1.3473704261055421, 2.9149468637949814,
+      0.9106507102917086};
+
+  DCHECK_GT(curvature, 0);
+  const float s = std::log2(curvature);
+  const float abs_s = std::abs(s);
+  const float slope =
+      p[0] + (p[6] - p[0]) * 0.5 * (1 + std::tanh(p[5] * (abs_s - p[1])));
+  const float base = 1 / (1 + std::exp(-slope * (0 - p[1])));
+  const float logistic = 1 / (1 + std::exp(-slope * (abs_s - p[1])));
+
+  const float a = (logistic - base) / (1 - base);
+  const float b = p[2] * std::exp(-p[3] * std::pow(abs_s, p[4]));
+
+  gfx::Vector2dF P1(a, 1);
+  gfx::Vector2dF P3 = SuperellipseAt(abs_s, 0.5);
+  gfx::Vector2dF P5(1, a);
+
+  if (s < 0) {
+    P1 = gfx::Vector2dF(1 - P1.y(), 1 - P1.x());
+    P3 = gfx::Vector2dF(1 - P3.y(), 1 - P3.x());
+    P5 = gfx::Vector2dF(1 - P5.y(), 1 - P5.x());
+  }
+
+  gfx::Vector2dF P2(P3.x() - b, P3.y() + b);
+  gfx::Vector2dF P4(P3.x() + b, P3.y() - b);
+  return {P1, P2, P3, P4, P5};
+}
 
 void AddCornerShape(SkPath& path,
                     gfx::RectF corner_rect,
@@ -100,7 +145,7 @@ void AddCornerShape(SkPath& path,
     }
     path.lineTo(gfx::PointFToSkPoint(control_point));
     path.lineTo(gfx::PointFToSkPoint(target_point));
-  } else {
+  } else if (curvature == 2) {
     gfx::PointF control_point;
     switch (corner) {
       case Corner::kTopLeft:
@@ -115,10 +160,40 @@ void AddCornerShape(SkPath& path,
       case Corner::kBottomLeft:
         control_point = corner_rect.bottom_left();
         break;
-        // TODO(crbug.com/394059604): render the other curvatures
     }
     path.conicTo(gfx::PointFToSkPoint(control_point),
                  gfx::PointFToSkPoint(target_point), SK_ScalarRoot2Over2);
+  } else {
+    auto control_points = ApproximateSuperellipseAsBezierCurvePair(
+        corner == Corner::kBottomRight || corner == Corner::kTopLeft
+            ? 1 / curvature
+            : curvature);
+    gfx::PointF starting_point;
+    switch (corner) {
+      case Corner::kTopLeft:
+        starting_point = corner_rect.bottom_left();
+        break;
+      case Corner::kTopRight:
+        starting_point = corner_rect.origin();
+        break;
+      case Corner::kBottomRight:
+        starting_point = corner_rect.top_right();
+        break;
+      case Corner::kBottomLeft:
+        starting_point = corner_rect.bottom_right();
+        break;
+    }
+    const gfx::Vector2dF target_vector = target_point - starting_point;
+    std::array<SkPoint, 5> points;
+    std::ranges::transform(
+        control_points, points.begin(), [&](gfx::Vector2dF cv) {
+          auto out_cv = gfx::ScaleVector2d(target_vector, cv.x(), 1 - cv.y());
+          return gfx::PointFToSkPoint(starting_point + out_cv);
+        });
+
+    path.cubicTo(points.at(0), points.at(1), points.at(2));
+    path.cubicTo(points.at(3), points.at(4),
+                 gfx::PointFToSkPoint(target_point));
   }
 }
 
