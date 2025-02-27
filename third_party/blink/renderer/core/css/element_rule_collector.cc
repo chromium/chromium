98a9b8203d1bfe1f7e9e9cb69ad473bab673cc7f
Daniil Sakhapov <sakhapov@chromium.org>
2025-02-27 09:51:24 -0800

    [carousel] Fix :focus-visible for ::scroll-marker
    CollectMatchingRulesInternal used `element` to check
    :focus-visible pseudo class matching, but for matching pseudo
    elements we should use `pseudo_element` instead. This CL fixes
    it for :focus-visible and :focus.
    
    Fixed: 399118908
    Change-Id: Ib94454ca84fb8a8f82c656672686e8e51f5f3a10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6308982
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1425809}

--- third_party/blink/renderer/core/css/element_rule_collector.cc
+++ third_party/blink/renderer/core/css/element_rule_collector.cc
@@ -901,8 +901,14 @@ DISABLE_CFI_PERF bool ElementRuleCollector::CollectMatchingRulesInternal(
     }
   }
 
+  // Get either `element` if we are collecting rules for regular element,
+  // or `pseudo_element` if we are collecting rules for pseudo element.
+  // 0u indicates the pseudo element we resolve for, look inside for more
+  // context.
+  Element& matching_element = context.context.GetElementForMatching(0u);
   if (match_request.HasAnyRuleSetsWithFocusPseudoClassRules()) {
-    if (SelectorChecker::MatchesFocusPseudoClass(element, kPseudoIdNone)) {
+    if (SelectorChecker::MatchesFocusPseudoClass(matching_element,
+                                                 kPseudoIdNone)) {
       for (const auto bundle :
            match_request.RuleSetsWithFocusPseudoClassRules()) {
         if (CollectMatchingRulesForList<stop_at_first_match>(
@@ -929,7 +935,7 @@ DISABLE_CFI_PERF bool ElementRuleCollector::CollectMatchingRulesInternal(
   }
 
   if (match_request.HasAnyRuleSetsWithFocusVisiblePseudoClassRules()) {
-    if (SelectorChecker::MatchesFocusVisiblePseudoClass(element)) {
+    if (SelectorChecker::MatchesFocusVisiblePseudoClass(matching_element)) {
       for (const auto bundle :
            match_request.RuleSetsWithFocusVisiblePseudoClassRules()) {
         if (CollectMatchingRulesForList<stop_at_first_match>(
