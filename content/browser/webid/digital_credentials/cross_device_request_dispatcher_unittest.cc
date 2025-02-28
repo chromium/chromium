15f410713471d0e94b4433d1423315fc3486c690
Mohamed Amir Yosef <mamir@chromium.org>
2025-02-28 09:26:15 -0800

    [DC] Support CTAP protocol response format in cross-device flow
    CTAP protocol [*] defines the response format to be a json object
    containing both a protocol and a data field. This CL adds the support
    for the proper response and fallbacks to the old format in case some
    mobile devices aren't migrated yet.
    
    [*]
    https://fidoalliance.org/specs/fido-v2.2-rd-20241003/fido-client-to-authenticator-protocol-v2.2-rd-20241003.html#sctn-feature-descriptions-jsonBasedMessages-response-props
    
    Bug: 336329411
    Change-Id: I5400f4182863c3c062d7b822c2b32baa1429ad10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309007
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426393}

--- content/browser/webid/digital_credentials/cross_device_request_dispatcher_unittest.cc
+++ content/browser/webid/digital_credentials/cross_device_request_dispatcher_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "content/browser/webid/digital_credentials/cross_device_request_dispatcher.h"
 
+#include "base/json/json_reader.h"
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
@@ -133,12 +134,12 @@ class DigitalCredentialsCrossDeviceRequestDispatcherTest
 };
 
 TEST_P(DigitalCredentialsCrossDeviceRequestDispatcherTest, Valid) {
-  base::expected<Response, RequestDispatcher::Error> result =
-      Transact(device::cablev2::PayloadType::kJSON,
-               R"({"response": {"digital": {"data": "ok"}}})");
+  base::expected<Response, RequestDispatcher::Error> result = Transact(
+      device::cablev2::PayloadType::kJSON,
+      R"({"response": {"digital": {"data": {"vp_token" : "token"}}}})");
   ASSERT_TRUE(result.has_value());
-  ASSERT_TRUE(result.value()->is_string());
-  ASSERT_EQ(result.value()->GetString(), "ok");
+  ASSERT_EQ(result.value().value(),
+            base::JSONReader::Read(R"({"vp_token" : "token"})").value());
 }
 
 TEST_P(DigitalCredentialsCrossDeviceRequestDispatcherTest, InvalidJson) {
@@ -189,6 +190,24 @@ TEST_P(DigitalCredentialsCrossDeviceRequestDispatcherTest, CTAPResponse) {
   ASSERT_FALSE(result.has_value());
   EXPECT_EQ(result.error(),
             RequestDispatcher::Error(ProtocolError::kTransportError));
+}
+
+TEST_P(DigitalCredentialsCrossDeviceRequestDispatcherTest, NewResponseFormat) {
+  base::expected<Response, RequestDispatcher::Error> result =
+      Transact(device::cablev2::PayloadType::kJSON,
+               R"({
+           "response": {
+             "digital": {
+               "data": {
+                 "data": {"key": "value"},
+                 "protocol" : "ProtocolInResponse"
+               }
+             }
+           }
+         })");
+  ASSERT_TRUE(result.has_value());
+  EXPECT_EQ(result.value().value(),
+            base::JSONReader::Read(R"({"key":"value"})").value());
 }
 
 INSTANTIATE_TEST_SUITE_P(,
