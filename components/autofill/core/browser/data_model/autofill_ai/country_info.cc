36c6ffb8ec8fce947ace4e89ad8cdf592a11b7f8
Jihad Hanna <jihadghanna@google.com>
2025-02-28 10:36:33 -0800

    Update CountryInfo
    This CL removes the FormGroup inheritance from Country and instead
    replaced the corresponding methods with 4 methods to read/write country
    names/codes into objects of this class.
    
    Bug: 389625753
    Change-Id: I5bc8d89b39ca3eb8971a57b58788d1f5c00940ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311003
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426441}

--- components/autofill/core/browser/data_model/autofill_ai/country_info.cc
+++ components/autofill/core/browser/data_model/autofill_ai/country_info.cc
@@ -29,56 +29,32 @@ CountryInfo& CountryInfo::operator=(CountryInfo&& info) = default;
 
 CountryInfo::~CountryInfo() = default;
 
-bool operator==(const CountryInfo& lhs, const CountryInfo& rhs) {
-  return lhs.country_code_ == rhs.country_code_;
-}
-
-std::u16string CountryInfo::GetRawInfo(FieldType type) const {
-  CHECK_EQ(ADDRESS_HOME_COUNTRY, type);
-  return base::UTF8ToUTF16(country_code_);
-}
-
-void CountryInfo::SetRawInfoWithVerificationStatus(FieldType type,
-                                                   const std::u16string& value,
-                                                   VerificationStatus status) {
-  CHECK_EQ(ADDRESS_HOME_COUNTRY, type);
-  country_code_ = data_util::IsValidCountryCode(value)
-                      ? base::UTF16ToUTF8(value)
-                      : std::string();
-}
-
-FieldTypeSet CountryInfo::GetSupportedTypes() const {
-  static constexpr FieldTypeSet supported_types{ADDRESS_HOME_COUNTRY};
-  return supported_types;
-}
-
-std::u16string CountryInfo::GetInfo(const AutofillType& type,
-                                    const std::string& app_locale) const {
-  CHECK_EQ(ADDRESS_HOME_COUNTRY, type.GetStorableType());
+std::u16string CountryInfo::GetCountryName(
+    const std::string& app_locale) const {
   return AutofillCountry(country_code_, app_locale).name();
 }
 
-bool CountryInfo::SetInfoWithVerificationStatus(const AutofillType& type,
-                                                const std::u16string& value,
-                                                const std::string& app_locale,
-                                                VerificationStatus status) {
-  CHECK_EQ(ADDRESS_HOME_COUNTRY, type.GetStorableType());
-  if (data_util::IsValidCountryCode(value)) {
-    country_code_ = base::UTF16ToUTF8(value);
-    return true;
-  }
+std::string CountryInfo::GetCountryCode() const {
+  return country_code_;
+}
 
+bool CountryInfo::SetCountryFromCountryName(const std::u16string& value,
+                                            const std::string& app_locale) {
   if (CountryNames* country_names =
           !value.empty() ? CountryNames::GetInstance() : nullptr) {
     country_code_ =
         country_names->GetCountryCodeForLocalizedCountryName(value, app_locale);
+    return !country_code_.empty();
   }
-
-  return !country_code_.empty();
+  return false;
 }
 
-VerificationStatus CountryInfo::GetVerificationStatus(FieldType type) const {
-  return VerificationStatus::kNoStatus;
+bool CountryInfo::SetCountryFromCountryCode(const std::u16string& value) {
+  if (data_util::IsValidCountryCode(value)) {
+    country_code_ = base::UTF16ToUTF8(value);
+    return true;
+  }
+  return false;
 }
 
 }  // namespace autofill
