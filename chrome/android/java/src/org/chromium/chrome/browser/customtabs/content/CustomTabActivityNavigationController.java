a1b30389bf8db67852bc32ab41216fee932e0f3a
Shu Yang <shuyng@google.com>
2025-02-28 12:25:18 -0800

    Add histogram Android.CustomTab.CannotOpenUrlInBrowser.IsPdf
    This replaces the silent crash.
    
    Bug: 384992232
    Change-Id: I854bcb43283604933afa34ce69d92d53b5c3fd6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311295
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426511}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityNavigationController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/CustomTabActivityNavigationController.java
@@ -25,6 +25,7 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.Log;
 import org.chromium.base.PackageManagerUtils;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
@@ -33,7 +34,6 @@ import org.chromium.chrome.browser.IntentHandler;
 import org.chromium.chrome.browser.back_press.MinimizeAppAndCloseTabBackPressHandler;
 import org.chromium.chrome.browser.back_press.MinimizeAppAndCloseTabBackPressHandler.MinimizeAppAndCloseTabType;
 import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider;
-import org.chromium.chrome.browser.crash.ChromePureJavaExceptionReporter;
 import org.chromium.chrome.browser.customtabs.CloseButtonNavigator;
 import org.chromium.chrome.browser.customtabs.CustomTabObserver;
 import org.chromium.chrome.browser.document.ChromeLauncherActivity;
@@ -364,16 +364,10 @@ public class CustomTabActivityNavigationController
                                 R.string.custom_tab_cant_perform_action_toast,
                                 Toast.LENGTH_LONG)
                         .show();
-                // Silently crash to investigate https://crbug.com/384992232
+                // TODO(crbug.com/384992232): Clean up the histogram.
                 boolean isPdf = tab.isNativePage() && tab.getNativePage().isPdf();
-                String logMessage =
-                        "This is not a crash. The intent to open the URL currently being"
-                                + " displayed in the Custom Tab in the regular browser can not be"
-                                + " resolved by any Activity on the system. intent.getPackage() = "
-                                + intent.getPackage()
-                                + " isPdf = "
-                                + isPdf;
-                ChromePureJavaExceptionReporter.reportJavaException(new Throwable(logMessage));
+                RecordHistogram.recordBooleanHistogram(
+                        "Android.CustomTab.CannotOpenUrlInBrowser.IsPdf", isPdf);
             }
         }
         return true;
