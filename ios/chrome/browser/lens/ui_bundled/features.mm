bf00df6c2ac1b4cc83a2e81429480a4ed9175d00
Charles Yang <cmyang@google.com>
2025-02-28 09:51:20 -0800

    [Lens] Add unary salient text API feature flag.
    Should be enabled by default. Bridging the flag to Lens in case we need to turn it off.
    
    Bug: 399656114
    Change-Id: I22a3ba82b191982c487512ad8b0b3dced1cb32a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311269
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Charles Yang <cmyang@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426410}

--- ios/chrome/browser/lens/ui_bundled/features.mm
+++ ios/chrome/browser/lens/ui_bundled/features.mm
@@ -40,6 +40,10 @@ BASE_FEATURE(kLensUnaryHttpTransportEnabled,
              "LensUnaryHttpTransportEnabled",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kLensUnaryApiSalientTextEnabled,
+             "LensUnaryApiSalientTextEnabled",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 BASE_FEATURE(kLensClearcutBackgroundUploadEnabled,
              "LensClearcutBackgroundUploadEnabled",
              base::FEATURE_DISABLED_BY_DEFAULT);
