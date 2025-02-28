425ab45118423c8e9239703c98ee74be8f784de3
Tom Sepez <tsepez@chromium.org>
2025-02-28 09:23:25 -0800

    Remove file-wide unsafe_buffers pragma from blink/ [9 of N]
    Suppress warnings on an expression by expression basis.
    
    -- Some fixed-index array operations are now recognized as safe
       by the compiler, so suppressions are not needed in two files.
    
    Bug: 351564777
    Change-Id: I9e893d31d5ceec34dff84fc483aa6299c088e534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6310942
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1426389}

--- third_party/blink/renderer/platform/transforms/transform_operations.cc
+++ third_party/blink/renderer/platform/transforms/transform_operations.cc
@@ -19,14 +19,11 @@
  *
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/transforms/transform_operations.h"
 
 #include <algorithm>
+
+#include "base/compiler_specific.h"
 #include "third_party/blink/renderer/platform/geometry/blend.h"
 #include "third_party/blink/renderer/platform/transforms/interpolated_transform_operation.h"
 #include "third_party/blink/renderer/platform/transforms/matrix_3d_transform_operation.h"
@@ -258,10 +255,10 @@ static void FindCandidatesInPlane(double px,
   candidates[0] = phi;  // The element at 0deg (maximum x)
 
   for (int i = 1; i < *num_candidates; ++i)
-    candidates[i] = candidates[i - 1] + M_PI_2;  // every 90 deg
+    UNSAFE_TODO(candidates[i] = candidates[i - 1] + M_PI_2);  // every 90 deg
   if (nz < 0.f) {
     for (int i = 0; i < *num_candidates; ++i)
-      candidates[i] *= -1;
+      UNSAFE_TODO(candidates[i] *= -1);
   }
 }
 
@@ -363,7 +360,7 @@ static void BoundingBoxForArc(const gfx::Point3F& point,
   // Once we have the candidates, we now filter them down to ones that
   // actually live on the arc, rather than the entire circle.
   for (int i = 0; i < num_candidates; ++i) {
-    double radians = candidates[i];
+    double radians = UNSAFE_BUFFERS(candidates[i]);
 
     while (radians < min_radians)
       radians += 2.0 * M_PI;
