767b20724199ff5cde44662c3fb4e002be885584
Maryia Mankevich <mmaryia@google.com>
2025-02-28 02:07:16 -0800

    Add comment on why/when frame is going to be updated
    Bug: 397385645
    Change-Id: I6b5140559e6ac38d4a0060bcb5120082b1e01cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6305041
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Maryia Mankevich <mmaryia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1426198}

--- chrome/browser/autofill/captured_sites_test_utils.h
+++ chrome/browser/autofill/captured_sites_test_utils.h
@@ -385,6 +385,10 @@ class TestRecipeReplayer {
       const content::ToRenderFrameHost& frame,
       const std::string& element_xpath,
       bool expect_to_be_shown);
+  // When returning true, `frame` points to the RenderFrameHost of the frame
+  // specified in `action`. This is determined dynamically because
+  // WaitForElementToBeReady may need to wait for navigations to finish and the
+  // frame to be loaded.
   bool WaitForElementToBeReady(const std::string& xpath,
                                const int visibility_enum_val,
                                const base::Value::Dict& action,
