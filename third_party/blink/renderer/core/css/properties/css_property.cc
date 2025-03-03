eb4a32e5fbe01ad2cd1402626fc3e292ace96922
Tom Sepez <tsepez@chromium.org>
2025-03-03 14:41:46 -0800

    Remove file-wide unsafe_buffers pragma from blink/ [13 of N]
    Suppress warnings on an expression by expression basis.
    
    Bug: 351564777
    Change-Id: I99727f5dc0d87fc616be7de2637cdd26c3ea3398
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317183
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427401}

--- third_party/blink/renderer/core/css/properties/css_property.cc
+++ third_party/blink/renderer/core/css/properties/css_property.cc
@@ -2,13 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/css/properties/css_property.h"
 
+#include "base/compiler_specific.h"
 #include "third_party/blink/renderer/core/css/cssom/cross_thread_unsupported_value.h"
 #include "third_party/blink/renderer/core/css/cssom/style_value_factory.h"
 #include "third_party/blink/renderer/core/css/properties/computed_style_utils.h"
@@ -79,7 +75,7 @@ void CSSProperty::FilterWebExposedCSSPropertiesIntoVector(
   DCHECK(out_vector.empty());
   out_vector.reserve(property_count);
   for (unsigned i = 0; i < property_count; i++) {
-    const CSSProperty& property = Get(properties[i]);
+    const CSSProperty& property = Get(UNSAFE_TODO(properties[i]));
     if (property.IsWebExposed(execution_context)) {
       out_vector.push_back(&property);
     }
