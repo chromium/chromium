e71dd729d327a369f633fad78858419b8ef39031
Michael Ludwig <michaelludwig@google.com>
2025-03-03 10:25:16 -0800

    Increase error tolerance in OopTextBlobPixelTest.Config/RecordShader_FilterOnLayer_PerspectiveCTM
    Required in order to land https://skia-review.googlesource.com/c/skia/+/917681.
    The switch to 4x4 matrices in tracking scale decomposition under perspective slightly changes
    the expected image, and given the differences in chrome's own raster-at-scale -> fixed-scale
    calculations it is somewhat expected.
    
    Bug: b/371007032
    Bug: b/345331745
    Bug: b/40042800
    Change-Id: Ic39e10c7ace0b16f91117cd332c239ec8a3c8fcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6316993
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Auto-Submit: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427197}

--- cc/paint/oop_pixeltest.cc
+++ cc/paint/oop_pixeltest.cc
@@ -2176,7 +2176,7 @@ class OopTextBlobPixelTest
           max_abs_error = std::max(237, max_abs_error);
           avg_error = std::max(60.9f, avg_error);
 #else
-          max_abs_error = std::max(228, max_abs_error);
+          max_abs_error = std::max(229, max_abs_error);
           avg_error = std::max(40.2f, avg_error);
 #endif
           break;
