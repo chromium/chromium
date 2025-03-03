d5890cc4f4df98500b107b999e56c71864e11bf2
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-03-03 13:05:15 -0800

    Remove theme tracking from BubbleDialogDelegate
    In crrev.com/c/6242094, BubbleDialogDelegate was migrated to use
    `ui::ColorVariant`. So now BubbleDialogDelegate does not need to resolve
    ColorId into SkColors, we can get rid of theme tracking.
    
    Bug: b:394420459
    Change-Id: I26580400f347481f4c00ede6227256b74b8c16b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6314413
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427338}

--- ui/views/bubble/bubble_dialog_delegate_view.h
+++ ui/views/bubble/bubble_dialog_delegate_view.h
@@ -402,7 +402,6 @@ class VIEWS_EXPORT BubbleDialogDelegate : public DialogDelegate {
   class AnchorViewObserver;
   class AnchorWidgetObserver;
   class BubbleWidgetObserver;
-  class ThemeObserver;
 
   FRIEND_TEST_ALL_PREFIXES(BubbleDialogDelegateViewTest,
                            VisibleWidgetShowsInkDropOnAttaching);
@@ -417,7 +416,6 @@ class VIEWS_EXPORT BubbleDialogDelegate : public DialogDelegate {
   friend class AnchorWidgetObserver;
   friend class BubbleWidgetObserver;
   friend class TestBubbleUmaLogger;
-  friend class ThemeObserver;
 
   friend class BubbleBorderDelegate;
   friend class BubbleWindowTargeter;
@@ -435,9 +433,7 @@ class VIEWS_EXPORT BubbleDialogDelegate : public DialogDelegate {
   void OnBubbleWidgetPaintAsActiveChanged();
 
   void OnDeactivate();
-
-  // Update the bubble color from the NativeTheme unless it was explicitly set.
-  void UpdateColorsFromTheme();
+  void UpdateFrameColors();
 
   // Notify this bubble that it is now the primary anchored bubble. When a new
   // bubble becomes the primary anchor, the previous primary silently loses its
@@ -460,7 +456,6 @@ class VIEWS_EXPORT BubbleDialogDelegate : public DialogDelegate {
   std::unique_ptr<AnchorViewObserver> anchor_view_observer_;
   std::unique_ptr<AnchorWidgetObserver> anchor_widget_observer_;
   std::unique_ptr<BubbleWidgetObserver> bubble_widget_observer_;
-  std::unique_ptr<ThemeObserver> theme_observer_;
   bool adjust_if_offscreen_ = true;
   bool focus_traversable_from_anchor_view_ = true;
   ViewTracker highlighted_button_tracker_;
@@ -503,7 +498,7 @@ class VIEWS_EXPORT BubbleDialogDelegate : public DialogDelegate {
   bool paint_client_to_layer_ = false;
 
   // If true, contents view will be forced to create a solid color background in
-  // UpdateColorsFromTheme().
+  // `UpdateFrameColors()`.
   bool force_create_contents_background_ = false;
 
 #if BUILDFLAG(IS_MAC)
