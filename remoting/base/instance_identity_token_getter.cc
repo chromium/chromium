5efd723cb9ec22177a1893775576d823f64c85ce
Joe Downing <joedow@google.com>
2025-03-03 13:01:21 -0800

    Create a class to parse and validate instance identity tokens
    This will be used to share common code and can be updated in the
    future if we want to expose more fields.
    
    Bug: 388885661
    Change-Id: I428fb8215c56262e1e6c156428ee0df1998213b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317331
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Commit-Queue: Gary Kacmarcik <garykac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427336}

--- remoting/base/instance_identity_token_getter.cc
+++ remoting/base/instance_identity_token_getter.cc
@@ -9,103 +9,22 @@
 #include <string_view>
 #include <utility>
 
-#include "base/base64.h"
 #include "base/functional/callback.h"
-#include "base/json/json_reader.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequence_checker.h"
-#include "base/strings/string_split.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "net/base/net_errors.h"
 #include "remoting/base/http_status.h"
+#include "remoting/base/instance_identity_token.h"
 #include "remoting/base/logging.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace remoting {
-
-namespace {
-// Returns the 'payload' of the ID Token as a Dict if the token is well-formed,
-// otherwise returns nullopt.
-std::optional<base::Value::Dict> parseIdTokenPayload(std::string_view token) {
-  // ID Tokens are comprised of three parts separated by periods:
-  //   1) Base64 encoded JSON header
-  //   2) Base64 encoded JSON payload
-  //   3) Signature bytes
-  auto parts =
-      base::SplitString(token, ".", base::WhitespaceHandling::KEEP_WHITESPACE,
-                        base::SplitResult::SPLIT_WANT_NONEMPTY);
-  // Return early if the token is malformed or missing the signature.
-  if (parts.size() != 3) {
-    LOG(WARNING) << "Invalid instance identity token: " << token;
-    return std::nullopt;
-  }
-  // Parse and validate the header.
-  auto encoded_header = parts[0];
-  std::string decoded_header;
-  if (!base::Base64Decode(encoded_header, &decoded_header,
-                          base::Base64DecodePolicy::kForgiving)) {
-    LOG(WARNING) << "Failed to decode instance identity token header: "
-                 << encoded_header;
-    return std::nullopt;
-  }
-  auto header = base::JSONReader::ReadDict(decoded_header);
-  if (!header.has_value()) {
-    LOG(WARNING) << "Failed to parse instance identity token header: "
-                 << decoded_header;
-    return std::nullopt;
-  }
-  if (!header->contains("kid") || !header->contains("alg") ||
-      !header->contains("typ")) {
-    LOG(WARNING) << "Invalid instance identity token header: " << *header;
-    return std::nullopt;
-  }
-
-  // Parse and validate the payload.
-  auto encoded_payload = parts[1];
-  std::string decoded_payload;
-  if (!base::Base64Decode(encoded_payload, &decoded_payload,
-                          base::Base64DecodePolicy::kForgiving)) {
-    LOG(WARNING) << "Failed to decode instance identity token payload: "
-                 << encoded_payload;
-    return std::nullopt;
-  }
-  auto payload = base::JSONReader::ReadDict(decoded_payload);
-  if (!payload.has_value()) {
-    LOG(WARNING) << "Failed to parse instance identity token payload: "
-                 << decoded_payload;
-    return std::nullopt;
-  }
-  if (!payload->contains("iss") || !payload->contains("aud") ||
-      !payload->contains("iat") || !payload->contains("exp") ||
-      !payload->contains("azp") || !payload->contains("google")) {
-    LOG(WARNING) << "Invalid instance identity token payload: " << *payload;
-    return std::nullopt;
-  }
-  auto* google_payload = payload->FindDict("google");
-  auto* compute_engine_payload =
-      google_payload ? google_payload->FindDict("compute_engine") : nullptr;
-  if (compute_engine_payload == nullptr ||
-      !compute_engine_payload->contains("instance_id") ||
-      !compute_engine_payload->contains("instance_name") ||
-      !compute_engine_payload->contains("project_id") ||
-      !compute_engine_payload->contains("project_number") ||
-      !compute_engine_payload->contains("zone")) {
-    LOG(WARNING) << "Invalid instance identity token payload: " << *payload;
-    return std::nullopt;
-  }
-
-  HOST_LOG << "Retrieved instance identity token: " << *header << ":"
-           << *payload;
-
-  return payload;
-}
-
-}  // namespace
 
 InstanceIdentityTokenGetter::InstanceIdentityTokenGetter(
     std::string_view audience,
@@ -145,28 +64,38 @@ void InstanceIdentityTokenGetter::OnTokenRetrieved(const HttpStatus& response) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (response.ok()) {
-    auto response_body = response.response_body();
-    auto payload = parseIdTokenPayload(response_body);
-    // If the payload can be retrieved then the token is correctly formatted.
-    if (payload.has_value()) {
-      identity_token_ = std::move(response_body);
-      auto token_exp_value = payload->FindInt("exp");
+    auto jwt = response.response_body();
+    auto validated_token = InstanceIdentityToken::Create(jwt);
+    if (validated_token.has_value()) {
+      HOST_LOG << "Retrieved fresh instance identity token";
+      if (identity_token_.empty()) {
+        // Log the token retrieved if this is the first successful fetch.
+        HOST_LOG << "Instance identity token contents:\n" << *validated_token;
+      }
+      identity_token_ = std::move(jwt);
+
+      auto token_exp_value = validated_token->payload().FindInt("exp");
       auto now = base::Time::Now();
       if (token_exp_value.has_value()) {
         token_expiration_time_ =
             base::Time::FromSecondsSinceUnixEpoch(*token_exp_value);
-        HOST_LOG << "Instance identity token expires at: "
-                 << token_expiration_time_;
         LOG_IF(WARNING, token_expiration_time_ < now + base::Minutes(30) ||
                             token_expiration_time_ > now + base::Minutes(90))
             << "Token expiration is outside of the expected lifetime window "
             << "which is ~60 minutes.";
       } else {
         LOG(WARNING) << "Token payload missing valid 'exp' integer value which "
-                     << "may mean other fields are also invalid: " << *payload;
+                     << "may mean other fields are also invalid: "
+                     << validated_token->payload();
         token_expiration_time_ = now;
       }
     }
+  } else {
+    int error_code = static_cast<int>(response.error_code());
+    LOG(WARNING) << "Failed to retrieve an Instance Identity token.\n"
+                 << "  Error code: " << error_code << "\n"
+                 << "  Message: " << response.error_message() << "\n"
+                 << "  Body: " << response.response_body();
   }
 
   auto callbacks = std::move(queued_callbacks_);
