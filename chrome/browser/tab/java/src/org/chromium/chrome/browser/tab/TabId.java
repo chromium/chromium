751f38ade9b820255f2f4f23950b9c01dcc9c711
Sky Malice <skym@chromium.org>
2025-03-03 10:40:19 -0800

    Create @TabId and annotate Tab.java
    Bug: 399900870
    Change-Id: Ice24bbbad47e7bb4f412a6bb50fb78b3918ce5f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313679
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427222}

---
+++ chrome/browser/tab/java/src/org/chromium/chrome/browser/tab/TabId.java
@@ -0,0 +1,16 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tab;
+
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/** Annotation to mark a tab id. */
+@Retention(SOURCE)
+@Target(TYPE_USE)
+public @interface TabId {}
