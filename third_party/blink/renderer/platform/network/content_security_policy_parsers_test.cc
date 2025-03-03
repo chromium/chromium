88bb629b4402d71c539b7feebabe6cacfa13c83f
Tom Sepez <tsepez@chromium.org>
2025-03-03 11:05:52 -0800

    Improve tests before refactoring MatchesTheSerializedCSPGrammar()
    Generate a few additional test cases.
    
    Change-Id: Ie893cf2fe9ca4ab57cbf5a788d49bbf6aac2a5c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6313397
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427266}

--- third_party/blink/renderer/platform/network/content_security_policy_parsers_test.cc
+++ third_party/blink/renderer/platform/network/content_security_policy_parsers_test.cc
@@ -13,12 +13,25 @@ TEST(ContentSecurityPolicyParsers, MatchesTheSerializedCSPGrammar) {
     String value;
     bool expected;
   } testCases[]{
+      {"", true},                    // Empty string
+      {" \t\n\r  ", true},           // Only whitespace
+      {";", true},                   // Only semicolon
+      {";;;", true},                 // Multiple semicolons
+      {" ; \t\n\r ; ", true},        // Semicolons and whitespace
+      {"default-src 'self'", true},  // Single directive
+      {"script-src data:;", true},   // Trailing semicolon
+      {"default-src 'self' 'unsafe-inline'", true},  // Multiple values
+      {"\t\n\r   default-src 'self'", true},         // Leading whitespace
+      {"default-src \t\n\r   'self'", true},         // Inner whitespace
       {"script-src 'none'; invalid-directive ", true},
       {"script-src 'none'; invalid-directive;", true},
       {" script-src 'none' https://www.example.org   ; ;invalid-directive;  ;",
        true},
-      {"script-src 'none', media-src 'none'", false},
-      {"script-src 'none'; /invalid-directive-name", false},
+      {" 'self'", false},                        // Missing directive.
+      {"default+src 'self'", false},             // Invalid directive character
+      {"default-src 'self\xffinvalid;", false},  // Invalid value character
+      {"script-src 'none', media-src 'none'", false},         // Comma separated
+      {"script-src 'none'; /invalid-directive-name", false},  // Invalid second
   };
 
   for (const auto& testCase : testCases) {
