23501a42a8d2654155f3b5dec4a4fb12d933a654
Lei Zhang <thestig@chromium.org>
2025-03-04 09:18:21 -0800

    Reland "Clean up some of Task Manager's Task code"
    This is a reland of commit 215544f8ed8a6745d34e7eb270320be9b13c5174
    
    This time, the CL avoids creating MSAN errors.
    
    Original change's description:
    > Clean up some of Task Manager's Task code
    >
    > - Consolidate identical UpdateFavicon() impls into
    >   RendererTask::DefaultUpdateFaviconImpl().
    > - Modernize code and fix nits.
    >
    > Change-Id: Idf80d352d02bb83397146aa08644242b4eee30f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6311034
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1427370}
    
    Change-Id: I0728490cf05a594ff770e28ab949d5cc26880d28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6321131
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427783}

--- chrome/browser/task_manager/providers/web_contents/isolated_web_app_task.cc
+++ chrome/browser/task_manager/providers/web_contents/isolated_web_app_task.cc
@@ -70,9 +70,7 @@ void IsolatedWebAppTask::UpdateTitle() {
 }
 
 void IsolatedWebAppTask::UpdateFavicon() {
-  const gfx::ImageSkia* icon =
-      RendererTask::GetFaviconFromWebContents(web_contents());
-  set_icon(icon ? *icon : gfx::ImageSkia());
+  DefaultUpdateFaviconImpl();
 }
 
 }  // namespace task_manager
