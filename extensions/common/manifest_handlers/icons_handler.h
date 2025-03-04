618b69aad7921d3880c05eeb8140e16dcf483ff3
Solomon Kinard <solomonkinard@chromium.org>
2025-03-04 13:25:29 -0800

    Extensions: IconVariants: Don't use default arguments
    The first link happened to be found by accident while browsing the
    codebase for a completely unrelated reason. That's why default arguments
    were avoided in the original CL. This new CL aims to ameliorate that.
    
    > please avoid default parameters in functions
    
    https://crrev.com/c/6215901/comments/12b3849e_dbd39092
    
    > prefer overloaded functions
    
    https://crrev.com/c/6302631/comment/3b567a54_24d61f5d
    
    Bug: chromium:344639840
    Change-Id: I0a0c0ed8ecd503f3485230ccfdda1d3a0a00cb5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6314453
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427952}

--- extensions/common/manifest_handlers/icons_handler.h
+++ extensions/common/manifest_handlers/icons_handler.h
@@ -10,6 +10,7 @@
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_resource.h"
 #include "extensions/common/icons/extension_icon_set.h"
+#include "extensions/common/icons/extension_icon_variant.h"
 #include "extensions/common/manifest_handler.h"
 #include "extensions/common/manifest_handlers/icon_variants_handler.h"
 
@@ -22,23 +23,37 @@ struct IconsInfo : public Extension::ManifestData {
   ExtensionIconSet icons;
 
   // Return the icon set for the given `extension`.
+  static const ExtensionIconSet& GetIcons(const Extension* extension) {
+    return GetIcons(extension, ExtensionIconVariant::ColorScheme::kLight);
+  }
   static const ExtensionIconSet& GetIcons(
       const Extension* extension,
-      std::optional<ExtensionIconVariant::ColorScheme> color_scheme =
-          std::nullopt);
+      std::optional<ExtensionIconVariant::ColorScheme> color_scheme);
 
-  // Get an extension icon as a resource or URL.
+  // Get an extension icon as a resource.
+  static ExtensionResource GetIconResource(const Extension* extension,
+                                           int size_in_px,
+                                           ExtensionIconSet::Match match_type) {
+    return GetIconResource(extension, size_in_px, match_type,
+                           ExtensionIconVariant::ColorScheme::kLight);
+  }
   static ExtensionResource GetIconResource(
       const Extension* extension,
       int size_in_px,
       ExtensionIconSet::Match match_type,
-      ExtensionIconVariant::ColorScheme color_scheme =
-          ExtensionIconVariant::ColorScheme::kLight);
+      ExtensionIconVariant::ColorScheme color_scheme);
+
+  // Get an extension icon as a URL.
+  static GURL GetIconURL(const Extension* extension,
+                         int size_in_px,
+                         ExtensionIconSet::Match match_type) {
+    return GetIconURL(extension, size_in_px, match_type,
+                      ExtensionIconVariant::ColorScheme::kLight);
+  }
   static GURL GetIconURL(const Extension* extension,
                          int size_in_px,
                          ExtensionIconSet::Match match_type,
-                         ExtensionIconVariant::ColorScheme color_scheme =
-                             ExtensionIconVariant::ColorScheme::kLight);
+                         ExtensionIconVariant::ColorScheme color_scheme);
 };
 
 // Parses the "icons" manifest key.
