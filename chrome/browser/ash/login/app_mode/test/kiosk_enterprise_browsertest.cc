861ace0db80ff20500313b4a60de5e67bd09e3a2
Edman Anjos <edmanp@google.com>
2025-03-04 02:48:49 -0800

    kiosk: Extract KioskIdentityTest from KioskEnterpriseTest
    Also migrates the extracted test to KioskMixin.
    
    Bug: 379246208
    Test: testing/xvfb.py tools/autotest.py -C out/Release chrome/browser/ash/app_mode/test/kiosk_identity_browsertest.cc chrome/browser/ash/login/app_mode/test/kiosk_enterprise_browsertest.cc
    Change-Id: I453ffba9f24a1241d3390b24ff7ebc3ea886f697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6321946
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1427638}

--- chrome/browser/ash/login/app_mode/test/kiosk_enterprise_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_enterprise_browsertest.cc
@@ -3,15 +3,11 @@
 // found in the LICENSE file.
 
 #include <string>
-#include <vector>
 
-#include "apps/test/app_window_waiter.h"
 #include "ash/public/cpp/login_accelerators.h"
-#include "base/run_loop.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_launch_error.h"
 #include "chrome/browser/ash/app_mode/kiosk_chrome_app_manager.h"
-#include "chrome/browser/ash/app_mode/test/kiosk_session_initialized_waiter.h"
 #include "chrome/browser/ash/login/app_mode/network_ui_controller.h"
 #include "chrome/browser/ash/login/app_mode/test/kiosk_base_test.h"
 #include "chrome/browser/ash/login/oobe_screen.h"
@@ -19,41 +15,18 @@
 #include "chrome/browser/ash/login/test/device_state_mixin.h"
 #include "chrome/browser/ash/login/test/js_checker.h"
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
-#include "chrome/browser/ash/policy/core/device_local_account.h"
-#include "chrome/browser/device_identity/device_oauth2_token_service.h"
-#include "chrome/browser/device_identity/device_oauth2_token_service_factory.h"
 #include "chrome/browser/lifetime/termination_notification.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/ash/login/login_display_host.h"
 #include "chrome/browser/ui/webui/ash/login/app_launch_splash_screen_handler.h"
 #include "chrome/browser/ui/webui/ash/login/error_screen_handler.h"
 #include "chromeos/ash/components/settings/cros_settings_names.h"
-#include "components/policy/core/common/device_local_account_type.h"
-#include "components/signin/public/base/consent_level.h"
-#include "components/signin/public/identity_manager/identity_manager.h"
 #include "content/public/test/browser_test.h"
-#include "content/public/test/browser_test_utils.h"
-#include "extensions/browser/app_window/app_window.h"
-#include "extensions/browser/app_window/app_window_registry.h"
-#include "google_apis/gaia/fake_gaia.h"
-#include "google_apis/gaia/gaia_constants.h"
-#include "google_apis/gaia/gaia_urls.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace ash {
 
 namespace {
 
-const char kTestEnterpriseServiceAccountEmail[] =
-    "service_account@system.gserviceaccount.com";
-const char kTestRefreshToken[] = "fake-refresh-token";
-const char kTestUserinfoToken[] = "fake-userinfo-token";
-const char kTestLoginToken[] = "fake-login-token";
-const char kTestAccessToken[] = "fake-access-token";
-const char kTestClientId[] = "fake-client-id";
-const char kTestAppScope[] = "https://www.googleapis.com/auth/userinfo.profile";
 const test::UIPath kErrorMessageContinueButton = {"error-message",
                                                   "continueButton"};
 
@@ -81,113 +54,10 @@ class KioskEnterpriseTest : public KioskBaseTest {
  protected:
   KioskEnterpriseTest() = default;
 
-  // KioskBaseTest:
-  void SetUpOnMainThread() override {
-    KioskBaseTest::SetUpOnMainThread();
-
-    // Configure OAuth authentication.
-    GaiaUrls* gaia_urls = GaiaUrls::GetInstance();
-
-    // This token satisfies the userinfo.email request from
-    // DeviceOAuth2TokenService used in token validation.
-    FakeGaia::AccessTokenInfo userinfo_token_info;
-    userinfo_token_info.token = kTestUserinfoToken;
-    userinfo_token_info.scopes.insert(
-        "https://www.googleapis.com/auth/userinfo.email");
-    userinfo_token_info.audience = gaia_urls->oauth2_chrome_client_id();
-    userinfo_token_info.email = kTestEnterpriseServiceAccountEmail;
-    fake_gaia_.fake_gaia()->IssueOAuthToken(kTestRefreshToken,
-                                            userinfo_token_info);
-
-    // The any-api access token for accessing the token minting endpoint.
-    FakeGaia::AccessTokenInfo login_token_info;
-    login_token_info.token = kTestLoginToken;
-    login_token_info.scopes.insert(GaiaConstants::kAnyApiOAuth2Scope);
-    login_token_info.audience = gaia_urls->oauth2_chrome_client_id();
-    fake_gaia_.fake_gaia()->IssueOAuthToken(kTestRefreshToken,
-                                            login_token_info);
-
-    // This is the access token requested by the app via the identity API.
-    FakeGaia::AccessTokenInfo access_token_info;
-    access_token_info.token = kTestAccessToken;
-    access_token_info.scopes.insert(kTestAppScope);
-    access_token_info.audience = kTestClientId;
-    access_token_info.email = kTestEnterpriseServiceAccountEmail;
-    fake_gaia_.fake_gaia()->IssueOAuthToken(kTestLoginToken, access_token_info);
-
-    DeviceOAuth2TokenService* token_service =
-        DeviceOAuth2TokenServiceFactory::Get();
-    token_service->SetAndSaveRefreshToken(
-        kTestRefreshToken, DeviceOAuth2TokenService::StatusCallback());
-    base::RunLoop().RunUntilIdle();
-  }
-
-  void ConfigureKioskAppInPolicy(const std::string& account_id,
-                                 const std::string& app_id,
-                                 const std::string& update_url) {
-    std::vector<policy::DeviceLocalAccount> accounts;
-    accounts.emplace_back(policy::DeviceLocalAccountType::kKioskApp,
-                          policy::DeviceLocalAccount::EphemeralMode::kUnset,
-                          account_id, app_id, update_url);
-    policy::SetDeviceLocalAccountsForTesting(owner_settings_service_.get(),
-                                             accounts);
-    settings_helper_.SetString(kAccountsPrefDeviceLocalAccountAutoLoginId,
-                               account_id);
-    settings_helper_.SetString(kServiceAccountIdentity,
-                               kTestEnterpriseServiceAccountEmail);
-  }
-
  private:
   DeviceStateMixin device_state_{
       &mixin_host_, DeviceStateMixin::State::OOBE_COMPLETED_CLOUD_ENROLLED};
 };
-
-IN_PROC_BROWSER_TEST_F(KioskEnterpriseTest, EnterpriseKioskApp) {
-  // Prepare Fake CWS to serve app crx.
-  SetTestApp(kTestEnterpriseKioskAppId);
-  SetupTestAppUpdateCheck();
-
-  // Configure `kTestEnterpriseKioskAppId` in device policy.
-  ConfigureKioskAppInPolicy(kTestEnterpriseAccountId, kTestEnterpriseKioskAppId,
-                            /*update_url=*/"");
-
-  PrepareAppLaunch();
-  EXPECT_TRUE(LaunchApp(kTestEnterpriseKioskAppId));
-
-  KioskSessionInitializedWaiter().Wait();
-
-  // Check installer status.
-  EXPECT_EQ(KioskAppLaunchError::Error::kNone, KioskAppLaunchError::Get());
-  EXPECT_EQ(ManifestLocation::kExternalPolicy, GetInstalledAppLocation());
-
-  // Wait for the window to appear.
-  extensions::AppWindow* window =
-      apps::AppWindowWaiter(extensions::AppWindowRegistry::Get(
-                                ProfileManager::GetPrimaryUserProfile()),
-                            kTestEnterpriseKioskAppId)
-          .Wait();
-  ASSERT_TRUE(window);
-  EXPECT_TRUE(content::WaitForLoadStop(window->web_contents()));
-
-  // Check whether the app can retrieve an OAuth2 access token.
-  EXPECT_EQ(kTestAccessToken,
-            content::EvalJs(window->web_contents(),
-                            "new Promise(resolve => {"
-                            "  chrome.identity.getAuthToken({ 'interactive': "
-                            "    false }, resolve);"
-                            "});"));
-
-  // Verify that the session is not considered to be logged in with a GAIA
-  // account.
-  Profile* app_profile = ProfileManager::GetPrimaryUserProfile();
-  ASSERT_TRUE(app_profile);
-  EXPECT_FALSE(IdentityManagerFactory::GetForProfile(app_profile)
-                   ->HasPrimaryAccount(signin::ConsentLevel::kSignin));
-
-  // Terminate the app.
-  window->GetBaseWindow()->Close();
-  base::RunLoop().RunUntilIdle();
-}
 
 IN_PROC_BROWSER_TEST_F(KioskEnterpriseTest,
                        HittingNetworkAcceleratorShouldShowNetworkScreen) {
