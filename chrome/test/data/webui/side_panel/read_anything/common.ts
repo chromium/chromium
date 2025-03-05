69f0d463d02d7d135000fd972c7e487ebeb58b38
Kristi Saney <kristislee@google.com>
2025-03-05 13:56:49 -0800

    [Read aloud] Use mock timer for waiting on spinner
    Bug: 397973923
    Change-Id: I325eea0159cc49c5dcc44fc9a49fc1431b39a044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331176
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1428567}

--- chrome/test/data/webui/side_panel/read_anything/common.ts
+++ chrome/test/data/webui/side_panel/read_anything/common.ts
@@ -5,7 +5,7 @@ import type {CrActionMenuElement} from '//resources/cr_elements/cr_action_menu/c
 import type {CrLazyRenderElement} from '//resources/cr_elements/cr_lazy_render/cr_lazy_render.js';
 import {flush} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import type {AppElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
-import {MetricsBrowserProxyImpl, playFromSelectionTimeout, spinnerDebounceTimeout} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {MetricsBrowserProxyImpl, playFromSelectionTimeout} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {MockTimer} from 'chrome-untrusted://webui-test/mock_timer.js';
 import {microtasksFinished} from 'chrome-untrusted://webui-test/test_util.js';
 
@@ -61,10 +61,6 @@ export function playFromSelectionWithMockTimer(app: AppElement): void {
   app.playSpeech();
   mockTimer.tick(playFromSelectionTimeout);
   mockTimer.uninstall();
-}
-
-export async function waitForSpinnerTimeout(): Promise<void> {
-  return new Promise(resolve => setTimeout(resolve, spinnerDebounceTimeout));
 }
 
 // Returns the list of items in the given dropdown menu
