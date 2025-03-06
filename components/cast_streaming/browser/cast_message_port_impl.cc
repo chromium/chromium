6902d4cab27b490a6ab6023cb342f9638604b3e7
Ho Cheung <hocheung@chromium.org>
2025-03-06 01:07:36 -0800

    [code health] Using ReadDict to simplify some JSON parsing
    Replaced the base::JSONReader::Read call with
    base::JSONReader::ReadDict for more concise and
    direct parsing of JSON dictionaries.
    
    This CL was uploaded by git cl split.
    
    Bug: 40912723
    Change-Id: Iad32d4eb906e1ff4abbea1f61cfe1e611080d07a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6309227
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428787}

--- components/cast_streaming/browser/cast_message_port_impl.cc
+++ components/cast_streaming/browser/cast_message_port_impl.cc
@@ -124,20 +124,14 @@ void CastMessagePortImpl::ResetClient() {
 
 void CastMessagePortImpl::SendInjectResponse(const std::string& sender_id,
                                              const std::string& message) {
-  std::optional<base::Value> value = base::JSONReader::Read(message);
+  std::optional<base::Value::Dict> value = base::JSONReader::ReadDict(message);
   if (!value) {
-    LOG(ERROR) << "Malformed message from sender " << sender_id
-               << ": not a json payload:" << message;
-    return;
-  }
-
-  if (!value->is_dict()) {
     LOG(ERROR) << "Malformed message from sender " << sender_id
                << ": non-dictionary json payload: " << message;
     return;
   }
 
-  const std::string* type = value->GetDict().FindString(kKeyType);
+  const std::string* type = value->FindString(kKeyType);
   if (!type) {
     LOG(ERROR) << "Malformed message from sender " << sender_id
                << ": no message type: " << message;
@@ -149,7 +143,7 @@ void CastMessagePortImpl::SendInjectResponse(const std::string& sender_id,
     return;
   }
 
-  std::optional<int> request_id = value->GetDict().FindInt(kKeyRequestId);
+  std::optional<int> request_id = value->FindInt(kKeyRequestId);
   if (!request_id) {
     LOG(ERROR) << "Malformed message from sender " << sender_id
                << ": no request id: " << message;
@@ -170,20 +164,14 @@ void CastMessagePortImpl::SendInjectResponse(const std::string& sender_id,
 
 void CastMessagePortImpl::HandleMediaMessage(const std::string& sender_id,
                                              const std::string& message) {
-  std::optional<base::Value> value = base::JSONReader::Read(message);
+  std::optional<base::Value::Dict> value = base::JSONReader::ReadDict(message);
   if (!value) {
-    LOG(ERROR) << "Malformed message from sender " << sender_id
-               << ": not a json payload: " << message;
-    return;
-  }
-
-  if (!value->is_dict()) {
     LOG(ERROR) << "Malformed message from sender " << sender_id
                << ": non-dictionary json payload: " << message;
     return;
   }
 
-  const std::string* type = value->GetDict().FindString(kKeyType);
+  const std::string* type = value->FindString(kKeyType);
   if (!type) {
     LOG(ERROR) << "Malformed message from sender " << sender_id
                << ": no message type: " << message;
@@ -201,8 +189,8 @@ void CastMessagePortImpl::HandleMediaMessage(const std::string& sender_id,
     return;
   }
 
-  std::optional<int> request_id = value->GetDict().FindInt(kKeyRequestId);
-  if (!request_id.has_value()) {
+  std::optional<int> request_id = value->FindInt(kKeyRequestId);
+  if (!request_id) {
     LOG(ERROR) << "Malformed message from sender " << sender_id
                << ": no request id: " << message;
     return;
