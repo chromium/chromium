fffb377a1d69bc9764d0a050f1526835a85c3b95
Tom Sepez <tsepez@chromium.org>
2025-03-06 09:15:00 -0800

    Remove file-wide unsafe_buffers pragma from blink/ [21 of N]
    Suppress warnings on an expression by expression basis.
    
    -- Re-write one expression to avoid two-arg span constructor.
    
    Bug: 351564777
    Change-Id: I07cbfd1b7cbcf556df99cfa048bf44b8b5a38302
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331296
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428976}

--- third_party/blink/renderer/modules/peerconnection/media_stream_remote_video_source.cc
+++ third_party/blink/renderer/modules/peerconnection/media_stream_remote_video_source.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/modules/peerconnection/media_stream_remote_video_source.h"
 
 #include <stdint.h>
@@ -56,9 +51,7 @@ class WebRtcEncodedVideoFrame : public EncodedVideoFrame {
     }
   }
 
-  base::span<const uint8_t> Data() const override {
-    return base::span(buffer_->data(), buffer_->size());
-  }
+  base::span<const uint8_t> Data() const override { return *buffer_; }
 
   media::VideoCodec Codec() const override { return codec_; }
 
