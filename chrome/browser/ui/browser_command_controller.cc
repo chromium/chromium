6226bdb9b32d184bd7f4d8c6d9fee63fb1c89bcc
Alex Ilin <alexilin@chromium.org>
2025-03-06 06:39:51 -0800

    Revert "[GTK] Support Emacs bindings on GTK4"
    This reverts commit 50a0f5cfa8058e6fa304fcd87fdc4f2e18252f25.
    
    Reason for revert: hits a DCHECK when pressing any key https://crbug.com/400996809 
    
    Original change's description:
    > [GTK] Support Emacs bindings on GTK4
    >
    > This change replaces the old vector‐based API
    > GetTextEditCommandsForEvent with a function that returns a single
    > ui::TextEditCommand. INVALID_COMMAND is returned when no valid command
    > applies. The command mapping no longer relies on heap-allocated data
    > (vector and string), so can now be a compile time constant.
    >
    > The old code used a complex signal handling approach that simulated a
    > keypress in a fake window and widget to observe the resulting text edit
    > command. However, the only "key theme" GTK ever supported was emacs
    > bindings before key themes were removed in GTK4. This CL simplifies the
    > approach by directly mapping key events to edit commands. It also
    > enables emacs bindings on GTK4 and (eventually, when this code is moved
    > out of //ui/gtk since it no longer has any GTK dependencies) QT.
    >
    > Change-Id: If4856d33aa8ec45f2e397aa517032ddc22b63def
    > Bug: None
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6314265
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Reviewed-by: Peter Kasting <pkasting@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1428340}
    
    Bug: None
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6f01261c100d6cd390a7d4e03518abd10a4053a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6333611
    Commit-Queue: Osama Fathy <osamafathy@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1428887}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -128,7 +128,6 @@
 #endif
 
 #if BUILDFLAG(IS_LINUX)
-#include "ui/base/ime/text_edit_commands.h"
 #include "ui/base/ime/text_input_flags.h"
 #include "ui/linux/linux_ui.h"
 #endif
@@ -335,9 +334,8 @@ bool BrowserCommandController::IsReservedCommandOrKey(
   // it is not reserved.
   auto* linux_ui = ui::LinuxUi::instance();
   if (linux_ui && event.os_event &&
-      linux_ui->GetTextEditCommandForEvent(*event.os_event,
-                                           ui::TEXT_INPUT_FLAG_NONE) !=
-          ui::TextEditCommand::INVALID_COMMAND) {
+      linux_ui->GetTextEditCommandsForEvent(
+          *event.os_event, ui::TEXT_INPUT_FLAG_NONE, nullptr)) {
     return false;
   }
 #endif
