c7abf925474409db929205d59ccd66b33afd4487
Frank Tang <ftang@chromium.org>
2025-03-07 16:26:51 -0800

    Add additional unit test for translit rules used.
    Bug: 388821484
    Change-Id: Ic02c14cffc1adb731a4b62ebf3e995ef3bc3f987
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6331285
    Commit-Queue: Frank Tang <ftang@chromium.org>
    Reviewed-by: Jungshik Shin <jshin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1429812}

--- base/i18n/transliterator_unittest.cc
+++ base/i18n/transliterator_unittest.cc
@@ -37,4 +37,48 @@ TEST(TransliteratorTest, LowerLatinASCIICorrect) {
   EXPECT_EQ(result, u"internationalization");
 }
 
+// Used in components/autofill/core/browser/data_model/transliterator.cc
+TEST(TransliteratorTest, KatakanaHiraganaCorrect) {
+  std::unique_ptr<base::i18n::Transliterator> transliterator(
+      base::i18n::CreateTransliterator("Katakana-Hiragana"));
+  ASSERT_TRUE(transliterator);
+  std::u16string text(
+      u"アメリカとウクライナの高官が来週、会談する見通しとなりました。");
+  std::u16string result = transliterator->Transliterate(text);
+  EXPECT_EQ(result,
+            u"あめりかとうくらいなの高官が来週、会談する見通しとなりました。");
+}
+
+// Used in components/autofill/core/browser/data_model/transliterator.cc
+TEST(TransliteratorTest, HiraganaKatakanaCorrect) {
+  std::unique_ptr<base::i18n::Transliterator> transliterator(
+      base::i18n::CreateTransliterator("Hiragana-Katakana"));
+  ASSERT_TRUE(transliterator);
+  std::u16string text(
+      u"アメリカとウクライナの高官が来週、会談する見通しとなりました。");
+  std::u16string result = transliterator->Transliterate(text);
+  EXPECT_EQ(result,
+            u"アメリカトウクライナノ高官ガ来週、会談スル見通シトナリマシタ。");
+}
+
+// Used in components/autofill/core/browser/data_model/transliterator.cc
+TEST(TransliteratorTest, NFCCorrect) {
+  std::unique_ptr<base::i18n::Transliterator> transliterator(
+      base::i18n::CreateTransliterator("NFC"));
+  ASSERT_TRUE(transliterator);
+  std::u16string text(u"Ää Öö Üü éàçñ Ẁẃ ff fi ffl Dj dz ṡ ᴯ");
+  std::u16string result = transliterator->Transliterate(text);
+  EXPECT_EQ(result, u"Ää Öö Üü éàçñ Ẁẃ ff fi ffl Dj dz ṡ ᴯ");
+}
+
+// Used in components/autofill/core/browser/data_model/transliterator.cc
+TEST(TransliteratorTest, NFDCorrect) {
+  std::unique_ptr<base::i18n::Transliterator> transliterator(
+      base::i18n::CreateTransliterator("NFD"));
+  ASSERT_TRUE(transliterator);
+  std::u16string text(u"Ää Öö Üü éàçñ Ẁẃ ﬀ ﬁ ﬄ ǅǆ ẛ ᴯ");
+  std::u16string result = transliterator->Transliterate(text);
+  EXPECT_EQ(result, u"Ää Öö Üü éàçñ Ẁẃ ﬀ ﬁ ﬄ ǅǆ ẛ ᴯ");
+}
+
 }  // namespace base::i18n
