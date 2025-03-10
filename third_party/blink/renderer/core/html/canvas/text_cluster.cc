bcb833448e53916720b4670d9a2b7cddc4d06e49
Andres Ricardo Perez <andresrperez@chromium.org>
2025-03-10 08:31:16 -0700

    Rename TextCluster begin attribute to start
    This CL renames the begin attribute of TextCluster objects into start.
    This matches how the function arguments defining character ranges are
    named, therefore making it consistent.
    
    This new naming came to be from the discussion around the spec for the
    TextMetrics additions: https://github.com/whatwg/html/pull/11000
    
    Bug: 341213359
    Change-Id: I55fa6a40d624b29aca56a6af3703efbdab39a541
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330469
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Andres Ricardo Perez <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430259}

--- third_party/blink/renderer/core/html/canvas/text_cluster.cc
+++ third_party/blink/renderer/core/html/canvas/text_cluster.cc
@@ -10,7 +10,7 @@ namespace blink {
 TextCluster::TextCluster(const String& text,
                          double x,
                          double y,
-                         unsigned begin,
+                         unsigned start,
                          unsigned end,
                          V8CanvasTextAlign align,
                          V8CanvasTextBaseline baseline,
@@ -18,7 +18,7 @@ TextCluster::TextCluster(const String& text,
     : text_(text),
       x_(x),
       y_(y),
-      begin_(begin),
+      start_(start),
       end_(end),
       align_(align),
       baseline_(baseline),
@@ -27,12 +27,12 @@ TextCluster::TextCluster(const String& text,
 TextCluster* TextCluster::Create(const String& text,
                                  double x,
                                  double y,
-                                 unsigned begin,
+                                 unsigned start,
                                  unsigned end,
                                  V8CanvasTextAlign align,
                                  V8CanvasTextBaseline baseline,
                                  TextMetrics& text_metrics) {
-  return MakeGarbageCollected<TextCluster>(text, x, y, begin, end, align,
+  return MakeGarbageCollected<TextCluster>(text, x, y, start, end, align,
                                            baseline, text_metrics);
 }
 
@@ -47,7 +47,7 @@ void TextCluster::OffsetPosition(double x_offset, double y_offset) {
 }
 
 void TextCluster::OffsetCharacters(unsigned offset) {
-  begin_ += offset;
+  start_ += offset;
   end_ += offset;
 }
 }  // namespace blink
--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -882,7 +882,7 @@ void BaseRenderingContext2D::fillTextCluster(
   }
   DrawTextInternal(text_cluster->text(), cluster_x + x, cluster_y + y,
                    CanvasRenderingContext2DState::kFillPaintType, cluster_align,
-                   cluster_baseline, text_cluster->begin(), text_cluster->end(),
+                   cluster_baseline, text_cluster->start(), text_cluster->end(),
                    nullptr, text_cluster->textMetrics()->GetFont());
 }
 
@@ -937,7 +937,7 @@ void BaseRenderingContext2D::strokeTextCluster(
   }
   DrawTextInternal(text_cluster->text(), cluster_x + x, cluster_y + y,
                    CanvasRenderingContext2DState::kStrokePaintType,
-                   cluster_align, cluster_baseline, text_cluster->begin(),
+                   cluster_align, cluster_baseline, text_cluster->start(),
                    text_cluster->end(), nullptr,
                    text_cluster->textMetrics()->GetFont());
 }
