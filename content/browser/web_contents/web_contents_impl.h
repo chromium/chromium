8a1493b87e56f577d4d0712902ee0077929dd92d
kenoss <kenoss@chromium.org>
2025-03-10 01:16:06 -0700

    DsePreload2: Plumb NVS hint on WebContents::StartPrefetch()
    This is a preparation of https://crrev.com/c/6239079.
    
    DefaultSearchEngine prefetch uses two URLs: The first is to prefetch,
    and the second is an user-initiated navigation or prerender. To handle
    the difference, we need NVS hint/header. Currently, NVS hint is only
    used for //content trigger. This CL admits to use NVS hint to embedder
    triggers, including DefaultSearchEngine prefetch.
    
    Bug: 394215571
    Change-Id: Ice949b23db34bdc91b42936c0008911d1bda9abb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6238417
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430123}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -997,6 +997,7 @@ class CONTENT_EXPORT WebContentsImpl
       bool use_prefetch_proxy,
       const blink::mojom::Referrer& referrer,
       const std::optional<url::Origin>& referring_origin,
+      std::optional<net::HttpNoVarySearchData> no_vary_search_hint,
       scoped_refptr<PreloadPipelineInfo> preload_pipeline_info,
       base::WeakPtr<PreloadingAttempt> attempt,
       std::optional<PreloadingHoldbackStatus> holdback_status_override)
