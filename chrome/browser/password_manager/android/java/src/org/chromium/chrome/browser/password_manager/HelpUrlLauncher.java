a46acccb7d64f01de26e097917affdf6ea38fcf4
Martin Kong <martinkong@google.com>
2025-03-10 12:15:36 -0700

    Add @NullMarked to some misc targets
    Bug: 389129271
    Change-Id: If96a6efe4d4b3555151fd1fde0eccef0ba0ce97a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6340047
    Auto-Submit: Martin Kong <martinkong@google.com>
    Owners-Override: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430399}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/HelpUrlLauncher.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/HelpUrlLauncher.java
@@ -12,6 +12,7 @@ import android.provider.Browser;
 import androidx.browser.customtabs.CustomTabsIntent;
 
 import org.chromium.base.IntentUtils;
+import org.chromium.build.annotations.NullMarked;
 
 /**
  * Used by the various password access loss warning surfaces to show p-link based help center
@@ -20,6 +21,7 @@ import org.chromium.base.IntentUtils;
  * <p>TODO(crbug.com/365755043): Replace this with HelpAndFeedbackLauncher once it starts supporting
  * p-links.
  */
+@NullMarked
 public class HelpUrlLauncher {
     public static final String KEEP_APPS_AND_DEVICES_WORKING_WITH_GMS_CORE_SUPPORT_URL =
             "https://support.google.com/googleplay/?p=keep_apps_and_devices_working_with_gms";
