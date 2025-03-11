19f319772c3074995ed1f110286ce3f541fb1d2d
thefrog <thefrog@chromium.org>
2025-03-11 13:54:39 -0700

    Add WPT for multiple registrations in one response
    New test: multiple-registrations.https.html
    
    Bug: 353767385
    Change-Id: I47be79642c9758eae66e7fa27cc556aa09af581b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6344016
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431126}

--- third_party/blink/web_tests/external/wpt/device-bound-session-credentials/login.py
+++ third_party/blink/web_tests/external/wpt/device-bound-session-credentials/login.py
@@ -1,11 +1,24 @@
+import json
 import importlib
 session_manager = importlib.import_module('device-bound-session-credentials.session_manager')
 
 def main(request, response):
+    request_body_raw = request.body.decode('utf-8')
+    request_body = json.loads(request_body_raw) if len(request_body_raw) > 0 else None
+    num_sessions = request_body.get("numSessions") if request_body is not None else 1
+    use_single_header = request_body.get("useSingleHeader") if request_body is not None else True
+
     authorization_value = session_manager.find_for_request(request).get_authorization_value()
     authorization_header = ""
     if authorization_value is not None:
         authorization_header = ';authorization="' + authorization_value + '"'
 
-    headers = [('Sec-Session-Registration', '(RS256);challenge="login_challenge_value";path="/device-bound-session-credentials/start_session.py"' + authorization_header)]
-    return (200, headers, "")
+    registrations = []
+    for i in range(num_sessions):
+        registrations.append(('Sec-Session-Registration', '(RS256);challenge="login_challenge_value";path="/device-bound-session-credentials/start_session.py"' + authorization_header))
+
+    if use_single_header:
+        combined_registrations = [("Sec-Session-Registration", ", ".join([registration[1] for registration in registrations]))]
+        return (200, combined_registrations, "")
+    else:
+        return (200, registrations, "")
