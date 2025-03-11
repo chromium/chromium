75722d463a846bdfe58a8133c2fd64bf40efe1c9
Zauri Meshveliani <zauri@google.com>
2025-03-11 02:56:20 -0700

    privacy-hub: Fix geolocation for secondary users for Browser
    The backend of Browser observes the active user's
    `kUserGeolocationAccessLevel` preference to fetch the system geolocation
    settings. This is an issue for the secondary users in the multi-user
    scenario as they should follow the primary user's location choice. Note
    that Secondary users are unable to update the location setting from the
    chrome://os-settings, this setting is read-only for them.
    
    Update the `SystemGeolocationSource` and
    `privacy_hub_util::ContentBlockObservationImpl` (used by the Browser
    parts) to base the logic on the primary user's pref value.
    
    Bug: 391241273
    Change-Id: I3176a3c8ed47815ebb8decbfed83b8069d0cb2fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6298017
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Auto-Submit: Zauri Meshveliani <zauri@chromium.org>
    Reviewed-by: Jan Láník <janlanik@google.com>
    Commit-Queue: Zauri Meshveliani <zauri@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430722}

--- chrome/browser/ash/geolocation/system_geolocation_source_browsertest.cc
+++ chrome/browser/ash/geolocation/system_geolocation_source_browsertest.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ash/geolocation/system_geolocation_source.h"
+
 #include <utility>
 
 #include "ash/constants/ash_pref_names.h"
@@ -13,14 +15,18 @@
 #include "base/test/bind.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
-#include "chrome/browser/ash/geolocation/system_geolocation_source.h"
+#include "chrome/browser/ash/login/login_manager_test.h"
+#include "chrome/browser/ash/login/test/login_manager_mixin.h"
+#include "chrome/browser/ui/ash/login/user_adding_screen.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/test/browser_test.h"
 #include "services/device/public/cpp/geolocation/geolocation_system_permission_manager.h"
 #include "services/device/public/cpp/geolocation/location_system_permission_status.h"
+#include "testing/gmock/include/gmock/gmock.h"
 
 namespace {
+
 class Observer
     : public device::GeolocationSystemPermissionManager::PermissionObserver {
  public:
@@ -31,47 +37,76 @@ class Observer
   }
   base::test::TestFuture<device::LocationSystemPermissionStatus> status_;
 };
+
+class MockObserver
+    : public device::GeolocationSystemPermissionManager::PermissionObserver {
+ public:
+  MOCK_METHOD(void,
+              OnSystemPermissionUpdated,
+              (device::LocationSystemPermissionStatus status),
+              (override));
+};
+
 }  // namespace
 
 namespace ash {
 
-class SystemGeolocationSourceBrowserTests : public InProcessBrowserTest {
+class SystemGeolocationSourceTestBase {
  protected:
-  SystemGeolocationSourceBrowserTests() : InProcessBrowserTest() {}
-
-  // InProcessBrowserTest:
-  void SetUp() override { InProcessBrowserTest::SetUp(); }
-
-  void SetUserPref(GeolocationAccessLevel access_level) {
+  void SetActiveUserPref(GeolocationAccessLevel access_level) {
     Shell::Get()->session_controller()->GetActivePrefService()->SetInteger(
         prefs::kUserGeolocationAccessLevel, static_cast<int>(access_level));
   }
 
-  GeolocationAccessLevel GetUserPref() {
+  void SetPrimaryUserPref(GeolocationAccessLevel access_level) {
+    Shell::Get()->session_controller()->GetPrimaryUserPrefService()->SetInteger(
+        prefs::kUserGeolocationAccessLevel, static_cast<int>(access_level));
+  }
+
+  GeolocationAccessLevel GetActiveUserPref() {
     return static_cast<GeolocationAccessLevel>(
         Shell::Get()->session_controller()->GetActivePrefService()->GetInteger(
             prefs::kUserGeolocationAccessLevel));
   }
-
- protected:
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 class SystemGeolocationSourceTestsGeolocationOn
-    : public SystemGeolocationSourceBrowserTests {
+    : public SystemGeolocationSourceTestBase,
+      public InProcessBrowserTest {
  protected:
   SystemGeolocationSourceTestsGeolocationOn() {
     scoped_feature_list_.InitWithFeatures({features::kCrosPrivacyHub}, {});
   }
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 class SystemGeolocationSourceTestsGeolocationOff
-    : public SystemGeolocationSourceBrowserTests {
+    : public SystemGeolocationSourceTestBase,
+      public InProcessBrowserTest {
  protected:
   SystemGeolocationSourceTestsGeolocationOff() {
     // Disables the geolocation part of the PrivacyHub
     scoped_feature_list_.InitWithFeatures({}, {ash::features::kCrosPrivacyHub});
   }
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+class SystemGeolocationSourceTestMultiUser
+    : public SystemGeolocationSourceTestBase,
+      public LoginManagerTest {
+ public:
+  SystemGeolocationSourceTestMultiUser() {
+    scoped_feature_list_.InitWithFeatures({features::kCrosPrivacyHub}, {});
+    login_mixin_.AppendRegularUsers(2);
+    primary_user_ = login_mixin_.users()[0].account_id;
+    secondary_user_ = login_mixin_.users()[1].account_id;
+  }
+
+ protected:
+  base::test::ScopedFeatureList scoped_feature_list_;
+  LoginManagerMixin login_mixin_{&mixin_host_};
+  AccountId primary_user_;
+  AccountId secondary_user_;
 };
 
 IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestsGeolocationOn,
@@ -84,12 +119,12 @@ IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestsGeolocationOn,
   manager->AddObserver(&observer);
 
   // Initial value should be to allow.
-  EXPECT_EQ(GetUserPref(), ash::GeolocationAccessLevel::kAllowed);
+  EXPECT_EQ(GetActiveUserPref(), ash::GeolocationAccessLevel::kAllowed);
   EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
             manager->GetSystemPermission());
 
   // Change the pref value
-  SetUserPref(ash::GeolocationAccessLevel::kDisallowed);
+  SetActiveUserPref(ash::GeolocationAccessLevel::kDisallowed);
   // Check that the change in pref was registered.
   EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
             manager->GetSystemPermission());
@@ -97,7 +132,7 @@ IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestsGeolocationOn,
             observer.status_.Take());
 
   // Change the pref value
-  SetUserPref(ash::GeolocationAccessLevel::kAllowed);
+  SetActiveUserPref(ash::GeolocationAccessLevel::kAllowed);
   // Check that the change in pref was registered.
   EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
             manager->GetSystemPermission());
@@ -105,8 +140,111 @@ IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestsGeolocationOn,
             observer.status_.Take());
 
   // Change the pref value
-  SetUserPref(ash::GeolocationAccessLevel::kOnlyAllowedForSystem);
+  SetActiveUserPref(ash::GeolocationAccessLevel::kOnlyAllowedForSystem);
   // Check that the change in pref was registered.
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            manager->GetSystemPermission());
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            observer.status_.Take());
+
+  // Observer needs to be removed here because it is allocated on stack.
+  manager->RemoveObserver(&observer);
+}
+
+IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestMultiUser,
+                       SecondaryUserObservation) {
+  // Sign in with the primary user.
+  LoginUser(primary_user_);
+  device::GeolocationSystemPermissionManager* manager =
+      device::GeolocationSystemPermissionManager::GetInstance();
+  ASSERT_TRUE(manager);
+
+  Observer observer;
+  manager->AddObserver(&observer);
+
+  // Initial value should be to allow.
+  EXPECT_EQ(GetActiveUserPref(), ash::GeolocationAccessLevel::kAllowed);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
+            manager->GetSystemPermission());
+
+  // Set pref to `kDisallowed`.
+  SetActiveUserPref(ash::GeolocationAccessLevel::kDisallowed);
+  // Check that the change in primary user pref affected the system location
+  // state.
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            manager->GetSystemPermission());
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            observer.status_.Take());
+
+  // Add second user to the session and sign in.
+  ash::UserAddingScreen::Get()->Start();
+  AddUser(secondary_user_);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            manager->GetSystemPermission());
+
+  // This observer is used to test that the secondary user pref changes below
+  // will not trigger the system location updates.
+  MockObserver NoOpObserver;
+  manager->AddObserver(&NoOpObserver);
+  // `OnSystemPermissionUpdated` hook shall not not be executed.
+  EXPECT_CALL(NoOpObserver, OnSystemPermissionUpdated(testing::_)).Times(0);
+
+  // Update secondary user pref for location pref and check it has no effect.
+  // Location setting should still follow the the primary user preference.
+  SetActiveUserPref(GeolocationAccessLevel::kAllowed);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            manager->GetSystemPermission());
+  // Ditto.
+  SetActiveUserPref(GeolocationAccessLevel::kOnlyAllowedForSystem);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            manager->GetSystemPermission());
+  // Ditto.
+  SetActiveUserPref(GeolocationAccessLevel::kDisallowed);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            manager->GetSystemPermission());
+
+  // Observers need to be removed here because it is allocated on stack.
+  manager->RemoveObserver(&NoOpObserver);
+  manager->RemoveObserver(&observer);
+}
+
+IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestMultiUser,
+                       SecondaryUserObserversPrimaryPrefUpdate) {
+  // Sign in with the primary user.
+  LoginUser(primary_user_);
+  device::GeolocationSystemPermissionManager* manager =
+      device::GeolocationSystemPermissionManager::GetInstance();
+  ASSERT_TRUE(manager);
+
+  Observer observer;
+  manager->AddObserver(&observer);
+
+  // Initial value should be to allow.
+  EXPECT_EQ(GetActiveUserPref(), ash::GeolocationAccessLevel::kAllowed);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
+            manager->GetSystemPermission());
+
+  // Add second user to the session and sign in.
+  ash::UserAddingScreen::Get()->Start();
+  AddUser(secondary_user_);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
+            manager->GetSystemPermission());
+
+  // Update primary user's location preference while secondary user is active
+  // and check it affects the system permission.
+  SetPrimaryUserPref(ash::GeolocationAccessLevel::kDisallowed);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            manager->GetSystemPermission());
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
+            observer.status_.Take());
+
+  SetPrimaryUserPref(ash::GeolocationAccessLevel::kAllowed);
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
+            manager->GetSystemPermission());
+  EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
+            observer.status_.Take());
+
+  SetPrimaryUserPref(ash::GeolocationAccessLevel::kOnlyAllowedForSystem);
   EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
             manager->GetSystemPermission());
   EXPECT_EQ(device::LocationSystemPermissionStatus::kDenied,
@@ -126,12 +264,12 @@ IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestsGeolocationOff,
   manager->AddObserver(&observer);
 
   // Initial value should be to allow.
-  EXPECT_EQ(GetUserPref(), ash::GeolocationAccessLevel::kAllowed);
+  EXPECT_EQ(GetActiveUserPref(), ash::GeolocationAccessLevel::kAllowed);
   EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
             manager->GetSystemPermission());
 
   // Change the pref value
-  SetUserPref(ash::GeolocationAccessLevel::kDisallowed);
+  SetActiveUserPref(ash::GeolocationAccessLevel::kDisallowed);
   // Check that the permission is not changed.
   EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
             manager->GetSystemPermission());
@@ -139,7 +277,7 @@ IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestsGeolocationOff,
             observer.status_.Take());
 
   // Change the pref value
-  SetUserPref(ash::GeolocationAccessLevel::kAllowed);
+  SetActiveUserPref(ash::GeolocationAccessLevel::kAllowed);
   // Check that the change in pref was registered.
   EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
             manager->GetSystemPermission());
@@ -147,7 +285,7 @@ IN_PROC_BROWSER_TEST_F(SystemGeolocationSourceTestsGeolocationOff,
             observer.status_.Take());
 
   // Change the pref value
-  SetUserPref(ash::GeolocationAccessLevel::kOnlyAllowedForSystem);
+  SetActiveUserPref(ash::GeolocationAccessLevel::kOnlyAllowedForSystem);
   // Check that the change in pref was registered.
   EXPECT_EQ(device::LocationSystemPermissionStatus::kAllowed,
             manager->GetSystemPermission());
