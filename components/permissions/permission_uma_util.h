c94cf40d17fef6070a3028194c0cfafe5ecc6339
Judith Hemp <hempjudith@google.com>
2025-03-11 08:56:30 -0700

    Add linter message and add missing label to the enum.xml
    The enum.xml file and the original enum class have diverged. This commit
    fixes this and adds linter messages and comments to prevent this from
    happening in the future.
    
    NO_IFTTT=initial setup
    
    Bug: 402105987
    Change-Id: I8153199f79086030b0cfb3cfd65df8c0de2ddd05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6343090
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Judith Hemp <hempjudith@google.com>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1430900}

--- components/permissions/permission_uma_util.h
+++ components/permissions/permission_uma_util.h
@@ -442,9 +442,13 @@ enum class AutoDSEPermissionRevertTransition {
   kMaxValue = INVALID_END_STATE,
 };
 
+// LINT.IfChange(PermissionPredictionSource)
+
 // This enum backs up the 'PermissionPredictionSource` histogram enum. It
 // indicates whether the permission prediction was done by the local on device
-// model or by the server side model.
+// model or by the server side model (or both).
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
 enum class PermissionPredictionSource {
   ON_DEVICE_TFLITE = 0,
   SERVER_SIDE = 1,
@@ -453,6 +457,8 @@ enum class PermissionPredictionSource {
   // Always keep at the end.
   kMaxValue = ONDEVICE_AI_AND_SERVER_SIDE,
 };
+
+// LINT.ThenChange(//tools/metrics/histograms/metadata/permissions/enums.xml:PermissionPredictionSource)
 
 // This enum backs up the 'PageInfoDialogAccessType' histogram enum.
 // It is used for collecting page info access type metrics in the context of
