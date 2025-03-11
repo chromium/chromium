56efa10de501b84de898ce87ef015dfc5f86d096
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-03-11 14:58:48 -0700

    Revert "coral: add suppression context to the desk template"
    This reverts commit f329a60d4286be4a1ee04a7dfd20bb35e397db69.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8720673385693254977
    
    Sample failed build: https://ci.chromium.org/b/8720673385693254977
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6330123&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8720673385693254977&type=BUG
    
    Original change's description:
    > coral: add suppression context to the desk template
    >
    > To write suppression context in the desk template, we add a data field
    > to save the tab and app entities of the group. The reasons we should
    > save all entities instead of getting tab and app info from the restore
    > data are:
    >
    > 1. the app IDs used by Coral and restore data are different.
    > 2. the web apps' titles used by Coral and restore data are different.
    >
    > Due to those reasons, directly saving the tab and app entities in
    > desk template when it is a Coral type template will be easier.
    >
    > Test: unit test + manual test
    > Bug: b:385040217
    > Change-Id: Idcdc85cef8ecc7259b619faf0782d5538f2aa61e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330123
    > Reviewed-by: April Zhou <aprilzhou@google.com>
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1430946}
    >
    
    Bug: b:385040217
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ie06bbc2d0752d166dbc21e01879ff30cb5c03b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6345376
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1431174}

--- ash/birch/birch_coral_provider.h
+++ ash/birch/birch_coral_provider.h
@@ -119,7 +119,6 @@ class ASH_EXPORT BirchCoralProvider : public BirchDataProvider,
  private:
   FRIEND_TEST_ALL_PREFIXES(CoralControllerTest, RestoreSuppressionContext);
   FRIEND_TEST_ALL_PREFIXES(CoralControllerTest, InSessionSuppressionContext);
-  FRIEND_TEST_ALL_PREFIXES(CoralSavedGroupTest, SaveSuppressionContext);
 
   // Gets if the system language is supported.
   bool GetLanguageAvailability();
