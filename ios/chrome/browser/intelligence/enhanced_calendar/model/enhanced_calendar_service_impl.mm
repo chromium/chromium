486786e6aa99226bd2ef7c7a0dbc33b241614b78
Nicolas MacBeth <nicolasmacbeth@google.com>
2025-03-11 10:41:03 -0700

    [iOS][Enhanced Cal] Moved page context work directly into Mojo service
    The mojo service now holds onto a webstate, and everything it needs to
    take care of populating the page context proto itself. This way, when
    its usage is simplified to creating the service, passing it the correct
    parameters, and calling `Execute...`. The mojo interface itself was
    modified to be passed the strict minimum in terms of necessary strings
    for the request to be created within the service itself. Organized in
    a `RequestParams` struct.
    
    Bug: 390621435
    Change-Id: I15b874ac0ec8fbbb30feaee7c9a001f8ac7a98eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6340887
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1430996}

--- ios/chrome/browser/intelligence/enhanced_calendar/model/enhanced_calendar_service_impl.mm
+++ ios/chrome/browser/intelligence/enhanced_calendar/model/enhanced_calendar_service_impl.mm
@@ -8,15 +8,18 @@
 #import <utility>
 
 #import "base/functional/bind.h"
+#import "base/strings/sys_string_conversions.h"
 #import "base/time/time.h"
 #import "components/optimization_guide/core/model_execution/feature_keys.h"
 #import "components/optimization_guide/core/model_quality/model_quality_log_entry.h"
 #import "components/optimization_guide/core/optimization_guide_model_executor.h"
 #import "components/optimization_guide/core/optimization_guide_util.h"
 #import "components/optimization_guide/proto/features/enhanced_calendar.pb.h"
+#import "ios/chrome/browser/intelligence/proto_wrappers/page_context_wrapper.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#import "ios/web/public/web_state.h"
 #import "mojo/public/cpp/base/proto_wrapper.h"
 
 namespace ai {
@@ -29,28 +32,72 @@ const base::TimeDelta kEnhancedCalendarRequestTimeout = base::Seconds(15);
 // the `OptimizationGuideService`.
 EnhancedCalendarServiceImpl::EnhancedCalendarServiceImpl(
     mojo::PendingReceiver<mojom::EnhancedCalendarService> receiver,
-    web::BrowserState* browser_state)
+    web::WebState* web_state)
     : service_(*OptimizationGuideServiceFactory::GetForProfile(
-          ProfileIOS::FromBrowserState(browser_state))),
-      receiver_(this, std::move(receiver)) {}
+          ProfileIOS::FromBrowserState(web_state->GetBrowserState()))),
+      receiver_(this, std::move(receiver)) {
+  web_state_ = web_state->GetWeakPtr();
+}
 
 EnhancedCalendarServiceImpl::~EnhancedCalendarServiceImpl() = default;
 
 void EnhancedCalendarServiceImpl::ExecuteEnhancedCalendarRequest(
-    ::mojo_base::ProtoWrapper request,
+    mojom::EnhancedCalendarServiceRequestParamsPtr request_params,
     ExecuteEnhancedCalendarRequestCallback request_callback) {
-  optimization_guide::proto::EnhancedCalendarRequest request_proto =
-      request.As<optimization_guide::proto::EnhancedCalendarRequest>().value();
+  if (!web_state_) {
+    mojom::EnhancedCalendarResponseResultPtr result_union =
+        mojom::EnhancedCalendarResponseResult::NewError(
+            "WebState destroyed before executing request");
+    std::move(request_callback).Run(std::move(result_union));
+    return;
+  }
+
+  // Create the request, and set the selected and surrounding texts on it.
+  optimization_guide::proto::EnhancedCalendarRequest request;
+  request.set_selected_text(request_params->selected_text);
+  request.set_surrounding_text(request_params->surrounding_text);
+
+  // Set the prompt, if it exists.
+  if (request_params->optional_prompt.has_value() &&
+      !request_params->optional_prompt.value().empty()) {
+    request.set_prompt(request_params->optional_prompt.value());
+  }
+
+  // Callback to execute when the PageContext proto is done being generated.
+  auto page_context_completion_callback =
+      base::BindOnce(&EnhancedCalendarServiceImpl::OnPageContextGenerated,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(request),
+                     std::move(request_callback));
+
+  // Populate the PageContext proto and then execute the query.
+  page_context_wrapper_ = [[PageContextWrapper alloc]
+        initWithWebState:web_state_.get()
+      completionCallback:std::move(page_context_completion_callback)];
+  [page_context_wrapper_ setShouldGetInnerText:YES];
+  [page_context_wrapper_ setShouldGetSnapshot:YES];
+  [page_context_wrapper_
+      setTextToHighlight:base::SysUTF8ToNSString(
+                             request_params->surrounding_text)];
+  [page_context_wrapper_ populatePageContextFieldsAsync];
+}
+
+#pragma mark - Private
+
+// Execute the Enhanced Calendar request with the generated Page Context.
+void EnhancedCalendarServiceImpl::OnPageContextGenerated(
+    optimization_guide::proto::EnhancedCalendarRequest request,
+    ExecuteEnhancedCalendarRequestCallback request_callback,
+    std::unique_ptr<optimization_guide::proto::PageContext> page_context) {
+  page_context_wrapper_ = nil;
+  request.set_allocated_page_context(page_context.release());
 
   service_->ExecuteModel(
-      optimization_guide::ModelBasedCapabilityKey::kEnhancedCalendar,
-      request_proto, kEnhancedCalendarRequestTimeout,
+      optimization_guide::ModelBasedCapabilityKey::kEnhancedCalendar, request,
+      kEnhancedCalendarRequestTimeout,
       base::BindOnce(&EnhancedCalendarServiceImpl::OnEnhancedCalendarResponse,
                      weak_ptr_factory_.GetWeakPtr(),
                      std::move(request_callback)));
 }
-
-#pragma mark - Private
 
 void EnhancedCalendarServiceImpl::OnEnhancedCalendarResponse(
     ExecuteEnhancedCalendarRequestCallback request_callback,
