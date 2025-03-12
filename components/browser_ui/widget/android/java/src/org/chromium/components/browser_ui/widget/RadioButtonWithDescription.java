88777abe701470c95f67a1ee8aed61c88d3e83a0
Yifan Luo <lyf@chromium.org>
2025-03-12 03:16:09 -0700

    Revert "[Gardener] Revert "[Site Settings] Add icon to radio buttons on Android.""
    This reverts commit 34e62808fe4fec8c60a9650cef1f95a1e9b95b45.
    
    Reason for revert: Decided to disable test other than revert cls.
    
    Original change's description:
    > [Gardener] Revert "[Site Settings] Add icon to radio buttons on Android."
    >
    > This reverts commit b97841327f9d4cebf668c87bc6aacf1faf7d4c2c.
    >
    > Reason for revert: crrev.com/c/6335832 is causing issues with
    > testAllowGeolocation test. This CL blocks reverting it.
    > See crbug.com/402655848 for details.
    >
    > Original change's description:
    > > [Site Settings] Add icon to radio buttons on Android.
    > >
    > > Add allow/block icons to each radio button for permission site
    > > setting's radio button groups on Android.
    > >
    > > UI design: go/site-settings-ui-design
    > >
    > > Binary-Size: Size increase is unavoidable. (Introduced a bench of new icons for site settings)
    > > Bug: 41494473
    > > Change-Id: I25e7f99133364769ab67e7bb57d5e65e73609e8b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6317567
    > > Commit-Queue: Yifan Luo <lyf@chromium.org>
    > > Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1430902}
    >
    > Bug: 41494473, 402655848
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I4812468ea2bbf4f43e8eb4ec5ea182008c563638
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6347196
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Ivana Žužić <izuzic@google.com>
    > Reviewed-by: Ivana Žužić <izuzic@google.com>
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1431385}
    
    Bug: 41494473, 402655848
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I95cdcd8d463c62fe75effe43f4c7b7f429faa159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6346589
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yifan Luo <lyf@chromium.org>
    Owners-Override: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431399}

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonWithDescription.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/RadioButtonWithDescription.java
@@ -179,7 +179,15 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
         return (ChromeImageView) findViewById(R.id.icon);
     }
 
-    /** @return TextView displayed as primary inside this {@link RadioButtonWithDescription}. */
+    /** Set the icon in the front of the primary texts {@link RadioButtonWithDescription}. */
+    public void setIcon(int resId) {
+        mIcon.setImageResource(resId);
+        mIcon.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * @return TextView displayed as primary inside this {@link RadioButtonWithDescription}.
+     */
     protected TextView getPrimaryTextView() {
         return (TextView) findViewById(R.id.primary);
     }
