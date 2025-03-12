b2a5cbc8e989fe129fc7ffda8e104f8e8da610d2
Tommy Steimel <steimel@chromium.org>
2025-03-12 07:47:06 -0700

    [Audio Ducker] Add OS-level ducking for Windows
    This CL adds the AudioDuckerWin, which (as part of the AudioDucker) is
    responsible for ducking the audio from other applications using
    OS-level APIs.
    
    Bug: 380124573
    Change-Id: I15b76f1d41f53413840f1b0da6e69629f6e2fcc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6292642
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431500}

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -171,6 +171,11 @@ namespace media {
 MEDIA_EXPORT BASE_DECLARE_FEATURE(kAudioDucking);
 MEDIA_EXPORT extern const base::FeatureParam<int> kAudioDuckingAttenuation;
 #endif  // !BUILDFLAG(IS_ANDROID)
+
+#if BUILDFLAG(IS_WIN)
+MEDIA_EXPORT BASE_DECLARE_FEATURE(kAudioDuckingWin);
+#endif  // BUILDFLAG(IS_WIN)
+
 MEDIA_EXPORT BASE_DECLARE_FEATURE(kAudioFocusDuckFlash);
 MEDIA_EXPORT BASE_DECLARE_FEATURE(kAudioInputConfirmReadsViaShmem);
 MEDIA_EXPORT BASE_DECLARE_FEATURE(kAutoPictureInPictureForVideoPlayback);
