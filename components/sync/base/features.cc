87906331c4f90329223f75f2401c6d1034ba41b9
Ankush Singh <ankushkush@google.com>
2025-03-12 15:56:26 -0700

    [themes] Enable MoveThemePrefsToSpecifics by default
    Metrics analysis: https://g-issues.chromium.org/issues/356148174#comment46
    
    Bug: 356148174
    Change-Id: I242dc7db9518b48c78baff6635a5fdb8b58b97ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6239204
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1431801}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -140,7 +140,7 @@ BASE_FEATURE(kSyncIncreaseNudgeDelayForSingleClient,
 
 BASE_FEATURE(kMoveThemePrefsToSpecifics,
              "MoveThemePrefsToSpecifics",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kWebApkBackupAndRestoreBackend,
