31f80f46998074b81cc8ed5dae1ad771cf90cb38
Elmehdi Rahmaoui <erahmaoui@google.com>
2025-03-12 12:31:17 -0700

    [iOS] Share extension - Image sharing UI
    This CL adds the image sharing UI for the new share extension.
    
    Bug: 396326568
    Change-Id: I905e7d9b6e4bb78fd8a4057d2073d16e8de66390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6342671
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1431692}

--- ios/chrome/share_extension/extended_share_view_controller.mm
+++ ios/chrome/share_extension/extended_share_view_controller.mm
@@ -164,11 +164,15 @@ const CGFloat kShareSheetCornerRadius = 20;
 #pragma mark - Private methods
 
 - (void)displayShareSheet {
-  self.shareSheet.sharedURL = _shareURL;
-  self.shareSheet.sharedTitle = _shareTitle;
-  if (_preview) {
-    self.shareSheet.sharedURLPreview = _preview;
+  if (_shareURL) {
+    self.shareSheet.sharedURL = _shareURL;
+    self.shareSheet.sharedTitle = _shareTitle;
+  } else if (_shareImage) {
+    self.shareSheet.sharedImage = _shareImage;
+  } else if (_shareText) {
+    self.shareSheet.sharedText = _shareText;
   }
+
   __weak ExtendedShareViewController* weakSelf = self;
   dispatch_async(dispatch_get_main_queue(), ^{
     [weakSelf presentViewController:weakSelf.shareSheet
@@ -256,7 +260,20 @@ const CGFloat kShareSheetCornerRadius = 20;
 - (void)handleSharedImage:(id)idImage
                   forItem:(NSExtensionItem*)item
                 withError:(NSError*)error {
-  // TODO(crbug.com/396326079): Handle shared images.
+  if ([idImage isKindOfClass:[UIImage class]]) {
+    self.shareImage = base::apple::ObjCCast<UIImage>(idImage);
+  } else if ([idImage isKindOfClass:[NSURL class]]) {
+    self.shareImage = [UIImage
+        imageWithData:[NSData
+                          dataWithContentsOfURL:base::apple::ObjCCast<NSURL>(
+                                                    idImage)]];
+  }
+  self.shareItem = item;
+  if (self.shareImage) {
+    [self displayShareSheet];
+  } else {
+    [self displayErrorView];
+  }
 }
 
 - (void)handleText:(id)idText
