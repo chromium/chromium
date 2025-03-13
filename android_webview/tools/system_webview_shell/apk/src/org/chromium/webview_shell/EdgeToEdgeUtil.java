6e8589a8947bfc4fe10737839f89fe8e2c8de77c
Peter Birk Pakkenberg <pbirk@chromium.org>
2025-03-13 04:41:09 -0700

    Fix inset handling for fullscreen webviews
    Adjust the inset handling for the system webview shell so each activity
    reachable from the UI applies inset handling and enables edge-to-edge
    rendering.
    
    The inset handling does not consume the insets, which means that child
    views, in particular any WebView full-screen views, will also get the
    inset information, so proper insets can be applied.
    
          insets. Manually navigated to other activities to observe
          rendering respects insets.
    
    Fixed: 402426414
    Test: Loaded https://dpogue.ca/safe-area-inset-test/ to test full-screen
    Change-Id: I81604878804862bb78fd52366adea574227d81b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6349182
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432027}

---
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/EdgeToEdgeUtil.java
@@ -0,0 +1,32 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.webview_shell;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public final class EdgeToEdgeUtil {
+
+    private EdgeToEdgeUtil() {} // Class is not meant to be instantiated.
+
+    /**
+     * Enable edge-to-edge rendering and add padding to the content view to avoid drawing under the
+     * insets.
+     */
+    public static void setupEdgeToEdge(AppCompatActivity activity) {
+        EdgeToEdge.enable(activity);
+        ViewCompat.setOnApplyWindowInsetsListener(
+                activity.findViewById(android.R.id.content),
+                (v, windowInsets) -> {
+                    Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    // Apply the insets paddings to the view.
+                    v.setPadding(insets.left, insets.top, insets.right, insets.bottom);
+                    return windowInsets;
+                });
+    }
+}
