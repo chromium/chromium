973534a937a98f5e9c557c542c10666503ca647f
thefrog <thefrog@chromium.org>
2025-03-13 05:58:30 -0700

    Reland "Add WPT for resolving registration and refresh endpoints"
    This is a reland of commit 78d84fcea84d92a13777b5f2814d6e8baf5b094e
    
    The fix in this CL is in the Patchset 1 -> Patchset 2 diff. It adjusts
    the call to waitForCookie based on its new signature.
    
    Original change's description:
    > Add WPT for resolving registration and refresh endpoints
    >
    > New test: resolving-urls.https.html
    >
    > Bug: 353767385
    > Change-Id: Ic35e05da48f87664c9c15adb6e937856c16d11e0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6345171
    > Reviewed-by: Daniel Rubery <drubery@chromium.org>
    > Commit-Queue: thefrog <thefrog@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1431606}
    
    Bug: 353767385
    Change-Id: Ibc33fcf500d8c72174b5b750e34abd0c22068a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348159
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1432053}

--- third_party/blink/web_tests/external/wpt/device-bound-session-credentials/login.py
+++ third_party/blink/web_tests/external/wpt/device-bound-session-credentials/login.py
@@ -1,11 +1,20 @@
+import json
 import importlib
 session_manager = importlib.import_module('device-bound-session-credentials.session_manager')
 
 def main(request, response):
+    registration_url = "/device-bound-session-credentials/start_session.py"
+    request_body_raw = request.body.decode('utf-8')
+    if len(request_body_raw) > 0:
+        request_body = json.loads(request_body_raw)
+        maybe_registration_url = request_body.get("registrationUrl")
+        if maybe_registration_url is not None:
+            registration_url = maybe_registration_url
+
     authorization_value = session_manager.find_for_request(request).get_authorization_value()
     authorization_header = ""
     if authorization_value is not None:
         authorization_header = ';authorization="' + authorization_value + '"'
 
-    headers = [('Sec-Session-Registration', '(RS256);challenge="login_challenge_value";path="/device-bound-session-credentials/start_session.py"' + authorization_header)]
+    headers = [('Sec-Session-Registration', '(RS256);challenge="login_challenge_value";path="' + registration_url + '"' + authorization_header)]
     return (200, headers, "")
