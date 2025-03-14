cd90fd65adddb4ac09d6ab678289cb202dcf1e2b
James Lee <ljjlee@google.com>
2025-03-14 08:08:22 -0700

    Compile part of the identity extensions API on Android desktop
    Bug: 399628344
    Change-Id: Iae2a536c92c883a1c88ed590621cc6c2eb96a2b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348743
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: James Lee <ljjlee@google.com>
    Auto-Submit: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1432737}

---
+++ chrome/browser/extensions/api/identity/identity_unimplemented_functions_android.cc
@@ -0,0 +1,20 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/api/identity/identity_unimplemented_functions_android.h"
+
+#include "extensions/browser/extension_function.h"
+
+DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(
+    IdentityClearAllCachedAuthTokensFunction,
+    "identity.clearAllCachedAuthTokens")
+
+DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(IdentityGetAuthTokenFunction,
+                                        "identity.getAuthToken")
+
+DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(IdentityLaunchWebAuthFlowFunction,
+                                        "identity.launchWebAuthFlow")
+
+DEFINE_UNIMPLEMENTED_EXTENSION_FUNCTION(IdentityRemoveCachedAuthTokenFunction,
+                                        "identity.removeCachedAuthToken")
