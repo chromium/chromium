c7611079f28220b620f53364c0d1c4ba11539c93
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-03-17 11:02:52 -0700

    Set RFH token in clipboard only after writing other data
    See crbug.com/401379144 for an explanation of the fix and fix.
    
    (cherry picked from commit 1f19c23b4507a98c28fee554ccaf828e82f40f04)
    
    Bug: 401379144, 403574896
    Change-Id: I6df206c6b0e3e64bc6c8c654f0ecfe357f64ee7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6337392
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1432185}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6362123
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6998@{#1978}
    Cr-Branched-From: de9c6fafd8ae5c6ea0438764076ca7d04a0b165d-refs/heads/main@{#1415337}

--- content/browser/renderer_host/clipboard_host_impl.cc
+++ content/browser/renderer_host/clipboard_host_impl.cc
@@ -701,9 +701,7 @@ void ClipboardHostImpl::OnCopyHtmlAllowedResult(
   DCHECK_GT(pending_writes_, 0);
   --pending_writes_;
 
-  clipboard_writer_->SetDataSourceURL(
-      render_frame_host().GetMainFrame()->GetLastCommittedURL(),
-      render_frame_host().GetLastCommittedURL());
+  AddSourceDataToClipboardWriter();
 
   if (replacement_data) {
     clipboard_writer_->WriteText(std::move(*replacement_data));
@@ -722,9 +720,7 @@ void ClipboardHostImpl::OnCopyAllowedResult(
   DCHECK_GT(pending_writes_, 0);
   --pending_writes_;
 
-  clipboard_writer_->SetDataSourceURL(
-      render_frame_host().GetMainFrame()->GetLastCommittedURL(),
-      render_frame_host().GetLastCommittedURL());
+  AddSourceDataToClipboardWriter();
 
   // If `replacement_data` is empty, only one of these fields should be
   // non-empty depending on which "Write" method was called by the renderer.
@@ -798,6 +794,12 @@ ClipboardEndpoint ClipboardHostImpl::CreateClipboardEndpoint() {
 void ClipboardHostImpl::ResetClipboardWriter() {
   clipboard_writer_ = std::make_unique<ui::ScopedClipboardWriter>(
       ui::ClipboardBuffer::kCopyPaste, CreateDataEndpoint());
+}
+
+void ClipboardHostImpl::AddSourceDataToClipboardWriter() {
+  clipboard_writer_->SetDataSourceURL(
+      render_frame_host().GetMainFrame()->GetLastCommittedURL(),
+      render_frame_host().GetLastCommittedURL());
   clipboard_writer_->WritePickledData(
       render_frame_host().GetGlobalFrameToken().ToPickle(),
       SourceRFHTokenType());
