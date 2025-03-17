01e774a57b916e12aa0ad3ccf7f72bb850a4ff6f
Min Qin <qinmin@chromium.org>
2025-03-17 09:21:21 -0700

    [M135] Changing the timeout when joining a tab group
    The delay is around 2 sync updates, so increasing the time out
    to 15 seconds for safety. And removes the TriggerRefresh() call
    as that is called too early.
    
    (cherry picked from commit ca385ec023a0a4f82d74768fe72c55edfb2928d6)
    
    Bug: 400543226,403637938
    Change-Id: I1e76c9fc5a4b3a1dc4688e5aae0b1c5c40027368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6320133
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1427927}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6358330
    Cr-Commit-Position: refs/branch-heads/7049@{#846}
    Cr-Branched-From: 2dab7846d0951a552bdc4f350dad497f986e6fed-refs/heads/main@{#1427262}

--- components/collaboration/internal/collaboration_controller.cc
+++ components/collaboration/internal/collaboration_controller.cc
@@ -38,6 +38,9 @@ using GroupDataOrFailureOutcome =
 using StateId = CollaborationController::StateId;
 using Flow = CollaborationController::Flow;
 
+constexpr base::TimeDelta kTimeoutWaitingForDataSharingGroup =
+    base::Seconds(20);
+
 std::string GetStateIdString(StateId state) {
   switch (state) {
     case StateId::kPending:
@@ -570,7 +573,7 @@ class WaitingForSyncAndDataSharingGroup
         FROM_HERE,
         base::BindOnce(&WaitingForSyncAndDataSharingGroup::HandleError,
                        weak_ptr_factory_.GetWeakPtr()),
-        base::Seconds(10));
+        kTimeoutWaitingForDataSharingGroup);
     tab_group_sync_observer_.Observe(controller->tab_group_sync_service());
     data_sharing_observer_.Observe(controller->data_sharing_service());
   }
@@ -586,11 +589,6 @@ class WaitingForSyncAndDataSharingGroup
     bool tab_group_exists = IsTabGroupInSync(group_id);
     bool people_group_exists = IsPeopleGroupInDataSharing(group_id);
     CHECK(!tab_group_exists || !people_group_exists);
-    // Force update sync.
-    if (!tab_group_exists) {
-      controller->sync_service()->TriggerRefresh(
-          {syncer::SHARED_TAB_GROUP_DATA});
-    }
     // Force update data sharing service.
     if (!IsPeopleGroupInDataSharing(group_id)) {
       controller->data_sharing_service()->ReadGroupDeprecated(
