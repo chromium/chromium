8b456d4ac89597b106e12052e70f7c9f216c5872
Justin Cohen <justincohen@google.com>
2025-03-17 06:26:41 -0700

    ios: Wire up gwp-asan in post crash processing.
    Implement gwp_asan::UserStreamDataSource within ProcessIntermediateDump
    and introduce a chrome://inducebrowsercrashforrealz?uaf test. This
    test will trigger a use-after-free with gwp_asan enabled. If the UAF
    fails to trigger, the test will still force a crash for safety.
    
    Change-Id: I05ad9915603d5e100e7b2b5a745152c8e097bd4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6355974
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433481}

--- ios/chrome/app/chrome_exe_main.mm
+++ ios/chrome/app/chrome_exe_main.mm
@@ -10,6 +10,7 @@
 #import "base/strings/sys_string_conversions.h"
 #import "build/blink_buildflags.h"
 #import "components/component_updater/component_updater_paths.h"
+#import "components/crash/core/app/crashpad.h"
 #import "ios/chrome/app/startup/ios_chrome_main.h"
 #import "ios/chrome/app/startup/ios_enable_sandbox_dump_buildflags.h"
 #import "ios/chrome/app/tests_hook.h"
@@ -112,6 +113,9 @@ int ChromeMain(int argc, char* argv[]) {
   // don't have yet preferences. Later on it is stopped if the user opted out.
   // In any case reports are not sent if the user opted out.
   StartCrashController();
+
+  crashpad::SimpleAddressRangeBag ios_extra_ranges;
+  crash_reporter::SetIntermediateDumpExtraMemoryRanges(&ios_extra_ranges);
 
   // Always ignore SIGPIPE.  We check the return value of write().
   CHECK_NE(SIG_ERR, signal(SIGPIPE, SIG_IGN));
