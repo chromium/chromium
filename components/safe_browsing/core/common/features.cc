6609f7a20fd7d605ac8cc68932754ed813a22571
Seung Jae Lim <andysjlim@chromium.org>
2025-03-17 10:11:41 -0700

    Enable the study ClientSideDetectionAcceptHCAllowlist by default
    Bug: 344767732
    Change-Id: Icd9fb2784c286671de56854c073f9458e1a4989b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6355584
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433603}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -46,7 +46,7 @@ BASE_FEATURE(kAddWarningShownTSToClientSafeBrowsingReport,
 
 BASE_FEATURE(kClientSideDetectionAcceptHCAllowlist,
              "ClientSideDetectionAcceptHCAllowlist",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kClientSideDetectionBrandAndIntentForScamDetection,
              "ClientSideDetectionBrandAndIntentForScamDetection",
