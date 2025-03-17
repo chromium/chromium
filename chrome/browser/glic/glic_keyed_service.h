25910dc070f2c66b8d60249e72fd30cec27c3d04
Jason Chase <chasej@chromium.org>
2025-03-17 15:08:24 -0700

    Add coordinator to handle glic actions
    This CL provides a coordinator to execute actions from glic. This is
    only the basic class structure, not a working implementation.
    
    Bug: 402235832
    Change-Id: Ia2f4357173318863c2e39215cbf86ab46c968859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6348718
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Jason Chase <chasej@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433795}

--- chrome/browser/glic/glic_keyed_service.h
+++ chrome/browser/glic/glic_keyed_service.h
@@ -25,11 +25,11 @@ class IdentityManager;
 
 namespace glic {
 class AuthController;
+class GlicActorController;
 class GlicEnabling;
 class GlicFocusedTabManager;
 class GlicMetrics;
 class GlicProfileManager;
-class GlicWindowController;
 class GlicWindowController;
 class GlicScreenshotCapturer;
 struct FocusedTabData;
@@ -188,6 +188,7 @@ class GlicKeyedService : public KeyedService {
   GlicFocusedTabManager focused_tab_manager_;
   std::unique_ptr<GlicScreenshotCapturer> screenshot_capturer_;
   std::unique_ptr<AuthController> auth_controller_;
+  std::unique_ptr<GlicActorController> actor_controller_;
   // Unowned
   raw_ptr<GlicProfileManager> profile_manager_;
   base::OnceCallbackList<void()> web_client_created_callbacks_;
