f706f268a66b9522b711a52f57eac60bd2a905e1
Gyuyoung Kim <gyuyoung@igalia.com>
2025-03-18 08:03:36 -0700

    [iOS] Remove unused WebKit.h header include
    crw_web_view_content_view.h/mm files have included
    WebKit.h file even though they have not used any
    WK* types. Instead, this CL moves the header file
    and the dep to the web_view_creation_util source set
    because they have used WKWebView.
    
    Bug: 391914246
    Change-Id: Iec2a16e8160e73332083fb32f032b19c890b1539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356107
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434133}

--- ios/web/common/web_view_creation_util.mm
+++ ios/web/common/web_view_creation_util.mm
@@ -4,6 +4,8 @@
 
 #import "ios/web/common/web_view_creation_util.h"
 
+#import <WebKit/WebKit.h>
+
 #import "base/check.h"
 #import "ios/web/common/user_agent.h"
 #import "ios/web/web_state/ui/wk_web_view_configuration_provider.h"
