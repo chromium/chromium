406f1e22360c2222f633fec59cadf2f1dc2121c2
junweifu <junwei.fu@intel.com>
2025-03-17 23:56:23 -0700

    webnn: Experiment TFLite GPU delegate with OpenCL
    Add a flag to build the GPU delegate for ChromeOS and Linux.
    
    The latest ChromeOS image has the OpenCL library by default.
    
    Compile-Size: Size increase is unavoidable.
    Change-Id: Ic321915a235d2eb6689c9c6d67041f43ec73689b
    Bug: 397474802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6246337
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Junwei Fu <junwei.fu@intel.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1433985}

--- services/webnn/tflite/op_resolver.cc
+++ services/webnn/tflite/op_resolver.cc
@@ -14,6 +14,10 @@
 #include "third_party/tflite/src/tensorflow/lite/delegates/nnapi/nnapi_delegate.h"
 #endif
 
+#if BUILDFLAG(BUILD_TFLITE_WITH_OPENCL)
+#include "third_party/tflite/src/tensorflow/lite/delegates/gpu/delegate.h"
+#endif
+
 #if BUILDFLAG(BUILD_TFLITE_WITH_XNNPACK)
 #include "third_party/tflite/src/tensorflow/lite/tflite_with_xnnpack_optional.h"
 #endif
@@ -312,6 +316,15 @@ OpResolver::OpResolver(const mojom::CreateContextOptions& options,
                 });
           });
     }
+  }
+#endif
+
+#if BUILDFLAG(BUILD_TFLITE_WITH_OPENCL)
+  if (options.device == mojom::CreateContextOptions::Device::kGpu) {
+    delegate_creators_.push_back([](TfLiteContext* context) {
+      return std::unique_ptr<TfLiteDelegate, void (*)(TfLiteDelegate*)>(
+          TfLiteGpuDelegateV2Create(nullptr), TfLiteGpuDelegateV2Delete);
+    });
   }
 #endif
 
