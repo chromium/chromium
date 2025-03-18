d3c3b18953850d07fc17ff4551ef3a396a360366
Henrique Nakashima <hnakashima@chromium.org>
2025-03-18 14:04:46 -0700

    Android: Add @NullMarked to com/autofill/android:main_autofill_java
    Bug: 389129271
    Change-Id: I6849d7bab954025133f53bdfd029899dbc7d97b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6360608
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434415}

--- components/autofill/android/java/src/org/chromium/components/autofill/AutofillAddressUiComponent.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/AutofillAddressUiComponent.java
@@ -8,7 +8,10 @@ import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 import org.jni_zero.JniType;
 
+import org.chromium.build.annotations.NullMarked;
+
 /** Description of an address editor input field. */
+@NullMarked
 @JNINamespace("autofill")
 public class AutofillAddressUiComponent {
     /** The type of the field, e.g., {@code FieldType.NAME_FULL}. */
