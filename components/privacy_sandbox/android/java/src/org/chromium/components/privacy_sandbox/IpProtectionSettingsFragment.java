c6353a303efd22db1debbb4917b58457ca7c4c44
Henrique Nakashima <hnakashima@chromium.org>
2025-03-18 12:36:43 -0700

    Android: Add @NullMarked to //components/privacy_sandbox
    Bug: 389129271
    Change-Id: I8c6b005fdab983d01f18821e5ef4333daff4e44a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368425
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434346}

--- components/privacy_sandbox/android/java/src/org/chromium/components/privacy_sandbox/IpProtectionSettingsFragment.java
+++ components/privacy_sandbox/android/java/src/org/chromium/components/privacy_sandbox/IpProtectionSettingsFragment.java
@@ -4,6 +4,8 @@
 
 package org.chromium.components.privacy_sandbox;
 
+import static org.chromium.build.NullUtil.assumeNonNull;
+
 import android.os.Bundle;
 
 import androidx.annotation.VisibleForTesting;
@@ -11,6 +13,9 @@ import androidx.annotation.VisibleForTesting;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
+import org.chromium.build.annotations.Initializer;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
 import org.chromium.components.browser_ui.settings.TextMessagePreference;
@@ -18,6 +23,7 @@ import org.chromium.ui.text.ChromeClickableSpan;
 import org.chromium.ui.text.SpanApplier;
 
 /** Fragment to manage settings for ip protection. */
+@NullMarked
 public class IpProtectionSettingsFragment extends PrivacySandboxBaseFragment {
     // Must match key in ip_protection_preferences.xml.
     private static final String PREF_IP_PROTECTION_SWITCH = "ip_protection_switch";
@@ -36,7 +42,7 @@ public class IpProtectionSettingsFragment extends PrivacySandboxBaseFragment {
     private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         SettingsUtils.addPreferencesFromResource(this, R.xml.ip_protection_preferences);
         mPageTitle.set(getString(R.string.privacy_sandbox_ip_protection_title));
 
@@ -54,6 +60,7 @@ public class IpProtectionSettingsFragment extends PrivacySandboxBaseFragment {
      *
      * @param delegate {@link TrackingProtectionDelegate} to set.
      */
+    @Initializer
     public void setTrackingProtectionDelegate(TrackingProtectionDelegate delegate) {
         mDelegate = delegate;
     }
@@ -62,6 +69,7 @@ public class IpProtectionSettingsFragment extends PrivacySandboxBaseFragment {
         ChromeSwitchPreference ipProtectionSwitch = findPreference(PREF_IP_PROTECTION_SWITCH);
         TextMessagePreference ipProtectionSummary = findPreference(PREF_IP_PROTECTION_SUMMARY);
 
+        assumeNonNull(ipProtectionSwitch);
         ipProtectionSwitch.setChecked(mDelegate.isIpProtectionEnabled());
         ipProtectionSwitch.setOnPreferenceChangeListener(
                 (preference, newValue) -> {
@@ -71,6 +79,7 @@ public class IpProtectionSettingsFragment extends PrivacySandboxBaseFragment {
                     return true;
                 });
 
+        assumeNonNull(ipProtectionSummary);
         ipProtectionSummary.setSummary(
                 SpanApplier.applySpans(
                         getResources().getString(R.string.privacy_sandbox_ip_protection_summary),
