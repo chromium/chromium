f93858ed0302384c0a70f7d5933f2ca7def918bd
Clemens Backes <clemensb@chromium.org>
2025-03-18 08:44:35 -0700

    [v8][test] Enter isolate before requesting GC, part 2
    The issue fixed in https://crrev.com/c/6355125 existed in two more tests
    in the same file. The previous DCHECK did not flush those out, but
    https://crrev.com/c/6367194 will.
    
    R=orrb@google.com
    
    Bug: 396607238
    Change-Id: I7f001399696e365a4319495160878055336523ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367435
    Reviewed-by: Orr Bernstein <orrb@google.com>
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434150}

--- content/services/auction_worklet/bidder_worklet_unittest.cc
+++ content/services/auction_worklet/bidder_worklet_unittest.cc
@@ -11148,6 +11148,7 @@ TEST_F(BidderWorkletSharedStorageAPIEnabledTest, ModifierMethodTypeHierarchy) {
   v8_helpers_[0]->v8_runner()->PostTask(
       FROM_HERE, base::BindOnce(
                      [](scoped_refptr<AuctionV8Helper> v8_helper) {
+                       v8::Isolate::Scope isolate_scope{v8_helper->isolate()};
                        v8_helper->isolate()->RequestGarbageCollectionForTesting(
                            v8::Isolate::kFullGarbageCollection);
                      },
@@ -11601,6 +11602,7 @@ TEST_F(BidderWorkletSharedStorageAPIEnabledTest,
   v8_helpers_[0]->v8_runner()->PostTask(
       FROM_HERE, base::BindOnce(
                      [](scoped_refptr<AuctionV8Helper> v8_helper) {
+                       v8::Isolate::Scope isolate_scope{v8_helper->isolate()};
                        v8_helper->isolate()->RequestGarbageCollectionForTesting(
                            v8::Isolate::kFullGarbageCollection);
                      },
