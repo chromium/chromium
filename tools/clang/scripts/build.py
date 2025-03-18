6a88f57f68f59b98d886c92904b37a8f6ae923dc
Arthur Eubanks <aeubanks@google.com>
2025-03-18 11:26:10 -0700

    [clang] Use native ninja when possible over ninja wrapper
    This dramatically speeds up CMake compiler flag checks.
    
    Grepping for `Configuring done (X.XXs)` in build.py logs on Linux without and with this change gives:
    
    46.0s
    11.0s
    48.8s
    7.5s
    7.4s
    8.0s
    6.9s
    53.0s
    51.9s
    52.1s
    52.2s
    
    vs
    
    6.5s
    1.7s
    17.6s
    2.0s
    1.7s
    1.8s
    1.6s
    15.5s
    15.9s
    15.9s
    16.2s
    
    Change-Id: I277f92920bd599f4486041bc0b67bc71a8e253aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368016
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1434284}

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -802,10 +802,11 @@ def main():
   if not args.use_system_cmake:
     AddCMakeToPath()
 
-  if sys.platform == 'win32':
-    # CMake on Windows doesn't like depot_tools's ninja.bat wrapper.
-    ninja_dir = os.path.join(THIRD_PARTY_DIR, 'ninja')
-    os.environ['PATH'] = ninja_dir + os.pathsep + os.environ.get('PATH', '')
+  # CMake on Windows doesn't like depot_tools's ninja.bat wrapper.
+  # Using depot_tools's ninja wrapper also significantly slows down CMake
+  # compiler flag checks, so directly use the native ninja binary.
+  ninja_dir = os.path.join(THIRD_PARTY_DIR, 'ninja')
+  os.environ['PATH'] = ninja_dir + os.pathsep + os.environ.get('PATH', '')
 
   if sys.platform.startswith('linux'):
     with timer.time('get sysroots'):
