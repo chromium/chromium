8b02c98316c61f3f2dca4c7f0997f367f9767979
François Doray <fdoray@chromium.org>
2025-03-18 12:10:29 -0700

    [freezing] Enable RecordFreezingEligibilityUKM by default.
    We are satisfied with the data and no negative impact observed on
    guiding metrics when recording the event.
    
    Fixed: 391646022
    Change-Id: Ib9605f14f94510faff2ea5ea440835a9e048f453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368054
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1434326}

--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -178,7 +178,7 @@ BASE_FEATURE(kFreezingFollowsDiscardOptOut,
 
 BASE_FEATURE(kRecordFreezingEligibilityUKM,
              "RecordFreezingEligibilityUKM",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kResourceAttributionIncludeOrigins,
              "ResourceAttributionIncludeOrigins",
