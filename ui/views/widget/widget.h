27e60655cc01d73abaa005e5efe87369bc5cd8dd
Lei Zhang <thestig@chromium.org>
2025-03-19 15:38:19 -0700

    Remove unused force_show_in_taskbar from views::Widget::InitParams
    This parameter is never set to true. https://crrev.com/441394 removed
    the last use of this field many years ago.
    
    Change-Id: I4b20878deb927be33a0097e6795bc9106ed96b50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6356649
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435132}

--- ui/views/widget/widget.h
+++ ui/views/widget/widget.h
@@ -214,6 +214,9 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
     kMaxValue = kAcceptButtonClicked
   };
 
+  // This struct had unused fields that were removed, but may be of interest to
+  // future users:
+  // - force_show_in_taskbar: https://crrev.com/c/6356649
   struct VIEWS_EXPORT InitParams {
     enum Type {
       TYPE_WINDOW,  // A decorated Window, like a frame window.
@@ -459,10 +462,6 @@ class VIEWS_EXPORT Widget : public internal::NativeWidgetDelegate,
     // Linux. Nullptr is allowed on Chrome OS, which will place the window on
     // the default desktop for new windows.
     gfx::NativeWindow context = gfx::NativeWindow();
-
-    // If true, forces the window to be shown in the taskbar, even for window
-    // types that do not appear in the taskbar by default (popup and bubble).
-    bool force_show_in_taskbar = false;
 
 #if BUILDFLAG(IS_WIN)
     // If true, force the window not to be shown in the taskbar, even for
