8f6abda4d168b221cfd97a49e6322b2f805ad237
Calder Kitagawa <ckitagawa@chromium.org>
2025-03-19 13:54:39 -0700

    [Tab Collection] Change supports_handles to only use int32_t
    Instead of supporting signed/unsigned and variable size ints just use
    int32_t. This helps improve cross-language compatibility with Java,
    which does not have unsigned integers.
    
    This also opens the door to possibly changing the null value to be
    a sentinel value of -1 for compat with Java's Tab Ids (although this
    change is still under investigation as it could break things).
    
    Bug: 404296389
    Change-Id: Ia995cfe64d2a18086b502c3e8303d92877b9d94a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372321
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435055}

--- components/tab_collections/public/supports_handles.h
+++ components/tab_collections/public/supports_handles.h
@@ -40,18 +40,16 @@
 // thread, though their handles and handle values may be copied to and stored on
 // any thread.
 //
-// The default raw handle type `V` is a 32-bit signed integer, since (a) there
-// are usually not more than 4 billion of any UI object, and (b) signed integers
-// provide the broadest possible language support. However, you may choose any
-// integral type for `V`.
+// Handles are backed by a 32-bit signed integer, since (a) there are usually not
+// more than 4 billion of any UI object, and (b) signed integers provide the
+// broadest possible language support.
 //
-// Regardless of choice of `V`, the null value for handles is always zero.
+// The null value for handles is always zero.
 //
-// It is a fatal error to try to create a new object if all valid values of `V`
-// have already been used. If an object is likely to run through all possible
-// values of V (that is, have more than 4 billion constructed over the life of
-// a chrome instance) then it is probably a poor candidate for handles (or at
-// the very least, you need to pick a larger V).
+// It is a fatal error to try to create a new object if all int32_t values have
+// already been used. If an object is likely to run through all possible values
+// (that is, have more than 4 billion constructed over the life of a chrome
+// instance) then it is probably a poor candidate for handles.
 
 #include <concepts>
 #include <cstdint>
@@ -67,13 +65,10 @@
 // class C : public SupportsHandles<C> { ... }
 // ```
 //
-// `V` defines the type (and therefore size in bits) of the underlying handle
-// value. A wider value type will provide more unique handle values.
-//
 // It is required that `T` derive from this class. This constraint is enforced
 // via a helper class, as it cannot be enforced before SupportsHandles is
 // defined.
-template <typename T, std::integral V = int32_t>
+template <typename T>
 class SupportsHandles {
  public:
   SupportsHandles();
@@ -88,7 +83,7 @@ class SupportsHandles {
   Handle GetHandle() const;
 
  private:
-  V handle_value_;
+  int32_t handle_value_;
 };
 
 // The handle type for an object of type `T`.
@@ -97,24 +92,21 @@ class SupportsHandles {
 //
 // Unlike WeakPtr there is some overhead in looking up a handle, so convenience
 // operators (bool, !, ->, *) are not provided.
-template <typename T, std::integral V>
-class SupportsHandles<T, V>::Handle {
+template <typename T>
+class SupportsHandles<T>::Handle {
  public:
   Handle() = default;
   Handle(const Handle& other) = default;
   ~Handle() = default;
   Handle& operator=(const Handle& other) = default;
 
-  // The underlying opaque handle value type.
-  using RawValueType = V;
-
   // The object type returned by `Get()`.
   using ObjectType = T;
 
   // Convert to/from a raw, opaque handle value. It is safe to pass this value
   // around, including to code running in other languages.
-  explicit Handle(RawValueType raw_value) : raw_value_(raw_value) {}
-  RawValueType raw_value() const { return raw_value_; }
+  explicit Handle(int32_t raw_value) : raw_value_(raw_value) {}
+  int32_t raw_value() const { return raw_value_; }
 
   // Retrieves the underlying object, or null if it is no longer present.
   ObjectType* Get() const;
@@ -131,11 +123,11 @@ class SupportsHandles<T, V>::Handle {
   }
 
   // Explicitly provide the null value and handle.
-  static constexpr RawValueType NullValue = 0;
+  static constexpr int32_t NullValue = 0;
   static Handle Null() { return Handle(NullValue); }
 
  private:
-  RawValueType raw_value_ = NullValue;
+  int32_t raw_value_ = NullValue;
 };
 
 class SupportsHandlesTest;
@@ -146,15 +138,15 @@ namespace internal {
 //
 // This object is strictly sequence-checked and should only ever be accessed
 // from the primary UI thread.
-template <typename T, std::integral V>
-  requires std::derived_from<T, SupportsHandles<T, V>>
+template <typename T>
+  requires std::derived_from<T, SupportsHandles<T>>
 class HandleHelper {
  public:
-  using StoredPointerType = SupportsHandles<T, V>*;
+  using StoredPointerType = SupportsHandles<T>*;
 
   // Assigns a new, unused handle value for `object` and returns the value.
   // Called from the constructor of `SupportsHandles`.
-  V AssignHandleValue(StoredPointerType object) {
+  int32_t AssignHandleValue(StoredPointerType object) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_);
     CHECK(object);
 
@@ -162,8 +154,7 @@ class HandleHelper {
     // back over to zero.
     ++last_handle_value_;
     CHECK(last_handle_value_)
-        << "Fatal handle reuse! Please use a larger raw value type (V) or "
-           "curtail object creation.";
+        << "Fatal handle reuse! Please curtail object creation.";
 
     lookup_table_.emplace(last_handle_value_, object);
     return last_handle_value_;
@@ -171,14 +162,14 @@ class HandleHelper {
 
   // Frees a handle with `handle_value`. Must be called from the destructor of
   // `SupportsHandles`.
-  void FreeHandleValue(V handle_value) {
+  void FreeHandleValue(int32_t handle_value) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_);
     CHECK(lookup_table_.erase(handle_value));
   }
 
   // Retrieves the object associated with the given `handle_value`, or null if
   // no such object exists.
-  T* LookupObject(V handle_value) const {
+  T* LookupObject(int32_t handle_value) const {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_);
     const auto it = lookup_table_.find(handle_value);
     return it != lookup_table_.end() ? static_cast<T*>(it->second) : nullptr;
@@ -192,37 +183,36 @@ class HandleHelper {
 
  private:
   friend SupportsHandlesTest;
-  friend class base::NoDestructor<HandleHelper<T, V>>;
+  friend class base::NoDestructor<HandleHelper<T>>;
   HandleHelper() = default;
   ~HandleHelper() = default;
 
-  V last_handle_value_ GUARDED_BY_CONTEXT(sequence_) = 0;
-  std::map<V, StoredPointerType> lookup_table_ GUARDED_BY_CONTEXT(sequence_);
+  int32_t last_handle_value_ GUARDED_BY_CONTEXT(sequence_) = 0;
+  std::map<int32_t, StoredPointerType> lookup_table_
+      GUARDED_BY_CONTEXT(sequence_);
   SEQUENCE_CHECKER(sequence_);
 };
 
 }  // namespace internal
 
-template <typename T, std::integral V>
-SupportsHandles<T, V>::SupportsHandles()
+template <typename T>
+SupportsHandles<T>::SupportsHandles()
     : handle_value_(
-          internal::HandleHelper<T, V>::GetInstance().AssignHandleValue(this)) {
+          internal::HandleHelper<T>::GetInstance().AssignHandleValue(this)) {}
+
+template <typename T>
+SupportsHandles<T>::~SupportsHandles() {
+  internal::HandleHelper<T>::GetInstance().FreeHandleValue(handle_value_);
 }
 
-template <typename T, std::integral V>
-SupportsHandles<T, V>::~SupportsHandles() {
-  internal::HandleHelper<T, V>::GetInstance().FreeHandleValue(handle_value_);
-}
-
-template <typename T, std::integral V>
-typename SupportsHandles<T, V>::Handle SupportsHandles<T, V>::GetHandle()
-    const {
+template <typename T>
+typename SupportsHandles<T>::Handle SupportsHandles<T>::GetHandle() const {
   return Handle(handle_value_);
 }
 
-template <typename T, std::integral V>
-T* SupportsHandles<T, V>::Handle::Get() const {
-  return internal::HandleHelper<T, V>::GetInstance().LookupObject(raw_value_);
+template <typename T>
+T* SupportsHandles<T>::Handle::Get() const {
+  return internal::HandleHelper<T>::GetInstance().LookupObject(raw_value_);
 }
 
 #endif  // COMPONENTS_TAB_COLLECTIONS_PUBLIC_SUPPORTS_HANDLES_H_
--- components/tab_collections/public/supports_handles_unittest.cc
+++ components/tab_collections/public/supports_handles_unittest.cc
@@ -12,83 +12,49 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
-class DefaultTestClass : public SupportsHandles<DefaultTestClass> {};
-template <std::integral V>
-class TestClass : public SupportsHandles<TestClass<V>, V> {};
+class TestClass : public SupportsHandles<TestClass> {};
 }  // namespace
 
 class SupportsHandlesTest : public testing::Test {
  public:
   template <typename T>
-  void ResetCounter() {
-    auto& helper =
-        internal::HandleHelper<T,
-                               typename T::Handle::RawValueType>::GetInstance();
+  void SetCounter(int32_t value) {
+    auto& helper = internal::HandleHelper<T>::GetInstance();
     DCHECK_CALLED_ON_VALID_SEQUENCE(helper.sequence_);
     CHECK(helper.lookup_table_.empty());
-    helper.last_handle_value_ =
-        SupportsHandles<T, typename T::Handle::RawValueType>::Handle::NullValue;
+    helper.last_handle_value_ = value;
   }
-
- private:
 };
 
 TEST_F(SupportsHandlesTest, NullHandle) {
-  using C = TestClass<int32_t>;
-  EXPECT_EQ(nullptr, C::Handle::Null().Get());
+  EXPECT_EQ(nullptr, TestClass::Handle::Null().Get());
 }
 
 TEST_F(SupportsHandlesTest, ValidHandle) {
-  using C = TestClass<int32_t>;
-  C c;
+  TestClass c;
   EXPECT_EQ(&c, c.GetHandle().Get());
 }
 
 TEST_F(SupportsHandlesTest, HandleBecomesInvalid) {
-  using C = TestClass<int32_t>;
-  C::Handle handle;
+  TestClass::Handle handle;
   {
-    C c;
-    handle = c.GetHandle();
-  }
-  EXPECT_EQ(nullptr, handle.Get());
-}
-
-// Verifies that ubsan CI builds can handle default template parameter.
-TEST_F(SupportsHandlesTest, DefaultHandleBecomesInvalid) {
-  DefaultTestClass::Handle handle;
-  {
-    DefaultTestClass c;
+    TestClass c;
     handle = c.GetHandle();
   }
   EXPECT_EQ(nullptr, handle.Get());
 }
 
 TEST_F(SupportsHandlesTest, IncrementsValues) {
-  using C = TestClass<int32_t>;
-  ResetCounter<C>();
-  C c1;
-  C c2;
-  C c3;
+  SetCounter<TestClass>(TestClass::Handle::NullValue);
+  TestClass c1;
+  TestClass c2;
+  TestClass c3;
   EXPECT_EQ(1, c1.GetHandle().raw_value());
   EXPECT_EQ(2, c2.GetHandle().raw_value());
   EXPECT_EQ(3, c3.GetHandle().raw_value());
 }
 
-TEST_F(SupportsHandlesTest, FailsOnRolloverUnsigned) {
-  using C = TestClass<uint8_t>;
-  ResetCounter<C>();
-  for (int i = 1; i < 256; ++i) {
-    C c;
-  }
-  EXPECT_DEATH_IF_SUPPORTED(C c, "");
-}
-
 TEST_F(SupportsHandlesTest, FailsOnRolloverSigned) {
-  using C = TestClass<int8_t>;
-  ResetCounter<C>();
-  for (int i = 1; i < 256; ++i) {
-    C c;
-  }
-  EXPECT_DEATH_IF_SUPPORTED(C c, "");
+  SetCounter<TestClass>(TestClass::Handle::NullValue - 1);
+  EXPECT_DEATH_IF_SUPPORTED(TestClass c, "");
 }
