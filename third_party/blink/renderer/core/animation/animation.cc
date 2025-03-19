bc11c85e09098d8957c375777ff913a7162320d0
Kevin Ellis <kevers@google.com>
2025-03-19 12:04:13 -0700

    Add CompositingDecisionAtAnimationPhaseBoundaries feature flag
    Previously we forced an update of the compositing decision when an
    animation enters or leaves the active phase in order for native paint
    worklet (NPW) animated properties to work with view timelines. This
    change introduced rendering glitches for view timeline based animations
    of opacity. The decision whether to force a new compositing decision is
    now behind a feature flag to restore the old behavior. This change
    intentionally reintroduces a bug with NPW, but as they are still in
    experimental, they can be safely fixed in a followup CL.
    
    (cherry picked from commit 176e3f801211c0b299bc757b7360cbd9dfea97dc)
    
    Bug: 403708813
    Fixed: 404507478
    Change-Id: I780ef8235d40346a75159980b44eff7d775222b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361481
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1434090}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6373385
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6998@{#2071}
    Cr-Branched-From: de9c6fafd8ae5c6ea0438764076ca7d04a0b165d-refs/heads/main@{#1415337}

--- third_party/blink/renderer/core/animation/animation.cc
+++ third_party/blink/renderer/core/animation/animation.cc
@@ -2282,6 +2282,11 @@ Animation::CheckCanStartAnimationOnCompositorInternal() const {
 }
 
 bool Animation::EffectivelyPlaying() const {
+  if (!RuntimeEnabledFeatures::
+          CompositingDecisionAtAnimationPhaseBoundariesEnabled()) {
+    return Playing();
+  }
+
   if (!Playing()) {
     return false;
   }
@@ -2294,6 +2299,9 @@ bool Animation::EffectivelyPlaying() const {
 }
 
 void Animation::OnActivePhaseStateChange(bool in_active_phase) {
+  DCHECK(RuntimeEnabledFeatures::
+             CompositingDecisionAtAnimationPhaseBoundariesEnabled());
+
   if (!timeline_ || timeline_->IsMonotonicallyIncreasing()) {
     return;
   }
