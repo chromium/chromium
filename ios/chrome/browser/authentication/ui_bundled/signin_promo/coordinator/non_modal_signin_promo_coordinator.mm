f9baa7aa7737882260887c61ff01d85b1ef5437c
Ibrahim Kanouche <kanouche@google.com>
2025-03-20 14:20:04 -0700

    [iOS] Add code skeleton for non modal sign-in promo.
    Change-Id: I390feb40a21654a84ac21036101396f0c057cee3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372869
    Auto-Submit: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435687}

---
+++ ios/chrome/browser/authentication/ui_bundled/signin_promo/coordinator/non_modal_signin_promo_coordinator.mm
@@ -0,0 +1,61 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/authentication/ui_bundled/signin_promo/coordinator/non_modal_signin_promo_coordinator.h"
+
+#import "ios/chrome/browser/authentication/ui_bundled/signin_promo/coordinator/non_modal_signin_promo_mediator.h"
+#import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/signin/model/authentication_service.h"
+#import "ios/chrome/browser/signin/model/authentication_service_factory.h"
+#import "ios/chrome/browser/signin/model/identity_manager_factory.h"
+
+@interface NonModalSignInPromoCoordinator () <
+    NonModalSignInPromoMediatorDelegate>
+
+@end
+
+@implementation NonModalSignInPromoCoordinator {
+  NonModalSignInPromoMediator* _mediator;
+  SignInPromoType* _promoType;
+}
+
+- (instancetype)initWithBaseViewController:(UIViewController*)viewController
+                                   browser:(Browser*)browser
+                                 promoType:(SignInPromoType)promoType {
+  self = [super initWithBaseViewController:viewController browser:browser];
+  if (self) {
+    _promoType = promoType;
+  }
+  return self;
+}
+
+- (void)start {
+  AuthenticationService* authService =
+      AuthenticationServiceFactory::GetForProfile(self.profile);
+
+  signin::IdentityManager* identityManager =
+      IdentityManagerFactory::GetForProfile(self.profile);
+
+  _mediator = [[NonModalSignInPromoMediator alloc]
+      initWithAuthenticationService:authService
+                    identityManager:identityManager];
+
+  _mediator.delegate = self;
+  [_mediator startPromoDisplayTimer];
+}
+
+- (void)stop {
+  [_mediator disconnect];
+  _mediator = nil;
+  _promoType = nil;
+}
+
+#pragma mark - NonModalSignInPromoMediatorDelegate
+
+- (bool)nonModalSignInPromoMediatorTimerExipred:
+    (NonModalSignInPromoMediator*)mediator {
+  // TODO(crbug.com/404844914): Implement Non modal sign in coordinator.
+}
+
+@end
