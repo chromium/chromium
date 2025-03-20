bb2f8554d9a83289b5956ef7a8a44f8687d53927
Daseul Lee <dslee@chromium.org>
2025-03-20 14:18:54 -0700

    Writing Assistance API: create AIWritingAssistanceBase
    This new template class holds shared code between Writing
    Assistance APIs. This change is made in preparation for the
    new namespace change, in order to avoid duplicating code.
    
    Bug: 402442890, 381969447
    Change-Id: I8be946db97adb9dcd26da8589937d3f86296e39e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372130
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435685}

--- third_party/blink/renderer/modules/ai/ai_summarizer.cc
+++ third_party/blink/renderer/modules/ai/ai_summarizer.cc
@@ -4,202 +4,72 @@
 
 #include "third_party/blink/renderer/modules/ai/ai_summarizer.h"
 
-#include "base/functional/callback_helpers.h"
-#include "base/metrics/histogram_functions.h"
 #include "third_party/blink/public/mojom/ai/model_streaming_responder.mojom-blink.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_ai_summarizer.h"
-#include "third_party/blink/renderer/core/dom/abort_signal.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_ai_summarizer_summarize_options.h"
 #include "third_party/blink/renderer/modules/ai/ai_metrics.h"
-#include "third_party/blink/renderer/modules/ai/exception_helpers.h"
-#include "third_party/blink/renderer/modules/ai/model_execution_responder.h"
 
 namespace blink {
 
 AISummarizer::AISummarizer(
-    ExecutionContext* context,
+    ExecutionContext* execution_context,
     scoped_refptr<base::SequencedTaskRunner> task_runner,
     mojo::PendingRemote<mojom::blink::AISummarizer> pending_remote,
     AISummarizerCreateOptions* options)
-    : ExecutionContextClient(context),
-      task_runner_(task_runner),
-      remote_(context),
-      options_(options) {
-  remote_.Bind(std::move(pending_remote), task_runner_);
-}
+    : AIWritingAssistanceBase<mojom::blink::AISummarizer,
+                              AISummarizerCreateOptions,
+                              AISummarizerSummarizeOptions>(
+          execution_context,
+          task_runner,
+          std::move(pending_remote),
+          std::move(options),
+          AIMetrics::AISessionType::kSummarizer,
+          /*echo_whitespace_input=*/false) {}
 
 void AISummarizer::Trace(Visitor* visitor) const {
   ScriptWrappable::Trace(visitor);
-  ExecutionContextClient::Trace(visitor);
-  visitor->Trace(remote_);
-  visitor->Trace(options_);
+  AIWritingAssistanceBase::Trace(visitor);
+}
+
+void AISummarizer::remoteExecute(
+    const String& input,
+    const String& context,
+    mojo::PendingRemote<blink::mojom::blink::ModelStreamingResponder>
+        responder) {
+  remote_->Summarize(input, context, std::move(responder));
 }
 
 ScriptPromise<IDLString> AISummarizer::summarize(
     ScriptState* script_state,
-    const String& input,
+    const String& writing_task,
     const AISummarizerSummarizeOptions* options,
     ExceptionState& exception_state) {
-  if (!script_state->ContextIsValid()) {
-    ThrowInvalidContextException(exception_state);
-  }
-
-  base::UmaHistogramEnumeration(
-      AIMetrics::GetAIAPIUsageMetricName(AIMetrics::AISessionType::kSummarizer),
-      AIMetrics::AIAPI::kSessionSummarize);
-
-  // TODO(crbug.com/356058216): Shall we add separate text size UMAs for
-  // summarization
-  base::UmaHistogramCounts1M(AIMetrics::GetAISessionRequestSizeMetricName(
-                                 AIMetrics::AISessionType::kSummarizer),
-                             int(input.CharactersSizeInBytes()));
-
-  if (is_destroyed_) {
-    ThrowSessionDestroyedException(exception_state);
-    return ScriptPromise<IDLString>();
-  }
-
-  auto* resolver =
-      MakeGarbageCollected<ScriptPromiseResolver<IDLString>>(script_state);
-  auto promise = resolver->Promise();
-  AbortSignal* signal = options->getSignalOr(nullptr);
-  if (signal && signal->aborted()) {
-    resolver->Reject(signal->reason(script_state));
-    return promise;
-  }
-
-  String trimmed_input = input.StripWhiteSpace();
-  if (trimmed_input.empty()) {
-    resolver->Resolve(trimmed_input);
-    return promise;
-  }
-
-  auto pending_remote = CreateModelExecutionResponder(
-      script_state, signal, resolver, task_runner_,
-      AIMetrics::AISessionType::kSummarizer,
-      /*complete_callback=*/base::DoNothing(),
-      /*overflow_callback=*/base::DoNothing());
-  remote_->Summarize(trimmed_input, options->getContextOr(g_empty_string),
-                     std::move(pending_remote));
-  return promise;
+  return AIWritingAssistanceBase::execute(
+      script_state, writing_task, options, exception_state,
+      AIMetrics::AIAPI::kSummarizerSummarize);
 }
 
 ReadableStream* AISummarizer::summarizeStreaming(
     ScriptState* script_state,
-    const String& input,
+    const String& writing_task,
     const AISummarizerSummarizeOptions* options,
     ExceptionState& exception_state) {
-  if (!script_state->ContextIsValid()) {
-    ThrowInvalidContextException(exception_state);
-    return nullptr;
-  }
-
-  base::UmaHistogramEnumeration(
-      AIMetrics::GetAIAPIUsageMetricName(AIMetrics::AISessionType::kSummarizer),
-      AIMetrics::AIAPI::kSessionSummarizeStreaming);
-
-  // TODO(crbug.com/356058216): Shall we add separate text size UMAs for
-  // summarization
-  base::UmaHistogramCounts1M(AIMetrics::GetAISessionRequestSizeMetricName(
-                                 AIMetrics::AISessionType::kSummarizer),
-                             int(input.CharactersSizeInBytes()));
-
-  if (is_destroyed_) {
-    ThrowSessionDestroyedException(exception_state);
-    return nullptr;
-  }
-
-  AbortSignal* signal = options->getSignalOr(nullptr);
-  if (HandleAbortSignal(signal, script_state, exception_state)) {
-    return nullptr;
-  }
-
-  String trimmed_input = input.StripWhiteSpace();
-  if (trimmed_input.empty()) {
-    return CreateEmptyReadableStream(script_state,
-                                     AIMetrics::AISessionType::kSummarizer);
-  }
-
-  auto [readable_stream, pending_remote] =
-      CreateModelExecutionStreamingResponder(
-          script_state, signal, task_runner_,
-          AIMetrics::AISessionType::kSummarizer,
-          /*complete_callback=*/base::DoNothing(),
-          /*overflow_callback=*/base::DoNothing());
-  remote_->Summarize(trimmed_input, options->getContextOr(g_empty_string),
-                     std::move(pending_remote));
-  return readable_stream;
+  return AIWritingAssistanceBase::executeStreaming(
+      script_state, writing_task, options, exception_state,
+      AIMetrics::AIAPI::kSummarizerSummarizeStreaming);
 }
 
-// TODO(crbug.com/402442890): Refactor Writing Assistance APIs to reduce
-// duplicated code.
 ScriptPromise<IDLDouble> AISummarizer::measureInputUsage(
     ScriptState* script_state,
-    const String& input,
+    const String& writing_task,
     const AISummarizerSummarizeOptions* options,
     ExceptionState& exception_state) {
-  if (!script_state->ContextIsValid()) {
-    ThrowInvalidContextException(exception_state);
-    return ScriptPromise<IDLDouble>();
-  }
-
-  if (is_destroyed_) {
-    ThrowSessionDestroyedException(exception_state);
-    return ScriptPromise<IDLDouble>();
-  }
-
-  auto* resolver =
-      MakeGarbageCollected<ScriptPromiseResolver<IDLDouble>>(script_state);
-  auto promise = resolver->Promise();
-  AbortSignal* signal = options->getSignalOr(nullptr);
-  if (signal && signal->aborted()) {
-    resolver->Reject(signal->reason(script_state));
-    return promise;
-  }
-
-  remote_->MeasureUsage(
-      input, options->getContextOr(g_empty_string),
-      WTF::BindOnce(
-          [](ScriptPromiseResolver<IDLDouble>* resolver, AbortSignal* signal,
-             std::optional<uint64_t> usage) {
-            ExecutionContext* context = resolver->GetExecutionContext();
-            if (!context) {
-              return;
-            }
-            if (signal && signal->aborted()) {
-              resolver->Reject(signal->reason(resolver->GetScriptState()));
-              return;
-            }
-            if (!usage.has_value()) {
-              resolver->Reject(
-                  DOMException::Create(kExceptionMessageUnableToCalculateUsage,
-                                       DOMException::GetErrorName(
-                                           DOMExceptionCode::kOperationError)));
-              return;
-            }
-            resolver->Resolve(static_cast<double>(usage.value()));
-          },
-          WrapPersistent(resolver),
-          WrapPersistent(options->getSignalOr(nullptr))));
-
-  return promise;
+  return AIWritingAssistanceBase::measureInputUsage(script_state, writing_task,
+                                                    options, exception_state);
 }
 
-// TODO(crbug.com/355967885): reset the remote to destroy the session.
 void AISummarizer::destroy(ScriptState* script_state,
                            ExceptionState& exception_state) {
-  if (!script_state->ContextIsValid()) {
-    ThrowInvalidContextException(exception_state);
-    return;
-  }
-
-  base::UmaHistogramEnumeration(
-      AIMetrics::GetAIAPIUsageMetricName(AIMetrics::AISessionType::kSummarizer),
-      AIMetrics::AIAPI::kSessionDestroy);
-
-  if (!is_destroyed_) {
-    is_destroyed_ = true;
-    remote_.reset();
-  }
+  AIWritingAssistanceBase::destroy(script_state, exception_state);
 }
 
 }  // namespace blink
--- third_party/blink/renderer/modules/ai/ai_summarizer.h
+++ third_party/blink/renderer/modules/ai/ai_summarizer.h
@@ -5,44 +5,48 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_AI_AI_SUMMARIZER_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_AI_AI_SUMMARIZER_H_
 
+#include "base/task/sequenced_task_runner.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
 #include "third_party/blink/public/mojom/ai/ai_summarizer.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ai_summarizer_create_options.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_ai_summarizer_format.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_ai_summarizer_length.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ai_summarizer_summarize_options.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_ai_summarizer_type.h"
-#include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
-#include "third_party/blink/renderer/core/streams/readable_stream.h"
+#include "third_party/blink/renderer/modules/ai/ai_writing_assistance_base.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
-#include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
-#include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
 
 // The class that represents a summarizer object.
-class AISummarizer final : public ScriptWrappable,
-                           public ExecutionContextClient {
+class AISummarizer final
+    : public ScriptWrappable,
+      AIWritingAssistanceBase<mojom::blink::AISummarizer,
+                              AISummarizerCreateOptions,
+                              AISummarizerSummarizeOptions> {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  AISummarizer(ExecutionContext* context,
+  AISummarizer(ExecutionContext* execution_context,
                scoped_refptr<base::SequencedTaskRunner> task_runner,
                mojo::PendingRemote<mojom::blink::AISummarizer> pending_remote,
                AISummarizerCreateOptions* options);
-  ~AISummarizer() override = default;
-
   void Trace(Visitor* visitor) const override;
 
-  // ai_summarizer.idl implementation.
+  // AIWritingAssistanceBase:
+  void remoteExecute(
+      const String& input,
+      const String& context,
+      mojo::PendingRemote<blink::mojom::blink::ModelStreamingResponder>
+          responder) override;
+
+  // ai_summarizer.idl:
   ScriptPromise<IDLString> summarize(
       ScriptState* script_state,
-      const String& input,
+      const String& writing_task,
       const AISummarizerSummarizeOptions* options,
       ExceptionState& exception_state);
   ReadableStream* summarizeStreaming(
       ScriptState* script_state,
-      const String& input,
+      const String& writing_task,
       const AISummarizerSummarizeOptions* options,
       ExceptionState& exception_state);
   ScriptPromise<IDLDouble> measureInputUsage(
@@ -52,38 +56,15 @@ class AISummarizer final : public ScriptWrappable,
       ExceptionState& exception_state);
   void destroy(ScriptState* script_state, ExceptionState& exception_state);
 
-  String sharedContext() const {
-    return options_->getSharedContextOr(g_empty_string);
-  }
   V8AISummarizerType type() const { return options_->type(); }
   V8AISummarizerFormat format() const { return options_->format(); }
   V8AISummarizerLength length() const { return options_->length(); }
-  std::optional<Vector<String>> expectedInputLanguages() const {
-    if (options_->hasExpectedInputLanguages()) {
-      return options_->expectedInputLanguages();
-    }
-    return std::nullopt;
-  }
-  std::optional<Vector<String>> expectedContextLanguages() const {
-    if (options_->hasExpectedContextLanguages()) {
-      return options_->expectedContextLanguages();
-    }
-    return std::nullopt;
-  }
-  String outputLanguage() const {
-    return options_->getOutputLanguageOr(String());
-  }
-  double inputQuota() const {
-    return static_cast<double>(
-        mojom::blink::kWritingAssistanceMaxInputTokenSize);
-  }
 
- private:
-  bool is_destroyed_ = false;
-
-  scoped_refptr<base::SequencedTaskRunner> task_runner_;
-  HeapMojoRemote<blink::mojom::blink::AISummarizer> remote_;
-  Member<AISummarizerCreateOptions> options_;
+  using AIWritingAssistanceBase::expectedContextLanguages;
+  using AIWritingAssistanceBase::expectedInputLanguages;
+  using AIWritingAssistanceBase::inputQuota;
+  using AIWritingAssistanceBase::outputLanguage;
+  using AIWritingAssistanceBase::sharedContext;
 };
 
 }  // namespace blink
