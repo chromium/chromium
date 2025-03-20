35578b89abacbb0245c2c5a868025066fa3d8eee
Samuel Huang <huangs@chromium.org>
2025-03-19 18:51:12 -0700

    [MVT Customization] Add Java-side Custom Links operation plumbing, including JNI.
    Custom tiles pin / unpin / edit flows are triggered from the tile
    context menu. The planned flow (which follows the tile removal /
    blocklist feature) is as follows:
    1.  Entry: ContextMenuManager.handleMenuItemClick().
    2a. Call: ContextMenuManager.Delegate methods.
    2b. Impl: TileGroup.TileInteractionDelegateImpl methods (business logic,
        which involves handling tile reload).
    3a. Call: TileGroup.Delegate methods.
    3b. Impl: TileGroupDelegateImpl methods (forwarding).
    4a. Call: MostVisitedSites methods.
    4b. Impl: MostVisitedSitesBridge methods (JNI hop).
    5. JNI: C++ MostVisitedSitesBridge member functions.
    6+. (C++ backend; worry about this later)
    
    Next, we'd like to add the following custom tile / link operations:
    * Add: Creates a new custom tile with given {name, URL}.
    * Assign: Change {name, URL} of an existing tile, which can be custom or
      Most Visited.
    * Delete: Remove an existing custom tile.
    * Query: Determine whether a custom tile with a given URL exists.
    
    This CL adds the above operations to (3a) -> (5). The remaining parts
    will be implemented in follow-up CLs:
    * (1) -> (3a): Menu handling, UI, and business logic.
    * (5)+: C++ backend for custom links.
    
    Details:
    * Add new interface CustomLinkOperations, with 4 methods:
      {add,assign,delete,query}CustomLink().
      * Methods all return boolean success flag.
      * {assign,delete,query}CustomLink() take |keyUrl| params to query
        existing links:
        * assignCustomLink() for custom or Most Visited links.
        * {delete,query}CustomLink() for custom links.
    * (3a), (4a): Have interfaces {TileGroup.Delegate, MostVisitedSites}
      extend CustomLinkOperations.
    * (3b): TileGroupDelegateImpl: Implement pass-through for 4 methods.
    * (4b): MostVisitedSitesBridge: Add JNI calls.
      * CTOR: Pass flag |sMostVisitedTilesCustomization.isEnabled()| to C++
        via JNI.
      * {add,assign}CustomLink(): Perform validity check on |url|.
    * (5): Add stub JNI receivers that all return false.
      * Now called {Add,Assign,Delete,Query}CustomLink().
      * Receive passed flag as |enable_custom_links|, currently unused.
    * (4a), (4b) test: FakeMostVisitedSites: Add stubs, which will be
      populated with simple implementation once we add (1)-(3) -> (4) tests
      that need it.
    
    Bug: 397422192
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Change-Id: I7b54d02cd0db1e104bdffe4092f169c027f55a93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6367262
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435216}

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/mostvisited/MostVisitedSitesBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/mostvisited/MostVisitedSitesBridge.java
@@ -8,6 +8,9 @@ import org.jni_zero.CalledByNative;
 import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.suggestions.SiteSuggestion;
 import org.chromium.chrome.browser.suggestions.tile.Tile;
@@ -16,6 +19,7 @@ import org.chromium.url.GURL;
 import java.util.List;
 
 /** Methods to bridge into native history to provide most recent urls, titles and thumbnails. */
+@NullMarked
 public class MostVisitedSitesBridge implements MostVisitedSites {
     /**
      * Maximum number of tiles that is explicitly supported. UMA relies on this value, so even if
@@ -25,7 +29,7 @@ public class MostVisitedSitesBridge implements MostVisitedSites {
 
     private long mNativeMostVisitedSitesBridge;
 
-    private MostVisitedSites.Observer mWrappedObserver;
+    private MostVisitedSites.@Nullable Observer mWrappedObserver;
 
     /**
      * MostVisitedSites constructor requires a valid user profile object.
@@ -33,10 +37,42 @@ public class MostVisitedSitesBridge implements MostVisitedSites {
      * @param profile The profile for which to fetch most visited sites.
      */
     public MostVisitedSitesBridge(Profile profile) {
+        boolean enable_custom_links = ChromeFeatureList.sMostVisitedTilesCustomization.isEnabled();
         mNativeMostVisitedSitesBridge =
-                MostVisitedSitesBridgeJni.get().init(MostVisitedSitesBridge.this, profile);
+                MostVisitedSitesBridgeJni.get()
+                        .init(MostVisitedSitesBridge.this, profile, enable_custom_links);
     }
 
+    // CustomLinkOperations -> MostVisitedSites implementation.
+    @Override
+    public boolean addCustomLink(String name, @Nullable GURL url) {
+        if (mNativeMostVisitedSitesBridge == 0 || GURL.isEmptyOrInvalid(url)) return false;
+        return MostVisitedSitesBridgeJni.get()
+                .addCustomLink(mNativeMostVisitedSitesBridge, name, url);
+    }
+
+    @Override
+    public boolean assignCustomLink(GURL keyUrl, String name, @Nullable GURL url) {
+        if (mNativeMostVisitedSitesBridge == 0 || GURL.isEmptyOrInvalid(url)) return false;
+        return MostVisitedSitesBridgeJni.get()
+                .assignCustomLink(mNativeMostVisitedSitesBridge, keyUrl, name, url);
+    }
+
+    @Override
+    public boolean deleteCustomLink(GURL keyUrl) {
+        if (mNativeMostVisitedSitesBridge == 0) return false;
+        return MostVisitedSitesBridgeJni.get()
+                .deleteCustomLink(mNativeMostVisitedSitesBridge, keyUrl);
+    }
+
+    @Override
+    public boolean queryCustomLink(GURL keyUrl) {
+        if (mNativeMostVisitedSitesBridge == 0) return false;
+        return MostVisitedSitesBridgeJni.get()
+                .queryCustomLink(mNativeMostVisitedSitesBridge, keyUrl);
+    }
+
+    // MostVisitedSites implementation.
     /**
      * Cleans up the C++ side of this class. This instance must not be used after calling destroy().
      */
@@ -129,9 +165,9 @@ public class MostVisitedSitesBridge implements MostVisitedSites {
     @CalledByNative
     private void onURLsAvailable(@JniType("std::vector") List<SiteSuggestion> suggestions) {
         // Don't notify observer if we've already been destroyed.
-        if (mNativeMostVisitedSitesBridge == 0) return;
-
-        mWrappedObserver.onSiteSuggestionsAvailable(suggestions);
+        if (mNativeMostVisitedSitesBridge != 0 && mWrappedObserver != null) {
+            mWrappedObserver.onSiteSuggestionsAvailable(suggestions);
+        }
     }
 
     /**
@@ -143,14 +179,32 @@ public class MostVisitedSitesBridge implements MostVisitedSites {
     @CalledByNative
     private void onIconMadeAvailable(@JniType("GURL") GURL siteUrl) {
         // Don't notify observer if we've already been destroyed.
-        if (mNativeMostVisitedSitesBridge != 0) {
+        if (mNativeMostVisitedSitesBridge != 0 && mWrappedObserver != null) {
             mWrappedObserver.onIconMadeAvailable(siteUrl);
         }
     }
 
     @NativeMethods
     interface Natives {
-        long init(MostVisitedSitesBridge caller, @JniType("Profile*") Profile profile);
+        long init(
+                MostVisitedSitesBridge caller,
+                @JniType("Profile*") Profile profile,
+                boolean enableCustomLinks);
+
+        boolean addCustomLink(
+                long nativeMostVisitedSitesBridge,
+                @JniType("std::u16string") String caller,
+                @JniType("GURL") GURL url);
+
+        boolean assignCustomLink(
+                long nativeMostVisitedSitesBridge,
+                @JniType("GURL") GURL keyUrl,
+                @JniType("std::u16string") String caller,
+                @JniType("GURL") GURL url);
+
+        boolean deleteCustomLink(long nativeMostVisitedSitesBridge, @JniType("GURL") GURL keyUrl);
+
+        boolean queryCustomLink(long nativeMostVisitedSitesBridge, @JniType("GURL") GURL keyUrl);
 
         void destroy(long nativeMostVisitedSitesBridge, MostVisitedSitesBridge caller);
 
