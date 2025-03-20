88a11c2fb4532a8b3c06d4a372d708685e6db383
Ramin Halavati <rhalavati@google.com>
2025-03-20 10:43:13 -0700

    Add metrics on reading mode heuristics.
    Add metrics to monitor usefulness of some heuristics in reading mode.
    
    AX-Relnots: n/a
    Bug: 359853518
    Change-Id: I00fd508b3473ea14c6a3b7ee152f20d3aaf75cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6375531
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435545}

--- chrome/renderer/accessibility/read_anything/read_anything_app_model.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_app_model.cc
@@ -60,6 +60,24 @@ bool GetIsGoogleDocs(const GURL& url) {
   return false;
 }
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+// LINT.IfChange(ReadAnythingHeuristics)
+enum class ReadAnythingHeuristics {
+  kNone = 0,
+  kNodeNotFound = 1,
+  kInvisibleOrIgnored = 2,
+  kNotExpanded = 3,
+  kNoDeepsetLastDecendent = 4,
+  kMaxValue = kNoDeepsetLastDecendent
+};
+// LINT.ThenChange(//tools/metrics/histograms/metadata/accessibility/enums.xml:ReadAnythingHeuristics)
+
+void RecordHeuristicMetric(ReadAnythingHeuristics heuristic) {
+  base::UmaHistogramEnumeration("Accessibility.ReadAnything.Heuristics",
+                                heuristic);
+}
+
 }  // namespace
 
 ReadAnythingAppModel::ReadAnythingAppModel() {
@@ -340,7 +358,13 @@ void ReadAnythingAppModel::ComputeDisplayNodeIdsForDistilledTree() {
     // TODO(abigailbklein) This prevents the crash in crbug.com/1402788, but may
     // not be the correct approach. Do we need a version of
     // GetDeepestLastUnignoredDescendant() that works on ignored nodes?
-    if (!content_node || content_node->IsInvisibleOrIgnored()) {
+    if (!content_node) {
+      RecordHeuristicMetric(ReadAnythingHeuristics::kNodeNotFound);
+      continue;
+    }
+
+    if (content_node->IsInvisibleOrIgnored()) {
+      RecordHeuristicMetric(ReadAnythingHeuristics::kInvisibleOrIgnored);
       continue;
     }
 
@@ -352,6 +376,7 @@ void ReadAnythingAppModel::ComputeDisplayNodeIdsForDistilledTree() {
       // attribute directly for that reason.
       if (!content_node->HasState(ax::mojom::State::kExpanded)) {
         // Don't include collapsed aria-expanded items.
+        RecordHeuristicMetric(ReadAnythingHeuristics::kNotExpanded);
         continue;
       }
     }
@@ -382,12 +407,15 @@ void ReadAnythingAppModel::ComputeDisplayNodeIdsForDistilledTree() {
     ui::AXNode* deepest_last_descendant =
         content_node->GetDeepestLastUnignoredDescendant();
     if (!deepest_last_descendant) {
+      RecordHeuristicMetric(ReadAnythingHeuristics::kNoDeepsetLastDecendent);
       continue;
     }
     while (next_node != deepest_last_descendant) {
       next_node = next_node->GetNextUnignoredInTreeOrder();
       InsertIdIfNotIgnored(next_node->id(), display_node_ids_);
     }
+
+    RecordHeuristicMetric(ReadAnythingHeuristics::kNone);
   }
 }
 
