e04664c90d4f9e51cc0c46e08f3efb92a6793688
Kevin McNee <mcnee@chromium.org>
2025-03-20 14:59:09 -0700

    Introduce actor automation allowlist
    Bug: 402085565
    Change-Id: I284dd4204e276c44c0bb121bc3044a4cc8b265e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6361826
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Jason Chase <chasej@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435713}

--- chrome/browser/actor/actor_coordinator.cc
+++ chrome/browser/actor/actor_coordinator.cc
@@ -7,23 +7,75 @@
 #include "base/check.h"
 #include "base/feature_list.h"
 #include "base/functional/callback.h"
+#include "base/memory/weak_ptr.h"
+#include "base/notimplemented.h"
+#include "chrome/browser/actor/site_policy.h"
 #include "chrome/browser/ui/tabs/public/tab_interface.h"
 #include "chrome/common/chrome_features.h"
 #include "components/optimization_guide/proto/features/actions_data.pb.h"
+#include "content/public/browser/web_contents.h"
+#include "url/origin.h"
 
 namespace actor {
 
 ActorCoordinator::ActorCoordinator() = default;
 
-ActorCoordinator::~ActorCoordinator() = default;
+ActorCoordinator::~ActorCoordinator() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+}
 
 void ActorCoordinator::Act(
     tabs::TabInterface* tab,
     const optimization_guide::proto::BrowserAction& action,
     ActionResultCallback callback) {
   CHECK(base::FeatureList::IsEnabled(features::kGlicActor));
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  content::WebContents& web_contents = *tab->GetContents();
+
+  MayActOnTab(
+      *tab,
+      base::BindOnce(
+          &ActorCoordinator::OnMayActOnTabResponse,
+          weak_ptr_factory_.GetWeakPtr(), web_contents.GetWeakPtr(), action,
+          web_contents.GetPrimaryMainFrame()->GetLastCommittedOrigin(),
+          std::move(callback)));
+}
+
+void ActorCoordinator::OnMayActOnTabResponse(
+    base::WeakPtr<content::WebContents> web_contents,
+    const optimization_guide::proto::BrowserAction& action,
+    const url::Origin& evaluated_origin,
+    ActionResultCallback callback,
+    bool may_act) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  if (!web_contents) {
+    std::move(callback).Run(/*succeeded=*/false);
+    return;
+  }
+
+  tabs::TabInterface* tab =
+      tabs::TabInterface::GetFromContents(web_contents.get());
+  CHECK(tab);
+
+  if (!evaluated_origin.IsSameOriginWith(
+          web_contents->GetPrimaryMainFrame()->GetLastCommittedOrigin())) {
+    // A cross-origin navigation occurred before we got permission. The result
+    // is no longer applicable. For now just fail.
+    // TODO(mcnee): Handle this gracefully.
+    NOTIMPLEMENTED();
+    std::move(callback).Run(/*succeeded=*/false);
+    return;
+  }
+
+  if (!may_act) {
+    std::move(callback).Run(/*succeeded=*/false);
+    return;
+  }
 
   // TODO(https://crbug.com/402086021): Use actor tool framework.
+  NOTIMPLEMENTED();
   std::move(callback).Run(/*succeeded=*/false);
 }
 
---
+++ chrome/browser/actor/actor_features.h
@@ -0,0 +1,22 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ACTOR_ACTOR_FEATURES_H_
+#define CHROME_BROWSER_ACTOR_ACTOR_FEATURES_H_
+
+#include <string>
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+
+namespace actor {
+
+BASE_DECLARE_FEATURE(kGlicActionAllowlist);
+
+BASE_DECLARE_FEATURE_PARAM(std::string, kAllowlist);
+BASE_DECLARE_FEATURE_PARAM(bool, kAllowlistOnly);
+
+}  // namespace actor
+
+#endif  // CHROME_BROWSER_ACTOR_ACTOR_FEATURES_H_
