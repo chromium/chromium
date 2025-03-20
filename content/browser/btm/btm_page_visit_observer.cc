0640503708e0bcdb88b3efa013e99d05936f5ade
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-03-20 16:07:45 -0700

    Revert "[BTM] Ignore non-CHIPS 3PC accesses in BtmPageVisitObserver"
    This reverts commit 6aae5de019401b0db000ed2689cfcc120b9bb971.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5131009814167552
    
    Sample build with failed test: https://ci.chromium.org/b/8719843562655064449
    Affected test(s):
    [ninja://content/test:content_browsertests/BtmPageVisitObserverBrowserTest.IframeDocumentCookie](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fcontent%2Ftest:content_browsertests%2FBtmPageVisitObserverBrowserTest.IframeDocumentCookie?q=VHash%3A64d8154d6a05e06f)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5131009814167552&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6372644&type=BUG
    
    Original change's description:
    > [BTM] Ignore non-CHIPS 3PC accesses in BtmPageVisitObserver
    >
    > BTM is only enabled when non-CHIPS 3PCs are disabled, so we should
    > mirror that behavior when determining whether a page accessed storage.
    >
    > Bug: 390201794
    > Change-Id: I95c0c34523a57a18f69959251a1f3c20821ac36e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372644
    > Reviewed-by: Andrew Liu <liu@chromium.org>
    > Commit-Queue: Svend Larsen <svend@chromium.org>
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1435669}
    >
    
    Bug: 390201794
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I253cf4fc4de7d8f857fc33cfafb73fc15e0f07b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6377260
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1435749}

--- content/browser/btm/btm_page_visit_observer.cc
+++ content/browser/btm/btm_page_visit_observer.cc
@@ -211,20 +211,6 @@ void BtmPageVisitObserver::OnCookiesAccessed(
     return;
   }
 
-  // Attribute accesses by iframes and other subresources to the first-party
-  // page they're embedded in.
-  const std::optional<GURL> first_party_url =
-      GetFirstPartyURL(render_frame_host);
-  if (!first_party_url.has_value()) {
-    return;
-  }
-  // BTM is only turned on when non-CHIPS 3PCs are blocked, so mirror that
-  // behavior by ignoring non-CHIPS 3PC accesses.
-  if (!HasCHIPS(details.cookie_access_result_list) &&
-      !IsSameSiteForBtm(first_party_url.value(), details.url)) {
-    return;
-  }
-
   // Check to see if this is a late report for a redirect. Only Navigation
   // cookie accesses should be attributed to redirects.
   //
@@ -250,8 +236,10 @@ void BtmPageVisitObserver::OnCookiesAccessed(
 
   // If the cookie was accessed by a subresource request in a now-bfcached
   // page, try to find that page's visit.
+  const GURL& page_url =
+      render_frame_host->GetMainFrame()->GetLastCommittedURL();
   for (VisitTuple& visit : pending_visits_) {
-    if (first_party_url.value() == visit.prev_page.url) {
+    if (page_url == visit.prev_page.url) {
       visit.prev_page.had_qualifying_storage_access = true;
       return;
     }
@@ -268,24 +256,8 @@ void BtmPageVisitObserver::OnCookiesAccessed(
     return;
   }
 
-  bool is_subframe_navigation = !navigation_handle->IsInMainFrame();
-  if (is_subframe_navigation) {
-    const std::optional<GURL> first_party_url =
-        GetFirstPartyURL(navigation_handle);
-    if (!first_party_url.has_value()) {
-      // We want to attribute subframe storage accesses to the top-level page,
-      // but there's no top-level page, so we can't do anything.
-      return;
-    }
-
-    // BTM is only turned on when non-CHIPS 3PCs are blocked, so mirror that
-    // behavior by ignoring non-CHIPS 3PC accesses.
-    if (!HasCHIPS(details.cookie_access_result_list) &&
-        !IsSameSiteForBtm(first_party_url.value(), details.url)) {
-      return;
-    }
-
-    // Attribute subframe storage accesses to the top-level page.
+  if (!navigation_handle->IsInMainFrame()) {
+    // Subframe navigation
     current_page_.had_qualifying_storage_access = true;
     return;
   }
