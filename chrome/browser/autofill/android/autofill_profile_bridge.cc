2c16cd352d75dfd33859b8b049b7cda6983892ec
Timofey Chudakov <tchudakov@google.com>
2025-03-20 02:20:47 -0700

    [Android] Move AutofillProfileBridge to c/b/autofill/android.
    The reason for the move in that the AutofillProfileBridge.java
    file is already in the c/b/autofill/android. There's no reason
    why the autofill_profile_bridge.{h,cc} should be placed outside
    of that directory.
    
    Bug: 402020076
    Change-Id: If667c589b59b3614a0daeac895a8deb2bcbd5bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6372041
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1435296}

--- chrome/browser/autofill/android/autofill_profile_bridge.cc
+++ chrome/browser/autofill/android/autofill_profile_bridge.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/android/preferences/autofill/autofill_profile_bridge.h"
+#include "chrome/browser/autofill/android/autofill_profile_bridge.h"
 
 #include <algorithm>
 
