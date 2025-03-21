6769610a88127ebcfe56f26f697eb7d6114a232e
Andrew Grieve <agrieve@chromium.org>
2025-03-21 11:55:02 -0700

    Add @NullMarked to //chrome/browser/.../settings_promo_card
    Bug: 389129271
    Change-Id: Ic086e5c3064c2ba1a3ac7519afadec266a126dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383056
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436202}

--- chrome/browser/ui/android/settings_promo_card/java/src/org/chromium/chrome/browser/ui/settings_promo_card/SettingsPromoCardPreference.java
+++ chrome/browser/ui/android/settings_promo_card/java/src/org/chromium/chrome/browser/ui/settings_promo_card/SettingsPromoCardPreference.java
@@ -4,19 +4,23 @@
 
 package org.chromium.chrome.browser.ui.settings_promo_card;
 
+import static org.chromium.build.NullUtil.assumeNonNull;
+
 import android.content.Context;
 import android.util.AttributeSet;
 
-import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
 
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.ui.default_browser_promo.DefaultBrowserPromoUtils;
 import org.chromium.components.feature_engagement.Tracker;
 
 /** A preference that displays a settings promo card. */
+@NullMarked
 public class SettingsPromoCardPreference extends Preference {
-    @Nullable private SettingsPromoCardProvider mProvider;
+    private @Nullable SettingsPromoCardProvider mProvider;
 
     /** Construct and initialize SettingsPromoCardPreference to be shown in main settings. */
     public SettingsPromoCardPreference(Context context, AttributeSet attrs, Tracker tracker) {
@@ -37,7 +41,7 @@ public class SettingsPromoCardPreference extends Preference {
         setVisible(false);
 
         if (mProvider != null && mProvider.isPromoShowing()) {
-            mProvider.setUpPromoCardView(holder.findViewById(R.id.promo_card_view));
+            mProvider.setUpPromoCardView(assumeNonNull(holder.findViewById(R.id.promo_card_view)));
             setVisible(true);
         }
     }
