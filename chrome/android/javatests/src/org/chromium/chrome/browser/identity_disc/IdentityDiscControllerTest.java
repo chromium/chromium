0a8b97b03bfcab0d1a031a07518d313b0249e981
Mahmoud Rashad <mmrashad@google.com>
2025-03-20 17:03:21 -0700

    Fix the accessibility string of the ntp avatar with identity error
    Bug: 394669947, 40721874
    Change-Id: I0de6b993ed2417d9730fe8c554cb0ec13588db5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6369180
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1435778}

--- chrome/android/javatests/src/org/chromium/chrome/browser/identity_disc/IdentityDiscControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/identity_disc/IdentityDiscControllerTest.java
@@ -238,12 +238,9 @@ public class IdentityDiscControllerTest {
 
     @Test
     @MediumTest
-    public void testIdentityDiscWithSignin() {
+    public void testIdentityDiscSignedIn() {
         // Identity Disc should be shown on sign-in state change with a NTP refresh.
         mSigninTestRule.addAccountThenSignin(TestAccounts.ACCOUNT1);
-        // TODO(crbug.com/40721874): Remove the reload once the sign-in without sync observer
-        //  is implemented.
-        ThreadUtils.runOnUiThreadBlocking(mTab::reload);
         String expectedContentDescription =
                 mActivityTestRule
                         .getActivity()
@@ -269,17 +266,86 @@ public class IdentityDiscControllerTest {
 
     @Test
     @MediumTest
-    public void testIdentityDiscWithSignin_nonDisplayableEmail() {
+    public void testIdentityDiscSignedIn_nonDisplayableEmail() {
         // Identity Disc should be shown on sign-in state change with a NTP refresh.
         AccountInfo accountInfo = addAndSigninAccountWithNonDisplayableEmail();
-        // TODO(crbug.com/40721874): Remove the reload once the sign-in without sync observer
-        //  is implemented.
-        ThreadUtils.runOnUiThreadBlocking(mTab::reload);
         String expectedContentDescription =
                 mActivityTestRule
                         .getActivity()
                         .getString(
                                 R.string.accessibility_toolbar_btn_identity_disc_with_name,
+                                accountInfo.getFullName());
+        ViewUtils.waitForVisibleView(
+                allOf(
+                        withId(R.id.optional_toolbar_button),
+                        isDisplayed(),
+                        withContentDescription(expectedContentDescription)));
+
+        mSigninTestRule.forceSignOut();
+        ViewUtils.waitForVisibleView(
+                allOf(
+                        withId(R.id.optional_toolbar_button),
+                        isDisplayed(),
+                        withContentDescription(
+                                R.string.accessibility_toolbar_btn_signed_out_identity_disc)));
+    }
+
+    @Test
+    @MediumTest
+    @EnableFeatures(ChromeFeatureList.UNO_PHASE_2_FOLLOW_UP)
+    public void testIdentityDiscWithErrorBadgeSignedIn() {
+        // Fake an identity error.
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    FakeSyncServiceImpl fakeSyncServiceImpl = new FakeSyncServiceImpl();
+                    SyncServiceFactory.setInstanceForTesting(fakeSyncServiceImpl);
+                    fakeSyncServiceImpl.setRequiresClientUpgrade(true);
+                });
+
+        // Identity Disc should be shown on sign-in state change with a NTP refresh.
+        mSigninTestRule.addAccountThenSignin(TestAccounts.ACCOUNT1);
+        String expectedContentDescription =
+                mActivityTestRule
+                        .getActivity()
+                        .getString(
+                                R.string
+                                        .accessibility_toolbar_btn_identity_disc_error_with_name_and_email,
+                                TestAccounts.ACCOUNT1.getFullName(),
+                                TestAccounts.ACCOUNT1.getEmail());
+        ViewUtils.waitForVisibleView(
+                allOf(
+                        withId(R.id.optional_toolbar_button),
+                        isDisplayed(),
+                        withContentDescription(expectedContentDescription)));
+
+        mSigninTestRule.signOut();
+        ViewUtils.waitForVisibleView(
+                allOf(
+                        withId(R.id.optional_toolbar_button),
+                        isDisplayed(),
+                        withContentDescription(
+                                R.string.accessibility_toolbar_btn_signed_out_identity_disc)));
+    }
+
+    @Test
+    @MediumTest
+    @EnableFeatures(ChromeFeatureList.UNO_PHASE_2_FOLLOW_UP)
+    public void testIdentityDiscWithErrorBadgeSignedIn_nonDisplayableEmail() {
+        // Fake an identity error.
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    FakeSyncServiceImpl fakeSyncServiceImpl = new FakeSyncServiceImpl();
+                    SyncServiceFactory.setInstanceForTesting(fakeSyncServiceImpl);
+                    fakeSyncServiceImpl.setRequiresClientUpgrade(true);
+                });
+
+        // Identity Disc should be shown on sign-in state change with a NTP refresh.
+        AccountInfo accountInfo = addAndSigninAccountWithNonDisplayableEmail();
+        String expectedContentDescription =
+                mActivityTestRule
+                        .getActivity()
+                        .getString(
+                                R.string.accessibility_toolbar_btn_identity_disc_error_with_name,
                                 accountInfo.getFullName());
         ViewUtils.waitForVisibleView(
                 allOf(
@@ -468,14 +534,12 @@ public class IdentityDiscControllerTest {
     public void testIdentityDisc_signedIn_unoPhase2FollowUpEnabled_identityErrorExist(
             boolean nightModeEnabled) throws IOException {
         // Fake an identity error.
-        FakeSyncServiceImpl fakeSyncService =
-                ThreadUtils.runOnUiThreadBlocking(
-                        () -> {
-                            FakeSyncServiceImpl fakeSyncServiceImpl = new FakeSyncServiceImpl();
-                            SyncServiceFactory.setInstanceForTesting(fakeSyncServiceImpl);
-                            return fakeSyncServiceImpl;
-                        });
-        fakeSyncService.setRequiresClientUpgrade(true);
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    FakeSyncServiceImpl fakeSyncServiceImpl = new FakeSyncServiceImpl();
+                    SyncServiceFactory.setInstanceForTesting(fakeSyncServiceImpl);
+                    fakeSyncServiceImpl.setRequiresClientUpgrade(true);
+                });
 
         // Sign-in and wait for the user profile image to appear.
         mSigninTestRule.addAccountThenSignin(TestAccounts.ACCOUNT1);
@@ -484,7 +548,7 @@ public class IdentityDiscControllerTest {
                         .getActivity()
                         .getString(
                                 R.string
-                                        .accessibility_toolbar_btn_identity_disc_with_name_and_email,
+                                        .accessibility_toolbar_btn_identity_disc_error_with_name_and_email,
                                 TestAccounts.ACCOUNT1.getFullName(),
                                 TestAccounts.ACCOUNT1.getEmail());
         ViewUtils.waitForVisibleView(
