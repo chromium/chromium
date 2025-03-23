435e8aac6ec3987223f47f7ed3cca37504a38c7f
Jan Keitel <jkeitel@google.com>
2025-03-22 20:03:16 -0700

    Remove kAutofillPredictionImprovementsEnabled pref
    The pref was never used outside of Teamfood and therefore we do not
    add migration/clean-up code.
    
    Fixed: 404485362
    Change-Id: I00f2973cae6ae42f94a4f55ac23c2fa27fea292a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6381748
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1436533}

--- components/autofill/core/common/autofill_prefs.cc
+++ components/autofill/core/common/autofill_prefs.cc
@@ -104,8 +104,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
     BUILDFLAG(IS_CHROMEOS)
-  registry->RegisterBooleanPref(prefs::kAutofillPredictionImprovementsEnabled,
-                                false);
   registry->RegisterBooleanPref(
       prefs::kAutofillBnplEnabled, true,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
