3eac73aa297e57a1d6e17bec25973848d16e91a4
Lukasz Anforowicz <lukasza@chromium.org>
2025-03-24 15:12:42 -0700

    [rust] Remove unneeded, grandparented-in, direct Rust crate deps.
    This CL removes dependencies from
    `third_party/rust/chromium_crates_io/Cargo.toml` that have been
    grand-parented-in in https://crrev.com/c/5019147, but that do not seem
    to have any direct dependencies from Chromium targets.
    
    The CL has been created by:
    
    * Manually editing `third_party/rust/chromium_crates_io/Cargo.toml`
      and `third_party/rust/chromium_crates_io/gnrt_config.toml`
    * Running:
        - `tools/crates/run_gnrt.py vendor`
        - `tools/crates/run_gnrt.py gen`
    
    Bug: 405468274
    Change-Id: I398efbc6570134df156302bba98dec4c8e6d7def
    Fixed: 378273233
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384041
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437127}

--- third_party/rust/chromium_crates_io/vendor/rstest-0.22.0/tests/resources/rstest/matrix/simple.rs
+++
@@ -1,9 +0,0 @@
-use rstest::rstest;
-
-#[rstest(
-    expected => [4, 2*3-2],
-    input => ["ciao", "buzz"],
-)]
-fn strlen_test(expected: usize, input: &str) {
-    assert_eq!(expected, input.len());
-}
