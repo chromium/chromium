9c752656bb7cb6aed276e3abed3d48fecd38a7b5
Ryo Hashimoto <hashimoto@chromium.org>
2025-03-23 17:39:10 -0700

    devtools: Build DevToolsWindow for Android
    BUG=b:400857544
    
    Change-Id: Ifed884fe6f1209b464007b528079c55580fbd63c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382212
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436648}

---
+++ chrome/browser/devtools/devtools_window_android.cc
@@ -0,0 +1,36 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/devtools/devtools_window.h"
+
+#include "content/public/browser/keyboard_event_processing_result.h"
+
+using content::WebContents;
+
+content::KeyboardEventProcessingResult DevToolsWindow::PreHandleKeyboardEvent(
+    WebContents* source,
+    const input::NativeWebKeyboardEvent& event) {
+  return content::KeyboardEventProcessingResult::NOT_HANDLED;
+}
+
+bool DevToolsWindow::HandleKeyboardEvent(
+    WebContents* source,
+    const input::NativeWebKeyboardEvent& event) {
+  return false;
+}
+
+std::unique_ptr<content::EyeDropper> DevToolsWindow::OpenEyeDropper(
+    content::RenderFrameHost* render_frame_host,
+    content::EyeDropperListener* listener) {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+void DevToolsWindow::UpdateBrowserToolbar() {
+  NOTIMPLEMENTED();
+}
+
+void DevToolsWindow::UpdateBrowserWindow() {
+  NOTIMPLEMENTED();
+}
