b8f80176b1636154c6bfc85a607b05cc3aec50cb
Alex Gough <ajgo@chromium.org>
2025-03-24 09:04:37 -0700

    Avoid receiving or sending sentinel handle values
    These values can be misinterpreted by OS functions, so
    avoid sending or receiving them over IPCZ.
    
    (cherry picked from commit 36dbbf38697dd1e23ef8944bb9e57f6e0b3d41ec)
    
    Bug: 405143032
    Change-Id: Ib578fb4727e78e2697c60c42005daa97e08695e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6380193
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1436135}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383569
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/6998@{#2315}
    Cr-Branched-From: de9c6fafd8ae5c6ea0438764076ca7d04a0b165d-refs/heads/main@{#1415337}

--- base/win/win_util.h
+++ base/win/win_util.h
@@ -49,6 +49,25 @@ struct NativeLibraryLoadError;
 
 namespace win {
 
+inline bool IsPseudoHandle(HANDLE h) {
+  // Note that there appears to be no official documentation covering the
+  // existence of specific pseudo handle values. In practice it's clear that
+  // e.g. -1 is the current process, -2 is the current thread, etc. The largest
+  // negative value known to be an issue with DuplicateHandle in fuzzers is
+  // -12.
+  //
+  // Note that there is virtually no risk of a real handle value falling within
+  // this range and being misclassified as a pseudo handle.
+  //
+  // Cast through uintptr_t and then unsigned int to make the truncation to
+  // 32 bits explicit. Handles are size of-pointer but are always 32-bit values.
+  // https://msdn.microsoft.com/en-us/library/aa384203(VS.85).aspx says:
+  // 64-bit versions of Windows use 32-bit handles for interoperability.
+  constexpr int kMinimumKnownPseudoHandleValue = -12;
+  const auto value = static_cast<int32_t>(reinterpret_cast<uintptr_t>(h));
+  return value < 0 && value >= kMinimumKnownPseudoHandleValue;
+}
+
 inline uint32_t HandleToUint32(HANDLE h) {
   // Cast through uintptr_t and then unsigned int to make the truncation to
   // 32 bits explicit. Handles are size of-pointer but are always 32-bit values.
