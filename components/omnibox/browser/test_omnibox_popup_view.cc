d07ef3fedd422bd5bf419d26f30e8d5ecf3d9aff
Alex Chen <alexwchen@chromium.org>
2025-03-24 10:16:42 -0700

    [omnibox][search aggregator] Show content type icon in location bar
    Updates OmniboxView to use the match icon URL for setting the omnibox
    match icon. This allows for content type aggregator suggestions to
    show the icon in the omnibox as well.
    
    Note: Since the match icon relies on GetPopupRichSuggestionBitmap, the
    icon is cleared whenever the popup is closed. As a result, the icon
    will revert to the default globe icon when the popup is closed. Fixing
    that behavior is low priority and is tracked in b:404335159.
    
    Screenshot: https://screenshot.googleplex.com/7cSk4q8tdUDe635
    
    Bug: 401229824
    Change-Id: Ied8fbd22ab4be45c114f954ffe0be00106aa930a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6335491
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Alex Chen <alexwchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436938}

---
+++ components/omnibox/browser/test_omnibox_popup_view.cc
@@ -0,0 +1,9 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/omnibox/browser/test_omnibox_popup_view.h"
+
+bool TestOmniboxPopupView::IsOpen() const {
+  return false;
+}
