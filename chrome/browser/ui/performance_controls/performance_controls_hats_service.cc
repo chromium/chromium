3d088eadbb0b8661cb44dc74de49171be759dbb1
Joe Mason <joenotcharles@google.com>
2025-03-24 16:14:53 -0700

    Add HaTS config for Performance Controls PPM survey
    This survey will open when the user visits the New Tab Page, after a
    random delay between 2 and 60 minutes after starting Chrome. It's
    similar to the general performance survey, but will be linked to UMA
    so that user reports of poor performance can be correlated with
    metrics that could be used to detect Poor Performance Moments.
    
    Design doc: go/performance-hats-ppm-plan
    
    Bug: 404915122
    Change-Id: I1f01bb7ec09601a91aeb52a8b12270d17ed4228f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6384143
    Commit-Queue: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437176}

--- chrome/browser/ui/performance_controls/performance_controls_hats_service.cc
+++ chrome/browser/ui/performance_controls/performance_controls_hats_service.cc
@@ -4,9 +4,15 @@
 
 #include "chrome/browser/ui/performance_controls/performance_controls_hats_service.h"
 
+#include <algorithm>
+#include <string>
+
+#include "base/check.h"
 #include "base/feature_list.h"
-#include "base/logging.h"
+#include "base/functional/callback_helpers.h"
+#include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/performance_manager/public/user_tuning/battery_saver_mode_manager.h"
 #include "chrome/browser/performance_manager/public/user_tuning/user_performance_tuning_manager.h"
@@ -15,8 +21,16 @@
 #include "chrome/browser/ui/hats/hats_service_factory.h"
 #include "components/performance_manager/public/features.h"
 
+using performance_manager::features::kPerformanceControlsPPMSurvey;
+using performance_manager::features::kPerformanceControlsPPMSurveyMaxDelay;
+using performance_manager::features::kPerformanceControlsPPMSurveyMinDelay;
+
 PerformanceControlsHatsService::PerformanceControlsHatsService(Profile* profile)
-    : profile_(profile) {
+    : profile_(profile),
+      delay_before_ppm_survey_(
+          base::RandTimeDelta(kPerformanceControlsPPMSurveyMinDelay.Get(),
+                              kPerformanceControlsPPMSurveyMaxDelay.Get())) {
+  CHECK(delay_before_ppm_survey_.is_positive());
   if (base::FeatureList::IsEnabled(
           performance_manager::features::
               kPerformanceControlsMemorySaverOptOutSurvey)) {
@@ -64,37 +78,45 @@ void PerformanceControlsHatsService::OpenedNewTabPage() {
                                      UserPerformanceTuningManager::GetInstance()
                                          ->IsMemorySaverModeActive();
 
+  auto launch_survey_if_enabled =
+      [hats_service, battery_saver_mode, memory_saver_mode](
+          const base::Feature& feature, const std::string& trigger) {
+        if (base::FeatureList::IsEnabled(feature)) {
+          hats_service->LaunchSurvey(
+              trigger, base::DoNothing(), base::DoNothing(),
+              {{"Memory Saver Mode Enabled", memory_saver_mode},
+               {"Battery Saver Mode Enabled", battery_saver_mode}},
+              {});
+        }
+      };
+
   // A general performance survey for all users.
-  if (base::FeatureList::IsEnabled(performance_manager::features::
-                                       kPerformanceControlsPerformanceSurvey)) {
-    hats_service->LaunchSurvey(kHatsSurveyTriggerPerformanceControlsPerformance,
-                               base::DoNothing(), base::DoNothing(),
-                               {{"high_efficiency_mode", memory_saver_mode},
-                                {"battery_saver_mode", battery_saver_mode}},
-                               {});
+  launch_survey_if_enabled(
+      performance_manager::features::kPerformanceControlsPerformanceSurvey,
+      kHatsSurveyTriggerPerformanceControlsPerformance);
+
+  // Survey to correlate UMA metrics with Poor Performance Moments.
+  if (MayLaunchPPMSurvey()) {
+    launch_survey_if_enabled(kPerformanceControlsPPMSurvey,
+                             kHatsSurveyTriggerPerformanceControlsPPM);
   }
 
-// ChromeOS defaults to the OS battery saver so this survey isn't relevant.
-#if !BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS)
+  // ChromeOS defaults to the OS battery saver so this survey isn't relevant.
+#else
   base::Time last_battery_timestamp =
       performance_manager::user_tuning::BatterySaverModeManager::GetInstance()
           ->GetLastBatteryUsageTimestamp();
 
   // A battery performance survey for users with a battery-powered device.
-  if (base::FeatureList::IsEnabled(
-          performance_manager::features::
-              kPerformanceControlsBatteryPerformanceSurvey) &&
-      (base::Time::Now() - last_battery_timestamp) <=
-          performance_manager::features::
-              kPerformanceControlsBatterySurveyLookback.Get()) {
-    hats_service->LaunchSurvey(
-        kHatsSurveyTriggerPerformanceControlsBatteryPerformance,
-        base::DoNothing(), base::DoNothing(),
-        {{"high_efficiency_mode", memory_saver_mode},
-         {"battery_saver_mode", battery_saver_mode}},
-        {});
+  if (base::Time::Now() - last_battery_timestamp <=
+      performance_manager::features::kPerformanceControlsBatterySurveyLookback
+          .Get()) {
+    launch_survey_if_enabled(
+        performance_manager::features::
+            kPerformanceControlsBatteryPerformanceSurvey,
+        kHatsSurveyTriggerPerformanceControlsBatteryPerformance);
   }
-
 #endif
 }
 
@@ -129,4 +151,20 @@ void PerformanceControlsHatsService::OnMemorySaverModeChanged() {
     hats_service->LaunchDelayedSurvey(
         kHatsSurveyTriggerPerformanceControlsMemorySaverOptOut, 10000);
   }
+}
+
+bool PerformanceControlsHatsService::MayLaunchPPMSurvey() const {
+  base::TimeDelta current_delay = base::Time::Now() - profile_->GetStartTime();
+  if (current_delay < delay_before_ppm_survey_) {
+    return false;
+  }
+  // Allow an extra minute in case the random delay is right at the end of the
+  // range.
+  base::TimeDelta max_delay =
+      std::max(kPerformanceControlsPPMSurveyMaxDelay.Get(),
+               delay_before_ppm_survey_ + base::Minutes(1));
+  if (current_delay > max_delay) {
+    return false;
+  }
+  return true;
 }
