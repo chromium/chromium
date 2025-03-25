9017162286a2ebb2f44b43ef46f0d447bb276613
Joshua Pawlicki <waffles@chromium.org>
2025-03-25 07:22:43 -0700

    [M126-LTS] Reland "lzma_sdk: Update to 24.09."
    M126 merge issues:
      Conflict in README.chromium file: different license types
    
    This is a reland of commit 1d69891ae775c74724558585929c89438a6fda93
    
    Original change's description:
    > lzma_sdk: Update to 24.09.
    >
    > Files originate from https://7-zip.org/a/lzma2409.7z
    > SHA-256(./lzma2409.7z)=
    > 79b39f10b7b69eea293caa90c3e7ea07faf8f01f8ae9db1bb1b90c092375e5f3
    >
    > Fixed: 383772517, 40849176
    > Change-Id: I7ccefbcc0dc92f94ae43566fe9d8b962369ea8b8
    > Low-Coverage-Reason: OTHER - It's not clear how to exercise some of these conditionally-compiled alternatives.
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091038
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1404227}
    
    (cherry picked from commit 968d9282d215058bc19a7c0cf22f2dcdea153740)
    
    Bug: 383772517, 388538957
    Include-Ci-Only-Tests: true
    No-Presubmit: true
    Change-Id: I3f36e233d52c0ba560a5d740111d1db62a32b8fc
    Low-Coverage-Reason: OTHER - It's not clear how to exercise some of these conditionally-compiled alternatives.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6163689
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1404814}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6186230
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Roger Felipe Zanoni da Silva (xWF) <rzanoni@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6478@{#2039}
    Cr-Branched-From: e6143acc03189c5e52959545b110d6d17ecd5286-refs/heads/main@{#1300313}

--- third_party/lzma_sdk/google/seven_zip_reader_unittest.cc
+++ third_party/lzma_sdk/google/seven_zip_reader_unittest.cc
@@ -43,6 +43,7 @@
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "build/build_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -352,8 +353,15 @@ class SevenZipReaderFakeCrcTableTest : public testing::Test {
   std::array<uint32_t, 2048> crc_table_;
 };
 
+// TODO(crbug.com/388538957): Make this work on ARM64, which uses a different
+// number of tables and the crc32b instruction.
+#if defined(ARCH_CPU_ARM64)
+#define MAYBE_EmptyCrcWithFakeTable DISABLED_EmptyCrcWithFakeTable
+#else
+#define MAYBE_EmptyCrcWithFakeTable EmptyCrcWithFakeTable
+#endif
 // This is useful functionality for the fuzzer, so we test it here.
-TEST_F(SevenZipReaderFakeCrcTableTest, EmptyCrcWithFakeTable) {
+TEST_F(SevenZipReaderFakeCrcTableTest, MAYBE_EmptyCrcWithFakeTable) {
   base::File file = OpenTestFile(FILE_PATH_LITERAL("fake_crc_table.7z"));
   ASSERT_TRUE(file.IsValid());
 
