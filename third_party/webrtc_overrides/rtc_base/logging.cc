57368cb688f57953997281524e3a9733c535393b
Collin Baker <collinbaker@chromium.org>
2025-03-25 11:29:16 -0700

    Reland "Use string_view in logging and trace_event location"
    This is a reland of commit 5439f8eb1e170ce515a12e211b5d20a2bff75ea5
    
    Original change's description:
    > Use string_view in logging and trace_event location
    >
    > Changes the LogMessage class to use a string_view for file names. This
    > required a matching change in trace_event: a const char* string was
    > passed from logging code, but there is no safe way to get a const
    > char* from a string_view since the latter may not be null-terminated.
    >
    > Many clients of base/logging.h are updated to use string_view as well.
    >
    > Code which attempts to pass a string_view to a LogMessage by its data
    > pointer already exists:
    > https://source.chromium.org/chromium/chromium/src/+/main:ash/quick_pair/common/logging.cc;l=49;drc=c6c99d03b1d2f4fab91d6be8665f81b540690c73
    > https://source.chromium.org/chromium/chromium/src/+/main:components/cross_device/logging/logging.cc;l=30;drc=00704dbac4f63b2476aac319572ffc42c9b71fc2
    >
    > Desire for Rust logging support prompted this change. In principle a
    > Rust log facility can be a thin wrapper around the base logging
    > implementation, but Rust has almost no support for null-terminated
    > strings. Instead, Rust provides a string_view equivalent built in to
    > the language. This change enables Rust code to pass file names
    > obtained by language-specific macros to the C++ implementation.
    >
    > Bug: None
    > Change-Id: I21b4f1c945b70d54f66d80adf3dcda1fe5a39f71
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765705
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Auto-Submit: Collin Baker <collinbaker@chromium.org>
    > Commit-Queue: Collin Baker <collinbaker@chromium.org>
    > Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Owners-Override: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1436283}
    
    Bug: None
    Change-Id: I725bac881a160caa6d97e7480bc4047b460fae34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387020
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437625}

--- third_party/webrtc_overrides/rtc_base/logging.cc
+++ third_party/webrtc_overrides/rtc_base/logging.cc
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include <atomic>
 #include <iomanip>
+#include <string_view>
 
 #include "base/logging.h"
 #include "base/notreached.h"
@@ -226,7 +227,7 @@ bool CheckVlogIsOnHelper(LoggingSeverity severity,
                          const char* file,
                          size_t N) {
   return WebRtcVerbosityLevel(severity) <=
-         ::logging::GetVlogLevelHelper(file, N);
+         ::logging::GetVlogLevelHelper(std::string_view(file, N - 1));
 }
 
 }  // namespace webrtc
