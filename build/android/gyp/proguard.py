0991431d875d18c6bb7453e4fdc0c241bd1a251f
Andrew Grieve <agrieve@chromium.org>
2025-03-25 14:12:32 -0700

    Reland #2: "Remove old proguard flag keeping enum values"
    This reverts commit d876368587fcf810b85948608ad4107ad6e286a6.
    
    Reason for reland: Fixed typo in flag name
    
    Original change's description:
    > Revert "Reland: Remove old proguard flag keeping enum values"
    >
    > This reverts commit ab757a71c19d278447ff745a94fae1b79eb06b39.
    >
    > Reason for revert: b/405202493
    >
    > Original change's description:
    > > Reland: Remove old proguard flag keeping enum values
    > >
    > > This reverts commit 214b6907bc072039ee5e68b5411d20f73da5529b.
    > >
    > > Reason for reland:
    > > R8 updated to recognize Enums serialized to Bundles
    > >
    > > Bug: 204939965
    > > Change-Id: Ie9095bdaadd051bd0e800928ccd2c18f450241b5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6214450
    > > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Sam Maier <smaier@chromium.org>
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1415598}
    >
    > Bug: 204939965, 405202493
    > Change-Id: Ib9a35bd0b4debd6727b1e657bc0022e1bb15601a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6392916
    > Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1437680}
    
    Bug: 204939965, 405202493
    Change-Id: I4da3743633578026295b9007bf00f1e5d86f4ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6393796
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437730}

--- build/android/gyp/proguard.py
+++ build/android/gyp/proguard.py
@@ -355,9 +355,9 @@ def _OptimizeWithR8(options, config_paths, libraries, dynamic_config_data):
         '-Dcom.android.tools.r8.allowCodeReplacement=false',
         # Required to use "-keep,allowcodereplacement"
         '-Dcom.android.tools.r8.allowTestProguardOptions=true',
-        # Can remove this once the pass is enabled by default.
-        # b/145280859
-        '-Dcom.android.tools.r8.enableListIterationRewriting=1',
+        # Needed because we don't add an unconditional -keep for Enum.values()
+        # methods. http://b/204939965
+        '-Dcom.android.tools.r8.experimentalTraceAndroidEnumSerialization=1',
     ]
     if options.sdk_extension_jars:
       # Enable API modelling for OS extensions. https://b/326252366
