2dbd1689b17cfdef199bcd206f96e6da3a228f87
Daseul Lee <dslee@chromium.org>
2025-03-25 10:21:25 -0700

    Writing Assistance API: Add create client base template
    Move the logic to create mojo client to the new shared template
    `AIWritingAssistanceCreateClient`. This will help removing dependency
    of create logic from factory classes.
    
    Bug: 381969447, 402442890
    Change-Id: I0d4edac016d79f7740c7ff92e601103e69b24232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6383305
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437581}

--- third_party/blink/renderer/modules/ai/ai_summarizer_factory.cc
+++ third_party/blink/renderer/modules/ai/ai_summarizer_factory.cc
@@ -10,121 +10,52 @@
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ai_create_monitor_callback.h"
 #include "third_party/blink/renderer/core/dom/abort_signal.h"
 #include "third_party/blink/renderer/modules/ai/ai.h"
-#include "third_party/blink/renderer/modules/ai/ai_context_observer.h"
 #include "third_party/blink/renderer/modules/ai/ai_create_monitor.h"
 #include "third_party/blink/renderer/modules/ai/ai_metrics.h"
 #include "third_party/blink/renderer/modules/ai/ai_summarizer.h"
 #include "third_party/blink/renderer/modules/ai/ai_utils.h"
+#include "third_party/blink/renderer/modules/ai/ai_writing_assistance_create_client.h"
 #include "third_party/blink/renderer/modules/ai/exception_helpers.h"
-#include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
 
 namespace blink {
 
 namespace {
 
-class CreateSummarizerClient
-    : public GarbageCollected<CreateSummarizerClient>,
-      public AIContextObserver<AISummarizer>,
-      public mojom::blink::AIManagerCreateSummarizerClient {
+class CreateSummarizerClient final
+    : public AIWritingAssistanceCreateClient<
+          mojom::blink::AISummarizer,
+          mojom::blink::AIManagerCreateSummarizerClient,
+          AISummarizerCreateOptions,
+          AISummarizer> {
  public:
-  explicit CreateSummarizerClient(
-      ScriptState* script_state,
-      AI* ai,
-      ScriptPromiseResolver<AISummarizer>* resolver,
-      AbortSignal* signal,
-      AISummarizerCreateOptions* options,
-      scoped_refptr<base::SequencedTaskRunner> task_runner)
-      : AIContextObserver(script_state, ai, resolver, signal),
-        ai_(ai),
-        receiver_(this, ai->GetExecutionContext()),
-        options_(options) {
+  CreateSummarizerClient(ScriptState* script_state,
+                         AI* ai,
+                         ScriptPromiseResolver<AISummarizer>* resolver,
+                         AISummarizerCreateOptions* options)
+      : AIWritingAssistanceCreateClient(script_state, ai, resolver, options) {
     if (options->hasMonitor()) {
       monitor_ = MakeGarbageCollected<AICreateMonitor>(
-          ai->GetExecutionContext(), task_runner);
+          ai->GetExecutionContext(), ai->GetTaskRunner());
       std::ignore = options->monitor()->Invoke(nullptr, monitor_);
-      ai_->GetAIRemote()->AddModelDownloadProgressObserver(
+      ai->GetAIRemote()->AddModelDownloadProgressObserver(
           monitor_->BindRemote());
     }
   }
 
-  ~CreateSummarizerClient() override = default;
+  void Trace(Visitor* visitor) const override {
+    visitor->Trace(monitor_);
+    AIWritingAssistanceCreateClient::Trace(visitor);
+  }
 
-  void CreateSummarizer() {
-    mojo::PendingRemote<mojom::blink::AIManagerCreateSummarizerClient>
-        client_remote;
-    receiver_.Bind(client_remote.InitWithNewPipeAndPassReceiver(),
-                   ai_->GetTaskRunner());
+  // AIWritingAssistanceCreateClient:
+  void RemoteCreate(
+      mojo::PendingRemote<mojom::blink::AIManagerCreateSummarizerClient>
+          client_remote) override {
     ai_->GetAIRemote()->CreateSummarizer(
         std::move(client_remote), ToMojoSummarizerCreateOptions(options_));
   }
 
-  void Trace(Visitor* visitor) const override {
-    AIContextObserver::Trace(visitor);
-    visitor->Trace(ai_);
-    visitor->Trace(receiver_);
-    visitor->Trace(options_);
-    visitor->Trace(monitor_);
-  }
-
-  void OnResult(mojo::PendingRemote<mojom::blink::AISummarizer>
-                    remote_summarizer) override {
-    if (!GetResolver()) {
-      // The creation was aborted by the user.
-      return;
-    }
-    if (!ai_->GetExecutionContext() || !remote_summarizer) {
-      GetResolver()->RejectWithDOMException(
-          DOMExceptionCode::kInvalidStateError,
-          kExceptionMessageUnableToCreateSession);
-    } else {
-      AISummarizer* summarizer = MakeGarbageCollected<AISummarizer>(
-          ai_->GetExecutionContext(), ai_->GetTaskRunner(),
-          std::move(remote_summarizer), options_);
-      GetResolver()->Resolve(summarizer);
-    }
-    Cleanup();
-  }
-
-  void OnError(mojom::blink::AIManagerCreateClientError error) override {
-    if (!GetResolver()) {
-      return;
-    }
-
-    using mojom::blink::AIManagerCreateClientError;
-
-    switch (error) {
-      case AIManagerCreateClientError::kUnableToCreateSession:
-      case AIManagerCreateClientError::kUnableToCalculateTokenSize: {
-        GetResolver()->RejectWithDOMException(
-            DOMExceptionCode::kInvalidStateError,
-            kExceptionMessageUnableToCreateSession);
-        break;
-      }
-      case AIManagerCreateClientError::kInitialInputTooLarge: {
-        GetResolver()->RejectWithDOMException(
-            DOMExceptionCode::kQuotaExceededError,
-            kExceptionMessageInputTooLarge);
-        break;
-      }
-      case AIManagerCreateClientError::kUnsupportedLanguage: {
-        GetResolver()->RejectWithDOMException(
-            DOMExceptionCode::kNotSupportedError,
-            kExceptionMessageUnsupportedLanguages);
-        break;
-      }
-    }
-    Cleanup();
-  }
-
-  void ResetReceiver() override { receiver_.reset(); }
-
  private:
-  Member<AI> ai_;
-  HeapMojoReceiver<mojom::blink::AIManagerCreateSummarizerClient,
-                   CreateSummarizerClient>
-      receiver_;
-
-  Member<AISummarizerCreateOptions> options_;
   Member<AICreateMonitor> monitor_;
 };
 
@@ -202,9 +133,9 @@ ScriptPromise<AISummarizer> AISummarizerFactory::create(
     return promise;
   }
 
-  MakeGarbageCollected<CreateSummarizerClient>(
-      script_state, ai_.Get(), resolver, signal, options, task_runner_)
-      ->CreateSummarizer();
+  MakeGarbageCollected<CreateSummarizerClient>(script_state, ai_, resolver,
+                                               options)
+      ->Create();
   return promise;
 }
 
