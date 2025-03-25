d61bb8dbc658963df8ea9df912e70a0a9976fa39
mikt <mikt@google.com>
2025-03-24 22:46:47 -0700

    [PA] Add base::CheckHeapIntegrity to investigate corruption
    Adds an utility function to check heap corruption.
    It performs some checks which were designed to fire at `free()`.
    The utility allows to crash earlier to better understand where the
    corruption happened.
    
    Bug: 371135823, 346464587
    Change-Id: Ia33f66fa48d8e11d37c7c4e123da6303990f2d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6332160
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1437314}

--- base/allocator/partition_allocator/src/partition_alloc/in_slot_metadata.h
+++ base/allocator/partition_allocator/src/partition_alloc/in_slot_metadata.h
@@ -301,6 +301,15 @@ class PA_COMPONENT_EXPORT(PARTITION_ALLOC) InSlotMetadata {
     return alive;
   }
 
+  // Assertion to allocation which ought to be alive.
+  PA_ALWAYS_INLINE void EnsureAlive() {
+    CountType count = count_.load(std::memory_order_relaxed);
+    if (!(count & kMemoryHeldByAllocatorBit)) {
+      DoubleFreeOrCorruptionDetected(count);
+    }
+    CheckCookieIfSupported();
+  }
+
   // Called when a raw_ptr is not banning dangling ptrs, but the user still
   // wants to ensure the pointer is not currently dangling. This is currently
   // used in UnretainedWrapper to make sure callbacks are not invoked with
