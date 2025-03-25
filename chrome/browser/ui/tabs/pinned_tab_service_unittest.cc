e097c73e2454e3ec59a3d63503e450fe65e34562
Joel Hockey <joelhockey@chromium.org>
2025-03-24 22:50:48 -0700

    Revert "[tabs] Convert PinnedTabServiceUnitTest to BrowserTest"
    This reverts commit a48f1e4093b392baed20d9779706efbbfda4a019.
    
    Reason for revert: test is failing
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/49873/blamelist
    
    Original change's description:
    > [tabs] Convert PinnedTabServiceUnitTest to BrowserTest
    >
    > Per guidelines in BrowserWithTestWindowTest, PinnedTabServiceUnitTest is being upgraded to a BrowserTest because it gives us all the utility we need. This prevents us from having to mock things the browser would do.
    >
    > Bug: 384547121
    > Change-Id: I01d00a7c6e745aa36cc854b1fc815f0c13ff84c2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6368778
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Commit-Queue: Sana Akbani <sanaakbani@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1437027}
    
    Bug: 384547121
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I3cafe9a15680439d798c7e123b096c4d6bb79364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6390236
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1437316}

---
+++ chrome/browser/ui/tabs/pinned_tab_service_unittest.cc
@@ -0,0 +1,82 @@
+// Copyright 2012 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/tabs/pinned_tab_service.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/functional/bind.h"
+#include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
+#include "chrome/browser/ui/tabs/pinned_tab_codec.h"
+#include "chrome/browser/ui/tabs/pinned_tab_service_factory.h"
+#include "chrome/browser/ui/tabs/pinned_tab_test_utils.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/test/base/testing_profile.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+std::unique_ptr<KeyedService> BuildPinnedTabService(
+    content::BrowserContext* profile) {
+  return std::make_unique<PinnedTabService>(static_cast<Profile*>(profile));
+}
+
+PinnedTabService* BuildForProfile(Profile* profile) {
+  return static_cast<PinnedTabService*>(
+      PinnedTabServiceFactory::GetInstance()->SetTestingFactoryAndUse(
+          profile, base::BindRepeating(&BuildPinnedTabService)));
+}
+
+class PinnedTabServiceTest : public BrowserWithTestWindowTest {
+ public:
+  PinnedTabServiceTest() : pinned_tab_service_(nullptr) {}
+  PinnedTabServiceTest(const PinnedTabServiceTest&) = delete;
+  PinnedTabServiceTest& operator=(const PinnedTabServiceTest&) = delete;
+
+ protected:
+  TestingProfile* CreateProfile(const std::string& profile_name) override {
+    TestingProfile* profile =
+        BrowserWithTestWindowTest::CreateProfile(profile_name);
+    pinned_tab_service_ = BuildForProfile(profile);
+    return profile;
+  }
+
+ private:
+  raw_ptr<PinnedTabService, DanglingUntriaged> pinned_tab_service_;
+};
+
+// Makes sure closing a popup triggers writing pinned tabs.
+TEST_F(PinnedTabServiceTest, Popup) {
+  GURL url("http://www.google.com");
+  AddTab(browser(), url);
+  browser()->tab_strip_model()->SetTabPinned(0, true);
+
+  // Create a popup.
+  Browser::CreateParams params(Browser::TYPE_POPUP, profile(), true);
+  std::unique_ptr<Browser> popup(CreateBrowserWithTestWindowForParams(params));
+
+  // Close the browser. This should trigger saving the tabs. No need to destroy
+  // the browser (this happens automatically in the test destructor).
+  browser()->OnWindowClosing();
+
+  std::string result = PinnedTabTestUtils::TabsToString(
+      PinnedTabCodec::ReadPinnedTabs(profile()));
+  EXPECT_EQ("http://www.google.com/:pinned", result);
+
+  // Close the popup. This shouldn't reset the saved state.
+  popup->tab_strip_model()->CloseAllTabs();
+  popup.reset();
+
+  // Check the state to make sure it hasn't changed.
+  result = PinnedTabTestUtils::TabsToString(
+      PinnedTabCodec::ReadPinnedTabs(profile()));
+  EXPECT_EQ("http://www.google.com/:pinned", result);
+}
+
+}  // namespace
