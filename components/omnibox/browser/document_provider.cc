35381eda9feedb3fb20c62a88099f25112e6a7fc
Ananya <ananyaseelam@google.com>
2025-03-26 11:23:07 -0700

    [search aggregator] Localize content suggestion metadata order
    This CL localizes metadata in content suggestions, which includes the
    last updated time, the owner of the content, and the mime type. This is
    done by adding translation string templates to `omnibox_strings.grdp`
    for each combination of metadata that could exist next to a content
    suggestion (Ex. last updated and owner, owner and mime_type,etc). This
    is done similar to `DocumentProvider::GetMatchDescription()`.
    
    This CL also sets `mime_description` to empty if the `mime_type` does
    not exist in kMimeTypeMapping whereas the previous code would set
    `file_type_description` to `mime_type`.
    
    Before: https://screenshot.googleplex.com/AL7vUo9ZnTAoeAY After:
    https://screenshot.googleplex.com/4yNBXpCHn7YC4qf
    
    Bug: 403265309, 402436108
    Change-Id: I88249a42c455d8d475c275c500d8cb4f5a4d0214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6374130
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438274}

--- components/omnibox/browser/document_provider.cc
+++ components/omnibox/browser/document_provider.cc
@@ -709,16 +709,16 @@ std::u16string DocumentProvider::GetMatchDescription(
         GenerateLastModifiedString(update_time, base::Time::Now());
     return owner.empty()
                ? l10n_util::GetStringFUTF16(
-                     IDS_DRIVE_SUGGESTION_DESCRIPTION_TEMPLATE_WITHOUT_OWNER,
+                     IDS_CONTENT_SUGGESTION_DESCRIPTION_TEMPLATE_WITHOUT_OWNER,
                      date_desc, mime_desc)
                : l10n_util::GetStringFUTF16(
-                     IDS_DRIVE_SUGGESTION_DESCRIPTION_TEMPLATE, date_desc,
+                     IDS_CONTENT_SUGGESTION_DESCRIPTION_TEMPLATE, date_desc,
                      base::UTF8ToUTF16(owner), mime_desc);
   }
   return owner.empty()
              ? std::move(mime_desc)
              : l10n_util::GetStringFUTF16(
-                   IDS_DRIVE_SUGGESTION_DESCRIPTION_TEMPLATE_WITHOUT_DATE,
+                   IDS_CONTENT_SUGGESTION_DESCRIPTION_TEMPLATE_WITHOUT_DATE,
                    base::UTF8ToUTF16(owner), mime_desc);
 }
 
