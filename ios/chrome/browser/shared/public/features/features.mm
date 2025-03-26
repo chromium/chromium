7f1286ee4f81d5bda18e3f0daee51ba2cbd41af6
adamta <adamta@google.com>
2025-03-26 09:40:06 -0700

    [iOS] IPH Gesture Recognition Improvements
    Adds 2 experiment params to the IPH ablation feature:
    1. CancelTouchesInViewForIPH: Prevents touches from being queued when
    dismissing an IPH. This is to validate a theory and is probably not
    shippable. This prevents certain NTP actions from firing off, such as
    tapping a feed article, since it will instead be used to dismiss the
    IPH. However, most touches continue to be recognized since they last
    for longer than 1 event (like a swipe).
    2. IPHGestureRecognitionImprovement: Improves the handling of touch
    events after the recognizers have been dismissed. The idea is that
    touches are being queued and handled after the IPH has been dismissed,
    leading to crashes.
    
    Bug: 398211689
    Change-Id: I4da8ad9867e1f590c2fb001e7f7c57b4365a8bf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6394655
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438191}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1229,6 +1229,9 @@ const char kIPHGestureRecognitionPanAblation[] =
     "IPHGestureRecognitionPanAblation";
 const char kIPHGestureRecognitionSwipeAblation[] =
     "IPHGestureRecognitionSwipeAblation";
+const char kCancelTouchesInViewForIPH[] = "CancelTouchesInViewForIPH";
+const char kIPHGestureRecognitionImprovement[] =
+    "IPHGestureRecognitionImprovement";
 
 bool IsIPHGestureRecognitionInsideTapAblationEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
@@ -1251,6 +1254,16 @@ bool IsIPHGestureRecognitionSwipeAblationEnabled() {
   return base::GetFieldTrialParamByFeatureAsBool(
       kIPHGestureRecognitionAblation, kIPHGestureRecognitionSwipeAblation,
       false);
+}
+
+bool ShouldCancelTouchesInViewForIPH() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      kIPHGestureRecognitionAblation, kCancelTouchesInViewForIPH, false);
+}
+
+bool IsIPHGestureRecognitionImprovementEnabled() {
+  return base::GetFieldTrialParamByFeatureAsBool(
+      kIPHGestureRecognitionAblation, kIPHGestureRecognitionImprovement, false);
 }
 
 BASE_FEATURE(kNonModalSignInPromo,
