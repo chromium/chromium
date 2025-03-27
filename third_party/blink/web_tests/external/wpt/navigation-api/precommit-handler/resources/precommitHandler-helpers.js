71ee6c15db28688bed431ec67070b8c04d36b956
Nate Chapin <japhet@chromium.org>
2025-03-27 12:16:00 -0700

    Update navigation API deferred commit prototype to use new precommitHandler mechanism
    Explainer updates:
    https://github.com/WICG/navigation-api/pull/281
    https://github.com/WICG/navigation-api/pull/282
    
    Change-Id: I210f47c1dba5064e069e5df059d706cb9d8ecb21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382278
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438913}

--- third_party/blink/web_tests/external/wpt/navigation-api/precommit-handler/resources/precommitHandler-helpers.js
+++ third_party/blink/web_tests/external/wpt/navigation-api/precommit-handler/resources/precommitHandler-helpers.js
@@ -1,4 +1,4 @@
-window.testAfterTransitionCommit = async (t, navigationType, mode, destinationIndex = 0) => {
+window.testDeferredCommit = async (t, navigationType, mode, destinationIndex = 0) => {
   let startHash = location.hash;
   let destinationHash;
   const err = new Error("boo!");
@@ -12,35 +12,25 @@ window.testAfterTransitionCommit = async (t, navigationType, mode, destinationIn
 
   // mode-specific logic for the navigate event handler
   let navigate_helpers = {
-    rejectBeforeCommit : async (e) => {
-      return Promise.reject(err);
-    },
+    rejectBeforeCommit : async (e) => { return Promise.reject("Should never run") },
     rejectAfterCommit : async (e) => {
-      e.commit();
       assert_equals(location.hash, destinationHash, "hash after commit");
-      assert_equals(navigationType == "traverse", popstate_fired, "popstate fired after commit");
+      assert_equals(false, popstate_fired, "popstate before handler starts");
       await new Promise(resolve => t.step_timeout(resolve, 0));
+      assert_equals(navigationType == "traverse", popstate_fired, "popstate fired after handler async step");
       return Promise.reject(err);
     },
-    successExplicitCommit : async (e) => {
-      e.commit();
+    success : async (e) => {
       assert_equals(location.hash, destinationHash, "hash after commit");
-      assert_equals(navigationType == "traverse", popstate_fired, "popstate fired after commit");
+      assert_equals(false, popstate_fired, "popstate before handler starts");
+      await new Promise(resolve => t.step_timeout(resolve, 0));
+      assert_equals(navigationType == "traverse", popstate_fired, "popstate fired after handler async step");
       return new Promise(resolve => t.step_timeout(resolve, 0));
     },
-    successNoExplicitCommit : async (e) => {
-      assert_equals(location.hash, startHash, "start has after first async step");
-      assert_false(popstate_fired, "popstate fired after first async step");
-      await new Promise(resolve => t.step_timeout(resolve, 0));
-      assert_equals(location.hash, startHash, "start has after second async step");
-      assert_false(popstate_fired, "popstate fired after second async step");
-      return new Promise(resolve => t.step_timeout(resolve, 0));
-    }
   }
 
   navigation.addEventListener("navigate", e => {
-    e.intercept({ commit: "after-transition",
-                  handler: t.step_func(async () => {
+    e.intercept({ precommitHandler: t.step_func(async () => {
                     assert_equals(e.navigationType, navigationType);
                     assert_equals(location.hash, startHash, "start hash");
                     assert_false(popstate_fired, "popstate fired at handler start");
@@ -49,21 +39,31 @@ window.testAfterTransitionCommit = async (t, navigationType, mode, destinationIn
                     assert_equals(location.hash, startHash, "hash after first async step");
                     assert_false(popstate_fired, "popstate fired after first async step");
 
-                    return navigate_helpers[mode](e);
-                  })});
+                    if (mode == "rejectBeforeCommit")
+                      return Promise.reject(err);
+                  }),
+                  handler: t.step_func(navigate_helpers[mode])
+                });
   }, { once: true });
+
+  let startingIndex = navigation.currentEntry.index;
+  let expectedIndexOnCommit;
 
   let promises;
   if (navigationType === "push" || navigationType === "replace") {
     destinationHash = (startHash === "" ? "#" : startHash) + "a";
     promises = navigation.navigate(destinationHash, { history: navigationType });
+    expectedIndexOnCommit = (navigationType === "push") ? startingIndex + 1
+                                                        : startingIndex;
   } else if (navigationType === "reload") {
     destinationHash = startHash;
     promises = navigation.reload();
+    expectedIndexOnCommit = startingIndex;
   } else if (navigationType === "traverse") {
     let destinationEntry = navigation.entries()[destinationIndex];
     destinationHash = new URL(destinationEntry.url).hash;
     promises = navigation.traverseTo(destinationEntry.key);
+    expectedIndexOnCommit = destinationIndex;
   }
 
   if (mode === "rejectBeforeCommit") {
@@ -72,6 +72,7 @@ window.testAfterTransitionCommit = async (t, navigationType, mode, destinationIn
     assert_false(popstate_fired, "popstate fired after promise resolution");
     assert_false(navigatesuccess_fired, "navigatesuccess fired");
     assert_true(navigateerror_fired, "navigateerror fired");
+    assert_equals(navigation.currentEntry.index, startingIndex);
   } else if (mode === "rejectAfterCommit") {
     await promises.committed;
     await assertCommittedFulfillsFinishedRejectsExactly(t, promises, navigation.currentEntry, err);
@@ -79,6 +80,7 @@ window.testAfterTransitionCommit = async (t, navigationType, mode, destinationIn
     assert_equals(navigationType == "traverse", popstate_fired, "popstate fired after promise resolution");
     assert_false(navigatesuccess_fired, "navigatesuccess fired");
     assert_true(navigateerror_fired, "navigateerror fired");
+    assert_equals(navigation.currentEntry.index, expectedIndexOnCommit);
   } else {
     await promises.committed;
     await assertBothFulfill(t, promises, navigation.currentEntry);
@@ -86,5 +88,6 @@ window.testAfterTransitionCommit = async (t, navigationType, mode, destinationIn
     assert_equals(navigationType == "traverse", popstate_fired, "popstate fired after promise resolution");
     assert_true(navigatesuccess_fired, "navigatesuccess fired");
     assert_false(navigateerror_fired, "navigateerror fired");
+    assert_equals(navigation.currentEntry.index, expectedIndexOnCommit);
   }
 }
