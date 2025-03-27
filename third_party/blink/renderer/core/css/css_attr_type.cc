1abd013baa6e3a9af7b58e7f88083b0b238beeb3
Rune Lillesveen <futhark@chromium.org>
2025-03-27 08:17:55 -0700

    Only allow numeric literals for attr(<attr-unit>)
    Fixes sibling-index() and sign() crashes.
    
    Bug: 405422528
    Change-Id: I9499c9247a419851091e9e3ca0240b5cde909d18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6395498
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438756}

--- third_party/blink/renderer/core/css/css_attr_type.cc
+++ third_party/blink/renderer/core/css/css_attr_type.cc
@@ -86,11 +86,12 @@ const CSSValue* CSSAttrType::Parse(StringView text,
     CSSParserTokenStream stream(text);
     CSSPrimitiveValue* number_value = css_parsing_utils::ConsumeNumber(
         stream, context, CSSPrimitiveValue::ValueRange::kAll);
-    if (!number_value) {
-      return nullptr;
+    if (CSSNumericLiteralValue* literal =
+            DynamicTo<CSSNumericLiteralValue>(number_value)) {
+      return MakeGarbageCollected<CSSNumericLiteralValue>(
+          literal->GetDoubleValue(), *dimension_unit_);
     }
-    return MakeGarbageCollected<CSSNumericLiteralValue>(
-        number_value->GetDoubleValue(), *dimension_unit_);
+    return nullptr;
   }
   if (IsSyntax()) {
     return syntax_->Parse(text, context, false);
