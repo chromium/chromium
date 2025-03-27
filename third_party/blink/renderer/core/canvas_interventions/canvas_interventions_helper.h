cee9bb61b37cc9a38f2a33a8d8ff20422b3657dc
John Kim <johnykim@google.com>
2025-03-27 11:52:40 -0700

    [FPF-CI] Connect NoiseTokens with CanvasInterventionsHelper.
    NoiseTokens are used to add randomness to the deterministic hash, this
    change replaces the kTokenHash constant with the value of
    CanvasNoiseToken. Ensures the CanvasNoiseToken was propagated to
    CanvasInterventionsHelper.
    
    Bug: 392627601
    Change-Id: Ie4edd5f0f7bd3b0740fa6ee71b02e29013bedf33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387030
    Commit-Queue: John Kim <johnykim@google.com>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1438908}

--- third_party/blink/renderer/core/canvas_interventions/canvas_interventions_helper.h
+++ third_party/blink/renderer/core/canvas_interventions/canvas_interventions_helper.h
@@ -10,29 +10,15 @@
 #include "third_party/blink/renderer/core/html/canvas/canvas_rendering_context.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_host.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
-#include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/heap/visitor.h"
-#include "third_party/blink/renderer/platform/supplementable.h"
 
 namespace blink {
 
-class ExecutionContext;
-
-class CORE_EXPORT CanvasInterventionsHelper
-    : public GarbageCollected<CanvasInterventionsHelper>,
-      public Supplement<ExecutionContext> {
+class CORE_EXPORT CanvasInterventionsHelper {
  public:
   enum class CanvasInterventionType {
     kNone,
     kNoise,
   };
-
-  static const char kSupplementName[];
-
-  static CanvasInterventionsHelper* Create(ExecutionContext* execution_context);
-
-  explicit CanvasInterventionsHelper(ExecutionContext& execution_context);
-  virtual ~CanvasInterventionsHelper() = default;
 
   // If allowed, performs noising on a copy of the snapshot StaticBitmapImage
   // and returns the noised snapshot, otherwise it will return the original
@@ -41,18 +27,6 @@ class CORE_EXPORT CanvasInterventionsHelper
                                  ExecutionContext* execution_context,
                                  scoped_refptr<StaticBitmapImage>& snapshot,
                                  RasterMode raster_mode);
-
-  void Trace(Visitor* visitor) const override {
-    visitor->Trace(execution_context_);
-    Supplement<ExecutionContext>::Trace(visitor);
-  }
-
- private:
-  ExecutionContext* GetExecutionContext() const {
-    return execution_context_.Get();
-  }
-
-  Member<ExecutionContext> execution_context_;
 };
 
 }  // namespace blink
