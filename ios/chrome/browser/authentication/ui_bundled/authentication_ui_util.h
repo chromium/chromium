14ad69606cc951750ad69aaa0bccf0a3d5dee9a2
Arthur Milchior <arthurmilchior@chromium.org>
2025-03-28 11:11:19 -0700

    [iOS] Add ChangeProfileContinuationProvider
    This new type will allow to provide the continuation to the
    authentication flow in profile.
    
    Bug: 375605572
    Change-Id: I769c0fc671a5f06264dfceab2d11624b4abee3f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6406413
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1439550}

--- ios/chrome/browser/authentication/ui_bundled/authentication_ui_util.h
+++ ios/chrome/browser/authentication/ui_bundled/authentication_ui_util.h
@@ -11,6 +11,7 @@
 #include <string_view>
 
 #include "base/ios/block_types.h"
+#include "ios/chrome/app/change_profile_continuation.h"
 
 @class ActionSheetCoordinator;
 @class AlertCoordinator;
@@ -137,5 +138,9 @@ ActionSheetCoordinator* GetLeavingPrimaryAccountConfirmationDialog(
     SignedInUserState signed_in_user_state,
     bool account_profile_switch,
     LeavingPrimaryAccountConfirmationDialogCompletion completion);
+
+// A block providing a continuation.
+using ChangeProfileContinuationProvider =
+    base::RepeatingCallback<ChangeProfileContinuation()>;
 
 #endif  // IOS_CHROME_BROWSER_AUTHENTICATION_UI_BUNDLED_AUTHENTICATION_UI_UTIL_H_
