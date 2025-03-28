a66aa607fbd14ce9cf8971329dde1fc799f533aa
Anthi Orfanou <anthie@google.com>
2025-03-28 11:02:10 -0700

    Revert "[STG] Disable feature for automotive users M136"
    This reverts commit aff8b1d20e98118d21d745088abe3e616fc1b134.
    
    Reason for revert: Change results in test failures in crbug.com/407001902 and crbug.com/406987845.
    
    Original change's description:
    > [STG] Disable feature for automotive users M136
    >
    > Bug: 405475656
    > Change-Id: I7336f095819bc59279206b4427041b21995ed4d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6395119
    > Commit-Queue: Hailey Wang <haileywang@google.com>
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1439091}
    
    Bug: 405475656
    Change-Id: Ib963dee2cf302da759d74381afcde8cd9c3b37ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6409291
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Owners-Override: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439544}

--- components/collaboration/internal/collaboration_service_impl.cc
+++ components/collaboration/internal/collaboration_service_impl.cc
@@ -23,7 +23,6 @@
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/service/sync_service.h"
 #include "components/sync/service/sync_user_settings.h"
-#include "ui/base/device_form_factor.h"
 
 namespace collaboration {
 
@@ -288,11 +287,6 @@ CollaborationStatus CollaborationServiceImpl::GetCollaborationStatus() {
   // Check if device policy allow signin.
   if (!profile_prefs_->GetBoolean(prefs::kSigninAllowed)) {
     return CollaborationStatus::kDisabledForPolicy;
-  }
-
-  // Disable for automotive users.
-  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_AUTOMOTIVE) {
-    return CollaborationStatus::kDisabled;
   }
 
   // TODO(haileywang): Support collaboration status updates.
