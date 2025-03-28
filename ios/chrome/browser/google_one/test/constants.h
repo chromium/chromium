63b764df4dccf212f40b641d2160efe01aea1747
Olivier ROBIN <olivierrobin@google.com>
2025-03-28 07:21:52 -0700

    Add EGTests for Save to drive manage storage
    Add an EG test that triggers the manage account alert on save to drive,
    both with and without G1.
    
    For G1 test, as the real feature requires network and real signin,
    create a test version of it that will allow to test the entry point.
    
    Part of a 3 CL patch
     - crrev.com/c/6395514 : update the API
     - crrev.com/i/8141475 : implement the API internally
    => crrev.com/c/6396502 : add the new tests
    
    Change-Id: I4fd36a758fde80d75df613173eb4bf05e33189d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396502
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439386}

---
+++ ios/chrome/browser/google_one/test/constants.h
@@ -0,0 +1,13 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_GOOGLE_ONE_TEST_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_GOOGLE_ONE_TEST_CONSTANTS_H_
+
+#import <Foundation/Foundation.h>
+
+// Accessibility Identifier for the TestGoogleOneController.
+extern NSString* const kTestGoogleOneControllerAccessibilityID;
+
+#endif  // IOS_CHROME_BROWSER_GOOGLE_ONE_TEST_CONSTANTS_H_
