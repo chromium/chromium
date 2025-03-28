29246bdaf6d89a9288f77e3fa161c12c4700717f
Vigen Issahhanjan <vigeni@google.com>
2025-03-28 15:55:54 -0700

    Rename the CastApplicaiton to CastBrowserApplication.
    This rename allows deduping the initialization logic used by Cast
    Service in a standalone APK build.
    
    Bug: b:341792617
    Test: Ran in split and standalone way on a device
    Change-Id: I0713d01c5ac9e5d686d39f3f5711c5008f9bf414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6409771
    Commit-Queue: Vigen Issahhanjan <vigeni@google.com>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Cr-Commit-Position: refs/heads/main@{#1439751}

--- chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastBrowserApplication.java
+++ chromecast/browser/android/apk/src/org/chromium/chromecast/shell/CastBrowserApplication.java
@@ -1,4 +1,4 @@
-// Copyright 2023 The Chromium Authors
+// Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
 import org.chromium.base.PathUtils;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LibraryProcessType;
@@ -18,13 +19,13 @@ import org.chromium.ui.base.ResourceBundle;
  * Entry point for the Android cast shell application. Handles initialization of information that
  * needs to be shared across the main activity and the child services created.
  */
-public class CastApplication extends Application {
+public class CastBrowserApplication extends Application {
+    private static final String TAG = "CastBrowserApplicatn";
     private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "cast_shell";
 
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        ContextUtils.initApplicationContext(this);
+    public static void initialize(Application application) {
+        Log.d(TAG, "initialize");
+        ContextUtils.initApplicationContext(application);
         ResourceBundle.setAvailablePakLocales(ProductConfig.LOCALES);
         LibraryLoader.getInstance().setLinkerImplementation(ProductConfig.USE_CHROMIUM_LINKER);
         LibraryLoader.getInstance()
@@ -32,15 +33,19 @@ public class CastApplication extends Application {
                         isBrowserProcess()
                                 ? LibraryProcessType.PROCESS_BROWSER
                                 : LibraryProcessType.PROCESS_CHILD);
+
+        PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX);
+        ApplicationStatus.initialize(application);
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX);
-        ApplicationStatus.initialize(this);
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        initialize(this);
     }
 
+    // Returns true if this is the main browser process; otherwise false for sandboxed and
+    // privileged processes.
     private static boolean isBrowserProcess() {
         return ContextUtils.getProcessName().contains("cast_browser_process");
     }
