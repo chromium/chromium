c8fbdfaad99d460adf4aed9ac588c6035b0cd8a4
Ernesto Izquierdo Clua <eic@google.com>
2025-03-31 10:39:04 -0700

    Move ios connectors utils
    Move utils to connectors instead of reporting subfolder as we need them
    for Url filtering as well.
    
    Bug: None
    Change-Id: I3f8381030a2f4631b438a39e858b51a4de86be94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6409551
    Auto-Submit: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Xu Li <mxlg@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1440389}

--- ios/chrome/browser/enterprise/connectors/connectors_util.h
+++ ios/chrome/browser/enterprise/connectors/connectors_util.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_REPORTING_IOS_REPORTING_UTIL_H_
-#define IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_REPORTING_IOS_REPORTING_UTIL_H_
+#ifndef IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_CONNECTORS_UTIL_H_
+#define IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_CONNECTORS_UTIL_H_
 
 #import "base/containers/flat_set.h"
 #import "base/values.h"
@@ -35,4 +35,4 @@ base::flat_set<std::string> GetUserAffiliationIds(ProfileIOS* profile);
 
 }  // namespace enterprise_connectors
 
-#endif  // IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_REPORTING_IOS_REPORTING_UTIL_H_
+#endif  // IOS_CHROME_BROWSER_ENTERPRISE_CONNECTORS_CONNECTORS_UTIL_H_
--- ios/chrome/browser/enterprise/connectors/connectors_util.mm
+++ ios/chrome/browser/enterprise/connectors/connectors_util.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/enterprise/connectors/reporting/ios_reporting_util.h"
+#import "ios/chrome/browser/enterprise/connectors/connectors_util.h"
 
 #import <optional>
 
