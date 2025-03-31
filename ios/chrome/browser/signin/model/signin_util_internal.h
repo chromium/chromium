0e1be521899139c9ba53603c33e3af5b08a65497
Jérôme Lebel <jlebel@chromium.org>
2025-03-31 03:43:51 -0700

    [iOS][Waffle] Add device restore timestamp
    This patch updates the APIs to detect a restore on iOS, to add
    a timestamp for the latest restore.
    
    Before this patch, `IsFirstSessionAfterDeviceRestore()` loads the
    restore device state into cache (if it was not loaded yet) and returns
    the value.
    
    With this patch, `LoadDeviceRestoreData()` is called `IsFirstSessionAfterDeviceRestore()` and it loads the data if it
    was not yet loaded.
    `IsFirstSessionAfterDeviceRestore()` still returns the same value.
    And `LastDeviceRestoreTimestamp()` is added to know when was the
    last device restore value.
    
    For crrev.com/c/6396506, `ResetDeviceRestoreDataForTesting()` is added
    for test purposes.
    
    This is required for crrev.com/c/6396506
    
    Bug: 404176285
    Change-Id: I835dd4ca7cfafb7fa529f1dfdc3e4ad15f208650
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6400794
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440183}

--- ios/chrome/browser/signin/model/signin_util_internal.h
+++ ios/chrome/browser/signin/model/signin_util_internal.h
@@ -5,10 +5,27 @@
 #ifndef IOS_CHROME_BROWSER_SIGNIN_MODEL_SIGNIN_UTIL_INTERNAL_H_
 #define IOS_CHROME_BROWSER_SIGNIN_MODEL_SIGNIN_UTIL_INTERNAL_H_
 
+#include <optional>
+
 #include "base/files/file_path.h"
+#include "base/time/time.h"
 
 namespace signin {
 enum class Tribool;
+
+// Struct returned by `LoadDeviceRestoreDataInternal()`.
+struct RestoreData {
+  // The value is `kTrue` if the current session is right after a device
+  // restore, otherwise `kFalse`.
+  // `kUnknown` if it was not possible know.
+  signin::Tribool is_first_session_after_device_restore;
+  // Timestamp of the latest device restore. The value is unset:
+  //   - if the device was not been restored,
+  //   - or if this is the first run after a device restore,
+  //   - or if `LoadDeviceRestoreData()` was not called yet.
+  std::optional<base::Time> last_restore_timestamp;
+};
+
 }  // namespace signin
 
 // File name for sentinel to backup in iOS backup device.
@@ -21,6 +38,6 @@ base::FilePath PathForSentinel(const base::FilePath::CharType* sentinel_name);
 // Returns whether Chrome has been started after a device restore. This method
 // needs to be called for the first time before IO is disallowed on UI thread.
 // The value is cached. The result is cached for later calls.
-signin::Tribool IsFirstSessionAfterDeviceRestoreInternal();
+signin::RestoreData LoadDeviceRestoreDataInternal();
 
 #endif  // IOS_CHROME_BROWSER_SIGNIN_MODEL_SIGNIN_UTIL_INTERNAL_H_
