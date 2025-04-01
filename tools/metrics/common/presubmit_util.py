fccdf781d05185ecde512a3595847eb1e094439f
Andrzej Fiedukowicz <afie@google.com>
2025-04-01 03:26:09 -0700

    Avoid running ukm/validate_format.py multiple times
    Currently we run the same exact script for each modified enums.xml file
    which multiplies the time needed to finish this check. It seems like an
    easy win to run it only once instead.
    
    Same treatment was done for tools/metrics/ukm checks (through helper).
    In this case we expect only one file to match, but just in case there is
    no much point in running the same validation scripts again.
    
    Bug: 402138576
    
    Change-Id: Ife375eb2ffcf9b55b0308c645efdd4c06334179a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6396841
    Commit-Queue: Andrzej Fiedukowicz <afie@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440813}

--- tools/metrics/common/presubmit_util.py
+++ tools/metrics/common/presubmit_util.py
@@ -114,30 +114,40 @@ def DoPresubmitMain(*args, **kwargs):
 
 def CheckChange(xml_file, input_api, output_api):
   """Checks that xml is pretty-printed and well-formatted."""
-  for f in input_api.AffectedFiles():
-    p = f.AbsoluteLocalPath()
-    if (input_api.basename(p) == xml_file
-        and input_api.os_path.dirname(p) == input_api.PresubmitLocalPath()):
-      cwd = input_api.os_path.dirname(p)
+  absolute_paths_of_affected_files = [
+      f.AbsoluteLocalPath() for f in input_api.AffectedFiles()
+  ]
+  xml_file_changed = any([
+      input_api.basename(p) == xml_file
+      and input_api.os_path.dirname(p) == input_api.PresubmitLocalPath()
+      for p in absolute_paths_of_affected_files
+  ])
 
-      exit_code = input_api.subprocess.call(
-          [input_api.python3_executable, 'pretty_print.py', '--presubmit'],
-          cwd=cwd)
-      if exit_code != 0:
-        return [
-            output_api.PresubmitError(
-                '%s is not prettified; run `git cl format` to fix.' % xml_file),
-        ]
+  if not xml_file_changed:
+    return []
 
-      exit_code = input_api.subprocess.call(
-          [input_api.python3_executable, 'validate_format.py', '--presubmit'],
-          cwd=cwd)
-      if exit_code != 0:
-        return [
-            output_api.PresubmitError(
-                '%s does not pass format validation; run %s/validate_format.py '
-                'and fix the reported error(s) or warning(s).' %
-                (xml_file, input_api.PresubmitLocalPath())),
-        ]
+  cwd = input_api.PresubmitLocalPath()
+  pretty_print_args = [
+      input_api.python3_executable, 'pretty_print.py', '--presubmit'
+  ]
+
+  exit_code = input_api.subprocess.call(pretty_print_args, cwd=cwd)
+  if exit_code != 0:
+    return [
+        output_api.PresubmitError(
+            '%s is not prettified; run `git cl format` to fix.' % xml_file),
+    ]
+
+  validate_format_args = [
+      input_api.python3_executable, 'validate_format.py', '--presubmit'
+  ]
+  exit_code = input_api.subprocess.call(validate_format_args, cwd=cwd)
+  if exit_code != 0:
+    return [
+        output_api.PresubmitError(
+            '%s does not pass format validation; run %s/validate_format.py '
+            'and fix the reported error(s) or warning(s).' %
+            (xml_file, input_api.PresubmitLocalPath())),
+    ]
 
   return []
