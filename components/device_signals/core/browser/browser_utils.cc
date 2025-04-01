ff2d63ca8f10976a8314704e56f961561c2ce9e3
Zonghan Xu <xzonghan@google.com>
2025-04-01 07:42:47 -0700

    [Signals Reporting] Refactor functions in signals_utils for Profile-level signals collection
    Refactor functions in chrome/browser/enterprise/signals/signals_utils.h to components/device_signals/core/browser/browser_utils.h so they can be utilized when we implement ProfileSignalsCollector later.
    
    CL derived from 6354126
    
    Bug: 398831728
    Change-Id: I86ac51f0b262314d31febe2469d3b66b99216aaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416690
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440911}

--- components/device_signals/core/browser/browser_utils.cc
+++ components/device_signals/core/browser/browser_utils.cc
@@ -1,29 +1,23 @@
-// Copyright 2021 The Chromium Authors
+// Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/enterprise/signals/signals_utils.h"
+#include "components/device_signals/core/browser/browser_utils.h"
 
 #include "base/check.h"
 #include "build/build_config.h"
-#include "chrome/common/pref_names.h"
 #include "components/policy/content/policy_blocklist_service.h"
-#include "components/policy/core/browser/url_blocklist_manager.h"
+#include "components/policy/core/common/cloud/cloud_policy_manager.h"
+#include "components/policy/core/common/cloud/cloud_policy_store.h"
 #include "components/prefs/pref_service.h"
-#include "components/safe_browsing/core/common/safe_browsing_prefs.h"
-
-#if BUILDFLAG(IS_WIN)
-#include "components/component_updater/pref_names.h"
-#endif  // BUILDFLAG(IS_WIN)
-
-namespace enterprise_signals {
-namespace utils {
+#include "content/public/browser/site_isolation_policy.h"
 
 namespace {
 
 bool IsURLBlocked(const GURL& url, PolicyBlocklistService* service) {
-  if (!service)
+  if (!service) {
     return false;
+  }
 
   policy::URLBlocklist::URLBlocklistState state =
       service->GetURLBlocklistState(url);
@@ -32,6 +26,8 @@ bool IsURLBlocked(const GURL& url, PolicyBlocklistService* service) {
 }
 
 }  // namespace
+
+namespace device_signals {
 
 safe_browsing::SafeBrowsingState GetSafeBrowsingProtectionLevel(
     PrefService* profile_prefs) {
@@ -42,25 +38,22 @@ safe_browsing::SafeBrowsingState GetSafeBrowsingProtectionLevel(
       profile_prefs->GetBoolean(prefs::kSafeBrowsingEnhanced);
 
   if (safe_browsing_enabled) {
-    if (safe_browsing_enhanced_enabled)
+    if (safe_browsing_enhanced_enabled) {
       return safe_browsing::SafeBrowsingState::ENHANCED_PROTECTION;
-    else
+    } else {
       return safe_browsing::SafeBrowsingState::STANDARD_PROTECTION;
+    }
   } else {
     return safe_browsing::SafeBrowsingState::NO_SAFE_BROWSING;
   }
 }
 
-bool GetBuiltInDnsClientEnabled(PrefService* local_state) {
-  DCHECK(local_state);
-  return local_state->GetBoolean(prefs::kBuiltInDnsClientEnabled);
-}
-
 std::optional<safe_browsing::PasswordProtectionTrigger>
 GetPasswordProtectionWarningTrigger(PrefService* profile_prefs) {
   DCHECK(profile_prefs);
-  if (!profile_prefs->HasPrefPath(prefs::kPasswordProtectionWarningTrigger))
+  if (!profile_prefs->HasPrefPath(prefs::kPasswordProtectionWarningTrigger)) {
     return std::nullopt;
+  }
   return static_cast<safe_browsing::PasswordProtectionTrigger>(
       profile_prefs->GetInteger(prefs::kPasswordProtectionWarningTrigger));
 }
@@ -71,5 +64,26 @@ bool GetChromeRemoteDesktopAppBlocked(PolicyBlocklistService* service) {
          IsURLBlocked(GURL("https://remotedesktop.corp.google.com"), service);
 }
 
-}  // namespace utils
-}  // namespace enterprise_signals
+std::optional<std::string> TryGetEnrollmentDomain(
+    policy::CloudPolicyManager* manager) {
+  policy::CloudPolicyStore* store = nullptr;
+  if (manager && manager->core() && manager->core()->store()) {
+    store = manager->core()->store();
+  }
+
+  if (store && store->has_policy()) {
+    const auto* policy = store->policy();
+    if (policy->has_managed_by()) {
+      return policy->managed_by();
+    } else if (policy->has_display_domain()) {
+      return policy->display_domain();
+    }
+  }
+  return std::nullopt;
+}
+
+bool GetSiteIsolationEnabled() {
+  return content::SiteIsolationPolicy::UseDedicatedProcessesForAllSites();
+}
+
+}  // namespace device_signals
