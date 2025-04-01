ef1ea23e5ce614091d8f093596d427a6c6feffee
Martin Kong <martinkong@google.com>
2025-04-01 07:43:31 -0700

    Create and extract a custom archive format compressed via zst
    This CL adds two new functionalities to the devil_util binary:
    (1) Create a zst-compressed custom archive file
    (2) Extract a zst-compressed custom archive file
    
    A followup change will be made to the catapult repository,
    to push a zst-compressed custom archive file instead of a zip
    to the device. Current benchmark shows that this gives us a
    ~30% reduction in the time spent in the PushChangedFiles() step.
    
    Bug: 397457369
    Change-Id: Iaab42f222435c22e2fb2ec2ae4d59db7fedfc43a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6382436
    Commit-Queue: Martin Kong <martinkong@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1440912}

--- tools/android/devil_util/zst_decompressor.cc
+++ tools/android/devil_util/zst_decompressor.cc
@@ -9,46 +9,46 @@
 #include "third_party/zstd/src/lib/zstd.h"
 
 ZstDecompressor::ZstDecompressor(std::istream& input_stream)
-    : _input_stream(input_stream) {
-  _input_buffer_size = ZSTD_DStreamInSize();
-  _input_buffer = new char[_input_buffer_size];
-  _input_struct = new ZSTD_inBuffer{/* src = */ _input_buffer,
+    : input_stream_(input_stream) {
+  input_buffer_size_ = ZSTD_DStreamInSize();
+  input_buffer_ = new char[input_buffer_size_];
+  input_struct_ = new ZSTD_inBuffer{/* src = */ input_buffer_,
                                     /* size = */ 0,
                                     /* pos = */ 0};
-  _output_buffer_size = ZSTD_DStreamOutSize();
-  _output_buffer = new char[_output_buffer_size];
-  _output_struct = new ZSTD_outBuffer{/* dst = */ _output_buffer,
-                                      /* size = */ _output_buffer_size,
+  output_buffer_size_ = ZSTD_DStreamOutSize();
+  output_buffer_ = new char[output_buffer_size_];
+  output_struct_ = new ZSTD_outBuffer{/* dst = */ output_buffer_,
+                                      /* size = */ output_buffer_size_,
                                       /* pos = */ 0};
-  _ctx = ZSTD_createDCtx();
-  if (_ctx == nullptr) {
+  ctx_ = ZSTD_createDCtx();
+  if (ctx_ == nullptr) {
     std::cerr << "ZSTD_createDCtx failed!" << std::endl;
     exit(1);
   }
-  _last_return_value = 0;
+  last_return_value_ = 0;
 }
 
 ZstDecompressor::~ZstDecompressor() {
-  delete[] _input_buffer;
-  delete _input_struct;
-  delete[] _output_buffer;
-  delete _output_struct;
-  ZSTD_freeDCtx(_ctx);
+  delete[] input_buffer_;
+  delete input_struct_;
+  delete[] output_buffer_;
+  delete output_struct_;
+  ZSTD_freeDCtx(ctx_);
 }
 
 bool ZstDecompressor::DecompressStreaming(DecompressedContent* output) {
   // If we have decompressed everything that we have read last time, we should
   // read a new chunk of the input_stream.
-  if (_input_struct->pos >= _input_struct->size) {
-    _input_stream.read(_input_buffer, _input_buffer_size);
-    if (_input_stream.fail() && !_input_stream.eof()) {
+  if (input_struct_->pos >= input_struct_->size) {
+    input_stream_.read(input_buffer_, input_buffer_size_);
+    if (input_stream_.fail() && !input_stream_.eof()) {
       std::cerr << "ZST decompressor failed to read from input_stream."
                 << std::endl;
       exit(1);
     }
-    size_t num_bytes_read = _input_stream.gcount();
+    size_t num_bytes_read = input_stream_.gcount();
     if (num_bytes_read == 0) {
-      if (_last_return_value != 0) {
+      if (last_return_value_ != 0) {
         std::cerr << "ZST decompressor has processed the entirety of "
                      "input_stream but ZSTD says the decoding process is not "
                      "done. This likely means the input_stream is malformed."
@@ -57,22 +57,22 @@ bool ZstDecompressor::DecompressStreaming(DecompressedContent* output) {
       }
       return true;
     }
-    _input_struct->size = num_bytes_read;
-    _input_struct->pos = 0;
+    input_struct_->size = num_bytes_read;
+    input_struct_->pos = 0;
   }
 
   // Now that there is some content in the input buffer that we have not
   // decompressed, we pass the input buffer to the zstd library function and ask
   // it to do the decompression work.
-  _output_struct->pos = 0;
-  _last_return_value =
-      ZSTD_decompressStream(_ctx, _output_struct, _input_struct);
-  if (ZSTD_isError(_last_return_value)) {
+  output_struct_->pos = 0;
+  last_return_value_ =
+      ZSTD_decompressStream(ctx_, output_struct_, input_struct_);
+  if (ZSTD_isError(last_return_value_)) {
     std::cerr << "ZSTD_decompressStream returned an error: "
-              << ZSTD_getErrorName(_last_return_value) << std::endl;
+              << ZSTD_getErrorName(last_return_value_) << std::endl;
     exit(1);
   }
-  output->buffer = _output_buffer;
-  output->size = _output_struct->pos;
+  output->buffer = output_buffer_;
+  output->size = output_struct_->pos;
   return false;
 }
