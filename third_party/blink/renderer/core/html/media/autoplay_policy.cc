35584abe727b773ec050c7b1c2dbae76fdbaec55
Dale Curtis <dalecurtis@chromium.org>
2025-04-01 12:10:58 -0700

    Revert "media-playback-while-not-visible permission policy origin trials"
    This reverts commit 21c1a58d83ab38e277f3e585a26a9a755581ecf3.
    
    Reason for revert: https://crbug.com/407726581
    
    Original change's description:
    > media-playback-while-not-visible permission policy origin trials
    >
    > This CL implements the origin trial (OT) for the
    > "media-playback-while-not-visible" permission policy (Chrome Status:
    > https://chromestatus.com/feature/5082950457884672)
    >
    > The origin trial controls the
    > MediaPlaybackWhileNotVisiblePermissionPolicy feature flag, which in turn
    > controls the permission policy availability. Only origins that
    > registered for the OT will be allowed to override the permission policy
    > default value by modifying an iframe's "allow" property:
    > allow="media-playback-while-not-visible 'none'"
    >
    > Bug: 351354996
    > Change-Id: Iec05b8e579b02dec0e8703f1599dd75b6a7eed4b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6349498
    > Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    > Reviewed-by: Evan Liu <evliu@google.com>
    > Commit-Queue: Gabriel Brito <gabrielbrito@microsoft.com>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1440552}
    
    Bug: 351354996
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5a079fa330148cc9c8fb6d082c2411ace867edb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6422992
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7103@{#11}
    Cr-Branched-From: e09430c64983fc906f37a9f7e6806275c9b67b86-refs/heads/main@{#1440670}

--- third_party/blink/renderer/core/html/media/autoplay_policy.cc
+++ third_party/blink/renderer/core/html/media/autoplay_policy.cc
@@ -25,7 +25,6 @@
 #include "third_party/blink/renderer/core/intersection_observer/intersection_observer_entry.h"
 #include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/network/network_state_notifier.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
@@ -287,18 +286,10 @@ bool AutoplayPolicy::HasTransientUserActivation() const {
 }
 
 std::optional<DOMExceptionCode> AutoplayPolicy::RequestPlay() {
-  if (!CanPlayWhileHidden()) {
-    // The "media-playback-while-not-visible" permission policy default value
-    // was overridden, which means that either this frame or an ancestor frame
-    // changed the permission policy's default value. This should only happen if
-    // the MediaPlaybackWhileNotVisiblePermissionPolicyEnabled runtime flag is
-    // enabled.
-    UseCounter::Count(
-        element_->GetExecutionContext(),
-        WebFeature::kMediaPlaybackWhileNotVisiblePermissionPolicy);
-    if (IsFrameHidden()) {
-      return DOMExceptionCode::kNotAllowedError;
-    }
+  if (RuntimeEnabledFeatures::
+          MediaPlaybackWhileNotVisiblePermissionPolicyEnabled() &&
+      !CanPlayWhileHidden() && IsFrameHidden()) {
+    return DOMExceptionCode::kNotAllowedError;
   }
 
   if (!HasTransientUserActivation()) {
