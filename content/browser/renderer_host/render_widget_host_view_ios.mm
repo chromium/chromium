da7e0352697af3efe203b70f2fdb67c6a645dfee
Dave Tapuska <dtapuska@chromium.org>
2025-04-02 12:12:32 -0700

    [ios blink] Fix screen updates between pages
    With the removal of the update of the screen rects in
    https://chromium-review.googlesource.com/c/chromium/src/+/6426921
    this caused an issue when the screen rects were correct but the
    size hasn't been set on the browser compositor. Bring the iOS
    inline with the MacOS code. Do not send two
    SynchronizedVisualProperties messages when the size is updated,
    just call NotifyScreenInfoChange which forces one ignoring the
    pending ack.
    
    Change-Id: Ib24afafe1c168e5e08d67c7a3dd8e94b4bcafec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426567
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441723}

--- content/browser/renderer_host/render_widget_host_view_ios.mm
+++ content/browser/renderer_host/render_widget_host_view_ios.mm
@@ -414,33 +414,21 @@ void RenderWidgetHostViewIOS::UpdateScreenInfo() {
       display_screen->GetScreenInfosNearestDisplay(
           display_screen->GetPrimaryDisplay().id());
 
-  if (screen_infos_ == new_screen_infos) {
-    return;
-  }
+  gfx::Rect view_bounds_dips([ui_view_->view_ bounds]);
+  const bool screen_info_changed = screen_infos_ != new_screen_infos;
+  const bool size_changed =
+      view_bounds_dips.size() != browser_compositor_->GetRendererSize();
+  screen_infos_ = std::move(new_screen_infos);
 
-  if (!IsTesting()) {
-    browser_compositor_->UpdateSurfaceFromUIView(
-        gfx::Rect([ui_view_->view_ bounds]).size());
+  if (!IsTesting() && (size_changed || screen_info_changed)) {
+    browser_compositor_->UpdateSurfaceFromUIView(view_bounds_dips.size());
   }
   ComputeDisplayFeature();
-
-  // We need to look at `orientation_type` which is marked as kUndefined at
-  // startup. Unlike `orientation_angle` that uses 0 degrees as the default.
-  // This accounts for devices which have a default landscape orientation, such
-  // as tablets. We do not want the first UpdateScreenInfo to be treated as a
-  // rotation.
-  const bool has_rotation_changed =
-      screen_infos_.current().orientation_type !=
-          display::mojom::ScreenOrientation::kUndefined &&
-      screen_infos_.current().orientation_type !=
-          new_screen_infos.current().orientation_type;
-  screen_infos_ = std::move(new_screen_infos);
 
   // Notify the associated RenderWidgetHostImpl when screen info has changed.
   // That will synchronize visual properties needed for frame tree rendering
   // and for web platform APIs that expose screen and window info and events.
-  if (host()) {
-    OnSynchronizedDisplayPropertiesChanged(has_rotation_changed);
+  if (size_changed || screen_info_changed) {
     host()->NotifyScreenInfoChanged();
   }
 }
