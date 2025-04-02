f46128cc5540856b13508957381bd820eccd6003
Daniel Cheng <dcheng@chromium.org>
2025-04-02 13:49:25 -0700

    Delete base::MD5Hash64Constexpr
    Bug: 406829393
    Change-Id: I6cce15f4b3fa68c145e54d5366a174b538c871fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6421515
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441784}

--- base/hash/md5_constexpr_unittest.cc
+++ base/hash/md5_constexpr_unittest.cc
@@ -9,15 +9,11 @@ namespace base {
 // Ensure that everything works at compile-time by comparing to a few
 // reference hashes.
 constexpr char kMessage0[] = "message digest";
-static_assert(MD5Hash64Constexpr(kMessage0) == 0xF96B697D7CB7938Dull,
-              "incorrect MD5Hash64 implementation");
 
 static_assert(MD5Hash32Constexpr(kMessage0) == 0xF96B697Dul,
               "incorrect MD5Hash32 implementation");
 
 constexpr char kMessage1[] = "The quick brown fox jumps over the lazy dog";
-static_assert(MD5Hash64Constexpr(kMessage1) == 0x9E107D9D372BB682ull,
-              "incorrect MD5Hash64 implementation");
 
 static_assert(MD5Hash32Constexpr(kMessage1) == 0x9E107D9Dul,
               "incorrect MD5Hash32 implementation");
