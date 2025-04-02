726123f2232b90de7aae546a91e220614fb06bcb
Jesse McKenna <jessemckenna@google.com>
2025-04-02 10:02:43 -0700

    pdf-default: enable opening settings dialog
    This change adds shell_util::ShowSetDefaultForFileExtensionSystemUI,
    which opens the Windows settings pop-up that allows the user to select a
    default app for a specified file extension. It also adds
    LaunchDefaultAppForFileExtensionSettings, which does the actual work of
    opening the pop-up.
    
    It separates some logic from existing methods in shell_util that the new
    code uses. It also moves some of that logic to default_apps_util.h,
    because it seems to fit (all four methods open Windows settings
    dialogs).
    
    Bug: 396202897
    Change-Id: I1b73e13ad4fd8bbc9037f4f06d73bef6af87bb57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330881
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Jesse McKenna <jessemckenna@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441563}

--- base/win/default_apps_util.h
+++ base/win/default_apps_util.h
@@ -8,6 +8,8 @@
 #include <string_view>
 
 #include "base/base_export.h"
+#include "base/strings/cstring_view.h"
+#include "base/win/windows_types.h"
 
 namespace base::win {
 
@@ -17,6 +19,26 @@ namespace base::win {
 // successfully opened, and the `protocol`, if not empty, was highlighted.
 BASE_EXPORT bool LaunchDefaultAppsSettingsModernDialog(
     std::wstring_view protocol);
+
+// Launches a Windows Settings app pop-up that reads:
+//   * Windows 10: "How do you want to open `file_extension` files from now on?"
+//   * Windows 11: "Select a default app for `file_extension` files"
+// Returns true if the dialog was successfully opened.
+// Returns false on failure or if `file_extension` is empty.
+// `parent_hwnd` is used by Windows to position the pop-up logically based on
+// the parent window's location.
+BASE_EXPORT bool LaunchDefaultAppForFileExtensionSettings(
+    base::wcstring_view file_extension,
+    HWND parent_hwnd);
+
+// Launches the Windows Settings app and navigates to the "Apps > Default apps"
+// page for `app_name`.
+BASE_EXPORT bool LaunchSettingsDefaultApps(std::wstring_view app_name,
+                                           bool is_per_user_install);
+
+// Launches the Windows Settings app and navigates to `uri`. Returns true if
+// successful.
+BASE_EXPORT bool LaunchSettingsUri(base::wcstring_view uri);
 
 }  // namespace base::win
 
