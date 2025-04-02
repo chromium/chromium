38ffbaf8d3a5a0c328397d97b46718803b2430ad
Fred Shih <ffred@chromium.org>
2025-04-02 15:08:03 -0700

    Add type trait unit test for result response in C++
    Bug: 40841428
    Change-Id: I47d9f700160af5a42812888f57e2890f7c5def50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6423353
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441830}

--- mojo/public/cpp/bindings/tests/result_response_unittest.cc
+++ mojo/public/cpp/bindings/tests/result_response_unittest.cc
@@ -10,6 +10,7 @@
 #include "mojo/public/cpp/bindings/remote.h"
 #include "mojo/public/cpp/bindings/tests/bindings_test_base.h"
 #include "mojo/public/cpp/bindings/tests/result_response.test-mojom.h"
+#include "result_response_unittest_mojom_traits.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace mojo::test {
@@ -37,6 +38,31 @@ class InterfaceImpl : public mojom::TestResultInterface {
   mojo::Receiver<mojom::TestResultInterface> receiver_;
 };
 
+class TraitInterfaceImpl : public mojom::TestResultInterfaceWithTrait {
+ public:
+  explicit TraitInterfaceImpl(
+      mojo::PendingReceiver<mojom::TestResultInterfaceWithTrait> receiver)
+      : receiver_(this, std::move(receiver)) {}
+  TraitInterfaceImpl(const TraitInterfaceImpl&) = delete;
+  TraitInterfaceImpl& operator=(const TraitInterfaceImpl&) = delete;
+  ~TraitInterfaceImpl() override = default;
+
+  // mojom::TestResultTraitInterface
+  void TestSuccess(TestSuccessCallback cb) override {
+    std::move(cb).Run(base::ok(MappedResultValue{1}));
+  }
+
+  void TestFailure(TestFailureCallback cb) override {
+    MappedResultError err;
+    err.is_game_over_ = true;
+    err.reason_ = "meltdown!";
+    std::move(cb).Run(base::unexpected(err));
+  }
+
+ private:
+  mojo::Receiver<mojom::TestResultInterfaceWithTrait> receiver_;
+};
+
 using ResultResponseTest = BindingsTestBase;
 
 TEST_P(ResultResponseTest, TestResult) {
@@ -61,6 +87,33 @@ TEST_P(ResultResponseTest, TestFailure) {
       "fail",
       base::BindLambdaForTesting([&](base::expected<bool, std::string> result) {
         ASSERT_EQ("fail", result.error());
+        loop.Quit();
+      }));
+  loop.Run();
+}
+
+TEST_P(ResultResponseTest, TestSuccessTrait) {
+  mojo::Remote<mojom::TestResultInterfaceWithTrait> remote;
+  TraitInterfaceImpl impl(remote.BindNewPipeAndPassReceiver());
+
+  base::RunLoop loop;
+  remote->TestSuccess(base::BindLambdaForTesting(
+      [&](base::expected<MappedResultValue, MappedResultError> result) {
+        ASSERT_EQ(1, result.value().magic_value);
+        loop.Quit();
+      }));
+  loop.Run();
+}
+
+TEST_P(ResultResponseTest, TestFailureTrait) {
+  mojo::Remote<mojom::TestResultInterfaceWithTrait> remote;
+  TraitInterfaceImpl impl(remote.BindNewPipeAndPassReceiver());
+
+  base::RunLoop loop;
+  remote->TestFailure(base::BindLambdaForTesting(
+      [&](base::expected<MappedResultValue, MappedResultError> result) {
+        ASSERT_TRUE(result.error().is_game_over_);
+        ASSERT_EQ(result.error().reason_, "meltdown!");
         loop.Quit();
       }));
   loop.Run();
