654b1e74021cabcb4b9eaf72a3fb4e83518b8a2b
Felipe Pazos <fpazos@google.com>
2025-04-02 11:16:47 -0700

    Refactor updater::UsageStatsProvider to use mocked location
    Now that the code has been refactored to use a `UsageStatsProvider`,
    the tests can be changed to point to a mocked location instead of the
    actual system values.
    
    Bug: 371595849
    Change-Id: I99a593630b94c9a069366d34d711f44c7a045b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6342890
    Feels: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Felipe Pazos <fpazos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1441645}

--- chrome/updater/update_usage_stats_task_linux.cc
+++ chrome/updater/update_usage_stats_task_linux.cc
@@ -21,10 +21,11 @@ class UsageStatsProviderImpl : public UsageStatsProvider {
   UsageStatsProviderImpl() = default;
 
   // TODO(crbug.com/40821596): Implement.
-  bool AnyAppEnablesUsageStats(UpdaterScope scope) override { return false; }
+  bool AnyAppEnablesUsageStats() override { return false; }
 };
 
-std::unique_ptr<UsageStatsProvider> UsageStatsProvider::Create() {
+std::unique_ptr<UsageStatsProvider> UsageStatsProvider::Create(
+    UpdaterScope scope) {
   return std::make_unique<UsageStatsProviderImpl>();
 }
 
