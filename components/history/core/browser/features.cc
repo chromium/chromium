5094e534f3bdfd0a4eb6331dc5e649c3293e10d7
Samuel Huang <huangs@chromium.org>
2025-04-02 11:14:38 -0700

    [Android MVT][Launch] Enable MostVisitedTilesNewScoring with Decay Staircase, Cap 10 (ToT by default).
    This CL changes the MVT scoring function to "Decay Staircase, Cap 10"
    on Android. The config changes are:
    * kMostVisitedTilesNewScoring: Disabled -> Enabled.
    * kMvtScoringParamRecencyFactor: Classic -> DecayStaircase.
    * kMvtScoringParamDailyVisitCountCap: INT_MAX -> 10.
    
    We will apply Finch 100% everywhere after this CL and the companion CL
    crrev.com/c/6416562 (to disable Repeatable Queries) land.
    
    Bug: 407528385
    Change-Id: I45039cf28f8749d9ca31e98bd2e8dc85af3f4e85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6418522
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Feels: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1441642}

--- components/history/core/browser/features.cc
+++ components/history/core/browser/features.cc
@@ -4,9 +4,6 @@
 
 #include "components/history/core/browser/features.h"
 
-#include <limits.h>
-
-#include "build/build_config.h"
 #include "components/history/core/browser/top_sites_impl.h"
 #include "components/sync/base/features.h"
 
@@ -84,6 +81,7 @@ BASE_FEATURE(kPopulateVisitedLinkDatabase,
 // If enabled, uses new scoring function for Most Visited Tiles computation.
 BASE_FEATURE(kMostVisitedTilesNewScoring,
              "MostVisitedTilesNewScoring",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             is_android ? base::FEATURE_ENABLED_BY_DEFAULT
+                        : base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace history
