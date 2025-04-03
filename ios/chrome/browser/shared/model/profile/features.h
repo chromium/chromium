e78b7829fc880c1029ece04c0e1a597b2d7853ff
Marc Treib <treib@chromium.org>
2025-04-03 01:15:14 -0700

    [iOS] Add ios/chrome/browser/shared/model/profile/features.h/mm files
    These are empty for now, and are in preparation for moving
    AreSeparateProfilesForManagedAccountsEnabled() here. It cannot be moved
    directly since there are some internal references to it which need to be
    updated to include the new file first, see https://crrev.com/i/8148658.
    
    Reason: ../shared/public/features/.. is used directly in EG tests. Some
    conditions (and dependencies) will soon be added to
    AreSeparateProfilesForManagedAccountsEnabled() which can not be used
    directly in EG tests, so it needs to move to a build target that's not
    used in EG tests.
    
    Bug: 383977223
    Change-Id: I769d65a008fc84a7b0a3107d5ade1fa245fd6a06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6409252
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442009}

---
+++ ios/chrome/browser/shared/model/profile/features.h
@@ -0,0 +1,14 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_FEATURES_H_
+#define IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_FEATURES_H_
+
+#import "base/feature_list.h"
+
+// TODO(crbug.com/383977223): Move
+// AreSeparateProfilesForManagedAccountsEnabled() here once internal references
+// are updated to include this file.
+
+#endif  // IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_FEATURES_H_
