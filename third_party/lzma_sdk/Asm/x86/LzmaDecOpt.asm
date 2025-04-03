b0db298afbd4a2c5de7232965f493b6d75f066b5
Joshua Pawlicki <waffles@chromium.org>
2025-04-03 06:02:27 -0700

    [M132-LTS] Reland "lzma_sdk: Update to 24.09."
    M132 merge issues:
      Conflict in README.chromium file: different license types
    
    This is a reland of commit 1d69891ae775c74724558585929c89438a6fda93
    
    Original change's description:
    > lzma_sdk: Update to 24.09.
    >
    > Files originate from https://7-zip.org/a/lzma2409.7z
    > SHA-256(./lzma2409.7z)=
    > 79b39f10b7b69eea293caa90c3e7ea07faf8f01f8ae9db1bb1b90c092375e5f3
    >
    > Fixed: 383772517, 40849176
    > Change-Id: I7ccefbcc0dc92f94ae43566fe9d8b962369ea8b8
    > Low-Coverage-Reason: OTHER - It's not clear how to exercise some of these conditionally-compiled alternatives.
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6091038
    > Commit-Queue: Will Harris <wfh@chromium.org>
    > Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1404227}
    
    (cherry picked from commit 968d9282d215058bc19a7c0cf22f2dcdea153740)
    
    Bug: 383772517, 388538957
    No-Presubmit: true
    Include-Ci-Only-Tests: true
    Change-Id: I3f36e233d52c0ba560a5d740111d1db62a32b8fc
    Low-Coverage-Reason: OTHER - It's not clear how to exercise some of these conditionally-compiled alternatives.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6163689
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1404814}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6363442
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Owners-Override: Michael Ershov <miersh@google.com>
    Commit-Queue: Roger Felipe Zanoni da Silva (xWF) <rzanoni@google.com>
    Cr-Commit-Position: refs/branch-heads/6834@{#5536}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- third_party/lzma_sdk/Asm/x86/LzmaDecOpt.asm
+++ third_party/lzma_sdk/Asm/x86/LzmaDecOpt.asm
@@ -1,5 +1,5 @@
 ; LzmaDecOpt.asm -- ASM version of LzmaDec_DecodeReal_3() function
-; 2021-02-23: Igor Pavlov : Public domain
+; 2024-06-18: Igor Pavlov : Public domain
 ;
 ; 3 - is the code compatibility version of LzmaDec_DecodeReal_*()
 ; function for check at link time.
@@ -17,11 +17,43 @@ include 7zAsm.asm
 
 MY_ASM_START
 
-LZMADEC SEGMENT ALIGN(64) 'CODE'
+; if Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT is     defined, we use additional SEGMENT with 64-byte alignment.
+; if Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT is not defined, we use default SEGMENT (where default 16-byte alignment of segment is expected).
+; The performance is almost identical in our tests.
+; But the performance can depend from position of lzmadec code inside instruction cache
+; or micro-op cache line (depending from low address bits in 32-byte/64-byte cache lines).
+; And 64-byte alignment provides a more consistent speed regardless
+; of the code's position in the executable.
+; But also it's possible that code without Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT can be
+; slightly faster than 64-bytes aligned code in some cases, if offset of lzmadec
+; code in 64-byte block after compilation provides better speed by some reason.
+; Note that Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT adds an extra section to the ELF file.
+; If you don't want to get that extra section, do not define Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT.
 
+ifndef Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT
+if (IS_LINUX gt 0)
+  Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT equ 1
+else
+  Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT equ 1
+endif
+endif
+
+ifdef Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT
+; Make this deterministic
+; _TEXT$LZMADECOPT SEGMENT ALIGN(64) 'CODE'
+LZMADEC SEGMENT ALIGN(64) 'CODE'
 MY_ALIGN macro num:req
         align  num
+        ; align  16
 endm
+else
+MY_ALIGN macro num:req
+        ; We expect that ".text" is aligned for 16-bytes.
+        ; So we don't need large alignment inside out function.
+        align  16
+endm
+endif
+
 
 MY_ALIGN_16 macro
         MY_ALIGN 16
@@ -610,7 +642,11 @@ PARAM_lzma      equ REG_ABI_PARAM_0
 PARAM_limit     equ REG_ABI_PARAM_1
 PARAM_bufLimit  equ REG_ABI_PARAM_2
 
+ifdef Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT
 ; MY_ALIGN_64
+else
+  MY_ALIGN_16
+endif
 MY_PROC LzmaDec_DecodeReal_3, 3
 MY_PUSH_PRESERVED_ABI_REGS
 
@@ -1298,6 +1334,8 @@ fin:
 MY_POP_PRESERVED_ABI_REGS
 MY_ENDP
 
+ifdef Z7_LZMA_DEC_OPT_ASM_USE_SEGMENT
 LZMADEC ENDS
+endif
 
 end
