d1d65957cdab9d519faee0c2bf24c7360fd1aa03
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-04-04 12:43:07 -0700

    Use GetOrCreateResourceProviderWithCurrentRasterModeHint consistently
    There were calls to GetOrCreateCanvasResourceProvider in
    CanvasResourceHost that were identical to calls to
    GetOrCreateResourceProviderWithCurrentRasterModeHint. For simplicity and
    uniformity, it's better to use the same function everywhere.
    
    Bug: 346766781
    Change-Id: Ic95dec1f47210e515a2dd5f03fe3de2e5407e0b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426232
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442851}

--- third_party/blink/renderer/platform/graphics/canvas_resource_host.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_host.cc
@@ -202,7 +202,7 @@ bool CanvasResourceHost::PrepareTransferableResource(
   // If the context is lost, we don't know if we should be producing GPU or
   // software frames, until we get a new context, since the compositor will
   // be trying to get a new context and may change modes.
-  if (!GetOrCreateCanvasResourceProvider(preferred_2d_raster_mode_)) {
+  if (!GetOrCreateResourceProviderWithCurrentRasterModeHint()) {
     return false;
   }
 
@@ -279,7 +279,7 @@ bool CanvasResourceHost::IsResourceValid() {
     return false;
   }
 
-  return !!GetOrCreateCanvasResourceProvider(preferred_2d_raster_mode_);
+  return !!GetOrCreateResourceProviderWithCurrentRasterModeHint();
 }
 
 }  // namespace blink
