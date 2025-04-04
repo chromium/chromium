c12d388b70687b801d11dae3440ac2bbfc19220c
Haiyang Pan <hypan@google.com>
2025-04-04 16:21:33 -0700

    Manual roll Android Lint from cpi5krkjlhoCfjDOZ... to vBqKHQdjLFM5l79fp...
    Maual: Suppress the lint error,
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/lint-chromium
    Please CC clank-build-core@google.com,clank-library-failures@google.com,wnwen@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: clank-library-failures@google.com
    Change-Id: Iccc0be25ec30e92baa18a5a4398bd1acbb85049b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6432318
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1442987}

--- components/webxr/android/java/src/org/chromium/components/webxr/XrHostActivity.java
+++ components/webxr/android/java/src/org/chromium/components/webxr/XrHostActivity.java
@@ -74,6 +74,7 @@ public class XrHostActivity extends Activity {
     }
 
     @Override
+    @SuppressWarnings("GestureBackNavigation")
     public void onBackPressed() {
         if (DEBUG_LOGS) Log.i(TAG, "onBackPressed");
         super.onBackPressed();
