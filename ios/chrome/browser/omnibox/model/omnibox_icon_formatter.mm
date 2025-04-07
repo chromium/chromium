7b7657762e5e88c1468c9303e2c2d81ba1dfb78f
Ameur Hosni <ameurhosni@google.com>
2025-04-07 08:12:47 -0700

    [IOS][OR] Move omnibox icon formatter to model folder
    Bug: 406737662
    Change-Id: I21f91a2d62b7d8d1cbb22403228739940ba81fdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437351
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443487}

--- ios/chrome/browser/omnibox/model/omnibox_icon_formatter.mm
+++ ios/chrome/browser/omnibox/model/omnibox_icon_formatter.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/omnibox/ui_bundled/popup/omnibox_icon_formatter.h"
+#import "ios/chrome/browser/omnibox/model/omnibox_icon_formatter.h"
 
 #import "base/notreached.h"
 #import "components/omnibox/browser/autocomplete_match.h"
