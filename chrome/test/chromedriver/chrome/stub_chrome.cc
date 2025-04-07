1aecfba7c061eb1de5b8c8c584ff8f681f0f5386
Maksim Sadym <sadym@google.com>
2025-04-07 06:05:09 -0700

    ChromeDriver runs BiDi Mapper in a hidden target
    Use hidden target to run BiDi CDP Mapper unless a flag
    `--debug-bidi-mapper` is set. In such case, run Mapper in a visible tab.
    
    Bug: 402339952
    Change-Id: I00230ad1bf93976afa60726bad5107a5d52dc0d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6433672
    Reviewed-by: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Maksim Sadym <sadym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443418}

--- chrome/test/chromedriver/chrome/stub_chrome.cc
+++ chrome/test/chromedriver/chrome/stub_chrome.cc
@@ -46,6 +46,12 @@ Status StubChrome::GetActivePageByWebViewId(const std::string& id,
   return Status(kOk);
 }
 
+Status StubChrome::NewHiddenTarget(const std::string& target_id,
+                                   bool w3c_compliant,
+                                   std::string* window_handle) {
+  return Status(kOk);
+}
+
 Status StubChrome::NewWindow(const std::string& target_id,
                              WindowType type,
                              bool is_background,
