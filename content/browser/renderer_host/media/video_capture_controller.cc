ceda0d1c3f1e42eb3d98b774822688628523ab5d
Henrique Ferreiro <hferreiro@igalia.com>
2025-04-08 01:07:08 -0700

    Replace VideoCaptureState with custom enum in VideoCaptureController
    VideoCaptureController is the only user of VideoCaptureState in
    browser-side code. Replace its usage with a custom enum. This change
    will allow to move third_party/blink/common/media/ to platform/media/, a
    prerequisite to finish the integration of platform/media/ in Blink.
    
    Bug: 40760651
    Change-Id: Iaa5e71e9e2e6927d29fa1497e91cb0ee7aeb69de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6424422
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1443975}

--- content/browser/renderer_host/media/video_capture_controller.cc
+++ content/browser/renderer_host/media/video_capture_controller.cc
@@ -14,7 +14,7 @@
 #include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/functional/bind.h"
-#include "base/functional/callback_forward.h"
+#include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
@@ -33,7 +33,7 @@
 #include "media/capture/video/video_capture_buffer_tracker_factory_impl.h"
 #include "media/capture/video/video_capture_device_client.h"
 #include "media/capture/video/video_capture_metrics.h"
-#include "services/video_effects/public/mojom/video_effects_processor.mojom-forward.h"
+#include "services/video_effects/public/mojom/video_effects_processor.mojom.h"
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "content/browser/compositor/image_transport_factory.h"
@@ -201,7 +201,6 @@ VideoCaptureController::VideoCaptureController(
       device_launcher_(std::move(device_launcher)),
       emit_log_message_cb_(std::move(emit_log_message_cb)),
       device_launch_observer_(nullptr),
-      state_(blink::VIDEO_CAPTURE_STATE_STARTING),
       has_received_frames_(false) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 }
@@ -255,7 +254,7 @@ void VideoCaptureController::AddClient(
   }
 
   // Signal error in case device is already in error state.
-  if (state_ == blink::VIDEO_CAPTURE_STATE_ERROR) {
+  if (state_ == State::kError) {
     event_handler->OnError(
         id,
         media::VideoCaptureError::kVideoCaptureControllerIsAlreadyInErrorState);
@@ -267,16 +266,15 @@ void VideoCaptureController::AddClient(
     return;
 
   // If the device has reported OnStarted event, report it to this client here.
-  if (state_ == blink::VIDEO_CAPTURE_STATE_STARTED)
+  if (state_ == State::kStarted) {
     event_handler->OnStarted(id);
+  }
 
   std::unique_ptr<ControllerClient> client =
       std::make_unique<ControllerClient>(id, event_handler, session_id, params);
   // If we already have gotten frame_info from the device, repeat it to the new
   // client.
-  if (state_ != blink::VIDEO_CAPTURE_STATE_ERROR) {
-    controller_clients_.push_back(std::move(client));
-  }
+  controller_clients_.push_back(std::move(client));
 }
 
 base::UnguessableToken VideoCaptureController::RemoveClient(
@@ -444,7 +442,7 @@ void VideoCaptureController::OnFrameReadyInBuffer(
       frame.buffer_id, frame.frame_feedback_id, std::move(frame.frame_info),
       &frame_context);
 
-  if (state_ != blink::VIDEO_CAPTURE_STATE_ERROR) {
+  if (state_ != State::kError) {
     // Inform all active clients of the frames.
     for (const auto& client : controller_clients_) {
       if (client->session_closed || client->paused)
@@ -551,7 +549,7 @@ void VideoCaptureController::OnBufferRetired(int buffer_id) {
 
 void VideoCaptureController::OnError(media::VideoCaptureError error) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
-  state_ = blink::VIDEO_CAPTURE_STATE_ERROR;
+  state_ = State::kError;
   PerformForClientsWithOpenSession(base::BindRepeating(&CallOnError, error));
 }
 
@@ -601,7 +599,7 @@ void VideoCaptureController::OnLog(const std::string& message) {
 void VideoCaptureController::OnStarted() {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   EmitLogMessage(__func__, 3);
-  state_ = blink::VIDEO_CAPTURE_STATE_STARTED;
+  state_ = State::kStarted;
   PerformForClientsWithOpenSession(base::BindRepeating(&CallOnStarted));
 }
 
