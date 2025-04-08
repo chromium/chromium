ee857208d2a6af468ac68f41407282cf63b05349
S. Ganesh <ganesh@chromium.org>
2025-04-08 16:18:51 -0700

    updater: fix timing issues with enterprise companion uninstall
    I was getting some occasional flakiness with the integration tests on
    windows without this change.
    
    Fixed: 409381853
    Change-Id: I3473f0da3f7c49537f3cca32856744856fa86548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6438775
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444438}

--- chrome/updater/app/app_uninstall.cc
+++ chrome/updater/app/app_uninstall.cc
@@ -22,6 +22,8 @@
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "base/threading/platform_thread.h"
+#include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/enterprise_companion/installer_paths.h"
 #include "chrome/updater/app/app.h"
@@ -112,6 +114,18 @@ namespace {
   }
   VLOG(1) << __func__ << ": Ran: " << command_line.GetCommandLineString()
           << ": " << output << ": " << exit_code;
+
+  // Wait until the enterprise companion install is completely removed. For
+  // instance, enterprise companion spawns a separate cmd script on Windows to
+  // complete the uninstall.
+  for (const auto deadline = base::TimeTicks::Now() + base::Seconds(20);
+       enterprise_companion::FindExistingInstall() &&
+       (base::TimeTicks::Now() < deadline);
+       base::PlatformThread::Sleep(base::Milliseconds(100))) {
+  }
+  VLOG(1) << __func__ << ": !enterprise_companion::FindExistingInstall(): "
+          << !enterprise_companion::FindExistingInstall();
+
   return exit_code == 0 ? kErrorOk : kErrorFailedToUninstallCompanionApp;
 }
 
