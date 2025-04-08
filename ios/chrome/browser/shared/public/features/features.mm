a25abcb9e9167cae48c16d090b52cae54613aa30
Robbie Gibson <rkgibson@google.com>
2025-04-08 14:11:26 -0700

    Reland "[iOS] Make use of the default status API"
    This is a reland of commit 925b71a1a3308507876f1f1229a02865c06137fa
    
    There were 2 issues in the initial commit that caused a revert:
    
    First, the Default Status API doesn't exist on maccatalyst, so many
    symbols are undefined there. This was fixed by skipping the default
    status checks on maccatalyst.
    
    Second, storing the fake system call directly caused a static
    initializer issue. This was fixed by storing the fake system call
    in a static variable declared in a method instead.
    
    Original change's description:
    > [iOS] Make use of the default status API
    >
    > Integrates with the Default Status system API. The integration works as
    > follows (high-level summary):
    >
    > - Clients that are on iOS 18.4+ and have completed the First Run
    >   Experience since at least 28 days are randomly assigned to a
    >   cohort number between 1 and 4 at startup. The idea is to spread
    >   clients out throughout the year:
    >     - Cohort 1: January, May, September
    >     - Cohort 2: February, June, October
    >     - Cohort 3: March, July, November
    >     - Cohort 4: April, August, December
    > - At each startup, if the current time is in or after the month
    >   corresponding to the client's cohort number for this quarter,
    >   the system's Default Status API is queried and the results are
    >   recorded to histograms and local prefs.
    > - Clients can still report their default status if they are outside
    >   their assigned cohort's reporting months, but they do so in
    >   different histograms, so that the impact of "cohort drift", a
    >   phenomenon where clients will tend to report later and later each
    >   year depending on usage frequency, can be better evaluated.
    >
    > Bug: 401075747
    > Change-Id: I97b28d0c41dff367abcff21de89f0fc5418a79aa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6282328
    > Reviewed-by: Robbie Gibson <rkgibson@google.com>
    > Commit-Queue: Robbie Gibson <rkgibson@google.com>
    > Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1439483}
    
    (cherry picked from commit ce2d1034d5a43f861ee67566a8fe02a19b47e171)
    
    Bug: 401075747
    Change-Id: I5345263c83ba7829f834c2948f8ae8097ce103e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6409732
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1441828}
    Bug: 408251065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6442097
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7103@{#479}
    Cr-Branched-From: e09430c64983fc906f37a9f7e6806275c9b67b86-refs/heads/main@{#1440670}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1353,3 +1353,11 @@ BASE_FEATURE(kNTPBackgroundCustomization,
 bool IsNTPBackgroundCustomizationEnabled() {
   return base::FeatureList::IsEnabled(kNTPBackgroundCustomization);
 }
+
+BASE_FEATURE(kRunDefaultStatusCheck,
+             "RunDefaultStatusCheck",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsRunDefaultStatusCheckEnabled() {
+  return base::FeatureList::IsEnabled(kRunDefaultStatusCheck);
+}
