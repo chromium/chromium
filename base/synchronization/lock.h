64977e10b188ff29d309ff99e4b9748edf7ed608
Anand Ravi <anandrv@google.com>
2025-04-08 08:16:18 -0700

    [base][win] Update comment about priority inversion mitigation
    Windows 11 changed how priority inversion is mitigated. This commit
    updates the comment in base/lock.h to reflect the change.
    
    Bug: 384902323
    Change-Id: I53b493f359476962cadbaaa78517382a141b080e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6416636
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Nafis Abedin <nafisabedin@google.com>
    Commit-Queue: Anand Ravi <anandrv@google.com>
    Reviewed-by: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444131}

--- base/synchronization/lock.h
+++ base/synchronization/lock.h
@@ -92,9 +92,12 @@ class LOCKABLE BASE_EXPORT Lock {
   // priorities.
   static bool HandlesMultipleThreadPriorities() {
 #if BUILDFLAG(IS_WIN)
-    // Windows mitigates priority inversion by randomly boosting the priority of
-    // ready threads.
-    // https://msdn.microsoft.com/library/windows/desktop/ms684831.aspx
+    // Prior to Windows 11, Windows mitigated priority inversion by randomly
+    // boosting the priority of ready threads. From Windows 11 onwards, priority
+    // inversion mitigation works similar to POSIX through a facility called
+    // AutoBoost which sets the priority floor of the thread holding the lock to
+    // the maximum priority of its waiters.
+    // https://github.com/MicrosoftDocs/win32/commit/a43cb3b5039c5cfc53642bfcea174003a2f1168f
     return true;
 #elif BUILDFLAG(IS_POSIX) || BUILDFLAG(IS_FUCHSIA)
     // POSIX mitigates priority inversion by setting the priority of a thread
