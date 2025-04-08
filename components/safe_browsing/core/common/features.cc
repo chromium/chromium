3e5284e6c6235271f1d68ea16171b788bf0ed884
Seung Jae Lim <andysjlim@chromium.org>
2025-04-08 14:44:44 -0700

    Enable ClientSideDetectionSendLlamaForcedTriggerInfo by default
    Bug: 390394822
    Change-Id: I8ab73092ef5e0fd1956549d3c7232e12e37069e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6439266
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444388}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -62,7 +62,7 @@ BASE_FEATURE(kClientSideDetectionKillswitch,
 
 BASE_FEATURE(kClientSideDetectionSendLlamaForcedTriggerInfo,
              "ClientSideDetectionSendLlamaForcedTriggerInfo",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kClientSideDetectionLlamaForcedTriggerInfoForScamDetection,
              "ClientSideDetectionLlamaForcedTriggerInfoForScamDetection",
