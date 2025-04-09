cc9f4d47aed82b2dfb8efc3ccd9b10b64c86838f
Jean-Philippe Gravel <jpgravel@chromium.org>
2025-04-09 07:49:40 -0700

    Remove call to ClearTexture in CanvasResourceHost::ResetLayer
    This function is redundant because the following call to ClearClient
    already calls ClearTexture.
    
    Bug: 346766781
    Change-Id: Ied1d9364b20278a303fc64554f004b4b654b61df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6440086
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1444719}

--- third_party/blink/renderer/platform/graphics/canvas_resource_host.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_host.cc
@@ -113,7 +113,6 @@ RasterMode CanvasResourceHost::GetRasterMode() const {
 
 void CanvasResourceHost::ResetLayer() {
   if (cc_layer_) {
-    cc_layer_->ClearTexture();
     // Orphaning the layer is required to trigger the recreation of a new
     // layer in the case where destruction is caused by a canvas resize. Test:
     // virtual/gpu/fast/canvas/canvas-resize-after-paint-without-layout.html
