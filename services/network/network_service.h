8eaa863ca968503249fcc85b6fa49f97fa0aa407
Dheeraj Pannala <kpannala@microsoft.com>
2025-04-10 07:07:01 -0700

    RELAND "Fix truncated NetLogs by introducing the --net-log-duration flag"
    Net logs generated via the command line were often
    truncated due to insufficient mechanisms to catch
    close event.This change introduces the --net-log-duration
    flag,which specifies the duration (in seconds)
    for network logging. When provided with a positive
    integer X, Browser will automatically stop logging
    after X seconds and flush the complete NetLogs to disk.
    
    Original CL reviewed on - https://chromium-review.googlesource.com/c/chromium/src/+/6293542
    
    Reason for CL failure - The test "StartsNetLogWithDuration"
    did not invoke the Shutdown method. Consequently, the
    network service was interfering with other tests utilizing
    the network service on mac ARM64 machines.
    
    Bug: 40283474
    Change-Id: I4cb59330eb2cf9fa787d4f70ec3147edddff7aac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6387620
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: krishna dheeraj Pannala <kpannala@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1445269}

--- services/network/network_service.h
+++ services/network/network_service.h
@@ -161,7 +161,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
   void StartNetLog(base::File file,
                    uint64_t max_total_size,
                    net::NetLogCaptureMode capture_mode,
-                   base::Value::Dict constants) override;
+                   base::Value::Dict constants,
+                   std::optional<base::TimeDelta> duration) override;
   void AttachNetLogProxy(
       mojo::PendingRemote<mojom::NetLogProxySource> proxy_source,
       mojo::PendingReceiver<mojom::NetLogProxySink>) override;
@@ -408,6 +409,8 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
 
   void DestroyNetworkContexts();
 
+  void StopNetLog();
+
   // Called by a NetworkContext when its mojo pipe is closed. Deletes the
   // context.
   void OnNetworkContextConnectionClosed(NetworkContext* network_context);
@@ -554,7 +557,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkService
   std::unique_ptr<network::tpcd::metadata::Manager> tpcd_metadata_manager_;
 
   bool exclusive_cookie_database_locking_ = true;
-
   base::WeakPtrFactory<NetworkService> weak_factory_{this};
 };
 
