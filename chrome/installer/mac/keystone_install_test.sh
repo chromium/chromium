7842473fb04e4dad8cb7096ca5e95ac55c8f1770
Adam Norberg <norberg@google.com>
2025-04-10 10:01:47 -0700

    Verify ksadmin calls in keystone_install_test.sh.
    Bug: 409435559
    Change-Id: I4f260ebf6cd2c3eb91885d7b9aa1ab80ef11ecb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6444146
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Adam Norberg <norberg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445356}

--- chrome/installer/mac/keystone_install_test.sh
+++ chrome/installer/mac/keystone_install_test.sh
@@ -6,6 +6,8 @@
 
 # Test of the Mac Chrome update script.
 # Runs from the same directory as `keystone_install.sh` itself.
+# Please note: this will destroy the brand code file for your user install of
+#     Chrome, if any.
 
 
 # Where I am
@@ -24,11 +26,15 @@ APPNAME_CANARY="Google Chrome Canary.app"
 FWKNAME="Google Chrome Framework.framework"
 
 # The version number for fake ksadmin to pretend to be
-KSADMIN_VERSION_LIE="1.0.7.1306"
+KSADMIN_VERSION_LIE="1.0.9.2318"
 
 # Temp directory to be used as the disk image (source)
 TEMPDIR=$(mktemp -d -t $(basename ${0}))
 PATH=$PATH:"${TEMPDIR}"
+OUTDIR="${TEMPDIR}/out"
+OUTFILE="${OUTDIR}/register_flags.txt"
+
+echo "using tempdir ${TEMPDIR}"
 
 # Clean up the temp directory
 function cleanup_tempdir() {
@@ -55,6 +61,7 @@ function fail_installer() {
   else
     echo "  Successfully failed with return code ${RETURN}"
   fi
+  assert_zero_registration_calls
 }
 
 # Make sure installer works!
@@ -70,6 +77,91 @@ function pass_installer() {
   else
     echo "  Succeeded"
   fi
+  assert_one_registration_call
+}
+
+function prepare_fake_ksadmin() {
+  if [ -e "${OUTDIR}" ] ; then
+   rm -rf "${OUTDIR}"
+  fi
+  mkdir "${OUTDIR}"
+  cat >"${TEMPDIR}"/ksadmin <<EOF
+#!/bin/bash
+if [ "\${1}" = "--ksadmin-version" ] ; then
+  # version check
+  echo "${KSADMIN_VERSION_LIE}"
+  exit 0
+fi
+if [ "\${1}" = "-pP" ] ; then
+  # finding app to update
+  echo " xc=<KSPathExistenceChecker:0x45 path=${DEST}>"
+  exit 0
+fi
+# assume it is registration; prepare to save args
+# rotate last flag file
+if [ -e "${OUTFILE}" ] ; then
+  n=1
+  while [ -e "${OUTFILE}.old.\${n}" ] ; do
+   n=\$(( n+1 ))
+  done
+  mv "${OUTFILE}" "${OUTFILE}.old.\${n}"
+fi
+# now actually save args
+touch "${OUTFILE}"
+while (( "\$#" )) ; do
+  echo "\${1}" >> "${OUTFILE}"
+  shift
+done
+exit 0
+EOF
+  chmod u+x "${TEMPDIR}"/ksadmin
+}
+
+function assert_zero_registration_calls() {
+  if [ -e "${OUTFILE}" ] ; then
+    echo "  FAILED. ksadmin unexpectedly invoked with registration-like flags"
+    echo "    See ${OUTDIR}"
+    exit 1
+  fi
+}
+
+function assert_one_registration_call() {
+  if [ ! -e "${OUTFILE}" ] ; then
+    echo "  FAILED. ksadmin not invoked for registration"
+    exit 1
+  fi
+  if [ -e "${OUTFILE}.old.1" ] ; then
+    echo "  FAILED. multiple registrations during invocation"
+    echo "    See ${OUTDIR}"
+    exit 1
+  fi
+}
+
+function expect_registration_flag() {
+  while read -r line; do
+    if [ "${line}" == "${1}" ] ; then
+      if read -r val && [ "${val}" == "${2}" ] ; then
+        return 0
+      fi
+      echo "  \"${1}\" was \"${val}\", wanted \"${2}\""
+      return 1
+    fi
+  done < "${OUTFILE}"
+  echo "  \"${1}\" absent, wanted \"${2}\""
+  return 1
+}
+
+function assert_registration_flags() {
+  local failure=0
+  while (( "$#" )) ; do
+    expect_registration_flag "${1}" "${2}"
+    failure=$(( $? + failure))
+    shift 2
+  done
+  if [[ ${failure} -gt 0 ]] ; then
+    echo "  FAILED. ${failure} bad flags."
+    exit 1
+  fi
 }
 
 # Make an old-style destination directory, to test updating from old-style
@@ -80,16 +172,7 @@ function make_old_dest() {
   rm -rf "${DEST}"
   mkdir -p "${DEST}"/Contents
   defaults write "${DEST}/Contents/Info" KSVersion 0
-  cat >"${TEMPDIR}"/ksadmin <<EOF
-#!/bin/sh
-if [ "\${1}" = "--ksadmin-version" ] ; then
-  echo "${KSADMIN_VERSION_LIE}"
-  exit 0
-fi
-echo " xc=<KSPathExistenceChecker:0x45 path=${DEST}>"
-exit 0
-EOF
-  chmod u+x "${TEMPDIR}"/ksadmin
+  prepare_fake_ksadmin
 }
 
 # Make a new-style destination directory, to test updating between new-style
@@ -100,16 +183,7 @@ function make_new_dest() {
   rm -rf "${DEST}"
   defaults write "${DEST}/Contents/Info" CFBundleShortVersionString 0
   defaults write "${DEST}/Contents/Info" KSVersion 0
-  cat >"${TEMPDIR}"/ksadmin <<EOF
-#!/bin/sh
-if [ "\${1}" = "--ksadmin-version" ] ; then
-  echo "${KSADMIN_VERSION_LIE}"
-  exit 0
-fi
-echo " xc=<KSPathExistenceChecker:0x45 path=${DEST}>"
-exit 0
-EOF
-  chmod u+x "${TEMPDIR}"/ksadmin
+  prepare_fake_ksadmin
 }
 
 # Make a simple source directory - the update that is to be applied
@@ -144,6 +218,18 @@ function remove_old_brand_code() {
   rm "$HOME/Library/Google/Google Chrome Brand.plist"
 }
 
+function assert_registration() {
+  assert_registration_flags "--productid" "com.google.Chrome" "--version" "2" \
+      "--xcpath" "${DEST}" "--version-path" "${DEST}/Contents/Info.plist" \
+      "--version-key" "KSVersion"
+}
+
+function assert_brand_file_registered() {
+  assert_registration_flags \
+      "--brand-path" "${HOME}/Library/Google/Google Chrome Brand.plist" \
+      "--brand-key" "KSBrandID"
+}
+
 fail_installer "No source anything" 2
 
 mkdir "${TEMPDIR}"/"${APPNAME_STABLE}"
@@ -161,17 +247,21 @@ make_old_dest
 defaults write "${TEMPDIR}/${APPNAME_STABLE}/Contents/Info" \
     KSUpdateURL "http://foobar"
 pass_installer "Old-style update"
+assert_registration
 
 make_basic_src_and_dest
 defaults write "${TEMPDIR}/${APPNAME_STABLE}/Contents/Info" \
     KSUpdateURL "http://foobar"
 pass_installer "New-style Stable"
+assert_registration
 
 make_old_brand_code
 make_basic_src_and_dest
 defaults write "${TEMPDIR}/${APPNAME_STABLE}/Contents/Info" \
     KSUpdateURL "http://foobar"
 pass_installer "Old brand code Stable"
+assert_registration
+assert_brand_file_registered
 remove_old_brand_code
 
 make_src "${APPNAME_CANARY}"
@@ -179,5 +269,6 @@ make_new_dest
 defaults write "${TEMPDIR}/${APPNAME_CANARY}/Contents/Info" \
     KSUpdateURL "http://foobar"
 pass_installer "New-style Canary"
+assert_registration
 
 cleanup_tempdir
