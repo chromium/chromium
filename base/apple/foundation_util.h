18400edb9f0d87f64adb0bec1eeb8ef49f88c041
Arthur Milchior <arthurmilchior@google.com>
2025-04-10 08:19:08 -0700

    [base]Improve ObjCCast documentation
    Recently I introduced a bug caused by using
    `ObjCCast<ClassName<ProtocolName>>`, and checking its return value for
    `nil`. While this allowed me to discriminate objects of ClassName from
    object of other classes, I realized that it didn’t properly
    discriminate for ProtocolName. I hope that making it explicit in the
    description will ensure less people commit the same error.
    
    Admittedly, it’d be even better to ensure that `T` has no protocol specifier. But I’ve no idea how it could be done.
    
    Bug: None
    Change-Id: If33996107172f4549952ed9be9d04aa416a1d17f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6441133
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445313}

--- base/apple/foundation_util.h
+++ base/apple/foundation_util.h
@@ -198,11 +198,13 @@ CF_CAST_DECL(SecPolicy);
 #if defined(__OBJC__)
 
 // ObjCCast<>() and ObjCCastStrict<>() cast a basic id to a more specific
-// (NSObject-derived) type. The compatibility of the passed object is found by
-// checking if it's a kind of the requested type identifier. If the supplied
-// object is not compatible with the requested return type, ObjCCast<>() returns
-// nil and ObjCCastStrict<>() will CHECK. Providing a nil pointer to either
-// variant results in nil being returned without triggering any CHECK.
+// (NSObject-derived) class type. The compatibility of the passed object is
+// found by checking if it's a kind of the requested class type identifier. If
+// the supplied object is not compatible with the class of the requested return
+// type, ObjCCast<>() returns nil and ObjCCastStrict<>() will CHECK. Providing a
+// nil pointer to either variant results in nil being returned without
+// triggering any CHECK. Any protocol specified in the requested return type are
+// ignored.
 //
 // The strict variant is useful when retrieving a value from a collection which
 // only has values of a specific type, e.g. an NSArray of NSStrings. The
