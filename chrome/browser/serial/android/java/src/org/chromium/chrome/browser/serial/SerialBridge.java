1f67bef54aa8ca28ac38cd3168cb3daf6e714fb8
Calder Kitagawa <ckitagawa@chromium.org>
2025-04-11 04:34:43 -0700

    [NullAway] NullMark c/b/serial
    Bug: 389129271
    Change-Id: I5f8f512929679fc5802bf0163eb0002a17623d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6446793
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445782}

--- chrome/browser/serial/android/java/src/org/chromium/chrome/browser/serial/SerialBridge.java
+++ chrome/browser/serial/android/java/src/org/chromium/chrome/browser/serial/SerialBridge.java
@@ -6,11 +6,14 @@ package org.chromium.chrome.browser.serial;
 
 import org.jni_zero.NativeMethods;
 
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.content_public.browser.WebContents;
 
 /** Java access point for SerialBridge, allowing for querying serial port state. */
+@NullMarked
 public class SerialBridge {
-    public static boolean isWebContentsConnectedToSerialPort(WebContents webContents) {
+    public static boolean isWebContentsConnectedToSerialPort(@Nullable WebContents webContents) {
         if (webContents == null) return false;
         return SerialBridgeJni.get().isWebContentsConnectedToSerialPort(webContents);
     }
