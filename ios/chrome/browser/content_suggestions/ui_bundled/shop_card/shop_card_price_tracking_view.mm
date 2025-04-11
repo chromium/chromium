0c7791efa36fbd4e5cd1740d2cc14fe6c4f070b0
Gloria Fang <gloriafang@google.com>
2025-04-11 15:09:27 -0700

    [shopcard] Add button for Arm 4. Button + placeholder UI for track prices arm of ShopCard.
    Unaffected buttons: screen/9qy2MHsVC2faeo6, screen/9ugzpcEDDLdrS77
    
    Screenshot: screen/4weEYgdEYYKoaVz
    Change-Id: I30f45eaee5aae16b61c69e31c628d6e031773499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6438520
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Gloria Fang <gloriafang@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446102}

--- ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_price_tracking_view.mm
+++ ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_price_tracking_view.mm
@@ -4,11 +4,33 @@
 
 #import "ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_price_tracking_view.h"
 
+#import "base/strings/sys_string_conversions.h"
+#import "components/url_formatter/elide_url.h"
+#import "ios/chrome/browser/content_suggestions/ui_bundled/shop_card/shop_card_data.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/tab_resumption/tab_resumption_item.h"
+#import "ios/chrome/browser/price_notifications/ui_bundled/cells/price_notifications_track_button.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "url/gurl.h"
+
+namespace {
+
+const CGFloat kVerticalStackSpacing = 6.0f;
+const CGFloat kContentStackSpacing = 2.0f;
+
+}  // namespace
 
 @implementation ShopCardPriceTrackingView {
   // Item used to configure the view.
   TabResumptionItem* _item;
+
+  UIStackView* _textStack;
+  UILabel* _titleLabel;
+  UILabel* _urlLabel;
+
+  UIStackView* _contentStack;
+  UIButton* _trackPriceButton;
 }
 
 - (instancetype)initWithItem:(TabResumptionItem*)item {
@@ -16,7 +38,66 @@
   if (self) {
     _item = item;
   }
+
+  _trackPriceButton =
+      [[PriceNotificationsTrackButton alloc] initWithLightVariant:YES];
+  [_trackPriceButton addTarget:self
+                        action:@selector(trackItem)
+              forControlEvents:UIControlEventTouchUpInside];
+
+  // Lay out text stack
+  [self populateTitleLabel];
+  [self populateUrlLabel];
+  _textStack = [[UIStackView alloc]
+      initWithArrangedSubviews:@[ _titleLabel, _urlLabel ]];
+  _textStack.axis = UILayoutConstraintAxisVertical;
+  _textStack.alignment = UIStackViewAlignmentLeading;
+  _textStack.spacing = kVerticalStackSpacing;
+
+  _contentStack = [[UIStackView alloc]
+      initWithArrangedSubviews:@[ _textStack, _trackPriceButton ]];
+  _contentStack.translatesAutoresizingMaskIntoConstraints = NO;
+  _contentStack.spacing = kContentStackSpacing;
+  _contentStack.alignment = UIStackViewAlignmentCenter;
+  _contentStack.axis = UILayoutConstraintAxisHorizontal;
+  [self addSubview:_contentStack];
+  AddSameConstraints(_contentStack, self);
+
   return self;
+}
+
+- (void)populateTitleLabel {
+  _titleLabel = [[UILabel alloc] init];
+  _titleLabel.textColor = [UIColor colorNamed:kTextPrimaryColor];
+  _titleLabel.numberOfLines = 1;
+  _titleLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+  _titleLabel.font =
+      PreferredFontForTextStyle(UIFontTextStyleFootnote, UIFontWeightSemibold);
+  _titleLabel.adjustsFontForContentSizeCategory = YES;
+  _titleLabel.text = _item.shopCardData.productTitle;  //@"placeholder title";
+}
+
+- (void)populateUrlLabel {
+  _urlLabel = [[UILabel alloc] init];
+  _urlLabel.text = [self hostnameFromGURL:_item.shopCardData.productURL];
+  _urlLabel.numberOfLines = 1;
+  _urlLabel.lineBreakMode = NSLineBreakByTruncatingTail;
+  _urlLabel.font = [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
+  _urlLabel.adjustsFontForContentSizeCategory = YES;
+  _urlLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
+}
+
+// Initiates price tracking.
+- (void)trackItem {
+  // TODO(crbug.com/392973604): add price tracking action.
+}
+
+// Returns the tab hostname from the given `URL`.
+- (NSString*)hostnameFromGURL:(GURL)URL {
+  return base::SysUTF16ToNSString(
+      url_formatter::
+          FormatUrlForDisplayOmitSchemePathTrivialSubdomainsAndMobilePrefix(
+              URL));
 }
 
 @end
