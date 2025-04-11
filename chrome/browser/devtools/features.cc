71c2f21932d0271464c424d0dfb78e93bcf85b21
Alina Varkki <alinavarkki@chromium.org>
2025-04-11 02:19:04 -0700

    Enable `DevToolsAiGeneratedTimelineLabels` by default 🎉
    Bug: 393063467
    Change-Id: I743501a168e00511eafb8663915d242db8846665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6448369
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alina Varkki <alinavarkki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445748}

--- chrome/browser/devtools/features.cc
+++ chrome/browser/devtools/features.cc
@@ -155,7 +155,7 @@ BASE_FEATURE(kDevToolsCssValueTracing,
 // Whether the DevTools AI generated annotation labels in timeline are enabled.
 BASE_FEATURE(kDevToolsAiGeneratedTimelineLabels,
              "DevToolsAiGeneratedTimelineLabels",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 // If enabled, DevTools does not accept remote debugging connections unless
