8ff51de4fbfb691f1fc1cd8396d98ca9e60963d9
Arthur Milchior <arthurmilchior@google.com>
2025-04-11 09:09:16 -0700

    [iOS]Repair multi profile with eg test signin
    Previously, the continuation was NotReached. Instead, it should have
    simulated the fact that the authentication flow and the helper were
    not being retained anymore, in order to be consistent with the actual
    implementation where a change of profile stop the current scene.
    
    Bug: 375605572
    Change-Id: I73399f8d1ae6337c615c6e9ec176ab3e575d12de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447436
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1445921}

--- ios/chrome/test/app/signin_test_util.mm
+++ ios/chrome/test/app/signin_test_util.mm
@@ -20,6 +20,7 @@
 #import "ios/chrome/browser/authentication/ui_bundled/authentication_flow/authentication_flow_request_helper.h"
 #import "ios/chrome/browser/authentication/ui_bundled/authentication_flow/test_authentication_flow_request_helper.h"
 #import "ios/chrome/browser/authentication/ui_bundled/cells/signin_promo_view.h"
+#import "ios/chrome/browser/authentication/ui_bundled/continuation.h"
 #import "ios/chrome/browser/authentication/ui_bundled/history_sync/history_sync_utils.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
@@ -170,12 +171,31 @@ void SignIn(id<SystemIdentity> identity) {
                     anchorView:nil
                     anchorRect:CGRectNull];
   // The delegate is retaining itself and the flow.
-  __block TestAuthenticationFlowRequest* testRequestHelper =
-      [[TestAuthenticationFlowRequest alloc]
-          initWithSigninCompletionCallback:^(SigninCoordinatorResult result) {
-            authenticationFlow = nil;
-            testRequestHelper = nil;
-          }];
+  __block TestAuthenticationFlowRequest* testRequestHelper = nil;
+  // Unsetting those variables to ensure that they are not retained anymore.
+  // The authentication flow should retain them.
+  void (^unsetVariables)() = ^() {
+    authenticationFlow = nil;
+    testRequestHelper = nil;
+  };
+  signin_ui::SigninCompletionCallback callback =
+      ^(SigninCoordinatorResult result) {
+        unsetVariables();
+      };
+  ChangeProfileContinuationProvider provider = base::BindRepeating(
+      [](void (^unsetVariables)()) {
+        return base::BindOnce(
+            [](void (^unsetVariables)(), SceneState*,
+               base::OnceClosure closure) {
+              unsetVariables();
+              std::move(closure).Run();
+            },
+            unsetVariables);
+      },
+      unsetVariables);
+  testRequestHelper = [[TestAuthenticationFlowRequest alloc]
+       initWithSigninCompletionCallback:callback
+      changeProfileContinuationProvider:provider];
   authenticationFlow.requestHelper = testRequestHelper;
   [authenticationFlow startSignIn];
 }
