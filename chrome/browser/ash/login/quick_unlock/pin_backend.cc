60690ab9c00576eab9f979e237f4b95443008e9d
Renato Silva <rrsilva@google.com>
2025-04-11 05:32:32 -0700

    [login] Track PIN autosubmit backfill
    Track when the PIN autosubmit backfill operation is being performed.
    
    Bug: b:268299968
    Change-Id: I6b62768ad27e95856944a4106f679bea799a7fc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6440289
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1445800}

--- chrome/browser/ash/login/quick_unlock/pin_backend.cc
+++ chrome/browser/ash/login/quick_unlock/pin_backend.cc
@@ -8,6 +8,7 @@
 
 #include "ash/constants/ash_features.h"
 #include "base/base64.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
@@ -614,6 +615,9 @@ void PinBackend::PinAutosubmitBackfill(const AccountId& account_id,
   if (PrefService(account_id)
           ->GetUserPrefValue(::prefs::kPinUnlockAutosubmitEnabled) != nullptr)
     return;
+
+  // Track when this operation is being performed.
+  base::debug::DumpWithoutCrashing();
 
   // Disabled if not allowed by policy. Since 'kPinUnlockAutosubmitEnabled'
   // is enabled by default, it is only false when recommended/mandatory by
