8f8eed7c88d0169866aa1a273cea1bceb250946b
Shu Yang <shuyng@google.com>
2025-04-14 14:04:51 -0700

    Add @NullMarked to components/privacy_sandbox/android
    Bug: 389129271
    Change-Id: Ia602f22f424a53925a02929ad881381d87f79a02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6454157
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Shu Yang <shuyng@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446754}

--- components/privacy_sandbox/android/java/src/org/chromium/components/privacy_sandbox/IncognitoTrackingProtectionsFragment.java
+++ components/privacy_sandbox/android/java/src/org/chromium/components/privacy_sandbox/IncognitoTrackingProtectionsFragment.java
@@ -8,14 +8,17 @@ import android.os.Bundle;
 
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
 
 /** Fragment to manage settings for incognito tracking protections. */
+@NullMarked
 public class IncognitoTrackingProtectionsFragment extends PrivacySandboxBaseFragment {
     private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
 
     @Override
-    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+    public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         SettingsUtils.addPreferencesFromResource(
                 this, R.xml.incognito_tracking_protections_preferences);
         mPageTitle.set(getString(R.string.incognito_tracking_protections_page_title));
