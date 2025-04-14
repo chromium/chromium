4c4d92eccdb91b5f611e8621ef5c03899079e53b
Abe Boujane <boujane@google.com>
2025-04-14 15:55:49 -0700

    Remove ps servicedependency on notice storage
    This is step 1/2 to remove the direct dependency on notice storage
    from the privacy sandbox service.
    
    this cl also modifies the NoticeServiceInterface so it inherits from
    the KeyedService.
    
    Bug: 409376841
    Change-Id: I9fb2fc6a3ee1d7db6be3f7c07b8d987423788052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6455304
    Reviewed-by: Aashna Sheth <aashnas@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446817}

--- chrome/browser/privacy_sandbox/notice/notice_service_interface.h
+++ chrome/browser/privacy_sandbox/notice/notice_service_interface.h
@@ -8,16 +8,15 @@
 #include <vector>
 
 #include "chrome/browser/privacy_sandbox/notice/notice.mojom-forward.h"
+#include "components/keyed_service/core/keyed_service.h"
 
 namespace privacy_sandbox {
 
 enum class SurfaceType;
 
 // This framework communicates to the view manager via this interface.
-class PrivacySandboxNoticeServiceInterface {
+class PrivacySandboxNoticeServiceInterface : public KeyedService {
  public:
-  virtual ~PrivacySandboxNoticeServiceInterface() = default;
-
   // Returns a required list of notices to show.
   virtual std::vector<notice::mojom::PrivacySandboxNotice> GetRequiredNotices(
       SurfaceType surface) = 0;
