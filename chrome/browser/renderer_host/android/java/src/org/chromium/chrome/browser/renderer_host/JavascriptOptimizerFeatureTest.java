a139be7a2cc5339d7c9ab45ddb3ea3fc37fe0035
Peter Kotwicz <pkotwicz@google.com>
2025-04-14 10:19:48 -0700

    Refactor JavascriptOptimizerFeatureTest
    This CL:
    - Adds tests for
    AdvancedProtectionStatusManagerAndroidBridge#isAdvancedProtectionRequestedByOs()
    - Removes some tests in JavascriptOptimizerFeatureTest which are
      redundant to the new AdvancedProtectionStatusManagerAndroidBridge
      tests.
    - Makes the JavascriptOptimizerFeatureTest work as Batched tests
    
    BUG=401528684
    
    Change-Id: I62e05b03dc9a104d52336526eed6bc2d18c1eb4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426304
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1446620}

--- chrome/browser/renderer_host/android/java/src/org/chromium/chrome/browser/renderer_host/JavascriptOptimizerFeatureTest.java
+++ chrome/browser/renderer_host/android/java/src/org/chromium/chrome/browser/renderer_host/JavascriptOptimizerFeatureTest.java
@@ -10,24 +10,22 @@ import static org.junit.Assert.assertTrue;
 import androidx.test.filters.MediumTest;
 
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import org.chromium.base.ServiceLoaderUtil;
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DoNotBatch;
-import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
+import org.chromium.chrome.test.util.AdvancedProtectionTestRule;
 import org.chromium.components.browser_ui.site_settings.WebsitePreferenceBridge;
 import org.chromium.components.content_settings.ContentSettingValues;
 import org.chromium.components.content_settings.ContentSettingsType;
-import org.chromium.components.permissions.OsAdditionalSecurityPermissionProvider;
-import org.chromium.components.permissions.PermissionsAndroidFeatureList;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.common.ContentSwitches;
 import org.chromium.net.test.EmbeddedTestServer;
@@ -41,33 +39,26 @@ import org.chromium.url.GURL;
     ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE,
     ContentSwitches.SITE_PER_PROCESS
 })
-@DoNotBatch(reason = "Tests manipulate global profile state")
+@Batch(Batch.PER_CLASS)
 public class JavascriptOptimizerFeatureTest {
     private static final String TEST_PAGE = "/chrome/test/data/android/test.html";
-
-    private static class TestPermissionProvider extends OsAdditionalSecurityPermissionProvider {
-        private boolean mIsAdvancedProtectionRequestedByOs;
-
-        public TestPermissionProvider(boolean isAdvancedProtectionRequestedByOs) {
-            mIsAdvancedProtectionRequestedByOs = isAdvancedProtectionRequestedByOs;
-        }
-
-        @Override
-        public boolean isAdvancedProtectionRequestedByOs() {
-            return mIsAdvancedProtectionRequestedByOs;
-        }
-    }
 
     private EmbeddedTestServer mTestServer;
 
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
 
+    @ClassRule
+    public static AdvancedProtectionTestRule sAdvancedProtectionRule =
+            new AdvancedProtectionTestRule();
+
     @Before
     public void setUp() {
         EmbeddedTestServerRule embeddedTestServerRule =
                 mActivityTestRule.getEmbeddedTestServerRule();
         mTestServer = embeddedTestServerRule.getServer();
+        sAdvancedProtectionRule.setIsAdvancedProtectionRequestedByOs(false);
+        mActivityTestRule.startMainActivityOnBlankPage();
     }
 
     private boolean queryJavascriptOptimizersEnabledForActiveWebContents() {
@@ -79,77 +70,26 @@ public class JavascriptOptimizerFeatureTest {
                 });
     }
 
-    /** Test that the provider is not queried when the kill switch is on. */
-    @Test
-    @EnableFeatures({PermissionsAndroidFeatureList.OS_ADDITIONAL_SECURITY_PERMISSION_KILL_SWITCH})
-    @MediumTest
-    public void testKillSwitchOn() {
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    ServiceLoaderUtil.setInstanceForTesting(
-                            OsAdditionalSecurityPermissionProvider.class,
-                            new TestPermissionProvider(
-                                    /* isAdvancedProtectionRequestedByOs= */ true));
-                });
-
-        mActivityTestRule.startMainActivityOnBlankPage();
-
-        mActivityTestRule.loadUrl(mTestServer.getURL(TEST_PAGE));
-        assertTrue(queryJavascriptOptimizersEnabledForActiveWebContents());
-    }
-
     /**
-     * Test that Javascript optimizers are enabled by default if no {@link
-     * OsAdditionalSecurityPermissionProvider} is provided.
+     * Test that Javascript optimizers are enabled by default when the operating system does not
+     * request advanced protection.
      */
     @Test
     @MediumTest
-    public void testNoServiceProvider() {
-        mActivityTestRule.startMainActivityOnBlankPage();
-
+    public void testOsDoesNotRequestAdvancedProtection() {
+        sAdvancedProtectionRule.setIsAdvancedProtectionRequestedByOs(false);
         mActivityTestRule.loadUrl(mTestServer.getURL(TEST_PAGE));
         assertTrue(queryJavascriptOptimizersEnabledForActiveWebContents());
     }
 
     /**
-     * Test that Javascript optimizers are enabled by default if a {@link
-     * OsAdditionalSecurityPermissionProvider} is provided and the OS does not request advanced
-     * protection.
-     */
-    @Test
-    @MediumTest
-    public void testServiceProviderDoesNotRequestAdvancedProtection() {
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    ServiceLoaderUtil.setInstanceForTesting(
-                            OsAdditionalSecurityPermissionProvider.class,
-                            new TestPermissionProvider(
-                                    /* isAdvancedProtectionRequestedByOs= */ false));
-                });
-
-        mActivityTestRule.startMainActivityOnBlankPage();
-
-        mActivityTestRule.loadUrl(mTestServer.getURL(TEST_PAGE));
-        assertTrue(queryJavascriptOptimizersEnabledForActiveWebContents());
-    }
-
-    /**
-     * Test that Javascript optimizers are disabled by default if a {@link
-     * OsAdditionalSecurityPermissionProvider} is provided and the OS requests advanced protection.
+     * Test that Javascript optimizers are disabled by default when the operating system requests
+     * advanced protection.
      */
     @Test
     @MediumTest
     public void testServiceProviderRequestsAdvancedProtection() {
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    ServiceLoaderUtil.setInstanceForTesting(
-                            OsAdditionalSecurityPermissionProvider.class,
-                            new TestPermissionProvider(
-                                    /* isAdvancedProtectionRequestedByOs= */ true));
-                });
-
-        mActivityTestRule.startMainActivityOnBlankPage();
-
+        sAdvancedProtectionRule.setIsAdvancedProtectionRequestedByOs(true);
         mActivityTestRule.loadUrl(mTestServer.getURL(TEST_PAGE));
         assertFalse(queryJavascriptOptimizersEnabledForActiveWebContents());
     }
@@ -162,16 +102,7 @@ public class JavascriptOptimizerFeatureTest {
     @MediumTest
     public void testCustomExceptionHasHigherPriorityThanService() {
         GURL pageUrl = new GURL(mTestServer.getURL(TEST_PAGE));
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    ServiceLoaderUtil.setInstanceForTesting(
-                            OsAdditionalSecurityPermissionProvider.class,
-                            new TestPermissionProvider(
-                                    /* isAdvancedProtectionRequestedByOs= */ true));
-                });
-
-        mActivityTestRule.startMainActivityOnBlankPage();
-
+        sAdvancedProtectionRule.setIsAdvancedProtectionRequestedByOs(true);
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     GURL pageOrigin = new GURL(pageUrl.getScheme() + "://" + pageUrl.getHost());
