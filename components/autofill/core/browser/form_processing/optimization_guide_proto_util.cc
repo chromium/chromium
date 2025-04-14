9c8698b2903d8441cc5d713ff3fca0e4a7302ed9
Artem Orlovskii <orlovskiia@yandex-team.ru>
2025-04-14 06:44:46 -0700

    Fix types mixup in autofill::ToFormControlTypeProto()
    kInputEmpail is accidentally converted towards
    FORM_CONTROL_TYPE_INPUT_DATE and vice versa after
    https://crrev.com/c/6330175
    
    Bug: 396325496
    Change-Id: Ibbab2e58479e58e5bedaf293e88a9b105172d010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6447213
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1446495}

--- components/autofill/core/browser/form_processing/optimization_guide_proto_util.cc
+++ components/autofill/core/browser/form_processing/optimization_guide_proto_util.cc
@@ -27,9 +27,9 @@ optimization_guide::proto::FormControlType ToFormControlTypeProto(
     case FormControlType::kInputCheckbox:
       return optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_CHECKBOX;
     case FormControlType::kInputEmail:
-      return optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_DATE;
-    case FormControlType::kInputDate:
       return optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_EMAIL;
+    case FormControlType::kInputDate:
+      return optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_DATE;
     case FormControlType::kInputMonth:
       return optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_MONTH;
     case FormControlType::kInputNumber:
