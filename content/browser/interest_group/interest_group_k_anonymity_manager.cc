a69b36819c3b86e1cbe8bc57a8add978a72cf567
Orr Bernstein <orrb@google.com>
2025-04-15 09:34:49 -0700

    Fix k-anonymity key caching when all keys are fetched from the cache.
    In https://crrev.com/c/6361984, we added caching of keys fetched from
    the k-anonymity server, guarded by the `FledgeCacheKAnonHashedKeys`
    feature. This has not yet been enabled on any channel.
    
    We since discovered that this change leaks `InProgressQueryState` in the
    `InterestGroupKAnonymityManager`'s queries_in_progress_ map whenever all
    of the keys being fetched are available from the cache, and also, the
    interest group's record of k-anonymous keys is not updated to reflect
    that query. This change fixes both of this issues by specifically
    handling the case where all of the keys are retrieved from the cache.
    
    (cherry picked from commit 03969b1952f6bc710978a1a1c3222cb8f0c43a38)
    
    Fixed: 409071576
    Bug: 410798091
    Change-Id: I05966330203069c2463b109b3c37685d5b43aab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6434042
    Commit-Queue: Orr Bernstein <orrb@google.com>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1443646}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6454526
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7103@{#955}
    Cr-Branched-From: e09430c64983fc906f37a9f7e6806275c9b67b86-refs/heads/main@{#1440670}

--- content/browser/interest_group/interest_group_k_anonymity_manager.cc
+++ content/browser/interest_group/interest_group_k_anonymity_manager.cc
@@ -136,8 +136,18 @@ void InterestGroupKAnonymityManager::FetchUncachedKAnonymityData(
     return;
   }
 
+  // If all of the keys we need were fetched from the cache, update k-anonymous
+  // keys on the interest group and clean up the `InProgressQueryState`.
+  if (cache_response.ids_to_query_from_server.empty()) {
+    interest_group_manager_->UpdateKAnonymity(
+        interest_group_key, cache_response.positive_hashed_keys_from_cache,
+        update_time, it->second.replace_existing_values);
+    queries_in_progress_.erase(it);
+    return;
+  }
+
   it->second.positive_hashed_keys_from_received_responses =
-      cache_response.positive_hashed_keys_from_cache;
+      std::move(cache_response.positive_hashed_keys_from_cache);
 
   KAnonymityServiceDelegate* k_anonymity_service =
       k_anonymity_service_callback_.Run();
@@ -148,8 +158,8 @@ void InterestGroupKAnonymityManager::FetchUncachedKAnonymityData(
   }
 
   std::vector<std::string> ids_to_query_in_next_batch;
-  for (const std::string& key : cache_response.ids_to_query_from_server) {
-    ids_to_query_in_next_batch.push_back(key);
+  for (std::string& key : cache_response.ids_to_query_from_server) {
+    ids_to_query_in_next_batch.emplace_back(std::move(key));
 
     if (ids_to_query_in_next_batch.size() >= kQueryBatchSizeLimit) {
       it->second.remaining_responses++;
