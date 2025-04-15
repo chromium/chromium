f359e7e9813c524e82a909ccef4ac667b8ec9f6f
James Godfrey-Kittle <jamesgk@google.com>
2025-04-15 09:08:59 -0700

    [skia] Add flag to guard uniform lifting changes
    Bug: skia:408241870
    Change-Id: Iecd92c68f5b9094deeab1cc03046eff3893319c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6449209
    Commit-Queue: James Godfrey-Kittle <jamesgk@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447160}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -135,6 +135,8 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_AVOID_SLOW_RASTER_PIPELINE_BLURS
 
+#define SK_USE_LEGACY_UNIFORM_LIFTING_GRAPHITE
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
