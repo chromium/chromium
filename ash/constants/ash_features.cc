74d069da5847d04d6d8c6a032e74e5a682ada719
Jimmy <jimmyxgong@chromium.org>
2025-04-16 15:56:04 -0700

    Enable Camera Access Toggle feature and App Launch Shortcut feature
    Features have been approved for M137
    
    Bug: 383772675
    Test: N/A
    Change-Id: Iaafe460b8b0e5f5068511da6719afc0fea2513c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6465475
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448061}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -754,7 +754,7 @@ BASE_FEATURE(kEnableRootNsDnsProxy,
 // Settings > Privacy controls.
 BASE_FEATURE(kEnableToggleCameraShortcut,
              "EnableToggleCameraShortcut",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // TODO:(b/345017297): If enabled, touchscreen mapping experience is visible in
 // settings.
@@ -1197,7 +1197,7 @@ BASE_FEATURE(kGameDashboardUtilities,
 // Enables the App launch keyboard shortcut.
 BASE_FEATURE(kAppLaunchShortcut,
              "AppLaunchShortcut",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables the Game Dashboard's Record Game feature. This flag is to be enabled
 // by the feature management module.
