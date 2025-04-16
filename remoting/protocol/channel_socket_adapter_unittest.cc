d10032c68e1e133915572be21c5630e4a4b5a6ac
Philipp Hancke <philipp.hancke@googlemail.com>
2025-04-16 15:10:09 -0700

    remoting: temporarily disable two unit tests to unblock webrtc migration
    this unblocks the move to an actual mock in
      https://webrtc-review.googlesource.com/c/src/+/386441
    and the tests will be re-enabled after the WebRTC roll.
    
    BUG=None
    
    Change-Id: Ib55445e572d585eef44faf3de73535cf5040fe0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6462701
    Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448034}

--- remoting/protocol/channel_socket_adapter_unittest.cc
+++ remoting/protocol/channel_socket_adapter_unittest.cc
@@ -90,9 +90,11 @@ TEST_F(TransportChannelSocketAdapterTest, ReadClose) {
 }
 
 // Verify that Send sends the packet and returns correct result.
-TEST_F(TransportChannelSocketAdapterTest, Send) {
+// TODO(bugs.webrtc.org/367395350): re-enable after webrtc roll.
+TEST_F(TransportChannelSocketAdapterTest, DISABLED_Send) {
   auto buffer = base::MakeRefCounted<IOBufferWithSize>(kTestDataSize);
 
+  // EXPECT_CALL(channel_, writable()).WillOnce(Return(true));
   EXPECT_CALL(channel_, SendPacket(buffer->data(), kTestDataSize, _, 0))
       .WillOnce(Return(kTestDataSize));
 
@@ -102,9 +104,11 @@ TEST_F(TransportChannelSocketAdapterTest, Send) {
 
 // Verify that the message is still sent if Send() is called while
 // socket is not open yet. The result is the packet is lost.
-TEST_F(TransportChannelSocketAdapterTest, SendPending) {
+// TODO(bugs.webrtc.org/367395350): re-enable after webrtc roll.
+TEST_F(TransportChannelSocketAdapterTest, DISABLED_SendPending) {
   auto buffer = base::MakeRefCounted<IOBufferWithSize>(kTestDataSize);
 
+  // EXPECT_CALL(channel_, writable()).WillOnce(Return(true));
   EXPECT_CALL(channel_, SendPacket(buffer->data(), kTestDataSize, _, 0))
       .Times(1)
       .WillOnce(Return(SOCKET_ERROR));
