bbced431e2aaf8852ae253cde6db99c0a4b17c75
Fiaz Muhammad <mfiaz@google.com>
2025-04-16 08:25:04 -0700

    [Tab Group Parity] Factory method for local favicon resolver
    - New factory method for implementing local favicon resolver for
    	use with OTR profiles
    - Use local version in incognito bottom sheet
    
    Bug: 409767677
    Change-Id: Id4ca5f992f0ac023b42f939fbaf9218a4968d4c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6462359
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1447760}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListBottomSheetCoordinator.java
@@ -116,10 +116,16 @@ public class TabGroupListBottomSheetCoordinator {
                         /* isTabStrip= */ false,
                         R.dimen.default_favicon_corner_radius,
                         TabFavicon::getBitmap);
+
+        boolean isProfileOffTheRecord = profile.isOffTheRecord();
         FaviconResolver faviconResolver =
-                TabGroupListFaviconResolverFactory.build(context, profile, mTabListFaviconProvider);
+                isProfileOffTheRecord
+                        ? TabGroupListFaviconResolverFactory.buildLocal(
+                                context, profile, mTabListFaviconProvider)
+                        : TabGroupListFaviconResolverFactory.build(
+                                context, profile, mTabListFaviconProvider);
         @Nullable TabGroupSyncService tabGroupSyncService =
-                profile.isOffTheRecord() ? null : TabGroupSyncServiceFactory.getForProfile(profile);
+                isProfileOffTheRecord ? null : TabGroupSyncServiceFactory.getForProfile(profile);
 
         CollaborationService collaborationService =
                 CollaborationServiceFactory.getForProfile(profile);
