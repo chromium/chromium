bfc0ca7b06dd7ff75594520ed83353381d7bdce8
Joe Mason <joenotcharles@google.com>
2025-04-16 14:45:03 -0700

    Add a "PageNode" tracing track to PageNodeImpl
    The track is named "PageNode" so that other events can be added without
    changing the name, and nested under a collapsible "PageNodes" track.
    
    Moves "PageNode Loading" trace events to this track and puts them in the
    "loading" category since they're no longer emitted as part of scenario
    detection.
    
    R=etiennep
    
    Change-Id: I8fa301705120428c34794c3cbc133b86d1d6eb28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6446796
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448024}

--- components/performance_manager/graph/page_node_impl.h
+++ components/performance_manager/graph/page_node_impl.h
@@ -25,6 +25,7 @@
 #include "components/performance_manager/public/graph/page_node.h"
 #include "components/performance_manager/resource_attribution/cpu_measurement_data.h"
 #include "components/performance_manager/scenarios/loading_scenario_data.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 #include "url/gurl.h"
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -118,6 +119,10 @@ class PageNodeImpl
   // Returns the unique token for the page node. This function can be called
   // from any thread.
   const PageToken& page_token() const { return page_token_; }
+
+  // Returns a Perfetto track that can record trace events for the page. This
+  // function can be called from any thread.
+  const perfetto::NamedTrack& tracing_track() const { return tracing_track_; }
 
   void SetType(PageType type);
   void SetIsFocused(bool is_focused);
@@ -255,11 +260,24 @@ class PageNodeImpl
   void SetHadUserEdits(bool had_user_edits);
   void SetHasFreezingOriginTrialOptOut(bool has_freezing_origin_trial_opt_out);
 
+  // Emits an instant event recording when the main frame url changed to `url`.
+  // Also includes `navigation_id` if it's not nullopt.
+  void EmitMainFrameUrlChangedEvent(
+      const GURL& url,
+      std::optional<int64_t> navigation_id = std::nullopt) const;
+
+  // Emits the beginning or end of a trace event when the LoadingState changes
+  // to `loading_state`.
+  void EmitLoadingTraceEvent(LoadingState loading_state) const;
+
   // The WebContents associated with this page.
   const base::WeakPtr<content::WebContents> web_contents_;
 
   // The unique token that identifies this PageNode for the life of the browser.
   const PageToken page_token_;
+
+  // Perfetto track that can record trace events for the page.
+  const perfetto::NamedTrack tracing_track_;
 
   // The main frame nodes of this page. There can be more than one main frame
   // in a page, among other reasons because during main frame navigation, the
