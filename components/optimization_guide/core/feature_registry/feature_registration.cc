c6491ef61e865280ec5cb59e1201f93ad47a1b29
Jan Keitel <jkeitel@google.com>
2025-04-16 10:24:27 -0700

    Remove MQLS features for deprecated AutofillAI models
    Those features were never used outside of Teamfood and are scheduled
    for deletion.
    
    Bug: 406776845
    Change-Id: Ic014d78c46d73810cf12b6568d47476a94fd154f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6403813
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447843}

--- components/optimization_guide/core/feature_registry/feature_registration.cc
+++ components/optimization_guide/core/feature_registry/feature_registration.cc
@@ -68,14 +68,6 @@ BASE_FEATURE(kProductSpecificationsMqlsLogging,
              "ProductSpecificationsMqlsLogging",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kFormsPredictionsMqlsLogging,
-             "FormsPredictionsMqlsLogging",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kFormsAnnotationsMqlsLogging,
-             "FormsAnnotationsMqlsLogging",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kFormsClassificationsMqlsLogging,
              "FormsClassificationsMqlsLogging",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -236,40 +228,13 @@ void RegisterProductSpecifications() {
 }
 
 void RegisterAutofillPredictions() {
-  EnterprisePolicyPref enterprise_policy =
-      EnterprisePolicyRegistry::GetInstance().Register(
-          prefs::kAutofillPredictionImprovementsEnterprisePolicyAllowed);
-
-  UserFeedbackCallback fp_logging_callback =
-      base::BindRepeating([](proto::LogAiDataRequest& request_proto) {
-        return request_proto.forms_predictions().quality().user_feedback();
-      });
-  auto fp_mqls_metadata = std::make_unique<MqlsFeatureMetadata>(
-      "FormsPredictions",
-      proto::LogAiDataRequest::FeatureCase::kFormsPredictions,
-      enterprise_policy, &features::kFormsPredictionsMqlsLogging,
-      fp_logging_callback);
-  MqlsFeatureRegistry::GetInstance().Register(std::move(fp_mqls_metadata));
-
-  // Forms annotations. In the same block as forms predictions since it
-  // leverages the same enterprise policy.
-  UserFeedbackCallback fa_logging_callback =
-      base::BindRepeating([](proto::LogAiDataRequest& request_proto) {
-        return request_proto.forms_annotations().quality().user_feedback();
-      });
-  auto fa_mqls_metadata = std::make_unique<MqlsFeatureMetadata>(
-      "FormsAnnotations",
-      proto::LogAiDataRequest::FeatureCase::kFormsAnnotations,
-      enterprise_policy, &features::kFormsAnnotationsMqlsLogging,
-      fa_logging_callback);
-  MqlsFeatureRegistry::GetInstance().Register(std::move(fa_mqls_metadata));
-
   MqlsFeatureRegistry::GetInstance().Register(
       std::make_unique<MqlsFeatureMetadata>(
           "FormsClassifications",
           proto::LogAiDataRequest::FeatureCase::kFormsClassifications,
-          enterprise_policy, &features::kFormsClassificationsMqlsLogging,
-          FeedbackUnspecified()));
+          EnterprisePolicyRegistry::GetInstance().Register(
+              prefs::kAutofillPredictionImprovementsEnterprisePolicyAllowed),
+          &features::kFormsClassificationsMqlsLogging, FeedbackUnspecified()));
 }
 
 }  // anonymous namespace
