47e29afbda967d4b15ca1102bb7d169dcd769561
Kalvin Lee <kdlee@chromium.org>
2025-04-16 03:11:57 -0700

    spanify: Refactor `reorder-pragma-and-includes.py`
    This CL modifies the script to also handle the `#include`s emitted by
    spanification.
    
    Change-Id: I5e60c0d2dc6c5c25b3ca525c8c6210013a6441a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6450382
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1447613}

--- tools/clang/spanify/reorder-pragma-and-includes.py
+++ tools/clang/spanify/reorder-pragma-and-includes.py
@@ -9,57 +9,125 @@
 # done to allow clang format to better format the #includes as it can't reorder
 # through the macros.
 
+import sys
 import os
 
-modified_files = os.popen("git diff --name-only HEAD~..HEAD").read().split(
-    "\n")
-modified_files = list(filter(None, modified_files))
+INCLUDE_ARRAY = "#include <array>"
+INCLUDE_SPAN = '#include "base/containers/span.h"'
+INCLUDE_RAW_SPAN = '#include "base/memory/raw_span.h"'
 
-# Invert the #include <array> and the #pragma allow_unsafe_buffers. This allow
-# clang format to better format the #includes as it can't reorder through the
-# macros.
-for file in modified_files:
-    lines = []
-    try:
-        with open(file, 'r') as f:
-            lines = f.readlines()
-    except:
-        continue  # Skip files that were deleted.
 
-    # Do we have #include <array> above #pragma allow_unsafe_buffers?
-    line_array = -1
-    line_pragma = -1
-    for i, line in enumerate(lines):
-        if "#include <array>" in line:
-            line_array = i
-        if "#pragma allow_unsafe_buffers" in line:
-            line_pragma = i
+class ReorderTarget:
 
-    if line_array == -1 or line_pragma == -1 or line_array > line_pragma:
-        continue  # Nothing to reorder.
+    def __find_line_numbers(self):
+        # Do we have any `#include`s above `#pragma allow_unsafe_buffers`?
+        for i, unstripped_line in enumerate(self.lines):
+            line = unstripped_line.strip()
+            if line == INCLUDE_ARRAY:
+                self.array_include_line = i
+            elif line == INCLUDE_SPAN:
+                self.span_include_line = i
+            elif line == INCLUDE_RAW_SPAN:
+                self.raw_span_include_line = i
+            elif "#pragma allow_unsafe_buffers" in line:
+                self.pragma_line = i
 
-    # Reorder:
-    with open(file, 'w') as f:
+    def __init__(self, path):
+        self.lines = None
+        self.array_include_line = None
+        self.span_include_line = None
+        self.raw_span_include_line = None
+        self.pragma_line = None
+        try:
+            with open(path, 'r') as f:
+                self.lines = f.readlines()
+        except FileNotFoundError:
+            return  # Skip files that were deleted.
+        self.__find_line_numbers()
+
+    def _should_reorder_impl(self, member):
+        try:
+            return member < self.pragma_line
+        except TypeError:
+            # One or both are `None`.
+            return False
+
+    def should_reorder_array_include(self):
+        return self._should_reorder_impl(self.array_include_line)
+
+    def should_reorder_span_include(self):
+        return self._should_reorder_impl(self.span_include_line)
+
+    def should_reorder_raw_span_include(self):
+        return self._should_reorder_impl(self.raw_span_include_line)
+
+    def should_reorder(self):
+        # Deleted file.
+        if self.lines is None:
+            return False
+        # No pragma? Then `git cl format` shouldn't be confused.
+        if self.pragma_line is None:
+            return False
+        return (self.should_reorder_array_include()
+                or self.should_reorder_span_include()
+                or self.should_reorder_raw_span_include())
+
+
+def reorder_pragma_and_includes(path):
+    target = ReorderTarget(path)
+    if not target.should_reorder():
+        return
+
+    # Entering this block means there _is_ something to reorder.
+    # 1.  The `#pragma` line exists. We _will_ pass through it as
+    #     we traverse the file.
+    # 2.  Either `span.h` or `<array>` is included - possibly both.
+    with open(path, 'w') as f:
         in_opt_out = False
-        for line in lines:
-            # Write out all lines except for "#include <array>" until we see the
-            # UNSAFE_BUFFERS_BUILD macro. Set |in_opt_out| at this point when
-            # this occurs.
-            if not in_opt_out:
-                if not "#include <array>" in line:
+        for (line_number, line) in enumerate(target.lines):
+            # Write out all lines except for the overly-high-up `#include`s
+            # until we step inside the `UNSAFE_BUFFERS_BUILD` macro. Set
+            # `in_opt_out` at the point when this occurs.
+            if not in_opt_out and line_number < target.pragma_line:
+                if line.strip() not in (INCLUDE_ARRAY, INCLUDE_SPAN,
+                                        INCLUDE_RAW_SPAN):
                     f.write(line)
                 if "#ifdef UNSAFE_BUFFERS_BUILD" in line:
                     in_opt_out = True
                 continue
 
             # Write out each line until we hit the end of the
-            # UNSAFE_BUFFERS_BUILD and afterwards write out the
-            # "#include <array>".
+            # `UNSAFE_BUFFERS_BUILD` and afterwards write out the
+            # necessary `#include`s.
             if in_opt_out:
                 f.write(line)
                 if "#endif" in line:
-                    f.write("\n#include <array>\n")
+                    if target.should_reorder_array_include():
+                        f.write(f"\n{INCLUDE_ARRAY}\n")
+                    if target.should_reorder_span_include():
+                        f.write(f"\n{INCLUDE_SPAN}\n")
+                    if target.should_reorder_raw_span_include():
+                        f.write(f"\n{INCLUDE_RAW_SPAN}\n")
                     in_opt_out = False
                 continue
 
-os.system("git cl format")
+            # We have passed the `#pragma` and can mindlessly spit out
+            # every subsequent line.
+            assert not in_opt_out and line_number > target.pragma_line
+            f.write(line)
+
+
+def main():
+    modified_files = [
+        f for f in os.popen("git diff --name-only HEAD~..HEAD").read().split(
+            "\n") if f
+    ]
+
+    for file in modified_files:
+        reorder_pragma_and_includes(file)
+
+    os.system("git cl format")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
