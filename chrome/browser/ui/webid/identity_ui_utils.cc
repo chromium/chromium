392f75d8ff11d5b458f64c464ed4fca73bb92b56
Yi Gu <yigu@chromium.org>
2025-04-17 09:15:18 -0700

    [FedCM] Refactor identity_ui_utils to make it accessible by autofill
    This patch moves some classes from cc to header. It should be no-op.
    
    Bug: 410533051
    Change-Id: I64bc8593958c4ccc2dbd7fdc7f05e9ffbc0a467e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6467125
    Reviewed-by: Zachary Tan <tanzachary@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1448353}

--- chrome/browser/ui/webid/identity_ui_utils.cc
+++ chrome/browser/ui/webid/identity_ui_utils.cc
@@ -7,14 +7,11 @@
 #include "base/i18n/break_iterator.h"
 #include "base/i18n/case_conversion.h"
 #include "base/strings/utf_string_conversions.h"
-#include "build/build_config.h"
-#include "build/buildflag.h"
 #include "chrome/browser/ui/monogram_utils.h"
 #include "chrome/grit/platform_locale_settings.h"
 #include "content/public/browser/identity_request_account.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/gfx/canvas.h"
-#include "ui/gfx/image/canvas_image_source.h"
 #include "ui/gfx/image/image_skia_operations.h"
 
 namespace {
@@ -34,87 +31,6 @@ inline constexpr int kLargeAvatarBadgeSize = 16;
 // account button.
 constexpr int kIdpBorderRadius = 10;
 #endif  // BUILDFLAG(IS_ANDROID)
-
-// A CanvasImageSource that draws a letter in a circle.
-class LetterCircleCroppedImageSkiaSource : public gfx::CanvasImageSource {
- public:
-  LetterCircleCroppedImageSkiaSource(const std::u16string& letter, int size)
-      : gfx::CanvasImageSource(gfx::Size(size, size)), letter_(letter) {}
-
-  LetterCircleCroppedImageSkiaSource(
-      const LetterCircleCroppedImageSkiaSource&) = delete;
-  LetterCircleCroppedImageSkiaSource& operator=(
-      const LetterCircleCroppedImageSkiaSource&) = delete;
-  ~LetterCircleCroppedImageSkiaSource() override = default;
-
-  void Draw(gfx::Canvas* canvas) override {
-    monogram::DrawMonogramInCanvas(canvas, size().width(), size().width(),
-                                   letter_, SK_ColorWHITE, SK_ColorGRAY);
-  }
-
- private:
-  const std::u16string letter_;
-};
-
-// A CanvasImageSource that:
-// 1) Applies an optional square center-crop.
-// 2) Resizes the cropped image (while maintaining the image's aspect ratio) to
-//    fit into the target canvas. If no center-crop was applied and the source
-//    image is rectangular, the image is resized so that
-//    `avatar` small edge size == `canvas_edge_size`.
-// 3) Circle center-crops the resized image.
-class CircleCroppedImageSkiaSource : public gfx::CanvasImageSource {
- public:
-  CircleCroppedImageSkiaSource(gfx::ImageSkia avatar,
-                               std::optional<int> pre_resize_avatar_crop_size,
-                               int canvas_edge_size)
-      : gfx::CanvasImageSource(gfx::Size(canvas_edge_size, canvas_edge_size)) {
-    int scaled_width = canvas_edge_size;
-    int scaled_height = canvas_edge_size;
-    if (pre_resize_avatar_crop_size) {
-      const float avatar_scale =
-          (canvas_edge_size / (float)*pre_resize_avatar_crop_size);
-      scaled_width = floor(avatar.width() * avatar_scale);
-      scaled_height = floor(avatar.height() * avatar_scale);
-    } else {
-      // Resize `avatar` so that it completely fills the canvas.
-      const float height_ratio =
-          ((float)avatar.height() / (float)avatar.width());
-      if (height_ratio >= 1.0f) {
-        scaled_height = floor(canvas_edge_size * height_ratio);
-      } else {
-        scaled_width = floor(canvas_edge_size / height_ratio);
-      }
-    }
-    avatar_ = gfx::ImageSkiaOperations::CreateResizedImage(
-        avatar, skia::ImageOperations::RESIZE_BEST,
-        gfx::Size(scaled_width, scaled_height));
-  }
-
-  CircleCroppedImageSkiaSource(const CircleCroppedImageSkiaSource&) = delete;
-  CircleCroppedImageSkiaSource& operator=(const CircleCroppedImageSkiaSource&) =
-      delete;
-  ~CircleCroppedImageSkiaSource() override = default;
-
-  // CanvasImageSource:
-  void Draw(gfx::Canvas* canvas) override {
-    const int canvas_edge_size = size().width();
-
-    // Center the avatar in the canvas.
-    const int x = (canvas_edge_size - avatar_.width()) / 2;
-    const int y = (canvas_edge_size - avatar_.height()) / 2;
-
-    SkPath circular_mask;
-    circular_mask.addCircle(SkIntToScalar(canvas_edge_size / 2),
-                            SkIntToScalar(canvas_edge_size / 2),
-                            SkIntToScalar(canvas_edge_size / 2));
-    canvas->ClipPath(circular_mask, true);
-    canvas->DrawImageInt(avatar_, x, y);
-  }
-
- private:
-  gfx::ImageSkia avatar_;
-};
 
 // Returns an image consisting of `base_image` with `badge_image` being badged
 // towards its bottom right corner. `badge_offset` is used to determine how much
@@ -170,6 +86,61 @@ gfx::ImageSkia CreateBadgedImageSkia(const gfx::ImageSkia& base_image,
 }
 
 }  // namespace
+
+LetterCircleCroppedImageSkiaSource::LetterCircleCroppedImageSkiaSource(
+    const std::u16string& letter,
+    int size)
+    : gfx::CanvasImageSource(gfx::Size(size, size)), letter_(letter) {}
+
+void LetterCircleCroppedImageSkiaSource::Draw(gfx::Canvas* canvas) {
+  monogram::DrawMonogramInCanvas(canvas, size().width(), size().width(),
+                                 letter_, SK_ColorWHITE, SK_ColorGRAY);
+}
+
+CircleCroppedImageSkiaSource::CircleCroppedImageSkiaSource(
+    gfx::ImageSkia avatar,
+    const std::optional<int>& pre_resize_avatar_crop_size,
+    int canvas_edge_size)
+    : gfx::CanvasImageSource(gfx::Size(canvas_edge_size, canvas_edge_size)) {
+  // The avatar may need to be scaled to match the canvas. To make sure that the
+  // avatar's aspect ratio is maintained, we'd need to adjust the scaled
+  // dimensions accordingly.
+  int scaled_width = canvas_edge_size;
+  int scaled_height = canvas_edge_size;
+  if (pre_resize_avatar_crop_size) {
+    const float avatar_scale =
+        canvas_edge_size / static_cast<float>(*pre_resize_avatar_crop_size);
+    scaled_width = floor(avatar.width() * avatar_scale);
+    scaled_height = floor(avatar.height() * avatar_scale);
+  } else {
+    // Resize `avatar` so that it completely fills the canvas.
+    const float height_ratio = static_cast<float>(avatar.height()) /
+                               static_cast<float>(avatar.width());
+    if (height_ratio >= 1.0f) {
+      scaled_height = floor(canvas_edge_size * height_ratio);
+    } else {
+      scaled_width = floor(canvas_edge_size / height_ratio);
+    }
+  }
+  avatar_ = gfx::ImageSkiaOperations::CreateResizedImage(
+      avatar, skia::ImageOperations::RESIZE_BEST,
+      gfx::Size(scaled_width, scaled_height));
+}
+
+void CircleCroppedImageSkiaSource::Draw(gfx::Canvas* canvas) {
+  const int canvas_edge_size = size().width();
+
+  // Center the avatar in the canvas.
+  const int x = (canvas_edge_size - avatar_.width()) / 2;
+  const int y = (canvas_edge_size - avatar_.height()) / 2;
+
+  SkPath circular_mask;
+  circular_mask.addCircle(SkIntToScalar(canvas_edge_size / 2),
+                          SkIntToScalar(canvas_edge_size / 2),
+                          SkIntToScalar(canvas_edge_size / 2));
+  canvas->ClipPath(circular_mask, true);
+  canvas->DrawImageInt(avatar_, x, y);
+}
 
 std::u16string GetInitialLetterAsUppercase(const std::string& utf8_string) {
   std::u16string utf16_string(base::UTF8ToUTF16(utf8_string));
