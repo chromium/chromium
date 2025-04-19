24af02483ece6ed0c16df50ee7737a9ca493ea55
Benjamin Keen <bkeen@google.com>
2025-04-18 18:16:30 -0700

    Reland "[CHAP] Add BrowserInitiatedAutomaticPictureInPicture to chrome://flags"
    This is a reland of commit 51e04dc75a78e8712fcdb2a64730eed442399ad8
    
    This reland does not modify any files, since the original issue was
    fixed on
    https://chromium-review.googlesource.com/c/chromium/src/+/6465615.
    
    Original change's description:
    > [CHAP] Add BrowserInitiatedAutomaticPictureInPicture to chrome://flags
    >
    > Bug: 408502322, b/318568892
    > Change-Id: I38ffd826498584c195f6a1118cd65513efc985de
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6437622
    > Commit-Queue: Benjamin Keen <bkeen@google.com>
    > Reviewed-by: Frank Liberato <liberato@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1447469}
    
    Bug: 408502322, b/318568892
    Change-Id: I58a4730895de1ae4e962179fe595edd542fe04ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6470322
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1449090}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3062,6 +3062,9 @@ extern const char kAllowAllSitesToInitiateMirroringDescription[];
 extern const char kAXTreeFixingName[];
 extern const char kAXTreeFixingDescription[];
 
+extern const char kBrowserInitiatedAutomaticPictureInPictureName[];
+extern const char kBrowserInitiatedAutomaticPictureInPictureDescription[];
+
 extern const char kDialMediaRouteProviderName[];
 extern const char kDialMediaRouteProviderDescription[];
 extern const char kDelayMediaSinkDiscoveryName[];
