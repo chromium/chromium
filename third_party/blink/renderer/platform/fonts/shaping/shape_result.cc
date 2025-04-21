95149be199164a5eaec377ec964575e5dd247f17
Koji Ishii <kojii@chromium.org>
2025-04-21 11:55:03 -0700

    [perf] Reduce `ShapeResultRun` by 4 bytes
    By storing [`hb_direction_t`] as `uint8_t`.
    
    This patch has no behavior changes.
    
    [`hb_direcion_t`]: https://source.chromium.org/chromium/chromium/src/+/main:third_party/harfbuzz-ng/src/src/hb-common.h
    
    Bug: 412125816, 406127861
    Change-Id: Id20b3ced99f771fd4b37a03199ea4de8ccf14bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476884
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449541}

--- third_party/blink/renderer/platform/fonts/shaping/shape_result.cc
+++ third_party/blink/renderer/platform/fonts/shaping/shape_result.cc
@@ -78,7 +78,7 @@ struct SameSizeAsRunInfo {
     unsigned integer;
   } glyph_data;
   Member<void*> pointer2[2];
-  int integers[6];
+  int integers[5];
 };
 
 ASSERT_SIZE(ShapeResultRun, SameSizeAsRunInfo);
@@ -755,7 +755,7 @@ float ShapeResult::ForEachGlyphImpl(float initial_advance,
                                     const ShapeResultRun& run) const {
   auto glyph_offsets = run.glyph_data_.GetOffsets<has_non_zero_glyph_offsets>();
   auto total_advance = InlineLayoutUnit::FromFloatRound(initial_advance);
-  bool is_horizontal = HB_DIRECTION_IS_HORIZONTAL(run.direction_);
+  bool is_horizontal = run.IsHorizontal();
   for (const auto& glyph_data : run.glyph_data_) {
     glyph_callback(context, run.start_index_ + glyph_data.character_index,
                    glyph_data.glyph, *glyph_offsets, total_advance,
@@ -793,7 +793,7 @@ float ShapeResult::ForEachGlyphImpl(float initial_advance,
   auto glyph_offsets = run.glyph_data_.GetOffsets<has_non_zero_glyph_offsets>();
   auto total_advance = InlineLayoutUnit::FromFloatRound(initial_advance);
   unsigned run_start = run.start_index_ + index_offset;
-  bool is_horizontal = HB_DIRECTION_IS_HORIZONTAL(run.direction_);
+  bool is_horizontal = run.IsLtr();
   const SimpleFontData* font_data = run.font_data_.Get();
 
   if (run.IsLtr()) {  // Left-to-right
@@ -1580,9 +1580,8 @@ void ShapeResult::InsertRun(ShapeResultRun* run) {
     return run->start_index_ > start_index;
   };
 
-  auto it = std::lower_bound(
-      runs_.begin(), runs_.end(), run->start_index_,
-      HB_DIRECTION_IS_FORWARD(run->direction_) ? ltr_comparer : rtl_comparer);
+  auto it = std::lower_bound(runs_.begin(), runs_.end(), run->start_index_,
+                             run->IsLtr() ? ltr_comparer : rtl_comparer);
   if (it != runs_.end()) {
     runs_.insert(static_cast<wtf_size_t>(it - runs_.begin()), run);
   } else {
@@ -2022,7 +2021,7 @@ void ShapeResult::ToString(StringBuilder* output) const {
     output->Append(", #chars=");
     output->AppendNumber(run.num_characters_);
     output->Append(", dir=");
-    output->AppendNumber(static_cast<uint32_t>(run.direction_));
+    output->AppendNumber(run.hb_direction_);
     output->Append(", glyphs[");
     output->AppendNumber(run.glyph_data_.size());
     output->Append("]{");
