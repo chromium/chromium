c32d4d97a26122fc5338431133564120b07ca1a9
Tommy Nyquist <nyquist@chromium.org>
2025-04-21 11:54:18 -0700

    Disable a flaky test
    The test testCreateIncognitoGroupAndCloseAllTabsInDialogTwice_Bug354745444 is sometimes failing and needs to be disabled.
    
    Bug: 412364951
    Change-Id: I4792d1c57e302fd666a3dfd324985cc17c06b947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6476546
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449540}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -1977,6 +1977,7 @@ public class TabGridDialogTest {
     @Test
     @MediumTest
     @EnableAnimations
+    @DisabledTest(message = "crbug.com/412364951")
     public void testCreateIncognitoGroupAndCloseAllTabsInDialogTwice_Bug354745444() {
         ChromeTabbedActivity cta = sActivityTestRule.getActivity();
         boolean incognito = true;
