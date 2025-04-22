24df3d6fae02d9dacd7102595b6ed7078a5043f6
Qijiang Fan <fqj@chromium.org>
2025-04-21 19:15:11 -0700

    [gardening] disable multiple assistant tests for ChromeOS
    Bug: b/412475160
    Change-Id: I0b9a31e0886ec9a9d59ebb91b796dcc2f211bd1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6475106
    Auto-Submit: Qijiang Fan <fqj@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Owners-Override: Qijiang Fan <fqj@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Qijiang Fan <fqj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1449716}

--- chrome/browser/ash/assistant/assistant_integration_test.cc
+++ chrome/browser/ash/assistant/assistant_integration_test.cc
@@ -74,7 +74,8 @@ class AssistantIntegrationTest : public AshIntegrationTest,
 
 INSTANTIATE_TEST_SUITE_P(All, AssistantIntegrationTest, testing::Bool());
 
-IN_PROC_BROWSER_TEST_P(AssistantIntegrationTest, Hotkey) {
+// Disabled due to b/412475160
+IN_PROC_BROWSER_TEST_P(AssistantIntegrationTest, DISABLED_Hotkey) {
   SetupContextWidget();
 
   // Login and wait for the user session to start.
