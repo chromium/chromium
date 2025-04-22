230f32d075eb0ef7a26d0639fc426b5c4f284331
Erik Chen <erikchen@chromium.org>
2025-04-22 15:54:14 -0700

    glic: Add another login special case.
    Bug: 407835686
    Change-Id: I3563ee9e17fd07ce485a7eaed19e361138d1124c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6478697
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450273}

--- chrome/browser/resources/glic/webview.ts
+++ chrome/browser/resources/glic/webview.ts
@@ -229,6 +229,7 @@ export class WebviewController {
     // TODO(https://crbug.com/388328847): Remove when login issues are resolved.
     if (url.startsWith('https://login.corp.google.com/') ||
         url.startsWith('https://accounts.google.com/') ||
+        url.startsWith('https://accounts.googlers.com/') ||
         url.startsWith('https://gaiastaging.corp.google.com/')) {
       this.delegate.webviewPageCommit('login');
     } else if (new URL(url).pathname.startsWith('/sorry/')) {
