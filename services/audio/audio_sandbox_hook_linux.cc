7cfb6bd84813639a53374a255e98452dd20094ae
Patrick Monette <pmonette@chromium.org>
2025-04-23 09:11:41 -0700

    String constants migration
    As a first step for changing base::Environment to take in a
    base::cstring_view as parameter, all constants that are eventually
    used in this API needs to be fully defined at the call site.
    
    This CL is part of relanding
    https://chromium-review.googlesource.com/c/chromium/src/+/6470383.
    It has been split into 2 changes to reduce the unnecessary churn if
    changing the base::Environment API is reverted again.
    
    This CL was uploaded by git cl split.
    
    R=olka@chromium.org
    
    Bug: 412679386
    Change-Id: I32bbfc2fc329669b454dc568669a9dd0d3a27ff4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6482848
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450594}

--- services/audio/audio_sandbox_hook_linux.cc
+++ services/audio/audio_sandbox_hook_linux.cc
@@ -9,7 +9,6 @@
 
 #include <optional>
 #include <string>
-#include <string_view>
 #include <vector>
 
 #include "base/environment.h"
@@ -17,6 +16,7 @@
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "base/strings/cstring_view.h"
 #include "base/strings/stringprintf.h"
 #include "sandbox/linux/syscall_broker/broker_command.h"
 #include "sandbox/linux/syscall_broker/broker_file_permission.h"
@@ -59,7 +59,7 @@ void AddAlsaFilePermissions(std::vector<BrokerFilePermission>* permissions) {
 // are specified through environment variables. |recursive_only| is used to
 // determine if the path itself should be allowed access or only its content.
 void AllowAccessToEnvSpecifiedPath(
-    std::string_view variable_name,
+    base::cstring_view variable_name,
     std::vector<BrokerFilePermission>* permissions,
     bool recursive_only) {
   std::unique_ptr<base::Environment> env(base::Environment::Create());
@@ -119,16 +119,17 @@ void AddPulseAudioFilePermissions(
   // "/tmp/pulse-<random string>".
   permissions->push_back(
       BrokerFilePermission::ReadWriteCreateRecursive("/tmp/"));
-  const char* env_tmp_paths[] = {"TMPDIR", "TMP", "TEMP", "TEMPDIR"};
-  for (const char* env_tmp_path : env_tmp_paths) {
+  static constexpr base::cstring_view env_tmp_paths[] = {"TMPDIR", "TMP",
+                                                         "TEMP", "TEMPDIR"};
+  for (base::cstring_view env_tmp_path : env_tmp_paths) {
     AllowAccessToEnvSpecifiedPath(env_tmp_path, permissions,
                                   /*recursive_only=*/true);
   }
   // Read up the Pulse paths specified via environment variable and allow for
   // read/write/create recursively on the directory.
-  const char* env_pulse_paths[] = {"PULSE_CONFIG_PATH", "PULSE_RUNTIME_PATH",
-                                   "PULSE_STATE_PATH"};
-  for (const char* env_pulse_path : env_pulse_paths) {
+  static constexpr base::cstring_view env_pulse_paths[] = {
+      "PULSE_CONFIG_PATH", "PULSE_RUNTIME_PATH", "PULSE_STATE_PATH"};
+  for (base::cstring_view env_pulse_path : env_pulse_paths) {
     AllowAccessToEnvSpecifiedPath(env_pulse_path, permissions,
                                   /*recursive_only=*/false);
   }
