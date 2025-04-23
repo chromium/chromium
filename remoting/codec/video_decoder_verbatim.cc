04b42b466eabcd822ecf985388ef26429057bc14
Ho Cheung <hocheung@chromium.org>
2025-04-22 19:48:58 -0700

    [remoting] Improve memory safety in VideoDecoderVerbatim using base::span
    Replace unsafe pointer arithmetic in VideoDecoderVerbatim with safer
    base::span operations
    
    Bug: 40285824
    Change-Id: Ic746f6b17ed9c3174ca287238a5807b8ddf8a568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6475746
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450327}

--- remoting/codec/video_decoder_verbatim.cc
+++ remoting/codec/video_decoder_verbatim.cc
@@ -2,15 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "remoting/codec/video_decoder_verbatim.h"
 
-#include <stdint.h>
+#include <cstddef>
+#include <cstdint>
 
+#include "base/containers/span.h"
 #include "base/logging.h"
 #include "base/notreached.h"
 #include "remoting/base/util.h"
@@ -35,7 +32,8 @@ bool VideoDecoderVerbatim::DecodePacket(const VideoPacket& packet,
                                         webrtc::DesktopFrame* frame) {
   webrtc::DesktopRegion* region = frame->mutable_updated_region();
   region->Clear();
-  const char* current_data_pos = packet.data().data();
+  base::span<const uint8_t> packet_data = base::as_byte_span(packet.data());
+  base::span<const uint8_t> remaining_data = packet_data;
   for (int i = 0; i < packet.dirty_rects_size(); ++i) {
     Rect proto_rect = packet.dirty_rects(i);
     webrtc::DesktopRect rect =
@@ -48,21 +46,18 @@ bool VideoDecoderVerbatim::DecodePacket(const VideoPacket& packet,
       return false;
     }
 
-    int rect_row_size = kBytesPerPixel * rect.width();
-    const char* rect_data_end =
-        current_data_pos + rect_row_size * rect.height();
-    if (rect_data_end > packet.data().data() + packet.data().size()) {
+    const size_t rect_data_size = kBytesPerPixel * rect.width() * rect.height();
+    if (remaining_data.size() < rect_data_size) {
       LOG(ERROR) << "Invalid packet received.";
       return false;
     }
 
-    uint8_t* source =
-        reinterpret_cast<uint8_t*>(const_cast<char*>(current_data_pos));
-    frame->CopyPixelsFrom(source, rect_row_size, rect);
-    current_data_pos = rect_data_end;
+    frame->CopyPixelsFrom(remaining_data.data(), kBytesPerPixel * rect.width(),
+                          rect);
+    remaining_data = remaining_data.subspan(rect_data_size);
   }
 
-  if (current_data_pos != packet.data().data() + packet.data().size()) {
+  if (!remaining_data.empty()) {
     LOG(ERROR) << "Invalid packet received.";
     return false;
   }
