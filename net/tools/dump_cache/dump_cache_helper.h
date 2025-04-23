4daac50e4285b97a780230492b4c51d6da8d2fb9
Matt Menke <mmenke@chromium.org>
2025-04-23 16:35:27 -0700

    Rename and move net::ViewCacheHelper.
    Move it to net/tools/dump_cache/, and rename it to DumpCacheHelper,
    moving it out of the net namespace (like the other files in that
    directory), as it's used by the DumpCache tool.
    
    I believe this used to be used by about:cache, which no longer exists.
    
    Unclear if it's useful to keep DumpCache around, as I suspect no one
    is currently using it, but might be nice to have if anyone starts
    investing in cache work again.
    
    Bug: None
    Change-Id: I7081ea9a460e47c558cb3509c4b7544a9a5ae9cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6483132
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450866}

--- net/tools/dump_cache/dump_cache_helper.h
+++ net/tools/dump_cache/dump_cache_helper.h
@@ -2,25 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_URL_REQUEST_VIEW_CACHE_HELPER_H_
-#define NET_URL_REQUEST_VIEW_CACHE_HELPER_H_
+#ifndef NET_TOOLS_DUMP_CACHE_DUMP_CACHE_HELPER_H_
+#define NET_TOOLS_DUMP_CACHE_DUMP_CACHE_HELPER_H_
 
 #include <stddef.h>
 
 #include <string>
 
-#include "net/base/net_export.h"
-
-namespace net {
-
-class NET_EXPORT ViewCacheHelper {
+class DumpCacheHelper {
  public:
   // Lower-level helper to produce a textual representation of binary data.
   // The results are appended to |result| and can be used in HTML pages
   // provided the dump is contained within <pre></pre> tags.
-  static void HexDump(const char *buf, size_t buf_len, std::string* result);
+  static void HexDump(const char* buf, size_t buf_len, std::string* result);
 };
 
-}  // namespace net.
-
-#endif  // NET_URL_REQUEST_VIEW_CACHE_HELPER_H_
+#endif  // NET_TOOLS_DUMP_CACHE_DUMP_CACHE_HELPER_H_
--- net/tools/dump_cache/dump_cache_helper_unittest.cc
+++ net/tools/dump_cache/dump_cache_helper_unittest.cc
@@ -7,23 +7,19 @@
 #pragma allow_unsafe_libc_calls
 #endif
 
-#include "net/url_request/view_cache_helper.h"
+#include "net/tools/dump_cache/dump_cache_helper.h"
 
 #include <cstring>
 
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace net {
-
-TEST(ViewCacheHelper, HexDump) {
+TEST(DumpCacheHelper, HexDump) {
   std::string out = "Prefix\n";
   const char kIn[] = "0123456789ABCDEFGHIJ\x01\x80<&>";
-  ViewCacheHelper::HexDump(kIn, strlen(kIn), &out);
+  DumpCacheHelper::HexDump(kIn, strlen(kIn), &out);
   EXPECT_EQ(
       "Prefix\n00000000: 30 31 32 33 34 35 36 37 38 39 41 42 43 44 45 46  "
       "0123456789ABCDEF\n00000010: 47 48 49 4a 01 80 3c 26 3e                  "
       "     GHIJ..&lt;&amp;&gt;\n",
       out);
 }
-
-}  // namespace net
