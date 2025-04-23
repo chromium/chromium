8d79a500a591d2e3363bdb2ead98917b8a5f104f
Claudio DeSouza <cdesouza@chromium.org>
2025-04-23 16:02:03 -0700

    [CodeHealth] Use `DownloadToString` with `optional`
    This CL change the use of `DownloadToString` in the discreet `Job`
    class, to make use of the newer `std::optional<std::string>` overload,
    rather than the `unique_ptr` deprecated one.
    
    Bug: 410044687
    Change-Id: I5991b979593b78286d3cba53ab2abaf94bc0b936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6473402
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450849}

--- services/cert_verifier/cert_net_url_loader/cert_net_fetcher_url_loader.cc
+++ services/cert_verifier/cert_net_url_loader/cert_net_fetcher_url_loader.cc
@@ -253,14 +253,14 @@ class CertNetFetcherURLLoader::RequestCore
 
   void OnJobCompleted(Job* job,
                       net::Error error,
-                      const std::string* response_body) {
+                      base::span<const uint8_t> response_body) {
     DCHECK(task_runner_->RunsTasksInCurrentSequence());
 
     DCHECK_EQ(job_, job);
     job_ = nullptr;
 
     error_ = error;
-    bytes_ = base::ToVector(base::as_byte_span(*response_body));
+    bytes_ = base::ToVector(response_body);
     completion_event_.Signal();
   }
 
@@ -385,18 +385,18 @@ class Job {
                          const network::mojom::URLResponseHead& response_head);
 
   // Callback for when |url_loader_| has finished.
-  void OnUrlLoaderCompleted(std::unique_ptr<std::string> response_body);
+  void OnUrlLoaderCompleted(std::optional<std::string> response_body);
 
   // Called when the Job has completed. The job may finish in response to a
   // timeout, an invalid URL, or the SimpleURLLoader completing.
   // This will delete the Job after calling |CompleteAndClearRequests()|.
   void OnJobCompleted(net::Error error,
-                      std::unique_ptr<std::string> response_body);
+                      std::optional<std::string> response_body);
 
   // Calls r->OnJobCompleted() for each RequestCore |r| currently attached
   // to this job, and then clears |requests_|.
   void CompleteAndClearRequests(net::Error error,
-                                std::unique_ptr<std::string> response_body);
+                                std::optional<std::string> response_body);
 
   // Cancels a request with a specified error code and calls
   // OnUrlRequestCompleted().
@@ -549,7 +549,7 @@ void Job::Cancel() {
   // Reset the SimpleURLLoader.
   url_loader_.reset();
   // Signal attached requests that they've been completed.
-  CompleteAndClearRequests(net::ERR_ABORTED, nullptr);
+  CompleteAndClearRequests(net::ERR_ABORTED, std::nullopt);
 }
 
 void Job::OnReceivedRedirect(
@@ -572,13 +572,13 @@ void Job::OnResponseStarted(
   }
 }
 
-void Job::OnUrlLoaderCompleted(std::unique_ptr<std::string> response_body) {
+void Job::OnUrlLoaderCompleted(std::optional<std::string> response_body) {
   net::Error error = static_cast<net::Error>(url_loader_->NetError());
   OnJobCompleted(error, std::move(response_body));
 }
 
 void Job::OnJobCompleted(net::Error error,
-                         std::unique_ptr<std::string> response_body) {
+                         std::optional<std::string> response_body) {
   // Reset the SimpleURLLoader.
   url_loader_.reset();
 
@@ -587,18 +587,19 @@ void Job::OnJobCompleted(net::Error error,
 }
 
 void Job::CompleteAndClearRequests(net::Error error,
-                                   std::unique_ptr<std::string> response_body) {
+                                   std::optional<std::string> response_body) {
   for (const auto& request : requests_) {
-    std::string empty_str;
     request->OnJobCompleted(this, error,
-                            response_body ? response_body.get() : &empty_str);
+                            response_body.has_value()
+                                ? base::as_byte_span(*response_body)
+                                : base::span<uint8_t>());
   }
 
   requests_.clear();
 }
 
 void Job::FailRequest(net::Error error) {
-  OnJobCompleted(error, nullptr);
+  OnJobCompleted(error, std::nullopt);
 }
 
 }  // namespace
