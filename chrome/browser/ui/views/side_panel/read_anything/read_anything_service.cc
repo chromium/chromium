d373403370bb964f389a5feb5f0ebb9c07279c18
Lauren Winston <lwinston@google.com>
2025-04-22 19:53:43 -0700

    [Read aloud] Stop automatic TTS extension installs.
    This delays installing the TTS extension via component updater until
    reading mode has been opened. This prevents unnecessary installs
    for Chrome users who are not using reading mode.
    
    This does mean that on first open, reading mode will be a little bit
    slower at getting natural voices (30+ seconds).
    
    Fixed: 405185042
    Change-Id: I2dfab69526d99dfec5663420c4b2d8618438d073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6378185
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Reviewed-by: Xiang Xiao <xiangxiao@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450335}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_service.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_service.cc
@@ -6,7 +6,6 @@
 
 #include "base/check_is_test.h"
 #include "chrome/browser/accessibility/embedded_a11y_extension_loader.h"
-#include "chrome/browser/extensions/component_loader.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -20,6 +19,11 @@
 #include "extensions/browser/extension_system.h"
 #include "ui/accessibility/accessibility_features.h"
 
+#if !BUILDFLAG(IS_CHROMEOS)
+#include "chrome/browser/component_updater/wasm_tts_engine_component_installer.h"
+#include "chrome/browser/extensions/component_loader.h"
+#endif  // !BUILDFLAG(IS_CHROMEOS)
+
 namespace {
 
 // The number of seconds to wait before removing the extension. This avoids
@@ -27,6 +31,11 @@ namespace {
 constexpr int kRemoveExtensionDelaySeconds = 30;
 
 }  // namespace
+
+#if !BUILDFLAG(IS_CHROMEOS)
+const base::FilePath::CharType kManifestFileName[] =
+    FILE_PATH_LITERAL("wasm_tts_manifest.json");
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 ReadAnythingService::ReadAnythingService(Profile* profile) : profile_(profile) {
   if (features::IsReadAnythingDocsIntegrationEnabled()) {
@@ -62,17 +71,9 @@ void ReadAnythingService::OnReadAnythingSidePanelEntryShown() {
 // The TTS download extension should only be installed on non-ChromeOS devices
 // when the Read Aloud flag is enabled.
 #if !BUILDFLAG(IS_CHROMEOS)
-  if (features::IsReadAnythingReadAloudEnabled() &&
-      !features::IsWasmTtsComponentUpdaterEnabled() &&
-      !features::IsWasmTtsEngineAutoInstallDisabled()) {
-    InstallTtsDownloadExtension();
-  } else {
-    // If the extension was previously installed but now the Read Aloud flag
-    // is disabled, or if the component updater flag is enabled, we should
-    // uninstall the extension.
-    RemoveTtsDownloadExtension();
-  }
+  SetupDesktopEngine();
 #endif  // !BUILDFLAG(IS_CHROMEOS)
+
   if (!features::IsReadAnythingDocsIntegrationEnabled()) {
     return;
   }
@@ -80,6 +81,35 @@ void ReadAnythingService::OnReadAnythingSidePanelEntryShown() {
   active_local_side_panel_count_++;
   InstallGDocsHelperExtension();
 }
+
+#if !BUILDFLAG(IS_CHROMEOS)
+void ReadAnythingService::SetupDesktopEngine() {
+  // If the WasmTtsComponentUpdater flag is disabled, install the Tts
+  // extension as a component extension.
+  if (features::IsReadAnythingReadAloudEnabled() &&
+      !features::IsWasmTtsComponentUpdaterEnabled() &&
+      !features::IsWasmTtsEngineAutoInstallDisabled()) {
+    InstallTtsDownloadExtension();
+    return;
+  }
+
+  // If the extension was previously installed but now the Read Aloud flag
+  // is disabled, or if the component updater flag is enabled, we should
+  // uninstall the component extension.
+  RemoveTtsDownloadExtension();
+
+  // Install the TTS extension via the component updater if the
+  // component updater flag is enabled.
+  if (features::IsReadAnythingReadAloudEnabled() &&
+      features::IsWasmTtsComponentUpdaterEnabled() &&
+      !features::IsWasmTtsEngineAutoInstallDisabled()) {
+    // Signal that the reading mode panel is opened and it's now safe to
+    // install the WasmTtsEngineComponent.
+    component_updater::WasmTtsEngineComponentInstallerPolicy::
+        GetWasmTTSEngineDirectory(base::BindOnce(InstallComponent));
+  }
+}
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 void ReadAnythingService::OnReadAnythingSidePanelEntryHidden() {
   if (!features::IsReadAnythingDocsIntegrationEnabled()) {
@@ -180,3 +210,13 @@ void ReadAnythingService::RemoveTtsDownloadExtension() {
   component_loader->Remove(extension_misc::kTTSEngineExtensionId);
 #endif  // !BUILDFLAG(IS_CHROMEOS)
 }
+
+#if !BUILDFLAG(IS_CHROMEOS)
+void ReadAnythingService::InstallComponent(const base::FilePath& new_dir) {
+  EmbeddedA11yExtensionLoader::GetInstance()->Init();
+  EmbeddedA11yExtensionLoader::GetInstance()->InstallExtensionWithIdAndPath(
+      extension_misc::kComponentUpdaterTTSEngineExtensionId, new_dir,
+      kManifestFileName,
+      /*should_localize=*/false);
+}
+#endif  // !BUILDFLAG(IS_CHROMEOS)
