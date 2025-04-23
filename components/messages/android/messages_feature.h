1d2685294417139baff8341dcab337220bcaffa3
Lijin Shen <lazzzis@google.com>
2025-04-23 11:43:22 -0700

    [Messages] Add feature flag of close button
    Bug: 400108297
    Change-Id: I4fa63af3f574811d27a6ea21d0405175bae7fcfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6481231
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Auto-Submit: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1450693}

--- components/messages/android/messages_feature.h
+++ components/messages/android/messages_feature.h
@@ -16,6 +16,9 @@ BASE_DECLARE_FEATURE(kMessagesForAndroidFullyVisibleCallback);
 // Feature that enables extra histogram recordings.
 BASE_DECLARE_FEATURE(kMessagesAndroidExtraHistograms);
 
+// Feature that enables a close button when mouses hovers over.
+BASE_DECLARE_FEATURE(kMessagesCloseButton);
+
 }  // namespace messages
 
 #endif  // COMPONENTS_MESSAGES_ANDROID_MESSAGES_FEATURE_H_
