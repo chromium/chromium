89aa34c2a9e43c519bf55c77c710191f008c661b
Dominik Röttsches <drott@chromium.org>
2025-04-24 05:40:44 -0700

    [gardener] Mark UsersPrivateApiTest.IsUserInList as flaky on CrOS
    Bug: chromium:412375846
    Change-Id: I28892e24c806fc2cd43d36199965642c4a331860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6487688
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451077}

--- chrome/browser/ash/extensions/users_private/users_private_apitest.cc
+++ chrome/browser/ash/extensions/users_private/users_private_apitest.cc
@@ -224,7 +224,13 @@ class UsersPrivateApiLockStatusTest : public UsersPrivateApiLoginStatusTest {
 
 }  // namespace
 
-IN_PROC_BROWSER_TEST_F(UsersPrivateApiTest, AddUser) {
+// TODO(crbug.com/413060105): Re-enable this test
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_AddUser DISABLED_AddUser
+#else
+#define MAYBE_AddUser AddUser
+#endif
+IN_PROC_BROWSER_TEST_F(UsersPrivateApiTest, MAYBE_AddUser) {
   EXPECT_TRUE(RunSubtest("addUser")) << message_;
 }
 
