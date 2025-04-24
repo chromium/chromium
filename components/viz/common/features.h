c8978c89182b6d7a2e7588717e1d791964d219ff
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-04-23 23:08:50 -0700

    Optimize occlusion culling for large quads with rounded corners
    The current occlusion strategy for quads with rounded corners, which
    uses the largest inscribable rectangle within a rounded rectangle as an
    occluder, can lead to excessive overdraw. This overdraw is particularly
    problematic in ChromeOS due to the UI's frequent use of large quads with
    significantly large rounded corners.
    
    This change introduces complex occluders for rounded rectangles to
    reduce overdraw. However, these complex occluders necessitate a higher
    quad split limit to resolve the occluded quads.
    
    The optimization will be enabled for ChromeOS in a followup CL.
    
    DD:
    https://docs.google.com/document/d/11OxMBldoGn-MysN7dn4180hEAK9fBHUDB8qHojUQ6lw/edit?usp=sharing
    
    Bug: b:294589500
    Change-Id: I6a40bfe67b0f8b9c7e31d7d0267b9d9273bc70d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6442675
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1450953}

--- components/viz/common/features.h
+++ components/viz/common/features.h
@@ -157,6 +157,7 @@ VIZ_COMMON_EXPORT bool UseWebViewNewInvalidateHeuristic();
 VIZ_COMMON_EXPORT bool UseSurfaceLayerForVideo();
 VIZ_COMMON_EXPORT int MaxOverlaysConsidered();
 VIZ_COMMON_EXPORT bool ShouldOnBeginFrameThrottleVideo();
+VIZ_COMMON_EXPORT bool IsComplexOccluderForQuadsWithRoundedCornersEnabled();
 VIZ_COMMON_EXPORT bool ShouldDrawImmediatelyWhenInteractive();
 VIZ_COMMON_EXPORT bool IsVSyncAlignedPresentEnabled();
 VIZ_COMMON_EXPORT bool ShouldLogFrameQuadInfo();
