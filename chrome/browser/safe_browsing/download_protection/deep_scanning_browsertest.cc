a1d898e4ea0441c4076c2c9fc615bccc59a2b241
Jiacheng Guo <gjc@google.com>
2025-04-24 23:47:08 -0700

    Revert "Modify browser tests to wait for UploadSecurityEventReport call."
    This reverts commit f699a825b3a55509394093d8057adf5086241493.
    
    Reason for revert: Causing test SavePackageDeepScanningBrowserTest.KeepAfterWarning to be flaky.
    
    Dashboard: https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FSavePackageDeepScanningBrowserTest.KeepAfterWarning?q=V%3Abuilder%3DLinux%2520Chromium%2520OS%2520ASan%2520LSan%2520Tests%2520%281%29+V%3Aos%3DUbuntu-22.04+V%3Atest_suite%3Dbrowser_tests
    
    Bug: 408452167
    Original change's description:
    > Modify browser tests to wait for UploadSecurityEventReport call.
    >
    > This CL adds base::RunLoop to browser tests that failed when the
    > LocalIpAddressesInEvents finch experiment was enabled for all tests,
    > which modifies the upload flow to be asynchronous.
    >
    > Bug: 408452167
    > Change-Id: I1f7036653523f6f9fb7e37eed42eee833cc56aff
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6460545
    > Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Commit-Queue: Lina Ismail <ismaill@google.com>
    > Reviewed-by: thefrog <thefrog@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1451298}
    
    Bug: 408452167
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iad893ccc3a659475b2a45e78428d9bc823316e0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491228
    Owners-Override: Jiacheng Guo <gjc@google.com>
    Auto-Submit: Jiacheng Guo <gjc@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1451638}

--- chrome/browser/safe_browsing/download_protection/deep_scanning_browsertest.cc
+++ chrome/browser/safe_browsing/download_protection/deep_scanning_browsertest.cc
@@ -9,7 +9,6 @@
 #include "base/files/file_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/path_service.h"
-#include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
@@ -162,7 +161,6 @@ class DownloadDeepScanningBrowserTestBase
                          enterprise_obfuscation::kEnterpriseFileObfuscation)
                    : disabled_features.push_back(
                          enterprise_obfuscation::kEnterpriseFileObfuscation);
-    enabled_features.push_back(safe_browsing::kLocalIpAddressInEvents);
 
     scoped_feature_list_.InitWithFeatures(std::move(enabled_features),
                                           std::move(disabled_features));
@@ -533,8 +531,8 @@ class DownloadDeepScanningBrowserTestBase
     }
 
     if (request.url == connector_url_) {
-      ASSERT_TRUE(GetResumableUploadMetadata(network::GetUploadData(request),
-                                             &last_request_));
+        ASSERT_TRUE(GetResumableUploadMetadata(network::GetUploadData(request),
+                                               &last_request_));
     }
   }
 
@@ -1265,9 +1263,7 @@ IN_PROC_BROWSER_TEST_F(SavePackageDeepScanningBrowserTest, Blocked) {
             enterprise_connectors::ContentAnalysisRequest::SAVE_AS_DOWNLOAD);
 
   // The blocking response should trigger a security event.
-  base::RunLoop validator_run_loop;
   enterprise_connectors::test::EventReportValidator validator(client());
-  validator.SetDoneClosure(validator_run_loop.QuitClosure());
   std::set<std::string> mimetypes = {"text/plain"};
   validator.ExpectSensitiveDataEvent(
       /*url*/ url.spec(),
@@ -1303,7 +1299,6 @@ IN_PROC_BROWSER_TEST_F(SavePackageDeepScanningBrowserTest, Blocked) {
   base::ScopedAllowBlockingForTesting allow_blocking;
   EXPECT_FALSE(base::PathExists(main_file));
   EXPECT_FALSE(base::PathExists(extra_files_dir));
-  validator_run_loop.Run();
 }
 
 IN_PROC_BROWSER_TEST_F(SavePackageDeepScanningBrowserTest, KeepAfterWarning) {
