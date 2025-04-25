dda4749e9a03ef28d59654abf61614c12202c4b5
Mike Wittman <wittman@chromium.org>
2025-04-24 17:13:02 -0700

    Add passage embeddings coordinator
    The passage embeddings coordinator listens for AnnotatedPageContent
    provided by the PageContentExtractionService, generates
    passages from the AnnotatedPageContent, then submits them to the
    ChromePassageEmbeddingsServiceController's Embedder to compute
    embeddings from the passages.
    
    The coordinator will be allocated per-profile via the
    BrowserContextKeyedServiceFactory subclass since the
    PageContentExtractionService also operates at profile granularity.
    
    Bug: 408250012
    Change-Id: I615d068050fc4e8a491b8bed8dc7750498a874a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6481578
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451549}

---
+++ chrome/browser/passage_embeddings/passage_embeddings_coordinator.cc
@@ -0,0 +1,207 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/passage_embeddings/passage_embeddings_coordinator.h"
+
+#include <algorithm>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "base/functional/bind.h"
+#include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/strings/strcat.h"
+#include "chrome/browser/page_content_annotations/page_content_extraction_service.h"
+#include "chrome/browser/passage_embeddings/chrome_passage_embeddings_service_controller.h"
+#include "components/passage_embeddings/passage_embeddings_features.h"
+#include "content/public/browser/render_frame_host.h"
+#include "content/public/browser/web_contents.h"
+
+namespace passage_embeddings {
+
+namespace {
+
+void CollectTextForContentNode(
+    const optimization_guide::proto::ContentNode& node,
+    std::vector<std::string>& text) {
+  if (!node.has_content_attributes()) {
+    return;
+  }
+
+  const auto& attributes = node.content_attributes();
+
+  switch (attributes.attribute_type()) {
+    case optimization_guide::proto::ContentAttributeType::
+        CONTENT_ATTRIBUTE_TABLE:
+      if (!attributes.table_data().table_name().empty()) {
+        text.push_back(attributes.table_data().table_name());
+      }
+      break;
+
+    case optimization_guide::proto::ContentAttributeType::
+        CONTENT_ATTRIBUTE_TEXT:
+      if (!attributes.text_data().text_content().empty()) {
+        text.push_back(attributes.text_data().text_content());
+      }
+      break;
+
+    case optimization_guide::proto::ContentAttributeType::
+        CONTENT_ATTRIBUTE_IMAGE:
+      if (!attributes.image_data().image_caption().empty()) {
+        text.push_back(attributes.image_data().image_caption());
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+void CollectTextForContentNodesRecursively(
+    const optimization_guide::proto::ContentNode& node,
+    std::vector<std::string>& text) {
+  CollectTextForContentNode(node, text);
+
+  for (const auto& child : node.children_nodes()) {
+    CollectTextForContentNodesRecursively(child, text);
+  }
+}
+
+int CountWords(std::string_view s) {
+  if (s.empty()) {
+    return 0;
+  }
+  int word_count = (s[0] == ' ') ? 0 : 1;
+  for (size_t i = 1; i < s.length(); i++) {
+    if (s[i] != ' ' && s[i - 1] == ' ') {
+      word_count++;
+    }
+  }
+  return word_count;
+}
+
+// Provide a translation of APC to passages for the purposes of measuring
+// embeddings performance. This translation is extremely simple and not intended
+// to be a full fidelity representation or to be reused outside of this limited
+// experiment.
+std::vector<std::string> CreatePassagesFromAnnotatedPageContent(
+    const optimization_guide::proto::AnnotatedPageContent&
+        annotated_page_content) {
+  std::vector<std::string> text;
+  CollectTextForContentNodesRecursively(annotated_page_content.root_node(),
+                                        text);
+
+  if (text.empty()) {
+    return {};
+  }
+
+  const auto append_with_whitespace_separator =
+      [](std::string& str, std::string_view str_to_append) {
+        if (str_to_append.empty()) {
+          return;
+        }
+
+        if (str.empty() || str.back() == ' ') {
+          str.append(str_to_append);
+          return;
+        }
+
+        base::StrAppend(&str, {" ", str_to_append});
+      };
+
+  const int max_words_per_aggregate_passage = kMinWordsPerPassage.Get();
+  const int min_words_per_passage = kMinWordsPerPassage.Get();
+  const int max_passages_per_page = kMaxPassagesPerPage.Get();
+
+  std::vector<std::string> passages;
+  passages.push_back("");
+  int current_passage_words = 0;
+
+  for (const std::string& item : text) {
+    const int item_words = CountWords(item);
+
+    if (current_passage_words >= max_words_per_aggregate_passage) {
+      if (passages.size() >= static_cast<size_t>(max_passages_per_page)) {
+        break;
+      }
+      passages.push_back("");
+      current_passage_words = 0;
+    }
+
+    const bool should_append =
+        current_passage_words < min_words_per_passage ||
+        current_passage_words + item_words <= max_words_per_aggregate_passage;
+
+    if (should_append) {
+      append_with_whitespace_separator(passages.back(), item);
+      current_passage_words += item_words;
+    }
+  }
+
+  if (passages.back() == "") {
+    passages.pop_back();
+  }
+
+  return passages;
+}
+
+}  // namespace
+
+PassageEmbeddingsCoordinator::PassageEmbeddingsCoordinator(
+    page_content_annotations::PageContentExtractionService*
+        page_content_extraction_service) {
+  page_content_extraction_observation_.Observe(page_content_extraction_service);
+}
+
+PassageEmbeddingsCoordinator::~PassageEmbeddingsCoordinator() = default;
+
+void PassageEmbeddingsCoordinator::OnPageContentExtracted(
+    content::Page& page,
+    const optimization_guide::proto::AnnotatedPageContent& page_content) {
+  std::vector<std::string> passages =
+      CreatePassagesFromAnnotatedPageContent(page_content);
+  VLOG(2) << "Received page content for url "
+          << page_content.main_frame_data().url() << ". Generated "
+          << passages.size() << " passages.";
+  auto* const web_contents =
+      content::WebContents::FromRenderFrameHost(&page.GetMainDocument());
+  const uintptr_t web_contents_id = reinterpret_cast<uintptr_t>(web_contents);
+  auto loc = web_contents_task_ids_.find(web_contents_id);
+  if (loc != web_contents_task_ids_.end()) {
+    ChromePassageEmbeddingsServiceController::Get()->GetEmbedder()->TryCancel(
+        loc->second);
+  }
+  const Embedder::TaskId task_id =
+      ChromePassageEmbeddingsServiceController::Get()
+          ->GetEmbedder()
+          ->ComputePassagesEmbeddings(
+              PassagePriority::kPassive, std::move(passages),
+              base::BindOnce(
+                  &PassageEmbeddingsCoordinator::OnPassageEmbeddingsComputed,
+                  weak_ptr_factory_.GetWeakPtr(), web_contents_id));
+  web_contents_task_ids_[web_contents_id] = task_id;
+}
+
+void PassageEmbeddingsCoordinator::OnPassageEmbeddingsComputed(
+    uintptr_t web_contents_id,
+    std::vector<std::string> passages,
+    std::vector<Embedding> embeddings,
+    Embedder::TaskId task_id,
+    ComputeEmbeddingsStatus status) {
+  VLOG(2) << "Got " << embeddings.size() << " embeddings with status "
+          << static_cast<int>(status) << " for task " << task_id;
+  const auto loc = web_contents_task_ids_.find(web_contents_id);
+  CHECK(loc != web_contents_task_ids_.end());
+  if (loc->second == task_id) {
+    web_contents_task_ids_.erase(loc);
+  } else {
+    if (status == ComputeEmbeddingsStatus::kSuccess) {
+      base::UmaHistogramCounts100("History.Embeddings.StaleComputedEmbeddings",
+                                  embeddings.size());
+    }
+  }
+}
+
+}  // namespace passage_embeddings
---
+++ chrome/browser/passage_embeddings/passage_embeddings_coordinator.h
@@ -0,0 +1,62 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PASSAGE_EMBEDDINGS_PASSAGE_EMBEDDINGS_COORDINATOR_H_
+#define CHROME_BROWSER_PASSAGE_EMBEDDINGS_PASSAGE_EMBEDDINGS_COORDINATOR_H_
+
+#include <cstdint>
+#include <map>
+
+#include "base/memory/weak_ptr.h"
+#include "base/scoped_observation.h"
+#include "chrome/browser/page_content_annotations/page_content_extraction_service.h"
+#include "components/keyed_service/core/keyed_service.h"
+#include "components/passage_embeddings/passage_embeddings_types.h"
+
+namespace content {
+class WebContents;
+}
+
+namespace page_content_annotations {
+class PageContentExtractionService;
+}
+
+namespace passage_embeddings {
+
+class PassageEmbeddingsCoordinator
+    : public KeyedService,
+      public page_content_annotations::PageContentExtractionService::Observer {
+ public:
+  explicit PassageEmbeddingsCoordinator(
+      page_content_annotations::PageContentExtractionService*
+          page_content_extraction_service);
+  ~PassageEmbeddingsCoordinator() override;
+
+  // PageContentExtractionService:
+  void OnPageContentExtracted(
+      content::Page& page,
+      const optimization_guide::proto::AnnotatedPageContent& page_content)
+      override;
+
+ private:
+  void OnPassageEmbeddingsComputed(uintptr_t web_contents_id,
+                                   std::vector<std::string> passages,
+                                   std::vector<Embedding> embeddings,
+                                   Embedder::TaskId task_id,
+                                   ComputeEmbeddingsStatus status);
+
+  // The key is an id for a WebContents.
+  std::map<uintptr_t, Embedder::TaskId> web_contents_task_ids_;
+
+  base::ScopedObservation<
+      page_content_annotations::PageContentExtractionService,
+      PassageEmbeddingsCoordinator>
+      page_content_extraction_observation_{this};
+
+  base::WeakPtrFactory<PassageEmbeddingsCoordinator> weak_ptr_factory_{this};
+};
+
+}  // namespace passage_embeddings
+
+#endif  // CHROME_BROWSER_PASSAGE_EMBEDDINGS_PASSAGE_EMBEDDINGS_COORDINATOR_H_
