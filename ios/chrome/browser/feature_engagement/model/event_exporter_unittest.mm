bdebbbe667dfa9086b4774cd796bc7e439ea4071
Louis Romero <lpromero@google.com>
2025-04-25 07:40:57 -0700

    Fix EventExporterTest for Shared Tab Groups
    Bug: 409499069
    Change-Id: Ia85ccb7b120c910fa0aa60824b9c599ef20cfc2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6490326
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451789}

--- ios/chrome/browser/feature_engagement/model/event_exporter_unittest.mm
+++ ios/chrome/browser/feature_engagement/model/event_exporter_unittest.mm
@@ -23,6 +23,7 @@
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity_manager.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
+#import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/platform_test.h"
 
@@ -110,6 +111,7 @@ class EventExporterTest : public PlatformTest {
     }
   }
 
+  IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   web::WebTaskEnvironment task_environment_;
   base::RunLoop run_loop_;
   std::unique_ptr<feature_engagement::Tracker> tracker_;
