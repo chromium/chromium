90d54b094409b2e1eefbb658184c5abf29765b52
Zhengzheng Liu <zhzhliu@chromium.org>
2025-04-25 09:00:45 -0700

    Chromium Task Manager - Change the horizontal space for search bar
    Description :
    Based on the UX mock, the horizontal space for search bar should be
    4dp.
    
    Screenshot demo:
    https://screenshot.googleplex.com/4fFyVaxWYnitnPv
    
    Bug: 413420661
    Change-Id: I773e17beb30edaf037a799c4c30fd1ad143e7f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489230
    Commit-Queue: Zhengzheng Liu <zhzhliu@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451847}

--- chrome/browser/ui/views/chrome_layout_provider.cc
+++ chrome/browser/ui/views/chrome_layout_provider.cc
@@ -166,7 +166,7 @@ int ChromeLayoutProvider::GetDistanceMetric(int metric) const {
     case DISTANCE_RICH_HOVER_BUTTON_ICON_HORIZONTAL:
       return 8;
     case DISTANCE_TASK_MANAGER_SEARCH_BAR_ICON_AND_BUTTON_HORIZONTAL_SPACING:
-      return 12;
+      return 4;
     case DISTANCE_TASK_MANAGER_SEARCH_ICON_SIZE:
       return 20;
     case DISTANCE_TASK_MANAGER_SEARCH_BAR_MIN_WIDTH:
