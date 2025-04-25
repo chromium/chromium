83959ec0bac67c2b85f0c581751d7f62a2c7884f
Paul Adedeji <pauladedeji@google.com>
2025-04-25 09:28:19 -0700

    Revert "[ntp footer] Move ntp background attributions into a new element."
    This reverts commit c598b3d9eaad44ded4dbd64f619f62d8b54b6284.
    
    Reason for revert: We're no longer creating the new footer element inside of c/b/r/new_tab_page.
    
    Bug: 394906782
    Original change's description:
    > [ntp footer] Move ntp background attributions into a new element.
    >
    > To support the NTP footer, detailed by crbug.com/385172374,
    > this CL creates a new footer element that will house the elements that
    > are currently at the bottom of the NTP, starting with background
    > attributions.
    >
    > * Other than moving where the attributions are defined, no behavior is
    > added or removed for them in this CL.
    >
    > Follow-up CLs will address moving the customize buttons and theme
    > attribution to `footer.html` as well.
    >
    > Screenshots:
    > NTP w/ image bg - screen/84nNUpyq93D445v
    > NTP w/ color bg - screen/6Uj7QAcVimGQSxV
    > NTP w/o bg - screen/3wQ7MdMPLsx46de
    > NTP in RTL mode - screen/3uhNsmAi6Nqa2vA
    >
    > Change-Id: I2b0bae025d339c0bdd5b495ef6120e462d5c63c6
    > Bug: 394906782
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6259597
    > Reviewed-by: Roman Arora <romanarora@chromium.org>
    > Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1422008}
    
    Bug: 394906782
    Change-Id: I33dbfc173f549f51989e802673b05fb5c021ecd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6483710
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1451861}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -4,7 +4,6 @@
 
 import './iframe.js';
 import './logo.js';
-import './footer.js';
 import '/strings.m.js';
 import 'chrome://resources/cr_components/searchbox/searchbox.js';
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
@@ -15,7 +14,7 @@ import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_to
 import type {ClickInfo} from 'chrome://resources/js/browser_command.mojom-webui.js';
 import {Command} from 'chrome://resources/js/browser_command.mojom-webui.js';
 import {BrowserCommandProxy} from 'chrome://resources/js/browser_command/browser_command_proxy.js';
-import {hexColorToSkColor} from 'chrome://resources/js/color_utils.js';
+import {hexColorToSkColor, skColorToRgba} from 'chrome://resources/js/color_utils.js';
 import {EventTracker} from 'chrome://resources/js/event_tracker.js';
 import {FocusOutlineManager} from 'chrome://resources/js/focus_outline_manager.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
@@ -36,7 +35,7 @@ import type {PageCallbackRouter, PageHandlerRemote, Theme} from './new_tab_page.
 import {CustomizeChromeSection, IphFeature, NtpBackgroundImageSource} from './new_tab_page.mojom-webui.js';
 import {NewTabPageProxy} from './new_tab_page_proxy.js';
 import type {MicrosoftAuthUntrustedDocumentRemote} from './ntp_microsoft_auth_shared_ui.mojom-webui.js';
-import {$$, rgbaOrInherit} from './utils.js';
+import {$$} from './utils.js';
 import {Action as VoiceAction, recordVoiceAction} from './voice_search_overlay.js';
 import {WindowProxy} from './window_proxy.js';
 
@@ -170,6 +169,9 @@ export class AppElement extends AppElementBase {
         type: Boolean,
       },
 
+      backgroundImageAttribution1_: {type: String},
+      backgroundImageAttribution2_: {type: String},
+      backgroundImageAttributionUrl_: {type: String},
       backgroundColor_: {type: Object},
 
       // Used in cr-searchbox component via host-context.
@@ -256,6 +258,9 @@ export class AppElement extends AppElementBase {
   private accessor selectedCustomizeDialogPage_: string|null;
   protected accessor showVoiceSearchOverlay_: boolean = false;
   protected accessor showBackgroundImage_: boolean;
+  protected accessor backgroundImageAttribution1_: string;
+  protected accessor backgroundImageAttribution2_: string;
+  protected accessor backgroundImageAttributionUrl_: string;
   protected accessor backgroundColor_: SkColor|null;
   protected accessor colorSourceIsBaseline: boolean;
   protected accessor logoColor_: SkColor|null = null;
@@ -487,6 +492,12 @@ export class AppElement extends AppElementBase {
 
     if (changedPrivateProperties.has('theme_')) {
       this.showBackgroundImage_ = this.computeShowBackgroundImage_();
+      this.backgroundImageAttribution1_ =
+          this.computeBackgroundImageAttribution1_();
+      this.backgroundImageAttribution2_ =
+          this.computeBackgroundImageAttribution2_();
+      this.backgroundImageAttributionUrl_ =
+          this.computeBackgroundImageAttributionUrl_();
       this.colorSourceIsBaseline = this.computeColorSourceIsBaseline();
       this.logoColor_ = this.computeLogoColor_();
       this.singleColoredLogo_ = this.computeSingleColoredLogo_();
@@ -524,7 +535,7 @@ export class AppElement extends AppElementBase {
 
     if (changedPrivateProperties.has('logoColor_')) {
       this.style.setProperty(
-          '--ntp-logo-color', rgbaOrInherit(this.logoColor_));
+          '--ntp-logo-color', this.rgbaOrInherit_(this.logoColor_));
     }
 
     if (changedPrivateProperties.has('showBackgroundImage_')) {
@@ -559,6 +570,20 @@ export class AppElement extends AppElementBase {
       return false;
     }
     return !this.showBackgroundImage_;
+  }
+
+  private computeBackgroundImageAttribution1_(): string {
+    return this.theme_ && this.theme_.backgroundImageAttribution1 || '';
+  }
+
+  private computeBackgroundImageAttribution2_(): string {
+    return this.theme_ && this.theme_.backgroundImageAttribution2 || '';
+  }
+
+  private computeBackgroundImageAttributionUrl_(): string {
+    return this.theme_ && this.theme_.backgroundImageAttributionUrl ?
+        this.theme_.backgroundImageAttributionUrl.url :
+        '';
   }
 
   private computeRealboxShown_(): boolean {
@@ -666,6 +691,10 @@ export class AppElement extends AppElementBase {
     }
   }
 
+  private rgbaOrInherit_(skColor: SkColor|null): string {
+    return skColor ? skColorToRgba(skColor) : 'inherit';
+  }
+
   private computeShowBackgroundImage_(): boolean {
     return !!this.theme_ && !!this.theme_.backgroundImage;
   }
@@ -678,8 +707,11 @@ export class AppElement extends AppElementBase {
     if (this.theme_) {
       this.backgroundManager_.setBackgroundColor(this.theme_.backgroundColor);
       this.style.setProperty(
+          '--color-new-tab-page-attribution-foreground',
+          this.rgbaOrInherit_(this.theme_.textColor));
+      this.style.setProperty(
           '--color-new-tab-page-most-visited-foreground',
-          rgbaOrInherit(this.theme_.textColor));
+          this.rgbaOrInherit_(this.theme_.textColor));
     }
     this.updateBackgroundImagePath_();
   }
