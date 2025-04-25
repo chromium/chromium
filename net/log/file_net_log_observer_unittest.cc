61d77ea87a9f6cf97f04509eb0098f0e03ea22e3
Matt Menke <mmenke@chromium.org>
2025-04-24 20:46:15 -0700

    Spanify FileNetLogObserver and its tests.
    They were using some C-style file reads/writes, and a base::ScopedFILE,
    instead of a base::File for unclear reasons.
    
    Bug: 390223051
    Change-Id: Iaefdfa98e514e0e24b9b1e78c5792a6f2204dc16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6489226
    Commit-Queue: mmenke <mmenke@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1451608}

--- net/log/file_net_log_observer_unittest.cc
+++ net/log/file_net_log_observer_unittest.cc
@@ -2,16 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "net/log/file_net_log_observer.h"
 
 #include <string>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -252,7 +248,7 @@ class FileNetLogObserverTest : public ::testing::TestWithParam<bool>,
                     base::File::FLAG_CREATE | base::File::FLAG_WRITE);
     EXPECT_TRUE(file.IsValid());
     // Stick in some nonsense to make sure the file gets cleared properly
-    file.Write(0, "not json", 8);
+    file.WriteAtCurrentPos(base::as_byte_span("not json"));
 
     logger_ = FileNetLogObserver::CreateBoundedFile(
         std::move(file), max_file_size, NetLogCaptureMode::kDefault,
@@ -269,7 +265,7 @@ class FileNetLogObserverTest : public ::testing::TestWithParam<bool>,
                     base::File::FLAG_CREATE | base::File::FLAG_WRITE);
     EXPECT_TRUE(file.IsValid());
     // Stick in some nonsense to make sure the file gets cleared properly
-    file.Write(0, "not json", 8);
+    file.WriteAtCurrentPos(base::as_byte_span("not json"));
 
     if (IsBounded()) {
       logger_ = FileNetLogObserver::CreateBoundedPreExisting(
@@ -1061,7 +1057,7 @@ TEST_F(FileNetLogObserverBoundedTest, PreExistingUsesSpecifiedDir) {
   ASSERT_TRUE(file.IsValid());
 
   // Stick in some nonsense to make sure the file gets cleared properly
-  file.Write(0, "not json", 8);
+  file.WriteAtCurrentPos(base::as_byte_span("not json"));
 
   logger_ = FileNetLogObserver::CreateBoundedPreExisting(
       scratch_dir.GetPath(), std::move(file), kLargeFileSize,
