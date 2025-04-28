284830b9307aa5ba47a2966c3c52c902d3b176dc
Federica Germinario <fedegermi@google.com>
2025-04-28 00:45:58 -0700

    [ios] Mark test TestHandleForgottenIdentityPromptSignIn as FLAKY
    The test is flaky both on simulator and on device.
    
    Bug: 413588295
    Change-Id: I015fc8adb372418e0795cfe9d41486fcc7982761
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6491587
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1452439}

--- ios/chrome/browser/signin/model/authentication_service_unittest.mm
+++ ios/chrome/browser/signin/model/authentication_service_unittest.mm
@@ -357,7 +357,9 @@ TEST_P(AuthenticationServiceTest, TestHandleForgottenIdentityNoPromptSignIn) {
 
 // The reauth prompt should be shown if the primary identity is removed from an
 // other app when the user was signed in.
-TEST_P(AuthenticationServiceTest, TestHandleForgottenIdentityPromptSignIn) {
+// TODO(crbug.com/413588295): Fix flaky test.
+TEST_P(AuthenticationServiceTest,
+       FLAKY_TestHandleForgottenIdentityPromptSignIn) {
   // Sign in.
   authentication_service()->SignIn(identity(0),
                                    signin_metrics::AccessPoint::kUnknown);
