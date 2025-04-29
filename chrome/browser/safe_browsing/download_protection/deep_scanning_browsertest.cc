280927d27aebfaf0764102f448a1bb1b7cd10d08
Lina Ismail <ismaill@google.com>
2025-04-29 14:27:57 -0700

    Fix SavePackageDeepScanningBrowserTest Blocked test case.
    Bug: 408452167
    Change-Id: I2637a866552412c6ff931d6d658056e588020d05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495673
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Lina Ismail <ismaill@google.com>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Auto-Submit: Lina Ismail <ismaill@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453543}

--- chrome/browser/safe_browsing/download_protection/deep_scanning_browsertest.cc
+++ chrome/browser/safe_browsing/download_protection/deep_scanning_browsertest.cc
@@ -1263,7 +1263,9 @@ IN_PROC_BROWSER_TEST_F(SavePackageDeepScanningBrowserTest, Blocked) {
             enterprise_connectors::ContentAnalysisRequest::SAVE_AS_DOWNLOAD);
 
   // The blocking response should trigger a security event.
+  base::RunLoop validator_run_loop;
   enterprise_connectors::test::EventReportValidator validator(client());
+  validator.SetDoneClosure(validator_run_loop.QuitClosure());
   std::set<std::string> mimetypes = {"text/plain"};
   validator.ExpectSensitiveDataEvent(
       /*url*/ url.spec(),
@@ -1299,6 +1301,7 @@ IN_PROC_BROWSER_TEST_F(SavePackageDeepScanningBrowserTest, Blocked) {
   base::ScopedAllowBlockingForTesting allow_blocking;
   EXPECT_FALSE(base::PathExists(main_file));
   EXPECT_FALSE(base::PathExists(extra_files_dir));
+  validator_run_loop.Run();
 }
 
 IN_PROC_BROWSER_TEST_F(SavePackageDeepScanningBrowserTest, KeepAfterWarning) {
