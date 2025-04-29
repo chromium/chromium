398217b8cfc37dfc7c1e964eddf9ef13c7c5a593
Jan Keitel <jkeitel@google.com>
2025-04-29 07:28:29 -0700

    Modernize more loops in Autofill code
    Bug: 40100455
    Change-Id: I1cda9b31ed3b59eeee570acc725c56a34629ed20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494521
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1453225}

--- components/autofill/core/browser/crowdsourcing/determine_possible_field_types_unittest.cc
+++ components/autofill/core/browser/crowdsourcing/determine_possible_field_types_unittest.cc
@@ -9,6 +9,7 @@
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/task_environment.h"
+#include "base/types/zip.h"
 #include "components/autofill/core/browser/data_model/addresses/autofill_i18n_api.h"
 #include "components/autofill/core/browser/data_model/addresses/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/payments/credit_card.h"
@@ -980,15 +981,15 @@ TEST_P(DeterminePossibleFormatStringsForUploadTest_MultipleTextInput,
 
   std::vector<Matcher<std::pair<FieldGlobalId, base::flat_set<std::u16string>>>>
       expectations;
-  for (size_t i = 0; i < GetParam().fields.size(); ++i) {
-    std::vector<std::u16string> format_strings =
-        base::ToVector(GetParam().fields[i].format_strings,
-                       [](std::string_view format_string) {
-                         return base::UTF8ToUTF16(format_string);
-                       });
+  for (auto [form_field, autofill_field] :
+       base::zip(GetParam().fields, fields)) {
+    std::vector<std::u16string> format_strings = base::ToVector(
+        form_field.format_strings, [](std::string_view format_string) {
+          return base::UTF8ToUTF16(format_string);
+        });
     if (!format_strings.empty()) {
       expectations.emplace_back(
-          Pair(fields[i]->global_id(),
+          Pair(autofill_field->global_id(),
                UnorderedElementsAreArray(std::move(format_strings))));
     }
   }
