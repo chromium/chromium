7324cd3687a6e6ea8e445630ed550cdf5a4b2603
Sylvain Defresne <sdefresne@chromium.org>
2025-04-30 00:52:10 -0700

    [ios] Remove use of the macros BROWSER_USER_DATA_KEY_(DECL,IMPL)
    Those macros are no longer necessary so convert the code to declare
    the friend class directly and remove the macro usages. This fixes
    new usage added since https://crrev.com/c/6458806 landed.
    
    Bug: none
    Change-Id: I964315ffdaaa51066a90a119d8ddc794bb271351
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495199
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1453751}

--- ios/chrome/browser/browser_view/model/browser_view_visibility_notifier_browser_agent.mm
+++ ios/chrome/browser/browser_view/model/browser_view_visibility_notifier_browser_agent.mm
@@ -8,8 +8,6 @@
 #import "ios/chrome/browser/browser_view/model/browser_view_visibility_handler.h"
 #import "ios/chrome/browser/browser_view/model/browser_view_visibility_observer.h"
 
-BROWSER_USER_DATA_KEY_IMPL(BrowserViewVisibilityNotifierBrowserAgent)
-
 BrowserViewVisibilityNotifierBrowserAgent::
     BrowserViewVisibilityNotifierBrowserAgent(Browser* browser) {
   BrowserViewVisibilityStateChangeCallback callback =
