56e8facc0ae9f8884b75bdc28f506648acfaaac2
Alexandra Pereira <alexandrasp@google.com>
2025-04-30 10:25:27 -0700

    [iOS] Adds GCRWeb class
    Update gCrWeb to become a TypeScript class that provides an API for
    registering and retrieving functions instead of using the current logic
    based on the built in JavaScript object type.
    
    Bug: 399666983
    Change-Id: Ia81b2944f0a9d441723769f367d9b15996e1b99e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6329725
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454028}

--- ios/web/public/js_messaging/resources/gcrweb.ts
+++ ios/web/public/js_messaging/resources/gcrweb.ts
@@ -3,23 +3,62 @@
 // found in the LICENSE file.
 
 /**
- * @fileoverview This file exports `gCrWeb` to be used by other
+ * @fileoverview This file exports `gCrWeb` API to be used by other
  * files to augment its functionality. `gCrWeb` is intended
  * to be used as a bridge for native code to access JavaScript functionality.
  * The functions added to `gCrWeb` are not intended to be used within
- * other JavaScript files.
+ * other TypeScript files.
  */
 
-/**
- * Allow the TypeScript compiler to recognize that the Window has an __gCrWeb
- * object.
- */
-type WindowWithGcrWeb = Window&(typeof globalThis)&{__gCrWeb: any};
+class CrWeb {
+  private readonly registeredApis: {[id: string]: CrWebApi} = {};
+
+  /*
+   * Register a Javascript API into the CrWeb object. In case
+   * of any collision, do not override a pre-registered API.
+   */
+  registerApi(apiIdentifier: string, api: CrWebApi): void {
+    if (this.registeredApis[apiIdentifier] !== undefined) {
+      throw new Error(`API ${apiIdentifier} already registered.`);
+    }
+    this.registeredApis[apiIdentifier] = api;
+  }
+
+  getRegisteredApi(apiIdentifier: string): CrWebApi|undefined {
+    return this.registeredApis[apiIdentifier];
+  }
+}
+
+export class CrWebApi {
+  private readonly contents: {[id: string]: unknown} = {};
+
+  addFunction(name: string, func: Function): void {
+    this.contents[name] = func;
+  }
+
+  addProperty(name: string, property: unknown): void {
+    this.contents[name] = property;
+  }
+
+  getFunction(name: string): Function|null {
+    if (typeof this.contents[name] === 'function') {
+      return this.contents[name];
+    }
+    return null;
+  }
+
+  getProperty(name: string): unknown {
+    return this.contents[name];
+  }
+}
+
+type CrWebType = Window&(typeof globalThis)&{__gCrWeb: CrWeb};
 
 // Initializes window's `__gCrWeb` property. Without this step,
 // the window's `__gCrWeb` property cannot be found.
-if (!(window as WindowWithGcrWeb).__gCrWeb) {
-  (window as WindowWithGcrWeb).__gCrWeb = {};
+if (!(window as CrWebType).__gCrWeb) {
+  (window as CrWebType).__gCrWeb = new CrWeb();
 }
 
-export const gCrWebLegacy: any = (window as WindowWithGcrWeb).__gCrWeb;
+export const gCrWebLegacy: any = (window as CrWebType).__gCrWeb;
+export const gCrWeb: CrWeb = (window as CrWebType).__gCrWeb;
