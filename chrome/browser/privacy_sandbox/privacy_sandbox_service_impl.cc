01c6d0b56ce5e3cea622d5c5b36c9c20f7ced1a6
Aashna Sheth <aashnas@google.com>
2025-05-01 08:21:37 -0700

    Reland "Add IsUrlSuitable to entrypoint handler"
    This is a reland of commit 8d3eac472d4240c2e50630bd8f8a0a5dbf355f49
    
    Patchset diff: https://chromium-review.googlesource.com/c/chromium/src/+/6497563/1..2
    
    Original change's description:
    > Add IsUrlSuitable to entrypoint handler
    >
    > This CL moves a new navigation related check IsUrlSuitable out of
    > the general service.
    >
    > Bug: b:408016824
    > Change-Id: I48403f18403d0bd5c0a7c914cae7c773a16b2196
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6492066
    > Reviewed-by: Abe Boujane <boujane@google.com>
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Reviewed-by: Christine Ying <chrstne@google.com>
    > Commit-Queue: Aashna Sheth <aashnas@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1452894}
    
    Bug: b:408016824
    Change-Id: Ia0a1cf312092267149f7e8a704e7de704585213f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6497563
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Aashna Sheth <aashnas@google.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1454468}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service_impl.cc
@@ -35,7 +35,6 @@
 #include "chrome/browser/user_education/user_education_service.h"
 #include "chrome/browser/user_education/user_education_service_factory.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/common/webui_url_constants.h"
 #include "components/browsing_topics/browsing_topics_service.h"
 #include "components/browsing_topics/common/common_types.h"
 #include "components/browsing_topics/common/semantic_tree.h"
@@ -441,33 +440,6 @@ void RecordAdMeasurementEnabledHistograms(Profile* profile, bool enabled) {
 }
 
 }  // namespace
-
-// static
-bool PrivacySandboxService::IsUrlSuitableForPrompt(const GURL& url) {
-  // The prompt should be shown on a limited list of pages:
-
-  // about:blank is valid.
-  if (url.IsAboutBlank()) {
-    return true;
-  }
-  // Chrome settings page is valid. The subpages aren't as most of them are not
-  // related to the prompt.
-  if (url == GURL(chrome::kChromeUISettingsURL)) {
-    return true;
-  }
-  // Chrome history is valid as the prompt mentions history.
-  if (url == GURL(chrome::kChromeUIHistoryURL)) {
-    return true;
-  }
-  // Only a Chrome controlled New Tab Page is valid. Third party NTP is still
-  // Chrome controlled, but is without Google branding.
-  if (url == GURL(chrome::kChromeUINewTabPageURL) ||
-      url == GURL(chrome::kChromeUINewTabPageThirdPartyURL)) {
-    return true;
-  }
-
-  return false;
-}
 
 // static
 void PrivacySandboxService::SetPromptDisabledForTests(bool disabled) {
