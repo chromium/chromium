ff661caad7d1393ce5e8f2a627e2a5f2b4595427
Martin Kong <martinkong@google.com>
2025-05-02 13:36:00 -0700

    Capture Finch experiments in ANR reports
    Ordinary crash reports contain information about which Finch experiments
    were active on the client when the process crashed. ANR reports do not
    contain this information, which is to the detriment of their usability.
    This CL captures the list of Finch experiments and adds them to the ANR
    reports for Chrome Android. This is done as follows:
    
    First, whenever there is a change in field trials (including synthetic
    trials), UpdateCrashKeys() will be called. Inside UpdateCrashKeys(),
    we send the list of field trials to the Java side via a JNI call.
    In AnrCollector.java, we receive the string containing the list of
    field trials, and we save the raw string to a file on disk, and we
    keep one such file for each Chrome run. Additionally, we also save a
    short hash of this string to Process State Summary (we only save a
    short hash and not the raw string because the Process State Summary
    is very small in size).
    
    Then, when an ANR happens, the app will be forcibly closed. When the
    app is launched again, AnrCollector.java will query the historical
    exit reasons. If the reason is ANR, it will query the associated
    Process State Summary and retrieve the short hash. It will then read
    each file on disk and try to find a file that matches the short hash.
    It will then read the raw string containing the list of field trials
    from that file, and then rewrite_minidumps_as_mimes.cc will add this
    raw string to the ANR reports.
    
    An example ANR report that contains the list of Finch experiments
    (click on Fields and then Experiments):
    https://crash-staging.corp.google.com/fd9736f0f9b2d707
    
    Bug: 402461197
    Change-Id: I3ec94a6126b506709093c0dcaa3924e2a8c7de09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6463854
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Martin Kong <martinkong@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455170}

--- components/crash/android/java/src/org/chromium/components/crash/anr/AnrCollector.java
+++ components/crash/android/java/src/org/chromium/components/crash/anr/AnrCollector.java
@@ -14,11 +14,14 @@ import android.util.Pair;
 import androidx.annotation.RequiresApi;
 import androidx.annotation.VisibleForTesting;
 
-import org.jni_zero.NativeMethods;
+import org.jni_zero.CalledByNative;
+import org.jni_zero.JNINamespace;
+import org.jni_zero.JniType;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.metrics.RecordHistogram;
+import org.chromium.base.version_info.VersionInfo;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.components.crash.anr.AnrDataOuterClass.AnrData;
@@ -30,8 +33,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -42,6 +51,7 @@ import java.util.regex.Pattern;
  * <p>We also grab the version number associated with the ANR and pair that with the ANR so we have
  * confidence knowing which version of Chrome actually caused this ANR.
  */
+@JNINamespace("anr_collector")
 @RequiresApi(Build.VERSION_CODES.R)
 @NullMarked
 public class AnrCollector {
@@ -52,8 +62,34 @@ public class AnrCollector {
 
     private static final String ANR_SKIPPED_UMA = "Crashpad.AnrUpload.Skipped";
 
+    // We convert strings to bytes using the UTF-8 encoding, and then pass the bytes to
+    // setProcessStateSummary(). Since the UTF-8 encoding never uses the byte 0xFF,
+    // we can safely use 0xFF as a delimiter between different strings.
+    private static final byte PROCESS_STATE_SUMMARY_DELIMITER = (byte) 0xFF;
+
+    private static final File CACHE_DIR = ContextUtils.getApplicationContext().getCacheDir();
+
+    private static final File CRASH_DIR = new File(CACHE_DIR, "Crash Reports");
+
+    private static final File ANR_VARIATIONS_DIR = new File(CRASH_DIR, "ANR Variations");
+
+    private static final int MAX_NUM_OF_ANR_VARIATIONS_FILES = 5;
+
+    private static @Nullable File sPreviousAnrVariationsFile;
+
+    // Convert a byte array to a string containing its hexadecimal representation.
+    // TODO(martinkong): We should use java.util.HexFormat when our min-sdk is at least 34.
+    @VisibleForTesting
+    static String byteArrayToHexString(byte[] input) {
+        StringBuilder sb = new StringBuilder(input.length * 2);
+        for (byte b : input) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
     /**
-     * Grabs ANR reports from Android and writes them as 3-tuples as 3 entries in a string list.
+     * Grabs ANR reports from Android and writes them as 4-tuples as 4 entries in a string list.
      * This writes to disk synchronously, so should be called on a background thread.
      */
     public static List<String> collectAndWriteAnrs(File outDir) {
@@ -61,10 +97,6 @@ public class AnrCollector {
             return Collections.emptyList();
         }
         return writeAnrs(collectAnrs(), outDir);
-    }
-
-    public static String getSharedLibraryBuildId() {
-        return AnrCollectorJni.get().getSharedLibraryBuildId();
     }
 
     @VisibleForTesting
@@ -106,7 +138,7 @@ public class AnrCollector {
         return anrData;
     }
 
-    private static @Nullable Pair<AnrData, String> getAnrPair(ApplicationExitInfo reason) {
+    private static @Nullable Pair<AnrData, byte[]> getAnrPair(ApplicationExitInfo reason) {
         AnrData anr = null;
         try (InputStream is = reason.getTraceInputStream()) {
             // This can be null - this was causing crashes in crbug.com/1298852.
@@ -135,11 +167,10 @@ public class AnrCollector {
                     ANR_SKIPPED_UMA, AnrSkippedReason.MISSING_VERSION, AnrSkippedReason.MAX_VALUE);
             return null;
         }
-        String processStateSummary = new String(processStateSummaryBytes, StandardCharsets.UTF_8);
-        return new Pair<>(anr, processStateSummary);
+        return new Pair<>(anr, processStateSummaryBytes);
     }
 
-    private static List<Pair<AnrData, String>> collectAnrs() {
+    private static List<Pair<AnrData, byte[]>> collectAnrs() {
         ActivityManager am =
                 (ActivityManager)
                         ContextUtils.getApplicationContext()
@@ -152,11 +183,11 @@ public class AnrCollector {
         long maxHandledTime = lastHandledTime;
 
         List<ApplicationExitInfo> reasons = am.getHistoricalProcessExitReasons(null, 0, 0);
-        List<Pair<AnrData, String>> anrs = new ArrayList<>();
+        List<Pair<AnrData, byte[]>> anrs = new ArrayList<>();
         for (ApplicationExitInfo reason : reasons) {
             long time = reason.getTimestamp();
             if (reason.getReason() == ApplicationExitInfo.REASON_ANR && time > lastHandledTime) {
-                Pair<AnrData, String> pair = getAnrPair(reason);
+                Pair<AnrData, byte[]> pair = getAnrPair(reason);
                 if (pair != null) {
                     anrs.add(pair);
                     if (time > maxHandledTime) {
@@ -171,33 +202,102 @@ public class AnrCollector {
         return anrs;
     }
 
-    private static List<String> writeAnrs(List<Pair<AnrData, String>> anrs, File outDir) {
+    private static List<String> writeAnrs(List<Pair<AnrData, byte[]>> anrs, File outDir) {
         List<String> anrFiles = new ArrayList<>();
-        for (Pair<AnrData, String> pair : anrs) {
+        for (Pair<AnrData, byte[]> pair : anrs) {
             AnrData anr = pair.first;
-            String[] splitStateSummary = pair.second.split(",");
-            String version = splitStateSummary[0];
-            // There will always be a version number, but there's a chance that there won't be a
-            // buildId.
-            String buildId = "";
-            if (splitStateSummary.length > 1) {
-                buildId = splitStateSummary[1];
-                RecordHistogram.recordEnumeratedHistogram(
-                        ANR_SKIPPED_UMA, AnrSkippedReason.NOT_SKIPPED, AnrSkippedReason.MAX_VALUE);
-            } else {
-                RecordHistogram.recordEnumeratedHistogram(
-                        ANR_SKIPPED_UMA,
-                        AnrSkippedReason.ONLY_MISSING_NATIVE,
-                        AnrSkippedReason.MAX_VALUE);
-            }
+            byte[] processStateSummary = pair.second;
+            List<String> parsedProcessStateSummary = handleProcessStateSummary(processStateSummary);
+            assert parsedProcessStateSummary.size() == 3;
+            String version = parsedProcessStateSummary.get(0);
+            String buildId = parsedProcessStateSummary.get(1);
+            String variationsString = parsedProcessStateSummary.get(2);
+
             String anrFileName = writeAnr(anr, outDir);
             if (anrFileName != null) {
                 anrFiles.add(anrFileName);
                 anrFiles.add(version);
                 anrFiles.add(buildId);
+                anrFiles.add(variationsString);
+                RecordHistogram.recordEnumeratedHistogram(
+                        ANR_SKIPPED_UMA, AnrSkippedReason.NOT_SKIPPED, AnrSkippedReason.MAX_VALUE);
             }
         }
         return anrFiles;
+    }
+
+    // Parse the process state summary and return a 3-tuple containing the version number, build id,
+    // and variations string. The process state summary should always contain the version number.
+    // If it does not contain the build id or variations string, an empty string will be returned.
+    @VisibleForTesting
+    static List<String> handleProcessStateSummary(byte[] processStateSummary) {
+        List<byte[]> splitProcessStateSummary = new ArrayList<>();
+        int delimiterIndex = -1;
+        for (int i = 0; i < processStateSummary.length; i++) {
+            if (processStateSummary[i] == PROCESS_STATE_SUMMARY_DELIMITER) {
+                byte[] entry = Arrays.copyOfRange(processStateSummary, delimiterIndex + 1, i);
+                splitProcessStateSummary.add(entry);
+                delimiterIndex = i;
+                if (splitProcessStateSummary.size() == 2) {
+                    break;
+                }
+            }
+        }
+        byte[] finalEntry =
+                Arrays.copyOfRange(
+                        processStateSummary, delimiterIndex + 1, processStateSummary.length);
+        splitProcessStateSummary.add(finalEntry);
+
+        // There will always be a version number, but there's a chance that there won't be a
+        // build id or variations hash.
+        String version = new String(splitProcessStateSummary.get(0), StandardCharsets.UTF_8);
+        String buildId = "";
+        String variationsString = "";
+        if (splitProcessStateSummary.size() > 1) {
+            buildId = new String(splitProcessStateSummary.get(1), StandardCharsets.UTF_8);
+            if (splitProcessStateSummary.size() > 2) {
+                variationsString = getVariationsMatchingHash(splitProcessStateSummary.get(2));
+            }
+        }
+        return List.of(version, buildId, variationsString);
+    }
+
+    // Given an MD5 hash of the variations string, iterate over all the ANR variations files,
+    // find the file that matches the hash, and return the variations string in that file.
+    // If there is no file matching the hash, or if there is any other error, return empty string.
+    private static String getVariationsMatchingHash(byte[] variationsHash) {
+        if (variationsHash.length == 0) {
+            // This should never happen in theory.
+            Log.e(
+                    TAG,
+                    "A previous run that encountered ANR has set an empty variations hash. This"
+                            + " means it was unable to find digest algorithm MD5.");
+            return "";
+        }
+
+        File[] variationsFiles = ANR_VARIATIONS_DIR.listFiles();
+        if (variationsFiles == null || variationsFiles.length == 0) {
+            return "";
+        }
+        String expectedFileName = byteArrayToHexString(variationsHash);
+        for (File curVariationsFile : variationsFiles) {
+            String curVariationsFileName = curVariationsFile.getName();
+            if (expectedFileName.equals(curVariationsFileName)) {
+                List<String> curFileLines;
+                try {
+                    curFileLines = Files.readAllLines(curVariationsFile.toPath());
+                } catch (IOException e) {
+                    Log.e(
+                            TAG,
+                            "Unable to read the ANR variations file at %s: %s",
+                            curVariationsFile.toString(),
+                            e.toString());
+                    continue;
+                }
+                return String.join("\n", curFileLines);
+            }
+        }
+        return "";
     }
 
     private static @Nullable String writeAnr(AnrData data, File outDir) {
@@ -219,11 +319,131 @@ public class AnrCollector {
         }
     }
 
+    @CalledByNative
+    public static void saveVariations(
+            @JniType("std::string") String variationsString,
+            @JniType("std::string") String buildIdString) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            byte[] variationsHash = computeMD5Hash(variationsString);
+            if (variationsHash != null) {
+                saveVariationsHashToProcessStateSummary(variationsHash, buildIdString);
+                saveVariationsToFile(variationsHash, variationsString, true);
+            } else {
+                saveVariationsHashToProcessStateSummary(new byte[0], buildIdString);
+            }
+        }
+    }
+
+    // Compute the hash of the given string using the MD5 hashing algorithm. Return a byte array of
+    // length 16 on success. Return null on failure.
+    @VisibleForTesting
+    static byte @Nullable [] computeMD5Hash(String hashInput) {
+        try {
+            byte[] hashOutput =
+                    MessageDigest.getInstance("MD5")
+                            .digest(hashInput.getBytes(StandardCharsets.UTF_8));
+            assert hashOutput.length == 16 : "MD5 produced a hash that is not 16 bytes in size";
+            return hashOutput;
+        } catch (NoSuchAlgorithmException e) {
+            // This should never happen in theory.
+            Log.e(
+                    TAG,
+                    "Unable to find digest algorithm MD5. If an ANR happens, the ANR report will"
+                            + " not have the list of Finch experiments.");
+            return null;
+        }
+    }
+
+    // Save the version number, build id, and the MD5 hash of the variations string to the
+    // process state summary. This way, after an ANR happens, we can retrieve these information
+    // from the process state summary and add them to the ANR report.
+    private static void saveVariationsHashToProcessStateSummary(
+            byte[] variationsHash, String buildIdString) {
+        ActivityManager am =
+                (ActivityManager)
+                        ContextUtils.getApplicationContext()
+                                .getSystemService(Context.ACTIVITY_SERVICE);
+        byte[] summary = getProcessStateSummaryToSave(variationsHash, buildIdString);
+        am.setProcessStateSummary(summary);
+    }
+
+    @VisibleForTesting
+    static byte[] getProcessStateSummaryToSave(byte[] variationsHash, String buildIdString) {
+        byte[] version = VersionInfo.getProductVersion().getBytes(StandardCharsets.UTF_8);
+        byte[] buildId = buildIdString.getBytes(StandardCharsets.UTF_8);
+        int summaryLength = version.length + buildId.length + variationsHash.length + 2;
+        assert summaryLength <= 128 : "The process state summary we want to save is over 128 bytes";
+        byte[] summary = new byte[summaryLength];
+        System.arraycopy(version, 0, summary, 0, version.length);
+        summary[version.length] = PROCESS_STATE_SUMMARY_DELIMITER;
+        System.arraycopy(buildId, 0, summary, version.length + 1, buildId.length);
+        summary[version.length + buildId.length + 1] = PROCESS_STATE_SUMMARY_DELIMITER;
+        System.arraycopy(
+                variationsHash,
+                0,
+                summary,
+                version.length + buildId.length + 2,
+                variationsHash.length);
+        return summary;
+    }
+
+    // Save the raw variations string to the ANR variations file, whose file name is the
+    // hexadecimal representation of the variations hash.
+    @VisibleForTesting
+    static void saveVariationsToFile(
+            byte[] variationsHash, String variationsString, boolean shouldCleanup) {
+        CRASH_DIR.mkdir();
+        ANR_VARIATIONS_DIR.mkdir();
+        String curAnrVariationsFileName = byteArrayToHexString(variationsHash);
+        File curAnrVariationsFile = new File(ANR_VARIATIONS_DIR, curAnrVariationsFileName);
+        try {
+            Files.write(
+                    curAnrVariationsFile.toPath(),
+                    List.of(variationsString),
+                    StandardOpenOption.WRITE,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to write to the ANR variations file: %s", e.toString());
+            return;
+        }
+
+        if (shouldCleanup) {
+            if (sPreviousAnrVariationsFile == null) {
+                // We are adding a new ANR variations file to the ANR variations directory,
+                // so we should make sure that the total number of files does not exceed the cap.
+                removeOldAnrVariationsFiles(curAnrVariationsFile);
+            } else {
+                // We are replacing the previous ANR variations file of the current run with a new
+                // file, so the total number of files will not change.
+                sPreviousAnrVariationsFile.delete();
+            }
+        }
+        sPreviousAnrVariationsFile = curAnrVariationsFile;
+    }
+
+    // To make sure that we are not using too much disk space, we need to make sure that the
+    // total number of ANR variations files is no more than MAX_NUM_OF_ANR_VARIATIONS_FILES.
+    private static void removeOldAnrVariationsFiles(File curAnrVariationsFile) {
+        File[] variationsFiles = ANR_VARIATIONS_DIR.listFiles();
+        if (variationsFiles == null || variationsFiles.length <= MAX_NUM_OF_ANR_VARIATIONS_FILES) {
+            return;
+        }
+        // Sort the ANR variations files by their last modified time. The files at the beginning of
+        // the array have smaller unix time which means they are older.
+        Arrays.sort(variationsFiles, Comparator.comparing(File::lastModified));
+        int remainingFilesCount = variationsFiles.length;
+        for (File variationsFile : variationsFiles) {
+            if (!variationsFile.equals(curAnrVariationsFile)) {
+                variationsFile.delete();
+                remainingFilesCount -= 1;
+                if (remainingFilesCount <= MAX_NUM_OF_ANR_VARIATIONS_FILES) {
+                    break;
+                }
+            }
+        }
+    }
+
     // Pure static class.
     private AnrCollector() {}
-
-    @NativeMethods
-    interface Natives {
-        String getSharedLibraryBuildId();
-    }
 }
--- components/variations/variations_crash_keys.cc
+++ components/variations/variations_crash_keys.cc
@@ -22,8 +22,16 @@
 #include "components/variations/synthetic_trials.h"
 #include "components/variations/variations_switches.h"
 
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
 #include "base/task/thread_pool.h"
+#endif
+
+#if BUILDFLAG(IS_ANDROID)
+#include "base/task/cancelable_task_tracker.h"
+#include "components/variations/variations_crash_keys_android.h"
+#endif
+
+#if BUILDFLAG(IS_CHROMEOS)
 #include "components/variations/variations_crash_keys_chromeos.h"
 #endif
 
@@ -31,9 +39,9 @@ namespace variations {
 
 namespace {
 
-// Size of the "num-experiments" crash key in bytes. 1024*6 bytes should be able
-// to hold about 341 entries, given each entry is 18 bytes long (due to being
-// of the form "8e7abfb0-c16397b7,").
+// Size of the "variations" crash key (kExperimentListKey) in bytes.
+// 1024*6 bytes should be able to hold about 341 entries, given each entry is
+// 18 bytes long (due to being of the form "8e7abfb0-c16397b7,").
 #if BUILDFLAG(LARGE_VARIATION_KEY_SIZE)
 constexpr size_t kVariationsKeySize = 1024 * 8;
 constexpr char kVariationKeySizeHistogram[] =
@@ -104,11 +112,17 @@ class VariationsCrashKeys final : public base::FieldTrialList::Observer {
   // observer calls that happen on a different thread.
   scoped_refptr<base::SequencedTaskRunner> ui_thread_task_runner_;
 
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
   // Task runner corresponding to a background thread, used for tasks that may
   // block.
   scoped_refptr<base::SequencedTaskRunner> background_thread_task_runner_;
-#endif  // IS_CHROMEOS
+#endif  // IS_CHROMEOS || IS_ANDROID
+
+#if BUILDFLAG(IS_ANDROID)
+  // A task tracker that allows us to cancel any tasks that have been posted
+  // but have not started to run.
+  base::CancelableTaskTracker cancelable_task_tracker_;
+#endif  // IS_ANDROID
 
   // A serialized string containing the variations state.
   std::string variations_string_;
@@ -148,10 +162,10 @@ VariationsCrashKeys::VariationsCrashKeys() {
   for (const auto& entry : active_groups) {
     AppendFieldTrial(entry.trial_name, entry.group_name, entry.is_overridden);
   }
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
   background_thread_task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
       {base::TaskPriority::BEST_EFFORT, base::MayBlock()});
-#endif  // IS_CHROMEOS
+#endif  // IS_CHROMEOS || IS_ANDROID
 
   UpdateCrashKeys();
 }
@@ -250,6 +264,11 @@ void VariationsCrashKeys::UpdateCrashKeys() {
     SetVariationsSeedVersionCrashKey(command_line->GetSwitchValueASCII(
         variations::switches::kVariationsSeedVersion));
   }
+
+#if BUILDFLAG(IS_ANDROID)
+  SaveVariationsForAnrReporting(&cancelable_task_tracker_,
+                                background_thread_task_runner_, info);
+#endif  // IS_ANDROID
 
 #if BUILDFLAG(IS_CHROMEOS)
   ReportVariationsToChromeOs(background_thread_task_runner_, info);
