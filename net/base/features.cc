46d9e91bf00f9b3e1cd1941f0d63e00617b678a5
Ciara McMullin <ciaramcmullin@google.com>
2025-05-05 12:02:06 -0700

    Set the PartitionProxyChains feature to be disabled by default
    Bug: 332930085
    Change-Id: Id75ec89296ab3e65a75738161557f9e121a865d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6501022
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Ciara McMullin <ciaramcmullin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1455865}

--- net/base/features.cc
+++ net/base/features.cc
@@ -632,7 +632,7 @@ BASE_FEATURE(kDeviceBoundSessionsRefreshQuota,
 
 BASE_FEATURE(kPartitionProxyChains,
              "PartitionProxyChains",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSpdySessionForProxyAdditionalChecks,
              "SpdySessionForProxyAdditionalChecks",
