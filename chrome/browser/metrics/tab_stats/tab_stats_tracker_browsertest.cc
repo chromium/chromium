de4360444fd5d0dd7ef3a7caa86153d122de7abd
Joe Mason <joenotcharles@google.com>
2025-05-05 12:22:44 -0700

    Reenable TabStatsTrackerBrowserTest on Mac
    https://crrev.com/c/6485366 made TabStatsTrackerBrowserTest less
    fragile. Reenable it on Mac to see if that fixed the flakes.
    
    Bug: 40752198
    Change-Id: I722fe15ffc00d88957fe0b072469e52ec6d6e552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512199
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1455878}

--- chrome/browser/metrics/tab_stats/tab_stats_tracker_browsertest.cc
+++ chrome/browser/metrics/tab_stats/tab_stats_tracker_browsertest.cc
@@ -490,14 +490,7 @@ using MockTabStatsObserver = testing::NiceMock<LenientMockTabStatsObserver>;
 
 }  // namespace
 
-// TODO(crbug.com/40752198): Fix the flakiness on MacOS and re-enable the test.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_TabStatsObserverBasics DISABLED_TabStatsObserverBasics
-#else
-#define MAYBE_TabStatsObserverBasics TabStatsObserverBasics
-#endif
-IN_PROC_BROWSER_TEST_F(TabStatsTrackerBrowserTest,
-                       MAYBE_TabStatsObserverBasics) {
+IN_PROC_BROWSER_TEST_F(TabStatsTrackerBrowserTest, TabStatsObserverBasics) {
   MockTabStatsObserver mock_observer;
   TestTabStatsObserver count_observer;
   tab_stats_tracker_->AddObserverAndSetInitialState(&count_observer);
@@ -624,13 +617,7 @@ IN_PROC_BROWSER_TEST_F(TabStatsTrackerBrowserTest,
   EXPECT_EQ(expected_window_count, count_observer.window_count());
 }
 
-// TODO(crbug.com/40919431): Re-enable this test
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_TabSwitch DISABLED_TabSwitch
-#else
-#define MAYBE_TabSwitch TabSwitch
-#endif
-IN_PROC_BROWSER_TEST_F(TabStatsTrackerBrowserTest, MAYBE_TabSwitch) {
+IN_PROC_BROWSER_TEST_F(TabStatsTrackerBrowserTest, TabSwitch) {
   MockTabStatsObserver mock_observer;
   TestTabStatsObserver count_observer;
   tab_stats_tracker_->AddObserverAndSetInitialState(&count_observer);
