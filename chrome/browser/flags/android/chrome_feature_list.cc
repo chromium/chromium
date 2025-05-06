88ee2c5f15a2facd2d046fa442cd838d22fcc2c7
Shu Yang <shuyng@google.com>
2025-05-06 15:23:55 -0700

    Enable AndroidPdfAssistContent on ToT
    Change-Id: Ibbecf6f3e5e129589261a8d34fa82427ebc83390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514714
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456643}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -557,7 +557,7 @@ BASE_FEATURE(kAndroidOpenPdfInlineBackport,
 
 BASE_FEATURE(kAndroidPdfAssistContent,
              "AndroidPdfAssistContent",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidSurfaceColorUpdate,
              "AndroidSurfaceColorUpdate",
