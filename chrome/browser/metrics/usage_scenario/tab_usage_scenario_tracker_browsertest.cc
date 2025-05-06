8bb41162fab46d704278d4738ac82d60f48dcf86
Joe Mason <joenotcharles@google.com>
2025-05-06 06:26:43 -0700

    Reenable TabUsageScenarioTrackerBrowserTest on Mac
    https://crrev.com/c/6485366 made TabUsageScenarioTrackerBrowserTest less
    fragile. Reenable it on Mac to see if that fixed the flakes.
    
    Bug: 40752198
    Change-Id: I83e8300565807fb21743f2c8ac97345b593fc923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512656
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456263}

--- chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker_browsertest.cc
+++ chrome/browser/metrics/usage_scenario/tab_usage_scenario_tracker_browsertest.cc
@@ -776,14 +776,8 @@ IN_PROC_BROWSER_TEST_F(TabUsageScenarioTrackerBrowserTest, TabAudio) {
             interval_data.source_id_for_longest_visible_origin_duration);
 }
 
-// TODO(crbug.com/40752198): Fix the flakiness on MacOS and re-enable the test.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_FullScreenVideoClosed DISABLED_FullScreenVideoClosed
-#else
-#define MAYBE_FullScreenVideoClosed FullScreenVideoClosed
-#endif
 IN_PROC_BROWSER_TEST_F(TabUsageScenarioTrackerBrowserTest,
-                       MAYBE_FullScreenVideoClosed) {
+                       FullScreenVideoClosed) {
   // Play fullscreen video in a tab and close it while it's playing, ensure that
   // things are tracked properly.
   EXPECT_TRUE(
@@ -837,14 +831,8 @@ IN_PROC_BROWSER_TEST_F(TabUsageScenarioTrackerBrowserTest,
             interval_data.source_id_for_longest_visible_origin_duration);
 }
 
-// TODO(crbug.com/40752198): Fix the flakiness on MacOS and re-enable the test.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_FullScreenVideoCrash DISABLED_FullScreenVideoCrash
-#else
-#define MAYBE_FullScreenVideoCrash FullScreenVideoCrash
-#endif
 IN_PROC_BROWSER_TEST_F(TabUsageScenarioTrackerBrowserTest,
-                       MAYBE_FullScreenVideoCrash) {
+                       FullScreenVideoCrash) {
   // Play fullscreen video in a tab and make the tab crash, ensure that things
   // are tracked properly.
   auto* contents = tab_strip().GetWebContentsAt(0);
