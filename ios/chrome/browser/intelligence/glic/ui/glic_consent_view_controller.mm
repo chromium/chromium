ac023e82cedd99684a92db811d25334e0923492c
“Prasanaa” <prasanaa@google.com>
2025-05-06 15:08:58 -0700

    [iOS] Partially implement UI for GLIC Consent Flow
    This CL sets up the main stack for the GLIC Consent Flow.
    
    Bug: 414777915, 414791668
    Change-Id: I4b848f0f64c21e34b3fc5e2d40dc014fd4760926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6512514
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1456637}

--- ios/chrome/browser/intelligence/glic/ui/glic_consent_view_controller.mm
+++ ios/chrome/browser/intelligence/glic/ui/glic_consent_view_controller.mm
@@ -5,43 +5,108 @@
 #import "ios/chrome/browser/intelligence/glic/ui/glic_consent_view_controller.h"
 
 #import "ios/chrome/browser/intelligence/glic/ui/glic_consent_mutator.h"
+#import "ios/chrome/browser/intelligence/glic/ui/glic_constants.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/promo_style/promo_style_view_controller_delegate.h"
 
 @interface GLICConsentViewController () <PromoStyleViewControllerDelegate>
-
 @end
 
-@implementation GLICConsentViewController
+@implementation GLICConsentViewController {
+  UIStackView* _mainStackView;
+}
 
 #pragma mark - UIViewController
 
 // TODO(crbug.com/414777915): Implement a basic UI.
 - (void)viewDidLoad {
   self.delegate = self;
+  [self configureSheetPresentation];
+  [self configurePromoStyleProperties];
+  [super viewDidLoad];
+  // The stackview should be added after `viewDidLoad`, so that the
+  // `UIScrollView` is not placed on top of the stackview.
+  [self setupStackView];
+}
+
+#pragma mark - Private
+
+// Configure all the stacks.
+- (void)setupStackView {
+  [self configureMainStackView];
+}
+
+// Configure the differents detents for the expanded and collapsed view.
+- (void)configureSheetPresentation {
+  self.modalPresentationStyle = UIModalPresentationPageSheet;
+
+  self.sheetPresentationController.detents = @[
+    [self customHeightDetentWithIdentifier:kGLICConsentPartialDetentIdentifier
+                                    height:kGLICConsentPartialDetentHeight],
+    [self customHeightDetentWithIdentifier:kGLICConsentFullDetentIdentifier
+                                    height:kGLICConsentFullDetentHeight]
+  ];
+
+  self.sheetPresentationController.selectedDetentIdentifier =
+      kGLICConsentPartialDetentIdentifier;
+
+  self.sheetPresentationController.preferredCornerRadius =
+      kGLICConsentPreferredCornerRadius;
+  self.sheetPresentationController.prefersScrollingExpandsWhenScrolledToEdge =
+      NO;
+}
+
+// Create a custom sheet presentation controller detent with a fixed height.
+- (UISheetPresentationControllerDetent*)
+    customHeightDetentWithIdentifier:(NSString*)identifier
+                              height:(CGFloat)height {
+  auto resolver = ^CGFloat(
+      id<UISheetPresentationControllerDetentResolutionContext> context) {
+    return height;
+  };
+
+  return
+      [UISheetPresentationControllerDetent customDetentWithIdentifier:identifier
+                                                             resolver:resolver];
+}
+
+// Configure promo style properties to add buttons. Ignores header image type.
+- (void)configurePromoStyleProperties {
   self.layoutBehindNavigationBar = YES;
   self.shouldHideBanner = YES;
   self.headerImageType = PromoStyleImageType::kNone;
 
-  self.modalPresentationStyle = UIModalPresentationPageSheet;
+  self.primaryActionString = kGLICConsentPrimaryAction;
+  self.secondaryActionString = kGLICConsentSecondaryAction;
+}
 
-  // TODO(crbug.com/414777890): Use a custom detent.
-  self.sheetPresentationController.detents = @[
-    [UISheetPresentationControllerDetent mediumDetent],
-    [UISheetPresentationControllerDetent largeDetent]
-  ];
+// Configure the main stack view.
+- (void)configureMainStackView {
+  _mainStackView = [[UIStackView alloc] init];
+  _mainStackView.axis = UILayoutConstraintAxisVertical;
+  _mainStackView.distribution = UIStackViewDistributionFill;
+  _mainStackView.alignment = UIStackViewAlignmentFill;
+  _mainStackView.spacing = kGLICConsentMainStackSpacing;
 
-  self.sheetPresentationController.preferredCornerRadius = 16.0;
+  _mainStackView.translatesAutoresizingMaskIntoConstraints = NO;
 
-  UIView* rootView = [[UIView alloc] initWithFrame:UIScreen.mainScreen.bounds];
-  rootView.backgroundColor = [UIColor systemBackgroundColor];
-  [self.view addSubview:rootView];
+  [self.view addSubview:_mainStackView];
 
-  // TODO(crbug.com/414778685): Add strings.
-  self.primaryActionString = @"Yes, I'm in";
-  self.secondaryActionString = @"No thanks";
-
-  self.bannerSize = BannerImageSizeType::kStandard;
-  [super viewDidLoad];
+  [NSLayoutConstraint activateConstraints:@[
+    [_mainStackView.leadingAnchor
+        constraintEqualToAnchor:self.view.leadingAnchor
+                       constant:kGLICConsentMainStackHorizontalInset],
+    [_mainStackView.trailingAnchor
+        constraintEqualToAnchor:self.view.trailingAnchor
+                       constant:-kGLICConsentMainStackHorizontalInset],
+    [_mainStackView.topAnchor
+        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor
+                       constant:kGLICConsentMainStackTopInset],
+    [_mainStackView.bottomAnchor
+        constraintLessThanOrEqualToAnchor:self.view.safeAreaLayoutGuide
+                                              .bottomAnchor]
+  ]];
 }
 
 #pragma mark - PromoStyleViewControllerDelegate
---
+++ ios/chrome/browser/intelligence/glic/ui/glic_constants.mm
@@ -0,0 +1,24 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/intelligence/glic/ui/glic_constants.h"
+
+// Sheet detents.
+NSString* const kGLICConsentPartialDetentIdentifier =
+    @"GLICConsentPartialDetentIdentifier";
+NSString* const kGLICConsentFullDetentIdentifier =
+    @"GLICConsentFullDetentIdentifier";
+const CGFloat kGLICConsentPartialDetentHeight = 300.0;
+const CGFloat kGLICConsentFullDetentHeight = 500.0;
+const CGFloat kGLICConsentPreferredCornerRadius = 10.0;
+
+// Stack view insets and spacing.
+const CGFloat kGLICConsentMainStackHorizontalInset = 20.0;
+const CGFloat kGLICConsentMainStackTopInset = 24.0;
+const CGFloat kGLICConsentMainStackSpacing = 16.0;
+
+// Promo style strings.
+// TODO(crbug.com/414778685): Add strings.
+NSString* const kGLICConsentPrimaryAction = @"Yes, I'm in";
+NSString* const kGLICConsentSecondaryAction = @"No thanks";
