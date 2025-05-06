ab10eec6920d2d25fa057f8bab561d76ac50a958
Christian Xu <christianxu@google.com>
2025-05-06 08:36:56 -0700

    [iOS][Omnibox] Fix internal build
    Fix missing file in internal build.
    
    Bug: None
    Change-Id: Id50e04f002757180f10ec42a694a9e73b43c24c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6511967
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456368}

---
+++ ios/chrome/browser/omnibox/ui_bundled/omnibox_icon_type.h
@@ -0,0 +1,10 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_OMNIBOX_ICON_TYPE_H_
+#define IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_OMNIBOX_ICON_TYPE_H_
+
+#import "ios/chrome/browser/omnibox/ui/omnibox_icon_type.h"
+
+#endif  // IOS_CHROME_BROWSER_OMNIBOX_UI_BUNDLED_OMNIBOX_ICON_TYPE_H_
