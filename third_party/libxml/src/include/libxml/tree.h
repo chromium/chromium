6b908d3535684161be3bee7eee6ccce3e33d19ae
Tom Sepez <tsepez@chromium.org>
2025-05-06 11:28:22 -0700

    Expand libxml structs to better integrate with libxslt.
    Introduce `extra` member in libxml's _xmlAttr and _xmlDoc structs. This
    is a precursor to a CL reworking libxslt's re-purposing of bits from
    other fields in these structs, which is a layering violation. This is
    similar to what is already present for _xmlNode.
    
    Change-Id: I1d7fb907b7385609ed51359c1606efe3878f6f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6507145
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1456486}

--- third_party/libxml/src/include/libxml/tree.h
+++ third_party/libxml/src/include/libxml/tree.h
@@ -454,6 +454,7 @@ struct _xmlAttr {
     xmlAttributeType atype;     /* the attribute type if validating */
     void            *psvi;	/* for type/PSVI information */
     struct _xmlID   *id;        /* the ID struct */
+    unsigned int     extra;     /* extra data for XPath/XSLT */
 };
 
 /**
@@ -592,6 +593,7 @@ struct _xmlDoc {
 				   document */
     int             properties;	/* set of xmlDocProperties for this document
 				   set at the end of parsing */
+    unsigned int    extra;      /* extra data for XPath/XSLT */
 };
 
 
