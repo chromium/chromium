cb3fdba61aaf45db2f9c6667553cb33d6ea4aee2
Stefan Zager <szager@chromium.org>
2025-05-07 15:54:01 -0700

    Initialize input router for opened window only once
    The input router is initialized first via WebContentsImpl::CreateViewForWidget, but is later reset and reinitialized by the call to GetInitialVisualProperties(), leaving the browser- and renderer-side input routers disconnected.
    
    Bug: chromium:414753769
    Change-Id: Idd5e91e639a7156eea2a89fbed808e0a2cf69c5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518845
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1457307}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -9975,7 +9975,7 @@ void RenderFrameHostImpl::CreateNewWindow(
 
     if (!shown_contents) {
       // These point to freed memory, so null them out to prevent inadvertent
-      // UAF in the feature (see NOTE above).
+      // UAF in the future (see NOTE above).
       new_frame_tree = nullptr;
       new_main_rfh = nullptr;
       new_rwh = nullptr;
@@ -9985,7 +9985,7 @@ void RenderFrameHostImpl::CreateNewWindow(
           new_main_rfh->GetView()->GetViewBounds());
       reply->window_screen_rect.emplace(
           new_main_rfh->GetView()->GetBoundsInRootWindow());
-      reply->visual_properties = new_rwh->GetInitialVisualProperties();
+      reply->visual_properties = new_rwh->GetVisualProperties();
     }
   }
 
