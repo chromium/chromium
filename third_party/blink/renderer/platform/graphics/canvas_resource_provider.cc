cee9c7545752b518b0ba7341ec08515a16017675
Mingjing Zhang <mjzhang@chromium.org>
2025-05-08 11:56:23 -0700

    Revert "Reland "[blink] Generate SyncToken in advance in RasterRecord()""
    This reverts commit 61fe47d005ae89bd4ad564a575cf9d40bfbde177.
    
    Reason for revert: This reland causes flaky failures in
    fbo_4_samples/fbo_max_samples tests. See https://issues.chromium.org/issues/408364831
    
    Bug: 40286368
    Original change's description:
    > Reland "[blink] Generate SyncToken in advance in RasterRecord()"
    >
    > This is a reland of commit 329f4d03ecacea56d2b09d04980e2c041c761bc3
    >
    > The reverted commit did not contain issues by itself, but rather
    > exposed an existing issue where the sample coverage values may not
    > be properly set on Adreno drivers. The underlying issue was addressed
    > by https://crrev.com/c/6447878.
    >
    > Original change's description:
    > > [blink] Generate SyncToken in advance in RasterRecord()
    > >
    > > Currently in RasterRecord(), WillDrawInternal() sets a boolean that,
    > > if true, will lead to the generation of a SyncToken at a later time.
    > > However, in order to apply scoped SyncToken access for raster, we need
    > > to ensure that the SyncToken's generation is localized inside
    > > RasterRecord().
    > >
    > > This CL is a step towards that purpose. We now generate the SyncToken
    > > right after the RasterRecordOOP() operation. This CL is not meant to
    > > cause any behavioral change.
    > >
    > > Bug: 40286368
    > > Change-Id: I8b8a0a121b5d2caef8722e4939669770775c86f2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6426236
    > > Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    > > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1442304}
    >
    > Bug: 40286368
    > Change-Id: I16eaadddf0350a9f8e7d76807f38425eb0cf01a1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6517154
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1456982}
    
    Bug: 40286368, 408364831
    Change-Id: I421b4b4dc7aa21bfce07297b81b47cc985daabbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6525746
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1457750}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -677,7 +677,6 @@ class CanvasResourceProviderSharedImage : public CanvasResourceProvider,
     is_cleared_ = true;
     RasterRecordOOP(std::move(last_recording), needs_clear,
                     resource()->GetClientSharedImage()->mailbox());
-    resource()->GetSyncToken();
   }
 
   bool ShouldReplaceTargetBuffer(
