79c1f80c3b9afb9134d0bf3d30b98ba5dc8aad91
Fredrik Söderquist <fs@opera.com>
2025-05-08 01:28:15 -0700

    Revert "Cache the fragment identifier in StyleFetchedImage"
    This reverts commit 6fc253ea3c89fe3fad0f7710f55fe102becd403e.
    
    Reason for revert: Didn't have sufficient effect
    
    Bug: 416059243
    Original change's description:
    > Cache the fragment identifier in StyleFetchedImage
    >
    > Getting the fragment identifier involves essentially (re)resolving the
    > URL again. This is costly, and is usually done at least twice per draw
    > of a StyleFetchedImage. Cache the result of the determine the fragment
    > to avoid this cost. A potential improvement would be to cache the
    > derived SVGImageViewInfo instead, since that's what the fragment is
    > ultimately used for, but that didn't show a progression when testing.
    >
    > Bug: 416059243
    > Change-Id: Ib2a6af01abf11d9d967a49ec6f31b01baf7ce4b9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6518163
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Commit-Queue: Fredrik Söderquist <fs@opera.com>
    > Cr-Commit-Position: refs/heads/main@{#1456902}
    
    Bug: 416059243
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I33b5a64581e7552dd7aac1d6e66c630f38b5317b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6522407
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1457458}

--- third_party/blink/renderer/core/style/style_fetched_image.h
+++ third_party/blink/renderer/core/style/style_fetched_image.h
@@ -104,9 +104,8 @@ class CORE_EXPORT StyleFetchedImage final : public StyleImage,
   bool GetImageAnimationPolicy(mojom::blink::ImageAnimationPolicy&) override;
   bool CanBeSpeculativelyDecoded() const override;
 
-  const String& FragmentIdentifier() const;
+  String FragmentIdentifier() const;
 
-  mutable String cached_fragment_identifier_;
   Member<ImageResourceContent> image_;
   Member<const CSSUrlData> url_data_;
   Member<const Document> document_;
