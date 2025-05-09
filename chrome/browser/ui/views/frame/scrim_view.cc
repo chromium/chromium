06911369fcb1a6ef5e24a7fd6381f29544b99c48
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-05-09 10:08:04 -0700

    [Merge 137] Revert "Use SolidColor layer to implement the scrim for Chrome browser"
    Using SolidColorLayer for scrim caused unexpected blending results on
    Mac. (b:415474097)
    
    Additionally on ChromeOS, the Scrim  unexpectedly became visible for
    browser windows in overview mode. (b:412948232)
    
    (cherry picked from commit e19dbe531998bb7a1b0e44a09ce50da573e2eff7)
    
    Bug: b:391903216, b:415474097, b:412948232
    Change-Id: I13451370c1237eb4ab07ded019a83b1c342da1bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6525489
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1457804}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527400
    Cr-Commit-Position: refs/branch-heads/7151@{#659}
    Cr-Branched-From: 8e0d32ed6e49a2415b16e5ed402957cac2349ce2-refs/heads/main@{#1453031}

--- chrome/browser/ui/views/frame/scrim_view.cc
+++ chrome/browser/ui/views/frame/scrim_view.cc
@@ -10,23 +10,22 @@
 #include "ui/compositor/layer_type.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/views/accessibility/view_accessibility.h"
+#include "ui/views/background.h"
 
 ScrimView::ScrimView() {
   SetCanProcessEventsWithinSubtree(false);
   // This view must be painted to a layer so that it can be drawn on top of the
   // contents view. Otherwise, the scrim is drawn on the BrowserView's layer,
   // which always stays behind the content's layer.
-  SetPaintToLayer(ui::LAYER_SOLID_COLOR);
+  SetPaintToLayer();
+  layer()->SetFillsBoundsOpaquely(false);
   GetViewAccessibility().SetIsInvisible(true);
-  SetVisible(false);
-}
-
-void ScrimView::AddedToWidget() {
   // Maybe consider using a different color for the scrim?
   // kColorSysStateScrim is a semi-transparent black which has no effect on a
   // pure black background. In contrast, macOS sheet uses a semi-transparent
   // grey scrim which lightens a dark background.
-  layer()->SetColor(GetColorProvider()->GetColor(ui::kColorSysStateScrim));
+  SetBackground(views::CreateSolidBackground(ui::kColorSysStateScrim));
+  SetVisible(false);
 }
 
 void ScrimView::SetRoundedCorners(const gfx::RoundedCornersF& radii) {
