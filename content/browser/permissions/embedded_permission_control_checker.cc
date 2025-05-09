a388d810627b6d59a228a50e0a58917e587f15cd
Ravjit <ravjit@chromium.org>
2025-05-09 05:19:33 -0700

    Show a console warning, if the PEPC instances per page limit is reached
    Currently only 3 permission elements are allowed per page. If this quota
    is reached, show a console nessage to the devs.
    
    Bug: 346956799
    Change-Id: If55a8dd09ea65667197b838e7a0992d7f007692b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6524746
    Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458071}

--- content/browser/permissions/embedded_permission_control_checker.cc
+++ content/browser/permissions/embedded_permission_control_checker.cc
@@ -4,6 +4,9 @@
 
 #include "content/browser/permissions/embedded_permission_control_checker.h"
 
+#include <string>
+
+#include "content/browser/log_console_message.h"
 #include "third_party/blink/public/common/features_generated.h"
 
 using blink::mojom::EmbeddedPermissionControlClient;
@@ -39,8 +42,14 @@ void EmbeddedPermissionControlChecker::CheckPageEmbeddedPermission(
           blink::features::kBypassPepcSecurityForTesting)) {
     client->OnEmbeddedPermissionControlRegistered(/*allow=*/true);
   }
-
   queue.push_back(std::move(client));
+  if (queue.size() == kMaxPEPCPerPage) {
+    page().GetMainDocument().AddMessageToConsole(
+        blink::mojom::ConsoleMessageLevel::kWarning,
+        "Maximum limit of " + base::NumberToString(kMaxPEPCPerPage) +
+            " permission elements has been reached. More permission"
+            " elements can be added but they will not be clickable");
+  }
 }
 
 PAGE_USER_DATA_KEY_IMPL(EmbeddedPermissionControlChecker);
