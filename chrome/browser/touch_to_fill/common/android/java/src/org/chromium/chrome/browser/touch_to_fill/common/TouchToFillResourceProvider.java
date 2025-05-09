f765f4a5f2652987bea0b7a94ca84597cc912fda
Timofey Chudakov <tchudakov@google.com>
2025-05-09 05:33:02 -0700

    [Wallet] Move TouchToFillResourceProvider [3/X].
    This CL move the TouchToFillResourceProvider from
    c/b/ttf/password_manager to the c/b/ttf/common directory to make the
    class more general purpose. It will we used from the payments TTF
    bottom sheet. The old version is kept for compatibility reasons
    and will be removed once the counterpart if removed from the internal
    repo.
    
    Bug: 415006335
    Change-Id: I3989421bf5540cb1f67df0e82601f3b17af155f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6530603
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458073}

--- chrome/browser/touch_to_fill/common/android/java/src/org/chromium/chrome/browser/touch_to_fill/common/TouchToFillResourceProvider.java
+++ chrome/browser/touch_to_fill/common/android/java/src/org/chromium/chrome/browser/touch_to_fill/common/TouchToFillResourceProvider.java
@@ -2,19 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.touch_to_fill;
+package org.chromium.chrome.browser.touch_to_fill.common;
 
 import androidx.annotation.DrawableRes;
+
+import org.chromium.build.annotations.NullMarked;
 
 /**
  * Provides functions that choose the correct resource id for touch-to-fill UI. Needed to
  * differentiate upstream and downstream resources. This exists to ensure all implementations of
- * TouchToFillResourceProviderImpl provide the same set of methods. Please use the
- * org.chromium.chrome.browser.touch_to_fill.common.TouchToFillResourceProvider instead.
+ * TouchToFillResourceProviderImpl provide the same set of methods.
  *
  * <p>TODO(wnwen): Remove this once downstream no longer depends on it.
  */
-@Deprecated
+@NullMarked
 public interface TouchToFillResourceProvider {
     /**
      * Returns the drawable id to be displayed as a bottom sheet header image.
