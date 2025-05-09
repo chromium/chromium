08e58d03279b006c37d765eae747474c78375a83
Aman Verma <amanvr@google.com>
2025-05-09 07:33:52 -0700

    [InputVizard] Fix a UAF bug in InputManager due to null RenderInputRouter in rir_map_.
    InputManager::GetRenderInputRouterFromFrameSinkId previously used
    operator[] on its RenderInputRouter map (rir_map_). This could insert a
    nullptr in the map if the FrameSinkId was not found which would result
    in a null RenderInputRouter pointer being stored and potentially used
    later. Subsequent operations, such as InputManager::SetBeginFrameSource,
    could then retrieve this null RenderInputRouter and attempt to call
    RenderInputRouter::SetBeginFrameSourceForFlingScheduler on the null
    instance. This would lead to a crash when the method tried to access
    member variables (e.g., fling_scheduler_).
    
    This change also adds CHECKs to ensure that RenderInputRouter is non
    null before calling SetBeginFrameSourceForFlingScheduler and
    fling_scheduler is non null when calling SetBeginFrameSource.
    
    Bug: 413442819
    Change-Id: I136f4d40943292e8492575e6b55a572447082697
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6530325
    Reviewed-by: Kartar Singh <kartarsingh@google.com>
    Commit-Queue: Aman Verma <amanvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1458102}

--- components/input/render_input_router.cc
+++ components/input/render_input_router.cc
@@ -682,13 +682,7 @@ void RenderInputRouter::SetView(RenderWidgetHostViewInput* view) {
 
 void RenderInputRouter::SetBeginFrameSourceForFlingScheduler(
     viz::BeginFrameSource* begin_frame_source) {
-  if (!fling_scheduler_) {
-    // In some cases, the fling_scheduler_ might not have been set yet, we
-    // should only attempt to set `nullptr` in such cases. See
-    // crbug.com/411461030.
-    CHECK(!begin_frame_source);
-    return;
-  }
+  CHECK(fling_scheduler_);
   fling_scheduler_->SetBeginFrameSource(begin_frame_source);
 }
 
