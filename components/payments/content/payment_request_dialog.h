8c7b01a08f4bcf9d8c9ed9aeb3db5db4dc43895f
David Sanders <dsanders11@ucsbalum.com>
2025-05-09 11:52:24 -0700

    Do IWYU in payment_request_dialog.h
    Bug: 40318405
    Change-Id: Ibdcc52781276ed01b993099ed95cfaf407523648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6529944
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458255}

--- components/payments/content/payment_request_dialog.h
+++ components/payments/content/payment_request_dialog.h
@@ -5,8 +5,9 @@
 #ifndef COMPONENTS_PAYMENTS_CONTENT_PAYMENT_REQUEST_DIALOG_H_
 #define COMPONENTS_PAYMENTS_CONTENT_PAYMENT_REQUEST_DIALOG_H_
 
-#include "components/autofill/core/browser/payments/full_card_request.h"
 #include "components/payments/content/payment_request_display_manager.h"
+
+class GURL;
 
 namespace payments {
 
