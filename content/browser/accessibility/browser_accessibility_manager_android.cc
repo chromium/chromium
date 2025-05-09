0a435536aed633cbccfff3e7bc7f25f87eb95f29
David Tseng <dtseng@google.com>
2025-05-09 12:18:39 -0700

    android-a11y: invalidate cached node on children changed
    Android a11y keeps a java side cache of nodes.
    
    This cache is currently cleared in
    BrowserAccessibility::OnDataChanged
    which gets called from
    BrowserAccessibilityManager::OnAtomicUpdateFinished
    for each
    Change
    for any
    ChangeType.
    
    The issue is that each
    Change
    references a node in the
    AXTree
    which isn't necessarily unignored.
    
    In the platform tree aka
    BrowserAccessibility
    we only surface unignored nodes.
    
    AXEventGenerateor does this unignored ancestor re-targeting when it
    computes a CHILDREN_CHANGE event.
    
    R=aleventhal@chromium.org
    
    Test: see bug. Removing java side cache also fixes and first confirmed issue.
    Bug: 353777615
    Change-Id: Iafc2db06bfde4316276040759dbae84b1a62e41a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6533277
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458271}

--- content/browser/accessibility/browser_accessibility_manager_android.cc
+++ content/browser/accessibility/browser_accessibility_manager_android.cc
@@ -387,6 +387,10 @@ void BrowserAccessibilityManagerAndroid::FireGeneratedEvent(
       }
       break;
 
+    case ui::AXEventGenerator::Event::CHILDREN_CHANGED:
+      wcax->ClearNodeInfoCacheForGivenId(android_node->GetUniqueId());
+      break;
+
     // Currently unused events on this platform.
     case ui::AXEventGenerator::Event::NONE:
     case ui::AXEventGenerator::Event::ACCESS_KEY_CHANGED:
@@ -399,7 +403,6 @@ void BrowserAccessibilityManagerAndroid::FireGeneratedEvent(
     case ui::AXEventGenerator::Event::BUSY_CHANGED:
     case ui::AXEventGenerator::Event::CARET_BOUNDS_CHANGED:
     case ui::AXEventGenerator::Event::CHECKED_STATE_DESCRIPTION_CHANGED:
-    case ui::AXEventGenerator::Event::CHILDREN_CHANGED:
     case ui::AXEventGenerator::Event::CONTROLS_CHANGED:
     case ui::AXEventGenerator::Event::DETAILS_CHANGED:
     case ui::AXEventGenerator::Event::DESCRIBED_BY_CHANGED:
