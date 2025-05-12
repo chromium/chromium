181321c1dd8ae5da93ff8c5130b63317284f00bb
Morten Stenshorne <mstensho@chromium.org>
2025-05-12 09:37:49 -0700

    Unit tests for layout tree structure and multicol.
    Check that all descendants of a multicol container get the
    IsInsideMulticol() flag set, and that it's reset when no longer inside a
    multicol container. The implementation depends on this flag being set
    for multicol to work. Resetting it when an ancestor multicol container
    ceases to be one isn't strictly required for correctness, but it seems
    cleaner (avoiding false positives when we can).
    
    Secondly, for implementation reasons, a multicol container with inline
    children needs to establish an anonymous wrapper block around such
    children. Check that this happens, and also check that this anonymous
    block is removed if a multicol container ceases to be one. The latter
    shouldn't be web-observable, but leaving anonymous blocks around when we
    don't need them is inconsistent.
    
    Bug: 371802475
    Change-Id: I8e7332a35e6ab8d7772af8e44332bc700ff98bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534020
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458902}

--- third_party/blink/renderer/core/layout/layout_block_flow_test.cc
+++ third_party/blink/renderer/core/layout/layout_block_flow_test.cc
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "third_party/blink/renderer/core/layout/layout_multi_column_flow_thread.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 
 namespace blink {
+namespace {
 
 class LayoutBlockFlowTest : public RenderingTest {};
 
@@ -32,4 +34,166 @@ var { column-count: 17179869184; }
   // The test passes if no DCHECK failure in ink_overflow.cc.
 }
 
+TEST_F(LayoutBlockFlowTest, IsInsideMulticol) {
+  SetBodyInnerHTML(R"HTML(
+<div id="outer">
+  <div id="inner" style="columns:3;">
+    <div id="container">
+      <div id="child"></div>
+    </div>
+  </div>
+</div>
+)HTML");
+
+  Element* outer_elm = GetElementById("outer");
+  ASSERT_TRUE(outer_elm);
+  Element* inner_elm = GetElementById("inner");
+  ASSERT_TRUE(inner_elm);
+
+  const LayoutObject* outer = GetLayoutObjectByElementId("outer");
+  ASSERT_TRUE(outer);
+  const LayoutObject* inner = GetLayoutObjectByElementId("inner");
+  ASSERT_TRUE(inner);
+  const LayoutObject* container = GetLayoutObjectByElementId("container");
+  ASSERT_TRUE(container);
+  const LayoutObject* child = GetLayoutObjectByElementId("child");
+  ASSERT_TRUE(child);
+
+  EXPECT_FALSE(outer->IsInsideMulticol());
+  EXPECT_FALSE(inner->IsInsideMulticol());
+  EXPECT_TRUE(container->IsInsideMulticol());
+  EXPECT_TRUE(child->IsInsideMulticol());
+
+  // Turn off multicol for #inner
+  inner_elm->style()->setProperty(GetDocument().GetExecutionContext(),
+                                  "columns", "auto", "", ASSERT_NO_EXCEPTION);
+  UpdateAllLifecyclePhasesForTest();
+
+  EXPECT_FALSE(outer->IsInsideMulticol());
+  EXPECT_FALSE(inner->IsInsideMulticol());
+  EXPECT_FALSE(container->IsInsideMulticol());
+  EXPECT_FALSE(child->IsInsideMulticol());
+
+  // Turn multicol for #inner back on
+  inner_elm->style()->setProperty(GetDocument().GetExecutionContext(),
+                                  "columns", "3", "", ASSERT_NO_EXCEPTION);
+  UpdateAllLifecyclePhasesForTest();
+
+  EXPECT_FALSE(outer->IsInsideMulticol());
+  EXPECT_FALSE(inner->IsInsideMulticol());
+  EXPECT_TRUE(container->IsInsideMulticol());
+  EXPECT_TRUE(child->IsInsideMulticol());
+
+  // Turn on multicol for #outer
+  outer_elm->style()->setProperty(GetDocument().GetExecutionContext(),
+                                  "columns", "3", "", ASSERT_NO_EXCEPTION);
+  UpdateAllLifecyclePhasesForTest();
+
+  EXPECT_FALSE(outer->IsInsideMulticol());
+  EXPECT_TRUE(inner->IsInsideMulticol());
+  EXPECT_TRUE(container->IsInsideMulticol());
+  EXPECT_TRUE(child->IsInsideMulticol());
+
+  // Turn off multicol for #inner
+  inner_elm->style()->setProperty(GetDocument().GetExecutionContext(),
+                                  "columns", "auto", "", ASSERT_NO_EXCEPTION);
+  UpdateAllLifecyclePhasesForTest();
+
+  EXPECT_FALSE(outer->IsInsideMulticol());
+  EXPECT_TRUE(inner->IsInsideMulticol());
+  EXPECT_TRUE(container->IsInsideMulticol());
+  EXPECT_TRUE(child->IsInsideMulticol());
+}
+
+// TODO(crbug.com/371802475): Remove this when no longer need.
+const LayoutBlockFlow* MulticolContentContainer(
+    const LayoutBlockFlow* multicol_container) {
+  if (LayoutBlockFlow* flow_thread =
+          multicol_container->MultiColumnFlowThread()) {
+    return flow_thread;
+  }
+  return multicol_container;
+}
+
+TEST_F(LayoutBlockFlowTest, AnonymousContainer) {
+  SetBodyInnerHTML(R"HTML(
+  <div id="multicol" style="columns:3;"><span id="child1"></span><span id="child2"></span></div>
+)HTML");
+
+  Element* multicol_elm = GetElementById("multicol");
+  ASSERT_TRUE(multicol_elm);
+  const auto* multicol =
+      DynamicTo<LayoutBlockFlow>(GetLayoutObjectByElementId("multicol"));
+  ASSERT_TRUE(multicol);
+  const LayoutObject* child1 = GetLayoutObjectByElementId("child1");
+  ASSERT_TRUE(child1);
+  const LayoutObject* child2 = GetLayoutObjectByElementId("child2");
+  ASSERT_TRUE(child2);
+
+  // The children are inline. Need an anonymous wrapper.
+  const auto* anonymous_wrapper = DynamicTo<LayoutBlockFlow>(
+      MulticolContentContainer(multicol)->FirstChild());
+  ASSERT_TRUE(anonymous_wrapper);
+  EXPECT_TRUE(anonymous_wrapper->IsAnonymousBlockFlow());
+  EXPECT_EQ(anonymous_wrapper->NextSibling(), nullptr);
+  ASSERT_TRUE(anonymous_wrapper->FirstChild());
+  ASSERT_TRUE(anonymous_wrapper->FirstChild()->NextSibling());
+  EXPECT_EQ(anonymous_wrapper->FirstChild(), child1);
+  EXPECT_EQ(anonymous_wrapper->FirstChild()->NextSibling(), child2);
+  EXPECT_EQ(anonymous_wrapper->FirstChild()->NextSibling()->NextSibling(),
+            nullptr);
+
+  // Turn off multicol.
+  multicol_elm->style()->setProperty(GetDocument().GetExecutionContext(),
+                                     "columns", "auto", "",
+                                     ASSERT_NO_EXCEPTION);
+  UpdateAllLifecyclePhasesForTest();
+
+  EXPECT_EQ(multicol->FirstChild(), child1);
+  ASSERT_TRUE(multicol->FirstChild()->NextSibling());
+  EXPECT_EQ(multicol->FirstChild()->NextSibling(), child2);
+  EXPECT_EQ(multicol->FirstChild()->NextSibling()->NextSibling(), nullptr);
+
+  // Turn multicol back on.
+  multicol_elm->style()->setProperty(GetDocument().GetExecutionContext(),
+                                     "columns", "3", "", ASSERT_NO_EXCEPTION);
+  UpdateAllLifecyclePhasesForTest();
+
+  anonymous_wrapper = DynamicTo<LayoutBlockFlow>(
+      MulticolContentContainer(multicol)->FirstChild());
+  ASSERT_TRUE(anonymous_wrapper);
+  EXPECT_TRUE(anonymous_wrapper->IsAnonymousBlockFlow());
+  EXPECT_EQ(anonymous_wrapper->NextSibling(), nullptr);
+  EXPECT_EQ(anonymous_wrapper->FirstChild(), child1);
+  ASSERT_TRUE(anonymous_wrapper->FirstChild());
+  ASSERT_TRUE(anonymous_wrapper->FirstChild()->NextSibling());
+  EXPECT_EQ(anonymous_wrapper->FirstChild()->NextSibling(), child2);
+  EXPECT_EQ(anonymous_wrapper->FirstChild()->NextSibling()->NextSibling(),
+            nullptr);
+}
+
+TEST_F(LayoutBlockFlowTest, NoAnonymousContainer) {
+  SetBodyInnerHTML(R"HTML(
+  <div id="multicol" style="columns:3;">
+    <div id="child1"></div>
+    <div id="child2"></div>
+  </div>
+)HTML");
+
+  const auto* multicol =
+      DynamicTo<LayoutBlockFlow>(GetLayoutObjectByElementId("multicol"));
+  ASSERT_TRUE(multicol);
+  multicol = MulticolContentContainer(multicol);
+  const LayoutObject* child1 = GetLayoutObjectByElementId("child1");
+  ASSERT_TRUE(child1);
+  const LayoutObject* child2 = GetLayoutObjectByElementId("child2");
+  ASSERT_TRUE(child2);
+
+  // The children are blocks. No need for an anonymous wrapper.
+  EXPECT_EQ(multicol->FirstChild(), child1);
+  EXPECT_EQ(multicol->FirstChild()->NextSibling(), child2);
+  EXPECT_EQ(multicol->FirstChild()->NextSibling()->NextSibling(), nullptr);
+}
+
+}  // anonymous namespace
 }  // namespace blink
