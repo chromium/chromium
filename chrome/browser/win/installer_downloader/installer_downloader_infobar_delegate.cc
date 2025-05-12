6d1f9b632456f92c4fb01581f2f9a659ef024f94
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-05-12 05:24:23 -0700

    [Installer Downloader] Move infobar delegate near to the core logic
    The installer downloader controller is responsible of showing the
    infobar. This CL move the infobar delegate near to the controller.
    
    Change-Id: I16c7e76c68802da64fef014ec546bac0773be9d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6531583
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1458772}

--- chrome/browser/win/installer_downloader/installer_downloader_infobar_delegate.cc
+++ chrome/browser/win/installer_downloader/installer_downloader_infobar_delegate.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/startup/installer_downloader/installer_downloader_infobar_delegate.h"
+#include "chrome/browser/win/installer_downloader/installer_downloader_infobar_delegate.h"
 
 #include <memory>
 #include <utility>
