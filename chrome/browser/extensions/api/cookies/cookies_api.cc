be5573c258db77af53589a4f62eca0e129fbfae8
Mitsuru Oshima <oshima@chromium.org>
2025-05-12 10:34:02 -0700

    [m137] Observer PrimaryOTR only in CookieEventRouter
    CookieEventRouter can support only one OTR profile whereas
    there can be multiple OTR profiles per main profile.
    
    (cherry picked from commit 6019a9039d46505947c5f88ed764825fadecc736)
    
    Bug: 415502787,416368589
    Change-Id: Ia6b229765d40bdf8ca4a41a61f34b8f0bc79fe8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6521183
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1457487}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6527130
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7151@{#828}
    Cr-Branched-From: 8e0d32ed6e49a2415b16e5ed402957cac2349ce2-refs/heads/main@{#1453031}

--- chrome/browser/extensions/api/cookies/cookies_api.cc
+++ chrome/browser/extensions/api/cookies/cookies_api.cc
@@ -220,11 +220,13 @@ void CookiesEventRouter::OnOffTheRecordProfileCreated(Profile* off_the_record) {
   // When an off-the-record spinoff of |profile_| is created, start listening
   // for cookie changes there. The OTR receiver should never be bound, since
   // there wasn't previously an OTR profile.
-  DCHECK(!otr_receiver_.is_bound());
+  if (off_the_record->IsPrimaryOTRProfile()) {
+    DCHECK(!otr_receiver_.is_bound());
 #if !BUILDFLAG(IS_ANDROID)
-  otr_profile_observation_.Observe(off_the_record);
+    otr_profile_observation_.Observe(off_the_record);
 #endif
-  BindToCookieManager(&otr_receiver_, off_the_record);
+    BindToCookieManager(&otr_receiver_, off_the_record);
+  }
 }
 
 void CookiesEventRouter::OnProfileWillBeDestroyed(Profile* profile) {
