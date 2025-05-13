ab3f141f408551a3580d0d8d729775620e8a734e
Jenna Himawan <jhimawan@google.com>
2025-05-13 12:03:41 -0700

    Enable android-keyboard-a11y flag
    Bug: 360423850, 399498687
    Change-Id: I2976b2cd6950b17ec555382b30d587cace2779aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542507
    Auto-Submit: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1459622}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -525,7 +525,7 @@ BASE_FEATURE(kAndroidElegantTextHeight,
 
 BASE_FEATURE(kAndroidKeyboardA11y,
              "AndroidKeyboardA11y",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidMetaClickHistoryNavigation,
              "AndroidMetaClickHistoryNavigation",
