f9679e57bbe00b519c2043de28bb9cbe34d241b9
Christine Ying <chrstne@google.com>
2025-05-13 06:36:10 -0700

    Reland "[PrivacyPolicy] Deflake privacy policy related tests"
    This reverts commit e9b287b92de67870791b63f789ff49cedf545906.
    
    Reason for revert: Fixes tests, need to approve new screenshots
    https://chrome-gold.skia.org/detail?grouping=name%3DBrowserUiDialog_PrivacySandboxDialogViewPrivacyPolicyBrowserTest_InvokeUi_PrivacyPolicy_crrev.com_c_6391455_windows%26source_type%3Dgtest-pixeltests&digest=c9ce78861e75ed3557c53c9a2f75fd0c old tests
    were failing because they were disabled and had no images to compare
    against
    
    Bug: 371041180, 415305952
    Original change's description:
    > Revert "[PrivacyPolicy] Deflake privacy policy related tests"
    >
    > This reverts commit a51ff7fa0957cc5936555ca78b70eb443ff9f8c6.
    >
    > Reason for revert: Tests are consistently failing on gardened bots
    >
    > Bug: 371041180, 415305952
    > Original change's description:
    > > [PrivacyPolicy] Deflake privacy policy related tests
    > >
    > > - Privacy policy page may open without fully loading due to change
    > > in loading happening after clicking the expand
    > > - replacing the src to load the content of the test url may happen after
    > > the screenshot is taken
    > >
    > > The previous test loads an error page, change this to load an
    > > instantaneous empty page instead.
    > >
    > > Bug: 371041180, 415305952
    > > Change-Id: Iaa44a9174226f284b55197a78063bc01ef03cdad
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6520071
    > > Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    > > Reviewed-by: Abe Boujane <boujane@google.com>
    > > Commit-Queue: Christine Ying <chrstne@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1458238}
    >
    > Bug: 371041180, 415305952
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ie8d85c76ddd7112973584400360af699da06f57b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6534569
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Noah Rose Ledesma <noahrose@google.com>
    > Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1458393}
    
    Bug: 371041180, 415305952
    Change-Id: I6771d60c87f406f773cb3c6e0d0cf11d04bccd9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6535290
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1459398}

--- chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view_browsertest.cc
+++ chrome/browser/ui/views/privacy_sandbox/privacy_sandbox_dialog_view_browsertest.cc
@@ -36,6 +36,8 @@ constexpr int kAverageBrowserWidth = 800;
 constexpr int kAverageBrowserHeight = 700;
 constexpr base::TimeDelta kMaxWaitTime = base::Seconds(30);
 
+// TODO(crbug.com/371041180): Refactor tests to remove these scripts and notify
+// from the notice components instead.
 std::string ScrollToBottomScript() {
   return R"```(
     (async () => {
@@ -76,6 +78,35 @@ std::string ScrollToBottomScript() {
       });
     })();
   )```";
+}
+
+std::string WaitForPrivacyPolicyPageLoadScript() {
+  return R"(
+    (async () => {
+      return new Promise(async (resolve) => {
+        requestAnimationFrame(async () => {
+          dialogElement = document.querySelector("body > "+$1);
+          if($2 !== "") dialogElement = dialogElement.shadowRoot.querySelector($2);
+          waitForPrivacyPolicyResolve = (el) => new Promise(privacyPolicyResolve => {
+            let timeout = setTimeout(() => {
+              el.removeEventListener('privacy-policy-loaded', privacyPolicyLoadCallback);
+              privacyPolicyResolve();
+            }, 2000);
+            const privacyPolicyLoadCallback = () => {
+              clearTimeout(timeout);
+              el.removeEventListener('privacy-policy-loaded', privacyPolicyLoadCallback);
+              privacyPolicyResolve();
+            };
+            el.addEventListener('privacy-policy-loaded', privacyPolicyLoadCallback);
+          });
+          const privacyPolicyEl = dialogElement.shadowRoot.querySelector('privacy-sandbox-privacy-policy-dialog');
+          privacyPolicyEl.shadowRoot.querySelector('#privacyPolicy').src = "about:blank";
+          await waitForPrivacyPolicyResolve(privacyPolicyEl);
+          setTimeout(resolve,0);
+        });
+      });
+    })();
+  )";
 }
 
 std::string ClickLearnMoreButton3TimesScript() {
@@ -123,10 +154,6 @@ class PrivacySandboxDialogViewBrowserTest : public DialogBrowserTest {
   }
 
   void SetUpOnMainThread() override {
-    host_resolver()->AddRule("*", "127.0.0.1");
-    embedded_test_server()->ServeFilesFromSourceDirectory("content/test/data");
-
-    ASSERT_TRUE(embedded_test_server()->Start());
     mock_service_ = static_cast<MockPrivacySandboxService*>(
         PrivacySandboxServiceFactory::GetInstance()->SetTestingFactoryAndUse(
             browser()->profile(),
@@ -318,7 +345,8 @@ class PrivacySandboxDialogViewPrivacyPolicyBrowserTest
                   privacy_sandbox_dialog_view->GetWebContentsForTesting()),
               nullptr);
 
-    // Click expand button.
+    // Privacy policy is initially not visible, click expand button and trigger
+    // visibility.
     EXPECT_TRUE(
         content::ExecJs(privacy_sandbox_dialog_view->GetWebContentsForTesting(),
                         "document.querySelector('body > "
@@ -326,6 +354,11 @@ class PrivacySandboxDialogViewPrivacyPolicyBrowserTest
                         "querySelector('#consent').shadowRoot.querySelector('"
                         "privacy-sandbox-dialog-learn-more').shadowRoot."
                         "querySelector('div > cr-expand-button').click()"));
+
+    EXPECT_TRUE(content::ExecJs(
+        privacy_sandbox_dialog_view->GetWebContentsForTesting(),
+        content::JsReplace(WaitForPrivacyPolicyPageLoadScript(),
+                           "privacy-sandbox-combined-dialog-app", "#consent")));
 
     // Click Privacy Policy link.
     EXPECT_TRUE(
@@ -337,17 +370,6 @@ class PrivacySandboxDialogViewPrivacyPolicyBrowserTest
                             GetPrivacyPolicyLinkElementId() +
                             "')"
                             ".click()"));
-
-    // Intentionally navigate to some blocked content to avoid flakiness.
-    auto script = content::JsReplace(
-        "document.querySelector('body > "
-        "privacy-sandbox-combined-dialog-app')"
-        ".shadowRoot.querySelector('#consent')"
-        ".shadowRoot.querySelector('privacy-sandbox-privacy-policy-dialog')"
-        ".shadowRoot.querySelector('#privacyPolicy').src = $1;",
-        embedded_test_server()->GetURL("/blue.html"));
-    EXPECT_TRUE(content::ExecJs(
-        privacy_sandbox_dialog_view->GetWebContentsForTesting(), script));
   }
 
  private:
@@ -356,7 +378,7 @@ class PrivacySandboxDialogViewPrivacyPolicyBrowserTest
 
 // TODO(https://crbug.com/415305952): High failure rate.
 IN_PROC_BROWSER_TEST_F(PrivacySandboxDialogViewPrivacyPolicyBrowserTest,
-                       DISABLED_InvokeUi_PrivacyPolicy) {
+                       InvokeUi_PrivacyPolicy) {
   ShowAndVerifyUi();
 }
 
@@ -382,7 +404,7 @@ class PrivacySandboxDialogViewAdsApiUxEnhancementPrivacyPolicyBrowserTest
 // TODO(https://crbug.com/415305952): High failure rate.
 IN_PROC_BROWSER_TEST_F(
     PrivacySandboxDialogViewAdsApiUxEnhancementPrivacyPolicyBrowserTest,
-    DISABLED_InvokeUi_PrivacyPolicy) {
+    InvokeUi_PrivacyPolicy) {
   ShowAndVerifyUi();
 }
 
