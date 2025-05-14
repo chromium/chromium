f60dc7bcff58e559f19e818d4da5bddeefc6e325
Dave Tapuska <dtapuska@chromium.org>
2025-05-14 09:27:07 -0700

    Revert "[ios blink] Make webnn work on the device"
    This reverts commit cd5897004cd48f7ed0a67980b9d2a73a8fa4b32e.
    
    Reason for revert: https://ci.chromium.org/ui/p/chromium/builders/ci/mac14-arm64-rel-tests/23238/overview
    
    Bug: 40254930
    Original change's description:
    > [ios blink] Make webnn work on the device
    >
    > tflite as a fallback works correctly on iOS in the GPU process.
    > CoreML model requires file system access to compile a model
    > and the GPU process is restricted from having file access. We've
    > asked Apple to provide an update how CoreML can be used in the
    > sandboxed rendering process.
    >
    > Bug: 40254930
    > Change-Id: I72598db882db38fa43d5d04f4c6055cbafb33a87
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543648
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1460093}
    
    Bug: 40254930
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I898a1e35dd74d9413cbdc2a88cef9214708fabab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6547278
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Owners-Override: Matt Reichhoff <mreichhoff@chromium.org>
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460154}

--- services/webnn/webnn_context_provider_impl.cc
+++ services/webnn/webnn_context_provider_impl.cc
@@ -24,9 +24,6 @@
 
 #if BUILDFLAG(IS_MAC)
 #include "base/mac/mac_util.h"
-#endif
-
-#if BUILDFLAG(IS_APPLE)
 #include "services/webnn/coreml/context_impl_coreml.h"
 #endif
 
@@ -190,18 +187,15 @@ void WebNNContextProviderImpl::CreateWebNNContext(
   }
 #endif  // BUILDFLAG(IS_WIN)
 
-#if BUILDFLAG(IS_APPLE)
-  if (__builtin_available(macOS 14, *)) {
-    if (base::FeatureList::IsEnabled(mojom::features::kWebNNCoreML)
 #if BUILDFLAG(IS_MAC)
-        && base::mac::GetCPUType() == base::mac::CPUType::kArm
-#endif  // BUILDFLAG(IS_MAC)
-    ) {
+  if (__builtin_available(macOS 14, *)) {
+    if (base::FeatureList::IsEnabled(mojom::features::kWebNNCoreML) &&
+        base::mac::GetCPUType() == base::mac::CPUType::kArm) {
       context_impl = std::make_unique<coreml::ContextImplCoreml>(
           std::move(receiver), this, std::move(options));
     }
   }
-#endif  // BUILDFLAG(IS_APPLE)
+#endif  // BUILDFLAG(IS_MAC)
 
 #if BUILDFLAG(WEBNN_USE_TFLITE)
   if (!context_impl) {
