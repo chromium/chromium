3ccaa4bd7decaaa3d7b724ad86a2437e6bcb681d
Marc Treib <treib@chromium.org>
2025-05-14 05:36:21 -0700

    [iOS] Multi-profile: Fix "save to photos" for account in other profile
    Before this CL, AccountPickerSelectionScreenCoordinator used
    ChromeAccountManagerService::GetIdentityWithGaiaID() to convert from
    GaiaId to SystemIdentity. That method only works for accounts that
    belong to the current profile, which is not always the case here.
    This CL changes it to GetIdentityOnDeviceWithGaiaID() which works for
    all identities on the device.
    
    This one instance was missed in the original migration to
    accounts-on-device in https://crrev.com/c/5993514
    
    Bug: 417393378
    Change-Id: Iaf859627a3331abe949a4a07958d61d21782124a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6540377
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460010}

--- ios/chrome/browser/account_picker/ui_bundled/account_picker_selection/account_picker_selection_screen_coordinator.mm
+++ ios/chrome/browser/account_picker/ui_bundled/account_picker_selection/account_picker_selection_screen_coordinator.mm
@@ -80,7 +80,7 @@
       ChromeAccountManagerServiceFactory::GetForProfile(self.profile);
 
   id<SystemIdentity> identity =
-      accountManagerService->GetIdentityWithGaiaID(GaiaId(gaiaID));
+      accountManagerService->GetIdentityOnDeviceWithGaiaID(GaiaId(gaiaID));
   DCHECK(identity);
   _mediator.selectedIdentity = identity;
   [self.delegate accountPickerSelectionScreenCoordinatorIdentitySelected:self];
