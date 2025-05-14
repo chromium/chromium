bf5802127a56e47588aa504ec77bea84cd4b5455
Jayson Adams <shrike@chromium.org>
2025-05-14 16:45:46 -0700

    [Mac Text Subs] Ignore text substitutions across input type changes
    When running the text substitution callback, ignore the substitution
    if the text input type has changed since the callback was created
    (an indication that the text substitution was meant for a different
    text object).
    
    Bug: 372217922
    Change-Id: I017d44474fedf426ce2d1327e7b89ce5c28afb97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6548775
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460441}

--- content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
+++ content/app_shim_remote_cocoa/render_widget_host_view_cocoa.mm
@@ -481,7 +481,7 @@ void ExtractUnderlines(NSAttributedString* string,
   NSRect textRectInViewCoordinates =
       [self convertRect:textRectInWindowCoordinates fromView:nil];
 
-  NSUInteger capturedChangeCounter = _availableTextChangeCounter;
+  ui::TextInputType capturedTextInputType = _textInputType;
 
   [self.spellChecker
       showCorrectionIndicatorOfType:NSCorrectionIndicatorTypeDefault
@@ -492,13 +492,13 @@ void ExtractUnderlines(NSAttributedString* string,
                   completionHandler:^(NSString* acceptedString) {
                     [self didAcceptReplacementString:acceptedString
                                forTextCheckingResult:candidateResult
-                                    withChangeNumber:capturedChangeCounter];
+                                   withTextInputType:capturedTextInputType];
                   }];
 }
 
 - (void)didAcceptReplacementString:(NSString*)acceptedString
              forTextCheckingResult:(NSTextCheckingResult*)correction
-                  withChangeNumber:(NSUInteger)changeNumber {
+                 withTextInputType:(ui::TextInputType)textInputType {
   // TODO: Keep NSSpellChecker up to date on the user's response via
   // -recordResponse:toCorrection:forWord:language:inSpellDocumentWithTag:.
   // Call it to report whether they initially accepted or rejected the
@@ -506,6 +506,12 @@ void ExtractUnderlines(NSAttributedString* string,
 
   if (acceptedString == nil)
     return;
+
+  // If the text input type is different now than when we set up the callback,
+  // the text replacement operation is stale and can be ignored.
+  if (textInputType != _textInputType) {
+    return;
+  }
 
   NSRange availableTextRange =
       NSMakeRange(_availableTextOffset, _availableText.length());
@@ -528,18 +534,6 @@ void ExtractUnderlines(NSAttributedString* string,
     if ([self.spellChecker preventsAutocorrectionBeforeString:trailingString
                                                      language:nil])
       return;
-
-    // Gather some info in case -doubleClickAtIndex: throws an exception.
-    // This change will eventually be reverted.
-    NSString* info = [NSString
-        stringWithFormat:@"%lu == %lu %lu %@ %@ %@ %@", changeNumber,
-                         _availableTextChangeCounter, attString.string.length,
-                         NSStringFromRange(availableTextRange),
-                         NSStringFromRange(correction.range),
-                         NSStringFromRange(trailingRange),
-                         NSStringFromRange(trailingRangeInAvailableText)];
-    SCOPED_CRASH_KEY_STRING256("RenderWidgetHostViewCocoa", "didAcceptTR",
-                               base::SysNSStringToUTF8(info));
 
     if ([attString doubleClickAtIndex:trailingRangeInAvailableText.location]
             .location < trailingRangeInAvailableText.location)
