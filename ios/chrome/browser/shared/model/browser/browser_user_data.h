ccf80c685f97199316e0565dc043fba46628d19c
Sylvain Defresne <sdefresne@chromium.org>
2025-05-14 05:23:54 -0700

    Reland "[ios] Store reference to owning Browser in BrowserUserData<T>"
    This is a reland of commit f1163499797dd8e9f8e3c08732580e95e05f92ee
    
    The original version of the CL was reverted because the constructor
    was still used in the internal repository. The usage has now been
    fixed and the CL can be relanded without any changes.
    
    Original change's description:
    > [ios] Store reference to owning Browser in BrowserUserData<T>
    >
    > Instead of having each BrowserUserData<T> subclasses managed the
    > pointer to the owning Browser, directly store it in the base class.
    > This is possible because now all sub-class pass the pointer to the
    > Browser to BrowserUserData<T> constructor.
    >
    > The subclasses will be changed in followup CLs to use the pointer
    > that is stored in the base class and stop managing it themselves.
    >
    > Bug: 409299519
    > Change-Id: Ib0c5ff68d61cb4ebc308523c8a0c186cad223477
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6519772
    > Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Reviewed-by: Mark Cogan <marq@chromium.org>
    > Commit-Queue: Mark Cogan <marq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1459393}
    
    Bug: 409299519
    Change-Id: Ie4bcf6feba6f3d3b3f3ac8afa99d5b0606a6f816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6543590
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460004}

--- ios/chrome/browser/shared/model/browser/browser_user_data.h
+++ ios/chrome/browser/shared/model/browser/browser_user_data.h
@@ -6,7 +6,9 @@
 #define IOS_CHROME_BROWSER_SHARED_MODEL_BROWSER_BROWSER_USER_DATA_H_
 
 #include "base/check.h"
+#include "base/check_deref.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/raw_ref.h"
 #include "base/supports_user_data.h"
 #include "ios/chrome/browser/shared/model/browser/browser.h"
 
@@ -59,8 +61,10 @@ class BrowserUserData : public base::SupportsUserData::Data {
   }
 
  protected:
-  BrowserUserData() {}
-  explicit BrowserUserData(Browser* browser) {}
+  explicit BrowserUserData(Browser* browser) : browser_(CHECK_DEREF(browser)) {}
+
+  // The owning Browser.
+  const raw_ref<Browser> browser_;
 
  private:
   // Default factory for T that invoke T's constructor. Can be overloaded
