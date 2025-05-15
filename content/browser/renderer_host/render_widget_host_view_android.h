2e37fc378fd58d41c6c19a4a174a6732be97edc4
Aldo Culquicondor <acondor@chromium.org>
2025-05-15 10:25:30 -0700

    [M137] Delay screenshot SendResult and compression
    Original change's description:
    > Delay screenshot SendResult and compression
    > 
    > SendResult and compression are expensive operations that can cause
    > contention with loading operations during navigation.
    > 
    > We don't need a screenshot immediately, but after the user reacts to a
    > page load, they might decide to do a history navigation gesture.
    > In other words, we can't wait that long. This CL uses a delay of
    > 400ms, configurable via trial fields.
    > 
    > Screenshot compression can be delayed until the browser reaches
    > quiescence.
    > 
    > This CL replaces the previous approach of using lower priorities, which
    > proved not to be enough.
    > 
    > Bug: 393558258
    > Change-Id: Ibf4092ceb9d7adc5ff265fe1beec3819509924cb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6502812
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1455899}
    
    Bug: 417749442,393558258
    Change-Id: Ibf4092ceb9d7adc5ff265fe1beec3819509924cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550748
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7151@{#1080}
    Cr-Branched-From: 8e0d32ed6e49a2415b16e5ed402957cac2349ce2-refs/heads/main@{#1453031}

--- content/browser/renderer_host/render_widget_host_view_android.h
+++ content/browser/renderer_host/render_widget_host_view_android.h
@@ -166,11 +166,11 @@ class CONTENT_EXPORT RenderWidgetHostViewAndroid
       const gfx::Rect& src_rect,
       const gfx::Size& output_size,
       base::OnceCallback<void(const SkBitmap&)> callback) override;
-  void CopyFromExactSurfaceWithIpcPriority(
+  void CopyFromExactSurfaceWithIpcDelay(
       const gfx::Rect& src_rect,
       const gfx::Size& output_size,
       base::OnceCallback<void(const SkBitmap&)> callback,
-      CopyOutputIpcPriority ipc_priority) override;
+      base::TimeDelta ipc_delay) override;
   void CopyFromExactSurface(
       const gfx::Rect& src_rect,
       const gfx::Size& output_size,
