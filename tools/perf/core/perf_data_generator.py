0d834fb0bd442e13a0e9c00f91fdc0fbe8b2f1f0
zijiehe-google-com <zijiehe@google.com>
2025-05-15 15:06:59 -0700

    [fuchsia] setup perf-arm64-pgo builder
    The recipe updated at https://crrev.com/c/6548558, so that the
    builder can be created.
    The fuchsia-perf-nsn-pgo and fuchsia-perf-shk-pgo will be in the
    src-internal, and will be created separately.
    
    Bug: 407068989
    Change-Id: I4518ca9b004eb636ab75d2f41ff9bb57cec4eb22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553701
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461018}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -360,6 +360,12 @@ FYI_BUILDERS = {
             'base_perftests'
         ],
     },
+    'fuchsia-builder-perf-arm64-pgo': {
+        'additional_compile_targets': [
+            'web_engine_shell_pkg', 'cast_runner_pkg', 'chromium_builder_perf',
+            'base_perftests'
+        ],
+    },
 }
 
 # These configurations are taken from chromium_perf.py in
