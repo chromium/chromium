00d70ec661279a298449cd2a909d830019364e2e
Florian Leimgruber <fleimgruber@google.com>
2025-05-15 05:26:52 -0700

    [Autofill AI] Populate form and field signatures in model request
    Currently, they are only populated when querying the form through the
    extension API. However, they are necessary even for client-triggered
    model requests to cache forms on the server-side.
    
    (cherry picked from commit 1d4fd935803b20f4af0de7753fe175c2217fe990)
    
    Bug: 397586322
    Fixed: 417926892
    Change-Id: Idf19e7da9cfd0f676c47cde03076d5be6778ca9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6542949
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1459946}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6551420
    Cr-Commit-Position: refs/branch-heads/7151@{#1059}
    Cr-Branched-From: 8e0d32ed6e49a2415b16e5ed402957cac2349ce2-refs/heads/main@{#1453031}

--- components/autofill/core/browser/form_processing/optimization_guide_proto_util.cc
+++ components/autofill/core/browser/form_processing/optimization_guide_proto_util.cc
@@ -68,16 +68,13 @@ void PopulateExtensionAPISpecificFields(
     optimization_guide::proto::FormData& form_proto) {
   // Add additional form-level metadata.
   *form_proto.mutable_global_id() = ToGlobalIdProto(form_data.global_id());
-  form_proto.set_form_signature(*CalculateFormSignature(form_data));
   // Add additional field-level metadata.
   CHECK_EQ(form_data.fields().size(),
            static_cast<size_t>(form_proto.fields_size()));
   for (size_t i = 0; i < form_data.fields().size(); i++) {
     const FormFieldData& field = form_data.fields()[i];
-    optimization_guide::proto::FormFieldData* proto_field =
-        form_proto.mutable_fields(i);
-    *proto_field->mutable_global_id() = ToGlobalIdProto(field.global_id());
-    proto_field->set_field_signature(*CalculateFieldSignatureForField(field));
+    *form_proto.mutable_fields(i)->mutable_global_id() =
+        ToGlobalIdProto(field.global_id());
   }
 }
 
@@ -87,11 +84,12 @@ optimization_guide::proto::FormData ToFormDataProto(
     const FormData& form_data,
     FormDataProtoConversionReason conversion_reason) {
   optimization_guide::proto::FormData form_data_proto;
+  form_data_proto.set_form_signature(*CalculateFormSignature(form_data));
   form_data_proto.set_form_name(base::UTF16ToUTF8(form_data.name()));
   for (const FormFieldData& field : form_data.fields()) {
     optimization_guide::proto::FormFieldData* field_proto =
         form_data_proto.add_fields();
-
+    field_proto->set_field_signature(*CalculateFieldSignatureForField(field));
     // Unconditionally assign html meta data to the field.
     field_proto->set_field_name(base::UTF16ToUTF8(field.name()));
     field_proto->set_field_label(base::UTF16ToUTF8(field.label()));
