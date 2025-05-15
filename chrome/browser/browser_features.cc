18dddf74d2914a158acf9a1cfb76df8efedecb65
Tom Anderson <thomasanderson@chromium.org>
2025-05-15 12:53:05 -0700

    Disable UseFreedesktopSecretKeyProviderForEncryption
    Currently the flag does nothing because UseFreedesktopSecretKeyProvider
    is disabled, however I'm going to create a rollout experiment for
    UseFreedesktopSecretKeyProvider so I don't want this flag to cause
    encryption to become enabled prematurely.
    
    R=thestig
    
    Bug: 40086962
    Change-Id: If444a3100265eb1a6b576bfe3adc299717c52fd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6553381
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1460931}

--- chrome/browser/browser_features.cc
+++ chrome/browser/browser_features.cc
@@ -238,7 +238,7 @@ BASE_FEATURE(kSecretPortalKeyProviderUseForEncryption,
 // FreedesktopSecretKeyProvider. Otherwise, it will only decrypt existing data.
 BASE_FEATURE(kUseFreedesktopSecretKeyProviderForEncryption,
              "UseFreedesktopSecretKeyProviderForEncryption",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_LINUX)
 
 // Enables migration of the network context data from `unsandboxed_data_path` to
