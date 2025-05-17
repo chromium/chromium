27592897af71e9aaa267c54cd3d53c364799c075
Jihad Hanna <jihadghanna@google.com>
2025-05-17 06:27:00 -0700

    Remove a not needed include
    Bug: 40100455
    Change-Id: I45f25a7bfce85b0673fb51a9800d6ff89d938f2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6556747
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1461781}

--- components/autofill_ai/core/browser/metrics/autofill_ai_logger.cc
+++ components/autofill_ai/core/browser/metrics/autofill_ai_logger.cc
@@ -7,7 +7,6 @@
 #include <string_view>
 
 #include "base/metrics/histogram_functions.h"
-#include "base/metrics/histogram_functions_internal_overloads.h"
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "components/autofill/core/common/unique_ids.h"
