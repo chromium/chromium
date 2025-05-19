e530c1578622adea59ae2ac3a586ae090681422b
Corentin Wallez <cwallez@chromium.org>
2025-05-19 08:07:17 -0700

    [webgl] Add stub WebGL[2]RenderingContextWebGPU
    These implementations of the WebGL IDL are meant to be implemented with
    ANGLE running on top of the WebGPU using dawn::wire, the same way that
    WebGPU's JS bindings are implemented. If successful, this would allow
    reducing the GPU process attack surface as it wouldn't need to support
    the GLES command buffer anymore.
    
    Binary-Size: Required initial increase to add WebGLOnWebGPU.
    Fuchsia-Binary-Size: Increase is temporary, will be more than compensated by linking ANGLE statically (-400kb).
    Bug: 414506479
    Change-Id: I63d88a06d0d91e9842b48414d335df99522bba8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494836
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462154}

--- third_party/blink/renderer/modules/canvas/htmlcanvas/v8_rendering_context.h
+++ third_party/blink/renderer/modules/canvas/htmlcanvas/v8_rendering_context.h
@@ -8,6 +8,6 @@
 // This is just a forwarding header to avoid including an enormous filename in
 // each file that needs the declaration of the union that is used for
 // RenderingContext.
-#include "third_party/blink/renderer/bindings/modules/v8/v8_union_canvasrenderingcontext2d_gpucanvascontext_imagebitmaprenderingcontext_webgl2renderingcontext_webglrenderingcontext.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_union_canvasrenderingcontext2d_gpucanvascontext_imagebitmaprenderingcontext_webgl2renderingcontext_webgl2renderingcontextwebgpu_webglrenderingcontext_webglrenderingcontextwebgpu.h"
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_CANVAS_HTMLCANVAS_V8_RENDERING_CONTEXT_H_
