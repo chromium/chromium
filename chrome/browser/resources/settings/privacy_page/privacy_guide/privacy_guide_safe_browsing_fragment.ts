3fea0c8f101cf98f01af255e2aeab635ac0df6bb
Oleh Lamzin <lamzin@google.com>
2025-05-19 08:22:06 -0700

    Privacy Guide: Add missing icons import for browsing fragment
    privacy_guide_safe_browsing_fragment.html uses the following icon(s):
      - settings20:data
      - settings20:download
      - settings20:gshield
      - settings:language
      - settings20:link
      - settings20:account
      - settings:performance
    
    but privacy_guide_safe_browsing_fragment.ts does not import corresponding
    .html.js file(s):
      - chrome/browser/resources/settings/icons.html for
        - settings20:* icon(s)
        - settings:* icon(s)
    
    Add missing icon import(s).
    
    Bug: 365922159
    Change-Id: I9b45a3912b8381911480701377e9bc72c5fe7dba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563291
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Auto-Submit: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462171}

--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_safe_browsing_fragment.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_safe_browsing_fragment.ts
@@ -12,6 +12,7 @@ import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import '/shared/settings/prefs/prefs.js';
 import './privacy_guide_fragment_shared.css.js';
 import '../../controls/settings_radio_group.js';
+import '../../icons.html.js';
 import '../../privacy_page/collapse_radio_button.js';
 
 import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
