8414edbad615a2793113f043f7f3c394902794e4
Ramin Halavati <rhalavati@google.com>
2025-05-20 11:08:56 -0700

    Update PDF Searchify browsertest.
    Add metrics and multi-page tests for PDF Searchify.
    
    AX-Relnotes: n/a
    Bug: 360803943
    Change-Id: I26bf57e7e4b1e04118160725319e264717f29c1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6562623
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462910}

--- chrome/browser/pdf/pdf_searchify_browsertest.cc
+++ chrome/browser/pdf/pdf_searchify_browsertest.cc
@@ -2,18 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/run_until.h"
 #include "base/test/test_future.h"
 #include "base/time/time.h"
 #include "chrome/browser/pdf/pdf_extension_test_base.h"
 #include "chrome/browser/screen_ai/screen_ai_install_state.h"
+#include "components/metrics/content/subprocess_metrics_provider.h"
 #include "components/pdf/browser/pdf_document_helper.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "pdf/pdf_features.h"
 #include "services/screen_ai/public/cpp/utilities.h"
 #include "ui/accessibility/accessibility_features.h"
+#include "ui/accessibility/accessibility_switches.h"
 #include "ui/accessibility/ax_features.mojom-features.h"
+
+namespace {
+
+bool IsScreenReaderEnabled() {
+  return base::CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kForceRendererAccessibility);
+}
+
+}  // namespace
 
 // Parameter: Searchify (ScreenAI OCR) availability.
 class PDFSearchifyTest : public PDFExtensionTestBase,
@@ -80,7 +93,7 @@ class PDFSearchifyTest : public PDFExtensionTestBase,
 
   // Searchify may be slow, so if the test expects text, `repeat_until_has_text`
   // should be set to true to repeat getting page text until it's not empty.
-  std::u16string GetPageText(bool repeat_until_has_text) {
+  std::u16string GetPageText(int32_t page_index, bool repeat_until_has_text) {
     auto* helper =
         pdf::PDFDocumentHelper::MaybeGetForWebContents(GetActiveWebContents());
     if (!helper) {
@@ -89,10 +102,10 @@ class PDFSearchifyTest : public PDFExtensionTestBase,
     }
 
     std::u16string result;
-    EXPECT_TRUE(
-        base::test::RunUntil([&helper, &repeat_until_has_text, &result]() {
+    EXPECT_TRUE(base::test::RunUntil(
+        [&helper, &page_index, &repeat_until_has_text, &result]() {
           base::test::TestFuture<const std::u16string&> future;
-          helper->GetPageText(0, future.GetCallback());
+          helper->GetPageText(page_index, future.GetCallback());
           EXPECT_TRUE(future.Wait());
           result = future.Take();
           return !result.empty() || !repeat_until_has_text;
@@ -116,24 +129,66 @@ INSTANTIATE_TEST_SUITE_P(All,
 #endif
 );
 
-IN_PROC_BROWSER_TEST_P(PDFSearchifyTest, HelloWorld) {
 // TODO(crbug.com/406839385): Re-enable this test on Mac.
 #if BUILDFLAG(IS_MAC)
-  GTEST_SKIP() << "Disable on macOS";
+#define MAYBE_HelloWorld DISABLED_HelloWorld
+#else
+#define MAYBE_HelloWorld HelloWorld
 #endif
+IN_PROC_BROWSER_TEST_P(PDFSearchifyTest, MAYBE_HelloWorld) {
+  base::HistogramTester histograms;
   ASSERT_TRUE(LoadPdf(embedded_test_server()->GetURL(
       "/pdf/accessibility/hello-world-in-image.pdf")));
 
-  std::u16string page_text = GetPageText(/*repeat_until_has_text=*/
-                                         IsSearchifyActive());
+  std::u16string page_text =
+      GetPageText(/*page_idex=*/0, /*repeat_until_has_text=*/
+                  IsSearchifyActive());
 
   EXPECT_EQ(page_text, IsSearchifyActive() ? u"Hello, world!" : u"");
+
+  metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
+
+  histograms.ExpectUniqueSample("PDF.PageHasText", false, 1);
+
+  // `ScreenReaderModeEnabled` is recorded only when searchify is active.
+  histograms.ExpectUniqueSample(
+      "Accessibility.ScreenAI.Searchify.ScreenReaderModeEnabled",
+      IsScreenReaderEnabled(), IsSearchifyActive());
 }
 
-// TODO(crbug.com/360803943): Add metrics tests similar to
-// `PdfOcrIntegrationTest`.
+// TODO(crbug.com/406839385): Re-enable this test on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_MultiPage DISABLED_MultiPage
+#else
+#define MAYBE_MultiPage MultiPage
+#endif
+IN_PROC_BROWSER_TEST_P(PDFSearchifyTest, MAYBE_MultiPage) {
+  base::HistogramTester histograms;
+  ASSERT_TRUE(LoadPdf(embedded_test_server()->GetURL(
+      "/pdf/accessibility/inaccessible-text-in-three-page.pdf")));
 
-// TODO(crbug.com/360803943): Consider adding save tests like
+  static constexpr std::u16string_view kExpectedTexts[3] = {
+      u"Hello, world!", u"Paragraph 1 on Page 2Paragraph 2 on Page 2",
+      u"Paragraph 1 on Page 3Paragraph 2 on Page 3"};
+
+  int page_index = 0;
+  for (const auto& expected_text : kExpectedTexts) {
+    std::u16string page_text =
+        GetPageText(page_index++, /*repeat_until_has_text=*/
+                    IsSearchifyActive());
+    EXPECT_EQ(page_text, IsSearchifyActive() ? expected_text : u"");
+  }
+
+  metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
+
+  histograms.ExpectUniqueSample("PDF.PageHasText", false, 3);
+
+  // `ScreenReaderModeEnabled` is recorded only when searchify is active and is
+  // recorded only once for each PDF.
+  histograms.ExpectUniqueSample(
+      "Accessibility.ScreenAI.Searchify.ScreenReaderModeEnabled",
+      IsScreenReaderEnabled(), IsSearchifyActive() ? 1 : 0);
+}
+
+// TODO(crbug.com/382610226): Consider adding save tests like
 // pdf_extension_download_test.cc
-
-// TODO(crbug.com/360803943): Add multi-page tests.
