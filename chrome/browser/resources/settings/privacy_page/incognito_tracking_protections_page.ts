35da5473f79c810f26cbbbd4278a01ff785b096f
Oleh Lamzin <lamzin@google.com>
2025-05-20 05:00:23 -0700

    Settings: Add missing icons import for incognito tracking protection
    incognito_tracking_protections_page.html uses the following icon(s):
      - settings20:network-ping
      - settings20:preview
      - settings20:account-circle
      - settings20:googleg
      - settings20:privacy-screen
    
    incognito_tracking_protections_page.ts does not import corresponding
    .html.js file(s):
      - chrome/browser/resources/settings/icons.html for
        - settings20:* icon(s)
    
    Add missing icon import(s).
    
    Bug: 365922159
    Change-Id: I6a05300818e07d9d925d01c8497ae976d14ae8dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563297
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Auto-Submit: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1462705}

--- chrome/browser/resources/settings/privacy_page/incognito_tracking_protections_page.ts
+++ chrome/browser/resources/settings/privacy_page/incognito_tracking_protections_page.ts
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import '/shared/settings/prefs/prefs.js';
+import '../icons.html.js';
 
 import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
