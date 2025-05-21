707707f71d520d9fda60bbfc81d0cb3e7949efde
Robert Lin <robertlin@chromium.org>
2025-05-20 22:45:08 -0700

    preload: Fix AndroidPrerenderManager::StopPrerendering crash
    AndroidPrerenderManager is a global singleton so it may be shared by
    multiple tabs, and a mapping is added to the class to fix the problem
    accessing a prerender handle which may not exist on the given
    WebContents.
    https://source.chromium.org/chromium/chromium/src/+/04050bb24084494fa941f38ce218e795b945408b:chrome/browser/ui/android/preloading/java/src/org/chromium/chrome/browser/preloading/AndroidPrerenderManager.java;l=53
    
    Bug: 418730039
    Change-Id: I9b6864f06bfcbf6eb03fe29553b53b828291070c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6564886
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463229}

--- chrome/browser/android/preloading/android_prerender_manager.cc
+++ chrome/browser/android/preloading/android_prerender_manager.cc
@@ -41,20 +41,28 @@ bool AndroidPrerenderManager::StartPrerendering(
   PrerenderManager::CreateForWebContents(web_contents);
   auto* prerender_manager = PrerenderManager::FromWebContents(web_contents);
   CHECK(prerender_manager);
-  prerender_handle_ = prerender_manager->StartPrerenderNewTabPage(
-      prerender_url, chrome_preloading_predictor::kTouchOnNewTabPage);
-  return prerender_handle_ != nullptr;
+  base::WeakPtr<content::PrerenderHandle> prerender_handle =
+      prerender_manager->StartPrerenderNewTabPage(
+          prerender_url, chrome_preloading_predictor::kTouchOnNewTabPage);
+  if (prerender_handle) {
+    prerender_handle_map_[web_contents] = prerender_handle;
+  } else if (prerender_handle_map_.contains(web_contents) &&
+             !prerender_handle_map_[web_contents]) {
+    prerender_handle_map_.erase(web_contents);
+  }
+  return prerender_handle != nullptr;
 }
 
 void AndroidPrerenderManager::StopPrerendering(
     JNIEnv* env,
     const base::android::JavaParamRef<jobject>& j_web_contents) {
-  if (prerender_handle_) {
-    content::WebContents* const web_contents =
-        content::WebContents::FromJavaWebContents(j_web_contents);
+  content::WebContents* const web_contents =
+      content::WebContents::FromJavaWebContents(j_web_contents);
+  if (prerender_handle_map_.contains(web_contents)) {
     auto* prerender_manager = PrerenderManager::FromWebContents(web_contents);
     CHECK(prerender_manager);
-    prerender_manager->StopPrerenderNewTabPage(prerender_handle_);
-    prerender_handle_ = nullptr;
+    prerender_manager->StopPrerenderNewTabPage(
+        prerender_handle_map_[web_contents]);
+    prerender_handle_map_.erase(web_contents);
   }
 }
