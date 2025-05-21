ef6e427bf94ae7ac1a6bbb38f36f9e4c05d11367
Anders Hartvoll Ruud <andruud@chromium.org>
2025-05-21 07:35:27 -0700

    [ident] Support ident() in timeline names
    The scroll-timeline-name, view-timeline-name, and timeline-scope
    properties were already working, but animation-timeline needed
    a call CSSCustomIdentValue::ComputeIdent() rather than
    CSSCustomIdentValue::Value().
    
    Bug: 384930424
    Change-Id: I39c3d85de8fc788369d5e669d8de238771f21c8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6573576
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463417}

--- third_party/blink/renderer/core/css/resolver/css_to_style_map.cc
+++ third_party/blink/renderer/core/css/resolver/css_to_style_map.cc
@@ -422,7 +422,8 @@ StyleTimeline CSSToStyleMap::MapAnimationTimeline(StyleResolverState& state,
   }
   if (auto* custom_ident = DynamicTo<CSSCustomIdentValue>(value)) {
     return StyleTimeline(MakeGarbageCollected<ScopedCSSName>(
-        custom_ident->Value(), custom_ident->GetTreeScope()));
+        custom_ident->ComputeIdent(state.CssToLengthConversionData()),
+        custom_ident->GetTreeScope()));
   }
   if (value.IsViewValue()) {
     const auto& view_value = To<cssvalue::CSSViewValue>(value);
