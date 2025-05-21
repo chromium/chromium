07bc7c3b78aa5e9da7fe12f33466e82f4e8c79f8
Justin Lulejian <jlulejian@chromium.org>
2025-05-21 10:51:37 -0700

    [Extensions] Enable the per-extension user script toggle by default.
    Bug: 390138269
    Change-Id: I74f9faf4de96750c891eafe8635db88a4d4a4eda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6564287
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463571}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -223,7 +223,7 @@ BASE_FEATURE(kDisableLoadExtensionCommandLineSwitch,
 
 BASE_FEATURE(kUserScriptUserExtensionToggle,
              "UserScriptUserExtensionToggle",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDebuggerAPIRestrictedToDevMode,
              "DebuggerAPIRestrictedToDevMode",
