05bc6382fad9963affbd915b78485f6d34f6de48
Corentin Wallez <cwallez@chromium.org>
2025-05-21 00:37:53 -0700

    Reland "[webgl] Add stub WebGL[2]RenderingContextWebGPU"
    This is a reland of commit e530c1578622adea59ae2ac3a586ae090681422b
    
    Reland after adding [Offscreen]RenderingContext to union_name_map.conf.
    
    Original change's description:
    > [webgl] Add stub WebGL[2]RenderingContextWebGPU
    >
    > These implementations of the WebGL IDL are meant to be implemented with
    > ANGLE running on top of the WebGPU using dawn::wire, the same way that
    > WebGPU's JS bindings are implemented. If successful, this would allow
    > reducing the GPU process attack surface as it wouldn't need to support
    > the GLES command buffer anymore.
    >
    > Binary-Size: Required initial increase to add WebGLOnWebGPU.
    > Fuchsia-Binary-Size: Increase is temporary, will be more than compensated by linking ANGLE statically (-400kb).
    > Bug: 414506479
    > Change-Id: I63d88a06d0d91e9842b48414d335df99522bba8d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6494836
    > Reviewed-by: Brandon Jones <bajones@chromium.org>
    > Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1462154}
    
    Binary-Size: Required initial increase to add WebGLOnWebGPU.
    Fuchsia-Binary-Size: Increase is temporary, will be more than compensated by linking ANGLE statically (-400kb).
    Bug: 413078308
    Change-Id: I1fa2fded8709e967440a6fb8e2f8e196598b5372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6563818
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463253}

---
+++ third_party/blink/renderer/modules/webgl/webgl2_rendering_context_webgpu.cc
@@ -0,0 +1,29 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/webgl/webgl2_rendering_context_webgpu.h"
+
+#include "base/notimplemented.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_union_offscreen_rendering_context.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_union_rendering_context.h"
+
+namespace blink {
+
+WebGL2RenderingContextWebGPU::WebGL2RenderingContextWebGPU(
+    CanvasRenderingContextHost* host,
+    const CanvasContextCreationAttributesCore& requested_attributes)
+    : WebGLRenderingContextWebGPUBase(host,
+                                      requested_attributes,
+                                      CanvasRenderingAPI::kWebgl2) {}
+
+V8RenderingContext* WebGL2RenderingContextWebGPU::AsV8RenderingContext() {
+  return MakeGarbageCollected<V8RenderingContext>(this);
+}
+
+V8OffscreenRenderingContext*
+WebGL2RenderingContextWebGPU::AsV8OffscreenRenderingContext() {
+  return MakeGarbageCollected<V8OffscreenRenderingContext>(this);
+}
+
+}  // namespace blink
