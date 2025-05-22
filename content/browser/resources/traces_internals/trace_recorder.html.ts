e2cda8128be4c5f109e20aa9445aaf45978c8640
Alex Attar <aattar@google.com>
2025-05-22 14:15:13 -0700

    Add UI for Trace Recorder, including state management and download.
    This CL introduces a new UI component (`trace-recorder`) for initiating,
    stopping, and cloning Chrome traces from a `chrome://` page.
    
    Key features include:
    - Button State Management: Buttons for "Start Tracing", "Stop Tracing",
      and "Clone Trace" now dynamically enable/disable based on the current
      tracing state.
      - "Start Tracing" is enabled only when tracing is idle.
      - "Stop Tracing" and "Clone Trace" are enabled when tracing is active
        or in the process of stopping.
    - UI Status Indicator: A visual circle and text display the current
      tracing state (Idle, Tracing, Stopping), providing immediate feedback
      to the user. The circle changes color based on the state.
    - Dynamic Download Filename: Cloned trace files are now saved with
      a timestamped filename in the format `dd_mm_yyyy_HH_MM.gz`,
      improving traceability and organization of trace data.
    
    Bug: crbug.com/413057267
    
    Change-Id: Ic47755fe483db27969c8c02a6453712069120f36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6550796
    Commit-Queue: Alex Attar <aattar@google.com>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464357}

--- content/browser/resources/traces_internals/trace_recorder.html.ts
+++ content/browser/resources/traces_internals/trace_recorder.html.ts
@@ -3,17 +3,35 @@
 // found in the LICENSE file.
 
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
-
 import type {TraceRecorderElement} from './trace_recorder.js';
 
 export function getHtml(this: TraceRecorderElement) {
   // clang-format off
   return html`
-<h1>Record Trace</h1>
-<div id="action-panel">
-  <cr-button class="action-button">Record</cr-button>
-</div>
-<cr-toast duration="5000">${this.toastMessage_}</cr-toast>
+    <h1>Record Trace</h1>
+    <div>Your current config proto: ${this.traceConfig}</div>
+    <div id="status-container" class="${this.statusClass}">
+        <div class="status-circle"></div>
+        <h3>Status: ${this.tracingState}</h3>
+    </div>
+    <div id="action-panel">
+      <cr-button
+          @click="${this.startTracing_}"
+          ?disabled="${!this.isStartTracingEnabled}">
+        Start Tracing
+      </cr-button>
+      <cr-button
+          @click="${this.stopTracing_}"
+          ?disabled="${!this.isStopTracingEnabled}">
+        Stop Tracing
+      </cr-button>
+      <cr-button
+          @click="${this.cloneTraceSession_}"
+          ?disabled="${!this.isCloneTraceEnabled}">
+        Clone Trace
+      </cr-button>
+    </div>
+    <cr-toast duration="5000">${this.toastMessage}</cr-toast>
   `;
   // clang-format on
 }
