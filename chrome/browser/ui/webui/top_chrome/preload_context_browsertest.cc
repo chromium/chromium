6b55b64374c2077fd5511bde8de3c30beb7b7a10
Mikita Kuchyn <kuchyn@google.com>
2025-05-22 01:43:59 -0700

    Migrate PreloadContextTest into InProcessBrowserTest
    Use of BrowserWithTestWindowTest is being deprecated and existing
    tests requiring a Browser environment are being migrated to
    being browser tests.
    
    This CL updates the PreloadContextTest fixture to an
    InProcessBrowserTest and removes the BrowserWithTestWindowTest
    dependency.
    
    Bug: 417766643
    
    Change-Id: Id776bd140649e7e78cdc3a7adfd126af98043ab5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6574882
    Reviewed-by: Piotr Kotynia <piotrkotynia@google.com>
    Commit-Queue: Mikita Kuchyn <kuchyn@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1463944}

--- chrome/browser/ui/webui/top_chrome/preload_context_browsertest.cc
+++ chrome/browser/ui/webui/top_chrome/preload_context_browsertest.cc
@@ -4,26 +4,27 @@
 
 #include "chrome/browser/ui/webui/top_chrome/preload_context.h"
 
-#include "chrome/test/base/browser_with_test_window_test.h"
-#include "testing/gtest/include/gtest/gtest.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "content/public/test/browser_test.h"
 
 namespace webui {
 
-using PreloadContextTest = BrowserWithTestWindowTest;
+using PreloadContextTest = InProcessBrowserTest;
 
 // Tests creating PreloadContext from Profile and Browser.
-TEST_F(PreloadContextTest, Basic) {
+IN_PROC_BROWSER_TEST_F(PreloadContextTest, Basic) {
   PreloadContext brower_context = PreloadContext::From(browser());
   EXPECT_TRUE(brower_context.IsBrowser());
   EXPECT_FALSE(brower_context.IsProfile());
   EXPECT_EQ(brower_context.GetBrowser(), browser());
   EXPECT_EQ(brower_context.GetProfile(), nullptr);
 
-  PreloadContext profile_context = PreloadContext::From(GetProfile());
+  PreloadContext profile_context = PreloadContext::From(browser()->profile());
   EXPECT_FALSE(profile_context.IsBrowser());
   EXPECT_TRUE(profile_context.IsProfile());
   EXPECT_EQ(profile_context.GetBrowser(), nullptr);
-  EXPECT_EQ(profile_context.GetProfile(), GetProfile());
+  EXPECT_EQ(profile_context.GetProfile(), browser()->profile());
 }
 
 }  // namespace webui
