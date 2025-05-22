00c905ea7b21df948563ce3e9c7bb983bb3fe909
Menghan Yang <myuu@google.com>
2025-05-22 05:14:30 -0700

    [iOS][Gardener] Revert "Turn on Hagrid test config flag on IOS"
    This reverts commit b05814ec71bdff8c6caecb6d6b9c83680f35a2b8.
    
    Reason for revert: The "IDS_AUTOFILL_CARD_UNMASK_PROGRESS_DIALOG_TITLE"
    related verifications added by this CL in
    PaymentsSuggestionBottomSheetEGTest/testUpdateBottomSheetOnAddServerCreditCard
    fail on the ios-fieldtrial-rel bot. First failing build:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios-fieldtrial-rel/43740/overview
    
    Original change's description:
    > Turn on Hagrid test config flag on IOS
    >
    > Bug:417491134, b/416069944
    >
    > Change-Id: I40fc6ddf616e6e76a984c7059fd1afbe63c644c5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509345
    > Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    > Reviewed-by: Siyu An <siyua@chromium.org>
    > Commit-Queue: Yiwen Qian <yiwenqian@google.com>
    > Reviewed-by: Vincent Boisselle <vincb@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1463112}
    
    Bug: 417491134, b/416069944
    Change-Id: I6d88ff6e8df05c654c606820ea115dca94343cc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578586
    Commit-Queue: Menghan Yang <myuu@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Menghan Yang <myuu@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464019}

--- ios/chrome/browser/autofill/ui_bundled/autofill_ui_constants.h
+++ ios/chrome/browser/autofill/ui_bundled/autofill_ui_constants.h
@@ -13,11 +13,6 @@ namespace autofill_ui_constants {
 // the UI does.
 const base::TimeDelta kSelectSuggestionDelay = base::Milliseconds(500);
 
-// The delay between showing the confirmation and dismissing the progress
-// dialog.
-const base::TimeDelta kProgressDialogConfirmationDismissDelay =
-    base::Seconds(1);
-
 }  // namespace autofill_ui_constants
 
 #endif  // IOS_CHROME_BROWSER_AUTOFILL_UI_BUNDLED_AUTOFILL_UI_CONSTANTS_H_
