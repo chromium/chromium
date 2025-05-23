69bcd1b6a85633d3ddb2c35268ced43873c6f0e8
Rune Lillesveen <futhark@chromium.org>
2025-05-23 09:07:33 -0700

    Use TreeCountingChecker for transform interpolations
    Bug: 415626999
    Change-Id: I1c07f4dd12ada1ae5d314e1d89ffb766e3856875
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578285
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464792}

--- third_party/blink/renderer/core/animation/css_transform_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_transform_interpolation_type.cc
@@ -9,6 +9,7 @@
 
 #include "third_party/blink/renderer/core/animation/interpolable_transform_list.h"
 #include "third_party/blink/renderer/core/animation/length_units_checker.h"
+#include "third_party/blink/renderer/core/animation/tree_counting_checker.h"
 #include "third_party/blink/renderer/core/animation/underlying_value_owner.h"
 #include "third_party/blink/renderer/core/css/css_function_value.h"
 #include "third_party/blink/renderer/core/css/css_primitive_value.h"
@@ -89,8 +90,11 @@ InterpolationValue CSSTransformInterpolationType::MaybeConvertValue(
     const CSSValue& value,
     const StyleResolverState& state,
     ConversionCheckers& conversion_checkers) const {
+  const CSSToLengthConversionData& conversion_data =
+      state.CssToLengthConversionData();
   if (auto* list_value = DynamicTo<CSSValueList>(value)) {
     CSSPrimitiveValue::LengthTypeFlags types;
+    bool needs_tree_counting_checker = false;
     for (const CSSValue* item : *list_value) {
       const auto& transform_function = To<CSSFunctionValue>(*item);
       if (transform_function.FunctionType() == CSSValueID::kMatrix ||
@@ -107,6 +111,12 @@ InterpolationValue CSSTransformInterpolationType::MaybeConvertValue(
         DCHECK(primitive_value ||
                (transform_function.FunctionType() == CSSValueID::kPerspective &&
                 argument->IsIdentifierValue()));
+        if (!primitive_value) {
+          continue;
+        }
+        if (primitive_value->IsElementDependent()) {
+          needs_tree_counting_checker = true;
+        }
         if (!primitive_value || (!primitive_value->IsLength() &&
                                  primitive_value->IsResolvableBeforeLayout())) {
           continue;
@@ -119,12 +129,14 @@ InterpolationValue CSSTransformInterpolationType::MaybeConvertValue(
             LengthUnitsChecker::MaybeCreate(types, state)) {
       conversion_checkers.push_back(length_units_checker);
     }
-    // TODO(crbug.com/415626999): Create a TreeCountingChecker for
-    // sibling-index() and sibling-count() if necessary.
+    if (needs_tree_counting_checker) {
+      conversion_checkers.push_back(
+          TreeCountingChecker::Create(conversion_data));
+    }
   }
 
   return InterpolationValue(InterpolableTransformList::ConvertCSSValue(
-      value, state.CssToLengthConversionData(),
+      value, conversion_data,
       TransformOperations::BoxSizeDependentMatrixBlending::kAllow));
 }
 
