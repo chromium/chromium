599add17ee44352908ec287e66ef7d604bebf535
Daniil Sakhapov <sakhapov@chromium.org>
2025-05-23 04:25:18 -0700

    Add length conversion checkers to CSSDynamicRangeLimitInterpolationType
    So that when the dynamic-range-limit changes during animation, we
    correctly update the relative-units dependent value.
    
    I wasn't able to write a test, as there are some normalizations and
    conditions on inputs, which I couldn't get right. But it shouldn't
    be different from the previous CLs.
    
    Bug: 415572412
    Change-Id: Ic34c0428dd4034d0924c1de63debb29bfc0a239f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578419
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464660}

--- third_party/blink/renderer/core/animation/css_dynamic_range_limit_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_dynamic_range_limit_interpolation_type.cc
@@ -6,6 +6,7 @@
 
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/animation/interpolable_dynamic_range_limit.h"
+#include "third_party/blink/renderer/core/animation/length_units_checker.h"
 #include "third_party/blink/renderer/core/css/resolver/style_builder_converter.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 
@@ -61,12 +62,15 @@ InterpolationValue CSSDynamicRangeLimitInterpolationType::MaybeConvertValue(
     const CSSValue& value,
     const StyleResolverState& state,
     ConversionCheckers& conversion_checkers) const {
-  // TODO(crbug.com/40946458): Relative units should not be resolved here, but
-  // that requires InterpolableDynamicRangeLimit to store calc-expressions.
   // TODO(crbug.com/415626999): Create a TreeCountingChecker for sibling-index()
-  // and sibling-count() if necessary.
-  // TODO(crbug.com/415572412): Create a LengthUnitsChecker for relative units
-  // if necessary.
+  if (auto* primitive_value = DynamicTo<CSSPrimitiveValue>(value)) {
+    CSSPrimitiveValue::LengthTypeFlags types;
+    primitive_value->AccumulateLengthUnitTypes(types);
+    if (InterpolationType::ConversionChecker* length_units_checker =
+            LengthUnitsChecker::MaybeCreate(types, state)) {
+      conversion_checkers.push_back(length_units_checker);
+    }
+  }
   return ConvertDynamicRangeLimit(
       StyleBuilderConverter::ConvertDynamicRangeLimit(state, value));
 }
