61ea6613e93855d2b12010060b8f7da6a79e4553
thefrog <thefrog@chromium.org>
2025-05-23 06:41:29 -0700

    Update test to focus on frame and call content::HandleMissingKeyWindow()
    This CL attempts to fix flaky tests that are failing with the message:
    ```
    NotAllowedError: Failed to execute 'writeText' on 'Clipboard': Document
    is not focused.
    ```
    
    This is done by calling Focus() on the frame and also by calling
    content::HandleMissingKeyWindow(), which was needed for a similar
    failure fixed in crrev.com/c/6221803.
    
    Bug: 418342665
    Change-Id: I26227808d0e963af1a836f303130d7f521eeaddb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579918
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1464705}

--- chrome/browser/safe_browsing/safe_browsing_navigation_observer_interactive_test.cc
+++ chrome/browser/safe_browsing/safe_browsing_navigation_observer_interactive_test.cc
@@ -14,6 +14,7 @@
 #include "components/prefs/pref_service.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "components/sessions/content/session_tab_helper.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/prerender_test_util.h"
@@ -103,6 +104,10 @@ class SBNavigationObserverBrowserTest : public InProcessBrowserTest {
       script_executing_frame =
           ChildFrameAt(script_executing_frame, subframe_index.value());
     }
+#if BUILDFLAG(IS_MAC)
+    content::HandleMissingKeyWindow();
+#endif
+    script_executing_frame->GetView()->Focus();
     std::string script = base::StringPrintf(
         "navigator.clipboard.writeText('%s');", urlToCopy.c_str());
     ASSERT_TRUE(content::ExecJs(script_executing_frame, script));
