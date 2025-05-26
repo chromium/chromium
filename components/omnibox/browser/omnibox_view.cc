254bc711794d7ad269495f3d419a209935b78cad
Ananya <ananyaseelam@google.com>
2025-05-26 08:57:59 -0700

    [M137] [omnibox] Show default search engine icon in omnibox
    Original change's description:
    > [omnibox] Show default search engine icon in omnibox
    > 
    > This CL fixes a regression that caused default search engine icons to
    > not be shown in the omnibox in the zero suggest state. Now, if a user
    > had previously navigated to the search engine, they should be able to
    > see the default search engine icon in the zero suggest state.
    > 
    > Before: https://screenshot.googleplex.com/3HmiaL39yrpTVqb
    > 
    > After (Google DSE): https://screenshot.googleplex.com/AAfp8nFs3rcrz26
    > After (Yahoo DSE): https://screenshot.googleplex.com/BtTGuUJPUatPoG7
    > 
    > Bug: 416753300
    > Change-Id: I0961ee676dbc3330066382a0aba93529a3bf93a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579564
    > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1464222}
    
    Bug: 419879525,416753300
    Change-Id: I0961ee676dbc3330066382a0aba93529a3bf93a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6580470
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7151@{#1757}
    Cr-Branched-From: 8e0d32ed6e49a2415b16e5ed402957cac2349ce2-refs/heads/main@{#1453031}

--- components/omnibox/browser/omnibox_view.cc
+++ components/omnibox/browser/omnibox_view.cc
@@ -233,8 +233,14 @@ ui::ImageModel OmniboxView::GetIcon(int dip_size,
             controller_->client()->GetSizedIcon(bitmap));
       }
     }
-    favicon = controller_->client()->GetFaviconForKeywordSearchProvider(
-        turl, std::move(on_icon_fetched));
+
+    if (!match.keyword.empty()) {
+      favicon = controller_->client()->GetFaviconForKeywordSearchProvider(
+          turl, std::move(on_icon_fetched));
+    } else {
+      favicon = controller_->client()->GetFaviconForDefaultSearchProvider(
+          std::move(on_icon_fetched));
+    }
   } else if (match.type != AutocompleteMatchType::HISTORY_CLUSTER) {
     // The starter pack suggestions are a unique case. These suggestions
     // normally use a favicon image that cannot be styled further by client
