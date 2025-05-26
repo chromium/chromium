171e1b9fed66bf0f4dff45ea5bdddce98f69c40a
Nohemi Fernandez <fernandex@google.com>
2025-05-26 02:50:57 -0700

    [iOS] Fix bug in side-swipe snapshot for overlays.
    Side-swiping between web views captures a snapshot synchronously prior
    to displaying the transition between tabs side by side. However, because
    the snapshotting action is asynchronous the snapshot may not have been
    captured prior to display. This is most obvious in overlays since the
    snapshot is captured on page loaded and the overlay is added by a user
    action after this.
    
    Bug: 417956837
    Change-Id: Iffeed00f309f8017229b1caef7c16f3282ce80d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6578390
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1465418}

--- ios/chrome/browser/side_swipe/ui_bundled/side_swipe_mediator.mm
+++ ios/chrome/browser/side_swipe/ui_bundled/side_swipe_mediator.mm
@@ -10,6 +10,7 @@
 #import "base/ios/block_types.h"
 #import "base/memory/raw_ptr.h"
 #import "base/scoped_observation.h"
+#import "base/timer/timer.h"
 #import "components/feature_engagement/public/event_constants.h"
 #import "components/feature_engagement/public/tracker.h"
 #import "ios/chrome/browser/fullscreen/ui_bundled/scoped_fullscreen_disabler.h"
@@ -26,6 +27,13 @@
 #import "ios/web/public/navigation/navigation_item.h"
 #import "ios/web/public/web_state_observer_bridge.h"
 
+namespace {
+
+// The timeout used for updating snapshots following a side-swipe gesture.
+constexpr base::TimeDelta kUpdateSnapshotTimeout = base::Milliseconds(100);
+
+}  // namespace
+
 @interface SideSwipeMediator () <CRWWebStateObserver,
                                  UIGestureRecognizerDelegate,
                                  WebStateListObserving> {
@@ -41,6 +49,10 @@
 
   // The webStateList owned by the current browser.
   raw_ptr<WebStateList> _webStateList;
+
+  // Timer to ensure that snapshot updates that take longer than a maximum
+  // delay will release their callback even if the update is incomplete.
+  base::OneShotTimer _snapshotTimer;
 }
 
 // The current active WebState.
@@ -213,9 +225,36 @@
   return _webStateList->active_index();
 }
 
-- (void)updateActiveTabSnapshot {
-  SnapshotTabHelper::FromWebState(self.activeWebState)
-      ->UpdateSnapshotWithCallback(nil);
+- (void)updateActiveTabSnapshot:(ProceduralBlock)callback {
+  SnapshotTabHelper* snapshotTabHelper =
+      SnapshotTabHelper::FromWebState(self.activeWebState);
+  if (!snapshotTabHelper) {
+    [self runSnapshotCallback:callback];
+    return;
+  }
+  __weak __typeof(self) weakSelf = self;
+  _snapshotTimer.Start(FROM_HERE, kUpdateSnapshotTimeout, base::BindOnce(^{
+                         [weakSelf runSnapshotCallback:callback];
+                       }));
+  snapshotTabHelper->UpdateSnapshotWithCallback(^(UIImage* image) {
+    [weakSelf onSnapshotUpdated:callback];
+  });
+}
+
+- (void)runSnapshotCallback:(ProceduralBlock)callback {
+  if (callback) {
+    callback();
+  }
+}
+
+- (void)onSnapshotUpdated:(ProceduralBlock)callback {
+  if (!_snapshotTimer.IsRunning()) {
+    // If the timer is not running, then the callback was already called.
+    return;
+  }
+  // Otherwise, timer should be stopped and callback should be called.
+  _snapshotTimer.Stop();
+  [self runSnapshotCallback:callback];
 }
 
 - (int)tabCount {
