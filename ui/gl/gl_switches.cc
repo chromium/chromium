140a60fe1458400abf5219dcf2fc674b3293c1af
Shahbaz Youssefi <syoussefi@chromium.org>
2025-05-26 13:14:06 -0700

    Enable ANGLE/Vulkan on Samsung GPUs unconditionally
    Data confirms that going over ANGLE twice is inefficient (surprise):
    
        https://uma.googleplex.com/p/chrome/variations?sid=a924377b487c728b045719c8da0d2a3f
    
    Unconditionally enable ANGLE/Vulkan on Samsung devices that are known to
    have ANGLE as the GLES driver.
    
    Bug: 371512561
    Change-Id: Ic14cdad06887808e007d719afc22ee6b7140f738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6588973
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Auto-Submit: Shahbaz Youssefi <syoussefi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1465638}

--- ui/gl/gl_switches.cc
+++ ui/gl/gl_switches.cc
@@ -349,9 +349,17 @@ bool IsDefaultANGLEVulkan() {
     return false;
 
 #if BUILDFLAG(IS_ANDROID)
+  // Samsung GPUs already use ANGLE as the GLES driver.  Always choose
+  // ANGLE/Vulkan on these GPUs to avoid the inefficiencies of translating
+  // over ANGLE twice.
+  if (active_gpu.driverId == VK_DRIVER_ID_SAMSUNG_PROPRIETARY) {
+    return true;
+  }
+
   // Exclude SwiftShader-based Android emulators for now.
-  if (active_gpu.driverId == VK_DRIVER_ID_GOOGLE_SWIFTSHADER)
+  if (active_gpu.driverId == VK_DRIVER_ID_GOOGLE_SWIFTSHADER) {
     return false;
+  }
 
   // Encountered bugs with older Imagination drivers.  New drivers seem fixed,
   // but disabled for the sake of experiment for now. crbug.com/371512561
