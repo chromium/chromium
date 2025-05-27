100e2d2894be32057b47379b026d1a6b3f4be3fd
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-05-27 15:41:52 -0700

    [M138] Revert "Revert "[LNA] rename fetch targetAddressSpace options""
    This reverts commit c088885ba810847bbd8f35fe039144ebefd39148.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/6225501782802432
    
    Sample build with failed test: https://ci.chromium.org/b/8713760342038285425
    Affected test(s):
    [ninja://chrome/test:browser_tests/LocalNetworkAccessBrowserTest.CheckPrivateAliasFeatureCounterLocalNotCounted](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:browser_tests%2FLocalNetworkAccessBrowserTest.CheckPrivateAliasFeatureCounterLocalNotCounted?q=VHash%3A1b090004a0f6be54)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F6225501782802432&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6588736&type=BUG
    
    Original change's description:
    > Revert "[LNA] rename fetch targetAddressSpace options"
    >
    > This reverts commit d3158d4e53f9c6bcf080da32eb3ab03b5fbe748c.
    >
    > Reason for revert: Chrome build gardener: It caused new failing tests b/420455025
    >
    > Bug: 418737577
    > Original change's description:
    > > [LNA] rename fetch targetAddressSpace options
    > >
    > > Rename the targetAddressSpace args based on LNA's naming. Mapping of PNA
    > > names -> LNA names:
    > >
    > > PNA     | LNA
    > > -------------------
    > > local   | loopback
    > > private | local
    > > public  | public
    > >
    > > keep private as an alias for local.
    > >
    > > Removing
    > > private-network-access/mixed-content-fetch.tentative.https.window.js as
    > > PNA is being removed and its not straightforward to keep this test
    > > working with the rename.
    > >
    > > LNA = Local Network Access = new hotness
    > > PNA = Private Network Access = old and busted
    > >
    > > Bug: 418737577
    > > Change-Id: I195a63e90a1300e58c40761069daad00b3680696
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6564545
    > > Reviewed-by: Chris Thompson <cthomp@chromium.org>
    > > Reviewed-by: Adam Rice <ricea@chromium.org>
    > > Commit-Queue: Hubert Chao <hchao@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1465461}
    >
    > Bug: 418737577
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I60ab2127ac0f54d24290234ebd1bb781c4ffc1e9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6588736
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Adam Rice <ricea@chromium.org>
    > Reviewed-by: Adam Rice <ricea@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1465696}
    >
    
    (cherry picked from commit 18051f32e5a546deac7b883e686acd6abd93dbce)
    
    Bug: 418737577
    Bug: 418737577
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I05b4ae8a803cc0098a9f4f8c996ddc59422b7a36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6589416
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1465790}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6593635
    Owners-Override: Tommy Steimel <steimel@google.com>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7204@{#13}
    Cr-Branched-From: d5de512dc9dc8ddfe4e6d71b0637578bb6158683-refs/heads/main@{#1465706}

--- third_party/blink/renderer/core/fetch/request.cc
+++ third_party/blink/renderer/core/fetch/request.cc
@@ -588,8 +588,16 @@ Request* Request::CreateRequestWithRequestOrString(
   // - "Let |targetAddressSpace| be |init|'s targetAddressSpace member if it is
   // present, and |unknown| otherwise."
   if (init->hasTargetAddressSpace()) {
-    if (init->targetAddressSpace() == "local") {
+    // 'private' is kept as an alias to 'local'; the previous PNA spec had
+    // 'private' for what LNA considers to be 'local'.
+    //
+    // TODO(crbug.com/418737577): Public names don't match
+    // network::mojom::IPAddressSpace enum yet. Finish rename by changing the
+    // enum.
+    if (init->targetAddressSpace() == "loopback") {
       request->SetTargetAddressSpace(network::mojom::IPAddressSpace::kLocal);
+    } else if (init->targetAddressSpace() == "local") {
+      request->SetTargetAddressSpace(network::mojom::IPAddressSpace::kPrivate);
     } else if (init->targetAddressSpace() == "private") {
       UseCounter::Count(execution_context,
                         WebFeature::kLocalNetworkAccessPrivateAliasUse);
@@ -1183,9 +1191,9 @@ bool Request::keepalive() const {
 V8IPAddressSpace Request::targetAddressSpace() const {
   switch (request_->TargetAddressSpace()) {
     case network::mojom::IPAddressSpace::kLocal:
-      return V8IPAddressSpace(V8IPAddressSpace::Enum::kLocal);
+      return V8IPAddressSpace(V8IPAddressSpace::Enum::kLoopback);
     case network::mojom::IPAddressSpace::kPrivate:
-      return V8IPAddressSpace(V8IPAddressSpace::Enum::kPrivate);
+      return V8IPAddressSpace(V8IPAddressSpace::Enum::kLocal);
     case network::mojom::IPAddressSpace::kPublic:
       return V8IPAddressSpace(V8IPAddressSpace::Enum::kPublic);
     case network::mojom::IPAddressSpace::kUnknown:
