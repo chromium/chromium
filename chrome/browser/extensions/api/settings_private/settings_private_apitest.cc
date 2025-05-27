01db1155058317cafa1d7c5f36a199d333fc841a
Hidehiko Abe <hidehiko@chromium.org>
2025-05-27 10:15:43 -0700

    Stabilize SettingsPrivateApiTest.OnPrefsChanged_CrOSSetting
    Setting value is ignored if owner status is not yet ready.
    This CL waits for the set up of owner status.
    
    BUG=413199195, 412375846
    TEST=Tryjob. Run locally repeated ~30 mins. (Used to be failed once
         in 10 mins or so)
    
    Change-Id: I1b8eccbc75b08261456b8a386af4b1c7c4df988a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6586018
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466015}

--- chrome/browser/extensions/api/settings_private/settings_private_apitest.cc
+++ chrome/browser/extensions/api/settings_private/settings_private_apitest.cc
@@ -8,6 +8,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "base/task/current_thread.h"
+#include "base/test/test_future.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -32,6 +33,11 @@
 #include "extensions/common/switches.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chrome/browser/ash/ownership/owner_settings_service_ash.h"
+#include "chrome/browser/ash/ownership/owner_settings_service_ash_factory.h"
+#endif
 
 using testing::Mock;
 using testing::Return;
@@ -58,6 +64,18 @@ class SettingsPrivateApiTest : public ExtensionApiTest,
     policy::BrowserPolicyConnector::SetPolicyProviderForTesting(&provider_);
     ExtensionApiTest::SetUpInProcessBrowserTestFixture();
   }
+
+#if BUILDFLAG(IS_CHROMEOS)
+  void SetUpOnMainThread() override {
+    ExtensionApiTest::SetUpOnMainThread();
+
+    auto* owner_settings_service =
+        ash::OwnerSettingsServiceAshFactory::GetForBrowserContext(profile());
+    base::test::TestFuture<bool> future;
+    owner_settings_service->IsOwnerAsync(future.GetCallback());
+    ASSERT_TRUE(future.Get());
+  }
+#endif
 
  protected:
   void SetUpCommandLine(base::CommandLine* command_line) override {
