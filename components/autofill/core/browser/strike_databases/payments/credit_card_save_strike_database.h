83fd9951774c268fd68aca559850a9be3a04dbde
Christoph Schwering <schwering@google.com>
2025-05-27 23:51:23 -0700

    [Autofill] IWYU in some files
    - No need to explicitly initialize base::Optional to base::nullopt.
    - Include what you use for base/ and STL headers.
    
    Fixed: 40763790
    Change-Id: I4cb1a559240e7af65840222d18a26316ea4e5709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6586550
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1466316}

--- components/autofill/core/browser/strike_databases/payments/credit_card_save_strike_database.h
+++ components/autofill/core/browser/strike_databases/payments/credit_card_save_strike_database.h
@@ -5,6 +5,12 @@
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_STRIKE_DATABASES_PAYMENTS_CREDIT_CARD_SAVE_STRIKE_DATABASE_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_STRIKE_DATABASES_PAYMENTS_CREDIT_CARD_SAVE_STRIKE_DATABASE_H_
 
+#include <stddef.h>
+
+#include <optional>
+#include <string_view>
+
+#include "base/time/time.h"
 #include "components/autofill/core/browser/strike_databases/simple_autofill_strike_database.h"
 #include "components/autofill/core/browser/strike_databases/strike_database.h"
 
@@ -12,9 +18,8 @@ namespace autofill {
 
 struct CreditCardSaveStrikeDatabaseTraits {
   static constexpr std::string_view kName = "CreditCardSave";
-  static constexpr std::optional<size_t> kMaxStrikeEntities = std::nullopt;
-  static constexpr std::optional<size_t> kMaxStrikeEntitiesAfterCleanup =
-      std::nullopt;
+  static constexpr std::optional<size_t> kMaxStrikeEntities;
+  static constexpr std::optional<size_t> kMaxStrikeEntitiesAfterCleanup;
   static constexpr size_t kMaxStrikeLimit = 3;
   static constexpr base::TimeDelta kExpiryTimeDelta = base::Days(183);
   static constexpr bool kUniqueIdRequired = true;
--- components/autofill/core/browser/strike_databases/payments/cvc_storage_strike_database.h
+++ components/autofill/core/browser/strike_databases/payments/cvc_storage_strike_database.h
@@ -5,15 +5,20 @@
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_STRIKE_DATABASES_PAYMENTS_CVC_STORAGE_STRIKE_DATABASE_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_STRIKE_DATABASES_PAYMENTS_CVC_STORAGE_STRIKE_DATABASE_H_
 
+#include <stddef.h>
+
+#include <optional>
+#include <string_view>
+
+#include "base/time/time.h"
 #include "components/autofill/core/browser/strike_databases/simple_autofill_strike_database.h"
 
 namespace autofill {
 
 struct CvcStorageStrikeDatabaseTraits {
   static constexpr std::string_view kName = "CvcStorage";
-  static constexpr std::optional<size_t> kMaxStrikeEntities = std::nullopt;
-  static constexpr std::optional<size_t> kMaxStrikeEntitiesAfterCleanup =
-      std::nullopt;
+  static constexpr std::optional<size_t> kMaxStrikeEntities;
+  static constexpr std::optional<size_t> kMaxStrikeEntitiesAfterCleanup;
   static constexpr size_t kMaxStrikeLimit = 3;
   static constexpr base::TimeDelta kExpiryTimeDelta = base::Days(183);
   static constexpr bool kUniqueIdRequired = true;
