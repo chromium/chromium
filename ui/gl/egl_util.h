f4d52252c2dd5744079f4a33b3b2bebf4e6cdad8
Shunya Shishido <sisidovski@chromium.org>
2025-05-27 23:17:15 -0700

    Revert "[WebGLOnWebGPU] Create the ANGLE display and context"
    This reverts commit b8187c6588640a066cfb6d1e7bbc939572252232.
    
    Reason for revert: Tests are failing on Linux CFI.
    
    Bug: 413078308
    Original change's description:
    > [WebGLOnWebGPU] Create the ANGLE display and context
    >
    > Set up the default framebuffer with an EGLImage of the webgpu swap chain
    > texture.
    >
    > Bug: 413078308
    > Change-Id: I7f47c564c2ddfa43b0269d7ed13d4cf5c1cde611
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6586715
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Geoff Lang <geofflang@chromium.org>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1465974}
    
    Bug: 413078308
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 420793500
    Change-Id: I073bf85f4de09130aa73202df3db2062ba0f0c2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6596169
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Owners-Override: Shunya Shishido <sisidovski@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466310}

--- ui/gl/egl_util.h
+++ ui/gl/egl_util.h
@@ -7,7 +7,6 @@
 
 #include <stdint.h>
 
-#include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_export.h"
 
 namespace ui {
@@ -16,16 +15,6 @@ GL_EXPORT const char* GetEGLErrorString(uint32_t error);
 
 // Returns the last EGL error as a string.
 GL_EXPORT const char* GetLastEGLErrorString();
-
-GL_EXPORT void EGLAPIENTRY LogEGLDebugMessage(EGLenum error,
-                                              const char* command,
-                                              EGLint message_type,
-                                              EGLLabelKHR thread_label,
-                                              EGLLabelKHR object_label,
-                                              const char* message);
-
-GL_EXPORT void SetEGLDebugCallback(const gl::DriverEGL& egl,
-                                   EGLDEBUGPROCKHR callback);
 
 }  // namespace ui
 
