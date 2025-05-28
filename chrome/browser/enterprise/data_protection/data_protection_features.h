f23e88eb11b64902c610b979fef4c1d181c979fa
Nasser Al-shawwa <alshawwa@chromium.org>
2025-05-28 08:40:53 -0700

    Add EnableSinglePageAppDataProtection finch flag
    Change-Id: I48a5ff09a0e62fbd7cd8dd60dec5d43d57725295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6594453
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466500}

---
+++ chrome/browser/enterprise/data_protection/data_protection_features.h
@@ -0,0 +1,16 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ENTERPRISE_DATA_PROTECTION_DATA_PROTECTION_FEATURES_H_
+#define CHROME_BROWSER_ENTERPRISE_DATA_PROTECTION_DATA_PROTECTION_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace enterprise_data_protection {
+
+BASE_DECLARE_FEATURE(kEnableSinglePageAppDataProtection);
+
+}  // namespace enterprise_data_protection
+
+#endif  // CHROME_BROWSER_ENTERPRISE_DATA_PROTECTION_DATA_PROTECTION_FEATURES_H_
