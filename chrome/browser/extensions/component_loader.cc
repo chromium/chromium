84d48dc0d896887d9d9e2a113a8a63f389ef9d90
Xiyuan Xia <xiyuan@chromium.org>
2025-05-28 15:37:44 -0700

    extensions: Follow symlink for component extensions on cros
    Allow component extensions to use symblinks to share data on ChromeOS.
    The current need is for migrating GoogleTTS component extension to MV3.
    The extension has about 90MB voice data and is bundled in rootfs. Rootfs
    size has a limit so it is better to share the data.
    
    Bug: 419866422
    Change-Id: Ib457d605189cabdc4753677a0acfcc319a8873e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598518
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466758}

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -458,6 +458,13 @@ scoped_refptr<const Extension> ComponentLoader::CreateExtension(
   // TODO(abarth): We should REQUIRE_MODERN_MANIFEST_VERSION once we've updated
   //               our component extensions to the new manifest version.
   int flags = Extension::REQUIRE_KEY;
+
+#if BUILDFLAG(IS_CHROMEOS)
+  // ChromeOS component extension (GoogleTTS) needs to use symlinks to share
+  // data during MV2 to MV3 migration.
+  flags |= Extension::FOLLOW_SYMLINKS_ANYWHERE;
+#endif
+
   return Extension::Create(info.root_directory,
                            mojom::ManifestLocation::kComponent, info.manifest,
                            flags, utf8_error);
