6250b7df5e22c9be2736be54eb5a8d1af0b01bc8
Caroline Rising <corising@chromium.org>
2025-05-28 11:28:13 -0700

    [SxS] Add tooltip to minitoolbar alert status indicator.
    Bug: 403350373
    Change-Id: I37c721ba7f47dc4c8bd6d9d8bd31aa571a64e98d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6593893
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1466621}

--- chrome/browser/ui/views/frame/multi_contents_view_mini_toolbar.cc
+++ chrome/browser/ui/views/frame/multi_contents_view_mini_toolbar.cc
@@ -15,6 +15,7 @@
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/tabs/split_tab_menu_model.h"
 #include "chrome/browser/ui/tabs/tab_renderer_data.h"
+#include "chrome/browser/ui/tabs/tab_utils.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/contents_web_view.h"
 #include "chrome/browser/ui/views/frame/top_container_background.h"
@@ -274,8 +275,11 @@ void MultiContentsViewMiniToolbar::OnAlertStatusIndicatorChanged(
         GetAlertStatusColor(new_alert.value(), GetColorProvider());
     alert_state_indicator_->SetImage(
         tabs::GetAlertImageModel(new_alert.value(), color));
+    alert_state_indicator_->SetTooltipText(
+        GetTabAlertStateText(new_alert.value()));
   } else {
     alert_state_indicator_->SetImage(ui::ImageModel());
+    alert_state_indicator_->SetTooltipText(std::u16string());
   }
 }
 
