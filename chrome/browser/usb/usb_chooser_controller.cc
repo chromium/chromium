c5bcfa02850e7cbd15ca4a061ba80991f3cae0c8
Alvin Ji <alvinji@chromium.org>
2025-05-29 13:20:51 -0700

    [CfM-R132] [M132-LTS] usb: Use GlobalRenderFrameHostId in UsbChooserController
    The UsbChooserController currently holds a raw pointer to the requesting
    RenderFrameHost. This can lead to use-after-free issues if the
    RenderFrameHost is destroyed before the chooser controller. This CL
    replaces the raw pointer with a `GlobalRenderFrameHostId`. This ID can
    be used to retrieve the RenderFrameHost when needed, and checks are
    added to ensure the RenderFrameHost is still valid before accessing it.
    
    (cherry picked from commit 0333ecde91425e518cd898614c7b018209a18511)
    
    (cherry picked from commit 1b0a64d3da587531cc4c00fe72acb7a017e69c42)
    
    Bug: 405292639
    Change-Id: Ifedaf80f6700d57ea28691abfaf4d2ff9cdbb448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6440254
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Original-Original-Commit-Position: refs/heads/main@{#1444224}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6450383
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Mohamed Omar <mohamedaomar@google.com>
    Owners-Override: Mohamed Omar <mohamedaomar@google.com>
    Commit-Queue: Gyuyoung Kim (xWF) <qkim@google.com>
    Cr-Original-Commit-Position: refs/branch-heads/6834@{#5555}
    Cr-Original-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}
    Signed-off-by: Kyle Williams <kdgwill@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6604447
    Owners-Override: Kyle Williams <kdgwill@chromium.org>
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Niko Tsirakis <ntsirakis@google.com>
    Auto-Submit: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/branch-heads/6834_160@{#76}
    Cr-Branched-From: cdae089eab830291f81deb011febbbdc520a019e-refs/branch-heads/6834@{#4409}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- chrome/browser/usb/usb_chooser_controller.cc
+++ chrome/browser/usb/usb_chooser_controller.cc
@@ -99,8 +99,8 @@ UsbChooserController::UsbChooserController(
           CreateChooserTitle(render_frame_host, IDS_USB_DEVICE_CHOOSER_PROMPT)),
       options_(std::move(options)),
       callback_(std::move(callback)),
-      requesting_frame_(render_frame_host) {
-  RenderFrameHost* main_frame = requesting_frame_->GetMainFrame();
+      render_frame_host_id_(render_frame_host->GetGlobalId()) {
+  RenderFrameHost* main_frame = render_frame_host->GetMainFrame();
   origin_ = main_frame->GetLastCommittedOrigin();
   Profile* profile =
       Profile::FromBrowserContext(main_frame->GetBrowserContext());
@@ -202,7 +202,15 @@ void UsbChooserController::Cancel() {
 void UsbChooserController::Close() {}
 
 void UsbChooserController::OpenHelpCenterUrl() const {
-  WebContents::FromRenderFrameHost(requesting_frame_)
+  content::RenderFrameHost* render_frame_host =
+      content::RenderFrameHost::FromID(render_frame_host_id_);
+  if (!render_frame_host) {
+    // When |render_frame_host| is not valid anymore we don't want to open help
+    // center url.
+    return;
+  }
+
+  WebContents::FromRenderFrameHost(render_frame_host)
       ->OpenURL(content::OpenURLParams(
                     GURL(chrome::kChooserUsbOverviewURL), content::Referrer(),
                     WindowOpenDisposition::NEW_FOREGROUND_TAB,
@@ -266,6 +274,14 @@ void UsbChooserController::GotUsbDeviceList(
 
 bool UsbChooserController::DisplayDevice(
     const device::mojom::UsbDeviceInfo& device_info) const {
+  content::RenderFrameHost* render_frame_host =
+      content::RenderFrameHost::FromID(render_frame_host_id_);
+  if (!render_frame_host) {
+    // When |render_frame_host| is not valid anymore we don't want to display
+    // any device information.
+    return false;
+  }
+
   if (!device::UsbDeviceFilterMatchesAny(options_->filters, device_info)) {
     return false;
   }
@@ -280,10 +296,9 @@ bool UsbChooserController::DisplayDevice(
   bool is_usb_unrestricted = false;
   if (base::FeatureList::IsEnabled(blink::features::kUnrestrictedUsb)) {
     is_usb_unrestricted =
-        requesting_frame_ &&
-        requesting_frame_->IsFeatureEnabled(
+        render_frame_host->IsFeatureEnabled(
             blink::mojom::PermissionsPolicyFeature::kUsbUnrestricted) &&
-        content::HasIsolatedContextCapability(requesting_frame_);
+        content::HasIsolatedContextCapability(render_frame_host);
   }
   // Isolated context with permission to access the policy-controlled feature
   // "usb-unrestricted" can bypass the USB blocklist.
