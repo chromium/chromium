62fcfaca2d91e67cbaef2e31cb2811c431769936
Di Zhang <dizhangg@chromium.org>
2025-05-29 16:03:56 -0700

    [menu] Make menuitem focusable and can invoke popover menulist
    This patch continues our implementation of HTMLMenuItemElement.
    
    We add functions to find the owner menubar or menulist:
    - OwnerMenuBarElement()
    - OwnerMenuListElement()
    - InsertedInto()
    - RemovedFrom()
    
    We add DefaultTabIndex(), SupportsFocus(), IsKeyboardFocusableSlow() and
    ShouldHaveFocusAppearance() so menuitem can receive focus by default.
    
    We add commandForElement(), command(), setCommand() and
    GetCommandEventType() methods to HTMLMenuItemElement so menuitem can
    toggle popover even if it is not a button.
    
    Change-Id: Ia401bd4a77dcfd0575240d2d53b7d1ecd8327012
    Bug: 406566432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6579596
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467233}

--- third_party/blink/renderer/core/html/html_menu_item_element.cc
+++ third_party/blink/renderer/core/html/html_menu_item_element.cc
@@ -4,17 +4,16 @@
 
 #include "third_party/blink/renderer/core/html/html_menu_item_element.h"
 
+#include "third_party/blink/renderer/core/css/selector_checker.h"
 #include "third_party/blink/renderer/core/dom/document.h"
-#include "third_party/blink/renderer/core/dom/shadow_root.h"
-#include "third_party/blink/renderer/core/events/keyboard_event.h"
-#include "third_party/blink/renderer/core/events/mouse_event.h"
-#include "third_party/blink/renderer/core/html/html_slot_element.h"
-#include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
-#include "third_party/blink/renderer/core/html/shadow/shadow_element_names.h"
+#include "third_party/blink/renderer/core/dom/events/event.h"
+#include "third_party/blink/renderer/core/events/command_event.h"
+#include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/html/html_menu_bar_element.h"
+#include "third_party/blink/renderer/core/html/html_menu_list_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
-#include "third_party/blink/renderer/core/layout/layout_theme.h"
-#include "third_party/blink/renderer/core/style/computed_style.h"
-#include "third_party/blink/renderer/platform/keyboard_codes.h"
+#include "third_party/blink/renderer/core/keywords.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -22,6 +21,12 @@ HTMLMenuItemElement::HTMLMenuItemElement(Document& document)
     : HTMLElement(html_names::kMenuitemTag, document), is_checked_(false) {}
 
 HTMLMenuItemElement::~HTMLMenuItemElement() = default;
+
+void HTMLMenuItemElement::Trace(Visitor* visitor) const {
+  visitor->Trace(nearest_ancestor_menu_bar_);
+  visitor->Trace(nearest_ancestor_menu_list_);
+  HTMLElement::Trace(visitor);
+}
 
 bool HTMLMenuItemElement::MatchesDefaultPseudoClass() const {
   return FastHasAttribute(html_names::kCheckedAttr);
@@ -50,6 +55,59 @@ void HTMLMenuItemElement::ParseAttribute(
   }
 }
 
+Element* HTMLMenuItemElement::commandForElement() const {
+  if (!IsInTreeScope() || IsDisabledFormControl()) {
+    return nullptr;
+  }
+
+  return GetElementAttributeResolvingReferenceTarget(
+      html_names::kCommandforAttr);
+}
+
+void HTMLMenuItemElement::setCommand(const AtomicString& type) {
+  setAttribute(html_names::kCommandAttr, type);
+}
+
+AtomicString HTMLMenuItemElement::command() const {
+  const AtomicString& action = FastGetAttribute(html_names::kCommandAttr);
+  CommandEventType type = GetCommandEventType(action);
+  switch (type) {
+    case CommandEventType::kNone:
+      return g_empty_atom;
+    case CommandEventType::kCustom:
+      return action;
+    default: {
+      const AtomicString& lower_action = action.LowerASCII();
+      DCHECK_EQ(GetCommandEventType(lower_action), type);
+      return lower_action;
+    }
+  }
+}
+
+CommandEventType HTMLMenuItemElement::GetCommandEventType(
+    const AtomicString& action) const {
+  if (action.IsNull() || action.empty()) {
+    return CommandEventType::kNone;
+  }
+
+  // Custom Invoke Action
+  if (action.StartsWith("--")) {
+    return CommandEventType::kCustom;
+  }
+
+  // Popover cases.
+  if (EqualIgnoringASCIICase(action, keywords::kTogglePopover)) {
+    return CommandEventType::kTogglePopover;
+  }
+  if (EqualIgnoringASCIICase(action, keywords::kShowPopover)) {
+    return CommandEventType::kShowPopover;
+  }
+  if (EqualIgnoringASCIICase(action, keywords::kHidePopover)) {
+    return CommandEventType::kHidePopover;
+  }
+  return CommandEventType::kNone;
+}
+
 bool HTMLMenuItemElement::Checked() const {
   return is_checked_;
 }
@@ -74,9 +132,100 @@ bool HTMLMenuItemElement::IsDisabledFormControl() const {
   return FastHasAttribute(html_names::kDisabledAttr);
 }
 
+FocusableState HTMLMenuItemElement::SupportsFocus(UpdateBehavior) const {
+  return IsDisabledFormControl() ? FocusableState::kNotFocusable
+                                 : FocusableState::kFocusable;
+}
+
+bool HTMLMenuItemElement::IsKeyboardFocusableSlow(
+    UpdateBehavior update_behavior) const {
+  // Interest invoker targets with partial interest aren't keyboard focusable.
+  if (IsInPartialInterestPopover()) {
+    CHECK(RuntimeEnabledFeatures::HTMLInterestTargetAttributeEnabled(
+        GetDocument().GetExecutionContext()));
+    return false;
+  }
+  // Menuitems are keyboard focusable if they are focusable and don't have a
+  // negative tabindex set.
+  return IsFocusable(update_behavior) && tabIndex() >= 0;
+}
+
+int HTMLMenuItemElement::DefaultTabIndex() const {
+  // Menuitems in menulist should be traversed using arrow keys and not tabbing.
+  if (nearest_ancestor_menu_list_) {
+    return -1;
+  }
+  return 0;
+}
+
+bool HTMLMenuItemElement::ShouldHaveFocusAppearance() const {
+  return SelectorChecker::MatchesFocusVisiblePseudoClass(*this);
+}
+
 void HTMLMenuItemElement::DefaultEventHandler(Event& event) {
   // TODO: Handle activation behavior events.
+  // TODO: Handle arrow key navigation for menuitems in menulist.
+  if (event.type() == event_type_names::kDOMActivate) {
+    // Menuitems with a commandfor will dispatch a CommandEvent on the
+    // invoker, and run HandleCommandInternal to perform default logic.
+    if (auto* command_target = commandForElement()) {
+      auto action =
+          GetCommandEventType(FastGetAttribute(html_names::kCommandAttr));
+      bool is_valid_builtin =
+          command_target->IsValidBuiltinCommand(*this, action);
+      bool should_dispatch =
+          is_valid_builtin || action == CommandEventType::kCustom;
+      if (should_dispatch) {
+        Event* commandEvent =
+            CommandEvent::Create(event_type_names::kCommand, command(), this);
+        command_target->DispatchEvent(*commandEvent);
+        if (is_valid_builtin && !commandEvent->defaultPrevented()) {
+          command_target->HandleCommandInternal(*this, action);
+        }
+      }
+      return;
+    }
+  }
+
+  if (HandleKeyboardActivation(event)) {
+    return;
+  }
   HTMLElement::DefaultEventHandler(event);
+}
+
+HTMLMenuBarElement* HTMLMenuItemElement::OwnerMenuBarElement() const {
+  return nearest_ancestor_menu_bar_;
+}
+
+HTMLMenuListElement* HTMLMenuItemElement::OwnerMenuListElement() const {
+  return nearest_ancestor_menu_list_;
+}
+
+void HTMLMenuItemElement::ResetNearestAncestorMenuBarOrMenuList() {
+  nearest_ancestor_menu_bar_ = nullptr;
+  nearest_ancestor_menu_list_ = nullptr;
+  for (Node& ancestor : NodeTraversal::AncestorsOf(*this)) {
+    if (auto* menu_bar = DynamicTo<HTMLMenuBarElement>(ancestor)) {
+      nearest_ancestor_menu_bar_ = menu_bar;
+      break;
+    } else if (auto* menu_list = DynamicTo<HTMLMenuListElement>(ancestor)) {
+      nearest_ancestor_menu_list_ = menu_list;
+      break;
+    }
+  }
+}
+
+Node::InsertionNotificationRequest HTMLMenuItemElement::InsertedInto(
+    ContainerNode& insertion_point) {
+  auto return_value = HTMLElement::InsertedInto(insertion_point);
+  ResetNearestAncestorMenuBarOrMenuList();
+  return return_value;
+}
+
+void HTMLMenuItemElement::RemovedFrom(ContainerNode& insertion_point) {
+  HTMLElement::RemovedFrom(insertion_point);
+  ResetNearestAncestorMenuBarOrMenuList();
+  return;
 }
 
 }  // namespace blink
