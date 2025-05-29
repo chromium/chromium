70d05a4ce781541dfe9be4f693c115f845994f5f
Avi Drissman <avi@chromium.org>
2025-05-29 08:19:23 -0700

    Remove code supporting macOS 11 and earlier from //base
    Bug: 408364834
    Change-Id: I10086fb30da2ffd159d0bc47b04199fea5ea2ad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598022
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467006}

--- base/mac/launch_application_unittest.mm
+++ base/mac/launch_application_unittest.mm
@@ -6,7 +6,6 @@
 
 #include <sys/select.h>
 
-#include "base/apple/bridging.h"
 #include "base/apple/foundation_util.h"
 #include "base/base_paths.h"
 #include "base/compiler_specific.h"
@@ -166,15 +165,8 @@ class LaunchApplicationTest : public testing::Test {
     // little bit of time for this to happen, and some tests might fail if the
     // app wasn't registered yet.
     while (true) {
-      NSArray<NSURL*>* apps = nil;
-      if (@available(macOS 12.0, *)) {
-        apps = [[NSWorkspace sharedWorkspace]
-            URLsForApplicationsWithBundleIdentifier:helper_bundle_id_];
-      } else {
-        apps =
-            apple::CFToNSOwnershipCast(LSCopyApplicationURLsForBundleIdentifier(
-                apple::NSToCFPtrCast(helper_bundle_id_), /*outError=*/nullptr));
-      }
+      NSArray<NSURL*>* apps = [NSWorkspace.sharedWorkspace
+          URLsForApplicationsWithBundleIdentifier:helper_bundle_id_];
       if (apps.count > 0) {
         break;
       }
@@ -391,24 +383,8 @@ TEST_F(LaunchApplicationTest, UrlSpecs) {
   EXPECT_NSEQ(LaunchEventName(1), @"applicationDidFinishLaunching");
   EXPECT_NSEQ(LaunchEventName(2), @"openURLs");
 
-  if (MacOSMajorVersion() == 11) {
-    // macOS 11 (and only macOS 11) appears to sometimes trigger the openURLs
-    // calls in reverse order.
-    std::vector<std::string> received_urls;
-    for (NSString* url in apple::ObjCCastStrict<NSArray>(
-             LaunchEventData(0)[@"urls"])) {
-      received_urls.push_back(SysNSStringToUTF8(url));
-    }
-    EXPECT_EQ(received_urls.size(), 1u);
-    for (NSString* url in apple::ObjCCastStrict<NSArray>(
-             LaunchEventData(2)[@"urls"])) {
-      received_urls.push_back(SysNSStringToUTF8(url));
-    }
-    EXPECT_THAT(received_urls, testing::UnorderedElementsAreArray(urls));
-  } else {
-    EXPECT_NSEQ(LaunchEventData(0)[@"urls"], @[ @"https://example.com" ]);
-    EXPECT_NSEQ(LaunchEventData(2)[@"urls"], @[ @"x-chrome-launch://1" ]);
-  }
+  EXPECT_NSEQ(LaunchEventData(0)[@"urls"], @[ @"https://example.com" ]);
+  EXPECT_NSEQ(LaunchEventData(2)[@"urls"], @[ @"x-chrome-launch://1" ]);
 }
 
 TEST_F(LaunchApplicationTest, CreateNewInstance) {
