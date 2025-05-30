978b838736c2af4ddc1cdc7af55f2b74b9089d18
Robert Ogden <robertogden@chromium.org>
2025-05-30 11:26:07 -0700

    Pull TF-Text files for needed ops in the new Bert Safety Model
    Bug: 414865959
    Change-Id: I53f3acfcba77dc887eb7e406257af72ac9567a71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6576263
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467580}

---
+++ third_party/tensorflow-text/shims/icu4c/source/common/unicode/umachine.h
@@ -0,0 +1,10 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_TENSORFLOW_TEXT_SHIMS_ICU4C_SOURCE_COMMON_UNICODE_UMACHINE_H_
+#define THIRD_PARTY_TENSORFLOW_TEXT_SHIMS_ICU4C_SOURCE_COMMON_UNICODE_UMACHINE_H_
+
+#include "third_party/icu/source/common/unicode/umachine.h"
+
+#endif  // THIRD_PARTY_TENSORFLOW_TEXT_SHIMS_ICU4C_SOURCE_COMMON_UNICODE_UMACHINE_H_
--- third_party/tensorflow-text/update.sh
+++ third_party/tensorflow-text/update.sh
@@ -12,12 +12,23 @@ fi
 
 files=(
   "LICENSE"
+  "tensorflow_text/core/kernels/darts_clone_trie_wrapper.h"
+  "tensorflow_text/core/kernels/disjoint_set_forest.h"
+  "tensorflow_text/core/kernels/fast_bert_normalizer.h"
+  "tensorflow_text/core/kernels/fast_bert_normalizer_kernel_template.h"
+  "tensorflow_text/core/kernels/fast_bert_normalizer_model.fbs"
+  "tensorflow_text/core/kernels/fast_bert_normalizer_tflite.h"
+  "tensorflow_text/core/kernels/fast_wordpiece_tokenizer.cc"
+  "tensorflow_text/core/kernels/fast_wordpiece_tokenizer.h"
+  "tensorflow_text/core/kernels/fast_wordpiece_tokenizer_kernel_template.h"
+  "tensorflow_text/core/kernels/fast_wordpiece_tokenizer_model.fbs"
+  "tensorflow_text/core/kernels/fast_wordpiece_tokenizer_tflite.h"
+  "tensorflow_text/core/kernels/fast_wordpiece_tokenizer_utils.h"
+  "tensorflow_text/core/kernels/mst_solver.h"
   "tensorflow_text/core/kernels/regex_split.cc"
   "tensorflow_text/core/kernels/regex_split.h"
   "tensorflow_text/core/kernels/wordpiece_tokenizer.cc"
   "tensorflow_text/core/kernels/wordpiece_tokenizer.h"
-  "tensorflow_text/core/kernels/mst_solver.h"
-  "tensorflow_text/core/kernels/disjoint_set_forest.h"
 )
 
 git clone --depth 1 https://github.com/tensorflow/text /tmp/text
