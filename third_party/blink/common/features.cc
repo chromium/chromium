89a065db3a4e721c0e93f32d1193de84f03a984d
Liang Zhao <lzhao@microsoft.com>
2025-05-30 10:01:57 -0700

    Enable NoThrowForCSPBlockedWorker feature by default
    Enable the feature to fire error event instead of throwing exception
    when worker creation is blocked by CSP.
    
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/ZUz_0RDjs8g.
    
    Bug: 41285169
    Change-Id: Ice8acd88ffdc8b97afc834193e6e808471a4631b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6593393
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Liang Zhao <lzhao@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1467533}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1977,7 +1977,7 @@ BASE_FEATURE(kNoThrottlingVisibleAgent,
 
 BASE_FEATURE(kNoThrowForCSPBlockedWorker,
              "NoThrowForCSPBlockedWorker",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOpenAllUrlsOrFilesOnDrop,
              "OpenAllUrlsOrFilesOnDrop",
