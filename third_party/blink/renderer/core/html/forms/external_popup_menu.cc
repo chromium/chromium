b2c3a536d33fe1ac34f9fde23797e3db16475d16
Perry <perryuwang@gmail.com>
2025-05-29 19:45:23 -0700

    Fix click event not firing for <select> with OS-native menus
    This CL adds a click event for <select> implemented using OS-native
    menus on macOS and Android.
    
    Bug: 40434449
    Change-Id: Iaf746ec8088ac9ce9c450fccb4687c864d02bb8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6575540
    Commit-Queue: Perry <perryuwang@gmail.com>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467288}

--- third_party/blink/renderer/core/html/forms/external_popup_menu.cc
+++ third_party/blink/renderer/core/html/forms/external_popup_menu.cc
@@ -48,6 +48,7 @@
 #include "third_party/blink/renderer/core/html/forms/html_option_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_select_element.h"
 #include "third_party/blink/renderer/core/html/html_hr_element.h"
+#include "third_party/blink/renderer/core/input/event_handler.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/page.h"
@@ -251,6 +252,19 @@ void ExternalPopupMenu::DidAcceptIndices(const Vector<int32_t>& indices) {
     for (wtf_size_t i = 0; i < list_count; ++i)
       list_indices.push_back(ToPopupMenuItemIndex(indices[i], *owner_element));
     owner_element->SelectMultipleOptionsByPopup(list_indices);
+  }
+
+  if (RuntimeEnabledFeatures::ExternalPopupMenuClickEventEnabled()) {
+    WebMouseEvent event;
+    event.SetFrameScale(1);
+    PhysicalRect bounding_box = owner_element->BoundingBox();
+    event.SetPositionInWidget(bounding_box.X(), bounding_box.Y());
+    event.SetTimeStamp(base::TimeTicks::Now());
+    if (LocalFrame* frame = owner_element->GetDocument().GetFrame()) {
+      frame->GetEventHandler().HandleTargetedMouseEvent(
+          owner_element, event, event_type_names::kClick,
+          Vector<WebMouseEvent>(), Vector<WebMouseEvent>());
+    }
   }
   Reset();
 }
