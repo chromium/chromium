01d331fab57f0b8582ea4645e47145db4f8a9c03
Dominik Klemba <tacet@google.com>
2025-05-30 11:58:40 -0700

    Migrate DownloadItemViewTest into InProcessBrowserTest
    Use of TestWithBrowserView is being deprecated and existing
    tests requiring a Browser environment are being migrated to
    being browser tests.
    
    This CL updates the DownloadItemViewTest fixture to an
    InProcessBrowserTest and removes the TestWithBrowserView
    dependency.
    
    Bug: 417766643
    Change-Id: I4fca510978fded16e42e7d80af383bdff96df455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6591465
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Dominik Klemba <tacet@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467596}

--- chrome/browser/ui/views/download/download_item_view_browsertest.cc
+++ chrome/browser/ui/views/download/download_item_view_browsertest.cc
@@ -5,11 +5,13 @@
 #include "chrome/browser/ui/views/download/download_item_view.h"
 
 #include "chrome/browser/download/download_item_model.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/views/download/download_shelf_view.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/frame/test_with_browser_view.h"
+#include "chrome/test/base/in_process_browser_test.h"
 #include "components/download/public/common/mock_download_item.h"
 #include "content/public/browser/download_item_utils.h"
+#include "content/public/test/browser_test.h"
 #include "content/public/test/fake_download_item.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -27,7 +29,7 @@ const base::FilePath::CharType kDefaultTargetFilePath[] =
     FILE_PATH_LITERAL("/foo/bar/foo.bar");
 const char kDefaultURL[] = "http://example.com/foo.bar";
 
-class DownloadItemViewTest : public TestWithBrowserView {
+class DownloadItemViewTest : public InProcessBrowserTest {
  public:
   DownloadItemViewTest() = default;
   ~DownloadItemViewTest() override = default;
@@ -35,8 +37,8 @@ class DownloadItemViewTest : public TestWithBrowserView {
   DownloadItemViewTest(const DownloadItemViewTest&) = delete;
   DownloadItemViewTest& operator=(const DownloadItemViewTest&) = delete;
 
-  void SetUp() override {
-    TestWithBrowserView::SetUp();
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
 
     mock_item_ = std::make_unique<NiceMock<download::MockDownloadItem>>();
     ON_CALL(*mock_item_, GetState())
@@ -48,18 +50,26 @@ class DownloadItemViewTest : public TestWithBrowserView {
     content::DownloadItemUtils::AttachInfoForTesting(
         mock_item_.get(), browser()->profile(), nullptr);
 
+    BrowserView* browser_view =
+        BrowserView::GetBrowserViewForBrowser(browser());
     shelf_view_ =
-        std::make_unique<DownloadShelfView>(browser(), browser_view());
-    browser_view()->SetDownloadShelfForTest(shelf_view_.get());
-    browser_view()->GetDownloadShelf()->AddDownload(
+        std::make_unique<DownloadShelfView>(browser(), browser_view);
+    browser_view->SetDownloadShelfForTest(shelf_view_.get());
+    browser_view->GetDownloadShelf()->AddDownload(
         DownloadItemModel::Wrap(mock_item_.get()));
   }
 
-  void TearDown() override {
-    browser_view()->SetDownloadShelfForTest(nullptr);
+  void TearDownOnMainThread() override {
+    BrowserView* browser_view =
+        BrowserView::GetBrowserViewForBrowser(browser());
+    // It's possible the browser or its window is already being torn down.
+    // We check browser_view before dereferencing it.
+    if (browser_view) {
+      browser_view->SetDownloadShelfForTest(nullptr);
+    }
     shelf_view_.reset();
     mock_item_.reset();
-    TestWithBrowserView::TearDown();
+    InProcessBrowserTest::TearDownOnMainThread();
   }
 
  protected:
@@ -68,7 +78,9 @@ class DownloadItemViewTest : public TestWithBrowserView {
   }
 
   DownloadShelfView* GetDownloadShelfView() {
-    return static_cast<DownloadShelfView*>(browser_view()->GetDownloadShelf());
+    BrowserView* browser_view =
+        BrowserView::GetBrowserViewForBrowser(browser());
+    return static_cast<DownloadShelfView*>(browser_view->GetDownloadShelf());
   }
 
  private:
@@ -76,7 +88,7 @@ class DownloadItemViewTest : public TestWithBrowserView {
   std::unique_ptr<NiceMock<download::MockDownloadItem>> mock_item_;
 };
 
-TEST_F(DownloadItemViewTest, AccessibleProperties) {
+IN_PROC_BROWSER_TEST_F(DownloadItemViewTest, AccessibleProperties) {
   auto* item_view = GetDownloadItemView();
   ui::AXNodeData data;
 
