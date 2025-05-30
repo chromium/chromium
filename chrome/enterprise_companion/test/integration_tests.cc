2610a551444d3a3eb3a9dcfe8711a51398e139d1
Noah Rose Ledesma <noahrose@google.com>
2025-05-30 10:10:24 -0700

    Initialize ICU in CECA for Windows
    Initialize the ICU library when starting CECA. Depending on the build
    configuration, this may require the ICU data file to be colocated with
    the program's executable. To ensure its availability, the data file is
    copied into the program's installation directory.
    
    Bug: 420737997
    Change-Id: I99c5dea95e71d2bc8a5e1a130e07feb533a324a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6598795
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1467542}

--- chrome/enterprise_companion/test/integration_tests.cc
+++ chrome/enterprise_companion/test/integration_tests.cc
@@ -829,6 +829,44 @@ TEST_F(IntegrationTests, GroupPolicyProxy_PacScript) {
   ASSERT_NO_FATAL_FAILURE(ExpectDefaultPolicyValuesPersisted());
 }
 
+// The application should canonicalize proxy URLs sources from PAC scripts
+// containing special characters.
+TEST_F(IntegrationTests, GroupPolicyProxy_PacProxyRequiresCanonicalization) {
+  base::Value::Dict overrides = GetDefaultConstantsOverrides();
+  overrides.Set(kDMServerUrlKey, "http://dm.server.not_exist/dmapi");
+  ASSERT_NO_FATAL_FAILURE(InstallConstantsOverrides(overrides));
+  ASSERT_NO_FATAL_FAILURE(SetLocalProxyPolicies(
+      /*proxy_mode=*/"pac_script", test_server_.proxy_pac_url().spec(),
+      /*proxy_server=*/std::nullopt,
+      /*cloud_policy_overrides_platform_policy=*/std::nullopt));
+  // URL canonicalization should remove the leading zero width space.
+  test_server_.ExpectOnce(
+      {CreatePacUrlMatcher(test_server_)},
+      base::StringPrintf(
+          "function FindProxyForURL(url, host) { return \"PROXY %s\"; }",
+          base::StrCat(
+              {"\u200b", ToProxyURL(dm_test_server_.GetServiceURL())})));
+
+  SetDefaultPolicyFetchResponses();
+  ASSERT_NO_FATAL_FAILURE(StoreEnrollmentToken(kFakeEnrollmentToken));
+  ASSERT_NO_FATAL_FAILURE(GetTestMethods().Install());
+  ASSERT_NO_FATAL_FAILURE(LaunchApp());
+  ASSERT_NO_FATAL_FAILURE(WaitForServerStart());
+
+  test_server_.ExpectOnce(
+      {CreateEventLogMatcher(
+          test_server_,
+          {{proto::EnterpriseCompanionEvent::kBrowserEnrollmentEvent,
+            EnterpriseCompanionStatus::Success()},
+           {proto::EnterpriseCompanionEvent::kPolicyFetchEvent,
+            EnterpriseCompanionStatus::Success()}})},
+      CreateLogResponse());
+
+  EXPECT_TRUE(CreateAppFetchPolicies()->Run().ok());
+
+  ASSERT_NO_FATAL_FAILURE(ExpectDefaultPolicyValuesPersisted());
+}
+
 // The application should exit with a failure if proxy navigation fails and the
 // server is not directly reachable.
 TEST_F(IntegrationTests, GroupPolicyProxy_BadProxyServer) {
