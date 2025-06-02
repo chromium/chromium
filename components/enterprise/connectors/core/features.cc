e33dcb461fe53cd8313449880344c5aa1020c4e5
Nancy Xiao <nancylanxiao@google.com>
2025-06-02 10:12:38 -0700

    Enable Reporting Connector on Clank by default
    Bug: 344578452
    Change-Id: Ia24740d623572febc8c016b7c706fc8da4be1a91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612198
    Auto-Submit: Nancy Xiao <nancylanxiao@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468201}

--- components/enterprise/connectors/core/features.cc
+++ components/enterprise/connectors/core/features.cc
@@ -8,7 +8,7 @@ namespace enterprise_connectors {
 
 BASE_FEATURE(kEnterpriseSecurityEventReportingOnAndroid,
              "EnterpriseSecurityEventReportingOnAndroid",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnterpriseUrlFilteringEventReportingOnAndroid,
              "EnterpriseUrlFilteringEventReportingOnAndroid",
