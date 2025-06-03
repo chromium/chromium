8a775c379e281e99b2d538b096dc5b8f8baacd2a
Lei Zhang <thestig@chromium.org>
2025-06-03 11:31:09 -0700

    [PDF Ink Signatures] Record crash reason in RecordStrokePosition()
    In PdfInkModule::RecordStrokePosition(), record the crash reason into
    the crash dump. Logging it in the CHECK() failure still does not show up
    in the crash dump.
    
    Bug: 421120183
    Change-Id: Iab6e7384f1220cf93bcb29fa89848d12054f6ccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6608816
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468876}

--- pdf/pdf_ink_module.cc
+++ pdf/pdf_ink_module.cc
@@ -19,6 +19,7 @@
 
 #include "base/check.h"
 #include "base/containers/fixed_flat_map.h"
+#include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
@@ -1493,7 +1494,12 @@ void PdfInkModule::RecordStrokePosition(const gfx::PointF& position,
   base::TimeDelta time_diff = timestamp - state.start_time.value();
   auto result = state.inputs.back().Append(
       CreateInkStrokeInput(tool_type, canonical_position, time_diff));
-  CHECK(result.ok()) << result.message();
+  if (!result.ok()) {
+    // TODO(crbug.com/421120183): Fix crash and remove.
+    SCOPED_CRASH_KEY_STRING256("PdfInkModule", "RecordStrokePosition",
+                               result.message());
+    CHECK(result.ok()) << result.message();
+  }
 }
 
 void PdfInkModule::ApplyUndoRedoCommands(
