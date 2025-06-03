d837e8fd1be55306667e0b9dc353ea8657661e32
Fredrik Söderquist <fs@opera.com>
2025-06-03 01:24:34 -0700

    Simplify handling of hard-coded colors in EmbeddedObjectPainter
    Rather than converting colors back-and-forth and modulating the alpha
    using ScaleAlpha(), just define the colors as locals directly with alpha
    applied.
    
    Bug: 399058536
    Change-Id: I065a70b2137dce6c6ee0fdbfef220f62a455606f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6610960
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468578}

--- third_party/blink/renderer/core/paint/embedded_object_painter.cc
+++ third_party/blink/renderer/core/paint/embedded_object_painter.cc
@@ -4,7 +4,6 @@
 
 #include "third_party/blink/renderer/core/paint/embedded_object_painter.h"
 
-#include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_object.h"
 #include "third_party/blink/renderer/core/layout/layout_theme_font_provider.h"
 #include "third_party/blink/renderer/core/paint/box_painter.h"
@@ -14,21 +13,24 @@
 #include "third_party/blink/renderer/platform/fonts/font.h"
 #include "third_party/blink/renderer/platform/fonts/font_selector.h"
 #include "third_party/blink/renderer/platform/fonts/plain_text_painter.h"
-#include "third_party/blink/renderer/platform/fonts/text_run_paint_info.h"
-#include "third_party/blink/renderer/platform/geometry/path.h"
+#include "third_party/blink/renderer/platform/graphics/color.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
-#include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/text/text_run.h"
 
 namespace blink {
 
-static const float kReplacementTextRoundedRectHeight = 18;
-static const float kReplacementTextRoundedRectLeftRightTextMargin = 6;
-static const float kReplacementTextRoundedRectOpacity = 0.20f;
-static const float kReplacementTextRoundedRectRadius = 5;
-static const float kReplacementTextTextOpacity = 0.55f;
+namespace {
 
-static Font* ReplacementTextFont(const Document* document) {
+constexpr float kReplacementTextRoundedRectHeight = 18;
+constexpr float kReplacementTextRoundedRectLeftRightTextMargin = 6;
+constexpr float kReplacementTextRoundedRectRadius = 5;
+
+constexpr Color kReplacementTextRoundedRectColor =
+    Color::FromRGBAFloat(1, 1, 1, 0.20f);
+constexpr Color kReplacementTextTextColor =
+    Color::FromRGBAFloat(0, 0, 0, 0.55f);
+
+Font* ReplacementTextFont(const Document* document) {
   const AtomicString& family = LayoutThemeFontProvider::SystemFontFamily(
       CSSValueID::kWebkitSmallControl);
   const float size = LayoutThemeFontProvider::SystemFontSize(
@@ -42,6 +44,8 @@ static Font* ReplacementTextFont(const Document* document) {
   font_description.SetComputedSize(size);
   return MakeGarbageCollected<Font>(font_description);
 }
+
+}  // namespace
 
 void EmbeddedObjectPainter::PaintReplaced(const PaintInfo& paint_info,
                                           const PhysicalOffset& paint_offset) {
@@ -86,18 +90,16 @@ void EmbeddedObjectPainter::PaintReplaced(const PaintInfo& paint_info,
   FloatRoundedRect rounded_background_rect(
       gfx::RectF(ToPixelSnappedRect(background_rect)),
       kReplacementTextRoundedRectRadius);
-  Color color = Color::FromSkColor(
-      ScaleAlpha(SK_ColorWHITE, kReplacementTextRoundedRectOpacity));
   AutoDarkMode auto_dark_mode(
       PaintAutoDarkMode(layout_embedded_object_.StyleRef(),
                         DarkModeFilter::ElementRole::kBackground));
-  context.FillRoundedRect(rounded_background_rect, color, auto_dark_mode);
+  context.FillRoundedRect(rounded_background_rect,
+                          kReplacementTextRoundedRectColor, auto_dark_mode);
 
   gfx::RectF text_rect(gfx::PointF(), text_geometry);
   text_rect.Offset(gfx::PointF(content_rect.Center()) -
                    text_rect.CenterPoint());
-  context.SetFillColor(Color::FromSkColor(
-      ScaleAlpha(SK_ColorBLACK, kReplacementTextTextOpacity)));
+  context.SetFillColor(kReplacementTextTextColor);
   context.DrawBidiText(
       *font, text_run,
       text_rect.origin() +
