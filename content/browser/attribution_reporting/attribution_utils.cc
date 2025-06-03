8d771f8ed3e037040d7c212972a6ebd787f3f10b
Nan Lin <linnan@chromium.org>
2025-06-03 06:34:33 -0700

    Replace DCHECK with DumpWithoutCrashing
    This is not ensured by the implementation. Replaced with
    DumpWithoutCrashing allows us to find out if this starts crashing.
    
    Change-Id: Ic501b0f708f1b79ae45ab7b254236139d8de47c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6611683
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1468677}

--- content/browser/attribution_reporting/attribution_utils.cc
+++ content/browser/attribution_reporting/attribution_utils.cc
@@ -4,7 +4,9 @@
 
 #include "content/browser/attribution_reporting/attribution_utils.h"
 
-#include "base/check.h"
+#include <string>
+
+#include "base/debug/dump_without_crashing.h"
 #include "base/json/json_writer.h"
 #include "base/values.h"
 
@@ -14,9 +16,9 @@ std::string SerializeAttributionJson(base::ValueView body, bool pretty_print) {
   int options = pretty_print ? base::JSONWriter::OPTIONS_PRETTY_PRINT : 0;
 
   std::string output_json;
-  bool success =
-      base::JSONWriter::WriteWithOptions(body, options, &output_json);
-  DCHECK(success);
+  if (!base::JSONWriter::WriteWithOptions(body, options, &output_json)) {
+    base::debug::DumpWithoutCrashing();
+  }
   return output_json;
 }
 
