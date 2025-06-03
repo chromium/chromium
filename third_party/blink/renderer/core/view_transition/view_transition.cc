4d13724c7f04573fed3f1cf2e3993832dc179abc
Vladimir Levin <vmpstr@chromium.org>
2025-06-03 09:52:52 -0700

    VT: Ensure to update container styles before resolving ready.
    Container styles are required for correct view transition styles. For
    painting this isn't a problem because we will be running these steps
    before pseudos are painted on screen.
    
    However, for getAnimations and keyframes, this value can be incorrect
    without the update.
    
    R=chrishtr@chromium.org
    
    Bug: 387030974
    Fixed: 421419263
    
    (cherry picked from commit 569f37edda73d85db8f24f3a4bd5fe1aacf8f089)
    
    Change-Id: I769b6c685e494e59a40734d460a8527bd87a74fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6592432
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1466060}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618006
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7151@{#1907}
    Cr-Branched-From: 8e0d32ed6e49a2415b16e5ed402957cac2349ce2-refs/heads/main@{#1453031}

--- third_party/blink/renderer/core/view_transition/view_transition.cc
+++ third_party/blink/renderer/core/view_transition/view_transition.cc
@@ -598,6 +598,13 @@ void ViewTransition::ProcessCurrentState() {
           break;
         }
 
+        if (RuntimeEnabledFeatures::
+                ViewTransitionUpdateLifecycleBeforeReadyEnabled()) {
+          document_->View()->UpdateAllLifecyclePhasesExceptPaint(
+              DocumentUpdateReason::kViewTransition);
+          style_tracker_->RunPostPrePaintSteps();
+        }
+
         delegate_->AddPendingRequest(
             ViewTransitionRequest::CreateAnimateRenderer(
                 transition_token_, MaybeCrossFrameSink()));
