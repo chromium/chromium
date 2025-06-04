27a8d747b28edbb860f055a6bd2eece153f65311
Jordan Bayles <jophba@chromium.org>
2025-06-03 17:49:23 -0700

    Simplify inclusive language dirs script
    This patch simply simplifies the inclusive language presubmit
    script to remove the counts, which are no longer consumed by the
    depot tools presubmit script.
    
    This lowers the amount of noise in the inclusive language presubmit
    exempt dirs file and simplifies the script that generates this file.
    
    Change-Id: I8c27706984667634bb99bcca716fb27b1be64df7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6615538
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469068}

--- infra/update_inclusive_language_presubmit_exempt_dirs.sh
+++ infra/update_inclusive_language_presubmit_exempt_dirs.sh
@@ -4,10 +4,8 @@
 # found in the LICENSE file.
 
 # Produce a list of directories that contain files with instances of
-# non-inclusive language (e.g. whitelist, blacklist, master, slave). Each line
-# is formatted: <path> <instance count across files> <distinct file count>
-# (separated by spaces). The counts are not recursive, so they do not sum up
-# values from child directories, only files directly in the listed path.
+# non-inclusive language (e.g. whitelist, blacklist, master, slave). Each
+# line is formatted as the relative path of the directory.
 #
 # The intended consumer of this list is the CheckInclusiveLanguage function
 # in ../PRESUBMIT.py. It does not perform its check on files directly under
@@ -27,7 +25,6 @@
 # instance, you should run this from the parent of the current directory
 # rather than from this directory.
 
-git grep --ignore-case --count -E '\b((black|white)list|master|slave)\b' | \
-  awk -F ":" 'NF {cmd=sprintf("dirname %s",$1);cmd | getline dirname; \
-  a[dirname] += $2; b[dirname] += 1} END {for (i in a) print i, a[i], b[i]}' \
-  | sort
+git grep --ignore-case -l -E '\b((black|white)list|master|slave)\b' | \
+  xargs -n1 dirname | \
+  sort -u
