8c6ae022afac4ef3185e7087d0facae7b30ad365
Andrew Grieve <agrieve@chromium.org>
2025-06-04 10:29:15 -0700

    Android: EmbeddedTestServer->TestWebServer & do not use BaseJUnit4ClassRunner
    We may start running these tests outside of chromium, so this makes that
    easier by using the standard test runner, and by not requiring an
    auxillary APK to run the web server.
    
    This also has the side-effect of not pre-initializing command-line flags
    in the test runner, although that behaviour happens only in debug builds
    (where R8 doesn't rename things).
    
    Bug: 417493991
    Change-Id: I1ecacce24cfe9b7c3576c8c82f2068e57f386a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6615457
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469433}

--- chrome/android/javatests/src/org/chromium/chrome/test/smoke/ChromeTabSwitcherTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/test/smoke/ChromeTabSwitcherTest.java
@@ -12,9 +12,11 @@ import android.net.Uri;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.LargeTest;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.ClassRule;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
@@ -22,7 +24,6 @@ import org.junit.rules.TestRule;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.Log;
-import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.chrome.R;
 import org.chromium.chrome.test.pagecontroller.rules.ChromeUiApplicationTestRule;
 import org.chromium.chrome.test.pagecontroller.rules.ChromeUiAutomatorTestRule;
@@ -30,18 +31,24 @@ import org.chromium.chrome.test.pagecontroller.utils.IUi2Locator;
 import org.chromium.chrome.test.pagecontroller.utils.Ui2Locators;
 import org.chromium.chrome.test.pagecontroller.utils.UiAutomatorUtils;
 import org.chromium.chrome.test.smoke.utilities.FirstRunNavigator;
-import org.chromium.net.test.EmbeddedTestServerRule;
+import org.chromium.net.test.util.TestWebServer;
 
 /** Basic Test for Chrome Android to switch Tabs. */
 @LargeTest
-@RunWith(BaseJUnit4ClassRunner.class)
+// Not using BaseJUnit4ClassRunner in order to avoid initializing command-line flags prematurely.
+@RunWith(AndroidJUnit4ClassRunner.class)
 // Do not disable this test unless you are confident you can re-enable it quickly.
 // This is the main test that prevents crash-on-launch bugs.
 public class ChromeTabSwitcherTest {
     private static final String TAG = "SmokeTest";
     private static final String ACTIVITY_NAME = "com.google.android.apps.chrome.IntentDispatcher";
-    private static final String TEST_PAGE =
-            "/chrome/android/javatests/src/org/chromium/chrome/test/smoke/test.html";
+    private static final String TEST_PAGE_HTML =
+            """
+            <!DOCTYPE html>
+            <html>
+            Hello Smoke Test
+            </html>
+            """;
 
     private final IUi2Locator mTabSwitcherButton =
             Ui2Locators.withAnyResEntry(R.id.tab_switcher_button);
@@ -50,17 +57,27 @@ public class ChromeTabSwitcherTest {
 
     private final IUi2Locator mTabList = Ui2Locators.withAnyResEntry(R.id.tab_list_recycler_view);
 
+    private final IUi2Locator mNewTabButton = Ui2Locators.withContentDesc("New tab");
+    private final IUi2Locator mNtpOmnibox = Ui2Locators.withAnyResEntry(R.id.search_box_text);
+
     private final FirstRunNavigator mFirstRunNavigator = new FirstRunNavigator();
 
     public static final long TIMEOUT_MS = 20000L;
-    public static final long UI_CHECK_INTERVAL = 1000L;
     private String mPackageName;
     public ChromeUiAutomatorTestRule mRule = new ChromeUiAutomatorTestRule();
     public ChromeUiApplicationTestRule mChromeUiRule = new ChromeUiApplicationTestRule();
     @Rule public final TestRule mChain = RuleChain.outerRule(mChromeUiRule).around(mRule);
+    private static TestWebServer sWebServer;
 
-    @ClassRule
-    public static EmbeddedTestServerRule sEmbeddedTestServerRule = new EmbeddedTestServerRule();
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        sWebServer = TestWebServer.start();
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        sWebServer.close();
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -76,7 +93,7 @@ public class ChromeTabSwitcherTest {
     // This is the main test that prevents crash-on-launch bugs.
     public void testTabSwitcher() throws Exception {
         Context context = ApplicationProvider.getApplicationContext();
-        String url = sEmbeddedTestServerRule.getServer().getURL(TEST_PAGE);
+        String url = sWebServer.setResponse("/test.html", TEST_PAGE_HTML, null);
         final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
         intent.addCategory(Intent.CATEGORY_BROWSABLE);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
