dc8a3f31b4d74676f71351b63c8d0f9225e0927e
Wenyu Zhang <zhangwenyu@google.com>
2025-06-04 13:50:51 -0700

    a11y: Add flag for using SupplementalDescription API
    A flag is necessary since the TalkBack version that consumes this API is
    not yet launched. A follow up CL will put the usage of this API behind
    this flag.
    
    Bug: 418962695
    Change-Id: Ia3f7d874c38716a6109fb7f7da74566fae6ea367
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6619106
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Wenyu Zhang <zhangwenyu@google.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1469598}

--- content/browser/android/content_feature_map.cc
+++ content/browser/android/content_feature_map.cc
@@ -31,6 +31,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kAccessibilityDeprecateTypeAnnounce,
     &features::kAccessibilityIncludeLongClickAction,
     &features::kAccessibilityPageZoomV2,
+    &features::kAccessibilityPopulateSupplementalDescriptionApi,
     &features::kAccessibilityUnifiedSnapshots,
     &features::kAccessibilityManageBroadcastReceiverOnBackground,
     &features::kAndroidFallbackToNextSlot,
