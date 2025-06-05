7cb480d6c18be8f44b3684887cb4b576a5e2ff4c
Stefan Zager <szager@chromium.org>
2025-06-05 16:23:17 -0700

    Enable feature SendExplicitDecodeRequestsImmediately
    Bug: chromium:420748301,378623805
    Change-Id: I1a8177c5ce31df5ef421d1031372355cb5183521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625096
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470255}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -150,7 +150,7 @@ BASE_FEATURE(kTreeAnimationsInViz,
 
 BASE_FEATURE(kSendExplicitDecodeRequestsImmediately,
              "SendExplicitDecodeRequestsImmediately",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNewContentForCheckerboardedScrolls,
              "NewContentForCheckerboardedScrolls",
