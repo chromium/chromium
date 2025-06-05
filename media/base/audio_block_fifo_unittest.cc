0661487b4a32c8c015bb0368e6ab57f985cd6247
Dale Curtis <dalecurtis@chromium.org>
2025-06-05 14:27:02 -0700

    Revert "Spanify AudioBlockFifo and AmplitudePeakDetector."
    This reverts commit 25b072d17c32dfffb955ca69aacb416c8e7f8c62.
    
    Reason for revert: crbug.com/422794607
    
    Bug: 40284755, 422794607
    Original change's description:
    > Spanify AudioBlockFifo and AmplitudePeakDetector.
    >
    > Bug: 40284755
    > Change-Id: Ie59f4ad9acb90c8463c8e1731dc043f8d54a1783
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6612462
    > Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1469833}
    
    Bug: 40284755
    Change-Id: Iea1507f4bef8fa2539b9d0e27409e3ebea21daae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625456
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470200}

--- media/base/audio_block_fifo_unittest.cc
+++ media/base/audio_block_fifo_unittest.cc
@@ -2,14 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "media/base/audio_block_fifo.h"
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/390223051): Remove C-library calls to fix the errors.
+#pragma allow_unsafe_libc_calls
+#endif
 
 #include <stdint.h>
-
-#include <algorithm>
 #include <memory>
 
 #include "base/time/time.h"
+#include "media/base/audio_block_fifo.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace media {
@@ -42,9 +44,9 @@ class AudioBlockFifoTest : public testing::Test {
     DCHECK_LE(frames_to_push, fifo->GetUnfilledFrames());
     const int bytes_per_sample = 2;
     const int data_byte_size = bytes_per_sample * channels * frames_to_push;
-    auto data = base::HeapArray<uint8_t>::Uninit(data_byte_size);
-    std::ranges::fill(data, 1);
-    fifo->Push(data, frames_to_push, bytes_per_sample);
+    auto data = std::make_unique<uint8_t[]>(data_byte_size);
+    memset(data.get(), 1, data_byte_size);
+    fifo->Push(data.get(), frames_to_push, bytes_per_sample);
   }
 
   void ConsumeAndVerify(AudioBlockFifo* fifo,
