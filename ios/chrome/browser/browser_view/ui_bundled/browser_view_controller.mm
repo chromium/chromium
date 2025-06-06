de002ea8ec12348e1597ab135bd5e08da6e6789f
Radu Nitescu <radunitescu@google.com>
2025-06-06 09:13:43 -0700

    [iOS][CoI] Prevent A11y focus to go to web content while Lens is active
    When web content is logically obstructed, it should be excluded from
    voice over.
    
    Fixed: 419651743
    Change-Id: I98ce533b1cd1fe392dfe39fe2b7a5af43c6ace55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624220
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470583}

--- ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_view_controller.mm
@@ -237,6 +237,9 @@ enum HeaderBehaviour {
   // Whether the Lens Overlay is currently active and visible for the browser
   // view.
   BOOL _lensOverlayVisible;
+
+  // Whether the find bar is currently visible.
+  BOOL _findBarVisible;
 }
 
 // Activates/deactivates the object. This will enable/disable the ability for
@@ -334,6 +337,10 @@ enum HeaderBehaviour {
 
 // Provider used to offload SceneStateBrowserAgent usage from BVC.
 @property(nonatomic, strong) SafeAreaProvider* safeAreaProvider;
+
+// Whether the content area is currently blocked by another view.
+@property(nonatomic, readonly, getter=isContentAreaObstructed)
+    BOOL contentAreaObstructed;
 
 @end
 
@@ -444,6 +451,14 @@ enum HeaderBehaviour {
 
 #pragma mark - Private Properties
 
+- (BOOL)isContentAreaObstructed {
+  return _visibilityState ==
+             BrowserViewVisibilityState::kCoveredByOmniboxPopup ||
+         _visibilityState ==
+             BrowserViewVisibilityState::kCoveredByVoiceSearch ||
+         _lensOverlayVisible || _findBarVisible;
+}
+
 - (void)setVisibilityState:(BrowserViewVisibilityState)state {
   if (_visibilityState == state) {
     return;
@@ -454,9 +469,7 @@ enum HeaderBehaviour {
       browserViewDidTransitionToVisibilityState:state
                                       fromState:previousState];
   [self updateBroadcastState];
-  self.contentArea.accessibilityElementsHidden =
-      state == BrowserViewVisibilityState::kCoveredByOmniboxPopup ||
-      state == BrowserViewVisibilityState::kCoveredByVoiceSearch;
+  self.contentArea.accessibilityElementsHidden = self.contentAreaObstructed;
 }
 
 - (void)setBroadcasting:(BOOL)broadcasting {
@@ -2705,8 +2718,9 @@ enum HeaderBehaviour {
 
 - (void)findBarDidAppearForFindBarCoordinator:
     (FindBarCoordinator*)findBarCoordinator {
+  _findBarVisible = YES;
   // When the Find bar is presented, hide underlying elements from VoiceOver.
-  self.contentArea.accessibilityElementsHidden = YES;
+  self.contentArea.accessibilityElementsHidden = self.contentAreaObstructed;
   self.toolbarCoordinator.primaryToolbarViewController.view
       .accessibilityElementsHidden = YES;
   self.toolbarCoordinator.secondaryToolbarViewController.view
@@ -2715,8 +2729,9 @@ enum HeaderBehaviour {
 
 - (void)findBarDidDisappearForFindBarCoordinator:
     (FindBarCoordinator*)findBarCoordinator {
+  _findBarVisible = NO;
   // When the Find bar is dismissed, show underlying elements to VoiceOver.
-  self.contentArea.accessibilityElementsHidden = NO;
+  self.contentArea.accessibilityElementsHidden = self.contentAreaObstructed;
   self.toolbarCoordinator.primaryToolbarViewController.view
       .accessibilityElementsHidden = NO;
   self.toolbarCoordinator.secondaryToolbarViewController.view
@@ -2752,11 +2767,13 @@ enum HeaderBehaviour {
 - (void)lensOverlayWillAppear {
   [_sideSwipeCoordinator setEnabled:NO];
   _lensOverlayVisible = YES;
+  self.contentArea.accessibilityElementsHidden = self.contentAreaObstructed;
 }
 
 - (void)lensOverlayWillDisappear {
   [_sideSwipeCoordinator setEnabled:YES];
   _lensOverlayVisible = NO;
+  self.contentArea.accessibilityElementsHidden = self.contentAreaObstructed;
 }
 
 - (NSDirectionalEdgeInsets)presentationInsetsForLensOverlay {
