114a51a0c8dd26ae7008563f338bdb0eaad65440
Alex N. Jose <alexnj@chromium.org>
2025-06-05 18:31:26 -0700

    Re-enable headless-shell window size tests.
    Bug: 42323661
    Change-Id: I2c4c224c4b9002323220e5215f88c5648b72bf48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6621336
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470305}

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -182,12 +182,6 @@ _BROWSER_AND_PLATFORM_SPECIFIC_FILTER['chrome-headless-shell']['mac'] = [
     # Unable to run chrome-headless-shell with logging enabled on Mac. See
     # crbug.com/1011000.
     'ChromeLogPathCapabilityTest.testChromeLogPath',
-    # https://crbug.com/chromedriver/4631
-    # chrome-headless-shell does not set the window rect as requested.
-    'ChromeDriverTest.testWindowMinimize',
-    'ChromeDriverTest.testWindowPosition',
-    'ChromeDriverTest.testWindowRect',
-    'ChromeDriverTest.testWindowSize',
     # https://crbug.com/chromedriver/4632
     # chrome-headless-shell ignores the selected range while inserting the text
     'ChromeDriverW3cTest.testSendKeysToElementDoesNotAppend',
