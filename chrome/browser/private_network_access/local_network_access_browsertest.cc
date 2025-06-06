ca4ef792cf6061e491c9209433b96e5d29a73294
Hubert Chao <hchao@chromium.org>
2025-06-06 05:05:22 -0700

    M138 merge: Reland "[LNA] add use counter for "private" address space alias"
    This reverts commit baa465c53ba97dff434f7357243fce7ef5cf94c1.
    
    Reason for revert: didn't realize that some builders had tests that
    specifically didn't use fieldtrial_testing_config.json. Only additional change was the enablement of the feature flag explicitly.
    
    Bug: 418737577, 420455025, 422142662
    Original change's description:
    > Revert "[LNA] add use counter for "private" address space alias"
    >
    > This reverts commit a128c8274fd2b456d95ab8fb1459d527a7e5bc75.
    >
    > Reason for revert: Added test failing on linux-chromeos-chrome and Win10 Tests x64
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/51259/overview
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64/100713/overview
    >
    > Bug: 418737577
    > Original change's description:
    > > [LNA] add use counter for "private" address space alias
    > >
    > > https://crrev.com/c/6564545 added an address space alias for "private"
    > > because it was previously used in PNA. This CL allows us to track usage
    > > of the "private" alias so that there are stats allowing us to inform
    > > our decision on when to remove the alias.
    > >
    > > Bug: 418737577
    > > Change-Id: I625f8b911a055f15361fe42a66d439347b91619d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6581038
    > > Reviewed-by: Chris Thompson <cthomp@chromium.org>
    > > Reviewed-by: Adam Rice <ricea@chromium.org>
    > > Commit-Queue: Hubert Chao <hchao@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1465528}
    >
    > Bug: 418737577
    > Change-Id: I21804baec96b57b14896069d2f4bd0239aede249
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6587569
    > Reviewed-by: Keishi Hattori <keishi@chromium.org>
    > Owners-Override: Keishi Hattori <keishi@chromium.org>
    > Commit-Queue: Keishi Hattori <keishi@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1465716}
    
    (cherry picked from commit 8eb13c8d7a54ca762ef3119569b5b29a8635fa51)
    
    Bug: 418737577
    Change-Id: I58225442852eab1f97c71593045dfb96f9f41704
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6617464
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7204@{#664}
    Cr-Branched-From: d5de512dc9dc8ddfe4e6d71b0637578bb6158683-refs/heads/main@{#1465706}

--- chrome/browser/private_network_access/local_network_access_browsertest.cc
+++ chrome/browser/private_network_access/local_network_access_browsertest.cc
@@ -7,6 +7,7 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/metrics/content/subprocess_metrics_provider.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/permissions/permission_request_manager.h"
 #include "components/permissions/test/mock_permission_prompt_factory.h"
@@ -18,6 +19,8 @@
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/test_data_directory.h"
+#include "services/network/public/cpp/features.h"
+#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom.h"
 
 // Path to a response that passes Local Network Access checks.
 constexpr char kLnaPath[] =
@@ -26,14 +29,50 @@ constexpr char kLnaPath[] =
 
 class LocalNetworkAccessBrowserTest : public policy::PolicyTest {
  public:
+  using WebFeature = blink::mojom::WebFeature;
+
   LocalNetworkAccessBrowserTest()
-      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
+      : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {
+    // Some builders run with field_trial disabled, need to  enable this
+    // manually.
+    features_.InitAndEnableFeature(
+        network::features::kLocalNetworkAccessChecks);
+  }
 
   content::WebContents* web_contents() const {
     return browser()->tab_strip_model()->GetActiveWebContents();
   }
 
   net::EmbeddedTestServer& https_server() { return https_server_; }
+
+  // Fetch the Blink.UseCounter.Features histogram in every renderer process
+  // until reaching, but not exceeding, |expected_count|.
+  void CheckCounter(WebFeature feature, int expected_count) {
+    CheckHistogramCount("Blink.UseCounter.Features", feature, expected_count);
+  }
+
+  // Fetch the |histogram|'s |bucket| in every renderer process until reaching,
+  // but not exceeding, |expected_count|.
+  template <typename T>
+  void CheckHistogramCount(std::string_view histogram,
+                           T bucket,
+                           int expected_count) {
+    while (true) {
+      content::FetchHistogramsFromChildProcesses();
+      metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
+
+      int count = histogram_.GetBucketCount(histogram, bucket);
+      CHECK_LE(count, expected_count);
+      if (count == expected_count) {
+        return;
+      }
+
+      base::RunLoop run_loop;
+      base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+          FROM_HERE, run_loop.QuitClosure(), base::Milliseconds(1));
+      run_loop.Run();
+    }
+  }
 
  protected:
   void SetUpOnMainThread() override {
@@ -54,6 +93,7 @@ class LocalNetworkAccessBrowserTest : public policy::PolicyTest {
 
   net::EmbeddedTestServer https_server_;
   base::test::ScopedFeatureList features_;
+  base::HistogramTester histogram_;
 };
 
 IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
@@ -100,4 +140,46 @@ IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
                   content::JsReplace("fetch($1).then(response => response.ok)",
                                      https_server().GetURL("b.com", kLnaPath))),
               content::EvalJsResult::IsError());
+}
+
+IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
+                       CheckPrivateAliasFeatureCounter) {
+  ASSERT_TRUE(content::NavigateToURL(
+      web_contents(),
+      https_server().GetURL(
+          "a.com",
+          "/private_network_access/no-favicon-treat-as-public-address.html")));
+
+  // LNA fetch fails due to mismatched targetAddressSpace. Result doesn't matter
+  // here though, as we're just checking a use counter that doesn't depend on
+  // fetch success.
+  EXPECT_THAT(content::EvalJs(web_contents(),
+                              content::JsReplace(
+                                  "fetch($1, {targetAddressSpace: "
+                                  "'private'}).then(response => response.ok)",
+                                  https_server().GetURL("b.com", kLnaPath))),
+              content::EvalJsResult::IsError());
+
+  CheckCounter(WebFeature::kLocalNetworkAccessPrivateAliasUse, 1);
+}
+
+IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
+                       CheckPrivateAliasFeatureCounterLocalNotCounted) {
+  ASSERT_TRUE(content::NavigateToURL(
+      web_contents(),
+      https_server().GetURL(
+          "a.com",
+          "/private_network_access/no-favicon-treat-as-public-address.html")));
+
+  // LNA fetch fails due to mismatched targetAddressSpace. Result doesn't matter
+  // here though, as we're just checking a use counter that doesn't depend on
+  // fetch success.
+  EXPECT_THAT(content::EvalJs(
+                  web_contents(),
+                  content::JsReplace("fetch($1, {targetAddressSpace: "
+                                     "'local'}).then(response => response.ok)",
+                                     https_server().GetURL("b.com", kLnaPath))),
+              content::EvalJsResult::IsError());
+
+  CheckCounter(WebFeature::kLocalNetworkAccessPrivateAliasUse, 0);
 }
