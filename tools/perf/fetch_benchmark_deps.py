c71bff82cd38e38172319c6e30c453520f4a90ad
Mateusz Zbikowski <zbikowski@google.com>
2025-06-06 09:18:17 -0700

    Add WPR archive for cb embedder benchmark
    Includes changes to fetch_benchmark_deps, and the relevant pydeps file
    
    Bug: 393058910
    Change-Id: I12600e7afb265f38924b8390f4cd3febd6f7d3bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6627838
    Auto-Submit: Mateusz Zbikowski <zbikowski@google.com>
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1470588}

--- tools/perf/fetch_benchmark_deps.py
+++ tools/perf/fetch_benchmark_deps.py
@@ -15,7 +15,8 @@ from six.moves import input  # pylint: disable=redefined-builtin
 from chrome_telemetry_build import chromium_config
 from core import benchmark_finders
 from core import path_util
-from page_sets import crossbench_loading, speedometer3_pages
+from page_sets import (
+    crossbench_embedder, crossbench_loading, speedometer3_pages)
 from py_utils import cloud_storage
 
 from telemetry.core import optparse_argparse_migration as oam
@@ -87,6 +88,7 @@ def FetchDepsForCrossbench():
   cb_story_sets = [
       speedometer3_pages.Speedometer30CrossbenchStory(),
       crossbench_loading.LoadingCrossbenchStorySet(),
+      crossbench_embedder.EmbedderCrossbenchStorySet(),
   ]
   for story_set in cb_story_sets:
     story_set.wpr_archive_info.DownloadArchivesIfNeeded()
