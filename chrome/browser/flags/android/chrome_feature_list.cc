e1096cdff31e43d7bd79b40ca3592c23d77a6173
Shu Yang <shuyng@google.com>
2025-06-08 23:02:15 -0700

    [M138] Revert "Enable AndroidPdfAssistContent on ToT"
    Original change's description:
    > Revert "Enable AndroidPdfAssistContent on ToT"
    > 
    > This reverts commit 88ee2c5f15a2facd2d046fa442cd838d22fcc2c7.
    > 
    > Reason for revert: decide to proceed with caution per team discussion
    > 
    > Original change's description:
    > > Enable AndroidPdfAssistContent on ToT
    > >
    > > Change-Id: Ibbecf6f3e5e129589261a8d34fa82427ebc83390
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6514714
    > > Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    > > Commit-Queue: Shu Yang <shuyng@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1456643}
    > 
    > Change-Id: I0e1f6296d8bcfc43ffa58431be5e00216d534836
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626272
    > Auto-Submit: Shu Yang <shuyng@google.com>
    > Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    > Commit-Queue: Shu Yang <shuyng@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1470742}
    
    Bug: 423447462
    Change-Id: I0e1f6296d8bcfc43ffa58431be5e00216d534836
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6625851
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7204@{#850}
    Cr-Branched-From: d5de512dc9dc8ddfe4e6d71b0637578bb6158683-refs/heads/main@{#1465706}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -564,7 +564,7 @@ BASE_FEATURE(kAndroidOpenPdfInlineBackport,
 
 BASE_FEATURE(kAndroidPdfAssistContent,
              "AndroidPdfAssistContent",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidSurfaceColorUpdate,
              "AndroidSurfaceColorUpdate",
