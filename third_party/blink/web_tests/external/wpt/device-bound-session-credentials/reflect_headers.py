23249538e6f0fa327ff8316aef91319eb92f1408
Daniel Rubery <drubery@chromium.org>
2025-06-09 11:08:09 -0700

    Add WPT test for DBSC debug header on redirect
    A same-site redirect should continue to skip the failed DBSC refresh, so
    it should also include a debug header. A cross-site redirect should not
    know about the DBSC status of the failed refreshes.
    
    Fixed: 417401759
    Change-Id: Id7738597604f51b57091075e2415c78e92aacb00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626340
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471313}

--- third_party/blink/web_tests/external/wpt/device-bound-session-credentials/reflect_headers.py
+++ third_party/blink/web_tests/external/wpt/device-bound-session-credentials/reflect_headers.py
@@ -2,6 +2,8 @@ import json
 
 from wptserve.utils import isomorphic_decode
 
+# Returns the request headers as JSON, so we can test if a header is
+# included or excluded.
 def main(request, response):
     normalized = dict()
 
@@ -9,4 +11,9 @@ def main(request, response):
         new_values = [isomorphic_decode(value) for value in values]
         normalized[isomorphic_decode(key.lower())] = new_values
 
-    return (200, response.headers, json.dumps(normalized))
+    headers = []
+    if request.headers.get(b"origin") != None:
+      headers = [(b"Access-Control-Allow-Origin",request.headers.get(b"origin")),
+                 (b"Access-Control-Allow-Credentials", b"true")]
+
+    return (200, headers, json.dumps(normalized))
