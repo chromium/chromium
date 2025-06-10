8450909c7c6c0b489e46a636e0269bb6f2181fc8
Rainhard Findling <rainhard@chromium.org>
2025-06-10 10:04:53 -0700

    [Synapse] Enable AiSettingsPageEnterpriseDisabledUi by default
    Bug: 362225975
    Change-Id: I570de92c226c79722d793d233095754149215e58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6610942
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471887}

--- components/optimization_guide/core/optimization_guide_features.cc
+++ components/optimization_guide/core/optimization_guide_features.cc
@@ -191,7 +191,7 @@ BASE_FEATURE(kPrivacyGuideAiSettings,
 
 BASE_FEATURE(kAiSettingsPageEnterpriseDisabledUi,
              "AiSettingsPageEnterpriseDisabledUi",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOnDeviceModelPerformanceParams,
              "OnDeviceModelPerformanceParams",
