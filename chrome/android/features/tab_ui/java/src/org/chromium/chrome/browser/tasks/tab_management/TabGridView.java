ab9667369d6540f661a4b1ea19c83d698d021a1f
Fiaz Muhammad <mfiaz@google.com>
2025-06-10 16:10:22 -0700

    [Tab Group Suggest] Fade in/out animations for GTS tab card highlight
    - Adds fade-in/out animation for highlight view for tab cards
    
    Bug: 422480549
    Change-Id: Ifbc6dc8141eb2175a555aecdb52fd6e938a7a135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633711
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472119}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridView.java
@@ -6,6 +6,8 @@ package org.chromium.chrome.browser.tasks.tab_management;
 
 import static org.chromium.chrome.browser.tasks.tab_management.TabListModel.CardProperties.BASE_ANIMATION_DURATION_MS;
 import static org.chromium.chrome.browser.tasks.tab_management.TabUiThemeProvider.getTabCardHighlightBackgroundTintList;
+import static org.chromium.ui.animation.CommonAnimationsFactory.createFadeInAnimation;
+import static org.chromium.ui.animation.CommonAnimationsFactory.createFadeOutAnimation;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -36,6 +38,7 @@ import org.chromium.chrome.browser.tasks.tab_management.TabProperties.TabActionS
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.components.browser_ui.widget.selectable_list.SelectableItemViewBase;
 import org.chromium.ui.UiUtils;
+import org.chromium.ui.animation.AnimationHandler;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -62,6 +65,7 @@ public class TabGridView extends SelectableItemViewBase<TabListEditorItemSelecti
         int NUM_ENTRIES = 3;
     }
 
+    private final AnimationHandler mHighlightAnimationHandler = new AnimationHandler();
     private boolean mIsAnimating;
     private boolean mShowOverflowButton;
     private @TabActionState int mTabActionState = TabActionState.UNSET;
@@ -205,15 +209,16 @@ public class TabGridView extends SelectableItemViewBase<TabListEditorItemSelecti
         View cardWrapper = findViewById(R.id.card_wrapper);
         if (isHighlighted) {
             Context context = getContext();
-            cardWrapper.setVisibility(VISIBLE);
             gridCardHighlightDrawable =
                     UiUtils.getTintedDrawable(
                             context,
                             R.drawable.tab_grid_card_highlight,
                             getTabCardHighlightBackgroundTintList(context, isIncognito));
-        } else {
-            cardWrapper.setVisibility(GONE);
         }
+        mHighlightAnimationHandler.startAnimation(
+                isHighlighted
+                        ? createFadeInAnimation(cardWrapper)
+                        : createFadeOutAnimation(cardWrapper));
         cardWrapper.setBackground(gridCardHighlightDrawable);
     }
 
