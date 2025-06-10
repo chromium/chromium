a1af3668974672c9172a0396c6390d5a88eeb87f
chromium-autoroll <chromium-autoroll@skia-public.iam.gserviceaccount.com>
2025-06-10 07:10:04 -0700

    Roll FreeType from 320b72a2955a to a67b2bab9ef4 (4 revisions)
    https://chromium.googlesource.com/chromium/src/third_party/freetype2.git/+log/320b72a2955a..a67b2bab9ef4
    
    2025-06-10 wl@gnu.org [gxvar] Really fix setting of named-instance to Regular.
    2025-06-10 wl@gnu.org Two more link fixes.
    2025-06-10 wl@gnu.org Update links.
    2025-06-07 behdad@behdad.org Fix setting of named-instance to Regular
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/freetype-chromium
    Please CC bungeman@google.com,drott@google.com,thestig@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_msan_rel_ng;luci.chromium.try:linux-blink-rel;luci.chromium.try:mac12.0-blink-rel;luci.chromium.try:win10.20h2-blink-rel
    Tbr: bungeman@google.com,drott@google.com,thestig@google.com
    Change-Id: Iad4abf1d74d80577489a83f54e39c0026e70e433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6632767
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Ben Wagner <bungeman@google.com>
    Bot-Commit: chromium-autoroll <chromium-autoroll@skia-public.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1471761}

--- third_party/freetype/include/freetype-custom/freetype/config/ftoption.h
+++ third_party/freetype/include/freetype-custom/freetype/config/ftoption.h
@@ -706,7 +706,7 @@ FT_BEGIN_HEADER
    * defined.
    *
    * [1]
-   * https://www.microsoft.com/typography/cleartype/truetypecleartype.aspx
+   * https://learn.microsoft.com/typography/cleartype/truetypecleartype
    */
 #define TT_CONFIG_OPTION_SUBPIXEL_HINTING
 
@@ -724,7 +724,7 @@ FT_BEGIN_HEADER
    * flags array which can be used to disambiguate, but old fonts will not
    * have them.
    *
-   *   https://www.microsoft.com/typography/otspec/glyf.htm
+   *   https://learn.microsoft.com/typography/opentype/spec/glyf
    *   https://developer.apple.com/fonts/TrueType-Reference-Manual/RM06/Chap6glyf.html
    */
 #undef TT_CONFIG_OPTION_COMPONENT_OFFSET_SCALED
