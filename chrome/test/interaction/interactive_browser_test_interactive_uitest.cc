783136f90846168d46b1d497894849089441e7b2
Dana Fried <dfried@chromium.org>
2025-06-10 07:11:17 -0700

    Revert "[gardener] Disable flaky `InteractiveBrowserTestUiTest .SendKeyToWebContents` on Windows"
    This reverts commit 45b8ef75f36ebd22d50508a2fc6569b877fb0783.
    
    Reason for revert: This apparently flaked randomly, once, and was disabled. I don't believe it was actually the test that failed, but a bot or sharding issue.
    
    I'm retrying relevant win CI builders to see if the problem reproduces.
    
    Bug: 422501415
    Original change's description:
    > [gardener] Disable flaky `InteractiveBrowserTestUiTest .SendKeyToWebContents` on Windows
    >
    > Bug: 422501415
    > Change-Id: Ia6083463c9ce383c233bbb7a53bd704f653b9b9f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6622715
    > Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    > Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    > Owners-Override: Ming-Ying Chung <mych@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1469804}
    
    Bug: 422501415
    Change-Id: I1be910c45f6f899cbae3b76a3188ccee7a88404b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633789
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1471763}

--- chrome/test/interaction/interactive_browser_test_interactive_uitest.cc
+++ chrome/test/interaction/interactive_browser_test_interactive_uitest.cc
@@ -493,14 +493,7 @@ IN_PROC_BROWSER_TEST_F(InteractiveBrowserTestUiTest,
       WaitForStateChange(kWebContentsId, clear_all_downloads_click));
 }
 
-// TODO(crbug.com/422501415): Re-enable this test on Windows.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_SendKeyToWebContents DISABLED_SendKeyToWebContents
-#else
-#define MAYBE_SendKeyToWebContents SendKeyToWebContents
-#endif
-IN_PROC_BROWSER_TEST_F(InteractiveBrowserTestUiTest,
-                       MAYBE_SendKeyToWebContents) {
+IN_PROC_BROWSER_TEST_F(InteractiveBrowserTestUiTest, SendKeyToWebContents) {
   DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kWebContentsId);
   const GURL url = embedded_test_server()->GetURL(kDocumentWithTextField);
   const DeepQuery kTextField = {"#value"};
