d917ecfe48e4e329ef1c73ab7cd9a44baf451f22
Jeff Yoon <jeffyoon@google.com>
2025-06-11 13:39:22 -0700

    [perf] disable tests for system_health_smoke_test
    Ticket filed against those tests for being flaky.
    
    Bug: 422824099
    Change-Id: I4535d77fab55df80275790499911cd48ab1875ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6638571
    Commit-Queue: John Chen <johnchen@chromium.org>
    Auto-Submit: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    Reviewed-by: John Chen <johnchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472652}

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -82,6 +82,14 @@ _DISABLED_TESTS = frozenset({
     # crbug.com/418717796 - flaky
     'system_health.memory_desktop/load:media:youtubelivingroom:2020',
 
+    # crbug.com/422824099
+    'system_health.memory_desktop/browse:news:nytimes:2020'
+    'system_health.memory_desktop/browse:tools:gmail-labelclick:2020'
+    'system_health.memory_desktop/load:games:lazors',
+    'system_health.memory_desktop/load:media:youtube:2018',
+    'system_health.memory_desktop/load:search:ebay:2018',
+    'system_health.memory_desktop/load:tools:gmail:2019',
+
     # The following tests are disabled because they are disabled on the perf
     # waterfall (using tools/perf/expectations.config) on one platform or
     # another. They may run fine on the CQ, but it isn't worth the bot time to
