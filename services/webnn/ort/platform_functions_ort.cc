a0d3fd17aa44f1e1819f303969530bbf214466c8
mingmingtasd <mingming1.xu@intel.com>
2025-06-10 18:55:09 -0700

    WebNN: Enable loading onnxruntime.dll from Windows ML within sandbox
    This CL introduces the Windows ML for ONNX Runtime backend and enables
    loading the onnxruntime.dll within sandbox.
    
    When creating the `PlatformFunctions` instance, Windows ML is
    initialized which allows loading the onnxruntime.dll from the
    installation path of Windows ML package. At current stage, you need
    manually install the Windows ML package [1] at first.
    
    [1]: https://apps.microsoft.com/detail/9mvl55dvgwww
    
    Bug: 420462738
    Change-Id: Id5de77ff00ff5bdaf2158a7cb840fd89d7a9a30f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6623244
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1472171}

--- services/webnn/ort/platform_functions_ort.cc
+++ services/webnn/ort/platform_functions_ort.cc
@@ -4,14 +4,125 @@
 
 #include "services/webnn/ort/platform_functions_ort.h"
 
+#include <windows.h>
+
+#include <appmodel.h>
+#include <winerror.h>
+#include <wrl.h>
+
+#include <optional>
+
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/native_library.h"
 #include "base/path_service.h"
+#include "base/strings/string_util_win.h"
 
 namespace webnn::ort {
 
+namespace {
+
 using OrtGetApiBaseProc = decltype(OrtGetApiBase)*;
+using TryCreatePackageDependencyProc = decltype(TryCreatePackageDependency)*;
+using AddPackageDependencyProc = decltype(AddPackageDependency)*;
+
+constexpr int kMinVersionMajor = 0;
+constexpr int kMinVersionMinor = 0;
+constexpr int kMinVersionBuild = 0;
+constexpr int kMinVersionRevision = 0;
+constexpr wchar_t kWindowsMLPackageFamilyName[] =
+    L"Microsoft.WindowsMLRuntime.0.3_8wekyb3d8bbwe";
+
+struct ScopedWcharTypeTraits {
+  static wchar_t* InvalidValue() { return nullptr; }
+  static void Free(wchar_t* value) {
+    if (value) {
+      ::HeapFree(GetProcessHeap(), 0, value);
+    }
+  }
+};
+using ScopedWcharType = base::ScopedGeneric<wchar_t*, ScopedWcharTypeTraits>;
+
+std::optional<base::FilePath> GetPackagePath(const wchar_t* package_full_name) {
+  uint32_t path_length = 0;
+
+  // Get the required path length.
+  int32_t result =
+      GetPackagePathByFullName(package_full_name, &path_length, nullptr);
+  if (result != ERROR_INSUFFICIENT_BUFFER) {
+    LOG(ERROR) << "Failed to get package path length for package: "
+               << package_full_name << ". Error: "
+               << logging::SystemErrorCodeToString(HRESULT_FROM_WIN32(result));
+    return std::nullopt;
+  }
+
+  // Get the actual path.
+  std::wstring path_buffer;
+  result = GetPackagePathByFullName(package_full_name, &path_length,
+                                    base::WriteInto(&path_buffer, path_length));
+  if (result != ERROR_SUCCESS) {
+    LOG(ERROR) << "Failed to get package path for package: "
+               << package_full_name << ". Error: "
+               << logging::SystemErrorCodeToString(HRESULT_FROM_WIN32(result));
+    return std::nullopt;
+  }
+
+  return base::FilePath(path_buffer);
+}
+
+std::optional<base::FilePath> InitializeWindowsML() {
+  // KernelBase should always be present on Win10+ machines.
+  base::ScopedNativeLibrary app_model_library(
+      base::LoadSystemLibrary(L"KernelBase.dll", nullptr));
+  CHECK(app_model_library.is_valid());
+
+  TryCreatePackageDependencyProc try_create_package_dependency_proc =
+      reinterpret_cast<TryCreatePackageDependencyProc>(
+          app_model_library.GetFunctionPointer("TryCreatePackageDependency"));
+  AddPackageDependencyProc add_package_dependency_proc =
+      reinterpret_cast<AddPackageDependencyProc>(
+          app_model_library.GetFunctionPointer("AddPackageDependency"));
+  if (!try_create_package_dependency_proc || !add_package_dependency_proc) {
+    LOG(ERROR) << "Failed to get TryCreatePackageDependency and "
+                  "AddPackageDependency functions from KernelBase.dll.";
+    return std::nullopt;
+  }
+
+  PACKAGE_VERSION min_version = {.Major = kMinVersionMajor,
+                                 .Minor = kMinVersionMinor,
+                                 .Build = kMinVersionBuild,
+                                 .Revision = kMinVersionRevision};
+  ScopedWcharType package_dependency_id;
+  HRESULT hr = try_create_package_dependency_proc(
+      /*user=*/nullptr, kWindowsMLPackageFamilyName, min_version,
+      PackageDependencyProcessorArchitectures_None,
+      PackageDependencyLifetimeKind_Process,
+      /*lifetimeArtifact=*/nullptr, CreatePackageDependencyOptions_None,
+      ScopedWcharType::Receiver(package_dependency_id).get());
+  if (FAILED(hr)) {
+    LOG(ERROR) << "TryCreatePackageDependency failed for package: "
+               << kWindowsMLPackageFamilyName
+               << ". Error: " << logging::SystemErrorCodeToString(hr);
+    return std::nullopt;
+  }
+
+  PACKAGEDEPENDENCY_CONTEXT context{};
+  ScopedWcharType package_full_name;
+  hr = add_package_dependency_proc(
+      package_dependency_id.get(), /*rank=*/0,
+      AddPackageDependencyOptions_PrependIfRankCollision, &context,
+      ScopedWcharType::Receiver(package_full_name).get());
+  if (FAILED(hr)) {
+    LOG(ERROR) << "AddPackageDependency failed for package: "
+               << package_full_name.get()
+               << ". Error: " << logging::SystemErrorCodeToString(hr);
+    return std::nullopt;
+  }
+
+  return GetPackagePath(package_full_name.get());
+}
+
+}  // namespace
 
 PlatformFunctions::PlatformFunctions() {
   // First try to load onnxruntime.dll from the module folder. This enables
@@ -21,13 +132,23 @@ PlatformFunctions::PlatformFunctions() {
                                   .Append(L"onnxruntime.dll"),
                               nullptr));
 
+  // If it failed to load from module folder, try to load from the Windows ML
+  // package.
   if (!ort_library.is_valid()) {
-    ort_library =
-        base::ScopedNativeLibrary(base::LoadSystemLibrary(L"onnxruntime.dll"));
-  }
-  if (!ort_library.is_valid()) {
-    LOG(ERROR) << "[WebNN] Failed to load onnxruntime.dll.";
-    return;
+    // Initialize Windows ML.
+    std::optional<base::FilePath> package_path = InitializeWindowsML();
+    if (!package_path) {
+      LOG(ERROR) << "Failed to initialize Windows ML and get the package path.";
+      return;
+    }
+
+    ort_library = base::ScopedNativeLibrary(base::LoadNativeLibrary(
+        package_path->Append(L"onnxruntime.dll"), nullptr));
+    if (!ort_library.is_valid()) {
+      LOG(ERROR) << "[WebNN] Failed to load onnxruntime.dll from package path: "
+                 << package_path->value();
+      return;
+    }
   }
 
   OrtGetApiBaseProc ort_get_api_base_proc = reinterpret_cast<OrtGetApiBaseProc>(
