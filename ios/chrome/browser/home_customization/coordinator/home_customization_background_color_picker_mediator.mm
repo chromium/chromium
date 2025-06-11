1be4e76282f410bf730ca88fa4f19cbd127cbb8e
Pier-Alexandre Bouchard <pabouchard@google.com>
2025-06-11 14:24:22 -0700

    [iOS] Add final colors to the background picker
    The background picker now includes the following colors:
    
    - Blue (#8cabe4)
    - Aqua (#26a69a)
    - Green (#00ff00)
    - Viridian (#87ba81)
    - Citron (#fadf73)
    - Orange (#ff8000)
    - Rose (#f3b2be)
    - Fuchsia (#ff00ff)
    - Violet (#e5d5fc)
    
    The first option in the top-left corner is the default "no background"
    selection, which reflects the standard appearance of the New Tab Page,
    including the base background color, omnibox background, and default
    blue used on buttons.
    
    Specs:
    https://www.figma.com/design/7WmuFjzXdr7XnJ0lvpHXtI/Bling-NTP-Customization--Old-Version-?node-id=1136-245339&p=f&m=dev
    
    https://drive.google.com/file/d/1zrk70aq9t7H6lql6MdYZGVmrHDLL_PTV/view?resourcekey=0-lRo9Y0ImuSnWvpMU1N09gQ
    
    Old experience: https://screenshot.googleplex.com/4YHEDpvSADrNpQG
    
    New experience: https://screenshot.googleplex.com/9wJsz2oUau5bwUT
    
    Fixed: 418004085
    
    Change-Id: I55f8a6ebbbfa208c2a20f86b3ed5f7af96b64784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633561
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472685}

--- ios/chrome/browser/home_customization/coordinator/home_customization_background_color_picker_mediator.mm
+++ ios/chrome/browser/home_customization/coordinator/home_customization_background_color_picker_mediator.mm
@@ -5,20 +5,43 @@
 #import "ios/chrome/browser/home_customization/coordinator/home_customization_background_color_picker_mediator.h"
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #import "ios/chrome/browser/home_customization/ui/home_customization_background_color_picker_consumer.h"
 #import "ios/chrome/browser/home_customization/ui/home_customization_color_palette_util.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+
+namespace {
+UIColor* const kBlue = UIColorFromRGB(0x8cabe4);
+UIColor* const kAqua = UIColorFromRGB(0x26a69a);
+UIColor* const kGreen = UIColorFromRGB(0x00ff00);
+UIColor* const kViridian = UIColorFromRGB(0x87ba81);
+UIColor* const kCitron = UIColorFromRGB(0xfadf73);
+UIColor* const kOrange = UIColorFromRGB(0xff8000);
+UIColor* const kRose = UIColorFromRGB(0xf3b2be);
+UIColor* const kFuchsia = UIColorFromRGB(0xff00ff);
+UIColor* const kViolet = UIColorFromRGB(0xe5d5fc);
+}  // namespace
 
 @implementation HomeCustomizationBackgroundColorPickerMediator
 
 - (void)configureColorPalettes {
   NSMutableArray* configs = [NSMutableArray array];
 
-  // TODO(crbug.com/408243803):Add the UIColor seeds that will be used by Monet
-  // to generate the color palettes.
+  HomeCustomizationColorPaletteConfiguration* defaultColorPalette =
+      [[HomeCustomizationColorPaletteConfiguration alloc] init];
+
+  // The first choice should be the "no background" option (default appearance
+  // colors).
+  defaultColorPalette.lightColor = [UIColor colorNamed:@"ntp_background_color"];
+  defaultColorPalette.darkColor = [UIColor colorNamed:kBlue600Color];
+  defaultColorPalette.mediumColor =
+      [UIColor colorNamed:@"fake_omnibox_solid_background_color"];
+  [configs addObject:defaultColorPalette];
+
   [@[
-    [UIColor redColor], [UIColor blueColor], [UIColor greenColor],
-    [UIColor orangeColor], [UIColor purpleColor]
+    kBlue, kAqua, kGreen, kViridian, kCitron, kOrange, kRose, kFuchsia, kViolet
   ] enumerateObjectsUsingBlock:^(UIColor* seedColor, NSUInteger index,
                                  BOOL* stop) {
     [configs addObject:CreateColorPaletteConfigurationFromSeedColor(seedColor)];
