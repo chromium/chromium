d13f73917564308b2f5f514938521d787565605f
Lambros Lambrou <lambroslambrou@chromium.org>
2025-06-11 16:06:43 -0700

    [remoting host] Enable Wayland if the command-line flag is present.
    This modifies CreateDesktopInteractionStrategyFactory() to create the
    GNOME-specific implementation if the `--enable-wayland` switch is found
    in the host process's command-line.
    
    Bug: 378932951
    Change-Id: I9a3d9e7ea6f12eeed1fd82a08bfc683dbfb5b8dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6637855
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472741}

--- remoting/host/create_desktop_interaction_strategy_factory.cc
+++ remoting/host/create_desktop_interaction_strategy_factory.cc
@@ -7,10 +7,15 @@
 #include <memory>
 #include <utility>
 
+#include "base/command_line.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/single_thread_task_runner.h"
 #include "remoting/host/desktop_interaction_strategy.h"
 #include "remoting/host/legacy_interaction_strategy.h"
+
+#if BUILDFLAG(IS_LINUX)
+#include "remoting/host/linux/gnome_interaction_strategy.h"
+#endif  // BUILDFLAG(IS_LINUX)
 
 namespace remoting {
 
@@ -20,6 +25,12 @@ CreateDesktopInteractionStrategyFactory(
     scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
     scoped_refptr<base::SingleThreadTaskRunner> video_capture_task_runner,
     scoped_refptr<base::SingleThreadTaskRunner> input_task_runner) {
+#if BUILDFLAG(IS_LINUX)
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch("enable-wayland")) {
+    return std::make_unique<GnomeInteractionStrategyFactory>(ui_task_runner);
+  }
+#endif  // BUILDFLAG(IS_LINUX)
+
   return std::make_unique<LegacyInteractionStrategyFactory>(
       std::move(caller_task_runner), std::move(ui_task_runner),
       std::move(video_capture_task_runner), std::move(input_task_runner));
