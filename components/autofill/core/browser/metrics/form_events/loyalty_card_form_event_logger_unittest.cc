711116b5f5c00a8b88d4319dcc35654ee3939fcd
Norge Vizcay <vizcay@google.com>
2025-06-12 01:49:33 -0700

    Include more key metric tests for loyalty cards.
    Bug: 422366498
    Change-Id: I199b83606a43b5b4a70de628acb1bb4ed981d622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634594
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1472909}

--- components/autofill/core/browser/metrics/form_events/loyalty_card_form_event_logger_unittest.cc
+++ components/autofill/core/browser/metrics/form_events/loyalty_card_form_event_logger_unittest.cc
@@ -6,6 +6,7 @@
 
 #include "base/test/metrics/histogram_tester.h"
 #include "components/autofill/core/browser/data_manager/valuables/valuables_data_manager_test_api.h"
+#include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics_test_base.h"
 #include "components/autofill/core/browser/test_utils/valuables_data_test_utils.h"
 #include "components/autofill/core/common/autofill_test_utils.h"
@@ -205,6 +206,166 @@ TEST_P(LoyaltyCardFormEventLoggerFunnelTest, LogKeyMetrics) {
         "Autofill.KeyMetrics.FormSubmission.Autofilled.LoyaltyCard",
         user_submitted_form ? 1 : 0, 1);
   }
+}
+
+// Tests for Autofill.KeyMetrics.* metrics.
+class LoyaltyCardFormEventLoggerBaseKeyMetricsTest
+    : public AutofillMetricsBaseTest,
+      public testing::Test {
+ public:
+  void SetUp() override {
+    SetUpHelper();
+
+    test_api(valuables_data_manager())
+        .SetLoyaltyCards(
+            {test::CreateLoyaltyCard(), test::CreateLoyaltyCard2()});
+
+    // Load a fillable form.
+    form_ =
+        CreateForm({CreateTestFormField("Loyalty Program", "loyalty-program",
+                                        "", FormControlType::kInputText),
+                    CreateTestFormField("Loyalty Number", "loyalty-number", "",
+                                        FormControlType::kInputText)});
+
+    field_types_ = {LOYALTY_MEMBERSHIP_PROGRAM, LOYALTY_MEMBERSHIP_ID};
+    autofill_manager().AddSeenForm(form_, field_types_, field_types_);
+  }
+
+  void TearDown() override { TearDownHelper(); }
+
+  // Fillable form.
+  FormData form_;
+  std::vector<FieldType> field_types_;
+};
+
+// Validate Autofill.KeyMetrics.* in case the user submits an empty form.
+// Empty in the sense that the user did not fill/type into the fields (not that
+// it has no fields).
+TEST_F(LoyaltyCardFormEventLoggerBaseKeyMetricsTest, LogEmptyForm) {
+  base::HistogramTester histogram_tester;
+
+  // Simulate page load.
+  SeeForm(form_);
+  autofill_manager().OnAskForValuesToFillTest(form_,
+                                              form_.fields()[0].global_id());
+  SubmitForm(form_);
+
+  ResetDriverToCommitMetrics();
+
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingReadiness.LoyaltyCard", 1, 1);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingAcceptance.LoyaltyCard", 0);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingCorrectness.LoyaltyCard", 0);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingAssistance.LoyaltyCard", 0, 1);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FormSubmission.NotAutofilled.LoyaltyCard", 0);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingAcceptance.GroupedByFocusedFieldType", 0);
+}
+
+// Validate Autofill.KeyMetrics.* in case the user does not accept a suggestion.
+TEST_F(LoyaltyCardFormEventLoggerBaseKeyMetricsTest,
+       LogUserDoesNotAcceptSuggestion) {
+  base::HistogramTester histogram_tester;
+
+  // Simulate that suggestion is shown but user does not accept it.
+  SeeForm(form_);
+  autofill_manager().OnAskForValuesToFillTest(form_,
+                                              form_.fields()[1].global_id());
+  DidShowAutofillSuggestions(form_, /*field_index=*/1);
+
+  SimulateUserChangedField(form_, form_.fields()[0]);
+  SimulateUserChangedField(form_, form_.fields()[1]);
+  SubmitForm(form_);
+
+  ResetDriverToCommitMetrics();
+
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingReadiness.LoyaltyCard", 1, 1);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingAcceptance.LoyaltyCard", 0, 1);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingCorrectness.LoyaltyCard", 0);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingAssistance.LoyaltyCard", 0, 1);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FormSubmission.NotAutofilled.LoyaltyCard", 1, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Autofill.KeyMetrics.FillingAcceptance.GroupedByFocusedFieldType",
+      GetBucketForAcceptanceMetricsGroupedByFieldType(
+          field_types_[1], /*suggestion_accepted=*/false),
+      1);
+}
+// Validate Autofill.KeyMetrics.* in case the user has to fix the filled data.
+TEST_F(LoyaltyCardFormEventLoggerBaseKeyMetricsTest, LogUserFixesFilledData) {
+  base::HistogramTester histogram_tester;
+
+  // Simulate that suggestion is shown and user accepts it.
+  SeeForm(form_);
+  autofill_manager().OnAskForValuesToFillTest(form_,
+                                              form_.fields()[1].global_id());
+  DidShowAutofillSuggestions(form_, /*field_index=*/1);
+  FillLoyaltyCard(form_, valuables_data_manager().GetLoyaltyCards()[0],
+                  /*field_index=*/1);
+
+  // Simulate user fixing the card.
+  SimulateUserChangedField(form_, form_.fields()[1]);
+  SubmitForm(form_);
+
+  ResetDriverToCommitMetrics();
+
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingReadiness.LoyaltyCard", 1, 1);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingAcceptance.LoyaltyCard", 1, 1);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingCorrectness.LoyaltyCard", 0, 1);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FillingAssistance.LoyaltyCard", 1, 1);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FormSubmission.Autofilled.LoyaltyCard", 1, 1);
+  histogram_tester.ExpectUniqueSample(
+      "Autofill.KeyMetrics.FillingAcceptance.GroupedByFocusedFieldType",
+      GetBucketForAcceptanceMetricsGroupedByFieldType(
+          field_types_[1], /*suggestion_accepted=*/true),
+      1);
+}
+
+// Validate Autofill.KeyMetrics.* in case the user fixes the filled data but
+// then does not submit the form.
+TEST_F(LoyaltyCardFormEventLoggerBaseKeyMetricsTest,
+       LogUserFixesFilledDataButDoesNotSubmit) {
+  base::HistogramTester histogram_tester;
+
+  // Simulate that suggestion is shown and user accepts it.
+  SeeForm(form_);
+  autofill_manager().OnAskForValuesToFillTest(form_,
+                                              form_.fields()[1].global_id());
+  DidShowAutofillSuggestions(form_);
+  FillLoyaltyCard(form_, valuables_data_manager().GetLoyaltyCards()[0],
+                  /*field_index=*/1);
+
+  // Simulate user fixing the card.
+  SimulateUserChangedField(form_, form_.fields()[1]);
+
+  // Don't submit form.
+  ResetDriverToCommitMetrics();
+
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingReadiness.LoyaltyCard", 0);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingAcceptance.LoyaltyCard", 0);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingCorrectness.LoyaltyCard", 0);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingAssistance.LoyaltyCard", 0);
+  histogram_tester.ExpectBucketCount(
+      "Autofill.KeyMetrics.FormSubmission.Autofilled.LoyaltyCard", 0, 1);
+  histogram_tester.ExpectTotalCount(
+      "Autofill.KeyMetrics.FillingAcceptance.GroupedByFocusedFieldType", 0);
 }
 
 }  // namespace autofill::autofill_metrics
