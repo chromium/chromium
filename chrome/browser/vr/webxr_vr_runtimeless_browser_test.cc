dbf01e1772610a58903ba84257e728e4d7d99feb
Brian Sheedy <bsheedy@chromium.org>
2025-06-12 14:39:32 -0700

    Disable XR browser test on Linux
    Disables WebXrVrRuntimelessBrowserTest.TestWebXrNoDevicesWithoutRuntime
    on Linux since it is flakily timing out.
    
    Bug: 424430986
    Change-Id: Ieab78652508c4bdb4308692958af76883d74ffcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6635518
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473178}

--- chrome/browser/vr/webxr_vr_runtimeless_browser_test.cc
+++ chrome/browser/vr/webxr_vr_runtimeless_browser_test.cc
@@ -9,8 +9,15 @@ namespace vr {
 
 // Tests that WebXR does not return any devices if all runtime support is
 // disabled.
+#if BUILDFLAG(IS_LINUX)
+// TODO(crbug.com/424430986): Re-enable this once the hang is fixed on Linux
+#define MAYBE_TestWebXrNoDevicesWithoutRuntime \
+  DISABLED_TestWebXrNoDevicesWithoutRuntime
+#else
+#define MAYBE_TestWebXrNoDevicesWithoutRuntime TestWebXrNoDevicesWithoutRuntime
+#endif
 IN_PROC_BROWSER_TEST_F(WebXrVrRuntimelessBrowserTest,
-                       TestWebXrNoDevicesWithoutRuntime) {
+                       MAYBE_TestWebXrNoDevicesWithoutRuntime) {
   LoadFileAndAwaitInitialization("test_webxr_does_not_return_device");
   WaitOnJavaScriptStep();
   EndTest();
