305524c87d25221670f30732359d9981b87fc1cb
Hidehiko Abe <hidehiko@chromium.org>
2025-06-11 22:34:39 -0700

    Remove ChapsService crosapi
    which is no longer used.
    
    BUG=354842935
    TEST=Tryjob
    
    Change-Id: Ie09b2f9086e39cb98efcec9a47f25f8a727034b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6636992
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Di Wu <diwux@google.com>
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1472850}

--- chrome/browser/ash/crosapi/crosapi_ash.h
+++ chrome/browser/ash/crosapi/crosapi_ash.h
@@ -44,7 +44,6 @@ class PrintPreviewWebcontentsAdapterAsh;
 namespace crosapi {
 
 class CertProvisioningAsh;
-class ChapsServiceAsh;
 class DeviceAttributesAsh;
 class DeviceOAuth2TokenServiceAsh;
 class DocumentScanAsh;
@@ -85,8 +84,6 @@ class CrosapiAsh : public mojom::Crosapi {
   void BindCfmServiceContext(
       mojo::PendingReceiver<chromeos::cfm::mojom::CfmServiceContext> receiver)
       override;
-  void BindChapsService(
-      mojo::PendingReceiver<mojom::ChapsService> receiver) override;
   void BindCrosDisplayConfigController(
       mojo::PendingReceiver<mojom::CrosDisplayConfigController> receiver)
       override;
@@ -172,8 +169,6 @@ class CrosapiAsh : public mojom::Crosapi {
     return cert_provisioning_ash_.get();
   }
 
-  ChapsServiceAsh* chaps_service_ash() { return chaps_service_ash_.get(); }
-
   DeviceAttributesAsh* device_attributes_ash() {
     return device_attributes_ash_.get();
   }
@@ -241,7 +236,6 @@ class CrosapiAsh : public mojom::Crosapi {
   void OnDisconnected();
 
   std::unique_ptr<CertProvisioningAsh> cert_provisioning_ash_;
-  std::unique_ptr<ChapsServiceAsh> chaps_service_ash_;
   std::unique_ptr<DeviceAttributesAsh> device_attributes_ash_;
   std::unique_ptr<DeviceOAuth2TokenServiceAsh> device_oauth2_token_service_ash_;
   std::unique_ptr<ash::DiagnosticsServiceAsh> diagnostics_service_ash_;
