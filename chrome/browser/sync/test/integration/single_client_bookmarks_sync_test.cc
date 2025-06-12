1d43bb36a45a3394e3c896b1f985ae8e2b67dd79
Mikel Astiz <mastiz@chromium.org>
2025-06-12 09:54:36 -0700

    [bookmarks] Fix histograms.xml to reflect bug in comparison metrics
    Due to a bug, annotated in this patch with a TODO, the instrumented
    metric ended up having a different behavior than the one intended. In
    this first patch, histograms.xml/enums.xml are updated to reflect the
    current (accidental) behavior.
    
    NO_IFTTT=Intentional bugfix
    
    Change-Id: I148f760a518e57446259259e3bd6bb091f3f3278
    Bug: 424163391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633477
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473118}

--- chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_bookmarks_sync_test.cc
@@ -3168,12 +3168,12 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, PRE_ComparisonMetrics) {
   histogram_tester.ExpectUniqueSample(
       "Sync.BookmarkModelMerger.Comparison.MatchesPreviousGaiaId."
       "ConsideringAllBookmarks.ByUrl",
-      /*sample=*/4 /*kLocalDataIsStrictSubsetOfAccountData*/,
+      /*sample=*/4 /*kAccountDataIsStrictSubsetOfLocalData*/,
       /*expected_bucket_count=*/1);
   histogram_tester.ExpectUniqueSample(
       "Sync.BookmarkModelMerger.Comparison.MatchesPreviousGaiaId."
       "ConsideringAllBookmarks.ByUrlAndUuid",
-      /*sample=*/4 /*kLocalDataIsStrictSubsetOfAccountData*/,
+      /*sample=*/4 /*kAccountDataIsStrictSubsetOfLocalData*/,
       /*expected_bucket_count=*/1);
   histogram_tester.ExpectUniqueSample(
       "Sync.BookmarkModelMerger.Comparison.MatchesPreviousGaiaId."
@@ -3183,7 +3183,7 @@ IN_PROC_BROWSER_TEST_F(SingleClientBookmarksSyncTest, PRE_ComparisonMetrics) {
   histogram_tester.ExpectUniqueSample(
       "Sync.BookmarkModelMerger.Comparison.MatchesPreviousGaiaId."
       "ConsideringAllBookmarks.ByUrlAndUuid.Between1And19LocalUrlBookmarks",
-      /*sample=*/4 /*kLocalDataIsStrictSubsetOfAccountData*/,
+      /*sample=*/4 /*kAccountDataIsStrictSubsetOfLocalData*/,
       /*expected_bucket_count=*/1);
 
   // Enable Sync with a different account.
