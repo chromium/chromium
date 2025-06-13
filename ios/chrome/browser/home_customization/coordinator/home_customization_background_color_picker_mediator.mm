d68806079fe013610f9e1091b521e4548c9287c1
Pier-Alexandre Bouchard <pabouchard@google.com>
2025-06-13 14:43:29 -0700

    [iOS] Add final colors to the background picker
    The background picker now includes the following colors:
    
    - Blue (#8cabe4)
    - Aqua (#26a69a)
    - Green (#00ff00)
    - Viridian (#87ba81)
    - Citron (#fadf73)
    - Orange (#ff8000)
    - Rose (#f3b2be)
    - Fuchsia (#ff00ff)
    - Violet (#e5d5fc)
    
    The first option in the top-left corner is the default "no background"
    selection, which reflects the standard appearance of the New Tab Page.
    
    Specs:
    https://www.figma.com/design/7WmuFjzXdr7XnJ0lvpHXtI/Bling-NTP-Customization--Old-Version-?node-id=1136-245339&p=f&m=dev
    
    https://drive.google.com/file/d/1zrk70aq9t7H6lql6MdYZGVmrHDLL_PTV/view?resourcekey=0-lRo9Y0ImuSnWvpMU1N09gQ
    
    Old experience: https://screenshot.googleplex.com/4YHEDpvSADrNpQG
    
    New experience (light): https://screenshot.googleplex.com/5p3snbxqrPPFurL
    
    New experience (dark):
    https://screenshot.googleplex.com/9i3ntXcTnFLgJ7Q
    
    Fixed: 418004085
    Change-Id: Ia25e74adcabf31ef2c2ae9cb669ed4e5de14dd8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6639870
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1473780}

--- ios/chrome/browser/home_customization/coordinator/home_customization_background_color_picker_mediator.mm
+++ ios/chrome/browser/home_customization/coordinator/home_customization_background_color_picker_mediator.mm
@@ -8,23 +8,64 @@
 
 #import "ios/chrome/browser/home_customization/ui/home_customization_background_color_picker_consumer.h"
 #import "ios/chrome/browser/home_customization/ui/home_customization_color_palette_util.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "skia/ext/skia_utils_ios.h"
+
+namespace {
+// Array of seed colors (in RGB integer format) used to generate background
+// color palette configurations in the color picker.
+const SkColor kSeedColorRGBs[] = {
+    0x8cabe4,  // Blue.
+    0x26a69a,  // Aqua.
+    0x00ff00,  // Green.
+    0x87ba81,  // Viridian.
+    0xfadf73,  // Citron.
+    0xff8000,  // Orange.
+    0xf3b2be,  // Rose.
+    0xff00ff,  // Fuchsia.
+    0xe5d5fc   // Violet.
+};
+
+// Returns a dynamic UIColor using two named color assets for light and dark
+// mode.
+UIColor* DynamicNamedColor(NSString* lightName, NSString* darkName) {
+  return
+      [UIColor colorWithDynamicProvider:^UIColor*(UITraitCollection* traits) {
+        BOOL isDark = (traits.userInterfaceStyle == UIUserInterfaceStyleDark);
+        return [UIColor colorNamed:isDark ? darkName : lightName];
+      }];
+}
+
+}  // namespace
 
 @implementation HomeCustomizationBackgroundColorPickerMediator
 
 - (void)configureColorPalettes {
   NSMutableArray* configs = [NSMutableArray array];
 
-  // TODO(crbug.com/408243803):Add the UIColor seeds that will be used by Monet
-  // to generate the color palettes.
-  [@[
-    [UIColor redColor], [UIColor blueColor], [UIColor greenColor],
-    [UIColor orangeColor], [UIColor purpleColor]
-  ] enumerateObjectsUsingBlock:^(UIColor* seedColor, NSUInteger index,
-                                 BOOL* stop) {
-    [configs addObject:CreateColorPaletteConfigurationFromSeedColor(seedColor)];
-  }];
+  HomeCustomizationColorPaletteConfiguration* defaultColorPalette =
+      [[HomeCustomizationColorPaletteConfiguration alloc] init];
 
-  [_consumer setColorPaletteConfigurations:configs selectedColorIndex:nil];
+  // The first choice should be the "no background" option (default appearance
+  // colors).
+  defaultColorPalette.lightColor =
+      DynamicNamedColor(@"ntp_background_color", kTertiaryBackgroundColor);
+  defaultColorPalette.mediumColor =
+      [UIColor colorNamed:@"fake_omnibox_solid_background_color"];
+  defaultColorPalette.darkColor =
+      DynamicNamedColor(kBlueColor, kTextPrimaryColor);
+
+  [configs addObject:defaultColorPalette];
+
+  for (int rgb : kSeedColorRGBs) {
+    [configs addObject:CreateColorPaletteConfigurationFromSeedColor(
+                           UIColorFromRGB(rgb))];
+  }
+
+  // TODO(crbug.com/408243803): Pass the current selection ID if the background
+  // is a color; pass 0 if the background is set to "no background".
+  [_consumer setColorPaletteConfigurations:configs selectedColorIndex:@(0)];
 }
 
 @end
