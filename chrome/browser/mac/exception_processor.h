307e7806805316bb1e6728ff1723e4837bd2e2df
Avi Drissman <avi@chromium.org>
2025-06-13 14:51:03 -0700

    Remove exception crashing code
    In https://crrev.com/c/5774165 the special-casing of crashing inside
    of Chrome was disabled, in favor of letting the crash server do its
    thing. The original code was left behind, gated behind a break-in-
    case-of-emergency flag.
    
    This was never used or needed, so remove this code.
    
    Fixed: 358138966
    Change-Id: I73d9d824d38ca6711bbf7340b998351d30199653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6629329
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1473788}

--- chrome/browser/mac/exception_processor.h
+++ chrome/browser/mac/exception_processor.h
@@ -5,23 +5,17 @@
 #ifndef CHROME_BROWSER_MAC_EXCEPTION_PROCESSOR_H_
 #define CHROME_BROWSER_MAC_EXCEPTION_PROCESSOR_H_
 
-#include <stddef.h>
-
-#include "base/feature_list.h"
-
-@class NSException;
-
 // Installs the Objective-C exception preprocessor. This records crash keys for
-// NSException objects. The preprocessor will also make fatal any exception that
-// is not handled.
+// NSException objects.
 void InstallObjcExceptionPreprocessor();
 
 // The items below are exposed only for testing.
 ////////////////////////////////////////////////////////////////////////////////
 
-BASE_DECLARE_FEATURE(kForceCrashOnExceptions);
+#if defined(UNIT_TEST)
 
-// Removes the exception preprocessor if it is installed.
-void UninstallObjcExceptionPreprocessor();
+void ResetObjcExceptionStateForTesting();
+
+#endif
 
 #endif  // CHROME_BROWSER_MAC_EXCEPTION_PROCESSOR_H_
--- chrome/browser/mac/exception_processor_unittest.mm
+++ chrome/browser/mac/exception_processor_unittest.mm
@@ -4,177 +4,68 @@
 
 #import "chrome/browser/mac/exception_processor.h"
 
-#import <Cocoa/Cocoa.h>
-#include <stddef.h>
-#include <sys/wait.h>
+#import <Foundation/Foundation.h>
 
 #include "base/mac/os_crash_dumps.h"
-#include "base/test/scoped_feature_list.h"
+#include "components/crash/core/common/crash_key.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class ExceptionProcessorTest : public testing::Test {
- public:
-  ExceptionProcessorTest() {
-    features_.InitWithFeatures({kForceCrashOnExceptions}, {});
+  void SetUp() override {
+    base::mac::DisableOSCrashDumps();
+    crash_reporter::InitializeCrashKeysForTesting();
+    ResetObjcExceptionStateForTesting();
   }
-
- protected:
-  base::test::ScopedFeatureList features_;
 };
 
-void RaiseExceptionInRunLoop() {
-  CFRunLoopRef run_loop = CFRunLoopGetCurrent();
-
-  CFRunLoopPerformBlock(run_loop, kCFRunLoopCommonModes, ^{
-    [NSException raise:@"ThrowExceptionInRunLoop" format:@""];
-  });
-  CFRunLoopPerformBlock(run_loop, kCFRunLoopCommonModes, ^{
-    CFRunLoopStop(run_loop);
-  });
-  CFRunLoopRun();
+struct CrashKeyValues {
+  std::string firstexception;
+  std::string firstexception_bt;
+  std::string lastexception;
+  std::string lastexception_bt;
+};
+CrashKeyValues GetExceptionCrashKeyValues() {
+  return {crash_reporter::GetCrashKeyValue("firstexception"),
+          crash_reporter::GetCrashKeyValue("firstexception_bt"),
+          crash_reporter::GetCrashKeyValue("lastexception"),
+          crash_reporter::GetCrashKeyValue("lastexception_bt")};
 }
 
-void ThrowExceptionInRunLoop() {
-  base::mac::DisableOSCrashDumps();
-  InstallObjcExceptionPreprocessor();
+TEST(ExceptionProcessorTest, CrashKeysRecorded) {
+  constexpr char kAtLeastEightHexValues[] = "(0x[[:xdigit:]]+ ){8}";
 
-  RaiseExceptionInRunLoop();
-
-  fprintf(stderr, "TEST FAILED\n");
-  exit(1);
-}
-
-// Tests that when the preprocessor is installed, exceptions thrown from
-// a runloop callout are made fatal, so that the stack trace is useful.
-TEST_F(ExceptionProcessorTest, ThrowExceptionInRunLoop) {
-  GTEST_FLAG_SET(death_test_style, "threadsafe");
-  EXPECT_DEATH(ThrowExceptionInRunLoop(),
-               ".*FATAL:.*exception_processor\\.mm.*"
-               "Terminating from Objective-C exception:.*");
-}
-
-void ThrowAndCatchExceptionInRunLoop() {
-  base::mac::DisableOSCrashDumps();
-  InstallObjcExceptionPreprocessor();
-
-  CFRunLoopRef run_loop = CFRunLoopGetCurrent();
-  CFRunLoopPerformBlock(run_loop, kCFRunLoopCommonModes, ^{
-    @try {
-      [NSException raise:@"ObjcExceptionPreprocessCaught" format:@""];
-    } @catch (id exception) {
-    }
-  });
-
-  CFRunLoopPerformBlock(run_loop, kCFRunLoopCommonModes, ^{
-    CFRunLoopStop(run_loop);
-  });
-
-  CFRunLoopRun();
-
-  fprintf(stderr, "TEST PASS\n");
-  exit(0);
-}
-
-// Tests that exceptions can still be caught when the preprocessor is enabled.
-TEST_F(ExceptionProcessorTest, ThrowAndCatchExceptionInRunLoop) {
-  GTEST_FLAG_SET(death_test_style, "threadsafe");
-  EXPECT_EXIT(ThrowAndCatchExceptionInRunLoop(),
-              [](int exit_code) -> bool {
-                return WEXITSTATUS(exit_code) == 0;
-              },
-              ".*TEST PASS.*");
-}
-
-void ThrowExceptionFromSelector() {
-  base::mac::DisableOSCrashDumps();
-  InstallObjcExceptionPreprocessor();
-
-  NSException* exception = [NSException exceptionWithName:@"ThrowFromSelector"
-                                                   reason:@""
-                                                 userInfo:nil];
-
-  [exception performSelector:@selector(raise) withObject:nil afterDelay:0.1];
-
-  [[NSRunLoop currentRunLoop] runUntilDate:
-      [NSDate dateWithTimeIntervalSinceNow:10]];
-
-  fprintf(stderr, "TEST FAILED\n");
-  exit(1);
-}
-
-TEST_F(ExceptionProcessorTest, ThrowExceptionFromSelector) {
-  GTEST_FLAG_SET(death_test_style, "threadsafe");
-  EXPECT_DEATH(ThrowExceptionFromSelector(),
-               ".*FATAL:.*exception_processor\\.mm.*"
-               "Terminating from Objective-C exception:.*");
-}
-
-void ThrowInNotificationObserver() {
-  base::mac::DisableOSCrashDumps();
-  InstallObjcExceptionPreprocessor();
-
-  NSNotification* notification =
-      [NSNotification notificationWithName:@"TestExceptionInObserver"
-                                    object:nil];
-
-  NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
-  [center addObserverForName:[notification name]
-                      object:nil
-                       queue:nil
-                  usingBlock:^(NSNotification*) {
-                    [NSException raise:@"ThrowInNotificationObserver"
-                                format:@""];
-                  }];
-
-  [center performSelector:@selector(postNotification:)
-               withObject:notification
-               afterDelay:0];
-
-  [[NSRunLoop currentRunLoop] runUntilDate:
-      [NSDate dateWithTimeIntervalSinceNow:10]];
-
-  fprintf(stderr, "TEST FAILED\n");
-  exit(1);
-}
-
-TEST_F(ExceptionProcessorTest, ThrowInNotificationObserver) {
-  GTEST_FLAG_SET(death_test_style, "threadsafe");
-  EXPECT_DEATH(ThrowInNotificationObserver(),
-               ".*FATAL:.*exception_processor\\.mm.*"
-               "Terminating from Objective-C exception:.*");
-}
-
-void ThrowExceptionInRunLoopWithoutProcessor() {
-  base::mac::DisableOSCrashDumps();
-  UninstallObjcExceptionPreprocessor();
+  CrashKeyValues initial_values = GetExceptionCrashKeyValues();
+  EXPECT_THAT(initial_values.firstexception, testing::IsEmpty());
+  EXPECT_THAT(initial_values.firstexception_bt, testing::IsEmpty());
+  EXPECT_THAT(initial_values.lastexception, testing::IsEmpty());
+  EXPECT_THAT(initial_values.lastexception_bt, testing::IsEmpty());
 
   @try {
-    RaiseExceptionInRunLoop();
+    [NSException raise:@"ExceptionProcessorTest" format:@""];
   } @catch (id exception) {
-    fprintf(stderr, "TEST PASS\n");
-    exit(0);
   }
 
-  fprintf(stderr, "TEST FAILED\n");
-  exit(1);
-}
+  CrashKeyValues after_first_exception = GetExceptionCrashKeyValues();
+  EXPECT_THAT(after_first_exception.firstexception,
+              testing::StartsWith("ExceptionProcessorTest reason"));
+  EXPECT_THAT(after_first_exception.firstexception_bt,
+              testing::ContainsRegex(kAtLeastEightHexValues));
+  EXPECT_THAT(after_first_exception.lastexception, testing::IsEmpty());
+  EXPECT_THAT(after_first_exception.lastexception_bt, testing::IsEmpty());
 
-// Under LSAN this dies from leaking the run loop instead of how we expect it to
-// die, so the exit code is wrong.
-#if defined(LEAK_SANITIZER)
-#define MAYBE_ThrowExceptionInRunLoopWithoutProcessor \
-  DISABLED_ThrowExceptionInRunLoopWithoutProcessor
-#else
-#define MAYBE_ThrowExceptionInRunLoopWithoutProcessor \
-  ThrowExceptionInRunLoopWithoutProcessor
-#endif
-// Tests basic exception handling when the preprocessor is disabled.
-TEST_F(ExceptionProcessorTest, MAYBE_ThrowExceptionInRunLoopWithoutProcessor) {
-  GTEST_FLAG_SET(death_test_style, "threadsafe");
-  EXPECT_EXIT(ThrowExceptionInRunLoopWithoutProcessor(),
-              [](int exit_code) -> bool {
-                return WEXITSTATUS(exit_code) == 0;
-              },
-              ".*TEST PASS.*");
+  @try {
+    [NSException raise:@"ExceptionProcessorTest2" format:@""];
+  } @catch (id exception) {
+  }
+
+  CrashKeyValues after_second_exception = GetExceptionCrashKeyValues();
+  EXPECT_THAT(after_second_exception.firstexception,
+              testing::StartsWith("ExceptionProcessorTest reason"));
+  EXPECT_THAT(after_second_exception.firstexception_bt,
+              testing::ContainsRegex(kAtLeastEightHexValues));
+  EXPECT_THAT(after_second_exception.lastexception,
+              testing::StartsWith("ExceptionProcessorTest2 reason"));
+  EXPECT_THAT(after_second_exception.lastexception_bt,
+              testing::ContainsRegex(kAtLeastEightHexValues));
 }
