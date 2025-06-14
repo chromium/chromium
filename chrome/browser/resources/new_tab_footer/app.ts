cb501c0d405868e148cc63762164cf952ea2e6b0
Paul Adedeji <pauladedeji@google.com>
2025-06-13 19:17:12 -0700

    [M138] [ntp footer] Update `FooterElement` max value.
    Original change's description:
    > [ntp footer] Update `FooterElement` max value.
    > 
    > The enum has an incorrect max value.
    > 
    > Change-Id: I971faf9b5974364ad1f7d56b4c887561e0b287dd
    > Bug: 420953568
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634716
    > Reviewed-by: Teresa Mao <temao@chromium.org>
    > Commit-Queue: Teresa Mao <temao@chromium.org>
    > Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    > Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1472053}
    
    Bug: 424209989,420953568
    Change-Id: I971faf9b5974364ad1f7d56b4c887561e0b287dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6638345
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7204@{#1250}
    Cr-Branched-From: d5de512dc9dc8ddfe4e6d71b0637578bb6158683-refs/heads/main@{#1465706}

--- chrome/browser/resources/new_tab_footer/app.ts
+++ chrome/browser/resources/new_tab_footer/app.ts
@@ -47,7 +47,7 @@ export enum FooterElement {
   OTHER = 0,
   CUSTOMIZE_BUTTON = 1,
   EXTENSION_NAME = 2,
-  MAX_VALUE = CUSTOMIZE_BUTTON,
+  MAX_VALUE = EXTENSION_NAME,
 }
 
 const CUSTOMIZE_URL_PARAM: string = 'customize';
