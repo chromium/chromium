53b853d1dfe242e379c1b04181ddc37af5472cfc
Juan Mojica <juanmojica@google.com>
2025-06-16 14:46:30 -0700

    [lensoverlay] Enable simplified selection by default.
    Bug: 425410110
    Change-Id: I4a7a898771a1bbbd6f2a2e88ae6e1e9591421190
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6648048
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1474594}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -65,7 +65,7 @@ BASE_FEATURE(kLensOverlaySidePanelOpenInNewTab,
 
 BASE_FEATURE(kLensOverlaySimplifiedSelection,
              "LensOverlaySimplifiedSelection",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensOverlayVisualSelectionUpdates,
              "LensOverlayVisualSelectionUpdates",
