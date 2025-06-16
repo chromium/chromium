13ce4ff924445f7de28c2b0e794980de1cd04585
Kaan Alsan <alsan@chromium.org>
2025-06-16 10:34:22 -0700

    Define PEH feature flags
    This CL copies PEH feature flags, which will be used for setting prefs.
    The PEH will be updated to use these flags in a separate CL.
    
    Bug: 423037244
    Change-Id: Iccf5c665fffa222e66892e97e973b8e42bd4db39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6647784
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474424}

---
+++ chrome/browser/platform_experience/features.cc
@@ -0,0 +1,19 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/platform_experience/features.h"
+
+#include "base/feature_list.h"
+
+namespace platform_experience::features {
+
+BASE_FEATURE(kDisablePEHNotifications,
+             "DisablePEHNotifications",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kShouldUseSpecificPEHNotificationText,
+             "ShouldUseSpecificPEHNotificationText",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace platform_experience::features
