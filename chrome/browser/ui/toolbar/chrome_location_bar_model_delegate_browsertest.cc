8b2b6500ee2d652a50efeb16a4d096f78b5bfe93
Dominik Klemba <tacet@google.com>
2025-06-15 18:55:21 -0700

    Migrate ChromeLocationBarModelDelegateTest to a browser test
    This patch migrates ChromeLocationBarModelDelegateTest from a unit test
    that uses the deprecated `BrowserWithTestWindowTest` fixture to a proper
    browser test that inherits from `InProcessBrowserTest`.
    
    The following changes were made:
    - The test fixture now inherits from `InProcessBrowserTest`.
    - `SetUp()` was renamed to `SetUpOnMainThread()` and the base class
      method is now called correctly.
    - `profile()` calls were replaced with `browser()->profile()`.
    - `TEST_F` macros were replaced with `IN_PROC_BROWSER_TEST_F`.
    - The `GetTestingFactories()` override was removed as it is no longer
      needed with the full browser environment provided by
      `InProcessBrowserTest`.
    - Unnecessary headers for the old test fixture were removed and new
      ones for browser tests were added.
    
    This is a pure refactoring with no change to the test logic.
    
    Change-Id: If6f255837bf1681c4f44e4fd1dbadc918a47350e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6641923
    Commit-Queue: Dominik Klemba <tacet@google.com>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474128}

--- chrome/browser/ui/toolbar/chrome_location_bar_model_delegate_browsertest.cc
+++ chrome/browser/ui/toolbar/chrome_location_bar_model_delegate_browsertest.cc
@@ -8,18 +8,17 @@
 
 #include "base/memory/raw_ptr.h"
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/search.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
-#include "chrome/browser/signin/chrome_signin_client_factory.h"
-#include "chrome/browser/signin/chrome_signin_client_test_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/search_test_utils.h"
-#include "chrome/test/base/testing_profile.h"
 #include "components/search_engines/template_url_service.h"
+#include "content/public/test/browser_test.h"
 
 // Concrete implementation of ChromeLocationBarModelDelegate.
 class TestChromeLocationBarModelDelegate
@@ -37,7 +36,6 @@ class TestChromeLocationBarModelDelegate
 
   // ChromeLocationBarModelDelegate:
   content::WebContents* GetActiveWebContents() const override {
-    browser_->tab_strip_model()->GetActiveWebContents();
     return browser_->tab_strip_model()->GetActiveWebContents();
   }
 
@@ -57,18 +55,22 @@ class TestChromeLocationBarModelDelegate
   net::CertStatus cert_status_ = 0;
 };
 
-class ChromeLocationBarModelDelegateTest : public BrowserWithTestWindowTest {
+class ChromeLocationBarModelDelegateTest : public InProcessBrowserTest {
  protected:
   ChromeLocationBarModelDelegateTest() = default;
 
-  void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
-
-    TemplateURLServiceFactory::GetInstance()->SetTestingFactoryAndUse(
-        profile(),
+  void SetUpBrowserContextKeyedServices(
+      content::BrowserContext* context) override {
+    TemplateURLServiceFactory::GetInstance()->SetTestingFactory(
+        context,
         base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor));
+  }
+
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
+
     TemplateURLService* template_url_service =
-        TemplateURLServiceFactory::GetForProfile(profile());
+        TemplateURLServiceFactory::GetForProfile(browser()->profile());
     search_test_utils::WaitForTemplateURLServiceToLoad(template_url_service);
 
     delegate_ = std::make_unique<TestChromeLocationBarModelDelegate>(browser());
@@ -76,7 +78,7 @@ class ChromeLocationBarModelDelegateTest : public BrowserWithTestWindowTest {
 
   void SetSearchProvider(bool set_ntp_url) {
     TemplateURLService* template_url_service =
-        TemplateURLServiceFactory::GetForProfile(profile());
+        TemplateURLServiceFactory::GetForProfile(browser()->profile());
     TemplateURLData data;
     data.SetShortName(u"foo.com");
     data.SetURL("http://foo.com/url?bar={searchTerms}");
@@ -97,14 +99,6 @@ class ChromeLocationBarModelDelegateTest : public BrowserWithTestWindowTest {
 
   TestChromeLocationBarModelDelegate* delegate() { return delegate_.get(); }
 
-  // BrowserWithTestWindowTest:
-  TestingProfile::TestingFactories GetTestingFactories() override {
-    return {TestingProfile::TestingFactory{
-        ChromeSigninClientFactory::GetInstance(),
-        base::BindRepeating(&BuildChromeSigninClientWithURLLoader,
-                            test_url_loader_factory())}};
-  }
-
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
   std::unique_ptr<TestChromeLocationBarModelDelegate> delegate_;
@@ -113,11 +107,11 @@ class ChromeLocationBarModelDelegateTest : public BrowserWithTestWindowTest {
 // Tests whether ChromeLocationBarModelDelegate::IsNewTabPage and
 // ChromeLocationBarModelDelegate::IsNewTabPageURL return the expected results
 // for various NTP scenarios.
-TEST_F(ChromeLocationBarModelDelegateTest, IsNewTabPage) {
+IN_PROC_BROWSER_TEST_F(ChromeLocationBarModelDelegateTest, IsNewTabPage) {
   chrome::NewTab(browser());
   // New Tab URL with Google DSP resolves to the local or the WebUI NTP URL.
   GURL ntp_url(chrome::kChromeUINewTabPageURL);
-  EXPECT_EQ(ntp_url, search::GetNewTabPageURL(profile()));
+  EXPECT_EQ(ntp_url, search::GetNewTabPageURL(browser()->profile()));
 
   EXPECT_TRUE(delegate()->IsNewTabPage());
   EXPECT_TRUE(delegate()->IsNewTabPageURL(GetURL()));
@@ -127,7 +121,7 @@ TEST_F(ChromeLocationBarModelDelegateTest, IsNewTabPage) {
   // New Tab URL with a user selected DSP without an NTP URL resolves to
   // chrome://new-tab-page-third-party/.
   EXPECT_EQ(GURL(chrome::kChromeUINewTabPageThirdPartyURL),
-            search::GetNewTabPageURL(profile()));
+            search::GetNewTabPageURL(browser()->profile()));
 
   EXPECT_FALSE(delegate()->IsNewTabPage());
   EXPECT_TRUE(delegate()->IsNewTabPageURL(GetURL()));
@@ -135,13 +129,15 @@ TEST_F(ChromeLocationBarModelDelegateTest, IsNewTabPage) {
   SetSearchProvider(true);
   chrome::NewTab(browser());
   // New Tab URL with a user selected DSP resolves to the DSP's NTP URL.
-  EXPECT_EQ("https://foo.com/newtab", search::GetNewTabPageURL(profile()));
+  EXPECT_EQ("https://foo.com/newtab",
+            search::GetNewTabPageURL(browser()->profile()));
 
   EXPECT_FALSE(delegate()->IsNewTabPage());
   EXPECT_TRUE(delegate()->IsNewTabPageURL(GetURL()));
 }
 
-TEST_F(ChromeLocationBarModelDelegateTest, CertErrorPreventsElision) {
+IN_PROC_BROWSER_TEST_F(ChromeLocationBarModelDelegateTest,
+                       CertErrorPreventsElision) {
   EXPECT_FALSE(delegate()->ShouldPreventElision());
   delegate()->set_cert_status(net::CERT_STATUS_REVOKED);
   EXPECT_TRUE(delegate()->ShouldPreventElision());
