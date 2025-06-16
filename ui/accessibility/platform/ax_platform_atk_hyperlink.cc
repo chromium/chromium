2f19f284d5d1ea8fa627535d9476ce5886ccc6d2
Greg Thompson <grt@chromium.org>
2025-06-16 11:20:41 -0700

    [M138] [Accessibility] Fix nullptr-deref in AXPlatformAtkHyperlink
    Original change's description:
    > [Accessibility] Fix nullptr-deref in AXPlatformAtkHyperlink
    > 
    > Once an accessibility tool has a ref-counted AtkHyperlink (created by
    > and referencing an AXPlatformNodeAuraLinux), it may attempt to call
    > through it after the originating AXPlatformNodeAuraLinux has been
    > destroyed. While AXPlatformNodeAuraLinux clears the pointer back to
    > itself from the AtkHyperlink on destruction to avoid a classic
    > use-after-free, each method on the AXPlatformAtkHyperlink must check for
    > this pointer being null and return appropriately. The implementations
    > for get_start_index and get_end_index were both missing this null check.
    > 
    > Fixed: 424390732
    > Change-Id: I764a56fa2edd7d6750434d0e154263e4dc895858
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6643264
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Joanmarie Diggs <jdiggs@igalia.com>
    > Auto-Submit: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1474030}
    
    Bug: 425089773,424390732
    Change-Id: I764a56fa2edd7d6750434d0e154263e4dc895858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6647307
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7204@{#1394}
    Cr-Branched-From: d5de512dc9dc8ddfe4e6d71b0637578bb6158683-refs/heads/main@{#1465706}

--- ui/accessibility/platform/ax_platform_atk_hyperlink.cc
+++ ui/accessibility/platform/ax_platform_atk_hyperlink.cc
@@ -86,17 +86,25 @@ static gboolean AXPlatformAtkHyperlinkIsSelectedLink(
 
 static int AXPlatformAtkHyperlinkGetStartIndex(AtkHyperlink* atk_hyperlink) {
   g_return_val_if_fail(IS_AX_PLATFORM_ATK_HYPERLINK(atk_hyperlink), 0);
-  AXPlatformAtkHyperlink* link = AX_PLATFORM_ATK_HYPERLINK(atk_hyperlink);
+  auto* platform_node =
+      ToAXPlatformNodeAuraLinux(AX_PLATFORM_ATK_HYPERLINK(atk_hyperlink));
+  if (!platform_node) {
+    return 0;
+  }
   std::optional<std::pair<int, int>> indices =
-      link->priv->platform_node->GetEmbeddedObjectIndices();
+      platform_node->GetEmbeddedObjectIndices();
   return indices.has_value() ? indices->first : 0;
 }
 
 static int AXPlatformAtkHyperlinkGetEndIndex(AtkHyperlink* atk_hyperlink) {
   g_return_val_if_fail(IS_AX_PLATFORM_ATK_HYPERLINK(atk_hyperlink), 0);
-  AXPlatformAtkHyperlink* link = AX_PLATFORM_ATK_HYPERLINK(atk_hyperlink);
+  auto* platform_node =
+      ToAXPlatformNodeAuraLinux(AX_PLATFORM_ATK_HYPERLINK(atk_hyperlink));
+  if (!platform_node) {
+    return 0;
+  }
   std::optional<std::pair<int, int>> indices =
-      link->priv->platform_node->GetEmbeddedObjectIndices();
+      platform_node->GetEmbeddedObjectIndices();
   return indices.has_value() ? indices->second : 0;
 }
 
