59aceb309d149e037e5c8f1c56296978a26a5df9
Peter Kvitek <kvitekp@chromium.org>
2025-06-16 10:44:54 -0700

    Reland "[headless] Add --start-fullscreen tests with --screen-info switch"
    The affected test was disabled on Macs pending investigation.
    
    This is a reland of commit 51bcbe62a35857a3633e7edb2d467cc44d3c1379
    
    Original change's description:
    > [headless] Add --start-fullscreen tests with --screen-info switch
    >
    > This CL adds tests for --start-fullscreen switch behavior in headless
    > mode. Apparently, there is an issue on Mac and Linux when running with
    > device pixel ratios > 1.
    >
    > Bug: 423274945, 423951863
    > Change-Id: I70a849ae492a9ac92d603bc0531118adecbceeca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634191
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1473310}
    
    Bug: 423274945, 423951863
    Change-Id: I91043d0567260888e2e5a885243d0902aa2a423f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6639024
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474432}

--- chrome/browser/headless/headless_mode_protocol_browsertest.cc
+++ chrome/browser/headless/headless_mode_protocol_browsertest.cc
@@ -409,4 +409,30 @@ HEADLESS_MODE_PROTOCOL_TEST_WITH_COMMAND_LINE_EXTRAS(
     " workAreaLeft=10 workAreaRight=90"
     " workAreaTop=20 workAreaBottom=80}")
 
+// TODO(crbug.com/424797525): Fails Mac 13.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_StartFullscreenSwitch DISABLED_StartFullscreenSwitch
+#else
+#define MAYBE_StartFullscreenSwitch StartFullscreenSwitch
+#endif
+
+HEADLESS_MODE_PROTOCOL_TEST_WITH_COMMAND_LINE_EXTRAS(
+    MAYBE_StartFullscreenSwitch,
+    "sanity/start-fullscreen-switch.js",
+    "--screen-info={1600x1200}"
+    "--start-fullscreen")
+
+// TODO(crbug.com/423951863): Fails on Linux and Mac.
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#define MAYBE_StartFullscreenSwitchScaled DISABLED_StartFullscreenSwitchScaled
+#else
+#define MAYBE_StartFullscreenSwitchScaled StartFullscreenSwitchScaled
+#endif
+
+HEADLESS_MODE_PROTOCOL_TEST_WITH_COMMAND_LINE_EXTRAS(
+    MAYBE_StartFullscreenSwitchScaled,
+    "sanity/start-fullscreen-switch-scaled.js",
+    "--screen-info={3000x2000 devicePixelRatio=2.0}"
+    "--start-fullscreen")
+
 }  // namespace headless
