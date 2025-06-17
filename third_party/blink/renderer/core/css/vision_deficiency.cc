80fbd4fdf09095096a23956654222c85f91000cc
Mathias Bynens <mathias@chromium.org>
2025-06-17 02:18:02 -0700

    Remove workaround in vision deficiency emulation
    The `color-interpolation-filters="linearRGB"` workaround became
    unnecessary once https://crbug.com/40347855 got fixed.
    
    Bug: chromium:40205471, chromium:40347855, chromium:40098695
    Change-Id: I73a8706b4d04bd677e0336e947107da49f7f0e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651506
    Auto-Submit: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1474832}

--- third_party/blink/renderer/core/css/vision_deficiency.cc
+++ third_party/blink/renderer/core/css/vision_deficiency.cc
@@ -15,11 +15,9 @@ namespace blink {
 namespace {
 
 AtomicString CreateFilterDataUrl(const char* piece) {
-  // TODO(mathias): Remove `color-interpolation-filters` attribute once
-  // crbug.com/335066 is fixed. See crbug.com/1270748.
   return AtomicString(StrCat(
       {"data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\"><filter "
-       "id=\"f\" color-interpolation-filters=\"linearRGB\">",
+       "id=\"f\">",
        piece, "</filter></svg>#f"}));
 }
 
