9df256f6ef3cb2789d6d6de20964129378a89118
Anton Bikineev <bikineev@chromium.org>
2025-06-18 11:10:55 -0700

    PA: Fix an include to be relative to PA source dir
    Otherwise, it breaks PA as a library.
    
    Bug: 392817524
    Change-Id: I6a2b88dd2a6a67476b6449a800fed40f42b17cd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651807
    Auto-Submit: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Anton Bikineev <bikineev@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1475720}

--- base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine_support.cc
+++ base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine_support.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/allocator/partition_allocator/src/partition_alloc/scheduler_loop_quarantine_support.h"
+#include "partition_alloc/scheduler_loop_quarantine_support.h"
 
 namespace partition_alloc {
 ScopedSchedulerLoopQuarantineExclusion::
