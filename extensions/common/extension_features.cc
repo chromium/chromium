ef3f5840843c8501001d2d6730f58024b3012869
Richard Chen <richche@chromium.org>
2025-06-18 14:44:31 -0700

    Remove Risky Extension Switches in Chrome by default
    Enables kDisableDisableExtensionsExceptCommandLineSwitch and
    kDisableExtensionsOnChromeUrlsSwitch features by default on
    chrome-branded release builds except for ChromeOS.
    
    Bug: 419530940
    Change-Id: I5d3c132bc15f20669071625ca946bf84c7e95d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6618267
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1475865}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -200,10 +200,8 @@ BASE_FEATURE(kRemoveCoreSiteInstance,
 
 BASE_FEATURE(kDisableDisableExtensionsExceptCommandLineSwitch,
              "DisableDisableExtensionsExceptCommandLineSwitch",
-// TODO(crbug.com/419530940): Enable feature for Google Chrome Branding once
-// ready.
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-             base::FEATURE_DISABLED_BY_DEFAULT
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && !BUILDFLAG(IS_CHROMEOS)
+             base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -223,10 +221,8 @@ BASE_FEATURE(kDisableLoadExtensionCommandLineSwitch,
 
 BASE_FEATURE(kDisableExtensionsOnChromeUrlsSwitch,
              "DisableExtensionsOnChromeUrlsSwitch",
-// TODO(crbug.com/419530940): Enable feature for Google Chrome Branding only
-// once ready.
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING) && !BUILDFLAG(IS_CHROMEOS)
-             base::FEATURE_DISABLED_BY_DEFAULT
+             base::FEATURE_ENABLED_BY_DEFAULT
 #else
              base::FEATURE_DISABLED_BY_DEFAULT
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING) && !BUILDFLAG(IS_CHROMEOS)
