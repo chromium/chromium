2f34e94937f7f3091843b911380173da54250c4a
Colin Blundell <blundell@chromium.org>
2025-06-18 23:49:46 -0700

    [//cc] Add unittest of TileDisplayLayerImpl tile quad production
    Bug: 424560417
    Change-Id: I689b3627f1a461a05e8e956a0b6f12524e84ceee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6650993
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476019}

--- cc/layers/tile_display_layer_impl_unittest.cc
+++ cc/layers/tile_display_layer_impl_unittest.cc
@@ -8,6 +8,7 @@
 #include "cc/layers/append_quads_context.h"
 #include "cc/layers/append_quads_data.h"
 #include "cc/test/test_layer_tree_host_base.h"
+#include "components/viz/client/client_resource_provider.h"
 #include "components/viz/common/quads/solid_color_draw_quad.h"
 
 namespace cc {
@@ -65,6 +66,54 @@ TEST_F(TileDisplayLayerImplTest, SettingSolidColorResultsInSolidColorQuad) {
       viz::SolidColorDrawQuad::MaterialCast(render_pass->quad_list.front())
           ->color,
       kLayerColor);
+}
+
+TEST_F(TileDisplayLayerImplTest, NonEmptyTilingResultsInPictureQuad) {
+  constexpr gfx::Size kLayerBounds(1300, 1900);
+  constexpr gfx::Rect kLayerRect(kLayerBounds);
+  constexpr float kOpacity = 1.0;
+
+  auto layer = std::make_unique<TileDisplayLayerImpl>(
+      CHECK_DEREF(host_impl()->active_tree()), /*id=*/42);
+  auto* raw_layer = layer.get();
+  host_impl()->active_tree()->AddLayer(std::move(layer));
+
+  // For the production code to actually append a quad, the layer must have
+  // non-zero size and not be completely transparent.
+  raw_layer->SetBounds(kLayerBounds);
+  raw_layer->draw_properties().visible_layer_rect = kLayerRect;
+  raw_layer->draw_properties().opacity = kOpacity;
+
+  auto& tiling = raw_layer->GetOrCreateTilingFromScaleKey(1.0);
+  tiling.SetTileSize(kLayerBounds);
+  tiling.SetTilingRect(kLayerRect);
+
+  auto resource_id = host_impl()->resource_provider()->ImportResource(
+      viz::TransferableResource::Make(
+          gpu::ClientSharedImage::CreateForTesting(),
+          viz::TransferableResource::ResourceSource::kTest, gpu::SyncToken()),
+      base::DoNothing());
+  TileDisplayLayerImpl::TileContents contents =
+      TileDisplayLayerImpl::TileResource(resource_id, kLayerBounds,
+                                         /*is_checkered=*/false);
+  tiling.SetTileContents(TileIndex{0, 0}, contents, /*update_damage=*/true);
+
+  SetupRootProperties(host_impl()->active_tree()->root_layer());
+
+  auto render_pass = viz::CompositorRenderPass::Create();
+  AppendQuadsData data;
+  raw_layer->AppendQuads(
+      AppendQuadsContext{DRAW_MODE_RESOURCELESS_SOFTWARE, {}, false},
+      render_pass.get(), &data);
+
+  EXPECT_EQ(render_pass->quad_list.size(), 1u);
+  EXPECT_EQ(render_pass->quad_list.front()->rect, kLayerRect);
+  EXPECT_EQ(render_pass->quad_list.front()->visible_rect, kLayerRect);
+  EXPECT_EQ(render_pass->quad_list.front()->shared_quad_state->opacity,
+            kOpacity);
+  EXPECT_EQ(render_pass->quad_list.front()->resource_id, resource_id);
+  EXPECT_EQ(render_pass->quad_list.front()->material,
+            viz::DrawQuad::Material::kTiledContent);
 }
 
 }  // namespace cc
