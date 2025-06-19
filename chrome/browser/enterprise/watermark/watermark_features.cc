dea069c12c671237bf92e6328129fc49cb1ab3cf
Adam Kassi Lahlou <adamkl@google.com>
2025-06-19 10:10:11 -0700

    [Watermark 2.0] Add kEnableWatermarkCustomization feature flag
    This change introduces the base::Feature flag
    kEnableWatermarkCustomization which will be used to control
    the availability of watermark style customization options.
    
    Currently, this flag is not connected to any logic; that will be
    done in a follow-up CL. This is part of the effort to allow rolling
    out the watermark customization (Watermark 2.0) features gradually.
    
    Bug: 425991737
    Change-Id: I7202634883fc476a2f58dc8a809597655d8fb258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652639
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Adam Kassi Lahlou <adamkl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1476208}

---
+++ chrome/browser/enterprise/watermark/watermark_features.cc
@@ -0,0 +1,13 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/enterprise/watermark/watermark_features.h"
+
+namespace enterprise_watermark {
+
+BASE_FEATURE(kEnableWatermarkCustomization,
+             "EnableWatermarkCustomization",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace enterprise_watermark
