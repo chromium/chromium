6aae0e2353c857d98980ff677bf304288d7c58de
Hans Wennborg <hans@chromium.org>
2025-06-19 03:57:48 -0700

    [rust] Define __rust_no_alloc_shim_is_unstable_v2
    https://github.com/rust-lang/rust/pull/141061 renames and changes the
    type of __rust_no_alloc_shim_is_unstable.
    
    Bug: 422538133
    Change-Id: I23e6f8fcafafd310e5df7169a32887063d92cd40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624733
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476082}

--- build/rust/allocator/lib.rs
+++ build/rust/allocator/lib.rs
@@ -86,6 +86,12 @@ mod both_allocators {
     /// As part of rustc's contract for using `#[global_allocator]` without
     /// rustc-generated shims we must define this symbol, since we are opting in
     /// to unstable functionality. See https://github.com/rust-lang/rust/issues/123015
+    #[rustc_std_internal_symbol]
+    #[linkage = "weak"]
+    fn __rust_no_alloc_shim_is_unstable_v2() {}
+
+    // TODO(crbug.com/422538133) Remove after rolling past
+    // https://github.com/rust-lang/rust/pull/141061
     #[no_mangle]
     #[linkage = "weak"]
     static __rust_no_alloc_shim_is_unstable: u8 = 0;
