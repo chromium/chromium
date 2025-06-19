1cdd84c07d7e9c3f8498e57efc35b2ac626c0e20
zhoupeng <zhoupeng.1996@bytedance.com>
2025-06-19 05:01:21 -0700

    Crash: Ship the crash-reporting endpoint
    See https://groups.google.com/a/chromium.org/g/blink-dev/c/4mhi9k87ar4.
    
    Bug: 414723480
    Change-Id: Ia99e16efbe8a5ac18f9ee6dcf00e8b9bb30bf4a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654579
    Auto-Submit: Peng Zhou <zhoupeng.1996@bytedance.com>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476097}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -61,7 +61,7 @@ BASE_FEATURE(kCrashReportingAPIMoreContextData,
 
 BASE_FEATURE(kOverrideCrashReportingEndpoint,
              "OverrideCrashReportingEndpoint",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLowerHighResolutionTimerThreshold,
              "LowerHighResolutionTimerThreshold",
