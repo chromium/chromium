659dc7cafe2213f310feb97ae4a5d40f9d97067b
Alex Ilin <alexilin@chromium.org>
2025-06-20 03:18:38 -0700

    [gardener] Disable flaky BookmarkPersonalizedSigninPromoTest
    The following tests are flaky on large factor Android bots:
    - BookmarkPersonalizedSigninPromoTest#shouldShowBookmarksSigninPromoIfBookmarkNotSyncing
    - BookmarkPersonalizedSigninPromoTest#shouldShowBookmarksSigninPromoIfBookmarksIsNotManagedByPolicy
    - BookmarkPersonalizedSigninPromoTest#shouldShowBookmarksSigninPromoIfReadingListNotSyncing
    
    Bug: 419932169
    Change-Id: Ib37b59b5d76e4614f9e0d154dfce46b7b98a9da9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6657160
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476411}

--- chrome/browser/bookmarks/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
+++ chrome/browser/bookmarks/android/javatests/src/org/chromium/chrome/browser/bookmarks/BookmarkPersonalizedSigninPromoTest.java
@@ -109,6 +109,7 @@ public class BookmarkPersonalizedSigninPromoTest {
     @Test
     @MediumTest
     @DisableIf.Build(sdk_equals = Build.VERSION_CODES.S_V2, message = "crbug.com/362215887")
+    @DisableIf.Device(DeviceFormFactor.TABLET_OR_DESKTOP) // crbug.com/419932169
     public void shouldShowBookmarksSigninPromoIfBookmarksIsNotManagedByPolicy() {
         SyncServiceFactory.setInstanceForTesting(mSyncService);
         when(mSyncService.isTypeManagedByPolicy(UserSelectableType.BOOKMARKS)).thenReturn(false);
@@ -131,6 +132,7 @@ public class BookmarkPersonalizedSigninPromoTest {
     @Test
     @MediumTest
     @DisableIf.Build(sdk_equals = Build.VERSION_CODES.S_V2, message = "crbug.com/362215887")
+    @DisableIf.Device(DeviceFormFactor.TABLET_OR_DESKTOP) // crbug.com/419932169
     public void shouldShowBookmarksSigninPromoIfBookmarkNotSyncing() {
         SyncServiceFactory.setInstanceForTesting(mSyncService);
         when(mSyncService.isTypeManagedByPolicy(UserSelectableType.BOOKMARKS)).thenReturn(false);
@@ -142,6 +144,7 @@ public class BookmarkPersonalizedSigninPromoTest {
     @Test
     @MediumTest
     @DisableIf.Build(sdk_equals = Build.VERSION_CODES.S_V2, message = "crbug.com/362215887")
+    @DisableIf.Device(DeviceFormFactor.TABLET_OR_DESKTOP) // crbug.com/419932169
     public void shouldShowBookmarksSigninPromoIfReadingListNotSyncing() {
         SyncServiceFactory.setInstanceForTesting(mSyncService);
         when(mSyncService.isTypeManagedByPolicy(UserSelectableType.BOOKMARKS)).thenReturn(false);
