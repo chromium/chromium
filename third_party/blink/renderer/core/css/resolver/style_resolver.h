3c19e22236ba8fbe70d4a66ce53b304e3b9a6167
David Sanders <dsanders11@ucsbalum.com>
2025-06-20 14:27:45 -0700

    Forward declare StyleResolverState in style_resolver.h
    Bug: 40318405
    Change-Id: Ia4e251bf6fce2052c6c7c08290995ac200850b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654415
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476746}

--- third_party/blink/renderer/core/css/resolver/style_resolver.h
+++ third_party/blink/renderer/core/css/resolver/style_resolver.h
@@ -29,10 +29,10 @@
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/color_scheme_flags.h"
 #include "third_party/blink/renderer/core/css/css_position_try_rule.h"
+#include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
 #include "third_party/blink/renderer/core/css/element_rule_collector.h"
 #include "third_party/blink/renderer/core/css/resolver/matched_properties_cache.h"
 #include "third_party/blink/renderer/core/css/resolver/style_builder.h"
-#include "third_party/blink/renderer/core/css/resolver/style_resolver_state.h"
 #include "third_party/blink/renderer/core/css/selector_checker.h"
 #include "third_party/blink/renderer/core/css/selector_filter.h"
 #include "third_party/blink/renderer/core/css/style_request.h"
@@ -56,6 +56,7 @@ class PageMarginsStyle;
 class PropertyHandle;
 class StyleCascade;
 class StyleRecalcContext;
+class StyleResolverState;
 class StyleRuleUsageTracker;
 
 // This class selects a ComputedStyle for a given element in a document based on
