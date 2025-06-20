991f212f198766325bc1a08ac0395a9f3b3b7d01
Colin Blundell <blundell@chromium.org>
2025-06-19 22:53:27 -0700

    [//cc] Add test of TileDisplayLayerImpl solid-color tile production
    Bug: 424560417
    Change-Id: Ib95c6ef6e4af77c0cb4b93f3d4c33f5cd2fb0a57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651545
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476367}

--- cc/layers/tile_display_layer_impl_unittest.cc
+++ cc/layers/tile_display_layer_impl_unittest.cc
@@ -67,7 +67,8 @@ TEST_F(TileDisplayLayerImplTest, SettingSolidColorResultsInSolidColorQuad) {
       kLayerColor);
 }
 
-TEST_F(TileDisplayLayerImplTest, NonEmptyTilingResultsInPictureQuad) {
+TEST_F(TileDisplayLayerImplTest,
+       NonEmptyTilingWithResourceResultsInPictureQuad) {
   constexpr gfx::Size kLayerBounds(1300, 1900);
   constexpr gfx::Rect kLayerRect(kLayerBounds);
   constexpr float kOpacity = 1.0;
@@ -115,6 +116,54 @@ TEST_F(TileDisplayLayerImplTest, NonEmptyTilingResultsInPictureQuad) {
   EXPECT_EQ(viz::TileDrawQuad::MaterialCast(render_pass->quad_list.front())
                 ->force_anti_aliasing_off,
             false);
+}
+
+TEST_F(TileDisplayLayerImplTest,
+       NonEmptyTilingWithColorResultsInSolidColorQuad) {
+  constexpr gfx::Size kLayerBounds(1300, 1900);
+  constexpr gfx::Rect kLayerRect(kLayerBounds);
+  constexpr float kOpacity = 1.0;
+  constexpr SkColor4f kTileColor = SkColors::kRed;
+
+  auto layer = std::make_unique<TileDisplayLayerImpl>(
+      CHECK_DEREF(host_impl()->active_tree()), /*id=*/42);
+  auto* raw_layer = layer.get();
+  host_impl()->active_tree()->AddLayer(std::move(layer));
+
+  // For the production code to actually append a quad, the layer must have
+  // non-zero size and not be completely transparent.
+  raw_layer->SetBounds(kLayerBounds);
+  raw_layer->draw_properties().visible_layer_rect = kLayerRect;
+  raw_layer->draw_properties().opacity = kOpacity;
+
+  auto& tiling = raw_layer->GetOrCreateTilingFromScaleKey(1.0);
+  tiling.SetTileSize(kLayerBounds);
+  tiling.SetTilingRect(kLayerRect);
+
+  tiling.SetTileContents(TileIndex{0, 0}, kTileColor, /*update_damage=*/true);
+
+  SetupRootProperties(host_impl()->active_tree()->root_layer());
+
+  auto render_pass = viz::CompositorRenderPass::Create();
+  AppendQuadsData data;
+  raw_layer->AppendQuads(AppendQuadsContext{DRAW_MODE_SOFTWARE, {}, false},
+                         render_pass.get(), &data);
+
+  EXPECT_EQ(render_pass->quad_list.size(), 1u);
+  EXPECT_EQ(render_pass->quad_list.front()->rect, kLayerRect);
+  EXPECT_EQ(render_pass->quad_list.front()->visible_rect, kLayerRect);
+  EXPECT_EQ(render_pass->quad_list.front()->shared_quad_state->opacity,
+            kOpacity);
+  EXPECT_EQ(render_pass->quad_list.front()->material,
+            viz::DrawQuad::Material::kSolidColor);
+  EXPECT_EQ(
+      viz::SolidColorDrawQuad::MaterialCast(render_pass->quad_list.front())
+          ->color,
+      kTileColor);
+  EXPECT_EQ(
+      viz::SolidColorDrawQuad::MaterialCast(render_pass->quad_list.front())
+          ->force_anti_aliasing_off,
+      false);
 }
 
 class TileDisplayLayerImplWithEdgeAADisabledTest
