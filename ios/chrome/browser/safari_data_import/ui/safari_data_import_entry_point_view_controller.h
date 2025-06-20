dbf586d0b5fa2c2b99a16c4a38a8297d7b95cb92
ginnnnnnny <ginnyhuang@chromium.org>
2025-06-20 08:03:07 -0700

    [iOS][safari-import] Add strings and image resources for entry point
    ... and also added the view controller using these resources. A future
    CL will hook the view controller up with the entry point coordinator in
    crrev.com/c/6653580.
    
    Bug: 420703283
    Change-Id: I504bb7e0717cfd7203f23bab169b01512beee964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6634718
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1476518}

---
+++ ios/chrome/browser/safari_data_import/ui/safari_data_import_entry_point_view_controller.h
@@ -0,0 +1,16 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_ENTRY_POINT_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_ENTRY_POINT_VIEW_CONTROLLER_H_
+
+#import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
+
+/// Screen presented for `SafariDataImportScreen::kEntryPoint`.
+@interface SafariDataImportEntryPointViewController
+    : ConfirmationAlertViewController
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SAFARI_DATA_IMPORT_UI_SAFARI_DATA_IMPORT_ENTRY_POINT_VIEW_CONTROLLER_H_
---
+++ ios/chrome/browser/safari_data_import/ui/safari_data_import_entry_point_view_controller.mm
@@ -0,0 +1,33 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/safari_data_import/ui/safari_data_import_entry_point_view_controller.h"
+
+#import "ios/chrome/browser/safari_data_import/public/safari_data_import_screen_type.h"
+#import "ios/chrome/browser/safari_data_import/ui/safari_data_import_transition_delegate.h"
+#import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
+#import "ios/chrome/grit/ios_branded_strings.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util_mac.h"
+
+@implementation SafariDataImportEntryPointViewController
+
+#pragma mark - ConfirmationAlertViewController
+
+- (void)viewDidLoad {
+  self.titleString =
+      l10n_util::GetNSString(IDS_IOS_SAFARI_IMPORT_ENTRY_POINT_TITLE);
+  self.subtitleString =
+      l10n_util::GetNSString(IDS_IOS_SAFARI_IMPORT_ENTRY_POINT_SUBTITLE);
+  self.primaryActionString =
+      l10n_util::GetNSString(IDS_IOS_SAFARI_IMPORT_ENTRY_POINT_PRIMARY_ACTION);
+  self.secondaryActionString = l10n_util::GetNSString(
+      IDS_IOS_SAFARI_IMPORT_ENTRY_POINT_SECONDARY_ACTION);
+  self.image = [UIImage imageNamed:@"safari_data_import"];
+  self.imageHasFixedSize = YES;
+  self.dismissBarButtonSystemItem = UIBarButtonSystemItemClose;
+  [super viewDidLoad];
+}
+
+@end
