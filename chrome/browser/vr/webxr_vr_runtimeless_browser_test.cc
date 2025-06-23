4028fc9ac88c9394d67b48b2bd27f4e0b0d7d056
Alexander Cooper <alcooper@chromium.org>
2025-06-23 12:36:42 -0700

    Re-enable TestWebXrNoDevicesWithoutRuntime
    It seems as though the test was perhaps incorrectly disabled. The actual
    full failure rate appears to be only on one run, long after any of the
    timing changes were made. The other interactive_ui_test failures appear
    to be unrelated to xr_browser_tests at all. There are a few tests long
    after the threading and timing changes were made, that indicate that a
    WebXR test (and for this particular test case only one of three) passed
    on retry, but all of those have logs which seem to indicate an error
    with initializing the GPU process. Further, even with many repeats
    locally and copying the CQ gn args, no repro is observed.
    
    Fixed: 424430986
    Change-Id: I414a8242f2ed7ac9e44a684a5ec50651e4385bd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6653868
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477486}

--- chrome/browser/vr/webxr_vr_runtimeless_browser_test.cc
+++ chrome/browser/vr/webxr_vr_runtimeless_browser_test.cc
@@ -9,15 +9,8 @@ namespace vr {
 
 // Tests that WebXR does not return any devices if all runtime support is
 // disabled.
-#if BUILDFLAG(IS_LINUX)
-// TODO(crbug.com/424430986): Re-enable this once the hang is fixed on Linux
-#define MAYBE_TestWebXrNoDevicesWithoutRuntime \
-  DISABLED_TestWebXrNoDevicesWithoutRuntime
-#else
-#define MAYBE_TestWebXrNoDevicesWithoutRuntime TestWebXrNoDevicesWithoutRuntime
-#endif
 IN_PROC_BROWSER_TEST_F(WebXrVrRuntimelessBrowserTest,
-                       MAYBE_TestWebXrNoDevicesWithoutRuntime) {
+                       TestWebXrNoDevicesWithoutRuntime) {
   LoadFileAndAwaitInitialization("test_webxr_does_not_return_device");
   WaitOnJavaScriptStep();
   EndTest();
