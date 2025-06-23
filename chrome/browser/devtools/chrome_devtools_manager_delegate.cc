cc0ee00ffc6012f4273304d296637b5f12790933
Xiyuan Xia <xiyuan@chromium.org>
2025-06-23 09:39:16 -0700

    devtoos: Give offscreen doc a type other than `page`
    Telemetry assumes `page` type targets are tab contents and would
    mistakenly include offscreen doc as part of its tab list. This
    breaks tests when component extensions such as eSpeak-NG MV3 creates
    its offscreen doc when chrome starts. This CL gives offscreen doc a
    type other than `page` to fix the telemetry test failures.
    
    Bug: 426010565
    Change-Id: I73138ea6a932209833ee58e1183fcddb3f8bbd80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6654815
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1477346}

--- chrome/browser/devtools/chrome_devtools_manager_delegate.cc
+++ chrome/browser/devtools/chrome_devtools_manager_delegate.cc
@@ -133,12 +133,23 @@ bool GetExtensionInfo(content::WebContents* wc,
 
   auto view_type = extensions::GetViewType(wc);
   if (view_type == extensions::mojom::ViewType::kExtensionPopup ||
-      view_type == extensions::mojom::ViewType::kExtensionSidePanel ||
-      view_type == extensions::mojom::ViewType::kOffscreenDocument) {
+      view_type == extensions::mojom::ViewType::kExtensionSidePanel) {
     // Note that we are intentionally not setting name here, so that we can
     // construct a name based on the URL or page title in
     // RenderFrameDevToolsAgentHost::GetTitle()
     *type = ChromeDevToolsManagerDelegate::kTypePage;
+    return true;
+  }
+
+  if (view_type == extensions::mojom::ViewType::kOffscreenDocument) {
+    // Note that we are intentionally not setting name here, so that we can
+    // construct a name based on the URL or page title in
+    // RenderFrameDevToolsAgentHost::GetTitle()
+    //
+    // Use `kTypeBackgroundPage` for offscreen doc until devtools frontend is
+    // updated to support a new `offscreen_document` target type. Otherwise,
+    // DOM is not inspectable.
+    *type = ChromeDevToolsManagerDelegate::kTypeBackgroundPage;
     return true;
   }
 
