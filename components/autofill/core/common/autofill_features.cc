9774ddbd44131ed41cd7c310e1a9c548cc416c37
Christoph Schwering <schwering@google.com>
2025-06-23 08:58:27 -0700

    [Autofill] Enable kAutofillAiNoTagTypes by default
    kAutofillAiNoTagTypes is a kill switch.
    
    Bug: 422563282
    Change-Id: Ic6fc367a7fd1b584f4d68ffbaa1c595bcff44379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6620869
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1477325}

--- components/autofill/core/common/autofill_features.cc
+++ components/autofill/core/common/autofill_features.cc
@@ -79,6 +79,7 @@ BASE_FEATURE(kAutofillAiIgnoreGeoIp,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // If enabled, `*_TAG` types are replaced with dynamic attribute assignments.
+// This is a kill switch.
 //
 // For example, if the feature is disabled, a passport name field requires a
 // `PASSPORT_NAME_TAG` prediction along with a classical `NAME_*` type.
@@ -86,10 +87,10 @@ BASE_FEATURE(kAutofillAiIgnoreGeoIp,
 // If the feature is enabled, `PASSPORT_NAME_TAG` is ignored and the assignment
 // to the passport entity is derived from the surrounding fields.
 //
-// TODO(crbug.com/422563282): Clean up when launched.
+// TODO(crbug.com/422563282): Remove after the M140 branch point (2025-08-04).
 BASE_FEATURE(kAutofillAiNoTagTypes,
              "AutofillAiNoTagTypes",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If enabled, the client may trigger the server model for AutofillAI type
 // predictions.
