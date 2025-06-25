d5ff6c055c388b0c7661cc12009002b33df192e3
Andrew Grieve <agrieve@chromium.org>
2025-06-25 07:19:51 -0700

    Cronet: Use a .build_config file for proguard config golden tests
    This will make it easier to refactor build logic by keeping proguard
    config aggregation in one spot (write_build_config.py).
    
    Also fixed some long-line presubmit warnings.
    
    Bug: 413331591
    Change-Id: If434387624fcb9f6fd557a0b11b033a600ce2591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667702
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478536}

--- components/cronet/tools/generate_proguard_file.py
+++ components/cronet/tools/generate_proguard_file.py
@@ -28,33 +28,11 @@ def _ReadFile(path):
   return pathlib.Path(path).read_text()
 
 
-def ReadProguardConfigsPath(build_config_path):
-  """
-  Reads the transitive proguard configs path of target whose build
-  config path is `build_config_path`
-
-  @param path: The path to the initial build_config
-  @returns A set of the proguard config paths found during traversal.
-  """
-  proguard_configs_path = set()
-  with open(build_config_path, 'r') as f:
-    build_config = json.load(f)
-    proguard_configs_path.update(build_config.get("proguard_all_configs",
-                                                  set()))
-
-  params_path = build_config_path.replace('.build_config.json', '.params.json')
-  with open(params_path, 'r') as f:
-    params_json = json.load(f)
-    # java_library targets don't have `proguard_all_configs` so we need
-    # to look at `proguard_configs` field instead.
-    proguard_configs_path.update(params_json.get("proguard_configs", set()))
-  return proguard_configs_path
-
-
 def _post_process_concatenated_rules_for_aosp(rules: str) -> str:
   """Post-process the concatenated rules to match the packages in AOSP. Cronet
-  in AOSP is repackaged where all packages are prefixed with `android.net.connectivity`.
-  So does method will append ** to every package name found.
+  in AOSP is repackaged where all packages are prefixed with
+  `android.net.connectivity`. So does method will append ** to every package
+  name found.
 
   Args:
     rules: Rules before processing
@@ -75,21 +53,28 @@ def main():
           help='Output file for the generated proguard file')
   parser.add_argument(
       '--aosp-mode',
-      help=
-      'This will apply additional processing for the combined rules to ensure they match HttpEngine (Cronet In AOSP).',
+      help='This will apply additional processing for the combined rules to '
+      'ensure they match HttpEngine (Cronet In AOSP).',
       action='store_true')
   parser.add_argument('--dep_file',
                       help='Depfile path to write the implicit inputs')
+  parser.add_argument(
+      'build_config',
+      help='Path to the generated build_config that contains the transitive '
+      'dependencies of the proguard rules')
 
-  args, input_files = parser.parse_known_args()
+  args = parser.parse_args()
+
   # Fetch all proguard configs
-  all_proguard_configs_path = set()
-  for input_file in input_files:
-    all_proguard_configs_path.update(ReadProguardConfigsPath(input_file))
+  with open(args.build_config, 'r') as f:
+    build_config = json.load(f)
+    all_proguard_configs_path = set(build_config['proguard_all_configs'])
+
   str_output = ""
   # Concatenate all proguard rules and sort to maintain deterministic output.
   for proguard_config_path in sorted(all_proguard_configs_path):
-    str_output += f"# -------- Config Path: {proguard_config_path.replace('../', '')} --------\n"
+    noramlized_path = proguard_config_path.replace('../', '')
+    str_output += f"# -------- Config Path: {noramlized_path} --------\n"
     str_output += _ReadFile(proguard_config_path)
   if args.aosp_mode:
     str_output = _post_process_concatenated_rules_for_aosp(str_output)
