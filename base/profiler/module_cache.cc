e6d8c998acd2e1b7191dcd2442f9ec1b5ba72eab
Thiabaud Engelbrecht <thiabaud@google.com>
2025-06-24 20:16:08 -0700

    [M132-LTS][ssm] Fix race condition
    This CL fixes a threading issue, by using locks to prevent racy access.
    
    (cherry picked from commit e9e247c09b76dc6813c70d2c7007438c830bea56)
    
    Bug: 421471016
    Change-Id: If41796b9eb49aa3f27df175c6be198f47947c4c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6620528
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1470083}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6651573
    Reviewed-by: Mohamed Omar <mohamedaomar@google.com>
    Reviewed-by: Sean Maher <spvm@chromium.org>
    Owners-Override: Mohamed Omar <mohamedaomar@google.com>
    Commit-Queue: Gyuyoung Kim (xWF) <qkim@google.com>
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/branch-heads/6834@{#5588}
    Cr-Branched-From: 47a3549fac11ee8cb7be6606001ede605b302b9f-refs/heads/main@{#1381561}

--- base/profiler/module_cache.cc
+++ base/profiler/module_cache.cc
@@ -85,6 +85,7 @@ std::vector<const ModuleCache::Module*> ModuleCache::GetModules() const {
   result.reserve(native_modules_.size());
   for (const std::unique_ptr<const Module>& module : native_modules_)
     result.push_back(module.get());
+  base::AutoLock locker(lock_);
   for (const std::unique_ptr<const Module>& module : non_native_modules_)
     result.push_back(module.get());
   return result;
@@ -97,6 +98,7 @@ void ModuleCache::UpdateNonNativeModules(
   flat_set<const Module*> defunct_modules_set(defunct_modules.begin(),
                                               defunct_modules.end());
 
+  base::AutoLock locker(lock_);
   // Reorder the modules to be removed to the last slots in the set, then move
   // them to the inactive modules, then erase the moved-from modules from the
   // set. This is a variation on the standard erase-remove idiom, which is
@@ -149,9 +151,12 @@ void ModuleCache::AddCustomNativeModule(std::unique_ptr<const Module> module) {
 
 const ModuleCache::Module* ModuleCache::GetExistingModuleForAddress(
     uintptr_t address) const {
-  const auto non_native_module_loc = non_native_modules_.find(address);
-  if (non_native_module_loc != non_native_modules_.end())
-    return non_native_module_loc->get();
+  {
+    base::AutoLock locker(lock_);
+    const auto non_native_module_loc = non_native_modules_.find(address);
+    if (non_native_module_loc != non_native_modules_.end())
+      return non_native_module_loc->get();
+  }
 
   const auto native_module_loc = native_modules_.find(address);
   if (native_module_loc != native_modules_.end())
