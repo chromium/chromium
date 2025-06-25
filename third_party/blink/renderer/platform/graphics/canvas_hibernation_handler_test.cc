638aa24e6f6f2ffbdddb68b61d5ce7ff18029651
Colin Blundell <blundell@chromium.org>
2025-06-25 00:37:46 -0700

    [Blink] Inline FakeCanvasResourceHost in hibernation handler test
    It is needed only for canvas_hibernation_handler_test.cc, and we don't
    want to encourage any further usage of it.
    
    Bug: 352263194
    Change-Id: Ib4c111397acccd9234f92d2762768771bbfe9f71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667082
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478391}

--- third_party/blink/renderer/platform/graphics/canvas_hibernation_handler_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_hibernation_handler_test.cc
@@ -11,10 +11,14 @@
 #include "base/test/scoped_feature_list.h"
 #include "base/trace_event/process_memory_dump.h"
 #include "components/viz/test/test_context_provider.h"
+#include "gpu/command_buffer/common/shared_image_usage.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
+#include "third_party/blink/renderer/platform/graphics/canvas_resource_host.h"
+#include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
+#include "third_party/blink/renderer/platform/graphics/gpu/shared_gpu_context.h"
+#include "third_party/blink/renderer/platform/graphics/skia/skia_utils.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
-#include "third_party/blink/renderer/platform/graphics/test/fake_canvas_resource_host.h"
 #include "third_party/blink/renderer/platform/graphics/test/gpu_memory_buffer_test_platform.h"
 #include "third_party/blink/renderer/platform/graphics/test/gpu_test_utils.h"
 #include "third_party/blink/renderer/platform/scheduler/public/main_thread_scheduler.h"
@@ -24,6 +28,61 @@
 namespace blink {
 
 using testing::Test;
+
+namespace {
+
+class FakeCanvasResourceHost : public CanvasResourceHost {
+ public:
+  explicit FakeCanvasResourceHost(gfx::Size size) : CanvasResourceHost(size) {}
+  ~FakeCanvasResourceHost() override = default;
+  void NotifyGpuContextLost() override {}
+  bool IsContextLost() const override { return false; }
+  void SetNeedsCompositingUpdate() override {}
+  void InitializeForRecording(cc::PaintCanvas*) const override {}
+  bool PrintedInCurrentTask() const override { return false; }
+  bool IsPageVisible() const override { return page_visible_; }
+  bool IsHibernating() const override { return is_hibernating_; }
+  void SetIsHibernating(bool is_hibernating) {
+    is_hibernating_ = is_hibernating;
+  }
+
+  CanvasResourceProvider* GetResourceProviderForCanvas2D() const override {
+    return resource_provider_.get();
+  }
+  void ResetResourceProviderForCanvas2D() override {
+    resource_provider_.reset();
+  }
+
+  CanvasResourceProvider* GetOrCreateCanvasResourceProviderForCanvas2D() {
+    if (GetResourceProviderForCanvas2D()) {
+      return GetResourceProviderForCanvas2D();
+    }
+    constexpr auto kShouldInitialize =
+        CanvasResourceProvider::ShouldInitialize::kCallClear;
+    constexpr gpu::SharedImageUsageSet kSharedImageUsageFlags =
+        gpu::SHARED_IMAGE_USAGE_DISPLAY_READ | gpu::SHARED_IMAGE_USAGE_SCANOUT;
+    resource_provider_ = CanvasResourceProvider::CreateSharedImageProvider(
+        Size(), GetN32FormatForCanvas(), kPremul_SkAlphaType,
+        gfx::ColorSpace::CreateSRGB(), kShouldInitialize,
+        SharedGpuContext::ContextProviderWrapper(), RasterMode::kGPU,
+        kSharedImageUsageFlags, this);
+
+    return resource_provider_.get();
+  }
+
+  void SetPageVisible(bool visible) {
+    if (page_visible_ != visible) {
+      page_visible_ = visible;
+    }
+  }
+
+ private:
+  std::unique_ptr<CanvasResourceProvider> resource_provider_;
+  bool page_visible_ = true;
+  bool is_hibernating_ = false;
+};
+
+}  // namespace
 
 class CanvasHibernationHandlerTest
     : public testing::TestWithParam<
