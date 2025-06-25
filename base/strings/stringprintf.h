023dddf8bbfb8283c95bc59c36254dec5e5b1a05
Takuto Ikuta <tikuta@chromium.org>
2025-06-25 10:22:10 -0700

    base: Remove unused includes from stringprintf.h
    Bug: 40318405
    Change-Id: Idcef527b2002f930dbd8bf29fc4d3d54c30962a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6671888
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478637}

--- base/strings/stringprintf.h
+++ base/strings/stringprintf.h
@@ -8,10 +8,8 @@
 #include <stdarg.h>  // va_list
 
 #include <string>
-#include <string_view>
 
 #include "base/base_export.h"
-#include "base/check.h"
 #include "base/compiler_specific.h"
 #include "third_party/abseil-cpp/absl/strings/str_format.h"
 
