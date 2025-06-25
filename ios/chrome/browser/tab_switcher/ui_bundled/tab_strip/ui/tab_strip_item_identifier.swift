4fa7901d0671898b284b71adae3b434ee5879fbd
Ewann Pelle <ewannpv@chromium.org>
2025-06-25 06:36:27 -0700

    [iOS][26] Safely converts item hash to Int
    Converting `NSUInteger` item hashes to Swift `Int` can cause crashes due
    to large hash values exceeding `Int`'s capacity. This change uses
    `Int(truncatingIfNeeded:)` to safely convert these hashes, truncating
    values if they're too large, thus preventing crashes.
    
    Bug: 427304663
    Change-Id: I08f7b4625a62f583831ffdfaa8842280fb92a90d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667886
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478517}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_strip/ui/tab_strip_item_identifier.swift
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_strip/ui/tab_strip_item_identifier.swift
@@ -46,7 +46,9 @@
 
   public init(_ tabGroupItem: TabGroupItem) {
     self.item = .group(tabGroupItem)
-    self.itemHash = Int(GetHashForTabGroupItem(tabGroupItem))
+    // Converting `NSUInteger` item hashes to Swift `Int` can
+    // cause crashes due to large hash values exceeding `Int`'s capacity.
+    self.itemHash = Int(truncatingIfNeeded: GetHashForTabGroupItem(tabGroupItem))
   }
 
   public convenience init?(_ tabSwitcherItem: TabSwitcherItem?) {
