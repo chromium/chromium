5742fa65738ca0e9eb4a300f72cd401b49274e66
Mikita Kuchyn <kuchyn@google.com>
2025-06-25 03:39:41 -0700

    Migrate FileSystemAccessRestorePermissionBubbleViewTest into InProcessBrowserTest
    Use of TestWithBrowserView is being deprecated and existing tests
    requiring a Browser environment are being migrated to being browser
    tests.
    
    This CL updates the FileSystemAccessRestorePermissionBubbleViewTest
    fixture to an InProcessBrowserTest and removes the TestWithBrowserView
    dependency.
    
    Bug: 417766643
    Change-Id: I5ec063dab8463f2af1a1133f941558cd36e5c24f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652844
    Auto-Submit: Mikita Kuchyn <kuchyn@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Mikita Kuchyn <kuchyn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1478448}

--- chrome/browser/ui/views/file_system_access/file_system_access_restore_permission_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/file_system_access/file_system_access_restore_permission_bubble_view_browsertest.cc
@@ -5,9 +5,15 @@
 #include "chrome/browser/ui/views/file_system_access/file_system_access_restore_permission_bubble_view.h"
 
 #include "base/test/bind.h"
-#include "chrome/browser/ui/views/frame/test_with_browser_view.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "components/permissions/permission_util.h"
 #include "content/public/browser/file_system_access_permission_context.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_test.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using AccessType = FileSystemAccessPermissionRequestManager::Access;
@@ -16,18 +22,10 @@ using RequestData = FileSystemAccessPermissionRequestManager::RequestData;
 using RequestType = FileSystemAccessPermissionRequestManager::RequestType;
 
 class FileSystemAccessRestorePermissionBubbleViewTest
-    : public TestWithBrowserView {
+    : public InProcessBrowserTest {
  public:
-  void SetUp() override {
-    TestWithBrowserView::SetUp();
-
-    AddTab(browser(), GURL("http://foo"));
-    web_content_ = browser()->tab_strip_model()->GetWebContentsAt(0);
-  }
-
-  void TearDown() override {
-    web_content_ = nullptr;
-    TestWithBrowserView::TearDown();
+  content::WebContents* GetWebContents() {
+    return browser()->tab_strip_model()->GetWebContentsAt(0);
   }
 
  protected:
@@ -36,76 +34,77 @@ class FileSystemAccessRestorePermissionBubbleViewTest
                   url::Origin::Create(GURL("https://example.com")),
                   {{content::PathInfo(FILE_PATH_LITERAL("/foo/bar.txt")),
                     HandleType::kFile, AccessType::kRead}});
-  raw_ptr<content::WebContents> web_content_;
 };
 
-TEST_F(FileSystemAccessRestorePermissionBubbleViewTest,
-       AllowOnceButtonPressed) {
+IN_PROC_BROWSER_TEST_F(FileSystemAccessRestorePermissionBubbleViewTest,
+                       AllowOnceButtonPressed) {
   permissions::PermissionAction callback_result;
   auto* bubble = GetFileSystemAccessRestorePermissionDialogForTesting(
       kRequestData,
       base::BindLambdaForTesting([&](permissions::PermissionAction result) {
         callback_result = result;
       }),
-      web_content_);
+      GetWebContents());
   bubble->OnButtonPressed(FileSystemAccessRestorePermissionBubbleView::
                               RestorePermissionButton::kAllowOnce);
 
   EXPECT_EQ(callback_result, permissions::PermissionAction::GRANTED_ONCE);
 }
 
-TEST_F(FileSystemAccessRestorePermissionBubbleViewTest,
-       AllowAlwaysButtonPressed) {
+IN_PROC_BROWSER_TEST_F(FileSystemAccessRestorePermissionBubbleViewTest,
+                       AllowAlwaysButtonPressed) {
   permissions::PermissionAction callback_result;
   auto* bubble = GetFileSystemAccessRestorePermissionDialogForTesting(
       kRequestData,
       base::BindLambdaForTesting([&](permissions::PermissionAction result) {
         callback_result = result;
       }),
-      web_content_);
+      GetWebContents());
   bubble->OnButtonPressed(FileSystemAccessRestorePermissionBubbleView::
                               RestorePermissionButton::kAllowAlways);
 
   EXPECT_EQ(callback_result, permissions::PermissionAction::GRANTED);
 }
 
-TEST_F(FileSystemAccessRestorePermissionBubbleViewTest, DenyButtonPressed) {
+IN_PROC_BROWSER_TEST_F(FileSystemAccessRestorePermissionBubbleViewTest,
+                       DenyButtonPressed) {
   permissions::PermissionAction callback_result;
   auto* bubble = GetFileSystemAccessRestorePermissionDialogForTesting(
       kRequestData,
       base::BindLambdaForTesting([&](permissions::PermissionAction result) {
         callback_result = result;
       }),
-      web_content_);
+      GetWebContents());
   bubble->OnButtonPressed(FileSystemAccessRestorePermissionBubbleView::
                               RestorePermissionButton::kDeny);
 
   EXPECT_EQ(callback_result, permissions::PermissionAction::DENIED);
 }
 
-TEST_F(FileSystemAccessRestorePermissionBubbleViewTest, CloseButtonPressed) {
+IN_PROC_BROWSER_TEST_F(FileSystemAccessRestorePermissionBubbleViewTest,
+                       CloseButtonPressed) {
   permissions::PermissionAction callback_result;
   auto* bubble = GetFileSystemAccessRestorePermissionDialogForTesting(
       kRequestData,
       base::BindLambdaForTesting([&](permissions::PermissionAction result) {
         callback_result = result;
       }),
-      web_content_);
+      GetWebContents());
   bubble->Close();
 
   EXPECT_EQ(callback_result, permissions::PermissionAction::DISMISSED);
 }
 
-TEST_F(FileSystemAccessRestorePermissionBubbleViewTest,
-       BubbleDismissedOnNavigation) {
+IN_PROC_BROWSER_TEST_F(FileSystemAccessRestorePermissionBubbleViewTest,
+                       BubbleDismissedOnNavigation) {
   permissions::PermissionAction callback_result;
   GetFileSystemAccessRestorePermissionDialogForTesting(
       kRequestData,
       base::BindLambdaForTesting([&](permissions::PermissionAction result) {
         callback_result = result;
       }),
-      web_content_);
-  NavigateAndCommit(web_content_, GURL("http://bar"));
+      GetWebContents());
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), GURL("http://bar")));
 
   EXPECT_EQ(callback_result, permissions::PermissionAction::DISMISSED);
 }
