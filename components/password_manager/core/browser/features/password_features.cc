c78300b9fcd828e7a1fadcc6693fd6e030e41425
Mohamed Amir Yosef <mamir@chromium.org>
2025-06-26 02:52:50 -0700

    Enable resync on undecryptable passwords feature on Windows
    On Windows there are issues with the system encryption keys that may lead to data loss. This feature enables recovering from this state automatically.
    
    Bug: 415979068
    Change-Id: I68f72f3b76cecf7310b8d468431104d50c61334f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6678754
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Auto-Submit: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479020}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -162,8 +162,13 @@ BASE_FEATURE(kTriggerPasswordResyncAfterDeletingUndecryptablePasswords,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTriggerPasswordResyncWhenUndecryptablePasswordsDetected,
-             "TriggerPasswordResyncWhenUndecryptablePasswordsDetected",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+            "TriggerPasswordResyncWhenUndecryptablePasswordsDetected",
+#if BUILDFLAG(IS_WIN)
+            base::FEATURE_ENABLED_BY_DEFAULT
+#else
+            base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kBiometricAuthIdentityCheck,
