684be8d5dcadde6ddd78ee7f06548a70be76b995
Di Zhang <dizhangg@chromium.org>
2025-06-26 16:48:55 -0700

    [gardener] Revert "Add unit tests for platform experience installation"
    This reverts commit e15f089b78ae6e5a2f3875ec9f4d8fcb9095a6f0.
    
    Reason for revert: Causing build failure for win-chrome
    https://ci.chromium.org/ui/p/chrome/builders/ci/win-chrome/37114/overview
    
    Bug: 422147293
    Original change's description:
    > Add unit tests for platform experience installation
    >
    > Updates `platform_experience::MaybeInstallPlatformExperienceHelper` to
    > accept an optional "delegate" interface for launching processes, which
    > allows us to use a mock for unit testing.
    >
    > Bug: 422147293
    > Change-Id: I4ce4ce1e91b107ab8d1cff0b11afcd644f874b24
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6667560
    > Commit-Queue: Kaan Alsan <alsan@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1479248}
    
    Bug: 422147293
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I2ed9022a4b4000215b33ca0cedd62a6f6e48c1b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6682218
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Owners-Override: Di Zhang <dizhangg@google.com>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479531}

--- chrome/browser/platform_experience/installer/installer_win.h
+++ chrome/browser/platform_experience/installer/installer_win.h
@@ -5,34 +5,7 @@
 #ifndef CHROME_BROWSER_PLATFORM_EXPERIENCE_INSTALLER_INSTALLER_WIN_H_
 #define CHROME_BROWSER_PLATFORM_EXPERIENCE_INSTALLER_INSTALLER_WIN_H_
 
-#include <wrl/client.h>
-
-#include "base/process/process.h"
-
-struct IAppCommandWeb;
-
-namespace base {
-class CommandLine;
-struct LaunchOptions;
-}  // namespace base
-
 namespace platform_experience {
-
-// Delegate for controlling how the Platform Experience Helper installer is
-// launched.
-class InstallerLauncherDelegate {
- public:
-  virtual ~InstallerLauncherDelegate() = default;
-
-  virtual Microsoft::WRL::ComPtr<IAppCommandWeb> GetUpdaterAppCommand(
-      const std::wstring& command_name) = 0;
-
-  virtual base::Process LaunchProcess(const base::CommandLine& cmd_line,
-                                      const base::LaunchOptions& options) = 0;
-};
-
-void SetInstallerLauncherDelegateForTesting(
-    InstallerLauncherDelegate* delegate);
 
 // Starts the installation of the PEH, if it hasn't already been installed yet.
 // This function might block.
