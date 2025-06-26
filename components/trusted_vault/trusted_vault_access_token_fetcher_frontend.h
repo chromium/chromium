d1818e43e8ca6abb07c627e5ee409377c8bb5851
Maksim Moskvitin <mmoskvitin@google.com>
2025-06-26 09:32:47 -0700

    [TrustedVault] Fix UAF in TrustedVaultAccessTokenFetcherFrontend
    Bug: 427316421
    Change-Id: I1d102fae921bead38f9de23f63d2647aed32131b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6677094
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479236}

--- components/trusted_vault/trusted_vault_access_token_fetcher_frontend.h
+++ components/trusted_vault/trusted_vault_access_token_fetcher_frontend.h
@@ -67,7 +67,7 @@ class TrustedVaultAccessTokenFetcherFrontend
                                    signin::AccessTokenInfo access_token_info);
 
   // Helper method to run and clear |pending_requests_|.
-  void FulfillPendingRequests(
+  void FulfillPendingRequestsAndMaybeDestroySelf(
       TrustedVaultAccessTokenFetcher::AccessTokenInfoOrError
           access_token_info_or_error);
 
