4268b723531c440286f675810724a0e0ba4b5850
Valerie Young <spectranaut@igalia.com>
2025-06-26 13:50:17 -0700

    [ChromeVox] Remove SRE reference and es6_loader.ts
    Bug: 388867840
    Change-Id: I803412581f38cb1b64847d92deb6c561baefb505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6657028
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479430}

--- chrome/browser/resources/chromeos/accessibility/chromevox/mv3/chromevox_loader.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/mv3/chromevox_loader.ts
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// ChromeVox ES6 modules. Non-ES6 modules cannot depend on these modules.
-import 'background/es6_loader.js';
+import {Background} from './background/background.js';
 
-// Third party - Speech Rule Engine.
-importScripts('../third_party/sre/sre_browser.js');
+Background.init();
