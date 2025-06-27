24c39397cf9b52d2e98d7b0e93b8961c94dc8868
Zijie He <zijiehe@google.com>
2025-06-27 15:03:25 -0700

    [Fuchsia] Handle the case when the gsutil.py wrapper is not present
    Since the tool is also used out of chromium environment,
    find_depot_tools.py and gsutil.py may not always be present.
    In the case, gsutil will be executed directly as a backup plan.
    
    Bug: 394836671
    Change-Id: I3c5985df1804ee39c491bd7f18f98e7ce76de7ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6676483
    Commit-Queue: Zijie He <zijiehe@google.com>
    Reviewed-by: Guocheng Wei <guochengwei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480113}

--- build/fuchsia/test/gs_util_wrapper.py
+++ build/fuchsia/test/gs_util_wrapper.py
@@ -8,13 +8,19 @@ import os
 import subprocess
 import sys
 
-from typing import List
+from typing import List, Optional
 
 from common import DIR_SRC_ROOT
 
 
-def _find_gsutil() -> str:
+def _find_gsutil() -> Optional[str]:
     """ Returns the location of the gsutil.py. """
+    if not os.path.isfile(
+            os.path.join(DIR_SRC_ROOT, 'build', 'find_depot_tools.py')):
+        # No gsutil.py and find_depot_tools.py wrapper, will run gsutil
+        # directly.
+        return None
+
     sys.path.append(os.path.join(DIR_SRC_ROOT, 'build'))
     # Do not pollute the environment, callers should not use find_depot_tools
     # directly.
@@ -31,4 +37,7 @@ GSUTIL_PATH = _find_gsutil()
 def run_gsutil(args: List[str]) -> None:
     """ Runs gsutil with |args| and throws CalledProcessError if the process
     failed. """
+    if not GSUTIL_PATH:
+        # Try to run gsutil directly if there isn't a gsutil.py wrapper.
+        return subprocess.run(['gsutil'] + args, check=True)
     return subprocess.run([sys.executable, GSUTIL_PATH] + args, check=True)
