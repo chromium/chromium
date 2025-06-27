ffc3efce7da03c2368180e7e3563fa2de3a380fe
Dan Polanco <polardz@google.com>
2025-06-27 11:27:50 -0700

    [M139][New Tab Animation] Insert background animation below message container
    (cherry picked from commit 408a0483de3956fbddb5d5f846338a7da33f1ce3)
    
    http://go/scrcast/NTQyODEzODM4ODQ4ODE5MnwwZDI2ZTMwYS0wYQ
    
    Bug: 424050950
    Fixed: 428046046
    Change-Id: I7775724ee6e81213342042b1b78924c59a41bef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6677513
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Dan Polanco <polardz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1479167}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6684386
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7258@{#214}
    Cr-Branched-From: f600d0656fd5b5fe4a82981f533d31ed6939e2e4-refs/heads/main@{#1477651}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -883,7 +883,7 @@ public class ChromeTabbedActivity extends ChromeActivity {
                             mRootUiCoordinator::getTopUiThemeColorProvider,
                             createHubLayoutDependencyHolder(),
                             getCompositorViewHolderSupplier(),
-                            getContentView(),
+                            getContentView().findViewById(R.id.coordinator),
                             getToolbarManager(),
                             mRootUiCoordinator.getScrimManager().getScrimVisibilitySupplier());
             mLayoutStateProviderSupplier.set(mLayoutManager);
