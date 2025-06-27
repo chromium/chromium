a46b7eee3ecd43c41349c2b104301166f9743367
Peter Wen <wnwen@chromium.org>
2025-06-27 10:46:12 -0700

    orderfile: Update merge script for orderfile
    The orderfile bot requires that the orderfile produced by the device and
    uploaded to the CAS output be copied over to the profile dir in order to
    be uploaded to CIPD together with the PGO profile.
    
    Bug: 372693140
    Change-Id: Ic880b11658a6893a1676d1ac2b67a08386d07ebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6675644
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1479948}

--- testing/merge_scripts/code_coverage/merge_results.py
+++ testing/merge_scripts/code_coverage/merge_results.py
@@ -15,6 +15,7 @@ import argparse
 import json
 import logging
 import os
+import shutil
 import subprocess
 import sys
 
@@ -123,6 +124,28 @@ def main():
     if rc != 0:
       failed = True
       logging.warning('%s exited with %s', merge_js_results_script, rc)
+
+  # The orderfile bot outputs only one orderfile that needs to be copied over
+  # to the profdata dir, no additional merging necessary.
+  if 'orderfile' in params.profdata_dir:
+    logging.info('Detected orderfile bot in %s', params.profdata_dir)
+    orderfiles = []
+    for dir_path, _sub_dirs, file_names in os.walk(params.task_output_dir):
+      logging.info('Found orderfile dir: %s', dir_path)
+      for fn in file_names:
+        if 'unpatched' in fn:
+          logging.info('Ignoring %s as it is unpatched.', fn)
+        else:
+          logging.info('Found orderfile: %s', fn)
+          orderfiles.append(os.path.join(dir_path, fn))
+    assert len(orderfiles) == 1, f'More than one orderfile found: {orderfiles}'
+    source_path = orderfiles[0]
+    # It is important to copy the file to the root profile dir with a
+    # predictable name for the bot to find it reliably.
+    dest_path = os.path.join(os.path.dirname(params.profdata_dir),
+                             'orderfile.out')
+    logging.info('Copying orderfile from %s to %s', source_path, dest_path)
+    shutil.copyfile(source_path, dest_path)
 
   # Name the output profdata file name as {test_target}.profdata or
   # default.profdata.
