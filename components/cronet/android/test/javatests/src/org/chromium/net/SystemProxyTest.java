428240f9c9c4d281dec98e5df8cd20a5d380bbce
Etienne Dechamps <edechamps@google.com>
2025-06-30 09:45:26 -0700

    Test https system proxy properties
    Also, clarify the way scheme works in standard Java system proxy
    properties - contrary to what I wrote earlier, the scheme refers to the
    destination, not the proxy itself.
    
    Change-Id: I5e988a64099cb5615580fba9229b784cda03b93e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6633412
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480570}

--- components/cronet/android/test/javatests/src/org/chromium/net/SystemProxyTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/SystemProxyTest.java
@@ -20,25 +20,24 @@ import org.chromium.base.test.util.Batch;
 public final class SystemProxyTest {
     @Rule public final CronetTestRule mTestRule = CronetTestRule.withManualEngineStartup();
 
-    private static final String TEST_PATH = "/test-path";
-    // This URL doesn't need to actually work, because Cronet won't try to connect to it - it
+    // This hostname doesn't need to actually work, because Cronet won't try to connect to it - it
     // expects the proxy to connect to it instead.
     //
     // Note that we can't use "localhost" or anything similar here, because //net implicitly
     // bypasses the proxy for local names and IP addresses; see
     // //net/docs/proxy.md and ProxyBypassRules::MatchesImplicitRules().
-    private static final String TEST_URL = "http://test-hostname" + TEST_PATH;
+    private static final String TEST_HOSTNAME = "test-hostname";
 
     /**
-     * Tests that, if we setup a cleartext HTTP proxy using the standard Java proxy system
-     * properties, then Cronet uses that proxy. Also, we send that request to a cleartext HTTP
-     * destination, so we expect Cronet to send the request to the proxy using the "traditional"
-     * HTTP proxy protocol (a.k.a "message forwarding", a.k.a "absolute-form", not tunnel/CONNECT -
-     * see section 3.2.2 of RFC 9112).
+     * Tests that, if we setup a proxy for the http:// scheme using the standard Java proxy system
+     * properties, then Cronet uses that proxy for http:// destinations. Also, because we send that
+     * request to a cleartext HTTP destination, we expect Cronet to send the request to the proxy
+     * using the "traditional" HTTP proxy protocol (a.k.a "message forwarding", a.k.a
+     * "absolute-form", not tunnel/CONNECT - see section 3.2.2 of RFC 9112).
      */
     @Test
     @SmallTest
-    public void testSystemProxy_cleartextHttpProxy_sendsPathToProxy() {
+    public void testHttpScheme_sendsPathToProxy() {
         var requestHandler =
                 new NativeTestServer.HandleRequestCallback() {
                     public NativeTestServer.HttpRequest mReceivedHttpRequest;
@@ -64,18 +63,29 @@ public final class SystemProxyTest {
             //
             // Android supports these, and will set them if the OS is configured to use a system
             // proxy.
-            try (var proxyHostScopedSystemProperty =
+            //
+            // Note we also set the https properties to invalid values; this way, we also check that
+            // Cronet ignores these properties. Indeed the "http." part in the system property key
+            // refers to the scheme of the *destination*, not the scheme used to talk to the proxy.
+            try (var httpProxyHostScopedSystemProperty =
                             new ScopedSystemProperty("http.proxyHost", "localhost");
-                    var proxyPortScopedSystemProperty =
+                    var httpProxyPortScopedSystemProperty =
                             new ScopedSystemProperty(
-                                    "http.proxyPort", String.valueOf(NativeTestServer.getPort()))) {
+                                    "http.proxyPort", String.valueOf(NativeTestServer.getPort()));
+                    var httpsProxyHostScopedSystemProperty =
+                            new ScopedSystemProperty("https.proxyHost", "invalid-host");
+                    var httpsProxyPortScopedSystemProperty =
+                            new ScopedSystemProperty("https.proxyPort", "42")) {
                 mTestRule.getTestFramework().startEngine();
 
                 var callback = new TestUrlRequestCallback();
                 mTestRule
                         .getTestFramework()
                         .getEngine()
-                        .newUrlRequestBuilder(TEST_URL, callback, callback.getExecutor())
+                        .newUrlRequestBuilder(
+                                "http://" + TEST_HOSTNAME + "/test-path",
+                                callback,
+                                callback.getExecutor())
                         .build()
                         .start();
                 callback.blockForDone();
@@ -83,9 +93,83 @@ public final class SystemProxyTest {
         }
 
         assertThat(requestHandler.mReceivedHttpRequest).isNotNull();
+        assertThat(requestHandler.mReceivedHttpRequest.getMethod()).isEqualTo("GET");
         // NativeTestServer actually sees "GET http://.../... HTTP/1.1", but sadly it strips the URL
         // and only passes the path section to the callback. See
         // net::test_server::HttpRequestParser::ParseHeaders().
-        assertThat(requestHandler.mReceivedHttpRequest.getRelativeUrl()).isEqualTo(TEST_PATH);
+        assertThat(requestHandler.mReceivedHttpRequest.getRelativeUrl()).isEqualTo("/test-path");
+    }
+
+    /**
+     * Tests that, if we setup a proxy for the https:// scheme using the standard Java proxy system
+     * properties, then Cronet uses that proxy for https:// destinations. Also, because we send that
+     * request to a secure HTTPS destination, we expect Cronet to send the request through the proxy
+     * using the tunnel mechanism (e.g. CONNECT HTTP method).
+     *
+     * <p>Note: there is no test for using HTTPS to talk to the proxy itself because that is not
+     * supported for system proxies - Android system configuration does not have any way to express
+     * that.
+     */
+    @Test
+    @SmallTest
+    public void testHttpsScheme_usesConnect() {
+        var requestHandler =
+                new NativeTestServer.HandleRequestCallback() {
+                    public NativeTestServer.HttpRequest mReceivedHttpRequest;
+
+                    @Override
+                    public NativeTestServer.RawHttpResponse handleRequest(
+                            NativeTestServer.HttpRequest httpRequest) {
+                        assertThat(mReceivedHttpRequest).isNull();
+                        mReceivedHttpRequest = httpRequest;
+                        return new NativeTestServer.RawHttpResponse("", "");
+                    }
+                };
+
+        try (var nativeTestServerScope =
+                new NativeTestServer.PreparedScope(mTestRule.getTestFramework().getContext())) {
+            NativeTestServer.registerRequestHandler(requestHandler);
+            NativeTestServer.startPrepared();
+
+            // These are the standard Java system properties for discovering system proxies,
+            // see:
+            //
+            // https://docs.oracle.com/en/java/javase/24/docs/api/java.base/java/net/doc-files/net-properties.html
+            //
+            // Android supports these, and will set them if the OS is configured to use a system
+            // proxy.
+            //
+            // Note we also set the http properties to invalid values; this way, we also check that
+            // Cronet ignores these properties. Indeed the "https." part in the system property key
+            // refers to the scheme of the *destination*, not the scheme used to talk to the proxy.
+            try (var httpsProxyHostScopedSystemProperty =
+                            new ScopedSystemProperty("https.proxyHost", "localhost");
+                    var httpsProxyPortScopedSystemProperty =
+                            new ScopedSystemProperty(
+                                    "https.proxyPort", String.valueOf(NativeTestServer.getPort()));
+                    var httpProxyHostScopedSystemProperty =
+                            new ScopedSystemProperty("http.proxyHost", "invalid-host");
+                    var httpProxyPortScopedSystemProperty =
+                            new ScopedSystemProperty("http.proxyPort", "42")) {
+                mTestRule.getTestFramework().startEngine();
+
+                var callback = new TestUrlRequestCallback();
+                mTestRule
+                        .getTestFramework()
+                        .getEngine()
+                        .newUrlRequestBuilder(
+                                "https://" + TEST_HOSTNAME + "/test-path",
+                                callback,
+                                callback.getExecutor())
+                        .build()
+                        .start();
+                callback.blockForDone();
+            }
+        }
+
+        assertThat(requestHandler.mReceivedHttpRequest).isNotNull();
+        assertThat(requestHandler.mReceivedHttpRequest.getMethod()).isEqualTo("CONNECT");
+        assertThat(requestHandler.mReceivedHttpRequest.getRelativeUrl())
+                .isEqualTo(TEST_HOSTNAME + ":443");
     }
 }
