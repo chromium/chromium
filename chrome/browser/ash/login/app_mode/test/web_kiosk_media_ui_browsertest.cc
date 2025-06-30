aae39affe956d751348a8483b804d9334a44d568
Edman Anjos <edmanp@google.com>
2025-06-30 07:28:59 -0700

    kiosk: Migrate WebKioskMediaUITest to KioskMixin
    Bug: 402454812
    Test: testing/xvfb.py tools/autotest.py -C out/Release chrome/browser/ash/login/app_mode/test/web_kiosk_media_ui_browsertest.cc
    Change-Id: Ieb5b3228d51b1f8ddaaa611a4972d810145d42f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6686152
    Reviewed-by: Ben Franz <bfranz@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1480506}

--- chrome/browser/ash/login/app_mode/test/web_kiosk_media_ui_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/web_kiosk_media_ui_browsertest.cc
@@ -2,42 +2,77 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+#include <vector>
+
 #include "ash/system/media/media_tray.h"
-#include "chrome/browser/ash/login/app_mode/test/web_kiosk_base_test.h"
+#include "base/check_deref.h"
+#include "base/functional/callback_helpers.h"
+#include "chrome/browser/ash/app_mode/test/kiosk_mixin.h"
+#include "chrome/browser/ash/app_mode/test/kiosk_test_utils.h"
 #include "chrome/browser/ui/global_media_controls/media_notification_service.h"
 #include "chrome/browser/ui/global_media_controls/media_notification_service_factory.h"
+#include "chrome/test/base/mixin_based_in_process_browser_test.h"
+#include "chrome/test/base/test_browser_window.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/media_router/browser/presentation/start_presentation_context.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+#include "url/origin.h"
 
 namespace ash {
 
-using WebKioskMediaUITest = WebKioskBaseTest;
+using kiosk::test::WaitKioskLaunched;
+
+namespace {
+
+// Returns a valid `StartPresentationContext` that does nothing.
+std::unique_ptr<media_router::StartPresentationContext>
+DummyPresentationContext(Browser& browser) {
+  return std::make_unique<media_router::StartPresentationContext>(
+      content::PresentationRequest(browser.tab_strip_model()
+                                       ->GetActiveWebContents()
+                                       ->GetPrimaryMainFrame()
+                                       ->GetGlobalId(),
+                                   /*presentation_urls=*/{GURL(), GURL()},
+                                   /*frame_origin=*/url::Origin()),
+      /*success_cb=*/base::DoNothing(),
+      /*error_cb=*/base::DoNothing());
+}
+
+}  // namespace
+
+class WebKioskMediaUITest : public MixinBasedInProcessBrowserTest {
+ public:
+  WebKioskMediaUITest() = default;
+
+  WebKioskMediaUITest(const WebKioskMediaUITest&) = delete;
+  WebKioskMediaUITest& operator=(const WebKioskMediaUITest&) = delete;
+
+  ~WebKioskMediaUITest() override = default;
+
+  KioskMixin kiosk_{
+      &mixin_host_,
+      /*cached_configuration=*/
+      KioskMixin::Config{/*name=*/{},
+                         KioskMixin::AutoLaunchAccount{
+                             KioskMixin::SimpleWebAppOption().account_id},
+                         {KioskMixin::SimpleWebAppOption()}}};
+};
 
 IN_PROC_BROWSER_TEST_F(WebKioskMediaUITest, MediaTrayStaysPinnedInKiosk) {
-  InitializeRegularOnlineKiosk();
-  SelectFirstBrowser();
-
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), embedded_test_server()->GetURL("/simple_page.html")));
+  ASSERT_TRUE(WaitKioskLaunched());
 
   ash::MediaTray::SetPinnedToShelf(false);
   ASSERT_FALSE(ash::MediaTray::IsPinnedToShelf());
 
-  auto presentation_context =
-      std::make_unique<media_router::StartPresentationContext>(
-          content::PresentationRequest(browser()
-                                           ->tab_strip_model()
-                                           ->GetActiveWebContents()
-                                           ->GetPrimaryMainFrame()
-                                           ->GetGlobalId(),
-                                       {GURL(), GURL()}, url::Origin()),
-          base::DoNothing(), base::DoNothing());
+  SelectFirstBrowser();
 
   MediaNotificationServiceFactory::GetForProfile(GetProfile())
-      ->ShowDialogAsh(std::move(presentation_context));
+      ->ShowDialogAsh(DummyPresentationContext(CHECK_DEREF(browser())));
 
   EXPECT_TRUE(ash::MediaTray::IsPinnedToShelf());
 }
+
 }  // namespace ash
