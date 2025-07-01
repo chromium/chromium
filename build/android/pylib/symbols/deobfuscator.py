47e91ac999d6acab39340b509e86962308eca880
Ben Pastene <bpastene@chromium.org>
2025-06-30 19:19:23 -0700

    Set a higher deobfuscator proc timeout for android-desktop hw bot tests
    Such bots have cpu limits applied, which leaads the deobfuscator procs
    to timeout. eg That's the cause for all the timeouts in
    http://shortn/_VXqrHIVItR
    
    Bumping the timeout should help avoid that while we work on relaxing
    the CPU constraints.
    
    Attempts were made to detect the current proc's cpu limit rather than
    checking hard-coded env vars (by using code like:
    https://source.chromium.org/chromium/chromium/src/+/main:build/toolchain/get_concurrent_links.py;drc=9e6de503ba23129d430dcac15062114f992ce0be;l=39).
    But couldn't get it to work, so this just checks some env vars we
    know will be uniquely set in the environments that have the cpu
    limits in place.
    
    Bug: 421235960
    Change-Id: I6644a6c81fb59c1ab032e5b9175f233dffea1624
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6693012
    Commit-Queue: Ben Pastene <bpastene@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480837}

--- build/android/pylib/symbols/deobfuscator.py
+++ build/android/pylib/symbols/deobfuscator.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import os
 
 from pylib import constants
@@ -18,8 +19,17 @@ _PASSTHROUH_ON_FAILURE = False
 
 class Deobfuscator(ExpensiveLineTransformer):
   def __init__(self, mapping_path):
-    super().__init__(_PROCESS_START_TIMEOUT, _MINIMUM_TIMEOUT,
-                     _PER_LINE_TIMEOUT)
+    min_timeout = _MINIMUM_TIMEOUT
+    for k in os.environ:
+      if k.startswith('DRONE') or k.startswith('SKYLAB'):
+        logging.warning(
+            'Likely running in a CPU-limited context due to the presence of '
+            'the %s env var. Setting min timeout for Deobfuscator to 10x.', k)
+        # TODO(crbug.com/421235960): Remove this if the CPU constraints are
+        # removed.
+        min_timeout = 10 * _MINIMUM_TIMEOUT
+        break
+    super().__init__(_PROCESS_START_TIMEOUT, min_timeout, _PER_LINE_TIMEOUT)
     script_path = os.path.join(constants.DIR_SOURCE_ROOT, 'build', 'android',
                                'stacktrace', 'java_deobfuscate.py')
     self._command = [script_path, mapping_path]
