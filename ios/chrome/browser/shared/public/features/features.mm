4848ea190ce483272137ae925da0e352d10a8a83
Olivier ROBIN <olivierrobin@google.com>
2025-07-01 01:02:12 -0700

    Mark IOSOneTapMiniMapRemoveSectionsBreaks default enabled
    This is a bug fix for MiniMap.
    Experiment looks good, marking default enabled.
    
    Bug: 413650990
    Change-Id: I76658cb33acb965a9a755d99bbbce857ae3ef43c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6687789
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1480903}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -1186,7 +1186,7 @@ constexpr base::FeatureParam<double>
 
 BASE_FEATURE(kIOSOneTapMiniMapRemoveSectionsBreaks,
              "IOSOneTapMiniMapRemoveSectionsBreaks",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIOSMiniMapUniversalLink,
              "IOSMiniMapUniversalLink",
