25df184504e7b2d5e31353a514217056e7226ac2
Lambros Lambrou <lambroslambrou@chromium.org>
2025-07-01 11:21:30 -0700

    [remoting] Remove unnecessary call to IgnoreXServerGrabs().
    In the remoting host, IgnoreXServerGrabs() is called in 3 places:
    
    1. BasicDesktopEnvironment ctor
    2. LegacyInteractionStrategy, before each screen-capturer is created
    3. InputInjectorX11
    
    In places 1 and 2, the call operates on the libX11 Display which is
    part of the webrtc::DesktopCaptureOptions. Those Displays are created
    and owned by the WebRTC desktop-capture module. In place 3, the call
    operates on an x11::Connection object, which is the Chromium
    implementation of the X11 protocol, separate from libX11.
    
    In place 1, the libX11 Display actually gets created during startup in
    the HostProcess ctor which calls through to CreateDefault() in WebRTC.
    But each screen-capturer runs on a separate libX11 Display - see
    http://crrev.com/c/3898352. So this Display is not used by anything.
    All other X11 calls either use the Chromium x11::Connection, or some
    separate X11 connection inside GTK/GDK.
    
    This CL removes the call from place 1. Of the 3 places, only place 1
    was guarded by a watchdog. This watchdog is deprecated in favor of
    HangWatcher, and is probably not needed since we initialized libX11
    for multi-threaded use in http://crrev.com/c/2780005. So, instead of
    moving the watchdog to place 2, we remove it completely.
    
    Removing this code also has the benefit of not calling it if Wayland
    is enabled.
    
    Bug: 40149664
    Change-Id: I9e2f86abf2fe37fc4ee78baa8cc4238d1fca6f16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6692991
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481170}

--- remoting/host/basic_desktop_environment.cc
+++ remoting/host/basic_desktop_environment.cc
@@ -47,49 +47,7 @@
 #include "remoting/host/win/evaluate_d3d.h"
 #endif
 
-#if defined(REMOTING_USE_X11)
-#include "base/threading/watchdog.h"
-#endif
-
 namespace remoting {
-
-#if defined(REMOTING_USE_X11)
-
-namespace {
-
-// The maximum amount of time we will wait for the IgnoreXServerGrabs() to
-// return before we crash the host.
-constexpr base::TimeDelta kWaitForIgnoreXServerGrabsTimeout = base::Seconds(30);
-
-// Helper class to monitor the call to
-// webrtc::SharedXDisplay::IgnoreXServerGrabs() (on a temporary thread), which
-// has been observed to occasionally hang forever and zombify the host.
-// This class crashes the host if the IgnoreXServerGrabs() call takes too long,
-// so that the ME2ME daemon process can respawn the host.
-// See: crbug.com/1130090
-class IgnoreXServerGrabsWatchdog : public base::Watchdog::Delegate {
- public:
-  IgnoreXServerGrabsWatchdog()
-      : watchdog_(kWaitForIgnoreXServerGrabsTimeout,
-                  "IgnoreXServerGrabs Watchdog",
-                  /*enabled=*/true,
-                  this) {}
-  ~IgnoreXServerGrabsWatchdog() override = default;
-
-  void Arm() { watchdog_.Arm(); }
-
-  void Alarm() override {
-    // Crash the host if IgnoreXServerGrabs() takes too long.
-    NOTREACHED() << "IgnoreXServerGrabs() timed out.";
-  }
-
- private:
-  base::Watchdog watchdog_;
-};
-
-}  // namespace
-
-#endif  // defined(REMOTING_USE_X11)
 
 BasicDesktopEnvironment::~BasicDesktopEnvironment() {
   DCHECK(caller_task_runner_->BelongsToCurrentThread());
@@ -211,14 +169,7 @@ BasicDesktopEnvironment::BasicDesktopEnvironment(
       client_session_control_(client_session_control),
       options_(options) {
   DCHECK(caller_task_runner_->BelongsToCurrentThread());
-#if defined(REMOTING_USE_X11)
-  // TODO(yuweih): The watchdog is just to test the hypothesis.
-  // The IgnoreXServerGrabs() call should probably be moved to whichever
-  // thread that created desktop_capture_options().x_display().
-  IgnoreXServerGrabsWatchdog watchdog;
-  watchdog.Arm();
-  desktop_capture_options().x_display()->IgnoreXServerGrabs();
-#elif BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_WIN)
   // Check whether D3D is available as long as the DirectX capturer wasn't
   // explicitly disabled. This check is necessary because the network process
   // runs in Session 0 and cannot check whether D3D is available or not so the
