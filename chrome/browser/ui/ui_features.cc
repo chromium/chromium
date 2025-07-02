135141742558f5f6fe05622cf1b0f574e4e28d36
Austin Zhang <austinzzr@google.com>
2025-07-02 13:17:03 -0700

    Enable the EnablePolicyPromotionBanner on non_cros by default.
    This corresponds to the Finch config: http://google3/googledata/googleclient/chrome/finch/gcl_studies/EnablePolicyPromotionBanner.gcl;rcl=777567969, to launch the non_cros config only.
    
    Change-Id: Idb4e88114d42efd96eaa86c2c1f7cd2ee8d5fccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694861
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Austin Zhang <austinzzr@google.com>
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1481840}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -400,9 +400,15 @@ BASE_FEATURE(kEnableManagementPromotionBanner,
              "EnableManagementPromotionBanner",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+#if BUILDFLAG(IS_CHROMEOS)
 BASE_FEATURE(kEnablePolicyPromotionBanner,
              "EnablePolicyPromotionBanner",
              base::FEATURE_DISABLED_BY_DEFAULT);
+#else
+BASE_FEATURE(kEnablePolicyPromotionBanner,
+             "EnablePolicyPromotionBanner",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
 
 BASE_FEATURE(kInlineFullscreenPerfExperiment,
              "InlineFullscreenPerfExperiment",
