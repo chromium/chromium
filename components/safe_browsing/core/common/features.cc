efd636e2c1e71c0f2783bcc281a6f35ed98efffc
Haihan Chen <haihan@google.com>
2025-07-02 08:48:02 -0700

    [DLP Support in FSA] Enable fsa enterprise deep scan by default
    Bug: 363248461
    Change-Id: I2b11a27214e0cdd125561a01361e6cb056767495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6698914
    Commit-Queue: Haihan Chen <haihan@google.com>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481646}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -151,7 +151,7 @@ BASE_FEATURE(kEnhancedSafeBrowsingPromo,
 
 BASE_FEATURE(kEnterpriseFileSystemAccessDeepScan,
              "EnterpriseFileSystemAccessDeepScan",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnterprisePasswordReuseUiRefresh,
              "EnterprisePasswordReuseUiRefresh",
