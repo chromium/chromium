0bc955d001d093596a2b7f40d8ab76220d81b15c
zhoupeng <zhoupeng.1996@bytedance.com>
2025-07-02 11:54:06 -0700

    Fix macOS Look Up not working for Shadow DOM content
    `InputMethodController::GetSelectionOffsets` considers the Shadow DOM
    when computing the `PlainTextRange`, as the Shadow DOM offsets are
    relative to the `ShadowRoot`. In contrast, the
    `AttributedSubstringInRange` function does not account for the Shadow
    DOM. Therefore, when the `Selection` resides within a Shadow DOM, it
    must be resolved with reference to the corresponding `ShadowRoot`.
    
    Adding blink unit tests and web_tests can be challenging, so we included
    a Look Up test case directly.
    
    Bug: 358393352, 40869037
    Change-Id: I510abb7748a4fa3b2311c07ff7c8716fd9eddc56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6685626
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481784}

--- third_party/blink/renderer/core/editing/substring_util.mm
+++ third_party/blink/renderer/core/editing/substring_util.mm
@@ -39,6 +39,7 @@
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/dom/node.h"
+#include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/editor.h"
 #include "third_party/blink/renderer/core/editing/ephemeral_range.h"
 #include "third_party/blink/renderer/core/editing/frame_selection.h"
@@ -58,6 +59,7 @@
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/fonts/font.h"
 #include "third_party/blink/renderer/platform/mac/color_mac.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -217,12 +219,26 @@ SubstringUtil::AttributedSubstringInRange(LocalFrame* frame,
                                           gfx::Point& baseline_point) {
   frame->View()->UpdateStyleAndLayout();
 
-  Element* editable = frame->Selection().RootEditableElementOrDocumentElement();
-  if (!editable) {
+  ContainerNode* container_node = nullptr;
+  if (RuntimeEnabledFeatures::HandleShadowDOMInSubstringUtilEnabled()) {
+    Position start =
+        frame->Selection().ComputeVisibleSelectionInDOMTree().Start();
+    if (IsEditablePosition(start)) {
+      container_node = RootEditableElementOf(start);
+    } else if (start.AnchorNode() && start.AnchorNode()->IsInShadowTree()) {
+      container_node = start.AnchorNode()->ContainingShadowRoot();
+    } else {
+      container_node = frame->GetDocument()->documentElement();
+    }
+  } else {
+    container_node = frame->Selection().RootEditableElementOrDocumentElement();
+  }
+  if (!container_node) {
     return base::apple::ScopedCFTypeRef<CFAttributedStringRef>();
   }
+
   const EphemeralRange ephemeral_range(
-      PlainTextRange(location, location + length).CreateRange(*editable));
+      PlainTextRange(location, location + length).CreateRange(*container_node));
   if (ephemeral_range.IsNull()) {
     return base::apple::ScopedCFTypeRef<CFAttributedStringRef>();
   }
