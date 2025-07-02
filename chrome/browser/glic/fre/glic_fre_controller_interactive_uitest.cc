a6ce57a80822c5ad8d1fc5605d5f6f67e5b218db
Wenbo Jie <wenbojie@chromium.org>
2025-07-01 18:51:55 -0700

    [Gardener] disable test ShowsErrorPanelOnLoadingTimeout on Mac
    The test is introduced in CL:6669255 and is consistently failing
    on some Mac bots.
    
    Bug: 429040435
    Change-Id: I5dee26daab7d0517c6ad281e42e5bdba4342f48f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695845
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Owners-Override: Wenbo Jie <wenbojie@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Auto-Submit: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481348}

--- chrome/browser/glic/fre/glic_fre_controller_interactive_uitest.cc
+++ chrome/browser/glic/fre/glic_fre_controller_interactive_uitest.cc
@@ -393,8 +393,15 @@ IN_PROC_BROWSER_TEST_F(GlicFreControllerUiTest, ShowsErrorPanelOnInvalidAuth) {
                                          {"#errorPanel:not([hidden])"})));
 }
 
+// TODO(crbug.com/429040435): Test is failing on Mac bots.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_ShowsErrorPanelOnLoadingTimeout \
+  DISABLED_ShowsErrorPanelOnLoadingTimeout
+#else
+#define MAYBE_ShowsErrorPanelOnLoadingTimeout ShowsErrorPanelOnLoadingTimeout
+#endif
 IN_PROC_BROWSER_TEST_F(GlicFreControllerUiTimeoutTest,
-                       ShowsErrorPanelOnLoadingTimeout) {
+                       MAYBE_ShowsErrorPanelOnLoadingTimeout) {
   auto server_running = fre_server().StartAcceptingConnectionsAndReturnHandle();
 
   RunTestSequence(
