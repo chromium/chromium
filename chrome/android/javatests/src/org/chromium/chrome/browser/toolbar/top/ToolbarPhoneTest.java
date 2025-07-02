d146ad0651689cbd2a77a6db1537c37b6a986cab
Xi Han <hanxi@google.com>
2025-07-02 11:05:13 -0700

    Reenable ToolbarPhoneTest# testToolbarBackgroundChangedWhenSearchEngineHasNoLogo.
    From the post test screenshot, it looks like the test failed due to the
    feature flag isn't enabled default on the bot. Explicitly enabling the
    flag make the test passed locally.
    
    Bug: 429143821, 426594110, 339034032
    Change-Id: I901c3734ad7e53932fcd3fd74b4cdee134cc2b15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701067
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481736}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhoneTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhoneTest.java
@@ -63,7 +63,6 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -573,7 +572,10 @@ public class ToolbarPhoneTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/426594110, crbug.com/339034032")
+    @EnableFeatures({
+        OmniboxFeatureList.OMNIBOX_MOBILE_PARITY_UPDATE,
+        OmniboxFeatureList.OMNIBOX_MOBILE_PARITY_UPDATE_V2
+    })
     public void testToolbarBackgroundChangedWhenSearchEngineHasNoLogo() {
         when(mTemplateUrlService.doesDefaultSearchEngineHaveLogo()).thenReturn(false);
 
