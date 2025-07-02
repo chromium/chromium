704553e522aae1796598a1958247da6a721059b9
Vasiliy Telezhnikov <vasilyt@chromium.org>
2025-07-02 06:16:01 -0700

    Make //gpu/command_buffer/common component
    Bug: 422797572
    Change-Id: Ie54202b6bcf8f5d179131e18f588d9999ae583b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6675585
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481543}

--- gpu/command_buffer/common/gpu_command_buffer_common_export.h
+++ gpu/command_buffer/common/gpu_command_buffer_common_export.h
@@ -8,11 +8,11 @@
 #if defined(COMPONENT_BUILD)
 #if defined(WIN32)
 
-#if defined(GPU_IMPLEMENTATION)
+#if defined(GPU_COMMAND_BUFFER_COMMON_IMPLEMENTATION)
 #define GPU_COMMAND_BUFFER_COMMON_EXPORT __declspec(dllexport)
 #else
 #define GPU_COMMAND_BUFFER_COMMON_EXPORT __declspec(dllimport)
-#endif  // defined(GPU_IMPLEMENTATION)
+#endif  // defined(GPU_COMMAND_BUFFER_COMMON_IMPLEMENTATION)
 
 #else  // defined(WIN32)
 #define GPU_COMMAND_BUFFER_COMMON_EXPORT __attribute__((visibility("default")))
