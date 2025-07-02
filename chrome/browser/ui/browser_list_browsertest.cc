71ffe3a8ef598fc9d64272a3dc0848905a3d8f81
Ho Cheung <hocheung@chromium.org>
2025-07-02 16:42:25 -0700

    Refactor BrowserListUnitTest to use InProcessBrowserTest
    1.Replace BrowserWithTestWindowTest with InProcessBrowserTest as the
    test fixture, and rename to BrowserListBrowserTest.
    
    2. Some other changes were necessary.
    
    Bug: 417766643
    Change-Id: Ideaf9a3d1cdaf9fb60fc60cbb18963d3ee90b4ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695566
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481962}

--- chrome/browser/ui/browser_list_browsertest.cc
+++ chrome/browser/ui/browser_list_browsertest.cc
@@ -7,38 +7,38 @@
 #include <memory>
 #include <set>
 
+#include "base/containers/contains.h"
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/browser/ui/browser_list_observer.h"
+#include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "content/public/test/browser_test.h"
 #include "ui/base/mojom/window_show_state.mojom.h"
 
-class BrowserListUnitTest : public BrowserWithTestWindowTest {
- public:
-  BrowserListUnitTest() = default;
-  BrowserListUnitTest(const BrowserListUnitTest&) = delete;
-  BrowserListUnitTest& operator=(const BrowserListUnitTest&) = delete;
-  ~BrowserListUnitTest() override = default;
-};
+using BrowserListBrowserTest = InProcessBrowserTest;
 
 // This tests that minimized windows get added to the active list, at the front
 // the list.
-TEST_F(BrowserListUnitTest, TestMinimized) {
+IN_PROC_BROWSER_TEST_F(BrowserListBrowserTest, TestMinimized) {
   const BrowserList* browser_list = BrowserList::GetInstance();
   EXPECT_EQ(1U, browser_list->size());
   EXPECT_EQ(browser(), browser_list->GetLastActive());
 
   // Create a minimized browser window. It should be prepended to the active
   // list, so browser() should still be at the end of the list.
-  Browser::CreateParams native_params(profile(), true);
-  native_params.initial_show_state = ui::mojom::WindowShowState::kMinimized;
-  std::unique_ptr<Browser> browser2(
-      CreateBrowserWithTestWindowForParams(native_params));
+  Browser::CreateParams params(GetProfile(), true);
+  params.initial_show_state = ui::mojom::WindowShowState::kMinimized;
+  Browser::Create(params);
   EXPECT_EQ(2U, browser_list->size());
   EXPECT_EQ(browser(), browser_list->GetLastActive());
 }
 
 // This tests that inactive windows do not get added to the active list.
-TEST_F(BrowserListUnitTest, TestInactive) {
+IN_PROC_BROWSER_TEST_F(BrowserListBrowserTest, TestInactive) {
   const BrowserList* browser_list = BrowserList::GetInstance();
   EXPECT_EQ(1U, browser_list->size());
   EXPECT_EQ(browser(), browser_list->GetLastActive());
@@ -46,17 +46,17 @@ TEST_F(BrowserListUnitTest, TestInactive) {
   // Create an inactive browser window. It should be prepended to
   // |BrowserList::browsers_ordered_by_activation_| so the default browser
   // should still be the last active.
-  Browser::CreateParams native_params(profile(), true);
-  native_params.initial_show_state = ui::mojom::WindowShowState::kInactive;
-  std::unique_ptr<Browser> browser2(
-      CreateBrowserWithTestWindowForParams(native_params));
+  Browser::CreateParams params(GetProfile(), true);
+  params.initial_show_state = ui::mojom::WindowShowState::kInactive;
+  Browser::Create(params);
   EXPECT_EQ(2U, browser_list->size());
   EXPECT_EQ(browser(), browser_list->GetLastActive());
 }
 
 // This tests if the browser list is returning the correct browser reference
 // for the context provided as input.
-TEST_F(BrowserListUnitTest, TestFindBrowserWithUiElementContext) {
+IN_PROC_BROWSER_TEST_F(BrowserListBrowserTest,
+                       TestFindBrowserWithUiElementContext) {
   const BrowserList* browser_list = BrowserList::GetInstance();
   EXPECT_EQ(1U, browser_list->size());
 
@@ -64,15 +64,12 @@ TEST_F(BrowserListUnitTest, TestFindBrowserWithUiElementContext) {
       browser_list->get(0)->window()->GetElementContext());
   EXPECT_EQ(browser_list->get(0), result);
 
-  Browser::CreateParams native_params(profile(), true);
-  std::unique_ptr<Browser> browser2(
-      CreateBrowserWithTestWindowForParams(native_params));
-  auto* browser_window2 = static_cast<TestBrowserWindow*>(browser2->window());
-  browser_window2->set_element_context(ui::ElementContext(2));
-
+  Browser* browser2 =
+      Browser::Create(Browser::CreateParams(GetProfile(), true));
+  ASSERT_EQ(2U, browser_list->size());
   result = chrome::FindBrowserWithUiElementContext(
       browser2->window()->GetElementContext());
-  EXPECT_EQ(browser2.get(), result);
+  EXPECT_EQ(browser2, result);
 
   result = chrome::FindBrowserWithUiElementContext(ui::ElementContext(100));
   EXPECT_EQ(nullptr, result);
@@ -142,20 +139,17 @@ class BrowserObserverParent : public BrowserListObserver {
   std::unique_ptr<BrowserObserverChild> child_observer_;
 };
 
-TEST_F(BrowserListUnitTest, ObserverAddedInFlight) {
+IN_PROC_BROWSER_TEST_F(BrowserListBrowserTest, ObserverAddedInFlight) {
   BrowserObserverParent parent_observer;
 
   const BrowserList* browser_list = BrowserList::GetInstance();
   EXPECT_EQ(1U, browser_list->size());
 
   // Adding second browser should not trigger double-observation.
-  Browser::CreateParams native_params(profile(), true);
-  std::unique_ptr<Browser> browser2(
-      CreateBrowserWithTestWindowForParams(native_params));
+  Browser::Create(Browser::CreateParams(GetProfile(), true));
   EXPECT_EQ(2U, browser_list->size());
 
   // Create one more browser to trigger BrowserObserverChild::OnBrowserAdded.
-  std::unique_ptr<Browser> browser3(
-      CreateBrowserWithTestWindowForParams(native_params));
+  Browser::Create(Browser::CreateParams(GetProfile(), true));
   EXPECT_EQ(3U, browser_list->size());
 }
