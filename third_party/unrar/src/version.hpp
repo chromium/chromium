30861c7ca5d2a1fadc10a25dd157d7d4e50cd856
Lily Chen <chlily@chromium.org>
2025-07-01 21:23:53 -0700

    Roll unrar to v7.12.0
    Updates unrar to v7.12.0 as extracted from
    https://www.rarlab.com/rar/unrarsrc-7.1.8.tar.gz and mirrored at
    https://github.com/aawc/unrar/releases/tag/v7.12.0.
    Note: the version number is [sic] 7.12.0 despite the tarball filename.
    Note: version 7.12.0 is a newer release than 7.12.1.
    
    Upstream release notes: https://www.rarlab.com/unrar7notes.htm
    
    The first patchset is a snapshot of the source. The Chromium patches and
    README updates are then applied on top of that in subsequent patchsets.
    
    BYPASS_TRAILING_WHITESPACE_REASON=third-party library contains trailing spaces
    
    Fixed: 427733003
    Change-Id: I84ed51cff771c1626bdb00cf63cd839b564dfad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6696593
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1481377}

--- third_party/unrar/src/version.hpp
+++ third_party/unrar/src/version.hpp
@@ -1,6 +1,6 @@
 #define RARVER_MAJOR     7
 #define RARVER_MINOR    12
-#define RARVER_BETA      1
-#define RARVER_DAY      10
+#define RARVER_BETA      0
+#define RARVER_DAY      23
 #define RARVER_MONTH     6
 #define RARVER_YEAR   2025
