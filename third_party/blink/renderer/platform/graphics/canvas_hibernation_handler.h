3a4fff96ae8d1ea723257c3acb1821ec2f6f31ab
Colin Blundell <blundell@chromium.org>
2025-07-02 23:52:34 -0700

    [Blink] Remove outdated comment on CanvasHibernationHandler::Delegate
    Bug: 352263194
    Change-Id: Ia3a0433a1b93345d23f07439fbf7e2e850d59f8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6694995
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1482068}

--- third_party/blink/renderer/platform/graphics/canvas_hibernation_handler.h
+++ third_party/blink/renderer/platform/graphics/canvas_hibernation_handler.h
@@ -35,11 +35,6 @@ class PLATFORM_EXPORT CanvasHibernationHandler {
     virtual bool IsContextLost() const = 0;
     virtual void ResetResourceProviderForCanvas2D() = 0;
     virtual void SetNeedsCompositingUpdate() = 0;
-
-    // Called when the CC texture layer that this instance is holding (if any)
-    // should be cleared. Subclasses that can hold a CC texture layer should
-    // override this method. Should only be called if the context is
-    // CanvasRenderingContext2D.
     virtual void ClearCanvas2DLayerTexture() {}
   };
 
