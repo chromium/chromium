ba8a8322a176d49b530142ca23909b37c1777156
Xi Han <hanxi@google.com>
2025-07-03 12:07:48 -0700

    [M139][Parity] Fix Logo and fake search box become invisible for 3p DSE with logo
    The bug was due to FeedSurfaceMediator#isScrollViewInitialized() returns false and make NTP believe the search box has scrolled off
    the screen and thus set its alpha as 0 (transparent). Update the check
    in isScrollViewInitialized() with feed is enabled.
    Video: https://drive.google.com/file/d/1Y4r4LXSYYA8GpLm1cngaiv2M0heWpaqW/view?usp=drive_link
    
    (cherry picked from commit e41b6386f0455ee83e0b9725b5191e50d9c751ec)
    
    Bug: 428037030, 429389883
    Change-Id: I70b9f6173f68c991affcf3cf4012b320b93f4cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6685449
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1480121}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6700672
    Cr-Commit-Position: refs/branch-heads/7258@{#641}
    Cr-Branched-From: f600d0656fd5b5fe4a82981f533d31ed6939e2e4-refs/heads/main@{#1477651}

--- chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
+++ chrome/android/feed/core/java/src/org/chromium/chrome/browser/feed/FeedSurfaceMediator.java
@@ -1323,7 +1323,7 @@ public class FeedSurfaceMediator
     @Override
     public boolean isScrollViewInitialized() {
         RecyclerView recyclerView = mCoordinator.getRecyclerView();
-        return recyclerView != null && recyclerView.getHeight() > 0;
+        return recyclerView != null && (!mFeedEnabled || recyclerView.getHeight() > 0);
     }
 
     @Override
