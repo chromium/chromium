b04cf425c85baea6bdf62490d839b97b982d947d
Sky Malice <skym@chromium.org>
2025-07-03 07:38:28 -0700

    Implement VersioningMessageBanner with a stub dialog.
    (cherry picked from commit 84fcf6c5483b583cf91c7e41569532273f44e3ff)
    
    Bug: 422514006, 428753561
    Change-Id: I9d5887bf573e2f866f56d9f3ba1cc1b977f6afad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6674616
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1479230}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697376
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7258@{#622}
    Cr-Branched-From: f600d0656fd5b5fe4a82981f533d31ed6939e2e4-refs/heads/main@{#1477651}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/ui/versioning/VersioningMessageBanner.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/ui/versioning/VersioningMessageBanner.java
@@ -4,12 +4,30 @@
 
 package org.chromium.chrome.browser.data_sharing.ui.versioning;
 
+import static org.chromium.components.messages.MessageBannerProperties.ALL_KEYS;
+import static org.chromium.components.messages.MessageBannerProperties.ICON;
+import static org.chromium.components.messages.MessageBannerProperties.MESSAGE_IDENTIFIER;
+import static org.chromium.components.messages.MessageBannerProperties.ON_FULLY_VISIBLE;
+import static org.chromium.components.messages.MessageBannerProperties.ON_PRIMARY_ACTION;
+import static org.chromium.components.messages.MessageBannerProperties.PRIMARY_BUTTON_TEXT;
+import static org.chromium.components.messages.MessageBannerProperties.TITLE;
+
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.tab_group_sync.TabGroupSyncServiceFactory;
 import org.chromium.components.messages.MessageDispatcher;
+import org.chromium.components.messages.MessageIdentifier;
+import org.chromium.components.messages.PrimaryActionClickBehavior;
+import org.chromium.components.tab_group_sync.MessageType;
+import org.chromium.components.tab_group_sync.TabGroupSyncService;
+import org.chromium.components.tab_group_sync.VersioningMessageController;
 import org.chromium.ui.modaldialog.ModalDialogManager;
+import org.chromium.ui.modelutil.PropertyModel;
 
 /**
  * Conditionally constructs and shows a message banner warning the user that they cannot use shared
@@ -31,6 +49,75 @@ public class VersioningMessageBanner {
             MessageDispatcher messageDispatcher,
             ModalDialogManager modalDialogManager,
             Profile profile) {
-        // TODO(https://crbug.com/422514006): Implement.
+        // TabGroupSyncService doesn't support OTR. Don't check/prompt the user in this case.
+        if (profile.isOffTheRecord()) return;
+
+        TabGroupSyncService tabGroupSyncService = TabGroupSyncServiceFactory.getForProfile(profile);
+        if (tabGroupSyncService == null) return;
+
+        VersioningMessageController versioningMessageController =
+                tabGroupSyncService.getVersioningMessageController();
+        if (versioningMessageController.shouldShowMessageUi(
+                MessageType.VERSION_OUT_OF_DATE_INSTANT_MESSAGE)) {
+            new VersioningMessageBanner(
+                            context,
+                            messageDispatcher,
+                            modalDialogManager,
+                            versioningMessageController)
+                    .show();
+        }
+    }
+
+    private final Context mContext;
+    private final MessageDispatcher mMessageDispatcher;
+    private final ModalDialogManager mModalDialogManager;
+    private final VersioningMessageController mVersioningMessageController;
+
+    private VersioningMessageBanner(
+            Context context,
+            MessageDispatcher messageDispatcher,
+            ModalDialogManager modalDialogManager,
+            VersioningMessageController versioningMessageController) {
+        mContext = context;
+        mMessageDispatcher = messageDispatcher;
+        mModalDialogManager = modalDialogManager;
+        mVersioningMessageController = versioningMessageController;
+    }
+
+    private void show() {
+        mMessageDispatcher.enqueueWindowScopedMessage(buildModel(), /* highPriority= */ false);
+    }
+
+    private PropertyModel buildModel() {
+        @MessageIdentifier int identifier = MessageIdentifier.UPDATE_CHROME_FOR_TAB_GROUP_SHARE;
+        String title =
+                mContext.getString(
+                        R.string
+                                .collaboration_shared_tab_groups_panel_out_of_date_message_cell_text);
+        String buttonText = mContext.getString(R.string.update);
+        Drawable icon = ContextCompat.getDrawable(mContext, R.drawable.ic_features_24dp);
+
+        PropertyModel.Builder builder =
+                new PropertyModel.Builder(ALL_KEYS)
+                        .with(MESSAGE_IDENTIFIER, identifier)
+                        .with(TITLE, title)
+                        .with(PRIMARY_BUTTON_TEXT, buttonText)
+                        .with(ICON, icon)
+                        .with(ON_PRIMARY_ACTION, this::onPrimary)
+                        .with(ON_FULLY_VISIBLE, this::onVisibleChange);
+        return builder.build();
+    }
+
+    private @PrimaryActionClickBehavior int onPrimary() {
+        VersioningModalDialog.show(mContext, mModalDialogManager);
+        return PrimaryActionClickBehavior.DISMISS_IMMEDIATELY;
+    }
+
+    private void onVisibleChange(Boolean fullyVisible) {
+        assert fullyVisible != null;
+        if (fullyVisible) {
+            mVersioningMessageController.onMessageUiShown(
+                    MessageType.VERSION_OUT_OF_DATE_INSTANT_MESSAGE);
+        }
     }
 }
