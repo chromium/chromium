069297daf7e3df34fad55e024e7a9ff347337cb6
Tom Sepez <tsepez@chromium.org>
2025-07-07 09:58:41 -0700

    Convert rewrite_directory.sh bash script to python.
    Bot generated file followed by minor tweaking.
    
    Change-Id: I7b9d9411acb44bc43fe2f40a6b8d6fa646482605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697354
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483223}

---
+++ tools/clang/unsafe_pragma_rewriter/rewrite_directory.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python3
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Script to use clang complier errors to enclose UNSAFE_TODO() regions.
+
+import argparse
+import os
+import re
+import subprocess
+import sys
+import tempfile
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      description="Rewrite UNSAFE_TODO regions using clang compiler errors.",
+      formatter_class=argparse.RawDescriptionHelpFormatter)
+
+  parser.add_argument("-C",
+                      dest="build_dir",
+                      default="out/Debug",
+                      help="Specify the build directory, defaults to out/Debug")
+  parser.add_argument("-t",
+                      dest="targets",
+                      action="append",
+                      default=[],
+                      help="Build target besides chrome (multiple times OK)")
+  parser.add_argument("-v",
+                      dest="verbose",
+                      action="store_true",
+                      help="Enable verbose logging")
+  parser.add_argument("directory",
+                      help="Directory under src/ to checkout and modify.")
+
+  args = parser.parse_args()
+  build_dir = args.build_dir
+  directory = args.directory
+  targets = ["chrome"] + args.targets
+  verbose = args.verbose
+
+  print("Checking GN build arg configuration ...")
+  try:
+    diag_cmd = [
+        "gn", "args", "-C", build_dir, "--short",
+        "--list=diagnostics_print_source_range_info"
+    ]
+    diag = subprocess.check_output(diag_cmd, text=True)
+    if "true" not in diag:
+      print("Set GN arg diagnostics_print_source_range_info = true",
+            file=sys.stderr)
+      sys.exit(1)
+
+    warn_cmd = [
+        "gn", "args", "-C", build_dir, "--short",
+        "--list=treat_warnings_as_errors"
+    ]
+    warn = subprocess.check_output(warn_cmd, text=True)
+    if "false" not in warn:
+      print("Set GN arg treat_warnings_as_errors = false", file=sys.stderr)
+      sys.exit(1)
+
+  except subprocess.CalledProcessError as e:
+    print(f"Error checking GN args: {e.stderr}", file=sys.stderr)
+    sys.exit(1)
+
+  tmpdir = tempfile.mkdtemp(None, "unsafe_pragma_rewriter.")
+  print(f"Temporary files will be written to {tmpdir}\n")
+
+  grep_cmd = ["git", "grep", "-l", "^#pragma allow_unsafe_", directory]
+  grep = subprocess.check_output(grep_cmd, text=True).strip()
+  grep_lines = grep.splitlines() if grep else []
+  source_files = [x for x in grep_lines if re.match(r".*\.cc$", x)]
+  if not source_files:
+    print("No files met criteria")
+    sys.exit(1)
+
+  if verbose:
+    print("Files containing unsafe pragmas:")
+    print("\n".join(source_files), "\n")
+
+  iffy_cmd = ["grep", "-c", "^#if"] + source_files
+  iffy = subprocess.check_output(iffy_cmd, text=True).strip()
+  iffy_lines = iffy.splitlines() if iffy else []
+  iffy_files = [x.split(":")[0] for x in iffy_lines if x.split(":")[1] != "1"]
+  if iffy_files:
+    if verbose:
+      print("Skipping conditionally-compiled files:")
+      print("\n".join(iffy_files), "\n")
+    source_files = [x for x in source_files if not x in set(iffy_files)]
+
+  if not source_files:
+    print("No remaining files")
+    sys.exit(1)
+
+  if verbose:
+    print("Remaing files:")
+    print("\n".join(source_files), "\n")
+
+  subprocess.run(
+      ["tools/clang/unsafe_pragma_rewriter/remove_unsafe_pragma.py"] +
+      source_files,
+      check=True)
+
+  print("Compile to find unsafe errors ...")
+  buildlog0 = os.path.join(tmpdir, "buildlog0")
+  with open(buildlog0, "w") as f_log:
+    subprocess.run(["autoninja", "-k", "1000", "-C", build_dir, "-v"] + targets,
+                   stdout=f_log,
+                   stderr=subprocess.STDOUT)
+
+  with open(buildlog0) as f_in:
+    compiled = subprocess.check_output(
+        ["tools/clang/unsafe_pragma_rewriter/extract_sources.py"],
+        stdin=f_in,
+        text=True).strip()
+    compiled_files = compiled.splitlines() if compiled else []
+
+  if not compiled_files:
+    print("No modified files were compiled")
+    sys.exit(1)
+
+  if verbose:
+    print("Set of files compiled")
+    print("\n".join(compiled_files), "\n")
+
+  source_files = [x for x in source_files if x in set(compiled_files)]
+  if verbose:
+    print("Set of modified files compiled")
+    print("\n".join(compiled_files), "\n")
+
+  with open(buildlog0) as f_in:
+    fail = subprocess.check_output(
+        ["tools/clang/unsafe_pragma_rewriter/extract_failures.py"],
+        stdin=f_in,
+        text=True).strip()
+    fail_files = fail.splitlines() if fail else []
+
+  if verbose and fail_files:
+    print("Set of files with detected warnings")
+    print("\n".join(fail_files), "\n")
+
+  print("Resetting to clean state ...")
+  subprocess.run(["git", "checkout", "--", directory], check=True)
+  subprocess.run(
+      ["tools/clang/unsafe_pragma_rewriter/remove_unsafe_pragma.py"] +
+      source_files,
+      check=True)
+
+  print("Adding UNSAFE_TODO() ...")
+  with open(buildlog0) as f_in:
+    subprocess.run(["tools/clang/unsafe_pragma_rewriter/fix_unsafe.py"],
+                   stdin=f_in,
+                   check=True)
+
+  if source_files:
+    needs_header = subprocess.check_output(
+        ["git", "grep", "-l", "UNSAFE_TODO"] + source_files, text=True).strip()
+    needs_header_files = needs_header.splitlines() if needs_header else []
+    if needs_header_files:
+      subprocess.run(
+          ["tools/add_header.py", "--header", '"base/compiler_specific.h"'] +
+          needs_header_files,
+          check=True)
+
+  for i in range(1, 5):
+    print(f"Compile to find bad rewrites (Pass {i}) ...")
+    buildlog_i = os.path.join(tmpdir, f"buildlog{i}")
+    with open(buildlog_i, "w") as f_log:
+      subprocess.run(["autoninja", "-k", "1000", "-C", build_dir] + targets,
+                     stdout=f_log,
+                     stderr=subprocess.STDOUT)
+
+    with open(buildlog_i) as f_in:
+      fail = subprocess.check_output(
+          ["tools/clang/unsafe_pragma_rewriter/extract_failures.py"],
+          stdin=f_in,
+          text=True).strip()
+      failures = fail.splitlines() if fail else []
+
+    if not failures:
+      break
+
+    if verbose:
+      print("Failed to compile, reverting:")
+      print("\n".join(failures), "\n")
+
+    for failure in failures:
+      subprocess.run(["git", "checkout", "--", failure], check=True)
+
+  print("Formatting changes")
+  subprocess.run(["git", "cl", "format"], check=True)
+
+  print("Finished.")
+
+
+if __name__ == "__main__":
+  main()
