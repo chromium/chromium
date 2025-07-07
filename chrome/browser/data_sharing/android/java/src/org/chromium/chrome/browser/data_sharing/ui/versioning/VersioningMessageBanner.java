dc8df1384e95d72e8b6ad07aac7a0e784cda03a9
Sky Malice <skym@chromium.org>
2025-07-07 12:04:14 -0700

    Wait for TGSS init before calling #shouldShowMessageUi().
    (cherry picked from commit 79b8da9da3ab11291104e7943408ee85967070ab)
    
    Bug: 428283086, 428753561
    Change-Id: I9107427343510472e12cf9c466fe46a530caacbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6691099
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1480685}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6697118
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/branch-heads/7258@{#861}
    Cr-Branched-From: f600d0656fd5b5fe4a82981f533d31ed6939e2e4-refs/heads/main@{#1477651}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/ui/versioning/VersioningMessageBanner.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/ui/versioning/VersioningMessageBanner.java
@@ -57,15 +57,17 @@ public class VersioningMessageBanner {
 
         VersioningMessageController versioningMessageController =
                 tabGroupSyncService.getVersioningMessageController();
-        if (versioningMessageController.shouldShowMessageUi(
-                MessageType.VERSION_OUT_OF_DATE_INSTANT_MESSAGE)) {
-            new VersioningMessageBanner(
-                            context,
-                            messageDispatcher,
-                            modalDialogManager,
-                            versioningMessageController)
-                    .show();
-        }
+        versioningMessageController.shouldShowMessageUiAsync(
+                MessageType.VERSION_OUT_OF_DATE_INSTANT_MESSAGE,
+                (Boolean shouldShow) -> {
+                    if (!Boolean.TRUE.equals(shouldShow)) return;
+                    new VersioningMessageBanner(
+                                    context,
+                                    messageDispatcher,
+                                    modalDialogManager,
+                                    versioningMessageController)
+                            .show();
+                });
     }
 
     private final Context mContext;
