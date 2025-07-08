c9d36c7ec9addfa0cf5540d5738ac695173cf5ed
Tom Anderson <thomasanderson@chromium.org>
2025-07-08 09:27:54 -0700

    [X11] Select touch events for all devices
    After [1], events were only selected for master devices. This works for
    keyboard, mouse, and focus events, but touch events have an issue for
    some reason.  This CL restores selecting touch events for all devices.
    
    This fix is speculative because:
    1. I'm not able to reproduce the issue to verify the fix
    2. The regression range for touch input breakage is large, so I'm not
       fully sure the suspected culprit is correct.
    
    [1] https://crrev.com/1463883
    
    Change-Id: I3de198d7fb9b741eb541df013e42788658b83b42
    Bug: 428153664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6711673
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483820}

--- ui/events/devices/x11/touch_factory_x11.cc
+++ ui/events/devices/x11/touch_factory_x11.cc
@@ -215,10 +215,6 @@ void TouchFactory::SetupXI2ForXWindow(x11::Window window) {
   SetXinputMask(mask_data, x11::Input::CrossingEvent::FocusIn);
   SetXinputMask(mask_data, x11::Input::CrossingEvent::FocusOut);
 
-  SetXinputMask(mask_data, x11::Input::DeviceEvent::TouchBegin);
-  SetXinputMask(mask_data, x11::Input::DeviceEvent::TouchUpdate);
-  SetXinputMask(mask_data, x11::Input::DeviceEvent::TouchEnd);
-
   SetXinputMask(mask_data, x11::Input::DeviceEvent::ButtonPress);
   SetXinputMask(mask_data, x11::Input::DeviceEvent::ButtonRelease);
   SetXinputMask(mask_data, x11::Input::DeviceEvent::Motion);
@@ -228,7 +224,15 @@ void TouchFactory::SetupXI2ForXWindow(x11::Window window) {
     SetXinputMask(mask_data, x11::Input::DeviceEvent::KeyRelease);
   }
 
-  connection->xinput().XISelectEvents({window, {mask}});
+  x11::Input::EventMask touch_mask{x11::Input::DeviceId::All};
+  touch_mask.mask.push_back({});
+  auto* touch_mask_data = touch_mask.mask.data();
+
+  SetXinputMask(touch_mask_data, x11::Input::DeviceEvent::TouchBegin);
+  SetXinputMask(touch_mask_data, x11::Input::DeviceEvent::TouchUpdate);
+  SetXinputMask(touch_mask_data, x11::Input::DeviceEvent::TouchEnd);
+
+  connection->xinput().XISelectEvents({window, {mask, touch_mask}});
   connection->Flush();
 }
 
