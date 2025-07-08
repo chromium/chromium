cd2886b467ec40bbd274987791df4122d0c6a140
Henrique Nakashima <hnakashima@chromium.org>
2025-07-08 11:25:06 -0700

    [Android] Clean up unused Transition subclasses and methods
    In Transition v2, all Transitions are Trips built with TripBuilder.
    
    Bug: 425379322
    Change-Id: Ifc5c455da2e79e2a72db90901cd46f2e3f131425
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6707773
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483897}

--- base/test/android/javatests/src/org/chromium/base/test/transit/Station.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/Station.java
@@ -6,8 +6,6 @@ package org.chromium.base.test.transit;
 
 import android.app.Activity;
 
-import org.chromium.base.test.transit.Transition.TransitionOptions;
-import org.chromium.base.test.transit.Transition.Trigger;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 
@@ -75,12 +73,6 @@ public abstract class Station<HostActivity extends Activity> extends Conditional
         mFacilities.add(facility);
     }
 
-    private void assertHasFacilities(List<Facility<?>> facilities) {
-        for (var facility : facilities) {
-            assert mFacilities.contains(facility) : this + " does not have " + facility;
-        }
-    }
-
     @Override
     public String getName() {
         return mName;
@@ -116,162 +108,6 @@ public abstract class Station<HostActivity extends Activity> extends Conditional
         if (mActivityElement != null) {
             mActivityElement.requireToBeInNewTask();
         }
-    }
-
-    /**
-     * @deprecated Use {@link TripBuilder#enterFacility(Facility)} instead.
-     */
-    @Deprecated
-    public <F extends Facility<?>> F enterFacilitySync(F facility, @Nullable Trigger trigger) {
-        return enterFacilitySync(facility, TransitionOptions.DEFAULT, trigger);
-    }
-
-    /**
-     * @deprecated Use {@link TripBuilder#enterFacility(Facility)} instead.
-     */
-    @Deprecated
-    public <F extends Facility<?>> F enterFacilitySync(
-            F facility, TransitionOptions options, @Nullable Trigger trigger) {
-        registerFacility(facility);
-        FacilityCheckIn checkIn = new FacilityCheckIn(List.of(facility), options, trigger);
-        checkIn.transitionSync();
-        return facility;
-    }
-
-    /**
-     * @deprecated Use {@link TripBuilder#enterFacilities(Facility[])} instead.
-     */
-    @Deprecated
-    public void enterFacilitiesSync(List<Facility<?>> facilities, @Nullable Trigger trigger) {
-        enterFacilitiesSync(facilities, TransitionOptions.DEFAULT, trigger);
-    }
-
-    /**
-     * @deprecated Use {@link TripBuilder#enterFacilities(Facility[])} instead.
-     */
-    @Deprecated
-    public void enterFacilitiesSync(
-            List<Facility<?>> facilities, TransitionOptions options, @Nullable Trigger trigger) {
-        for (Facility<?> f : facilities) {
-            registerFacility(f);
-        }
-        FacilityCheckIn checkIn = new FacilityCheckIn(facilities, options, trigger);
-        checkIn.transitionSync();
-    }
-
-    /**
-     * @deprecated Use {@link TripBuilder#exitFacility(Facility)} instead.
-     */
-    @Deprecated
-    public void exitFacilitySync(Facility<?> facility, @Nullable Trigger trigger) {
-        exitFacilitySync(facility, TransitionOptions.DEFAULT, trigger);
-    }
-
-    /**
-     * @deprecated Use {@link TripBuilder#exitFacility(Facility)} instead.
-     */
-    @Deprecated
-    public void exitFacilitySync(
-            Facility<?> facility, TransitionOptions options, @Nullable Trigger trigger) {
-        exitFacilitiesSync(List.of(facility), options, trigger);
-    }
-
-    /**
-     * @deprecated Use {@link TripBuilder#exitFacilities(Facility[])} instead.
-     */
-    @Deprecated
-    public void exitFacilitiesSync(List<Facility<?>> facilities, @Nullable Trigger trigger) {
-        exitFacilitiesSync(facilities, TransitionOptions.DEFAULT, trigger);
-    }
-
-    /**
-     * @deprecated Use {@link TripBuilder#exitFacilities(Facility[])} instead.
-     */
-    @Deprecated
-    public void exitFacilitiesSync(
-            List<Facility<?>> facilities, TransitionOptions options, @Nullable Trigger trigger) {
-        assertHasFacilities(facilities);
-        FacilityCheckOut checkOut = new FacilityCheckOut(facilities, options, trigger);
-        checkOut.transitionSync();
-    }
-
-    /**
-     * @deprecated Use {@code tripBuilder.exitFacilityAnd(f).enterFacility(g)| instead.
-     */
-    @Deprecated
-    public <F extends Facility<?>> F swapFacilitySync(
-            Facility<?> facilityToExit, F facilityToEnter, @Nullable Trigger trigger) {
-        return swapFacilitySync(
-                List.of(facilityToExit), facilityToEnter, TransitionOptions.DEFAULT, trigger);
-    }
-
-    /**
-     * @deprecated Use {@code tripBuilder.exitFacilityAnd(f).enterFacility(g)| instead.
-     */
-    @Deprecated
-    public <F extends Facility<?>> F swapFacilitySync(
-            Facility<?> facilityToExit,
-            F facilityToEnter,
-            TransitionOptions options,
-            @Nullable Trigger trigger) {
-        return swapFacilitySync(List.of(facilityToExit), facilityToEnter, options, trigger);
-    }
-
-    /**
-     * @deprecated Use {@code tripBuilder.exitFacilitiesAnd(f, g).enterFacility(h)| instead.
-     */
-    @Deprecated
-    public <F extends Facility<?>> F swapFacilitySync(
-            List<Facility<?>> facilitiesToExit, F facilityToEnter, @Nullable Trigger trigger) {
-        return swapFacilitySync(
-                facilitiesToExit, facilityToEnter, TransitionOptions.DEFAULT, trigger);
-    }
-
-    /**
-     * @deprecated Use {@code tripBuilder.exitFacilitiesAnd(f, g).enterFacility(h)| instead.
-     */
-    @Deprecated
-    public <F extends Facility<?>> F swapFacilitySync(
-            List<Facility<?>> facilitiesToExit,
-            F facilityToEnter,
-            TransitionOptions options,
-            @Nullable Trigger trigger) {
-        assertHasFacilities(facilitiesToExit);
-        registerFacility(facilityToEnter);
-        FacilitySwap swap =
-                new FacilitySwap(facilitiesToExit, List.of(facilityToEnter), options, trigger);
-        swap.transitionSync();
-        return facilityToEnter;
-    }
-
-    /**
-     * @deprecated Use {@code tripBuilder.exitFacilitiesAnd(f, g).enterFacilities(h, i)| instead.
-     */
-    @Deprecated
-    public void swapFacilitiesSync(
-            List<Facility<?>> facilitiesToExit,
-            List<Facility<?>> facilitiesToEnter,
-            @Nullable Trigger trigger) {
-        swapFacilitiesSync(facilitiesToExit, facilitiesToEnter, TransitionOptions.DEFAULT, trigger);
-    }
-
-    /**
-     * Version of {@link #swapFacilitiesSync(List, List, Trigger)} with extra TransitionOptions.
-     *
-     * @deprecated use {@link TripBuilder} instead.
-     */
-    @Deprecated
-    public void swapFacilitiesSync(
-            List<Facility<?>> facilitiesToExit,
-            List<Facility<?>> facilitiesToEnter,
-            TransitionOptions options,
-            @Nullable Trigger trigger) {
-        assertHasFacilities(facilitiesToExit);
-        for (Facility<?> facility : facilitiesToEnter) {
-            registerFacility(facility);
-        }
-        FacilitySwap swap = new FacilitySwap(facilitiesToExit, facilitiesToEnter, options, trigger);
-        swap.transitionSync();
     }
 
     /** Get the activity element associate with this station, if there's any. */
