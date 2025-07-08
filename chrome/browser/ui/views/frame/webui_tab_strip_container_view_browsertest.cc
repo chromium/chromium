ecc2312a4d56da0508af7c9ae83fc87e23f38f21
Ho Cheung <hocheung@chromium.org>
2025-07-07 23:08:45 -0700

    Reland "Refactor WebUITabStripContainerViewTest to InProcessBrowserTest"
    This is a reland of commit 31035741e67d8d073007896abfb814288e58c880
    
    Differences from the original CL:
    
    The test cases have not changed, but we decided to exclude the relevant
    tests from the `linux-blink-web-tests-force-accessibility-rel` tryjobs,
    since the WebUI TabStrip tests are not activated in screen reader mode.
    
    Cq-Include-Trybots: luci.chromium.try:linux-blink-web-tests-force-accessibility-rel
    
    Original change's description:
    > Refactor WebUITabStripContainerViewTest to InProcessBrowserTest
    >
    > 1. Refactored `WebUITabStripContainerViewTest` and related tests from
    > unit test fixture to `InProcessBrowserTest`.
    >
    > 2. Removed all dependencies on `TestWithBrowserView`.
    >
    > 3. Refactored regression test to use `DevToolsWindowTesting` for real
    > DevTools window creation and verification.
    >
    > 4. Some other useful cleanups.
    >
    > Bug: 417766643
    > Change-Id: I7a6626999ac1304376b1aabbba7c63626b6890d7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701930
    > Commit-Queue: Ho Cheung <hocheung@chromium.org>
    > Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1482433}
    
    Bug: 417766643
    Change-Id: I68e85cdb63a91778df51d730fc325ab0085ad7d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6701936
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1483557}

--- chrome/browser/ui/views/frame/webui_tab_strip_container_view_browsertest.cc
+++ chrome/browser/ui/views/frame/webui_tab_strip_container_view_browsertest.cc
@@ -4,66 +4,59 @@
 
 #include "chrome/browser/ui/views/frame/webui_tab_strip_container_view.h"
 
-#include <utility>
-
-#include "base/functional/bind.h"
 #include "base/test/scoped_feature_list.h"
-#include "chrome/browser/extensions/extension_tab_util.h"
+#include "chrome/browser/devtools/devtools_window_testing.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
-#include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/frame/test_with_browser_view.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/browser/ui/webui/tab_strip/tab_strip_ui.h"
-#include "chrome/test/base/test_browser_window.h"
+#include "chrome/test/base/in_process_browser_test.h"
 #include "components/feature_engagement/public/feature_constants.h"
-#include "components/feature_engagement/test/mock_tracker.h"
-#include "content/public/common/drop_data.h"
-#include "testing/gmock/include/gmock/gmock.h"
+#include "content/public/test/browser_test.h"
 #include "ui/base/pointer/touch_ui_controller.h"
 
-class WebUITabStripContainerViewTest : public TestWithBrowserView {
+class WebUITabStripContainerViewTest : public InProcessBrowserTest {
  public:
-  template <typename... Args>
-  explicit WebUITabStripContainerViewTest(Args... args)
-      : TestWithBrowserView(args...) {
+  WebUITabStripContainerViewTest() {
     feature_override_.InitAndEnableFeature(features::kWebUITabStrip);
   }
-
   ~WebUITabStripContainerViewTest() override = default;
+
+ protected:
+  BrowserView* browser_view() {
+    return BrowserView::GetBrowserViewForBrowser(browser());
+  }
 
  private:
   base::test::ScopedFeatureList feature_override_;
   ui::TouchUiController::TouchUiScoperForTesting touch_ui_scoper_{true};
 };
 
-TEST_F(WebUITabStripContainerViewTest, TabStripStartsClosed) {
-  EXPECT_TRUE(WebUITabStripContainerView::UseTouchableTabStrip(
-      browser_view()->browser()));
+IN_PROC_BROWSER_TEST_F(WebUITabStripContainerViewTest, TabStripStartsClosed) {
+  EXPECT_TRUE(WebUITabStripContainerView::UseTouchableTabStrip(browser()));
   ASSERT_NE(nullptr, browser_view()->webui_tab_strip());
   EXPECT_FALSE(browser_view()->webui_tab_strip()->GetVisible());
 }
 
-TEST_F(WebUITabStripContainerViewTest, TouchModeTransition) {
-  EXPECT_TRUE(WebUITabStripContainerView::UseTouchableTabStrip(
-      browser_view()->browser()));
+IN_PROC_BROWSER_TEST_F(WebUITabStripContainerViewTest, TouchModeTransition) {
+  EXPECT_TRUE(WebUITabStripContainerView::UseTouchableTabStrip(browser()));
   EXPECT_NE(nullptr, browser_view()->webui_tab_strip());
   EXPECT_FALSE(browser_view()->GetTabStripVisible());
 
   ui::TouchUiController::TouchUiScoperForTesting disable_touch_mode(false);
-  EXPECT_FALSE(WebUITabStripContainerView::UseTouchableTabStrip(
-      browser_view()->browser()));
+  EXPECT_FALSE(WebUITabStripContainerView::UseTouchableTabStrip(browser()));
   EXPECT_TRUE(browser_view()->GetTabStripVisible());
 
   ui::TouchUiController::TouchUiScoperForTesting reenable_touch_mode(true);
-  EXPECT_TRUE(WebUITabStripContainerView::UseTouchableTabStrip(
-      browser_view()->browser()));
+  EXPECT_TRUE(WebUITabStripContainerView::UseTouchableTabStrip(browser()));
   EXPECT_FALSE(browser_view()->GetTabStripVisible());
   ASSERT_NE(nullptr, browser_view()->webui_tab_strip());
 }
 
-TEST_F(WebUITabStripContainerViewTest, ButtonsPresentInToolbar) {
+IN_PROC_BROWSER_TEST_F(WebUITabStripContainerViewTest,
+                       ButtonsPresentInToolbar) {
   ASSERT_NE(nullptr, browser_view()->toolbar()->new_tab_button_for_testing());
   EXPECT_TRUE(browser_view()->toolbar()->Contains(
       browser_view()->toolbar()->new_tab_button_for_testing()));
@@ -74,7 +67,8 @@ TEST_F(WebUITabStripContainerViewTest, ButtonsPresentInToolbar) {
       browser_view()->webui_tab_strip()->tab_counter()));
 }
 
-TEST_F(WebUITabStripContainerViewTest, CloseContainerOnRendererCrash) {
+IN_PROC_BROWSER_TEST_F(WebUITabStripContainerViewTest,
+                       CloseContainerOnRendererCrash) {
   auto* webui_tab_strip = browser_view()->webui_tab_strip();
   webui_tab_strip->PrimaryMainFrameRenderProcessGone(
       base::TerminationStatus::TERMINATION_STATUS_PROCESS_CRASHED);
@@ -83,22 +77,29 @@ TEST_F(WebUITabStripContainerViewTest, CloseContainerOnRendererCrash) {
   EXPECT_EQ(true, webui_tab_strip->GetVisible());
 }
 
-class WebUITabStripDevToolsTest : public WebUITabStripContainerViewTest {
- public:
-  WebUITabStripDevToolsTest()
-      : WebUITabStripContainerViewTest(Browser::TYPE_DEVTOOLS) {}
-  ~WebUITabStripDevToolsTest() override = default;
-};
+using WebUITabStripDevToolsTest = WebUITabStripContainerViewTest;
 
 // Regression test for crbug.com/1010247, crbug.com/1090208.
-TEST_F(WebUITabStripDevToolsTest, DevToolsWindowHasNoTabStrip) {
-  EXPECT_FALSE(WebUITabStripContainerView::UseTouchableTabStrip(
-      browser_view()->browser()));
-  EXPECT_EQ(nullptr, browser_view()->webui_tab_strip());
+IN_PROC_BROWSER_TEST_F(WebUITabStripDevToolsTest, DevToolsWindowHasNoTabStrip) {
+  DevToolsWindow* devtools_window =
+      DevToolsWindowTesting::OpenDevToolsWindowSync(browser(), false);
+  Browser* dev_tools_browser = nullptr;
+  for (Browser* browser_instance : *BrowserList::GetInstance()) {
+    if (browser_instance->type() == Browser::TYPE_DEVTOOLS) {
+      dev_tools_browser = browser_instance;
+    }
+  }
+  ASSERT_TRUE(dev_tools_browser);
+
+  EXPECT_FALSE(
+      WebUITabStripContainerView::UseTouchableTabStrip(dev_tools_browser));
+  EXPECT_EQ(nullptr, dev_tools_browser->GetBrowserView().webui_tab_strip());
 
   ui::TouchUiController::TouchUiScoperForTesting disable_touch_mode(false);
   ui::TouchUiController::TouchUiScoperForTesting reenable_touch_mode(true);
-  EXPECT_EQ(nullptr, browser_view()->webui_tab_strip());
+  EXPECT_EQ(nullptr, dev_tools_browser->GetBrowserView().webui_tab_strip());
+
+  DevToolsWindowTesting::CloseDevToolsWindowSync(devtools_window);
 }
 
 // TODO(crbug.com/40124617): add coverage of open and close gestures.
