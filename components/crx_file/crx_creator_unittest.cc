e843ebd78acd34fb1d5f95992d64e9f2b1614448
Sorin Jianu <sorin@chromium.org>
2025-07-08 15:49:18 -0700

    crx_file: decluter unit test
    Change-Id: I1062bd950d667a5a06421abb6186924917888866
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6711732
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484048}

--- components/crx_file/crx_creator_unittest.cc
+++ components/crx_file/crx_creator_unittest.cc
@@ -4,14 +4,20 @@
 
 #include "components/crx_file/crx_creator.h"
 
+#include <cstdint>
+#include <string>
+#include <vector>
+
 #include "base/base64.h"
 #include "base/base_paths.h"
+#include "base/containers/to_vector.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/path_service.h"
 #include "components/crx_file/crx_verifier.h"
 #include "crypto/keypair.h"
 #include "crypto/test_support.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -27,10 +33,11 @@ base::FilePath TestFile(const std::string& file) {
 }
 
 // Gzip compression of UTF-8 encoded string `sample_verified_contents`.
-constexpr char kTestCompressedVerifiedContents[] =
-    "\x1f\x8b\x08\x00\xbb\xf7\x1a`\x02\xff+N\xcc-\xc8I\x8d/"
-    "K-\xcaL\xcbLM\x89O\xce\xcf+I\xcd+)"
-    "\x06\x00\x8a\x10\xc9\x01\x18\x00\x00\x00";
+constexpr uint8_t kTestCompressedVerifiedContents[] = {
+    0x1f, 0x8b, 0x8,  0x0,  0xbb, 0xf7, 0x1a, 0x60, 0x2,  0xff, 0x2b, 0x4e,
+    0xcc, 0x2d, 0xc8, 0x49, 0x8d, 0x2f, 0x4b, 0x2d, 0xca, 0x4c, 0xcb, 0x4c,
+    0x4d, 0x89, 0x4f, 0xce, 0xcf, 0x2b, 0x49, 0xcd, 0x2b, 0x29, 0x6,  0x0,
+    0x8a, 0x10, 0xc9, 0x1,  0x18, 0x0,  0x0,  0x0,  0x0};
 
 }  // namespace
 
@@ -50,13 +57,12 @@ TEST_F(CrxCreatorTest, Create) {
             Create(temp_file, TestFile("sample.zip"), private_key));
 
   // Test that the created file can be verified.
-  const std::vector<std::vector<uint8_t>> keys;
-  const std::vector<uint8_t> hash;
   std::string public_key_in_crx;
   EXPECT_EQ(
       VerifierResult::OK_FULL,
-      Verify(temp_file, VerifierFormat::CRX3, keys, hash, &public_key_in_crx,
-             nullptr, /*compressed_verified_contents=*/nullptr));
+      Verify(temp_file, VerifierFormat::CRX3, /*required_key_hashes=*/{},
+             /*required_file_hash=*/{}, &public_key_in_crx, /*crx_id=*/nullptr,
+             /*compressed_verified_contents=*/nullptr));
   EXPECT_EQ(expected_public_key, public_key_in_crx);
 
   // Delete the file.
@@ -71,26 +77,23 @@ TEST_F(CrxCreatorTest, VerifyCrxWithVerifiedContents) {
   // Create a CRX File.
   base::FilePath temp_file;
   EXPECT_TRUE(base::CreateTemporaryFile(&temp_file));
-  std::string test_compressed_verified_contents(
-      kTestCompressedVerifiedContents);
+
   EXPECT_EQ(CreatorResult::OK,
             CreateCrxWithVerifiedContentsInHeader(
                 temp_file, TestFile("sample.zip"), private_key,
-                test_compressed_verified_contents));
+                std::string(std::begin(kTestCompressedVerifiedContents),
+                            std::end(kTestCompressedVerifiedContents))));
 
   // Test that the created file can be verified.
-  const std::vector<std::vector<uint8_t>> keys;
-  const std::vector<uint8_t> hash;
   std::string public_key_in_crx;
   std::vector<uint8_t> compressed_verified_contents;
   EXPECT_EQ(VerifierResult::OK_FULL,
-            Verify(temp_file, VerifierFormat::CRX3, keys, hash,
-                   &public_key_in_crx, nullptr, &compressed_verified_contents));
+            Verify(temp_file, VerifierFormat::CRX3, /*required_key_hashes=*/{},
+                   /*required_file_hash=*/{}, &public_key_in_crx,
+                   /*crx_id=*/nullptr, &compressed_verified_contents));
   EXPECT_EQ(expected_public_key, public_key_in_crx);
-  const std::vector<uint8_t> expected_verified_contents(
-      test_compressed_verified_contents.begin(),
-      test_compressed_verified_contents.end());
-  EXPECT_EQ(compressed_verified_contents, expected_verified_contents);
+  EXPECT_THAT(compressed_verified_contents,
+              testing::ElementsAreArray(kTestCompressedVerifiedContents));
 
   // Delete the file.
   EXPECT_TRUE(base::DeleteFile(temp_file));
