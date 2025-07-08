bc0556576fb93e55dd4e50ecf9bdb3dc74b406b1
Elly <ellyjones@chromium.org>
2025-07-08 13:50:41 -0700

    leveldb: fix DecodeIDBKey fuzzer
    This fuzzer has two major problems:
    1. It uses assert() to check one of its success conditions, but most
       fuzzers are run with asserts disabled so that part of the fuzzer does
       nothing;
    2. It wrongly assumes that DecodeIDBKey() leaves the input string view
       untouched, but actually it adjusts the input string view to contain
       only the unparsed suffix of the input, so the round-trip condition
       the fuzzer is looking for can never be satisfied
    
    So this fuzzer could never have passed, and was only "passing" because
    its actual success condition was compiled out. Oops.
    
    Fixed: 430201817
    Change-Id: I1cf53a175ed2a90586d5cc341b3fba174cbded45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715858
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1483986}

--- content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkey_fuzzer.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkey_fuzzer.cc
@@ -2,24 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stddef.h>
-#include <stdint.h>
-
+#include <cstdint>
 #include <string_view>
-#include <tuple>
 
+#include "base/logging.h"
 #include "content/browser/indexed_db/indexed_db_leveldb_coding.h"
 #include "third_party/blink/public/common/indexeddb/indexeddb_key.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  std::string_view key_str_view(reinterpret_cast<const char*>(data), size);
-  if (blink::IndexedDBKey key =
-          content::indexed_db::DecodeIDBKey(&key_str_view);
-      key.IsValid()) {
-    // Ensure that encoding |indexed_db_key| produces the same result.
+  std::string_view input(reinterpret_cast<const char*>(data), size);
+  std::string_view parsed_input(input);
+  blink::IndexedDBKey key = content::indexed_db::DecodeIDBKey(&parsed_input);
+
+  // If any prefix of the input decoded into a valid key, re-encode it, then
+  // ensure that the encoding matches the original prefix that got decoded.
+  if (key.IsValid()) {
     std::string result;
     content::indexed_db::EncodeIDBKey(key, &result);
-    assert(std::string_view(result) == key_str_view);
+    // DecodeIDBKey() leaves the unparsed suffix in parsed_input, so strip that
+    // much off the end of input before comparing.
+    input.remove_suffix(parsed_input.size());
+    CHECK_EQ(std::string_view(result), input);
   }
 
   return 0;
