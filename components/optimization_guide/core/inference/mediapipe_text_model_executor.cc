f905da4e3a20a3922808155c1efe14dc25e407c4
Christine Hollingsworth <christinesm@chromium.org>
2025-07-09 12:04:52 -0700

    [Code Health] Spanification of mediapipe_text_model_executor.cc
    Replace usage of std::unique_ptr<T[]> with base::HeapArray<T>.
    
    Bug: 40285824
    Change-Id: Ib504bb5782bdf1b9ccfa63338cb88d9e262c5eaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719168
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484518}

--- components/optimization_guide/core/inference/mediapipe_text_model_executor.cc
+++ components/optimization_guide/core/inference/mediapipe_text_model_executor.cc
@@ -7,6 +7,8 @@
 #include <memory>
 #include <string>
 
+#include "base/containers/heap_array.h"
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "components/optimization_guide/core/tflite_op_resolver.h"
 
@@ -54,11 +56,17 @@ std::optional<std::vector<Category>> MediapipeTextModelExecutor::Execute(
 base::expected<std::unique_ptr<TextClassifier>, ExecutionStatus>
 MediapipeTextModelExecutor::BuildModelExecutionTask(base::File& model_file) {
   const int buffer_size = model_file.GetLength();
-  std::unique_ptr<char[]> buffer(new char[buffer_size]);
-  // SAFETY: buffer_size is the size of the allocation
-  size_t read_size =
-      UNSAFE_BUFFERS(model_file.Read(0, buffer.get(), buffer_size));
-  if (read_size != static_cast<size_t>(buffer_size)) {
+
+  // Handle potential error from `GetLength()`.
+  if (buffer_size < 0) {
+    LOG(ERROR) << "Failed to read model file";
+    return base::unexpected(ExecutionStatus::kErrorModelFileNotValid);
+  }
+
+  base::HeapArray<char> buffer = base::HeapArray<char>::Uninit(buffer_size);
+  base::span<char> buffer_span = buffer.span();
+
+  if (!model_file.ReadAndCheck(0, base::as_writable_bytes(buffer_span))) {
     LOG(ERROR) << "Failed to read model file";
     return base::unexpected(ExecutionStatus::kErrorModelFileNotValid);
   }
@@ -71,10 +79,8 @@ MediapipeTextModelExecutor::BuildModelExecutionTask(base::File& model_file) {
               // TODO(https://crbug.com/413070228): Upstream changes to support
               // passing a file descriptor / file handle rather than buffer, so
               // that mediapipe can memory map the file contents.
-              // SAFETY: buffer_size is the size of the allocation
-              .model_asset_buffer =
-                  UNSAFE_BUFFERS(std::make_unique<std::string>(
-                      buffer.get(), buffer.get() + buffer_size)),
+              .model_asset_buffer = std::make_unique<std::string>(
+                  buffer_span.data(), buffer_span.size()),
               .op_resolver = std::make_unique<TFLiteOpResolver>(),
           },
   };
