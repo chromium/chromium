c1e9f30484c419e38b79f6ffb33dbaef574f70f8
Evan Stade <evanstade@microsoft.com>
2025-07-09 11:45:18 -0700

    IDB: fix decodieidbkeypath fuzzer
    assert often does nothing; change to CHECK_EQ
    
    Bug: 430201817
    Change-Id: I6a905498c4659a351a2433ae273034819b182a00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6713927
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1484499}

--- content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkeypath_fuzzer.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkeypath_fuzzer.cc
@@ -8,18 +8,19 @@
 #include <string_view>
 #include <tuple>
 
+#include "base/check.h"
 #include "content/browser/indexed_db/indexed_db_leveldb_coding.h"
 #include "third_party/blink/public/common/indexeddb/indexeddb_key_path.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   std::string_view key_path_str_view(reinterpret_cast<const char*>(data), size);
   blink::IndexedDBKeyPath indexed_db_key_path;
-  std::ignore = content::indexed_db::DecodeIDBKeyPath(&key_path_str_view,
-                                                      &indexed_db_key_path);
-
-  // Ensure that encoding |indexed_db_key_path| produces the same result.
-  std::string result;
-  content::indexed_db::EncodeIDBKeyPath(indexed_db_key_path, &result);
-  assert(std::string_view(result) == key_path_str_view);
+  if (content::indexed_db::DecodeIDBKeyPath(&key_path_str_view,
+                                            &indexed_db_key_path)) {
+    // Ensure that encoding |indexed_db_key_path| produces the same result.
+    std::string result;
+    content::indexed_db::EncodeIDBKeyPath(indexed_db_key_path, &result);
+    CHECK_EQ(std::string_view(result), key_path_str_view);
+  }
   return 0;
 }
