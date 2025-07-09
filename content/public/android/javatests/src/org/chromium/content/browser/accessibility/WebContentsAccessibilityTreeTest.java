549688143f539b3a8cdb17387e9ea7ce7f3a61bd
Camillia Smith Barnes <cammie@chromium.org>
2025-07-09 14:05:32 -0700

    [Gardener] Remove unused import in java test
    Remove unused import: org.chromium.base.test.util.DisableIf in
    WebContentsAccessibilityTreeTest.java to fix failing presubmits.
    
    Bug: 430626264
    Change-Id: Ia35bddafb0616d14ea5928b289f6c054afca77f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719154
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: Cammie Smith Barnes <cammie@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Owners-Override: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484589}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
@@ -20,7 +20,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
