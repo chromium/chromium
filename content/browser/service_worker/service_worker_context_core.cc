ea192e35447907c840ffc840c091e8f16a622cf5
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2025-07-10 10:36:23 -0700

    Revert CL for the high confidence crash culprit for http://crash/909012de2c0db2e3
    The Suspect CL https://chromium-review.googlesource.com/q/I09bcc6c6abc9dbfe7cb49364579763846a8f5254 is highly likely the cause of the crash. Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Change-Id: Ifa9f6994703fd97e02b71d5cfae8bf93bd46e84c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6720001
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485073}

--- content/browser/service_worker/service_worker_context_core.cc
+++ content/browser/service_worker/service_worker_context_core.cc
@@ -945,7 +945,11 @@ void ServiceWorkerContextCore::RemoveLiveVersion(int64_t id) {
     observer_list_->Notify(FROM_HERE,
                            &ServiceWorkerContextCoreObserver::OnStopped, id);
     for (auto& observer : sync_observer_list_->observers) {
-      observer.OnStopped(id, version->scope());
+      const std::optional<ServiceWorkerRunningInfo> running_info =
+          wrapper_->GetRunningServiceWorkerInfo(id);
+      if (running_info.has_value()) {
+        observer.OnStopped(id, version->scope());
+      }
     }
   }
 
@@ -1014,7 +1018,11 @@ void ServiceWorkerContextCore::DeleteAndStartOver(StatusCallback callback) {
   for (const auto& live_version_itr : live_versions_) {
     ServiceWorkerVersion* live_version = live_version_itr.second;
     for (auto& observer : sync_observer_list_->observers) {
-      observer.OnStopped(live_version->version_id(), live_version->scope());
+      const std::optional<ServiceWorkerRunningInfo> running_info =
+          wrapper_->GetRunningServiceWorkerInfo(live_version->version_id());
+      if (running_info.has_value()) {
+        observer.OnStopped(live_version->version_id(), live_version->scope());
+      }
     }
   }
 
@@ -1227,7 +1235,11 @@ void ServiceWorkerContextCore::OnRunningStateChanged(
                              &ServiceWorkerContextCoreObserver::OnStopped,
                              version->version_id());
       for (auto& observer : sync_observer_list_->observers) {
-        observer.OnStopped(version->version_id(), version->scope());
+        const std::optional<ServiceWorkerRunningInfo> running_info =
+            wrapper_->GetRunningServiceWorkerInfo(version->version_id());
+        if (running_info.has_value()) {
+          observer.OnStopped(version->version_id(), version->scope());
+        }
       }
       break;
     case blink::EmbeddedWorkerStatus::kStarting:
@@ -1247,7 +1259,11 @@ void ServiceWorkerContextCore::OnRunningStateChanged(
                              &ServiceWorkerContextCoreObserver::OnStopping,
                              version->version_id());
       for (auto& observer : sync_observer_list_->observers) {
-        observer.OnStopping(version->version_id(), version->scope());
+        const std::optional<ServiceWorkerRunningInfo> running_info =
+            wrapper_->GetRunningServiceWorkerInfo(version->version_id());
+        if (running_info.has_value()) {
+          observer.OnStopping(version->version_id(), version->scope());
+        }
       }
       break;
   }
