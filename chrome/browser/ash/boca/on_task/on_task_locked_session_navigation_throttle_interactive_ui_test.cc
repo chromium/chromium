3ca6b79aa28dbd780b3e4f4d071d041ade7bb9e1
Vignesh Shenvi <vshenvi@google.com>
2025-07-09 19:52:07 -0700

    Limit OnTask special case redirects to 1LD nav restriction
    This change limits redirects triggered as separate navigation requests
    to 1LD nav restriction. This is to prevent users from circumventing nav
    restrictions for other restriction types.
    
    Bug: b:429257570
    Change-Id: I40a29ddd293e596abf0863f69ebde3c9d6e8547f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6720236
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1484726}

--- chrome/browser/ash/boca/on_task/on_task_locked_session_navigation_throttle_interactive_ui_test.cc
+++ chrome/browser/ash/boca/on_task/on_task_locked_session_navigation_throttle_interactive_ui_test.cc
@@ -1397,6 +1397,48 @@ IN_PROC_BROWSER_TEST_F(
 
 IN_PROC_BROWSER_TEST_F(
     OnTaskLockedSessionNavigationThrottleWorkspaceNavigationInteractiveUITest,
+    BlockRedirectsCreatedAsSeparateNavigationRequests) {
+  // Launch OnTask SWA.
+  base::test::TestFuture<bool> launch_future;
+  system_web_app_manager()->LaunchSystemWebAppAsync(
+      launch_future.GetCallback());
+  ASSERT_TRUE(launch_future.Get());
+  Browser* const boca_app_browser = FindBocaSystemWebAppBrowser();
+  ASSERT_THAT(boca_app_browser, NotNull());
+  ASSERT_TRUE(boca_app_browser->IsLockedForOnTask());
+
+  // Set up window tracker to track the app window.
+  const SessionID window_id = boca_app_browser->session_id();
+  ASSERT_TRUE(window_id.is_valid());
+  system_web_app_manager()->SetWindowTrackerForSystemWebAppWindow(
+      window_id, /*observers=*/{});
+
+  // Spawn tab for testing purposes.
+  CreateBackgroundTabAndWait(
+      window_id, embedded_test_server()->GetURL(kTabUrl1Host, "/"),
+      ::boca::LockedNavigationOptions::LIMITED_NAVIGATION);
+  auto* const tab_strip_model = boca_app_browser->tab_strip_model();
+  ASSERT_EQ(tab_strip_model->count(), 2);
+  tab_strip_model->ActivateTabAt(1);
+  WaitForUrlBlocklistUpdate();
+
+  // Simulate URL redirect as a separate navigation request.
+  const GURL google_redirect_url = embedded_test_server()->GetURL(
+      kTabGoogleHost, "/url?q=https://www.foo.com");
+  ASSERT_TRUE(
+      ui_test_utils::NavigateToURL(boca_app_browser, google_redirect_url));
+  EXPECT_EQ(tab_strip_model->GetActiveWebContents()->GetLastCommittedURL(),
+            google_redirect_url);
+
+  const GURL url_1 = embedded_test_server()->GetURL(kTabUrl1Host, "/");
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(boca_app_browser, url_1));
+  EXPECT_NE(tab_strip_model->GetActiveWebContents()->GetLastCommittedURL(),
+            url_1);
+  VerifyUrlBlockedToastShown(/*toast_was_shown=*/true);
+}
+
+IN_PROC_BROWSER_TEST_F(
+    OnTaskLockedSessionNavigationThrottleWorkspaceNavigationInteractiveUITest,
     AllowOneLevelDeepNavigationWithGoogleCaptcha) {
   // Launch OnTask SWA.
   base::test::TestFuture<bool> launch_future;
