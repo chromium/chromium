5b9cec9db9660b028e49989a83991a16c0e33c0c
Andrew Grieve <agrieve@chromium.org>
2025-07-09 18:35:15 -0700

    Revert "Manual Roll android_deps from eZs-lpr1SNe_wJ5UB... to Y1zpcB2HOm4J1EH6d..."
    This reverts commit 5f57ed73ad2f804361ec4c1f334ad71f9c2a4eba.
    
    Reason for revert: Broke internal builds. E.g.:
    https://ci.chromium.org/ui/p/chrome/builders/try/test-emulator/1193/overview
    
    Bug: 430098881
    Original change's description:
    > Manual Roll android_deps from eZs-lpr1SNe_wJ5UB... to Y1zpcB2HOm4J1EH6d...
    >
    > * Removed unneeded entries from non-autorolled build.gradle
    > * Ran fetch_all.py to resolve duplicates
    >
    > Bug: 430098881
    > Change-Id: I12fd691dba018280f4e698455131a8909a9f4538
    > Cq-Include-Trybots: luci.chrome.try:android-internal-binary-size;luci.chrome.try:android-internal-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6714008
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Peter Wen <wnwen@chromium.org>
    > Reviewed-by: Peter Wen <wnwen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1484475}
    
    Bug: 430098881
    Cq-Include-Trybots: luci.chrome.try:android-internal-binary-size;luci.chrome.try:android-internal-rel
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I76d342aa0789b302c32a112300e9d233a3db11c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6718974
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1484715}

---
+++ third_party/android_deps/libs/com_google_firebase_firebase_messaging/3pp/fetch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Copyright 2021 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This is generated, do not edit. Update BuildConfigGenerator.groovy and
+# 3ppFetch.template instead.
+
+import pathlib
+import sys
+
+_3PP_DIR = pathlib.Path(__file__).resolve().parent
+sys.path.insert(0, str(_3PP_DIR.parents[2]))
+import fetch_common
+
+_REPO_URL = 'https://dl.google.com/dl/android/maven2'
+SPEC = fetch_common.Spec(repo_url=_REPO_URL,
+                         group_name='com/google/firebase',
+                         module_name='firebase-messaging',
+                         file_ext='aar',
+                         patch_version='cr1',
+                         version_override=None,
+                         version_filter=None)
+
+if __name__ == '__main__':
+    fetch_common.main(SPEC)
