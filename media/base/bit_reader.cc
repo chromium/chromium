dfb3b484016e01fe626e55daabb1476365e327f1
weidongliu <weidongliu@chromium.org>
2025-07-09 22:58:04 -0700

    Merge BitReaderCore and BitReader
    BitReaderCore is only used by BitReader, and there are multiple
    duplicate checks between the two. Some content can be simplified.
    However, in this CL, only simple moves are made. More optimizations will
    appear in subsequent CLs.
    
    Change-Id: I1128c4424b92e9c3a21829774d1acc4e7e592498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719531
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1484781}

--- media/base/bit_reader.cc
+++ media/base/bit_reader.cc
@@ -9,18 +9,36 @@
 
 #include "media/base/bit_reader.h"
 
+#include "base/check_op.h"
+#include "base/containers/span.h"
+#include "base/numerics/byte_conversions.h"
+#include "base/numerics/safe_conversions.h"
+
 namespace media {
 
+namespace {
+constexpr int kRegWidthInBits = sizeof(uint64_t) * 8;
+}
+
 BitReader::BitReader(const uint8_t* data, int size)
-    : initial_size_(size),
-      data_(data),
-      bytes_left_(size),
-      bit_reader_core_(this) {
+    : initial_size_(size), data_(data), bytes_left_(size) {
   DCHECK(data != nullptr);
   DCHECK_GE(size, 0);
 }
 
 BitReader::~BitReader() = default;
+
+bool BitReader::ReadFlag(bool* flag) {
+  if (nbits_ == 0 && !Refill(1)) {
+    return false;
+  }
+
+  *flag = (reg_ & (UINT64_C(1) << (kRegWidthInBits - 1))) != 0;
+  reg_ <<= 1;
+  nbits_--;
+  bits_read_++;
+  return true;
+}
 
 bool BitReader::ReadString(int num_bits, std::string* str) {
   DCHECK_EQ(num_bits % 8, 0);
@@ -34,6 +52,162 @@ bool BitReader::ReadString(int num_bits, std::string* str) {
       return false;
   }
   return true;
+}
+
+int BitReader::PeekBitsMsbAligned(int num_bits, uint64_t* out) {
+  // Try to have at least |num_bits| in the bit register.
+  if (nbits_ < num_bits) {
+    Refill(num_bits);
+  }
+
+  *out = reg_;
+  return nbits_;
+}
+
+bool BitReader::SkipBitsSmall(int num_bits) {
+  DCHECK_GE(num_bits, 0);
+  uint64_t dummy;
+  while (num_bits >= kRegWidthInBits) {
+    if (!ReadBitsInternal(kRegWidthInBits, &dummy)) {
+      return false;
+    }
+    num_bits -= kRegWidthInBits;
+  }
+  return ReadBitsInternal(num_bits, &dummy);
+}
+
+bool BitReader::SkipBits(int num_bits) {
+  DCHECK_GE(num_bits, 0);
+
+  const int remaining_bits = nbits_ + nbits_next_;
+  if (remaining_bits >= num_bits) {
+    return SkipBitsSmall(num_bits);
+  }
+
+  // Skip first the remaining available bits.
+  num_bits -= remaining_bits;
+  bits_read_ += remaining_bits;
+  nbits_ = 0;
+  reg_ = 0;
+  nbits_next_ = 0;
+  reg_next_ = 0;
+
+  // Next, skip an integer number of bytes.
+  const int nbytes = num_bits / 8;
+  if (nbytes > 0) {
+    const uint8_t* byte_stream_window;
+    const int window_size = GetBytes(nbytes, &byte_stream_window);
+    DCHECK_GE(window_size, 0);
+    DCHECK_LE(window_size, nbytes);
+    if (window_size < nbytes) {
+      // Note that some bytes were consumed.
+      bits_read_ += 8 * window_size;
+      return false;
+    }
+    num_bits -= 8 * nbytes;
+    bits_read_ += 8 * nbytes;
+  }
+
+  // Skip the remaining bits.
+  return SkipBitsSmall(num_bits);
+}
+
+int BitReader::bits_read() const {
+  return bits_read_;
+}
+
+bool BitReader::ReadBitsInternal(int num_bits, uint64_t* out) {
+  DCHECK_GE(num_bits, 0);
+
+  if (num_bits == 0) {
+    *out = 0;
+    return true;
+  }
+
+  if (num_bits > nbits_ && !Refill(num_bits)) {
+    // Any subsequent ReadBits should fail:
+    // empty the current bit register for that purpose.
+    nbits_ = 0;
+    reg_ = 0;
+    *out = 0;
+    return false;
+  }
+
+  bits_read_ += num_bits;
+
+  if (num_bits == kRegWidthInBits) {
+    // Special case needed since for example for a 64 bit integer "a"
+    // "a << 64" is not defined by the C/C++ standard.
+    *out = reg_;
+    reg_ = 0;
+    nbits_ = 0;
+    return true;
+  }
+
+  *out = reg_ >> (kRegWidthInBits - num_bits);
+  reg_ <<= num_bits;
+  nbits_ -= num_bits;
+  return true;
+}
+
+bool BitReader::Refill(int min_nbits) {
+  DCHECK_LE(min_nbits, kRegWidthInBits);
+
+  // Transfer from the next to the current register.
+  RefillCurrentRegister();
+  if (min_nbits <= nbits_) {
+    return true;
+  }
+  DCHECK_EQ(nbits_next_, 0);
+  DCHECK_EQ(reg_next_, 0u);
+
+  // Max number of bytes to refill.
+  int max_nbytes = sizeof(reg_next_);
+
+  // Refill.
+  const uint8_t* byte_stream_window_ptr;
+  const auto window_size =
+      base::checked_cast<size_t>(GetBytes(max_nbytes, &byte_stream_window_ptr));
+  auto byte_stream_window =
+      // TODO(crbug.com/40284755): GetBytes() should return a span.
+      UNSAFE_TODO(base::span(byte_stream_window_ptr, window_size));
+  if (byte_stream_window.empty()) {
+    return false;
+  }
+
+  // Pad the window to 8 big-endian bytes to fill `reg_next_`. `reg_next_` is
+  // read from the MSB, so the new bytes are written to the front in big-endian.
+  std::array<uint8_t, 8u> bytes = {};
+  base::span(bytes).copy_prefix_from(byte_stream_window);
+  reg_next_ = base::U64FromBigEndian(bytes);
+  nbits_next_ = base::checked_cast<int>(byte_stream_window.size() * 8u);
+
+  // Transfer from the next to the current register.
+  RefillCurrentRegister();
+
+  return (nbits_ >= min_nbits);
+}
+
+void BitReader::RefillCurrentRegister() {
+  // No refill possible if the destination register is full
+  // or the source register is empty.
+  if (nbits_ == kRegWidthInBits || nbits_next_ == 0) {
+    return;
+  }
+
+  reg_ |= (reg_next_ >> nbits_);
+
+  int free_nbits = kRegWidthInBits - nbits_;
+  if (free_nbits >= nbits_next_) {
+    nbits_ += nbits_next_;
+    reg_next_ = 0;
+    nbits_next_ = 0;
+    return;
+  }
+
+  nbits_ += free_nbits;
+  reg_next_ <<= free_nbits;
+  nbits_next_ -= free_nbits;
 }
 
 int BitReader::GetBytes(int max_nbytes, const uint8_t** out) {
--- media/base/bit_reader.h
+++ media/base/bit_reader.h
@@ -9,14 +9,14 @@
 
 #include <string>
 
+#include "base/check_op.h"
 #include "base/compiler_specific.h"
 #include "base/memory/raw_ptr.h"
-#include "media/base/bit_reader_core.h"
 #include "media/base/media_export.h"
 
 namespace media {
 
-class MEDIA_EXPORT BitReader : private BitReaderCore::ByteStreamProvider {
+class MEDIA_EXPORT BitReader {
  public:
   // Initialize the reader to start reading at |data|, |size| being size
   // of |data| in bytes.
@@ -25,36 +25,90 @@ class MEDIA_EXPORT BitReader : private BitReaderCore::ByteStreamProvider {
   BitReader(const BitReader&) = delete;
   BitReader& operator=(const BitReader&) = delete;
 
-  ~BitReader() override;
+  virtual ~BitReader();
 
+  // Read |num_bits| next bits from stream and return in |*out|, first bit
+  // from the stream starting at |num_bits| position in |*out|,
+  // bits of |*out| whose position is strictly greater than |num_bits|
+  // are all set to zero.
+  // Notes:
+  // - |num_bits| cannot be larger than the bits the type can hold.
+  // - From the above description, passing a signed type in |T| does not
+  //   mean the first bit read from the stream gives the sign of the value.
+  // Return false if the given number of bits cannot be read (not enough
+  // bits in the stream), true otherwise. When return false, the stream will
+  // enter a state where further ReadBits/SkipBits operations will always
+  // return false unless |num_bits| is 0. The type |T| has to be a primitive
+  // integer type.
   template<typename T> bool ReadBits(int num_bits, T* out) {
-    return bit_reader_core_.ReadBits(num_bits, out);
+    DCHECK_LE(num_bits, static_cast<int>(sizeof(T) * 8));
+    uint64_t temp = 0;
+    bool ret = ReadBitsInternal(num_bits, &temp);
+    if (ret) {
+      *out = static_cast<T>(temp);
+    }
+    return ret;
   }
 
-  bool ReadFlag(bool* flag) {
-    return bit_reader_core_.ReadFlag(flag);
+  // Read one bit from the stream and return it as a boolean in |*out|.
+  // Remark: we do not use the template version for reading a bool
+  // since it generates some optimization warnings during compilation
+  // on Windows platforms.
+  bool ReadBits(int num_bits, bool* out) {
+    DCHECK_EQ(num_bits, 1);
+    return ReadFlag(out);
   }
+
+  // Read one bit from the stream and return it as a boolean in |*flag|.
+  bool ReadFlag(bool* flag);
 
   // Read |num_bits| of binary data into |str|. |num_bits| must be a positive
   // multiple of 8. This is not efficient for extracting large strings.
   // If false is returned, |str| may not be valid.
   bool ReadString(int num_bits, std::string* str);
 
-  bool SkipBits(int num_bits) {
-    return bit_reader_core_.SkipBits(num_bits);
-  }
+  // Retrieve some bits without actually consuming them.
+  // Bits returned in |*out| are shifted so the most significant bit contains
+  // the next bit that can be read from the stream.
+  // Return the number of bits actually written in |out|.
+  // Note: |num_bits| is just a suggestion of how many bits the caller
+  // wish to get in |*out| and must be less than 64:
+  // - The number of bits returned can be more than |num_bits|.
+  // - However, it will be strictly less than |num_bits|
+  //   if and only if there are not enough bits left in the stream.
+  int PeekBitsMsbAligned(int num_bits, uint64_t* out);
+
+  // Skip |num_bits| next bits from stream. Return false if the given number of
+  // bits cannot be skipped (not enough bits in the stream), true otherwise.
+  // When return false, the stream will enter a state where further
+  // ReadBits/ReadFlag/SkipBits operations
+  // will always return false unless |num_bits| is 0.
+  bool SkipBits(int num_bits);
 
   int bits_available() const {
     return initial_size_ * 8 - bits_read();
   }
 
-  int bits_read() const {
-    return bit_reader_core_.bits_read();
-  }
+  // Returns the number of bits read so far.
+  int bits_read() const;
 
  private:
-  // BitReaderCore::ByteStreamProvider implementation.
-  int GetBytes(int max_n, const uint8_t** out) override;
+  // This function can skip any number of bits but is more efficient
+  // for small numbers. Return false if the given number of bits cannot be
+  // skipped (not enough bits in the stream), true otherwise.
+  bool SkipBitsSmall(int num_bits);
+
+  // Help function used by ReadBits to avoid inlining the bit reading logic.
+  bool ReadBitsInternal(int num_bits, uint64_t* out);
+
+  // Refill bit registers to have at least |min_nbits| bits available.
+  // Return true if the mininimum bit count condition is met after the refill.
+  bool Refill(int min_nbits);
+
+  // Refill the current bit register from the next bit register.
+  void RefillCurrentRegister();
+
+  int GetBytes(int max_n, const uint8_t** out);
 
   // Total number of bytes that was initially passed to BitReader.
   const int initial_size_;
@@ -65,7 +119,18 @@ class MEDIA_EXPORT BitReader : private BitReaderCore::ByteStreamProvider {
   // Bytes left in the stream.
   int bytes_left_;
 
-  BitReaderCore bit_reader_core_;
+  // Number of bits read so far.
+  int bits_read_ = 0;
+
+  // Number of bits in |reg_| that have not been consumed yet.
+  // Note: bits are consumed from MSB to LSB.
+  int nbits_ = 0;
+  uint64_t reg_ = 0;
+
+  // Number of bits in |reg_next_| that have not been consumed yet.
+  // Note: bits are consumed from MSB to LSB.
+  int nbits_next_ = 0;
+  uint64_t reg_next_ = 0;
 };
 
 }  // namespace media
