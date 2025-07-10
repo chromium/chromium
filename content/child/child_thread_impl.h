46619c237efc878e049e6062b9b692a989b99d9c
Tom Sepez <tsepez@chromium.org>
2025-07-10 10:34:02 -0700

    Remove IPC::MessageRouter.
    It is unused.
    
    -- fix IWYU for IPC::Listener in one place.
    
    Bug: 40621529
    Change-Id: Ie82520c7a09f458487649912107f0f5f3dbd5aa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6719999
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485069}

--- content/child/child_thread_impl.h
+++ content/child/child_thread_impl.h
@@ -25,8 +25,8 @@
 #include "content/public/child/child_thread.h"
 #include "ipc/ipc.mojom.h"
 #include "ipc/ipc_buildflags.h"  // For BUILDFLAG(IPC_MESSAGE_LOG_ENABLED).
+#include "ipc/ipc_listener.h"
 #include "ipc/ipc_platform_file.h"
-#include "ipc/message_router.h"
 #include "mojo/public/cpp/bindings/associated_receiver_set.h"
 #include "mojo/public/cpp/bindings/generic_pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
