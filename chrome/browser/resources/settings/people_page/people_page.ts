chromium/chromium

commit 5238bfdb1418f9587d1e00ab68385bb3940dec7f

[UNO] Create account settings page
This page currently only contains an account card. In future CLs, more
settings to manage account data will be added to this page, as well as
an entry point in chrome://settings/people.
This change is behind the flag `ReplaceSyncPromosWithSignInPromos`.

Mocks are linked in the bug.

Screenshot: https://screenshot.googleplex.com/V9799EhmQTND5nV
Design Doc: https://docs.google.com/document/d/1wODtIP74PJkXXZbSdB5vOQlcHwJ7Ue4gW4kswfKis9o/edit?usp=sharing&resourcekey=0-6re9dwE7rf6qhNT3J4NvVg

Bug: 428159656
Change-Id: I7bfda744100d98146b9b5c68b28f0b01b6ff06f4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6682129
Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
Commit-Queue: Amelie Schneider <amelies@google.com>
Cr-Commit-Position: refs/heads/main@{#1484226}

chrome/browser/resources/settings/people_page/people_page.html
@@ -191,6 +191,13 @@ shown on non-ChromeOS platforms -->
           </settings-manage-profile>
         </settings-subpage>
       </template>
+
+      <template is="dom-if" route-path="/account">
+        <settings-subpage page-title="$i18n{accountPageTitle}">
+          <settings-account-page prefs="{{prefs}}" sync-status="[[syncStatus]]">
+          </settings-account-page>
+        </settings-subpage>
+      </template>
 </if>
     </settings-animated-pages>
 

chrome/browser/resources/settings/route.ts
@@ -186,6 +186,7 @@ function createRoutes(): SettingsRoutes {
     r.SIGN_OUT.isNavigableDialog = true;
     r.IMPORT_DATA = r.PEOPLE.createChild('/importData');
     r.IMPORT_DATA.isNavigableDialog = true;
+    r.ACCOUNT = r.PEOPLE.createChild('/account');
     // </if>
 
     r.SYNC = r.PEOPLE.createChild('/syncSetup');

chrome/test/data/webui/settings/settings_browsertest.cc
@@ -70,6 +70,12 @@ IN_PROC_BROWSER_TEST_F(SettingsTest, A11yPage) {
 }
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)
 
+#if !BUILDFLAG(IS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(SettingsTest, AccountPage) {
+  RunTest("settings/account_page_test.js", "mocha.run()");
+}
+#endif
+
 IN_PROC_BROWSER_TEST_F(SettingsTest, AntiAbusePage) {
   RunTest("settings/anti_abuse_page_test.js", "mocha.run()");
 }

chrome/app/settings_strings.grdp
@@ -3627,6 +3627,9 @@
     <message name="IDS_SETTINGS_CREATE_SHORTCUT_SUBTITLE" desc="Subtitle of create shortcut section on the manage profile page.">
       Create a desktop shortcut on your device to access directly to this profile
     </message>
+    <message name="IDS_SETTINGS_ACCOUNT_PAGE_TITLE" desc="Name of the settings page which manages the signed in account.">
+      Your account
+    </message>
   </if>
 
   <message name="IDS_SETTINGS_SYNC_DISCONNECT_EXPLANATION" desc="The text to display in the Sign out of Chrome dialog to stop syncing for non-managed profiles.">

chrome/app/settings_strings_grdp/IDS_SETTINGS_ACCOUNT_PAGE_TITLE.png.sha1
@@ -1,0 +1,1 @@
+f1d4417aa6f2e10267c05758ad096634eca94a3c

chrome/browser/resources/settings/BUILD.gn
@@ -228,6 +228,7 @@ build_webui("build") {
       "languages_page/spell_check_page.ts",
       "languages_page/translate_page.ts",
       "people_page/import_data_dialog.ts",
+      "people_page/account_page.ts",
       "people_page/manage_profile.ts",
       "people_page/sync_account_control.ts",
       "relaunch_confirmation_dialog.ts",

chrome/browser/resources/settings/lazy_load.ts
@@ -58,8 +58,7 @@ import './site_settings/zoom_levels.js';
 // <if expr="not is_chromeos">
 import './a11y_page/live_caption_section.js';
 import './people_page/import_data_dialog.js';
-// </if>
-// <if expr="not is_chromeos">
+import './people_page/account_page.js';
 import './people_page/manage_profile.js';
 // </if>
 import './people_page/signout_dialog.js';
@@ -168,12 +167,9 @@ export {LanguageSettingsActionType, LanguageSettingsMetricsProxy, LanguageSettin
 export {LanguageHelper, LanguagesModel} from './languages_page/languages_types.js';
 export {SettingsSpellCheckPageElement} from './languages_page/spell_check_page.js';
 export {SettingsTranslatePageElement} from './languages_page/translate_page.js';
-// </if>
-// <if expr="not is_chromeos">
+export {SettingsAccountPageElement} from './people_page/account_page.js';
 export {BrowserProfile, ImportDataBrowserProxy, ImportDataBrowserProxyImpl, ImportDataStatus} from './people_page/import_data_browser_proxy.js';
 export {SettingsImportDataDialogElement} from './people_page/import_data_dialog.js';
-// </if>
-// <if expr="not is_chromeos">
 export {SettingsManageProfileElement} from './people_page/manage_profile.js';
 export {ManageProfileBrowserProxy, ManageProfileBrowserProxyImpl, ProfileShortcutStatus} from './people_page/manage_profile_browser_proxy.js';
 // </if>

chrome/browser/resources/settings/people_page/account_page.html
@@ -1,0 +1,3 @@
+<settings-sync-account-control sync-status="[[syncStatus]]"
+    prefs="{{prefs}}" hide-banner>
+</settings-sync-account-control>

commit 3e1a398b63df5ac4eeadf0e6b2cbc51a81ce6982

[UNO] Exclude account settings subpage without feature flag
https://chromium.googlesource.com/chromium/src/+/5238bfdb1418f9587d1e00ab68385bb3940dec7f
introduced a bug where the sync settings page could not be opened with
the search bar being in use. This was because the subpage does not yet
have an `associated-control`, which caused an assert failure in
`revealParentSection()`.

This is fixed by not including the subpage in the dom without the
feature flag `ReplaceSyncPromosWithSignInPromos` enabled and adding a
`no-search` parameter to the element. In future CLs (before the flag is
enabled), there will be an `associated-control` added to the account
settings subpage through the addition of an entry point in the people
page.

Fixed: 430799297
Change-Id: I195c4aa96d3fbcba6a52056661dc8e36815eaf12
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6722273
Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
Commit-Queue: Amelie Schneider <amelies@google.com>
Cr-Commit-Position: refs/heads/main@{#1485434}

chrome/browser/resources/settings/people_page/people_page.html
@@ -192,11 +192,14 @@ shown on non-ChromeOS platforms -->
         </settings-subpage>
       </template>
 
-      <template is="dom-if" route-path="/account">
-        <settings-subpage page-title="$i18n{accountPageTitle}">
-          <settings-account-page prefs="{{prefs}}" sync-status="[[syncStatus]]">
-          </settings-account-page>
-        </settings-subpage>
+      <template is="dom-if" if="[[shouldShowAccountSettingsPage_]]" restamp>
+        <template is="dom-if" route-path="/account">
+          <settings-subpage page-title="$i18n{accountPageTitle}">
+            <settings-account-page prefs="{{prefs}}"
+                sync-status="[[syncStatus]]" no-search>
+            </settings-account-page>
+          </settings-subpage>
+        </template>
       </template>
 </if>
     </settings-animated-pages>

chrome/browser/resources/settings/people_page/people_page.ts
@@ -173,6 +173,12 @@ export class SettingsPeoplePageElement extends SettingsPeoplePageElementBase {
             'storedAccounts.length, syncStatus.signedIn, syncStatus.hasError)',
       },
 
+      shouldShowAccountSettingsPage_: {
+        type: Boolean,
+        value: () =>
+            loadTimeData.getBoolean('replaceSyncPromosWithSignInPromos'),
+      },
+
       showImportDataDialog_: {
         type: Boolean,
         value: false,
@@ -216,6 +222,7 @@ export class SettingsPeoplePageElement extends SettingsPeoplePageElementBase {
   // <if expr="not is_chromeos">
   declare storedAccounts: StoredAccount[]|null;
   declare private shouldShowGoogleAccount_: boolean;
+  declare private shouldShowAccountSettingsPage_: boolean;
   declare private showImportDataDialog_: boolean;
   declare private showSignoutDialog_: boolean;
   // </if>

chrome/browser/resources/settings/route.ts
@@ -186,7 +186,9 @@ function createRoutes(): SettingsRoutes {
     r.SIGN_OUT.isNavigableDialog = true;
     r.IMPORT_DATA = r.PEOPLE.createChild('/importData');
     r.IMPORT_DATA.isNavigableDialog = true;
-    r.ACCOUNT = r.PEOPLE.createChild('/account');
+    if (loadTimeData.getBoolean('replaceSyncPromosWithSignInPromos')) {
+      r.ACCOUNT = r.PEOPLE.createChild('/account');
+    }
     // </if>
 
     r.SYNC = r.PEOPLE.createChild('/syncSetup');

chrome/test/data/webui/settings/settings_browsertest.cc
@@ -19,6 +19,7 @@
 #include "components/permissions/features.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/safe_browsing/core/common/features.h"
+#include "components/sync/base/features.h"
 #include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 #include "crypto/crypto_buildflags.h"
@@ -46,6 +47,7 @@ class SettingsBrowserTest : public WebUIMochaBrowserTest {
 #if BUILDFLAG(ENABLE_GLIC)
             features::kGlic, features::kTabstripComboButton,
 #endif
+            syncer::kReplaceSyncPromosWithSignInPromos,
             privacy_sandbox::kPrivacySandboxRelatedWebsiteSetsUi,
             privacy_sandbox::kFingerprintingProtectionUx},
         /*disabled_features=*/{