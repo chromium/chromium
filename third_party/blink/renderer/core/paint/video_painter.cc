b8051e745c27061e0945610bc7288610726a1aaf
Stephen Chenney <schenney@chromium.org>
2025-07-11 11:02:29 -0700

    [HiC] Ignore cross-origin video when preserving privacy
    Change the video painting to always draw the poster when
    using drawElement because attempts to paint video do not yet work.
    Then add code to not draw cross origin video or posters.
    
    Test the poster code because the video path does not work yet.
    
    Bug: 426580072
    Change-Id: Iab03ed8a3630ab5118390b2af2738f6a01f080fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6715819
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1485693}

--- third_party/blink/renderer/core/paint/video_painter.cc
+++ third_party/blink/renderer/core/paint/video_painter.cc
@@ -25,14 +25,28 @@ void VideoPainter::PaintReplaced(const PaintInfo& paint_info,
 
   WebMediaPlayer* media_player =
       layout_video_.MediaElement()->GetWebMediaPlayer();
+  // TODO(crbug.com/419834322): Canvas drawElement does not yet draw video, so
+  // force the poster until it does.
   bool force_video_poster =
       layout_video_.GetDocument().GetPaintPreviewState() ==
-      Document::kPaintingPreviewSkipAcceleratedContent;
+          Document::kPaintingPreviewSkipAcceleratedContent ||
+      (RuntimeEnabledFeatures::CanvasDrawElementEnabled() &&
+       (paint_info.GetPaintFlags() & PaintFlag::kPlacedElement));
   bool should_display_poster =
       layout_video_.GetDisplayMode() == LayoutVideo::kPoster ||
       force_video_poster;
   if (!should_display_poster && !media_player)
     return;
+
+  if (paint_info.IsPrivacyPreserving()) {
+    if (should_display_poster) {
+      if (!layout_video_.ImageResource()->IsAccessAllowed()) {
+        return;
+      }
+    } else if (media_player->WouldTaintOrigin()) {
+      return;
+    }
+  }
 
   PhysicalRect replaced_rect = layout_video_.ReplacedContentRect();
   replaced_rect.Move(paint_offset);
