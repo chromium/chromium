2d08120b3cfa6750a150cf6d5ed07a00d85baaf6
Tom Anderson <thomasanderson@chromium.org>
2025-07-14 08:32:36 -0700

    [DBus Types] Type signatures
    Bug: 429203538
    Change-Id: Iefcf626463f531ca68cddf369ed7f6921cff6039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6695995
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486332}

---
+++ components/dbus/utils/signature.h
@@ -0,0 +1,123 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_DBUS_UTILS_SIGNATURE_H_
+#define COMPONENTS_DBUS_UTILS_SIGNATURE_H_
+
+#include <dbus/dbus.h>
+
+#include <array>
+#include <cstddef>
+#include <string>
+#include <utility>
+
+#include "base/containers/span.h"
+#include "base/files/scoped_file.h"
+#include "components/dbus/utils/types.h"
+#include "dbus/object_path.h"
+
+namespace dbus_utils {
+
+class Variant;
+
+namespace internal {
+
+template <typename T>
+struct DBusSignature {
+  static_assert(false, "Unsupported type for D-Bus");
+};
+
+#define DEFINE_SIMPLE_SIGNATURE(type, signature)             \
+  template <>                                                \
+  struct DBusSignature<type> {                               \
+    static constexpr auto kValue = std::to_array(signature); \
+  };
+
+DEFINE_SIMPLE_SIGNATURE(int16_t, DBUS_TYPE_INT16_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(uint16_t, DBUS_TYPE_UINT16_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(int32_t, DBUS_TYPE_INT32_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(uint32_t, DBUS_TYPE_UINT32_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(int64_t, DBUS_TYPE_INT64_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(uint64_t, DBUS_TYPE_UINT64_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(bool, DBUS_TYPE_BOOLEAN_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(double, DBUS_TYPE_DOUBLE_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(uint8_t, DBUS_TYPE_BYTE_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(std::string, DBUS_TYPE_STRING_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(dbus::ObjectPath, DBUS_TYPE_OBJECT_PATH_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(Variant, DBUS_TYPE_VARIANT_AS_STRING)
+DEFINE_SIMPLE_SIGNATURE(base::ScopedFD, DBUS_TYPE_UNIX_FD_AS_STRING)
+
+#undef DEFINE_SIMPLE_SIGNATURE
+
+// StrJoin takes char[N]s and std::array<char, M>s, each of which is null
+// terminated string, and returns the std::array<char, ...> which is the
+// result of concat, also null terminated.
+template <typename T>
+struct IsStrJoinable : std::false_type {};
+template <size_t N>
+struct IsStrJoinable<char[N]> : std::true_type {};
+template <size_t N>
+struct IsStrJoinable<std::array<char, N>> : std::true_type {};
+
+template <typename... Ts>
+constexpr auto StrJoin(Ts&&... args) {
+  static_assert((IsStrJoinable<std::remove_cvref_t<Ts>>::value && ...),
+                "All types passed to StrJoin must be either char[N] or "
+                "std::array<char, N>.");
+
+  constexpr size_t kSize =
+      (std::size(std::remove_cvref_t<Ts>{}) + ...) - sizeof...(args) + 1;
+  std::array<char, kSize> result = {};
+  size_t i = 0;
+  for (auto span : {base::span<const char>(args)...}) {
+    for (size_t j = 0; j < span.size(); ++j) {
+      result[i + j] = span[j];
+    }
+    i += span.size() - 1;  // Excluding trailing '\0'.
+  }
+  return result;
+}
+
+template <>
+constexpr auto StrJoin() {
+  return std::array<char, 1>{0};
+}
+
+template <typename T>
+  requires IsSupportedArray<T>::value
+struct DBusSignature<T> {
+  static constexpr auto kValue =
+      StrJoin(DBUS_TYPE_ARRAY_AS_STRING,
+              DBusSignature<typename T::value_type>::kValue);
+};
+
+template <typename T>
+  requires IsSupportedMap<T>::value
+struct DBusSignature<T> {
+  using KeyType = typename T::key_type;
+  using ValueType = typename T::mapped_type;
+  static constexpr auto kValue = StrJoin(DBUS_TYPE_ARRAY_AS_STRING,
+                                         DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING,
+                                         DBusSignature<KeyType>::kValue,
+                                         DBusSignature<ValueType>::kValue,
+                                         DBUS_DICT_ENTRY_END_CHAR_AS_STRING);
+};
+
+template <typename... Ts>
+  requires IsSupportedStruct<std::tuple<Ts...>>::value
+struct DBusSignature<std::tuple<Ts...>> {
+  static constexpr auto kValue = StrJoin(DBUS_STRUCT_BEGIN_CHAR_AS_STRING,
+                                         StrJoin(DBusSignature<Ts>::kValue...),
+                                         DBUS_STRUCT_END_CHAR_AS_STRING);
+};
+
+template <typename T>
+constexpr const char* GetDBusTypeSignature() {
+  return DBusSignature<T>::kValue.data();
+}
+
+}  // namespace internal
+}  // namespace dbus_utils
+
+#endif  // COMPONENTS_DBUS_UTILS_SIGNATURE_H_
---
+++ components/dbus/utils/signature_unittest.cc
@@ -0,0 +1,61 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/dbus/utils/signature.h"
+
+#include <map>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "dbus/object_path.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace dbus_utils {
+namespace {
+
+// Helper to get a signature as a std::string.
+template <typename T>
+std::string GetSignature() {
+  return internal::GetDBusTypeSignature<T>();
+}
+
+TEST(DBusSignatureTest, GetSignature) {
+  using TestTuple = std::tuple<int32_t, std::string, std::vector<uint8_t>>;
+  using TestTupleWithVector = std::tuple<std::string, std::vector<int32_t>>;
+  using TestTupleWithMap = std::tuple<int32_t, std::map<std::string, bool>>;
+  using TestMapWithTupleKey = std::map<std::tuple<int32_t, std::string>, bool>;
+
+  EXPECT_EQ(GetSignature<bool>(), "b");
+  EXPECT_EQ(GetSignature<int16_t>(), "n");
+  EXPECT_EQ(GetSignature<uint16_t>(), "q");
+  EXPECT_EQ(GetSignature<int32_t>(), "i");
+  EXPECT_EQ(GetSignature<uint32_t>(), "u");
+  EXPECT_EQ(GetSignature<int64_t>(), "x");
+  EXPECT_EQ(GetSignature<uint64_t>(), "t");
+  EXPECT_EQ(GetSignature<double>(), "d");
+  EXPECT_EQ(GetSignature<uint8_t>(), "y");
+  EXPECT_EQ(GetSignature<std::string>(), "s");
+  EXPECT_EQ(GetSignature<dbus::ObjectPath>(), "o");
+  EXPECT_EQ(GetSignature<base::ScopedFD>(), "h");
+
+  EXPECT_EQ(GetSignature<std::vector<int32_t>>(), "ai");
+  EXPECT_EQ(GetSignature<std::vector<std::string>>(), "as");
+  EXPECT_EQ(GetSignature<std::vector<bool>>(), "ab");
+  EXPECT_EQ(GetSignature<std::vector<std::vector<int32_t>>>(), "aai");
+
+  EXPECT_EQ((GetSignature<std::map<std::string, int32_t>>()), "a{si}");
+
+  EXPECT_EQ(GetSignature<TestTuple>(), "(isay)");
+  EXPECT_EQ(GetSignature<TestTupleWithVector>(), "(sai)");
+  EXPECT_EQ(GetSignature<TestTupleWithMap>(), "(ia{sb})");
+  EXPECT_EQ(GetSignature<TestMapWithTupleKey>(), "a{(is)b}");
+
+  EXPECT_EQ((GetSignature<std::vector<TestTuple>>()), "a(isay)");
+  EXPECT_EQ((GetSignature<std::map<std::string, TestTuple>>()), "a{s(isay)}");
+  EXPECT_EQ((GetSignature<std::map<TestTuple, int32_t>>()), "a{(isay)i}");
+}
+
+}  // namespace
+}  // namespace dbus_utils
