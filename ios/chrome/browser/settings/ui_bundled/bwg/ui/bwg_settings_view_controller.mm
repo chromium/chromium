d135422d5b33601ddb65f88e5136e3f9c8532554
Joemer Ramos <joemerramos@google.com>
2025-07-15 10:55:13 -0700

    [iOS] Add Location and Page Content Row in BWG Settings Page
    Creates initial section, identifiers, and rows for BWG settings page.
    UI interactions are not covered in this CL such as tapping, highlights,
    etc. and will be covered in follow up CLs.
    
    UI screenshot: screen/BhuELPfELiPTiYx
    
    Bug: 427226904
    Change-Id: I42c8624c4d1900f580535d38648eb176a535e017
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6717912
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1487077}

--- ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
+++ ios/chrome/browser/settings/ui_bundled/bwg/ui/bwg_settings_view_controller.mm
@@ -6,8 +6,88 @@
 
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_multi_detail_text_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_switch_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/grit/ios_branded_strings.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 
-@implementation BWGSettingsViewController
+namespace {
+
+// Section identifiers in the BWG settings table view.
+typedef NS_ENUM(NSInteger, SectionIdentifier) {
+  SectionIdentifierBrowsingData = kSectionIdentifierEnumZero,
+  SectionIdentifierActivity,
+};
+
+typedef NS_ENUM(NSInteger, ItemType) {
+  ItemTypeLocation = kItemTypeEnumZero,
+  ItemTypePageContentSharing,
+  ItemTypeAppActivity,
+};
+
+// Table identifier.
+NSString* const kBWGSettingsViewTableIdentifier =
+    @"BWGSettingsViewTableIdentifier";
+
+// Row identifiers.
+NSString* const kLocationCellId = @"LocationCellId";
+NSString* const kPageContentSharingCellId = @"PageContentSharingCellId";
+
+// Action identifier on a tap on links.
+NSString* const kLocationLinkAction = @"LocationLinkAction";
+NSString* const kPageContentSharingAction = @"PageContentSharingAction";
+
+}  // namespace
+
+@implementation BWGSettingsViewController {
+  // Location item.
+  TableViewMultiDetailTextItem* _locationDetailItem;
+  // Switch item for toggling page content sharing.
+  TableViewSwitchItem* _pageContentSharingItem;
+}
+
+#pragma mark - UIViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  self.tableView.accessibilityIdentifier = kBWGSettingsViewTableIdentifier;
+  self.title = l10n_util::GetNSString(IDS_IOS_BWG_SETTINGS_TITLE);
+  [self loadModel];
+}
+
+#pragma mark - CollectionViewController
+
+- (void)loadModel {
+  [super loadModel];
+  TableViewModel* model = self.tableViewModel;
+  [model addSectionWithIdentifier:SectionIdentifierBrowsingData];
+
+  _locationDetailItem =
+      [self detailItemWithType:ItemTypeLocation
+                             text:l10n_util::GetNSString(
+                                      IDS_IOS_BWG_SETTINGS_LOCATION_TITLE)
+                       detailText:l10n_util::GetNSString(
+                                      IDS_IOS_BWG_SETTINGS_LOCATION_DESCRIPTION)
+               trailingDetailText:l10n_util::GetNSString(IDS_IOS_SETTING_OFF)
+          accessibilityIdentifier:kLocationCellId];
+  _pageContentSharingItem = [self
+           switchItemWithType:ItemTypePageContentSharing
+                         text:
+                             l10n_util::GetNSString(
+                                 IDS_IOS_BWG_SETTINGS_PAGE_CONTENT_SHARING_TITLE)
+                   detailText:
+                       l10n_util::GetNSString(
+                           IDS_IOS_BWG_SETTINGS_PAGE_CONTENT_SHARING_DESCRIPTION)
+      accessibilityIdentifier:kPageContentSharingCellId];
+  [model addItem:_locationDetailItem
+      toSectionWithIdentifier:SectionIdentifierBrowsingData];
+  [model addItem:_pageContentSharingItem
+      toSectionWithIdentifier:SectionIdentifierBrowsingData];
+}
 
 #pragma mark - SettingsControllerProtocol
 
@@ -17,6 +97,39 @@
 
 - (void)reportBackUserAction {
   base::RecordAction(base::UserMetricsAction("MobileBWGSettingsBack"));
+}
+
+#pragma mark - Private
+
+- (TableViewMultiDetailTextItem*)detailItemWithType:(NSInteger)type
+                                               text:(NSString*)text
+                                         detailText:(NSString*)detailText
+                                 trailingDetailText:(NSString*)trailingText
+                            accessibilityIdentifier:
+                                (NSString*)accessibilityIdentifier {
+  TableViewMultiDetailTextItem* detailItem =
+      [[TableViewMultiDetailTextItem alloc] initWithType:type];
+  detailItem.text = text;
+  detailItem.leadingDetailText = detailText;
+  // TODO(crbug.com/427226904): Update text based on a pref.
+  detailItem.trailingDetailText = trailingText;
+  detailItem.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+  detailItem.accessibilityTraits |= UIAccessibilityTraitButton;
+  detailItem.accessibilityIdentifier = accessibilityIdentifier;
+  return detailItem;
+}
+
+- (TableViewSwitchItem*)switchItemWithType:(NSInteger)type
+                                      text:(NSString*)title
+                                detailText:(NSString*)detailText
+                   accessibilityIdentifier:(NSString*)accessibilityIdentifier {
+  TableViewSwitchItem* switchItem =
+      [[TableViewSwitchItem alloc] initWithType:type];
+  switchItem.text = title;
+  switchItem.detailText = detailText;
+  switchItem.accessibilityIdentifier = accessibilityIdentifier;
+
+  return switchItem;
 }
 
 @end
