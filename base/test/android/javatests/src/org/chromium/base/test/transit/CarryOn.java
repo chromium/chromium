d432cc31d78d922c79479157f62310d25361ac7e
Henrique Nakashima <hnakashima@chromium.org>
2025-07-15 09:24:56 -0700

    [Android] Add name param to CarryOn
    Useful to declare CarryOns without subclassing it.
    
    Change-Id: Iba948157d5aea02c74004b8f2c610c1eeea94646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6735603
    Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1486994}

--- base/test/android/javatests/src/org/chromium/base/test/transit/CarryOn.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/CarryOn.java
@@ -5,6 +5,7 @@
 package org.chromium.base.test.transit;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 
 /** CarryOn is a lightweight, stand-alone ConditionalState not tied to any Station. */
 @NullMarked
@@ -14,13 +15,32 @@ public class CarryOn extends ConditionalState {
     private final String mName;
     private static int sLastCarryOnId = 2000;
 
-    public CarryOn() {
+    /**
+     * Constructor for named subclasses.
+     *
+     * <p>Named subclasses should let name default to the simple class name, e.g. "<C1:
+     * SubclassNameCarryOn>".
+     */
+    protected CarryOn() {
+        this(/* name= */ null);
+    }
+
+    /**
+     * Create an empty CarryOn. Elements can be declared after creation.
+     *
+     * @param name Direct instantiations should provide a name which will be displayed as "<C1:
+     *     ProvidedName>",
+     */
+    public CarryOn(@Nullable String name) {
         mId = ++sLastCarryOnId;
-        String className = getClass().getSimpleName();
+        if (name == null) {
+            name = getClass().getSimpleName();
+        }
+
         mName =
-                className.isBlank()
+                name.isBlank()
                         ? String.format("<C%d>", mId)
-                        : String.format("<C%d: %s>", mId, className);
+                        : String.format("<C%d: %s>", mId, name);
     }
 
     @Override
