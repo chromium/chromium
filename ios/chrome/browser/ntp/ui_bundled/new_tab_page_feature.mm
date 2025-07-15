c561a61b369bbc7ac3bd8b85470c79140303d1e1
Derek Haas <derekhaas@google.com>
2025-07-15 11:41:47 -0700

    Enable DiscoFeed by default for ios chrome.
    The LE was running at 10% for the past 6 weeks, with all client/server metrics looking good, and acquired all approvals.
    
    Change-Id: I590c45144c29ae7dfc9c60c300765c7065fa5f10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6735573
    Commit-Queue: Derek Haas <derekhaas@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487122}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.mm
@@ -35,7 +35,7 @@ BASE_FEATURE(kEnableDiscoverFeedStaticResourceServing,
 
 BASE_FEATURE(kEnableDiscoverFeedDiscoFeedEndpoint,
              "EnableDiscoFeedEndpoint",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableNTPViewHierarchyRepair,
              "NTPViewHierarchyRepair",
