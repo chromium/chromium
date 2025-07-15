e6243f2bb86c7bc4720d1e8bd204544c101efce2
Mickey Burks <mickeyburks@chromium.org>
2025-07-15 08:43:54 -0700

    Revert "[Collaboration] Scale Avatars to the correct size on retina displays"
    This reverts commit 70ab134d00dcd772a658ef4ab5664d0e9681e055.
    
    Reason for revert: Introduced visual defect
    
    Bug: 430962819
    Original change's description:
    > [Collaboration] Scale Avatars to the correct size on retina displays
    >
    > The icon widths in `GetPageActionImage` and `GetHoverCardImage` were not
    > scaled with the device scale factor causing them to look blurry on
    > retina displays.
    >
    > Bug: 430962819
    > Change-Id: I0c56ebdae5ceff2fbbb6a87848ab10c9b8deea4b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6728454
    > Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1486464}
    
    Bug: 430962819
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I169ab288e49d26d3b3de219f1a7cac5636366d08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6757270
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1486960}

--- chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_tab_data.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/collaboration_messaging_tab_data.cc
@@ -27,18 +27,6 @@
 using collaboration::messaging::PersistentMessage;
 
 namespace tab_groups {
-namespace {
-
-// Get the scale factor for the widget.
-float GetScale(const views::Widget* widget) {
-  float scale_factor = 1.0f;
-  if (widget->GetCompositor()) {
-    scale_factor = widget->GetCompositor()->device_scale_factor();
-  }
-  return scale_factor;
-}
-
-}  // namespace
 
 CollaborationMessagingTabData::CollaborationMessagingTabData(Profile* profile)
     : profile_(profile) {}
@@ -161,15 +149,11 @@ void CollaborationMessagingTabData::CommitMessage(
 
 ui::ImageModel CollaborationMessagingTabData::GetPageActionImage(
     const views::Widget* widget) const {
-  CHECK(widget);
-
   if (!HasMessage()) {
     return ui::ImageModel();
   }
 
-  const float scale_factor = GetScale(widget);
-  const int icon_width =
-      GetLayoutConstant(LOCATION_BAR_TRAILING_ICON_SIZE) * scale_factor;
+  const int icon_width = GetLayoutConstant(LOCATION_BAR_TRAILING_ICON_SIZE);
   if (!avatar_.IsEmpty()) {
     return ui::ImageModel::FromImage(
         gfx::ResizedImage(avatar_, gfx::Size(icon_width, icon_width)));
@@ -180,15 +164,11 @@ ui::ImageModel CollaborationMessagingTabData::GetPageActionImage(
 
 ui::ImageModel CollaborationMessagingTabData::GetHoverCardImage(
     const views::Widget* widget) const {
-  CHECK(widget);
-
   if (!HasMessage()) {
     return ui::ImageModel();
   }
 
-  const float scale_factor = GetScale(widget);
-  const int icon_width =
-      GetLayoutConstant(TAB_ALERT_INDICATOR_ICON_WIDTH) * scale_factor;
+  const int icon_width = GetLayoutConstant(TAB_ALERT_INDICATOR_ICON_WIDTH);
   if (!avatar_.IsEmpty()) {
     return ui::ImageModel::FromImage(
         gfx::ResizedImage(avatar_, gfx::Size(icon_width, icon_width)));
@@ -201,8 +181,13 @@ ui::ImageModel CollaborationMessagingTabData::CreateSizedFallback(
     const views::Widget* widget,
     int icon_width,
     bool add_border) const {
+  CHECK(widget);
+
   // Get devices scale factor for scaling the bitmaps.
-  float scale_factor = GetScale(widget);
+  float scale_factor = 1.0f;
+  if (widget->GetCompositor()) {
+    scale_factor = widget->GetCompositor()->device_scale_factor();
+  }
 
   const ui::ColorProvider* color_provider = widget->GetColorProvider();
   const int icon_padding = ChromeLayoutProvider::Get()->GetDistanceMetric(
