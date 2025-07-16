cd4aedd90b215e057a32a67f67fadae7d9044fc2
Niko Tsirakis <ntsirakis@google.com>
2025-07-16 12:19:36 -0700

    artemis: Replace the now-banned std::filesystem API
    Bug: None
    Change-Id: I9937d19d00e5062d1ed9c271b9615a6b777a6e92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6757349
    Auto-Submit: Niko Tsirakis <ntsirakis@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487830}

--- chrome/browser/ash/chromebox_for_meetings/artemis/log_source_unittest.cc
+++ chrome/browser/ash/chromebox_for_meetings/artemis/log_source_unittest.cc
@@ -4,10 +4,10 @@
 
 #include "chrome/browser/ash/chromebox_for_meetings/artemis/log_source.h"
 
-#include <filesystem>
 #include <fstream>
 #include <map>
 
+#include "base/files/file_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/test/task_environment.h"
 #include "base/time/time.h"
@@ -87,8 +87,8 @@ class LogSourceForTesting : public LogSource {
 class ArtemisLogSourceTest : public testing::Test {
  public:
   ArtemisLogSourceTest()
-      : test_file_("test_file.log"),
-        rotated_file_("test_file.log.1"),
+      : test_file_(base::FilePath::FromASCII("test_file.log")),
+        rotated_file_(base::FilePath::FromASCII("test_file.log.1")),
         rotate_log_prefix_("ROTATE: ") {}
   ArtemisLogSourceTest(const ArtemisLogSourceTest&) = delete;
   ArtemisLogSourceTest& operator=(const ArtemisLogSourceTest&) = delete;
@@ -96,21 +96,23 @@ class ArtemisLogSourceTest : public testing::Test {
   void SetUp() override {
     test_db_ = std::make_unique<PersistentDbForTesting>();
     PersistentDb::InitializeForTesting(test_db_.get());
-    AppendNewLines(test_file_, kTestFileNumLines, "");
+    AppendNewLines(test_file_.value(), kTestFileNumLines, "");
   }
 
   void TearDown() override {
-    std::filesystem::remove(test_file_);
-    if (std::filesystem::exists(rotated_file_)) {
-      std::filesystem::remove(rotated_file_);
+    base::DeleteFile(test_file_);
+
+    if (base::PathExists(rotated_file_)) {
+      base::DeleteFile(rotated_file_);
     }
+
     test_db_.reset(nullptr);
     PersistentDb::ShutdownForTesting();
   }
 
   void RotateFile() {
-    std::filesystem::rename(test_file_, rotated_file_);
-    AppendNewLines(test_file_, kTestFileNumLines, rotate_log_prefix_);
+    base::ReplaceFile(test_file_, rotated_file_, nullptr);
+    AppendNewLines(test_file_.value(), kTestFileNumLines, rotate_log_prefix_);
   }
 
   void AppendNewLines(const std::string& filename,
@@ -125,14 +127,14 @@ class ArtemisLogSourceTest : public testing::Test {
   }
 
   int GetFileSize() {
-    std::ifstream tmp_stream(test_file_);
+    std::ifstream tmp_stream(test_file_.value());
     tmp_stream.seekg(0, tmp_stream.end);
     return (int)tmp_stream.tellg();
   }
 
  protected:
-  const std::string test_file_;
-  const std::string rotated_file_;
+  const base::FilePath test_file_;
+  const base::FilePath rotated_file_;
   const std::string rotate_log_prefix_;
 
  private:
@@ -142,8 +144,8 @@ class ArtemisLogSourceTest : public testing::Test {
 // ------- Start LogFile tests -------
 
 TEST_F(ArtemisLogSourceTest, OpenFileAtVariousOffsets) {
-  LogFile logfile_bad(test_file_ + "noexist");
-  LogFile logfile(test_file_);
+  LogFile logfile_bad(test_file_.value() + "noexist");
+  LogFile logfile(test_file_.value());
 
   // File doesn't exist
   EXPECT_FALSE(logfile_bad.OpenAtOffset(0));
@@ -168,7 +170,7 @@ TEST_F(ArtemisLogSourceTest, OpenFileAtVariousOffsets) {
 }
 
 TEST_F(ArtemisLogSourceTest, CheckEOFStateAfterVariousOpens) {
-  LogFile logfile(test_file_);
+  LogFile logfile(test_file_.value());
   int file_size = GetFileSize();
 
   // File opened at beginning
@@ -191,7 +193,7 @@ TEST_F(ArtemisLogSourceTest, CheckEOFStateAfterVariousOpens) {
 }
 
 TEST_F(ArtemisLogSourceTest, RequestVaryingAmountOfLogLines) {
-  LogFile logfile(test_file_);
+  LogFile logfile(test_file_.value());
   logfile.OpenAtOffset(0);
 
   // Read all logs. Note EOF is expected to be false until
@@ -225,7 +227,7 @@ TEST_F(ArtemisLogSourceTest, CheckByteCapLimitsDataOutput) {
   // each line is an incrementing integer starting at 1. This is
   // insufficient for this particular test, so let's recreate it
   // and add larger data.
-  std::filesystem::remove(test_file_);
+  base::DeleteFile(test_file_);
 
   size_t new_file_num_lines = 3;
   size_t line_size = 100;
@@ -233,13 +235,13 @@ TEST_F(ArtemisLogSourceTest, CheckByteCapLimitsDataOutput) {
   std::ofstream stream;
   std::string large_line = std::string(line_size, '!');
 
-  stream.open(test_file_, std::ios_base::app);
+  stream.open(test_file_.value(), std::ios_base::app);
   for (unsigned int i = 0; i < new_file_num_lines; i++) {
     stream << large_line << std::endl;
   }
   stream.close();
 
-  LogFile logfile(test_file_);
+  LogFile logfile(test_file_.value());
   logfile.OpenAtOffset(0);
   size_t byte_cap = line_size;
 
@@ -266,7 +268,7 @@ TEST_F(ArtemisLogSourceTest, CheckByteCapLimitsDataOutput) {
 }
 
 TEST_F(ArtemisLogSourceTest, VerifyNewLinesAppearAfterRefresh) {
-  LogFile logfile(test_file_);
+  LogFile logfile(test_file_.value());
   logfile.OpenAtOffset(0);
 
   // Note for the below tests: there are two cases we need to
@@ -286,7 +288,7 @@ TEST_F(ArtemisLogSourceTest, VerifyNewLinesAppearAfterRefresh) {
   // case #1 above.
   logfile.RetrieveNextLogs(kTestFileNumLines + 1, kBatchByteLimit);
   EXPECT_TRUE(logfile.IsAtEOF());
-  AppendNewLines(test_file_, kTestFileNumLines, "NEW: ");
+  AppendNewLines(test_file_.value(), kTestFileNumLines, "NEW: ");
 
   // Verify no new lines are reported before Refresh()
   auto new_lines = logfile.RetrieveNextLogs(kTestFileNumLines, kBatchByteLimit);
@@ -308,7 +310,7 @@ TEST_F(ArtemisLogSourceTest, VerifyNewLinesAppearAfterRefresh) {
   // should be false and newly appended lines should be available
   // immediately. Add more lines and test case #2 above.
   EXPECT_FALSE(logfile.IsAtEOF());
-  AppendNewLines(test_file_, kTestFileNumLines, "NEW2: ");
+  AppendNewLines(test_file_.value(), kTestFileNumLines, "NEW2: ");
 
   // Verify lines are immediately observable.
   new_lines = logfile.RetrieveNextLogs(kTestFileNumLines, kBatchByteLimit);
@@ -327,7 +329,8 @@ TEST_F(ArtemisLogSourceTest, TestBatchSizeCorrectlyLimitsOutput) {
   const size_t batch_size = 2;
   const size_t expected_num_reads = kTestFileNumLines / batch_size;
 
-  auto log_source = LogSource(test_file_, kDefaultPollFrequency, batch_size);
+  auto log_source =
+      LogSource(test_file_.value(), kDefaultPollFrequency, batch_size);
 
   for (size_t i = 0; i < expected_num_reads; ++i) {
     auto data = log_source.GetNextData();
@@ -340,7 +343,7 @@ TEST_F(ArtemisLogSourceTest, TestBatchSizeCorrectlyLimitsOutput) {
 
 TEST_F(ArtemisLogSourceTest, VerifyNewLinesAppearAfterRotation) {
   auto log_source =
-      LogSource(test_file_, kDefaultPollFrequency, kDefaultBatchSize);
+      LogSource(test_file_.value(), kDefaultPollFrequency, kDefaultBatchSize);
 
   // Initial setup. Read everything from original file
   auto data = log_source.GetNextData();
@@ -349,7 +352,7 @@ TEST_F(ArtemisLogSourceTest, VerifyNewLinesAppearAfterRotation) {
   EXPECT_EQ(data.size(), 0u);
 
   // Add more lines to original file
-  AppendNewLines(test_file_, kTestFileNumLines, "NEW: ");
+  AppendNewLines(test_file_.value(), kTestFileNumLines, "NEW: ");
 
   // Rotate file and verify that next fetch returns lines from new file
   RotateFile();
@@ -369,7 +372,7 @@ TEST_F(ArtemisLogSourceTest, TestCrashRecovery) {
 
   size_t batch_size = 2;
   auto log_source = std::make_unique<LogSourceForTesting>(
-      test_file_, kDefaultPollFrequency, batch_size);
+      test_file_.value(), kDefaultPollFrequency, batch_size);
 
   // Add <batch_size> lines to internal buffer. Then fetch & drop.
   log_source->FillDataBufferForTesting();
@@ -387,7 +390,7 @@ TEST_F(ArtemisLogSourceTest, TestCrashRecovery) {
   // Tear down and reset the log source. Then add more data.
   log_source.reset(nullptr);
   log_source = std::make_unique<LogSourceForTesting>(
-      test_file_, kDefaultPollFrequency, batch_size);
+      test_file_.value(), kDefaultPollFrequency, batch_size);
   log_source->FillDataBufferForTesting();
 
   // Run the next Fetch and expect the data to be continued from the last
@@ -404,7 +407,7 @@ TEST_F(ArtemisLogSourceTest, TestCrashRecovery) {
   // Explicitly do not Flush()! Tear down and reset again. Add more data.
   log_source.reset(nullptr);
   log_source = std::make_unique<LogSourceForTesting>(
-      test_file_, kDefaultPollFrequency, batch_size);
+      test_file_.value(), kDefaultPollFrequency, batch_size);
   log_source->FillDataBufferForTesting();
 
   // Because Flush() was not called, we assume that the last attempt failed,
@@ -421,7 +424,7 @@ TEST_F(ArtemisLogSourceTest, TestCrashRecovery) {
   // Tear down and reset again.
   log_source.reset(nullptr);
   log_source = std::make_unique<LogSourceForTesting>(
-      test_file_, kDefaultPollFrequency, batch_size);
+      test_file_.value(), kDefaultPollFrequency, batch_size);
 
   // This time, before adding data, rotate the file.
   RotateFile();
@@ -481,7 +484,7 @@ TEST(ArtemisLogSourceTestBadFile, TestFileInitializationIsRetried) {
   run_loop.RunUntilIdle();
 
   // Clean up.
-  std::filesystem::remove(filename);
+  base::DeleteFile(base::FilePath::FromASCII(filename));
 }
 
 }  // namespace
