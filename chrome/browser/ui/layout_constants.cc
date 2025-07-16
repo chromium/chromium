ce4fc092f34a50c78a44464d94f114ef8b5f53f8
Yuheng Huang <yuhengh@chromium.org>
2025-07-16 10:30:19 -0700

    Tab strip: Fix missing margin in tablet mode
    Fix 0 margin in tablet when WebUITabStrip is disabled. This CL sets the
    margin to the same one when WebUITabStrip is enabled.
    
    Before: https://screenshot.googleplex.com/6eF546X8U7fVSyT After:
    https://screenshot.googleplex.com/7bP6pA7PsxAe2Jr Thumbnail tab strip:
    https://screenshot.googleplex.com/3VbyAJHbAdnmnyM
    
    Bug: 422551531
    Change-Id: I0df89ee150e6ce7eeed2d2992088aaa6bfe69855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6626548
    Auto-Submit: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1487751}

--- chrome/browser/ui/layout_constants.cc
+++ chrome/browser/ui/layout_constants.cc
@@ -189,7 +189,7 @@ gfx::Insets GetLayoutInsets(LayoutInset inset) {
       }
 
     case TOOLBAR_INTERIOR_MARGIN:
-      return touch_ui ? gfx::Insets() : gfx::Insets::VH(6, 5);
+      return touch_ui ? gfx::Insets::VH(4, 0) : gfx::Insets::VH(6, 5);
 
     case WEBUI_TAB_STRIP_TOOLBAR_INTERIOR_MARGIN:
       return gfx::Insets::VH(4, 0);
