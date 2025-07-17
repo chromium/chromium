a18d8f4e730cbcfe61df084b00fb85939342bccb
François Doray <fdoray@chromium.org>
2025-07-17 13:37:41 -0700

    [freezing] Enable freezing on battery saver by default.
    launch/4282880
    
    Bug: 325954772
    Change-Id: I6dd0b768105f2853c467bd0bac91f053551ec8a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6766914
    Commit-Queue: Alex Attar <aattar@google.com>
    Reviewed-by: Alex Attar <aattar@google.com>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488503}

--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -232,7 +232,7 @@ BASE_FEATURE_PARAM(base::TimeDelta,
 
 BASE_FEATURE(kFreezingOnBatterySaver,
              "FreezingOnBatterySaver",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFreezingOnBatterySaverForTesting,
              "FreezingOnBatterySaverForTesting",
