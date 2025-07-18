96d5cd7020d649e7e30be7dbee5e5d90c7edbb36
Ian Kilpatrick <ikilpatrick@chromium.org>
2025-07-17 17:06:25 -0700

    Revert "[webview] Skip ContentsPreferredMinimumSize within UpdateContentsSize"
    This reverts commit 04a2a423b463bc65a82777d3918b9149e6dac126.
    
    Reason for revert: crbug.com/432484933
    
    Bug: 422933487
    Original change's description:
    > [webview] Skip ContentsPreferredMinimumSize within UpdateContentsSize
    >
    > Behind a the kill-switch: kWebViewSkipPreferredSizeForContentsSize
    >
    > ContentsPreferredMinimumSize is relative dangerous outside of a blink
    > layout. We have logic guarding known bad cases here:
    > https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/layout/block_node.cc;l=973-975;drc=cfaac192dca61003af46bcc752ac16652a675ade
    > For these cases we basically just lie about this size.
    >
    > This code invoking ContentsPreferredMinimumSize was originally added
    > here:
    > https://chromium.googlesource.com/chromium/src/+/d01b2a644559414d0c4632f3829f3165e4054d84%5E%21/#F14
    >
    > But without tests :(.
    >
    > Additionally the "contents_size.IsEmpty" check is bad. E.g. IsEmpty()
    > will return true for something like 100x0 - so the
    > ContentsPreferredMinimumSize wont actually change it to non-empty.
    >
    > This codepath is causing crashes again (see relevant bug) - but we can't
    > diagnose the specific issue this time around - lets just disable the
    > root cause instead of attempting to monkey patch it again.
    >
    > Bug: 422933487
    > Change-Id: Ib5d3723b8e28471c30f0d6149ac64ba1d5f7f107
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6624723
    > Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    > Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1471873}
    
    Bug: 422933487
    Change-Id: Ifaa5f1e00deef04d575ef1c71f770876ef5fb242
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764932
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488576}

--- android_webview/common/aw_features.h
+++ android_webview/common/aw_features.h
@@ -51,7 +51,6 @@ extern const base::FeatureParam<bool> kWebViewPrefetchFromRenderer;
 BASE_DECLARE_FEATURE(kWebViewSafeAreaIncludesSystemBars);
 BASE_DECLARE_FEATURE(kWebViewSeparateResourceContext);
 BASE_DECLARE_FEATURE(kWebViewSkipInterceptsForPrefetch);
-BASE_DECLARE_FEATURE(kWebViewSkipPreferredSizeForContentsSize);
 BASE_DECLARE_FEATURE(kWebViewDoNotSendAccessibilityEventsOnGSU);
 BASE_DECLARE_FEATURE(kWebViewHyperlinkContextMenu);
 BASE_DECLARE_FEATURE(kCreateSpareRendererOnBrowserContextCreation);
