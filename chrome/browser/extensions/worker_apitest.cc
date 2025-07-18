bbb83b559e99fc231a55cc2004681fda002db776
Minoru Chikamune <chikamune@chromium.org>
2025-07-18 01:37:45 -0700

    Disable flaky WorkerTest.WorkerInBackgroundPage on win-asan
    The win-asan started to be flaky from 2025-07-08.
    
    <<test history on win-asan>>
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FWorkerTest.WorkerInBackgroundPage?q=V%3Abuilder%3Dwin-asan+V%3Aos%3DWindows-10-19045+V%3Atest_suite%3Dbrowser_tests
    
    <<test history>>
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FWorkerTest.WorkerInBackgroundPage
    
    Bug: 431290255
    Change-Id: I53c26ceaba8316a98582c075754d8056d3b58590
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6768395
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1488706}

--- chrome/browser/extensions/worker_apitest.cc
+++ chrome/browser/extensions/worker_apitest.cc
@@ -18,8 +18,13 @@ class WorkerTest : public ExtensionApiTest {
   }
 };
 
-// TODO(crbug.com/431290255): Disable the test due to flakiness.
-IN_PROC_BROWSER_TEST_F(WorkerTest, DISABLED_WorkerInBackgroundPage) {
+// TODO(crbug.com/431290255): Flaky on Windows with ASAN.
+#if BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER)
+#define MAYBE_WorkerInBackgroundPage DISABLED_WorkerInBackgroundPage
+#else
+#define MAYBE_WorkerInBackgroundPage WorkerInBackgroundPage
+#endif
+IN_PROC_BROWSER_TEST_F(WorkerTest, MAYBE_WorkerInBackgroundPage) {
   embedded_test_server()->ServeFilesFromDirectory(test_data_dir_);
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("worker")) << message_;
