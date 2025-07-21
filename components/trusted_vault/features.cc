17ce892822bfa0f316f336e69802f88392c1b1fc
Thomas Thrainer <thomasth@google.com>
2025-07-21 01:58:35 -0700

    Enable the iCloud Keychain recovery factor feature by default
    The feature is enabled at 50% stable currently and the launch is fully
    approved. Thus, enable the feature by default before launching it to
    100% stable.
    
    Bug: 398160323
    Change-Id: I7e403f3548e71de3c89bf0565db6a4829306e667
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771810
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Thomas Thrainer <thomasth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489409}

--- components/trusted_vault/features.cc
+++ components/trusted_vault/features.cc
@@ -18,7 +18,7 @@ BASE_FEATURE(kSetClientEncryptionKeysJsApi,
 #if BUILDFLAG(IS_MAC)
 BASE_FEATURE(kEnableICloudKeychainRecoveryFactor,
              "EnableICloudKeychainRecoveryFactor",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kEnableRegistrationStateSecurityDomainFiltering,
