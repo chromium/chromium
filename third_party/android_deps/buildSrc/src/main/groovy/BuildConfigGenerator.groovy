836fd76efcb4fe6008dc6db3747e57bbce861b30
Andrew Grieve <agrieve@chromium.org>
2025-07-21 10:24:59 -0700

    Android: Do not default "include_java_resources = true" in android_aar_prebuilt
    These take space in .build_config.json files, and I've never seen an
    .aar with embedded resources. If we find one, we can always set it to
    "true" for the target (this is also how android_java_prebuilt targets
    work).
    
    Size of all .build_config.json: 206144306 -> 205005514
    
    Bug: 413331591
    Change-Id: Ifa504027a91ef5b92a05a6c62fda212794df333d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6766067
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489626}

--- third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
+++ third_party/android_deps/buildSrc/src/main/groovy/BuildConfigGenerator.groovy
@@ -871,7 +871,8 @@ No modifications.
                 sb.append('  bypass_platform_checks = true\n')
                 break
             case 'com_google_android_apps_common_testing_accessibility_framework_accessibility_test_framework':
-                sb.append('  proguard_configs = [ "local_modifications/accessibility_test_framework.pcfg" ]')
+                sb.append('  include_java_resources = true\n')
+                sb.append('  proguard_configs = [ "local_modifications/accessibility_test_framework.pcfg" ]\n')
                 break
         }
     }
