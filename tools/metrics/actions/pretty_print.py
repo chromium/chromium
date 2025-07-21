3434288981f7f406c918b61f36f773629cafb6da
Alicja Opalinska <aopalinska@google.com>
2025-07-20 23:39:54 -0700

    Added cleanup command to all of the pretty_print.py scripts.
    The initial implementation only included the script in the histograms
    module, and skipped the remaining 4 files. This CL adds the same arg
    parser logic to all the pretty_print scripts that use the same
    presubmit_util module. initial CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/6701592
    
    Bug: 402130955
    Change-Id: I91e39240c96ce10a5601e8ef88551f9c535f4c8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6764670
    Reviewed-by: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Alicja Opalinska <aopalinska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489373}

--- tools/metrics/actions/pretty_print.py
+++ tools/metrics/actions/pretty_print.py
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging
+import argparse
 import os
 import sys
 
@@ -11,10 +11,33 @@ import extract_actions
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 import presubmit_util
 
-def main(argv):
-  presubmit_util.DoPresubmitMain(argv, 'actions.xml', 'actions.old.xml',
+
+def main():
+  """Pretty-prints the User Actions in actions.xml file.
+
+  Args:
+    --non-interactive: (Optional) Does not print log info messages and does not
+        prompt user to accept the diff.
+    --presubmit: (Optional) Simply prints a message if the input is not
+        formatted correctly instead of modifying the file.
+    --diff: (Optional) Prints diff to stdout rather than modifying the file.
+    --cleanup: (Optional) Removes any backup file created during the execution.
+
+  Example usage:
+    pretty_print.py --diff --cleanup
+  """
+  parser = argparse.ArgumentParser()
+  # The following optional flags are used by common/presubmit_util.py
+  parser.add_argument('--non-interactive', action="store_true")
+  parser.add_argument('--presubmit', action="store_true")
+  parser.add_argument('--diff', action="store_true")
+  parser.add_argument('--cleanup',
+                      action="store_true",
+                      help="Remove the backup file after a successful run.")
+
+  presubmit_util.DoPresubmitMain(sys.argv, 'actions.xml', 'actions.old.xml',
                                  extract_actions.UpdateXml)
 
 
-if '__main__' == __name__:
-  sys.exit(main(sys.argv))
+if __name__ == '__main__':
+  sys.exit(main())
