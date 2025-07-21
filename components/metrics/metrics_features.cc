3b58a722acbc806d861772384cdacdc2f6506c86
Robert Kaplow <rkaplow@google.com>
2025-07-21 12:45:52 -0700

    Enable ResetMetricsUploadBackoffOnForeground.
    Bug:420459511
    
    Change-Id: I5abf7ddf5a1d1dfb30a2c282697d44dc31fe4e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6774666
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1489725}

--- components/metrics/metrics_features.cc
+++ components/metrics/metrics_features.cc
@@ -25,7 +25,7 @@ BASE_FEATURE(kMetricsLogTrimming,
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kResetMetricsUploadBackoffOnForeground,
              "ResetMetricsUploadBackoffOnForeground",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 }  // namespace metrics::features
