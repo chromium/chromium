d4996f4c2903f369cc6b8ef1809d9e237c58b30c
Michael Ershov <miersh@google.com>
2025-07-21 04:50:05 -0700

    kcer_nss_fuzzer: Pick out of the supported signing algorithms
    Make the test cert builder use one of the supported signing
    algorithms based on the currently used key type (RSA or EC).
    Also remove the alternative ways to set the signing algorithm,
    they all seem to lead to the same end result (or fail on an
    ASSERT if an invalid value is provided).
    
    Fixed: b:432394613
    Change-Id: I96db984283186ee3c3a8ed93845c1c2fb111b92e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6768722
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1489448}

--- chromeos/ash/components/kcer/kcer_nss/kcer_nss_fuzzer.cc
+++ chromeos/ash/components/kcer/kcer_nss/kcer_nss_fuzzer.cc
@@ -258,7 +258,8 @@ class CertGenerator {
   inline GURL GetGurl();
   inline net::IPAddress GetIpAddress();
   std::vector<bssl::KeyUsageBit> GetKeyUsages();
-  inline bssl::SignatureAlgorithm GetSignatureAlgorithm();
+  inline bssl::SignatureAlgorithm GetSignatureAlgorithm(
+      bool issuer_uses_rsa_key);
   std::string GetValidOid();
 
   void GenerateCert();
@@ -369,18 +370,21 @@ std::vector<bssl::KeyUsageBit> CertGenerator::GetKeyUsages() {
   return result;
 }
 
-bssl::SignatureAlgorithm CertGenerator::GetSignatureAlgorithm() {
-  SupportedSignatureAlgorithm algorithm =
-      data_provider_->ConsumeEnum<SupportedSignatureAlgorithm>();
-  switch (algorithm) {
-    case SupportedSignatureAlgorithm::kRsaPkcs1Sha1:
+// Picks a supported algorithm for the currently used key.
+bssl::SignatureAlgorithm CertGenerator::GetSignatureAlgorithm(
+    bool issuer_uses_rsa_key) {
+  if (issuer_uses_rsa_key) {
+    if (GetBool()) {
       return bssl::SignatureAlgorithm::kRsaPkcs1Sha1;
-    case SupportedSignatureAlgorithm::kRsaPkcs1Sha256:
+    } else {
       return bssl::SignatureAlgorithm::kRsaPkcs1Sha256;
-    case SupportedSignatureAlgorithm::kEcdsaSha1:
+    }
+  } else {
+    if (GetBool()) {
       return bssl::SignatureAlgorithm::kEcdsaSha1;
-    case SupportedSignatureAlgorithm::kEcdsaSha256:
+    } else {
       return bssl::SignatureAlgorithm::kEcdsaSha256;
+    }
   }
 }
 
@@ -574,16 +578,8 @@ void CertGenerator::GenerateCert() {
     cert_builder_->SetAuthorityKeyIdentifier(GetString());
   }
   if (GetBool()) {
-    cert_builder_->SetSignatureAlgorithm(GetSignatureAlgorithm());
-  }
-  if (GetBool()) {
-    cert_builder_->SetSignatureAlgorithmTLV(GetString());
-  }
-  if (GetBool()) {
-    cert_builder_->SetOuterSignatureAlgorithmTLV(GetString());
-  }
-  if (GetBool()) {
-    cert_builder_->SetTBSSignatureAlgorithmTLV(GetString());
+    cert_builder_->SetSignatureAlgorithm(
+        GetSignatureAlgorithm(issuer_uses_rsa_key));
   }
 }
 
