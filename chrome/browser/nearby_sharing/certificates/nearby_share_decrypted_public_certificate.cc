9031cbd8a9f72643d78d66779123e27f53980f4a
Elly <ellyjones@chromium.org>
2025-07-22 13:47:51 -0700

    nearby: use crypto/sign for cert verification
    This change:
    * Has NearbyShareDecryptedPublicCertificate hold a parsed PublicKey
      rather than an un-parsed SubjectPublicKeyInfo;
    * Has constructing the NearbyShareDecryptedPublicCertificate (in
      DecryptPublicCertificate) fail if the public certificate doesn't
      contain a valid SubjectPublicKeyInfo - previously constructing it
      would succeed but signature verification would always fail later
    * Switches to the one-shot crypto::sign::Verify() function rather than
      SignatureVerifier
    
    Bug: 372283556
    Change-Id: Iceb687a422a681a31807d5da9d1f660226c339a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6775223
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490410}

--- chrome/browser/nearby_sharing/certificates/nearby_share_decrypted_public_certificate.cc
+++ chrome/browser/nearby_sharing/certificates/nearby_share_decrypted_public_certificate.cc
@@ -14,17 +14,16 @@
 #include "crypto/aead.h"
 #include "crypto/aes_ctr.h"
 #include "crypto/hmac.h"
-#include "crypto/signature_verifier.h"
+#include "crypto/sign.h"
 
 namespace {
 
 bool IsDataValid(base::Time not_before,
                  base::Time not_after,
-                 base::span<const uint8_t> public_key,
                  base::span<const uint8_t> id,
                  base::span<const uint8_t> encrypted_metadata,
                  base::span<const uint8_t> metadata_encryption_key_tag) {
-  return not_before < not_after && !public_key.empty() &&
+  return not_before < not_after &&
          id.size() == kNearbyShareNumBytesCertificateId &&
          !encrypted_metadata.empty() &&
          metadata_encryption_key_tag.size() ==
@@ -94,8 +93,12 @@ NearbyShareDecryptedPublicCertificate::DecryptPublicCertificate(
       public_certificate.start_time().seconds());
   base::Time not_after = base::Time::FromSecondsSinceUnixEpoch(
       public_certificate.end_time().seconds());
-  std::vector<uint8_t> public_key(public_certificate.public_key().begin(),
-                                  public_certificate.public_key().end());
+
+  auto public_key = crypto::keypair::PublicKey::FromSubjectPublicKeyInfo(
+      base::as_byte_span(public_certificate.public_key()));
+  if (!public_key || !public_key->IsEc()) {
+    return std::nullopt;
+  }
 
   auto secret_key = base::as_byte_span(public_certificate.secret_key())
                         .to_fixed_extent<kNearbyShareNumBytesSecretKey>();
@@ -112,7 +115,7 @@ NearbyShareDecryptedPublicCertificate::DecryptPublicCertificate(
       public_certificate.metadata_encryption_key_tag().begin(),
       public_certificate.metadata_encryption_key_tag().end());
 
-  if (!IsDataValid(not_before, not_after, public_key, id, encrypted_metadata,
+  if (!IsDataValid(not_before, not_after, id, encrypted_metadata,
                    metadata_encryption_key_tag)) {
     return std::nullopt;
   }
@@ -154,7 +157,7 @@ NearbyShareDecryptedPublicCertificate::DecryptPublicCertificate(
   }
 
   return NearbyShareDecryptedPublicCertificate(
-      not_before, not_after, *secret_key, std::move(public_key), std::move(id),
+      not_before, not_after, *secret_key, *public_key, std::move(id),
       std::move(unencrypted_metadata), public_certificate.for_self_share());
 }
 
@@ -162,7 +165,7 @@ NearbyShareDecryptedPublicCertificate::NearbyShareDecryptedPublicCertificate(
     base::Time not_before,
     base::Time not_after,
     base::span<const uint8_t, kNearbyShareNumBytesSecretKey> secret_key,
-    std::vector<uint8_t> public_key,
+    crypto::keypair::PublicKey public_key,
     std::vector<uint8_t> id,
     nearby::sharing::proto::EncryptedMetadata unencrypted_metadata,
     bool for_self_share)
@@ -176,7 +179,8 @@ NearbyShareDecryptedPublicCertificate::NearbyShareDecryptedPublicCertificate(
 }
 
 NearbyShareDecryptedPublicCertificate::NearbyShareDecryptedPublicCertificate(
-    const NearbyShareDecryptedPublicCertificate& other) {
+    const NearbyShareDecryptedPublicCertificate& other)
+    : public_key_(other.public_key_) {
   *this = other;
 }
 
@@ -202,24 +206,14 @@ NearbyShareDecryptedPublicCertificate::NearbyShareDecryptedPublicCertificate(
 NearbyShareDecryptedPublicCertificate&
 NearbyShareDecryptedPublicCertificate::operator=(
     NearbyShareDecryptedPublicCertificate&&) = default;
-
 NearbyShareDecryptedPublicCertificate::
     ~NearbyShareDecryptedPublicCertificate() = default;
 
 bool NearbyShareDecryptedPublicCertificate::VerifySignature(
     base::span<const uint8_t> payload,
     base::span<const uint8_t> signature) const {
-  crypto::SignatureVerifier verifier;
-  if (!verifier.VerifyInit(crypto::SignatureVerifier::ECDSA_SHA256, signature,
-                           public_key_)) {
-    CD_LOG(ERROR, Feature::NS)
-        << "Verification failed: Initialization unsuccessful.";
-    return false;
-  }
-
-  verifier.VerifyUpdate(payload);
-
-  return verifier.VerifyFinal();
+  return crypto::sign::Verify(crypto::sign::ECDSA_SHA256, public_key_, payload,
+                              signature);
 }
 
 std::array<uint8_t, kNearbyShareNumBytesAuthenticationTokenHash>
