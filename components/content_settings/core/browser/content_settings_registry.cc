c0838dd2aebdf1fdc358622ae994708763ea5508
Ron Sailer <rsailer@google.com>
2025-07-22 08:58:37 -0700

    Change BUILD so that privacy_sandbox component unittests run on iOS
    Tests never ran on iOS because the components_unittests had the entire
    privacy_sandbox:unit_tests dep under !is_ios. Even though the
    privacy_sandbox:unit_tests rule has an "if (use_blink)" conditional
    sources that is meant to separate by platform. Because the entire dep
    was under !is_ios, even the shared rules would never execute outside of
    blink.
    
    Also expose tracking_protection_settings_unittests to all platforms
    because user bypass support is being added on iOS.
    
    Bug: 433328492, 433490530, 428754923
    Change-Id: I1d56e7694ba62c5675a57d852d74fa214f88a6bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778500
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ron Sailer <rsailer@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1490207}

--- components/content_settings/core/browser/content_settings_registry.cc
+++ components/content_settings/core/browser/content_settings_registry.cc
@@ -807,7 +807,8 @@ void ContentSettingsRegistry::Init() {
       {CONTENT_SETTING_ALLOW, CONTENT_SETTING_BLOCK},
       WebsiteSettingsInfo::REQUESTING_ORIGIN_WITH_TOP_ORIGIN_EXCEPTIONS_SCOPE,
       WebsiteSettingsRegistry::DESKTOP |
-          WebsiteSettingsRegistry::PLATFORM_ANDROID,
+          WebsiteSettingsRegistry::PLATFORM_ANDROID |
+          WebsiteSettingsRegistry::PLATFORM_IOS,
       ContentSettingsInfo::INHERIT_IN_INCOGNITO,
       ContentSettingsInfo::EXCEPTIONS_ON_SECURE_AND_INSECURE_ORIGINS);
 
