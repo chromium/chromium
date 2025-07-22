ba0ad99b8ec6244bbd31bec3afe219887403073b
Ravjit <ravjit@chromium.org>
2025-07-22 13:39:39 -0700

    [PEPC] Restrict Permission icon's height and margin.
    Permission icon's height or width should not exceed 1.5em The
    margin-inline-end should not exceed 3em. Other margins are immutable.
    DD: go/permission-icon-dd
    
    (cherry picked from commit c878dea9fc20cee29ceb491b88ec4a98150e3241)
    
    Bug: 417423571, 432306923
    Change-Id: I579f9bc6b3ea315db36cdd244ec630784e9e4aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6652569
    Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1483797}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6732653
    Auto-Submit: Ravjit Uppal <ravjit@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7258@{#1791}
    Cr-Branched-From: f600d0656fd5b5fe4a82981f533d31ed6939e2e4-refs/heads/main@{#1477651}

--- third_party/blink/renderer/core/html/html_permission_icon_element.cc
+++ third_party/blink/renderer/core/html/html_permission_icon_element.cc
@@ -5,10 +5,17 @@
 #include "third_party/blink/renderer/core/html/html_permission_icon_element.h"
 
 #include "third_party/blink/public/resources/grit/blink_resources.h"
+#include "third_party/blink/renderer/core/html/html_permission_element_utils.h"
 #include "third_party/blink/renderer/core/html/shadow/shadow_element_names.h"
+#include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/data_resource_helper.h"
 
 namespace blink {
+
+namespace {
+constexpr float kMaxIconSizeToFontSizeRatio = 1.5;
+constexpr float kMaxMarginInlineEndToFontSizeRatio = 3.0;
+}  // namespace
 
 using mojom::blink::PermissionName;
 
@@ -16,6 +23,7 @@ HTMLPermissionIconElement::HTMLPermissionIconElement(Document& document)
     : HTMLSpanElement(document) {
   SetIdAttribute(shadow_element_names::kIdPermissionIcon);
   SetShadowPseudoId(shadow_element_names::kIdPermissionIcon);
+  SetHasCustomStyleCallbacks();
 }
 
 void HTMLPermissionIconElement::SetIcon(PermissionName permission_type,
@@ -42,4 +50,72 @@ void HTMLPermissionIconElement::SetIcon(PermissionName permission_type,
   }
   is_icon_set_ = true;
 }
+
+void HTMLPermissionIconElement::AdjustStyle(ComputedStyleBuilder& builder) {
+  Element::AdjustStyle(builder);
+  if (!builder.Width().IsAuto() && !width_console_error_sent_) {
+    width_console_error_sent_ = true;
+    const String warning =
+        "Setting the width has no effect on the icon size. Please use the "
+        "height property to control the icon size.";
+    AddConsoleMessage(mojom::blink::ConsoleMessageSource::kRendering,
+                      mojom::blink::ConsoleMessageLevel::kWarning, warning);
+  }
+
+  float max_height = builder.FontSize() * kMaxIconSizeToFontSizeRatio;
+
+  builder.SetHeight(
+      AdjustedBoundedLengthWrapper(builder.Height(),
+                                   /*lower_bound=*/0,
+                                   /*upper_bound=*/max_height,
+                                   /*should_multiply_by_content_size=*/false));
+  // Width is set to be same as height, as the icon is always a square.
+  builder.SetWidth(builder.Height());
+
+  builder.SetMinHeight(
+      AdjustedBoundedLengthWrapper(builder.MinHeight(),
+                                   /*lower_bound=*/0,
+                                   /*upper_bound=*/max_height,
+                                   /*should_multiply_by_content_size=*/false));
+  // Min-width is set to be same as min-height, as the icon is always a square.
+  builder.SetMinWidth(builder.MinHeight());
+
+  float max_margin = builder.FontSize() * kMaxMarginInlineEndToFontSizeRatio;
+  if (builder.Direction() == TextDirection::kLtr) {
+    builder.SetMarginLeft(Length(0, Length::Type::kFixed));
+    builder.SetMarginRight(AdjustedBoundedLengthWrapper(
+        builder.MarginRight(),
+        /*lower_bound=*/0,
+        /*upper_bound=*/max_margin,
+        /*should_multiply_by_content_size=*/false));
+  } else {
+    builder.SetMarginRight(Length(0, Length::Type::kFixed));
+    builder.SetMarginLeft(AdjustedBoundedLengthWrapper(
+        builder.MarginLeft(),
+        /*lower_bound=*/0,
+        /*upper_bound=*/max_margin,
+        /*should_multiply_by_content_size=*/false));
+  }
+}
+
+Length HTMLPermissionIconElement::AdjustedBoundedLengthWrapper(
+    const Length& length,
+    std::optional<float> lower_bound,
+    std::optional<float> upper_bound,
+    bool should_multiply_by_content_size) {
+  CHECK(lower_bound.has_value() || upper_bound.has_value());
+  bool is_content_or_stretch =
+      length.HasContentOrIntrinsic() || length.HasStretch();
+  if (is_content_or_stretch && !length_console_error_sent_) {
+    length_console_error_sent_ = true;
+    const String warning =
+        "content, intrinsic, or stretch sizes are not supported as values for "
+        "the height or min height of the permission element's icon";
+    AddConsoleMessage(mojom::blink::ConsoleMessageSource::kRendering,
+                      mojom::blink::ConsoleMessageLevel::kWarning, warning);
+  }
+  return HTMLPermissionElementUtils::AdjustedBoundedLength(
+      length, lower_bound, upper_bound, should_multiply_by_content_size);
+}
+
 }  // namespace blink
