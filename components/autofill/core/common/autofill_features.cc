a8952ced7f78615151e13d57029b0c438fa29a69
Christoph Schwering <schwering@google.com>
2025-07-23 09:46:05 -0700

    Revert "[Autofill] Enable AutofillRelaxAddressImport by default"
    This reverts commit 5acdc0917638470627caf29c91907cc52e8504c4.
    
    Reason for revert: There was a bug in the feature check for
    AutofillEnableImportWhenMultiplePhoneNumbers, which must launch
    togother with AutofillRelaxAddressImport: crrev.com/c/6781183
    
    Bug: 395855125
    Original change's description:
    > [Autofill] Enable AutofillRelaxAddressImport by default
    >
    > Bug: 395855125
    > Change-Id: Ic0b10f2b2f2bcf39792734bf0c2026e65ff66eb4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6771330
    > Commit-Queue: Jan Keitel <jkeitel@google.com>
    > Auto-Submit: Christoph Schwering <schwering@google.com>
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1489971}
    
    Bug: 395855125
    Change-Id: Icad8e39d5d3d25e18640b177c028dee9bd5d9366
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6781186
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1490884}

--- components/autofill/core/common/autofill_features.cc
+++ components/autofill/core/common/autofill_features.cc
@@ -467,7 +467,7 @@ BASE_FEATURE(kAutofillPreferSavedFormAsSubmittedForm,
 // TODO(crbug.com/395855125): Remove when launched.
 BASE_FEATURE(kAutofillRelaxAddressImport,
              "AutofillRelaxAddressImport",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Replaces blink::WebFormElementObserver usage in FormTracker by updated logic
 // for tracking the disappearance of forms as well as other submission
