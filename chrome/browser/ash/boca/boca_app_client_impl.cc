e9325102472dbc0daceab1b72f5332ffb767d128
Antonio Gomes <tonikitoo@igalia.com>
2025-07-22 17:18:27 -0700

    Do not operate over Browser* and BrowserList in BocaAppClientImpl::HasApp()
    ... from boca_app_client_impl.cc.
    
    This CL does the following:
    
    1/ changes the logic in BocaAppClientImpl() function to operate over
    the `ash::FindSystemWebAppBrowser()` overload that returns an
    BrowserDelegate instance, and uses its ::IsClosing() method.
    
    2/ removes the inclusion of c/b/ui/browser.h and
    c/b/ui/browser_list.h from boca_app_client_impl.cc
    
    3/ removes the circular dependency annotation that //c/b/ash/boca:boca
    has against //c/b/ui:ui.
    
    4/ removes the dependency to //c/b/ui:browser_list from
    //c/b/ash/boca:boca.
    
    5/ cleans up / organizes DEPS entries
    
    R=neis@chromium.org
    
    Bug: 427342311
    Change-Id: I516a8ec834b5feb3cc1a0aeb961094b2187ece24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6773000
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490516}

--- chrome/browser/ash/boca/boca_app_client_impl.cc
+++ chrome/browser/ash/boca/boca_app_client_impl.cc
@@ -5,14 +5,13 @@
 #include "chrome/browser/ash/boca/boca_app_client_impl.h"
 
 #include "ash/webui/system_apps/public/system_web_app_type.h"
+#include "chrome/browser/ash/browser_delegate/browser_delegate.h"
 #include "chrome/browser/ash/settings/device_settings_service.h"
 #include "chrome/browser/feedback/show_feedback_page.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 namespace ash::boca {
@@ -52,11 +51,10 @@ void BocaAppClientImpl::LaunchApp() {
 }
 
 bool BocaAppClientImpl::HasApp() {
-  auto* const browser = ash::FindSystemWebAppBrowser(
-      ProfileManager::GetActiveUserProfile(), SystemWebAppType::BOCA);
-  return browser &&
-         !BrowserList::GetInstance()->currently_closing_browsers().contains(
-             browser);
+  auto* const browser_delegate =
+      ash::FindSystemWebAppBrowser(ProfileManager::GetActiveUserProfile(),
+                                   SystemWebAppType::BOCA, BrowserType::kApp);
+  return browser_delegate && !browser_delegate->IsClosing();
 }
 
 void BocaAppClientImpl::OpenFeedbackDialog() {
