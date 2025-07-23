c54e7a30b4f59e46c3f139d973cff59f8c325390
Fiaz Muhammad <mfiaz@google.com>
2025-07-23 07:58:22 -0700

    [Tab Group Suggest] Implement hide instant message functionality
    - Implements a tracking mechanism for new messages along with extra
    	data.
    - Messages are removed from the tracking map on being dismissed.
    - We use this cache to provide PropertyModels, which are used
    	to dismiss their associated InstantMessage.
    
    Bug: 416264627
    Change-Id: I838fafd0556eb1f08fad3fae79b8eee72fd593ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6720231
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1490823}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/InstantMessageDelegateImplUnitTest.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/InstantMessageDelegateImplUnitTest.java
@@ -81,6 +81,8 @@ import org.chromium.url.JUnitTestGURLs;
 
 import java.lang.ref.WeakReference;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 /** Unit tests for {@link InstantMessageDelegateImpl}. */
@@ -387,5 +389,42 @@ public class InstantMessageDelegateImplUnitTest {
         verify(mDataSharingNotificationManager)
                 .showOtherJoinedNotification(any(), eq(SYNC_GROUP_ID1), eq(9));
         verify(mSuccessCallback).onResult(true);
+    }
+
+    @Test
+    public void testHideInstantMessage_MessageIsShowing() {
+        String messageIdToHide = "1";
+        InstantMessage message = newInstantMessage(CollaborationEvent.TAB_REMOVED);
+        message.attributions.get(0).id = messageIdToHide;
+
+        mDelegate.displayInstantaneousMessage(message, mSuccessCallback);
+        verify(mManagedMessageDispatcher)
+                .enqueueWindowScopedMessage(mPropertyModelCaptor.capture(), anyBoolean());
+        PropertyModel displayedModel = mPropertyModelCaptor.getValue();
+
+        Set<String> idsToHide = new HashSet<>();
+        idsToHide.add(messageIdToHide);
+        mDelegate.hideInstantaneousMessage(idsToHide);
+        ShadowLooper.runUiThreadTasks();
+
+        verify(mManagedMessageDispatcher)
+                .dismissMessage(displayedModel, DismissReason.DISMISSED_BY_FEATURE);
+    }
+
+    @Test
+    public void testHideInstantMessage_MessageNotShowing() {
+        InstantMessage messageToShow = newInstantMessage(CollaborationEvent.TAB_REMOVED);
+        messageToShow.attributions.get(0).id = "1";
+        mDelegate.displayInstantaneousMessage(messageToShow, mSuccessCallback);
+        verify(mManagedMessageDispatcher).enqueueWindowScopedMessage(any(), anyBoolean());
+
+        // Attempt to hide a message ID that was never displayed.
+        Set<String> idsToHide = new HashSet<>();
+        idsToHide.add("2");
+        mDelegate.hideInstantaneousMessage(idsToHide);
+        ShadowLooper.runUiThreadTasks();
+
+        // This should have noop-ed.
+        verify(mManagedMessageDispatcher, never()).dismissMessage(any(), anyInt());
     }
 }
