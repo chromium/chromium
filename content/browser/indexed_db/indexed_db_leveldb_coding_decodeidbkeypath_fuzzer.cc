ded53accaafbb4f08fed24da60473d875e16ff9b
Evan Stade <evanstade@microsoft.com>
2025-07-23 09:43:44 -0700

    Fix indexed_db_leveldb_coding_decodeidbkeypath_fuzzer
    Two problems:
    1) DecodeIDBKeyPath modifies its first parameter, which the test didn't
       account for
    2) Re-encoding doesn't always create an identical encoding. In
       particular, EncodeIDBKeyPath always encodes in a format that stores
       the type, whereas DecodeIDBKeyPath still supports an old format that
       assumed the type was string.
    
    Fixed: 432522612
    Change-Id: I1da554134ad8a0a8c33b8b5508494c0036c1fede
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6777223
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1490880}

--- content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkeypath_fuzzer.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkeypath_fuzzer.cc
@@ -13,14 +13,33 @@
 #include "third_party/blink/public/common/indexeddb/indexeddb_key_path.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  std::string_view key_path_str_view(reinterpret_cast<const char*>(data), size);
+  std::string_view parsed_input(reinterpret_cast<const char*>(data), size);
+  std::string_view input(parsed_input);
   blink::IndexedDBKeyPath indexed_db_key_path;
-  if (content::indexed_db::DecodeIDBKeyPath(&key_path_str_view,
+  if (content::indexed_db::DecodeIDBKeyPath(&parsed_input,
                                             &indexed_db_key_path)) {
     // Ensure that encoding |indexed_db_key_path| produces the same result.
     std::string result;
     content::indexed_db::EncodeIDBKeyPath(indexed_db_key_path, &result);
-    CHECK_EQ(std::string_view(result), key_path_str_view);
+
+    input.remove_suffix(parsed_input.size());
+
+    // There is an old style of encoding where the type is not encoded, and the
+    // keypath is just a string. In this case, when we re-encode, the output
+    // *will* encode the type, and this format takes 6 extra bytes.
+    if (input.size() >= 3 && input[0] != 0 && input[1] != 0) {
+      CHECK_EQ(indexed_db_key_path.type(),
+               blink::mojom::IDBKeyPathType::String);
+      CHECK_EQ(result.size(), input.size() + 6);
+
+      std::string_view reencoded(result);
+      blink::IndexedDBKeyPath redecoded;
+      CHECK(content::indexed_db::DecodeIDBKeyPath(&reencoded, &redecoded));
+      CHECK_EQ(redecoded.string(), indexed_db_key_path.string());
+      return 0;
+    }
+
+    CHECK_EQ(std::string_view(result), input);
   }
   return 0;
 }
