28d491c4c65d9e951ae116a5d674f2be7fe89bf5
Anand Ravi <anandrv@google.com>
2025-07-24 08:05:50 -0700

    [blink] Scale busy-loop duration around input and loading
    The current iteration of field trial shows improvements in input
    responsiveness and page loading due to busy-looping but at the cost of a
    non-trivial increase in power consumption. This change adds a scaling
    factor to busy looping to reduce the amount of busy-looping when not
    handling input or loading.
    
    Bug: 432238927
    Change-Id: Id210ed56fd6bfec947cda7ab7fc71f807551ce05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6761992
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Commit-Queue: Anand Ravi <anandrv@google.com>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491465}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.cc
@@ -60,6 +60,7 @@
 #include "third_party/blink/renderer/platform/scheduler/main_thread/page_scheduler_impl.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/pending_user_input.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/task_type_names.h"
+#include "third_party/blink/renderer/platform/scheduler/main_thread/use_case.h"
 #include "third_party/blink/renderer/platform/scheduler/main_thread/widget_scheduler_impl.h"
 #include "third_party/blink/renderer/platform/scheduler/public/event_loop.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
@@ -177,14 +178,12 @@ BASE_FEATURE_PARAM(base::TimeDelta,
                    base::Milliseconds(2));
 
 void MaybeSetBusyLoop(raw_ptr<base::MessagePump> message_pump,
-                      bool backgrounded) {
+                      double scale_factor) {
   if (!message_pump || !base::FeatureList::IsEnabled(kBusyLoopOnRendererMain)) {
     return;
   }
 
-  base::TimeDelta busy_loop_duration =
-      backgrounded ? base::Microseconds(0) : kBusyLoopTime.Get();
-  message_pump->SetBusyLoop(busy_loop_duration);
+  message_pump->SetBusyLoop(kBusyLoopTime.Get() * scale_factor);
 }
 
 }  // namespace
@@ -194,7 +193,7 @@ MainThreadSchedulerImpl::MainThreadSchedulerImpl(
     : MainThreadSchedulerImpl(sequence_manager.get()) {
   owned_sequence_manager_ = std::move(sequence_manager);
   MaybeSetBusyLoop(main_thread_only().message_pump,
-                   main_thread_only().renderer_backgrounded);
+                   main_thread_only().renderer_backgrounded ? 0. : 1.);
 }
 
 MainThreadSchedulerImpl::MainThreadSchedulerImpl(
@@ -992,7 +991,7 @@ void MainThreadSchedulerImpl::SetRendererBackgrounded(bool backgrounded) {
   base::TimeTicks now = NowTicks();
   main_thread_only().background_status_changed_at = now;
   main_thread_only().metrics_helper.SetRendererBackgrounded(backgrounded, now);
-  MaybeSetBusyLoop(main_thread_only().message_pump, backgrounded);
+  MaybeSetBusyLoop(main_thread_only().message_pump, backgrounded ? 0. : 1.);
 
   UpdatePolicy();
 
@@ -1444,6 +1443,17 @@ void MainThreadSchedulerImpl::UpdatePolicyLocked(UpdateType update_type) {
   } else {
     main_thread_only().current_policy_expiration_time = base::TimeTicks();
   }
+
+  double busy_loop_scale_factor;
+  if (main_thread_only().renderer_backgrounded) {
+    busy_loop_scale_factor = 0.;
+  } else if (main_thread_only().current_use_case != UseCase::kNone ||
+             main_thread_only().blocking_input_expected_soon) {
+    busy_loop_scale_factor = 1.;
+  } else {
+    busy_loop_scale_factor = 0.5;
+  }
+  MaybeSetBusyLoop(main_thread_only().message_pump, busy_loop_scale_factor);
 
   // Avoid prioritizing main thread compositing (e.g., rAF) if it is extremely
   // slow, because that can cause starvation in other task sources.
