bbc9a32ea49d63e3325aeb41f6c58883a9fcad6b
Wenyu Fu <wenyufu@chromium.org>
2025-07-24 16:05:44 -0700

    [FRE] Enable FRE Launch bounds by default
    Enable the fix in crrev.com/c/6762312. As we cannot found ways to
    override task exiting transition, no new changes are planned behind this
    flag.
    
    Bug: 395937830
    Change-Id: Idcea729cddde8c82363df2231811f50090540fc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779010
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491677}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -574,7 +574,7 @@ BASE_FEATURE(kAndroidElegantTextHeight,
 
 BASE_FEATURE(kAndroidFirstRunLaunchBounds,
              "AndroidFirstRunLaunchBounds",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidNativePagesInNewTab,
              "AndroidNativePagesInNewTab",
