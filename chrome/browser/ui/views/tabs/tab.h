d8348a0936a18444258c4323b875df4ee6ecb539
David Sanders <dsanders11@ucsbalum.com>
2025-07-24 16:02:10 -0700

    Remove unnecessary include in tab.h
    Should save ~30 MiB of compiler input size.
    
    There's already a forward declaration for `GlicTabUnderlineView` in this
    file so the include is unnecessary.
    
    Bug: 40318405
    Change-Id: I315083bee0bcae68a952a122f85233237d2d8b90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6782263
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: David Sanders <dsanders11@ucsbalum.com>
    Cr-Commit-Position: refs/heads/main@{#1491671}

--- chrome/browser/ui/views/tabs/tab.h
+++ chrome/browser/ui/views/tabs/tab.h
@@ -11,7 +11,6 @@
 
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/glic/browser_ui/glic_tab_underline_view.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_renderer_data.h"
 #include "chrome/browser/ui/views/tabs/tab_slot_view.h"
