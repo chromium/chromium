2c6cc22cc288dd2cb918f4b546ace618499d6661
Peter Kotwicz <pkotwicz@chromium.org>
2025-07-24 07:54:45 -0700

    Revert "Add user-agent client hints browser tests on Android"
    This reverts commit 24bca9b5ed98d2608a8be12cc68d3b6045355bf5.
    
    Reason for revert:
    Test fails on android-desktop-16-x64-rel-brya-tests bot
    Sample failing bot run: https://ci.chromium.org/ui/p/chrome/builders/ci/android-desktop-16-x64-rel-brya-tests/1317/overview
    
    Original change's description:
    > Add user-agent client hints browser tests on Android
    >
    > In order to better prepare the changes on XR devices related to
    > User-Agent and User-Agent client hints, add some example tests on
    > Android.
    >
    > Change-Id: I7fa2184b77e04477ef6e0c8c69497f342ce8b2fe
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6778002
    > Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    > Commit-Queue: Victor Tan <victortan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1491136}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1dffa8c1c52c9eaf2394ec93fb579b9dc3bdf801
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6780372
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Victor Tan <victortan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1491454}

--- chrome/browser/user_agent/android_user_agent_browsertest.cc
+++ chrome/browser/user_agent/android_user_agent_browsertest.cc
@@ -10,10 +10,7 @@
 #include "chrome/test/base/chrome_test_utils.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
-#include "testing/gmock/include/gmock/gmock.h"
-#include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
-#include "url/origin.h"
 
 // Browser tests that consider ReduceUserAgentAndroidVersionDeviceModel feature
 // enabled.
@@ -80,74 +77,4 @@ IN_PROC_BROWSER_TEST_F(DisableFeatureReduceUserAgentAndroidPlatformBrowserTest,
   // We should not reduce android navigator.platform when feature is disabled.
   EXPECT_EQ(GetPlatform(),
             content::EvalJs(GetActiveWebContents(), "navigator.platform"));
-}
-
-// Verifying User-Agent and User-Agent Client Hints on Android.
-class AndroidUserAgentClientHintsBrowserTest
-    : public ReduceUserAgentAndroidPlatformBrowserTest {
- public:
-  void SetUpOnMainThread() override {
-    AndroidBrowserTest::SetUpOnMainThread();
-    embedded_test_server()->ServeFilesFromSourceDirectory("content/test/data");
-    ASSERT_TRUE(embedded_test_server()->Start());
-  }
-
-  content::EvalJsResult getUserAgentClientHints() {
-    const std::string script = R"(
-      (async () => {
-        const hints = await navigator.userAgentData.getHighEntropyValues([
-          "architecture", "bitness", "formFactors", "fullVersionList",
-          "model", "platformVersion", "uaFullVersion", "wow64"
-        ]);
-        return hints;
-      })()
-    )";
-
-    content::EvalJsResult result =
-        content::EvalJs(GetActiveWebContents(), script);
-    EXPECT_TRUE(result.is_dict());
-    return result;
-  }
-};
-
-IN_PROC_BROWSER_TEST_F(AndroidUserAgentClientHintsBrowserTest,
-                       UserAgentAndUserAgentClientHints) {
-  GURL url = embedded_test_server()->GetURL("/simple_page.html");
-  ASSERT_TRUE(content::NavigateToURL(GetActiveWebContents(), url));
-
-  // 1. Verify the User Agent string.
-  std::string user_agent =
-      content::EvalJs(GetActiveWebContents(), "navigator.userAgent")
-          .ExtractString();
-  EXPECT_THAT(user_agent, testing::HasSubstr("Linux; Android 10; K"));
-
-  // 2. Verify User Agent Client Hints.
-  content::EvalJsResult result = getUserAgentClientHints();
-  const base::Value::Dict& hints = result.ExtractDict();
-
-  // Verify low-entropy client hints.
-  EXPECT_FALSE(hints.Find("brands")->GetList().empty());
-  EXPECT_EQ(hints.Find("platform")->GetString(), "Android");
-  EXPECT_TRUE(hints.Find("mobile")->is_bool());
-
-  // Verify high-entropy client hints.
-  EXPECT_EQ(hints.Find("architecture")->GetString(), "");
-  EXPECT_EQ(hints.Find("bitness")->GetString(), "");
-  EXPECT_FALSE(hints.Find("fullVersionList")->GetList().empty());
-  EXPECT_FALSE(hints.Find("model")->GetString().empty());
-  EXPECT_FALSE(hints.Find("platformVersion")->GetString().empty());
-  EXPECT_FALSE(hints.Find("uaFullVersion")->GetString().empty());
-  EXPECT_FALSE(hints.Find("wow64")->GetBool());
-
-  std::vector<std::string> form_factors;
-  for (const auto& form_factor : hints.Find("formFactors")->GetList()) {
-    if (form_factor.is_string()) {
-      form_factors.push_back(form_factor.GetString());
-    }
-  }
-  EXPECT_EQ(form_factors.size(), 1u);
-  // Expects the default "Desktop" or "Mobile" form factor, it depends on
-  // whether it turns on the command lined switch `kUseMobileUserAgent`.
-  EXPECT_THAT(form_factors[0],
-              testing::AnyOf(testing::Eq("Mobile"), testing::Eq("Desktop")));
 }
