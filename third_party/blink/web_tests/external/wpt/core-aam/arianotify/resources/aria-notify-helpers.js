f1cd331a8d53d0b19461bc94e93c29bb90604022
Jonathan Lee <jonathanjlee@google.com>
2025-07-25 16:05:30 -0700

    Import wpt@950a214d5871ffbf1d38e18b9b48d41dac5bfcf4
    https://github.com/web-platform-tests/wpt/compare/978340f53...950a214d5
    
    Using wpt-import in Chromium c2ed9ed87b6a7a3c82c08a7f5905c9f55eeb2359.
    With Chromium commits locally applied on WPT:
      926c3b2c1e "WPT for lost-capture removing a pending capture [...]
      a7ebbbf11a "Fix timeout in mouse_wheel test"
      224828f907 "Remove <titles> from DBSC WPTs"
      9875942138 "Add step-by-step instructions to cache usage [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    daniec@microsoft.com, siliu@microsoft.com, snianu@microsoft.com:
      external/wpt/editing
    drott@chromium.org:
      external/wpt/css/css-text-decor
    meade@chromium.org:
      external/wpt/web-animations
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: If07f951b338032b4989313fe92ca0caba2eb2d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788870
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492340}

--- third_party/blink/web_tests/external/wpt/core-aam/arianotify/resources/aria-notify-helpers.js
+++
@@ -1,9 +0,0 @@
-// Helper for ariaNotify manual tests.
-function tryCallAriaNotify(element, message, options = {}) {
-  if (element.ariaNotify) {
-    element.ariaNotify(message, options);
-    return 'ariaNotify called' + ` with message: "${message}" and options: ${JSON.stringify(options)}`;
-  } else {
-    return 'the ariaNotify API is not supported in this browser';
-  }
-}
