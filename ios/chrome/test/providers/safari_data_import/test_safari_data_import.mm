13741c937357850ac59631280a757d7e61dd7537
ginnnnnnny <ginnyhuang@chromium.org>
2025-07-25 12:46:09 -0700

    [iOS][safari-data-import] Public interface to open Settings app
    This CL declares the function to open Settings app from the Safari
    data import workflow, so that the user could export their data there.
    Implementation is in crrev.com/i/8452219.
    
    Bug: 420703283
    Change-Id: Iae10bf9a3b5642e6e6467450af0222f9a1b00ad3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786906
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492247}

---
+++ ios/chrome/test/providers/safari_data_import/test_safari_data_import.mm
@@ -0,0 +1,13 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <UIKit/UIKit.h>
+
+#import "ios/public/provider/chrome/browser/safari_data_import/safari_data_import_api.h"
+
+namespace ios {
+namespace provider {
+void OpenSettingsToExportDataFromSafari() {}
+}  // namespace provider
+}  // namespace ios
