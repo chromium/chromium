e70a5aa35c68b5edd6136c560b6aea90e6b573fa
Tomek Jurkiewicz <tju@google.com>
2025-07-25 10:50:56 -0700

    Check if content filter observers are noop when disabled
    Asserts that no callbacks are invoked with the feature disabled.
    
    Bug: 428154611
    Change-Id: I972a4b101fd966c63d67e8e4b487f09f12d3957b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787212
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Auto-Submit: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492177}

---
+++ components/supervised_user/core/browser/android/content_filters_observer_bridge_unittest.cc
@@ -0,0 +1,36 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/supervised_user/core/browser/android/content_filters_observer_bridge.h"
+
+#include "base/test/scoped_feature_list.h"
+#include "components/supervised_user/core/common/features.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace supervised_user {
+namespace {
+
+class ContentFiltersObserverBridgeTest : public testing::Test {};
+
+TEST_F(ContentFiltersObserverBridgeTest,
+       WithFeatureDisabledCallbacksAreNotCalled) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      kPropagateDeviceContentFiltersToSupervisedUser);
+
+  ContentFiltersObserverBridge bridge(
+      "test_setting", base::BindRepeating([]() {
+        CHECK(false) << "Callback called when feature is disabled";
+      }),
+      base::BindRepeating([]() {
+        CHECK(false) << "Callback called when feature is disabled";
+      }));
+
+  bridge.Init();
+  bridge.Shutdown();
+}
+
+}  // namespace
+}  // namespace supervised_user
