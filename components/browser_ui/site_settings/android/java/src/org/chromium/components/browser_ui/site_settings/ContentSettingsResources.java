e25c8de58bd9694d312c71b7e1aa47884f8b7db8
Phil Yan <philyan@chromium.org>
2025-07-25 12:47:20 -0700

    Add assets needed for auto pip permission on Android
    UX review: https://crbug.com/421606013#comment3
    
    Bug: 421606013
    Change-Id: Ic53164f0db9de56678a2a4e8105ff6a1e99e5dd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6782510
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Phil Yan <philyan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492249}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/ContentSettingsResources.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/ContentSettingsResources.java
@@ -30,8 +30,8 @@ import org.chromium.device.DeviceFeatureList;
 import org.chromium.device.DeviceFeatureMap;
 
 /**
- * A class with utility functions that get the appropriate string and icon resources for the
- * Android UI that allows managing content settings.
+ * A class with utility functions that get the appropriate string and icon resources for the Android
+ * UI that allows managing content settings.
  */
 // The Linter suggests using SparseArray<ResourceItem> instead of a HashMap
 // because our key is an int but we're changing the key to a string soon so
@@ -210,6 +210,19 @@ public class ContentSettingsResources {
                         0,
                         0,
                         0);
+
+            case ContentSettingsType.AUTO_PICTURE_IN_PICTURE:
+                return new ResourceItem(
+                        R.drawable.picture_in_picture_24px,
+                        R.string.auto_picture_in_picture_permission_title,
+                        ContentSettingValues.ALLOW,
+                        ContentSettingValues.BLOCK,
+                        R.string.website_settings_category_automatic_picture_in_picture_allowed,
+                        R.string.website_settings_category_automatic_picture_in_picture_blocked,
+                        R.string.website_settings_category_automatic_picture_in_picture_a11y,
+                        R.drawable.picture_in_picture_off_24px,
+                        R.string.website_settings_automatic_picture_in_picture_allow,
+                        R.string.website_settings_automatic_picture_in_picture_block);
 
             case ContentSettingsType.BACKGROUND_SYNC:
                 return new ResourceItem(
@@ -707,8 +720,8 @@ public class ContentSettingsResources {
     }
 
     /**
-     * @return A {@link Drawable} that is the blocked version of the square icon passed in.
-     *         Achieved by adding a diagonal strike through the icon.
+     * @return A {@link Drawable} that is the blocked version of the square icon passed in. Achieved
+     *     by adding a diagonal strike through the icon.
      */
     private static @Nullable Drawable getBlockedSquareIcon(Resources resources, Drawable icon) {
         if (icon == null) return null;
@@ -879,8 +892,8 @@ public class ContentSettingsResources {
     }
 
     /**
-     * Returns the summary for Cookie content settings when it is allowed
-     * except for those from third party sources.
+     * Returns the summary for Cookie content settings when it is allowed except for those from
+     * third party sources.
      */
     public static int getCookieAllowedExceptThirdPartySummary() {
         return R.string.website_settings_category_allowed_except_third_party;
