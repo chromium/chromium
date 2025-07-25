754818cab4dbc83365aba41f402689f5e3c3578b
Wenyu Fu <wenyufu@chromium.org>
2025-07-25 11:51:37 -0700

    Revert "[FRE] Enable FRE Launch bounds by default"
    This reverts commit bbc9a32ea49d63e3325aeb41f6c58883a9fcad6b.
    
    Reason for revert: Regression on crbug.com/434139164
    
    Bug: 395937830, 434139164
    Original change's description:
    > [FRE] Enable FRE Launch bounds by default
    >
    > Enable the fix in crrev.com/c/6762312. As we cannot found ways to
    > override task exiting transition, no new changes are planned behind this
    > flag.
    >
    > Bug: 395937830
    > Change-Id: Idcea729cddde8c82363df2231811f50090540fc1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6779010
    > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    > Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    > Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1491677}
    
    Bug: 395937830
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1feff178d5b3dba4437604c11940c2eee1771964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6788792
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1492217}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -575,7 +575,7 @@ BASE_FEATURE(kAndroidElegantTextHeight,
 
 BASE_FEATURE(kAndroidFirstRunLaunchBounds,
              "AndroidFirstRunLaunchBounds",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidNativePagesInNewTab,
              "AndroidNativePagesInNewTab",
