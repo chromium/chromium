0db13e2e76cc24b2414d60fbd9484cf5b3d80930
Michael Ludwig <michaelludwig@google.com>
2025-07-25 07:02:39 -0700

    [skia] Add staging for blur flickering improvements
    Change-Id: I9e1b265b8b9e7c352ff941b0bb6a83ce41f8a1f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786268
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Auto-Submit: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492028}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -141,6 +141,8 @@ SK_API void SkDebugf_FileLine(const char* file,
 
 #define SK_DISABLE_CLIP_DRAW_GEOMETRIC_INTERSECTION
 
+#define SK_DISABLE_BLUR_OVERSCALING
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
