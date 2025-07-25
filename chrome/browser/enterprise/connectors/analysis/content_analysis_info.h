b5ef0a9638c6f8688b5560ea7cef1fcf1cd5d3b8
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-07-25 11:11:50 -0700

    Create ContentAnalysisInfoBase
    This new base class will allow a platform-agnostic interface to access
    content analysis information, which will be used to refactor URLF active
    user detection code and corresponding reporting code.
    
    Change-Id: I8f276165626118909458595f62a97989d6260307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6786829
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1492191}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_info.h
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_info.h
@@ -7,44 +7,15 @@
 
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.h"
-#include "components/enterprise/common/proto/connectors.pb.h"
-#include "components/enterprise/connectors/core/analysis_settings.h"
+#include "components/enterprise/connectors/core/content_analysis_info_base.h"
 #include "components/safe_browsing/core/common/proto/csd.pb.h"
 #include "content/public/browser/clipboard_types.h"
 
-namespace signin {
-class IdentityManager;
-}  // namespace signin
-
 namespace enterprise_connectors {
 
-// Interface providing data about a given content analysis action. This should
-// be used as an abstraction layer to access information about some content
-// analysis context when the exact action that triggered is not important (ex.
-// when populating protos).
-class ContentAnalysisInfo {
+// Implementation of `ContentAnalysisInfoBase` for chrome/ platforms.
+class ContentAnalysisInfo : public ContentAnalysisInfoBase {
  public:
-  // The `AnalysisSettings` that should be applied to the content analysis scan.
-  virtual const AnalysisSettings& settings() const = 0;
-
-  // The `signin::IdentityManager` that corresponds to the browser context where
-  // content analysis is taking place.
-  virtual signin::IdentityManager* identity_manager() const = 0;
-
-  // These methods correspond to fields in `BinaryUploadService::Request`.
-  virtual int user_action_requests_count() const = 0;
-  virtual std::string tab_title() const = 0;
-  virtual std::string user_action_id() const = 0;
-  virtual std::string email() const = 0;
-  virtual std::string url() const = 0;
-  virtual const GURL& tab_url() const = 0;
-  virtual ContentAnalysisRequest::Reason reason() const = 0;
-  virtual google::protobuf::RepeatedPtrField<
-      ::safe_browsing::ReferrerChainEntry>
-  referrer_chain() const = 0;
-  virtual google::protobuf::RepeatedPtrField<std::string> frame_url_chain()
-      const = 0;
-
   // Adds shared fields to `request` before sending it to the binary upload
   // service. Connector-specific fields need to be added to the request
   // separately.
