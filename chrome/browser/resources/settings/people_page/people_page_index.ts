04516729ce4b75a3d60a71e15be03ce2ac3ff78f
dpapad <dpapad@chromium.org>
2025-07-28 13:47:23 -0700

    Settings: Correctly show signout dialog from chrome://settings/syncSetup
    Since the signout dialog resides within settings-people-page, need to
    switch to it when routes.SIGN_OUT route is encountered, otherwise the
    dialog will not be visible.
    
    Fixed: 434133823
    Change-Id: Ie380c5c30aff44576adf8eeeb873efcd545f4cd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6787976
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493058}

--- chrome/browser/resources/settings/people_page/people_page_index.ts
+++ chrome/browser/resources/settings/people_page/people_page_index.ts
@@ -85,6 +85,12 @@ export class SettingsPeoplePageIndexElement extends
               'syncControls', 'no-animation', 'no-animation');
           break;
         // <if expr="not is_chromeos">
+        case routes.SIGN_OUT:
+          // Switch to settings-people-page since the sign out dialog resides
+          // there, otherwise it will not be visible even if open.
+          this.$.viewManager.switchView(
+              'parent', 'no-animation', 'no-animation');
+          break;
         case routes.ACCOUNT:
           assert(this.shouldShowAccountSettingsPage_);
           this.$.viewManager.switchView(
