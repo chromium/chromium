a6fbb461fbe3f28e30ffc7fdaac36fd57d27047d
Darren Wu <darrenwu@google.com>
2025-07-28 08:55:00 -0700

    pgo: add android-desktop-arm64
    The PGO android-desktop-arm64 is produced and ready in gs bucket. Create
    the first version of android-desktop-arm64.pgo.txt. Add
    android-desktop-arm64 to update_pgo_profiles.py
    
    Bug: 415130383
    Test: python tools/update_pgo_profiles.py --target android-desktop-arm64 update --gs-url-base chromium-optimization-profiles/pgo_profiles
    Change-Id: If220143cb588e85e68d5f438711450159aacc197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6772951
    Auto-Submit: Darren Wu <darrenwu@chromium.org>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Jeff Yoon <jeffyoon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1492869}

--- tools/update_pgo_profiles.py
+++ tools/update_pgo_profiles.py
@@ -155,6 +155,7 @@ def main():
           'linux',
           'android-arm32',
           'android-arm64',
+          'android-desktop-arm64',
           'android-desktop-x64',
       ],
       help='Identifier of a specific target platform + architecture.')
