8c3bebb3ae5f7d7c7472bbf4120444f47402ced9
Zijie He <zijiehe@google.com>
2025-07-28 15:25:50 -0700

    Release FrameResource::scoped_mapping on MediaThread
    Crash stack when DCHECK is enabled, e.g. https://crrev.com/i/8394026.
    Or in short, ClientNativePixmapFuchsia expects the Unmap call to
    happen in the same thread as Map call.
    
    More details can be found at http://crbug.com/434746980.
    
    The CL needs to be tested in src-internal.
    
    Cq-Include-Trybots: luci.chrome.try:fuchsia-fyi-sherlock
    Bug: 425745937
    Fixed: 434746980
    Change-Id: I2ee913d53a3f0d02b7e0e0f7e9bfcd4fad1cf8f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792984
    Commit-Queue: Zijie He <zijiehe@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493137}

--- media/video/gpu_memory_buffer_video_frame_pool.cc
+++ media/video/gpu_memory_buffer_video_frame_pool.cc
@@ -789,10 +789,6 @@ void GpuMemoryBufferVideoFramePool::PoolImpl::OnCopiesDone(
     bool copy_failed,
     scoped_refptr<VideoFrame> video_frame,
     FrameResource* frame_resource) {
-  if (!copy_failed && frame_resource->scoped_mapping) {
-    frame_resource->scoped_mapping.reset();
-  }
-
   TRACE_EVENT_NESTABLE_ASYNC_END0(
       "media", "CopyVideoFrameToGpuMemoryBuffer",
       TRACE_ID_WITH_SCOPE("CopyVideoFrameToGpuMemoryBuffer",
@@ -978,6 +974,9 @@ void GpuMemoryBufferVideoFramePool::PoolImpl::OnCopiesDoneOnMediaThread(
     scoped_refptr<VideoFrame> video_frame,
     FrameResource* frame_resource) {
   DCHECK(media_task_runner_->RunsTasksInCurrentSequence());
+  if (frame_resource->scoped_mapping) {
+    frame_resource->scoped_mapping.reset();
+  }
   if (copy_failed) {
     // Drop the resource if there was an error with it. If we're not in
     // shutdown we also need to remove the pool entry for the resource.
