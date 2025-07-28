aaef449066938e35381245330400063a9a07dd8f
Fiona Macintosh <fmacintosh@google.com>
2025-07-28 16:03:57 -0700

    [UB] Check if a dialog is showing when UB loses focus
    Bug: b:433471612
    Change-Id: Ie434f885776b3a003176a166d1ebe13d98a3068d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6792587
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Michelle Abreo <michelleabreo@google.com>
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1493168}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.cc
@@ -12,6 +12,7 @@
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_content_view.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/javascript_dialogs/app_modal_dialog_queue.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -143,6 +144,12 @@ bool CookieControlsBubbleViewImpl::OnCloseRequested(
   // will automatically close when the page has loaded.
   if (GetReloadingView()->GetVisible() ||
       GetContentView()->GetTrackingProtectionsButton()->GetSpinnerVisible()) {
+    // Always close the bubble if a JS dialog is being shown.
+    if (auto* app_modal_queue =
+            javascript_dialogs::AppModalDialogQueue::GetInstance();
+        app_modal_queue && app_modal_queue->HasActiveDialog()) {
+      return true;
+    }
     return close_reason != views::Widget::ClosedReason::kLostFocus;
   }
 
