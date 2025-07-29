8bda8717abc4df63b69003c1746b8cd6c4a18c8c
dpapad <dpapad@chromium.org>
2025-07-29 13:38:48 -0700

    Settings: Add SearchableViewContainerMixin 'no-search' tests.
    SearchableViewContainerMixin already respected 'no-search' attributes,
    but support was not explicitly tested.
    
    'no-search' will be leveraged in upcoming People section CLs where some
    subpages don't always apply even if they exist in the DOM and therefore
    shouldn't be searched when not applicable.
    
    Fixed: 434659233
    Change-Id: I792ac397c709bd58b8f70e6c26bb06bb3d701abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6795227
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493770}

--- chrome/test/data/webui/settings/searchable_view_container_mixin_test.ts
+++ chrome/test/data/webui/settings/searchable_view_container_mixin_test.ts
@@ -5,6 +5,7 @@
 import {html, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {Router, routes, SearchableViewContainerMixin, SettingsViewMixin} from 'chrome://settings/settings.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {isVisible} from 'chrome://webui-test/test_util.js';
 
 suite('SearchableViewContainerMixin', function() {
   const TestParentViewElementBase = SettingsViewMixin(PolymerElement);
@@ -37,23 +38,32 @@ suite('SearchableViewContainerMixin', function() {
 
     static get template() {
       return html`
+        <style>
+          cr-view-manager [hidden-by-search],
+          cr-view-manager[show-all] [slot=view][data-parent-view-id] {
+            display: none;
+          }
+          cr-view-manager [slot=view]:not(.closing) {
+            position: initial;
+          }
+        </style>
         <div id="container" scrollable>
           <cr-view-manager show-all$="[[shouldShowAll]]">
+            <test-parent-view slot="view" id="parentView0">
+              <span>ParentView0</span>
+              <button data-child-trigger="childView0"></button>
+            </test-parent-view>
+
             <test-parent-view slot="view" id="parentView1">
               <span>ParentView1</span>
               <button data-child-trigger="childView1"></button>
             </test-parent-view>
 
-            <test-parent-view slot="view" id="parentView2">
-              <span>ParentView2</span>
-              <button data-child-trigger="childView2"></button>
-            </test-parent-view>
-
+            <div slot="view" id="childView0" data-parent-view-id="parentView0">
+              ChildView0
+            </div>
             <div slot="view" id="childView1" data-parent-view-id="parentView1">
               ChildView1
-            </div>
-            <div slot="view" id="childView2" data-parent-view-id="parentView2">
-              ChildView2
             </div>
           </cr-view-manager>
         </div>
@@ -107,6 +117,7 @@ suite('SearchableViewContainerMixin', function() {
         parentVisible: [boolean, boolean], childHasHits: [boolean, boolean]) {
       // Check parent views hidden-by-search attribute.
       for (let i = 0; i < parentVisible.length; i++) {
+        assertEquals(parentVisible[i], isVisible(parentViews[i]!));
         assertEquals(
             parentVisible[i],
             !parentViews[i]!.hasAttribute('hidden-by-search'));
@@ -129,56 +140,58 @@ suite('SearchableViewContainerMixin', function() {
       }
     }
 
-    // Case1: Search results exist only in #parentView1.
-    let result = await testElement.searchContents('ParentView1');
+    testElement.inSearchMode = true;
+
+    // Case1: Search results exist only in #parentView0.
+    let result = await testElement.searchContents('ParentView0');
     assertFalse(result.canceled);
     assertEquals(1, result.matchCount);
     assertFalse(result.wasClearSearch);
     assertDomState([true, false], [false, false]);
 
-    // Case2: Search results exist only in #parentView2.
-    result = await testElement.searchContents('ParentView2');
+    // Case2: Search results exist only in #parentView1.
+    result = await testElement.searchContents('ParentView1');
     assertFalse(result.canceled);
     assertEquals(1, result.matchCount);
     assertFalse(result.wasClearSearch);
     assertDomState([false, true], [false, false]);
 
-    // Case3: Search results exist in both #parentView1, #parentView2.
+    // Case3: Search results exist in both #parentView0, #parentView1.
     result = await testElement.searchContents('ParentView');
     assertFalse(result.canceled);
     assertEquals(2, result.matchCount);
     assertFalse(result.wasClearSearch);
     assertDomState([true, true], [false, false]);
 
-    // Case4: Search results exist in both #parentView1, #childView1.
+    // Case4: Search results exist in both #parentView0, #childView0.
+    result = await testElement.searchContents('View0');
+    assertFalse(result.canceled);
+    assertEquals(2, result.matchCount);
+    assertFalse(result.wasClearSearch);
+    assertDomState([true, false], [true, false]);
+
+    // Case5: Search results exist only #childView0.
+    result = await testElement.searchContents('ChildView0');
+    assertFalse(result.canceled);
+    assertEquals(1, result.matchCount);
+    assertFalse(result.wasClearSearch);
+    assertDomState([true, false], [true, false]);
+
+    // Case6: Search results exist in both #parentView1, #childView1.
     result = await testElement.searchContents('View1');
     assertFalse(result.canceled);
     assertEquals(2, result.matchCount);
     assertFalse(result.wasClearSearch);
-    assertDomState([true, false], [true, false]);
+    assertDomState([false, true], [false, true]);
 
-    // Case5: Search results exist only #childView1.
+    // Case7: Search results exist only #childView1.
     result = await testElement.searchContents('ChildView1');
     assertFalse(result.canceled);
     assertEquals(1, result.matchCount);
     assertFalse(result.wasClearSearch);
-    assertDomState([true, false], [true, false]);
-
-    // Case6: Search results exist in both #parentView2, #childView2.
-    result = await testElement.searchContents('View2');
-    assertFalse(result.canceled);
-    assertEquals(2, result.matchCount);
-    assertFalse(result.wasClearSearch);
     assertDomState([false, true], [false, true]);
 
-    // Case7: Search results exist only #childView2.
-    result = await testElement.searchContents('ChildView2');
-    assertFalse(result.canceled);
-    assertEquals(1, result.matchCount);
-    assertFalse(result.wasClearSearch);
-    assertDomState([false, true], [false, true]);
-
-    // Case8: Search results exist only in #childView1, #childView2.
+    // Case8: Search results exist only in #childView0, #childView1.
     result = await testElement.searchContents('ChildView');
     assertFalse(result.canceled);
     assertEquals(2, result.matchCount);
@@ -191,5 +204,23 @@ suite('SearchableViewContainerMixin', function() {
     assertEquals(0, result.matchCount);
     assertTrue(result.wasClearSearch);
     assertDomState([true, true], [false, false]);
+
+    // Case10: childView0 has 'no-search'.
+    childViews[0]!.toggleAttribute('no-search', true);
+    result = await testElement.searchContents('ChildView');
+    assertFalse(result.canceled);
+    assertEquals(1, result.matchCount);
+    assertFalse(result.wasClearSearch);
+    assertDomState([false, true], [false, true]);
+
+    // Case11: childView1 also has 'no-search'.
+    childViews[1]!.toggleAttribute('no-search', true);
+    // Clear search first and then search again for 'ChildView'.
+    await testElement.searchContents('');
+    result = await testElement.searchContents('ChildView');
+    assertFalse(result.canceled);
+    assertEquals(0, result.matchCount);
+    assertFalse(result.wasClearSearch);
+    assertDomState([false, false], [false, false]);
   });
 });
