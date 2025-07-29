26d26bce92e246299e4dced068230a0edea80798
Ho Cheung <hocheung@chromium.org>
2025-07-29 09:18:12 -0700

    Add test coverage for DevUI external Activity launch failures
    Adds a test case to verify DevUI handles ActivityNotFoundException
    properly when external activities cannot be started. The test stubs
    external intents to throw ActivityNotFoundException and verifies that
    an appropriate error dialog is shown to the user.
    
    Bug: 40904567
    Change-Id: I61e97749fc2c86b817cf33fc0f06fbf8039acca0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6796290
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Ho Cheung <hocheung@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493588}

--- android_webview/javatests/src/org/chromium/android_webview/test/devui/DeveloperUiTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/devui/DeveloperUiTest.java
@@ -13,6 +13,8 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.assertNoUnverifiedIntents;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.intent.matcher.UriMatchers.hasHost;
 import static androidx.test.espresso.intent.matcher.UriMatchers.hasParamWithValue;
 import static androidx.test.espresso.intent.matcher.UriMatchers.hasPath;
@@ -31,6 +33,7 @@ import static org.junit.Assert.assertTrue;
 
 import android.app.Activity;
 import android.app.Instrumentation.ActivityResult;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
@@ -332,6 +335,34 @@ public class DeveloperUiTest {
         onView(withText("SafeMode status")).perform(click());
 
         onView(withId(R.id.fragment_safe_mode)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"AndroidWebView"})
+    public void testMenuOptions_reportBug_activityNotFound() throws Throwable {
+        launchHomeFragment();
+
+        // Stub external intents to throw ActivityNotFoundException.
+        intending(hasAction(Intent.ACTION_VIEW))
+                .respondWithFunction(
+                        intent -> {
+                            throw new ActivityNotFoundException();
+                        });
+
+        openOptionsMenu();
+
+        onView(withText("Report WebView Bug")).check(matches(isDisplayed())).perform(click());
+
+        // Verify that the error dialog is shown.
+        onView(withText("Can't find a browser to open URL")).check(matches(isDisplayed()));
+        onView(withText("OK")).check(matches(isDisplayed())).perform(click());
+
+        intended(
+                allOf(
+                        hasAction(Intent.ACTION_VIEW),
+                        hasData(hasScheme("https")),
+                        hasData(hasHost("issues.chromium.org"))));
     }
 
     private void switchToFlagsUi() {
