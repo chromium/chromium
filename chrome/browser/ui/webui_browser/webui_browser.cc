be78d952891beef1e12be0ab219a4d57218d3a57
Keren Zhu <kerenzhu@chromium.org>
2025-07-28 22:30:41 -0700

    webium: use WebUIBrowserWindow when the feature is enabled
    Bug: 393144165
    Change-Id: I438d196d544bb8b8328a2e1aefe7fe06f4ae2caa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6785521
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1493293}

--- chrome/browser/ui/webui_browser/webui_browser.cc
+++ chrome/browser/ui/webui_browser/webui_browser.cc
@@ -4,10 +4,13 @@
 
 #include "chrome/browser/ui/webui_browser/webui_browser.h"
 
+#include "base/feature_list.h"
+#include "chrome/common/chrome_features.h"
+
 namespace webui_browser {
 
 bool IsWebUIBrowserEnabled() {
-  return false;
+  return base::FeatureList::IsEnabled(features::kWebium);
 }
 
 }  // namespace webui_browser
