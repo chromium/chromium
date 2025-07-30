1842cacc62df215320223d5873a739530496ad61
Sylvain Defresne <sdefresne@chromium.org>
2025-07-30 06:21:14 -0700

    [ios] Fix a crash in DataSharingTabHelper
    Taking a policy decision to cancel a navigation may result in the
    destruction of the WebState which would result in a crash because
    DataSharingTabHelper would have forgotten about the WebState.
    
    Fix this by using a base::WeakPtr<web::WebState> to ensure that
    the code only try to close the WebState if it has not already
    been destroyed.
    
    Fixed: 434172545
    Change-Id: I5f19628c39b6baac1b081da2f93f7fca4d1bd1a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6799320
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494134}

--- ios/chrome/browser/collaboration/model/data_sharing_tab_helper.mm
+++ ios/chrome/browser/collaboration/model/data_sharing_tab_helper.mm
@@ -5,6 +5,8 @@
 #import "ios/chrome/browser/collaboration/model/data_sharing_tab_helper.h"
 
 #import "base/check.h"
+#import "base/functional/bind.h"
+#import "base/functional/callback.h"
 #import "components/collaboration/public/collaboration_flow_entry_point.h"
 #import "components/collaboration/public/collaboration_service.h"
 #import "components/data_sharing/public/data_sharing_utils.h"
@@ -35,6 +37,26 @@ bool ShouldHandleShareURLNavigation(
 
   return true;
   // LINT.ThenChange(/chrome/browser/data_sharing/data_sharing_navigation_throttle.cc:ShouldHandleShareURLNavigation)
+}
+
+// Closes `weak_web_state` if the url interception ended up with an empty page.
+void CloseWebStateIfEmptyPage(base::WeakPtr<web::WebState> weak_web_state) {
+  web::WebState* web_state = weak_web_state.get();
+  if (!web_state || web_state->IsBeingDestroyed()) {
+    return;
+  }
+
+  const GURL& url = web_state->GetLastCommittedURL();
+  if (!url.is_valid() || url.IsAboutBlank() || url.is_empty()) {
+    // This will destroy the WebState, so the `web_state` and `url` will
+    // become invalid after this line and they must not be used anymore.
+    return web_state->CloseWebState();
+  }
+}
+
+// Returns a closure that invokes `CloseWebStateIfEmptyPage` with `web_state`.
+base::OnceClosure CloseWebStateIfEmptyPageClosure(web::WebState* web_state) {
+  return base::BindOnce(&CloseWebStateIfEmptyPage, web_state->GetWeakPtr());
 }
 
 }  // namespace
@@ -86,14 +108,17 @@ void DataSharingTabHelper::ShouldAllowRequest(
           collaboration::GetEntryPointFromPageTransition(
               request_info.transition_type));
     }
-    std::move(callback).Run(PolicyDecision::Cancel());
 
-    // Close the tab if the url interception ends with an empty page.
-    const GURL& last_committed_url = web_state()->GetLastCommittedURL();
-    if (!last_committed_url.is_valid() || last_committed_url.IsAboutBlank() ||
-        last_committed_url.is_empty()) {
-      web_state()->CloseWebState();
-    }
+    // Invoking `callback` may destroy the WebState and thus the current
+    // object. To avoid a crash (see https://crbug.com/434172545 for more
+    // information) create a callback that will try to close the WebState
+    // (if it still exists) and chain it with `callback`.
+    base::BindOnce(std::move(callback), PolicyDecision::Cancel())
+        .Then(CloseWebStateIfEmptyPageClosure(current_web_state))
+        .Run();
+
+    // The current object may have been destroyed, nothing should happen
+    // now (except returning from the current method).
     return;
   }
 
