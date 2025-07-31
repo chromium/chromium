6295166b6fe451ceef4ca060889492be064d7b5b
Nidhi Jaju <nidhijaju@chromium.org>
2025-07-31 00:55:33 -0700

    Enable simdutf feature by default on some Desktop platforms
    Based on experiment analysis and launch approvals, this CL enables
    using simdutf for base64 decoding data URLs on Mac, Windows, and Linux.
    
    Bug: 348442535
    Change-Id: Iaf69035f6ed7ace5e8b380e4e2e391818bce38bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6804609
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1494653}

--- net/base/features.cc
+++ net/base/features.cc
@@ -701,7 +701,12 @@ BASE_FEATURE(kReportingApiEnableEnterpriseCookieIssues,
 
 BASE_FEATURE(kSimdutfBase64Support,
              "SimdutfBase64Support",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
+             base::FEATURE_ENABLED_BY_DEFAULT
+#else
+             base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 
 BASE_FEATURE(kFurtherOptimizeParsingDataUrls,
              "FurtherOptimizeParsingDataUrls",
