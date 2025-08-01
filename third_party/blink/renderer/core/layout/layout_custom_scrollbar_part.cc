a906d6330a0799cab789b83b0cbfdb5ec9bdbb54
Fredrik Söderquist <fs@opera.com>
2025-08-01 09:29:13 -0700

    Remove "LayoutReplacedReturnExplicitDefaultNaturalSize" flag
    The change associated with this kill-switch has shipped since M138 with
    no known issues. Remove it.
    
    Fixed: 414738824
    Change-Id: I4e963fab432af5c35ffd5e7aff4de13309989c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6811477
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1495523}

--- third_party/blink/renderer/core/layout/layout_custom_scrollbar_part.cc
+++ third_party/blink/renderer/core/layout/layout_custom_scrollbar_part.cc
@@ -193,16 +193,6 @@ PhysicalSize LayoutCustomScrollbarPart::Size() const {
 PhysicalNaturalSizingInfo LayoutCustomScrollbarPart::GetNaturalDimensions()
     const {
   NOT_DESTROYED();
-  if (RuntimeEnabledFeatures::
-          LayoutReplacedReturnExplicitDefaultNaturalSizeEnabled()) {
-    // 300x150, no aspect ratio. (Should probably be none.)
-    PhysicalSize natural_size{LayoutUnit(kDefaultWidth),
-                              LayoutUnit(kDefaultHeight)};
-    natural_size.Scale(StyleRef().EffectiveZoom());
-    PhysicalNaturalSizingInfo sizing_info;
-    sizing_info.size = natural_size;
-    return sizing_info;
-  }
   return PhysicalNaturalSizingInfo::None();
 }
 
