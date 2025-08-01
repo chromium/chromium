7818ddd82dc8e21b7f9f121e5c53f21cdddb39d4
anthie@google.com <anthie@google.com>
2025-08-01 05:59:31 -0700

    Enable by default Local Web Approvals on iOS
    Bug: 435189018
    Change-Id: I3350e4c2f0c31e7cb33f3ba4e0d31f8e1320a786
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6800059
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1495382}

--- components/supervised_user/core/common/features.cc
+++ components/supervised_user/core/common/features.cc
@@ -22,7 +22,7 @@ namespace supervised_user {
 // user's device.
 BASE_FEATURE(kLocalWebApprovals,
              "LocalWebApprovals",
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_IOS)
              base::FEATURE_ENABLED_BY_DEFAULT);
 #else
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -60,7 +60,11 @@ BASE_FEATURE(kLocalWebApprovalsWidgetSupportsUrlPayload,
 
 BASE_FEATURE(kSupervisedUserBlockInterstitialV3,
              "SupervisedUserBlockInterstitialV3",
+#if BUILDFLAG(IS_IOS)
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#else
              base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  //  BUILDFLAG(IS_IOS)
 
 bool IsGoogleBrandedBuild() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
