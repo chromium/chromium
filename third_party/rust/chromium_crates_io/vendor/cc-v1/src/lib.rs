7c6104e1f455119cbb2a9318e145dc7b8cb03b10
Manish Goregaokar <manishearth@google.com>
2025-08-04 08:10:33 -0700

    Roll cc: 1.2.30 => 1.2.31 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * cc: 1.2.30 => 1.2.31; https://docs.rs/crate/cc/1.2.31
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: Ibe4f95fe355207c35e3cdb3b89dfee4d57d69fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6815594
    Auto-Submit: Manish Goregaokar <manishearth@google.com>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Manish Goregaokar <manishearth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496347}

--- third_party/rust/chromium_crates_io/vendor/cc-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/cc-v1/src/lib.rs
@@ -6,6 +6,6 @@
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `cc-1.2.30` crate.
+// `cc-1.2.31` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
