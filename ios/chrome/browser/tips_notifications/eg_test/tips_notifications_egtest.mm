5097e7b7e6b8f350fd56f9847ce96be6361e17b2
Scott Yoder <scottyoder@google.com>
2025-08-04 10:59:27 -0700

    [ios] Add IOSOneTimeDefaultBrowserNotification field trial
    Bug: 434666845
    Change-Id: I70b7038d083bc030b50cfcce643ea5a87c84ac95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813749
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496455}

--- ios/chrome/browser/tips_notifications/eg_test/tips_notifications_egtest.mm
+++ ios/chrome/browser/tips_notifications/eg_test/tips_notifications_egtest.mm
@@ -107,6 +107,8 @@ void MaybeDismissNotification() {
     config.features_enabled.push_back(kSeparateProfilesForManagedAccounts);
   }
 
+  config.features_disabled.push_back(kIOSOneTimeDefaultBrowserNotification);
+
   return config;
 }
 
