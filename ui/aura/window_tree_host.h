cf8f6ac8f621763aa3bde92a844b163ad907d004
Mitsuru Oshima <oshima@chromium.org>
2025-08-04 02:44:32 -0700

    Make WindowTreeHostObserver checked observer.
    Bug: None
    
    Change-Id: I66785f143e8fbdf91eb8d0f4d490da35c3f54e5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6813895
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496189}

--- ui/aura/window_tree_host.h
+++ ui/aura/window_tree_host.h
@@ -384,9 +384,7 @@ class AURA_EXPORT WindowTreeHost : public ui::ImeKeyEventDispatcher,
   virtual gfx::Rect GetTransformedRootWindowBoundsFromPixelSize(
       const gfx::Size& size_in_pixels) const;
 
-  base::ObserverList<WindowTreeHostObserver>::Unchecked& observers() {
-    return observers_;
-  }
+  base::ObserverList<WindowTreeHostObserver>& observers() { return observers_; }
 
   // Called to enabled/disable native window occlusion calculation.
   void SetNativeWindowOcclusionEnabled(bool enable);
@@ -461,7 +459,7 @@ class AURA_EXPORT WindowTreeHost : public ui::ImeKeyEventDispatcher,
   // figures this out, so it's cheaper to store the fact here.
   std::optional<bool> on_current_workspace_;
 
-  base::ObserverList<WindowTreeHostObserver>::Unchecked observers_;
+  base::ObserverList<WindowTreeHostObserver> observers_;
 
   display::ScopedDisplayObserver display_observer_{this};
 
