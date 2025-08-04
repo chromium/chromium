6a0fe65d9d369893574648f2c2fdb515595b7380
Manish Goregaokar <manishearth@google.com>
2025-08-04 08:52:53 -0700

    Roll serde_json: 1.0.141 => 1.0.142 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * serde_json: 1.0.141 => 1.0.142;
      https://docs.rs/crate/serde_json/1.0.142
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: Ib14241713c084d2a29187be53dfb7574e89998e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6815625
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Manish Goregaokar <manishearth@google.com>
    Commit-Queue: Manish Goregaokar <manishearth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496375}

--- third_party/rust/chromium_crates_io/vendor/serde_json-v1/src/value/mod.rs
+++ third_party/rust/chromium_crates_io/vendor/serde_json-v1/src/value/mod.rs
@@ -924,6 +924,13 @@ impl Default for Value {
     }
 }
 
+impl Default for &Value {
+    fn default() -> Self {
+        const DEFAULT: Value = Value::Null;
+        &DEFAULT
+    }
+}
+
 mod de;
 mod from;
 mod index;
