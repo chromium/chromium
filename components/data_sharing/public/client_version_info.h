222b6b66f7f23f983ea02c35a1e290a759bca494
Tommy Nyquist <nyquist@chromium.org>
2025-08-04 03:11:04 -0700

    [data_sharing] Expose ClientVersionInfo publicly for group creation
    The `ClientVersionInfo` enum was previously internal and only used when
    reading group data to verify client compatibility. The SDK used to
    create collaboration groups now supports manually setting this version
    field.
    
    This change moves the enum to the public `data_sharing` component API.
    This allows all platforms (iOS, Android, and Desktop) to access the
    value and set it correctly when creating new groups.
    
    Changes:
    - Moved `client_version_info.h` to `components/data_sharing/public/`.
    - Updated `BUILD.gn` files to reflect the new location.
    - Added the `data_sharing` namespace to the `ClientVersionInfo` enum.
    - Enabled Java enum generation for the new public header.
    
    Bug: 378159137, 398253297, 407709367, 407710395
    Change-Id: I371bbaa12dd049d31add391d849fe2cffddd1a70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6817618
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496215}

--- components/data_sharing/public/client_version_info.h
+++ components/data_sharing/public/client_version_info.h
@@ -2,16 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_DATA_SHARING_INTERNAL_CLIENT_VERSION_INFO_H_
-#define COMPONENTS_DATA_SHARING_INTERNAL_CLIENT_VERSION_INFO_H_
+#ifndef COMPONENTS_DATA_SHARING_PUBLIC_CLIENT_VERSION_INFO_H_
+#define COMPONENTS_DATA_SHARING_PUBLIC_CLIENT_VERSION_INFO_H_
+
+namespace data_sharing {
 
 // The version is used to verify if the group data provided by the server
 // is supported in client current version of the code. Update the version
 // number for any new feature added to the proto, and add a log of the new
 // changes in the current version in this file.
 // Version 1 supports collaboration group features create, join and manage.
-enum class ClientVersionInfo {
-    CURRENT_VERSION = 1
-};
+// GENERATED_JAVA_ENUM_PACKAGE: (
+//   org.chromium.components.data_sharing)
+enum class ClientVersionInfo { CURRENT_VERSION = 1 };
 
-#endif  // COMPONENTS_DATA_SHARING_INTERNAL_CLIENT_VERSION_INFO_H_
+}  // namespace data_sharing
+
+#endif  // COMPONENTS_DATA_SHARING_PUBLIC_CLIENT_VERSION_INFO_H_
