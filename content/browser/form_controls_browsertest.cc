64abcf489e3b92e7e3b58217d50b4e1fb357c0ec
Michael Ludwig <michaelludwig@google.com>
2025-08-05 03:37:15 -0700

    Relax additional FormControlsBrowserTest pixel tolerance
    DarkModeTextSelection needs an increase to accept up to 12 for max
    average error per channel.
    
    Bug: 436274262
    Change-Id: I6d882623e4690aab14cf8d835d744e4a1c5a2afc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819160
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Kuan Huang <kuanhuang@chromium.org>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496824}

--- content/browser/form_controls_browsertest.cc
+++ content/browser/form_controls_browsertest.cc
@@ -126,7 +126,7 @@ class FormControlsBrowserTest : public ContentBrowserTest {
     auto comparator = cc::FuzzyPixelComparator()
                           .DiscardAlpha()
                           .SetErrorPixelsPercentageLimit(11.f)
-                          .SetAvgAbsErrorLimit(11.f)
+                          .SetAvgAbsErrorLimit(12.f)
                           .SetAbsErrorLimit(140);
 #else
     cc::AlphaDiscardingExactPixelComparator comparator;
