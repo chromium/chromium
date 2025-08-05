73d9cf69dae01f3fbd4d5f0e46ba71004648813d
Jiawei Shao <jiawei.shao@intel.com>
2025-08-05 00:56:06 -0700

    WebGPU: Use `base::raw_span` in `ReadHandleImpl` and `WriteHandleImpl`
    This patch replaces the raw pointers with `base::raw_span` in
    `ReadHandleImpl` and `WriteHandleImpl` for better code safety and
    readability.
    
    Bug: 42240963
    Change-Id: Ib00e37a5fe8484effae59b8eb7e5241811639b36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6809485
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1496763}

--- gpu/command_buffer/service/dawn_service_memory_transfer_service.cc
+++ gpu/command_buffer/service/dawn_service_memory_transfer_service.cc
@@ -5,7 +5,7 @@
 #include "gpu/command_buffer/service/dawn_service_memory_transfer_service.h"
 
 #include "base/compiler_specific.h"
-#include "base/memory/raw_ptr.h"
+#include "base/memory/raw_span.h"
 #include "gpu/command_buffer/common/dawn_memory_transfer_handle.h"
 #include "gpu/command_buffer/service/command_buffer_service.h"
 #include "gpu/command_buffer/service/common_decoder.h"
@@ -18,8 +18,9 @@ namespace {
 class ReadHandleImpl
     : public dawn::wire::server::MemoryTransferService::ReadHandle {
  public:
-  ReadHandleImpl(scoped_refptr<Buffer> buffer, void* ptr, uint32_t size)
-      : buffer_(std::move(buffer)), ptr_(ptr), size_(size) {}
+  ReadHandleImpl(scoped_refptr<Buffer> buffer,
+                 base::raw_span<uint8_t> buffer_data_view)
+      : buffer_(std::move(buffer)), buffer_data_view_(buffer_data_view) {}
 
   ~ReadHandleImpl() override = default;
 
@@ -37,26 +38,26 @@ class ReadHandleImpl
     // DCHECK, do a CHECK here to crash the release build. The crash is fine
     // since it is not reachable from normal behavior. WebGPU post-V1 will have
     // a refactored API.
-    CHECK_LE(offset, size_);
-    CHECK_LE(size, size_ - offset);
+    CHECK_LE(offset, buffer_data_view_.size());
+    CHECK_LE(size, buffer_data_view_.size() - offset);
     // Copy the data into the shared memory allocation.
     // In the case of buffer mapping, this is the mapped GPU memory which we
     // copy into client-visible shared memory.
-    UNSAFE_TODO(memcpy(static_cast<uint8_t*>(ptr_) + offset, data, size));
+    UNSAFE_TODO(memcpy(buffer_data_view_.data() + offset, data, size));
   }
 
  private:
   scoped_refptr<gpu::Buffer> buffer_;
-  // Pointer to client-visible shared memory owned by buffer_.
-  raw_ptr<void> ptr_;
-  uint32_t size_;
+  // Data view to client-visible shared memory owned by buffer_.
+  base::raw_span<uint8_t> buffer_data_view_;
 };
 
 class WriteHandleImpl
     : public dawn::wire::server::MemoryTransferService::WriteHandle {
  public:
-  WriteHandleImpl(scoped_refptr<Buffer> buffer, const void* ptr, uint32_t size)
-      : buffer_(std::move(buffer)), ptr_(ptr), size_(size) {}
+  WriteHandleImpl(scoped_refptr<Buffer> buffer,
+                  base::raw_span<const uint8_t> buffer_data_view)
+      : buffer_(std::move(buffer)), buffer_data_view_(buffer_data_view) {}
 
   ~WriteHandleImpl() override = default;
 
@@ -68,12 +69,13 @@ class WriteHandleImpl
     // Nothing is serialized because we're using shared memory.
     DCHECK_EQ(deserialize_size, 0u);
     DCHECK(mTargetData);
-    DCHECK(ptr_);
+    DCHECK(buffer_data_view_.data());
 
     if (offset > mDataLength || size > mDataLength - offset) {
       return false;
     }
-    if (offset > size_ || size > size_ - offset) {
+    if (offset > buffer_data_view_.size() ||
+        size > buffer_data_view_.size() - offset) {
       return false;
     }
 
@@ -81,15 +83,14 @@ class WriteHandleImpl
     // mTargetData will always be the starting address
     // of the backing buffer after the dawn side change.
     UNSAFE_TODO(memcpy(static_cast<uint8_t*>(mTargetData) + offset,
-                       static_cast<const uint8_t*>(ptr_) + offset, size));
+                       buffer_data_view_.data() + offset, size));
     return true;
   }
 
  private:
   scoped_refptr<gpu::Buffer> buffer_;
-  // Pointer to client-visible shared memory owned by buffer_.
-  raw_ptr<const void> ptr_;
-  uint32_t size_;
+  // Data view to client-visible shared memory owned by buffer_.
+  base::raw_span<const uint8_t> buffer_data_view_;
 };
 
 }  // namespace
@@ -129,7 +130,12 @@ bool DawnServiceMemoryTransferService::DeserializeReadHandle(
   }
 
   DCHECK(read_handle);
-  *read_handle = new ReadHandleImpl(std::move(buffer), ptr, size);
+  // SAFETY: gpu::Buffer::GetDataAddress() will return a valid pointer only when
+  // `shm_offset + size` is neither overflow nor greater than the total size of
+  // the whole transfer buffer.
+  auto buffer_data_view =
+      UNSAFE_BUFFERS(base::raw_span<uint8_t>(static_cast<uint8_t*>(ptr), size));
+  *read_handle = new ReadHandleImpl(std::move(buffer), buffer_data_view);
 
   return true;
 }
@@ -163,7 +169,12 @@ bool DawnServiceMemoryTransferService::DeserializeWriteHandle(
   }
 
   DCHECK(write_handle);
-  *write_handle = new WriteHandleImpl(std::move(buffer), ptr, size);
+  // SAFETY: gpu::Buffer::GetDataAddress() will return a valid pointer only when
+  // `shm_offset + size` is neither overflow nor greater than the total size of
+  // the whole transfer buffer.
+  auto buffer_data_view = UNSAFE_BUFFERS(
+      base::raw_span<const uint8_t>(static_cast<const uint8_t*>(ptr), size));
+  *write_handle = new WriteHandleImpl(std::move(buffer), buffer_data_view);
 
   return true;
 }
