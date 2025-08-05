a4af7e05b836b2b0100a1869a8aa26400be5ff62
Dominique Fauteux-Chapleau <domfc@chromium.org>
2025-08-05 07:36:53 -0700

    Move DataControlsDialogFactory to content/ sub-directory
    That file seems to have been placed under a core/ sub-directory by
    mistake as it uses content::WebContents.
    
    Change-Id: I67947a9ab33568034bcd4c40c2be978a87281ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819279
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Haihan Chen <haihan@google.com>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1496909}

--- components/enterprise/data_controls/content/browser/data_controls_dialog_factory.cc
+++ components/enterprise/data_controls/content/browser/data_controls_dialog_factory.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/enterprise/data_controls/core/browser/data_controls_dialog_factory.h"
+#include "components/enterprise/data_controls/content/browser/data_controls_dialog_factory.h"
 
 #include <map>
 
