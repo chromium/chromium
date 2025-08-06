625a969d4db103024ab3fd634a75f99ff4974d3d
Nick Birnie <birnie@google.com>
2025-08-06 16:19:16 -0700

    Disable flaky GlicFreControllerUiTest.PressNoThanksButton
    Bug: 436190211
    Change-Id: I53078ae1a83120df409ab2a194424947767760d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825544
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1497918}

--- chrome/browser/glic/fre/glic_fre_controller_interactive_uitest.cc
+++ chrome/browser/glic/fre/glic_fre_controller_interactive_uitest.cc
@@ -228,7 +228,8 @@ IN_PROC_BROWSER_TEST_F(GlicFreControllerUiTest, PreconnectOnButtonHover) {
   EXPECT_EQ(connection_tracker.GetAcceptedSocketCount(), 1u);
 }
 
-IN_PROC_BROWSER_TEST_F(GlicFreControllerUiTest, PressNoThanksButton) {
+// TODO(crbug.com/436190211): Fix and re-enable.
+IN_PROC_BROWSER_TEST_F(GlicFreControllerUiTest, DISABLED_PressNoThanksButton) {
   auto server_running = fre_server().StartAcceptingConnectionsAndReturnHandle();
 
   // Tests that pressing the "No Thanks" button in the FRE closes the FRE
