c352ef816365d2338b55171272472ac235eedf74
Olivier Robin <olivierrobin@google.com>
2025-08-06 14:58:56 -0700

    Fix dino game on Chrome iOS
    audioContext is not set on ios [1] so the assert would fail. This is a
    fix after crrev.com/c/6614817. Note that sound was disabled on iOS to
    support earlier versions of iOS, but this is no longer needed.
    
    [1]https://source.chromium.org/chromium/chromium/src/+/main:components/neterror/resources/dino_game/offline.ts;l=477
    
    Fixed: 436792445
    Change-Id: Iac2713f1efc908a61bccfea271c6d5536789d8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6819029
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1497871}

--- components/neterror/resources/dino_game/offline.ts
+++ components/neterror/resources/dino_game/offline.ts
@@ -1558,11 +1558,13 @@ export class Runner {
    * Play a sound.
    */
   private playSound(soundBuffer?: AudioBuffer) {
-    assert(this.audioContext);
-    const sourceNode = this.audioContext.createBufferSource();
-    sourceNode.buffer = soundBuffer || null;
-    sourceNode.connect(this.audioContext.destination);
-    sourceNode.start(0);
+    if (soundBuffer) {
+      assert(this.audioContext);
+      const sourceNode = this.audioContext.createBufferSource();
+      sourceNode.buffer = soundBuffer || null;
+      sourceNode.connect(this.audioContext.destination);
+      sourceNode.start(0);
+    }
   }
 
   /**
