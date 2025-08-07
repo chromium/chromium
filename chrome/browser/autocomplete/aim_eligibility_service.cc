bef969493f0545b34afa42ae02157f384d006762
manukh <manukh@chromium.org>
2025-08-07 15:05:50 -0700

    [omnibox][ai-mode] Add GWS server request
    This CL makes an HTTP GET request to hardcoded GWS URL:
    http://www.google.com/async/folae?async=_fmt:pb and parses its proto
    response. The response contains bool fields for feature enabled/disabled
    states.
    
    Follow up CLs will trigger this request, save its response, use its
    response to gate features, etc. This CL alone doesn't affect user
    experience.
    
    Low-Coverage-Reason: HARD_TO_TEST service that is mostly wiring with little business logic
    Bug: 436898147, 436225340
    Change-Id: Ic221399472d0b79ed58d5fc4de4aedf7416b36a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6825324
    Reviewed-by: Aiden Chiavatti <chiav@chromium.org>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498490}

--- chrome/browser/autocomplete/aim_eligibility_service.cc
+++ chrome/browser/autocomplete/aim_eligibility_service.cc
@@ -4,15 +4,25 @@
 
 #include "chrome/browser/autocomplete/aim_eligibility_service.h"
 
+#include <memory>
 #include <string>
 
+#include "base/functional/bind.h"
+#include "base/memory/scoped_refptr.h"
 #include "chrome/browser/autocomplete/aim_eligibility_service_observer.h"
 #include "chrome/browser/browser_process.h"
 #include "components/omnibox/browser/omnibox_prefs.h"
 #include "components/prefs/pref_service.h"
 #include "components/search/search.h"
 #include "components/search_engines/template_url_service.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/variations/service/variations_service.h"
+#include "net/traffic_annotation/network_traffic_annotation.h"
+#include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/public/cpp/simple_url_loader.h"
+#include "third_party/omnibox_proto/aim_eligibility_response.pb.h"
+#include "url/gurl.h"
 
 namespace {
 
@@ -31,15 +41,95 @@ std::string GetCountryCode() {
   return country_code;
 }
 
+static constexpr char kGwsRequestEndpoint[] =
+    "http://www.google.com/async/folae?async=_fmt:pb";
+
+const net::NetworkTrafficAnnotationTag kGwsRequestTrafficAnnotation =
+    net::DefineNetworkTrafficAnnotation("aim_eligibility_fetch", R"(
+      semantics {
+        sender: "Chrome AI Mode Eligibility Service"
+        description:
+          "Retrieves the set of AI Mode features the client is eligible for "
+          "from the server."
+        trigger:
+          "Requests are made on startup, when user's profile state changes, "
+          "and periodically while Chrome is running."
+        user_data {
+          type: NONE
+        }
+        data:
+          "No request body is sent; this is a GET request with no query params."
+        destination: GOOGLE_OWNED_SERVICE
+        internal {
+          contacts { email: "chrome-desktop-search@google.com" }
+        }
+        last_reviewed: "2025-08-06"
+      }
+      policy {
+        cookies_allowed: NO
+        setting: "Coupled to Google default search."
+        policy_exception_justification:
+          "Not gated by policy. Setting AIModeSetting to '1' prevents the "
+          "response from being used. But Google Chrome still makes the "
+          "requests and saves the response to disk so that it's available when "
+          "the policy is unset."
+      })");
+
 }  // namespace
 
 AimEligibilityService::AimEligibilityService(
     PrefService* pref_service,
-    TemplateURLService* template_url_service)
+    TemplateURLService* template_url_service,
+    signin::IdentityManager* identity_manager,
+    scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory)
     : pref_service_(pref_service),
-      template_url_service_(template_url_service) {}
+      template_url_service_(template_url_service),
+      url_loader_factory_(url_loader_factory),
+      identity_manager_(identity_manager) {
+  // TODO(crbug.com/436898763): Call `StartGwsRequest()` to refresh the server
+  //   response when service is constructed and when user state changes. E.g.
+  //   user signs in/out, starts/stops syncing, switches profiles. Some of those
+  //   actions may create a new service; if so, we don't need to listen to those
+  //   events and start StartGwsRequest manually, because it'll be called in the
+  //   constructor anyways. Switching profiles probably creates a new service.
+}
 
 AimEligibilityService::~AimEligibilityService() = default;
+
+void AimEligibilityService::StartGwsRequest() {
+  std::unique_ptr<network::ResourceRequest> request =
+      std::make_unique<network::ResourceRequest>();
+  request->url = GURL{kGwsRequestEndpoint};
+  request->credentials_mode = network::mojom::CredentialsMode::kOmit;
+  std::unique_ptr<network::SimpleURLLoader> loader =
+      network::SimpleURLLoader::Create(std::move(request),
+                                       kGwsRequestTrafficAnnotation);
+  loader->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
+      url_loader_factory_.get(),
+      base::BindOnce(&AimEligibilityService::OnGwsResponse,
+                     base::Unretained(this), std::move(loader)));
+}
+
+void AimEligibilityService::OnGwsResponse(
+    std::unique_ptr<network::SimpleURLLoader> loader,
+    std::unique_ptr<std::string> response_string) {
+  // TODO(crbug.com/436900259): Add UMA metrics for whether the response
+  //   returned 200, was parsed successfully, and which features were eligible.
+  //   This will let us know how watered down UMA and finch are compared due to
+  //   mismatched GWS eligibility criteria and estimate the actual population
+  //   size.
+  // TODO(crbug.com/436899694): Save the response to disk so we can enable AI
+  //   features on startup without having to wait for a successful GWS response.
+  if (!response_string)
+    return;
+  // Parse into a temporary variable 1st so that if parsing fails,
+  // `most_recent_aim_eligibility_response` isn't cleared.
+  omnibox::AimEligibilityResponse response_proto;
+  if (!response_proto.ParseFromString(*response_string))
+    return;
+  most_recent_aim_eligibility_response = response_proto;
+  NotifyObservers();
+}
 
 void AimEligibilityService::AddObserver(
     AimEligibilityServiceObserver* observer) {
@@ -59,6 +149,9 @@ bool AimEligibilityService::IsCountryAndLocale(const std::string& country,
 }
 
 bool AimEligibilityService::IsAimEligible() const {
+  // TODO(crbug.com/436901669): Conditionally check
+  //   `most_recent_aim_eligibility_response.is_eligible()` and
+  //   `IsCountryAndLocale()` depending on feature param state.
   return search::DefaultSearchProviderIsGoogle(template_url_service_) &&
          IsCountryAndLocale("us", "en-US") &&
          omnibox::IsAimAllowedByPolicy(pref_service_);
