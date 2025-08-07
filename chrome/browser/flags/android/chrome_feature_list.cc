56af546f40d26b4c0148dcf89a8c35d970697f4c
Calder Kitagawa <ckitagawa@chromium.org>
2025-08-07 10:56:27 -0700

    [Tab Model] Enable Headless Tab Model by default
    Bug: 399995422
    Change-Id: I20c88bee8ea98bee3ccad0cc59c1300a96d9c2f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823707
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498355}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -1062,7 +1062,7 @@ BASE_FEATURE(kGroupNewTabWithParent,
 
 BASE_FEATURE(kHeadlessTabModel,
              "HeadlessTabModel",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kHistoryPaneAndroid,
              "HistoryPaneAndroid",
