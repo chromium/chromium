c4a38f8b5ee54bb2c42c4ea56015a8a65394d4df
Michael Wilson <mjwilson@chromium.org>
2025-08-07 14:27:49 -0700

    Get DEBUG_AUDIONODE_REFERENCES compiling again
    Since this code is usually disabled a compile-time, it has become out
    of date and no longer complies when the debug level is set higher than
    0.
    
    Get things compiling again by adding UNSAFE_TODO and casting enums to
    index values.
    
    Ideally we would not use a compile-time switch to control this behavior
    so that the code won't go out of date.
    
    Bug: 436880902
    Change-Id: Ib280037e944b4a16cc9e096f34ba582b94117ca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6829144
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1498477}

--- third_party/blink/renderer/modules/webaudio/audio_handler.h
+++ third_party/blink/renderer/modules/webaudio/audio_handler.h
@@ -329,7 +329,7 @@ class MODULES_EXPORT AudioHandler : public ThreadSafeRefCounted<AudioHandler> {
 
 #if DEBUG_AUDIONODE_REFERENCES
   static bool is_node_count_initialized_;
-  static int node_count_[kNodeTypeEnd];
+  static int node_count_[static_cast<int>(NodeType::kNodeTypeEnd)];
 #endif
 
   V8ChannelCountMode::Enum channel_count_mode_;
