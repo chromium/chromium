00972e23d0374ca3c3d3a6041dabb29db9eff359
Christoph Schwering <schwering@google.com>
2025-08-06 20:05:57 -0700

    [Autofill] Ignore adjacent kVehiclePlateNumber fields
    (cherry picked from commit a4a4d6b8793ad2f6a4636b83ed60231b65d96f54)
    
    Fixed: 436820616
    Bug: 436634673
    Change-Id: I47b22cec001ab23de451542a51e3315c80367173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6822152
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1497227}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6826441
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7339@{#97}
    Cr-Branched-From: 27be8b77710f4405fdfeb4ee946fcabb0f6c92b2-refs/heads/main@{#1496484}

--- components/autofill/core/browser/autofill_ai_form_rationalization.cc
+++ components/autofill/core/browser/autofill_ai_form_rationalization.cc
@@ -17,21 +17,64 @@ namespace {
 
 // Checks whether `fields` matches `entity_of_interst`'s required fields,
 // returns an empty vector if not.
+void EnforceRequiredFieldsConstraints(
+    std::vector<AutofillFieldWithAttributeType>& fields,
+    EntityType entity_of_interest) {
+  DenseSet<AttributeType> types =
+      DenseSet<AttributeType>(fields, &AutofillFieldWithAttributeType::type);
+  bool attributes_satisfy_required_fields =
+      std::ranges::any_of(entity_of_interest.required_fields(),
+                          [&](DenseSet<AttributeType> constraint) {
+                            return types.contains_all(constraint);
+                          });
+  if (!attributes_satisfy_required_fields) {
+    fields.clear();
+  }
+}
+
+// Looks for adjacent kVehicleLicensePlate fields. Since we don't have
+// heuristics to split license plates reliably, we do not want to fill them.
+void RemoveAdjacentLicensePlateNumbers(
+    std::vector<AutofillFieldWithAttributeType>& fields,
+    EntityType entity_of_interest) {
+  if (entity_of_interest != EntityType(EntityTypeName::kVehicle)) {
+    return;
+  }
+
+  // The rank is the field's index in the form. Since `fields` only contains
+  // fields that belong to `entity_of_interest` and they appear in the same
+  // order as in the form, their std::distance() in `fields` is less than or
+  // equal to their rank_distance().
+  auto rank_distance = [](auto begin, auto end) {
+    CHECK(begin != end);
+    ptrdiff_t lo = begin->field->rank();
+    ptrdiff_t hi = std::prev(end)->field->rank();
+    return hi - lo + 1;
+  };
+
+  // Erases all subranges where all fields `pred`, provided that these subranges
+  // have more than one field and the ranks of the fields are reasonably close
+  // (i.e., there aren't too many fields in between those).
+  auto pred = [](const AutofillFieldWithAttributeType& f) {
+    return f.type == AttributeType(AttributeTypeName::kVehiclePlateNumber);
+  };
+  auto begin = fields.begin();
+  auto end = fields.begin();
+  while ((begin = std::find_if(end, fields.end(), pred)) != fields.end()) {
+    end = std::find_if_not(std::next(begin), fields.end(), pred);
+    if (std::distance(begin, end) > 1 &&
+        std::distance(begin, end) * 3 / 2 >= rank_distance(begin, end)) {
+      end = fields.erase(begin, end);
+    }
+  }
+}
+
 std::vector<AutofillFieldWithAttributeType> RationalizeAttributeTypes(
     std::vector<AutofillFieldWithAttributeType> fields,
     EntityType entity_of_interest) {
-  bool attributes_satisfy_required_fields = [&]() {
-    DenseSet<AttributeType> types =
-        DenseSet<AttributeType>(fields, &AutofillFieldWithAttributeType::type);
-    return std::ranges::any_of(entity_of_interest.required_fields(),
-                               [&](DenseSet<AttributeType> constraint) {
-                                 return types.contains_all(constraint);
-                               });
-  }();
-  if (attributes_satisfy_required_fields) {
-    return fields;
-  }
-  return std::vector<AutofillFieldWithAttributeType>();
+  EnforceRequiredFieldsConstraints(fields, entity_of_interest);
+  RemoveAdjacentLicensePlateNumbers(fields, entity_of_interest);
+  return fields;
 }
 
 }  // namespace
