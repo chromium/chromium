27ba58fbc1f2ccd05a0fd327318169c47b56d1f3
Alison Maher <almaher@microsoft.com>
2025-08-07 10:30:07 -0700

    [Masonry] Tests for repeat(auto-fill) + other intrinsic sized tracks
    This change adds tests to confirm repeat(auto-fill/fit) is working as
    expected for min-content, max-content, and fit-content(). Once we parsed
    and checked for these additional intrinsic track sizes, the rest of the
    implementation just worked as expected.
    
    The row direction tests aren't as interesting for these values, but
    added tests for completeness. These tests also aren't as thorough as
    those for auto since percentages and spanners aren't as interesting with
    these values, and is well tested with the auto scenarios.
    
    There are two tests failing because we don't allow intrinsic tracks
    alongside these kinds of intrinsic repeaters yet. This is a known issue
    and will be addressed in a future change.
    
    There were also a handful of tests that failed in the wpt runner but
    match the expected file in the browser (including Canary). I tried
    setting all experimental/test feature flags to stable, and was still
    unable to repro the behavior observed in the wpt runner. Mark these
    failing for now, to follow up on separately, but the rendering in the
    browser is as expected for these tests.
    
    I also noticed I forgot to update some of the new variable naming in
    masonry_layout_algorithm.h, so those have been updated, as well.
    
    Bug: 343257585
    Change-Id: I65b8643519f00f10a7e8509b8f9d0eb60ee4f073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823263
    Reviewed-by: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1498330}

--- third_party/blink/renderer/core/layout/masonry/masonry_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/masonry/masonry_layout_algorithm.h
@@ -49,56 +49,59 @@ class CORE_EXPORT MasonryLayoutAlgorithm
   void PlaceOutOfFlowItems(HeapVector<Member<LayoutBox>>& oof_children);
 
   // Returns the track collection given the provided `sizing_constraint`.
-  // If `auto_repeat_track_size` is non-null, this contains the track size to
-  // use for an auto sized track inside a repeat() track definition. The
+  // If `intrinsic_repeat_track_size` is non-null, this contains the track size
+  // to use for an intrinsic sized track inside a repeat() track definition. The
   // `masonry_items` and `start_offset` associated with the returned track
   // collection are returned via the corresponding output params. If we hit an
-  // auto sized track within a repeat() definition and don't provide
-  // `auto_repeat_track_size`, then `needs_auto_track_size` will be set to true,
-  // indicating that another track sizing pass will be required once we've
-  // computed the auto track size. `opt_oof_children` is an optional vector of
-  // out-of-flow direct children of the masonry container that this method will
-  // populate. `collapsed_track_indexes` will be populated with all the grid
-  // track indexes that were collapsed as a result of auto-fit.
+  // intrinsic sized track within a repeat() definition and don't provide
+  // `intrinsic_repeat_track_size`, then `needs_intrinsic_track_size` will be
+  // set to true, indicating that another track sizing pass will be required
+  // once we've computed the intrinsic track size. `opt_oof_children` is an
+  // optional vector of out-of-flow direct children of the masonry container
+  // that this method will populate. `collapsed_track_indexes` will be populated
+  // with all the grid track indexes that were collapsed as a result of
+  // auto-fit.
   GridSizingTrackCollection ComputeGridAxisTracks(
       const SizingConstraint sizing_constraint,
-      std::optional<LayoutUnit> auto_repeat_track_size,
+      std::optional<LayoutUnit> intrinsic_repeat_track_size,
       GridItems& masonry_items,
       Vector<wtf_size_t>& collapsed_track_indexes,
       wtf_size_t& start_offset,
-      bool& needs_auto_track_size,
+      bool& needs_intrinsic_track_size,
       HeapVector<Member<LayoutBox>>* opt_oof_children = nullptr) const;
 
   GridSizingTrackCollection BuildGridAxisTracks(
       const GridLineResolver& line_resolver,
       const GridItems& masonry_items,
       SizingConstraint sizing_constraint,
-      bool& needs_auto_track_size,
+      bool& needs_intrinsic_track_size,
       Vector<wtf_size_t>& collapsed_track_indexes,
       wtf_size_t& start_offset) const;
 
-  // If `auto_repeat_track_size` is non-null, this indicates the track
-  // size to use for an auto sized track inside a repeat() track definition.
-  // If we hit an auto sized track within a repeat() definition and don't
-  // provide `auto_repeat_track_size`, then `needs_auto_track_size` will
-  // be set to true, indicating that another track sizing pass will be
-  // required once we've computed the auto track size.
+  // If `intrinsic_repeat_track_size` is non-null, this indicates the track
+  // size to use for an intrinsic sized track inside a repeat() track
+  // definition. If we hit an intrinsic sized track within a repeat() definition
+  // and don't provide `intrinsic_repeat_track_size`, then
+  // `needs_intrinsic_track_size` will be set to true, indicating that another
+  // track sizing pass will be required once we've computed the intrinsic track
+  // size.
   wtf_size_t ComputeAutomaticRepetitions(
-      std::optional<LayoutUnit> auto_repeat_track_size,
-      bool& needs_auto_track_size) const;
+      std::optional<LayoutUnit> intrinsic_repeat_track_size,
+      bool& needs_intrinsic_track_size) const;
 
   // From https://drafts.csswg.org/css-grid-3/#track-sizing-performance:
   //   "... synthesize a virtual masonry item that has the maximum of every
   //   intrinsic size contribution among the items in that group."
   // Returns a collection of items that reflect the intrinsic contributions from
   // the item groups, which will be used to resolve the grid axis' track sizes.
-  // If `needs_auto_track_size` is true, that means that we are in the first
-  // track size pass required to compute auto track sizes within a repeat
-  // definition, which requires adjustments to virtual item creation and track
-  // sizing per https://www.w3.org/TR/css-grid-3/#masonry-intrinsic-repeat.
+  // If `needs_intrinsic_track_size` is true, that means that we are in the
+  // first track size pass required to compute intrinsic track sizes within a
+  // repeat definition, which requires adjustments to virtual item creation and
+  // track sizing per
+  // https://www.w3.org/TR/css-grid-3/#masonry-intrinsic-repeat.
   GridItems BuildVirtualMasonryItems(const GridLineResolver& line_resolver,
                                      const GridItems& masonry_items,
-                                     const bool needs_auto_track_size,
+                                     const bool needs_intrinsic_track_size,
                                      SizingConstraint sizing_constraint,
                                      const wtf_size_t auto_repetition_count,
                                      wtf_size_t& start_offset) const;
@@ -108,7 +111,7 @@ class CORE_EXPORT MasonryLayoutAlgorithm
       SizingConstraint sizing_constraint,
       const ConstraintSpace space_for_measure,
       const GridItemData* virtual_item,
-      const bool needs_auto_track_size) const;
+      const bool needs_intrinsic_track_size) const;
 
   ConstraintSpace CreateConstraintSpace(
       const GridItemData& masonry_item,
@@ -128,7 +131,7 @@ class CORE_EXPORT MasonryLayoutAlgorithm
 
   ConstraintSpace CreateConstraintSpaceForMeasure(
       const GridItemData& masonry_item,
-      const bool needs_auto_track_size = false,
+      const bool needs_intrinsic_track_size = false,
       std::optional<LayoutUnit> opt_fixed_inline_size = std::nullopt,
       bool is_for_min_max_sizing = false) const;
 
