23e97090cfdc1d4b6c22692ebf87c2e8ffd736f0
Noémie St-Onge <noemies@google.com>
2025-08-07 12:21:53 -0700

    [iOSLiquidGlass] Update look of the expanded views's close button
    Before iOS 26 (i.e., current):
    https://screenshot.googleplex.com/AtW5aMYsVvvBZ9g.png
    
    As of iOS 26: https://screenshot.googleplex.com/6iGZimqDoqZnMmh.png
    
    Mocks:
    https://www.figma.com/design/xH8lYMwzSzW6fGYEaxKJQ8/iOS-26---Bling?node-id=337-49013&t=l8gvLGJ55T7zNEZS-4
    
    Change-Id: I5e80e06a88af12badb8035567854a35e22608cfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823750
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498402}

--- ios/chrome/browser/autofill/ui_bundled/manual_fill/expanded_manual_fill_view_controller.mm
+++ ios/chrome/browser/autofill/ui_bundled/manual_fill/expanded_manual_fill_view_controller.mm
@@ -23,8 +23,8 @@ namespace {
 
 // Size of the Chrome logo.
 constexpr CGFloat kChromeLogoSize = 24;
-// Size of the close button.
-constexpr CGFloat kCloseButtonSize = 30;
+// Size of the close button when liquid glass is disabled.
+constexpr CGFloat kCloseButtonSizePreLiquidGlass = 30;
 // Size of the data type icons representing the different segments
 // of the segmented control.
 constexpr CGFloat kDataTypeIconSize = 18;
@@ -56,6 +56,11 @@ constexpr CGFloat kSegmentedControlLeadingSpacingWideLayout = 18;
 // the wide layout only.
 constexpr CGFloat kSegmentedControlTrailingSpacingWideLayout = 15;
 
+#if defined(__IPHONE_26_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_26_0
+// Size of the close button.
+constexpr CGFloat kCloseButtonSize = 44;
+#endif
+
 // Helper method to get the right segment index depending on the `data_type`.
 int GetSegmentIndexForDataType(ManualFillDataType data_type) {
   switch (data_type) {
@@ -79,6 +84,23 @@ UIColor* GetBackgroundColor() {
 #endif
 
   return [UIColor colorNamed:kGroupedPrimaryBackgroundColor];
+}
+
+// Returns the symbol configuration to use for the close button.
+UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration() {
+#if defined(__IPHONE_26_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_26_0
+  if (@available(iOS 26, *)) {
+    return [UIImageSymbolConfiguration
+        configurationWithPointSize:kCloseButtonSize
+                            weight:UIImageSymbolWeightThin
+                             scale:UIImageSymbolScaleDefault];
+  }
+#endif
+
+  return [UIImageSymbolConfiguration
+      configurationWithPointSize:kCloseButtonSizePreLiquidGlass
+                          weight:UIImageSymbolWeightRegular
+                           scale:UIImageSymbolScaleMedium];
 }
 
 }  // namespace
@@ -371,10 +393,8 @@ UIColor* GetBackgroundColor() {
   closeButton.accessibilityLabel = l10n_util::GetNSString(
       IDS_IOS_EXPANDED_MANUAL_FILL_CLOSE_BUTTON_ACCESSIBILITY_LABEL);
 
-  UIImageSymbolConfiguration* symbolConfiguration = [UIImageSymbolConfiguration
-      configurationWithPointSize:kCloseButtonSize
-                          weight:UIImageSymbolWeightRegular
-                           scale:UIImageSymbolScaleMedium];
+  UIImageSymbolConfiguration* symbolConfiguration =
+      GetCloseButtonSymbolConfiguration();
   UIImage* buttonImage = SymbolWithPalette(
       DefaultSymbolWithConfiguration(kXMarkCircleFillSymbol,
                                      symbolConfiguration),
