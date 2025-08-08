63d185dd415a690fbca9f34f9f9f0e1c6223efba
Zijie He <zijiehe@google.com>
2025-08-08 11:52:49 -0700

    [M140] [Fuchsia] Allow `kProtectedServiceWorkers` switch in WebEngine.
    Original change's description:
    > [Fuchsia] Allow `kProtectedServiceWorkers` switch in WebEngine.
    > 
    > Add `switches::kProtectedServiceWorkers` to the list of allowed
    > command-line arguments for WebEngine. It was added by
    > https://crrev.com/c/6822273, and needed to be updated in the
    > config.json.
    > 
    > Bug: 436314901, 434764000, 435256867
    > Change-Id: I3214ffc6771b2c02fd86b1430c4fc86c71cb4b9e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828502
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Auto-Submit: Zijie He <zijiehe@google.com>
    > Commit-Queue: Zijie He <zijiehe@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1498494}
    
    Bug: 437183899,436314901,434764000,435256867
    Change-Id: I3214ffc6771b2c02fd86b1430c4fc86c71cb4b9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6830428
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7339@{#237}
    Cr-Branched-From: 27be8b77710f4405fdfeb4ee946fcabb0f6c92b2-refs/heads/main@{#1496484}

--- fuchsia_web/webengine/browser/web_engine_config.cc
+++ fuchsia_web/webengine/browser/web_engine_config.cc
@@ -104,6 +104,7 @@ bool AddCommandLineArgsFromConfig(const base::Value::Dict& config,
       switches::kVulkanSyncCpuMemoryLimitMb,
       switches::kWebglAntialiasingMode,
       switches::kWebglMSAASampleCount,
+      switches::kProtectedServiceWorkers,
   };
 
   for (const auto arg : *args) {
