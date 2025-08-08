ad326acd384af3234e51316fb056ec9dd54a1c1e
Evan Stade <evanstade@microsoft.com>
2025-08-08 13:10:51 -0700

    IDB: SQLite store - extend fuzzers to cover new encoding as well as old
    The newer key encoding/decoding schemes ("Sortable") are now covered by
    these fuzzers. The new encoding scheme should eventually replace the
    old, and the pre-existing portions of these fuzzers can then be removed
    (aside from shared utility code).
    
    Bug: 419272072
    Change-Id: I99ca9ae6808bade95ef1974150ea08669f1741d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828624
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1498990}

--- content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkey_fuzzer.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkey_fuzzer.cc
@@ -10,19 +10,37 @@
 #include "third_party/blink/public/common/indexeddb/indexeddb_key.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  std::string_view input(reinterpret_cast<const char*>(data), size);
-  std::string_view parsed_input(input);
-  blink::IndexedDBKey key = content::indexed_db::DecodeIDBKey(&parsed_input);
+  // Old encoding scheme.
+  {
+    std::string_view input(reinterpret_cast<const char*>(data), size);
+    std::string_view parsed_input(input);
+    blink::IndexedDBKey key = content::indexed_db::DecodeIDBKey(&parsed_input);
 
-  // If any prefix of the input decoded into a valid key, re-encode it, then
-  // ensure that the encoding matches the original prefix that got decoded.
-  if (key.IsValid()) {
-    std::string result;
-    content::indexed_db::EncodeIDBKey(key, &result);
-    // DecodeIDBKey() leaves the unparsed suffix in parsed_input, so strip that
-    // much off the end of input before comparing.
-    input.remove_suffix(parsed_input.size());
-    CHECK_EQ(std::string_view(result), input);
+    // If any prefix of the input decoded into a valid key, re-encode it, then
+    // ensure that the encoding matches the original prefix that got decoded.
+    if (key.IsValid()) {
+      std::string result;
+      content::indexed_db::EncodeIDBKey(key, &result);
+      // DecodeIDBKey() leaves the unparsed suffix in parsed_input, so strip
+      // that much off the end of input before comparing.
+      input.remove_suffix(parsed_input.size());
+      CHECK_EQ(std::string_view(result), input);
+    }
+  }
+
+  // New (sortable) encoding scheme.
+  {
+    std::string_view input(reinterpret_cast<const char*>(data), size);
+    std::string_view parsed_input(input);
+    blink::IndexedDBKey key =
+        content::indexed_db::DecodeSortableIDBKey(parsed_input);
+
+    // Unlike the older encoding, the input has to be exactly decodeable (rather
+    // than just a prefix being decodeable).
+    if (key.IsValid()) {
+      CHECK_EQ(std::string_view(content::indexed_db::EncodeSortableIDBKey(key)),
+               input);
+    }
   }
 
   return 0;
