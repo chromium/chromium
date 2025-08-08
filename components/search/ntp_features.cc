cf49fef8fc5fa9a4070e2d5fbc56e1dbcc6cb105
Salma Elmahallawy <esalma@google.com>
2025-08-08 15:39:59 -0700

    [NTPFooter] Enable `kNtpFooter` by default.
    Bug: 437395682
    Change-Id: Id2ba7f19a6f3357a756b60cc5e0fe14cbd77a11b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6829012
    Auto-Submit: Salma Elmahallawy <esalma@google.com>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499074}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -303,7 +303,7 @@ BASE_FEATURE(kNtpOneGoogleBarAsyncBarParts,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // If enabled, a footer will show on the NTP.
-BASE_FEATURE(kNtpFooter, "NtpFooter", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kNtpFooter, "NtpFooter", base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If enabled, tab groups module will be shown.
 BASE_FEATURE(kNtpTabGroupsModule,
