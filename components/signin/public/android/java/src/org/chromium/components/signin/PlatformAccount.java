46cc2f26d5cc099cf7f808a69b7b305a855330ac
Liza Bipin <mlbipin@google.com>
2025-08-08 01:20:00 -0700

    [Signin][Android]Mark PlatformAccount capabilities API to be synchronous
    Bug: 429143376
    Change-Id: I0ccade5c520cd2541ce64d5622211a8e6320c97e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6823443
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1498645}

--- components/signin/public/android/java/src/org/chromium/components/signin/PlatformAccount.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/PlatformAccount.java
@@ -3,22 +3,29 @@
 // found in the LICENSE file.
 package org.chromium.components.signin;
 
+import androidx.annotation.MainThread;
+import androidx.annotation.WorkerThread;
+
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.components.signin.AccountManagerDelegate.CapabilityResponse;
 import org.chromium.google_apis.gaia.GaiaId;
 
 /** Provides details about an account. */
 @NullMarked
 public interface PlatformAccount {
     /** Returns gaiaId of the PlatformAccount. */
+    @MainThread
     GaiaId getId();
 
     /** Returns email of the PlatformAccount. */
+    @MainThread
     String getEmail();
 
     /**
-     * Returns a {@link CapabilityResponse} that indicates whether the account has the requested or
-     * has exception.
+     * Returns a {@link CapabilityResponse} that indicates whether the account has the requested
+     * capability or has an exception.
      */
-    @AccountManagerDelegate.CapabilityResponse
-    int hasCapability();
+    @WorkerThread
+    @CapabilityResponse
+    int fetchCapability(String capability);
 }
