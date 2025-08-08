4acfa2a3ec12a751d475bfbc40235732d9b1fdb8
Ritika Gupta <ritikagup@google.com>
2025-08-08 14:36:09 -0700

    [STG Comments] Adding CommentsService API interface for Java
    Bug: 435005417
    Change-Id: I68f973c53154a1766fb3968729f59d1831b33ead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798924
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499041}

--- components/collaboration/internal/android/comments/comments_service_bridge.h
+++ components/collaboration/internal/android/comments/comments_service_bridge.h
@@ -29,6 +29,39 @@ class CommentsServiceBridge : public base::SupportsUserData::Data {
                      const base::android::JavaParamRef<jobject>& j_caller);
   bool IsEmptyService(JNIEnv* env,
                       const base::android::JavaParamRef<jobject>& j_caller);
+  base::android::ScopedJavaLocalRef<jstring> AddComment(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& j_caller,
+      const base::android::JavaParamRef<jstring>& j_collaboration_id,
+      const base::android::JavaParamRef<jobject>& j_url,
+      const base::android::JavaParamRef<jstring>& j_content,
+      const base::android::JavaParamRef<jstring>& j_parent_comment_id,
+      const base::android::JavaParamRef<jobject>& j_success_callback);
+  void EditComment(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& j_caller,
+      const base::android::JavaParamRef<jstring>& j_comment_id,
+      const base::android::JavaParamRef<jstring>& j_new_content,
+      const base::android::JavaParamRef<jobject>& j_success_callback);
+  void DeleteComment(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& j_caller,
+      const base::android::JavaParamRef<jstring>& j_comment_id,
+      const base::android::JavaParamRef<jobject>& j_success_callback);
+  void QueryComments(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& j_caller,
+      const base::android::JavaParamRef<jobject>& j_filter_criteria,
+      const base::android::JavaParamRef<jobject>& j_pagination_criteria,
+      const base::android::JavaParamRef<jobject>& j_callback);
+  void AddObserver(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& j_caller,
+      const base::android::JavaParamRef<jobject>& j_observer,
+      const base::android::JavaParamRef<jobject>& j_filter_criteria);
+  void RemoveObserver(JNIEnv* env,
+                      const base::android::JavaParamRef<jobject>& j_caller,
+                      const base::android::JavaParamRef<jobject>& j_observer);
 
   // Returns the companion Java object for this bridge.
   base::android::ScopedJavaLocalRef<jobject> GetJavaObject();
