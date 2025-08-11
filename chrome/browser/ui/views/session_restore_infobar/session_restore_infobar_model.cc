1f9854be292e835ea441c4944e41651fe572fa23
Muhammad Salmaan <musalmaan@chromium.org>
2025-08-11 14:01:25 -0700

    Add session restore infobar model
    This commit introduces the creation of a model
    (SessionRestoreInfobarModel) to manage the logic and data related to
    session restore. Specifically it gets the preferences value of session
    restore from the user profile to be used later to determine when
    the infobar should be displayed.
    
    Bug: 431828875
    Change-Id: Ic6702531ca4242edd9c54b85d64c1e34da98bc42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838716
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1499800}

---
+++ chrome/browser/ui/views/session_restore_infobar/session_restore_infobar_model.cc
@@ -0,0 +1,49 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/session_restore_infobar/session_restore_infobar_model.h"
+
+#include "base/feature_list.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
+#include "chrome/common/pref_names.h"
+#include "components/prefs/pref_service.h"
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/web_contents.h"
+
+namespace session_restore_infobar {
+
+SessionRestoreInfobarModel::~SessionRestoreInfobarModel() = default;
+
+SessionRestoreInfobarModel::SessionRestoreMessageValue
+SessionRestoreInfobarModel::GetSessionRestoreMessageValue() {
+  // Ensure the profile and its preference service are available.
+  Profile* profile =
+      GetLastActiveBrowserWindowInterfaceWithAnyProfile()->GetProfile();
+  if (!profile || !profile->GetPrefs()) {
+    return NoProfile;
+  }
+  // Get the integer value from the user's profile preferences.
+  int restore_on_startup_value =
+      profile->GetPrefs()->GetInteger(prefs::kRestoreOnStartup);
+  // Get the value for chrome session restore.
+  switch (restore_on_startup_value) {
+    case 1:
+      return ContinueWhereLeftOff;
+    case 4:
+      return OpenSpecificPages;
+    case 5:
+      return OpenNewTabPage;
+    default:
+      return OpenNewTabPage;
+  }
+}
+
+void SessionRestoreInfobarModel::SetInfobarDelegate() {
+  // Called to pass enum to the infobar delegate to display the correct
+  // message in the infobar.
+}
+
+}  // namespace session_restore_infobar
---
+++ chrome/browser/ui/views/session_restore_infobar/session_restore_infobar_model.h
@@ -0,0 +1,38 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_SESSION_RESTORE_INFOBAR_SESSION_RESTORE_INFOBAR_MODEL_H_
+#define CHROME_BROWSER_UI_VIEWS_SESSION_RESTORE_INFOBAR_SESSION_RESTORE_INFOBAR_MODEL_H_
+
+namespace session_restore_infobar {
+
+// `SessionRestoreInfobarModel` class is responsible for encapsulating the logic
+// related to session restore functionality. It determines the appropriate
+// message to display in the infobar based on the user's current settings.
+class SessionRestoreInfobarModel {
+ public:
+  // Enum for session restore message values. Based on the message value, there
+  // will be a different message displayed on the infobar.
+  enum SessionRestoreMessageValue {
+    OpenNewTabPage,
+    ContinueWhereLeftOff,
+    OpenSpecificPages,
+    NoProfile
+  };
+
+  SessionRestoreInfobarModel() = default;
+  ~SessionRestoreInfobarModel();
+
+  // Retrieves a value indicating the user's preference for session restore
+  // behavior. This value is used to determine if a session restore message
+  // should be displayed.
+  SessionRestoreMessageValue GetSessionRestoreMessageValue();
+
+  // Sets the infobar delegate to display the correct message in the infobar.
+  void SetInfobarDelegate();
+};
+
+}  // namespace session_restore_infobar
+
+#endif  // CHROME_BROWSER_UI_VIEWS_SESSION_RESTORE_INFOBAR_SESSION_RESTORE_INFOBAR_MODEL_H_
