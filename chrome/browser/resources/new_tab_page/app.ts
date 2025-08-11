82b0288c533c8ce24688cdc6b280c7b51710b192
Marlon Facey <mfacey@chromium.org>
2025-08-11 15:22:46 -0700

    Revert "[M139] [ntp-composebox] Transition opacity over 1s"
    This reverts commit 1e260a1f5932a2c5f3fadf49e2c57e892de02770.
    
    Reason for revert: This CL introduced a bug that was not fixed in time for stable respin
    
    Bug: 436354240,434721919
    Original change's description:
    > [M139] [ntp-composebox] Transition opacity over 1s
    >
    > Original change's description:
    > > [ntp-composebox] Transition opacity over 1s
    > > 
    > > The transition from background to composebox scrim background was too
    > > jarring. This CL fades the background in by increasing the opacity over
    > > 1s. Also fixes a bug where the text would not populate the searchbox
    > > when the composebox is closed 2 or more times.
    > > 
    > > Light theme: http://screencast/cast/NTM4MjkzOTY5NjE2ODk2MHxmNTEwMDcwZC00NA
    > > Dark theme: http://screencast/cast/NTUxNjUwOTA4NTQzMzg1Nnw1Mzg4MDUyYy0zNQ
    > > Dark theme + Dark Background: http://screencast/cast/NjUyNDU0NjE2NDE5NTMyOHwzNjBkOGEwYy01ZQ
    > > Light theme + Dark Background: http://screencast/cast/NTI5ODMzNDUxMTEzNjc2OHw0NmVjMjE0YS1kMw
    > > 
    > > Closing animation:
    > > http://screencast/cast/NTA1NTkxMzgzOTgyMDgwMHw0YzM2MTUyNy0wNQ
    > > 
    > > Bug: 434721919
    > > Change-Id: Idfda0ce6233bdcdaa5ef3e9b97a0d15c93837ab1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6798286
    > > Reviewed-by: Riley Tatum <rtatum@google.com>
    > > Reviewed-by: Roman Arora <romanarora@chromium.org>
    > > Commit-Queue: Marlon Facey <mfacey@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1496626}
    >
    > Bug: 436354240,434721919
    > Change-Id: Idfda0ce6233bdcdaa5ef3e9b97a0d15c93837ab1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6832085
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/branch-heads/7258@{#2550}
    > Cr-Branched-From: f600d0656fd5b5fe4a82981f533d31ed6939e2e4-refs/heads/main@{#1477651}
    
    Bug: 436354240,434721919
    Change-Id: Ia9bfec0a20f747ef0987c4177502f35d6d8000a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838361
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7258@{#2579}
    Cr-Branched-From: f600d0656fd5b5fe4a82981f533d31ed6939e2e4-refs/heads/main@{#1477651}

--- chrome/browser/resources/new_tab_page/app.ts
+++ chrome/browser/resources/new_tab_page/app.ts
@@ -15,7 +15,6 @@ import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_
 import {HelpBubbleMixinLit} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin_lit.js';
 import type {SearchboxElement} from 'chrome://resources/cr_components/searchbox/searchbox.js';
 import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
-import {assert} from 'chrome://resources/js/assert.js';
 import type {ClickInfo} from 'chrome://resources/js/browser_command.mojom-webui.js';
 import {Command} from 'chrome://resources/js/browser_command.mojom-webui.js';
 import {BrowserCommandProxy} from 'chrome://resources/js/browser_command/browser_command_proxy.js';
@@ -208,10 +207,6 @@ export class AppElement extends AppElementBase {
         notify: true,
       },
 
-      closingComposebox: {
-        type: Boolean,
-        reflect: true,
-      },
       composeboxCloseByClickOutside_: {type: Boolean},
       composeboxEnabled: {type: Boolean},
       composeButtonEnabled: {type: Boolean},
@@ -309,7 +304,6 @@ export class AppElement extends AppElementBase {
   protected accessor wallpaperSearchButtonEnabled_: boolean =
       loadTimeData.getBoolean('wallpaperSearchButtonEnabled');
   protected accessor showWallpaperSearchButton_: boolean = false;
-  accessor closingComposebox: boolean = false;
   accessor composeButtonEnabled: boolean =
       loadTimeData.getBoolean('searchboxShowComposeEntrypoint');
   protected accessor composeboxCloseByClickOutside_: boolean =
@@ -601,7 +595,7 @@ export class AppElement extends AppElementBase {
   private updateOneGoogleBarAppearance_() {
     if (this.oneGoogleBarLoaded_) {
       let isNtpDarkTheme;
-      if (this.isComposeboxVisible_()) {
+      if (this.showComposebox_) {
         isNtpDarkTheme = this.theme_ && this.theme_.isDark;
       } else {
         isNtpDarkTheme = this.theme_ &&
@@ -639,7 +633,7 @@ export class AppElement extends AppElementBase {
   private computeRealboxShown_(): boolean {
     // Do not show the realbox if the upload dialog is showing.
     return !!this.theme_ && !this.showLensUploadDialog_ &&
-        !this.isComposeboxVisible_();
+        !this.showComposebox_;
   }
 
   private computePromoAndModulesLoaded_(): boolean {
@@ -670,55 +664,25 @@ export class AppElement extends AppElementBase {
   }
 
   protected onComposeboxClickOutside_() {
-    const composebox =
-        this.shadowRoot.querySelector<ComposeboxElement>('#composebox');
-    assert(composebox);
-    const closeComposebox = new CustomEvent('closeComposebox', {
-      detail: {composeboxText: composebox.getText()},
-      bubbles: true,
-      cancelable: true,
-    });
-
-    this.closeComposebox_(closeComposebox);
+    this.closeComposebox_(this.$.composebox.getText());
   }
 
-  protected closeComposebox_(e: CustomEvent) {
-    const composeboxText = e.detail.composeboxText;
+  protected closeComposebox_(e: CustomEvent|string) {
+    let composeboxText: string;
+
+    if (typeof e === 'string') {
+      composeboxText = e;
+    } else {
+      composeboxText = e.detail.uuid;
+    }
 
     if (composeboxText && composeboxText.trim()) {
       this.$.searchbox.setInputText(composeboxText);
     }
-    const composebox =
-        this.shadowRoot.querySelector<ComposeboxElement>('#composebox');
-    assert(composebox);
-    composebox.resetText();
-    this.fadeoutScrim_();
+    this.$.composebox.resetText();
+    this.toggleComposebox_();
     const composeboxHandler = ComposeboxProxyImpl.getInstance().handler;
     composeboxHandler.notifySessionAbandoned();
-  }
-
-  private fadeoutScrim_() {
-    const composeboxScrim =
-        this.shadowRoot.querySelector<HTMLElement>('#composeboxScrim');
-    assert(composeboxScrim);
-    composeboxScrim.addEventListener('transitionend', (e: TransitionEvent) => {
-      if (e.propertyName === 'opacity') {  // Match the animation name
-        this.toggleComposebox_();
-        this.closingComposebox = false;
-      }
-    });
-    composeboxScrim.classList.add('fade-out');
-    const composebox = this.shadowRoot.querySelector('#composebox');
-    assert(composebox);
-    composebox.classList.add('fade-out');
-    this.closingComposebox = true;
-    this.logoColor_ = this.computeLogoColor_();
-    this.singleColoredLogo_ = this.computeSingleColoredLogo_();
-    this.updateOneGoogleBarAppearance_();
-  }
-
-  private isComposeboxVisible_() {
-    return this.showComposebox_ && !this.closingComposebox;
   }
 
   protected onOpenVoiceSearch_() {
@@ -882,7 +846,7 @@ export class AppElement extends AppElementBase {
       return null;
     }
 
-    if (this.isComposeboxVisible_()) {
+    if (this.showComposebox_) {
       return this.theme_.isDark ? hexColorToSkColor('#ffffff') : null;
     }
 
@@ -891,7 +855,7 @@ export class AppElement extends AppElementBase {
   }
 
   private computeSingleColoredLogo_(): boolean {
-    if (this.isComposeboxVisible_()) {
+    if (this.showComposebox_) {
       return !!this.theme_ && this.theme_.isDark;
     }
     return !!this.theme_ && (!!this.theme_.logoColor || this.theme_.isDark);
