5da31eb81c66036feeb02e030478a2dcf9a1efbe
Louis Romero <lpromero@google.com>
2025-08-12 03:40:18 -0700

    [iOS] Split menu builder logic for iOS 26
    This CL splits the menu builder logic to have a separate codepath for
    iOS 26 and later. This will allow for future changes to the iOS 26 menu
    bar without affecting older versions.
    
    Bug: 438114271
    Change-Id: I00340eae65528f169f1e66809ebe41d14245ad35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6841234
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1500059}

--- ios/chrome/browser/keyboard/ui_bundled/menu_builder.mm
+++ ios/chrome/browser/keyboard/ui_bundled/menu_builder.mm
@@ -22,97 +22,191 @@
     return;
   }
 
-  // Application: Remove the Application menu, as it contains a system entry to
-  // open the Settings app, as it conflicts with the in-app settings key
-  // command.
-  [builder removeMenuForIdentifier:UIMenuApplication];
+  if (@available(iOS 26, *)) {
+    // Application: Remove the Application menu, as it contains a system entry
+    // to open the Settings app, as it conflicts with the in-app settings key
+    // command.
+    [builder removeMenuForIdentifier:UIMenuApplication];
 
-  // File: Add elements.
-  NSArray<UIMenuElement*>* fileElements = @[
-    UIKeyCommand.cr_openNewTab,
-    UIKeyCommand.cr_openNewIncognitoTab,
-    UIKeyCommand.cr_openNewWindow,
-    UIKeyCommand.cr_openNewIncognitoWindow,
-    UIKeyCommand.cr_openLocation,
-    UIKeyCommand.cr_closeTab,
-    UIKeyCommand.cr_voiceSearch,
-    UIKeyCommand.cr_closeAll,
-  ];
-  [self insertElements:fileElements
-      atStartOfMenuForIdentifier:UIMenuFile
-                       inBuilder:builder];
+    // File: Add elements.
+    NSArray<UIMenuElement*>* fileElements = @[
+      UIKeyCommand.cr_openNewTab,
+      UIKeyCommand.cr_openNewIncognitoTab,
+      UIKeyCommand.cr_openNewWindow,
+      UIKeyCommand.cr_openNewIncognitoWindow,
+      UIKeyCommand.cr_openLocation,
+      UIKeyCommand.cr_closeTab,
+      UIKeyCommand.cr_voiceSearch,
+      UIKeyCommand.cr_closeAll,
+    ];
+    [self insertElements:fileElements
+        atStartOfMenuForIdentifier:UIMenuFile
+                         inBuilder:builder];
 
-  // Edit: Replace the Find actions.
-  NSArray<UIMenuElement*>* findElements = @[
-    UIKeyCommand.cr_find,
-    UIKeyCommand.cr_findNext,
-    UIKeyCommand.cr_findPrevious,
-  ];
-  [builder replaceChildrenOfMenuForIdentifier:UIMenuFind
-                            fromChildrenBlock:^(NSArray<UIMenuElement*>* _) {
-                              return findElements;
-                            }];
+    // Edit: Replace the Find actions.
+    NSArray<UIMenuElement*>* findElements = @[
+      UIKeyCommand.cr_find,
+      UIKeyCommand.cr_findNext,
+      UIKeyCommand.cr_findPrevious,
+    ];
+    [builder replaceChildrenOfMenuForIdentifier:UIMenuFind
+                              fromChildrenBlock:^(NSArray<UIMenuElement*>* _) {
+                                return findElements;
+                              }];
 
-  // Format: Remove the system entry to format text.
-  [builder removeMenuForIdentifier:UIMenuFormat];
+    // Format: Remove the system entry to format text.
+    [builder removeMenuForIdentifier:UIMenuFormat];
 
-  // View: Add elements.
-  NSArray<UIMenuElement*>* viewElements = @[
-    UIKeyCommand.cr_stop,
-    UIKeyCommand.cr_reload,
-    UIKeyCommand.cr_goToTabGrid,
-  ];
-  [self insertElements:viewElements
-      atStartOfMenuForIdentifier:UIMenuView
-                       inBuilder:builder];
+    // View: Add elements.
+    NSArray<UIMenuElement*>* viewElements = @[
+      UIKeyCommand.cr_stop,
+      UIKeyCommand.cr_reload,
+      UIKeyCommand.cr_goToTabGrid,
+    ];
+    [self insertElements:viewElements
+        atStartOfMenuForIdentifier:UIMenuView
+                         inBuilder:builder];
 
-  // History: Add new menu.
-  UIMenu* historyMenu =
-      [UIMenu menuWithTitle:NSLocalizedString(@"IDS_IOS_KEYBOARD_HISTORY", @"")
-                   children:@[
-                     UIKeyCommand.cr_back,
-                     UIKeyCommand.cr_forward,
-                     UIKeyCommand.cr_reopenLastClosedTab,
-                     UIKeyCommand.cr_showHistory,
-                     UIKeyCommand.cr_clearBrowsingData,
-                   ]];
-  [builder insertSiblingMenu:historyMenu afterMenuForIdentifier:UIMenuView];
+    // History: Add new menu.
+    UIMenu* historyMenu = [UIMenu
+        menuWithTitle:NSLocalizedString(@"IDS_IOS_KEYBOARD_HISTORY", @"")
+             children:@[
+               UIKeyCommand.cr_back,
+               UIKeyCommand.cr_forward,
+               UIKeyCommand.cr_reopenLastClosedTab,
+               UIKeyCommand.cr_showHistory,
+               UIKeyCommand.cr_clearBrowsingData,
+             ]];
+    [builder insertSiblingMenu:historyMenu afterMenuForIdentifier:UIMenuView];
 
-  // Bookmarks: Add new menu.
-  UIMenu* bookmarksMenu = [UIMenu
-      menuWithTitle:NSLocalizedString(@"IDS_IOS_KEYBOARD_BOOKMARKS", @"")
-           children:@[
-             UIKeyCommand.cr_showBookmarks,
-             UIKeyCommand.cr_addToBookmarks,
-             UIKeyCommand.cr_showReadingList,
-             UIKeyCommand.cr_addToReadingList,
-           ]];
-  [builder insertSiblingMenu:bookmarksMenu
-      afterMenuForIdentifier:historyMenu.identifier];
+    // Bookmarks: Add new menu.
+    UIMenu* bookmarksMenu = [UIMenu
+        menuWithTitle:NSLocalizedString(@"IDS_IOS_KEYBOARD_BOOKMARKS", @"")
+             children:@[
+               UIKeyCommand.cr_showBookmarks,
+               UIKeyCommand.cr_addToBookmarks,
+               UIKeyCommand.cr_showReadingList,
+               UIKeyCommand.cr_addToReadingList,
+             ]];
+    [builder insertSiblingMenu:bookmarksMenu
+        afterMenuForIdentifier:historyMenu.identifier];
 
-  // Window: Add elements.
-  NSArray<UIMenuElement*>* windowElements = @[
-    UIKeyCommand.cr_showNextTab,
-    UIKeyCommand.cr_showPreviousTab,
-    UIKeyCommand.cr_select1,
-    UIKeyCommand.cr_select2,
-    UIKeyCommand.cr_select3,
-    UIKeyCommand.cr_select9,
-    UIKeyCommand.cr_showDownloads,
-    UIKeyCommand.cr_showSettings,
-  ];
-  [self insertElements:windowElements
-      atStartOfMenuForIdentifier:UIMenuWindow
-                       inBuilder:builder];
+    // Window: Add elements.
+    NSArray<UIMenuElement*>* windowElements = @[
+      UIKeyCommand.cr_showNextTab,
+      UIKeyCommand.cr_showPreviousTab,
+      UIKeyCommand.cr_select1,
+      UIKeyCommand.cr_select2,
+      UIKeyCommand.cr_select3,
+      UIKeyCommand.cr_select9,
+      UIKeyCommand.cr_showDownloads,
+      UIKeyCommand.cr_showSettings,
+    ];
+    [self insertElements:windowElements
+        atStartOfMenuForIdentifier:UIMenuWindow
+                         inBuilder:builder];
 
-  // Help: Add elements.
-  NSArray<UIMenuElement*>* helpElements = @[
-    UIKeyCommand.cr_showHelp,
-    UIKeyCommand.cr_reportAnIssue,
-  ];
-  [self insertElements:helpElements
-      atStartOfMenuForIdentifier:UIMenuHelp
-                       inBuilder:builder];
+    // Help: Add elements.
+    NSArray<UIMenuElement*>* helpElements = @[
+      UIKeyCommand.cr_showHelp,
+      UIKeyCommand.cr_reportAnIssue,
+    ];
+    [self insertElements:helpElements
+        atStartOfMenuForIdentifier:UIMenuHelp
+                         inBuilder:builder];
+  } else {
+    // Application: Remove the Application menu, as it contains a system entry
+    // to open the Settings app, as it conflicts with the in-app settings key
+    // command.
+    [builder removeMenuForIdentifier:UIMenuApplication];
+
+    // File: Add elements.
+    NSArray<UIMenuElement*>* fileElements = @[
+      UIKeyCommand.cr_openNewTab,
+      UIKeyCommand.cr_openNewIncognitoTab,
+      UIKeyCommand.cr_openNewWindow,
+      UIKeyCommand.cr_openNewIncognitoWindow,
+      UIKeyCommand.cr_openLocation,
+      UIKeyCommand.cr_closeTab,
+      UIKeyCommand.cr_voiceSearch,
+      UIKeyCommand.cr_closeAll,
+    ];
+    [self insertElements:fileElements
+        atStartOfMenuForIdentifier:UIMenuFile
+                         inBuilder:builder];
+
+    // Edit: Replace the Find actions.
+    NSArray<UIMenuElement*>* findElements = @[
+      UIKeyCommand.cr_find,
+      UIKeyCommand.cr_findNext,
+      UIKeyCommand.cr_findPrevious,
+    ];
+    [builder replaceChildrenOfMenuForIdentifier:UIMenuFind
+                              fromChildrenBlock:^(NSArray<UIMenuElement*>* _) {
+                                return findElements;
+                              }];
+
+    // Format: Remove the system entry to format text.
+    [builder removeMenuForIdentifier:UIMenuFormat];
+
+    // View: Add elements.
+    NSArray<UIMenuElement*>* viewElements = @[
+      UIKeyCommand.cr_stop,
+      UIKeyCommand.cr_reload,
+      UIKeyCommand.cr_goToTabGrid,
+    ];
+    [self insertElements:viewElements
+        atStartOfMenuForIdentifier:UIMenuView
+                         inBuilder:builder];
+
+    // History: Add new menu.
+    UIMenu* historyMenu = [UIMenu
+        menuWithTitle:NSLocalizedString(@"IDS_IOS_KEYBOARD_HISTORY", @"")
+             children:@[
+               UIKeyCommand.cr_back,
+               UIKeyCommand.cr_forward,
+               UIKeyCommand.cr_reopenLastClosedTab,
+               UIKeyCommand.cr_showHistory,
+               UIKeyCommand.cr_clearBrowsingData,
+             ]];
+    [builder insertSiblingMenu:historyMenu afterMenuForIdentifier:UIMenuView];
+
+    // Bookmarks: Add new menu.
+    UIMenu* bookmarksMenu = [UIMenu
+        menuWithTitle:NSLocalizedString(@"IDS_IOS_KEYBOARD_BOOKMARKS", @"")
+             children:@[
+               UIKeyCommand.cr_showBookmarks,
+               UIKeyCommand.cr_addToBookmarks,
+               UIKeyCommand.cr_showReadingList,
+               UIKeyCommand.cr_addToReadingList,
+             ]];
+    [builder insertSiblingMenu:bookmarksMenu
+        afterMenuForIdentifier:historyMenu.identifier];
+
+    // Window: Add elements.
+    NSArray<UIMenuElement*>* windowElements = @[
+      UIKeyCommand.cr_showNextTab,
+      UIKeyCommand.cr_showPreviousTab,
+      UIKeyCommand.cr_select1,
+      UIKeyCommand.cr_select2,
+      UIKeyCommand.cr_select3,
+      UIKeyCommand.cr_select9,
+      UIKeyCommand.cr_showDownloads,
+      UIKeyCommand.cr_showSettings,
+    ];
+    [self insertElements:windowElements
+        atStartOfMenuForIdentifier:UIMenuWindow
+                         inBuilder:builder];
+
+    // Help: Add elements.
+    NSArray<UIMenuElement*>* helpElements = @[
+      UIKeyCommand.cr_showHelp,
+      UIKeyCommand.cr_reportAnIssue,
+    ];
+    [self insertElements:helpElements
+        atStartOfMenuForIdentifier:UIMenuHelp
+                         inBuilder:builder];
+  }
 }
 
 #pragma mark - Private
