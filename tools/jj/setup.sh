6b4bec780afe1b75a4f837bbba1f03ed000b4260
Matt Stark <msta@google.com>
2025-08-11 21:26:37 -0700

    Add a jj setup script which initializes the chromium repo with all mandatory configs.
    Change-Id: I6a6a69641534139e74f319ae0db199ac64a3dbd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6840414
    Auto-Submit: Matt Stark <msta@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1499958}

---
+++ tools/jj/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Repo root
+cd "$(dirname ${BASH_SOURCE[0]})/../.."
+
+if [[ ! -d .jj ]]; then
+  jj git init --colocate .
+  ln -s "$(realpath tools/jj/config.toml)" .jj/repo/config.toml
+fi
+
+# Ensure that jj snapshots your current commit so it doesn't get lost with git
+# switch.
+jj new
+
+# Fix issues with line endings. See go/jj-in-chromium.
+git config core.autocrlf false
+git switch origin/main --detach
+jj abandon
+git add -A
+
+echo "Reminder: If you haven't already, we recommend joining https://groups.google.com/g/chromium-jj-users"
+
