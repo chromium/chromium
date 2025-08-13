62246851e990677fad12b0de1902d41188ec955c
Tom Anderson <thomasanderson@chromium.org>
2025-08-12 20:09:04 -0700

    Reland "Run ShutdownOnDBusThreadAndBlock in RenderViewHostTestHarness::TearDown"
    This is a reland of commit 5e82e976f14f095e1c709b7c3becf2812f1f73ab
    
    Cq-Include-Trybots: luci.chromium.try:linux-cast-arm64-rel
    
    Original change's description:
    > Run ShutdownOnDBusThreadAndBlock in RenderViewHostTestHarness::TearDown
    >
    > Recreating the bus between test runs is necessary since the
    > TaskEnvironment gets recreated, and the bus would otherwise hold onto
    > references to the task runners from the stale TaskEnvironment.
    >
    > Bug: 437901065
    > Change-Id: Ib7511c74ad809067df829f6dcfa9efff92b60c1a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6838820
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1500316}
    
    Bug: 437901065
    Change-Id: I91beed4ce41529eab828fbcea178557ee7dab071
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6842654
    Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500572}

--- content/public/test/test_renderer_host.cc
+++ content/public/test/test_renderer_host.cc
@@ -11,6 +11,7 @@
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/task_environment.h"
 #include "build/build_config.h"
+#include "build/config/linux/dbus/buildflags.h"
 #include "components/input/render_widget_host_input_event_router.h"
 #include "content/browser/gpu/gpu_data_manager_impl.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
@@ -60,6 +61,10 @@
 
 #if BUILDFLAG(IS_MAC)
 #include "ui/accelerated_widget_mac/window_resize_helper_mac.h"
+#endif
+
+#if BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
+#include "components/dbus/thread_linux/dbus_thread_linux.h"
 #endif
 
 namespace content {
@@ -274,6 +279,10 @@ void RenderViewHostTestHarness::TearDown() {
   // Make sure that we flush any messages related to WebContentsImpl destruction
   // before we destroy the browser context.
   base::RunLoop().RunUntilIdle();
+
+#if BUILDFLAG(IS_LINUX) && BUILDFLAG(USE_DBUS)
+  dbus_thread_linux::ShutdownOnDBusThreadAndBlock();
+#endif
 
 #if BUILDFLAG(IS_WIN)
   ole_initializer_.reset();
