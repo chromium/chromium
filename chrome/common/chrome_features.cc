760bcf2cd0f1e09e9f6adeb905a0d32b58223edf
Erik Chen <erikchen@chromium.org>
2025-08-13 12:26:50 -0700

    actor: Reduce page stabilization timeout to 1s.
    Page stabilization waiting logic is not working properly.
    
    Bug: 438478203
    Change-Id: If68647c366cfab7156cc7dccd7d80b0fc9519c11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6847064
    Reviewed-by: David Bokan <bokan@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1500949}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -398,7 +398,7 @@ const base::FeatureParam<base::TimeDelta> kGlicActorPageStabilityLocalTimeout{
 
 // The overall observation timeout when waiting on a renderer tool to complete.
 const base::FeatureParam<base::TimeDelta> kGlicActorPageStabilityTimeout{
-    &kGlicActor, "glic-actor-page-stability-timeout", base::Seconds(10)};
+    &kGlicActor, "glic-actor-page-stability-timeout", base::Seconds(1)};
 
 // An artificial delay before signalling the tools that the page has become
 // stable.
