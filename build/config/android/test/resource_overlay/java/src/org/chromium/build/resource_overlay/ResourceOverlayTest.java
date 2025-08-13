chromium/chromium

commit 814fb1e64612b502875b64e82a95a2ca3a4a5af9

Android: Refactor write_build_config.py
The main goal is to speed it up by having it read only .params.json
files instead of also needing to read dependent .build_config.json
files.

There's also a good number of simplifications included, including
fixing lists not always being topologically sorted.

Running: autoninja --offline $(cat all_build_configs.txt)
Before: 50 seconds
After: 16 seconds

This removes dependent .build_config files as inputs in most cases,
so improves remote build times.
Running: autoninja $(cat all_build_configs.txt)
Before: 42 seconds
After: 14 seconds

Bug: 413331591
Change-Id: I58f424ffc14943089b1f7625a417abb586e6fb92
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6639823
Commit-Queue: Andrew Grieve <agrieve@chromium.org>
Reviewed-by: Sam Maier <smaier@chromium.org>
Owners-Override: Andrew Grieve <agrieve@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1478885}

third_party/mockito/BUILD.gn
@@ -35,6 +35,9 @@ android_assets("mockito_apk_assets") {
 android_library("mockito_modified_files_java") {
   testonly = true
   deps = [
+    # Depend on org_mockito_mockito_android_java to ensure that this .jar comes
+    # earlier in the classpath.
+    "//third_party/android_deps:org_mockito_mockito_android_java",
     "//third_party/android_deps:org_mockito_mockito_core_java",
     "//third_party/junit:junit",
   ]

build/android/PRESUBMIT.py
@@ -98,7 +98,6 @@ def CommonChecks(input_api, output_api, *, is_upload):
       J('gyp', 'java_cpp_features_tests.py'),
       J('gyp', 'java_cpp_strings_tests.py'),
       J('gyp', 'java_google_api_keys_tests.py'),
-      J('gyp', 'util', 'build_utils_test.py'),
       J('gyp', 'util', 'manifest_utils_test.py'),
       J('gyp', 'util', 'md5_check_test.py'),
       J('gyp', 'util', 'resource_utils_test.py'),

build/android/gradle/generate_gradle.py
@@ -158,9 +158,9 @@ class _ProjectEntry:
     return cls._cached_entries[gn_target]
 
   @classmethod
-  def FromBuildConfigPath(cls, path):
+  def FromParamsJsonPath(cls, path):
     prefix = 'gen/'
-    suffix = '.build_config.json'
+    suffix = '.params.json'
     assert path.startswith(prefix) and path.endswith(suffix), path
     subdir = path[len(prefix):-len(suffix)]
     gn_target = '//%s:%s' % (os.path.split(subdir))
@@ -249,9 +249,12 @@ class _ProjectEntry:
     This includes the entry itself to make iterating simpler."""
     if self._all_entries is None:
       logging.debug('Generating entries for %s', self.GnTarget())
-      deps = [_ProjectEntry.FromBuildConfigPath(p)
-          for p in self.Gradle()['dependent_android_projects']]
-      deps.extend(_ProjectEntry.FromBuildConfigPath(p)
+      deps = [
+          _ProjectEntry.FromParamsJsonPath(p)
+          for p in self.Gradle()['dependent_android_projects']
+      ]
+      deps.extend(
+          _ProjectEntry.FromParamsJsonPath(p)
           for p in self.Gradle()['dependent_java_projects'])
       all_entries = set()
       for dep in deps:
@@ -369,11 +372,15 @@ class _ProjectContextGenerator:
     self.processed_res_dirs.update(res_dirs)
     variables['res_dirs'] = self._Relativize(root_entry, res_dirs)
     if self.split_projects:
-      deps = [_ProjectEntry.FromBuildConfigPath(p)
-              for p in root_entry.Gradle()['dependent_android_projects']]
+      deps = [
+          _ProjectEntry.FromParamsJsonPath(p)
+          for p in root_entry.Gradle()['dependent_android_projects']
+      ]
       variables['android_project_deps'] = [d.ProjectName() for d in deps]
-      deps = [_ProjectEntry.FromBuildConfigPath(p)
-              for p in root_entry.Gradle()['dependent_java_projects']]
+      deps = [
+          _ProjectEntry.FromParamsJsonPath(p)
+          for p in root_entry.Gradle()['dependent_java_projects']
+      ]
       variables['java_project_deps'] = [d.ProjectName() for d in deps]
     return variables
 
@@ -725,7 +732,7 @@ def _FindAllProjectEntries(main_entries):
     found.add(cur_entry)
     sub_config_paths = cur_entry.BuildConfig()['deps_configs']
     to_scan.extend(
-        _ProjectEntry.FromBuildConfigPath(p) for p in sub_config_paths)
+        _ProjectEntry.FromParamsJsonPath(p) for p in sub_config_paths)
   return list(found)
 
 

build/android/gyp/util/build_utils.py
@@ -5,7 +5,6 @@
 """Contains common helpers for GN action()s."""
 
 import atexit
-import collections
 import contextlib
 import fnmatch
 import json
@@ -325,34 +324,6 @@ def ExtractAll(zip_path, path=None, no_clobber=True, pattern=None,
 def MatchesGlob(path, filters):
   """Returns whether the given path matches any of the given glob patterns."""
   return filters and any(fnmatch.fnmatch(path, f) for f in filters)
-
-
-def GetSortedTransitiveDependencies(top, deps_func):
-  """Gets the list of all transitive dependencies in sorted order.
-
-  There should be no cycles in the dependency graph (crashes if cycles exist).
-
-  Args:
-    top: A list of the top level nodes
-    deps_func: A function that takes a node and returns a list of its direct
-        dependencies.
-  Returns:
-    A list of all transitive dependencies of nodes in top, in order (a node will
-    appear in the list at a higher index than all of its dependencies).
-  """
-  # Find all deps depth-first, maintaining original order in the case of ties.
-  deps_map = collections.OrderedDict()
-
-  def discover(nodes):
-    for node in nodes:
-      if node in deps_map:
-        continue
-      deps = deps_func(node)
-      discover(deps)
-      deps_map[node] = deps
-
-  discover(top)
-  return list(deps_map)
 
 
 def InitLogging(enabling_env):

build/android/gyp/util/build_utils_test.py
@@ -1,48 +1,0 @@
-#!/usr/bin/env python3
-# Copyright 2018 The Chromium Authors
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-import collections
-import os
-import sys
-import unittest
-
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
-from util import build_utils
-
-_DEPS = collections.OrderedDict()
-_DEPS['a'] = []
-_DEPS['b'] = []
-_DEPS['c'] = ['a']
-_DEPS['d'] = ['a']
-_DEPS['e'] = ['f']
-_DEPS['f'] = ['a', 'd']
-_DEPS['g'] = []
-_DEPS['h'] = ['d', 'b', 'f']
-_DEPS['i'] = ['f']
-
-
-class BuildUtilsTest(unittest.TestCase):
-  def testGetSortedTransitiveDependencies_all(self):
-    TOP = _DEPS.keys()
-    EXPECTED = ['a', 'b', 'c', 'd', 'f', 'e', 'g', 'h', 'i']
-    actual = build_utils.GetSortedTransitiveDependencies(TOP, _DEPS.get)
-    self.assertEqual(EXPECTED, actual)
-
-  def testGetSortedTransitiveDependencies_leaves(self):
-    TOP = ['c', 'e', 'g', 'h', 'i']
-    EXPECTED = ['a', 'c', 'd', 'f', 'e', 'g', 'b', 'h', 'i']
-    actual = build_utils.GetSortedTransitiveDependencies(TOP, _DEPS.get)
-    self.assertEqual(EXPECTED, actual)
-
-  def testGetSortedTransitiveDependencies_leavesReverse(self):
-    TOP = ['i', 'h', 'g', 'e', 'c']
-    EXPECTED = ['a', 'd', 'f', 'i', 'b', 'h', 'g', 'e', 'c']
-    actual = build_utils.GetSortedTransitiveDependencies(TOP, _DEPS.get)
-    self.assertEqual(EXPECTED, actual)
-
-
-if __name__ == '__main__':
-  unittest.main()

build/android/gyp/util/params_json_util.py
@@ -1,0 +1,459 @@
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Provides functions for reading and parsing .params.json files.
+
+This module is used by build scripts to access parameters of GN targets and
+their transitive dependencies.
+
+It exposes helper methods to:
+  * Read and cache JSON files.
+  * Access transitive dependencies.
+  * Filter and collect information from dependencies.
+
+The main entry point is `get_params()`, which returns a `ParamsJson` object.
+This object is a dictionary-like view of a .params.json file with added helper
+methods for dependency traversal.
+"""
+
+import functools
+import json
+
+# Types that should never be used as a dependency of another build config.
+_ROOT_TYPES = frozenset([
+    'android_apk', 'java_binary', 'java_annotation_processor',
+    'robolectric_binary', 'android_app_bundle'
+])
+# Types that should not allow code deps to pass through.
+_RESOURCE_TYPES = frozenset(['android_assets', 'android_resources'])
+
+_COLLECTS_RESOURCES_TYPES = frozenset([
+    'android_apk',
+    'android_app_bundle_module',
+    'dist_aar',
+    'robolectric_binary',
+])
+
+_COLLECTS_DEVICE_CLASSPATH_TYPES = frozenset([
+    'android_apk',
+    'android_app_bundle_module',
+    'android_app_bundle',
+    'dist_jar',  # dist_dex uses this.
+])
+
+_COLLECTS_HOST_CLASSPATH_TYPES = frozenset([
+    'dist_aar',
+    'dist_jar',
+    'java_binary',
+    'robolectric_binary',
+])
+
+_COLLECTS_NATIVE_LIBRARIES_TYPES = frozenset([
+    'android_apk',
+    'android_app_bundle_module',
+    'robolectric_binary',
+])
+
+_CLASSPATH_TYPES = frozenset([
+    'android_apk',
+    'android_app_bundle_module',
+    'dist_aar',
+    'dist_jar',
+    'java_annotation_processor',
+    'java_binary',
+    'java_library',
+    'robolectric_binary',
+])
+
+# Track inputs for use in depfiles.
+_input_paths = []
+
+
+def all_read_file_paths():
+  """Returns a list of all paths read by _get_json()."""
+  return list(_input_paths)
+
+
+def _get_json(path):
+  """Reads a JSON file and records the path for depfile tracking."""
+  _input_paths.append(path)
+  with open(path, encoding='utf-8') as f:
+    config = json.load(f)
+  return config
+
+
+@functools.cache  # pylint: disable=method-cache-max-size-none
+def get_build_config(path):
+  """Cached version of _get_json() for .build_config.json files."""
+  return _get_json(path)
+
+
+@functools.cache  # pylint: disable=method-cache-max-size-none
+def get_params(path):
+  """Returns a cached, dictionary-like object for a .params.json file."""
+  # It's important to cache the ParamsJson object rather than the json dict
+  # because some ParamsJson methods use @cache.
+  return ParamsJson(path, _get_json(path))
+
+
+def _topological_walk(top, deps_func):
+  """Gets the list of all transitive dependencies in topological order.
+
+  Args:
+    top: A list of the top level nodes
+    deps_func: A function that takes a node and returns a list of its direct
+        dependencies.
+  Returns:
+    A list of all transitive dependencies of nodes in top in order (a node will
+    appear in the list at a lower index than all of its dependencies).
+  """
+  seen = {}
+
+  def discover(nodes):
+    for node in nodes:
+      if node in seen:
+        continue
+      deps = deps_func(node)
+      discover(deps)
+      seen[node] = True
+
+  discover(top)
+  return list(reversed(seen))
+
+
+def _filter_deps(deps, restrict_to_resource_types=False):
+  """Filters dependencies based on their type."""
+  if restrict_to_resource_types:
+    # Root types are never is_resource_type().
+    keep_func = lambda x: x.is_resource_type() or x.is_group()
+  else:
+    keep_func = lambda x: not x.is_root_type()
+
+  return [d for d in deps if keep_func(d)]
+
+
+def _collect_public_deps(deps):
+  """Recursively collects all public_deps from a list of dependencies."""
+  ret = []
+  # public_deps() contains public_deps of public_deps, so no need to recurse.
+  for x in deps:
+    ret += x.public_deps()
+  return ret
+
+
+def _deps_for_traversal(dep):
+  """Returns the direct dependencies of a node for traversal."""
+  # Do not let restrict_to_resource_types targets traverse groups directly.
+  # rely on the group's public_deps (which are filtered by
+  # restrict_to_resource_types) to surface the deps.
+  return [] if dep.is_group() else dep.deps()
+
+
+class _HashableList(list):
+  """A list that can be used in a set / dict key."""
+
+  def __init__(self, *args, sealed=False, **kwargs):
+    super().__init__(*args, **kwargs)
+    self._sealed = sealed
+
+  def __hash__(self):
+    self._sealed = True
+    return id(self)
+
+  def __iadd__(self, other):
+    self._check_not_sealed()
+    return super().__iadd__(other)
+
+  def __add__(self, other):
+    return DepsList(super().__add__(other))
+
+  def _check_not_sealed(self):
+    assert not self._sealed, 'Cannot mutate a DepsList after it has been used.'
+
+  def append(self, value):
+    self._check_not_sealed()
+    return super().append(value)
+
+  def extend(self, iterable):
+    self._check_not_sealed()
+    return super().extend(iterable)
+
+  def insert(self, index, value):
+    self._check_not_sealed()
+    return super().insert(index, value)
+
+  def remove(self, value):
+    self._check_not_sealed()
+    return super().remove(value)
+
+  def pop(self, index=-1):
+    self._check_not_sealed()
+    return super().pop(index)
+
+  def clear(self):
+    self._check_not_sealed()
+    return super().clear()
+
+  def sort(self, *, key=None, reverse=False):
+    self._check_not_sealed()
+    return super().sort(key=key, reverse=reverse)
+
+  def reverse(self):
+    self._check_not_sealed()
+    return super().reverse()
+
+
+class DepsList(_HashableList):
+  """A list of ParamsJson objects with helper methods for traversal."""
+
+  def __repr__(self):
+    return ','.join(repr(x) for x in self)
+
+  @functools.cache  # pylint: disable=method-cache-max-size-none
+  def recursive(self):
+    """Returns all transitive dependencies."""
+    # Reverse so that deps appear with higher indices.
+    return self.walk(lambda x: True)
+
+  @functools.cache  # pylint: disable=method-cache-max-size-none
+  def recursive_resource_deps(self):
+    """Returns all transitive resource dependencies.
+
+    This is a special traversal for resources, which have different rules
+    for how they depend on other targets.
+    """
+
+    def helper(x):
+      if not x.get('recursive_resource_deps'):
+        return True
+      # Get unfiltered deps so that libraries come through.
+      ret = [get_params(p) for p in x.get('public_deps_configs', [])]
+      ret += _collect_public_deps(ret)
+      return ret
+
+    return self.walk(helper)
+
+  def of_type(self, target_type):
+    """Filters the list to dependencies of a specific GN target type."""
+    return DepsList(d for d in self if d.type == target_type)
+
+  def not_of_type(self, target_type):
+    """Filters the list to dependencies not of a specific GN target type."""
+    return DepsList(d for d in self if d.type != target_type)
+
+  def filter(self, cond):
+    """Filters the list based on a custom condition."""
+    return DepsList(p for p in self if cond(p))
+
+  def walk(self, visit_func):
+    """Performs a topological walk, allowing for custom traversal logic.
+
+    Args:
+      visit_func: A function called for each node. It can return:
+          * True (or None): Recurse normally.
+          * False: Prune this node and its descendants from the result.
+          * A list: Use this list as the node's children instead of its
+            actual dependencies.
+    """
+    to_prune = set()
+
+    def deps_func(dep):
+      children = visit_func(dep)
+
+      if children in (None, True):
+        children = _deps_for_traversal(dep)
+      if children is False:
+        to_prune.add(dep)
+        children = []
+      return children
+
+    ret = _topological_walk(self, deps_func)
+    if to_prune:
+      ret = [x for x in ret if x not in to_prune]
+    return DepsList(ret)
+
+  def collect(self, key_name, flatten=False):
+    """Collects values for a given key from all dependencies in the list.
+
+    Args:
+      key_name: The key to look up in each dependency's params.
+      flatten: If True, and the values are lists, flatten them into a
+          single list.
+    """
+    if flatten:
+      ret = []
+      for p in self:
+        if values := p.get(key_name):
+          ret += values
+      return ret
+
+    return [p[key_name] for p in self if key_name in p]
+
+
+class ParamsJson(dict):
+  """A dictionary-like view of a .params.json file with helper methods."""
+
+  def __init__(self, path, json_dict):
+    super().__init__(json_dict)
+    self.path = path
+    self.type = self['type']
+
+  def __hash__(self):
+    return id(self)
+
+  def __eq__(self, other):
+    return self is other
+
+  def __repr__(self):
+    return f'<{self.path}>'
+
+  def build_config_path(self):
+    """Returns the corresponding .build_config.json path."""
+    return self.path.replace('.params.json', '.build_config.json')
+
+  def build_config_json(self):
+    """Returns the parsed JSON of the corresponding .build_config.json."""
+    return get_build_config(self.build_config_path())
+
+  def is_root_type(self):
+    """Returns True if the target type is a "root" type (e.g., an APK)."""
+    return self.type in _ROOT_TYPES
+
+  def collects_resources(self):
+    """Returns True if the target type collects Android resources."""
+    return self.type in _COLLECTS_RESOURCES_TYPES
+
+  def collects_device_classpath(self):
+    """Returns True if the target type collects a device-side classpath."""
+    return self.type in _COLLECTS_DEVICE_CLASSPATH_TYPES
+
+  def collects_host_classpath(self):
+    """Returns True if the target type collects a host-side classpath."""
+    return self.type in _COLLECTS_HOST_CLASSPATH_TYPES
+
+  def collects_native_libraries(self):
+    """Returns True if the target type collects native libraries."""
+    return self.type in _COLLECTS_NATIVE_LIBRARIES_TYPES
+
+  def has_classpath(self):
+    """Returns True if the target type has a classpath."""
+    return self.type in _CLASSPATH_TYPES
+
+  def is_resource_type(self):
+    """Returns True if the target is an Android resource type."""
+    return self.type in _RESOURCE_TYPES
+
+  def is_apk(self):
+    return self.type == 'android_apk'
+
+  def is_bundle(self):
+    return self.type == 'android_app_bundle'
+
+  def is_bundle_module(self):
+    return self.type == 'android_app_bundle_module'
+
+  def is_dist_jar(self):
+    return self.type == 'dist_jar'
+
+  def is_group(self):
+    return self.type == 'group'
+
+  def is_library(self):
+    return self.type == 'java_library'
+
+  def is_system_library(self):
+    return self.type == 'system_java_library'
+
+  def requires_android(self):
+    """Returns True if the target requires the Android platform."""
+    return self.is_resource_type or self.get('requires_android', False)
+
+  def supports_android(self):
+    """Returns True if the target supports the Android platform."""
+    return self.get('supports_android', True)
+
+  def _direct_deps(self):
+    """Returns only the direct dependencies (from `deps_configs`)."""
+    # android_resources use only public_dep_configs, so no need for
+    # restrict_to_resource_types.
+    return [get_params(p) for p in self.get('deps_configs', [])]
+
+  @functools.cache  # pylint: disable=method-cache-max-size-none
+  def _cached_direct_public_deps(self):
+    """Returns only the direct public dependencies."""
+    deps = [get_params(p) for p in self.get('public_deps_configs', [])]
+    return _filter_deps(deps,
+                        restrict_to_resource_types=self.is_resource_type())
+
+  @functools.cache  # pylint: disable=method-cache-max-size-none
+  def deps(self):
+    """Returns all dependencies, from both deps and public_deps."""
+    deps = DepsList(self._direct_deps())
+    deps += self._cached_direct_public_deps()
+    deps += _filter_deps(_collect_public_deps(deps),
+                         restrict_to_resource_types=self.is_resource_type())
+    # Return a cached DepsList so that multiple calls to .deps().recursive()
+    # result in a cache hit.
+    return DepsList(dict.fromkeys(deps), sealed=True)
+
+  @functools.cache  # pylint: disable=method-cache-max-size-none
+  def public_deps(self):
+    """Returns direct public dependencies and their transitive public_deps."""
+    deps = self._cached_direct_public_deps()
+    deps = deps + _filter_deps(
+        _collect_public_deps(deps),
+        restrict_to_resource_types=self.is_resource_type())
+    return DepsList(deps, sealed=True)
+
+  @functools.cache  # pylint: disable=method-cache-max-size-none
+  def processor_deps(self):
+    """Returns all transitive annotation processor dependencies."""
+    deps = [get_params(p) for p in self.get('processor_configs', [])]
+    deps += _filter_deps(_collect_public_deps(deps))
+    return DepsList(dict.fromkeys(deps), sealed=True)
+
+  def apk_under_test(self):
+    """Returns the ParamsJson for the apk_under_test, or None."""
+    if path := self.get('apk_under_test_config'):
+      return get_params(path)
+    return None
+
+  def module_deps(self):
+    """For a bundle, returns the ParamsJson for all module dependencies."""
+    return self.deps().of_type('android_app_bundle_module')
+
+  def parent_module(self):
+    """For a bundle module, returns its direct parent module."""
+    assert self.is_bundle_module()
+    module_deps = self.module_deps()
+
+    if self['module_name'] == 'base':
+      assert not module_deps, ('Base module should not depend on ' +
+                               ','.join(module_deps.collect('module_name')))
+      return None
+
+    assert len(module_deps) != 0, 'Should depend on base module'
+    assert len(module_deps) == 1, (
+        'Can depend on only one parent module. Found: ' +
+        ','.join(module_deps.collect('module_name')))
+    return module_deps[0]
+
+  def base_module(self):
+    """For a bundle module, returns the root 'base' module."""
+    # Find the base split.
+    ret = self
+    while not ret.is_base_module():
+      ret = ret.parent_module()
+    return ret
+
+  def is_base_module(self):
+    """Returns True if this is the base module of an app bundle."""
+    return self.is_bundle_module() and self.parent_module() is None
+
+  def resource_deps(self):
+    """Returns the transitive resource dependencies."""
+    # For Java libraries, restrict to resource targets that are direct deps, or
+    # are indirect via other resource targets.
+    if self.is_library():
+      return self.deps().of_type('android_resources')
+    return self.deps().recursive_resource_deps().of_type('android_resources')

build/android/gyp/write_build_config.pydeps
@@ -4,4 +4,5 @@
 ../../gn_helpers.py
 util/__init__.py
 util/build_utils.py
+util/params_json_util.py
 write_build_config.py

commit 3f8f497e8cf5577f13da969344c48517389d9f3b

Fix dependency resource overlays incorrectly overriding root overlays
Resource overlays were being processed in the wrong order when both
dependency and root targets had resource_overlay=true, causing the
dependency resources to override the root ones instead of the intended
behavior.

This regression was introduced in 814fb1e64612b when
build_utils.GetSortedTransitiveDependencies() was replaced with
params_json_utils._topological_walk(). The old function returned
dependencies before root targets, which worked correctly with AAPT2's -R
flag processing. The new topological walk reverses the list, putting
root targets before dependencies, causing dependencies to override
root targets in AAPT2.

The fix reverses the dependency_zip_overlays list so that root targets
come after their dependencies in AAPT2's processing order, restoring
the correct override behavior where root targets override dependencies.

Added test case for both_tagged scenario where both dependency and
root targets have resource_overlay=true.

Bug: 438476052
Change-Id: Iebc6263e4f4bfcdb95335640064e048de779d578
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6847100
Reviewed-by: Andrew Grieve <agrieve@chromium.org>
Commit-Queue: Andrew Grieve <agrieve@chromium.org>
Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1500951}

build/android/gyp/write_build_config.py
@@ -732,7 +732,14 @@ def main():
   if params.collects_resources():
     # Safe to sort: Build checks that non-overlay resource have no overlap.
     config['dependency_zips'] = sorted(tv.dependency_zips)
-    config['dependency_zip_overlays'] = list(tv.dependency_zip_overlays)
+
+    overlay_list = list(tv.dependency_zip_overlays)
+    # Reverse overlay list so that dependents override dependencies
+    # The topological walk puts dependencies before dependents, but for resource
+    # overlays we need dependents to come after dependencies in AAPT2's -R list
+    overlay_list.reverse()
+    config['dependency_zip_overlays'] = overlay_list
+
     config['assets'] = sorted(tv.assets)
     config['uncompressed_assets'] = sorted(tv.uncompressed_assets)
 

build/config/android/test/resource_overlay/BUILD.gn
@@ -43,6 +43,19 @@ test_resources("root_tagged_root_resources") {
   deps = [ ":root_tagged_dependency_resources" ]
 }
 
+test_resources("both_tagged_dependency_resources") {
+  resource_overlay = true
+  resource_name = "resource_overlay_both_tagged_secret"
+  resource_value = 41
+}
+
+test_resources("both_tagged_root_resources") {
+  resource_overlay = true
+  resource_name = "resource_overlay_both_tagged_secret"
+  resource_value = 42
+  deps = [ ":both_tagged_dependency_resources" ]
+}
+
 android_library("unit_device_javatests") {
   testonly = true
   sources = [
@@ -50,6 +63,7 @@ android_library("unit_device_javatests") {
   ]
   resources_package = "org.chromium.build.resource_overlay"
   deps = [
+    ":both_tagged_root_resources",
     ":dependency_tagged_root_resources",
     ":root_tagged_root_resources",
     "//base:base_java_test_support",

build/config/android/test/resource_overlay/java/src/org/chromium/build/resource_overlay/ResourceOverlayTest.java
@@ -44,4 +44,16 @@ public class ResourceOverlayTest {
         Resources resources = InstrumentationRegistry.getTargetContext().getResources();
         assertEquals(42, resources.getInteger(R.integer.resource_overlay_root_tagged_secret));
     }
+
+    /**
+     * Test that when both an android_resources() target and its dependency have
+     * resource_overlay=true with resources of the same name but different values, the dependent
+     * target's value is used. This tests the fix for dependency ordering in resource overlays.
+     */
+    @Test
+    @SmallTest
+    public void testBothTagged() {
+        Resources resources = InstrumentationRegistry.getTargetContext().getResources();
+        assertEquals(42, resources.getInteger(R.integer.resource_overlay_both_tagged_secret));
+    }
 }