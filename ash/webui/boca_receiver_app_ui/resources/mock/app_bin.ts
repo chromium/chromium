190efb0b712cf08ce9eef2e4c14f4c3bbdec8cc2
Ahmed Nasr <anasr@google.com>
2025-08-13 14:24:23 -0700

    Add boca receiver mock app for non chrome branded builds.
    Bug: b:435165759
    Change-Id: I51ab90e1349e70de4932f64dc0cd8a4c4f356e3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846979
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501032}

---
+++ ash/webui/boca_receiver_app_ui/resources/mock/app_bin.ts
@@ -0,0 +1,8 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const bocaReceiverApp = document.createElement('boca-receiver-app');
+bocaReceiverApp.textContent = `Please build branded Chrome (set
+is_chrome_branded = true) for the official Boca Receiver app.`;
+document.body.appendChild(bocaReceiverApp);
