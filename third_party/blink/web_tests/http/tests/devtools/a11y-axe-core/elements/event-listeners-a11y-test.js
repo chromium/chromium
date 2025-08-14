520df1a9f140311489658d415a0f5e03ef326e71
Changhao Han <changhaohan@chromium.org>
2025-08-14 06:17:46 -0700

    Re-enable skipped test for EventListenersWidget migration
    frontend CL: https://crrev.com/c/6796222
    
    Bug: 407751016
    Change-Id: Ie1f992bfa94ac0e61575c4b0ebaedf54c1e53b5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846623
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501345}

--- third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/elements/event-listeners-a11y-test.js
+++ third_party/blink/web_tests/http/tests/devtools/a11y-axe-core/elements/event-listeners-a11y-test.js
@@ -28,6 +28,7 @@ import * as UI from 'devtools/ui/legacy/legacy.js';
     TestRunner.addResult('Running the axe-core linter on tree element.');
     await AxeCoreTestRunner.runValidation(treeElement, NO_REQUIRED_CHILDREN_RULESET);
 
+    await widget.doUpdate();
     const toolbarElement =
         treeElement.parentElement?.querySelector('devtools-toolbar');
     TestRunner.addResult('Running the axe-core linter on toolbar.');
