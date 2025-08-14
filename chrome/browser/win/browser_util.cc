01381c2ffdf43433d375026d0190f748449996a2
Greg Thompson <grt@chromium.org>
2025-08-14 02:11:35 -0700

    [Windows] Fix logic in IsBrowserAlreadyRunning
    This function uses a named global event object to detect if the same
    executable is running as more than one browser process. The intent is
    that it considers there to be another browser if the event already
    exists or if the event cannot be created. Since the contract with a
    thread's last error code on Windows is generally that functions do not
    modify the value on success, it's important to only consult this value
    for errors when functions report a failure. In this particular case, we
    were checking for the error condition even when the event was created.
    It's within the realm of possibility that `CreateEventW` would return
    success and not set the thread's last error code at all when the event
    did not previously exist, thereby leaving whatever the previous error
    was in place. If a previous operation on the thread had failed with
    `ERROR_ACCESS_DENIED`, we would have interpreted this as "can't create
    the event because another process has it" when, in fact, we should
    consider it "created the event anew".
    
    Bug: 427475108
    Change-Id: I29eec5b19ef8f1804c821bdbe72eba153750cced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6828788
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501267}

--- chrome/browser/win/browser_util.cc
+++ chrome/browser/win/browser_util.cc
@@ -16,6 +16,7 @@
 #include <memory>
 #include <optional>
 #include <string>
+#include <utility>
 
 #include "base/base_paths.h"
 #include "base/check.h"
@@ -82,7 +83,7 @@ bool IsBrowserAlreadyRunning() {
   std::ranges::transform(*nt_dir_name, nt_dir_name->begin(), tolower);
   nt_dir_name = L"Global\\" + nt_dir_name.value();
   if (handle != NULL)
-    ::CloseHandle(handle);
+    ::CloseHandle(std::exchange(handle, nullptr));
 
   // For this to work for both user and system installs, we need the event to be
   // accessible to all interactive users so that we can correctly detect any
@@ -108,9 +109,12 @@ bool IsBrowserAlreadyRunning() {
   }
   base::win::ScopedLocalAlloc scoped_sd(attributes.lpSecurityDescriptor);
 
-  handle = ::CreateEventW(&attributes, TRUE, TRUE, nt_dir_name->c_str());
+  handle = ::CreateEventW(&attributes, /*bManualReset=*/TRUE,
+                          /*bInitialState=*/TRUE, nt_dir_name->c_str());
   int error = ::GetLastError();
-  return (error == ERROR_ALREADY_EXISTS || error == ERROR_ACCESS_DENIED);
+  // There is another browser running if `CreateEventW` succeeded and the object
+  // existed prior to the call or if `CreateEventW` failed due to access denied.
+  return error == (handle ? ERROR_ALREADY_EXISTS : ERROR_ACCESS_DENIED);
 }
 
 }  // namespace browser_util
