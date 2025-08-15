2dd40d16d7f18e1467ee3ff9a128bb67958782e4
Noémie St-Onge <noemies@google.com>
2025-08-15 08:07:54 -0700

    [iOS] Disable virtual card enrollment EG test
    Test is now also failing consistently on ipad-device. Failure example:
    https://ci.chromium.org/ui/p/chrome/builders/ci/ipad-device/56107/overview
    
    Bug: 415396933
    Change-Id: If553a7dcd58abfac022c7b90cb8a663d3a001449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6849360
    Auto-Submit: Noémie St-Onge <noemies@google.com>
    Owners-Override: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1501921}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_egtest.mm
@@ -285,15 +285,8 @@ id<GREYMatcher> ActivityIndicatorMatcher() {
 }
 
 // TODO(crbug.com/415396933): Re-enable the test.
-#if TARGET_OS_SIMULATOR
-#define MAYBE_testVirtualCardEnrollmentShowsLoadingAndConfirmationAfterAcceptPushed \
-  DISABLED_testVirtualCardEnrollmentShowsLoadingAndConfirmationAfterAcceptPushed
-#else
-#define MAYBE_testVirtualCardEnrollmentShowsLoadingAndConfirmationAfterAcceptPushed \
-  testVirtualCardEnrollmentShowsLoadingAndConfirmationAfterAcceptPushed
-#endif
 - (void)
-    MAYBE_testVirtualCardEnrollmentShowsLoadingAndConfirmationAfterAcceptPushed {
+    DISABLED_testVirtualCardEnrollmentShowsLoadingAndConfirmationAfterAcceptPushed {
   // TODO(crbug.com/437268290): Re-enable the test on iOS26.
   if (base::ios::IsRunningOnIOS26OrLater()) {
     EARL_GREY_TEST_DISABLED(@"Test disabled on iOS 26.");
