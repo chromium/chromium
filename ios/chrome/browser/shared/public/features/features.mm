4946643c59c00709674f046e2d0574ea7213383b
Tommy Martino <tmartino@chromium.org>
2025-08-15 08:16:08 -0700

    Enable `kIOSKeyboardAccessoryUpgradeForIPad` by default.
    Change-Id: I0d9f423fd588b1066b8b7fe915be9748907b7d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6852527
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501926}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -21,7 +21,7 @@
 
 BASE_FEATURE(kIOSKeyboardAccessoryUpgradeForIPad,
              "IOSKeyboardAccessoryUpgradeForIPad",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTestFeature, "TestFeature", base::FEATURE_DISABLED_BY_DEFAULT);
 
