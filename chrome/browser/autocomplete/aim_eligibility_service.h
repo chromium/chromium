f5bdc191b5b7ff2cb542aee903dbfe599a6f8a28
manukh <manukh@chromium.org>
2025-08-15 08:30:20 -0700

    [M140] [omnibox][ai-mode] Add observer interface for eligibility service
    Original change's description:
    > [omnibox][ai-mode] Add observer interface for eligibility service
    > 
    > Add `AimEligibilityServiceObserver` that users of the eligiblity service
    > that need to be notified when a feature's eligibility may have changed
    > can use.
    > 
    > At the moment,
    > `AimEligibilityServiceObserver::OnAimEligibilityServiceChanged()` is
    > never called since eligibility is static. But once we begin using a
    > cached server response, the service will send out notifications whenever
    > a new response is returned so feature code can see if they should turn
    > themselves off/on.
    > 
    > Bug: 436225340
    > Change-Id: I4c9d987bce0c8ec7999578f3ad7530c86c7632b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6821387
    > Commit-Queue: manuk hovanesian <manukh@chromium.org>
    > Auto-Submit: manuk hovanesian <manukh@chromium.org>
    > Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1497791}
    
    Bug: 438942494,436225340
    Change-Id: I4c9d987bce0c8ec7999578f3ad7530c86c7632b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6853820
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7339@{#633}
    Cr-Branched-From: 27be8b77710f4405fdfeb4ee946fcabb0f6c92b2-refs/heads/main@{#1496484}

--- chrome/browser/autocomplete/aim_eligibility_service.h
+++ chrome/browser/autocomplete/aim_eligibility_service.h
@@ -8,10 +8,12 @@
 #include <string>
 
 #include "base/memory/raw_ptr.h"
+#include "base/observer_list.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 class PrefService;
 class TemplateURLService;
+class AimEligibilityServiceObserver;
 
 // Utility service to check if the user is eligible for certain features based
 // on e.g. locale. Does not check feature states.
@@ -23,6 +25,11 @@ class AimEligibilityService : public KeyedService {
   AimEligibilityService(const AimEligibilityService&) = delete;
   AimEligibilityService& operator=(const AimEligibilityService&) = delete;
 
+  // Register observers to notify when eligibility may have changed. Eligibility
+  // is re-checked periodically.
+  void AddObserver(AimEligibilityServiceObserver* observer);
+  void RemoveObserver(AimEligibilityServiceObserver* observer);
+
   // Verifies country and locale.
   static bool IsCountryAndLocale(const std::string& country,
                                  const std::string& locale);
@@ -32,7 +39,12 @@ class AimEligibilityService : public KeyedService {
   bool IsAimEligible() const;
 
  private:
+  // Notify `observers_` ineligibles may have changed.
+  void NotifyObservers() const;
+
+  base::ObserverList<AimEligibilityServiceObserver> observers_;
   const raw_ptr<PrefService> pref_service_;
+  // Guaranteed not-null because this server `DependsOn` `TemplateURLService`.
   const raw_ptr<TemplateURLService> template_url_service_;
 };
 
