e86a07530d87f81b78d77ef795ed5dcfbe20e3c9
Noémie St-Onge <noemies@google.com>
2025-08-15 13:37:43 -0700

    [iOS] Re-disable testSharingPromptsToUpdate
    This test is still failing on iphone-device.
    
    https://ci.chromium.org/ui/p/chrome/builders/ci/iphone-device/55630/overview
    
    Bug: 429118547
    Change-Id: I1a80258f9671aa8c276c6369dee12db548538855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6849801
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Owners-Override: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1502104}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_versioning_egtest.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_groups_versioning_egtest.mm
@@ -67,7 +67,13 @@ id<GREYMatcher> NotNowButton() {
 
 // Tests that trying to share a group fails with an alert offering to update the
 // app.
-- (void)testSharingPromptsToUpdate {
+// TODO(crbug.com/429118547): Test fails on iPhone device.
+#if !TARGET_IPHONE_SIMULATOR
+#define MAYBE_testSharingPromptsToUpdate DISABLED_testSharingPromptsToUpdate
+#else
+#define MAYBE_testSharingPromptsToUpdate testSharingPromptsToUpdate
+#endif
+- (void)MAYBE_testSharingPromptsToUpdate {
   // Open the tab grid.
   [ChromeEarlGreyUI openTabGrid];
 
