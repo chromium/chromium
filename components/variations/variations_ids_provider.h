c0cabbb7ab07f4a70e5f795e81130c01469e9805
Caitlin Fischer <caitlinfischer@google.com>
2025-08-15 09:31:21 -0700

    Clean up some remnants of cros_early_boot.
    The bulk of this was cleaned up in
    https://chromium-review.googlesource.com/6806259.
    
    Bug: 434969200
    Change-Id: Ic5001bd584c9ff843e893b86699b558facfed65a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6846360
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1501955}

--- components/variations/variations_ids_provider.h
+++ components/variations/variations_ids_provider.h
@@ -24,11 +24,6 @@
 namespace variations {
 class VariationsClient;
 
-namespace cros_early_boot::evaluate_seed {
-// For friend purposes (for DestroyInstanceForTesting().)
-class VariationsCrosEvaluateSeedMainTest;
-}  // namespace cros_early_boot::evaluate_seed
-
 // The key for a VariationsIdsProvider's |variations_headers_map_|. A
 // VariationsHeaderKey provides more details about the VariationsIDs included in
 // a particular header. For example, the header associated with a key with true
@@ -165,9 +160,6 @@ class COMPONENT_EXPORT(VARIATIONS) VariationsIdsProvider
   typedef std::pair<VariationID, IDCollectionKey> VariationIDEntry;
 
   friend class ScopedVariationsIdsProvider;
-  // For DestroyInstanceForTesting
-  friend class cros_early_boot::evaluate_seed::
-      VariationsCrosEvaluateSeedMainTest;
 
   FRIEND_TEST_ALL_PREFIXES(VariationsIdsProviderTest, ForceVariationIds_Valid);
   FRIEND_TEST_ALL_PREFIXES(VariationsIdsProviderTest,
