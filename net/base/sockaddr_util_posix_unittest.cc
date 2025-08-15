aba997dc44b8e4cb8829f9d4b77e9d66e5b22c0a
Matt Menke <mmenke@chromium.org>
2025-08-15 12:24:38 -0700

    Spanify net::FillUnixAddress().
    This is a little awkward, because it's dealing with offsets into a
    sockaddr_storage that it also casts as a sockaddr_un, but
    base::as_writeable_chars() makes this not too bad.
    
    Fixed: 416481248
    Change-Id: I0de361648c69cf5773ca3bc63a855a4044637bef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6495315
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1502065}

--- net/base/sockaddr_util_posix_unittest.cc
+++ net/base/sockaddr_util_posix_unittest.cc
@@ -2,18 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "net/base/sockaddr_util_posix.h"
 
 #include <string.h>
-
 #include <sys/socket.h>
 #include <sys/un.h>
 
+#include "base/containers/span.h"
+#include "base/strings/string_view_util.h"
 #include "net/base/sockaddr_storage.h"
 #include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -22,13 +18,9 @@ namespace net {
 
 namespace {
 
-size_t MaxPathLength(SockaddrStorage* storage) {
-  // |storage.addr_len| is initialized to the largest possible platform-
-  // dependent value. Subtracting the size of the initial fields in
-  // sockaddr_un gives us the longest permissible path value including space
-  // for an extra NUL character at the front or back.
-  return storage->addr_len - offsetof(struct sockaddr_un, sun_path) - 1;
-}
+// The largest possible platform-dependent value. Subtracting one for an extra
+// NUL character at the front or back.
+constexpr size_t kMaxUnixAddressPath = sizeof(sockaddr_un::sun_path) - 1;
 
 }  // namespace
 
@@ -45,7 +37,7 @@ TEST(FillUnixAddressTest, SimpleAddress) {
             (unsigned int)storage.addr_len);
 
   struct sockaddr_un* socket_addr =
-      reinterpret_cast<struct sockaddr_un*>(storage.addr());
+      reinterpret_cast<struct sockaddr_un*>(&storage.addr_storage);
   EXPECT_EQ(socket_addr->sun_family, AF_UNIX);
 
   // Implicit conversion to std::string for comparison is fine since the path
@@ -62,22 +54,20 @@ TEST(FillUnixAddressTest, PathEmpty) {
 
 TEST(FillUnixAddressTest, AddressMaxLength) {
   SockaddrStorage storage;
-  size_t path_max = MaxPathLength(&storage);
-  std::string path(path_max, '0');
+  std::string path(kMaxUnixAddressPath, '0');
 
   EXPECT_TRUE(
       FillUnixAddress(path, /*use_abstract_namespace=*/false, &storage));
 
   struct sockaddr_un* socket_addr =
-      reinterpret_cast<struct sockaddr_un*>(storage.addr());
+      reinterpret_cast<struct sockaddr_un*>(&storage.addr_storage);
   EXPECT_EQ(socket_addr->sun_family, AF_UNIX);
   EXPECT_EQ(socket_addr->sun_path, path);
 }
 
 TEST(FillUnixAddressTest, AddressTooLong) {
   SockaddrStorage storage;
-  size_t path_max = MaxPathLength(&storage);
-  std::string path(path_max + 1, '0');
+  std::string path(kMaxUnixAddressPath + 1, '0');
 
   EXPECT_FALSE(
       FillUnixAddress(path, /*use_abstract_namespace=*/false, &storage));
@@ -85,27 +75,31 @@ TEST(FillUnixAddressTest, AddressTooLong) {
 
 TEST(FillUnixAddressTest, AbstractLinuxAddress) {
   SockaddrStorage storage;
-  size_t path_max = MaxPathLength(&storage);
-  std::string path(path_max, '0');
+  std::string path(kMaxUnixAddressPath, '0');
 
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
   EXPECT_TRUE(FillUnixAddress(path, /*use_abstract_namespace=*/true, &storage));
 
   EXPECT_EQ(path.size() + 1U + offsetof(struct sockaddr_un, sun_path),
-            (unsigned int)storage.addr_len);
+            static_cast<size_t>(storage.addr_len));
 
   struct sockaddr_un* socket_addr =
-      reinterpret_cast<struct sockaddr_un*>(storage.addr());
+      reinterpret_cast<struct sockaddr_un*>(&storage.addr_storage);
   EXPECT_EQ(socket_addr->sun_family, AF_UNIX);
 
   // The path buffer is preceded by a NUL character for abstract Linux
   // addresses.
   EXPECT_EQ(socket_addr->sun_path[0], '\0');
 
-  // The path string may not be NUL terminated, so do a buffer copy when
-  // converting to std::string.
-  std::string unix_path(reinterpret_cast<char*>(socket_addr->sun_path + 1),
-                        path.size());
+  // The rest of the path. Note that `addr_len` has already been verified to be
+  // correct, so only need to check the remaining path.size() characters of the
+  // address struct, after the nul. Create a span and then a string_view from it
+  // because span's constructor creates a span from the length of the array
+  // that's passed in, while string_view's constructor will treat the input as
+  // C-string and look for the terminating null at the end, and abstract Linux
+  // paths are not guaranteed to be null terminated when in a `sockaddr_un`.
+  std::string_view unix_path = base::as_string_view(
+      base::span(socket_addr->sun_path).subspan(1u, path.size()));
   EXPECT_EQ(unix_path, path);
 #else
   // Other platforms don't support the abstract Linux namespace.
