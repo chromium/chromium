e3dcb845c2927a239edf392d658ec1e44e0b9ff5
Moe Ahmadi <mahmadi@chromium.org>
2025-08-18 12:39:56 -0700

    [M140] [omnibox] Moves AimEligibilityService to //components
    Original change's description:
    > [omnibox] Moves AimEligibilityService to //components
    > 
    > Bug: 436900485
    > Change-Id: Ifecc66a307e2f9a8367ceddac89275160c422f2c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6849947
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    > Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1501435}
    
    Bug: 439279712,436900485
    Change-Id: Ifecc66a307e2f9a8367ceddac89275160c422f2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6857505
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7339@{#846}
    Cr-Branched-From: 27be8b77710f4405fdfeb4ee946fcabb0f6c92b2-refs/heads/main@{#1496484}

--- components/omnibox/browser/aim_eligibility_service.cc
+++ components/omnibox/browser/aim_eligibility_service.cc
@@ -2,96 +2,72 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/autocomplete/aim_eligibility_service.h"
+#include "components/omnibox/browser/aim_eligibility_service.h"
 
 #include <memory>
 #include <string>
 
 #include "base/base64.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
-#include "chrome/browser/autocomplete/aim_eligibility_service_observer.h"
-#include "chrome/browser/browser_process.h"
-#include "chrome/browser/global_features.h"
-#include "chrome/browser/profiles/profile.h"
-#include "components/application_locale_storage/application_locale_storage.h"
+#include "components/omnibox/browser/aim_eligibility_service_observer.h"
 #include "components/omnibox/browser/omnibox_prefs.h"
 #include "components/omnibox/common/omnibox_feature_configs.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/search/search.h"
 #include "components/search_engines/template_url_service.h"
-#include "components/variations/service/variations_service.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "services/network/public/cpp/resource_request.h"
-#include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "third_party/omnibox_proto/aim_eligibility_response.pb.h"
 #include "url/gurl.h"
 
+namespace {
+
+// If disabled, AIM is completely turned off (kill switch).
 BASE_FEATURE(kAimEnabled, "AimEnabled", base::FEATURE_ENABLED_BY_DEFAULT);
 
+// If enabled, uses the server response for AIM eligibility for all locales.
 BASE_FEATURE(kAimServerEligibilityEnabled,
              "AimServerEligibilityEnabled",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+// If enabled, uses the server response for AIM eligibility for English locales.
+// Has no effect if kAimServerEligibilityEnabled is enabled.
 BASE_FEATURE(kAimServerEligibilityEnabledEn,
              "AimServerEligibilityEnabledEn",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-namespace {
-
-// Returns the country code from the variations service.
-std::string GetCountryCode() {
-  std::string country_code;
-  // The variations service may be nullptr in unit tests.
-  variations::VariationsService* variations_service =
-      g_browser_process ? g_browser_process->variations_service() : nullptr;
-  if (variations_service) {
-    country_code = variations_service->GetStoredPermanentCountry();
-    if (country_code.empty()) {
-      country_code = variations_service->GetLatestCountry();
-    }
-  }
-  return country_code;
-}
-
-// Returns the locale from the browser process.
-std::string GetLocale() {
-  return g_browser_process ? g_browser_process->GetFeatures()
-                                 ->application_locale_storage()
-                                 ->Get()
-                           : "";
-}
-
 // For recording UMA metrics. These aren't strictly omnibox-only, but omnibox is
 // a major consumer of `AimEligibilityService`, and the few metrics here don't
 // warrant creating a new metric namespace.
-// The status of the GWS request. See `GwsRequestStatus`.
-static constexpr char kUmaGwsRequestStatusHistogramName[] =
-    "Omnibox.AimEligibility.GwsRequestStatus";
-// Which AIM features were eligible according to the GWS request.
-static constexpr char kUmaGwsEligibilityHistogramPrefix[] =
-    "Omnibox.AimEligibility.GwsEligibility.";
+// The status of the server request. See `ServerRequestStatus`.
+static constexpr char kUmaServerRequestStatusHistogramName[] =
+    "Omnibox.AimEligibility.ServerRequestStatus";
+// Which AIM features were eligible according to the server request.
+static constexpr char kUmaServerEligibilityHistogramPrefix[] =
+    "Omnibox.AimEligibility.ServerEligibility.";
 // These values are persisted to logs. Entries should not be renumbered and
 // numeric values should never be reused.
-// LINT.IfChange(GwsAimEligibilityRequestStatus)
-enum class GwsRequestStatus {
+// LINT.IfChange(ServerAimEligibilityRequestStatus)
+enum class ServerRequestStatus {
   kSent = 0,
   kErrorResponse = 1,
   kFailedToParse = 2,
   kSuccess = 3,
   kMaxValue = kSuccess,
 };
-// LINT.ThenChange(//tools/metrics/histograms/metadata/omnibox/enums.xml:GwsAimEligibilityRequestStatus)
+// LINT.ThenChange(//tools/metrics/histograms/metadata/omnibox/enums.xml:ServerAimEligibilityRequestStatus)
 
-static constexpr char kGwsRequestEndpoint[] =
+static constexpr char kRequestEndpoint[] =
     "http://www.google.com/async/folae?async=_fmt:pb";
 
-const net::NetworkTrafficAnnotationTag kGwsRequestTrafficAnnotation =
+const net::NetworkTrafficAnnotationTag kRequestTrafficAnnotation =
     net::DefineNetworkTrafficAnnotation("aim_eligibility_fetch", R"(
       semantics {
         sender: "Chrome AI Mode Eligibility Service"
@@ -136,26 +112,25 @@ AimEligibilityService::AimEligibilityService(
     : pref_service_(pref_service),
       template_url_service_(template_url_service),
       url_loader_factory_(url_loader_factory) {
-  // TODO(crbug.com/436898763): Call `StartGwsRequest()` to refresh the server
-  //   response when service is constructed and when user state changes. E.g.
-  //   user signs in/out, starts/stops syncing, switches profiles. Some of those
-  //   actions may create a new service; if so, we don't need to listen to those
-  //   events and start StartGwsRequest manually, because it'll be called in the
-  //   constructor anyways. Switching profiles probably creates a new service.
+  // TODO(crbug.com/436898763): Call `StartServerEligibilityRequest()` to
+  // refresh the server response when service is constructed and when user state
+  // changes. E.g. user signs in/out, starts/stops syncing, switches profiles.
+  // Some of those actions may create a new service; if so, we don't need to
+  // listen to those events and start StartServerEligibilityRequest manually,
+  // because it'll be called in the constructor anyways. Switching profiles
+  // probably creates a new service.
   ReadFromPref();
 }
 
 AimEligibilityService::~AimEligibilityService() = default;
 
-// static
-bool AimEligibilityService::IsCountry(const std::string& country) {
+bool AimEligibilityService::IsCountry(const std::string& country) const {
   // Country codes are in lowercase ISO 3166-1 alpha-2 format; e.g., us, br, in.
   // See components/variations/service/variations_service.h
   return GetCountryCode() == country;
 }
 
-// static
-bool AimEligibilityService::IsLanguage(const std::string& language) {
+bool AimEligibilityService::IsLanguage(const std::string& language) const {
   // Locale follows BCP 47 format; e.g., en-US, fr-FR, ja-JP.
   // See ui/base/l10n/l10n_util.h
   return base::StartsWith(GetLocale(), language, base::CompareCase::SENSITIVE);
@@ -207,8 +182,9 @@ bool AimEligibilityService::IsAimEligible() const {
 }
 
 void AimEligibilityService::NotifyObservers() const {
-  for (auto& observer : observers_)
-    observer.OnAimEligibilityServiceChanged();
+  for (auto& observer : observers_) {
+    observer.OnAimEligibilityChanged();
+  }
 }
 
 bool AimEligibilityService::ParseResponseString(
@@ -216,8 +192,9 @@ bool AimEligibilityService::ParseResponseString(
   // Parse into a temporary variable 1st so that if parsing fails,
   // `most_recent_response_` isn't cleared.
   omnibox::AimEligibilityResponse response_proto;
-  if (!response_proto.ParseFromString(response_string))
+  if (!response_proto.ParseFromString(response_string)) {
     return false;
+  }
   most_recent_response_ = response_proto;
   return true;
 }
@@ -231,11 +208,12 @@ void AimEligibilityService::WriteToPref(
 void AimEligibilityService::ReadFromPref() {
   const std::string& read_string = pref_service_->GetString(kResponsePrefName);
   std::string decoded;
-  if (base::Base64Decode(read_string, &decoded))
+  if (base::Base64Decode(read_string, &decoded)) {
     ParseResponseString(decoded);
+  }
 }
 
-void AimEligibilityService::StartGwsRequest() {
+void AimEligibilityService::StartServerEligibilityRequest() {
   // Don't make server requests if AIM or server requests are disabled.
   if (!base::FeatureList::IsEnabled(kAimEnabled) ||
       !IsServerEligibilityEnabled()) {
@@ -244,41 +222,41 @@ void AimEligibilityService::StartGwsRequest() {
 
   std::unique_ptr<network::ResourceRequest> request =
       std::make_unique<network::ResourceRequest>();
-  request->url = GURL{kGwsRequestEndpoint};
+  request->url = GURL{kRequestEndpoint};
   request->credentials_mode = network::mojom::CredentialsMode::kOmit;
   std::unique_ptr<network::SimpleURLLoader> loader =
       network::SimpleURLLoader::Create(std::move(request),
-                                       kGwsRequestTrafficAnnotation);
-  base::UmaHistogramEnumeration(kUmaGwsRequestStatusHistogramName,
-                                GwsRequestStatus::kSent);
+                                       kRequestTrafficAnnotation);
+  base::UmaHistogramEnumeration(kUmaServerRequestStatusHistogramName,
+                                ServerRequestStatus::kSent);
   loader->DownloadToStringOfUnboundedSizeUntilCrashAndDie(
       url_loader_factory_.get(),
-      base::BindOnce(&AimEligibilityService::OnGwsResponse,
+      base::BindOnce(&AimEligibilityService::OnServerEligibilityResponse,
                      weak_factory_.GetWeakPtr(), std::move(loader)));
 }
 
-void AimEligibilityService::OnGwsResponse(
+void AimEligibilityService::OnServerEligibilityResponse(
     std::unique_ptr<network::SimpleURLLoader> loader,
     std::unique_ptr<std::string> response_string) {
   // TODO(crbug.com/436900259): Add UMA metrics for whether the response
   //   returned 200, was parsed successfully, and which features were eligible.
   //   This will let us know how watered down UMA and finch are compared due to
-  //   mismatched GWS eligibility criteria and estimate the actual population
+  //   mismatched server eligibility criteria and estimate the actual population
   //   size.
   if (!response_string) {
-    base::UmaHistogramEnumeration(kUmaGwsRequestStatusHistogramName,
-                                  GwsRequestStatus::kErrorResponse);
+    base::UmaHistogramEnumeration(kUmaServerRequestStatusHistogramName,
+                                  ServerRequestStatus::kErrorResponse);
     return;
   }
   if (!ParseResponseString(*response_string)) {
-    base::UmaHistogramEnumeration(kUmaGwsRequestStatusHistogramName,
-                                  GwsRequestStatus::kFailedToParse);
+    base::UmaHistogramEnumeration(kUmaServerRequestStatusHistogramName,
+                                  ServerRequestStatus::kFailedToParse);
     return;
   }
-  base::UmaHistogramEnumeration(kUmaGwsRequestStatusHistogramName,
-                                GwsRequestStatus::kSuccess);
+  base::UmaHistogramEnumeration(kUmaServerRequestStatusHistogramName,
+                                ServerRequestStatus::kSuccess);
   base::UmaHistogramBoolean(
-      base::StrCat({kUmaGwsEligibilityHistogramPrefix, "is_eligible"}),
+      base::StrCat({kUmaServerEligibilityHistogramPrefix, "is_eligible"}),
       most_recent_response_.is_eligible());
   WriteToPref(*response_string);
   NotifyObservers();
