11411f892aaec720fb7ce0b450e3afd6f7cd4da4
Matt Menke <mmenke@chromium.org>
2025-08-18 17:09:37 -0700

    Try to re-enable two URLRequestTests that are marked as flaky on iOS.
    URLRequestTestHTTP.OverrideUserAgent and
    URLRequestTest.DoNotSaveCookies_ViaPolicy were marked as flaky 10 years
    ago. Both are fairly simple tests and shouldn't depend on the underlying
    platform, so it's not clear why they were failing.
    
    This CL leaves the HSTS tests in that file disabled on iOS, though
    may be worth experimenting there as well.
    
    Bug: 41225288
    Change-Id: Ib6dc474c58a96200840de909df6080ff3f3e297a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6859178
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503058}

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -2195,13 +2195,7 @@ TEST_F(URLRequestTest, DoNotSendCookies_ViaPolicy) {
   }
 }
 
-// TODO(crbug.com/41225288) This test is flaky on iOS.
-#if BUILDFLAG(IS_IOS)
-#define MAYBE_DoNotSaveCookies_ViaPolicy FLAKY_DoNotSaveCookies_ViaPolicy
-#else
-#define MAYBE_DoNotSaveCookies_ViaPolicy DoNotSaveCookies_ViaPolicy
-#endif
-TEST_F(URLRequestTest, MAYBE_DoNotSaveCookies_ViaPolicy) {
+TEST_F(URLRequestTest, DoNotSaveCookies_ViaPolicy) {
   HttpTestServer test_server;
   ASSERT_TRUE(test_server.Start());
 
@@ -8693,15 +8687,7 @@ TEST_F(URLRequestTestHTTP, DefaultUserAgent) {
             d.data_received());
 }
 
-// Check that if request overrides the User-Agent header,
-// the default is not appended.
-// TODO(crbug.com/41225288) This test is flaky on iOS.
-#if BUILDFLAG(IS_IOS)
-#define MAYBE_OverrideUserAgent FLAKY_OverrideUserAgent
-#else
-#define MAYBE_OverrideUserAgent OverrideUserAgent
-#endif
-TEST_F(URLRequestTestHTTP, MAYBE_OverrideUserAgent) {
+TEST_F(URLRequestTestHTTP, OverrideUserAgent) {
   ASSERT_TRUE(http_test_server()->Start());
 
   TestDelegate d;
