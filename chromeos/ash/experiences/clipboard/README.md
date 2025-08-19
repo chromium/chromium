b2ffa2d85968d7745b6aa360fdb4a631bd5f7860
Jun Ishiguro <junis@google.com>
2025-08-18 21:58:57 -0700

    ash-refactor: Relocate clipboard_util to //chromeos/ash/experiences
    This CL moves clipboard_util from //chrome/browser/ui/ash/clipboard to a
    new, more appropriate home in //chromeos/ash/experiences/clipboard.
    
    This refactoring is part of the larger effort to separate Ash-specific
    code from //chrome/browser.
    
    Bug: b:426116843
    Change-Id: I9371a08b04f745c35954cf0ffd97e99bc21133d6
    Bypass-Check-License: Moving files
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6850935
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503142}

---
+++ chromeos/ash/experiences/clipboard/README.md
@@ -0,0 +1,4 @@
+# //chromeos/ash/experiences/clipboard
+
+This directory contains supplemental classes that require dependency on
+//content. Main implementation should reside in //ash/clipboard.
--- chromeos/ash/experiences/clipboard/clipboard_util.h
+++ chromeos/ash/experiences/clipboard/clipboard_util.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_CLIPBOARD_CLIPBOARD_UTIL_H_
-#define CHROME_BROWSER_UI_ASH_CLIPBOARD_CLIPBOARD_UTIL_H_
+#ifndef CHROMEOS_ASH_EXPERIENCES_CLIPBOARD_CLIPBOARD_UTIL_H_
+#define CHROMEOS_ASH_EXPERIENCES_CLIPBOARD_CLIPBOARD_UTIL_H_
 
 #include "base/functional/callback_forward.h"
 
@@ -30,4 +30,4 @@ void ReadFileAndCopyToClipboard(const base::FilePath& local_file,
 
 }  // namespace clipboard_util
 
-#endif  // CHROME_BROWSER_UI_ASH_CLIPBOARD_CLIPBOARD_UTIL_H_
+#endif  // CHROMEOS_ASH_EXPERIENCES_CLIPBOARD_CLIPBOARD_UTIL_H_
