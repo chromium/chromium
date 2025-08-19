2c91f5986f634afad68e6a917e14e386f5e85228
Federica Germinario <fedegermi@google.com>
2025-08-19 10:52:03 -0700

    [ios] Disable test TestOpenInSearchMode
    The test is failing when skipped only on ios26 (failure happens at teardown).
    Always disable the test until the issue gets fixed.
    
    Bug: 437314312
    Change-Id: Ic23a8a2c1e4885fac7cccbd4c60f721ccc46e8e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6859005
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1503460}

--- ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_manager_view_controller_unittest.mm
@@ -637,11 +637,8 @@ TEST_F(PasswordManagerViewControllerTest,
 
 // Tests that opening the PasswordManagerViewController in search mode shows the
 // expected content.
-TEST_F(PasswordManagerViewControllerTest, TestOpenInSearchMode) {
-  // TODO(crbug.com/437314312): Re-enable the test on iOS26.
-  if (base::ios::IsRunningOnIOS26OrLater()) {
-    return;
-  }
+// TODO(crbug.com/437314312): Re-enable the test.
+TEST_F(PasswordManagerViewControllerTest, DISABLED_TestOpenInSearchMode) {
   // Call `settingsWillBeDismissed` on the initial view controller so that its
   // observers are reset.
   [GetPasswordManagerViewController() settingsWillBeDismissed];
