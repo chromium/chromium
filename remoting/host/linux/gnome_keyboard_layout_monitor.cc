c32fccd7b762bb0d64edc4940fef0b78d82031ba
Jamie Walch <jamiewalch@chromium.org>
2025-08-20 16:58:37 -0700

    Fix shift level handling.
    The `KeyboardLayout` proto defines per-level actions for each key, but
    the old code was interpreting the level incorrectly. Specifically, it
    expects a zero-based analog of ISO shift levels, which corresponds to
    bit-0 reflecting the Shift state, and bit-1 reflecting the AltGr state
    (if the keyboard has one). By contrast, the
    `xkb_keymap_key_get_syms_by_level` refers to the number of levels that
    the individual key has defined, which may be less. For example, the
    Space key always produces the same symbol and so it only has one level
    defined--XKB will return that it produces nothing at level 2.
    
    The solution is to instead use an `xkb_state` object to simulate
    specific modifiers being pressed, and then query what the key does in
    that context. This is broadly similar to how it's implemented in
    keyboard_layout_monitor_linux.cc.
    
    Change-Id: I9d519b54c58105b3ac43286c528ab097578176e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6866645
    Auto-Submit: Jamie Walch <jamiewalch@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504279}

--- remoting/host/linux/gnome_keyboard_layout_monitor.cc
+++ remoting/host/linux/gnome_keyboard_layout_monitor.cc
@@ -8,6 +8,7 @@
 #include "base/strings/utf_string_conversion_utils.h"
 #include "remoting/host/linux/keyboard_layout_monitor_utils.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
+#include "ui/events/keycodes/scoped_xkb.h"
 
 namespace remoting {
 
@@ -45,18 +46,37 @@ bool SetActionFromDeadKey(xkb_keysym_t keysym,
   return true;
 }
 
+struct Context {
+  Context(protocol::KeyboardLayout& proto, struct xkb_keymap* keymap)
+      : proto(proto), xkb_state(xkb_state_new(keymap)) {
+    auto numlock_index = xkb_keymap_mod_get_index(keymap, "NumLock");
+    auto numlock_mask = (numlock_index == XKB_MOD_INVALID) ? 0 : 1 << numlock_index;
+    auto shift_index = xkb_keymap_mod_get_index(keymap, "Shift");
+    auto shift_mask = (shift_index == XKB_MOD_INVALID) ? 0 : 1 << shift_index;
+    auto altgr_index = xkb_keymap_mod_get_index(keymap, "Mod5");
+    auto altgr_mask = (altgr_index == XKB_MOD_INVALID) ? 0 : 1 << altgr_index;
+    shift_level_to_mask[0] = numlock_mask;
+    shift_level_to_mask[1] = shift_mask | numlock_mask;
+    if (altgr_mask) {
+      shift_level_to_mask[2] = altgr_mask | numlock_mask;
+      shift_level_to_mask[3] = shift_mask | altgr_mask | numlock_mask;
+    }
+  }
+
+  raw_ref<protocol::KeyboardLayout> proto;
+  std::map<int, int> shift_level_to_mask;
+  std::unique_ptr<xkb_state, ui::XkbStateDeleter> xkb_state;
+};
+
 void ProcessKeyCode(struct xkb_keymap* keymap,
                     xkb_keycode_t keycode,
                     void* data) {
-  auto* proto = static_cast<protocol::KeyboardLayout*>(data);
-  int layout = 0;
-  xkb_level_index_t num_levels =
-      xkb_keymap_num_levels_for_key(keymap, keycode, layout);
-  for (xkb_level_index_t level = 0; level < num_levels; level++) {
-    const xkb_keysym_t* syms;
-    auto num_syms =
-        xkb_keymap_key_get_syms_by_level(keymap, keycode, layout, level, &syms);
-    if (num_syms != 1) {
+  auto* ctx = static_cast<Context*>(data);
+  for (auto [level, mask] : ctx->shift_level_to_mask) {
+    xkb_state_update_mask(ctx->xkb_state.get(), mask, 0, 0, 0, 0, 0);
+    auto keysym =
+        xkb_state_key_get_one_sym(ctx->xkb_state.get(), keycode);
+    if (keysym == XKB_KEY_NoSymbol) {
       // Keys with no symbols are fairly common, and should be ignored. In
       // theory, keys with multiple symbols might exist, and could perhaps
       // be supported (for example, if all symbols correspond to characters
@@ -64,21 +84,21 @@ void ProcessKeyCode(struct xkb_keymap* keymap,
       // so they are ignored for now.
       continue;
     }
-    if (syms[0] == XKB_KEY_Num_Lock || syms[0] == XKB_KEY_Caps_Lock) {
+    if (keysym == XKB_KEY_Num_Lock || keysym == XKB_KEY_Caps_Lock) {
       // Don't include Num Lock or Caps Lock because the client keyboard
       // doesn't support them.
       continue;
     }
     auto usb_keycode = ui::KeycodeConverter::NativeKeycodeToUsbKeycode(keycode);
-    auto& actions = *(*proto->mutable_keys())[usb_keycode].mutable_actions();
+    auto& actions = *(*ctx->proto->mutable_keys())[usb_keycode].mutable_actions();
     bool had_existing_actions = !actions.empty();
-    if (!SetActionFromLayoutFunction(syms[0], actions[level]) &&
-        !SetActionFromUtfCodepoint(syms[0], actions[level]) &&
-        !SetActionFromDeadKey(syms[0], actions[level]) &&
+    if (!SetActionFromLayoutFunction(keysym, actions[level]) &&
+        !SetActionFromUtfCodepoint(keysym, actions[level]) &&
+        !SetActionFromDeadKey(keysym, actions[level]) &&
         !had_existing_actions) {
       // Delete the entry that was implicitly added by the []-operator if
       // there are no actions.
-      proto->mutable_keys()->erase(usb_keycode);
+      ctx->proto->mutable_keys()->erase(usb_keycode);
     }
   }
 }
@@ -116,7 +136,8 @@ void GnomeKeyboardLayoutMonitor::OnKeymapChanged(xkb_keymap* keymap) {
                    << " for the client keyboard. Re-order the layouts to"
                    << " change this.";
     }
-    xkb_keymap_key_for_each(keymap, &ProcessKeyCode, &layout_proto_);
+    Context ctx(layout_proto_, keymap);
+    xkb_keymap_key_for_each(keymap, &ProcessKeyCode, &ctx);
     if (started_) {
       callback_.Run(layout_proto_);
     }
