0dee8fcbbc85c75d72a5260da535456f2561ff41
Austin Zhang <austinzzr@google.com>
2025-08-20 10:04:00 -0700

    Enable the Management Promotion Banner by default.
    This changes the default state of the `kEnableManagementPromotionBanner` feature from disabled to enabled.
    
    Change-Id: I60fbf0e4bcde9f613f1439e3a0198819fab5f3ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6861845
    Commit-Queue: Owen Min <zmin@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Auto-Submit: Austin Zhang <austinzzr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504037}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -452,7 +452,7 @@ BASE_FEATURE(kPageSpecificDataDialogRelatedInstalledAppsSection,
 
 BASE_FEATURE(kEnableManagementPromotionBanner,
              "EnableManagementPromotionBanner",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_CHROMEOS)
 BASE_FEATURE(kEnablePolicyPromotionBanner,
