646d49f300faa275dda8ae21910a3c7104bd8937
Matt Stark <msta@google.com>
2025-08-19 21:25:07 -0700

    Allow updating BUILD.gn for all existing platforms at the same time.
    It was previously possible to manually update each platform, as there
    were only two platforms. With the advent of new platforms, this is no
    longer feasible.
    
    Bug: 435303825
    Change-Id: I6a6a6964b3d089523add04470f11e35414c0059e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6864970
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Matt Stark <msta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503752}

--- build/modules/modularize/modularize.py
+++ build/modules/modularize/modularize.py
@@ -6,11 +6,13 @@
 """Modularize modularizes a platform."""
 
 import argparse
+import concurrent.futures
 import logging
 import pathlib
 import shutil
+import subprocess
 import sys
-import tempfile
+import traceback
 
 from config import fix_graph
 from graph import IncludeDir
@@ -24,11 +26,63 @@ SOURCE_ROOT = pathlib.Path(__file__).parents[3].resolve()
 def main(args):
   logging.basicConfig(level=logging.getLevelNamesMapping()[args.verbosity])
 
+  if args.C is not None:
+    _modularize(
+        out_dir=args.C,
+        error_log=args.error_log,
+        use_cache=args.cache,
+        compile=args.compile,
+    )
+  elif args.all:
+    existing_platforms = [
+        build.parent.name
+        for build in (SOURCE_ROOT / 'build/modules').glob('*-*/BUILD.gn')
+    ]
+    futures = {}
+    # Use a ProcessPoolExecutor rather than a ThreadPoolExecutor because:
+    # * No shared state between instances
+    # * GIL prevents a performance benefit from a thread pool executor.
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+      for platform in existing_platforms:
+        os, cpu = platform.split('-')
+        out_dir = SOURCE_ROOT / 'out' / platform
+        args_gn = out_dir / 'args.gn'
+        if not args_gn.exists():
+          out_dir.mkdir(exist_ok=True, parents=True)
+          args_gn.write_text("".join(
+              [f'target_os = "{os}"\n'
+               f'target_cpu = "{cpu}"\n']))
+
+        error_log = None if args.error_log is None else args.error_log / platform
+        futures[platform] = executor.submit(
+            _modularize,
+            out_dir=out_dir,
+            error_log=error_log,
+            use_cache=args.cache,
+            compile=args.compile,
+        )
+
+    success = True
+    for platform, future in sorted(futures.items()):
+      exc = future.exception()
+      if exc is not None:
+        success = False
+        print(f'{platform} raised an exception:', file=sys.stderr)
+        traceback.print_exception(exc)
+    if not success:
+      exit(1)
+
+
+def _modularize(out_dir: pathlib.Path, error_log: pathlib.Path | None,
+                use_cache: bool, compile: bool):
+  # Modularize requires gn gen to have been run at least once.
+  if not (out_dir / 'build.ninja').is_file():
+    subprocess.run(['gn', 'gen', out_dir], check=True)
   compiler = Compiler(
       source_root=SOURCE_ROOT,
-      gn_out=args.C.resolve(),
-      error_dir=None if args.error_log is None else args.error_log.resolve(),
-      use_cache=args.cache,
+      gn_out=out_dir,
+      error_dir=error_log,
+      use_cache=use_cache,
   )
   platform = f'{compiler.os}-{compiler.cpu}'
   logging.info('Detected platform %s', platform)
@@ -40,8 +94,8 @@ def main(args):
     out_dir.mkdir(exist_ok=True)
     shutil.copyfile(out_dir.parent / 'linux-x64/BUILD.gn', out_build)
 
-  if args.compile:
-    ps, files = compiler.compile_one(args.compile)
+  if compile:
+    ps, files = compiler.compile_one(compile)
     print('stderr:', ps.stderr.decode('utf-8'), file=sys.stderr)
     print('Files used:')
     print('\n'.join(sorted(map(str, files))))
@@ -64,9 +118,20 @@ def main(args):
   )
 
 
+def _optional_path(s: str) -> pathlib.Path | None:
+  if s:
+    return pathlib.Path(s).resolve()
+
+
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
-  parser.add_argument('-C', type=pathlib.Path)
+  which = parser.add_mutually_exclusive_group(required=True)
+  which.add_argument('-C', type=_optional_path)
+  which.add_argument(
+      '--all',
+      action='store_true',
+      help='Update all existing platforms',
+  )
 
   # Make it required so the user understands how compilation works.
   cache = parser.add_mutually_exclusive_group(required=True)
@@ -88,8 +153,7 @@ if __name__ == '__main__':
       'Compile a single header file (eg. --compile=sys/types.h) instead of the whole sysroot. Useful for debugging.',
   )
 
-  parser.add_argument('--error-log',
-                      type=lambda value: pathlib.Path(value) if value else None)
+  parser.add_argument('--error-log', type=_optional_path)
 
   parser.add_argument(
       '--verbosity',
