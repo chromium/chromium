180a32b00531cbcda1047c5c693b394044a35473
Michael Tatarski <mtatarski@google.com>
2025-08-20 03:14:34 -0700

    [CodeHealth] Migrate `DiscardableMemoryImpl` to use `base::HeapArray`.
    This change replaces `std::unique_ptr<uint8_t[]>` with
    `base::HeapArray<uint8_t>` for managing the allocated memory in
    `DiscardableMemoryImpl`. `base::HeapArray` is a more appropriate
    container for heap-allocated arrays, and `Uninit()` is used as the
    memory does not need to be zero-initialized. It also removes the `size_`
    member variable as it is not needed anymore.
    
    Bug: 40285824
    Change-Id: Ibdf462f64b53163415388c461b1537c246bd096a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6859884
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1503837}

--- base/test/test_discardable_memory_allocator.cc
+++ base/test/test_discardable_memory_allocator.cc
@@ -9,6 +9,7 @@
 
 #include "base/check.h"
 #include "base/compiler_specific.h"
+#include "base/containers/heap_array.h"
 #include "base/memory/discardable_memory.h"
 #include "base/memory/ptr_util.h"
 
@@ -18,7 +19,7 @@ namespace {
 class DiscardableMemoryImpl : public DiscardableMemory {
  public:
   explicit DiscardableMemoryImpl(size_t size)
-      : data_(new uint8_t[size]), size_(size) {}
+      : data_(base::HeapArray<uint8_t>::Uninit(size)) {}
 
   // Overridden from DiscardableMemory:
   bool Lock() override {
@@ -32,12 +33,12 @@ class DiscardableMemoryImpl : public DiscardableMemory {
     is_locked_ = false;
     // Force eviction to catch clients not correctly checking the return value
     // of Lock().
-    UNSAFE_TODO(memset(data_.get(), 0, size_));
+    UNSAFE_TODO(memset(data_.data(), 0, data_.size()));
   }
 
   void* data() const override {
     DCHECK(is_locked_);
-    return data_.get();
+    return const_cast<uint8_t*>(data_.data());
   }
 
   void DiscardForTesting() override {}
@@ -50,8 +51,7 @@ class DiscardableMemoryImpl : public DiscardableMemory {
 
  private:
   bool is_locked_ = true;
-  std::unique_ptr<uint8_t[]> data_;
-  size_t size_;
+  base::HeapArray<uint8_t> data_;
 };
 
 }  // namespace
