7f2f4b98d98415661567bebb76cf6feb2b9c6a77
Mohamed Adel <adelm@google.com>
2025-08-20 13:18:40 -0700

    Set kCCTPredictiveBackGesture to enabled by default
    Bug: 40285983
    Change-Id: Ie64a4b9b3d683fb2f1d714e6c8af9077e5c23030
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6868213
    Commit-Queue: Moe Adel <adelm@google.com>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504160}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -874,7 +874,7 @@ BASE_FEATURE(kCCTOpenInBrowserButtonIfEnabledByEmbedder,
 
 BASE_FEATURE(kCCTPredictiveBackGesture,
              "CCTPredictiveBackGesture",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGridTabSwitcherUpdate,
              "GridTabSwitcherUpdate",
