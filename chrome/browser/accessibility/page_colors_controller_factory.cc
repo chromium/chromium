077c56c29d4fd718f95c6bc7dfdfb2099c36597c
Peter Kasting <pkasting@chromium.org>
2025-08-20 14:38:41 -0700

    Don't instantiate the PageColorsController for ash internals.
    Page Colors affect how web content colors appear, and so are irrelevant
    in this case.
    
    This also prevents failures with some future refactoring of mine that
    would otherwise lead to the PageColorsController undesirably affecting
    ash tests.
    
    Bug: 432019728
    Change-Id: I1dc3592b9afbf9ac001a63204c1951b5a5081be4
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6867374
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1504215}

--- chrome/browser/accessibility/page_colors_controller_factory.cc
+++ chrome/browser/accessibility/page_colors_controller_factory.cc
@@ -24,14 +24,12 @@ PageColorsControllerFactory::PageColorsControllerFactory()
     : ProfileKeyedServiceFactory(
           "PageColorsController",
           ProfileSelections::Builder()
-              // The incognito profile shares the PageColors with it's original
+              // The incognito profile shares the PageColors with its original
               // profile.
               .WithRegular(ProfileSelection::kRedirectedToOriginal)
               .WithGuest(ProfileSelection::kNone)
               .WithSystem(ProfileSelection::kNone)
-              // TODO(crbug.com/41488885): Check if this service is needed for
-              // Ash Internals.
-              .WithAshInternals(ProfileSelection::kRedirectedToOriginal)
+              .WithAshInternals(ProfileSelection::kNone)
               .Build()) {}
 
 PageColorsControllerFactory::~PageColorsControllerFactory() = default;
