9454c78a50373feac19a7e0ccbeb9034afaf2b38
Andrea Orru <andreaorru@chromium.org>
2025-08-20 17:37:07 -0700

    Temporarily disable flaky DebuggerExtensionApiTest.DebuggerMv3
    These seem to be dependent on the behavior established by
    OptimizeServiceWorkerStartRequests. With the feature disabled, they
    appear to be highly flaky.
    
    That feature flag is causing breakage in Chrome, so it has to be
    disabled. To allow for that, we need to temporarily disable this test
    too.
    
    Bug: 439995191
    Change-Id: I0e597af1440133cb5ac9cc1a7cc548b8edfe2e82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869030
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504298}

--- chrome/browser/extensions/api/debugger/debugger_apitest.cc
+++ chrome/browser/extensions/api/debugger/debugger_apitest.cc
@@ -858,7 +858,9 @@ IN_PROC_BROWSER_TEST_F(DebuggerExtensionApiTest, Debugger) {
 }
 #endif
 
-IN_PROC_BROWSER_TEST_F(DebuggerExtensionApiTest, DebuggerMv3) {
+// TODO(crbug.com/40276609): Reenable this test once the
+// OptimizeServiceWorkerStartRequests feature is re-enabled.
+IN_PROC_BROWSER_TEST_F(DebuggerExtensionApiTest, DISABLED_DebuggerMv3) {
   ASSERT_TRUE(RunExtensionTest("debugger_mv3")) << message_;
 }
 
