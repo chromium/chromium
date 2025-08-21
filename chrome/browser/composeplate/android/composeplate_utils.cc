b3ff4974cbe6ad875aad008d8c9969c7afcceda7
Xi Han <hanxi@google.com>
2025-08-21 11:01:59 -0700

    [M140][Composeplate] Gates Clank's AIM Entrypoint behind AimEligibilityService
    (cherry picked from commit 732a22dec7a05d61ca785c613d839d015523c244)
    
    Bug: 438960563, 440298902
    Change-Id: If578d84ce4c682979265789363968548dbe0e0b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6854220
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1503415}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6870940
    Cr-Commit-Position: refs/branch-heads/7339@{#1044}
    Cr-Branched-From: 27be8b77710f4405fdfeb4ee946fcabb0f6c92b2-refs/heads/main@{#1496484}

--- chrome/browser/composeplate/android/composeplate_utils.cc
+++ chrome/browser/composeplate/android/composeplate_utils.cc
@@ -3,16 +3,32 @@
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
+#include "chrome/browser/autocomplete/aim_eligibility_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/omnibox/browser/aim_eligibility_service.h"
 #include "components/omnibox/browser/omnibox_prefs.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/browser/composeplate/android/jni_headers/ComposeplateUtils_jni.h"
 
 // static
-jboolean JNI_ComposeplateUtils_IsEnabledByPolicy(JNIEnv* env,
-                                                 Profile* profile) {
+jboolean JNI_ComposeplateUtils_IsAimEntrypointEligible(JNIEnv* env,
+                                                       Profile* profile) {
   DCHECK(profile);
 
-  return omnibox::IsAimAllowedByPolicy(profile->GetPrefs());
+  const auto* aim_eligibility_service =
+      AimEligibilityServiceFactory::GetForProfile(profile);
+
+  // If the server eligibility is enabled, check overall eligibility alone.
+  // The service will control locale rollout so there's no need to check locale.
+  if (aim_eligibility_service->IsServerEligibilityEnabled()) {
+    return aim_eligibility_service->IsAimEligible();
+  }
+
+  if (!aim_eligibility_service->IsAimLocallyEligible()) {
+    return false;
+  }
+
+  return aim_eligibility_service->IsCountry("us") &&
+         aim_eligibility_service->IsLanguage("en");
 }
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediatorTest.java
@@ -258,7 +258,7 @@ public class LocationBarMediatorTest {
         templateUrlServiceSupplier.set(mTemplateUrlService);
         mUiOverrides = new LocationBarEmbedderUiOverrides();
         ComposeplateUtilsJni.setInstanceForTesting(mMockComposeplateUtilsJni);
-        when(mMockComposeplateUtilsJni.isEnabledByPolicy(eq(mProfile))).thenReturn(true);
+        when(mMockComposeplateUtilsJni.isAimEntrypointEligible(eq(mProfile))).thenReturn(true);
 
         mMediator =
                 new LocationBarMediator(
@@ -1560,7 +1560,7 @@ public class LocationBarMediatorTest {
     @Test
     @EnableFeatures(ChromeFeatureList.ANDROID_COMPOSEPLATE)
     public void testButtonVisibility_dontShowComposeplateUnfocused_disabledByPolicy() {
-        when(mMockComposeplateUtilsJni.isEnabledByPolicy(eq(mProfile))).thenReturn(false);
+        when(mMockComposeplateUtilsJni.isAimEntrypointEligible(eq(mProfile))).thenReturn(false);
         mProfileSupplier.set(mProfile);
 
         enableBothVoiceAndLensButtons();
