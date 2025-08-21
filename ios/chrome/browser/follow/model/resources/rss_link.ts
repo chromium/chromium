b05f7ff6b0a42b801a050d95d4b593b80a4587a1
Alexandra Pereira <alexandrasp@google.com>
2025-08-21 10:38:33 -0700

    [iOS][gCrWeb] Migrate ios/chrome/browser/follow/model to new gCrWeb API
    Update the TypeScript feature rssLink from the deprecated `gCrWebLegacy`
    object to the new `gCrWeb` API. No functional change is intended.
    
    Bug: 399666983
    Change-Id: If12514e2e6577b422efacdcfb5f606f8868e7602
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6814272
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1504620}

--- ios/chrome/browser/follow/model/resources/rss_link.ts
+++ ios/chrome/browser/follow/model/resources/rss_link.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {gCrWebLegacy} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+import {CrWebApi, gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
 
 /**
  * @fileoverview Functions used to parse RSS links from a web page.
@@ -27,4 +27,6 @@ function getRSSLinks(): string[] {
   return rssLinks;
 }
 
-gCrWebLegacy.rssLink = {getRSSLinks};
+const rssLinkApi = new CrWebApi();
+rssLinkApi.addFunction('getRSSLinks', getRSSLinks);
+gCrWeb.registerApi('rssLink', rssLinkApi);
