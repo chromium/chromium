96a2cad51688a45b1ad979819dfaa9659bab2c45
Julia Hansbrough <flowerhack@google.com>
2025-08-22 08:32:57 -0700

    Roll arbitrary: 1.4.1 => 1.4.2 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * arbitrary: 1.4.1 => 1.4.2; https://docs.rs/crate/arbitrary/1.4.2
    
    Bug: None
    Change-Id: I877359f83434797db9fa23a0af3a0d346387bc3b
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6869211
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505088}

--- third_party/rust/chromium_crates_io/vendor/arbitrary-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/arbitrary-v1/src/lib.rs
@@ -6,6 +6,6 @@
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `arbitrary-1.4.1` crate.
+// `arbitrary-1.4.2` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
