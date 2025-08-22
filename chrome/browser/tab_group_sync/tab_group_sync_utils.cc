67a6aa13200092109adb873e210648af767271b1
Min Qin <qinmin@chromium.org>
2025-08-22 14:35:03 -0700

    Check if the initiator origin is extension URL instead of empty
    For extensions, the chrome.tab.update() will use extension URL in
    the initiator origin, rather than empty one.
    
    Bug: 430076891
    Change-Id: I39a9c8478fa95fa6a93fa3eac3ba645488553058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6876522
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505338}

--- chrome/browser/tab_group_sync/tab_group_sync_utils.cc
+++ chrome/browser/tab_group_sync/tab_group_sync_utils.cc
@@ -9,6 +9,7 @@
 #include "components/saved_tab_groups/public/utils.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/render_frame_host.h"
+#include "extensions/common/constants.h"
 #include "net/http/http_request_headers.h"
 
 namespace tab_groups {
@@ -62,13 +63,20 @@ bool TabGroupSyncUtils::IsSaveableNavigation(
   // auto triggered on restoration. So there is no need to save them.
   if (navigation_handle->IsRendererInitiated() &&
       !navigation_handle->HasUserGesture()) {
-    // For navigations without initiator origin, they are likely from an
-    // extension.
-    if (!is_extension_navigation_allowed ||
-        navigation_handle->GetInitiatorOrigin().has_value() ||
-        navigation_handle->IsHistory()) {
+    if (!is_extension_navigation_allowed || navigation_handle->IsHistory()) {
       return false;
     }
+
+    // Navigation triggered by Extension's chrome.tab.update will have extension
+    // URL as the initiator origin.
+    if (navigation_handle->GetInitiatorOrigin().has_value()) {
+      GURL origin_url = navigation_handle->GetInitiatorOrigin()->GetURL();
+      if (origin_url.SchemeIs(extensions::kExtensionScheme)) {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   return true;
