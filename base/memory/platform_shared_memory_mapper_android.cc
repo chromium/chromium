352ccecb988dfddfcc7c36ce5cc7f4bd3ee29518
Egor Pasko <pasko@chromium.org>
2025-08-22 15:42:27 -0700

    doc: android: Clarify usage of MAP_SHARED in PlatformSharedMemoryMapper
    As we are transitioning to memfd in new Android releases, I wanted to
    clarify how the transition will (beautifully) _not_ break the function
    to map read-only regions.
    
    Bug: 410781850
    Change-Id: Ia1662cce29bc9ac6ce94801c4aea57188574f593
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6876719
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505378}

--- base/memory/platform_shared_memory_mapper_android.cc
+++ base/memory/platform_shared_memory_mapper_android.cc
@@ -18,8 +18,31 @@ std::optional<span<uint8_t>> PlatformSharedMemoryMapper::Map(
     uint64_t offset,
     size_t size) {
   // IMPORTANT: Even if the mapping is readonly and the mapped data is not
-  // changing, the region must ALWAYS be mapped with MAP_SHARED, otherwise with
-  // ashmem the mapping is equivalent to a private anonymous mapping.
+  // changing, the region should be mapped with MAP_SHARED.
+  //
+  // With ashmem doing a mmap(MAP_PRIVATE) would be equivalent anonymous
+  // mapping, not a shared one.
+  //
+  // Historically, memfd had the opposite and confusing behavior: attempting to
+  // mmap(MAP_SHARED) a sealed, read-only region would fail, while
+  // mmap(MAP_PRIVATE) would correctly create a read-only shared mapping.
+  //
+  // This inconsistency was resolved in Linux kernel 6.7, which now allows
+  // MAP_SHARED to work as well. This fix may also be cherry-picked to
+  // older LTS kernels.
+  //
+  // See the upstream patches:
+  //    https://lkml.kernel.org/r/cover.1697116581.git.lstoakes@gmail.com
+  //
+  // For old devices PlatformSharedMemoryMapper only receives ashmem regions.
+  // New devices shipping with Android 17 do not have ashmem, and memfd should
+  // be used instead. These new systems are based on 6.12+ kernels (in other
+  // words, MAP_SHARED works for them). Devices updating to Android 17
+  // (potentially with kernels as old as 6.6) continue using ashmem.
+  //
+  // Therefore, it is unnecessary to dynamically check the memory region type
+  // (ashmem vs. memfd), as MAP_SHARED is the correct flag for all scenarios
+  // this code handles.
   void* address =
       mmap(nullptr, size, PROT_READ | (write_allowed ? PROT_WRITE : 0),
            MAP_SHARED, handle, checked_cast<off_t>(offset));
