5a754d953f52df635179e5a7b8d56826f4367db3
Sylvain Defresne <sdefresne@chromium.org>
2025-08-25 03:33:00 -0700

    [ios] Implement PrerenderBrowserAgent by calling PrerenderService
    As a first step to converting PrerenderService into a Browser agent,
    implement PrerenderBrowserAgent by calling PrerenderService. This
    will allow converting code using PrerenderService to use the new API
    without introducing any behaviour change.
    
    Bug: 40726702
    Change-Id: I117f27aa1d649679f5c3ff733b4fa3f86e3abaa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6850506
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505780}

--- ios/chrome/browser/prerender/model/prerender_browser_agent.h
+++ ios/chrome/browser/prerender/model/prerender_browser_agent.h
@@ -5,12 +5,58 @@
 #ifndef IOS_CHROME_BROWSER_PRERENDER_MODEL_PRERENDER_BROWSER_AGENT_H_
 #define IOS_CHROME_BROWSER_PRERENDER_MODEL_PRERENDER_BROWSER_AGENT_H_
 
+#include "base/time/time.h"
+#include "ios/chrome/browser/prerender/model/prerender_tab_helper_delegate.h"
 #include "ios/chrome/browser/shared/model/browser/browser_user_data.h"
+#include "ios/web/public/navigation/referrer.h"
+#include "ui/base/page_transition_types.h"
+#include "url/gurl.h"
+
+@protocol PreloadControllerDelegate;
 
 // A BrowserAgent responsible for managing the pre-rendering of web pages.
-class PrerenderBrowserAgent : public BrowserUserData<PrerenderBrowserAgent> {
+class PrerenderBrowserAgent final
+    : public BrowserUserData<PrerenderBrowserAgent>,
+      public PrerenderTabHelperDelegate {
  public:
-  ~PrerenderBrowserAgent() override;
+  // Policy for starting pre-rendering.
+  enum PrerenderPolicy {
+    kNoDelay,
+    kDefaultDelay,
+  };
+
+  ~PrerenderBrowserAgent() final;
+
+  // Sets the delegate that will provide information to this agent.
+  void SetDelegate(id<PreloadControllerDelegate> delegate);
+
+  // Prerenders the given `url` with the given `transition` after `delay`.
+  //
+  // If there is already an existing request for `url`, this method does
+  // nothing and does not reset the delay. If there is an existing request
+  // for a different URL, it is cancelled before the new request is queued.
+  //
+  // Unless `policy` is `kNoDelay` the pre-rendering is only started after
+  // a short delay, to prevent unnecessary pre-rendering while the user is
+  // typing.
+  void StartPrerender(const GURL& url,
+                      const web::Referrer& referrer,
+                      ui::PageTransition transition,
+                      PrerenderPolicy policy);
+
+  // Promotes the pre-rendered tab to a real tab, replacing the Browser's
+  // active WebState if it is used to pre-render `url`. Otherwise cancels
+  // the pre-rendering.
+  //
+  // Returns whether the active WebState was replaced or not.
+  bool ValidatePrerender(const GURL& url, ui::PageTransition transition);
+
+  // Returns whether a pre-rendered WebState is being inserted into the
+  // Browser by this agent.
+  bool IsInsertingPrerender() const;
+
+  // PrerenderTabHelperDelegate implementation.
+  void CancelPrerender() final;
 
  private:
   friend class BrowserUserData<PrerenderBrowserAgent>;
--- ios/chrome/browser/prerender/model/prerender_browser_agent.mm
+++ ios/chrome/browser/prerender/model/prerender_browser_agent.mm
@@ -4,7 +4,44 @@
 
 #import "ios/chrome/browser/prerender/model/prerender_browser_agent.h"
 
+#import "ios/chrome/browser/prerender/model/prerender_service.h"
+#import "ios/chrome/browser/prerender/model/prerender_service_factory.h"
+#import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
+
 PrerenderBrowserAgent::~PrerenderBrowserAgent() = default;
 
 PrerenderBrowserAgent::PrerenderBrowserAgent(Browser* browser)
     : BrowserUserData(browser) {}
+
+void PrerenderBrowserAgent::SetDelegate(
+    id<PreloadControllerDelegate> delegate) {
+  PrerenderServiceFactory::GetForProfile(browser_->GetProfile())
+      ->SetDelegate(delegate);
+}
+
+void PrerenderBrowserAgent::StartPrerender(const GURL& url,
+                                           const web::Referrer& referrer,
+                                           ui::PageTransition transition,
+                                           PrerenderPolicy policy) {
+  PrerenderServiceFactory::GetForProfile(browser_->GetProfile())
+      ->StartPrerender(url, referrer, transition,
+                       browser_->GetWebStateList()->GetActiveWebState(),
+                       /*immediately=*/policy == PrerenderPolicy::kNoDelay);
+}
+
+bool PrerenderBrowserAgent::ValidatePrerender(const GURL& url,
+                                              ui::PageTransition transition) {
+  return PrerenderServiceFactory::GetForProfile(browser_->GetProfile())
+      ->MaybeLoadPrerenderedURL(url, transition, browser_);
+}
+
+bool PrerenderBrowserAgent::IsInsertingPrerender() const {
+  return PrerenderServiceFactory::GetForProfile(browser_->GetProfile())
+      ->IsLoadingPrerender();
+}
+
+void PrerenderBrowserAgent::CancelPrerender() {
+  PrerenderServiceFactory::GetForProfile(browser_->GetProfile())
+      ->CancelAllPrerenders();
+}
