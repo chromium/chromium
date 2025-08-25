a2757b19616506e041c3da6f8608ff38cdd11443
Yuly Novikov <ynovikov@chromium.org>
2025-08-25 04:57:50 -0700

    Revert "Add a fieldtrial testing entry for OptimizationGuideOnDeviceModelCpuBackend."
    This reverts commit 1a358de7105106812fb87d9daddd0acf5765f0bc.
    
    Reason for revert: flaky win_optional_gpu_tests_rel failures, e.g.
    https://ci.chromium.org/ui/p/chromium/builders/try/win_optional_gpu_tests_rel/187347/overview
    
    Original change's description:
    > Add a fieldtrial testing entry for OptimizationGuideOnDeviceModelCpuBackend.
    >
    > This CL was created on behalf of asully@chromium.org. Please review the
    > CL and approve it if it looks good. Contact the listed reviewers if you
    > have any questions.
    >
    > Change-Id: I34fc79512cbcabfbf1e3c363e41d9922b2588072
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6866686
    > Auto-Submit: Austin Sullivan <asully@chromium.org>
    > Commit-Queue: Steven Holte <holte@chromium.org>
    > Reviewed-by: Steven Holte <holte@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1505337}
    
    Bug: 440862258
    Change-Id: I6912622114680eb0b6f103b6e41dfb2e9fd4a2d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6873925
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Piotr Wrótniak <piotrwrotniak@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Auto-Submit: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1505798}

--- chrome/browser/optimization_guide/model_execution/model_execution_browsertest.cc
+++ chrome/browser/optimization_guide/model_execution/model_execution_browsertest.cc
@@ -55,7 +55,6 @@
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
-#include "services/on_device_model/public/cpp/cpu.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "third_party/tflite/buildflags.h"
 
@@ -673,13 +672,8 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
 #endif
 IN_PROC_BROWSER_TEST_F(ModelExecutionEnabledBrowserTest,
                        MAYBE_GetOnDeviceModelEligibilityModelNotEligible) {
-  // The CPU backend can be used when the GPU is unavailable.
-  const auto expected_elibility =
-      on_device_model::IsCpuCapable()
-          ? OnDeviceModelEligibilityReason::kModelToBeInstalled
-          : OnDeviceModelEligibilityReason::kModelNotEligible;
   EXPECT_EQ(GetOnDeviceModelEligibility(ModelBasedCapabilityKey::kCompose),
-            expected_elibility);
+            OnDeviceModelEligibilityReason::kModelNotEligible);
 }
 
 IN_PROC_BROWSER_TEST_F(
