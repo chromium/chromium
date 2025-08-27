ea1a9ac36ccb6d70c205020be058ebef297b0097
Keigo Oka <oka@google.com>
2025-08-26 23:43:03 -0700

    extensions: Add comments to unimplemented developer_private functions
    This adds comments to explain why some functions in developer_private
    are not implemented on desktop Android.
    
    Bug: 392777363
    Change-Id: I1d45431d548567227bff3153e0a4930f31a5198c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6882839
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1506928}

--- chrome/browser/extensions/api/developer_private/developer_private_functions_android.h
+++ chrome/browser/extensions/api/developer_private/developer_private_functions_android.h
@@ -5,12 +5,16 @@
 #ifndef CHROME_BROWSER_EXTENSIONS_API_DEVELOPER_PRIVATE_DEVELOPER_PRIVATE_FUNCTIONS_ANDROID_H_
 #define CHROME_BROWSER_EXTENSIONS_API_DEVELOPER_PRIVATE_DEVELOPER_PRIVATE_FUNCTIONS_ANDROID_H_
 
-#include "chrome/common/extensions/api/developer_private.h"
 #include "extensions/browser/extension_function.h"
 
+// We don't have to port it to desktop android because it's an old API for
+// chromeos, that is kept just in case there's an app still using it.
 DECLARE_UNIMPLEMENTED_EXTENSION_FUNCTION(DeveloperPrivateLoadDirectoryFunction,
                    "developerPrivate.loadDirectory",
                    DEVELOPERPRIVATE_LOADUNPACKEDCROS);
+
+// We don't have to port it to desktop android because MV2 is not supported
+// there.
 DECLARE_UNIMPLEMENTED_EXTENSION_FUNCTION(
     DeveloperPrivateDismissMv2DeprecationNoticeForExtensionFunction,
     "developerPrivate.dismissMv2DeprecationNoticeForExtension",
