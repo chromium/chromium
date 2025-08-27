0bbf3cb308c6a2873f83c6531ee67d645ed9a534
Devlin Cronin <rdevlin.cronin@chromium.org>
2025-08-27 11:45:49 -0700

    [Extensions + Apps] Put some more files behind enable_platform_apps
    Put app_base_window.[h|cc] and app_window_controller.[h|cc] behind
    the enable_platform_apps gn flag.
    
    Bug: 41407868
    Change-Id: I3f2e265641e3e1aaf882717f837461393d89561e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6854389
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507245}

--- chrome/browser/extensions/api/tabs/app_base_window.h
+++ chrome/browser/extensions/api/tabs/app_base_window.h
@@ -6,8 +6,11 @@
 #define CHROME_BROWSER_EXTENSIONS_API_TABS_APP_BASE_WINDOW_H_
 
 #include "base/memory/raw_ptr.h"
+#include "extensions/buildflags/buildflags.h"
 #include "ui/base/base_window.h"
 #include "ui/base/mojom/window_show_state.mojom-forward.h"
+
+static_assert(BUILDFLAG(ENABLE_PLATFORM_APPS));
 
 namespace extensions {
 
