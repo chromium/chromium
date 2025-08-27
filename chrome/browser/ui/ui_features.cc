1c12e7e9c45f5353d0c441ee30f997a4ba43f6e0
Ian Clelland <iclelland@chromium.org>
2025-08-27 09:33:45 -0700

    Disable "OfferPinToTaskbarInFirstRunExperience" by default.
    Change-Id: Id23bf30d8ca0b316ebdb35ac05cfb087f89814b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6890420
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Auto-Submit: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507139}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -60,7 +60,7 @@ BASE_FEATURE(kOfferPinToTaskbarWhenSettingToDefault,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kOfferPinToTaskbarInFirstRunExperience,
              "OfferPinToTaskbarInFirstRunExperience",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(OfferPinToTaskbarInSettings, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
