ee8fb19d14cf5c1ccd9695cae31cb170dd45fdd5
David Yeung <dayeung@chromium.org>
2025-08-27 14:09:52 -0700

    [TabStrip API] Add utility method to filter batched events
    Add a C++ client library to filter batched events. Clients can use these
    methods to target events they care about.
    
    Bug: 439964657, 437340218
    Change-Id: I58e52a2f8a554f6b378af25dd488c7c2ec5ed46d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879392
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507365}

---
+++ chrome/browser/ui/tabs/tab_strip_api/utilities/event_filters.h
@@ -0,0 +1,38 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_TABS_TAB_STRIP_API_UTILITIES_EVENT_FILTERS_H_
+#define CHROME_BROWSER_UI_TABS_TAB_STRIP_API_UTILITIES_EVENT_FILTERS_H_
+
+#include <vector>
+
+#include "chrome/browser/ui/tabs/tab_strip_api/tab_strip_api.mojom.h"
+#include "chrome/browser/ui/tabs/tab_strip_api/tab_strip_api_events.mojom.h"
+
+namespace tabs_api::events {
+
+// Note these utiliy methods return a non-owning pointers to the event objects.
+//
+std::vector<const tabs_api::mojom::OnTabsCreatedEvent*>
+FilterForTabsCreatedEvents(
+    const std::vector<tabs_api::mojom::TabsEventPtr>& events);
+
+std::vector<const tabs_api::mojom::OnTabsClosedEvent*>
+FilterForTabsClosedEvents(
+    const std::vector<tabs_api::mojom::TabsEventPtr>& events);
+
+std::vector<const tabs_api::mojom::OnTabMovedEvent*> FilterForTabMovedEvents(
+    const std::vector<tabs_api::mojom::TabsEventPtr>& events);
+
+std::vector<const tabs_api::mojom::OnDataChangedEvent*>
+FilterForDataChangedEvents(
+    const std::vector<tabs_api::mojom::TabsEventPtr>& events);
+
+std::vector<const tabs_api::mojom::OnTabGroupCreatedEvent*>
+FilterForTabGroupCreatedEvents(
+    const std::vector<tabs_api::mojom::TabsEventPtr>& events);
+
+}  // namespace tabs_api::events
+
+#endif  // CHROME_BROWSER_UI_TABS_TAB_STRIP_API_UTILITIES_EVENT_FILTERS_H_
