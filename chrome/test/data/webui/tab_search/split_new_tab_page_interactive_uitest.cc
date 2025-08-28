80be27162323b6abf4361675b67cb88b67a59f02
Daniil Sakhapov <sakhapov@chromium.org>
2025-08-28 02:06:14 -0700

    [Gardener] Disable flaky test on Mac
    The test is known to be flaky per comments in the original CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/6867957/comment/6e72d6fb_e319de01/
    
    Bug: 441648206
    Change-Id: I628e3a1b9b39e29015964c2d6e184e118d1d8086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6894652
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507603}

--- chrome/test/data/webui/tab_search/split_new_tab_page_interactive_uitest.cc
+++ chrome/test/data/webui/tab_search/split_new_tab_page_interactive_uitest.cc
@@ -45,7 +45,7 @@ class SplitNewTabPageUiTest
   GURL GetTestUrl() { return embedded_test_server()->GetURL("/title1.html"); }
 };
 
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC)
 #define MAYBE_Focus DISABLED_Focus
 #else
 #define MAYBE_Focus Focus
