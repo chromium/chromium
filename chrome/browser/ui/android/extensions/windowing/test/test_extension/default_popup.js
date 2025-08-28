7d750f4a0ea940923fb561db02328a316d9d0caf
Linyu He <linyuh@google.com>
2025-08-28 15:02:16 -0700

    Support all chrome.windows.get*() APIs in the test extension
    Changes:
    
    * Add a button for each of the following:
      chrome.windows.getAll()
      chrome.windows.getCurrent()
      chrome.windows.getLastFocused()
      chrome.windows.get()
    
    * chrome.windows.get() requires a window ID, so this CL adds a text
      field next to the button.
    
    * Add a text area to show an API return value when a button is clicked,
      which will make it easier to test/demo.
    
    Bug: 441377984
    Change-Id: I1c5b38f88e3438ca3ecc972ac7b9c4ee125e8eee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897417
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508044}

--- chrome/browser/ui/android/extensions/windowing/test/test_extension/default_popup.js
+++ chrome/browser/ui/android/extensions/windowing/test/test_extension/default_popup.js
@@ -2,9 +2,52 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+function setApiReturnValue(text) {
+    const apiReturnValueTextArea = document.getElementById(
+        "api_return_value_text_area");
+    apiReturnValueTextArea.value = text;
+}
+
 document.getElementById("get_all_windows_button").onclick = async () => {
     const windows = await chrome.windows.getAll();
-
-    const numWindows = document.getElementById("num_windows");
-    numWindows.textContent = windows.length;
+    const result =
+        `All windows: ${windows.length}\n\n${JSON.stringify(windows, null, 2)}`;
+    setApiReturnValue(result);
 };
+
+document.getElementById("get_current_window_button").onclick = async () => {
+    const window = await chrome.windows.getCurrent();
+    const result = `Current window:\n\n${JSON.stringify(window, null, 2)}`;
+    setApiReturnValue(result);
+};
+
+document.getElementById("get_last_focused_window_button").onclick =
+    async () => {
+        const window = await chrome.windows.getLastFocused();
+        const result =
+            `Last focused window:\n\n${JSON.stringify(window, null, 2)}`;
+        setApiReturnValue(result);
+    }
+
+document.getElementById("get_window_button").onclick = async () => {
+    const windowIdString = document.getElementById("window_id_input").value;
+    if (!windowIdString) {
+        setApiReturnValue("Please enter a window ID.");
+        return;
+    }
+
+    const windowId = parseInt(windowIdString);
+    if (isNaN(windowId)) {
+        setApiReturnValue(`Invalid window ID: ${windowIdString}`);
+        return;
+    }
+
+    try {
+        const window = await chrome.windows.get(windowId);
+        const result =
+            `Window with ID ${windowId}: ${JSON.stringify(window, null, 2)}`;
+        setApiReturnValue(result);
+    } catch (error) {
+        setApiReturnValue(`${error}`);
+    }
+}
