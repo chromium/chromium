1d50b7216dd3ff41e75b58164f81a70866b271ea
Andrew Grieve <agrieve@chromium.org>
2025-08-28 10:15:10 -0700

    Android: Delete Supplier.hasValue()
    Bug: 440309602
    Change-Id: I8e8e944cb2e42b9bf2da5f899cdaf1b1d8a36744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6893785
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507805}

--- base/android/java/src/org/chromium/base/supplier/Supplier.java
+++ base/android/java/src/org/chromium/base/supplier/Supplier.java
@@ -9,14 +9,9 @@ import org.chromium.build.annotations.Nullable;
 
 /**
  * Based on Java 8's java.util.function.Supplier. Similar to Callable<T>, but without a checked
- * Exception and with #hasValue().
+ * Exception.
  *
  * @param <T> Return type.
  */
 @NullMarked
-public interface Supplier<T extends @Nullable Object> extends java.util.function.Supplier<T> {
-    /** Returns whether the supplier holds a value currently. */
-    default boolean hasValue() {
-        return get() != null;
-    }
-}
+public interface Supplier<T extends @Nullable Object> extends java.util.function.Supplier<T> {}
