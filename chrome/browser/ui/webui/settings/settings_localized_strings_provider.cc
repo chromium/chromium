1a3f8c7a61dd821e6ab50af42ab83158b9bd145e
David Bienvenu <davidbienvenu@chromium.org>
2025-08-28 10:17:19 -0700

    Add settings pin to taskbar strings
    These strings will be used to tell the user that setting Chrome as the
    default browser in the Default Browser settings UI will also attempt to
    pin to the taskbar, if supported (Windows only).
    
    crrev.com/c/6871963 gives the full context of the settings taskbar
    pinning feature.
    
    Bug: 393629107
    Change-Id: Iea0b1157aa8824480085f0a0cbf0c2cc24c45ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6896926
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1507808}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -704,6 +704,8 @@ void AddDefaultBrowserStrings(content::WebUIDataSource* html_source) {
       {"defaultBrowser", IDS_SETTINGS_DEFAULT_BROWSER},
       {"defaultBrowserDefault", IDS_SETTINGS_DEFAULT_BROWSER_DEFAULT},
       {"defaultBrowserMakeDefault", IDS_SETTINGS_DEFAULT_BROWSER_MAKE_DEFAULT},
+      {"defaultBrowserMakeDefaultAndPin",
+       IDS_SETTINGS_DEFAULT_BROWSER_MAKE_DEFAULT_AND_PIN},
       {"defaultBrowserMakeDefaultButton",
        IDS_SETTINGS_DEFAULT_BROWSER_MAKE_DEFAULT_BUTTON},
       {"defaultBrowserError", IDS_SETTINGS_DEFAULT_BROWSER_ERROR},
