670b6f192f4668d2ac2c06bd77ec3e4eeda7d648
Helmut Januschka <helmut@januschka.com>
2025-08-29 11:50:09 -0700

    Revert "Fix modulepreload to send proper referrer headers"
    This reverts commit 33458870989e18fbc949b5c538fcaa60daa2f7f1.
    
    Reason for revert: it landed accidentally before feature approval!!
    Bug: 409959472
    
    Original change's description:
    > Fix modulepreload to send proper referrer headers
    >
    > Modulepreload requests were using NoReferrer() instead of
    > ClientReferrerString(). This change ensures modulepreload sends the
    > client's referrer, consistent with the HTML specification which requires
    > using the client's referrer for module fetches.
    >
    > Added a test that verifies both dynamic imports and modulepreload
    > correctly send referrer headers. Used an onload event rather than
    > a timeout for better test reliability.
    >
    > Bug: 409959472
    > Change-Id: I85ee55f2f027948a853512fe8893804bd8c1d3c5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6509110
    > Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    > Commit-Queue: Helmut Januschka <helmut@januschka.com>
    > Cr-Commit-Position: refs/heads/main@{#1489322}
    
    (cherry picked from commit 3bf88f10d41c288aee1f2ffcaaf852e43cff982c)
    
    Bug: 409959472
    Change-Id: I21295d7a1752f40b42ddc00d55fbc5f4d524b5cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6895357
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1507900}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6900936
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7339_41@{#3}
    Cr-Branched-From: ad7a45a75341bd4b1918ad98336e3ec6731363ec-refs/branch-heads/7339@{#1349}
    Cr-Branched-From: 27be8b77710f4405fdfeb4ee946fcabb0f6c92b2-refs/heads/main@{#1496484}

--- third_party/blink/renderer/core/loader/preload_helper.cc
+++ third_party/blink/renderer/core/loader/preload_helper.cc
@@ -700,7 +700,7 @@ void PreloadHelper::ModulePreloadIfNeeded(
                          params.referrer_policy,
                          mojom::blink::FetchPriorityHint::kAuto,
                          RenderBlockingBehavior::kNonBlocking),
-      Referrer::ClientReferrerString(), TextPosition::MinimumPosition(),
+      Referrer::NoReferrer(), TextPosition::MinimumPosition(),
       ModuleImportPhase::kEvaluation);
 
   // Step 13. "Fetch a modulepreload module script graph given url, destination,
