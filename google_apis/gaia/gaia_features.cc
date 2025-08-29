76c04206468b659116b337cde09a7d87bca9a9b9
Alex Ilin <alexilin@chromium.org>
2025-08-29 04:43:52 -0700

    [signin] Disable GetAccountCapabilitiesUsesGetAllVisibleUrl feature
    "getAllVisible" API fails if one of the capabilities visible to Chrome
    cannot be evaluated for some accounts.
    
    We cannot enable this feature until this issue is fixed.
    
    Bug: 436151197
    Change-Id: I53768af1bde776785ea5bca677155e71121ed5ac
    Fixed: 441896954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6897630
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1508289}

--- google_apis/gaia/gaia_features.cc
+++ google_apis/gaia/gaia_features.cc
@@ -17,9 +17,10 @@ BASE_FEATURE(kListAccountsUsesBinaryFormat,
 // Switches account capabilities fetch to the "getAllVisible" URL that fetches
 // all capabilities visible to Chrome instead of asking for a hardcoded list of
 // capabilities that might be only partially available.
+// Consult https://crbug.com/436151197 before enabling.
 COMPONENT_EXPORT(GOOGLE_APIS)
 BASE_FEATURE(kGetAccountCapabilitiesUsesGetAllVisibleUrl,
              "GetAccountCapabilitiesUsesGetAllVisibleUrl",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 }  // namespace gaia::features
