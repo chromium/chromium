3971365c18b14513cd6b3f0b48307d08667e2eb7
Viktor Semeniuk <vsemeniuk@google.com>
2025-08-29 07:49:50 -0700

    Enable ApplyClientsideModelPredictionsForOtps by default
    Enabled-by-default-reason: feature alone has no effect. It is relevant for both password change and OTP experiments, but indeed has no user-
    visible changes attached to it, and it's not possible to control the
    feature from 2 Finch experiments at once, so we are transforming it into
    a kill switch.
    
    Bug: 392020509
    Change-Id: Icc060e0ab4c6e825aa45be83041bbfbcd4961474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6899686
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1508366}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -25,7 +25,7 @@ BASE_FEATURE(kApplyClientsideModelPredictionsForPasswordTypes,
 
 BASE_FEATURE(kApplyClientsideModelPredictionsForOtps,
              "ApplyClientsideModelPredictionsForOtps",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAutoApproveSharedPasswordUpdatesFromSameSender,
              "AutoApproveSharedPasswordUpdatesFromSameSender",
