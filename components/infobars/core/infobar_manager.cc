81789ae74e9ee621ef3680de0a7cd72361937909
Foromo Daniel Soromou <koretadaniel@chromium.org>
2025-09-02 06:16:16 -0700

    [Cleanup] remove stale comment, since the check is invariant
    Change-Id: I989dd00e55fe6deae5d74ff9205d4558998bc080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6898353
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509520}

--- components/infobars/core/infobar_manager.cc
+++ components/infobars/core/infobar_manager.cc
@@ -154,9 +154,6 @@ void InfoBarManager::RemoveInfoBarInternal(InfoBar* infobar, bool animate) {
   DCHECK(infobar);
 
   auto i = std::ranges::find(infobars_, infobar);
-  // TODO(crbug.com/): Temporarily a CHECK instead of a DCHECK CHECK() in order
-  // to help diagnose suspected memory smashing caused by invalid call of this
-  // method happening in production code on iOS.
   CHECK(i != infobars_.end());
 
   // Remove the infobar before notifying, so that if any observers call back to
