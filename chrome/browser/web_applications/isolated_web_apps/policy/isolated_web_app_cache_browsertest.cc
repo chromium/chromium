5e89cf68a121597ed2b973dcd7388452e54dd600
Irina <irfedorova@google.com>
2025-09-02 01:03:50 -0700

    iwa caching: fix ShowNetworkDialogWhenLaunchFromCacheFailed test
    The test was disabled because of the timeut in linux-chromeos-dbg
    build. This happened because it enabled the Internet first and only
    then added it to the server. In fast builds it means the kiosk is
    trying to launch again since it is online, but the IWA is not available.
    
    Now I fix the test by adding the IWA to the server first and only after
    that I enable Internet.
    
    Fixed: 428841050
    Change-Id: I17cbbee47b7419ddff60964c28fe24618a412225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904547
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1509398}

--- chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_cache_browsertest.cc
+++ chrome/browser/web_applications/isolated_web_apps/policy/isolated_web_app_cache_browsertest.cc
@@ -1145,19 +1145,12 @@ IN_PROC_BROWSER_TEST_F(IwaCacheKioskTest,
   WaitNetworkScreen();
 
   network_state_.SimulateOnline();
-  AddNewIwaToServer(
-      IwaServerConfig{kWebBundleId, GetBaseVersion(), kPublicKeyPair});
-
   ASSERT_TRUE(WaitKioskLaunched());
 }
 
-// This test times out on ASan / LSan:
-// https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/65295/overview
-// and on a (less exotic) Linux CQ bot:
-// https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/41086/overview
 // Cache is not available, the network dialog should be shown.
 IN_PROC_BROWSER_TEST_F(IwaCacheKioskTest,
-                       DISABLED_ShowNetworkDialogWhenLaunchFromCacheFailed) {
+                       ShowNetworkDialogWhenLaunchFromCacheFailed) {
   CheckPathDoesNotExist(GetCachedBundlePath(kWebBundleId, GetBaseVersion()));
   network_state_.SimulateOffline();
   RemoveAllBundlesFromUpdateServer();
@@ -1165,9 +1158,9 @@ IN_PROC_BROWSER_TEST_F(IwaCacheKioskTest,
 
   WaitNetworkScreen();
 
-  network_state_.SimulateOnline();
   AddNewIwaToServer(
       IwaServerConfig{kWebBundleId, GetBaseVersion(), kPublicKeyPair});
+  network_state_.SimulateOnline();
   ASSERT_TRUE(WaitKioskLaunched());
 }
 
