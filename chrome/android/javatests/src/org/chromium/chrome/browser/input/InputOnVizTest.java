618b1b5b3de5608f04a126bad07ac072516d1d78
Eleanor Lee <eleanorlee@google.com>
2025-09-02 17:12:40 -0700

    Disable input onviz test
    Disable InputOnVizTest#handlesOverscrollsWithInputVizard
    
    Bug: 442663835
    Change-Id: I665e7f2e0478aeca2a3437b2e4cb4b91dfc0e21e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909917
    Reviewed-by: Shu Yang <shuyng@google.com>
    Owners-Override: Eleanor Lee <eleanorlee@google.com>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1509953}

--- chrome/android/javatests/src/org/chromium/chrome/browser/input/InputOnVizTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/input/InputOnVizTest.java
@@ -138,6 +138,7 @@ public class InputOnVizTest {
     }
 
     @Test
+    @DisabledTest(message = "https://crbug.com/442663835")
     public void handlesOverscrollsWithInputVizard() throws Exception {
         TabLoadObserver observer = new TabLoadObserver(mActivityTestRule.getActivityTab());
         observer.fullyLoadUrl(mLongHtmlTestPageUri);
