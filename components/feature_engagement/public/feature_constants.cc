85c3fdabef2dcff6213e997971e7f369c86f1868
Nohemi Fernandez <fernandex@google.com>
2025-09-03 08:09:59 -0700

    [iOS] Enable What's New badge for Reading Mode in overflow menu.
    Enabled-by-default-reason: killswitch
    
    Bug: 428904617
    Change-Id: Ibc5f59308aca36f034b4dde17d4c054cb8eacae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909032
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510241}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -608,7 +608,7 @@ BASE_FEATURE(kIPHLongPressToolbarTipFeature,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHBadgedReaderModeFeature,
              "IPH_BadgedReaderMode",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHBadgedReadingListFeature,
              "IPH_BadgedReadingList",
              base::FEATURE_DISABLED_BY_DEFAULT);
