323bcde750b3c7b8af4da72068112b973eda5999
Hans Wennborg <hans@chromium.org>
2025-09-03 04:59:35 -0700

    Remove operator<< for UpdateService::PolicySameVersionUpdate
    https://chromium-review.googlesource.com/c/chromium/src/+/6901981 made
    that type alias the mojo type, which also has an operator<<, at which
    point the lookup became ambiguous.
    
    This was only flagged by the latest clang version, which is why it
    wasn't noticed by the CQ.
    
    Bug: 442600664
    Change-Id: Iee99480db8d0b9c3e95e666a9cd40b1accb63edb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909874
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510150}

--- chrome/updater/update_service.h
+++ chrome/updater/update_service.h
@@ -200,19 +200,6 @@ struct EnumTraits<UpdateService::ErrorCategory> {
 std::ostream& operator<<(std::ostream& os,
                          const UpdateService::UpdateState& update_state);
 
-inline std::ostream& operator<<(
-    std::ostream& os,
-    const UpdateService::PolicySameVersionUpdate& policy_same_version_update) {
-  return os << [&policy_same_version_update] {
-    switch (policy_same_version_update) {
-      case UpdateService::PolicySameVersionUpdate::kNotAllowed:
-        return "not allowed";
-      case UpdateService::PolicySameVersionUpdate::kAllowed:
-        return "allowed";
-    }
-  }();
-}
-
 bool operator==(const UpdateService::UpdateState& lhs,
                 const UpdateService::UpdateState& rhs);
 
