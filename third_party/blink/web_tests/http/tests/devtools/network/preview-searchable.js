23ebab2664f9cc5b4d77b253dcaf8ef2475a4581
Philip Pfaffe <pfaffe@chromium.org>
2025-09-03 02:44:53 -0700

    Update and reenable two devtools tests
    This also reverts commit 67a77865ab1c8511bb9040befb33dce38674d76f.
    
    Bug: 407752215
    Change-Id: Ibd7f9ec4d1bd675e498e6d781b16a73ac28f14f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6905423
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510113}

--- third_party/blink/web_tests/http/tests/devtools/network/preview-searchable.js
+++ third_party/blink/web_tests/http/tests/devtools/network/preview-searchable.js
@@ -2,19 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {TestRunner} from 'test_runner';
-import {NetworkTestRunner} from 'network_test_runner';
-
 import * as Common from 'devtools/core/common/common.js';
 import * as Platform from 'devtools/core/platform/platform.js';
 import * as Network from 'devtools/panels/network/network.js';
 import * as SourceFrame from 'devtools/ui/legacy/components/source_frame/source_frame.js';
 import * as UI from 'devtools/ui/legacy/legacy.js';
+import {NetworkTestRunner} from 'network_test_runner';
+import {TestRunner} from 'test_runner';
 
 (async function() {
-  TestRunner.addResult(`Tests that resources with JSON MIME types are previewed with the JSON viewer.\n`);
+  TestRunner.addResult(
+      `Tests that resources with JSON MIME types are previewed with the JSON viewer.\n`);
   await TestRunner.showPanel('network');
 
+  /**
+   * @param {UI.SearchableView.SearchableView} view
+   */
   async function testSearches(view, searches) {
     await new Promise(resolve => setTimeout(resolve, 0));
     for (var search of searches) {
@@ -22,17 +25,20 @@ import * as UI from 'devtools/ui/legacy/legacy.js';
       view.regexButton.toggled = false;
       view.caseSensitiveButton.toggled = false;
       view.showSearchField();
+      await new Promise(r => requestAnimationFrame(r));
       TestRunner.addResult('Should have found and highlighted all: ' + search);
 
-      var foundItems = view.element.childTextNodes()
-                           .filter(node => node.parentElement.classList.contains('highlighted-search-result'))
-                           .map(node => node.parentElement);
-      TestRunner.addResult('Normal search found ' + foundItems.length + ' results in dom.');
+      const searchResults = view.contentElement.querySelector('.search-results-matches')?.textContent?.match(/^(\d+) of (\d+)$/);
+      TestRunner.addResult(
+          'Normal search found ' + searchResults?.[2] + ' results in dom.');
 
-      foundItems = view.element.childTextNodes()
-                       .filter(node => node.parentElement.classList.contains('cm-search-highlight-start'))
+      const foundItems = view.element.childTextNodes()
+                       .filter(
+                           node => node.parentElement.classList.contains(
+                               'cm-search-highlight-start'))
                        .map(node => node.parentElement);
-      TestRunner.addResult('CodeMirror search found ' + foundItems.length + ' results in dom.');
+      TestRunner.addResult(
+          'CodeMirror search found ' + foundItems.length + ' results in dom.');
       TestRunner.addResult('');
     }
   }
@@ -49,24 +55,28 @@ import * as UI from 'devtools/ui/legacy/legacy.js';
     if (isSearchable)
       compontentView = searchableView.searchProvider;
 
-    if (compontentView instanceof SourceFrame.ResourceSourceFrame.ResourceSourceFrame) {
+    if (compontentView instanceof
+        SourceFrame.ResourceSourceFrame.ResourceSourceFrame) {
       typeName = 'ResourceSourceFrame';
       compontentView.ensureContentLoaded();
       if (!compontentView.loaded) {
         // try again when content is loaded.
         TestRunner.addSniffer(
-            compontentView, 'setContent', previewViewHandled.bind(this, searches, callback, view));
+            compontentView, 'setContent',
+            previewViewHandled.bind(this, searches, callback, view));
         return;
       }
     } else if (compontentView instanceof SourceFrame.XMLView.XMLView) {
       typeName = 'XMLView';
     } else if (compontentView instanceof SourceFrame.JSONView.JSONView) {
       typeName = 'JSONView';
-    } else if (compontentView instanceof Network.RequestHTMLView.RequestHTMLView) {
+    } else if (
+        compontentView instanceof Network.RequestHTMLView.RequestHTMLView) {
       typeName = 'RequestHTMLView';
     } else if (compontentView instanceof UI.EmptyWidget.EmptyWidget) {
       typeName = 'EmptyWidget';
-    } else if (compontentView instanceof Network.RequestHTMLView.RequestHTMLView) {
+    } else if (
+        compontentView instanceof Network.RequestHTMLView.RequestHTMLView) {
       typeName = 'RequestHTMLView';
     }
 
@@ -82,10 +92,12 @@ import * as UI from 'devtools/ui/legacy/legacy.js';
 
   function trySearches(request, searches, callback) {
     var networkPanel = Network.NetworkPanel.NetworkPanel.instance();
-    TestRunner.addSniffer(Network.RequestPreviewView.RequestPreviewView.prototype, 'doShowPreview', async function() {
-      previewViewHandled(searches, callback, await this.contentViewPromise);
-      networkPanel.hideRequestPanel();
-    });
+    TestRunner.addSniffer(
+        Network.RequestPreviewView.RequestPreviewView.prototype,
+        'doShowPreview', async function() {
+          previewViewHandled(searches, callback, await this.contentViewPromise);
+          networkPanel.hideRequestPanel();
+        });
     networkPanel.onRequestSelected({data: request});
     networkPanel.showRequestPanel();
     var itemView = networkPanel.networkItemView;
@@ -94,8 +106,10 @@ import * as UI from 'devtools/ui/legacy/legacy.js';
 
   function testType(contentType, content, searches, callback) {
     var url = 'data:' + contentType + ',' + encodeURIComponent(content);
+    TestRunner.addResult('URL: ' + url);
     NetworkTestRunner.makeSimpleXHR('GET', url, true, function() {
-      var request = NetworkTestRunner.findRequestsByURLPattern(new RegExp(Platform.StringUtilities.escapeForRegExp(url)))[0];
+      var request = NetworkTestRunner.findRequestsByURLPattern(
+          new RegExp(Platform.StringUtilities.escapeForRegExp(url)))[0];
       request.setResourceType(Common.ResourceType.resourceTypes.Document);
       trySearches(request, searches, callback);
     });
@@ -109,22 +123,31 @@ import * as UI from 'devtools/ui/legacy/legacy.js';
       testType('application/json', '[533,3223]', ['533', '322'], next);
     },
     function jsonSpecialMimeTest(next) {
-      testType('application/vnd.document+json', '{"foo0foo": 123}', ['foo'], next);
+      testType(
+          'application/vnd.document+json', '{"foo0foo": 123}', ['foo'], next);
     },
     function xmlMultipleSearchTest(next) {
-      testType('text/xml', '<bar><foo/>test</bar>', ['bar', 'foo', 'bar', 'test'], next);
+      testType(
+          'text/xml', '<bar><foo/>test</bar>', ['bar', 'foo', 'bar', 'test'],
+          next);
     },
     function xmlSingleSearchTest(next) {
       testType('text/xml', '<bar></bar>', ['bar'], next);
     },
     function xmlCommentSearchTest(next) {
-      testType('text/xml', '<bar><!-- TEST --></bar>', ['TEST', '/bar', 'bar'], next);
+      testType(
+          'text/xml', '<bar><!-- TEST --></bar>', ['TEST', '/bar', 'bar'],
+          next);
     },
     function xmlCDATASearchTest(next) {
-      testType('text/xml', '<a><![CDATA[GGG]]><g tee="gee">tee</g></a>', ['GGG', 'tee', 'CDATA'], next);
+      testType(
+          'text/xml', '<a><![CDATA[GGG]]><g tee="gee">tee</g></a>',
+          ['GGG', 'tee', 'CDATA'], next);
     },
     function xmlMimeTypeJsonTest(next) {
-      testType('text/xml', '{"foo0": "barr", "barr": "fooo"}', ['fooo', 'bar'], next);
+      testType(
+          'text/xml', '{"foo0": "barr", "barr": "fooo"}', ['fooo', 'bar'],
+          next);
     }
   ]);
 })();
