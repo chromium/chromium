dea250071dc98e72638438c5ac0db739e62f66c4
Michael Tatarski <mtatarski@google.com>
2025-09-04 10:45:39 -0700

    [OSmosis] Add integration test for bookmark import via TargetService gRPC
    This change introduces a new test case in `TargetServiceGrpcTest` to
    verify the end-to-end bookmark import process. It sets up a real
    `BookmarkModel`, creates a temporary file containing a Netscape bookmark
    file, and uses a `ParcelFileDescriptor` to pass the file content via
    gRPC metadata. The test then asserts that the bookmarks are successfully
    imported into the `BookmarkModel`. New dependencies are added to support
    this integration test.
    
    Bug: 436171659
    Change-Id: I48b30065e1bbd15200ad00bc6fb8a1877e4aae5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6904488
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1510995}

--- chrome/android/modules/on_demand/javatests/src/org/chromium/chrome/browser/data_import/TargetServiceGrpcTest.java
+++ chrome/android/modules/on_demand/javatests/src/org/chromium/chrome/browser/data_import/TargetServiceGrpcTest.java
@@ -8,64 +8,81 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import static org.chromium.base.ThreadUtils.runOnUiThreadBlocking;
+
+import android.os.ParcelFileDescriptor;
+
+import androidx.test.filters.MediumTest;
 import androidx.test.filters.SmallTest;
 
 import com.google.protobuf.ByteString;
 
 import io.grpc.ManagedChannel;
+import io.grpc.Metadata;
 import io.grpc.ServerInterceptors;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
+import io.grpc.stub.MetadataUtils;
 import io.grpc.testing.GrpcCleanupRule;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Features;
+import org.chromium.base.test.util.UrlUtils;
+import org.chromium.chrome.browser.bookmarks.BookmarkModel;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
+import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
+import org.chromium.chrome.browser.profiles.ProfileManager;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.components.bookmarks.BookmarkId;
+import org.chromium.url.GURL;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
 
 /**
- * Tests for {@link DataImporterServiceImpl}. These tests use an in-process gRPC server to test the
- * service implementation, including interceptors.
+ * Integration tests for the {@link TargetService} gRPC service.
+ *
+ * <p>This test class sets up an in-process gRPC server with the {@link TargetService} and tests its
+ * RPCs.
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @Batch(Batch.PER_CLASS)
-// TODO(crbug.com/436171659): Also add a simple test with the feature disabled, which will just
-// verify that onCreate and onBind don't do anything
 @Features.EnableFeatures(ChromeFeatureList.ANDROID_DATA_IMPORTER_SERVICE)
 public class TargetServiceGrpcTest {
 
-    @Rule public final GrpcCleanupRule mGrpcCleanup = new GrpcCleanupRule();
+    @Rule public GrpcCleanupRule mGrpcCleanup = new GrpcCleanupRule();
 
-    @Mock private DataImporterBridge mBridge;
+    private static final ByteString SESSION_ID = ByteString.copyFromUtf8("session_id");
 
-    private TargetServiceGrpc.TargetServiceBlockingStub mStub;
     private TargetService mTargetService;
-    private static final ByteString SESSION_ID = ByteString.copyFromUtf8("test_session_id");
+    private TargetServiceGrpc.TargetServiceBlockingStub mStub;
 
     @Before
     public void setUp() throws IOException {
-        MockitoAnnotations.initMocks(this);
-        mTargetService = new TargetService();
-        mTargetService.mBridge = mBridge;
-
+        runOnUiThreadBlocking(
+                () -> ChromeBrowserInitializer.getInstance().handleSynchronousStartup());
         String serverName = InProcessServerBuilder.generateName();
+        mTargetService = new TargetService();
+
         mGrpcCleanup.register(
                 InProcessServerBuilder.forName(serverName)
-                        .directExecutor()
                         .addService(
                                 ServerInterceptors.intercept(
                                         mTargetService,
                                         new DataImporterServiceImpl
                                                 .ParcelableMetadataInterceptor()))
+                        .directExecutor()
                         .build()
                         .start());
 
@@ -78,7 +95,7 @@ public class TargetServiceGrpcTest {
 
     @Test
     @SmallTest
-    public void testHandshake() {
+    public void testHandshake_supportedType() {
         TargetHandshakeRequest request =
                 TargetHandshakeRequest.newBuilder()
                         .setItemType(SystemAppApiItemType.SYSTEM_APP_API_ITEM_TYPE_BROWSER_DATA)
@@ -99,5 +116,109 @@ public class TargetServiceGrpcTest {
                         .build();
         TargetHandshakeResponse response = mStub.handshake(request);
         assertFalse(response.getSupported());
+    }
+
+    @Test
+    @MediumTest
+    public void testImportBookmarks_Basic() throws Exception {
+        String bookmarksFilePath =
+                UrlUtils.getTestFilePath("android/bookmarks/Valid_entries.html");
+        byte[] bookmarksContent = Files.readAllBytes(Paths.get(bookmarksFilePath));
+
+        ParcelFileDescriptor[] pipe = ParcelFileDescriptor.createPipe();
+        ParcelFileDescriptor readSide = pipe[0];
+
+        // The write side of the pipe will be closed automatically by the AutoCloseOutputStream.
+        try (FileOutputStream fos = new ParcelFileDescriptor.AutoCloseOutputStream(pipe[1])) {
+            fos.write(bookmarksContent);
+        }
+
+        Metadata headers = new Metadata();
+        headers.put(
+                DataImporterServiceImpl.ParcelableMetadataInterceptor.PFD_METADATA_KEY, readSide);
+
+        BrowserFileMetadata fileMetadata =
+                BrowserFileMetadata.newBuilder()
+                        .setFileType(BrowserFileType.BROWSER_FILE_TYPE_BOOKMARKS)
+                        .build();
+        ImportItemRequest request =
+                ImportItemRequest.newBuilder()
+                        .setItemType(SystemAppApiItemType.SYSTEM_APP_API_ITEM_TYPE_BROWSER_DATA)
+                        .setSessionId(SESSION_ID)
+                        .setFileMetadata(
+                                Proto3Any.newBuilder().setValue(fileMetadata.toByteString()))
+                        .build();
+
+        TargetServiceGrpc.TargetServiceBlockingStub stubWithHeaders =
+                mStub.withInterceptors(MetadataUtils.newAttachHeadersInterceptor(headers));
+        stubWithHeaders.importItem(request);
+
+        ImportItemsDoneRequest doneRequest =
+                ImportItemsDoneRequest.newBuilder()
+                        .setItemType(SystemAppApiItemType.SYSTEM_APP_API_ITEM_TYPE_BROWSER_DATA)
+                        .setSessionId(SESSION_ID)
+                        .setStatus(ImportItemsDoneRequest.CompleteStatus.COMPLETE_STATUS_SUCCESS)
+                        .build();
+        ImportItemsDoneResponse doneResponse = mStub.importItemsDone(doneRequest);
+        assertEquals(
+                /* amount of successful imported files */ 1, doneResponse.getSuccessItemCount());
+
+        final BookmarkModel bookmarkModel = waitForBookmarkModelLoaded();
+        runOnUiThreadBlocking(
+                () -> {
+                    GURL url = new GURL("https://www.google.com/");
+                    assertTrue("Bookmark should be present", bookmarkModel.isBookmarked(url));
+                    List<BookmarkId> bookmarkIds = bookmarkModel.searchBookmarks(url.getSpec(), 2);
+                    assertEquals("Exactly one bookmark should be found", 1, bookmarkIds.size());
+                    BookmarkId bookmarkId = bookmarkIds.get(0);
+                    assertEquals(
+                            "Bookmark title should match",
+                            "Google",
+                            bookmarkModel.getBookmarkById(bookmarkId).getTitle());
+
+                    url = new GURL("https://login.live.com/");
+                    assertTrue("Bookmark should be present", bookmarkModel.isBookmarked(url));
+                    bookmarkIds = bookmarkModel.searchBookmarks(url.getSpec(), 2);
+                    assertEquals("Exactly one bookmark should be found", 1, bookmarkIds.size());
+                    bookmarkId = bookmarkIds.get(0);
+                    assertEquals(
+                            "Bookmark title should match",
+                            "Outlook",
+                            bookmarkModel.getBookmarkById(bookmarkId).getTitle());
+
+                    url = new GURL("http://www.speedtest.net/");
+                    assertTrue("Bookmark should be present", bookmarkModel.isBookmarked(url));
+                    bookmarkIds = bookmarkModel.searchBookmarks(url.getSpec(), 2);
+                    assertEquals("Exactly one bookmark should be found", 1, bookmarkIds.size());
+                    bookmarkId = bookmarkIds.get(0);
+                    assertEquals(
+                            "Bookmark title should match",
+                            "Speed Test",
+                            bookmarkModel.getBookmarkById(bookmarkId).getTitle());
+                });
+    }
+
+    /**
+     * Waits until the bookmark model is loaded, i.e. until {@link
+     * BookmarkModel#isBookmarkModelLoaded()} is true.
+     */
+    public static BookmarkModel waitForBookmarkModelLoaded() throws TimeoutException {
+        final CallbackHelper loadedCallback = new CallbackHelper();
+        final BookmarkModel bookmarkModel =
+                ThreadUtils.runOnUiThreadBlocking(
+                        () -> {
+                            BookmarkModel model =
+                                    BookmarkModel.getForProfile(
+                                            ProfileManager.getLastUsedRegularProfile());
+                            if (model.isBookmarkModelLoaded()) {
+                                loadedCallback.notifyCalled();
+                            } else {
+                                model.finishLoadingBookmarkModel(loadedCallback::notifyCalled);
+                            }
+                            return model;
+                        });
+
+        loadedCallback.waitForCallback(0);
+        return bookmarkModel;
     }
 }
