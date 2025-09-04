c365a2aa0338b529114dc5daeb025a7a97eb93c2
Evan Stade <evanstade@microsoft.com>
2025-09-04 08:30:39 -0700

    IDB: fix errors in decodeidbkeypath fuzzer wrt old encoding format
    Fixed: 436990308,436979575
    Change-Id: I58c32d5fcf71fc680ecc6cf854ed3f9c0004c8e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6879690
    Commit-Queue: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Gabriel Brito <gabrielbrito@microsoft.com>
    Auto-Submit: Evan Stade <evanstade@microsoft.com>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510928}

--- content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkeypath_fuzzer.cc
+++ content/browser/indexed_db/indexed_db_leveldb_coding_decodeidbkeypath_fuzzer.cc
@@ -9,6 +9,7 @@
 #include <tuple>
 
 #include "base/check.h"
+#include "components/services/storage/indexed_db/scopes/varint_coding.h"
 #include "content/browser/indexed_db/indexed_db_leveldb_coding.h"
 #include "third_party/blink/public/common/indexeddb/indexeddb_key_path.h"
 
@@ -26,11 +27,19 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
 
     // There is an old style of encoding where the type is not encoded, and the
     // keypath is just a string. In this case, when we re-encode, the output
-    // *will* encode the type, and this format takes 6 extra bytes.
-    if (input.size() >= 3 && input[0] != 0 && input[1] != 0) {
+    // *will* encode the type, and this format takes some extra bytes.
+    if (input.size() < 3 || input[0] != 0 || input[1] != 0) {
       CHECK_EQ(indexed_db_key_path.type(),
                blink::mojom::IDBKeyPathType::String);
-      CHECK_EQ(result.size(), input.size() + 6);
+
+      // The number of extra bytes is:
+      // * 2 for "TypeCodedByte"
+      // * 1 for the type (in this case, string)
+      // * N for the varint encoding of the string's length
+      std::string encoded_varint;
+      content::indexed_db::EncodeVarInt(indexed_db_key_path.string().size(),
+                                        &encoded_varint);
+      CHECK_EQ(result.size(), input.size() + 3 + encoded_varint.size());
 
       std::string_view reencoded(result);
       blink::IndexedDBKeyPath redecoded;
