0fb44b6e516336f55ab2e2e27e97f2af216ce182
Ewann Pelle <ewannpv@chromium.org>
2025-09-04 04:22:03 -0700

    [iOS] Replaces MDCSnackbarMessage in Price Notifications
    Bug: 438409021, 436248990
    Change-Id: I110d9c450ed287d04f8256eaa8e3a4d254bbcd54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6911126
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1510828}

--- ios/chrome/browser/price_notifications/ui_bundled/price_notifications_table_view_controller.mm
+++ ios/chrome/browser/price_notifications/ui_bundled/price_notifications_table_view_controller.mm
@@ -4,8 +4,6 @@
 
 #import "ios/chrome/browser/price_notifications/ui_bundled/price_notifications_table_view_controller.h"
 
-#import <MaterialComponents/MaterialSnackbar.h>
-
 #import "base/apple/foundation_util.h"
 #import "ios/chrome/browser/net/model/crurl.h"
 #import "ios/chrome/browser/price_notifications/ui_bundled/cells/price_notifications_image_container_view.h"
@@ -274,10 +272,10 @@ const char kBookmarksSettingsURL[] = "settings://open_bookmarks";
 
   NSString* messageText = l10n_util::GetNSString(
       IDS_IOS_PRICE_NOTIFICATIONS_PRICE_TRACK_MENU_ITEM_STOP_TRACKING_SNACKBAR);
-  MDCSnackbarMessage* message = CreateSnackbarMessage(messageText);
+  SnackbarMessage* message = CreateCustomSnackbarMessage(messageText);
   [self.snackbarCommandsHandler
-      showSnackbarMessage:message
-           withHapticType:UINotificationFeedbackTypeSuccess];
+      showCustomSnackbarMessage:message
+                 withHapticType:UINotificationFeedbackTypeSuccess];
 
   if (!self.viewIfLoaded.window) {
     return;
