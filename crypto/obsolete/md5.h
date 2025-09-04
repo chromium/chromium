0679ae153612a917748669507d2021336ab666d4
Stacy Gaikovaia <gaiko@google.com>
2025-09-04 14:35:07 -0700

    blink/renderer/core: Stop using base/hash/md5
    Chrome wants to migrate away from use of MD5 but migrating this
    particular use case is not possible because it has to match existing
    stored data:
    https://source.chromium.org/chromium/chromium/src/+/main:base/metrics/metrics_hashes.cc;l=32;drc=eacc76afbe1d2a055571fc8f66ab67d17530956c
    
    Move implementation to crypto/obsolete/md5.h to control for future uses
    of md5.
    
    Bug: 406729261
    Change-Id: Ia048916a046c9a4026d273983c1f85fb04510cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6912624
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Stacy Gaikovaia <gaiko@google.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511169}

--- crypto/obsolete/md5.h
+++ crypto/obsolete/md5.h
@@ -32,6 +32,10 @@ namespace autofill {
 crypto::obsolete::Md5 MakeMd5HasherForPasswordRequirementsSpec();
 }
 
+namespace blink {
+uint32_t MD5Hash32ForBackgroundTracingHelper(std::string_view string);
+}
+
 namespace bookmarks {
 class BookmarkCodec;
 }  // namespace bookmarks
@@ -120,6 +124,8 @@ class CRYPTO_EXPORT Md5 {
   friend Md5 extensions::image_writer::MakeMd5HasherForImageWriter();
   friend Md5 cachetool::MakeMd5HasherForCachetools();
   friend std::string remoting::GetHostHash();
+  friend uint32_t blink::MD5Hash32ForBackgroundTracingHelper(
+      std::string_view string);
 
   // TODO(b/298652869): get rid of these.
   friend Md5 ash::printing::MakeMd5HasherForPrinterConfigurer();
