74b1027ea79be0e04e6afd1a1bf0e576ed74d85f
Tim Judkins <tjudkins@chromium.org>
2025-09-05 11:38:18 -0700

    [Extensions WebIDL] Convert virtualKeyboard to WebIDL
    This conversion was largely done using a detailed description of the
    conversion process passed to Gemini CLI. To double check this work, the
    file has also been copied into the converted schemas test to verify no
    functional difference in output.
    
    Bug: 436624834
    Change-Id: I9b83057bc59e131f68c61fc319ac8c5355b1cda8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6913352
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511761}

--- tools/json_schema_compiler/web_idl_diff_tool_test.py
+++ tools/json_schema_compiler/web_idl_diff_tool_test.py
@@ -25,6 +25,7 @@ class WebIdlDiffToolTest(unittest.TestCase):
         ('audio.idl', 'audio.webidl'),
         ('cec_private.idl', 'cec_private.webidl'),
         ('diagnostics.idl', 'diagnostics.webidl'),
+        ('virtual_keyboard.idl', 'virtual_keyboard.webidl'),
     ]
     # LoadAndReturnUnifiedDiff expects file paths relative to the repo root.
     converted_schema_path = 'tools/json_schema_compiler/test/converted_schemas/'
