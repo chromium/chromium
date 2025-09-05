1e5c2491fd80c3b4351b7dc3662c614492ff6742
Gary Klassen <gklassen@chromium.org>
2025-09-05 12:56:39 -0700

    Handle meta tags with null `name` attribute in `FrameMetadataObserverRegistry`.
    (cherry picked from commit 00411e9a5565f9c5eb8d04fc8edfa3c4b97161a1)
    
    Fixed: 442885719
    Change-Id: I5653f6bccc34d12c99aa6e485e0739600476417d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6912424
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1510464}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917517
    Cr-Commit-Position: refs/branch-heads/7390@{#237}
    Cr-Branched-From: d481efce5eb300acbb896686676ebd0352a6f1db-refs/heads/main@{#1509326}

--- third_party/blink/renderer/modules/content_extraction/frame_metadata_observer_registry_unittest.cc
+++ third_party/blink/renderer/modules/content_extraction/frame_metadata_observer_registry_unittest.cc
@@ -323,6 +323,28 @@ TEST_F(FrameMetadataObserverRegistryTest, MetaTagsUpdated) {
   EXPECT_EQ(meta_tags3[0]->content, "testing");
 }
 
+TEST_F(FrameMetadataObserverRegistryTest, MetaTagsWithNamelessTag) {
+  LoadHTML(R"HTML(
+    <head>
+      <meta charset="UTF-8">
+      <meta name="author" content="Gary">
+    </head>
+    <body></body>
+  )HTML");
+  BindRegistry();
+
+  MockMetaTagsObserver observer;
+  Vector<String> names_to_observe;
+  names_to_observe.push_back("author");
+
+  registry_->AddMetaTagsObserver(names_to_observe,
+                                 observer.BindNewPipeAndPassRemote());
+  test::RunPendingTasks();
+
+  ASSERT_TRUE(observer.future().IsReady());
+  VerifyAuthorMetaTag(observer.future().Take());
+}
+
 // Re-enable this test once we support observing head elements that are added
 // dynamically.
 TEST_F(FrameMetadataObserverRegistryTest, DISABLED_MetaTagsAddedWithHead) {
