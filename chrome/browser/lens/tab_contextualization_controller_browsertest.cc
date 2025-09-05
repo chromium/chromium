e4a3cd9fd9bf6dd7f18127f935171685a3dcda8e
Ananya <ananyaseelam@google.com>
2025-09-05 15:21:32 -0700

    [context-input] Implement tab eligibility using APC
    This CL updates the tab eligibility based on the annotated page contents
    of the tab that the tab feature is associated with.
    
    This CL also adds a browser test suite for the tab contextualization
    controller.
    
    Bug: 439597165
    Change-Id: I70507c887f56a056c6d94ab121c3ad8d3d2bec46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915399
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511904}

---
+++ chrome/browser/lens/tab_contextualization_controller_browsertest.cc
@@ -0,0 +1,78 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/lens/tab_contextualization_controller.h"
+
+#include "base/test/bind.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
+#include "net/dns/mock_host_resolver.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace lens {
+
+class TabContextualizationControllerBrowserTest : public InProcessBrowserTest {
+ public:
+  TabContextualizationControllerBrowserTest() = default;
+  ~TabContextualizationControllerBrowserTest() override = default;
+
+  void SetUpOnMainThread() override {
+    host_resolver()->AddRule("*", "127.0.0.1");
+    ASSERT_TRUE(embedded_test_server()->Start());
+    InProcessBrowserTest::SetUpOnMainThread();
+  }
+
+ protected:
+  TabContextualizationController* GetTabContextualizationController() {
+    tabs::TabInterface* tab = tabs::TabInterface::GetFromContents(
+        browser()->tab_strip_model()->GetWebContentsAt(0));
+    return TabContextualizationController::From(tab);
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(TabContextualizationControllerBrowserTest,
+                       EligibilityIsTrueForEligiblePage) {
+  auto* controller = GetTabContextualizationController();
+
+  GURL url(embedded_test_server()->GetURL("/empty.html"));
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+
+  base::RunLoop run_loop;
+  controller->UpdatePageContextEligibility(
+      base::BindOnce(&TabContextualizationController::OnEligibilityChecked,
+                     base::Unretained(controller))
+          .Then(run_loop.QuitClosure()));
+  run_loop.Run();
+
+  EXPECT_TRUE(controller->GetCurrentPageContextEligibility());
+}
+
+IN_PROC_BROWSER_TEST_F(TabContextualizationControllerBrowserTest,
+                       EligibilityIsFalseForIneligiblePage) {
+  auto* controller = GetTabContextualizationController();
+
+  GURL url(embedded_test_server()->GetURL("/empty.html"));
+  ASSERT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+
+  base::RunLoop run_loop;
+  controller->UpdatePageContextEligibility(
+      base::BindOnce(&TabContextualizationController::OnEligibilityChecked,
+                     base::Unretained(controller))
+          .Then(run_loop.QuitClosure()));
+  run_loop.Run();
+
+  controller->OnEligibilityChecked(false);
+
+  EXPECT_FALSE(controller->GetCurrentPageContextEligibility());
+}
+
+}  // namespace lens
