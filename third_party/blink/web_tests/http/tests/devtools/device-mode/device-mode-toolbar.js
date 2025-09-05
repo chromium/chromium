d02960bcf1809e4069e962289dfe4e49a306748a
Jack Franklin <jacktfranklin@chromium.org>
2025-09-05 03:38:18 -0700

    Enable DevTools device mode tests
    They were disabled in crrev.com/c/6905654 to allow for a DevTools CL to
    land. This CL updates them and enables the tests.
    
    Bug: 442514656
    Change-Id: I68b86d64ade99901331ab48fcc65f70976ecdaa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6918779
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511433}

--- third_party/blink/web_tests/http/tests/devtools/device-mode/device-mode-toolbar.js
+++ third_party/blink/web_tests/http/tests/devtools/device-mode/device-mode-toolbar.js
@@ -7,7 +7,7 @@ import {DeviceModeTestRunner} from 'device_mode_test_runner';
 
 import * as Emulation from 'devtools/panels/emulation/emulation.js';
 import * as EmulationModel from 'devtools/models/emulation/emulation.js';
-import * as UIModule from 'devtools/ui/legacy/legacy.js';
+import * as Geometry from 'devtools/models/geometry/geometry.js'
 
 (async function() {
   TestRunner.addResult(`Test toolbar state when switching modes.\n`);
@@ -16,7 +16,7 @@ import * as UIModule from 'devtools/ui/legacy/legacy.js';
   var view = new Emulation.DeviceModeView.DeviceModeView();
   var toolbar = view.toolbar;
   var model = view.model;
-  var viewportSize = new UIModule.Geometry.Size(800, 600);
+  var viewportSize = new Geometry.Size(800, 600);
   model.setAvailableSize(viewportSize, viewportSize);
 
   // Check that default model has type None.
