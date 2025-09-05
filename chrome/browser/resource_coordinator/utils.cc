f89c37c8c1b2da5ae919a7aabe4831feab888b97
Kuo-Hsin Yang <vovoy@google.com>
2025-09-05 08:45:17 -0700

    [resource coordinator] Remove unused utility methods
    Remove SerializeOriginIntoDatabaseKey and
    URLShouldBeStoredInLocalDatabase.
    
    Bug: 394889323
    Change-Id: I6c42514701492c29bc1ad7691c5ed823f32613a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6918617
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Vovo Yang <vovoy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1511636}

--- chrome/browser/resource_coordinator/utils.cc
+++ chrome/browser/resource_coordinator/utils.cc
@@ -4,20 +4,10 @@
 
 #include "chrome/browser/resource_coordinator/utils.h"
 
-#include "base/hash/md5.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/resource_coordinator/resource_coordinator_parts.h"
 
 namespace resource_coordinator {
-
-std::string SerializeOriginIntoDatabaseKey(const url::Origin& origin) {
-  return base::MD5String(origin.host());
-}
-
-bool URLShouldBeStoredInLocalDatabase(const GURL& url) {
-  // Only store information for the HTTP(S) sites for now.
-  return url.SchemeIsHTTPOrHTTPS();
-}
 
 TabLifecycleUnitSource* GetTabLifecycleUnitSource() {
   DCHECK(g_browser_process);
--- chrome/browser/resource_coordinator/utils.h
+++ chrome/browser/resource_coordinator/utils.h
@@ -5,22 +5,9 @@
 #ifndef CHROME_BROWSER_RESOURCE_COORDINATOR_UTILS_H_
 #define CHROME_BROWSER_RESOURCE_COORDINATOR_UTILS_H_
 
-#include <string>
-
-#include "url/gurl.h"
-#include "url/origin.h"
-
 namespace resource_coordinator {
 
 class TabLifecycleUnitSource;
-
-// Serialize an Origin into the representation used by the different databases
-// that need it.
-std::string SerializeOriginIntoDatabaseKey(const url::Origin& origin);
-
-// Indicates if |url| should have an entry in the local site characteristics
-// database.
-bool URLShouldBeStoredInLocalDatabase(const GURL& url);
 
 // Returns the TabLifecycleUnitSource indirectly owned by g_browser_process.
 TabLifecycleUnitSource* GetTabLifecycleUnitSource();
