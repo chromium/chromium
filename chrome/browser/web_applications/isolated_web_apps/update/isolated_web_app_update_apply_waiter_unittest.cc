0875338dd17ea798a2d476c373d9e5753a19c1ad
Dominik Bylica <bylica@google.com>
2025-09-08 06:07:03 -0700

    [IWA] Move isolated_web_app_update_apply_waiter under update dir.
    Bypass-Check-License: moved files
    Bug: 434649255
    Change-Id: If208a93c3ab4d57d41f5e735b12bbb8fbe1ecf80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6909711
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Dominik Bylica <bylica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512341}

--- chrome/browser/web_applications/isolated_web_apps/update/isolated_web_app_update_apply_waiter_unittest.cc
+++ chrome/browser/web_applications/isolated_web_apps/update/isolated_web_app_update_apply_waiter_unittest.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_update_apply_waiter.h"
+#include "chrome/browser/web_applications/isolated_web_apps/update/isolated_web_app_update_apply_waiter.h"
 
 #include "base/functional/callback_helpers.h"
 #include "base/test/mock_callback.h"
