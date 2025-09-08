65af5d192a10f811aca327c20843ab2898dbe1f7
Sylvain Defresne <sdefresne@chromium.org>
2025-09-08 16:21:19 -0700

    [ios] Convert tests to ProfileTestingFactory
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a testing
    factory accepting a ProfileIOS* parameter, so convert tests
    to use this new API.
    
    This convert tests in //ios/chrome/browser/ntp/ui_bundled.
    
    Bug: 436484783
    Change-Id: I27c1e7261aacf8fd79a87fcb004d48c0f0b790f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919351
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512742}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator_unittest.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_coordinator_unittest.mm
@@ -94,7 +94,7 @@ class NewTabPageCoordinatorTest : public PlatformTest {
     test_profile_builder.AddTestingFactory(
         commerce::ShoppingServiceFactory::GetInstance(),
         base::BindRepeating(
-            [](web::BrowserState*) -> std::unique_ptr<KeyedService> {
+            [](ProfileIOS* profile) -> std::unique_ptr<KeyedService> {
               return std::make_unique<commerce::MockShoppingService>();
             }));
     test_profile_builder.AddTestingFactory(
