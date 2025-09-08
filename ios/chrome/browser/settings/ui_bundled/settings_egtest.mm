e8217f7fb158792b51ec9f8db3720550633778ea
Asami Doi <asamidoi@chromium.org>
2025-09-08 08:00:32 -0700

    [ios][gardening] Disable SettingsTestCase tests on simulator
    Bug: 382632442
    Change-Id: I03be4e9204b028e69a354879d1c29e423e1864ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923306
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Owners-Override: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512405}

--- ios/chrome/browser/settings/ui_bundled/settings_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/settings_egtest.mm
@@ -341,14 +341,26 @@ id<GREYMatcher> ClearBrowsingDataCell() {
 
 // Verifies that metrics reporting works properly under possible settings of the
 // preference kMetricsReportingEnabled.
-- (void)testMetricsReporting {
+// TODO(crbug.com/382632442): Test disabled on simulator.
+#if TARGET_OS_SIMULATOR
+#define MAYBE_testMetricsReporting DISABLED_testMetricsReporting
+#else
+#define MAYBE_testMetricsReporting testMetricsReporting
+#endif
+- (void)MAYBE_testMetricsReporting {
   [self assertsMetricsPrefsForService:kMetrics];
 }
 
 // Verifies that crashpad reporting works properly under possible settings of
 // the preference `kMetricsReportingEnabled`.
 // NOTE: crashpad only allows uploading for non-first-launch runs.
-- (void)testCrashpadReporting {
+// TODO(crbug.com/382632442): Test disabled on simulator.
+#if TARGET_OS_SIMULATOR
+#define MAYBE_testCrashpadReporting DISABLED_testCrashpadReporting
+#else
+#define MAYBE_testCrashpadReporting testCrashpadReporting
+#endif
+- (void)MAYBE_testCrashpadReporting {
   [self assertsMetricsPrefsForService:kCrashpad];
 }
 
