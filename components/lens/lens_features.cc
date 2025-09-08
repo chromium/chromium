9fca51596950e56eb772f137aab354183b88070e
Bryan Nguyen <nguyenbryan@google.com>
2025-09-08 16:11:57 -0700

    [lensoverlay] Enable permission bubble alt by default.
    Enabled-by-default-reason: launching
    Bug: 425758321
    Change-Id: I8f8e12d04b2c95eba04258c7147846c9a5d2d813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6926791
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512730}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -122,7 +122,7 @@ BASE_FEATURE(kLensOverlayKeyboardSelection,
 
 BASE_FEATURE(kLensOverlayPermissionBubbleAlt,
              "LensOverlayPermissionBubbleAlt",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensOverlayBackToPage,
              "LensOverlayBackToPage",
