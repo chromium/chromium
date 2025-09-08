b5c6787cc41b51e23c224f976d58dcc290484066
Sylvain Defresne <sdefresne@chromium.org>
2025-09-08 08:27:30 -0700

    [ios] Convert tests to ProfileTestingFactory
    {Refcounted,}ProfileKeyedServiceFactoryIOS exposes a testing
    factory accepting a ProfileIOS* parameter, so convert tests
    to use this new API.
    
    This convert tests in //ios/chrome/browser/shared/coordinator/default_browser_promo.
    
    Bug: 436484783
    Change-Id: I201818ef994527c452143802786f3d48b2998420
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923588
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512430}

--- ios/chrome/browser/shared/coordinator/default_browser_promo/non_modal_default_browser_promo_scheduler_scene_agent_unittest.mm
+++ ios/chrome/browser/shared/coordinator/default_browser_promo/non_modal_default_browser_promo_scheduler_scene_agent_unittest.mm
@@ -54,7 +54,7 @@
 namespace {
 
 std::unique_ptr<KeyedService> BuildFeatureEngagementMockTracker(
-    web::BrowserState* context) {
+    ProfileIOS* profile) {
   return std::make_unique<feature_engagement::test::MockTracker>();
 }
 
