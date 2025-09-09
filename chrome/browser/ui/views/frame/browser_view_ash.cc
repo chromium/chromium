8041a290f7779bdae6b8cab7a5daa2765a56f31d
Takashi Sakamoto <tasak@google.com>
2025-09-08 19:12:11 -0700

    Revert "Use correct container bounds BrowserViewAsh::UpdateWindowRoundedCorners"
    This reverts commit 614f0ee9f3d4c0fea4abc7db08c312e75936a5eb.
    
    Reason for revert: the revert CL: crrev.com/c/6926803 needs to revert this CL.
    
    Bug: b:434571584
    Original change's description:
    > Use correct container bounds BrowserViewAsh::UpdateWindowRoundedCorners
    >
    > When the SideBySide feature is active and dev-tools are hidden, the
    > contents_container bounds are 1 DIP larger than the contents bounds.
    > This difference occurs because, with SideBySide enabled, the contents
    > separator is part of the contents_container.
    >
    > Bug: b:434571584
    > Change-Id: I6d53a2b055879f1e33f438c0ca0427642383bc9f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6923367
    > Reviewed-by: Caroline Rising <corising@chromium.org>
    > Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1512673}
    
    Bug: b:434571584
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1a7b3764fdf8f50b7a826eb773e28fa1d148c36b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6926912
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1512808}

--- chrome/browser/ui/views/frame/browser_view_ash.cc
+++ chrome/browser/ui/views/frame/browser_view_ash.cc
@@ -16,7 +16,6 @@
 #include "chrome/browser/ui/views/sad_tab_view.h"
 #include "chrome/browser/ui/views/side_panel/side_panel.h"
 #include "ui/compositor/layer.h"
-#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
 #include "ui/views/controls/webview/webview.h"
 
@@ -111,9 +110,9 @@ void BrowserViewAsh::UpdateWindowRoundedCorners(
 
   const gfx::Rect contents_bounds =
       GetActiveContentsContainerView()->GetContentsViewBounds();
-  const gfx::Rect container_bounds =
-      GetActiveContentsContainerView()->GetLocalBounds();
-  const bool devtools_showing = contents_bounds != container_bounds;
+  const views::View* container = contents_container();
+
+  const bool devtools_showing = contents_bounds != container->GetLocalBounds();
 
   // With window controls overlay enabled, the web content extends over the
   // entire window height, overlapping the window's top-two rounded corners.
