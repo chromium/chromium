451722b064bc9eecc6bd7a7dc506bf0783d5753d
Ankush Singh <ankushkush@google.com>
2025-09-09 03:56:44 -0700

    Reland "Replace CanSyncFeatureStart usage for profile picker"
    This is a reland of commit 5b73cd312b066f0857027457208ec32d316eac23
    
    The tests were failing out for debug builds and builds with sanitizers
    while waiting for theme sync start notification to be applied because of
    a 3 seconds timer in ProfileCustomizationSyncedThemeWaiter. To resolve
    this without making any major change, the tests issue a theme sync
    started notification before the profile creation flow ends, which allows
    the machinery to detect the presence of a synced theme and avoids the
    timer altogether.
    
    This also reverts the changes to
    ProfileCustomizationBubbleSyncController which will be part of a
    follow-up CL.
    
    Original change's description:
    > Replace CanSyncFeatureStart usage for profile picker
    >
    > Theme is now synced upon sign in and should not be gated behind
    > sync-the-feature. This CL updates this usage for considering the account
    > theme during profile color picker flow.
    >
    > This CL also addresses a bug where an account color theme was not
    > identified as a custom theme.
    >
    > Bug: 433935323,417950948
    > Change-Id: I49f86c5b31896039628d1d6f74651c24dcb994e0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6890111
    > Reviewed-by: David Roger <droger@chromium.org>
    > Commit-Queue: David Roger <droger@chromium.org>
    > Auto-Submit: Ankush Singh <ankushkush@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1509149}
    
    Bug: 433935323,417950948
    Change-Id: I5086a0ef318a5f6c94647469c2ee00529c8e90c0
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_msan_rel_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6907377
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512967}

--- chrome/browser/ui/profiles/profile_customization_synced_theme_waiter.cc
+++ chrome/browser/ui/profiles/profile_customization_synced_theme_waiter.cc
@@ -12,6 +12,7 @@
 #include "chrome/browser/themes/theme_service.h"
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/service/sync_service.h"
+#include "components/sync/service/sync_service_utils.h"
 #include "components/sync/service/sync_user_settings.h"
 
 ProfileCustomizationSyncedThemeWaiter::ProfileCustomizationSyncedThemeWaiter(
@@ -32,9 +33,9 @@ ProfileCustomizationSyncedThemeWaiter::
 // static
 bool ProfileCustomizationSyncedThemeWaiter::CanThemeSyncStart(
     syncer::SyncService* sync_service) {
-  return sync_service && sync_service->CanSyncFeatureStart() &&
-         sync_service->GetUserSettings()->GetSelectedTypes().Has(
-             syncer::UserSelectableType::kThemes);
+  return syncer::GetUploadToGoogleState(sync_service,
+                                        syncer::DataType::THEMES) !=
+         syncer::UploadState::NOT_ACTIVE;
 }
 
 void ProfileCustomizationSyncedThemeWaiter::Run() {
