615c53d6159185ee0c66d1c79c4456a54ab2f469
Roman Arora <romanarora@chromium.org>
2025-09-09 16:13:52 -0700

    [ntp][composebox] Wire add tab context context menu action
    Follow up work will include updating the carousel attachment image
    associated with an attached tab context entry.
    
    Recording: https://drive.google.com/file/d/18hBlEStLTcyfXgu8bkFhAZGo8O1knKsy/view?usp=sharing
    
    Bug: 441346584
    Change-Id: Ia9a536eb9ebf60bf8ced0e98827a7fc31c408273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6915963
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1513398}

--- chrome/test/data/webui/cr_components/composebox/context_menu_entrypoint_test.ts
+++ chrome/test/data/webui/cr_components/composebox/context_menu_entrypoint_test.ts
@@ -2,21 +2,47 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {PageCallbackRouter, PageHandlerRemote} from 'chrome://resources/cr_components/composebox/composebox.mojom-webui.js';
+import {ComposeboxProxyImpl} from 'chrome://resources/cr_components/composebox/composebox_proxy.js';
 import {ContextMenuEntrypointElement} from 'chrome://resources/cr_components/composebox/context_menu_entrypoint.js';
+import {PageCallbackRouter as SearchboxPageCallbackRouter, PageHandlerRemote as SearchboxPageHandlerRemote} from 'chrome://resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
 import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {TestMock} from 'chrome://webui-test/test_mock.js';
 import {$$, eventToPromise, microtasksFinished} from 'chrome://webui-test/test_util.js';
+
+type Constructor<T> = new (...args: any[]) => T;
+type Installer<T> = (instance: T) => void;
+
+export function installMock<T extends object>(
+    clazz: Constructor<T>, installer?: Installer<T>): TestMock<T> {
+  installer = installer ||
+      (clazz as unknown as {setInstance: Installer<T>}).setInstance;
+  const mock = TestMock.fromClass(clazz);
+  installer(mock);
+  return mock;
+}
 
 suite('ContextMenuEntrypoint', () => {
   let entrypoint: ContextMenuEntrypointElement;
+  let handler: TestMock<PageHandlerRemote>;
 
   setup(async () => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
+
+    handler = installMock(
+        PageHandlerRemote,
+        mock => ComposeboxProxyImpl.setInstance(new ComposeboxProxyImpl(
+            mock, new PageCallbackRouter(), new SearchboxPageHandlerRemote(),
+            new SearchboxPageCallbackRouter())));
+
     entrypoint = new ContextMenuEntrypointElement();
     document.body.appendChild(entrypoint);
     await microtasksFinished();
   });
 
   test('clicking entrypoint shows context menu', async () => {
+    handler.setResultFor('getTabs', Promise.resolve([]));
+
     // Act.
     entrypoint.$.entrypoint.click();
     await microtasksFinished();
@@ -34,6 +60,7 @@ suite('ContextMenuEntrypoint', () => {
             `clicking ${selector} propagates ${eventName} before closing menu`,
             async () => {
               // Arrange.
+              handler.setResultFor('getTabs', Promise.resolve([]));
               entrypoint.$.entrypoint.click();
               await microtasksFinished();
               assertTrue(entrypoint.$.menu.open);
@@ -47,6 +74,7 @@ suite('ContextMenuEntrypoint', () => {
 
               // Assert.
               assertTrue(!!eventFired);
+
               assertFalse(entrypoint.$.menu.open);
             });
       });
