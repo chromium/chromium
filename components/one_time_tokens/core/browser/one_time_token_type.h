31711045a26587d45cacebfe04c1a9a96fdb30ea
Dominic Battre <battre@chromium.org>
2025-09-09 01:11:09 -0700

    Introduce one_time_token component
    This CL introduces a basic one_time_token component with the vary basic
    data types of OneTimeToken and OneTimeTokenType.
    
    This is part of a refactoring that moves the one time token fetching
    logic from components/password_manager/core/browser/one_time_passwords/
    into an isolated component/one_time_token/ and prepares it for being
    extended in functionality.
    
    Bug: 415273270
    Change-Id: I5fa94cd5ec56ffd53e2f796f3cc324030888508f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6919147
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1512900}

---
+++ components/one_time_tokens/core/browser/one_time_token_type.h
@@ -0,0 +1,15 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ONE_TIME_TOKENS_CORE_BROWSER_ONE_TIME_TOKEN_TYPE_H_
+#define COMPONENTS_ONE_TIME_TOKENS_CORE_BROWSER_ONE_TIME_TOKEN_TYPE_H_
+
+namespace one_time_tokens {
+
+// Do not change or reuse values.
+enum class OneTimeTokenType { kSmsOtp = 0, kMaxValue = kSmsOtp };
+
+}  // namespace one_time_tokens
+
+#endif  // COMPONENTS_ONE_TIME_TOKENS_CORE_BROWSER_ONE_TIME_TOKEN_TYPE_H_
