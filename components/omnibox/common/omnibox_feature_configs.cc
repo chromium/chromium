3924065da7c102fe414720db6f53df60f0e76916
Juan Mojica <juanmojica@google.com>
2025-09-10 10:36:23 -0700

    Enable feature to allow contextual suggestions fulfilled by Lens.
    This will be controlled via a server-side experiment. Ramp to enabled so
    there is no need for a Finch-coordinated experiment.
    
    Bug: 441134410
    Enabled-by-default-reason: will serve as a client side killswitch for the server side experiment. otherwise, no user visible changes
    Change-Id: I6631cf6cb05d5e2bbba1f0967d2b437b23ef8f4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6920356
    Reviewed-by: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1513816}

--- components/omnibox/common/omnibox_feature_configs.cc
+++ components/omnibox/common/omnibox_feature_configs.cc
@@ -161,7 +161,7 @@ BASE_FEATURE(ContextualSearch::kOpenLensActionUITweaks,
 
 BASE_FEATURE(ContextualSearch::kSuggestionsFulfilledByLensSupported,
              "SuggestionsFulfilledByLensSupported",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(ContextualSearch::kLoadingSuggestionsAnimation,
              "LoadingSuggestionsAnimation",
