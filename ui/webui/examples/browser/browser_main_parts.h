30571e0cbc389a2c4760efab044b9473bb74b124
Robert Liao <robliao@chromium.org>
2025-09-10 16:54:10 -0700

    Enable kAttachUnownedInnerWebContents in Webshell
    Webshell makes use of inner WebContents codepaths that assert
    kAttachUnownedInnerWebContents is enabled.
    
    Change-Id: Ic00329fc8e55d68e61596c466440bea1f893fa19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6937226
    Auto-Submit: Robert Liao <robliao@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514018}

--- ui/webui/examples/browser/browser_main_parts.h
+++ ui/webui/examples/browser/browser_main_parts.h
@@ -8,8 +8,10 @@
 #include <string>
 
 #include "base/files/scoped_temp_dir.h"
+#include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "content/public/browser/browser_main_parts.h"
+#include "content/public/common/content_features.h"
 
 #if BUILDFLAG(IS_MAC)
 #include "ui/display/screen.h"
@@ -64,6 +66,9 @@ class BrowserMainParts : public content::BrowserMainParts {
                                             const std::u16string& title);
 
   void QuitMessageLoop();
+
+  base::test::ScopedFeatureList scoped_feature_list_{
+      features::kAttachUnownedInnerWebContents};
 
   base::ScopedTempDir temp_dir_;
 #if BUILDFLAG(IS_MAC)
