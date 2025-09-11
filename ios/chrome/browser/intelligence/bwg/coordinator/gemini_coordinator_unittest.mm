6f664781395a39984765bbdf3f85a2d06c0787b8
Joemer Ramos <joemerramos@google.com>
2025-09-11 15:47:48 -0700

    [iOS] Add More Test Coverage for BWGCoordinator
    Adds test coverage for the AI Hub IPH, general promo display, and
    iPad multi-window dismissal
    
    Fixed: 442834367
    Change-Id: I8e1ae3e50d9b8dc38068bab72c46249eca9db0ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6941344
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Joemer Ramos <joemerramos@google.com>
    Cr-Commit-Position: refs/heads/main@{#1514563}

--- ios/chrome/browser/intelligence/bwg/coordinator/gemini_coordinator_unittest.mm
+++ ios/chrome/browser/intelligence/bwg/coordinator/gemini_coordinator_unittest.mm
@@ -17,6 +17,7 @@
 #import "ios/chrome/browser/shared/model/browser/browser_list.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_manager_ios.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
@@ -29,8 +30,14 @@
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
+#import "third_party/ocmock/gtest_support.h"
 
 namespace {
+const CGFloat kPromoMaxImpressionCount = 3;
+
+const std::string kFirstProfileName = "FirstProfile";
+const std::string kSecondProfileName = "SecondProfile";
+
 std::unique_ptr<KeyedService> CreateTestTracker(ProfileIOS* context) {
   return std::make_unique<
       testing::NiceMock<feature_engagement::test::MockTracker>>();
@@ -47,7 +54,7 @@ class GeminiCoordinatorTest : public PlatformTest {
             std::make_unique<FakeAuthenticationServiceDelegate>()));
     builder.AddTestingFactory(feature_engagement::TrackerFactory::GetInstance(),
                               base::BindOnce(&CreateTestTracker));
-    builder.SetName("TestProfile");
+    builder.SetName(kFirstProfileName);
     ProfileIOS* profile =
         profile_manager_.AddProfileWithBuilder(std::move(builder));
 
@@ -57,15 +64,14 @@ class GeminiCoordinatorTest : public PlatformTest {
     TestFullscreenController::CreateForBrowser(browser_.get());
 
     CommandDispatcher* dispatcher = browser_->GetCommandDispatcher();
-    id mock_application_command_handler =
-        OCMProtocolMock(@protocol(BWGCommands));
-    id mock_settings_command_handler = OCMProtocolMock(@protocol(HelpCommands));
+    mock_bwg_command_handler_ = OCMProtocolMock(@protocol(BWGCommands));
+    mock_help_command_handler_ = OCMProtocolMock(@protocol(HelpCommands));
     id mock_application_commands_handler =
         OCMProtocolMock(@protocol(ApplicationCommands));
 
-    [dispatcher startDispatchingToTarget:mock_application_command_handler
+    [dispatcher startDispatchingToTarget:mock_bwg_command_handler_
                              forProtocol:@protocol(BWGCommands)];
-    [dispatcher startDispatchingToTarget:mock_settings_command_handler
+    [dispatcher startDispatchingToTarget:mock_help_command_handler_
                              forProtocol:@protocol(HelpCommands)];
     [dispatcher startDispatchingToTarget:mock_application_commands_handler
                              forProtocol:@protocol(ApplicationCommands)];
@@ -89,13 +95,16 @@ class GeminiCoordinatorTest : public PlatformTest {
   raw_ptr<BrowserList> browser_list_;
   UIViewController* base_view_controller_;
   BWGCoordinator* coordinator_;
+  id mock_help_command_handler_;
+  id mock_bwg_command_handler_;
 };
 
-// Tests starting the coordinator from the AI Hub entry point.
+// Tests fullscreen mode not exiting when promo shows from the AI Hub entry
+// point.
 TEST_F(GeminiCoordinatorTest, FullscreenNotExitedOnAIHubEntryPoint) {
   auto* tracker = static_cast<feature_engagement::test::MockTracker*>(
       feature_engagement::TrackerFactory::GetForProfile(
-          profile_manager_.GetProfileWithName("TestProfile")));
+          profile_manager_.GetProfileWithName(kFirstProfileName)));
 
   TestFullscreenController* controller =
       TestFullscreenController::FromBrowser(browser_.get());
@@ -113,4 +122,120 @@ TEST_F(GeminiCoordinatorTest, FullscreenNotExitedOnAIHubEntryPoint) {
 
   // Check that fullscreen mode is still active.
   EXPECT_EQ(0.0, controller->GetProgress());
+}
+
+// Tests fullscreen mode exiting when promo shows from the promo entry point.
+TEST_F(GeminiCoordinatorTest, FullscreenExitedOnPromoEntryPoint) {
+  auto* tracker = static_cast<feature_engagement::test::MockTracker*>(
+      feature_engagement::TrackerFactory::GetForProfile(
+          profile_manager_.GetProfileWithName(kFirstProfileName)));
+  EXPECT_CALL(*tracker, WouldTriggerHelpUI(testing::Ref(
+                            feature_engagement::kIPHIOSPageActionMenu)))
+      .WillRepeatedly(testing::Return(true));
+
+  TestFullscreenController* controller =
+      TestFullscreenController::FromBrowser(browser_.get());
+  controller->EnterFullscreen();
+  ASSERT_EQ(0.0, controller->GetProgress());
+
+  EXPECT_CALL(
+      *tracker,
+      NotifyEvent(feature_engagement::events::kIOSGeminiPromoFirstCompletion));
+
+  StartCoordinatorWithEntryPoint(bwg::EntryPoint::Promo);
+
+  // Check that fullscreen mode is deactivated.
+  EXPECT_EQ(1.0, controller->GetProgress());
+}
+
+// Tests that the promo doesn't show after the maximum impression count.
+TEST_F(GeminiCoordinatorTest, GeminiPromoNotShown) {
+  ProfileIOS* profile = profile_manager_.GetProfileWithName(kFirstProfileName);
+  PrefService* prefs = profile->GetPrefs();
+  prefs->SetInteger(prefs::kIOSBWGPromoImpressionCount,
+                    kPromoMaxImpressionCount);
+
+  auto* tracker = static_cast<feature_engagement::test::MockTracker*>(
+      feature_engagement::TrackerFactory::GetForProfile(profile));
+
+  // `kIOSGeminiPromoFirstCompletion` should not be notified if the promo is not
+  // shown.
+  EXPECT_CALL(
+      *tracker,
+      NotifyEvent(feature_engagement::events::kIOSGeminiPromoFirstCompletion))
+      .Times(0);
+
+  StartCoordinatorWithEntryPoint(bwg::EntryPoint::Promo);
+
+  // Checks that a promo didn't start and the impression count didn't
+  // increase.
+  EXPECT_EQ(kPromoMaxImpressionCount,
+            prefs->GetInteger(prefs::kIOSBWGPromoImpressionCount));
+}
+
+// Tests AI Hub IPH starts when the user is shown the promo from initially
+// tapping a non AI Hub entry point.
+TEST_F(GeminiCoordinatorTest, AIHubIPHWasTriggered) {
+  OCMExpect([mock_help_command_handler_
+      presentInProductHelpWithType:InProductHelpType::kPageActionMenu]);
+
+  StartCoordinatorWithEntryPoint(bwg::EntryPoint::Promo);
+  [coordinator_ stop];
+
+  EXPECT_OCMOCK_VERIFY(mock_help_command_handler_);
+}
+
+// Tests AI Hub IPH doesn't start when the user is shown the promo from
+// initially tapping the AI Hub entry point.
+TEST_F(GeminiCoordinatorTest, AIHubIPHNotTriggered) {
+  OCMReject([mock_help_command_handler_
+      presentInProductHelpWithType:InProductHelpType::kPageActionMenu]);
+
+  StartCoordinatorWithEntryPoint(bwg::EntryPoint::AIHub);
+  [coordinator_ stop];
+
+  EXPECT_OCMOCK_VERIFY(mock_help_command_handler_);
+}
+
+// Tests that other Gemini floaty instances are dismissed before when starting a
+// new one.
+TEST_F(GeminiCoordinatorTest, DismissOtherWindows) {
+  // Build second profile.
+  TestProfileIOS::Builder second_builder;
+  second_builder.SetName(kSecondProfileName);
+  ProfileIOS* second_profile =
+      profile_manager_.AddProfileWithBuilder(std::move(second_builder));
+  std::unique_ptr<TestBrowser> second_browser_ =
+      std::make_unique<TestBrowser>(second_profile);
+  BrowserListFactory::GetForProfile(second_profile)
+      ->AddBrowser(second_browser_.get());
+
+  id second_bwg_handler = OCMProtocolMock(@protocol(BWGCommands));
+  [second_browser_->GetCommandDispatcher()
+      startDispatchingToTarget:second_bwg_handler
+                   forProtocol:@protocol(BWGCommands)];
+
+  OCMExpect([second_bwg_handler dismissBWGFlowWithCompletion:[OCMArg any]])
+      .andDo(^(NSInvocation* invocation) {
+        __weak ProceduralBlock block;
+        [invocation getArgument:&block atIndex:2];
+        if (block) {
+          block();
+        }
+      });
+
+  StartCoordinatorWithEntryPoint(bwg::EntryPoint::Promo);
+
+  // Emulate starting the floaty from the first window.
+  OCMStub([mock_bwg_command_handler_ dismissBWGFlowWithCompletion:[OCMArg any]])
+      .andDo(^(NSInvocation* invocation) {
+        __weak ProceduralBlock block;
+        [invocation getArgument:&block atIndex:2];
+        if (block) {
+          block();
+        }
+      });
+
+  EXPECT_OCMOCK_VERIFY(mock_bwg_command_handler_);
+  EXPECT_OCMOCK_VERIFY(second_bwg_handler);
 }
