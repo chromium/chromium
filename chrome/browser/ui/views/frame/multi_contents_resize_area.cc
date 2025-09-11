e024155e01df7e9fdcb740dc10283636b4462555
Zoraiz Naeem <zoraiznaeem@chromium.org>
2025-09-11 01:05:51 -0700

    Paint MultiContentsResizeHandle to a texture layer of its own
    In crrev.com/c/6929585, the MultiContentsView will background on layer
    of its own. Due to this, MultiContentsResizeHandle ends up getting a
    lower z-order, even thought MultiContentsResizeHandle is after
    MultiContentsView::BackgroundView in the view hierarchy.
    
    By painting MultiContentsResizeHandle to a layer fixes the z-order
    issue. Additionally it is more efficient than painting a background onto
    the widget's layer.
    
    Bug: b:434571584
    Change-Id: I73b5defff022588c2daf579942de6e85552af51f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6928869
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514141}

--- chrome/browser/ui/views/frame/multi_contents_resize_area.cc
+++ chrome/browser/ui/views/frame/multi_contents_resize_area.cc
@@ -12,6 +12,8 @@
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/color/color_provider.h"
+#include "ui/compositor/layer.h"
+#include "ui/compositor/layer_type.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
@@ -33,6 +35,11 @@ DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(MultiContentsResizeArea,
                                       kMultiContentsResizeAreaElementId);
 
 MultiContentsResizeHandle::MultiContentsResizeHandle() {
+  // Cannot use `SOLID_COLOR_LAYER`, since resize handle has rounded corners,
+  // and applying layer based rounded corners will clip the focus ring.
+  SetPaintToLayer(ui::LAYER_TEXTURED);
+  layer()->SetFillsBoundsOpaquely(false);
+
   SetPreferredSize(gfx::Size(kHandleWidth, kHandleHeight));
   SetCanProcessEventsWithinSubtree(false);
   SetFocusBehavior(FocusBehavior::ALWAYS);
@@ -42,17 +49,13 @@ MultiContentsResizeHandle::MultiContentsResizeHandle() {
       l10n_util::GetStringUTF16(IDS_ACCNAME_SPLIT_TABS_RESIZE));
   SetProperty(views::kElementIdentifierKey,
               kMultiContentsResizeHandleElementId);
+
+  SetBackground(views::CreateRoundedRectBackground(
+      kColorSidePanelHoverResizeAreaHandle, kHandleCornerRadius));
 }
 
 void MultiContentsResizeHandle::UpdateVisibility(bool visible) {
-  if (visible) {
-    const SkColor resize_handle_color =
-        GetColorProvider()->GetColor(kColorSidePanelHoverResizeAreaHandle);
-    SetBackground(views::CreateRoundedRectBackground(resize_handle_color,
-                                                     kHandleCornerRadius));
-  } else {
-    SetBackground(nullptr);
-  }
+  layer()->SetVisible(visible);
 }
 
 void MultiContentsResizeHandle::AddedToWidget() {
