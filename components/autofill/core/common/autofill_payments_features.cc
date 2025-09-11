939eaf15f63ddeebee534b1e9ec0308b9659bc17
Fernando Ramirez <ferny@google.com>
2025-09-10 17:00:41 -0700

    [Card Benefits] Enable kAutofillEnableCardBenefitsForBmo by default
    This CL enables kAutofillEnableCardBenefitsForBmo by default for Chrome
    desktop platforms. It's disabled by default for all other platforms as
    there is an ongoing experiment for Clank.
    
    Enabled-by-default-reason: launching
    Bug: 415116925
    Change-Id: I25f86aa7edfe707eb380573cba6f2d82d47a558c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6933668
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Fernando Ramirez <ferny@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514021}

--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -94,7 +94,13 @@ BASE_FEATURE(kAutofillEnableCardBenefitsForAmericanExpress,
 // UI.
 BASE_FEATURE(kAutofillEnableCardBenefitsForBmo,
              "AutofillEnableCardBenefitsForBmo",
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS)
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#else
              base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
+        // BUILDFLAG(IS_CHROMEOS)
 
 // When enabled, card benefits IPH will be shown in Payments Autofill UI.
 BASE_FEATURE(kAutofillEnableCardBenefitsIph,
