0c6aaf6bc1d718433e99ef9826c87f7d845128fa
Tom Sepez <tsepez@chromium.org>
2025-09-10 17:01:39 -0700

    Remove ipc_message_null_macros.h
    The only two files which include this are already doing nearly all
    this cleanup themselves.
    
    Change-Id: I57dfa23c7c9deee54332dfe625721d7cc972ab25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6934232
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1514022}

--- ipc/param_traits_write_macros.h
+++ ipc/param_traits_write_macros.h
@@ -5,27 +5,28 @@
 #ifndef IPC_PARAM_TRAITS_WRITE_MACROS_H_
 #define IPC_PARAM_TRAITS_WRITE_MACROS_H_
 
-// Null out all the macros that need nulling.
-#include "ipc/ipc_message_null_macros.h"
-
 // Set up so next include will generate write methods.
 #undef IPC_STRUCT_TRAITS_BEGIN
 #undef IPC_STRUCT_TRAITS_MEMBER
 #undef IPC_STRUCT_TRAITS_PARENT
 #undef IPC_STRUCT_TRAITS_END
+#undef IPC_ENUM_TRAITS_VALIDATE
+#undef IPC_MESSAGE_DECL
+
 #define IPC_STRUCT_TRAITS_BEGIN(struct_name) \
   void ParamTraits<struct_name>::Write(base::Pickle* m, const param_type& p) {
 #define IPC_STRUCT_TRAITS_MEMBER(name) WriteParam(m, p.name);
 #define IPC_STRUCT_TRAITS_PARENT(type) ParamTraits<type>::Write(m, p);
 #define IPC_STRUCT_TRAITS_END() }
 
-#undef IPC_ENUM_TRAITS_VALIDATE
 #define IPC_ENUM_TRAITS_VALIDATE(enum_name, validation_expression) \
   void ParamTraits<enum_name>::Write(base::Pickle* m,              \
                                      const param_type& value) {    \
     DCHECK(validation_expression);                                 \
     m->WriteInt(static_cast<int>(value));                          \
   }
+
+#define IPC_MESSAGE_DECL(...)
 
 #endif  // IPC_PARAM_TRAITS_WRITE_MACROS_H_
 
