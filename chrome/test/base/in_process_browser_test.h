36b6f5bd60c1737c71a9a4cfc655524307d2bc87
Titouan Rigoudy <titouan@chromium.org>
2025-09-12 01:35:49 -0700

    Revert "Add missing Browser forward declare to InProcessBrowserTests"
    This reverts commit 6e9870aed2bcad6c865defd20990a051f0b35285.
    
    Reason for revert:
    
      Need to revert base CL https://crrev.com/c/6937629, which is
      failing a lot of tests on the linux ubsan bot.
    
    Original change's description:
    > Add missing Browser forward declare to InProcessBrowserTests
    >
    > Necessary for non-desktop tests that use this fixture.
    >
    > Gardener note: Skipping checks to bring the tree back up.
    >
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: None
    > Change-Id: Ib7555c460c2acd11413ad87e44fcadd4facc0766
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6938946
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1514550}
    
    Bug: 444471181
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    
    Bug: None
    Change-Id: I980a7f9c73367aa4e82353eb3ab8f4ead1ede6ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6939357
    Owners-Override: Titouan Rigoudy <titouan@chromium.org>
    Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1514719}

--- chrome/test/base/in_process_browser_test.h
+++ chrome/test/base/in_process_browser_test.h
@@ -69,7 +69,6 @@ class ScopedLaunchBrowserForTesting;
 }  // namespace ash::full_restore
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
-class Browser;
 class BrowserWindowInterface;
 class PrefService;
 class Profile;
