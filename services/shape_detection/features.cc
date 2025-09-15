17b7756abee74bd835905fe7f80ce87a712ad8b8
Matt Reynolds <mattreynolds@google.com>
2025-09-15 16:58:28 -0700

    shapedetection: Add killswitch for crbug.com/442001297
    Enabled-by-default-reason: killswitch
    Bug: 442001297
    Change-Id: Id084c8745dbe9ed8343a09f3f9574c827a3af4ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944689
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515769}

---
+++ services/shape_detection/features.cc
@@ -0,0 +1,13 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/shape_detection/features.h"
+
+namespace features {
+
+BASE_FEATURE(kBarhopperAztecRefineTransformFallback,
+             "BarhopperAztecRefineTransformFallback",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+}  // namespace features
