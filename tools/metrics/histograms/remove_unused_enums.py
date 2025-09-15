0343e682e21d80ed40db945c5d3948a87b876cc4
Alexei Svitkine <asvitkine@chromium.org>
2025-09-15 10:58:48 -0700

    Update remove_unused_enums.py to support dwa.xml.
    In crrev.com/c/6942774, the changes didn't account for an enum
    referenced in dwa.xml. This correct that and also has some
    improvements to the output of the script.
    
    In a future CL, I plan to move this logic to a separate library
    which we can also use from a presubmit to ensure unused enums
    aren't left behind.
    
    Bug: 445128242
    Change-Id: I467b1133fd3bc4f51e63ec9a057dc94735238e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6949725
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1515608}

--- tools/metrics/histograms/remove_unused_enums.py
+++ tools/metrics/histograms/remove_unused_enums.py
@@ -2,11 +2,7 @@
 # Copyright 2023 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-"""
-Removes unused enums from the monolithic enums.xml file.
-
-Note: This does not handle sharded enums.xml files yet.
-"""
+"""Removes unused enums from enums.xml files."""
 
 import io
 import logging
@@ -23,7 +19,7 @@ import merge_xml
 import path_util
 
 
-def _get_enums_from_histogram_files(files):
+def _get_enums_from_histogram_files(files: list[str]):
   """Finds the names of all referenced enums from the specified XML files."""
   merged = merge_xml.MergeFiles(files)
   histograms, _ = extract_histograms.ExtractHistogramsFromDom(merged)
@@ -37,18 +33,20 @@ def _get_enums_from_histogram_files(files):
   return enums_used_in_file
 
 
-def _get_enums_from_ukm():
-  """Finds enums used by ukm.xml."""
-  with open(histogram_paths.UKM_XML, 'r') as f:
-    document = minidom.parse(f)
+def _get_enums_referenced_by_metric_nodes_in_xml_files(files: list[str]):
+  """Finds enums used by ukm.xml and similar files."""
+  enums_used_in_files = set()
 
-  enums_used_in_file = set()
-  for node in document.getElementsByTagName('metric'):
-    if not 'enum' in node.attributes:
-      continue
-    enums_used_in_file.add(node.attributes['enum'].value)
+  for file_path in files:
+    with open(file_path, 'r') as f:
+      document = minidom.parse(f)
 
-  return enums_used_in_file
+    for node in document.getElementsByTagName('metric'):
+      if not 'enum' in node.attributes:
+        continue
+      enums_used_in_files.add(node.attributes['enum'].value)
+
+  return enums_used_in_files
 
 
 def _remove_enum_nodes_not_in_list(enum_names, filepath):
@@ -70,24 +68,36 @@ def _remove_enum_nodes_not_in_list(enum_names, filepath):
 
 def _remove_unused_enums():
   """Removes unused enums from ALL enums.xml files."""
-  print(f'Reading XML files...')
+  print(f'Reading histogram XML files...')
   enum_names = _get_enums_from_histogram_files(histogram_paths.ALL_XMLS)
   print(f'Found {len(enum_names)} enums from histograms.')
 
-  ukm_enum_names = _get_enums_from_ukm()
-  print(f'Found {len(ukm_enum_names)} enums from ukm.')
+  metric_files = [
+      histogram_paths.UKM_XML,
+      histogram_paths.DWA_XML,
+  ]
+  metric_enum_names = (
+      _get_enums_referenced_by_metric_nodes_in_xml_files(metric_files))
+  print(f'Found {len(metric_enum_names)} enums from ukm and dwa.')
 
-  enum_names.update(ukm_enum_names)
+  enum_names.update(metric_enum_names)
   print(f'Found {len(enum_names)} enums total.')
 
   for enum_file in histogram_paths.ENUMS_XMLS:
     enum_nodes, updated_enum_xml = _remove_enum_nodes_not_in_list(
         enum_names, enum_file)
-    print(f'Removed {len(enum_nodes)} that were not referenced.')
+    if not enum_nodes:
+      print(f'All enums in {enum_file} are referenced')
+      continue
 
-    print(f'Writing updated file: {enum_file}')
+    print(f'Removing {len(enum_nodes)} enum nodes from {enum_file}:')
+    for enum_node in enum_nodes:
+      enum_name = enum_node.attributes['name'].value
+      print(f' - {enum_name}')
+
     with open(enum_file, 'w', encoding='utf-8', newline='') as f:
       f.write(updated_enum_xml)
+    print('File updated.')
 
 
 if __name__ == '__main__':
