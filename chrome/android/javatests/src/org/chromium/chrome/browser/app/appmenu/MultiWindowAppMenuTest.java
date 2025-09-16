8b5199ee8030c21b41dd5c372d79a3855253eb7b
Henrique Nakashima <hnakashima@chromium.org>
2025-09-16 07:38:28 -0700

    [Android] Re-enable #testOpenNewWindow_fromWebPage
    Bug was fixed by:
    https://chromium-review.googlesource.com/c/chromium/src/+/6944844
    
    Bug: 443035606
    Change-Id: I453a9c02168afedb6dc9eeab0f94bc8af64a823f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950458
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516029}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/MultiWindowAppMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/appmenu/MultiWindowAppMenuTest.java
@@ -16,7 +16,6 @@ import org.chromium.base.test.transit.Station;
 import org.chromium.base.test.transit.TransitAsserts;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DoNotBatch;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -42,7 +41,6 @@ public class MultiWindowAppMenuTest {
 
     @Test
     @LargeTest
-    @DisabledTest(message = "Flaky on ci/test-tablet builder crbug.com/443035606")
     public void testOpenNewWindow_fromWebPage() {
         WebPageStation pageInFirstWindow = mCtaTestRule.startOnBlankPage();
         RegularNewTabPageStation pageInSecondWindow =
