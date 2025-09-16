511d2f64c5d658353d3040e1fa95acc98398ec4c
Ahmed Nasr <anasr@google.com>
2025-09-16 11:25:37 -0700

    Add receiver connection state proto to string conversion function.
    Bug: b:442592057
    Change-Id: I92479e9c8ad7f52086211e0bdcbd088836556081
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950687
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516169}

--- chromeos/ash/components/boca/receiver/kiosk_receiver_parser.cc
+++ chromeos/ash/components/boca/receiver/kiosk_receiver_parser.cc
@@ -4,34 +4,54 @@
 
 #include "chromeos/ash/components/boca/receiver/kiosk_receiver_parser.h"
 
+#include <string>
+#include <string_view>
+#include <vector>
+
 #include "chromeos/ash/components/boca/proto/receiver.pb.h"
 #include "chromeos/ash/components/boca/session_api/constants.h"
 #include "google_apis/common/base_requests.h"
 
 namespace ash::boca_receiver {
+namespace {
+
+struct StateStringProtoPair {
+  std::string_view state_string;
+  ::boca::ReceiverConnectionState state_proto;
+};
+
+constexpr std::string_view kReceiverStateUknown = "RECEIVER_STATE_UNKNOWN";
+static constexpr StateStringProtoPair kStateStringProtoPairs[] = {
+    {kReceiverStateUknown,
+     ::boca::ReceiverConnectionState::RECEIVER_CONNECTION_STATE_UNKNOWN},
+    {"START_REQUESTED", ::boca::ReceiverConnectionState::START_REQUESTED},
+    {"STOP_REQUESTED", ::boca::ReceiverConnectionState::STOP_REQUESTED},
+    {"CONNECTING", ::boca::ReceiverConnectionState::CONNECTING},
+    {"CONNECTED", ::boca::ReceiverConnectionState::CONNECTED},
+    {"DISCONNECTED", ::boca::ReceiverConnectionState::DISCONNECTED},
+    {"ERROR", ::boca::ReceiverConnectionState::ERROR},
+};
+
+}  // namespace
+
 ::boca::ReceiverConnectionState ReceiverConnectionStateProtoFromJson(
     const std::string& state) {
-  if (state == "RECEIVER_STATE_UNKNOWN") {
-    return ::boca::ReceiverConnectionState::RECEIVER_CONNECTION_STATE_UNKNOWN;
-  }
-  if (state == "START_REQUESTED") {
-    return ::boca::ReceiverConnectionState::START_REQUESTED;
-  }
-  if (state == "STOP_REQUESTED") {
-    return ::boca::ReceiverConnectionState::STOP_REQUESTED;
-  }
-  if (state == "CONNECTING") {
-    return ::boca::ReceiverConnectionState::CONNECTING;
-  }
-  if (state == "CONNECTED") {
-    return ::boca::ReceiverConnectionState::CONNECTED;
-  }
-  if (state == "DISCONNECTED") {
-    return ::boca::ReceiverConnectionState::DISCONNECTED;
-  }
-  if (state == "ERROR") {
-    return ::boca::ReceiverConnectionState::ERROR;
+  for (const StateStringProtoPair& pair : kStateStringProtoPairs) {
+    if (pair.state_string == state) {
+      return pair.state_proto;
+    }
   }
   return ::boca::ReceiverConnectionState::RECEIVER_CONNECTION_STATE_UNKNOWN;
 }
+
+std::string ReceiverConnectionStateStringFromProto(
+    ::boca::ReceiverConnectionState state) {
+  for (const StateStringProtoPair& pair : kStateStringProtoPairs) {
+    if (pair.state_proto == state) {
+      return std::string(pair.state_string);
+    }
+  }
+  return std::string(kReceiverStateUknown);
+}
+
 }  // namespace ash::boca_receiver
--- chromeos/ash/components/boca/receiver/kiosk_receiver_parser.h
+++ chromeos/ash/components/boca/receiver/kiosk_receiver_parser.h
@@ -10,8 +10,13 @@
 #include "chromeos/ash/components/boca/proto/receiver.pb.h"
 
 namespace ash::boca_receiver {
+
 ::boca::ReceiverConnectionState ReceiverConnectionStateProtoFromJson(
     const std::string& state);
+
+std::string ReceiverConnectionStateStringFromProto(
+    ::boca::ReceiverConnectionState state);
+
 }  // namespace ash::boca_receiver
 
 #endif  // CHROMEOS_ASH_COMPONENTS_BOCA_RECEIVER_KIOSK_RECEIVER_PARSER_H_
