8adb29b433258cff05fe31f1cace0cfb2a6ccae8
Shu Yang <shuyng@google.com>
2025-09-17 01:04:29 -0700

    Enable AndroidPdfAssistContent on ToT
    Launch approval:
    https://launch.corp.google.com/launch/4352108/approver/14628
    
    Enabled-by-default-reason: launching
    Change-Id: Id8b40b6a1c37e99270756cb1421df6c9ba8a9af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6956857
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516485}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -590,7 +590,7 @@ BASE_FEATURE(kAndroidOmniboxFocusedNewTabPage,
 
 BASE_FEATURE(kAndroidOpenPdfInlineBackport, base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAndroidPdfAssistContent, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAndroidPdfAssistContent, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidSettingsContainment, base::FEATURE_DISABLED_BY_DEFAULT);
 
