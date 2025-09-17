8d797fb9ec0792ddf16efd94d6cd9f163c4d9f20
Nasser Al-shawwa <alshawwa@chromium.org>
2025-09-17 16:21:56 -0700

    Reland https://crrev.com/c/6952293
    The MSan failure was caused by using `watermark_block_` prior to
    initializing it. In https://crrev.com/c/6952293, `SetString()` was
    modified to conditionally set `watermark_block_`, and when constructing
    the view with an empty string, `watermark_block_` was never initialized.
    To fix, we simply unconditionally initialize `watermark_block_` in the
    constructor.
    
    Bug: 441743588
    Change-Id: I9c94ccdd991e29c12ed5970aabe6099b6bb6ada6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6961899
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1516921}

--- chrome/browser/enterprise/watermark/watermark_view.cc
+++ chrome/browser/enterprise/watermark/watermark_view.cc
@@ -30,12 +30,42 @@ WatermarkView::WatermarkView() : background_color_(SkColorSetARGB(0, 0, 0, 0)) {
   SetCanProcessEventsWithinSubtree(false);
   SetPaintToLayer();
   layer()->SetFillsBoundsOpaquely(false);
-  SetString(/*text=*/std::string(""), GetDefaultFillColor(),
-            GetDefaultOutlineColor(), GetDefaultFontSize());
+  UpdateWatermarkBlock(/*watermark_text=*/std::string(""),
+                       GetDefaultFillColor(), GetDefaultOutlineColor(),
+                       GetDefaultFontSize());
   GetViewAccessibility().SetIsInvisible(true);
 }
 
 WatermarkView::~WatermarkView() = default;
+
+void WatermarkView::InvalidateView() {
+  SchedulePaint();
+}
+
+void WatermarkView::MaybeUpdateWatermarkBlock(const std::string& watermark_text,
+                                              SkColor fill_color,
+                                              SkColor outline_color,
+                                              int font_size) {
+  // No need to invalidate when both old and new strings are empty, even if the
+  // style values change.
+  if (watermark_text_.empty() && watermark_text.empty()) {
+    return;
+  }
+
+  // If at least one value changes, invalidate the view.
+  bool should_update =
+      (watermark_text_ != watermark_text) || (fill_color_ != fill_color) ||
+      (outline_color_ != outline_color) || (font_size_ != font_size);
+
+  if (should_update) {
+    watermark_text_ = watermark_text;
+    fill_color_ = fill_color;
+    outline_color_ = outline_color;
+    font_size_ = font_size;
+
+    UpdateWatermarkBlock(watermark_text, fill_color, outline_color, font_size);
+  }
+}
 
 void WatermarkView::SetString(const std::string& text,
                               SkColor fill_color,
@@ -44,11 +74,7 @@ void WatermarkView::SetString(const std::string& text,
   DCHECK(base::IsStringUTF8(text));
   CHECK_GE(font_size, 1);
 
-  watermark_block_ =
-      DrawWatermarkToPaintRecord(text, fill_color, outline_color, font_size);
-
-  // Invalidate the state of the view.
-  SchedulePaint();
+  MaybeUpdateWatermarkBlock(text, fill_color, outline_color, font_size);
 }
 
 void WatermarkView::OnPaint(gfx::Canvas* canvas) {
@@ -59,9 +85,18 @@ void WatermarkView::OnPaint(gfx::Canvas* canvas) {
       SkSize::Make(contents_bounds.width(), contents_bounds.height()));
 }
 
+void WatermarkView::UpdateWatermarkBlock(const std::string& text,
+                                         SkColor fill_color,
+                                         SkColor outline_color,
+                                         int font_size) {
+  watermark_block_ =
+      DrawWatermarkToPaintRecord(text, fill_color, outline_color, font_size);
+  InvalidateView();
+}
+
 void WatermarkView::SetBackgroundColor(SkColor background_color) {
   background_color_ = background_color;
-  SchedulePaint();
+  InvalidateView();
 }
 
 BEGIN_METADATA(WatermarkView)
