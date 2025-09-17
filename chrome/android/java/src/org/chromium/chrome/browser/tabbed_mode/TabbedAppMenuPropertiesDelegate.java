fa58bfcde83dda136c47cf8b6c750ad0e2409bee
Aishwarya Rajesh <aishwaryarj@google.com>
2025-09-16 18:33:39 -0700

    [RWM] Update Manage Windows app menu option text
    Omit the number of windows from the option text.
    Screenshot: https://screenshot.googleplex.com/BkCuMLSz26B64A2
    
    Bug: 445523981
    Change-Id: I8a672567a58410ab8a3d0643484890716a543d10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6954567
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516369}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedAppMenuPropertiesDelegate.java
@@ -595,16 +595,12 @@ public class TabbedAppMenuPropertiesDelegate extends AppMenuPropertiesDelegateIm
 
     private MVCListAdapter.ListItem buildManageWindowsItem() {
         assert MultiWindowUtils.shouldShowManageWindowsMenu();
-        PropertyModel model =
+        return new MVCListAdapter.ListItem(
+                AppMenuHandler.AppMenuItemType.STANDARD,
                 buildModelForStandardMenuItem(
                         R.id.manage_all_windows_menu_id,
                         R.string.menu_manage_all_windows,
-                        shouldShowIconBeforeItem() ? R.drawable.ic_select_window : 0);
-        model.set(
-                AppMenuItemProperties.TITLE,
-                mContext.getString(R.string.menu_manage_all_windows, getInstanceCount()));
-
-        return new MVCListAdapter.ListItem(AppMenuHandler.AppMenuItemType.STANDARD, model);
+                        shouldShowIconBeforeItem() ? R.drawable.ic_select_window : 0));
     }
 
     private MVCListAdapter.ListItem buildHistoryItem() {
