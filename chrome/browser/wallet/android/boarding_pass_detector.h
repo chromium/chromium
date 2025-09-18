05e64948423a7ffe9fae448e98abdceff9702759
Norge Vizcay <vizcay@google.com>
2025-09-18 02:03:01 -0700

    Add documentation to BoardingPassDetector.
    Bug: 40942769
    Change-Id: I5ec0161f48953eb507067da6203cecdd3679683c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6948153
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Hanfeng Zhu <hanfeng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517126}

--- chrome/browser/wallet/android/boarding_pass_detector.h
+++ chrome/browser/wallet/android/boarding_pass_detector.h
@@ -12,30 +12,44 @@
 
 namespace wallet {
 
+// Detects boarding passes on a web page. This class is self-owned and will
+// delete itself after the detection is complete or if the renderer process
+// crashes.
 class BoardingPassDetector {
  public:
   BoardingPassDetector();
   ~BoardingPassDetector();
 
-  // Detects boarding pass on cureent web_contents.
+  // Initiates the boarding pass detection on the given `web_contents`.
+  // `callback` is invoked with a vector of detected boarding pass strings.
   void DetectBoardingPass(
       content::WebContents* web_contents,
       base::OnceCallback<void(const std::vector<std::string>&)> callback);
 
+  // Performs the boarding pass detection using the provided `remote`.
   void DetectBoardingPassWithRemote(
       mojo::Remote<mojom::BoardingPassExtractor> remote,
       base::OnceCallback<void(const std::vector<std::string>&)> callback);
 
-  // Decides whether to run boarding pass detection on given url.
+  // Returns true if the boarding pass detection should be run on the given
+  // `url`.
   static bool ShouldDetect(const std::string& url);
 
  private:
+  // Invoked when the boarding pass extraction is complete.
   void ExtractBoardingPassComplete(
       const std::vector<std::string>& boarding_passes);
+
+  // Sends the detection result to the callback and deletes this object.
   void SendResultAndDeleteSelf();
 
+  // The remote for the boarding pass extractor service.
   mojo::Remote<mojom::BoardingPassExtractor> remote_;
+
+  // The callback to be invoked with the detection results.
   base::OnceCallback<void(const std::vector<std::string>&)> callback_;
+
+  // The result of the boarding pass extraction.
   std::vector<std::string> extraction_result_;
 };
 
