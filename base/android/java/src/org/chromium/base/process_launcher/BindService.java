2a410302d23683dbd02b40966188be3ce508c2d2
Shin Kawamura <kawasin@google.com>
2025-09-17 18:05:55 -0700

    Reland "Expand Binder IPC call counting to include unbindService and updateServiceGroup."
    This is a reland of commit a39596869ecdb2d9fd280dc3376a80c5d521f517
    
    no change from the original CL. The original CL was reverted because
    of the parent CL.
    
    Original change's description:
    > Expand Binder IPC call counting to include unbindService and updateServiceGroup.
    >
    > This deprecates `Android.ChildProcessBinding.BinderIPC.Count` and adds
    > `Android.ChildProcessBinding.BinderIPC.{Type}.Count` UMA metrics to
    > count binder IPC calls.
    >
    > We will keep the deparecated
    > `Android.ChildProcessBinding.BinderIPC.Count` for a while because
    > ongoing experiment uses the metrics.
    >
    > Bug: 436110326
    > Change-Id: Ieb3ae95b20b677308c21d71b55de096232a09fce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6940597
    > Commit-Queue: Shin Kawamura <kawasin@google.com>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Bo Liu <boliu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1516349}
    
    Bug: 436110326
    Change-Id: I10626579fa8f32ae88ef679083dbdaae9b0a23a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6957499
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1516978}

--- base/android/java/src/org/chromium/base/process_launcher/BindService.java
+++ base/android/java/src/org/chromium/base/process_launcher/BindService.java
@@ -27,8 +27,13 @@ import java.util.concurrent.Executor;
 @NullMarked
 public final class BindService {
     private static @Nullable Method sBindServiceAsUserMethod;
-    private static int sBindServiceCount;
-    private static boolean sEnableCounting;
+    private static @Nullable BinderCallCounter sBinderCallCounter;
+
+    public static final class BinderCallCounter {
+        public int mBindServiceCount;
+        public int mUnbindServiceCount;
+        public int mUpdateServiceGroupCount;
+    }
 
     static boolean supportVariableConnections() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
@@ -45,8 +50,8 @@ public final class BindService {
             Handler handler,
             Executor executor,
             @Nullable String instanceName) {
-        if (sEnableCounting) {
-            sBindServiceCount++;
+        if (sBinderCallCounter != null) {
+            sBinderCallCounter.mBindServiceCount++;
         }
         if (supportVariableConnections() && instanceName != null) {
             return context.bindIsolatedService(intent, flags, instanceName, executor, connection);
@@ -68,16 +73,36 @@ public final class BindService {
         }
     }
 
+    static void doUnbindService(Context context, ServiceConnection connection) {
+        if (sBinderCallCounter != null) {
+            sBinderCallCounter.mUnbindServiceCount++;
+        }
+        context.unbindService(connection);
+    }
+
+    static void doUpdateServiceGroup(
+            Context context, ServiceConnection connection, int group, int importanceInGroup) {
+        if (sBinderCallCounter != null) {
+            sBinderCallCounter.mUpdateServiceGroupCount++;
+        }
+        context.updateServiceGroup(connection, group, importanceInGroup);
+    }
+
     /**
-     * Enables counting of bindService calls.
+     * Enables counting of service binding Binder calls.
      *
      * <p>Note that counter is not thread-safe. setEnableCounting(), doBindService(),
-     * getAndResetBindServiceCount() should be called on the same thread.
+     * doUnbindService(), doUpdateServiceGroup(), and getAndResetBinderCallCounter() should be
+     * called on the same thread.
      *
-     * @param enabled Whether to enable counting of bindService calls.
+     * @param enabled Whether to enable counting of binder calls.
      */
     public static void setEnableCounting(boolean enabled) {
-        sEnableCounting = enabled;
+        if (enabled) {
+            sBinderCallCounter = new BinderCallCounter();
+        } else {
+            sBinderCallCounter = null;
+        }
     }
 
     /**
@@ -85,10 +110,12 @@ public final class BindService {
      *
      * @return The number of bindService calls.
      */
-    public static int getAndResetBindServiceCount() {
-        int count = sBindServiceCount;
-        sBindServiceCount = 0;
-        return count;
+    public static @Nullable BinderCallCounter getAndResetBinderCallCounter() {
+        BinderCallCounter counter = sBinderCallCounter;
+        if (counter != null) {
+            sBinderCallCounter = new BinderCallCounter();
+        }
+        return counter;
     }
 
     private static boolean bindServiceByCall(
