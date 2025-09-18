93dfc610492812f8262c6fa6eae9966bfa005eae
Alexei Svitkine <asvitkine@chromium.org>
2025-09-18 08:43:18 -0700

    Refactor enum usage detection into a common module.
    Moves the logic for identifying all enums referenced in histograms, UKM, and DWA XML files from `remove_unused_enums.py` to a new file `tools/metrics/common/enums.py`. This makes the enum usage detection reusable and moves it to the common directory, since it's not just
    about histograms.
    
    My plan is to reuse this in presubmits to guard against unused
    enums being left behind.
    
    Change-Id: Ieb04fb0547fa57817f28e0c26c72c511e9d26241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6950874
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1517327}

--- tools/metrics/histograms/remove_unused_enums.py
+++ tools/metrics/histograms/remove_unused_enums.py
@@ -12,44 +12,13 @@ from xml.dom import minidom
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
 
-import extract_histograms
+import enums
 import histogram_configuration_model
 import histogram_paths
-import merge_xml
-import path_util
 
 
-def _get_enums_from_histogram_files(files: list[str]):
-  """Finds the names of all referenced enums from the specified XML files."""
-  merged = merge_xml.MergeFiles(files)
-  histograms, _ = extract_histograms.ExtractHistogramsFromDom(merged)
-  enums_used_in_file = set()
-  for histogram_name, data in histograms.items():
-    # Skip non-enum histograms.
-    if 'enumDetails' not in data:
-      continue
-    enum_name = data['enumDetails']['name']
-    enums_used_in_file.add(enum_name)
-  return enums_used_in_file
-
-
-def _get_enums_referenced_by_metric_nodes_in_xml_files(files: list[str]):
-  """Finds enums used by ukm.xml and similar files."""
-  enums_used_in_files = set()
-
-  for file_path in files:
-    with open(file_path, 'r') as f:
-      document = minidom.parse(f)
-
-    for node in document.getElementsByTagName('metric'):
-      if not 'enum' in node.attributes:
-        continue
-      enums_used_in_files.add(node.attributes['enum'].value)
-
-  return enums_used_in_files
-
-
-def _remove_enum_nodes_not_in_list(enum_names, filepath):
+def _remove_enum_nodes_not_in_list(
+    enum_names: set[str], filepath: str) -> tuple[list[minidom.Element], str]:
   """Returns the <enum> nodes not corresponding to the specified names."""
   with io.open(filepath, 'r', encoding='utf-8') as f:
     document = minidom.parse(f)
@@ -68,37 +37,27 @@ def _remove_enum_nodes_not_in_list(enum_names, filepath):
 
 def _remove_unused_enums():
   """Removes unused enums from ALL enums.xml files."""
-  print(f'Reading histogram XML files...')
-  enum_names = _get_enums_from_histogram_files(histogram_paths.ALL_XMLS)
-  print(f'Found {len(enum_names)} enums from histograms.')
-
-  metric_files = [
-      histogram_paths.UKM_XML,
-      histogram_paths.DWA_XML,
-  ]
-  metric_enum_names = (
-      _get_enums_referenced_by_metric_nodes_in_xml_files(metric_files))
-  print(f'Found {len(metric_enum_names)} enums from ukm and dwa.')
-
-  enum_names.update(metric_enum_names)
-  print(f'Found {len(enum_names)} enums total.')
+  enum_names = enums.get_enums_used_in_files()
 
   for enum_file in histogram_paths.ENUMS_XMLS:
     enum_nodes, updated_enum_xml = _remove_enum_nodes_not_in_list(
         enum_names, enum_file)
     if not enum_nodes:
-      print(f'All enums in {enum_file} are referenced')
+      logging.info(f'All enums in {enum_file} are referenced')
       continue
 
-    print(f'Removing {len(enum_nodes)} enum nodes from {enum_file}:')
+    logging.info(f'Removing {len(enum_nodes)} enum nodes from {enum_file}:')
     for enum_node in enum_nodes:
       enum_name = enum_node.attributes['name'].value
       print(f' - {enum_name}')
 
     with open(enum_file, 'w', encoding='utf-8', newline='') as f:
       f.write(updated_enum_xml)
-    print('File updated.')
+    logging.info('File updated.')
 
 
 if __name__ == '__main__':
+  logging.basicConfig(level=logging.INFO,
+                      stream=sys.stderr,
+                      format='%(message)s')
   _remove_unused_enums()
