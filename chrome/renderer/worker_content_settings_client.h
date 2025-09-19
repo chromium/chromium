42bed731eaf6ddd1d0dc6d60efe8fd9202c123bf
Michael Wilson <mjwilson@chromium.org>
2025-09-19 09:13:31 -0700

    Fix null pointer dereference in WorkerContentSettingsClient
    A ClusterFuzz test case found a null pointer dereference in the
    WorkerContentSettingsClient constructor.
    
    This may be due to a race condition where the agent is destroyed before
    the client is constructed.
    
    Fix this by checking the pointer before using it, and setting default
    values if the pointer is null.
    
    Bug: 441823822
    Change-Id: I1833a87b63c8131a9a1fd8551bc29b7a3dcec4c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6967332
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518005}

--- chrome/renderer/worker_content_settings_client.h
+++ chrome/renderer/worker_content_settings_client.h
@@ -49,7 +49,7 @@ class WorkerContentSettingsClient : public blink::WebContentSettingsClient {
   url::Origin document_origin_;
   net::SiteForCookies site_for_cookies_;
   url::Origin top_frame_origin_;
-  bool allow_running_insecure_content_;
+  bool allow_running_insecure_content_ = false;
   const blink::LocalFrameToken frame_token_;
   std::unique_ptr<RendererContentSettingRules> content_setting_rules_;
 
