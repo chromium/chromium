932e34f43c57c9158c4a6c668b10bafcf8844c87
Patrick Monette <pmonette@chromium.org>
2025-09-19 12:08:45 -0700

    [UrlIndex] Make the MemoryPressureListener async
    In the future, MemoryPressureListener will be migrated to be
    synchronously invoked on the main thread. Migrating the one in UrlIndex
    should be technically possible, as it lives on the renderer's main
    thread, but --single-process mode breaks that assumption.
    
    This change is a no-op, as all MemoryPressureListeners are currently
    async.
    
    Bug: 436324601
    Change-Id: I360257e62e52de20480cf87eb72809bf8151ba47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966556
    Auto-Submit: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518107}

--- third_party/blink/renderer/platform/media/url_index.h
+++ third_party/blink/renderer/platform/media/url_index.h
@@ -340,7 +340,9 @@ class PLATFORM_EXPORT UrlIndex {
   // Currently only changed for testing purposes.
   const int block_shift_;
 
-  base::MemoryPressureListener memory_pressure_listener_;
+  // Must be async, because it runs on the renderer's main thread, which is not
+  // the process's main thread in --single-process mode.
+  base::AsyncMemoryPressureListener memory_pressure_listener_;
   const scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
 
   base::WeakPtrFactory<UrlIndex> weak_factory_{this};
