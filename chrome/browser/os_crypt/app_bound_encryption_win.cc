e812f295f5fe116a5f5e60191c76b1d06ef91dd1
Will Harris <wfh@chromium.org>
2025-09-22 15:17:03 -0700

    Enable v3 key upgrade by default
    This is launching shortly, so this CL enables the code by default so it
    can be shipped to all users.
    
    BUG=382234536
    
    Enabled-by-default-reason: Enabling
    
    Change-Id: I505221af948674d10e9b7570f1ea74cb0681fe00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973345
    Commit-Queue: Will Harris <wfh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518971}

--- chrome/browser/os_crypt/app_bound_encryption_win.cc
+++ chrome/browser/os_crypt/app_bound_encryption_win.cc
@@ -58,7 +58,7 @@ ProtectionLevel AddFlags(ProtectionLevel protection_level,
 }  // namespace
 
 namespace features {
-BASE_FEATURE(kAppBoundDataReencrypt, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAppBoundDataReencrypt, base::FEATURE_ENABLED_BY_DEFAULT);
 }  // namespace features
 
 SupportLevel GetAppBoundEncryptionSupportLevel(PrefService* local_state) {
