7bb1659e7e496ec2aefb302e2eda0b69aac0c92b
Dan Harrington <harringtond@chromium.org>
2025-09-22 09:41:26 -0700

    Restore `sys.path` after modifying it in GLIC presubmit checks.
    Bug: 446220082
    Change-Id: I687694d9e56cd041334e32a7cd5de89ef9be58ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6969839
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1518759}

--- chrome/browser/glic/host/PRESUBMIT.py
+++ chrome/browser/glic/host/PRESUBMIT.py
@@ -3,9 +3,14 @@
 # found in the LICENSE file.
 
 def _CommonChecks(input_api, output_api):
-    input_api.sys.path.insert(0, "../../../..")
-    from chrome.browser.resources.glic.common_checks import GlicCommonChecks
-    return GlicCommonChecks(input_api, output_api)
+    old_path = input_api.sys.path[:]
+    try:
+        input_api.sys.path.insert(0, "../../../..")
+        from chrome.browser.resources.glic.common_checks import GlicCommonChecks
+        return GlicCommonChecks(input_api, output_api)
+    finally:
+        # Restore the original path, or other presubmits may fail.
+        input_api.sys.path = old_path
 
 
 def CheckChangeOnUpload(input_api, output_api):
--- chrome/browser/resources/glic/PRESUBMIT.py
+++ chrome/browser/resources/glic/PRESUBMIT.py
@@ -122,13 +122,18 @@ def CheckApiChangesAreBackwardsCompatibleIfModified(input_api, output_api,
 
 
 def _CommonChecks(input_api, output_api, on_upload):
-    input_api.sys.path.insert(0, "../../../..")
-    from chrome.browser.resources.glic.common_checks import GlicCommonChecks
-    return sum([
-        CheckApiChangesAreBackwardsCompatibleIfModified(
-            input_api, output_api, on_upload),
-        GlicCommonChecks(input_api, output_api),
-    ], [])
+    old_path = input_api.sys.path[:]
+    try:
+        input_api.sys.path.insert(0, "../../../..")
+        from chrome.browser.resources.glic.common_checks import GlicCommonChecks
+        return sum([
+            CheckApiChangesAreBackwardsCompatibleIfModified(
+                input_api, output_api, on_upload),
+            GlicCommonChecks(input_api, output_api),
+        ], [])
+    finally:
+        # Restore the original path, or other presubmits may fail.
+        input_api.sys.path = old_path
 
 
 def CheckChangeOnUpload(input_api, output_api):
