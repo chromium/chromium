146250badea1ba10d07681a352cd572d5bb1855a
Moe Ahmadi <mahmadi@chromium.org>
2025-09-22 03:47:54 -0700

    [141][omnibox] Better Control for AimEligibilityService Server Requests.
    This change introduces feature flags for more granular control over
    when AimEligibilityService makes server requests. This allows for
    flexible configuration of request frequency and helps manage QPS.
    
    The default behavior for identity-change related requests is changed
    from cookie jar changes to primary account changes. This is intended
    to minimize QPS, as the server likely uses the primary account for
    authentication, making primary account changes the more relevant.
    
    - Adds `kAimServerRequestOnStartupEnabled` to control making a request
     on service startup (enabled by default).
    - Adds `kAimServerRequestOnIdentityChangeEnabled` to control making
      requests on identity-related changes (enabled by default).
      - `request_on_cookie_jar_changes` param controls requests on cookie
         jar updates (false by default).
      - `request_on_primary_account_changes` param controls requests on
         primary account changes (true by default).
    
    Enabled-by-default-reason: kill switch
    
    Bug: 436225340, 446259598
    
    (cherry picked from commit feed8b760626e13c7515267e375e89cdc0d58618)
    
    Change-Id: Ia35405e83b12dbcad312a5bd8034360faf8bf0b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965898
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1517890}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966231
    Auto-Submit: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/branch-heads/7390@{#1498}
    Cr-Branched-From: d481efce5eb300acbb896686676ebd0352a6f1db-refs/heads/main@{#1509326}

--- components/omnibox/browser/aim_eligibility_service_features.cc
+++ components/omnibox/browser/aim_eligibility_service_features.cc
@@ -4,20 +4,36 @@
 
 #include "components/omnibox/browser/aim_eligibility_service_features.h"
 
+#include "base/metrics/field_trial_params.h"
+
 namespace omnibox {
 
-// If enabled, uses the server response for AIM eligibility for all locales.
+BASE_FEATURE(kAimEnabled, "AimEnabled", base::FEATURE_ENABLED_BY_DEFAULT);
+
 BASE_FEATURE(kAimServerEligibilityEnabled,
              "AimServerEligibilityEnabled",
              base::FEATURE_DISABLED_BY_DEFAULT);
-// If enabled, uses the server response for AIM eligibility for English locales.
-// Has no effect if kAimServerEligibilityEnabled is enabled.
+
 BASE_FEATURE(kAimServerEligibilityEnabledEn,
              "AimServerEligibilityEnabledEn",
              base::FEATURE_DISABLED_BY_DEFAULT);
-// If enabled, notifies AIM eligibility changes.
+
 BASE_FEATURE(kAimServerEligibilityChangedNotification,
              "AimServerEligibilityChangedNotification",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAimServerRequestOnStartupEnabled,
+             "AimServerRequestOnStartupEnabled",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAimServerRequestOnIdentityChangeEnabled,
+             "AimServerRequestOnIdentityChangeEnabled",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+const base::FeatureParam<bool> kRequestOnCookieJarChanges{
+    &kAimServerRequestOnIdentityChangeEnabled, "request_on_cookie_jar_changes",
+    false};
+const base::FeatureParam<bool> kRequestOnPrimaryAccountChanges{
+    &kAimServerRequestOnIdentityChangeEnabled,
+    "request_on_primary_account_changes", true};
 
 }  // namespace omnibox
