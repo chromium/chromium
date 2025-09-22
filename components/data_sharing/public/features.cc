8dbf8bd29d446d642708a0035301a3b558db0b8f
Hailey Wang <haileywang@google.com>
2025-09-22 15:30:41 -0700

    [Comments] Enable the PersonalCollaborationService
    Enabled-by-default-reason: launching
    Bug: 433329690
    Change-Id: I888f072bc6ac25a39c4b4e9778831ea142f97f88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966345
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1518977}

--- components/data_sharing/public/features.cc
+++ components/data_sharing/public/features.cc
@@ -27,7 +27,8 @@ BASE_FEATURE(kDataSharingFeature,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDataSharingAccountDataMigration,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             "DataSharingAccountDataMigration",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDataSharingJoinOnly, base::FEATURE_DISABLED_BY_DEFAULT);
 
