f38b4772a1502c3c1a241f7a7dc885d66b36ce42
Ted Meyer <tmathmeyer@chromium.org>
2025-09-22 23:01:20 -0700

    Use std::string_view for TypedStatus constructors
    Ultimately a copy of the string is still being made at the end to store
    it in `data_`, but at least now there can't be any extra copies.
    
    Bug: 444259588
    Change-Id: Iac02e56022f0485d7f709bb79354130716658d7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6973027
    Auto-Submit: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519117}

--- media/base/status.h
+++ media/base/status.h
@@ -200,10 +200,10 @@ class MEDIA_EXPORT TypedStatus {
   template <typename D>
     requires(TypedStatusConstructableFrom<TypedStatus<T>, D>)
   TypedStatus(Codes code,
-              std::string message,
+              std::string_view message,
               const D& data,
               const base::Location& location = FROM_HERE)
-      : TypedStatus(code, std::move(message), location) {
+      : TypedStatus(code, message, location) {
     DCHECK(data_);
     T::OnCreateFrom(this, data);
   }
@@ -222,10 +222,10 @@ class MEDIA_EXPORT TypedStatus {
   // Used to allow returning {TypedStatus::Codes::kValue, "message", cause}
   template <TypedStatusImplTraits O>
   TypedStatus(Codes code,
-              std::string message,
+              std::string_view message,
               TypedStatus<O>&& cause,
               const base::Location& location = FROM_HERE)
-      : TypedStatus(code, std::move(message), location) {
+      : TypedStatus(code, message, location) {
     DCHECK(data_);
     AddCause(std::move(cause));
   }
@@ -238,7 +238,7 @@ class MEDIA_EXPORT TypedStatus {
   // Also used to allow returning {TypedStatus::Codes::kValue, "message"}
   // implicitly as a typed status.
   TypedStatus(Codes code,
-              std::string message,
+              std::string_view message,
               const base::Location& location = FROM_HERE) {
     // Note that |message| would be dropped when code is the default value,
     // so DCHECK that it is not set.
@@ -248,8 +248,7 @@ class MEDIA_EXPORT TypedStatus {
     }
     data_ = std::make_unique<internal::StatusData>(
         T::Group(), static_cast<StatusCodeType>(code),
-        internal::StatusTraitsHelper<Traits>::GetMessage(std::move(message),
-                                                         code));
+        internal::StatusTraitsHelper<Traits>::GetMessage(message, code));
     data_->AddLocation(location);
   }
 
