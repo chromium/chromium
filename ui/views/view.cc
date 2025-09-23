b06a995e5debb417de22fa9123fd916d8ea12108
Phillis Tang <phillis@chromium.org>
2025-09-23 16:58:07 -0700

    Revert "Remove redundant calls to UnregisterAccelerators"
    This reverts commit 4208d865d5ffc775aa23e75579ae415e258a671c.
    
    Reason for revert: cause test failures https://ci.chromium.org/ui/p/chromium/builders/ci/mac15-x64-rel-tests/7354/blamelist
    
    Original change's description:
    > Remove redundant calls to UnregisterAccelerators
    >
    > This is already called when view is removed in [1]. This actually
    > is causing additional O(n^2) > cost becacuse it calls subtree while walking through the same tree.
    >
    > [1] https://source.chromium.org/chromium/chromium/src/+/main:ui/views/view.cc;drc=5b67f78e5b7b1825c3e4efe1ef7dde15d6e2ded9;l=3214
    >
    > Bug: 443993719
    > Change-Id: If915ea1295aef8139cafe3b79837a63716c2156e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6975195
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1519555}
    
    Bug: 443993719
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I872bb1a5d6c4cc4f28e8f5f371c1506c885f15b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977693
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Phillis Tang <phillis@chromium.org>
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1519683}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -389,6 +389,16 @@ std::optional<size_t> View::GetIndexOf(const View* view) const {
                                      std::distance(children_.cbegin(), i)));
 }
 
+void View::PropagateWillClearFocusManager() {
+  {
+    internal::ScopedChildrenLock lock(this);
+    for (views::View* child : children_) {
+      child->PropagateWillClearFocusManager();
+    }
+  }
+  WillClearFocusManager();
+}
+
 // Size and disposition --------------------------------------------------------
 
 void View::SetBounds(int x, int y, int width, int height) {
@@ -2326,6 +2336,10 @@ void View::OnVisibleBoundsChanged() {}
 void View::ViewHierarchyChanged(const ViewHierarchyChangedDetails& details) {}
 
 void View::VisibilityChanged(View* starting_from, bool is_visible) {}
+
+void View::WillClearFocusManager() {
+  UnregisterAccelerators(true);
+}
 
 void View::NativeViewHierarchyChanged() {
   RegisterPendingAccelerators();
