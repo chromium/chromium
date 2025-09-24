ae33346a2bea2adccfb83de0795258bdad48b7a6
Joe Mason <joenotcharles@google.com>
2025-09-24 10:44:50 -0700

    Remove HeapProfilerMultiKeyHashSet features
    Stable data shows no performance improvement with this feature.
    
    Reverts https://crrev.com/c/6639029 and https://crrev.com/c/6196565 and
    removes the fieldtrial config.
    
    Bug: 383374205
    Change-Id: Ide80b35bb0df6ef2c83b87ef0518dbae27783214
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977292
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520095}

--- base/sampling_heap_profiler/poisson_allocation_sampler.cc
+++ base/sampling_heap_profiler/poisson_allocation_sampler.cc
@@ -15,7 +15,6 @@
 #include "base/allocator/dispatcher/tls.h"
 #include "base/check.h"
 #include "base/compiler_specific.h"
-#include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "base/rand_util.h"
 #include "build/build_config.h"
@@ -23,8 +22,6 @@
 namespace base {
 
 namespace {
-
-BASE_FEATURE(kHeapProfilerMultiKeyHashSet, base::FEATURE_DISABLED_BY_DEFAULT);
 
 using ::base::allocator::dispatcher::ReentryGuard;
 
@@ -212,8 +209,7 @@ constinit std::atomic<PoissonAllocationSampler::ProfilingStateFlagMask>
 
 PoissonAllocationSampler::PoissonAllocationSampler() {
   Init();
-  auto* sampled_addresses = new LockFreeAddressHashSet(
-      64, mutex_, base::FeatureList::IsEnabled(kHeapProfilerMultiKeyHashSet));
+  auto* sampled_addresses = new LockFreeAddressHashSet(64, mutex_);
   g_sampled_addresses_set.store(sampled_addresses, std::memory_order_release);
 }
 
@@ -407,8 +403,7 @@ void PoissonAllocationSampler::BalanceAddressesHashSet() {
     return;
   }
   auto new_set = std::make_unique<LockFreeAddressHashSet>(
-      current_set.buckets_count() * 2, mutex_,
-      base::FeatureList::IsEnabled(kHeapProfilerMultiKeyHashSet));
+      current_set.buckets_count() * 2, mutex_);
   new_set->Copy(current_set);
   // Atomically switch all the new readers to the new set.
   g_sampled_addresses_set.store(new_set.release(), std::memory_order_release);
