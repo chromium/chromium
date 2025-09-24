8a28764f4176ec4fbac8be57e9d43c7f1d0e0595
Justin Lulejian <jlulejian@chromium.org>
2025-09-24 07:53:39 -0700

    [Extensions] Update PortId to use C++20 comparators.
    Converts the pre-C++20 comparators using std::tie to use the spaceship
    operator instead.
    
    Bug: 40753031
    Change-Id: Id569fb7f8a5110d0befeff9d168838e9758c8fec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974606
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519983}

--- extensions/common/api/messaging/port_id.cc
+++ extensions/common/api/messaging/port_id.cc
@@ -4,8 +4,6 @@
 
 #include "extensions/common/api/messaging/port_id.h"
 
-#include <tuple>
-
 namespace extensions {
 
 PortId::PortId() = default;
@@ -21,17 +19,5 @@ PortId::~PortId() = default;
 PortId::PortId(PortId&& other) = default;
 PortId::PortId(const PortId& other) = default;
 PortId& PortId::operator=(const PortId& other) = default;
-
-bool PortId::operator==(const PortId& other) const {
-  return context_id == other.context_id && port_number == other.port_number &&
-         is_opener == other.is_opener &&
-         serialization_format == other.serialization_format;
-}
-
-bool PortId::operator<(const PortId& other) const {
-  return std::tie(context_id, port_number, is_opener, serialization_format) <
-         std::tie(other.context_id, other.port_number, other.is_opener,
-                  other.serialization_format);
-}
 
 }  // namespace extensions
