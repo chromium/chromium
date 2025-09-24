f8e42a94b75e26b22803aba044bb0fdba681d428
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-09-24 14:20:38 -0700

    Revert "Use unordered_map in AcceleratorMap"
    This reverts commit 6a35d380753103116d7d2ba51eec077fe75b8aac.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5660724471791616
    
    Sample build with failed test: https://ci.chromium.org/b/8702894488255818913
    Affected test(s):
    [ninja://chrome/test:interactive_ui_tests/BrowserCommandControllerInteractiveTest.ShortcutsShouldTakeEffectInWindowMode](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FBrowserCommandControllerInteractiveTest.ShortcutsShouldTakeEffectInWindowMode?q=VHash%3A092a749d643094cb)
    [ninja://chrome/test:interactive_ui_tests/BrowserFocusTest.TabsRememberFocus](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FBrowserFocusTest.TabsRememberFocus?q=VHash%3A092a749d643094cb)
    [ninja://chrome/test:interactive_ui_tests/BrowserKeyEventsTest.CtrlKeyEvents](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FBrowserKeyEventsTest.CtrlKeyEvents?q=VHash%3A092a749d643094cb)
    [ninja://chrome/test:interactive_ui_tests/BubbleSignInPromoInteractiveUITest.AddressSignInPromoDismissedEscapeKey](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FBubbleSignInPromoInteractiveUITest.AddressSignInPromoDismissedEscapeKey?q=VHash%3A092a749d643094cb)
    [ninja://chrome/test:interactive_ui_tests/CommandsApiTest.ActionKeyUpdated](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:interactive_ui_tests%2FCommandsApiTest.ActionKeyUpdated?q=VHash%3A092a749d643094cb)
    and 15 more ...
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5660724471791616&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F6974085&type=BUG
    
    Original change's description:
    > Use unordered_map in AcceleratorMap
    >
    > Also changed the erase code, which also showed improvements.
    >
    > On my cloud top, this reduced the wall time of
    > Widget::HandleWidgetDestroy around 50% (200~ns to ~100ns)
    > for browser window. (release, no dcheck)
    >
    > Bug: 443993719
    >
    > Change-Id: Ie2a41a96ecbbb33958bd866764d32207bd67c4d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6974085
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1519637}
    >
    
    Bug: 443993719
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I10975bc0c0eb4b5b367fcbfe07135fbe58f43103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6980547
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Owners-Override: David Bienvenu <davidbienvenu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520125}

--- ui/base/accelerators/accelerator_manager.cc
+++ ui/base/accelerators/accelerator_manager.cc
@@ -139,12 +139,9 @@ bool AcceleratorManager::AcceleratorTargetInfo::Unregister(
     has_priority_handler_ = false;
 
   // Attempt to remove the target and return true if it was present.
-  auto iter = std::find(targets_.begin(), targets_.end(), target);
-  if (iter != targets_.end()) {
-    targets_.erase(iter);
-    return true;
-  }
-  return false;
+  const size_t original_target_count = targets_.size();
+  targets_.remove(target);
+  return original_target_count != targets_.size();
 }
 
 bool AcceleratorManager::AcceleratorTargetInfo::TryProcess(
