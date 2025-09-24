640a3d5ff78589eee26f0c9352bb5568acb68bd1
Elly <ellyjones@chromium.org>
2025-09-24 08:09:39 -0700

    android: use crypto/evp to parse browser binding certs
    Bug: 425864544
    Change-Id: Ibc321fd9328663e74a13c0f43254622a920d73a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6977540
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1519990}

--- components/enterprise/client_certificates/android/browser_binding/browser_key_android.cc
+++ components/enterprise/client_certificates/android/browser_binding/browser_key_android.cc
@@ -18,19 +18,14 @@
 #include "base/check.h"
 #include "base/containers/flat_set.h"
 #include "base/logging.h"
+#include "crypto/evp.h"
 #include "net/android/keystore.h"
 #include "net/base/net_errors.h"
 #include "net/ssl/ssl_platform_key_android.h"
 #include "net/ssl/ssl_platform_key_util.h"
 #include "net/ssl/ssl_private_key.h"
 #include "net/ssl/threaded_ssl_private_key.h"
-#include "third_party/boringssl/src/include/openssl/bytestring.h"
-#include "third_party/boringssl/src/include/openssl/ecdsa.h"
 #include "third_party/boringssl/src/include/openssl/evp.h"
-#include "third_party/boringssl/src/include/openssl/mem.h"
-#include "third_party/boringssl/src/include/openssl/nid.h"
-#include "third_party/boringssl/src/include/openssl/rsa.h"
-#include "third_party/boringssl/src/include/openssl/ssl.h"
 #include "third_party/jni_zero/jni_zero.h"
 
 // Must come after all headers that sepcialize ToJniType()/FromJniType()
@@ -63,16 +58,13 @@ std::vector<uint8_t> BrowserKeyAndroid::GetPublicKeyAsSPKI() const {
 scoped_refptr<net::SSLPrivateKey> BrowserKeyAndroid::GetSSLPrivateKey() const {
   JNIEnv* env = jni_zero::AttachCurrentThread();
 
-  std::vector<uint8_t> spki_bytes = GetPublicKeyAsSPKI();
-  if (spki_bytes.empty()) {
+  std::vector<uint8_t> spki = GetPublicKeyAsSPKI();
+  if (spki.empty()) {
     return nullptr;
   }
 
   // Use BoringSSL's public API to parse the SPKI DER bytes.
-  CBS cbs;
-  CBS_init(&cbs, spki_bytes.data(), spki_bytes.size());
-  bssl::UniquePtr<EVP_PKEY> public_key(EVP_parse_public_key(&cbs));
-
+  bssl::UniquePtr<EVP_PKEY> public_key = crypto::evp::PublicKeyFromBytes(spki);
   if (!public_key) {
     return nullptr;
   }
