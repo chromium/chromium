54396d199b21c8fe780459fc62c6ada3e15ddcb3
Joey Arhar <jarhar@chromium.org>
2025-09-24 10:24:03 -0700

    Relax blur conditions when removing href attribute
    Removing the href attribute from an anchor element was almost always
    causing it to be blurred, even if it is still focusable. This patch
    changes the logic to consult focusability before blurring.
    
    Fixed: 443585794
    Change-Id: Id9bef50d4d5db8b266464c595e8b2e56dc75cc61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6944690
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520081}

--- third_party/blink/renderer/core/html/html_anchor_element.cc
+++ third_party/blink/renderer/core/html/html_anchor_element.cc
@@ -250,8 +250,17 @@ void HTMLAnchorElementBase::AttributeChanged(
     return;
   if (params.name != html_names::kHrefAttr)
     return;
-  if (!IsLink() && AdjustedFocusedElementInTreeScope() == this)
+  if (RuntimeEnabledFeatures::LinkBlurImprovementEnabled()) {
+    if (!IsLink() && AdjustedFocusedElementInTreeScope() == this) {
+      // Removing the href attribute might make this element no longer
+      // focusable, but other attributes like tabindex can keep it focusable.
+      // Style and layout are needed in order to check focusability.
+      GetDocument().UpdateStyleAndLayoutTree();
+      GetDocument().ClearFocusedElementIfNeeded();
+    }
+  } else if (!IsLink() && AdjustedFocusedElementInTreeScope() == this) {
     blur();
+  }
 }
 
 void HTMLAnchorElementBase::ParseAttribute(
