6309d6a6d9f5b55c7380fa25a4b90027d62d3229
Michael Checo <michaelcheco@google.com>
2025-09-25 14:53:54 -0700

    boca: Enable courseWorkMaterials API flag by default
    Change-Id: Ie05a98ac29bb1dffdc506a6ccf53d1c998ad1abd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986106
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Auto-Submit: Michael Checo <michaelcheco@google.com>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1520897}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -271,7 +271,7 @@ constexpr base::FeatureParam<int> kBocaMaxNumStudentsAllowed{
     &kBocaConfigureMaxStudents, "BocaMaxNumStudentsAllowed", 100};
 
 // Enables or disables use of the courseWorkMaterials API in the Boca app.
-BASE_FEATURE(kBocaCourseWorkMaterialApi, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kBocaCourseWorkMaterialApi, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables or disables sharing teacher's screen in the Boca app.
 BASE_FEATURE(kBocaScreenSharingTeacher, base::FEATURE_DISABLED_BY_DEFAULT);
