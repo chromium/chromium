eacd296a45fa37d71d4bee39f17696b3fbb09cee
Chris Fredrickson <cfredric@chromium.org>
2025-09-24 21:27:20 -0700

    [SAA] Clean up stale SAH OT prefs on disk
    We'll leave this cleanup in place for a few milestones before removing
    the ContentSettingType entirely.
    
    Bug: 332335089
    Change-Id: I4d76ab9580dac9def0d8c1f97acafdb27280da5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6979385
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520202}

--- chrome/browser/storage_access_api/storage_access_api_service_impl.cc
+++ chrome/browser/storage_access_api/storage_access_api_service_impl.cc
@@ -2,15 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/storage_access_api/storage_access_api_service_impl.h"
+
 #include <utility>
 
 #include "base/check.h"
 #include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
-#include "chrome/browser/storage_access_api/storage_access_api_service_impl.h"
+#include "chrome/browser/profiles/profile.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/content_settings/core/browser/website_settings_info.h"
 #include "components/content_settings/core/common/content_settings.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/storage_partition.h"
 #include "third_party/blink/public/common/features.h"
@@ -32,6 +36,29 @@ StorageAccessAPIServiceImpl::StorageAccessAPIServiceImpl(
       FROM_HERE, kTimerPeriod,
       base::BindRepeating(&StorageAccessAPIServiceImpl::OnPeriodicTimerFired,
                           weak_ptr_factory_.GetWeakPtr()));
+
+  Profile* profile = Profile::FromBrowserContext(browser_context);
+  if (!profile) {
+    return;
+  }
+  PrefService* prefs = profile->GetPrefs();
+  if (!prefs) {
+    return;
+  }
+  // None of the parameters matter here except the type and the name; they must
+  // match the usage at
+  // https://crsrc.org/c/components/content_settings/core/browser/content_settings_registry.cc;drc=e8fbdf97bea0f7e89f2b12f4127bb651878b8660;l=348.
+  content_settings::WebsiteSettingsInfo info(
+      ContentSettingsType::STORAGE_ACCESS_HEADER_ORIGIN_TRIAL,
+      "storage-access-header-origin-trial", base::Value(CONTENT_SETTING_BLOCK),
+      content_settings::WebsiteSettingsInfo::UNSYNCABLE,
+      content_settings::WebsiteSettingsInfo::NOT_LOSSY,
+      content_settings::WebsiteSettingsInfo::
+          REQUESTING_ORIGIN_AND_TOP_SCHEMEFUL_SITE_SCOPE,
+      content_settings::WebsiteSettingsInfo::INHERIT_IN_INCOGNITO);
+
+  prefs->ClearPref(info.pref_name());
+  prefs->ClearPref(info.default_value_pref_name());
 }
 
 StorageAccessAPIServiceImpl::~StorageAccessAPIServiceImpl() = default;
