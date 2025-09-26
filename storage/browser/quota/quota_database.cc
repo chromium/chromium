c8ebe59118907e98a1e8f3e4d2452115634c030a
Ari Chivukula <arichiv@chromium.org>
2025-09-26 06:27:06 -0700

    [Quota] (1/3) Remove support for versions 7 and 8
    These are both over 2 years old so can be reaped. Technically we can
    remove v9 as well, but that would remove all code that validates the
    migration path so let's keep that for now.
    
    This CL is part of a series:
    (1/3) Remove support for versions 7 and 8
    (2/3) Add new db version (11) that drops unused type field
    (3/3) Remove support for version 9
    
    OBSOLETE_HISTOGRAMS=Removed in http://crrev.com/c/6979204 along with support for versions 7 and 8.
    
    Bug: 447037096
    Change-Id: Id6305d4442f9a48e5357f3bf120a0036d27d174c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6979204
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521230}

--- storage/browser/quota/quota_database.cc
+++ storage/browser/quota/quota_database.cc
@@ -52,8 +52,8 @@ static const int kStaleBucketCutoffInDays = 400;
 // Version 4 - 2011-10-17 - http://crrev.com/105822 (unsupported)
 // Version 5 - 2015-10-19 - https://crrev.com/354932 (unsupported)
 // Version 6 - 2021-04-27 - https://crrev.com/c/2757450 (unsupported)
-// Version 7 - 2021-05-20 - https://crrev.com/c/2910136
-// Version 8 - 2021-09-01 - https://crrev.com/c/3119831
+// Version 7 - 2021-05-20 - https://crrev.com/c/2910136 (unsupported)
+// Version 8 - 2021-09-01 - https://crrev.com/c/3119831 (unsupported)
 // Version 9 - 2022-05-13 - https://crrev.com/c/3601253
 // Version 10 - 2023-04-10 - https://crrev.com/c/4412082
 //
