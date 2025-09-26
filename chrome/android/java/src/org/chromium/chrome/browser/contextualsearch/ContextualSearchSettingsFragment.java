35f2cf2bb93d4edc3bf5651388bf949268a56da9
Gazal <agazal@google.com>
2025-09-26 12:11:53 -0700

    [Settings containment] Reduce text size for summaries
    Do no track: https://screenshot.googleplex.com/4FepTayTi2ejZTz.png
    Touch to search: https://screenshot.googleplex.com/BmDj4Jk4uNWj5Ei.png
    
    Bug: 433576895, 439725777
    Change-Id: I38e11e90d92278bba33431e89f261cd5c488a2f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6986515
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521465}

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchSettingsFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchSettingsFragment.java
@@ -10,15 +10,18 @@ import androidx.preference.Preference;
 
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
+import org.chromium.build.NullUtil;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
 import org.chromium.chrome.browser.settings.ChromeManagedPreferenceDelegate;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.SettingsFragment;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
+import org.chromium.components.browser_ui.settings.TextMessagePreference;
 
 /**
  * Fragment to manage the Contextual Search Settings in Chrome Settings, and to explain to the user
@@ -27,6 +30,7 @@ import org.chromium.components.browser_ui.settings.SettingsUtils;
 @NullMarked
 public class ContextualSearchSettingsFragment extends ChromeBaseSettingsFragment {
     static final String PREF_CONTEXTUAL_SEARCH_SWITCH = "contextual_search_switch";
+    static final String PREF_CONTEXTUAL_SEARCH_DESCRIPTION = "contextual_search_description";
     static final String PREF_WAS_FULLY_ENABLED_SWITCH = "see_better_results_switch";
 
     private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
@@ -37,6 +41,15 @@ public class ContextualSearchSettingsFragment extends ChromeBaseSettingsFragment
         mPageTitle.set(getString(R.string.contextual_search_title));
         setHasOptionsMenu(true);
         initSwitches();
+
+        if (ChromeFeatureList.sAndroidSettingsContainment.isEnabled()) {
+            // TODO(crbug.com/439911511): Set the summary instead of the title in the layout file.
+            TextMessagePreference contextualSearchDescription =
+                    findPreference(PREF_CONTEXTUAL_SEARCH_DESCRIPTION);
+            NullUtil.assertNonNull(contextualSearchDescription)
+                    .setSummary(contextualSearchDescription.getTitle());
+            contextualSearchDescription.setTitle(null);
+        }
     }
 
     @Override
