3b0206afef100331a83231e3306e5a28b749b541
Sky Malice <skym@chromium.org>
2025-09-25 23:46:24 -0700

    Revert "[Persist] Maintain an in memory map of handles to storage ids."
    This reverts commit 10ed6a77540a524d308f934ce196806c49e2be6d.
    
    Reason for revert: iOS compile error.
    
    Original change's description:
    > [Persist] Maintain an in memory map of handles to storage ids.
    >
    > Bug: 427254267
    > Change-Id: I1882c7b84e747531e046509461f67cd6c0c400e6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6965952
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1520708}
    
    Bug: 427254267, 447492422
    
    Change-Id: I56f4492259040c9db0dbd27580bbde4428a1729c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6985934
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1521064}

--- chrome/browser/tab/tab_state_storage_service.cc
+++ chrome/browser/tab/tab_state_storage_service.cc
@@ -5,34 +5,8 @@
 #include "chrome/browser/tab/tab_state_storage_service.h"
 
 #include "base/token.h"
-#include "chrome/browser/tab/protocol/children.pb.h"
-#include "components/tabs/public/direct_child_walker.h"
-#include "components/tabs/public/tab_collection.h"
-#include "components/tabs/public/tab_interface.h"
 
 namespace tabs {
-
-class TabStateStorageService::SaveProcessor
-    : public DirectChildWalker::Processor {
- public:
-  SaveProcessor(TabStateStorageService* service,
-                tabs_pb::Children* children_proto)
-      : service_(service), children_proto_(children_proto) {}
-
-  void ProcessTab(const TabInterface* tab) override {
-    children_proto_->add_storage_id(service_->GetOrCreateStorageId(
-        tab, service_->tab_handle_to_storage_id_));
-  }
-
-  void ProcessCollection(const TabCollection* collection) override {
-    children_proto_->add_storage_id(service_->GetOrCreateStorageId(
-        collection, service_->collection_handle_to_storage_id_));
-  }
-
- private:
-  raw_ptr<TabStateStorageService> service_;
-  raw_ptr<tabs_pb::Children> children_proto_;
-};
 
 TabStateStorageService::TabStateStorageService(
     std::unique_ptr<TabStateStorageBackend> tab_backend)
@@ -83,11 +57,6 @@ void TabStateStorageService::SaveTab(
   tab_state.set_is_pinned(is_pinned);
   std::string payload;
   tab_state.SerializeToString(&payload);
-  // TODO(https://crbug.com/427254267): Using Java Tab id for storage id is now
-  // a completely invalid approach and will conflict with storage ids generated
-  // for the collections objects. This method needs to be reworked to take a
-  // collections object instead.
-  // TODO(https://crbug.com/427254267): Create enum for types.
   tab_backend_->SaveNode(id, 1, std::move(payload), "");
 }
 
@@ -99,13 +68,8 @@ void TabStateStorageService::LoadAllTabs(LoadAllTabsCallback callback) {
 
 void TabStateStorageService::OnAllTabsLoaded(LoadAllTabsCallback callback,
                                              std::vector<NodeState> entries) {
-  int max_id = 0;
   std::vector<tabs_pb::TabState> tab_states;
   for (auto& entry : entries) {
-    if (entry.id > max_id) {
-      max_id = entry.id;
-    }
-    // TODO(https://crbug.com/427254267): Create enum for types.
     if (entry.type == 1) {
       tabs_pb::TabState tab_state;
       if (tab_state.ParseFromString(entry.payload)) {
@@ -113,40 +77,7 @@ void TabStateStorageService::OnAllTabsLoaded(LoadAllTabsCallback callback,
       }
     }
   }
-  // TODO(https://crbug.com/427254267): While this avoids collisions and does
-  // set up a mapping for live Tab objects from Java, doesn't interact nicely
-  // with collections objects mappings. Ideally this will be fully replaced as
-  // soon as we can create and pass up collections objects on start up.
-  next_storage_id_ = max_id + 1;
   std::move(callback).Run(std::move(tab_states));
-}
-
-void TabStateStorageService::Save(const tabs::TabCollection* tab_collection) {
-  tabs_pb::Children children_proto;
-  SaveProcessor processor(this, &children_proto);
-  DirectChildWalker walker(tab_collection, &processor);
-  walker.Walk();
-
-  int storage_id =
-      GetOrCreateStorageId(tab_collection, collection_handle_to_storage_id_);
-  std::string children_string;
-  children_proto.SerializeToString(&children_string);
-  // TODO(https://crbug.com/427254267): Write payload for metadata about
-  // collections object once extractor mechaism exists.
-  // TODO(https://crbug.com/427254267): Create enum for types.
-  tab_backend_->SaveNode(storage_id, 2, "", std::move(children_string));
-}
-
-template <typename T>
-int TabStateStorageService::GetOrCreateStorageId(
-    T* object,
-    std::map<int32_t, int>& handle_map) {
-  int32_t handle_id = object->GetHandle().raw_value();
-  auto [it, inserted] = handle_map.try_emplace(handle_id, next_storage_id_);
-  if (inserted) {
-    next_storage_id_++;
-  }
-  return it->second;
 }
 
 }  // namespace tabs
