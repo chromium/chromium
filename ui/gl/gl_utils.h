e7680727cb5978508a36710c49b985b3ce5813c8
Colin Blundell <blundell@chromium.org>
2025-09-26 00:34:20 -0700

    [//gpu] Remove disable_es3gl_context_for_testing
    Unused.
    
    Bug: 444049511
    Change-Id: I32426df241aea5bc390c1ada646d23378717428d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6983291
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1521086}

--- ui/gl/gl_utils.h
+++ ui/gl/gl_utils.h
@@ -45,7 +45,6 @@ struct GlWorkarounds {
   bool disable_d3d11 = false;
   bool disable_metal = false;
   bool disable_es3gl_context = false;
-  bool disable_es3gl_context_for_testing = false;
   bool disable_direct_composition_video_overlays = false;
   bool disable_vp_auto_hdr = false;
 };
