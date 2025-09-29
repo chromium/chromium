127bb9898f615543523cc1e4d9633e05389bf90c
Ashutosh Singhal <macinashutosh@google.com>
2025-09-29 12:05:53 -0700

    [Fix] Use WeakPtr in NavigationWaiter to prevent crash [M138]
    Replaces `base::Unretained` with a `base::WeakPtr` for the callback
    passed to NavigationWaiter.
    This fixes a use-after-free crash that occurred when a
    KioskBrowserWindowHandler was destroyed before the navigation callback
    could be run. The weak pointer ensures the callback is safely
    invalidated.
    
    (cherry picked from commit 0475dc5655a706020c913942336e91e18ea1936a)
    
    (cherry picked from commit 8e151c03f493461a11e822cf6e239df907f65cb3)
    
    Bug: b:439563444
    Change-Id: I42b96a4e60a8e6484eb1fbe4f2de1b984d04dbde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6874448
    Commit-Queue: Ashutosh Singhal <macinashutosh@google.com>
    Reviewed-by: Sergii Bykov <sbykov@google.com>
    Cr-Original-Original-Commit-Position: refs/heads/main@{#1504926}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6918962
    Cr-Original-Commit-Position: refs/branch-heads/7204@{#3417}
    Cr-Original-Branched-From: d5de512dc9dc8ddfe4e6d71b0637578bb6158683-refs/heads/main@{#1465706}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6991554
    Auto-Submit: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Ashutosh Singhal <macinashutosh@google.com>
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Owners-Override: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7204_184@{#63}
    Cr-Branched-From: 7ea839044480a944888296dc0cccc5afb60b736c-refs/branch-heads/7204@{#2436}
    Cr-Branched-From: d5de512dc9dc8ddfe4e6d71b0637578bb6158683-refs/heads/main@{#1465706}

--- chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.cc
+++ chrome/browser/chromeos/app_mode/kiosk_browser_window_handler.cc
@@ -263,12 +263,14 @@ void KioskBrowserWindowHandler::OnCompleteBrowserAdded(Browser* browser) {
   // This URL is required for our triaging, so we'll wait for it.
   url_waiters_[browser] = std::make_unique<NavigationWaiter>(
       browser, base::BindOnce(
-                   [](KioskBrowserWindowHandler* self, Browser* browser) {
-                     if (self->TriageNewBrowserWindow(browser)) {
-                       browser->window()->Show();
-                     }
-                   },
-                   base::Unretained(this), base::Unretained(browser)));
+                   &KioskBrowserWindowHandler::OnBrowserNavigationStarted,
+                   weak_ptr_factory_.GetWeakPtr(), base::Unretained(browser)));
+}
+
+void KioskBrowserWindowHandler::OnBrowserNavigationStarted(Browser* browser) {
+  if (TriageNewBrowserWindow(browser)) {
+    browser->window()->Show();
+  }
 }
 
 void KioskBrowserWindowHandler::OnBrowserRemoved(Browser* browser) {
