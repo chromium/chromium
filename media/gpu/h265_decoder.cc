8a2c90940f662830f1a3c11cb0fa7c442b363e29
Eugene Zemtsov <eugene@chromium.org>
2025-09-29 12:59:34 -0700

    Remove unneeded allow_unsafe_buffers
    Bug: 40285824
    Change-Id: I5d726c394211ca90a84293abb7f2341201b28447
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994327
    Auto-Submit: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1522434}

--- media/gpu/h265_decoder.cc
+++ media/gpu/h265_decoder.cc
@@ -1,12 +1,6 @@
 // Copyright 2020 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "media/gpu/h265_decoder.h"
 
 #include <algorithm>
