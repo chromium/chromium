f2c5d96f3c89e0c29c3b915eaea77c0895609373
Nikhil Jakhar <nikhiljakhar@google.com>
2025-09-30 01:05:43 -0700

    legion: Introduce a basic interface for transport layer
    A basic Transport layer interface that will be used by Secure Channel Layer.
    
    Bug: b:444610593
    
    Change-Id: Ide8b8f1e372177f03dbcf5c6226dcf9c952bba07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6993939
    Commit-Queue: Nikhil Jakhar <nikhiljakhar@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Mohamad Arab <arabm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1522714}

---
+++ components/legion/transport.h
@@ -0,0 +1,46 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_LEGION_TRANSPORT_H_
+#define COMPONENTS_LEGION_TRANSPORT_H_
+
+#include <optional>
+#include <vector>
+
+#include "base/functional/callback.h"
+#include "base/types/expected.h"
+
+namespace legion {
+
+// Interface for the Transport Layer.
+// Responsible for raw connection and data transfer.
+class Transport {
+ public:
+  enum class TransportError {
+    // Socket was closed by the server.
+    kSocketClosed,
+    // An error occurred on the client. Socket is now closed.
+    kError,
+  };
+  using Request = std::vector<uint8_t>;
+  using Response = std::vector<uint8_t>;
+
+  // Callback for when a send operation completes or the status of the
+  // connection changes.
+  using MessageCallback =
+      base::RepeatingCallback<void(base::expected<Response, TransportError>)>;
+
+  virtual ~Transport() = default;
+
+  // Asynchronously sends data to the server.
+  // The transport implementation will handle connection management.
+  // Results, responses, and status updates related to this send or the
+  // connection itself will be delivered via the MessageCallback
+  // that the concrete implementation was constructed with.
+  virtual void Send(Request request) = 0;
+};
+
+}  // namespace legion
+
+#endif  // COMPONENTS_LEGION_TRANSPORT_H_
