22bbf1d3b46a46747d777e7122ed938994b1db5e
Jonathan Lee <jonathanjlee@google.com>
2025-09-30 15:17:57 -0700

    Import wpt@ed2e8d002f3d7081d669b82ae23fe79bb19b0afa
    https://github.com/web-platform-tests/wpt/compare/91580c2bb...ed2e8d002
    
    Using wpt-import in Chromium e61238586e964cf6c0982344bbafdc2742ce7767.
    With Chromium commits locally applied on WPT:
      e3e2322e54 "Explicit align-self makes last-successful tests pass [...]
      468e313cf6 "[BackdropFilter] Create a backdrop root for clip [...]
      bf882df84a "[SAA] Fix test failures due to default cookie [...]
      844f0be7b6 "Fix document.caretPositionFromPoint().offset for [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    dcheng@chromium.org, jarhar@chromium.org:
      external/wpt/domxpath
    dgrogan@chromium.org:
      external/wpt/css/css-tables
    domenic@chromium.org:
      external/wpt/html/browsers/origin/origin-keyed-agent-clusters
    domenic@chromium.org, hiroshige@chromium.org:
      external/wpt/import-maps
    domenic@chromium.org, ricea@chromium.org:
      external/wpt/streams
    foolip@chromium.org:
      external/wpt/infrastructure
    jfernandez@igalia.com:
      external/wpt/css/css-align
    jkummerow@chromium.org, clemensb@chromium.org, ahaas@chromium.org:
      external/wpt/wasm
    kozyatinskiy@chromium.org:
      external/wpt/console
    mkwst@chromium.org:
      external/wpt/x-frame-options
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    vogelheim@chromium.org:
      external/wpt/trusted-types
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I1a37dc47281ef017157dcd5dc11558bddc33461e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6997209
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523184}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browser/set_download_behavior/download_behavior_allowed.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/browser/set_download_behavior/download_behavior_allowed.py
@@ -15,17 +15,6 @@ async def test_allow_and_reset(bidi_session, new_tab, temp_dir,
     assert await is_download_allowed(new_tab) == default_is_download_allowed
 
 
-async def test_allow_and_reset_no_destination_folder(bidi_session, new_tab,
-        is_download_allowed, default_is_download_allowed):
-    await bidi_session.browser.set_download_behavior(download_behavior={
-        "type": "allowed"
-    })
-    assert await is_download_allowed(new_tab) == True
-
-    await bidi_session.browser.set_download_behavior(download_behavior=None)
-    assert await is_download_allowed(new_tab) == default_is_download_allowed
-
-
 async def test_destination_folder(bidi_session, new_tab, temp_dir,
         trigger_download):
     await bidi_session.browser.set_download_behavior(download_behavior={
