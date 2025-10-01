9bdd1b216e7c468bf3baad36042a69d46d86f4fd
Ireneusz Szulc <ireneuszszulc@google.com>
2025-10-01 01:34:23 -0700

    [Settings/Autofill] Add basic data type counters to Your Saved Info
    This change introduces new counters to be displayed on Your Saved
    Info settings page:
    - number of addresses,
    - number of credit cards,
    - number of IBANs,
    - number of Pay over time issuers (Buy Now Pay Later).
    
    This is part of the Home of Transactions project, which aims to create a
    central "Your saved info" page in settings.
    
    The new counters are fetched from the existing AutofillManager
    and PaymentsManager. The counts are updated in real-time to reflect
    the changes from other tabs or sync.
    
    This change only makes these counters available on 'Your saved info'
    page as properties. They will be used and displayed once the data chips
    components are ready.
    
    Bug: 448349516
    Change-Id: I5b3d3e9db5c4a8eb21bd13536c2756d20a959bd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994310
    Reviewed-by: Stanislav Mikheyev <mikhe@google.com>
    Reviewed-by: Julia Sobiech <jsobiech@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523399}

--- chrome/browser/resources/settings/your_saved_info_page/your_saved_info_page.ts
+++ chrome/browser/resources/settings/your_saved_info_page/your_saved_info_page.ts
@@ -12,6 +12,10 @@ import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
 import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {PaymentsManagerImpl} from '../autofill_page/payments_manager_proxy.js';
+import type {PaymentsManagerProxy} from '../autofill_page/payments_manager_proxy.js';
+import type {AutofillManagerProxy, PersonalDataChangedListener} from '../autofill_page/autofill_manager_proxy.js';
+import {AutofillManagerImpl} from '../autofill_page/autofill_manager_proxy.js';
 import {SettingsViewMixin} from '../settings_page/settings_view_mixin.js';
 
 import {SavedInfoHandlerImpl} from './saved_info_handler_proxy.js';
@@ -37,12 +41,26 @@ export class SettingsYourSavedInfoPageElement extends
 
       passwordsCount: Number,
       passkeysCount: Number,
+      addressesCount: Number,
+      creditCardsCount: Number,
+      ibansCount: Number,
+      payOverTimeIssuersCount: Number,
     };
   }
 
   declare prefs: {[key: string]: any};
   declare passwordsCount: number|undefined;
   declare passkeysCount: number|undefined;
+  declare addressesCount: number|undefined;
+  declare creditCardsCount: number|undefined;
+  declare ibansCount: number|undefined;
+  declare payOverTimeIssuersCount: number|undefined;
+
+  private paymentsManager_: PaymentsManagerProxy =
+      PaymentsManagerImpl.getInstance();
+  private autofillManager_: AutofillManagerProxy =
+      AutofillManagerImpl.getInstance();
+  private setPersonalDataListener_: PersonalDataChangedListener|null = null;
 
   override connectedCallback() {
     super.connectedCallback();
@@ -59,6 +77,56 @@ export class SettingsYourSavedInfoPageElement extends
     this.addWebUiListener('password-count-changed', setPasswordCount);
     SavedInfoHandlerImpl.getInstance().getPasswordCount().then(
       setPasswordCount);
+
+    // Addresses: Request initial data.
+    const setAddressesListener =
+      (addresses: chrome.autofillPrivate.AddressEntry[]) => {
+        this.addressesCount = addresses.length;
+      };
+    this.autofillManager_.getAddressList().then(setAddressesListener);
+
+    // Payments: Request initial data.
+    const setCreditCardsListener =
+      (creditCards: chrome.autofillPrivate.CreditCardEntry[]) => {
+      this.creditCardsCount = creditCards.length;
+    };
+    const setIbansListener = (ibans: chrome.autofillPrivate.IbanEntry[]) => {
+      this.ibansCount = ibans.length;
+    };
+    const setPayOverTimeListener =
+      (payOverTimeIssuers: chrome.autofillPrivate.PayOverTimeIssuerEntry[]) => {
+      this.payOverTimeIssuersCount = payOverTimeIssuers.length;
+    };
+    this.paymentsManager_.getCreditCardList().then(setCreditCardsListener);
+    this.paymentsManager_.getIbanList().then(setIbansListener);
+    this.paymentsManager_.getPayOverTimeIssuerList().then(
+      setPayOverTimeListener);
+
+    // Addresses and Payments: Listen for changes.
+    const setPersonalDataListener: PersonalDataChangedListener =
+      (addresses: chrome.autofillPrivate.AddressEntry[],
+        creditCards: chrome.autofillPrivate.CreditCardEntry[],
+        ibans: chrome.autofillPrivate.IbanEntry[],
+        payOverTimeIssuers: chrome.autofillPrivate.PayOverTimeIssuerEntry[],
+        _accountInfo?: chrome.autofillPrivate.AccountInfo) => {
+        this.addressesCount = addresses.length;
+        this.creditCardsCount = creditCards.length;
+        this.ibansCount = ibans.length;
+        this.payOverTimeIssuersCount = payOverTimeIssuers.length;
+      };
+    this.setPersonalDataListener_ = setPersonalDataListener;
+    this.autofillManager_.setPersonalDataManagerListener(
+      setPersonalDataListener);
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+
+    if (this.setPersonalDataListener_) {
+      this.autofillManager_.removePersonalDataManagerListener(
+          this.setPersonalDataListener_);
+      this.setPersonalDataListener_ = null;
+    }
   }
 }
 
--- chrome/test/data/webui/settings/your_saved_info_page_test.ts
+++ chrome/test/data/webui/settings/your_saved_info_page_test.ts
@@ -5,20 +5,59 @@
 import 'chrome://settings/settings.js';
 
 import type {SettingsYourSavedInfoPageElement} from 'chrome://settings/settings.js';
-import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {AutofillManagerImpl, PaymentsManagerImpl} from 'chrome://settings/lazy_load.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+
+import {createAddressEntry, createCreditCardEntry, createIbanEntry, createPayOverTimeIssuerEntry, TestAutofillManager, TestPaymentsManager} from './autofill_fake_data.js';
 
 suite('YourSavedInfoPage', function() {
   let yourSavedInfoPage: SettingsYourSavedInfoPageElement;
+  let autofillManager: TestAutofillManager;
+  let paymentsManager: TestPaymentsManager;
 
-  setup(function() {
+  setup(async function() {
+    autofillManager = new TestAutofillManager();
+    autofillManager.data.addresses = [createAddressEntry()];
+    AutofillManagerImpl.setInstance(autofillManager);
+    paymentsManager = new TestPaymentsManager();
+    PaymentsManagerImpl.setInstance(paymentsManager);
+
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     yourSavedInfoPage = document.createElement('settings-your-saved-info-page');
     document.body.appendChild(yourSavedInfoPage);
+    await flushTasks();
   });
 
   test('TitleExists', function() {
     const yourSavedInfoPageTitleElement =
         yourSavedInfoPage.shadowRoot!.querySelector('#yourSavedInfoPageTitle');
     assertTrue(!!yourSavedInfoPageTitleElement);
+  });
+
+  test('AddressesAndPaymentsCountersAreUpdated', async function() {
+    await autofillManager.whenCalled('getAddressList');
+    await paymentsManager.whenCalled('getCreditCardList');
+    await paymentsManager.whenCalled('getIbanList');
+    await paymentsManager.whenCalled('getPayOverTimeIssuerList');
+
+    assertEquals(1, yourSavedInfoPage.addressesCount);
+    assertEquals(0, yourSavedInfoPage.creditCardsCount);
+    assertEquals(0, yourSavedInfoPage.ibansCount);
+    assertEquals(0, yourSavedInfoPage.payOverTimeIssuersCount);
+
+    const addressList = [createAddressEntry(), createAddressEntry()];
+    const cardList = [createCreditCardEntry()];
+    const ibanList = [createIbanEntry(), createIbanEntry(), createIbanEntry()];
+    const payOverTimeIssuerList = [createPayOverTimeIssuerEntry()];
+
+    autofillManager.lastCallback.setPersonalDataManagerListener!
+        (addressList, cardList, ibanList, payOverTimeIssuerList);
+    await flushTasks();
+
+    assertEquals(2, yourSavedInfoPage.addressesCount);
+    assertEquals(1, yourSavedInfoPage.creditCardsCount);
+    assertEquals(3, yourSavedInfoPage.ibansCount);
+    assertEquals(1, yourSavedInfoPage.payOverTimeIssuersCount);
   });
 });
