a1cf9ec97a62fa04e2d84b241f2bccdf6542a099
Colin Blundell <blundell@chromium.org>
2025-10-01 00:02:33 -0700

    [//gpu] Remove DiscardableHandle::IsDeletedForTracing
    No longer used post-OOP-C.
    
    Bug: 391648152
    Change-Id: I46b09dba72ae65b9160322c61eff1f8062880204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998028
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523351}

--- gpu/command_buffer/common/discardable_handle.h
+++ gpu/command_buffer/common/discardable_handle.h
@@ -54,9 +54,6 @@ class GPU_COMMAND_BUFFER_COMMON_EXPORT DiscardableHandleBase {
   // Ensures this is a valid allocation for use with a DiscardableHandleBase.
   static bool ValidateParameters(const Buffer* buffer, uint32_t byte_offset);
 
-  // Functions for tracing only.
-  bool IsDeletedForTracing() const;
-
   // Test only functions.
   bool IsLockedForTesting() const;
   bool IsDeletedForTesting() const;
