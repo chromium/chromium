5814cfb655e496470c55be867a9acfd34a895fe1
Le Hoang Quyen <lehoangquyen@chromium.org>
2025-10-01 07:17:02 -0700

    gpu: Add unit tests for GpuPersistentCache
    Bug: 399642827
    Change-Id: Ide8470d5295f68330fab7d0719c3f93ca5a56bc8
    
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998538
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Quyen Le <lehoangquyen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1523530}

---
+++ gpu/command_buffer/service/gpu_persistent_cache_unittest.cc
@@ -0,0 +1,129 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gpu/command_buffer/service/gpu_persistent_cache.h"
+
+#include "base/barrier_closure.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_temp_dir.h"
+#include "base/run_loop.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/task/thread_pool.h"
+#include "base/test/task_environment.h"
+#include "components/persistent_cache/sqlite/vfs/sandboxed_file.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace gpu {
+
+class GpuPersistentCacheTest : public testing::Test {
+ public:
+  void SetUp() override {
+    ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
+    auto db_path = temp_dir_.GetPath().AppendASCII("test.db");
+    auto journal_path = temp_dir_.GetPath().AppendASCII("test.journal");
+
+    base::File db_file = CreateFile(db_path);
+    base::File journal_file = CreateFile(journal_path);
+    auto shared_lock = base::UnsafeSharedMemoryRegion::Create(
+        sizeof(persistent_cache::LockState));
+    ASSERT_TRUE(db_file.IsValid());
+    ASSERT_TRUE(journal_file.IsValid());
+    ASSERT_TRUE(shared_lock.IsValid());
+
+    cache_.InitializeCache(std::move(db_file), std::move(journal_file),
+                           std::move(shared_lock));
+  }
+
+ protected:
+  base::File CreateFile(const base::FilePath& path) {
+    return base::File(path, base::File::FLAG_CREATE_ALWAYS |
+                                base::File::FLAG_READ | base::File::FLAG_WRITE);
+  }
+
+  base::test::TaskEnvironment task_environment_;
+  base::ScopedTempDir temp_dir_;
+  GpuPersistentCache cache_{"Test"};
+};
+
+// Tests basic store and load functionality on a single thread.
+TEST_F(GpuPersistentCacheTest, StoreAndLoadData) {
+  const std::string key = "my_key";
+  const std::string value = "my_value";
+  cache_.StoreData(key.c_str(), key.size(), value.c_str(), value.size());
+
+  std::vector<char> buffer(value.size());
+  size_t loaded_size =
+      cache_.LoadData(key.c_str(), key.size(), buffer.data(), buffer.size());
+
+  EXPECT_EQ(loaded_size, value.size());
+  EXPECT_EQ(std::string(buffer.begin(), buffer.end()), value);
+}
+
+// Tests that loading a non-existent key returns 0.
+TEST_F(GpuPersistentCacheTest, LoadNonExistentKey) {
+  const std::string key = "non_existent_key";
+  std::vector<char> buffer(16);
+  size_t loaded_size =
+      cache_.LoadData(key.c_str(), key.size(), buffer.data(), buffer.size());
+  EXPECT_EQ(loaded_size, 0u);
+}
+
+// Tests that the cache can be safely written to and read from by multiple
+// threads concurrently.
+TEST_F(GpuPersistentCacheTest, StoreAndLoadDataMultiThreaded) {
+  constexpr int kNumThreads = 8;
+  constexpr int kNumOperationsPerThread = 2;
+
+  base::RunLoop run_loop;
+  auto barrier = base::BarrierClosure(kNumThreads, run_loop.QuitClosure());
+
+  // Post tasks to multiple threads to store and immediately load data.
+  for (int i = 0; i < kNumThreads; ++i) {
+    base::ThreadPool::PostTask(
+        FROM_HERE, {base::MayBlock()},
+        base::BindOnce(
+            [](GpuPersistentCache* cache, int thread_id,
+               base::OnceClosure done_closure) {
+              for (int j = 0; j < kNumOperationsPerThread; ++j) {
+                std::string key = "key_" + base::NumberToString(thread_id) +
+                                  "_" + base::NumberToString(j);
+                std::string value = "value_" + base::NumberToString(thread_id) +
+                                    "_" + base::NumberToString(j);
+
+                cache->StoreData(key.c_str(), key.size(), value.c_str(),
+                                 value.size());
+
+                std::vector<char> buffer(value.size());
+                size_t loaded_size = cache->LoadData(
+                    key.c_str(), key.size(), buffer.data(), buffer.size());
+                ASSERT_EQ(loaded_size, value.size());
+                ASSERT_EQ(std::string(buffer.begin(), buffer.end()), value);
+              }
+              std::move(done_closure).Run();
+            },
+            &cache_, i, barrier));
+  }
+
+  // Wait for all threads to complete.
+  run_loop.Run();
+
+  // After all threads are done, verify from the main thread that all data is
+  // still present and correct. This ensures that writes from different threads
+  // did not corrupt each other's data.
+  for (int i = 0; i < kNumThreads; ++i) {
+    for (int j = 0; j < kNumOperationsPerThread; ++j) {
+      std::string key =
+          "key_" + base::NumberToString(i) + "_" + base::NumberToString(j);
+      std::string value =
+          "value_" + base::NumberToString(i) + "_" + base::NumberToString(j);
+      std::vector<char> buffer(value.size());
+      size_t loaded_size = cache_.LoadData(key.c_str(), key.size(),
+                                           buffer.data(), buffer.size());
+      EXPECT_EQ(loaded_size, value.size());
+      EXPECT_EQ(std::string(buffer.begin(), buffer.end()), value);
+    }
+  }
+}
+
+}  // namespace gpu
