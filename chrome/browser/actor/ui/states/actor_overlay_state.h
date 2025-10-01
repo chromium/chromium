6c14ab8be03d8d0c017177bb20bc780c1c2b3a7d
Abe Boujane <boujane@google.com>
2025-10-01 13:37:03 -0700

    Remove unnecessary constructor for OverlayState struct
    Bug: 447114657
    Change-Id: I7ab36e14d27f58005b49b825131e8dc719b09b34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002454
    Auto-Submit: Abe Boujane <boujane@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1523824}

--- chrome/browser/actor/ui/states/actor_overlay_state.h
+++ chrome/browser/actor/ui/states/actor_overlay_state.h
@@ -16,19 +16,13 @@ struct ActorOverlayState {
   // Whether or not the component is active.
   // This member is intended to be used alongside the relevant tab's visibility
   // status to determine whether or not the actor overlay should be shown.
-  bool is_active;
+  bool is_active = false;
   // A magic mouse click was triggered.
-  bool mouse_down;
+  bool mouse_down = false;
   // The target at which the magic mouse should be over.
   std::optional<gfx::Point> mouse_target = std::nullopt;
 
-  explicit ActorOverlayState(
-      bool is_active = false,
-      bool mouse_down = false,
-      std::optional<gfx::Point> mouse_target = std::nullopt);
-  ~ActorOverlayState();
-  ActorOverlayState(const ActorOverlayState&);
-  bool operator==(const ActorOverlayState& other) const;
+  bool operator==(const ActorOverlayState& other) const = default;
 };
 
 inline std::ostream& operator<<(std::ostream& os,
