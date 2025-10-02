981d6af1b9e498ecad6c6c4a990a05b8eeb5cf7a
Radu Nitescu <radunitescu@google.com>
2025-10-02 03:36:48 -0700

    [iOS] Implement custom bottom sheet logic for Lens Overlay
    Change-Id: I00a80ff947ee9ebaa77f16ca3cfa64b3cead03f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6999774
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524102}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_bottom_sheet_view_controller.mm
@@ -4,7 +4,95 @@
 
 #import "ios/chrome/browser/lens_overlay/ui/lens_overlay_bottom_sheet_view_controller.h"
 
-@implementation LensOverlayBottomSheetViewController
+#import <algorithm>
+#import <ostream>
+#import <utility>
+
+#import "base/ios/block_types.h"
+#import "base/notreached.h"
+#import "ios/chrome/browser/lens_overlay/model/lens_overlay_pan_tracker.h"
+#import "ios/chrome/browser/lens_overlay/ui/lens_overlay_panel.h"
+#import "ios/chrome/common/ui/util/constraints_ui_util.h"
+
+namespace {
+
+// The threshold that determines when the close sequence should start.
+const CGFloat kCloseBottomSheetThreshold = 200;
+
+// The minimum display height of the bottom sheet. Below this value, the bottom
+// sheet will start sliding down instead of resizing.
+const CGFloat kBottomSheetMinimumDisplayHeight = 100;
+
+// The deceleration of the outstanding sheet's motion just past its outermost
+// top (and optionally bottom) detent is modeled by a logarithmic growth
+// function that correlates the pan's vertical translation data with the actual
+// displacement of the sheet.
+//
+// This coefficient controls the rate of logarithmic growth; lowering it makes
+// the deceleration appear more aggressive.
+const CGFloat kRubberBandCoefficient = 8.0;
+
+// Approximate average time until the bottom sheet settles after user release.
+const CGFloat kTimeToSteadyStateAfterRelease = 0.3;
+
+// The identifier of the default detent.
+NSString* const kDefaultDetentIdentifier = @"kDefaultDetentIdentifier";
+
+}  // namespace
+
+// Delegates the touch response to another responder.
+@protocol ViewTouchDelegate
+
+// Indicates whether the given view contains the specified point.
+- (BOOL)respondToPointInside:(CGPoint)point
+                   withEvent:(UIEvent*)event
+                     forView:(UIView*)view;
+
+@end
+
+// View for which the touch transparecy can be controlled by a different
+@interface TouchDelegatingView : UIView
+
+// Delegate for this instance.
+@property(nonatomic, weak) id<ViewTouchDelegate> delegate;
+
+@end
+
+@implementation TouchDelegatingView
+
+- (BOOL)pointInside:(CGPoint)point withEvent:(UIEvent*)event {
+  return [_delegate respondToPointInside:point withEvent:event forView:self];
+}
+
+@end
+
+@interface LensOverlayBottomSheetViewController () <
+    LensOverlayPanTrackerDelegate,
+    ViewTouchDelegate>
+
+// The current height of the bottom sheet, in points.
+@property(nonatomic) CGFloat bottomSheetHeight;
+
+// Tracks the pan inside the bottom sheet.
+@property(nonatomic, readonly) LensOverlayPanTracker* panTracker;
+
+// Constraints for handling the bottom sheet vertical position.
+@property(nonatomic, strong) NSLayoutConstraint* bottomSheetHeightConstraint;
+@property(nonatomic, strong) NSLayoutConstraint* bottomSheetBottomConstraint;
+
+@end
+
+@implementation LensOverlayBottomSheetViewController {
+  // An array of unowned scroll views, modeled through provider blocks that
+  // capture weak references to the views in question.
+  NSMutableArray<UIScrollView* (^)()>* _disabledScrollViews;
+
+  // The bottom sheet container for the results page.
+  LensOverlayPanel* _bottomSheet;
+
+  // The default detent to be used if the list of detents was not overridden.
+  LensOverlayBottomSheetDetent* _defaultDetent;
+}
 
 @synthesize selectedDetentIdentifier = _selectedDetentIdentifier;
 @synthesize detentsDelegate = _detentsDelegate;
@@ -12,38 +100,401 @@
 @synthesize detents = _detents;
 
 - (instancetype)init {
-  return [super init];
+  self = [super init];
+  if (self) {
+    TouchDelegatingView* view = [[TouchDelegatingView alloc] init];
+    self.view = view;
+    view.delegate = self;
+    _disabledScrollViews = [[NSMutableArray alloc] init];
+
+    __weak __typeof(self) weakSelf = self;
+    _defaultDetent = [[LensOverlayBottomSheetDetent alloc]
+        initWithIdentifier:kDefaultDetentIdentifier
+             valueResolver:^{
+               return [weakSelf bottomSheetContainerHeight] / 2;
+             }];
+
+    _detents = @[ _defaultDetent ];
+  }
+
+  return self;
+}
+
+- (void)setDetents:(NSArray<LensOverlayBottomSheetDetent*>*)detents {
+  LensOverlayBottomSheetDetent* currentDetent =
+      [self bottomSheetRestDetentWithVelocity:0];
+  _detents = detents.count > 0 ? [detents copy] : @[ _defaultDetent ];
+
+  LensOverlayBottomSheetDetent* nextClosestDetent =
+      [self bottomSheetRestDetentWithVelocity:0];
+
+  if (currentDetent.identifier == nextClosestDetent.identifier) {
+    return;
+  }
+
+  [self setSelectedDetentIdentifier:currentDetent.identifier animated:YES];
 }
 
 - (void)setContent:(UIViewController*)contentViewController {
-  // NO-OP: Not implemented.
+  _bottomSheet = [[LensOverlayPanel alloc] initWithContent:contentViewController
+                                              insetContent:NO];
 }
 
 - (BOOL)isBottomSheetPresented {
-  return NO;
+  return _bottomSheet != nil;
 }
 
 - (void)presentAnimated:(BOOL)animated completion:(ProceduralBlock)completion {
-  // NO-OP: Not implemented.
-  if (completion) {
-    completion();
+  _bottomSheet.view.translatesAutoresizingMaskIntoConstraints = NO;
+  [self addChildViewController:_bottomSheet];
+  [self.view addSubview:_bottomSheet.view];
+
+  AddSameConstraintsToSides(_bottomSheet.view, self.view,
+                            (LayoutSides::kLeading | LayoutSides::kTrailing));
+
+  self.bottomSheetHeightConstraint =
+      [_bottomSheet.view.heightAnchor constraintEqualToConstant:0];
+  self.bottomSheetBottomConstraint = [_bottomSheet.view.bottomAnchor
+      constraintEqualToAnchor:self.view.bottomAnchor];
+
+  [NSLayoutConstraint activateConstraints:@[
+    self.bottomSheetHeightConstraint,
+    self.bottomSheetBottomConstraint,
+  ]];
+
+  _panTracker = [[LensOverlayPanTracker alloc] initWithView:_bottomSheet.view];
+  _panTracker.delegate = self;
+
+  [self.view layoutIfNeeded];
+
+  CGFloat restHeight = [self bottomSheetRestHeight];
+
+  if (!animated) {
+    [self.panTracker startTracking];
+    self.bottomSheetHeightConstraint.constant = restHeight;
+    if (completion) {
+      completion();
+    }
+    return;
   }
+
+  [self animateBottomSheetHeight:restHeight
+                      completion:^{
+                        [self.panTracker startTracking];
+                        if (completion) {
+                          completion();
+                        }
+                      }];
 }
 
 - (void)dismissAnimated:(BOOL)animated completion:(ProceduralBlock)completion {
-  // NO-OP: Not implemented.
-  if (completion) {
-    completion();
+  [_panTracker stopTracking];
+
+  if (!animated) {
+    if (completion) {
+      completion();
+    }
+    return;
   }
+
+  [self animateBottomSheetHeight:0
+                      completion:^{
+                        if (completion) {
+                          completion();
+                        }
+                      }];
 }
 
 - (void)setSelectedDetentIdentifier:(NSString*)selectedDetentIdentifier
                            animated:(BOOL)animated {
-  // NO-OP: Not implemented.
+  _selectedDetentIdentifier = selectedDetentIdentifier;
+  CGFloat restHeight = [self bottomSheetRestHeight];
+  if (!animated) {
+    self.bottomSheetHeight = restHeight;
+    return;
+  }
+
+  [_panTracker stopTracking];
+  [self animateBottomSheetHeight:restHeight
+                      completion:^{
+                        [self.panTracker startTracking];
+                      }];
+}
+
+- (void)animateBottomSheetHeight:(CGFloat)height
+                      completion:(ProceduralBlock)completion {
+  [UIView animateWithDuration:0.2
+      delay:0
+      usingSpringWithDamping:0.9
+      initialSpringVelocity:5
+      options:UIViewAnimationOptionCurveEaseInOut
+      animations:^{
+        self.bottomSheetHeightConstraint.constant = height;
+        [self.view layoutIfNeeded];
+      }
+      completion:^(BOOL) {
+        completion();
+      }];
 }
 
 - (BOOL)isInLargestDetent {
-  return NO;
+  LensOverlayBottomSheetDetent* currentDetent =
+      [self bottomSheetRestDetentWithVelocity:0];
+  for (LensOverlayBottomSheetDetent* detent in self.detents) {
+    if (detent.value > currentDetent.value) {
+      return NO;
+    }
+  }
+
+  return YES;
+}
+
+// Returns the interval of height the bottom sheet can move freely without being
+// subject to the visual deceleration.
+- (std::pair<CGFloat, CGFloat>)unrestrictedDetentMovementInterval {
+  CGFloat maximumDetentValue = 0;
+  CGFloat minimumDetentValue = self.detents.firstObject.value;
+
+  for (LensOverlayBottomSheetDetent* detent in self.detents) {
+    if (detent.value < minimumDetentValue) {
+      minimumDetentValue = detent.value;
+    }
+    if (detent.value > maximumDetentValue) {
+      maximumDetentValue = detent.value;
+    }
+  }
+
+  // The minimum end is only taken into account if the bottom sheet's closing
+  // sequence is prevented.
+  if ([self bottomSheetShouldClose]) {
+    return std::pair<CGFloat, CGFloat>(0, maximumDetentValue);
+  } else {
+    return std::pair<CGFloat, CGFloat>(minimumDetentValue, maximumDetentValue);
+  }
+}
+
+- (CGFloat)bottomSheetRestHeight {
+  CGFloat minimumDetentValue = self.detents.firstObject.value;
+  for (LensOverlayBottomSheetDetent* detent in self.detents) {
+    if (minimumDetentValue > detent.value) {
+      minimumDetentValue = detent.value;
+    }
+    if ([detent.identifier isEqualToString:self.selectedDetentIdentifier]) {
+      return std::clamp<CGFloat>(detent.value, 0,
+                                 [self bottomSheetContainerHeight]);
+    }
+  }
+
+  // If the selectedDetentIdentifier is `nil`, use the minimum value at the
+  // bottom sheet rest height.
+  if (!self.selectedDetentIdentifier) {
+    return minimumDetentValue;
+  }
+
+  NOTREACHED() << "The non nil selectedDetentIdentifier should be one of the "
+                  " available detents in the bottom sheet";
+}
+
+- (LensOverlayBottomSheetDetent*)bottomSheetRestDetentWithVelocity:
+    (CGFloat)velocity {
+  // Estimates the bottom sheet final position, assuming a continuous
+  // decelerating movement for a brief time. This estimation is not visually
+  // displayed, but  used to
+  CGFloat estimatedFinalRestingHeight =
+      self.bottomSheetHeight + kTimeToSteadyStateAfterRelease * velocity;
+
+  BOOL shouldClose = [self bottomSheetShouldClose];
+  BOOL underCloseThreshold =
+      estimatedFinalRestingHeight < kCloseBottomSheetThreshold;
+  if (shouldClose && underCloseThreshold) {
+    return nil;
+  }
+
+  // Pickes the closest detent to the estimated final resting height.
+  LensOverlayBottomSheetDetent* closestDetent = nil;
+  for (LensOverlayBottomSheetDetent* candidateClosest in self.detents) {
+    if (!closestDetent) {
+      closestDetent = candidateClosest;
+      continue;
+    }
+
+    CGFloat currentClosestDistance =
+        abs(closestDetent.value - estimatedFinalRestingHeight);
+    CGFloat candidateClosestDistance =
+        abs(candidateClosest.value - estimatedFinalRestingHeight);
+    if (candidateClosestDistance < currentClosestDistance) {
+      closestDetent = candidateClosest;
+    }
+  }
+
+  return closestDetent;
+}
+
+- (CGFloat)bottomSheetHeight {
+  // The visible bottom sheet height is the size of the sheet minus the part
+  // that slid under the bottom.
+  return _bottomSheetHeightConstraint.constant -
+         _bottomSheetBottomConstraint.constant;
+}
+
+- (void)setBottomSheetHeight:(CGFloat)height {
+  // The height is capped once it reaches `kBottomSheetMinimumDisplayHeight`,
+  // and the bottom sheet starts sliding down instead of resizing.
+  if (height < kBottomSheetMinimumDisplayHeight) {
+    _bottomSheetHeightConstraint.constant = kBottomSheetMinimumDisplayHeight;
+    _bottomSheetBottomConstraint.constant =
+        kBottomSheetMinimumDisplayHeight - height;
+  } else {
+    _bottomSheetHeightConstraint.constant = height;
+    _bottomSheetBottomConstraint.constant = 0;
+  }
+}
+
+- (BOOL)bottomSheetShouldClose {
+  if (self.detentsDelegate) {
+    return [self.detentsDelegate lensOverlayBottomSheetShouldDismiss:self];
+  }
+
+  return YES;
+}
+
+// The maximum height the bottom sheet can move within the view.
+- (CGFloat)bottomSheetContainerHeight {
+  return self.view.frame.size.height;
+}
+
+#pragma mark - LensOverlayPanTrackerDelegate
+
+// Called when the tracker ended recognizing a pan gesture.
+- (void)lensOverlayPanTracker:(LensOverlayPanTracker*)panTracker
+    didEndPanGestureWithVelocity:(CGPoint)velocity {
+  for (UIScrollView* (^scrollViewProvider)() in _disabledScrollViews) {
+    UIScrollView* scrollView = scrollViewProvider();
+    scrollView.scrollEnabled = YES;
+  }
+  _disabledScrollViews = [NSMutableArray new];
+
+  [_panTracker stopTracking];
+
+  CGFloat sheetVelocity = -velocity.y;
+
+  LensOverlayBottomSheetDetent* restDetent =
+      [self bottomSheetRestDetentWithVelocity:sheetVelocity];
+  CGFloat futureHeight = restDetent.value;
+  _selectedDetentIdentifier = restDetent.identifier;
+
+  BOOL exitingBottomSheet = futureHeight == 0;
+  if (exitingBottomSheet) {
+    [self.sheetDelegate lensOverlayBottomSheetWillDismiss:self];
+  }
+
+  [self
+      animateBottomSheetHeight:futureHeight
+                    completion:^{
+                      [self.detentsDelegate
+                          lensOverlayBottomSheetDidChangeSelectedDetentIdentifier:
+                              self];
+                      if (exitingBottomSheet) {
+                        [self.sheetDelegate
+                            lensOverlayBottomSheetDidDismiss:self];
+                      } else {
+                        [self.panTracker startTracking];
+                      }
+                    }];
+}
+
+- (void)lensOverlayPanTracker:(LensOverlayPanTracker*)panTracker
+        didPanWithTranslation:(CGPoint)translation
+                     velocity:(CGPoint)velocity {
+  std::pair<CGFloat, CGFloat> unrestrictedMovementInterval =
+      [self unrestrictedDetentMovementInterval];
+  CGFloat minimumDetentValue = unrestrictedMovementInterval.first;
+  CGFloat maximumDetentValue = unrestrictedMovementInterval.second;
+
+  CGFloat verticalMovement = -translation.y;
+  CGFloat newHeight = MAX([self bottomSheetRestHeight] + verticalMovement, 0);
+
+  // A slight, continuous deceleration is applied to the bottom sheet's movement
+  // for a small distance (a couple of pixels) past the top and bottom of its
+  // allowed interval. This 'braking' mechanism provides smoother, more
+  // continuous feedback to the user, making the sheet's endpoints feel less
+  // abrupt.
+  if (newHeight > maximumDetentValue) {
+    CGFloat diff = newHeight - maximumDetentValue;
+    CGFloat constrainedHeightMovement = kRubberBandCoefficient * log(diff);
+    newHeight = maximumDetentValue + constrainedHeightMovement;
+  } else if (newHeight < minimumDetentValue) {
+    CGFloat diff = minimumDetentValue - newHeight;
+    CGFloat constrainedHeightMovement = kRubberBandCoefficient * log(diff);
+    newHeight = MAX(minimumDetentValue - constrainedHeightMovement, 0);
+  }
+
+  [self setBottomSheetHeight:newHeight];
+}
+
+- (BOOL)lensOverlayPanTracker:(LensOverlayPanTracker*)panTracker
+    shouldRecognizeSimultaneouslyWithGestureRecognizer:
+        (UIGestureRecognizer*)gestureRecognizer {
+  BOOL isPanRecognizer =
+      [gestureRecognizer isKindOfClass:[UIPanGestureRecognizer class]];
+  BOOL isScrollable =
+      [gestureRecognizer.view isKindOfClass:[UIScrollView class]];
+  BOOL inBottomSheet =
+      [gestureRecognizer.view isDescendantOfView:_bottomSheet.view];
+  if (isScrollable && isPanRecognizer && inBottomSheet) {
+    __weak UIScrollView* scrollView = (UIScrollView*)gestureRecognizer.view;
+
+    CGFloat verticalVelocity =
+        [(UIPanGestureRecognizer*)gestureRecognizer velocityInView:scrollView]
+            .y;
+    CGPoint translation = [(UIPanGestureRecognizer*)gestureRecognizer
+        translationInView:scrollView];
+
+    // Horizontal scroll should not drag the bottom sheet.
+    BOOL verticalScroll = abs(translation.y) > 3 * abs(translation.x);
+    if (!verticalScroll) {
+      return NO;
+    }
+
+    BOOL draggingDown = verticalVelocity > 0;
+    BOOL isAtTop = scrollView.contentOffset.y <= 0;
+    BOOL isInLargestDetent = [self isInLargestDetent];
+
+    // When in the largest detent and the scroll view would s
+    BOOL sheetMovementForLargestDetent =
+        isInLargestDetent && isAtTop && draggingDown;
+    BOOL sheetMovementForSmallerDetents = !isInLargestDetent && isAtTop;
+
+    if (sheetMovementForLargestDetent || sheetMovementForSmallerDetents) {
+      // Enable dragging of the bottom sheet while disabling scrolling in all
+      // referenced scroll views. Re-enable scrolling once the bottom sheet
+      // movement finishes.
+      if (scrollView.scrollEnabled) {
+        [_disabledScrollViews addObject:^{
+          return scrollView;
+        }];
+        scrollView.scrollEnabled = NO;
+      }
+
+      return YES;
+    }
+
+    // Prevent dragging the bottom sheet and allow only the pan interactions
+    // withing the scroll view.
+    return NO;
+  }
+
+  return YES;
+}
+
+#pragma mark - ViewTouchDelegate
+
+- (BOOL)respondToPointInside:(CGPoint)point
+                   withEvent:(UIEvent*)event
+                     forView:(UIView*)view {
+  // Forward touches that fall outside the bottom sheet frame to the view
+  // behind.
+  return CGRectContainsPoint(_bottomSheet.view.frame, point);
 }
 
 @end
