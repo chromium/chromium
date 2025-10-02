269c1c5befe84b6264571855ea2991990b51cdab
Owen Min <zmin@chromium.org>
2025-10-02 13:06:31 -0700

    Revert "Only obfuscate profile ID in profile reports for unaffiliated profiles."
    This reverts commit 8a1e709e09915e1d1f49620b095ee6ab7972fcff.
    
    Reason for revert: Cause outage for profile reporting.
    
    Original change's description:
    > Only obfuscate profile ID in profile reports for unaffiliated profiles.
    >
    > When profile is affiliated, profile path is no longer considered as
    > secret to admins. Provide the real list will help us identify the
    > profile instance better.
    >
    > Bug: None
    > Change-Id: I38a8c3e08debbfe9a3b10e528c59d9bbf655d51a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6891395
    > Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Commit-Queue: Owen Min <zmin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1508431}
    
    Bug: 448916151
    Change-Id: I7965f05f2b484aced44cab324b3d9048cc3a6e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7004095
    Reviewed-by: Tien Mai <tienmai@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524406}

--- components/enterprise/browser/reporting/profile_report_generator.cc
+++ components/enterprise/browser/reporting/profile_report_generator.cc
@@ -55,23 +55,13 @@ void ProfileReportGenerator::MaybeGenerate(
 
   report_ = std::make_unique<em::ChromeUserProfileInfo>();
 
-#if !BUILDFLAG(IS_CHROMEOS)
-  delegate_->GetAffiliationInfo(report_.get());
-#endif
-
   switch (report_type) {
-    // TODO(crbug.com/441536805): Rename report type `kFull` to `kBrowser`.
+    // TODO(crbug.com/330336666): Rename report type `kFull` to `kBrowser`.
     case ReportType::kFull:
       report_->set_id(path.AsUTF8Unsafe());
       break;
     case ReportType::kProfileReport:
-      if (report_->has_affiliation() &&
-          report_->affiliation().has_is_affiliated() &&
-          report_->affiliation().is_affiliated()) {
-        report_->set_id(path.AsUTF8Unsafe());
-      } else {
-        report_->set_id(ObfuscateFilePath(path.AsUTF8Unsafe()));
-      }
+      report_->set_id(ObfuscateFilePath(path.AsUTF8Unsafe()));
       break;
     case ReportType::kBrowserVersion:
       NOTREACHED();
@@ -81,6 +71,10 @@ void ProfileReportGenerator::MaybeGenerate(
 
   delegate_->GetSigninUserInfo(report_.get());
   delegate_->GetProfileName(report_.get());
+
+#if !BUILDFLAG(IS_CHROMEOS)
+  delegate_->GetAffiliationInfo(report_.get());
+#endif
 
   if (extensions_enabled_ &&
       (!extensions_enabled_callback_ || extensions_enabled_callback_.Run())) {
