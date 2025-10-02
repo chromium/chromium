e90052c7fa8c1cf41fd163abb38d4ac2639339ec
Jenna Himawan <jhimawan@google.com>
2025-10-02 10:10:23 -0700

    Make context header width smaller to enable tests on tablet
    Bug: 444485377
    Change-Id: I8593dea1fb31c6271f73bda1be2727f348119c13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7001162
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1524290}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuHeaderViewTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuHeaderViewTest.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.contextmenu;
 
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 import static android.view.accessibility.AccessibilityNodeInfo.AccessibilityAction.ACTION_COLLAPSE;
 import static android.view.accessibility.AccessibilityNodeInfo.AccessibilityAction.ACTION_EXPAND;
 
@@ -13,13 +14,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 
-import static org.chromium.ui.base.DeviceFormFactor.PHONE;
-
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -37,7 +38,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.ui.listmenu.ListMenuItemProperties;
@@ -51,13 +51,13 @@ import org.chromium.ui.test.util.BlankUiTestActivity;
 public class ContextMenuHeaderViewTest {
     private static final String SHORT_TITLE_STRING = "Some Very Cool Title";
     private static final String LONG_TITLE_STRING =
-            "Some Very Cool Title Which Will Definitely Need To Be Ellipsized"
-                + " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
+            "Some Very Cool Title Which Will Definitely Need To Be Ellipsized";
     private static final String SHORT_URL_STRING = "www.website.com";
     private static final String LONG_URL_STRING =
             "www.website.com/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
     private static final String SECONDARY_URL_STRING = "cct.website.com";
     private static final String TERTIARY_URL_STRING = "cct.website.com/test";
+    private static final int MAXIMUM_WIDTH = 500;
 
     @ClassRule
     public static BaseActivityTestRule<BlankUiTestActivity> sActivityTestRule =
@@ -86,7 +86,15 @@ public class ContextMenuHeaderViewTest {
     public void setUp() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
-                    sActivity.setContentView(R.layout.context_menu_header);
+                    int deviceWidth = sActivity.getResources().getConfiguration().screenWidthDp;
+                    if (deviceWidth > MAXIMUM_WIDTH) {
+                        sActivity.setContentView(
+                                LayoutInflater.from(sActivity)
+                                        .inflate(R.layout.context_menu_header, null, false),
+                                new ViewGroup.LayoutParams(MAXIMUM_WIDTH, WRAP_CONTENT));
+                    } else {
+                        sActivity.setContentView(R.layout.context_menu_header);
+                    }
                     mHeaderView = sActivity.findViewById(android.R.id.content);
                     mTitle = mHeaderView.findViewById(R.id.menu_header_title);
                     mUrl = mHeaderView.findViewById(R.id.menu_header_url);
@@ -280,7 +288,6 @@ public class ContextMenuHeaderViewTest {
 
     @Test
     @SmallTest
-    @Restriction(PHONE)
     public void testTitleAndUrlClick() {
         // Clicking on the title or the URL expands/shrinks both of them.
         assertFalse(
@@ -328,7 +335,6 @@ public class ContextMenuHeaderViewTest {
 
     @Test
     @SmallTest
-    @Restriction(PHONE)
     public void testEllipsizedText_longTitle_isExpandable() {
         setupForCollapsedState(/* longTitle= */ true, /* longUrl= */ false);
 
@@ -339,7 +345,6 @@ public class ContextMenuHeaderViewTest {
 
     @Test
     @SmallTest
-    @Restriction(PHONE)
     public void testNonEllipsizedText_isNotExpandable() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -355,7 +360,6 @@ public class ContextMenuHeaderViewTest {
 
     @Test
     @SmallTest
-    @Restriction(PHONE)
     public void testAccessibility() {
         setupForCollapsedState(/* longTitle= */ true, /* longUrl= */ false);
         ThreadUtils.runOnUiThreadBlocking(mHeaderTextView::callOnClick);
@@ -386,7 +390,6 @@ public class ContextMenuHeaderViewTest {
 
     @Test
     @SmallTest
-    @Restriction(PHONE)
     public void testPerformAccessibilityAction() {
         setupForCollapsedState(/* longTitle= */ true, /* longUrl= */ false);
 
