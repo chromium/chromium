6f5ee8dfc8959dd58c6bdc7af9fc5300e3760bd5
Joe Downing <joedow@google.com>
2025-10-03 14:32:11 -0700

    Update CorpMessagingPlayground  to allow for sending host messages
    This change allows the 'host' messaging CLI to send messages to the
    client over the messaging channel. Several options are provided so
    we can use it for basic testing (send 1 message) or a burst of
    messages (10 or 100 (or more)). I'll add some more modes in a follow
    up (including echo and large payload).
    
    Note: This CL uses the EndpointId of the last message received to
    figure out where to send the message. I'll update the tool to accept
    a 'username' CLI arg in a later CL.
    
    Bug: 359620500
    Change-Id: I5416d496240a042bb65b24617b39f90ac2a9a1dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008164
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525031}

--- remoting/test/corp_messaging_playground.cc
+++ remoting/test/corp_messaging_playground.cc
@@ -4,14 +4,18 @@
 
 #include "remoting/test/corp_messaging_playground.h"
 
+#include <iostream>
 #include <memory>
 
+#include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/logging.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/run_loop.h"
+#include "base/task/bind_post_task.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/task/thread_pool.h"
 #include "net/ssl/client_cert_store.h"
 #include "remoting/base/certificate_helpers.h"
 #include "remoting/base/http_status.h"
@@ -21,6 +25,48 @@
 #include "services/network/transitional_url_loader_factory_owner.h"
 
 namespace remoting {
+
+class CorpMessagingPlayground::Core {
+ public:
+  using OnInputCallback = base::RepeatingCallback<void(char)>;
+
+  explicit Core(OnInputCallback on_input_callback);
+  ~Core();
+
+  Core(const Core&) = delete;
+  Core& operator=(const Core&) = delete;
+
+  void Start();
+
+ private:
+  OnInputCallback on_input_callback_;
+};
+
+CorpMessagingPlayground::Core::Core(OnInputCallback on_input_callback)
+    : on_input_callback_(on_input_callback) {}
+
+CorpMessagingPlayground::Core::~Core() = default;
+
+void CorpMessagingPlayground::Core::Start() {
+  base::SetNonBlocking(STDIN_FILENO);
+  std::cout << "Press '1' to send a small message to the client." << std::endl;
+  std::cout << "Press '2' to send a burst of 10 messages to the client."
+            << std::endl;
+  std::cout << "Press '3' to send a burst of 100 messages to the client."
+            << std::endl;
+  std::cout << "Press 'x' to quit." << std::endl << std::endl;
+
+  while (true) {
+    char c;
+    int n = read(STDIN_FILENO, &c, 1);
+    if (n == 1) {
+      on_input_callback_.Run(c);
+    } else if (n == -1 && errno != EAGAIN) {
+      LOG(ERROR) << "Error reading from stdin";
+      break;
+    }
+  }
+}
 
 CorpMessagingPlayground::CorpMessagingPlayground() {
   auto url_request_context_getter =
@@ -32,14 +78,18 @@ CorpMessagingPlayground::CorpMessagingPlayground() {
   client_ = std::make_unique<CorpMessagingClient>(
       url_loader_factory_owner_->GetURLLoaderFactory(),
       CreateClientCertStoreInstance());
+  core_ = std::make_unique<Core>(base::BindPostTask(
+      base::SingleThreadTaskRunner::GetCurrentDefault(),
+      base::BindRepeating(&CorpMessagingPlayground::OnCharacterInput,
+                          weak_factory_.GetWeakPtr())));
 }
 
 CorpMessagingPlayground::~CorpMessagingPlayground() = default;
 
 void CorpMessagingPlayground::Start() {
-  base::RunLoop run_loop;
+  run_loop_ = std::make_unique<base::RunLoop>();
 
-  // `callback_subscription` is automatically unregistered after `run_loop`
+  // `callback_subscription` is automatically unregistered after `run_loop_`
   // completes and this function goes out of scope.
   auto callback_subscription = client_->RegisterMessageCallback(
       base::BindRepeating(&CorpMessagingPlayground::OnSimpleMessageReceived,
@@ -48,26 +98,59 @@ void CorpMessagingPlayground::Start() {
       base::BindOnce(&CorpMessagingPlayground::OnStreamOpened,
                      base::Unretained(this)),
       base::BindOnce(&CorpMessagingPlayground::OnStreamClosed,
-                     base::Unretained(this), run_loop.QuitClosure()));
+                     base::Unretained(this)));
 
-  run_loop.Run();
+  base::ThreadPool::PostTask(
+      FROM_HERE, {base::MayBlock()},
+      base::BindOnce(&Core::Start, base::Unretained(core_.get())));
+
+  run_loop_->Run();
 }
 
 void CorpMessagingPlayground::OnStreamOpened() {
   LOG(INFO) << "Stream opened...";
 }
 
-void CorpMessagingPlayground::OnStreamClosed(base::OnceClosure on_closed,
-                                             const HttpStatus& status) {
+void CorpMessagingPlayground::OnStreamClosed(const HttpStatus& status) {
   LOG(INFO) << "Stream closed: " << status.ok() << ", "
             << static_cast<int>(status.error_code()) << ", "
             << status.error_message();
-  std::move(on_closed).Run();
+  run_loop_->Quit();
 }
 
 void CorpMessagingPlayground::OnSimpleMessageReceived(
     const internal::SimpleMessageStruct& message) {
-  LOG(INFO) << "SimpleMessage received: " << message.payload;
+  LOG(INFO) << "SimpleMessage received from " << message.sender_id.username
+            << ": " << message.payload;
+  last_sender_id_ = message.sender_id;
+}
+
+void CorpMessagingPlayground::OnCharacterInput(char c) {
+  switch (c) {
+    case '1':
+      SendMessage();
+      break;
+    case '2':
+      SendMessage(10);
+      break;
+    case '3':
+      SendMessage(100);
+      break;
+    case 'x':
+      run_loop_->Quit();
+      break;
+  }
+}
+
+void CorpMessagingPlayground::SendMessage(int count) {
+  if (last_sender_id_.username.empty()) {
+    LOG(WARNING) << "No message received yet, destination ID is unknown.";
+    return;
+  }
+  for (int i = 0; i < count; i++) {
+    client_->SendMessage(last_sender_id_, "Hello from the playground!",
+                         base::DoNothing());
+  }
 }
 
 }  // namespace remoting
--- remoting/test/corp_messaging_playground.h
+++ remoting/test/corp_messaging_playground.h
@@ -8,14 +8,17 @@
 #include <memory>
 
 #include "base/functional/callback_forward.h"
+#include "base/memory/weak_ptr.h"
 #include "remoting/base/internal_headers.h"
 
+namespace base {
+class RunLoop;
+}  // namespace base
 namespace network {
 class TransitionalURLLoaderFactoryOwner;
-}
+}  // namespace network
 
 namespace remoting {
-
 class CorpMessagingClient;
 class HttpStatus;
 
@@ -30,13 +33,21 @@ class CorpMessagingPlayground {
   void Start();
 
  private:
+  class Core;
+
   void OnStreamOpened();
-  void OnStreamClosed(base::OnceClosure on_closed, const HttpStatus& status);
+  void OnStreamClosed(const HttpStatus& status);
   void OnSimpleMessageReceived(const internal::SimpleMessageStruct& message);
+  void OnCharacterInput(char c);
+  void SendMessage(int count = 1);
 
   std::unique_ptr<network::TransitionalURLLoaderFactoryOwner>
       url_loader_factory_owner_;
   std::unique_ptr<CorpMessagingClient> client_;
+  std::unique_ptr<base::RunLoop> run_loop_;
+  std::unique_ptr<Core> core_;
+  internal::EndpointIdStruct last_sender_id_;
+  base::WeakPtrFactory<CorpMessagingPlayground> weak_factory_{this};
 };
 
 }  // namespace remoting
