b0ed4fca7e5b53f5ab5a04288770246c34f19cf5
Ginny Huang <ginnyhuang@chromium.org>
2025-10-03 11:20:03 -0700

    Revert "[ios] re-enable testSettingsPageWithExceptions on device"
    This reverts commit 96debd46be9dad0a428e341f56e7a800d9d53859.
    
    Reason for revert: Still flaky.https://ci.chromium.org/ui/test/chrome/ninja%3A%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2%3Aios_chrome_settings_eg2tests_module%2FBlockPopupsTestCase%2FtestSettingsPageWithExceptions
    
    Original change's description:
    > [ios] re-enable testSettingsPageWithExceptions on device
    >
    > The test is passing locally on device and also on flaky tests bot.
    >
    > Fixed: 441738071
    > Change-Id: I7fc3d658dcf67e619d44ba31379d31974d74ee1f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6994962
    > Auto-Submit: Federica Germinario <fedegermi@google.com>
    > Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    > Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1522952}
    
    Change-Id: I06a458a868bf496e65f87c771d5b03777457f293
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008230
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1524885}

--- ios/chrome/browser/settings/ui_bundled/content_settings/block_popups_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/content_settings/block_popups_egtest.mm
@@ -173,7 +173,15 @@ class ScopedBlockPopupsException {
 
 // Tests that the "exceptions" section on the settings page is hidden and
 // revealed properly when the preference switch is toggled.
-- (void)testSettingsPageWithExceptions {
+// TODO(crbug.com/441738071): Flaky on device.
+#if TARGET_OS_IPHONE && !TARGET_OS_SIMULATOR
+#define MAYBE_testSettingsPageWithExceptions \
+  FLAKY_testSettingsPageWithExceptions
+#else
+#define MAYBE_testSettingsPageWithExceptions \
+  testSettingsPageWithExceptions
+#endif
+- (void)MAYBE_testSettingsPageWithExceptions {
   std::string allowedPattern = "[*.]example.com";
   ScopedBlockPopupsPref prefSetter(CONTENT_SETTING_BLOCK);
   ScopedBlockPopupsException exceptionSetter(allowedPattern);
