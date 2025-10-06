chromium/chromium

commit 661de127ee2777f0bf412b384519ca56a7b6315d

webrtc-internals: emit stats in map-compatible format
the old way was required for legacy stats but support for legacy stats
has been removed a while back. Change the format to be an array of arrays that is suitable for constructing a Map() which makes it behave just like the getStats() result.

Bug: chromium:417262733
Change-Id: Iff6f6ee0e3989fae2b5bde889cff07d04628b7cc
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917904
Reviewed-by: Guido Urdaneta <guidou@chromium.org>
Reviewed-by: Henrik Boström <hbos@chromium.org>
Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
Cr-Commit-Position: refs/heads/main@{#1514177}

content/browser/webrtc/resources/stats_rates_calculator.js
@@ -82,133 +82,6 @@ class CalculatedStats {
   }
 }
 
-// Contains the metrics of an RTCStatsReport, as well as calculated metrics
-// associated with metrics from the original report. Convertible to and from the
-// "internal reports" format used by webrtc_internals.js to pass stats from C++
-// to JavaScript.
-export class StatsReport {
-  constructor() {
-    // Represents an RTCStatsReport. It is a Map RTCStats.id -> RTCStats.
-    // https://w3c.github.io/webrtc-pc/#dom-rtcstatsreport
-    this.statsById = new Map();
-    // RTCStats.id -> CalculatedStats
-    this.calculatedStatsById = new Map();
-  }
-
-  // |internalReports| is an array, each element represents an RTCStats object,
-  // but the format is a little different from the spec. This is the format:
-  // {
-  //   id: "string",
-  //   type: "string",
-  //   stats: {
-  //     timestamp: <milliseconds>,
-  //     values: ["member1", value1, "member2", value2...]
-  //   }
-  // }
-  static fromInternalsReportList(internalReports) {
-    const result = new StatsReport();
-    internalReports.forEach(internalReport => {
-      if (!internalReport.stats || !internalReport.stats.values) {
-        return;  // continue;
-      }
-      const stats = {
-        id: internalReport.id,
-        type: internalReport.type,
-        timestamp: internalReport.stats.timestamp / 1000.0  // ms -> s
-      };
-      const values = internalReport.stats.values;
-      for (let i = 0; i < values.length; i += 2) {
-        // Metric "name: value".
-        stats[values[i]] = values[i + 1];
-      }
-      result.statsById.set(stats.id, stats);
-    });
-    return result;
-  }
-
-  toInternalsReportList() {
-    const result = [];
-    for (const stats of this.statsById.values()) {
-      const internalReport = {
-        id: stats.id,
-        type: stats.type,
-        stats: {
-          timestamp: stats.timestamp * 1000.0,  // s -> ms
-          values: []
-        }
-      };
-      Object.keys(stats).forEach(metricName => {
-        if (metricName === 'id' || metricName === 'type' ||
-            metricName === 'timestamp') {
-          return;  // continue;
-        }
-        internalReport.stats.values.push(metricName);
-        internalReport.stats.values.push(stats[metricName]);
-        const calculatedMetrics =
-            this.getCalculatedMetrics(stats.id, metricName);
-        calculatedMetrics.forEach(calculatedMetric => {
-          internalReport.stats.values.push(calculatedMetric.name);
-          // Treat calculated metrics that are undefined as 0 to ensure graphs
-          // can be created anyway.
-          internalReport.stats.values.push(
-              calculatedMetric.value ? calculatedMetric.value : 0);
-        });
-      });
-      result.push(internalReport);
-    }
-    return result;
-  }
-
-  toString() {
-    let str = '';
-    for (const stats of this.statsById.values()) {
-      if (str !== '') {
-        str += ',';
-      }
-      str += JSON.stringify(stats);
-    }
-    let str2 = '';
-    for (const stats of this.calculatedStatsById.values()) {
-      if (str2 !== '') {
-        str2 += ',';
-      }
-      str2 += stats.toString();
-    }
-    return '[original:' + str + '],calculated:[' + str2 + ']';
-  }
-
-  get(id) {
-    return this.statsById.get(id);
-  }
-
-  getByType(type) {
-    const result = [];
-    for (const stats of this.statsById.values()) {
-      if (stats.type === type) {
-        result.push(stats);
-      }
-    }
-    return result;
-  }
-
-  addCalculatedMetric(id, insertAtOriginalMetricName, name, value) {
-    let calculatedStats = this.calculatedStatsById.get(id);
-    if (!calculatedStats) {
-      calculatedStats = new CalculatedStats(id);
-      this.calculatedStatsById.set(id, calculatedStats);
-    }
-    calculatedStats.addCalculatedMetric(
-        insertAtOriginalMetricName, new Metric(name, value));
-  }
-
-  getCalculatedMetrics(id, originalMetricName) {
-    const calculatedStats = this.calculatedStatsById.get(id);
-    return calculatedStats ?
-        calculatedStats.getCalculatedMetrics(originalMetricName) :
-        [];
-  }
-}
-
 // Shows a `DOMHighResTimeStamp` as a human readable date time.
 // The "metric + timestampOffsetMs" must be a time value in milliseconds with
 // Unix epoch as time origin.
@@ -638,7 +511,8 @@ export class StatsRatesCalculator {
   // bytesSent/s).
   updateCalculatedMetrics_() {
     Object.keys(this.statsCalculators).forEach(statsType => {
-      this.currentReport.getByType(statsType).forEach(stats => {
+      this.currentReport.forEach(stats => {
+        if (stats.type !== statsType) return;
         Object.keys(this.statsCalculators[statsType])
             .forEach(originalMetric => {
               let metricCalculators =
@@ -647,11 +521,11 @@ export class StatsRatesCalculator {
                 metricCalculators = [metricCalculators];
               }
               metricCalculators.forEach(metricCalculator => {
-                this.currentReport.addCalculatedMetric(
-                    stats.id, originalMetric,
-                    metricCalculator.getCalculatedMetricName(),
-                    metricCalculator.calculate(
-                        stats.id, this.previousReport, this.currentReport));
+                const name = metricCalculator.getCalculatedMetricName();
+                this.currentReport.get(stats.id)[name] =
+                    metricCalculator.calculate(stats.id,
+                                               this.previousReport,
+                                               this.currentReport);
               });
             });
       });

content/browser/webrtc/resources/stats_graph_helper.js
@@ -28,13 +28,11 @@ function isReportBlocklisted(report) {
   }
   // Unused data channels can stay in "connecting" indefinitely and their
   // counters stay zero.
-  if (report.type === 'data-channel' &&
-      readReportStat(report, 'state') === 'connecting') {
+  if (report.type === 'data-channel' && report.state === 'connecting') {
     return true;
   }
   // The same is true for transports and "new".
-  if (report.type === 'transport' &&
-      readReportStat(report, 'dtlsState') === 'new') {
+  if (report.type === 'transport' && report.dtlsState === 'new') {
     return true;
   }
   // Local and remote candidates don't change over time and there are several of
@@ -43,16 +41,6 @@ function isReportBlocklisted(report) {
     return true;
   }
   return false;
-}
-
-function readReportStat(report, stat) {
-  const values = report.stats.values;
-  for (let i = 0; i < values.length; i += 2) {
-    if (values[i] === stat) {
-      return values[i + 1];
-    }
-  }
-  return undefined;
 }
 
 function isStatBlocklisted(report, statName) {
@@ -96,26 +84,23 @@ export function drawSingleReport(
     peerConnectionElement, report) {
   const reportType = report.type;
   const reportId = report.id;
-  const stats = report.stats;
-  if (!stats || !stats.values) {
-    return;
-  }
 
   const childrenBefore = peerConnectionElement.hasChildNodes() ?
       Array.from(peerConnectionElement.childNodes) :
       [];
 
-  for (let i = 0; i < stats.values.length - 1; i = i + 2) {
-    const rawLabel = stats.values[i];
+  Object.keys(report).forEach(property => {
+    if (['timestamp', 'id'].includes(property)) return;
+    const rawLabel = property;
     const rawDataSeriesId = reportId + '-' + rawLabel;
-    const rawValue = getNumberFromValue(rawLabel, stats.values[i + 1]);
+    const rawValue = getNumberFromValue(rawLabel, report[property]);
     if (isNaN(rawValue)) {
       // We do not draw non-numerical values, but still want to record it in the
       // data series.
       addDataSeriesPoints(
           peerConnectionElement, reportType, rawDataSeriesId, rawLabel,
-          [stats.timestamp], [stats.values[i + 1]]);
-      continue;
+          [report.timestamp], [report[property]]);
+      return;
     }
     let finalDataSeriesId = rawDataSeriesId;
     let finalLabel = rawLabel;
@@ -124,12 +109,12 @@ export function drawSingleReport(
     // Updates the final dataSeries to draw.
     addDataSeriesPoints(
         peerConnectionElement, reportType, finalDataSeriesId, finalLabel,
-        [stats.timestamp], [finalValue]);
+        [report.timestamp], [finalValue]);
 
     if (isReportBlocklisted(report) || isStatBlocklisted(report, rawLabel)) {
       // We do not want to draw certain reports but still want to
       // record them in the data series.
-      continue;
+      return;
     }
 
     // Updates the graph.
@@ -149,7 +134,7 @@ export function drawSingleReport(
           graphViews[graphViewId].setScale(statsInterval);
         }
       }
-      const date = new Date(stats.timestamp);
+      const date = new Date(report.timestamp);
       graphViews[graphViewId].setDateRange(date, date);
     }
     // Ensures the stats graph title is up-to-date.
@@ -163,11 +148,11 @@ export function drawSingleReport(
       graphViews[graphViewId].addDataSeries(dataSeries);
     }
     graphViews[graphViewId].updateEndDate();
-  }
+  });
   // Add a synthetic data series for the timestamp.
   addDataSeriesPoints(
     peerConnectionElement, reportType, reportId + '-timestamp',
-    reportId + '-timestamp', [stats.timestamp], [stats.timestamp]);
+    reportId + '-timestamp', [report.timestamp], [report.timestamp]);
 
   const childrenAfter = peerConnectionElement.hasChildNodes() ?
       Array.from(peerConnectionElement.childNodes) :

content/browser/webrtc/resources/stats_helper.js
@@ -3,17 +3,12 @@
 // found in the LICENSE file.
 
 /**
- * @param {!Object} statsValues The object containing stats, an
- *     array [key1, val1, key2, val2, ...] so searching a certain
- *     key needs to ensure it does not collide with a value.
+ * @param {!Object} stats A stats report.
  */
-function generateLabel(key, statsValues) {
+function generateLabel(key, stats) {
   let label = '';
-  const statIndex = statsValues.findIndex((value, index) => {
-    return value === key && index % 2 === 0;
-  });
-  if (statIndex !== -1) {
-    label += key + '=' + statsValues[statIndex + 1];
+  if (stats.hasOwnProperty(key)) {
+    label += key + '=' + stats[key];
   }
   return label;
 }
@@ -31,7 +26,7 @@ export function generateStatsLabel(report) {
   let label = report.type + ' (';
   let labels = [];
   if (['outbound-rtp', 'remote-outbound-rtp', 'inbound-rtp',
-      'remote-inbound-rtp'].includes(report.type) && report.stats.values) {
+      'remote-inbound-rtp'].includes(report.type)) {
     labels = ['kind', 'mid', 'rid', 'ssrc', 'rtxSsrc', 'fecSsrc',
       'frameHeight', 'contentType',
       'active', 'scalabilityMode',
@@ -52,7 +47,7 @@ export function generateStatsLabel(report) {
     labels = ['label', 'state'];
   }
   labels = labels
-    .map(stat => generateLabel(stat, report.stats.values))
+    .map(stat => generateLabel(stat, report))
     .filter(label => !!label);
   if (labels.length) {
     label += labels.join(', ') + ', ';

content/browser/webrtc/resources/stats_table.js
@@ -28,10 +28,8 @@ export class StatsTable {
     statsTable.parentElement.firstElementChild.innerText =
         generateStatsLabel(report);
 
-    if (report.stats) {
-      this.addStatsToTable_(
-          statsTable, report.stats.timestamp, report.stats.values);
-    }
+    this.addStatsToTable_(
+        statsTable, report.timestamp, report);
   }
 
   clearStatsLists(peerConnectionElement) {
@@ -130,11 +128,8 @@ export class StatsTable {
    * @param {Array<string>} statsData An array of stats name and value pairs.
    * @private
    */
-  addStatsToTable_(statsTable, time, statsData) {
-    const definedMetrics = new Set();
-    for (let i = 0; i < statsData.length - 1; i = i + 2) {
-      definedMetrics.add(statsData[i]);
-    }
+  addStatsToTable_(statsTable, time, report) {
+    const definedMetrics = new Set(Object.keys(report));
     // For any previously reported metric that is no longer defined, replace its
     // now obsolete value with the magic string "(removed)".
     const metricsContainer = statsTable.firstChild;
@@ -158,9 +153,10 @@ export class StatsTable {
     // Add or update all "metric: value" that have a defined value.
     const date = new Date(time);
     this.updateStatsTableRow_(statsTable, 'timestamp', date.toLocaleString());
-    for (let i = 0; i < statsData.length - 1; i = i + 2) {
-      this.updateStatsTableRow_(statsTable, statsData[i], statsData[i + 1]);
-    }
+    Object.keys(report).forEach(property => {
+      if (['timestamp', 'id'].includes(property)) return;
+      this.updateStatsTableRow_(statsTable, property, report[property]);
+    });
   }
 
   /**

content/browser/webrtc/resources/webrtc_internals.js
@@ -10,7 +10,7 @@ import {MAX_STATS_DATA_POINT_BUFFER_SIZE} from './data_series.js';
 import {DumpCreator, peerConnectionDataStore, userMediaRequests} from './dump_creator.js';
 import {PeerConnectionUpdateTable} from './peer_connection_update_table.js';
 import {drawSingleReport, removeStatsReportGraphs} from './stats_graph_helper.js';
-import {StatsRatesCalculator, StatsReport} from './stats_rates_calculator.js';
+import {StatsRatesCalculator} from './stats_rates_calculator.js';
 import {StatsTable} from './stats_table.js';
 import {TabView} from './tab_view.js';
 import {UserMediaTable} from './user_media_table.js';
@@ -368,16 +368,13 @@ function addStandardStats(data) {
     statsRatesCalculator = new StatsRatesCalculator();
     statsRatesCalculatorById.set(pcId, statsRatesCalculator);
   }
-  const r = StatsReport.fromInternalsReportList(data.reports);
-  statsRatesCalculator.addStatsReport(r);
-  data.reports = statsRatesCalculator.currentReport.toInternalsReportList();
-  for (let i = 0; i < data.reports.length; ++i) {
-    const report = data.reports[i];
+  // Create a map from the stats entries so it behaves like a getStats maplike.
+  const stats = new Map(data.reports);
+  stats.forEach(report => {
     statsTable.addStatsReport(peerConnectionElement, report);
     drawSingleReport(peerConnectionElement, report);
-  }
-  // Determine currently connected candidate pair.
-  const stats = r.statsById;
+  });
+  statsRatesCalculator.addStatsReport(stats);
 
   let ids = [];
   stats.forEach(report => {
@@ -454,7 +451,7 @@ function addStandardStats(data) {
     }
   }
 
-  updateIceCandidateGrid(peerConnectionElement, r.statsById);
+  updateIceCandidateGrid(peerConnectionElement, stats);
 
   // Mark inactive outbound-rtp in grey.
   const inactiveStatsIds = [];

third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.cc
@@ -460,27 +460,23 @@ class InternalStandardStatsObserver : public webrtc::RTCStatsCollectorCallback {
     // Used for string comparisons with const char* below.
     const std::string kTypeMediaSource = "media-source";
     for (const auto& stats : *report) {
-      // The format of "stats_subdictionary" is:
-      // {timestamp:<milliseconds>, values: [<key-value pairs>]}
+      base::Value::Dict stats_dictionary;
+      stats_dictionary.Set("id", stats.id());
+      stats_dictionary.Set("type", stats.type());
       // The timestamp unit is milliseconds but we want decimal
       // precision so we convert ourselves.
-      base::Value::Dict stats_subdictionary;
       base::TimeDelta monotonic_time =
           time_converter.MonotonicTimeToPseudoWallTime(
               ConvertToBaseTimeTicks(stats.timestamp()));
-      stats_subdictionary.Set(
+      stats_dictionary.Set(
           "timestamp",
           monotonic_time.InMicrosecondsF() /
               static_cast<double>(base::Time::kMicrosecondsPerMillisecond));
-      // Values are reported as
-      // "values": ["attribute1", value, "attribute2", value...]
-      base::Value::List name_value_pairs;
       for (const auto& attribute : stats.Attributes()) {
         if (!attribute.has_value()) {
           continue;
         }
-        name_value_pairs.Append(attribute.name());
-        name_value_pairs.Append(AttributeToValue(attribute));
+        stats_dictionary.Set(attribute.name(), AttributeToValue(attribute));
       }
       // Modify "media-source" to also contain the result of the
       // MediaStreamTrack Statistics API, if applicable.
@@ -493,29 +489,24 @@ class InternalStandardStatsObserver : public webrtc::RTCStatsCollectorCallback {
           if (it != tracks_by_id.end()) {
             MediaStreamTrackPlatform::VideoFrameStats video_frame_stats =
                 it->second->GetVideoFrameStats();
-            name_value_pairs.Append("track.deliveredFrames");
-            name_value_pairs.Append(base::Value(
-                static_cast<int>(video_frame_stats.deliverable_frames)));
-            name_value_pairs.Append("track.discardedFrames");
-            name_value_pairs.Append(base::Value(
-                static_cast<int>(video_frame_stats.discarded_frames)));
-            name_value_pairs.Append("track.totalFrames");
-            name_value_pairs.Append(base::Value(
-                static_cast<int>(video_frame_stats.deliverable_frames +
-                                 video_frame_stats.discarded_frames +
-                                 video_frame_stats.dropped_frames)));
+            stats_dictionary.Set("track.deliveredFrames",
+                                 base::Value(static_cast<int>(
+                                     video_frame_stats.deliverable_frames)));
+            stats_dictionary.Set("track.discardedFrames",
+                                 base::Value(static_cast<int>(
+                                     video_frame_stats.discarded_frames)));
+            stats_dictionary.Set("track.totalFrames",
+                                 base::Value(static_cast<int>(
+                                     video_frame_stats.deliverable_frames +
+                                     video_frame_stats.discarded_frames +
+                                     video_frame_stats.dropped_frames)));
           }
         }
       }
-      stats_subdictionary.Set("values", std::move(name_value_pairs));
-
-      // The format of "stats_dictionary" is:
-      // {id:<string>, stats:<stats_subdictionary>, type:<string>}
-      base::Value::Dict stats_dictionary;
-      stats_dictionary.Set("stats", std::move(stats_subdictionary));
-      stats_dictionary.Set("id", stats.id());
-      stats_dictionary.Set("type", stats.type());
-      result_list.Append(std::move(stats_dictionary));
+      base::Value::List list;
+      list.Append(stats.id());
+      list.Append(std::move(stats_dictionary));
+      result_list.Append(std::move(list));
     }
     return result_list;
   }

commit 102e34c80aab91fdc859cf4d66affa32d2ab2c45

webrtc-internals: fix timestamp conversion and delta calculation
the stats rates calculator (surprisingly) assumed the stats timestamps were in seconds due to the conversion done by https://chromium-review.googlesource.com/c/chromium/src/+/6917904/5/content/browser/webrtc/resources/stats_rates_calculator.js#b117 while the spec stats use DOMHighresTimestamp (i.e. milliseconds). This led to wrong calculations and missing deltas.

Also ignore NaN results in augmented stats reports from types for
which the calculated values do not apply.

Regression from
  https://chromiumdash.appspot.com/commit/661de127ee2777f0bf412b384519ca56a7b6315d

Bug: chromium:417262733
Change-Id: I6441a7e7112529f2c7a8ad5563bb870485430891
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7003814
Reviewed-by: Henrik Boström <hbos@chromium.org>
Reviewed-by: Guido Urdaneta <guidou@chromium.org>
Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
Cr-Commit-Position: refs/heads/main@{#1525542}

content/browser/webrtc/resources/stats_rates_calculator.js
@@ -139,6 +139,7 @@ class RateCalculator {
     if (!previousStats || !currentStats) {
       return undefined;
     }
+    // Timestamp is in milliseconds.
     const deltaTime = currentStats.timestamp - previousStats.timestamp;
     if (deltaTime <= 0) {
       return undefined;
@@ -159,6 +160,10 @@ class RateCalculator {
     }
     const deltaValue = currentValue - previousValue;
     const deltaSamples = currentSamples - previousSamples;
+    if (samplesMetric === 'timestamp') {
+      // Timestamp is in milliseconds but we expect seconds as output.
+      return 1000 * deltaValue / deltaSamples;
+    }
     return deltaValue / deltaSamples;
   }
 }
@@ -305,6 +310,7 @@ class PsnrRateCalculator {
     if (!previousStats || !currentStats) {
       return undefined;
     }
+    // Timestamp is in milliseconds.
     const deltaTime = currentStats.timestamp - previousStats.timestamp;
     if (deltaTime <= 0) {
       return undefined;
@@ -522,10 +528,12 @@ export class StatsRatesCalculator {
               }
               metricCalculators.forEach(metricCalculator => {
                 const name = metricCalculator.getCalculatedMetricName();
-                this.currentReport.get(stats.id)[name] =
-                    metricCalculator.calculate(stats.id,
+                const result = metricCalculator.calculate(stats.id,
                                                this.previousReport,
                                                this.currentReport);
+                if (!isNaN(result)) {
+                  this.currentReport.get(stats.id)[name] = result;
+                }
               });
             });
       });