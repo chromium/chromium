f08d795c1503242c989d33bf499b2c4b70884e80
Oleksii Tkachenko <tkachenkoo@google.com>
2025-10-06 04:28:01 -0700

    [M142] Fix web contents NullPointerException in WebAppLaunchHandler
    Original change's description:
    > Fix web contents NullPointerException in WebAppLaunchHandler
    > 
    > Bug: 448568294
    > Change-Id: Ie2e1b9ffac24d13500fff4fc7013b5be856f380e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7002859
    > Commit-Queue: Oleksii Tkachenko <tkachenkoo@google.com>
    > Reviewed-by: Peter Conn <peconn@chromium.org>
    > Reviewed-by: Serhii Selyvon <sselyvon@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1523592}
    
    (cherry picked from commit 86ba34ab01d59037c490f7c8098adddcd39bfcc0)
    
    Bug: 449652039,448568294
    Change-Id: Ie2e1b9ffac24d13500fff4fc7013b5be856f380e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7011427
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7444@{#395}
    Cr-Branched-From: 29907d3c18078029695f458b42fb8e6fda3e493d-refs/heads/main@{#1522585}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/WebAppLaunchHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/content/WebAppLaunchHandler.java
@@ -306,7 +306,7 @@ public class WebAppLaunchHandler extends WebContentsObserver {
                                 return;
                             }
 
-                            if (mWebContents.isDestroyed()) {
+                            if (mWebContents == null || mWebContents.isDestroyed()) {
                                 Log.w(TAG, "Web contents was destroyed.");
                                 return;
                             }
