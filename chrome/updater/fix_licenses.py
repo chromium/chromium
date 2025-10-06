dc1f6976edb96275128d61c7e2a1f8024d30c7be
Joshua Pawlicki <waffles@chromium.org>
2025-10-06 10:48:10 -0700

    Updater: Fix comma/semicolon issue in license collator.
    Fixed: 449702194
    Change-Id: Ie5850e82f5b6238cc9d9c914c857ec815359f694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7012292
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1525704}

--- chrome/updater/fix_licenses.py
+++ chrome/updater/fix_licenses.py
@@ -29,7 +29,7 @@ Copyright © 2012-2015 Dan Nicholson <dbn.lists@gmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation, either version 2 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful, but
