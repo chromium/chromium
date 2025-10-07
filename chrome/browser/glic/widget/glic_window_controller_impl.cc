93d69c163fa65e23424c9ffd483359ad821385ff
Ian Wells <iwells@chromium.org>
2025-10-07 16:08:59 -0700

    Don't CHECK when getting GlicView
    When rapidly showing and hiding glic, it's possible for
    SetDraggableAreas() and maybe other GlicWindowController methods to be
    called when glic isn't showing, so GetGlicView() shouldn't CHECK but
    should just return null.
    
    Bug: 444397927, 450062329
    Change-Id: I4b1c4f73d5ad7be9f304200d4bb83a7a2cc096bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7018832
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526577}

--- chrome/browser/glic/widget/glic_window_controller_impl.cc
+++ chrome/browser/glic/widget/glic_window_controller_impl.cc
@@ -947,7 +947,10 @@ void GlicWindowControllerImpl::SetDraggingAreasAndWatchForMouseEvents() {
 }
 
 GlicView* GlicWindowControllerImpl::GetGlicView() const {
-  CHECK(IsShowing());
+  if (!IsShowing()) {
+    return nullptr;
+  }
+
   if (glic_view_) {
     return glic_view_;
   }
