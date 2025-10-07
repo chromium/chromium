d65fd4e01898f64936e3b8ecc2fc91b3b4891117
Duncan Mercer <mercerd@google.com>
2025-10-07 13:37:26 -0700

    [AIM] Hide image carousel icon
    This icon has been there for a while, and its snuck into Beta. I want to
    cherrypick this to avoid hitting stable.
    
    Bug: b:449812278
    Change-Id: I7da79be7d0b73d3708af6b18d458925864c6e0df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7014568
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526501}

--- ui/webui/resources/cr_components/composebox/composebox.html.ts
+++ ui/webui/resources/cr_components/composebox/composebox.html.ts
@@ -42,7 +42,7 @@ export function getHtml(this: ComposeboxElement) {
           `: ''}
         </div>
       </div>
-      <contextual-entrypoint-and-carousel id="context"
+      <contextual-entrypoint-and-carousel id="context" part="context-entrypoint"
           @add-tab-context="${this.addTabContext_}"
           @add-file-context="${this.addFileContext_}"
           @delete-context="${this.deleteContext_}"
