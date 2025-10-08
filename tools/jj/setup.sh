cbc287ff7a3cdbec39b44dd7fa4362d0d7fde69c
Andrew Liu <liu@chromium.org>
2025-10-08 14:30:15 -0700

    Force symlinking of JJ config upon initializing repo
    Running `jj git init` will create a default file at `.jj/repo/config.toml`. That will cause the symlinking of the custom config file to always fail. Instead, we forcibly overwrite the default file immediately after creating a jj repo.
    
    See this comment thread for context: https://docs.google.com/document/d/1nFpVtcel4-8TWfbMbAU8W8FROmTjbRTv4-UrRqLRM_E/edit?disco=AAABpvTqO4Q
    
    Change-Id: I9a55481c02e8d52291c58ad5a278ac1b0e34dcbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022690
    Reviewed-by: Matt Stark <msta@google.com>
    Commit-Queue: Andrew Liu <liu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527163}

--- tools/jj/setup.sh
+++ tools/jj/setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 # Repo root
 cd "$(dirname ${BASH_SOURCE[0]})/../.."
 
 if [[ ! -d .jj ]]; then
   jj git init --colocate .
-  ln -s "$(realpath tools/jj/config.toml)" .jj/repo/config.toml
+  ln -sf "$(realpath tools/jj/config.toml)" .jj/repo/config.toml
 fi
 
 # Ensure that jj snapshots your current commit so it doesn't get lost with git
