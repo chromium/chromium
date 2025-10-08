a8caa803790ccb5177200f03889e408e2bbe2cbd
Seung Jae Lim <andysjlim@chromium.org>
2025-10-08 11:13:03 -0700

    Enable feature ClientSideDetectionOnlyExtractVisualFeatures by default
    Bug: 421193064
    Change-Id: I0619f1e0c0800b8d69a59187b3a3f71b01a812c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022789
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527028}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -84,7 +84,7 @@ BASE_FEATURE(kClientSideDetectionLlamaForcedTriggerInfoForScamDetection,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kClientSideDetectionOnlyExtractVisualFeatures,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kClientSideDetectionRedirectChainKillswitch,
              base::FEATURE_DISABLED_BY_DEFAULT);
