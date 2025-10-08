68c20efeb28b46c228e215f3b195cb83a388b5b0
Kent Tamura <tkent@chromium.org>
2025-10-08 00:58:44 -0700

    Remove the "TextareaSplitText" runtime flag
    It has been enabled since M139, and we have seen no issues about it.
    This CL has no behavior changes.
    
    Bug: 341564372
    Change-Id: I7974476da548903100fec294b70a1b489962ca4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7015287
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1526737}

--- third_party/blink/renderer/core/html/forms/text_control_element.cc
+++ third_party/blink/renderer/core/html/forms/text_control_element.cc
@@ -128,11 +128,6 @@ void AppendText(const String& value,
                 wtf_size_t limit,
                 ContainerNode& container) {
   Document& doc = container.GetDocument();
-  if (!RuntimeEnabledFeatures::TextareaSplitTextEnabled()) {
-    container.AppendChild(
-        Text::Create(doc, value.Substring(start, limit - start)));
-    return;
-  }
   constexpr wtf_size_t kTextChunkSize = 8192u;
   for (wtf_size_t i = start; i < limit; i += kTextChunkSize) {
     container.AppendChild(Text::Create(
@@ -1003,12 +998,8 @@ void TextControlElement::SetInnerEditorValue(const String& value) {
     inner_editor->RemoveChildren();
   } else if (!RuntimeEnabledFeatures::TextareaLineEndingsAsBrEnabled() ||
              IsA<HTMLInputElement>(this)) {
-    if (RuntimeEnabledFeatures::TextareaSplitTextEnabled()) {
-      inner_editor->RemoveChildren();
-      AppendText(value, 0, value.length(), *inner_editor);
-    } else {
-      ReplaceChildrenWithText(inner_editor, value, ASSERT_NO_EXCEPTION);
-    }
+    inner_editor->RemoveChildren();
+    AppendText(value, 0, value.length(), *inner_editor);
   } else {
     inner_editor->RemoveChildren();
     // For <textarea>, \n is replaced with <br>.
