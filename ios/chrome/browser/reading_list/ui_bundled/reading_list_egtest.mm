240702cb895a268a2a3f4073c79e07911224370c
Rushan Suleymanov <rushans@google.com>
2025-10-08 04:30:21 -0700

    Revert "Reenable Reading list test"
    This reverts commit 3ff2e417ff9802463424b60a94c8a4ab7cfa5ed6.
    
    Reason for revert: seems to cause test failures (see the bug for details):
    ReadingListTestCase/testContextMenuCopyLink
    
    Original change's description:
    > Reenable Reading list test
    >
    > Fix the scrolling logic and the scroll to top that were causing flakyness.
    >
    > Bug: 446871565
    > Change-Id: Idd53fed38daa0432c0ad7aee4525a18902973e62
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6988714
    > Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    > Reviewed-by: Federica Germinario <fedegermi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1526774}
    
    Bug: 446871565
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I7295e22f114bb566e2c5195175505af9403710a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021150
    Owners-Override: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1526825}

--- ios/chrome/browser/reading_list/ui_bundled/reading_list_egtest.mm
+++ ios/chrome/browser/reading_list/ui_bundled/reading_list_egtest.mm
@@ -112,9 +112,8 @@ std::string operator*(const std::string& s, unsigned int n) {
 
 // Scroll to the top of the Reading List.
 void ScrollToTop() {
-  XCUIApplication* springboardApplication = [[XCUIApplication alloc]
-      initWithBundleIdentifier:@"com.apple.springboard"];
-  [springboardApplication.statusBars.firstMatch tap];
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(kReadingListViewID)]
+      performAction:[ChromeActionsAppInterface scrollToTop]];
 }
 
 // Asserts that the "mark" toolbar button is visible and has the a11y label of
@@ -171,7 +170,7 @@ void TapActionSheetButtonWithA11yLabelID(int a11y_label_id) {
 void PerformActionOnEntry(NSString* entryTitle, id<GREYAction> action) {
   ScrollToTop();
   [[[EarlGrey selectElementWithMatcher:VisibleReadingListItem(entryTitle)]
-         usingSearchAction:grey_swipeSlowInDirection(kGREYDirectionUp)
+         usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 100)
       onElementWithMatcher:grey_accessibilityID(kReadingListViewID)]
       performAction:action];
 }
@@ -191,7 +190,7 @@ void LongPressEntry(NSString* entryTitle) {
 void AssertEntryVisible(NSString* entryTitle) {
   ScrollToTop();
   [[[EarlGrey selectElementWithMatcher:VisibleReadingListItem(entryTitle)]
-         usingSearchAction:grey_swipeSlowInDirection(kGREYDirectionUp)
+         usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 100)
       onElementWithMatcher:grey_accessibilityID(kReadingListViewID)]
       assertWithMatcher:grey_notNil()];
 }
@@ -208,7 +207,6 @@ void AssertAllEntriesVisible() {
                   @"The number of entries have changed");
   GREYAssertEqual((size_t)2, kNumberUnreadEntries,
                   @"The number of entries have changed");
-  ScrollToTop();
 }
 
 // Asserts that the entry `title` is not visible.
@@ -218,7 +216,7 @@ void AssertEntryNotVisible(NSString* title) {
   NSError* error;
 
   [[[EarlGrey selectElementWithMatcher:VisibleReadingListItem(title)]
-         usingSearchAction:grey_swipeSlowInDirection(kGREYDirectionUp)
+         usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 100)
       onElementWithMatcher:grey_accessibilityID(kReadingListViewID)]
       assertWithMatcher:grey_notNil()
                   error:&error];
@@ -826,7 +824,7 @@ void AssertIsShowingDistillablePage(bool online, const GURL& distillable_url) {
   AddEntriesAndOpenReadingList();
 
   [[[EarlGrey selectElementWithMatcher:VisibleReadingListItem(kReadTitle)]
-         usingSearchAction:grey_swipeSlowInDirection(kGREYDirectionUp)
+         usingSearchAction:grey_scrollInDirection(kGREYDirectionDown, 100)
       onElementWithMatcher:grey_accessibilityID(kReadingListViewID)]
       performAction:grey_swipeFastInDirection(kGREYDirectionLeft)];
 
@@ -1252,8 +1250,9 @@ void AssertIsShowingDistillablePage(bool online, const GURL& distillable_url) {
       verifyOpenInIncognitoActionWithURL:distillablePageURL.GetContent()];
 }
 
+// TODO(crbug.com/446871565): This test is flaky.
 // Tests the Mark as Read/Unread context menu action for a reading list entry.
-- (void)testContextMenuMarkAsReadAndBack {
+- (void)FLAKY_testContextMenuMarkAsReadAndBack {
 #if TARGET_IPHONE_SIMULATOR
   // TODO(crbug.com/433982582): Flaky on an iPhone simulator.
   if ([ChromeEarlGrey isIPhoneIdiom]) {
@@ -1272,9 +1271,6 @@ void AssertIsShowingDistillablePage(bool online, const GURL& distillable_url) {
                   [ReadingListAppInterface unreadEntriesCount],
                   @"Wrong number of unread entry.");
 
-  // TODO(crbug.com/446889046): Investigate if there is a better solution to fix
-  // flakiness on iOS26.
-  base::test::ios::SpinRunLoopWithMinDelay(base::Seconds(1));
   // Mark an unread entry as read.
   LongPressEntry(kUnreadTitle);
 
@@ -1289,9 +1285,6 @@ void AssertIsShowingDistillablePage(bool online, const GURL& distillable_url) {
                   [ReadingListAppInterface unreadEntriesCount],
                   @"Wrong number of unread entry after marking read.");
 
-  // TODO(crbug.com/446889046): Investigate if there is a better solution to fix
-  // flakiness on iOS26.
-  base::test::ios::SpinRunLoopWithMinDelay(base::Seconds(1));
   // Now mark it back as unread.
   LongPressEntry(kUnreadTitle);
 
