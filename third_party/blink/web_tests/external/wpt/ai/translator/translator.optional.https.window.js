eea57f87a01dee0340844531b565b2129f204501
Mike Wasserman <msw@chromium.org>
2025-10-09 16:17:00 -0700

    Built-In AI APIs: Fix Translator and Language Detector tests
    These tests presume API availability is initially 'downloadable'.
    Formalize that expectation with assert_implements_optional.
    Align test structure with Summarizer, etc.
    
    Fixed: 443908031, 443911549
    Test: Chromium CI still WAI, wpt.live now surfaces failing precondition when used with a user-data-dir that previously downloaded API models and configs, yielding a different initial availability.
    Change-Id: I5884f02a8613df6fa20f091354cadfdceac6a7c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7024029
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527826}

--- third_party/blink/web_tests/external/wpt/ai/translator/translator.optional.https.window.js
+++ third_party/blink/web_tests/external/wpt/ai/translator/translator.optional.https.window.js
@@ -10,24 +10,20 @@
 
 'use strict';
 
+// TODO(crbug.com/390246212): Support model state controls for WPTs.
 promise_test(async t => {
+  // Create requires user activation when availability is 'downloadable'.
   const languagePair = {sourceLanguage: 'en', targetLanguage: 'ja'};
+  assert_implements_optional(await Translator.availability(languagePair) == 'downloadable');
+  assert_false(navigator.userActivation.isActive);
+  await promise_rejects_dom(t, 'NotAllowedError', Translator.create(languagePair));
+  await test_driver.bless('Translator.create', async () => { await Translator.create(languagePair); });
 
-  // Creating the translator without user activation rejects with
-  // NotAllowedError.
-  const createPromise = Translator.create(languagePair);
-  await promise_rejects_dom(t, 'NotAllowedError', createPromise);
-
-  // Creating the translator with user activation succeeds.
-  await createTranslator(languagePair);
-
-  // Creating it should have switched it to available.
-  const availability = await Translator.availability(languagePair);
-  assert_equals(availability, 'available');
-
-  // Now that it is available, we should no longer need user activation.
+  // Create does not require user activation when availability is 'available'.
+  assert_equals(await Translator.availability(languagePair), 'available');
+  assert_false(navigator.userActivation.isActive);
   await Translator.create(languagePair);
-}, 'Translator.create() requires user activation when availability is "downloadable.');
+}, 'Create requires user activation when availability is "downloadable"');
 
 promise_test(async t => {
   const translator =
