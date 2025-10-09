61402c4dbffd02a342e321af507379113a8c9da9
Hubert Chao <hchao@chromium.org>
2025-10-09 08:16:16 -0700

    [LNA] refactor LNA browser tests in chrome/browser
    Refactor chrome/browser/lna/local_network_acccess_browsertests.cc to
    better support breaking tests out into multiple files. Start with
    breaking out the recently added iframe tests.
    
    Bug: 406991278
    Change-Id: I9fa45800b56cebf4099eedd6098d0f97a1ae7c2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021553
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527536}

--- chrome/browser/local_network_access/local_network_access_browsertest.cc
+++ chrome/browser/local_network_access/local_network_access_browsertest.cc
@@ -5,8 +5,6 @@
 #include "base/command_line.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
-#include "base/threading/platform_thread.h"
-#include "base/time/time.h"
 #include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -44,6 +42,7 @@ constexpr char kSharedWorkerHtmlPath[] =
 constexpr char kServiceWorkerHtmlPath[] =
     "/local_network_access/fetch-from-service-worker-as-public-address.html";
 
+// TODO(crbug.com/406991278): refactor to use LocalNetworkAccessBrowserTestBase
 class LocalNetworkAccessBrowserTest : public policy::PolicyTest {
  public:
   using WebFeature = blink::mojom::WebFeature;
@@ -100,43 +99,6 @@ class LocalNetworkAccessBrowserTest : public policy::PolicyTest {
 
   permissions::MockPermissionPromptFactory* bubble_factory() {
     return mock_permission_prompt_factory_.get();
-  }
-
-  // Helper function for running iframe navigation tests that are intended to
-  // succeed.
-  //
-  // TODO(crbug.com/406991278): refactor this file to move the Iframe tests into
-  // their own file.
-  // TODO(crbug.com/406991278): work on covering more iframe tests cases,
-  // including those that we expect to fail.
-  void RunIframeNavigationTest(const GURL& initial_url,
-                               const GURL& iframe_url,
-                               const GURL& nav_url) {
-    ASSERT_TRUE(content::NavigateToURL(web_contents(), initial_url));
-
-    // Enable auto-accept of LNA permission request.
-    bubble_factory()->set_response_type(
-        permissions::PermissionRequestManager::AutoResponseType::ACCEPT_ALL);
-
-    content::TestNavigationManager iframe_url_nav_manager(web_contents(),
-                                                          iframe_url);
-    content::TestNavigationManager nav_url_nav_manager(web_contents(), nav_url);
-    std::string_view script_template = R"(
-      const child = document.createElement("iframe");
-      child.src = $1;
-      child.allow = "local-network-access";
-      document.body.appendChild(child);
-    )";
-    EXPECT_THAT(
-        content::EvalJs(web_contents(),
-                        content::JsReplace(script_template, iframe_url)),
-        content::EvalJsResult::IsOk());
-    // Check that the child iframe was successfully fetched.
-    ASSERT_TRUE(iframe_url_nav_manager.WaitForNavigationFinished());
-    EXPECT_TRUE(iframe_url_nav_manager.was_successful());
-
-    ASSERT_TRUE(nav_url_nav_manager.WaitForNavigationFinished());
-    EXPECT_TRUE(nav_url_nav_manager.was_successful());
   }
 
  protected:
@@ -213,146 +175,6 @@ IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest, FetchAcceptPermission) {
                 web_contents(),
                 content::JsReplace("fetch($1).then(response => response.ok)",
                                    https_server().GetURL("b.com", kLnaPath))));
-}
-
-IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest, IframeDenyPermission) {
-  ASSERT_TRUE(content::NavigateToURL(
-      web_contents(),
-      https_server().GetURL(
-          "a.com",
-          "/local_network_access/no-favicon-treat-as-public-address.html")));
-
-  // Enable auto-denial of LNA permission request.
-  bubble_factory()->set_response_type(
-      permissions::PermissionRequestManager::AutoResponseType::DENY_ALL);
-
-  GURL iframe_url = https_server().GetURL("b.com", kLnaPath);
-  content::TestNavigationManager nav_manager(web_contents(), iframe_url);
-  std::string_view script_template = R"(
-    const child = document.createElement("iframe");
-    child.src = $1;
-    document.body.appendChild(child);
-  )";
-  EXPECT_THAT(content::EvalJs(web_contents(),
-                              content::JsReplace(script_template, iframe_url)),
-              content::EvalJsResult::IsOk());
-  ASSERT_TRUE(nav_manager.WaitForNavigationFinished());
-
-  // Check that the child iframe failed to fetch.
-  EXPECT_FALSE(nav_manager.was_successful());
-}
-
-IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest, IframeAcceptPermission) {
-  ASSERT_TRUE(content::NavigateToURL(
-      web_contents(),
-      https_server().GetURL(
-          "a.com",
-          "/local_network_access/no-favicon-treat-as-public-address.html")));
-
-  // Enable auto-accept of LNA permission request.
-  bubble_factory()->set_response_type(
-      permissions::PermissionRequestManager::AutoResponseType::ACCEPT_ALL);
-
-  GURL iframe_url = https_server().GetURL("b.com", kLnaPath);
-  content::TestNavigationManager nav_manager(web_contents(), iframe_url);
-  std::string_view script_template = R"(
-    const child = document.createElement("iframe");
-    child.src = $1;
-    document.body.appendChild(child);
-  )";
-  EXPECT_THAT(content::EvalJs(web_contents(),
-                              content::JsReplace(script_template, iframe_url)),
-              content::EvalJsResult::IsOk());
-  ASSERT_TRUE(nav_manager.WaitForNavigationFinished());
-
-  // Check that the child iframe was successfully fetched.
-  EXPECT_TRUE(nav_manager.was_successful());
-}
-
-// Open a public page that iframes a public page, then navigate it to a loopback
-// page.
-IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
-                       IframeNavigationPublicPagePublicIframe) {
-  GURL initial_url = https_server().GetURL(
-      "a.com", "/local_network_access/no-favicon-treat-as-public-address.html");
-  GURL final_url = https_server().GetURL("c.com", "/defaultresponse");
-  GURL iframe_url = https_server().GetURL(
-      "b.com",
-      "/local_network_access/"
-      "client-redirect-treat-as-public-address.html?url=" +
-          final_url.spec());
-
-  RunIframeNavigationTest(initial_url, iframe_url, final_url);
-}
-
-// Open a public page that iframes a loopback page, then navigate it to a
-// loopback page.
-IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
-                       IframeNavigationPublicPageLoopbackIframe) {
-  GURL initial_url = https_server().GetURL(
-      "a.com", "/local_network_access/no-favicon-treat-as-public-address.html");
-  GURL final_url = https_server().GetURL("c.com", "/defaultresponse");
-  GURL iframe_url = https_server().GetURL(
-      "b.com",
-      "/local_network_access/client-redirect.html?url=" + final_url.spec());
-  RunIframeNavigationTest(initial_url, iframe_url, final_url);
-}
-
-// Open a loopback page that iframes a public page, then navigate it to a
-// loopback page.
-IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
-                       IframeNavLoopbackPagePublicIframe) {
-  GURL initial_url =
-      https_server().GetURL("a.com", "/local_network_access/no-favicon.html");
-  GURL final_url = https_server().GetURL("c.com", "/defaultresponse");
-  GURL iframe_url = https_server().GetURL(
-      "b.com",
-      "/local_network_access/"
-      "client-redirect-treat-as-public-address.html?url=" +
-          final_url.spec());
-  RunIframeNavigationTest(initial_url, iframe_url, final_url);
-}
-
-// Open a loopback page that iframes a loopback page, then navigate it to a
-// loopback page.
-IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
-                       IframeNavLoopbackPageLoopbackIframe) {
-  GURL initial_url =
-      https_server().GetURL("a.com", "/local_network_access/no-favicon.html");
-  GURL final_url = https_server().GetURL("c.com", "/defaultresponse");
-  GURL iframe_url = https_server().GetURL(
-      "b.com",
-      "/local_network_access/client-redirect.html?url=" + final_url.spec());
-  RunIframeNavigationTest(initial_url, iframe_url, final_url);
-}
-
-// Open a public page that iframes a public page, then navigate it to a loopback
-// page. The page and the iframe have the same origin
-IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
-                       IframeNavPublicPagePublicLoopbackSameOrigin) {
-  GURL initial_url = https_server().GetURL(
-      "a.com", "/local_network_access/no-favicon-treat-as-public-address.html");
-  GURL final_url = https_server().GetURL("c.com", "/defaultresponse");
-  GURL iframe_url = https_server().GetURL(
-      "a.com",
-      "/local_network_access/"
-      "client-redirect-treat-as-public-address.html?url=" +
-          final_url.spec());
-
-  RunIframeNavigationTest(initial_url, iframe_url, final_url);
-}
-
-// Open a loopback page that iframes a loopback page, then navigate it to a
-// loopback page. The page and the iframe have the same origin.
-IN_PROC_BROWSER_TEST_F(LocalNetworkAccessBrowserTest,
-                       IframeNavLoopbackPageLoopbackIframeSameOrigin) {
-  GURL initial_url =
-      https_server().GetURL("a.com", "/local_network_access/no-favicon.html");
-  GURL final_url = https_server().GetURL("c.com", "/defaultresponse");
-  GURL iframe_url = https_server().GetURL(
-      "a.com",
-      "/local_network_access/client-redirect.html?url=" + final_url.spec());
-  RunIframeNavigationTest(initial_url, iframe_url, final_url);
 }
 
 // Tests that a script tag that is included in the main page HTML (and thus
