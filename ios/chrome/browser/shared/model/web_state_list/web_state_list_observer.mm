a71702230493d84d0209566525f2c6941a6c2934
Sylvain Defresne <sdefresne@chromium.org>
2025-10-09 06:55:24 -0700

    [ios] Cleanup style on WebStateListChange sub-classes
    Use raw C++ pointers instead of raw_ptr<...> as parameters and return
    value of the WebStateListChange sub-classes (as recommended in the
    documentation for raw_ptr<...>, see [1]).
    
    CHECK(...) that the pointers are non-null at construction instead of
    checking in the accessor implementation (we want to detect the error
    when it happens, i.e. creating the instance with null pointer, instead
    of when the error fallout happens, i.e. when client receive null, as
    it is too late by this point to understand why the error occurred).
    
    Re-order some fields of WebStateListChange sub-classes to reduce the
    padding (thus reduce the stack used).
    
    [1]: //base/memory/raw_ptr.md#pointers-in-locations-other-than-fields)
    
    Bug: none
    Change-Id: I449aab0e0b8f328f6c2e2ed9876290a32739c6d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022734
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1527489}

--- ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.mm
+++ ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.mm
@@ -9,77 +9,89 @@
 #import "base/check.h"
 
 WebStateListChangeStatusOnly::WebStateListChangeStatusOnly(
-    raw_ptr<web::WebState> web_state,
+    web::WebState* web_state,
     int index,
     bool pinned_state_changed,
-    raw_ptr<const TabGroup> old_group,
-    raw_ptr<const TabGroup> new_group)
+    const TabGroup* old_group,
+    const TabGroup* new_group)
     : web_state_(web_state),
-      index_(index),
-      pinned_state_changed_(pinned_state_changed),
       old_group_(old_group),
-      new_group_(new_group) {}
+      new_group_(new_group),
+      index_(index),
+      pinned_state_changed_(pinned_state_changed) {
+  if (index != WebStateList::kInvalidIndex) {
+    CHECK(web_state_);
+  }
+}
 
 WebStateListChange::Type WebStateListChangeStatusOnly::type() const {
   return kType;
 }
 
 WebStateListChangeDetach::WebStateListChangeDetach(
-    raw_ptr<web::WebState> detached_web_state,
+    web::WebState* detached_web_state,
     int detached_from_index,
     DetachReason detach_reason,
-    raw_ptr<const TabGroup> group)
+    const TabGroup* group)
     : detached_web_state_(detached_web_state),
+      group_(group),
       detached_from_index_(detached_from_index),
-      detach_reason_(detach_reason),
-      group_(group) {}
+      detach_reason_(detach_reason) {
+  CHECK(detached_web_state_);
+}
 
 WebStateListChange::Type WebStateListChangeDetach::type() const {
   return kType;
 }
 
-WebStateListChangeMove::WebStateListChangeMove(
-    raw_ptr<web::WebState> moved_web_state,
-    int moved_from_index,
-    int moved_to_index,
-    bool pinned_state_changed,
-    raw_ptr<const TabGroup> old_group,
-    raw_ptr<const TabGroup> new_group)
+WebStateListChangeMove::WebStateListChangeMove(web::WebState* moved_web_state,
+                                               int moved_from_index,
+                                               int moved_to_index,
+                                               bool pinned_state_changed,
+                                               const TabGroup* old_group,
+                                               const TabGroup* new_group)
     : moved_web_state_(moved_web_state),
+      old_group_(old_group),
+      new_group_(new_group),
       moved_from_index_(moved_from_index),
       moved_to_index_(moved_to_index),
-      pinned_state_changed_(pinned_state_changed),
-      old_group_(old_group),
-      new_group_(new_group) {}
+      pinned_state_changed_(pinned_state_changed) {
+  CHECK(moved_web_state_);
+}
 
 WebStateListChange::Type WebStateListChangeMove::type() const {
   return kType;
 }
 
 WebStateListChangeReplace::WebStateListChangeReplace(
-    raw_ptr<web::WebState> replaced_web_state,
-    raw_ptr<web::WebState> inserted_web_state,
+    web::WebState* replaced_web_state,
+    web::WebState* inserted_web_state,
     int index)
     : replaced_web_state_(replaced_web_state),
       inserted_web_state_(inserted_web_state),
-      index_(index) {}
+      index_(index) {
+  CHECK(replaced_web_state_);
+  CHECK(inserted_web_state_);
+}
 
 WebStateListChange::Type WebStateListChangeReplace::type() const {
   return kType;
 }
 
 WebStateListChangeInsert::WebStateListChangeInsert(
-    raw_ptr<web::WebState> inserted_web_state,
+    web::WebState* inserted_web_state,
     int index,
-    raw_ptr<const TabGroup> group)
-    : inserted_web_state_(inserted_web_state), index_(index), group_(group) {}
+    const TabGroup* group)
+    : inserted_web_state_(inserted_web_state), group_(group), index_(index) {
+  CHECK(inserted_web_state_);
+}
 
 WebStateListChange::Type WebStateListChangeInsert::type() const {
   return kType;
 }
 
 WebStateListChangeGroupCreate::WebStateListChangeGroupCreate(
-    raw_ptr<const TabGroup> created_group)
+    const TabGroup* created_group)
     : created_group_(created_group) {
   CHECK(created_group_);
 }
@@ -90,10 +102,10 @@ WebStateListChange::Type WebStateListChangeGroupCreate::type() const {
 
 WebStateListChangeGroupVisualDataUpdate::
     WebStateListChangeGroupVisualDataUpdate(
-        raw_ptr<const TabGroup> updated_group,
+        const TabGroup* updated_group,
         const tab_groups::TabGroupVisualData& old_visual_data)
     : updated_group_(updated_group), old_visual_data_(old_visual_data) {
-  DCHECK(updated_group_);
+  CHECK(updated_group_);
 }
 
 WebStateListChange::Type WebStateListChangeGroupVisualDataUpdate::type() const {
@@ -101,7 +113,7 @@ WebStateListChange::Type WebStateListChangeGroupVisualDataUpdate::type() const {
 }
 
 WebStateListChangeGroupMove::WebStateListChangeGroupMove(
-    raw_ptr<const TabGroup> moved_group,
+    const TabGroup* moved_group,
     TabGroupRange moved_from_range,
     TabGroupRange moved_to_range)
     : moved_group_(moved_group),
@@ -116,7 +128,7 @@ WebStateListChange::Type WebStateListChangeGroupMove::type() const {
 }
 
 WebStateListChangeGroupDelete::WebStateListChangeGroupDelete(
-    raw_ptr<const TabGroup> deleted_group)
+    const TabGroup* deleted_group)
     : deleted_group_(deleted_group) {
   CHECK(deleted_group_);
 }
