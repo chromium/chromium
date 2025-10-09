1c5486862a65142f87ad5d2623d679da1faaa076
Jonathan Lee <jonathanjlee@google.com>
2025-10-09 10:03:15 -0700

    Import wpt@1edd9494b207d892b1dbd8fdd675cfc531f590cf
    https://github.com/web-platform-tests/wpt/compare/ced062fdf...1edd9494b
    
    Using wpt-import in Chromium e731affb71a178853b958c996eb58ceb1c59a4e6.
    With Chromium commits locally applied on WPT:
      468e313cf6 "[BackdropFilter] Create a backdrop root for clip [...]
      bf882df84a "[SAA] Fix test failures due to default cookie [...]
      844f0be7b6 "Fix document.caretPositionFromPoint().offset for [...]
      fb79f6a82a "[anchor] Fix anchor-scroll-position-try-{008,009,010}"
      adc0b917f1 "Reland "Extend timeout duration in reporting-common.js""
      2dd53a27f9 "Anchor: Update anchor-scroll-position-try-011.html [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    daniec@microsoft.com, siliu@microsoft.com, snianu@microsoft.com:
      external/wpt/editing
    drott@chromium.org:
      external/wpt/css/css-text-decor
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    foolip@chromium.org:
      external/wpt/fullscreen
    guidou@chromium.org:
      external/wpt/screen-capture
    guidou@chromium.org, armax@chromium.org:
      external/wpt/mediacapture-streams
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    igrigorik@chromium.org, yoavweiss@chromium.org:
      external/wpt/preload
    japhet@chromium.org:
      external/wpt/fetch
    kojii@chromium.org:
      external/wpt/css/css-font-loading
      external/wpt/css/css-writing-modes
    pastithas@chromium.org:
      external/wpt/resources
    vogelheim@chromium.org:
      external/wpt/trusted-types
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Change-Id: I2ef010105f790286132dba5a1954f92dc1ddaad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7022694
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1527604}

--- third_party/blink/web_tests/external/wpt/compression/decompression-correct-input.any.js
+++ third_party/blink/web_tests/external/wpt/compression/decompression-correct-input.any.js
@@ -1,14 +1,7 @@
 // META: global=window,worker,shadowrealm
+// META: script=decompression-correct-input.js
 
 'use strict';
-
-const deflateChunkValue = new Uint8Array([120, 156, 75, 173, 40, 72, 77, 46, 73, 77, 81, 200, 47, 45, 41, 40, 45, 1, 0, 48, 173, 6, 36]);
-const gzipChunkValue = new Uint8Array([31, 139, 8, 0, 0, 0, 0, 0, 0, 3, 75, 173, 40, 72, 77, 46, 73, 77, 81, 200, 47, 45, 41, 40, 45, 1, 0, 176, 1, 57, 179, 15, 0, 0, 0]);
-const deflateRawChunkValue = new Uint8Array([
-    0x4b, 0xad, 0x28, 0x48, 0x4d, 0x2e, 0x49, 0x4d, 0x51, 0xc8,
-    0x2f, 0x2d, 0x29, 0x28, 0x2d, 0x01, 0x00,
-]);
-const trueChunkValue = new TextEncoder().encode('expected output');
 
 promise_test(async t => {
     const ds = new DecompressionStream('deflate');
@@ -18,7 +11,6 @@ promise_test(async t => {
     const { done, value } = await reader.read();
     assert_array_equals(Array.from(value), trueChunkValue, "value should match");
 }, 'decompressing deflated input should work');
-
 
 promise_test(async t => {
     const ds = new DecompressionStream('gzip');
