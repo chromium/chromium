6010bb9d8524020f73e36903bc7267c084f633d8
Camillia Smith Barnes <cammie@chromium.org>
2025-10-10 15:30:45 -0400

    Make SecureEmbedHost trivially implement CrossProcessFrameConnectorBase

--- components/secure_embed/browser/secure_embed_host.cc
+++ components/secure_embed/browser/secure_embed_host.cc
@@ -6,8 +6,11 @@
 
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/no_destructor.h"
+#include "content/public/browser/cross_process_frame_connector_base.h"
 #include "content/public/browser/render_frame_host.h"
 #include "mojo/public/cpp/bindings/self_owned_associated_receiver.h"
+#include "third_party/blink/public/mojom/frame/viewport_intersection_state.mojom.h"
 
 namespace secure_embed {
 
@@ -38,6 +41,237 @@ void SecureEmbedHost::Attach(int64_t content_id) {
 // static
 size_t SecureEmbedHost::GetInstanceCountForTesting() {
   return instance_count_for_testing_;
+}
+
+void SecureEmbedHost::SetView(content::RenderWidgetHostViewChildFrame* view,
+                              bool allow_paint_holding) {
+  NOTIMPLEMENTED();
+}
+
+content::RenderWidgetHostViewBase*
+SecureEmbedHost::GetParentRenderWidgetHostView() {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+content::RenderWidgetHostViewBase*
+SecureEmbedHost::GetRootRenderWidgetHostView() {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+void SecureEmbedHost::RenderProcessGone() {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::FirstSurfaceActivation(
+    const viz::SurfaceInfo& surface_info) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::SendIntrinsicSizingInfoToParent(
+    blink::mojom::IntrinsicSizingInfoPtr) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::SynchronizeVisualProperties(
+    const blink::FrameVisualProperties& visual_properties,
+    bool propagate) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::UpdateCursor(const ui::Cursor& cursor) {
+  NOTIMPLEMENTED();
+}
+
+content::CrossProcessFrameConnectorBase::RootViewFocusState
+SecureEmbedHost::HasFocus() {
+  NOTIMPLEMENTED();
+  return content::CrossProcessFrameConnectorBase::RootViewFocusState::kNullView;
+}
+
+void SecureEmbedHost::FocusRootView() {
+  NOTIMPLEMENTED();
+}
+
+blink::mojom::PointerLockResult SecureEmbedHost::LockPointer(
+    bool request_unadjusted_movement) {
+  NOTIMPLEMENTED();
+  return blink::mojom::PointerLockResult::kUnknownError;
+}
+
+blink::mojom::PointerLockResult SecureEmbedHost::ChangePointerLock(
+    bool request_unadjusted_movement) {
+  NOTIMPLEMENTED();
+  return blink::mojom::PointerLockResult::kUnknownError;
+}
+
+void SecureEmbedHost::UnlockPointer() {
+  NOTIMPLEMENTED();
+}
+
+bool SecureEmbedHost::HasSize() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+const display::ScreenInfos& SecureEmbedHost::GetScreenInfos() const {
+  NOTIMPLEMENTED();
+  static const base::NoDestructor<display::ScreenInfos> screen_infos;
+  return *screen_infos;
+}
+
+const viz::LocalSurfaceId& SecureEmbedHost::GetLocalSurfaceId() const {
+  NOTIMPLEMENTED();
+  static const viz::LocalSurfaceId local_surface_id;
+  return local_surface_id;
+}
+
+const blink::mojom::ViewportIntersectionState&
+SecureEmbedHost::GetIntersectionState() const {
+  NOTIMPLEMENTED();
+  static const base::NoDestructor<blink::mojom::ViewportIntersectionState>
+      intersection_state;
+  return *intersection_state;
+}
+
+uint32_t SecureEmbedHost::GetCaptureSequenceNumber() const {
+  NOTIMPLEMENTED();
+  return 0;
+}
+
+const gfx::Rect& SecureEmbedHost::GetRectInParentViewInDip() const {
+  NOTIMPLEMENTED();
+  static const gfx::Rect rect;
+  return rect;
+}
+
+const gfx::Size& SecureEmbedHost::GetLocalFrameSizeInDip() const {
+  NOTIMPLEMENTED();
+  static const gfx::Size size;
+  return size;
+}
+
+const gfx::Size& SecureEmbedHost::GetLocalFrameSizeInPixels() const {
+  NOTIMPLEMENTED();
+  static const gfx::Size size;
+  return size;
+}
+
+double SecureEmbedHost::GetCssZoomFactor() const {
+  NOTIMPLEMENTED();
+  return 1.0;
+}
+
+void SecureEmbedHost::EnableAutoResize(const gfx::Size& min_size,
+                                       const gfx::Size& max_size) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::DisableAutoResize() {
+  NOTIMPLEMENTED();
+}
+
+bool SecureEmbedHost::IsInert() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+cc::TouchAction SecureEmbedHost::InheritedEffectiveTouchAction() const {
+  NOTIMPLEMENTED();
+  return cc::TouchAction::kAuto;
+}
+
+bool SecureEmbedHost::IsHidden() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool SecureEmbedHost::IsThrottled() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool SecureEmbedHost::IsSubtreeThrottled() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+bool SecureEmbedHost::IsDisplayLocked() const {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void SecureEmbedHost::DidUpdateVisualProperties(
+    const cc::RenderFrameMetadata& metadata) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::SetVisibilityForChildViews(bool visible) const {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::SetLocalFrameSize(const gfx::Size& local_frame_size) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::SetRectInParentView(
+    const gfx::Rect& rect_in_parent_view) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::SetIsInert(bool inert) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::OnSetInheritedEffectiveTouchAction(cc::TouchAction) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::OnVisibilityChanged(
+    blink::mojom::FrameVisibility visibility) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::UpdateRenderThrottlingStatus(bool is_throttled,
+                                                   bool subtree_throttled,
+                                                   bool display_locked) {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::UpdateViewportIntersection(
+    const blink::mojom::ViewportIntersectionState& intersection_state,
+    const std::optional<blink::FrameVisualProperties>& visual_properties) {
+  NOTIMPLEMENTED();
+}
+
+bool SecureEmbedHost::IsVisible() {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+void SecureEmbedHost::DelegateWasShown() {
+  NOTIMPLEMENTED();
+}
+
+void SecureEmbedHost::OnSynchronizeVisualProperties(
+    const blink::FrameVisualProperties& visual_properties) {
+  NOTIMPLEMENTED();
+}
+
+content::Visibility SecureEmbedHost::EmbedderVisibility() {
+  NOTIMPLEMENTED();
+  return content::Visibility::HIDDEN;
+}
+
+input::RenderWidgetHostViewInput* SecureEmbedHost::GetParentViewInput() {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+input::RenderWidgetHostViewInput* SecureEmbedHost::GetRootViewInput() {
+  NOTIMPLEMENTED();
+  return nullptr;
 }
 
 }  // namespace secure_embed
--- components/secure_embed/browser/secure_embed_host.h
+++ components/secure_embed/browser/secure_embed_host.h
@@ -6,7 +6,9 @@
 #define COMPONENTS_SECURE_EMBED_BROWSER_SECURE_EMBED_HOST_H_
 
 #include "base/component_export.h"
+#include "base/notimplemented.h"
 #include "components/secure_embed/common/secure_embed.mojom.h"
+#include "content/public/browser/cross_process_frame_connector_base.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/self_owned_associated_receiver.h"
 
@@ -17,7 +19,8 @@ class RenderFrameHost;
 namespace secure_embed {
 
 class COMPONENT_EXPORT(SECURE_EMBED) SecureEmbedHost
-    : public mojom::SecureEmbedHost {
+    : public mojom::SecureEmbedHost,
+      public content::CrossProcessFrameConnectorBase {
  public:
   ~SecureEmbedHost() override;
 
@@ -28,10 +31,74 @@ class COMPONENT_EXPORT(SECURE_EMBED) SecureEmbedHost
       content::RenderFrameHost* render_frame_host,
       mojo::PendingAssociatedReceiver<mojom::SecureEmbedHost> receiver);
 
+  static size_t GetInstanceCountForTesting();
+
   // mojom::SecureEmbedHost implementation:
   void Attach(int64_t content_id) override;
 
-  static size_t GetInstanceCountForTesting();
+  // content::CrossProcessFrameConnectorBase:
+  void SetView(content::RenderWidgetHostViewChildFrame* view,
+               bool allow_paint_holding) override;
+  content::RenderWidgetHostViewBase* GetParentRenderWidgetHostView() override;
+  content::RenderWidgetHostViewBase* GetRootRenderWidgetHostView() override;
+  void RenderProcessGone() override;
+  void FirstSurfaceActivation(const viz::SurfaceInfo& surface_info) override;
+  void SendIntrinsicSizingInfoToParent(
+      blink::mojom::IntrinsicSizingInfoPtr) override;
+  void SynchronizeVisualProperties(
+      const blink::FrameVisualProperties& visual_properties,
+      bool propagate) override;
+  void UpdateCursor(const ui::Cursor& cursor) override;
+  RootViewFocusState HasFocus() override;
+  void FocusRootView() override;
+  blink::mojom::PointerLockResult LockPointer(
+      bool request_unadjusted_movement) override;
+  blink::mojom::PointerLockResult ChangePointerLock(
+      bool request_unadjusted_movement) override;
+  void UnlockPointer() override;
+  bool HasSize() const override;
+  const display::ScreenInfos& GetScreenInfos() const override;
+  const viz::LocalSurfaceId& GetLocalSurfaceId() const override;
+  const blink::mojom::ViewportIntersectionState& GetIntersectionState()
+      const override;
+  uint32_t GetCaptureSequenceNumber() const override;
+  const gfx::Rect& GetRectInParentViewInDip() const override;
+  const gfx::Size& GetLocalFrameSizeInDip() const override;
+  const gfx::Size& GetLocalFrameSizeInPixels() const override;
+  double GetCssZoomFactor() const override;
+  void EnableAutoResize(const gfx::Size& min_size,
+                        const gfx::Size& max_size) override;
+  void DisableAutoResize() override;
+  bool IsInert() const override;
+  cc::TouchAction InheritedEffectiveTouchAction() const override;
+  bool IsHidden() const override;
+  bool IsThrottled() const override;
+  bool IsSubtreeThrottled() const override;
+  bool IsDisplayLocked() const override;
+  void DidUpdateVisualProperties(
+      const cc::RenderFrameMetadata& metadata) override;
+  void SetVisibilityForChildViews(bool visible) const override;
+  void SetLocalFrameSize(const gfx::Size& local_frame_size) override;
+  void SetRectInParentView(const gfx::Rect& rect_in_parent_view) override;
+  void SetIsInert(bool inert) override;
+  void OnSetInheritedEffectiveTouchAction(cc::TouchAction) override;
+  void OnVisibilityChanged(blink::mojom::FrameVisibility visibility) override;
+  void UpdateRenderThrottlingStatus(bool is_throttled,
+                                    bool subtree_throttled,
+                                    bool display_locked) override;
+  void UpdateViewportIntersection(
+      const blink::mojom::ViewportIntersectionState& intersection_state,
+      const std::optional<blink::FrameVisualProperties>& visual_properties)
+      override;
+  bool IsVisible() override;
+  void DelegateWasShown() override;
+  void OnSynchronizeVisualProperties(
+      const blink::FrameVisualProperties& visual_properties) override;
+  content::Visibility EmbedderVisibility() override;
+
+  // input::ChildFrameInputHelper::Delegate:
+  input::RenderWidgetHostViewInput* GetParentViewInput() override;
+  input::RenderWidgetHostViewInput* GetRootViewInput() override;
 
  private:
   explicit SecureEmbedHost(content::RenderFrameHost* render_frame_host);
