f2f1bffb57b0bb04dab849ed8a620daaf6fc910a
Rafał Godlewski <rgod@google.com>
2025-10-10 11:20:45 -0700

    Reland "[iOS] Fetch keys for credential export"
    This is a reland of commit aa6cf7b99213ca8cdd8653dac829e749b79496ff
    
    The CL is now adding a correct passkey keychain provider dependency
    
    Original change's description:
    > [iOS] Fetch keys for credential export
    >
    > Adds fetching security domain secret in the credential export flow. For
    > now it's just fetched every time the flow is accepted and the passkey
    > welcome screens are not being displayed.
    >
    > In the follow-up CLs the welcome screens will be added and the secret
    > will only be fetched if there are any passkeys present in the export
    > list.
    >
    > Bug: 449701042
    > Change-Id: Ie551c7fd8101554e972a3a5048c0107d2c7b14a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7008379
    > Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    > Commit-Queue: Rafał Godlewski <rgod@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1525850}
    
    Bug: 449701042
    Change-Id: I94399ff6bc70f8086df2eb6c2da7935074906f28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7021570
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1528254}

--- ios/chrome/browser/webauthn/coordinator/credential_export_coordinator.mm
+++ ios/chrome/browser/webauthn/coordinator/credential_export_coordinator.mm
@@ -6,12 +6,21 @@
 
 #import "base/memory/raw_ptr.h"
 #import "components/password_manager/core/browser/ui/saved_passwords_presenter.h"
+#import "components/signin/public/identity_manager/account_info.h"
+#import "components/signin/public/identity_manager/identity_manager.h"
+#import "ios/chrome/browser/settings/ui_bundled/password/create_password_manager_title_view.h"
+#import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 #import "ios/chrome/browser/webauthn/coordinator/credential_export_mediator.h"
 #import "ios/chrome/browser/webauthn/ui/credential_export_view_controller.h"
 #import "ios/chrome/browser/webauthn/ui/credential_export_view_controller_presentation_delegate.h"
+#import "ios/chrome/common/credential_provider/passkey_keychain_provider_bridge.h"
+#import "ios/chrome/common/ui/elements/branded_navigation_item_title_view.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util.h"
 
 @interface CredentialExportCoordinator () <
-    CredentialExportViewControllerPresentationDelegate>
+    CredentialExportViewControllerPresentationDelegate,
+    PasskeyKeychainProviderBridgeDelegate>
 @end
 
 @implementation CredentialExportCoordinator {
@@ -23,6 +32,9 @@
 
   // Used to fetch the user's saved passwords for export.
   raw_ptr<password_manager::SavedPasswordsPresenter> _savedPasswordsPresenter;
+
+  // Bridge to the PasskeyKeychainProvider that manages passkey vault keys.
+  PasskeyKeychainProviderBridge* _passkeyKeychainProviderBridge;
 }
 
 @synthesize baseNavigationController = _baseNavigationController;
@@ -56,8 +68,64 @@
 #pragma mark - CredentialExportViewControllerPresentationDelegate
 
 - (void)userDidStartExport {
+  // TODO(crbug.com/449701042): Only fetch keys if there are selected passkeys.
+  _passkeyKeychainProviderBridge = [[PasskeyKeychainProviderBridge alloc]
+        initWithEnableLogging:NO
+         navigationController:_baseNavigationController
+      navigationItemTitleView:password_manager::CreatePasswordManagerTitleView(
+                                  l10n_util::GetNSString(
+                                      IDS_IOS_PASSWORD_MANAGER))];
+  _passkeyKeychainProviderBridge.delegate = self;
+
+  CoreAccountInfo account =
+      IdentityManagerFactory::GetForProfile(self.profile)
+          ->GetPrimaryAccountInfo(signin::ConsentLevel::kSignin);
+
+  __weak __typeof(self) weakSelf = self;
+  [_passkeyKeychainProviderBridge
+      fetchSecurityDomainSecretForGaia:account.gaia.ToNSString()
+                            credential:nil
+                               // TODO(crbug.com/449701042): Consider adding new
+                               // reauth purpose.
+                               purpose:PasskeyKeychainProvider::
+                                           ReauthenticatePurpose::kEncrypt
+                            completion:^(
+                                NSArray<NSData*>* securityDomainSecrets) {
+                              [weakSelf startExportWithSecurityDomainSecrets:
+                                            securityDomainSecrets];
+                            }];
+}
+
+#pragma mark - PasskeyKeychainProviderBridgeDelegate
+
+- (void)performUserVerificationIfNeeded:(ProceduralBlock)completion {
+  // TODO(crbug.com/449701042): Perform user verification.
+  completion();
+}
+
+// TODO(crbug.com/449701042): Display all types of welcome views.
+- (void)showEnrollmentWelcomeScreen:(ProceduralBlock)enrollBlock {
+  enrollBlock();
+}
+
+- (void)showFixDegradedRecoverabilityWelcomeScreen:
+    (ProceduralBlock)fixDegradedRecoverabilityBlock {
+  fixDegradedRecoverabilityBlock();
+}
+
+- (void)showReauthenticationWelcomeScreen:(ProceduralBlock)reauthenticateBlock {
+  reauthenticateBlock();
+}
+
+- (void)providerDidCompleteReauthentication {
+}
+
+#pragma mark - Private
+
+- (void)startExportWithSecurityDomainSecrets:
+    (NSArray<NSData*>*)securityDomainSecrets {
   if (@available(iOS 26, *)) {
-    [_mediator startExport];
+    [_mediator startExportWithSecurityDomainSecrets:securityDomainSecrets];
   }
 }
 
