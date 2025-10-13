d56a404c842249000691b1f84bc8502b7b0ad427
Lukasz Anforowicz <lukasza@chromium.org>
2025-10-13 16:02:17 -0700

    [rust] Prepare to switch Rust language edition to 2024 (2/N).
    This CL makes preparatory (mostly no-op) changes for switching the
    default Rust language edition from 2021 to 2024.
    
    Bug: 450891119
    Change-Id: I0882b7a21ed201210054e6c977319bf0e1b9b15f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7038304
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529186}

--- codelabs/rust/solutions/exercise_1_build_rules/hellolib.rs
+++ codelabs/rust/solutions/exercise_1_build_rules/hellolib.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn hello_from_rust() {
     println!("Hello from Rust!")
 }
--- codelabs/rust/solutions/exercise_2_testing/addnumberslib.rs
+++ codelabs/rust/solutions/exercise_2_testing/addnumberslib.rs
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub fn add_numbers(a: i32, b: i32) -> i32 {
     a + b
 }
