e8675c363be53546e1f5165cf5cc0d1f49331472
dongguoz <dongguoz@google.com>
2025-10-13 10:28:33 -0700

    Refactor: Enable ShapeDetectionService to content/public/browser
    This CL allows the ShapeDetectionService to be used by other
    components. This is a refactoring that does not change any
    functionality
    
    Design doc: go/shape-detection-service-content-public-api
    
    Bug: 432158448
    Change-Id: I6bcc5fe62b66e973a18d186774ffc7fc0f39a8c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6992001
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Dongguo Zhang <dongguo@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528993}

--- content/browser/shape_detection/shape_detection_service.cc
+++ content/browser/shape_detection/shape_detection_service.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/shape_detection/shape_detection_service_host.h"
+#include "content/public/browser/shape_detection_service.h"
 
 #include "base/no_destructor.h"
 #include "build/branding_buildflags.h"
