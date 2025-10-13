1bffe212755a9cc4e0059bfa69ae1d3dd2bf3953
Yihong Guo <yhguo@google.com>
2025-10-13 12:29:08 -0700

    [Save to Drive] Improve upload time estimation using `RateEstimator`
    This change updates the `TimeRemainingCalculator` by integrating the
    `download::RateEstimator`. This provides a more normalized and stable
    upload speed calculation, which in turn reduces the abrupt, jerky
    changes in the estimated time remaining, especially for users on
    unstable network connections.
    
    The test `CalculateTimeRemainingTextAfterUploadRestarted` is removed
    since the `TimeRemainingCalculator` instance won't be reused in between
    uploads.
    
    Demo:
    https://screencast.googleplex.com/cast/NjE5Njg2NTgxMTg3Mzc5MnxhN2UyMjBiNy1mZQ
    
    Bug: 450585575
    Change-Id: I4127d51e3c59ecd5daa26e50230f8ef7d2d5644b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7030588
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Yihong Guo <yhguo@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529064}

--- chrome/browser/save_to_drive/time_remaining_calculator.h
+++ chrome/browser/save_to_drive/time_remaining_calculator.h
@@ -9,7 +9,7 @@
 #include <string>
 
 #include "base/byte_count.h"
-#include "base/time/time.h"
+#include "components/download/public/common/rate_estimator.h"
 
 namespace extensions::api::pdf_viewer_private {
 struct SaveToDriveProgress;
@@ -27,26 +27,22 @@ class TimeRemainingCalculator {
 
   // Calculates and returns a human-readable string for the estimated time
   // remaining for the save to drive operation. Returns std::nullopt if the
-  // upload speed is zero or progress is invalid. Determines the upload speed by
-  // comparing against the progress from the previous call, so this should be
-  // called with each new update. This should only be called when the progress
+  // upload speed is zero or progress is invalid. It determines the upload speed
+  // by using the `RateEstimator`. This should only be called when the progress
   // status is `kUploadStarted` or `kUploadInProgress`.
   virtual std::optional<std::u16string> CalculateTimeRemainingText(
       const extensions::api::pdf_viewer_private::SaveToDriveProgress& progress);
 
  private:
-  // Returns the upload speed in bytes per second.
-  int GetUploadSpeed(const base::ByteCount& uploaded_bytes) const;
-
   // Returns the remaining time for the save to drive operation.
   std::optional<base::TimeDelta> GetRemainingTime(
       const base::ByteCount& uploaded_bytes,
       const base::ByteCount& file_size_bytes) const;
 
-  // The last time the upload speed was updated.
-  base::TimeTicks last_upload_speed_update_time_;
   // The last uploaded bytes.
   base::ByteCount last_uploaded_bytes_;
+  // Used to get a smooth estimate of the upload speed.
+  download::RateEstimator upload_speed_estimator_;
 };
 
 }  // namespace save_to_drive
--- chrome/browser/save_to_drive/time_remaining_calculator_unittest.cc
+++ chrome/browser/save_to_drive/time_remaining_calculator_unittest.cc
@@ -42,56 +42,70 @@ class SaveToDriveTimeRemainingCalculatorTest : public testing::Test {
 };
 
 TEST_F(SaveToDriveTimeRemainingCalculatorTest,
-       CalculateTimeRemainingTextWithNoTimePassed) {
+       CalculateTimeRemainingTextWithNoAdditionalBytesUploaded) {
   pdf_api::SaveToDriveProgress progress = CreateUploadStartedProgress();
+  progress.status = pdf_api::SaveToDriveStatus::kUploadInProgress;
   EXPECT_EQ(calculator().CalculateTimeRemainingText(progress), std::nullopt);
 
-  progress.status = pdf_api::SaveToDriveStatus::kUploadInProgress;
-  progress.uploaded_bytes = 10 * 1024 * 1024;
+  FastForwardBy(base::Seconds(10));
   EXPECT_EQ(calculator().CalculateTimeRemainingText(progress), std::nullopt);
 
-  progress.status = pdf_api::SaveToDriveStatus::kUploadInProgress;
-  progress.uploaded_bytes = 50 * 1024 * 1024;
+  FastForwardBy(base::Minutes(10));
   EXPECT_EQ(calculator().CalculateTimeRemainingText(progress), std::nullopt);
 }
 
 TEST_F(SaveToDriveTimeRemainingCalculatorTest,
        CalculateTimeRemainingTextAfterTimePassed) {
   pdf_api::SaveToDriveProgress progress = CreateUploadStartedProgress();
+  progress.status = pdf_api::SaveToDriveStatus::kUploadInProgress;
   EXPECT_EQ(calculator().CalculateTimeRemainingText(progress), std::nullopt);
 
-  progress.status = pdf_api::SaveToDriveStatus::kUploadInProgress;
+  // Speed estimate: 1 MiB/s. Remaining: 90 MiB.
+  // Time: 90 MiB / 1 MiB/s = 90s.
   progress.uploaded_bytes = 10 * 1024 * 1024;
   FastForwardBy(base::Seconds(10));
   EXPECT_THAT(calculator().CalculateTimeRemainingText(progress),
               testing::Optional(std::u16string_view(u"2 minutes left")));
 
-  progress.uploaded_bytes = 40 * 1024 * 1024;
-  FastForwardBy(base::Seconds(6));
-  EXPECT_THAT(calculator().CalculateTimeRemainingText(progress),
-              testing::Optional(std::u16string_view(u"12 seconds left")));
-}
-
-TEST_F(SaveToDriveTimeRemainingCalculatorTest,
-       CalculateTimeRemainingTextAfterUploadRestarted) {
-  pdf_api::SaveToDriveProgress progress = CreateUploadStartedProgress();
-  EXPECT_EQ(calculator().CalculateTimeRemainingText(progress), std::nullopt);
-
-  progress.status = pdf_api::SaveToDriveStatus::kUploadInProgress;
-  progress.uploaded_bytes = 10 * 1024 * 1024;
+  // Speed estimate: 1 MiB/s. Remaining: 80 MiB.
+  // Time: 80 MiB / 1 MiB/s = 80s.
+  progress.uploaded_bytes = 20 * 1024 * 1024;
   FastForwardBy(base::Seconds(10));
   EXPECT_THAT(calculator().CalculateTimeRemainingText(progress),
-              testing::Optional(std::u16string_view(u"2 minutes left")));
+              testing::Optional(std::u16string_view(u"1 minute left")));
 
-  progress.status = pdf_api::SaveToDriveStatus::kUploadStarted;
-  progress.uploaded_bytes = 0;
-  EXPECT_EQ(calculator().CalculateTimeRemainingText(progress), std::nullopt);
+  // Speed estimate: 0 MiB/s.
+  // Time: no speed output, so no estimate.
+  FastForwardBy(base::Seconds(10));
+  EXPECT_THAT(calculator().CalculateTimeRemainingText(progress), std::nullopt);
 
-  progress.status = pdf_api::SaveToDriveStatus::kUploadInProgress;
-  progress.uploaded_bytes = 30 * 1024 * 1024;
-  FastForwardBy(base::Minutes(2));
+  // Speed estimate: 3 MiB/s. Remaining: 50 MiB.
+  // Time: 50 MiB / 3 MiB/s = 16.67s.
+  progress.uploaded_bytes = 50 * 1024 * 1024;
+  FastForwardBy(base::Seconds(10));
   EXPECT_THAT(calculator().CalculateTimeRemainingText(progress),
-              testing::Optional(std::u16string_view(u"5 minutes left")));
+              testing::Optional(std::u16string_view(u"16 seconds left")));
+
+  // Speed estimate: 0.1 MiB/s. Remaining: 49 MiB.
+  // Time: 49 MiB / 0.1 MiB/s = 490s (8.16 minutes).
+  progress.uploaded_bytes = 51 * 1024 * 1024;
+  FastForwardBy(base::Seconds(10));
+  EXPECT_THAT(calculator().CalculateTimeRemainingText(progress),
+              testing::Optional(std::u16string_view(u"8 minutes left")));
+
+  // Speed estimate: 4 MiB/s. Remaining: 45 MiB.
+  // Time: 45/4 = 11.25s.
+  progress.uploaded_bytes = 55 * 1024 * 1024;
+  FastForwardBy(base::Seconds(15));
+  EXPECT_THAT(calculator().CalculateTimeRemainingText(progress),
+              testing::Optional(std::u16string_view(u"11 seconds left")));
+
+  // Speed estimate: 1 MiB/s. Remaining: 44 MiB.
+  // Time: 44/1 = 44s.
+  progress.uploaded_bytes = 56 * 1024 * 1024;
+  FastForwardBy(base::Seconds(20));
+  EXPECT_THAT(calculator().CalculateTimeRemainingText(progress),
+              testing::Optional(std::u16string_view(u"44 seconds left")));
 }
 
 }  // namespace
