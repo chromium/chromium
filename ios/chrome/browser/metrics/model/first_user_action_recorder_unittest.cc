9125b2d11feb82f912ba4dc7b1a3bf2cb1182dd7
Olivier Robin <olivierrobin@google.com>
2025-10-13 09:42:20 -0700

    Add bucket checs in FirstUserActionRecorder tests
    Fixed: 41211458
    Change-Id: I8f6a74bfeeff89b97c49e46f9c2ee561c1e180da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7038011
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1528956}

--- ios/chrome/browser/metrics/model/first_user_action_recorder_unittest.cc
+++ ios/chrome/browser/metrics/model/first_user_action_recorder_unittest.cc
@@ -50,10 +50,8 @@ TEST_F(FirstUserActionRecorderTest, Expire) {
       FirstUserActionRecorder::EXPIRATION, 1);
 
   // Verify the expiration histogram contains a single duration value.
-  // TODO(crbug.com/41211458): Ideally this would also verify the value is in
-  // the correct bucket.
-  histogram_tester_->ExpectTotalCount(
-      kFirstUserActionExpirationHistogramName[is_pad_], 1);
+  histogram_tester_->ExpectUniqueSample(
+      kFirstUserActionExpirationHistogramName[is_pad_], 1, 1);
 }
 
 TEST_F(FirstUserActionRecorderTest, RecordStartOnNTP) {
@@ -76,10 +74,8 @@ TEST_F(FirstUserActionRecorderTest, OnUserAction_Continuation) {
       FirstUserActionRecorder::CONTINUATION, 1);
 
   // Verify the continuation histogram contains a single duration value.
-  // TODO(crbug.com/41211458): Ideally this would also verify the value is in
-  // the correct bucket.
-  histogram_tester_->ExpectTotalCount(
-      kFirstUserActionContinuationHistogramName[is_pad_], 1);
+  histogram_tester_->ExpectUniqueSample(
+      kFirstUserActionContinuationHistogramName[is_pad_], 1, 1);
 }
 
 TEST_F(FirstUserActionRecorderTest, OnUserAction_NewTask) {
@@ -92,10 +88,8 @@ TEST_F(FirstUserActionRecorderTest, OnUserAction_NewTask) {
       FirstUserActionRecorder::NEW_TASK, 1);
 
   // Verify the 'new task' histogram contains a single duration value.
-  // TODO(crbug.com/41211458): Ideally this would also verify the value is in
-  // the correct bucket.
-  histogram_tester_->ExpectTotalCount(
-      kFirstUserActionNewTaskHistogramName[is_pad_], 1);
+  histogram_tester_->ExpectUniqueSample(
+      kFirstUserActionNewTaskHistogramName[is_pad_], 1, 1);
 }
 
 TEST_F(FirstUserActionRecorderTest, OnUserAction_Ignored) {
@@ -125,10 +119,8 @@ TEST_F(FirstUserActionRecorderTest, OnUserAction_RethrowAction_Continuation) {
       FirstUserActionRecorder::CONTINUATION, 1);
 
   // Verify the continuation histogram contains a single duration value.
-  // TODO(crbug.com/41211458): Ideally this would also verify the value is in
-  // the correct bucket.
-  histogram_tester_->ExpectTotalCount(
-      kFirstUserActionContinuationHistogramName[is_pad_], 1);
+  histogram_tester_->ExpectUniqueSample(
+      kFirstUserActionContinuationHistogramName[is_pad_], 1, 1);
 }
 
 TEST_F(FirstUserActionRecorderTest, OnUserAction_RethrowAction_NewTask) {
@@ -142,8 +134,6 @@ TEST_F(FirstUserActionRecorderTest, OnUserAction_RethrowAction_NewTask) {
       FirstUserActionRecorder::NEW_TASK, 1);
 
   // Verify the 'new task' histogram contains the a single duration value.
-  // TODO(crbug.com/41211458): Ideally this would also verify the value is in
-  // the correct bucket.
-  histogram_tester_->ExpectTotalCount(
-      kFirstUserActionNewTaskHistogramName[is_pad_], 1);
+  histogram_tester_->ExpectUniqueSample(
+      kFirstUserActionNewTaskHistogramName[is_pad_], 1, 1);
 }
