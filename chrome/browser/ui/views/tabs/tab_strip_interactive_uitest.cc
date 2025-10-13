89a238a91984443e253623644501b813f2b6ba56
Dominic Austria <dominicaustria@google.com>
2025-10-13 13:52:06 -0700

    Reland "Add split view to new tab button context menu"
    This is a reland of commit 629461e9d788c475f946dfd7d2e1beede0dbae2f
    
    Original change's description:
    > Add split view to new tab button context menu
    >
    > This adds another option to the new tab button context menu. It will
    > make a split view with the current active tab, and the new split will
    > be on the new tab page.
    >
    > Bug: 450398347
    > Change-Id: I04dcf3b5d6d4a1577be781520d78af9780f5008c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7024472
    > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    > Reviewed-by: Alison Gale <agale@chromium.org>
    > Commit-Queue: Dominic Austria <dominicaustria@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1528279}
    
    Bug: 450398347
    Change-Id: I6f815535327f557d1dd63b5d00a788600fa37136
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7035051
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Dominic Austria <dominicaustria@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1529118}

--- chrome/browser/ui/views/tabs/tab_strip_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_strip_interactive_uitest.cc
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/tabs/split_tab_metrics.h"
 #include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "chrome/browser/ui/views/tabs/browser_tab_strip_controller.h"
 #include "chrome/browser/ui/views/tabs/new_tab_button.h"
@@ -77,12 +79,11 @@ IN_PROC_BROWSER_TEST_F(TabStripInteractiveUiTest, HoverEffectShowsOnMouseOver) {
 DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(ui::test::PollingStateObserver<int>,
                                     kTabCountState);
 
-class TabStripTabGroupMenuMoreEntryPointsEnabled
-    : public TabStripInteractiveUiTest {
+class TestNewTabButtonContextMenu : public TabStripInteractiveUiTest {
  public:
-  TabStripTabGroupMenuMoreEntryPointsEnabled() {
-    scoped_feature_list_.InitAndEnableFeature(
-        features::kTabGroupMenuMoreEntryPoints);
+  TestNewTabButtonContextMenu() {
+    scoped_feature_list_.InitWithFeatures(
+        {features::kTabGroupMenuMoreEntryPoints, features::kSideBySide}, {});
   }
 
   TabStrip* tabstrip() {
@@ -107,7 +108,7 @@ class TabStripTabGroupMenuMoreEntryPointsEnabled
 // TODO (crbug.com/447617263) rewrite these tests so that they work on mac and
 // enable them there so that it works on mac and re-enable it.
 #if !BUILDFLAG(IS_MAC)
-IN_PROC_BROWSER_TEST_F(TabStripTabGroupMenuMoreEntryPointsEnabled,
+IN_PROC_BROWSER_TEST_F(TestNewTabButtonContextMenu,
                        VerifyNewTabButtonContextMenu) {
   RunTestSequence(
       FinishTabstripAnimations(), EnsurePresent(kNewTabButtonElementId),
@@ -116,12 +117,52 @@ IN_PROC_BROWSER_TEST_F(TabStripTabGroupMenuMoreEntryPointsEnabled,
           ClickMouse(ui_controls::RIGHT),
           EnsurePresent(NewTabButtonMenuModel::kNewTab),
           EnsurePresent(NewTabButtonMenuModel::kNewTabInGroup),
+          EnsurePresent(NewTabButtonMenuModel::kNewSplitView),
           EnsurePresent(NewTabButtonMenuModel::kCreateNewTabGroup),
           SendAccelerator(NewTabButtonMenuModel::kNewTab,
                           ui::Accelerator(ui::VKEY_ESCAPE, ui::EF_NONE))));
 }
 
-IN_PROC_BROWSER_TEST_F(TabStripTabGroupMenuMoreEntryPointsEnabled,
+IN_PROC_BROWSER_TEST_F(TestNewTabButtonContextMenu,
+                       NewTabButtonContextMenuSplitView) {
+  RunTestSequence(FinishTabstripAnimations(),
+                  EnsurePresent(kNewTabButtonElementId),
+                  MoveMouseTo(kNewTabButtonElementId),
+                  MayInvolveNativeContextMenu(
+                      ClickMouse(ui_controls::RIGHT),
+                      EnsurePresent(NewTabButtonMenuModel::kNewTab),
+                      EnsurePresent(NewTabButtonMenuModel::kNewSplitView),
+                      SelectMenuItem(NewTabButtonMenuModel::kNewSplitView)));
+
+  BrowserView* browser_view = static_cast<BrowserView*>(browser()->window());
+
+  // Split view should be open
+  EXPECT_TRUE(browser_view->IsInSplitView());
+}
+
+IN_PROC_BROWSER_TEST_F(TestNewTabButtonContextMenu,
+                       NewTabButtonContextMenuSplitViewDisabled) {
+  chrome::NewSplitTab(browser(),
+                      split_tabs::SplitTabCreatedSource::kNewTabButton);
+  RunTestSequence(
+      FinishTabstripAnimations(), EnsurePresent(kNewTabButtonElementId),
+      MoveMouseTo(kNewTabButtonElementId),
+      MayInvolveNativeContextMenu(
+          ClickMouse(ui_controls::RIGHT),
+          EnsurePresent(NewTabButtonMenuModel::kNewTab),
+          EnsurePresent(NewTabButtonMenuModel::kNewSplitView),
+          WaitForViewProperty(NewTabButtonMenuModel::kNewSplitView, views::View,
+                              Enabled, false),
+          SendAccelerator(NewTabButtonMenuModel::kNewTab,
+                          ui::Accelerator(ui::VKEY_ESCAPE, ui::EF_NONE))));
+
+  BrowserView* browser_view = static_cast<BrowserView*>(browser()->window());
+
+  // Split view should be open
+  EXPECT_TRUE(browser_view->IsInSplitView());
+}
+
+IN_PROC_BROWSER_TEST_F(TestNewTabButtonContextMenu,
                        NewTabButtonNewTabInGroupDisabledWhenNoOpenGroups) {
   RunTestSequence(
       EnsurePresent(kNewTabButtonElementId),
@@ -135,7 +176,7 @@ IN_PROC_BROWSER_TEST_F(TabStripTabGroupMenuMoreEntryPointsEnabled,
                           ui::Accelerator(ui::VKEY_ESCAPE, ui::EF_NONE))));
 }
 
-IN_PROC_BROWSER_TEST_F(TabStripTabGroupMenuMoreEntryPointsEnabled,
+IN_PROC_BROWSER_TEST_F(TestNewTabButtonContextMenu,
                        NewTabButtonNewTabInMostRecentGroup) {
   controller()->CreateNewTab();
   controller()->CreateNewTab();
@@ -167,4 +208,32 @@ IN_PROC_BROWSER_TEST_F(TabStripTabGroupMenuMoreEntryPointsEnabled,
           },
           2));
 }
+
+class TestNewTabButtonContextMenuSideBySideDisabled
+    : public TabStripInteractiveUiTest {
+ public:
+  TestNewTabButtonContextMenuSideBySideDisabled() {
+    scoped_feature_list_.InitWithFeatures(
+        {features::kTabGroupMenuMoreEntryPoints}, {features::kSideBySide});
+  }
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(TestNewTabButtonContextMenuSideBySideDisabled,
+                       VerifyNewTabButtonContextMenuSplitViewNotPresent) {
+  RunTestSequence(
+      FinishTabstripAnimations(), EnsurePresent(kNewTabButtonElementId),
+      MoveMouseTo(kNewTabButtonElementId),
+      MayInvolveNativeContextMenu(
+          ClickMouse(ui_controls::RIGHT),
+          EnsurePresent(NewTabButtonMenuModel::kNewTab),
+          EnsurePresent(NewTabButtonMenuModel::kNewTabInGroup),
+          EnsurePresent(NewTabButtonMenuModel::kCreateNewTabGroup),
+          EnsureNotPresent(NewTabButtonMenuModel::kNewSplitView),
+          SendAccelerator(NewTabButtonMenuModel::kNewTab,
+                          ui::Accelerator(ui::VKEY_ESCAPE, ui::EF_NONE))));
+}
+
 #endif  // !BUILDFLAG(IS_MAC)
