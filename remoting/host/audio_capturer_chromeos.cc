3537d97c2960982270b15eebc2265815995e0d30
Michael Ershov <miersh@google.com>
2025-10-13 03:37:42 -0700

    Revert "Boca: Implement AudioCapturer to provide CRD host audio"
    This reverts commit 9dde7fef38c04494db381e8b85871f3b5f7c7da0.
    
    Reason for revert: 
    The tests consistently fail with a sanitizer, more info on the attached bug.
    
    Original change's description:
    > Boca: Implement AudioCapturer to provide CRD host audio
    >
    > This change introduces `AudioCapturerChromeOs`, an implementation of the
    > `AudioCapturer` interface to support capturing system audio for CrOS
    > CRD hosts.
    >
    > `AudioCapturerChromeOs` is created by CRD and manages the overall state
    > of the audio capture and provides the captured audio packets back to
    > CRD.
    >
    > Interactions with CRAS (ChromeOS Audio Server) must occur on the
    > dedicated audio thread. To achieve this the helper class
    > `AudioHelperChromeos` is introduced and owned by
    > `AudioCapturerChromeOs`.
    >
    > Bug: b/449194420
    > Change-Id: I3f6f93f1e32aa4835ee5aa1ec35a7826f053dad1
    > Tests: remoting_unittests --gtest_filter=*AudioCapturerChromeOsTest*
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023837
    > Reviewed-by: Joe Downing <joedow@chromium.org>
    > Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1528432}
    
    Bug: b/449194420
    Change-Id: I5a9aa4de4d86264918e5ff4ee02cfce332fcc6cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7035652
    Owners-Override: Michael Ershov <miersh@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1528822}

--- remoting/host/audio_capturer_chromeos.cc
+++ remoting/host/audio_capturer_chromeos.cc
@@ -2,86 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "remoting/host/audio_capturer_chromeos.h"
+#include "remoting/host/audio_capturer.h"
 
-#include <memory>
-#include <utility>
-
-#include "ash/constants/ash_features.h"
-#include "base/functional/bind.h"
-#include "base/logging.h"
-#include "base/sequence_checker.h"
-#include "base/task/sequenced_task_runner.h"
-#include "base/threading/sequence_bound.h"
-#include "media/audio/audio_manager.h"
-#include "remoting/host/chromeos/audio_helper_chromeos.h"
-#include "remoting/host/chromeos/audio_helper_chromeos_impl.h"
-#include "remoting/proto/audio.pb.h"
+#include "base/notimplemented.h"
 
 namespace remoting {
 
-AudioCapturerChromeOs::AudioCapturerChromeOs(
-    std::unique_ptr<AudioHelperChromeOs> audio_helper_chromeos)
-    : audio_helper_chromeos_(media::AudioManager::Get()->GetTaskRunner(),
-                             std::move(audio_helper_chromeos)) {}
-
-AudioCapturerChromeOs::~AudioCapturerChromeOs() = default;
-
-bool AudioCapturerChromeOs::Start(const PacketCapturedCallback& callback) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  packet_captured_callback_ = callback;
-
-  // Post the StartAudioStream call to the audio thread, as all interactions
-  // with AudioManager must happen on that thread. We pass the current
-  // SequencedTaskRunner so the AudioHelperChromeos can post
-  // HandleAudioData and HandleAudioError callbacks back to this main sequence.
-  //
-  // Note: AudioCapturerChromeOs currently runs on the main sequence. If we
-  // observe performance issues like audio packet delays, we may need to
-  // revisit this and move AudioCapturerChromeOs to its own  higher-priority
-  // thread.
-  audio_helper_chromeos_.AsyncCall(&AudioHelperChromeOs::StartAudioStream)
-      .WithArgs(base::SequencedTaskRunner::GetCurrentDefault(),
-                base::BindRepeating(&AudioCapturerChromeOs::HandleAudioData,
-                                    weak_ptr_factory_.GetWeakPtr()),
-                base::BindRepeating(&AudioCapturerChromeOs::HandleAudioError,
-                                    weak_ptr_factory_.GetWeakPtr()));
-  return true;
-}
-
-void AudioCapturerChromeOs::Stop() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  packet_captured_callback_.Reset();
-  audio_helper_chromeos_.AsyncCall(&AudioHelperChromeOs::StopAudioStream);
-}
-
-void AudioCapturerChromeOs::HandleAudioData(
-    std::unique_ptr<AudioPacket> packet) {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (packet_captured_callback_) {
-    packet_captured_callback_.Run(std::move(packet));
-  }
-}
-
-// TODO(crbug.com/450048643): Figure out error handling
-void AudioCapturerChromeOs::HandleAudioError() {
-  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  Stop();
-}
-
-// static
 bool AudioCapturer::IsSupported() {
-  return ash::features::IsBocaHostAudioEnabled();
+  return false;
 }
 
-// static
 std::unique_ptr<AudioCapturer> AudioCapturer::Create() {
-  if (!IsSupported()) {
-    return nullptr;
-  }
-
-  return std::make_unique<AudioCapturerChromeOs>(
-      std::make_unique<AudioHelperChromeOsImpl>());
+  NOTIMPLEMENTED();
+  return nullptr;
 }
 
 }  // namespace remoting
