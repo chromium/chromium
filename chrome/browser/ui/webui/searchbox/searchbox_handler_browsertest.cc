2f3afa0e4856e0308ec228af5990471d8e299d99
Aviv Kiss <avivkiss@google.com>
2025-10-15 06:21:39 -0700

    [ntp-composebox] Improve NTP AIM thumbnail preview rendering for high DPI devices.
    Factors in GetDeviceScaleFactor when computing the dimensions of the requested tab preview.
    
    Before: https://screenshot.googleplex.com/BDEg2EboFfgdapC
    After: https://screenshot.googleplex.com/8GJbzThRypcN8Jx
    
    Bug: 443939809
    Fixed: 452036543
    
    (cherry picked from commit cb9de086726d446cd489b413bb0944ffa0e047e6)
    
    Change-Id: I6a30f6dc8d408738a7011ca25b2acb7ce7c8effe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966861
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Auto-Submit: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1524952}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042444
    Auto-Submit: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7444@{#1045}
    Cr-Branched-From: 29907d3c18078029695f458b42fb8e6fda3e493d-refs/heads/main@{#1522585}

---
+++ chrome/browser/ui/webui/searchbox/searchbox_handler_browsertest.cc
@@ -0,0 +1,106 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/searchbox/searchbox_handler.h"
+
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/omnibox/omnibox_controller.h"
+#include "chrome/browser/ui/webui/searchbox/searchbox_test_utils.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "components/lens/tab_contextualization_controller.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_test.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "ui/display/display_switches.h"
+
+// TODO(crbug.com/447629531): Migrate the methods under tests and their tests to
+// ContextualSearchboxHandler.
+class TestSearchboxHandler : public SearchboxHandler {
+ public:
+  TestSearchboxHandler(
+      mojo::PendingReceiver<searchbox::mojom::PageHandler> pending_page_handler,
+      Profile* profile,
+      content::WebContents* web_contents,
+      MetricsReporter* metrics_reporter,
+      std::unique_ptr<OmniboxController> controller)
+      : SearchboxHandler(std::move(pending_page_handler),
+                         profile,
+                         web_contents,
+                         metrics_reporter,
+                         std::move(controller)) {}
+
+  ~TestSearchboxHandler() override = default;
+
+  void OnThumbnailRemoved() override {}
+};
+
+class SearchboxHandlerBrowserTest : public InProcessBrowserTest {
+ protected:
+  testing::NiceMock<MockSearchboxPage> page_;
+  std::unique_ptr<TestSearchboxHandler> handler_;
+
+  void SetUpOnMainThread() override {
+    InProcessBrowserTest::SetUpOnMainThread();
+    auto omnibox_controller = std::make_unique<OmniboxController>(
+        /*view=*/nullptr, std::make_unique<TestOmniboxClient>(),
+        kAutocompleteDefaultStopTimerDuration);
+    handler_ = std::make_unique<TestSearchboxHandler>(
+        mojo::PendingReceiver<searchbox::mojom::PageHandler>(),
+        browser()->profile(),
+        /*web_contents=*/browser()->tab_strip_model()->GetActiveWebContents(),
+        /*metrics_reporter=*/nullptr, std::move(omnibox_controller));
+    handler_->SetPage(page_.BindAndGetRemote());
+  }
+
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    InProcessBrowserTest::SetUpCommandLine(command_line);
+    command_line->RemoveSwitch(switches::kForceDeviceScaleFactor);
+    command_line->AppendSwitchASCII(switches::kForceDeviceScaleFactor, "1");
+  }
+
+  void TearDownOnMainThread() override { handler_.reset(); }
+};
+
+IN_PROC_BROWSER_TEST_F(SearchboxHandlerBrowserTest,
+                       CreateTabPreviewEncodingOptions_NotScaled) {
+  // When no device scale factor is applied, physical pixels should translate to
+  // CSS pixels at a 1:1 ratio.
+  int expected_width = 125 * 1;
+  int expected_height = 200 * 1;
+
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  auto options = handler_->CreateTabPreviewEncodingOptions(web_contents);
+
+  ASSERT_TRUE(options.has_value());
+  EXPECT_EQ(options->max_width, expected_width);
+  EXPECT_EQ(options->max_height, expected_height);
+}
+
+class SearchboxHandlerBrowserTestDSF2 : public SearchboxHandlerBrowserTest {
+ protected:
+  void SetUpCommandLine(base::CommandLine* command_line) override {
+    SearchboxHandlerBrowserTest::SetUpCommandLine(command_line);
+    command_line->RemoveSwitch(switches::kForceDeviceScaleFactor);
+    command_line->AppendSwitchASCII(switches::kForceDeviceScaleFactor, "2");
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(SearchboxHandlerBrowserTestDSF2,
+                       CreateTabPreviewEncodingOptions_Scaled) {
+  // 60 physical pixels translates to 30 CSS pixels when the device scale factor
+  // = 2 (2 physical pixels : 1 CSS pixel);
+  int expected_width = 125 * 2;
+  int expected_height = 200 * 2;
+
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  auto options = handler_->CreateTabPreviewEncodingOptions(web_contents);
+
+  ASSERT_TRUE(options.has_value());
+  EXPECT_EQ(options->max_width, expected_width);
+  EXPECT_EQ(options->max_height, expected_height);
+}
