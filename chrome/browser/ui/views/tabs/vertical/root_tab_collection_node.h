54531d7b2f066bd529c7c6e1b8266ab90e5d6251
Charles Meng <charlesmeng@chromium.org>
2025-10-15 12:36:54 -0700

    [Vertical Tabs] Pipe data for TabsCreatedEvent
    Make the root node observe browser tab strip API, and handle
    TabsCreatedEvent by creating a node for the tab and appending it to its
    parent node. Handling the insertion index properly will be handled in
    a follow-up CL when add_node_to_parent_ and add_child_to_node_ are
    refactored.
    
    Fixed: 450305404
    Change-Id: I9ddf23875cc914bedde0f21f2f6cc4b7418e478b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7023270
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Auto-Submit: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530397}

--- chrome/browser/ui/views/tabs/vertical/root_tab_collection_node.h
+++ chrome/browser/ui/views/tabs/vertical/root_tab_collection_node.h
@@ -5,7 +5,9 @@
 #ifndef CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_ROOT_TAB_COLLECTION_NODE_H_
 #define CHROME_BROWSER_UI_VIEWS_TABS_VERTICAL_ROOT_TAB_COLLECTION_NODE_H_
 
+#include "base/scoped_observation.h"
 #include "base/types/expected.h"
+#include "chrome/browser/ui/tabs/tab_strip_api/observation/tab_strip_api_observer.h"
 #include "chrome/browser/ui/views/tabs/vertical/tab_collection_node.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -17,16 +19,31 @@ class TabStripService;
 
 // The RootTabCollectionNode is the entry point for the TabStripAPI. It is
 // responsible for fetching the initial tab state and listening for updates.
-class RootTabCollectionNode : public TabCollectionNode {
+class RootTabCollectionNode
+    : public TabCollectionNode,
+      public tabs_api::observation::TabStripApiObserver {
  public:
   explicit RootTabCollectionNode(tabs_api::TabStripService* service_register,
                                  views::View* parent_view,
                                  CustomAddChildView add_node_to_parent_);
   ~RootTabCollectionNode() override;
 
-  // TODO(crbug.com/441256673): Add event iface so root node can react to
-  // updates.
+  // tabs_api::observation::TabStripApiObserver
+  void OnTabsCreated(const tabs_api::mojom::OnTabsCreatedEventPtr&
+                         tabs_created_event) override;
+  void OnTabsClosed(
+      const tabs_api::mojom::OnTabsClosedEventPtr& tabs_closed_event) override;
+  void OnNodeMoved(
+      const tabs_api::mojom::OnNodeMovedEventPtr& node_moved_event) override;
+  void OnDataChanged(const tabs_api::mojom::OnDataChangedEventPtr&
+                         data_changed_event) override;
+  void OnCollectionCreated(const tabs_api::mojom::OnCollectionCreatedEventPtr&
+                               collection_created_event) override;
+
  private:
+  base::ScopedObservation<tabs_api::TabStripService,
+                          tabs_api::observation::TabStripApiObserver>
+      service_observer_{this};
   base::WeakPtrFactory<RootTabCollectionNode> weak_ptr_factory_{this};
 };
 
