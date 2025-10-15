5309ca989b4cde1807584dfbbf2965cf71183733
Abe Boujane <boujane@google.com>
2025-10-15 05:57:12 -0700

    Enable kPrivacySandboxGetPromptFromNoticeService by default
    This change enables the kPrivacySandboxGetPromptFromNoticeService feature flag by default.
    
    Bug: 442804203
    Change-Id: I7e1b92f3779fe28756c8601601f2930a5015f01d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042345
    Auto-Submit: Abe Boujane <boujane@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530143}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -123,7 +123,7 @@ BASE_FEATURE(kPsDualWritePrefsToNoticeStorage,
 BASE_FEATURE(kPrivateStateTokensDevUI, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPrivacySandboxGetPromptFromNoticeService,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPrivacySandboxActivityTypeStorage,
              base::FEATURE_ENABLED_BY_DEFAULT);
