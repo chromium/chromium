9057282eafec5256bac7531a32ca6b70d1f15008
Ken Buchanan <kenrb@chromium.org>
2025-10-15 12:48:41 -0700

    [WebAuthn] Re-enable Android conditionalUiHybrid test
    This test was disabled because on older Android versions hybrid is
    not available. A null callback was being invoked as a result.
    
    This change enables the test, and skips it on old versions.
    
    Fixed: 347310677
    Change-Id: I1f6d0aedd92fb16cfb70dca3930002591e3033d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046738
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530404}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webauth/Fido2CredentialRequestTest.java
@@ -59,7 +59,6 @@ import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -100,6 +99,7 @@ import org.chromium.components.webauthn.Fido2ApiTestHelper;
 import org.chromium.components.webauthn.Fido2CredentialRequest;
 import org.chromium.components.webauthn.FidoIntentSender;
 import org.chromium.components.webauthn.GetAssertionOutcome;
+import org.chromium.components.webauthn.GmsCoreUtils;
 import org.chromium.components.webauthn.GpmBrowserOptionsHelper;
 import org.chromium.components.webauthn.InternalAuthenticator;
 import org.chromium.components.webauthn.InternalAuthenticatorJni;
@@ -2678,8 +2678,11 @@ public class Fido2CredentialRequestTest {
 
     @Test
     @SmallTest
-    @DisabledTest(message = "crbug.com/347310677")
     public void testGetAssertion_conditionalUiHybrid_success() {
+        if (!GmsCoreUtils.isHybridClientApiSupported()) {
+            return;
+        }
+
         mIntentSender.setNextResultIntent(Fido2ApiTestHelper.createSuccessfulGetAssertionIntent());
         mMockBrowserBridge.setExpectedCredentialDetailsList(
                 Arrays.asList(Fido2ApiTestHelper.getCredentialDetails()));
