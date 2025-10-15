8df7a27fe3d3dce264926678915a0c260e80b654
Vikram Pasupathy <vpasupathy@chromium.org>
2025-10-15 12:18:10 -0700

    Report Initialize HRESULT for all HW secure Key systems.
    This keeps the HardwareSecure.Initialize reporting in line with
    HardwareSecure.FirstInitialize reporting as per
    https://source.chromium.org/chromium/chromium/src/+/main:media/cdm/win/media_foundation_cdm_factory.cc;l=151-162
    
    When I hardcode a failure case and play PR content, I get the following
    histogram:
    
    Histogram:
    Media.EME.MediaFoundationCdm.PlayReady.HardwareSecure.Initialize
    recorded 1 samples (flags = 0x41) [#] 0 -O (1 = 100.0%)
    
    which matches the pattern of:
    
    Media.EME.MediaFoundationCdm.{KeySystem}.HardwareSecure.Initialize
    
    Bug: 378869813
    Change-Id: I75d84c5eedca48d913c10d8d3ca3e78c49c7a610
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6955640
    Reviewed-by: Sangbaek Park <sangbaekpark@chromium.org>
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1530388}

--- media/cdm/win/media_foundation_cdm_factory_unittest.cc
+++ media/cdm/win/media_foundation_cdm_factory_unittest.cc
@@ -9,7 +9,9 @@
 
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
+#include "base/strings/strcat.h"
 #include "base/test/gmock_callback_support.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/task_environment.h"
 #include "media/base/mock_filters.h"
@@ -33,8 +35,19 @@ using ::testing::StrictMock;
 
 namespace media {
 
+namespace {
 const CdmConfig kClearKeyHardwareSecureCdmConfig = {kClearKeyKeySystem, true,
                                                     true, true};
+
+// 'HardwareSecure' suffix is not included in the Uma name because for
+// the ClearKey key system, we do not differentiate between software and
+// hardware security.
+static constexpr char kFirstInitializeHistogram[] =
+    "Media.EME.MediaFoundationCdm.ClearKey.FirstInitialize";
+static constexpr char kInitializeHistogram[] =
+    "Media.EME.MediaFoundationCdm.ClearKey.Initialize";
+
+}  // namespace
 
 using Microsoft::WRL::ComPtr;
 
@@ -101,6 +114,7 @@ class MediaFoundationCdmFactoryTest : public testing::Test {
 };
 
 TEST_F(MediaFoundationCdmFactoryTest, Create) {
+  base::HistogramTester histogram_tester;
   SetCreateCdmFactoryCallbackForTesting(/*expect_success=*/true);
 
   COM_EXPECT_CALL(mf_cdm_factory_, IsTypeSupported(NotNull(), IsNull()))
@@ -116,6 +130,10 @@ TEST_F(MediaFoundationCdmFactoryTest, Create) {
 
   EXPECT_CALL(cdm_created_cb_, Run(NotNull(), _));
   Create();
+
+  // Verify Histograms for success
+  histogram_tester.ExpectUniqueSample(kFirstInitializeHistogram, S_OK, 1);
+  histogram_tester.ExpectTotalCount(kInitializeHistogram, 0);
 }
 
 TEST_F(MediaFoundationCdmFactoryTest, CreateCdmFactoryFail) {
@@ -170,6 +188,7 @@ TEST_F(MediaFoundationCdmFactoryTest, NullCdmOriginIdFail) {
 }
 
 TEST_F(MediaFoundationCdmFactoryTest, CreateCdmFail) {
+  base::HistogramTester histogram_tester;
   SetCreateCdmFactoryCallbackForTesting(/*expect_success=*/true);
 
   COM_EXPECT_CALL(mf_cdm_factory_, IsTypeSupported(NotNull(), IsNull()))
@@ -185,6 +204,10 @@ TEST_F(MediaFoundationCdmFactoryTest, CreateCdmFail) {
 
   EXPECT_CALL(cdm_created_cb_, Run(IsNull(), _));
   Create();
+
+  // Verify Histograms for failure
+  histogram_tester.ExpectUniqueSample(kFirstInitializeHistogram, E_FAIL, 1);
+  histogram_tester.ExpectUniqueSample(kInitializeHistogram, E_FAIL, 1);
 }
 
 }  // namespace media
