e776807014c00ef659e704414680bae4360b2c0c
Eric Ekey <ericekey@google.com>
2025-10-16 07:39:33 -0700

    [iOS] Stub Coordinator for Synced Set Up
    This change stubs the coordinator for the new Synced Set Up experience
    (DD: go/synced-set-up-dd).
    
    Fixed: 452400687
    Change-Id: Ie9bb59dc36a216cc47e30c73e5f9b6bc1a5993c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7042258
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Auto-Submit: Eric Ekey <ericekey@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530838}

---
+++ ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.h
@@ -0,0 +1,15 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SYNCED_SET_UP_COORDINATOR_SYNCED_SET_UP_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_SYNCED_SET_UP_COORDINATOR_SYNCED_SET_UP_COORDINATOR_H_
+
+#import "ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.h"
+
+// Coordinator that orchestrates the Synced Set Up experience.
+@interface SyncedSetUpCoordinator : ChromeCoordinator
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SYNCED_SET_UP_COORDINATOR_SYNCED_SET_UP_COORDINATOR_H_
---
+++ ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.mm
@@ -0,0 +1,9 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/synced_set_up/coordinator/synced_set_up_coordinator.h"
+
+@implementation SyncedSetUpCoordinator
+
+@end
