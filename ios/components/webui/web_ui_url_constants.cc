1683e4329a3d271cd98620fe85d1bf38b67c75dc
Sylvain Defresne <sdefresne@chromium.org>
2025-10-17 02:30:17 -0700

    [ios] Modernize definition of //ios/components/webui constants
    Declare the constants as `inline constexpr char kConstant[] = "...";`
    in the header (this allow initializing other constexpr constants from
    them).
    
    Bug: none
    Change-Id: Iae79669bf5d16042c9cdc66247cd88ee878b7521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046203
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1531309}

--- ios/components/webui/web_ui_url_constants.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2020 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "ios/components/webui/web_ui_url_constants.h"
-
-const char kChromeUIScheme[] = "chrome";
-
-const char kChromeUISyncInternalsHost[] = "sync-internals";
