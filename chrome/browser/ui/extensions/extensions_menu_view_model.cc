2ec8258af7a3510672c5b223bd8ecacc3938190e
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2025-10-17 17:48:02 -0700

    Revert "Reland: [Extensions] Add UpdateSiteAccess to extensions menu model"
    This reverts commit 0fb50ca2ac3e8806f8fd4507a8be2e366b81fe9f.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8700718456181822257
    
    Sample failed build: https://ci.chromium.org/b/8700718456181822257
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7055624&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8700718456181822257&type=BUG
    
    Original change's description:
    > Reland: [Extensions] Add UpdateSiteAccess to extensions menu model
    >
    > This is a reland of crrev.com/c/7024121. Original CL was reverted due
    > to usage of Browser in a browser test compiled on Android. Instead, it
    > should use BrowserWindowInterface
    >
    > Original change's description:
    > > [Extensions] Add UpdateSiteAccess to extensions menu model
    > >
    > > Move the extensions logic handling of OnSiteAccessSelected on the
    > > extensions menu from the platform delegate views to the platform-agnostic menu model.
    > >
    > > Bug: 449814184
    > > Change-Id: Ib6136a7cc2d1e888b3df48de61c1c09b4944781c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7024121
    > > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    > > Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1531010}
    >
    > Bug: 449814184
    > Change-Id: I374ff5d1939578b4035f8ca046f76b5cbaeedd97
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7055624
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1531702}
    >
    
    Bug: 449814184
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I4079ebc756b1bb38910a955bd52dd26b8379fe47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7056674
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1531715}

--- chrome/browser/ui/extensions/extensions_menu_view_model.cc
+++ chrome/browser/ui/extensions/extensions_menu_view_model.cc
@@ -4,71 +4,12 @@
 
 #include "chrome/browser/ui/extensions/extensions_menu_view_model.h"
 
-#include "base/metrics/user_metrics.h"
-#include "base/metrics/user_metrics_action.h"
-#include "chrome/browser/extensions/permissions/site_permissions_helper.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
-#include "chrome/browser/ui/tabs/tab_list_interface.h"
-#include "extensions/browser/extension_registry.h"
-
-namespace {
-
-using PermissionsManager = extensions::PermissionsManager;
-using SitePermissionsHelper = extensions::SitePermissionsHelper;
-
-// Returns the extension corresponding to `extension_id` on `profile`.
-const extensions::Extension* GetExtension(
-    Profile& profile,
-    const extensions::ExtensionId& extension_id) {
-  return extensions::ExtensionRegistry::Get(&profile)
-      ->enabled_extensions()
-      .GetByID(extension_id);
-}
-
-void LogSiteAccessUpdate(PermissionsManager::UserSiteAccess site_access) {
-  switch (site_access) {
-    case PermissionsManager::UserSiteAccess::kOnClick:
-      base::RecordAction(
-          base::UserMetricsAction("Extensions.Menu.OnClickSelected"));
-      break;
-    case PermissionsManager::UserSiteAccess::kOnSite:
-      base::RecordAction(
-          base::UserMetricsAction("Extensions.Menu.OnSiteSelected"));
-      break;
-    case PermissionsManager::UserSiteAccess::kOnAllSites:
-      base::RecordAction(
-          base::UserMetricsAction("Extensions.Menu.OnAllSitesSelected"));
-      break;
-    default:
-      NOTREACHED() << "Unknown site access";
-  }
-}
-
-}  // namespace
-
 ExtensionsMenuViewModel::ExtensionsMenuViewModel(
-    BrowserWindowInterface* browser,
     std::unique_ptr<ExtensionsMenuViewPlatformDelegate> platform_delegate)
-    : browser_(browser), platform_delegate_(std::move(platform_delegate)) {
+    : platform_delegate_(std::move(platform_delegate)) {
   platform_delegate_->AttachToModel(this);
 }
 
 ExtensionsMenuViewModel::~ExtensionsMenuViewModel() {
   platform_delegate_->DetachFromModel();
-}
-
-void ExtensionsMenuViewModel::UpdateSiteAccess(
-    const extensions::ExtensionId& extension_id,
-    PermissionsManager::UserSiteAccess site_access) {
-  LogSiteAccessUpdate(site_access);
-
-  Profile* profile = browser_->GetProfile();
-  SitePermissionsHelper permissions(profile);
-  permissions.UpdateSiteAccess(*GetExtension(*profile, extension_id),
-                               GetActiveWebContents(), site_access);
-}
-
-content::WebContents* ExtensionsMenuViewModel::GetActiveWebContents() {
-  return TabListInterface::From(browser_)->GetActiveTab()->GetContents();
 }
