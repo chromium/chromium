b43cc4d3c78cc97172fa9e22b762304bca27f795
Marijn Kruisselbrink <mek@chromium.org>
2025-10-20 14:57:54 -0700

    Turn on Navigation Capturing with Scope Extensions
    Bug: 338640785
    Change-Id: If1dd6192490e45b736e971b1999b68cc6412e2d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7040575
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1532566}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -236,7 +236,7 @@ BASE_FEATURE(kDesktopPWAsPreventClose,
 
 BASE_FEATURE(kPwaNavigationCapturingWithScopeExtensions,
              "DesktopPWAsLinkCapturingWithScopeExtensions",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Adds a user settings that allows PWAs to be opened with a tab strip.
 BASE_FEATURE(kDesktopPWAsTabStripSettings, base::FEATURE_DISABLED_BY_DEFAULT);
