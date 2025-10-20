fdcc70f7f1803b92797c72ce455a4ac5822da90f
Sourav <sinhasourav@google.com>
2025-10-20 14:23:01 -0700

    [iOSFidoImportExport] Subtitle header for Credential Export UI
    This CL adds the subtitle header to the credential export selection
    screen.
    
    Mock: https://screenshot.googleplex.com/9L6QYnuQcVjusyT
    Impl: https://screenshot.googleplex.com/7uChobieMsFgwVe
    
    Bug: 449701042
    Change-Id: Ie86c69c01ae684620d69dbebe35ff0e1e03692c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7043445
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532536}

--- ios/chrome/browser/webauthn/ui/credential_export_view_controller.mm
+++ ios/chrome/browser/webauthn/ui/credential_export_view_controller.mm
@@ -4,12 +4,23 @@
 
 #import "ios/chrome/browser/webauthn/ui/credential_export_view_controller.h"
 
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_header_footer_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 #import "ios/chrome/browser/webauthn/ui/credential_export_view_controller_presentation_delegate.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
 
-@implementation CredentialExportViewController
+namespace {
+
+// Identifier for the table view section that lists the credentials.
+NSString* const kCredentialSectionIdentifier = @"CredentialSection";
+
+}  // namespace
+
+@implementation CredentialExportViewController {
+  // The data source that will manage the table view.
+  UITableViewDiffableDataSource<NSString*, NSNumber*>* _dataSource;
+}
 
 - (instancetype)init {
   return [super initWithStyle:ChromeTableViewStyle()];
@@ -25,6 +36,8 @@
       initWithBarButtonSystemItem:UIBarButtonSystemItemDone
                            target:self
                            action:@selector(didTapDone)];
+
+  [self configureDataSource];
 }
 
 #pragma mark - Actions
@@ -32,6 +45,44 @@
 - (void)didTapDone {
   // TODO(crbug.com/449701042): Handle credentials selection
   [self.delegate userDidStartExport];
+}
+
+#pragma mark - UITableViewDelegate
+
+- (UIView*)tableView:(UITableView*)tableView
+    viewForHeaderInSection:(NSInteger)section {
+  TableViewTextHeaderFooterView* header =
+      DequeueTableViewHeaderFooter<TableViewTextHeaderFooterView>(
+          self.tableView);
+
+  [header setSubtitle:l10n_util::GetNSString(
+                          IDS_IOS_EXPORT_PASSWORDS_AND_PASSKEYS_SUBTITLE)];
+
+  return header;
+}
+
+#pragma mark - Private
+
+// Sets up the diffable data source and applies the initial snapshot.
+- (void)configureDataSource {
+  RegisterTableViewHeaderFooter<TableViewTextHeaderFooterView>(self.tableView);
+
+  UITableViewDiffableDataSourceCellProvider cellProvider =
+      ^UITableViewCell*(UITableView* tableView, NSIndexPath* indexPath,
+                        NSNumber* itemIdentifier) {
+        // TODO(crbug.com/449701042): Implement cell creation.
+        return nil;
+      };
+
+  _dataSource =
+      [[UITableViewDiffableDataSource alloc] initWithTableView:self.tableView
+                                                  cellProvider:cellProvider];
+
+  NSDiffableDataSourceSnapshot<NSString*, NSNumber*>* snapshot =
+      [[NSDiffableDataSourceSnapshot alloc] init];
+  [snapshot appendSectionsWithIdentifiers:@[ kCredentialSectionIdentifier ]];
+
+  [_dataSource applySnapshot:snapshot animatingDifferences:NO];
 }
 
 @end
