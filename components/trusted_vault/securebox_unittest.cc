c05f6191249c20960e677f17526322a88d647d55
Mikel Astiz <mastiz@chromium.org>
2025-10-19 20:57:40 -0700

    [M138-LTS][TrustedVault] Fix public import missing size validation
    `SecureBoxPublicKey::CreateByImport()` should have safeguards against
    input data not matching the expected size, and return null in that case.
    
    (cherry picked from commit 0e63d7ab62928f5645a42484fbf47cba43b0128f)
    
    Bug: 443196747
    Change-Id: I0996dbe383a51b1fd8269fbe429c78f24f283989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6990129
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1521442}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6998609
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Owners-Override: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Gyuyoung Kim (xWF) <qkim@google.com>
    Cr-Commit-Position: refs/branch-heads/7204@{#3434}
    Cr-Branched-From: d5de512dc9dc8ddfe4e6d71b0637578bb6158683-refs/heads/main@{#1465706}

--- components/trusted_vault/securebox_unittest.cc
+++ components/trusted_vault/securebox_unittest.cc
@@ -28,6 +28,7 @@ namespace {
 
 using testing::Eq;
 using testing::IsEmpty;
+using testing::IsNull;
 using testing::Ne;
 using testing::NotNull;
 using testing::SizeIs;
@@ -46,6 +47,16 @@ class SecureBoxTest : public testing::Test {
   const std::vector<uint8_t> kTestHeader = StringToBytes("TEST_HEADER");
   const std::vector<uint8_t> kTestPayload = StringToBytes("TEST_PAYLOAD");
 };
+
+TEST_F(SecureBoxTest, ShouldReturnNullIfCreateByImportWithIncorrectSize) {
+  // Sizes other than `kPublicKeyLengthInBytes` should return null.
+  EXPECT_THAT(SecureBoxPublicKey::CreateByImport(std::vector<uint8_t>(64, 1)),
+              IsNull());
+  EXPECT_THAT(SecureBoxPublicKey::CreateByImport(std::vector<uint8_t>(66, 1)),
+              IsNull());
+  EXPECT_THAT(SecureBoxPublicKey::CreateByImport(std::vector<uint8_t>()),
+              IsNull());
+}
 
 TEST_F(SecureBoxTest, ShouldExportAndImportPublicKey) {
   std::unique_ptr<SecureBoxKeyPair> key_pair =
