85f8d60ca50b04f483047359bf34af94ccc9ddac
Charles Hager <clhager@google.com>
2025-10-21 10:31:14 -0700

    Clean up E2E for DownloadActivityV2Test
    The E2E bottom chin has been default enabled and fully launched, so this
    is no longer needed.
    
    Bug: 390022792
    Change-Id: I8474be0e2ea31bc4ec4197ccbb61c4d776239331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7064540
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533063}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/DownloadActivityV2Test.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/home/DownloadActivityV2Test.java
@@ -71,14 +71,12 @@ import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisabledTest;
-import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.back_press.BackPressHelper;
 import org.chromium.chrome.browser.download.home.list.ListUtils;
 import org.chromium.chrome.browser.download.home.list.holder.ListItemViewHolder;
 import org.chromium.chrome.browser.download.home.rename.RenameUtils;
 import org.chromium.chrome.browser.download.home.toolbar.DownloadHomeToolbar;
 import org.chromium.chrome.browser.download.internal.R;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeController;
 import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeControllerFactory;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
@@ -938,7 +936,6 @@ public class DownloadActivityV2Test {
 
     @Test
     @MediumTest
-    @EnableFeatures(ChromeFeatureList.EDGE_TO_EDGE_BOTTOM_CHIN)
     public void testEdgeToEdge() {
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
