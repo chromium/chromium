dde276748661a81aed335cb9cb46804911f5f23f
Mike West <mkwst@chromium.org>
2025-10-21 11:11:52 -0700

    [ReduceDeviceMemory] Drop this experiment.
    As per https://privacysandbox.google.com/overview/status, we're not
    going to ship this. Let's drop it from the codebase.
    
    Bug: 435582603
    Change-Id: I68ce49650ad176eebe39f1a92d60c8d3727614a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062351
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533087}

--- third_party/blink/renderer/core/frame/navigator_device_memory.cc
+++ third_party/blink/renderer/core/frame/navigator_device_memory.cc
@@ -10,23 +10,10 @@
 #include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-shared.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
-namespace {
-
-// TODO(435582603): Hard-coding this to a common value is a reasonable start,
-// but it likely makes sense to vary the hard-coded number by platform and
-// form-factor in order to maintain plausibility over time.
-constexpr float kReducedDeviceMemoryValue = 8.0;
-
-}  // namespace
-
 float NavigatorDeviceMemory::deviceMemory() const {
-  if (RuntimeEnabledFeatures::ReduceDeviceMemoryEnabled()) {
-    return kReducedDeviceMemoryValue;
-  }
   return ApproximatedDeviceMemory::GetApproximatedDeviceMemory();
 }
 
