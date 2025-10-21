9ba53de6112b6646d945de0600460b6ee3ccd015
Gauthier Ambard <gambard@chromium.org>
2025-10-21 08:38:59 -0700

    [iOS] Add a content configuration for an info button
    This CL adds a new content configuration, used when adding an Info
    button to a cell.
    
    Bug: 443034511
    Change-Id: I10189470f018b6b6e70dcd377fcbb1f9fd525ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062941
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1532984}

---
+++ ios/chrome/browser/shared/ui/table_view/content_configuration/info_button_content_configuration.h
@@ -0,0 +1,33 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_CONTENT_CONFIGURATION_INFO_BUTTON_CONTENT_CONFIGURATION_H_
+#define IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_CONTENT_CONFIGURATION_INFO_BUTTON_CONTENT_CONFIGURATION_H_
+
+#import <UIKit/UIKit.h>
+
+#import "ios/chrome/browser/shared/ui/table_view/content_configuration/chrome_content_configuration.h"
+
+// Content configuration for a view with a single info button.
+@interface InfoButtonContentConfiguration
+    : NSObject <ChromeContentConfiguration>
+
+// The updates to properties must be reflected in the copy method.
+// LINT.IfChange(Copy)
+
+// The view's button is configured to call `selector` on `target`.
+@property(nonatomic, weak) id target;
+@property(nonatomic, assign) SEL selector;
+
+// Whether the button is enabled. Default is YES.
+@property(nonatomic, assign) BOOL enabled;
+
+// The tag for the button.
+@property(nonatomic, assign) NSInteger tag;
+
+// LINT.ThenChange(info_button_content_configuration.mm:Copy)
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SHARED_UI_TABLE_VIEW_CONTENT_CONFIGURATION_INFO_BUTTON_CONTENT_CONFIGURATION_H_
---
+++ ios/chrome/browser/shared/ui/table_view/content_configuration/info_button_content_view.mm
@@ -0,0 +1,105 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/shared/ui/table_view/content_configuration/info_button_content_view.h"
+
+#import "base/apple/foundation_util.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/chrome/browser/shared/ui/table_view/content_configuration/info_button_content_configuration.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
+#import "ios/chrome/common/ui/util/constraints_ui_util.h"
+
+namespace {
+const CGFloat kInfoSymbolSize = 22;
+}
+
+@implementation InfoButtonContentView {
+  // The configuration of the view.
+  InfoButtonContentConfiguration* _configuration;
+  // The info button.
+  UIButton* _infoButton;
+}
+
+- (instancetype)initWithConfiguration:
+    (InfoButtonContentConfiguration*)configuration {
+  self = [super initWithFrame:CGRectZero];
+  if (self) {
+    UIButtonConfiguration* buttonConfig =
+        [UIButtonConfiguration plainButtonConfiguration];
+    buttonConfig.image =
+        DefaultSymbolTemplateWithPointSize(kInfoCircleSymbol, kInfoSymbolSize);
+    buttonConfig.contentInsets = NSDirectionalEdgeInsetsZero;
+    _infoButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    _infoButton.configuration = buttonConfig;
+    _infoButton.tintColor = [UIColor colorNamed:kBlueColor];
+    _infoButton.accessibilityIdentifier = kTableViewCellInfoButtonViewId;
+    _infoButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [_infoButton setContentHuggingPriority:UILayoutPriorityRequired - 1
+                                   forAxis:UILayoutConstraintAxisHorizontal];
+    [_infoButton
+        setContentCompressionResistancePriority:UILayoutPriorityRequired
+                                        forAxis:
+                                            UILayoutConstraintAxisHorizontal];
+    [self addSubview:_infoButton];
+
+    _configuration = configuration;
+    [self applyConfiguration];
+
+    AddSameConstraints(_infoButton, self);
+  }
+  return self;
+}
+
+- (UIButton*)infoButtonForTesting {
+  return _infoButton;
+}
+
+#pragma mark - ChromeContentView
+
+- (BOOL)hasCustomAccessibilityActivationPoint {
+  return YES;
+}
+
+#pragma mark - UIContentView
+
+- (id<UIContentConfiguration>)configuration {
+  return _configuration;
+}
+
+- (void)setConfiguration:(id<UIContentConfiguration>)configuration {
+  _configuration = [base::apple::ObjCCastStrict<InfoButtonContentConfiguration>(
+      configuration) copy];
+  [self applyConfiguration];
+}
+
+- (BOOL)supportsConfiguration:(id<UIContentConfiguration>)configuration {
+  return [configuration isMemberOfClass:InfoButtonContentConfiguration.class];
+}
+
+#pragma mark - UIAccessibility
+
+- (CGPoint)accessibilityActivationPoint {
+  CGRect frameInScreenCoordinates =
+      UIAccessibilityConvertFrameToScreenCoordinates(_infoButton.bounds,
+                                                     _infoButton);
+  return CGPointMake(CGRectGetMidX(frameInScreenCoordinates),
+                     CGRectGetMidY(frameInScreenCoordinates));
+}
+
+#pragma mark - Private
+
+// Updates the content view with the current configuration.
+- (void)applyConfiguration {
+  [_infoButton removeTarget:nil
+                     action:NULL
+           forControlEvents:UIControlEventAllEvents];
+  [_infoButton addTarget:_configuration.target
+                  action:_configuration.selector
+        forControlEvents:UIControlEventTouchUpInside];
+  _infoButton.tag = _configuration.tag;
+  _infoButton.enabled = _configuration.enabled;
+}
+
+@end
