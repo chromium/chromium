90ed359b4ff92bd440cc4a9c20dc2e76e70467b9
Charles Hager <clhager@google.com>
2025-10-21 11:46:26 -0700

    Add fieldtrials for ToolbarTabletResizeRefactor
    Bug: 427260734
    Change-Id: Id66e0e9a7deaae20343024a4eb0d22bbc56f89bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7049944
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533110}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -1081,7 +1081,10 @@ public abstract class ChromeFeatureList {
                     /* defaultValue= */ false,
                     /* defaultValueInTests= */ true);
     public static final CachedFlag sToolbarTabletResizeRefactor =
-            newCachedFlag(TOOLBAR_TABLET_RESIZE_REFACTOR, /* defaultValue= */ false);
+            newCachedFlag(
+                    TOOLBAR_TABLET_RESIZE_REFACTOR,
+                    /* defaultValue= */ false,
+                    /* defaultValueInTests= */ true);
     public static final CachedFlag sTopControlsRefactor =
             newCachedFlag(
                     TOP_CONTROLS_REFACTOR,
