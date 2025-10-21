e349858ebb9c3c0ce792ea192c9065b4c7060996
Christine Hollingsworth <christinesm@chromium.org>
2025-10-21 12:42:50 -0700

    [Mac] Enable views first run dialog by default
    Enables the views implementation of the first run dialog
    by default, on Mac.
    
    Once determined that there are no issues switching to this
    updated UI surface, remove deprecated / corresponding
    Cocoa UI code to fully replace with the views UI
    implementation.
    
    Bug: 7017709
    Change-Id: I3c066921fa88e851231224ea0a979ef77dfa42a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7066897
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533159}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -448,7 +448,7 @@ BASE_FEATURE(kWebUITabStripContextMenuAfterTap,
 );
 
 #if BUILDFLAG(IS_MAC)
-BASE_FEATURE(kViewsFirstRunDialog, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kViewsFirstRunDialog, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kViewsJSAppModalDialog, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
