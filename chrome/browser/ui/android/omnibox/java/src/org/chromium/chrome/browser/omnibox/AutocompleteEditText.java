7c10a7b03a7fbd9ff785247dc5fec27f110bc9d8
Tomasz Wiszkowski <ender@google.com>
2025-10-22 16:08:23 -0700

    Unblock vertical scroll of the Url bar
    Unfortunately, UrlBar was inheriting from `VerticallyFixedEditText`
    for a much longer time than we have the history.
    
    Tracked this down to https://codereview.chromium.org/1141283003 - it
    was there right from the very beginning. There's no clear reason why
    we would still need this, so tentatively changing the parent class
    to unblock the vertical scroll.
    
    Change-Id: I671dadf1bdd544c30d5cd7342e73813d4289ab8e
    Fixed: 452329826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076715
    Reviewed-by: Jian Li <jianli@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533944}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/AutocompleteEditText.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/AutocompleteEditText.java
@@ -20,19 +20,19 @@ import android.widget.EditText;
 
 import androidx.annotation.CallSuper;
 import androidx.annotation.VisibleForTesting;
+import androidx.appcompat.widget.AppCompatEditText;
 
 import org.chromium.base.Log;
 import org.chromium.build.annotations.EnsuresNonNull;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
-import org.chromium.components.browser_ui.widget.text.VerticallyFixedEditText;
 import org.chromium.components.omnibox.OmniboxFeatures;
 import org.chromium.ui.accessibility.AccessibilityState;
 import org.chromium.ui.text.EmptyTextWatcher;
 
 /** An {@link EditText} that shows autocomplete text at the end. */
 @NullMarked
-public class AutocompleteEditText extends VerticallyFixedEditText
+public class AutocompleteEditText extends AppCompatEditText
         implements AutocompleteEditTextModelBase.Delegate {
     private static final String TAG = "AutocompleteEdit";
     private static final boolean DEBUG = OmniboxFeatures.sDiagInputConnection.getValue();
