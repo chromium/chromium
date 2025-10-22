dbb14cd1c29437616e5d0c803f8135439a91ebe3
Haihan Chen <haihan@google.com>
2025-10-22 07:58:06 -0700

    [Cache Encryption] Add cache encryption feature flag and common encryption directory
    Create common enterprise encryption directory for all future encryption work (history, cache, etc.) and cache-specific feature flag.
    
    Will add specific buildflags.gni once cache-specific code and dir is added.
    
    Bug: 454275908
    Change-Id: Icf55501c0433ae082fdc4d9571e73efd2e51ef4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7072952
    Commit-Queue: Haihan Chen <haihan@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1533620}

---
+++ components/enterprise/encryption/core/features.cc
@@ -0,0 +1,11 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/encryption/core/features.h"
+
+namespace enterprise_encryption {
+
+BASE_FEATURE(kEnableCacheEncryption, base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace enterprise_encryption
