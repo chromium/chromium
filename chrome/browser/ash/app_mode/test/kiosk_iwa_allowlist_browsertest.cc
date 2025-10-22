7dd23e57db60fec4d7bdf4d01e321d78b7aaf0e3
Robert Ferens <rferens@google.com>
2025-10-22 03:52:17 -0700

    iwa: Add IWA allowlist browser tests for Kiosk
    Not allowlisted apps should not be installed. We want to test this
    behavior for Kiosk IWAs.
    
    Fixed: 441947314, 430536355
    Change-Id: I4be771bd98f238e037a99feac25e322141bc546c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7062732
    Commit-Queue: Robert Ferens <rferens@google.com>
    Reviewed-by: Dominik Bylica <bylica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1533523}

---
+++ chrome/browser/ash/app_mode/test/kiosk_iwa_allowlist_browsertest.cc
@@ -0,0 +1,122 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/task/current_thread.h"
+#include "base/test/gmock_expected_support.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/threading/thread_restrictions.h"
+#include "chrome/browser/ash/app_mode/kiosk_app_launcher.h"
+#include "chrome/browser/ash/app_mode/test/kiosk_mixin.h"
+#include "chrome/browser/ash/app_mode/test/kiosk_test_utils.h"
+#include "chrome/browser/ash/login/app_mode/kiosk_launch_controller.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/ui/ash/login/login_display_host.h"
+#include "chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_builder.h"
+#include "chrome/browser/web_applications/isolated_web_apps/test/isolated_web_app_test_update_server.h"
+#include "chrome/browser/web_applications/isolated_web_apps/test/key_distribution/test_utils.h"
+#include "chrome/test/base/mixin_based_in_process_browser_test.h"
+#include "components/webapps/isolated_web_apps/features.h"
+#include "components/webapps/isolated_web_apps/test_support/signing_keys.h"
+#include "content/public/test/browser_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ash {
+
+namespace {
+
+const web_package::SignedWebBundleId kTestWebBundleId =
+    web_app::test::GetDefaultEd25519WebBundleId();
+
+KioskMixin::Config GetKioskIwaManualLaunchConfig(
+    const GURL& update_manifest_url) {
+  KioskMixin::IsolatedWebAppOption iwa_option(
+      /*account_id=*/"simple-iwa@localhost",
+      /*web_bundle_id=*/kTestWebBundleId,
+      /*update_manifest_url=*/update_manifest_url);
+  iwa_option.skip_iwa_allowlist_checks = false;
+
+  KioskMixin::Config kiosk_iwa_config = {
+      /*name=*/"IsolatedWebApp",
+      /*auto_launch_account_id=*/std::nullopt,
+      {iwa_option}};
+  return kiosk_iwa_config;
+}
+
+}  // anonymous namespace
+
+using ash::KioskAppLaunchError;
+using kiosk::test::LaunchAppManually;
+using kiosk::test::TheKioskApp;
+using kiosk::test::WaitKioskLaunched;
+
+class KioskIwaAllowlistTest : public MixinBasedInProcessBrowserTest {
+ public:
+  KioskIwaAllowlistTest() {
+    iwa_test_server_.AddBundle(
+        web_app::IsolatedWebAppBuilder(
+            web_app::ManifestBuilder().SetVersion("1.0.0"))
+            .BuildBundle(web_app::test::GetDefaultEd25519KeyPair()));
+  }
+
+  ~KioskIwaAllowlistTest() override = default;
+  KioskIwaAllowlistTest(const KioskIwaAllowlistTest&) = delete;
+  KioskIwaAllowlistTest& operator=(const KioskIwaAllowlistTest&) = delete;
+
+  void WaitForIwaNotAllowedLaunchError() {
+    ASSERT_TRUE(base::test::RunUntil([&]() {
+      return KioskAppLaunchError::Get(
+                 CHECK_DEREF(g_browser_process->local_state())) ==
+             KioskAppLaunchError::Error::kIsolatedAppNotAllowed;
+    }));
+  }
+
+  bool PressBailoutAccelerator() {
+    return ash::LoginDisplayHost::default_host()->HandleAccelerator(
+        ash::LoginAcceleratorAction::kAppLaunchBailout);
+  }
+
+  void SetIwaAllowlist(
+      const std::vector<web_package::SignedWebBundleId>& managed_allowlist) {
+    base::ScopedAllowBlockingForTesting allow_blocking;
+    EXPECT_THAT(web_app::test::UpdateKeyDistributionInfoWithAllowlist(
+                    base::Version("1.0"), std::move(managed_allowlist)),
+                base::test::HasValue());
+  }
+
+ protected:
+  web_app::IsolatedWebAppTestUpdateServer iwa_test_server_;
+  KioskMixin kiosk_{
+      &mixin_host_,
+      GetKioskIwaManualLaunchConfig(
+          iwa_test_server_.GetUpdateManifestUrl(kTestWebBundleId))};
+  base::test::ScopedFeatureList scoped_feature_list_{
+      features::kIsolatedWebAppManagedAllowlist};
+};
+
+IN_PROC_BROWSER_TEST_F(KioskIwaAllowlistTest,
+                       AllowlistedAppInstalledAndLaunched) {
+  SetIwaAllowlist({web_app::test::GetDefaultEd25519WebBundleId()});
+
+  ASSERT_TRUE(LaunchAppManually(TheKioskApp()));
+  ASSERT_TRUE(WaitKioskLaunched());
+}
+
+IN_PROC_BROWSER_TEST_F(KioskIwaAllowlistTest,
+                       NotAllowlistedAppInstallationError) {
+  // Clear the allowlist
+  SetIwaAllowlist(/*managed_allowlist=*/{});
+
+  ASSERT_TRUE(LaunchAppManually(TheKioskApp()));
+  WaitForIwaNotAllowedLaunchError();
+
+  EXPECT_TRUE(ash::KioskController::Get().IsSessionStarting());
+  PressBailoutAccelerator();
+  RunUntilBrowserProcessQuits();
+  EXPECT_EQ(
+      KioskAppLaunchError::Get(CHECK_DEREF(g_browser_process->local_state())),
+      KioskAppLaunchError::Error::kUserCancel);
+  EXPECT_FALSE(ash::KioskController::Get().IsSessionStarting());
+}
+
+}  // namespace ash
