4e25b60dafbd9118248fb8af42b630e4a8c7258f
Amelie Schneider <amelies@google.com>
2025-10-23 02:27:59 -0700

    [UNO] Merge extensions sign in promo code path
    Until now, there was a separate code path for uploading an extension
    after sign in through the extension sign in promo. With this CL, this
    code path is no longer used and the same path that is already being used
    for bookmarks and autofill promos is taken instead.
    
    These changes are behind the flag `UnoPhase2FollowUp`. After this flag
    is enabled, the old code path should be removed.
    
    Bug: 453576273
    Change-Id: Ibfc1468bbebe9b0358e3cfeee5dd6ea0bb576b41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7068232
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534183}

--- chrome/browser/extensions/sync/account_extension_tracker.cc
+++ chrome/browser/extensions/sync/account_extension_tracker.cc
@@ -19,6 +19,7 @@
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_change_event.h"
+#include "components/sync/base/features.h"
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/service/sync_service.h"
 #include "components/sync/service/sync_user_settings.h"
@@ -290,8 +291,11 @@ AccountExtensionTracker::GetAccountExtensionType(
   return static_cast<AccountExtensionType>(type_int);
 }
 
+#if !BUILDFLAG(IS_CHROMEOS)
 void AccountExtensionTracker::OnSignInInitiatedFromExtensionPromo(
     const ExtensionId& extension_id) {
+  CHECK(!base::FeatureList::IsEnabled(syncer::kUnoPhase2FollowUp));
+
   bool already_has_primary_account =
       IdentityManagerFactory::GetForProfile(profile_)->HasPrimaryAccount(
           signin::ConsentLevel::kSignin);
@@ -316,6 +320,7 @@ void AccountExtensionTracker::OnSignInInitiatedFromExtensionPromo(
                      weak_factory_.GetWeakPtr(), extension_id),
       kMaxSigninFromExtensionBubbleDelay);
 }
+#endif  // !BUILDFLAG(IS_CHROMEOS)
 
 bool AccountExtensionTracker::CanUploadAsAccountExtension(
     const Extension& extension) const {
