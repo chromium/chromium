ae769c0ee3a45c1c78ab0b9078fcc75884a58d59
Ramin Halavati <rhalavati@chromium.org>
2025-10-22 22:41:48 -0700

    [a11y] Return empty role description for RadioButton role on Android.
    Talkback announces the type of `kRadioButton` nodes as "Radio button"
    and does not need Chrome to provide role description.
    
    Update `BrowserAccessibilityAndroid::GetRoleDescription` and remove role
    description for radio buttons in all Android test expectations.
    
    AX-Relnotes: n/a
    Bug: 447360631
    Change-Id: Ifbcfb5f7fed421b1143d1883f7e606cbe2e48c16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7072533
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1534081}

--- content/browser/accessibility/browser_accessibility_android.cc
+++ content/browser/accessibility/browser_accessibility_android.cc
@@ -1540,6 +1540,7 @@ std::u16string BrowserAccessibilityAndroid::GetRoleDescription() const {
     case ax::mojom::Role::kGrid:
     case ax::mojom::Role::kImage:
     case ax::mojom::Role::kProgressIndicator:
+    case ax::mojom::Role::kRadioButton:
     case ax::mojom::Role::kRowGroup:
     case ax::mojom::Role::kSectionFooter:
     case ax::mojom::Role::kSectionHeader:
