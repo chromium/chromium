ecdf8cbec427f6df53c187ab62abd85297ad42a8
Colin Blundell <blundell@chromium.org>
2025-10-22 23:50:17 -0700

    [Blink] Eliminate more nesting in StaticBitmapImageTransform method
    Streamlines the code here in anticipation of decoupling the CRPSI and
    CRPBitmap codepaths.
    
    Bug: 352263194
    Change-Id: If2e2df7cb7d34df3348408dc8c3d598738980507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7069746
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534102}

--- third_party/blink/renderer/platform/graphics/static_bitmap_image_transform.cc
+++ third_party/blink/renderer/platform/graphics/static_bitmap_image_transform.cc
@@ -265,20 +265,19 @@ scoped_refptr<StaticBitmapImage> StaticBitmapImageTransform::ApplyWithBlit(
 
   // Create the resource provider for the target for the blit.
   std::unique_ptr<CanvasResourceProvider> resource_provider;
-  // If `source` is accelerated, then use a SharedImage provider.
-  if (source_paint_image.IsTextureBacked()) {
-    base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider =
-        source->ContextProviderWrapper();
-    if (context_provider) {
-      const gpu::SharedImageUsageSet shared_image_usage_flags =
-          source->GetSharedImage()->usage();
-      resource_provider = CanvasResourceProvider::CreateSharedImageProvider(
-          gfx::Size(dest_size.width(), dest_size.height()),
-          viz::SkColorTypeToSinglePlaneSharedImageFormat(dest_color_type),
-          dest_alpha_type, SkColorSpaceToGfxColorSpace(dest_color_space),
-          CanvasResourceProvider::ShouldInitialize::kNo, context_provider,
-          RasterMode::kGPU, shared_image_usage_flags);
-    }
+  // If `source` is accelerated and there is a context provider, try to use an
+  // accelerated SharedImage provider.
+  if (source_paint_image.IsTextureBacked() &&
+      source->ContextProviderWrapper()) {
+    const gpu::SharedImageUsageSet shared_image_usage_flags =
+        source->GetSharedImage()->usage();
+    resource_provider = CanvasResourceProvider::CreateSharedImageProvider(
+        gfx::Size(dest_size.width(), dest_size.height()),
+        viz::SkColorTypeToSinglePlaneSharedImageFormat(dest_color_type),
+        dest_alpha_type, SkColorSpaceToGfxColorSpace(dest_color_space),
+        CanvasResourceProvider::ShouldInitialize::kNo,
+        source->ContextProviderWrapper(), RasterMode::kGPU,
+        shared_image_usage_flags);
   }
   // If not (or if the SharedImage provider fails), fall back to software.
   if (!resource_provider) {
