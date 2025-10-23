0f4964d19cc24fd11eadccd2a1a0d000929b2725
Lei Zhang <thestig@chromium.org>
2025-10-23 13:01:22 -0700

    Disable flaky SettingsClearBrowsingDataV2Test.OtherGoogleDataDialog
    This test was recently re-enabled, but is still somewhat flaky, so
    disable it again.
    
    Bug: 440503425
    Change-Id: Icf221a1352ad8eb0ef72e5bb6d7345c163cd8c69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7077215
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534553}

--- chrome/test/data/webui/settings/settings_browsertest.cc
+++ chrome/test/data/webui/settings/settings_browsertest.cc
@@ -959,7 +959,8 @@ IN_PROC_BROWSER_TEST_F(SettingsClearBrowsingDataV2Test,
 }
 
 // TODO(crbug.com/440503425): Flaky on all platforms.
-IN_PROC_BROWSER_TEST_F(SettingsClearBrowsingDataV2Test, OtherGoogleDataDialog) {
+IN_PROC_BROWSER_TEST_F(SettingsClearBrowsingDataV2Test,
+                       DISABLED_OtherGoogleDataDialog) {
   RunTest("settings/other_google_data_dialog_test.js",
           "runMochaSuite('OtherGoogleDataDialog')");
 }
