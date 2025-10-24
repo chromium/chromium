cdf6ab82596bbf865adf6aec9fe6c8684c4b9c8b
Yiren Wang <yrw@chromium.org>
2025-10-23 17:42:41 -0700

    Fix flaky ActorMediaControlToolBrowserTest tests
    Bug: 409427974
    Change-Id: I642954324139c0a511f72ff785c3ba551f6530a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075875
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1534705}

--- chrome/browser/actor/tools/media_control_tool_browsertest.cc
+++ chrome/browser/actor/tools/media_control_tool_browsertest.cc
@@ -8,10 +8,8 @@
 #include "chrome/browser/actor/tools/media_control_tool_request.h"
 #include "chrome/browser/actor/tools/tools_test_util.h"
 #include "chrome/common/actor.mojom.h"
-#include "content/public/browser/media_session.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
-#include "services/media_session/public/cpp/test/mock_media_session.h"
 
 namespace actor {
 
@@ -54,10 +52,6 @@ IN_PROC_BROWSER_TEST_P(ActorMediaControlToolBrowserTest, PauseAndPlayMedia) {
 
   // Start playback.
   ASSERT_TRUE(content::ExecJs(web_contents(), "play()"));
-  media_session::test::MockMediaSessionMojoObserver observer(
-      *content::MediaSession::Get(web_contents()));
-  observer.WaitForPlaybackState(
-      media_session::mojom::MediaPlaybackState::kPlaying);
 
   // Pause the media.
   ActResultFuture pause_result;
@@ -65,9 +59,7 @@ IN_PROC_BROWSER_TEST_P(ActorMediaControlToolBrowserTest, PauseAndPlayMedia) {
       MakeMediaControlRequest(*active_tab(), PauseMedia());
   actor_task().Act(ToRequestList(pause_request), pause_result.GetCallback());
   ExpectOkResult(pause_result);
-
-  // TODO(b/409427974): Fix flaky browser test.
-  // EXPECT_EQ("pause", content::EvalJs(web_contents(), "event_log.join(',')"));
+  EXPECT_EQ("pause", content::EvalJs(web_contents(), "event_log.join(',')"));
 
   // Play the media.
   ActResultFuture play_result;
@@ -75,10 +67,8 @@ IN_PROC_BROWSER_TEST_P(ActorMediaControlToolBrowserTest, PauseAndPlayMedia) {
       MakeMediaControlRequest(*active_tab(), PlayMedia());
   actor_task().Act(ToRequestList(play_request), play_result.GetCallback());
   ExpectOkResult(play_result);
-
-  // TODO(b/409427974): Fix flaky browser test.
-  // EXPECT_EQ("pause,play",
-  //          content::EvalJs(web_contents(), "event_log.join(',')"));
+  EXPECT_EQ("pause,play",
+            content::EvalJs(web_contents(), "event_log.join(',')"));
 }
 
 IN_PROC_BROWSER_TEST_P(ActorMediaControlToolBrowserTest, SeekMedia) {
@@ -88,10 +78,6 @@ IN_PROC_BROWSER_TEST_P(ActorMediaControlToolBrowserTest, SeekMedia) {
 
   // Start playback.
   ASSERT_TRUE(content::ExecJs(web_contents(), "play()"));
-  media_session::test::MockMediaSessionMojoObserver observer(
-      *content::MediaSession::Get(web_contents()));
-  observer.WaitForPlaybackState(
-      media_session::mojom::MediaPlaybackState::kPlaying);
 
   // Seek the media.
   ActResultFuture result;
@@ -99,10 +85,7 @@ IN_PROC_BROWSER_TEST_P(ActorMediaControlToolBrowserTest, SeekMedia) {
       MakeMediaControlRequest(*active_tab(), SeekMedia(1000000));
   actor_task().Act(ToRequestList(request), result.GetCallback());
   ExpectOkResult(result);
-
-  // TODO(b/409427974): Fix flaky browser test.
-  // EXPECT_EQ("seek 1", content::EvalJs(web_contents(),
-  // "event_log.join(',')"));
+  EXPECT_EQ("seek 1", content::EvalJs(web_contents(), "event_log.join(',')"));
 }
 
 }  // namespace
