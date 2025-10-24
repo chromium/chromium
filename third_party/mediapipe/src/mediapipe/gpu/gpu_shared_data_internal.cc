4835d0c2c287354ecf3a73678f7312c96b5d992b
ahmedmoussa <ahmedmoussa@google.com>
2025-10-24 11:05:49 -0700

    Remove mediapipe_build_with_gpu_support flag
    This is no longer needed. It was added previously to manage and apply
    effects on video frames. Now, the camera effects project has been
    canceled.
    
    Bug: b:443177498
    Change-Id: I32e6f3fe83b0df09e4c01975d8913533843515e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7083148
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535168}

--- third_party/mediapipe/src/mediapipe/gpu/gpu_shared_data_internal.cc
+++ third_party/mediapipe/src/mediapipe/gpu/gpu_shared_data_internal.cc
@@ -206,7 +206,9 @@ absl::Status GpuResources::PrepareGpuNode(CalculatorNode* node) {
 #ifndef __EMSCRIPTEN__
   // TODO Allow calculators to request a separate context.
   // For now, allow a few calculators to run in their own context.
-  bool gets_own_context = false;
+  bool gets_own_context = (node_type == "ImageFrameToGpuBufferCalculator") ||
+                          (node_type == "GpuBufferToImageFrameCalculator") ||
+                          (node_type == "GlSurfaceSinkCalculator");
 
   const auto& options =
       node->GetCalculatorState().Options<mediapipe::GlContextOptions>();
