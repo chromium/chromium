c1a384862c61891414b54cbb6e5d32ad05cb41e9
Bhaskar Sharma <bhaskarsharma@google.com>
2025-10-27 06:52:52 -0700

    Make the versions appear in descending order on web-app-internals.
    When installing IWA from Update Manifest, the versions will appear in
    descending order and the latest will be selected by default.
    
    Fix: 453998057
    Change-Id: I6c2c1358c41e255c9649dfdb353b0d4c8ab8fb54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079448
    Reviewed-by: Dominik Bylica <bylica@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Bhaskar Sharma <bhaskarsharma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1535915}

--- chrome/browser/resources/web_app_internals/web_app_internals.ts
+++ chrome/browser/resources/web_app_internals/web_app_internals.ts
@@ -225,6 +225,27 @@ async function iwaDevFetchUpdateManifest() {
       'iwa-update-manifest-version-select');
   select.replaceChildren();
 
+  function compareStringVersions(v1: string, v2: string): number {
+    const parts1 = v1.split('.').map(Number);
+    const parts2 = v2.split('.').map(Number);
+
+    for (let i = 0; i < Math.max(parts1.length, parts2.length); i++) {
+      const part1 = parts1[i] || 0;
+      const part2 = parts2[i] || 0;
+
+      if (part1 < part2) {
+        return -1;
+      } else if (part1 > part2) {
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+
+  // Sort versions in descending order
+  versions.sort((a, b) => -compareStringVersions(a.version, b.version));
+
   for (const versionEntry of versions) {
     const option = document.createElement('option');
     option.value = versionEntry.version;
