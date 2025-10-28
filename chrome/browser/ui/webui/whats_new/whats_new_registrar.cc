772e662084f8b7df4e17493e35cd9d6d0de92d48
Andy Phan <andyphan@chromium.org>
2025-10-28 15:04:52 -0700

    [PDF Ink Signatures] Add What's New module for Ink2
    Add a What's New module for the Ink2 freehand annotation feature.
    
    Bug: 455529627
    Change-Id: I8de2dc439822fcbbdff91998ab33f78d055c1e16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7086872
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536923}

--- chrome/browser/ui/webui/whats_new/whats_new_registrar.cc
+++ chrome/browser/ui/webui/whats_new/whats_new_registrar.cc
@@ -12,8 +12,13 @@
 #include "components/lens/lens_features.h"
 #include "components/performance_manager/public/features.h"
 #include "components/user_education/webui/whats_new_registry.h"
+#include "pdf/buildflags.h"
 #include "ui/accessibility/accessibility_features.h"
 #include "ui/webui/resources/js/browser_command/browser_command.mojom.h"
+
+#if BUILDFLAG(ENABLE_PDF)
+#include "pdf/pdf_features.h"
+#endif  // BUILDFLAG(ENABLE_PDF)
 
 namespace whats_new {
 using BrowserCommand = browser_command::mojom::Command;
@@ -50,6 +55,12 @@ void RegisterWhatsNewModules(whats_new::WhatsNewRegistry* registry) {
   registry->RegisterModule(WhatsNewModule(::features::kSideBySide,
                                           "agale@google.com",
                                           BrowserCommand::kOpenSplitView));
+
+  // M144
+#if BUILDFLAG(ENABLE_PDF)
+  registry->RegisterModule(
+      WhatsNewModule(chrome_pdf::features::kPdfInk2, "andyphan@chromium.org"));
+#endif  // BUILDFLAG(ENABLE_PDF)
 }
 
 void RegisterWhatsNewEditions(whats_new::WhatsNewRegistry* registry) {
