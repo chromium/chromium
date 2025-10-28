afe7eb41c6798d400d6e203a5296b6711d8be852
Peter Wen <wnwen@chromium.org>
2025-10-28 10:20:29 -0700

    android: Fix x64 render test model key (reland)
    Original CL: https://crrev.com/c/7068312
    
    Reland fixed:
    - Rebased on infra change ignoring tablet Skia Goldens.
    
    For gardeners: Please avoid reverting this CL for golden failures. This
    CL adds API level 35 to the list of android render test APIs, so Skia
    golden failures caused by this CL are likely just ones that need to be
    triaged as "positive" new images. Thank you!
    
    Update RENDER_TEST_MODEL_SDK_CONFIGS to reflect the current product
    model name used by the x64 Android emulators ('sdk_gphone64_x86_64').
    
    Also add a comment indicating how to find the required product model.
    
    Fix
    chrome/android/javatests/src/org/chromium/chrome/browser/app/edge_to_edge/EdgeToEdgeInstrumentationTest.java
    to a specific port to avoid endlessly triaging goldens with a different
    port in the URL.
    
    R=hnakashima@chromium.org
    
    Bug: 447426658
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:android-15-x64-rel,android-15-tablet-landscape-x64-rel
    Change-Id: I5c58ba1b55bbaa7c084c5add36944b6905bdbad1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7088856
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Owners-Override: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1536765}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/edge_to_edge/EdgeToEdgeInstrumentationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/edge_to_edge/EdgeToEdgeInstrumentationTest.java
@@ -105,6 +105,7 @@ public class EdgeToEdgeInstrumentationTest {
 
     @Before
     public void setUp() {
+        mActivityTestRule.getEmbeddedTestServerRule().setServerPort(12345);
         mTestServer = mActivityTestRule.getTestServer();
         mActivity = mActivityTestRule.getActivity();
         assertNotNull(mActivity);
