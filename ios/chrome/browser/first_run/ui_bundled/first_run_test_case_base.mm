671b1601e6f52f474a05cb38d5a39c376a064b02
Robbie Gibson <rkgibson@google.com>
2025-10-28 11:37:12 -0700

    [iOS] Fix FRE tests when fieldtrial config is disabled
    The FRESignInHeaderTextUpdate feature changes the header text here, so
    the tests were failing due to looking for the wrong string. Instead,
    search for any of the 3 possible strings (default, feature arm 0,
    feature arm 1).
    
    Fixed: b:454956602
    Change-Id: Ib4aa31a724b4e626241a1cb9bde9fe226400cb0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7079161
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1536811}

--- ios/chrome/browser/first_run/ui_bundled/first_run_test_case_base.mm
+++ ios/chrome/browser/first_run/ui_bundled/first_run_test_case_base.mm
@@ -130,12 +130,16 @@
                  grey_accessibilityID(
                      first_run::kFirstRunSignInScreenAccessibilityIdentifier)]
       assertWithMatcher:grey_notNil()];
-  NSString* title = nil;
+  id<GREYMatcher> titleMatcher = nil;
   NSString* subtitle = nil;
   NSArray* disclaimerStrings = nil;
   switch (FRESigninIntent) {
     case FRESigninIntentRegular:
-      title = l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_0);
+      titleMatcher = grey_anyOf(
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE)),
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_0)),
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_1)),
+          nil);
       subtitle = l10n_util::GetNSString(
           IDS_IOS_FIRST_RUN_SIGNIN_BENEFITS_SUBTITLE_SHORT);
       disclaimerStrings = @[
@@ -146,8 +150,8 @@
       ];
       break;
     case FRESigninIntentSigninForcedByPolicy:
-      title =
-          l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_SIGNIN_FORCED);
+      titleMatcher = grey_text(
+          l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_SIGNIN_FORCED));
       subtitle = l10n_util::GetNSString(
           IDS_IOS_FIRST_RUN_SIGNIN_SUBTITLE_SIGNIN_FORCED);
       disclaimerStrings = @[
@@ -161,11 +165,11 @@
       break;
     case FRESigninIntentSigninDisabledByPolicy:
       if ([ChromeEarlGrey isIPadIdiom]) {
-        title =
-            l10n_util::GetNSString(IDS_IOS_FIRST_RUN_WELCOME_SCREEN_TITLE_IPAD);
+        titleMatcher = grey_text(l10n_util::GetNSString(
+            IDS_IOS_FIRST_RUN_WELCOME_SCREEN_TITLE_IPAD));
       } else {
-        title = l10n_util::GetNSString(
-            IDS_IOS_FIRST_RUN_WELCOME_SCREEN_TITLE_IPHONE);
+        titleMatcher = grey_text(l10n_util::GetNSString(
+            IDS_IOS_FIRST_RUN_WELCOME_SCREEN_TITLE_IPHONE));
       }
       subtitle =
           l10n_util::GetNSString(IDS_IOS_FIRST_RUN_WELCOME_SCREEN_SUBTITLE);
@@ -179,7 +183,11 @@
       ];
       break;
     case FRESigninIntentSigninWithSyncDisabledPolicy:
-      title = l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_0);
+      titleMatcher = grey_anyOf(
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE)),
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_0)),
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_1)),
+          nil);
       // Note: With SyncDisabled, the "benefits" string is not used.
       subtitle =
           l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_SUBTITLE_SHORT);
@@ -193,7 +201,11 @@
       ];
       break;
     case FRESigninIntentSigninWithPolicy:
-      title = l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_0);
+      titleMatcher = grey_anyOf(
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE)),
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_0)),
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_1)),
+          nil);
       subtitle = l10n_util::GetNSString(
           IDS_IOS_FIRST_RUN_SIGNIN_BENEFITS_SUBTITLE_SHORT);
       disclaimerStrings = @[
@@ -206,7 +218,11 @@
       ];
       break;
     case FRESigninIntentSigninWithUMAReportingDisabledPolicy:
-      title = l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_0);
+      titleMatcher = grey_anyOf(
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE)),
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_0)),
+          grey_text(l10n_util::GetNSString(IDS_IOS_FIRST_RUN_SIGNIN_TITLE_1)),
+          nil);
       subtitle = l10n_util::GetNSString(
           IDS_IOS_FIRST_RUN_SIGNIN_BENEFITS_SUBTITLE_SHORT);
       disclaimerStrings = @[
@@ -219,7 +235,7 @@
   }
   // Validate the Title text.
   [[self elementInteractionWithGreyMatcher:grey_allOf(
-                                               grey_text(title),
+                                               titleMatcher,
                                                grey_sufficientlyVisible(), nil)
                       scrollViewIdentifier:
                           kPromoStyleScrollViewAccessibilityIdentifier]
