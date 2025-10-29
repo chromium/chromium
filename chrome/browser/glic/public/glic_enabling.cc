f88e45ca4ac028856760355c397232f86c1aee2b
Michelle Abreo <michelleabreo@google.com>
2025-10-29 13:36:40 -0700

    Add multi instance to unified FRE check.
    Change-Id: I66b59bae3165de77ef8f5a9a62e7c43b6f8910da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7096705
    Commit-Queue: Dan Harrington <harringtond@chromium.org>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Auto-Submit: Michelle Abreo <michelleabreo@google.com>
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537510}

--- chrome/browser/glic/public/glic_enabling.cc
+++ chrome/browser/glic/public/glic_enabling.cc
@@ -183,7 +183,8 @@ void GlicEnabling::OnGlicSettingsPolicyChanged() {
 }
 
 bool GlicEnabling::IsUnifiedFreEnabled(Profile* profile) {
-  return base::FeatureList::IsEnabled(features::kGlicUnifiedFreScreen);
+  return base::FeatureList::IsEnabled(features::kGlicMultiInstance) &&
+         base::FeatureList::IsEnabled(features::kGlicUnifiedFreScreen);
 }
 
 GlicEnabling::GlicEnabling(Profile* profile,
