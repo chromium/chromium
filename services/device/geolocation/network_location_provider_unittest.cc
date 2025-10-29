f1b87800d4ff2668594d263ffc24171bcded5379
Alvin Ji <alvinji@chromium.org>
2025-10-28 17:20:35 -0700

    Remove geolocation_service_unittest
    Moves the API key URL verification test from
    geolocation_service_unittest.cc` to
    `network_location_provider_unittest.cc`. The existing `NonEmptyApiKey`
    test was updated to perform the more thorough URL check.
    
    Change-Id: I4e12f71943dd9b30c5e38102ebc12099fe591dc3
    Bug: 453817118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7076181
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537012}

--- services/device/geolocation/network_location_provider_unittest.cc
+++ services/device/geolocation/network_location_provider_unittest.cc
@@ -309,15 +309,20 @@ TEST_F(GeolocationNetworkProviderTest, EmptyApiKey) {
 // Tests that, with non-empty api_key, a "key" query string parameter is
 // included in the request.
 TEST_F(GeolocationNetworkProviderTest, NonEmptyApiKey) {
-  const std::string api_key = "something";
-  std::unique_ptr<LocationProvider> provider(CreateProvider(true, api_key));
+  const char kTestGeolocationApiKey[] = "fake_api_key_for_test";
+  std::unique_ptr<LocationProvider> provider(
+      CreateProvider(true, kTestGeolocationApiKey));
   provider->StartProvider(false);
 
   ASSERT_EQ(1, test_url_loader_factory_.NumPending());
   const GURL& request_url =
       test_url_loader_factory_.pending_requests()->back().request.url;
-  EXPECT_TRUE(request_url.has_query());
-  EXPECT_TRUE(base::StartsWith(request_url.query(), "key="));
+
+  std::string expected_url =
+      "https://www.googleapis.com/geolocation/v1/geolocate?key=";
+  expected_url.append(kTestGeolocationApiKey);
+
+  EXPECT_EQ(request_url.spec(), expected_url);
 }
 
 // Tests that, after StartProvider(), a TestURLFetcher can be extracted,
