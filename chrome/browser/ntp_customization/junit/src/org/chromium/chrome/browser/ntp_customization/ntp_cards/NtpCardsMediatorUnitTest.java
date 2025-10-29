7cb63b93c43929bf9d2ea1eca80eb968de13c52b
Jenna Himawan <jhimawan@google.com>
2025-10-29 12:26:14 -0700

    [Canary] Remove updateUserPrefs call
    (cherry picked from commit a6ee8db1f62e5676d71b467ad307a9331f2131d2)
    
    Bug: 444690101, 454723872
    Change-Id: I1824fbb5f9b57fb2447e0fa04953c972ecd15fd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7097503
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Original-Commit-Position: refs/branch-heads/7500@{#13}
    Cr-Original-Branched-From: ff3e023445af793ab788c3452efd20bf1f9953da-refs/heads/main@{#1537021}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7097601
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Cr-Commit-Position: refs/branch-heads/7501@{#6}
    Cr-Branched-From: 3ec080c8b46cc8ed9f3d9997e13a513705b8c4f6-refs/heads/main@{#1537268}

--- chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/ntp_cards/NtpCardsMediatorUnitTest.java
+++ chrome/browser/ntp_customization/junit/src/org/chromium/chrome/browser/ntp_customization/ntp_cards/NtpCardsMediatorUnitTest.java
@@ -161,7 +161,6 @@ public class NtpCardsMediatorUnitTest {
 
         verify(mBottomSheetPropertyModel).set(eq(BACK_PRESS_HANDLER), eq(null));
         verify(mContainerPropertyModel).set(eq(LIST_CONTAINER_VIEW_DELEGATE), eq(null));
-        verify(spy).updateUserPrefs();
     }
 
     @Test
