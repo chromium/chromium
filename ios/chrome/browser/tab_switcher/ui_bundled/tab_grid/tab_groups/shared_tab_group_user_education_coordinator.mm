8a6a6338d4d4c3800606d0a74c1713944b60dad5
Ewann Pelle <ewannpv@chromium.org>
2025-10-29 09:51:26 -0700

    [iOS] Configure ButtonStack in Tab Switcher UI
    This change refactors the Tab Switcher UI to use the new ButtonStackViewController.
    
    Bug: 444648926
    Change-Id: I657f12f385bc287cd927898f72a908429c73b4be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7083633
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537403}

--- ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_group_user_education_coordinator.mm
+++ ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/shared_tab_group_user_education_coordinator.mm
@@ -6,6 +6,7 @@
 
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/tab_groups/tab_groups_constants.h"
+#import "ios/chrome/common/ui/button_stack/button_stack_configuration.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_action_handler.h"
 #import "ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.h"
@@ -86,14 +87,17 @@ UIImage* TabGroupImage(UITraitCollection* trait_collection) {
 }
 
 - (void)start {
+  ButtonStackConfiguration* configuration =
+      [[ButtonStackConfiguration alloc] init];
+  configuration.primaryActionString =
+      l10n_util::GetNSString(IDS_IOS_SHARED_GROUP_USER_EDUCATION_SHEET_GOT_IT);
   ConfirmationAlertViewController* confirmationAlert =
-      [[ConfirmationAlertViewController alloc] init];
+      [[ConfirmationAlertViewController alloc]
+          initWithConfiguration:configuration];
   confirmationAlert.titleString =
       l10n_util::GetNSString(IDS_IOS_SHARED_GROUP_USER_EDUCATION_SHEET_TITLE);
   confirmationAlert.subtitleString = l10n_util::GetNSString(
       IDS_IOS_SHARED_GROUP_USER_EDUCATION_SHEET_SUBTITLE);
-  confirmationAlert.primaryActionString =
-      l10n_util::GetNSString(IDS_IOS_SHARED_GROUP_USER_EDUCATION_SHEET_GOT_IT);
   confirmationAlert.showDismissBarButton = NO;
   confirmationAlert.image = TabGroupImage(confirmationAlert.traitCollection);
   confirmationAlert.imageHasFixedSize = YES;
