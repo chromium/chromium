0ebf6db89ab78365e7aa20dde6ff68e606d7aa86
Eric Ekey <ericekey@google.com>
2025-10-29 11:51:41 -0700

    [iOS][SyncedSetUp] Add Utils for Pref Selection
    This change adds utils for selecting a set of prefs to apply to the
    local device during the Synced Set Up experience. A future change will
    move these C++ utils to components/ to be reusable for Android and iOS.
    The SyncedSetUpMediator will invoke the `GetRemoteDevicePrefs()` method
    to retrieve necessary pref data.
    
    R=bwwilliams@google.com, scottyoder@google.com
    
    Fixed: 455542654
    Change-Id: Ie2211bee117dd0e4ccd0c74612a500c62a6c3795
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7080262
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537478}

--- ios/chrome/browser/synced_set_up/utils/utils.h
+++ ios/chrome/browser/synced_set_up/utils/utils.h
@@ -5,10 +5,36 @@
 #ifndef IOS_CHROME_BROWSER_SYNCED_SET_UP_UTILS_UTILS_H_
 #define IOS_CHROME_BROWSER_SYNCED_SET_UP_UTILS_UTILS_H_
 
-#import <Foundation/Foundation.h>
+#import <map>
+#import <string_view>
+
+namespace base {
+class Value;
+}  // namespace base
+
+namespace syncer {
+class DeviceInfo;
+class DeviceInfoTracker;
+}  // namespace syncer
+
+namespace sync_preferences {
+class CrossDevicePrefTracker;
+}  // namespace sync_preferences
 
 @class ProfileState;
 @class SceneState;
+
+// Returns a map of tracked pref names and values corresponding to the "best
+// match" prefs for the Synced Set Up flow to apply.
+std::map<std::string_view, base::Value> GetRemoteDevicePrefs(
+    const sync_preferences::CrossDevicePrefTracker* pref_tracker,
+    const syncer::DeviceInfoTracker* device_info_tracker,
+    const syncer::DeviceInfo* local_device);
+
+// Helper that returns the name of a tracked pref for the current platform,
+// given the corresponding cross device pref name.
+std::string_view GetTrackedPrefName(
+    const std::string_view& cross_device_pref_name);
 
 // Returns the active, non-incognito `SceneState` if preconditions for
 // triggering the Synced Set Up flow are met based on `profile_state`, and `nil`
