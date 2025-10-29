8a0ac7f9769a4d5d3815a28d7ae6950612285a79
Kelvin Jiang <kelvinjiang@chromium.org>
2025-10-29 15:17:06 -0700

    [Extensions] Add new native message host subclass for Android
    Add NativeMessageAndroidHost which is compiled/used only on Android.
    This is required since native messaging hosts cannot be supported on
    Android because it does not support (by design/definition) directly
    running executable files unlike Windows/Mac/Linux.
    
    This CL contains no implementation logic.
    
    Bug: 417786914
    Change-Id: Ifa9ef72a7b7e90ca794ea12a89b179bf194753e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7075839
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1537576}

---
+++ chrome/browser/extensions/api/messaging/native_message_android_host.cc
@@ -0,0 +1,60 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/api/messaging/native_message_android_host.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <memory>
+#include <utility>
+
+#include "base/logging.h"
+#include "base/task/single_thread_task_runner.h"
+#include "content/public/browser/browser_context.h"
+#include "content/public/browser/browser_thread.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
+
+namespace extensions {
+
+// static
+std::unique_ptr<NativeMessageHost> NativeMessageHost::Create(
+    content::BrowserContext* browser_context,
+    gfx::NativeView native_view,
+    const ExtensionId& source_extension_id,
+    const std::string& native_host_name,
+    bool allow_user_level,
+    std::string* error_message) {
+  return std::make_unique<NativeMessageAndroidHost>(source_extension_id,
+                                                    native_host_name);
+}
+
+NativeMessageAndroidHost::NativeMessageAndroidHost(
+    const ExtensionId& source_extension_id,
+    const std::string& native_host_name)
+    : source_extension_id_(source_extension_id),
+      native_app_name_(native_host_name) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  task_runner_ = content::GetIOThreadTaskRunner({});
+}
+
+NativeMessageAndroidHost::~NativeMessageAndroidHost() {
+  DCHECK(task_runner_->BelongsToCurrentThread());
+}
+
+void NativeMessageAndroidHost::OnMessage(const std::string& json) {}
+
+void NativeMessageAndroidHost::Start(Client* client) {
+  DCHECK(task_runner_->BelongsToCurrentThread());
+  DCHECK(!client_);
+  client_ = client;
+}
+
+scoped_refptr<base::SingleThreadTaskRunner>
+NativeMessageAndroidHost::task_runner() const {
+  return task_runner_;
+}
+
+}  // namespace extensions
