eae55276637b9335cd5dc7b0aabdf4563301542b
Fiona Macintosh <fmacintosh@google.com>
2025-10-30 08:03:27 -0700

    [ACT] Clean up unused PrivacyElementInteractions
    Bug: b:452905381
    Change-Id: I4e3cf35971891d95306588c582be2e242a59e2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101300
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1537938}

--- chrome/browser/resources/settings/metrics_browser_proxy.ts
+++ chrome/browser/resources/settings/metrics_browser_proxy.ts
@@ -39,8 +39,8 @@ export enum PrivacyElementInteractions {
   THIRD_PARTY_COOKIES_BLOCK_IN_INCOGNITO = 21,
   THIRD_PARTY_COOKIES_BLOCK = 22,
   BLOCK_ALL_THIRD_PARTY_COOKIES = 23,
-  IP_PROTECTION = 24,
-  FINGERPRINTING_PROTECTION = 25,
+  // IP_PROTECTION = 24,
+  // FINGERPRINTING_PROTECTION = 25,
   // Max value should be updated whenever new entries are added.
   MAX_VALUE = 26,
 }
