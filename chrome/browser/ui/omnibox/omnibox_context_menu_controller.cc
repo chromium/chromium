0fb43ce1928e10f9bdb0e424583989db5492f612
Marlon Facey <mfacey@chromium.org>
2025-10-31 13:06:51 -0700

    [Omnibox Next] Add icons to context menu
    Add vector icons for add file, add image and deep search icons. Add
    png for create images icon.
    
    Light: http://screenshot/53L8NN2GykZBs5V
    Dark: http://screenshot/8ms2Ssqr2z2kPhB
    
    Bug: 452331116
    Change-Id: Ieee95fa1df3271359a8f9353b94dc696804f3fe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7098312
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538803}

--- chrome/browser/ui/omnibox/omnibox_context_menu_controller.cc
+++ chrome/browser/ui/omnibox/omnibox_context_menu_controller.cc
@@ -11,6 +11,7 @@
 
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
+#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
@@ -18,7 +19,10 @@
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/grit/generated_resources.h"
+#include "chrome/grit/omnibox_popup_resources.h"
 #include "content/public/common/url_constants.h"
+#include "ui/base/models/image_model.h"
+#include "ui/gfx/image/image.h"
 
 OmniboxContextMenuController::OmniboxContextMenuController(
     BrowserWindowInterface* browser_window_interface)
@@ -31,19 +35,18 @@ OmniboxContextMenuController::~OmniboxContextMenuController() = default;
 
 void OmniboxContextMenuController::BuildMenu() {
   AddRecentTabItems();
-  AddItem(IDC_OMNIBOX_CONTEXT_ADD_IMAGE, IDS_NTP_COMPOSE_ADD_IMAGE);
-  AddItem(IDC_OMNIBOX_CONTEXT_ADD_FILE, IDS_NTP_COMPOSE_ADD_FILE);
-  AddSeparator();
-  AddItem(IDC_OMNIBOX_CONTEXT_CREATE_IMAGES, IDS_NTP_COMPOSE_CREATE_IMAGES);
-  AddItem(IDC_OMNIBOX_CONTEXT_DEEP_RESEARCH, IDS_NTP_COMPOSE_DEEP_SEARCH);
+  AddStaticItems();
 }
 
 void OmniboxContextMenuController::AddItem(int id, const std::u16string str) {
   menu_model_->AddItem(id, str);
 }
 
-void OmniboxContextMenuController::AddItem(int id, int localization_id) {
-  menu_model_->AddItemWithStringId(id, localization_id);
+void OmniboxContextMenuController::AddItemWithStringIdAndIcon(
+    int id,
+    int localization_id,
+    const ui::ImageModel& icon) {
+  menu_model_->AddItemWithStringIdAndIcon(id, localization_id, icon);
 }
 
 void OmniboxContextMenuController::AddSeparator() {
@@ -53,11 +56,7 @@ void OmniboxContextMenuController::AddSeparator() {
 void OmniboxContextMenuController::AddRecentTabItems() {
   // Iterate through the tab strip model, getting the data for each tab.
   auto* tab_strip_model = browser_window_interface_->GetTabStripModel();
-  if (tab_strip_model->count() == 1 &&
-      !IsValidTab(TabRendererData::FromTabInModel(tab_strip_model, 0)
-                      .last_committed_url)) {
-    return;
-  }
+  size_t valid_tab_count = 0;
   AddTitleWithStringId(IDS_NTP_COMPOSE_MOST_RECENT_TABS);
   for (int i = 0; i < tab_strip_model->count(); i++) {
     TabRendererData tab_renderer_data =
@@ -67,8 +66,39 @@ void OmniboxContextMenuController::AddRecentTabItems() {
       continue;
     }
     AddItem(i, tab_renderer_data.title);
+    valid_tab_count += 1;
+  }
+  if (!valid_tab_count) {
+    auto index = menu_model_->GetIndexOfCommandId(ui::MenuModel::kTitleId);
+    if (index) {
+      menu_model_->RemoveItemAt(index.value());
+      return;
+    }
   }
   AddSeparator();
+}
+
+void OmniboxContextMenuController::AddStaticItems() {
+  auto add_image_icon =
+      ui::ImageModel::FromVectorIcon(kAddPhotoAlternateIcon, ui::kColorMenuIcon,
+                                     ui::SimpleMenuModel::kDefaultIconSize);
+  AddItemWithStringIdAndIcon(IDC_OMNIBOX_CONTEXT_ADD_IMAGE,
+                             IDS_NTP_COMPOSE_ADD_IMAGE, add_image_icon);
+  auto add_file_icon =
+      ui::ImageModel::FromVectorIcon(kAttachFileIcon, ui::kColorMenuIcon,
+                                     ui::SimpleMenuModel::kDefaultIconSize);
+  AddItemWithStringIdAndIcon(IDC_OMNIBOX_CONTEXT_ADD_FILE,
+                             IDS_NTP_COMPOSE_ADD_FILE, add_file_icon);
+  AddSeparator();
+  auto deep_search_icon =
+      ui::ImageModel::FromVectorIcon(kTravelExploreIcon, ui::kColorMenuIcon,
+                                     ui::SimpleMenuModel::kDefaultIconSize);
+  AddItemWithStringIdAndIcon(IDC_OMNIBOX_CONTEXT_DEEP_RESEARCH,
+                             IDS_NTP_COMPOSE_DEEP_SEARCH, deep_search_icon);
+  auto create_images_icon = ui::ImageModel::FromResourceId(
+      IDR_OMNIBOX_POPUP_IMAGES_CREATE_IMAGES_PNG);
+  AddItemWithStringIdAndIcon(IDC_OMNIBOX_CONTEXT_CREATE_IMAGES,
+                             IDS_NTP_COMPOSE_CREATE_IMAGES, create_images_icon);
 }
 
 void OmniboxContextMenuController::AddTitleWithStringId(int localization_id) {
