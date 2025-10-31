4280616259627fc3c23e5ccf9542db2c3129c1ae
Emilia Paz <emiliapaz@chromium.org>
2025-10-30 17:03:27 -0700

    Revert "Add cws zero state iph variation 3"
    This reverts commit 1e41e69094f303403c7e032ee45ef6481e951531.
    
    Reason for revert: 
    crrev.com/c/7084950 is causing many builders to fail. Revert has conflicts with this CL, so reverting both since they are related
    
    Original change's description:
    > Add cws zero state iph variation 3
    >
    > This cl is similar to https://chromium-review.googlesource.com/c/chromium/src/+/7084950
    >
    > Demo: https://screenshot.googleplex.com/Bw3Mfz9CxjRaJnz
    >
    > Bug: 409573170
    > Change-Id: I8545e2674ec70d26ac99e241ed38ad9a972ff320
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7092648
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    > Commit-Queue: Howard Chang <howardcha@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1538136}
    
    Bug: 409573170
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ie65f3c8335a93ab3789db8bc40c5a49d9e3a4918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103721
    Owners-Override: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1538295}

--- chrome/browser/resources/extensions_zero_state_promo/zero_state_promo_app.html.ts
+++ chrome/browser/resources/extensions_zero_state_promo/zero_state_promo_app.html.ts
@@ -10,9 +10,7 @@ export function getHtml(this: ZeroStatePromoAppElement) {
   // clang-format off
   return html`<!--_html_template_start_-->
 <div id="headerContainer">
-  ${this.showChipsV2_ || this.showChipsV3_ ?
-    html`<h3 id="title">$i18n{extensionsZeroStateIphHeaderV2}</h3>` :
-    html`<h3 id="title">$i18n{extensionsZeroStateIphHeader}</h3>`}
+  <h3 id="title">$i18n{extensionsZeroStateIphHeader}</h3>
   <cr-icon-button id="dismissButton"
       class="icon-clear"
       title="$i18n{extensionsZeroStateIphDismissButtonTitle}"
@@ -48,10 +46,10 @@ ${this.showPlainLinksUi_ ? html`
       $i18n{extensionsZeroStateIphCustomActionButtonLabel}
     </cr-button>
   </div>` : html`
-  ${this.showChipsV1_ ? html`
   <div id="sectionHeaderContainer">
     $i18nRaw{extensionsZeroStateChipsIphDesc}
   </div>
+  ${this.showChipsV1_ ? html`
   <div id='labelContainer'>
     <div class="v1Chip">
       <cr-chip id="couponsButton" chip-role="link"
@@ -81,10 +79,7 @@ ${this.showPlainLinksUi_ ? html`
         $i18n{extensionsZeroStateIphAiProductivityCollectionLabel}
       </cr-chip>
     </div>
-  </div>` : this.showChipsV2_ ? html`
-  <div id="sectionHeaderContainer">
-    $i18nRaw{extensionsZeroStateChipsIphDesc}
-  </div>
+  </div>` : html`
   <div id='labelContainer'>
     <div class="v2Chip">
       <cr-chip id="aiButton"
@@ -112,40 +107,6 @@ ${this.showPlainLinksUi_ ? html`
           @click="${this.onChromeWebStoreButtonClick_}">
         <cr-icon icon="zero-state-promo:webstore"></cr-icon>
         $i18n{extensionsZeroStateIphWebStoreLink}
-      </cr-chip>
-    </div>
-  </div>` : html`
-  <div id="sectionHeaderContainer">
-    <a class="extensionIphLink" href="https://chromewebstore.google.com/">$i18nRaw{extensionsZeroStateChipsWithLinkIphLinkLabel}</a>
-    $i18nRaw{extensionsZeroStateChipsWithLinkIphDesc}
-  </div>
-  <div id='labelContainer'>
-    <div class="v3Chip">
-      <cr-chip id="aiButton"
-          @click="${this.onAiButtonClick_}">
-        <cr-icon icon="zero-state-promo:ai"></cr-icon>
-        $i18n{extensionsZeroStateIphAiProductivityCollectionLabel}
-      </cr-chip>
-    </div>
-    <div class="v3Chip">
-      <cr-chip id="couponsButton" chip-role="link"
-          @click="${this.onCouponsButtonClick_}">
-        <cr-icon icon="zero-state-promo:coupons"></cr-icon>
-        $i18n{extensionsZeroStateIphShoppingCategoryLabel}
-      </cr-chip>
-    </div>
-    <div class="v3Chip">
-      <cr-chip id="writingButton" chip-role="link"
-          @click="${this.onWritingButtonClick_}">
-        <cr-icon icon="zero-state-promo:writing"></cr-icon>
-        $i18n{extensionsZeroStateIphWritingHelpCollectionLabel}
-      </cr-chip>
-    </div>
-    <div class="v3Chip">
-      <cr-chip id="productivityButton"
-          @click="${this.onProductivityButtonClick_}">
-        <cr-icon icon="zero-state-promo:productivity"></cr-icon>
-        $i18n{extensionsZeroStateIphProductivityCategoryLabel}
       </cr-chip>
     </div>
   </div>` }
--- chrome/browser/resources/extensions_zero_state_promo/zero_state_promo_app.ts
+++ chrome/browser/resources/extensions_zero_state_promo/zero_state_promo_app.ts
@@ -39,7 +39,6 @@ export class ZeroStatePromoAppElement extends CrLitElement {
       showPlainLinksUi_: {type: Boolean},
       showChipsV1_: {type: Boolean},
       showChipsV2_: {type: Boolean},
-      showChipsV3_: {type: Boolean},
     };
   }
 
@@ -56,9 +55,6 @@ export class ZeroStatePromoAppElement extends CrLitElement {
 
   protected accessor showChipsV2_: boolean =
       loadTimeData.getBoolean('showChipsUiV2');
-
-  protected accessor showChipsV3_: boolean =
-      loadTimeData.getBoolean('showChipsUiV3');
 
   protected onChromeWebStoreButtonClick_() {
     this.apiProxy_.launchWebStoreLink(WebStoreLinkClicked.kDiscoverExtension);
