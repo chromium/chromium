7b3ff2dea8454839638d01cf30b82f7ad74826a1
Patrick Noland <pnoland@google.com>
2025-11-01 16:30:35 -0700

    Account for status bar in max control container height
    Bug: 456813868
    Change-Id: I55114ca3557f2659f75f4f70d2225100c1282ddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7107413
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539059}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarPositionControllerTest.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarPositionControllerTest.java
@@ -1107,7 +1107,7 @@ public class ToolbarPositionControllerTest {
         doReturn(430).when(mDisplayAndroid).getDisplayHeight();
         mKeyboardHeightSupplier.set(401);
         verify(mControlContainerView).setTranslationY(-(430f - TOOLBAR_HEIGHT - statusBarHeight));
-        verify(mControlContainer, atLeast(1)).setMaxHeight(30);
+        verify(mControlContainer, atLeast(1)).setMaxHeight(20);
     }
 
     @Test
