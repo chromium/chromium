21438746e9803813c2e07eb5b49f6fdfaf77832e
Sylvain Defresne <sdefresne@chromium.org>
2025-11-03 09:27:33 -0800

    [ios] Unconditionally use SessionRestorationServiceImpl
    As the support for legacy session storage has been dropped and all
    users migrated, unconditionally use SessionRestorationServiceImpl
    in SessionRestorationServiceFactory.
    
    This is part of a multi-CLs change:
      - https://crrev.com/c/6979449
      - https://crrev.com/c/6979586
     => https://crrev.com/c/6979667
      - https://crrev.com/c/6979626
      - https://crrev.com/c/6983123
      - https://crrev.com/c/6983202
      - https://crrev.com/c/7074893
      - https://crrev.com/c/6983024
      - https://crrev.com/c/6983144
      - https://crrev.com/c/6982943
      - https://crrev.com/c/6983445
      - https://crrev.com/c/6983504
      - https://crrev.com/c/6983010
      - https://crrev.com/c/6983050
      - https://crrev.com/c/6983423
      - https://crrev.com/c/6983287
      - https://crrev.com/c/7003327
      - https://crrev.com/c/7002783
      - https://crrev.com/c/7002962
      - https://crrev.com/c/7003743
      - https://crrev.com/c/7003123
      - https://crrev.com/c/7003225
      - https://crrev.com/c/7003227
      - https://crrev.com/c/7003087
      - https://crrev.com/c/7027213
    
    Bug: 40945317
    Change-Id: Idb6f223575ef288dad21b479184802cb39e14065
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6979667
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539406}

--- ios/chrome/browser/sessions/model/session_restoration_service_factory.mm
+++ ios/chrome/browser/sessions/model/session_restoration_service_factory.mm
@@ -9,11 +9,8 @@
 #import "base/types/cxx23_to_underlying.h"
 #import "components/pref_registry/pref_registry_syncable.h"
 #import "components/prefs/pref_service.h"
-#import "ios/chrome/browser/sessions/model/legacy_session_restoration_service.h"
 #import "ios/chrome/browser/sessions/model/session_constants.h"
 #import "ios/chrome/browser/sessions/model/session_restoration_service_impl.h"
-#import "ios/chrome/browser/sessions/model/session_service_ios.h"
-#import "ios/chrome/browser/sessions/model/web_session_state_cache_factory.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 
@@ -21,20 +18,6 @@ namespace {
 
 // Value taken from Desktop Chrome.
 constexpr base::TimeDelta kSaveDelay = base::Seconds(2.5);
-
-// Returns the value of the session storage format pref.
-SessionStorageFormat GetSessionStorageFormatPref(PrefService* prefs) {
-  switch (prefs->GetInteger(kSessionStorageFormatPref)) {
-    case base::to_underlying(SessionStorageFormat::kLegacy):
-      return SessionStorageFormat::kLegacy;
-
-    case base::to_underlying(SessionStorageFormat::kOptimized):
-      return SessionStorageFormat::kOptimized;
-
-    default:
-      return SessionStorageFormat::kUnknown;
-  }
-}
 
 }  // namespace
 
@@ -55,7 +38,6 @@ SessionRestorationServiceFactory::GetInstance() {
 SessionRestorationServiceFactory::SessionRestorationServiceFactory()
     : ProfileKeyedServiceFactoryIOS("SessionRestorationService",
                                     ProfileSelection::kOwnInstanceInIncognito) {
-  DependsOn(WebSessionStateCacheFactory::GetInstance());
 }
 
 SessionRestorationServiceFactory::~SessionRestorationServiceFactory() = default;
@@ -63,39 +45,12 @@ SessionRestorationServiceFactory::~SessionRestorationServiceFactory() = default;
 std::unique_ptr<KeyedService>
 SessionRestorationServiceFactory::BuildServiceInstanceFor(
     ProfileIOS* profile) const {
-  scoped_refptr<base::SequencedTaskRunner> task_runner =
+  return std::make_unique<SessionRestorationServiceImpl>(
+      kSaveDelay, IsPinnedTabsEnabled(), profile->GetStatePath(),
       base::ThreadPool::CreateSingleThreadTaskRunner(
           {base::MayBlock(), base::TaskPriority::USER_VISIBLE,
            base::TaskShutdownBehavior::BLOCK_SHUTDOWN},
-          base::SingleThreadTaskRunnerThreadMode::DEDICATED);
-
-  const base::FilePath storage_path = profile->GetStatePath();
-
-  SessionStorageFormat format =
-      GetSessionStorageFormatPref(profile->GetPrefs());
-
-  // During unit tests, it is the method MigrateSessionStorageFormat(...)
-  // will not be called before the service is created and the preference
-  // will have its default value of `SessionStorageFormat::kUnknown`. Use
-  // the feature flag to select which implementation to use.
-  if (format == SessionStorageFormat::kUnknown) {
-    format = SessionStorageFormat::kOptimized;
-  }
-
-  // If the optimised session restoration format is not enabled, create a
-  // LegacySessionRestorationService instance which wraps the legacy API.
-  if (format == SessionStorageFormat::kLegacy) {
-    SessionServiceIOS* session_service_ios =
-        [[SessionServiceIOS alloc] initWithSaveDelay:kSaveDelay
-                                          taskRunner:task_runner];
-
-    return std::make_unique<LegacySessionRestorationService>(
-        IsPinnedTabsEnabled(), storage_path, session_service_ios,
-        WebSessionStateCacheFactory::GetForProfile(profile));
-  }
-
-  return std::make_unique<SessionRestorationServiceImpl>(
-      kSaveDelay, IsPinnedTabsEnabled(), storage_path, task_runner);
+          base::SingleThreadTaskRunnerThreadMode::DEDICATED));
 }
 
 void SessionRestorationServiceFactory::RegisterProfilePrefs(
--- ios/chrome/browser/sessions/model/session_restoration_service_factory_unittest.mm
+++ ios/chrome/browser/sessions/model/session_restoration_service_factory_unittest.mm
@@ -4,32 +4,11 @@
 
 #import "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
 
-#import "base/types/cxx23_to_underlying.h"
-#import "components/prefs/pref_service.h"
-#import "ios/chrome/browser/sessions/model/session_constants.h"
 #import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
 #import "testing/platform_test.h"
-
-namespace {
-
-// Configures preferences storing session storage format and session storage
-// migration status.
-void WriteSessionStoragePref(PrefService* prefs,
-                             SessionStorageFormat storage_format,
-                             SessionStorageMigrationStatus migration_status,
-                             base::Time last_migration_attempt_time) {
-  prefs->SetInteger(kSessionStorageFormatPref,
-                    base::to_underlying(storage_format));
-  prefs->SetInteger(kSessionStorageMigrationStatusPref,
-                    base::to_underlying(migration_status));
-  prefs->SetTime(kSessionStorageMigrationStartedTimePref,
-                 last_migration_attempt_time);
-}
-
-}  // namespace
 
 class SessionRestorationServiceFactoryTest : public PlatformTest {
  public:
@@ -45,95 +24,20 @@ class SessionRestorationServiceFactoryTest : public PlatformTest {
   std::unique_ptr<TestProfileIOS> profile_;
 };
 
-// Tests that the factory correctly instantiate a new service when the storage
-// format is "unknown".
-TEST_F(SessionRestorationServiceFactoryTest, CreateInstance_Unknown) {
-  WriteSessionStoragePref(profile()->GetPrefs(), SessionStorageFormat::kUnknown,
-                          SessionStorageMigrationStatus::kSuccess,
-                          base::Time());
-
+// Tests that the factory correctly instantiate a new service for regular
+// profile.
+TEST_F(SessionRestorationServiceFactoryTest, CreateInstance) {
   EXPECT_TRUE(SessionRestorationServiceFactory::GetForProfile(profile()));
 }
 
 // Tests that the factory correctly instantiate a new service for off-the-record
-// BrowserState when the storage format is "unknown".
-TEST_F(SessionRestorationServiceFactoryTest, CreateOTRInstance_Unknown) {
-  WriteSessionStoragePref(profile()->GetPrefs(), SessionStorageFormat::kUnknown,
-                          SessionStorageMigrationStatus::kSuccess,
-                          base::Time());
-
+// profile.
+TEST_F(SessionRestorationServiceFactoryTest, CreateOTRInstance) {
   EXPECT_TRUE(SessionRestorationServiceFactory::GetForProfile(otr_profile()));
 }
 
-// Tests that regular and off-the-record BrowserState uses distinct instances
-// when the storage format is "unknown".
-TEST_F(SessionRestorationServiceFactoryTest, InstancesAreDistinct_Unknown) {
-  WriteSessionStoragePref(profile()->GetPrefs(), SessionStorageFormat::kUnknown,
-                          SessionStorageMigrationStatus::kSuccess,
-                          base::Time());
-
-  EXPECT_NE(SessionRestorationServiceFactory::GetForProfile(profile()),
-            SessionRestorationServiceFactory::GetForProfile(otr_profile()));
-}
-
-// Tests that the factory correctly instantiate a new service when using
-// the "legacy" storage.
-TEST_F(SessionRestorationServiceFactoryTest, CreateInstance_Legacy) {
-  WriteSessionStoragePref(profile()->GetPrefs(), SessionStorageFormat::kLegacy,
-                          SessionStorageMigrationStatus::kSuccess,
-                          base::Time());
-
-  EXPECT_TRUE(SessionRestorationServiceFactory::GetForProfile(profile()));
-}
-
-// Tests that the factory correctly instantiate a new service for off-the-record
-// BrowserState when using the "legacy" storage.
-TEST_F(SessionRestorationServiceFactoryTest, CreateOTRInstance_Legacy) {
-  WriteSessionStoragePref(profile()->GetPrefs(), SessionStorageFormat::kLegacy,
-                          SessionStorageMigrationStatus::kSuccess,
-                          base::Time());
-
-  EXPECT_TRUE(SessionRestorationServiceFactory::GetForProfile(otr_profile()));
-}
-
-// Tests that regular and off-the-record BrowserState uses distinct instances
-// when using the "legacy" storage.
-TEST_F(SessionRestorationServiceFactoryTest, InstancesAreDistinct_Legacy) {
-  WriteSessionStoragePref(profile()->GetPrefs(), SessionStorageFormat::kLegacy,
-                          SessionStorageMigrationStatus::kSuccess,
-                          base::Time());
-
-  EXPECT_NE(SessionRestorationServiceFactory::GetForProfile(profile()),
-            SessionRestorationServiceFactory::GetForProfile(otr_profile()));
-}
-
-// Tests that the factory correctly instantiate a new service when using
-// the "optimized" storage.
-TEST_F(SessionRestorationServiceFactoryTest, CreateInstance_Optimized) {
-  WriteSessionStoragePref(
-      profile()->GetPrefs(), SessionStorageFormat::kOptimized,
-      SessionStorageMigrationStatus::kSuccess, base::Time());
-
-  EXPECT_TRUE(SessionRestorationServiceFactory::GetForProfile(profile()));
-}
-
-// Tests that the factory correctly instantiate a new service for off-the-record
-// BrowserState when using the "optimized" storage.
-TEST_F(SessionRestorationServiceFactoryTest, CreateOTRInstance_Optimized) {
-  WriteSessionStoragePref(
-      profile()->GetPrefs(), SessionStorageFormat::kOptimized,
-      SessionStorageMigrationStatus::kSuccess, base::Time());
-
-  EXPECT_TRUE(SessionRestorationServiceFactory::GetForProfile(otr_profile()));
-}
-
-// Tests that regular and off-the-record BrowserState uses distinct instances
-// when using the "optimized" storage.
-TEST_F(SessionRestorationServiceFactoryTest, InstancesAreDistinct_Optimized) {
-  WriteSessionStoragePref(
-      profile()->GetPrefs(), SessionStorageFormat::kOptimized,
-      SessionStorageMigrationStatus::kSuccess, base::Time());
-
+// Tests that regular and off-the-record profiles uses distinct instances.
+TEST_F(SessionRestorationServiceFactoryTest, InstancesAreDistinct) {
   EXPECT_NE(SessionRestorationServiceFactory::GetForProfile(profile()),
             SessionRestorationServiceFactory::GetForProfile(otr_profile()));
 }
