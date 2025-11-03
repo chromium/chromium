1704a9a26e1a24bdc6c38084dd4150ff360c2072
viplavkadam <viplavkadam@google.com>
2025-11-03 09:37:23 -0800

    Reland "[BNPL][Clank] Add a toggle to enable or disable Buy Now Pay Later"
    This is a reland of commit ab6034921764fab634ff6d25a11b07299d225013
    
    This CL was reverted because it was based on a previous CL that
    also got reverted:
    https://chromium-review.googlesource.com/c/chromium/src/+/7088349
    
    The fix for the original revert is handled in the reland of that
    previous CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/7098836
    
    Therefore, no additional fix is needed as part of this reland.
    
    Original change's description:
    > [BNPL][Clank] Add a toggle to enable or disable Buy Now Pay Later
    >
    > The BNPL settings page shows the 'Pay over time' toggle. The state of
    > this toggle is observed and linked to the kAutofillBnplEnabled
    > preference, ensuring consistent behavior across both desktop and
    > Clank. When the toggle is disabled, the BNPL option is not shown
    > on the bottom sheet for the user to select; when the toggle is
    > enabled, it is shown.
    >
    > Mock:
    > https://screenshot.googleplex.com/65h65mwvdupb3Uw
    > Implementation:
    > https://screenshot.googleplex.com/4yDuAgxHo3d5HCs
    > Video:
    > http://screencast/cast/NTg4MDI5MzMxODcyMTUzNnw2ZmM5ZmU0NC1jYQ
    v
    > Design: go/autofill-clank-bnpl
    >
    > Bug: b:438786292, 430575808
    > Change-Id: I488c5918e5d00a45d61a51b1125b37b84fe293fc
    > Reviewed-on: https://chromium-review.google
    > Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    > Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    > Reviewed-by: Vinny Persky <vinnypersky@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1536302}
    
    Bug: b:438786292, 430575808
    Change-Id: Ie5f688992e8f1f7d44ec8b7b9ff271f514ace5ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103527
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Viplav Kadam <viplavkadam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1539415}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillBuyNowPayLaterFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillBuyNowPayLaterFragment.java
@@ -7,6 +7,8 @@ package org.chromium.chrome.browser.autofill.settings;
 import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
 import org.chromium.base.supplier.ObservableSupplier;
@@ -14,14 +16,23 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.autofill.PersonalDataManager;
+import org.chromium.chrome.browser.autofill.PersonalDataManagerFactory;
 import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
+import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.SettingsFragment;
 
 /** Preferences fragment to allow users to manage Buy Now Pay Later application settings. */
 @NullMarked
-public class AutofillBuyNowPayLaterFragment extends ChromeBaseSettingsFragment {
+public class AutofillBuyNowPayLaterFragment extends ChromeBaseSettingsFragment
+        implements PersonalDataManager.PersonalDataManagerObserver,
+                Preference.OnPreferenceChangeListener {
+    @VisibleForTesting
+    static final String PREF_KEY_ENABLE_BUY_NOW_PAY_LATER = "enable_buy_now_pay_later";
 
     private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
+
+    private PersonalDataManager mPersonalDataManager;
 
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
@@ -30,6 +41,54 @@ public class AutofillBuyNowPayLaterFragment extends ChromeBaseSettingsFragment {
         // Create blank preference screen.
         PreferenceScreen screen = getPreferenceManager().createPreferenceScreen(getStyledContext());
         setPreferenceScreen(screen);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Rebuild the preference list in case any of the underlying data has been updated.
+        rebuildPage();
+    }
+
+    private void rebuildPage() {
+        getPreferenceScreen().removeAll();
+        getPreferenceScreen().setOrderingAsAdded(true);
+        createBuyNowPayLaterSwitch();
+    }
+
+    private void createBuyNowPayLaterSwitch() {
+        ChromeSwitchPreference buyNowPayLaterSwitch =
+                new ChromeSwitchPreference(getStyledContext());
+        buyNowPayLaterSwitch.setTitle(R.string.autofill_bnpl_settings_label);
+        buyNowPayLaterSwitch.setSummary(R.string.autofill_bnpl_settings_toggle_sublabel);
+        buyNowPayLaterSwitch.setKey(PREF_KEY_ENABLE_BUY_NOW_PAY_LATER);
+        buyNowPayLaterSwitch.setChecked(mPersonalDataManager.isBuyNowPayLaterEnabled());
+        buyNowPayLaterSwitch.setOnPreferenceChangeListener(this);
+        getPreferenceScreen().addPreference(buyNowPayLaterSwitch);
+    }
+
+    @Override
+    public void onPersonalDataChanged() {
+        rebuildPage();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        mPersonalDataManager.setBuyNowPayLater((boolean) newValue);
+        return true;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mPersonalDataManager = PersonalDataManagerFactory.getForProfile(getProfile());
+        mPersonalDataManager.registerDataObserver(this);
+    }
+
+    @Override
+    public void onDestroyView() {
+        mPersonalDataManager.unregisterDataObserver(this);
+        super.onDestroyView();
     }
 
     @Override
--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/AutofillBuyNowPayLaterFragmentTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/settings/AutofillBuyNowPayLaterFragmentTest.java
@@ -5,40 +5,52 @@
 package org.chromium.chrome.browser.autofill.settings;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentFactory;
 import androidx.fragment.app.testing.FragmentScenario;
 
+import org.jspecify.annotations.NonNull;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.autofill.PersonalDataManager;
+import org.chromium.chrome.browser.autofill.PersonalDataManagerFactory;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 
 /** JUnit tests of the class {@link AutofillBuyNowPayLaterFragment} */
 @RunWith(BaseRobolectricTestRunner.class)
 public class AutofillBuyNowPayLaterFragmentTest {
-    private FragmentScenario<AutofillBuyNowPayLaterFragment> mScenario;
+    @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
+    private FragmentScenario<AutofillBuyNowPayLaterFragment> mScenario;
     private AutofillBuyNowPayLaterFragment mAutofillBuyNowPayLaterFragment;
+    private ChromeSwitchPreference mBnplToggle;
+
+    @Mock private PersonalDataManager mPersonalDataManager;
+    @Mock private Profile mProfile;
 
     @Before
     public void setUp() {
-        mScenario =
-                FragmentScenario.launchInContainer(
-                        AutofillBuyNowPayLaterFragment.class,
-                        Bundle.EMPTY,
-                        R.style.Theme_Chromium_Settings);
-        mScenario.onFragment(
-                fragment -> {
-                    assertNotNull(fragment.getPreferenceScreen());
-                    mAutofillBuyNowPayLaterFragment = (AutofillBuyNowPayLaterFragment) fragment;
-                });
+        PersonalDataManagerFactory.setInstanceForTesting(mPersonalDataManager);
     }
 
     @After
@@ -48,12 +60,105 @@ public class AutofillBuyNowPayLaterFragmentTest {
         }
     }
 
+    private void launchAutofillBuyNowPayLaterFragment() {
+        mScenario =
+                FragmentScenario.launchInContainer(
+                        AutofillBuyNowPayLaterFragment.class,
+                        Bundle.EMPTY,
+                        R.style.Theme_Chromium_Settings,
+                        new FragmentFactory() {
+                            @NonNull
+                            @Override
+                            public Fragment instantiate(
+                                    @NonNull ClassLoader classLoader, @NonNull String className) {
+                                Fragment fragment = super.instantiate(classLoader, className);
+                                if (fragment instanceof AutofillBuyNowPayLaterFragment) {
+                                    ((AutofillBuyNowPayLaterFragment) fragment)
+                                            .setProfile(mProfile);
+                                }
+                                return fragment;
+                            }
+                        });
+        mScenario.onFragment(
+                fragment -> {
+                    mAutofillBuyNowPayLaterFragment = (AutofillBuyNowPayLaterFragment) fragment;
+                    mBnplToggle =
+                            (ChromeSwitchPreference)
+                                    fragment.findPreference(
+                                            AutofillBuyNowPayLaterFragment
+                                                    .PREF_KEY_ENABLE_BUY_NOW_PAY_LATER);
+                });
+    }
+
     // Test to verify that the Preference screen is displayed and its title is visible as expected.
     @Test
     public void testBuyNowPayLaterPreferenceScreen_shownWithTitle() {
+        launchAutofillBuyNowPayLaterFragment();
+
+        assertNotNull(mAutofillBuyNowPayLaterFragment.getPreferenceScreen());
         assertEquals(
                 ContextUtils.getApplicationContext()
                         .getString(R.string.autofill_bnpl_settings_label),
                 mAutofillBuyNowPayLaterFragment.getPageTitle().get());
+    }
+
+    // Test to verify that the BNPL toggle is displayed on the Preference screen and its label and
+    // sublabel are visible as expected.
+    @Test
+    public void testBuyNowPayLaterPreferenceScreen_ToggleIsShownWithLabelAndSubLabel() {
+        launchAutofillBuyNowPayLaterFragment();
+
+        assertNotNull(mBnplToggle);
+        assertEquals(
+                ContextUtils.getApplicationContext()
+                        .getString(R.string.autofill_bnpl_settings_label),
+                mBnplToggle.getTitle());
+        assertEquals(
+                ContextUtils.getApplicationContext()
+                        .getString(R.string.autofill_bnpl_settings_toggle_sublabel),
+                mBnplToggle.getSummary());
+    }
+
+    // Test to verify that when the `isBuyNowPayLaterEnabled` returns true, the BNPL toggle
+    // is in the checked state.
+    @Test
+    public void testBuyNowPayLaterPreferenceScreen_WhenBuyNowPayLaterIsEnabled_ToggleIsChecked() {
+        when(mPersonalDataManager.isBuyNowPayLaterEnabled()).thenReturn(true);
+
+        launchAutofillBuyNowPayLaterFragment();
+
+        assertTrue(mBnplToggle.isChecked());
+    }
+
+    // Test to verify that when the `isBuyNowPayLaterEnabled` returns false, the BNPL toggle
+    // is in the unchecked state.
+    @Test
+    public void
+            testBuyNowPayLaterPreferenceScreen_WhenBuyNowPayLaterIsDisabled_ToggleIsUnchecked() {
+        when(mPersonalDataManager.isBuyNowPayLaterEnabled()).thenReturn(false);
+
+        launchAutofillBuyNowPayLaterFragment();
+
+        assertFalse(mBnplToggle.isChecked());
+    }
+
+    // Test to verify that when the BNPL toggle is clicked, the `setBuyNowPayLater` method is
+    // called to set the BNPL status accordingly.
+    @Test
+    public void
+            testBuyNowPayLaterPreferenceScreen_WhenToggleIsClicked_SetsEnableStatusAccordingly() {
+        when(mPersonalDataManager.isBuyNowPayLaterEnabled()).thenReturn(true);
+        launchAutofillBuyNowPayLaterFragment();
+        assertTrue(mBnplToggle.isChecked());
+
+        mBnplToggle.performClick();
+
+        assertFalse(mBnplToggle.isChecked());
+        verify(mPersonalDataManager).setBuyNowPayLater(eq(false));
+
+        mBnplToggle.performClick();
+
+        assertTrue(mBnplToggle.isChecked());
+        verify(mPersonalDataManager).setBuyNowPayLater(eq(true));
     }
 }
