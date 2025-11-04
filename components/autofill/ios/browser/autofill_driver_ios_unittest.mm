9c15076d40423a55edd7f900d252b9467dc832b9
Jihad Hanna <jihadghanna@google.com>
2025-11-04 07:55:12 -0800

    Improve AutofillDriverIOSTest
    This CL modifies the FormFound test to also check for correct routing
    across drivers, which is similar to the equivalent ContentAutofillDriver
    test [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/autofill/content/browser/content_autofill_driver_unittest.cc;l=855-880;drc=621bf26829352a21241127f6c684301dac842088
    
    Bug: 455870070
    Change-Id: I56c400bbe383b98fba9a8605eabf2d7c8935ca81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7105386
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540053}

--- components/autofill/ios/browser/autofill_driver_ios_unittest.mm
+++ components/autofill/ios/browser/autofill_driver_ios_unittest.mm
@@ -6,25 +6,25 @@
 
 #import <memory>
 #import <optional>
-#import <string_view>
 
 #import "base/test/mock_callback.h"
 #import "base/test/test_future.h"
+#import "components/autofill/core/browser/test_utils/autofill_form_test_utils.h"
 #import "components/autofill/core/browser/test_utils/autofill_test_utils.h"
 #import "components/autofill/core/common/autofill_test_utils.h"
 #import "components/autofill/core/common/form_data.h"
+#import "components/autofill/core/common/form_field_data.h"
 #import "components/autofill/core/common/unique_ids.h"
 #import "components/autofill/ios/browser/autofill_driver_ios_factory.h"
 #import "components/autofill/ios/browser/autofill_java_script_feature.h"
 #import "components/autofill/ios/browser/test_autofill_client_ios.h"
+#import "components/autofill/ios/form_util/child_frame_registrar.h"
 #import "ios/web/public/test/fakes/fake_web_frame.h"
 #import "ios/web/public/test/fakes/fake_web_frames_manager.h"
 #import "ios/web/public/test/fakes/fake_web_state.h"
-#import "ios/web/public/test/web_task_environment.h"
 #import "ios/web/public/test/web_test.h"
 #import "testing/gmock/include/gmock/gmock.h"
 #import "testing/gtest/include/gtest/gtest.h"
-#import "testing/platform_test.h"
 
 // AutofillDriverIosBridge used for testing. Provides a simple implementation of
 // the methods that are used during testing, e.g. call the completion block upon
@@ -99,91 +99,152 @@ class AutofillDriverIOSTest : public web::WebTest {
     OverrideJavaScriptFeatures(
         {autofill::AutofillJavaScriptFeature::GetInstance()});
 
+    GURL url("https://example.com");
+    web_state_.SetCurrentURL(url);
     web_state_.SetBrowserState(GetBrowserState());
     web_state_.SetContentIsHTML(true);
 
-    auto frames_manager = std::make_unique<web::FakeWebFramesManager>();
-    web_state_.SetWebFramesManager(
-        AutofillJavaScriptFeature::GetInstance()->GetSupportedContentWorld(),
-        std::move(frames_manager));
+    // Define frames managers for both content worlds so that the
+    // `ChildFrameRegistrar` below can be created.
+    for (auto content_world : {web::ContentWorld::kIsolatedWorld,
+                               web::ContentWorld::kPageContentWorld}) {
+      auto frames_manager = std::make_unique<web::FakeWebFramesManager>();
+      web_state_.SetWebFramesManager(content_world, std::move(frames_manager));
+    }
+
     web_frames_manager_ = static_cast<web::FakeWebFramesManager*>(
         web_state_.GetWebFramesManager(AutofillJavaScriptFeature::GetInstance()
                                            ->GetSupportedContentWorld()));
 
-    GURL url("https://example.com");
-    web_state_.SetCurrentURL(url);
+    web::WebFrame* main_frame = CreateFrame(url, /*main_frame=*/true);
+    web::WebFrame* iframe = CreateFrame(url, /*main_frame=*/false);
 
-    // Create the main frame.
-    auto main_frame = web::FakeWebFrame::CreateMainWebFrame(url);
-    main_frame->set_browser_state(GetBrowserState());
-    web::WebFrame* main_frame_ptr = main_frame.get();
-    web_frames_manager_->AddWebFrame(std::move(main_frame));
-
+    // Create the main Autofill classes.
     test_bridge_ = [[FakeAutofillDriverIOSBridge alloc] init];
-
     autofill_client_ =
         std::make_unique<TestAutofillClientIOS>(&web_state_, test_bridge_);
     main_frame_driver_ =
-        AutofillDriverIOS::FromWebStateAndWebFrame(&web_state_, main_frame_ptr);
+        AutofillDriverIOS::FromWebStateAndWebFrame(&web_state_, main_frame);
+    iframe_driver_ =
+        AutofillDriverIOS::FromWebStateAndWebFrame(&web_state_, iframe);
+
     ASSERT_TRUE(main_frame_driver_ && main_frame_driver_->GetFrameToken());
+    ASSERT_TRUE(iframe_driver_ && iframe_driver_->GetFrameToken());
+    ASSERT_NE(iframe_driver_->GetFrameToken(),
+              main_frame_driver_->GetFrameToken());
+
+    // Register the remote-local frame token mapping for the iframe driver so
+    // that the main frame driver can accurately set itself as a parent of the
+    // iframe driver.
+    registrar()->RegisterMapping(GetRemoteFrameToken(iframe_driver_),
+                                 iframe_driver_->GetFrameToken());
   }
 
   AutofillDriverIOS* main_frame_driver() { return main_frame_driver_; }
+  AutofillDriverIOS* iframe_driver() { return iframe_driver_; }
   FakeAutofillDriverIOSBridge* bridge() { return test_bridge_; }
 
-  FormGlobalId MakeFormId() {
-    return {main_frame_driver()->GetFrameToken(), test::MakeFormRendererId()};
-  }
-
-  FormData MakeForm() {
-    FormGlobalId form_id = MakeFormId();
+  FormData MakeForm(bool main_frame) {
+    FormGlobalId form_id{main_frame ? main_frame_driver()->GetFrameToken()
+                                    : iframe_driver()->GetFrameToken(),
+                         test::MakeFormRendererId()};
     FormData form;
     form.set_renderer_id(form_id.renderer_id);
     form.set_host_frame(form_id.frame_token);
+
+    // Add a field to the form to make it non-trivial.
+    FormFieldData field = test::GetFormFieldData({});
+    field.set_host_form_id(form.renderer_id());
+    field.set_host_frame(form.host_frame());
+    field.set_value(main_frame ? u"Main frame field" : u"Iframe field");
+    form.set_fields({std::move(field)});
+
+    if (main_frame) {
+      std::vector<FrameTokenWithPredecessor> child_frames(1);
+      child_frames.back().token = GetRemoteFrameToken(iframe_driver());
+      form.set_child_frames(std::move(child_frames));
+    }
     return form;
   }
 
  private:
+  web::WebFrame* CreateFrame(GURL url, bool main_frame) {
+    auto frame = main_frame ? web::FakeWebFrame::CreateMainWebFrame(url)
+                            : web::FakeWebFrame::CreateChildWebFrame(url);
+    frame->set_browser_state(GetBrowserState());
+    web::WebFrame* frame_ptr = frame.get();
+    web_frames_manager_->AddWebFrame(std::move(frame));
+    return frame_ptr;
+  }
+
+  autofill::ChildFrameRegistrar* registrar() {
+    return autofill::ChildFrameRegistrar::GetOrCreateForWebState(&web_state_);
+  }
+
+  RemoteFrameToken GetRemoteFrameToken(AutofillDriver* driver) {
+    return RemoteFrameToken(driver->GetFrameToken().value());
+  }
+
   test::AutofillUnitTestEnvironment autofill_test_environment_;
   web::FakeWebState web_state_;
   raw_ptr<web::FakeWebFramesManager> web_frames_manager_ = nullptr;
   FakeAutofillDriverIOSBridge* test_bridge_ = nullptr;
   std::unique_ptr<TestAutofillClientIOS> autofill_client_;
   raw_ptr<AutofillDriverIOS> main_frame_driver_ = nullptr;
+  raw_ptr<AutofillDriverIOS> iframe_driver_ = nullptr;
 };
 
 // Test when ExtractForm() is called and where the form is found.
 TEST_F(AutofillDriverIOSTest, ExtractForm_FormFound) {
-  FormData form_to_find = MakeForm();
-  FormData form_not_to_find = MakeForm();
-  std::vector<FormData> bridge_result = {form_to_find, form_not_to_find};
+  FormData iframe_form = MakeForm(/*main_frame=*/false);
+  FormData main_frame_form = MakeForm(/*main_frame=*/true);
+
+  // Notify the drivers of seeing these forms so that the router can register
+  // them along with their frame.
+  main_frame_driver()->FormsSeen({main_frame_form}, /*removed_forms=*/{});
+  iframe_driver()->FormsSeen({iframe_form}, /*removed_forms=*/{});
+  ASSERT_EQ(iframe_driver()->GetParent(), main_frame_driver());
+
+  // Update the value of the fields in both forms to check the result of the
+  // extraction later.
+  auto update_form = [](FormData& form) {
+    std::vector<FormFieldData> fields = form.ExtractFields();
+    fields.back().set_value(fields.back().value() + u" extracted");
+    form.set_fields(std::move(fields));
+  };
+  update_form(main_frame_form);
+  update_form(iframe_form);
 
   // Setup the forms that the bridge should return upon calling
   // fetchFormsFiltered(). Those should be the two forms created above.
+  std::vector<FormData> bridge_result{iframe_form, main_frame_form};
   [bridge() setForms:(bridge_result)];
 
-  // Notify the driver of seeing these forms so that the router can register
-  // them along with their frame.
-  main_frame_driver()->FormsSeen({form_to_find, form_not_to_find},
-                                 /*removed_forms=*/{});
-
-  // Expect that `final_callback` will be called with a form with the correct
-  // renderer id.
   base::MockCallback<
       base::OnceCallback<void(AutofillDriver*, const std::optional<FormData>&)>>
       final_callback;
-  EXPECT_CALL(final_callback,
-              Run(main_frame_driver(),
-                  Optional(Property(&FormData::renderer_id,
-                                    form_to_find.global_id().renderer_id))));
-  main_frame_driver()->ExtractForm(form_to_find.global_id(),
+  // The callback must be called with the result of the extraction operation,
+  // this should be the browser form containing the iframe form, with the
+  // updated version of the iframe form.
+  EXPECT_CALL(
+      final_callback,
+      Run(main_frame_driver(),
+          Optional(
+              AllOf(Property(&FormData::global_id, main_frame_form.global_id()),
+                    Property(&FormData::fields,
+                             ElementsAre(Property(&FormFieldData::value,
+                                                  u"Iframe field extracted"),
+                                         Property(&FormFieldData::value,
+                                                  u"Main frame field")))))));
+  main_frame_driver()->ExtractForm(iframe_form.global_id(),
                                    final_callback.Get());
 }
 
 // Test the case where the bridge returns forms, but not the one we want.
 TEST_F(AutofillDriverIOSTest, ExtractForm_FormNotFound) {
-  FormGlobalId form_id = MakeFormId();
-  FormData form_not_to_find = MakeForm();
+  FormGlobalId form_id{main_frame_driver()->GetFrameToken(),
+                       test::MakeFormRendererId()};
+  FormData form_not_to_find = MakeForm(/*main_frame=*/true);
 
   std::vector<FormData> bridge_result = {form_not_to_find};
 
