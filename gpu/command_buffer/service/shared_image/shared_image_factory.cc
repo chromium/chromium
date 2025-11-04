3b96082d321bf193c7a248936e282cacd2d8fa70
Colin Blundell <blundell@chromium.org>
2025-11-04 01:08:24 -0800

    [//gpu] Remove gpu_memory_buffer_support.*
    Inline its one method into its callsite in shared_image_factory.cc.
    
    Bug: 404905709
    Change-Id: Ia28a829f7d900c6dbd46a399310fd420f234016e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112463
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539846}

--- gpu/command_buffer/service/shared_image/shared_image_factory.cc
+++ gpu/command_buffer/service/shared_image/shared_image_factory.cc
@@ -37,7 +37,6 @@
 #include "gpu/command_buffer/service/shared_image/wrapped_sk_image_backing_factory.h"
 #include "gpu/config/gpu_finch_features.h"
 #include "gpu/config/gpu_preferences.h"
-#include "gpu/ipc/common/gpu_memory_buffer_support.h"
 #include "ui/base/ozone_buildflags.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/gfx/gpu_memory_buffer_handle.h"
@@ -406,8 +405,84 @@ bool SharedImageFactory::IsNativeBufferSupported(
   }
 #endif  // BUILDFLAG(IS_OZONE_X11)
 
-  return gpu::GpuMemoryBufferSupport::
-      IsNativeGpuMemoryBufferConfigurationSupported(format, usage);
+#if BUILDFLAG(IS_APPLE)
+  switch (usage) {
+    case gfx::BufferUsage::GPU_READ:
+    case gfx::BufferUsage::SCANOUT:
+    case gfx::BufferUsage::SCANOUT_CPU_READ_WRITE:
+    case gfx::BufferUsage::GPU_READ_CPU_READ_WRITE:
+    case gfx::BufferUsage::SCANOUT_FRONT_RENDERING:
+    case gfx::BufferUsage::SCANOUT_VEA_CPU_READ:
+      return format == viz::SinglePlaneFormat::kBGRA_8888 ||
+             format == viz::SinglePlaneFormat::kRGBA_8888 ||
+             format == viz::SinglePlaneFormat::kBGRX_8888 ||
+             format == viz::SinglePlaneFormat::kRGBX_8888 ||
+             format == viz::SinglePlaneFormat::kR_8 ||
+             format == viz::SinglePlaneFormat::kRG_88 ||
+             format == viz::SinglePlaneFormat::kR_16 ||
+             format == viz::SinglePlaneFormat::kRG_1616 ||
+             format == viz::SinglePlaneFormat::kRGBA_F16 ||
+             format == viz::SinglePlaneFormat::kBGRA_1010102 ||
+             format == viz::MultiPlaneFormat::kNV12 ||
+             format == viz::MultiPlaneFormat::kNV12A ||
+             format == viz::MultiPlaneFormat::kP010;
+    case gfx::BufferUsage::SCANOUT_VDA_WRITE:
+    case gfx::BufferUsage::PROTECTED_SCANOUT:
+    case gfx::BufferUsage::PROTECTED_SCANOUT_VDA_WRITE:
+    case gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE:
+    case gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE:
+    case gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE:
+      return false;
+  }
+  NOTREACHED();
+#elif BUILDFLAG(IS_ANDROID)
+  switch (usage) {
+    case gfx::BufferUsage::GPU_READ:
+    case gfx::BufferUsage::SCANOUT:
+      return format == viz::SinglePlaneFormat::kRGBA_8888 ||
+             format == viz::SinglePlaneFormat::kRGBX_8888 ||
+             format == viz::SinglePlaneFormat::kBGR_565;
+    case gfx::BufferUsage::SCANOUT_CPU_READ_WRITE:
+    case gfx::BufferUsage::GPU_READ_CPU_READ_WRITE:
+    case gfx::BufferUsage::SCANOUT_VDA_WRITE:
+    case gfx::BufferUsage::PROTECTED_SCANOUT:
+    case gfx::BufferUsage::PROTECTED_SCANOUT_VDA_WRITE:
+    case gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE:
+    case gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE:
+    case gfx::BufferUsage::SCANOUT_VEA_CPU_READ:
+    case gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE:
+    case gfx::BufferUsage::SCANOUT_FRONT_RENDERING:
+      return false;
+  }
+  NOTREACHED();
+#elif BUILDFLAG(IS_OZONE)
+  auto buffer_format = viz::SharedImageFormatToBufferFormat(format);
+  return ui::OzonePlatform::GetInstance()->IsNativePixmapConfigSupported(
+      buffer_format, usage);
+#elif BUILDFLAG(IS_WIN)
+  switch (usage) {
+    case gfx::BufferUsage::GPU_READ:
+    case gfx::BufferUsage::SCANOUT:
+      return format == viz::SinglePlaneFormat::kRGBA_8888 ||
+             format == viz::SinglePlaneFormat::kRGBX_8888 ||
+             format == viz::SinglePlaneFormat::kBGRA_8888 ||
+             format == viz::SinglePlaneFormat::kBGRX_8888;
+    case gfx::BufferUsage::SCANOUT_CPU_READ_WRITE:
+    case gfx::BufferUsage::GPU_READ_CPU_READ_WRITE:
+    case gfx::BufferUsage::SCANOUT_VDA_WRITE:
+    case gfx::BufferUsage::PROTECTED_SCANOUT:
+    case gfx::BufferUsage::PROTECTED_SCANOUT_VDA_WRITE:
+    case gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE:
+    case gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE:
+    case gfx::BufferUsage::SCANOUT_VEA_CPU_READ:
+    case gfx::BufferUsage::VEA_READ_CAMERA_AND_CPU_READ_WRITE:
+    case gfx::BufferUsage::SCANOUT_FRONT_RENDERING:
+      return false;
+  }
+  NOTREACHED();
+#else
+  return false;
+#endif
 }
 
 bool SharedImageFactory::CreateSharedImage(const Mailbox& mailbox,
