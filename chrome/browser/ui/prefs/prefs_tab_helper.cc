b38f988adb1957f1fc4823c9fb78da679137f14e
Yeunjoo Choi <ychoi@igalia.com>
2025-11-04 03:28:26 -0800

    Android: Fix crash with comma-seperated fonts
    When language is changed to Korean or Japaneses on Android, the DCHECK
    fails due to comma-sepereated fonts.
    
    [FATAL:chrome/browser/ui/prefs/prefs_tab_helper.cc:437] DCHECK failed: !value.starts_with(','). This platform doesn't support default font lists. webkit.webprefs.fonts.standard.Zyyy=,Noto Sans KR,Noto Sans CJK KR
    
    This CL supports comma-sepereated fonts on Android.
    
    Bug: 456307057
    Change-Id: If2fddb9f2008615ec561298af2af1e4980bf2af2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7104581
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yeunjoo Choi <ychoi@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1539906}

--- chrome/browser/ui/prefs/prefs_tab_helper.cc
+++ chrome/browser/ui/prefs/prefs_tab_helper.cc
@@ -64,7 +64,7 @@
 #endif
 
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS)
+    BUILDFLAG(IS_ANDROID) || BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS)
 // If a font name in prefs default values starts with a comma, consider it's a
 // comma-separated font list and resolve it to the first available font.
 #define PREFS_FONT_LIST 1
