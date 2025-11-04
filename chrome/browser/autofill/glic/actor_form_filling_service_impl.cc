8e6dc3e2f7238e5bd8a82c2de698597340371032
Jan Keitel <jkeitel@google.com>
2025-11-03 23:42:13 -0800

    Generate address suggestions in ActorFormFillingService
    This CL starts implementing suggestion generation for actor integration.
    As described in code comments, the current implementation only handles
    the simplest cases well - as soon as an "actor form" consists of
    multiple sections, suggestion generation is, at best, incomplete.
    
    Follow-up CLs will add credit card suggestion generation and filling.
    
    Bug: 455788947
    Change-Id: I8d0032124aa78664a65c48ab36fb076e35c171fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7100805
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539805}

--- chrome/browser/autofill/glic/actor_form_filling_service_impl.cc
+++ chrome/browser/autofill/glic/actor_form_filling_service_impl.cc
@@ -4,12 +4,142 @@
 
 #include "chrome/browser/autofill/glic/actor_form_filling_service_impl.h"
 
+#include <utility>
+#include <vector>
+
+#include "base/containers/flat_map.h"
+#include "base/containers/to_vector.h"
 #include "base/functional/callback.h"
+#include "base/strings/utf_string_conversions.h"
 #include "base/types/expected.h"
+#include "components/autofill/content/browser/content_autofill_client.h"
+#include "components/autofill/core/browser/foundations/autofill_manager.h"
 #include "components/autofill/core/browser/integrators/glic/actor_form_filling_types.h"
+#include "components/autofill/core/browser/suggestions/addresses/address_suggestion_generator.h"
+#include "components/autofill/core/browser/suggestions/suggestion.h"
+#include "components/autofill/core/browser/ui/autofill_external_delegate.h"
 #include "components/tabs/public/tab_interface.h"
 
 namespace autofill {
+
+namespace {
+
+struct ActorSuggestionWithFillData {
+  ActorSuggestion suggestion;
+  ActorFormFillingServiceImpl::FillData filling_payload;
+};
+
+// Attempts to generate an `ActorSuggestion` and the data needed for filling
+// a suggestion. Returns `std::nullopt` if the suggestion does not contain an
+// address payload.
+std::optional<ActorSuggestionWithFillData> GetActorAddressSuggestion(
+    const AddressDataManager& adm,
+    base::span<const FieldGlobalId> fields,
+    const Suggestion& suggestion) {
+  const Suggestion::AutofillProfilePayload* const profile_payload =
+      std::get_if<Suggestion::AutofillProfilePayload>(&suggestion.payload);
+  if (!profile_payload) {
+    return std::nullopt;
+  }
+
+  ActorSuggestion actor_suggestion;
+  // TODO(crbug.com/455788947): Consider making `ActorSuggestion` use UTF16
+  // strings.
+  actor_suggestion.title = base::UTF16ToUTF8(suggestion.main_text.value);
+  actor_suggestion.details =
+      (!suggestion.labels.empty() && !suggestion.labels[0].empty())
+          ? base::UTF16ToUTF8(suggestion.labels[0][0].value)
+          : "";
+  std::optional<AutofillProfile> profile =
+      GetProfileFromPayload(adm, *profile_payload);
+  if (!profile) {
+    return std::nullopt;
+  }
+  ActorFormFillingServiceImpl::FillData fill_data = {base::ToVector(fields),
+                                                     std::move(*profile)};
+  return ActorSuggestionWithFillData{std::move(actor_suggestion),
+                                     std::move(fill_data)};
+}
+
+// Generates address suggestions and the accompanying data that is needed for
+// filling.
+// Note that this is a preliminary implementation that is deficient in a number
+// of ways:
+// - The first entry in `fields` is used as the trigger field. This means that
+//   we only return suggestions that have a non-empty value for this field.
+// - Because only the first entry of `fields` is passed in, we may "deduplicate"
+//   suggestions that would fill the same values in the form section represented
+//   by the first field but that would fill different values in other sections.
+//
+// TODO(crbug.com/455788947): Improve suggestion generation.
+std::vector<ActorSuggestionWithFillData> GetAddressSuggestions(
+    base::span<const FieldGlobalId> fields,
+    const AutofillManager& autofill_manager) {
+  if (fields.empty()) {
+    return {};
+  }
+
+  // For now, we simply take the first field.
+  const FormStructure* const form_structure =
+      autofill_manager.FindCachedFormById(fields[0]);
+  if (!form_structure) {
+    return {};
+  }
+  const FormData& form = form_structure->ToFormData();
+  const AutofillField* const autofill_field =
+      form_structure->GetFieldById(fields[0]);
+  CHECK(autofill_field);
+
+  std::vector<ActorSuggestionWithFillData> result;
+  const AddressDataManager& adm =
+      autofill_manager.client().GetPersonalDataManager().address_data_manager();
+
+  auto convert_and_save_in_result =
+      [&](std::pair<FillingProduct, std::vector<Suggestion>> response) {
+        result.reserve(response.second.size());
+        for (const Suggestion& s : response.second) {
+          if (std::optional<ActorSuggestionWithFillData> actor_suggestion =
+                  GetActorAddressSuggestion(adm, fields, s)) {
+            result.emplace_back(*std::move(actor_suggestion));
+          }
+        }
+      };
+
+  AddressSuggestionGenerator generator(
+      /*plus_address_email_override=*/std::nullopt,
+      /*log_manager=*/nullptr);
+  auto generate_suggestions =
+      [&](std::pair<SuggestionGenerator::SuggestionDataSource,
+                    std::vector<SuggestionGenerator::SuggestionData>> data) {
+        generator.GenerateSuggestions(form, *autofill_field, form_structure,
+                                      autofill_field, autofill_manager.client(),
+                                      {std::move(data)},
+                                      convert_and_save_in_result);
+      };
+  generator.FetchSuggestionData(form, *autofill_field, form_structure,
+                                autofill_field, autofill_manager.client(),
+                                generate_suggestions);
+  return result;
+}
+
+}  // namespace
+
+ActorFormFillingServiceImpl::FillData::FillData() = default;
+
+ActorFormFillingServiceImpl::FillData::FillData(
+    std::vector<FieldGlobalId> field_ids,
+    std::variant<std::monostate, AutofillProfile> filling_payload)
+    : field_ids(std::move(field_ids)),
+      filling_payload(std::move(filling_payload)) {}
+
+ActorFormFillingServiceImpl::FillData::FillData(FillData&&) = default;
+
+ActorFormFillingServiceImpl::FillData&
+ActorFormFillingServiceImpl::FillData::operator=(FillData&&) = default;
+
+ActorFormFillingServiceImpl::FillData::~FillData() = default;
+
+ActorFormFillingServiceImpl::ActorFormFillingServiceImpl() = default;
 
 ActorFormFillingServiceImpl::~ActorFormFillingServiceImpl() = default;
 
@@ -17,7 +147,72 @@ void ActorFormFillingServiceImpl::GetSuggestions(
     const tabs::TabInterface& tab,
     base::span<const FillRequest> fill_requests,
     base::OnceCallback<void(base::expected<std::vector<ActorFormFillingRequest>,
-                                           ActorFormFillingError>)> callback) {}
+                                           ActorFormFillingError>)> callback) {
+  using enum ActorFormFillingError;
+  if (!tab.GetContents()) {
+    std::move(callback).Run(base::unexpected(kAutofillNotAvailable));
+    return;
+  }
+  ContentAutofillClient* const client =
+      ContentAutofillClient::FromWebContents(tab.GetContents());
+  if (!client) {
+    std::move(callback).Run(base::unexpected(kAutofillNotAvailable));
+    return;
+  }
+  const AutofillManager* const autofill_manager =
+      client->GetAutofillManagerForPrimaryMainFrame();
+  if (!autofill_manager) {
+    std::move(callback).Run(base::unexpected(kAutofillNotAvailable));
+    return;
+  }
+
+  // Fill requests should not be empty.
+  if (fill_requests.empty()) {
+    std::move(callback).Run(base::unexpected(kOther));
+    return;
+  }
+
+  std::vector<ActorFormFillingRequest> requests;
+  requests.reserve(fill_requests.size());
+  for (const auto& [requested_data, representative_fields] : fill_requests) {
+    std::vector<ActorSuggestionWithFillData> data;
+    using enum ActorFormFillingRequest::RequestedData;
+    switch (requested_data) {
+      case FormFillingRequest_RequestedData_ADDRESS:
+      case FormFillingRequest_RequestedData_SHIPPING_ADDRESS:
+      case FormFillingRequest_RequestedData_BILLING_ADDRESS:
+      case FormFillingRequest_RequestedData_HOME_ADDRESS:
+      case FormFillingRequest_RequestedData_WORK_ADDRESS:
+        data = GetAddressSuggestions(representative_fields, *autofill_manager);
+        break;
+      case FormFillingRequest_RequestedData_CREDIT_CARD:
+        // TODO(crbug.com/455788947): Add credit card suggestions.
+        break;
+      default:
+        // Invalid request type.
+        std::move(callback).Run(base::unexpected(kOther));
+        return;
+    }
+
+    // For now, we require that every form is fillable.
+    // TODO(crbug.com/455788947): Consider weakening this condition.
+    if (data.empty()) {
+      std::move(callback).Run(base::unexpected(kNoSuggestions));
+      return;
+    }
+
+    requests.emplace_back();
+    requests.back().requested_data = requested_data;
+    requests.back().suggestions.reserve(data.size());
+    for (ActorSuggestionWithFillData& entry : data) {
+      entry.suggestion.id =
+          ActorSuggestionId(suggestion_id_generator_.GenerateNextId());
+      fill_data_[entry.suggestion.id] = std::move(entry.filling_payload);
+      requests.back().suggestions.emplace_back(std::move(entry.suggestion));
+    }
+  }
+  std::move(callback).Run(std::move(requests));
+}
 
 void ActorFormFillingServiceImpl::FillSuggestions(
     const tabs::TabInterface& tab,
---
+++ chrome/browser/autofill/glic/actor_form_filling_service_unittest.cc
@@ -0,0 +1,200 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/autofill/glic/actor_form_filling_service.h"
+
+#include <vector>
+
+#include "base/check_deref.h"
+#include "base/test/gmock_expected_support.h"
+#include "base/test/test_future.h"
+#include "base/types/expected.h"
+#include "chrome/browser/autofill/glic/actor_form_filling_service_impl.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
+#include "chrome/test/base/testing_profile.h"
+#include "components/autofill/content/browser/content_autofill_client.h"
+#include "components/autofill/content/browser/test_autofill_client_injector.h"
+#include "components/autofill/content/browser/test_autofill_driver_injector.h"
+#include "components/autofill/content/browser/test_autofill_manager_injector.h"
+#include "components/autofill/content/browser/test_content_autofill_client.h"
+#include "components/autofill/content/browser/test_content_autofill_driver.h"
+#include "components/autofill/core/browser/foundations/test_browser_autofill_manager.h"
+#include "components/autofill/core/browser/integrators/glic/actor_form_filling_types.h"
+#include "components/autofill/core/browser/test_utils/autofill_form_test_utils.h"
+#include "components/autofill/core/browser/test_utils/autofill_test_utils.h"
+#include "components/autofill/core/common/autofill_test_utils.h"
+#include "components/tabs/public/mock_tab_interface.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_task_environment.h"
+#include "content/public/test/test_web_contents_factory.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace autofill {
+
+namespace {
+
+using ::base::test::ErrorIs;
+using ::base::test::ValueIs;
+using ::testing::AllOf;
+using ::testing::Each;
+using ::testing::ElementsAre;
+using ::testing::Field;
+using ::testing::IsEmpty;
+using ::testing::Matcher;
+using ::testing::Not;
+using ::testing::Return;
+using ::testing::SizeIs;
+using FillRequest = ActorFormFillingService::FillRequest;
+using GetSuggestionsFuture =
+    base::test::TestFuture<base::expected<std::vector<ActorFormFillingRequest>,
+                                          ActorFormFillingError>>;
+
+[[nodiscard]] Matcher<ActorSuggestion> NonEmptyActorSuggestion() {
+  return AllOf(Field(&ActorSuggestion::title, Not(IsEmpty())),
+               Field(&ActorSuggestion::details, Not(IsEmpty())));
+}
+
+[[nodiscard]] Matcher<ActorFormFillingRequest> IsActorFormFillingRequest(
+    ActorFormFillingRequest::RequestedData requested_data) {
+  return AllOf(Field(&ActorFormFillingRequest::requested_data, requested_data),
+               Field(&ActorFormFillingRequest::suggestions,
+                     Each(NonEmptyActorSuggestion())));
+}
+
+// Returns a fill-request with a non-sensical (because null) field id.
+FillRequest UnfindableFillRequest() {
+  return {ActorFormFillingRequest::RequestedData::
+              FormFillingRequest_RequestedData_ADDRESS,
+          {FieldGlobalId()}};
+}
+
+FillRequest AddressFillRequest(std::vector<FieldGlobalId> field_ids) {
+  return {ActorFormFillingRequest::RequestedData::
+              FormFillingRequest_RequestedData_ADDRESS,
+          std::move(field_ids)};
+}
+
+class ActorFormFillingServiceTest : public ChromeRenderViewHostTestHarness {
+ public:
+  ActorFormFillingServiceTest() = default;
+
+  void SetUp() override {
+    ChromeRenderViewHostTestHarness::SetUp();
+    ON_CALL(mock_tab, GetContents()).WillByDefault(Return(web_contents()));
+    NavigateAndCommit(GURL("about:blank"));
+    client().GetPersonalDataManager().address_data_manager().AddProfile(
+        test::GetFullProfile());
+  }
+
+  FormData SeeForm(test::FormDescription form_description) {
+    FormData form = test::GetFormData(form_description);
+    manager().AddSeenForm(form, test::GetHeuristicTypes(form_description),
+                          test::GetServerTypes(form_description));
+    return form;
+  }
+
+ protected:
+  AutofillClient& client() {
+    return CHECK_DEREF(autofill_client_injector_[web_contents()]);
+  }
+  TestBrowserAutofillManager& manager() {
+    return CHECK_DEREF(autofill_manager_injector_[web_contents()]);
+  }
+  ActorFormFillingService& service() { return service_; }
+  tabs::TabInterface& tab() { return mock_tab; }
+
+ private:
+  test::AutofillUnitTestEnvironment autofill_test_environment_;
+  tabs::MockTabInterface mock_tab;
+  TestAutofillClientInjector<TestContentAutofillClient>
+      autofill_client_injector_;
+  TestAutofillDriverInjector<TestContentAutofillDriver>
+      autofill_driver_injector_;
+  TestAutofillManagerInjector<TestBrowserAutofillManager>
+      autofill_manager_injector_;
+  ActorFormFillingServiceImpl service_;
+};
+
+// Tests that a `kNoSuggestions` error is returned if we cannot find the form
+// specified in the request.
+TEST_F(ActorFormFillingServiceTest, UnfindableForm) {
+  GetSuggestionsFuture future;
+  service().GetSuggestions(tab(), {UnfindableFillRequest()},
+                           future.GetCallback());
+  EXPECT_THAT(future.Get(), ErrorIs(ActorFormFillingError::kNoSuggestions));
+}
+
+// Tests that a `kOther` error is returned if the fill request is empty.
+TEST_F(ActorFormFillingServiceTest, EmptyFillRequest) {
+  GetSuggestionsFuture future;
+  service().GetSuggestions(tab(), /*fill_requests=*/{}, future.GetCallback());
+  EXPECT_THAT(future.Get(), ErrorIs(ActorFormFillingError::kOther));
+}
+
+// Tests that a `kOther` error is returned if invalid request data is passed.
+TEST_F(ActorFormFillingServiceTest, InvalidRequestData) {
+  GetSuggestionsFuture future;
+  service().GetSuggestions(
+      tab(), /*fill_requests=*/
+      {FillRequest{static_cast<ActorFormFillingRequest::RequestedData>(234),
+                   {FieldGlobalId()}}},
+      future.GetCallback());
+  EXPECT_THAT(future.Get(), ErrorIs(ActorFormFillingError::kOther));
+}
+
+// Tests that a suggestion is returned when invoking on an address form.
+TEST_F(ActorFormFillingServiceTest, SimpleAddressForm) {
+  FormData form = SeeForm({.fields = {{.server_type = NAME_FULL},
+                                      {.server_type = ADDRESS_HOME_LINE1},
+                                      {.server_type = ADDRESS_HOME_CITY}}});
+
+  GetSuggestionsFuture future;
+  service().GetSuggestions(tab(),
+                           {AddressFillRequest({form.fields()[0].global_id()})},
+                           future.GetCallback());
+  EXPECT_THAT(future.Get(),
+              ValueIs(ElementsAre(IsActorFormFillingRequest(
+                  ActorFormFillingRequest::RequestedData::
+                      FormFillingRequest_RequestedData_ADDRESS))));
+}
+
+// Tests that `kAutofillNotAvailable` is returned if the tab has no web
+// contents.
+TEST(ActorFormFillingServiceWithoutAutofillTest, NoWebContents) {
+  tabs::MockTabInterface mock_tab;
+  ON_CALL(mock_tab, GetContents()).WillByDefault(Return(nullptr));
+
+  GetSuggestionsFuture future;
+  ActorFormFillingServiceImpl service;
+  service.GetSuggestions(mock_tab, {UnfindableFillRequest()},
+                         future.GetCallback());
+  EXPECT_THAT(future.Get(),
+              ErrorIs(ActorFormFillingError::kAutofillNotAvailable));
+}
+
+// Tests that `kAutofillNotAvailable` is returned if the tab has no
+// `AutofillClient`.
+TEST(ActorFormFillingServiceWithoutAutofillTest, NoAutofillClient) {
+  content::BrowserTaskEnvironment task_environment;
+  TestingProfile profile;
+  content::TestWebContentsFactory test_web_contents_factory;
+  content::WebContents* web_contents =
+      test_web_contents_factory.CreateWebContents(&profile);
+
+  tabs::MockTabInterface mock_tab;
+  ON_CALL(mock_tab, GetContents()).WillByDefault(Return(web_contents));
+  ASSERT_FALSE(ContentAutofillClient::FromWebContents(web_contents));
+
+  GetSuggestionsFuture future;
+  ActorFormFillingServiceImpl service;
+  service.GetSuggestions(mock_tab, {UnfindableFillRequest()},
+                         future.GetCallback());
+  EXPECT_THAT(future.Get(),
+              ErrorIs(ActorFormFillingError::kAutofillNotAvailable));
+}
+
+}  // namespace
+
+}  // namespace autofill
