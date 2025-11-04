415ccbfbece59b8c15136201ad8212fd86279301
Palak Agarwal <agpalak@chromium.org>
2025-11-04 00:37:38 -0800

    Mark ReceiveFramesFromFakeCaptureDevice test flaky for Android
    Bug: 456328657
    Change-Id: I707b3ad6e057497118e6b470fb397073579252a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7116179
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1539824}

--- content/browser/renderer_host/media/video_capture_browsertest.cc
+++ content/browser/renderer_host/media/video_capture_browsertest.cc
@@ -265,7 +265,8 @@ IN_PROC_BROWSER_TEST_P(VideoCaptureBrowserTest, StartAndImmediatelyStop) {
 }
 
 // Flaky on MSAN. https://crbug.com/840294
-#if defined(MEMORY_SANITIZER) || BUILDFLAG(IS_MAC)
+// Flaky on Android. https://crbug.com/456328657
+#if defined(MEMORY_SANITIZER) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_ANDROID)
 #define MAYBE_ReceiveFramesFromFakeCaptureDevice \
   DISABLED_ReceiveFramesFromFakeCaptureDevice
 #else
