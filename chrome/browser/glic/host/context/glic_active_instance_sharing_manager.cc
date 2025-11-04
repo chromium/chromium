3eb892a52cbf77c56d90b985abd12b592b0b6314
Michelle Abreo <michelleabreo@google.com>
2025-11-03 16:09:48 -0800

    Update sharing manager delegate when FRE is not accepted.
    This CL ensures that the sharing manager delegate is not set unless the
    FRE is accepted. This means that neither the underline view nor the
    border glow will show up when the user opens the sidepanel if they have
    not accepted the FRE.
    
    Bug: b:455630352
    Change-Id: I78aa66b4f2c155ccc66c7a3055e2e226703618c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101485
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1539689}

--- chrome/browser/glic/host/context/glic_active_instance_sharing_manager.cc
+++ chrome/browser/glic/host/context/glic_active_instance_sharing_manager.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/glic/host/context/glic_active_instance_sharing_manager.h"
 
+#include "chrome/browser/glic/public/glic_enabling.h"
 #include "chrome/browser/glic/public/glic_instance.h"
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/glic_keyed_service_factory.h"
@@ -13,20 +14,50 @@
 namespace glic {
 
 GlicActiveInstanceSharingManager::GlicActiveInstanceSharingManager(
-    GlicInstanceCoordinator* instance_coordinator) {
+    Profile* profile,
+    GlicEnabling* enabling,
+    GlicInstanceCoordinator* instance_coordinator)
+    : profile_(profile) {
   active_instance_subscription_ =
-      instance_coordinator->AddActiveInstanceChangedCallbackAndNotifyImmediately(
-          base::BindRepeating(
-              &GlicActiveInstanceSharingManager::OnActiveInstanceChanged,
-              base::Unretained(this)));
+      instance_coordinator
+          ->AddActiveInstanceChangedCallbackAndNotifyImmediately(
+              base::BindRepeating(
+                  &GlicActiveInstanceSharingManager::OnActiveInstanceChanged,
+                  base::Unretained(this)));
+
+  CHECK(enabling);
+  profile_state_subscription_ =
+      enabling->RegisterProfileReadyStateChanged(base::BindRepeating(
+          &GlicActiveInstanceSharingManager::OnProfileReadyStateChanged,
+          base::Unretained(this)));
 }
 
 GlicActiveInstanceSharingManager::~GlicActiveInstanceSharingManager() = default;
 
 void GlicActiveInstanceSharingManager::OnActiveInstanceChanged(
     GlicInstance* instance) {
-  if (instance) {
-    SetDelegate(&instance->host().sharing_manager());
+  if (GlicEnabling::IsUnifiedFreEnabled(profile_)) {
+    pending_active_instance_ = instance;
+    UpdateDelegate();
+  } else {
+    if (instance) {
+      SetDelegate(&instance->host().sharing_manager());
+    } else {
+      SetDelegate(nullptr);
+    }
+  }
+}
+
+void GlicActiveInstanceSharingManager::OnProfileReadyStateChanged() {
+  if (GlicEnabling::IsUnifiedFreEnabled(profile_)) {
+    UpdateDelegate();
+  }
+}
+
+void GlicActiveInstanceSharingManager::UpdateDelegate() {
+  if (pending_active_instance_ &&
+      GlicEnabling::IsEnabledAndConsentForProfile(profile_)) {
+    SetDelegate(&pending_active_instance_->host().sharing_manager());
   } else {
     SetDelegate(nullptr);
   }
