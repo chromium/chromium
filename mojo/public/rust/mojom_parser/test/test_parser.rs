4083a04d4ad8adf052eda36d2797fdb076537a98
Devon Loehr <dloehr@google.com>
2025-11-05 11:34:50 -0800

    [rustmojo] Write cxx bindings for validation test parser
    The mojom tests have a textual format for defining the contents of a
    mojom message. This CL adds cxx bindings for the parser that turns that
    format into actual test data we can use.
    
    Bug: 444509367
    Change-Id: I3606588f3c22f51b0ac35eef99879d781c262a54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7114419
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1540790}

--- mojo/public/rust/mojom_parser/test/test_parser.rs
+++ mojo/public/rust/mojom_parser/test/test_parser.rs
@@ -12,3 +12,20 @@
 //!
 //! The types in this file correspond to those defined in
 //! //mojo/public/rust/test_mojom/parser_unittests.mojom
+
+chromium::import! {
+    "//mojo/public/rust/mojom_parser:validation_parser";
+}
+
+use rust_gtest_interop::prelude::*;
+
+// Quick test to make sure things basically work.
+// FOR_RELEASE: Write real tests.
+#[gtest(MojomParserTestSuite, TestSomething)]
+fn test_something() -> Result<(), String> {
+    // Should produce an array of 4-byte value 2, then 2-byte value 257
+    let parsed_data = validation_parser::parse("[u4]2\n[u2]257")?;
+    expect_eq!(parsed_data.data.as_ref(), &[2, 0, 0, 0, 1, 1]);
+    expect_eq!(parsed_data.num_handles, 0);
+    Ok(())
+}
