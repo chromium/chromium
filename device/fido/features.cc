a66eda0c6c8aa605e1d5d8366e23d7330fe6f0d2
Ken Buchanan <kenrb@chromium.org>
2025-11-05 06:34:52 -0800

    [WebAuthn] Enable Signal API on Android by default
    Shipping in m144.
    
    Bug: 444399082
    Change-Id: I2ef2ebae2a65d48988c882a96e0a053d6086232c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7120699
    Auto-Submit: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1540596}

--- device/fido/features.cc
+++ device/fido/features.cc
@@ -113,10 +113,10 @@ BASE_FEATURE(kWebAuthnHelloSignal,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
-// Not yet enabled by default.
+// Enabled by default in M144 Remove in or after M146.
 BASE_FEATURE(kWebAuthnAndroidSignal,
              "WebAuthenticationAndroidSignal",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 // Disabled by default.
