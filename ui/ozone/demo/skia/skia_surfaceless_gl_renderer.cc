02862e1a56751d63bf0fa8adcfa964a40a21c62e
Colin Blundell <blundell@chromium.org>
2025-11-05 23:51:34 -0800

    [//ui] Port SkiaSurfacelessGLRenderer's call of ImportNativePixmap()
    Move away from the deprecated variant taking in BufferFormat.
    
    Bug: 450325664
    Change-Id: Iea89e323cd18ee0b5f6b9974c3bc59e3c99bfe1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122284
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541066}

--- ui/ozone/demo/skia/skia_surfaceless_gl_renderer.cc
+++ ui/ozone/demo/skia/skia_surfaceless_gl_renderer.cc
@@ -118,9 +118,6 @@ bool SurfacelessSkiaGlRenderer::BufferWrapper::Initialize(
   glGenTextures(1, &gl_tex_);
 
   auto format = display::DisplaySnapshot::PrimaryFormat();
-  gfx::BufferFormat buffer_format =
-      viz::SinglePlaneSharedImageFormatToBufferFormat(format);
-
   pixmap_ = OzonePlatform::GetInstance()
                 ->GetSurfaceFactoryOzone()
                 ->CreateNativePixmap(widget, nullptr, size, format,
@@ -130,8 +127,7 @@ bool SurfacelessSkiaGlRenderer::BufferWrapper::Initialize(
       OzonePlatform::GetInstance()
           ->GetSurfaceFactoryOzone()
           ->GetCurrentGLOzone()
-          ->ImportNativePixmap(pixmap_, buffer_format,
-                               gfx::BufferPlane::DEFAULT, size,
+          ->ImportNativePixmap(pixmap_, format, gfx::BufferPlane::DEFAULT, size,
                                gfx::ColorSpace(), GL_TEXTURE_2D, gl_tex_);
 
   if (!pixmap_gl_binding_) {
