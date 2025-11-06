a96e40da62678ff2565295187e59aec80c218264
Elly <ellyjones@chromium.org>
2025-11-06 13:16:25 -0800

    tracked-prefs: add a test that encrypted hash values are stable
    Specifically, that the hash contained inside the encrypted wrapper is
    stable; the encrypted wrapper's value is not because it is encrypted
    with a random key and nonce per test run.
    
    Bug: 453961766
    Change-Id: I1368a695082168eb52989763ad94acb8998e24b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128317
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541409}

--- services/preferences/tracked/pref_hash_calculator_unittest.cc
+++ services/preferences/tracked/pref_hash_calculator_unittest.cc
@@ -290,3 +290,29 @@ TEST_F(PrefHashCalculatorEncryptedTest, ValidateEncryptedHash) {
             calculator_.ValidateEncrypted(path, null_value_ptr,
                                           valid_hash_base64, &test_encryptor_));
 }
+
+TEST_F(PrefHashCalculatorEncryptedTest, EncryptedHashValuesAreStable) {
+  base::Value::Dict dict;
+  dict.Set("key", "value");
+  std::optional<std::string> encrypted_hash =
+      calculator_.CalculateEncryptedHash("p.dict", &dict, &test_encryptor_);
+
+  // The hash was encrypted with test_encryptor_, then base64-encoded. Since
+  // TestEncryptor uses a random key, and Encryptors always use a random nonce,
+  // the actual ciphertext isn't stable between runs. We decode the base64 here
+  // then decrypt the hash to get the raw hash value, and compare it against a
+  // known hash.
+  std::optional<std::string> decrypted_hash =
+      test_encryptor_.DecryptData(*base::Base64Decode(*encrypted_hash));
+  ASSERT_TRUE(decrypted_hash.has_value());
+
+  // Despite using a std::string to represent it, the decrypted hash is actually
+  // a byte string, so we compare it against raw bytes below.
+  constexpr auto kExpectedHash = std::to_array<uint8_t>({
+      0xd9, 0x01, 0x6e, 0x93, 0x04, 0x0a, 0xfc, 0xcb, 0x86, 0x87, 0x90,
+      0x31, 0x93, 0xc5, 0x67, 0xdc, 0xdf, 0xad, 0x49, 0x36, 0x14, 0xee,
+      0xad, 0xd0, 0x48, 0x34, 0x81, 0x52, 0x8c, 0xfc, 0x1b, 0x0e,
+  });
+
+  EXPECT_EQ(base::as_byte_span(*decrypted_hash), kExpectedHash);
+}
