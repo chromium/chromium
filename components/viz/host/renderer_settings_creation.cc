bf8f96f84a2436e51b577251661439d43fdf8442
Michael Tang <tangm@microsoft.com>
2025-11-07 11:02:05 -0800

    Enable generate_complex_occluder_for_rounded_corners only on ChromeOS
    This change pulls out enabling
    generate_complex_occluder_for_rounded_corners into its own block,
    guarded by IS_CHROMEOS.
    
    This is because we've seen some perf regressions on Windows in Edge when
    this feature is enabled. Edge doesn't use large rounded corners so in
    its case it's possible that using the simple occluder + small amounts of
    overdaw is faster than generating the complex occluder.
    
    Bug: 294589500
    Change-Id: Id42466c54443ccecf7eab30ff1b7c958e2ebed85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7122084
    Reviewed-by: Peter McNeeley <petermcneeley@google.com>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1541927}

--- components/viz/host/renderer_settings_creation.cc
+++ components/viz/host/renderer_settings_creation.cc
@@ -73,11 +73,14 @@ RendererSettings CreateRendererSettings() {
   // Used finch experiment to determine the best value. See b:330617490 for
   // details.
   renderer_settings.occlusion_culler_settings.quad_split_limit = 12;
-  renderer_settings.occlusion_culler_settings
-      .generate_complex_occluder_for_rounded_corners = true;
 #else
   renderer_settings.occlusion_culler_settings.quad_split_limit =
       features::DrawQuadSplitLimit();
+#endif
+
+#if BUILDFLAG(IS_CHROMEOS)
+  renderer_settings.occlusion_culler_settings
+      .generate_complex_occluder_for_rounded_corners = true;
 #endif
 
 #if BUILDFLAG(IS_OZONE)
