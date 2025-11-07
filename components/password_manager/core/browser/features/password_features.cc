cf2a83f646144c4e219dfd9b8112dc1199730348
Oleksandr Tara <otara@google.com>
2025-11-07 06:02:14 -0800

    Enable same site iframes by default
    Bug: 454961534
    Fixed: 457968910
    Change-Id: I65315e4b2ac3d1a580e59691916a66958aaf425a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7123001
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Oleksandr Tara <otara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541757}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -20,7 +20,7 @@ BASE_FEATURE(kActorLoginLocalClassificationModel,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kActorLoginReauthTaskRefocus, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kActorLoginSameSiteIframeSupport,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableActorLoginPermissions, base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS)
 
