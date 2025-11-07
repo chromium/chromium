0fe44c7c669e3d12e2863f3318f52aa0192a64aa
Nikhil Jakhar <nikhiljakhar@google.com>
2025-11-07 09:01:58 -0800

    legion: Move Error Codes to a separate file
    This change moves error codes to a separate file named error.h to
    organize the code better.
    
    Bug: b:457709491
    Change-Id: I5a23c58b773cf9051e32cab2ffc85a51be501e07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7130599
    Commit-Queue: Oleh Lamzin <lamzin@google.com>
    Auto-Submit: Nikhil Jakhar <nikhiljakhar@google.com>
    Reviewed-by: Oleh Lamzin <lamzin@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1541851}

--- components/legion/secure_channel.h
+++ components/legion/secure_channel.h
@@ -7,33 +7,10 @@
 
 #include "base/functional/callback.h"
 #include "base/types/expected.h"
+#include "components/legion/error_code.h"
 #include "components/legion/legion_common.h"
 
 namespace legion {
-
-// Represents errors that can occur during a secure channel operation.
-enum class ErrorCode {
-  // A non-transient error occurred. The client should not retry the request.
-  kError,
-  // Authentication failed, e.g., due to an invalid API key.
-  kAuthenticationFailed,
-  // A transient network error occurred. The client may retry the request.
-  kNetworkError,
-  // Attestation failed. The client should not retry the request.
-  kAttestationFailed,
-  // Handshake or attestation failed. The client should not retry the request.
-  kHandshakeFailed,
-  // Encryption failed. The client should not retry the request.
-  kEncryptionFailed,
-  // Decryption failed. The client should not retry the request.
-  kDecryptionFailed,
-  // Failed to parse the server response.
-  kResponseParseError,
-  // The server response did not contain any content.
-  kNoContent,
-  // The server response did not contain a generate_content_response.
-  kNoResponse,
-};
 
 // Interface for the Secure Channel Layer.
 // This layer is responsible for handling the secure communication
