8281b8a09b6050e9f936dc728fa1091039af441a
Norge Vizcay <vizcay@google.com>
2025-11-07 03:25:07 -0800

    Remove invalid TODOs from valuables bridge.
    Bug: 434122759
    Change-Id: Ie4cb1486a7f2bd2f3c7a4850424dacb1f857c3c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7130801
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1541703}

--- components/autofill/core/browser/webdata/valuables/valuable_sync_bridge.cc
+++ components/autofill/core/browser/webdata/valuables/valuable_sync_bridge.cc
@@ -370,12 +370,8 @@ bool ValuableSyncBridge::IsEntityDataValid(
     case sync_pb::AutofillValuableSpecifics::kLoyaltyCard:
       return AreAutofillLoyaltyCardSpecificsValid(autofill_valuable);
     case sync_pb::AutofillValuableSpecifics::kFlightReservation:
-      // TODO(crbug.com/436547381): Add stronger validation for flight
-      // reservation.
       return IsSyncWalletFlightReservationsEnabled();
     case sync_pb::AutofillValuableSpecifics::kVehicleRegistration:
-      // TODO(crbug.com/436547381): Add stronger validation for vehicle
-      // registration.
       return IsSyncWalletVehicleRegistrationsEnabled();
     case sync_pb::AutofillValuableSpecifics::VALUABLE_DATA_NOT_SET:
       // Ignore new entry types that the client doesn't know about.
