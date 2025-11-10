99bceb477153ce830aa315c9c70195985b240a80
Dan Harrington <harringtond@chromium.org>
2025-11-10 12:14:04 -0800

    Revert "autotest: use new gn refs flag --relation=source "
    This reverts commit 73092f5f8337ba3025b927d23adc2c5ed139e739.
    
    Reason for revert: seems to break some autotest usage
    
    Original change's description:
    > autotest: use new gn refs flag --relation=source 
    >
    > This fixes a problem where test files under chrome/test/data, e.g.
    > chrome/test/data/webui/webview/webui_webview_browsertest.cc,
    > would fail to find the correct target because chrome/test/data
    > is a data dependency of many test targets.
    >
    > Change-Id: I69419e49d3802a6ce426acf32a3a88f16a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7138180
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Dan Harrington <harringtond@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1542628}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I0da17497ece2c66e3aba0cdab10e6b801de640c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7138261
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Dan Harrington <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542706}

--- tools/autotest.py
+++ tools/autotest.py
@@ -610,9 +610,7 @@ def FindTestTargets(target_cache, out_dir, paths, args):
     # back to a list of known test targets if that fails.
     gn_path = os.path.join(DEPOT_TOOLS_DIR, 'gn.py')
 
-    cmd = [
-        sys.executable, gn_path, 'refs', out_dir, '--all', '--relation=source'
-    ] + paths
+    cmd = [sys.executable, gn_path, 'refs', out_dir, '--all'] + paths
     targets = _ParseRefsOutput(RunCommand(cmd))
     test_targets = _TestTargetsFromGnRefs(targets)
 
