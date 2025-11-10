0cede4184ebdaf069ca19b21149c93c17b86c452
James Leung <jamesleung@google.com>
2025-11-10 11:16:49 -0800

    [composebox] Add Portable Drag and Drop Handler
    * Added drag and drop handler class that can be plugged into
    any host (parent component) for drag and drop functionality. Ported over
    from cr_components/composebox/composebox.ts with minor tweaks to the
    logic.
    * Drag and drop functionality includes dragEnter, dragLeave, dragOver,
    Drop events. Changes host's drag and animation states, if any, based on
    if feature param flag is on, and drag events.
    * Handler is used by initializing in host and passing in host as
    argument. Then, eventHandlers should be called in html element of
    interest in the host.
    * dragLeave only fully triggers state change if drag leave is the last
    drag leave event in order to ignore false preemptive dragLeaves. This is
    due to how easily HTML drag events are fired. Previous checks of
    relatedTarget being a child of currentTarget in event object do not work
    since the handler is not part of the host (the main lit component).
    
    Todo: add tests in another CL (not required since the addFiles function
    is already tested but would be helpful )
    
    tests in a follow-up CL; this is needed to unblock others + bug bash
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL I will add
    Bug: b:456477155
    Change-Id: Ic926b2ad52ea56c2715bf749d02e5061962467f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7126012
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1542668}

--- ui/webui/resources/cr_components/search/animated_glow.html.ts
+++ ui/webui/resources/cr_components/search/animated_glow.html.ts
@@ -1,6 +1,8 @@
-/* Copyright 2025 The Chromium Authors
+/**
+ * Copyright 2025 The Chromium Authors
  * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file. */
+ * found in the LICENSE file.
+ */
 
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
 
@@ -9,8 +11,9 @@ import type {SearchAnimatedGlowElement} from './animated_glow.js';
 export function getHtml(this: SearchAnimatedGlowElement) {
   // TODO(crbug.com/454730356): replace
   // drop string with translatable string.
-  /*
-  * Note: this does not include ::before and ::after notes.
+
+  /**
+  Note: this does not include ::before and ::after notes.
   See .css file comments for more details on implementation.
   * DragDropPlaceholder for drag and drop text overlay.
   * Gradient outer glow for so glow appears
@@ -23,6 +26,7 @@ export function getHtml(this: SearchAnimatedGlowElement) {
   * Background is to apply a frosted glass effect in drag and drop mode,
   and to act as an overlay to help create the gradient border.
   */
+
   // clang-format off
   return html`<!--_html_template_start_-->
     <div id="dragDropPlaceholder"> Drop your file here </div>
--- ui/webui/resources/cr_components/search/animated_glow.ts
+++ ui/webui/resources/cr_components/search/animated_glow.ts
@@ -1,6 +1,8 @@
-/* Copyright 2025 The Chromium Authors
+/**
+ * Copyright 2025 The Chromium Authors
  * Use of this source code is governed by a BSD-style license that can be
- * found in the LICENSE file. */
+ * found in the LICENSE file.
+ */
 
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 
