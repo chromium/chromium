315fbe220489b91d3a1307f92257d8c0fe182ae7
Muhammad Salmaan <musalmaan@chromium.org>
2025-11-10 10:19:46 -0800

    Add session restore infobar to infobar internals
    Added session restore infobar to infobar internals for better testing.
    
    Change-Id: I6c925c42e2949c1827b7122f3bfed1cc1c9ef9fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7127263
    Reviewed-by: Daniel Soromou <koretadaniel@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1542637}

--- chrome/browser/ui/webui/infobar_internals/infobar_internals_handler.cc
+++ chrome/browser/ui/webui/infobar_internals/infobar_internals_handler.cc
@@ -12,6 +12,9 @@
 #include "build/branding_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/global_features.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/web_ui.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -19,6 +22,11 @@
 #if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #include "chrome/browser/win/installer_downloader/installer_downloader_controller.h"
 #include "chrome/browser/win/installer_downloader/installer_downloader_pref_names.h"
+#endif
+
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#include "chrome/browser/ui/views/session_restore_infobar/session_restore_infobar_delegate.h"
+#include "chrome/browser/ui/views/session_restore_infobar/session_restore_infobar_manager.h"
 #endif
 
 using InfoBarType = infobar_internals::mojom::InfoBarType;
@@ -36,12 +44,16 @@ void InfoBarInternalsHandler::TriggerInfoBar(InfoBarType type,
 }
 
 void InfoBarInternalsHandler::GetInfoBars(GetInfoBarsCallback callback) {
-  static const base::NoDestructor<std::array<InfoBarEntry, 1>> kInfobars(
+  static const base::NoDestructor<std::array<InfoBarEntry, 2>> kInfobars(
       {InfoBarEntry{
-          /*type=*/InfoBarType::kInstallerDownloader, "Installer Downloader",
-          "The Installer Downloader can only be triggered on Windows. The "
-          "manual trigger consist to reset any browser state that can prevent "
-          "it to shown and then trigger a show request."}});
+           /*type=*/InfoBarType::kInstallerDownloader, "Installer Downloader",
+           "The Installer Downloader can only be triggered on Windows. The "
+           "manual trigger consist to reset any browser state that can prevent "
+           "it to shown and then trigger a show request."},
+       InfoBarEntry{
+           /*type=*/InfoBarType::kSessionRestore, "Session Restore",
+           "Triggers the session restore infobar. This infobar can only be "
+           "triggered on Mac, Windows and Linux."}});
 
   std::vector<infobar_internals::mojom::InfoBarEntryPtr> infobar_list;
   for (const auto& infobar : *kInfobars) {
@@ -83,6 +95,22 @@ bool InfoBarInternalsHandler::TriggerInfoBarInternal(InfoBarType type) {
       }
 #endif
       return false;
+    }
+    case InfoBarType::kSessionRestore: {
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+      BrowserWindowInterface* const bwi =
+          GetLastActiveBrowserWindowInterfaceWithAnyProfile();
+      Profile* profile = bwi->GetProfile();
+
+      if (!profile) {
+        return false;
+      }
+      session_restore_infobar::SessionRestoreInfoBarManager::GetInstance()
+          ->ShowInfoBar(*profile,
+                        session_restore_infobar::SessionRestoreInfoBarDelegate::
+                            InfobarMessageType::kTurnOffFromRestart);
+      return true;
+#endif
     }
   }
 
