c0101a8048d398d9432566d0eb369f53ee3d585c
Olivier Li <olivierli@chromium.org>
2025-11-11 11:57:26 -0800

    [persistent_cache]: Flesh out perf tests.
    Cases for Insert() and Find() are added. Common code is extracted to
    the fixture to avoid introducing subtle differences between cases.
    Checks are added to ensure that costly setup operations are not done
    under timing.
    
    Bug: 377475540
    Change-Id: I0f70d9b2f2ee6511c780207423291c85748a9cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7103505
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543271}

--- components/persistent_cache/persistent_cache_perftest.cc
+++ components/persistent_cache/persistent_cache_perftest.cc
@@ -4,6 +4,13 @@
 
 #include "components/persistent_cache/persistent_cache.h"
 
+#include <algorithm>
+
+#include "base/auto_reset.h"
+#include "base/containers/heap_array.h"
+#include "base/containers/span.h"
+#include "base/functional/function_ref.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/test/gmock_expected_support.h"
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
@@ -11,41 +18,145 @@
 #include "components/persistent_cache/backend.h"
 #include "components/persistent_cache/entry.h"
 #include "components/persistent_cache/sqlite/test_helper.h"
+#include "components/persistent_cache/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/perf/perf_result_reporter.h"
 
 namespace persistent_cache {
 
-using PersistentCachePerftest = testing::Test;
+class PersistentCachePerftest : public testing::Test {
+ protected:
+  std::unique_ptr<PersistentCache> CreateCache() {
+    std::unique_ptr<Backend> backend =
+        provider_.CreateBackendWithFiles(BackendType::kSqlite);
+    CHECK(backend);
+    return std::make_unique<PersistentCache>(std::move(backend));
+  }
+
+  void RunAndTimeTest(std::string operation_name,
+                      int iteration_count,
+                      base::FunctionRef<void()> test_body) {
+    base::AutoReset<bool> resetter(&under_measurment_, true);
+    base::ElapsedTimer elapsed_timer;
+    base::ElapsedThreadTimer elapsed_thread_timer;
+
+    test_body();
+
+    ReportMeasurment(operation_name, iteration_count, elapsed_timer.Elapsed(),
+                     elapsed_thread_timer.Elapsed());
+  }
+
+  // Pregenerates keys. Use to avoid timing allocation overhead.
+  std::vector<std::string> GenerateKeys(int iteration_count) {
+    CHECK(!under_measurment_);
+
+    std::vector<std::string> keys(iteration_count);
+    std::generate(keys.begin(), keys.end(),
+                  [i = 0]() mutable { return base::NumberToString(i++); });
+    return keys;
+  }
+
+  // Generates a value buffer to be inserted according to params. Should be done
+  // outside of timing to avoid measuring overhead.
+  base::HeapArray<uint8_t> MakeValue() {
+    CHECK(!under_measurment_);
+
+    // Median size of entries for a use case of PersistentCache as reported by
+    // UMA on November 7th 2025.
+    static constexpr size_t kValueSize = 6958;
+    auto value = base::HeapArray<uint8_t>::Uninit(kValueSize);
+
+    // Fill the data with random bytes to avoid unknown optimizations for
+    // identical pages.
+    base::RandBytes(value);
+    return value;
+  }
+
+ private:
+  void ReportMeasurment(std::string operation_name,
+                        int iteration_count,
+                        base::TimeDelta elapsed_time,
+                        base::TimeDelta elapsed_thread_time) {
+    const std::string reporter_name("PersistentCache");
+    perf_test::PerfResultReporter reporter(reporter_name, operation_name);
+    reporter.RegisterImportantMetric(".wall_time", "us");
+    reporter.AddResult(
+        ".wall_time",
+        static_cast<size_t>(elapsed_time.InMicroseconds() / iteration_count));
+    reporter.RegisterImportantMetric(".thread_time", "us");
+    reporter.AddResult(
+        ".thread_time",
+        static_cast<size_t>(elapsed_thread_time.InMicroseconds() /
+                            iteration_count));
+  }
+
+  test_support::TestHelper provider_;
+  bool under_measurment_ = false;
+};
 
 #if !BUILDFLAG(IS_FUCHSIA)
 
 TEST_F(PersistentCachePerftest, OpenClose) {
-  test_support::TestHelper provider;
-  std::unique_ptr<Backend> backend =
-      provider.CreateBackendWithFiles(BackendType::kSqlite);
-  ASSERT_TRUE(backend);
-  PersistentCache persistent_cache(std::move(backend));
+  auto persistent_cache = CreateCache();
 
-  // Ensures there are entries in the cache.
-  const char* kKey = "foo";
-  ASSERT_THAT(persistent_cache.Insert(kKey, base::byte_span_from_cstring("1")),
-              base::test::HasValue());
-  ASSERT_THAT(persistent_cache.Find(kKey),
-              base::test::ValueIs(testing::NotNull()));
+  static constexpr int kIterationCount = 1024;
 
-  base::ElapsedTimer timer;
-  const int kAmountOfIteration = 16 * 1024;
-  for (size_t i = 0; i < kAmountOfIteration; ++i) {
-    auto persistent_cache_under_test =
-        PersistentCache::Open(*persistent_cache.ExportReadWriteBackendParams());
+  int success_count = 0;
+  RunAndTimeTest("OpenClose", kIterationCount, [&] {
+    for (size_t i = 0; i < kIterationCount; ++i) {
+      auto persistent_cache_under_test = PersistentCache::Open(
+          *persistent_cache->ExportReadWriteBackendParams());
+      if (persistent_cache_under_test) {
+        ++success_count;
+      }
+    }
+  });
+
+  ASSERT_EQ(success_count, kIterationCount);
+}
+
+TEST_F(PersistentCachePerftest, Insert) {
+  auto persistent_cache = CreateCache();
+
+  static constexpr int kIterationCount = 1024;
+  std::vector<std::string> keys = GenerateKeys(kIterationCount);
+  base::HeapArray<uint8_t> value = MakeValue();
+
+  int success_count = 0;
+  RunAndTimeTest("Insert", kIterationCount, [&] {
+    success_count = std::ranges::count_if(
+        keys, [&cache = *persistent_cache, &value](const auto& key) {
+          return cache.Insert(key, value.as_span()).has_value();
+        });
+  });
+  ASSERT_EQ(success_count, kIterationCount);
+}
+
+TEST_F(PersistentCachePerftest, Find) {
+  auto persistent_cache = CreateCache();
+
+  static constexpr int kIterationCount = 1024;
+  std::vector<std::string> keys = GenerateKeys(kIterationCount);
+  base::HeapArray<uint8_t> value = MakeValue();
+
+  // Fill the cache.
+  for (const std::string& key : keys) {
+    ASSERT_THAT(persistent_cache->Insert(key, value.as_span()),
+                base::test::HasValue());
   }
 
-  perf_test::PerfResultReporter reporter("PersistentCache", "OpenClose");
-  reporter.RegisterImportantMetric(".wall_time", "us");
-  reporter.AddResult(".wall_time",
-                     static_cast<size_t>(timer.Elapsed().InMicroseconds() /
-                                         kAmountOfIteration));
+  // Shuffle the keys around to avoid taking advantage of file-system caching
+  // behavior.
+  base::RandomShuffle(keys.begin(), keys.end());
+
+  int success_count = 0;
+  RunAndTimeTest("Find", kIterationCount, [&] {
+    success_count = std::ranges::count_if(
+        keys, [&cache = *persistent_cache](const auto& key) {
+          return cache.Find(key).has_value();
+        });
+  });
+  ASSERT_EQ(success_count, kIterationCount);
 }
 
 #endif  // !BUILDFLAG(IS_FUCHSIA)
