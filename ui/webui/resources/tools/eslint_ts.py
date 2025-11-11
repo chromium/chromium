8a4c2f45fa3059010e7175f6fe804f43a84222e7
dpapad <dpapad@chromium.org>
2025-11-11 10:56:11 -0800

    WebUI: Include generated .html.ts files in build_webui() ESLint checks.
    Also, had to modify how ESLint is invoked on Windows to bypass the
    command line length limit, which is being hit after passing more files
    on the command line.
    
    Bug: 457650895
    Change-Id: I08ecf0e1a27e5eda2a705d8c84dc31532e0db205
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7141419
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1543233}

--- ui/webui/resources/tools/eslint_ts.py
+++ ui/webui/resources/tools/eslint_ts.py
@@ -64,14 +64,29 @@ def main(argv):
   if len(args.in_files) == 0:
     return
 
-  node.RunNode([
-      node_modules.PathToEsLint(),
-      # Force colored output, otherwise no colors appear when running locally.
-      '--color',
-      '--quiet',
-      '--config',
-      config_file,
-  ] + list(map(lambda f: os.path.join(args.in_folder, f), args.in_files)))
+  in_files = list(map(lambda f: os.path.join(args.in_folder, f), args.in_files))
+
+  chunk_size = len(in_files)
+  if sys.platform == 'win32' and len(' '.join(in_files)) > 30000:
+    # On Windows the maximum command line length for Python's CreateProcess()
+    # function is 32767 characters. When the limit is exceeded a confusing
+    # "FileNotFoundError: [WinError 206] The filename or extension is too long"
+    # error is thrown. Split to multiple invocations to work around that.
+    chunk_size = min(round(len(in_files) / 2), 100)
+
+  in_files_chunks = [
+      in_files[i:i + chunk_size] for i in range(0, len(in_files), chunk_size)
+  ]
+
+  for files in in_files_chunks:
+    node.RunNode([
+        node_modules.PathToEsLint(),
+        # Force colored output, otherwise no colors appear when running locally.
+        '--color',
+        '--quiet',
+        '--config',
+        config_file,
+    ] + files)
 
 
 if __name__ == '__main__':
