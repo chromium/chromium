1f7b61879cf6e46a0d50998a8c53b44ed988b4d2
Justin Donnelly <jdonnelly@chromium.org>
2025-11-12 10:55:55 -0800

    [143][ntp-next] Action Chips click and impression metrics
    This CL adds the click and impression metrics for the NTP Action Chips.
    Note that the impression metrics are static for the time being until we
    can show the action chips contents dynamically.
    
    (cherry picked from commit 6fa021220cca18d648d06fcc589d6f24b38ac888)
    
    Bug: 453649623, 459935737
    Change-Id: Ic9a46a754431f75bf5862c0327aefe53403fea5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7108018
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Gianni Chen <giannich@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1539674}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7146564
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/branch-heads/7499@{#1508}
    Cr-Branched-From: b30439823e5177773584139e72e0593e36863899-refs/heads/main@{#1536371}

--- chrome/browser/resources/new_tab_page/action_chips/action_chips.ts
+++ chrome/browser/resources/new_tab_page/action_chips/action_chips.ts
@@ -11,6 +11,33 @@ import {getCss} from './action_chips.css.js';
 import {getHtml} from './action_chips.html.js';
 
 /**
+ * TODO: Move the enum to the Mojo model once it's created.
+ * Elements on the Action Chips. This enum must match the numbering for
+ * ActionChipsType in enums.xml. These values are persisted to logs. Entries
+ * should not be renumbered, removed or reused.
+ */
+
+// LINT.IfChange(ActionChipsType)
+export enum ActionChipsType {
+  CREATE_IMAGE = 0,
+  DEEP_SEARCH = 1,
+  MAX_VALUE = DEEP_SEARCH,
+}
+// LINT.ThenChange(//tools/metrics/histograms/metadata/new_tab_page/enums.xml:ActionChipsType)
+
+// Records a click metric for the given action chip type.
+function recordClick(element: ActionChipsType) {
+  chrome.metricsPrivate.recordEnumerationValue(
+      'NewTabPage.ActionChips.Click', element, ActionChipsType.MAX_VALUE + 1);
+}
+
+// Records an impression metric for the given action chip type.
+function recordShown(element: ActionChipsType) {
+  chrome.metricsPrivate.recordEnumerationValue(
+      'NewTabPage.ActionChips.Shown', element, ActionChipsType.MAX_VALUE + 1);
+}
+
+/**
  * The element for displaying Action Chips.
  */
 export class ActionChipsElement extends CrLitElement {
@@ -26,11 +53,22 @@ export class ActionChipsElement extends CrLitElement {
     return getHtml.bind(this)();
   }
 
+  // TODO (crbug.com/453650248): Move the impression metrics logging to the
+  // handler logic once the changes are merged in. Right now we are only
+  // statically showing these two chips.
+  override firstUpdated() {
+    recordShown(ActionChipsType.CREATE_IMAGE);
+    recordShown(ActionChipsType.DEEP_SEARCH);
+  }
+
+
   protected onCreateImageClick_() {
+    recordClick(ActionChipsType.CREATE_IMAGE);
     this.onActionChipClick_('Create an image ', ComposeboxMode.CREATE_IMAGE);
   }
 
   protected onDeepSearchClick_() {
+    recordClick(ActionChipsType.DEEP_SEARCH);
     this.onActionChipClick_('Help me research ', ComposeboxMode.DEEP_SEARCH);
   }
 
