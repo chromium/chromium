c4f7610e49b45330dbab7fc1ae0cfe4f3d9d552a
Jacob Stanley <jacobstanley@google.com>
2025-11-12 12:30:04 -0800

    [M143][Tab Indicator Spinner] Enable ignore reduced motion feature by default
    This CL enables GlicActorUiTabIndicatorSpinnerIgnoreReducedMotion by
    default as per the instructions of the M143 Post Branch Merges.
    
    (cherry picked from commit a2618a6e6cbeb6e111f154b24bdb1dc2c35f9011)
    
    Bug: b:457763247, b:457746083
    Fixed: 458769475
    Change-Id: I25463a6c45f1000735600782edc29816d7323be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7128418
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1541294}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7136877
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7499@{#1520}
    Cr-Branched-From: b30439823e5177773584139e72e0593e36863899-refs/heads/main@{#1536371}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -308,9 +308,8 @@ BASE_FEATURE(kGlicActorUiNudgeRedesign, base::FEATURE_DISABLED_BY_DEFAULT);
 // Controls whether we ignore users preference of reduced motion enabled and
 // still show the tab indicator spinner. No-op if kGlicActorUiTabIndicator is
 // disabled.
-// TODO(crbug.com/457746083): Enable feature by default.
 BASE_FEATURE(kGlicActorUiTabIndicatorSpinnerIgnoreReducedMotion,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If enabled, hides handoff button when the client is in control.
 BASE_FEATURE(kGlicHandoffButtonHiddenClientControl,
