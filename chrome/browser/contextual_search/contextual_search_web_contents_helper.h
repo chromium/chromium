70f34bd3dc991e4c2cc74ca4881ec04ff6b92b27
Justin Donnelly <jdonnelly@chromium.org>
2025-11-12 20:44:53 -0800

    [143][context-input] Split ContextualSessionService into multiple files
    There will be more changes that affect the SessionHandle coming up, so
    having it in its own file will help organize the code.
    
    (cherry picked from commit c86d195a17803c506db320ddb6d6d0aa78ecdc59)
    
    Bug: b:457425602, 460125833
    Change-Id: Ie1e51603323287ac577a43a61f825b495ea03b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7112930
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1540264}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7148319
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/branch-heads/7499@{#1578}
    Cr-Branched-From: b30439823e5177773584139e72e0593e36863899-refs/heads/main@{#1536371}

--- chrome/browser/contextual_search/contextual_search_web_contents_helper.h
+++ chrome/browser/contextual_search/contextual_search_web_contents_helper.h
@@ -7,14 +7,14 @@
 
 #include <memory>
 
-#include "components/contextual_search/contextual_search_service.h"
+#include "components/contextual_search/contextual_search_session_handle.h"
 #include "content/public/browser/web_contents_user_data.h"
 
 namespace content {
 class WebContents;
 }
 
-// Helper class that scopes the `ContextualSearchService::SessionHandle`'s
+// Helper class that scopes the `ContextualSearchSessionHandle`'s
 // lifetime to a `content::WebContents`.
 class ContextualSearchWebContentsHelper
     : public content::WebContentsUserData<ContextualSearchWebContentsHelper> {
@@ -27,13 +27,12 @@ class ContextualSearchWebContentsHelper
 
   // Takes ownership of a contextual session handle.
   void set_session_handle(
-      std::unique_ptr<contextual_search::ContextualSearchService::SessionHandle>
+      std::unique_ptr<contextual_search::ContextualSearchSessionHandle>
           handle) {
     session_handle_ = std::move(handle);
   }
   // Returns the owned contextual session handle. May return nullptr.
-  contextual_search::ContextualSearchService::SessionHandle* session_handle()
-      const {
+  contextual_search::ContextualSearchSessionHandle* session_handle() const {
     return session_handle_.get();
   }
 
@@ -42,7 +41,7 @@ class ContextualSearchWebContentsHelper
       content::WebContents* web_contents);
   friend class content::WebContentsUserData<ContextualSearchWebContentsHelper>;
 
-  std::unique_ptr<contextual_search::ContextualSearchService::SessionHandle>
+  std::unique_ptr<contextual_search::ContextualSearchSessionHandle>
       session_handle_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
