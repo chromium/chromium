c4fe9ee4a764f54bf2e73ae8f46360068cf840e8
Nihar Majmudar <niharm@google.com>
2025-11-13 21:32:10 -0800

    [composebox] Move composebox handler to searchbox directory.
    CL is precursor to creating a new OmniboxComposebox handler which
    will extend this class (cannot currently do this without causing a
    circular dependency between the ntp directory and searchbox directory).
    
    Overall, the handler also just makes more sense in this file since
    this is where most other searchbox components exist.
    
    Change-Id: I3c0fe679aefa400496a9d8a0a441f003a5f66d93
    Bug: 458054953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153218
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1544741}

--- chrome/browser/ui/webui/searchbox/composebox_handler.cc
+++ chrome/browser/ui/webui/searchbox/composebox_handler.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/webui/new_tab_page/composebox/composebox_handler.h"
+#include "chrome/browser/ui/webui/searchbox/composebox_handler.h"
 
 #include <optional>
 #include <string>
