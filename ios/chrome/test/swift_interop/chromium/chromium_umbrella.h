54df01886a1fc6e41c2b0ebde912363e183fd067
Justin Novosad <junov@chromium.org>
2025-11-14 08:36:47 -0800

    Test that stuff from base/ can be called by swift code.
    This CL establishes a viable pattern for importing C++ code from
    chromium into swift.
    
    Due to a Swift compiler bug that is fixed in Swift 6.3, there is a
    temporary workaround, which is explained in crbug.com/458741717
    
    The swift_source_set GN template was modified in order to reduce the
    gn boilerplate that is necessary for integrating modulemaps.
    
    A new Swift XCTest target, ios/chrome/test/swift_interop/chromium:tests,
    has been added. This target includes:
     - chromium.modulemap: A modulemap for the Base module.
     - chromium_umbrella.h: An umbrella header to expose
       base/system/sys_info.h to Swift, addressing include path resolution
       issues with modulemaps.
     - sys_info_xctest.swift: A Swift test case to verify the
       GetIOSBuildNumber() method from base.SysInfo.
    
    Bug: 452365528
    Change-Id: Ide4ddf022d01fd6f000f165b94b37c9090f93197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7115760
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545002}

---
+++ ios/chrome/test/swift_interop/chromium/chromium_umbrella.h
@@ -0,0 +1,12 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_TEST_SWIFT_INTEROP_CHROMIUM_CHROMIUM_UMBRELLA_H_
+#define IOS_CHROME_TEST_SWIFT_INTEROP_CHROMIUM_CHROMIUM_UMBRELLA_H_
+
+// TODO(crbug.com/458741717): Remove this file once on swift 6.3
+
+#include "base/system/sys_info.h"
+
+#endif  // IOS_CHROME_TEST_SWIFT_INTEROP_CHROMIUM_CHROMIUM_UMBRELLA_H_
