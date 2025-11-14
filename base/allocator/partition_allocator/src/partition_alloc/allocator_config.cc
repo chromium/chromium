661466bf107906cb8564a988d9f562b37a2d15b7
Takashi Sakamoto <tasak@google.com>
2025-11-14 01:34:53 -0800

    [M143] [PA] Ramp up External Metadata trial to 25% on canary
    Original change's description:
    > [PA] Ramp up External Metadata trial to 25% on canary
    >
    > Bug: crbug.com/435341877
    > Change-Id: I18a3c33d4646045836de99267fa6e6f74b02c80e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7139661
    > Commit-Queue: Takashi Sakamoto <tasak@google.com>
    > Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1543513}
    
    (cherry picked from commit 3c8379fd7efbcaa1f00f5d57e1af2b5866dd5700)
    
    Bug: 459996600, crbug.com/435341877
    Change-Id: I330f9cfe75048fb1581dfd52bb6622e70fb824ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7156168
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7499@{#1741}
    Cr-Branched-From: b30439823e5177773584139e72e0593e36863899-refs/heads/main@{#1536371}

--- base/allocator/partition_allocator/src/partition_alloc/allocator_config.cc
+++ base/allocator/partition_allocator/src/partition_alloc/allocator_config.cc
@@ -15,8 +15,8 @@ namespace partition_alloc {
 namespace {
 
 enum ExternalMetadataTrialGroupPercentage {
-  kEnabled = 1,   // 1% enabled
-  kDisabled = 1,  // 1% control
+  kEnabled = 25,   // 25% enabled
+  kDisabled = 25,  // 25% control
   // Otherwise, disabled (default behavior)
 };
 // Rather than doing percentage group based assignment, set all clients to
