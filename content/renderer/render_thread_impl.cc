262557333c83960c204eb6c0eb39de3c9ee6ffd3
Etienne Pierre-doray <etiennep@chromium.org>
2025-11-14 10:53:51 -0800

    [tab switch] Avoid unnecessary isolate priority updates
    Outside of kIsolatesPriorityUseProcessPriority feature,
    SetIsolatePriority was only called on transitions.
    With kIsolatesPriorityUseProcessPriority, this CL avoids
    calling SetIsolatePriority if the value didn't change.
    
    Bug: 456731897
    Change-Id: I57acc639b7dc9351ffc83fe83bbb79d1d9c3ddba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152084
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1545112}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1281,7 +1281,8 @@ void RenderThreadImpl::SetProcessState(
             features::kIsolatesPriorityBestEffortWhenHidden)) {
       blink::WebV8Features::SetIsolatePriority(
           base::Process::Priority::kBestEffort);
-    } else {
+    } else if (!process_priority_.has_value() ||
+               *process_priority_ != process_priority) {
       blink::WebV8Features::SetIsolatePriority(process_priority);
     }
   }
