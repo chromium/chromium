c3e769440ba9434d3aab5eeb25bc157742e2419c
Mahmoud Rashad <mmrashad@google.com>
2025-11-14 01:12:41 -0800

    [SyncTest] Override GetSetupSyncMode() for SingleClientWorkspaceDeskSy.*
    This file is ChromeOS specific, where there's only Sync-the-feature.
    
    Bug: 353425612
    Change-Id: I13cffba4041ef9cc10a7c668e4e9b8cfc784c904
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153639
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1544801}

--- chrome/browser/sync/test/integration/single_client_workspace_desk_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_workspace_desk_sync_test.cc
@@ -72,6 +72,11 @@ class SingleClientWorkspaceDeskSyncTest : public SyncTest {
       const SingleClientWorkspaceDeskSyncTest&) = delete;
   ~SingleClientWorkspaceDeskSyncTest() override = default;
 
+  // This test suite is ChromeOS specific, where there's only Sync-the-feature.
+  SyncTest::SetupSyncMode GetSetupSyncMode() const override {
+    return SetupSyncMode::kSyncTheFeature;
+  }
+
   base::Time AdvanceAndGetTime(base::TimeDelta delta = base::Milliseconds(10)) {
     clock_.Advance(delta);
     return clock_.Now();
