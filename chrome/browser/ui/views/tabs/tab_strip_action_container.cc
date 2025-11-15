20a81fe7a0d8a928d8bd2e77c43b5ec7f7fea412
Jacob Stanley <jacobstanley@google.com>
2025-11-14 19:25:58 -0800

    [M143] [Task Nudge] Remove spacing between GlicButton and GlicActorTaskIcon
    Original change's description:
    > [Task Nudge] Remove spacing between GlicButton and GlicActorTaskIcon
    > 
    > This is gated behind the GlicActorUiNudgeRedesign feature.
    > 
    > Screenshots:
    > Highlighted GlicButton: https://screenshot.googleplex.com/745spaecwiVsJAU
    > Unhighlighted GlicButton: https://screenshot.googleplex.com/5DB4MhqrJagWsvb
    > 
    > Fixed: 460470195
    > Change-Id: I49752a6e93b6d6ccdedaf19991990d043eef6cae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7153500
    > Reviewed-by: Abe Boujane <boujane@google.com>
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1544540}
    
    (cherry picked from commit f2a055995f3f5d256a2d26ca9603e2a9b84ce577)
    
    Bug: 460774172,460470195
    Change-Id: I49752a6e93b6d6ccdedaf19991990d043eef6cae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7159348
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7499@{#1845}
    Cr-Branched-From: b30439823e5177773584139e72e0593e36863899-refs/heads/main@{#1536371}

--- chrome/browser/ui/views/tabs/tab_strip_action_container.cc
+++ chrome/browser/ui/views/tabs/tab_strip_action_container.cc
@@ -472,14 +472,18 @@ TabStripActionContainer::CreateGlicActorButtonContainer() {
 void TabStripActionContainer::UpdateGlicActorButtonContainerBorders() {
   CHECK(glic_button_);
   gfx::Insets glic_border;
+  const bool is_redesign_enabled =
+      base::FeatureList::IsEnabled(features::kGlicActorUiNudgeRedesign);
   // GlicActorTaskIcon will only ever be shown alongside the GlicButton.
   if (glic_actor_task_icon_ && glic_actor_task_icon_->IsDrawn()) {
     gfx::Insets task_icon_border;
     const gfx::Insets right_icon_border = gfx::Insets().set_left_right(
-        kInsideBorderAroundGlicButtons, kOutsideBorderAroundGlicButtons);
+        is_redesign_enabled ? 0 : kInsideBorderAroundGlicButtons,
+        kOutsideBorderAroundGlicButtons);
     const gfx::Insets left_icon_border = gfx::Insets().set_left_right(
-        kOutsideBorderAroundGlicButtons, kInsideBorderAroundGlicButtons);
-    if (base::FeatureList::IsEnabled(features::kGlicActorUiNudgeRedesign)) {
+        kOutsideBorderAroundGlicButtons,
+        is_redesign_enabled ? 0 : kInsideBorderAroundGlicButtons);
+    if (is_redesign_enabled) {
       task_icon_border = right_icon_border + border_insets_;
       // If the GlicActorTaskIcon is also present, adjust the border on the
       // GlicButton to allow the two buttons to sit closer together.
@@ -490,7 +494,7 @@ void TabStripActionContainer::UpdateGlicActorButtonContainerBorders() {
     }
     glic_actor_task_icon_->SetBorder(
         views::CreateEmptyBorder(task_icon_border));
-    if (base::FeatureList::IsEnabled(features::kGlicActorUiNudgeRedesign)) {
+    if (is_redesign_enabled) {
       // Force a background repaint to account for the new border insets.
       glic_actor_task_icon_->RefreshBackground();
     }
@@ -501,7 +505,7 @@ void TabStripActionContainer::UpdateGlicActorButtonContainerBorders() {
                   border_insets_;
   }
   glic_button_->SetBorder(views::CreateEmptyBorder(glic_border));
-  if (base::FeatureList::IsEnabled(features::kGlicActorUiNudgeRedesign)) {
+  if (is_redesign_enabled) {
     // Force a background repaint to account for the new border insets.
     glic_button_->RefreshBackground();
   }
