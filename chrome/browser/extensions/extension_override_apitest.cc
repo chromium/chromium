888b2658d9fb5a51d9a42a1668e61b43969f20de
Tim Judkins <tjudkins@chromium.org>
2025-11-18 10:01:39 -0800

    [Extensions] Reenable history override test for windows
    This test was disabled for constantly timing out on Windows bots ~16
    years ago in crbug.com/41153714, but it seems fine to reenable now.
    
    Test was run for 3000 iterations on the bots without any failures (see
    patchset 1).
    
    Fixed: 41153714
    Change-Id: I592f0c8aa4656f76b4f4844d5991be2e6b1fc896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7164339
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546640}

--- chrome/browser/extensions/extension_override_apitest.cc
+++ chrome/browser/extensions/extension_override_apitest.cc
@@ -285,14 +285,7 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_TRUE(contents->GetRenderWidgetHostView()->HasFocus());
 }
 
-// Times out consistently on Win, http://crbug.com/45173.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_OverrideHistory DISABLED_OverrideHistory
-#else
-#define MAYBE_OverrideHistory OverrideHistory
-#endif  // BUILDFLAG(IS_WIN)
-
-IN_PROC_BROWSER_TEST_F(ExtensionOverrideTest, MAYBE_OverrideHistory) {
+IN_PROC_BROWSER_TEST_F(ExtensionOverrideTest, OverrideHistory) {
   ASSERT_TRUE(RunExtensionTest("override/history")) << message_;
   {
     ResultCatcher catcher;
