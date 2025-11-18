64a7b0dec7c17d7e7e240349d24b4f4b504dd2d4
Jason Hu <hujasonx@google.com>
2025-11-18 10:04:57 -0800

    [context-input] Make ContextualTasksComposebox use shared base class
    This change makes it so that most code is shared with the other
    composeboxes, matching OmniboxComposeboxHandler
    
    Currently, issuing a query opens a search page in a new tab. Followup
    work is needed to connect the searchbox with the embedded webpage.
    
    Bug: b:461621346
    Change-Id: Id9492c511c9e59b3a4d9231bd869b79ecdf2905d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7165438
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1546643}

--- chrome/browser/contextual_tasks/contextual_tasks_composebox_handler.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_composebox_handler.cc
@@ -3,11 +3,10 @@
 // found in the LICENSE file.
 #include "chrome/browser/contextual_tasks/contextual_tasks_composebox_handler.h"
 
-#include "base/notreached.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/omnibox/omnibox_controller.h"
+#include "chrome/browser/ui/webui/searchbox/composebox_handler.h"
 #include "chrome/browser/ui/webui/searchbox/searchbox_omnibox_client.h"
-#include "ui/base/window_open_disposition_utils.h"
 
 namespace {
 
@@ -46,96 +45,14 @@ ContextualTasksComposeboxHandler::ContextualTasksComposeboxHandler(
     mojo::PendingRemote<composebox::mojom::Page> pending_page,
     mojo::PendingReceiver<searchbox::mojom::PageHandler>
         pending_searchbox_handler)
-    : SearchboxHandler(
+    : ComposeboxHandler(
+          std::move(pending_handler),
+          std::move(pending_page),
           std::move(pending_searchbox_handler),
           profile,
           web_contents,
           std::make_unique<OmniboxController>(
               std::make_unique<ContextualTasksOmniboxClient>(profile,
-                                                             web_contents))),
-      page_{std::move(pending_page)},
-      handler_(this, std::move(pending_handler)) {
-  autocomplete_controller_observation_.Observe(autocomplete_controller());
-}
+                                                             web_contents))) {}
 
 ContextualTasksComposeboxHandler::~ContextualTasksComposeboxHandler() = default;
-
-void ContextualTasksComposeboxHandler::NotifySessionStarted() {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::NotifySessionAbandoned() {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::SubmitQuery(
-    const std::string& query_text,
-    uint8_t mouse_button,
-    bool alt_key,
-    bool ctrl_key,
-    bool meta_key,
-    bool shift_key) {}
-
-void ContextualTasksComposeboxHandler::AddFileContext(
-    searchbox::mojom::SelectedFileInfoPtr file_info,
-    mojo_base::BigBuffer file_bytes,
-    AddFileContextCallback callback) {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::AddTabContext(int32_t tab_id,
-                                                     bool delay_upload,
-                                                     AddTabContextCallback) {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::DeleteContext(
-    const base::UnguessableToken& file_token) {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::ClearFiles() {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::DeleteAutocompleteMatch(
-    uint8_t line,
-    const GURL& url) {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::ExecuteAction(
-    uint8_t line,
-    uint8_t action_index,
-    const GURL& url,
-    base::TimeTicks match_selection_timestamp,
-    uint8_t mouse_button,
-    bool alt_key,
-    bool ctrl_key,
-    bool meta_key,
-    bool shift_key) {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::OnThumbnailRemoved() {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::FocusChanged(bool focused) {
-  // noop.
-}
-
-void ContextualTasksComposeboxHandler::SetDeepSearchMode(bool enabled) {
-  // Ignore, intentionally unimplemented for Lens. Deep search not implemented
-  // in Lens.
-}
-
-void ContextualTasksComposeboxHandler::SetCreateImageMode(bool enabled,
-                                                          bool image_present) {
-  // Ignore, intentionally unimplemented for Lens. Create image not implemented
-  // in Lens.
-}
-
-void ContextualTasksComposeboxHandler::HandleLensButtonClick() {
-  // noop
-}
