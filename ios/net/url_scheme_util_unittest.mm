e41f3542087362715f0d1d838ae6f3b0d9d114de
Sylvain Defresne <sdefresne@chromium.org>
2025-11-18 14:51:06 -0800

    [ios] Fix unsafe buffer in URLSchemeUtilTest
    Use std::to_array<T>(...) to define constant arrays and use range
    based for loop to perform the iteration with accessing arrays by
    indexes.
    
    Bug: 40285824
    Change-Id: I9c1f93c7440aad8ecbf7ece0f582a1ad3827c9f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7167231
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1546824}

--- ios/net/url_scheme_util_unittest.mm
+++ ios/net/url_scheme_util_unittest.mm
@@ -2,33 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #import "ios/net/url_scheme_util.h"
 
 #import <Foundation/Foundation.h>
 
+#import <array>
+#import <string_view>
+
+#import "base/strings/sys_string_conversions.h"
 #import "testing/gtest_mac.h"
 #import "testing/platform_test.h"
 #import "url/gurl.h"
 
 namespace net {
-
-const char* kSchemeTestData[] = {
-    "http://foo.com", "https://foo.com",   "data:text/html;charset=utf-8,Hello",
-    "about:blank",    "chrome://settings",
-};
+namespace {
+constexpr auto kSchemeTestData = std::to_array<std::string_view>({
+    "http://foo.com",
+    "https://foo.com",
+    "data:text/html;charset=utf-8,Hello",
+    "about:blank",
+    "chrome://settings",
+});
+}  // anonymous namespace
 
 using URLSchemeUtilTest = PlatformTest;
 
 TEST_F(URLSchemeUtilTest, NSURLHasDataScheme) {
-  for (unsigned int i = 0; i < std::size(kSchemeTestData); ++i) {
-    const char* url = kSchemeTestData[i];
-    bool nsurl_result = UrlHasDataScheme(
-        [NSURL URLWithString:[NSString stringWithUTF8String:url]]);
+  for (std::string_view url : kSchemeTestData) {
+    bool nsurl_result =
+        UrlHasDataScheme([NSURL URLWithString:base::SysUTF8ToNSString(url)]);
     bool gurl_result = GURL(url).SchemeIs(url::kDataScheme);
     EXPECT_EQ(gurl_result, nsurl_result) << "Scheme check failed for " << url;
   }
