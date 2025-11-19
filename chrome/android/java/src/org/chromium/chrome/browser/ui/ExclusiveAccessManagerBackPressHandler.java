206b0509cc4c9d2a499a4d4152b71fe1ba47f2ef
Wojciech Zwonarz <wzwonarz@google.com>
2025-11-19 05:00:59 -0800

    Exclusive Access Manager Back Handler does not intercept Esc key
    The Esc key should not trigger back press action as the key should be
    handled by the separate key event logic.
    
    Bug: 461715539
    
    Change-Id: I054925b95949512cb1ed5e54c22b49e57e1c6635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7159099
    Auto-Submit: Wojciech Zwonarz <wzwonarz@google.com>
    Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    Reviewed-by: Abdelrahman Eed <abdoeed@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547114}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessManagerBackPressHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/ExclusiveAccessManagerBackPressHandler.java
@@ -6,6 +6,7 @@ package org.chromium.chrome.browser.ui;
 
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.components.browser_ui.widget.gesture.BackPressHandler;
 
 /**
@@ -28,6 +29,19 @@ public class ExclusiveAccessManagerBackPressHandler implements BackPressHandler 
                         : BackPressResult.FAILURE;
         mExclusiveAccessManager.exitExclusiveAccess();
         return res;
+    }
+
+    @Override
+    public boolean invokeBackActionOnEscape() {
+        // Back press and Esc press should be handled separately as Esc key handling has additional
+        // logic in ExclusiveAccessManager native PreHandleKeyboardEvent function.
+        return false;
+    }
+
+    @Override
+    public @Nullable Boolean handleEscPress() {
+        // Not handling Escape here to let it go to the native part for long key press handling
+        return Boolean.FALSE;
     }
 
     @Override
