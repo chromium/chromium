96dd307e6d1c699895aa9668ea1e9911814e0bd4
Ryan Thomas <rathomas@google.com>
2025-11-19 12:48:04 -0800

    Reverts `AutoResetCtaTransitTestRule` to `FreshCtaTransitTestRule`.
    Bug: 439491767
    Change-Id: Iecef86488df5b2e22de6cf74c4557975f8fe15e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7170279
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Ryan Thomas <rathomas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1547415}

--- chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoProfileDestroyerIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/incognito/IncognitoProfileDestroyerIntegrationTest.java
@@ -37,8 +37,8 @@ import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabClosureParams;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
-import org.chromium.chrome.test.transit.AutoResetCtaTransitTestRule;
 import org.chromium.chrome.test.transit.ChromeTransitTestRules;
+import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
 import org.chromium.chrome.test.transit.ntp.IncognitoNewTabPageStation;
 import org.chromium.chrome.test.transit.page.WebPageStation;
 
@@ -51,8 +51,8 @@ public class IncognitoProfileDestroyerIntegrationTest {
     @Rule public final MockitoRule mMockitoRule = MockitoJUnit.rule();
 
     @Rule
-    public AutoResetCtaTransitTestRule mCtaTestRule =
-            ChromeTransitTestRules.autoResetCtaActivityRule();
+    public FreshCtaTransitTestRule mCtaTestRule =
+            ChromeTransitTestRules.freshChromeTabbedActivityRule();
 
     private TabModel mIncognitoTabModel;
     private IncognitoNewTabPageStation mIncognitoNtp;
