5f8502ceb18dda95ceeff8759609a4d8406c1899
Olga Sharonova <olka@chromium.org>
2025-11-19 02:38:09 -0800

    [BreakoutBox] FIFO traces
    Bug: 460102252
    
    Change-Id: I5c9c8b012e48264705c9bb5319fc19475227f8e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7170540
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Auto-Submit: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1547068}

--- third_party/blink/renderer/modules/breakout_box/frame_queue.h
+++ third_party/blink/renderer/modules/breakout_box/frame_queue.h
@@ -8,6 +8,8 @@
 #include <optional>
 
 #include "base/synchronization/lock.h"
+#include "base/trace_event/trace_event.h"
+#include "third_party/blink/renderer/platform/scheduler/common/tracing_helper.h"
 #include "third_party/blink/renderer/platform/wtf/deque.h"
 #include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
 
@@ -23,6 +25,8 @@ class FrameQueue : public ThreadSafeRefCounted<FrameQueue<NativeFrameType>> {
   base::Lock& GetLock() { return lock_; }
 
   std::optional<NativeFrameType> Push(NativeFrameType frame) {
+    TRACE_EVENT_INSTANT("mediastream", "FrameQueue::Push", "this",
+                        static_cast<void*>(this));
     base::AutoLock locker_(GetLock());
     return PushLocked(std::move(frame));
   }
@@ -30,13 +34,21 @@ class FrameQueue : public ThreadSafeRefCounted<FrameQueue<NativeFrameType>> {
   std::optional<NativeFrameType> PushLocked(NativeFrameType frame)
       EXCLUSIVE_LOCKS_REQUIRED(GetLock()) {
     std::optional<NativeFrameType> ret;
-    if (queue_.size() == max_size_)
+    if (queue_.size() == max_size_) {
+      TRACE_EVENT_INSTANT("mediastream", "FrameQueue::Push no space left",
+                          "max_size_", max_size_);
       ret = queue_.TakeFirst();
+    }
     queue_.push_back(std::move(frame));
+    TRACE_COUNTER("mediastream",
+                  scheduler::MakeCounterTrack("FrameQueue", this),
+                  queue_.size());
     return ret;
   }
 
   std::optional<NativeFrameType> Pop() {
+    TRACE_EVENT_INSTANT("mediastream", "FrameQueue::Pop", "this",
+                        static_cast<void*>(this));
     base::AutoLock locker_(GetLock());
     return PopLocked();
   }
