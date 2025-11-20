b077d10686c9016a649f962998c41433e16ae9ce
Divyansh Mangal <dmangal@microsoft.com>
2025-11-19 21:17:54 -0800

    [SVG] Add UseCounter for getBBox when when width or height is zero
    This change introduces a UseCounter metric for calls to
    `SVGGraphicsElement.getBBox()` where the element’s width or height
    attribute is zero. Such cases typically disable rendering for
    certain SVG elements, including:
    <rect>, <foreignObject>, <image> and <svg> (nested)
    
    The implementation ensures accurate counting by checking bounding
    box validity during `getBBox()` calls rather than layout time,
    using logic similar to `HasValidBoundingBoxForContainer()`.
    It also handles non-rendered elements via `IsNonRendered` checks.
    
    Tests have been added for known cases to validate behavior of the
    UseCounter.
    
    Bug: 460180379
    Change-Id: I901d7d80fcf7299f63f69d094dc7e153f0c8bbbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7142239
    Commit-Queue: Divyansh Mangal <dmangal@microsoft.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Vinay Singh <vinaysingh@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1547625}

---
+++ third_party/blink/renderer/core/layout/svg/layout_svg_rect_test.cc
@@ -0,0 +1,88 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/core/layout/svg/layout_svg_rect.h"
+
+#include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-blink.h"
+#include "third_party/blink/renderer/core/svg/svg_graphics_element.h"
+#include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
+#include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
+
+namespace blink {
+
+class LayoutSVGRectTest : public RenderingTest {};
+
+TEST_F(LayoutSVGRectTest, GetBBoxUseCounterForZeroHeight) {
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForElementWithZeroWidthOrHeight));
+
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <rect id="rect" width="100" height="0"/>
+    </svg>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  To<SVGGraphicsElement>(GetElementById("rect"))->GetBBox();
+
+  EXPECT_TRUE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForElementWithZeroWidthOrHeight));
+}
+
+TEST_F(LayoutSVGRectTest, GetBBoxUseCounterForZeroWidth) {
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForElementWithZeroWidthOrHeight));
+
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <rect id="rect" width="0" height="100"/>
+    </svg>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  To<SVGGraphicsElement>(GetElementById("rect"))->GetBBox();
+
+  EXPECT_TRUE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForElementWithZeroWidthOrHeight));
+}
+
+TEST_F(LayoutSVGRectTest, GetBBoxUseCounterForZeroWidthAndHeight) {
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForElementWithZeroWidthOrHeight));
+
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <rect id="rect"/>
+    </svg>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  To<SVGGraphicsElement>(GetElementById("rect"))->GetBBox();
+
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForElementWithZeroWidthOrHeight));
+}
+
+TEST_F(LayoutSVGRectTest, GetBBoxUseCounterForNonZeroWidthAndHeight) {
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForElementWithZeroWidthOrHeight));
+
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <rect id="rect" width="100" height="100"/>
+    </svg>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  To<SVGGraphicsElement>(GetElementById("rect"))->GetBBox();
+
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForElementWithZeroWidthOrHeight));
+}
+
+}  // namespace blink
--- third_party/blink/renderer/core/layout/svg/layout_svg_viewport_container_test.cc
+++ third_party/blink/renderer/core/layout/svg/layout_svg_viewport_container_test.cc
@@ -5,6 +5,7 @@
 #include "third_party/blink/renderer/core/layout/svg/layout_svg_viewport_container.h"
 
 #include "third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom-blink.h"
+#include "third_party/blink/renderer/core/svg/svg_graphics_element.h"
 #include "third_party/blink/renderer/core/testing/core_unit_test_helper.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 
@@ -71,6 +72,87 @@ TEST_F(LayoutSVGViewportContainerTest, NestedSvgCssSizingPropertiesUseCounterNot
   )HTML");
 
   EXPECT_FALSE(GetDocument().IsUseCounted(WebFeature::kNestedSvgCssSizingProperties));
+}
+
+TEST_F(LayoutSVGViewportContainerTest, GetBBoxUseCounterForZeroHeight) {
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForNestedSVGElementWithZeroWidthOrHeight));
+
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <svg id="svg" width="100" height="0">
+        <rect width="100" height="100"/>
+      </svg>
+    </svg>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  To<SVGGraphicsElement>(GetElementById("svg"))->GetBBox();
+
+  EXPECT_TRUE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForNestedSVGElementWithZeroWidthOrHeight));
+}
+
+TEST_F(LayoutSVGViewportContainerTest, GetBBoxUseCounterForZeroWidth) {
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForNestedSVGElementWithZeroWidthOrHeight));
+
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <svg id="svg" width="0" height="100">
+        <rect width="100" height="100"/>
+      </svg>
+    </svg>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  To<SVGGraphicsElement>(GetElementById("svg"))->GetBBox();
+
+  EXPECT_TRUE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForNestedSVGElementWithZeroWidthOrHeight));
+}
+
+TEST_F(LayoutSVGViewportContainerTest, GetBBoxUseCounterForZeroWidthAndHeight) {
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForNestedSVGElementWithZeroWidthOrHeight));
+
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <svg id="svg" width="0" height="0">
+        <rect width="100" height="100"/>
+      </svg>
+    </svg>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  To<SVGGraphicsElement>(GetElementById("svg"))->GetBBox();
+
+  EXPECT_TRUE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForNestedSVGElementWithZeroWidthOrHeight));
+}
+
+TEST_F(LayoutSVGViewportContainerTest,
+       GetBBoxUseCounterForNonZeroWidthAndHeight) {
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForNestedSVGElementWithZeroWidthOrHeight));
+
+  SetBodyInnerHTML(R"HTML(
+    <svg>
+      <svg id="svg" width="100" height="100">
+        <rect width="100" height="100"/>
+      </svg>
+    </svg>
+  )HTML");
+
+  UpdateAllLifecyclePhasesForTest();
+
+  To<SVGGraphicsElement>(GetElementById("svg"))->GetBBox();
+
+  EXPECT_FALSE(GetDocument().IsUseCounted(
+      WebFeature::kGetBBoxForNestedSVGElementWithZeroWidthOrHeight));
 }
 
 }  // namespace blink
