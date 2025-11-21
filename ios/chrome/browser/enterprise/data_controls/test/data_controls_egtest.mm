54b6bddf4b69bff1d023ea5b57141bb62ad6392d
Yixi Wu <yixiwu@google.com>
2025-11-21 07:28:40 -0800

    [iOS] Add EG Test for Copy/Paste Protection in Reader Mode
    Add EG Tests for copy/paste protection to test when policy is set to
    block and warn, the correct message or popup is displayed.
    
    Bug: 457399846
    Change-Id: Ie2ed97efabce87f203505f2255e0956c3a8d31e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7180500
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Xu Li <mxlg@google.com>
    Commit-Queue: Yixi Wu <yixiwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1548471}

--- ios/chrome/browser/enterprise/data_controls/test/data_controls_egtest.mm
+++ ios/chrome/browser/enterprise/data_controls/test/data_controls_egtest.mm
@@ -2,11 +2,50 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/enterprise/data_controls/test/data_controls_app_interface.h"
+#import "ios/chrome/browser/reader_mode/model/features.h"
+#import "ios/chrome/browser/reader_mode/ui/constants.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
+#import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/components/enterprise/data_controls/features.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
+#import "ios/web/public/test/element_selector.h"
+#import "net/test/embedded_test_server/default_handlers.h"
+#import "ui/base/l10n/l10n_util.h"
+
+using chrome_test_util::ContextMenuItemWithAccessibilityLabelId;
+
+namespace {
+
+// Path to a page compatible with reader mode.
+const char kReaderModeArticlePath[] = "/article.html";
+
+// URL to a page with a static message.
+const char kDestinationPageUrl[] = "/destination";
+
+// Returns an ElementSelector for long pressing the first link in the page.
+ElementSelector* ElementSelectorToLongPressLink() {
+  return [ElementSelector selectorWithCSSSelector:"a"];
+}
+
+// Matcher for the copy link button in the context menu.
+id<GREYMatcher> CopyLinkButton() {
+  return ContextMenuItemWithAccessibilityLabelId(
+      IDS_IOS_COPY_LINK_ACTION_TITLE);
+}
+
+// Taps on `context_menu_item_button` context menu item.
+void TapOnContextMenuButton(id<GREYMatcher> context_menu_item_button) {
+  [ChromeEarlGrey waitForUIElementToAppearWithMatcher:context_menu_item_button];
+  [[EarlGrey selectElementWithMatcher:context_menu_item_button]
+      performAction:grey_tap()];
+}
+
+}  // namespace
 
 @interface DataControlsTestCase : ChromeTestCase
 @end
@@ -15,22 +54,152 @@
 
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
+
+  if ([self isRunningTest:@selector(testCopyBlockedOnReaderMode)] ||
+      [self isRunningTest:@selector(testCopyLinkWarnProceedOnReaderMode)] ||
+      [self isRunningTest:@selector(testCopyLinkWarnCancelOnReaderMode)]) {
+    config.features_enabled.push_back(kEnableReaderMode);
+    config.features_enabled.push_back(kEnableReaderModeInUS);
+  }
   config.features_enabled.push_back(
       data_controls::kEnableClipboardDataControlsIOS);
+
   return config;
 }
 
 - (void)setUp {
   [super setUp];
+
+  GREYAssertTrue(self.testServer->Start(), @"Server did not start.");
 }
 
 - (void)tearDownHelper {
   [super tearDownHelper];
 }
 
+#pragma mark - Tests
+
 // Tests that copy is blocked when a "BLOCK" rule matches the page URL.
 - (void)DISABLED_testCopyBlocked {
   // TODO(crbug.com/457472925): This is a placeholder, update the tests.
+}
+
+// Tests that copying a link is blocked in reader mode when the DataControlsRule
+// policy is set to do so.
+- (void)testCopyBlockedOnReaderMode {
+  [DataControlsAppInterface setBlockCopyRule];
+
+  [ChromeEarlGrey clearPasteboard];
+  const GURL initialURL = self.testServer->GetURL(kReaderModeArticlePath);
+  [ChromeEarlGrey loadURL:initialURL];
+  [ChromeEarlGrey waitForPageToFinishLoading];
+
+  // Open Reader Mode UI.
+  GREYAssertTrue(
+      [ChromeEarlGrey showReaderModeAndWaitUntilReaderModeWebStateIsReady],
+      @"Reader mode content could not be loaded");
+
+  // Wait for Reader Mode UI to appear on-screen.
+  [ChromeEarlGrey
+      waitForSufficientlyVisibleElementWithMatcher:
+          grey_accessibilityID(kReaderModeViewAccessibilityIdentifier)];
+  [ChromeEarlGrey
+      waitForWebStateContainingElement:ElementSelectorToLongPressLink()];
+
+  // Open the context menu and tap on an action.
+  [ChromeEarlGreyUI longPressElementOnWebView:ElementSelectorToLongPressLink()];
+  TapOnContextMenuButton(CopyLinkButton());
+
+  // Check that the snackbar is shown.
+  id<GREYMatcher> snackbarMessage = grey_text(
+      l10n_util::GetNSString(IDS_POLICY_ACTION_BLOCKED_BY_ORGANIZATION));
+  [ChromeEarlGrey waitForUIElementToAppearWithMatcher:snackbarMessage];
+
+  // Check that the link was not copied.
+  GREYAssertTrue([ChromeEarlGrey pasteboardURL].is_empty(),
+                 @"Link should not have been copied");
+  [DataControlsAppInterface clearDataControlRules];
+}
+
+// Tests that copying a link is allowed in reader mode after the user proceeds
+// through the warning triggered by DataControlRules policy.
+- (void)testCopyLinkWarnProceedOnReaderMode {
+  [DataControlsAppInterface setWarnCopyRule];
+
+  [ChromeEarlGrey clearPasteboard];
+  const GURL initialURL = self.testServer->GetURL(kReaderModeArticlePath);
+  [ChromeEarlGrey loadURL:initialURL];
+  [ChromeEarlGrey waitForPageToFinishLoading];
+
+  // Open Reader Mode UI.
+  GREYAssertTrue(
+      [ChromeEarlGrey showReaderModeAndWaitUntilReaderModeWebStateIsReady],
+      @"Reader mode content could not be loaded");
+
+  // Wait for Reader Mode UI to appear on-screen.
+  [ChromeEarlGrey
+      waitForSufficientlyVisibleElementWithMatcher:
+          grey_accessibilityID(kReaderModeViewAccessibilityIdentifier)];
+  [ChromeEarlGrey
+      waitForWebStateContainingElement:ElementSelectorToLongPressLink()];
+
+  // Open the context menu and tap on an action.
+  [ChromeEarlGreyUI longPressElementOnWebView:ElementSelectorToLongPressLink()];
+  TapOnContextMenuButton(CopyLinkButton());
+
+  // Tap the "Copy anyways" button on the warning dialog.
+  [[EarlGrey selectElementWithMatcher:
+                 chrome_test_util::AlertItemWithAccessibilityLabelId(
+                     IDS_DATA_CONTROLS_COPY_WARN_CONTINUE_BUTTON)]
+      performAction:grey_tap()];
+
+  // Check that the link was copied.
+  const GURL destinationURL = self.testServer->GetURL(kDestinationPageUrl);
+  GREYCondition* copyCondition = [GREYCondition
+      conditionWithName:@"Link copied condition"
+                  block:^BOOL {
+                    return [ChromeEarlGrey pasteboardURL] == destinationURL;
+                  }];
+  GREYAssertTrue([copyCondition waitWithTimeout:5], @"Copying link failed");
+  [ChromeEarlGrey clearPasteboard];
+  [DataControlsAppInterface clearDataControlRules];
+}
+
+// Tests that copying a link is cancelled in reader mode when the user cancels
+// on the warning triggered by DataControlRules policy.
+- (void)testCopyLinkWarnCancelOnReaderMode {
+  [DataControlsAppInterface setWarnCopyRule];
+
+  [ChromeEarlGrey clearPasteboard];
+  const GURL initialURL = self.testServer->GetURL(kReaderModeArticlePath);
+  [ChromeEarlGrey loadURL:initialURL];
+  [ChromeEarlGrey waitForPageToFinishLoading];
+
+  // Open Reader Mode UI.
+  GREYAssertTrue(
+      [ChromeEarlGrey showReaderModeAndWaitUntilReaderModeWebStateIsReady],
+      @"Reader mode content could not be loaded");
+
+  // Wait for Reader Mode UI to appear on-screen.
+  [ChromeEarlGrey
+      waitForSufficientlyVisibleElementWithMatcher:
+          grey_accessibilityID(kReaderModeViewAccessibilityIdentifier)];
+  [ChromeEarlGrey
+      waitForWebStateContainingElement:ElementSelectorToLongPressLink()];
+
+  // Open the context menu and tap on an action.
+  [ChromeEarlGreyUI longPressElementOnWebView:ElementSelectorToLongPressLink()];
+  TapOnContextMenuButton(CopyLinkButton());
+
+  // Tap the "cancel" button on the warning dialog.
+  [[EarlGrey selectElementWithMatcher:
+                 chrome_test_util::AlertItemWithAccessibilityLabelId(
+                     IDS_DATA_CONTROLS_COPY_WARN_CANCEL_BUTTON)]
+      performAction:grey_tap()];
+  // Check that the link was not copied.
+  GREYAssertTrue([ChromeEarlGrey pasteboardURL].is_empty(),
+                 @"Link should not have been copied");
+  [DataControlsAppInterface clearDataControlRules];
 }
 
 @end
