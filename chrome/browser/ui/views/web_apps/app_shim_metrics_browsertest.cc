aaca7289c9699e76d500577a726f2b8d64883714
Marijn Kruisselbrink <mek@chromium.org>
2025-11-21 10:05:39 -0800

    Deflake AppShimMetricsTest.Basics
    Delaying BEST_EFFORT tasks made this test flaky, because the metrics
    code uses a BEST_EFFORT task runner to fetch histogram data from child
    processes. By changing the test from the sync to async version of
    MergeHistogramDeltasForTesting we can ensure that the relevant
    BEST_EFFORT task has ran before trying to verify that histograms
    were logged.
    
    Bug: 450134727
    Include-Ci-Only-Tests: chromium.mac:mac15-x64-rel-tests|browser_tests
    Change-Id: I4495006a197182094f1692a4677790f97b13706a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7181863
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1548578}

--- chrome/browser/ui/views/web_apps/app_shim_metrics_browsertest.cc
+++ chrome/browser/ui/views/web_apps/app_shim_metrics_browsertest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/test_future.h"
 #include "chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h"
 #include "components/metrics/content/subprocess_metrics_provider.h"
 #include "content/public/test/browser_test.h"
@@ -13,13 +14,20 @@ namespace {
 
 void FetchHistogramsFromChildProcesses() {
   content::FetchHistogramsFromChildProcesses();
-  metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
+  // Do an "async" merging if histogram deltas to make sure that any previously
+  // posted but not yet ran merge operations (such as those triggered by the app
+  // shim terminating) have also completed. A sync merge operation merely gets
+  // data from still existing subprocesses and does not ensure that data from
+  // already destroyed subpresses also gets merged.
+  base::test::TestFuture<void> future;
+  metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting(
+      /*async=*/true, future.GetCallback());
+  EXPECT_TRUE(future.Wait());
 }
 
 using AppShimMetricsTest = WebAppIntegrationTest;
 
-// TODO(crbug.com/450134727): Re-enable this test
-IN_PROC_BROWSER_TEST_F(AppShimMetricsTest, DISABLED_Basics) {
+IN_PROC_BROWSER_TEST_F(AppShimMetricsTest, Basics) {
   base::HistogramTester histogram_tester;
   helper_.InstallMenuOption(Site::kStandalone);
   helper_.CheckWindowCreated();
