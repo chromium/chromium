0e7f67576befc47dd2b3e787d7d69eb3401d454f
Gabrielle Lau <gabriellelau@google.com>
2025-11-21 12:51:13 -0800

    [3pp][cts] Add WebView CTS 16 to cts_archive CIPD for Android 16
    This is the first step to support Android 16 WebView CTS.
    
    Also updated the rest and extracted host-driven CTS 15 for V based on:
    https://source.android.com/docs/compatibility/cts/downloads
    
    Bug: 424259075
    Change-Id: I6b12ded2eadf62bc3fe45bf927d21d9848aa59ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7040460
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Gabrielle Lau <gabriellelau@google.com>
    Cr-Commit-Position: refs/heads/main@{#1548678}

--- android_webview/tools/cts_archive/3pp/fetch.py
+++ android_webview/tools/cts_archive/3pp/fetch.py
@@ -8,7 +8,7 @@ import json
 
 
 def do_latest():
-  print('2025.09.16')  # Update to current date when updating the URLs below
+  print('2025.11.17')  # Update to current date when updating the URLs below
 
 
 def get_download_url():
@@ -17,14 +17,16 @@ def get_download_url():
       "android-cts-10_r16-linux_x86-x86.zip",
       "android-cts-11_r16-linux_x86-arm.zip",
       "android-cts-11_r16-linux_x86-x86.zip",
-      "android-cts-12_r14-linux_x86-arm.zip",
-      "android-cts-12_r14-linux_x86-x86.zip",
-      "android-cts-13_r10-linux_x86-arm.zip",
-      "android-cts-13_r10-linux_x86-x86.zip",
-      "android-cts-14_r6-linux_x86-arm.zip",
-      "android-cts-14_r6-linux_x86-x86.zip",
-      "android-cts-15_r2-linux_x86-arm.zip",
-      "android-cts-15_r2-linux_x86-x86.zip",
+      "android-cts-12_r16-linux_x86-arm.zip",
+      "android-cts-12_r16-linux_x86-x86.zip",
+      "android-cts-13_r14-linux_x86-arm.zip",
+      "android-cts-13_r14-linux_x86-x86.zip",
+      "android-cts-14_r10-linux_x86-arm.zip",
+      "android-cts-14_r10-linux_x86-x86.zip",
+      "android-cts-15_r6-linux_x86-arm.zip",
+      "android-cts-15_r6-linux_x86-x86.zip",
+      "android-cts-16_r3-linux_x86-arm.zip",
+      "android-cts-16_r3-linux_x86-x86.zip",
   ]
   url_prefix = "https://dl.google.com/dl/android/cts/"
   urls = [url_prefix + f for f in filenames]
