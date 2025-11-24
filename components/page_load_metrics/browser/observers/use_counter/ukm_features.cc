99857d2fe05da75ad0bc8a9098e0e79837f28e0f
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2025-11-24 11:21:23 -0800

    LNA: Enable UKM for WebRTC Local Network Access use counters
    Enable UKM collection for the WebRTC Local Network Access UseCounters.
    This will help us understand the prevalence of these checks in the wild
    and reason about the impact of potential changes.
    
    Verified all three metrics are below 1% of page loads with
    the highest metric (Public To Loopback) being 0.9%.
    
    Bug: 421223919
    Change-Id: I436614e86030451db4865995a1c691c656053d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7168785
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549327}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -467,6 +467,9 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kDeviceBoundSessionRequestDeferral,
           WebFeature::kFedCmErrorCodeAttribute,
           WebFeature::kFedCmErrorErrorAttribute,
+          WebFeature::kWebRTCLocalNetworkAccessPublicToLocal,
+          WebFeature::kWebRTCLocalNetworkAccessPublicToLoopback,
+          WebFeature::kWebRTCLocalNetworkAccessLocalToLoopback,
           // NOTE: before adding new use counters here, verify in UMA that
           // their emissions are very rare, e.g. <1% of page loads.
       }));
