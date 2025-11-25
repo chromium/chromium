49172377d41c9db6f58f673edba53f90d3d2a9eb
Gazal <agazal@google.com>
2025-11-25 15:55:49 -0800

    [Settings containment] Hide dividers in LegalInformationSettings
    Bug: 433576895, 463686804
    Change-Id: I86bd5375c96da010a890b2c14cfce5debaa0f54d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7204063
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550132}

--- chrome/android/java/src/org/chromium/chrome/browser/about_settings/LegalInformationSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/about_settings/LegalInformationSettings.java
@@ -4,16 +4,14 @@
 
 package org.chromium.chrome.browser.about_settings;
 
-
 import android.os.Bundle;
-
-import androidx.preference.PreferenceFragmentCompat;
 
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
 import org.chromium.chrome.browser.settings.search.BaseSearchIndexProvider;
 import org.chromium.components.browser_ui.settings.EmbeddableSettingsPage;
 import org.chromium.components.browser_ui.settings.SettingsFragment;
@@ -21,7 +19,7 @@ import org.chromium.components.browser_ui.settings.SettingsUtils;
 
 /** Fragment to display legal information about Chrome. */
 @NullMarked
-public class LegalInformationSettings extends PreferenceFragmentCompat
+public class LegalInformationSettings extends ChromeBaseSettingsFragment
         implements EmbeddableSettingsPage {
     private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
 
