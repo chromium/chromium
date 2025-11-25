436ceffc72528ad9a9031d14486cbe10f15b0c35
Lei Zhang <thestig@chromium.org>
2025-11-25 14:39:13 -0800

    Add missing copyright to chromecast/cast_core/grpc/test_utils.*
    Fix a presubmit error.
    
    Change-Id: I5b510dc890583e6dcbe2fe35d6aa0e1b23f6bb40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7200287
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550081}

--- chromecast/cast_core/grpc/test_utils.cc
+++ chromecast/cast_core/grpc/test_utils.cc
@@ -1,3 +1,7 @@
+// Copyright 2022 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "chromecast/cast_core/grpc/test_utils.h"
 
 #include "base/functional/callback_forward.h"
