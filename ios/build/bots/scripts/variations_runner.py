30b17fb9a1d29555082213ba4e7e50bd5425b5c9
Arkadiusz Tomczak <artomczak@google.com>
2025-11-25 07:10:38 -0800

    Rollback VariationsSmokeTest name change.
    In https://chromium-review.googlesource.com/c/chromium/src/+/6771568
    we changed the name of the tests to include dot, to make the code
    parsing the test name work. With changes from
    https://chromium-review.googlesource.com/c/chromium/src/+/7187989
    we should no longer pass the fake dot.
    
    Change-Id: I654db606c408d27ab0ea581edcabdda42f3bf316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7191942
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Commit-Queue: Arkadiusz Tomczak <artomczak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1549750}

--- ios/build/bots/scripts/variations_runner.py
+++ ios/build/bots/scripts/variations_runner.py
@@ -200,10 +200,10 @@ class VariationsSimulatorParallelTestRunner(SimulatorParallelTestRunner):
     success, log = self._launch_variations_smoke_test()
 
     test_status = TestStatus.PASS if success else TestStatus.FAIL
-    # Report a single test named |Variations.SmokeTest| as part of runner
+    # Report a single test named |VariationsSmokeTest| as part of runner
     # output.
     overall_result = ResultCollection(test_results=[
-        TestResult('Variations.SmokeTest', test_status, test_log=log)
+        TestResult('VariationsSmokeTest', test_status, test_log=log)
     ])
     overall_result.report_to_result_sink()
     self.test_results = overall_result.standard_json_output(path_delimiter='/')
