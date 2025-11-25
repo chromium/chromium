chromium/chromium

commit 2f0aa4a649f4a43dccd82246559e16ddb89ff1c1

webrtc-internals: serialize RTCSessionDescription as actual JSON
instead of naive 'type: ..., sdp: ...'.

Bug: chromium:443112515
Change-Id: I72414f4d4a0207599bb4e2ed3049c2e8a2b371d3
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6917097
Reviewed-by: Henrik Boström <hbos@chromium.org>
Reviewed-by: Guido Urdaneta <guidou@chromium.org>
Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
Cr-Commit-Position: refs/heads/main@{#1511730}

content/browser/webrtc/resources/peer_connection_update_table.js
@@ -169,9 +169,9 @@ export class PeerConnectionUpdateTable {
       valueContainer.parentElement.classList.add('update-log-failure');
     }
 
-    // RTCSessionDescription is serialized as 'type: <type>, sdp:'
-    if (update.value.indexOf(', sdp:') !== -1) {
-      const [type, sdp] = update.value.substring(6).split(', sdp: ');
+    // RTCSessionDescription is serialized as JSON.
+    if (update.value.startsWith("{\"type\":")) {
+      const {type, sdp} = JSON.parse(update.value);
       if (type === 'rollback') {
         // Rollback has no SDP.
         summary.textContent += ' (type: "rollback")';

third_party/blink/renderer/modules/peerconnection/peer_connection_tracker.cc
@@ -35,6 +35,7 @@
 #include "third_party/blink/renderer/modules/mediastream/media_constraints.h"
 #include "third_party/blink/renderer/modules/mediastream/user_media_request.h"
 #include "third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.h"
+#include "third_party/blink/renderer/platform/json/json_values.h"
 #include "third_party/blink/renderer/platform/mediastream/media_stream_component.h"
 #include "third_party/blink/renderer/platform/mojo/mojo_binding_context.h"
 #include "third_party/blink/renderer/platform/peerconnection/rtc_answer_options_platform.h"
@@ -820,11 +821,17 @@ void PeerConnectionTracker::TrackSetSessionDescription(
   int id = GetLocalIDForHandler(pc_handler);
   if (id == -1)
     return;
-  String value = StrCat({"type: ", type, ", sdp: ", sdp});
+  auto json = std::make_unique<JSONObject>();
+  json->SetString("type", type);
+  if (!sdp.empty()) {
+    json->SetString("sdp", sdp);
+  }
+  StringBuilder value;
+  json->WriteJSON(&value);
   SendPeerConnectionUpdate(
       id,
       source == kSourceLocal ? "setLocalDescription" : "setRemoteDescription",
-      value);
+      value.ToString());
 }
 
 void PeerConnectionTracker::TrackSetSessionDescriptionImplicit(

third_party/blink/renderer/modules/peerconnection/rtc_peer_connection_handler.cc
@@ -185,13 +185,19 @@ class CreateSessionDescriptionRequest
 
     auto tracker = tracker_.Lock();
     if (tracker && handler_) {
-      std::string value;
+      StringBuilder result;
       if (desc) {
+        std::string value;
         desc->ToString(&value);
-        value = "type: " + desc->type() + ", sdp: " + value;
+        auto json = std::make_unique<JSONObject>();
+        json->SetString("type", String::FromUTF8(desc->type()));
+        if (!value.empty()) {
+          json->SetString("sdp", String::FromUTF8(value));
+        }
+        json->WriteJSON(&result);
       }
-      tracker->TrackSessionDescriptionCallback(
-          handler_.get(), action_, "OnSuccess", String::FromUTF8(value));
+      tracker->TrackSessionDescriptionCallback(handler_.get(), action_,
+                                               "OnSuccess", result.ToString());
       tracker->TrackSessionId(handler_.get(),
                               String::FromUTF8(desc->session_id()));
     }

commit 5fe2cdea060abc08c868a4c86cc5194a3e1d3e8c

webrtc-internals: fix SDP munging detection
which was broken after
  https://chromiumdash.appspot.com/commit/2f0aa4a649f4a43dccd82246559e16ddb89ff1c1
as this changed the format from JSON-lookalike (parsed by
split+substring) to actual JSON with more quotes.

Bug: chromium:462810887
Change-Id: I7f4546a02de152cac374d6595da940a5110f27f2
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7198186
Reviewed-by: Harald Alvestrand <hta@chromium.org>
Reviewed-by: Henrik Boström <hbos@chromium.org>
Commit-Queue: Philipp Hancke <philipp.hancke@googlemail.com>
Cr-Commit-Position: refs/heads/main@{#1549685}

content/browser/webrtc/resources/peer_connection_update_table.js
@@ -185,7 +185,7 @@ export class PeerConnectionUpdateTable {
         if (update.type === 'setLocalDescription' && lastOfferAnswer &&
             lastOfferAnswer !== update.value) {
           lastSections = SDPUtils.splitSections(
-              lastOfferAnswer.substring(6).split(', sdp: ')[1]);
+              JSON.parse(lastOfferAnswer).sdp);
         }
         // Fold the SDP sections.
         const sections = SDPUtils.splitSections(sdp);