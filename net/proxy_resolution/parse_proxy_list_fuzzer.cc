6e76fce80975a9effea52cd239e6043b186c1ce2
Maksim Moskvitin <mmoskvitin@google.com>
2025-11-25 07:14:45 -0800

    Fix unsafe buffer usage in parse_proxy_list_fuzzer
    Migrated from libfuzzer to FuzzTest, thus no need for UNSAFE_BUFFER
    anymore.
    
    Fixed: 454827865
    Change-Id: Ia8facf6a05463660982ccd3fc218def9da4fcf8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7200550
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1549757}

--- net/proxy_resolution/parse_proxy_list_fuzzer.cc
+++ net/proxy_resolution/parse_proxy_list_fuzzer.cc
@@ -2,16 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stddef.h>
-#include <stdint.h>
-
-#include "base/compiler_specific.h"
 #include "net/proxy_resolution/proxy_list.h"
+#include "third_party/fuzztest/src/fuzztest/fuzztest.h"
 
-// Entry point for LibFuzzer.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+namespace {
+
+void TestOneInput(const std::string& input) {
   net::ProxyList list;
-  std::string input(data, UNSAFE_TODO(data + size));
   list.Set(input);
-  return 0;
 }
+
+}  // namespace
+
+FUZZ_TEST(ParseProxyListFuzzer, TestOneInput);
