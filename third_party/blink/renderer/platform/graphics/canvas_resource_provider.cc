0886aed80c3e0b2e211f0f32e750b0b8567cb016
Colin Blundell <blundell@chromium.org>
2025-11-25 23:51:04 -0800

    [Blink] Inline GetColorSpace() call in CRPBitmap::DoExternalDraw...()
    The GetColorSpace() implementation is here [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.h;l=222?q=CanvasResourceProvider::GetColorSpace&sq=&ss=chromium
    
    Bug: 352263194
    Change-Id: If1632afdb788b97f9f8c54062238c0aba42765ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7203184
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550275}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -191,7 +191,7 @@ CanvasResourceProviderBitmap::DoExternalDrawAndSnapshot(
             &Image::SharedCCDecodeCache(kN32_SkColorType);
 
         canvas_image_provider_ = std::make_unique<CanvasImageProvider>(
-            cache_rgba8, cache_f16, GetColorSpace(), GetSharedImageFormat(),
+            cache_rgba8, cache_f16, color_space_, GetSharedImageFormat(),
             cc::PlaybackImageProvider::RasterMode::kSoftware);
       }
 
