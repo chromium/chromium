8efd2df8a19b243499e4d6e260208741363eb893
Martín Gómez <martinglopez@google.com>
2025-11-26 10:10:32 -0800

    Add a TabStripModelObserver to the ReadAnythingController to keep track of active tab changes.
    Make ReadAnythingController observe for tab change events through
    the OnTabStripModelChanged method. This allows the
    ReadAnythingController to keep track a tab becoming active or
    inactive. This CL also adds some validations for the
    OnTabStripModelChanged observer in the browser tests.
    
    This change is part of the preparations for Immersive Reading Mode
    (go/immersive-mode-hld).
    
    Bug: 457826574
    Change-Id: Iebd65ca7d823c145018512fdf18b2514ca096e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7152020
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Commit-Queue: Martín Gómez <martinglopez@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1550602}

--- chrome/browser/ui/read_anything/read_anything_controller.h
+++ chrome/browser/ui/read_anything/read_anything_controller.h
@@ -6,6 +6,7 @@
 #define CHROME_BROWSER_UI_READ_ANYTHING_READ_ANYTHING_CONTROLLER_H_
 
 #include "base/memory/raw_ptr.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_key.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_enums.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_ui.h"
@@ -15,6 +16,8 @@
 #include "content/public/browser/web_contents_user_data.h"
 #include "ui/base/unowned_user_data/scoped_unowned_user_data.h"
 
+class TabStripModel;
+
 // Manages the core logic for the Reading Mode feature.
 //
 // This controller is owned by TabFeatures and is instantiated once per tab.
@@ -22,11 +25,11 @@
 //
 // It acts as the primary entry point for all Reading Mode commands and is
 // responsible for orchestrating the display of the Reading Mode UI.
-class ReadAnythingController {
+class ReadAnythingController : public TabStripModelObserver {
  public:
   ReadAnythingController(const ReadAnythingController&) = delete;
   ReadAnythingController& operator=(const ReadAnythingController&) = delete;
-  ~ReadAnythingController();
+  ~ReadAnythingController() override;
 
   enum class PresentationState {
     kInactive,
@@ -62,6 +65,17 @@ class ReadAnythingController {
       std::unique_ptr<WebUIContentsWrapperT<ReadAnythingUntrustedUI>>
           web_ui_wrapper);
 
+  // Test function while OnTabStripModelChanged events are implemented.
+  // TODO(crbug.com/463732840): Remove this function once the
+  // OnTabStripModelChanged events are implemented.
+  bool isActiveTab();
+
+  // TabStripModelObserver:
+  void OnTabStripModelChanged(
+      TabStripModel* tab_strip_model,
+      const TabStripModelChange& change,
+      const TabStripSelectionChange& selection_change) override;
+
  private:
   // Returns the SidePanelUI for the active tab if it can be shown.
   // Otherwise, returns nullptr.
@@ -72,6 +86,11 @@ class ReadAnythingController {
 
   std::unique_ptr<WebUIContentsWrapperT<ReadAnythingUntrustedUI>>
       web_ui_wrapper_;
+
+  // Keeps track of whether the tab is active.
+  // TODO(crbug.com/463732840): Detemrine if this variable is needed once the
+  // OnTabStripModelChanged events are implemented.
+  bool is_active_tab_ = false;
 };
 
 #endif  // CHROME_BROWSER_UI_READ_ANYTHING_READ_ANYTHING_CONTROLLER_H_
