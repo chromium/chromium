f8fd1638ced490ee0b6af87bd44787ea078817a6
Hidehiko Abe <hidehiko@chromium.org>
2025-11-26 05:40:38 -0800

    Enable ShouldPreloadForProfile_NotSupportedProfile for ChromeOS.
    Now the test passes.
    
    BUG=460825008
    TEST=Tryjob
    
    Change-Id: I1a171c7121e598ce1863192101e257deebd842f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7206388
    Reviewed-by: Jun Ishiguro <junis@google.com>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1550441}

--- chrome/browser/glic/glic_profile_manager_browsertest.cc
+++ chrome/browser/glic/glic_profile_manager_browsertest.cc
@@ -297,16 +297,8 @@ IN_PROC_BROWSER_TEST_P(GlicProfileManagerPreloadingTest,
                            : GlicPrewarmingChecksResult::kWarmingDisabled);
 }
 
-// TODO(crbug.com/460825008): Enable on ChromeOS.
-#if BUILDFLAG(IS_CHROMEOS)
-#define MAYBE_ShouldPreloadForProfile_NotSupportedProfile \
-  DISABLED_ShouldPreloadForProfile_NotSupportedProfile
-#else
-#define MAYBE_ShouldPreloadForProfile_NotSupportedProfile \
-  ShouldPreloadForProfile_NotSupportedProfile
-#endif
 IN_PROC_BROWSER_TEST_P(GlicProfileManagerPreloadingTest,
-                       MAYBE_ShouldPreloadForProfile_NotSupportedProfile) {
+                       ShouldPreloadForProfile_NotSupportedProfile) {
   if (!IsPrewarmingEnabled()) {
     GTEST_SKIP() << "This test only applies if prewarming is enabled.";
   }
