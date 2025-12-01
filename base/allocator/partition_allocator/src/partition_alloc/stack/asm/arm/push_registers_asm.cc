3c5e671a4bb18e2b951baec44323ba1949a2a77b
Arthur Sonzogni <arthursonzogni@chromium.org>
2025-11-30 22:45:49 -0800

    Convert to UNSAFE_TODO in partition_alloc in mac_linux_pkey_asm
    This is a reland of commit 28c7cb6b99dffd261c254343f151aa2021602866
    https://chromium-review.googlesource.com/c/chromium/src/+/7157555
    
    The diff was adding "Linux ChromiumOS MSan Builder" to the script.  Then
    I splat the patch to ease review and ensure most can ship without
    revert.  It has been split in 12 patches.
    
    Original change's description:
    -----------------------------
    Convert to UNSAFE_TODO in partition_alloc
    
    This is an automated #cleanup patch using the [Script] below.
    
    We are migrating from coarse-grained file-level suppression (#pragma
    allow_unsafe_buffers) to granular, expression-level markers
    (UNSAFE_TODO()). The pragma disables safety checks for an entire file,
    whereas UNSAFE_TODO() isolates specific potentially unsafe operations,
    allowing the rest of the file to be enforced as safe.
    
    This CL was uploaded by git cl split.
    
    Script: https://docs.google.com/document/d/1ORQGBNn2R-CEvNbDTjRd-GrOBOFlCxIHdcvSUA_EhR4/edit?usp=sharing
    AX-Relnotes: N/A
    Cleanup: This is an automated #cleanup.
    Bug: 409340989
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_msan_rel_ng,linux_chromium_chromeos_msan_rel_ng,ios-asan
    Change-Id: I21b32a3b4e537d4f7990d35972f006db7f3f40db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7198002
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1551937}

--- base/allocator/partition_allocator/src/partition_alloc/stack/asm/arm/push_registers_asm.cc
+++ base/allocator/partition_allocator/src/partition_alloc/stack/asm/arm/push_registers_asm.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 // Push all callee-saved registers to get them on the stack for conservative
 // stack scanning.
 //
--- base/allocator/partition_allocator/src/partition_alloc/thread_isolation/pkey.cc
+++ base/allocator/partition_allocator/src/partition_alloc/thread_isolation/pkey.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "partition_alloc/thread_isolation/pkey.h"
 
 #if PA_BUILDFLAG(ENABLE_PKEYS)
