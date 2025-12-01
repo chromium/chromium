c28f30af809667b9222cdd690261d6f86b59c90a
Michael Wu <mklw@google.com>
2025-12-01 12:26:10 -0800

    Disable Incognito windows for HubLayoutPublicTransitTest test case
    Disables the feature and adds a TODO so we can enable the tablet field
    trial. Bug is currently deprioritized until crbug.com/440167607.
    
    Bug: 461916575
    Include-Ci-Only-Tests: true
    Change-Id: I693e647f70486969b1f7b87ae8b5beff306478d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7164343
    Commit-Queue: Michael Wu <mklw@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552354}

--- chrome/android/java/src/org/chromium/chrome/browser/hub/HubLayoutPublicTransitTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/hub/HubLayoutPublicTransitTest.java
@@ -110,6 +110,8 @@ public class HubLayoutPublicTransitTest {
     @Test
     @LargeTest
     @EnableFeatures(ChromeFeatureList.TAB_GROUP_ENTRY_POINTS_ANDROID)
+    // TODO(crbug.com/461916575): Test disabled for Incognito windowing, delete once fixed
+    @DisableFeatures(ChromeFeatureList.ANDROID_OPEN_INCOGNITO_AS_WINDOW)
     public void testTabGroupPane_newTabGroup() {
         WebPageStation firstPage = mCtaTestRule.startOnBlankPage();
         int firstTabId = firstPage.loadedTabElement.value().getId();
