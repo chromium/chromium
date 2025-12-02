379c574a3abd61360e22447ac7a576a12e97c612
Peter Birk Pakkenberg <pbirk@chromium.org>
2025-12-02 08:26:05 -0800

    Remove soon-to-expire histogram in WebView
    Removes `Permissions.Query.QueryResponseTime` as it hasn't been used,
    and it only records entries in the overflow bucket. Any future work to
    implement a new permissions API should add a new histogram.
    
    OBSOLETE_HISTOGRAMS=Permissions.Query.QueryResponseTime was never used and contains no useful data.
    Fixed: 465126819
    Change-Id: I7610c50304d6b56dd462b82917e64cf8c3e11031
    
    Change-Id: I7610c50304d6b56dd462b82917e64cf8c3e11031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7215909
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Auto-Submit: Peter Birk Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Peter Birk Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Judith Hemp <hempjudith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1552862}

--- third_party/blink/renderer/modules/permissions/permissions.cc
+++ third_party/blink/renderer/modules/permissions/permissions.cc
@@ -8,7 +8,6 @@
 #include <utility>
 
 #include "base/metrics/histogram_functions.h"
-#include "base/time/time.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 #include "third_party/blink/public/mojom/page/page.mojom-blink.h"
 #include "third_party/blink/public/platform/platform.h"
@@ -98,12 +97,10 @@ ScriptPromise<PermissionStatus> Permissions::query(
   // permission prompt will be shown even if the returned permission will most
   // likely be "prompt".
   PermissionDescriptorPtr descriptor_copy = descriptor->Clone();
-  base::TimeTicks query_start_time;
   GetService(context)->HasPermission(
       std::move(descriptor),
-      blink::BindOnce(&Permissions::QueryTaskComplete, WrapPersistent(this),
-                      WrapPersistent(resolver), std::move(descriptor_copy),
-                      query_start_time));
+      blink::BindOnce(&Permissions::TaskComplete, WrapPersistent(this),
+                      WrapPersistent(resolver), std::move(descriptor_copy)));
   return promise;
 }
 
@@ -242,15 +239,6 @@ PermissionService* Permissions::GetService(
 
 void Permissions::ServiceConnectionError() {
   service_.reset();
-}
-void Permissions::QueryTaskComplete(
-    ScriptPromiseResolver<PermissionStatus>* resolver,
-    mojom::blink::PermissionDescriptorPtr descriptor,
-    base::TimeTicks query_start_time,
-    mojom::blink::PermissionStatus result) {
-  base::UmaHistogramTimes("Permissions.Query.QueryResponseTime",
-                          base::TimeTicks::Now() - query_start_time);
-  TaskComplete(resolver, std::move(descriptor), result);
 }
 
 void Permissions::TaskComplete(
