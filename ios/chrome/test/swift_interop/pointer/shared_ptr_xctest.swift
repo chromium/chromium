c4546dbc3543540cbbf345206d3f81d0067bbd94
Justin Novosad <junov@chromium.org>
2025-12-02 10:51:34 -0800

    Fix lines >80col in shared_ptr_xctest.swift
    Bug: 463992124
    Change-Id: I98ec1881882c0a21b59ee45c1a2d84abc24ae2b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7205477
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1552949}

--- ios/chrome/test/swift_interop/pointer/shared_ptr_xctest.swift
+++ ios/chrome/test/swift_interop/pointer/shared_ptr_xctest.swift
@@ -23,33 +23,47 @@ class SharedPointerTest: XCTestCase {
     let object = SharedObject.MakeForSwift(42)
     XCTAssertEqual(object!.IsValid(), true, "Object should be valid")
     XCTAssertEqual(object!.GetValue(), 42, "The value is wrong")
-    XCTAssertEqual(GetSharedObjectLiveCount(), 1, "The count of living objects is wrong")
+    XCTAssertEqual(
+      GetSharedObjectLiveCount(), 1,
+      "The count of living objects is wrong")
   }
 
   func testLifetimeManagedByARC() throws {
     var object: SharedObject? = SharedObject.MakeForSwift(42)
     XCTAssertEqual(object!.IsValid(), true, "Object should be valid")
     XCTAssertEqual(object!.GetValue(), 42, "The value is wrong")
-    XCTAssertEqual(GetSharedObjectLiveCount(), 1, "The count of living objects is wrong")
+    XCTAssertEqual(
+      GetSharedObjectLiveCount(), 1,
+      "The count of living objects is wrong")
 
     var object2: SharedObject? = object
     XCTAssertEqual(object2!.IsValid(), true, "Object should be valid")
     XCTAssertEqual(object2!.GetValue(), 42, "The value is wrong")
-    XCTAssertEqual(GetSharedObjectLiveCount(), 2, "The count of living objects is wrong")
+    XCTAssertEqual(
+      GetSharedObjectLiveCount(), 2,
+      "The count of living objects is wrong")
 
     var object3: SharedObject? = object
     XCTAssertEqual(object3!.IsValid(), true, "Object should be valid")
     XCTAssertEqual(object3!.GetValue(), 42, "The value is wrong")
-    XCTAssertEqual(GetSharedObjectLiveCount(), 3, "The count of living objects is wrong")
+    XCTAssertEqual(
+      GetSharedObjectLiveCount(), 3,
+      "The count of living objects is wrong")
 
     object = nil
-    XCTAssertEqual(GetSharedObjectLiveCount(), 2, "The count of living objects is wrong")
+    XCTAssertEqual(
+      GetSharedObjectLiveCount(), 2,
+      "The count of living objects is wrong")
 
     object2 = nil
-    XCTAssertEqual(GetSharedObjectLiveCount(), 1, "The count of living objects is wrong")
+    XCTAssertEqual(
+      GetSharedObjectLiveCount(), 1,
+      "The count of living objects is wrong")
 
     object3 = nil
-    XCTAssertEqual(GetSharedObjectLiveCount(), 0, "The count of living objects is wrong")
+    XCTAssertEqual(
+      GetSharedObjectLiveCount(), 0,
+      "The count of living objects is wrong")
   }
 
   func testLifetimeByAutoreleasePool() {
@@ -57,9 +71,13 @@ class SharedPointerTest: XCTestCase {
       let object = SharedObject.MakeForSwift(42)
       XCTAssertEqual(object!.IsValid(), true, "Object should be valid")
       XCTAssertEqual(object!.GetValue(), 42, "The value is wrong")
-      XCTAssertEqual(GetSharedObjectLiveCount(), 1, "The count of living objects is wrong")
+      XCTAssertEqual(
+        GetSharedObjectLiveCount(), 1,
+        "The count of living objects is wrong")
     }
-    XCTAssertEqual(GetSharedObjectLiveCount(), 0, "The count of living objects is wrong")
+    XCTAssertEqual(
+      GetSharedObjectLiveCount(), 0,
+      "The count of living objects is wrong")
   }
 
 }
