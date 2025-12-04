f877d6ae70565441ae1ff6c47046a9eea5dfe30e
Matt Reichhoff <mreichhoff@chromium.org>
2025-12-04 05:04:48 -0800

    [iOS][Composebox] Move attachment corner radius to constant
    There was previously a similar calculation in several places.
    
    Bug: 464255476
    Change-Id: Ie39da0e6a6c24c42c855d9a10991d1305fe5bb12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7225575
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553921}

--- ios/chrome/browser/composebox/ui/composebox_input_item_view.h
+++ ios/chrome/browser/composebox/ui/composebox_input_item_view.h
@@ -13,6 +13,8 @@
 namespace composeboxAttachments {
 /// The height of input items.
 const float kAttachmentHeight = 44.0f;
+/// The corner radius of input items.
+const float kAttachmentCornerRadius = kAttachmentHeight / 2;
 /// Image input item size.
 const CGSize kImageInputItemSize = {86.0f, kAttachmentHeight};
 /// Tab/File input item size.
