d31be2e483d131e4229922e8d99ef6fcc21a6955
Patrick Noland <pnoland@google.com>
2025-12-04 13:13:38 -0800

    [M144] Trigger glif animation for Create Image
    Original change's description:
    > Trigger glif animation for Create Image
    > 
    > Bug: 465516531
    > Change-Id: Id99c60aefba94ba6cb85611634977dadc0220b12
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7219168
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Commit-Queue: Patrick Noland <pnoland@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1553122}
    
    (cherry picked from commit 4e83e599360de2468b2807f66fd71495902a4277)
    
    Bug: 466106853,465516531
    Change-Id: Id99c60aefba94ba6cb85611634977dadc0220b12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7229743
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7559@{#171}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -480,6 +480,7 @@ public class ToolbarPhone extends ToolbarLayout
 
         mLocationBarBackground.setHairlineBehavior(
                 type == AutocompleteRequestType.AI_MODE
+                                || type == AutocompleteRequestType.IMAGE_GENERATION
                         ? HairlineBehavior.RAINBOW
                         : HairlineBehavior.NONE);
     }
