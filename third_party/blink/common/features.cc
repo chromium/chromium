cbf1ff051632e02d152c02063acc6e2e2bf317c7
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-12-03 17:54:44 -0800

    Enable AvoidTrustedparamsCopies by default
    Bug: 332706093
    Change-Id: Ibec44b82cb9f6fa7f3ef05563e266d43ab7e4888
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7226613
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1553774}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -55,7 +55,7 @@ BASE_FEATURE(kAndroidSpellcheckFullApiBlink, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
 // Avoids copying ResourceRequest::TrustedParams when possible.
-BASE_FEATURE(kAvoidTrustedParamsCopies, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAvoidTrustedParamsCopies, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Async touchmoves after scroll.
 BASE_FEATURE(kAsyncTouchMovesImmediatelyAfterScroll,
