4a3bc2bf13c573903520875518ccfb3efc82775a
Alexander Hendrich <hendrich@google.com>
2025-12-04 06:19:21 -0800

    [M144] Only check GAIA URL for mirror request header
    When the SecondaryGoogleAccountSigninAllowed policy is set, we only
    check if cookies are enabled on https://accounts.google.com now and no
    longer check https://google.com.
    
    Otherwise, blocking cookies on google.com would prevent us from sending
    the mirror account consistency header, which then allows to add
    secondary accounts.
    
    Manually verified that the policy bypass no longer works.
    
    (cherry picked from commit 952ce664996d6e54650b707d58f9f3a96105c3c8)
    
    Bug: 463848190
    Fixed: 465958408
    Change-Id: I91fc0305e814627fa5970c74af1bad8816713359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7209221
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1553330}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7221990
    Auto-Submit: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#132}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- components/signin/core/browser/cookie_settings_util.cc
+++ components/signin/core/browser/cookie_settings_util.cc
@@ -20,23 +20,35 @@ namespace signin {
 // take in CookieSettingOverrides rather than default to none.
 
 bool SettingsAllowSigninCookies(
-    const content_settings::CookieSettings* cookie_settings) {
+    const content_settings::CookieSettings* cookie_settings,
+    bool check_only_gaia_url) {
 #if BUILDFLAG(IS_IOS)
   // There is no way for users to set the cookies content setting in iOS so
   // sign-in cookies will always be allowed.
   return true;
 #else
+  if (!cookie_settings) {
+    return false;
+  }
+
   GURL gaia_url = GaiaUrls::GetInstance()->gaia_url();
+  if (!cookie_settings->IsFullCookieAccessAllowed(
+          gaia_url, net::SiteForCookies::FromUrl(gaia_url),
+          url::Origin::Create(gaia_url), net::CookieSettingOverrides(),
+          /*cookie_partition_key=*/std::nullopt)) {
+    return false;
+  }
+
   GURL google_url = GaiaUrls::GetInstance()->secure_google_url();
-  return cookie_settings &&
-         cookie_settings->IsFullCookieAccessAllowed(
-             gaia_url, net::SiteForCookies::FromUrl(gaia_url),
-             url::Origin::Create(gaia_url), net::CookieSettingOverrides(),
-             /*cookie_partition_key=*/std::nullopt) &&
-         cookie_settings->IsFullCookieAccessAllowed(
-             google_url, net::SiteForCookies::FromUrl(google_url),
-             url::Origin::Create(google_url), net::CookieSettingOverrides(),
-             /*cookie_partition_key=*/std::nullopt);
+  if (!check_only_gaia_url &&
+      !cookie_settings->IsFullCookieAccessAllowed(
+          google_url, net::SiteForCookies::FromUrl(google_url),
+          url::Origin::Create(google_url), net::CookieSettingOverrides(),
+          /*cookie_partition_key=*/std::nullopt)) {
+    return false;
+  }
+
+  return true;
 #endif
 }
 
