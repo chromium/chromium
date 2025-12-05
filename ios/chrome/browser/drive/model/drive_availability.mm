528cad38ad27d504d4d5fec59261975f65453843
Quentin Pubert <qpubert@google.com>
2025-12-05 10:09:07 -0800

    [iOS] Add IsChooseFromDriveAvailable function
    This CL adds a function IsChooseFromDriveAvailable similar to
    IsSaveToDriveAvailable except that it will be used to determine whether
    Choose from Drive is available in a given WebState.
    
    Bug: 441659098
    Change-Id: Ib56df77bbb0966016a4f1f7f2f314ca1956a13e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7227514
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1554757}

--- ios/chrome/browser/drive/model/drive_availability.mm
+++ ios/chrome/browser/drive/model/drive_availability.mm
@@ -6,12 +6,33 @@
 
 #import <Foundation/Foundation.h>
 
+#import "base/metrics/histogram_functions.h"
 #import "components/prefs/pref_service.h"
 #import "components/signin/public/identity_manager/identity_manager.h"
 #import "ios/chrome/browser/drive/model/drive_policy.h"
 #import "ios/chrome/browser/drive/model/drive_service.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
+
+namespace {
+
+// LINT.IfChange(FilePickerDriveDisplayed)
+
+// Whether the Drive element was displayed, and the reason why it was not.
+enum class FilePickerDriveDisplayed {
+  kDisplayed = 0,
+  kNoWebState = 1,
+  kDisabled = 2,
+  kOTR = 3,
+  kNotSignedIn = 4,
+  kDisabledByPolicy = 5,
+  kDriveServiceNotSupported = 6,
+  kMaxValue = kDriveServiceNotSupported,
+};
+
+// LINT.ThenChange(//tools/metrics/histograms/metadata/ios/enums.xml:IOSFilePickerDriveDisplayed)
+
+}  // namespace
 
 namespace drive {
 
@@ -43,6 +64,63 @@ bool IsSaveToDriveAvailable(bool is_incognito,
     return false;
   }
 
+  return true;
+}
+
+bool IsChooseFromDriveAvailable(web::WebState* web_state,
+                                bool is_incognito,
+                                signin::IdentityManager* identity_manager,
+                                drive::DriveService* drive_service,
+                                PrefService* pref_service) {
+  // Check WebState still exists.
+  if (!web_state) {
+    base::UmaHistogramEnumeration("IOS.FilePicker.Drive.Displayed",
+                                  FilePickerDriveDisplayed::kNoWebState);
+    return false;
+  }
+
+  // Check flag is enabled.
+  if (!base::FeatureList::IsEnabled(kIOSChooseFromDrive)) {
+    base::UmaHistogramEnumeration("IOS.FilePicker.Drive.Displayed",
+                                  FilePickerDriveDisplayed::kDisabled);
+    return false;
+  }
+
+  // Check WebState is not Incognito.
+  if (is_incognito) {
+    base::UmaHistogramEnumeration("IOS.FilePicker.Drive.Displayed",
+                                  FilePickerDriveDisplayed::kOTR);
+    return false;
+  }
+
+  // Check user is signed in.
+  if (!identity_manager ||
+      !identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin)) {
+    base::UmaHistogramEnumeration("IOS.FilePicker.Drive.Displayed",
+                                  FilePickerDriveDisplayed::kNotSignedIn);
+    return false;
+  }
+
+  // Check enterprise policy.
+  CHECK(pref_service);
+  if (pref_service->GetInteger(
+          prefs::kIosChooseFromDriveFilePickerPolicySettings) ==
+      static_cast<int>(ChooseFromDrivePolicySettings::kDisabled)) {
+    base::UmaHistogramEnumeration("IOS.FilePicker.Drive.Displayed",
+                                  FilePickerDriveDisplayed::kDisabledByPolicy);
+    return false;
+  }
+
+  // Check if DriveService is supported.
+  if (!drive_service || !drive_service->IsSupported()) {
+    base::UmaHistogramEnumeration(
+        "IOS.FilePicker.Drive.Displayed",
+        FilePickerDriveDisplayed::kDriveServiceNotSupported);
+    return false;
+  }
+
+  base::UmaHistogramEnumeration("IOS.FilePicker.Drive.Displayed",
+                                FilePickerDriveDisplayed::kDisplayed);
   return true;
 }
 
