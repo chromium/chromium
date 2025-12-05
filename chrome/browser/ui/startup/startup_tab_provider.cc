7ccd5c50ed00f77c65b8f344a815939c5636017d
Tsuyoshi Horo <horo@chromium.org>
2025-12-04 21:51:44 -0800

    Revert "Remove CHROME_FOR_TESTING compiler pre-processing directive for StripGoogleChromeScheme() function"
    This reverts commit eda0ff8c03acad2823ad161dc2fa5c3b450e3677.
    
    Reason for revert: Caused ChromeForTestingInfoBarTests failure.
    Bug: 466244540
    
    
    Original change's description:
    > Remove CHROME_FOR_TESTING compiler pre-processing directive for StripGoogleChromeScheme() function
    >
    > One of the previous CL 7087621 is failing on Chrome for Testing mode.
    > Looks like it is not needed.
    >
    > TESTED= Manual build on - cft - out/Default/chrome --password-store=basic --enable-features=GoogleChromeScheme chromium://http://www.nytimes.co
    >
    >
    > Bug: b:446672134
    > Change-Id: I8bce4cde8c8e80ac4eafde7bacb2594724490984
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7229506
    > Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    > Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Commit-Queue: Deepak Ravichandran <deepakr@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1554323}
    
    Bug: b:446672134
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I44b20e14e3ba3958fab67e3e423177806250fc7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7231046
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1554469}

--- chrome/browser/ui/startup/startup_tab_provider.cc
+++ chrome/browser/ui/startup/startup_tab_provider.cc
@@ -78,6 +78,9 @@ namespace {
 // `kGoogleChromeScheme` feature is enabled. Returns true if the prefix was
 // stripped.
 bool StripGoogleChromeScheme(base::FilePath::StringViewType& arg) {
+#if BUILDFLAG(CHROME_FOR_TESTING)
+  return false;
+#else
   const base::FilePath kFullPrefixPath = base::FilePath::FromASCII(
       base::StrCat({shell_integration::GetDirectLaunchUrlScheme(),
                     url::kStandardSchemeSeparator}));
@@ -92,6 +95,7 @@ bool StripGoogleChromeScheme(base::FilePath::StringViewType& arg) {
     return true;
   }
   return false;
+#endif  // BUILDFLAG(CHROME_FOR_TESTING)
 }
 
 // Attempts to find an existing, non-empty tabbed browser for this profile.
