d0a04c0fb65a8c0551668c60b7459a1c5621bbe4
Daniel Soromou <koretadaniel@chromium.org>
2025-12-05 10:35:57 -0800

    [Gardener] Disable WebAppEngagementBrowserTest.DefaultApp on Linux.
    The test is flaky on Linux, so it is being disabled along with Mac and
    ChromeOS.
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/137811/overview
    
    Bug: 40884336
    Change-Id: I854a38979e706fefac694b9b50075b45e09f871a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7233590
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Owners-Override: Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Daniel Soromou <koretadaniel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1554773}

--- chrome/browser/ui/web_applications/web_app_engagement_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_engagement_browsertest.cc
@@ -483,13 +483,8 @@ IN_PROC_BROWSER_TEST_F(WebAppEngagementBrowserTest, ManyUserApps) {
                      /*tabLaunches=*/0);
 }
 
-// TODO(crbug.com/40884336): Flaky on Mac.
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS)
-#define MAYBE_DefaultApp DISABLED_DefaultApp
-#else
-#define MAYBE_DefaultApp DefaultApp
-#endif
-IN_PROC_BROWSER_TEST_F(WebAppEngagementBrowserTest, MAYBE_DefaultApp) {
+// TODO(crbug.com/40884336): Flaky on all platforms.
+IN_PROC_BROWSER_TEST_F(WebAppEngagementBrowserTest, DISABLED_DefaultApp) {
   base::HistogramTester tester;
   ASSERT_TRUE(embedded_test_server()->Start());
 
