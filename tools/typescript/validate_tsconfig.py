05a125b9d94eb0a1d49bbdd1ca55ae208f48107d
dpapad <dpapad@chromium.org>
2025-12-08 10:52:02 -0800

    CrOS PasswordChange: Leverage build_webui() GN rule.
    This preparatory step unblocks migrating this UI to TypeScript, which in
    turn will unblock migrating c/b/r/gaia_auth_host/ to TypeScript.
    
    Bug: 40222185
    Change-Id: Ic201a29ec6d2bc37b4e48e3bee9efa5be680a9eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7234452
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1555590}

--- tools/typescript/validate_tsconfig.py
+++ tools/typescript/validate_tsconfig.py
@@ -147,9 +147,7 @@ def validateJavaScriptAllowed(source_dir, out_dir, platform):
         'ash/webui/help_app_ui/',
         # TODO(b/267329383): Migrate A11y to TypeScript.
         'chrome/browser/resources/chromeos/accessibility',
-        'chrome/browser/resources/chromeos/crostini_installer',
-        'chrome/browser/resources/chromeos/crostini_upgrader',
-        'chrome/browser/resources/chromeos/gaia_action_buttons',
+        'chrome/browser/resources/chromeos/password_change',
         'chrome/test/data/webui/chromeos',
         'chrome/test/data/webui/chromeos/ash_common',
         'chrome/test/data/webui/chromeos/nearby_share',
