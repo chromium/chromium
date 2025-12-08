71a0dbd6672e2ccb6d1008376cbb7acd315cb8d6
Justin DeWitt <dewittj@chromium.org>
2025-12-08 12:35:33 -0800

    [M143] Use IsMultiInstanceEnabled in IsUnifiedFreEnabled.
    Original change's description:
    > Use IsMultiInstanceEnabled in IsUnifiedFreEnabled.
    > 
    > This change makes IsUnifiedFreEnabled() use the helper function
    > IsMultiInstanceEnabled() to check for multi-instance support, ensuring
    > consistency with other multi-instance checks (such as the sync multi
    > instance deployment flag).
    > 
    > Change-Id: I2376f5a5d6db478e204cec4dd0cc17708064d275
    > Bug: b:462434467
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7178962
    > Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    > Reviewed-by: Michelle Abreo <michelleabreo@google.com>
    > Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1547976}
    
    (cherry picked from commit 909da8aa8991edbe014b6a258bf0ba0b3cfb7a73)
    
    Bug: 462469170,b:462434467
    Change-Id: I2376f5a5d6db478e204cec4dd0cc17708064d275
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7232700
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Reviewed-by: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7499@{#3142}
    Cr-Branched-From: b30439823e5177773584139e72e0593e36863899-refs/heads/main@{#1536371}

--- chrome/browser/glic/public/glic_enabling.cc
+++ chrome/browser/glic/public/glic_enabling.cc
@@ -251,7 +251,7 @@ void GlicEnabling::OnGlicSettingsPolicyChanged() {
 }
 
 bool GlicEnabling::IsUnifiedFreEnabled(Profile* profile) {
-  return base::FeatureList::IsEnabled(features::kGlicMultiInstance) &&
+  return IsMultiInstanceEnabled() &&
          base::FeatureList::IsEnabled(features::kGlicUnifiedFreScreen);
 }
 
