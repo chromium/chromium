152d5247f34bf59052ce5b332a6f9758a427837b
Ryan Sultanem <rsult@google.com>
2025-12-09 09:23:14 -0800

    [ProfilePicker] Fix documentation of CombineCallbacks()
    Change-Id: If53451938f95697b36630470436dd16ec6a1063d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7241925
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556202}

--- chrome/browser/ui/views/profiles/profile_management_types.h
+++ chrome/browser/ui/views/profiles/profile_management_types.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_MANAGEMENT_TYPES_H_
 #define CHROME_BROWSER_UI_VIEWS_PROFILES_PROFILE_MANAGEMENT_TYPES_H_
 
+#include <vector>
+
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
 #include "base/types/strong_alias.h"
@@ -29,15 +31,11 @@ using PostHostClearedCallback =
     base::StrongAlias<class PostHostClearedCallbackTag,
                       base::OnceCallback<void(Browser*)>>;
 
-// Generic template to combine two callbacks of the same type `CallbackType`
-// without needing to forward the input parameters from the `callback1` to
-// `callback2`. `Params` must match with `CallbackType` input parameters.
-// Empty/Null callbacks are accepted and ignored.
+// Generic template to combine multiple callbacks of the same type without
+// needing to forward the input parameters between all callbacks. `Params` must
+// match with `CallbackType` input parameters.  The callbacks are run in the
+// order that they are provided. Empty/Null callbacks are accepted and ignored.
 // Note: `CallbackType` should be of type `base::StrongAlias<Tag, Callback>`.
-#include <vector>
-
-// Combines multiple callbacks into a single callback. The callbacks are run in
-// the order that they are provided.
 template <class CallbackType, class... Params>
 CallbackType CombineCallbacks(std::vector<CallbackType> callbacks) {
   return CallbackType(base::BindOnce(
