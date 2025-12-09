5b4080031eb10f85864e9da535ac4add7e590e75
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2025-12-09 17:29:10 +0100

    fix: extra headers can have duplicates (#3937)
    https://github.com/puppeteer/puppeteer/issues/14325

--- third_party/chromium-bidi/src/bidiMapper/modules/network/NetworkRequest.ts
+++ third_party/chromium-bidi/src/bidiMapper/modules/network/NetworkRequest.ts
@@ -339,14 +339,14 @@ export class NetworkRequest {
     if (this.#requestOverrides?.headers) {
       const headerMap = new DefaultMap<string, string[]>(() => []);
       for (const header of this.#requestOverrides.headers) {
-        headerMap.get(header.name).push(header.value.value);
+        headerMap.get(header.name).push(header.value.value.trim());
       }
-      for (const [name, value] of headerMap.entries()) {
+      for (const [name, values] of headerMap.entries()) {
         headers.push({
           name,
           value: {
             type: 'string',
-            value: value.join('\n').trimEnd(),
+            value: values.join(', ').trimEnd(),
           },
         });
       }
