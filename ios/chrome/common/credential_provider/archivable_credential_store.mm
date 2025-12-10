331303740ad629bd4a1684d28d8037ee254f557e
Alexis Hetu <sugoi@chromium.org>
2025-12-10 10:56:05 -0800

    [iOS] Make loadStorage more reliable
    This CL attempts to make loadStorage more reliable by:
    1) using a NSFileCoordinator to coordinate reads and writes to the
       archived credential storage file. This should allow the CPE and
       Chrome to safely attempt to modify the file simultaneously without
       one of them failing to access the file because the other process
       has the file locked and unaccessible.
    2) reducing the number of points of failure. dataWithContentsOfURL
       will fail anyway if the file is not accessible, so this CL
       removes the redundant checkResourceIsReachableAndReturnError call,
       which was returning an unclear error anyway, so maybe
       dataWithContentsOfURL will return a clearer error.
    3) returning nil only when the file is unaccessible, to make sure
       this is the only case where we try to reopen the file. On bad
       data, we won't try to reopen the file again.
    
    Bug: 367439455
    Change-Id: I7834077c09e41311d7c5f6b24d57ce50db37248a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7239506
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1556864}

--- ios/chrome/common/credential_provider/archivable_credential_store.mm
+++ ios/chrome/common/credential_provider/archivable_credential_store.mm
@@ -16,6 +16,9 @@
 // The fileURL to the disk file.
 @property(nonatomic, strong) NSURL* fileURL;
 
+// The NSFileCoordinator instance used for inter-process locking.
+@property(nonatomic, strong, readonly) NSFileCoordinator* fileCoordinator;
+
 @end
 
 @implementation ArchivableCredentialStore
@@ -27,6 +30,7 @@
   if (self) {
     CHECK(fileURL.isFileURL) << "URL must be a file URL.";
     _fileURL = fileURL;
+    _fileCoordinator = [[NSFileCoordinator alloc] initWithFilePresenter:nil];
   }
   return self;
 }
@@ -58,22 +62,51 @@
 #if !defined(NDEBUG)
   dispatch_assert_queue(self.workingQueue);
 #endif  // !defined(NDEBUG)
+
   if (!self.fileURL) {
     return [[NSMutableDictionary alloc] init];
   }
+
+  __block NSMutableDictionary<NSString*, ArchivableCredential*>*
+      resultDictionary = nil;
   NSError* error = nil;
-  [self.fileURL checkResourceIsReachableAndReturnError:&error];
-  if (error) {
-    if (error.code == NSFileReadNoSuchFileError) {
+
+  __weak __typeof(self) weakSelf = self;
+  [self.fileCoordinator
+      coordinateReadingItemAtURL:self.fileURL
+                         options:NSFileCoordinatorReadingWithoutChanges
+                           error:&error
+                      byAccessor:^(NSURL* newURL) {
+                        resultDictionary = [weakSelf loadStorageAtURL:newURL];
+                      }];
+
+  // File system errors do happen and are out of our control, so errors are
+  // handled gracefully without crashing here. This is for debugging purposes
+  // only.
+  DCHECK(!error) << base::SysNSStringToUTF8(error.description);
+
+  return resultDictionary;
+}
+
+#pragma mark - Private
+
+// Loads the storage at the given file URL. This is a block protected by a
+// `coordinateReadingItemAtURL` call.
+- (NSMutableDictionary<NSString*, ArchivableCredential*>*)loadStorageAtURL:
+    (NSURL*)fileURL {
+  NSError* error = nil;
+  NSData* data = [NSData dataWithContentsOfURL:fileURL options:0 error:&error];
+  if (!data) {
+    if (error && error.code == NSFileReadNoSuchFileError) {
       // File has not been created, return a fresh mutable set.
       return [[NSMutableDictionary alloc] init];
     }
+
     DUMP_WILL_BE_NOTREACHED() << error.localizedDescription;
+    // There was an error accessing the file, so return nil to attempt to open
+    // the file again next time MemoryCredentialStore::memoryStorage is called.
+    return nil;
   }
-  NSData* data = [NSData dataWithContentsOfURL:self.fileURL
-                                       options:0
-                                         error:&error];
-  DCHECK(!error) << base::SysNSStringToUTF8(error.description);
   NSSet* classes =
       [NSSet setWithObjects:[ArchivableCredential class],
                             [NSMutableDictionary class], [NSString class], nil];
@@ -81,18 +114,27 @@
       [NSKeyedUnarchiver unarchivedObjectOfClasses:classes
                                           fromData:data
                                              error:&error];
+  // On error, return an empty dictionary. This is for debugging purposes only.
   DCHECK(!error) << base::SysNSStringToUTF8(error.description);
-  return dictionary;
+  // On error, the file may contain bad data, so return an empty dictionary to
+  // avoid trying to unarchive the same bad data again.
+  return dictionary ?: [[NSMutableDictionary alloc] init];
 }
-
-#pragma mark - Private
 
 // Body of the `saveDataWithCompletion`'s block. Body was extracted so that the
 // `self`/`weak self` management is easier. `saveDataWithCompletion` takes the
-// responsability of calling `saveDataWithCompletionBlockBody` on a weak
+// responsibility of calling `saveDataWithCompletionBlockBody` on a weak
 // version of `self`. There is therefore no need to use a weak reference
 // everywhere in the method here.
 - (void)saveDataWithCompletionBlockBody:(void (^)(NSError* error))completion {
+  // There are odd crashes during shutdown which appear to only be possible if
+  // the object no longer exists. Even though this should never happen, this
+  // check adds extra safety to avoid accessing released memory.
+  // See http://crbug.com/40914898 for details.
+  if (!self) {
+    return;
+  }
+
   auto executeCompletionIfPresent = ^(NSError* error) {
     if (completion) {
       dispatch_async(dispatch_get_main_queue(), ^{
@@ -101,35 +143,66 @@
     }
   };
 
-  NSError* error = nil;
-
-  if (self) {
-    NSData* data =
-        [NSKeyedArchiver archivedDataWithRootObject:self.memoryStorage
-                              requiringSecureCoding:YES
-                                              error:&error];
-    DCHECK(!error) << base::SysNSStringToUTF8(error.description);
-    if (error) {
-      executeCompletionIfPresent(error);
-      return;
-    }
-
-    [[NSFileManager defaultManager]
-               createDirectoryAtURL:self.fileURL.URLByDeletingLastPathComponent
-        withIntermediateDirectories:YES
-                         attributes:nil
-                              error:&error];
-
-    if (error) {
-      executeCompletionIfPresent(error);
-      return;
-    }
-
-    [data writeToURL:self.fileURL options:NSDataWritingAtomic error:&error];
-    DCHECK(!error) << base::SysNSStringToUTF8(error.description);
+  __block NSError* error = nil;
+  NSData* data = [NSKeyedArchiver archivedDataWithRootObject:self.memoryStorage
+                                       requiringSecureCoding:YES
+                                                       error:&error];
+  // On error, return early. This is for debugging purposes only.
+  DCHECK(!error) << base::SysNSStringToUTF8(error.description);
+  if (error) {
+    executeCompletionIfPresent(error);
+    return;
   }
 
+  // Make sure the file URL's directory exists.
+  // Note that even though we're creating a directory, not deleting it, the
+  // NSFileCoordinatorWritingForDeleting option can be used to guard directory
+  // structure changes.
+  [self.fileCoordinator
+      coordinateWritingItemAtURL:self.fileURL.URLByDeletingLastPathComponent
+                         options:NSFileCoordinatorWritingForDeleting
+                           error:&error
+                      byAccessor:^(NSURL* newDirectoryURL) {
+                        [ArchivableCredentialStore
+                            createDirectoryIfNecessary:newDirectoryURL
+                                                 error:&error];
+                      }];
+
+  // On error, return early. This is for debugging purposes only.
+  DCHECK(!error) << base::SysNSStringToUTF8(error.description);
+  if (error) {
+    executeCompletionIfPresent(error);
+    return;
+  }
+
+  // Write the archived data to the provided file URL. This may create a new
+  // file or replace an existing file.
+  [self.fileCoordinator
+      coordinateWritingItemAtURL:self.fileURL
+                         options:NSFileCoordinatorWritingForReplacing
+                           error:&error
+                      byAccessor:^(NSURL* newURL) {
+                        [data writeToURL:newURL
+                                 options:NSDataWritingAtomic
+                                   error:&error];
+                      }];
+
+  // On error, still call completion block. This is for debugging purposes only.
+  DCHECK(!error) << base::SysNSStringToUTF8(error.description);
+
   executeCompletionIfPresent(error);
+}
+
+// Creates a directory if it doesn't exist yet. Propagates unexpected errors.
++ (void)createDirectoryIfNecessary:(NSURL*)directoryURL
+                             error:(NSError**)outError {
+  NSError* error = nil;
+  if (![[NSFileManager defaultManager] createDirectoryAtURL:directoryURL
+                                withIntermediateDirectories:YES
+                                                 attributes:nil
+                                                      error:&error]) {
+    *outError = error;
+  }
 }
 
 @end
