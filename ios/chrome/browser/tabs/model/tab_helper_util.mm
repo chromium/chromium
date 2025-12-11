9353954df2242bf7763da5e9cfb07816d688071c
ginnnnnnny <ginnyhuang@chromium.org>
2025-12-11 09:20:45 -0800

    [iOS] Attach ChromeIOSTranslateClient to web state after InfobarBadgeTH
    Recently, a dependency on infobar manager is added to
    ChromeIOSTranslateClient. Since the translate infobar is associated with
    a badge, if the translate client is registered earlier than the infobar
    badge tab helper and displays an infobar as soon as the registration
    happens, the infobar might not be observed by the badge tab helper
    correctly, causing a DWC.
    
    Bug: 465908300
    Change-Id: Ic8df4fc57a02031092bb9d1774c2a491100f601b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7245911
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557474}

--- ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -394,14 +394,14 @@ void AttachTabHelpers(web::WebState* web_state, TabHelperFilter filter_flags) {
       attacher.IsNotInTabHelperFilter());
   attacher.CreateWhen<AutofillTabHelper>(attacher.IsNotInTabHelperFilter());
 
-  attacher.CreateWhen<ChromeIOSTranslateClient>(
-      attacher.IsNotInTabHelperFilter(),
-      InfoBarManagerImpl::FromWebState(web_state));
-
   // Special case for use of GetOrCreateForWebState.
   if (!attacher.IsForStandardNavigation()) {
     InfobarBadgeTabHelper::GetOrCreateForWebState(web_state);
   }
+  // Needs to be created after `InfobarBadgeTabHelper`.
+  attacher.CreateWhen<ChromeIOSTranslateClient>(
+      attacher.IsNotInTabHelperFilter(),
+      InfoBarManagerImpl::FromWebState(web_state));
 
   attacher
       .CreateDeferredWhen<webauthn::PasskeyTabHelper>(
