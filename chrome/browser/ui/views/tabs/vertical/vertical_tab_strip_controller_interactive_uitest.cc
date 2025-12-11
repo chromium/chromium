62da6be6a9cb360a32da26ab50ea9b19541ed14c
Kunal Daftari <kunaldaftari@google.com>
2025-12-11 15:13:38 -0800

    [Vertical Tabs] Creating Test for Keyboard Tab Selection
    This CL adds interactive ui tests for the vertical tab strip's Keyboard
    Tab Selection functionalities. These are present to ensure that if a
    regression occurs based on a new change, then we can easily detect it.
    
    Change-Id: I628b578f8b178ff32119815a2ecc178a9c79ea70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7227668
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1557718}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller_interactive_uitest.cc
@@ -2,9 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
+#include "chrome/browser/ui/tabs/tab_group_model.h"
 #include "chrome/browser/ui/tabs/tab_menu_model.h"
 #include "chrome/browser/ui/views/frame/vertical_tab_strip_region_view.h"
 #include "chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h"
@@ -12,11 +14,13 @@
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
+#include "components/tabs/public/tab_group.h"
 #include "content/public/test/browser_test.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/test/ui_controls.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/events/event_constants.h"
+#include "ui/events/keycodes/keyboard_codes.h"
 #include "ui/menus/simple_menu_model.h"
 #include "ui/views/interaction/interactive_views_test.h"
 
@@ -108,8 +112,6 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripControllerInteractiveUiTest,
       // Name views so we can interact with them.
       NameDescendantViewByType<VerticalTabView>(kBrowserViewElementId,
                                                 kFirstTabName, 0),
-      NameDescendantViewByType<VerticalTabView>(kBrowserViewElementId,
-                                                kSecondTabName, 1),
       // Close tab at index 0 w/middle mouse button and verify tab count.
       MoveMouseTo(kFirstTabName), ClickMouse(ui_controls::MIDDLE),
       CheckResult([this]() { return browser()->tab_strip_model()->count(); },
@@ -212,6 +214,106 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripControllerInteractiveUiTest,
           false),
       CheckResult(
           [this]() { return browser()->tab_strip_model()->active_index(); },
+          0));
+}
+
+IN_PROC_BROWSER_TEST_F(VerticalTabStripControllerInteractiveUiTest,
+                       KeyboardTabSelection) {
+  ui::Accelerator previous_tab_accelerator, next_tab_accelerator;
+
+  ASSERT_TRUE(BrowserView::GetBrowserViewForBrowser(browser())->GetAccelerator(
+      IDC_SELECT_NEXT_TAB, &previous_tab_accelerator));
+  ASSERT_TRUE(BrowserView::GetBrowserViewForBrowser(browser())->GetAccelerator(
+      IDC_SELECT_PREVIOUS_TAB, &next_tab_accelerator));
+
+  RunTestSequence(
+      // Verify Vertical Tabs is showing.
+      WaitForShow(kVerticalTabStripBottomContainerElementId),
+      // Create a second tab.
+      EnsurePresent(kNewTabButtonElementId),
+      PressButton(kNewTabButtonElementId,
+                  ui::test::InteractionTestUtil::InputType::kDontCare),
+      // Move to left (Tab 0) and verify active index.
+      SendAccelerator(kBrowserViewElementId, previous_tab_accelerator),
+      CheckResult(
+          [this]() { return browser()->tab_strip_model()->active_index(); }, 0),
+      // Move to right (Tab 1) and verify active index.
+      SendAccelerator(kBrowserViewElementId, next_tab_accelerator),
+      CheckResult(
+          [this]() { return browser()->tab_strip_model()->active_index(); },
+          1));
+}
+
+// TODO(crbug.com/466391046): Tab Group Accelerators are not defined on
+// ChromeOS.
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_KeyboardTabGroupCommands DISABLED_KeyboardTabGroupCommands
+#else
+#define MAYBE_KeyboardTabGroupCommands KeyboardTabGroupCommands
+#endif
+IN_PROC_BROWSER_TEST_F(VerticalTabStripControllerInteractiveUiTest,
+                       MAYBE_KeyboardTabGroupCommands) {
+  ui::Accelerator create_new_tab_group_accelerator,
+      add_new_tab_to_group_accelerator, close_tab_group_accelerator;
+
+  ASSERT_TRUE(BrowserView::GetBrowserViewForBrowser(browser())->GetAccelerator(
+      IDC_CREATE_NEW_TAB_GROUP, &create_new_tab_group_accelerator));
+  ASSERT_TRUE(BrowserView::GetBrowserViewForBrowser(browser())->GetAccelerator(
+      IDC_ADD_NEW_TAB_TO_GROUP, &add_new_tab_to_group_accelerator));
+  ASSERT_TRUE(BrowserView::GetBrowserViewForBrowser(browser())->GetAccelerator(
+      IDC_CLOSE_TAB_GROUP, &close_tab_group_accelerator));
+
+  RunTestSequence(
+      // Verify Vertical Tabs is showing.
+      WaitForShow(kVerticalTabStripBottomContainerElementId),
+      // Keyboard Command to Create New Tab Group.
+      SendAccelerator(kBrowserViewElementId, create_new_tab_group_accelerator),
+      // Verify One Tab Group Exists.
+      CheckResult(
+          [this]() {
+            return browser()
+                ->tab_strip_model()
+                ->group_model()
+                ->ListTabGroups()
+                .size();
+          },
+          1),
+      // Hide Tab Group Bubble to avoid failing future inputs.
+      SendAccelerator(kTabGroupEditorBubbleId,
+                      ui::Accelerator(ui::VKEY_ESCAPE, ui::EF_NONE)),
+      WaitForHide(kTabGroupEditorBubbleId),
+      // Keyboard Command to Add New Tab in Group.
+      SendAccelerator(kBrowserViewElementId, add_new_tab_to_group_accelerator),
+      // Verify One Tab Group Exists and its Tab Count is 2.
+      CheckResult(
+          [this]() {
+            return browser()
+                ->tab_strip_model()
+                ->group_model()
+                ->ListTabGroups()
+                .size();
+          },
+          1),
+      CheckResult(
+          [this]() {
+            auto* group_model = browser()->tab_strip_model()->group_model();
+            return group_model
+                ->GetTabGroup(group_model->ListTabGroups().front())
+                ->ListTabs()
+                .length();
+          },
+          2),
+      // Keyboard Command to Delete Current Tab Group.
+      SendAccelerator(kBrowserViewElementId, close_tab_group_accelerator),
+      // Verify No Tab Groups Exist.
+      CheckResult(
+          [this]() {
+            return browser()
+                ->tab_strip_model()
+                ->group_model()
+                ->ListTabGroups()
+                .size();
+          },
           0));
 }
 
