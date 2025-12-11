695365fe3bfc7bc59ee56dd235d0b86de9c79d21
Julia Sobiech <jsobiech@google.com>
2025-12-10 22:42:31 -0800

    [Refill] Enable AutofillRefillForFormsIos by default
    Bug: 399160644
    Change-Id: I43c5428e9a57eab36ea2aa6ce3f43e931f248901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7238937
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557149}

--- components/autofill/ios/common/features.mm
+++ components/autofill/ios/common/features.mm
@@ -39,7 +39,7 @@ BASE_FEATURE(kAutofillPaymentsSheetV2Ios, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAutofillPaymentsSheetV3Ios, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAutofillRefillForFormsIos, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAutofillRefillForFormsIos, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // LINT.IfChange(autofill_report_form_submission_errors)
 BASE_FEATURE(kAutofillReportFormSubmissionErrors,
