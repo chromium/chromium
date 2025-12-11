18fe4d25ee44053c060d0c7a93edf9c9e4065936
Fiaz Muhammad <mfiaz@google.com>
2025-12-11 08:46:07 -0800

    [Public Transit] Add context menu items and matchers
    - Add items for context menu.
    - Add matchers for items.
    
    Bug: 467341609
    Change-Id: I1578d11a4e5b5c0b7f091326dafc3c72555b4df3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248086
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1557440}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherTabCardContextMenuFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/TabSwitcherTabCardContextMenuFacility.java
@@ -5,11 +5,25 @@
 package org.chromium.chrome.test.transit.hub;
 
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.junit.Assert.assertNotEquals;
+
+import android.view.View;
+
+import androidx.annotation.StringRes;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 
 import org.chromium.base.test.transit.ScrollableFacility;
 import org.chromium.base.test.transit.ViewElement;
+import org.chromium.base.test.transit.ViewSpec;
 import org.chromium.chrome.test.R;
 import org.chromium.components.browser_ui.widget.list_view.TouchTrackingListView;
+import org.chromium.ui.listmenu.ListMenuItemProperties;
+import org.chromium.ui.modelutil.MVCListAdapter;
 
 /**
  * Facility for a tab switcher card that appears upon long press or right click.
@@ -23,6 +37,58 @@ public class TabSwitcherTabCardContextMenuFacility<HostStationT extends TabSwitc
     public final ViewElement<TouchTrackingListView> listElement =
             declareView(TouchTrackingListView.class, withId(R.id.tab_group_action_menu_list));
 
+    public Item share;
+    public Item addTabToGroup;
+    public Item addTabToNewGroup;
+    public Item moveTabToGroup;
+    public Item addTabToBookmarks;
+    public Item editBookmark;
+    public Item selectTab;
+    public Item pinTab;
+    public Item unpinTab;
+    public Item closeTab;
+
     @Override
-    protected void declareItems(ItemsBuilder items) {}
+    protected void declareItems(ItemsBuilder items) {
+        share = declarePossibleItemWithText(items, "Share");
+        addTabToGroup = declarePossibleItemWithText(items, "Add tab to group");
+        addTabToNewGroup = declarePossibleItemWithText(items, "Add tab to new group");
+        moveTabToGroup = declarePossibleItemWithText(items, "Move tab to group");
+        addTabToBookmarks = declarePossibleItemWithText(items, "Add to bookmarks");
+        editBookmark = declarePossibleItemWithText(items, "Edit bookmark");
+        selectTab = declarePossibleItemWithText(items, "Select tab");
+        pinTab = declarePossibleItemWithText(items, "Pin tab");
+        unpinTab = declarePossibleItemWithText(items, "Unpin tab");
+        closeTab = declarePossibleItemWithText(items, "Close tab");
+    }
+
+    private Item declarePossibleItemWithText(ItemsBuilder builder, String text) {
+        return builder.declarePossibleItem(getItemViewSpec(text), withMenuItemTitle(text));
+    }
+
+    private ViewSpec<View> getItemViewSpec(String text) {
+        return listElement.descendant(withText(text));
+    }
+
+    private Matcher<MVCListAdapter.ListItem> withMenuItemTitle(String text) {
+        return new TypeSafeMatcher<>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("with menu item title ");
+                description.appendText(text);
+            }
+
+            @Override
+            protected boolean matchesSafely(MVCListAdapter.ListItem listItem) {
+                if (listItem.model.containsKey(ListMenuItemProperties.TITLE_ID)) {
+                    @StringRes int titleId = listItem.model.get(ListMenuItemProperties.TITLE_ID);
+                    assertNotEquals(0, titleId);
+
+                    String title = mHostStation.getActivity().getString(titleId);
+                    return text.equals(title);
+                }
+                return false;
+            }
+        };
+    }
 }
