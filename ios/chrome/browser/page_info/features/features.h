b7d350f8130d0d6025aadfee432a641ad0897e43
Gauthier Ambard <gambard@chromium.org>
2025-12-15 02:59:16 -0800

    [iOS] Prepare to move page info
    To be landed before crrev.com/i/8840756.
    
    Bug: None
    Change-Id: I7a31b053428a8f6810405225132c9c591d6699b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7260701
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1558637}

---
+++ ios/chrome/browser/page_info/features/features.h
@@ -0,0 +1,10 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PAGE_INFO_FEATURES_FEATURES_H_
+#define IOS_CHROME_BROWSER_PAGE_INFO_FEATURES_FEATURES_H_
+
+#import "ios/chrome/browser/page_info/ui_bundled/features.h"
+
+#endif  // IOS_CHROME_BROWSER_PAGE_INFO_FEATURES_FEATURES_H_
---
+++ ios/chrome/browser/page_info/test/page_info_app_interface.h
@@ -0,0 +1,10 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_PAGE_INFO_TEST_PAGE_INFO_APP_INTERFACE_H_
+#define IOS_CHROME_BROWSER_PAGE_INFO_TEST_PAGE_INFO_APP_INTERFACE_H_
+
+#import "ios/chrome/browser/page_info/ui_bundled/page_info_app_interface.h"
+
+#endif  // IOS_CHROME_BROWSER_PAGE_INFO_TEST_PAGE_INFO_APP_INTERFACE_H_
