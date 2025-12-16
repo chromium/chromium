8363b9a9b1433959024f6d9892c091399f7b1c02
Phil Yan <philyan@chromium.org>
2025-12-15 17:36:00 -0800

    [Auto-PiP] Temporarily add device restriction to testCanAutopipWithConferenceCall
    Skip this test on non-desktop devices while investigating the cause of
    the flakiness. This won't cause regression as auto-pip is only launched
    on android desktop for now.
    
    Bug: 469068794
    Change-Id: I4c16fa89300d7e52720361d2c107761c46b4aa7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262104
    Reviewed-by: Benjamin Keen <bkeen@google.com>
    Auto-Submit: Phil Yan <philyan@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559093}

--- chrome/browser/picture_in_picture/test/android/java/src/org/chromium/chrome/browser/picture_in_picture/AutoPictureInPictureTabHelperTest.java
+++ chrome/browser/picture_in_picture/test/android/java/src/org/chromium/chrome/browser/picture_in_picture/AutoPictureInPictureTabHelperTest.java
@@ -59,6 +59,7 @@ import org.chromium.content_public.browser.test.util.JavaScriptUtils;
 import org.chromium.content_public.common.ContentSwitches;
 import org.chromium.media.MediaFeatures;
 import org.chromium.media.MediaSwitches;
+import org.chromium.ui.base.DeviceFormFactor;
 
 import java.util.List;
 import java.util.concurrent.TimeoutException;
@@ -145,6 +146,8 @@ public class AutoPictureInPictureTabHelperTest {
 
     @Test
     @MediumTest
+    // TODO(crbug.com/469068794): fix test flakiness before launch on all android devices
+    @Restriction(DeviceFormFactor.DESKTOP)
     public void testCanAutopipWithConferenceCall() {
         WebContents webContents = loadUrlAndInitializeForTest(VIDEO_CONFERENCING_PAGE);
         // Verify if the loaded page registers auto pip.
