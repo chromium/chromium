0733bb7f02ab90ec818d9edb3a58544d31524a12
Dominic Battre <battre@chromium.org>
2025-12-16 02:08:32 -0800

    Prevent undefined behavior in gmock tests.
    As per
    https://google.github.io/googletest/gmock_cook_book.html#forcing-a-verification:
    "Do not set new expectations after verifying and clearing a mock after
    its use. Setting expectations after code that exercises the mock has
    undefined behavior. See Using Mocks in Tests for more information."
    
    This was generated with the following prompt:
    Read the sections "Forcing a Verification" and "Using Checkpoints" of
    https://google.github.io/googletest/gmock_cook_book.html. Then search
    for all invocations of VerifyAndClearExpectations in files that contain
    the word "autofill" in the filename or path and check if they use the
    pattern of calling VerifyAndClearExpectations and then setting new
    expectations. If you find them, fix them as described in the "Using
    Checkpoints" section. To make sure that you don't miss any cases, do
    this is steps: First assemble the list of cases that need to be
    investigated, then identify which files need to be fixed, then fix them
    one by one.
    
    Bug: 40100455
    Change-Id: I50b6ae7276feaecf4a07eda3a4277ab36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7241688
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559209}

--- chrome/browser/ui/views/autofill/popup/popup_view_views_unittest.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views_unittest.cc
@@ -1389,25 +1389,34 @@ TEST_F(PopupViewViewsTest, RemoveLine) {
                      SuggestionType::kAddressEntry,
                      SuggestionType::kManageAddress});
 
+  MockFunction<void(std::string_view)> check;
+  {
+    InSequence s;
+    EXPECT_CALL(controller(), RemoveSuggestion).Times(0);
+    EXPECT_CALL(check, Call("1: verify no RemoveSuggestion calls"));
+
+    EXPECT_CALL(controller(), RemoveSuggestion).Times(0);
+    EXPECT_CALL(check, Call("2: verify no RemoveSuggestion calls"));
+
+    EXPECT_CALL(controller(),
+                RemoveSuggestion(1, AutofillMetrics::SingleEntryRemovalMethod::
+                                        kKeyboardShiftDeletePressed));
+  }
+
   // If no cell is selected, pressing delete has no effect.
   EXPECT_FALSE(view().GetSelectedCell().has_value());
-  EXPECT_CALL(controller(), RemoveSuggestion).Times(0);
   SimulateKeyPress(ui::VKEY_DELETE, /*shift_modifier_pressed=*/true);
-  Mock::VerifyAndClearExpectations(&controller());
+  check.Call("1: verify no RemoveSuggestion calls");
 
   view().SetSelectedCell(CellIndex{1u, CellType::kContent},
                          PopupCellSelectionSource::kNonUserInput);
   EXPECT_EQ(view().GetSelectedCell(),
             std::make_optional<CellIndex>(1u, CellType::kContent));
 
-  EXPECT_CALL(controller(), RemoveSuggestion).Times(0);
   // If no shift key is pressed, no suggestion is removed.
   SimulateKeyPress(ui::VKEY_DELETE, /*shift_modifier_pressed=*/false);
-  Mock::VerifyAndClearExpectations(&controller());
+  check.Call("2: verify no RemoveSuggestion calls");
 
-  EXPECT_CALL(controller(),
-              RemoveSuggestion(1, AutofillMetrics::SingleEntryRemovalMethod::
-                                      kKeyboardShiftDeletePressed));
   SimulateKeyPress(ui::VKEY_DELETE, /*shift_modifier_pressed=*/true);
 }
 
