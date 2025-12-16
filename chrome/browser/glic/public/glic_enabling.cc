9dd5694425f3d4b97af2a5a0358d498c0e6b82e0
Nick Birnie <birnie@google.com>
2025-12-16 13:43:36 -0800

    glic: Extend countries to AU/CA/NZ
    Bug: b:467721693
    Change-Id: I81cd034f0045c99007abf73f8085eaf7dca3e847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7248917
    Reviewed-by: Dan Harrington <harringtond@chromium.org>
    Commit-Queue: Nick Birnie <birnie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559559}

--- chrome/browser/glic/public/glic_enabling.cc
+++ chrome/browser/glic/public/glic_enabling.cc
@@ -64,7 +64,7 @@ namespace glic {
 
 // Comma separated list of countries to enable GLIC, by default, if country
 // filtering is enabled.
-constexpr char kDefaultEnabledCountries[] = "us";
+constexpr char kDefaultEnabledCountries[] = "us,au,ca,nz";
 
 // Feature flag kGlicLocaleFiltering controls whether locale filtering is
 // applied client side. Two finch params are used to control this, both are a
