672f8e7287c38a8b4780633a32706a0b080fe292
Darryl James <dljames@chromium.org>
2025-12-16 12:30:47 -0800

    Reland "[VerticalTabs] Re-enable all flaky tests on Windows"
    This reverts commit 2c90ff7110af57a1cfd15ecfb6cd9f4aa5ae6ad4.
    
    Reason for revert: Fix for layout related issues found here crrev.com/c/7258875
    
    Original change's description:
    > Revert "[VerticalTabs] Re-enable all flaky tests on Windows"
    >
    > This reverts commit 8cf8d413f28c600d9aa358704b626f88f5ed3009.
    >
    > Reason for revert: Test still flaking
    >
    > Original change's description:
    > > [VerticalTabs] Re-enable all flaky tests on Windows
    > >
    > > Regarding the change in crrev.com/c/7253837, previously disabled
    > > VerticalTabStrip tests are being re-enabled since it is believed the
    > > issues causing the tests to be flaky have been resolved at this point in
    > > time. Fixed by this CL - crrev.com/c/7254711 - where the order of
    > > entering vertical tabs mode was changed to happen before the browser was
    > > setup. This prevents a race condition where both tabstrips (horizontal
    > > and vertical) are referenced within the same test.
    > >
    > > Change-Id: I48500c3b6b5c3791303660496b3914d7f6f045ed
    > > Bug: 464486134
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256795
    > > Reviewed-by: Alison Gale <agale@chromium.org>
    > > Commit-Queue: Darryl James <dljames@chromium.org>
    > > Reviewed-by: Caroline Rising <corising@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1558911}
    >
    > Bug: 464486134
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I5c4929874bcf2d9ce101e7f490f4b1ce8aa8467a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265413
    > Reviewed-by: Bruno Braga <brunobraga@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Bruno Braga <brunobraga@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1559338}
    
    Bug: 464486134
    Change-Id: I3e6521a24501109039e1a204f98e88d349c14cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7264614
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559512}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view_browsertest.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view_browsertest.cc
@@ -71,9 +71,6 @@ class VerticalTabViewTest
   }
 };
 
-// TODO(crbug.com/464486134): All test flaky on Windows.
-#if !BUILDFLAG(IS_WIN)
-
 IN_PROC_BROWSER_TEST_F(VerticalTabViewTest, IconDataChanged) {
   ASSERT_TRUE(embedded_test_server()->Start());
   std::unique_ptr<views::View> parent_view = std::make_unique<views::View>();
@@ -343,5 +340,3 @@ IN_PROC_BROWSER_TEST_F(VerticalTabViewTest, PinnedTabsRenderBorder) {
 
   EXPECT_FALSE(tab->GetBorder());
 }
-
-#endif  // !BUILDFLAG(IS_WIN)
