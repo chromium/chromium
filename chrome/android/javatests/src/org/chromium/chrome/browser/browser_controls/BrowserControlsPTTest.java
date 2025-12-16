2de6e07e0d269370b4f3e3cf8d12b3361dd39d13
Kay Lin <kaiyilin@google.com>
2025-12-15 20:09:38 -0800

    Revert "Re-enable BrowserControlsPTTest"
    This reverts commit 433773cb0e74a2f6d54e0ebccd82615377af1f00.
    
    Reason for revert: Re-enable the test causes another test failure `java.lang.AssertionError: Visible content never moved as expected.
    `. Check https://ci.chromium.org/ui/p/chrome/builders/ci/android-automotive-12l-x64-rel-tests/16441/overview for more detail.
    
    Original change's description:
    > Re-enable BrowserControlsPTTest
    >
    > Flakiness caused by unrelated feature flag check. See crbug.com/469108637. Stack:
    > https://paste.googleplex.com/4896078128349184
    >
    > Validate-Test-Flakiness: skip
    > Fixed: 455558607, 455947814
    > Change-Id: Ie03c12fdfa8a78fe5e167249729fbbd810635f28
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7255023
    > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1558980}
    
    Bug: 455558607, 455947814
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I276addb8db7b126da84232c4e24ad6ad329b77ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7260378
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559123}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browser_controls/BrowserControlsPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browser_controls/BrowserControlsPTTest.java
@@ -17,12 +17,11 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
-import org.chromium.chrome.browser.ViewportTestUtils;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.fullscreen.FullscreenManagerTestUtils;
+import org.chromium.chrome.browser.tab.TabStateBrowserControlsVisibilityDelegate;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.transit.AutoResetCtaTransitTestRule;
@@ -36,39 +35,34 @@ import org.chromium.chrome.test.transit.testhtmls.TopBottomLinksPageStation;
     ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE,
     ChromeSwitches.DISABLE_MINIMUM_SHOW_DURATION
 })
-@EnableFeatures({
-    ChromeFeatureList.TOP_CONTROLS_REFACTOR,
-    ChromeFeatureList.TOP_CONTROLS_REFACTOR_V2
-})
 public class BrowserControlsPTTest {
 
     @Rule
-    public AutoResetCtaTransitTestRule mActivityTestRule =
+    public AutoResetCtaTransitTestRule sActivityTestRule =
             ChromeTransitTestRules.fastAutoResetCtaActivityRule();
 
     private TopBottomLinksPageStation.TopFacility mTopFacility;
     private ChromeTabbedActivity mActivity;
     private View mControlContainer;
-    private ViewportTestUtils mViewportTestUtils;
 
     @Before
     public void setup() {
-        mViewportTestUtils = new ViewportTestUtils(mActivityTestRule);
-        mViewportTestUtils.setUpForBrowserControls();
-
-        var blankPage = mActivityTestRule.startOnBlankPage();
+        var blankPage = sActivityTestRule.startOnBlankPage();
         var pair =
                 TopBottomLinksPageStation.loadPage(
-                        mActivityTestRule.getActivityTestRule(), blankPage);
+                        sActivityTestRule.getActivityTestRule(), blankPage);
         mTopFacility = pair.second;
-        mActivity = mActivityTestRule.getActivity();
+        mActivity = sActivityTestRule.getActivity();
         mControlContainer = mActivity.findViewById(R.id.control_container);
 
-        FullscreenManagerTestUtils.waitForBrowserControlsToBeMoveable(mActivity);
+        FullscreenManagerTestUtils.disableBrowserOverrides();
+        ThreadUtils.runOnUiThreadBlocking(
+                TabStateBrowserControlsVisibilityDelegate::disablePageLoadDelayForTests);
     }
 
     @Test
     @LargeTest
+    @DisabledTest(message = "Flaky, see crbug.com/455947814")
     public void topControlsScroll() {
         waitForControlsVisibility(true);
 
@@ -81,29 +75,21 @@ public class BrowserControlsPTTest {
 
     @Test
     @LargeTest
+    @DisabledTest(message = "Flaky, see crbug.com/455558607")
     public void topControlsScrollingDisabled() {
         // before test case - set scrolling disabled
         TopControlsStacker topControlsStacker =
                 mActivity.getRootUiCoordinatorForTesting().getTopControlsStacker();
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    topControlsStacker.setScrollingDisabled(true);
-                    topControlsStacker.requestLayerUpdateSync(false);
-                });
+        ThreadUtils.runOnUiThreadBlocking(() -> topControlsStacker.setScrollingDisabled(true));
 
         mTopFacility.scrollToBottom();
         waitForControlsVisibility(true);
 
         // after test case - set scrolling back.
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    topControlsStacker.setScrollingDisabled(false);
-                    topControlsStacker.requestLayerUpdateSync(false);
-                });
+        ThreadUtils.runOnUiThreadBlocking(() -> topControlsStacker.setScrollingDisabled(false));
     }
 
     private void waitForControlsVisibility(boolean visible) {
-        mViewportTestUtils.waitForBrowserControlsState(visible);
         int expectedVisibility = visible ? View.VISIBLE : View.INVISIBLE;
         CriteriaHelper.pollUiThread(() -> mControlContainer.getVisibility() == expectedVisibility);
     }
