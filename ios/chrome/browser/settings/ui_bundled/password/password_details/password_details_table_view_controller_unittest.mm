f63c4c5f807a363aac67d5db237874e59872f6de
Angela Novakovic <novakovica@google.com>
2025-12-17 13:41:43 -0800

    [iOS] Enabled PasswordDetailsTableViewControllerTest.CopySite.
    The unit test seems to no longer fail on iPad Air (5th generation) iOS
    26.0. Changes to the code between now and August 2025 might have solved
    the issue. They include a custom 'FakeSnackBarImplementation` and
    addition of 'last used' timestamp to
    `password_manger::PasskeyCredential`.
    
    Referred CL:https://chromium-review.googlesource.com/c/chromium/src/+/7163715.
    
    Fixed: 440059889
    Change-Id: I03fb9bf336794edd74d8e09e04f28c3ca1a3171f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7270425
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560172}

--- ios/chrome/browser/settings/ui_bundled/password/password_details/password_details_table_view_controller_unittest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_details/password_details_table_view_controller_unittest.mm
@@ -787,10 +787,6 @@ TEST_F(PasswordDetailsTableViewControllerTest, TestBlockedOrigin) {
 
 // Tests copy website works as intended.
 TEST_F(PasswordDetailsTableViewControllerTest, CopySite) {
-  // TODO(crbug.com/440059889): Re-enable the test on iOS26.
-  if (base::ios::IsRunningOnIOS26OrLater()) {
-    return;
-  }
   std::vector<std::string> websites = {kExampleCom};
   NSString* expected_pasteboard = HTTPWebsite();
   CheckCopyWebsites(
