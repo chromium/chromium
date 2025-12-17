433227d503fe9c1b1c6e9eed8c90bc32ab41e823
Asami Doi <asamidoi@chromium.org>
2025-12-17 02:28:59 -0800

    [ios] Add tests to use Swift's API from Obj-C with C++/Swift enabled
    Add more test cases to use Swift's API from Obj-C/Obj-C++ with the
    C++/Swift feature enabled.
    
    Bug: 465115095
    Change-Id: I361c61ce0f31b6723252ee963f280699094232f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7107019
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1559835}

--- ios/chrome/test/swift_interop/objc_xctest.mm
+++ ios/chrome/test/swift_interop/objc_xctest.mm
@@ -6,6 +6,7 @@
 #import <XCTest/XCTest.h>
 
 #import "base/logging.h"
+#import "ios/chrome/test/swift_interop/swift_api.h"
 #import "ios/chrome/test/swift_interop/swift_interop_tests.h"
 
 @interface ObjCInteropTestCase : XCTestCase
@@ -25,6 +26,57 @@
   BOOL success = [test testEnumsAndReturnError:&error];
   XCTAssertTrue(success);
   XCTAssertNil(error, @"Error: %@", error);
+}
+
+- (void)testSwiftString {
+  swift::String string = "string";
+  swift::String updatedString =
+      ios_chrome_test_swift_interop_swift_api::updateSwiftString(string);
+
+  XCTAssertFalse(updatedString.isEmpty());
+  XCTAssertEqual(14, updatedString.getCount());
+  XCTAssertEqual("string updated", (std::string)updatedString);
+}
+
+- (void)testSwiftArray {
+  auto array = swift::Array<swift::Int>::init();
+  array.append(0);
+  XCTAssertEqual(1, array.getCount());
+
+  swift::Array<swift::Int> updatedArray =
+      ios_chrome_test_swift_interop_swift_api::updateSwiftArray(array);
+  XCTAssertEqual(2, array.getCount());
+  XCTAssertEqual(0, updatedArray[0]);
+  XCTAssertEqual(42, updatedArray[1]);
+}
+
+- (void)testSwiftOptional {
+  {
+    swift::Optional<swift::String> string =
+        ios_chrome_test_swift_interop_swift_api::createOptionalString(true);
+    XCTAssertFalse(string.get().isEmpty());
+    XCTAssertEqual(14, string.get().getCount());
+    XCTAssertEqual("string created", (std::string)string.get());
+  }
+
+  {
+    swift::Optional<swift::String> string =
+        ios_chrome_test_swift_interop_swift_api::createOptionalString(false);
+    XCTAssertFalse(string);
+  }
+}
+
+- (void)testSwiftStruct {
+  ios_chrome_test_swift_interop_swift_api::TestStruct obj =
+      ios_chrome_test_swift_interop_swift_api::TestStruct::init("name", 1);
+  XCTAssertEqual(1, obj.getNumber());
+  XCTAssertEqual("name", (std::string)obj.getName());
+
+  obj.incrementNumber();
+  obj.updateName("new name");
+
+  XCTAssertEqual(2, obj.getNumber());
+  XCTAssertEqual("new name", (std::string)obj.getName());
 }
 
 @end
