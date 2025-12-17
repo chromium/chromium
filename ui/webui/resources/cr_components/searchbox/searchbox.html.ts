316f836e012ab817820f194114277299a41f4c94
Dhruv Kathpalia <dhruvkathpalia@google.com>
2025-12-17 13:16:25 -0800

    [ntp-realbox] Reorder searchbox elements focus for compact layout
    This change introduces a "compact layout" mode for the searchbox, which
    reorders the dropdown, input, and buttons. The dropdown is placed above
    the input field in compact mode.
    Screencast:
    http://screencast/cast/NTMwOTU3OTc5MjQ4MjMwNHw0ODRmZDRlMC0yOQ
    
    Bug: b:461871718
    Change-Id: Ib8d4d21de7b5f9f6262ed584bd87d5d4026909a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7213125
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Hannah Collins <hannahrogers@google.com>
    Cr-Commit-Position: refs/heads/main@{#1560152}

--- ui/webui/resources/cr_components/searchbox/searchbox.html.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox.html.ts
@@ -8,93 +8,11 @@ import type {SearchboxElement} from './searchbox.js';
 
 export function getHtml(this: SearchboxElement) {
   // clang-format off
-  return html`<!--_html_template_start_-->
-${this.ntpRealboxNextEnabled ? html`
-<ntp-error-scrim id="errorScrim"
-    ?compact-mode="${this.searchboxLayoutMode === 'Compact'}">
-</ntp-error-scrim>` : nothing}
-<div id="inputWrapper" @focusout="${this.onInputWrapperFocusout_}"
-    @keydown="${this.onInputWrapperKeydown_}"
-    @dragenter="${this.dragAndDropHandler?.handleDragEnter}"
-    @dragover="${this.dragAndDropHandler?.handleDragOver}"
-    @dragleave="${this.dragAndDropHandler?.handleDragLeave}"
-    @drop="${this.dragAndDropHandler?.handleDrop}">
-  <input id="input" class="truncate" type="search" autocomplete="off"
-      part="searchbox-input"
-      spellcheck="false" aria-live="${this.inputAriaLive_}" role="combobox"
-      aria-expanded="${this.dropdownIsVisible}" aria-controls="matches"
-      aria-description="${this.searchboxAriaDescription}"
-      placeholder="${this.computePlaceholderText_(this.placeholderText)}"
-      @copy="${this.onInputCutCopy_}"
-      @cut="${this.onInputCutCopy_}" @focus="${this.onInputFocus_}"
-      @focusout="${this.onInputFocusout_}"
-      @input="${this.onInputInput_}" @keydown="${this.onInputKeydown_}"
-      @keyup="${this.onInputKeyup_}" @mousedown="${this.onInputMouseDown_}"
-      @paste="${this.onInputPaste_}">
-  </input>
-  <cr-searchbox-icon id="icon" .match="${this.selectedMatch_}"
-      default-icon="${this.searchboxIcon_}" in-searchbox>
-  </cr-searchbox-icon>
-  ${this.showThumbnail ? html`
-    <div id="thumbnailContainer">
-      <!--Tabindex is set to 1 so that the thumbnail is tabbed first,
-        then the search box. -->
-      <cr-searchbox-thumbnail id="thumbnail" thumbnail-url_="${this.thumbnailUrl_}"
-          ?is-deletable_="${this.isThumbnailDeletable_}"
-          @remove-thumbnail-click="${this.onRemoveThumbnailClick_}"
-          role="button" aria-label="${this.i18n('searchboxThumbnailLabel')}"
-          tabindex="${this.getThumbnailTabindex_()}">
-      </cr-searchbox-thumbnail>
-    </div>
-  ` : nothing}
+  const compactLayout = this.ntpRealboxNextEnabled && this.searchboxLayoutMode === 'Compact';
 
-  ${this.composeButtonEnabled ? html`
-    <cr-searchbox-compose-button id="composeButton"
-        @compose-click="${this.onComposeButtonClick_}">
-    </cr-searchbox-compose-button>
-  ` : nothing}
-
-  ${this.ntpRealboxNextEnabled ? html`
-    <div class="dropdownContainer">
-      <search-animated-glow animation-state="${this.animationState}" part="animated-glow">
-      </search-animated-glow>
-      <contextual-entrypoint-and-carousel id="context"
-          part="contextual-entrypoint-and-carousel"
-          exportparts="composebox-entrypoint, context-menu-entrypoint-icon, voice-icon"
-          .tabSuggestions="${this.tabSuggestions_}"
-          entrypoint-name="Realbox"
-          @add-tab-context="${this.addTabContext_}"
-          @add-file-context="${this.addFileContext_}"
-          @on-file-validation-error="${this.onFileValidationError_}"
-          @set-deep-search-mode="${this.setDeepSearchMode_}"
-          @set-create-image-mode="${this.setCreateImageMode_}"
-          @open-voice-search="${this.onVoiceSearchClick_}"
-          @get-tab-preview="${this.getTabPreview_}"
-          @context-menu-container-click="${this.onContextMenuContainerClick_}"
-          ?show-dropdown="${this.dropdownIsVisible}"
-          ?show-voice-search="${this.shouldShowVoiceSearch_}"
-          searchbox-layout-mode="${this.searchboxLayoutMode}"
-          context-menu-glif-animation-state="${this.contextMenuGlifAnimationState}">
-        <cr-searchbox-dropdown id="matches" part="searchbox-dropdown"
-            exportparts="dropdown-content"
-            role="listbox" .result="${this.result_}"
-            selected-match-index="${this.selectedMatchIndex_}"
-            @selected-match-index-changed="${this.onSelectedMatchIndexChanged_}"
-            ?can-show-secondary-side="${this.canShowSecondarySide}"
-            ?had-secondary-side="${this.hadSecondarySide}"
-            @had-secondary-side-changed="${this.onHadSecondarySideChanged_}"
-            ?has-secondary-side="${this.hasSecondarySide}"
-            @has-secondary-side-changed="${this.onHasSecondarySideChanged_}"
-            @match-focusin="${this.onMatchFocusin_}"
-            @match-click="${this.onMatchClick_}"
-            ?hidden="${!this.dropdownIsVisible}"
-            ?show-thumbnail="${this.showThumbnail}">
-        </cr-searchbox-dropdown>
-      </contextual-entrypoint-and-carousel>
-    </div>
-  ` : html`
-    <cr-searchbox-dropdown class="dropdownContainer" id="matches"
-        part="searchbox-dropdown"
+  const dropdown = html`
+    <cr-searchbox-dropdown id="matches" part="searchbox-dropdown"
+        class="${!this.ntpRealboxNextEnabled ? 'dropdownContainer' : nothing}"
         exportparts="dropdown-content"
         role="listbox" .result="${this.result_}"
         selected-match-index="${this.selectedMatchIndex_}"
@@ -108,28 +26,136 @@ ${this.ntpRealboxNextEnabled ? html`
         @match-click="${this.onMatchClick_}"
         ?hidden="${!this.dropdownIsVisible}"
         ?show-thumbnail="${this.showThumbnail}">
-    </cr-searchbox-dropdown>
+    </cr-searchbox-dropdown>`;
+
+  const contextualEntrypoint = html`
+    <contextual-entrypoint-and-carousel id="context"
+        part="contextual-entrypoint-and-carousel"
+        exportparts="composebox-entrypoint, context-menu-entrypoint-icon, voice-icon"
+        .tabSuggestions="${this.tabSuggestions_}"
+        entrypoint-name="Realbox"
+        @add-tab-context="${this.addTabContext_}"
+        @add-file-context="${this.addFileContext_}"
+        @on-file-validation-error="${this.onFileValidationError_}"
+        @set-deep-search-mode="${this.setDeepSearchMode_}"
+        @set-create-image-mode="${this.setCreateImageMode_}"
+        @open-voice-search="${this.onVoiceSearchClick_}"
+        @get-tab-preview="${this.getTabPreview_}"
+        @context-menu-container-click="${this.onContextMenuContainerClick_}"
+        ?show-dropdown="${this.dropdownIsVisible}"
+        ?show-voice-search="${this.shouldShowVoiceSearch_}"
+        searchbox-layout-mode="${this.searchboxLayoutMode}"
+        context-menu-glif-animation-state="${this.contextMenuGlifAnimationState}">
+      ${!compactLayout ? dropdown : nothing}
+    </contextual-entrypoint-and-carousel>`;
+
+  const inputContent = html`
+    <input id="input" class="truncate" type="search" autocomplete="off"
+        part="searchbox-input"
+        spellcheck="false" aria-live="${this.inputAriaLive_}" role="combobox"
+        aria-expanded="${this.dropdownIsVisible}" aria-controls="matches"
+        aria-description="${this.searchboxAriaDescription}"
+        placeholder="${this.computePlaceholderText_(this.placeholderText)}"
+        @copy="${this.onInputCutCopy_}"
+        @cut="${this.onInputCutCopy_}" @focus="${this.onInputFocus_}"
+        @focusout="${this.onInputFocusout_}"
+        @input="${this.onInputInput_}" @keydown="${this.onInputKeydown_}"
+        @keyup="${this.onInputKeyup_}" @mousedown="${this.onInputMouseDown_}"
+        @paste="${this.onInputPaste_}">
+    </input>
+    <cr-searchbox-icon id="icon" .match="${this.selectedMatch_}"
+        default-icon="${this.searchboxIcon_}" in-searchbox>
+    </cr-searchbox-icon>
+    ${this.showThumbnail ? html`
+      <div id="thumbnailContainer">
+        <cr-searchbox-thumbnail id="thumbnail" thumbnail-url_="${this.thumbnailUrl_}"
+            ?is-deletable_="${this.isThumbnailDeletable_}"
+            @remove-thumbnail-click="${this.onRemoveThumbnailClick_}"
+            role="button" aria-label="${this.i18n('searchboxThumbnailLabel')}"
+            tabindex="${this.getThumbnailTabindex_()}">
+        </cr-searchbox-thumbnail>
+      </div>
+    ` : nothing}`;
+
+  const voiceSearchButton = html`
+    ${this.searchboxVoiceSearchEnabled_ ? html`
+      <div class="searchbox-icon-button-container voice">
+        <button id="voiceSearchButton" class="searchbox-icon-button"
+            @click="${this.onVoiceSearchClick_}"
+            title="${this.i18n('voiceSearchButtonLabel')}">
+        </button>
+      </div>
+    ` : nothing}`;
+
+  const lensSearchButton = html`
+    ${this.searchboxLensSearchEnabled_ ? html`
+      <div class="searchbox-icon-button-container lens">
+        <button id="lensSearchButton" class="searchbox-icon-button lens"
+            @click="${this.onLensSearchClick_}"
+            title="${this.i18n('lensSearchButtonLabel')}">
+        </button>
+      </div>
+    ` : nothing}`;
+
+  const composeButton = html`
+    ${this.composeButtonEnabled ? html`
+      <cr-searchbox-compose-button id="composeButton"
+          @compose-click="${this.onComposeButtonClick_}">
+      </cr-searchbox-compose-button>
+    ` : nothing}`;
+
+  return html`<!--_html_template_start_-->
+${this.ntpRealboxNextEnabled ? html`
+<ntp-error-scrim id="errorScrim"
+    ?compact-mode="${this.searchboxLayoutMode === 'Compact'}">
+</ntp-error-scrim>` : nothing}
+<div id="inputWrapper" @focusout="${this.onInputWrapperFocusout_}"
+    @keydown="${this.onInputWrapperKeydown_}"
+    @dragenter="${this.dragAndDropHandler?.handleDragEnter}"
+    @dragover="${this.dragAndDropHandler?.handleDragOver}"
+    @dragleave="${this.dragAndDropHandler?.handleDragLeave}"
+    @drop="${this.dragAndDropHandler?.handleDrop}">
+  ${compactLayout ?
+    html`
+      <div class="contextualEntrypointContainer contextualEntrypointContainerCompact">
+        ${contextualEntrypoint}
+      </div>
+      ${inputContent}
+      ${voiceSearchButton}
+      ${lensSearchButton}
+      ${composeButton}
+      <div class="dropdownContainer">
+        <search-animated-glow animation-state="${this.animationState}" part="animated-glow">
+        </search-animated-glow>
+        ${dropdown}
+        ${this.recentTabForChip_ && this.dropdownIsVisible ? html`
+        <div id="recentTabChipContainer">
+          <composebox-recent-tab-chip
+              .recentTab="${this.recentTabForChip_}"
+              @add-tab-context="${this.addTabContext_}">
+          </composebox-recent-tab-chip>
+        </div>
+        ` : nothing}
+      </div>
+    ` :
+    html`
+      ${inputContent}
+      ${composeButton}
+      ${this.ntpRealboxNextEnabled ? html`
+        <div class="contextualEntrypointContainer">
+          <search-animated-glow animation-state="${this.animationState}" part="animated-glow">
+          </search-animated-glow>
+          ${contextualEntrypoint}
+        </div>
+      ` : dropdown}
   `}
 </div>
 
-  ${this.searchboxVoiceSearchEnabled_ ? html`
-    <div class="searchbox-icon-button-container voice">
-      <button id="voiceSearchButton" class="searchbox-icon-button"
-          @click="${this.onVoiceSearchClick_}"
-          title="${this.i18n('voiceSearchButtonLabel')}">
-      </button>
-    </div>
-  ` : nothing}
-
-  ${this.searchboxLensSearchEnabled_ ? html`
-    <div class="searchbox-icon-button-container lens">
-      <button id="lensSearchButton" class="searchbox-icon-button lens"
-          @click="${this.onLensSearchClick_}"
-          title="${this.i18n('lensSearchButtonLabel')}">
-      </button>
-    </div>
-  ` : nothing}
-
+${compactLayout ? nothing :
+    html`
+      ${voiceSearchButton}
+      ${lensSearchButton}
+    `}
 <!--_html_template_end_-->`;
   // clang-format on
 }
