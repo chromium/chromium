8b0c8d29eefb7bb2b1d37430e2bbcc4560984e8b
Muyao Xu <muyaoxu@google.com>
2025-12-16 22:39:50 -0800

    [Gardener] Disable the flaky TouchSelectionControllerClientAuraSiteIsolationTest
    Bug: 468430621
    Change-Id: Ibf0f1a57144ed88a0f2ec4333ee602ac439289db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7257203
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Owners-Override: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1559754}

--- content/browser/renderer_host/input/touch_selection_controller_client_aura_browsertest.cc
+++ content/browser/renderer_host/input/touch_selection_controller_client_aura_browsertest.cc
@@ -1102,8 +1102,16 @@ IN_PROC_BROWSER_TEST_F(TouchSelectionControllerClientAuraTest,
 
 // Tests that touch selection dragging adjusts the selection using a direction
 // strategy (roughly, expands by word and shrinks by character).
+// TODO(crbug.com/468430621): Re-enable this test on Linux.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_SelectionDraggingDirectionStrategy \
+  DISABLED_SelectionDraggingDirectionStrategy
+#else
+#define MAYBE_SelectionDraggingDirectionStrategy \
+  SelectionDraggingDirectionStrategy
+#endif
 IN_PROC_BROWSER_TEST_F(TouchSelectionControllerClientAuraTest,
-                       SelectionDraggingDirectionStrategy) {
+                       MAYBE_SelectionDraggingDirectionStrategy) {
   // Set the test page up.
   ASSERT_NO_FATAL_FAILURE(StartTestWithPage("/touch_selection.html"));
 
