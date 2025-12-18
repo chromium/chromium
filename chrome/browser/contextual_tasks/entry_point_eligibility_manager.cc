e0586638d7d23d1be930bb736e00b5a03b441265
Steven Luong <stluong@chromium.org>
2025-12-18 13:39:45 -0800

    [ContextualTasks] Entry points depend on contextual task eligibility
    The contextual tasks entry points should show if the contextual tasks
    service is eligible. Otherwise the entry points should be hidden.
    
    Bug: 467774686
    Change-Id: Ie23d6bb16eae2985de442b65fc11a5921d024124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7269369
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560802}

--- chrome/browser/contextual_tasks/entry_point_eligibility_manager.cc
+++ chrome/browser/contextual_tasks/entry_point_eligibility_manager.cc
@@ -5,9 +5,14 @@
 #include "chrome/browser/contextual_tasks/entry_point_eligibility_manager.h"
 
 #include "base/callback_list.h"
+#include "chrome/browser/autocomplete/aim_eligibility_service_factory.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks_context_controller.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks_context_controller_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "components/contextual_tasks/public/contextual_tasks_service.h"
+#include "components/omnibox/browser/aim_eligibility_service.h"
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/primary_account_change_event.h"
@@ -29,6 +34,16 @@ EntryPointEligibilityManager::EntryPointEligibilityManager(
     identity_manager_observation_.Observe(identity_manager);
   }
 
+  entry_points_are_eligible_ = AreEntryPointsEligible();
+  AimEligibilityService* const aim_eligibility_service =
+      AimEligibilityServiceFactory::GetForProfile(profile_);
+
+  CHECK(aim_eligibility_service);
+  aim_eligibility_callback_subscription_ =
+      aim_eligibility_service->RegisterEligibilityChangedCallback(
+          base::BindRepeating(&EntryPointEligibilityManager::
+                                  MaybeNotifyEntryPointEligibilityChanged,
+                              base::Unretained(this)));
   entry_points_are_eligible_ = AreEntryPointsEligible();
 }
 
@@ -59,8 +74,17 @@ bool EntryPointEligibilityManager::AreEntryPointsEligible() {
     return false;
   }
 
-  return !identity_manager->GetPrimaryAccountInfo(signin::ConsentLevel::kSignin)
-              .IsEmpty();
+  const bool is_browser_logged_in =
+      !identity_manager->GetPrimaryAccountInfo(signin::ConsentLevel::kSignin)
+           .IsEmpty();
+
+  ContextualTasksService* const contextual_task_service =
+      ContextualTasksContextControllerFactory::GetForProfile(profile_);
+  CHECK(contextual_task_service);
+  const bool is_feature_eligible =
+      contextual_task_service->GetFeatureEligibility().IsEligible();
+
+  return is_feature_eligible && is_browser_logged_in;
 }
 
 void EntryPointEligibilityManager::MaybeNotifyEntryPointEligibilityChanged() {
