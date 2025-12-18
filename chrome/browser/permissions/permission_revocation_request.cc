c5f1ca2fd5e3322186f07873e650485b79e8b4af
jj <jj@imput.net>
2025-12-18 15:54:34 -0800

    Fix building with safe_browsing_mode=0 (3/14)
    Fixes the following:
    ../../chrome/browser/permissions/permission_revocation_request.cc:87:6: error: unused function 'SetOriginStatus' [-Werror,-Wunused-function]
       87 | void SetOriginStatus(Profile* profile,
          |      ^~~~~~~~~~~~~~~
    
    Bug: 390205854
    Change-Id: I55007849a7eb1cc2755a45b0776bbf3285a308b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7278253
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: jj <jj@chromium.org>
    Auto-Submit: jj <jj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560893}

--- chrome/browser/permissions/permission_revocation_request.cc
+++ chrome/browser/permissions/permission_revocation_request.cc
@@ -19,13 +19,14 @@
 #include "components/permissions/permission_uma_util.h"
 #include "components/permissions/permissions_client.h"
 #include "components/prefs/pref_service.h"
+#include "components/safe_browsing/buildflags.h"
+
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
+#include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "components/safe_browsing/core/browser/db/database_manager.h"
 #include "components/safe_browsing/core/browser/safe_browsing_metrics_collector.h"
 #include "components/safe_browsing/core/common/features.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
-
-#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
-#include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #endif
 
 namespace {
@@ -84,6 +85,7 @@ void SetOriginStatusFromHostContentSettingsMap(HostContentSettingsMap* hcsm,
       base::Value(std::move(dict)));
 }
 
+#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 void SetOriginStatus(Profile* profile,
                      const GURL& origin,
                      const OriginStatus& status) {
@@ -92,7 +94,6 @@ void SetOriginStatus(Profile* profile,
       status);
 }
 
-#if BUILDFLAG(SAFE_BROWSING_AVAILABLE)
 void RevokePermission(const GURL& origin, Profile* profile) {
   if (base::FeatureList::IsEnabled(
           safe_browsing::kShowManualNotificationRevocationsSafetyHub)) {
