b2ab138157ef1f78d23136d770ae8f82a9d6a7d7
Rakina Zata Amni <rakina@chromium.org>
2025-12-17 22:06:24 -0800

    Disable ShiftMultiTabSelection test on macOS.
    The test VerticalTabStripControllerInteractiveUiTest.ShiftMultiTabSelection is failing on macOS and is being disabled.
    
    
    Bug: 469912247
    Change-Id: I21277bd8be92ab432d1a29cef902ebc4225b66ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7273505
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Owners-Override: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560376}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller_interactive_uitest.cc
@@ -124,8 +124,14 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripControllerInteractiveUiTest,
       WaitForHide(kFirstTabName));
 }
 
+// TODO(crbug.com/469912247): Fails on mac-rel-ready bot.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_ShiftMultiTabSelection DISABLED_ShiftMultiTabSelection
+#else
+#define MAYBE_ShiftMultiTabSelection ShiftMultiTabSelection
+#endif
 IN_PROC_BROWSER_TEST_F(VerticalTabStripControllerInteractiveUiTest,
-                       ShiftMultiTabSelection) {
+                       MAYBE_ShiftMultiTabSelection) {
   RunTestSequence(
       // Verify Vertical Tabs is showing.
       WaitForShow(kVerticalTabStripBottomContainerElementId),
