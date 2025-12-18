659c0b06e7e3519659c6eecb1e594c2e6784ae3c
Asami Doi <asamidoi@chromium.org>
2025-12-18 10:05:08 -0800

    [ios] Add a test case for std::vector with non-copyable elements
    This CL adds a test case to demonstrate how to use std::vector with
    non-copyable elements (e.g. std::unique_ptr). Swift treats std::vector
    as a value type and it expects that elements are copyable and it
    triggers the build error while generating a copy constructor for
    non-copyable elements. Wrapping a vector with an implicitly non-copyable
    class tells the Swift compiler to treat the vector as Non-Copyable Type
    instead of Value type.
    
    Bug: 465131182
    Change-Id: Id071190110f7333ae36f4b70e00089ebe1b0a315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7268730
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560659}

--- ios/chrome/test/swift_interop/stl/vector.cc
+++ ios/chrome/test/swift_interop/stl/vector.cc
@@ -4,10 +4,20 @@
 
 #include "ios/chrome/test/swift_interop/include/vector.h"
 
+SWIFT_DEFINE_MOVE_ONLY_INTEROP_WRAPPER(CxxStringVector, StringVector)
+
 IntVector GetFortyTwoVector() {
   return {42};
 }
 
 bool CheckFortyTwoInVector(const IntVector& input) {
   return input.size() == 1 && input[0] == 42;
+}
+
+CxxStringVector GetStringVector() {
+  std::vector<std::unique_ptr<std::string>> v;
+  v.push_back(std::make_unique<std::string>("a"));
+  v.push_back(std::make_unique<std::string>("b"));
+  v.push_back(std::make_unique<std::string>("c"));
+  return v;
 }
--- ios/chrome/test/swift_interop/stl/vector_xctest.swift
+++ ios/chrome/test/swift_interop/stl/vector_xctest.swift
@@ -5,6 +5,24 @@
 import Interop
 import XCTest
 
+// TODO(crbug.com/465131182): We manually implements subscript() to make an
+// array with move-only elements accessible via array[0].
+extension CxxStringVector {
+  subscript(index: Int) -> String {
+    let itemPtr = self.__atUnsafe(index)
+
+    // Rebind an unique pointer `UnsafePointer<std.unique_ptr<std.string>>` to
+    // a raw pointer `UnsafePointer<UnsafePointer<std.string>>` to bypass the
+    // "Move-Only" consumption check.
+    return itemPtr.withMemoryRebound(to: UnsafePointer<std.string>.self, capacity: 1) { rawPtrPtr in
+      // Read the inner pointer.
+      let stringPtr = rawPtrPtr.pointee
+      // Create a string based on the dereference of the raw pointer.
+      return String(stringPtr.pointee)
+    }
+  }
+}
+
 final class VectorTests: XCTestCase {
 
   func testVectorReturnValue() {
@@ -24,4 +42,10 @@ final class VectorTests: XCTestCase {
     XCTAssertTrue(CheckFortyTwoInVector(testVector))
   }
 
+  func testUniqueStringVector() {
+    let array = GetStringVector()
+    XCTAssertEqual(array[0], "a")
+    XCTAssertEqual(array[1], "b")
+    XCTAssertEqual(array[2], "c")
+  }
 }
