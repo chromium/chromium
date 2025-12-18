596e165265fe0d30b49189547590102030dcdc50
Gyuyoung Kim <gyuyoung@igalia.com>
2025-12-18 07:42:36 -0800

    [iOS Blink] Add a dummy NeedsBeginFrameWithId()
    When https://crrev.com/c/7256537 implemented the interface methods for
    mojom::ExternalBeginFrameController and
    mojom::ExternalBeginFrameControllerClient, it missed adding an
    implementation of NeedsBeginFrameWithId() for the iOS Blink port.
    
    This CL adds a stub (unimplemented) method to BeginFrameSourceIOS to fix
    the build break on the iOS Blink port.
    
    Bug: 436909728
    Change-Id: Ieb2afe232bc1ff6f5e4259dd633541c9b83abba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7273066
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1560554}

--- content/browser/renderer_host/begin_frame_source_ios.mm
+++ content/browser/renderer_host/begin_frame_source_ios.mm
@@ -5,6 +5,7 @@
 #include "content/browser/renderer_host/begin_frame_source_ios.h"
 
 #include "base/functional/callback_helpers.h"
+#include "base/notimplemented.h"
 
 namespace content {
 
@@ -55,6 +56,11 @@ void BeginFrameSourceIOS::SetNeedsBeginFrame(bool needs_begin_frames) {
   } else {
     begin_frame_source_.RemoveObserver(this);
   }
+}
+
+void BeginFrameSourceIOS::NeedsBeginFrameWithId(int64_t display_id,
+                                                bool needs_begin_frames) {
+  NOTIMPLEMENTED();
 }
 
 void BeginFrameSourceIOS::SetPreferredInterval(base::TimeDelta interval) {
