82bda9b67686ade55becfe3b6122e3a55ade842c
Kaan Alsan <alsan@chromium.org>
2025-12-18 12:13:21 -0800

    Add interactive ui test for vertical tab dragging
    Adds the first tests for detaching to a new window.
    
    Bug: 439963720
    Change-Id: Id4695e8fe89967496a4b84d89410c88826536c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262260
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1560745}

---
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_drag_interactive_uitest.cc
@@ -0,0 +1,161 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/functional/bind.h"
+#include "base/functional/callback_forward.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/tabs/vertical/vertical_tab_view.h"
+#include "chrome/browser/ui/views/test/vertical_tabs_interactive_test_mixin.h"
+#include "chrome/common/webui_url_constants.h"
+#include "chrome/test/base/interactive_test_utils.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
+#include "components/tabs/public/tab_interface.h"
+#include "content/public/test/browser_test.h"
+#include "ui/base/ozone_buildflags.h"
+#include "ui/base/test/ui_controls.h"
+#include "ui/events/event.h"
+#include "ui/events/keycodes/keyboard_codes.h"
+#include "ui/views/view.h"
+#include "url/gurl.h"
+
+namespace {
+
+base::RepeatingCallback<size_t()> GetBrowserCount() {
+  return base::BindRepeating([]() { return chrome::GetTotalBrowserCount(); });
+}
+
+base::RepeatingCallback<bool()> GetDragActive() {
+  return base::BindRepeating([]() { return TabDragController::IsActive(); });
+}
+
+}  // namespace
+
+class VerticalTabDragHandlerTest
+    : public VerticalTabsInteractiveTestMixin<InteractiveBrowserTest> {
+ public:
+  VerticalTabDragHandlerTest() = default;
+  ~VerticalTabDragHandlerTest() override = default;
+
+ protected:
+  auto DragTabTo(const std::string_view& tab_view_name,
+                 const gfx::Point& point) {
+    return Steps(
+        MoveMouseTo(tab_view_name),
+        ClickMouse(ui_controls::MouseButton::LEFT, /*release=*/false),
+        Do([&]() {
+          // TODO(crbug.com/40249472): Since DnD creates a blocking
+          // loop, the initiating mouse movement must be executed
+          // asynchronously.
+          EXPECT_TRUE(ui_controls::SendMouseMove(point.x(), point.y()));
+        }));
+  }
+
+  // TODO(crbug.com/40249472): Due to the nature of dragging, events for
+  // ending the drag must be executed asynchronoulsy.
+  auto ReleaseMouseAsync() {
+    return Do([&]() {
+      ui_controls::SendMouseEvents(ui_controls::MouseButton::LEFT,
+                                   ui_controls::MouseButtonState::UP);
+    });
+  }
+
+  auto PressEscAsync() {
+    return Do([&]() {
+      ui_controls::SendKeyPress(
+          GetLatestBrowser().GetWindow()->GetNativeWindow(), ui::VKEY_ESCAPE,
+          false, false, false, false);
+    });
+  }
+
+  BrowserView& GetBrowserView() {
+    BrowserView* browser_view =
+        BrowserView::GetBrowserViewForBrowser(browser());
+    EXPECT_TRUE(browser_view != nullptr);
+    return *browser_view;
+  }
+
+  BrowserWindowInterface& GetLatestBrowser() {
+    CHECK(!BrowserList::GetInstance()->empty());
+    BrowserWindowInterface* browser = *(--BrowserList::GetInstance()->end());
+    CHECK(browser);
+    return *browser;
+  }
+};
+
+DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kNewTab);
+DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kSecondTab);
+DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(ui::test::PollingStateObserver<size_t>,
+                                    kBrowserCountPoller);
+DEFINE_LOCAL_STATE_IDENTIFIER_VALUE(ui::test::PollingStateObserver<bool>,
+                                    kDragStatePoller);
+
+// TODO(crbug.com/40249472): Tab DnD tests not working on ChromeOS and Mac, and
+// flakes on Wayland
+#if !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_CHROMEOS) && \
+    !BUILDFLAG(IS_OZONE_WAYLAND)
+#define MAYBE_DragToDetachIntoNewWindow DragToDetachIntoNewWindow
+#else
+#define MAYBE_DragToDetachIntoNewWindow DISABLED_DragToDetachIntoNewWindow
+#endif
+IN_PROC_BROWSER_TEST_F(VerticalTabDragHandlerTest,
+                       MAYBE_DragToDetachIntoNewWindow) {
+  constexpr char kSecondTabName[] = "SecondTab";
+  RunTestSequence(
+      AddInstrumentedTab(kNewTab, GURL(chrome::kChromeUIBookmarksURL), 1),
+      AddInstrumentedTab(kSecondTab, GURL(chrome::kChromeUISettingsURL), 2),
+      NameDescendantViewByType<VerticalTabView>(kBrowserViewElementId,
+                                                kSecondTabName, 1),
+      DragTabTo(kSecondTabName,
+                GetBrowserView().GetBoundsInScreen().top_right() +
+                    gfx::Vector2d(50, 50)),
+      PollState(kBrowserCountPoller, GetBrowserCount()),
+      WaitForState(kBrowserCountPoller, 2), ReleaseMouseAsync(),
+      PollState(kDragStatePoller, GetDragActive()),
+      WaitForState(kDragStatePoller, false), Do([&]() {
+        TabStripModel* tab_strip_model = GetLatestBrowser().GetTabStripModel();
+        ASSERT_NE(nullptr, tab_strip_model);
+        EXPECT_EQ(1, tab_strip_model->count());
+        EXPECT_EQ(GURL(chrome::kChromeUIBookmarksURL),
+                  tab_strip_model->GetWebContentsAt(0)->GetURL());
+        EXPECT_EQ(2, browser()->GetTabStripModel()->count());
+      }));
+}
+
+// TODO(crbug.com/40249472): Tab DnD tests not working on ChromeOS and Mac, and
+// flakes on Wayland
+#if !BUILDFLAG(IS_MAC) && !BUILDFLAG(IS_CHROMEOS) && \
+    !BUILDFLAG(IS_OZONE_WAYLAND)
+#define MAYBE_DragToDetachThenCancel DragToDetachThenCancel
+#else
+#define MAYBE_DragToDetachThenCancel DISABLED_DragToDetachThenCancel
+#endif
+IN_PROC_BROWSER_TEST_F(VerticalTabDragHandlerTest,
+                       MAYBE_DragToDetachThenCancel) {
+  constexpr char kSecondTabName[] = "SecondTab";
+  RunTestSequence(
+      AddInstrumentedTab(kNewTab, GURL(chrome::kChromeUIBookmarksURL), 1),
+      AddInstrumentedTab(kSecondTab, GURL(chrome::kChromeUISettingsURL), 2),
+      NameDescendantViewByType<VerticalTabView>(kBrowserViewElementId,
+                                                kSecondTabName, 1),
+      DragTabTo(kSecondTabName,
+                GetBrowserView().GetBoundsInScreen().top_right() +
+                    gfx::Vector2d(50, 50)),
+      PollState(kBrowserCountPoller, GetBrowserCount()),
+      WaitForState(kBrowserCountPoller, 2), PressEscAsync(),
+      WaitForState(kBrowserCountPoller, 1),
+      PollState(kDragStatePoller, GetDragActive()),
+      WaitForState(kDragStatePoller, false), Do([&]() {
+        TabStripModel* tab_strip_model = browser()->GetTabStripModel();
+        ASSERT_NE(nullptr, tab_strip_model);
+        ASSERT_EQ(3, tab_strip_model->count());
+        EXPECT_EQ(GURL(chrome::kChromeUIBookmarksURL),
+                  tab_strip_model->GetWebContentsAt(1)->GetURL());
+      }));
+}
