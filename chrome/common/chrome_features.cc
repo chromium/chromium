6e36fe77f6d4a644c5c0e575cd35b4a448a23864
Maggie Jennings <mjenn@google.com>
2025-12-19 09:11:17 -0800

    [Task Indicator] Add feature flag for M145 icon/nudge work
    Bug: 470118212
    Change-Id: I09ee41f79e17500f05fa0a495017cd8088e81e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276328
    Reviewed-by: Christine Ying <chrstne@google.com>
    Auto-Submit: Maggie Jennings <mjenn@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561192}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -286,6 +286,9 @@ BASE_FEATURE(kGlicActorUi, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kGlicActorUiTaskIconV2, base::FEATURE_ENABLED_BY_DEFAULT);
 // Controls whether the task nudge UI fixes are enabled.
 BASE_FEATURE(kGlicActorUiTaskNudgeUiFix, base::FEATURE_ENABLED_BY_DEFAULT);
+// Controls whether the global task indicator and related features are enabled.
+BASE_FEATURE(kGlicActorUiGlobalTaskIndicator,
+             base::FEATURE_DISABLED_BY_DEFAULT);
 // Controls whether we ignore users preference of reduced motion enabled and
 // still show the tab indicator spinner. No-op if kGlicActorUiTabIndicator is
 // disabled.
