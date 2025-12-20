c1fd8a70620fa496c368112cf3f80c519cc8a00b
Justin Donnelly <jdonnelly@chromium.org>
2025-12-20 10:59:47 -0800

    [144] [Omnibox Next] Remove param from all variations by default
    Disable show recent tabs as fulfillment does not work yet.
    
    (cherry picked from commit 8cb9655e04e249400da0efe77ce65d0245b60171)
    
    Bug: 468073635, 470455439
    Change-Id: I48732f4ac902153faa82fbf388ca1f9ca651803e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7254128
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Reviewed-by: Aviv Kiss <avivkiss@google.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1558370}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7287782
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#1768}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -871,7 +871,6 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonBelowResults[] = {
         {"AddContextButtonVariant", "below_results"},
         {"ShowCreateImageTool", "true"},
-        {"ShowRecentTabChip", "true"},
         {"ShowToolsAndModels", "true"},
         {"ShowVoiceSearchInSteadyComposebox","true"},
         {"ShowVoiceSearchInExpandedComposebox","true"},
@@ -880,7 +879,6 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonAboveResults[] = {
         {"AddContextButtonVariant", "above_results"},
         {"ShowCreateImageTool", "true"},
-        {"ShowRecentTabChip", "true"},
         {"ShowToolsAndModels", "true"},
         {"ShowVoiceSearchInSteadyComposebox","true"},
         {"ShowVoiceSearchInExpandedComposebox","true"},
@@ -889,7 +887,6 @@ const FeatureEntry::FeatureParam kWebUIOmniboxAimPopupAddContextButtonInline[] =
     {
         {"AddContextButtonVariant", "inline"},
         {"ShowCreateImageTool", "true"},
-        {"ShowRecentTabChip", "true"},
         {"ShowToolsAndModels", "true"},
         {"ShowVoiceSearchInSteadyComposebox","true"},
         {"ShowVoiceSearchInExpandedComposebox","true"},
@@ -898,9 +895,17 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonMultiFile[] = {
         {"AddContextButtonVariant", "below_results"},
         {"ShowCreateImageTool", "true"},
-        {"ShowRecentTabChip", "true"},
         {"ShowToolsAndModels", "true"},
         {"MaxNumFiles", "5"},
+};
+const FeatureEntry::FeatureParam
+    kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip[] = {
+        {"AddContextButtonVariant", "below_results"},
+        {"ShowCreateImageTool", "true"},
+        {"ShowToolsAndModels", "true"},
+        {"ShowVoiceSearchInSteadyComposebox", "true"},
+        {"ShowVoiceSearchInExpandedComposebox", "true"},
+        {"ShowRecentTabChip", "true"},
 };
 
 const FeatureEntry::FeatureVariation kWebUIOmniboxAimPopupVariations[] = {
@@ -917,7 +922,11 @@ const FeatureEntry::FeatureVariation kWebUIOmniboxAimPopupVariations[] = {
      std::size(kWebUIOmniboxAimPopupAddContextButtonInline), nullptr},
     {"- \"Add Context\" Button below results, 5 File Limit",
      kWebUIOmniboxAimPopupAddContextButtonMultiFile,
-     std::size(kWebUIOmniboxAimPopupAddContextButtonMultiFile), nullptr}};
+     std::size(kWebUIOmniboxAimPopupAddContextButtonMultiFile), nullptr},
+    {"- \"Add Context\" Button below results, Recent Tab Chip",
+     kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip,
+     std::size(kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip),
+     nullptr}};
 
 const FeatureEntry::FeatureParam kWebUIOmniboxPopupDebugSxS[] = {
     {"SxS", "true"}};
