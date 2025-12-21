d7f13463ce8df41f6242ec8c609159b2a0649659
Justin Donnelly <jdonnelly@chromium.org>
2025-12-21 10:44:37 -0800

    [144] [contextual_tasks] Create default URLDeduplicationHelper
    This CL provides a default URL deduplication helper for contextual
    tasks.
    
    (cherry picked from commit 690d2d4ff15a1787cb56ca3c10e93c326f457307)
    
    Bug: 470455439
    Change-Id: I6c200ec38ead6aa55419f0ce8cd6c284d696a176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7256612
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1558988}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7289983
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#1839}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- components/contextual_tasks/public/contextual_task_context_unittest.cc
+++ components/contextual_tasks/public/contextual_task_context_unittest.cc
@@ -6,6 +6,7 @@
 
 #include "base/uuid.h"
 #include "components/contextual_tasks/public/contextual_task.h"
+#include "components/contextual_tasks/public/utils.h"
 #include "components/url_deduplication/url_deduplication_helper.h"
 #include "components/visited_url_ranking/public/url_visit_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -54,21 +55,48 @@ TEST(ContextualTaskContextTest, ContainsURL) {
   base::Uuid task_id = base::Uuid::GenerateRandomV4();
   ContextualTask task(task_id);
   GURL url1("https://google.com");
-  GURL url2("https://youtube.com");
+  GURL url2("https://www.youtube.com/watch?v=123");
   task.AddUrlResource(UrlResource(base::Uuid::GenerateRandomV4(), url1));
   task.AddUrlResource(UrlResource(base::Uuid::GenerateRandomV4(), url2));
 
   ContextualTaskContext context(task);
-  auto deduplication_helper =
-      visited_url_ranking::CreateDefaultURLDeduplicationHelper();
+  auto deduplication_helper = CreateURLDeduplicationHelperForContextualTask();
 
+  // Exact match.
+  EXPECT_TRUE(context.ContainsURL(url1, deduplication_helper.get()));
+  EXPECT_TRUE(context.ContainsURL(url2, deduplication_helper.get()));
+
+  // Variations in scheme.
   EXPECT_TRUE(context.ContainsURL(GURL("https://google.com/"),
                                   deduplication_helper.get()));
   EXPECT_TRUE(context.ContainsURL(GURL("https://www.google.com"),
                                   deduplication_helper.get()));
-  EXPECT_TRUE(context.ContainsURL(GURL("http://google.com"),
+
+  // Variations in port.
+  EXPECT_TRUE(context.ContainsURL(GURL("http://google.com:8080"),
                                   deduplication_helper.get()));
+
+  // Variations in username/password.
+  EXPECT_TRUE(context.ContainsURL(GURL("https://user:pass@google.com:443"),
+                                  deduplication_helper.get()));
+  // Variations in ref.
+  EXPECT_TRUE(
+      context.ContainsURL(GURL("https://www.youtube.com/watch?v=123#t=30s"),
+                          deduplication_helper.get()));
+
+  // Non-matching URLs.
   EXPECT_FALSE(context.ContainsURL(GURL("https://example.com"),
+                                   deduplication_helper.get()));
+  // Different path.
+  EXPECT_FALSE(context.ContainsURL(GURL("https://google.com/maps"),
+                                   deduplication_helper.get()));
+  EXPECT_FALSE(context.ContainsURL(GURL("https://www.youtube.com"),
+                                   deduplication_helper.get()));
+
+  // Different query param.
+  EXPECT_FALSE(context.ContainsURL(GURL("https://www.youtube.com/watch?v=234"),
+                                   deduplication_helper.get()));
+  EXPECT_FALSE(context.ContainsURL(GURL("https://www.youtube.com/watch"),
                                    deduplication_helper.get()));
 }
 
