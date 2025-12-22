686f2d4a2ac2f84f1f86e7193855cfb8b568c321
Boris Sazonov <bsazonov@chromium.org>
2025-12-22 08:15:45 -0800

    [Signin][iOS] Reimplement and reenable testErrorBadge
    The test is reenabled and reimplemented:
    * AccountMenu is used instead of Settings
    * Actual auth error is triggered and resolved instead of passphrase
      error
    
    Bug: 394268777, 417649965
    Change-Id: I72e3e0fb8836db62d9c2a988c5ac0b8aaf76fa2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7276999
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561808}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_egtest.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_egtest.mm
@@ -9,6 +9,7 @@
 #import "components/policy/core/common/policy_test_utils.h"
 #import "components/policy/policy_constants.h"
 #import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/authentication/account_menu/public/account_menu_constants.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey_ui_test_util.h"
 #import "ios/chrome/browser/content_suggestions/ui_bundled/ntp_home_constant.h"
@@ -21,6 +22,7 @@
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
+#import "ios/chrome/browser/signin/model/test_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
@@ -491,10 +493,12 @@ void VerifyMIAButtonVisible(bool mia_button_visible) {
 
 // Tests that the error badge is shown on top of the identity disc when the
 // primary account has a persistent error.
-// TODO(crbug.com/394268777): Reenable test.
-- (void)DISABLED_testErrorBadge {
+- (void)testErrorBadge {
   [SigninEarlGrey signinWithFakeIdentity:kPrimaryIdentity];
-  [ChromeEarlGrey addBookmarkWithSyncPassphrase:kPassphrase];
+  [SigninEarlGrey
+      setPersistentAuthErrorForAccount:CoreAccountId::FromGaiaId(
+                                           kPrimaryIdentity.gaiaId)];
+  //[ChromeEarlGrey addBookmarkWithSyncPassphrase:kPassphrase];
   [ChromeEarlGreyUI waitForAppToIdle];
 
   // Wait for the error badge to appear.
@@ -511,30 +515,30 @@ void VerifyMIAButtonVisible(bool mia_button_visible) {
                  base::test::ios::kWaitForUIElementTimeout, condition),
              @"Error badge didn't appear in the allotted time");
 
-  // Tap on the identity disc to open Settings.
+  // Tap on the identity disc to open the account menu.
   [[EarlGrey
       selectElementWithMatcher:grey_accessibilityID(kNTPFeedHeaderIdentityDisc)]
       performAction:grey_tap()];
-
-  // Open account settings.
-  [ChromeEarlGreyUI
-      tapSettingsMenuButton:chrome_test_util::SettingsAccountButton()];
-
-  // Verify the error section is showing.
+  // Ensure the Account Menu is displayed.
   [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(kSyncErrorButtonIdentifier)]
+      selectElementWithMatcher:grey_accessibilityID(kAccountMenuTableViewId)]
       assertWithMatcher:grey_sufficientlyVisible()];
 
-  // Tap "Enter Passphrase" button.
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(kSyncErrorButtonIdentifier)]
+  // Check the error button is displayed.
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
+                                          kAccountMenuErrorActionButtonId)]
+      assertWithMatcher:grey_sufficientlyVisible()];
+  // Tap on it.
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
+                                          kAccountMenuErrorActionButtonId)]
       performAction:grey_tap()];
 
-  // Enter the passphrase.
-  [SigninEarlGreyUI submitSyncPassphrase:kPassphrase];
-
-  // Dismiss settings.
-  [[EarlGrey selectElementWithMatcher:chrome_test_util::SettingsDoneButton()]
+  // Confirm the fake reauthentication dialog.
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(
+                                   grey_accessibilityID(
+                                       kFakeAuthAddAccountButtonIdentifier),
+                                   grey_sufficientlyVisible(), nil)]
       performAction:grey_tap()];
 
   // Verify the error badge on the ADP disappears.
