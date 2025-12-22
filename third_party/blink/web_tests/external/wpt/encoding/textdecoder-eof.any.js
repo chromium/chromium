2ff10f8c11cffcbcc3c57bd93a66b33ced8eddad
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2025-12-22 10:32:17 -0800

    Import wpt@8ccef0c765a54aea5e2a1cde0d169a85d7e282c5
    https://github.com/web-platform-tests/wpt/compare/0a62ad28d...8ccef0c76
    
    Using wpt-import in Chromium be4524fee1f37564864489fc4c5ae98273fa30d0.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    daniec@microsoft.com, siliu@microsoft.com, snianu@microsoft.com:
      external/wpt/editing
    dtapuska@chromium.org:
      external/wpt/uievents
    emilio@chromium.org:
      external/wpt/css/css-display
    igrigorik@chromium.org, yoavweiss@chromium.org:
      external/wpt/preload
    japhet@chromium.org:
      external/wpt/fetch
    kojii@chromium.org:
      external/wpt/css/css-scoping
    mkwst@chromium.org:
      external/wpt/secure-contexts
    mustaq@chromium.org:
      external/wpt/html/interaction/focus
    ricea@chromium.org, yyanagisawa@chromium.org:
      external/wpt/encoding
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win10.20h2-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel
    Change-Id: I7b8bb990422209a21c7ec62473d094b48701355a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7296269
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1561861}

--- third_party/blink/web_tests/external/wpt/encoding/textdecoder-eof.any.js
+++ third_party/blink/web_tests/external/wpt/encoding/textdecoder-eof.any.js
@@ -9,10 +9,15 @@ test(() => {
   assert_equals(new TextDecoder().decode(new Uint8Array([0xF0, 0x41, 0x42])), "\uFFFDAB");
   assert_equals(new TextDecoder().decode(new Uint8Array([0xF0, 0x41, 0xF0])), "\uFFFDA\uFFFD");
   assert_equals(new TextDecoder().decode(new Uint8Array([0xF0, 0x8F, 0x92])), "\uFFFD\uFFFD\uFFFD");
+  assert_equals(new TextDecoder("Big5").decode(new Uint8Array([0x81, 0x40])), "\uFFFD@");
+  assert_equals(new TextDecoder("Big5").decode(new Uint8Array([0x81, 0x81])), "\uFFFD");
+  assert_equals(new TextDecoder("Big5").decode(new Uint8Array([0x87, 0x87, 0x40])), "\uFFFD@");
 }, "TextDecoder end-of-queue handling");
 
 test(() => {
   const decoder = new TextDecoder();
+  const big5Decoder = new TextDecoder("Big5");
+
   assert_equals(decoder.decode(new Uint8Array([0xF0]), { stream: true }), "");
   assert_equals(decoder.decode(), "\uFFFD");
 
@@ -55,4 +60,11 @@ test(() => {
   assert_equals(decoder.decode(new Uint8Array([0xF0]), { stream: true }), "");
   assert_equals(decoder.decode(new Uint8Array([0xC2, 0x80, 0x2A]), { stream: true }), "\uFFFD\x80*");
   assert_equals(decoder.decode(), "");
+
+  assert_equals(big5Decoder.decode(new Uint8Array([0x81, 0x40]), { stream: true }), "\uFFFD@");
+  assert_equals(big5Decoder.decode(), "");
+
+  assert_equals(big5Decoder.decode(new Uint8Array([0x81]), { stream: true }), "");
+  assert_equals(big5Decoder.decode(new Uint8Array([0x40]), { stream: true }), "\uFFFD@");
+  assert_equals(big5Decoder.decode(), "");
 }, "TextDecoder end-of-queue handling using stream: true");
