de357fdfff45b290983dafab71b1c57dcdcb59d7
Andrew Williams <awillia@chromium.org>
2025-12-22 09:30:07 -0800

    Improve PrivacyMode documentation
    Improves the comments for PrivacyMode based on the discussion in
    crbug.com/40662990.
    
    Bug: None
    Change-Id: Ie4f9723a232a9589e45cb3fcda9971dfb9095dd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7281409
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1561831}

--- net/base/privacy_mode.h
+++ net/base/privacy_mode.h
@@ -7,17 +7,25 @@
 
 namespace net {
 
-// Privacy Mode is enabled if cookies to particular site are blocked, so
-// Channel ID is disabled on that connection (https, spdy or quic).
+// `PrivacyMode` corresponds to the fetch spec "credentials mode" and determines
+// whether cookies, client certificates, and server HTTP auth credentials should
+// be sent (for requests) or are enabled (when used by the various partitioning
+// keys).
 enum PrivacyMode {
+  // Cookies and other credentials are enabled. Corresponds to the fetch spec
+  // credentials mode of "include".
   PRIVACY_MODE_DISABLED = 0,
+
+  // Cookies and server HTTP auth credentials are not enabled, but due to
+  // http://crbug.com/775438, client certs are still enabled.
   PRIVACY_MODE_ENABLED = 1,
 
-  // Due to http://crbug.com/775438, PRIVACY_MODE_ENABLED still sends client
-  // certs. This mode ensures that the request is sent without client certs.
+  // Same as PRIVACY_MODE_ENABLED but with client certs disabled. Corresponds to
+  // the fetch spec credentials mode "omit".
   PRIVACY_MODE_ENABLED_WITHOUT_CLIENT_CERTS = 2,
 
-  // Privacy mode is enabled but partitioned HTTP cookies are still allowed.
+  // Same as PRIVACY_MODE_ENABLED but partitioned HTTP cookies are still
+  // enabled.
   PRIVACY_MODE_ENABLED_PARTITIONED_STATE_ALLOWED = 3,
 };
 
