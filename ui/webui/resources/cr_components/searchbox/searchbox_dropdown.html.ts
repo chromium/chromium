1a714b161d3691c802bbd66637559bb34950e5f4
Justin Donnelly <jdonnelly@chromium.org>
2025-12-22 09:32:06 -0800

    [144] [ntp][realbox] Suggestion group screen reader announcements
    Adds relevant aria labels to ensure the suggestion group name is
    included as part of the screen reader announcement for realbox
    navigation suggestions. Ensures behavior parity with the omnibox
    screen reader announcements for navigation suggestions belonging to a
    suggestion group.
    
    Before: screenshot/36QKUdzGVPqMJoV
    After: screenshot/4yYhwDxMXxpLT5F
    
    Follow up work will explore announcements that avoid repetition of
    the suggestion group title for each navigation suggestion under a common
    group to reduce cognitive load.
    
    (cherry picked from commit bd569d52bec21e9954331f0921d591a340bc156d)
    
    Bug: b:461861839, 470807341
    Change-Id: Ia0b36b1536b94665c6b8ac173ebb6f1ad1a2c700
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265415
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Auto-Submit: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Marlon Facey <mfacey@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1559476}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7289969
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#1925}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- ui/webui/resources/cr_components/searchbox/searchbox_dropdown.html.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox_dropdown.html.ts
@@ -2,38 +2,50 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {html} from '//resources/lit/v3_0/lit.rollup.js';
+import {html, nothing} from '//resources/lit/v3_0/lit.rollup.js';
+import type {SideType} from '//resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
 
 import type {SearchboxDropdownElement} from './searchbox_dropdown.js';
+
+export function getMatchesForGroupHtml(
+    this: SearchboxDropdownElement, sideType: SideType, groupId: number) {
+  const hasHeader = this.hasHeaderForGroup_(groupId);
+  // clang-format off
+  const matchesHtml = html`
+    <div class="matches ${this.renderTypeClassForGroup_(groupId)}">
+    ${this.matchesForGroup_(groupId).map(match => html`
+      <cr-searchbox-match tabindex="0" role="option"
+          aria-describedby="${hasHeader ? `hg_${groupId}` : nothing}"
+          .match="${match}" match-index="${this.matchIndex_(match)}"
+          side-type="${sideType}"
+          ?selected="${this.isSelected_(match)}"
+          ?show-thumbnail="${this.showThumbnail}">
+      </cr-searchbox-match>
+    `)}
+    </div>
+  `;
+  return hasHeader ? html`
+    <!-- Header cannot be tabbed into but gets focus when clicked. This
+        stops the dropdown from losing focus and closing as a result. -->
+    <h3 class="header" data-id="${groupId}" tabindex="-1"
+        id="hg_${groupId}"
+        @mousedown="${this.onHeaderMousedown_}">
+        <span class="text">${this.headerForGroup_(groupId)}</span>
+    </h3>
+    ${matchesHtml}` : matchesHtml;
+  // clang-format on
+}
 
 export function getHtml(this: SearchboxDropdownElement) {
   // clang-format off
   return html`<!--_html_template_start_-->
 <div id="content" part="dropdown-content">
-  ${this.sideTypes_().map(sideType => html`
-    <div class="${this.sideTypeClass_(sideType)}">
-      ${this.groupIdsForSideType_(sideType).map(groupId => html`
-        ${this.hasHeaderForGroup_(groupId) ? html`
-          <!-- Header cannot be tabbed into but gets focus when clicked. This
-              stops the dropdown from losing focus and closing as a result. -->
-          <h3 class="header" data-id="${groupId}" tabindex="-1"
-              @mousedown="${this.onHeaderMousedown_}" aria-hidden="true">
-            <span class="text">${this.headerForGroup_(groupId)}</span>
-          </h3>
-        ` : ''}
-        <div class="matches ${this.renderTypeClassForGroup_(groupId)}">
-          ${this.matchesForGroup_(groupId).map(match => html`
-            <cr-searchbox-match tabindex="0" role="option"
-                .match="${match}" match-index="${this.matchIndex_(match)}"
-                side-type="${sideType}"
-                ?selected="${this.isSelected_(match)}"
-                ?show-thumbnail="${this.showThumbnail}">
-            </cr-searchbox-match>
-          `)}
-        </div>
-      `)}
-    </div>
-  `)}
+${this.sideTypes_().map(sideType => html`
+  <div class="${this.sideTypeClass_(sideType)}">
+    ${this.groupIdsForSideType_(sideType)
+        .map(groupId => getMatchesForGroupHtml.bind(this)(sideType, groupId))}
+  </div>
+`)}
 </div>
 <!--_html_template_end_-->`;
   // clang-format on
