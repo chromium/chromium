dcd2525093818bd4cabc0cc2c5959bc9758c4dd0
Fiaz Muhammad <mfiaz@google.com>
2025-12-23 08:21:54 -0800

    [Url Override] Deprecate use of static url constant variables
    Bug: 455616895
    Change-Id: I6b88f623f73fd82ebd420a1815fc95f9049012fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7303008
    Owners-Override: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1562198}

--- components/embedder_support/android/java/src/org/chromium/components/embedder_support/util/UrlConstants.java
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/util/UrlConstants.java
@@ -10,7 +10,7 @@ import org.chromium.build.annotations.NullMarked;
  * Java side version of chrome/common/url_constants.cc
  *
  * <p>Do not add any more NTP related constants. TODO(crbug.com/40281619) Move NTP related constants
- * to ChromeUrlConstants.java
+ * to UrlConstantsResolver.java
  */
 @NullMarked
 public class UrlConstants {
@@ -54,16 +54,12 @@ public class UrlConstants {
     public static final String ABOUT_URL = "chrome://about/";
 
     public static final String NTP_HOST = "newtab";
-    public static final String NTP_URL = "chrome-native://newtab/";
-    public static final String NTP_NON_NATIVE_URL = "chrome://newtab/";
     public static final String NTP_ABOUT_URL = "about:newtab";
 
     // Don't use this URL. The constant is added for legacy reasons.
     public static final String NEW_TAB_PAGE_URL_LEGACY = "chrome://new-tab-page/";
 
     public static final String BOOKMARKS_HOST = "bookmarks";
-    public static final String BOOKMARKS_URL = "chrome://bookmarks/";
-    public static final String BOOKMARKS_NATIVE_URL = "chrome-native://bookmarks/";
     public static final String BOOKMARKS_FOLDER_URL = "chrome-native://bookmarks/folder/";
     public static final String BOOKMARKS_UNCATEGORIZED_URL =
             "chrome-native://bookmarks/uncategorized/";
@@ -76,10 +72,7 @@ public class UrlConstants {
     public static final String RECENT_TABS_URL = "chrome-native://recent-tabs/";
     public static final String GRID_TAB_SWITCHER_URL = "chrome-native://gts/";
 
-    // TODO(dbeam): do we need both HISTORY_URL and NATIVE_HISTORY_URL?
     public static final String HISTORY_HOST = "history";
-    public static final String HISTORY_URL = "chrome://history/";
-    public static final String NATIVE_HISTORY_URL = "chrome-native://history/";
 
     public static final String LAUNCHPAD_HOST = "apps";
     public static final String LAUNCHPAD_URL = "chrome://apps/";
@@ -147,4 +140,17 @@ public class UrlConstants {
     public static final String PDF_URL = "chrome-native://pdf/";
     public static final String PDF_URL_PARAM = "link?url=";
     public static final String PDF_URL_QUERY_PARAM = "url";
+
+    /*
+     * Clients should not use these constants directly, and instead should use UrlConstantResolver
+     * to ensure URL overrides from extensions are respected.
+     */
+    @Deprecated public static final String NTP_URL = "chrome-native://newtab/";
+    @Deprecated public static final String NTP_NON_NATIVE_URL = "chrome://newtab/";
+    @Deprecated public static final String BOOKMARKS_URL = "chrome://bookmarks/";
+    @Deprecated public static final String BOOKMARKS_NATIVE_URL = "chrome-native://bookmarks/";
+
+    // TODO(dbeam): do we need both HISTORY_URL and NATIVE_HISTORY_URL?
+    @Deprecated public static final String HISTORY_URL = "chrome://history/";
+    @Deprecated public static final String NATIVE_HISTORY_URL = "chrome-native://history/";
 }
