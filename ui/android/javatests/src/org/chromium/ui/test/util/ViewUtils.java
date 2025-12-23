256f32449b2573f954d94cb561323855702f4517
Henrique Nakashima <hnakashima@chromium.org>
2025-12-23 15:40:06 -0800

    [Android] Convert waitForDialogViewCheckingState() to waitForView()
    ViewFinder.waitForView() checks visibility already, and can restrict the
    search to dialogs.
    
    Bug: 471254641
    Change-Id: I746ab588727d7be910341ef009ff4621f82d1ef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7310848
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562368}

--- ui/android/javatests/src/org/chromium/ui/test/util/ViewUtils.java
+++ ui/android/javatests/src/org/chromium/ui/test/util/ViewUtils.java
@@ -5,7 +5,6 @@
 package org.chromium.ui.test.util;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
 
 import static org.hamcrest.Matchers.greaterThan;
@@ -148,30 +147,13 @@ public class ViewUtils {
     }
 
     /**
-     * Waits until a view in a dialog root view that matches the given matcher and any of the given
-     * {@link ExpectedViewState}s. waitForViewCheckingState is flaky with dialogs after api 29.
+     * Waits until a view matches the given matcher and any of the given {@link ExpectedViewState}s.
      * Fails if the matcher applies to multiple views. Times out after {@link
      * CriteriaHelper#DEFAULT_MAX_TIME_TO_POLL} milliseconds.
      *
      * @param viewMatcher The matcher matching the view that should be waited for.
      * @param viewState State that the matching view should be in. If multiple states are passed,
      *     the waiting will stop if at least one applies.
-     */
-    public static void waitForDialogViewCheckingState(
-            Matcher<View> viewMatcher, @ExpectedViewState int viewState) {
-        onView(isRoot())
-                .inRoot(isDialog())
-                .check(withEventualExpectedViewState(viewMatcher, viewState));
-    }
-
-    /**
-     * Waits until a view matches the given matcher and any of the given {@link ExpectedViewState}s.
-     * Fails if the matcher applies to multiple views. Times out after
-     * {@link CriteriaHelper#DEFAULT_MAX_TIME_TO_POLL} milliseconds.
-     *
-     * @param viewMatcher The matcher matching the view that should be waited for.
-     * @param viewState State that the matching view should be in. If multiple states are passed,
-     *                  the waiting will stop if at least one applies.
      */
     public static void waitForViewCheckingState(
             Matcher<View> viewMatcher, @ExpectedViewState int viewState) {
