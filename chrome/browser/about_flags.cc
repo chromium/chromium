1e10e729cc98036dea9b3275a457807376e896cb
Justin Donnelly <jdonnelly@chromium.org>
2025-12-23 15:55:50 -0800

    [144] [Omnibox Next] Enable recent tab chips on all variants
    (cherry picked from commit 4a5264e805260917fcfb4a68795542875da85497)
    
    Bug: 450006744, 471210631
    Change-Id: Iac9103dcb5cf7e43ecedd5b2dbac7026305b8491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7272244
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Auto-Submit: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1560325}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7311138
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#2089}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -869,6 +869,7 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonBelowResults[] = {
         {"AddContextButtonVariant", "below_results"},
         {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
@@ -876,6 +877,7 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonAboveResults[] = {
         {"AddContextButtonVariant", "above_results"},
         {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
@@ -883,6 +885,7 @@ const FeatureEntry::FeatureParam kWebUIOmniboxAimPopupAddContextButtonInline[] =
     {
         {"AddContextButtonVariant", "inline"},
         {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
@@ -890,23 +893,17 @@ const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonMultiFile[] = {
         {"AddContextButtonVariant", "below_results"},
         {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
         {"MaxNumFiles", "5"},
 };
 const FeatureEntry::FeatureParam
-    kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip[] = {
-        {"AddContextButtonVariant", "below_results"},
-        {"ShowVoiceSearchInSteadyComposebox", "true"},
-        {"ShowVoiceSearchInExpandedComposebox", "true"},
-        {"ShowRecentTabChip", "true"},
-};
-const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonDragAndDrop[] = {
         {"AddContextButtonVariant", "below_results"},
-        {"ShowCreateImageTool", "true"},
-        {"ShowToolsAndModels", "true"},
         {"EnableContextDragAndDrop", "true"},
+        {"ShowLensSearchChip", "true"},
+        {"ShowRecentTabChip", "true"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
 };
@@ -926,10 +923,6 @@ const FeatureEntry::FeatureVariation kWebUIOmniboxAimPopupVariations[] = {
     {"- \"Add Context\" Button below results, 5 File Limit",
      kWebUIOmniboxAimPopupAddContextButtonMultiFile,
      std::size(kWebUIOmniboxAimPopupAddContextButtonMultiFile), nullptr},
-    {"- \"Add Context\" Button below results, Recent Tab Chip",
-     kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip,
-     std::size(kWebUIOmniboxAimPopupAddContextButtonBelowResultsRecentTabChip),
-     nullptr},
     {"- \"Add Context\" Button enable drag and drop",
      kWebUIOmniboxAimPopupAddContextButtonDragAndDrop,
      std::size(kWebUIOmniboxAimPopupAddContextButtonDragAndDrop), nullptr}};
