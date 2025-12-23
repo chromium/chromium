1eac4a722f1b545eda5d57a36059280e48da8258
Justin Donnelly <jdonnelly@chromium.org>
2025-12-23 13:07:41 -0800

    [144] [ntp-next] Update action chip eligibility
    (cherry picked from commit 051d0ac4984e5485b95abec5dec17f4dcc866a31)
    
    Bug: 465832704, 471210631
    Change-Id: I5e69d7e31fb018c63979e4a0c5e03047ea7fda42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7268700
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1560225}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7311747
    Owners-Override: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#2069}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_chrome_ui.cc
@@ -36,6 +36,7 @@
 #include "chrome/grit/side_panel_customize_chrome_resources_map.h"
 #include "chrome/grit/side_panel_shared_resources.h"
 #include "chrome/grit/side_panel_shared_resources_map.h"
+#include "components/contextual_search/contextual_search_service.h"
 #include "components/ntp_tiles/features.h"
 #include "components/omnibox/browser/aim_eligibility_service.h"
 #include "components/search/ntp_features.h"
@@ -303,10 +304,19 @@ CustomizeChromeUI::CustomizeChromeUI(content::WebUI* web_ui)
 
   const auto* aim_eligibility_service =
       AimEligibilityServiceFactory::GetForProfile(profile_);
-  source->AddBoolean("aimPolicyEnabled",
-                     aim_eligibility_service &&
-                         aim_eligibility_service->IsDeepSearchEligible() &&
-                         aim_eligibility_service->IsCreateImagesEligible());
+  bool action_chips_eligible =
+      ntp_features::kNtpNextShowSimplificationUIParam.Get()
+          ? aim_eligibility_service &&
+                (aim_eligibility_service->IsDeepSearchEligible() ||
+                 aim_eligibility_service->IsCreateImagesEligible())
+          : aim_eligibility_service &&
+                aim_eligibility_service->IsDeepSearchEligible() &&
+                aim_eligibility_service->IsCreateImagesEligible();
+  bool show_tools =
+      action_chips_eligible &&
+      contextual_search::ContextualSearchService::IsContextSharingEnabled(
+          profile_->GetPrefs());
+  source->AddBoolean("aimPolicyEnabled", show_tools);
 
   source->AddBoolean("footerEnabled",
                      base::FeatureList::IsEnabled(ntp_features::kNtpFooter));
