c11496ff2d0e2b4de110d2df04afbafa3a35fbe2
Suyeon Ji <zeesuyeon@gmail.com>
2025-12-24 01:28:34 -0800

    Refactor GlobalFetch to remove templates and use ExecutionContext
    This CL simplifies GlobalFetch by removing the GlobalFetchImpl template.
    ScopedFetcher now directly inherits from Supplement<ExecutionContext>,
    unifying the logic for different global scopes (window, worker) into a
    single concrete class.
    
    This reduces code duplication and template overhead without any behavior
    changes.
    
    Bug: 449023252
    Change-Id: I5245810887c7d95fd8045e111d90154859071937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7262498
    Reviewed-by: Jinho Bang <zino@chromium.org>
    Commit-Queue: Jinho Bang <zino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562451}

--- third_party/blink/renderer/core/fetch/global_fetch.cc
+++ third_party/blink/renderer/core/fetch/global_fetch.cc
@@ -8,7 +8,6 @@
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_deferred_request_init.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_request_init.h"
-#include "third_party/blink/renderer/core/execution_context/navigator_base.h"
 #include "third_party/blink/renderer/core/fetch/fetch_later_result.h"
 #include "third_party/blink/renderer/core/fetch/fetch_manager.h"
 #include "third_party/blink/renderer/core/fetch/request.h"
@@ -37,190 +36,138 @@ void MeasureFetchProperties(ExecutionContext* execution_context,
     UseCounter::Count(execution_context, WebFeature::kFetchCacheReload);
 }
 
-template <typename T>
-class GlobalFetchImpl final : public GarbageCollected<GlobalFetchImpl<T>>,
-                              public GlobalFetch::ScopedFetcher,
-                              public Supplement<T> {
- public:
-  static const char kSupplementName[];
-
-  static ScopedFetcher* From(T& supplementable,
-                             ExecutionContext* execution_context) {
-    GlobalFetchImpl* supplement =
-        Supplement<T>::template From<GlobalFetchImpl>(supplementable);
-    if (!supplement) {
-      supplement = MakeGarbageCollected<GlobalFetchImpl>(supplementable,
-                                                         execution_context);
-      Supplement<T>::ProvideTo(supplementable, supplement);
-    }
-    return supplement;
-  }
-
-  explicit GlobalFetchImpl(T& supplementable,
-                           ExecutionContext* execution_context)
-      : Supplement<T>(supplementable),
-        fetch_manager_(MakeGarbageCollected<FetchManager>(execution_context)),
-        // TODO(crbug.com/1356128): FetchLater is only supported in Document.
-        fetch_later_manager_(
-            base::FeatureList::IsEnabled(blink::features::kFetchLaterAPI) &&
-                    execution_context->IsWindow()
-                ? MakeGarbageCollected<FetchLaterManager>(execution_context)
-                : nullptr) {}
-
-  ScriptPromise<Response> Fetch(ScriptState* script_state,
-                                const V8RequestInfo* input,
-                                const RequestInit* init,
-                                ExceptionState& exception_state) override {
-    fetch_count_ += 1;
-
-    ExecutionContext* execution_context = fetch_manager_->GetExecutionContext();
-    if (!script_state->ContextIsValid() || !execution_context) {
-      // TODO(yhirano): Should this be moved to bindings?
-      exception_state.ThrowTypeError("The global scope is shutting down.");
-      return EmptyPromise();
-    }
-
-    // "Let |r| be the associated request of the result of invoking the
-    // initial value of Request as constructor with |input| and |init| as
-    // arguments. If this throws an exception, reject |p| with it."
-    Request* r = Request::Create(script_state, input, init, exception_state);
-    if (exception_state.HadException())
-      return EmptyPromise();
-
-    probe::WillSendXMLHttpOrFetchNetworkRequest(execution_context, r->url());
-    FetchRequestData* request_data =
-        r->PassRequestData(script_state, exception_state);
-    MeasureFetchProperties(execution_context, request_data);
-
-    // Even if this was checked at the beginning of the function, it might
-    // have been set to nullptr during Request::Create.
-    if (!fetch_manager_->GetExecutionContext()) {
-      exception_state.ThrowTypeError("The global scope is shutting down.");
-      return EmptyPromise();
-    }
-
-    auto promise = fetch_manager_->Fetch(script_state, request_data,
-                                         r->signal(), exception_state);
-    if (exception_state.HadException())
-      return EmptyPromise();
-
-    return promise;
-  }
-
-  FetchLaterResult* FetchLater(ScriptState* script_state,
-                               const V8RequestInfo* input,
-                               const DeferredRequestInit* init,
-                               ExceptionState& exception_state) override {
-    if (!base::FeatureList::IsEnabled(blink::features::kFetchLaterAPI) ||
-        !fetch_later_manager_) {
-      exception_state.ThrowTypeError(
-          "FetchLater is not supported in this scope.");
-      return nullptr;
-    }
-    ExecutionContext* ec = fetch_later_manager_->GetExecutionContext();
-    if (!script_state->ContextIsValid() || !ec) {
-      exception_state.ThrowTypeError("The global scope is shutting down.");
-      return nullptr;
-    }
-
-    // https://whatpr.org/fetch/1647.html#dom-global-fetch-later
-    // Run the fetchLater(input, init) method steps:
-
-    // 1. Let `r` be the result of invoking the initial value of Request as
-    // constructor with `input` and `init` as arguments. This may throw an
-    // exception.
-    Request* r =
-        Request::Create(script_state, input,
-                        static_cast<const RequestInit*>(init), exception_state);
-    if (exception_state.HadException()) {
-      return nullptr;
-    }
-
-    probe::WillSendXMLHttpOrFetchNetworkRequest(ec, r->url());
-    FetchRequestData* request_data =
-        r->PassRequestData(script_state, exception_state);
-    MeasureFetchProperties(ec, request_data);
-    // 5. If init is given and init ["activateAfter"] exists, then set
-    // `activate_after` to init ["activateAfter"].
-    std::optional<DOMHighResTimeStamp> activate_after =
-        (init->hasActivateAfter() ? std::make_optional(init->activateAfter())
-                                  : std::nullopt);
-    auto* result = fetch_later_manager_->FetchLater(script_state, request_data,
-                                                    r->signal(), activate_after,
-                                                    exception_state);
-    if (exception_state.HadException()) {
-      return nullptr;
-    }
-
-    return result;
-  }
-
-  uint32_t FetchCount() const override { return fetch_count_; }
-
-  void UpdateDeferredBytesQuota(const KURL& url,
-                                uint64_t& quota_for_url_origin,
-                                uint64_t& total_quota) const override {
-    DCHECK(base::FeatureList::IsEnabled(blink::features::kFetchLaterAPI));
-    CHECK(fetch_later_manager_);
-    return fetch_later_manager_->UpdateDeferredBytesQuota(
-        url, quota_for_url_origin, total_quota);
-  }
-
-  void Trace(Visitor* visitor) const override {
-    visitor->Trace(fetch_manager_);
-    visitor->Trace(fetch_later_manager_);
-    ScopedFetcher::Trace(visitor);
-    Supplement<T>::Trace(visitor);
-  }
-
- private:
-  Member<FetchManager> fetch_manager_;
-  Member<FetchLaterManager> fetch_later_manager_;
-  uint32_t fetch_count_ = 0;
-};
-
-// static
-template <typename T>
-const char GlobalFetchImpl<T>::kSupplementName[] = "GlobalFetchImpl";
-
 }  // namespace
 
-GlobalFetch::ScopedFetcher::~ScopedFetcher() {}
+const char GlobalFetch::ScopedFetcher::kSupplementName[] = "ScopedFetcher";
+
+GlobalFetch::ScopedFetcher::ScopedFetcher(ExecutionContext& context)
+    : Supplement<ExecutionContext>(context),
+      fetch_manager_(MakeGarbageCollected<FetchManager>(&context)),
+      // TODO(crbug.com/1356128): FetchLater is only supported in Document.
+      fetch_later_manager_(
+          base::FeatureList::IsEnabled(blink::features::kFetchLaterAPI) &&
+                  context.IsWindow()
+              ? MakeGarbageCollected<FetchLaterManager>(&context)
+              : nullptr) {}
+
+// static
+GlobalFetch::ScopedFetcher* GlobalFetch::ScopedFetcher::From(
+    ExecutionContext& context) {
+  ScopedFetcher* supplement =
+      Supplement<ExecutionContext>::From<ScopedFetcher>(context);
+  if (!supplement) {
+    supplement = MakeGarbageCollected<ScopedFetcher>(context);
+    Supplement<ExecutionContext>::ProvideTo(context, supplement);
+  }
+  return supplement;
+}
+
+ScriptPromise<Response> GlobalFetch::ScopedFetcher::Fetch(
+    ScriptState* script_state,
+    const V8RequestInfo* input,
+    const RequestInit* init,
+    ExceptionState& exception_state) {
+  fetch_count_ += 1;
+
+  ExecutionContext* execution_context = GetSupplementable();
+  if (!script_state->ContextIsValid() || !execution_context) {
+    // TODO(yhirano): Should this be moved to bindings?
+    exception_state.ThrowTypeError("The global scope is shutting down.");
+    return EmptyPromise();
+  }
+
+  // "Let |r| be the associated request of the result of invoking the
+  // initial value of Request as constructor with |input| and |init| as
+  // arguments. If this throws an exception, reject |p| with it."
+  Request* r = Request::Create(script_state, input, init, exception_state);
+  if (exception_state.HadException()) {
+    return EmptyPromise();
+  }
+
+  probe::WillSendXMLHttpOrFetchNetworkRequest(execution_context, r->url());
+  FetchRequestData* request_data =
+      r->PassRequestData(script_state, exception_state);
+  MeasureFetchProperties(execution_context, request_data);
+
+  // Even if this was checked at the beginning of the function, it might
+  // have been set to nullptr during Request::Create.
+  if (!fetch_manager_->GetExecutionContext()) {
+    exception_state.ThrowTypeError("The global scope is shutting down.");
+    return EmptyPromise();
+  }
+
+  auto promise = fetch_manager_->Fetch(script_state, request_data, r->signal(),
+                                       exception_state);
+  if (exception_state.HadException()) {
+    return EmptyPromise();
+  }
+
+  return promise;
+}
 
 FetchLaterResult* GlobalFetch::ScopedFetcher::FetchLater(
     ScriptState* script_state,
     const V8RequestInfo* input,
     const DeferredRequestInit* init,
     ExceptionState& exception_state) {
-  NOTREACHED();
+  if (!base::FeatureList::IsEnabled(blink::features::kFetchLaterAPI) ||
+      !fetch_later_manager_) {
+    exception_state.ThrowTypeError(
+        "FetchLater is not supported in this scope.");
+    return nullptr;
+  }
+  ExecutionContext* ec = GetSupplementable();
+  if (!script_state->ContextIsValid() || !ec) {
+    exception_state.ThrowTypeError("The global scope is shutting down.");
+    return nullptr;
+  }
+
+  // https://whatpr.org/fetch/1647.html#dom-global-fetch-later
+  // Run the fetchLater(input, init) method steps:
+
+  // 1. Let `r` be the result of invoking the initial value of Request as
+  // constructor with `input` and `init` as arguments. This may throw an
+  // exception.
+  Request* r =
+      Request::Create(script_state, input,
+                      static_cast<const RequestInit*>(init), exception_state);
+  if (exception_state.HadException()) {
+    return nullptr;
+  }
+
+  probe::WillSendXMLHttpOrFetchNetworkRequest(ec, r->url());
+  FetchRequestData* request_data =
+      r->PassRequestData(script_state, exception_state);
+  MeasureFetchProperties(ec, request_data);
+  // 5. If init is given and init ["activateAfter"] exists, then set
+  // `activate_after` to init ["activateAfter"].
+  std::optional<DOMHighResTimeStamp> activate_after =
+      (init->hasActivateAfter() ? std::make_optional(init->activateAfter())
+                                : std::nullopt);
+  auto* result = fetch_later_manager_->FetchLater(
+      script_state, request_data, r->signal(), activate_after, exception_state);
+  if (exception_state.HadException()) {
+    return nullptr;
+  }
+
+  return result;
 }
 
 void GlobalFetch::ScopedFetcher::UpdateDeferredBytesQuota(
     const KURL& url,
     uint64_t& quota_for_url_origin,
     uint64_t& total_quota) const {
-  NOTREACHED();
+  DCHECK(base::FeatureList::IsEnabled(blink::features::kFetchLaterAPI));
+  CHECK(fetch_later_manager_);
+  return fetch_later_manager_->UpdateDeferredBytesQuota(
+      url, quota_for_url_origin, total_quota);
 }
 
-GlobalFetch::ScopedFetcher* GlobalFetch::ScopedFetcher::From(
-    LocalDOMWindow& window) {
-  return GlobalFetchImpl<LocalDOMWindow>::From(window,
-                                               window.GetExecutionContext());
+void GlobalFetch::ScopedFetcher::Trace(Visitor* visitor) const {
+  visitor->Trace(fetch_manager_);
+  visitor->Trace(fetch_later_manager_);
+  Supplement<ExecutionContext>::Trace(visitor);
 }
-
-GlobalFetch::ScopedFetcher* GlobalFetch::ScopedFetcher::From(
-    WorkerGlobalScope& worker) {
-  return GlobalFetchImpl<WorkerGlobalScope>::From(worker,
-                                                  worker.GetExecutionContext());
-}
-
-GlobalFetch::ScopedFetcher* GlobalFetch::ScopedFetcher::From(
-    NavigatorBase& navigator) {
-  return GlobalFetchImpl<NavigatorBase>::From(navigator,
-                                              navigator.GetExecutionContext());
-}
-
-void GlobalFetch::ScopedFetcher::Trace(Visitor* visitor) const {}
 
 ScriptPromise<Response> GlobalFetch::fetch(ScriptState* script_state,
                                            LocalDOMWindow& window,
