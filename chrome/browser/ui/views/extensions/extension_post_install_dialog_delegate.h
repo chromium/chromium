c3d97c774845891986c98e5df397a008bbb59d69
EmiliaPaz <emiliapaz@chromium.org>
2025-12-26 09:58:29 -0800

    [Extensions] Update includes on post install dialog delegate
    This CL updates some unused code in the post install dialog delegate:
    1. Removes unused includes
    2. Adds necessary includes, for the correct buildflag
    3. Removes unused Show()
    4. Updates class comment
    
    Bug: 450296898
    Change-Id: I8d057ca9e650cc647f6f861d4f08a3aa22bfb305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7331199
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Eva Su <evasu@chromium.org>
    Commit-Queue: Eva Su <evasu@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1562753}

--- chrome/browser/ui/views/extensions/extension_post_install_dialog_delegate.h
+++ chrome/browser/ui/views/extensions/extension_post_install_dialog_delegate.h
@@ -5,29 +5,13 @@
 #ifndef CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSION_POST_INSTALL_DIALOG_DELEGATE_H_
 #define CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSION_POST_INSTALL_DIALOG_DELEGATE_H_
 
-#include <string>
-
-#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "build/build_config.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/chrome_pages.h"
-#include "chrome/browser/ui/extensions/extension_installed_watcher.h"
 #include "chrome/browser/ui/extensions/extension_post_install_dialog_model.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_contents_observer.h"
-#include "extensions/common/extension.h"
 #include "ui/base/models/dialog_model.h"
 
-// Provides feedback to the user upon successful installation of an
-// extension. Depending on the type of extension, the Bubble will
-// point to:
-//    OMNIBOX_KEYWORD-> The omnibox.
-//    BROWSER_ACTION -> The browserAction icon in the toolbar.
-//    PAGE_ACTION    -> A preview of the pageAction icon in the location
-//                      bar which is shown while the Bubble is shown.
-//    GENERIC        -> The app menu. This case includes pageActions that don't
-//                      specify a default icon.
+// Model delegate for the dialog that provides feedback to the user upon
+// successful installation of an extension.
 class ExtensionPostInstallDialogDelegate : public ui::DialogModelDelegate {
  public:
   ExtensionPostInstallDialogDelegate(
@@ -38,9 +22,6 @@ class ExtensionPostInstallDialogDelegate : public ui::DialogModelDelegate {
   ExtensionPostInstallDialogDelegate& operator=(
       const ExtensionPostInstallDialogDelegate&) = delete;
   ~ExtensionPostInstallDialogDelegate() override;
-
-  static void Show(Browser* browser,
-                   std::unique_ptr<ExtensionPostInstallDialogModel> model);
 
   const ExtensionPostInstallDialogModel* model() const { return model_.get(); }
 
