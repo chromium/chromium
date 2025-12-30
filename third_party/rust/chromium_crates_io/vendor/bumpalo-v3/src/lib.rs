9c342f319efcec2275a1104bf82c0807b78099d1
Lukasz Anforowicz <lukasza@chromium.org>
2025-12-29 16:36:16 -0800

    Roll bumpalo: 3.19.0 => 3.19.1 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * bumpalo: 3.19.0 => 3.19.1; https://docs.rs/crate/bumpalo/3.19.1
    
    Bug: None
    Change-Id: Id458d8a28780e2c3bef7a7848d8471f7eb84be6e
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7354427
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563274}

--- third_party/rust/chromium_crates_io/vendor/bumpalo-v3/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/bumpalo-v3/src/lib.rs
@@ -6,6 +6,6 @@
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `bumpalo-3.19.0` crate.
+// `bumpalo-3.19.1` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
