28628907f24e27fff20d26471482f377047db3c8
Kelvin Jiang <kelvinjiang@chromium.org>
2025-12-30 11:22:24 -0800

    [Extensions] Do not apply DNR rules for Webview requests
    Extensions should not be able to apply DNR rules to requests originating
    from WebViews.
    
    Bug: 463155954
    Change-Id: I50bcf9d32480407cfa76bb0bfe6cab67351c43ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7354432
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563452}

--- extensions/browser/api/declarative_net_request/ruleset_manager.cc
+++ extensions/browser/api/declarative_net_request/ruleset_manager.cc
@@ -521,6 +521,12 @@ bool RulesetManager::ShouldEvaluateRequest(
     return false;
   }
 
+  // Declarative Net Request rules should not be matched against requests
+  // originating from WebViews.
+  if (request.is_web_view) {
+    return false;
+  }
+
   return true;
 }
 
