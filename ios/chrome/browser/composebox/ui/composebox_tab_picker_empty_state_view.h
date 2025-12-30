320d188f76438b66300fa9be07fbf50c5beaae7c
Aliona DANGLA <alionadangla@google.com>
2025-12-30 04:13:08 -0800

    [iOS] Removes NTPs from tab picker
    See video:
    https://drive.google.com/file/d/1QAChHLP0B6-9OxL5VguKQKPZfalutFJm/view?usp=drive_link
    
    See dynamic type:
    https://drive.google.com/file/d/10uXTlUsrUKaomluC0druFLzwBdFEvf6v/view?usp=drive_link
    
    Fixed: 464539333
    Change-Id: Id43ac82a32afc84aed93bcbb3e163f877ebc7dd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7232093
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563362}

---
+++ ios/chrome/browser/composebox/ui/composebox_tab_picker_empty_state_view.h
@@ -0,0 +1,14 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_COMPOSEBOX_UI_COMPOSEBOX_TAB_PICKER_EMPTY_STATE_VIEW_H_
+#define IOS_CHROME_BROWSER_COMPOSEBOX_UI_COMPOSEBOX_TAB_PICKER_EMPTY_STATE_VIEW_H_
+
+#import "ios/chrome/browser/tab_switcher/ui_bundled/tab_grid/grid/grid_empty_view.h"
+
+// A view that informs the user that there are no tabs to pick.
+@interface ComposeboxTabPickerEmptyStateView : UIView <GridEmptyView>
+@end
+
+#endif  // IOS_CHROME_BROWSER_COMPOSEBOX_UI_COMPOSEBOX_TAB_PICKER_EMPTY_STATE_VIEW_H_
--- ios/chrome/browser/composebox/ui/composebox_tab_picker_view_controller.mm
+++ ios/chrome/browser/composebox/ui/composebox_tab_picker_view_controller.mm
@@ -4,6 +4,7 @@
 
 #import "ios/chrome/browser/composebox/ui/composebox_tab_picker_view_controller.h"
 
+#import "ios/chrome/browser/composebox/ui/composebox_tab_picker_empty_state_view.h"
 #import "ios/chrome/browser/composebox/ui/composebox_tab_picker_mutator.h"
 #import "ios/chrome/browser/shared/public/commands/composebox_tab_picker_commands.h"
 #import "ios/chrome/browser/tab_switcher/tab_grid/base_grid/ui/base_grid_view_controller.h"
@@ -35,6 +36,8 @@
   [super viewDidLoad];
   self.view.backgroundColor = [UIColor colorNamed:kSecondaryBackgroundColor];
 
+  _gridViewController.emptyStateView =
+      [[ComposeboxTabPickerEmptyStateView alloc] init];
   UIView* gridView = _gridViewController.view;
   gridView.translatesAutoresizingMaskIntoConstraints = NO;
   [self addChildViewController:_gridViewController];
