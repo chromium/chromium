e0f9a667d38ef917e5bc71b8d15af914473379ed
Henrique Nakashima <hnakashima@chromium.org>
2026-01-01 19:30:43 -0800

    [Android] Re-enable testOpenTypeDelete_fromWebPage in S
    Behavior seems to be fixed since 2025-12-22.
    
    Bug: 372709072
    Change-Id: If1cbd9026c5a8078b50e6270fda68fb9e5411247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7347520
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1563765}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/OmniboxPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/OmniboxPTTest.java
@@ -4,8 +4,6 @@
 
 package org.chromium.chrome.browser.omnibox;
 
-import android.os.Build;
-
 import androidx.test.filters.LargeTest;
 
 import org.junit.AfterClass;
@@ -17,7 +15,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.transit.TransitAsserts;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
@@ -54,12 +51,6 @@ public class OmniboxPTTest {
 
     @LargeTest
     @Test
-    @DisableIf.Build(
-            sdk_is_greater_than = Build.VERSION_CODES.R,
-            sdk_is_less_than = Build.VERSION_CODES.TIRAMISU,
-            message =
-                    "Text not cleared in S upon Omnibox focus, crbug.com/470413188,"
-                            + " crbug.com/372709072")
     public void testOpenTypeDelete_fromWebPage() {
         ChromeFeatureList.sAndroidBottomToolbarV2ForceBottomForFocusedOmnibox.setForTesting(false);
         WebPageStation blankPage = mCtaTestRule.start();
