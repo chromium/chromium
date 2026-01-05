d8641c7804f3d61aeb5d4cfb6fd7a0f03e01fae4
David Song <wintermelons@google.com>
2026-01-05 10:25:19 -0800

    [fuchsia] Disable flaky test on Fuchsia
    Test flakes regularly on fuchsia-fyi-x64-asan, and is disabled on Linux,
    Android and some iOS builders (via filters).
    
    Bug: 460575998
    Change-Id: Idec3cf6b7e1e36eeef86d4170245740f54e3b09e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7396435
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Auto-Submit: David Song <wintermelons@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564461}

--- components/optimization_guide/content/browser/page_content_metadata_observer_browsertest.cc
+++ components/optimization_guide/content/browser/page_content_metadata_observer_browsertest.cc
@@ -129,7 +129,8 @@ IN_PROC_BROWSER_TEST_F(PageContentMetadataObserverBrowserTest,
 
 // TODO(https://crbug.com/455915204): Test is flaky on android-arm64-tests.
 // TODO(https://crbug.com/455816130): Test is flaky on linux tests.
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
+// TODO(https://crbug.com/460575998): Test is flaky on fuchsia-fyi-x64-asan.
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_FUCHSIA)
 #define MAYBE_NoMetaTags DISABLED_NoMetaTags
 #else
 #define MAYBE_NoMetaTags NoMetaTags
