66fc86ab8b9fd3b51403c5245dfbd3957f813236
Mike Wittman <wittman@chromium.org>
2026-01-05 11:20:49 -0800

    [on_device_model] Ensure weights_file is initialized
    The ChromeMLTest.FailToLoadAssets/* tests were triggering a code path
    that used this field without it being initialized.
    https://crrev.com/c/7004149, which enabled strict handle checking in
    tests, caused these tests to crash.
    
    Fixed: 472402540
    Change-Id: Iacceed72495ab898c9a27b0ba7579b2b6f518ae0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362131
    Auto-Submit: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1564490}

--- services/on_device_model/ml/chrome_ml_api.h
+++ services/on_device_model/ml/chrome_ml_api.h
@@ -62,7 +62,7 @@ struct ChromeMLModelData {
   // File holding the weights data. The file will be owned by the inference
   // library and closed once weight loading is complete. kApuBackend provides
   // the `model_path` and not this field.
-  PlatformFile weights_file;
+  PlatformFile weights_file = kInvalidPlatformFile;
   // A unique ID to identify `weights_file`s which point to the same data.
   // Matching `file_id` tells the backend that the data also matches.
   std::optional<uint32_t> file_id;
