05a1afcc6adde15164e16c56c6f9365dbf46b069
Etienne Dechamps <edechamps@google.com>
2026-01-06 07:11:34 -0800

    Improve CronetUrlRequestHTTPSTest
    Change-Id: I400aa63582c5e3e22a2f73c2db1557e4d43317aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7366862
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Auto-Submit: Etienne Dechamps <edechamps@google.com>
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1564951}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestHTTPSTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestHTTPSTest.java
@@ -6,12 +6,15 @@ package org.chromium.net;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import static org.junit.Assume.assumeFalse;
+
 import static org.chromium.net.truth.UrlResponseInfoSubject.assertThat;
+
+import android.os.Build;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -27,23 +30,71 @@ import org.chromium.net.test.ServerCertificate;
 @DoNotBatch(reason = "crbug/1459563")
 @RunWith(AndroidJUnit4.class)
 public class CronetUrlRequestHTTPSTest {
-    private static final String TAG = CronetUrlRequestHTTPSTest.class.getSimpleName();
-
     @Rule public final CronetTestRule mTestRule = CronetTestRule.withAutomaticEngineStartup();
 
-    private NativeTestServer mNativeTestServer;
-
     @Before
-    public void setUp() {
-        mNativeTestServer =
-                NativeTestServer.createNativeTestServerWithHTTPS(
-                        mTestRule.getTestFramework().getContext(), ServerCertificate.CERT_EXPIRED);
-        mNativeTestServer.start();
+    public void assumeCanInjectTestCertificates() {
+        // The fallback implementation does not go through X509Util, so X509Util-based test
+        // certificate injection will not work. So we have to rely on the trust anchors configured
+        // in the Network Security Config (see res/xml/network_security_config.xml), but that's only
+        // supported from API 24 (N). Therefore, if we are running on API <24, there is no way for
+        // us to inject a test certificate that will work with the fallback implementation.
+        assumeFalse(
+                mTestRule.implementationUnderTest()
+                                == CronetTestFramework.CronetImplementation.FALLBACK
+                        && Build.VERSION.SDK_INT < Build.VERSION_CODES.N);
     }
 
-    @After
-    public void tearDown() {
-        mNativeTestServer.close();
+    @Test
+    @SmallTest
+    public void testSSL() throws Exception {
+        try (var nativeTestServer =
+                NativeTestServer.createNativeTestServerWithHTTPS(
+                        mTestRule.getTestFramework().getContext(), ServerCertificate.CERT_OK)) {
+            nativeTestServer.start();
+            TestUrlRequestCallback callback = new TestUrlRequestCallback();
+            mTestRule
+                    .getTestFramework()
+                    .getEngine()
+                    .newUrlRequestBuilder(
+                            nativeTestServer.getEchoMethodURL(), callback, callback.getExecutor())
+                    .build()
+                    .start();
+            callback.blockForDone();
+            assertThat(callback.mResponseAsString).isEqualTo("GET");
+            assertThat(callback.getResponseInfoWithChecks()).hasHttpStatusCodeThat().isEqualTo(200);
+        }
+    }
+
+    @Test
+    @SmallTest
+    @IgnoreFor(
+            implementations = {CronetImplementation.FALLBACK},
+            reason = "crbug.com/1495320: Refactor error checking")
+    public void testSSLCertificateError() throws Exception {
+        try (var nativeTestServer =
+                NativeTestServer.createNativeTestServerWithHTTPS(
+                        mTestRule.getTestFramework().getContext(),
+                        ServerCertificate.CERT_EXPIRED)) {
+            nativeTestServer.start();
+            TestUrlRequestCallback callback = new TestUrlRequestCallback();
+            mTestRule
+                    .getTestFramework()
+                    .getEngine()
+                    .newUrlRequestBuilder(
+                            nativeTestServer.getEchoMethodURL(), callback, callback.getExecutor())
+                    .build()
+                    .start();
+            callback.blockForDone();
+
+            assertThat(callback.getResponseInfo()).isNull();
+            assertThat(callback.mOnErrorCalled).isTrue();
+            assertThat(callback.mError)
+                    .hasMessageThat()
+                    .contains("Exception in CronetUrlRequest: net::ERR_CERT_DATE_INVALID");
+            mTestRule.assertCronetInternalErrorCode((NetworkException) callback.mError, -201);
+            assertThat(callback.mResponseStep).isEqualTo(ResponseStep.ON_FAILED);
+        }
     }
 
     /**
@@ -55,33 +106,39 @@ public class CronetUrlRequestHTTPSTest {
     @IgnoreFor(
             implementations = {CronetImplementation.FALLBACK},
             reason = "crbug.com/1495320: Refactor error checking")
-    public void testSSLCertificateError() throws Exception {
-        TestUrlRequestCallback callback = new TestUrlRequestCallback();
-        UrlRequest.Builder builder =
-                mTestRule
-                        .getTestFramework()
-                        .getEngine()
-                        .newUrlRequestBuilder(
-                                mNativeTestServer.getFileURL("/"),
-                                callback,
-                                callback.getExecutor());
+    public void testSSLCertificateErrorWithUpload() throws Exception {
+        try (var nativeTestServer =
+                NativeTestServer.createNativeTestServerWithHTTPS(
+                        mTestRule.getTestFramework().getContext(),
+                        ServerCertificate.CERT_EXPIRED)) {
+            nativeTestServer.start();
+            TestUrlRequestCallback callback = new TestUrlRequestCallback();
+            UrlRequest.Builder builder =
+                    mTestRule
+                            .getTestFramework()
+                            .getEngine()
+                            .newUrlRequestBuilder(
+                                    nativeTestServer.getFileURL("/"),
+                                    callback,
+                                    callback.getExecutor());
 
-        TestUploadDataProvider dataProvider =
-                new TestUploadDataProvider(
-                        TestUploadDataProvider.SuccessCallbackMode.SYNC, callback.getExecutor());
-        dataProvider.addRead("test".getBytes());
-        builder.setUploadDataProvider(dataProvider, callback.getExecutor());
-        builder.addHeader("Content-Type", "useless/string");
-        builder.build().start();
-        callback.blockForDone();
-        dataProvider.assertClosed();
+            TestUploadDataProvider dataProvider =
+                    new TestUploadDataProvider(
+                            TestUploadDataProvider.SuccessCallbackMode.SYNC,
+                            callback.getExecutor());
+            dataProvider.addRead("test".getBytes());
+            builder.setUploadDataProvider(dataProvider, callback.getExecutor());
+            builder.addHeader("Content-Type", "useless/string");
+            builder.build().start();
+            callback.blockForDone();
 
-        assertThat(callback.getResponseInfo()).isNull();
-        assertThat(callback.mOnErrorCalled).isTrue();
-        assertThat(callback.mError)
-                .hasMessageThat()
-                .contains("Exception in CronetUrlRequest: net::ERR_CERT_DATE_INVALID");
-        mTestRule.assertCronetInternalErrorCode((NetworkException) callback.mError, -201);
-        assertThat(callback.mResponseStep).isEqualTo(ResponseStep.ON_FAILED);
+            assertThat(callback.getResponseInfo()).isNull();
+            assertThat(callback.mOnErrorCalled).isTrue();
+            assertThat(callback.mError)
+                    .hasMessageThat()
+                    .contains("Exception in CronetUrlRequest: net::ERR_CERT_DATE_INVALID");
+            mTestRule.assertCronetInternalErrorCode((NetworkException) callback.mError, -201);
+            assertThat(callback.mResponseStep).isEqualTo(ResponseStep.ON_FAILED);
+        }
     }
 }
