ed41e69a16900a52bccafd8fec73c0f6296ce9b9
Christian Biesinger <cbiesinger@chromium.org>
2026-01-06 14:24:27 -0800

    [M144] [FedCM] Allow the permission context service in ash-internals
    Original change's description:
    > [FedCM] Allow the permission context service in ash-internals
    > 
    > As evidenced by the crash report in the linked bug, ash internal
    > profiles can use FedCM, perhaps through setting the signin status from
    > the signin flow. This allows the service in such profiles to make this
    > work and removes comments about this.
    > 
    > In addition, this CL removes comments about the guest profile. Since
    > guest profiles, to the best of my knowledge, are used for regular web
    > browsing, we want FedCM to work there and so we need those services.
    > 
    > Bug: 470341161
    > Change-Id: I8d4394cef9446bb1623516cf27b198eed35627dc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7275842
    > Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    > Reviewed-by: Nicolás Peña <npm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1561321}
    
    (cherry picked from commit 14ce7b9da13d2f4484edf2c6a4ebcbdd1aadd253)
    
    Bug: 473815475,470341161
    Change-Id: I8d4394cef9446bb1623516cf27b198eed35627dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7363758
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#3129}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- chrome/browser/webid/federated_identity_api_permission_context_factory.cc
+++ chrome/browser/webid/federated_identity_api_permission_context_factory.cc
@@ -31,11 +31,7 @@ FederatedIdentityApiPermissionContextFactory::
           "FederatedIdentityApiPermissionContext",
           ProfileSelections::Builder()
               .WithRegular(ProfileSelection::kOwnInstance)
-              // TODO(crbug.com/40257657): Check if this service is needed in
-              // Guest mode.
               .WithGuest(ProfileSelection::kOwnInstance)
-              // TODO(crbug.com/41488885): Check if this service is needed for
-              // Ash Internals.
               .WithAshInternals(ProfileSelection::kOwnInstance)
               .Build()) {
   DependsOn(HostContentSettingsMapFactory::GetInstance());
