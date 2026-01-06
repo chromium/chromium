1b9aac200745933776265d17393907ebdd7d4f8c
Mohannad Farrag <aymanm@google.com>
2026-01-06 13:22:32 -0800

    Revert "Stop checking the package name in CronetTestRule"
    This reverts commit 770b73ded7d203e27c3ecc1fefcb5b170b0d9615.
    
    Reason for revert: Broke GN2BP: http://android-build/test_investigate/invocation/I87500010495325149/test/TR38630033881213628/ and http://ag/37767750
    
    Original change's description:
    > Stop checking the package name in CronetTestRule
    >
    > This doesn't seem to make any sense, and no-one seems to know why this
    > check exists.
    >
    > Change-Id: Ic56f23273ecaeabf3451ab9ac0a057540c87e7a6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7366891
    > Commit-Queue: Etienne Dechamps <edechamps@google.com>
    > Reviewed-by: Stefano Duo <stefanoduo@google.com>
    > Auto-Submit: Etienne Dechamps <edechamps@google.com>
    > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1564955}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I551424ad0411adf2d22518e79cac4179d8bf68c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7362621
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1565235}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetTestRule.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetTestRule.java
@@ -237,32 +237,38 @@ public class CronetTestRule implements TestRule {
 
         Log.i(TAG, "Implementations to be tested against: %s", implementationsUnderTest);
 
-        boolean allImplementionsSkipped = true;
-        AssumptionViolatedException lastAssumptionViolatedException = null;
-        for (CronetTestFramework.CronetImplementation implementation : implementationsUnderTest) {
-            if (BuildConfig.CRONET_FOR_AOSP_BUILD
-                    && implementation.equals(CronetTestFramework.CronetImplementation.FALLBACK)) {
-                // Skip executing tests for JavaCronetEngine.
-                continue;
+        if (packageName.startsWith("org.chromium.net")) {
+            boolean allImplementionsSkipped = true;
+            AssumptionViolatedException lastAssumptionViolatedException = null;
+            for (CronetTestFramework.CronetImplementation implementation :
+                    implementationsUnderTest) {
+                if (BuildConfig.CRONET_FOR_AOSP_BUILD
+                        && implementation.equals(
+                                CronetTestFramework.CronetImplementation.FALLBACK)) {
+                    // Skip executing tests for JavaCronetEngine.
+                    continue;
+                }
+                Log.i(TAG, "Running test against " + implementation + " implementation.");
+                setImplementationUnderTest(implementation);
+                boolean implementationSkipped = false;
+                try {
+                    evaluateWithFramework(base, testName, netLogEnabled, desc);
+                    allImplementionsSkipped = false;
+                } catch (AssumptionViolatedException assumptionViolatedException) {
+                    // Some tests may want so skip based on the implementation under test. Make sure
+                    // we try every implementation before declaring the test skipped.
+                    Log.i(
+                            TAG,
+                            implementation + " skipped due to violated assumption",
+                            assumptionViolatedException);
+                    lastAssumptionViolatedException = assumptionViolatedException;
+                }
             }
-            Log.i(TAG, "Running test against " + implementation + " implementation.");
-            setImplementationUnderTest(implementation);
-            boolean implementationSkipped = false;
-            try {
-                evaluateWithFramework(base, testName, netLogEnabled, desc);
-                allImplementionsSkipped = false;
-            } catch (AssumptionViolatedException assumptionViolatedException) {
-                // Some tests may want so skip based on the implementation under test. Make sure
-                // we try every implementation before declaring the test skipped.
-                Log.i(
-                        TAG,
-                        implementation + " skipped due to violated assumption",
-                        assumptionViolatedException);
-                lastAssumptionViolatedException = assumptionViolatedException;
+            if (allImplementionsSkipped) {
+                throw lastAssumptionViolatedException;
             }
-        }
-        if (allImplementionsSkipped) {
-            throw lastAssumptionViolatedException;
+        } else {
+            evaluateWithFramework(base, testName, netLogEnabled, desc);
         }
     }
 
