9c9e135f39cfb1b8570ebbe1811adc55e7383d4e
Etienne Pierre-doray <etiennep@chromium.org>
2026-01-07 10:17:49 -0800

    [v8] Enable IsolatesPriorityUseProcessPriority in testing config
    Bug: 456731897
    Change-Id: If65e837773a80277d15ff7370caa82ad4fb7f81a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7408995
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565762}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -1159,5 +1159,6 @@ public final class ProductionSupportedFlagList {
                 CcFeatures.THROTTLE_MAIN_FRAME_TO60_HZ_WEBVIEW,
                 "When enabled, requestAnimationFrame() and related main-thread effects are capped"
                         + " at 60Hz on 120Hz devices."),
+        Flag.baseFeature("IsolatesPriorityUseProcessPriority"),
     };
 }
