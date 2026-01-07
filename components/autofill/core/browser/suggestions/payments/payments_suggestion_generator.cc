32145b3470ac0a2cf73dfdf8fa1f43834a0d088d
Mikita Kuchyn <kuchyn@google.com>
2026-01-07 09:51:02 -0800

    [CCSG-2] Replace 'base::FindOrNull' with 'base::Contains'
    This CL chains follows the strategy outlined in
    go/ccsg-refactor-strategy. It implements the step that prepares
    payments_suggestion_generator* to the refactor itself.
    
    Bug: 409962888
    Change-Id: I396f2edbfe9ff581698ae8565c84f20370936417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380108
    Commit-Queue: Mikita Kuchyn <kuchyn@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1565748}

--- components/autofill/core/browser/suggestions/payments/payments_suggestion_generator.cc
+++ components/autofill/core/browser/suggestions/payments/payments_suggestion_generator.cc
@@ -294,8 +294,8 @@ std::vector<Suggestion> GenerateCreditCardSuggestionsSync(
     bool is_card_number_field_empty,
     const payments::AmountExtractionStatus& amount_extraction_status) {
   std::vector<Suggestion> suggestions;
-  if (base::FindOrNull(suggestion_data,
-                       SuggestionDataSource::kSaveAndFillPromo)) {
+  if (base::Contains(suggestion_data,
+                     SuggestionDataSource::kSaveAndFillPromo)) {
     bool display_gpay_logo = false;
     suggestions.push_back(
         CreateSaveAndFillSuggestion(client, display_gpay_logo));
@@ -306,8 +306,8 @@ std::vector<Suggestion> GenerateCreditCardSuggestionsSync(
             display_gpay_logo, amount_extraction_status),
         std::back_inserter(suggestions));
     return suggestions;
-  } else if (base::FindOrNull(suggestion_data,
-                              SuggestionDataSource::kVirtualStandaloneCvc)) {
+  } else if (base::Contains(suggestion_data,
+                            SuggestionDataSource::kVirtualStandaloneCvc)) {
     // Only trigger GetVirtualCreditCardsForStandaloneCvcField if it's
     // standalone CVC field.
     base::flat_map<std::string, VirtualCardUsageData::VirtualCardLastFour>
