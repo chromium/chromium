6263e2bf23c81c6a76f40e6cc000ea568046a3f3
Nasser Al-shawwa <alshawwa@chromium.org>
2026-01-07 13:55:33 -0800

    [M144] Fix watermark in popups
    (cherry picked from commit 148f9926b9d9e07473845a6fb3ae84a3fe527c21)
    
    Bug: 473583671
    Change-Id: Ic7d12f31703181b32eee39e5f02658e63e5e7f94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7387375
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1564508}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7407374
    Cr-Commit-Position: refs/branch-heads/7559@{#3171}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- chrome/browser/ui/browser_window/internal/browser_window_features.cc
+++ chrome/browser/ui/browser_window/internal/browser_window_features.cc
@@ -794,16 +794,19 @@ void BrowserWindowFeatures::InitPostBrowserViewConstruction(
               *browser_, browser_, std::move(container_overlay_view_pairs));
     }
 
+    if (features::HasTabSearchToolbarButton()) {
+      tab_search_toolbar_button_controller_ =
+          std::make_unique<TabSearchToolbarButtonController>(browser_view);
+    }
+  }
+
+  if (browser_->GetType() == BrowserWindowInterface::Type::TYPE_NORMAL ||
+      browser_->GetType() == BrowserWindowInterface::Type::TYPE_POPUP) {
     data_protection_ui_controller_ =
         GetUserDataFactory()
             .CreateInstance<
                 enterprise_data_protection::DataProtectionUIController>(
                 *browser_view->browser(), browser_view);
-
-    if (features::HasTabSearchToolbarButton()) {
-      tab_search_toolbar_button_controller_ =
-          std::make_unique<TabSearchToolbarButtonController>(browser_view);
-    }
   }
 
 #if !BUILDFLAG(IS_CHROMEOS)
