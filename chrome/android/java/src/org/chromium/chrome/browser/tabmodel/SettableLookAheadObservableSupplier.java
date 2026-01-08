c16c1b9f95ace64a5a779f87abce2c12b2e945af
Fiaz Muhammad <mfiaz@google.com>
2026-01-08 11:06:22 -0800

    Several changes to LookAheadObservableSupplier
    - Add tests for LookAheadObservableSupplierUnitTest
    - Split to a settable implementation, and a non-settable interface.
             The interface will remain in chrome/browser/tab for use in
             Tab.java.
    
    Bug: 473780461
    Change-Id: I45dfb1d674a7bd212d1cdcbb5c0a1e0ffe19784a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7398072
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566438}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/SettableLookAheadObservableSupplier.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/SettableLookAheadObservableSupplier.java
@@ -11,18 +11,27 @@ import org.chromium.base.supplier.ObservableSupplier.NotifyBehavior;
 import org.chromium.base.supplier.SettableNullableObservableSupplier;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
+import org.chromium.chrome.browser.tab.LookAheadObservableSupplier;
 
 import java.util.Objects;
 
-/**
- * An observable supplier that allows for the registration of observers that are notified of the new
- * value before the value of the supplier is actually changed.
- */
+/** An settable implementation of {@link LookAheadObservableSupplier}. */
 @NullMarked
-public class LookAheadObservableSupplier<T> implements SettableNullableObservableSupplier<T> {
+public class SettableLookAheadObservableSupplier<T>
+        implements LookAheadObservableSupplier<T>, SettableNullableObservableSupplier<T> {
     private final SettableNullableObservableSupplier<T> mLookAheadSupplier = createNullable();
     private final SettableNullableObservableSupplier<T> mObservableSupplier = createNullable();
     private boolean mLookAheadValueSet;
+
+    /**
+     * Manually declare to look ahead observers that the supplier will be set to |value|.
+     *
+     * @param value The value the supplier will be set to.
+     */
+    public void willSet(@Nullable T value) {
+        mLookAheadSupplier.set(value);
+        mLookAheadValueSet = true;
+    }
 
     @Override
     public void set(@Nullable T value) {
@@ -42,43 +51,18 @@ public class LookAheadObservableSupplier<T> implements SettableNullableObservabl
         mLookAheadSupplier.destroy();
     }
 
-    /**
-     * Manually declare to look ahead observers that the supplier will be set to |value|.
-     *
-     * @param value The value the supplier will be set to.
-     */
-    public void willSet(@Nullable T value) {
-        mLookAheadSupplier.set(value);
-        mLookAheadValueSet = true;
-    }
-
-    /**
-     * Adds an observer that is notified before the supplier's value is changed. The observer
-     * receives the new value that is about to be set.
-     *
-     * @param obs The observer to add.
-     */
+    @Override
     public @Nullable T addLookAheadObserver(Callback<@Nullable T> obs) {
         return mLookAheadSupplier.addObserver(obs);
     }
 
-    /**
-     * Adds an observer that is notified before the supplier's value is changed. The observer
-     * receives the new value that is about to be set.
-     *
-     * @param obs The observer to add.
-     * @param behavior The {@link NotifyBehavior} the observer will exhibit.
-     */
+    @Override
     public @Nullable T addLookAheadObserver(
             Callback<@Nullable T> obs, @NotifyBehavior int behavior) {
         return mLookAheadSupplier.addObserver(obs, behavior);
     }
 
-    /**
-     * Removes a look ahead observer.
-     *
-     * @param obs The observer to remove.
-     */
+    @Override
     public void removeLookAheadObserver(Callback<@Nullable T> obs) {
         mLookAheadSupplier.removeObserver(obs);
     }
