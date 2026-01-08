3670059b590071e3c5e053e52613d9e1d55ffa22
Stefano Duo <stefanoduo@google.com>
2026-01-08 05:19:59 -0800

    Reland "Drop deprecated Cronet proxy APIs"
    This reverts commit a4bfdba422e8fc89a5e93bb62448068edf34d1b2.
    
    Reason for revert: http://ag/37785898 moved HttpEngine proxy APIs implementation away from the deprecated Cronet proxy APIs.
    
    Original change's description:
    > Revert "Drop deprecated Cronet proxy APIs"
    >
    > This reverts commit c8a6c6ca663a9c4805ac4215db3dfe772bbaec75.
    >
    > Reason for revert: Broke GN2BP: http://ag/37766692
    >
    > Original change's description:
    > > Drop deprecated Cronet proxy APIs
    > >
    > > Now that internal clients have moved onto the new APIs the backward
    > > compatibility layer is no longer necessary.
    > >
    > > Bug: b:469376436
    > > Change-Id: I4041ebbe25e61571a175c2bb5609f3a441888802
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7265607
    > > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > > Reviewed-by: Etienne Dechamps <edechamps@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1564873}
    >
    > Bug: b:469376436
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I5a1d1b611677786054c0e5a2f52f07b24c98b29b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380023
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Mohannad Farrag <aymanm@google.com>
    > Reviewed-by: Stefano Duo <stefanoduo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1564941}
    
    Bug: b:469376436
    Cq-Include-Trybots: luci.chrome.try:cronet-arm64-gn2bp-debug
    Change-Id: Iea7a420f573dfc47de2f3b001e555504c409f81e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380990
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1566228}

--- components/cronet/android/test/javatests/src/org/chromium/net/impl/VersionSafeProxyOptionsTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/impl/VersionSafeProxyOptionsTest.java
@@ -49,7 +49,7 @@ public class VersionSafeProxyOptionsTest {
     @Test
     @SmallTest
     public void testDirectProxy_correctlyCreatesProxyCallback() {
-        ProxyOptions proxyOptions = new ProxyOptions(Arrays.asList((Proxy) null));
+        ProxyOptions proxyOptions = ProxyOptions.fromProxyList(Arrays.asList((Proxy) null));
         VersionSafeProxyOptions safeProxyOptions = new VersionSafeProxyOptions(proxyOptions);
         List<VersionSafeProxyCallback> safeProxyCallbacks =
                 safeProxyOptions.createProxyCallbackList();
@@ -60,7 +60,7 @@ public class VersionSafeProxyOptionsTest {
     @Test
     @SmallTest
     public void testDirectProxy_correctlyCreatesProxyOptionsProto() {
-        ProxyOptions proxyOptions = new ProxyOptions(Arrays.asList((Proxy) null));
+        ProxyOptions proxyOptions = ProxyOptions.fromProxyList(Arrays.asList((Proxy) null));
         VersionSafeProxyOptions safeProxyOptions = new VersionSafeProxyOptions(proxyOptions);
         org.chromium.net.impl.proto.ProxyOptions proxyOptionsProto =
                 safeProxyOptions.createProxyOptionsProto();
@@ -78,9 +78,9 @@ public class VersionSafeProxyOptionsTest {
     @RequiresMinAndroidApi(Build.VERSION_CODES.N)
     public void testHttpProxy_correctlyCreatesProxyOptionsProto() {
         ProxyOptions proxyOptions =
-                new ProxyOptions(
+                ProxyOptions.fromProxyList(
                         Arrays.asList(
-                                new Proxy(
+                                Proxy.createHttpProxy(
                                         Proxy.SCHEME_HTTP,
                                         "not-existing-hostname",
                                         8080,
@@ -104,9 +104,9 @@ public class VersionSafeProxyOptionsTest {
     @RequiresMinAndroidApi(Build.VERSION_CODES.N)
     public void testHttpProxy_correctlyCreatesProxyCallback() {
         ProxyOptions proxyOptions =
-                new ProxyOptions(
+                ProxyOptions.fromProxyList(
                         Arrays.asList(
-                                new Proxy(
+                                Proxy.createHttpProxy(
                                         Proxy.SCHEME_HTTP,
                                         "not-existing-hostname",
                                         8080,
@@ -127,9 +127,9 @@ public class VersionSafeProxyOptionsTest {
     @RequiresMinAndroidApi(Build.VERSION_CODES.N)
     public void testHttpsProxy_correctlyCreatesProxyOptionsProto() {
         ProxyOptions proxyOptions =
-                new ProxyOptions(
+                ProxyOptions.fromProxyList(
                         Arrays.asList(
-                                new Proxy(
+                                Proxy.createHttpProxy(
                                         Proxy.SCHEME_HTTPS,
                                         "not-existing-hostname",
                                         8080,
@@ -153,9 +153,9 @@ public class VersionSafeProxyOptionsTest {
     @RequiresMinAndroidApi(Build.VERSION_CODES.N)
     public void testHttpsProxy_correctlyCreatesProxyCallback() {
         ProxyOptions proxyOptions =
-                new ProxyOptions(
+                ProxyOptions.fromProxyList(
                         Arrays.asList(
-                                new Proxy(
+                                Proxy.createHttpProxy(
                                         Proxy.SCHEME_HTTPS,
                                         "not-existing-hostname",
                                         8080,
@@ -179,15 +179,15 @@ public class VersionSafeProxyOptionsTest {
                 Mockito.mock(Proxy.HttpConnectCallback.class);
         Proxy.HttpConnectCallback httpProxyCallback = Mockito.mock(Proxy.HttpConnectCallback.class);
         ProxyOptions proxyOptions =
-                new ProxyOptions(
+                ProxyOptions.fromProxyList(
                         Arrays.asList(
-                                new Proxy(
+                                Proxy.createHttpProxy(
                                         Proxy.SCHEME_HTTPS,
                                         "not-existing-hostname",
                                         8080,
                                         Executors.newSingleThreadExecutor(),
                                         httpsProxyCallback),
-                                new Proxy(
+                                Proxy.createHttpProxy(
                                         Proxy.SCHEME_HTTP,
                                         "not-existing-hostname",
                                         8080,
