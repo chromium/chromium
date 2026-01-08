f87fb5cff911d7fed95ad35cb8f3093bb53c2212
Gauthier Ambard <gambard@chromium.org>
2026-01-08 00:54:18 -0800

    [iOS] Prepare to rename BrowserContainer
    Needed for downstream compat.
    To be landed before crrev.com/i/8902538.
    
    Bug: None
    Change-Id: I6fdc86923cda24e682c2561beb708d904554a424
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7410640
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1566143}

---
+++ ios/chrome/browser/browser_content/ui_bundled/edit_menu_app_interface.h
@@ -0,0 +1,10 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_BROWSER_CONTENT_UI_BUNDLED_EDIT_MENU_APP_INTERFACE_H_
+#define IOS_CHROME_BROWSER_BROWSER_CONTENT_UI_BUNDLED_EDIT_MENU_APP_INTERFACE_H_
+
+#import "ios/chrome/browser/browser_container/ui_bundled/edit_menu_app_interface.h"
+
+#endif  // IOS_CHROME_BROWSER_BROWSER_CONTENT_UI_BUNDLED_EDIT_MENU_APP_INTERFACE_H_
---
+++ ios/chrome/browser/browser_content/ui_bundled/edit_menu_matchers.h
@@ -0,0 +1,10 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_BROWSER_CONTENT_UI_BUNDLED_EDIT_MENU_MATCHERS_H_
+#define IOS_CHROME_BROWSER_BROWSER_CONTENT_UI_BUNDLED_EDIT_MENU_MATCHERS_H_
+
+#import "ios/chrome/browser/browser_container/ui_bundled/edit_menu_matchers.h"
+
+#endif  // IOS_CHROME_BROWSER_BROWSER_CONTENT_UI_BUNDLED_EDIT_MENU_MATCHERS_H_
