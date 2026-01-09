cda747d787a424dc6e6b55259bbc38e4dd1df967
Radu Nitescu <radunitescu@google.com>
2026-01-09 12:33:04 -0800

    [M144] [iOS][AIM] Cleanup carrousel cell when the item is removed
    Original change's description:
    > [iOS][AIM] Cleanup carrousel cell when the item is removed
    > 
    > Previously, image data remained in memory as long as the ComposeBox was
    > active because the collection view deferred UI cleanup until cell
    > recycling. This led to unnecessary memory overhead when users added and
    > then removed multiple images.
    > 
    > Memory retention was limited by the maximum number of visible carousel
    > items—up to 5 in portrait and 8 in landscape. However, because the app
    > held full-sized images (approx. 25 MB each for camera images), this
    > overhead quickly reached 125MB–200 MB, significantly inflating the
    > memory footprint of the composebox.
    > 
    > Prior to
    > https://chromium-review.googlesource.com/c/chromium/src/+/7379110 the
    > images & cell UI would have been retained indefinitely.
    > 
    > Bug: 473466935, 470352744
    > Change-Id: Iecdb2abc3ec5a95a4f7be50b23d6bcbf412fb69f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7380104
    > Reviewed-by: Louis Romero <lpromero@google.com>
    > Reviewed-by: Christian Xu <christianxu@chromium.org>
    > Auto-Submit: Radu Nitescu <radunitescu@google.com>
    > Commit-Queue: Christian Xu <christianxu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1565601}
    
    (cherry picked from commit e1fc23a4098ee57174f31ed8ebf7469858bc53ba)
    
    Bug: 474048902,473466935,470352744
    Change-Id: Iecdb2abc3ec5a95a4f7be50b23d6bcbf412fb69f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7420700
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7559@{#3252}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- ios/chrome/browser/composebox/ui/composebox_input_item_cell.mm
+++ ios/chrome/browser/composebox/ui/composebox_input_item_cell.mm
@@ -78,6 +78,7 @@ const CGFloat kCloseButtonAlpha = 0.9;
 
 - (void)prepareForReuse {
   [super prepareForReuse];
+  _associatedItem = nil;
   [_inputItemView prepareForReuse];
 }
 
@@ -85,6 +86,7 @@ const CGFloat kCloseButtonAlpha = 0.9;
 
 - (void)configureWithItem:(ComposeboxInputItem*)item
                     theme:(ComposeboxTheme*)theme {
+  _associatedItem = item;
   [_inputItemView configureWithItem:item theme:theme];
 
   BOOL isLoading = item.state == ComposeboxInputItemState::kLoading ||
