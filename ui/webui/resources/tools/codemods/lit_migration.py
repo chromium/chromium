4446bf7822f8952921e6c1af1832944a873d89c6
dpapad <dpapad@chromium.org>
2026-01-12 15:23:34 -0800

    WebUI: Check in ui/webui/resources/tools/codemods/package.json file.
    This is done to avoid any confusion about which versions of the
    underlying 'jscodeshift' tool should be used when WebUI developers run
    scripts in this folder locally.
    
    Bug: None
    Change-Id: I0a53d47e7bcd9ed50ac06ffd6ffc9794a5bceadb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7456933
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568103}

--- ui/webui/resources/tools/codemods/lit_migration.py
+++ ui/webui/resources/tools/codemods/lit_migration.py
@@ -16,17 +16,12 @@ import node
 
 """
  Instructions to run this script locally.
- 1) Create a package.json file in the same folder with the following contents.
+ 1) From the root of the repository run the following command
 
-{
-  "dependencies": {
-    "jscodeshift": "^0.15.1"
-  }
-}
+    npm install --prefix ui/webui/resources/tools/codemods/ --no-bin-links \
+        --no-fund --ignore-scripts --omit=dev --omit=optional
 
- 2) Run 'npm install' in the same folder.
-
- 3) Invoke the script from the root directory of the repository. For example
+ 2) Invoke the script from the root directory of the repository. For example
 
     python3 ui/webui/resources/tools/codemods/lit_migration.py \
         --file ui/webui/resources/cr_components/most_visited/most_visited.ts
