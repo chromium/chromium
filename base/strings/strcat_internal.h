aee988aa7dc3b3120e7dac3707c7bee11688397a
Jan Wilken Dörrie <jdoerrie@chromium.org>
2026-01-12 00:48:18 -0800

    [base] Modernize StrCat internals using resize_and_overwrite
    Simplify `StrCat` implementation by leveraging the C++23 feature
    `std::string::resize_and_overwrite`. This allows resizing the
    destination string without the overhead of zero-initializing the new
    memory, replacing the previous custom `Resize` helper that relied on the
    libc++-specific `__resize_default_init`.
    
    Modernize the implementation further by:
    - Using `base::span` operations for copying data instead of
      `std::char_traits::copy`.
    - Adopting C++17 nested namespace syntax.
    
    Change-Id: Ie55a811a2df8687fb46dfd29c16167eeb82efc22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7422373
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1567642}

--- base/strings/strcat_internal.h
+++ base/strings/strcat_internal.h
@@ -5,65 +5,52 @@
 #ifndef BASE_STRINGS_STRCAT_INTERNAL_H_
 #define BASE_STRINGS_STRCAT_INTERNAL_H_
 
-#include <concepts>
 #include <string>
 
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
 
-namespace base {
+namespace base::internal {
 
-namespace internal {
-
-// Default to regular `std::basic_string::resize()`.
-template <typename CharT>
-void Resize(std::basic_string<CharT>& str, size_t total_size) {
-  str.resize(total_size);
+// Trims the first `n` elements of `span`.
+template <typename T>
+void RemovePrefix(base::span<T>& span, size_t n) {
+  span = span.subspan(n);
 }
 
-// Optimized version of `std::basic_string::resize()` that skips zero
-// initialization of appended characters. Reading from the newly allocated
-// characters results in undefined behavior if they are not explicitly
-// initialized afterwards. Available in C++23 as
-// `std::basic_string::resize_and_overwrite()`:
-// https://en.cppreference.com/w/cpp/string/basic_string/resize_and_overwrite
-template <typename CharT>
-  requires requires(std::basic_string<CharT>& str, size_t total_size) {
-    { str.__resize_default_init(total_size) } -> std::same_as<void>;
-  }
-auto Resize(std::basic_string<CharT>& str, size_t total_size) {
-  str.__resize_default_init(total_size);
-}
-
-// Appends `pieces` to `dest`. Instead of simply calling `dest.append()`
-// `pieces.size()` times, this method first resizes `dest` to be of the desired
-// size, and then appends each piece via `std::char_traits::copy`. This achieves
+// Appends `strings` to `dest`. Instead of simply calling `dest.append()`
+// `strings.size()` times, this method first resizes `dest` to be of the desired
+// size, and then appends each string via `std::ranges::copy`. This achieves
 // two goals:
 // 1) Allocating the desired size all at once avoids other allocations that
 //    could happen if intermediate allocations did not reserve enough capacity.
-// 2) Invoking std::char_traits::copy instead of std::basic_string::append
+// 2) Invoking base::span::copy_prefix_from instead of std::basic_string::append
 //    avoids having to write the terminating '\0' character n times.
 template <typename CharT, typename StringT>
-void StrAppendT(std::basic_string<CharT>& dest, span<const StringT> pieces) {
+void StrAppendT(std::basic_string<CharT>& dest, span<const StringT> strings) {
   const size_t initial_size = dest.size();
   size_t total_size = initial_size;
-  for (const auto& cur : pieces) {
-    total_size += cur.size();
+  for (const StringT& str : strings) {
+    total_size += str.size();
   }
 
-  // Note: As opposed to `reserve()` calling `resize()` with an argument smaller
-  // than the current `capacity()` does not result in the string releasing spare
-  // capacity. Furthermore, common std::string implementations apply a geometric
-  // growth strategy if the current capacity is not sufficient for the newly
-  // added characters. Since this codepath is also triggered by `resize()`, we
-  // don't have to manage the std::string's capacity ourselves here to avoid
-  // performance hits in case `StrAppend()` gets called in a loop.
-  Resize(dest, total_size);
-  CharT* dest_char = &dest[initial_size];
-  for (const auto& cur : pieces) {
-    std::char_traits<CharT>::copy(dest_char, cur.data(), cur.size());
-    UNSAFE_TODO(dest_char += cur.size());
-  }
+  dest.resize_and_overwrite(total_size, [&](CharT* p, size_t n) {
+    // SAFETY: `std::basic_string::resize_and_overwrite` guarantees that the
+    // range `[p, p + n]` is valid.
+    UNSAFE_BUFFERS(base::span to_overwrite(p, n));
+
+    // The first `initial_size` characters are guaranteed to be the previous
+    // contents of `dest`.
+    RemovePrefix(to_overwrite, initial_size);
+
+    // Copy each string into the destination, trimming the written prefix after
+    // every iteration.
+    for (const StringT& str : strings) {
+      to_overwrite.copy_prefix_from(str);
+      RemovePrefix(to_overwrite, str.size());
+    }
+    return n;
+  });
 }
 
 template <typename StringT>
@@ -73,8 +60,6 @@ auto StrCatT(span<const StringT> pieces) {
   return result;
 }
 
-}  // namespace internal
-
-}  // namespace base
+}  // namespace base::internal
 
 #endif  // BASE_STRINGS_STRCAT_INTERNAL_H_
