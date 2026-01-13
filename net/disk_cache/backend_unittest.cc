02c003866d0017f95d8ad7cf10137eaf38282c9a
Mark Schillaci <mschillaci@google.com>
2026-01-13 13:41:31 -0800

    [Gardener] Disable DiskCacheBackendTest#SimpleCacheNegMaxSize on Android
    Bug: 475586889
    Change-Id: I28f4e69b9ccafb1e76394048b60eab4e65feb9bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7455424
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Owners-Override: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1568694}

--- net/disk_cache/backend_unittest.cc
+++ net/disk_cache/backend_unittest.cc
@@ -4314,7 +4314,13 @@ TEST_F(DiskCacheBackendTest, SimpleCacheLateDoom) {
             simple_cache_impl_->index()->init_method());
 }
 
-TEST_F(DiskCacheBackendTest, SimpleCacheNegMaxSize) {
+// TODO(crbug.com/475586889): Test has flaked regularly.
+#if BUILDFLAG(IS_ANDROID)
+#define MAYBE_SimpleCacheNegMaxSize DISABLED_SimpleCacheNegMaxSize
+#else
+#define MAYBE_SimpleCacheNegMaxSize SimpleCacheNegMaxSize
+#endif
+TEST_F(DiskCacheBackendTest, MAYBE_SimpleCacheNegMaxSize) {
   SetCacheType(net::GENERATED_BYTE_CODE_CACHE);
 
   SetMaxSize(-1);
