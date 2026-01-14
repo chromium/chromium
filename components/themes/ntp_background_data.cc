a91926c5471590339f4eb34cec255e37c691286f
Xinyi Ji <xinyiji@google.com>
2026-01-14 05:50:22 -0800

    [CustomizationV2] Configure FIFE image options for theme collection images (Part 2)
    This CL updates the Android-specific FIFE URL options for NTP theme collections. Specifically, it changes the filtering label to chrome_android_ntp. Note that Android will continue to use the same theme collections as iOS.
    
    Bug: 423579377, 469447684
    Change-Id: I4659447d79eb5eae52391222eef66233cd404967
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7414341
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569063}

--- components/themes/ntp_background_data.cc
+++ components/themes/ntp_background_data.cc
@@ -18,11 +18,8 @@ constexpr char kThumbnailImageOptions[] = "=s639-k-no-nd";
 // etc. Options appear on an image URL after the '=' character.
 constexpr char kImageOptions[] = "=s2556-k-no-nd";
 #elif BUILDFLAG(IS_ANDROID)
-// TODO(crbug.com/423579377): Update the filter label to 'chrome_android_ntp'.
-
-// Android label added to request to filter out unwanted collections. We use the
-// same collections as iOS.
-constexpr char kFilteringLabel[] = "chrome_ios_ntp";
+// Android label added to request to filter out unwanted collections.
+constexpr char kFilteringLabel[] = "chrome_android_ntp";
 // The Android options to be added to a thumbnail image URL, specifying
 // resolution, cropping, etc. Options appear on an image URL after the '='
 // character. This resolution matches the height an width of bg-sel-tile.
