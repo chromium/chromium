d5c13295e97d4560c365b1fd3a093e0d82906d0d
Lynne Jiang <lyjiang@google.com>
2026-01-14 13:31:28 -0800

    Reland "[webnn] Implement litert graph."
    This reverts commit d50c947ac18b20434030696c7b9831fb03a487d5.
    
    Reason for revert: Move forward changes with gated build flag.
    
    Original change's description:
    > Revert "[webnn] Implement litert graph."
    >
    > This reverts commit b56af4ce129f48b06b2aa2d6b4e1c6c3d2d973ff.
    >
    > (Keeps the LiteRT roll in that CL).
    >
    > Reason for revert: Maybe causing build failures
    > https://logs.chromium.org/logs/chrome/buildbucket/cr-buildbucket/8693118135882057857/+/u/compile/raw_io.output_text_failure_summary_
    >
    > Original change's description:
    > > [webnn] Implement litert graph.
    > >
    > > All files branched from the tflite implementations. Current LiteRT flow only works when kWebNNTfliteDumpModel is turned on, and only support CPU and MLDrift WebGPU as accelerator for now.
    > > Pending support:
    > > * LiteRT External Weight Loader.
    > > * LiteRT Profiler Summarizer.
    > > * Other accelerators eg. NPU etc.
    > >
    > >
    > > BYPASS_LARGE_CHANGE_WARNING: Expected large code change for initial cl, most of the code are branched from the tflite graph implementations.
    > >
    > > Change-Id: I733813fb4118d56435883bf0804eb21dc8482eff
    > > Bug: 454732289
    > > Compile-Size: Introducing litert deps in webnn, approval granted in http://shortn/_UhjvMr8lfI.
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7272010
    > > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > > Commit-Queue: Lynne Jiang <lyjiang@google.com>
    > > Reviewed-by: Hu, Ningxin <ningxin.hu@intel.com>
    > > Reviewed-by: Phillis Tang <phillis@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1567156}
    >
    > Bug: 454732289
    > Change-Id: I2fb72d8301cee537b3a593f9a930c2178f1f4210
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7455507
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1568326}
    
    Bug: 454732289
    Change-Id: I9b8207bd3400350da5f595658d35263c096315cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7467865
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Lynne Jiang <lyjiang@google.com>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1569284}

---
+++ services/webnn/tflite/context_impl_litert.h
@@ -0,0 +1,95 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_WEBNN_TFLITE_CONTEXT_IMPL_LITERT_H_
+#define SERVICES_WEBNN_TFLITE_CONTEXT_IMPL_LITERT_H_
+
+#include "mojo/public/cpp/bindings/pending_associated_receiver.h"
+#include "services/webnn/public/cpp/webnn_trace.h"
+#include "services/webnn/public/cpp/webnn_types.h"
+#include "services/webnn/webnn_context_impl.h"
+#include "services/webnn/webnn_graph_impl.h"
+
+namespace webnn {
+
+class WebNNConstantOperand;
+
+namespace litert {
+
+// `ContextImplLiteRt` is created by `WebNNContextProviderImpl` and responsible
+// for creating a `GraphImplLiteRt` which uses LiteRt for inference.
+class ContextImplLiteRt final : public WebNNContextImpl {
+ public:
+  // Constructs a new `ContextImplLiteRt`. Must be called on
+  // `owning_task_runner`.
+  static std::unique_ptr<WebNNContextImpl, OnTaskRunnerDeleter> Create(
+      mojo::PendingReceiver<mojom::WebNNContext> receiver,
+      base::WeakPtr<WebNNContextProviderImpl> context_provider,
+      mojom::CreateContextOptionsPtr options,
+      mojo::ScopedDataPipeConsumerHandle write_tensor_consumer,
+      mojo::ScopedDataPipeProducerHandle read_tensor_producer,
+      std::unique_ptr<ScopedGpuSequence> gpu_sequence,
+      scoped_refptr<gpu::MemoryTracker> memory_tracker,
+      scoped_refptr<base::SingleThreadTaskRunner> owning_task_runner,
+      gpu::SharedImageManager* shared_image_manager,
+      scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
+      ScopedTrace scoped_trace);
+
+  ContextImplLiteRt(
+      mojo::PendingReceiver<mojom::WebNNContext> receiver,
+      base::WeakPtr<WebNNContextProviderImpl> context_provider,
+      mojom::CreateContextOptionsPtr options,
+      mojo::ScopedDataPipeConsumerHandle write_tensor_consumer,
+      mojo::ScopedDataPipeProducerHandle read_tensor_producer,
+      std::unique_ptr<ScopedGpuSequence> gpu_sequence,
+      scoped_refptr<gpu::MemoryTracker> memory_tracker,
+      scoped_refptr<base::SingleThreadTaskRunner> owning_task_runner,
+      gpu::SharedImageManager* shared_image_manager,
+      scoped_refptr<base::SingleThreadTaskRunner> main_task_runner);
+
+  ContextImplLiteRt(const WebNNContextImpl&) = delete;
+  ContextImplLiteRt& operator=(const ContextImplLiteRt&) = delete;
+
+  // WebNNContextImpl:
+  base::WeakPtr<WebNNContextImpl> AsWeakPtr() override;
+
+ private:
+  ~ContextImplLiteRt() override;
+
+  void CreateGraphImpl(
+      mojo::PendingAssociatedReceiver<mojom::WebNNGraph> receiver,
+      mojom::GraphInfoPtr graph_info,
+      WebNNGraphImpl::ComputeResourceInfo compute_resource_info,
+      base::flat_map<OperandId, std::unique_ptr<WebNNConstantOperand>>
+          constant_operands,
+      base::flat_map<OperandId, WebNNTensorImpl*> constant_tensor_operands,
+      CreateGraphImplCallback callback) override;
+
+  void DidCreateWeightsFile(
+      mojo::PendingAssociatedReceiver<mojom::WebNNGraph> receiver,
+      mojom::GraphInfoPtr graph_info,
+      WebNNGraphImpl::ComputeResourceInfo compute_resource_info,
+      base::flat_map<OperandId, std::unique_ptr<WebNNConstantOperand>>
+          constant_operands,
+      base::flat_map<OperandId, WebNNTensorImpl*> constant_tensor_operands,
+      CreateGraphImplCallback callback,
+      base::File weights_file);
+
+  base::expected<scoped_refptr<WebNNTensorImpl>, mojom::ErrorPtr>
+  CreateTensorImpl(mojo::PendingAssociatedReceiver<mojom::WebNNTensor> receiver,
+                   mojom::TensorInfoPtr tensor_info) override;
+
+  base::expected<scoped_refptr<WebNNTensorImpl>, mojom::ErrorPtr>
+  CreateTensorFromSharedImageImpl(
+      mojo::PendingAssociatedReceiver<mojom::WebNNTensor> receiver,
+      mojom::TensorInfoPtr tensor_info,
+      WebNNTensorImpl::RepresentationPtr representation) override;
+
+  base::WeakPtrFactory<ContextImplLiteRt> weak_factory_{this};
+};
+
+}  // namespace litert
+}  // namespace webnn
+
+#endif  // SERVICES_WEBNN_TFLITE_CONTEXT_IMPL_LITERT_H_
