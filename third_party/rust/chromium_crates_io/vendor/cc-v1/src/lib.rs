750b71b4984f982de1de887d0d3b788db305730c
Daniel Rubery <drubery@chromium.org>
2026-01-13 17:00:23 -0800

    Roll cc: 1.2.51 => 1.2.52 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * cc: 1.2.51 => 1.2.52; https://docs.rs/crate/cc/1.2.52
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: I00ec4b9c29dba05574cf081db24e998ba610fa6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7455786
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568826}

--- third_party/rust/chromium_crates_io/vendor/cc-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/cc-v1/src/lib.rs
@@ -6,6 +6,6 @@
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `cc-1.2.51` crate.
+// `cc-1.2.52` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
