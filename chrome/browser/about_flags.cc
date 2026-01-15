f9f782d27c164a2625cf60aac5efbfc4af96a7c5
Justin Donnelly <jdonnelly@chromium.org>
2026-01-15 09:39:09 -0800

    [144] [Omnibox Next] Add back showRecentTabChip to variants
    Seems like this flag was accidentally removed here: https://chromium-review.googlesource.com/c/chromium/src/+/7269248
    
    (cherry picked from commit d6c2c0687fb6df0138ec9f3702fa81fb95adaa4a)
    
    Bug: 450006744, 475848051
    Change-Id: I68ab07e0275cd67e7a2b77674637f7f73227c2ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7300545
    Commit-Queue: Aviv Kiss <avivkiss@google.com>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Aviv Kiss <avivkiss@google.com>
    Auto-Submit: Marlon Facey <mfacey@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1561811}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7477184
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#3518}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -871,10 +871,12 @@ const FeatureEntry::FeatureParam
         {"AddContextButtonVariant", "below_results"},
         {"ShowVoiceSearchInSteadyComposebox", "true"},
         {"ShowVoiceSearchInExpandedComposebox", "true"},
+        {"ShowRecentTabChip", "true"},
 };
 const FeatureEntry::FeatureParam
     kWebUIOmniboxAimPopupAddContextButtonAboveResults[] = {
         {"AddContextButtonVariant", "above_results"},
+        {"ShowRecentTabChip", "true"},
 };
 const FeatureEntry::FeatureParam kWebUIOmniboxAimPopupAddContextButtonInline[] =
     {
