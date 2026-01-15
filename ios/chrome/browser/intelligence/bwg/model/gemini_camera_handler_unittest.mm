caa157972b634edc7f34d8395a79d8ad2ec608ea
Nicolas MacBeth <nicolasmacbeth@google.com>
2026-01-15 07:29:35 -0800

    [iOS] Fix and re-enable GeminiCameraHandler test
    Bug: 475828386
    Change-Id: I269caccaf3868f1addbc1213f08cd1c06b2d5676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7474072
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Auto-Submit: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1569744}

--- ios/chrome/browser/intelligence/bwg/model/gemini_camera_handler_unittest.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_camera_handler_unittest.mm
@@ -7,7 +7,9 @@
 #import <AVFoundation/AVFoundation.h>
 #import <UIKit/UIKit.h>
 
+#import "components/prefs/pref_registry_simple.h"
 #import "components/prefs/testing_pref_service.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
 #import "third_party/ocmock/OCMock/OCMock.h"
@@ -16,6 +18,8 @@
 class GeminiCameraHandlerTest : public PlatformTest {
  protected:
   GeminiCameraHandlerTest() {
+    pref_service_.registry()->RegisterBooleanPref(
+        prefs::kIOSGeminiCameraSetting, true);
     handler_ = [[GeminiCameraHandler alloc] initWithPrefService:&pref_service_];
   }
 
@@ -28,9 +32,8 @@ TEST_F(GeminiCameraHandlerTest, TestConformsToProtocol) {
   EXPECT_TRUE([handler_ conformsToProtocol:@protocol(GeminiCameraDelegate)]);
 }
 
-// TODO(crbug.com/475828386): Fix disabled test.
 // Tests that openCameraFromViewController can be called without crashing.
-TEST_F(GeminiCameraHandlerTest, DISABLED_TestOpenCamera) {
+TEST_F(GeminiCameraHandlerTest, TestOpenCamera) {
   // Mock AVCaptureDevice to simulate an authorized state.
   id mockDevice = OCMClassMock([AVCaptureDevice class]);
   OCMStub([mockDevice authorizationStatusForMediaType:AVMediaTypeVideo])
