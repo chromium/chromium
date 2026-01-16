95629fff60b6a4f4183cdcb65a5d01fd9a8ede06
Fiaz Muhammad <mfiaz@google.com>
2026-01-16 05:10:00 -0800

    [Persist] Add shadow store functionality to CustomTMO and HeadlessTMO
    - Add shadow store functionality to CustomTTMOrchestrator and
            HeadlessTMOrchestrator.
    - Switch to using an Activity in CTTMO.createTabModels so we can
            extract its task ID to use as the window tag.
    
    Bug: 475252643
    Change-Id: I3ac77bacdcd842ae5259ae2e53f2910a6c2d7fe7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7465613
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1570316}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/CustomTabsTabModelOrchestrator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/CustomTabsTabModelOrchestrator.java
@@ -4,14 +4,19 @@
 
 package org.chromium.chrome.browser.app.tabmodel;
 
-import android.content.Context;
+import static org.chromium.chrome.browser.app.tabmodel.TabPersistentStoreFactory.buildShadowStore;
+
+import android.app.Activity;
 
 import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.app.tabwindow.TabWindowManagerSingleton;
 import org.chromium.chrome.browser.crypto.CipherFactory;
 import org.chromium.chrome.browser.flags.ActivityType;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.profiles.ProfileProvider;
+import org.chromium.chrome.browser.tabmodel.AccumulatingTabCreator;
 import org.chromium.chrome.browser.tabmodel.AsyncTabParamsManager;
 import org.chromium.chrome.browser.tabmodel.NextTabPolicy;
 import org.chromium.chrome.browser.tabmodel.NextTabPolicy.NextTabPolicySupplier;
@@ -20,6 +25,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelectorImpl;
 import org.chromium.chrome.browser.tabmodel.TabPersistencePolicy;
 import org.chromium.chrome.browser.tabmodel.TabPersistentStore;
 import org.chromium.chrome.browser.tabmodel.TabPersistentStoreImpl;
+import org.chromium.chrome.browser.tabwindow.TabWindowManager;
 
 /**
  * Glue-level class that manages lifetime of root .tabmodel objects: {@link TabPersistentStore} and
@@ -29,9 +35,24 @@ import org.chromium.chrome.browser.tabmodel.TabPersistentStoreImpl;
 public class CustomTabsTabModelOrchestrator extends TabModelOrchestrator {
     public CustomTabsTabModelOrchestrator() {}
 
+    private static final String WINDOW_PREFIX = TabPersistentStoreImpl.CLIENT_TAG_CUSTOM + "_";
+    private final AccumulatingTabCreator mRegularShadowTabCreator = new AccumulatingTabCreator();
+    private final AccumulatingTabCreator mIncognitoShadowTabCreator = new AccumulatingTabCreator();
+
+    private @Nullable TabPersistentStore mShadowTabPersistentStore;
+
+    @Override
+    public void destroy() {
+        if (mShadowTabPersistentStore != null) {
+            mShadowTabPersistentStore.destroy();
+        }
+
+        super.destroy();
+    }
+
     /** Creates the TabModelSelector and the TabPersistentStore. */
     public void createTabModels(
-            Context context,
+            Activity activity,
             OneshotSupplier<ProfileProvider> profileProviderSupplier,
             TabCreatorManager tabCreatorManager,
             TabPersistencePolicy persistencePolicy,
@@ -42,7 +63,7 @@ public class CustomTabsTabModelOrchestrator extends TabModelOrchestrator {
         NextTabPolicySupplier nextTabPolicySupplier = () -> NextTabPolicy.LOCATIONAL;
         mTabModelSelector =
                 new TabModelSelectorImpl(
-                        context,
+                        activity,
                         /* modalDialogManager= */ null,
                         profileProviderSupplier,
                         tabCreatorManager,
@@ -54,6 +75,7 @@ public class CustomTabsTabModelOrchestrator extends TabModelOrchestrator {
                         false);
 
         // Instantiate TabPersistentStore
+        TabWindowManager tabWindowManager = TabWindowManagerSingleton.getInstance();
         mTabPersistencePolicy = persistencePolicy;
         mTabPersistentStore =
                 new TabPersistentStoreImpl(
@@ -61,8 +83,27 @@ public class CustomTabsTabModelOrchestrator extends TabModelOrchestrator {
                         mTabPersistencePolicy,
                         mTabModelSelector,
                         tabCreatorManager,
-                        TabWindowManagerSingleton.getInstance(),
+                        tabWindowManager,
                         cipherFactory);
+
+        profileProviderSupplier.onAvailable(
+                provider -> {
+                    Profile profile = provider.getOriginalProfile();
+                    assert profile != null;
+
+                    String windowTag = WINDOW_PREFIX + activity.getTaskId();
+                    mShadowTabPersistentStore =
+                            buildShadowStore(
+                                    profile,
+                                    mRegularShadowTabCreator,
+                                    mIncognitoShadowTabCreator,
+                                    mTabModelSelector,
+                                    mTabPersistencePolicy,
+                                    mTabPersistentStore,
+                                    windowTag,
+                                    cipherFactory,
+                                    /* recordMetrics= */ false);
+                });
 
         wireSelectorAndStore();
         markTabModelsInitialized();
