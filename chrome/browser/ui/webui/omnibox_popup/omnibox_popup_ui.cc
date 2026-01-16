26adbd9041769482725accc2d6a809ec69a5e1be
Justin Donnelly <jdonnelly@chromium.org>
2026-01-16 08:16:28 -0800

    [144] [omnibox-next] Fix up "lens enabled" loadTimeData value.
    This CL updates the value passed in for the
    "composeboxShowLensSearchChip" loadTimeData to take AIM popup enabled
    state into account. Without this fix clients w/ WebUI popup, but w/o
    AIM popup will end up seeing the Lens search chip, due to the
    default-enabled state of the relevant feature param.
    
    (cherry picked from commit 9896ad3aacfb48db8b629b7c416e3efa034f9de7)
    
    Bug: 450007883, 476233712
    Change-Id: I7d443f6514408aea561e6323521fc11037776ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7274800
    Auto-Submit: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1561880}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487273
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#3589}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- chrome/browser/ui/webui/omnibox_popup/omnibox_popup_ui.cc
+++ chrome/browser/ui/webui/omnibox_popup/omnibox_popup_ui.cc
@@ -141,7 +141,8 @@ OmniboxPopupUI::OmniboxPopupUI(content::WebUI* web_ui)
   source->AddBoolean("composeboxShowImageSuggest",
                      omnibox::kShowComposeboxImageSuggestions.Get());
   source->AddBoolean("composeboxShowLensSearchChip",
-                     omnibox::kShowLensSearchChip.Get());
+                     omnibox::IsAimPopupEnabled(profile_) &&
+                         omnibox::kShowLensSearchChip.Get());
   source->AddBoolean("addTabUploadDelayOnRecentTabChipClick",
                      omnibox::kAddTabUploadDelayOnRecentTabChipClick.Get());
   source->AddBoolean("composeboxShowRecentTabChip",
