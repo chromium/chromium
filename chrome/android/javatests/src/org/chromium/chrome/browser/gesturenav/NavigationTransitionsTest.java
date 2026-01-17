2e5b7056cb6ed33cabe92ad7e741e1b37ee46fd6
Ritika Gupta <ritikagup@google.com>
2026-01-16 17:15:09 -0800

    [Gardener] Disabling failing NavigationTransitionsTest#startBackNavWithTopControlHidden
    Bug: 476505895
    Change-Id: I2c2860bf17e93ea82f0f976b8afc87adcc7b17d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489719
    Auto-Submit: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Owners-Override: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1570706}

--- chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationTransitionsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationTransitionsTest.java
@@ -684,6 +684,7 @@ public class NavigationTransitionsTest {
      */
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/476505895")
     public void startBackNavWithTopControlHidden() throws Throwable {
         if (mTestNavigationMode == NAVIGATION_MODE_GESTURAL
                 && VERSION.SDK_INT < VERSION_CODES.UPSIDE_DOWN_CAKE) return;
