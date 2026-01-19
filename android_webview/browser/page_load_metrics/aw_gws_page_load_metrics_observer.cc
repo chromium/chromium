25f6522f430963fa428a981c822ddc2a7e3b0f25
Takashi Toyoshima <toyoshim@chromium.org>
2026-01-19 04:54:38 -0800

    DSEPrewarm: Implement AwGWSPageLoadMetricsObserver::IsSignedIn
    This was not implemented on Android WebView as the check depends on a
    component that Android WebView doesn't use. This change uses WebView
    specific CookieManager implementation to introduce an equivalent check
    without using the component.
    
    Change-Id: Idf75bd3ea25049f28cb86b738eda762fa0199d7e
    Bug: 476919129
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489160
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571119}

--- android_webview/browser/page_load_metrics/aw_gws_page_load_metrics_observer.cc
+++ android_webview/browser/page_load_metrics/aw_gws_page_load_metrics_observer.cc
@@ -4,7 +4,20 @@
 
 #include "android_webview/browser/page_load_metrics/aw_gws_page_load_metrics_observer.h"
 
+#include "android_webview/browser/aw_browser_context.h"
+#include "android_webview/browser/cookie_manager.h"
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
+#include "net/cookies/cookie_util.h"
+#include "net/cookies/parsed_cookie.h"
+#include "url/gurl.h"
+
 namespace android_webview {
+
+namespace {
+constexpr char kGwsOriginUrl[] = "https://www.google.com/";
+constexpr char kWarmaUpCookieName[] = "SSID";
+}  // namespace
 
 AwGWSPageLoadMetricsObserver::AwGWSPageLoadMetricsObserver() = default;
 
@@ -24,7 +37,24 @@ bool AwGWSPageLoadMetricsObserver::IsIncognitoProfile() const {
 
 bool AwGWSPageLoadMetricsObserver::IsSignedIn(
     content::BrowserContext* browser_context) const {
-  // Not implemented. Always returns false.
+  AwBrowserContext* aw_browser_context =
+      static_cast<AwBrowserContext*>(browser_context);
+  CHECK(aw_browser_context);
+  CookieManager* cookie_manager = aw_browser_context->GetCookieManager();
+  CHECK(cookie_manager);
+
+  JNIEnv* env = base::android::AttachCurrentThread();
+  base::android::ScopedJavaLocalRef<jstring> url =
+      base::android::ConvertUTF8ToJavaString(env, kGwsOriginUrl);
+  std::string cookies = cookie_manager->GetCookie(env, url);
+  net::cookie_util::ParsedRequestCookies parsed_cookies;
+  net::cookie_util::ParseRequestCookieLine(cookies, &parsed_cookies);
+  for (const auto& pair : parsed_cookies) {
+    if (pair.first == kWarmaUpCookieName) {
+      return true;
+    }
+  }
+
   return false;
 }
 
