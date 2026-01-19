f63b582071db0497cb831f9adaf2b5a49ebd6f53
Colin Blundell <blundell@chromium.org>
2026-01-19 07:37:10 -0800

    [Blink] Remove `recording` from CanvasResourceProvider::Clear()
    This local variable is no longer necessary.
    
    Bug: 352263194
    Change-Id: I25d35ee3b8cc75982cf1ff7f1865b6a152344e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488058
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571205}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -1720,9 +1720,7 @@ void CanvasResourceProvider::Clear() {
   auto timer = CreateScopedRasterTimer();
 
   clear_frame_ = true;
-  cc::PaintRecord recording;
-  recording = recorder_->ReleaseMainRecording();
-  RasterRecord(recording);
+  RasterRecord(recorder_->ReleaseMainRecording());
   // Images are locked for the duration of the rasterization, in case they get
   // used multiple times. We can unlock them once the rasterization is complete.
   ReleaseLockedImages();
