99af97b71e1c880d822df6caa22681009b7b7781
Gabriel Charette <gab@chromium.org>
2026-01-20 09:08:38 -0800

    Remove PerformFortuitousMemoryBarrierIfNecessary
    All depending data races were fixed.
    
    Fixed: 458682617
    Change-Id: Ia70f9f8db76adc4a5b20782245907428ca84d65f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7416326
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571681}

--- base/features.cc
+++ base/features.cc
@@ -207,7 +207,6 @@ void Init() {
                                  std::memory_order_relaxed);
 
   sequence_manager::internal::SequenceManagerImpl::InitializeFeatures();
-  sequence_manager::internal::ThreadController::InitializeFeatures();
   base::internal::JobTaskSource::InitializeFeatures();
 
   debug::StackTrace::InitializeFeatures();
