f33444bca7df4dbc71c16eef5b461979b01f0b76
Adem Derinel <derinel@google.com>
2026-01-19 23:14:58 -0800

    Move WebAuthenticationImmediateGet to experimental
    The origin trial for WebAuthenticationImmediateGet has ended in M144.
    This CL moves the feature to "experimental" status and removes the
    origin trial configuration from runtime_enabled_features.json5.
    
    Change-Id: I80dd42839ea4a37b90337191af93a46c241e2aca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488599
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571455}

--- device/fido/public/features.cc
+++ device/fido/public/features.cc
@@ -168,15 +168,10 @@ BASE_FEATURE_PARAM(int,
                    "window_seconds",
                    kDefaultWindowSeconds);
 
-// Enabled by default on Desktop for the Origin Trial. Do not remove until the
-// Origin Trial expires.
+// Not yet enabled by default.
 BASE_FEATURE(kWebAuthnImmediateGet,
              "WebAuthenticationImmediateGet",
-#if BUILDFLAG(IS_ANDROID)
              base::FEATURE_DISABLED_BY_DEFAULT);
-#else
-             base::FEATURE_ENABLED_BY_DEFAULT);
-#endif  // BUILDFLAG(IS_ANDROID)
 
 BASE_FEATURE_PARAM(int,
                    kWebAuthnImmediateMediationTimeoutMilliseconds,
