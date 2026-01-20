a5756537adf903f55bcd9ccd170afe6b5577d972
Noah Rose Ledesma <noahrose@google.com>
2026-01-20 08:19:32 -0800

    Implement GetUpdaterStates on PageHandler
    Add a method which may be used by the WebUI to query the state of the
    per-system and per-user updater instances on the system.
    
    Bug: 456498251
    Change-Id: Iddddf9b27bf950110a2008ba2a07b6e06a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487771
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571656}

--- chrome/browser/ui/webui/updater/updater_page_handler.h
+++ chrome/browser/ui/webui/updater/updater_page_handler.h
@@ -5,15 +5,49 @@
 #ifndef CHROME_BROWSER_UI_WEBUI_UPDATER_UPDATER_PAGE_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_UPDATER_UPDATER_PAGE_HANDLER_H_
 
+#include <optional>
+#include <string>
+
+#include "base/files/file_path.h"
+#include "base/functional/callback.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/sequence_checker.h"
 #include "chrome/browser/ui/webui/updater/updater_ui.mojom.h"
+#include "chrome/updater/mojom/updater_service.mojom.h"
+#include "chrome/updater/updater_scope.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
 class UpdaterPageHandler final : public updater_ui::mojom::PageHandler {
  public:
+  class Delegate : public base::RefCountedThreadSafe<Delegate> {
+   public:
+    static scoped_refptr<Delegate> CreateDefault();
+
+    virtual std::optional<base::FilePath> GetInstallDirectory(
+        updater::UpdaterScope scope) const = 0;
+    virtual void GetSystemUpdaterState(
+        base::OnceCallback<void(const updater::mojom::UpdaterState&)> callback)
+        const = 0;
+    virtual void GetUserUpdaterState(
+        base::OnceCallback<void(const updater::mojom::UpdaterState&)> callback)
+        const = 0;
+    virtual void GetSystemPoliciesJson(
+        base::OnceCallback<void(const std::string&)> callback) const = 0;
+    virtual void GetUserPoliciesJson(
+        base::OnceCallback<void(const std::string&)> callback) const = 0;
+
+   protected:
+    friend class base::RefCountedThreadSafe<Delegate>;
+
+    virtual ~Delegate() = default;
+  };
+
   UpdaterPageHandler(
       mojo::PendingReceiver<updater_ui::mojom::PageHandler> receiver,
-      mojo::PendingRemote<updater_ui::mojom::Page> page);
+      mojo::PendingRemote<updater_ui::mojom::Page> page,
+      scoped_refptr<Delegate> delegate = Delegate::CreateDefault());
 
   UpdaterPageHandler(const UpdaterPageHandler&) = delete;
   UpdaterPageHandler& operator=(const UpdaterPageHandler&) = delete;
@@ -21,10 +55,13 @@ class UpdaterPageHandler final : public updater_ui::mojom::PageHandler {
   ~UpdaterPageHandler() override;
 
   void GetAllUpdaterEvents(GetAllUpdaterEventsCallback callback) override;
+  void GetUpdaterStates(GetUpdaterStatesCallback callback) override;
 
  private:
+  SEQUENCE_CHECKER(sequence_checker_);
   mojo::Receiver<updater_ui::mojom::PageHandler> receiver_;
   mojo::Remote<updater_ui::mojom::Page> page_;
+  scoped_refptr<Delegate> delegate_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_UPDATER_UPDATER_PAGE_HANDLER_H_
