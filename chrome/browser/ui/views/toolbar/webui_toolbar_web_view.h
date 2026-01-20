4e055f05caae0af292135dceb23249508f9c6ebf
Mingyu Lei <leimy@chromium.org>
2026-01-19 23:05:19 -0800

    Webium Product: recover the crashed renderer process
    This CL introduces a recovery mechanism for the `WebUIToolbarWebView`
    renderer process. If the renderer process for the WebUI-based toolbar
    crashes or terminates abnormally, the system will now attempt to reload
    the content.
    
    New params are added to the `kWebUIReloadButton` to control the recovery
    behavior.
    
    Bug: 474228715
    Change-Id: Ic307612971f6e96022d4007ff8be0d0a69854c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7462015
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1571445}

--- chrome/browser/ui/views/toolbar/webui_toolbar_web_view.h
+++ chrome/browser/ui/views/toolbar/webui_toolbar_web_view.h
@@ -6,9 +6,12 @@
 #define CHROME_BROWSER_UI_VIEWS_TOOLBAR_WEBUI_TOOLBAR_WEB_VIEW_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "chrome/browser/ui/browser_command_controller.h"
 #include "chrome/browser/ui/views/toolbar/webui_reload_control.h"
 #include "chrome/browser/ui/webui/webui_toolbar/webui_toolbar_page_handler.h"
+#include "chrome/common/webui_url_constants.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
@@ -47,12 +50,16 @@ class WebUIToolbarWebView
 
   // content::WebContentsObserver:
   void DidFirstVisuallyNonEmptyPaint() override;
+  void PrimaryMainFrameRenderProcessGone(
+      base::TerminationStatus status) override;
 
   void SetDidFirstNonEmptyPaintCallbackForTesting(base::OnceClosure callback);
+  views::WebView* GetWebViewForTesting() { return web_view_; }
 
  private:
   friend WebUIReloadControl;
 
+  void InitializeWebView();
   chrome::BrowserCommandController* controller() { return controller_; }
   WebUIToolbarUI* GetWebUIToolbarUI();
 
@@ -62,6 +69,10 @@ class WebUIToolbarWebView
   WebUIReloadControl reload_control_;
   base::OnceClosure did_first_non_empty_paint_callback_;
   bool has_finished_first_non_empty_paint_ = false;
+  uint32_t crash_count_ = 0;
+  base::TimeTicks last_crash_time_;
+
+  base::WeakPtrFactory<WebUIToolbarWebView> weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TOOLBAR_WEBUI_TOOLBAR_WEB_VIEW_H_
