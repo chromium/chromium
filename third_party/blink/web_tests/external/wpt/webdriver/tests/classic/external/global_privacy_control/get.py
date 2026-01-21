a556ab7852c5cbf251a7b1d91e1d9ce4e4a40c58
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2026-01-21 10:23:36 -0800

    Import wpt@9bd4d659822e4f1b71ad4b039786cfbadbd3dd44
    https://github.com/web-platform-tests/wpt/compare/1d7daa67a...9bd4d6598
    
    Using wpt-import in Chromium 6d8aa6648c1772983e9f0a6df79f8662244d0abe.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    japhet@chromium.org:
      external/wpt/fetch
    kojii@chromium.org:
      external/wpt/css/css-font-loading
    smcgruer@chromium.org:
      external/wpt/css/css-position
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac-skia-alt-arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac12.0.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac13.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel
    Cq-Include-Trybots: luci.chromium.try:mac15.arm64-blink-rel
    Cq-Include-Trybots: luci.chromium.try:win10.20h2-blink-rel
    Change-Id: I9c215f04cb6f07067e7ca6f64bb8ccba57ad1001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499716
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1572427}

--- third_party/blink/web_tests/external/wpt/webdriver/tests/classic/external/global_privacy_control/get.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/classic/external/global_privacy_control/get.py
@@ -1,14 +1,11 @@
 from tests.support.asserts import assert_success
 
-
-def get_gpc(session):
-    return session.transport.send(
-        "GET", "/session/{session_id}/privacy".format(
-            session_id=session.session_id))
+from . import get_gpc
 
 
 def test_get_gpc(session):
     response = get_gpc(session)
     value = assert_success(response)
+
     assert "gpc" in value
     assert type(value["gpc"]) is bool
--- third_party/blink/web_tests/external/wpt/webdriver/tests/classic/external/global_privacy_control/set.py
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/classic/external/global_privacy_control/set.py
@@ -1,26 +1,21 @@
 import pytest
+
 from tests.support.asserts import assert_error, assert_success
+from . import get_gpc, set_gpc
 
-
-def set_gpc(session, value):
-    return session.transport.send(
-        "POST", "/session/{session_id}/privacy".format(
-            session_id=session.session_id), {"gpc": value})
-
-def get_gpc(session):
-    return session.transport.send(
-        "GET", "/session/{session_id}/privacy".format(
-            session_id=session.session_id))
 
 @pytest.mark.parametrize("value", [True, False])
 def test_set_gpc_success(session, value):
     response = set_gpc(session, value)
     newValue = assert_success(response)
+
     assert "gpc" in newValue
     assert type(newValue["gpc"]) is bool
     assert newValue["gpc"] == value
+
     getResponse = get_gpc(session)
     value = assert_success(getResponse)
+
     assert "gpc" in value
     assert type(value["gpc"]) is bool
     assert newValue["gpc"] == value["gpc"]
@@ -29,4 +24,4 @@ def test_set_gpc_success(session, value):
 @pytest.mark.parametrize("value", [None, 1, "hello", [], {}])
 def test_set_gpc_failure(session, value):
     response = set_gpc(session, value)
-    error = assert_error(response, "invalid argument")
+    assert_error(response, "invalid argument")
