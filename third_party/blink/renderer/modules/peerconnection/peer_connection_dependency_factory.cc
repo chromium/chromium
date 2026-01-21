e959b9e888f70864e4bc7d20236efbb6b7dc58a8
François Doray <fdoray@chromium.org>
2026-01-21 12:19:46 -0800

    [ppm] Add trace events around waits in PeerConnectionDependencyFactory::CreatePeerConnectionFactory.
    The waits in this method are a known source of high input latency. Emit
    trace events around those so they can easily be identifed in traces. Use
    the "latency" category because unlike "webrtc", it's typically enabled
    in generic performance investigation traces (we are interested in these
    events in investigations unrelated to webrtc performance).
    
    Bug: 476408124
    Change-Id: Ibaec7e6e32cd8932bfc4672e21ae383e562a9588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7489830
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Tomas Gunnarsson <tommi@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572502}

--- third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.cc
+++ third_party/blink/renderer/modules/peerconnection/peer_connection_dependency_factory.cc
@@ -25,6 +25,7 @@
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
+#include "base/trace_event/trace_event.h"
 #include "base/unguessable_token.h"
 #include "build/build_config.h"
 #include "components/webrtc/thread_wrapper.h"
@@ -780,7 +781,12 @@ void PeerConnectionDependencyFactory::CreatePeerConnectionFactory() {
 
   // Wait for the worker thread, since `InitializeSignalingThread` needs to
   // refer to `worker_thread_`.
-  worker_thread_started_event.Wait();
+  {
+    TRACE_EVENT("latency",
+                "PeerConnectionDependencyFactory::CreatePeerConnectionFactory "
+                "- Wait for worker thread started");
+    worker_thread_started_event.Wait();
+  }
   CHECK(GetWorkerThread());
 
   // Only the JS main thread can establish mojo connection with a browser
@@ -803,7 +809,12 @@ void PeerConnectionDependencyFactory::CreatePeerConnectionFactory() {
           CreateMojoVideoEncoderMetricsProviderFactory(DomWindow()->GetFrame()),
           CrossThreadUnretained(&start_signaling_event)));
 
-  start_signaling_event.Wait();
+  {
+    TRACE_EVENT("latency",
+                "PeerConnectionDependencyFactory::CreatePeerConnectionFactory "
+                "- Wait for start signaling");
+    start_signaling_event.Wait();
+  }
 
   CHECK(pc_factory_);
   CHECK(socket_factory_);
