3b5c82a8aef6b41674005333c47ce6494280ddd2
Lukasz Anforowicz <lukasza@chromium.org>
2026-01-21 13:59:14 -0800

    [rust] Bump `RUST_SUB_REVISION` to refresh `cc_bindings_from_rs`.
    For some reason it seems that https://crrev.com/c/7281250 didn't
    actually add `cc_bindings_from_rs` to `third_party/rust-toolchain/bin`.
    So let's try again in this CL.
    
    Bug: 460482110
    Change-Id: I28bbd25840d0453582d3ebc4a4f0e92c2221cb60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499109
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572557}

--- tools/rust/update_rust.py
+++ tools/rust/update_rust.py
@@ -37,7 +37,7 @@ sys.path.append(
 # should not be changed manually.
 # They are also read by build/config/compiler/BUILD.gn.
 RUST_REVISION = 'd2015e2359d5d0b154c2b192d4039f9b5711fcdc'
-RUST_SUB_REVISION = 2
+RUST_SUB_REVISION = 3
 
 # The revision of Crubit to use from https://github.com/google/crubit
 #
