447f4d309c4a18d2123f16004d24e1eadd2c4fd5
Ayumi Ono <ayumiohno@google.com>
2026-01-20 16:31:17 -0800

    PA: Remove thread_local in parition_alloc_base PlatformThread::CurrentId
    This change removes the thread_local caching mechanism for thread IDs
    within partition_alloc::internal::base::PlatformThread::CurrentId.
    
    While base/threading/platform_thread_posix.cc utilizes thread_local for
    caching, doing so within PartitionAlloc's context can lead to reentrancy
    issues, because accessing thread_local storage can trigger calls to the
    underlying memory allocator. This reentrancy was observed to cause
    crashes, particularly with multiple ThreadCache support, as detailed in
    b/476192650. Here, PlatformThread::CurrentId() is called in the
    ThreadCache constructor within ThreadCache::Create.
    
    partition_alloc::internal::base::PlatformThread::CurrentId is called
    relatively infrequently compared to the general
    base::PlatformThread::CurrentId.
    - During ThreadCache::Create (a few times per thread, unless we support tens of thread caches per thread).
    - In NotifyThreadCreated/NotifyThreadDestoyed.
    - In GetStackEnd (only when dumping stack trace).
    Thus, the performance benefit provided by thread_local is outweighed by
    the risks associated with potential allocator reentrancy.
    
    Bug: 476192650
    Change-Id: Id597dcab3d98462746a80eb8f83a5372c05e424d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7488430
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Commit-Queue: Ayumi Ono <ayumiohno@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Mikihito Matsuura <mikt@google.com>
    Cr-Commit-Position: refs/heads/main@{#1571955}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/threading/platform_thread_posix.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/threading/platform_thread_posix.cc
@@ -29,51 +29,6 @@
 
 namespace partition_alloc::internal::base {
 
-#if PA_BUILDFLAG(IS_LINUX) || PA_BUILDFLAG(IS_CHROMEOS)
-
-namespace {
-
-// Store the thread ids in local storage since calling the SWI can be
-// expensive and PlatformThread::CurrentId is used liberally.
-thread_local pid_t g_thread_id = -1;
-
-// A boolean value that indicates that the value stored in |g_thread_id| on the
-// main thread is invalid, because it hasn't been updated since the process
-// forked.
-//
-// This used to work by setting |g_thread_id| to -1 in a pthread_atfork handler.
-// However, when a multithreaded process forks, it is only allowed to call
-// async-signal-safe functions until it calls an exec() syscall. However,
-// accessing TLS may allocate (see crbug.com/1275748), which is not
-// async-signal-safe and therefore causes deadlocks, corruption, and crashes.
-//
-// It's Atomic to placate TSAN.
-std::atomic<bool> g_main_thread_tid_cache_valid = false;
-
-// Tracks whether the current thread is the main thread, and therefore whether
-// |g_main_thread_tid_cache_valid| is relevant for the current thread. This is
-// also updated by PlatformThread::CurrentId().
-thread_local bool g_is_main_thread = true;
-
-class InitAtFork {
- public:
-  InitAtFork() {
-    pthread_atfork(nullptr, nullptr, internal::InvalidateTidCache);
-  }
-};
-
-}  // namespace
-
-namespace internal {
-
-void InvalidateTidCache() {
-  g_main_thread_tid_cache_valid.store(false, std::memory_order_relaxed);
-}
-
-}  // namespace internal
-
-#endif  // PA_BUILDFLAG(IS_LINUX) || PA_BUILDFLAG(IS_CHROMEOS)
-
 // static
 PlatformThreadId PlatformThread::CurrentId() {
   // Pthreads doesn't have the concept of a thread ID, so we have to reach down
@@ -81,31 +36,15 @@ PlatformThreadId PlatformThread::CurrentId() {
 #if PA_BUILDFLAG(IS_APPLE)
   return pthread_mach_thread_np(pthread_self());
 #elif PA_BUILDFLAG(IS_LINUX) || PA_BUILDFLAG(IS_CHROMEOS)
-  static InitAtFork init_at_fork;
-  if (g_thread_id == -1 ||
-      (g_is_main_thread &&
-       !g_main_thread_tid_cache_valid.load(std::memory_order_relaxed))) {
-    // Update the cached tid.
-    g_thread_id = syscall(__NR_gettid);
-    // If this is the main thread, we can mark the tid_cache as valid.
-    // Otherwise, stop the current thread from always entering this slow path.
-    if (g_thread_id == getpid()) {
-      g_main_thread_tid_cache_valid.store(true, std::memory_order_relaxed);
-    } else {
-      g_is_main_thread = false;
-    }
-  } else {
-#if PA_BUILDFLAG(DCHECKS_ARE_ON)
-    if (g_thread_id != syscall(__NR_gettid)) {
-      PA_RAW_LOG(
-          FATAL,
-          "Thread id stored in TLS is different from thread id returned by "
-          "the system. It is likely that the process was forked without going "
-          "through fork().");
-    }
-#endif
-  }
-  return g_thread_id;
+  // Unlike base/threading/platform_thread_posix.cc, we avoid using thread_local
+  // for thread_id caching. This prevents reentrancy issues within
+  // PartitionAlloc, as reported in crbug.com/476192650. Since
+  // partition_alloc::internal::base::PlatformThread::CurrentId is called
+  // infrequently (typically just a few times per thread), the performance
+  // overhead of not using thread_local is acceptable and outweighed by avoiding
+  // the bug. If necessary we could cache it but we'd have to ensure it was
+  // while we were inside PartitionAlloc itself for allocation already.
+  return syscall(__NR_gettid);
 #elif PA_BUILDFLAG(IS_ANDROID)
   // Note: do not cache the return value inside a thread_local variable on
   // Android (as above). The reasons are:
