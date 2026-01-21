b6614de39905b25380e6f88cd34eb16a14f6a9d8
Radu Nitescu <radunitescu@google.com>
2026-01-21 05:21:43 -0800

    Reland "[iOS][AIM] Add flag for additional tools in the input plate"
    This is a reland of commit 99fcdf118ce392bbf1053d4784286e73e701d732
    
    Original change's description:
    > [iOS][AIM] Add flag for additional tools in the input plate
    >
    > Change-Id: If9a53a782ad75a2c45af473ce24e5cd791a8a58e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7497675
    > Reviewed-by: Louis Romero <lpromero@google.com>
    > Auto-Submit: Radu Nitescu <radunitescu@google.com>
    > Commit-Queue: Radu Nitescu <radunitescu@google.com>
    > Commit-Queue: Louis Romero <lpromero@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1571631}
    
    Change-Id: I0a3f3eaf5e52a787ea897c35d8b77acf8ef9a301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7502756
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572242}

--- ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
+++ ios/chrome/browser/flags/ios_chrome_flag_descriptions.h
@@ -184,6 +184,9 @@ extern const char kComposeboxAIMDisabledDescription[];
 extern const char kComposeboxAIMNudgeName[];
 extern const char kComposeboxAIMNudgeDescription[];
 
+extern const char kComposeboxAdditionalAdvancedToolsName[];
+extern const char kComposeboxAdditionalAdvancedToolsDescription[];
+
 extern const char kComposeboxAttachmentsTypedStateName[];
 extern const char kComposeboxAttachmentsTypedStateDescription[];
 
