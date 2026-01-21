276d8b20ae43938af5cf5fc3ac4824cf63e6190b
Zoraiz Naeem <zoraiznaeem@chromium.org>
2026-01-21 14:57:05 -0800

    Add skeleton implementation for ImagedTrayIcon
    ImagedTrayIcon is a simple shelf pod that hosts an image as its content.
    
    Bug: b:463431615
    Change-Id: I605521198882534de80026f4ffbd7cbba2a76f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499344
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572599}

---
+++ ash/system/tray/imaged_tray_icon.h
@@ -0,0 +1,52 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_TRAY_IMAGED_TRAY_ICON_H_
+#define ASH_SYSTEM_TRAY_IMAGED_TRAY_ICON_H_
+
+#include "ash/ash_export.h"
+#include "ash/system/tray/tray_background_view.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+
+namespace ui {
+class LocatedEvent;
+}  // namespace ui
+
+namespace views {
+class ImageView;
+}  // namespace views
+
+namespace ash {
+
+class Shelf;
+class TrayBubbleView;
+
+class ASH_EXPORT ImagedTrayIcon : public TrayBackgroundView {
+  METADATA_HEADER(ImagedTrayIcon, TrayBackgroundView)
+
+ public:
+  ImagedTrayIcon(Shelf* shelf,
+                 const TrayBackgroundViewCatalogName catalog_name);
+
+  ImagedTrayIcon(const ImagedTrayIcon&) = delete;
+  ImagedTrayIcon& operator=(const ImagedTrayIcon&) = delete;
+
+  ~ImagedTrayIcon() override;
+
+  // TrayBackgroundView:
+  void ClickedOutsideBubble(const ui::LocatedEvent& event) override;
+  void UpdateTrayItemColor(bool is_active) override;
+  void HandleLocaleChange() override;
+  void HideBubbleWithView(const TrayBubbleView* bubble_view) override;
+  void HideBubble(const TrayBubbleView* bubble_view) override;
+
+  views::ImageView* image_view() { return image_view_; }
+
+ private:
+  raw_ptr<views::ImageView> image_view_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_TRAY_IMAGED_TRAY_ICON_H_
