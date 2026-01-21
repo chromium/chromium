adc96b6be1af28c8a904e441bf3534bee18ef119
Jeffrey Yu <yuje@chromium.org>
2026-01-21 14:47:38 -0800

    Avoid duplicate map lookups.
    Instead of using `.contains(key)` followed by a lookup, use
    `.find(key)`, and reuse the iterator to access the value.
    
    Bug: 452950815
    Change-Id: I34347d6a4fcb4c2cb3b6ee0d9877f53e80f8b93e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7457857
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572585}

--- extensions/browser/api/bluetooth/bluetooth_event_router.cc
+++ extensions/browser/api/bluetooth/bluetooth_event_router.cc
@@ -223,9 +223,11 @@ void BluetoothEventRouter::SetDiscoveryFilter(
 
 BluetoothApiPairingDelegate* BluetoothEventRouter::GetPairingDelegate(
     const ExtensionId& extension_id) {
-  return pairing_delegate_map_.contains(extension_id)
-             ? pairing_delegate_map_[extension_id]
-             : nullptr;
+  if (auto it = pairing_delegate_map_.find(extension_id);
+      it != pairing_delegate_map_.end()) {
+    return it->second;
+  }
+  return nullptr;
 }
 
 void BluetoothEventRouter::OnAdapterInitialized(
@@ -266,28 +268,29 @@ void BluetoothEventRouter::AddPairingDelegateImpl(
     return;
   }
 
-  if (pairing_delegate_map_.contains(extension_id)) {
+  auto [it, inserted] =
+      pairing_delegate_map_.try_emplace(extension_id, nullptr);
+  if (!inserted) {
     // For WebUI there may be more than one page open to the same url
     // (e.g. chrome://settings). These will share the same pairing delegate.
     BLUETOOTH_LOG(EVENT) << "Pairing delegate already exists for extension_id: "
                          << extension_id;
     return;
   }
-  BluetoothApiPairingDelegate* delegate =
-      new BluetoothApiPairingDelegate(browser_context_);
+  it->second = new BluetoothApiPairingDelegate(browser_context_);
   DCHECK(adapter_.get());
   adapter_->AddPairingDelegate(
-      delegate, device::BluetoothAdapter::PAIRING_DELEGATE_PRIORITY_HIGH);
-  pairing_delegate_map_[extension_id] = delegate;
+      it->second, device::BluetoothAdapter::PAIRING_DELEGATE_PRIORITY_HIGH);
 }
 
 void BluetoothEventRouter::RemovePairingDelegate(
     const ExtensionId& extension_id) {
-  if (pairing_delegate_map_.contains(extension_id)) {
-    BluetoothApiPairingDelegate* delegate = pairing_delegate_map_[extension_id];
+  if (auto it = pairing_delegate_map_.find(extension_id);
+      it != pairing_delegate_map_.end()) {
+    BluetoothApiPairingDelegate* delegate = it->second;
     if (adapter_.get())
       adapter_->RemovePairingDelegate(delegate);
-    pairing_delegate_map_.erase(extension_id);
+    pairing_delegate_map_.erase(it);
     delete delegate;
     MaybeReleaseAdapter();
   }
