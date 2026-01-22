d6634568e29c337af21a501bdd1f6495553fb267
Anton Goncharenko <agonch@google.com>
2026-01-22 03:35:15 -0800

    Reland "[Signin][Android] NTP Seamless Signin Full Flow with Snackbar Test"
    This is a reland of commit 7365e82578198d3725cb6fa8877fe0f42767ec63.
    Reason for revert: crbug.com/477321646
    
    I am able to reproduce the error in landscape mode. Adding scrollTo() to
    address button visibility in landscape mode.
    
    Original change's description:
    > [Signin][Android] NTP Seamless Signin Full Flow with Snackbar Test
    >
    > This verifies the "full happy path" of the seamless signin flow,
    > starting from the new tab entry point.
    >
    > Edge cases for signin flow should be added to
    > BottomSheetSigninAndHistorySyncIntegrationTest.
    >
    > Bug: 437039927
    > Change-Id: I384ee3180d430753a2e09e52dcd1d697cea2e67e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7486768
    > Commit-Queue: Anton Goncharenko <agonch@google.com>
    > Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    > Reviewed-by: Lucia Giorgi <quacksort@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1571708}
    
    Bug: 437039927, 477321646
    Change-Id: I4c2957d242d09610a7bbc642d244959005086f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7502598
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Anton Goncharenko <agonch@google.com>
    Cr-Commit-Position: refs/heads/main@{#1572863}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageSigninPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageSigninPromoTest.java
@@ -5,25 +5,40 @@
 package org.chromium.chrome.browser.ntp;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import android.widget.TextView;
+
 import androidx.test.espresso.matcher.ViewMatchers.Visibility;
 import androidx.test.filters.MediumTest;
 
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.Criteria;
+import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.ui.messages.snackbar.Snackbar;
+import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.transit.ChromeTransitTestRules;
@@ -31,6 +46,7 @@ import org.chromium.chrome.test.transit.FreshCtaTransitTestRule;
 import org.chromium.chrome.test.util.NewTabPageTestUtils;
 import org.chromium.chrome.test.util.browser.signin.SigninTestRule;
 import org.chromium.components.signin.SigninFeatures;
+import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.signin.test.util.TestAccounts;
 
 /**
@@ -115,5 +131,87 @@ public class NewTabPageSigninPromoTest {
         openNewTabPage();
 
         onView(withId(R.id.signin_promo_view_container)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"FeedNewTabPage"})
+    @EnableFeatures({
+        "EnableSeamlessSignin"
+                + ":seamless-signin-promo-type/twoButtons"
+                + "/seamless-signin-string-type/signinButton"
+    })
+    public void testSeamlessSigninFlow_WithFinalSnackbarUndoSignin() {
+        mSigninTestRule.addAccount(TestAccounts.AADC_ADULT_ACCOUNT);
+        openNewTabPage();
+        // Check that the sign-in promo is displayed for a signed-out user.
+        onView(withId(R.id.signin_promo_view_container)).check(matches(isDisplayed()));
+        assertNull(mSigninTestRule.getPrimaryAccount(ConsentLevel.SIGNIN));
+        // Initiate seamless sign-in flow by clicking primary CTA on sign-in promo. For landscape
+        // mode tests, scroll to ensure button is visible.
+        onView(withId(R.id.signin_promo_primary_button)).perform(scrollTo(), click());
+
+        onView(withId(R.id.signin_promo_view_container))
+                .check(matches(withEffectiveVisibility(Visibility.GONE)));
+        // Once the sign-in promo disappears, the sign-in flow is complete.
+        assertNotNull(mSigninTestRule.getPrimaryAccount(ConsentLevel.SIGNIN));
+
+        // Click "Undo" button in the snackbar. This should sign the user out and permanently
+        // dismiss the promo.
+        verifySigninSnackbarShown();
+        clickSigninSnackbarActionButton();
+        verifySignoutSnackbarShown();
+
+        assertNull(mSigninTestRule.getPrimaryAccount(ConsentLevel.SIGNIN));
+        onView(withId(R.id.signin_promo_view_container))
+                .check(matches(withEffectiveVisibility(Visibility.GONE)));
+    }
+
+    private void verifySigninSnackbarShown() {
+        ChromeTabbedActivity activity = mActivityTestRule.getActivity();
+        CriteriaHelper.pollUiThread(
+                () -> {
+                    String email = TestAccounts.AADC_ADULT_ACCOUNT.getEmail();
+                    String expectedSnackbarSigninMessage =
+                            activity.getString(R.string.snackbar_signed_in_as, email);
+                    SnackbarManager snackbarManager = activity.getSnackbarManager();
+                    Criteria.checkThat(snackbarManager.isShowing(), Matchers.is(true));
+                    TextView snackbarMessage = activity.findViewById(R.id.snackbar_message);
+                    Snackbar snackbar = snackbarManager.getCurrentSnackbarForTesting();
+
+                    Criteria.checkThat(
+                            snackbar.getIdentifierForTesting(), Matchers.is(Snackbar.UMA_SIGN_IN));
+                    Criteria.checkThat(snackbarMessage, Matchers.notNullValue());
+                    Criteria.checkThat(
+                            snackbarMessage.getText().toString(),
+                            Matchers.is(expectedSnackbarSigninMessage));
+                    Criteria.checkThat(snackbar.getController(), Matchers.notNullValue());
+                });
+    }
+
+    private void clickSigninSnackbarActionButton() {
+        ChromeTabbedActivity activity = mActivityTestRule.getActivity();
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    Snackbar snackbar =
+                            activity.getSnackbarManager().getCurrentSnackbarForTesting();
+                    assertEquals(
+                            "Expecting to click on the sign-in snackbar",
+                            Snackbar.UMA_SIGN_IN,
+                            snackbar.getIdentifierForTesting());
+                    snackbar.getController().onAction(snackbar.getActionData());
+                });
+    }
+
+    private void verifySignoutSnackbarShown() {
+        ChromeTabbedActivity activity = mActivityTestRule.getActivity();
+        CriteriaHelper.pollUiThread(
+                () -> {
+                    SnackbarManager snackbarManager = activity.getSnackbarManager();
+                    Criteria.checkThat(snackbarManager.isShowing(), Matchers.is(true));
+                    Snackbar snackbar = snackbarManager.getCurrentSnackbarForTesting();
+                    Criteria.checkThat(
+                            snackbar.getIdentifierForTesting(), Matchers.is(Snackbar.UMA_SIGN_OUT));
+                });
     }
 }
