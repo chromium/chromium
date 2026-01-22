9a46fee12af02305813c472b8d6fcaf531dd0596
Tomasz Lichoń <lichon@google.com>
2026-01-22 03:27:37 -0800

    [TDR] Remove TODOs linked to issue 304553492
    This CL was generated by an AI agent and verified by the CL owner before
    sending for review. The TODOs were identified as stale by TODO Removal
    Agent (http://go/dead-todo-removal-agent).
    * Detailed reasoning can be found in the CL comments.
    * How to review this CL: http://go/tdr-review-instructions
    * To opt-out from future reviews: http://go/tdr-opt-out.
    
    The associated bug b/304553492 is marked as 'FIXED'. The changelist that
    fixes the bug (https://crrev.com/c/6086380) adds new files
    command_metrics.h and command_metrics.cc, which implement the per-
    command metrics requested in the TODO comments.
    
    Change-Id: Ia0d5b387d0549b8355f4668e2ca8afba0ebb7771
    Bug: 304553492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7484755
    Reviewed-by: Erik Staab <estaab@chromium.org>
    Commit-Queue: Tomasz Lichoń <lichon@google.com>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572860}

--- chrome/browser/web_applications/commands/command_result.h
+++ chrome/browser/web_applications/commands/command_result.h
@@ -5,7 +5,7 @@
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_COMMANDS_COMMAND_RESULT_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_COMMANDS_COMMAND_RESULT_H_
 
-// TODO(b/304553492): Use this for recording per-command metrics.
+// TODO(https://crbug.com/384519679): Use this for recording per-command metrics.
 enum class CommandResult {
   // The command completed without any unexpected errors.
   kSuccess,
@@ -13,7 +13,7 @@ enum class CommandResult {
   kFailure,
 };
 
-// TODO(dmurph): Put utilities for recording command metrics into this file.
-// b/304553492
+// TODO(https://crbug.com/384519679): Put utilities for recording command metrics
+// into this file.
 
 #endif  // CHROME_BROWSER_WEB_APPLICATIONS_COMMANDS_COMMAND_RESULT_H_
