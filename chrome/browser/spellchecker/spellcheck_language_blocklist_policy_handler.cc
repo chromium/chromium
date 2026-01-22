ce6814c3c0f27031089fe24dd2e9041520f8045a
Jeffrey Yu <yuje@chromium.org>
2026-01-21 16:31:32 -0800

    Replace `std::unordered_set` with `absl::flat_hash_set`.
    The Chromium containers guide
    (https://chromium.googlesource.com/chromium/src/+/HEAD/base/containers/README.md)
    recommends using `absl::flat_hash_set` as a default map choice instead
    of `std::unordered_set` when pointer stability isn't needed, and
    `absl::node_hash_set` when it is. Both will still generally perform
    better and require less overhead than `std::unordered_set`.
    
    Bug: 473916362
    Change-Id: Ia35b778bf1609bccdb77a404d51aa1f2c9b455bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7504546
    Reviewed-by: Mehrab N <mehrab@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1572643}

--- chrome/browser/spellchecker/spellcheck_language_blocklist_policy_handler.cc
+++ chrome/browser/spellchecker/spellcheck_language_blocklist_policy_handler.cc
@@ -4,7 +4,6 @@
 
 #include "chrome/browser/spellchecker/spellcheck_language_blocklist_policy_handler.h"
 
-#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -20,6 +19,7 @@
 #include "components/spellcheck/browser/pref_names.h"
 #include "components/spellcheck/common/spellcheck_features.h"
 #include "components/strings/grit/components_strings.h"
+#include "third_party/abseil-cpp/absl/container/flat_hash_set.h"
 
 SpellcheckLanguageBlocklistPolicyHandler::
     SpellcheckLanguageBlocklistPolicyHandler(const char* policy_name)
@@ -106,7 +106,7 @@ void SpellcheckLanguageBlocklistPolicyHandler::SortBlocklistedLanguages(
   // Build a lookup of force-enabled spellcheck languages to find duplicates.
   const base::Value* forced_enabled_value = policies.GetValue(
       policy::key::kSpellcheckLanguage, base::Value::Type::LIST);
-  std::unordered_set<std::string> forced_languages_lookup;
+  absl::flat_hash_set<std::string> forced_languages_lookup;
   if (forced_enabled_value) {
     for (const auto& forced_language : forced_enabled_value->GetList())
       forced_languages_lookup.insert(forced_language.GetString());
@@ -123,8 +123,7 @@ void SpellcheckLanguageBlocklistPolicyHandler::SortBlocklistedLanguages(
     if (current_language.empty()) {
       unknown->emplace_back(language.GetString());
     } else {
-      if (forced_languages_lookup.find(language.GetString()) !=
-          forced_languages_lookup.end()) {
+      if (forced_languages_lookup.contains(language.GetString())) {
         // If a language is both force-enabled and force-disabled, force-enable
         // wins. Put the language in the list of duplicates.
         duplicates->emplace_back(std::move(current_language));
