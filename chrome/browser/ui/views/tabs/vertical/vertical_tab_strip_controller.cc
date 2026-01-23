cc1078d51f90719563a8eb54be506cf3bbc7e4d3
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-01-23 13:43:33 -0800

    Revert "Reland "[Vertical tabs] Add save tab groups related icons to group header.""
    This reverts commit 69dad14515c7bd5163ba1fd1ca6ae2418f4e8097.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8691849918734665121
    
    Sample failed build: https://ci.chromium.org/b/8691849918734665121
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7512187&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8691849918734665121&type=BUG
    
    Original change's description:
    > Reland "[Vertical tabs] Add save tab groups related icons to group header."
    >
    > Added necessary feature flag to tests which should fix revert reason.
    >
    > This is a reland of commit a5de14ef1c7c109aa1e08106fde918e9ef3a7e04
    >
    > Original change's description:
    > > [Vertical tabs] Add save tab groups related icons to group header.
    > >
    > > Bug: 465164420
    > > Change-Id: I68ddfdeb890067f87e5f995e5304dc0b11e23b58
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7499153
    > > Reviewed-by: David Pennington <dpenning@chromium.org>
    > > Commit-Queue: Caroline Rising <corising@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1572350}
    >
    > Bug: 465164420
    > Change-Id: I1aa89da1fadd32f3c1c20b3234f7ed4616ac4436
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7512187
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Commit-Queue: Caroline Rising <corising@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1573900}
    >
    
    Bug: 465164420
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I5516746c7b349a336ebd84b346b9ae454b487759
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7514625
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1573929}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller.cc
@@ -8,7 +8,6 @@
 
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
-#include "chrome/browser/tab_group_sync/tab_group_sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/tabs/tab_menu_model_factory.h"
@@ -222,12 +221,6 @@ bool VerticalTabStripController::IsCollapsed() const {
   const tabs::VerticalTabStripStateController* state_controller =
       tabs::VerticalTabStripStateController::From(browser_view_->browser());
   return state_controller && state_controller->IsCollapsed();
-}
-
-tab_groups::TabGroupSyncService*
-VerticalTabStripController::GetTabGroupSyncService() {
-  return tab_groups::TabGroupSyncServiceFactory::GetForProfile(
-      browser_view_->GetProfile());
 }
 
 bool VerticalTabStripController::IsContextMenuCommandChecked(
