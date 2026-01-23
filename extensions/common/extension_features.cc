05a42da0d1ad398b28281d16de075c58726dec41
Julia Przybytniowska <przybytniowska@google.com>
2026-01-23 00:19:37 -0800

    Switch CWS Banner flag to enabled
    In following review I will remove the flag
    
    Launch is Approved - https://launch.corp.google.com/launch/4416686
    
    Change-Id: I87260db5483bebfe7bb28e1db7714b2c33215ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511013
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Austin Zhang <austinzzr@google.com>
    Commit-Queue: Julia Przybytniowska <przybytniowska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573540}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -183,7 +183,7 @@ BASE_FEATURE(kContentVerifyJobUseJobVersionForHashing,
 BASE_FEATURE(kRuntimeOnMessageWebExtensionPolyfillSupport,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kEnableShouldShowPromotion, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableShouldShowPromotion, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSearchEngineExplicitChoiceDialog,
              base::FEATURE_DISABLED_BY_DEFAULT);
