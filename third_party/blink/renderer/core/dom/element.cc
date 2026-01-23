5828a4525a60cf8dcebdf29c0a76162cd9043c80
Morten Stenshorne <mstensho@chromium.org>
2026-01-23 02:18:44 -0800

    Less special-code for ::backdrop creation.
    We cannot decide whether or not a ::backdrop pseudo-element is needed
    when in Element::ApplyPendingBackdropPseudoElementUpdate(), because we
    first need computed style.
    
    For instance, if we have ::backdrop { display:none; }, no pseudo-element
    should be created. That function would create one unconditionally, mark
    the pseudo-element for style recalc, leave its computed style as nullptr
    until style recalc. During style calc, computed style would normally be
    set, but not if e.g. it has display:none - in which case a ::backdrop
    element is left with no computed style indefinitely, which is unexpected
    behavior. For one, it might crash in
    PseudoElementStylesDependOnFontMetrics().
    
    Just mark the originating element for style recalc instead, and leave
    the rest to normal style recalc, so that any pseudo-elements that
    shouldn't exist won't be inserted at all.
    
    Additionally add a DCHECK for this situation in style recalc, which
    would fail without the actual code changes in this fix. As would four
    existing browser_tests:
    
    NewTabPageAppTest.ActionChips
      NewTabPageAppTest.Composebox
      NewTabPageAppTest.RealboxNext
      NewTabPageAppTest.ThreadsRail
    
    (cherry picked from commit baa6ddbb7a249a7ed9add6990cd7d99fba47aede)
    
    Bug: 470512590
    Fixed: 477775115
    Change-Id: I4f9527048c1c6073381b568db1931b1d0a3b986a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7497751
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1572196}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511414
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7632@{#823}
    Cr-Branched-From: 0bbdf2913883391365383b0a5dfe7bf9fd1a5213-refs/heads/main@{#1568190}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -9937,23 +9937,13 @@ void Element::UpdateBackdropPseudoElement(
 }
 
 void Element::ApplyPendingBackdropPseudoElementUpdate() {
-  PseudoElement* element = GetPseudoElement(PseudoId::kPseudoIdBackdrop,
-                                            /* pseudo_argument */ g_null_atom);
-
-  if (!element && CanGeneratePseudoElement(PseudoId::kPseudoIdBackdrop)) {
-    element = PseudoElement::Create(this, PseudoId::kPseudoIdBackdrop,
-                                    /* pseudo_argument */ g_null_atom);
-    EnsureElementRareData().SetPseudoElement(PseudoId::kPseudoIdBackdrop,
-                                             element,
-                                             /* pseudo_argument */ g_null_atom);
-    element->InsertedInto(*this);
-    GetDocument().AddToTopLayer(element, this);
-  }
-
-  DCHECK(element);
-  element->SetNeedsStyleRecalc(kLocalStyleChange,
-                               StyleChangeReasonForTracing::Create(
-                                   style_change_reason::kConditionalBackdrop));
+  // Mark for style recalc, in order to trigger creation of a ::backdrop pseudo-
+  // element if needed. There's no way of telling if there'll be any need for it
+  // at this point, since we need computed style first (e.g. if it's
+  // `display:none`, there should be no pseudo-element generated).
+  SetNeedsStyleRecalc(kLocalStyleChange,
+                      StyleChangeReasonForTracing::Create(
+                          style_change_reason::kConditionalBackdrop));
 }
 
 void Element::UpdateFirstLetterPseudoElement(StyleUpdatePhase phase) {
@@ -10146,6 +10136,9 @@ PseudoElement* Element::UpdatePseudoElement(
       ClearPseudoElement(pseudo_id, pseudo_argument);
       element = nullptr;
     }
+
+    // A pseudo-element without computed style should not exist.
+    DCHECK(!element || element->GetComputedStyle());
   }
 
   return element;
