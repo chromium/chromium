5f696328185c84f9bc9eb1608d00270198bc087d
Anna Tsvirchkova <atsvirchkova@google.com>
2026-01-23 07:53:29 -0800

    [Gardener] Disable flaky test on android-desktop-16-x64-rel-brya-tests bot
    UndoBarControllerTest#testDeleteTabGroups_Undo is not flaky on other bots.
    
    Bug: 478176353
    Change-Id: I52188bb3cb47709f45bdc7835bd0173cfa485751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511036
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Auto-Submit: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1573697}

--- chrome/android/javatests/src/org/chromium/chrome/browser/undo_tab_close_snackbar/UndoBarControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/undo_tab_close_snackbar/UndoBarControllerTest.java
@@ -21,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -414,6 +415,7 @@ public class UndoBarControllerTest {
 
     @Test
     @SmallTest
+    @DisableIf.Device(DeviceFormFactor.DESKTOP)
     public void testDeleteTabGroups_Undo() throws Exception {
         ChromeTabUtils.newTabFromMenu(
                 InstrumentationRegistry.getInstrumentation(), mActivityTestRule.getActivity());
