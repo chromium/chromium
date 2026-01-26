ade99101968e1bbba931ac4bbbb7a2eaa1c64cd5
Henrique Nakashima <hnakashima@chromium.org>
2026-01-26 14:23:17 -0800

    [Android] Migrate Android Desktop .filters to @DisableIf (part 16/20)
    Migrated:
    - PaymentRequestShowPromiseSingleOptionShippingTest
      - *
    - PaymentRequestShowPromiseSingleOptionShippingWithUpdateTest
      - *
    - PaymentRequestShowPromiseUSOnlyShippingTest
      - *
    - PaymentRequestShowPromiseUnsupportedTest
      - *
    - PaymentRequestTabTest
      - *
    
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Validate-Test-Flakiness: skip
    Bug: 459810720, 376100658
    Change-Id: I6346d44102ef6a146a1471cf8b85ac3fb64949c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7515164
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574783}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestShowPromiseSingleOptionShippingWithUpdateTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestShowPromiseSingleOptionShippingWithUpdateTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.autofill.AutofillTestHelper;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -22,6 +23,7 @@ import org.chromium.chrome.browser.payments.PaymentRequestTestRule.FactorySpeed;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
 import org.chromium.components.autofill.AutofillProfile;
+import org.chromium.ui.base.DeviceFormFactor;
 
 import java.util.concurrent.TimeoutException;
 
@@ -31,6 +33,7 @@ import java.util.concurrent.TimeoutException;
  */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/376100658
 public class PaymentRequestShowPromiseSingleOptionShippingWithUpdateTest {
     @Rule
     public PaymentRequestTestRule mRule =
