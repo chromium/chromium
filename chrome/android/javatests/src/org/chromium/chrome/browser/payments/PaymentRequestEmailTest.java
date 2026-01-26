2df715d925966801ba2926003bbb4338a82eb264
Henrique Nakashima <hnakashima@chromium.org>
2026-01-26 14:27:33 -0800

    [Android] Migrate Android Desktop .filters to @DisableIf (part 11/20)
    Migrated:
    - PaymentRequestCanMakePaymentQueryTest
      - *
    - PaymentRequestContactDetailsTest
      - *
    - PaymentRequestDataUrlTest
      - *
    - PaymentRequestEmailAndPhoneTest
      - *
    - PaymentRequestEmailTest
      - *
    
    Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk
    Validate-Test-Flakiness: skip
    Bug: 459810720, 376100658
    Change-Id: I3497e65379bd1df78b5d2429cf4064f7e4c7b06c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7511459
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1574790}

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestEmailTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestEmailTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.autofill.AutofillTestHelper;
@@ -24,12 +25,14 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
 import org.chromium.components.autofill.AutofillProfile;
 import org.chromium.components.payments.Event2;
+import org.chromium.ui.base.DeviceFormFactor;
 
 import java.util.concurrent.TimeoutException;
 
 /** A payment integration test for a merchant that requests email address. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+@DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/376100658
 public class PaymentRequestEmailTest {
     @Rule
     public PaymentRequestTestRule mPaymentRequestTestRule =
