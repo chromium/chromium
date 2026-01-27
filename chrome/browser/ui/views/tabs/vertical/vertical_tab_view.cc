169dde38ec561a17ea65b398d38355d0060d0288
Alison Gale <agale@chromium.org>
2026-01-27 11:23:44 -0800

    [Vertical Tabs] Fix crash when closing tab alert
    Change-Id: I74d3b0c44055e493574b15bfdc609771850df2d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7521989
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575402}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
@@ -640,9 +640,9 @@ void VerticalTabView::CloseButtonPressed(const ui::Event& event) {
   CHECK(alert_indicator_);
   if (!alert_indicator_->GetVisible()) {
     base::RecordAction(base::UserMetricsAction("CloseTab_NoAlertIndicator"));
-  } else {
-    std::optional<tabs::TabAlert> alert_state =
-        tabs::TabAlertController::GetAlertStateToShow(tab_data_.alert_state);
+  } else if (auto alert_state = tabs::TabAlertController::GetAlertStateToShow(
+                 tab_data_.alert_state);
+             alert_state.has_value()) {
     if (alert_state.value() == tabs::TabAlert::kAudioPlaying) {
       base::RecordAction(base::UserMetricsAction("CloseTab_AudioIndicator"));
     } else if (alert_state.value() == tabs::TabAlert::kAudioRecording ||
