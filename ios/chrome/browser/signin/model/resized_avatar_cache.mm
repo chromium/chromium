317870291a10b0c4c3d220ca6919ae6eca604717
Jérôme Lebel <jlebel@chromium.org>
2026-01-27 07:08:53 -0800

    [iSO] Use gaia id as a key to store images in ResizedAvatarCache
    With this patch updates ResizedAvatarCache to store cached avatar
    based on gaia id instead of SystemIdentity.
    The point is to reduce the impact of SystemIdentity in Chrome code.
    
    Now ResizedAvatarCache listen to SystemIdentityManager to clear
    its cache when an identity is removed or when an identity is updated.
    
    Bug: 476947704
    Change-Id: Iee22a63b0642fba1abe6898bdafbcd193e462b6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7493478
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575213}

--- ios/chrome/browser/signin/model/resized_avatar_cache.mm
+++ ios/chrome/browser/signin/model/resized_avatar_cache.mm
@@ -8,10 +8,11 @@
 #import "ios/chrome/browser/signin/model/signin_util.h"
 #import "ios/chrome/browser/signin/model/system_identity.h"
 #import "ios/chrome/browser/signin/model/system_identity_manager.h"
+#import "ios/chrome/browser/signin/model/system_identity_manager_observer_bridge.h"
 #import "ios/chrome/common/ui/util/image_util.h"
 #import "ios/public/provider/chrome/browser/signin/signin_resources_api.h"
 
-@interface ResizedAvatarCache ()
+@interface ResizedAvatarCache () <SystemIdentityManagerObserving>
 
 // Default avatar at `_expectedSize` size.
 @property(nonatomic, strong) UIImage* defaultResizedAvatar;
@@ -22,54 +23,60 @@
   // Size of resized avatar.
   CGSize _expectedSize;
   // Retains resized images. Key is Chrome Identity.
-  NSCache<id<SystemIdentity>, UIImage*>* _resizedImages;
-  // Holds weak references to the cached avatar image from the
-  // SystemIdentityManager. Key is Chrome Identity.
-  NSMapTable<id<SystemIdentity>, UIImage*>* _originalImages;
+  NSCache<NSString*, UIImage*>* _avatarForGaiaID;
+  // List of gaia ids, in `_avatarForGaiaID`. NSCache cannot return keys in the
+  // cache. Since NSCache can remove values, it is possible to a gaia id in
+  // `_knownGaiaIDs` but not in `_avatarForGaiaID`.
+  NSMutableSet<NSString*>* _knownGaiaIDs;
+  std::unique_ptr<SystemIdentityManagerObserverBridge>
+      _systemIdentityManagerObserverBridge;
 }
 
 - (instancetype)initWithIdentityAvatarSize:(IdentityAvatarSize)avatarSize {
   self = [super init];
   if (self) {
     _expectedSize = GetSizeForIdentityAvatarSize(avatarSize);
-    _resizedImages = [[NSCache alloc] init];
-    _originalImages = [NSMapTable strongToWeakObjectsMapTable];
+    _avatarForGaiaID = [[NSCache alloc] init];
+    _knownGaiaIDs = [NSMutableSet set];
     [[NSNotificationCenter defaultCenter]
         addObserver:self
            selector:@selector(notificicationReceived:)
                name:UIApplicationDidReceiveMemoryWarningNotification
              object:nil];
+    SystemIdentityManager* systemIdentityManager =
+        GetApplicationContext()->GetSystemIdentityManager();
+    _systemIdentityManagerObserverBridge =
+        std::make_unique<SystemIdentityManagerObserverBridge>(
+            systemIdentityManager, self);
   }
   return self;
 }
 
 - (UIImage*)resizedAvatarForIdentity:(id<SystemIdentity>)identity {
   CHECK(identity, base::NotFatalUntil::M150);
+
+  NSString* gaiaIDString = identity.gaiaId.ToNSString();
+  UIImage* avatar = [_avatarForGaiaID objectForKey:gaiaIDString];
+  if (avatar) {
+    return avatar;
+  }
+
   SystemIdentityManager* system_identity_manager =
       GetApplicationContext()->GetSystemIdentityManager();
-
-  UIImage* image =
-      system_identity_manager->GetCachedAvatarForIdentity(identity);
-  if (!image) {
+  avatar = system_identity_manager->GetCachedAvatarForIdentity(identity);
+  if (!avatar) {
     // No cached image, trigger a fetch, which will notify all observers.
     system_identity_manager->FetchAvatarForIdentity(identity);
     return self.defaultResizedAvatar;
   }
 
-  // If the currently used image has already been resized, use it.
-  if ([_resizedImages objectForKey:identity] &&
-      [_originalImages objectForKey:identity] == image) {
-    return [_resizedImages objectForKey:identity];
-  }
-
-  [_originalImages setObject:image forKey:identity];
-
   // Resize the profile image if it is not of the expected size.
-  if (!CGSizeEqualToSize(image.size, _expectedSize)) {
-    image = ResizeImage(image, _expectedSize, ProjectionMode::kAspectFit);
+  if (!CGSizeEqualToSize(avatar.size, _expectedSize)) {
+    avatar = ResizeImage(avatar, _expectedSize, ProjectionMode::kAspectFit);
   }
-  [_resizedImages setObject:image forKey:identity];
-  return image;
+  [_avatarForGaiaID setObject:avatar forKey:gaiaIDString];
+  [_knownGaiaIDs addObject:gaiaIDString];
+  return avatar;
 }
 
 - (void)notificicationReceived:(NSNotification*)notification {
@@ -88,6 +95,35 @@
         ResizeImage(image, _expectedSize, ProjectionMode::kAspectFit);
   }
   return _defaultResizedAvatar;
+}
+
+#pragma mark - SystemIdentityManagerObserving
+
+- (void)onIdentityListChanged {
+  NSMutableSet<NSString*>* gaiaIDsToRemove = [_knownGaiaIDs mutableCopy];
+  SystemIdentityManager* system_identity_manager =
+      GetApplicationContext()->GetSystemIdentityManager();
+  SystemIdentityManager::IdentityIteratorCallback callback =
+      base::BindRepeating(
+          [](NSMutableSet* gaiaIDsToRemove, id<SystemIdentity> identity) {
+            [gaiaIDsToRemove removeObject:identity.gaiaId.ToNSString()];
+            return SystemIdentityManager::IteratorResult::kContinueIteration;
+          },
+          gaiaIDsToRemove);
+  system_identity_manager->IterateOverIdentities(callback);
+  // Clear gaia ids that don't exist anymore.
+  for (NSString* gaiaIDString in gaiaIDsToRemove) {
+    [_avatarForGaiaID removeObjectForKey:gaiaIDString];
+    [_knownGaiaIDs removeObject:gaiaIDString];
+  }
+}
+
+- (void)onIdentityUpdated:(id<SystemIdentity>)identity {
+  // Forget about the current cached avatar. It will be recomputed automatically
+  // on the next request.
+  NSString* gaiaIDString = identity.gaiaId.ToNSString();
+  [_avatarForGaiaID removeObjectForKey:gaiaIDString];
+  [_knownGaiaIDs removeObject:gaiaIDString];
 }
 
 @end
--- ios/chrome/browser/signin/model/resized_avatar_cache_unittest.mm
+++ ios/chrome/browser/signin/model/resized_avatar_cache_unittest.mm
@@ -12,6 +12,7 @@
 #import "ios/chrome/browser/signin/model/signin_util.h"
 #import "testing/gtest_mac.h"
 #import "testing/platform_test.h"
+#import "ui/base/test/ios/ui_image_test_utils.h"
 
 // Test fixture for testing ResizedAvatarCache class.
 class ResizedAvatarCacheTest : public PlatformTest {
@@ -22,14 +23,8 @@ class ResizedAvatarCacheTest : public PlatformTest {
         initWithIdentityAvatarSize:IdentityAvatarSize::TableViewIcon];
     identity1_ = [FakeSystemIdentity fakeIdentity1];
     fake_system_identity_manager()->AddIdentity(identity1_);
-    // Resets the avatar to simulate that it was not fetched yet.
-    fake_system_identity_manager()->UpdateSystemIdentityAvatar(
-        identity1_.gaiaId, nil, /*send_notification=*/false);
     identity2_ = [FakeSystemIdentity fakeIdentity2];
     fake_system_identity_manager()->AddIdentity(identity2_);
-    // Resets the avatar to simulate that it was not fetched yet.
-    fake_system_identity_manager()->UpdateSystemIdentityAvatar(
-        identity2_.gaiaId, nil, /*send_notification=*/false);
   }
 
   FakeSystemIdentityManager* fake_system_identity_manager() {
@@ -45,6 +40,11 @@ class ResizedAvatarCacheTest : public PlatformTest {
 
 // Tests that the default avatar is the same between 2 identities.
 TEST_F(ResizedAvatarCacheTest, DefaultAvatarSize) {
+  // Resets the avatar to simulate that it was not fetched yet.
+  fake_system_identity_manager()->UpdateSystemIdentityAvatar(
+      identity1_.gaiaId, nil, /*send_notification=*/false);
+  fake_system_identity_manager()->UpdateSystemIdentityAvatar(
+      identity2_.gaiaId, nil, /*send_notification=*/false);
   UIImage* avatar1 =
       [resized_avatar_cache_ resizedAvatarForIdentity:identity1_];
   EXPECT_NE(nil, avatar1);
@@ -61,6 +61,9 @@ TEST_F(ResizedAvatarCacheTest, DefaultAvatarSize) {
 
 // Tests that the avatar is updated after waiting for the fetch.
 TEST_F(ResizedAvatarCacheTest, FetchAvatar) {
+  // Resets the avatar to simulate that it was not fetched yet.
+  fake_system_identity_manager()->UpdateSystemIdentityAvatar(
+      identity1_.gaiaId, nil, /*send_notification=*/false);
   UIImage* default_avatar =
       [resized_avatar_cache_ resizedAvatarForIdentity:identity1_];
   EXPECT_NE(nil, default_avatar);
@@ -85,13 +88,52 @@ TEST_F(ResizedAvatarCacheTest, FetchAvatar) {
 // Tests that the default avatar is forgotten after a memory warning
 // notification.
 TEST_F(ResizedAvatarCacheTest, MemoryWarning) {
+  // Resets the avatar to simulate that it was not fetched yet.
+  fake_system_identity_manager()->UpdateSystemIdentityAvatar(
+      identity1_.gaiaId, nil, /*send_notification=*/false);
+  fake_system_identity_manager()->UpdateSystemIdentityAvatar(
+      identity2_.gaiaId, nil, /*send_notification=*/false);
   UIImage* first_default_avatar =
       [resized_avatar_cache_ resizedAvatarForIdentity:identity1_];
   EXPECT_NE(nil, first_default_avatar);
+  // Sends the memory warning to drop the default avatar.
   [[NSNotificationCenter defaultCenter]
       postNotificationName:UIApplicationDidReceiveMemoryWarningNotification
                     object:nil];
   UIImage* second_default_avatar =
       [resized_avatar_cache_ resizedAvatarForIdentity:identity2_];
+  // The default avatar needs to be recomputed, therefore the pointers are
+  // different.
   EXPECT_NE(first_default_avatar, second_default_avatar);
+}
+
+// Tests that the avatar is reset to the default avatar when identity is removed
+// and added back.
+TEST_F(ResizedAvatarCacheTest, AddRemoveAndAddAgainIdentity) {
+  CGSize expected_size =
+      GetSizeForIdentityAvatarSize(IdentityAvatarSize::TableViewIcon);
+  UIImage* first_avatar = ui::test::uiimage_utils::UIImageWithSizeAndSolidColor(
+      expected_size, UIColor.whiteColor);
+  fake_system_identity_manager()->UpdateSystemIdentityAvatar(
+      identity1_.gaiaId, first_avatar, /*send_notification=*/false);
+  UIImage* avatar_from_cache =
+      [resized_avatar_cache_ resizedAvatarForIdentity:identity1_];
+  // It should be the same instance since the identity avatar is at the right
+  // size.
+  EXPECT_EQ(avatar_from_cache, first_avatar);
+
+  // Remove the identity, and add it again.
+  fake_system_identity_manager()->ForgetIdentity(identity1_, base::DoNothing());
+  fake_system_identity_manager()->WaitForServiceCallbacksToComplete();
+  fake_system_identity_manager()->AddIdentity(identity1_);
+  // Set a different avatar.
+  UIImage* second_avatar =
+      ui::test::uiimage_utils::UIImageWithSizeAndSolidColor(expected_size,
+                                                            UIColor.whiteColor);
+  fake_system_identity_manager()->UpdateSystemIdentityAvatar(
+      identity1_.gaiaId, second_avatar, /*send_notification=*/false);
+
+  avatar_from_cache =
+      [resized_avatar_cache_ resizedAvatarForIdentity:identity1_];
+  EXPECT_EQ(avatar_from_cache, second_avatar);
 }
