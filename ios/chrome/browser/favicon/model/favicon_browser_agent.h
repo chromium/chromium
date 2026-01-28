604f0efedb89cdcd690e8e58e1cd7fc125042980
Sylvain Defresne <sdefresne@chromium.org>
2026-01-28 08:43:54 -0800

    [ios] Convert FaviconBrowserAgent to use TabsDependencyInstaller
    The implementation of FaviconBrowserAgent lead to an O(n**2) complexity
    due to the way base::ScopedMultiSourceObservation<...> works. Convert
    to use TabsDependencyInstaller which does not depends on calling the
    IsObservingSource(...) method.
    
    This causes the FaviconBrowserAgent to start fetching favicons for new
    tabs and tabs moved between Browser but this trade some extra work for
    smaller complexity overall.
    
    Bug: 477014186
    Change-Id: I7f85cc04cdab40fca13276fdd593b367f5418621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7497655
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1575978}

--- ios/chrome/browser/favicon/model/favicon_browser_agent.h
+++ ios/chrome/browser/favicon/model/favicon_browser_agent.h
@@ -6,65 +6,27 @@
 #define IOS_CHROME_BROWSER_FAVICON_MODEL_FAVICON_BROWSER_AGENT_H_
 
 #include "base/memory/raw_ptr.h"
-#include "base/scoped_multi_source_observation.h"
-#include "base/scoped_observation.h"
-#include "ios/chrome/browser/sessions/model/session_restoration_observer.h"
 #include "ios/chrome/browser/shared/model/browser/browser_user_data.h"
-#include "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer.h"
-#include "ios/web/public/web_state_observer.h"
-
-class SessionRestorationService;
+#include "ios/chrome/browser/tabs/model/tabs_dependency_installer.h"
 
 // A BrowserAgent that prepares WebStates to fetch their favicon after
 // a session restoration.
 class FaviconBrowserAgent final : public BrowserUserData<FaviconBrowserAgent>,
-                                  public SessionRestorationObserver,
-                                  public web::WebStateObserver,
-                                  public WebStateListObserver {
+                                  public TabsDependencyInstaller {
  public:
   ~FaviconBrowserAgent() final;
 
-  // SessionRestorationObserver implementation.
-  void WillStartSessionRestoration(Browser* browser) final;
-  void SessionRestorationFinished(
-      Browser* browser,
-      const std::vector<web::WebState*>& restored_web_states) final;
-
-  // WebStateListObserver implementation.
-  void WebStateListDidChange(WebStateList* web_state_list,
-                             const WebStateListChange& change,
-                             const WebStateListStatus& status) override;
-
-  // web::WebStateObserver implementation.
-  void WebStateRealized(web::WebState* web_state) override;
-  void WebStateDestroyed(web::WebState* web_state) override;
+  // TabsDependencyInstaller implementation.
+  void OnWebStateInserted(web::WebState* web_state) final;
+  void OnWebStateRemoved(web::WebState* web_state) final;
+  void OnWebStateDeleted(web::WebState* web_state) final;
+  void OnActiveWebStateChanged(web::WebState* old_active,
+                               web::WebState* new_active) final;
 
  private:
   friend class BrowserUserData<FaviconBrowserAgent>;
 
   explicit FaviconBrowserAgent(Browser* browser);
-
-  // Helper called when starting to observe an unrealized WebState.
-  void StartObservingWebState(web::WebState* web_state);
-
-  // Helper called when stopping to observe WebState (either because
-  // it became realized, was detached or destroyed).
-  void StopObservingWebState(web::WebState* web_state);
-
-  // Starts fetching the favicon for a WebState.
-  void FetchFaviconForWebState(web::WebState* web_state);
-
-  // Observation for SessionRestorationService events.
-  base::ScopedObservation<SessionRestorationService, SessionRestorationObserver>
-      session_restoration_service_observation_{this};
-
-  // Observation of the WebStateList.
-  base::ScopedObservation<WebStateList, WebStateListObserver>
-      web_state_list_observation_{this};
-
-  // Observation for unrealized WebStates.
-  base::ScopedMultiSourceObservation<web::WebState, web::WebStateObserver>
-      web_state_observations_{this};
 };
 
 #endif  // IOS_CHROME_BROWSER_FAVICON_MODEL_FAVICON_BROWSER_AGENT_H_
--- ios/chrome/browser/favicon/model/favicon_browser_agent.mm
+++ ios/chrome/browser/favicon/model/favicon_browser_agent.mm
@@ -5,141 +5,40 @@
 #import "ios/chrome/browser/favicon/model/favicon_browser_agent.h"
 
 #import "base/check.h"
-#import "components/favicon/core/favicon_service.h"
 #import "components/favicon/ios/web_favicon_driver.h"
-#import "components/keyed_service/core/service_access_type.h"
-#import "ios/chrome/browser/favicon/model/favicon_service_factory.h"
-#import "ios/chrome/browser/sessions/model/session_restoration_service.h"
-#import "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
-#import "ios/chrome/browser/shared/model/profile/profile_ios.h"
-#import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
-#import "ios/web/common/features.h"
 #import "ios/web/public/web_state.h"
 #import "url/gurl.h"
 
 FaviconBrowserAgent::FaviconBrowserAgent(Browser* browser)
     : BrowserUserData(browser) {
-  // All the BrowserAgent are attached to the Browser during the creation,
-  // the WebStateList must be empty at this point.
-  DCHECK(browser_->GetWebStateList()->empty())
-      << "PagePlaceholderBrowserAgent created for a Browser with a non-empty "
-         "WebStateList.";
-
-  ProfileIOS* profile = browser_->GetProfile();
-  session_restoration_service_observation_.Observe(
-      SessionRestorationServiceFactory::GetForProfile(profile));
+  StartObserving(browser, TabsDependencyInstaller::Policy::kOnlyRealized);
 }
 
-FaviconBrowserAgent::~FaviconBrowserAgent() = default;
-
-#pragma mark - SessionRestorationObserver
-
-void FaviconBrowserAgent::WillStartSessionRestoration(Browser* browser) {
-  // Nothing to do.
+FaviconBrowserAgent::~FaviconBrowserAgent() {
+  StopObserving();
 }
 
-void FaviconBrowserAgent::SessionRestorationFinished(
-    Browser* browser,
-    const std::vector<web::WebState*>& restored_web_states) {
-  // Ignore the event if it does not correspond to the browser this
-  // object is bound to (which can happen with the optimised session
-  // storage code).
-  if (browser_.get() != browser) {
-    return;
-  }
+#pragma mark - TabsDependencyInstaller
 
-  // Start fetching the favicon for the restored WebStates if necessary.
-  for (web::WebState* web_state : restored_web_states) {
-    FetchFaviconForWebState(web_state);
-  }
-}
-
-void FaviconBrowserAgent::WebStateListDidChange(
-    WebStateList* web_state_list,
-    const WebStateListChange& change,
-    const WebStateListStatus& status) {
-  CHECK(web::features::CreateTabHelperOnlyForRealizedWebStates());
-  switch (change.type()) {
-    case WebStateListChange::Type::kStatusOnly:
-      // Nothing to do.
-      break;
-
-    case WebStateListChange::Type::kDetach:
-      StopObservingWebState(
-          change.As<WebStateListChangeDetach>().detached_web_state());
-      break;
-
-    case WebStateListChange::Type::kMove:
-      // Nothing do do.
-      break;
-
-    case WebStateListChange::Type::kReplace:
-      StopObservingWebState(
-          change.As<WebStateListChangeReplace>().replaced_web_state());
-      break;
-
-    case WebStateListChange::Type::kInsert:
-      // Nothing to do.
-      break;
-
-    case WebStateListChange::Type::kGroupCreate:
-      // Nothing to do.
-      break;
-
-    case WebStateListChange::Type::kGroupVisualDataUpdate:
-      // Nothing to do.
-      break;
-
-    case WebStateListChange::Type::kGroupMove:
-      // Nothing to do.
-      break;
-
-    case WebStateListChange::Type::kGroupDelete:
-      // Nothing to do.
-      break;
-  }
-}
-
-void FaviconBrowserAgent::WebStateRealized(web::WebState* web_state) {
-  CHECK(web::features::CreateTabHelperOnlyForRealizedWebStates());
-  FetchFaviconForWebState(web_state);
-  StopObservingWebState(web_state);
-}
-
-void FaviconBrowserAgent::WebStateDestroyed(web::WebState* web_state) {
-  CHECK(web::features::CreateTabHelperOnlyForRealizedWebStates());
-  StopObservingWebState(web_state);
-}
-
-void FaviconBrowserAgent::StartObservingWebState(web::WebState* web_state) {
-  CHECK(web::features::CreateTabHelperOnlyForRealizedWebStates());
-  if (!web_state_observations_.IsObservingAnySource()) {
-    web_state_list_observation_.Observe(browser_->GetWebStateList());
-  }
-  web_state_observations_.AddObservation(web_state);
-}
-
-void FaviconBrowserAgent::StopObservingWebState(web::WebState* web_state) {
-  CHECK(web::features::CreateTabHelperOnlyForRealizedWebStates());
-  if (web_state_observations_.IsObservingSource(web_state)) {
-    web_state_observations_.RemoveObservation(web_state);
-    if (!web_state_observations_.IsObservingAnySource()) {
-      web_state_list_observation_.Reset();
-    }
-  }
-}
-
-void FaviconBrowserAgent::FetchFaviconForWebState(web::WebState* web_state) {
-  if (web::features::CreateTabHelperOnlyForRealizedWebStates()) {
-    if (!web_state->IsRealized()) {
-      StartObservingWebState(web_state);
-      return;
-    }
-  }
-
+void FaviconBrowserAgent::OnWebStateInserted(web::WebState* web_state) {
+  CHECK(web_state->IsRealized());
   const GURL& visible_url = web_state->GetVisibleURL();
   if (visible_url.is_valid()) {
+    // Starts fetching the favicon for the WebState.
     favicon::WebFaviconDriver::FromWebState(web_state)->FetchFavicon(
         visible_url, /*is_same_document=*/false);
   }
+}
+
+void FaviconBrowserAgent::OnWebStateRemoved(web::WebState* web_state) {
+  // Nothing to do.
+}
+
+void FaviconBrowserAgent::OnWebStateDeleted(web::WebState* web_state) {
+  // Nothing to do.
+}
+
+void FaviconBrowserAgent::OnActiveWebStateChanged(web::WebState* old_active,
+                                                  web::WebState* new_active) {
+  // Nothing to do.
 }
