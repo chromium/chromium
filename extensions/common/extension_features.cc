25c364f08b9addb3b3fe70b1b7404e3f9337707d
Andrea Orru <andreaorru@chromium.org>
2026-01-28 10:58:29 -0800

    [Extensions] Enable WebRequestAlternativeAddListener feature flag
    This introduces an alternative path for registering webRequest event
    listeners flag. This approach leverages the standard EventRouter
    observer pattern instead of the custom webRequestInternal API.
    
    Bug: 474558883
    Change-Id: I4546b1b98c966d20147ee7c60cb4ddafdea5d76b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7518387
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576076}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -216,6 +216,6 @@ BASE_FEATURE(kWebRequestPersistFilteredEvents,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kWebRequestAlternativeAddListener,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace extensions_features
