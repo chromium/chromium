af08fb2098836c5d4b187e04f1e4ce3859800f9c
Nohemi Fernandez <fernandex@google.com>
2026-01-29 09:12:10 -0800

    [iOS] Fix DistilledPagePrefs::Observer crash in Reading Mode options.
    Ensure that all DistilledPagePrefs observers in the Reading Mode
    feature are managed safely to minimize the risk of use-after-free
    crashes during theme or font updates.
    
    Bug: 479469797
    Change-Id: I89a0acd1abf095de9b04af80938b9f4d24f521d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532440
    Commit-Queue: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576655}

--- components/dom_distiller/ios/distilled_page_prefs_observer_bridge_unittest.mm
+++ components/dom_distiller/ios/distilled_page_prefs_observer_bridge_unittest.mm
@@ -4,6 +4,7 @@
 
 #import "components/dom_distiller/ios/distilled_page_prefs_observer_bridge.h"
 
+#import "components/sync_preferences/testing_pref_service_syncable.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
 
@@ -39,25 +40,34 @@
 // Test fixture for DistilledPagePrefsObserverBridge.
 class DistilledPagePrefsObserverBridgeTest : public PlatformTest {
  public:
-  DistilledPagePrefsObserverBridgeTest()
-      : observer_([[FakeDistilledPagePrefsObserver alloc] init]),
-        observer_bridge_(observer_) {}
+  DistilledPagePrefsObserverBridgeTest() {
+    observer_ = [[FakeDistilledPagePrefsObserver alloc] init];
+    dom_distiller::DistilledPagePrefs::RegisterProfilePrefs(
+        pref_service_.registry());
+    distilled_page_prefs_ =
+        std::make_unique<dom_distiller::DistilledPagePrefs>(&pref_service_);
+    observer_bridge_ = std::make_unique<DistilledPagePrefsObserverBridge>(
+        observer_, distilled_page_prefs_.get());
+  }
 
  protected:
   FakeDistilledPagePrefsObserver* observer_;
-  DistilledPagePrefsObserverBridge observer_bridge_;
+  sync_preferences::TestingPrefServiceSyncable pref_service_;
+  std::unique_ptr<dom_distiller::DistilledPagePrefs> distilled_page_prefs_;
+  std::unique_ptr<DistilledPagePrefsObserverBridge> observer_bridge_;
 };
 
 // Tests that the observer is notified of font family changes.
 TEST_F(DistilledPagePrefsObserverBridgeTest, OnChangeFontFamily) {
-  observer_bridge_.OnChangeFontFamily(dom_distiller::mojom::FontFamily::kSerif);
+  observer_bridge_->OnChangeFontFamily(
+      dom_distiller::mojom::FontFamily::kSerif);
   EXPECT_EQ([observer_ notifiedFontFamily],
             dom_distiller::mojom::FontFamily::kSerif);
 }
 
 // Tests that the observer is notified of user preference theme changes.
 TEST_F(DistilledPagePrefsObserverBridgeTest, OnChangeUserPreferenceTheme) {
-  observer_bridge_.OnChangeTheme(
+  observer_bridge_->OnChangeTheme(
       dom_distiller::mojom::Theme::kDark,
       dom_distiller::ThemeSettingsUpdateSource::kUserPreference);
   EXPECT_EQ([observer_ notifiedTheme], dom_distiller::mojom::Theme::kDark);
@@ -65,22 +75,23 @@ TEST_F(DistilledPagePrefsObserverBridgeTest, OnChangeUserPreferenceTheme) {
 
 // Tests that the observer is notified of system theme changes.
 TEST_F(DistilledPagePrefsObserverBridgeTest, OnChangeSystemTheme) {
-  observer_bridge_.OnChangeTheme(dom_distiller::mojom::Theme::kDark,
-                                 dom_distiller::ThemeSettingsUpdateSource::kSystem);
+  observer_bridge_->OnChangeTheme(
+      dom_distiller::mojom::Theme::kDark,
+      dom_distiller::ThemeSettingsUpdateSource::kSystem);
   EXPECT_EQ([observer_ notifiedTheme], dom_distiller::mojom::Theme::kDark);
 }
 
 // Tests that the observer is notified of font scaling changes.
 TEST_F(DistilledPagePrefsObserverBridgeTest, OnChangeFontScaling) {
-  observer_bridge_.OnChangeFontScaling(1.5f);
+  observer_bridge_->OnChangeFontScaling(1.5f);
   EXPECT_EQ([observer_ notifiedFontScaling], 1.5f);
 }
 
 // Tests that the observer is notified of changes to enabling links.
 TEST_F(DistilledPagePrefsObserverBridgeTest, OnChangeLinksEnabled) {
-  observer_bridge_.OnChangeLinksEnabled(true);
+  observer_bridge_->OnChangeLinksEnabled(true);
   EXPECT_TRUE([observer_ notifiedLinksEnabled]);
 
-  observer_bridge_.OnChangeLinksEnabled(false);
+  observer_bridge_->OnChangeLinksEnabled(false);
   EXPECT_FALSE([observer_ notifiedLinksEnabled]);
 }
