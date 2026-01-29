c892cff6bf75ba0f7e3e88e051f6a1c4a67acd5e
Dale Curtis <dalecurtis@chromium.org>
2026-01-28 16:39:33 -0800

    [Cast] Fix a couple issues with PopulateVideoFrame() test helper
    This code mixed the coded and visible sizes as well as incorrectly
    calculated the visible row bytes. This fixes crashes revealed by
    reducing the VideoFrame allocation size.
    
    R=jophba
    
    Bug: 479577744
    Change-Id: I9d5eca17cebc094bbdb8636c5b7bc83cc611eac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7529012
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1576275}

--- media/cast/test/utility/video_utility.cc
+++ media/cast/test/utility/video_utility.cc
@@ -19,19 +19,19 @@ namespace media::cast {
 
 void FillPlane(VideoFrame& frame,
                VideoFrame::Plane plane,
-               int height,
                int stripe_size,
                int start_value) {
   const int stride = frame.stride(plane);
   base::span<uint8_t> plane_to_fill = frame.GetWritableVisiblePlaneData(plane);
 
-  for (int j = 0; j < height; ++j) {
+  for (int j = 0; j < frame.GetVisibleRows(plane); ++j) {
     const int stripe_j = (j / stripe_size) * stripe_size;
-    for (int i = 0; i < stride; ++i) {
+    for (int i = 0; i < frame.GetVisibleRowBytes(plane); ++i) {
       const int stripe_i = (i / stripe_size) * stripe_size;
       // Special case:: if this is the UV plane, odd indices should be set to
       // the same value as the even index before them.
-      if (plane == VideoFrame::Plane::kUV && i % 2 == 1) {
+      if (frame.format() == PIXEL_FORMAT_NV12 &&
+          plane == VideoFrame::Plane::kUV && i % 2 == 1) {
         plane_to_fill[j * stride + i] = plane_to_fill[j * stride + i - 1];
       } else {
         plane_to_fill[j * stride + i] =
@@ -42,32 +42,27 @@ void FillPlane(VideoFrame& frame,
 }
 
 void PopulateVideoFrame(VideoFrame* frame, int start_value) {
-  const gfx::Size frame_size = frame->coded_size();
+  const gfx::Size frame_size = frame->visible_rect().size();
   const int stripe_size =
       std::max(32, std::min(frame_size.width(), frame_size.height()) / 8) & -2;
 
   // Set Y.
-  const int height = frame_size.height();
-  FillPlane(*frame, VideoFrame::Plane::kY, height, stripe_size, start_value);
+  FillPlane(*frame, VideoFrame::Plane::kY, stripe_size, start_value);
 
-  const int half_height = (height + 1) / 2;
+  // Set U.
   if (frame->format() == PIXEL_FORMAT_NV12) {
-    FillPlane(*frame, VideoFrame::Plane::kUV, half_height, stripe_size,
-              start_value);
+    FillPlane(*frame, VideoFrame::Plane::kUV, stripe_size, start_value);
   } else {
     CHECK(frame->format() == PIXEL_FORMAT_I420 ||
           frame->format() == PIXEL_FORMAT_YV12);
-    FillPlane(*frame, VideoFrame::Plane::kU, half_height, stripe_size,
-              start_value);
-    FillPlane(*frame, VideoFrame::Plane::kV, half_height, stripe_size,
-              start_value);
+    FillPlane(*frame, VideoFrame::Plane::kU, stripe_size, start_value);
+    FillPlane(*frame, VideoFrame::Plane::kV, stripe_size, start_value);
   }
 }
 
 void PopulateVideoFrameWithNoise(VideoFrame* frame) {
-  for (auto plane :
-       {VideoFrame::Plane::kY, VideoFrame::Plane::kU, VideoFrame::Plane::kV}) {
-    base::RandBytes(frame->GetWritableVisiblePlaneData(plane));
+  for (size_t i = 0; i < frame->layout().num_planes(); ++i) {
+    base::RandBytes(frame->GetWritableVisiblePlaneData(i));
   }
 }
 }  // namespace media::cast
