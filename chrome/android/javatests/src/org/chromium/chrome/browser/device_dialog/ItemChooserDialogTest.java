41e94638506d3740cbb7bb2299227a505f15f60a
Henrique Nakashima <hnakashima@chromium.org>
2026-01-30 14:06:47 -0800

    [Android] Disable 3 cases in ItemChooserDialogTest on tablets, desktop
    * testPairButtonDisabledAfterSelectedItemRemoved
    * testSelectAnItemAndRemoveAnotherItem
    * testSelectAnItemAndRemoveTheSelectedItem
    
    Consistently failing on tablet and desktop configurations due to layout
    issues causing zero-width buttons.
    
    Bug: 480179765
    Change-Id: I2efa301ea20b50c536d3f7844d100f5ec060ee9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7533699
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Garfield Tan <xutan@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Garfield Tan <xutan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577540}

--- chrome/android/javatests/src/org/chromium/chrome/browser/device_dialog/ItemChooserDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/device_dialog/ItemChooserDialogTest.java
@@ -31,6 +31,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.R;
@@ -40,6 +41,7 @@ import org.chromium.components.permissions.DeviceItemAdapter;
 import org.chromium.components.permissions.ItemChooserDialog;
 import org.chromium.components.permissions.PermissionsAndroidFeatureList;
 import org.chromium.content_public.browser.test.util.TouchCommon;
+import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.widget.TextViewWithClickableSpans;
 
 /**
@@ -600,6 +602,7 @@ public class ItemChooserDialogTest implements ItemChooserDialog.ItemSelectedCall
 
     @Test
     @SmallTest
+    @DisableIf.Device(DeviceFormFactor.TABLET_OR_DESKTOP) // crbug.com/480179765
     public void testPairButtonDisabledAfterSelectedItemRemoved() throws Throwable {
         final Dialog dialog =
                 ThreadUtils.runOnUiThreadBlocking(
@@ -627,6 +630,7 @@ public class ItemChooserDialogTest implements ItemChooserDialog.ItemSelectedCall
 
     @Test
     @SmallTest
+    @DisableIf.Device(DeviceFormFactor.TABLET_OR_DESKTOP) // crbug.com/480179765
     public void testSelectAnItemAndRemoveAnotherItem() throws Throwable {
         final Dialog dialog =
                 ThreadUtils.runOnUiThreadBlocking(
@@ -661,6 +665,7 @@ public class ItemChooserDialogTest implements ItemChooserDialog.ItemSelectedCall
 
     @Test
     @SmallTest
+    @DisableIf.Device(DeviceFormFactor.TABLET_OR_DESKTOP) // crbug.com/480179765
     public void testSelectAnItemAndRemoveTheSelectedItem() throws Throwable {
         final Dialog dialog =
                 ThreadUtils.runOnUiThreadBlocking(
