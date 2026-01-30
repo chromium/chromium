7c15310e8e7c9a1b1ce59a8fb906fb71770cf16f
Shuhei Takahashi <nya@chromium.org>
2026-01-29 19:58:13 -0800

    Revert "Re-reland "[Extensions] Change extensions menu mediator lifetime"""
    This reverts commit e0a00c03924c07bfadf59882e7a377b18e1a1f3c.
    
    Reason for revert: Suspected to break ExtensionToolbarTest#testExtensionsMenu (b/479959722)
    
    Original change's description:
    > Re-reland "[Extensions] Change extensions menu mediator lifetime""
    >
    > THis is a reland of commit 5b04562720c80f36104b4a3c6fc7bf0d1cdf9fe8
    >
    > CL was reverted because of a missing include in the test file. This
    > was not caught (twice) because tests targets are not compiled for
    > desktop android in CQ. This time, made sure that test files compiled
    > locally
    >
    > Original change's description:
    > > Reland "[Extensions] Change extensions menu mediator lifetime"
    > >
    > > This is a reland of commit 906f265572d0196bec4e2a5a912efa0b570f2f10
    > >
    > > CL was reverted because "could not resolve DisabledTest". Added the
    > > missing include.
    > >
    > > Original change's description:
    > > > [Extensions] Change extensions menu mediator lifetime
    > > >
    > > > This CL changes the lifecycle of the ExtensionsMenuMediator to be
    > > > created only when the extensions menu is about to be shown and destroyed
    > > > when the menu is dismissed.
    > > >
    > > > Previously, the ExtensionsMenuMediator was instantiated along with
    > > > the ExtensionsMenuCoordinator and remained active for its entire
    > > > duration, even when the menu was not visible. Since the
    > > > ExtensionsMenuMediator should construct a JNI bridge to ExtensionsMenuViewModel, we want to limit its lifetime only when the
    > > > extensions menu is opened as the ViewModel observes various changes that are not relevant when the menu is closed.
    > > >
    > > > The Runnable that was used to show the menu after extension actions were updated has been removed because action updates should be
    > > > notified through the bridge now. This will be implemented in next
    > > > CLs, to reduce the complexity of this CL (we are aware that
    > > > functionality is not ready, e.g the menu should only open once
    > > > mediator has populated its actions).
    > > >
    > > > A new test has been added to verify the mediator's lifetime.
    > > >
    > > > EDIT on Patchset #9:
    > > > This CL is also connecting ExtensionsMenuBridge with the ExtensionsMenuMediator. This was going to be made on a precursor CL,
    > > > but it had build failures because of lifetime issues which are
    > > > addressed here
    > > >
    > > > Screenshot: https://drive.google.com/file/d/1LGK36B_BWhuFwyfEusNoqXAeOvVinb6W/view?usp=sharing
    > > >
    > > > Bug: 473509167
    > > > Change-Id: I92215a79718000632fa914a3844deb9f2354d4a9
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7513187
    > > > Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    > > > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    > > > Cr-Commit-Position: refs/heads/main@{#1575692}
    > >
    > > Bug: 473509167
    > > Change-Id: I4db1ad1552d6504f6d061353e8acf9e0b151891b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7526447
    > > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    > > Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1575799}
    >
    > Bug: 73509167
    > Change-Id: Ic9c803aae5b5841f638d9ae21c72301bec31a166
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7528474
    > Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    > Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1576921}
    
    Bug: 73509167
    Bug: 479959722
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I9d84eddd11ee8257bcec58e1cae0ce11cd581ef7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7534675
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Shuhei Takahashi <nya@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577008}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/extensions/ExtensionsMenuCoordinator.java
@@ -32,7 +32,6 @@ import org.chromium.ui.base.PageTransition;
 import org.chromium.ui.listmenu.ListMenu;
 import org.chromium.ui.listmenu.ListMenuButton;
 import org.chromium.ui.listmenu.ListMenuDelegate;
-import org.chromium.ui.listmenu.ListMenuHost;
 import org.chromium.ui.modelutil.LayoutViewBuilder;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -47,7 +46,6 @@ import org.chromium.ui.widget.RectProvider;
 @NullMarked
 public class ExtensionsMenuCoordinator implements Destroyable {
     private final Context mContext;
-    private final ListMenu mExtensionsMenu;
     private final ListMenuButton mExtensionsMenuButton;
     private final ThemeColorProvider mThemeColorProvider;
     private final NullableObservableSupplier<Tab> mCurrentTabSupplier;
@@ -55,11 +53,12 @@ public class ExtensionsMenuCoordinator implements Destroyable {
     private final View mContentView;
     private final PropertyModelChangeProcessor mChangeProcessor;
     private final ModelList mExtensionModels;
-    private final ChromeAndroidTask mTask;
 
     private final ThemeColorProvider.TintObserver mTintObserver = this::onTintChanged;
 
     @Nullable @VisibleForTesting ExtensionsMenuMediator mMediator;
+
+    private boolean mShouldShowMenuOnInit;
 
     /**
      * Constructor.
@@ -81,11 +80,27 @@ public class ExtensionsMenuCoordinator implements Destroyable {
         mContext = context;
         mCurrentTabSupplier = currentTabSupplier;
         mTabCreator = tabCreator;
-        mTask = task;
+
+        mExtensionsMenuButton = extensionsMenuButton;
+        mExtensionsMenuButton.setOnClickListener(view -> mShouldShowMenuOnInit = true);
+        mExtensionsMenuButton.setMenuMaxWidth(
+                context.getResources().getDimensionPixelSize(R.dimen.extension_menu_max_width));
+
+        mThemeColorProvider = themeColorProvider;
+        mThemeColorProvider.addTintObserver(mTintObserver);
 
         mContentView = LayoutInflater.from(mContext).inflate(R.layout.extensions_menu, null, false);
 
-        mExtensionsMenu =
+        PropertyModel model = createMenuPropertyModel();
+
+        mChangeProcessor =
+                PropertyModelChangeProcessor.create(
+                        model, mContentView, ExtensionsMenuViewBinder::bind);
+
+        mExtensionModels = new ModelList();
+        setUpExtensionsRecyclerView(mContentView, mContext, mExtensionModels);
+
+        ListMenu listMenu =
                 new ListMenu() {
                     @Override
                     public View getContentView() {
@@ -102,83 +117,37 @@ public class ExtensionsMenuCoordinator implements Destroyable {
                     }
                 };
 
-        mExtensionsMenuButton = extensionsMenuButton;
-        mExtensionsMenuButton.setMenuMaxWidth(
-                context.getResources().getDimensionPixelSize(R.dimen.extension_menu_max_width));
-        mExtensionsMenuButton.setDelegate(
-                new ListMenuDelegate() {
-                    @Override
-                    public ListMenu getListMenu() {
-                        // TODO(crbug.com/473213114): we should only show the menu after actions are
-                        // initialized. This will be easier to do once we remove
-                        // ActionsUpdateDelegate and instead we populate actions from the ViewModel
-                        // (C++).
-                        createMediator();
-                        return mExtensionsMenu;
-                    }
-
-                    @Override
-                    public RectProvider getRectProvider(View listMenuHostingView) {
-                        return MenuBuilderHelper.getRectProvider(mExtensionsMenuButton);
-                    }
-                });
-        mExtensionsMenuButton.addPopupListener(
-                new ListMenuHost.PopupMenuShownListener() {
-                    @Override
-                    public void onPopupMenuShown() {}
-
-                    @Override
-                    public void onPopupMenuDismissed() {
-                        destroyMediator();
-                    }
-                });
-
-        mThemeColorProvider = themeColorProvider;
-        mThemeColorProvider.addTintObserver(mTintObserver);
-
-        PropertyModel model = createMenuPropertyModel();
-
-        mChangeProcessor =
-                PropertyModelChangeProcessor.create(
-                        model, mContentView, ExtensionsMenuViewBinder::bind);
-
-        mExtensionModels = new ModelList();
-        setUpExtensionsRecyclerView(mContentView, mContext, mExtensionModels);
-    }
-
-    /**
-     * Creates the extensions menu mediator and the associated JNI bridge.
-     *
-     * <p>This should only be called when the menu is about to be shown.
-     */
-    private void createMediator() {
-        if (mMediator != null) {
-            return;
-        }
-
-        // Clear old data before repopulating.
-        mExtensionModels.clear();
-
-        // Instantiate the mediator, which will initialize the JNI bridge to the native code.
         mMediator =
                 new ExtensionsMenuMediator(
                         mContext,
-                        mTask,
+                        task,
                         mCurrentTabSupplier,
                         mExtensionModels,
-                        mExtensionsMenuButton.getRootView());
-    }
+                        () -> {
+                            mExtensionsMenuButton.setDelegate(
+                                    new ListMenuDelegate() {
+                                        @Override
+                                        public RectProvider getRectProvider(
+                                                View listMenuHostingView) {
+                                            return MenuBuilderHelper.getRectProvider(
+                                                    mExtensionsMenuButton);
+                                        }
 
-    /**
-     * Destroys the extensions menu mediator.
-     *
-     * <p>This should be called when the menu is closed.
-     */
-    private void destroyMediator() {
-        if (mMediator != null) {
-            mMediator.destroy();
-            mMediator = null;
-        }
+                                        @Override
+                                        public ListMenu getListMenu() {
+                                            return listMenu;
+                                        }
+                                    });
+                            if (mShouldShowMenuOnInit) {
+                                if (mExtensionsMenuButton.getHost().isMenuShowing()) {
+                                    mExtensionsMenuButton.dismiss();
+                                } else {
+                                    mExtensionsMenuButton.showMenu();
+                                }
+                                mShouldShowMenuOnInit = false;
+                            }
+                        },
+                        mExtensionsMenuButton.getRootView());
     }
 
     private void openUrlFromMenu(String url) {
@@ -238,7 +207,10 @@ public class ExtensionsMenuCoordinator implements Destroyable {
 
     @Override
     public void destroy() {
-        destroyMediator();
+        if (mMediator != null) {
+            mMediator.destroy();
+            mMediator = null;
+        }
         mExtensionsMenuButton.setOnClickListener(null);
         mThemeColorProvider.removeTintObserver(mTintObserver);
         mChangeProcessor.destroy();
