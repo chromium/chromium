7b6c7b34e04a29e9069683c0a3c19489c67212e7
James Cook <jamescook@chromium.org>
2026-01-29 20:07:25 -0800

    extensions: Add manifest key options_page tests to desktop Android
    The manifest key is already parsed at the //extensions layer. This
    adds the browser test for it.
    
    Bug: 371435654
    Change-Id: I1987e356c34f8e03568f4a74ebaf7ad46a60a4e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7534175
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1577012}

--- chrome/browser/extensions/options_page_apitest.cc
+++ chrome/browser/extensions/options_page_apitest.cc
@@ -7,18 +7,57 @@
 #include "base/functional/bind.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/tabs/tab_list_interface.h"
+#include "chrome/browser/ui/tabs/tab_list_interface_observer.h"
 #include "chrome/common/url_constants.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/extension_registry.h"
+#include "extensions/buildflags/buildflags.h"
 #include "extensions/common/extension.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/test_extension_dir.h"
 
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
+
 namespace extensions {
+namespace {
+
+// Waits for a tab to be added to the tab list. Proceeds immediately if the tab
+// was added before the call to Wait().
+class TabAddedWaiter : public TabListInterfaceObserver {
+ public:
+  explicit TabAddedWaiter(TabListInterface* tab_list) : tab_list_(tab_list) {
+    tab_list_->AddTabListInterfaceObserver(this);
+  }
+
+  ~TabAddedWaiter() override {
+    tab_list_->RemoveTabListInterfaceObserver(this);
+  }
+
+  void Wait() {
+    // If the tab was already added, there's nothing to do.
+    if (tab_added_) {
+      return;
+    }
+    run_loop_.Run();
+  }
+
+  // TabListInterfaceObserver:
+  void OnTabAdded(tabs::TabInterface* tab, int index) override {
+    tab_added_ = true;
+    if (run_loop_.running()) {
+      run_loop_.Quit();
+    }
+  }
+
+ private:
+  raw_ptr<TabListInterface> tab_list_;
+  base::RunLoop run_loop_;
+  bool tab_added_ = false;
+};
+
+}  // namespace
 
 // Test that an extension with an options page makes an 'Options' button appear
 // on chrome://extensions, and that clicking the button opens a new tab with the
@@ -39,10 +78,15 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, OptionsPage) {
   ASSERT_TRUE(extension.get());
 
   // Go to the Extension Settings page and click the button.
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), GURL("chrome://extensions?id=" + extension->id())));
-  TabStripModel* tab_strip = browser()->tab_strip_model();
-  ui_test_utils::TabAddedWaiter tab_add(browser());
+  auto* web_contents = GetActiveWebContents();
+  ASSERT_TRUE(NavigateToURL(web_contents,
+                            GURL("chrome://extensions?id=" + extension->id())));
+  ASSERT_TRUE(content::WaitForLoadStop(web_contents));
+  auto* tab_list = TabListInterface::From(browser_window_interface());
+  ASSERT_TRUE(tab_list);
+
+  // The click will open a new tab.
+  TabAddedWaiter tab_add(tab_list);
 
   // Used to simulate a click on the 'Extension options' link.
   // NOTE: This relies on the layout of the chrome://extensions page, and may
@@ -55,11 +99,12 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, OptionsPage) {
       button.click();
     })();)";
 
-  EXPECT_TRUE(content::ExecJs(tab_strip->GetActiveWebContents(),
-                              kScriptClickOptionButton));
+  EXPECT_TRUE(content::ExecJs(web_contents, kScriptClickOptionButton));
   tab_add.Wait();
-  ASSERT_EQ(2, tab_strip->count());
-  content::WebContents* tab = tab_strip->GetWebContentsAt(1);
+
+  ASSERT_EQ(2, tab_list->GetTabCount());
+  content::WebContents* tab = tab_list->GetTab(1)->GetContents();
+  ASSERT_TRUE(tab);
   EXPECT_TRUE(content::WaitForLoadStop(tab));
   EXPECT_EQ(extension->GetResourceURL("options.html"),
             tab->GetLastCommittedURL());
@@ -88,8 +133,9 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
   scoped_refptr<const Extension> extension =
       InstallExtension(extension_dir.Pack(), 1);
   ASSERT_TRUE(extension.get());
-  ASSERT_TRUE(ui_test_utils::NavigateToURL(
-      browser(), GURL("chrome://extensions?options=" + extension->id())));
+  ASSERT_TRUE(
+      NavigateToURL(GetActiveWebContents(),
+                    GURL("chrome://extensions?options=" + extension->id())));
   ASSERT_TRUE(listener.WaitUntilSatisfied());
   ASSERT_EQ("embedded", listener.message());
 }
