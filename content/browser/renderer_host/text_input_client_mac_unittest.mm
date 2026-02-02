88b38fc828b1b6b5f0a336d4283e4a00b406c794
Joe Mason <joenotcharles@google.com>
2026-02-02 13:36:36 -0800

    Type-parameterize TextInputClientMacTest
    This test suite runs almost identical tests for GetCharacterIndexAtPoint
    (returning uint32_t) and GetFirstRectForRange (returning gfx::Rect). To
    reduce repeated test code, write parameterized tests with a
    TestTraits<ResponseType> class that holds everything that differs for
    the two functions.
    
    This will simplify followup patches that add more tests for both
    functions. Also fixes an existing divergence: GetFirstRectForRange
    didn't have any equivalent of
    TextInputClientMacTest.NotFoundCharacterIndex.
    
    Bug: 475270375
    Change-Id: Id95351c5d22db7c53bb0ba5259d3af27a956da10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539439
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578367}

--- content/browser/renderer_host/text_input_client_mac_unittest.mm
+++ content/browser/renderer_host/text_input_client_mac_unittest.mm
@@ -8,6 +8,7 @@
 #include <stdint.h>
 
 #include <memory>
+#include <type_traits>
 #include <utility>
 
 #include "base/containers/queue.h"
@@ -40,6 +41,65 @@ namespace {
 
 constexpr base::TimeDelta kTaskDelay = base::Milliseconds(200);
 
+// TextInputClientMacTest exercises two sync functions,
+// GetCharacterIndexAtPoint() and GetFirstRectForRange(), that should have
+// identical behaviour except for their return type. To avoid repeated test
+// code, this traits class implements only the code that must change to test
+// each function, parameteriezd on the return type.
+template <typename ResponseType>
+struct TestTraits {
+  // Calls the TextInputClientMac sync getter method under test, with `rwh` as a
+  // parameter.
+  static ResponseType TextInputClientGetSync(RenderWidgetHost* rwh);
+
+  // Calls the appropriate Got*() method of `host` with the given `response`,
+  // which will unblock the waiting TextInputClientMac.
+  static void TextInputHostGotResponse(TextInputHostImpl* host,
+                                       ResponseType response);
+
+  // Initializes a ResponseType value from an arbitrary integer.
+  static constexpr ResponseType CreateResponse(int value);
+
+  // The ResponseType value that's used when the sync getter method times out.
+  static constexpr ResponseType kTimeoutResponse;
+};
+
+template <>
+struct TestTraits<uint32_t> {
+  static uint32_t TextInputClientGetSync(RenderWidgetHost* rwh) {
+    return TextInputClientMac::GetInstance()->GetCharacterIndexAtPoint(
+        rwh, gfx::Point(2, 2));
+  }
+
+  static void TextInputHostGotResponse(TextInputHostImpl* host,
+                                       uint32_t response) {
+    host->GotCharacterIndexAtPoint(response);
+  }
+
+  static constexpr uint32_t CreateResponse(int value) { return value; }
+
+  static constexpr uint32_t kTimeoutResponse = UINT32_MAX;
+};
+
+template <>
+struct TestTraits<gfx::Rect> {
+  static gfx::Rect TextInputClientGetSync(RenderWidgetHost* rwh) {
+    return TextInputClientMac::GetInstance()->GetFirstRectForRange(
+        rwh, gfx::Range(NSMakeRange(0, 32)));
+  }
+
+  static void TextInputHostGotResponse(TextInputHostImpl* host,
+                                       gfx::Rect response) {
+    host->GotFirstRectForRange(response);
+  }
+
+  static constexpr gfx::Rect CreateResponse(int value) {
+    return gfx::Rect(value, value, value, value);
+  }
+
+  static constexpr gfx::Rect kTimeoutResponse = gfx::Rect();
+};
+
 // Fake that replaces mojo messages sent through LocalFrame by posting tasks
 // directly to TextInputHostImpl on the IO thread.
 //
@@ -48,9 +108,12 @@ constexpr base::TimeDelta kTaskDelay = base::Milliseconds(200);
 // not the IO thread. Blocking TextInputClientMac methods are also called on the
 // main thread, and wait for the responses to those messages, so receivers bound
 // to FakeLocalFrame won't get called until after the blocking method times out.
+template <typename ResponseType>
 class FakeAsyncRequestDelegate final
     : public TextInputClientMac::AsyncRequestDelegate {
  public:
+  using Traits = TestTraits<ResponseType>;
+
   FakeAsyncRequestDelegate(RenderWidgetHost* widget) : widget_(widget) {
     // Wait until `host_impl_` is created on the IO thread.
     base::test::TestFuture<std::unique_ptr<TextInputHostImpl>> host_future;
@@ -71,67 +134,56 @@ class FakeAsyncRequestDelegate final
   FakeAsyncRequestDelegate(const FakeAsyncRequestDelegate&) = delete;
   FakeAsyncRequestDelegate& operator=(const FakeAsyncRequestDelegate&) = delete;
 
-  void AddCharacterIndexResponse(uint32_t index, base::TimeDelta delay) {
+  void AddResponse(ResponseType response, base::TimeDelta delay) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-    index_responses_.emplace(index, delay);
-  }
-
-  void AddFirstRectResponse(gfx::Rect rect, base::TimeDelta delay) {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-    rect_responses_.emplace(std::move(rect), delay);
+    responses_.emplace(std::move(response), delay);
   }
 
   // AsyncRequestDelegate:
 
   void GetCharacterIndexAtPoint(RenderFrameHost* rfh,
                                 const gfx::Point& point) final {
-    DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-    ASSERT_TRUE(rfh);
-    ASSERT_EQ(rfh->GetRenderWidgetHost(), widget_);
-    if (index_responses_.empty()) {
-      return;
+    if constexpr (std::is_same_v<ResponseType, uint32_t>) {
+      SendNextResponse(rfh);
+    } else {
+      FAIL() << "Wrong test type for GetCharacterIndexAtPoint";
     }
-    auto [index, delay] = index_responses_.front();
-    index_responses_.pop();
-
-    // Unretained is safe since `host_impl_` is deleted on the IO thread.
-    GetIOThreadTaskRunner()->PostDelayedTask(
-        FROM_HERE,
-        base::BindOnce(&TextInputHostImpl::GotCharacterIndexAtPoint,
-                       base::Unretained(host_impl_.get()), index),
-        delay);
   }
 
   void GetFirstRectForRange(RenderFrameHost* rfh,
                             const gfx::Range& range) final {
+    if constexpr (std::is_same_v<ResponseType, gfx::Rect>) {
+      SendNextResponse(rfh);
+    } else {
+      FAIL() << "Wrong test type for GetFirstRectForRange";
+    }
+  }
+
+ private:
+  void SendNextResponse(RenderFrameHost* rfh) {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     ASSERT_TRUE(rfh);
     ASSERT_EQ(rfh->GetRenderWidgetHost(), widget_);
-    if (rect_responses_.empty()) {
+    if (responses_.empty()) {
       return;
     }
-    auto [rect, delay] = rect_responses_.front();
-    rect_responses_.pop();
+    auto [response, delay] = responses_.front();
+    responses_.pop();
 
     // Unretained is safe since `host_impl_` is deleted on the IO thread.
     GetIOThreadTaskRunner()->PostDelayedTask(
         FROM_HERE,
-        base::BindOnce(&TextInputHostImpl::GotFirstRectForRange,
-                       base::Unretained(host_impl_.get()), std::move(rect)),
+        base::BindOnce(&Traits::TextInputHostGotResponse,
+                       base::Unretained(host_impl_.get()), std::move(response)),
         delay);
   }
 
- private:
   SEQUENCE_CHECKER(sequence_checker_);
 
   raw_ptr<RenderWidgetHost> widget_ GUARDED_BY_CONTEXT(sequence_checker_);
 
-  // Responses to the next GetCharacterIndexAtPoint calls, with delay.
-  base::queue<std::pair<uint32_t, base::TimeDelta>> index_responses_
-      GUARDED_BY_CONTEXT(sequence_checker_);
-
-  // Responses to the next GetFirstRectForRange calls, with delay.
-  base::queue<std::pair<gfx::Rect, base::TimeDelta>> rect_responses_
+  // Queue of responses to send, with delay.
+  base::queue<std::pair<ResponseType, base::TimeDelta>> responses_
       GUARDED_BY_CONTEXT(sequence_checker_);
 
   // The TextInputHostImpl object must be accessed on the IO thread, but the
@@ -144,8 +196,15 @@ class FakeAsyncRequestDelegate final
 // This test does not test the WebKit side of the dictionary system (which
 // performs the actual data fetching), but rather this just tests that the
 // service's signaling system works.
+//
+// Tests a different sync getter function depending on ResponseType:
+//  * uint32_t -> GetCharacterIndexAtPoint()
+//  * gfx::Rect -> GetFirstRectForRange()
+template <typename ResponseType>
 class TextInputClientMacTest : public content::RenderViewHostTestHarness {
  public:
+  using Delegate = FakeAsyncRequestDelegate<ResponseType>;
+
   TextInputClientMacTest()
       : RenderViewHostTestHarness(BrowserTaskEnvironment::REAL_IO_THREAD) {}
 
@@ -153,7 +212,7 @@ class TextInputClientMacTest : public content::RenderViewHostTestHarness {
     RenderViewHostTestHarness::SetUp();
     RenderViewHostTester::For(rvh())->CreateTestRenderView();
 
-    auto delegate = std::make_unique<FakeAsyncRequestDelegate>(widget());
+    auto delegate = std::make_unique<Delegate>(widget());
     request_delegate_ = delegate.get();
     TextInputClientMac::GetInstance()->SetAsyncRequestDelegateForTesting(
         std::move(delegate));
@@ -176,75 +235,66 @@ class TextInputClientMacTest : public content::RenderViewHostTestHarness {
 
   RenderWidgetHost* widget() { return rvh()->GetWidget(); }
 
-  FakeAsyncRequestDelegate& request_delegate() { return *request_delegate_; }
+  Delegate& request_delegate() { return *request_delegate_; }
 
  private:
-  raw_ptr<FakeAsyncRequestDelegate> request_delegate_;
+  raw_ptr<Delegate> request_delegate_;
 };
+
+TYPED_TEST_SUITE_P(TextInputClientMacTest);
 
 }  // namespace
 
 // Test Cases //////////////////////////////////////////////////////////////////
 
-TEST_F(TextInputClientMacTest, GetCharacterIndex) {
-  const NSUInteger kSuccessValue = 42;
+TYPED_TEST_P(TextInputClientMacTest, SyncGetter_Basic) {
+  using Traits = TestTraits<TypeParam>;
 
-  request_delegate().AddCharacterIndexResponse(kSuccessValue, kTaskDelay);
+  constexpr TypeParam kSuccessValue = Traits::CreateResponse(42);
 
-  NSUInteger index =
-      TextInputClientMac::GetInstance()->GetCharacterIndexAtPoint(
-          widget(), gfx::Point(2, 2));
+  this->request_delegate().AddResponse(kSuccessValue, kTaskDelay);
 
-  EXPECT_EQ(kSuccessValue, index);
+  EXPECT_EQ(Traits::TextInputClientGetSync(this->widget()), kSuccessValue);
 }
 
-TEST_F(TextInputClientMacTest, TimeoutCharacterIndex) {
+TYPED_TEST_P(TextInputClientMacTest, SyncGetter_Timeout) {
+  using Traits = TestTraits<TypeParam>;
+
   base::test::ScopedFeatureList feature_list;
   feature_list.InitAndEnableFeatureWithParameters(features::kTextInputClient,
                                                   {{"ipc_timeout", "300ms"}});
 
-  uint32_t index = TextInputClientMac::GetInstance()->GetCharacterIndexAtPoint(
-      widget(), gfx::Point(2, 2));
-
-  EXPECT_EQ(UINT32_MAX, index);
+  EXPECT_EQ(Traits::TextInputClientGetSync(this->widget()),
+            Traits::kTimeoutResponse);
 }
 
-TEST_F(TextInputClientMacTest, NotFoundCharacterIndex) {
-  const NSUInteger kPreviousValue = 42;
+// Tests that TextInputClient doesn't get confused if TextInputHost sends a
+// response that's also used if a request times out. (eg.
+// GetCharacterIndexAtPoint() can return NSNotFound, which is UINT32_MAX.)
+TYPED_TEST_P(TextInputClientMacTest, SyncGetter_NotFound) {
+  using Traits = TestTraits<TypeParam>;
 
-  // Set an arbitrary value to ensure the index is not |NSNotFound|.
-  request_delegate().AddCharacterIndexResponse(kPreviousValue, kTaskDelay);
+  const TypeParam kPreviousValue = Traits::CreateResponse(42);
 
-  // Set UINT32_MAX to the index |kTaskDelay| msec after the previous setting.
-  request_delegate().AddCharacterIndexResponse(UINT32_MAX, kTaskDelay);
+  // Set an arbitrary value to ensure the response doesn't just default to the
+  // timeout value.
+  this->request_delegate().AddResponse(kPreviousValue, kTaskDelay);
 
-  uint32_t index = TextInputClientMac::GetInstance()->GetCharacterIndexAtPoint(
-      widget(), gfx::Point(2, 2));
-  EXPECT_EQ(kPreviousValue, index);
+  // Set the response to the timeout value after the previous setting.
+  this->request_delegate().AddResponse(Traits::kTimeoutResponse, kTaskDelay);
 
-  index = TextInputClientMac::GetInstance()->GetCharacterIndexAtPoint(
-      widget(), gfx::Point(2, 2));
-  EXPECT_EQ(UINT32_MAX, index);
+  EXPECT_EQ(Traits::TextInputClientGetSync(this->widget()), kPreviousValue);
+  EXPECT_EQ(Traits::TextInputClientGetSync(this->widget()),
+            Traits::kTimeoutResponse);
 }
 
-TEST_F(TextInputClientMacTest, GetRectForRange) {
-  const gfx::Rect kSuccessValue(42, 43, 44, 45);
+REGISTER_TYPED_TEST_SUITE_P(TextInputClientMacTest,
+                            SyncGetter_Basic,
+                            SyncGetter_Timeout,
+                            SyncGetter_NotFound);
 
-  request_delegate().AddFirstRectResponse(kSuccessValue, kTaskDelay);
-
-  gfx::Rect rect = TextInputClientMac::GetInstance()->GetFirstRectForRange(
-      widget(), gfx::Range(NSMakeRange(0, 32)));
-  EXPECT_EQ(kSuccessValue, rect);
-}
-
-TEST_F(TextInputClientMacTest, TimeoutRectForRange) {
-  base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeatureWithParameters(features::kTextInputClient,
-                                                  {{"ipc_timeout", "300ms"}});
-
-  gfx::Rect rect = TextInputClientMac::GetInstance()->GetFirstRectForRange(
-      widget(), gfx::Range(NSMakeRange(0, 32)));
-  EXPECT_EQ(gfx::Rect(), rect);
-}
-
+using ResponseTypes = ::testing::Types<uint32_t, gfx::Rect>;
+INSTANTIATE_TYPED_TEST_SUITE_P(ResponseType,
+                               TextInputClientMacTest,
+                               ResponseTypes);
 }  // namespace content
