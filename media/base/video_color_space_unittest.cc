52477851bf746dadeb198c8194415285dabeffb8
Christopher Cameron <ccameron@chromium.org>
2026-02-03 10:38:56 -0800

    media_unittests: Remove gfx::ColorTransform usage
    A gfx::ColorTransform is not necessary for this test.
    
    Bug: 463702880
    Change-Id: I544dfd911298cfcec0c51b3d20a82ab73893e22b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7541358
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1578920}

--- media/base/video_color_space_unittest.cc
+++ media/base/video_color_space_unittest.cc
@@ -5,7 +5,6 @@
 #include "media/base/video_color_space.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/color_space.h"
-#include "ui/gfx/color_transform.h"
 #include "ui/gfx/geometry/transform.h"
 
 namespace media {
@@ -35,33 +34,16 @@ TEST(VideoColorSpaceTest, PartiallyValid) {
                                         gfx::ColorSpace::RangeID::DERIVED));
 }
 
-TEST(VideoColorSpaceTest, UnknownVideoToSRGB) {
+TEST(VideoColorSpaceTest, UnknownVideo) {
   // Invalid video spaces should be BT709.
   VideoColorSpace invalid_video_color_space = VideoColorSpace(
       VideoColorSpace::PrimaryID::INVALID, VideoColorSpace::TransferID::INVALID,
       VideoColorSpace::MatrixID::INVALID, gfx::ColorSpace::RangeID::LIMITED);
   gfx::ColorSpace unknown = invalid_video_color_space.GuessGfxColorSpace();
-  gfx::ColorSpace sRGB = gfx::ColorSpace::CreateSRGB();
-  std::unique_ptr<gfx::ColorTransform> t(
-      gfx::ColorTransform::NewColorTransform(unknown, sRGB));
-
-  gfx::ColorTransform::TriStim tmp(16.0f / 255.0f, 0.5f, 0.5f);
-  t->Transform(&tmp, 1);
-  EXPECT_NEAR(tmp.x(), 0.0f, 0.001f);
-  EXPECT_NEAR(tmp.y(), 0.0f, 0.001f);
-  EXPECT_NEAR(tmp.z(), 0.0f, 0.001f);
-
-  tmp = gfx::ColorTransform::TriStim(235.0f / 255.0f, 0.5f, 0.5f);
-  t->Transform(&tmp, 1);
-  EXPECT_NEAR(tmp.x(), 1.0f, 0.001f);
-  EXPECT_NEAR(tmp.y(), 1.0f, 0.001f);
-  EXPECT_NEAR(tmp.z(), 1.0f, 0.001f);
-
-  // Test a blue color
-  tmp = gfx::ColorTransform::TriStim(128.0f / 255.0f, 240.0f / 255.0f, 0.5f);
-  t->Transform(&tmp, 1);
-  EXPECT_GT(tmp.z(), tmp.x());
-  EXPECT_GT(tmp.z(), tmp.y());
+  EXPECT_EQ(unknown, gfx::ColorSpace(gfx::ColorSpace::PrimaryID::BT709,
+                                     gfx::ColorSpace::TransferID::BT709,
+                                     gfx::ColorSpace::MatrixID::BT709,
+                                     gfx::ColorSpace::RangeID::LIMITED));
 }
 
 TEST(VideoColorSpaceTest, IsHDR) {
