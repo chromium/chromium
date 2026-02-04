03661b1cd966e51e9f92408271914339afe7aadb
Norge Vizcay <vizcay@google.com>
2026-02-04 03:12:36 -0800

    Add AutofillAiWalletApiClient for Autofill AI and Wallet integration
    This CL introduces the AutofillAiWalletApiClient interface, which serves
    as a bridge for communication between Autofill AI and the Wallet
    backend. It defines methods for upserting and unmasking wallet entities
    using Autofill-specific data models.
    
    Bug: 478783796
    Change-Id: Ib0d749efd9b45a535b11a290f43ac066df3d1bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540137
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1579324}

---
+++ components/autofill/core/browser/network/autofill_ai/wallet_pass_access_manager.h
@@ -0,0 +1,57 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_WALLET_PASS_ACCESS_MANAGER_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_WALLET_PASS_ACCESS_MANAGER_H_
+
+#include "base/functional/callback.h"
+#include "base/types/expected.h"
+#include "components/wallet/core/browser/network/wallet_http_client.h"
+
+namespace autofill {
+
+class EntityId;
+class EntityInstance;
+
+// A client interface that allows Autofill AI to communicate with the Wallet
+// backend via `wallet::WalletHttpClient`.
+// It maps `autofill::EntityInstance`s to `wallet::WalletPass`es and vice versa
+// to issue UpsertPass and GetUnmaskedPass requests.
+class WalletPassAccessManager {
+ public:
+  // Callback for save and update requests. On success, it returns
+  // the masked `EntityInstance` as it is stored in the Wallet backend
+  // (including its `id`). Returns `std::nullopt` on failure.
+  using UpsertEntityInstanceCallback =
+      base::OnceCallback<void(std::optional<EntityInstance>)>;
+
+  // Callback for `GetUnmaskedWalletEntityInstance` requests. On success, it
+  // returns the unmasked `EntityInstance` corresponding to the requested
+  // `entity_id`. Returns `std::nullopt` on failure.
+  using GetUnmaskedEntityInstanceCallback =
+      base::OnceCallback<void(std::optional<EntityInstance>)>;
+
+  virtual ~WalletPassAccessManager() = default;
+
+  // Issues an save request to the Wallet backend for the given `entity`.
+  // Notably, the returned entity will always have a new entity id.
+  virtual void SaveWalletEntityInstance(
+      const EntityInstance& entity,
+      UpsertEntityInstanceCallback callback) = 0;
+
+  // Issues an update request to the Wallet backend for the given `entity`.
+  virtual void UpdateWalletEntityInstance(
+      const EntityInstance& entity,
+      UpsertEntityInstanceCallback callback) = 0;
+
+  // Issues a GetUnmaskedPass request to the Wallet backend for the given
+  // `entity_id`.
+  virtual void GetUnmaskedWalletEntityInstance(
+      const EntityId& entity_id,
+      GetUnmaskedEntityInstanceCallback callback) = 0;
+};
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_NETWORK_AUTOFILL_AI_WALLET_PASS_ACCESS_MANAGER_H_
