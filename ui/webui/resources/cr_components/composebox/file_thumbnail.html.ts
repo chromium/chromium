810a2941f6bd5713480ad3a5fe3592ac9457663a
James Leung <jamesleung@google.com>
2026-02-05 15:54:39 -0800

    [composebox] tab spinners
    * Currently, large tab uploads do not indicate when they are finished
      uploading, leading to user confusion
    * This adds loading spinners for tabs and refactors old code
    
    https://screencast.googleplex.com/cast/NjY0NTg3NjA1NDI5NDUyOHwzYmEzYjQ2Yy00NA
    
    Bug: b:481373366
    Change-Id: I94f400f63266fa77581c1a30241c9b180b013c6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540715
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580478}

--- ui/webui/resources/cr_components/composebox/file_thumbnail.html.ts
+++ ui/webui/resources/cr_components/composebox/file_thumbnail.html.ts
@@ -4,83 +4,89 @@
 
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
 
-import {FileUploadStatus} from './composebox_query.mojom-webui.js';
 import type {ComposeboxFileThumbnailElement} from './file_thumbnail.js';
 
 export function getHtml(this: ComposeboxFileThumbnailElement) {
   // clang-format off
+  const spinner = html`
+      <svg role="image" class="spinner" viewBox="0 0 100 100">
+        <circle class="spinner-circle" cx="50" cy="50" r="40" />
+      </svg>`;
+
   return html`<!--_html_template_start_-->
-<div id="container">
-  ${this.file.url ? html`
-    <div id="tabChip" class="chip">
-      <div id="tabThumbnail" class="thumbnail">
-        <cr-composebox-tab-favicon .url="${this.file.url}" .size="${24}">
-        </cr-composebox-tab-favicon>
-      </div>
-      <div class="tabInfo">
-        <div class="title">${this.file.name}</div>
-        <div class="url">${this.formattedUrl_}</div>
-      </div>
-      <div class="overlay">
-        <div class="gradient-protection"></div>
-        ${this.file.isDeletable ? html`<cr-icon-button
-          id="removeTabButton"
-          class="remove-button"
-          iron-icon="cr:clear"
-          title="${this.file.name}"
-          aria-label="${this.deleteFileButtonTitle_}"
-          @click="${this.deleteFile_}">
+    <div id="container">
+      ${this.file.url ? html`
+        <div id="tabChip" class="chip">
+          <div id="tabThumbnail" class="thumbnail">
+            <cr-composebox-tab-favicon .url="${this.file.url}" .size="${24}">
+            </cr-composebox-tab-favicon>
+            ${this.isUploading_ ? html`
+              <div id="spinnerOverlay">
+                ${spinner}
+              </div>
+            ` : ''}
+          </div>
+
+          <div class="tabInfo">
+            <div class="title">${this.file.name}</div>
+            <div class="url">${this.formattedUrl_}</div>
+          </div>
+          <div class="overlay">
+            <div class="gradient-protection"></div>
+            ${this.file.isDeletable ? html`<cr-icon-button
+              id="removeTabButton"
+              class="remove-button"
+              iron-icon="cr:clear"
+              title="${this.file.name}"
+              aria-label="${this.deleteFileButtonTitle_}"
+              @click="${this.deleteFile_}">
+              </cr-icon-button>`: ''}
+          </div>
+          <div class="chip-overlay"></div>
+        </div>
+      ` : (this.file.objectUrl || this.file.dataUrl) ? html`
+        <div id="imgChip">
+          ${this.isUploading_ ? html`
+              ${spinner}
+          ` : html`
+            <img class="img-thumbnail"
+              src="${this.file.objectUrl || this.file.dataUrl}"
+              aria-label="${this.file.name}">
+          `}
+          ${this.file.isDeletable ? html`<cr-icon-button
+              class="img-overlay"
+              id="removeImgButton"
+              iron-icon="cr:clear"
+              title="${this.file.name}"
+              aria-label="${this.deleteFileButtonTitle_}"
+              @click="${this.deleteFile_}">
           </cr-icon-button>`: ''}
-      </div>
-      <div class="chip-overlay"></div>
-    </div>
-  ` : (this.file.objectUrl || this.file.dataUrl) ? html`
-    <div id="imgChip">
-      ${this.file.status !== FileUploadStatus.kUploadSuccessful ? html`
-        <svg role="image" class="spinner" viewBox="0 0 100 100">
-          <circle class="spinner-circle" cx="50" cy="50" r="40" />
-        </svg>
-      ` : html`
-        <img class="img-thumbnail"
-          src="${this.file.objectUrl || this.file.dataUrl}"
-          aria-label="${this.file.name}">
+        </div>` : html`
+        <div id="pdfChip" class="chip">
+          <div id="pdfThumbnail" class="thumbnail" part="thumbnail">
+            ${this.isUploading_ ? html`
+                ${spinner}`
+              : html`
+                  <cr-icon icon="thumbnail:pdf" class="pdf-icon">
+                  </cr-icon>
+            `}
+          </div>
+          <p class="title" id="pdfTitle">${this.file.name}</p>
+          <div class="overlay">
+            <div class="gradient-protection"></div>
+            ${this.file.isDeletable ? html`<cr-icon-button
+                id="removePdfButton"
+                class="remove-button"
+                iron-icon="cr:clear"
+                title="${this.file.name}"
+                aria-label="${this.deleteFileButtonTitle_}"
+                @click="${this.deleteFile_}">
+            </cr-icon-button>`: ''}
+          </div>
+          <div class="chip-overlay"></div>
+        </div>
       `}
-      ${this.file.isDeletable ? html`<cr-icon-button
-          class="img-overlay"
-          id="removeImgButton"
-          iron-icon="cr:clear"
-          title="${this.file.name}"
-          aria-label="${this.deleteFileButtonTitle_}"
-          @click="${this.deleteFile_}">
-      </cr-icon-button>`: ''}
-    </div>` : html`
-    <div id="pdfChip" class="chip">
-      <div id="pdfThumbnail" class="thumbnail" part="thumbnail">
-        ${this.file.status === FileUploadStatus.kUploadSuccessful ? html`
-          <cr-icon icon="thumbnail:pdf" class="pdf-icon">
-          </cr-icon>
-        ` : html`
-          <svg class="spinner" viewBox="0 0 100 100">
-            <circle class="spinner-circle" cx="50" cy="50" r="40" />
-          </svg>
-        `}
-      </div>
-      <p class="title" id="pdfTitle">${this.file.name}</p>
-      <div class="overlay">
-        <div class="gradient-protection"></div>
-        ${this.file.isDeletable ? html`<cr-icon-button
-            id="removePdfButton"
-            class="remove-button"
-            iron-icon="cr:clear"
-            title="${this.file.name}"
-            aria-label="${this.deleteFileButtonTitle_}"
-            @click="${this.deleteFile_}">
-        </cr-icon-button>`: ''}
-      </div>
-      <div class="chip-overlay"></div>
     </div>
-  `}
-</div>
-<!--_html_template_end_-->`;
+  <!--_html_template_end_-->`;
   // clang-format on
 }
