ab0d98e6be7e9410b08b0de86f9c537657e7094d
Colin Blundell <blundell@chromium.org>
2026-02-05 00:03:21 -0800

    [//cc] Remove OOP raster reference in paint_op.h
    Missed this in [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7535134
    
    Bug: 391648152
    Change-Id: Ib74d42bdd69f413721b695887a2e7596fca51f4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544869
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1579921}

--- cc/paint/paint_op.h
+++ cc/paint/paint_op.h
@@ -609,7 +609,7 @@ class CC_PAINT_EXPORT DrawImageRectOp final
   DrawImageRectOp();
 
   // Scale that has already been applied to the decoded image during
-  // serialization. Used with OOP raster.
+  // serialization. Used with GPU raster.
   SkSize scale_adjustment = SkSize::Make(1.f, 1.f);
 };
 
