c9db74a0d1f15a87f284a0629794634f556845e7
Devon Loehr <dloehr@google.com>
2026-02-05 08:47:57 -0800

    [rustmojo] Use `strum` crate to generate MojoError methods.
    Now that we've gotten the crate approved for use in chromium,
    we can simplify the conversion methods for MojoError.
    
    Bug: 444509367
    Change-Id: Iccc70cfd30d82c5c4dcb1c262aa34388004a03ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543715
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1580160}

--- mojo/public/rust/system/ffi/result.rs
+++ mojo/public/rust/system/ffi/result.rs
@@ -16,11 +16,8 @@ chromium::import! {
 use raw_ffi::MojoResult as MojoResultCode;
 
 // TODO(https://crbug.com/457917334): See if `bindgen` can generate
-// bindings for `MOJO_RESULT_CANCELLED` and use that here instead of
+// bindings for the code definitions and use those here instead of
 // manually hardcoding constants for each variant.
-
-// FOR_RELEASE: See if we can get approval for the `strum` crate and use it here
-// instead of manually writing string/int conversion functions.
 
 /// MojoError represents all failures that can happen as a result of performing
 /// some operation in Mojo.
@@ -29,7 +26,7 @@ use raw_ffi::MojoResult as MojoResultCode;
 /// (//mojo/public/c/system/types.h) so this enum can be used across the FFI
 /// boundary simply by using "as u32" (but do not that the "OK" code is not a
 /// valid value, since this represents errors only).
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, strum::FromRepr, strum::IntoStaticStr)]
 #[repr(u32)]
 pub enum MojoError {
     Cancelled = 1,
@@ -59,49 +56,13 @@ impl MojoError {
     pub fn result_from_code(code: MojoResultCode) -> MojoResult<()> {
         match code {
             0 => Ok(()),
-            1 => Err(MojoError::Cancelled),
-            2 => Err(MojoError::Unknown),
-            3 => Err(MojoError::InvalidArgument),
-            4 => Err(MojoError::DeadlineExceeded),
-            5 => Err(MojoError::NotFound),
-            6 => Err(MojoError::AlreadyExists),
-            7 => Err(MojoError::PermissionDenied),
-            8 => Err(MojoError::ResourceExhausted),
-            9 => Err(MojoError::FailedPrecondition),
-            10 => Err(MojoError::Aborted),
-            11 => Err(MojoError::OutOfRange),
-            12 => Err(MojoError::Unimplemented),
-            13 => Err(MojoError::Internal),
-            14 => Err(MojoError::Unavailable),
-            15 => Err(MojoError::DataLoss),
-            16 => Err(MojoError::Busy),
-            17 => Err(MojoError::ShouldWait),
-            _ => unreachable!("Received unexpected return code from mojo function: {}", code),
+            _ => Err(MojoError::from_repr(code)
+                .expect("Received unexpected return code from mojo function")),
         }
     }
 
     pub fn as_str(&self) -> &'static str {
-        // TODO(https://crbug.com/456535277): Deduplicate MojoResult string
-        // definitions across different language APIs.
-        match *self {
-            MojoError::Cancelled => "Cancelled",
-            MojoError::Unknown => "Unknown",
-            MojoError::InvalidArgument => "Invalid Argument",
-            MojoError::DeadlineExceeded => "Deadline Exceeded",
-            MojoError::NotFound => "Not Found",
-            MojoError::AlreadyExists => "Already Exists",
-            MojoError::PermissionDenied => "Permission Denied",
-            MojoError::ResourceExhausted => "Resource Exhausted",
-            MojoError::FailedPrecondition => "Failed Precondition",
-            MojoError::Aborted => "Aborted",
-            MojoError::OutOfRange => "Out Of Range",
-            MojoError::Unimplemented => "Unimplemented",
-            MojoError::Internal => "Internal",
-            MojoError::Unavailable => "Unavailable",
-            MojoError::DataLoss => "Data Loss",
-            MojoError::Busy => "Busy",
-            MojoError::ShouldWait => "Should Wait",
-        }
+        self.into()
     }
 }
 
