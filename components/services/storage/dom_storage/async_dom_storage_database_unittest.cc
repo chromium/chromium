00501dd894d88944d92263f337e1cf0c703e5e57
Steve Becker <stevebe@microsoft.com>
2026-02-04 21:00:53 -0800

    [DomStorage] Run AsyncDomStorageDatabaseTest with SQLite
    Parameterizes the test to run using both SQLite and LevelDB. Updates
    test expectations for SQLite, which returns `map_id` values when reading
    `MapMetadata`. LevelDB does not assign map IDs, instead using the
    storage key as the map ID.
    
    Bug: 377242771
    Change-Id: I49505a0184b2551dc1707d24c6e89d15b0912531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7540977
    Commit-Queue: Steve Becker <stevebe@microsoft.com>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1579880}

--- components/services/storage/dom_storage/async_dom_storage_database_unittest.cc
+++ components/services/storage/dom_storage/async_dom_storage_database_unittest.cc
@@ -14,8 +14,10 @@
 #include "base/task/thread_pool.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
+#include "base/test/with_feature_override.h"
 #include "components/services/storage/dom_storage/dom_storage_constants.h"
 #include "components/services/storage/dom_storage/dom_storage_database.h"
+#include "components/services/storage/dom_storage/features.h"
 #include "components/services/storage/dom_storage/test_support/dom_storage_database_testing.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/storage_key/storage_key.h"
@@ -29,10 +31,13 @@ constexpr const char kThirdFakeUrlString[] = "https://c-fake-url.test";
 constexpr const char kFourthFakeUrlString[] = "https://d-fake-url.test";
 }  // namespace
 
-class AsyncDomStorageDatabaseTest : public testing::Test {
+class AsyncDomStorageDatabaseTest : public base::test::WithFeatureOverride,
+                                    public testing::Test {
  public:
   AsyncDomStorageDatabaseTest();
   ~AsyncDomStorageDatabaseTest() override = default;
+
+  bool IsSqliteEnabled() const { return GetParam(); }
 
  protected:
   base::test::TaskEnvironment task_environment_;
@@ -42,8 +47,17 @@ class AsyncDomStorageDatabaseTest : public testing::Test {
   const blink::StorageKey kFourthStorageKey;
 };
 
+INSTANTIATE_TEST_SUITE_P(
+    /*no prefix*/,
+    AsyncDomStorageDatabaseTest,
+    testing::Bool(),
+    /*name_generator=*/
+    [](const testing::TestParamInfo<AsyncDomStorageDatabaseTest::ParamType>&
+           info) { return info.param ? "SQLite" : "LevelDB"; });
+
 AsyncDomStorageDatabaseTest::AsyncDomStorageDatabaseTest()
-    : kFirstStorageKey(
+    : base::test::WithFeatureOverride(kDomStorageSqlite),
+      kFirstStorageKey(
           blink::StorageKey::CreateFromStringForTesting(kFirstFakeUrlString)),
       kSecondStorageKey(
           blink::StorageKey::CreateFromStringForTesting(kSecondFakeUrlString)),
@@ -53,10 +67,10 @@ AsyncDomStorageDatabaseTest::AsyncDomStorageDatabaseTest()
           blink::StorageKey::CreateFromStringForTesting(kFourthFakeUrlString)) {
 }
 
-TEST_F(AsyncDomStorageDatabaseTest,
+TEST_P(AsyncDomStorageDatabaseTest,
        WriteAndReadThenDeleteLocalStorageMetadata) {
   // Define test values to write to the database.
-  const DomStorageDatabase::MapMetadata kExpectedMapMetadataArray[] = {
+  const DomStorageDatabase::MapMetadata kInitialMapMetadataArray[] = {
       {
           .map_locator{kLocalStorageSessionId, kFirstStorageKey},
           .last_accessed{base::Time::Now() - base::Days(7)},
@@ -77,8 +91,8 @@ TEST_F(AsyncDomStorageDatabaseTest,
           .total_size{211114},
       },
   };
-  const base::span<const DomStorageDatabase::MapMetadata> kExpectedMapMetadata =
-      kExpectedMapMetadataArray;
+  const base::span<const DomStorageDatabase::MapMetadata> kInitialMapMetadata =
+      kInitialMapMetadataArray;
 
   // Open the database.
   std::unique_ptr<AsyncDomStorageDatabase> database;
@@ -86,11 +100,11 @@ TEST_F(AsyncDomStorageDatabaseTest,
       StorageType::kLocalStorage, &database));
 
   // Write each map's metadata to the database.
-  for (size_t i = 0; i < kExpectedMapMetadata.size(); ++i) {
+  for (size_t i = 0; i < kInitialMapMetadata.size(); ++i) {
     // Write the metadata for a single map.
     DomStorageDatabase::Metadata cloned_metadata;
     cloned_metadata.map_metadata =
-        CloneMapMetadataVector(base::span_from_ref(kExpectedMapMetadata[i]));
+        CloneMapMetadataVector(base::span_from_ref(kInitialMapMetadata[i]));
 
     ASSERT_NO_FATAL_FAILURE(
         PutMetadataSync(*database, std::move(cloned_metadata)));
@@ -100,9 +114,32 @@ TEST_F(AsyncDomStorageDatabaseTest,
     ASSERT_NO_FATAL_FAILURE(ReadAllMetadataSync(*database, &read_metadata));
 
     // Read back the metadata written so far.
-    auto expected_read_metadata = kExpectedMapMetadata.first(/*count=*/i + 1);
+    base::span<const DomStorageDatabase::MapMetadata> written_metadata_span =
+        kInitialMapMetadata.first(/*count=*/i + 1);
+
+    std::vector<DomStorageDatabase::MapMetadata> expected_map_metadata;
+    if (IsSqliteEnabled()) {
+      // Copy `written_metadata_span`, inserting the expected `map_id`.
+      for (size_t j = 0u; j < written_metadata_span.size(); ++j) {
+        const DomStorageDatabase::MapMetadata& written_metadata =
+            written_metadata_span[j];
+
+        expected_map_metadata.push_back({
+            .map_locator{kLocalStorageSessionId,
+                         written_metadata.map_locator.storage_key(),
+                         /*map_id=*/static_cast<int64_t>(j + 1)},
+            .last_accessed = written_metadata.last_accessed,
+            .last_modified = written_metadata.last_modified,
+            .total_size = written_metadata.total_size,
+        });
+      }
+    } else {
+      // LevelDB does not create map IDs, associating maps by storage key only.
+      expected_map_metadata = CloneMapMetadataVector(written_metadata_span);
+    }
+
     ExpectEqualsMapMetadataSpan(read_metadata.map_metadata,
-                                expected_read_metadata);
+                                expected_map_metadata);
 
     // Local storage does not store the next map id number.
     EXPECT_EQ(read_metadata.next_map_id, std::nullopt);
@@ -123,21 +160,44 @@ TEST_F(AsyncDomStorageDatabaseTest,
 
   // Add the second and fourth storage keys as expected.
   std::vector<DomStorageDatabase::MapMetadata> expected_metadata_after_delete;
-  expected_metadata_after_delete.push_back(
-      CloneMapMetadata(kExpectedMapMetadata[1]));
-  expected_metadata_after_delete.push_back(
-      CloneMapMetadata(kExpectedMapMetadata[3]));
+  if (IsSqliteEnabled()) {
+    // Copy the second and fourth `kInitialMapMetadata`, inserting the expected
+    // `map_id`.
+    expected_metadata_after_delete.push_back({
+        .map_locator{kLocalStorageSessionId,
+                     kInitialMapMetadata[1].map_locator.storage_key(),
+                     /*map_id=*/2},
+        .last_accessed = kInitialMapMetadata[1].last_accessed,
+        .last_modified = kInitialMapMetadata[1].last_modified,
+        .total_size = kInitialMapMetadata[1].total_size,
+    });
+
+    expected_metadata_after_delete.push_back({
+        .map_locator{kLocalStorageSessionId,
+                     kInitialMapMetadata[3].map_locator.storage_key(),
+                     /*map_id=*/4},
+        .last_accessed = kInitialMapMetadata[3].last_accessed,
+        .last_modified = kInitialMapMetadata[3].last_modified,
+        .total_size = kInitialMapMetadata[3].total_size,
+    });
+  } else {
+    // LevelDB does not create map IDs, associating maps by storage key only.
+    expected_metadata_after_delete.push_back(
+        CloneMapMetadata(kInitialMapMetadata[1]));
+    expected_metadata_after_delete.push_back(
+        CloneMapMetadata(kInitialMapMetadata[3]));
+  }
 
   ExpectEqualsMapMetadataSpan(read_metadata.map_metadata,
                               expected_metadata_after_delete);
 }
 
-TEST_F(AsyncDomStorageDatabaseTest, EnqueuePendingTasksWhileOpening) {
+TEST_P(AsyncDomStorageDatabaseTest, EnqueuePendingTasksWhileOpening) {
   // Define test values to write to the database.
   const blink::StorageKey kStorageKey =
       blink::StorageKey::CreateFromStringForTesting("https://a-fake-url.test");
 
-  const DomStorageDatabase::MapMetadata kExpectedMapMetadata[] = {
+  const DomStorageDatabase::MapMetadata kInitialMapMetadata[] = {
       {
           .map_locator{kLocalStorageSessionId, kStorageKey},
           .last_modified{base::Time::Now() - base::Seconds(12)},
@@ -155,7 +215,7 @@ TEST_F(AsyncDomStorageDatabaseTest, EnqueuePendingTasksWhileOpening) {
   // Immediately start using the database, which will enqueue pending tasks
   // while opening.
   DomStorageDatabase::Metadata cloned_metadata(
-      CloneMapMetadataVector(kExpectedMapMetadata));
+      CloneMapMetadataVector(kInitialMapMetadata));
 
   base::test::TestFuture<DbStatus> write_status_future;
   database->PutMetadata(std::move(cloned_metadata),
@@ -174,11 +234,23 @@ TEST_F(AsyncDomStorageDatabaseTest, EnqueuePendingTasksWhileOpening) {
   EXPECT_TRUE(write_status.ok()) << write_status.ToString();
   ASSERT_TRUE(metadata.has_value()) << metadata.error().ToString();
 
-  ExpectEqualsMapMetadataSpan(metadata->map_metadata, kExpectedMapMetadata);
+  std::vector<DomStorageDatabase::MapMetadata> expected_map_metadata;
+  if (IsSqliteEnabled()) {
+    // Copy `kInitialMapMetadata`, inserting the expected map ID.
+    expected_map_metadata.push_back(
+        {.map_locator{kLocalStorageSessionId, kStorageKey, /*map_id=*/1},
+         .last_modified = kInitialMapMetadata[0].last_modified,
+         .total_size = kInitialMapMetadata[0].total_size});
+  } else {
+    // LevelDB does not create map IDs, associating maps by storage key only.
+    expected_map_metadata.push_back(CloneMapMetadata(kInitialMapMetadata[0]));
+  }
+
+  ExpectEqualsMapMetadataSpan(metadata->map_metadata, expected_map_metadata);
   EXPECT_EQ(metadata->next_map_id, std::nullopt);
 }
 
-TEST_F(AsyncDomStorageDatabaseTest, MapLocatorToDebugStringWithoutSessions) {
+TEST_P(AsyncDomStorageDatabaseTest, MapLocatorToDebugStringWithoutSessions) {
   DomStorageDatabase::MapLocator map_locator{"session_id1", kFirstStorageKey,
                                              /*map_id=*/216};
   map_locator.RemoveSession("session_id1");
@@ -188,7 +260,7 @@ TEST_F(AsyncDomStorageDatabaseTest, MapLocatorToDebugStringWithoutSessions) {
             "chain bit: Same-Site }, map_id:216");
 }
 
-TEST_F(AsyncDomStorageDatabaseTest,
+TEST_P(AsyncDomStorageDatabaseTest,
        MapLocatorToDebugStringWithMultipleSessions) {
   DomStorageDatabase::MapLocator map_locator{"session_id1", kFirstStorageKey,
                                              /*map_id=*/216};
@@ -200,7 +272,7 @@ TEST_F(AsyncDomStorageDatabaseTest,
             "map_id:216");
 }
 
-TEST_F(AsyncDomStorageDatabaseTest, MapLocatorToDebugStringWithoutMapId) {
+TEST_P(AsyncDomStorageDatabaseTest, MapLocatorToDebugStringWithoutMapId) {
   DomStorageDatabase::MapLocator map_locator{"session_id1", kFirstStorageKey};
   EXPECT_EQ(map_locator.ToDebugString(),
             "sessions_ids:session_id1, storage_key:{ origin: "
