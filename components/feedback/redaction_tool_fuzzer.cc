61ebb4f402f52cb7b66afb2485e215768dea6b65
Kalvin Lee <kdlee@chromium.org>
2026-02-05 19:07:32 -0800

    redaction_tool_fuzzer: Prefer `base::HeapArray`
    This idiom is to be preferred over the array form of `std::unique_ptr`.
    More importantly, it
    
    *   removes both `UNSAFE_TODO()`s from this fuzzer and
    
    *   opens up the way for migration to `std::string_view` (in later
        spanification CLs).
    
    Bug: 439455382
    Change-Id: Iae3200e17b6b4316900888c78f44c592cd948b11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545042
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1580573}

--- components/feedback/redaction_tool_fuzzer.cc
+++ components/feedback/redaction_tool_fuzzer.cc
@@ -8,42 +8,50 @@
 #include <stddef.h>
 #include <stdint.h>
 
-#include <memory>
 #include <string>
 #include <vector>
 
 #include "base/compiler_specific.h"
+#include "base/containers/heap_array.h"
+
+namespace {
+
+constexpr int kArbitraryMaxNameLength = 4096;
+
+}  // namespace
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   FuzzedDataProvider provider(data, size);
 
   int first_party_extension_id_count = provider.ConsumeIntegralInRange(-1, 50);
-  // This is the storage for the strings inside first_party_extension_ids. This
-  // is to make sure the char *'s we pass to the RedactionTool constructor are
-  // deleted correctly -- they must be deleted after redactor is destructed, but
+  // This is the storage for the strings inside
+  // `unowned_first_party_extension_ids`. This is to make sure the
+  // char *'s we pass to the RedactionTool constructor are deleted
+  // correctly -- they must be deleted after redactor is destructed, but
   // not leaked.
   std::vector<std::string> first_party_extension_id_store;
+
   // The first_party_extension_ids we pass to the RedactionTool constructor.
   // This owns the array but not the pointed-to strings. Note that if
   // first_party_extension_id_count is -1, this is not set so we pass nullptr to
   // the constructor; that's deliberate.
-  std::unique_ptr<const char*[]> first_party_extension_ids;
+  base::HeapArray<const char*> unowned_first_party_extension_ids;
+
   if (first_party_extension_id_count >= 0) {
     first_party_extension_id_store.reserve(first_party_extension_id_count);
-    first_party_extension_ids =
-        std::make_unique<const char*[]>(first_party_extension_id_count + 1);
+    unowned_first_party_extension_ids = base::HeapArray<const char*>::WithSize(
+        first_party_extension_id_count + 1);
+
     for (int i = 0; i < first_party_extension_id_count; ++i) {
-      constexpr int kArbitraryMaxNameLength = 4096;
       first_party_extension_id_store.emplace_back(
           provider.ConsumeRandomLengthString(kArbitraryMaxNameLength));
-      UNSAFE_TODO(first_party_extension_ids[i]) =
+      unowned_first_party_extension_ids[i] =
           first_party_extension_id_store[i].c_str();
     }
-    UNSAFE_TODO(first_party_extension_ids[first_party_extension_id_count]) =
-        nullptr;
+    unowned_first_party_extension_ids[first_party_extension_id_count] = nullptr;
   }
 
-  redaction::RedactionTool redactor(first_party_extension_ids.get());
+  redaction::RedactionTool redactor(unowned_first_party_extension_ids.data());
   redactor.EnableCreditCardRedaction(true);
   redactor.Redact(provider.ConsumeRemainingBytesAsString());
   return 0;
