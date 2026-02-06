967f151eb2c498eed0955bc2132c0388ab750e1b
Jordan Bayles <jophba@chromium.org>
2026-02-06 13:38:13 -0800

    [codehealth] Autogenerate kLayoutToChannels
    This patch updates the channel_layout.cc file, replacing the
    manually populated kLayoutToChannels array with a compile-time
    auto generated version, based on kChannelOrderings.
    
    This will hopefully make adding new channel layouts easier.
    
    Change-Id: I1475ac577ae773d9c29eb7c2f80b3f43968051a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543273
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581100}

--- media/base/channel_layout.cc
+++ media/base/channel_layout.cc
@@ -18,45 +18,6 @@ namespace media {
 
 namespace {
 
-constexpr auto kLayoutToChannels = std::to_array<int>({
-    0,  // CHANNEL_LAYOUT_NONE
-    0,  // CHANNEL_LAYOUT_UNSUPPORTED
-    1,  // CHANNEL_LAYOUT_MONO
-    2,  // CHANNEL_LAYOUT_STEREO
-    3,  // CHANNEL_LAYOUT_2_1
-    3,  // CHANNEL_LAYOUT_SURROUND
-    4,  // CHANNEL_LAYOUT_4_0
-    4,  // CHANNEL_LAYOUT_2_2
-    4,  // CHANNEL_LAYOUT_QUAD
-    5,  // CHANNEL_LAYOUT_5_0
-    6,  // CHANNEL_LAYOUT_5_1
-    5,  // CHANNEL_LAYOUT_5_0_BACK
-    6,  // CHANNEL_LAYOUT_5_1_BACK
-    7,  // CHANNEL_LAYOUT_7_0
-    8,  // CHANNEL_LAYOUT_7_1
-    8,  // CHANNEL_LAYOUT_7_1_WIDE
-    2,  // CHANNEL_LAYOUT_STEREO_DOWNMIX
-    3,  // CHANNEL_LAYOUT_2POINT1
-    4,  // CHANNEL_LAYOUT_3_1
-    5,  // CHANNEL_LAYOUT_4_1
-    6,  // CHANNEL_LAYOUT_6_0
-    6,  // CHANNEL_LAYOUT_6_0_FRONT
-    6,  // CHANNEL_LAYOUT_HEXAGONAL
-    7,  // CHANNEL_LAYOUT_6_1
-    7,  // CHANNEL_LAYOUT_6_1_BACK
-    7,  // CHANNEL_LAYOUT_6_1_FRONT
-    7,  // CHANNEL_LAYOUT_7_0_FRONT
-    8,  // CHANNEL_LAYOUT_7_1_WIDE_BACK
-    8,  // CHANNEL_LAYOUT_OCTAGONAL
-    0,  // CHANNEL_LAYOUT_DISCRETE
-    3,  // CHANNEL_LAYOUT_STEREO_AND_KEYBOARD_MIC, deprecated
-    5,  // CHANNEL_LAYOUT_4_1_QUAD_SIDE
-    0,  // CHANNEL_LAYOUT_BITSTREAM
-    6,  // CHANNEL_LAYOUT_5_1_4 (downmixed to 5.1)
-    2,  // CHANNEL_LAYOUT_1_1
-    4,  // CHANNEL_LAYOUT_3_1_BACK
-});
-
 // The channel orderings for each layout as specified by FFmpeg. Each value
 // represents the index of each channel in each layout.  Values of -1 mean the
 // channel at that index is not used for that layout. For example, the left side
@@ -181,6 +142,16 @@ constexpr std::array<std::array<int, CHANNELS_MAX + 1>, CHANNEL_LAYOUT_MAX + 1>
         // CHANNEL_LAYOUT_3_1_BACK
         {0, 1, -1, 2, -1, -1, -1, -1, 3, -1, -1},
     }};
+
+constexpr auto kLayoutToChannels = []() {
+  std::array<int, CHANNEL_LAYOUT_MAX + 1> counts;
+  std::ranges::transform(
+      kChannelOrderings, counts.begin(), [](const auto& row) {
+        return static_cast<int>(
+            std::ranges::count_if(row, [](int c) { return c != -1; }));
+      });
+  return counts;
+}();
 
 // Helper to compute bitmask for a layout at compile-time.
 constexpr ChannelMask ComputeChannelMask(ChannelLayout layout) {
