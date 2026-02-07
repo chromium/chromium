956ab52594464e6ab2be594c704a9a0954638083
Alison Gale <agale@chromium.org>
2026-02-06 17:03:37 -0800

    [Vertical Tabs] Fix close button crash by reordering hide
    We need to hide the close button before the collection node initiates
    the close of the tab to avoid the close button being destroyed before it
    is hidden.
    
    Bug: 482403889
    Change-Id: I8e282208b2d517c89511543a23fb32912139b033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7551154
    Reviewed-by: Dominic Austria <dominicaustria@google.com>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581217}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_view.cc
@@ -757,13 +757,13 @@ void VerticalTabView::CloseButtonPressed(const ui::Event& event) {
                                                    : "CloseTab_EndTabInSplit"));
   }
 
-  if (collection_node_) {
-    collection_node_->GetController()->CloseTab(GetTabInterface());
-  }
-
   // Hide the interactive close button while the tab is animating out.
   if (close_button_) {
     close_button_->SetVisible(false);
+  }
+
+  if (collection_node_) {
+    collection_node_->GetController()->CloseTab(GetTabInterface());
   }
 }
 
