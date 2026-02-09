b2aeb1683a1e8bfb6e9b96da669f3b6e40fa2815
Joe Peplowski <joepeplowski@google.com>
2026-02-09 08:56:46 -0800

    Add GeminiStartupConfiguration
    This object will be used in future CLs in order to configure GCRGemini
    on startup instead of waiting for the floaty to be invoked.
    
    Bug: 476399222
    Change-Id: Iaf13f95658f46fd7fed50aa7c2f6d72409f9dcfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548230
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Reviewed-by: Joemer Ramos <joemerramos@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1581795}

---
+++ ios/chrome/browser/intelligence/bwg/model/gemini_startup_configuration.h
@@ -0,0 +1,26 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INTELLIGENCE_BWG_MODEL_GEMINI_STARTUP_CONFIGURATION_H_
+#define IOS_CHROME_BROWSER_INTELLIGENCE_BWG_MODEL_GEMINI_STARTUP_CONFIGURATION_H_
+
+#import <Foundation/Foundation.h>
+
+class AuthenticationService;
+
+@protocol BWGGatewayProtocol;
+
+// GeminiStartupConfiguration is a configuration class that holds all the data
+// necessary to configure Gemini at startup.
+@interface GeminiStartupConfiguration : NSObject
+
+// The authentication service to be used.
+@property(nonatomic, assign) AuthenticationService* authService;
+
+// The BWG gateway for bridging internal protocols.
+@property(nonatomic, weak) id<BWGGatewayProtocol> gateway;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_INTELLIGENCE_BWG_MODEL_GEMINI_STARTUP_CONFIGURATION_H_
---
+++ ios/chrome/browser/intelligence/bwg/model/gemini_startup_configuration.mm
@@ -0,0 +1,8 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/intelligence/bwg/model/gemini_startup_configuration.h"
+
+@implementation GeminiStartupConfiguration
+@end
