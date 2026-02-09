979601ec893aeeca63ead0a4a0316893667274dd
Nohemi Fernandez <fernandex@google.com>
2026-02-09 02:34:59 -0800

    [iOS] Update strings for Reading Mode Content Settings section.
    Updates the strings for Reading Mode Content Settings options and turns
    on the new screen by default behind a kill-switch.
    
    Bug: 463918572
    Change-Id: Ie2669b50aa5da9dec0b7133e0fac456e78fb1c3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548151
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Nohemi Fernandez <fernandex@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1581620}

--- ios/chrome/browser/reader_mode/model/features.mm
+++ ios/chrome/browser/reader_mode/model/features.mm
@@ -41,7 +41,7 @@ BASE_FEATURE(kEnableReaderModeOptimizationGuideEligibility,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableContentSettingsOptionForLinks,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsReaderModeAvailable() {
   if (IsUSCountryCode() &&
