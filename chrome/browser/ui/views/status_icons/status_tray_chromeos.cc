bb46938679a5656327bda041ea00ba964417762a
Zoraiz Naeem <zoraiznaeem@chromium.org>
2026-02-10 13:50:20 -0800

    [M145] Allow-list custom icons for the ChromeOS status area.
    This change introduces an allow-list to control which icons from the
    Chrome browser can be added to the Ash status area (the system tray).
    
    Currently, StatusTray in ChromeOS has limited real estate and the OWNERS
    of StatusTray in ChromeOS want a mechanism to control what can be added.
    
    (cherry picked from commit 88db807246fbfc1e87f99bf5a67792d1e1a9108c)
    
    Bug: b:463430271
    Change-Id: I7aac42dd1a924e7bb7e732c1ff8bb67bce0cc2b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543387
    Auto-Submit: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1580484}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7561649
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7632@{#2388}
    Cr-Branched-From: 0bbdf2913883391365383b0a5dfe7bf9fd1a5213-refs/heads/main@{#1568190}

--- chrome/browser/ui/views/status_icons/status_tray_chromeos.cc
+++ chrome/browser/ui/views/status_icons/status_tray_chromeos.cc
@@ -6,14 +6,32 @@
 
 #include <memory>
 
+#include "ash/constants/chrome_custom_icon_catalog.h"
+#include "base/notreached.h"
 #include "chrome/browser/ui/views/status_icons/status_icon_chromeos.h"
 #include "chrome/common/chrome_features.h"
 
 namespace {
 
+using ChromeCustomIconCatalogName = ash::ChromeCustomIconCatalogName;
+
 constexpr int64_t kBaseIconId = 2;
 int64_t ReservedIconId(StatusTray::StatusIconType type) {
   return kBaseIconId + static_cast<int64_t>(type);
+}
+
+ChromeCustomIconCatalogName GetCatalogName(StatusTray::StatusIconType type) {
+  switch (type) {
+    case StatusTray::GLIC_ICON:
+      return ChromeCustomIconCatalogName::kGlic;
+    case StatusTray::NOTIFICATION_TRAY_ICON:
+    case StatusTray::MEDIA_STREAM_CAPTURE_ICON:
+    case StatusTray::BACKGROUND_MODE_ICON:
+    case StatusTray::OTHER_ICON:
+      return ChromeCustomIconCatalogName::kNotSupported;
+    default:
+      NOTREACHED();
+  }
 }
 
 }  // namespace
@@ -26,6 +44,12 @@ std::unique_ptr<StatusIcon> StatusTrayChromeOS::CreatePlatformStatusIcon(
     StatusIconType type,
     const gfx::ImageSkia& image,
     const std::u16string& tool_tip) {
+  // To add icons of other `StatusIconType` to StatusTray of ChromeOS, please
+  // update a new entry in `ash::ChromeCustomIconCatalogName`.
+  if (GetCatalogName(type) == ChromeCustomIconCatalogName::kNotSupported) {
+    return nullptr;
+  }
+
   int64_t next_icon_id;
   if (type == StatusTray::OTHER_ICON) {
     next_icon_id = NextIconId();
