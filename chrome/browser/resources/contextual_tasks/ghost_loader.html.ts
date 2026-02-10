e447c67763a7d0121656906bcf8a7e8f1e5d1612
rbpotter <rbpotter@chromium.org>
2026-02-10 12:50:03 -0800

    [145] WebUI: Remove logic from HTML template files in contextual_tasks
    Original change's description:
    > WebUI: Remove logic from HTML template files in contextual_tasks
    > 
    > See styleguide:
    > https://chromium.googlesource.com/chromium/src/+/main/docs/webui/webui_lit_style_guide.md#Logic_free-Templates
    > 
    > This is in preparation for adding an automated check that will detect
    > cases like this with methods/variables defined in template files.
    > 
    > Bug: 481419338
    > Change-Id: Icea525786635100c3629f2388c1431a69596f612
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7548857
    > Reviewed-by: Jason Hu <hujasonx@google.com>
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1580886}
    
    Bug: 482802660
    Change-Id: Icea525786635100c3629f2388c1431a69596f612
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562450
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7632@{#2373}
    Cr-Branched-From: 0bbdf2913883391365383b0a5dfe7bf9fd1a5213-refs/heads/main@{#1568190}

--- chrome/browser/resources/contextual_tasks/ghost_loader.html.ts
+++ chrome/browser/resources/contextual_tasks/ghost_loader.html.ts
@@ -6,38 +6,16 @@ import '/strings.m.js';
 
 import {html} from '//resources/lit/v3_0/lit.rollup.js';
 
+import {getBottomDelay, getPairDelays, getSecondGroupDelay, ROW_DELAYS} from './ghost_loader.js';
 import type {GhostLoaderElement} from './ghost_loader.js';
-
-// All delays are calculated modulo this value.
-const delayMod = 2000;
-// Delay for the tall element of the first group of each row.
-const rowDelays = [0, 800, 1600];
-// Delay between each group in a row.
-const delayBetweenGroups = 200;
-// Delays added to each pair of groups in a row. Add elements to add pairs.
-const pairDelays = [0, 2 * delayBetweenGroups % delayMod];
-// Delay between the top element and the bottom element in a group.
-const bottomDelay = 400;
-
-function getpairDelays(initialDelay: number) {
-  return pairDelays.map((x) => (initialDelay + x) % delayMod);
-}
-
-function getSecondGroupDelay(pairDelay: number) {
-  return (pairDelay + delayBetweenGroups) % delayMod;
-}
-
-function getBottomDelay(topDelay: number) {
-  return (topDelay + bottomDelay) % delayMod;
-}
 
 // clang-format off
 export function getHtml(this: GhostLoaderElement) {
   return html`<!--_html_template_start_-->
   <div class="container">
-${rowDelays.map(initialDelay => html`
+${ROW_DELAYS.map(initialDelay => html`
     <div class="row">
-  ${getpairDelays(initialDelay).map(pairDelay => html`
+  ${getPairDelays(initialDelay).map(pairDelay => html`
       <div class="line-container-group">
         <div class="line-container tall delay${pairDelay}">
           <div class="line bg"></div>
--- chrome/browser/resources/contextual_tasks/ghost_loader.ts
+++ chrome/browser/resources/contextual_tasks/ghost_loader.ts
@@ -7,6 +7,29 @@ import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 import {getCss} from './ghost_loader.css.js';
 import {getHtml} from './ghost_loader.html.js';
 
+
+// All delays are calculated modulo this value.
+const DELAY_MOD = 2000;
+// Delay for the tall element of the first group of each row.
+export const ROW_DELAYS = [0, 800, 1600];
+// Delay between each group in a row.
+const DELAY_BETWEEN_GROUPS = 200;
+// Delays added to each pair of groups in a row. Add elements to add pairs.
+const PAIR_DELAYS = [0, 2 * DELAY_BETWEEN_GROUPS % DELAY_MOD];
+// Delay between the top element and the bottom element in a group.
+const BOTTOM_DELAY = 400;
+
+export function getPairDelays(initialDelay: number) {
+  return PAIR_DELAYS.map((x) => (initialDelay + x) % DELAY_MOD);
+}
+
+export function getSecondGroupDelay(pairDelay: number) {
+  return (pairDelay + DELAY_BETWEEN_GROUPS) % DELAY_MOD;
+}
+
+export function getBottomDelay(topDelay: number) {
+  return (topDelay + BOTTOM_DELAY) % DELAY_MOD;
+}
 export class GhostLoaderElement extends CrLitElement {
   static get is() {
     return 'ghost-loader';
