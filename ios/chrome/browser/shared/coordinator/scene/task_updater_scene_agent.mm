e7608ff0a1232f3e925e76dcabdb038b98e48cf1
Federica Germinario <fedegermi@google.com>
2026-02-10 07:28:15 -0800

    [ios] Trigger update to TaskExecutionUIReady
    Make sure that TaskExecutionUIReady is called only when the following
    conditions are true:
     - UI is enabled
     - There is no currentUIBlocker
     - Scene level is SceneActivationLevelForegroundActive
     - We're not presenting a modal overlay
     - Profile state is kFinal
     - If sign-in is required by policy, there is a signed-in account
    
    Note that most of the conditions are coming from:
    - (BOOL)canHandleIntents inside scene_controller.mm
    with the exception of currentUIBlocker.
    
    Bug: 462018636
    Change-Id: I79f1135d303546cfb893123043ff90a43040d434
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7544581
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1582506}

--- ios/chrome/browser/shared/coordinator/scene/task_updater_scene_agent.mm
+++ ios/chrome/browser/shared/coordinator/scene/task_updater_scene_agent.mm
@@ -9,8 +9,12 @@
 #import "ios/chrome/app/profile/profile_state_observer.h"
 #import "ios/chrome/app/task_orchestrator.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
+#import "ios/chrome/browser/signin/model/authentication_service.h"
+#import "ios/chrome/browser/signin/model/authentication_service_factory.h"
+#import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 
-@interface TaskUpdaterSceneAgent () <ProfileStateObserver>
+@interface TaskUpdaterSceneAgent () <ProfileStateObserver,
+                                     UIBlockerManagerObserver>
 @end
 
 @implementation TaskUpdaterSceneAgent
@@ -20,6 +24,7 @@
 - (void)setSceneState:(SceneState*)sceneState {
   [super setSceneState:sceneState];
   [self.sceneState.profileState addObserver:self];
+  [self.sceneState.profileState addUIBlockerManagerObserver:self];
 
   // Make sure that the execution stage is updated also if a scene is connected
   // after the ProfileState has reached stage ProfileInitStage::kProfileLoaded
@@ -37,14 +42,42 @@
                fromInitStage:(ProfileInitStage)fromInitStage {
   if (nextInitStage == ProfileInitStage::kProfileLoaded) {
     [self updateToProfileLoaded];
+  } else if (nextInitStage == ProfileInitStage::kFinal) {
+    [self maybeUpdateToUIReady];
   }
+}
+
+#pragma mark - UIBlockerManagerObserver
+
+- (void)currentUIBlockerRemoved {
+  [self maybeUpdateToUIReady];
 }
 
 #pragma mark - SceneStateObserver
 
+- (void)sceneState:(SceneState*)sceneState
+    transitionedToActivationLevel:(SceneActivationLevel)level {
+  [self maybeUpdateToUIReady];
+}
+
+- (void)sceneStateDidHideModalOverlay:(SceneState*)sceneState {
+  [self maybeUpdateToUIReady];
+}
+
+- (void)sceneStateDidEnableUI:(SceneState*)sceneState {
+  [self maybeUpdateToUIReady];
+}
+
 - (void)sceneStateDidDisableUI:(SceneState*)sceneState {
   [self.sceneState.profileState removeObserver:self];
   [self.sceneState removeObserver:self];
+  [self.sceneState.profileState removeUIBlockerManagerObserver:self];
+}
+
+- (void)signinDidEnd:(SceneState*)sceneState {
+  // Handle intents after sign-in is done when the forced sign-in policy
+  // is enabled.
+  [self maybeUpdateToUIReady];
 }
 
 #pragma mark - Private
@@ -54,6 +87,60 @@
   [self.sceneState.profileState.appState.taskOrchestrator
       updateToStage:TaskExecutionStage::TaskExecutionProfileLoaded
            forScene:self.sceneState.sceneSessionID];
+}
+
+// Updates the scene to TaskExecutionUIReady if conditions are met.
+- (void)maybeUpdateToUIReady {
+  if (![self canUpdateToUIReady]) {
+    return;
+  }
+  [self.sceneState.profileState.appState.taskOrchestrator
+      updateToStage:TaskExecutionStage::TaskExecutionUIReady
+           forScene:self.sceneState.sceneSessionID];
+}
+
+// YES if UI is ready to handle tasks.
+- (BOOL)canUpdateToUIReady {
+  SceneState* sceneState = self.sceneState;
+  if (!sceneState.UIEnabled) {
+    return NO;
+  }
+  if (sceneState.profileState.currentUIBlocker) {
+    return NO;
+  }
+  if (sceneState.activationLevel < SceneActivationLevelForegroundActive) {
+    return NO;
+  }
+  if (sceneState.presentingModalOverlay) {
+    return NO;
+  }
+  if (sceneState.profileState.initStage < ProfileInitStage::kFinal) {
+    return NO;
+  }
+  if ([self signinStatusInSyncWithPolicy]) {
+    return NO;
+  }
+  return YES;
+}
+
+// YES if the UI should wait for an authentication flow forced by policy.
+- (BOOL)signinStatusInSyncWithPolicy {
+  ProfileIOS* profile = self.sceneState.profileState.profile;
+  AuthenticationService* authService =
+      AuthenticationServiceFactory::GetForProfile(profile);
+  if (authService->GetServiceStatus() !=
+      AuthenticationService::ServiceStatus::SigninForcedByPolicy) {
+    return NO;
+  }
+  // Wait for sign-in to complete before handling UI tasks.
+  if (self.sceneState.signinInProgress) {
+    return YES;
+  }
+
+  // UI tasks can be handled if there is already a primary account signed in.
+  signin::IdentityManager* identityManager =
+      IdentityManagerFactory::GetForProfile(profile);
+  return !identityManager->HasPrimaryAccount(signin::ConsentLevel::kSignin);
 }
 
 @end
