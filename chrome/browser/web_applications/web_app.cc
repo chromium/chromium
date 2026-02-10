b24c4688ffa3d89eaa052085acafba1a776d1fab
Nate Chapin <japhet@chromium.org>
2026-02-10 11:55:02 -0800

    [PWA] Remove query/fragment from scope url in WebApp's constructor
    The WebApp constructor previously CHECKed that the scope url
    doesn't have a query or fragment component. This required callers
    to remove them. Have the WebApp constructor do the removal instead.
    
    Fixed: 482462843
    Change-Id: I3f273669b6dca0895cc9a284ea4cbdee6dc44c73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7560634
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1582708}

--- chrome/browser/web_applications/web_app.cc
+++ chrome/browser/web_applications/web_app.cc
@@ -270,11 +270,18 @@ WebApp::WebApp(const webapps::AppId& app_id,
       << manifest_id_.spec() << " vs " << start_url_.spec();
   CHECK(url::IsSameOriginWith(start_url_, scope_))
       << start_url_.spec() << " vs " << scope_.spec();
-  CHECK(!scope_.has_ref() && !scope_.has_query());
   CHECK(!manifest_id_.has_ref());
+
+  // Must drop the fragments and queries per `scope` rules
+  // https://w3c.github.io/manifest/#scope-member
+  GURL::Replacements replacements;
+  replacements.ClearRef();
+  replacements.ClearQuery();
+  scope_ = scope.ReplaceComponents(replacements);
   CHECK(base::StartsWith(start_url_.spec(), scope_.spec(),
                          base::CompareCase::SENSITIVE))
       << "Start URL " << start_url_ << " must be nested in scope " << scope_;
+
   // Ensure sync proto is initialized.
   SetSyncProto(sync_proto_);
 }
