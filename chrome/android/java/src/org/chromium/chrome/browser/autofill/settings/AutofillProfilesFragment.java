ad855864062cf9d987da81b4dd4b64b828e02000
Bruno Braga <brunobraga@google.com>
2026-02-12 05:43:00 -0800

    Revert "[Autofill Ai - M4] Render entities list"
    This reverts commit f14f7e21b52b86140f509a040a56e157deea82db.
    
    Reason for revert: b/483858148
    
    Original change's description:
    > [Autofill Ai - M4] Render entities list
    >
    > screenshot: https://screenshot.googleplex.com/6J3jiEp9TUzk5SH
    >
    > Bug: 476754838
    > Change-Id: I80a7dd87db81af082c68efefd491fe907744efb9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7562053
    > Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    > Commit-Queue: Bruno Braga <brunobraga@google.com>
    > Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1583121}
    
    Bug: 476754838
    Change-Id: I844dbaf4d859b07bdfdef661567753b69cb0409c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571342
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1583903}

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/settings/AutofillProfilesFragment.java
@@ -16,7 +16,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.preference.Preference;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 
 import org.chromium.base.ApiCompatibilityUtils;
@@ -35,8 +34,6 @@ import org.chromium.chrome.browser.autofill.AutofillFallbackSurfaceLauncher;
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
 import org.chromium.chrome.browser.autofill.PersonalDataManagerFactory;
 import org.chromium.chrome.browser.autofill.SaveUpdateAddressProfilePromptMode;
-import org.chromium.chrome.browser.autofill.autofill_ai.EntityDataManager;
-import org.chromium.chrome.browser.autofill.autofill_ai.EntityDataManagerFactory;
 import org.chromium.chrome.browser.autofill.editors.address.AddressEditorCoordinator;
 import org.chromium.chrome.browser.autofill.editors.address.AddressEditorCoordinator.Delegate;
 import org.chromium.chrome.browser.autofill.editors.address.EditorDialogView;
@@ -56,7 +53,6 @@ import org.chromium.chrome.browser.sync.SyncServiceFactory;
 import org.chromium.components.autofill.AutofillProfile;
 import org.chromium.components.autofill.FieldType;
 import org.chromium.components.autofill.RecordType;
-import org.chromium.components.autofill.autofill_ai.EntityInstanceWithLabels;
 import org.chromium.components.browser_ui.settings.CardWithButtonPreference;
 import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.SettingsFragment;
@@ -69,11 +65,6 @@ import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.components.sync.SyncService;
 import org.chromium.components.sync.UserSelectableType;
 import org.chromium.components.user_prefs.UserPrefs;
-
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 /** Autofill profiles fragment, which allows the user to edit autofill profiles. */
 @NullMarked
@@ -226,7 +217,6 @@ public class AutofillProfilesFragment extends ChromeBaseSettingsFragment
             addPlusAddressesPreference(screen);
         }
         // LINT.ThenChange(:DynamicPreferences)
-        addAutofillAiEntities(screen);
 
         updateDynamicPreferences(getProfile());
     }
@@ -361,36 +351,6 @@ public class AutofillProfilesFragment extends ChromeBaseSettingsFragment
         // LINT.ThenChange(:DynamicAddPlusAddressesPreference)
 
         screen.addPreference(pref);
-    }
-
-    private void addAutofillAiEntities(PreferenceScreen screen) {
-        EntityDataManager entityDataManager = EntityDataManagerFactory.getForProfile(getProfile());
-        List<EntityInstanceWithLabels> entities = entityDataManager.getEntitiesWithLabels();
-        if (entities.isEmpty()) {
-            return;
-        }
-
-        Map<String, List<EntityInstanceWithLabels>> groupedEntities = new LinkedHashMap<>();
-        for (EntityInstanceWithLabels entity : entities) {
-            groupedEntities
-                    .computeIfAbsent(entity.getEntityInstanceLabel(), k -> new ArrayList<>())
-                    .add(entity);
-        }
-
-        for (Map.Entry<String, List<EntityInstanceWithLabels>> entry : groupedEntities.entrySet()) {
-            PreferenceCategory category = new PreferenceCategory(getStyledContext());
-            category.setTitle(entry.getKey());
-            category.setKey(entry.getKey());
-            screen.addPreference(category);
-
-            for (EntityInstanceWithLabels entity : entry.getValue()) {
-                Preference pref = new Preference(getStyledContext());
-                pref.setTitle(entity.getEntityInstanceLabel());
-                pref.setSummary(entity.getEntityInstanceSubLabel());
-                pref.setKey(entity.getGuid());
-                category.addPreference(pref);
-            }
-        }
     }
 
     @Override
