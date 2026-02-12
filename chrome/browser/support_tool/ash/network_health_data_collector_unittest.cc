fffb4fd77d051dc0a9d36fee6f89ae97a1472560
Kalvin Lee <kdlee@chromium.org>
2026-02-12 01:32:25 -0800

    Spanify `RedactionTool` (1.?/2)
    Migrate constructors for `RedactionTool` and `RedactionToolContainer` in
    chrome/browser/support_tool.
    
    (N.B. in most cases this simply means dropping a `nullptr` argument that
    allows one to fall through to the `base::span` constructor with a
    defaulted empty span.)
    
    This CL was uploaded by git cl split.
    
    R=nedol@google.com
    
    Bug: 439455382
    Change-Id: Ia536e92f7469b4af746b6162a6bfb06f9b7f5c20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7570147
    Commit-Queue: Andrei Nedoluzhko <nedol@google.com>
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Andrei Nedoluzhko <nedol@google.com>
    Cr-Commit-Position: refs/heads/main@{#1583818}

--- chrome/browser/support_tool/ash/network_health_data_collector_unittest.cc
+++ chrome/browser/support_tool/ash/network_health_data_collector_unittest.cc
@@ -124,7 +124,7 @@ class NetworkHealthDataCollectorTest : public ::testing::Test {
         base::ThreadPool::CreateSequencedTaskRunner({});
     redaction_tool_container_ =
         base::MakeRefCounted<redaction::RedactionToolContainer>(
-            task_runner_for_redaction_tool_, nullptr);
+            task_runner_for_redaction_tool_);
   }
 
   NetworkHealthDataCollectorTest(const NetworkHealthDataCollectorTest&) =
--- chrome/browser/support_tool/updater_data_collector_unittest.cc
+++ chrome/browser/support_tool/updater_data_collector_unittest.cc
@@ -92,8 +92,7 @@ class UpdaterDataCollectorTest : public ::testing::Test {
       base::ThreadPool::CreateSequencedTaskRunner({});
   scoped_refptr<redaction::RedactionToolContainer> redaction_tool_container_ =
       base::MakeRefCounted<redaction::RedactionToolContainer>(
-          task_runner_for_redaction_tool_,
-          nullptr);
+          task_runner_for_redaction_tool_);
 };
 
 TEST_F(UpdaterDataCollectorTest, CollectAndExportData) {
