1981246cccad51060453c03d7533d31da8703bcb
Colin Blundell <blundell@chromium.org>
2026-02-12 22:57:26 -0800

    [Blink] Update reference to "mappable" in video_frame_image_util.cc
    This should now refer to "has direct CPU access".
    
    Bug: 404905709
    Change-Id: I4c679735f665d2a60cab62782819bacb0fc931b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566488
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584452}

--- third_party/blink/renderer/platform/graphics/video_frame_image_util.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_image_util.cc
@@ -133,7 +133,7 @@ scoped_refptr<StaticBitmapImage> CreateImageFromVideoFrame(
       frame->metadata().transformation.value_or(media::kNoTransformation);
 
   // If the provider isn't accelerated, avoid GPU round trips to upload frame
-  // data from GpuMemoryBuffer backed frames which aren't mappable.
+  // data from MappableSI-backed frames which don't have direct CPU access.
   if (frame->HasMappableSharedImage() && !frame->HasDirectCpuAccess() &&
       !snapshot_provider->IsAccelerated()) {
     frame = media::ConvertToMemoryMappedFrame(std::move(frame));
