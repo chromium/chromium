9b9f5ca534a7bb16deedb7f088b75e7dd09abbd9
Glenn Hartmann <hartmanng@chromium.org>
2026-02-13 07:19:25 -0800

    Enable some tests in global_browser_collection_browsertest.cc on desktop Android.
    Activation/Deactivation tracking is not implemented yet, but we can
    enable the tests that only use Creation/Closure tracking.
    
    Bug: 477251911
    Change-Id: If0ab3597ab14a342724e5fd82041f8636a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7515956
    Reviewed-by: Tom Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1584642}

--- chrome/browser/ui/browser_window/internal/global_browser_collection_interactive_ui_test.cc
+++ chrome/browser/ui/browser_window/internal/global_browser_collection_interactive_ui_test.cc
@@ -8,18 +8,22 @@
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/ui/browser_window/public/browser_collection_observer.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/browser_window/public/create_browser_window.h"
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/test/base/chrome_test_utils.h"
-#include "chrome/test/base/platform_browser_test.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "content/public/test/browser_test.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "ui/base/base_window.h"
 
-#if BUILDFLAG(IS_CHROMEOS)
-#include "ash/constants/ash_switches.h"
-#include "base/command_line.h"
-#endif  // BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/ui/browser_window/test/android/browser_window_android_browsertest_base.h"
+#define TestBase BrowserWindowAndroidBrowserTestBase
+#else
+#include "chrome/test/base/in_process_browser_test.h"
+#define TestBase InProcessBrowserTest
+#endif  // BUILDFLAG(IS_ANDROID)
 
 using testing::_;
 
@@ -32,36 +36,119 @@ class MockBrowserCollectionObserver
   MOCK_METHOD(void, OnBrowserDeactivated, (BrowserWindowInterface * browser));
 };
 
-class GlobalBrowserCollectionTest : public PlatformBrowserTest {
+// TODO(crbug.com/480103891): Refactor this out with the rest of the utilities
+// with similar functionality in ui_test_utils and interactive_test_utils.
+class BrowserEventWaiter : public BrowserCollectionObserver {
  public:
-#if BUILDFLAG(IS_CHROMEOS)
-  // This makes multi-profile work on ChromeOS.
-  void SetUpCommandLine(base::CommandLine* command_line) override {
-    command_line->AppendSwitch(
-        ash::switches::kIgnoreUserProfileMappingForTests);
-  }
-#endif  // BUILDFLAG(IS_CHROMEOS)
+  enum class Event { CREATED, CLOSED, ACTIVATED, DEACTIVATED };
 
+  // Start observing a GlobalBrowserCollection for |event|. The destructor will
+  // wait for the event to happen (if it hasn't already).
+  explicit BrowserEventWaiter(Event event)
+      : BrowserEventWaiter(event, nullptr) {}
+
+  // Start observing a GlobalBrowserCollection for |event| on |browser|. The
+  // destructor will wait for the event to happen (if it hasn't already).
+  BrowserEventWaiter(Event event, BrowserWindowInterface* browser)
+      : event_(event), browser_(browser) {
+    browser_collection_observation_.Observe(
+        GlobalBrowserCollection::GetInstance());
+  }
+
+  // Block and wait for the desired event, or finish immediately if it has
+  // already happened.
+  ~BrowserEventWaiter() override { run_loop_.Run(); }
+
+ private:
+  // BrowserCollectionObserver:
+  void OnBrowserCreated(BrowserWindowInterface* browser) override {
+    OnBrowserEvent(Event::CREATED, browser);
+  }
+
+  void OnBrowserClosed(BrowserWindowInterface* browser) override {
+    OnBrowserEvent(Event::CLOSED, browser);
+  }
+
+  void OnBrowserActivated(BrowserWindowInterface* browser) override {
+    OnBrowserEvent(Event::ACTIVATED, browser);
+  }
+
+  void OnBrowserDeactivated(BrowserWindowInterface* browser) override {
+    OnBrowserEvent(Event::DEACTIVATED, browser);
+  }
+
+  // Called when any event comes in. Causes the RunLoop to quit if the event is
+  // the one we're interested in.
+  void OnBrowserEvent(Event event, BrowserWindowInterface* browser) {
+    if (event == event_ && (browser_ == nullptr || browser == browser_)) {
+      run_loop_.Quit();
+    }
+  }
+
+  Event event_;
+  BrowserWindowInterface* browser_;
+  base::RunLoop run_loop_;
+  base::ScopedObservation<GlobalBrowserCollection, BrowserCollectionObserver>
+      browser_collection_observation_{this};
+};
+
+// Runs on all desktop platforms except ChromeOS (including desktop Android),
+// but not on mobile.
+class GlobalBrowserCollectionTest : public TestBase {
  protected:
-  BrowserWindowInterface* GetPrimaryBrowser() {
-#if BUILDFLAG(IS_ANDROID)
-    // TODO(crbug.com/477251911): Add implementation for Android.
-    return nullptr;
-#else
-    return browser();
-#endif  // BUILDFLAG(IS_ANDROID)
+  // TestBase:
+  void SetUpOnMainThread() override {
+    TestBase::SetUpOnMainThread();
+
+    primary_browser_ = GetLastActiveBrowserWindowInterfaceWithAnyProfile();
+    primary_profile_ = primary_browser_->GetProfile();
+    active_browser_ = primary_browser_;
   }
 
-  Profile* GetPrimaryProfile() { return GetPrimaryBrowser()->GetProfile(); }
+  void TearDownOnMainThread() override {
+    active_browser_ = nullptr;
+    primary_profile_ = nullptr;
+    primary_browser_ = nullptr;
+
+    TestBase::TearDownOnMainThread();
+  }
+
+  BrowserWindowInterface* GetPrimaryBrowser() { return primary_browser_.get(); }
+  Profile* GetPrimaryProfile() { return primary_profile_.get(); }
+
+  // TODO(crbug.com/356183782): Consider using
+  // ui_test_utils::BringBrowserWindowToFront() instead.
+  void ActivateBrowser(BrowserWindowInterface* browser) {
+    BrowserEventWaiter browser_activated_waiter(
+        BrowserEventWaiter::Event::ACTIVATED, browser);
+
+// TODO(crbug.com/483363917): Enable this on Linux once the
+// BaseWindow::Activate() behaviour is fixed.
+#if !BUILDFLAG(IS_LINUX)
+    BrowserEventWaiter browser_deactivated_waiter(
+        BrowserEventWaiter::Event::DEACTIVATED, active_browser_);
+#endif  // !BUILDFLAG(IS_LINUX)
+
+    browser->GetWindow()->Activate();
+    active_browser_ = browser;
+  }
 
   BrowserWindowInterface* CreateAndActivateBrowser(Profile* profile) {
-    BrowserWindowInterface* browser = CreateBrowserWindow(
-        BrowserWindowCreateParams(*profile, /*from_user_gesture=*/true));
-#if BUILDFLAG(IS_ANDROID)
-    // TODO(crbug.com/477251911): Add |browser| activation on Android.
-#else
-    browser->GetBrowserForMigrationOnly()->DidBecomeActive();
-#endif  // BUILDFLAG(IS_ANDROID)
+    BrowserWindowInterface* browser;
+    {
+      BrowserEventWaiter browser_created_waiter(
+          BrowserEventWaiter::Event::CREATED);
+      browser = CreateBrowserWindow(
+          BrowserWindowCreateParams(*profile, /*from_user_gesture=*/true));
+    }
+
+    // TODO(crbug.com/477251911): Enable this on Android (if Android browsers
+    // aren't already activated upon creation) once we implement activation
+    // tracking in GlobalBrowserCollection for Android.
+#if !BUILDFLAG(IS_ANDROID)
+    ActivateBrowser(browser);
+#endif  // !BUILDFLAG(IS_ANDROID)
+
     return browser;
   }
 
@@ -80,43 +167,56 @@ class GlobalBrowserCollectionTest : public PlatformBrowserTest {
     return CreateAndActivateBrowser(new_profile);
   }
 
-  // TODO(crbug.com/356183782): Consider rewriting this test as an interactive
-  // ui test and using ui_test_utils::BringBrowserWindowToFront() instead.
-  void ActivatePrimaryBrowser(BrowserWindowInterface* const secondary_browser) {
-#if BUILDFLAG(IS_ANDROID)
-    // TODO(crbug.com/477251911): Add implementation for Android.
-#else
-    browser()->DidBecomeActive();
-    secondary_browser->GetBrowserForMigrationOnly()->DidBecomeInactive();
-#endif  // BUILDFLAG(IS_ANDROID)
-  }
-
-  void ActivateSecondaryBrowser(
-      BrowserWindowInterface* const secondary_browser) {
-#if BUILDFLAG(IS_ANDROID)
-    // TODO(crbug.com/477251911): Add implementation for Android.
-#else
-    secondary_browser->GetBrowserForMigrationOnly()->DidBecomeActive();
-    browser()->DidBecomeInactive();
-#endif  // BUILDFLAG(IS_ANDROID)
-  }
-
+  // This should be close to the last thing called. Calling Activate() after
+  // this function will not do what you want.
   void CloseBrowserSynchronouslyCrossPlatform(BrowserWindowInterface* browser) {
+    BrowserEventWaiter browser_closed_waiter(BrowserEventWaiter::Event::CLOSED,
+                                             browser);
+    if (browser == active_browser_) {
+      // This avoids a dangling raw_ptr. It means that we can't use Activate()
+      // after this function is called, but the windows would have been in an
+      // unknown state after closing a browser anyway, since we wouldn't know
+      // which other window the OS would activate.
+      active_browser_ = nullptr;
+    }
+
 #if BUILDFLAG(IS_ANDROID)
-    // TODO(crbug.com/477251911): Add implementation for Android.
+    browser->GetWindow()->Close();
 #else
+    // TODO(crbug.com/478908209): I don't know why, but
+    // `browser->GetWindow()->Close();` doesn't seem to trigger
+    // GlobalBrowserCollection::OnBrowserClosed() on desktop.
     CloseBrowserSynchronously(browser);
 #endif  // BUILDFLAG(IS_ANDROID)
   }
+
+  // Returns the result of browser->IsActive() on platforms that support it.
+  // Otherwise, just returns the |want| value.
+  bool IsActiveIfAvailable(BrowserWindowInterface* browser, bool want) {
+#if BUILDFLAG(IS_ANDROID)
+    return want;
+#else
+    return browser->IsActive();
+#endif  // BUILDFLAG(IS_ANDROID)
+  }
+
+ private:
+  raw_ptr<BrowserWindowInterface> primary_browser_ = nullptr;
+  raw_ptr<Profile> primary_profile_ = nullptr;
+  raw_ptr<BrowserWindowInterface> active_browser_ = nullptr;
 };
 
-// TODO(crbug.com/477251911): Make this test work on Android.
-#if BUILDFLAG(IS_ANDROID)
+// TODO(crbug.com/477251911): Enable this on Android once we implement
+// activation tracking in GlobalBrowserCollection for Android.
+//
+// TODO(crbug.com/483363917): Enable this on Linux once the
+// BaseWindow::Activate() behaviour is fixed.
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
 #define MAYBE_TestObservationWithSingleProfile \
   DISABLED_TestObservationWithSingleProfile
 #else
 #define MAYBE_TestObservationWithSingleProfile TestObservationWithSingleProfile
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
 IN_PROC_BROWSER_TEST_F(GlobalBrowserCollectionTest,
                        MAYBE_TestObservationWithSingleProfile) {
   // Observe GlobalBrowserCollection.
@@ -125,40 +225,52 @@ IN_PROC_BROWSER_TEST_F(GlobalBrowserCollectionTest,
       observation{&observer};
   observation.Observe(GlobalBrowserCollection::GetInstance());
 
+  EXPECT_TRUE(IsActiveIfAvailable(GetPrimaryBrowser(), /*want=*/true));
+
   // Create secondary browser and expect events.
   EXPECT_CALL(observer, OnBrowserCreated(_)).Times(1);
   BrowserWindowInterface* const secondary_browser =
       CreateAndActivateBrowser(GetPrimaryProfile());
   testing::Mock::VerifyAndClearExpectations(&observer);
 
-  // Start with secondary browser active (and primary browser inactive).
-  ActivateSecondaryBrowser(secondary_browser);
+  EXPECT_FALSE(IsActiveIfAvailable(GetPrimaryBrowser(), /*want=*/false));
+  EXPECT_TRUE(IsActiveIfAvailable(secondary_browser, /*want=*/true));
 
   // Activate primary browser and expect events.
   EXPECT_CALL(observer, OnBrowserActivated(GetPrimaryBrowser())).Times(1);
   EXPECT_CALL(observer, OnBrowserDeactivated(secondary_browser)).Times(1);
-  ActivatePrimaryBrowser(secondary_browser);
+  ActivateBrowser(GetPrimaryBrowser());
   testing::Mock::VerifyAndClearExpectations(&observer);
+
+  EXPECT_TRUE(IsActiveIfAvailable(GetPrimaryBrowser(), /*want=*/true));
+  EXPECT_FALSE(IsActiveIfAvailable(secondary_browser, /*want=*/false));
 
   // Reactivate secondary browser and expect events.
   EXPECT_CALL(observer, OnBrowserDeactivated(GetPrimaryBrowser())).Times(1);
   EXPECT_CALL(observer, OnBrowserActivated(secondary_browser)).Times(1);
-  ActivateSecondaryBrowser(secondary_browser);
+  ActivateBrowser(secondary_browser);
   testing::Mock::VerifyAndClearExpectations(&observer);
+
+  EXPECT_FALSE(IsActiveIfAvailable(GetPrimaryBrowser(), /*want=*/false));
+  EXPECT_TRUE(IsActiveIfAvailable(secondary_browser, /*want=*/true));
 
   // Close secondary browser and expect events.
   EXPECT_CALL(observer, OnBrowserClosed(secondary_browser)).Times(1);
   CloseBrowserSynchronouslyCrossPlatform(secondary_browser);
 }
 
-// TODO(crbug.com/477251911): Make this test work on Android.
-#if BUILDFLAG(IS_ANDROID)
+// TODO(crbug.com/477251911): Enable this on Android once we implement
+// activation tracking in GlobalBrowserCollection for Android.
+//
+// TODO(crbug.com/483363917): Enable this on Linux once the
+// BaseWindow::Activate() behaviour is fixed.
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
 #define MAYBE_TestObservationWithMultipleProfiles \
   DISABLED_TestObservationWithMultipleProfiles
 #else
 #define MAYBE_TestObservationWithMultipleProfiles \
   TestObservationWithMultipleProfiles
-#endif  // BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX)
 IN_PROC_BROWSER_TEST_F(GlobalBrowserCollectionTest,
                        MAYBE_TestObservationWithMultipleProfiles) {
   // This test is only relevant on platforms that support multi-profile.
@@ -172,26 +284,34 @@ IN_PROC_BROWSER_TEST_F(GlobalBrowserCollectionTest,
       observation{&observer};
   observation.Observe(GlobalBrowserCollection::GetInstance());
 
+  EXPECT_TRUE(IsActiveIfAvailable(GetPrimaryBrowser(), /*want=*/true));
+
   // Create secondary browser and expect events.
   EXPECT_CALL(observer, OnBrowserCreated(_)).Times(1);
   BrowserWindowInterface* const secondary_browser =
       CreateAndActivateBrowserWithNewProfileIfEnabled();
   testing::Mock::VerifyAndClearExpectations(&observer);
 
-  // Start with secondary browser active (and primary browser inactive).
-  ActivateSecondaryBrowser(secondary_browser);
+  EXPECT_FALSE(IsActiveIfAvailable(GetPrimaryBrowser(), /*want=*/false));
+  EXPECT_TRUE(IsActiveIfAvailable(secondary_browser, /*want=*/true));
 
   // Activate primary browser and expect events.
   EXPECT_CALL(observer, OnBrowserActivated(GetPrimaryBrowser())).Times(1);
   EXPECT_CALL(observer, OnBrowserDeactivated(secondary_browser)).Times(1);
-  ActivatePrimaryBrowser(secondary_browser);
+  ActivateBrowser(GetPrimaryBrowser());
   testing::Mock::VerifyAndClearExpectations(&observer);
+
+  EXPECT_TRUE(IsActiveIfAvailable(GetPrimaryBrowser(), /*want=*/true));
+  EXPECT_FALSE(IsActiveIfAvailable(secondary_browser, /*want=*/false));
 
   // Reactivate secondary browser and expect events.
   EXPECT_CALL(observer, OnBrowserDeactivated(GetPrimaryBrowser())).Times(1);
   EXPECT_CALL(observer, OnBrowserActivated(secondary_browser)).Times(1);
-  ActivateSecondaryBrowser(secondary_browser);
+  ActivateBrowser(secondary_browser);
   testing::Mock::VerifyAndClearExpectations(&observer);
+
+  EXPECT_FALSE(IsActiveIfAvailable(GetPrimaryBrowser(), /*want=*/false));
+  EXPECT_TRUE(IsActiveIfAvailable(secondary_browser, /*want=*/true));
 
   // Close secondary browser and expect events.
   EXPECT_CALL(observer, OnBrowserClosed(secondary_browser)).Times(1);
@@ -234,15 +354,8 @@ class GlobalBrowserCollectionTestWithOrder
   std::vector<raw_ptr<BrowserWindowInterface>> browsers_;
 };
 
-// TODO(crbug.com/477251911): Make this test work on Android.
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_ForEachIteratesOverAllBrowsers \
-  DISABLED_ForEachIteratesOverAllBrowsers
-#else
-#define MAYBE_ForEachIteratesOverAllBrowsers ForEachIteratesOverAllBrowsers
-#endif  // BUILDFLAG(IS_ANDROID)
 IN_PROC_BROWSER_TEST_P(GlobalBrowserCollectionTestWithOrder,
-                       MAYBE_ForEachIteratesOverAllBrowsers) {
+                       ForEachIteratesOverAllBrowsers) {
   std::vector<BrowserWindowInterface*> visited;
   GlobalBrowserCollection::GetInstance()->ForEach(
       [&](BrowserWindowInterface* b) {
@@ -263,16 +376,8 @@ IN_PROC_BROWSER_TEST_P(GlobalBrowserCollectionTestWithOrder,
   }
 }
 
-// TODO(crbug.com/477251911): Make this test work on Android.
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_ForEachStopsWhenCallbackReturnsFalse \
-  DISABLED_ForEachStopsWhenCallbackReturnsFalse
-#else
-#define MAYBE_ForEachStopsWhenCallbackReturnsFalse \
-  ForEachStopsWhenCallbackReturnsFalse
-#endif  // BUILDFLAG(IS_ANDROID)
 IN_PROC_BROWSER_TEST_P(GlobalBrowserCollectionTestWithOrder,
-                       MAYBE_ForEachStopsWhenCallbackReturnsFalse) {
+                       ForEachStopsWhenCallbackReturnsFalse) {
   std::vector<BrowserWindowInterface*> visited;
   GlobalBrowserCollection::GetInstance()->ForEach(
       [&](BrowserWindowInterface* b) {
@@ -289,16 +394,8 @@ IN_PROC_BROWSER_TEST_P(GlobalBrowserCollectionTestWithOrder,
   }
 }
 
-// TODO(crbug.com/477251911): Make this test work on Android.
-#if BUILDFLAG(IS_ANDROID)
-#define MAYBE_ForEachResilientToBrowserDestruction \
-  DISABLED_ForEachResilientToBrowserDestruction
-#else
-#define MAYBE_ForEachResilientToBrowserDestruction \
-  ForEachResilientToBrowserDestruction
-#endif  // BUILDFLAG(IS_ANDROID)
 IN_PROC_BROWSER_TEST_P(GlobalBrowserCollectionTestWithOrder,
-                       MAYBE_ForEachResilientToBrowserDestruction) {
+                       ForEachResilientToBrowserDestruction) {
   std::vector<BrowserWindowInterface*> visited;
   GlobalBrowserCollection::GetInstance()->ForEach(
       [&](BrowserWindowInterface* b) {
@@ -326,8 +423,14 @@ IN_PROC_BROWSER_TEST_P(GlobalBrowserCollectionTestWithOrder,
 INSTANTIATE_TEST_SUITE_P(
     ,
     GlobalBrowserCollectionTestWithOrder,
-    ::testing::Values(BrowserCollection::Order::kCreation,
-                      BrowserCollection::Order::kActivation),
+    ::testing::Values(BrowserCollection::Order::kCreation
+// TODO(crbug.com/477251911): Enable this on Android once we implement
+// activation tracking in GlobalBrowserCollection for Android.
+#if !BUILDFLAG(IS_ANDROID)
+                      ,
+                      BrowserCollection::Order::kActivation
+#endif  // !BUILDFLAG(IS_ANDROID)
+                      ),
     [](const testing::TestParamInfo<BrowserCollection::Order>& param) {
       switch (param.param) {
         case BrowserCollection::Order::kCreation:
