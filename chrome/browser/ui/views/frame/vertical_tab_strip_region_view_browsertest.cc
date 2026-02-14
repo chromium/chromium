ce9254f664168d4a0499d2ccd5d57d1352ace0e2
Vince Lugli <lugli@google.com>
2026-02-13 21:59:41 -0800

    [VerticalTabs][Glic] Implement ToolbarGlicButton.
    Create a ToolbarGlicButton that inherits and implements
    GlicButton<ToolbarButton> template class. This is used in the
    toolbar_view to display the Gemini button only when we are in vertical
    tabs. The button is created on Init, but will only be visible when
    vertical tabs should be shown.
    
    Bug: 480980150
    Change-Id: I1229d78a47c9e3b6666389933fb32f978cd956c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7568024
    Commit-Queue: Vince Lugli <lugli@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1585083}

--- chrome/browser/ui/views/frame/vertical_tab_strip_region_view_browsertest.cc
+++ chrome/browser/ui/views/frame/vertical_tab_strip_region_view_browsertest.cc
@@ -211,7 +211,7 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripRegionViewTest, ResizeViewSmaller) {
 }
 
 // TODO(https://crbug.com/481074869): Re-enable this test
-#if BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(ENABLE_GLIC)
 #define MAYBE_ResizeViewBigger DISABLED_ResizeViewBigger
 #else
 #define MAYBE_ResizeViewBigger ResizeViewBigger
