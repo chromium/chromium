2f9bbbe19d40ebf30857cae17dfd6fab5a83f2f4
Masa Fujita <massan@google.com>
2026-02-13 21:09:57 -0800

    flyout: Add hover listener property keys to radio / checkbox items
    This CL fixes a bug where when the user hovers on menu items with radio
    or checkbox, its ancestor items were not highlighted. This was because
    of missing property keys.
    
    Bug: 441733637
    Change-Id: I173729760e55a11a6ad7c22a51a0bec1cf4db6e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7574907
    Reviewed-by: Jenna Himawan <jhimawan@google.com>
    Commit-Queue: Masa Fujita <massan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1585079}

--- ui/android/java/src/org/chromium/ui/listmenu/ListMenuCheckItemProperties.java
+++ ui/android/java/src/org/chromium/ui/listmenu/ListMenuCheckItemProperties.java
@@ -6,6 +6,8 @@ package org.chromium.ui.listmenu;
 
 import static org.chromium.ui.listmenu.ListMenuItemProperties.CLICK_LISTENER;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.ENABLED;
+import static org.chromium.ui.listmenu.ListMenuItemProperties.HOVER_LISTENER;
+import static org.chromium.ui.listmenu.ListMenuItemProperties.IS_HIGHLIGHTED;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.KEY_LISTENER;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.TITLE;
 
@@ -19,6 +21,6 @@ public class ListMenuCheckItemProperties {
     public static final WritableBooleanPropertyKey CHECKED = new WritableBooleanPropertyKey();
 
     public static final PropertyKey[] ALL_KEYS = {
-        TITLE, CLICK_LISTENER, ENABLED, CHECKED, KEY_LISTENER
+        TITLE, CLICK_LISTENER, ENABLED, CHECKED, KEY_LISTENER, HOVER_LISTENER, IS_HIGHLIGHTED
     };
 }
--- ui/android/java/src/org/chromium/ui/listmenu/ListMenuRadioItemProperties.java
+++ ui/android/java/src/org/chromium/ui/listmenu/ListMenuRadioItemProperties.java
@@ -6,6 +6,8 @@ package org.chromium.ui.listmenu;
 
 import static org.chromium.ui.listmenu.ListMenuItemProperties.CLICK_LISTENER;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.ENABLED;
+import static org.chromium.ui.listmenu.ListMenuItemProperties.HOVER_LISTENER;
+import static org.chromium.ui.listmenu.ListMenuItemProperties.IS_HIGHLIGHTED;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.KEY_LISTENER;
 import static org.chromium.ui.listmenu.ListMenuItemProperties.TITLE;
 
@@ -19,6 +21,6 @@ public class ListMenuRadioItemProperties {
     public static final WritableBooleanPropertyKey SELECTED = new WritableBooleanPropertyKey();
 
     public static final PropertyKey[] ALL_KEYS = {
-        TITLE, CLICK_LISTENER, ENABLED, SELECTED, KEY_LISTENER
+        TITLE, CLICK_LISTENER, ENABLED, SELECTED, KEY_LISTENER, HOVER_LISTENER, IS_HIGHLIGHTED
     };
 }
