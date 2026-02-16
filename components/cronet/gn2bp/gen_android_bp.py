15e5c52abc2a31887f4e6344c4aac19e14b05fd5
Stefano Duo <stefanoduo@google.com>
2026-02-16 15:29:39 -0800

    Ignore -fno-lifetime-dse for Cronet in Android
    Android's clang is not aware of that option and fails with
    clang++-real: error: unknown argument: '-fno-lifetime-dse'
    
    Bug: b:484919839
    Change-Id: I41c5da71c7efdff1e325e7a0af1a3e7de4069a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580515
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1585520}

--- components/cronet/gn2bp/gen_android_bp.py
+++ components/cronet/gn2bp/gen_android_bp.py
@@ -2682,6 +2682,10 @@ def _is_cflag_allowed(cflag):
       #   clang++-real: error: unknown argument: '-fsanitize-ignore-for-ubsan-feature=array-bounds'
       # See https://crbug.com/481594099
       '-fsanitize-ignore-for-ubsan-feature=array-bounds',
+      # Causes the build to fail with:
+      #   clang++-real: error: unknown argument: '-fno-lifetime-dse'
+      # See https://crbug.com/484919839
+      '-fno-lifetime-dse',
   ])
 
 def _get_cflags(cflags, defines):
