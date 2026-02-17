4ff46dbf285d78edfc16e7dccc73f785da2e068d
Florian Leimgruber <fleimgruber@google.com>
2026-02-17 02:53:46 -0800

    Implement EntityInstance -> PrivatePass conversion
    Implements the conversion of private pass EntityInstance attributes to
    the corresponding PrivatePass one-of proto messages.
    
    Bug: 478783796
    Change-Id: I0e424a08ad72673d86d4e68efa927f9e7d8a4db7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7570965
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1585612}

--- components/autofill/core/browser/network/autofill_ai/private_pass_conversion_util.cc
+++ components/autofill/core/browser/network/autofill_ai/private_pass_conversion_util.cc
@@ -4,11 +4,20 @@
 
 #include "components/autofill/core/browser/network/autofill_ai/private_pass_conversion_util.h"
 
+#include <string>
+
 #include "base/notreached.h"
 #include "base/strings/strcat.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/utf_string_conversions.h"
+#include "base/time/time.h"
+#include "base/types/optional_ref.h"
+#include "components/autofill/core/browser/autofill_field.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
+#include "components/autofill/core/browser/data_model/autofill_ai/entity_type.h"
 #include "components/autofill/core/browser/data_model/autofill_ai/entity_type_names.h"
 #include "components/autofill/core/browser/proto/autofill_ai_chrome_metadata.pb.h"
+#include "components/autofill/core/browser/proto/server.pb.h"
 #include "components/autofill/core/browser/webdata/autofill_ai/entity_sync_util.h"
 #include "components/wallet/core/browser/proto/common.pb.h"
 #include "components/wallet/core/browser/proto/private_pass.pb.h"
@@ -25,6 +34,8 @@ using KnownTravelerNumber = ::wallet::PrivatePass::KnownTravelerNumber;
 using Passport = ::wallet::PrivatePass::Passport;
 using RedressNumber = ::wallet::PrivatePass::RedressNumber;
 
+using enum AttributeTypeName;
+
 Any SerializeMetadata(const EntityInstance& entity) {
   ChromeValuablesMetadata metadata = SerializeChromeValuablesMetadata(entity);
   Any serialised_metadata;
@@ -34,35 +45,135 @@ Any SerializeMetadata(const EntityInstance& entity) {
   return serialised_metadata;
 }
 
+// Helper class to populate the fields of a given private `Pass` proto with
+// attributes of an entity.
+template <typename Pass>
+class AttributeSetter {
+ public:
+  explicit AttributeSetter(const EntityInstance& entity, Pass& pass)
+      : entity_(entity), pass_(pass) {}
+
+  void SetString(AttributeTypeName attribute_name,
+                 void (Pass::*setter)(const std::string&)) const {
+    base::optional_ref<const AttributeInstance> attribute =
+        entity_->attribute(AttributeType(attribute_name));
+    if (!attribute.has_value()) {
+      return;
+    }
+    std::invoke(setter, pass_,
+                base::UTF16ToUTF8(attribute->GetCompleteRawInfo()));
+  }
+
+  void SetDate(AttributeTypeName attribute_name,
+               void (Pass::*setter)(int64_t)) {
+    base::optional_ref<const AttributeInstance> attribute =
+        entity_->attribute(AttributeType(attribute_name));
+    if (!attribute.has_value()) {
+      return;
+    }
+    // Note that for retrieving date components the app locale doesn't matter.
+    FieldType date_type = attribute->type().field_type();
+    std::u16string day = attribute->GetInfo(
+        date_type, "", AutofillFormatString(u"D", FormatString_Type_DATE));
+    std::u16string month = attribute->GetInfo(
+        date_type, "", AutofillFormatString(u"M", FormatString_Type_DATE));
+    std::u16string year = attribute->GetInfo(
+        date_type, "", AutofillFormatString(u"YYYY", FormatString_Type_DATE));
+    std::optional<base::Time> date = ParseDate(day, month, year);
+    if (!date) {
+      return;
+    }
+    std::invoke(setter, pass_,
+                (*date - base::Time::UnixEpoch()).InMicroseconds());
+  }
+
+ private:
+  std::optional<base::Time> ParseDate(const std::u16string& day_str,
+                                      const std::u16string& month_str,
+                                      const std::u16string& year_str) {
+    int day, month, year;
+    if (!base::StringToInt(day_str, &day) ||
+        !base::StringToInt(month_str, &month) ||
+        !base::StringToInt(year_str, &year)) {
+      return std::nullopt;
+    }
+    base::Time::Exploded exploded = {
+        .year = year, .month = month, .day_of_month = day};
+    base::Time parsed_date;
+    if (!base::Time::FromUTCExploded(exploded, &parsed_date)) {
+      return std::nullopt;
+    }
+    return parsed_date;
+  }
+
+  const raw_ref<const EntityInstance> entity_;
+  raw_ref<Pass> pass_;
+};
+
 Passport EntityInstanceToPassport(const EntityInstance& entity) {
   CHECK_EQ(entity.type().name(), EntityTypeName::kPassport);
-  // TODO(crbug.com/478783796): Implement conversion.
-  return Passport();
+  Passport pass;
+  AttributeSetter setter(entity, pass);
+  setter.SetString(kPassportName, &Passport::set_owner_name);
+  setter.SetString(kPassportNumber, &Passport::set_passport_number);
+  setter.SetString(kPassportCountry, &Passport::set_country_code);
+  setter.SetDate(kPassportIssueDate,
+                 &Passport::set_issue_date_unix_epoch_micros);
+  setter.SetDate(kPassportExpirationDate,
+                 &Passport::set_expiration_date_unix_epoch_micros);
+  return pass;
 }
 
 DriverLicense EntityInstanceToDriverLicense(const EntityInstance& entity) {
   CHECK_EQ(entity.type().name(), EntityTypeName::kDriversLicense);
-  // TODO(crbug.com/478783796): Implement conversion.
-  return DriverLicense();
+  DriverLicense pass;
+  AttributeSetter setter(entity, pass);
+  setter.SetString(kDriversLicenseName, &DriverLicense::set_owner_name);
+  setter.SetString(kDriversLicenseNumber,
+                   &DriverLicense::set_driver_license_number);
+  setter.SetString(kDriversLicenseState, &DriverLicense::set_region);
+  setter.SetDate(kDriversLicenseIssueDate,
+                 &DriverLicense::set_issue_date_unix_epoch_micros);
+  setter.SetDate(kDriversLicenseExpirationDate,
+                 &DriverLicense::set_expiration_date_unix_epoch_micros);
+  return pass;
 }
 
 IdCard EntityInstanceToIdCard(const EntityInstance& entity) {
   CHECK_EQ(entity.type().name(), EntityTypeName::kNationalIdCard);
-  // TODO(crbug.com/478783796): Implement conversion.
-  return IdCard();
+  IdCard pass;
+  AttributeSetter setter(entity, pass);
+  setter.SetString(kNationalIdCardName, &IdCard::set_owner_name);
+  setter.SetString(kNationalIdCardNumber, &IdCard::set_id_number);
+  setter.SetString(kNationalIdCardCountry, &IdCard::set_country_code);
+  setter.SetDate(kNationalIdCardIssueDate,
+                 &IdCard::set_issue_date_unix_epoch_micros);
+  setter.SetDate(kNationalIdCardExpirationDate,
+                 &IdCard::set_expiration_date_unix_epoch_micros);
+  return pass;
 }
 
 KnownTravelerNumber EntityInstanceToKnownTravelerNumber(
     const EntityInstance& entity) {
   CHECK_EQ(entity.type().name(), EntityTypeName::kKnownTravelerNumber);
-  // TODO(crbug.com/478783796): Implement conversion.
-  return KnownTravelerNumber();
+  KnownTravelerNumber pass;
+  AttributeSetter setter(entity, pass);
+  setter.SetString(kKnownTravelerNumberName,
+                   &KnownTravelerNumber::set_owner_name);
+  setter.SetString(kKnownTravelerNumberNumber,
+                   &KnownTravelerNumber::set_known_traveler_number);
+  setter.SetDate(kKnownTravelerNumberExpirationDate,
+                 &KnownTravelerNumber::set_expiration_date_unix_epoch_micros);
+  return pass;
 }
 
 RedressNumber EntityInstanceToRedressNumber(const EntityInstance& entity) {
   CHECK_EQ(entity.type().name(), EntityTypeName::kRedressNumber);
-  // TODO(crbug.com/478783796): Implement conversion.
-  return RedressNumber();
+  RedressNumber pass;
+  AttributeSetter setter(entity, pass);
+  setter.SetString(kRedressNumberName, &RedressNumber::set_owner_name);
+  setter.SetString(kRedressNumberNumber, &RedressNumber::set_redress_number);
+  return pass;
 }
 
 }  // namespace
---
+++ components/autofill/core/browser/network/autofill_ai/private_pass_conversion_util_unittest.cc
@@ -0,0 +1,175 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/network/autofill_ai/private_pass_conversion_util.h"
+
+#include <cstdint>
+
+#include "components/autofill/core/browser/data_model/autofill_ai/entity_instance.h"
+#include "components/autofill/core/browser/data_model/autofill_ai/entity_type.h"
+#include "components/autofill/core/browser/data_model/autofill_ai/entity_type_names.h"
+#include "components/autofill/core/browser/test_utils/entity_data_test_utils.h"
+#include "components/wallet/core/browser/proto/private_pass.pb.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace autofill {
+
+namespace {
+
+using DriverLicense = ::wallet::PrivatePass::DriverLicense;
+using IdCard = ::wallet::PrivatePass::IdCard;
+using KnownTravelerNumber = ::wallet::PrivatePass::KnownTravelerNumber;
+using Passport = ::wallet::PrivatePass::Passport;
+using RedressNumber = ::wallet::PrivatePass::RedressNumber;
+using wallet::PrivatePass;
+
+int64_t MicrosSinceUnixEpoch(int year, int month, int day) {
+  base::Time::Exploded exploded = {
+      .year = year, .month = month, .day_of_month = day};
+  base::Time date;
+  EXPECT_TRUE(base::Time::FromUTCExploded(exploded, &date));
+  return (date - base::Time::UnixEpoch()).InMicroseconds();
+}
+
+TEST(PrivatePassConversionUtil, Passport) {
+  EntityInstance entity = test::GetPassportEntityInstance({
+      .name = u"Name",
+      .number = u"12345678",
+      .country = u"Austria",
+      .expiry_date = u"2019-08-30",
+      .issue_date = u"2010-09-01",
+  });
+  PrivatePass private_pass = EntityInstanceToPrivatePass(entity);
+  EXPECT_EQ(private_pass.pass_id(), entity.guid().value());
+  ASSERT_TRUE(private_pass.has_passport());
+  const Passport& pass = private_pass.passport();
+  EXPECT_EQ(pass.owner_name(), "Name");
+  EXPECT_EQ(pass.passport_number(), "12345678");
+  EXPECT_EQ(pass.country_code(), "AT");
+  EXPECT_EQ(pass.expiration_date_unix_epoch_micros(),
+            MicrosSinceUnixEpoch(2019, 8, 30));
+  EXPECT_EQ(pass.issue_date_unix_epoch_micros(),
+            MicrosSinceUnixEpoch(2010, 9, 1));
+}
+
+TEST(PrivatePassConversionUtil, DriverLicense) {
+  EntityInstance entity = test::GetDriversLicenseEntityInstance({
+      .name = u"Name",
+      .region = u"California",
+      .number = u"12345678",
+      .expiration_date = u"2019-08-30",
+      .issue_date = u"2010-09-01",
+  });
+  PrivatePass private_pass = EntityInstanceToPrivatePass(entity);
+  EXPECT_EQ(private_pass.pass_id(), entity.guid().value());
+  ASSERT_TRUE(private_pass.has_driver_license());
+  const DriverLicense& pass = private_pass.driver_license();
+  EXPECT_EQ(pass.owner_name(), "Name");
+  EXPECT_EQ(pass.driver_license_number(), "12345678");
+  EXPECT_EQ(pass.region(), "California");
+  EXPECT_EQ(pass.expiration_date_unix_epoch_micros(),
+            MicrosSinceUnixEpoch(2019, 8, 30));
+  EXPECT_EQ(pass.issue_date_unix_epoch_micros(),
+            MicrosSinceUnixEpoch(2010, 9, 1));
+}
+
+TEST(PrivatePassConversionUtil, NationalIdCard) {
+  EntityInstance entity = test::GetNationalIdCardEntityInstance({
+      .name = u"Name",
+      .number = u"12345678",
+      .country = u"Germany",
+      .issue_date = u"2010-09-01",
+      .expiry_date = u"2019-08-30",
+  });
+  PrivatePass private_pass = EntityInstanceToPrivatePass(entity);
+  EXPECT_EQ(private_pass.pass_id(), entity.guid().value());
+  ASSERT_TRUE(private_pass.has_id_card());
+  const IdCard& pass = private_pass.id_card();
+  EXPECT_EQ(pass.owner_name(), "Name");
+  EXPECT_EQ(pass.id_number(), "12345678");
+  EXPECT_EQ(pass.country_code(), "DE");
+  EXPECT_EQ(pass.issue_date_unix_epoch_micros(),
+            MicrosSinceUnixEpoch(2010, 9, 1));
+  EXPECT_EQ(pass.expiration_date_unix_epoch_micros(),
+            MicrosSinceUnixEpoch(2019, 8, 30));
+}
+
+TEST(PrivatePassConversionUtil, KnownTravelerNumber) {
+  EntityInstance entity = test::GetKnownTravelerNumberInstance({
+      .name = u"Name",
+      .number = u"12345678",
+      .expiration_date = u"2019-08-30",
+  });
+  PrivatePass private_pass = EntityInstanceToPrivatePass(entity);
+  EXPECT_EQ(private_pass.pass_id(), entity.guid().value());
+  ASSERT_TRUE(private_pass.has_known_traveler_number());
+  const KnownTravelerNumber& pass = private_pass.known_traveler_number();
+  EXPECT_EQ(pass.owner_name(), "Name");
+  EXPECT_EQ(pass.known_traveler_number(), "12345678");
+  EXPECT_EQ(pass.expiration_date_unix_epoch_micros(),
+            MicrosSinceUnixEpoch(2019, 8, 30));
+}
+
+TEST(PrivatePassConversionUtil, RedressNumber) {
+  EntityInstance entity = test::GetRedressNumberEntityInstance({
+      .name = u"Name",
+      .number = u"12345678",
+  });
+  PrivatePass private_pass = EntityInstanceToPrivatePass(entity);
+  EXPECT_EQ(private_pass.pass_id(), entity.guid().value());
+  ASSERT_TRUE(private_pass.has_redress_number());
+  const RedressNumber& pass = private_pass.redress_number();
+  EXPECT_EQ(pass.owner_name(), "Name");
+  EXPECT_EQ(pass.redress_number(), "12345678");
+}
+
+// Tests that missing attributes are omitted from the conversion.
+// Only tested for passports, since the conversion logic is implemented in an
+// entity-agnostic way.
+TEST(PrivatePassConversionUtil, PartialEntities) {
+  EntityInstance entity = test::GetPassportEntityInstance({
+      .name = nullptr,
+      .number = u"12345678",
+      .country = nullptr,
+      .expiry_date = nullptr,
+      .issue_date = nullptr,
+  });
+  PrivatePass private_pass = EntityInstanceToPrivatePass(entity);
+  EXPECT_EQ(private_pass.pass_id(), entity.guid().value());
+  ASSERT_TRUE(private_pass.has_passport());
+  const Passport& pass = private_pass.passport();
+  EXPECT_EQ(pass.passport_number(), "12345678");
+  EXPECT_FALSE(pass.has_owner_name());
+  EXPECT_FALSE(pass.has_country_code());
+  EXPECT_FALSE(pass.has_expiration_date_unix_epoch_micros());
+  EXPECT_FALSE(pass.has_issue_date_unix_epoch_micros());
+}
+
+// Tests that invalid and partial dates are dropped during conversion.
+// Only tested for passports, since the conversion logic is implemented in an
+// entity-agnostic way.
+TEST(PrivatePassConversionUtil, Dates) {
+  EntityInstance entity = test::GetPassportEntityInstance({
+      .name = nullptr,
+      .number = u"12345678",
+      .country = nullptr,
+      .expiry_date = u"1234",    // Invalid date
+      .issue_date = u"2010-09",  // Partial date
+  });
+  using enum AttributeTypeName;
+  ASSERT_TRUE(entity.attribute(AttributeType(kPassportExpirationDate)));
+  ASSERT_TRUE(entity.attribute(AttributeType(kPassportIssueDate)));
+  PrivatePass private_pass = EntityInstanceToPrivatePass(entity);
+  EXPECT_EQ(private_pass.pass_id(), entity.guid().value());
+  ASSERT_TRUE(private_pass.has_passport());
+  const Passport& pass = private_pass.passport();
+  EXPECT_EQ(pass.passport_number(), "12345678");
+  EXPECT_FALSE(pass.has_expiration_date_unix_epoch_micros());
+  EXPECT_FALSE(pass.has_issue_date_unix_epoch_micros());
+}
+
+}  // namespace
+
+}  // namespace autofill
