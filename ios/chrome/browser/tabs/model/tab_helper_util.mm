fd0f01ee77d362647f9ea482344af5a6687b9b51
Amarjot Gill <amarjotgill@chromium.org>
2026-02-17 09:48:04 -0800

    [M146] Attach tab_helper to ios::provider::AttachClassificationMetricsTabHelper
    Original change's description:
    > Attach tab_helper to ios::provider::AttachClassificationMetricsTabHelper
    > 
    > Bug: 469817333
    > Change-Id: I75c8866567a4df7c44d93802251688d64259c5fd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545858
    > Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Commit-Queue: Amarjot Gill <amarjotgill@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1584899}
    
    (cherry picked from commit a9ea2ffeb02de2a4193030b2144fad80a015032c)
    
    Bug: 485197317,469817333
    Change-Id: I75c8866567a4df7c44d93802251688d64259c5fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7584414
    Commit-Queue: Amarjot Gill <amarjotgill@chromium.org>
    Commit-Queue: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7680@{#521}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- ios/chrome/browser/tabs/model/tab_helper_util.mm
+++ ios/chrome/browser/tabs/model/tab_helper_util.mm
@@ -62,6 +62,7 @@
 #import "ios/chrome/browser/infobars/model/overlays/translate_overlay_tab_helper.h"
 #import "ios/chrome/browser/intelligence/bwg/model/bwg_tab_helper.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
+#import "ios/public/provider/chrome/browser/intelligence/classification_metrics_tab_helper_api.h"
 #import "ios/chrome/browser/itunes_urls/model/itunes_urls_handler_tab_helper.h"
 #import "ios/chrome/browser/lens/model/lens_tab_helper.h"
 #import "ios/chrome/browser/lens_overlay/coordinator/lens_overlay_availability.h"
@@ -477,4 +478,9 @@ void AttachTabHelpers(web::WebState* web_state, TabHelperFilter filter_flags) {
   attacher.CreateWhen<LastTapLocationTabHelper>(
       attacher.IsNotInTabHelperFilter() &&
       base::FeatureList::IsEnabled(kIOSCustomFileUploadMenu));
+
+  if (!attacher.IsOffTheRecord() && !attacher.IsForPrerender() &&
+      IsModelBasedPageClassificationEnabled()) {
+    ios::provider::AttachClassificationMetricsTabHelper(web_state);
+  }
 }
