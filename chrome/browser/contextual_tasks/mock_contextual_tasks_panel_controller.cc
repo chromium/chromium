1a1f1157077291ed2bce7a3167af2022390f6ecf
Shakti Sahu <shaktisahu@chromium.org>
2026-02-18 12:25:25 -0800

    [146] [contextual_tasks] MockContextualTasksPanelController
    Original change's description:
    > [contextual_tasks] MockContextualTasksPanelController
    > 
    > This CL moves MockContextualTasksPanelController to it's own
    > file to be reusable by other tests.
    > 
    > Bug: None
    > Change-Id: I4970f2bbcc0120865b94dcabde1974c6ded27a43
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7566283
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1584402}
    
    Bug: 485255493
    Change-Id: I4970f2bbcc0120865b94dcabde1974c6ded27a43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7589329
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7680@{#659}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

---
+++ chrome/browser/contextual_tasks/mock_contextual_tasks_panel_controller.cc
@@ -0,0 +1,15 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/contextual_tasks/mock_contextual_tasks_panel_controller.h"
+
+namespace contextual_tasks {
+
+MockContextualTasksPanelController::MockContextualTasksPanelController() =
+    default;
+
+MockContextualTasksPanelController::~MockContextualTasksPanelController() =
+    default;
+
+}  // namespace contextual_tasks
