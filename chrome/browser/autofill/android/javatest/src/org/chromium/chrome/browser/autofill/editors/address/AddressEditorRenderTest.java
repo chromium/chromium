7efda85f1d92f503599a1f700966bf7c6b128c98
Grzegorz (Greg) Grzelachowski <ggrzelachowski@google.com>
2026-02-18 09:17:42 -0800

    Revert "Disable failing AddressEditorRenderTest."
    This reverts commit 304d40dffc3252a0c31727b7a1d8805d1a882c61.
    
    Reason for revert: The problem with the test was mitigated
    by Henrique.
    
    Original change's description:
    > Disable failing AddressEditorRenderTest.
    >
    > Bug: 481769817
    > Change-Id: I0570c31ba63526fba403e2f9911b50b4a7b61328
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7545812
    > Commit-Queue: Evan Luo <evanluo@google.com>
    > Owners-Override: Sky Malice <skym@chromium.org>
    > Reviewed-by: Evan Luo <evanluo@google.com>
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Reviewed-by: Brandon Wylie <wylieb@google.com>
    > Commit-Queue: Dan Polanco <polardz@google.com>
    > Auto-Submit: Sky Malice <skym@chromium.org>
    > Reviewed-by: Dan Polanco <polardz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1579716}
    
    Bug: 481769817
    Change-Id: I12800d37efb73248a5158b596e11c8b27195d16a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7573133
    Commit-Queue: Evan Luo <evanluo@google.com>
    Auto-Submit: Grzegorz (Greg) Grzelachowski <ggrzelachowski@google.com>
    Reviewed-by: Evan Luo <evanluo@google.com>
    Reviewed-by: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586477}

--- chrome/browser/autofill/android/javatest/src/org/chromium/chrome/browser/autofill/editors/address/AddressEditorRenderTest.java
+++ chrome/browser/autofill/android/javatest/src/org/chromium/chrome/browser/autofill/editors/address/AddressEditorRenderTest.java
@@ -30,8 +30,6 @@ import org.chromium.base.test.params.ParameterAnnotations;
 import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
@@ -79,9 +77,6 @@ import java.util.List;
 @ParameterAnnotations.UseRunnerDelegate(ChromeJUnit4RunnerDelegate.class)
 @Restriction({DeviceFormFactor.PHONE, RESTRICTION_TYPE_NON_LOW_END_DEVICE})
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
-@DisableIf.Build(supported_abis_includes = "x86", message = "https://crbug.com/378544621")
-@DisableIf.Build(supported_abis_includes = "x86_64", message = "https://crbug.com/378544621")
-@DisabledTest(message = "https://crbug.com/481769817")
 public class AddressEditorRenderTest {
     private static final String USER_EMAIL = "example@gmail.com";
     private static final List<AutofillAddressUiComponent> SUPPORTED_ADDRESS_FIELDS =
