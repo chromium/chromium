881807591109234dd1ca052b63bc9efe9fc30aef
David Benjamin <davidben@chromium.org>
2026-02-18 09:54:35 -0800

    Replace wincrypt_shim.h's undefs with WINCRYPT_USE_SYMBOL_PREFIX
    The Windows SDK now lets you suppress the offending macros. This CL
    makes wincrypt_shim.h a no-op. It if goes through, follow up changes can
    remove this shim.
    
    Bug: 483973077
    Change-Id: I76825101e215d404c4120a443bdea53f153a8c52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571424
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586522}

--- base/win/wincrypt_shim.h
+++ base/win/wincrypt_shim.h
@@ -5,23 +5,13 @@
 #ifndef BASE_WIN_WINCRYPT_SHIM_H_
 #define BASE_WIN_WINCRYPT_SHIM_H_
 
-// Any Chromium headers which want to `#include <wincrypt.h>` should instead
-// #include this header.
+// Deprecated. Historically, it was necessary to use this shim header to include
+// <wincrypt.h>.
+// TODO(crbug.com/483973077): Replace uses of this header with a normal
+// wincrypt.h include.
 
 #include <windows.h>
 
 #include <wincrypt.h>
-
-// wincrypt.h defines macros which conflict with OpenSSL's types. See
-// http://msdn.microsoft.com/en-us/library/windows/desktop/aa378145(v=vs.85).aspx
-#undef PKCS7_SIGNER_INFO
-#undef X509_CERT_PAIR
-#undef X509_EXTENSIONS
-#undef X509_NAME
-
-#define WINCRYPT_PKCS7_SIGNER_INFO ((LPCSTR)500)
-#define WINCRYPT_X509_CERT_PAIR ((LPCSTR)53)
-#define WINCRYPT_X509_EXTENSIONS ((LPCSTR)5)
-#define WINCRYPT_X509_NAME ((LPCSTR)7)
 
 #endif  // BASE_WIN_WINCRYPT_SHIM_H_
