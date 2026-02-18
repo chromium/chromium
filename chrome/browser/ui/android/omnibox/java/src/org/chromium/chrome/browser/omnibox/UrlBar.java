6a0ed6c755275f9f12cf4904567a939f7e37fd1b
Tomasz Wiszkowski <ender@google.com>
2026-02-18 15:47:49 -0800

    Ensure scrollToBeginning and scrollToTLD reveal the top text line.
    When navigating with a multiline query that scrolls the fusebox, the
    toolbar stays scrolled post-focus/nav. This change ensures we reset
    the scroll position when calling scrollToTLD or scrollToBeginning.
    
    Both these calls are used to reveal the contents of the UrlBar at
    specific point, however at the moment the focus is lost the Y scroll
    position is offset depending on where the user was when they were done
    editing the text. Both calls propagated this vertical scroll position
    while focusing on horizontal adjustment only. For scrolled edit text
    this meant the content would be outside the viewport.
    
    The original `getScrollY()` call made here previously made sense for the
    UrlBar with vertical layout `match_parent`; we migrated to
    `wrap_content` since so 0 should do fine.
    
    https://crsrc.org/chromium/chromium/src/+/main:chrome/android/java/res/layout/url_bar.xml;l=12
    
    Fixed: 483875381
    Change-Id: Ie7e6bfaba80d5c8708c6ba42c8482d6045e5bcf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7590154
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1586739}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
@@ -836,7 +836,7 @@ public class UrlBar extends AutocompleteEditText {
                 && currentTextSize == mPreviousScrollFontSize
                 && currentIsRtl == mPreviousScrollWasRtl
                 && isVisibleTextTheSame(text)) {
-            scrollTo(mPreviousScrollResultXPosition, getScrollY());
+            scrollTo(mPreviousScrollResultXPosition, 0);
 
             return;
         }
@@ -891,7 +891,7 @@ public class UrlBar extends AutocompleteEditText {
             float width = layout.getPaint().measureText(text.toString());
             scrollPos = Math.max(0, endPointX - measuredWidth + width);
         }
-        scrollTo((int) scrollPos, getScrollY());
+        scrollTo((int) scrollPos, 0);
     }
 
     @Override
@@ -1088,7 +1088,7 @@ public class UrlBar extends AutocompleteEditText {
                 scrollPos = endPointX + measuredWidth;
             }
         }
-        scrollTo((int) scrollPos, getScrollY());
+        scrollTo((int) scrollPos, 0);
     }
 
     @Override
