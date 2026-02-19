3d02e6a8e71661bd6c0f2ac8837b2653d6b288d7
Eleanor Lee <eleanorlee@google.com>
2026-02-19 15:46:46 -0800

    [FRE] Create the new primer UI for arm 2 (landscape mode)
    After: https://screencast.googleplex.com/cast/NDcyMTY0MTM4Mzc4ODU0NHw1MjAwNGJmMi05Mg
    
    Notice this CL is currently not using the correct fre product logo shown in Figma. This will be modified in a follow-up CL.
    
    DISABLE_SPELLCHECKER
    
    Bug: 483539670
    Change-Id: I67a3710424a88bc75997b221e3d003d7db5e8ed9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7578843
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Eleanor Lee <eleanorlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1587480}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/DefaultBrowserPromoFirstRunFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/DefaultBrowserPromoFirstRunFragment.java
@@ -6,17 +6,20 @@ package org.chromium.chrome.browser.firstrun;
 
 import static org.chromium.build.NullUtil.assumeNonNull;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.FrameLayout;
 
 import androidx.fragment.app.Fragment;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ui.signin.SigninUtils;
 
 @NullMarked
 public class DefaultBrowserPromoFirstRunFragment extends Fragment implements FirstRunFragment {
@@ -27,18 +30,45 @@ public class DefaultBrowserPromoFirstRunFragment extends Fragment implements Fir
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
 
-        DefaultBrowserPromoFirstRunView view =
-                (DefaultBrowserPromoFirstRunView)
-                        inflater.inflate(
-                                R.layout.default_browser_promo_fre_portrait_view, container, false);
+        FrameLayout rootView = new FrameLayout(getActivity());
+        updateView(inflater, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        ViewGroup rootView = (ViewGroup) getView();
+        if (rootView != null) {
+            // Remove the old view (e.g. the portrait/landscape version) that's still physically
+            // inside the FrameLayout.
+            rootView.removeAllViews();
+            updateView(getLayoutInflater(), rootView);
+        }
+    }
+
+    private void updateView(LayoutInflater inflater, ViewGroup container) {
+        // TODO(https://crbug.com/483539670): Re-use this method and move it to a shared location.
+        boolean useLandscape = SigninUtils.shouldShowDualPanesHorizontalLayout(getActivity());
+
+        int layoutId =
+                useLandscape
+                        ? R.layout.default_browser_promo_fre_landscape_view
+                        : R.layout.default_browser_promo_fre_portrait_view;
+
+        // Inflate without attaching to root to avoid a ClassCastException, since we can't cast a
+        // FrameLayout to a DefaultBrowserPromoFirstRunView.
+        View inflatedView = inflater.inflate(layoutId, container, false);
+        // Perform the cast here.
+        DefaultBrowserPromoFirstRunView view = (DefaultBrowserPromoFirstRunView) inflatedView;
+        // Manually add it to the FrameLayout wrapper.
+        container.addView(view);
 
         // These are just placeholders for the primer. Will upload a follow-up CL for the actual
         // logic.
         var pageDelegate = assumeNonNull(getPageDelegate());
         view.getContinueButtonView().setOnClickListener(v -> pageDelegate.advanceToNextPage());
         view.getDismissButtonView().setOnClickListener(v -> pageDelegate.advanceToNextPage());
-
-        return view;
     }
 
     /** Implements {@link FirstRunFragment}. */
