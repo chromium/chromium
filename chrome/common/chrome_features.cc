42d6551341a572198196ed0959cd5c443a2e6f63
Sana Akbani <sanaakbani@google.com>
2026-02-19 14:55:24 -0800

    Default enable GlicMITabContextMenu
    Bug: b:469768350
    Change-Id: I6b182997678b28927bcf7bdeba48ef0e5e3ed27e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595141
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587441}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -986,7 +986,7 @@ BASE_FEATURE(kGlicDaisyChainNewTabs, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGlicLiveModeOnlyGlow, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kGlicMITabContextMenu, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kGlicMITabContextMenu, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGlicWebContinuity, base::FEATURE_DISABLED_BY_DEFAULT);
 const base::FeatureParam<std::string> kGlicWebContinuityUrl{
