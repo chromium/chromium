f0a5ecc188de996ca7e942c8d3101202b12c0f1c
William Liu <liuwilliam@chromium.org>
2026-02-18 18:26:31 -0800

    [1/N] Add mojom for glic selection overlay
    This is part of an effort that adds a new WebUI component for glic's
    selection overlay. In this CL only a skeleton C++ implementation is
    included. The WebUI will land as follow-ups.
    
    Bug: b:479179977
    Change-Id: I10b7e666327821ed5ebaac02e33f1036607117f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7585593
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1586836}

--- chrome/browser/glic/selection/selection_overlay_controller.cc
+++ chrome/browser/glic/selection/selection_overlay_controller.cc
@@ -4,12 +4,44 @@
 
 #include "chrome/browser/glic/selection/selection_overlay_controller.h"
 
+#include "base/strings/to_string.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/branded_strings.h"
 #include "components/viz/common/frame_sinks/copy_output_result.h"
 #include "content/public/browser/render_view_host.h"
+#include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkPaint.h"
+#include "ui/gfx/codec/jpeg_codec.h"
+#include "ui/gfx/geometry/skia_conversions.h"
+
+// TODO(http://b/485358530): Consider `OverlayBaseController::State` to the
+// mojom file so the << operator is auto generated.
+std::ostream& operator<<(std::ostream& os, OverlayBaseController::State value) {
+  switch (value) {
+    case OverlayBaseController::State::kOff:
+      return os << "kOff";
+    case OverlayBaseController::State::kClosingOpenedSidePanel:
+      return os << "kClosingOpenedSidePanel";
+    case OverlayBaseController::State::kScreenshot:
+      return os << "kScreenshot";
+    case OverlayBaseController::State::kStartingWebUI:
+      return os << "kStartingWebUI";
+    case OverlayBaseController::State::kOverlay:
+      return os << "kOverlay";
+    case OverlayBaseController::State::kHidden:
+      return os << "kHidden";
+    case OverlayBaseController::State::kBackground:
+      return os << "kBackground";
+    case OverlayBaseController::State::kClosing:
+      return os << "kClosing";
+    case OverlayBaseController::State::kIsReshowing:
+      return os << "kIsReshowing";
+    case OverlayBaseController::State::kHiding:
+      return os << "kHiding";
+  }
+}
 
 namespace glic {
 
@@ -37,6 +69,19 @@ SelectionOverlayController* SelectionOverlayController::FromTabWebContents(
                  ->GetUnownedUserDataHost());
 }
 
+void SelectionOverlayController::BindOverlay(
+    mojo::PendingReceiver<selection::SelectionOverlayPageHandler> receiver,
+    mojo::PendingRemote<selection::SelectionOverlayPage> page) {
+  CHECK_EQ(state(), State::kStartingWebUI) << base::ToString(state());
+
+  receiver_.Bind(std::move(receiver));
+  receiver_.set_disconnect_handler(base::BindOnce(
+      &SelectionOverlayController::Reset, weak_factory_.GetWeakPtr()));
+  page_.Bind(std::move(page));
+
+  InitializeOverlay();
+}
+
 void SelectionOverlayController::Show() {
   ShowModalUI();
 }
@@ -59,6 +104,9 @@ void SelectionOverlayController::InitializeOverlay() {
   }
 
   InitializeOverlayImpl();
+
+  CHECK(page_);
+  page_->ScreenshotReceived(initial_rgb_screenshot_);
 }
 
 void SelectionOverlayController::StartScreenshotFlow() {
@@ -134,5 +182,77 @@ void SelectionOverlayController::NotifyPageNavigated() {}
 void SelectionOverlayController::NotifyTabForegrounded() {}
 
 void SelectionOverlayController::NotifyTabWillEnterBackground() {}
+
+void SelectionOverlayController::DismissOverlay(
+    selection::DismissOverlayReason reason) {
+  CloseUI();
+}
+
+void SelectionOverlayController::AdjustRegion(
+    selection::SelectedRegionPtr target) {
+  auto it = selected_regions_.find(target->id);
+  if (it != selected_regions_.end()) {
+    it->second = std::move(target);
+  } else {
+    selected_regions_[target->id] = std::move(target);
+  }
+
+  RenderRegions();
+}
+
+void SelectionOverlayController::DeleteRegion(
+    const base::UnguessableToken& id) {
+  if (selected_regions_.erase(id)) {
+    RenderRegions();
+  }
+}
+
+void SelectionOverlayController::Reset() {
+  receiver_.reset();
+  page_.reset();
+  initial_rgb_screenshot_.reset();
+  initial_screenshot_.reset();
+  screenshot_available_ = false;
+  encoded_.reset();
+  selected_regions_.clear();
+}
+
+void SelectionOverlayController::RenderRegions() {
+  if (initial_screenshot_.empty()) {
+    return;
+  }
+
+  SkBitmap deep_copy_bitmap;
+  // TODO(http://b/485358530): Record proper histograms for the error case.
+  // Allocate memory for the deep copy.
+  if (!deep_copy_bitmap.tryAllocPixels(initial_screenshot_.info())) {
+    LOG(ERROR) << "Alloc failure";
+    return;
+  }
+
+  SkCanvas canvas(deep_copy_bitmap);
+  canvas.drawImage(initial_screenshot_.asImage(), 0, 0);
+  SkPaint paint;
+  paint.setColor(SK_ColorMAGENTA);
+  paint.setStyle(SkPaint::kStroke_Style);
+  paint.setStrokeWidth(2.0f);
+
+  // TODO(http://b/452032491): Reconsider what happens if the regions overlap.
+  // TODO(http://b/452032491): Currently this class is only used once per
+  // selection and only one region is supported, so it is fine to always loop
+  // through all the regions. Revisit once we expand the selections.
+  for (const auto& [id, region] : selected_regions_) {
+    SkRect rect_on_canvas = gfx::RectFToSkRect(region->region);
+    if (!rect_on_canvas.isEmpty() &&
+        initial_screenshot_.bounds().contains(rect_on_canvas)) {
+      canvas.drawRect(rect_on_canvas, paint);
+    } else {
+      // TODO(http://b/485358530): Record proper histograms for the error case.
+      LOG(ERROR) << "Invalid region selected " << region->region.ToString();
+    }
+  }
+
+  encoded_ = gfx::JPEGCodec::Encode(deep_copy_bitmap, 40);
+}
 
 }  // namespace glic
--- chrome/browser/glic/selection/selection_overlay_controller.h
+++ chrome/browser/glic/selection/selection_overlay_controller.h
@@ -5,13 +5,22 @@
 #ifndef CHROME_BROWSER_GLIC_SELECTION_SELECTION_OVERLAY_CONTROLLER_H_
 #define CHROME_BROWSER_GLIC_SELECTION_SELECTION_OVERLAY_CONTROLLER_H_
 
+#include "base/containers/flat_map.h"
 #include "base/memory/weak_ptr.h"
+#include "base/unguessable_token.h"
+#include "chrome/browser/glic/selection/selection_overlay.mojom.h"
 #include "chrome/browser/ui/lens/overlay_base_controller.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/remote.h"
 #include "ui/base/unowned_user_data/scoped_unowned_user_data.h"
 
 namespace glic {
 
-class SelectionOverlayController : public OverlayBaseController {
+class SelectionOverlayController
+    : public OverlayBaseController,
+      public selection::SelectionOverlayPageHandler {
  public:
   SelectionOverlayController(tabs::TabInterface* tab,
                              PrefService* pref_service);
@@ -32,6 +41,13 @@ class SelectionOverlayController : public OverlayBaseController {
   // `tab_web_contents`.
   static SelectionOverlayController* FromTabWebContents(
       content::WebContents* tab_web_contents);
+
+  // This method is used to set up communication between this instance and the
+  // overlay WebUI. This is called by the WebUIController when the WebUI is
+  // executing javascript and ready to bind.
+  void BindOverlay(
+      mojo::PendingReceiver<selection::SelectionOverlayPageHandler> receiver,
+      mojo::PendingRemote<selection::SelectionOverlayPage> page);
 
   void Show();
   void Hide();
@@ -58,15 +74,36 @@ class SelectionOverlayController : public OverlayBaseController {
   void NotifyTabForegrounded() override;
   void NotifyTabWillEnterBackground() override;
 
+  // `selection::SelectionOverlayPageHandler`:
+  void DismissOverlay(selection::DismissOverlayReason reason) override;
+  void AdjustRegion(selection::SelectedRegionPtr target) override;
+  void DeleteRegion(const base::UnguessableToken& id) override;
+
  private:
   void OnScreenshotTaken(const content::CopyFromSurfaceResult& result);
 
   void SetScreenshot(const SkBitmap& screenshot, SkBitmap rgb_screenshot);
 
+  // Render all the `selected_regions_` on top of `initial_screenshot_`.
+  void RenderRegions();
+
+  void Reset();
+
+  // Connections to and from the overlay WebUI. Only valid while
+  // `OverlayBaseController::overlay_view_` is showing and the underlying
+  // renderer is alive.
+  mojo::Receiver<selection::SelectionOverlayPageHandler> receiver_{this};
+  mojo::Remote<selection::SelectionOverlayPage> page_;
+
+  // Stateful members. They should be added to Reset().
   bool screenshot_available_ = false;
   SkBitmap initial_screenshot_;
   SkBitmap initial_rgb_screenshot_;
   std::optional<std::vector<uint8_t>> encoded_;
+  // Caches the user-selected region. To be renderer on top of
+  // `initial_screenshot_`.
+  base::flat_map<base::UnguessableToken, selection::SelectedRegionPtr>
+      selected_regions_;
 
   ui::ScopedUnownedUserData<SelectionOverlayController>
       scoped_unowned_user_data_;
