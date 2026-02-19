910b1ea4183f7d7488db0c5802c5b9e9794208e1
Alexander Cooper <alcooper@chromium.org>
2026-02-19 06:01:42 -0800

    Enable WebXR Plane Detection Feature
    Enables the "plane-detection" feature for WebXR sessions.
    
    https://chromestatus.com/feature/5177993049800704
    
    Fixed: 394636076
    Change-Id: Iff5c58f463d88826ef0745c9d661812cda706b47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7572746
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1587053}

--- device/vr/public/cpp/features.cc
+++ device/vr/public/cpp/features.cc
@@ -37,7 +37,7 @@ BASE_FEATURE(kWebXROrientationSensorDevice,
 );
 
 // Enables access to the WebXR plane-detection feature
-BASE_FEATURE(kWebXRPlaneDetection, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kWebXRPlaneDetection, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Allows blink to process the `visible-blurred` state.
 BASE_FEATURE(kWebXrVisibleBlurred, base::FEATURE_ENABLED_BY_DEFAULT);
