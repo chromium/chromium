75870515c9d90b27fe94cb1d547eebb61b79a5dc
Baran Erfani <baranerf@google.com>
2026-02-19 15:00:56 -0800

    Revert "Record metrics on why an update hasn't yet been applied"
    This reverts commit bfaae419b3084cebb410039b5d2951329f62fd08.
    
    Reason for revert: Causing failures Mac12 Tests - please see https://ci.chromium.org/ui/p/chromium/builders/ci/Mac12%20Tests/40267/overview
    
    Original change's description:
    > Record metrics on why an update hasn't yet been applied
    >
    > This CL emits histograms on every UMA upload to track
    > the state of Chrome when an update is available and not
    > yet applied. This will inform future work to optimize
    > when updates are applied. Metrics are added only for
    > desktop platforms as Android, iOS and ChromeOS use
    > OS provided update mechanisms.
    >
    > Bug: 423663200
    > Change-Id: I205398928160ab06add3a0fa80f51e50ab00ac0a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7543057
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Mark Pearson <mpearson@chromium.org>
    > Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Commit-Queue: Alex Gough <ajgo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1587190}
    
    Bug: 423663200
    Change-Id: I51076f8d5ee718aefae14cb21e9a9292cf8b2200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595736
    Owners-Override: Baran Erfani <baranerf@google.com>
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Auto-Submit: Baran Erfani <baranerf@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1587446}

--- chrome/browser/metrics/chrome_metrics_service_client.cc
+++ chrome/browser/metrics/chrome_metrics_service_client.cc
@@ -235,10 +235,6 @@
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) ||
         // BUILDFLAG(IS_CHROMEOS)
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-#include "chrome/browser/updates/update_metrics_provider.h"
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-
 namespace {
 
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS)
@@ -1027,8 +1023,6 @@ void ChromeMetricsServiceClient::RegisterMetricsServiceProviders() {
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
   metrics_service_->RegisterMetricsProvider(
       metrics::CreateDesktopSessionMetricsProvider());
-  metrics_service_->RegisterMetricsProvider(
-      std::make_unique<UpdateMetricsProvider>());
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || (BUILDFLAG(IS_LINUX)
 
 #if BUILDFLAG(ENABLE_GLIC)
