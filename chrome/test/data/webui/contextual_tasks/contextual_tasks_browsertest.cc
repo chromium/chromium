b3483f7377b1e47249b5abdc290cec3ceec7fa97
Hiroki Nakagawa <nhiroki@chromium.org>
2026-02-19 16:17:05 -0800

    [146] Gardedning: Disable ContextualTasksBrowserTest.Composebox
    Original change's description:
    > Gardedning: Disable ContextualTasksBrowserTest.Composebox
    > 
    > The test is flaky:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests%20(dbg)(1)/130553/overview
    > 
    > Bug: 480689282
    > Change-Id: I5d98ea592645bf08eac3f83dbc05bc3dedce9bce
    > No-try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7580137
    > Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    > Auto-Submit: Hiroki Nakagawa <nhiroki@chromium.org>
    > Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    > Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1585265}
    
    Bug: 485880948
    Change-Id: I5d98ea592645bf08eac3f83dbc05bc3dedce9bce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594878
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7680@{#791}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- chrome/test/data/webui/contextual_tasks/contextual_tasks_browsertest.cc
+++ chrome/test/data/webui/contextual_tasks/contextual_tasks_browsertest.cc
@@ -27,7 +27,8 @@ IN_PROC_BROWSER_TEST_F(ContextualTasksBrowserTest, App) {
   RunTest("contextual_tasks/app_test.js", "mocha.run();");
 }
 
-IN_PROC_BROWSER_TEST_F(ContextualTasksBrowserTest, Composebox) {
+// TODO(crbug.com/480689282): Test disabled because it is flaky.
+IN_PROC_BROWSER_TEST_F(ContextualTasksBrowserTest, DISABLED_Composebox) {
   RunTest("contextual_tasks/composebox_test.js", "mocha.run();");
 }
 
