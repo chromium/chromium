9755d262e3fc0a4cf91c24139c490556a96b487f
Erik Chen <erikchen@chromium.org>
2026-02-20 00:54:53 -0800

    [m145] Hook up auto send for glic summarize button
    > Hook up auto send for glic summarize button
    >
    > Bug: 483727155
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7586756
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1586212}
    >
    
    Bug: 483727155
    Change-Id: Ic1ee4633dc2ddf74061ecf3771b7079a07ab22f1
    Fixed: 485983500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7595072
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7632@{#3066}
    Cr-Branched-From: 0bbdf2913883391365383b0a5dfe7bf9fd1a5213-refs/heads/main@{#1568190}

--- chrome/browser/extensions/api/pdf_viewer_private/pdf_viewer_private_api.cc
+++ chrome/browser/extensions/api/pdf_viewer_private/pdf_viewer_private_api.cc
@@ -318,7 +318,9 @@ ExtensionFunction::ResponseAction PdfViewerPrivateGlicSummarizeFunction::Run() {
 
   glic_service->ToggleUI(tab_interface->GetBrowserWindowInterface(),
                          /*prevent_close=*/true,
-                         glic::mojom::InvocationSource::kPdfSummarizeButton);
+                         glic::mojom::InvocationSource::kPdfSummarizeButton,
+                         /*prompt_suggestion=*/"summarize the pdf",
+                         /*auto_send=*/true);
 
   success = true;
   return RespondNow(NoArguments());
