8fc25b627c103212229371bfcc89d6f1d45d4dae
Devon Loehr <dloehr@google.com>
2026-02-21 12:07:08 -0800

    [rustmojo] Add support for mojom files including other mojom files
    This CL adds the ability for the rust generator to handle mojom files
    that depend on each other. We do so as follows:
    1. Each `mojom` GN target creates a new crate
    2. Each mojom file creates a new module within that crate
    3. The generator uses metadata provided by the mojom parser to determine
       what path to use to refer to each type.
    
    We had to tweak the gni file a bit to make sure the generator is invoked
    with all the required metatada.
    
    Note that we completely ignore the `module` keyword in the mojom file
    itself. That keyword is meant to emulate C++ namespaces, but rust very
    aggressively does _not_ support such a thing. In particular, if we have
    two mojom files that declare the same `module`, but they appear in
    different crates, we have no way to unify their modules the way that C++
    would.
    
    Therefore, instead of trying to fake namespaces where possible, we opt
    to ignore them entirely and save ourselves the clutter of having
    a bunch of useless modules and submodules.
    
    To test this code, we add a new directory for holding tests that don't
    belong to any particular component. I tried to re-use existing tests,
    but our GN setup is extremely hostile to this -- you can't put a mojom
    file in multiple GN targets, but we also can't re-use the existing
    targets because we don't support all the mojom features yet. For the
    foreseeable future, we'll just have to write our own tests.
    
    There are also a few opportunistic fixes to templates to make
    whitespace nicer, and remove a leading comma that we would sometimes
    generate which cause compilation errors.
    
    Bug: 444509367
    Change-Id: Ic9f99383481b27a7c4e8e254c20a1df351f86bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594953
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1588342}

---
+++ mojo/public/rust/tests/README.md
@@ -0,0 +1,2 @@
+This folder contains integration tests that don't belong to any single component
+of the mojo rust bindings.
--- mojo/public/tools/bindings/generators/mojom_rust_generator.py
+++ mojo/public/tools/bindings/generators/mojom_rust_generator.py
@@ -4,6 +4,7 @@
 """Generates Rust source files from a mojom.Module."""
 
 import sys
+import json
 
 import mojom.generate.generator as generator
 import mojom.generate.module as mojom
@@ -29,29 +30,82 @@ _mojom_primitive_type_to_rust_type = {
 }
 
 
-def _MojomTypeToRustType(ty: mojom.Kind) -> str:
+def _SameGNTarget(mod1: mojom.Module, mod2: mojom.Module,
+                  source_to_target_map: dict) -> bool:
+  return source_to_target_map[mod1.path] == source_to_target_map[mod2.path]
+
+
+# Determine the right path to use to refer to `ty` from the current module.
+#
+# Each `mojom` GN target gets its own crate, and each `.mojom` file gets its
+# own module within that crate. All the elements of a module appear at top
+# level (there are no submodules). In order to figure out how to refer to `ty`,
+# we need to determine if it was defined in the current file, the current crate,
+# or an external crate.
+#
+# IMPORTANT NOTE:
+# Mojom provides its own namespace mechanism using the `module` keyword. These
+# are meant to mimic C++ namespaces; items declared inside the same `module` can
+# be referred to using the same qualified path, regardless of what file they
+# were defined in.
+#
+# Unfortunately, we can't provide this in rust; if two files are in different
+# crates, there's no way we can automatically put them in the same `mod`.
+# Therefore, we _completely ignore_ the `module` keyword when generating Rust
+# code from a mojom file, and name things using _only_ the file system and GN
+# structure.
+def _GetQualifiedName(ty: mojom.Kind, current_module: mojom.Module,
+                      source_to_target_map: dict) -> str:
+  # This is the last part of the name in the mojom file, e.g. foo.bar.T -> T
+  base_name = ty.qualified_name.split('.')[-1]
+  # If the type was defined in this file, we can use its name unqualified
+  if ty.module.path == current_module.path:
+    return base_name
+
+  # The module has the same name as the file that defined it, sans extension
+  # Map foo/bar/baz.mojom -> baz
+  ty_module_name = ty.module.path.split('/')[-1].split('.')[0]
+
+  # If the type was defined as part of the same GN target as this file, then
+  # it's in the same crate.
+  if _SameGNTarget(ty.module, current_module, source_to_target_map):
+    return f"crate::{ty_module_name}::{base_name}"
+
+  # Otherwise, it was defined in a different crate, which has the same name as
+  # as the GN target that defined it.
+  extern_target_name = source_to_target_map[ty.module.path]
+  # Map //foo/bar:baz -> baz, and //foo/bar -> bar
+  extern_crate = extern_target_name.split(':')[-1].split('/')[-1]
+  return f"{extern_crate}::{ty_module_name}::{base_name}"
+
+
+def _MojomTypeToRustType(ty: mojom.Kind, current_module: mojom.Module,
+                         source_to_target_map: dict) -> str:
   '''Return the name of the input type in rust syntax'''
   if mojom.IsNullableKind(ty):
-    inner_ty = _MojomTypeToRustType(ty.MakeUnnullableKind())
+    inner_ty = _MojomTypeToRustType(ty.MakeUnnullableKind(), current_module,
+                                    source_to_target_map)
     return f"Option<{inner_ty}>"
 
   # FOR_RELEASE: We don't support nested enums yet
   if mojom.IsStructKind(ty) or mojom.IsEnumKind(ty) or mojom.IsUnionKind(ty):
-    return ty.name
+    return _GetQualifiedName(ty, current_module, source_to_target_map)
 
   if mojom.IsArrayKind(ty):
-    elt_ty = _MojomTypeToRustType(ty.kind)
+    elt_ty = _MojomTypeToRustType(ty.kind, current_module, source_to_target_map)
     if ty.length is not None:
       return f"[{elt_ty}; {ty.length}]"
     else:
       return f"Vec<{elt_ty}>"
 
   if mojom.IsMapKind(ty):
-    key_ty = _MojomTypeToRustType(ty.key_kind)
+    key_ty = _MojomTypeToRustType(ty.key_kind, current_module,
+                                  source_to_target_map)
     # Rust requires comparison operators to use floats as keys in a map
     if ty.key_kind == mojom.FLOAT or ty.key_kind == mojom.DOUBLE:
       key_ty = f"OrderedFloat<{key_ty}>"
-    value_ty = _MojomTypeToRustType(ty.value_kind)
+    value_ty = _MojomTypeToRustType(ty.value_kind, current_module,
+                                    source_to_target_map)
     return f"HashMap<{key_ty}, {value_ty}>"
 
   if ty not in _mojom_primitive_type_to_rust_type:
@@ -62,23 +116,25 @@ def _MojomTypeToRustType(ty: mojom.Kind) -> str:
 
   return _mojom_primitive_type_to_rust_type[ty]
 
+
 def _ShouldDeriveClone(ty: mojom.Kind) -> bool:
   '''We derive clone as a convenience to the user, but we can't do so if the
      type contains any handles, since those can't be copied.'''
   return not mojom.ContainsHandlesOrInterfaces(ty)
 
+
 class Generator(generator.Generator):
 
   def __init__(self, *args, **kwargs):
     super(Generator, self).__init__(*args, **kwargs)
+    self.source_to_target_map = {}
 
   @staticmethod
   def GetTemplatePrefix():
     '''Returns the name of the directory storing the rust jinja templates.'''
     return "rust_templates"
 
-  @staticmethod
-  def GetFilters():
+  def GetFilters(self):
     '''
     Returns a dictionary of functions that will be callable when processing
     the mojom template, using the syntax `arg|f` to mean `f(arg)`.
@@ -86,8 +142,11 @@ class Generator(generator.Generator):
     Called by the @UseJinja decorator.
     '''
     return {
-        "to_rust_type": _MojomTypeToRustType,
-        "should_derive_clone": _ShouldDeriveClone
+        "to_rust_type":
+        lambda ty: _MojomTypeToRustType(ty, self.module, self.
+                                        source_to_target_map),
+        "should_derive_clone":
+        _ShouldDeriveClone,
     }
 
   @UseJinja("module.tmpl")
@@ -99,7 +158,18 @@ class Generator(generator.Generator):
     Before decoration, returns a dictionary of variables that will be bound at
     the top level when processing the jinja template.
     '''
-    return {"module": self.module}
+
+    # Take all our imports and determine the set of GN targets they come from
+    imported_targets = set([
+        self.source_to_target_map[imprt.path] for imprt in self.module.imports
+    ])
+    # Remove our own target, since we don't import ourselves
+    imported_targets -= {self.source_to_target_map[self.module.path]}
+
+    return {
+        "module": self.module,
+        "imports": imported_targets,
+    }
 
   def GenerateFiles(self, unparsed_args):
     '''
@@ -109,5 +179,19 @@ class Generator(generator.Generator):
     '''
     # Make sure all the AST nodes have pretty names
     self.module.Stylize(generator.Stylizer())
+
+    # When GN calls this script, it provides a JSON file with a list of GN
+    # targets, and their source files. It contains one entry for the mojom
+    # target we're generating now, and one for each of its dependencies.
+    # We want to convert it to a map from source files to GN targets.
+    for arg in unparsed_args:
+      if arg.startswith("--rust_dep_info="):
+        dep_info_path = arg.split('=')[1]
+        with open(dep_info_path, 'r') as f:
+          raw_dep_info = json.load(f)
+          for target in raw_dep_info:
+            target_name = target['target_name']
+            for source in target['mojom_sources']:
+              self.source_to_target_map[source] = target_name
 
     self.WriteWithComment(self._GenerateModule(), f"{self.module.path}.rs")
