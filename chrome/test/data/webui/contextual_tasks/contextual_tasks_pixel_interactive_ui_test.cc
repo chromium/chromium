652a874ca92128f6d93986ea74273e6b473798e1
Diana OuYang <dianaou@google.com>
2026-02-23 09:36:15 -0800

    [146] [Contextual Tasks] Change font size from 18 to 16
    Original change's description:
    > [Contextual Tasks] Change font size from 18 to 16
    > 
    > I11e33fb5984d7189ab9c8fb940b19d13d2d716b0
    > 
    > Bug: b:480256471
    > Change-Id: I9d7a023fc9c628979b785ce8d0e41d5a4ea43428
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7589836
    > Auto-Submit: Diana OuYang <dianaou@google.com>
    > Reviewed-by: Duncan Mercer <mercerd@google.com>
    > Commit-Queue: Diana OuYang <dianaou@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1587411}
    
    Bug: 486259912
    Change-Id: I9d7a023fc9c628979b785ce8d0e41d5a4ea43428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7599476
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7680@{#1110}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
+++ chrome/test/data/webui/contextual_tasks/contextual_tasks_pixel_interactive_ui_test.cc
@@ -205,7 +205,7 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksComposeBoxPixelTest, Screenshots) {
       // Take a screenshot of the composebox.
       ScreenshotWebUi(kActiveTab, kComposebox,
                       /*screenshot_name=*/"ContextualTasksComposebox",
-                      /*baseline_cl=*/"7542872"));
+                      /*baseline_cl=*/"7589836"));
 }
 
 struct AppPixelTestParams {
@@ -316,7 +316,7 @@ IN_PROC_BROWSER_TEST_P(ContextualTasksAppPixelTest, Screenshots) {
       SetOnIncompatibleAction(OnIncompatibleAction::kIgnoreAndContinue,
                               "Screenshots not captured on this platform."),
       ScreenshotWebUi(kActiveTab, kApp, "ContextualTasksApp",
-                      /*baseline_cl=*/"7577477"));
+                      /*baseline_cl=*/"7589836"));
 }
 
 enum class TitleType { kNone, kShort, kLong };
