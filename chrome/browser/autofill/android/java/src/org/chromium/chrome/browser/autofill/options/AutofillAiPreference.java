83a7da1b40f63fac5fd7ca310e2847cd0568ffea
Timofey Chudakov <tchudakov@google.com>
2026-02-23 07:10:48 -0800

    [AutofillAi - M4] Group AI toggle with info items.
    "When on" and "Things to consider" were not grouped with
    the main Autofill AI toggle before this CL. The preference
    screen located in the res/xml should consist only of
    preferences. Similarly, the layouts in the res/layout define
    a view hierarchy. That's why it's not possible to combine
    the ChromeSwitchPreference directly with other items inside
    the LinearLayout.
    
    This CL reuses the ChromeSwitchPreference layout to group
    it with the "When on" and "Things to consider" items inside
    the LinearLayout. It defines a new AutofillAiPreference class
    which subclasses the ChromeSwitchPreference. AutofillAiPreference
    is effectively a ChromeSwitchPreference that reuses the original
    layout and adds a couple of things below it.
    
    Before: screen/9uBFHXCh4mQFZBA
    
    Implementation (no settings redesign): screen/BDbXLKL9FojTSm3
    Implementation (with settings redesign): screen/92qTPyMuyqKTEka
    
    Mocks: screen/8XeR9kQLbf2Q3LE
    
    Bug: 467563819
    Change-Id: Ic47bf53c522d4d20a3ca8d1552f8e5d18fba8f1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594799
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1588677}

---
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/options/AutofillAiPreference.java
@@ -0,0 +1,82 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill.options;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.Px;
+import androidx.annotation.StringRes;
+import androidx.appcompat.content.res.AppCompatResources;
+import androidx.preference.PreferenceViewHolder;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.autofill.R;
+import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
+
+/**
+ * A regular {@link ChromeSwitchPreference} that displays information items below it. It's not
+ * possible to directly combine the {@link ChromeSwitchPreference} with the text info items inside
+ * the {@code LinearLayout}. This preference defines a custom layout what reuses the {@link
+ * ChromeSwitchPreference} layout and adds the information items below it.
+ */
+@NullMarked
+public class AutofillAiPreference extends ChromeSwitchPreference {
+
+    /** Constructor for inflating from XML. */
+    public AutofillAiPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setLayoutResource(R.layout.autofill_ai_preference);
+    }
+
+    @Override
+    public void onBindViewHolder(PreferenceViewHolder holder) {
+        super.onBindViewHolder(holder);
+
+        setInfoItemDetails(
+                holder.findViewById(R.id.autofill_ai_when_on),
+                R.string.settings_autofill_ai_when_on,
+                R.string.settings_autofill_ai_when_on_can_fill_difficult_fields,
+                R.drawable.ic_bar_chart_24dp);
+        setInfoItemDetails(
+                holder.findViewById(R.id.autofill_ai_things_to_consider),
+                R.string.settings_autofill_ai_things_to_consider,
+                R.string.settings_autofill_ai_to_consider_data_usage,
+                R.drawable.ic_logo_googleg_24dp);
+    }
+
+    private void setInfoItemDetails(
+            View infoItem,
+            @StringRes int titleId,
+            @StringRes int summaryId,
+            @DrawableRes int iconId) {
+        TextView titleView = (TextView) infoItem.findViewById(R.id.info_item_title);
+        TextView summaryView = (TextView) infoItem.findViewById(R.id.info_item_summary);
+
+        Context context = getContext();
+
+        titleView.setText(context.getString(titleId));
+        summaryView.setText(context.getString(summaryId));
+
+        Drawable icon = AppCompatResources.getDrawable(context, iconId);
+        if (icon != null) {
+            @Px
+            int iconSize =
+                    context.getResources()
+                            .getDimensionPixelSize(
+                                    R.dimen.autofill_ai_preference_info_item_icon_size);
+            icon.setBounds(0, 0, iconSize, iconSize);
+            summaryView.setCompoundDrawablesRelative(icon, null, null, null);
+            summaryView.setCompoundDrawablePadding(
+                    context.getResources()
+                            .getDimensionPixelSize(
+                                    R.dimen.autofill_ai_preference_info_item_icon_padding));
+        }
+    }
+}
