9c25e4153ceace6982d7281454196f320a7c4784
Sho Biswas <shobiz@google.com>
2026-02-23 15:19:31 -0800

    [146] Create interface for ContextualTasks Panel view logic
    Original change's description:
    > Create interface for ContextualTasks Panel view logic
    > 
    > Step 2 (after http://crrev.com/c/7529915) in breaking
    > sidepanelcoordinator into a component for business logic, and a
    > different one for view logic that's implemented per-platform.
    > 
    > The interface might change slightly in the followup cl I'm currently
    > working on, but for now it has a basic empty desktop implementation.
    > 
    > Next steps:
    > - complete implementation of desktop_panel_host
    > - turn sidepanelcoordinator into generic panelcontrollerimpl that refers
    >   to the interface introduced in this cl
    > - implement Android version
    > 
    > Bug: b:478282903
    > Change-Id: If875fe6e77e7ea6328e742cdc69c1f6dde4ffd00
    > Skip-Clang-Tidy-Checks: google-default-arguments
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7560862
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Sho Biswas <shobiz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1587509}
    
    Bug: 486871369
    Change-Id: If875fe6e77e7ea6328e742cdc69c1f6dde4ffd00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601032
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7680@{#1146}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

---
+++ chrome/browser/contextual_tasks/contextual_tasks_panel_host_desktop_impl.h
@@ -0,0 +1,59 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_PANEL_HOST_DESKTOP_IMPL_H_
+#define CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_PANEL_HOST_DESKTOP_IMPL_H_
+
+#include "chrome/browser/contextual_tasks/contextual_tasks_panel_host.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_entry_observer.h"
+
+class BrowserWindowInterface;
+class SidePanelUI;
+
+namespace content {
+class WebContents;
+}  // namespace content
+
+namespace contextual_tasks {
+
+// Desktop-specific implementation of the `ContextualTasksPanelHost` interface.
+// This class manages the display of the contextual tasks UI within the
+// browser's side panel framework and coordinates with `SidePanelUI` and
+// `SidePanelRegistry` to handle visibility and lifecycle events.
+class ContextualTasksPanelHostDesktopImpl : public ContextualTasksPanelHost,
+                                            public SidePanelEntryObserver {
+ public:
+  // For testing only. Use static ContextualTasksPanelHost::Create to get an
+  // instance rather than explicitly constructing.
+  ContextualTasksPanelHostDesktopImpl(BrowserWindowInterface* browser_window,
+                                      SidePanelUI* side_panel_ui);
+
+  // Disallow copy/assign.
+  ContextualTasksPanelHostDesktopImpl(
+      const ContextualTasksPanelHostDesktopImpl&) = delete;
+  ContextualTasksPanelHostDesktopImpl& operator=(
+      const ContextualTasksPanelHostDesktopImpl&) = delete;
+  ~ContextualTasksPanelHostDesktopImpl() override;
+
+  // ContextualTasksPanelHost:
+  void Show(bool transition_from_tab) override;
+  void Close() override;
+  void SetWebContents(content::WebContents* web_contents) override;
+  void PromoteToTab() override;
+
+  // SidePanelEntryObserver:
+  void OnEntryShown(SidePanelEntry* entry) override;
+  void OnEntryHidden(SidePanelEntry* entry) override;
+
+ private:
+  // Browser window of the current panel.
+  const raw_ptr<BrowserWindowInterface> browser_window_;
+
+  // Used to show, close, and query the state of the side panel.
+  const raw_ptr<SidePanelUI> side_panel_ui_;
+};
+
+}  // namespace contextual_tasks
+
+#endif  // CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_PANEL_HOST_DESKTOP_IMPL_H_
