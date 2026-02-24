203cdde9dd8be88c87343f7d5e363e14e6d1d20e
Andrzej Fiedukowicz <afie@google.com>
2026-02-24 09:05:58 -0800

    Make versioning of PRESUBMIT cache more robust
    During recent changes I managed to make the caching forward compatible
    but my attempt to make it backward compatible failed. Unfortunately
    this is hard to fix now, but this CL looks forward attempting to make
    the caching mechanism for the future.
    
    The improvements are:
    
     1. The `_CURRENT_CACHE_FILE_SCHEMA_VERSION` is bumped
     2. The `_CURRENT_CACHE_FILE_SCHEMA_VERSION` actually influences directory path and is not stored within pickle. Even though it was intended as backward compatibility it didn't work like one because if pickle failed to load we couldn't change the schema version. This should be possible with separate directory for each version.
     3. Instead of ignoring specific exceptions when cache reading fails, the blanked catch was added. This is with the idea that ignoring cache is better then failing presubmit all together. If this happens - the cache is wiped. We do print message when it happens so if it becomes common we might get some reports around that.
     4. Tests were added to validate the new behavior.
    
    Bug: 486257405
    Change-Id: I8e87027a764e074e4a78ac334709fce3c22773aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7600792
    Reviewed-by: Ramon Cano Aparicio <rcanoaparicio@google.com>
    Commit-Queue: Andrzej Fiedukowicz <afie@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589458}

--- tools/metrics/histograms/presubmit_caching_support_test.py
+++ tools/metrics/histograms/presubmit_caching_support_test.py
@@ -6,6 +6,8 @@ import os
 import tempfile
 import unittest
 
+from typing import Optional
+
 import setup_modules
 
 import chromium_src.tools.metrics.histograms.presubmit_caching_support as presubmit_caching_support
@@ -17,16 +19,28 @@ class DummyResult:
     self.msg = msg
 
 
-def _TempCacheFile():
-  file_handle, file_path = tempfile.mkstemp(suffix='.json', text=True)
-  os.close(file_handle)
-  return file_path
+def _TempCacheDir():
+  return tempfile.mkdtemp()
+
+
+def _prepend_text_to_file(file_path: str, new_text: str):
+  extra_bytes = bytes(new_text, encoding="utf-8")
+
+  with open(file_path, 'rb') as file:
+    original_content = file.read()
+
+  with open(file_path, 'wb') as file:
+    file.write(extra_bytes)
+    file.write(original_content)
 
 class PresubmitCachingSupportTest(unittest.TestCase):
+  checked_dir: str = ""
+  storage_path: str = ""
+  cache: Optional[presubmit_caching_support.PresubmitCache] = None
 
   def setUp(self):
     self.checked_dir = tempfile.mkdtemp()
-    self.storage_path = _TempCacheFile()
+    self.storage_path = _TempCacheDir()
     self.cache = presubmit_caching_support.PresubmitCache(
         self.storage_path, self.checked_dir)
     self.cache.StoreResultInCache(1, DummyResult('dummy result'))
@@ -59,10 +73,15 @@ class PresubmitCachingSupportTest(unittest.TestCase):
     new_retrieved_result = self.cache.RetrieveResultFromCache(1)
     self.assertIsNone(new_retrieved_result)
 
-  checked_dir: str = ""
-  storage_path: str = ""
-  cache: presubmit_caching_support.PresubmitCache = None
+  def testBrokenCleansTheCache(self):
+    cache_storage_file = self.cache._storage_file_path
+    _prepend_text_to_file(cache_storage_file, "THIS_IS_INVALID_PICKLE")
 
+    restored_cache = presubmit_caching_support.PresubmitCache(
+        self.storage_path, self.checked_dir)
+
+    self.assertIsNone(restored_cache.RetrieveResultFromCache(1))
+    self.assertFalse(os.path.exists(self.cache._storage_file_path))
 
 if __name__ == '__main__':
   unittest.main()
