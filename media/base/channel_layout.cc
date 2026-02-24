29d442fbb0cf01f440c7c34df4fddac261527135
Thomas Guilbert <tguilbert@chromium.org>
2026-02-23 16:58:24 -0800

    Harden ChannelLayoutConfig constraints
    This CL adds a CHECK to ensure that ChannelLayoutConfigs created from
    the CHANNEL_LAYOUT_DISCRETE are in the [1, media::limits::kMaxChannels]
    interval.
    
    It also adds a CHECK to make sure that CHANNEL_LAYOUT_5_1_4_DOWNMIX is
    either has 6 or 10. The only valid value should really be 6, but the
    current code has some special handling, which was previously
    unconstrained.
    
    Bug: 483461268, 486962136
    Change-Id: I50411f32acbe1250ded5cbaea7358b1f083d881a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7564649
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589047}

--- media/base/channel_layout.cc
+++ media/base/channel_layout.cc
@@ -177,13 +177,21 @@ constexpr auto kChannelMaskToLayoutMap = []() {
 
 int ComputeChannelCount(ChannelLayout channel_layout, int channels) {
   if (channel_layout == CHANNEL_LAYOUT_DISCRETE) {
-    CHECK_NE(0, channels);
+    CHECK_GT(channels, 0);
+    CHECK_LE(channels, limits::kMaxChannels);
 
     return channels;
   } else if (channel_layout == CHANNEL_LAYOUT_5_1_4_DOWNMIX && channels != 0) {
-    // For CHANNEL_LAYOUT_5_1_4_DOWNMIX we can set a custom number of channels,
-    // but we are not forced to.
-    return channels;
+    // `channels` should really only be 6 here, but we might end up with the
+    // original 5.1.4 channel count. For now, handle this gracefully, and force
+    // the value down to 6. Eventually, we should remove this special case
+    // altogether.
+    CHECK(channels == 6 || channels == 10);
+
+    // TODO(crbug.com/486962136): Track whether this condition arises in the
+    // wild, and remove this branch entirely.
+    CHECK_NE(channels, 10, base::NotFatalUntil::M151);
+    return 6;
   }
   const int calculated_channel_count =
       ChannelLayoutToChannelCount(channel_layout);
