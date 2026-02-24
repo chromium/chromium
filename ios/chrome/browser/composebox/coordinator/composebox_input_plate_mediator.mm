406fd247f983bba19181a5ee399aac713c094c6b
Radu Nitescu <radunitescu@google.com>
2026-02-24 04:59:38 -0800

    [iOS][AIM] Remove TOOL_MODE_IMAGE_GEN_UPLOAD requirement for upload
    Create Image tool enablement should only be based on TOOL_MODE_IMAGE_GEN
    and adding the attachment should only be constrained by the input_types
    limits. IMAGE_GEN_UPLOAD servers for more granular suggestions.
    
    Change-Id: Ib87ba85251745f9cb40d52c119efe55d45d38d54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603052
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589324}

--- ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
+++ ios/chrome/browser/composebox/coordinator/composebox_input_plate_mediator.mm
@@ -1009,8 +1009,7 @@ CreateInputDataFromAnnotatedPageContent(
     return;
   }
 
-  BOOL imageGenUploadMode =
-      (_items.count) > 0 && [self uploadAllowedInImageGeneration];
+  BOOL imageGenUploadMode = _items.count > 0;
 
   omnibox::ToolMode toolMode =
       imageGenUploadMode ? omnibox::ToolMode::TOOL_MODE_IMAGE_GEN_UPLOAD
@@ -1547,19 +1546,6 @@ CreateInputDataFromAnnotatedPageContent(
   }
 }
 
-// Whether upload is permitted when in image generation.
-- (BOOL)uploadAllowedInImageGeneration {
-  if (![self imageToolAllowed]) {
-    return NO;
-  }
-  if (EnableComposeboxServerSideState()) {
-    return [self
-        toolAllowedInInputState:omnibox::ToolMode::TOOL_MODE_IMAGE_GEN_UPLOAD];
-  }
-
-  return YES;
-}
-
 // Whether the client is allowed to access canvas mode.
 - (BOOL)canvasToolAllowed {
   if (!ShowComposeboxAdditionalAdvancedTools()) {
@@ -1779,12 +1765,6 @@ CreateInputDataFromAnnotatedPageContent(
   if (EnableComposeboxServerSideState() &&
       ![self inputStateAllowsType:omnibox::INPUT_TYPE_LENS_IMAGE]) {
     return NO;
-  }
-
-  BOOL isImageCreationMode =
-      _modeHolder.mode == ComposeboxMode::kImageGeneration;
-  if (isImageCreationMode) {
-    return [self uploadAllowedInImageGeneration];
   }
 
   return YES;
