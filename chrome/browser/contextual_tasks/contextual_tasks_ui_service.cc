c2541457509ad5a3b8a617c3654a8c78750439cb
Nihar Majmudar <niharm@google.com>
2026-02-23 21:24:36 -0800

    [contextual tasks] Add entrypoint recording from lens.
    It is possible to enter co-browsing from a lens query so this should
    be added as an entrypoint.
    
    Change-Id: I6db1d9f7b414127626b69141a901de29d1ca737d
    Bug: 473526001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7601620
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1589160}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service.cc
@@ -134,8 +134,9 @@ enum class EntrypointSource {
   kFromWeb = 0,
   kFromOmnibox = 1,
   kFromNewTabPage = 2,
+  kFromLens = 3,
 
-  kMaxValue = kFromNewTabPage,
+  kMaxValue = kFromLens,
 };
 
 // LINT.ThenChange(//tools/metrics/histograms/metadata/contextual_tasks/enums.xml:EntrypointSource)
@@ -148,8 +149,9 @@ EntrypointSource ConvertContextualSearchSourceToEntrypointSource(
     case contextual_search::ContextualSearchSource::kNewTabPage:
       return EntrypointSource::kFromNewTabPage;
     case contextual_search::ContextualSearchSource::kLens:
+      return EntrypointSource::kFromLens;
     case contextual_search::ContextualSearchSource::kContextualTasks:
-      // These shouldn't happen but if they do - just fall through to say it's
+      // This shouldn't happen but is it does - just fall through to say it's
       // from web.
     case contextual_search::ContextualSearchSource::kUnknown:
       return EntrypointSource::kFromWeb;
