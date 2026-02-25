523d8b83478e992b99dcbfdf347e06e4e7ac9371
Simon Zünd <szuend@chromium.org>
2026-02-25 01:54:12 -0800

    [M144] [inspector] Pass ManagedChannel when connecting to V8
    This CL makes DevToolsSession inherit from V8Inspector::ManagedChannel.
    This allows the V8 inspector to extend the life-time as needed, should
    there still be CDP commands in-flight when the session detaches.
    
    Bug: 40071155
    Fixed: 483853098
    (cherry picked from commit 7d8b22f0bcaa82823f31b8bd53e026a670c5a64f)
    
    Change-Id: If97b600d695e066aacd283d92c7dba7ba1e112c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7606141
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#4760}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- third_party/blink/renderer/core/inspector/devtools_session.cc
+++ third_party/blink/renderer/core/inspector/devtools_session.cc
@@ -400,6 +400,7 @@ void DevToolsSession::FlushProtocolNotifications() {
 }
 
 void DevToolsSession::Trace(Visitor* visitor) const {
+  v8_inspector::V8Inspector::ManagedChannel::Trace(visitor);
   visitor->Trace(receiver_);
   visitor->Trace(host_remote_);
   visitor->Trace(agent_);
