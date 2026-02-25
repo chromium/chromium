f3c33268c90c271c3d8bb2a6bc1bd784f8ea0213
Kaan Alsan <alsan@chromium.org>
2026-02-25 05:30:12 -0800

    Re-enable flaky cookie controls interactive UI tests
    This CL re-enables several flaky tests in
    cookie_controls_interactive_uitest.cc that were previously disabled or
    conditionally disabled on certain platforms. These have been fixed by
    https://chromium-review.googlesource.com/c/7539436.
    
    Tests re-enabled:
    - CookieControlsWithIphUiTest.ShowAndDismissIphOnHighSiteEngagement
    - CookieControlsWithIphUiTest.OpenUserBypassViaIph
    - CookieControlsWithIphUiTest.OpenUserBypassViaIconWhenIphVisible
    
    Bug: 409294185, 409272227
    Change-Id: I83e20743c6bf5a867f134c724ef6ed31f99d1aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7602377
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590089}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_interactive_uitest.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_interactive_uitest.cc
@@ -329,17 +329,8 @@ class CookieControlsWithIphUiTest : public CookieControlsInteractiveTestBase {
   ~CookieControlsWithIphUiTest() override = default;
 };
 
-// TODO(crbug.com/409272227): IPH tests are flaky on Linux and Win asan bots
-#if (BUILDFLAG(IS_LINUX) && defined(NDEBUG)) || \
-    (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
-#define MAYBE_ShowAndDismissIphOnHighSiteEngagement \
-  DISABLED_ShowAndDismissIphOnHighSiteEngagement
-#else
-#define MAYBE_ShowAndDismissIphOnHighSiteEngagement \
-  ShowAndDismissIphOnHighSiteEngagement
-#endif
 IN_PROC_BROWSER_TEST_F(CookieControlsWithIphUiTest,
-                       MAYBE_ShowAndDismissIphOnHighSiteEngagement) {
+                       ShowAndDismissIphOnHighSiteEngagement) {
   BlockThirdPartyCookies();
   SetHighSiteEngagement();
   RunTestSequence(
@@ -360,15 +351,7 @@ IN_PROC_BROWSER_TEST_F(CookieControlsWithIphUiTest,
       EnsureNotPresent(CookieControlsBubbleView::kCookieControlsBubble));
 }
 
-// TODO(crbug.com/409272227): Flaky on linux-rel and win-asan.
-#if (BUILDFLAG(IS_LINUX) && defined(NDEBUG)) || \
-    (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
-#define MAYBE_OpenUserBypassViaIph DISABLED_OpenUserBypassViaIph
-#else
-#define MAYBE_OpenUserBypassViaIph OpenUserBypassViaIph
-#endif
-IN_PROC_BROWSER_TEST_F(CookieControlsWithIphUiTest,
-                       MAYBE_OpenUserBypassViaIph) {
+IN_PROC_BROWSER_TEST_F(CookieControlsWithIphUiTest, OpenUserBypassViaIph) {
   BlockThirdPartyCookies();
   SetHighSiteEngagement();
   RunTestSequence(
@@ -386,17 +369,8 @@ IN_PROC_BROWSER_TEST_F(CookieControlsWithIphUiTest,
           user_education::HelpBubbleView::kHelpBubbleElementIdForTesting));
 }
 
-// TODO(crbug.com/409272227): Flaky on linux-rel and win-asan.
-#if (BUILDFLAG(IS_LINUX) && defined(NDEBUG)) || \
-    (BUILDFLAG(IS_WIN) && defined(ADDRESS_SANITIZER))
-#define MAYBE_OpenUserBypassViaIconWhenIphVisible \
-  DISABLED_OpenUserBypassViaIconWhenIphVisible
-#else
-#define MAYBE_OpenUserBypassViaIconWhenIphVisible \
-  OpenUserBypassViaIconWhenIphVisible
-#endif
 IN_PROC_BROWSER_TEST_F(CookieControlsWithIphUiTest,
-                       MAYBE_OpenUserBypassViaIconWhenIphVisible) {
+                       OpenUserBypassViaIconWhenIphVisible) {
   BlockThirdPartyCookies();
   SetHighSiteEngagement();
   RunTestSequence(
