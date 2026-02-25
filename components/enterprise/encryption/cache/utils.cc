33d01d626462c826b97ffb536bb4d8f7b6f7a64a
Haihan Chen <haihan@google.com>
2026-02-24 16:13:56 -0800

    [M146] [Cache Encryption] Add histogram for logging encryption policy state
    Original change's description:
    > [Cache Encryption] Add histogram for logging encryption policy state
    > 
    > New metric to filter users based on enterprise policy to more easily
    > identify performance regressions.
    > 
    > `ShouldEncryptHttpCache` is only called from
    > `profile_network_context_service.cc`, so always at profile level.
    > 
    > Bug: 474585860
    > Change-Id: Ic425759f9a6eaf5f0c29f4914fea6863c31ad6b8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7584900
    > Reviewed-by: David Schinazi <dschinazi@chromium.org>
    > Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    > Commit-Queue: Haihan Chen <haihan@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1587214}
    
    (cherry picked from commit 7fbb81779822cc3971770f1d29db722d7fe218cd)
    
    Bug: 487358416,474585860
    Change-Id: Ic425759f9a6eaf5f0c29f4914fea6863c31ad6b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7605073
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7680@{#1285}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- components/enterprise/encryption/cache/utils.cc
+++ components/enterprise/encryption/cache/utils.cc
@@ -5,6 +5,7 @@
 #include "components/enterprise/encryption/cache/utils.h"
 
 #include "base/feature_list.h"
+#include "base/metrics/histogram_functions.h"
 #include "components/enterprise/connectors/core/connectors_prefs.h"
 #include "components/enterprise/encryption/core/features.h"
 #include "components/prefs/pref_service.h"
@@ -12,16 +13,22 @@
 namespace enterprise_encryption {
 
 bool ShouldEncryptHttpCache(const PrefService* prefs) {
-  if (base::FeatureList::IsEnabled(kEnableCacheEncryptionForTesting)) {
-    return true;
-  }
+  bool is_testing_flag_enabled =
+      base::FeatureList::IsEnabled(kEnableCacheEncryptionForTesting);
 
-  if (!prefs) {
-    return false;
-  }
+  bool is_policy_enabled =
+      prefs &&
+      prefs->GetBoolean(enterprise_connectors::kCacheEncryptionEnabledPref);
 
-  return base::FeatureList::IsEnabled(kEnableCacheEncryption) &&
-         prefs->GetBoolean(enterprise_connectors::kCacheEncryptionEnabledPref);
+  // Log intended cache encryption state.
+  // TODO(crbug.com/474585860): Improve metrics to allow for better performance
+  // data slicing.
+  base::UmaHistogramBoolean("Enterprise.CacheEncryptionPolicyEnabled",
+                            is_policy_enabled || is_testing_flag_enabled);
+
+  return is_testing_flag_enabled ||
+         (base::FeatureList::IsEnabled(kEnableCacheEncryption) &&
+          is_policy_enabled);
 }
 
 }  // namespace enterprise_encryption
