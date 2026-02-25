fad3589cac3fe7600541a76178b3109389229b45
Eriko Kurimoto <elkurin@chromium.org>
2026-02-24 21:55:50 -0800

    gardener: Disable StorageServiceRestartBrowserTest.LocalStorageRecovery
    The test is flaky for a long time, and now it even more frequently failing.
    
    Bug: 40678245
    Change-Id: I97ff992a45d302edcdd971be397c2dd43321393b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7603128
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1589921}

--- content/browser/storage_service_restart_browsertest.cc
+++ content/browser/storage_service_restart_browsertest.cc
@@ -122,7 +122,14 @@ IN_PROC_BROWSER_TEST_F(StorageServiceRestartBrowserTest,
                          R"(getSessionStorageValue("foo"))"));
 }
 
-IN_PROC_BROWSER_TEST_F(StorageServiceRestartBrowserTest, LocalStorageRecovery) {
+// TODO(crbug.com/40678245): Flaky on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_LocalStorageRecovery DISABLED_LocalStorageRecovery
+#else
+#define MAYBE_LocalStorageRecovery LocalStorageRecovery
+#endif
+IN_PROC_BROWSER_TEST_F(StorageServiceRestartBrowserTest,
+                       MAYBE_LocalStorageRecovery) {
   // Tests that the Local Storage API can recover and continue normal operation
   // after a Storage Service crash.
   EXPECT_TRUE(
