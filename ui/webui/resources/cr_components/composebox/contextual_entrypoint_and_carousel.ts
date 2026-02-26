5773887cab81a086a39d7a2b19d3c91993d87fb9
Roman Arora <romanarora@chromium.org>
2026-02-25 20:04:12 -0800

    [146] [contextual-tasks] Add accidentally deleted stop propagation call
    Original change's description:
    > [contextual-tasks] Add accidentally deleted stop propagation call
    > 
    > The duplicate 'add-tab-context' event is causing issues.
    > 
    > Bug: b:485599459
    > Change-Id: I5b4f32ecbe1ccdcf2c83e5733bceab3dea11a0c4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604618
    > Reviewed-by: Hannah Collins <hannahrogers@google.com>
    > Commit-Queue: Roman Arora <romanarora@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1589651}
    
    Bug: 487636515
    Change-Id: I5b4f32ecbe1ccdcf2c83e5733bceab3dea11a0c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7610153
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7680@{#1392}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_carousel.ts
@@ -919,6 +919,8 @@ export class ContextualEntrypointAndCarouselElement extends I18nMixinLit
     delayUpload: boolean,
     origin: TabUploadOrigin,
   }>) {
+    e.stopPropagation();
+
     this.addTabContextHandleCallback_({
       tabId: e.detail.id,
       title: e.detail.title,
