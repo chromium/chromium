chromium/chromium

commit 23a10a21a6aec27428a3de49419e5033dd28b5f3

Add UA styles for customizable combobox
Bug: 453705243
Change-Id: Ie9201d4b79c751b929b7d569c4b00584e8a888ac
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7101780
Reviewed-by: Joey Arhar <jarhar@chromium.org>
Reviewed-by: David Baron <dbaron@chromium.org>
Commit-Queue: Joey Arhar <jarhar@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1578965}

third_party/blink/renderer/core/html/resources/html.css
@@ -2469,3 +2469,63 @@ select:-internal-select-has-slotted-button::-internal-select-button-slot {
     outline-offset: -1px;
   }
 }
+
+@supports blink-feature(CustomizableCombobox) {
+  /* This should be set as an inline style in DataListIndicatorElement, but
+   * -internal-auto-base() can only be used in the UA stylesheet. The series of
+   * :not() selectors are here in order to match the text input type, which can
+   * be type=text or any other invalid type attribute. */
+  input:not([type="date" i]):not([type="datetime-local" i]):not([type="month" i]):not([type="week" i]):not([type="time" i])::-webkit-calendar-picker-indicator {
+    display: -internal-auto-base(list-item, none);
+  }
+
+  /* TODO(crbug.com/481324376): Consider making these styles not use
+   * :popover-open to avoid potential compat issues. */
+  datalist:popover-open {
+    /* [popover] styles */
+    position: fixed;
+    inset: 0;
+    position-anchor: auto;
+    width: fit-content;
+    height: fit-content;
+    margin: auto;
+    border: solid;
+    padding: 0.25em;
+    overflow: auto;
+    color: CanvasText;
+    background-color: Canvas;
+
+    /* [popover]:popover-open styles */
+    display: block;
+
+    /* ::picker(select) styles */
+    box-sizing: border-box;
+    border: 1px solid;
+    padding: 0;
+    color: inherit;
+    background-color: Canvas;
+    margin: 0;
+    inset: auto;
+    min-inline-size: anchor-size(self-inline);
+    /* Go to the edge of the viewport, and add scrollbars if needed. */
+    max-block-size: stretch;
+    overflow: auto;
+    /* Below and span-right, by default. */
+    position-area: block-end span-inline-end;
+    position-try-order: most-block-size;
+    position-try-fallbacks:
+        block-start span-inline-end,   /* First try above and span-right. */
+        block-end span-inline-start,   /* Then below but span-left. */
+        block-start span-inline-start; /* Then above and span-left. */
+  }
+
+  datalist option {
+    min-inline-size: 24px;
+    /* TODO(crbug.com/409942762): Consider removing -internal-auto-base() here
+     * if underlying performance issues with lh units are fixed. */
+    min-block-size: -internal-auto-base(24px, max(24px, 1lh));
+    font-weight: -internal-auto-base(normal, inherit);
+    padding-inline: -internal-auto-base(2px, 0.5em);
+    padding-block-end: -internal-auto-base(1px, 0);
+  }
+}

third_party/blink/renderer/core/html/forms/text_field_input_type.cc
@@ -93,7 +93,13 @@ class DataListIndicatorElement final : public HTMLDivElement {
     DCHECK(ContainingShadowRoot()->IsUserAgent());
     SetShadowPseudoId(shadow_element_names::kPseudoCalendarPickerIndicator);
     setAttribute(html_names::kIdAttr, shadow_element_names::kIdPickerIndicator);
-    SetInlineStyleProperty(CSSPropertyID::kDisplay, CSSValueID::kListItem);
+    if (!RuntimeEnabledFeatures::CustomizableComboboxEnabled()) {
+      // When CustomizableCombobox is enabled, this property is set in the UA
+      // stylesheet. It must be in the UA stylesheet because it uses
+      // -internal-auto-base(), which can only be parsed from the UA
+      // stylesheet.
+      SetInlineStyleProperty(CSSPropertyID::kDisplay, CSSValueID::kListItem);
+    }
     SetInlineStyleProperty(CSSPropertyID::kListStyle, "disclosure-open inside");
     SetInlineStyleProperty(CSSPropertyID::kCounterIncrement, "list-item 0");
     SetInlineStyleProperty(CSSPropertyID::kBlockSize, 1.0,

third_party/blink/web_tests/external/wpt/html/semantics/forms/customizable-combobox/customizable-combobox-appearance-ref.html
@@ -1,0 +1,13 @@
+<!DOCTYPE html>
+<link rel=stylesheet href="resources/customizable-combobox-styles.css">
+
+<div class=customizable-combobox-input tabindex=0></div>
+<div class=customizable-combobox-datalist popover=auto>
+  <div class=customizable-combobox-option>one</div>
+  <div class=customizable-combobox-option>two</div>
+</div>
+
+<script>
+  document.querySelector('.customizable-combobox-input').focus();
+  document.querySelector('.customizable-combobox-datalist').showPopover();
+</script>

third_party/blink/web_tests/external/wpt/html/semantics/forms/customizable-combobox/customizable-combobox-appearance.html
@@ -1,0 +1,23 @@
+<!DOCTYPE html>
+<link rel=author href="mailto:jarhar@chromium.org">
+<link rel=help href="https://open-ui.org/components/combobox.explainer/">
+<link rel=match href="customizable-combobox-appearance-ref.html">
+
+<style>
+input, datalist {
+  appearance: base;
+}
+input {
+  width: 150px;
+}
+</style>
+
+<input list=datalist>
+<datalist id=datalist>
+  <option>one</option>
+  <option>two</option>
+</datalist>
+
+<script>
+document.querySelector('input').focus();
+</script>

third_party/blink/web_tests/external/wpt/html/semantics/forms/customizable-combobox/resources/customizable-combobox-styles.css
@@ -1,0 +1,41 @@
+.customizable-combobox-input {
+  anchor-name: --input;
+  border: 1px solid;
+  width: 150px;
+  height: 1lh;
+}
+
+.customizable-combobox-datalist {
+  position-anchor: --input;
+  position: fixed;
+  width: fit-content;
+  height: fit-content;
+  box-sizing: border-box;
+  border: 1px solid;
+  padding: 0;
+  color: CanvasText;
+  background-color: Canvas;
+  margin: 0;
+  inset: auto;
+  min-inline-size: anchor-size(self-inline);
+  min-block-size: 1lh;
+  max-block-size: stretch;
+  overflow: auto;
+  position-area: block-end span-inline-end;
+  position-try-order: most-block-size;
+  position-try-fallbacks:
+    block-start span-inline-end,
+    block-end span-inline-start,
+    block-start span-inline-start;
+}
+
+.customizable-combobox-option {
+  min-inline-size: 24px;
+  min-block-size: max(24px, 1lh);
+  padding-inline: 0.5em;
+  padding-block-end: 0;
+  display: flex;
+  align-items: center;
+  gap: 0.5em;
+  white-space: nowrap;
+}

commit 59c2bbce38e0142b5c179f71e6d75656cf835b3f

Fix memory regression from combobox UA styles
This fixes a memory regression from
commit 23a10a21a6aec27428a3de49419e5033dd28b5f3 which added a very long
selector to the UA stylesheet by replacing the long selector with a new
internal pseudo-class.

The invalidation for the new pseudo-class, :-internal-text-field, can't
be tested right now because its usage in the UA stylesheet doesn't
depend on invalidation due to changes in the DOM of the UA shadowroot,
and because we can't use element.matches() on an internal pseudo-class.
However, the added call to PseudoStateChanged should correctly cover
invalidation for :-internal-text-field.

Bug: 481458369
Change-Id: If427279f5a0b56580582c6b14537cee3787b7808
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7571691
Commit-Queue: Joey Arhar <jarhar@chromium.org>
Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
Reviewed-by: Joey Arhar <jarhar@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1590584}

third_party/blink/renderer/core/css/css_selector.cc
@@ -510,6 +510,7 @@ PseudoId CSSSelector::GetPseudoId(PseudoType type) {
     case kPseudoTargetCurrent:
     case kPseudoTargetBefore:
     case kPseudoTargetAfter:
+    case kPseudoTextField:
     case kPseudoToolFormActive:
     case kPseudoToolSubmitActive:
     case kPseudoUnknown:
@@ -592,6 +593,7 @@ constexpr static NameToPseudoStruct kPseudoTypeWithoutArgumentsMap[] = {
      CSSSelector::kPseudoHostHasNonAutoAppearance},
     {"-internal-spatial-navigation-focus",
      CSSSelector::kPseudoSpatialNavigationFocus},
+    {"-internal-text-field", CSSSelector::kPseudoTextField},
     {"-internal-video-persistent", CSSSelector::kPseudoVideoPersistent},
     {"-internal-video-persistent-ancestor",
      CSSSelector::kPseudoVideoPersistentAncestor},
@@ -1093,6 +1095,7 @@ void CSSSelector::UpdatePseudoType(const AtomicString& value,
     case kPseudoTargetCurrent:
     case kPseudoTargetBefore:
     case kPseudoTargetAfter:
+    case kPseudoTextField:
     case kPseudoUnknown:
     case kPseudoUnparsed:
     case kPseudoUserInvalid:
@@ -1869,6 +1872,7 @@ bool CSSSelector::IsAllowedAfterPart() const {
     case kPseudoTargetCurrent:
     case kPseudoTargetBefore:
     case kPseudoTargetAfter:
+    case kPseudoTextField:
     case kPseudoToolFormActive:
     case kPseudoToolSubmitActive:
     case kPseudoVideoPersistent:
@@ -2220,6 +2224,7 @@ bool CSSSelector::SupportsPseudoStateChange(PseudoType type) {
     case CSSSelector::kPseudoTargetAfter:
     case CSSSelector::kPseudoTargetBefore:
     case CSSSelector::kPseudoTargetCurrent:
+    case CSSSelector::kPseudoTextField:
     case CSSSelector::kPseudoToolFormActive:
     case CSSSelector::kPseudoToolSubmitActive:
     case CSSSelector::kPseudoUserInvalid:

third_party/blink/renderer/core/css/css_selector.h
@@ -326,6 +326,7 @@ class CORE_EXPORT CSSSelector {
     kPseudoStart,
     kPseudoState,
     kPseudoTarget,
+    kPseudoTextField,
     kPseudoToolFormActive,
     kPseudoToolSubmitActive,
     kPseudoUnknown,

third_party/blink/renderer/core/css/selector_checker.cc
@@ -823,6 +823,7 @@ SelectorChecker::FeaturelessMatch SelectorChecker::MatchShadowHost(
     case CSSSelector::kPseudoTargetCurrent:
     case CSSSelector::kPseudoTargetBefore:
     case CSSSelector::kPseudoTargetAfter:
+    case CSSSelector::kPseudoTextField:
     case CSSSelector::kPseudoToolFormActive:
     case CSSSelector::kPseudoToolSubmitActive:
     case CSSSelector::kPseudoViewTransition:
@@ -2846,6 +2847,12 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
                    : order_result == 1;
       }
       break;
+    }
+    case CSSSelector::kPseudoTextField: {
+      if (auto* input = DynamicTo<HTMLInputElement>(element)) {
+        return input->IsTextField();
+      }
+      return false;
     }
     case CSSSelector::kPseudoIndeterminate: {
       probe::ForcePseudoState(&element, CSSSelector::kPseudoIndeterminate,

third_party/blink/renderer/core/html/forms/html_input_element.cc
@@ -472,6 +472,9 @@ void HTMLInputElement::UpdateType(const AtomicString& type_attribute_value) {
   if (input_type_->ShouldRespectListAttribute() !=
       new_type->ShouldRespectListAttribute())
     PseudoStateChanged(CSSSelector::kPseudoHasDatalist);
+  if (input_type_->IsTextFieldInputType() != new_type->IsTextFieldInputType()) {
+    PseudoStateChanged(CSSSelector::kPseudoTextField);
+  }
 
   bool placeholder_changed =
       input_type_->SupportsPlaceholder() != new_type->SupportsPlaceholder();

third_party/blink/renderer/core/html/resources/html.css
@@ -2351,11 +2351,11 @@ select:-internal-select-has-slotted-button::-internal-select-button-slot {
 }
 
 @supports blink-feature(CustomizableCombobox) {
-  /* This should be set as an inline style in DataListIndicatorElement, but
-   * -internal-auto-base() can only be used in the UA stylesheet. The series of
-   * :not() selectors are here in order to match the text input type, which can
-   * be type=text or any other invalid type attribute. */
-  input:not([type="date" i]):not([type="datetime-local" i]):not([type="month" i]):not([type="week" i]):not([type="time" i])::-webkit-calendar-picker-indicator {
+  /* This would be set as an inline style in DataListIndicatorElement, but
+   * -internal-auto-base() can only be used in the UA stylesheet.
+   * :-internal-text-field is needed for this because many attribute selectors
+   * would not be performant enough. */
+  input:-internal-text-field::-webkit-calendar-picker-indicator {
     display: -internal-auto-base(list-item, none);
   }
 