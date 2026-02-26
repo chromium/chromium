223feefd76cd0167fd8c1a9aee988f428eba31b4
Andrea Orru <andreaorru@chromium.org>
2026-02-25 17:20:23 -0800

    [Extensions] Enable kWebRequestPersistFilteredEventsViaEventRouter
    Previously, webRequest events relied on an ad-hoc persistence
    implementation.
    
    This CL enables the kWebRequestPersistFilteredEventsViaEventRouter
    feature flag. This transfers the responsibility of persisting/restoring
    webRequest event listeners to EventRouter.
    
    Bug: 474558883
    Change-Id: Id198d84e2a6a327e1738834d05cdf6ae3c779f89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7587175
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1590544}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -209,6 +209,6 @@ BASE_FEATURE(kSearchEngineUnconditionalDialog,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kWebRequestPersistFilteredEventsViaEventRouter,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace extensions_features
