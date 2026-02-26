0e03334a99ac0c63ceb45cc6ffe2c91b0cadc4f6
Jacob Stanley <jacobstanley@google.com>
2026-02-26 09:14:57 -0800

    [Permissions Views] Ignore a11y input events
    Previously WebContents didn't have the ability to ignore a11y input
    alongside regular input events. This CL uses the new functionality (https://chromium-review.git.corp.google.com/c/chromium/src/+/7050137) to also ignore a11y input, treating it the same as normal input events.
    
    Setting the new parameter to true blocks interactive accessibility
    actions (like clicking or focusing) while a permission prompt is shown,
    but intentionally still allows kHitTest actions. This ensures screen
    readers can still read the underlying page contents, even while
    interaction is blocked.
    
    Bug: 452693512
    Change-Id: Ice8dd7090ac29b4664fb963df10c21c5ad1bc42a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7604837
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Judith Hemp <hempjudith@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590880}

--- chrome/browser/ui/views/permissions/exclusive_access_permission_prompt.cc
+++ chrome/browser/ui/views/permissions/exclusive_access_permission_prompt.cc
@@ -58,7 +58,8 @@ bool ExclusiveAccessPermissionPrompt::ShowPrompt() {
       new ExclusiveAccessPermissionPromptView(browser(),
                                               GetPermissionPromptDelegate());
   prompt_view_tracker_.SetView(prompt_view);
-  scoped_ignore_input_events_ = web_contents()->IgnoreInputEvents(std::nullopt);
+  scoped_ignore_input_events_ = web_contents()->IgnoreInputEvents(
+      std::nullopt, /*should_ignore_a11y_input=*/true);
   content_scrim_widget_ =
       EmbeddedPermissionPromptContentScrimView::CreateScrimWidget(
           weak_factory_.GetWeakPtr(),
