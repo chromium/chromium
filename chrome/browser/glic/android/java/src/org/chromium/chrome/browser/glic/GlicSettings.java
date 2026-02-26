dc472c92535c03efb278be66568e23b7f3e728df
Alexis Wu <alexiswu@google.com>
2026-02-26 10:49:09 -0800

    [GlicSettings] Add Share Current Tab Entry
    Screenshot1: https://screenshot.googleplex.com/NLXodLrNBNZgZHp
    Screenshot2: https://screenshot.googleplex.com/5CsAhMPQWia3D4k
    
    Bug: 478006171, 481440452, 485347702
    Change-Id: I03b3315905f73d0d3a5dc1e05c9972d9f348e982
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7594699
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Alexis Wu <alexiswu@google.com>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590950}

--- chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicSettings.java
+++ chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicSettings.java
@@ -6,10 +6,13 @@ package org.chromium.chrome.browser.glic;
 
 import static org.chromium.build.NullUtil.assertNonNull;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.preference.Preference;
 
+import org.chromium.base.IntentUtils;
 import org.chromium.base.supplier.MonotonicObservableSupplier;
 import org.chromium.base.supplier.ObservableSuppliers;
 import org.chromium.base.supplier.SettableMonotonicObservableSupplier;
@@ -17,12 +20,18 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.settings.ChromeBaseSettingsFragment;
 import org.chromium.chrome.browser.settings.search.ChromeBaseSearchIndexProvider;
+import org.chromium.components.browser_ui.settings.ChromeSwitchPreference;
 import org.chromium.components.browser_ui.settings.SettingsFragment;
 import org.chromium.components.browser_ui.settings.SettingsUtils;
+import org.chromium.ui.text.ChromeClickableSpan;
+import org.chromium.ui.text.SpanApplier;
 
 /** Fragment for GLIC related configurations to Chrome. */
 @NullMarked
 public class GlicSettings extends ChromeBaseSettingsFragment {
+    private static final String PERMISSION_DEFAULT_TAB_ACCESS =
+            "glic_permissions_default_tab_access";
+    private static final String LEARN_MORE_AI_URL = "https://support.google.com/a/answer/15706919";
 
     public static final String PREF_KEY_GLIC_PERMISSIONS_ACTIVITY = "glic_permissions_activity";
     public static final String PREF_KEY_GLIC_EXTENSIONS = "glic_extensions";
@@ -34,6 +43,15 @@ public class GlicSettings extends ChromeBaseSettingsFragment {
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         SettingsUtils.addPreferencesFromResource(this, R.xml.glic_settings);
         mPageTitle.set(getString(R.string.settings_glic_button_toggle));
+
+        ChromeSwitchPreference tabAccessPref =
+                assertNonNull(findPreference(PERMISSION_DEFAULT_TAB_ACCESS));
+        var summary =
+                getString(
+                        R.string
+                                .settings_glic_permissions_default_tab_access_toggle_sublabel_data_protected);
+        tabAccessPref.setSummary(
+                SpanApplier.applySpans(summary, getLearnMoreSpanInfo(LEARN_MORE_AI_URL)));
 
         Preference permissionActivityPref =
                 assertNonNull(findPreference(PREF_KEY_GLIC_PERMISSIONS_ACTIVITY));
@@ -53,6 +71,18 @@ public class GlicSettings extends ChromeBaseSettingsFragment {
                         return true;
                     });
         }
+    }
+
+    private SpanApplier.SpanInfo getLearnMoreSpanInfo(String url) {
+        return new SpanApplier.SpanInfo(
+                "<a href=\"#\">",
+                "</a>",
+                new ChromeClickableSpan(
+                        getContext(),
+                        v -> {
+                            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                            IntentUtils.safeStartActivity(getContext(), intent);
+                        }));
     }
 
     @Override
