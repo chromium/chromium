4fabf87a56426bc1d8f45c65b2176e8d00548ad9
Sophie Crowley <crowleso@google.com>
2026-02-26 06:26:43 -0800

    Reland "[Signin][Android] Display button when SigninLevelUpButton flag enabled"
    This is a reland of commit 713f83782eaaf16a63e3a75217e9e3f0680878a3
    
    Fixed failing test by modifying final line of
    testSigninButtonHiddenOnIncognitoNtp in SigninButtonCoordinatorTest to
    also check for stub id in case the new tab has not inflated the
    SigninButton yet.
    
    Original change's description:
    > [Signin][Android] Display button when SigninLevelUpButton flag enabled
    >
    > Iterating on the SigninButton skeleton classes added in
    > https://crrev.com/c/7511406 to add a button which is visible in the
    > toolbar when on the NTP (behind the SigninLevelUpButton flag). The
    > button currently does not do anything.
    >
    > In future changes, different states (signed-in avatar, sign-in text
    > button) will be added as well as functionality for when the button is
    > clicked, when profile is updated, etc.
    >
    > Emulator screenshots of change can be found in
    > https://crbug.com/478828569#comment3
    >
    > Bug: 478828569
    > Change-Id: I84e60ec85f26776f13051662ab1b053304af021c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7539858
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Commit-Queue: Sophie Crowley <crowleso@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1589999}
    
    Bug: 478828569
    Change-Id: Ib7334838caa2aa142ac080d5e44e659c1aa19106
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7608240
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Sophie Crowley <crowleso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1590801}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/signin_button/SigninButtonProperties.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/signin_button/SigninButtonProperties.java
@@ -4,14 +4,27 @@
 
 package org.chromium.chrome.browser.toolbar.signin_button;
 
+import android.graphics.drawable.Drawable;
+
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableBooleanPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 
-/**
- * TODO(crbug.com/475816843): Implement a SigninButton Model to allow for interactions between View
- * and Controller.
- */
+/** The SigninButton model allows for interactions between the view and the controller. */
 @NullMarked
 final class SigninButtonProperties {
-    public static final PropertyKey[] ALL_KEYS = new PropertyKey[] {};
+
+    // Indicates whether the signin button view should be displayed.
+    public static final WritableBooleanPropertyKey SHOW_BUTTON = new WritableBooleanPropertyKey();
+
+    // Indicates whether the inner signin avatar button should be displayed.
+    public static final WritableBooleanPropertyKey SHOW_AVATAR = new WritableBooleanPropertyKey();
+
+    // The image displayed within the avatar button, i.e. profile picture or generic account circle.
+    public static final WritableObjectPropertyKey<Drawable> BUTTON_AVATAR =
+            new WritableObjectPropertyKey<>();
+
+    public static final PropertyKey[] ALL_KEYS =
+            new PropertyKey[] {SHOW_BUTTON, SHOW_AVATAR, BUTTON_AVATAR};
 }
--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/signin_button/SigninButtonViewBinder.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/signin_button/SigninButtonViewBinder.java
@@ -4,16 +4,27 @@
 
 package org.chromium.chrome.browser.toolbar.signin_button;
 
+import android.view.View;
+
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.widget.ChromeImageButton;
 
-/**
- * TODO(crbug.com/475816843): Implement ViewBinder connecting SigninButtonProperties and
- * SigninButtonView.
- */
+/** The ViewBinder connecting SigninButtonProperties and SigninButtonView. */
 @NullMarked
 final class SigninButtonViewBinder {
-
-    public static void bind(PropertyModel model, SigninButtonView view, PropertyKey propertyKey) {}
+    public static void bind(PropertyModel model, SigninButtonView view, PropertyKey propertyKey) {
+        if (SigninButtonProperties.BUTTON_AVATAR.equals(propertyKey)) {
+            ChromeImageButton avatarButton = view.getAvatarButton();
+            avatarButton.setImageDrawable(model.get(SigninButtonProperties.BUTTON_AVATAR));
+        } else if (SigninButtonProperties.SHOW_BUTTON.equals(propertyKey)) {
+            view.setVisibility(
+                    model.get(SigninButtonProperties.SHOW_BUTTON) ? View.VISIBLE : View.GONE);
+        } else if (SigninButtonProperties.SHOW_AVATAR.equals(propertyKey)) {
+            ChromeImageButton avatarButton = view.getAvatarButton();
+            avatarButton.setVisibility(
+                    model.get(SigninButtonProperties.SHOW_AVATAR) ? View.VISIBLE : View.GONE);
+        }
+    }
 }
