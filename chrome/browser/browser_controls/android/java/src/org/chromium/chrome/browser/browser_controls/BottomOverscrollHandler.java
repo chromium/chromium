ea0ce76a84d4b283f4932b1330239231cd00fc1d
Wenyu Fu <wenyufu@chromium.org>
2026-02-26 17:08:25 -0800

    Remove Android.OverscrollFromBottom.BottomControlsStatus histogram
    No longer needed. Snapshot see attached bug.
    
    OBSOLETE_HISTOGRAM[Android.OverscrollFromBottom.BottomControlsStatus]
    
    Bug: 475224211
    Change-Id: I9f8abf1ac39e0e60208c60a00f16fd25cf9b0939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7613832
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1591210}

--- chrome/browser/browser_controls/android/java/src/org/chromium/chrome/browser/browser_controls/BottomOverscrollHandler.java
+++ chrome/browser/browser_controls/android/java/src/org/chromium/chrome/browser/browser_controls/BottomOverscrollHandler.java
@@ -6,48 +6,13 @@ package org.chromium.chrome.browser.browser_controls;
 
 import android.os.Handler;
 
-import androidx.annotation.IntDef;
-import androidx.annotation.VisibleForTesting;
-
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.cc.input.BrowserControlsState;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
 /** Handles overscroll on the bottom. */
 @NullMarked
 public class BottomOverscrollHandler {
-
-    /**
-     * Enum for "Android.EdgeToEdge.OverscrollFromBottom.BottomControlsStatus", demonstrate the
-     * current status for the bottom browser controls. These values are persisted to logs. Entries
-     * should not be renumbered and numeric values should never be reused.
-     */
-    @Retention(RetentionPolicy.SOURCE)
-    @IntDef({
-        BottomControlsStatus.HEIGHT_ZERO,
-        BottomControlsStatus.HIDDEN,
-        BottomControlsStatus.VISIBLE_FULL_HEIGHT,
-        BottomControlsStatus.VISIBLE_PARTIAL_HEIGHT,
-        BottomControlsStatus.NUM_TOTAL
-    })
-    @interface BottomControlsStatus {
-        /** Controls has a height of 0. */
-        int HEIGHT_ZERO = 0;
-
-        /** Controls has height > 0, and it's hidden */
-        int HIDDEN = 1;
-
-        /** Controls has height > 0 and is fully visible. */
-        int VISIBLE_FULL_HEIGHT = 2;
-
-        /** Controls has height > 0 and is partially visible (e.g. showing its min Height) */
-        int VISIBLE_PARTIAL_HEIGHT = 3;
-
-        int NUM_TOTAL = 4;
-    }
 
     private final BrowserControlsVisibilityManager mBrowserControls;
     private final Handler mHandler = new Handler();
@@ -70,7 +35,6 @@ public class BottomOverscrollHandler {
      * @return Whether the overscroll started.
      */
     public boolean start() {
-        recordEdgeToEdgeOverscrollFromBottom(mBrowserControls);
         mOverscrollStarted = canStartBottomOverscroll();
 
         RecordHistogram.recordBooleanHistogram(
@@ -125,30 +89,6 @@ public class BottomOverscrollHandler {
     private void showControlsTransient() {
         mBrowserControls.getBrowserVisibilityDelegate().showControlsTransient();
         recordDidTriggerOverscroll(true);
-    }
-
-    /**
-     * Record histogram "Android.OverscrollFromBottom.BottomControlsStatus" based on the current
-     * browser controls status.
-     */
-    @VisibleForTesting
-    static void recordEdgeToEdgeOverscrollFromBottom(BrowserControlsStateProvider browserControls) {
-        @BottomControlsStatus int sample;
-        if (browserControls.getBottomControlsHeight() == 0) {
-            sample = BottomControlsStatus.HEIGHT_ZERO;
-        } else if (browserControls.getBottomControlOffset() == 0) {
-            sample = BottomControlsStatus.VISIBLE_FULL_HEIGHT;
-        } else if (browserControls.getBottomControlOffset()
-                == browserControls.getBottomControlsHeight()) {
-            sample = BottomControlsStatus.HIDDEN;
-        } else {
-            sample = BottomControlsStatus.VISIBLE_PARTIAL_HEIGHT;
-        }
-
-        RecordHistogram.recordEnumeratedHistogram(
-                "Android.OverscrollFromBottom.BottomControlsStatus",
-                sample,
-                BottomControlsStatus.NUM_TOTAL);
     }
 
     /** Record whether the bottom overscroll is triggered, or canceled. */
--- chrome/browser/browser_controls/android/java/src/org/chromium/chrome/browser/browser_controls/BottomOverscrollHandlerUnitTest.java
+++ chrome/browser/browser_controls/android/java/src/org/chromium/chrome/browser/browser_controls/BottomOverscrollHandlerUnitTest.java
@@ -23,14 +23,11 @@ import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.RobolectricUtil;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.cc.input.BrowserControlsState;
-import org.chromium.chrome.browser.browser_controls.BottomOverscrollHandler.BottomControlsStatus;
 
 /** Unit test for {@link BottomOverscrollHandler}. */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class BottomOverscrollHandlerUnitTest {
-    private static final String OVERSCROLL_FROM_EDGE_UMA_NAME =
-            "Android.OverscrollFromBottom.BottomControlsStatus";
     private static final String CAN_START_OVERSCROLL_UMA_NAME =
             "Android.OverscrollFromBottom.CanStart";
     private static final String DID_TRIGGER_OVERSCROLL_UMA_NAME =
@@ -62,55 +59,6 @@ public class BottomOverscrollHandlerUnitTest {
         mDelegate = new FakeBrowserStateBrowserControlsVisibilityDelegate();
         doReturn(mDelegate).when(mBrowserControls).getBrowserVisibilityDelegate();
         mHandler = new BottomOverscrollHandler(mBrowserControls);
-    }
-
-    /**
-     * Test method for {@link
-     * BottomOverscrollHandler#recordEdgeToEdgeOverscrollFromBottom(BrowserControlsStateProvider)}
-     * .}
-     */
-    @Test
-    public void testRecordEdgeToEdgeOverscrollFromBottom_Zero() {
-        doReturn(0).when(mBrowserControls).getBottomControlsHeight();
-        try (var watcher =
-                HistogramWatcher.newSingleRecordWatcher(
-                        OVERSCROLL_FROM_EDGE_UMA_NAME, BottomControlsStatus.HEIGHT_ZERO)) {
-            BottomOverscrollHandler.recordEdgeToEdgeOverscrollFromBottom(mBrowserControls);
-        }
-    }
-
-    @Test
-    public void testRecordEdgeToEdgeOverscrollFromBottom_Hidden() {
-        doReturn(50).when(mBrowserControls).getBottomControlsHeight();
-        doReturn(50).when(mBrowserControls).getBottomControlOffset();
-        try (var watcher =
-                HistogramWatcher.newSingleRecordWatcher(
-                        OVERSCROLL_FROM_EDGE_UMA_NAME, BottomControlsStatus.HIDDEN)) {
-            BottomOverscrollHandler.recordEdgeToEdgeOverscrollFromBottom(mBrowserControls);
-        }
-    }
-
-    @Test
-    public void testRecordEdgeToEdgeOverscrollFromBottom_Full() {
-        doReturn(50).when(mBrowserControls).getBottomControlsHeight();
-        doReturn(0).when(mBrowserControls).getBottomControlOffset();
-        try (var watcher =
-                HistogramWatcher.newSingleRecordWatcher(
-                        OVERSCROLL_FROM_EDGE_UMA_NAME, BottomControlsStatus.VISIBLE_FULL_HEIGHT)) {
-            BottomOverscrollHandler.recordEdgeToEdgeOverscrollFromBottom(mBrowserControls);
-        }
-    }
-
-    @Test
-    public void testRecordEdgeToEdgeOverscrollFromBottom_Partial() {
-        doReturn(50).when(mBrowserControls).getBottomControlsHeight();
-        doReturn(20).when(mBrowserControls).getBottomControlOffset();
-        try (var watcher =
-                HistogramWatcher.newSingleRecordWatcher(
-                        OVERSCROLL_FROM_EDGE_UMA_NAME,
-                        BottomControlsStatus.VISIBLE_PARTIAL_HEIGHT)) {
-            BottomOverscrollHandler.recordEdgeToEdgeOverscrollFromBottom(mBrowserControls);
-        }
     }
 
     @Test
