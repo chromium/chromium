eef4bcc74bf6cb0ef89b9c2639ac34960fb34152
Nicola Tommasi <tommasin@chromium.org>
2026-02-27 00:48:24 -0800

    [glic][onboarding] Profile picker should open FRE in a new tab
    When a profile is selected from the profile picker and
    TrustFirstOonboarding is enabled, open the FRE in a new tab within a
    newly created browser window. This behavior only applies if the user has
    not yet completed the TrustFirstOoboarding FRE and fixes b/486207622. If
    the user has already consented, the existing behavior of toggling the UI
    is maintained.
    
    Screencast: b/486207622#comment7
    Bug: b:486207622
    Change-Id: Ia5df25a8160b805e50cef5830d3cee88b76e2df5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7608699
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591387}

--- chrome/browser/glic/glic_profile_manager.cc
+++ chrome/browser/glic/glic_profile_manager.cc
@@ -28,9 +28,11 @@
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/network_service_instance.h"
+#include "url/gurl.h"
 
 #if !BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/profiles/profile_picker.h"
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #endif
@@ -310,6 +312,18 @@ void GlicProfileManager::DidSelectProfile(Profile* profile) {
                                    mojom::InvocationSource::kProfilePicker);
 #else
     NOTIMPLEMENTED() << "OpenFreDialogInNewTab";
+#endif
+  } else if (GlicEnabling::IsTrustFirstOnboardingEnabledForProfile(profile)) {
+#if !BUILDFLAG(IS_ANDROID)
+    // Open a browser and show the FRE in a new tab.
+    chrome::ScopedTabbedBrowserDisplayer displayer(profile);
+    Browser* browser = displayer.browser();
+    chrome::AddAndReturnTabAt(browser, GURL(), /*index=*/-1,
+                              /*foreground=*/true);
+    service->ToggleUI(browser, /*prevent_close=*/true,
+                      mojom::InvocationSource::kProfilePicker);
+#else
+    NOTIMPLEMENTED() << "ToggleUIOnNewTab";
 #endif
   } else {
     // Toggle glic but prevent close if it is already open for the selected
