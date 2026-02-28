8ccbd26186cba16cce39711b2d93e808b9fc14e5
Thomas Guilbert <tguilbert@chromium.org>
2026-02-27 20:01:36 -0800

    [CodeHealth] Spanify AudioEncoderOpus
    This CL rewrites AudioEncoderOpus to use spans instead of raw pointers.
    Previous attempts at gradual spanification were unsuccessful, due to the
    complexity of making incremental changes.
    
    It breaks down the encoding flows into two distinct flows: with and
    without resampling. To help manage samples before resampling, the CL
    introduces an internal helper FIFO, which minimizes copies.
    
    The CL also cleans up a previous TODO, which makes sure to use the
    proper resampling input size, and adds more unit test coverage.
    
    Bug: 373960632
    Change-Id: Ie41f5bd97d72ee258db58107edbde06f9e2e7636
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614743
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1591960}

--- remoting/codec/audio_encoder_opus.h
+++ remoting/codec/audio_encoder_opus.h
@@ -7,6 +7,7 @@
 
 #include "base/memory/aligned_memory.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_span.h"
 #include "remoting/codec/audio_encoder.h"
 #include "remoting/proto/audio.pb.h"
 
@@ -23,6 +24,28 @@ class AudioPacket;
 
 class AudioEncoderOpus : public AudioEncoder {
  public:
+  // Helper class which segments samples into chunks, while minimizing copies.
+  // Exposed as an interface here for ease of testing.
+  class ResamplerFifo {
+   public:
+    virtual ~ResamplerFifo() = default;
+
+    // Add samples to the FIFO, without copying them.
+    virtual void AddNewSamples(base::span<const int16_t> samples) = 0;
+
+    // Copies unused samples added by `AddNewSamples()` to internal storage.
+    virtual void SaveNewSamples() = 0;
+
+    // Consumes samples from the FIFO.
+    virtual base::span<const int16_t> TakeChunk() = 0;
+
+    // Returns the number of samples currently in the FIFO, saved or not.
+    virtual size_t remaining_samples() const = 0;
+
+    // Returns the size of each chunk returned by `TakeChunk()`.
+    virtual size_t GetChunkSizeForTesting() const = 0;
+  };
+
   AudioEncoderOpus();
 
   AudioEncoderOpus(const AudioEncoderOpus&) = delete;
@@ -35,32 +58,53 @@ class AudioEncoderOpus : public AudioEncoder {
       std::unique_ptr<AudioPacket> packet) override;
   int GetBitrate() override;
 
+  static std::unique_ptr<ResamplerFifo> GetEmptyFifoForTesting(
+      size_t size_in_frames,
+      size_t channels);
+
  private:
   void InitEncoder();
   void DestroyEncoder();
   bool ResetForPacket(AudioPacket* packet);
 
+  std::unique_ptr<AudioPacket> CreatePacket();
+
+  std::unique_ptr<AudioPacket> EncodeInternal(base::span<const int16_t> data);
+  std::unique_ptr<AudioPacket> EncodeInternalWithResampling(
+      base::span<const int16_t> data);
+
   void FetchBytesToResample(int resampler_frame_delay,
                             media::AudioBus* audio_bus);
+
+  bool EncodeData(base::span<const int16_t> data, AudioPacket* destination);
+
+  bool needs_resampling_ = false;
+
+  // Holds samples (always at 48kHz) that have not yet been encoded.
+  base::AlignedHeapArray<int16_t> encoder_input_;
+
+  // The portion of `encoder_input_` which contains samples that have not yet
+  // been encoded.
+  // Unused when `needs_resampling_` is true, since extra samples will be stored
+  // in `resampler_fifo_` instead.
+  base::raw_span<int16_t> leftover_encoder_samples_;
+
+  // Number of samples needed to encode a single "Opus frame".
+  size_t encoder_samples_needed_ = 0;
+
+  // Manages samples which have not been resampled yet.
+  std::unique_ptr<ResamplerFifo> resampler_fifo_;
+
+  // The minimum number of samples needed to guarantee to have enough for
+  // one resample call.
+  size_t resampling_samples_needed_ = 0;
 
   int sampling_rate_ = 0;
   AudioPacket::Channels channels_ = AudioPacket::CHANNELS_STEREO;
   raw_ptr<OpusEncoder, DanglingUntriaged> encoder_ = nullptr;
 
-  int frame_size_ = 0;
   std::unique_ptr<media::MultiChannelResampler> resampler_;
-  base::AlignedHeapArray<int16_t> resample_buffer_;
   std::unique_ptr<media::AudioBus> resampler_bus_;
-
-  // Used to pass packet to the FetchBytesToResampler() callback.
-  const char* resampling_data_ = nullptr;
-  int resampling_data_size_ = 0;
-  int resampling_data_pos_ = 0;
-
-  // Left-over unencoded samples from the previous AudioPacket.
-  std::unique_ptr<int16_t[]> leftover_samples_;
-  int leftover_samples_size_in_frames_ = 0;
-  int leftover_frames_ = 0;
 };
 
 }  // namespace remoting
