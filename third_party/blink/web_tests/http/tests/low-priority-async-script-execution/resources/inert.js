95d35e998513218e13c26a13fa75c37f425ac509
Minoru Chikamune <chikamune@chromium.org>
2026-03-01 17:46:14 -0800

    Remove low-priority-async-script-execution virtual test suite
    The feature is already enabled by default, so the virtual test suite is
    no longer needed. This CL removes the suite and its associated tests to
    reduce test overhead.
    
    Bug: 488729188
    Change-Id: I8f870a71855e7210e00bd3af65d98f3573e9af83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7622512
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592213}

--- third_party/blink/web_tests/http/tests/low-priority-async-script-execution/resources/inert.js
+++
@@ -1,2 +0,0 @@
-// this is a script that's not contributing to lcp
-logScript('inert.js');
--- third_party/blink/web_tests/http/tests/low-priority-async-script-execution/resources/lcp-img-insert.js
+++
@@ -1,5 +0,0 @@
-logScript('lcp-img-insert.js');
-setTimeout(async () => {
-  const img = await window.__lcpImage;
-  document.body.append(img);
-}, 0);
