7624ea03cf07794ea65e7b10ff3a29913bb82fa0
Stephen Nusko <nuskos@chromium.org>
2026-03-02 00:51:55 -0800

    [M146] Update SchedulerLoopQuarantineWebContentBrowser to exclude navigations
    Original change's description:
    > Update SchedulerLoopQuarantineWebContentBrowser to exclude navigations
    > 
    > We've found that performance during navigations (in particular tab
    > switches) is impacting in the lower percentiles.
    > 
    > To reduce this for now we exclude navigations, I plan to follow up and
    > make this configurable by finch, but I want to get this cherrypicked
    > into M146 so make this the default for now for ease
    > 
    > Change-Id: Ifa113f99077292e08b0f3bbe4f880c1f021ca539
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7596098
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    > Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1590021}
    
    (cherry picked from commit 0d826d9e94faf4fefc8be376dacdeee88d24174c)
    
    Bug: 487746359
    Change-Id: Ifa113f99077292e08b0f3bbe4f880c1f021ca539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7616985
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7680@{#1724}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- content/browser/memory/scheduler_loop_quarantine_web_contents_observer.cc
+++ content/browser/memory/scheduler_loop_quarantine_web_contents_observer.cc
@@ -4,17 +4,59 @@
 
 #include "content/browser/memory/scheduler_loop_quarantine_web_contents_observer.h"
 
-#include "base/allocator/partition_alloc_support.h"
+#include "base/memory/safety_checks.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/navigation_handle_user_data.h"
 #include "content/public/browser/web_contents.h"
+#include "partition_alloc/buildflags.h"
+
+// `//chrome` and `//android_webview` expect `use_partition_alloc=true`,
+// but some other `//content` embedders like Edge do support both.
+// Hence this #ifdef.
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+#include "base/allocator/partition_alloc_support.h"
+#endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 
 namespace content {
 namespace {
 // We only care about the very first navigation so we use a global to track this
-// state. This variable isn't atomic because we only every access it on the
+// state. This variable isn't atomic because we only ever access it on the
 // Browser UI thread.
 bool g_reconfiguration_done = false;
+
+bool IsEligibleForQuarantineExclusion(NavigationHandle& navigation_handle) {
+  // We ignore non-primary main frame navigations to prevent abuse from
+  // subframes to disable the queue, but also because most user impact occurs on
+  // the primary main frame.
+  const bool is_primary_main_frame = navigation_handle.IsInPrimaryMainFrame();
+  // SAFETY: We want to start the quarantine/zapping of free'd memory if it
+  // is exploitable by the web. "about:blank" is specifically the empty
+  // blank no content page.
+  const bool is_about_blank = navigation_handle.GetURL().IsAboutBlank();
+  return is_primary_main_frame && !is_about_blank;
+}
+
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+// Ties the lifetime of the quarantine exclusion directly to the
+// NavigationHandle. This naturally handles parallel subframe or same-document
+// navigations without any manual state tracking or interference.
+class QuarantineExclusionHolder
+    : public NavigationHandleUserData<QuarantineExclusionHolder> {
+ public:
+  ~QuarantineExclusionHolder() override = default;
+  NAVIGATION_HANDLE_USER_DATA_KEY_DECL();
+
+ private:
+  explicit QuarantineExclusionHolder(NavigationHandle& handle) {
+    CHECK_CURRENTLY_ON(BrowserThread::UI);
+    CHECK(IsEligibleForQuarantineExclusion(handle));
+  }
+  friend class NavigationHandleUserData<QuarantineExclusionHolder>;
+  base::allocator::ScopedSchedulerLoopQuarantineExclusion exclusion_;
+};
+NAVIGATION_HANDLE_USER_DATA_KEY_IMPL(QuarantineExclusionHolder);
+#endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 }  // namespace
 
 SchedulerLoopQuarantineWebContentsObserver::
@@ -28,29 +70,49 @@ SchedulerLoopQuarantineWebContentsObserver::
 SchedulerLoopQuarantineWebContentsObserver::
     ~SchedulerLoopQuarantineWebContentsObserver() = default;
 
+void SchedulerLoopQuarantineWebContentsObserver::DidStartNavigation(
+    NavigationHandle* navigation_handle) {
+  CHECK_CURRENTLY_ON(BrowserThread::UI);
+  // If the quarantine hasn't been configured yet, there is no need to disable
+  // the quarantine.
+  if (!g_reconfiguration_done ||
+      !IsEligibleForQuarantineExclusion(*navigation_handle)) {
+    return;
+  }
+  // `opt_out_scheduler_loop_quarantine_` is only available when
+  // PartitionAlloc is malloc. Without PA the quarantine is a no-op.
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+  QuarantineExclusionHolder::CreateForNavigationHandle(*navigation_handle);
+#endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+}
+
 void SchedulerLoopQuarantineWebContentsObserver::ReadyToCommitNavigation(
     NavigationHandle* navigation_handle) {
   CHECK_CURRENTLY_ON(BrowserThread::UI);
   if (g_reconfiguration_done) {
-    // Some other WebContents has already triggered the reconfiguration, we are
-    // not needed anymore, so we can detach.
-    web_contents()->RemoveUserData(UserDataKey());  // will delete `this`.
+    // Some other WebContents has already triggered the reconfiguration.
     return;
   }
   // SAFETY: We want to start the quarantine/zapping of free'd memory if it is
   // exploitable by the web. "about:blank" is specifically the empty blank no
   // content page.
-  if (navigation_handle->GetURL().IsAboutBlank()) {
+  if (!IsEligibleForQuarantineExclusion(*navigation_handle)) {
     return;
   }
 
-  // We are only interested in the first navigation, reconfigure, remove
-  // ourselves, and mark it as configured for other observers.
+  // We are only interested in the first navigation for reconfigure so we mark
+  // it as configured for other observers.
+  // ReconfigureSchedulerLoopQuarantineBranch() is only available when
+  // PartitionAlloc is built. Without PA the quarantine is a no-op.
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
   base::allocator::ReconfigureSchedulerLoopQuarantineBranch(
       base::allocator::SchedulerLoopQuarantineBranchType::kMain);
+  // We currently opt navigation code out of the quarantine which is normally
+  // done in DidStartNavigation, but because we hadn't configured yet we need to
+  // do it now.
+  QuarantineExclusionHolder::CreateForNavigationHandle(*navigation_handle);
+#endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
   g_reconfiguration_done = true;
-  web_contents()->RemoveUserData(UserDataKey());  // will delete `this`.
-  // `this` is now deleted do not touch anything here anymore.
   return;
 }
 
@@ -58,11 +120,14 @@ void SchedulerLoopQuarantineWebContentsObserver::ReadyToCommitNavigation(
 void SchedulerLoopQuarantineWebContentsObserver::MaybeCreateForWebContents(
     WebContents* web_contents) {
   CHECK_CURRENTLY_ON(BrowserThread::UI);
-  if (base::allocator::IsSchedulerLoopQuarantineEnabled("") &&
-      !AlreadyTriggeredReconfiguration()) {
+  // Without PartitionAlloc there is nothing to reconfigure, so skip creating
+  // the observer entirely.
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+  if (base::allocator::IsSchedulerLoopQuarantineEnabled("")) {
     SchedulerLoopQuarantineWebContentsObserver::CreateForWebContents(
         web_contents);
   }
+#endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 }
 
 // static
--- content/browser/memory/scheduler_loop_quarantine_web_contents_observer_browsertest.cc
+++ content/browser/memory/scheduler_loop_quarantine_web_contents_observer_browsertest.cc
@@ -4,7 +4,11 @@
 
 #include "content/browser/memory/scheduler_loop_quarantine_web_contents_observer.h"
 
-#include "base/allocator/partition_alloc_features.h"
+#include <algorithm>
+#include <string>
+
+#include "base/strings/escape.h"
+#include "base/strings/string_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/browser_test.h"
@@ -15,8 +19,39 @@
 #include "content/shell/browser/shell.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
+#include "partition_alloc/buildflags.h"
+
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+#include "base/allocator/partition_alloc_features.h"
+#include "partition_alloc/scheduler_loop_quarantine_support.h"
+#include "partition_alloc/shim/allocator_shim_default_dispatch_to_partition_alloc.h"
+#endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 
 namespace content {
+namespace {
+// We currently only test the browser UI thread quarantine, to minimize the
+// impact if params get changed we only do the bar minimum config the test
+// needs.
+constexpr char kQuarantineConfigJson[] = R"(
+  {
+    "browser": {
+      "main": {
+        "enable-quarantine":true,
+        "enable-zapping":true,
+        "branch-capacity-in-bytes":524288
+      }
+    }
+  })";
+// The finch jsons requires there be no spaces or new lines for some reason
+// (encoded or otherwise).
+std::string GetQuarantineConfigJson() {
+  std::string config_json = kQuarantineConfigJson;
+  base::RemoveChars(kQuarantineConfigJson, " \n", &config_json);
+  return base::EscapeQueryParamValue(config_json,
+                                     /*use_plus=*/false);
+}
+}  // namespace
+
 class SchedulerLoopQuarantineWebContentsObserverBrowserTestBase
     : public ContentBrowserTest {
  public:
@@ -44,9 +79,14 @@ class SchedulerLoopQuarantineWebContentsObserverBrowserTest
  public:
   SchedulerLoopQuarantineWebContentsObserverBrowserTest() {
 #if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
-    feature_list_.InitWithFeatures(
-        {base::features::kPartitionAllocSchedulerLoopQuarantine,
-         base::features::kPartitionAllocWithAdvancedChecks},
+    feature_list_.InitWithFeaturesAndParameters(
+        {{base::features::kPartitionAllocSchedulerLoopQuarantine,
+          std::map<std::string, std::string> {
+            { base::features::kPartitionAllocSchedulerLoopQuarantineConfig.name,
+              GetQuarantineConfigJson() }
+          }},
+         { base::features::kPartitionAllocWithAdvancedChecks,
+           {} }},
         // Disable preloading `kPrewarm` because it causes the browser to load a
         // webpage before each test starts (during SetUp) which breaks our
         // tests.
@@ -59,6 +99,9 @@ class SchedulerLoopQuarantineWebContentsObserverBrowserTest
         // tests.
         {});
 #endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+    // Ensure even when USE_PARTITION_ALLOC_AS_MALLOC is false we use the
+    // function and that the string parses properly.
+    EXPECT_TRUE(!GetQuarantineConfigJson().empty());
   }
 
  protected:
@@ -125,8 +168,50 @@ IN_PROC_BROWSER_TEST_F(SchedulerLoopQuarantineWebContentsObserverBrowserTest,
                 AlreadyTriggeredReconfiguration());
 }
 
+// This helper function, observer, and test only make sense when partition alloc
+// is malloc.
+#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+// By allocating and freeing a pointer we can check if the quarantine is active
+// or not.
+bool IsQuarantineActive() {
+  auto* root = allocator_shim::internal::PartitionAllocMalloc::Allocator();
+  CHECK(root != nullptr);
+  partition_alloc::internal::
+      ScopedSchedulerLoopQuarantineBranchAccessorForTesting branch_accessor(
+          root);
+  void* ptr = root->Alloc(10);
+  CHECK(ptr != nullptr);
+  CHECK(!branch_accessor.IsQuarantined(ptr));
+  // We explicitly request the quarantine here, but if it was paused it might
+  // return false.
+  root->Free<partition_alloc::FreeFlags::kSchedulerLoopQuarantine>(ptr);
+  return branch_accessor.IsQuarantined(ptr);
+}
+
+// A helper class that we use be able to check mid navigation if the quarantine
+// is active. It will fail the test if the quarantine is active during the
+// ReadyToCommitNavigation stage.
+class TestQuarantinePausedWebContentsObserver
+    : public WebContentsObserver,
+      public WebContentsUserData<TestQuarantinePausedWebContentsObserver> {
+  explicit TestQuarantinePausedWebContentsObserver(WebContents* web_contents)
+      : WebContentsObserver(web_contents),
+        WebContentsUserData<TestQuarantinePausedWebContentsObserver>(
+            *web_contents) {}
+
+  void ReadyToCommitNavigation(NavigationHandle* navigation_handle) override {
+    EXPECT_FALSE(IsQuarantineActive());
+  }
+
+ private:
+  friend class WebContentsUserData<TestQuarantinePausedWebContentsObserver>;
+  WEB_CONTENTS_USER_DATA_KEY_DECL();
+};
+
+WEB_CONTENTS_USER_DATA_KEY_IMPL(TestQuarantinePausedWebContentsObserver);
+
 IN_PROC_BROWSER_TEST_F(SchedulerLoopQuarantineWebContentsObserverBrowserTest,
-                       SecondNavigationDoesNotTrigger) {
+                       SecondNavigationPausesQuarantine) {
   ASSERT_TRUE(embedded_test_server()->Start());
   ASSERT_FALSE(SchedulerLoopQuarantineWebContentsObserver::
                    AlreadyTriggeredReconfiguration());
@@ -139,21 +224,16 @@ IN_PROC_BROWSER_TEST_F(SchedulerLoopQuarantineWebContentsObserverBrowserTest,
             SchedulerLoopQuarantineWebContentsObserver::
                 AlreadyTriggeredReconfiguration());
 
-  // To prove it only triggers once per observer instance, we can reset the
-  // global state and see that a second navigation in the same tab does nothing,
-  // because the observer has detached itself.
-  SchedulerLoopQuarantineWebContentsObserver::ResetForTesting();
-  ASSERT_FALSE(SchedulerLoopQuarantineWebContentsObserver::
-                   AlreadyTriggeredReconfiguration());
-
+  // Before the navigation starts the quarantine is still active.
+  EXPECT_TRUE(IsQuarantineActive());
+  // The second navigation should block the quarantine.
+  TestQuarantinePausedWebContentsObserver::CreateForWebContents(web_contents());
   const GURL url2 = embedded_test_server()->GetURL("/title1.html");
   ASSERT_TRUE(NavigateToURL(web_contents(), url2));
-
-  // The observer is detached after the first `ReadyToCommitNavigation`, so
-  // subsequent navigations in the same WebContents won't trigger it.
-  EXPECT_FALSE(SchedulerLoopQuarantineWebContentsObserver::
-                   AlreadyTriggeredReconfiguration());
+  // But after the navigation finishes it should be active again.
+  EXPECT_TRUE(IsQuarantineActive());
 }
+#endif  // PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
 
 IN_PROC_BROWSER_TEST_F(SchedulerLoopQuarantineWebContentsObserverBrowserTest,
                        NewTabNavigationTriggersIfNotTriggered) {
