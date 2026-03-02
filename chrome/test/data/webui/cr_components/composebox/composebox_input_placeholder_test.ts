e8be8188f5369bee996b106e27f5098eb45ada91
Bofeng Chen <bofengchen@google.com>
2026-03-02 11:50:00 -0800

    [146] [composebox-componentization] Move Composebox input placeholder tests.
    Original change's description:
    > [composebox-componentization] Move Composebox input placeholder tests.
    >
    > The tests for updating the input placeholder based on ToolConfig are
    > moved from new_tab_page/composebox/composebox_test.ts to the more
    > specific cr_components/composebox/composebox_input_placeholder_test.ts.
    > A new test case for updating input placeholder based on ModelConfig is
    > also added. This change centralizes the testing of placeholder logic
    > within the cr-composebox component's test file.
    >
    > Bug: b:487489932
    > Change-Id: I9ba0cfd474205183d27afe8755d5b66a0cf75d97
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7602445
    > Reviewed-by: Hannah Collins <hannahrogers@google.com>
    > Commit-Queue: Bofeng Chen <bofengchen@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1591684}
    
    Bug: 488836444
    Change-Id: I9ba0cfd474205183d27afe8755d5b66a0cf75d97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7620756
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7680@{#1768}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- chrome/test/data/webui/cr_components/composebox/composebox_input_placeholder_test.ts
+++ chrome/test/data/webui/cr_components/composebox/composebox_input_placeholder_test.ts
@@ -8,18 +8,43 @@ import 'chrome://resources/cr_components/composebox/composebox.js';
 import type {ComposeboxElement} from 'chrome://resources/cr_components/composebox/composebox.js';
 import {PageCallbackRouter, PageHandlerRemote} from 'chrome://resources/cr_components/composebox/composebox.mojom-webui.js';
 import {ComposeboxProxyImpl} from 'chrome://resources/cr_components/composebox/composebox_proxy.js';
+import {ModelMode, ToolMode as ComposeboxToolMode} from 'chrome://resources/cr_components/composebox/composebox_query.mojom-webui.js';
+import type {InputState} from 'chrome://resources/cr_components/composebox/composebox_query.mojom-webui.js';
 import {WindowProxy} from 'chrome://resources/cr_components/composebox/window_proxy.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {PageCallbackRouter as SearchboxPageCallbackRouter, PageHandlerRemote as SearchboxPageHandlerRemote} from 'chrome://resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
 import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import type {TestMock} from 'chrome://webui-test/test_mock.js';
 import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
-import {installMock} from './composebox_test_utils.js';
+import {createInputState, installMock} from './composebox_test_utils.js';
 
 suite('ComposeboxInputPlaceholder', () => {
   let composebox: ComposeboxElement;
   let searchboxHandler: TestMock<SearchboxPageHandlerRemote>;
   let windowProxy: TestMock<WindowProxy>;
+
+  async function setupComposeboxWithInputState(inputState: InputState) {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    composebox = document.createElement('cr-composebox');
+    composebox.ntpRealboxNextEnabled = true;
+
+    // We need to wait for the event to be fired when the element is connected.
+    const whenInitialized = new Promise<CustomEvent>((resolve) => {
+      composebox.addEventListener('composebox-initialized', (e: Event) => {
+        resolve(e as CustomEvent);
+      }, {once: true});
+    });
+
+    document.body.appendChild(composebox);
+    const event = await whenInitialized;
+
+    // Call the callback to initialize state.
+    event.detail.initializeComposeboxState(
+        '', [], ComposeboxToolMode.kUnspecified, ModelMode.kUnspecified,
+        inputState);
+    await microtasksFinished();
+  }
 
   setup(async () => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
@@ -35,18 +60,8 @@ suite('ComposeboxInputPlaceholder', () => {
         mock => ComposeboxProxyImpl.getInstance().searchboxHandler = mock);
 
     searchboxHandler.setResultFor('getRecentTabs', Promise.resolve({tabs: []}));
-    searchboxHandler.setResultFor('getInputState', Promise.resolve({
-      state: {
-        allowedModels: [],
-        allowedTools: [],
-        allowedInputTypes: [],
-        activeModel: 0,
-        activeTool: 0,
-        disabledModels: [],
-        disabledTools: [],
-        disabledInputTypes: [],
-      },
-    }));
+    searchboxHandler.setResultFor(
+        'getInputState', Promise.resolve({state: createInputState()}));
 
     windowProxy = installMock(WindowProxy);
     windowProxy.setResultFor('setTimeout', 0);
@@ -73,5 +88,84 @@ suite('ComposeboxInputPlaceholder', () => {
     await composebox.updateComplete;
 
     assertEquals(initialPlaceholder, input.placeholder);
+  });
+
+  test('InputPlaceholderFromModelConfig', async () => {
+    const modelHint = 'Ask a model';
+    const testInputState = createInputState({
+      activeModel: ModelMode.kGeminiRegular,
+      modelConfigs: [{
+        model: ModelMode.kGeminiRegular,
+        hintText: modelHint,
+        menuLabel: '',
+        aimUrlParams: [],
+      }],
+    });
+
+    await setupComposeboxWithInputState(testInputState);
+    assertEquals(modelHint, composebox.$.input.placeholder);
+  });
+
+  const defaultApiHint = loadTimeData.getString('searchboxComposePlaceholder');
+
+  const toolConfigTestCases = [
+    {
+      tool: ComposeboxToolMode.kDeepSearch,
+      hint: 'Research anything',
+      name: 'DeepSearch',
+    },
+    {
+      tool: ComposeboxToolMode.kImageGen,
+      hint: 'Describe your image',
+      name: 'ImageGen',
+    },
+    {
+      tool: ComposeboxToolMode.kCanvas,
+      hint: 'Create anything',
+      name: 'Canvas',
+    },
+  ];
+
+  toolConfigTestCases.forEach(({tool, hint, name}) => {
+    test(`InputPlaceholderFromToolConfig_${name}`, async () => {
+      const mockInputState = createInputState({
+        hintText: defaultApiHint,
+        toolConfigs:
+            toolConfigTestCases.map(t => ({
+                                      tool: t.tool,
+                                      hintText: t.hint,
+                                      menuLabel: '',
+                                      chipLabel: '',
+                                      disableActiveModelSelection: false,
+                                      aimUrlParams: [],
+                                    })),
+      });
+
+      await setupComposeboxWithInputState(mockInputState);
+
+      // Initial placeholder check.
+      assertEquals(defaultApiHint, composebox.$.input.placeholder);
+
+      // Enable tool mode.
+      const contextEntrypoint =
+          composebox.shadowRoot.querySelector('#contextEntrypoint');
+      assertTrue(!!contextEntrypoint);
+      contextEntrypoint.dispatchEvent(new CustomEvent('tool-click', {
+        bubbles: true,
+        composed: true,
+        detail: {toolMode: tool},
+      }));
+      await microtasksFinished();
+      assertEquals(hint, composebox.$.input.placeholder);
+
+      // Disable tool mode.
+      contextEntrypoint.dispatchEvent(new CustomEvent('tool-click', {
+        bubbles: true,
+        composed: true,
+        detail: {toolMode: tool},
+      }));
+      await microtasksFinished();
+      assertEquals(defaultApiHint, composebox.$.input.placeholder);
+    });
   });
 });
