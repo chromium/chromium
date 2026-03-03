c3bf8ce6c5a76cf08e29a5fb138610af67c0fe4c
vsoulgard <vsoulgard@gmail.com>
2026-03-03 11:51:39 -0800

    Add contents vector size reserve
    Simple optimization to use a single allocation
    
    Change-Id: I1c0b7d81c8a4b18047b7aba7146606d6fe9b6651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7363997
    Auto-Submit: V <vsoulgard@gmail.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1593398}

--- chrome/browser/background/background_contents_service.cc
+++ chrome/browser/background/background_contents_service.cc
@@ -242,6 +242,7 @@ void BackgroundContentsService::ShowBalloonForTesting(
 std::vector<BackgroundContents*>
 BackgroundContentsService::GetBackgroundContents() const {
   std::vector<BackgroundContents*> contents;
+  contents.reserve(contents_map_.size());
   for (auto it = contents_map_.begin(); it != contents_map_.end(); ++it)
     contents.push_back(it->second.contents.get());
   return contents;
