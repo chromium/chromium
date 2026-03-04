27354b6da6af44b8a07b374867db818389440a31
Alison Gale <agale@chromium.org>
2026-03-04 11:50:54 -0800

    Fix bookmarks shown metric logging
    When the tree view flag was enabled we were logging a lot of 0 because
    the keyArrowNavigationService hadn't rebuilt the navigation elements due
    to debouncing. This moves the logging until after that is built. I
    tested expanding/collapsing folders and stepping into a folder.
    
    Bug: 489466748
    Change-Id: Iaa76c5ca737d2dc64ffc1156e349024134315a1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7629429
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594123}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmarks_list.ts
@@ -404,10 +404,7 @@ export class PowerBookmarksListElement extends PolymerElement implements
     this.keyArrowNavigationService_.startListening();
 
     this.addEventListener(BOOKMARK_ROW_LOAD_EVENT, () => {
-      this.rebuildNavigationElementsDebouncer_ = Debouncer.debounce(
-          this.rebuildNavigationElementsDebouncer_, timeOut.after(1), () => {
-            this.keyArrowNavigationService_.rebuildNavigationElements();
-          });
+      this.rebuildNavigationElements_();
     });
   }
 
@@ -544,9 +541,7 @@ export class PowerBookmarksListElement extends PolymerElement implements
     // If the parent folder is visible, notify to ensure its displayed
     // child count is updated.
     this.notifyPathIfVisible_(bookmark.parentId, 'children');
-    afterNextRender(this, () => {
-      this.keyArrowNavigationService_.rebuildNavigationElements();
-    });
+    this.rebuildNavigationElements_();
   }
 
   getTrackedProductInfos(): {[key: string]: BookmarkProductInfo} {
@@ -797,7 +792,6 @@ export class PowerBookmarksListElement extends PolymerElement implements
 
     if (this.recordCountMetricsOnNextUpdate_) {
       this.recordBookmarkCountMetrics_();
-      this.recordCountMetricsOnNextUpdate_ = false;
     }
 
     // After the lists are updated and all children updates are complete,
@@ -812,7 +806,7 @@ export class PowerBookmarksListElement extends PolymerElement implements
 
               // Make sure the keyboard navigation tree is rebuilt whenever the
               // iron-list is updated.
-              this.keyArrowNavigationService_.rebuildNavigationElements();
+              this.rebuildNavigationElements_();
             });
       }
     });
@@ -857,7 +851,28 @@ export class PowerBookmarksListElement extends PolymerElement implements
     this.recordCountMetricsOnNextUpdate_ = true;
   }
 
+  private rebuildNavigationElements_() {
+    this.rebuildNavigationElementsDebouncer_ = Debouncer.debounce(
+        this.rebuildNavigationElementsDebouncer_, timeOut.after(1), () => {
+          this.keyArrowNavigationService_.rebuildNavigationElements();
+          if (this.recordCountMetricsOnNextUpdate_) {
+            this.recordBookmarkCountMetricsInternal_();
+            this.recordCountMetricsOnNextUpdate_ = false;
+          }
+        });
+  }
+
   private recordBookmarkCountMetrics_() {
+    if (this.bookmarksTreeViewEnabled_) {
+      this.recordCountMetricsOnNextUpdate_ = true;
+      this.rebuildNavigationElements_();
+    } else {
+      this.recordBookmarkCountMetricsInternal_();
+      this.recordCountMetricsOnNextUpdate_ = false;
+    }
+  }
+
+  private recordBookmarkCountMetricsInternal_() {
     const count = this.bookmarksTreeViewEnabled_ ?
         this.keyArrowNavigationService_.getElementCount() :
         this.displayLists_.reduce((prev, curr) => prev + curr.length, 0);
@@ -890,7 +905,7 @@ export class PowerBookmarksListElement extends PolymerElement implements
     event: MouseEvent,
   }>) {
     this.notifyBookmarksListResize_();
-    afterNextRender(this, () => this.recordBookmarkCountMetrics_());
+    this.recordBookmarkCountMetrics_();
   }
   /**
    * Invoked when the user clicks a power bookmarks row. This will either
