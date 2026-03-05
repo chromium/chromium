10d9c01b72dc5b59905022e8c81a521177d92fe2
Abdelrahman Eed <abdoeed@google.com>
2026-03-05 02:50:27 -0800

    Reland "[Document PiP] Adjust Document PiP window bounds on global layout"
    This is a reland of commit a758ac160ec2e059989a11d89936b635a7a7bc55
    
    Reason for Reland:
    
    Replaced the failing test with a unit test instead
    
    Original change's description:
    > [Document PiP] Adjust Document PiP window bounds on global layout
    >
    > Adds a global layout listener to the content view of
    > DocumentPictureInPictureActivity to adjust the activity's bounds. The
    > bounds are calculated using the content view's current width & height
    > compared with the desired width & height provided in the window options
    >
    > Bug: b:465413462
    > Change-Id: Ib97a7cc15737907c92696b2328dba49131886ba1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7602554
    > Auto-Submit: Abdelrahman Eed <abdoeed@google.com>
    > Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    > Reviewed-by: Phil Yan <philyan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1593072}
    
    Bug: b:465413462
    Change-Id: I2817b997be959fb7a04a75f4eb97c657f8db2c34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7632075
    Reviewed-by: Phil Yan <philyan@chromium.org>
    Auto-Submit: Abdelrahman Eed <abdoeed@google.com>
    Commit-Queue: Abdelrahman Eed <abdoeed@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594562}

--- chrome/android/java/src/org/chromium/chrome/browser/media/DocumentPictureInPictureActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/DocumentPictureInPictureActivity.java
@@ -16,9 +16,11 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 
 import androidx.annotation.CallSuper;
+import androidx.annotation.VisibleForTesting;
 
 import org.jni_zero.NativeMethods;
 
@@ -67,6 +69,8 @@ import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.common.ResourceRequestBody;
 import org.chromium.ui.base.ActivityWindowAndroid;
 import org.chromium.ui.base.ViewAndroidDelegate;
+import org.chromium.ui.display.DisplayAndroid;
+import org.chromium.ui.display.DisplayUtil;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.url.GURL;
 
@@ -319,6 +323,64 @@ public class DocumentPictureInPictureActivity extends AsyncInitializationActivit
                                                 this, mInitiatorTab, this::finish));
             }
         }
+
+        if (mWindowOptions != null && mWindowOptions.windowBounds != null) {
+            contentLayout
+                    .getViewTreeObserver()
+                    .addOnGlobalLayoutListener(
+                            new ViewTreeObserver.OnGlobalLayoutListener() {
+                                @Override
+                                public void onGlobalLayout() {
+                                    resizeContents(
+                                            assumeNonNull(mWindowOptions.windowBounds).width(),
+                                            assumeNonNull(mWindowOptions.windowBounds).height());
+
+                                    contentLayout
+                                            .getViewTreeObserver()
+                                            .removeOnGlobalLayoutListener(this);
+                                }
+                            });
+        }
+    }
+
+    /**
+     * Resizes the contents of the activity to the given DP dimensions.
+     *
+     * <p>This method resizes the contents of the activity to the given DP dimensions by resizing
+     * the window. Note that Android has a minimum size (220dp) & a maximum size (70% of display
+     * size in width and height) for pinned windows, so the requested size may not be respected.
+     */
+    @VisibleForTesting
+    void resizeContents(int widthDp, int heightDp) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.R) {
+            // This method is not supported on API versions below 30.
+            return;
+        }
+
+        FrameLayout contentLayout = findViewById(R.id.document_picture_in_picture_content);
+        DisplayAndroid display = assumeNonNull(getWindowAndroid()).getDisplay();
+        int curContentsWidth = DisplayUtil.pxToDp(display, contentLayout.getWidth());
+        int curContentsHeight = DisplayUtil.pxToDp(display, contentLayout.getHeight());
+
+        if (curContentsWidth == widthDp && curContentsHeight == heightDp) {
+            return;
+        }
+
+        int widthDiff = widthDp - curContentsWidth;
+        int heightDiff = heightDp - curContentsHeight;
+
+        Rect currentWindowBounds =
+                DisplayUtil.convertLocalPxToGlobalDipCoordinates(
+                        display,
+                        new Rect(getWindowManager().getCurrentWindowMetrics().getBounds()));
+
+        MultiWindowUtils.moveActivityToBounds(
+                this,
+                new Rect(
+                        currentWindowBounds.left - widthDiff,
+                        currentWindowBounds.top - heightDiff,
+                        currentWindowBounds.right,
+                        currentWindowBounds.bottom));
     }
 
     @Override
@@ -497,7 +559,7 @@ public class DocumentPictureInPictureActivity extends AsyncInitializationActivit
 
         @Override
         public void setContentsBounds(WebContents source, Rect bounds) {
-            MultiWindowUtils.moveActivityToBounds(DocumentPictureInPictureActivity.this, bounds);
+            resizeContents(bounds.width(), bounds.height());
         }
     }
 
---
+++ chrome/android/junit/src/org/chromium/chrome/browser/media/DocumentPictureInPictureActivityUnitTest.java
@@ -0,0 +1,138 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.media;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import android.app.ActivityManager.AppTask;
+import android.graphics.Rect;
+import android.os.Build;
+import android.view.WindowManager;
+import android.view.WindowMetrics;
+import android.widget.FrameLayout;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.AconfigFlaggedApiDelegate;
+import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.util.AndroidTaskUtils;
+import org.chromium.ui.base.ActivityWindowAndroid;
+import org.chromium.ui.display.DisplayAndroid;
+import org.chromium.ui.display.DisplayAndroidManager;
+
+/** Unit tests for {@link DocumentPictureInPictureActivity}. */
+@RunWith(BaseRobolectricTestRunner.class)
+public class DocumentPictureInPictureActivityUnitTest {
+    @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
+
+    @Mock private ActivityWindowAndroid mActivityWindowAndroid;
+    @Mock private DisplayAndroid mDisplayAndroid;
+    @Mock private WindowManager mWindowManager;
+    @Mock private WindowMetrics mWindowMetrics;
+    @Mock private FrameLayout mContentLayout;
+    @Mock private AconfigFlaggedApiDelegate mAconfigFlaggedApiDelegate;
+    @Mock private AppTask mAppTask;
+    @Mock private DisplayAndroidManager mDisplayAndroidManager;
+
+    private DocumentPictureInPictureActivity mActivity;
+
+    @Before
+    public void setUp() {
+        AconfigFlaggedApiDelegate.setInstanceForTesting(mAconfigFlaggedApiDelegate);
+        AndroidTaskUtils.setAppTaskForTesting(mAppTask);
+        DisplayAndroidManager.setInstanceForTesting(mDisplayAndroidManager);
+        DisplayAndroid.setNonMultiDisplayForTesting(mDisplayAndroid);
+
+        // Common Display setup
+        when(mDisplayAndroid.getDipScale()).thenReturn(1.0f);
+        Rect displayBounds = new Rect(0, 0, 1000, 1000);
+        when(mDisplayAndroid.getBounds()).thenReturn(displayBounds);
+        when(mDisplayAndroid.getLocalBounds()).thenReturn(displayBounds);
+        when(mDisplayAndroid.getDisplayId()).thenReturn(0);
+
+        mActivity = spy(Robolectric.buildActivity(DocumentPictureInPictureActivity.class).get());
+
+        doReturn(mActivityWindowAndroid).when(mActivity).getWindowAndroid();
+        when(mActivityWindowAndroid.getDisplay()).thenReturn(mDisplayAndroid);
+
+        doReturn(mWindowManager).when(mActivity).getWindowManager();
+        when(mWindowManager.getCurrentWindowMetrics()).thenReturn(mWindowMetrics);
+
+        doReturn(mContentLayout)
+                .when(mActivity)
+                .findViewById(R.id.document_picture_in_picture_content);
+
+        when(mDisplayAndroidManager.getDisplayMatching(any(Rect.class)))
+                .thenReturn(mDisplayAndroid);
+    }
+
+    @After
+    public void tearDown() {
+        AconfigFlaggedApiDelegate.setInstanceForTesting(null);
+        AndroidTaskUtils.setAppTaskForTesting(null);
+        DisplayAndroidManager.resetInstanceForTesting();
+        DisplayAndroid.setNonMultiDisplayForTesting(null);
+    }
+
+    @Test
+    @Config(sdk = Build.VERSION_CODES.S)
+    public void testResizeContents() {
+        when(mContentLayout.getWidth()).thenReturn(100);
+        when(mContentLayout.getHeight()).thenReturn(100);
+
+        int newWidthDp = 150;
+        int newHeightDp = 150;
+
+        // Current window bounds in pixels (from WindowMetrics)
+        Rect windowBoundsPx = new Rect(100, 100, 300, 300); // 200x200
+        when(mWindowMetrics.getBounds()).thenReturn(windowBoundsPx);
+
+        mActivity.resizeContents(newWidthDp, newHeightDp);
+
+        // Expectation:
+        // widthDiff = 150 - 100 = 50
+        // heightDiff = 150 - 100 = 50
+        // currentWindowBounds (Global Dip) = windowBoundsPx (since density 1.0 and origin 0,0) =
+        // 100, 100, 300, 300
+        // newBounds (Global Dip) = left - 50, top - 50, right, bottom
+        //                        = 100 - 50, 100 - 50, 300, 300
+        //                        = 50, 50, 300, 300
+        // localBounds (Px) = newBounds (Global Dip) (since density 1.0 and origin 0,0) = 50, 50,
+        // 300, 300
+
+        verify(mAconfigFlaggedApiDelegate)
+                .moveTaskTo(
+                        eq(mAppTask),
+                        eq(0), // displayId
+                        eq(new Rect(50, 50, 300, 300)));
+    }
+
+    @Test
+    @Config(sdk = Build.VERSION_CODES.S)
+    public void testResizeContents_NoChange() {
+        when(mContentLayout.getWidth()).thenReturn(100);
+        when(mContentLayout.getHeight()).thenReturn(100);
+
+        mActivity.resizeContents(100, 100);
+
+        verifyNoInteractions(mAconfigFlaggedApiDelegate);
+    }
+}
