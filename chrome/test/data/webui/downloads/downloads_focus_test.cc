bfced36b4a7b2eefbe7e500812011f19911ff6f4
Michael Ershov <miersh@google.com>
2026-03-05 03:02:32 -0800

    [gardener] Disable flaky DownloadsFocusTest.Item test
    Bug: crbug.com/489979009
    Test: DownloadsFocusTest.Item
    Change-Id: Ic32119beab63a51f1c30cecd6981185f8103aef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637091
    Reviewed-by: Alexander Hendrich <hendrich@chromium.org>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1594569}

--- chrome/test/data/webui/downloads/downloads_focus_test.cc
+++ chrome/test/data/webui/downloads/downloads_focus_test.cc
@@ -8,7 +8,13 @@
 
 typedef WebUIMochaFocusTest DownloadsFocusTest;
 
-IN_PROC_BROWSER_TEST_F(DownloadsFocusTest, Item) {
+// TODO(crbug.com/489979009): Re-enable when the test is not flaky anymore.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_Item DISABLED_Item
+#else
+#define MAYBE_Item Item
+#endif
+IN_PROC_BROWSER_TEST_F(DownloadsFocusTest, MAYBE_Item) {
   set_test_loader_host(chrome::kChromeUIDownloadsHost);
   RunTest("downloads/item_test.js", "runMochaSuite('ItemFocusTest')");
 }
