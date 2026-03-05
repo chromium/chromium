cad5903484d19fe15c828ee76c0a507ddc42184e
Saifuddin Hitawala <hitawala@chromium.org>
2026-03-05 08:03:21 -0800

    [gpu] Add DumpWillBeCheck for WebGPUTextureScopedAccess
    As part of enforcing clients pass the correct usages when creating
    ScopedAccess on the shared image, this CL adds a DUMP_WILL_BE_CHECK
    for WebGPU usage read/write when clients try to perform a
    BeginWebGPUTextureAccess.
    
    Bug: 420632430
    Change-Id: I327abc6064b08eb4e3dc052ed9f3df88c3f29377
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7634298
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594699}

--- gpu/command_buffer/client/client_shared_image.cc
+++ gpu/command_buffer/client/client_shared_image.cc
@@ -836,6 +836,11 @@ ClientSharedImage::BeginWebGPUTextureAccess(
     const wgpu::dawn::wire::client::TextureDescriptor& desc,
     uint64_t usage,
     webgpu::MailboxFlags mailbox_flags) {
+  SCOPED_CRASH_KEY_STRING64("ClientSharedImage", "DebugLabel", debug_label_);
+  SCOPED_CRASH_KEY_STRING256("ClientSharedImage", "Usage",
+                             metadata_.usage.ToString());
+  DUMP_WILL_BE_CHECK(metadata_.usage.Has(SHARED_IMAGE_USAGE_WEBGPU_READ) ||
+                     metadata_.usage.Has(SHARED_IMAGE_USAGE_WEBGPU_WRITE));
   return base::WrapUnique(new WebGPUTextureScopedAccess(
       webgpu, this, sync_token, device, desc, usage, mailbox_flags));
 }
@@ -996,6 +1001,18 @@ WebGPUTextureScopedAccess::WebGPUTextureScopedAccess(
                                          wgpu::TextureUsage::RenderAttachment |
                                          wgpu::TextureUsage::StorageBinding;
   readonly_ = !((desc.usage | wgpu::TextureUsage{usage}) & write_flags);
+  SCOPED_CRASH_KEY_STRING64("ClientSharedImage", "DebugLabel",
+                            shared_image_->debug_label());
+  SCOPED_CRASH_KEY_STRING256("ClientSharedImage", "Usage",
+                             shared_image_->usage().ToString());
+  if (readonly_) {
+    DUMP_WILL_BE_CHECK(
+        shared_image_->usage().Has(SHARED_IMAGE_USAGE_WEBGPU_READ));
+  } else {
+    DUMP_WILL_BE_CHECK(
+        shared_image_->usage().Has(SHARED_IMAGE_USAGE_WEBGPU_WRITE));
+  }
+
   shared_image_->BeginAccess(readonly_);
   texture_ = base::WrapUnique(
       new wgpu::Texture(wgpu::Texture::Acquire(reservation.texture)));
