bdff26557f3c1c7a497ca87fe376ee5cc5bf7c96
Noah Rose Ledesma <noahrose@google.com>
2026-03-05 12:53:25 -0800

    Updater UI: Add ARIA roles for policy table
    Include roles for the enterprise policy table to communicate the table
    structure to assistive technologies.
    
    Additionally, use the aria-expanded and aria-controls attributes to
    link the expand button to the collapsible section.
    
    Fixed: 489516790
    Change-Id: I3d4a9bb09e187aa3633d43eb17a36bea6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639809
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1594893}

--- chrome/browser/resources/updater/enterprise_policy_table/enterprise_policy_table_section.html.ts
+++ chrome/browser/resources/updater/enterprise_policy_table/enterprise_policy_table_section.html.ts
@@ -10,24 +10,28 @@ export function getHtml(this: EnterprisePolicyTableSectionElement) {
   // clang-format off
   return html`
 <!--_html_template_start_-->
-  <div class="policy-table">
-    <div class="header-row">
-      <div class="column-name">$i18n{policyName}</div>
-      <div class="column-source">$i18n{policySource}</div>
-      <div class="column-value">$i18n{policyValue}</div>
-      <div class="column-warning">$i18n{policyStatus}</div>
-      <div class="column-expand"></div>
+  <div class="policy-table" role="table">
+    <div class="header-row" role="row">
+      <div class="column-name" role="columnheader">$i18n{policyName}</div>
+      <div class="column-source" role="columnheader">$i18n{policySource}</div>
+      <div class="column-value" role="columnheader">$i18n{policyValue}</div>
+      <div class="column-warning" role="columnheader">$i18n{policyStatus}</div>
+      <div class="column-expand" role="columnheader"></div>
     </div>
     ${this.rowData.map((item, index) => html`
-      <div class="policy-row" ?expanded="${item.isExpanded}">
-        <div class="column-name" title="${item.name}">${item.name}</div>
-        <div class="column-source">${item.policy.prevailingSource}</div>
-        <div class="column-value">
+      <div class="policy-row" role="row" ?expanded="${item.isExpanded}">
+        <div class="column-name" role="cell" title="${item.name}">
+          ${item.name}
+        </div>
+        <div class="column-source" role="cell">
+          ${item.policy.prevailingSource}
+        </div>
+        <div class="column-value" role="cell">
           <enterprise-policy-value .policyName="${item.name}"
               .value="${this.prevailingValue(item)}">
           </enterprise-policy-value>
         </div>
-        <div class="column-warning">
+        <div class="column-warning" role="cell">
           ${item.hasConflict ? html`
             <cr-icon class="warning-icon" icon="cr:warning" role="img"
                 aria-label="$i18n{policyConflictWarning}"
@@ -39,32 +43,36 @@ export function getHtml(this: EnterprisePolicyTableSectionElement) {
             </cr-icon>
           `}
         </div>
-        <div class="column-expand">
+        <div class="column-expand" role="cell">
           ${this.canExpand(item) ? html`
             <cr-icon-button class="expand-icon"
                 data-index="${index}"
                 iron-icon="${item.isExpanded ? 'cr:expand-less' :
                     'cr:expand-more'}"
+                aria-expanded="${item.isExpanded}"
+                aria-controls="expanded-section-${index}"
                 @click="${this.onExpandButtonClick}">
             </cr-icon-button>
           ` : ''}
         </div>
       </div>
-      <cr-collapse class="expanded-section" ?opened="${item.isExpanded}"
-          no-animation>
+      <cr-collapse id="expanded-section-${index}" class="expanded-section"
+          ?opened="${item.isExpanded}" no-animation>
         ${Object.entries(item.policy.valuesBySource)
             .filter(([source, _]) => source !== item.policy.prevailingSource)
             .map(([source, value]) => html`
-          <div class="expanded-row">
-            <div class="column-name">$i18n{policyIgnored}</div>
-            <div class="column-source">${source}</div>
-            <div class="column-value">
+          <div class="expanded-row" role="row">
+            <div class="column-name" role="cell">
+              $i18n{policyIgnored}
+            </div>
+            <div class="column-source" role="cell">${source}</div>
+            <div class="column-value" role="cell">
               <enterprise-policy-value .policyName="${item.name}"
                   .value="${value}">
               </enterprise-policy-value>
             </div>
-            <div class="column-warning"></div>
-            <div class="column-expand"></div>
+            <div class="column-warning" role="cell"></div>
+            <div class="column-expand" role="cell"></div>
           </div>
         `)}
       </cr-collapse>
