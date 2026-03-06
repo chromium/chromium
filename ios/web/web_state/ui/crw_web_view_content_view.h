dce09748208f818286f8a4a6776fc23e6f4c60f4
Gauthier Ambard <gambard@chromium.org>
2026-03-06 04:56:04 -0800

    [iOS] Prepare to move crw_web_view_content_view.h to new location
    This CL creates a forward header for the content view in its new
    location to help downstream changes.
    
    Prep for crrev.com/i/9080758
    
    Bug: 483998779
    Change-Id: If29d241f25f525d9676af7dbb03e3c5d18f85bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642033
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595326}

---
+++ ios/web/web_state/ui/crw_web_view_content_view.h
@@ -0,0 +1,10 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
+#define IOS_WEB_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
+
+#import "ios/web/common/crw_web_view_content_view.h"
+
+#endif  // IOS_WEB_WEB_STATE_UI_CRW_WEB_VIEW_CONTENT_VIEW_H_
