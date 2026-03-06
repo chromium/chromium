b31ae81e1d112cd81689e808368769bfedf8be2e
Kay Lin <kaiyilin@google.com>
2026-03-05 23:56:37 -0800

    Site Search: Replace add dialog
    This CL replaces the AddSearchEngineDialogCoordinator with the general
    dialog introduced in previous CLs.
    
    Bug: 489037492
    Change-Id: If29519417c6b6c15d124712bd593704308fb6d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7632095
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595223}

--- chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/custom_site_search/CustomSiteSearchCoordinator.java
+++ chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/custom_site_search/CustomSiteSearchCoordinator.java
@@ -27,7 +27,6 @@ public class CustomSiteSearchCoordinator {
     private final CustomSiteSearchMediator mMediator;
     private final PropertyModel mPropertyModel;
     private final PropertyModelChangeProcessor mPropertyModelChangeProcessor;
-    private final AddSearchEngineDialogCoordinator mAddSearchEngineDialogCoordinator;
     private final SiteSearchDialogCoordinator mSiteSearchDialogCoordinator;
 
     public CustomSiteSearchCoordinator(
@@ -44,12 +43,6 @@ public class CustomSiteSearchCoordinator {
                         profile,
                         this::openAddSearchEngineDialog,
                         this::openEditSearchEngineDialog);
-
-        mAddSearchEngineDialogCoordinator =
-                new AddSearchEngineDialogCoordinator(
-                        context,
-                        modalDialogManager,
-                        TemplateUrlServiceFactory.getForProfile(profile));
 
         mSiteSearchDialogCoordinator =
                 new SiteSearchDialogCoordinator(
@@ -68,7 +61,6 @@ public class CustomSiteSearchCoordinator {
     }
 
     public void destroy() {
-        mAddSearchEngineDialogCoordinator.dismiss();
         mSiteSearchDialogCoordinator.dismiss();
         mPropertyModel.set(SiteSearchProperties.ADAPTER, null);
         mPropertyModelChangeProcessor.destroy();
@@ -77,7 +69,7 @@ public class CustomSiteSearchCoordinator {
     }
 
     private void openAddSearchEngineDialog() {
-        mAddSearchEngineDialogCoordinator.show();
+        mSiteSearchDialogCoordinator.showAddDialog();
     }
 
     private void openEditSearchEngineDialog(TemplateUrl templateUrl) {
