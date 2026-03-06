d7cbd750a115dd4a5ea49baa08b178c266c1c9cc
Jacob Stanley <jacobstanley@google.com>
2026-03-06 11:42:54 -0800

    [Web Modal] Remove a11y arg from IgnoreInputEvents
    A previous CL
    (https://chromium-review.googlesource.com/c/chromium/src/+/7634939)
    updated the base behavior of WebContents::IgnoreInputEvents to ignore
    a11y input alongside regular input. Because of this, explicitly passing
    `should_ignore_a11y_input=true` is no longer necessary.
    
    This CL updates the WebContentsModalDialogManager to use the standard 1
    parameter version of IgnoreInputEvents. This is part of an ongoing
    migration to clean up and eventually remove the 2-parameter overloaded
    method.
    
    Bug: 452693512
    Change-Id: I5949cd8a571dde3cba738ad146f0b1343688fff5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7639052
    Commit-Queue: Mike Wittman <wittman@chromium.org>
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Auto-Submit: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1595580}

--- components/web_modal/web_contents_modal_dialog_manager.cc
+++ components/web_modal/web_contents_modal_dialog_manager.cc
@@ -128,8 +128,7 @@ void WebContentsModalDialogManager::BlockWebContentsInteraction(bool blocked) {
   }
 
   if (blocked) {
-    scoped_ignore_input_events_ = contents->IgnoreInputEvents(
-        std::nullopt, /*should_ignore_a11y_input=*/true);
+    scoped_ignore_input_events_ = contents->IgnoreInputEvents(std::nullopt);
   } else {
     scoped_ignore_input_events_.reset();
   }
