d4fc818e7e616d48a499a6bc6574f1e452dec875
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-06 12:47:41 -0800

    Revert "[headless] Fix SetZoomedWindowBounds tests"
    This reverts commit f6a626c8fe3433f6ab1c9fe4fa988335b83e18b2.
    
    Reason for revert: Test is failing.
    Sample failed build:
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac15-x64-rel-tests/14907/overview
    
    Original change's description:
    > [headless] Fix SetZoomedWindowBounds tests
    >
    > This CL refactors Headless Protocol SetZoomedWindowBounds test to
    > produce stable results.
    >
    > Bug: 446247998
    > Change-Id: I92027893a59e5a994d185433d22733973aeaa422
    > Cq-Include-Trybots: luci.chromium.try:mac12-tests,mac13-tests,mac14-tests
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7640060
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1595478}
    
    Bug: 446247998
    Cq-Include-Trybots: luci.chromium.try:mac12-tests,mac13-tests,mac14-tests
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ic902246b50089bc0714e83345d3f75d4bba78ce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644597
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Owners-Override: Devlin Cronin <rdevlin.cronin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595627}

--- chrome/browser/headless/headless_mode_protocol_browsertest.cc
+++ chrome/browser/headless/headless_mode_protocol_browsertest.cc
@@ -320,9 +320,6 @@ HEADLESS_MODE_PROTOCOL_TEST(FullscreenWindowSizeScaled,
                             "shared/fullscreen-window-size-scaled.js")
 #endif  // !BUILDFLAG(IS_MAC)
 
-HEADLESS_MODE_PROTOCOL_TEST(SetZoomedWindowBounds,
-                            "shared/set-zoomed-window-bounds.js")
-
 HEADLESS_MODE_PROTOCOL_TEST(PrintToPdfTinyPage,
                             "shared/print-to-pdf-tiny-page.js")
 
@@ -523,4 +520,12 @@ HEADLESS_MODE_PROTOCOL_TEST(SetPrimaryScreenScaled,
 HEADLESS_MODE_PROTOCOL_TEST(RangeMouseEventAfterNodeRemoval,
                             "shared/range-mouse-event-after-node-removal.js")
 
+// TODO(crbug.com/423951863): Fails on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_SetZoomedWindowBounds DISABLED_SetZoomedWindowBounds
+#else
+#define MAYBE_SetZoomedWindowBounds SetZoomedWindowBounds
+#endif
+HEADLESS_MODE_PROTOCOL_TEST(MAYBE_SetZoomedWindowBounds,
+                            "shared/set-zoomed-window-bounds.js")
 }  // namespace headless
--- components/headless/test/data/protocol/shared/set-zoomed-window-bounds.js
+++ components/headless/test/data/protocol/shared/set-zoomed-window-bounds.js
@@ -3,6 +3,10 @@
 // found in the LICENSE file.
 //
 // META: --screen-info={1600x1200}
+//
+// This test produces unexpected results in headless mode,
+// see http://crbug.com/446247998.
+// META: fork_headless_mode_expectations
 
 (async function(testRunner) {
   const {dp} = await testRunner.startBlank(
@@ -12,22 +16,20 @@
 
   async function logWindowBounds() {
     const {bounds} = (await dp.Browser.getWindowBounds({windowId})).result;
-    testRunner.log(`Window: ${bounds.left},${bounds.top} ${bounds.width}x${
+    testRunner.log(`${bounds.left},${bounds.top} ${bounds.width}x${
         bounds.height} ${bounds.windowState}`);
   }
-
-  testRunner.log(`Initial state:`);
-  dp.Browser.setWindowBounds(
-      {windowId, bounds: {left: 10, top: 10, width: 700, height: 500}});
-  await logWindowBounds();
 
   for (const state of ['maximized', 'fullscreen']) {
     testRunner.log(`Setting '${state}' state`);
     dp.Browser.setWindowBounds({windowId, bounds: {windowState: state}});
     await logWindowBounds();
 
-    testRunner.log(`Restoring to normal state:`);
-    dp.Browser.setWindowBounds({windowId, bounds: {windowState: `normal`}});
+    const bounds =
+        {left: 10, top: 10, width: 700, height: 500, windowState: `normal`};
+
+    testRunner.log(bounds, `Setting bounds: `);
+    dp.Browser.setWindowBounds({windowId, bounds});
     await logWindowBounds();
   }
 
