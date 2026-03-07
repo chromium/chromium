95b78bcfd5b677e168f17c7049dc12e09e1bb302
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-06 16:09:22 -0800

    [Extensions] Add regression test for OpenTabHelper crash
    There was a crash when OpenTabHelper needed to create a new browser
    window. In practice, this happens when OpenTabHelper couldn't find a
    suitable browser window to use to host the new tab. This, in turn, could
    happen in the following scenario:
    * The user has *only* an incognito browser window open
    * A platform app calls browser.openTab() from its background page
    
    Since the app isn't allowed to run in incognito, the tab can't be
    opened in that window, and a new one has to be created.
    
    This was fixed in crrev.com/7439300b2ff11, but this CL adds a regression
    test.
    
    Bug: 490330704
    Change-Id: I95c59b405e24f350f0a14fb45c376b3e094c845e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7644928
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595773}

--- chrome/browser/apps/platform_apps/api/browser/browser_apitest.cc
+++ chrome/browser/apps/platform_apps/api/browser/browser_apitest.cc
@@ -8,6 +8,7 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
+#include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
 #include "extensions/test/test_extension_dir.h"
 #include "net/dns/mock_host_resolver.h"
@@ -79,6 +80,61 @@ IN_PROC_BROWSER_TEST_F(AppsBrowserApiTest, OpenTab) {
   EXPECT_TRUE(content::WaitForLoadStop(web_contents));
   EXPECT_EQ(embedded_test_server()->GetURL("example.com", "/simple.html"),
             web_contents->GetLastCommittedURL());
+}
+
+// Tests calling browser.openTab() when the only active browser is an incognito
+// browser.
+// Regression test for https://crbug.com/490330704.
+IN_PROC_BROWSER_TEST_F(AppsBrowserApiTest,
+                       OpenTabWithOnlyIncognitoBrowserOpen) {
+  static constexpr char kManifest[] =
+      R"({
+           "name": "Browser API test incognito",
+           "version": "2.0",
+           "manifest_version": 2,
+           "permissions": ["browser"],
+           "app": {
+             "background": {
+               "scripts": ["background.js"]
+             }
+           }
+         })";
+
+  static constexpr char kBackgroundJs[] =
+      R"(chrome.test.getConfig((config) => {
+           const url =
+               `http://example.com:${config.testServer.port}/simple.html`;
+           chrome.test.sendMessage('ready', function(reply) {
+             chrome.browser.openTab({url: url}, function() {
+                chrome.test.assertNoLastError();
+                chrome.test.succeed();
+             });
+           });
+         });)";
+  extensions::TestExtensionDir test_dir;
+  test_dir.WriteManifest(kManifest);
+  test_dir.WriteFile(FILE_PATH_LITERAL("background.js"), kBackgroundJs);
+
+  ExtensionTestMessageListener listener("ready", ReplyBehavior::kWillReply);
+  extensions::ResultCatcher result_catcher;
+
+  const extensions::Extension* extension =
+      LoadExtension(test_dir.UnpackedPath());
+  ASSERT_TRUE(extension);
+  EXPECT_TRUE(listener.WaitUntilSatisfied());
+
+  // Create an incognito browser window.
+  Browser* incognito_browser = CreateIncognitoBrowser(profile());
+  ASSERT_TRUE(incognito_browser);
+
+  // Close the main browser window. This leaves the incognito window as the
+  // only open browser.
+  CloseBrowserSynchronously(browser());
+
+  // Tell the extension to continue and call openTab.
+  listener.Reply("");
+
+  EXPECT_TRUE(result_catcher.GetNextResult()) << result_catcher.message();
 }
 
 }  // namespace apps
