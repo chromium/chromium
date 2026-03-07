a01932c73492756b84a8242ef74384b568b25472
Camillia Smith Barnes <cammie@chromium.org>
2026-03-06 16:22:46 -0800

    WebUI Browser: Define SurfaceEmbedConnector public interface
    This change introduces the `SurfaceEmbedConnector` API in
    `content/public/browser`, which enables embedding a child `WebContents`
    within a parent `WebContents` without establishing a traditional OOPIF
    connection (CrossProcessFrameConnector and such). This architecture is
    designed for scenarios where the embedded content requires full
    interactive browsing experience while it needs to be disconnected from
    its embedder frame for scripting capability.
    
    The implementation defines the `SurfaceEmbedConnector` class, providing
    static `Attach` and `Detach` methods for lifecycle management, and a
    `Delegate` interface for embedder-specific callbacks. The API supports
    essential features for embedding, including FrameSinkId exchange and
    visual property synchronization. Visibility updates, and bidirectional
    focus propagation between the embedder and the guest will be added
    later.
    
    The current CL roadmap is: 1. Add public interface (this CL). 2. Add
    stub implementation (https://crrev.com/c/7539321); this is where we
    begin to implement SurfaceEmbedConnector inside of content. 3. Add a
    dummy surface provider to the implementation
    (https://crrev.com/c/7596122). 4. Add SurfaceEmbedConnector::Attach(),
    which creates the object (https://crrev.com/c/7614153). 5. Connect
    SurfaceEmbedHost to SurfaceEmbedHostConnector
    (https://crrev.com/c/7620778); this is where the new
    SurfaceEmbedConnector::Delegate content/public API is used outside of
    content. 6.+ Continue implementation...
    
    This is part of the WebUI Browser Surface Embed project. Design doc:
    https://docs.google.com/document/d/1ZubECnybyRJqfvB4mS4SRwlHiMvITVkHw9NchE0oXPw/edit?usp=sharing
    
    Bug: 479524242
    Change-Id: Idf3e2693b38fff61450c8edc7f729137141e89e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7532621
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1595782}

---
+++ content/public/browser/surface_embed_connector.h
@@ -0,0 +1,75 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_PUBLIC_BROWSER_SURFACE_EMBED_CONNECTOR_H_
+#define CONTENT_PUBLIC_BROWSER_SURFACE_EMBED_CONNECTOR_H_
+
+#include "components/viz/common/surfaces/frame_sink_id.h"
+#include "components/viz/common/surfaces/local_surface_id.h"
+#include "content/common/content_export.h"
+
+namespace blink {
+struct FrameVisualProperties;
+}  // namespace blink
+
+namespace content {
+
+class WebContents;
+
+// This connector embeds a child/inner WebContents within a parent/outer
+// WebContents without using the OOPIF connections (CrossProcessFrameConnector
+// and such). It is created by SurfaceEmbedHost when the child WebContents is
+// attached and subsequently owned by the child WebContents; it is destroyed
+// when the child is detached.
+class CONTENT_EXPORT SurfaceEmbedConnector {
+ public:
+  // The SurfaceEmbedConnector::Delegate class is implemented by
+  // surface_embed::SurfaceEmbedHost.
+  class Delegate {
+   public:
+    // Embeds a surface by its FrameSinkId.
+    virtual void SetFrameSinkId(const viz::FrameSinkId& frame_sink_id) = 0;
+
+    // Called when the child frame initiates a LocalSurfaceId update.
+    // This typically happens when an auto-resized child frame has a new
+    // intrinsic size. However, this has also been observed on non-auto-resized
+    // frames (e.g., www.nytimes.com) due to an unclear triggering condition.
+    virtual void UpdateLocalSurfaceIdFromChild(
+        const viz::LocalSurfaceId& local_surface_id) = 0;
+
+    // Called when the connector is being detached at a SurfaceEmbedHost's
+    // request instead of the SurfaceEmbed plugin's request.
+    virtual void DetachedByHost() = 0;
+
+    // Returns whether this delegate's parent WebContents still has an attached
+    // SurfaceEmbed child WebContents.
+    virtual bool IsAttachedForTesting() const = 0;
+  };
+
+  // Attach a child WebContents to a parent WebContents. This creates a
+  // SurfaceEmbedConnector owned by the child WebContents.
+  static void Attach(WebContents* child_web_contents,
+                     WebContents* parent_web_contents,
+                     SurfaceEmbedConnector::Delegate* delegate);
+
+  // Detach the SurfaceEmbedConnector from the child WebContents. This destroys
+  // the SurfaceEmbedConnector owned by the child WebContents.
+  static void Detach(WebContents* child_web_contents);
+
+  virtual ~SurfaceEmbedConnector() = default;
+
+  virtual Delegate* GetDelegate() = 0;
+
+  // Called by the SecureEmbedHost to synchronize visual properties between the
+  // parent and child WebContents.
+  virtual void OnSynchronizeVisualProperties(
+      const blink::FrameVisualProperties& visual_properties) = 0;
+
+  // Gets the FrameSinkId of the child's view.
+  virtual const viz::FrameSinkId& GetFrameSinkId() const = 0;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_PUBLIC_BROWSER_SURFACE_EMBED_CONNECTOR_H_
