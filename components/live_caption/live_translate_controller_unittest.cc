84f0d5f259d2fa66b7134c70434b69cf022ce1d1
Paula Gómez <paugomez@google.com>
2026-03-07 20:24:28 -0800

    Add unit tests for Live Translate UMA metrics.
    Removed test for profile pref registration. This should resolve tests
    failures for UBSan
    
    Bug: 490264487
    Change-Id: I99786c7cf6e9b99a7fa2b4d6faa14820663bea2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637887
    Commit-Queue: Paula Gómez <paugomez@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Danilo Françoso Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596002}

--- components/live_caption/live_translate_controller_unittest.cc
+++ components/live_caption/live_translate_controller_unittest.cc
@@ -4,15 +4,36 @@
 
 #include "components/live_caption/live_translate_controller.h"
 
+#include <memory>
+
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
+#include "base/test/metrics/histogram_tester.h"
+#include "base/test/mock_callback.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/types/expected.h"
+#include "components/live_caption/features.h"
 #include "components/live_caption/pref_names.h"
 #include "components/live_caption/translation_dispatcher.h"
-#include "components/prefs/testing_pref_service.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace captions {
+
+class MockTranslationDispatcher : public TranslationDispatcher {
+ public:
+  MockTranslationDispatcher() = default;
+  ~MockTranslationDispatcher() override = default;
+
+  MOCK_METHOD(void,
+              GetTranslation,
+              (absl::string_view result,
+               absl::string_view source_language,
+               absl::string_view target_language,
+               TranslateEventCallback callback),
+              (override));
+};
 
 class LiveTranslateControllerTest : public testing::Test {
  public:
@@ -25,26 +46,73 @@ class LiveTranslateControllerTest : public testing::Test {
 
  protected:
   sync_preferences::TestingPrefServiceSyncable prefs_;
-  TestingPrefServiceSimple testing_pref_service_;
 };
 
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_ProfilePrefsAreRegisteredCorrectly \
-  DISABLED_ProfilePrefsAreRegisteredCorrectly
-#else
-#define MAYBE_ProfilePrefsAreRegisteredCorrectly \
-  ProfilePrefsAreRegisteredCorrectly
-#endif
-//  Test that profile prefs are registered correctly.
-TEST_F(LiveTranslateControllerTest, MAYBE_ProfilePrefsAreRegisteredCorrectly) {
-  LiveTranslateController::RegisterProfilePrefs(
-      static_cast<user_prefs::PrefRegistrySyncable*>(
-          testing_pref_service_.registry()));
+TEST_F(LiveTranslateControllerTest,
+       GetTranslationRecordsMetrics_GoogleApi_Success) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndDisableFeature(
+      live_caption::kLiveCaptionOnDeviceTranslation);
 
-  EXPECT_FALSE(testing_pref_service_.GetBoolean(prefs::kLiveTranslateEnabled));
-  EXPECT_EQ(
-      testing_pref_service_.GetString(prefs::kLiveTranslateTargetLanguageCode),
-      speech::kEnglishLocaleNoCountry);
+  base::HistogramTester histogram_tester;
+  auto mock_translation_dispatcher =
+      std::make_unique<testing::StrictMock<MockTranslationDispatcher>>();
+  auto* translation_dispatcher_ptr = mock_translation_dispatcher.get();
+  LiveTranslateController controller(&prefs_,
+                                     std::move(mock_translation_dispatcher));
+
+  // Verify that the controller forwards translation requests to the underlying
+  // dispatcher and successfully records true (success) in the Google API UMA
+  // histogram.
+  EXPECT_CALL(*translation_dispatcher_ptr,
+              GetTranslation("hello", "en", "es", testing::_))
+      .WillOnce([](absl::string_view result, absl::string_view source_language,
+                   absl::string_view target_language,
+                   TranslateEventCallback callback) {
+        std::move(callback).Run(base::ok("hola"));
+      });
+
+  base::MockCallback<TranslateEventCallback> translate_callback;
+  EXPECT_CALL(translate_callback, Run(testing::_)).Times(1);
+
+  controller.GetTranslation("hello", "en", "es", translate_callback.Get());
+
+  histogram_tester.ExpectUniqueSample(
+      "Accessibility.LiveTranslate.GoogleApiTranslation.Result",
+      /*sample=*/true, /*expected_bucket_count=*/1);
+}
+
+TEST_F(LiveTranslateControllerTest,
+       GetTranslationRecordsMetrics_OnDevice_Failure) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitAndEnableFeature(
+      live_caption::kLiveCaptionOnDeviceTranslation);
+
+  base::HistogramTester histogram_tester;
+  auto mock_translation_dispatcher =
+      std::make_unique<testing::StrictMock<MockTranslationDispatcher>>();
+  auto* translation_dispatcher_ptr = mock_translation_dispatcher.get();
+  LiveTranslateController controller(&prefs_,
+                                     std::move(mock_translation_dispatcher));
+
+  // Controller calls dispatcher and successfully records false (failure) in the
+  // On-Device UMA histogram.
+  EXPECT_CALL(*translation_dispatcher_ptr,
+              GetTranslation("hello", "en", "es", testing::_))
+      .WillOnce([](absl::string_view result, absl::string_view source_language,
+                   absl::string_view target_language,
+                   TranslateEventCallback callback) {
+        std::move(callback).Run(base::unexpected("translation failed"));
+      });
+
+  base::MockCallback<TranslateEventCallback> translate_callback;
+  EXPECT_CALL(translate_callback, Run(testing::_)).Times(1);
+
+  controller.GetTranslation("hello", "en", "es", translate_callback.Get());
+
+  histogram_tester.ExpectUniqueSample(
+      "Accessibility.LiveTranslate.OnDeviceTranslation.Result",
+      /*sample=*/false, /*expected_bucket_count=*/1);
 }
 
 }  // namespace captions
