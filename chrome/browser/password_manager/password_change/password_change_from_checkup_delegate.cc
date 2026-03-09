0784373ccbb6922a9ff4c2c483c196c5c686d353
Fiorella <fiorellab@google.com>
2026-03-09 10:35:09 -0700

    Open new tab for password change from Checkup and use it to invoke GiC
    With this CL, when an automated password change flow is triggered from
    Password Checkup, it first opens the target credential's site in a new
    tab. This new tab is then used as additional context to invoke GiC
    (Gemini in Chrome) so the actor can use it to start the flow instead of
    attempting to create a new tab for that.
    
    Bug: 485620841
    Change-Id: I7e1129b3e95ed564fee2b52904652195a50ad742
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7628084
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Fiorella Barrientos Villalta <fiorellab@google.com>
    Cr-Commit-Position: refs/heads/main@{#1596442}

--- chrome/browser/password_manager/password_change/password_change_from_checkup_delegate.cc
+++ chrome/browser/password_manager/password_change/password_change_from_checkup_delegate.cc
@@ -8,14 +8,20 @@
 
 #include "base/strings/strcat.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/actor/actor_keyed_service.h"
 #include "chrome/browser/glic/public/glic_invoke_options.h"
 #include "chrome/browser/glic/public/glic_keyed_service.h"
 #include "chrome/browser/glic/public/glic_passkeys.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "components/password_manager/core/browser/ui/credential_ui_entry.h"
+#include "components/sessions/content/session_tab_helper.h"
 #include "components/tabs/public/tab_interface.h"
+#include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/base/page_transition_types.h"
+#include "ui/base/window_open_disposition.h"
 #include "url/gurl.h"
 
 namespace {
@@ -23,9 +29,9 @@ namespace {
 std::string GetPromptToReachForm(std::string_view domain,
                                  std::string_view username) {
   return base::StrCat(
-      {"I want to change my password in ", domain,
-       ". Help me login to this site and reach the change password form. "
-       "You can login using ",
+      {"I want to change my password for ", domain,
+       ". Please help me complete the password change flow starting from "
+       "this current page. You can login using ",
        username,
        ". The task is finished when the change password form is visible."});
 }
@@ -44,30 +50,65 @@ void PasswordChangeFromCheckupDelegate::StartPasswordChangeFlow(
   if (!web_contents) {
     return;
   }
+  originator_ = std::move(web_contents);
+
+  actor::ActorKeyedService* actor_service = actor::ActorKeyedService::Get(
+      Profile::FromBrowserContext(originator_->GetBrowserContext()));
+  if (actor_service && actor_service->GetActiveTasksCount() > 0) {
+    return;
+  }
 
   tabs::TabInterface* tab_interface =
-      tabs::TabInterface::MaybeGetFromContents(web_contents.get());
+      tabs::TabInterface::MaybeGetFromContents(originator_.get());
   if (!tab_interface) {
     return;
   }
 
-  originator_ = std::move(web_contents);
   glic::GlicKeyedService* glic_service = GetGlicService();
   if (!glic_service) {
     return;
   }
 
-  // TODO(crbug.com/485620841): Handle Android passwords.
-  std::string site_domain(credential.GetURL().host());
+  // TODO(crbug.com/485620841): Handle non-web URLs for Android passwords.
+  const GURL& credential_url = credential.GetURL();
+  std::string site_domain(credential_url.host());
+
   // TODO(crbug.com/485620841): Replace with internal prompt.
   std::string prompt =
       GetPromptToReachForm(site_domain, base::UTF16ToUTF8(credential.username));
+  content::OpenURLParams open_url_params(
+      credential_url.GetWithEmptyPath(), content::Referrer(),
+      WindowOpenDisposition::NEW_FOREGROUND_TAB,
+      ui::PAGE_TRANSITION_AUTO_TOPLEVEL,
+      /*is_renderer_initiated=*/false);
+
+  content::WebContents* new_contents =
+      originator_->OpenURL(open_url_params, /*navigation_handle_callback=*/{});
+
+  if (!new_contents) {
+    return;
+  }
 
   glic::GlicInvokeOptions options(glic::mojom::InvocationSource::kSharedTab);
   options.prompts.push_back(std::move(prompt));
+  options.additional_context = glic::mojom::AdditionalContext::New();
+
+  sessions::SessionTabHelper* session_tab_helper =
+      sessions::SessionTabHelper::FromWebContents(new_contents);
+  if (session_tab_helper) {
+    options.additional_context->tab_id = session_tab_helper->session_id().id();
+  }
+
+  tabs::TabInterface* new_tab_interface =
+      tabs::TabInterface::MaybeGetFromContents(new_contents);
+
+  if (!new_tab_interface) {
+    return;
+  }
+
   glic_service->InvokeWithAutoSubmit(
-      glic::InvokeWithAutoSubmitPasskeyProvider::GetPassKey(), tab_interface,
-      std::move(options));
+      glic::InvokeWithAutoSubmitPasskeyProvider::GetPassKey(),
+      new_tab_interface, std::move(options));
 }
 
 glic::GlicKeyedService* PasswordChangeFromCheckupDelegate::GetGlicService() {
