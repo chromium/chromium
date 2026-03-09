266446f6bbbda1182dc46fd9cbe09b6e0281418b
Charlie Reis <creis@chromium.org>
2026-03-09 10:55:25 -0700

    [M146] Validate ResourceRequestBody in CreateNewWindowParams.
    (cherry picked from commit b496550e39c5c1752d504a684ebc4d88b4009ed3)
    
    Bug: 487768779
    Change-Id: I8c1c6f6f6ab4584decc94fe590bf335c699c14fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7635726
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1594735}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7647963
    Cr-Commit-Position: refs/branch-heads/7680@{#2212}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -3464,6 +3464,8 @@ class CONTENT_EXPORT RenderFrameHostImpl
                            AttemptDuplicateRenderWidgetHost);
   FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
                            BindToWebUIFromWebViaMojo);
+  FRIEND_TEST_ALL_PREFIXES(SecurityExploitBrowserTest,
+                           CreateNewWindowWithInaccessibleFile);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
                            RenderViewHostIsNotReusedAfterDelayedUnloadACK);
   FRIEND_TEST_ALL_PREFIXES(SitePerProcessBrowserTest,
