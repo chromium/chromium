a44592c85e49cd5a5c5e85207768b8dc78ed8d84
Andrew Paseltiner <apaseltiner@chromium.org>
2026-03-09 06:39:13 -0700

    Simplify span construction in single_install_event_log_fuzzer
    Bug: 435317390
    Change-Id: I4e7e6a712296df56673d4a35a3ad6232ab96861a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7646832
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596292}

--- chrome/browser/ash/policy/reporting/single_install_event_log_fuzzer.cc
+++ chrome/browser/ash/policy/reporting/single_install_event_log_fuzzer.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <stddef.h>
 #include <stdint.h>
 #include <unistd.h>
 
@@ -10,41 +9,39 @@
 
 #include "base/check.h"
 #include "base/check_op.h"
-#include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/files/scoped_file.h"
 #include "base/logging.h"
 #include "base/posix/eintr_wrapper.h"
 #include "chrome/browser/ash/policy/reporting/single_arc_app_install_event_log.h"
 #include "chrome/browser/ash/policy/reporting/single_extension_install_event_log.h"
+#include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
 namespace policy {
 
 namespace {
 
-base::File CreatePipeFileWithContents(const uint8_t* data, size_t size) {
+base::File CreatePipeFileWithContents(base::span<const uint8_t> data) {
   int pipefd[2];
   CHECK_EQ(HANDLE_EINTR(pipe(pipefd)), 0);
   base::File pipe_read_end = base::File(base::ScopedFD(pipefd[0]));
   base::File pipe_write_end = base::File(base::ScopedFD(pipefd[1]));
-  if (size) {
-    UNSAFE_TODO(CHECK(pipe_write_end.WriteAtCurrentPos(
-        reinterpret_cast<const char*>(data), size)));
-  }
+  CHECK(pipe_write_end.WriteAtCurrentPosAndCheck(data));
   return pipe_read_end;
 }
 
 }  // namespace
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(const base::span<const uint8_t> data) {
   {
-    base::File file = CreatePipeFileWithContents(data, size);
+    base::File file = CreatePipeFileWithContents(data);
     std::unique_ptr<SingleArcAppInstallEventLog> log;
     SingleArcAppInstallEventLog::Load(&file, &log);
   }
 
   {
-    base::File file = CreatePipeFileWithContents(data, size);
+    base::File file = CreatePipeFileWithContents(data);
     std::unique_ptr<SingleExtensionInstallEventLog> log;
     SingleExtensionInstallEventLog::Load(&file, &log);
   }
