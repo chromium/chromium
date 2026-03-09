8f455d15b3c304196f36a9dc4a23c8f4d8793143
Dana Fried <dfried@chromium.org>
2026-03-09 11:52:10 -0700

    [Chrome Next] Add pixel tests for toolbar-height side panel
    This adds pixel tests at all of the corners and borders that exist when
    THSP is open, with and without vertical tabstrip.
    
    Bug: 489794761
    Change-Id: Id62dfde2fcb68782514d6e7f39450b5ac74b7adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7636419
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596485}

--- chrome/browser/ui/views/frame/layout/browser_view_tabbed_layout_impl_interactive_uitest.cc
+++ chrome/browser/ui/views/frame/layout/browser_view_tabbed_layout_impl_interactive_uitest.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <memory>
+
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/test/scoped_feature_list.h"
@@ -10,20 +12,28 @@
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/layout_constants.h"
+#include "chrome/browser/ui/side_panel/side_panel_entry.h"
+#include "chrome/browser/ui/side_panel/side_panel_registry.h"
+#include "chrome/browser/ui/side_panel/side_panel_ui.h"
 #include "chrome/browser/ui/tabs/features.h"
+#include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/views/frame/layout/browser_view_layout.h"
+#include "chrome/browser/ui/views/frame/layout/browser_view_tabbed_layout_impl.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
 #include "components/bookmarks/common/bookmark_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/test/browser_test.h"
+#include "ui/base/interaction/element_identifier.h"
 #include "ui/base/interaction/element_specifier.h"
 #include "ui/base/interaction/element_tracker.h"
 #include "ui/base/interaction/interactive_test_definitions.h"
 #include "ui/gfx/animation/animation.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/views/layout/fill_layout.h"
+#include "ui/views/view_class_properties.h"
 #include "url/gurl.h"
 
 class BrowserViewTabbedLayoutImplUiTest : public InteractiveBrowserTest {
@@ -33,6 +43,8 @@ class BrowserViewTabbedLayoutImplUiTest : public InteractiveBrowserTest {
   BrowserViewTabbedLayoutImplUiTest() = default;
 
   ~BrowserViewTabbedLayoutImplUiTest() override = default;
+
+  DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kSidePanelTestElement);
 
   void SetUp() override {
     feature_list_.InitWithFeatures({tabs::kVerticalTabs}, {});
@@ -49,6 +61,39 @@ class BrowserViewTabbedLayoutImplUiTest : public InteractiveBrowserTest {
   void SetUpOnMainThread() override {
     InteractiveBrowserTest::SetUpOnMainThread();
     chrome::AddTabAt(browser(), GURL("about:blank"), -1, true);
+  }
+
+  auto ReplaceAndShowSidePanel(SidePanelEntry::PanelType type) {
+    return Steps(
+        Do([this, type]() {
+          // Note: there is a registry at browser level and one per tab; we want
+          // the tab-specific one.
+          auto* const registry = browser()
+                                     ->GetActiveTabInterface()
+                                     ->GetTabFeatures()
+                                     ->side_panel_registry();
+          const auto key =
+              SidePanelEntry::Key(SidePanelEntry::Id::kCustomizeChrome);
+          CHECK(!registry->GetEntryForKey(key) || registry->Deregister(key));
+          CHECK(registry->Register(std::make_unique<SidePanelEntry>(
+              type, key, base::BindRepeating([](SidePanelEntryScope&) {
+                auto content_view = std::make_unique<views::View>();
+                content_view->SetLayoutManager(
+                    std::make_unique<views::FillLayout>());
+                auto label =
+                    std::make_unique<views::Label>(u"Side Panel Ready");
+                label->SetProperty(views::kElementIdentifierKey,
+                                   kSidePanelTestElement);
+                content_view->AddChildView(std::move(label));
+                return content_view;
+              }),
+              /*default_content_width_callback=*/base::NullCallback())));
+          browser()->browser_window_features()->side_panel_ui()->Show(
+              SidePanelEntry::Id::kCustomizeChrome);
+        }),
+        WaitForShow(kSidePanelElementId),
+        // Wait for the indicator to show.
+        WaitForShow(kSidePanelTestElement));
   }
 
   static int GetCornerScreenshotSize() {
@@ -187,6 +232,9 @@ class BrowserViewTabbedLayoutImplUiTest : public InteractiveBrowserTest {
   base::test::ScopedFeatureList feature_list_;
 };
 
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(BrowserViewTabbedLayoutImplUiTest,
+                                      kSidePanelTestElement);
+
 IN_PROC_BROWSER_TEST_F(BrowserViewTabbedLayoutImplUiTest,
                        HorizontalTabsFirstTabActive) {
   RunTestSequence(
@@ -280,4 +328,78 @@ IN_PROC_BROWSER_TEST_F(BrowserViewTabbedLayoutImplUiTest,
           "bottom_corner", 5),
       ScreenshotLeft(ToolbarView::kToolbarElementId, "toolbar_leading", 3),
       ScreenshotRight(ToolbarView::kToolbarElementId, "toolbar_trailing", 3));
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserViewTabbedLayoutImplUiTest,
+                       HorizontalTabsWithToolbarHeightSidePanel) {
+  RunTestSequence(
+      SelectTab(kBrowserViewElementId, 0),
+      ReplaceAndShowSidePanel(SidePanelEntry::PanelType::kToolbar),
+      SetOnIncompatibleAction(OnIncompatibleAction::kSkipTest,
+                              "Test is screenshot-only."),
+      ScreenshotLeft(kTabStripRegionElementId, "tabstrip_leading", 3),
+      ScreenshotRight(kTabStripRegionElementId, "tabstrip_trailing", 3),
+      ScreenshotLeft(ToolbarView::kToolbarElementId, "toolbar_leading", 3),
+      ScreenshotRight(ToolbarView::kToolbarElementId, "toolbar_trailing", 3),
+      ScreenshotUpperLeft(BrowserViewLayoutViews::kShadowOverlayElementId,
+                          "shadow_upper_leading", 3),
+      ScreenshotUpperRight(BrowserViewLayoutViews::kShadowOverlayElementId,
+                           "shadow_upper_trailing", 3),
+      ScreenshotLowerRight(BrowserViewLayoutViews::kShadowOverlayElementId,
+                           "shadow_lower_trailing", 3),
+      ScreenshotLowerLeft(BrowserViewLayoutViews::kShadowOverlayElementId,
+                          "shadow_lower_leading", 3),
+      ScreenshotUpperLeft(
+          BrowserViewLayoutViews::kMainBackgroundRegionElementId,
+          "main_upper_leading", 3),
+      ScreenshotUpperRight(
+          BrowserViewLayoutViews::kMainBackgroundRegionElementId,
+          "main_upper_trailing", 3),
+      ScreenshotLowerRight(
+          BrowserViewLayoutViews::kMainBackgroundRegionElementId,
+          "main_lower_trailing", 3),
+      ScreenshotLowerLeft(
+          BrowserViewLayoutViews::kMainBackgroundRegionElementId,
+          "main_lower_leading", 3),
+      ScreenshotTop(kSidePanelElementId, "side_panel_top", 8),
+      ScreenshotBottom(kSidePanelElementId, "side_panel_bottom", 8));
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserViewTabbedLayoutImplUiTest,
+                       VerticalTabsWithToolbarHeightSidePanel) {
+  tabs::VerticalTabStripStateController::From(browser())
+      ->SetVerticalTabsEnabled(true);
+  RunScheduledLayouts();
+  RunTestSequence(
+      WaitForShow(kVerticalTabStripRegionElementId),
+      SelectTab(kBrowserViewElementId, 0),
+      ReplaceAndShowSidePanel(SidePanelEntry::PanelType::kToolbar),
+      SetOnIncompatibleAction(OnIncompatibleAction::kSkipTest,
+                              "Test is screenshot-only."),
+      ScreenshotTop(kVerticalTabStripRegionElementId, "tabstrip_top", 3),
+      ScreenshotBottom(kVerticalTabStripRegionElementId, "tabstrip_bottom", 3),
+      ScreenshotLeft(ToolbarView::kToolbarElementId, "toolbar_leading", 3),
+      ScreenshotRight(ToolbarView::kToolbarElementId, "toolbar_trailing", 3),
+      ScreenshotUpperLeft(BrowserViewLayoutViews::kShadowOverlayElementId,
+                          "shadow_upper_leading", 3),
+      ScreenshotUpperRight(BrowserViewLayoutViews::kShadowOverlayElementId,
+                           "shadow_upper_trailing", 3),
+      ScreenshotLowerRight(BrowserViewLayoutViews::kShadowOverlayElementId,
+                           "shadow_lower_trailing", 3),
+      ScreenshotLowerLeft(BrowserViewLayoutViews::kShadowOverlayElementId,
+                          "shadow_lower_leading", 3),
+      ScreenshotUpperLeft(
+          BrowserViewLayoutViews::kMainBackgroundRegionElementId,
+          "main_upper_leading", 3),
+      ScreenshotUpperRight(
+          BrowserViewLayoutViews::kMainBackgroundRegionElementId,
+          "main_upper_trailing", 3),
+      ScreenshotLowerRight(
+          BrowserViewLayoutViews::kMainBackgroundRegionElementId,
+          "main_lower_trailing", 3),
+      ScreenshotLowerLeft(
+          BrowserViewLayoutViews::kMainBackgroundRegionElementId,
+          "main_lower_leading", 3),
+      ScreenshotTop(kSidePanelElementId, "side_panel_top", 8),
+      ScreenshotBottom(kSidePanelElementId, "side_panel_bottom", 8));
 }
