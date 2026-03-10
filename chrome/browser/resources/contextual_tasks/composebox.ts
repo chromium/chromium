20e796b06713cfb5ba48e072297075ae54e8a9da
Duncan Mercer <mercerd@google.com>
2026-03-10 12:07:08 -0700

    [146] [Contextual Tasks] Fix clip-rect height not updating on side panel open
    Original change's description:
    > [Contextual Tasks] Fix clip-rect height not updating on side panel open
    >
    > Due to an issue where the resize observer was being replaced when the
    > tooltip appears, the clip-rect was not updating on height changes. This
    > fixes that by seperating the two observers so they don't conflict.
    >
    > Fixed: b:490218295
    > Change-Id: I37f0af185d281a0e23fb21e72576d3dc84f7c733
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7642930
    > Reviewed-by: Diana OuYang <dianaou@google.com>
    > Commit-Queue: Diana OuYang <dianaou@google.com>
    > Auto-Submit: Duncan Mercer <mercerd@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1595608}
    
    Bug: 491222710
    Change-Id: I37f0af185d281a0e23fb21e72576d3dc84f7c733
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7652160
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7680@{#2281}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- chrome/browser/resources/contextual_tasks/composebox.ts
+++ chrome/browser/resources/contextual_tasks/composebox.ts
@@ -175,7 +175,11 @@ export class ContextualTasksComposeboxElement extends I18nMixinLit
   private isOnboardingTooltipDismissCountBelowCap_: boolean =
       loadTimeData.getBoolean('isOnboardingTooltipDismissCountBelowCap');
   private userDismissedTooltip_: boolean = false;
+  // Tracks the resize of the composebox to provide height updates.
   private resizeObserver_: ResizeObserver|null = null;
+  // Tracks the resize of the composebox and the auto added chip to provide
+  // position updates to the tooltip.
+  private tooltipResizeObserver_: ResizeObserver|null = null;
   private tooltipImpressionTimer_: number|null = null;
   private readonly tooltipImpressionDelay_: number =
       loadTimeData.getInteger('composeboxShowOnboardingTooltipImpressionDelay');
@@ -278,7 +282,7 @@ export class ContextualTasksComposeboxElement extends I18nMixinLit
   override disconnectedCallback() {
     super.disconnectedCallback();
     this.clearTooltipImpressionTimer_();
-    this.stopObservingResize_();
+    this.stopObservingTooltipResize_();
     if (this.resizeObserver_) {
       this.resizeObserver_.disconnect();
       this.resizeObserver_ = null;
@@ -340,7 +344,7 @@ export class ContextualTasksComposeboxElement extends I18nMixinLit
         !this.$.composebox.getHasAutomaticActiveTabChipToken()) {
       tooltip.hide();
       this.onboardingTooltipIsVisible_ = false;
-      this.stopObservingResize_();
+      this.stopObservingTooltipResize_();
       // Clear the timer if the tooltip is hidden. This will prevent it being
       // count as an impression if the chip only showed up briefly.
       this.clearTooltipImpressionTimer_();
@@ -354,7 +358,7 @@ export class ContextualTasksComposeboxElement extends I18nMixinLit
         tooltip.updatePosition();
       } else if (this.shouldShowOnboardingTooltip()) {
         tooltip.show();
-        this.startObservingResize_(target);
+        this.startObservingTooltipResize_(target);
         this.onboardingTooltipIsVisible_ = true;
 
         // Start the impression timer if the tooltip is newly shown.
@@ -379,7 +383,7 @@ export class ContextualTasksComposeboxElement extends I18nMixinLit
   protected onTooltipDismissed_() {
     this.userDismissedTooltip_ = true;
     this.onboardingTooltipIsVisible_ = false;
-    this.stopObservingResize_();
+    this.stopObservingTooltipResize_();
     this.clearTooltipImpressionTimer_();
   }
 
@@ -455,26 +459,26 @@ export class ContextualTasksComposeboxElement extends I18nMixinLit
     this.pageHandler_.handleFileUpload(false);
   }
 
-  private startObservingResize_(target: Element|null) {
-    if (this.resizeObserver_) {
-      this.resizeObserver_.disconnect();
+  private startObservingTooltipResize_(target: Element|null) {
+    if (this.tooltipResizeObserver_) {
+      this.tooltipResizeObserver_.disconnect();
     }
-    this.resizeObserver_ = new ResizeObserver(() => {
+    this.tooltipResizeObserver_ = new ResizeObserver(() => {
       const tooltip = this.$.onboardingTooltip;
       if (tooltip && tooltip.target) {
         tooltip.updatePosition();
       }
     });
-    this.resizeObserver_.observe(this.$.composebox);
+    this.tooltipResizeObserver_.observe(this.$.composebox);
     if (target) {
-      this.resizeObserver_.observe(target);
+      this.tooltipResizeObserver_.observe(target);
     }
   }
 
-  private stopObservingResize_() {
-    if (this.resizeObserver_) {
-      this.resizeObserver_.disconnect();
-      this.resizeObserver_ = null;
+  private stopObservingTooltipResize_() {
+    if (this.tooltipResizeObserver_) {
+      this.tooltipResizeObserver_.disconnect();
+      this.tooltipResizeObserver_ = null;
     }
   }
 
@@ -515,6 +519,14 @@ export class ContextualTasksComposeboxElement extends I18nMixinLit
 
   updateTooltipVisibilityForTesting() {
     this.updateTooltipVisibility_();
+  }
+
+  get resizeObserverForTesting() {
+    return this.resizeObserver_;
+  }
+
+  get tooltipResizeObserverForTesting() {
+    return this.tooltipResizeObserver_;
   }
 }
 
