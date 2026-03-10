ad2c92450c43290e8130b584663025ec8849d377
Oleksandr Tara <otara@google.com>
2026-03-10 00:30:47 -0700

    [Actor Login M2] Add a confirmation dialog for removing login permissions.
    The dialog is a copy of chrome/browser/resources/password_manager/dialogs/remove_actor_login_permission_dialog.html with minor adjustments. The existing dialog is still needed in the password settings but once M2 is launched, it will be removed.
    
    Implemented UI: screen/AG3dPCTHNSuw9Q6
    
    Screen recording of the full flow: http://shortn/_cxOoAEVR3B
    
    Bug: b:481214358
    Change-Id: Ie13baf706a4919d21d994738fe3ee6cdb6614156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7598784
    Commit-Queue: Oleksandr Tara <otara@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596875}

--- chrome/browser/resources/settings/glic_page/glic_login_permissions_page.ts
+++ chrome/browser/resources/settings/glic_page/glic_login_permissions_page.ts
@@ -6,10 +6,15 @@ import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import '../settings_page/settings_subpage.js';
+import '../simple_confirmation_dialog.js';
 import '../site_favicon.js';
 
+import {assert} from '//resources/js/assert.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import type {DomRepeatEvent} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import type {SettingsSimpleConfirmationDialogElement} from '../simple_confirmation_dialog.js';
 
 import {getTemplate} from './glic_login_permissions_page.html.js';
 
@@ -37,10 +42,36 @@ export class SettingsGlicLoginPermissionsPageElement extends
         type: Array,
         value: () => [],
       },
+
+      selectedPermissionToRemove_: {
+        type: Object,
+        value: null,
+      },
     };
   }
 
   declare private actorLoginPermissions_: LoginPermission[];
+  declare private selectedPermissionToRemove_: LoginPermission|null;
+
+  private onRemoveActorLoginPermissionClick_(
+      e: DomRepeatEvent<LoginPermission>) {
+    this.selectedPermissionToRemove_ = e.model.item;
+  }
+
+  private onRemoveDialogClose_() {
+    const dialog =
+        this.shadowRoot!.querySelector<SettingsSimpleConfirmationDialogElement>(
+            'settings-simple-confirmation-dialog');
+    assert(dialog);
+    if (dialog.wasConfirmed()) {
+      // TODO(crbug.com/481214101): Implement remove logic.
+    }
+    this.selectedPermissionToRemove_ = null;
+  }
+
+  private getRemoveDialogDescription_(url: string): string {
+    return this.i18n('glicRemoveActorLoginDialogDescription', url);
+  }
 }
 
 declare global {
--- chrome/test/data/webui/settings/glic_login_permissions_page_test.ts
+++ chrome/test/data/webui/settings/glic_login_permissions_page_test.ts
@@ -3,18 +3,21 @@
 // found in the LICENSE file.
 
 import 'chrome://settings/settings.js';
-import 'chrome://settings/settings.js';
 
-import type {SettingsGlicLoginPermissionsPageElement} from 'chrome://settings/lazy_load.js';
-import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import type {SettingsGlicLoginPermissionsPageElement, SettingsSimpleConfirmationDialogElement} from 'chrome://settings/lazy_load.js';
+import {assertEquals, assertNull, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {eventToPromise} from 'chrome://webui-test/test_util.js';
 
 suite('GlicLoginPermissionsPage', function() {
   let page: SettingsGlicLoginPermissionsPageElement;
 
+  const origin = 'example.com';
+
   setup(async function() {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     page = document.createElement('settings-glic-login-permissions-page');
+    page.set('actorLoginPermissions_', [{url: origin, username: 'user'}]);
     document.body.appendChild(page);
     await flushTasks();
   });
@@ -24,6 +27,36 @@ suite('GlicLoginPermissionsPage', function() {
         page.shadowRoot!.querySelector('#actorLoginPermissionsList');
     assertTrue(!!loginPermissionsList);
     assertEquals(
-        0, page.shadowRoot!.querySelectorAll('.permission-item').length);
+        1, page.shadowRoot!.querySelectorAll('.permission-item').length);
+  });
+
+  test('remove dialog is shown', async () => {
+    // Check that the remove dialog is not shown.
+    assertNull(
+        page.shadowRoot!.querySelector('settings-simple-confirmation-dialog'));
+
+    // Click the remove button.
+    const removeButton =
+        page.shadowRoot!.querySelector<HTMLElement>('.icon-clear');
+    assertTrue(!!removeButton);
+    removeButton.click();
+    await flushTasks();
+
+    // Check that the remove dialog is shown.
+    const dialog =
+        page.shadowRoot!.querySelector<SettingsSimpleConfirmationDialogElement>(
+            'settings-simple-confirmation-dialog');
+    assertTrue(!!dialog);
+    assertTrue(dialog.bodyText.includes(origin));
+
+    // Close the dialog.
+    const closePromise = eventToPromise('close', dialog);
+    dialog.$.cancel.click();
+    await closePromise;
+    await flushTasks();
+
+    // Check that the remove dialog is not shown.
+    assertNull(
+        page.shadowRoot!.querySelector('settings-simple-confirmation-dialog'));
   });
 });
