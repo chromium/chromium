1797f86096dc252187f650855dcd839421b5ad1a
Marc Treib <treib@chromium.org>
2026-03-10 03:01:26 -0700

    Revert "mojo_unittests: Work around Checked Span"
    This reverts commit b0c3bb6f7d5efc47b6942cc04403ffc01fdff637.
    
    Reason for revert: Seems to have broken these tests (at least) on Win10 x64 dbg, see https://ci.chromium.org/ui/p/chromium/builders/ci/Win10%20Tests%20x64%20(dbg)/51794/overview
    
    Original change's description:
    > mojo_unittests: Work around Checked Span
    >
    > These artificially overlarge spans set off Checked Span (when enabled).
    > Since they are deliberately minted with the wrong size, hinge the sizes
    > (both of backing storage and the span) on PartitionAlloc's buildflags.
    >
    > Bug: 484171909
    > Change-Id: Ie1fce6bea91b4c7e7f5ac8fa8a5132cdfd4d40d3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7637373
    > Reviewed-by: Fred Shih <ffred@chromium.org>
    > Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1596860}
    
    Bug: 484171909
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I0560366e9f856e308bb0dc577de4f9beb25c13b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651854
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Owners-Override: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596933}

--- mojo/public/cpp/system/tests/data_pipe_unittest.cc
+++ mojo/public/cpp/system/tests/data_pipe_unittest.cc
@@ -9,12 +9,9 @@
 
 #include "base/compiler_specific.h"
 #include "base/containers/span.h"
-#include "base/sanitizer_buildflags.h"
 #include "base/test/task_environment.h"
 #include "mojo/public/c/system/types.h"
 #include "mojo/public/cpp/system/data_pipe.h"
-#include "partition_alloc/buildflags.h"
-#include "partition_alloc/partition_alloc_constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace mojo {
@@ -62,75 +59,6 @@ TEST(DataPipeCppTest, EndWriteDataErrorWhenSizeTooBig) {
   }
 }
 
-// Returns a buffer size that will avoid crashing.
-//
-// Considerations:
-// *  When used in PartitionAlloc-Everywhere, PartitionAlloc is
-//    configured to crash on rather than return `nullptr` for huge
-//    allocation sizes. (PartitionAlloc-Everywhere is widely used ---
-//    but big exemptions exist, e.g. sanitizer builds.)
-// *  When Checked Span is enabled, you are allowed an unspecified slack
-//    in constructing a `base::span` (which _could be zero_). You cannot
-//    generally rely on this slack space to construct bogus spans.
-// *  Canonically, you can opt a span out of checking by passing
-//    `base::unchecked` as the first argument to the unsafe constructor,
-//    but this is unworkable here. Mojo explodes the span into a pointer
-//    and size (`saturated_cast`ing it to `uint32_t`) and reconstitutes
-//    the span after passing through the C API layer. We don't want to
-//    leave this latter span unchecked.
-struct SubtleSizes {
-  static constexpr size_t ForBackingStorage() {
-#if PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
-
-#if BUILDFLAG(IS_ANDROID) && !PA_BUILDFLAG(HAS_64_BIT_POINTERS)
-    // x86 Android simply fails with garden-variety OOM.
-    // Substitute an arbitrarily chosen allocation size.
-    return 1u << 20;
-#else
-    // Account for PartitionAlloc implementation detail: there are
-    // extras added (imposing an invisible tax). Actually attempting
-    // to allocate `MaxDirectMapped()` will fail.
-    return partition_alloc::MaxDirectMapped() - (1u << 14);
-#endif  // BUILDFLAG(IS_ANDROID) && PA_BUILDFLAG(HAS_64_BIT_POINTERS)
-
-#elif BUILDFLAG(USING_SANITIZER)
-
-    // ASan (at least from my x64 Linux machine) reports a max
-    // allocation size of 0x10000000000. Attempting to allocate even
-    // half this causes the backing allocator to OOM.
-    //
-    // Given that it's a sanitizer-ish build, be stingy and let the
-    // sanitizer catch any potential overruns (which there shouldn't
-    // be!).
-    return 128u;
-
-#else
-
-    // `vector` has its own limits and calls `__throw_length_error()` if
-    // we try to allocate the max `size_t`.
-    return std::numeric_limits<std::vector<uint8_t>::difference_type>::max();
-
-#endif
-  }
-
-  static constexpr size_t ForHugeSpan() {
-    // PartitionAlloc has other uses beyond PartitionAlloc-Everywhere,
-    // so the PartitionAlloc-Everywhere buildflag is logically separate
-    // from that of Checked Span. However, the `std::vector`s in these
-    // test cases are specifically allocated with
-    // PartitionAlloc-Everywhere (when build support is present), so
-    // both these conditions must be true for the "do the sizes match"
-    // to be a concern.
-#if PA_BUILDFLAG(CHECKED_SPAN) && PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
-    return ForBackingStorage();
-#else
-    // Since this is not allocated, PA can't choke on it; since we are
-    // not building with Checked Span, we doubly won't choke on this.
-    return std::numeric_limits<size_t>::max();
-#endif
-  }
-};
-
 // Test that `WriteData` continues to work well when `*num_bytes` is greater
 // than `std::numeric_limits<uint32.::max()`.
 TEST(DataPipeCppTest, WriteDataGracefullyHandlesBigSize) {
@@ -154,10 +82,10 @@ TEST(DataPipeCppTest, WriteDataGracefullyHandlesBigSize) {
   // TODO(lukasza): Avoid UB risk by testing with a `std::vector` that contains
   // `std::numeric_limits<uint32_t>::max() + 123` bytes.  (Once our allocator
   // supports such big vectors.)
-  std::vector<uint8_t> kData(SubtleSizes::ForBackingStorage(), 0x00);
+  std::vector<uint8_t> kData(1024, 0x00);
   base::span<const uint8_t> big_span = UNSAFE_BUFFERS(base::span<const uint8_t>(
       kData.data(),
-      SubtleSizes::ForHugeSpan()));  // subtle - see above why ok
+      std::numeric_limits<size_t>::max()));  // subtle - see above why ok
   size_t bytes_written = 0;
   ASSERT_EQ(producer_handle->WriteData(
                 big_span, MOJO_BEGIN_WRITE_DATA_FLAG_NONE, bytes_written),
@@ -188,9 +116,9 @@ TEST(DataPipeCppTest, ReadDataGracefullyHandlesBigSize) {
   // TODO(lukasza): Avoid UB risk by testing with a `std::vector` that can
   // accommodate `std::numeric_limits<uint32_t>::max() + 123` bytes.  (Once our
   // allocator supports such big vectors.)
-  std::vector<uint8_t> read_buffer(SubtleSizes::ForBackingStorage());
-  base::span<uint8_t> big_span = UNSAFE_BUFFERS(
-      base::span<uint8_t>(read_buffer.data(), SubtleSizes::ForHugeSpan()));
+  std::vector<uint8_t> read_buffer(100);
+  base::span<uint8_t> big_span = UNSAFE_BUFFERS(base::span<uint8_t>(
+      read_buffer.data(), std::numeric_limits<size_t>::max()));
   size_t actually_read_bytes;
   ASSERT_EQ(consumer_handle->ReadData(MOJO_READ_DATA_FLAG_NONE, big_span,
                                       actually_read_bytes),
