17a681c930125dd15b4f645c540bcf508a9208b7
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-09 17:11:38 -0700

    [Desktop Android] Clean up Android's TabsEventRouterPlatformDelegate
    Clean up the android implementation of TabsEventRouterPlatformDelegate.
    * TabRemoved() is not necessary. The platform-agnostic TabsEventRouter
      monitors this already.
    * With that, TabsEventRouterPlatformDelegate no longer needs to be a
      TabModelObserver.
    * Remove lots of unused includes.
    * Remove declared-but-not-defined method DispatchEvent().
    
    Bug: 473593117
    Change-Id: I702628c5df224332d05134e55ad828de5f1f6b16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7649920
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1596715}

--- chrome/browser/extensions/api/tabs/tabs_event_router_platform_delegate_android.cc
+++ chrome/browser/extensions/api/tabs/tabs_event_router_platform_delegate_android.cc
@@ -4,17 +4,10 @@
 
 #include "chrome/browser/extensions/api/tabs/tabs_event_router_platform_delegate_android.h"
 
-#include "base/debug/dump_without_crashing.h"
-#include "base/notimplemented.h"
-#include "chrome/browser/android/tab_android.h"
-#include "chrome/browser/extensions/api/tabs/tabs_constants.h"
 #include "chrome/browser/extensions/api/tabs/tabs_event_router.h"
-#include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
-#include "components/sessions/core/session_id.h"
-#include "content/public/browser/web_contents.h"
 
 // NOTE: This entire file is throwaway code. It is being used to bootstrap tests
 // on the desktop Android platform. It will eventually be replaced by a cross
@@ -43,30 +36,13 @@ void TabsEventRouterPlatformDelegate::OnTabModelAdded(TabModel* tab_model) {
     return;
   }
   if (profile_->IsSameOrParent(tab_model->GetProfile())) {
-    tab_model_observations_.AddObservation(tab_model);
     router_->TrackTabList(*tab_model);
   }
 }
 
 void TabsEventRouterPlatformDelegate::OnTabModelRemoved(TabModel* tab_model) {
-  if (tab_model_observations_.IsObservingSource(tab_model)) {
-    tab_model_observations_.RemoveObservation(tab_model);
-  }
-}
-
-void TabsEventRouterPlatformDelegate::TabRemoved(TabAndroid* tab) {
-  if (!tab || !tab->web_contents()) {
-    return;
-  }
-  int tab_id = ExtensionTabUtil::GetTabId(tab->web_contents());
-  if (!SessionID::IsValidValue(tab_id)) {
-    return;
-  }
-  // NOTE: Some tests call `TabRemoved()` without calling `DidAddTab()`, so
-  // there may not be anything to erase.
-  bool expect_registered = false;
-  router_->UnregisterForTabNotifications(*tab->web_contents(),
-                                         expect_registered);
+  // No need to do anything here. The TabsEventRouter handles untracking the
+  // tab models when they go away.
 }
 
 }  // namespace extensions
--- chrome/browser/extensions/api/tabs/tabs_event_router_platform_delegate_android.h
+++ chrome/browser/extensions/api/tabs/tabs_event_router_platform_delegate_android.h
@@ -5,22 +5,10 @@
 #ifndef CHROME_BROWSER_EXTENSIONS_API_TABS_TABS_EVENT_ROUTER_PLATFORM_DELEGATE_ANDROID_H_
 #define CHROME_BROWSER_EXTENSIONS_API_TABS_TABS_EVENT_ROUTER_PLATFORM_DELEGATE_ANDROID_H_
 
-#include <map>
-#include <memory>
-#include <set>
-#include <string>
-
-#include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
-#include "base/scoped_multi_source_observation.h"
 #include "chrome/browser/ui/android/tab_model/tab_model_list_observer.h"
-#include "chrome/browser/ui/android/tab_model/tab_model_observer.h"
-#include "content/public/browser/web_contents_observer.h"
-#include "extensions/browser/event_router.h"
-#include "url/gurl.h"
 
 class Profile;
-class TabAndroid;
 class TabModel;
 
 namespace extensions {
@@ -29,8 +17,7 @@ class TabsEventRouter;
 // TODO(crbug.com/473593117): This class is a temporary solution to unblock the
 // Tabs API on desktop Android. It can be deleted once TabsEventRouter works on
 // desktop Android.
-class TabsEventRouterPlatformDelegate : public TabModelListObserver,
-                                        public TabModelObserver {
+class TabsEventRouterPlatformDelegate : public TabModelListObserver {
  public:
   TabsEventRouterPlatformDelegate(TabsEventRouter& router, Profile& profile);
   TabsEventRouterPlatformDelegate(const TabsEventRouterPlatformDelegate&) =
@@ -43,24 +30,13 @@ class TabsEventRouterPlatformDelegate : public TabModelListObserver,
   void OnTabModelAdded(TabModel* tab_model) override;
   void OnTabModelRemoved(TabModel* tab_model) override;
 
-  // TabModelObserver:
-  void TabRemoved(TabAndroid* tab) override;
-
  private:
-  void DispatchEvent(events::HistogramValue histogram_value,
-                     const std::string& event_name,
-                     base::ListValue args,
-                     EventRouter::UserGestureState user_gesture);
-
   // The platform-agnostic TabsEventRouter.
   // TODO(https://crbug.com/473593117): This should go away; it's just here
   // while we migrate code.
   raw_ref<TabsEventRouter> router_;
 
   raw_ref<Profile> profile_;
-
-  base::ScopedMultiSourceObservation<TabModel, TabModelObserver>
-      tab_model_observations_{this};
 };
 
 }  // namespace extensions
