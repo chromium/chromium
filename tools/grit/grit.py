e203fe8b3224b1c7190937f124cf7bb4f8e76fbb
Takuto Ikuta <tikuta@chromium.org>
2026-03-11 06:18:41 -0700

    grit: Explicitly flush stdout and stderr before os._exit()
    os._exit() bypasses Python's normal cleanup, such as flushing file
    objects. This CL adds explicit sys.stdout.flush() and sys.stderr.flush()
    calls before calling os._exit() to prevent silent output drops. It also
    adds comments explaining why this is necessary and includes a reference
    to the Python documentation.
    
    This is followup for https://crrev.com/c/7640979/7/tools/grit/grit.py#19
    
    Bug: 491046167
    Change-Id: Icdde294a8c7a823705f90bf91c3d2c9943aed10c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7652317
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597690}

--- tools/grit/grit.py
+++ tools/grit/grit.py
@@ -17,4 +17,11 @@ if __name__ == '__main__':
   ret = grit.grit_runner.Main(sys.argv[1:])
   # Use os._exit() instead of sys.exit() to skip the Python garbage
   # collector teardown at script exit, which takes 1.4s for the 100MB AST.
+  # As os._exit() stops the process immediately without calling cleanup handlers,
+  # we must explicitly flush stdout and stderr to avoid losing any output.
+  # Note: Since os._exit() bypasses Python's normal file cleanup, we must ensure
+  # that all other file objects are properly closed inside grit_runner.Main().
+  # See https://docs.python.org/3.14/library/os.html#os._exit
+  sys.stdout.flush()
+  sys.stderr.flush()
   os._exit(ret or 0)
