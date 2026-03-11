93e22e77ab9932a5958dd039cfd516efd4c4c44a
Jason Jeremy Iman <jasongustaman@google.com>
2026-03-10 20:02:23 -0700

    Enable EnableRootNsDnsProxy by default
    Bug: 363147822
    Change-Id: I946f1407f7d3651bb0cdedb0465b3c2779b739a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7641452
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Jason Iman <jasongustaman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1597493}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -577,7 +577,7 @@ BASE_FEATURE(kEnableOAuthIpp, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableRFC8925, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enable the DNS proxy service running in root network namespace for ChromeOS.
-BASE_FEATURE(kEnableRootNsDnsProxy, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableRootNsDnsProxy, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enable the shortcut to toggle whether the camera is enabled/disabled in
 // Settings > Privacy controls.
