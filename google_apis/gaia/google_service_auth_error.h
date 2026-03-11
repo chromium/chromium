e4849c95d1cbab73d068edd7cfd8ceebfd8d1904
Arnaud Yameogo <wendlassida@google.com>
2026-03-11 10:31:57 -0700

    [iOS][Signin] Add get method for DeviceManagementError
    Change-Id: Ie20d9c33a0a92ef3de15edd198928cb077523d44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7584394
    Reviewed-by: Samar Chehade <samarchehade@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Arnaud Yameogo <wendlassida@google.com>
    Cr-Commit-Position: refs/heads/main@{#1597859}

--- google_apis/gaia/google_service_auth_error.h
+++ google_apis/gaia/google_service_auth_error.h
@@ -237,6 +237,11 @@ class COMPONENT_EXPORT(GOOGLE_APIS) GoogleServiceAuthError {
   // to resolve
   bool IsDeviceManagementErrorUserActionable() const;
 
+  // Returns the details for a device management error. The returned reference
+  // is valid as long as the GoogleServiceAuthError object is alive.
+  // Should only be used when the error state is DEVICE_MANAGEMENT_ERROR.
+  const DeviceManagementErrorDetails& GetDeviceManagementErrorDetails() const;
+
 #if BUILDFLAG(IS_ANDROID)
   static GoogleServiceAuthError FromJavaObject(
       JNIEnv* env,
