b851d8dc99bd18e16585a9c8d7f535d06fd789fa
Ananya <ananyaseelam@google.com>
2026-03-12 16:06:36 -0700

    [aim-eligibility] Add fusebox check to Omnibox AIM Updates
    Before, if aim eligibility updated, the fusebox check was not accounted
    for. This CL ensures that it is and the omnibox fusebox capabilities are
    not shown if AIM Eligibility does not allow it.
    
    Bug: b:489854090
    Change-Id: I904347e9a401a83c28609128e66789d2ff1b1963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7662609
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1598752}

--- chrome/browser/ui/webui/searchbox/webui_omnibox_handler.cc
+++ chrome/browser/ui/webui/searchbox/webui_omnibox_handler.cc
@@ -457,7 +457,8 @@ void WebuiOmniboxHandler::OnAimEligibilityChanged() {
   if (!IsRemoteBound()) {
     return;
   }
-  bool eligible = aim_eligibility_service->IsAimEligible();
+  bool eligible = aim_eligibility_service->IsAimEligible() &&
+                  aim_eligibility_service->IsFuseboxEligible();
   page_->UpdateAimEligibility(eligible);
 }
 
