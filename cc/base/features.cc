e2021125e3762b52eaa1affbf1cf4ba913dcb0a7
Jonathan Ross <jonross@chromium.org>
2026-03-12 12:02:05 -0700

    Enable UnlockDuringGpuImageOperations by default.
    Slight improvements to ScrollJank on Pixels, neutral overall.
    
    Bug: 401787391
    Change-Id: I11ff6d54f2765de03615c9def430d6e4f2839550
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659287
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1598594}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -249,8 +249,7 @@ BASE_FEATURE(kOrderScrollJankV4EventMetricsByArrivedInRendererCompositor,
 
 BASE_FEATURE(kManualBeginFrame, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kUnlockDuringGpuImageOperations,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kUnlockDuringGpuImageOperations, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMainIdleBypassScheduler, base::FEATURE_ENABLED_BY_DEFAULT);
 
