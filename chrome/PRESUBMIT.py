29ef7af349d809ed3167e7068ed5de39328404d2
Lei Zhang <thestig@chromium.org>
2026-03-13 14:46:39 -0700

    Auto-format chrome/PRESUBMIT*.py
    Auto-format these files with `git cl format --python` to improve
    readability and allow future changes to be auto-formatted without
    triggering a large CL diff.
    
    Rewrite a comment with active voice to avoid going 80 columns.
    
    Change-Id: I7ecfb8e73390c41775d67f72577451ab2ee20b7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7666945
    Reviewed-by: Mike Wittman <wittman@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599328}

--- chrome/PRESUBMIT.py
+++ chrome/PRESUBMIT.py
@@ -35,143 +35,167 @@ EXCLUDE = (
 )
 
 def _CheckChangeLintsClean(input_api, output_api):
-  """Makes sure that the chrome/ code is cpplint clean."""
-  files_to_skip = input_api.DEFAULT_FILES_TO_SKIP + EXCLUDE
-  sources = lambda x: input_api.FilterSourceFile(
-    x, files_to_check=INCLUDE_CPP_FILES_ONLY, files_to_skip=files_to_skip)
-  return input_api.canned_checks.CheckChangeLintsClean(
-      input_api, output_api, sources)
+    """Makes sure that the chrome/ code is cpplint clean."""
+    files_to_skip = input_api.DEFAULT_FILES_TO_SKIP + EXCLUDE
+    sources = lambda x: input_api.FilterSourceFile(
+        x, files_to_check=INCLUDE_CPP_FILES_ONLY, files_to_skip=files_to_skip)
+    return input_api.canned_checks.CheckChangeLintsClean(
+        input_api, output_api, sources)
 
 
 def _CheckNoContentUnitTestsInChrome(input_api, output_api):
-  """Makes sure that no unit tests from content/ are included in unit_tests."""
-  problems = []
-  for f in input_api.AffectedFiles():
-    if not f.LocalPath().endswith('BUILD.gn'):
-      continue
+    """Makes sure that unit_tests does not include unit tests from content/."""
+    problems = []
+    for f in input_api.AffectedFiles():
+        if not f.LocalPath().endswith('BUILD.gn'):
+            continue
 
-    for line_num, line in f.ChangedContents():
-      m = re.search(r"'(.*\/content\/.*unittest.*)'", line)
-      if m:
-        problems.append(m.group(1))
+        for line_num, line in f.ChangedContents():
+            m = re.search(r"'(.*\/content\/.*unittest.*)'", line)
+            if m:
+                problems.append(m.group(1))
 
-  if not problems:
-    return []
-  return [output_api.PresubmitPromptWarning(
-      'Unit tests located in content/ should be added to the ' +
-      'content_unittests target.',
-      items=problems)]
+    if not problems:
+        return []
+    return [
+        output_api.PresubmitPromptWarning(
+            'Unit tests located in content/ should be added to the ' +
+            'content_unittests target.',
+            items=problems)
+    ]
 
 
 def _CheckNoIsAppleBuildFlagsInChromeFile(input_api, f):
-  """Check for IS_APPLE in a given file in chrome/."""
-  preprocessor_statement = input_api.re.compile(r'^\s*#')
-  apple_buildflag = input_api.re.compile(r'BUILDFLAG\(IS_APPLE\)')
-  results = []
-  for lnum, line in f.ChangedContents():
-    if preprocessor_statement.search(line) and apple_buildflag.search(line):
-      results.append('    %s:%d' % (f.LocalPath(), lnum))
+    """Check for IS_APPLE in a given file in chrome/."""
+    preprocessor_statement = input_api.re.compile(r'^\s*#')
+    apple_buildflag = input_api.re.compile(r'BUILDFLAG\(IS_APPLE\)')
+    results = []
+    for lnum, line in f.ChangedContents():
+        if preprocessor_statement.search(line) and apple_buildflag.search(
+                line):
+            results.append('    %s:%d' % (f.LocalPath(), lnum))
 
-  return results
+    return results
 
 
 def _CheckNoIsAppleBuildFlagsInChrome(input_api, output_api):
-  """Check for IS_APPLE which isn't used in chrome/."""
-  apple_buildflags = []
-  def SourceFilter(affected_file):
-    return input_api.FilterSourceFile(affected_file, INCLUDE_SOURCE_FILES_ONLY,
-                                      input_api.DEFAULT_FILES_TO_SKIP)
-  for f in input_api.AffectedSourceFiles(SourceFilter):
-    apple_buildflags.extend(_CheckNoIsAppleBuildFlagsInChromeFile(input_api, f))
+    """Check for IS_APPLE which isn't used in chrome/."""
+    apple_buildflags = []
 
-  if not apple_buildflags:
-    return []
+    def SourceFilter(affected_file):
+        return input_api.FilterSourceFile(affected_file,
+                                          INCLUDE_SOURCE_FILES_ONLY,
+                                          input_api.DEFAULT_FILES_TO_SKIP)
 
-  return [output_api.PresubmitError(
-      'IS_APPLE is not used in chrome/ but found in:\n', apple_buildflags)]
+    for f in input_api.AffectedSourceFiles(SourceFilter):
+        apple_buildflags.extend(
+            _CheckNoIsAppleBuildFlagsInChromeFile(input_api, f))
+
+    if not apple_buildflags:
+        return []
+
+    return [
+        output_api.PresubmitError(
+            'IS_APPLE is not used in chrome/ but found in:\n',
+            apple_buildflags)
+    ]
 
 
 def _CheckNoIsIOSBuildFlagsInChromeFile(input_api, f):
-  """Check for IS_IOS in a given file in chrome/."""
-  preprocessor_statement = input_api.re.compile(r'^\s*#')
-  ios_buildflag = input_api.re.compile(r'BUILDFLAG\(IS_IOS\)')
-  results = []
-  for lnum, line in f.ChangedContents():
-    if preprocessor_statement.search(line) and ios_buildflag.search(line):
-      results.append('    %s:%d' % (f.LocalPath(), lnum))
+    """Check for IS_IOS in a given file in chrome/."""
+    preprocessor_statement = input_api.re.compile(r'^\s*#')
+    ios_buildflag = input_api.re.compile(r'BUILDFLAG\(IS_IOS\)')
+    results = []
+    for lnum, line in f.ChangedContents():
+        if preprocessor_statement.search(line) and ios_buildflag.search(line):
+            results.append('    %s:%d' % (f.LocalPath(), lnum))
 
-  return results
+    return results
 
 
 def _CheckNoIsIOSBuildFlagsInChrome(input_api, output_api):
-  """Check for IS_IOS which isn't used in chrome/."""
-  ios_buildflags = []
-  def SourceFilter(affected_file):
-    return input_api.FilterSourceFile(affected_file, INCLUDE_SOURCE_FILES_ONLY,
-                                      input_api.DEFAULT_FILES_TO_SKIP)
-  for f in input_api.AffectedSourceFiles(SourceFilter):
-    ios_buildflags.extend(_CheckNoIsIOSBuildFlagsInChromeFile(input_api, f))
+    """Check for IS_IOS which isn't used in chrome/."""
+    ios_buildflags = []
 
-  if not ios_buildflags:
-    return []
+    def SourceFilter(affected_file):
+        return input_api.FilterSourceFile(affected_file,
+                                          INCLUDE_SOURCE_FILES_ONLY,
+                                          input_api.DEFAULT_FILES_TO_SKIP)
 
-  return [output_api.PresubmitError(
-      'IS_IOS is not used in chrome/ but found in:\n', ios_buildflags)]
+    for f in input_api.AffectedSourceFiles(SourceFilter):
+        ios_buildflags.extend(_CheckNoIsIOSBuildFlagsInChromeFile(
+            input_api, f))
+
+    if not ios_buildflags:
+        return []
+
+    return [
+        output_api.PresubmitError(
+            'IS_IOS is not used in chrome/ but found in:\n', ios_buildflags)
+    ]
 
 
 def _CheckBreakingInstallerVersionBumpNeeded(input_api, output_api):
-  files = []
-  breaking_version_installer_updated = False
+    files = []
+    breaking_version_installer_updated = False
 
-  def _FilterFile(affected_file):
-    return input_api.FilterSourceFile(
-        affected_file,
-        files_to_check=input_api.DEFAULT_FILES_TO_CHECK + (r'.*\.release',))
-  for f in input_api.AffectedSourceFiles(_FilterFile):
-    # Normalize the local path to Linux-style path separators so that the path
-    # comparisons work on Windows as well.
-    local_path = f.LocalPath().replace('\\', '/')
-    breaking_version_installer_updated |= (local_path ==
-    'chrome/installer/setup/last_breaking_installer_version.cc')
-    if (local_path == 'chrome/installer/mini_installer/chrome.release' or
-        local_path.startswith('chrome/test/mini_installer')):
-      files.append(local_path)
+    def _FilterFile(affected_file):
+        return input_api.FilterSourceFile(
+            affected_file,
+            files_to_check=input_api.DEFAULT_FILES_TO_CHECK +
+            (r'.*\.release', ))
 
-  if files and not breaking_version_installer_updated:
-    return [output_api.PresubmitPromptWarning('''
+    for f in input_api.AffectedSourceFiles(_FilterFile):
+        # Normalize the local path to Linux-style path separators so that the
+        # path comparisons work on Windows as well.
+        local_path = f.LocalPath().replace('\\', '/')
+        breaking_version_installer_updated |= (
+            local_path ==
+            'chrome/installer/setup/last_breaking_installer_version.cc')
+        if (local_path == 'chrome/installer/mini_installer/chrome.release'
+                or local_path.startswith('chrome/test/mini_installer')):
+            files.append(local_path)
+
+    if files and not breaking_version_installer_updated:
+        return [
+            output_api.PresubmitPromptWarning('''
 Update chrome/installer/setup/last_breaking_installer_version.cc if the changes
 found in the following files might break make downgrades not possible beyond
-this browser's version.''', items=files)]
+this browser's version.''',
+                                              items=files)
+        ]
 
-  if not files and breaking_version_installer_updated:
-    return [output_api.PresubmitPromptWarning('''
+    if not files and breaking_version_installer_updated:
+        return [
+            output_api.PresubmitPromptWarning('''
 No installer breaking changes detected but
 chrome/installer/setup/last_breaking_installer_version.cc was updated. Please
 update chrome/installer/PRESUBMIT.py if more files need to be watched for
-breaking installer changes.''')]
+breaking installer changes.''')
+        ]
 
-  return []
+    return []
 
 
 def _CommonChecks(input_api, output_api):
-  """Checks common to both upload and commit."""
-  results = []
-  results.extend(_CheckNoContentUnitTestsInChrome(input_api, output_api))
-  results.extend(_CheckNoIsAppleBuildFlagsInChrome(input_api, output_api))
-  results.extend(_CheckNoIsIOSBuildFlagsInChrome(input_api, output_api))
-  results.extend(_CheckBreakingInstallerVersionBumpNeeded(input_api,
-                 output_api))
-  return results
+    """Checks common to both upload and commit."""
+    results = []
+    results.extend(_CheckNoContentUnitTestsInChrome(input_api, output_api))
+    results.extend(_CheckNoIsAppleBuildFlagsInChrome(input_api, output_api))
+    results.extend(_CheckNoIsIOSBuildFlagsInChrome(input_api, output_api))
+    results.extend(
+        _CheckBreakingInstallerVersionBumpNeeded(input_api, output_api))
+    return results
 
 
 def CheckChangeOnUpload(input_api, output_api):
-  results = []
-  results.extend(_CommonChecks(input_api, output_api))
-  results.extend(_CheckChangeLintsClean(input_api, output_api))
-  return results
+    results = []
+    results.extend(_CommonChecks(input_api, output_api))
+    results.extend(_CheckChangeLintsClean(input_api, output_api))
+    return results
 
 
 def CheckChangeOnCommit(input_api, output_api):
-  results = []
-  results.extend(_CommonChecks(input_api, output_api))
-  return results
+    results = []
+    results.extend(_CommonChecks(input_api, output_api))
+    return results
