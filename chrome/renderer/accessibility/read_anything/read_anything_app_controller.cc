cb04e76927b794538de4a99e14b4899a8f10410e
Jacobo Tapia <jatapiaro@google.com>
2026-03-13 11:10:25 -0700

    [Immersive Reading Mode]: Fix visible but inactive links
    This CL fixes a race condition that occurs when AXTree updates are
    received before Readability finishes its distillation.
    
    Previously, we only processed AXTree updates if the
    should_extract_anchors_from_tree_for_readability property in
    ReadAnythingModel was set to true. This property was only enabled upon
    receiving the Readability content. If AXTree updates arrived first, they
    were essentially discarded. Consequently, by the time we attempted to
    process the anchors, the necessary data from the AXTree was missing,
    leaving the links visible but inactive.
    
    Fixed: 491205174
    Change-Id: I3192700547b0b7dd7244f678c11fea4f6b705ae9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7651741
    Commit-Queue: Jacobo Tapia <jatapiaro@google.com>
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599182}

--- chrome/renderer/accessibility/read_anything/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything/read_anything_app_controller.cc
@@ -596,8 +596,7 @@ void ReadAnythingAppController::AccessibilityEventReceived(
     const std::vector<ui::AXTreeUpdate>& updates,
     const std::vector<ui::AXEvent>& events) {
   model_.PrepareForAXTreeUpdates(tree_id);
-  if (IsReadabilityEnabled() && IsReadabilityWithLinksEnabled() &&
-      model_.should_extract_anchors_from_tree_for_readability()) {
+  if (model_.should_apply_accessibility_updates_for_readability()) {
     model_.ApplyAccessibilityUpdates(
         tree_id, const_cast<std::vector<ui::AXTreeUpdate>&>(updates),
         const_cast<std::vector<ui::AXEvent>&>(events));
@@ -607,6 +606,7 @@ void ReadAnythingAppController::AccessibilityEventReceived(
     if (didProcessAnchors) {
       ExecuteJavaScript("chrome.readingMode.onAnchorsReadyForReadability();");
     }
+
     return;
   }
 
