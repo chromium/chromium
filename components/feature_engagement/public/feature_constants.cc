68516a6c49ed9e8c206f7852258d1ebed46629b2
Briana McClure <bmcclure@google.com>
2026-03-13 15:58:17 -0700

    [M147] [x-plat promos] Fix Wave 1 Promo Presentation
    Original change's description:
    > [x-plat promos] Fix Wave 1 Promo Presentation
    >
    > This change enables the Tab Groups and Price Tracking IPHs by default so
    > they appear when the promo is triggered. This change also updates
    > `RecordIOSDesktopPromoShownHistogram` to make testing the promos easier.
    >
    > Bug: 491509253
    > Change-Id: If0bae93151a8bbe361c240ec36704af0a9b4fcca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7656527
    > Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    > Commit-Queue: Briana McClure <bmcclure@google.com>
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1598067}
    
    (cherry picked from commit e3cb5bab81285cfb0c7ae3081e028f3b0a856d85)
    
    Bug: 492482278,491509253
    Change-Id: If0bae93151a8bbe361c240ec36704af0a9b4fcca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667317
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7727@{#283}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -1011,10 +1011,10 @@ BASE_FEATURE(kIPHiOSEnhancedBrowsingDesktopFeature,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHiOSTabGroupsDesktopFeature,
              "IPH_iOSTabGroupsDesktop",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHiOSPriceTrackingDesktopFeature,
              "IPH_iOSPriceTrackingDesktop",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // !BUILDFLAG(IS_ANDROID)
 
 #if !BUILDFLAG(IS_IOS)
