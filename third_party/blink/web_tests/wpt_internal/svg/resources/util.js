9485eba077c80d48f5970aad59606ba5eb05e96a
Ari Chivukula <arichiv@chromium.org>
2026-03-13 10:13:58 -0700

    [SVG] (1) Split tests into multiple files
    This should cut down on flakiness and failures.
    
    Everything here is a no-op, pure refactor.
    
    This CL is part of a series:
    (1) Split tests into multiple files
    (2) Await iframe loads before reading counters
    
    Bug: 492394458
    Change-Id: I5f2c66cdee551eb67cbf91bd4d3b4a8168e8b950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663753
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599146}

---
+++ third_party/blink/web_tests/wpt_internal/svg/resources/util.js
@@ -0,0 +1,40 @@
+import {WebFeature} from '/gen/third_party/blink/public/mojom/use_counter/metrics/web_feature.mojom.m.js';
+
+export function expectCounters(local, remote, plugin, context) {
+  let counters = getCounters();
+  assert_equals(
+    printCounters(counters.local, counters.remote, counters.plugin),
+    printCounters(local, remote, plugin),
+    context
+  );
+}
+
+export function getCounters() {
+  return {
+    local: internals.isUseCounted(document, WebFeature.kSvgFilterPaintedOnLocalFrame),
+    remote: internals.isUseCounted(document, WebFeature.kSvgFilterPaintedOnRemoteFrame),
+    plugin: internals.isUseCounted(document, WebFeature.kSvgFilterPaintedOnWebPlugin),
+  };
+}
+
+export function printCounters(local, remote, plugin) {
+  return `Local: ${local}, Remote: ${remote}, Plugin: ${plugin}`;
+}
+
+export function resetCounters() {
+  internals.clearUseCounter(document, WebFeature.kSvgFilterPaintedOnLocalFrame);
+  internals.clearUseCounter(document, WebFeature.kSvgFilterPaintedOnRemoteFrame);
+  internals.clearUseCounter(document, WebFeature.kSvgFilterPaintedOnWebPlugin);
+}
+
+export async function waitForLoad() {
+  if (document.readyState === 'complete') {
+    return;
+  }
+  await new Promise(resolve => window.addEventListener('load', resolve));
+}
+
+export async function waitForRender() {
+  await new Promise(requestAnimationFrame);
+  await new Promise(setTimeout);
+}
