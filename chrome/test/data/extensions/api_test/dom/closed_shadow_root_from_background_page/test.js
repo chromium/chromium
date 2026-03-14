261f692199dbb02c2f59afd29d68503e5df976db
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-13 17:58:06 -0700

    [Extensions Cleanup] Simple JS updates in chrome.dom API
    This CL updates extension JS files in API tests for the chrome.dom
    API to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: If1987a215909648c17e3e6b6fa1d0dcd95aa324a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7662301
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599416}

--- chrome/test/data/extensions/api_test/dom/closed_shadow_root_from_background_page/test.js
+++ chrome/test/data/extensions/api_test/dom/closed_shadow_root_from_background_page/test.js
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 chrome.test.runTests([function() {
-  var host = document.getElementById('bghost');
-  var shadowRoot1 = host.attachShadow({mode: 'closed'});
+  const host = document.getElementById('bghost');
+  const shadowRoot1 = host.attachShadow({mode: 'closed'});
   shadowRoot1.appendChild(document.createElement('div'));
 
   let shadowRoot2 = chrome.dom.openOrClosedShadowRoot(host);
