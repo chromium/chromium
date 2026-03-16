c483594e1922744485ac1591e21e860aefe813c5
rbpotter <rbpotter@chromium.org>
2026-03-16 14:30:28 -0700

    WebUI Lit Migration script: Update to handle empty style + remove css.js
    - Handle cases where there are included shared styles but no content
      between style tags.
    - Remove .css.js imports from the TS file, since these are now moved
      over as part of the script.
    - Fix regex used to select class name since this script runs after
      lit_migration.js transforms the TS code.
    
    Change-Id: I8cce62f9ecb43fcea460be7d096e366005743703
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7667480
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600126}

--- ui/webui/resources/tools/codemods/lit_migration_templates.mjs
+++ ui/webui/resources/tools/codemods/lit_migration_templates.mjs
@@ -12,7 +12,12 @@ import {parseArgs} from 'node:util';
 // Regular expression to extract CSS Content from within <style>...</style> or
 // <style include="...">...</style> tags. The 'd' flag is needed to obtain the
 // start/end indices of the match.
-const CSS_REGEX = /<style(?<deps>[^>]*)?>(?<content>[^]+)<\/style>/d;
+const CSS_REGEX = /<style(?<deps>[^>]*)?>(?<content>[^]*?)<\/style>/d;
+
+const CSS_IMPORT_REGEX = /import '(?<path>[^']+)\.css\.js';?\n/g;
+
+const LIT_MIGRATION_STYLES_METHOD_REGEX =
+    /  static override get styles\(\) \{\n    return getCss\(\);\n  \}/;
 
 // Header to place on top of the newly created CSS file.
 const CSS_FILE_HEADER = `/* Copyright 2024 The Chromium Authors
@@ -51,7 +56,7 @@ const LISTENER_BINDING_REGEX =
 // Regular expression to parse 2-way bindings like value="{{myValue_}}",
 // and extract 'value' and 'myValue_' into captured groups for further
 // processing.
-const LISTENER_BIDNING_TWO_WAY_REGEX =
+const LISTENER_BINDING_TWO_WAY_REGEX =
     /(?<childProp>[a-z-]+)="\{\{(?<parentProp>[a-zA-Z0-9_]+)\}\}"/g;
 
 // Regular expression to extract a references to TS methods or member variables
@@ -60,11 +65,83 @@ const LISTENER_BIDNING_TWO_WAY_REGEX =
 const TS_REFERENCE_REGEX = /\$\{this\.(?<reference>[^(}]+)(\([^)]*\)){0,1}}/g;
 
 const TS_CLASSNAME_REGEX =
-    /class (?<className>[a-zA-Z0-9]*) extends (PolymerElement|.*ElementBase)/;
+    /class (?<className>[a-zA-Z0-9]*) extends (CrLitElement|.*ElementBase)/;
 
 // Replaces part of a string with a the provided replacement string.
 function replaceRange(string, start, end, replacement) {
   return string.substring(0, start) + replacement + string.substring(end);
+}
+
+function toCamelCase(str) {
+  return str.replace(/-([a-z])/g, g => g[1].toUpperCase())
+      .replace(/^[a-z]/, g => g[0].toUpperCase());
+}
+
+// Processes the case where the style found in the HTML file contains included
+// shared styles but no local content.
+function handleEmptyStyleTag(tsContent, polymerDeps, basename) {
+  const aliasedGets = [];
+  let newImports = '';
+  for (const dep of polymerDeps) {
+    const underscoreDep = dep.replaceAll('-', '_');
+    const alias = `get${toCamelCase(dep)}Css`;
+    aliasedGets.push(`${alias}()`);
+
+    // Find the import for this dep and replace it with an aliased getCss
+    // import.
+    const depImportRegex = new RegExp(
+        `import '(?<path>[^']*(?<filename>${underscoreDep}))\\.css\\.js';?\\n`);
+    const depMatch = tsContent.match(depImportRegex);
+    if (depMatch) {
+      const basePath = depMatch.groups['path'] + '_lit';
+      tsContent = tsContent.replace(
+          depMatch[0],
+          `import {getCss as ${alias}} from '${basePath}.css.js';\n`);
+    } else {
+      // Handle the case where the original Polymer file is missing the
+      // correct import by adding a placeholder import and a TODO.
+      const stylePath = 'STYLE_PATH';  // Placeholder
+      newImports +=
+          `// TODO: identify the correct path and replace the placeholder.\n` +
+          `import {getCss as ${alias}} from '${stylePath}/${
+              underscoreDep}_lit.css.js';\n`;
+    }
+  }
+
+  const litMigrationCssImport =
+      `import {getCss} from './${basename}.css.js';\n`;
+  // Remove the local getCss import added by lit_migration.js, and replace with
+  // any missing shared style imports.
+  tsContent = tsContent.replace(litMigrationCssImport, newImports);
+
+  // Replace the single getCss() call added by lit_migration.js with an array
+  // of shared style calls.
+  const newStylesMethod = `  static override get styles() {
+    return [
+      ${aliasedGets.join(',\n      ')},
+    ];
+  }`;
+
+  return tsContent.replace(LIT_MIGRATION_STYLES_METHOD_REGEX, newStylesMethod);
+}
+
+// Generates the header for the newly created standalone CSS file.
+function getCssFileHeader(litDeps, cssImports) {
+  const importStrings = cssImports.map(importMatch => {
+    const path = importMatch.groups['path'];
+    return path.endsWith('vars') ?
+        ` * #import=${path}.css.js` : ` * #import=${path}_lit.css.js`;
+  });
+
+  let header = CSS_FILE_HEADER;
+  if (litDeps) {
+    header += ` * #include=${litDeps}\n`;
+  }
+  if (importStrings.length > 0) {
+    header += importStrings.join('\n') + '\n';
+  }
+  header += CSS_FILE_HEADER_END;
+  return header;
 }
 
 function processFile(file, outputHtml) {
@@ -74,7 +151,9 @@ function processFile(file, outputHtml) {
   const htmlTsFile = path.join(path.dirname(file), basename + '.html.ts');
   const cssFile = path.join(path.dirname(file), basename + '.css');
 
-  // Step 1: Extract a standalone CSS file and write to disk.
+  let tsContent = fs.readFileSync(tsFile, 'utf8');
+
+  // Step 1: Extract a standalone CSS file.
   let htmlContent = fs.readFileSync(htmlFile, 'utf8');
   const match = htmlContent.match(CSS_REGEX);
 
@@ -82,34 +161,27 @@ function processFile(file, outputHtml) {
     // Extract the deps from the <style> tag and convert them to Lit styles.
     const deps = match.groups['deps'];
     let litDeps = '';
+    let polymerDeps = [];
     if (deps) {
-      let polymerDeps = deps.slice(deps.indexOf('"') + 1);
-      polymerDeps = polymerDeps.slice(0, polymerDeps.indexOf('"')).trim();
-      litDeps = polymerDeps.split(' ').map(dep => dep + '-lit').join(' ');
+      let depString = deps.slice(deps.indexOf('"') + 1);
+      depString = depString.slice(0, depString.indexOf('"')).trim();
+      polymerDeps = depString.split(' ');
+      litDeps = polymerDeps.map(dep => dep + '-lit').join(' ');
     }
 
-    // Extract any .css.js imports from the .ts file and convert to CSS
-    // metadata import lines.
-    let tsContent = fs.readFileSync(tsFile, 'utf8');
-    const cssImports =
-        Array.from(tsContent.matchAll(/import '(?<path>[^\.]*)\.css\.js'/g));
-    const importStrings = cssImports.map(importMatch => {
-      const path = importMatch.groups['path'];
-      return path.endsWith('vars') ?
-          ` * #import=${path}.css.js` : ` * #import=${path}_lit.css.js`;
-    });
-    let cssHeader = CSS_FILE_HEADER;
-    if (deps) {
-      cssHeader += ` * #include=${litDeps}\n`;
+    const content = match.groups['content'];
+    if (content.trim() === '' && polymerDeps.length > 0) {
+      tsContent = handleEmptyStyleTag(tsContent, polymerDeps, basename);
+    } else {
+      const header = getCssFileHeader(
+          litDeps, Array.from(tsContent.matchAll(CSS_IMPORT_REGEX)));
+      fs.writeFileSync(cssFile, header + content, 'utf8');
     }
-    if (importStrings.length > 0) {
-      cssHeader += importStrings.join('\n') + '\n';
-    }
-    cssHeader += CSS_FILE_HEADER_END;
-    fs.writeFileSync(
-        cssFile, cssHeader + match.groups['content'], 'utf8');
 
-    // Step 2: Remove <style>...</style> CSS content from HTML template file.
+    // Always remove any remaining .css.js imports from the .ts file.
+    tsContent = tsContent.replaceAll(CSS_IMPORT_REGEX, '');
+
+    // Step 2: Remove <style>...</style> content from HTML template file.
     htmlContent = htmlContent.substring(match.indices[0][1]);
   }
 
@@ -127,7 +199,8 @@ function processFile(file, outputHtml) {
   htmlContent = htmlContent.replaceAll(/\]\]/g, () => '}');
 
   // Step 5: Update property access syntax in HTML template (2-way bindings)
-  const matches = Array.from(htmlContent.matchAll(LISTENER_BIDNING_TWO_WAY_REGEX));
+  const matches =
+      Array.from(htmlContent.matchAll(LISTENER_BINDING_TWO_WAY_REGEX));
   // Reverse the order so that the character indices don't get messed up after
   // modifying the original string, effectively processing the matches from the
   // end of the string to the start.
@@ -144,7 +217,7 @@ function processFile(file, outputHtml) {
     listenerPart = listenerPart.replace('_', '');
     const listener =
         `@${g['childProp']}-changed="\${this.on${listenerPart}Changed_}"`
-    const binding = matches[i][0].replace("{{", "${this.").replace("}}", "}");
+    const binding = matches[i][0].replace('{{', '${this.').replace('}}', '}');
     const start = matches[i].index;
     const end = matches[i].index + matches[i][0].length;
     htmlContent =
@@ -159,7 +232,6 @@ function processFile(file, outputHtml) {
     // for the import path and class name.
     let htmlTsHeader =
         HTML_TS_FILE_HEADER.replace('FILE_PATH_PLACEHOLDER', basename);
-    let tsContent = fs.readFileSync(tsFile, 'utf8');
     const classNameMatch = tsContent.match(TS_CLASSNAME_REGEX);
     if (classNameMatch) {
       htmlTsHeader = htmlTsHeader.replaceAll(
@@ -168,22 +240,20 @@ function processFile(file, outputHtml) {
 
     // Step 6: Write .html.ts file with updated HTML content and wrapper
     // file content to disk.
-    fs.writeFileSync(htmlTsFile,
-        htmlTsHeader + htmlContent + HTML_TS_FILE_FOOTER, 'utf8');
+    fs.writeFileSync(
+        htmlTsFile, htmlTsHeader + htmlContent + HTML_TS_FILE_FOOTER, 'utf8');
   }
 
   // Step 7: Extract all methods/variables being referenced from the template
   //         and if they are 'private' change them to 'protected'.
-  const references = Array.from(
-      htmlContent.matchAll(TS_REFERENCE_REGEX)).map(m => m[1]);
-  if (references.length > 0) {
-    let tsContent = fs.readFileSync(tsFile, 'utf8');
-    for (const ref of references) {
-      tsContent = tsContent.replaceAll(`private ${ref}`, `protected ${ref}`);
-    }
-    // Step 7b: Write updated TS content to disk.
-    fs.writeFileSync(tsFile, tsContent, 'utf8');
+  const references =
+      Array.from(htmlContent.matchAll(TS_REFERENCE_REGEX)).map(m => m[1]);
+  for (const ref of references) {
+    tsContent = tsContent.replaceAll(`private ${ref}`, `protected ${ref}`);
   }
+
+  // Step 8: Write updated TS content to disk.
+  fs.writeFileSync(tsFile, tsContent, 'utf8');
 }
 
 function main() {
