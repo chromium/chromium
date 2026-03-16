4fa118b901c253423622479392e63ac8ed716c1e
Asami Doi <asamidoi@chromium.org>
2026-03-16 06:21:52 -0700

    [ios] Deprecate initWithFrame in ScopedKeyWindow
    initWithFrame was deprecated on iOS 26, initWithWindowScene should be
    used instead.
    
    Bug: 489089329
    Change-Id: Ifa3bb6ca163bc75593e65e1537fa1f506e27336e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7633482
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1599844}

--- ios/chrome/test/scoped_key_window.mm
+++ ios/chrome/test/scoped_key_window.mm
@@ -7,28 +7,21 @@
 #import "base/check_op.h"
 #import "base/ios/ios_util.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/chrome/test/app/uikit_test_util.h"
 
 ScopedKeyWindow::ScopedKeyWindow() {
   NSSet<UIScene*>* scenes =
       ([[UIApplication sharedApplication] connectedScenes]);
   // Only one scene is supported in unittests at the moment.
   DCHECK_EQ([scenes count], 1u);
-  UIScene* scene =
-      [[[UIApplication sharedApplication] connectedScenes] anyObject];
-  DCHECK([scene isKindOfClass:[UIWindowScene class]]);
-  UIWindowScene* windowScene = static_cast<UIWindowScene*>(scene);
-  for (UIWindow* window in windowScene.windows) {
-    if ([window isKeyWindow]) {
-      original_key_window_ = window;
-    }
-  }
+
+  UIWindowScene* window_scene = chrome_test_util::GetAnyWindowScene();
+  original_key_window_ = window_scene.keyWindow;
   DCHECK(original_key_window_);
-  current_key_window_ = [[UIWindow alloc] initWithWindowScene:windowScene];
-  if (!current_key_window_) {
-    current_key_window_ =
-        [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    original_key_window_ = GetAnyKeyWindow();
-  }
+
+  current_key_window_ = [[UIWindow alloc] initWithWindowScene:window_scene];
+  DCHECK(current_key_window_);
+
   [current_key_window_ makeKeyAndVisible];
 }
 
