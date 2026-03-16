fdd90ed7f115a054ddd24c0b415cd026006c997e
Alfredo Higuera <alfredohiguera@google.com>
2026-03-16 13:23:26 -0700

    Refactor: Use eventToPromise to wait for CLOSE_ALL_MENUS in font_menu tests
    This change updates Read Anything's font menu test to use eventToPromise
    to asynchronously wait for the ToolbarEvent.CLOSE_ALL_MENUS event to be
    dispatched after a menu item is selected. This replaces a manual event
    counter, improving test reliability by ensuring the event has occurred
    before subsequent assertions.
    
    Fixed: 476147785
    Change-Id: Id5faac7e45acc1a83b69059e2bf952777d51069d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663086
    Reviewed-by: Jacobo Tapia <jatapiaro@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Alfredo Higuera <alfredohiguera@google.com>
    Cr-Commit-Position: refs/heads/main@{#1600081}

--- chrome/test/data/webui/side_panel/read_anything/font_menu_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/font_menu_test.ts
@@ -7,7 +7,7 @@ import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js'
 import {DEFAULT_SETTINGS, ReadAnythingSettingsChange, ToolbarEvent} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import type {FontMenuElement} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
-import {microtasksFinished} from 'chrome-untrusted://webui-test/test_util.js';
+import {eventToPromise, microtasksFinished} from 'chrome-untrusted://webui-test/test_util.js';
 
 import {assertCheckMarksForDropdown, assertHeadersForDropdown, getItemsInMenu, mockMetrics, stubAnimationFrame} from './common.js';
 import {FakeReadingMode} from './fake_reading_mode.js';
@@ -127,23 +127,29 @@ suite('FontMenu', () => {
 
   test('propagates font', async () => {
     const numberOfFonts = 3;
-    let closeAllMenusCount = 0;
-    document.addEventListener(
-        ToolbarEvent.CLOSE_ALL_MENUS, () => closeAllMenusCount += 1);
 
     const font1 = 'Times';
+    const closePromise1 =
+        eventToPromise(ToolbarEvent.CLOSE_ALL_MENUS, document);
     fontMenu.$.menu.dispatchEvent(
         new CustomEvent(ToolbarEvent.FONT, {detail: {data: font1}}));
+    await closePromise1;
     assertEquals(font1, chrome.readingMode.fontName);
 
     const font2 = 'Poppins';
+    const closePromise2 =
+        eventToPromise(ToolbarEvent.CLOSE_ALL_MENUS, document);
     fontMenu.$.menu.dispatchEvent(
         new CustomEvent(ToolbarEvent.FONT, {detail: {data: font2}}));
+    await closePromise2;
     assertEquals(font2, chrome.readingMode.fontName);
 
     const font3 = 'STIX Two Text';
+    const closePromise3 =
+        eventToPromise(ToolbarEvent.CLOSE_ALL_MENUS, document);
     fontMenu.$.menu.dispatchEvent(
         new CustomEvent(ToolbarEvent.FONT, {detail: {data: font3}}));
+    await closePromise3;
     assertEquals(font3, chrome.readingMode.fontName);
 
     assertEquals(
@@ -151,7 +157,6 @@ suite('FontMenu', () => {
         await metrics.whenCalled('recordTextSettingsChange'));
     assertEquals(
         numberOfFonts, metrics.getCallCount('recordTextSettingsChange'));
-    assertEquals(numberOfFonts, closeAllMenusCount);
   });
 
   test('can be closed programatically', () => {
