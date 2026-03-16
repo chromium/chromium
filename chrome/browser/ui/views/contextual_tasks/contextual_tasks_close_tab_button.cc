c8bec9ff9c230086ce7d8350210a3074a8b1e4a8
Yuheng Huang <yuhengh@chromium.org>
2026-03-16 14:35:41 -0700

    [146] [Contextual tasks] Add user action for toolbar close tab button
    Original change's description:
    > [Contextual tasks] Add user action for toolbar close tab button
    >
    > Bug: 491894531
    > Change-Id: I26fdfbb9c59d54b1704072f2556b09bde60fc941
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7659384
    > Reviewed-by: Min Qin <qinmin@chromium.org>
    > Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1598494}
    
    Bug: 493206283
    Change-Id: I26fdfbb9c59d54b1704072f2556b09bde60fc941
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7670171
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7680@{#2680}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- chrome/browser/ui/views/contextual_tasks/contextual_tasks_close_tab_button.cc
+++ chrome/browser/ui/views/contextual_tasks/contextual_tasks_close_tab_button.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ui/views/contextual_tasks/contextual_tasks_close_tab_button.h"
 
 #include "base/functional/bind.h"
+#include "base/metrics/user_metrics.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/views/contextual_tasks/contextual_tasks_close_button_controller.h"
 #include "components/strings/grit/components_strings.h"
@@ -49,6 +50,9 @@ void ContextualTasksCloseTabButton::OnButtonPress() {
       ContextualTasksCloseButtonController::From(browser_window_interface_);
   CHECK(controller);
   controller->MaybeCloseTabExpandSidePanel();
+  base::RecordAction(
+      base::UserMetricsAction("ContextualTasks.ToolbarCloseTabButton."
+                              "UserAction.CloseTabAndExpandSidePanel"));
 }
 
 void ContextualTasksCloseTabButton::OnShouldUpdateVisibility(bool should_show) {
