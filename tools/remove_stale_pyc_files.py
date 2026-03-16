10c9f78329effde8e70df489166e940fc265cc3c
Adam Rice <ricea@chromium.org>
2026-03-16 04:20:15 -0700

    Use fdfind in remove_stale_pyc_files.py
    Modify tools/remove_stale_pyc_files.py to use fdfind if available. Check
    both the upstream binary name "fd" and the Debian/Ubuntu binary name
    "fdfind". If present and appearing functional use it in preference to
    os.walk. Otherwise fall back to the existing os.walk implementation.
    
    On my machine this improves the time taken to run the script from 1.8
    seconds to 0.2 seconds. Since this is run by "gclient sync" it is good
    for productivity for it to be faster.
    
    The fd-find package is included in install-build-deps.py so many
    Chromium devs will already have it installed.
    
    Change-Id: I618f8fb451640d5b260b898e7e4718b7f51e26df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7665460
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Jesse McKenna <jessemckenna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1599800}

--- tools/remove_stale_pyc_files.py
+++ tools/remove_stale_pyc_files.py
@@ -4,30 +4,72 @@
 # found in the LICENSE file.
 
 import os
+import subprocess
 import sys
 
 
-def RemoveAllStalePycFiles(base_dir):
-  """Scan directories for old .pyc files without a .py file and delete them."""
-  for dirname, _, filenames in os.walk(base_dir, topdown=False):
-    if '.svn' in dirname or '.git' in dirname:
+def GetFdCommand():
+  """Returns the name of the 'fd' command if it is available and suitable."""
+  for cmd in ('fdfind', 'fd'):
+    try:
+      # We check for --help output to ensure it's the right program and
+      # supports the flags we need.
+      res = subprocess.run([cmd, '--help'],
+                           capture_output=True,
+                           text=True,
+                           check=False)
+      if (res.returncode == 0 and '--glob' in res.stdout
+          and '--print0' in res.stdout and '--no-ignore' in res.stdout):
+        return cmd
+    except OSError:
       continue
-    for filename in filenames:
-      root, ext = os.path.splitext(filename)
-      if ext != '.pyc':
+  return None
+
+
+def RemoveIfStale(pyc_path):
+  """Deletes pyc_path if it doesn't have a corresponding .py file."""
+  root, _ = os.path.splitext(pyc_path)
+  py_path = root + '.py'
+  try:
+    if not os.path.exists(py_path):
+      os.remove(pyc_path)
+  except OSError:
+    # Wrap OS calls in try/except in case another process touched this file.
+    pass
+
+
+def RemoveAllStalePycFiles(base_dirs):
+  """Scan directories for old .pyc files without a .py file and delete them."""
+  base_dirs = [d for d in base_dirs if os.path.exists(d)]
+  if not base_dirs:
+    return
+  fd_cmd = GetFdCommand()
+  if fd_cmd:
+    output = None
+    try:
+      res = subprocess.run(
+          [fd_cmd, '--no-ignore', '--print0', '--glob', '*.pyc'] + base_dirs,
+          capture_output=True,
+          check=True)
+      output = res.stdout
+    except (subprocess.CalledProcessError, OSError):
+      # Fall back to os.walk if fd fails.
+      pass
+
+    if output is not None:
+      for pyc_path in output.split(b'\0'):
+        if pyc_path:
+          RemoveIfStale(os.fsdecode(pyc_path))
+      return
+
+  for base_dir in base_dirs:
+    for dirname, _, filenames in os.walk(base_dir, topdown=False):
+      if '.svn' in dirname or '.git' in dirname:
         continue
-
-      pyc_path = os.path.join(dirname, filename)
-      py_path = os.path.join(dirname, root + '.py')
-
-      try:
-        if not os.path.exists(py_path):
-          os.remove(pyc_path)
-      except OSError:
-        # Wrap OS calls in try/except in case another process touched this file.
-        pass
+      for filename in filenames:
+        if filename.endswith('.pyc'):
+          RemoveIfStale(os.path.join(dirname, filename))
 
 
 if __name__ == '__main__':
-  for path in sys.argv[1:]:
-    RemoveAllStalePycFiles(path)
+  RemoveAllStalePycFiles(sys.argv[1:])
