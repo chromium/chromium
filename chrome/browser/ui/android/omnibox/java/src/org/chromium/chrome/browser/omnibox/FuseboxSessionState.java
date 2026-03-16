480dcb86c70d9930a4fb2f98f1b2da5a2342f991
Tomasz Wiszkowski <ender@google.com>
2026-03-16 15:30:23 -0700

    [M147] Ensure FuseboxSessionState is cleanly destroyed when Chrome exits.
    Original change's description:
    > Ensure FuseboxSessionState is cleanly destroyed when Chrome exits.
    >
    > This change guarantees that we release all Composebox instances
    > created to drive active user sessions when the application exits.
    >
    > Explicit destruction of Composebox will ensure any weak pointers are
    > invalidated, rendering any pending async callbacks to be canceled.
    >
    > Bug: 491839821
    > Change-Id: I618c930f5bf2a6f87da4f1c66c35f7d4cb49623d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7658842
    > Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    > Reviewed-by: Jason Hu <hujasonx@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1597961}
    
    (cherry picked from commit 663f2cd27f846b2b95a08fc67dd0279924322cdb)
    
    Bug: 492150172,491839821
    Change-Id: I618c930f5bf2a6f87da4f1c66c35f7d4cb49623d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663131
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/branch-heads/7727@{#514}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/FuseboxSessionState.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/FuseboxSessionState.java
@@ -199,6 +199,11 @@ public class FuseboxSessionState implements UserData {
         if (onFullyActivated != null) onFullyActivated.run();
     }
 
+    @Override
+    public void destroy() {
+        tearDownSessionControllers();
+    }
+
     /** Tear down session controllers. */
     private void tearDownSessionControllers() {
         if (mFuseboxAttachmentModelList != null) {
