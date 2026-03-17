3233143d858e3ec2fe72af75a0f46a9153b327d9
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-03-16 17:01:26 -0700

    [Extensions Cleanup] Simple JS updates in extension_resource_request_policy
    This CL updates extension JS files in
    api_test/extension_resource_request_policy API to
    * use `let` and `const` instead of `var`
    * update variable naming patterns (avoiding underscores, properly
      naming consts)
    * prefer single quotes instead of double quotes and use template
      literals when appropriate
    
    Bug: 492151886
    Change-Id: I40e910e5f1430e1a7124e2758ed1c06e222a968c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7663793
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1600202}

--- chrome/test/data/extensions/api_test/extension_resource_request_policy/web_accessible/iframe.js
+++ chrome/test/data/extensions/api_test/extension_resource_request_policy/web_accessible/iframe.js
@@ -5,7 +5,7 @@
 // Test that both static <iframe> tags and dynamically generated ones can be
 // loaded by their parent extension (iframe-contents.html is not listed in
 // web_accessible_resources).
-var staticIframeLoaded = false;
+let staticIframeLoaded = false;
 function iframeLoaded() {
   if (staticIframeLoaded) {
     chrome.test.notifyPass();
@@ -14,7 +14,7 @@ function iframeLoaded() {
 
   staticIframeLoaded = true;
 
-  var iframe = document.createElement('iframe');
+  const iframe = document.createElement('iframe');
   document.body.appendChild(iframe);
   iframe.src = 'iframe-contents.html';
 }
