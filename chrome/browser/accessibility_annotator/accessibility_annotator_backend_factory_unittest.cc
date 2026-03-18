a5a5031518578778198c053b7681b17ae2c3f89e
Emily Fu <fumengyao@google.com>
2026-03-18 10:42:26 -0700

    Integrate with chrome history service
    Make AccessibilityAnnotatorBackend as a direct observer of
    HistoryService to listen to history events.
    
    Bug: b:489871834
    Change-Id: I969038a414447cbbb2cee7d14d2a2f12eb807bcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7669706
    Commit-Queue: Emily Fu <fumengyao@google.com>
    Reviewed-by: Andrew Vuong <akvuong@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601393}

--- chrome/browser/accessibility_annotator/accessibility_annotator_backend_factory_unittest.cc
+++ chrome/browser/accessibility_annotator/accessibility_annotator_backend_factory_unittest.cc
@@ -5,8 +5,12 @@
 #include "chrome/browser/accessibility_annotator/accessibility_annotator_backend_factory.h"
 
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/history/history_service_factory.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/accessibility_annotator/core/accessibility_annotator_features.h"
+#include "components/history/core/browser/history_service.h"
+#include "components/history/core/browser/history_service.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -17,26 +21,43 @@ class AccessibilityAnnotatorBackendFactoryTest : public testing::Test {
   AccessibilityAnnotatorBackendFactoryTest() = default;
   ~AccessibilityAnnotatorBackendFactoryTest() override = default;
 
+  void SetUp() override {
+    TestingProfile::Builder builder;
+    builder.AddTestingFactory(HistoryServiceFactory::GetInstance(),
+                              HistoryServiceFactory::GetDefaultFactory());
+    profile_ = builder.Build();
+  }
+
  protected:
   content::BrowserTaskEnvironment task_environment_;
   base::test::ScopedFeatureList scoped_feature_list_{
       accessibility_annotator::kAccessibilityAnnotator};
+  std::unique_ptr<TestingProfile> profile_;
 };
 
 TEST_F(AccessibilityAnnotatorBackendFactoryTest,
        ServiceCreatedForRegularProfile) {
-  TestingProfile profile;
   EXPECT_NE(nullptr,
-            AccessibilityAnnotatorBackendFactory::GetForProfile(&profile));
+            AccessibilityAnnotatorBackendFactory::GetForProfile(
+               profile_.get()));
 }
 
 TEST_F(AccessibilityAnnotatorBackendFactoryTest,
        ServiceRedirectedForIncognitoProfile) {
-  TestingProfile profile;
-  Profile* otr_profile = profile.GetOffTheRecordProfile(
+  Profile* otr_profile = profile_->GetOffTheRecordProfile(
       Profile::OTRProfileID::PrimaryID(), /*create_if_needed=*/true);
-  EXPECT_EQ(AccessibilityAnnotatorBackendFactory::GetForProfile(&profile),
+
+  EXPECT_EQ(AccessibilityAnnotatorBackendFactory::GetForProfile(profile_.get()),
             AccessibilityAnnotatorBackendFactory::GetForProfile(otr_profile));
+}
+
+TEST_F(AccessibilityAnnotatorBackendFactoryTest, ServiceDisabled) {
+  scoped_feature_list_.Reset();
+  scoped_feature_list_.InitAndDisableFeature(kAccessibilityAnnotator);
+
+  EXPECT_EQ(nullptr,
+            AccessibilityAnnotatorBackendFactory::GetForProfile(
+               profile_.get()));
 }
 
 }  // namespace accessibility_annotator
