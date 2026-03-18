0924c5bd7041b238e0dabd2321b2863b59a60e97
Daniel Angulo <angdaniel@google.com>
2026-03-18 09:18:41 -0700

    spanification of libyuv_scale_fuzzer.cc and advance of RNG state preservation.
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli
    
    The random number generator state is passed as a copy to the
    FillBufferWithRandomData, so the outer rng is never advanced making the
    three palnes populated with the exact same sequence of random numbers,
    reducing the entropy and effectiveness of the fuzzer. With this change
    the RNG is passed as a reference and its state is preserved and advanced
    as expected.
    
    gemini-run/batch-run-1761662609/group_2
    
    BUG: 439964610
    Change-Id: I734f12c47738d4ffbea696945346aeadb3e19055
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7654277
    Commit-Queue: Jon Toohill <jtoohill@google.com>
    Reviewed-by: Jon Toohill <jtoohill@google.com>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601344}

--- testing/libfuzzer/fuzzers/libyuv_scale_fuzzer.cc
+++ testing/libfuzzer/fuzzers/libyuv_scale_fuzzer.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "testing/libfuzzer/fuzzers/libyuv_scale_fuzzer.h"
 
 #include <stddef.h>
@@ -16,19 +11,22 @@
 #include <random>
 #include <string>
 
+#include "base/containers/heap_array.h"
+#include "base/containers/span.h"
 #include "third_party/libyuv/include/libyuv.h"
 
-static void FillBufferWithRandomData(uint8_t* dst,
-                                     size_t len,
-                                     std::minstd_rand0 rng) {
-  size_t i;
-  for (i = 0; i + 3 < len; i += 4) {
-    *reinterpret_cast<uint32_t*>(dst) = rng();
-    dst += 4;
+static void FillBufferWithRandomData(base::span<uint8_t> dst,
+                                     std::minstd_rand0& rng) {
+  // Maximize fuzzer throughput by writing in 32-bit chunks to minimize RNG
+  // calls.
+  const size_t aligned_bytes = (dst.size() / 4) * 4;
+  auto [aligned_dst, trailing_dst] = dst.split_at(aligned_bytes);
+  for (uint32_t& val : base::subtle::reinterpret_span<uint32_t>(aligned_dst)) {
+    val = rng();
   }
-  for (; i < len; ++i) {
-    *dst++ = rng();
-  }
+  uint32_t trailing_rand = rng();
+  trailing_dst.copy_from(base::as_bytes(base::span_from_ref(trailing_rand))
+                             .first(trailing_dst.size()));
 }
 
 void Scale(bool is420,
@@ -53,16 +51,13 @@ void Scale(bool is420,
   int src_stride_y = std::abs(src_width);
   int src_stride_uv = src_width_uv;
 
-  uint8_t* src_y = reinterpret_cast<uint8_t*>(malloc(src_y_plane_size));
-  uint8_t* src_u = reinterpret_cast<uint8_t*>(malloc(src_uv_plane_size));
-  uint8_t* src_v = reinterpret_cast<uint8_t*>(malloc(src_uv_plane_size));
+  auto src_y = base::HeapArray<uint8_t>::Uninit(src_y_plane_size);
+  auto src_u = base::HeapArray<uint8_t>::Uninit(src_uv_plane_size);
+  auto src_v = base::HeapArray<uint8_t>::Uninit(src_uv_plane_size);
 
-  uint16_t* p_src_y_16 =
-      reinterpret_cast<uint16_t*>(malloc(src_y_plane_size * 2));
-  uint16_t* p_src_u_16 =
-      reinterpret_cast<uint16_t*>(malloc(src_uv_plane_size * 2));
-  uint16_t* p_src_v_16 =
-      reinterpret_cast<uint16_t*>(malloc(src_uv_plane_size * 2));
+  auto p_src_y_16 = base::HeapArray<uint16_t>::Uninit(src_y_plane_size);
+  auto p_src_u_16 = base::HeapArray<uint16_t>::Uninit(src_uv_plane_size);
+  auto p_src_v_16 = base::HeapArray<uint16_t>::Uninit(src_uv_plane_size);
 
   std::seed_seq seed(seed_str.begin(), seed_str.end());
   std::minstd_rand0 rng(seed);
@@ -70,9 +65,9 @@ void Scale(bool is420,
   // TODO - consider taking directly as parameters when this code
   // is being run using FuzzTest, though it would probably require
   // complex domains to ensure they're the right size.
-  FillBufferWithRandomData(src_y, src_y_plane_size, rng);
-  FillBufferWithRandomData(src_u, src_uv_plane_size, rng);
-  FillBufferWithRandomData(src_v, src_uv_plane_size, rng);
+  FillBufferWithRandomData(src_y, rng);
+  FillBufferWithRandomData(src_u, rng);
+  FillBufferWithRandomData(src_v, rng);
 
   for (size_t i = 0; i < src_y_plane_size; ++i) {
     p_src_y_16[i] = src_y[i];
@@ -97,54 +92,37 @@ void Scale(bool is420,
   int dst_stride_y = dst_width;
   int dst_stride_uv = dst_width_uv;
 
-  uint8_t* dst_y_c = reinterpret_cast<uint8_t*>(malloc(dst_y_plane_size));
-  uint8_t* dst_u_c = reinterpret_cast<uint8_t*>(malloc(dst_uv_plane_size));
-  uint8_t* dst_v_c = reinterpret_cast<uint8_t*>(malloc(dst_uv_plane_size));
+  auto dst_y_c = base::HeapArray<uint8_t>::Uninit(dst_y_plane_size);
+  auto dst_u_c = base::HeapArray<uint8_t>::Uninit(dst_uv_plane_size);
+  auto dst_v_c = base::HeapArray<uint8_t>::Uninit(dst_uv_plane_size);
 
-  uint16_t* p_dst_y_16 =
-      reinterpret_cast<uint16_t*>(malloc(dst_y_plane_size * 2));
-  uint16_t* p_dst_u_16 =
-      reinterpret_cast<uint16_t*>(malloc(dst_uv_plane_size * 2));
-  uint16_t* p_dst_v_16 =
-      reinterpret_cast<uint16_t*>(malloc(dst_uv_plane_size * 2));
+  auto p_dst_y_16 = base::HeapArray<uint16_t>::Uninit(dst_y_plane_size);
+  auto p_dst_u_16 = base::HeapArray<uint16_t>::Uninit(dst_uv_plane_size);
+  auto p_dst_v_16 = base::HeapArray<uint16_t>::Uninit(dst_uv_plane_size);
 
   if (is420) {
-    I420Scale(src_y, src_stride_y, src_u, src_stride_uv, src_v, src_stride_uv,
-              src_width, src_height, dst_y_c, dst_stride_y, dst_u_c,
-              dst_stride_uv, dst_v_c, dst_stride_uv, dst_width, dst_height,
+    I420Scale(src_y.data(), src_stride_y, src_u.data(), src_stride_uv,
+              src_v.data(), src_stride_uv, src_width, src_height,
+              dst_y_c.data(), dst_stride_y, dst_u_c.data(), dst_stride_uv,
+              dst_v_c.data(), dst_stride_uv, dst_width, dst_height,
               static_cast<libyuv::FilterMode>(filter_num));
 
-    I420Scale_16(p_src_y_16, src_stride_y, p_src_u_16, src_stride_uv,
-                 p_src_v_16, src_stride_uv, src_width, src_height, p_dst_y_16,
-                 dst_stride_y, p_dst_u_16, dst_stride_uv, p_dst_v_16,
-                 dst_stride_uv, dst_width, dst_height,
-                 static_cast<libyuv::FilterMode>(filter_num));
+    I420Scale_16(p_src_y_16.data(), src_stride_y, p_src_u_16.data(),
+                 src_stride_uv, p_src_v_16.data(), src_stride_uv, src_width,
+                 src_height, p_dst_y_16.data(), dst_stride_y, p_dst_u_16.data(),
+                 dst_stride_uv, p_dst_v_16.data(), dst_stride_uv, dst_width,
+                 dst_height, static_cast<libyuv::FilterMode>(filter_num));
   } else {
-    I444Scale(src_y, src_stride_y, src_u, src_stride_uv, src_v, src_stride_uv,
-              src_width, src_height, dst_y_c, dst_stride_y, dst_u_c,
-              dst_stride_uv, dst_v_c, dst_stride_uv, dst_width, dst_height,
+    I444Scale(src_y.data(), src_stride_y, src_u.data(), src_stride_uv,
+              src_v.data(), src_stride_uv, src_width, src_height,
+              dst_y_c.data(), dst_stride_y, dst_u_c.data(), dst_stride_uv,
+              dst_v_c.data(), dst_stride_uv, dst_width, dst_height,
               static_cast<libyuv::FilterMode>(filter_num));
 
-    I444Scale_16(p_src_y_16, src_stride_y, p_src_u_16, src_stride_uv,
-                 p_src_v_16, src_stride_uv, src_width, src_height, p_dst_y_16,
-                 dst_stride_y, p_dst_u_16, dst_stride_uv, p_dst_v_16,
-                 dst_stride_uv, dst_width, dst_height,
-                 static_cast<libyuv::FilterMode>(filter_num));
+    I444Scale_16(p_src_y_16.data(), src_stride_y, p_src_u_16.data(),
+                 src_stride_uv, p_src_v_16.data(), src_stride_uv, src_width,
+                 src_height, p_dst_y_16.data(), dst_stride_y, p_dst_u_16.data(),
+                 dst_stride_uv, p_dst_v_16.data(), dst_stride_uv, dst_width,
+                 dst_height, static_cast<libyuv::FilterMode>(filter_num));
   }
-
-  free(src_y);
-  free(src_u);
-  free(src_v);
-
-  free(p_src_y_16);
-  free(p_src_u_16);
-  free(p_src_v_16);
-
-  free(dst_y_c);
-  free(dst_u_c);
-  free(dst_v_c);
-
-  free(p_dst_y_16);
-  free(p_dst_u_16);
-  free(p_dst_v_16);
 }
