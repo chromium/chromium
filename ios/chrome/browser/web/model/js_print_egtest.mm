e641304616a7c78fc2406e4b0221f60278d7bd37
Alexandra Pereira <alexandrasp@google.com>
2026-03-18 09:52:05 -0700

    [iOS] Convert JSPrintTestCase to use EmbeddedTestServer
    Migrate JSPrintTestCase from the deprecated web::HttpServer to the
    modern net::EmbeddedTestServer. This change resolves architectural
    technical debt and aligns the test case with current networking
    standards in the iOS browser.
    
    Bug: 41311220, 40596517
    Change-Id: If2a058602adc1fdd7e111ee39087b3d670faa17f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7676178
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601361}

--- ios/chrome/browser/web/model/js_print_egtest.mm
+++ ios/chrome/browser/web/model/js_print_egtest.mm
@@ -4,17 +4,20 @@
 
 #import <XCTest/XCTest.h>
 
-#import <map>
+#import <memory>
 
+#import "base/functional/bind.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
-#import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
-#import "ios/testing/earl_grey/disabled_test_macros.h"
+#import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
-#import "ios/web/public/test/http_server/http_server.h"
-#import "ios/web/public/test/http_server/http_server_util.h"
+#import "net/test/embedded_test_server/embedded_test_server.h"
+#import "net/test/embedded_test_server/http_request.h"
+#import "net/test/embedded_test_server/http_response.h"
 
 namespace {
+const char kPrintPath[] = "/printpage";
+
 // Matcher for the cancel button on the printer options view.
 id<GREYMatcher> PrintOptionsCancelButton() {
   if (@available(iOS 26, *)) {
@@ -25,29 +28,46 @@ id<GREYMatcher> PrintOptionsCancelButton() {
   return grey_allOf(grey_accessibilityLabel(@"Cancel"),
                     grey_kindOfClass([UIButton class]), nil);
 }
+
+// A request handler that provides the print page HTML.
+std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
+    const net::test_server::HttpRequest& request) {
+  if (request.relative_url != kPrintPath) {
+    auto response = std::make_unique<net::test_server::BasicHttpResponse>();
+    response->set_code(net::HTTP_NOT_FOUND);
+    return response;
+  }
+  std::unique_ptr<net::test_server::BasicHttpResponse> response =
+      std::make_unique<net::test_server::BasicHttpResponse>();
+  response->set_code(net::HTTP_OK);
+  response->set_content_type("text/html");
+  response->set_content(
+      "<html><body>"
+      "<input onclick='window.print();' type='button' id=\"printButton\" "
+      "value='Print Page' />"
+      "</body></html>");
+  return std::move(response);
+}
 }  // namespace
 
 // Test case for bringing up the print dialog when a web site's JavaScript runs
 // "window.print".
-@interface JSPrintTestCase : WebHttpServerChromeTestCase
+@interface JSPrintTestCase : ChromeTestCase
 @end
 
 @implementation JSPrintTestCase
 
+- (void)setUp {
+  [super setUp];
+  self.testServer->RegisterRequestHandler(base::BindRepeating(&HandleRequest));
+  GREYAssertTrue(self.testServer->Start(),
+                 @"EmbeddedTestServer failed to start.");
+}
+
 // Tests that tapping a button with onclick='window.print' brings up the
 // print dialog.
 - (void)testWebPrintButton {
-  // Create map of canned responses and set up the test HTML server.
-  std::map<GURL, std::string> responses;
-  const GURL testURL = web::test::HttpServer::MakeUrl("http://printpage");
-
-  // Page containing button with onclick attribute calling window.print.
-  responses[testURL] =
-      "<input onclick='window.print();' type='button' id=\"printButton\" "
-      "value='Print Page' />";
-
-  web::test::SetUpSimpleHttpServer(responses);
-  [ChromeEarlGrey loadURL:testURL];
+  [ChromeEarlGrey loadURL:self.testServer->GetURL(kPrintPath)];
 
   // Tap print button.
   [ChromeEarlGrey tapWebStateElementWithID:@"printButton"];
