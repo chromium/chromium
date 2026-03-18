9204edaa7732d15610b7f070c26969ad92794648
James Leung <jamesleung@google.com>
2026-03-18 16:57:33 -0700

    [searchbox] fix aria error
    * aria error due to having aria label in a child but aria ignore in
      the parent.
    * Created aria none for the non-tabbable items
    
    screenreader still works:
    ui fixed from above screencast: https://screencast.googleplex.com/cast/NjI4NzQ3MzU4MjgwMDg5NnxjNzdhYTNhZi00MA
    
    https: //screencast.googleplex.com/cast/NDcwNjE0NzY1ODM2Njk3Nnw0ZTAyZDNiOC0zYQ
    Bug: b:492539983
    Change-Id: I4d4e4423d0b21b1ebf4c7e4dfefed14c680c95fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7681730
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: James Leung <jamesleung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601633}

--- ui/webui/resources/cr_components/searchbox/searchbox_match.html.ts
+++ ui/webui/resources/cr_components/searchbox/searchbox_match.html.ts
@@ -9,40 +9,41 @@ import type {SearchboxMatchElement} from './searchbox_match.js';
 export function getHtml(this: SearchboxMatchElement) {
   // clang-format off
   return html`<!--_html_template_start_-->
-<div class="container" aria-hidden="true">
-  <div id="focusIndicator" class="${this.getFocusIndicatorCssClass_()}"></div>
-  <cr-searchbox-icon id="icon" .match="${this.match}"></cr-searchbox-icon>
-  <div id="text-container">
-    <span id="tail-suggest-prefix" ?hidden="${!this.tailSuggestPrefix_}">
-      <span id="prefix">${this.tailSuggestPrefix_}</span>
-      <!-- This is equivalent to AutocompleteMatch::kEllipsis which is
-           prepended to the match content in other surfaces-->
-      <span id="ellipsis">...&nbsp;</span>
-    </span>
-    <!-- When a thumbnail is in the searchbox all results should have an
-         ellipsis prepended to the suggestion. -->
-    <span id="ellipsis" ?hidden="${!this.showEllipsis}">...&nbsp;</span>
-    <span id="suggestion">
-      <span id="contents" .innerHTML="${this.contentsHtml_}"></span>
-      <span id="separator" class="dim">${this.separatorText_}</span>
-      <span id="description" .innerHTML="${this.descriptionHtml_}"></span>
-    </span>
-  </div>
+<div class="container">
   <div aria-hidden="true">
-    ${this.match.keywordChipHint ? html`
-      <div id="actions-focus-border">
-        <cr-searchbox-action id="keyword"
-            class="${this.getKeywordCssClass_()}"
-            hint="${this.match.keywordChipHint}"
-            icon-path="//resources/images/icon_search.svg"
-            aria-label="${this.match.keywordChipA11y}"
-            @execute-action="${this.onKeywordExecuteAction_}"
-            tabindex="1">
-        </cr-searchbox-action>
-      </div>
-    ` : ''}
+    <div id="focusIndicator" class="${this.getFocusIndicatorCssClass_()}"></div>
+    <cr-searchbox-icon id="icon" .match="${this.match}"></cr-searchbox-icon>
+    <div id="text-container">
+      <span id="tail-suggest-prefix" ?hidden="${!this.tailSuggestPrefix_}">
+        <span id="prefix">${this.tailSuggestPrefix_}</span>
+        <!-- This is equivalent to AutocompleteMatch::kEllipsis which is
+            prepended to the match content in other surfaces-->
+        <span id="ellipsis">...&nbsp;</span>
+      </span>
+      <!-- When a thumbnail is in the searchbox all results should have an
+          ellipsis prepended to the suggestion. -->
+      <span id="ellipsis" ?hidden="${!this.showEllipsis}">...&nbsp;</span>
+      <span id="suggestion">
+        <span id="contents" .innerHTML="${this.contentsHtml_}"></span>
+        <span id="separator" class="dim">${this.separatorText_}</span>
+        <span id="description" .innerHTML="${this.descriptionHtml_}"></span>
+      </span>
+    </div>
   </div>
-  <div id="actions-container" class="actions container" aria-hidden="true">
+
+  ${this.match.keywordChipHint ? html`
+    <div id="actions-focus-border">
+      <cr-searchbox-action id="keyword"
+          class="${this.getKeywordCssClass_()}"
+          hint="${this.match.keywordChipHint}"
+          icon-path="//resources/images/icon_search.svg"
+          aria-label="${this.match.keywordChipA11y}"
+          @execute-action="${this.onKeywordExecuteAction_}"
+          tabindex="1">
+      </cr-searchbox-action>
+    </div>
+  ` : ''}
+  <div id="actions-container" class="actions container">
     ${this.match.actions.map((item, index) => html`
       <div id="actions-focus-border">
         <cr-searchbox-action id="action"
