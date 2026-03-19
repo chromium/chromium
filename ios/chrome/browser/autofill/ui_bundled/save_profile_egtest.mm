bb9a31c11ab02ca686667369ac729391497488c0
Alexandra Pereira <alexandrasp@google.com>
2026-03-19 07:39:05 -0700

    [iOS] Clean up SaveProfileEGTest by removing deprecated HTTP server
    Remove the dependency on the deprecated ios/web/public/test/http_server
    within the save_profile_egtest.mm file.
    
    Bug: 41311220, 40596517
    Change-Id: I8d8e5eab364e6d090ca1358dfc1c6d943f9513aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7681474
    Commit-Queue: Alexandra Pereira <alexandrasp@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1601961}

--- ios/chrome/browser/autofill/ui_bundled/save_profile_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/save_profile_egtest.mm
@@ -39,7 +39,6 @@
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_test_case.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
-#import "ios/web/public/test/http_server/http_server.h"
 #import "net/test/embedded_test_server/embedded_test_server.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "ui/base/l10n/l10n_util.h"
