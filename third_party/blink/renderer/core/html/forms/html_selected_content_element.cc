19412fd4e28c68d592569c759909a6e90c618f64
Joey Arhar <jarhar@chromium.org>
2026-03-19 09:01:57 -0700

    Calculate selectedcontent disabledness on insertion
    This was suggested here and has been included in the corresponding spec
    PR: https://github.com/whatwg/html/issues/12096#issuecomment-3871096593
    
    This shouldn't affect behavior now that inserting a selectedcontent
    element can't trigger an update of a different selectedcontent element,
    but it still seemed like a good suggestion to me to avoid potential
    issues.
    
    Bug: 458113204
    Change-Id: I25196c1d23b1e169b11d2693c7e2ec3fd4c1a544
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7669200
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1602015}

--- third_party/blink/renderer/core/html/forms/html_selected_content_element.cc
+++ third_party/blink/renderer/core/html/forms/html_selected_content_element.cc
@@ -14,8 +14,6 @@ HTMLSelectedContentElement::HTMLSelectedContentElement(Document& document)
 
 void HTMLSelectedContentElement::CloneContentsFromOptionElement(
     const HTMLOptionElement* option) {
-  // TODO(crbug.com/458113204): This disabled check does not exist in the spec.
-  // It should be added to the spec or removed.
   if (disabled_) {
     return;
   }
@@ -43,11 +41,35 @@ Node::InsertionNotificationRequest HTMLSelectedContentElement::InsertedInto(
     // through all descendant selectedcontent elements within the nearest
     // ancestor select element, which we optimize with a TreeOrderedList which
     // gets updated in SelectedContentElementInserted.
-    HTMLSelectElement* new_nearest_ancestor_select =
-        Traversal<HTMLSelectElement>::FirstAncestor(*this);
-    if (nearest_ancestor_select_ != new_nearest_ancestor_select) {
+    //
+    // The disabled state is also calculated here instead of the post-connection
+    // steps to avoid having time in between insertion and post-insertion where
+    // the element isn't considered disabled yet.
+
+    disabled_ = false;
+    HTMLSelectElement* first_ancestor_select = nullptr;
+    for (auto* ancestor = parentNode(); ancestor;
+         ancestor = ancestor->parentNode()) {
+      if (IsA<HTMLOptionElement>(ancestor) ||
+          IsA<HTMLSelectedContentElement>(ancestor)) {
+        // Putting a <selectedcontent> inside an <option> or another
+        // <selectedcontent> can lead to infinite loops.
+        disabled_ = true;
+      }
+      if (auto* select = DynamicTo<HTMLSelectElement>(ancestor)) {
+        if (first_ancestor_select) {
+          // If there are multiple ancestor selects, then cloning can lead to
+          // infinite loops, so disable this element.
+          disabled_ = true;
+          break;
+        }
+        first_ancestor_select = select;
+      }
+    }
+
+    if (nearest_ancestor_select_ != first_ancestor_select) {
       CHECK(!nearest_ancestor_select_);
-      nearest_ancestor_select_ = new_nearest_ancestor_select;
+      nearest_ancestor_select_ = first_ancestor_select;
       nearest_ancestor_select_->SelectedContentElementInserted(this);
     }
   }
@@ -61,30 +83,7 @@ void HTMLSelectedContentElement::DidNotifySubtreeInsertionsToDocument() {
   // disabled.
   // TODO(crbug.com/40236878): Use a flat tree traversal here.
   if (RuntimeEnabledFeatures::SelectedcontentSpecEnabled()) {
-    // TODO(crbug.com/458113204): Calculate disabled state in InsertedInto
-    // instead of this method.
-    disabled_ = false;
-    HTMLSelectElement* first_ancestor_select = nullptr;
-    for (auto* ancestor = parentNode(); ancestor;
-         ancestor = ancestor->parentNode()) {
-      if (IsA<HTMLOptionElement>(ancestor) ||
-          IsA<HTMLSelectedContentElement>(ancestor)) {
-        // Putting a <selectedcontent> inside an <option> or another
-        // <selectedcontent> can lead to infinite loops.
-        disabled_ = true;
-        break;
-      }
-      if (auto* select = DynamicTo<HTMLSelectElement>(ancestor)) {
-        if (first_ancestor_select) {
-          // If there are multiple ancestor selects, then cloning can lead to
-          // infinite loops, so disable this element.
-          disabled_ = true;
-          break;
-        }
-        first_ancestor_select = select;
-        CHECK_EQ(first_ancestor_select, nearest_ancestor_select_);
-      }
-    }
+    DCHECK_EQ(nearest_ancestor_select_, Traversal<HTMLSelectElement>::FirstAncestor(*this));
 
     if (!disabled_ && nearest_ancestor_select_ &&
         !nearest_ancestor_select_->IsMultiple()) {
