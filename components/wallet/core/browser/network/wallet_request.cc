6bff79ee37a3ade78a772ac3793dfc2857cbec1d
Florian Leimgruber <fleimgruber@google.com>
2026-03-19 03:40:51 -0700

    WalletHttpClient: Add Platform to ClientInfo
    As requested by the server-side team. The proto structure was agreed
    upon with them.
    
    Bug: 478783796
    Change-Id: I9e03b296af504684fbed3eb69895ed3c1417e774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7679257
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1601868}

--- components/wallet/core/browser/network/wallet_request.cc
+++ components/wallet/core/browser/network/wallet_request.cc
@@ -5,9 +5,33 @@
 #include "components/wallet/core/browser/network/wallet_request.h"
 
 #include "base/values.h"
+#include "build/build_config.h"
 #include "components/version_info/version_info.h"
 
 namespace wallet {
+
+namespace {
+
+ClientInfo::ChromeClientInfo::Platform GetPlatform() {
+#if BUILDFLAG(IS_WIN)
+  return ClientInfo::ChromeClientInfo::PLATFORM_WINDOWS;
+#elif BUILDFLAG(IS_MAC)
+  return ClientInfo::ChromeClientInfo::PLATFORM_MACOS;
+#elif BUILDFLAG(IS_CHROMEOS)
+  return ClientInfo::ChromeClientInfo::PLATFORM_CHROMEOS;
+#elif BUILDFLAG(IS_LINUX)
+  return ClientInfo::ChromeClientInfo::PLATFORM_LINUX;
+#elif BUILDFLAG(IS_ANDROID)
+  return ClientInfo::ChromeClientInfo::PLATFORM_ANDROID;
+#elif BUILDFLAG(IS_IOS)
+  return ClientInfo::ChromeClientInfo::PLATFORM_IOS;
+#else
+  // Unsupported platforms like Fuchsia.
+  return ClientInfo::ChromeClientInfo::PLATFORM_UNSPECIFIED;
+#endif
+}
+
+}  // namespace
 
 net::HttpRequestHeaders WalletRequest::GetRequestHeaders() const {
   return net::HttpRequestHeaders();
@@ -16,8 +40,10 @@ net::HttpRequestHeaders WalletRequest::GetRequestHeaders() const {
 // static
 ClientInfo WalletRequest::BuildClientInfo() {
   ClientInfo client_info;
-  client_info.mutable_chrome_client_info()->set_version(
-      version_info::GetVersionNumber());
+  ClientInfo::ChromeClientInfo& chrome_client_info =
+      *client_info.mutable_chrome_client_info();
+  chrome_client_info.set_version(version_info::GetVersionNumber());
+  chrome_client_info.set_platform(GetPlatform());
   return client_info;
 }
 
