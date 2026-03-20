5554ebc7caad54d2e06328be974a46a60afb9211
Kay Lin <kaiyilin@google.com>
2026-03-20 11:39:07 -0700

    Site Search: Add PreferenceListRecyclerView with custom divider
    This CL introduces a new inner class
    PreferenceListDividerItemDecoration, which handles drawing list and
    dividers for recycler view: this custom decoration applies bottom
    offsets and draws dividers for items except the last item
    
    We also remove obsoleted attributes in XML in this change.
    
    Bug: 493756417
    Change-Id: I87f04eaa26b3e10b90149cbdd953f3f223ec7eb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7682547
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Kay Lin <kaiyilin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1602751}

--- chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/common/PreferenceListRecyclerView.java
+++ chrome/browser/search_engines/android/java/src/org/chromium/chrome/browser/search_engines/settings/common/PreferenceListRecyclerView.java
@@ -4,23 +4,72 @@
 
 package org.chromium.chrome.browser.search_engines.settings.common;
 
-import android.content.Context;
-import android.util.AttributeSet;
+import static org.chromium.build.NullUtil.assertNonNull;
 
-import androidx.recyclerview.widget.DividerItemDecoration;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.chromium.build.annotations.NullMarked;
 
+/** Custom RecyclerView that displays a list of items with dividers. */
 @NullMarked
 public class PreferenceListRecyclerView extends RecyclerView {
     public PreferenceListRecyclerView(Context context, AttributeSet attrs) {
         super(context, attrs);
         LinearLayoutManager layoutManager = new LinearLayoutManager(context);
         setLayoutManager(layoutManager);
-        DividerItemDecoration divider =
-                new DividerItemDecoration(context, layoutManager.getOrientation());
-        addItemDecoration(divider);
+        addItemDecoration(new PreferenceListDividerItemDecoration(context));
+    }
+
+    private static class PreferenceListDividerItemDecoration extends ItemDecoration {
+        private static final int[] ATTRS = new int[] {android.R.attr.listDivider};
+        private final Drawable mDivider;
+        private final int mDividerHeight;
+
+        PreferenceListDividerItemDecoration(Context context) {
+            final TypedArray a = context.obtainStyledAttributes(ATTRS);
+            mDivider = assertNonNull(a.getDrawable(0));
+            mDividerHeight = mDivider.getIntrinsicHeight();
+            a.recycle();
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, State state) {
+            int position = parent.getChildAdapterPosition(view);
+            if (position == state.getItemCount() - 1) {
+                outRect.bottom = 0;
+            } else {
+                outRect.bottom = mDividerHeight;
+            }
+        }
+
+        @Override
+        public void onDraw(Canvas c, RecyclerView parent, State state) {
+            int left = parent.getPaddingLeft();
+            int right = parent.getWidth() - parent.getPaddingRight();
+
+            for (int i = 0; i < parent.getChildCount(); i++) {
+                View child = parent.getChildAt(i);
+                int position = parent.getChildAdapterPosition(child);
+
+                if (position == state.getItemCount() - 1) {
+                    continue;
+                }
+
+                LayoutParams params = (LayoutParams) child.getLayoutParams();
+                int top = child.getBottom() + params.bottomMargin;
+                int bottom = top + mDividerHeight;
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(c);
+            }
+        }
     }
 }
