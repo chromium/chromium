0827dfba7dfedd03f37247f0a2fc3112123cc3ff
Ryan Akiyama <ryanakiyama@chromium.org>
2026-03-20 12:43:09 -0700

    [147] [composebox] Refactor Composebox to use a state property
    Original change's description:
    > [composebox] Refactor Composebox to use a state property
    > 
    > This CL replaces the `composebox-initialized` event and callback
    > mechanism with a direct property binding. The New Tab Page (`app.ts`)
    > now sets a state property on the `cr-composebox` element, which triggers
    > the initialization logic within `cr-composebox`. This simplifies the
    > data flow and makes the component more reactive to state changes.
    > 
    > In addition, this CL also fixes a race condition where the contextual
    > entrypoint menu could silently fail to open when attempting to attach a
    > tab. The `openMenuForMultiSelection` method in
    > `contextual_entrypoint_and_menu.ts` was firing too quickly before its
    > internal shadow DOM elements (the #entrypoint button) had fully
    > rendered.
    > 
    > This also fixes the context menu being unable to close after clicking
    > the AI Mode button, which was reverted in this CL:
    > https://chromium-review.git.corp.google.com/c/chromium/src/+/7667904
    > 
    > * Recording: http://screencast/cast/NDYxNTI2NTM3ODYzMTY4MHxiYmI0MzJkNi1lYw
    > 
    > Bug: b:491126593
    > Change-Id: I6308cfc7d8f5e8aa23022bf38f1769124d56a30d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7671201
    > Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    > Commit-Queue: Ryan Akiyama <ryanakiyama@chromium.org>
    > Reviewed-by: Roman Arora <romanarora@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1600664}
    
    Bug: 494545637
    Change-Id: I6308cfc7d8f5e8aa23022bf38f1769124d56a30d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7687738
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7727@{#995}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_menu.ts
+++ ui/webui/resources/cr_components/composebox/contextual_entrypoint_and_menu.ts
@@ -8,6 +8,7 @@ import './contextual_entrypoint_button.js';
 import {I18nMixinLit} from '//resources/cr_elements/i18n_mixin_lit.js';
 import {assert} from '//resources/js/assert.js';
 import {loadTimeData} from '//resources/js/load_time_data.js';
+import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {TabInfo} from '//resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
 import type {InputState} from '//resources/mojo/components/omnibox/composebox/composebox_query.mojom-webui.js';
@@ -89,13 +90,27 @@ export class ContextualEntrypointAndMenuElement extends
   protected accessor enableMultiTabSelection_: boolean =
       loadTimeData.getBoolean('composeboxContextMenuEnableMultiTabSelection');
 
-  constructor() {
-    super();
-  }
+  // TODO(crbug.com/491126593): Explore avoiding/removing this local property.
+  private shouldOpenMenuForMultiSelection_: boolean = false;
 
   openMenuForMultiSelection() {
     if (this.enableMultiTabSelection_) {
-      this.updateComplete.then(this.showMenuAtEntrypoint_.bind(this));
+      this.shouldOpenMenuForMultiSelection_ = true;
+      this.requestUpdate();
+    }
+  }
+
+  override updated(changedProperties: PropertyValues<this>) {
+    super.updated(changedProperties);
+
+    if (this.shouldOpenMenuForMultiSelection_) {
+      const entrypoint =
+             this.$.entrypointButton.shadowRoot.querySelector<HTMLElement>(
+                 '#entrypoint');
+      if (entrypoint) {
+        this.shouldOpenMenuForMultiSelection_ = false;
+        this.showMenuAtEntrypoint_();
+      }
     }
   }
 
