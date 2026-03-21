af83b95d2fd27e481e6c914af214e3540f04d565
Lauren Winston <lwinston@google.com>
2026-03-20 17:18:35 -0700

    [M147] [Images] Disable images toggle when read aloud is playing.
    Original change's description:
    > [Images] Disable images toggle when read aloud is playing.
    >
    > This is consistent with links being disabled when read aloud is
    > playing.
    >
    > This is step 1 to fixing the bug where the read aloud model
    > doesn't properly update when images are toggled on and off.
    >
    > Bug: 494026909
    > Change-Id: I19c4c579be38a38c2195b4e0e64067c9886c3f20
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7685738
    > Commit-Queue: Lauren Winston <lwinston@google.com>
    > Reviewed-by: Kristi Saney <kristislee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1602269}
    
    (cherry picked from commit 883ea76cc1dcd9900e34d095b7c7e7e6edb6060b)
    
    Bug: 494633610,494026909
    Change-Id: I19c4c579be38a38c2195b4e0e64067c9886c3f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7689433
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7727@{#1056}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- chrome/browser/resources/side_panel/read_anything/menus/settings_menu.html.ts
+++ chrome/browser/resources/side_panel/read_anything/menus/settings_menu.html.ts
@@ -20,6 +20,7 @@ export function getHtml(this: SettingsMenuElement) {
           data-index="${index}"
           title="${item.ariaLabel || item.title}"
           aria-label="${item.ariaLabel || item.title}"
+          ?disabled="${item.disabled}"
           aria-haspopup="${item.itemType === SettingsItemType.MENU ?
              'menu' : 'false'}"
           aria-expanded="${this.getAriaExpanded_(item)}"
@@ -39,8 +40,9 @@ export function getHtml(this: SettingsMenuElement) {
             <cr-toggle
               title="${item.ariaLabel || item.title}"
               aria-label="${item.ariaLabel || item.title}"
+              ?disabled="${item.disabled}"
               @click="${this.onMenuItemClick_}"
-              ?checked="${item.enabled || false}"
+              ?checked="${item.checked || false}"
               data-index="${index}">
             </cr-toggle>
         ` : html`
