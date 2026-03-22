ed4f7e54203ef54e91d490d8de63ccf129761d6a
thomas lukaszewicz <tluk@chromium.org>
2026-03-22 15:27:38 -0700

    [bedrock] Migrate handoff_observer.mm away from BrowserListObserver.
    This migration is part of project bedrock to reduce the dependencies on
    Browser and BrowserList. See https://crbug.com/431671320 for more info.
    
    Bug: 459777668
    Change-Id: Ie18e63351051935ad1edbec388f91b7b511bf405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7686811
    Reviewed-by: Qikai Zhong <qikaizhong@microsoft.com>
    Commit-Queue: Tom Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603211}

--- chrome/browser/ui/cocoa/handoff_observer.mm
+++ chrome/browser/ui/cocoa/handoff_observer.mm
@@ -5,9 +5,8 @@
 #include "chrome/browser/ui/cocoa/handoff_observer.h"
 
 #include "base/check.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/web_contents.h"
@@ -15,26 +14,27 @@
 HandoffObserver::HandoffObserver(NSObject<HandoffObserverDelegate>* delegate)
     : delegate_(delegate) {
   DCHECK(delegate_);
-  BrowserList::AddObserver(this);
-  SetActiveBrowser(chrome::FindLastActive());
+  auto* const browser_collection = GlobalBrowserCollection::GetInstance();
+  browser_collection_observation_.Observe(browser_collection);
+  SetActiveBrowser(browser_collection->GetLastActiveBrowser());
 }
 
 HandoffObserver::~HandoffObserver() {
-  BrowserList::RemoveObserver(this);
   SetActiveBrowser(nullptr);
 }
 
-void HandoffObserver::OnBrowserSetLastActive(Browser* browser) {
+void HandoffObserver::OnBrowserActivated(BrowserWindowInterface* browser) {
   SetActiveBrowser(browser);
   [delegate_ handoffContentsChanged:GetActiveWebContents()];
 }
 
-void HandoffObserver::OnBrowserRemoved(Browser* removed_browser) {
+void HandoffObserver::OnBrowserClosed(BrowserWindowInterface* removed_browser) {
   if (active_browser_ != removed_browser) {
     return;
   }
 
-  SetActiveBrowser(chrome::FindLastActive());
+  SetActiveBrowser(
+      GlobalBrowserCollection::GetInstance()->GetLastActiveBrowser());
   [delegate_ handoffContentsChanged:GetActiveWebContents()];
 }
 
@@ -58,20 +58,20 @@ void HandoffObserver::TitleWasSet(content::NavigationEntry* entry) {
   [delegate_ handoffContentsChanged:GetActiveWebContents()];
 }
 
-void HandoffObserver::SetActiveBrowser(Browser* active_browser) {
+void HandoffObserver::SetActiveBrowser(BrowserWindowInterface* active_browser) {
   if (active_browser == active_browser_) {
     return;
   }
 
   if (active_browser_) {
-    active_browser_->tab_strip_model()->RemoveObserver(this);
+    active_browser_->GetTabStripModel()->RemoveObserver(this);
     StopObservingWebContents();
   }
 
   active_browser_ = active_browser;
 
   if (active_browser_) {
-    active_browser_->tab_strip_model()->AddObserver(this);
+    active_browser_->GetTabStripModel()->AddObserver(this);
     content::WebContents* web_contents = GetActiveWebContents();
     if (web_contents) {
       StartObservingWebContents(web_contents);
@@ -94,5 +94,5 @@ content::WebContents* HandoffObserver::GetActiveWebContents() {
     return nullptr;
   }
 
-  return active_browser_->tab_strip_model()->GetActiveWebContents();
+  return active_browser_->GetTabStripModel()->GetActiveWebContents();
 }
