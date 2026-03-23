4a7f34fa8f3a32551d712dd3be4b269c57517c72
Marke Hallowell <wry@chromium.org>
2026-03-23 15:38:40 -0700

    [GlicInvokeFre] Add an fre override to notifyPanelWillOpen.
    In order to support simultaneous independent experiments across
    different FRE flows, the invoke API will be expanded to allow for
    setting an explicit override to the otherwise-determined FRE. This
    change adds the necessary underlying api for Invoke to use to carry this
    out.
    
    Bug: b:493610812,493610823
    NO_IFTTT=False-positives from linter cleanup
    
    Change-Id: Ifa66647a0d785254e38a675c5810ebe16a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7686172
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603748}

--- chrome/browser/glic/host/host.cc
+++ chrome/browser/glic/host/host.cc
@@ -324,7 +324,7 @@ void Host::PanelWillOpen(mojom::InvocationSource invocation_source,
                   std::move(options.prompt_suggestion), options.auto_send,
                   /*skill_to_invoke=*/nullptr,
                   std::move(options.recently_active_conversations),
-                  std::move(options.conversation_info))
+                  std::move(options.conversation_info), options.fre_override)
             : mojom::PanelOpeningData::New(),
         base::BindOnce(
             &Host::PanelWillOpenComplete,
@@ -517,6 +517,7 @@ void Host::SetWebClient(GlicWebClientAccess* web_client) {
     auto conversation_info = mojom::ConversationInfo::New();
 
     bool auto_send = false;
+    mojom::FreOverride fre_override = mojom::FreOverride::kUnspecified;
     if (pending_panel_open_options_) {
       prompt_suggestion =
           std::move(pending_panel_open_options_->prompt_suggestion);
@@ -525,6 +526,7 @@ void Host::SetWebClient(GlicWebClientAccess* web_client) {
       conversation_info =
           std::move(pending_panel_open_options_->conversation_info);
       auto_send = pending_panel_open_options_->auto_send;
+      fre_override = pending_panel_open_options_->fre_override;
       pending_panel_open_options_.reset();
     }
 
@@ -535,7 +537,7 @@ void Host::SetWebClient(GlicWebClientAccess* web_client) {
             *invocation_source_, std::move(prompt_suggestion), auto_send,
             /*skill_to_invoke=*/nullptr,
             std::move(recently_active_conversations),
-            std::move(conversation_info)),
+            std::move(conversation_info), fre_override),
         base::BindOnce(
             &Host::PanelWillOpenComplete,
             // Unretained is safe because web client is owned by `contents_`.
