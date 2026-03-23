3c99c80b5eb6bfffa4368220ed4da39c80bd2421
Georg Neis <neis@chromium.org>
2026-03-22 23:34:44 -0700

    ash: Remove obsolete TestCrosapiEnvironment
    Bug: b:365902693
    Change-Id: I6fcd351627e7cdf5e427e2704555a6f2f42ed04b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7692617
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1603278}

--- chrome/browser/ui/global_media_controls/media_notification_service_unittest.cc
+++ chrome/browser/ui/global_media_controls/media_notification_service_unittest.cc
@@ -44,10 +44,6 @@
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if BUILDFLAG(IS_CHROMEOS)
-#include "chrome/browser/ash/crosapi/test_crosapi_environment.h"
-#endif
-
 namespace mojom {
 using global_media_controls::mojom::DeviceListClient;
 using global_media_controls::mojom::DeviceListHost;
@@ -79,9 +75,6 @@ class MediaNotificationServiceTest : public ChromeRenderViewHostTestHarness {
 
   void SetUp() override {
     ChromeRenderViewHostTestHarness::SetUp();
-#if BUILDFLAG(IS_CHROMEOS)
-    crosapi_environment_.SetUp();
-#endif
     media_router::ChromeMediaRouterFactory::GetInstance()->SetTestingFactory(
         profile(), base::BindRepeating(&media_router::MockMediaRouter::Create));
     service_ = std::make_unique<MediaNotificationService>(profile(), false);
@@ -90,9 +83,6 @@ class MediaNotificationServiceTest : public ChromeRenderViewHostTestHarness {
   void TearDown() override {
     SimulateCloseDialog();
     service_.reset();
-#if BUILDFLAG(IS_CHROMEOS)
-    crosapi_environment_.TearDown();
-#endif
     ChromeRenderViewHostTestHarness::TearDown();
   }
 
@@ -185,12 +175,6 @@ class MediaNotificationServiceTest : public ChromeRenderViewHostTestHarness {
 
  private:
   std::unique_ptr<MediaNotificationService> service_;
-#if BUILDFLAG(IS_CHROMEOS)
-  TestingProfileManager testing_profile_manager_{
-      TestingBrowserProcess::GetGlobal()};
-  crosapi::TestCrosapiEnvironment crosapi_environment_{
-      &testing_profile_manager_};
-#endif
 };
 
 // This class enables the features for starting/stopping cast sessions from
