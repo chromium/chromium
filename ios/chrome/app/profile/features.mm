88e9bc079ca503894237304085ef4cd41008a5e0
Justin Cohen <justincohen@google.com>
2026-03-24 05:05:57 -0700

    ios: Enable kDisableCookieStoreIOSFlushOnBackgrounding by default
    This CL enables kDisableCookieStoreIOSFlushOnBackgrounding by default.
    The feature flag was introduced to verify if flushing the cookie store
    on backgrounding is redundant and to potentially improve performance
    and battery life.
    
    Bug: 475254652
    Change-Id: I52e30a3bb52697c3ecc686e99c508c32e189f2c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7694332
    Auto-Submit: Justin Cohen <justincohen@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1604034}

--- ios/chrome/app/profile/features.mm
+++ ios/chrome/app/profile/features.mm
@@ -8,4 +8,4 @@ BASE_FEATURE(kLogApplicationStorageSizeMetrics,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDisableCookieStoreIOSFlushOnBackgrounding,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
