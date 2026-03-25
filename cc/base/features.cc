a7300be56cff6193dc4c02367d321654c74c3d9c
Petr Čermák <petrcermak@chromium.org>
2026-03-25 07:48:24 -0700

    [scroll jank v4] Order events based on their arrival in the compositor by default
    This change enables the
    kOrderScrollJankV4EventMetricsByArrivedInRendererCompositor feature by
    default. This means that the scroll jank v4 metric will always order
    scroll events within a single frame based on the timestamps of their
    arrival in the renderer compositor (instead of their input generation
    timestamps). This provides a more accurate sequence of events as seen by
    the compositor's scheduling and processing logic.
    
    Bug: b:480030877
    Test: cc_unittests
    Change-Id: Idb19578fe4401194ac819983266313cf7be892ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7695458
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Petr Cermak <petrcermak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1604804}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -241,7 +241,7 @@ BASE_FEATURE_PARAM(double,
                    0.2);
 
 BASE_FEATURE(kOrderScrollJankV4EventMetricsByArrivedInRendererCompositor,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kManualBeginFrame, base::FEATURE_DISABLED_BY_DEFAULT);
 
