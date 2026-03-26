79f7d16abda1915c9998ec6f0baa9f571d3736f5
Tomasz Lichoń <lichon@google.com>
2026-03-26 06:08:58 -0700

    [TDR] Remove TODOs linked to issue 41240161
    This CL was generated by an AI agent and verified by the CL owner before
    sending for review. The TODOs were identified as stale by TODO Removal
    Agent (http://go/dead-todo-removal-agent).
    * Detailed reasoning can be found in the CL comments.
    * How to review this CL: http://go/tdr-review-instructions
    * To opt-out from future reviews: http://go/tdr-opt-out.
    
    The associated bug crbug.com/41240161 was marked as OBSOLETE, and a
    comment within the bug explains that implementing the 'appearance'
    feature is not possible on macOS due to platform API limitations. To
    preserve this important context for future developers, the TODO can be
    rephrased into a descriptive comment.
    
    Change-Id: Ib412a688ea5f10149190c2fcbc79ee28cc38c075
    Bug: 41240161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7487333
    Auto-Submit: Tomasz Lichoń <lichon@google.com>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Tomasz Lichoń <lichon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1605435}

--- device/bluetooth/bluetooth_low_energy_adapter_apple.mm
+++ device/bluetooth/bluetooth_low_energy_adapter_apple.mm
@@ -493,7 +493,8 @@ void BluetoothLowEnergyAdapterApple::LowEnergyDeviceUpdated(
         device_mac->GetAddress(), device_name_opt,
         local_name == nil ? std::nullopt : local_name_opt, rssi,
         tx_power == nil ? std::nullopt : std::make_optional(clamped_tx_power),
-        std::nullopt, /* TODO(crbug.com/41240161) Implement appearance */
+        std::nullopt,  // macOS does not provide device appearance. See
+                       // crbug.com/41240161.
         advertised_uuids, service_data_map, manufacturer_data_map);
   }
 
