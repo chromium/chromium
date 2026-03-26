11befc2875969ec963efbecb718f401946298f11
James Cook <jamescook@chromium.org>
2026-03-26 12:47:44 -0700

    extensions: Fix search API navigation on desktop Android
    An earlier CL (http://crrev.com/c/7698693) enabled some search API
    tests and attempted to fix navigation triggered by the API. It
    incorrectly assumed that calls to ExtensionTabUtil::NavigateToURL()
    took a WebContents in order to use it for content (e.g. via
    NavigateParams::contents_to_insert). That isn't supported yet on
    desktop Android.
    
    However, the NavigateToURL() WebContents is *actually* the "source
    contents" for the navigation, and desktop Android does support
    that, as long as the asynchronous version of Navigate() is used.
    
    Switch to async Navigate() on all platforms for consistency and
    update the relevant tests.
    
    Bug: 442873371, 496596048
    Change-Id: Ie76247943fd16fade3955c855579b46f4ceeddbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702234
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1605719}

--- chrome/test/data/extensions/api_test/search/query/normal/background.js
+++ chrome/test/data/extensions/api_test/search/query/normal/background.js
@@ -71,7 +71,6 @@ chrome.test.runTests([
 
   // Display results in new window if said disposition is provided.
   async function QueryPopulatedDispositionNewWindow() {
-    const isAndroid = (await chrome.runtime.getPlatformInfo()).os == 'android';
     chrome.windows.getAll({}, (initialWindows) => {
       const initialWindowIds = initialWindows.map(window => window.id);
       Promise
@@ -81,17 +80,10 @@ chrome.test.runTests([
               chrome.search.query(
                   {text: SEARCH_WORDS, disposition: 'NEW_WINDOW'}, () => {
                     chrome.windows.getAll({}, (windows) => {
-                      // TODO(crbug.com/477944342): Desktop Android does not yet
-                      // support navigating to a new window with an existing web
-                      // contents.
-                      if (isAndroid) {
-                        assertEq(windows.length, initialWindowIds.length);
-                      } else {
-                        const window = windows.find(
+                      const window = windows.find(
                           window => !initialWindowIds.includes(window.id));
-                        assertEq(windows.length, initialWindowIds.length + 1);
-                        assertTrue(!!window);
-                      }
+                      assertEq(windows.length, initialWindowIds.length + 1);
+                      assertTrue(!!window);
                       resolve();
                     });
                   });
