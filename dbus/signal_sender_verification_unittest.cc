af687bc5e07d5b8c5bcb90808ba57495546a4347
Alexei Svitkine <asvitkine@chromium.org>
2026-03-26 06:04:03 -0700

    Cleanup: Remove histogram macros header includes in dbus
    Bug: None
    Change-Id: I50876285d6abbef629c425adb72ed15db4a85f74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7700161
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1605431}

--- dbus/signal_sender_verification_unittest.cc
+++ dbus/signal_sender_verification_unittest.cc
@@ -8,7 +8,6 @@
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/message_loop/message_pump_type.h"
-#include "base/metrics/histogram_macros.h"
 #include "base/metrics/histogram_samples.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
