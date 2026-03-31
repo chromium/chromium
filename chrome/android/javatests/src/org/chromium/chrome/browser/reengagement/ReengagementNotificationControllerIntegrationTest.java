e6a98ba49850255b3b51d256dd7b48fc7227a9c4
Xinyi Ji <xinyiji@chromium.org>
2026-03-30 17:31:25 -0700

    Revert "[gardening] Restrict failing test ReengagementNotificationControllerIntegrationTest#testReengagementActivity"
    This reverts commit 5fbbb5219f30cd270cf99472e067c9fd5c4d1dbd.
    
    Reason for revert: This seems enable the test on desktop instead.
    
    Original change's description:
    > [gardening] Restrict failing test ReengagementNotificationControllerIntegrationTest#testReengagementActivity
    >
    > This is a follow up of CL
    > https://chromium-review.git.corp.google.com/c/chromium/src/+/7712193 to
    > only disable this test on desktop.
    >
    > Bug: 497812812
    > Change-Id: I84c215a828ab8af65d689427e3c706182755b41a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712245
    > Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    > Owners-Override: Xinyi Ji <xinyiji@chromium.org>
    > Reviewed-by: Evan Luo <evanluo@google.com>
    > Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    > Reviewed-by: Brandon Wylie <wylieb@google.com>
    > Reviewed-by: Dan Polanco <polardz@google.com>
    > Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1607360}
    
    Bug: 497812812
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1e0e1d2e4054f1ac6fee4eb89fa59a850ac400e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7713838
    Owners-Override: Xinyi Ji <xinyiji@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607502}

--- chrome/android/javatests/src/org/chromium/chrome/browser/reengagement/ReengagementNotificationControllerIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/reengagement/ReengagementNotificationControllerIntegrationTest.java
@@ -40,7 +40,6 @@ import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
-import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.browser.app.reengagement.ReengagementActivity;
 import org.chromium.chrome.browser.customtabs.CustomTabActivityTestRule;
@@ -63,7 +62,6 @@ import org.chromium.components.feature_engagement.EventConstants;
 import org.chromium.components.feature_engagement.FeatureConstants;
 import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.content_public.common.ContentUrlConstants;
-import org.chromium.ui.base.DeviceFormFactor;
 
 /** Integration tests for {@link ReengagementNotificationController}. */
 @RunWith(ChromeJUnit4ClassRunner.class)
@@ -275,7 +273,7 @@ public class ReengagementNotificationControllerIntegrationTest {
 
     @Test
     @MediumTest
-    @Restriction(DeviceFormFactor.DESKTOP) // Flaky on desktop crbug.com/497812812
+    @DisabledTest(message = "https://crbug.com/497812812")
     public void testReengagementActivity() throws Exception {
         WebPageStation blankPage = mTabbedActivityTestRule.startOnBlankPage();
         int initialTabCount =
