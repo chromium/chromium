0966c32434c4cd539b1193316f5ed28577db4995
Hitarth Kothari <hitarthkothari@google.com>
2026-03-31 08:38:53 -0700

    [PreviewTab] Disable context menu
    This cl disables the context menu for preview tabs only.
    
    Bug: 496874765
    Change-Id: If6fb311d736d36e46d5a26131d217f01ee12d5a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7713579
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1607884}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -701,7 +701,7 @@ BASE_FEATURE(kEmptyTabListAnimationKillSwitch, base::FEATURE_ENABLED_BY_DEFAULT)
 BASE_FEATURE(kEnableAndroidSidePanel, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableAndroidSidePanelDevFeature, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableBrowserWindowInterfaceForCustomTabActivity, base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kEnableContextMenuForPreviewTab, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableContextMenuForPreviewTab, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableEscapeHandlingForSecondaryActivities, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableSwipeToSwitchPane, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableToolbarPositioningInResizeMode, base::FEATURE_ENABLED_BY_DEFAULT);
