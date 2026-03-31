1d2db9bf579f06e7e4607ace82b080a5127c83f3
Takashi Nakayama <tnak@chromium.org>
2026-03-30 19:23:22 -0700

    [Gardener] Disable ChunkedPolicyPrefsTest.PolicyToPrefsMapping on ChromeOS
    This test is failing on the builder `linux-chromeos-chrome`.
    
    Bug: 497928956
    Change-Id: I7af1c0ace07290227e9ac12c2c8b5216afddda52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7713619
    Auto-Submit: Takashi Nakayama <tnak@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    Owners-Override: Takashi Nakayama <tnak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607562}

--- chrome/browser/policy/policy_prefs_browsertest.cc
+++ chrome/browser/policy/policy_prefs_browsertest.cc
@@ -171,7 +171,13 @@ class ChunkedPolicyPrefsTest : public PolicyPrefsTest,
 // failure/flakiness.
 // IMPORTANT: Please add hendrich@chromium.org on any related bugs when
 // disabling this test.
-IN_PROC_BROWSER_TEST_P(ChunkedPolicyPrefsTest, PolicyToPrefsMapping) {
+// TODO(crbug.com/497928956): Re-enable this test.
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_PolicyToPrefsMapping DISABLED_PolicyToPrefsMapping
+#else
+#define MAYBE_PolicyToPrefsMapping PolicyToPrefsMapping
+#endif
+IN_PROC_BROWSER_TEST_P(ChunkedPolicyPrefsTest, MAYBE_PolicyToPrefsMapping) {
   base::ScopedAllowBlockingForTesting allow_blocking;
 
 #if !BUILDFLAG(IS_CHROMEOS)
