2e3a535512700ff4b1e6081ac7d06387951e1d4f
Takashi Sakamoto <tasak@google.com>
2026-03-30 19:03:37 -0700

    Block PrepareScript() while a document state is preserving-atomic-move-in-progress.
    Bug: 496292089
    Change-Id: I1b357a0dc34874e5e3795ea54d9dd1da2b584ac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7701234
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607555}

--- third_party/blink/renderer/core/html/html_script_element.cc
+++ third_party/blink/renderer/core/html/html_script_element.cc
@@ -86,7 +86,10 @@ bool HTMLScriptElement::HasLegalLinkAttribute(const QualifiedName& name) const {
 
 void HTMLScriptElement::ChildrenChanged(const ChildrenChange& change) {
   HTMLElement::ChildrenChanged(change);
-  loader_->ChildrenChanged(change);
+
+  if (!GetDocument().StatePreservingAtomicMoveInProgress()) {
+    loader_->ChildrenChanged(change);
+  }
 
   // We'll record whether the script element children were ever changed by
   // the API (as opposed to the parser).
