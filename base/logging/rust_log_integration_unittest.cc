a154bed06baf2e21b22c658f03fe338602b9333e
Phillis Tang <phillis@chromium.org>
2026-03-31 11:03:59 -0700

    [Gardener] Disable RustLogIntegrationTestWithoutMocking.Panic on windows
    Test is failing on win-chrome.
    
    Bug: 497896152
    Change-Id: I95e67b41a3e927901794b52cd064cdbad7f806fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7717820
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Owners-Override: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Owners-Override: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1607979}

--- base/logging/rust_log_integration_unittest.cc
+++ base/logging/rust_log_integration_unittest.cc
@@ -76,8 +76,10 @@ TEST_F(RustLogIntegrationTest, MAYBE_Placeholders) {
 }
 
 // TODO(crbug.com/374023535): Logging does not work in component builds.
-// TODO(crbug.com/497896152): Re-enable this test on Linux and ChromeOS.
-#if defined(COMPONENT_BUILD) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+// TODO(crbug.com/497896152): Re-enable this test on Linux and ChromeOS and
+// Windows.
+#if defined(COMPONENT_BUILD) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_WIN)
 #define MAYBE_Panic DISABLED_Panic
 #else
 #define MAYBE_Panic Panic
