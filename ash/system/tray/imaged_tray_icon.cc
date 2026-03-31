d0beb44838726c16e391bee773d35fc30513744b
Zoraiz Naeem <zoraiznaeem@google.com>
2026-03-31 10:16:35 -0700

    [M147] Add session-based visibility to ImagedTrayIcon
    This CL introduces a mechanism to control the visibility of
    ImagedTrayIcon based on the session state.
    
    A new IconVisibilityHandler class is added to ImagedTrayIcon. It
    observes session state changes and updates the icon's visibility based
    on a callback provided by the client.
    
    StatusAreaWidget is updated to use this new functionality to hide custom
    tray icons when the user session is blocked (e.g., on the lock screen).
    
    (cherry picked from commit c80d3e7ccc36480646ff7a0c51093e8f0c768f41)
    
    Bug: b:495887156
    Change-Id: Iea0515dd30be833429dffd556ff329e032ca03c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7706661
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1607541}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712964
    Cr-Commit-Position: refs/branch-heads/7727@{#2001}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- ash/system/tray/imaged_tray_icon.cc
+++ ash/system/tray/imaged_tray_icon.cc
@@ -4,11 +4,15 @@
 
 #include "ash/system/tray/imaged_tray_icon.h"
 
+#include <memory>
 #include <string>
 #include <utility>
 
 #include "ash/constants/tray_background_view_catalog.h"
+#include "ash/public/cpp/session/session_observer.h"
+#include "ash/session/session_controller_impl.h"
 #include "ash/shelf/shelf.h"
+#include "ash/shell.h"
 #include "ash/system/tray/tray_background_view.h"
 #include "ash/system/tray/tray_bubble_view.h"
 #include "ash/system/tray/tray_container.h"
@@ -19,13 +23,56 @@
 
 namespace ash {
 
+////////////////////////////////////////////////////////////////////////////////
+// ImagedTrayIcon::IconVisibilityHandler
+
+// A helper class that observes session state changes and updates the
+// visibility of the tray icon based on a provided callback.
+class ImagedTrayIcon::IconVisibilityHandler : public SessionObserver {
+ public:
+  explicit IconVisibilityHandler(ImagedTrayIcon* tray_icon)
+      : tray_icon_(tray_icon) {
+    CHECK(tray_icon_);
+  }
+
+  IconVisibilityHandler(const IconVisibilityHandler&) = delete;
+  IconVisibilityHandler& operator=(const IconVisibilityHandler&) = delete;
+
+  ~IconVisibilityHandler() override = default;
+
+  void set_visibility_callback(IconVisibilityCallback callback) {
+    visibility_callback_ = std::move(callback);
+    if (!visibility_callback_.is_null()) {
+      tray_icon_->SetVisiblePreferred(visibility_callback_.Run(
+          Shell::Get()->session_controller()->GetSessionState()));
+    }
+  }
+
+  // SessionObserver:
+  void OnSessionStateChanged(session_manager::SessionState state) override {
+    if (!visibility_callback_.is_null()) {
+      tray_icon_->SetVisiblePreferred(visibility_callback_.Run(state));
+    }
+  }
+
+ private:
+  const raw_ptr<ImagedTrayIcon> tray_icon_;
+  IconVisibilityCallback visibility_callback_;
+  ScopedSessionObserver session_observer_{this};
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// ImagedTrayIcon
+
 ImagedTrayIcon::ImagedTrayIcon(Shelf* shelf,
                                const ui::ImageModel& image_model,
                                const std::u16string& tooltip,
                                const TrayBackgroundViewCatalogName catalog_name)
     : TrayBackgroundView(shelf,
                          catalog_name,
-                         RoundedCornerBehavior::kAllRounded) {
+                         RoundedCornerBehavior::kAllRounded),
+      session_visibility_handler_(
+          std::make_unique<IconVisibilityHandler>(this)) {
   auto image_view =
       views::Builder<views::ImageView>()
           .SetHorizontalAlignment(views::ImageView::Alignment::kCenter)
@@ -39,6 +86,11 @@ ImagedTrayIcon::ImagedTrayIcon(Shelf* shelf,
 }
 
 ImagedTrayIcon::~ImagedTrayIcon() = default;
+
+void ImagedTrayIcon::set_icon_visibility_callback(
+    IconVisibilityCallback callback) {
+  session_visibility_handler_->set_visibility_callback(std::move(callback));
+}
 
 void ImagedTrayIcon::ClickedOutsideBubble(const ui::LocatedEvent& event) {}
 void ImagedTrayIcon::UpdateTrayItemColor(bool is_active) {}
--- ash/system/tray/imaged_tray_icon.h
+++ ash/system/tray/imaged_tray_icon.h
@@ -5,6 +5,7 @@
 #ifndef ASH_SYSTEM_TRAY_IMAGED_TRAY_ICON_H_
 #define ASH_SYSTEM_TRAY_IMAGED_TRAY_ICON_H_
 
+#include <memory>
 #include <string>
 
 #include "ash/ash_export.h"
@@ -29,6 +30,9 @@ class ASH_EXPORT ImagedTrayIcon : public TrayBackgroundView {
   METADATA_HEADER(ImagedTrayIcon, TrayBackgroundView)
 
  public:
+  using IconVisibilityCallback =
+      base::RepeatingCallback<bool(session_manager::SessionState state)>;
+
   ImagedTrayIcon(Shelf* shelf,
                  const ui::ImageModel& image_model,
                  const std::u16string& tooltip,
@@ -38,6 +42,10 @@ class ASH_EXPORT ImagedTrayIcon : public TrayBackgroundView {
   ImagedTrayIcon& operator=(const ImagedTrayIcon&) = delete;
 
   ~ImagedTrayIcon() override;
+
+  // Sets a callback that is run when the session state changes. The callback
+  // should return true if the icon should be visible in the given state.
+  void set_icon_visibility_callback(IconVisibilityCallback callback);
 
   // TrayBackgroundView:
   void ClickedOutsideBubble(const ui::LocatedEvent& event) override;
@@ -49,7 +57,11 @@ class ASH_EXPORT ImagedTrayIcon : public TrayBackgroundView {
   views::ImageView* image_view() { return image_view_; }
 
  private:
+  class IconVisibilityHandler;
+
   raw_ptr<views::ImageView> image_view_;
+
+  std::unique_ptr<IconVisibilityHandler> session_visibility_handler_;
 };
 
 }  // namespace ash
