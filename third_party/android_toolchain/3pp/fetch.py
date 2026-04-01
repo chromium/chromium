8bdf903754be15c101fa19bc8105461ccdb322b1
Sam Maier <smaier@google.com>
2026-04-01 14:47:12 -0700

    Package latest NDK canaries
    Change-Id: Ic84e479a50149cbe33281977070437626a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711703
    Commit-Queue: Sam Maier <smaier@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sam Maier <smaier@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608785}

--- third_party/android_toolchain/3pp/fetch.py
+++ third_party/android_toolchain/3pp/fetch.py
@@ -4,47 +4,53 @@
 # found in the LICENSE file.
 
 import argparse
-import hashlib
 import json
-import pathlib
+import os
+import re
+import urllib.request
 
-
-# Update this when upgrading NDK.
-_URL = "https://dl.google.com/android/repository/android-ndk-r28-linux.zip"
 
 def do_latest():
-  # Change version time this file changes.
-  md5 = hashlib.md5()
-  md5.update(pathlib.Path(__file__).read_bytes())
+    data = json.load(
+        urllib.request.urlopen(
+            'https://ci.android.com/builds/branches/aosp-master-ndk/status.json'
+        ))
+    for t in data['targets']:
+        if t['name'] == 'linux':
+            print(t['last_known_good_build'])
+            return
 
-  this_dir = pathlib.Path(__file__).parent
-  # Trigger on changes to other files in this directory.
-  md5.update((this_dir / '3pp.pb').read_bytes())
-  md5.update((this_dir / 'install.sh').read_bytes())
-  print(md5.hexdigest())
 
+def get_url():
+    v = os.environ['_3PP_VERSION']
+    html_url = (f'https://ci.android.com/builds/submitted/{v}/linux/latest/'
+                f'android-ndk-{v}-linux-x86_64.zip')
+    with urllib.request.urlopen(html_url) as response:
+        html = response.read().decode('utf-8')
 
-def do_get_url():
-  partial_manifest = {
-    'url': [_URL],
-    'ext': '.zip',
-  }
-  print(json.dumps(partial_manifest))
+    # Extract the artifactUrl from the JSVariables in the HTML.
+    match = re.search(r'"artifactUrl":("[^"]+")', html)
+    if not match:
+        raise Exception(f'Could not find artifactUrl in HTML at {html_url}')
+
+    # Using json.loads on the quoted string will handle Unicode escapes like
+    # \u0026 automatically.
+    artifact_url = json.loads(match.group(1))
+
+    partial_manifest = {'url': [artifact_url], 'ext': '.zip'}
+    print(json.dumps(partial_manifest))
 
 
 def main():
-  ap = argparse.ArgumentParser()
-  sub = ap.add_subparsers(required=True)
+    ap = argparse.ArgumentParser()
+    sub = ap.add_subparsers(required=True)
 
-  latest = sub.add_parser("latest")
-  latest.set_defaults(func=do_latest)
+    sub.add_parser('latest').set_defaults(func=do_latest)
+    sub.add_parser('get_url').set_defaults(func=get_url)
 
-  download = sub.add_parser("get_url")
-  download.set_defaults(func=lambda: do_get_url())
-
-  opts = ap.parse_args()
-  opts.func()
+    opts = ap.parse_args()
+    opts.func()
 
 
 if __name__ == '__main__':
-  main()
+    main()
