268d769cfcdb600fa211809040d60b3a8527a9b3
Boris Sazonov <bsazonov@chromium.org>
2026-04-01 06:43:44 -0700

    [Signin] Remove AccessTokenRestrictionTest tests for kSignedIn
    OAuth2ScopeRestriction::kSignedIn is the default behavior, so testing
    a small subset of scopes for this value doesn't add any value. This
    also impedes making these scopes private, so it's easier to just
    remove those tests altogether.
    
    Bug: None
    Change-Id: Ib240859812efda76645a49ee629c2eb3aa63e5b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7717960
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1608484}

--- components/signin/public/identity_manager/access_token_restriction_unittest.cc
+++ components/signin/public/identity_manager/access_token_restriction_unittest.cc
@@ -26,38 +26,11 @@ using signin::OAuth2ScopeRestriction;
 // clang-format off
 const AccessTokenRestrictionTestParam kTestParams[] = {
  // keep-sorted start
- {GaiaConstants::kAccountsReauthOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
  {GaiaConstants::kAnyApiOAuth2Scope, OAuth2ScopeRestriction::kPrivilegedOAuth2Consumer},
- {GaiaConstants::kCalendarReadOnlyOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kChromeSafeBrowsingOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kChromeSyncOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kChromeSyncSupervisedOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kClassifyUrlKidPermissionOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kCloudSearchQueryOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kCryptAuthOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kDiscoveryEngineCompleteQueryOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kDriveReadOnlyOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kFCMOAuthScope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kFeedOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kGoogleTalkOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
  {GaiaConstants::kGoogleUserInfoEmail, OAuth2ScopeRestriction::kNoRestriction},
  {GaiaConstants::kGoogleUserInfoProfile, OAuth2ScopeRestriction::kNoRestriction},
- {GaiaConstants::kKAnonymityServiceOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kKidFamilyReadonlyOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kKidManagementPrivilegedOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kKidsSupervisionSetupChildOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
  {GaiaConstants::kOAuth1LoginScope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kOptimizationGuideServiceGetHintsOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kOptimizationGuideServiceModelExecutionOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kParentApprovalOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kPasskeysEnclaveOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kPasswordsLeakCheckOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kPaymentsOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kPhotosModuleImageOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kPhotosModuleOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kProgrammaticChallengeOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
  {GaiaConstants::kSecureConnectOAuth2Scope, OAuth2ScopeRestriction::kNoRestriction},
- {GaiaConstants::kSupportContentOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
  // keep-sorted end
  {GaiaConstants::kDeviceManagementServiceOAuth,
 #if BUILDFLAG(IS_ANDROID)
@@ -66,23 +39,6 @@ const AccessTokenRestrictionTestParam kTestParams[] = {
   OAuth2ScopeRestriction::kSignedIn
 #endif
  },
-#if BUILDFLAG(IS_CHROMEOS)
- // keep-sorted start
- {GaiaConstants::kAssistantOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kAuditRecordingOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kCastBackdropOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kClearCutOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kContactsOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kExperimentsAndConfigsOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kGCMGroupServerOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kNearbyDevicesOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kNearbyPresenceOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kNearbyShareOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kPeopleApiReadOnlyOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kPhotosOAuth2Scope, OAuth2ScopeRestriction::kSignedIn},
- {GaiaConstants::kTachyonOAuthScope, OAuth2ScopeRestriction::kSignedIn},
- // keep-sorted end
-#endif  // BUILDFLAG(IS_CHROMEOS)
 #if !BUILDFLAG(IS_FUCHSIA)
 #if BUILDFLAG(ENABLE_PDF_SAVE_TO_DRIVE)
  {GaiaConstants::kDriveOAuth2Scope, OAuth2ScopeRestriction::kNoRestriction},
