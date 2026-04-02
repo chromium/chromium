8da650b0a774971a34e4849e1867474182083f00
Hailey Wang <haileywang@google.com>
2026-04-02 16:22:30 -0700

    [Glic] Fix some issues with the MTB entry point
    1. Copyright prevent the json file from parsing correctly.
    2. Lottie spinner is not tinted properly.
    
    Bug: 485624827
    Change-Id: I7b779d59fdc48950dbf85f5208fab021aca2accd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7712933
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609552}

--- chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicToolbarButtonController.java
+++ chrome/browser/glic/android/java/src/org/chromium/chrome/browser/glic/GlicToolbarButtonController.java
@@ -5,7 +5,10 @@
 package org.chromium.chrome.browser.glic;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.view.View;
@@ -15,6 +18,9 @@ import androidx.appcompat.content.res.AppCompatResources;
 
 import com.airbnb.lottie.LottieCompositionFactory;
 import com.airbnb.lottie.LottieDrawable;
+import com.airbnb.lottie.LottieProperty;
+import com.airbnb.lottie.model.KeyPath;
+import com.airbnb.lottie.value.LottieValueCallback;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
@@ -83,19 +89,25 @@ public class GlicToolbarButtonController extends BaseButtonDataProvider
         mToggleGlicCallback = toggleGlicCallback;
         mTrackerSupplier = trackerSupplier;
         mDefaultSpec = mButtonData.getButtonSpec();
-        mReviewSpec =
-                new ButtonSpec(
-                        mDefaultSpec.getDrawable(),
-                        this,
-                        mDefaultSpec.getOnLongClickListener(),
-                        mDefaultSpec.getContentDescription(),
-                        mDefaultSpec.getSupportsTinting(),
-                        mDefaultSpec.getIphCommandBuilder(),
-                        mDefaultSpec.getButtonVariant(),
-                        R.string.glic_button_status_review,
-                        mDefaultSpec.getHoverTooltipTextId(),
-                        mDefaultSpec.hasErrorBadge());
+        mReviewSpec = createReviewSpec();
+        mWorkingSpec = createWorkingSpec(context);
+    }
 
+    private ButtonSpec createReviewSpec() {
+        return new ButtonSpec(
+                mDefaultSpec.getDrawable(),
+                this,
+                mDefaultSpec.getOnLongClickListener(),
+                mDefaultSpec.getContentDescription(),
+                mDefaultSpec.getSupportsTinting(),
+                mDefaultSpec.getIphCommandBuilder(),
+                mDefaultSpec.getButtonVariant(),
+                R.string.glic_button_status_review,
+                mDefaultSpec.getHoverTooltipTextId(),
+                mDefaultSpec.hasErrorBadge());
+    }
+
+    private ButtonSpec createWorkingSpec(Context context) {
         // TODO(haileywang): Handle other button states.
         LottieDrawable lottieDrawable = new LottieDrawable();
         LottieCompositionFactory.fromRawRes(context, R.raw.glic_spinner)
@@ -106,25 +118,64 @@ public class GlicToolbarButtonController extends BaseButtonDataProvider
                             lottieDrawable.playAnimation();
                         });
         Drawable sparkIcon = AppCompatResources.getDrawable(context, R.drawable.ic_spark_24dp);
-        LayerDrawable layerDrawable = new LayerDrawable(new Drawable[] {lottieDrawable, sparkIcon});
+        LayerDrawable layerDrawable =
+                new LayerDrawable(new Drawable[] {lottieDrawable, sparkIcon}) {
+                    private @Nullable ColorStateList mTintList;
+
+                    @Override
+                    public void setTintList(@Nullable ColorStateList tint) {
+                        super.setTintList(tint);
+                        mTintList = tint;
+                        updateLottieTint();
+                    }
+
+                    @Override
+                    protected boolean onStateChange(int[] state) {
+                        boolean changed = super.onStateChange(state);
+                        if (updateLottieTint()) {
+                            changed = true;
+                        }
+                        return changed;
+                    }
+
+                    @Override
+                    public boolean isStateful() {
+                        return (mTintList != null && mTintList.isStateful()) || super.isStateful();
+                    }
+
+                    private boolean updateLottieTint() {
+                        if (mTintList != null) {
+                            int color =
+                                    mTintList.getColorForState(
+                                            getState(), mTintList.getDefaultColor());
+                            lottieDrawable.addValueCallback(
+                                    new KeyPath("**"),
+                                    LottieProperty.COLOR_FILTER,
+                                    new LottieValueCallback<>(
+                                            new PorterDuffColorFilter(
+                                                    color, PorterDuff.Mode.SRC_IN)));
+                            return true;
+                        }
+                        return false;
+                    }
+                };
         float density = context.getResources().getDisplayMetrics().density;
         // Adjust sizes of the spark and spinner.
         int sparkInset = Math.round(2 * density);
         int spinnerInset = Math.round(-10 * density);
         layerDrawable.setLayerInset(0, spinnerInset, spinnerInset, spinnerInset, spinnerInset);
         layerDrawable.setLayerInset(1, sparkInset, sparkInset, sparkInset, sparkInset);
-        mWorkingSpec =
-                new ButtonSpec(
-                        layerDrawable,
-                        this,
-                        mDefaultSpec.getOnLongClickListener(),
-                        mDefaultSpec.getContentDescription(),
-                        /* supportsTinting= */ true,
-                        mDefaultSpec.getIphCommandBuilder(),
-                        mDefaultSpec.getButtonVariant(),
-                        mDefaultSpec.getActionChipLabelResId(),
-                        mDefaultSpec.getHoverTooltipTextId(),
-                        mDefaultSpec.hasErrorBadge());
+        return new ButtonSpec(
+                layerDrawable,
+                this,
+                mDefaultSpec.getOnLongClickListener(),
+                mDefaultSpec.getContentDescription(),
+                /* supportsTinting= */ true,
+                mDefaultSpec.getIphCommandBuilder(),
+                mDefaultSpec.getButtonVariant(),
+                mDefaultSpec.getActionChipLabelResId(),
+                mDefaultSpec.getHoverTooltipTextId(),
+                mDefaultSpec.hasErrorBadge());
     }
 
     @Override
