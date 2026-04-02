1e063def662fcdb630e887c299cb1d000680ce46
Sam Maier <smaier@google.com>
2026-04-02 13:48:15 -0700

    Autoroll NDK by scraping ndk wiki page
    The github wiki page seems to actually be the source of truth over
    https://developer.android.com/ndk/downloads - the Android site links to
    the GitHub site.
    
    Bug: 477295731
    Change-Id: I484cba8b0c8f70c8c52abc2c5f1fdd8a6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727982
    Commit-Queue: Sam Maier <smaier@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sam Maier <smaier@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609476}

--- third_party/android_toolchain/3pp/fetch.py
+++ third_party/android_toolchain/3pp/fetch.py
@@ -4,47 +4,67 @@
 # found in the LICENSE file.
 
 import argparse
-import hashlib
 import json
-import pathlib
+import os
+import re
+import urllib.request
 
 
-# Update this when upgrading NDK.
-_URL = "https://dl.google.com/android/repository/android-ndk-r28-linux.zip"
+def get_md():
+    # We read the raw wiki markdown as it is the easiest to parse.
+    with urllib.request.urlopen(
+            'https://raw.githubusercontent.com/wiki/android/ndk/Home.md'
+    ) as response:
+        return response.read().decode('utf-8')
+
+
+def version_key(v):
+    # Eg. "29.0.14206865"
+    return [int(x) for x in v.split('.')]
+
 
 def do_latest():
-  # Change version time this file changes.
-  md5 = hashlib.md5()
-  md5.update(pathlib.Path(__file__).read_bytes())
-
-  this_dir = pathlib.Path(__file__).parent
-  # Trigger on changes to other files in this directory.
-  md5.update((this_dir / '3pp.pb').read_bytes())
-  md5.update((this_dir / 'install.sh').read_bytes())
-  print(md5.hexdigest())
+    md = get_md()
+    versions = re.findall(r'ndkVersion\s+"([^"]+)"', md)
+    if not versions:
+        raise Exception('Could not find any ndkVersion in Home.md')
+    print(max(versions, key=version_key))
 
 
-def do_get_url():
-  partial_manifest = {
-    'url': [_URL],
-    'ext': '.zip',
-  }
-  print(json.dumps(partial_manifest))
+def get_url():
+    v = os.environ['_3PP_VERSION']
+    md = get_md()
+
+    # Split by sections starting with ### as these headers seperate the LTS,
+    # current release, and beta/RC.
+    sections = md.split('###')
+    for section in sections:
+        if f'ndkVersion "{v}"' in section:
+            # Find linux download url in this section.
+            match = re.search(r'href="(http\S+android-ndk-[^"]+-linux\.zip)"',
+                              section)
+            if match:
+                artifact_url = match.group(1)
+                partial_manifest = {'url': [artifact_url], 'ext': '.zip'}
+                print(json.dumps(partial_manifest))
+                return
+
+    raise Exception(f'Could not find URL for version {v} in github wiki.')
 
 
 def main():
-  ap = argparse.ArgumentParser()
-  sub = ap.add_subparsers(required=True)
+    ap = argparse.ArgumentParser()
+    sub = ap.add_subparsers(required=True)
 
-  latest = sub.add_parser("latest")
-  latest.set_defaults(func=do_latest)
+    latest = sub.add_parser("latest")
+    latest.set_defaults(func=do_latest)
 
-  download = sub.add_parser("get_url")
-  download.set_defaults(func=lambda: do_get_url())
+    download = sub.add_parser("get_url")
+    download.set_defaults(func=get_url)
 
-  opts = ap.parse_args()
-  opts.func()
+    opts = ap.parse_args()
+    opts.func()
 
 
 if __name__ == '__main__':
-  main()
+    main()
