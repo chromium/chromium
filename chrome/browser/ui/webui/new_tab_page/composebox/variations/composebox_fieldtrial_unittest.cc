e1887b3c5fbf3cbd87581b4b543f259afcaca0be
Jilin Yang <jilinyang@google.com>
2026-04-03 11:46:48 -0700

    [NTP-Real-Box]: Replace flaky NtpRealboxNextFieldTrial with unit tests
    The 128-combo parameterized browser test mirrored implementation logic in GetExpectedEnabled(), causing repeated breakages as enablement logic evolved. Replace with 6 focused unit tests that hard-code expected values and cover all critical boundaries (null profile, features disabled,, missing composebox prerequisite, eligibility gating, fusebox gating).
    
    Fixed: b:498763323
    
    Change-Id: If1af3633a56ed8725a7b3abbdf467f51b789b1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7727686
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Jilin Yang <jilinyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1609891}

---
+++ chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial_unittest.cc
@@ -0,0 +1,115 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.h"
+
+#include <memory>
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/autocomplete/aim_eligibility_service_factory.h"
+#include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/test/base/testing_profile.h"
+#include "components/omnibox/browser/mock_aim_eligibility_service.h"
+#include "content/public/test/browser_task_environment.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ntp_realbox {
+namespace {
+
+std::unique_ptr<KeyedService> BuildMockAimEligibilityService(
+    content::BrowserContext* context) {
+  auto* profile = Profile::FromBrowserContext(context);
+  return std::make_unique<testing::NiceMock<MockAimEligibilityService>>(
+      *profile->GetPrefs(),
+      TemplateURLServiceFactory::GetForProfile(profile),
+      /*url_loader_factory=*/nullptr,
+      /*identity_manager=*/nullptr);
+}
+
+class IsNtpRealboxNextEnabledTest : public testing::Test {
+ public:
+  void SetUp() override {
+    TestingProfile::Builder builder;
+    builder.AddTestingFactory(
+        TemplateURLServiceFactory::GetInstance(),
+        base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor));
+    builder.AddTestingFactory(
+        AimEligibilityServiceFactory::GetInstance(),
+        base::BindRepeating(&BuildMockAimEligibilityService));
+    profile_ = builder.Build();
+
+    mock_service_ =
+        static_cast<testing::NiceMock<MockAimEligibilityService>*>(
+            AimEligibilityServiceFactory::GetForProfile(profile_.get()));
+  }
+
+ protected:
+  void SetAllEligible(bool eligible) {
+    ON_CALL(*mock_service_, IsAimLocallyEligible())
+        .WillByDefault(testing::Return(eligible));
+    ON_CALL(*mock_service_, IsAimEligible())
+        .WillByDefault(testing::Return(eligible));
+    ON_CALL(*mock_service_, IsFuseboxEligible())
+        .WillByDefault(testing::Return(eligible));
+  }
+
+  content::BrowserTaskEnvironment task_environment_;
+  std::unique_ptr<TestingProfile> profile_;
+  raw_ptr<testing::NiceMock<MockAimEligibilityService>> mock_service_ =
+      nullptr;
+};
+
+TEST_F(IsNtpRealboxNextEnabledTest, ReturnsFalseForNullProfile) {
+  EXPECT_FALSE(IsNtpRealboxNextEnabled(nullptr));
+}
+
+TEST_F(IsNtpRealboxNextEnabledTest, ReturnsFalseWhenFeatureDisabledByDefault) {
+  // Both kNtpComposebox and kNtpRealboxNext are FEATURE_DISABLED_BY_DEFAULT.
+  SetAllEligible(true);
+  EXPECT_FALSE(IsNtpRealboxNextEnabled(profile_.get()));
+}
+
+TEST_F(IsNtpRealboxNextEnabledTest,
+       ReturnsFalseWhenOnlyRealboxNextEnabled) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures({kNtpRealboxNext}, {});
+  SetAllEligible(true);
+  // kNtpComposebox is still disabled by default.
+  EXPECT_FALSE(IsNtpRealboxNextEnabled(profile_.get()));
+}
+
+TEST_F(IsNtpRealboxNextEnabledTest, ReturnsFalseWhenNotEligible) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      {ntp_composebox::kNtpComposebox, kNtpRealboxNext}, {});
+  SetAllEligible(true);
+  EXPECT_TRUE(IsNtpRealboxNextEnabled(profile_.get()));
+}
+
+TEST_F(IsNtpRealboxNextEnabledTest, ReturnsTrueWhenEnabledAndEligible) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      {ntp_composebox::kNtpComposebox, kNtpRealboxNext}, {});
+  SetAllEligible(true);
+  EXPECT_TRUE(IsNtpRealboxNextEnabled(profile_.get()));
+}
+
+TEST_F(IsNtpRealboxNextEnabledTest,
+       ReturnsFalseWhenAimEligibleButNotFuseboxEligible) {
+  base::test::ScopedFeatureList features;
+  features.InitWithFeatures(
+      {ntp_composebox::kNtpComposebox, kNtpRealboxNext}, {});
+  ON_CALL(*mock_service_, IsAimLocallyEligible())
+      .WillByDefault(testing::Return(true));
+  ON_CALL(*mock_service_, IsAimEligible())
+      .WillByDefault(testing::Return(true));
+  ON_CALL(*mock_service_, IsFuseboxEligible())
+      .WillByDefault(testing::Return(false));
+  EXPECT_FALSE(IsNtpRealboxNextEnabled(profile_.get()));
+}
+
+}  // namespace
+}  // namespace ntp_realbox
