5a60730046ac765201cc9fcb570cdcb4d35b6f06
Justin DeWitt <dewittj@chromium.org>
2026-04-03 11:45:04 -0700

    Disable IPH_GlicTryIt by default.
    Change-Id: Ibcc4fb92217b61636bcec93ed2814975386c6a3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726844
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Auto-Submit: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1609888}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -121,7 +121,7 @@ BASE_FEATURE(kIPHGlicTrustFirstOnboardingShortcutToastPromoFeature,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHGlicTryItFeature,
              "IPH_GlicTryIt",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHGMCCastStartStopFeature,
              "IPH_GMCCastStartStop",
              base::FEATURE_ENABLED_BY_DEFAULT);
