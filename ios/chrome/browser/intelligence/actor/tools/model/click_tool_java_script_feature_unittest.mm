b3fed572e96fc66a082a9696aba1966a9b165155
Kirubel <kaklilu@chromium.org>
2026-04-06 09:56:12 -0700

    Use ActorToolJavaScriptFeatureTestBase in tests.
    The ClickToolJavaScriptFeatureTest and 
    TypeToolJavaScriptFeatureTest can be updated to use
    this base class to reduce duplicating test setup code
    throughout.
    
    Change-Id: Ic49f7a751b4818b4419621035ef7ebb56a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7729184
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1610331}

--- ios/chrome/browser/intelligence/actor/tools/model/click_tool_java_script_feature_unittest.mm
+++ ios/chrome/browser/intelligence/actor/tools/model/click_tool_java_script_feature_unittest.mm
@@ -4,62 +4,44 @@
 
 #import "ios/chrome/browser/intelligence/actor/tools/model/click_tool_java_script_feature.h"
 
-#import "base/strings/sys_string_conversions.h"
-#import "base/test/scoped_feature_list.h"
 #import "base/test/test_future.h"
 #import "components/optimization_guide/proto/features/actions_data.pb.h"
 #import "ios/chrome/browser/intelligence/actor/tools/model/actor_tool.h"
 #import "ios/chrome/browser/intelligence/actor/tools/model/actor_tool_error.h"
-#import "ios/chrome/browser/intelligence/features/features.h"
-#import "ios/chrome/test/ios_chrome_test_with_web_state.h"
-#import "ios/web/common/features.h"
-#import "ios/web/public/test/js_test_util.h"
-#import "ios/web/public/test/web_state_test_util.h"
-#import "ios/web/public/web_state.h"
+#import "ios/chrome/browser/intelligence/actor/tools/model/actor_tool_java_script_feature_test_base.h"
 #import "testing/gtest/include/gtest/gtest.h"
-
-namespace actor {
 
 using optimization_guide::proto::ClickAction;
 
-namespace {
+namespace actor {
 
-// Constants for the standard HTML page.
-constexpr int kHtmlWidth = 500;
-constexpr int kHtmlHeight = 500;
-constexpr int kMidPointX = kHtmlWidth / 2;
-constexpr int kMidPointY = kHtmlHeight / 2;
-
-class ClickToolJavaScriptFeatureTest : public IOSChromeTestWithWebState {
+class ClickToolJavaScriptFeatureTest
+    : public ActorToolJavaScriptFeatureTestBase {
  protected:
-  ClickToolJavaScriptFeatureTest()
-      : IOSChromeTestWithWebState(WebClientMode::kChromeWebClient) {
-    scoped_feature_list_.InitWithFeatures(
-        {web::features::kAssertOnJavaScriptErrors, kActorTools}, {});
-  }
+  ClickToolJavaScriptFeatureTest() : ActorToolJavaScriptFeatureTestBase() {}
 
-  void SetUp() override {
-    IOSChromeTestWithWebState::SetUp();
-    NSString* html = [NSString
-        stringWithFormat:
-            @"<html><body style='width: %dpx; height: %dpx;'></body></html>",
-            kHtmlWidth, kHtmlHeight];
-    LoadHtml(html);
-  }
+  void SetUp() override { ActorToolJavaScriptFeatureTestBase::SetUp(); }
 
   ClickToolJavaScriptFeature* feature() {
     return ClickToolJavaScriptFeature::GetInstance();
   }
 
-  ClickAction CreateClickActionWithCoordinates(int x = kMidPointX,
-                                               int y = kMidPointY) {
+  // Mocks both JavaScript functions for clicking to return the given result.
+  void MockClickJsFunctions(const std::string& mock_return_value) {
+    MockJsFunction(feature(), "click_tool", "clickByCoordinate",
+                   mock_return_value);
+    MockJsFunction(feature(), "click_tool", "clickByNodeId", mock_return_value);
+  }
+
+  ClickAction CreateClickActionWithCoordinates() {
     ClickAction action;
-    action.mutable_target()->mutable_coordinate()->set_x(x);
-    action.mutable_target()->mutable_coordinate()->set_y(y);
-    action.set_click_type(ClickAction::LEFT);
-    action.set_click_count(ClickAction::SINGLE);
+    // Use arbitrary values since the JS function is mocked.
+    action.mutable_target()->mutable_coordinate()->set_x(1);
+    action.mutable_target()->mutable_coordinate()->set_y(2);
     action.mutable_target()->mutable_coordinate()->set_pixel_type(
-        optimization_guide::proto::Coordinate::PIXEL_TYPE_DIPS);
+        optimization_guide::proto::Coordinate::PIXEL_TYPE_UNSPECIFIED);
+    action.set_click_type(ClickAction::UNKNOWN_CLICK_TYPE);
+    action.set_click_count(ClickAction::UNKNOWN_CLICK_COUNT);
     return action;
   }
 
@@ -70,148 +52,108 @@ class ClickToolJavaScriptFeatureTest : public IOSChromeTestWithWebState {
     action.mutable_target()
         ->mutable_document_identifier()
         ->set_serialized_token("doc_id");
-    action.set_click_type(ClickAction::LEFT);
-    action.set_click_count(ClickAction::SINGLE);
+    action.set_click_type(ClickAction::UNKNOWN_CLICK_TYPE);
+    action.set_click_count(ClickAction::UNKNOWN_CLICK_COUNT);
     return action;
   }
-
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
-TEST_F(ClickToolJavaScriptFeatureTest, ClickByCoordinate_JsReturnsNonDict) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
+TEST_F(ClickToolJavaScriptFeatureTest, JsReturnsNonDict) {
+  MockClickJsFunctions(/*mock_return_value=*/"'unexpected type'");
+  ClickAction click_by_coordinate = CreateClickActionWithCoordinates();
+  ClickAction click_by_node_id = CreateClickActionWithNodeId();
+  base::test::TestFuture<ActorTool::ToolExecutionResult> coordinate_future;
+  base::test::TestFuture<ActorTool::ToolExecutionResult> node_id_future;
 
-  // Override the JS function to return a string.
-  web::test::ExecuteJavaScriptForFeature(web_state(),
-                                         base::SysUTF8ToNSString(R"(
-        __gCrWeb.getRegisteredApi('click_tool').addFunction('clickByCoordinate',
-          function() { return 'unexpected type'; }
-        ); true;
-      )"),
-                                         feature());
+  feature()->Click(GetMainFrame(feature()).get(), click_by_coordinate,
+                   coordinate_future.GetCallback());
+  feature()->Click(GetMainFrame(feature()).get(), click_by_node_id,
+                   node_id_future.GetCallback());
 
-  ClickAction action = CreateClickActionWithCoordinates();
+  auto coordinate_result = coordinate_future.Get();
+  EXPECT_FALSE(coordinate_result.has_value());
+  EXPECT_EQ(coordinate_result.error().code,
+            ActorToolErrorCode::kJavascriptFeatureGotInvalidResult);
 
-  base::test::TestFuture<ActorTool::ToolExecutionResult> future;
-  feature()->Click(main_frame, action, future.GetCallback());
-
-  auto result = future.Get();
-  EXPECT_FALSE(result.has_value());
-  EXPECT_EQ(result.error().code,
+  auto node_id_result = node_id_future.Get();
+  EXPECT_FALSE(node_id_result.has_value());
+  EXPECT_EQ(node_id_result.error().code,
             ActorToolErrorCode::kJavascriptFeatureGotInvalidResult);
 }
 
-TEST_F(ClickToolJavaScriptFeatureTest, ClickByCoordinate_JsReturnsError) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
+TEST_F(ClickToolJavaScriptFeatureTest, JsReturnsError) {
+  MockClickJsFunctions(
+      /*mock_return_value=*/"{success: false, message: 'Custom JS Error'}");
+  ClickAction click_by_coordinate = CreateClickActionWithCoordinates();
+  ClickAction click_by_node_id = CreateClickActionWithNodeId();
+  base::test::TestFuture<ActorTool::ToolExecutionResult> coordinate_future;
+  base::test::TestFuture<ActorTool::ToolExecutionResult> node_id_future;
 
-  // Override the JS function to return an error dictionary.
-  web::test::ExecuteJavaScriptForFeature(web_state(),
-                                         base::SysUTF8ToNSString(R"(
-        __gCrWeb.getRegisteredApi('click_tool').addFunction('clickByCoordinate',
-          function() { return {success: false, message: 'Custom JS Error'}; }
-        ); true;
-      )"),
-                                         feature());
+  feature()->Click(GetMainFrame(feature()).get(), click_by_coordinate,
+                   coordinate_future.GetCallback());
+  feature()->Click(GetMainFrame(feature()).get(), click_by_node_id,
+                   node_id_future.GetCallback());
 
-  ClickAction action = CreateClickActionWithCoordinates();
-
-  base::test::TestFuture<ActorTool::ToolExecutionResult> future;
-  feature()->Click(main_frame, action, future.GetCallback());
-
-  auto result = future.Get();
-  EXPECT_FALSE(result.has_value());
-  EXPECT_EQ(result.error().code,
+  auto coordinate_result = coordinate_future.Get();
+  EXPECT_FALSE(coordinate_result.has_value());
+  EXPECT_EQ(coordinate_result.error().code,
             ActorToolErrorCode::kJavascriptFeatureFailedInJavaScriptExecution);
-  EXPECT_EQ(result.error().message, "Custom JS Error");
+  EXPECT_EQ(coordinate_result.error().message, "Custom JS Error");
+
+  auto node_id_result = node_id_future.Get();
+  EXPECT_FALSE(node_id_result.has_value());
+  EXPECT_EQ(node_id_result.error().code,
+            ActorToolErrorCode::kJavascriptFeatureFailedInJavaScriptExecution);
+  EXPECT_EQ(node_id_result.error().message, "Custom JS Error");
 }
 
-TEST_F(ClickToolJavaScriptFeatureTest,
-       ClickByCoordinate_JsReturnsErrorWithoutMessage) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
+TEST_F(ClickToolJavaScriptFeatureTest, JsReturnsErrorWithoutMessage) {
+  MockClickJsFunctions(/*mock_return_value=*/"{success: false}");
+  ClickAction click_by_coordinate = CreateClickActionWithCoordinates();
+  ClickAction click_by_node_id = CreateClickActionWithNodeId();
+  base::test::TestFuture<ActorTool::ToolExecutionResult> coordinate_future;
+  base::test::TestFuture<ActorTool::ToolExecutionResult> node_id_future;
 
-  // Override the JS function to return an error dictionary without message.
-  web::test::ExecuteJavaScriptForFeature(web_state(),
-                                         base::SysUTF8ToNSString(R"(
-        __gCrWeb.getRegisteredApi('click_tool').addFunction('clickByCoordinate',
-          function() { return {success: false}; }
-        ); true;
-      )"),
-                                         feature());
+  feature()->Click(GetMainFrame(feature()).get(), click_by_coordinate,
+                   coordinate_future.GetCallback());
+  feature()->Click(GetMainFrame(feature()).get(), click_by_node_id,
+                   node_id_future.GetCallback());
 
-  ClickAction action = CreateClickActionWithCoordinates();
-
-  base::test::TestFuture<ActorTool::ToolExecutionResult> future;
-  feature()->Click(main_frame, action, future.GetCallback());
-
-  auto result = future.Get();
-  EXPECT_FALSE(result.has_value());
-  EXPECT_EQ(result.error().code,
+  auto coordinate_result = coordinate_future.Get();
+  EXPECT_FALSE(coordinate_result.has_value());
+  EXPECT_EQ(coordinate_result.error().code,
             ActorToolErrorCode::kJavascriptFeatureFailedInJavaScriptExecution);
-  EXPECT_EQ(result.error().message, "Unknown error in JS.");
+  EXPECT_EQ(coordinate_result.error().message, "Unknown error in JS.");
+
+  auto node_id_result = node_id_future.Get();
+  EXPECT_FALSE(node_id_result.has_value());
+  EXPECT_EQ(node_id_result.error().code,
+            ActorToolErrorCode::kJavascriptFeatureFailedInJavaScriptExecution);
+  EXPECT_EQ(node_id_result.error().message, "Unknown error in JS.");
 }
 
 TEST_F(ClickToolJavaScriptFeatureTest, ClickByCoordinate_Success) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
+  MockClickJsFunctions(
+      /*mock_return_value=*/"{success: true, message: 'fake success!'}");
   ClickAction action = CreateClickActionWithCoordinates();
-
   base::test::TestFuture<ActorTool::ToolExecutionResult> future;
-  feature()->Click(main_frame, action, future.GetCallback());
+
+  feature()->Click(GetMainFrame(feature()).get(), action, future.GetCallback());
 
   auto result = future.Get();
   EXPECT_TRUE(result.has_value());
-}
-
-TEST_F(ClickToolJavaScriptFeatureTest, ClickByNodeId_JsReturnsError) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
-
-  // Override the JS function to return an error dictionary.
-  web::test::ExecuteJavaScriptForFeature(web_state(),
-                                         base::SysUTF8ToNSString(R"(
-        __gCrWeb.getRegisteredApi('click_tool').addFunction('clickByNodeId',
-          function() { return {success: false, message: 'Custom JS Error'}; }
-        ); true;
-      )"),
-                                         feature());
-
-  ClickAction action = CreateClickActionWithNodeId();
-
-  base::test::TestFuture<ActorTool::ToolExecutionResult> future;
-  feature()->Click(main_frame, action, future.GetCallback());
-
-  auto result = future.Get();
-  EXPECT_FALSE(result.has_value());
-  EXPECT_EQ(result.error().code,
-            ActorToolErrorCode::kJavascriptFeatureFailedInJavaScriptExecution);
-  EXPECT_EQ(result.error().message, "Custom JS Error");
 }
 
 TEST_F(ClickToolJavaScriptFeatureTest, ClickByNodeId_Success) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
-
-  // Override the JS function to return a success dictionary.
-  web::test::ExecuteJavaScriptForFeature(web_state(),
-                                         base::SysUTF8ToNSString(R"(
-        __gCrWeb.getRegisteredApi('click_tool').addFunction('clickByNodeId',
-          function() { return {success: true}; }
-        ); true;
-      )"),
-                                         feature());
-
+  MockClickJsFunctions(
+      /*mock_return_value=*/"{success: true, message: 'fake success!'}");
   ClickAction action = CreateClickActionWithNodeId();
-
   base::test::TestFuture<ActorTool::ToolExecutionResult> future;
-  feature()->Click(main_frame, action, future.GetCallback());
+
+  feature()->Click(GetMainFrame(feature()).get(), action, future.GetCallback());
 
   auto result = future.Get();
   EXPECT_TRUE(result.has_value());
 }
-
-}  // namespace
 
 }  // namespace actor
--- ios/chrome/browser/intelligence/actor/tools/model/type_tool_java_script_feature_unittest.mm
+++ ios/chrome/browser/intelligence/actor/tools/model/type_tool_java_script_feature_unittest.mm
@@ -4,132 +4,73 @@
 
 #import "ios/chrome/browser/intelligence/actor/tools/model/type_tool_java_script_feature.h"
 
-#import "base/strings/stringprintf.h"
-#import "base/strings/sys_string_conversions.h"
-#import "base/test/bind.h"
-#import "base/test/ios/wait_util.h"
-#import "base/test/scoped_feature_list.h"
 #import "base/test/test_future.h"
-#import "components/autofill/ios/browser/autofill_util.h"
-#import "components/autofill/ios/form_util/child_frame_registrar.h"
 #import "components/optimization_guide/proto/features/actions_data.pb.h"
-#import "ios/chrome/browser/intelligence/actor/tools/model/action_target_java_script_feature.h"
 #import "ios/chrome/browser/intelligence/actor/tools/model/actor_tool.h"
 #import "ios/chrome/browser/intelligence/actor/tools/model/actor_tool_error.h"
-#import "ios/chrome/browser/intelligence/features/features.h"
-#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
-#import "ios/chrome/test/ios_chrome_test_with_web_state.h"
-#import "ios/web/common/features.h"
-#import "ios/web/public/js_messaging/web_frames_manager.h"
-#import "ios/web/public/test/fakes/fake_web_client.h"
-#import "ios/web/public/test/fakes/fake_web_state.h"
-#import "ios/web/public/test/js_test_util.h"
-#import "ios/web/public/test/scoped_testing_web_client.h"
-#import "ios/web/public/test/web_state_test_util.h"
-#import "ios/web/public/test/web_task_environment.h"
-#import "ios/web/public/web_state.h"
+#import "ios/chrome/browser/intelligence/actor/tools/model/actor_tool_java_script_feature_test_base.h"
 #import "testing/gtest/include/gtest/gtest.h"
-#import "testing/platform_test.h"
-
-namespace actor {
 
 using optimization_guide::proto::TypeAction;
 
-namespace {
+namespace actor {
 
-constexpr int kHtmlWidth = 500;
-constexpr int kHtmlHeight = 500;
-constexpr int kMidPointX = kHtmlWidth / 2;
-constexpr int kMidPointY = kHtmlHeight / 2;
-
-class TypeToolJavaScriptFeatureTest : public IOSChromeTestWithWebState {
+class TypeToolJavaScriptFeatureTest
+    : public ActorToolJavaScriptFeatureTestBase {
  protected:
-  TypeToolJavaScriptFeatureTest()
-      : IOSChromeTestWithWebState(WebClientMode::kChromeWebClient) {
-    scoped_feature_list_.InitWithFeatures(
-        {web::features::kAssertOnJavaScriptErrors, kActorTools}, {});
-  }
+  TypeToolJavaScriptFeatureTest() : ActorToolJavaScriptFeatureTestBase() {}
 
-  void SetUp() override {
-    IOSChromeTestWithWebState::SetUp();
-    NSString* html =
-        base::SysUTF8ToNSString(base::StringPrintf(R"(
-          <html>
-            <body style='width: %dpx; height: %dpx;'>
-              <input style='width: 100%%; height: 100%%;'>
-            </body>
-          </html>
-        )",
-                                                   kHtmlWidth, kHtmlHeight));
-    LoadHtml(html);
-  }
+  void SetUp() override { ActorToolJavaScriptFeatureTestBase::SetUp(); }
 
   TypeToolJavaScriptFeature* feature() {
     return TypeToolJavaScriptFeature::GetInstance();
   }
 
-  TypeAction CreateTypeActionWithCoordinates(
-      const std::string& text = "default",
-      TypeAction::TypeMode mode = TypeAction::DELETE_EXISTING,
-      bool follow_by_enter = false) {
+  // Mocks both JavaScript functions for typing to return the given result.
+  void MockTypeJsFunctions(const std::string& mock_return_value) {
+    MockJsFunction(feature(), "type_tool", "typeByCoordinate",
+                   mock_return_value);
+    MockJsFunction(feature(), "type_tool", "typeByNodeId", mock_return_value);
+  }
+
+  TypeAction CreateTypeActionWithCoordinates() {
     TypeAction action;
-    action.mutable_target()->mutable_coordinate()->set_x(kMidPointX);
-    action.mutable_target()->mutable_coordinate()->set_y(kMidPointY);
+    // Use arbitrary values since the JS function is mocked.
+    action.mutable_target()->mutable_coordinate()->set_x(1);
+    action.mutable_target()->mutable_coordinate()->set_y(2);
     action.mutable_target()->mutable_coordinate()->set_pixel_type(
-        optimization_guide::proto::Coordinate::PIXEL_TYPE_DIPS);
-    action.set_text(text);
-    action.set_mode(mode);
-    action.set_follow_by_enter(follow_by_enter);
+        optimization_guide::proto::Coordinate::PIXEL_TYPE_UNSPECIFIED);
+    action.set_text("default");
+    action.set_mode(TypeAction::UNKNOWN_TYPE_MODE);
+    action.set_follow_by_enter(false);
     return action;
   }
 
-  TypeAction CreateTypeActionWithIdentifiers(
-      uint32_t node_id,
-      const std::string& serialized_token,
-      const std::string& text = "default",
-      TypeAction::TypeMode mode = TypeAction::DELETE_EXISTING,
-      bool follow_by_enter = false) {
+  TypeAction CreateTypeActionWithIdentifiers() {
     TypeAction action;
-    action.mutable_target()->set_content_node_id(node_id);
+    // Use arbitrary values since the JS function is mocked.
+    action.mutable_target()->set_content_node_id(0);
     action.mutable_target()
         ->mutable_document_identifier()
-        ->set_serialized_token(serialized_token);
-    action.set_text(text);
-    action.set_mode(mode);
-    action.set_follow_by_enter(follow_by_enter);
+        ->set_serialized_token("token");
+    action.set_text("default");
+    action.set_mode(TypeAction::UNKNOWN_TYPE_MODE);
+    action.set_follow_by_enter(false);
     return action;
   }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 TEST_F(TypeToolJavaScriptFeatureTest, JsReturnsNonDict) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
-
-  web::test::ExecuteJavaScriptForFeature(web_state(),
-                                         base::SysUTF8ToNSString(R"(
-        __gCrWeb.getRegisteredApi('type_tool').addFunction('typeByCoordinate',
-          function() { return 'unexpected type'; }
-        );
-        __gCrWeb.getRegisteredApi('type_tool').addFunction('typeByNodeId',
-          function() { return 'unexpected type'; }
-        );
-        true;
-      )"),
-                                         feature());
-
+  MockTypeJsFunctions(/*mock_return_value=*/"'unexpected type'");
   TypeAction type_by_coordinate = CreateTypeActionWithCoordinates();
-  TypeAction type_by_node_id = CreateTypeActionWithIdentifiers(
-      /*node_id=*/1, /*serialized_token=*/"token");
-
+  TypeAction type_by_node_id = CreateTypeActionWithIdentifiers();
   base::test::TestFuture<ActorTool::ToolExecutionResult> coordinate_future;
   base::test::TestFuture<ActorTool::ToolExecutionResult> node_id_future;
 
-  feature()->Type(main_frame, type_by_coordinate,
+  feature()->Type(GetMainFrame(feature()).get(), type_by_coordinate,
                   coordinate_future.GetCallback());
-  feature()->Type(main_frame, type_by_node_id, node_id_future.GetCallback());
+  feature()->Type(GetMainFrame(feature()).get(), type_by_node_id,
+                  node_id_future.GetCallback());
 
   auto coordinate_result = coordinate_future.Get();
   EXPECT_FALSE(coordinate_result.has_value());
@@ -143,31 +84,17 @@ TEST_F(TypeToolJavaScriptFeatureTest, JsReturnsNonDict) {
 }
 
 TEST_F(TypeToolJavaScriptFeatureTest, JsReturnsError) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
-
-  web::test::ExecuteJavaScriptForFeature(web_state(),
-                                         base::SysUTF8ToNSString(R"(
-        __gCrWeb.getRegisteredApi('type_tool').addFunction('typeByCoordinate',
-          function() { return {success: false, message: 'Custom JS Error'}; }
-        );
-        __gCrWeb.getRegisteredApi('type_tool').addFunction('typeByNodeId',
-          function() { return {success: false, message: 'Custom JS Error'}; }
-        );
-        true;
-      )"),
-                                         feature());
-
+  MockTypeJsFunctions(
+      /*mock_return_value=*/"{success: false, message: 'Custom JS Error'}");
   TypeAction type_by_coordinate = CreateTypeActionWithCoordinates();
-  TypeAction type_by_node_id = CreateTypeActionWithIdentifiers(
-      /*node_id=*/1, /*serialized_token=*/"token");
-
+  TypeAction type_by_node_id = CreateTypeActionWithIdentifiers();
   base::test::TestFuture<ActorTool::ToolExecutionResult> coordinate_future;
   base::test::TestFuture<ActorTool::ToolExecutionResult> node_id_future;
 
-  feature()->Type(main_frame, type_by_coordinate,
+  feature()->Type(GetMainFrame(feature()).get(), type_by_coordinate,
                   coordinate_future.GetCallback());
-  feature()->Type(main_frame, type_by_node_id, node_id_future.GetCallback());
+  feature()->Type(GetMainFrame(feature()).get(), type_by_node_id,
+                  node_id_future.GetCallback());
 
   auto coordinate_result = coordinate_future.Get();
   EXPECT_FALSE(coordinate_result.has_value());
@@ -183,44 +110,27 @@ TEST_F(TypeToolJavaScriptFeatureTest, JsReturnsError) {
 }
 
 TEST_F(TypeToolJavaScriptFeatureTest, TypeByCoordinate_Success) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-  ASSERT_TRUE(main_frame);
+  MockTypeJsFunctions(
+      /*mock_return_value=*/"{success: true, message: 'fake success!'}");
   TypeAction action = CreateTypeActionWithCoordinates();
-
   base::test::TestFuture<ActorTool::ToolExecutionResult> future;
-  feature()->Type(main_frame, action, future.GetCallback());
+
+  feature()->Type(GetMainFrame(feature()).get(), action, future.GetCallback());
 
   auto result = future.Get();
   EXPECT_TRUE(result.has_value());
 }
 
 TEST_F(TypeToolJavaScriptFeatureTest, TypeByIdentifier_Success) {
-  web::WebFrame* main_frame = WaitForMainFrame(feature());
-
-  // Mock out the JS call since we don't have a way to get the DOM Node ID
-  // in this test.
-  web::test::ExecuteJavaScriptForFeature(web_state(),
-                                         base::SysUTF8ToNSString(R"(
-        __gCrWeb.getRegisteredApi('type_tool').addFunction('typeByNodeId',
-          function() { return {success: true, message: "fake success!"}; }
-        ); true;
-      )"),
-                                         feature());
-
-  // Neither identifier is actually relevant for the test since we mock out the
-  // JS call.
-  std::string document_identifier = "arbitrary_id";
-  int node_id = 123;
-  TypeAction action =
-      CreateTypeActionWithIdentifiers(node_id, document_identifier);
-
+  MockTypeJsFunctions(
+      /*mock_return_value=*/"{success: true, message: 'fake success!'}");
+  TypeAction action = CreateTypeActionWithIdentifiers();
   base::test::TestFuture<ActorTool::ToolExecutionResult> future;
-  feature()->Type(main_frame, action, future.GetCallback());
+
+  feature()->Type(GetMainFrame(feature()).get(), action, future.GetCallback());
 
   auto result = future.Get();
   EXPECT_TRUE(result.has_value());
 }
-
-}  // namespace
 
 }  // namespace actor
