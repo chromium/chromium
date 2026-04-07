143abf6d0adbdf6ab0b2169f41a2e051bd57332b
Madhav Pruthi <madhavpruthi@google.com>
2026-04-07 11:22:23 -0700

    [Clank IA] Add ToolbarVariationUtils to handle the different variants of tollbar
    Change-Id: Ibf15d591e35e4d92114f3873d649a28cb44a4faf
    Bug: 497898172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735489
    Reviewed-by: Dan Polanco <polardz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1610898}

---
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarVariationUtils.java
@@ -0,0 +1,40 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.toolbar;
+
+import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
+/** Utility class for determining the configuration of the toolbar variations. */
+@NullMarked
+public final class ToolbarVariationUtils {
+
+    private ToolbarVariationUtils() {}
+
+    /** Whether the back button should be in the omnibox. */
+    public static boolean shouldBackButtonBeInOmnibox() {
+        boolean keepAppMenu = ChromeFeatureList.sAndroidBottomBarKeepAppMenuInToolbar.getValue();
+        boolean keepHome = ChromeFeatureList.sAndroidBottomBarKeepHomeButtonInToolbar.getValue();
+        // Returns true if home is kept (Arm 1C) or app menu is not kept (Arm 1A).
+        return keepHome || !keepAppMenu;
+    }
+
+    /** Whether the app menu should be in the toolbar. */
+    public static boolean shouldAppMenuBeInToolbar() {
+        // Arm 1B and Arm 1C have app menu in toolbar.
+        return ChromeFeatureList.sAndroidBottomBarKeepAppMenuInToolbar.getValue();
+    }
+
+    /** Whether the home button should be at the start of the toolbar. */
+    public static boolean shouldHomeButtonBeAtStartOfToolbar() {
+        // Arm 1C has home button at start of toolbar.
+        return ChromeFeatureList.sAndroidBottomBarKeepHomeButtonInToolbar.getValue();
+    }
+
+    /** Whether the new toolbar variation UI is enabled. */
+    public static boolean isNewToolbarUiEnabled() {
+        return ChromeFeatureList.sAndroidBottomBar.isEnabled();
+    }
+}
---
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarVariationUtilsUnitTest.java
@@ -0,0 +1,69 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.toolbar;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Features.DisableFeatures;
+import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
+/** Unit tests for {@link ToolbarVariationUtils}. */
+@RunWith(BaseRobolectricTestRunner.class)
+public class ToolbarVariationUtilsUnitTest {
+
+    @Test
+    @EnableFeatures({
+        ChromeFeatureList.ANDROID_BOTTOM_BAR + ":keep_app_menu_in_toolbar/false",
+        ChromeFeatureList.ANDROID_BOTTOM_BAR + ":keep_home_button_in_toolbar/false"
+    })
+    public void testArm1A() {
+        // Arm 1A: Back in omnibox, no home, no appmenu
+        assertTrue(ToolbarVariationUtils.shouldBackButtonBeInOmnibox());
+        assertFalse(ToolbarVariationUtils.shouldAppMenuBeInToolbar());
+        assertFalse(ToolbarVariationUtils.shouldHomeButtonBeAtStartOfToolbar());
+    }
+
+    @Test
+    @EnableFeatures({
+        ChromeFeatureList.ANDROID_BOTTOM_BAR + ":keep_app_menu_in_toolbar/true",
+        ChromeFeatureList.ANDROID_BOTTOM_BAR + ":keep_home_button_in_toolbar/false"
+    })
+    public void testArm1B() {
+        // Arm 1B: Back out of omnibox, no home, yes appmenu
+        assertFalse(ToolbarVariationUtils.shouldBackButtonBeInOmnibox());
+        assertTrue(ToolbarVariationUtils.shouldAppMenuBeInToolbar());
+        assertFalse(ToolbarVariationUtils.shouldHomeButtonBeAtStartOfToolbar());
+    }
+
+    @Test
+    @EnableFeatures({
+        ChromeFeatureList.ANDROID_BOTTOM_BAR + ":keep_app_menu_in_toolbar/true",
+        ChromeFeatureList.ANDROID_BOTTOM_BAR + ":keep_home_button_in_toolbar/true"
+    })
+    public void testArm1C() {
+        // Arm 1C: Back in omnibox, yes home, yes appmenu
+        assertTrue(ToolbarVariationUtils.shouldBackButtonBeInOmnibox());
+        assertTrue(ToolbarVariationUtils.shouldAppMenuBeInToolbar());
+        assertTrue(ToolbarVariationUtils.shouldHomeButtonBeAtStartOfToolbar());
+    }
+
+    @Test
+    @EnableFeatures(ChromeFeatureList.ANDROID_BOTTOM_BAR)
+    public void testIsNewToolbarUiEnabled_Enabled() {
+        assertTrue(ToolbarVariationUtils.isNewToolbarUiEnabled());
+    }
+
+    @Test
+    @DisableFeatures(ChromeFeatureList.ANDROID_BOTTOM_BAR)
+    public void testIsNewToolbarUiEnabled_Disabled() {
+        assertFalse(ToolbarVariationUtils.isNewToolbarUiEnabled());
+    }
+}
