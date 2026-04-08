6536bd673eb172c8aa5c86dffdf211e12922bcbc
Katie Lu <ktlu@google.com>
2026-04-08 12:10:14 -0700

    Add feature flag for PDF translation.
    This change introduces `translate_features.h` and `translate_features.cc` to define a new base::Feature, `kEnableTranslatePdf`, which will control whether PDF translation is enabled.
    
    Change-Id: I247909d3999fe3f63d44c600f62675668db53262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7732986
    Commit-Queue: Katie Lu <ktlu@google.com>
    Reviewed-by: Danilo Françoso Tedeschi <danft@google.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611705}

---
+++ components/translate/core/common/translate_features.cc
@@ -0,0 +1,11 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/translate/core/common/translate_features.h"
+
+namespace translate {
+
+BASE_FEATURE(kEnableTranslatePdf, base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace translate
