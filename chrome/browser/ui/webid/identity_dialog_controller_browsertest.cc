77fa33265085ad7ddd3ad0771225386f8d239fce
Nicolás Peña <npm@chromium.org>
2026-04-08 10:42:19 -0700

    Reland "[FedCM] Guard active mode UIs under ShouldShowDialog"
    This reverts commit fa542b30bb8d6a52f378eab5f8c8ea5056253a29.
    
    Reason for revert: fix MSAN issue
    
    Original change's description:
    > Revert "[FedCM] Guard active mode UIs under ShouldShowDialog"
    >
    > This reverts commit 0051195464fe9b67745c347da496b1e8d08af996.
    >
    > Reason for revert: Suspicious about causing test failures of
    > IdentityDialogControllerBrowserTest.ActorTaskStateChangesCanShowWidget
    > https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/59072/overview
    >
    > Original change's description:
    > > [FedCM] Guard active mode UIs under ShouldShowDialog
    > >
    > > Currently we may crash due to invoking active mode dialogs during actor
    > > tasks that are running in the background. We fix this by returning early
    > > in the controller when an active mode dialog is meant to be shown but
    > > should not be shown. In particular, we reject the request when this is
    > > the case for accounts dialog where there was no embedder request, since
    > > this means the user cannot pick an account.
    > >
    > > Fixed: b:498695908
    > > Change-Id: I1e5254f4804457e732f891078a30bceb40abf8d7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736624
    > > Reviewed-by: Yi Gu <yigu@chromium.org>
    > > Commit-Queue: Nicolás Peña <npm@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1611073}
    >
    > Bug: b:498695908
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I3760bb0b57d84aff42694ebf8a66c556481ec8c7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7734230
    > Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1611224}
    
    Bug: b:498695908
    Change-Id: Ifb0c6d5f1493444b40c57ff116cd163278302aec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7737531
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1611637}

--- chrome/browser/ui/webid/identity_dialog_controller_browsertest.cc
+++ chrome/browser/ui/webid/identity_dialog_controller_browsertest.cc
@@ -254,15 +254,15 @@ IN_PROC_BROWSER_TEST_F(IdentityDialogControllerBrowserTest,
   IdentityProviderDataPtr idp_data = CreateIdentityProviderData(accounts);
 
   EXPECT_CALL(*view_ptr, Show).WillOnce(testing::Return(true));
-  controller->ShowAccountsDialog(
+  EXPECT_TRUE(controller->ShowAccountsDialog(
       content::RelyingPartyData(kTopFrameEtldPlusOne,
                                 /*iframe_for_display=*/u""),
       {idp_data}, accounts, /*filtered_accounts=*/{},
-      blink::mojom::RpMode::kActive,
+      blink::mojom::RpMode::kPassive,
       /*on_selected=*/base::DoNothing(),
       /*on_add_account=*/base::DoNothing(),
       /*dismiss_callback=*/base::DoNothing(),
-      /*accounts_displayed_callback=*/base::DoNothing());
+      /*accounts_displayed_callback=*/base::DoNothing()));
 
   EXPECT_FALSE(controller->DidShowUi());
 }
