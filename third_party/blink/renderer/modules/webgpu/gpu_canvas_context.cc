eb7aa91f5b7c08a9eea309a36716d6a87db6cf41
Colin Blundell <blundell@chromium.org>
2026-04-09 00:49:38 -0700

    [Blink] GpuCanvasContext specify DoExternalDrawAndSnapshot as overwrite
    This call clears the CRP's canvas, which is clearly an overwrite of its
    contents.
    
    Bug: 352263194
    Change-Id: Ibaa64c43bebb668d3bbdeb50279bb5eb40caa71d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7739161
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612016}

--- third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc
@@ -263,7 +263,7 @@ GPUCanvasContext::PaintRenderingResultsToSnapshot(
                           : SkColors::kTransparent;
     return resource_provider->DoExternalDrawAndSnapshot(
         [color](cc::PaintCanvas& canvas) { canvas.clear(color); },
-        ImageOrientationEnum::kDefault, /*is_overwrite=*/false);
+        ImageOrientationEnum::kDefault, /*is_overwrite=*/true);
   }
 
   wgpu::Texture texture;
