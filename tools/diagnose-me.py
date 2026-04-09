19fb9f60a0a3a972bdfd50ea089eb0d12ab5ce83
Alexei Svitkine <asvitkine@chromium.org>
2026-04-09 07:37:55 -0700

    diagnose-me: Add network connectivity check for Google Storage.
    - Test both IPv4 and IPv6 to identify broken dual-stack configurations.
    - Suggest G_PREFER_IPV4=1 if only IPv6 fails.
    - Fix macOS compatibility for the build dependencies check.
    - Restructure to support platform-specific checks (e.g. Linux-only checks).
    
    Note: I was personally experiencing the IPv6 issue causing my gclient
    sync to hang, hence why I'm adding the check.
    
    Bug: none
    Change-Id: I585ead930c94ba5181802dd6173f29beadaea027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736164
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612214}

--- tools/diagnose-me.py
+++ tools/diagnose-me.py
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Diagnose some common system configuration problems on Linux, and
+"""Diagnose some common system configuration problems on Linux and macOS, and
 suggest fixes."""
 
 from __future__ import print_function
@@ -11,45 +11,54 @@ from __future__ import print_function
 import os
 import subprocess
 import sys
+import socket
+from typing import List, Optional, Callable, Tuple, Any
 
-all_checks = []
+CheckCallable = Callable[[], Optional[str]]
 
-def Check(name):
+all_checks: List[Tuple[str, CheckCallable]] = []
+
+
+def Check(
+    name: str,
+    platforms: Optional[List[str]] = None
+) -> Callable[[CheckCallable], CheckCallable]:
   """Decorator that defines a diagnostic check."""
 
   def wrap(func):
-    all_checks.append((name, func))
+    if not platforms or sys.platform in platforms:
+      all_checks.append((name, func))
     return func
 
   return wrap
 
 
-@Check("/usr/bin/ld is not gold")
-def CheckSystemLd():
+@Check('/usr/bin/ld is not gold', platforms=['linux'])
+def CheckSystemLd() -> Optional[str]:
   proc = subprocess.Popen(['/usr/bin/ld', '-v'], stdout=subprocess.PIPE)
   stdout = proc.communicate()[0].decode('utf-8')
   if 'GNU gold' in stdout:
-    return ("When /usr/bin/ld is gold, system updates can silently\n"
-            "corrupt your graphics drivers.\n"
-            "Try 'sudo apt-get remove binutils-gold'.\n")
+    return ('When /usr/bin/ld is gold, system updates can silently\n'
+            'corrupt your graphics drivers.\n'
+            'Try \'sudo apt-get remove binutils-gold\'.\n')
   return None
 
 
-@Check("random lds are not in the $PATH")
-def CheckPathLd():
+@Check('random lds are not in the $PATH', platforms=['linux'])
+def CheckPathLd() -> Optional[str]:
   proc = subprocess.Popen(['which', '-a', 'ld'], stdout=subprocess.PIPE)
   stdout = proc.communicate()[0].decode('utf-8')
   instances = stdout.split()
   if len(instances) > 1:
-    return ("You have multiple 'ld' binaries in your $PATH:\n" +
-            '\n'.join(' - ' + i for i in instances) + "\n"
-            "You should delete all of them but your system one.\n"
-            "gold is hooked into your build via depot tools.\n")
+    return ('You have multiple \'ld\' binaries in your $PATH:\n' +
+            '\n'.join(' - ' + i for i in instances) + '\n'
+            'You should delete all of them but your system one.\n'
+            'gold is hooked into your build via depot tools.\n')
   return None
 
 
-@Check("/usr/bin/ld doesn't point to gold")
-def CheckLocalGold():
+@Check('/usr/bin/ld doesn\'t point to gold', platforms=['linux'])
+def CheckLocalGold() -> Optional[str]:
   # Check /usr/bin/ld* symlinks.
   for path in ('ld.bfd', 'ld'):
     path = '/usr/bin/' + path
@@ -62,28 +71,29 @@ def CheckLocalGold():
         continue  # Not a symlink
       raise
     if '/usr/local/gold' in target:
-      return ("%s is a symlink into /usr/local/gold.\n"
-              "It's difficult to make a recommendation, because you\n"
-              "probably set this up yourself.  But you should make\n"
-              "/usr/bin/ld be the standard linker, which you likely\n"
-              "renamed /usr/bin/ld.bfd or something like that.\n" % path)
+      return ('%s is a symlink into /usr/local/gold.\n'
+              'It\'s difficult to make a recommendation, because you\n'
+              'probably set this up yourself.  But you should make\n'
+              '/usr/bin/ld be the standard linker, which you likely\n'
+              'renamed /usr/bin/ld.bfd or something like that.\n' % path)
 
   return None
 
 
-@Check("random ninja binaries are not in the $PATH")
-def CheckPathNinja():
+@Check('random ninja binaries are not in the $PATH')
+def CheckPathNinja() -> Optional[str]:
   proc = subprocess.Popen(['which', 'ninja'], stdout=subprocess.PIPE)
   stdout = proc.communicate()[0].decode('utf-8')
   if not 'depot_tools' in stdout:
-    return ("The ninja binary in your path isn't from depot_tools:\n" + "    " +
-            stdout + "Remove custom ninjas from your path so that the one\n"
-            "in depot_tools is used.\n")
+    return ('The ninja binary in your path isn\'t from depot_tools:\n' +
+            '    ' + stdout +
+            'Remove custom ninjas from your path so that the one\n'
+            'in depot_tools is used.\n')
   return None
 
 
-@Check("build dependencies are satisfied")
-def CheckBuildDeps():
+@Check('build dependencies are satisfied', platforms=['linux'])
+def CheckBuildDeps() -> Optional[str]:
   script_path = os.path.join(
       os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'build',
       'install-build-deps.sh')
@@ -91,20 +101,50 @@ def CheckBuildDeps():
                           stdout=subprocess.PIPE)
   stdout = proc.communicate()[0].decode('utf-8')
   if 'WARNING' in stdout:
-    return ("Your build dependencies are out-of-date.\n"
-            "Run '" + script_path + "' to update.")
+    return ('Your build dependencies are out-of-date.\n'
+            'Run \'' + script_path + '\' to update.')
   return None
 
 
-def RunChecks():
+@Check('can connect to Google Storage')
+def CheckNetwork() -> Optional[str]:
+  # Try both IPv4 and IPv6 to detect broken dual-stack configs.
+  results = []
+  for family in (socket.AF_INET, socket.AF_INET6):
+    try:
+      s = socket.socket(family, socket.SOCK_STREAM)
+      s.settimeout(2)
+      s.connect(('storage.googleapis.com', 443))
+      s.close()
+      results.append(True)
+    except (OSError, socket.timeout):
+      results.append(False)
+
+  if not any(results):
+    return ('Total network failure: Cannot connect to storage.googleapis.com '
+            'via IPv4 or IPv6.\n'
+            'Check your internet connection, proxy settings, or firewall.')
+
+  if False in results:
+    family_name = 'IPv6' if not results[1] else 'IPv4'
+    return ('Partial network failure: %s connection to storage.googleapis.com '
+            'timed out.\n'
+            'This can cause gclient sync and gsutil to hang while they wait '
+            'for timeouts.\n'
+            'Check your network routing, try disabling %s, or set '
+            'G_PREFER_IPV4=1.' % (family_name, family_name))
+  return None
+
+
+def RunChecks() -> None:
   for name, check in all_checks:
-    sys.stdout.write("* Checking %s: " % name)
+    sys.stdout.write('* Checking %s: ' % name)
     sys.stdout.flush()
     error = check()
     if not error:
-      print("ok")
+      print('ok')
     else:
-      print("FAIL")
+      print('FAIL')
       print(error)
 
 
