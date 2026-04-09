d75c6e585f8514b99e1ed43e9cdbe9f81195969d
Yoshisto Yanagisawa <yyanagisawa@chromium.org>
2026-04-09 00:23:49 -0700

    [M146] Wrap MainScriptResponse consolidation behind a feature flag
    Original change's description:
    > Wrap MainScriptResponse consolidation behind a feature flag
    >
    > This CL introduces the
    > kServiceWorkerStaticRouterConsolidateMainScriptResponse feature flag to
    > wrap the architectural changes introduced in
    > https://chromium-review.googlesource.com/c/chromium/src/+/7728664.
    >
    > CL 7728664 consolidated the MainScriptResponse fetching into
    > ServiceWorkerVersion to resolve a Use-After-Free (UAF) vulnerability
    > (crbug.com/497302265). By placing these changes behind a feature flag,
    > we can safely manage the merge and provide a kill-switch in case of
    > unforeseen regressions.
    >
    > The feature is enabled by default to maintain the security fix.
    >
    > Bug: 497302265
    > Change-Id: Ife157578c7f208703f1e1dd841defebe81f77107
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738567
    > Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1611935}
    
    (cherry picked from commit 8888b59532fa0d0723cd1be2c15c3b5d9db23d8c)
    
    Bug: 500246778,497302265
    Change-Id: Ife157578c7f208703f1e1dd841defebe81f77107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7741321
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Auto-Submit: Chrome Cherry Picker <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7680@{#3903}
    Cr-Branched-From: 76b7d80e5cda23fe6537eed26d68c92e995c7f39-refs/heads/main@{#1582197}

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -1341,10 +1341,16 @@ void ServiceWorkerVersion::SetDevToolsAttached(bool attached) {
 
 void ServiceWorkerVersion::SetMainScriptResponse(
     std::unique_ptr<MainScriptResponse> response) {
-  main_script_fetched_ = true;
-  if (!response) {
-    main_script_response_callbacks_.Notify();
-    return;
+  if (base::FeatureList::IsEnabled(
+          features::kServiceWorkerStaticRouterConsolidateMainScriptResponse)) {
+    main_script_fetched_ = true;
+    if (!response) {
+      main_script_response_callbacks_.Notify();
+      return;
+    }
+  } else {
+    // In the old code path, this should never be called with a null response.
+    CHECK(response);
   }
 
   script_response_time_for_devtools_ = response->response_time;
@@ -1365,7 +1371,10 @@ void ServiceWorkerVersion::SetMainScriptResponse(
     context_->OnMainScriptResponseSet(version_id(), *main_script_response_);
   }
 
-  main_script_response_callbacks_.Notify();
+  if (base::FeatureList::IsEnabled(
+          features::kServiceWorkerStaticRouterConsolidateMainScriptResponse)) {
+    main_script_response_callbacks_.Notify();
+  }
 }
 
 bool ServiceWorkerVersion::main_script_fetched() const {
@@ -1374,6 +1383,10 @@ bool ServiceWorkerVersion::main_script_fetched() const {
 
 void ServiceWorkerVersion::EnsureMainScriptResponseSet(
     base::OnceClosure callback) {
+  if (!base::FeatureList::IsEnabled(
+          features::kServiceWorkerStaticRouterConsolidateMainScriptResponse)) {
+    return;
+  }
   if (main_script_fetched_) {
     std::move(callback).Run();
     return;
@@ -2516,13 +2529,24 @@ void ServiceWorkerVersion::StartWorkerInternal() {
   params->main_script_load_params = std::move(main_script_load_params_);
 
   if (IsInstalled(status())) {
-    if (!installed_scripts_sender_) {
+    if (base::FeatureList::IsEnabled(
+            features::
+                kServiceWorkerStaticRouterConsolidateMainScriptResponse)) {
+      if (!installed_scripts_sender_) {
+        installed_scripts_sender_ =
+            std::make_unique<ServiceWorkerInstalledScriptsSender>(this);
+        installed_scripts_sender_->Start();
+      }
+      params->installed_scripts_info =
+          installed_scripts_sender_->CreateInfoAndBind();
+    } else {
+      DCHECK(!installed_scripts_sender_);
       installed_scripts_sender_ =
           std::make_unique<ServiceWorkerInstalledScriptsSender>(this);
+      params->installed_scripts_info =
+          installed_scripts_sender_->CreateInfoAndBind();
       installed_scripts_sender_->Start();
     }
-    params->installed_scripts_info =
-        installed_scripts_sender_->CreateInfoAndBind();
   }
 
   params->service_worker_receiver =
