d8776f8d77bb20fbeb9a5bc88167ab3675124a1f
Awad Osman <awado@google.com>
2026-04-09 07:23:06 -0700

    Enable MovePasswordLeakDetectionToggleIos feature by default
    Removes fieldtrial testing config and enables the feature by default.
    The metrics look fine and we have leadership approval to launch.
    
    Bug: b:444244681
    Change-Id: I03ec57d2cb706b72913c1e8d329ff95e98c8bf07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7736173
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1612201}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -330,7 +330,7 @@ BASE_FEATURE(kMigrateToBlockV8OptimizerOnUnfamiliarSites,
 BASE_FEATURE(kModifiedESBFetchErrorHandling, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMovePasswordLeakDetectionToggleIos,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNoticeQueueForEsb, base::FEATURE_ENABLED_BY_DEFAULT);
 
