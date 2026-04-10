61ee685030b751295782f1f2d345d33e4d21e5f0
Jessica Huang <jessing@google.com>
2026-04-10 13:41:19 -0700

    fix spelling mistake
    Change-Id: I1e1dd060ff5d7eac86c6c99dfd8dc5011281439f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747280
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Jessica Huang <jessing@google.com>
    Cr-Commit-Position: refs/heads/main@{#1613072}

--- chrome/browser/ui/webui/signin/signin_utils.h
+++ chrome/browser/ui/webui/signin/signin_utils.h
@@ -123,7 +123,7 @@ Browser* GetDesktopBrowser(content::WebUI* web_ui);
 base::TimeDelta GetMinorModeRestrictionsDeadline();
 
 // Sets the height of the WebUI modal dialog after its initialization. This is
-// needed to better accomodate different locales' text heights.
+// needed to better accommodate different locales' text heights.
 void SetInitializedModalHeight(Browser* browser,
                                content::WebUI* web_ui,
                                const base::ListValue& args);
