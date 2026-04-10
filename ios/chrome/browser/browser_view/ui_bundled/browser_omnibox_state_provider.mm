2291b96b4d6f5c70ef849aad05b8c8150ce7792b
Gauthier Ambard <gambard@chromium.org>
2026-04-10 03:16:27 -0700

    [iOS] Don't check location bar state when Next is enabled
    When composebox is enabled, the location bar is no longer in charge of
    knowing if the omnibox is focused or not.
    In that case, the composebox should be used.
    
    This is gated behind Next flag and not composebox for now.
    
    Fixed: 488962794
    Change-Id: I9f0bb627cbd2c00d1c3fb82a979cbbe6db342532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748364
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612727}

--- ios/chrome/browser/browser_view/ui_bundled/browser_omnibox_state_provider.mm
+++ ios/chrome/browser/browser_view/ui_bundled/browser_omnibox_state_provider.mm
@@ -4,11 +4,16 @@
 
 #import "ios/chrome/browser/browser_view/ui_bundled/browser_omnibox_state_provider.h"
 
+#import "ios/chrome/browser/shared/public/features/features.h"
+
 @implementation BrowserOmniboxStateProvider
 
 #pragma mark - OmniboxStateProvider
 
 - (BOOL)isOmniboxFocused {
+  if (IsChromeNextIaEnabled()) {
+    return [self.composeboxStateProvider isOmniboxFocused];
+  }
   // First check if the composebox is focused. If it is, return true.
   // Otherwise, check if the location bar is focused.
   return [self.composeboxStateProvider isOmniboxFocused] ||
