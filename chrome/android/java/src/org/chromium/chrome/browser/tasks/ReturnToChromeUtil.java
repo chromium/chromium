8973b3e686c3befcc3f5986cdb93ff23c0d73cef
Peter Wen <wnwen@chromium.org>
2026-04-10 11:37:57 -0700

    android: Remove assert in ReturnToChromeUtil
    ReturnToChromeUtil.createNewTabAndShowHomeSurfaceUi attempts to display
    the home surface UI upon returning to Chrome by listening for the
    restoration of the last active tab. The assertion that the restored
    tab's URL matches the URL stored in the tab metadata file can fail if
    there is a data inconsistency. Since the code handles this scenario
    gracefully by returning from willAddTab, the assert is overly aggressive
    and doesn't provide any insights. Remove the assert so the application
    can gracefully handle this data discrepancy without terminating.
    
    Bug: 501387783
    Change-Id: I56137fa5c0fc130b2f72c920efcf3f832f91fcab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7749784
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613000}

--- chrome/android/java/src/org/chromium/chrome/browser/tasks/ReturnToChromeUtil.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tasks/ReturnToChromeUtil.java
@@ -46,7 +46,6 @@ import org.chromium.content_public.browser.LoadUrlParams;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.util.Locale;
 
 /**
  * This is a utility class for managing features related to returning to Chrome after haven't used
@@ -208,16 +207,6 @@ public final class ReturnToChromeUtil {
                         public void willAddTab(Tab tab, int type) {
                             boolean isTabExpected =
                                     TextUtils.equals(lastActiveTabUrl, tab.getUrl().getSpec());
-                            assert isTabExpected
-                                    : String.format(
-                                            Locale.ENGLISH,
-                                            "The URL of first Tab restored doesn't match the URL of"
-                                                + " the last active Tab read from the Tab state"
-                                                + " metadata file! Existing Tab count = %d. Last"
-                                                + " active tab = %s. First tab = %s.",
-                                            tabModelSelector.getModel(false).getCount(),
-                                            lastActiveTabUrl,
-                                            tab.getUrl().getSpec());
                             if (!isTabExpected) {
                                 return;
                             }
