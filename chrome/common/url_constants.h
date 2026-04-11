081cba11a43d5c1e8eb5b00536d43326f002d93a
Kunjan Vinay Patel <ksukh@google.com>
2026-04-10 17:36:35 -0700

    [PWA_Install_Dialog] Use persistent link for PWA "Learn More" button
    Updates the kInstallDialogFlowLearnMoreURL constant in
    chrome/common/url_constants.h to use a persistent link (p-link) instead
    of a direct support article URL.
    
    P-link: https://support.google.com/chrome?p=pwa_install
    Fixed: 498632327
    Change-Id: Ie03b0f06d586a9640de1a81eb84933a05e99b458
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748441
    Reviewed-by: Adriana Ixba <aixba@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Kunjan Vinay Patel <ksukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613196}

--- chrome/common/url_constants.h
+++ chrome/common/url_constants.h
@@ -324,11 +324,10 @@ inline constexpr char16_t kMemorySaverModeTabDiscardingHelpUrl[] =
 inline constexpr char16_t kIncognitoHelpCenterURL[] =
     u"https://support.google.com/chrome?p=incognito";
 
-// TODO(crbug.com/498632327): Replace this URL with P-link.
 // "Learn more" URL for the web app installation flow, linked from installation
 // dialog and options view.
 inline constexpr char kInstallDialogFlowLearnMoreURL[] =
-    "https://support.google.com/chrome/answer/9658361";
+    "https://support.google.com/chrome?p=pwa_install";
 
 // The URL for the "Learn more" page for the usage/crash reporting option in the
 // first run dialog.
