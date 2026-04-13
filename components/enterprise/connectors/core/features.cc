3e0b8b297504de2a524e4c668b4f6b936b2fcefd
Elias Homsi <eliashomsi@google.com>
2026-04-13 09:13:29 -0700

    Enable EnableEncryptedFileUpload by default
    The feature is already 100% on finch
    
    Change-Id: Ib7c391a274591a2e26d060645cee21540be729a4
    Bug: 418020892
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7745384
    Commit-Queue: Elias Homsi <eliashomsi@google.com>
    Auto-Submit: Elias Homsi <eliashomsi@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1613775}

--- components/enterprise/connectors/core/features.cc
+++ components/enterprise/connectors/core/features.cc
@@ -33,7 +33,7 @@ BASE_FEATURE_PARAM(size_t,
                    /*default_value=*/kDefaultMaxParallelActiveRequests);
 
 // Controls whether encrypted file upload is enabled.
-BASE_FEATURE(kEnableEncryptedFileUpload, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableEncryptedFileUpload, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables scanning of pasted images for DLP.
 BASE_FEATURE(kDlpScanPastedImages, base::FEATURE_ENABLED_BY_DEFAULT);
