51b12493c5a72b4d5d236fb5680e1e9a03a3dfdd
Bryan Oltman <bryanoltman@google.com>
2026-04-13 17:40:47 -0700

    Add test to validate occlusion detection
    This CL adds an interactive UI test to verify that occluding a browser
    window triggers a WebContents visibility change. This ensures that the
    occlusion state is detected and propagated, preventing regressions like
    the one addressed in crrev.com/c/7563790.
    
    Bug: 500927998
    Change-Id: I1859c3dbee6bd156e74b2646d4b88d8db810e426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7756720
    Commit-Queue: Bryan Oltman <bryanoltman@google.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614099}

---
+++ chrome/browser/ui/views/occlusion_visibility_interactive_uitest.cc
@@ -0,0 +1,89 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/test/base/interactive_test_utils.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
+#include "content/public/common/content_switches.h"
+#include "content/public/test/browser_test.h"
+#include "ui/views/test/widget_test.h"
+
+class VisibilityWaiter : public content::WebContentsObserver {
+ public:
+  VisibilityWaiter(content::WebContents* web_contents,
+                   content::Visibility expected_visibility)
+      : WebContentsObserver(web_contents),
+        expected_visibility_(expected_visibility) {}
+
+  void Wait() {
+    if (web_contents()->GetVisibility() == expected_visibility_) {
+      return;
+    }
+    run_loop_.Run();
+  }
+
+  // WebContentsObserver:
+  void OnVisibilityChanged(content::Visibility visibility) override {
+    if (visibility == expected_visibility_) {
+      run_loop_.Quit();
+    }
+  }
+
+ private:
+  content::Visibility expected_visibility_;
+  base::RunLoop run_loop_;
+};
+
+class OcclusionVisibilityInteractiveUITest : public InteractiveBrowserTest {
+ public:
+  OcclusionVisibilityInteractiveUITest() = default;
+
+  void SetUpOnMainThread() override {
+    InteractiveBrowserTest::SetUpOnMainThread();
+
+    // Occlusion is normally disabled for testing to prevent test flakiness,
+    // but is enabled here because we're testing occlusion.
+    base::CommandLine::ForCurrentProcess()->RemoveSwitch(
+        switches::kDisableBackgroundingOccludedWindowsForTesting);
+  }
+};
+
+// Verify that occluding a browser window triggers a WebContents visibility
+// change.
+// TODO(https://crbug.com/40122973): BringWindowToFront can get stuck in a loop
+// on Linux.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_OcclusionTriggersVisiblityChange \
+  DISABLED_OcclusionTriggersVisiblityChange
+#else
+#define MAYBE_OcclusionTriggersVisiblityChange OcclusionTriggersVisiblityChange
+#endif
+IN_PROC_BROWSER_TEST_F(OcclusionVisibilityInteractiveUITest,
+                       MAYBE_OcclusionTriggersVisiblityChange) {
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+
+  browser()->window()->SetBounds(gfx::Rect(100, 100, 640, 480));
+  browser()->window()->Show();
+  ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
+
+  Browser* browser2 = CreateBrowser(browser()->profile());
+
+  {
+    VisibilityWaiter waiter(web_contents, content::Visibility::OCCLUDED);
+    gfx::Rect bounds = browser()->window()->GetBounds();
+    bounds.Outset(50);
+    browser2->window()->SetBounds(bounds);
+    browser2->window()->Show();
+    ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser2));
+    waiter.Wait();
+  }
+
+  {
+    VisibilityWaiter waiter(web_contents, content::Visibility::VISIBLE);
+    browser2->window()->Close();
+    waiter.Wait();
+  }
+}
