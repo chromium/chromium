f869cd2250e139f0fc051bbab788e122ca1e0777
Zeyad El-arabaty <zelarabaty@google.com>
2026-04-13 17:37:50 -0700

    Sort sync bridge accessibility annotations by timestamp.
    This change modifies SyncBridgeDataProvider to sort the retrieved
    AccessibilityAnnotationSpecifics by their latest source timestamp in
    descending order before converting them into MemorySearchResults. This
    ensures that more recent annotations are prioritized.
    
    Bug: b:501524683
    Change-Id: I2ccd59b92004258574e0e7391cfcb9ed4606ed25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7747493
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Victor Shu <victorshu@google.com>
    Commit-Queue: Zeyad El-arabaty <zelarabaty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1614096}

--- components/accessibility_annotator/core/annotation_reducer/sync_bridge_data_provider.cc
+++ components/accessibility_annotator/core/annotation_reducer/sync_bridge_data_provider.cc
@@ -4,6 +4,7 @@
 
 #include "components/accessibility_annotator/core/annotation_reducer/sync_bridge_data_provider.h"
 
+#include <algorithm>
 #include <vector>
 
 #include "base/functional/bind.h"
@@ -12,8 +13,52 @@
 #include "components/accessibility_annotator/core/annotation_reducer/memory_search_result.h"
 #include "components/accessibility_annotator/core/data_models/entity_converter.h"
 #include "components/accessibility_annotator/core/storage/accessibility_annotator_backend.h"
+#include "components/sync/protocol/accessibility_annotation_specifics.pb.h"
 
 namespace accessibility_annotator {
+namespace {
+
+void SortAnnotationsByTimestamp(
+    std::vector<sync_pb::AccessibilityAnnotationSpecifics>& annotations) {
+  std::sort(
+      annotations.begin(), annotations.end(),
+      [](const sync_pb::AccessibilityAnnotationSpecifics& a,
+         const sync_pb::AccessibilityAnnotationSpecifics& b) {
+        auto get_timestamp = [](const sync_pb::AccessibilityAnnotationSpecifics&
+                                    specifics) {
+          int64_t max_timestamp = 0;
+          for (const auto& source : specifics.sources()) {
+            switch (source.source_case()) {
+              case sync_pb::AccessibilityAnnotationSpecifics::Source::
+                  kGmailSource:
+                max_timestamp = std::max(
+                    max_timestamp,
+                    source.gmail_source().received_time_unix_epoch_seconds());
+                break;
+              case sync_pb::AccessibilityAnnotationSpecifics::Source::
+                  kCalendarSource:
+                max_timestamp = std::max(
+                    max_timestamp, source.calendar_source()
+                                       .modified_time_unix_epoch_seconds());
+                break;
+              case sync_pb::AccessibilityAnnotationSpecifics::Source::
+                  kPhotosSource:
+                max_timestamp = std::max(
+                    max_timestamp,
+                    source.photos_source().creation_time_unix_epoch_seconds());
+                break;
+              case sync_pb::AccessibilityAnnotationSpecifics::Source::
+                  SOURCE_NOT_SET:
+                break;
+            }
+          }
+          return max_timestamp;
+        };
+        return get_timestamp(a) > get_timestamp(b);
+      });
+}
+
+}  // namespace
 
 SyncBridgeDataProvider::SyncBridgeDataProvider(
     AccessibilityAnnotatorBackend& backend)
@@ -35,6 +80,8 @@ void SyncBridgeDataProvider::RetrieveAll(
              base::OnceCallback<void(std::vector<MemorySearchResult>)> callback,
              std::vector<sync_pb::AccessibilityAnnotationSpecifics>
                  annotations) {
+            SortAnnotationsByTimestamp(annotations);
+
             std::vector<MemorySearchResult> results;
             for (const sync_pb::AccessibilityAnnotationSpecifics& specifics :
                  annotations) {
--- components/accessibility_annotator/core/annotation_reducer/sync_bridge_data_provider_unittest.cc
+++ components/accessibility_annotator/core/annotation_reducer/sync_bridge_data_provider_unittest.cc
@@ -25,9 +25,9 @@ namespace accessibility_annotator {
 namespace {
 
 using ::testing::AllOf;
+using ::testing::ElementsAre;
 using ::testing::Field;
 using ::testing::IsEmpty;
-using ::testing::UnorderedElementsAre;
 
 testing::Matcher<MemorySearchResult> MatchesMemorySearchResult(
     EntryType expected_type,
@@ -36,10 +36,17 @@ testing::Matcher<MemorySearchResult> MatchesMemorySearchResult(
                Field(&MemorySearchResult::value, expected_value));
 }
 
-sync_pb::AccessibilityAnnotationSpecifics CreateSpecifics(const std::string& id,
-                                                          EntityType type) {
+sync_pb::AccessibilityAnnotationSpecifics
+CreateSpecifics(const std::string& id, EntityType type, int64_t timestamp = 0) {
   sync_pb::AccessibilityAnnotationSpecifics specifics;
   specifics.set_id(id);
+
+  if (timestamp > 0) {
+    auto* source = specifics.add_sources();
+    source->mutable_gmail_source()->set_received_time_unix_epoch_seconds(
+        timestamp);
+  }
+
   switch (type) {
     case EntityType::kOrder:
       specifics.mutable_order()->set_order_id("order_" + id);
@@ -76,7 +83,7 @@ TEST_F(SyncBridgeDataProviderTest, RetrieveAll_SingleEntry) {
   base::RunLoop run_loop;
   base::MockCallback<base::OnceCallback<void(std::vector<MemorySearchResult>)>>
       callback;
-  EXPECT_CALL(callback, Run(UnorderedElementsAre(MatchesMemorySearchResult(
+  EXPECT_CALL(callback, Run(ElementsAre(MatchesMemorySearchResult(
                             EntryType::kOrderId, u"order_1"))))
       .WillOnce([&]() { run_loop.Quit(); });
 
@@ -84,19 +91,66 @@ TEST_F(SyncBridgeDataProviderTest, RetrieveAll_SingleEntry) {
   run_loop.Run();
 }
 
-TEST_F(SyncBridgeDataProviderTest, RetrieveAll_MultipleEntriesAndFiltering) {
+TEST_F(SyncBridgeDataProviderTest,
+       RetrieveAll_MultipleEntriesAndFiltering_SortsByTimestamp) {
   backend_.SetSyncAnnotations(
-      {CreateSpecifics("1", EntityType::kOrder),
-       CreateSpecifics("2", EntityType::kFlightReservation),
-       CreateSpecifics("3", EntityType::kOrder)});
+      {CreateSpecifics("1", EntityType::kOrder, /*timestamp=*/100),
+       CreateSpecifics("2", EntityType::kFlightReservation, /*timestamp=*/400),
+       CreateSpecifics("3", EntityType::kOrder, /*timestamp=*/200),
+       CreateSpecifics("4", EntityType::kOrder, /*timestamp=*/50)});
 
   base::RunLoop run_loop;
   base::MockCallback<base::OnceCallback<void(std::vector<MemorySearchResult>)>>
       callback;
+  // Order 3 has timestamp 200, Order 1 has 100, Order 4 has 50.
+  // We expect them to be sorted in descending order of timestamp.
   EXPECT_CALL(callback,
-              Run(UnorderedElementsAre(
+              Run(ElementsAre(
+                  MatchesMemorySearchResult(EntryType::kOrderId, u"order_3"),
                   MatchesMemorySearchResult(EntryType::kOrderId, u"order_1"),
-                  MatchesMemorySearchResult(EntryType::kOrderId, u"order_3"))))
+                  MatchesMemorySearchResult(EntryType::kOrderId, u"order_4"))))
+      .WillOnce([&]() { run_loop.Quit(); });
+  provider()->RetrieveAll(EntryType::kOrderId, callback.Get());
+  run_loop.Run();
+}
+
+TEST_F(SyncBridgeDataProviderTest,
+       RetrieveAll_MultipleSourcesForSameEntity_SortsByMaxTimestamp) {
+  sync_pb::AccessibilityAnnotationSpecifics specifics1 =
+      CreateSpecifics("1", EntityType::kOrder, /*timestamp=*/100);
+
+  sync_pb::AccessibilityAnnotationSpecifics specifics2;
+  specifics2.set_id("2");
+  specifics2.mutable_order()->set_order_id("order_2");
+  auto* source2_1 = specifics2.add_sources();
+  source2_1->mutable_gmail_source()->set_received_time_unix_epoch_seconds(50);
+  auto* source2_2 = specifics2.add_sources();
+  source2_2->mutable_calendar_source()->set_modified_time_unix_epoch_seconds(
+      400);
+  auto* source2_3 = specifics2.add_sources();
+  source2_3->mutable_photos_source()->set_creation_time_unix_epoch_seconds(150);
+
+  sync_pb::AccessibilityAnnotationSpecifics specifics3;
+  specifics3.set_id("3");
+  specifics3.mutable_order()->set_order_id("order_3");
+  auto* source3_1 = specifics3.add_sources();
+  source3_1->mutable_photos_source()->set_creation_time_unix_epoch_seconds(200);
+  auto* source3_2 = specifics3.add_sources();
+  source3_2->mutable_gmail_source()->set_received_time_unix_epoch_seconds(10);
+
+  backend_.SetSyncAnnotations({specifics1, specifics2, specifics3});
+
+  base::RunLoop run_loop;
+  base::MockCallback<base::OnceCallback<void(std::vector<MemorySearchResult>)>>
+      callback;
+  // Order 2 has max timestamp 400, Order 3 has max timestamp 200, Order 1 has
+  // 100. We expect them to be sorted in descending order of their max
+  // timestamp.
+  EXPECT_CALL(callback,
+              Run(ElementsAre(
+                  MatchesMemorySearchResult(EntryType::kOrderId, u"order_2"),
+                  MatchesMemorySearchResult(EntryType::kOrderId, u"order_3"),
+                  MatchesMemorySearchResult(EntryType::kOrderId, u"order_1"))))
       .WillOnce([&]() { run_loop.Quit(); });
   provider()->RetrieveAll(EntryType::kOrderId, callback.Get());
   run_loop.Run();
