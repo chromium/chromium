1159e58adb8784372b94c5d9e6ccbd6aa30e3cf3
Michael Ershov <miersh@google.com>
2026-04-14 07:03:57 -0700

    [gardening] Temporary disable a flaky test
    Example failure: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Tests/178769/overview
    
    Bug: 500570908
    Test: PasswordManagerSyncTest.OfferToUpdatePrimaryAccountCredential
    Change-Id: Id9d8f8af8a78ee9d32d9a14097c961a446b66abc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762244
    Auto-Submit: Michael Ershov <miersh@google.com>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: Pavol Marko <pmarko@chromium.org>
    Owners-Override: Michael Ershov <miersh@google.com>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1614418}

--- chrome/browser/sync/test/integration/password_manager_sync_test.cc
+++ chrome/browser/sync/test/integration/password_manager_sync_test.cc
@@ -689,7 +689,7 @@ IN_PROC_BROWSER_TEST_F(PasswordManagerSyncTest,
 }
 
 // TODO(crbug.com/500570908): Enable the test.
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) && defined(ADDRESS_SANITIZER)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 #define MAYBE_OfferToSaveNonPrimaryAccountCredential \
   DISABLED_OfferToSaveNonPrimaryAccountCredential
 #else
