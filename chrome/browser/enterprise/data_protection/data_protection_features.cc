abf1c2ee4604f2f6b49ac78c0bb170a0cca2a203
Elias Homsi <eliashomsi@google.com>
2026-04-15 09:47:32 -0700

    Enable ForceDownloadToOneDrive by default.
    This changes the default state of the kEnableForceDownloadToOneDrive
    feature flag to enabled.
    
    Bug: 458033434
    Change-Id: Id51f75b097de8c9c152026208aba998cdd125f5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7757504
    Commit-Queue: Elias Homsi <eliashomsi@google.com>
    Auto-Submit: Elias Homsi <eliashomsi@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615246}

--- chrome/browser/enterprise/data_protection/data_protection_features.cc
+++ chrome/browser/enterprise/data_protection/data_protection_features.cc
@@ -10,7 +10,7 @@ namespace enterprise_data_protection {
 
 BASE_FEATURE(kEnableForceDownloadToCloud, base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kEnableForceDownloadToOneDrive, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableForceDownloadToOneDrive, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kEnableTabSharingProtection, base::FEATURE_DISABLED_BY_DEFAULT);
 
