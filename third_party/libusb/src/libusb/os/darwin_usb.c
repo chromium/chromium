921dab5eb5cd8a6f941d16e2544960c938dcab71
Matt Reynolds <mattreynolds@google.com>
2026-04-15 12:38:18 -0700

    [M147] usb: Fix potential UaF in darwin_release_interface
    Original change's description:
    > usb: Fix potential UaF in darwin_release_interface
    >
    > Imports https://github.com/libusb/libusb/commit/edd1a4bfe4b037e67b65e4538fd880123fb004f7
    >
    > Bug: 499013281
    > Change-Id: Id25ec131e5a3e4059c5ee15ecdf476cf46a43aa8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759936
    > Reviewed-by: Rob Pitkin <robpitkin@chromium.org>
    > Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1614701}
    
    (cherry picked from commit 7095817f5582391e2ea5787ab65777f84b993fa4)
    
    Bug: 502820686,499013281
    Change-Id: Id25ec131e5a3e4059c5ee15ecdf476cf46a43aa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7764471
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7727@{#2983}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- third_party/libusb/src/libusb/os/darwin_usb.c
+++ third_party/libusb/src/libusb/os/darwin_usb.c
@@ -1261,6 +1261,7 @@ static int darwin_release_interface(struct libusb_device_handle *dev_handle, int
   if (cInterface->cfSource) {
     CFRunLoopRemoveSource (libusb_darwin_acfl, cInterface->cfSource, kCFRunLoopDefaultMode);
     CFRelease (cInterface->cfSource);
+    cInterface->cfSource = NULL;
   }
 
   kresult = (*(cInterface->interface))->USBInterfaceClose(cInterface->interface);
