709089c1ddf3a4b07230d1c87c9cba7b3fe6e025
Oleksandr Tara <otara@google.com>
2026-04-15 11:59:59 -0700

    [M148] Enable kActorLoginFederatedClickFromActor by default.
    Original change's description:
    > Enable kActorLoginFederatedClickFromActor by default.
    >
    > Bug: b:480920277
    > Change-Id: I6a1aff1c04f6bb8ca446607959f5a1ae9061277f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761764
    > Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    > Auto-Submit: Oleksandr Tara <otara@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1614547}
    
    (cherry picked from commit ed657fb86f6f638ac517f4ae3c4931038d0a8eac)
    
    Bug: 502823985,b:480920277
    Change-Id: I6a1aff1c04f6bb8ca446607959f5a1ae9061277f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7763106
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7778@{#720}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -15,7 +15,7 @@ BASE_FEATURE(kActorLogin, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kActorLoginConflictingPermissionCleanup,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kActorLoginFederatedClickFromActor,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kActorLoginFieldVisibilityCheck, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kActorLoginLocalClassificationModel,
              base::FEATURE_ENABLED_BY_DEFAULT);
