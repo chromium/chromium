053b2cfe77aa9e1230db40b267df4d1fa97a8711
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-04-15 15:09:22 -0700

    [Extensions] Add missing licenses
    Bug: None
    Change-Id: I267cdfe19ac4e04a4c820e073785fc88061dbad4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7764463
    Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    Reviewed-by: Andrea Orru <andreaorru@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615440}

--- chrome/test/data/extensions/file_iframe/window_onload.js
+++ chrome/test/data/extensions/file_iframe/window_onload.js
@@ -1,3 +1,7 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 window.onload = function() {
   var iframe = document.getElementById('file_iframe');
   try {
--- chrome/test/data/extensions/permissions/unknown/test.js
+++ chrome/test/data/extensions/permissions/unknown/test.js
@@ -1,3 +1,7 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // place holder...
 var x = 0;
 x = x + 1;
