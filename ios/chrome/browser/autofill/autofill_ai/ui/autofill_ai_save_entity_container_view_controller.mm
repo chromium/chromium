c0d12d6143138913e3cbc5cf14fffd237f3b9d83
Leo Zhao <leozhao@google.com>
2026-04-16 12:00:32 -0700

    [iOS][Forms AI] Branded title for kVehicle entity type
    This CL restores a boolean flag from `isLocalSave` to
    `saveIsSynchronous` (the previous name). The reason is that `kVehicle`
    can be a wallet item, but it is saved synchronously. As part of the
    renaming, this CL also updates the code using isLocalSave to determine
    whether to show branded title to use `record_type` of the entity.
    
    Screenshots:
    
    Before: https://screenshot.googleplex.com/7YYkudN6KXmcjNq
    After: https://screenshot.googleplex.com/6F5ep3bKQg5SXwn
    
    Bug: 503337205
    Change-Id: I24c4a7ffbb61edc0246f6ef93adcb3a32410cc47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7768554
    Commit-Queue: Leo Zhao <leozhao@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616039}

--- ios/chrome/browser/autofill/autofill_ai/ui/autofill_ai_save_entity_container_view_controller.mm
+++ ios/chrome/browser/autofill/autofill_ai/ui/autofill_ai_save_entity_container_view_controller.mm
@@ -44,8 +44,8 @@ constexpr CGFloat kButtonStackVerticalMargin = 16;
   // Button title.
   NSString* _buttonTitle;
 
-  // Denotes if the save is local (synchronous).
-  BOOL _isLocalSave;
+  // Denotes if the save is synchronous.
+  BOOL _saveIsSynchronous;
 }
 
 - (instancetype)init {
@@ -126,9 +126,9 @@ constexpr CGFloat kButtonStackVerticalMargin = 16;
 #pragma mark - AutofillAISaveEntityConsumer
 
 - (void)setNewEntity:(autofill::EntityInstance)newEntity
-           oldEntity:(std::optional<autofill::EntityInstance>)oldEntity
-           userEmail:(const std::u16string&)userEmail
-         isLocalSave:(BOOL)isLocalSave {
+            oldEntity:(std::optional<autofill::EntityInstance>)oldEntity
+            userEmail:(const std::u16string&)userEmail
+    saveIsSynchronous:(BOOL)saveIsSynchronous {
   // Forward the data to the table view controller for display.
   [_tableViewController setNewEntity:newEntity
                            oldEntity:oldEntity
@@ -139,14 +139,15 @@ constexpr CGFloat kButtonStackVerticalMargin = 16;
       oldEntity.has_value() ? autofill::GetDialogTitleForUpdateEntity(typeName)
                             : autofill::GetDialogTitleForSaveEntity(typeName);
 
-  if (isLocalSave) {
+  if (newEntity.record_type() !=
+      autofill::EntityInstance::RecordType::kServerWallet) {
     self.navigationItem.titleView = nil;
     self.title = titleString;
   } else {
     self.navigationItem.titleView =
         autofill::CreateBrandedTitleForWalletSave(titleString);
   }
-  _isLocalSave = isLocalSave;
+  _saveIsSynchronous = saveIsSynchronous;
 
   // Update the button title based on whether it's an update or save.
   _buttonTitle = l10n_util::GetNSString(
@@ -190,7 +191,7 @@ constexpr CGFloat kButtonStackVerticalMargin = 16;
   // Only dismiss immediately if it's a synchronous local save.
   // Otherwise, the UI stays open to show the loading state. Once the async call
   // is completed, the UI is informed and the loading state is dismissed.
-  if (_isLocalSave) {
+  if (_saveIsSynchronous) {
     [self.autofillHandler dismissSaveEntityDialog];
   }
 }
