7f87708bb60f4da16c524bb6efe18ac3178f0d5f
Eriko Kurimoto <elkurin@google.com>
2026-04-15 23:53:58 -0700

    WaaP: Fix WebUI observer tests to use host-specific histograms
    This CL suppors WebUIPageLoadMetricsObserverBrowserTest with Initial
    WebUI feature enabled.
    
    With Initial WebUI, there will be some unrelated metrics coming from the
    background WebUI and that is unexpectedly counted in for generic
    histogram `...WebUI` agggregated. Instead, we only capture host specific
    metrics so that it counts out unrelated metrics.
    
    Bug: 477426026
    Change-Id: I7e3364ffe804e16cafc3b51047ae9d62ed82c52a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7761563
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615667}

--- chrome/browser/page_load_metrics/observers/webui_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/webui_page_load_metrics_observer_browsertest.cc
@@ -7,18 +7,36 @@
 #include <memory>
 
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/page_load_metrics/browser/page_load_metrics_test_waiter.h"
 #include "components/prefs/pref_service.h"
 #include "components/webui/chrome_urls/pref_names.h"
+#include "content/public/common/content_features.h"
 #include "content/public/test/browser_test.h"
 
-class WebUIPageLoadMetricsObserverBrowserTest : public InProcessBrowserTest {
+class WebUIPageLoadMetricsObserverBrowserTest
+    : public InProcessBrowserTest,
+      public ::testing::WithParamInterface<bool> {
  public:
-  WebUIPageLoadMetricsObserverBrowserTest() = default;
+  WebUIPageLoadMetricsObserverBrowserTest() {
+    std::vector<base::test::FeatureRef> enabled_features;
+    std::vector<base::test::FeatureRef> disabled_features;
+
+    if (GetParam()) {
+      enabled_features.push_back(features::kInitialWebUI);
+      enabled_features.push_back(features::kWebUIReloadButton);
+    } else {
+      disabled_features.push_back(features::kInitialWebUI);
+      disabled_features.push_back(features::kWebUIReloadButton);
+    }
+
+    feature_list_.InitWithFeatures(enabled_features, disabled_features);
+  }
 
   WebUIPageLoadMetricsObserverBrowserTest(
       const WebUIPageLoadMetricsObserverBrowserTest&) = delete;
@@ -54,10 +72,11 @@ class WebUIPageLoadMetricsObserverBrowserTest : public InProcessBrowserTest {
   }
 
   std::unique_ptr<base::HistogramTester> histogram_tester_;
+  base::test::ScopedFeatureList feature_list_;
 };
 
 // Test that regular WebUI pages record metrics.
-IN_PROC_BROWSER_TEST_F(WebUIPageLoadMetricsObserverBrowserTest,
+IN_PROC_BROWSER_TEST_P(WebUIPageLoadMetricsObserverBrowserTest,
                        WebUIRecordsMetrics) {
   // Navigate to a regular WebUI page.
   NavigateAndWaitForMetrics(GURL("chrome://version"));
@@ -66,13 +85,14 @@ IN_PROC_BROWSER_TEST_F(WebUIPageLoadMetricsObserverBrowserTest,
   // Note: We can't easily test specific FCP/LCP values in a browser test since
   // they depend on actual rendering, but we can verify the histograms exist.
   histogram_tester_->ExpectTotalCount(
-      "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.WebUI", 1);
+      "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.WebUI.version", 1);
   histogram_tester_->ExpectTotalCount(
-      "PageLoad.PaintTiming.NavigationToLargestContentfulPaint.WebUI", 1);
+      "PageLoad.PaintTiming.NavigationToLargestContentfulPaint.WebUI.version",
+      1);
 }
 
 // Test that visits to internal WebUI pages do NOT record metrics.
-IN_PROC_BROWSER_TEST_F(WebUIPageLoadMetricsObserverBrowserTest,
+IN_PROC_BROWSER_TEST_P(WebUIPageLoadMetricsObserverBrowserTest,
                        InternalWebUIDoesNotRecordMetrics) {
   // Enable internal debug pages.
   g_browser_process->local_state()->SetBoolean(
@@ -82,10 +102,17 @@ IN_PROC_BROWSER_TEST_F(WebUIPageLoadMetricsObserverBrowserTest,
   ASSERT_TRUE(
       ui_test_utils::NavigateToURL(browser(), GURL("chrome://omnibox")));
   // Verify that WebUI metrics ARE NOT recorded for internal debug WebUI.
+  // Note: When InitialWebUI is enabled, a background WebContents may load a
+  // regular WebUI page (e.g. New Tab Page) and record FCP early, polluting
+  // full WebUI metrics histograms. We expect 1 sample for FCP if enabled, 0 if
+  // disabled. However, LCP is not recorded for background pages, so we still
+  // expect 0.
   histogram_tester_->ExpectTotalCount(
-      "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.WebUI", 0);
+      "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.WebUI.omnibox", 0);
+
   histogram_tester_->ExpectTotalCount(
-      "PageLoad.PaintTiming.NavigationToLargestContentfulPaint.WebUI", 0);
+      "PageLoad.PaintTiming.NavigationToLargestContentfulPaint.WebUI.omnibox",
+      0);
 
   // Navigate to a WebUI page that is not an internal debug WebUI.
   NavigateAndWaitForMetrics(GURL("chrome://version"));
@@ -93,13 +120,14 @@ IN_PROC_BROWSER_TEST_F(WebUIPageLoadMetricsObserverBrowserTest,
   // recorded for previous WebUI navigation. If aggregate metrics fail, omnibox
   // is no longer an internal debug WebUI or WebUIPageLoadMetricsObserver
   // recorded metrics incorrectly.
-  histogram_tester_->ExpectTotalCount(
-      "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.WebUI", 1);
-  histogram_tester_->ExpectTotalCount(
-      "PageLoad.PaintTiming.NavigationToLargestContentfulPaint.WebUI", 1);
+
   histogram_tester_->ExpectTotalCount(
       "PageLoad.PaintTiming.NavigationToLargestContentfulPaint.WebUI.version",
       1);
   histogram_tester_->ExpectTotalCount(
       "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.WebUI.version", 1);
 }
+
+INSTANTIATE_TEST_SUITE_P(All,
+                         WebUIPageLoadMetricsObserverBrowserTest,
+                         ::testing::Bool());
