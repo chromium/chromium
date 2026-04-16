ba34409c98068984892765f2724c25c4f231ea63
Anna Sato <annasato@chromium.org>
2026-04-15 23:33:39 -0700

    Replace internal WebSocket BFCache test with external WPT.
    This CL replaces the internal
    `disconnect-websocket-on-bfcache.window.js` test with a new external
    WPT,
    `back-forward-cache-closes-open-websocket-connection.tentative.window.js`,
    within the `disconnect-websocket-on-bfcache-disabled` virtual test
    suite. The new test verifies that open WebSocket connections are closed
    when a page enters the Back-Forward Cache.
    
    Bug: 467838624
    Change-Id: Ice31d26211078116b90408a9792eb9d742cb9a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7766812
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615658}

--- third_party/blink/web_tests/external/wpt/websockets/back-forward-cache-closes-open-websocket-connection.tentative.window.js
+++ third_party/blink/web_tests/external/wpt/websockets/back-forward-cache-closes-open-websocket-connection.tentative.window.js
@@ -1,10 +1,10 @@
-// META: title=Testing BFCache support for page with open WebSocket connection.
+// META: title=Testing BFCache support for page with open WebSocket connection, where the WebSocket connection is closed on BFCache.
 // META: script=/common/dispatcher/dispatcher.js
 // META: script=/common/utils.js
 // META: script=/html/browsers/browsing-the-web/back-forward-cache/resources/rc-helper.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
 // META: script=/websockets/constants.sub.js
-// META: script=/websockets/resources/websockets-test-helpers.sub.js
+// META: script=resources/websockets-test-helpers.sub.js
 
 'use strict';
 
@@ -15,11 +15,9 @@ promise_test(async t => {
   const rc1 = await rcHelper.addWindow(
       /*config=*/ null, /*options=*/ { features: 'noopener' });
   await openWebSocket(rc1);
-
-  // The page should be eligible for BFCache because the WebSocket connection
-  // will be closed when the page is entered into BFCache.
+  // The page should be eligible for BFCache and the WebSocket connection will
+  // be closed when the page is entered into BFCache.
   await assertBFCacheEligibility(rc1, /*shouldRestoreFromBFCache=*/ true);
-
   // Read WebSocket event flags
   const { wsError, wsClose } = await readWebSocketCloseAndErrorFlags(rc1);
   assert_true(wsError, 'WebSocket should have error');
