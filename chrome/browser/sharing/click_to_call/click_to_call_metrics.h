0353b87d53fedaf8280d2e82523d9a04c8d251ba
Gazal <agazal@google.com>
2026-04-16 08:35:51 -0700

    [lint-sync] Add missing LINT guards in click_to_call_metrics.h
    Change-Id: I6a240e4ce41ee94670d3409734e8b13d295d4f51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759145
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1615897}

--- chrome/browser/sharing/click_to_call/click_to_call_metrics.h
+++ chrome/browser/sharing/click_to_call/click_to_call_metrics.h
@@ -11,25 +11,27 @@ class WebContents;
 
 // Entry point of a Click to Call journey.
 // These values are logged to UKM. Entries should not be renumbered and numeric
-// values should never be reused. Please keep in sync with
-// "SharingClickToCallEntryPoint" in src/tools/metrics/histograms/enums.xml.
+// values should never be reused.
+// LINT.IfChange(SharingClickToCallEntryPoint)
 enum class SharingClickToCallEntryPoint {
   kLeftClickLink = 0,
   kRightClickLink = 1,
   kRightClickSelection = 2,
   kMaxValue = kRightClickSelection,
 };
+// LINT.ThenChange(//tools/metrics/histograms/enums.xml:SharingClickToCallEntryPoint)
 
 // Selection at the end of a Click to Call journey.
 // These values are logged to UKM. Entries should not be renumbered and numeric
-// values should never be reused. Please keep in sync with
-// "SharingClickToCallSelection" in src/tools/metrics/histograms/enums.xml.
+// values should never be reused.
+// LINT.IfChange(SharingClickToCallSelection)
 enum class SharingClickToCallSelection {
   kNone = 0,
   kDevice = 1,
   kApp = 2,
   kMaxValue = kApp,
 };
+// LINT.ThenChange(//tools/metrics/histograms/enums.xml:SharingClickToCallSelection)
 
 // Records a Click to Call selection to UKM. This is logged after a completed
 // action like selecting an app or a device to send the phone number to.
