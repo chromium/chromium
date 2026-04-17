14a63d98b5f855db621acc4e0b01409b425db642
Jeffrey Yu <yuje@chromium.org>
2026-04-17 14:12:15 -0700

    Change string to string_view to avoid an unnecessary copy.
    `.substr()` on a string_view returns another string_view instead of
    creating a new string.
    
    Bug: 501076278
    Change-Id: Ifa61bdb75ba7f91decb466f781d2bdee330c19eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7758040
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616849}

--- net/proxy_resolution/pac_file_fetcher_impl.cc
+++ net/proxy_resolution/pac_file_fetcher_impl.cc
@@ -66,7 +66,7 @@ struct BomMapping {
   const char* charset;
 };
 
-const BomMapping kBomMappings[] = {
+constexpr BomMapping kBomMappings[] = {
     {"\xFE\xFF", "utf-16be"},
     {"\xFF\xFE", "utf-16le"},
     {"\xEF\xBB\xBF", "utf-8"},
@@ -76,13 +76,12 @@ const BomMapping kBomMappings[] = {
 // to |*utf16|.
 // If |charset| is empty, then we don't know what it was and guess.
 void ConvertResponseToUTF16(const std::string& charset,
-                            const std::string& bytes,
+                            std::string_view bytes,
                             std::u16string* utf16) {
   if (charset.empty()) {
     // Guess the charset by looking at the BOM.
-    std::string_view bytes_str(bytes);
     for (const auto& bom : kBomMappings) {
-      if (bytes_str.starts_with(bom.prefix)) {
+      if (bytes.starts_with(bom.prefix)) {
         return ConvertResponseToUTF16(
             bom.charset,
             // Strip the BOM in the converted response.
