df79699323bb464e0abba15aac29244b8c7b945b
Oleksandr Tara <otara@google.com>
2026-04-17 06:27:56 -0700

    [M148] [Actor Login M2] Remove heuristics-based button clicking
    Original change's description:
    > [Actor Login M2] Remove heuristics-based button clicking
    >
    > Bug: b:480920277
    > Change-Id: I4697d56592db129e06f956a7777fab12e43999af
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7762001
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Oleksandr Tara <otara@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1615722}
    
    (cherry picked from commit a20ff5d886e2d2015c34b284663f3183baeaf6a7)
    
    Bug: 503254444,b:480920277
    Change-Id: I4697d56592db129e06f956a7777fab12e43999af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771191
    Reviewed-by: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Oleksandr Tara <otara@google.com>
    Cr-Commit-Position: refs/branch-heads/7778@{#915}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- chrome/browser/password_manager/actor_login/internal/actor_login_siwg_controller.cc
+++ chrome/browser/password_manager/actor_login/internal/actor_login_siwg_controller.cc
@@ -4,7 +4,6 @@
 
 #include "chrome/browser/password_manager/actor_login/internal/actor_login_siwg_controller.h"
 
-#include "base/barrier_callback.h"
 #include "base/containers/to_vector.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
@@ -14,12 +13,11 @@
 #include "chrome/common/actor.mojom.h"
 #include "chrome/common/actor/action_result.h"
 #include "chrome/common/chrome_render_frame.mojom.h"
-#include "components/autofill/content/browser/content_autofill_driver.h"
-#include "components/autofill/content/common/mojom/autofill_agent.mojom.h"
 #include "components/password_manager/core/browser/features/password_features.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/webid/identity_credential_source.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
@@ -57,19 +55,6 @@ AttemptLoginOutcomeMqls FromFederatedLoginResultToMqls(
     case content::webid::FederatedLoginResult::kTimeoutByEmbedder:
       return AttemptLoginOutcomeMqls::kFederatedTimeout;
   }
-}
-
-// Finds the local root of a given RenderFrameHost.
-content::RenderFrameHost* GetLocalRoot(content::RenderFrameHost* rfh) {
-  content::RenderFrameHost* local_root = rfh;
-  while (local_root && local_root->GetParent()) {
-    if (local_root->GetRenderWidgetHost() !=
-        local_root->GetParent()->GetRenderWidgetHost()) {
-      break;
-    }
-    local_root = local_root->GetParent();
-  }
-  return local_root;
 }
 
 LoginStatusResult FromFederatedLoginResult(
@@ -169,24 +154,10 @@ void ActorLoginSiwgController::StartFederatedLogin(
   }
   if (!source->SelectAccount(credential_.federation_detail->idp_origin,
                              credential_.federation_detail->account_id)) {
-    if (base::FeatureList::IsEnabled(
-            password_manager::features::kActorLoginFederatedClickFromActor)) {
-      federated_attempt_login_details_.set_button_click_required(true);
-      std::move(on_finished_callback_)
-          .Run(LoginStatusResult::kRequiresButtonClick);
-    } else {
-      ClickSiwgButton();
-    }
+    federated_attempt_login_details_.set_button_click_required(true);
+    std::move(on_finished_callback_)
+        .Run(LoginStatusResult::kRequiresButtonClick);
   }
-}
-
-void ActorLoginSiwgController::ClickSiwgButton() {
-  get_page_content_provider_.Run(
-      web_contents(),
-      optimization_guide::ActionableAIPageContentOptions(
-          /*on_critical_path=*/false),
-      base::BindOnce(&ActorLoginSiwgController::OnPageContentReceived,
-                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 void ActorLoginSiwgController::OnFederatedLoginResultReceived(
@@ -242,119 +213,6 @@ void ActorLoginSiwgController::LogFederatedLoginResult(
       (base::TimeTicks::Now() - attempt_login_tool_start_time_)
           .InMilliseconds());
   mqls_logger_->AddAttemptLoginDetails(federated_attempt_login_details_);
-}
-
-void ActorLoginSiwgController::OnPageContentReceived(
-    optimization_guide::AIPageContentResultOrError content) {
-  if (!content.has_value()) {
-    if (on_finished_callback_) {
-      std::move(on_finished_callback_)
-          // TODO(crbug.com/478799141): add new status for SiwG failure.
-          .Run(base::unexpected(ActorLoginError::kFillingNotAllowed));
-    }
-    return;
-  }
-
-  // Move the page content directly into the finder.
-  siwg_finder_ = std::make_unique<SiwgButtonFinder>(std::move(content->proto));
-
-  // This will find 0 or 1 buttons per frame and click each one found. If we
-  // ever want to productionize this code, we should add some logic to choose
-  // between buttons in different frames.
-  std::vector<autofill::ContentAutofillDriver*> drivers;
-  web_contents()->GetPrimaryMainFrame()->ForEachRenderFrameHost(
-      [&](content::RenderFrameHost* rfh) {
-        if (autofill::ContentAutofillDriver* driver =
-                autofill::ContentAutofillDriver::GetForRenderFrameHost(rfh)) {
-          drivers.push_back(driver);
-        }
-      });
-
-  auto all_frames_scanned_barrier =
-      base::BarrierCallback<FrameSiwgButtonCandidates>(
-          drivers.size(),
-          base::BindOnce(&ActorLoginSiwgController::OnAllFramesScanned,
-                         weak_ptr_factory_.GetWeakPtr()));
-
-  for (autofill::ContentAutofillDriver* driver : drivers) {
-    driver->GetAutofillAgent()->FindPotentialSiwgButtons(
-        base::BindOnce(&ActorLoginSiwgController::OnPotentialSiwgButtonsFound,
-                       weak_ptr_factory_.GetWeakPtr(),
-                       driver->render_frame_host()->GetGlobalId(),
-                       all_frames_scanned_barrier));
-  }
-}
-
-void ActorLoginSiwgController::OnPotentialSiwgButtonsFound(
-    content::GlobalRenderFrameHostId rfh_id,
-    base::OnceCallback<void(FrameSiwgButtonCandidates)> barrier_callback,
-    std::vector<autofill::mojom::SiwgButtonDataPtr> buttons) {
-  std::move(barrier_callback).Run(std::make_pair(rfh_id, std::move(buttons)));
-}
-
-void ActorLoginSiwgController::OnAllFramesScanned(
-    std::vector<FrameSiwgButtonCandidates> results) {
-  CHECK(siwg_finder_);
-  CHECK(on_finished_callback_);
-
-  for (const auto& [rfh_id, buttons] : results) {
-    content::RenderFrameHost* rfh = content::RenderFrameHost::FromID(rfh_id);
-    if (!rfh) {
-      // Frame went away.
-      continue;
-    }
-    std::optional<SiwgButtonFinder::SiwgButton> button =
-        siwg_finder_->FindButton(rfh, buttons);
-    if (button) {
-      ClickButton(rfh, button->dom_node_id, std::move(button->observed_target));
-      // Ensure we only click one button.
-      // ClickButton is the terminal state of this flow - it will resolve
-      // on_finished_callback_ after the click result is received.
-      return;
-    }
-  }
-
-  std::move(on_finished_callback_)
-      // TODO(crbug.com/478799141): add new status for SiwG failure.
-      .Run(base::unexpected(ActorLoginError::kFillingNotAllowed));
-}
-
-void ActorLoginSiwgController::ClickButton(
-    content::RenderFrameHost* rfh,
-    int dom_node_id,
-    actor::mojom::ObservedToolTargetPtr observed_target) {
-  CHECK(!base::FeatureList::IsEnabled(
-      password_manager::features::kActorLoginFederatedClickFromActor));
-  GetLocalRoot(rfh)->GetRemoteAssociatedInterfaces()->GetInterface(
-      &chrome_render_frame_);
-
-  auto invocation = actor::mojom::ToolInvocation::New();
-  auto click = actor::mojom::ClickAction::New();
-  click->type = actor::mojom::ClickType::kLeft;
-  click->count = actor::mojom::ClickCount::kSingle;
-  invocation->action = actor::mojom::ToolAction::NewClick(std::move(click));
-  invocation->target = actor::mojom::ToolTarget::NewDomNodeId(dom_node_id);
-  invocation->observed_target = std::move(observed_target);
-
-  chrome_render_frame_->InvokeTool(
-      std::move(invocation),
-      base::BindOnce(&ActorLoginSiwgController::OnClickFinished,
-                     weak_ptr_factory_.GetWeakPtr()));
-}
-
-void ActorLoginSiwgController::OnClickFinished(
-    actor::mojom::ActionResultPtr result) {
-  // If the flow already finished (e.g. federated login completed before click
-  // returned), we don't need to do anything.
-  if (!on_finished_callback_) {
-    return;
-  }
-
-  if (result->code != actor::mojom::ActionResultCode::kOk) {
-    std::move(on_finished_callback_)
-        // TODO(crbug.com/478799141): add new status for SiwG failure.
-        .Run(base::unexpected(ActorLoginError::kFillingNotAllowed));
-  }
 }
 
 }  // namespace actor_login
