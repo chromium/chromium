843bb34eda0ae967314db09e885e8ab99308beaf
Khalid Peer <khalidpeer@chromium.org>
2026-04-17 15:27:49 -0700

    Revert "[gardening] Disable `ClickTabInImmersiveMode` test on MacOS builders."
    This reverts commit 88fdec6fe80d7424869b30230c2f979db4f1acfb.
    
    Reason for revert: https://www.crbug.com/503757209#comment3 notes that the this test should run normally now.
    
    Include-Ci-Only-Tests: chromium.mac:mac15-x64-rel-tests|browser_tests
    
    Original change's description:
    > [gardening] Disable `ClickTabInImmersiveMode` test on MacOS builders.
    >
    > Bug: 503757209
    > Change-Id: I94a661528b013f8ed01a82a8142b61bff9a0495d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7772056
    > Reviewed-by: Hamda Mare <hmare@google.com>
    > Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    > Owners-Override: Khalid Peer <khalidpeer@chromium.org>
    > Auto-Submit: Khalid Peer <khalidpeer@chromium.org>
    > Commit-Queue: Hamda Mare <hmare@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1616725}
    
    Bug: 503757209
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I15d47b47673ee64c7795aaba2c8d0158eff91475
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771056
    Owners-Override: Khalid Peer <khalidpeer@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Khalid Peer <khalidpeer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616908}

--- chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller_browsertest.cc
+++ chrome/browser/ui/views/tabs/vertical/vertical_tab_strip_controller_browsertest.cc
@@ -162,15 +162,9 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripControllerBrowserTest,
   EXPECT_EQ(0, model->GetIndexOfTab(tab_to_move));
 }
 
-// TODO(crbug.com/503757209): Test is consistently crashing on MacOS builders.
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_ClickTabInImmersiveMode DISABLED_ClickTabInImmersiveMode
-#else
-#define MAYBE_ClickTabInImmersiveMode ClickTabInImmersiveMode
-#endif
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC)
 IN_PROC_BROWSER_TEST_F(VerticalTabStripControllerBrowserTest,
-                       MAYBE_ClickTabInImmersiveMode) {
+                       ClickTabInImmersiveMode) {
   // Add another tab to switch to.
   AppendTab();
 
