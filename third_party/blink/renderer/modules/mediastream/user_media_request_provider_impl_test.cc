4ec9347dc807984fc15829416ef95546ca5b75a4
Ravjit Uppal <ravjit@chromium.org>
2026-04-17 06:04:04 -0700

    [M148] [CE] Support gUM constraints in <usermedia> element
    Original change's description:
    > [CE] Support gUM constraints in <usermedia> element
    >
    > Type attribute takes precedence over setConstraints.
    >
    > The CL also updates the error attribute to match the gUM JS API. If the
    > constraints don't match the type of the usermedia element or are
    > undefined/false, a type error is throwm.
    >
    > Bug: 494481412
    > Change-Id: Ifb0229cbf82f3cc219043d4ef076922771f195f1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7738744
    > Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    > Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1615696}
    
    (cherry picked from commit af62b762302dcaf0fcf94b8f5a028ae94c8c48e7)
    
    Bug: 503631728,494481412
    Change-Id: Ifb0229cbf82f3cc219043d4ef076922771f195f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7770021
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7778@{#912}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- third_party/blink/renderer/modules/mediastream/user_media_request_provider_impl_test.cc
+++ third_party/blink/renderer/modules/mediastream/user_media_request_provider_impl_test.cc
@@ -5,6 +5,7 @@
 #include "third_party/blink/renderer/modules/mediastream/user_media_request_provider_impl.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_testing.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_typedefs.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_union_domexception_overconstrainederror.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -18,6 +19,8 @@
 #include "third_party/blink/renderer/core/testing/page_test_base.h"
 #include "third_party/blink/renderer/modules/mediastream/html_user_media_element_media_stream.h"
 #include "third_party/blink/renderer/modules/mediastream/media_stream.h"
+#include "third_party/blink/renderer/modules/mediastream/user_media_element_constraints.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_union_boolean_mediatrackconstraints.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/testing/testing_platform_support.h"
 
@@ -34,10 +37,17 @@ class UserMediaRequestProviderImplTest : public PageTestBase {
 // Verifies that the provider doesn't crash when attempting to process a request
 // without a valid UserMediaClient.
 TEST_F(UserMediaRequestProviderImplTest, StartRequestEarlyExitNoClient) {
+  V8TestingScope scope;
   auto* provider = UserMediaRequestProvider::From(*GetDocument().domWindow());
 
   auto* element = MakeGarbageCollected<HTMLUserMediaElement>(GetDocument());
   element->setAttribute(html_names::kTypeAttr, AtomicString("camera"));
+
+  MediaStreamConstraints* constraints = MediaStreamConstraints::Create();
+  constraints->setVideo(
+      MakeGarbageCollected<V8UnionBooleanOrMediaTrackConstraints>(true));
+  UserMediaElementConstraints::setConstraints(*element, constraints);
+
   provider->StartRequest(element, element->GetPermissionDescriptors());
   // Test passes if it doesn't crash.
 }
@@ -45,10 +55,16 @@ TEST_F(UserMediaRequestProviderImplTest, StartRequestEarlyExitNoClient) {
 // Verifies that StartRequest gracefully exits and makes no changes when an
 // active stream is already present.
 TEST_F(UserMediaRequestProviderImplTest, StartRequestActiveStreamExists) {
+  V8TestingScope scope;
   auto* provider = UserMediaRequestProvider::From(*GetDocument().domWindow());
 
   auto* element = MakeGarbageCollected<HTMLUserMediaElement>(GetDocument());
   element->setAttribute(html_names::kTypeAttr, AtomicString("camera"));
+
+  MediaStreamConstraints* constraints = MediaStreamConstraints::Create();
+  constraints->setVideo(
+      MakeGarbageCollected<V8UnionBooleanOrMediaTrackConstraints>(true));
+  UserMediaElementConstraints::setConstraints(*element, constraints);
 
   auto* stream = MediaStream::Create(GetDocument().GetExecutionContext());
   HTMLUserMediaElementMediaStream::From(*element).SetMediaStream(stream);
@@ -86,6 +102,7 @@ class TestEventListener : public NativeEventListener {
 };
 
 TEST_F(UserMediaRequestProviderImplTest, CallbacksOnError) {
+  V8TestingScope scope;
   auto* element = MakeGarbageCollected<HTMLUserMediaElement>(GetDocument());
   auto* callbacks =
       MakeGarbageCollected<UserMediaRequestProviderCallbacks>(element);
@@ -94,7 +111,7 @@ TEST_F(UserMediaRequestProviderImplTest, CallbacksOnError) {
   auto* listener = MakeGarbageCollected<TestEventListener>();
   element->addEventListener(event_type_names::kStream, listener);
 
-  EXPECT_EQ(HTMLUserMediaElementMediaStream::error(*element), nullptr);
+  EXPECT_TRUE(HTMLUserMediaElementMediaStream::error(scope.GetScriptState(), *element).IsNull());
 
   DOMException* dom_exception =
       DOMException::Create("Some error message", "NotFoundError");
@@ -105,13 +122,53 @@ TEST_F(UserMediaRequestProviderImplTest, CallbacksOnError) {
 
   // Check that the event was fired and the error was set
   EXPECT_TRUE(listener->fired());
-  EXPECT_NE(HTMLUserMediaElementMediaStream::error(*element), nullptr);
-  EXPECT_TRUE(
-      HTMLUserMediaElementMediaStream::error(*element)->IsDOMException());
-  EXPECT_EQ(HTMLUserMediaElementMediaStream::error(*element)
-                ->GetAsDOMException()
-                ->name(),
+  ScriptValue stored_error = HTMLUserMediaElementMediaStream::error(scope.GetScriptState(), *element);
+  EXPECT_FALSE(stored_error.IsEmpty());
+  EXPECT_TRUE(stored_error.V8Value()->IsObject());
+  EXPECT_EQ(ToCoreString(scope.GetIsolate(), stored_error.V8Value()
+                                                 .As<v8::Object>()
+                                                 ->Get(scope.GetContext(),
+                                                       V8String(scope.GetIsolate(),
+                                                                "name"))
+                                                 .ToLocalChecked()
+                                                 .As<v8::String>()),
             "NotFoundError");
+}
+
+TEST_F(UserMediaRequestProviderImplTest, StartRequestNoConstraintsError) {
+  V8TestingScope scope;
+  auto* provider = UserMediaRequestProvider::From(*GetDocument().domWindow());
+
+  auto* element = MakeGarbageCollected<HTMLUserMediaElement>(GetDocument());
+  element->setAttribute(html_names::kTypeAttr, AtomicString("camera microphone"));
+
+  MediaStreamConstraints* constraints = MediaStreamConstraints::Create();
+  UserMediaElementConstraints::setConstraints(*element, constraints);
+
+  // Set up event listener
+  auto* listener = MakeGarbageCollected<TestEventListener>();
+  element->addEventListener(event_type_names::kStream, listener);
+
+  provider->StartRequest(element, element->GetPermissionDescriptors());
+
+  EXPECT_TRUE(listener->fired());
+  ScriptValue stored_error = HTMLUserMediaElementMediaStream::error(scope.GetScriptState(), *element);
+  EXPECT_FALSE(stored_error.IsEmpty());
+  EXPECT_TRUE(stored_error.V8Value()->IsObject());
+
+  v8::Local<v8::Object> error_obj = stored_error.V8Value().As<v8::Object>();
+  EXPECT_EQ(ToCoreString(scope.GetIsolate(), error_obj->Get(scope.GetContext(),
+                                                       V8String(scope.GetIsolate(),
+                                                                "name"))
+                                                 .ToLocalChecked()
+                                                 .As<v8::String>()),
+            "TypeError");
+  EXPECT_EQ(ToCoreString(scope.GetIsolate(), error_obj->Get(scope.GetContext(),
+                                                       V8String(scope.GetIsolate(),
+                                                                "message"))
+                                                 .ToLocalChecked()
+                                                 .As<v8::String>()),
+            "No constraints set");
 }
 
 }  // namespace blink
