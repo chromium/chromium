chromium/chromium

commit f71e47fc0edf5b2bae628835c5d9def62169b65d

[iOS arm64e] Fix base/profiler to work correctly with arm64e
With this CL, it is now possible to build base_unittests for arm64e.
Some of the tests still crash due to missing explicit pointer
authentication handling in low level routines and assembly code.

Access to pointer registers in the thread state had to be refactored to
use getter and setter methods (instead of readwrite access via
references) so that opaque pointers can be encoded and decoded correctly
using arm64e pointer authentication.

There are no automated tests that cover the arm64e builds at this
time because the build is not yet functional. However, it is
possible to manually test this CL using the following steps:
 * In a Bling checkout, set the target_cpu gn arg to "arm64e" in
   a build configuration that is otherwise set up to build for
   a physical device (e.g. out/Debug-iphoneos)
 * Build the base_unittests target.
   Note: much of Chromium won't currently build correctly in arm64e,
   but base unittests does, as of this CL. Because rust does not
   support arm64e, you will see linker warnings about rust deps being
   ignored. This is fine, you will still be able to build and run
   the tests and it will still include the unit tests affected by
   this CL.
 * Run base_unittests on a physical device. To get around tests that
   still crash, use the --gtest_filter command line arg to successfully
   run the following test cases, which are relevant to this CL:
   StackCopierTest, StackCopierSuspendTest, StackBufferTest,
   StackTraceTest, StackTraceDeathTest, FramePointerUnwinder,

Low-Coverage-Reason: COVERAGE_UNDERREPORTED The gaps in coverage
signalled by the code coverage checks are in areas of the code that are
specific to certain CPU architectures. These gaps due to a combination
of certain tests being disabled on linux and not all runs of
base_unittests being tracked for code coverage analysis.

Bug: 495838671
Change-Id: Icdbf3d8859c5751aa5bac473c71cd0d891216aae

base/profiler/register_context_registers.h
@@ -8,6 +8,7 @@
 #ifndef BASE_PROFILER_REGISTER_CONTEXT_REGISTERS_H_
 #define BASE_PROFILER_REGISTER_CONTEXT_REGISTERS_H_
 
+#include <ptrauth.h>
 #include <stdint.h>
 
 #include "build/build_config.h"
@@ -38,46 +39,89 @@ uintptr_t& AsUintPtr(T* value) {
 
 #if BUILDFLAG(IS_WIN)
 
-inline uintptr_t& RegisterContextStackPointer(::CONTEXT* context) {
+inline uintptr_t RegisterContextStackPointer(::CONTEXT* context) {
 #if defined(ARCH_CPU_X86_64)
   return context->Rsp;
 #elif defined(ARCH_CPU_ARM64)
   return context->Sp;
+#elif defined(ARCH_CPU_X86)
+  return context->Esp;
 #else
-  return AsUintPtr(&context->Esp);
+#error "Unknown architecture"
 #endif
 }
 
-inline uintptr_t& RegisterContextFramePointer(::CONTEXT* context) {
+inline void SetRegisterContextStackPointer(::CONTEXT* context, uintptr_t val) {
+#if defined(ARCH_CPU_X86_64)
+  context->Rsp = val;
+#elif defined(ARCH_CPU_ARM64)
+  context->Sp = val;
+#elif defined(ARCH_CPU_X86)
+  context->Esp = val;
+#else
+#error "Unknown architecture"
+#endif
+}
+
+inline uintptr_t RegisterContextFramePointer(::CONTEXT* context) {
 #if defined(ARCH_CPU_X86_64)
   return context->Rbp;
 #elif defined(ARCH_CPU_ARM64)
   return context->Fp;
+#elif defined(ARCH_CPU_X86)
+  return context->Ebp;
 #else
-  return AsUintPtr(&context->Ebp);
+#error "Unknown architecture"
 #endif
 }
 
-inline uintptr_t& RegisterContextInstructionPointer(::CONTEXT* context) {
+inline void SetRegisterContextFramePointer(::CONTEXT* context, uintptr_t val) {
+#if defined(ARCH_CPU_X86_64)
+  context->Rbp = val;
+#elif defined(ARCH_CPU_ARM64)
+  context->Fp = val;
+#elif defined(ARCH_CPU_X86)
+  context->Ebp = val;
+#else
+#error "Unknown architecture"
+#endif
+}
+
+inline uintptr_t RegisterContextInstructionPointer(::CONTEXT* context) {
 #if defined(ARCH_CPU_X86_64)
   return context->Rip;
 #elif defined(ARCH_CPU_ARM64)
   return context->Pc;
+#elif defined(ARCH_CPU_X86)
+  return context->Eip;
 #else
-  return AsUintPtr(&context->Eip);
+#error "Unknown architecture"
+#endif
+}
+
+inline void SetRegisterContextInstructionPointer(::CONTEXT* context,
+                                                 uintptr_t val) {
+#if defined(ARCH_CPU_X86_64)
+  context->Rip = val;
+#elif defined(ARCH_CPU_ARM64)
+  context->Pc = val;
+#elif defined(ARCH_CPU_X86)
+  context->Eip = val;
+#else
+#error "Unknown architecture"
 #endif
 }
 
 #elif BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
 
-inline uintptr_t& RegisterContextStackPointer(mcontext_t* context) {
-#if defined(ARCH_CPU_ARM_FAMILY) && defined(ARCH_CPU_32_BITS)
-  return AsUintPtr(&context->arm_sp);
-#elif defined(ARCH_CPU_ARM_FAMILY) && defined(ARCH_CPU_64_BITS)
-  return AsUintPtr(&context->sp);
-#elif defined(ARCH_CPU_X86_FAMILY) && defined(ARCH_CPU_32_BITS)
+inline uintptr_t RegisterContextStackPointer(mcontext_t* context) {
+#if defined(ARCH_CPU_ARMEL)
+  return context->arm_sp;
+#elif defined(ARCH_CPU_ARM64)
+  return context->sp;
+#elif defined(ARCH_CPU_X86)
   return AsUintPtr(&context->gregs[REG_ESP]);
-#elif defined(ARCH_CPU_X86_FAMILY) && defined(ARCH_CPU_64_BITS)
+#elif defined(ARCH_CPU_X86_64)
   return AsUintPtr(&context->gregs[REG_RSP]);
 #else
   // The implementations here and below are placeholders for other POSIX
@@ -86,82 +130,199 @@ inline uintptr_t& RegisterContextStackPointer(mcontext_t* context) {
 #endif
 }
 
-inline uintptr_t& RegisterContextFramePointer(mcontext_t* context) {
-#if defined(ARCH_CPU_ARM_FAMILY) && defined(ARCH_CPU_32_BITS)
-  return AsUintPtr(&context->arm_fp);
-#elif defined(ARCH_CPU_ARM_FAMILY) && defined(ARCH_CPU_64_BITS)
-  // r29 is the FP register on 64-bit ARM per the Procedure Call Standard,
-  // section 5.1.1.
-  return AsUintPtr(&context->regs[29]);
-#elif defined(ARCH_CPU_X86_FAMILY) && defined(ARCH_CPU_32_BITS)
-  return AsUintPtr(&context->gregs[REG_EBP]);
-#elif defined(ARCH_CPU_X86_FAMILY) && defined(ARCH_CPU_64_BITS)
-  return AsUintPtr(&context->gregs[REG_RBP]);
+inline void SetRegisterContextStackPointer(mcontext_t* context, uintptr_t val) {
+#if defined(ARCH_CPU_ARMEL)
+  context->arm_sp = val;
+#elif defined(ARCH_CPU_ARM64)
+  context->sp = val;
+#elif defined(ARCH_CPU_X86)
+  AsUintPtr(&context->gregs[REG_ESP]) = val;
+#elif defined(ARCH_CPU_X86_64)
+  AsUintPtr(&context->gregs[REG_RSP]) = val;
 #else
-  return *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 1));
+  // The implementations here and below are placeholders for other POSIX
+  // platforms that just the first three register slots in the context.
+  *reinterpret_cast<uintptr_t*>(context) = val;
 #endif
 }
 
-inline uintptr_t& RegisterContextInstructionPointer(mcontext_t* context) {
-#if defined(ARCH_CPU_ARM_FAMILY) && defined(ARCH_CPU_32_BITS)
-  return AsUintPtr(&context->arm_pc);
-#elif defined(ARCH_CPU_ARM_FAMILY) && defined(ARCH_CPU_64_BITS)
-  return AsUintPtr(&context->pc);
-#elif defined(ARCH_CPU_X86_FAMILY) && defined(ARCH_CPU_32_BITS)
+inline uintptr_t RegisterContextFramePointer(mcontext_t* context) {
+#if defined(ARCH_CPU_ARMEL)
+  return context->arm_fp;
+#elif defined(ARCH_CPU_ARM64)
+  // r29 is the FP register on 64-bit ARM per the Procedure Call Standard,
+  // section 5.1.1.
+  return context->regs[29];
+#elif defined(ARCH_CPU_X86)
+  return AsUintPtr(&context->gregs[REG_EBP]);
+#elif defined(ARCH_CPU_X86_64)
+  return AsUintPtr(&context->gregs[REG_RBP]);
+#else
+  return *(reinterpret_cast<uintptr_t*>(context) + 1);
+#endif
+}
+
+inline void SetRegisterContextFramePointer(mcontext_t* context, uintptr_t val) {
+#if defined(ARCH_CPU_ARMEL)
+  context->arm_fp = val;
+#elif defined(ARCH_CPU_ARM64)
+  // r29 is the FP register on 64-bit ARM per the Procedure Call Standard,
+  // section 5.1.1.
+  context->regs[29] = val;
+#elif defined(ARCH_CPU_X86)
+  AsUintPtr(&context->gregs[REG_EBP]) = val;
+#elif defined(ARCH_CPU_X86_64)
+  AsUintPtr(&context->gregs[REG_RBP]) = val;
+#else
+  *(reinterpret_cast<uintptr_t*>(context) + 1) = val;
+#endif
+}
+
+inline uintptr_t RegisterContextInstructionPointer(mcontext_t* context) {
+#if defined(ARCH_CPU_ARMEL)
+  return context->arm_pc;
+#elif defined(ARCH_CPU_ARM64)
+  return context->pc;
+#elif defined(ARCH_CPU_X86)
   return AsUintPtr(&context->gregs[REG_EIP]);
-#elif defined(ARCH_CPU_X86_FAMILY) && defined(ARCH_CPU_64_BITS)
+#elif defined(ARCH_CPU_X86_64)
   return AsUintPtr(&context->gregs[REG_RIP]);
 #else
-  return *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 2));
+  return *(reinterpret_cast<uintptr_t*>(context) + 2);
+#endif
+}
+
+inline void SetRegisterContextInstructionPointer(mcontext_t* context,
+                                                 uintptr_t val) {
+#if defined(ARCH_CPU_ARMEL)
+  context->arm_pc = val;
+#elif defined(ARCH_CPU_ARM64)
+  context->pc = val;
+#elif defined(ARCH_CPU_X86)
+  AsUintPtr(&context->gregs[REG_EIP]) = val;
+#elif defined(ARCH_CPU_X86_64)
+  AsUintPtr(&context->gregs[REG_RIP]) = val;
+#else
+  *(reinterpret_cast<uintptr_t*>(context) + 2) = val;
 #endif
 }
 
 #elif BUILDFLAG(IS_APPLE) && defined(ARCH_CPU_X86_64)
 
-inline uintptr_t& RegisterContextStackPointer(x86_thread_state64_t* context) {
-  return AsUintPtr(&context->__rsp);
+inline uintptr_t RegisterContextStackPointer(x86_thread_state64_t* context) {
+  return context->__rsp;
 }
 
-inline uintptr_t& RegisterContextFramePointer(x86_thread_state64_t* context) {
-  return AsUintPtr(&context->__rbp);
+inline void SetRegisterContextStackPointer(x86_thread_state64_t* context,
+                                           uintptr_t val) {
+  context->__rsp = val;
 }
 
-inline uintptr_t& RegisterContextInstructionPointer(
+inline uintptr_t RegisterContextFramePointer(x86_thread_state64_t* context) {
+  return context->__rbp;
+}
+
+inline void SetRegisterContextFramePointer(x86_thread_state64_t* context,
+                                           uintptr_t val) {
+  context->__rbp = val;
+}
+
+inline uintptr_t RegisterContextInstructionPointer(
     x86_thread_state64_t* context) {
-  return AsUintPtr(&context->__rip);
+  return context->__rip;
+}
+
+inline void SetRegisterContextInstructionPointer(x86_thread_state64_t* context,
+                                                 uintptr_t val) {
+  context->__rip = val;
 }
 
 #elif BUILDFLAG(IS_APPLE) && defined(ARCH_CPU_ARM64)
 
-// TODO(thakis): Have getter/setter functions instead of returning a ref to
-// prepare for arm64e. See __DARWIN_OPAQUE_ARM_THREAD_STATE6 in
-// mach/arm/_structs.h
-inline uintptr_t& RegisterContextStackPointer(arm_thread_state64_t* context) {
-  return AsUintPtr(&context->__sp);
+#if BUILDFLAG(ARCH_CPU_PTRAUTH)
+inline auto& Arm64Sp(arm_thread_state64_t* ctx) {
+  return ctx->__opaque_sp;
+}
+inline auto& Arm64Fp(arm_thread_state64_t* ctx) {
+  return ctx->__opaque_fp;
+}
+inline auto& Arm64Pc(arm_thread_state64_t* ctx) {
+  return ctx->__opaque_pc;
+}
+#else   // !BUILDFLAG(ARCH_CPU_PTRAUTH)
+inline auto& Arm64Sp(arm_thread_state64_t* ctx) {
+  return ctx->__sp;
+}
+inline auto& Arm64Fp(arm_thread_state64_t* ctx) {
+  return ctx->__fp;
+}
+inline auto& Arm64Pc(arm_thread_state64_t* ctx) {
+  return ctx->__pc;
+}
+#endif  // !BUILDFLAG(ARCH_CPU_PTRAUTH)
+
+inline uintptr_t RegisterContextStackPointer(arm_thread_state64_t* context) {
+  void* val = ptrauth_strip(reinterpret_cast<void*>(Arm64Sp(context)),
+                            ptrauth_key_asda);
+  return AsUintPtr(&val);
 }
 
-inline uintptr_t& RegisterContextFramePointer(arm_thread_state64_t* context) {
-  return AsUintPtr(&context->__fp);
+inline void SetRegisterContextStackPointer(arm_thread_state64_t* context,
+                                           uintptr_t val) {
+  AsUintPtr(&Arm64Sp(context)) = val;
 }
 
-inline uintptr_t& RegisterContextInstructionPointer(
+inline uintptr_t RegisterContextFramePointer(arm_thread_state64_t* context) {
+  void* val = ptrauth_strip(reinterpret_cast<void*>(Arm64Fp(context)),
+                            ptrauth_key_asda);
+  return AsUintPtr(&val);
+}
+
+inline void SetRegisterContextFramePointer(arm_thread_state64_t* context,
+                                           uintptr_t val) {
+  AsUintPtr(&Arm64Fp(context)) = val;
+}
+
+inline uintptr_t RegisterContextInstructionPointer(
     arm_thread_state64_t* context) {
-  return AsUintPtr(&context->__pc);
+  void* val = ptrauth_strip(reinterpret_cast<void*>(Arm64Pc(context)),
+                            ptrauth_key_asia);
+  return AsUintPtr(&val);
 }
 
-#else
+inline void SetRegisterContextInstructionPointer(arm_thread_state64_t* context,
+                                                 uintptr_t val) {
+  AsUintPtr(&Arm64Pc(context)) = val;
+}
+
+#else  // BUILDFLAG(IS_APPLE) && defined(ARCH_CPU_ARM64)
 
 // Placeholders for other cases.
-inline uintptr_t& RegisterContextStackPointer(RegisterContext* context) {
+inline uintptr_t RegisterContextStackPointer(RegisterContext* context) {
   return context->stack_pointer;
 }
 
-inline uintptr_t& RegisterContextFramePointer(RegisterContext* context) {
+inline void SetRegisterContextStackPointer(RegisterContext* context,
+                                           uintptr_t val) {
+  context->stack_pointer = val;
+}
+
+inline uintptr_t RegisterContextFramePointer(RegisterContext* context) {
   return context->frame_pointer;
 }
 
-inline uintptr_t& RegisterContextInstructionPointer(RegisterContext* context) {
+inline void SetRegisterContextFramePointer(RegisterContext* context,
+                                           uintptr_t val) {
+  context->frame_pointer = val;
+}
+
+inline uintptr_t RegisterContextInstructionPointer(RegisterContext* context) {
   return context->instruction_pointer;
+}
+
+inline void SetRegisterContextInstructionPointer(RegisterContext* context,
+                                                 uintptr_t val) {
+  context->instruction_pointer = val;
 }
 
 #endif

base/debug/stack_trace_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "base/debug/stack_trace.h"
 
+#include <ptrauth.h>
 #include <stddef.h>
 
 #include <limits>
@@ -350,8 +351,10 @@ code_start:
 
   constexpr size_t frame_index = Depth - 1;
   const void* frame = frames[frame_index];
-  EXPECT_GE(frame, &&code_start) << "For frame at index " << frame_index;
-  EXPECT_LE(frame, &&code_end) << "For frame at index " << frame_index;
+  const void* start = ptrauth_strip(&&code_start, ptrauth_key_function_pointer);
+  const void* end = ptrauth_strip(&&code_end, ptrauth_key_function_pointer);
+  EXPECT_GE(frame, start) << "For frame at index " << frame_index;
+  EXPECT_LE(frame, end) << "For frame at index " << frame_index;
 code_end:
   return;
 }
@@ -366,8 +369,10 @@ code_start:
   ASSERT_EQ(frames.size(), count);
 
   const void* frame = frames[0];
-  EXPECT_GE(frame, &&code_start) << "For the top frame";
-  EXPECT_LE(frame, &&code_end) << "For the top frame";
+  const void* start = ptrauth_strip(&&code_start, ptrauth_key_function_pointer);
+  const void* end = ptrauth_strip(&&code_end, ptrauth_key_function_pointer);
+  EXPECT_GE(frame, start) << "For the top frame";
+  EXPECT_LE(frame, end) << "For the top frame";
 code_end:
   return;
 }

base/profiler/chrome_unwinder_android_32.cc
@@ -41,9 +41,10 @@ bool PopRegister(RegisterContext* context, uint8_t register_index) {
   const uintptr_t sp = RegisterContextStackPointer(context);
   const uintptr_t stacktop_value = *reinterpret_cast<uintptr_t*>(sp);
   const auto new_sp = CheckedNumeric<uintptr_t>(sp) + sizeof(uintptr_t);
-  const bool success =
-      new_sp.AssignIfValid(&RegisterContextStackPointer(context));
+  uintptr_t sp_val;
+  const bool success = new_sp.AssignIfValid(&sp_val);
   if (success) {
+    SetRegisterContextStackPointer(context, sp_val);
     *GetRegisterPointer(context, register_index) = stacktop_value;
   }
   return success;
@@ -192,9 +193,11 @@ UnwindInstructionResult ExecuteUnwindInstruction(
     const auto new_sp =
         CheckedNumeric<uintptr_t>(RegisterContextStackPointer(thread_context)) +
         offset;
-    if (!new_sp.AssignIfValid(&RegisterContextStackPointer(thread_context))) {
+    uintptr_t sp_val;
+    if (!new_sp.AssignIfValid(&sp_val)) {
       return UnwindInstructionResult::kAborted;
     }
+    SetRegisterContextStackPointer(thread_context, sp_val);
   } else if (GetTopBits(*instruction, 2) == 0b01) {
     // 01xxxxxx
     // vsp = vsp - (xxxxxx << 2) - 4. Covers range 0x04-0x100 inclusive.
@@ -203,9 +206,11 @@ UnwindInstructionResult ExecuteUnwindInstruction(
     const auto new_sp =
         CheckedNumeric<uintptr_t>(RegisterContextStackPointer(thread_context)) -
         offset;
-    if (!new_sp.AssignIfValid(&RegisterContextStackPointer(thread_context))) {
+    uintptr_t sp_val;
+    if (!new_sp.AssignIfValid(&sp_val)) {
       return UnwindInstructionResult::kAborted;
     }
+    SetRegisterContextStackPointer(thread_context, sp_val);
   } else if (GetTopBits(*instruction, 4) == 0b1001) {
     // 1001nnnn (nnnn != 13,15)
     // Set vsp = r[nnnn].
@@ -216,8 +221,8 @@ UnwindInstructionResult ExecuteUnwindInstruction(
     // using this instruction.
     DCHECK_GE(register_index, 4);
 
-    RegisterContextStackPointer(thread_context) =
-        *GetRegisterPointer(thread_context, register_index);
+    SetRegisterContextStackPointer(
+        thread_context, *GetRegisterPointer(thread_context, register_index));
   } else if (GetTopBits(*instruction, 5) == 0b10101) {
     // 10101nnn
     // Pop r4-r[4+nnn], r14
@@ -275,9 +280,11 @@ UnwindInstructionResult ExecuteUnwindInstruction(
         CheckedNumeric<uintptr_t>(RegisterContextStackPointer(thread_context)) +
         (CheckedNumeric<uintptr_t>(DecodeULEB128(instruction)) << 2) + 0x204;
 
-    if (!new_sp.AssignIfValid(&RegisterContextStackPointer(thread_context))) {
+    uintptr_t sp_val;
+    if (!new_sp.AssignIfValid(&sp_val)) {
       return UnwindInstructionResult::kAborted;
     }
+    SetRegisterContextStackPointer(thread_context, sp_val);
   } else {
     NOTREACHED();
   }

base/profiler/chrome_unwinder_android_32_unittest.cc
@@ -1121,8 +1121,8 @@ TEST(ChromeUnwinderAndroid32Test, TryUnwind) {
 
   std::vector<Frame> unwound_frames = {{first_pc, chrome_module}};
   RegisterContext context;
-  RegisterContextInstructionPointer(&context) = first_pc;
-  RegisterContextStackPointer(&context) = stack_memory.stack_start_address();
+  SetRegisterContextInstructionPointer(&context, first_pc);
+  SetRegisterContextStackPointer(&context, stack_memory.stack_start_address());
   context.arm_lr = second_pc;
 
   EXPECT_EQ(
@@ -1189,8 +1189,8 @@ TEST(ChromeUnwinderAndroid32Test, TryUnwindInfiniteLoopSingleFrame) {
 
   std::vector<Frame> unwound_frames = {{pc, chrome_module}};
   RegisterContext context;
-  RegisterContextInstructionPointer(&context) = pc;
-  RegisterContextStackPointer(&context) = stack_memory.stack_start_address();
+  SetRegisterContextInstructionPointer(&context, pc);
+  SetRegisterContextStackPointer(&context, stack_memory.stack_start_address());
 
   // Set lr = pc so that both sp and pc stays the same after first round of
   // unwind.
@@ -1284,8 +1284,8 @@ TEST(ChromeUnwinderAndroid32Test, TryUnwindInfiniteLoopMultipleFrames) {
 
   std::vector<Frame> unwound_frames = {{first_pc, chrome_module}};
   RegisterContext context;
-  RegisterContextInstructionPointer(&context) = first_pc;
-  RegisterContextStackPointer(&context) = stack_memory.stack_start_address();
+  SetRegisterContextInstructionPointer(&context, first_pc);
+  SetRegisterContextStackPointer(&context, stack_memory.stack_start_address());
 
   context.arm_lr = second_pc;
   context.arm_r4 = stack_memory.stack_start_address();
@@ -1354,10 +1354,10 @@ TEST(ChromeUnwinderAndroid32Test, TryUnwindUnalignedSPFrameUnwind) {
 
   std::vector<Frame> unwound_frames = {{pc, chrome_module}};
   RegisterContext context;
-  RegisterContextInstructionPointer(&context) = pc;
+  SetRegisterContextInstructionPointer(&context, pc);
   // Make stack memory not aligned to 2 * sizeof(uintptr_t);
-  RegisterContextStackPointer(&context) =
-      stack_memory.stack_start_address() + sizeof(uintptr_t);
+  SetRegisterContextStackPointer(
+      &context, stack_memory.stack_start_address() + sizeof(uintptr_t));
 
   // The address is outside chrome module, which will result the unwind to
   // stop with result kUnrecognizedFrame if SP alignment issue was not detected.
@@ -1428,8 +1428,8 @@ TEST(ChromeUnwinderAndroid32Test, TryUnwindUnalignedSPInstructionUnwind) {
 
   std::vector<Frame> unwound_frames = {{pc, chrome_module}};
   RegisterContext context;
-  RegisterContextInstructionPointer(&context) = pc;
-  RegisterContextStackPointer(&context) = stack_memory.stack_start_address();
+  SetRegisterContextInstructionPointer(&context, pc);
+  SetRegisterContextStackPointer(&context, stack_memory.stack_start_address());
 
   // The address is outside chrome module, which will result the unwind to
   // stop with result kUnrecognizedFrame if SP alignment issue was not detected.
@@ -1499,8 +1499,8 @@ TEST(ChromeUnwinderAndroid32Test, TryUnwindSPOverflow) {
   };
   std::vector<Frame> unwound_frames = {{pc, chrome_module}};
   RegisterContext context;
-  RegisterContextInstructionPointer(&context) = pc;
-  RegisterContextStackPointer(&context) = stack_memory.stack_start_address();
+  SetRegisterContextInstructionPointer(&context, pc);
+  SetRegisterContextStackPointer(&context, stack_memory.stack_start_address());
 
   // Setting vsp = 0xffffffff should cause SP overflow.
   context.arm_r4 = 0xffffffff;
@@ -1572,8 +1572,8 @@ TEST(ChromeUnwinderAndroid32Test, TryUnwindNullSP) {
   };
   std::vector<Frame> unwound_frames = {{pc, chrome_module}};
   RegisterContext context;
-  RegisterContextInstructionPointer(&context) = pc;
-  RegisterContextStackPointer(&context) = stack_memory.stack_start_address();
+  SetRegisterContextInstructionPointer(&context, pc);
+  SetRegisterContextStackPointer(&context, stack_memory.stack_start_address());
 
   // Setting vsp = 0x0 should cause the unwinder to abort.
   context.arm_r4 = 0x0;
@@ -1648,8 +1648,8 @@ TEST(ChromeUnwinderAndroid32Test, TryUnwindInvalidSPOperation) {
   };
   std::vector<Frame> unwound_frames = {{pc, chrome_module}};
   RegisterContext context;
-  RegisterContextInstructionPointer(&context) = pc;
-  RegisterContextStackPointer(&context) = stack_memory.stack_start_address();
+  SetRegisterContextInstructionPointer(&context, pc);
+  SetRegisterContextStackPointer(&context, stack_memory.stack_start_address());
 
   context.arm_r4 = stack_memory.stack_start_address() - 2 * sizeof(uintptr_t);
   context.arm_r5 = stack_memory.stack_start_address() + 2 * sizeof(uintptr_t);

base/profiler/frame_pointer_unwinder.cc
@@ -119,9 +119,10 @@ UnwindResult FramePointerUnwinder::TryUnwind(
       return UnwindResult::kAborted;
     }
 
-    RegisterContextFramePointer(thread_context) = next_frame;
-    RegisterContextInstructionPointer(thread_context) = retaddr;
-    RegisterContextStackPointer(thread_context) = frame + sizeof(uintptr_t) * 2;
+    SetRegisterContextFramePointer(thread_context, next_frame);
+    SetRegisterContextInstructionPointer(thread_context, retaddr);
+    SetRegisterContextStackPointer(thread_context,
+                                   frame + sizeof(uintptr_t) * 2);
     stack->emplace_back(retaddr, module);
   }
 

commit d53f489d4e1f7e05b056e0adfdf8fbcf04e0f8a1

Fix RISCV build of register_contex_registers.h
This change fixes a build breakage cause by
commit f71e47fc0edf5b2bae628835c5d9def62169b65d

The offending CL accidentally removed UNSAFE_TODO macro invocations
that were necessary to suppress compiler errors on RISCV

Bug: b/503665835
Change-Id: I7d29ffcf651c4adff1e58205d4183b0781cf1d5e
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771895
Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
Commit-Queue: Justin Novosad <junov@chromium.org>
Auto-Submit: Justin Novosad <junov@chromium.org>
Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
Cr-Commit-Position: refs/heads/main@{#1616515}

base/profiler/register_context_registers.h
@@ -158,7 +158,7 @@ inline uintptr_t RegisterContextFramePointer(mcontext_t* context) {
 #elif defined(ARCH_CPU_X86_64)
   return AsUintPtr(&context->gregs[REG_RBP]);
 #else
-  return *(reinterpret_cast<uintptr_t*>(context) + 1);
+  return *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 1));
 #endif
 }
 
@@ -174,7 +174,7 @@ inline void SetRegisterContextFramePointer(mcontext_t* context, uintptr_t val) {
 #elif defined(ARCH_CPU_X86_64)
   AsUintPtr(&context->gregs[REG_RBP]) = val;
 #else
-  *(reinterpret_cast<uintptr_t*>(context) + 1) = val;
+  *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 1)) = val;
 #endif
 }
 
@@ -188,7 +188,7 @@ inline uintptr_t RegisterContextInstructionPointer(mcontext_t* context) {
 #elif defined(ARCH_CPU_X86_64)
   return AsUintPtr(&context->gregs[REG_RIP]);
 #else
-  return *(reinterpret_cast<uintptr_t*>(context) + 2);
+  return *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 2));
 #endif
 }
 
@@ -203,7 +203,7 @@ inline void SetRegisterContextInstructionPointer(mcontext_t* context,
 #elif defined(ARCH_CPU_X86_64)
   AsUintPtr(&context->gregs[REG_RIP]) = val;
 #else
-  *(reinterpret_cast<uintptr_t*>(context) + 2) = val;
+  *(UNSAFE_TODO(reinterpret_cast<uintptr_t*>(context) + 2)) = val;
 #endif
 }
 