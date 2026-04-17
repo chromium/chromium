f2fccfa798d3545323712c9f76b6c7a0c2625700
Adriana Ixba <aixba@chromium.org>
2026-04-16 18:40:31 -0700

    [PWA_Install_Dialog] Opt in to screenshot tests for WebAppInstallDialogBrowserTest
    This includes all the dialog views and tests that do not close the
    dialog. Will accept the skia diffs after approval.
    
    drive bys:
    - adjust and simplify test names
    
    Bug: 492657940
    Change-Id: I2a5cdf4b492c87a14d2d3b0e066c066887097d26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7728368
    Commit-Queue: Adriana Ixba <aixba@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1616265}

--- chrome/browser/ui/views/web_apps/web_app_install_dialog_browsertest.cc
+++ chrome/browser/ui/views/web_apps/web_app_install_dialog_browsertest.cc
@@ -3,14 +3,19 @@
 // found in the LICENSE file.
 
 #include <memory>
+#include <numeric>
 #include <optional>
 #include <string>
 #include <tuple>
 #include <vector>
 
+#include "base/memory/weak_ptr.h"
+#include "base/run_loop.h"
 #include "base/strings/string_split.h"
 #include "base/test/bind.h"
+#include "base/test/run_until.h"
 #include "base/test/scoped_feature_list.h"
+#include "build/build_config.h"
 #include "chrome/browser/shortcuts/shortcut_icon_generator.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
@@ -25,6 +30,7 @@
 #include "components/webapps/browser/installable/ml_install_operation_tracker.h"
 #include "components/webapps/browser/installable/ml_installability_promoter.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/browser_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -37,6 +43,27 @@
 #include "ui/views/window/dialog_delegate.h"
 
 namespace web_app {
+
+namespace {
+
+int GetTimesToAcceptDialog(const std::string& name) {
+  if (name == "Intro") {
+    return 0;
+  } else if (name == "InstallOptions") {
+    return 1;
+  } else if (name == "Progress") {
+    return 2;
+  } else if (name == "Successful") {
+    return 3;
+  }
+
+  NOTREACHED();
+}
+
+// The number of times the dialog could be accepted before the dialog closes.
+const int kAcceptsBeforeClosure = 3;
+
+}  // namespace
 
 class TestWebAppScreenshotFetcher : public WebAppScreenshotFetcher {
  public:
@@ -117,29 +144,27 @@ class WebAppInstallDialogBrowserTest
 
     views::Widget* widget = waiter.WaitIfNeededAndGet();
     ASSERT_NE(nullptr, widget);
-
-    base::WeakPtr<views::Widget> weak_widget = widget->GetWeakPtr();
-    views::test::WidgetDestroyedWaiter destruction_waiter(widget);
-
+    widget_ = widget->GetWeakPtr();
     std::vector<std::string> parts = base::SplitString(
         name, "_", base::TRIM_WHITESPACE, base::SPLIT_WANT_NONEMPTY);
-    // Iterate through the parts and perform the corresponding action.
+    std::vector<int> parts_to_accept_times;
+    // Map the separate parts into their "number of times to accept".
+    // "Successful_Cancel" -> [3, -1]
     for (const std::string& part : parts) {
-      if (!weak_widget) {
-        break;
-      }
-      if (part == "Accept") {
-        weak_widget->widget_delegate()->AsDialogDelegate()->AcceptDialog();
-        if (weak_widget && weak_widget->IsClosed()) {
-          destruction_waiter.Wait();
-          dialog_closed_ = true;
-        }
-      } else if (part == "Cancel") {
-        weak_widget->widget_delegate()->AsDialogDelegate()->CancelDialog();
-        if (weak_widget && weak_widget->IsClosed()) {
-          destruction_waiter.Wait();
-          dialog_closed_ = true;
-        }
+      parts_to_accept_times.push_back(GetTimesToAcceptDialog(part));
+    }
+
+    int total_accepts = std::accumulate(parts_to_accept_times.begin(),
+                                        parts_to_accept_times.end(), 0);
+    // The number of times Accept is clicked should not exceed the minimum
+    // amount.
+    ASSERT_LE(total_accepts, kAcceptsBeforeClosure);
+
+    // Accept the dialog according to GetTimesToAcceptDialog.
+    for (int part_times : parts_to_accept_times) {
+      for (int i = 0; i < part_times; ++i) {
+        ASSERT_TRUE(widget_);
+        widget_->widget_delegate()->AsDialogDelegate()->AcceptDialog();
       }
     }
   }
@@ -150,20 +175,11 @@ class WebAppInstallDialogBrowserTest
     dialog_install_info_ = std::move(install_info);
   }
 
-  bool VerifyUi() override {
-    // If the dialog was closed (e.g. accepted or cancelled), we don't need to
-    // verify the UI, because the dialog is gone.
-    if (dialog_closed_) {
-      return true;
-    }
-    return DialogBrowserTest::VerifyUi();
-  }
-
  protected:
   std::optional<bool> dialog_accepted_;
   std::unique_ptr<WebAppInstallInfo> dialog_install_info_;
-  bool dialog_closed_ = false;
   TestWebAppScreenshotFetcher screenshot_fetcher_;
+  base::WeakPtr<views::Widget> widget_ = nullptr;
 
  private:
   InstallOsType GetOsType() { return std::get<0>(GetParam()); }
@@ -173,20 +189,20 @@ class WebAppInstallDialogBrowserTest
   base::WeakPtrFactory<WebAppInstallDialogBrowserTest> weak_ptr_factory_{this};
 };
 
-IN_PROC_BROWSER_TEST_P(WebAppInstallDialogBrowserTest, InvokeUi) {
-  ShowAndVerifyUi();
-}
-
-IN_PROC_BROWSER_TEST_P(WebAppInstallDialogBrowserTest, InvokeUi_Accept) {
-  ShowAndVerifyUi();
-}
-
-IN_PROC_BROWSER_TEST_P(WebAppInstallDialogBrowserTest, InvokeUi_Accept_Accept) {
+IN_PROC_BROWSER_TEST_P(WebAppInstallDialogBrowserTest, InvokeUi_Intro) {
   ShowAndVerifyUi();
 }
 
 IN_PROC_BROWSER_TEST_P(WebAppInstallDialogBrowserTest,
-                       InvokeUi_Accept_Accept_Accept) {
+                       InvokeUi_InstallOptions) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_P(WebAppInstallDialogBrowserTest, InvokeUi_Progress) {
+  ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_P(WebAppInstallDialogBrowserTest, InvokeUi_Successful) {
   ShowAndVerifyUi();
 }
 
@@ -208,17 +224,32 @@ INSTANTIATE_TEST_SUITE_P(
 
 using WebAppInstallDialogClosedTest = WebAppInstallDialogBrowserTest;
 
-IN_PROC_BROWSER_TEST_P(WebAppInstallDialogClosedTest, Cancel) {
-  ShowUi("Cancel");
-  EXPECT_TRUE(dialog_closed_);
+IN_PROC_BROWSER_TEST_P(WebAppInstallDialogClosedTest, CancelInIntro) {
+  ShowUi("Intro");
+  ASSERT_TRUE(widget_);
+  views::test::WidgetDestroyedWaiter destruction_waiter(widget_.get());
+  widget_->widget_delegate()->AsDialogDelegate()->CancelDialog();
+  destruction_waiter.Wait();
   EXPECT_EQ(dialog_accepted_, false);
 }
 
-IN_PROC_BROWSER_TEST_P(WebAppInstallDialogClosedTest, InstallThenLaunch) {
+IN_PROC_BROWSER_TEST_P(WebAppInstallDialogClosedTest, CancelInSuccess) {
+  ShowUi("Successful");
+  ASSERT_TRUE(widget_);
+  views::test::WidgetDestroyedWaiter destruction_waiter(widget_.get());
+  widget_->widget_delegate()->AsDialogDelegate()->CancelDialog();
+  destruction_waiter.Wait();
+  EXPECT_EQ(dialog_accepted_, false);
+}
+
+IN_PROC_BROWSER_TEST_P(WebAppInstallDialogClosedTest, Success) {
   // Accept through all the dialog steps
   // kInstallDialog -> kInstallerOptions -> kProgress -> kSuccessful
-  ShowUi("Accept_Accept_Accept_Accept");
-  EXPECT_TRUE(dialog_closed_);
+  ShowUi("Successful");
+  ASSERT_TRUE(widget_);
+  views::test::WidgetDestroyedWaiter destruction_waiter(widget_.get());
+  widget_->widget_delegate()->AsDialogDelegate()->AcceptDialog();
+  destruction_waiter.Wait();
   EXPECT_EQ(dialog_accepted_, true);
   EXPECT_TRUE(dialog_install_info_);
 }
