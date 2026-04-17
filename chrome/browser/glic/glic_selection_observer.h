0c63405ee6a25ddd6b016828533c9cd88c730a9d
Gary Klassen <gklassen@chromium.org>
2026-04-17 10:58:11 -0700

    Revert "[M147] Glic Selection Widget"
    This reverts commit f75fbb033e4bfc03d8482776e054d4084e262618.
    
    Reason for revert: Part a set that did not merge cleanly and so abandoning the merge for safety.
    
    Original change's description:
    > [M147] Glic Selection Widget
    >
    > Original change's description:
    > > Glic Selection Widget
    > >
    > > This change introduces GlicSelectionWidgetDelegate to display a button
    > > anchored to the text selection. The GlicSelectionObserver is updated to
    > > observe input events to track mouse and keyboard activity, allowing the
    > > widget to be shown only on mouse-based selections and dismissed on
    > > various user interactions.
    > >
    > > BYPASS_LARGE_CHANGE_WARNING: Changes conceptually related and primarily within a single source file.
    > >
    > > bug: b:488019937
    > > Change-Id: I11e191177177456ebf5deeb4843bda661cf0fe24
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7679661
    > > Commit-Queue: Gary Klassen <gklassen@chromium.org>
    > > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > > Reviewed-by: Ian Clelland <iclelland@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1602075}
    >
    > (cherry picked from commit 2243d088475145edbef4df648bd76dd90f016331)
    >
    > Bug: 502299572
    > Change-Id: I11e191177177456ebf5deeb4843bda661cf0fe24
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7759201
    > Reviewed-by: Gary Klassen <gklassen@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Gary Klassen <gklassen@chromium.org>
    > Cr-Commit-Position: refs/branch-heads/7727@{#3076}
    > Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}
    
    Bug: 502299572
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I9850f430166594235880b65c04bbf220c17a2ca4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7771381
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7727@{#3128}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- chrome/browser/glic/glic_selection_observer.h
+++ chrome/browser/glic/glic_selection_observer.h
@@ -5,35 +5,23 @@
 #ifndef CHROME_BROWSER_GLIC_GLIC_SELECTION_OBSERVER_H_
 #define CHROME_BROWSER_GLIC_GLIC_SELECTION_OBSERVER_H_
 
-#include <memory>
 #include <optional>
 #include <string>
 
 #include "base/memory/raw_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
-#include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents_observer.h"
 
 namespace content {
-class Page;
 class RenderFrameHost;
 }  // namespace content
-
-namespace views {
-class Widget;
-}  // namespace views
-
-class BrowserWindowInterface;
 
 namespace glic {
 
 class GlicKeyedService;
 
-class GlicSelectionObserver
-    : public content::WebContentsObserver,
-      public content::RenderWidgetHost::InputEventObserver {
+class GlicSelectionObserver : public content::WebContentsObserver {
  public:
   explicit GlicSelectionObserver(content::WebContents* web_contents);
   ~GlicSelectionObserver() override;
@@ -46,28 +34,8 @@ class GlicSelectionObserver
   // Virtual for testing.
   virtual void UpdateSelectionState(const std::u16string& text);
 
-  // content::WebContentsObserver:
-  void RenderFrameCreated(content::RenderFrameHost* render_frame_host) override;
-  void RenderFrameDeleted(content::RenderFrameHost* render_frame_host) override;
-  void OnVisibilityChanged(content::Visibility visibility) override;
-  void PrimaryPageChanged(content::Page& page) override;
-
-  // content::RenderWidgetHost::InputEventObserver:
-  void OnInputEvent(
-      const content::RenderWidgetHost& host,
-      const blink::WebInputEvent& event,
-      content::RenderWidgetHost::InputEventObserver::InputEventSource source)
-      override;
-
  private:
   void ProcessPendingSelection();
-
-  static void InvokeGlicFromSelectionWidget(
-      std::string prompt_text,
-      base::WeakPtr<content::WebContents> web_contents);
-
-  void ShowSelectionAffordance(const std::u16string& selected_text,
-                               BrowserWindowInterface* bwi);
 
   raw_ptr<GlicKeyedService> glic_keyed_service_;
 
@@ -79,14 +47,6 @@ class GlicSelectionObserver
 
   // The text of the last selection that was ignored due to rate limiting.
   std::optional<std::u16string> pending_selection_text_;
-
-  content::GlobalRenderFrameHostId last_selection_frame_id_;
-
-  bool is_mouse_down_ = false;
-  bool is_key_selection_ = false;
-  int bounds_retry_count_ = 0;
-
-  base::WeakPtr<views::Widget> selection_widget_;
 };
 
 }  // namespace glic
