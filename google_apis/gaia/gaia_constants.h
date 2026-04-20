9a66060d5fc65d047b8f0d4f295fdbce6a5a1d5e
Ali Hijazi <ahijazi@chromium.org>
2026-04-20 07:32:02 -0700

    [gaia] Refactor GaiaConstants to use inline constexpr
    Move all constants from `gaia_constants.cc` to `gaia_constants.h` as
    `inline constexpr char[]` to align with the Chromium style guide. This
    avoids duplicate definitions across translation units and prevents
    unnecessary relocations.
    
    As part of this change:`COMPONENT_EXPORT(GOOGLE_APIS)` is removed from
    these constants as they are now header-only and don't need to be
    exported from the shared library.
    
    Change-Id: I7ae8fea0d10be4f21d7c93ecf20c5ecde66f1b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7776793
    Commit-Queue: Ali Hijazi <ahijazi@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617474}

--- google_apis/gaia/gaia_constants.h
+++ google_apis/gaia/gaia_constants.h
@@ -14,34 +14,68 @@
 namespace GaiaConstants {
 
 // Gaia sources for accounting
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kChromeOSSource[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kChromeSource[];
+inline constexpr char kChromeOSSource[] = "chromeos";
+inline constexpr char kChromeSource[] = "ChromiumBrowser";
 // Used as Gaia source suffix to detect retry requests because of
 // |GoogleServiceAuthError::UNEXPECTED_SERVICE_RESPONSE|.
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kUnexpectedServiceResponse[];
+inline constexpr char kUnexpectedServiceResponse[] =
+    "UnexpectedServiceResponse";
 
 // OAuth2 scopes.
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kOAuth1LoginScope[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kDeviceManagementServiceOAuth[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kAnyApiOAuth2Scope[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kChromeSyncOAuth2Scope[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kGoogleUserInfoEmail[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kGoogleUserInfoProfile[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kFCMOAuthScope[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kDriveOAuth2Scope[];
-COMPONENT_EXPORT(GOOGLE_APIS)
-extern const char kAccountCapabilitiesOAuth2Scope[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kSupportContentOAuth2Scope[];
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kSecureConnectOAuth2Scope[];
+inline constexpr char kOAuth1LoginScope[] =
+    "https://www.google.com/accounts/OAuthLogin";
 
-// Used by wallet sign in helper.
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kClientOAuthEmailKey[];
+// Service/scope names for device management (cloud-based policy) server.
+inline constexpr char kDeviceManagementServiceOAuth[] =
+    "https://www.googleapis.com/auth/chromeosdevicemanagement";
+
+// OAuth2 scope for access to all Google APIs.
+inline constexpr char kAnyApiOAuth2Scope[] =
+    "https://www.googleapis.com/auth/any-api";
+
+// OAuth2 scope for access to Chrome sync APIs
+inline constexpr char kChromeSyncOAuth2Scope[] =
+    "https://www.googleapis.com/auth/chromesync";
+
+// OAuth2 scope for access to Google account information.
+inline constexpr char kGoogleUserInfoEmail[] =
+    "https://www.googleapis.com/auth/userinfo.email";
+inline constexpr char kGoogleUserInfoProfile[] =
+    "https://www.googleapis.com/auth/userinfo.profile";
+
+// OAuth2 scope for FCM, the Firebase Cloud Messaging service.
+inline constexpr char kFCMOAuthScope[] =
+    "https://www.googleapis.com/auth/firebase.messaging";
+
+// OAuth2 scope for access to Drive.
+inline constexpr char kDriveOAuth2Scope[] =
+    "https://www.googleapis.com/auth/drive";
+
+// OAuth2 scope for access to Account Capabilities API.
+inline constexpr char kAccountCapabilitiesOAuth2Scope[] =
+    "https://www.googleapis.com/auth/account.capabilities";
+
+// OAuth2 scope for support content API.
+inline constexpr char kSupportContentOAuth2Scope[] =
+    "https://www.googleapis.com/auth/supportcontent";
+
+// OAuth2 scope for access to the SecureConnect API.
+inline constexpr char kSecureConnectOAuth2Scope[] =
+    "https://www.googleapis.com/auth/bce.secureconnect";
+
+// Used to build ClientOAuth requests.  These are the names of keys used when
+// building base::DictionaryValue that represent the json data that makes up
+// the ClientOAuth endpoint protocol.  The comment above each constant explains
+// what value is associated with that key.
+//
+// Canonical email of the account to sign in.
+inline constexpr char kClientOAuthEmailKey[] = "email";
 
 // Refresh token that is guaranteed to be invalid.
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kInvalidRefreshToken[];
+inline constexpr char kInvalidRefreshToken[] = "invalid_refresh_token";
 
 // Name of the Google authentication cookie.
-COMPONENT_EXPORT(GOOGLE_APIS) extern const char kGaiaSigninCookieName[];
+inline constexpr char kGaiaSigninCookieName[] = "SAPISID";
 
 // Constants for the Chrome Refresh Token Binding.
 inline constexpr std::string_view kTokenBindingAssertionSentinel =
