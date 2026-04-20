2cf61be73a70412e5829154f0276b485f512a40e
Yann Dago <ydago@chromium.org>
2026-04-20 13:12:27 -0700

    Enable EnableExtensionInstallPolicyFetching by default.
    The feature is now enabled by default, so the corresponding field trial testing configuration is no longer necessary and has been removed.
    
    Change-Id: I90fa52c13eae9706a0fde2aee094933431598958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7777038
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617731}

--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -54,7 +54,7 @@ const base::FeatureParam<std::string> kDesktopAndroidPolicyBlocklist{
 
 // Used to enable extension install policy support.
 BASE_FEATURE(kEnableExtensionInstallPolicyFetching,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // When enabled, uses ManagementService to determine whether to honor sensitive
 // policies. When disabled, falls back to the original ShouldHonorPolicies()
