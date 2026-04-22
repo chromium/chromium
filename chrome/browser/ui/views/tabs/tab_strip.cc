94d1e9b8beafc2688e1e2411fecf5b95c5462462
Charles Meng <charlesmeng@chromium.org>
2026-04-22 14:12:35 -0700

    Fix split view tutorial crash when switching tabstrip orientation
    The split view tutorial bubble closing causes a widget activation change
    but at that point we are already tearing down the horizontal tabstrip,
    so add a check for the tab container existing to
    TabStrip::OnWidgetActivationChanged. Now, the tutorial aborts instead.
    This is consistent with the behavior of other tutorials when switching
    tabstrip orientations. We could consider making tutorials continue
    through tabstrip orientation changes as a follow-up.
    
    Fixed: 504700948
    Change-Id: I04a48941bf88f5c54a4e9d6ac134bfeed467a3e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7786914
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619081}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -2434,6 +2434,10 @@ void TabStrip::OnGestureEvent(ui::GestureEvent* event) {
 }
 
 void TabStrip::OnWidgetActivationChanged(views::Widget* widget, bool active) {
+  if (!tab_container_) {
+    return;
+  }
+
   if (active && selected_tabs_.active().has_value()) {
     // When the browser window is activated, set the accessible selection and
     // fire a selection event on the currently active tab, to help enable
