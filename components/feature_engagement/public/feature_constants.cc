627ef18b48ef9e15b1198636601d4e4a868c570c
Alison Gale <agale@chromium.org>
2026-04-23 11:24:44 -0700

    [Vertical Tabs] Disable split view tutorial in field trial and ToT
    Turning down the feature on ToT as part of the Emergency kill switch process to expedite the turndown of the feature via Finch
    
    Bug: 493628274, 505491832
    Change-Id: Ib9cd3c896239f5260a6764d2f2f2050931e351f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7787497
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Auto-Submit: Alison Gale <agale@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1619640}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -231,7 +231,7 @@ BASE_FEATURE(kIPHSideBySidePinnableFeature,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHSideBySideTabSwitchFeature,
              "IPH_SideBySideTabSwitchFeature",
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHVerticalTabsExpandOnHoverFeature,
              "IPH_VerticalTabsExpandOnHoverFeature",
              base::FEATURE_DISABLED_BY_DEFAULT);
