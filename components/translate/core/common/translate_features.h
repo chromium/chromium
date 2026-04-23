7f9eb373601253c6db724cc3aaee07981eed58aa
Katie Lu <ktlu@google.com>
2026-04-23 16:40:59 -0700

    [M148] Add feature flag for PDF translation.
    Original change's description:
    > Add feature flag for PDF translation.
    >
    > This change introduces `translate_features.h` and `translate_features.cc` to define a new base::Feature, `kEnableTranslatePdf`, which will control whether PDF translation is enabled.
    >
    > Change-Id: I247909d3999fe3f63d44c600f62675668db53262
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7732986
    > Commit-Queue: Katie Lu <ktlu@google.com>
    > Reviewed-by: Danilo Françoso Tedeschi <danft@google.com>
    > Reviewed-by: Ian Wells <iwells@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1611705}
    
    (cherry picked from commit 6536bd673eb172c8aa5c86dffdf211e12922bcbc)
    
    Bug: 505786823
    Change-Id: I247909d3999fe3f63d44c600f62675668db53262
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7791448
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Jacobo Tapia <jatapiaro@google.com>
    Commit-Queue: Jacobo Tapia <jatapiaro@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7778@{#1503}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

---
+++ components/translate/core/common/translate_features.h
@@ -0,0 +1,17 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_TRANSLATE_CORE_COMMON_TRANSLATE_FEATURES_H_
+#define COMPONENTS_TRANSLATE_CORE_COMMON_TRANSLATE_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace translate {
+
+// Controls whether PDF translation is enabled.
+BASE_DECLARE_FEATURE(kEnableTranslatePdf);
+
+}  // namespace translate
+
+#endif  // COMPONENTS_TRANSLATE_CORE_COMMON_TRANSLATE_FEATURES_H_
