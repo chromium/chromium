238abb0dc6bfdf1d35d06758ccd172644d1313f7
Sky Malice <skym@chromium.org>
2026-04-24 10:43:55 -0700

    [M147] Disable CompositorViewHolderObscuring.
    Original change's description:
    > Disable CompositorViewHolderObscuring.
    >
    > In preparation for activating killswitch via Finch.
    >
    > Bug: 504629075
    > Change-Id: I9c27339b2fbd73822b045c83cec984a9e0205bb7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7780186
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1617834}
    
    (cherry picked from commit 69be62ce4e9af4228d76931d643898a987e5e971)
    
    Bug: 506129522,504629075
    Change-Id: I9c27339b2fbd73822b045c83cec984a9e0205bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793169
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7727@{#3625}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -653,7 +653,7 @@ BASE_FEATURE(kClankStartupLatencyInjection, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kClankWhatsNew, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kClearIntentWhenRecreated, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kCommandLineOnNonRooted, base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kCompositorViewHolderObscuring, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kCompositorViewHolderObscuring, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kCompositorViewRemeasureFix, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kContextMenuTranslateWithGoogleLens, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kContextualSearchDisableOnlineDetection, base::FEATURE_DISABLED_BY_DEFAULT);
