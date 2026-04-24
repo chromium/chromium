bc02a9619d5a63f6d18ac2b43fbc60b44478e572
Nasser Al-shawwa <alshawwa@chromium.org>
2026-04-24 14:23:23 -0700

    Add EnableDeepScanVerdictCacheSize feature flag
    Bug: 498626434
    Change-Id: I06c81a25e213b7c9074eccdc7ebf3ef970bc4eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793467
    Reviewed-by: Shanthanu (Shanth) Bhardwaj <xanth@google.com>
    Commit-Queue: Shanthanu (Shanth) Bhardwaj <xanth@google.com>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620468}

---
+++ components/enterprise/data_protection/features.cc
@@ -0,0 +1,12 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/enterprise/data_protection/features.h"
+
+namespace enterprise_data_protection {
+
+BASE_FEATURE(kEnableDeepScanVerdictCacheSize,
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace enterprise_data_protection
---
+++ components/enterprise/data_protection/features.h
@@ -0,0 +1,15 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ENTERPRISE_DATA_PROTECTION_FEATURES_H_
+#define COMPONENTS_ENTERPRISE_DATA_PROTECTION_FEATURES_H_
+#include "base/feature_list.h"
+
+namespace enterprise_data_protection {
+
+BASE_DECLARE_FEATURE(kEnableDeepScanVerdictCacheSize);
+
+}  // namespace enterprise_data_protection
+
+#endif  // COMPONENTS_ENTERPRISE_DATA_PROTECTION_FEATURES_H_
