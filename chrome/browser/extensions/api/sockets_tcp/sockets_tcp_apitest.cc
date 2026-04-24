26469c944ef0f32355b75af43ed25c17e0110b3c
James Cook <jamescook@chromium.org>
2026-04-24 15:40:00 -0700

    extensions: Move SocketsTcpApiTest to chrome's browser_tests
    Currently the //extensions directory has a small number of integration
    (API and browser) tests that run under a long-deprecated binary called
    "app shell". We want to migrate them to run in chrome's browser_tests
    suite, as this is a more realistic environment for integration tests.
    
    - Move the test to //chrome/browser/extensions
    - Run it as ExtensionApiTest
    - Update feature flags needed for expected DNS lookup behavior (with
      respect to "network anonymization keys")
    - Move test data to //chrome/test
    
    Bug: 40425056
    Change-Id: I97f01a74b86b1b7bb50c49ec6cc6d94eca74788f
    Bypass-Check-License: file move
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793611
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1620520}

--- chrome/browser/extensions/api/sockets_tcp/sockets_tcp_apitest.cc
+++ chrome/browser/extensions/api/sockets_tcp/sockets_tcp_apitest.cc
@@ -8,19 +8,20 @@
 #include "base/memory/ref_counted.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/scoped_feature_list.h"
+#include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
+#include "content/public/test/browser_test.h"
 #include "content/public/test/network_service_test_helper.h"
 #include "extensions/browser/api/socket/write_quota_checker.h"
 #include "extensions/browser/api/sockets_tcp/sockets_tcp_api.h"
 #include "extensions/browser/api_test_utils.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_builder.h"
-#include "extensions/shell/test/shell_apitest.h"
-#include "extensions/shell/test/shell_test.h"
 #include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -33,24 +34,30 @@
 #include "services/network/public/mojom/network_service.mojom.h"
 #include "services/network/test/test_dns_util.h"
 
+// This API is not supported on Android.
+static_assert(!BUILDFLAG(IS_ANDROID));
+
 namespace extensions {
+namespace {
 
 const char kHostname[] = "www.foo.test";
 
-class SocketsTcpApiTest : public ShellApiTest {
+class SocketsTcpApiTest : public ExtensionApiTest {
  public:
   SocketsTcpApiTest() {
-    // Enable kPartitionConnectionsByNetworkIsolationKey so the test can verify
-    // that the correct NetworkAnonymizationKey was used for the DNS lookup.
-    scoped_feature_list_.InitAndEnableFeature(
-        net::features::kPartitionConnectionsByNetworkIsolationKey);
+    // Enable the features required for the test to verify that the correct
+    // NetworkAnonymizationKey was used for the DNS lookup.
+    scoped_feature_list_.InitWithFeatures(
+        {net::features::kPartitionConnectionsByNetworkIsolationKey,
+         net::features::kSplitHostCacheByNetworkAnonymizationKey},
+        {});
 
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kUseMockCertVerifierForTesting);
   }
 
   void SetUpOnMainThread() override {
-    ShellApiTest::SetUpOnMainThread();
+    ExtensionApiTest::SetUpOnMainThread();
     host_resolver()->AddRule(kHostname, "127.0.0.1");
   }
 
@@ -68,7 +75,7 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketsTcpCreateGood) {
 
   std::optional<base::Value> result(
       api_test_utils::RunFunctionAndReturnSingleResult(
-          socket_create_function.get(), "[]", browser_context()));
+          socket_create_function.get(), "[]", profile()));
   ASSERT_TRUE(result);
   ASSERT_TRUE(result->is_dict());
   std::optional<int> socket_id = result->GetDict().FindInt("socketId");
@@ -77,11 +84,10 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketsTcpCreateGood) {
 }
 
 IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpExtension) {
-  net::EmbeddedTestServer test_server(net::EmbeddedTestServer::TYPE_HTTP);
-  test_server.AddDefaultHandlers();
-  EXPECT_TRUE(test_server.Start());
+  embedded_test_server()->AddDefaultHandlers();
+  ASSERT_TRUE(embedded_test_server()->Start());
 
-  net::HostPortPair host_port_pair = test_server.host_port_pair();
+  net::HostPortPair host_port_pair = embedded_test_server()->host_port_pair();
   int port = host_port_pair.port();
   ASSERT_TRUE(port > 0);
 
@@ -89,12 +95,13 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpExtension) {
   host_port_pair.set_host(kHostname);
 
   ResultCatcher catcher;
-  catcher.RestrictToBrowserContext(browser_context());
+  catcher.RestrictToBrowserContext(profile());
 
   ExtensionTestMessageListener listener("info_please",
                                         ReplyBehavior::kWillReply);
 
-  scoped_refptr<const Extension> test_extension = LoadApp("sockets_tcp/api");
+  scoped_refptr<const Extension> test_extension =
+      LoadExtension(test_data_dir_.AppendASCII("sockets_tcp/api"));
   ASSERT_TRUE(test_extension);
 
   EXPECT_TRUE(listener.WaitUntilSatisfied());
@@ -107,7 +114,7 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpExtension) {
   // DNS lookup using the expected NIK, and make sure the IP address is
   // retrieved.
   network::mojom::NetworkContext* network_context =
-      browser_context()->GetDefaultStoragePartition()->GetNetworkContext();
+      profile()->GetDefaultStoragePartition()->GetNetworkContext();
   network::mojom::ResolveHostParametersPtr params =
       network::mojom::ResolveHostParameters::New();
   // Cache only lookup.
@@ -130,6 +137,7 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpExtension) {
   network::DnsLookupResult result2 = network::BlockingDnsLookup(
       network_context, host_port_pair, std::move(params),
       net::NetworkAnonymizationKey());
+  EXPECT_EQ(0, result2.resolved_addresses.size());
   EXPECT_EQ(net::ERR_NAME_NOT_RESOLVED, result2.error);
 }
 
@@ -154,12 +162,12 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpExtensionTLS) {
   ASSERT_GT(https_port, 0);
 
   ResultCatcher catcher;
-  catcher.RestrictToBrowserContext(browser_context());
+  catcher.RestrictToBrowserContext(profile());
 
   ExtensionTestMessageListener listener("info_please",
                                         ReplyBehavior::kWillReply);
 
-  ASSERT_TRUE(LoadApp("sockets_tcp/api"));
+  ASSERT_TRUE(LoadExtension(test_data_dir_.AppendASCII("sockets_tcp/api")));
   EXPECT_TRUE(listener.WaitUntilSatisfied());
   listener.Reply(base::StringPrintf(
       "https:%s:%d", https_host_port_pair.host().c_str(), https_port));
@@ -168,8 +176,7 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpExtensionTLS) {
 }
 
 IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpSendWriteQuota) {
-  WriteQuotaChecker* write_quota_checker =
-      WriteQuotaChecker::Get(browser_context());
+  WriteQuotaChecker* write_quota_checker = WriteQuotaChecker::Get(profile());
   constexpr size_t kBytesLimit = 1;
   WriteQuotaChecker::ScopedBytesLimitForTest scoped_quota(write_quota_checker,
                                                           kBytesLimit);
@@ -186,12 +193,13 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpSendWriteQuota) {
   host_port_pair.set_host(kHostname);
 
   ResultCatcher catcher;
-  catcher.RestrictToBrowserContext(browser_context());
+  catcher.RestrictToBrowserContext(profile());
 
   ExtensionTestMessageListener listener("info_please",
                                         ReplyBehavior::kWillReply);
 
-  scoped_refptr<const Extension> test_extension = LoadApp("sockets_tcp/api");
+  scoped_refptr<const Extension> test_extension =
+      LoadExtension(test_data_dir_.AppendASCII("sockets_tcp/api"));
   ASSERT_TRUE(test_extension);
 
   EXPECT_TRUE(listener.WaitUntilSatisfied());
@@ -201,4 +209,5 @@ IN_PROC_BROWSER_TEST_F(SocketsTcpApiTest, SocketTcpSendWriteQuota) {
   EXPECT_TRUE(catcher.GetNextResult()) << catcher.message();
 }
 
+}  // namespace
 }  // namespace extensions
