6819a106c96188395dda3c08050ab97185b6ac9d
Joey Arhar <jarhar@chromium.org>
2026-04-25 10:48:06 -0700

    Add missing SetNeedsValidityCheck for <select multiple size=1>
    Without this call to SetNeedsValidityCheck, the :valid and :invalid
    pseudo-classes were not being updated after selecting or deselecting
    options in <select multiple size=1> when the user clicks or uses the
    keyboard.
    
    Fixed: 504907360
    Change-Id: Ibb77053fe6815096454ae2770a7e664c8e2ab103
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793366
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620712}

--- third_party/blink/renderer/core/html/forms/html_select_element.cc
+++ third_party/blink/renderer/core/html/forms/html_select_element.cc
@@ -1030,6 +1030,7 @@ void HTMLSelectElement::SelectOptionFromPopoverPickerOrListbox(
         }
       }
     }
+    SetNeedsValidityCheck();
     DispatchInputEvent();
     DispatchChangeEvent();
     // TODO call UpdateAllSelectedcontents()
