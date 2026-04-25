fabe117bfedfd9413ed9cc76de7de9077d16e205
Riley Tatum <rtatum@google.com>
2026-04-24 17:02:29 -0700

    [148] Cleanup unnecessary cr_icons.css.js usage
    Original change's description:
    > Cleanup unnecessary cr_icons.css.js usage
    > 
    > Removes importing of cr_icons.css.js within Lit components, which
    > should be using the Lit version instead. Importing the Polymer version
    > was causing the NTP to load Polymer, even though it doesn't need it.
    > 
    > Included an omnibox and bookmarks cleanup as well to cleanup more uses.
    > 
    > Fixed: 504715479
    > Change-Id: Ia0af2b12ecc9e4cb5dae6483d45d9bbce8ee11f9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7779827
    > Commit-Queue: Riley Tatum <rtatum@google.com>
    > Reviewed-by: Nihar Majmudar <niharm@google.com>
    > Reviewed-by: David Roger <droger@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1618252}
    
    Bug: 506127643
    Change-Id: Ia0af2b12ecc9e4cb5dae6483d45d9bbce8ee11f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793809
    Owners-Override: Krishna Govind <govind@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7778@{#1598}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- chrome/browser/resources/bookmarks/promo_card.ts
+++ chrome/browser/resources/bookmarks/promo_card.ts
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
-import 'chrome://resources/cr_elements/cr_icons.css.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
