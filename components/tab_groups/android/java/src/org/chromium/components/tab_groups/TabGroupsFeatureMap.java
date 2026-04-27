00dca678c071c600c02ce86554060a9adb7ba693
Hitarth Kothari <hitarthkothari@google.com>
2026-04-27 07:59:03 -0700

    [TabGroups] Add feature map and flag for tab group colors
    NO_IFTTT=Flag in different feature map
    
    Bug: 502156538
    Change-Id: Ic01f3a82ed5971fc3beecfabea20eb745192530c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7757982
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621086}

---
+++ components/tab_groups/android/java/src/org/chromium/components/tab_groups/TabGroupsFeatureMap.java
@@ -0,0 +1,53 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.tab_groups;
+
+import org.jni_zero.JNINamespace;
+import org.jni_zero.NativeMethods;
+
+import org.chromium.base.FeatureMap;
+import org.chromium.base.MutableFlagWithSafeDefault;
+import org.chromium.build.annotations.NullMarked;
+
+/** Java accessor for base::Features listed in native */
+@JNINamespace("tab_groups_android")
+@NullMarked
+public class TabGroupsFeatureMap extends FeatureMap {
+    public static final String ANDROID_TAB_GROUP_COLORS = "AndroidTabGroupColors";
+
+    private static final TabGroupsFeatureMap sInstance = new TabGroupsFeatureMap();
+
+    public static final MutableFlagWithSafeDefault sAndroidTabGroupColors =
+            newMutableFlagWithSafeDefault(ANDROID_TAB_GROUP_COLORS, false);
+
+    // Do not instantiate this class.
+    private TabGroupsFeatureMap() {}
+
+    /** Returns the singleton TabGroupsFeatureMap. */
+    public static TabGroupsFeatureMap getInstance() {
+        return sInstance;
+    }
+
+    /** Convenience method to call {@link #isEnabledInNative(String)} statically. */
+    public static boolean isEnabled(String featureName) {
+        return getInstance().isEnabledInNative(featureName);
+    }
+
+    @Override
+    protected long getNativeMap() {
+        return TabGroupsFeatureMapJni.get().getNativeMap();
+    }
+
+    @NativeMethods
+    public interface Natives {
+        long getNativeMap();
+    }
+
+    private static MutableFlagWithSafeDefault newMutableFlagWithSafeDefault(
+            String featureName, boolean defaultValue) {
+        return TabGroupsFeatureMap.getInstance()
+                .mutableFlagWithSafeDefault(featureName, defaultValue);
+    }
+}
---
+++ components/tab_groups/android/tab_groups_feature_map.cc
@@ -0,0 +1,37 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/android/feature_map.h"
+#include "base/no_destructor.h"
+#include "components/tab_groups/android/features.h"
+#include "components/tab_groups/tab_groups_jni_headers/TabGroupsFeatureMap_jni.h"
+
+namespace tab_groups_android {
+
+namespace {
+
+// Array of features exposed through the Java TabGroupsFeatureMap API.
+// Entries in this array may either refer to features defined in
+// components/tab_groups/android/features.h or in other locations in the code
+// base.
+const base::Feature* const kFeaturesExposedToJava[] = {
+    &kAndroidTabGroupColors,
+};
+
+// static
+base::android::FeatureMap* GetFeatureMap() {
+  static base::NoDestructor<base::android::FeatureMap> kFeatureMap(
+      kFeaturesExposedToJava);
+  return kFeatureMap.get();
+}
+
+}  // namespace
+
+static int64_t JNI_TabGroupsFeatureMap_GetNativeMap(JNIEnv* env) {
+  return reinterpret_cast<int64_t>(GetFeatureMap());
+}
+
+DEFINE_JNI(TabGroupsFeatureMap)
+
+}  // namespace tab_groups_android
