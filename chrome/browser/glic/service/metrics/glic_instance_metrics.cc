84dbc4d8113df815769419c142308e9a5ef30ce0
Michelle Abreo <michelleabreo@google.com>
2026-04-27 12:29:37 -0700

    [M148] [Skills] Clean up skills menu telemetry enums
    Original change's description:
    > [Skills] Clean up skills menu telemetry enums
    >
    > This CL clarifies the skills menu enum naming conventions regarding the
    > 'browse' vs. 'manage' actions from the previous implementation. These
    > changes ensure accurate telemetry mapping without breaking the existing
    > API.
    >
    > - Fixed `kClickedManageFromMenu` (Value 5) comment to match its name ('Manage skills').
    > - Deprecated Value 15 since Value 5 now covers the 'Manage' action.
    > - Added Value 17 (`kClickedBrowseSkillsFromMenu`) for the 'Browse' action.
    > - Updated enums.xml labels to use "Slash Menu" and avoid duplicate entries.
    >
    > This update standardizes the telemetry events and improves overall
    > consistency.
    >
    > Bug: b:505036365
    > Change-Id: I543bcf2169f4b53287d49ed662ce7980d42fa993
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782429
    > Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    > Reviewed-by: Nick Birnie <birnie@google.com>
    > Commit-Queue: Michelle Abreo <michelleabreo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1619304}
    
    (cherry picked from commit f8ed3b1c6f2d40e09a6ce09f85e2632738ae8b9f)
    
    Bug: 505844150,b:505036365
    Change-Id: I543bcf2169f4b53287d49ed662ce7980d42fa993
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797015
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7778@{#1792}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- chrome/browser/glic/service/metrics/glic_instance_metrics.cc
+++ chrome/browser/glic/service/metrics/glic_instance_metrics.cc
@@ -1127,6 +1127,7 @@ void GlicInstanceMetrics::RecordSkillsWebClientEvent(
     case kClickedMoreFromMenu:
     case kClickedManageSkillFromMenu:
     case kClickedSkillFromMenu:
+    case kClickedBrowseSkillsFromMenu:
       break;
     // --- SkillBuilder Metrics ---
     case kSkillBuilderClickedPromoChip:
