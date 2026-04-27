8b92891958a0a20ba79418f882fafe61b6a6782d
Ahmed Raslan <rslan@google.com>
2026-04-27 08:49:39 -0700

    Enable kDataControlsDragEnforcement by default.
    BUG=406716605
    
    Change-Id: Ifd644fa9ed4f7a19382845b8e466e49289887831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7796798
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Anatoli Hancharou <antoli@google.com>
    Reviewed-by: Aliaksei Bahdzevich <thelex@google.com>
    Commit-Queue: Ahmed Raslan <rslan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1621122}

--- components/enterprise/data_controls/core/browser/features.cc
+++ components/enterprise/data_controls/core/browser/features.cc
@@ -13,7 +13,7 @@ BASE_FEATURE(kEnableClipboardDataControlsAndroid,
 
 BASE_FEATURE(kEnableDownloadDataControls, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kDataControlsDragEnforcement, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kDataControlsDragEnforcement, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDataControlsSearchWith, base::FEATURE_DISABLED_BY_DEFAULT);
 
