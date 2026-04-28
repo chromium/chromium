6d727c8ba99a7dbfa5891e1c1b6aacf815658d30
Tim Judkins <tjudkins@chromium.org>
2026-04-28 16:46:04 -0700

    [Extensions] Split WebIdlDiffToolTest to separate test cases
    Currently the WebIdlDiffToolTest runs as a single test case that runs
    over every converted schema file. This CL changes it to generate a
    separate test case for each pair of files, as it's much more useful to
    see a progress of dots as the test runs, rather than a single dot when
    it has finished. No functional changes expected (other than more dots).
    
    Bug: N/A
    Change-Id: I507f6e1c7e49c88b96c599fadf6bc84b71d4d014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793738
    Commit-Queue: Tim <tjudkins@chromium.org>
    Auto-Submit: Tim <tjudkins@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622113}

--- tools/json_schema_compiler/web_idl_diff_tool_test.py
+++ tools/json_schema_compiler/web_idl_diff_tool_test.py
@@ -12,92 +12,96 @@ import web_idl_diff_tool
 # initially converted to WebIDL.
 
 
-class WebIdlDiffToolTest(unittest.TestCase):
+# Note: the following schemas were copied over at the time of their conversion
+# and are not intended to be kept up to date with any more recent updates. Once
+# all the old IDL has been converted and we are ready to remove the old parser,
+# these can all be deleted and this test removed.
+_CONVERTED_SCHEMAS = [
+    ('alarms.idl', 'alarms.webidl'),
+    ('app_current_window_internal.idl', 'app_current_window_internal.webidl'),
+    ('bluetooth.idl', 'bluetooth.webidl'),
+    ('dns.idl', 'dns.webidl'),
+    ('audio.idl', 'audio.webidl'),
+    ('cec_private.idl', 'cec_private.webidl'),
+    ('diagnostics.idl', 'diagnostics.webidl'),
+    ('virtual_keyboard.idl', 'virtual_keyboard.webidl'),
+    ('webcam_private.idl', 'webcam_private.webidl'),
+    ('extension_options_internal.idl', 'extension_options_internal.webidl'),
+    ('system_cpu.idl', 'system_cpu.webidl'),
+    ('system_memory.idl', 'system_memory.webidl'),
+    ('system_network.idl', 'system_network.webidl'),
+    ('system_storage.idl', 'system_storage.webidl'),
+    ('app_runtime.idl', 'app_runtime.webidl'),
+    ('bluetooth_low_energy.idl', 'bluetooth_low_energy.webidl'),
+    ('bluetooth_socket.idl', 'bluetooth_socket.webidl'),
+    ('clipboard.idl', 'clipboard.webidl'),
+    ('system_display.idl', 'system_display.webidl'),
+    ('usb.idl', 'usb.webidl'),
+    ('chrome_url_overrides.idl', 'chrome_url_overrides.webidl'),
+    ('cross_origin_isolation.idl', 'cross_origin_isolation.webidl'),
+    ('enterprise_reporting_private.idl', 'enterprise_reporting_private.webidl'),
+    ('file_handlers.idl', 'file_handlers.webidl'),
+    ('oauth2.idl', 'oauth2.webidl'),
+    ('protocol_handlers.idl', 'protocol_handlers.webidl'),
+    ('shared_module.idl', 'shared_module.webidl'),
+    ('web_accessible_resources.idl', 'web_accessible_resources.webidl'),
+    ('enterprise_platform_keys_internal.idl',
+     'enterprise_platform_keys_internal.webidl'),
+    ('web_accessible_resources_mv2.idl', 'web_accessible_resources_mv2.webidl'),
+    ('automation_internal.idl', 'automation_internal.webidl'),
+    ('automation.idl', 'automation.webidl'),
+    ('feedback_private.idl', 'feedback_private.webidl'),
+    ('media_perception_private.idl', 'media_perception_private.webidl'),
+    ('mojo_private.idl', 'mojo_private.webidl'),
+    ('networking_onc.idl', 'networking_onc.webidl'),
+    ('networking_private.idl', 'networking_private.webidl'),
+    ('offscreen.idl', 'offscreen.webidl'),
+    ('power.idl', 'power.webidl'),
+    ('serial.idl', 'serial.webidl'),
+    ('bluetooth_private.idl', 'bluetooth_private.webidl'),
+    ('braille_display_private.idl', 'braille_display_private.webidl'),
+    ('content_scripts.idl', 'content_scripts.webidl'),
+    ('declarative_net_request.idl', 'declarative_net_request.webidl'),
+    ('printer_provider.idl', 'printer_provider.webidl'),
+    ('printer_provider_internal.idl', 'printer_provider_internal.webidl'),
+    ('scripts_internal.idl', 'scripts_internal.webidl'),
+    ('user_scripts.idl', 'user_scripts.webidl'),
+    ('app_window.idl', 'app_window.webidl'),
+    ('accessibility_service_private.idl',
+     'accessibility_service_private.webidl'),
+    ('appview_tag.idl', 'appview_tag.webidl'),
+    ('autotest_private.idl', 'autotest_private.webidl'),
+    ('certificate_provider.idl', 'certificate_provider.webidl'),
+    ('certificate_provider_internal.idl',
+     'certificate_provider_internal.webidl'),
+    ('crash_report_private.idl', 'crash_report_private.webidl'),
+    ('developer_private.idl', 'developer_private.webidl'),
+    ('document_scan.idl', 'document_scan.webidl'),
+    ('downloads.idl', 'downloads.webidl'),
+    ('downloads_internal.idl', 'downloads_internal.webidl'),
+    ('enterprise_device_attributes.idl', 'enterprise_device_attributes.webidl'),
+    ('enterprise_hardware_platform.idl', 'enterprise_hardware_platform.webidl'),
+    ('enterprise_kiosk_input.idl', 'enterprise_kiosk_input.webidl'),
+    ('enterprise_login.idl', 'enterprise_login.webidl'),
+    ('enterprise_networking_attributes.idl',
+     'enterprise_networking_attributes.webidl'),
+    ('enterprise_platform_keys.idl', 'enterprise_platform_keys.webidl'),
+]
 
-  def testIdlToWebIdlConversion(self):
-    # Note: the following schemas were copied over at the time of their
-    # conversion and are not intended to be kept up to date with any more recent
-    # updates. Once all the old IDL has been converted and we are ready to
-    # remove the old parser, these can all be deleted and this test removed.
-    converted_schemas = [
-        ('alarms.idl', 'alarms.webidl'),
-        ('app_current_window_internal.idl',
-         'app_current_window_internal.webidl'),
-        ('bluetooth.idl', 'bluetooth.webidl'),
-        ('dns.idl', 'dns.webidl'),
-        ('audio.idl', 'audio.webidl'),
-        ('cec_private.idl', 'cec_private.webidl'),
-        ('diagnostics.idl', 'diagnostics.webidl'),
-        ('virtual_keyboard.idl', 'virtual_keyboard.webidl'),
-        ('webcam_private.idl', 'webcam_private.webidl'),
-        ('extension_options_internal.idl', 'extension_options_internal.webidl'),
-        ('system_cpu.idl', 'system_cpu.webidl'),
-        ('system_memory.idl', 'system_memory.webidl'),
-        ('system_network.idl', 'system_network.webidl'),
-        ('system_storage.idl', 'system_storage.webidl'),
-        ('app_runtime.idl', 'app_runtime.webidl'),
-        ('bluetooth_low_energy.idl', 'bluetooth_low_energy.webidl'),
-        ('bluetooth_socket.idl', 'bluetooth_socket.webidl'),
-        ('clipboard.idl', 'clipboard.webidl'),
-        ('system_display.idl', 'system_display.webidl'),
-        ('usb.idl', 'usb.webidl'),
-        ('chrome_url_overrides.idl', 'chrome_url_overrides.webidl'),
-        ('cross_origin_isolation.idl', 'cross_origin_isolation.webidl'),
-        ('enterprise_reporting_private.idl',
-         'enterprise_reporting_private.webidl'),
-        ('file_handlers.idl', 'file_handlers.webidl'),
-        ('oauth2.idl', 'oauth2.webidl'),
-        ('protocol_handlers.idl', 'protocol_handlers.webidl'),
-        ('shared_module.idl', 'shared_module.webidl'),
-        ('web_accessible_resources.idl', 'web_accessible_resources.webidl'),
-        ('enterprise_platform_keys_internal.idl',
-         'enterprise_platform_keys_internal.webidl'),
-        ('web_accessible_resources_mv2.idl',
-         'web_accessible_resources_mv2.webidl'),
-        ('automation_internal.idl', 'automation_internal.webidl'),
-        ('automation.idl', 'automation.webidl'),
-        ('feedback_private.idl', 'feedback_private.webidl'),
-        ('media_perception_private.idl', 'media_perception_private.webidl'),
-        ('mojo_private.idl', 'mojo_private.webidl'),
-        ('networking_onc.idl', 'networking_onc.webidl'),
-        ('networking_private.idl', 'networking_private.webidl'),
-        ('offscreen.idl', 'offscreen.webidl'),
-        ('power.idl', 'power.webidl'),
-        ('serial.idl', 'serial.webidl'),
-        ('bluetooth_private.idl', 'bluetooth_private.webidl'),
-        ('braille_display_private.idl', 'braille_display_private.webidl'),
-        ('content_scripts.idl', 'content_scripts.webidl'),
-        ('declarative_net_request.idl', 'declarative_net_request.webidl'),
-        ('printer_provider.idl', 'printer_provider.webidl'),
-        ('printer_provider_internal.idl', 'printer_provider_internal.webidl'),
-        ('scripts_internal.idl', 'scripts_internal.webidl'),
-        ('user_scripts.idl', 'user_scripts.webidl'),
-        ('app_window.idl', 'app_window.webidl'),
-        ('accessibility_service_private.idl',
-         'accessibility_service_private.webidl'),
-        ('appview_tag.idl', 'appview_tag.webidl'),
-        ('autotest_private.idl', 'autotest_private.webidl'),
-        ('certificate_provider.idl', 'certificate_provider.webidl'),
-        ('certificate_provider_internal.idl',
-         'certificate_provider_internal.webidl'),
-        ('crash_report_private.idl', 'crash_report_private.webidl'),
-        ('developer_private.idl', 'developer_private.webidl'),
-        ('document_scan.idl', 'document_scan.webidl'),
-        ('downloads.idl', 'downloads.webidl'),
-        ('downloads_internal.idl', 'downloads_internal.webidl'),
-        ('enterprise_device_attributes.idl',
-         'enterprise_device_attributes.webidl'),
-        ('enterprise_hardware_platform.idl',
-         'enterprise_hardware_platform.webidl'),
-        ('enterprise_kiosk_input.idl', 'enterprise_kiosk_input.webidl'),
-        ('enterprise_login.idl', 'enterprise_login.webidl'),
-        ('enterprise_networking_attributes.idl',
-         'enterprise_networking_attributes.webidl'),
-        ('enterprise_platform_keys.idl', 'enterprise_platform_keys.webidl'),
-    ]
-    # LoadAndReturnUnifiedDiff expects file paths relative to the repo root.
-    converted_schema_path = 'tools/json_schema_compiler/test/converted_schemas/'
-    for old_schema_name, new_schema_name in converted_schemas:
+class WebIdlDiffToolTest(unittest.TestCase):
+  pass
+
+
+# Helper function to loop over the converted schema file pairs and add a
+# separate test case function to WebIdlDiffToolTest for each one.
+def _GenerateTestMethods():
+  # LoadAndReturnUnifiedDiff expects file paths relative to the repo root.
+  converted_schema_path = 'tools/json_schema_compiler/test/converted_schemas/'
+  for old_schema_name, new_schema_name in _CONVERTED_SCHEMAS:
+
+    def test(self,
+             old_schema_name=old_schema_name,
+             new_schema_name=new_schema_name):
       old_filename = converted_schema_path + old_schema_name
       new_filename = converted_schema_path + new_schema_name
       diff = web_idl_diff_tool.LoadAndReturnUnifiedDiff(old_filename,
@@ -108,6 +112,12 @@ class WebIdlDiffToolTest(unittest.TestCase):
           f"Difference detected between '{old_filename}' and"
           f" '{new_filename}':\n{diff}",
       )
+
+    test_name = f'test_{old_schema_name.replace(".", "_")}'
+    setattr(WebIdlDiffToolTest, test_name, test)
+
+
+_GenerateTestMethods()
 
 
 if __name__ == '__main__':
