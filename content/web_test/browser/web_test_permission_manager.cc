244bfa97904f8d7a90dfdd5469aa0745d0a81c4d
Chris Fredrickson <cfredric@chromium.org>
2026-04-30 08:07:09 -0700

    Run TLSAA WPTs using `chrome` product
    This WebTestPermissionManager code path was incorrect (it used incorrect
    ContentSettingsPatterns), but it's better to avoid this code path anyway
    since the real permissions machinery is implemented in the //chrome
    layer. This also reveals a test failure.
    
    (We can't remove the STORAGE_ACCESS code path, because some internal
    web_tests [which must be run with content_shell] rely on it.)
    
    Change-Id: I3ab77b7406d0dc38bfcdd2356d61b8472bd2c0e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7795693
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623200}

--- content/web_test/browser/web_test_permission_manager.cc
+++ content/web_test/browser/web_test_permission_manager.cc
@@ -473,49 +473,9 @@ void WebTestPermissionManager::OnPermissionChanged(
                   permission_result.status),
               base::BindOnce(std::move(permission_callback), /*success=*/true));
       break;
-    case blink::PermissionType::TOP_LEVEL_STORAGE_ACCESS: {
-      // We dual-write `TOP_LEVEL_STORAGE_ACCESS` and `STORAGE_ACCESS_GRANT` due
-      // to the former granting a superset of the latter. Accordingly, we wait
-      // until both permissions have been written, including the notification to
-      // the network service, to run the permission callback. This could happen
-      // in either order without issue, so a barrier callback is used to ensure
-      // whichever finishes last then runs the callback. The asynchronicity
-      // comes in the form of the updates to the network service.
-      auto barrier_callback = base::BarrierCallback<bool>(
-          /*num_callbacks=*/3,
-          base::BindOnce(
-              [](blink::test::mojom::PermissionAutomation::SetPermissionCallback
-                     permission_callback,
-                 const std::vector<bool>& successes) {
-                std::move(permission_callback)
-                    .Run(std::ranges::all_of(successes, std::identity()));
-              },
-              std::move(permission_callback)));
-      SetPermission(blink::PermissionType::STORAGE_ACCESS_GRANT,
-                    blink::mojom::PermissionStatus::GRANTED, permission.origin,
-                    permission.embedding_origin, barrier_callback);
-
-      auto* cookie_manager = browser_context_->GetDefaultStoragePartition()
-                                 ->GetCookieManagerForBrowserProcess();
-
-      cookie_manager->SetContentSettings(
-          ContentSettingsType::STORAGE_ACCESS,
-          GetContentSettings(
-              ContentSettingsPattern::FromURL(permission.origin),
-              ContentSettingsPattern::FromURL(permission.embedding_origin),
-              permission_result.status),
-          base::BindOnce(barrier_callback, true));
-
-      cookie_manager->SetContentSettings(
-          ContentSettingsType::TOP_LEVEL_STORAGE_ACCESS,
-          GetContentSettings(
-              ContentSettingsPattern::FromURL(permission.origin),
-              ContentSettingsPattern::FromURL(permission.embedding_origin),
-              permission_result.status),
-          base::BindOnce(barrier_callback, true));
-
-      break;
-    }
+    case blink::PermissionType::TOP_LEVEL_STORAGE_ACCESS:
+      // Tested via `chrome`, not `content_shell`.
+      NOTREACHED();
     default:
       std::move(permission_callback).Run(true);
       break;
