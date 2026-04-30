e6dc46419d4056f86ebd756572212bec23a91673
Troy Wang <troywang@google.com>
2026-04-30 10:49:45 -0700

    Introduce an allowlist of histograms to be logged to Westworld
    Changes that involve modifying the allowlist will require explicit
    approval from PWG.
    
    Bug: b:505049871
    
    Change-Id: Ie3a163ea18df0b360fbf66b8b43420ef96a8f82b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7785669
    Reviewed-by: Deepak Ravichandran <deepakr@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Troy Wang <troywang@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1623323}

---
+++ chrome/browser/android/metrics/westworld_histogram_allowlist.h
@@ -0,0 +1,26 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_METRICS_WESTWORLD_HISTOGRAM_ALLOWLIST_H_
+#define CHROME_BROWSER_ANDROID_METRICS_WESTWORLD_HISTOGRAM_ALLOWLIST_H_
+
+#include <tuple>
+
+#include "chrome/browser/android/metrics/uma_utils.h"
+
+namespace chrome::android {
+
+// An allowlist of UMA histograms that are registered for Westworld logging.
+//
+// To add a new histogram to Westworld:
+// 1. Add the histogram name string here in the list.
+// 2. Define and atom to be sent in `westworld_histogram_mapping.h`.
+inline constexpr const char* kWestworldHistogramsAllowlist[] = {
+    "Tabs.TabCount",
+    "Tabs.WindowCount",
+};
+
+}  // namespace chrome::android
+
+#endif  // CHROME_BROWSER_ANDROID_METRICS_WESTWORLD_HISTOGRAM_ALLOWLIST_H_
