783b4f2fc4043268ba0e8f269fd34f0aac72d1a2
David Yeung <dayeung@chromium.org>
2026-04-29 17:32:59 -0700

    [webium] Update webui to use TabStripUIController
    Replace the TabStripExperimentService with the more generalized
    TabStripUIController in the tab strip component. This change updates the
    interface used for triggering tab context menus.
    
    Bug: 470136275
    Change-Id: I3517ee2f9cf653de7d2d5036abf52b925e67374f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7801428
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1622840}

--- chrome/browser/resources/webui_browser/tab_strip/tab.ts
+++ chrome/browser/resources/webui_browser/tab_strip/tab.ts
@@ -7,8 +7,8 @@ import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
 import type {Tab as TabData} from '/tab_strip_api/tab_strip_api_data_model.mojom-webui.js';
-import {TabStripExperimentService} from '/tab_strip_api/tab_strip_experiment_api.mojom-webui.js';
-import type {TabStripExperimentServiceRemote} from '/tab_strip_api/tab_strip_experiment_api.mojom-webui.js';
+import {TabStripUIController} from '/tab_strip_api/tab_strip_ui_controller.mojom-webui.js';
+import type {TabStripUIControllerRemote} from '/tab_strip_api/tab_strip_ui_controller.mojom-webui.js';
 
 import {TabNetworkState} from '../tabs.mojom-webui.js';
 
@@ -59,12 +59,12 @@ export class TabElement extends CrLitElement {
   protected accessor dragInProgress = false;
   protected accessor active = false;
 
-  private readonly tabStripExperimentService_: TabStripExperimentServiceRemote;
+  private readonly tabStripUiController_: TabStripUIControllerRemote;
 
   constructor() {
     super();
 
-    this.tabStripExperimentService_ = TabStripExperimentService.getRemote();
+    this.tabStripUiController_ = TabStripUIController.getRemote();
   }
 
   override connectedCallback() {
@@ -77,7 +77,7 @@ export class TabElement extends CrLitElement {
     e.stopPropagation();
     e.preventDefault();
 
-    this.tabStripExperimentService_.showTabContextMenu(this.data.id, {
+    this.tabStripUiController_.showTabContextMenu(this.data.id, {
       x: e.screenX,
       y: e.screenY,
     });
