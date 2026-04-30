d27456472bed79dab485a2353e230231539b1612
Rakina Zata Amni <rakina@chromium.org>
2026-04-29 20:06:50 -0700

    Revert "[Glic] Add new native dialog to host Experimental Triggering Opt-In"
    This reverts commit 80c3aa276bd918a9b73f9c6488465ad3477ea5f9.
    
    Reason for revert: Causing failures at https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/140465/overview
    
    Original change's description:
    > [Glic] Add new native dialog to host Experimental Triggering Opt-In
    >
    > This is the first CL for creating a new tab-modal dialog to host the
    > Glic Experimental Triggering Opt-in. This CL creates the native dialog,
    > and simple show / close functionality.
    >
    > Simple interactave UI tests are added to validate these basic
    > operations.
    >
    > A Prototype CL which explore fleshing out the dialog completely, is
    > available at http://crrev.com/c/7796940
    >
    > Bug: b:505511789
    > Change-Id: I8ce6f939ba139ae00ef8d01ee253c35be699489a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797769
    > Reviewed-by: Dan H <harringtond@chromium.org>
    > Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1622745}
    
    Bug: b:505511789
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I29bbc6d90d254b96eefb0c3cd8a3fc3cb3b00479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7805824
    Owners-Override: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1622918}

--- chrome/browser/glic/public/glic_keyed_service.h
+++ chrome/browser/glic/public/glic_keyed_service.h
@@ -59,10 +59,6 @@ class GlicTabDataObserver;
 class GlicTabFaviconObserver;
 class GlicInstanceCoordinator;
 
-#if !BUILDFLAG(IS_ANDROID)
-class GlicExperimentalOptInController;
-#endif
-
 enum class GlicPrewarmingChecksResult;
 
 // The GlicKeyedService is created for each eligible (i.e. non-incognito,
@@ -135,9 +131,6 @@ class GlicKeyedService : public KeyedService, public base::SupportsUserData {
 
   GlicMetrics* metrics() { return metrics_.get(); }
   virtual GlicFreController& fre_controller();
-#if !BUILDFLAG(IS_ANDROID)
-  virtual GlicExperimentalOptInController& opt_in_controller();
-#endif
   virtual GlicInstanceCoordinator& instance_coordinator() const;
 
   // Return a `GlicActiveInstanceSharingManager` which tracks the sharing state
@@ -316,9 +309,6 @@ class GlicKeyedService : public KeyedService, public base::SupportsUserData {
   std::unique_ptr<GlicEnabling> enabling_;
   std::unique_ptr<GlicMetrics> metrics_;
   std::unique_ptr<GlicFreController> fre_controller_;
-#if !BUILDFLAG(IS_ANDROID)
-  std::unique_ptr<GlicExperimentalOptInController> opt_in_controller_;
-#endif
   // Is a GlicInstanceCoordinatorImpl.
   std::unique_ptr<GlicInstanceCoordinator> instance_coordinator_;
   std::unique_ptr<GlicSharingManager> sharing_manager_;
