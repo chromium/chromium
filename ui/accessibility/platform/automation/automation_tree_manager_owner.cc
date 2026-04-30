e574774aaf5206b33d063da4e48df327d2dd00ed
Addison Luh <aluh@chromium.org>
2026-04-30 13:34:46 -0700

    [M147] Reland "Prevent invalid tree state in AutomationTreeManagerOwner"
    Original change's description:
    > Reland "Prevent invalid tree state in AutomationTreeManagerOwner"
    >
    > This is a reland of commit d2ab7831eecf3a1c192ae64b05c2c6a797b556e9
    >
    > Fixed tree id in the test.
    >
    > Original change's description:
    > > Prevent invalid tree state in AutomationTreeManagerOwner
    > >
    > > If an AXTree fails to fully unserialize in the automation tree manager
    > > owner, drop and destroy the corrupted client wrapper and its nested
    > > static caches immediately. This prevents the tree manager from
    > > referencing the corrupted, dangling pointers subsequently.
    > >
    > > Bug: b:502978647
    > > Test: out/Default/accessibility_unittests --gtest_filter=AutomationTreeManagerOwnerTest.UnserializeFailureClearsCachedTree
    > > Change-Id: Idbcbdab0e7b7a46d8d094a38f3ba4a32cf301ce7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7793837
    > > Reviewed-by: David Tseng <dtseng@chromium.org>
    > > Auto-Submit: Addison Luh <aluh@chromium.org>
    > > Commit-Queue: David Tseng <dtseng@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1621369}
    >
    > Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    > Bug: b:502978647
    > Change-Id: I720b30a6baa76570ea54b072e1e9bc0ad6c50f0a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802331
    > Auto-Submit: Addison Luh <aluh@chromium.org>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Commit-Queue: Addison Luh <aluh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1622148}
    
    (cherry picked from commit a72c80884d88cb68fba22a552ec795f3b552f1a1)
    
    Bug: 507877571,b:502978647
    Change-Id: I720b30a6baa76570ea54b072e1e9bc0ad6c50f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7807367
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7727@{#4083}
    Cr-Branched-From: ce01102937348db7b88c8a4257ee4b3ac702eb1a-refs/heads/main@{#1596535}

--- ui/accessibility/platform/automation/automation_tree_manager_owner.cc
+++ ui/accessibility/platform/automation/automation_tree_manager_owner.cc
@@ -1075,6 +1075,7 @@ void AutomationTreeManagerOwner::DispatchAccessibilityEvents(
                                            mouse_location)) {
     DLOG(ERROR) << tree_wrapper->ax_tree()->error();
     GetAutomationV8Bindings()->SendTreeSerializationError(tree_id);
+    DestroyAccessibilityTree(tree_id);
     return;
   }
 
