24ded9368c63111247f458f3738997708e3cc7ca
Alex Kallam <aakallam@chromium.org>
2026-05-01 15:11:49 -0700

    Roll toml_parser: 1.1.0+spec-1.1.0 => 1.1.2+spec-1.1.0 in //third_party/rust.
    This CL has been created semi-automatically. The expected review process
    and other details can be found at //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * toml_parser: 1.1.0+spec-1.1.0 => 1.1.2+spec-1.1.0;
      https://docs.rs/crate/toml_parser/1.1.2+spec-1.1.0
    
    Bug: None
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Change-Id: Ic8409e802223d22e02228389f4db581689470d47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7802117
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Alex Kallam <aakallam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624107}

--- third_party/rust/chromium_crates_io/vendor/toml_parser-v1/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/toml_parser-v1/src/lib.rs
@@ -6,6 +6,6 @@
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `toml_parser-1.1.0+spec-1.1.0` crate.
+// `toml_parser-1.1.2+spec-1.1.0` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
