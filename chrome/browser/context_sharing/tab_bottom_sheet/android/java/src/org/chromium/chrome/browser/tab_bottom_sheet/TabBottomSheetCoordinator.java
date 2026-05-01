f4ad5cdb758891cedeef580ceaafdae24708ffb2
Fiaz Muhammad <mfiaz@google.com>
2026-05-01 12:22:14 -0700

    [TabBottomSheet] Remove max offset bounding
    This may cause bugs when a layout pass has not occurred successfully,
    leading to bad height setting.
    
    Bug: 508633672, 507466979
    Change-Id: I7cca7fbe780629d0b8b27a6845dc7067bf3feffb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7810334
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Hitarth Kothari <hitarthkothari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1623987}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetCoordinator.java
@@ -445,16 +445,7 @@ public class TabBottomSheetCoordinator {
     private void setToFixedHeightOrFallback() {
         if (mWindowAndroid.getWindow() == null) return;
         @Px int fixedHeight = (int) (getVisibleViewportHeight() * getDefaultHeightRatio());
-
-        // The bottom sheet controller max offset calculations may not be correct without the
-        // sheet content being set to a fixed height first.
         mMediator.setToFixedHeight(fixedHeight);
-
-        int newFixedHeight = Math.min(fixedHeight, mBottomSheetController.getMaxOffset());
-        if (newFixedHeight != fixedHeight) {
-            fixedHeight = newFixedHeight;
-            mMediator.setToFixedHeight(fixedHeight);
-        }
 
         // In the case the bottom sheet is unable to set to our desired fixed height, fallback to
         // use of flexible heights.
