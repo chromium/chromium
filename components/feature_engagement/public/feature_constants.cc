46393f9bd9575f3466c17a8abe1b3db6e969fdf0
Alison Gale <agale@chromium.org>
2026-05-04 09:12:18 -0700

    [SxS] Re-enabled split view tutorial after crash fix
    The field trial can be removed now that the finch turndown of the IPH is
    launched and since a fix for the crash is at ToT the IPH can be enabled
    by default again.
    
    Bug: 505491832
    Change-Id: Ie1a0952583e12e538513368ab577527445d04cc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797615
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Auto-Submit: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624732}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -228,7 +228,7 @@ BASE_FEATURE(kIPHSideBySidePinnableFeature,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHSideBySideTabSwitchFeature,
              "IPH_SideBySideTabSwitchFeature",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHVerticalTabsExpandOnHoverFeature,
              "IPH_VerticalTabsExpandOnHoverFeature",
              base::FEATURE_DISABLED_BY_DEFAULT);
