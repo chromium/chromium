4ada492892fd295a55bcef0512b911c90e83893e
Gayane Petrosyan <gayane@google.com>
2026-05-04 07:32:42 -0700

    [iOS][gnp] Add backend promo service and factory
    Create file structure and empty service and factory for
    BackendPromoService.
    
    Bug: 503366680
    Change-Id: Iaf376f952641bf9251ebb00f25615359ab0c9dc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7799637
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1624650}

---
+++ ios/chrome/browser/backend_promo_service/model/backend_promo_service.h
@@ -0,0 +1,30 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_BACKEND_PROMO_SERVICE_MODEL_BACKEND_PROMO_SERVICE_H_
+#define IOS_CHROME_BROWSER_BACKEND_PROMO_SERVICE_MODEL_BACKEND_PROMO_SERVICE_H_
+
+#import <memory>
+
+#import "components/keyed_service/core/keyed_service.h"
+
+// BackendPromoService is responsible for backend promo features.
+class BackendPromoService : public KeyedService {
+ public:
+  BackendPromoService() = default;
+
+  BackendPromoService(const BackendPromoService&) = delete;
+  BackendPromoService& operator=(const BackendPromoService&) = delete;
+
+  ~BackendPromoService() override = default;
+};
+
+namespace ios::provider {
+
+// Creates a new instance of BackendPromoService.
+std::unique_ptr<BackendPromoService> CreateBackendPromoService();
+
+}  // namespace ios::provider
+
+#endif  // IOS_CHROME_BROWSER_BACKEND_PROMO_SERVICE_MODEL_BACKEND_PROMO_SERVICE_H_
---
+++ ios/chrome/browser/providers/backend_promo_service/chromium_backend_promo_service.mm
@@ -0,0 +1,19 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/backend_promo_service/model/backend_promo_service.h"
+
+class ChromiumBackendPromoService final : public BackendPromoService {
+ public:
+  ChromiumBackendPromoService() = default;
+  ~ChromiumBackendPromoService() final = default;
+};
+
+namespace ios::provider {
+
+std::unique_ptr<BackendPromoService> CreateBackendPromoService() {
+  return std::make_unique<ChromiumBackendPromoService>();
+}
+
+}  // namespace ios::provider
