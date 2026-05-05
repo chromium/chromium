e15a3e2019be1da3514a9ebc97026754a3e278cf
Devlin Cronin <rdevlin.cronin@chromium.org>
2026-05-05 10:43:29 -0700

    [Extensions] Clean up / modernize api_test/tabs/basics/pinned
    api_test/tabs/basics/pinned/pinned.js had a huge amount of callback
    nesting and heavy use of callbackPass() and listenForever(), which
    made the test difficult to read, understand, debug, and extend.
    
    Simplify it to use async patterns where possible.
    
    Bug: None
    Change-Id: I5e3cd10d87bbac1a6abd733aa77b6d53b1e9dd33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7814328
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625566}

--- chrome/test/data/extensions/api_test/tabs/basics/pinned/pinned.js
+++ chrome/test/data/extensions/api_test/tabs/basics/pinned/pinned.js
@@ -9,56 +9,64 @@ const loadScript = chrome.test.loadScript(SCRIPT_URL);
 
 loadScript.then(async function() {
   chrome.test.runTests([
-    function setupWindow() {
-      createWindow(
-          ['about:blank', 'chrome://newtab/', pageUrl('a')], {},
-          pass(function(winId, tabIds) {
-            firstWindowId = winId;
-          }));
+    async function setupWindow() {
+      await (new Promise(resolve => {
+        createWindow(
+            ['about:blank', 'chrome://newtab/', pageUrl('a')], {},
+            function(winId, tabIds) {
+              firstWindowId = winId;
+              resolve();
+            });
+      }));
+      chrome.test.succeed();
     },
 
-    function createPinned() {
+    async function createPinned() {
       const winOptions = {windowId: firstWindowId, pinned: true};
-      const onUpdatedCompleted = chrome.test.listenForever(
-          chrome.tabs.onUpdated,
-          function(tabId, changeInfo, tab) {
-            if ('pinned' in changeInfo) {
-              assertEq(false, changeInfo.pinned);
-              assertEq(false, tab.pinned);
-              onUpdatedCompleted();
-            }
-          },
-      );
-      chrome.tabs.create(winOptions, pass(function(tab) {
-                           assertEq(true, tab.pinned);
-                           chrome.tabs.update(
-                               tab.id, {pinned: false}, pass(function() {
-                                 // Leave a clean slate for the next test.
-                                 chrome.tabs.remove(tab.id);
-                               }));
-                         }));
+      const updatedPromise = new Promise(resolve => {
+        chrome.tabs.onUpdated.addListener(
+            function listener(tabId, changeInfo, tab) {
+              if ('pinned' in changeInfo) {
+                assertEq(false, changeInfo.pinned);
+                assertEq(false, tab.pinned);
+                chrome.tabs.onUpdated.removeListener(listener);
+                resolve();
+              }
+            });
+      });
+
+      // Create a new pinned tab, then unpin it, and ensure we get the proper
+      // event.
+      const tab = await chrome.tabs.create(winOptions);
+      assertEq(true, tab.pinned);
+      await chrome.tabs.update(tab.id, {pinned: false});
+      await updatedPromise;
+
+      // Leave a clean slate for the next test.
+      await chrome.tabs.remove(tab.id);
+
+      chrome.test.succeed();
     },
 
-    function updatePinned() {
+    async function updatePinned() {
       // A helper function that (un)pins a tab and verifies that both the
       // callback and the chrome.tabs.onUpdated event listeners are called.
-      const pinTab = function(id, pinnedState, callback) {
-        const onUpdatedCompleted = chrome.test.listenForever(
-            chrome.tabs.onUpdated,
-            function(tabId, changeInfo, tab) {
-              if ('pinned' in changeInfo) {
-                assertEq(tabId, id);
-                assertEq(pinnedState, tab.pinned);
-                onUpdatedCompleted();
-                if (callback) {
-                  callback(tab);
+      const pinTab = async function(id, pinnedState) {
+        const updatedPromise = new Promise(async resolve => {
+          chrome.tabs.onUpdated.addListener(
+              function listener(tabId, changeInfo, tab) {
+                if ('pinned' in changeInfo) {
+                  assertEq(tabId, id);
+                  assertEq(pinnedState, tab.pinned);
+                  chrome.tabs.onUpdated.removeListener(listener);
+                  resolve(tab);
                 }
-              }
-            },
-        );
-        chrome.tabs.update(id, {pinned: pinnedState}, pass(function(tab) {
-                             assertEq(pinnedState, tab.pinned);
-                           }));
+              });
+        });
+        const tab = await chrome.tabs.update(id, {pinned: pinnedState});
+        assertEq(pinnedState, tab.pinned);
+
+        return await updatedPromise;
       };
 
       // We pin and unpin these tabs because the TabStripModelObserver used to
@@ -69,25 +77,22 @@ loadScript.then(async function() {
       //   2.  Pin 3rd tab (moves to 2nd tab, pinning)
       //   3.  Unpin 1st tab (moves to 2nd tab, unpinning)
       //   4.  Unpin (new) 1st tab (does not move. unpinning)
-      chrome.tabs.query({windowId: firstWindowId}, pass(function(tabs) {
-                          assertEq(tabs.length, 3);
-                          for (let i = 0; i < tabs.length; i++) {
-                            assertEq(false, tabs[i].pinned);
-                          }
+      const tabs = await chrome.tabs.query({windowId: firstWindowId});
+      assertEq(tabs.length, 3);
+      for (let i = 0; i < tabs.length; i++) {
+        assertEq(false, tabs[i].pinned);
+      }
 
-                          pinTab(tabs[0].id, true, function(tab) {
-                            assertEq(tabs[0].index, tab.index);
-                            pinTab(tabs[2].id, true, function(tab) {
-                              assertEq(1, tab.index);
-                              pinTab(tabs[0].id, false, function(tab) {
-                                assertEq(1, tab.index);
-                                pinTab(tabs[2].id, false, function(tab) {
-                                  assertEq(0, tab.index);
-                                });
-                              });
-                            });
-                          });
-                        }));
+      let tab = await pinTab(tabs[0].id, true);
+      assertEq(tabs[0].index, tab.index);
+      tab = await pinTab(tabs[2].id, true);
+      assertEq(1, tab.index);
+      tab = await pinTab(tabs[0].id, false);
+      assertEq(1, tab.index);
+      tab = await pinTab(tabs[2].id, false);
+      assertEq(0, tab.index);
+
+      chrome.test.succeed();
     },
   ]);
 });
