efdc69122033a68e052651fae9e80b71864cb8b6
Theodore Olsauskas-Warren <sauski@google.com>
2026-05-04 17:55:35 -0700

    Reland "Reland "[Glic] Add new native dialog to host Experimental Triggering Opt-In""
    This is a reland of commit 3566581ad32cc41455d4862847159b2123d2e1b4
    
    This re-land changes the unique_ptr for the dialog_view to be retained.
    Existing examples that release their unique_ptr have a _different_ close
    pathway. Our close pathway aligns with examples that retain their
    unique_ptr.
    
    Original change's description:
    > Reland "[Glic] Add new native dialog to host Experimental Triggering Opt-In"
    >
    > This is a reland of commit 80c3aa276bd918a9b73f9c6488465ad3477ea5f9
    >
    > This CL tweaks the original approach to use the tab_dialog_manager,
    > CLIENT_OWNS_WIDGET & MakeCloseSynchronous to update widget ownership
    > semantics to the current best practice.
    >
    > Original change's description:
    > > [Glic] Add new native dialog to host Experimental Triggering Opt-In
    > >
    > > This is the first CL for creating a new tab-modal dialog to host the
    > > Glic Experimental Triggering Opt-in. This CL creates the native dialog,
    > > and simple show / close functionality.
    > >
    > > Simple interactave UI tests are added to validate these basic
    > > operations.
    > >
    > > A Prototype CL which explore fleshing out the dialog completely, is
    > > available at http://crrev.com/c/7796940
    > >
    > > Bug: b:505511789
    > > Change-Id: I8ce6f939ba139ae00ef8d01ee253c35be699489a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7797769
    > > Reviewed-by: Dan H <harringtond@chromium.org>
    > > Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1622745}
    >
    > Bug: b:505511789
    > Change-Id: I97c58b883e18373f09d467097dff17279c6aa77d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7806194
    > Reviewed-by: Dan H <harringtond@chromium.org>
    > Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1623909}
    
    Bug: b:505511789
    Change-Id: Ie1d229ef4234c03147cbe223a3891a57c764cbad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7812146
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1625066}

---
+++ chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_controller.cc
@@ -0,0 +1,65 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_controller.h"
+
+#include <memory>
+
+#include "chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_dialog_view.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/tabs/public/tab_dialog_manager.h"
+#include "chrome/browser/ui/tabs/public/tab_features.h"
+#include "components/tabs/public/tab_interface.h"
+#include "content/public/browser/web_contents.h"
+#include "ui/views/widget/widget.h"
+
+namespace glic {
+
+GlicExperimentalOptInController::GlicExperimentalOptInController(
+    Profile* profile)
+    : profile_(profile) {}
+
+GlicExperimentalOptInController::~GlicExperimentalOptInController() = default;
+
+views::Widget* GlicExperimentalOptInController::ShowDialog(
+    content::WebContents* web_contents) {
+  if (dialog_widget_) {
+    dialog_widget_->Show();
+    return dialog_widget_.get();
+  }
+
+  tabs::TabInterface* tab_interface =
+      tabs::TabInterface::MaybeGetFromContents(web_contents);
+  if (!tab_interface) {
+    return nullptr;
+  }
+
+  dialog_view_ = std::make_unique<GlicExperimentalOptInDialogView>(profile_);
+  dialog_view_->SetOwnershipOfNewWidget(
+      views::Widget::InitParams::CLIENT_OWNS_WIDGET);
+
+  dialog_widget_ = tab_interface->GetTabFeatures()
+                       ->tab_dialog_manager()
+                       ->CreateAndShowDialog(
+                           dialog_view_.get(),
+                           std::make_unique<tabs::TabDialogManager::Params>());
+
+  dialog_widget_->MakeCloseSynchronous(
+      base::BindOnce(&GlicExperimentalOptInController::CloseWidget,
+                     weak_ptr_factory_.GetWeakPtr()));
+
+  return dialog_widget_.get();
+}
+
+void GlicExperimentalOptInController::CloseDialog() {
+  CloseWidget(views::Widget::ClosedReason::kUnspecified);
+}
+
+void GlicExperimentalOptInController::CloseWidget(
+    views::Widget::ClosedReason reason) {
+  dialog_widget_.reset();
+  dialog_view_.reset();
+}
+
+}  // namespace glic
