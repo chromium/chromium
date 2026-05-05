e3e1a04f99e97b8c483c8472a58a34b6d9251b02
François Degros <fdegros@chromium.org>
2026-05-04 23:58:28 -0700

    [ReadAnything] Disable ImmersiveReadAnythingMochaTest.ToolbarSettingsMenu on Windows
    This test is consistently failing on Windows (observed on Win10 Tests
    x64 builder). Disabling it to green up the builder while the issue is
    investigated.
    
    Bug: 509759561
    Change-Id: I2e42c37f07a615903a4cb5163f1b6462437b97ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7815164
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Owners-Override: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625241}

--- chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
+++ chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
@@ -342,7 +342,14 @@ IN_PROC_BROWSER_TEST_F(ImmersiveReadAnythingMochaTest, SettingsMenu) {
                    "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(ImmersiveReadAnythingMochaTest, ToolbarSettingsMenu) {
+// TODO(crbug.com/509759561): Re-enable after fixing flakiness.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_ToolbarSettingsMenu DISABLED_ToolbarSettingsMenu
+#else
+#define MAYBE_ToolbarSettingsMenu ToolbarSettingsMenu
+#endif
+IN_PROC_BROWSER_TEST_F(ImmersiveReadAnythingMochaTest,
+                       MAYBE_ToolbarSettingsMenu) {
   RunSidePanelTest("side_panel/read_anything/toolbar_settings_menu_test.js",
                    "mocha.run()");
 }
