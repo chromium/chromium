9ff01f767cd85a7bd9272e5563bb1b6562607cd0
Dan Murphy <dmurph@chromium.org>
2026-05-05 12:53:14 -0700

    Reland "[PWA] Robust WebContents manifest waiting in tests"
    This is a reland of commit 4b3f90b9fb68e1c1a1187595879e7e28a5194f2a
    
    The second reland was reverted due to test failures with unclear causes.
    Now, the patch is being split up to avoid the huge revert and iteration
    on this new waiting utility.
    
    Reason for revert: https://crbug.com/509684641
    
    Breaks IsolatedWebAppRetryTest.RetryTriggeredWhenAllTasksDone and
    WebAppUpdateDialogBrowserTests.Accept on Mac12 Tests.
    
    Original change's description:
    > [PWA] Robust WebContents manifest waiting in tests
    >
    > Replaced flaky ad-hoc heuristics for waiting for page loads and
    > manifests in browser tests with a robust WebContentsWaiter utility.
    >
    > This utility uses a new event created on the WebAppTabHelper designed
    > to signify if all web app system activity is scheduled for a given
    > WebContents page.
    >
    > Test: FetchManifestAndUpdateCommandMigrationTest.*
    > Test: FetchManifestAndUpdateCommandTest.*
    > Test: NavigationCapturingBrowserNavigatorBrowserTest.*
    > Test: WebAppBlockedMigrationInfoBarDelegateBrowserTest.*
    > Test: SingleClientWebAppsSyncGeneratedIconFixSyncTest.*
    > Test: WebAppIntegration.WAI_InstallMenuStandalone_SwitchProfileClientsClient2_InstallLocallyStandalone_LaunchFromPlatformShortcutStandalone/kSyncTheFeature
    > Change-Id: Iaed859d4aa8e2a744bcc18ebc3a99592a69f448b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7782642
    > Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1624185}
    
    Change-Id: Id41e2eed430aaa1085537e65938d4e454753a7cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7816619
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625661}

---
+++ chrome/browser/web_applications/test/web_app_page_waiter.cc
@@ -0,0 +1,303 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/web_applications/test/web_app_page_waiter.h"
+
+#include "base/functional/bind.h"
+#include "base/functional/callback_helpers.h"
+#include "base/logging.h"
+#include "base/notreached.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/web_applications/web_app_command_manager.h"
+#include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/browser/web_applications/web_app_tab_helper.h"
+#include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/page.h"
+#include "content/public/browser/page_manifest_manager.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/test/browser_test_utils.h"
+#include "third_party/blink/public/mojom/manifest/manifest.mojom.h"
+
+namespace web_app::test {
+
+WebAppPageWaiter::WebAppPageWaiter(content::WebContents* web_contents)
+    : content::WebContentsObserver(web_contents) {
+  CHECK(web_contents);
+}
+
+WebAppPageWaiter::~WebAppPageWaiter() = default;
+
+WebAppPageWaiter& WebAppPageWaiter::ExpectUrlIf(
+    base::RepeatingCallback<bool(const GURL&)> url_matcher) {
+  url_matcher_ = std::move(url_matcher);
+  return *this;
+}
+
+WebAppPageWaiter& WebAppPageWaiter::ExpectUrl(const GURL& url) {
+  return ExpectUrlIf(
+      base::BindRepeating([](const GURL& expected,
+                             const GURL& actual) { return expected == actual; },
+                          url));
+}
+
+WebAppPageWaiter& WebAppPageWaiter::ExpectManifest(
+    std::optional<webapps::ManifestId> manifest_id) {
+  CHECK_EQ(expectation_, Expectation::kUnset);
+  expectation_ = Expectation::kManifest;
+  expected_manifest_id_ = std::move(manifest_id);
+  return *this;
+}
+
+WebAppPageWaiter& WebAppPageWaiter::ExpectNoManifest() {
+  CHECK_EQ(expectation_, Expectation::kUnset);
+  expectation_ = Expectation::kNoManifest;
+  return *this;
+}
+
+WebAppPageWaiter& WebAppPageWaiter::ManifestOrLoadedNoManifest() {
+  CHECK_EQ(expectation_, Expectation::kUnset);
+  expectation_ = Expectation::kManifestOrLoadedNoManifest;
+  return *this;
+}
+
+testing::AssertionResult WebAppPageWaiter::WaitAndFlushCommands() {
+  CHECK_NE(expectation_, Expectation::kUnset);
+
+  if (destroyed_) {
+    return testing::AssertionFailure()
+           << "WebContents destroyed before Wait() called";
+  }
+
+  // Wait for the expected URL to load first (if set), otherwise just wait for
+  // current load to stop.
+  if (url_matcher_) {
+    while (!UrlMatches() || web_contents()->IsLoading()) {
+      // Nestable tasks ensure other testing base::RunLoops don't deadlock the
+      // test.
+      base::RunLoop url_loop(base::RunLoop::Type::kNestableTasksAllowed);
+      url_quit_closure_ = url_loop.QuitClosure();
+      url_loop.Run();
+      url_quit_closure_.Reset();
+      if (destroyed_) {
+        return testing::AssertionFailure()
+               << "WebContents destroyed while waiting for URL";
+      }
+    }
+  } else {
+    content::WaitForLoadStop(web_contents());
+    if (destroyed_) {
+      return testing::AssertionFailure()
+             << "WebContents destroyed during WaitForLoadStop";
+    }
+  }
+
+  document_loaded_ =
+      web_contents()->IsDocumentOnLoadCompletedInPrimaryMainFrame();
+
+  WebAppTabHelper* tab_helper =
+      WebAppTabHelper::FromWebContents(web_contents());
+
+  if (!tab_helper) {
+    switch (expectation_) {
+      case Expectation::kUnset:
+        break;
+      case Expectation::kManifest:
+        return testing::AssertionFailure()
+               << "No WebAppTabHelper attached to WebContents, cannot wait for "
+                  "manifest.";
+      case Expectation::kNoManifest:
+        break;
+      case Expectation::kManifestOrLoadedNoManifest:
+        break;
+    }
+  }
+
+  // It's possible the manifest was already processed while we were waiting
+  // for the load to stop. If so, we can note that down right away!
+  if (tab_helper && tab_helper->manifest_processed_for_current_page()) {
+    processed_manifest_id_ =
+        tab_helper->last_processed_manifest_id_for_current_page();
+    if (!expected_manifest_id_.has_value() ||
+        processed_manifest_id_ == expected_manifest_id_) {
+      matching_manifest_processed_ = true;
+    }
+  }
+
+  if (tab_helper) {
+    manifest_subscription_ =
+        tab_helper->AddOnManifestProcessedCallbackForTesting(
+            base::BindRepeating(&WebAppPageWaiter::OnManifestProcessed,
+                                base::Unretained(this)));
+  }
+
+  MaybeQuit();
+
+  run_loop_.Run();
+
+  if (destroyed_) {
+    return testing::AssertionFailure() << "WebContents destroyed while waiting";
+  }
+
+  bool has_manifest_url =
+      web_contents()->GetPrimaryPage().GetManifestUrl().has_value();
+
+  switch (expectation_) {
+    case Expectation::kUnset:
+      NOTREACHED();
+    case Expectation::kManifest:
+      if (!matching_manifest_processed_) {
+        if (!has_manifest_url) {
+          return testing::AssertionFailure()
+                 << "Expected manifest but no manifest URL was found on the "
+                    "page before loading completed or was stopped.";
+        }
+        if (processed_manifest_id_.has_value()) {
+          return testing::AssertionFailure()
+                 << "Expected manifest ID: " << expected_manifest_id_->spec()
+                 << " but got: " << processed_manifest_id_->spec();
+        }
+        return testing::AssertionFailure()
+               << "Expected manifest but none was processed";
+      }
+      CHECK(processed_manifest_id_)
+          << "Cannot have a matching manifest without a processed manifest id.";
+
+      break;
+    case Expectation::kNoManifest:
+      if (has_manifest_url) {
+        return testing::AssertionFailure()
+               << "Expected no manifest but found manifest URL: "
+               << web_contents()->GetPrimaryPage().GetManifestUrl()->spec()
+               << (processed_manifest_id_.has_value()
+                       ? "With id " + processed_manifest_id_->spec()
+                       : "");
+      }
+      break;
+    case Expectation::kManifestOrLoadedNoManifest:
+      if (!document_loaded_ && !load_failed_ && !matching_manifest_processed_) {
+        return testing::AssertionFailure()
+               << "Timed out waiting for page load or manifest processing.";
+      }
+      break;
+  }
+
+  // Wait for all commands to complete in the command manager to ensure any
+  // spawned tasks are done.
+  Profile* profile =
+      Profile::FromBrowserContext(web_contents()->GetBrowserContext());
+  WebAppProvider* provider = WebAppProvider::GetForTest(profile);
+  if (provider) {
+    provider->command_manager().AwaitAllCommandsCompleteForTesting();
+  }
+
+  return testing::AssertionSuccess();
+}
+
+void WebAppPageWaiter::DidStopLoading() {
+  if (url_quit_closure_) {
+    url_quit_closure_.Run();
+    return;
+  }
+  MaybeQuit();
+}
+
+void WebAppPageWaiter::DocumentOnLoadCompletedInPrimaryMainFrame() {
+  MaybeQuit();
+}
+
+void WebAppPageWaiter::DidFailLoad(content::RenderFrameHost* render_frame_host,
+                                   const GURL& validated_url,
+                                   int error_code) {
+  if (render_frame_host && render_frame_host->IsInPrimaryMainFrame()) {
+    load_failed_ = true;
+    MaybeQuit();
+  }
+}
+
+void WebAppPageWaiter::WebContentsDestroyed() {
+  destroyed_ = true;
+  if (url_quit_closure_) {
+    url_quit_closure_.Run();
+  }
+  Observe(nullptr);
+  run_loop_.Quit();
+}
+
+void WebAppPageWaiter::OnManifestProcessed(
+    const webapps::ManifestId& manifest_id) {
+  processed_manifest_id_ = manifest_id;
+  if (!expected_manifest_id_.has_value() ||
+      processed_manifest_id_ == expected_manifest_id_) {
+    matching_manifest_processed_ = true;
+  }
+  // MaybeQuit & the waiter should always quit if we are waiting for a manifest
+  // and it matches - thus we don't need to handle the case where
+  // matching_manifest_processed_ was set to 'true' BEFORE this method call.
+  MaybeQuit();
+}
+
+void WebAppPageWaiter::MaybeQuit() {
+  if (destroyed_) {
+    run_loop_.Quit();
+    return;
+  }
+
+  document_loaded_ =
+      web_contents()->IsDocumentOnLoadCompletedInPrimaryMainFrame();
+
+  // Note: This method is NOT responsible for making test assertions, it is
+  // responsible ending waiting, and `WaitAndFlushCommands` is responsible
+  // for checking test assertions.
+  bool has_manifest_url =
+      web_contents()->GetPrimaryPage().GetManifestUrl().has_value();
+  bool has_load_completed_enough_for_manifest_url_check =
+      document_loaded_ || load_failed_;
+  switch (expectation_) {
+    case Expectation::kManifestOrLoadedNoManifest:
+      if (matching_manifest_processed_) {
+        run_loop_.Quit();
+        return;
+      }
+      if (has_load_completed_enough_for_manifest_url_check) {
+        bool webapps_enabled_for_web_contents = !!manifest_subscription_;
+        if (webapps_enabled_for_web_contents && has_manifest_url &&
+            !matching_manifest_processed_) {
+          // Keep waiting for manifest.
+          return;
+        }
+        run_loop_.Quit();
+      }
+      return;
+    case Expectation::kManifest:
+      // If the manifest url doesn't exist and load has completed, assume that
+      // there will not be a manifest set on the page, and fail early so we
+      // don't time out.
+      if (matching_manifest_processed_ ||
+          (has_load_completed_enough_for_manifest_url_check &&
+           !has_manifest_url)) {
+        run_loop_.Quit();
+      }
+      return;
+    case Expectation::kNoManifest:
+      if (has_load_completed_enough_for_manifest_url_check ||
+          has_manifest_url) {
+        run_loop_.Quit();
+      }
+      break;
+    case Expectation::kUnset:
+      NOTREACHED();
+  }
+}
+
+bool WebAppPageWaiter::UrlMatches() const {
+  if (!url_matcher_) {
+    return true;
+  }
+  content::NavigationController& controller = web_contents()->GetController();
+  content::NavigationEntry* entry = controller.GetVisibleEntry();
+  return entry && url_matcher_.Run(entry->GetVirtualURL());
+}
+
+}  // namespace web_app::test
