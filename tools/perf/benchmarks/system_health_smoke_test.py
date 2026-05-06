fd832fbf2dd323cb094135e565512effb1e21ee4
François Degros <fdegros@chromium.org>
2026-05-05 20:17:11 -0700

    [Telemetry] Fix syntax bug in _DISABLED_TESTS set
    Add missing commas after three disabled tests in
    system_health_smoke_test.py. The lack of commas caused implicit string
    concatenation (e.g. load_accessibility:shopping:amazon:2018 was
    concatenated with browse:media:pinterest:2018), rendering the
    disablements inactive and causing tests to run and fail/flake.
    
    Bug: 509294498
    Change-Id: I72084721717f9abecca370084981904bde19c491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7817617
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Owners-Override: François Degros <fdegros@chromium.org>
    Reviewed-by: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1625951}

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -80,15 +80,15 @@ _DISABLED_TESTS = frozenset({
     'system_health.memory_desktop/load:media:youtubelivingroom:2020',
 
     # crbug.com/422824099
-    'system_health.memory_desktop/browse:news:nytimes:2020'
-    'system_health.memory_desktop/browse:tools:gmail-labelclick:2020'
+    'system_health.memory_desktop/browse:news:nytimes:2020',
+    'system_health.memory_desktop/browse:tools:gmail-labelclick:2020',
     'system_health.memory_desktop/load:games:lazors',
     'system_health.memory_desktop/load:media:youtube:2018',
     'system_health.memory_desktop/load:search:ebay:2018',
     'system_health.memory_desktop/load:tools:gmail:2019',
 
     # crbug.com/509294498, crashing on Linux dbg
-    'system_health.memory_desktop/load_accessibility:shopping:amazon:2018'
+    'system_health.memory_desktop/load_accessibility:shopping:amazon:2018',
 
     # The following tests are disabled because they are disabled on the perf
     # waterfall (using tools/perf/expectations.config) on one platform or
