ef8cb5434e047b6c985cdb4bf9da2b05e01e6ace
Tzarial <zork@chromium.org>
2026-05-06 16:49:44 -0700

    [viz] Add fuzz tests for RendererSettings and DebugRendererSettings mojom traits.
    This change introduces fuzz tests using the FuzzTest framework to validate the serialization and deserialization of `viz::RendererSettings` and `viz::DebugRendererSettings`. Custom domains are defined to generate arbitrary instances of these structs, including handling platform-specific fields. The tests ensure that serializing and then deserializing the structs results in the original values.
    
    Bug: 507773114
    Change-Id: If0884dded150a39b63322edb88a1388d24c78460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7812279
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Tzarial <zork@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626563}

--- services/viz/privileged/mojom/mojom_traits_unittest.cc
+++ services/viz/privileged/mojom/mojom_traits_unittest.cc
@@ -4,12 +4,156 @@
 
 #include <utility>
 
+#include "build/build_config.h"
+#include "components/viz/common/buildflags.h"
 #include "components/viz/common/display/renderer_settings.h"
 #include "services/viz/privileged/mojom/compositing/renderer_settings.mojom.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/fuzztest/src/fuzztest/fuzztest.h"
 #include "ui/gfx/mojom/color_space_mojom_traits.h"
 
 namespace viz {
+
+namespace {
+
+#if BUILDFLAG(IS_OZONE)
+auto AnyOverlayStrategy() {
+  return fuzztest::ElementOf({OverlayStrategy::kFullscreen,
+                              OverlayStrategy::kSingleOnTop,
+                              OverlayStrategy::kUnderlay
+#if BUILDFLAG(ENABLE_CAST_OVERLAY_STRATEGY)
+                              ,
+                              OverlayStrategy::kUnderlayCast
+#endif
+  });
+}
+#endif
+
+auto AnyOcclusionCullerSettings() {
+  return fuzztest::Map(
+      [](int maximum_occluder_complexity, int quad_split_limit,
+         int minimum_fragments_reduced, int occluder_minium_visible_quad_size,
+         bool generate_complex_occluder_for_rounded_corners,
+         int minumum_quad_size_with_rounded_corners) {
+        RendererSettings::OcclusionCullerSettings settings;
+        settings.maximum_occluder_complexity = maximum_occluder_complexity;
+        settings.quad_split_limit = quad_split_limit;
+        settings.minimum_fragments_reduced = minimum_fragments_reduced;
+        settings.occluder_minium_visible_quad_size =
+            occluder_minium_visible_quad_size;
+        settings.generate_complex_occluder_for_rounded_corners =
+            generate_complex_occluder_for_rounded_corners;
+        settings.minumum_quad_size_with_rounded_corners =
+            minumum_quad_size_with_rounded_corners;
+        return settings;
+      },
+      fuzztest::Arbitrary<int>(), fuzztest::Arbitrary<int>(),
+      fuzztest::Arbitrary<int>(), fuzztest::Arbitrary<int>(),
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<int>());
+}
+
+auto AnyRendererSettings() {
+  return fuzztest::Map(
+      [](bool allow_antialiasing, bool force_antialiasing,
+         bool force_blending_with_shaders, bool partial_swap_enabled,
+         bool should_clear_root_render_pass,
+         bool release_overlay_resources_after_gpu_query,
+         bool dont_round_texture_sizes_for_pixel_tests, int highp_threshold_min,
+         bool auto_resize_output_surface, bool requires_alpha_channel,
+         bool disable_render_pass_bypassing,
+         bool force_non_scanout_backing_for_pixel_tests,
+         int slow_down_compositing_scale_factor,
+         RendererSettings::OcclusionCullerSettings occlusion_culler_settings
+#if BUILDFLAG(IS_OZONE)
+         ,
+         const std::vector<OverlayStrategy>& overlay_strategies
+#endif
+#if BUILDFLAG(IS_MAC)
+         ,
+         int64_t display_id
+#endif
+      ) {
+        RendererSettings settings;
+        settings.allow_antialiasing = allow_antialiasing;
+        settings.force_antialiasing = force_antialiasing;
+        settings.force_blending_with_shaders = force_blending_with_shaders;
+        settings.partial_swap_enabled = partial_swap_enabled;
+        settings.should_clear_root_render_pass = should_clear_root_render_pass;
+        settings.release_overlay_resources_after_gpu_query =
+            release_overlay_resources_after_gpu_query;
+        settings.dont_round_texture_sizes_for_pixel_tests =
+            dont_round_texture_sizes_for_pixel_tests;
+        settings.highp_threshold_min = highp_threshold_min;
+        settings.auto_resize_output_surface = auto_resize_output_surface;
+        settings.requires_alpha_channel = requires_alpha_channel;
+        settings.disable_render_pass_bypassing = disable_render_pass_bypassing;
+        settings.force_non_scanout_backing_for_pixel_tests =
+            force_non_scanout_backing_for_pixel_tests;
+        settings.slow_down_compositing_scale_factor =
+            slow_down_compositing_scale_factor;
+        settings.occlusion_culler_settings = occlusion_culler_settings;
+#if BUILDFLAG(IS_OZONE)
+        settings.overlay_strategies = overlay_strategies;
+#endif
+#if BUILDFLAG(IS_MAC)
+        settings.display_id = display_id;
+#endif
+        return settings;
+      },
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<bool>(),
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<bool>(),
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<bool>(),
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<int>(),
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<bool>(),
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<bool>(),
+      fuzztest::Arbitrary<int>(), AnyOcclusionCullerSettings()
+#if BUILDFLAG(IS_OZONE)
+                                      ,
+      fuzztest::VectorOf(AnyOverlayStrategy())
+#endif
+#if BUILDFLAG(IS_MAC)
+          ,
+      fuzztest::Arbitrary<int64_t>()
+#endif
+  );
+}
+
+auto AnyDebugRendererSettings() {
+  return fuzztest::Map(
+      [](bool tint_composited_content, bool tint_composited_content_modulate,
+         bool show_overdraw_feedback, bool show_dc_layer_debug_borders,
+         bool show_aggregated_damage) {
+        DebugRendererSettings settings;
+        settings.tint_composited_content = tint_composited_content;
+        settings.tint_composited_content_modulate =
+            tint_composited_content_modulate;
+        settings.show_overdraw_feedback = show_overdraw_feedback;
+        settings.show_dc_layer_debug_borders = show_dc_layer_debug_borders;
+        settings.show_aggregated_damage = show_aggregated_damage;
+        return settings;
+      },
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<bool>(),
+      fuzztest::Arbitrary<bool>(), fuzztest::Arbitrary<bool>(),
+      fuzztest::Arbitrary<bool>());
+}
+
+}  // namespace
+
+void RendererSettingsFuzz(const RendererSettings& input) {
+  RendererSettings output;
+  mojom::RendererSettings::Deserialize(
+      mojom::RendererSettings::Serialize(&input), &output);
+}
+FUZZ_TEST(StructTraitsTest, RendererSettingsFuzz)
+    .WithDomains(AnyRendererSettings());
+
+void DebugRendererSettingsFuzz(const DebugRendererSettings& input) {
+  DebugRendererSettings output;
+  mojom::DebugRendererSettings::Deserialize(
+      mojom::DebugRendererSettings::Serialize(&input), &output);
+}
+FUZZ_TEST(StructTraitsTest, DebugRendererSettingsFuzz)
+    .WithDomains(AnyDebugRendererSettings());
 
 namespace {
 
