8926f6d0fa95eab245b4c8c3c085ba5614da250e
Piotr Paulski <piotrpaulski@chromium.org>
2026-05-07 09:02:31 -0700

    Update blink tests b4 Live Heap Profile Experiment removal from devtools
    Nesting heap profile samples was only needed for the experiment and
    will no longer be supported by devtools.
    
    See https://chromium-review.git.corp.google.com/c/devtools/devtools-frontend/+/7820638
    
    Bug: 476073965
    Change-Id: Iae07ae01ca456146841f1397a8988a573053973a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7829857
    Commit-Queue: Piotr Paulski <piotrpaulski@chromium.org>
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Auto-Submit: Piotr Paulski <piotrpaulski@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1626975}

--- third_party/blink/web_tests/http/tests/devtools/profiler/sampling-profiler-basic.js
+++ third_party/blink/web_tests/http/tests/devtools/profiler/sampling-profiler-basic.js
@@ -7,20 +7,16 @@ import {TestRunner} from 'test_runner';
 import * as SDK from 'devtools/core/sdk/sdk.js';
 
 (async function() {
-  TestRunner.addResult(`Tests that the sampling heap profiler works and supports nesting.\n`);
+  TestRunner.addResult(`Tests that the sampling heap profiler works.\n`);
 
   const profiler = SDK.TargetManager.TargetManager.instance().primaryPageTarget().model(SDK.HeapProfilerModel.HeapProfilerModel);
   await profiler.startSampling();
-  await profiler.startSampling();
   await TestRunner.evaluateInPagePromise(`let dump = new Array(5e4).fill(42.42)`);
-  const profile1 = await profiler.stopSampling();
-  await TestRunner.evaluateInPagePromise(`let dump2 = new Array(5e4).fill(42.42)`);
-  const profile2 = await profiler.stopSampling();
+  const profile = await profiler.stopSampling();
 
   const totalSize = node => node.children.reduce((acc, c) => acc + totalSize(c), node.selfSize);
   const checkValue = (expected, value) => expected * 0.8 < value && value < expected * 1.2;
-  TestRunner.addResult(`profile1 size is ok: ${checkValue(400e3, totalSize(profile1.head))}`);
-  TestRunner.addResult(`profile2 size is ok: ${checkValue(800e3, totalSize(profile2.head))}`);
+  TestRunner.addResult(`profile size is ok: ${checkValue(400e3, totalSize(profile.head))}`);
 
   try {
     await profiler.stopSampling();
