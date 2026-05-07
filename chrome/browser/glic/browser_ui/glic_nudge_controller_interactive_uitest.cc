a253c1974c516bca054bba87389486c33e6ae52e
Ian Wells <iwells@chromium.org>
2026-05-07 11:44:10 -0700

    Glic contextual cue v1 refactor 0/N: test GlicNudgeController
    Add some UI tests that verify that the tab strip glic nudge shows and
    hides.
    
    Bug: 485340385
    Change-Id: I16ac1cdd22f3627016c0069ebbbe46e0db6fbfa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7819435
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627098}

---
+++ chrome/browser/glic/browser_ui/glic_nudge_controller_interactive_uitest.cc
@@ -0,0 +1,95 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/test/scoped_feature_list.h"
+#include "chrome/browser/contextual_cueing/features.h"
+#include "chrome/browser/glic/browser_ui/glic_nudge_controller.h"
+#include "chrome/browser/glic/glic_pref_names.h"
+#include "chrome/browser/glic/public/glic_enabling.h"
+#include "chrome/browser/glic/suggestions/contextual_cueing_features.h"
+#include "chrome/browser/glic/test_support/interactive_glic_test.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/actions/chrome_action_id.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_features.h"
+#include "chrome/browser/ui/page_action/page_action_controller.h"
+#include "chrome/browser/ui/page_action/page_action_observer.h"
+#include "chrome/browser/ui/tabs/public/tab_features.h"
+#include "chrome/browser/ui/views/interaction/browser_elements_views.h"
+#include "chrome/browser/ui/views/tabs/tab_strip_action_container.h"
+#include "chrome/test/base/interactive_test_utils.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "chrome/test/interaction/interactive_browser_test.h"
+#include "components/prefs/pref_service.h"
+#include "content/public/test/browser_test.h"
+
+namespace glic {
+
+class GlicNudgeControllerInteractiveUiTest : public test::InteractiveGlicTest {
+ public:
+  GlicNudgeControllerInteractiveUiTest() {
+    feature_list_.InitWithFeatures(
+        /*enabled_features=*/{},
+        /*disabled_features=*/{contextual_cueing::kContextualCueingV2});
+  }
+
+  void SetUpOnMainThread() override {
+    InteractiveBrowserTest::SetUpOnMainThread();
+    GlicEnabling::SetBypassEnablementChecksForTesting(true);
+    browser()->profile()->GetPrefs()->SetBoolean(prefs::kGlicPinnedToTabstrip,
+                                                 true);
+
+    ASSERT_TRUE(tab_strip_action_container());
+  }
+
+  void TearDownOnMainThread() override {
+    GlicEnabling::SetBypassEnablementChecksForTesting(false);
+  }
+
+  GlicNudgeController* nudge_controller() {
+    return browser()->browser_window_features()->glic_nudge_controller();
+  }
+
+  TabStripActionContainer* tab_strip_action_container() {
+    return BrowserElementsViews::From(browser())
+        ->GetViewAs<TabStripActionContainer>(kTabStripActionContainerElementId);
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
+IN_PROC_BROWSER_TEST_F(GlicNudgeControllerInteractiveUiTest,
+                       ShowsTabStripNudge) {
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+
+  EXPECT_FALSE(tab_strip_action_container()->GetIsShowingGlicNudge());
+  nudge_controller()->UpdateNudgeLabel(
+      web_contents, "Nudge Label", "Prompt Suggestion", "Anchored Message Text",
+      std::nullopt, base::DoNothing());
+
+  EXPECT_TRUE(tab_strip_action_container()->GetIsShowingGlicNudge());
+}
+
+IN_PROC_BROWSER_TEST_F(GlicNudgeControllerInteractiveUiTest,
+                       HidesTabStripNudge) {
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+
+  EXPECT_FALSE(tab_strip_action_container()->GetIsShowingGlicNudge());
+  nudge_controller()->UpdateNudgeLabel(
+      web_contents, "Nudge Label", "Prompt Suggestion", "Anchored Message Text",
+      std::nullopt, base::DoNothing());
+
+  EXPECT_TRUE(tab_strip_action_container()->GetIsShowingGlicNudge());
+
+  nudge_controller()->UpdateNudgeLabel(
+      web_contents, std::string(), std::nullopt, std::string(),
+      GlicNudgeActivity::kNudgeDismissed, base::DoNothing());
+  EXPECT_FALSE(tab_strip_action_container()->GetIsShowingGlicNudge());
+}
+
+}  // namespace glic
