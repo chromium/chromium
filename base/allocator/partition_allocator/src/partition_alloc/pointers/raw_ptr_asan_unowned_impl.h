8779a41a5f26682a87af8aa6b18e85b20e3355f7
Fergal Daly <fergal@chromium.org>
2026-05-08 00:14:43 -0700

    Mark 2 calculations in raw_ptr_asan_unowned.cc as PA_UNSAFE_TODO
    Bug: 510915930
    Change-Id: I2d3dfd25240825e04b8a2b65ddca2c04e9c0db3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831823
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627499}

--- base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_asan_unowned_impl.h
+++ base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_asan_unowned_impl.h
@@ -89,7 +89,7 @@ struct RawPtrAsanUnownedImpl {
           std::enable_if_t<partition_alloc::internal::is_offset_type<Z>, void>>
   PA_ALWAYS_INLINE static constexpr T*
   Advance(T* wrapped_ptr, Z delta_elems, bool /*is_in_pointer_modification*/) {
-    return wrapped_ptr + delta_elems;
+    return PA_UNSAFE_TODO(wrapped_ptr + delta_elems);
   }
 
   // Retreat the wrapped pointer by `delta_elems`.
@@ -100,7 +100,7 @@ struct RawPtrAsanUnownedImpl {
           std::enable_if_t<partition_alloc::internal::is_offset_type<Z>, void>>
   PA_ALWAYS_INLINE static constexpr T*
   Retreat(T* wrapped_ptr, Z delta_elems, bool /*is_in_pointer_modification*/) {
-    return wrapped_ptr - delta_elems;
+    return PA_UNSAFE_TODO(wrapped_ptr - delta_elems);
   }
 
   template <typename T>
