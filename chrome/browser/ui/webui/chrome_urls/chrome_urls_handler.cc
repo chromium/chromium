96b1eb3dc27361ce47dd19d6330c6225e5f49405
Jeffrey Yu <yuje@chromium.org>
2026-05-08 16:08:42 -0700

    Use GURL string_view APIs in chrome_urls_handler.
    Since `GetScheme` and `GetHost` return a string by value, using the
    string_view APIs for comparison saves up to 4 string copies per
    comparison.
    
    Bug: 473916362
    Change-Id: Idfb6f327b94251f59cb2710dd571d5550ecf5188
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7833385
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Jeffrey Yu <yuje@chromium.org>
    Auto-Submit: Jeffrey Yu <yuje@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1627996}

--- chrome/browser/ui/webui/chrome_urls/chrome_urls_handler.cc
+++ chrome/browser/ui/webui/chrome_urls/chrome_urls_handler.cc
@@ -48,10 +48,10 @@ bool CompareWebuiUrlInfos(const chrome_urls::mojom::WebuiUrlInfoPtr& info1,
         info2->url.SchemeIs(content::kChromeUIUntrustedScheme));
   // Sort chrome:// before chrome-untrusted://. If the schemes are not equal,
   // given the check above one must be chrome:// and one chrome-untrusted://.
-  if (info1->url.GetScheme() != info2->url.GetScheme()) {
+  if (info1->url.scheme() != info2->url.scheme()) {
     return info1->url.SchemeIs(content::kChromeUIScheme);
   }
-  return info1->url.GetHost() < info2->url.GetHost();
+  return info1->url.host() < info2->url.host();
 }
 
 }  // namespace
