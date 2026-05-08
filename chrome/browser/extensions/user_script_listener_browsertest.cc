f96d3036a17560f73abdc003779f7ab0c1b8ddf2
Igor Kraskevich <kraskevich@google.com>
2026-05-08 03:13:21 -0700

    Revert "extensions: Port UserScriptListenerTest to desktop Android"
    This reverts commit 2cd7ff3f66f4078798c5a64f729ddc7d9129611d.
    
    Reason for revert: [Android EMEA Gardener] - one bot is consistently broken with this change https://ci.chromium.org/ui/p/chrome/builders/ci/android-desktop-16-arm64-rel-rauru-tests/2187/overview
    
    Original change's description:
    > extensions: Port UserScriptListenerTest to desktop Android
    >
    > We missed this one during initial bring-up of desktop Android. The test
    > passes with minimal modifications.
    >
    > Bug: 469417243
    > Change-Id: I9654051b0ea822611224168379a313fe2d401dfd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7830235
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1627452}
    
    Bug: 469417243
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1c50d3beb8aa39ee499a7468e7dbe83c571033c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7831039
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Owners-Override: Igor Kraskevich <kraskevich@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1627548}

--- chrome/browser/extensions/user_script_listener_browsertest.cc
+++ chrome/browser/extensions/user_script_listener_browsertest.cc
@@ -2,20 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/user_script_listener.h"
-
 #include "chrome/browser/extensions/extension_browsertest.h"
+#include "chrome/browser/extensions/user_script_listener.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/testing_profile.h"
-#include "content/public/browser/navigation_controller.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/navigation_handle.h"
-#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/test_navigation_observer.h"
 #include "extensions/browser/extensions_browser_client.h"
-#include "extensions/buildflags/buildflags.h"
-
-static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
@@ -35,9 +31,9 @@ IN_PROC_BROWSER_TEST_F(UserScriptListenerTest,
   content::TestNavigationObserver nav_observer(web_contents, 1);
   content::DidStartNavigationObserver start_observer(web_contents);
 
-  GURL url = embedded_test_server()->GetURL("/echo");
-  web_contents->GetController().LoadURL(
-      url, content::Referrer(), ui::PAGE_TRANSITION_LINK, std::string());
+  ui_test_utils::NavigateToURLWithDisposition(
+      browser(), embedded_test_server()->GetURL("/echo"),
+      WindowOpenDisposition::CURRENT_TAB, ui_test_utils::BROWSER_TEST_NO_WAIT);
 
   start_observer.Wait();
   ASSERT_TRUE(start_observer.navigation_handle());
