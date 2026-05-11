9c069177a7a984cd434c21478e4e08f0da54f5fe
Robbie Gibson <rkgibson@google.com>
2026-05-11 10:10:31 -0700

    Revert "[ios] Use 100% quality when transcoding NTP background images"
    This reverts commit 3fbb76eb56a216af85db005b572482f535be5476.
    
    Reason for revert: Temporarily rolling back using the JS Image Transcoder here.
    
    Original change's description:
    > [ios] Use 100% quality when transcoding NTP background images
    >
    > To minimize generation loss when re-encoding lossy images like JPEG in
    > the sandbox, this CL updates the call to web::JavaScriptImageTranscoder
    > to use a 100% quality setting.
    >
    > Previously, passing `nil` for the quality parameter resulted in using
    > the system default quality, as the transcoder uses the
    > HTMLCanvasElement.toBlob() API internally (see transcode_image.ts:
    > https://source.chromium.org/chromium/chromium/src/+/main:ios/web/js_image_transcoder/resources/transcode_image.ts),
    > which defaults to the user-agent setting when quality is omitted:
    > https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/toBlob
    >
    > Change-Id: I444b32b1a22af44cf4a2f6886ff717564353646e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808170
    > Commit-Queue: Pier-Alexandre Bouchard <pabouchard@google.com>
    > Reviewed-by: Adam Arcaro <adamta@google.com>
    > Reviewed-by: Robbie Gibson <rkgibson@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1623932}
    
    Bug: b:512089069
    Change-Id: Ic87636b9ffdfb39274b08836db30f325b6baeef2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7834831
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1628653}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_mediator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_mediator.mm
@@ -1009,7 +1009,7 @@ void CleanupImageFetcherCacheIfNeeded(PrefService* pref_service,
   __weak __typeof(self) weakSelf = self;
   _imageTranscoder->TranscodeImage(
       [NSData dataWithBytes:imageData.data() length:imageData.length()],
-      base::SysUTF8ToNSString(metadata.mime_type), nil, nil, @1.0,
+      base::SysUTF8ToNSString(metadata.mime_type), nil, nil, nil,
       base::BindOnce(^(NSData* safeData, NSError* error) {
         UIImage* image = [UIImage imageWithData:safeData];
         if (image) {
