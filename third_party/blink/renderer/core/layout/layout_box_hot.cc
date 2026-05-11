ce086b6b6df9ca14519e28fd2ae48f7a1515f7da
Morten Stenshorne <mstensho@chromium.org>
2026-05-11 08:39:02 -0700

    Remove unreachable cache-miss code for MinimalSpaceShortage.
    There's code further up that just bails in all cases if
    MinimalSpaceShortage is set.
    
    Change-Id: I2115d5056479593f5ddd144904ee7b1674c3e105
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828689
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628592}

--- third_party/blink/renderer/core/layout/layout_box_hot.cc
+++ third_party/blink/renderer/core/layout/layout_box_hot.cc
@@ -384,20 +384,6 @@ const LayoutResult* LayoutBox::CachedLayoutResult(
         if (cached_layout_result->IsTruncatedByFragmentationLine())
           return nullptr;
 
-        // TODO(layout-dev): This likely shouldn't be scoped to just OOFs, but
-        // scoping it more widely results in several perf regressions[1].
-        //
-        // [1] https://bugs.chromium.org/p/chromium/issues/detail?id=1362550
-        if (node.IsOutOfFlowPositioned()) {
-          // If the fragmentainer size has changed, and there previously was
-          // space shortage reported, we should re-run layout to avoid reporting
-          // the same space shortage again.
-          std::optional<LayoutUnit> space_shortage =
-              cached_layout_result->MinimalSpaceShortage();
-          if (space_shortage && *space_shortage > LayoutUnit())
-            return nullptr;
-        }
-
         // Returns true if there are any floats added by |cached_layout_result|
         // which will end up crossing the fragmentation line.
         auto DoFloatsCrossFragmentationLine = [&]() -> bool {
