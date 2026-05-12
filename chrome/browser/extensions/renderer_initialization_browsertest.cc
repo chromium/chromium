b91a22a6d7040ad373e27054b49663a145a23c3d
James Cook <jamescook@chromium.org>
2026-05-11 23:08:48 -0700

    extensions: Port RendererInitializationTest to desktop Android
    We missed it when doing the initial bring-up. One test passes with
    minor modifications. The other is skipped because desktop Android
    doesn't support themes.
    
    Bug: 469417243
    Change-Id: I6961e08b0c7e6a2921f127bc44ad75e3af58d517
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7836364
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1629076}

--- chrome/browser/extensions/renderer_initialization_browsertest.cc
+++ chrome/browser/extensions/renderer_initialization_browsertest.cc
@@ -3,20 +3,29 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/extensions/extension_browsertest.h"
-#include "chrome/browser/themes/theme_service.h"
-#include "chrome/browser/ui/browser.h"
+#include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/extension_registrar.h"
+#include "ui/base/page_transition_types.h"
+#include "url/gurl.h"
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/themes/theme_service.h"
+#endif
 
 namespace extensions {
+namespace {
+
+using RendererInitializationTest = ExtensionBrowserTest;
 
 // Test that opening a window with an extension recorded as active, then
 // unloading the extension, all before the renderer is fully initialized,
 // doesn't crash. This addresses crbug.com/40434302, where messages could be
 // sent out of order if an extension unloaded before the activation message was
 // sent.
-IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
+IN_PROC_BROWSER_TEST_F(RendererInitializationTest,
                        TestRendererStartupWithConflictingMessages) {
   // Load up an extension an begin opening an URL to a page within it. Since
   // this will be an extension tab, the extension will be active within that
@@ -24,25 +33,31 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
   const Extension* extension =
       LoadExtension(test_data_dir_.AppendASCII("simple_with_file"));
   ASSERT_TRUE(extension);
+
+  // Open a new tab.
+  NavigateToURLInNewTab(GURL("about:blank"));
+  content::WebContents* web_contents = GetActiveWebContents();
+
+  // Start loading a file inside the extension.
   GURL url = extension->GetResourceURL("file.html");
-  browser()->OpenURL(
-      content::OpenURLParams(url, content::Referrer(),
-                             WindowOpenDisposition::NEW_FOREGROUND_TAB,
-                             ui::PAGE_TRANSITION_TYPED, false),
-      /*navigation_handle_callback=*/{});
+  web_contents->GetController().LoadURL(
+      url, content::Referrer(), ui::PAGE_TRANSITION_TYPED, std::string());
+
   // Without waiting for the tab to finish, unload the extension.
   extension_registrar()->RemoveExtension(extension->id(),
                                          UnloadedExtensionReason::TERMINATE);
-  content::WebContents* web_contents = GetActiveWebContents();
+
   // Wait for the web contents to stop loading.
   content::WaitForLoadStop(web_contents);
   EXPECT_EQ(url, web_contents->GetLastCommittedURL());
   ASSERT_FALSE(web_contents->IsCrashed());
 }
 
+// Android does not support themes.
+#if !BUILDFLAG(IS_ANDROID)
 // Tests that loading a file from a theme in a tab doesn't crash anything.
 // Another part of crbug.com/40434302 and related.
-IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
+IN_PROC_BROWSER_TEST_F(RendererInitializationTest,
                        TestRendererInitializationWithThemesTab) {
   // Don't create "Cached Theme.pak" in the extension directory, so as not to
   // modify the source tree.
@@ -60,5 +75,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest,
   EXPECT_EQ(url, web_contents->GetLastCommittedURL());
   ASSERT_FALSE(web_contents->IsCrashed());
 }
+#endif  // !BUILDFLAG(IS_ANDROID)
 
+}  // namespace
 }  // namespace extensions
