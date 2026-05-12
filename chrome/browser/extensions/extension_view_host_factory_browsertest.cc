e9065dd47ba84af9bd2a2e962b7eb0c06b7dcfef
James Cook <jamescook@chromium.org>
2026-05-11 18:55:38 -0700

    extensions: Port ExtensionViewHostFactoryTest to desktop Android
    The main test passes with minor modifications.
    
    The side panel test is skipped because desktop Android doesn't support
    the side panel UI. I updated it to use the more modern
    BrowserWindowInterface and TabListInterface in anticipation of wanting
    to run this on Android someday.
    
    Bug: 469417243
    Change-Id: I084e9ac63178ceda303d54820db3f4951fe6fb5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7837670
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1628928}

--- chrome/browser/extensions/extension_view_host_factory_browsertest.cc
+++ chrome/browser/extensions/extension_view_host_factory_browsertest.cc
@@ -4,12 +4,19 @@
 
 #include "chrome/browser/extensions/extension_view_host_factory.h"
 
+#include <memory>
+
+#include "base/memory/scoped_refptr.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_view_host.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/tab_list/tab_list_interface.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/buildflags/buildflags.h"
+#include "extensions/common/extension.h"
 #include "extensions/common/mojom/view_type.mojom.h"
+
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
 
@@ -29,11 +36,14 @@ IN_PROC_BROWSER_TEST_F(ExtensionViewHostFactoryTest, CreateExtensionHosts) {
   // Popup hosts are created with the correct type and profile.
   std::unique_ptr<ExtensionViewHost> host =
       ExtensionViewHostFactory::CreatePopupHost(*extension, extension->url(),
-                                                browser());
+                                                GetBrowserWindowInterface());
   EXPECT_EQ(extension.get(), host->extension());
   EXPECT_EQ(browser_context, host->browser_context());
   EXPECT_EQ(mojom::ViewType::kExtensionPopup, host->extension_host_type());
 }
+
+// TODO(crbug.com/458688998): Desktop Android doesn't support side panel yet.
+#if BUILDFLAG(ENABLE_EXTENSIONS)
 
 // Tests that side panel hosts are created with the correct profile and
 // browsers.
@@ -62,15 +72,19 @@ IN_PROC_BROWSER_TEST_F(ExtensionViewHostFactoryTest,
 
   {
     // Create a side panel host with a tab based WebContents passed in.
+    auto* browser = GetBrowserWindowInterface();
+    auto* tab_list = TabListInterface::From(browser);
+    auto* active_tab = tab_list->GetActiveTab();
     std::unique_ptr<ExtensionViewHost> host =
         ExtensionViewHostFactory::CreateSidePanelHost(
-            *extension, extension->url(), /*browser=*/nullptr,
-            browser()->tab_strip_model()->GetActiveTab());
+            *extension, extension->url(), /*browser=*/nullptr, active_tab);
     EXPECT_EQ(extension.get(), host->extension());
     EXPECT_EQ(browser_context, host->browser_context());
     EXPECT_EQ(mojom::ViewType::kExtensionSidePanel,
               host->extension_host_type());
   }
 }
+
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 }  // namespace extensions
