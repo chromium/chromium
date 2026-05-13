3445b40d3e0b63f8e50cbe0d0aefe8fc6b7c8ce6
Julia Sobiech <jsobiech@google.com>
2026-05-13 07:07:25 -0700

    [HoT][Bling] Enable item selection and edition for Travel page
    Connects the mediator to the coordinator, which handles the actual
    navigation to the Autofill AI entity editor. Enables users to select,
    view and edit the details of individual items.
    
    Bug: 491417039
    Change-Id: Icbe25a7dd3f7d964b96b7a635450cbd392c700f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7838561
    Commit-Queue: Julia Sobiech <jsobiech@google.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1629940}

--- ios/chrome/browser/settings/autofill/autofill_and_passwords/coordinator/travel_info_coordinator.mm
+++ ios/chrome/browser/settings/autofill/autofill_and_passwords/coordinator/travel_info_coordinator.mm
@@ -5,19 +5,30 @@
 #import "ios/chrome/browser/settings/autofill/autofill_and_passwords/coordinator/travel_info_coordinator.h"
 
 #import "base/check_op.h"
+#import "components/autofill/core/browser/data_manager/autofill_ai/entity_data_manager.h"
 #import "ios/chrome/browser/autofill/model/ios_autofill_entity_data_manager_factory.h"
+#import "ios/chrome/browser/settings/autofill/autofill_ai/coordinator/autofill_ai_entity_edit_coordinator.h"
+#import "ios/chrome/browser/settings/autofill/autofill_ai/coordinator/autofill_ai_entity_edit_coordinator_delegate.h"
 #import "ios/chrome/browser/settings/autofill/autofill_and_passwords/coordinator/travel_info_mediator.h"
 #import "ios/chrome/browser/settings/autofill/autofill_and_passwords/ui/travel_info_table_view_controller.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 
-@interface TravelInfoCoordinator () <TravelInfoTableViewControllerDelegate>
+@interface TravelInfoCoordinator () <AutofillAIEntityEditCoordinatorDelegate,
+                                     AutofillAIBaseMediatorDelegate,
+                                     TravelInfoTableViewControllerDelegate>
 @end
 
 @implementation TravelInfoCoordinator {
+  // View controller providing the UI for the Travel Info list.
   TravelInfoTableViewController* _viewController;
+
+  // Mediator providing the data and fulfilling mutator actions for the view.
   TravelInfoMediator* _mediator;
+
+  // Coordinator for displaying and editing a selected travel info entity.
+  AutofillAIEntityEditCoordinator* _entityEditCoordinator;
 }
 
 @synthesize baseNavigationController = _baseNavigationController;
@@ -48,6 +59,7 @@
   _mediator =
       [[TravelInfoMediator alloc] initWithEntityDataManager:entityDataManager];
   _mediator.consumer = _viewController;
+  _mediator.delegate = self;
   _viewController.mutator = _mediator;
 
   [self.baseNavigationController pushViewController:_viewController
@@ -55,6 +67,8 @@
 }
 
 - (void)stop {
+  [self stopEntityEditCoordinator];
+
   [_mediator disconnect];
   _mediator = nil;
 
@@ -68,6 +82,42 @@
     (TravelInfoTableViewController*)controller {
   CHECK_EQ(_viewController, controller);
   [self.delegate travelInfoCoordinatorDidRemove:self];
+}
+
+#pragma mark - AutofillAIBaseMediatorDelegate
+
+- (void)autofillAIBaseMediator:(AutofillAIBaseMediator*)mediator
+    didRequestToOpenEntityWithID:(autofill::EntityInstance::EntityId)entityID {
+  [self startEntityEditCoordinatorWithID:entityID];
+}
+
+#pragma mark - AutofillAIEntityEditCoordinatorDelegate
+
+- (void)autofillAIEntityEditCoordinatorDidFinish:
+    (AutofillAIEntityEditCoordinator*)coordinator {
+  [self stopEntityEditCoordinator];
+}
+
+#pragma mark - Private
+
+// Starts the coordinator responsible for displaying and editing the travel info
+// entity with the specified ID.
+- (void)startEntityEditCoordinatorWithID:
+    (autofill::EntityInstance::EntityId)entityID {
+  [self stopEntityEditCoordinator];
+  _entityEditCoordinator = [[AutofillAIEntityEditCoordinator alloc]
+      initWithBaseNavigationController:self.baseNavigationController
+                               browser:self.browser
+                              entityID:entityID];
+  _entityEditCoordinator.delegate = self;
+  [_entityEditCoordinator start];
+}
+
+// Stops and disconnects the active entity edit coordinator.
+- (void)stopEntityEditCoordinator {
+  [_entityEditCoordinator stop];
+  _entityEditCoordinator.delegate = nil;
+  _entityEditCoordinator = nil;
 }
 
 @end
