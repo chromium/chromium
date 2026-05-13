e4eb0c4875a8edd989d5843ecffbe8e82096ff83
Shu Yang <shuyng@google.com>
2026-05-13 10:37:29 -0700

    [PDF V2] Stabilize PDF page layout constraints using container block
    Wraps the PDF toolbar ViewStub inside a stable FrameLayout container
    to natively resolve ConstraintLayout targets purely in XML. This ensures
    the PDF fragment container correctly anchors below the toolbar block
    regardless of inflation state, removing imperative runtime LayoutParams
    manipulation and build dependencies from PdfCoordinator.
    
    Bug: 496180649
    Change-Id: I8feb2c0b3d9ffce06dc4ee9bb952db88775499cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7843084
    Commit-Queue: Shu Yang <shuyng@google.com>
    Reviewed-by: Ryan Thomas <rathomas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630073}

--- chrome/browser/ui/android/pdf/internal/java/src/org/chromium/chrome/browser/pdf/PdfCoordinator.java
+++ chrome/browser/ui/android/pdf/internal/java/src/org/chromium/chrome/browser/pdf/PdfCoordinator.java
@@ -18,7 +18,6 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
 import androidx.annotation.VisibleForTesting;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
@@ -199,14 +198,6 @@ public class PdfCoordinator
         }
         mToolbarCoordinator =
                 PdfUtils.isInlinePdfV2Enabled() ? new PdfToolbarCoordinator(mView, this) : null;
-        if (!PdfUtils.isInlinePdfV2Enabled()) {
-            View container = mView.findViewById(R.id.pdf_fragment_container);
-            ConstraintLayout.LayoutParams params =
-                    (ConstraintLayout.LayoutParams) container.getLayoutParams();
-            params.topToBottom = ConstraintLayout.LayoutParams.UNSET;
-            params.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-            container.setLayoutParams(params);
-        }
     }
 
     /**
