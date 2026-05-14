09632d9766aa8933b47e1a70d3e7c9c516d92956
Xu Li <mxlg@google.com>
2026-05-14 12:54:50 -0700

    [iOS] Update the voiceOver focus.
    This CL set accessibilityViewIsModal as true to insure that when the
    voiceOVer is enabled, the focus will be restricted to the dialog. Also
    added the logic to restore the focus to the previous item when the
    dialog is dismissed.
    
    Bug: 512530015
    Change-Id: I364289881ff91882255307cc00679f9f69806efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842755
    Reviewed-by: Francis Beauchamp <fbeauchamp@google.com>
    Commit-Queue: Xu Li <mxlg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630723}

--- ios/chrome/browser/enterprise/enterprise_dialog/coordinator/enterprise_dialog_coordinator.mm
+++ ios/chrome/browser/enterprise/enterprise_dialog/coordinator/enterprise_dialog_coordinator.mm
@@ -6,7 +6,10 @@
 
 #import "base/functional/callback.h"
 #import "ios/chrome/browser/enterprise/enterprise_dialog/model/warning_dialog.h"
+#import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+#import "ios/web/public/web_state.h"
 
 @implementation EnterpriseDialogCoordinator {
   // The underlying alert controller used to show the dialog.
@@ -49,6 +52,8 @@
                                                                 completion:nil];
   _alertController = nil;
 
+  [self restoreFocus];
+
   // If stop is called before a choice was made, consider it a "cancel".
   if (_callback) {
     std::move(_callback).Run(false);
@@ -86,6 +91,7 @@
   // Hide the keyboard to prevent it from flickering when dismissing the alert.
   [self dismissKeyboard];
 
+  _alertController.view.accessibilityViewIsModal = YES;
   [self.baseViewController presentViewController:_alertController
                                         animated:YES
                                       completion:nil];
@@ -97,6 +103,29 @@
 - (void)onWarningDialogDismissed:(bool)result {
   if (_callback) {
     std::move(_callback).Run(result);
+  }
+
+  [self restoreFocus];
+}
+
+// Restores focus to the active web content.
+- (void)restoreFocus {
+  if (!self.browser) {
+    return;
+  }
+  web::WebState* activeWebState =
+      self.browser->GetWebStateList()->GetActiveWebState();
+  UIView* viewToFocus = activeWebState ? activeWebState->GetView() : nil;
+  if (viewToFocus) {
+    // Delay focus restoration slightly to ensure the alert dismissal transition
+    // has started and VoiceOver can focus on the underlying view.
+    auto postAccessibilityChangeBlock = ^{
+      UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification,
+                                      viewToFocus);
+    };
+    dispatch_after(
+        dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)),
+        dispatch_get_main_queue(), postAccessibilityChangeBlock);
   }
 }
 
