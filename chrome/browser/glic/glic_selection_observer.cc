101b0587c75157923f78dfd7f19640028792168e
Dave Tapuska <dtapuska@chromium.org>
2026-05-14 08:40:24 -0700

    Reland "Refactor Glic selection handling and add pending selection state."
    This reverts commit 9a329c6a0b1d12566665b8b99542bfd5d6d02a20.
    
    Reason for revert: Incorrectly blamed for unrelated glic failures
    
    Original change's description:
    > Revert "Refactor Glic selection handling and add pending selection state."
    >
    > This reverts commit 3e5c6de2c4b507b2acf23300f6b2bc74e9e60bba.
    >
    > Reason for revert: likely culprit for failures https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ASan%20LSan%20Tests%20(1)/140731/overview
    >
    > Original change's description:
    > > Refactor Glic selection handling and add pending selection state.
    > >
    > > This change refactors GlicSelectionObserver::UpdateSelectionState to
    > > better manage the display of selection affordances (widget/nudge) and
    > > sending context to the Glic panel. A new is_pending_selection parameter
    > > is introduced to distinguish between text selections from the renderer
    > > and updates triggered by global panel state changes. Helper methods
    > > IsPanelShowing, SendAdditionalContextToPanel, and
    > > ShowSelectionAffordance are added for clarity and testability. The logic
    > > for showing the affordance is now more granular, considering feature
    > > flags and whether the panel is currently open.
    > >
    > > BUG=b:511137657
    > >
    > > Change-Id: Ie890414318ae61783773c539a676e3e5f3ef9af5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7843434
    > > Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    > > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1630004}
    >
    > Bug: b:511137657
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I9397110412e4b6d5c5e7b66b5582eab2a025d6a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7846802
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Andrea Orru <andreaorru@chromium.org>
    > Owners-Override: Andrea Orru <andreaorru@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1630242}
    
    Bug: b:511137657
    Change-Id: If40ae6b37ff4355d172983499bb5d8bad1ec342b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7847233
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630640}

--- chrome/browser/glic/glic_selection_observer.cc
+++ chrome/browser/glic/glic_selection_observer.cc
@@ -308,7 +308,8 @@ void GlicSelectionObserver::OnInputEvent(
         is_selecting_ = true;
         ResetPendingSelection();
         if (has_sent_selection_context_) {
-          UpdateSelectionState(std::u16string());
+          UpdateSelectionState(std::u16string(),
+                               /*is_pending_selection=*/false);
         }
       }
       break;
@@ -356,7 +357,7 @@ void GlicSelectionObserver::OnTextSelectionChanged(
 
   if (is_key_selection_) {
     pending_selection_text_ = std::u16string();
-    UpdateSelectionState(std::u16string());
+    UpdateSelectionState(std::u16string(), /*is_pending_selection=*/false);
     return;
   }
 
@@ -415,7 +416,7 @@ void GlicSelectionObserver::ProcessPendingSelection() {
   std::u16string selected_text = std::move(*pending_selection_text_);
   ResetPendingSelection();
 
-  UpdateSelectionState(selected_text);
+  UpdateSelectionState(selected_text, /*is_pending_selection=*/true);
 }
 
 void GlicSelectionObserver::ResetPendingSelection() {
@@ -477,7 +478,8 @@ void GlicSelectionObserver::InvokeGlicFromSelectionAffordance(
 }
 
 void GlicSelectionObserver::UpdateSelectionState(
-    const std::u16string& selected_text) {
+    const std::u16string& selected_text,
+    bool is_pending_selection) {
   last_selected_text_ = selected_text;
   auto* tab_interface =
       tabs::TabInterface::MaybeGetFromContents(web_contents());
@@ -499,13 +501,8 @@ void GlicSelectionObserver::UpdateSelectionState(
                            base::DoNothing());
     }
 
-    if (has_sent_selection_context_ && glic_keyed_service_) {
-      if (auto* instance =
-              glic_keyed_service_->GetInstanceForTab(tab_interface)) {
-        // TODO(b/508916357): Use the invoke API.
-        instance->SendAdditionalContext(
-            CreateAdditionalContext(web_contents(), u""));
-      }
+    if (has_sent_selection_context_) {
+      SendAdditionalContextToPanel(tab_interface, u"");
       has_sent_selection_context_ = false;
     }
 
@@ -516,27 +513,25 @@ void GlicSelectionObserver::UpdateSelectionState(
     return;
   }
 
-  bool panel_showing = false;
-  if (glic_keyed_service_ &&
-      glic_keyed_service_->GetInstanceForTab(tab_interface)) {
-    panel_showing = glic_keyed_service_->IsPanelShowingForBrowser(*bwi);
-  }
+  bool panel_showing = IsPanelShowing(tab_interface, bwi);
 
   if (panel_showing) {
-    if (selection_widget_) {
+    if (is_pending_selection && features::kGlicSelectionPromptUseWidget.Get()) {
+      if (!features::kGlicSelectionPromptUpdatesOnly.Get()) {
+        ShowSelectionAffordance(selected_text, bwi);
+      }
+    } else if (selection_widget_) {
       selection_widget_->CloseWithReason(
           views::Widget::ClosedReason::kLostFocus);
     }
 
     // TODO(b/508916357): Use the invoke API.
-    if (auto* instance =
-            glic_keyed_service_->GetInstanceForTab(tab_interface)) {
-      instance->SendAdditionalContext(
-          CreateAdditionalContext(web_contents(), selected_text));
-    }
+    SendAdditionalContextToPanel(tab_interface, selected_text);
     has_sent_selection_context_ = true;
   } else {
-    if (!features::kGlicSelectionPromptUpdatesOnly.Get()) {
+    if ((!features::kGlicSelectionPromptUseWidget.Get() ||
+         is_pending_selection) &&
+        !features::kGlicSelectionPromptUpdatesOnly.Get()) {
       ShowSelectionAffordance(selected_text, bwi);
     }
     has_sent_selection_context_ = false;
@@ -761,6 +756,29 @@ void GlicSelectionObserver::OnLinkGenerated(
   }
 }
 
+bool GlicSelectionObserver::IsPanelShowing(tabs::TabInterface* tab_interface,
+                                           BrowserWindowInterface* bwi) {
+  if (glic_keyed_service_ &&
+      glic_keyed_service_->GetInstanceForTab(tab_interface)) {
+    return glic_keyed_service_->IsPanelShowingForBrowser(*bwi);
+  }
+  return false;
+}
+
+void GlicSelectionObserver::SendAdditionalContextToPanel(
+    tabs::TabInterface* tab_interface,
+    const std::u16string& selected_text) {
+  if (!glic_keyed_service_) {
+    return;
+  }
+  if (auto* instance =
+          glic_keyed_service_->GetInstanceForTab(tab_interface)) {
+    // TODO(b/508916357): Use the invoke API.
+    instance->SendAdditionalContext(
+        CreateAdditionalContext(web_contents(), selected_text));
+  }
+}
+
 void GlicSelectionObserver::CopyLinkToHighlight(
     content::WeakDocumentPtr weak_document_ptr) {
   if (generated_link_.has_value() && generated_link_->is_valid()) {
@@ -773,7 +791,7 @@ void GlicSelectionObserver::OnGlobalPanelShowHide() {
     return;
   }
 
-  UpdateSelectionState(last_selected_text_);
+  UpdateSelectionState(last_selected_text_, /*is_pending_selection=*/false);
 }
 
 }  // namespace glic
