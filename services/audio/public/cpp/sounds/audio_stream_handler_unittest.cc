f6b5d44251d8f82f71318d3f1562aa64ee9c5a24
Ernest Nguyen Hung <ernn@google.com>
2026-05-13 23:25:54 -0700

    audio: Refactor `AudioStreamHandler` unit tests
    It refactors `AudioStreamHandler` unit tests to improve the clarify and
    structure by: separating the test for bad audio data into its own
    non-parameterized test case and creating a base test fixture and a
    parameterized derived fixture for better organization.
    
    Bug: 485150597
    Change-Id: Iec4c51d4b1b30eb27bc29ea2aa81cf128b3b1948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828059
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Ernest Nguyen Hung <ernn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1630511}

--- services/audio/public/cpp/sounds/audio_stream_handler_unittest.cc
+++ services/audio/public/cpp/sounds/audio_stream_handler_unittest.cc
@@ -5,6 +5,7 @@
 #include "services/audio/public/cpp/sounds/audio_stream_handler.h"
 
 #include <memory>
+#include <string>
 #include <string_view>
 #include <utility>
 
@@ -30,137 +31,122 @@
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace audio {
-
 namespace {
 
-const char kTestBadWavAudioData[] = "RIFF1234WAVEjunkjunkjunkjunk";
-const size_t kTestBadWavAudioDataSize = std::size(kTestBadWavAudioData) - 1;
+using ::testing::TestParamInfo;
+using ::testing::ValuesIn;
+
+constexpr std::string_view kTestBadWavAudioData =
+    "RIFF1234WAVEjunkjunkjunkjunk";
+
+std::string ReadTestMediaFile(std::string_view file_name) {
+  const base::FilePath file_path = media::GetTestDataFilePath(file_name);
+  std::string data;
+  CHECK(base::ReadFileToString(file_path, &data));
+  return data;
+}
 
 struct TestParams {
-  const media::AudioCodec codec;
-  const bool is_bad;
-  const bool is_file;
-  const char* const source;
-  const size_t data_size = 0;
+  std::string audio_data;
+  media::AudioCodec codec = media::AudioCodec::kUnknown;
+  std::string test_suffix;
 };
 
-constexpr TestParams kTestParamsWav[] = {
-    {media::AudioCodec::kPCM, /*is_bad=*/false, /*is_file=*/false,
-     kTestAudioData, kTestAudioDataSize},
-    {media::AudioCodec::kPCM, /*is_bad=*/true, /*is_file=*/false,
-     kTestBadWavAudioData, kTestBadWavAudioDataSize},
-};
+std::vector<TestParams> GetTestParams() {
+  return {
+      {.audio_data = std::string(kTestAudioData, kTestAudioDataSize),
+       .codec = media::AudioCodec::kPCM,
+       .test_suffix = "Wav"},
+      {.audio_data = ReadTestMediaFile("bear.flac"),
+       .codec = media::AudioCodec::kFLAC,
+       .test_suffix = "Flac"},
+  };
+}
 
-constexpr TestParams kTestParamsFlac[] = {
-    {media::AudioCodec::kFLAC, /*is_bad=*/false, /*is_file=*/true, "bear.flac"},
-};
-
-}  // namespace
-
-class AudioStreamHandlerTest : public ::testing::TestWithParam<TestParams> {
+class AudioStreamHandlerTest : public testing::Test {
  public:
-  AudioStreamHandlerTest()
-      : codec_(GetParam().codec),
-        is_bad_(GetParam().is_bad),
-        is_file_(GetParam().is_file),
-        source_(GetParam().source),
-        data_size_(GetParam().data_size) {}
-
   ~AudioStreamHandlerTest() override = default;
 
-  bool is_bad() const { return is_bad_; }
-  AudioStreamHandler* handler() { return audio_stream_handler_.get(); }
-
-  void SetUp() override {
-    CreateHandler();
-    DCHECK(audio_stream_handler_);
-  }
-
-  void CreateHandler(bool loop = false) {
-    if (is_file_) {
-      const base::FilePath file_path = media::GetTestDataFilePath(source_);
-      EXPECT_TRUE(base::ReadFileToString(file_path, &bitstream_));
-      audio_stream_handler_ = std::make_unique<AudioStreamHandler>(
-          base::DoNothing(), bitstream_, codec_, loop);
-    } else {
-      std::string_view data(source_, data_size_);
-      audio_stream_handler_ = std::make_unique<AudioStreamHandler>(
-          base::DoNothing(), data, codec_, loop);
-    }
-  }
-
-  void SetObserverForTesting(AudioStreamHandler::TestObserver* observer) {
-    AudioStreamHandler::SetObserverForTesting(observer);
-  }
-
  private:
-  base::test::TaskEnvironment env_;
-  const media::AudioCodec codec_;
-  const bool is_bad_;
-  const bool is_file_;
-  const char* const source_;
-  const size_t data_size_;
-  std::string bitstream_;
-  std::unique_ptr<AudioStreamHandler> audio_stream_handler_;
+  base::test::TaskEnvironment task_environment_;
 };
 
-TEST_P(AudioStreamHandlerTest, Play) {
-  if (is_bad()) {
-    return;
+TEST_F(AudioStreamHandlerTest, BadDataDoesNotInitialize) {
+  AudioStreamHandler handler(/*stream_factory_binder=*/base::DoNothing(),
+                             kTestBadWavAudioData, media::AudioCodec::kPCM);
+
+  // The handler should not be initialized with bad data, and `Play` should
+  // return `false`.
+  EXPECT_FALSE(handler.IsInitialized());
+  EXPECT_FALSE(handler.Play());
+
+  // Call `Stop` to ensure that there is no crash.
+  handler.Stop();
+}
+
+class AudioStreamHandlerTestWithParams
+    : public AudioStreamHandlerTest,
+      public testing::WithParamInterface<TestParams> {
+ protected:
+  std::unique_ptr<AudioStreamHandler> CreateHandler(bool loop = false) {
+    return std::make_unique<AudioStreamHandler>(
+        /*stream_factory_binder=*/base::DoNothing(), GetParam().audio_data,
+        GetParam().codec, loop);
   }
+};
+
+TEST_P(AudioStreamHandlerTestWithParams, Play) {
+  std::unique_ptr<AudioStreamHandler> handler = CreateHandler();
+  ASSERT_NE(handler, nullptr);
 
   base::RunLoop run_loop;
   TestObserver observer(run_loop.QuitClosure());
-  SetObserverForTesting(&observer);
+  AudioStreamHandler::SetObserverForTesting(&observer);
 
-  ASSERT_TRUE(handler()->IsInitialized());
-  ASSERT_TRUE(handler()->Play());
+  ASSERT_TRUE(handler->IsInitialized());
+  ASSERT_TRUE(handler->Play());
 
   run_loop.Run();
 
-  SetObserverForTesting(nullptr);
+  AudioStreamHandler::SetObserverForTesting(nullptr);
 
-  ASSERT_EQ(1, observer.num_play_requests());
-  ASSERT_EQ(1, observer.num_stop_requests());
+  EXPECT_EQ(observer.num_play_requests(), 1);
+  EXPECT_EQ(observer.num_stop_requests(), 1);
 }
 
-TEST_P(AudioStreamHandlerTest, ConsecutivePlayRequests) {
-  if (is_bad()) {
-    return;
-  }
+TEST_P(AudioStreamHandlerTestWithParams, ConsecutivePlayRequests) {
+  std::unique_ptr<AudioStreamHandler> handler = CreateHandler();
+  ASSERT_NE(handler, nullptr);
 
   base::RunLoop run_loop;
   TestObserver observer(run_loop.QuitClosure());
-  SetObserverForTesting(&observer);
+  AudioStreamHandler::SetObserverForTesting(&observer);
 
-  ASSERT_TRUE(handler()->IsInitialized());
-  ASSERT_TRUE(handler()->Play());
+  ASSERT_TRUE(handler->IsInitialized());
+  ASSERT_TRUE(handler->Play());
 
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
       FROM_HERE,
       base::BindOnce(base::IgnoreResult(&AudioStreamHandler::Play),
-                     base::Unretained(handler())),
+                     base::Unretained(handler.get())),
       base::Seconds(1));
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
       FROM_HERE,
-      base::BindOnce(&AudioStreamHandler::Stop, base::Unretained(handler())),
+      base::BindOnce(&AudioStreamHandler::Stop,
+                     base::Unretained(handler.get())),
       base::Seconds(2));
 
   run_loop.Run();
 
-  SetObserverForTesting(nullptr);
+  AudioStreamHandler::SetObserverForTesting(nullptr);
 
-  ASSERT_EQ(1, observer.num_play_requests());
-  ASSERT_EQ(1, observer.num_stop_requests());
+  EXPECT_EQ(observer.num_play_requests(), 1);
+  EXPECT_EQ(observer.num_stop_requests(), 1);
 }
 
-TEST_P(AudioStreamHandlerTest, PlayWithLoop) {
-  if (is_bad()) {
-    return;
-  }
-
-  // Create a new handler with looping enabled.
-  CreateHandler(/*loop=*/true);
+TEST_P(AudioStreamHandlerTestWithParams, PlayWithLoop) {
+  std::unique_ptr<AudioStreamHandler> handler = CreateHandler(/*loop=*/true);
+  ASSERT_NE(handler, nullptr);
 
   base::RunLoop quit_run_loop;
   base::RunLoop render_run_loop;
@@ -175,43 +161,31 @@ TEST_P(AudioStreamHandlerTest, PlayWithLoop) {
           render_run_loop.Quit();
         }
       }));
-  SetObserverForTesting(&observer);
+  AudioStreamHandler::SetObserverForTesting(&observer);
 
-  ASSERT_TRUE(handler()->IsInitialized());
-  ASSERT_TRUE(handler()->Play());
+  ASSERT_TRUE(handler->IsInitialized());
+  ASSERT_TRUE(handler->Play());
 
   render_run_loop.Run();
 
-  handler()->Stop();
+  handler->Stop();
   // Wait until the playback is stopped before teardown.
   quit_run_loop.Run();
 
-  SetObserverForTesting(nullptr);
+  AudioStreamHandler::SetObserverForTesting(nullptr);
 
-  // The render callback is called 10 times (looping), but the play callback is
-  // called only once.
+  // The render callback is called 10 times (looping), but the play callback
+  // is called only once.
   EXPECT_EQ(observer.num_play_requests(), 1);
   EXPECT_EQ(observer.num_stop_requests(), 1);
 }
 
-TEST_P(AudioStreamHandlerTest, BadDataDoesNotInitialize) {
-  if (!is_bad()) {
-    return;
-  }
-  // The class members and SetUp() will be ignored for this test. Create a
-  // handler on the stack with some bad WAV or FLAC data.
-  EXPECT_FALSE(handler()->IsInitialized());
-  EXPECT_FALSE(handler()->Play());
+INSTANTIATE_TEST_SUITE_P(,
+                         AudioStreamHandlerTestWithParams,
+                         ValuesIn(GetTestParams()),
+                         [](const TestParamInfo<TestParams>& info) {
+                           return info.param.test_suffix;
+                         });
 
-  // Call Stop() to ensure that there is no crash.
-  handler()->Stop();
-}
-
-INSTANTIATE_TEST_SUITE_P(Wav,
-                         AudioStreamHandlerTest,
-                         testing::ValuesIn(kTestParamsWav));
-INSTANTIATE_TEST_SUITE_P(Flac,
-                         AudioStreamHandlerTest,
-                         testing::ValuesIn(kTestParamsFlac));
-
+}  // namespace
 }  // namespace audio
