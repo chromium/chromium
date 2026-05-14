d890a12afdea11ee14601ba0ce5f87cf17c8749f
Sayed Elabady <elabadysayed@chromium.org>
2026-05-14 01:09:19 -0700

    Revert "[Media] Re-enable AutoPictureInPictureTabHelperTest on Desktop Freeform"
    This reverts commit 10b6177d49fd649d8e644d71c1b25042369a5066.
    
    Reason for revert: This is causing failures to android-arm-tests, example -> https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-tests/37655/overview
    
    Original change's description:
    > [Media] Re-enable AutoPictureInPictureTabHelperTest on Desktop Freeform
    >
    > Re-enable testHideAutoPip and testBackToTabPostHideTimeRecorded in
    > AutoPictureInPictureTabHelperTest on DESKTOP_FREEFORM.
    >
    > Extensive local verification (30 runs) using a true Desktop Freeform
    > AVD and a desktop-configured build shows that these tests are now 100%
    > stable (31/31 successful runs for all tests in the class).
    >
    > Bug: 511288544
    > Change-Id: Ie55fbcfb1bf074935834c001cbc451bc957e102f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7843952
    > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > Commit-Queue: Phil Yan <philyan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1630099}
    
    Bug: 511288544
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1af030d7fd57cfaeafced74b7f517637fa2a7186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849033
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Owners-Override: Sayed Elabady <elabadysayed@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1630531}

--- chrome/browser/picture_in_picture/test/android/java/src/org/chromium/chrome/browser/picture_in_picture/AutoPictureInPictureTabHelperTest.java
+++ chrome/browser/picture_in_picture/test/android/java/src/org/chromium/chrome/browser/picture_in_picture/AutoPictureInPictureTabHelperTest.java
@@ -178,6 +178,7 @@ public class AutoPictureInPictureTabHelperTest {
 
     @Test
     @MediumTest
+    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511288544
     public void testHideAutoPip() throws TimeoutException {
         WebContents webContents = loadUrlAndInitializeForTest(AUTO_PIP_VIDEO_PAGE);
         Tab originalTab = mPage.getTab();
@@ -542,6 +543,7 @@ public class AutoPictureInPictureTabHelperTest {
 
     @Test
     @MediumTest
+    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511288544
     public void testBackToTabPostHideTimeRecorded() throws TimeoutException {
         WebContents webContents = loadUrlAndInitializeForTest(AUTO_PIP_VIDEO_PAGE);
         Tab originalTab = mPage.getTab();
