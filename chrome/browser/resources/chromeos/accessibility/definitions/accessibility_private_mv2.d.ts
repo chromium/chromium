a8074938b23d870c24ee331b93721874c8320846
Kyle Williams <kdgwill@google.com>
2026-05-13 20:28:34 -0700

    Revert "[cfm-a11y]: remove chrome.accessibilityPrivate.onTwoFingerTouch* apis"
    This reverts commit b111267dbdcb455a52d655aa31e77f5743deb1e7.
    
    Reason for revert: Variety of accessibility issues are occurring
    * crbug.com/361500636 - Two-finger long press does not enable a11y
    * crbug.com/362429272 - Two-finger long press stateful issue
    
    AX-Relnotes: n/a.
    
    Bug: b:510325488
    Test: manually by CFM team
    
    Change-Id: I3cd4013bfff99a19c9e82c60140763f2aa74ae6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7828241
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630415}

--- chrome/browser/resources/chromeos/accessibility/definitions/accessibility_private_mv2.d.ts
+++ chrome/browser/resources/chromeos/accessibility/definitions/accessibility_private_mv2.d.ts
@@ -523,6 +523,10 @@ declare global {
       export const onAccessibilityGesture:
           ChromeEvent<(gesture: Gesture, x: number, y: number) => void>;
 
+      export const onTwoFingerTouchStart: ChromeEvent<() => void>;
+
+      export const onTwoFingerTouchStop: ChromeEvent<() => void>;
+
       export const onSelectToSpeakContextMenuClicked: ChromeEvent<() => void>;
 
       export const onSelectToSpeakFocusChanged:
