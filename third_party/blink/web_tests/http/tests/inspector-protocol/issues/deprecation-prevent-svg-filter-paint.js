e587e670941adea500542d7acce48827a4b7395a
Ari Chivukula <arichiv@chromium.org>
2026-05-14 13:14:43 -0700

    [SVG] Simplify expected output for deprecation test
    Whether or not the line number is available changes with whether the
    frame is treated as remote or local, so we should bypass that problem
    and just highlight the important parts.
    
    Bug: 476646486
    Change-Id: I6c663259ced3e8d410cb17d51037d8af720d7df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849826
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630742}

--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/deprecation-prevent-svg-filter-paint.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/deprecation-prevent-svg-filter-paint.js
@@ -2,9 +2,10 @@
   const { page, session, dp } = await testRunner.startBlank(`Tests that deprecation issues are reported for svg filters on restricted content`);
   await dp.Network.enable();
   await dp.Audits.enable();
-  const promise = dp.Audits.onceIssueAdded();
+  const promise = dp.Audits.onceIssueAdded(issue => issue.params.issue.code == 'DeprecationIssue');
   page.navigate('https://devtools.test:8443/inspector-protocol/issues/resources/svg-sandbox-iframe.html');
   const result = await promise;
-  testRunner.log(result.params, "Inspector issue: ");
+  const issue = result.params.issue.details.deprecationIssueDetails;
+  testRunner.log(`Inspector issue type ${issue.type} on url ${issue.sourceCodeLocation.url}`)
   testRunner.completeTest();
 })
