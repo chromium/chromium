63b9fa37c07f261a4890a31b7246e126abacf57e
James Cook <jamescook@chromium.org>
2026-05-14 13:08:17 -0700

    extensions: Update ChromeContentRendererClientTest for desktop Android
    The test was using ENABLE_EXTENSIONS instead of ENABLE_EXTENSIONS_CORE.
    Also remove a bunch of unused code and includes.
    
    Bug: 469417243
    Change-Id: I3469a75fa755d9a9644dc92bd15d54a2b1477ebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7846958
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1630735}

--- chrome/renderer/chrome_content_renderer_client_unittest.cc
+++ chrome/renderer/chrome_content_renderer_client_unittest.cc
@@ -4,8 +4,6 @@
 
 #include "chrome/renderer/chrome_content_renderer_client.h"
 
-#include <stddef.h>
-
 #include <map>
 #include <memory>
 #include <string>
@@ -13,35 +11,15 @@
 #include <vector>
 
 #include "base/command_line.h"
-#include "base/values.h"
 #include "build/build_config.h"
 #include "content/public/common/content_switches.h"
 #include "extensions/buildflags/buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/platform/web_string.h"
-#include "third_party/blink/public/platform/web_url_response.h"
-#include "url/gurl.h"
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #include "chrome/common/extensions/extension_test_util.h"
-#include "extensions/common/extension.h"
-#include "extensions/common/extension_builder.h"
 #include "extensions/common/extensions_client.h"
-#include "extensions/common/manifest_constants.h"
 #endif
-
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-using extensions::mojom::ManifestLocation;
-#endif
-
-namespace {
-
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-const bool kNotHostedApp = false;
-const bool kHostedApp = true;
-#endif
-
-}  // namespace
 
 class ChromeContentRendererClientTest : public testing::Test {
  public:
@@ -53,51 +31,7 @@ class ChromeContentRendererClientTest : public testing::Test {
   }
 };
 
-#if BUILDFLAG(ENABLE_EXTENSIONS)
-scoped_refptr<const extensions::Extension> CreateTestExtension(
-    ManifestLocation location,
-    bool is_from_webstore,
-    bool is_hosted_app,
-    const std::string& app_url) {
-  int flags = is_from_webstore ?
-      extensions::Extension::FROM_WEBSTORE:
-      extensions::Extension::NO_FLAGS;
-
-  base::DictValue manifest;
-  manifest.Set("name", "NaCl Extension");
-  manifest.Set("version", "1");
-  manifest.Set("manifest_version", 2);
-  if (is_hosted_app) {
-    base::ListValue url_list;
-    url_list.Append(app_url);
-    manifest.SetByDottedPath(extensions::manifest_keys::kWebURLs,
-                             std::move(url_list));
-    manifest.SetByDottedPath(extensions::manifest_keys::kLaunchWebURL, app_url);
-  }
-  std::u16string error;
-  return extensions::Extension::Create(base::FilePath(), location, manifest,
-                                       flags, &error);
-}
-
-scoped_refptr<const extensions::Extension> CreateExtension(
-    bool is_from_webstore) {
-  return CreateTestExtension(ManifestLocation::kInternal, is_from_webstore,
-                             kNotHostedApp, std::string());
-}
-
-scoped_refptr<const extensions::Extension> CreateExtensionWithLocation(
-    ManifestLocation location,
-    bool is_from_webstore) {
-  return CreateTestExtension(
-      location, is_from_webstore, kNotHostedApp, std::string());
-}
-
-scoped_refptr<const extensions::Extension> CreateHostedApp(
-    bool is_from_webstore, const std::string& app_url) {
-  return CreateTestExtension(ManifestLocation::kInternal, is_from_webstore,
-                             kHostedApp, app_url);
-}
-
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 TEST_F(ChromeContentRendererClientTest, ExtensionsClientInitialized) {
   auto* extensions_client = extensions::ExtensionsClient::Get();
   ASSERT_TRUE(extensions_client);
@@ -106,9 +40,9 @@ TEST_F(ChromeContentRendererClientTest, ExtensionsClientInitialized) {
   const auto& map =
       extensions_client->GetFeatureDelegatedAvailabilityCheckMap();
   EXPECT_TRUE(!map.empty());
-  for (const auto* feature :
+  for (const char* feature :
        extension_test_util::GetExpectedDelegatedFeaturesForTest()) {
-    EXPECT_EQ(1u, map.count(feature));
+    EXPECT_EQ(1u, map.count(feature)) << feature;
   }
 }
-#endif  // BUILDFLAG(ENABLE_EXTENSIONS)
+#endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
