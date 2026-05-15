f9f3936c233822b3021c67062156bf4717dae5af
Maggie Chen <magchen@chromium.org>
2026-05-15 10:19:50 -0700

    Add experiments for CADisplayLinkInBrowser in fieldtrial_testing_config.json
    This CL adds two new experiments for CADisplayLinkInBrowser:
    1. EnabledInBrowser: Enables CADisplayLinkInBrowser and disables
       CADisplayLinkInGpuThenBrowser.
    2. EnabledInGpuThenBrowser: Enables both CADisplayLinkInBrowser and
       CADisplayLinkInGpuThenBrowser. This tests starting CADisplayLink
       in the GPU process and switching to the Browser process on certain
       events.
    
    Bug: 345275139
    Change-Id: Ia510ba48d541c3ab3bf2c8eae60aa3b009384a5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7847971
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631386}

--- ui/display/mac/ca_display_link_mac.mm
+++ ui/display/mac/ca_display_link_mac.mm
@@ -40,7 +40,7 @@ API_AVAILABLE(macos(14.0))
 
 namespace ui {
 
-BASE_FEATURE(kCADisplayLinkInGpuThenBrowser, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kCADisplayLinkInGpuThenBrowser, base::FEATURE_DISABLED_BY_DEFAULT);
 
 namespace {
 struct CADisplayLinkGlobals {
