182cafcda3a82051605fc7f84d2406bb40b82874
Yixi Wu <yixiwu@google.com>
2026-05-15 08:00:54 -0700

    [iOS] Add EG Test for Sharesheet File Download
    Added EG Test to ensure sharing works as intended with the Enterprise
    Download Protection feature. Added EG Tests to make sure the feature is
    working as intended when scan result is:
      1. Allow: Sharesheet open normally.
      2. Block: Download is blocked and user cannot open the sharesheet.
      3. Warn: Sharesheet open normally if user decides to bypass or
               Download is blocked and sharesheet will not open if user
               decides to cancel.
    
    Bug: 505352059
    Change-Id: I246fe4324175dd9d9db49f29f0540db5330f95dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7841353
    Commit-Queue: Yixi Wu <yixiwu@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Xu Li <mxlg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1631279}

--- ios/chrome/browser/sharing/ui_bundled/share_file_download_egtest.mm
+++ ios/chrome/browser/sharing/ui_bundled/share_file_download_egtest.mm
@@ -5,6 +5,11 @@
 #import <UIKit/UIKit.h>
 
 #import "base/test/ios/wait_util.h"
+#import "components/enterprise/connectors/core/cloud_content_scanning/upload_request_test_server.h"
+#import "components/safe_browsing/core/common/safebrowsing_switches.h"
+#import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/enterprise/connectors/analysis/test/analysis_connectors_app_interface.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -12,6 +17,7 @@
 #import "ios/components/enterprise/analysis/features.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "net/test/embedded_test_server/embedded_test_server.h"
+#import "ui/base/l10n/l10n_util_mac.h"
 
 NSString* const kPNGFilename = @"chromium_logo";
 
@@ -35,20 +41,78 @@ using base::test::ios::WaitUntilConditionOrTimeout;
 @interface ShareFileDownloadTestCase : ChromeTestCase
 @end
 
-@implementation ShareFileDownloadTestCase
+@implementation ShareFileDownloadTestCase {
+  std::unique_ptr<enterprise_connectors::test::UploadRequestTestServer>
+      _uploadServer;
+}
 
 - (void)setUp {
+  if ([self isEnterpriseDownloadTest]) {
+    _uploadServer = std::make_unique<
+        enterprise_connectors::test::UploadRequestTestServer>();
+    // Start must be called before setUp as the server URL is passed in the
+    // APPLaunchConfiguration.
+    if (!_uploadServer->Start()) {
+      // Use NOTREACHED() instead of GREYAssertTrue because GREYAssertTrue can
+      // only be used after calling the -setUp method of the super class.
+      NOTREACHED();
+    }
+  }
   [super setUp];
   GREYAssertTrue(self.testServer->Start(), @"Server did not start.");
 }
 
+- (void)tearDownHelper {
+  [AnalysisConnectorsAppInterface clearBrowserDMToken];
+  [AnalysisConnectorsAppInterface clearDownloadProtectionRules];
+  [super tearDownHelper];
+}
+
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
-  if ([self isRunningTest:@selector(testEnterpriseDLPEnabledOpenInPDF)]) {
+  if ([self isEnterpriseDownloadTest]) {
     config.features_enabled.push_back(
         enterprise_connectors::kEnableFileDownloadConnectorIOS);
+    config.additional_args.push_back(base::StrCat(
+        {"--", safe_browsing::switches::kCloudBinaryUploadServiceUrlFlag, "=",
+         _uploadServer->GetServiceURL().spec()}));
+  }
+  if ([self isEnterpriseDownloadAllowTest]) {
+    _uploadServer->SetScanResultSuccess();
+  } else if ([self isEnterpriseDownloadWarnTest]) {
+    _uploadServer->SetScanResultWarn();
+  } else if ([self isEnterpriseDownloadBlockTest]) {
+    _uploadServer->SetScanResultBlock();
   }
   return config;
+}
+
+- (bool)isEnterpriseDownloadTest {
+  return
+      [self isRunningTest:@selector(testEnterpriseDLPEnabledOpenInPDF)] ||
+      [self isRunningTest:@selector
+            (testEnterpriseDLPEnabledOpenInPDFScanSucceed)] ||
+      [self isRunningTest:@selector(testEnterpriseDLPEnabledOpenInPDFBlock)] ||
+      [self isRunningTest:@selector
+            (testEnterpriseDLPEnabledOpenInPDFWarnProceed)] ||
+      [self
+          isRunningTest:@selector(testEnterpriseDLPEnabledOpenInPDFWarnCancel)];
+}
+
+- (bool)isEnterpriseDownloadAllowTest {
+  return [self
+      isRunningTest:@selector(testEnterpriseDLPEnabledOpenInPDFScanSucceed)];
+}
+
+- (bool)isEnterpriseDownloadWarnTest {
+  return [self isRunningTest:@selector
+               (testEnterpriseDLPEnabledOpenInPDFWarnProceed)] ||
+         [self isRunningTest:@selector
+               (testEnterpriseDLPEnabledOpenInPDFWarnCancel)];
+}
+
+- (bool)isEnterpriseDownloadBlockTest {
+  return [self isRunningTest:@selector(testEnterpriseDLPEnabledOpenInPDFBlock)];
 }
 
 #pragma mark - Public
@@ -86,6 +150,100 @@ using base::test::ios::WaitUntilConditionOrTimeout;
   // Check that tapping on the Cancel button closes the activity menu and hides
   // the open in toolbar.
   [ChromeEarlGrey closeActivitySheet];
+  [ChromeEarlGrey verifyActivitySheetNotVisible];
+}
+
+// Tests that for Enterprise user, when the scan result for the file is
+// successful, the activity sheet will open normally.
+- (void)testEnterpriseDLPEnabledOpenInPDFScanSucceed {
+  [AnalysisConnectorsAppInterface setDownloadProtectionRules];
+  [AnalysisConnectorsAppInterface setBrowserDMToken];
+
+  // Open the activity menu.
+  [ChromeEarlGrey loadURL:self.testServer->GetURL(kPDFPath)];
+  [self openActivityMenu];
+
+  [ChromeEarlGrey verifyActivitySheetVisible];
+
+  // Check that tapping on the Cancel button closes the activity menu and hides
+  // the open in toolbar.
+  [ChromeEarlGrey closeActivitySheet];
+  [ChromeEarlGrey verifyActivitySheetNotVisible];
+}
+
+// Tests that for Enterprise user, when the scan result for the file is block,
+// the activity sheet will not open and a snackbar message will show.
+- (void)testEnterpriseDLPEnabledOpenInPDFBlock {
+  [AnalysisConnectorsAppInterface setDownloadProtectionRules];
+  [AnalysisConnectorsAppInterface setBrowserDMToken];
+
+  // Open the activity menu.
+  [ChromeEarlGrey loadURL:self.testServer->GetURL(kPDFPath)];
+  [self openActivityMenu];
+
+  // Wait for download to finish and check that the snackbar is shown.
+  id<GREYMatcher> snackbarMessage = grey_text(l10n_util::GetNSString(
+      IDS_IOS_ENTERPRISE_FILE_SHARE_BLOCKED_SNACKBAR_TEXT));
+  [ChromeEarlGrey waitForUIElementToAppearWithMatcher:snackbarMessage
+                                              timeout:kWaitForDownloadTimeout];
+
+  // Check that the activity sheet will not open.
+  [ChromeEarlGrey verifyActivitySheetNotVisible];
+}
+
+// Tests that for Enterprise user, when the scan result for the file is warn and
+// user chooses to proceed anyway, the activity sheet will open normally.
+- (void)testEnterpriseDLPEnabledOpenInPDFWarnProceed {
+  [AnalysisConnectorsAppInterface setDownloadProtectionRules];
+  [AnalysisConnectorsAppInterface setBrowserDMToken];
+
+  // Open the activity menu.
+  [ChromeEarlGrey loadURL:self.testServer->GetURL(kPDFPath)];
+  [self openActivityMenu];
+
+  // Wait for download to finish and the dialog to show up.
+  [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
+                      chrome_test_util::AlertItemWithAccessibilityLabelId(
+                          IDS_IOS_ENTERPRISE_FILE_DOWNLOAD_WARN_CONTINUE_BUTTON)
+                                              timeout:kWaitForDownloadTimeout];
+
+  // Tap the "Continue" button on the warning dialog.
+  [[EarlGrey selectElementWithMatcher:
+                 chrome_test_util::AlertItemWithAccessibilityLabelId(
+                     IDS_IOS_ENTERPRISE_FILE_DOWNLOAD_WARN_CONTINUE_BUTTON)]
+      performAction:grey_tap()];
+
+  // Test that activity sheet can be open normally.
+  [ChromeEarlGrey verifyActivitySheetVisible];
+
+  // Check that tapping on the Cancel button closes the activity menu and hides
+  // the open in toolbar.
+  [ChromeEarlGrey closeActivitySheet];
+  [ChromeEarlGrey verifyActivitySheetNotVisible];
+}
+
+// Tests that for Enterprise user, when the scan result for the file is warn and
+// user chooses to cancel, the activity sheet will not open.
+- (void)testEnterpriseDLPEnabledOpenInPDFWarnCancel {
+  [AnalysisConnectorsAppInterface setDownloadProtectionRules];
+  [AnalysisConnectorsAppInterface setBrowserDMToken];
+
+  // Open the activity menu.
+  [ChromeEarlGrey loadURL:self.testServer->GetURL(kPDFPath)];
+  [self openActivityMenu];
+
+  // Wait for download to finish and the dialog to show up.
+  [ChromeEarlGrey
+      waitForUIElementToAppearWithMatcher:
+          chrome_test_util::AlertItemWithAccessibilityLabelId(IDS_CANCEL)
+                                  timeout:kWaitForDownloadTimeout];
+
+  // Tap the "Cancel" button on the warning dialog.
+  [[EarlGrey selectElementWithMatcher:
+                 chrome_test_util::AlertItemWithAccessibilityLabelId(
+                     IDS_CANCEL)] performAction:grey_tap()];
+
+  // Check that the activity sheet will not open.
   [ChromeEarlGrey verifyActivitySheetNotVisible];
 }
 
