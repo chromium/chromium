95efe1e7971dbc93886ca5bc2cacb58750881f25
Christoph Schwering <schwering@google.com>
2026-05-15 08:21:00 -0700

    [Passwords] Reinstate CHECK_EQ() instead of CHECK()
    crrev.com/c/7849414 had replaced a CHECK_EQ() with a CHECK(). The
    reason that CHECK_EQ() didn't work was a missing include of
    base/strings/utf_ostream_operators.h that gives us
      std::ostream& operator<<(std::ostream&, const std::u16string&)
    used in
    https://chromium.googlesource.com/chromium/src/+/main/base/check_op.h#87
    
    Bug: 489636471
    Change-Id: I017ff5a723c7c46d63d1139dad702d8a9067283e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849082
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631292}

--- chrome/browser/ui/passwords/bubble_controllers/save_update_bubble_controller.cc
+++ chrome/browser/ui/passwords/bubble_controllers/save_update_bubble_controller.cc
@@ -5,12 +5,12 @@
 #include "chrome/browser/ui/passwords/bubble_controllers/save_update_bubble_controller.h"
 
 #include <algorithm>
-#include <string>
 
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/functional/bind.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/strings/utf_ostream_operators.h"
 #include "base/time/default_clock.h"
 #include "chrome/browser/password_manager/factories/password_counter_factory.h"
 #include "chrome/browser/password_manager/factories/profile_password_store_factory.h"
@@ -127,7 +127,7 @@ SaveUpdateBubbleController::SaveUpdateBubbleController(
     const password_manager::InteractionsStats* stats =
         delegate_->GetCurrentInteractionStats();
     if (stats) {
-      CHECK(interaction_stats_.username_value == stats->username_value);
+      CHECK_EQ(interaction_stats_.username_value, stats->username_value);
       CHECK_EQ(interaction_stats_.origin_domain, stats->origin_domain);
       interaction_stats_.dismissal_count = stats->dismissal_count;
     }
