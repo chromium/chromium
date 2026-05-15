b4e1abbe6358b3cb431ebb82abdcc6a7352619e1
Christoph Schwering <schwering@google.com>
2026-05-15 04:59:21 -0700

    [Autofill] Fix missing include in card_name_fix_flow_controller_impl.cc
    Bug: 489636471
    Change-Id: I99803440497ef1aedc51cce58557f0ad79e38946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7852455
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Commit-Queue: Side YILMAZ <sideyilmaz@chromium.org>
    Owners-Override: Side Yilmaz <sideyilmaz@google.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631202}

--- components/autofill/core/browser/ui/payments/card_name_fix_flow_controller_impl.cc
+++ components/autofill/core/browser/ui/payments/card_name_fix_flow_controller_impl.cc
@@ -16,6 +16,7 @@
 #include "components/autofill/core/browser/metrics/payments/credit_card_save_metrics.h"
 #include "components/autofill/core/browser/payments/payments_autofill_client.h"
 #include "components/autofill/core/browser/ui/payments/card_name_fix_flow_view.h"
+#include "components/grit/components_scaled_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
