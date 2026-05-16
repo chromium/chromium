4e41bc298da7f6c1f7319773ba918da4b9e9b5ed
Ari Chivukula <arichiv@chromium.org>
2026-05-15 20:23:25 -0700

    [SVG] Deprecation issue isn't set on same-origin frames
    We should ensure that we don't see the issue shared in same-origin
    frames unless they are restricted in some way. The promise grabs the
    first issue seen and the current expectations verifies that it's sent
    from the second page's URL and not the first.
    
    Bug: 476646486
    Change-Id: I8967bc47967a8c7fd002a153061496e916275855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849862
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1631709}

--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/deprecation-prevent-svg-filter-paint.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/deprecation-prevent-svg-filter-paint.js
@@ -1,8 +1,9 @@
 (async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
-  const { page, session, dp } = await testRunner.startBlank(`Tests that deprecation issues are reported for svg filters on restricted content`);
+  const { page, session, dp } = await testRunner.startBlank(`Tests that deprecation issues are not reported on same-origin iframes but are reports on sandboxed iframes`);
   await dp.Network.enable();
   await dp.Audits.enable();
   const promise = dp.Audits.onceIssueAdded(issue => issue.params.issue.code == 'DeprecationIssue');
+  page.navigate('https://devtools.test:8443/inspector-protocol/issues/resources/svg-iframe.html');
   page.navigate('https://devtools.test:8443/inspector-protocol/issues/resources/svg-sandbox-iframe.html');
   const result = await promise;
   const issue = result.params.issue.details.deprecationIssueDetails;
