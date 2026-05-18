c714c238659cf824d7414b1efaf817af26ca6ace
Daniel Rubery <drubery@chromium.org>
2026-05-18 11:32:41 -0700

    [bedrock] Remove BrowserWithTestWindowTest from session_data_service_unittest
    By manually informing the GlobalBrowserCollection of
    MockBrowserWindowInterface construction, we can test the same code.
    
    Fixed: 513637238
    Change-Id: Ieb268e200812648558df1fe9dbe3e060ae8cd549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7848407
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632302}

--- chrome/browser/sessions/session_data_service_unittest.cc
+++ chrome/browser/sessions/session_data_service_unittest.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/sessions/session_data_service.h"
 
+#include <algorithm>
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
@@ -14,17 +15,22 @@
 #include "chrome/browser/extensions/extension_special_storage_policy.h"
 #include "chrome/browser/lifetime/browser_shutdown.h"
 #include "chrome/browser/sessions/session_data_deleter.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface_iterator.h"
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/browser/ui/browser_window/public/global_browser_collection_platform_delegate.h"
+#include "chrome/browser/ui/browser_window/test/mock_browser_window_interface.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
 #include "components/content_settings/core/browser/cookie_settings.h"
 #include "components/content_settings/core/common/content_settings.h"
+#include "content/public/test/browser_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using testing::_;
 using testing::Mock;
+using testing::Return;
 using testing::StrictMock;
 
 namespace {
@@ -41,22 +47,56 @@ void RunCallback(bool skip_session_cookies, base::OnceClosure callback) {
 }
 }  // namespace
 
-class SessionDataServiceTest : public BrowserWithTestWindowTest {
+class SessionDataServiceTest : public testing::Test {
  public:
   void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
-    auto cookie_settings = CookieSettingsFactory::GetForProfile(profile());
+    profile_manager_ =
+        TestingBrowserProcess::GetGlobal()->SetUpGlobalFeaturesForTesting(
+            /*profile_manager=*/true);
+    profile_ = profile_manager_->CreateTestingProfile("test_profile");
+
+    auto cookie_settings = CookieSettingsFactory::GetForProfile(profile_);
     cookie_settings->SetDefaultCookieSetting(CONTENT_SETTING_SESSION_ONLY);
-    profile()->SetExtensionSpecialStoragePolicy(
+    profile_->SetExtensionSpecialStoragePolicy(
         base::MakeRefCounted<ExtensionSpecialStoragePolicy>(
             cookie_settings.get()));
+
+    // Simulate an initial browser window open, matching the initial state
+    // previously provided by BrowserWithTestWindowTest.
+    initial_browser_ = CreateMockBrowser(profile_);
+    BrowserCollectionObserver* observer =
+        GlobalBrowserCollection::GetInstance()->GetPlatformDelegate();
+    observer->OnBrowserCreated(initial_browser_.get());
+
     RestartService(CreateDeleter());
   }
 
   void TearDown() override {
     session_data_service_.reset();
     browser_shutdown::SetTryingToQuit(false);
-    BrowserWithTestWindowTest::TearDown();
+
+    // Ensure the initial browser is safely closed and removed from the global
+    // collection if the individual test case didn't already close it.
+    auto browsers = GetAllBrowserWindowInterfaces();
+    if (std::ranges::find(browsers, initial_browser_.get()) != browsers.end()) {
+      BrowserCollectionObserver* observer =
+          GlobalBrowserCollection::GetInstance()->GetPlatformDelegate();
+      observer->OnBrowserClosed(initial_browser_.get());
+    }
+    initial_browser_.reset();
+
+    profile_ = nullptr;
+    profile_manager_ = nullptr;
+    TestingBrowserProcess::GetGlobal()->TearDownGlobalFeaturesForTesting();
+  }
+
+  std::unique_ptr<StrictMock<MockBrowserWindowInterface>> CreateMockBrowser(
+      Profile* profile) {
+    auto browser = std::make_unique<StrictMock<MockBrowserWindowInterface>>();
+    EXPECT_CALL(*browser, GetProfile()).WillRepeatedly(Return(profile));
+    EXPECT_CALL(*browser, GetType())
+        .WillRepeatedly(Return(BrowserWindowInterface::TYPE_NORMAL));
+    return browser;
   }
 
   std::unique_ptr<StrictMock<TestSessionDataDeleter>> CreateDeleter() {
@@ -75,6 +115,17 @@ class SessionDataServiceTest : public BrowserWithTestWindowTest {
     return session_data_deleter_;
   }
   SessionDataService* service() { return session_data_service_.get(); }
+  TestingProfile* profile() { return profile_; }
+  TestingProfileManager* profile_manager() { return profile_manager_; }
+  MockBrowserWindowInterface* initial_browser() {
+    return initial_browser_.get();
+  }
+
+ protected:
+  content::BrowserTaskEnvironment task_environment_;
+  raw_ptr<TestingProfileManager> profile_manager_ = nullptr;
+  raw_ptr<TestingProfile> profile_ = nullptr;
+  std::unique_ptr<StrictMock<MockBrowserWindowInterface>> initial_browser_;
 
  private:
   raw_ptr<StrictMock<TestSessionDataDeleter>, DanglingUntriaged>
@@ -91,16 +142,19 @@ TEST_F(SessionDataServiceTest, StartCleanup) {
 TEST_F(SessionDataServiceTest, CleanupOnWindowClosed) {
   EXPECT_EQ(1U, GlobalBrowserCollection::GetInstance()->GetSize());
 
-  auto new_browser = CreateBrowser(profile(), Browser::TYPE_NORMAL, false);
+  auto new_browser = CreateMockBrowser(profile());
+  BrowserCollectionObserver* observer =
+      GlobalBrowserCollection::GetInstance()->GetPlatformDelegate();
+  observer->OnBrowserCreated(new_browser.get());
   EXPECT_EQ(2U, GlobalBrowserCollection::GetInstance()->GetSize());
 
-  new_browser.reset();
+  observer->OnBrowserClosed(new_browser.get());
   EXPECT_EQ(1U, GlobalBrowserCollection::GetInstance()->GetSize());
   Mock::VerifyAndClearExpectations(deleter());
 
   bool skip_session_cookies = browser_defaults::kBrowserAliveWithNoWindows;
   EXPECT_CALL(*deleter(), DeleteSessionOnlyData(skip_session_cookies, _));
-  release_browser();
+  observer->OnBrowserClosed(initial_browser());
   EXPECT_EQ(0U, GlobalBrowserCollection::GetInstance()->GetSize());
   Mock::VerifyAndClearExpectations(deleter());
 }
@@ -109,21 +163,29 @@ TEST_F(SessionDataServiceTest, CleanupOnWindowClosedWithOtherProfileOpen) {
   EXPECT_EQ(1U, GlobalBrowserCollection::GetInstance()->GetSize());
 
   auto* new_profile = profile_manager()->CreateTestingProfile("second_profile");
-  auto new_browser = CreateBrowser(new_profile, Browser::TYPE_NORMAL, false);
+  auto new_browser = CreateMockBrowser(new_profile);
+  BrowserCollectionObserver* observer =
+      GlobalBrowserCollection::GetInstance()->GetPlatformDelegate();
+  observer->OnBrowserCreated(new_browser.get());
   EXPECT_EQ(2U, GlobalBrowserCollection::GetInstance()->GetSize());
 
   bool skip_session_cookies = browser_defaults::kBrowserAliveWithNoWindows;
   EXPECT_CALL(*deleter(), DeleteSessionOnlyData(skip_session_cookies, _));
-  release_browser();
+  observer->OnBrowserClosed(initial_browser());
   EXPECT_EQ(1U, GlobalBrowserCollection::GetInstance()->GetSize());
   Mock::VerifyAndClearExpectations(deleter());
+
+  observer->OnBrowserClosed(new_browser.get());
 }
 
 TEST_F(SessionDataServiceTest, RepeatCleanupAfterNewWindowOpened) {
+  BrowserCollectionObserver* observer =
+      GlobalBrowserCollection::GetInstance()->GetPlatformDelegate();
+
   // Close browser and expect cleanup.
   bool skip_session_cookies = browser_defaults::kBrowserAliveWithNoWindows;
   EXPECT_CALL(*deleter(), DeleteSessionOnlyData(skip_session_cookies, _));
-  release_browser();
+  observer->OnBrowserClosed(initial_browser());
   Mock::VerifyAndClearExpectations(deleter());
 
   // Additional requests for cleanup will be ignored.
@@ -131,13 +193,16 @@ TEST_F(SessionDataServiceTest, RepeatCleanupAfterNewWindowOpened) {
   Mock::VerifyAndClearExpectations(deleter());
 
   // Unless a new browser is opened.
-  auto new_browser = CreateBrowser(profile(), Browser::TYPE_NORMAL, false);
+  auto new_browser = CreateMockBrowser(profile());
+  observer->OnBrowserCreated(new_browser.get());
   Mock::VerifyAndClearExpectations(deleter());
 
   // And another cleanup is started.
   EXPECT_CALL(*deleter(), DeleteSessionOnlyData(false, _));
   service()->StartCleanup();
   Mock::VerifyAndClearExpectations(deleter());
+
+  observer->OnBrowserClosed(new_browser.get());
 }
 
 TEST_F(SessionDataServiceTest, SkipOnShutdown) {
