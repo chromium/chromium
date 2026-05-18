d35219bd51b2e40ced2696edd45a2a23376ed492
Chirag Arora <heychirag@google.com>
2026-05-18 03:08:21 -0700

    [iOS] call MetricsMediator when kMetricsReportingLevel changes
    This commit adds parallel changes for kMetricsReportingLevel that
    already exist for kMetricsReportingEnabled. We need to listen to changes
    in kMetricsReportingLevel and call
    updateMetricsStateBasedOnPrefsUserTriggered. This will only happen if
    MetricsReportingChoiceService::ShouldUseMetricsConsentRestructure is
    true.
    
    Bug: b:483043192
    Change-Id: I0d7dbbad04a10b0b3370fbf0f9a4f058c2b277e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7849514
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632044}

--- ios/chrome/app/main_controller.mm
+++ ios/chrome/app/main_controller.mm
@@ -34,6 +34,7 @@
 #import "components/component_updater/installer_policies/safety_tips_component_installer.h"
 #import "components/content_settings/core/browser/host_content_settings_map.h"
 #import "components/metrics/metrics_pref_names.h"
+#import "components/metrics/metrics_reporting_choice_service.h"
 #import "components/metrics/metrics_service.h"
 #import "components/password_manager/core/common/password_manager_features.h"
 #import "components/password_manager/core/common/passwords_directory_util_ios.h"
@@ -703,7 +704,7 @@ std::string GetProfileNameForChoice(ProfileChoice choice,
 
   CustomizeUIAppearance();
 
-  // Schedule the prefs observer init first to ensure kMetricsReportingEnabled
+  // Schedule the prefs observer init first to ensure the metrics reporting pref
   // is synced before starting uploads.
   [self schedulePrefObserverInitialization];
   [self scheduleCrashReportUpload];
@@ -1310,6 +1311,8 @@ std::string GetProfileNameForChoice(ProfileChoice choice,
   _localStatePrefObserverBridge->ObserveChangesForPreference(
       metrics::prefs::kMetricsReportingEnabled,
       &_localStatePrefChangeRegistrar);
+  _localStatePrefObserverBridge->ObserveChangesForPreference(
+      metrics::prefs::kMetricsReportingLevel, &_localStatePrefChangeRegistrar);
 
   // Calls the onPreferenceChanged function in case there was a change to the
   // observed preferences before the observer bridge was set up. However, if the
@@ -1324,9 +1327,14 @@ std::string GetProfileNameForChoice(ProfileChoice choice,
   // discarded, as would happen here if onPreferenceChanged was called while the
   // user was still on the welcome screen and did yet enable/disable metrics
   // reporting.
-  if (!localState->FindPreference(metrics::prefs::kMetricsReportingEnabled)
-           ->IsDefaultValue()) {
-    [self onPreferenceChanged:metrics::prefs::kMetricsReportingEnabled];
+  const std::string& prefName =
+      metrics::MetricsReportingChoiceService::
+              ShouldUseMetricsConsentRestructure(localState)
+          ? metrics::prefs::kMetricsReportingLevel
+          : metrics::prefs::kMetricsReportingEnabled;
+
+  if (!localState->FindPreference(prefName)->IsDefaultValue()) {
+    [self onPreferenceChanged:prefName];
   }
 }
 
@@ -1599,9 +1607,18 @@ std::string GetProfileNameForChoice(ProfileChoice choice,
 #pragma mark - Preferences Management
 
 - (void)onPreferenceChanged:(const std::string&)preferenceName {
-  // Turn on or off metrics & crash reporting when either preference changes.
-  if (preferenceName == metrics::prefs::kMetricsReportingEnabled) {
-    [_metricsMediator updateMetricsStateBasedOnPrefsUserTriggered:YES];
+  // Turn on or off metrics & crash reporting when the metrics reporting
+  // preference changes.
+  PrefService* localState = GetApplicationContext()->GetLocalState();
+  if (metrics::MetricsReportingChoiceService::
+          ShouldUseMetricsConsentRestructure(localState)) {
+    if (preferenceName == metrics::prefs::kMetricsReportingLevel) {
+      [_metricsMediator updateMetricsStateBasedOnPrefsUserTriggered:YES];
+    }
+  } else {
+    if (preferenceName == metrics::prefs::kMetricsReportingEnabled) {
+      [_metricsMediator updateMetricsStateBasedOnPrefsUserTriggered:YES];
+    }
   }
 }
 
