c030c897c2b4e65a97c8d14b07229c625eb79b35
Ziad Youssef <ziadyoussef@chromium.org>
2026-05-19 09:36:02 -0700

    Revert "Add experiment id for Standalone [1/2]"
    This reverts commit 75f92a7bb64f2ddd3b04eb5bacd5566e48e8b83b.
    
    Bug: 514593324
    Change-Id: I27bb4f2c96c86ca554ad859d30e24dc1af35554f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7857852
    Reviewed-by: Mateusz Zbikowski <zbikowski@google.com>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7778_178@{#5}
    Cr-Branched-From: d096af1c9e98c45c3596e59620622b1a049bfecb-refs/branch-heads/7778@{#3196}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- android_webview/browser/aw_browser_main_parts.cc
+++ android_webview/browser/aw_browser_main_parts.cc
@@ -190,7 +190,6 @@ std::vector<std::string> getIdsForWebViewApkType(const ApkType& apk_type) {
       gws_experiment_ids.push_back("3393840");
     }
   } else if (apk_type == ApkType::STANDALONE) {
-    gws_experiment_ids.push_back("3397229");
     gws_experiment_ids.push_back("3393823");
     if (channel == version_info::Channel::STABLE) {
       gws_experiment_ids.push_back("3393825");
