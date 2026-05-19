0a1c51ca10dcdde70681b26c7c7cbc37bb1ef3cb
Devi Varaprasad <jonnalad@google.com>
2026-05-18 19:44:39 -0700

    [149] [ntp-composebox] Create base ntp-composebox element
    Original change's description:
    > [ntp-composebox] Create base ntp-composebox element
    > 
    > Introduce `ntp-composebox`, a dedicated Web Component fork of the shared
    > `cr-composebox` for the New Tab Page. This allows custom behavior and
    > styling specific to the NTP UI while maintaining compatibility with the
    > underlying composebox mixin.
    > 
    > Specifically, this commit:
    > * Adds `ntp_composebox.ts`, along with its corresponding HTML template
    > and CSS styling, inheriting from `ComposeboxEmbedderMixin`.
    > * Integrates the new component into the main NTP app layout, guarded by
    > the `useNtpComposeboxFork_` property.
    > * Provides a temporary mock implementation for the dropdown interface to
    > prevent runtime crashes while full functionality is developed.
    > * Updates GN build files to include the new source files in the
    > compilation pipeline.
    > 
    > Screencast: http://screencast/cast/NjE1NTAwMjE2MTY1OTkwNHw1OTNhZmZhNC1lNA
    > Bug: b:512604360
    > Change-Id: Iff9b83f0c0907f8e52394cdd617c19d19108990c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7842003
    > Reviewed-by: Sho Mizoe <shomizoe@google.com>
    > Commit-Queue: Devi Varaprasad Jonnala <jonnalad@google.com>
    > Reviewed-by: Marlon Facey <mfacey@chromium.org>
    > Reviewed-by: Riley Tatum <rtatum@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1630983}
    
    Bug: 514063383
    Change-Id: Iff9b83f0c0907f8e52394cdd617c19d19108990c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7857629
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7827@{#1115}
    Cr-Branched-From: 9f3e9aaccba63bd2ec30334e45e0bfd07ebcc8f1-refs/heads/main@{#1625079}

---
+++ chrome/browser/resources/new_tab_page/ntp_composebox.ts
@@ -0,0 +1,106 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '//resources/cr_components/composebox/composebox_input.js';
+
+import type {PageHandlerRemote} from '//resources/cr_components/composebox/composebox.mojom-webui.js';
+import type {ComposeboxDropdownElement} from '//resources/cr_components/composebox/composebox_dropdown.js';
+import type {ComposeboxInputElement} from '//resources/cr_components/composebox/composebox_input.js';
+import {ComposeboxEmbedderMixin} from '//resources/cr_components/composebox/composebox_mixin.js';
+import {ComposeboxProxyImpl} from '//resources/cr_components/composebox/composebox_proxy.js';
+import {EventTracker} from '//resources/js/event_tracker.js';
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+import type {PageCallbackRouter as SearchboxPageCallbackRouter, PageHandlerRemote as SearchboxPageHandlerRemote} from '//resources/mojo/components/omnibox/browser/searchbox.mojom-webui.js';
+
+import {getCss} from './ntp_composebox.css.js';
+import {getHtml} from './ntp_composebox.html.js';
+
+export interface NtpComposeboxElement {
+  $: {
+    composeboxInput: ComposeboxInputElement,
+    composebox: HTMLElement,
+  };
+}
+
+export class NtpComposeboxElement extends ComposeboxEmbedderMixin
+(CrLitElement) {
+  static get is() {
+    return 'ntp-composebox';
+  }
+
+  static override get styles() {
+    return getCss();
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  private searchboxCallbackRouter_: SearchboxPageCallbackRouter;
+  private pageHandler_: PageHandlerRemote;
+  private searchboxHandler_: SearchboxPageHandlerRemote;
+  private eventTracker_: EventTracker = new EventTracker();
+
+  override getPageHandler(): PageHandlerRemote {
+    return this.pageHandler_;
+  }
+
+  override getSearchboxHandler(): SearchboxPageHandlerRemote {
+    return this.searchboxHandler_;
+  }
+
+  override getSearchboxCallbackRouter(): SearchboxPageCallbackRouter {
+    return this.searchboxCallbackRouter_;
+  }
+
+  override getActiveElement(): Element|null {
+    return this.shadowRoot?.activeElement || null;
+  }
+
+  override getInputElement(): ComposeboxInputElement {
+    return this.$.composeboxInput;
+  }
+
+  // Mock Object to prevent crashes.
+  // TODO(crbug.com/512604547): Implement dropdown element.
+  override getDropdownElement(): ComposeboxDropdownElement {
+    return {
+      unselect: () => {},
+      selectIndex: () => {},
+      focusSelected: () => {},
+    } as unknown as ComposeboxDropdownElement;
+  }
+
+  constructor() {
+    super();
+    this.pageHandler_ = ComposeboxProxyImpl.getInstance().handler;
+    this.searchboxCallbackRouter_ =
+        ComposeboxProxyImpl.getInstance().searchboxCallbackRouter;
+    this.searchboxHandler_ = ComposeboxProxyImpl.getInstance().searchboxHandler;
+  }
+
+  override connectedCallback() {
+    super.connectedCallback();
+    this.focusInput();
+  }
+
+  override disconnectedCallback() {
+    super.disconnectedCallback();
+    this.eventTracker_.removeAll();
+  }
+
+  protected computeCancelButtonTitle_() {
+    return this.input.trim().length > 0 || this.files.size > 0 ?
+        this.i18n('composeboxCancelButtonTitleInput') :
+        this.i18n('composeboxCancelButtonTitle');
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ntp-composebox': NtpComposeboxElement;
+  }
+}
+
+customElements.define(NtpComposeboxElement.is, NtpComposeboxElement);
