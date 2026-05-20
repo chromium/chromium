c43ee58ac4b8db5c1c90f36d6a2563071082f9b4
Alex Kravchuk <alexanderkr@google.com>
2026-05-20 01:30:47 -0700

    Revert "Reland "Use R8's resource shrinker""
    This reverts commit 0e0e8906509fd60b0354adc53860837c960bc59a.
    
    Reason for revert: suspected culprit for rel-ready failures
    
    Original change's description:
    > Reland "Use R8's resource shrinker"
    >
    > This reverts commit 8be28b90b97d525daee0891c76d9075e1aa5bffc.
    >
    > Reason for reland: tested chrome_bundle install and
    > chrome_test_apk --list-tests
    >
    > Original change's description:
    > > Revert "Use R8's resource shrinker"
    > >
    > > This reverts commit bc1ff55d24abcb3af7ad21c9cacf134ad88a2ce2.
    > >
    > > Reason for revert: Failing to list tests in some configs
    > > https://luci-milo.appspot.com/ui/p/chrome/builders/ci/android-x64-rel-ready/47402/overview
    > >
    > > Original change's description:
    > > > Use R8's resource shrinker
    > > >
    > > > This deletes our custom unused resources code, and uses an extra output
    > > > from R8 (shrunk.proto.ap_) to feed as input to optimize_resources.
    > > >
    > > > This also removes any seperately-applicable resource shrinking
    > > > (shortening names, stripping names, stripping unused) and just has a
    > > > single global gn arg (optimize_resources) which does all
    > > > optimizations.
    > > >
    > > > Based on some quick local benchmarks, it seems this adds ~10s to
    > > > !is_java_debug (versus having it off completely), so I think it's fine
    > > > to keep on !is_java_debug vs is_official_build.
    > > >
    > > > Bug: 362524381
    > > > Link: https://chromium-review.googlesource.com/id/I3984a4ddfbc665191fec7819ff897e666a6a6964
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614749
    > > > Owners-Override: Sam Maier <smaier@google.com>
    > > > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > > > Commit-Queue: Sam Maier <smaier@google.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1630915}
    > >
    > > Bug: 362524381
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Change-Id: I2f53d05e8915fe4ef9465a2f96432121bc328a6e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7850882
    > > Owners-Override: Sam Maier <smaier@google.com>
    > > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > > Auto-Submit: Sam Maier <smaier@google.com>
    > > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > > Cr-Commit-Position: refs/heads/main@{#1631241}
    >
    > Bug: 362524381
    > Link: https://chromium-review.googlesource.com/id/I34886a0dc3274f202dc434d66f41103d6a6a6964
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7854136
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1633088}
    
    Bug: 362524381
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I85aa24b8ce5710c9b7ceb53a60bcbec1770777bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7863361
    Auto-Submit: Alex Kravchuk <alexanderkr@google.com>
    Commit-Queue: Alex Kravchuk <alexanderkr@google.com>
    Owners-Override: Alex Kravchuk <alexanderkr@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1633416}

--- build/android/gyp/proguard.py
+++ build/android/gyp/proguard.py
@@ -161,10 +161,6 @@ def _ParseOptions():
       '--feature-jars',
       action='append',
       help='GN list of path to jars which comprise the corresponding feature.')
-  parser.add_argument('--feature-resources',
-                      action='append',
-                      help='List of feature resource zips to shrink: '
-                      'feature_name:proto_path:shrunk_proto_path')
   parser.add_argument(
       '--dex-dest',
       action='append',
@@ -215,12 +211,6 @@ def _ParseOptions():
       help='File to touch upon success. Mutually exclusive with --output-path')
   parser.add_argument('--desugared-library-keep-rule-output',
                       help='Path to desugared library keep rule output file.')
-
-  parser.add_argument('--resources-input',
-                      help='Path to resource proto file for the main module.')
-  parser.add_argument(
-      '--resources-output',
-      help='Path to optimized resource proto file for the main module.')
 
   diff_utils.AddCommandLineFlags(parser)
   options = parser.parse_args(args)
@@ -284,17 +274,6 @@ def _ParseOptions():
           parser.error('"%s" referenced in --uses-split not present.' % name)
       split_map[child] = parent
   options.uses_split = split_map
-
-  feature_resources = {}
-  if options.feature_resources:
-    for feat_res in options.feature_resources:
-      feat_name, proto_path, shrunk_proto_path = feat_res.split(':')
-      if feat_name not in options.feature_names:
-        parser.error(
-            '"%s" referenced in --feature-resources not present in features.' %
-            feat_name)
-      feature_resources[feat_name] = (proto_path, shrunk_proto_path)
-  options.feature_resources = feature_resources
 
   return options
 
@@ -459,26 +438,11 @@ def _OptimizeWithR8(options, config_paths, libraries, dynamic_config_data):
           options.input_art_profile,
       ]
 
-    if options.resources_input:
-      cmd += [
-          '--android-resources', options.resources_input,
-          options.resources_output
-      ]
-
     for split_context in split_contexts_by_name.values():
       if split_context is base_context:
         continue
-      sorted_jars = sorted(split_context.input_jars)
-      for in_jar in sorted_jars:
+      for in_jar in sorted(split_context.input_jars):
         cmd += ['--feature', in_jar, split_context.staging_dir]
-
-      feat_res = options.feature_resources.get(split_context.name)
-      if feat_res:
-        proto_in, shrunk_out = feat_res
-        cmd += [
-            '--feature', f':{proto_in}',
-            f'{split_context.staging_dir}:{shrunk_out}'
-        ]
 
     cmd += sorted(base_context.input_jars)
 
@@ -689,9 +653,6 @@ def _Run(options):
                                    exclude_generated=True)
 
   depfile_inputs = options.proguard_configs + options.input_paths + libraries
-  if options.feature_resources:
-    for proto_in, _ in options.feature_resources.values():
-      depfile_inputs.append(proto_in)
   if options.expected_file:
     diff_utils.CheckExpectations(merged_configs, options)
     if options.only_verify_expectations:
