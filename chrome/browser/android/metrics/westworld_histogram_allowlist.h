8ee185360b338a2c7767381150c08f436d3c271f
Troy Wang <troywang@google.com>
2026-05-20 02:16:47 -0700

    [Metrics] Refactor Westworld UMA logging to use inline mapping
    Simplify UMA to Westworld Atom mapping by merging the mapping details
    directly into the allowlist.
    
    Bug: b:483013446
    Change-Id: Ic4cab3e39ec9014f7afa6f1879bfb1112dd9fb5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7860963
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Troy Wang <troywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633442}

--- chrome/browser/android/metrics/westworld_histogram_allowlist.h
+++ chrome/browser/android/metrics/westworld_histogram_allowlist.h
@@ -5,20 +5,35 @@
 #ifndef CHROME_BROWSER_ANDROID_METRICS_WESTWORLD_HISTOGRAM_ALLOWLIST_H_
 #define CHROME_BROWSER_ANDROID_METRICS_WESTWORLD_HISTOGRAM_ALLOWLIST_H_
 
-#include <tuple>
+#include <string_view>
 
 #include "chrome/browser/android/metrics/uma_utils.h"
 
 namespace chrome::android::westworld {
 
+// Defines the expected data type for the Westworld atom.
+// Westworld atoms expect specific types (e.g., integers or booleans). This enum
+// ensures the correct JNI method is called when logging to Westworld.
+enum class MetricType { kInt, kBoolean };
+
+// Holds information about a UMA histogram that is mapped to a Westworld atom.
+struct HistogramInfo {
+  // The name of the UMA histogram to observe.
+  std::string_view histogram_name;
+  // The corresponding Westworld Atom ID.
+  int ww_atom_id;
+  // The data type expected by the Westworld atom.
+  MetricType type;
+};
+
 // An allowlist of UMA histograms that are registered for Westworld logging.
 //
 // To add a new histogram to Westworld:
-// 1. Add the histogram name string here in the list.
-// 2. Define an atom to be sent in `westworld_histogram_mapping.cc`.
-inline constexpr const char* kWestworldHistogramAllowlist[] = {
-    "Tabs.TabCount",
-    "Tabs.WindowCount",
+// Add a new HistogramInfo entry to this array with the UMA histogram name,
+// the corresponding Westworld Atom ID, and the expected MetricType.
+inline constexpr const HistogramInfo kWestworldHistogramAllowlist[] = {
+    {"Tabs.TabCount", 215200, MetricType::kInt},
+    {"Tabs.WindowCount", 215201, MetricType::kInt},
 };
 
 }  // namespace chrome::android::westworld
