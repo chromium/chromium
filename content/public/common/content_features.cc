bdccc716dd31fe290c2c5ff7bf8b3e4976ae7ac4
Calder Kitagawa <ckitagawa@chromium.org>
2026-05-20 15:07:56 -0700

    Default enable LazyBrowserInterfaceBroker
    Saves 1% browser memory on Android at 99th percentile. No notable impact
    to other platforms.
    
    Bug: 454312827
    Change-Id: I9b7204ee7d5511345383cb18450872a1f21413c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7861022
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633853}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -638,7 +638,7 @@ BASE_FEATURE(kKeepAliveReportBlockedByClient, base::FEATURE_ENABLED_BY_DEFAULT);
 // becomes lazy. i.e. the BrowserInterfaceBroker is constructed only when it is
 // needed, typically when a renderer process becomes associated with the frame.
 // See https://crbug.com/450912216 for more details.
-BASE_FEATURE(kLazyBrowserInterfaceBroker, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kLazyBrowserInterfaceBroker, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If this is enabled, LoadingPredictor restricts the number of preconnects for
 // the same destination to one.
