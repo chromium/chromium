c84be061a5bb93a8accfdd2eb1a97d0f3f479b65
Edgar Aguilar <eledgar@google.com>
2026-05-20 12:12:13 -0700

    [Fuzzing] Fix ICU deps and initialization.
    Move environment initialization so it is handled correctly from
    an APK execution. Due to how the JVM initializes variables if
    the environment is defined outside LLVMFuzzer initialization
    it crashes.
    
    Updating fuzzers in third_party/blink/renderer/platform/storage
    
    This CL was uploaded by git cl split.
    
    R=titouan@chromium.org
    
    Bug: 500525162
    Change-Id: I3d858b71df5b8badd7089f7855590803cb4ab7bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808134
    Auto-Submit: Edgar Aguilar <eledgar@google.com>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633745}

--- third_party/blink/renderer/platform/storage/blink_storage_key_fuzzer.cc
+++ third_party/blink/renderer/platform/storage/blink_storage_key_fuzzer.cc
@@ -7,6 +7,7 @@
 #include "base/at_exit.h"
 #include "base/check.h"
 #include "base/i18n/icu_util.h"
+#include "base/no_destructor.h"
 #include "base/test/scoped_feature_list.h"
 #include "mojo/core/embedder/embedder.h"
 #include "net/base/features.h"
@@ -28,9 +29,8 @@ struct Environment {
   base::AtExitManager at_exit_manager;
 };
 
-Environment* env = new Environment();
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  static const base::NoDestructor<Environment> env;
   std::string serialized_storage_key(reinterpret_cast<const char*>(data), size);
   for (const bool toggle : {false, true}) {
     base::test::ScopedFeatureList scope_feature_list;
