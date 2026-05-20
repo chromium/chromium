774d14b887c63d7a309985dd9828ecb6fb22481c
Lingqi Chi <lingqi@chromium.org>
2026-05-20 03:40:16 -0700

    [prerender] add trace event to debug nvs URL update
    We noticed that some prerender activated pages do not update the URLs,
    and this CL adds some trace events to debug.
    
    Bug: 504626592
    Change-Id: I868a152f2b0a284dcf51905899299b25e3f5ac1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7863905
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1633487}

--- content/browser/preloading/prerender/prerender_no_vary_search_commit_deferring_condition.cc
+++ content/browser/preloading/prerender/prerender_no_vary_search_commit_deferring_condition.cc
@@ -8,6 +8,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/time/time.h"
+#include "base/trace_event/typed_macros.h"
 #include "content/browser/preloading/prerender/prerender_host.h"
 #include "content/browser/renderer_host/frame_tree.h"
 #include "content/browser/renderer_host/frame_tree_node.h"
@@ -30,6 +31,69 @@ FrameTreeNode* GetRootPrerenderFrameTreeNode(
   return root;
 }
 
+enum class WillCommitDecision {
+  kProceedNoPrerenderFrameTreeNode,
+  kProceedNoUrlMatch,
+  kProceedAlreadyNavigated,
+  kProceedUrlIdentical,
+  kDeferToUpdatePrerenderURL,
+};
+
+const char* DecisionToReasonString(WillCommitDecision decision) {
+  switch (decision) {
+    case WillCommitDecision::kProceedNoPrerenderFrameTreeNode:
+      return "no_prerender_frame_tree_node";
+    case WillCommitDecision::kProceedNoUrlMatch:
+      return "no_url_match";
+    case WillCommitDecision::kProceedAlreadyNavigated:
+      return "already_navigated";
+    case WillCommitDecision::kProceedUrlIdentical:
+      return "url_identical";
+    case WillCommitDecision::kDeferToUpdatePrerenderURL:
+      return "defer_to_update_prerender_url";
+  }
+}
+
+WillCommitDecision GetWillCommitDecision(
+    FrameTreeNodeId candidate_prerender_frame_tree_node_id,
+    const GURL& navigation_url) {
+  FrameTreeNode* prerender_frame_tree_node =
+      GetRootPrerenderFrameTreeNode(candidate_prerender_frame_tree_node_id);
+  // If the prerender FrameTreeNode is gone, the prerender activation is allowed
+  // to continue here but will fail soon.
+  if (!prerender_frame_tree_node) {
+    return WillCommitDecision::kProceedNoPrerenderFrameTreeNode;
+  }
+  // If there is no need to change the URL of the prerender before using it
+  // continue. Otherwise inform the prerender renderer to update its URL
+  // and wait for it to finish before proceeding. This is needed because
+  // the omnibox should reflect the navigation URL which is an inexact match
+  // via No Vary Search to the prerender URL.
+  PrerenderHost& prerender_host =
+      PrerenderHost::GetFromFrameTreeNode(*prerender_frame_tree_node);
+  // If we cannot match the navigation URL the prerender activation is allowed
+  // to continue here but will fail soon. This can happen when matching
+  // by No-Vary-Search hint and the No-Vary-Search header doesn't agree.
+  if (!prerender_host.IsUrlMatch(navigation_url)) {
+    return WillCommitDecision::kProceedNoUrlMatch;
+  }
+  // If the prerender has been redirected or navigated, we do not change URL.
+  // A prerendered page can navigate to another same-site URL during
+  // prerendering. E.g. a prerendered page navigates from
+  // URL-A-with-NoVarySearch to URL-B. The omnibox should reflect URL-B instead
+  // of URL-A-with-NoVarySearch.
+  if (prerender_host.GetInitialUrl() !=
+      prerender_host.GetPrerenderedMainFrameHost()->GetLastCommittedURL()) {
+    return WillCommitDecision::kProceedAlreadyNavigated;
+  }
+  // If the prerender initial URL is the same as the navigation URL there is no
+  // need to change URL.
+  if (prerender_host.GetInitialUrl() == navigation_url) {
+    return WillCommitDecision::kProceedUrlIdentical;
+  }
+  return WillCommitDecision::kDeferToUpdatePrerenderURL;
+}
+
 }  // namespace
 
 // static
@@ -42,6 +106,8 @@ PrerenderNoVarySearchCommitDeferringCondition::MaybeCreate(
   if (navigation_type != NavigationType::kPrerenderedPageActivation) {
     return nullptr;
   }
+  TRACE_EVENT("navigation",
+              "PrerenderNoVarySearchCommitDeferringCondition::MaybeCreate");
 
   return base::WrapUnique(new PrerenderNoVarySearchCommitDeferringCondition(
       navigation_request, candidate_prerender_frame_tree_node_id.value()));
@@ -76,40 +142,28 @@ PrerenderNoVarySearchCommitDeferringCondition::
 CommitDeferringCondition::Result
 PrerenderNoVarySearchCommitDeferringCondition::WillCommitNavigation(
     base::OnceClosure resume) {
+  TRACE_EVENT("navigation",
+              "PrerenderNoVarySearchCommitDeferringCondition::"
+              "WillCommitNavigation");
+
+  WillCommitDecision decision = GetWillCommitDecision(
+      candidate_prerender_frame_tree_node_id_, GetNavigationHandle().GetURL());
+  TRACE_EVENT_INSTANT(
+      "navigation",
+      "PrerenderNoVarySearchCommitDeferringCondition::WillCommitNavigation::"
+      "Decision",
+      "reason", DecisionToReasonString(decision));
+
+  if (decision != WillCommitDecision::kDeferToUpdatePrerenderURL) {
+    return Result::kProceed;
+  }
+
   FrameTreeNode* prerender_frame_tree_node =
       GetRootPrerenderFrameTreeNode(candidate_prerender_frame_tree_node_id_);
-  // If the prerender FrameTreeNode is gone, the prerender activation is allowed
-  // to continue here but will fail soon.
-  if (!prerender_frame_tree_node) {
-    return Result::kProceed;
-  }
-  // If there is no need to change the URL of the prerender before using it
-  // continue. Otherwise inform the prerender renderer to update its URL
-  // and wait for it to finish before proceeding. This is needed because
-  // the omnibox should reflect the navigation URL which is an inexact match
-  // via No Vary Search to the prerender URL.
+  CHECK(prerender_frame_tree_node);
   PrerenderHost& prerender_host =
       PrerenderHost::GetFromFrameTreeNode(*prerender_frame_tree_node);
-  // If we cannot match the navigation URL the prerender activation is allowed
-  // to continue here but will fail soon. This can happen when matching
-  // by No-Vary-Search hint and the No-Vary-Search header doesn't agree.
-  if (!prerender_host.IsUrlMatch(GetNavigationHandle().GetURL())) {
-    return Result::kProceed;
-  }
-  // If the prerender has been redirected or navigated, we do not change URL.
-  // A prerendered page can navigate to another same-site URL during
-  // prerendering. E.g. a prerendered page navigates from
-  // URL-A-with-NoVarySearch to URL-B. The omnibox should reflect URL-B instead
-  // of URL-A-with-NoVarySearch.
-  if (prerender_host.GetInitialUrl() !=
-      prerender_host.GetPrerenderedMainFrameHost()->GetLastCommittedURL()) {
-    return Result::kProceed;
-  }
-  // If the prerender initial URL is the same as the navigation URL there is no
-  // need to change URL.
-  if (prerender_host.GetInitialUrl() == GetNavigationHandle().GetURL()) {
-    return Result::kProceed;
-  }
+
   // Inform prerender renderer to change its URL. Make sure the two URLs are
   // same origin.
   CHECK(prerender_host.GetPrerenderedMainFrameHost()
