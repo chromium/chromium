78eb9a919a14e291dce9de8249361f41841debfb
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2026-05-20 12:58:03 -0700

    [views-ax] Disable menu event dump tests on Mac
    Disable the MenuShowHide and MenuNested dump accessibility event tests on Mac. Bug 514918391 is tracking failures in these tests, so disable them temporarily to unblock other work while we investigate.
    
    Fixed: 514918391
    Bug: 40672441
    Change-Id: I09d300d053ac4c39af812741504e453d02efd34b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7865304
    Reviewed-by: Elly <ellyjones@chromium.org>
    Commit-Queue: Elly <ellyjones@chromium.org>
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1633779}

--- chrome/browser/ui/views/accessibility/menu_dump_accessibility_events_browsertest.cc
+++ chrome/browser/ui/views/accessibility/menu_dump_accessibility_events_browsertest.cc
@@ -109,7 +109,13 @@ class DumpAccessibilityEventsMenuTest
   }
 };
 
-IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsMenuTest, MenuShowHide) {
+// TODO(crbug.com/40672441): Re-enable on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_MenuShowHide DISABLED_MenuShowHide
+#else
+#define MAYBE_MenuShowHide MenuShowHide
+#endif
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsMenuTest, MAYBE_MenuShowHide) {
 #if BUILDFLAG(IS_WIN)
   if (GetApiType() == ui::AXApiType::kWinUIA) {
     // TODO(crbug.com/40672441): Re-enable once UIA supports PID-based event
@@ -141,7 +147,13 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsMenuTest, MenuShowHide) {
   WaitForSubmenuSerialization(submenu);
 }
 
-IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsMenuTest, MenuNested) {
+// TODO(crbug.com/40672441): Re-enable on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_MenuNested DISABLED_MenuNested
+#else
+#define MAYBE_MenuNested MenuNested
+#endif
+IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsMenuTest, MAYBE_MenuNested) {
 #if BUILDFLAG(IS_WIN)
   if (GetApiType() == ui::AXApiType::kWinUIA) {
     // TODO(crbug.com/40672441): Re-enable once UIA supports PID-based event
