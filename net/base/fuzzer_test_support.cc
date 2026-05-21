25597d6f2fecdd8d0c10d5fb692da301c1cfe7c7
Edgar Aguilar <eledgar@google.com>
2026-05-21 11:16:22 -0700

    [Fuzzing] Fix ICU initialization.
    Move environment initialization so it is handled correctly from
    an APK execution. Due to how the JVM initializes variables if
    the environment is defined outside LLVMFuzzer initialization
    it crashes.
    
    Bug: 500525162
    Change-Id: I5bddaae50dcc00387612625bd1798e3c02cfb314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7815198
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Edgar Aguilar <eledgar@google.com>
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634414}

--- net/base/fuzzer_test_support.cc
+++ net/base/fuzzer_test_support.cc
@@ -6,9 +6,11 @@
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
 #include "base/logging.h"
+#include "base/no_destructor.h"
 #include "base/test/scoped_run_loop_timeout.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_timeouts.h"
+#include "testing/libfuzzer/libfuzzer_exports.h"
 
 namespace {
 
@@ -50,6 +52,12 @@ struct InitGlobals {
   base::AtExitManager at_exit_manager;
 };
 
-InitGlobals* init_globals = new InitGlobals();
-
 }  // namespace
+
+// Use LLVMFuzzerInitialize to centralize initialization for all fuzzers
+// linking against this file, avoiding the need to duplicate this setup
+// in their individual LLVMFuzzerTestOneInput functions.
+extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
+  static const base::NoDestructor<InitGlobals> init_globals;
+  return 0;
+}
