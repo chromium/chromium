6e4d88bd59624a6290af12c33875fa4babdae4a4
Rafał Godlewski <rgod@google.com>
2026-05-20 17:18:27 -0700

    [iOS] Fix flaky testPasswordCheckupHomepageDeviceOrientation
    * Ensures that rotation of the device is reset to portrait after
    each test, so it definitely does not interfere with the test rotating
    it left and right
    * Ensures that credentials are cleared from the stores, so that the
    string of the password checkup page are what each tests expects
    
    Fixed: 435095080
    Change-Id: I42e12452b1a5c94711077f33c0790b8d8be182ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7859562
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1633907}

--- ios/chrome/browser/settings/ui_bundled/password/password_checkup/password_checkup_egtest.mm
+++ ios/chrome/browser/settings/ui_bundled/password/password_checkup/password_checkup_egtest.mm
@@ -300,6 +300,14 @@ NSString* LeakedPasswordDescription() {
                                     ReauthenticationResult::kSuccess];
 }
 
+- (void)tearDownHelper {
+  [EarlGrey rotateInterfaceToOrientation:UIInterfaceOrientationPortrait
+                                   error:nil];
+  [PasswordSettingsAppInterface clearPasswordStores];
+  [PasswordSettingsAppInterface clearPasskeyStore];
+  [super tearDownHelper];
+}
+
 #pragma mark - Tests
 
 // Tests the safe state of the Password Checkup Homepage.
@@ -502,11 +510,7 @@ NSString* LeakedPasswordDescription() {
 
 // Tests that the Password Checkup Homepage header image view is correctly
 // shown/hidden depending on the device's orientation.
-// TODO(crbug.com/435095080): Reenable this test.
 - (void)testPasswordCheckupHomepageDeviceOrientation {
-  if (![ChromeEarlGrey isIPadIdiom]) {
-    EARL_GREY_TEST_DISABLED(@"Failing on iPhone Simulator");
-  }
   if ([ChromeEarlGrey isIPadIdiom]) {
     EARL_GREY_TEST_SKIPPED(@"Landscape orientation doesn't change the look of "
                            @"the Password Checkup Homepage.");
