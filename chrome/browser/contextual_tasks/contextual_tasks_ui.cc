4533275bea1b480b56d2f626454c221c72dc2b79
Ananya Seelam <ananyaseelam@google.com>
2026-05-21 23:56:14 -0700

    [149] [context-input] Ensure `Sharing n tabs` counter is shown in RB and CoBr
    Original change's description:
    > [context-input] Ensure `Sharing n tabs` counter is shown in RB and CoBr
    > 
    > This CL adds the plural string in both contextual tasks and composebox
    > code to ensure it shows up.
    > 
    > Screencast: http://screencast/cast/NTczNDU1OTM5MDg5MjAzMnw4Y2JmZjAzMi05MQ
    > Bug: b:509961237
    > Change-Id: I5a8fa250b43cb9663c56e3041e18d6de8ccd1aea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7850001
    > Reviewed-by: Nihar Majmudar <niharm@google.com>
    > Commit-Queue: Nihar Majmudar <niharm@google.com>
    > Auto-Submit: Ananya Seelam <ananyaseelam@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1631919}
    
    Bug: 515487546
    Change-Id: I5a8fa250b43cb9663c56e3041e18d6de8ccd1aea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869475
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7827@{#1458}
    Cr-Branched-From: 9f3e9aaccba63bd2ec30334e45e0bfd07ebcc8f1-refs/heads/main@{#1625079}

--- chrome/browser/contextual_tasks/contextual_tasks_ui.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui.cc
@@ -43,6 +43,7 @@
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/webui/new_tab_page/composebox/variations/composebox_fieldtrial.h"
 #include "chrome/browser/ui/webui/sanitized_image/sanitized_image_source.h"
+#include "chrome/browser/ui/webui/plural_string_handler.h"
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/webui_url_constants.h"
@@ -265,7 +266,7 @@ void AddZeroStateStrings(content::WebUIDataSource* source, Profile* profile) {
 
 ContextualTasksUI::ContextualTasksUI(content::WebUI* web_ui)
     : ui::MojoWebUIController(web_ui,
-                              /*enable_chrome_send=*/false,
+                              /*enable_chrome_send=*/true,
                               /*enable_chrome_histograms=*/true),
       auto_suggestion_manager_(
           std::make_unique<
@@ -278,6 +279,12 @@ ContextualTasksUI::ContextualTasksUI(content::WebUI* web_ui)
               Profile::FromBrowserContext(
                   web_ui->GetWebContents()->GetBrowserContext()))) {
   Profile* profile = Profile::FromWebUI(web_ui);
+
+  // Add a handler to provide plural strings.
+  auto plural_string_handler = std::make_unique<PluralStringHandler>();
+  plural_string_handler->AddLocalizedString("sharingTabs",
+                                            IDS_COMPOSE_SHARING_TABS);
+  web_ui->AddMessageHandler(std::move(plural_string_handler));
 
   // In MPArch, a single webcontents is used to host multiple frame trees rather
   // than having a separate webcontents for each. In that case there's no need
