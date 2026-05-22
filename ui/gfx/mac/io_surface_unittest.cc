655bc1c6faa910e7af66c06038bbacb2007188e4
Bryan Oltman <bryanoltman@google.com>
2026-05-22 10:17:42 -0700

    [M148] Fix over-release handling in IOSurfaceMachPortTraits::Retain
    Original change's description:
    > Fix over-release handling in IOSurfaceMachPortTraits::Retain
    >
    > Retain() now correctly returns MACH_PORT_NULL if mach_port_mod_refs
    > fails, preventing the adoption of unreferenced port names.
    >
    > Fixed: 502104354
    > Change-Id: If9b731f548e354ca101df659152ac5b6aacf9fd9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7815394
    > Commit-Queue: Bryan Oltman <bryanoltman@google.com>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1625633}
    
    (cherry picked from commit f88c4aae15d560eaf60f64e26b0175ab5b8d5c21)
    
    Bug: 514923822,502104354
    Change-Id: If9b731f548e354ca101df659152ac5b6aacf9fd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7869571
    Reviewed-by: Bryan Oltman <bryanoltman@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7778@{#3501}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- ui/gfx/mac/io_surface_unittest.cc
+++ ui/gfx/mac/io_surface_unittest.cc
@@ -21,6 +21,31 @@ TEST(IOSurface, OddSizeMultiPlanar) {
   EXPECT_EQ(IOSurfaceGetHeightOfPlane(io_surface.get(), 1), 50u);
 }
 
+TEST(IOSurface, MachPortRetainDeadName) {
+  const mach_port_t task = mach_task_self();
+
+  // Create a port and give it send rights so that it will transition to a dead
+  // name when the receive right is removed.
+  mach_port_t port = MACH_PORT_NULL;
+  ASSERT_EQ(KERN_SUCCESS,
+            mach_port_allocate(task, MACH_PORT_RIGHT_RECEIVE, &port));
+  ASSERT_EQ(KERN_SUCCESS,
+            mach_port_insert_right(task, port, port, MACH_MSG_TYPE_MAKE_SEND));
+
+  // Remove the receive right.
+  ASSERT_EQ(KERN_SUCCESS,
+            mach_port_mod_refs(task, port, MACH_PORT_RIGHT_RECEIVE, -1));
+  mach_port_type_t port_type = MACH_PORT_TYPE_NONE;
+  ASSERT_EQ(KERN_SUCCESS, mach_port_type(task, port, &port_type));
+  ASSERT_TRUE(port_type & MACH_PORT_TYPE_DEAD_NAME)
+      << "port should have transitioned to a dead name";
+
+  // Attempting to retain a dead name fails, `Retain(port)` should return NULL.
+  mach_port_t result = internal::IOSurfaceMachPortTraits::Retain(port);
+  EXPECT_EQ(result, static_cast<mach_port_t>(MACH_PORT_NULL))
+      << "IOSurface should not retain a dead port";
+}
+
 }  // namespace
 
 }  // namespace gfx
