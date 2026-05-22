44703f611a87eea93524c0dba8b0d8b50b746508
Sourav <sinhasourav@google.com>
2026-05-22 13:26:13 -0700

    [iOS] Force disable Automatic Passkey Upgrade
    This CL temporarily disables the Automatic Passkey Upgrade to prevent a
    crash.
    
    MemoryCredentialStore has a long-standing, low-volume crash (see
    crbug.com/424222404) related to access of the underlying storage on a
    GCD queue. The volume of these crashes greatly increased in M149, and
    the only changes to how we interact with the credential store in that
    version involved the Automatic Passkey Upgrade feature.
    
    Our best guess of what happened is: 1. Reads and writes in the
    MemoryCredentialStore trigger
       (via the ArchivableCredentialStore subclass implementation of
       loadStorage) interactions with NSFileCoordinator.
    2. The main app also has one or more instances of this credential
       store. Because NSFileCoordinator acts as a lock, interactions in
       the CPE can delay execution of queued work in the main app.
    3. The CPE now reads much more frequently from the credential
       store, because OS-triggered calls to
       `performPasskeyRegistrationWithoutUserInteractionIfPossible`
       now search the credential store as part of verifying
       eligibility, and so whatever race condition we were encountering
       now happens more often.
    
    Fixing this problem is nontrivial, but reverting to the volume of
    CPE-triggered reads we had in 148 is safe and easy.
    
    The downside is that this will break automatic passkey upgrades (we will
    always reject them). We think that's a reasonable tradeoff for the short
    term. Users mostly won't notice the difference as this only ever happens
    "magically" in the background, and only in certain circumstances.
    
    Bug: 515318495
    Change-Id: I7b0ef163b8a0bcec3122c510a8a1a5f8c5da1124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868597
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1635177}

--- ios/chrome/credential_provider_extension/credential_provider_view_controller.mm
+++ ios/chrome/credential_provider_extension/credential_provider_view_controller.mm
@@ -322,6 +322,14 @@ enum class PasskeyUserVerificationStatus {
 
 - (void)performPasskeyRegistrationWithoutUserInteractionIfPossible:
     (ASPasskeyCredentialRequest*)registrationRequest {
+  // TODO(crbug.com/515318495): Force disable registration to prevent the CPE
+  // startup crash in M149
+  [self exitWithErrorCode:ASExtensionErrorCodeFailed];
+  return;
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunreachable-code"
+
   PasskeyRequestDetails* passkeyRequestDetails =
       [self passkeyDetailsFromConditionalCreateRequest:registrationRequest];
   if (![passkeyRequestDetails
@@ -344,6 +352,8 @@ enum class PasskeyUserVerificationStatus {
 
   // Try to create a passkey while user interaction is disallowed.
   [self createPasskeyWithDetails:passkeyRequestDetails gaia:gaia];
+
+#pragma clang diagnostic pop
 }
 
 - (void)prepareInterfaceForPasskeyRegistration:
