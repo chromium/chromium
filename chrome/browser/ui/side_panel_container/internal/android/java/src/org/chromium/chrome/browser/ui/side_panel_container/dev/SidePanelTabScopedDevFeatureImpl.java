911442fdab9fc1baa4ada1962a9bea526e2d6ce5
Mark Schillaci <mschillaci@google.com>
2026-05-22 18:33:32 -0700

    [Side Panel] Add more views to the dev panel for testing
    This CL only affects the dev testing side panel feature. It adds a few
    more views which will help accessibility testing going forward.
    
    New view: https://screenshot.googleplex.com/7p48sNRyR8iDjHK.png
    
    Bug: N/A
    Change-Id: Iaeef31f7cc5ba63d7642fc691d093e87824c309b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7871680
    Reviewed-by: Linyu He <linyuh@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1635342}

--- chrome/browser/ui/side_panel_container/internal/android/java/src/org/chromium/chrome/browser/ui/side_panel_container/dev/SidePanelTabScopedDevFeatureImpl.java
+++ chrome/browser/ui/side_panel_container/internal/android/java/src/org/chromium/chrome/browser/ui/side_panel_container/dev/SidePanelTabScopedDevFeatureImpl.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.ui.side_panel_container.dev;
 import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.jni_zero.CalledByNative;
@@ -23,6 +24,27 @@ import java.util.function.Supplier;
 /** Implements a tab scoped {@link SidePanelDevFeature}. */
 @NullMarked
 public final class SidePanelTabScopedDevFeatureImpl implements SidePanelDevFeature {
+    private static final String DEV_LINK_URL = "https://www.chromium.org";
+    private static final String DEV_INPUT_HINT = "Enter some text";
+
+    private static final String LOREM_IPSUM =
+            """
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor \
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud \
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute \
+            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla \
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia \
+            deserunt mollit anim id est laborum.
+
+            Morbi tincidunt, dui sit amet facilisis convallis, leo augue interdum magna, ac \
+            convallis metus nisl eget ante. Phasellus ac turpis arcu. In convallis porta dictum. \
+            Integer tristique, augue ac pellentesque semper, arcu turpis efficitur nisl, quis \
+            tincidunt magna lacus eu elit. Mauris non tellus elementum, vehicula tellus eget, \
+            feugiat lectus. Ut a nisl vel mauris condimentum hendrerit. Vestibulum ante ipsum \
+            primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras eget hendrerit \
+            elit, ac congue lorem.\
+            """;
+
     private final Supplier<Tab> mTabSupplier;
 
     /**
@@ -57,6 +79,15 @@ public final class SidePanelTabScopedDevFeatureImpl implements SidePanelDevFeatu
         TextView textView = view.findViewById(R.id.tab_title);
         String hash = Integer.toHexString(System.identityHashCode(tab));
         textView.setText("Tab title: " + tab.getTitle() + " " + hash);
+
+        TextView linkTextView = view.findViewById(R.id.dev_link);
+        linkTextView.setText(DEV_LINK_URL);
+
+        TextView fillerTextView = view.findViewById(R.id.filler_text);
+        fillerTextView.setText(LOREM_IPSUM);
+
+        EditText inputEditText = view.findViewById(R.id.dev_input);
+        inputEditText.setHint(DEV_INPUT_HINT);
 
         return view;
     }
