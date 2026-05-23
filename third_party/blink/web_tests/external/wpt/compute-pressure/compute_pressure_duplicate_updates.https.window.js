a8f43adb8805e8462900e0a1830073c50317c4fe
Wei Wang <wei4.wang@intel.com>
2026-05-23 04:12:28 -0700

    [PATCH 2/4] Revert "compute pressure: Consolidate duplicate updates test"
    This reverts commit 4df1295c94a6e99f521655c49cc8fe05a7a1c487.
    
    This feature is creating information leak. There is no active
    development on this feature anymore, therefore it was decided to
    revert the patches related to this feature.
    
    Bug: 513009005
    Change-Id: I76fc469c9de32224f1a61cf5a77b338a261a3168
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7863840
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Wang, Wei4 <wei4.wang@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1635413}

--- third_party/blink/web_tests/external/wpt/compute-pressure/compute_pressure_duplicate_updates.https.window.js
+++ third_party/blink/web_tests/external/wpt/compute-pressure/compute_pressure_duplicate_updates.https.window.js
@@ -22,7 +22,6 @@ pressure_test(async (t) => {
   await update_virtual_pressure_source('cpu', 'critical', 0.2);
   await syncObserver.waitForUpdate();
   assert_equals(syncObserver.changes()[0][0].state, 'critical');
-  assert_equals(syncObserver.changes()[0][0].ownContributionEstimate, 0.2);
 
   await update_virtual_pressure_source('cpu', 'critical', 0.2);
   await new Promise(resolve => {t.step_timeout(resolve, 3000)});
@@ -31,7 +30,6 @@ pressure_test(async (t) => {
   await update_virtual_pressure_source('cpu', 'nominal', 0.2);
   await syncObserver.waitForUpdate();
   assert_equals(syncObserver.changes()[1][0].state, 'nominal');
-  assert_equals(syncObserver.changes()[1][0].ownContributionEstimate, 0.2);
 
   assert_equals(syncObserver.changes().length, 2);
 }, 'Changes that fail the "should dispatch" test are discarded.');
