48b44d2d40de8b245129a42dda1ac37dc7be083e
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-05-26 04:37:51 -0700

    Revert "Use IDNA2008 on Android API 24+"
    This reverts commit 2c4aacb85e2756af5d9b917669f6bf8387a22e83.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8680744491180794513
    
    Sample failed build: https://ci.chromium.org/b/8680744491180794513
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7868313&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8680744491180794513&type=BUG
    
    Original change's description:
    > Use IDNA2008 on Android API 24+
    >
    > Bug: 513446116
    > Change-Id: Ib466aabeff577114b3c6cb77a11be35a63b75b55
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868313
    > Reviewed-by: Stefano Duo <stefanoduo@google.com>
    > Commit-Queue: Etienne Dechamps <edechamps@google.com>
    > Reviewed-by: Mike West <mkwst@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1636053}
    >
    
    Bug: 513446116
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I30ad3ec5c98e6572042f46f70e7e8d2cb8eccf25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874650
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1636057}

--- components/cronet/android/test/javatests/src/org/chromium/net/PkpTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/PkpTest.java
@@ -483,14 +483,10 @@ public class PkpTest {
         // Note the strategic use of ß as our test character, which is handled differently based on
         // which version of IDNA is used - see Unicode Technical Standard #46.
         final var IDN_UNICODE = "example-idn-begin-ß-end";
-        // On Android API <24 Cronet uses IDNA2003, under which "ß" is mapped to "ss".
-        // On Android API 24+ Cronet uses IDNA2008, under which "ß" is preserved and triggers
-        // punycode conversion.
-        // See also https://crbug.com/513446116.
-        final var EXPECTED_IDN_ASCII =
-                (Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
-                        ? "example-idn-begin-ss-end"
-                        : "xn--example-idn-begin--end-71b";
+        // Cronet currently uses IDNA2003, under which "ß" is mapped to "ss".
+        // TODO(https://crbug.com/513446116): ideally, Cronet should use IDNA2008, which is the
+        // recommended version and the one used by modern browsers.
+        final var EXPECTED_IDN_ASCII = "example-idn-begin-ss-end";
 
         final var testFramework = mTestRule.getTestFramework();
         applyCronetEngineBuilderConfigurationPatchWithMockCertVerifier(
