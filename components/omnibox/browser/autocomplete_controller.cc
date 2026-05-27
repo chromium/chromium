c5e9250863ea34815ce1a0f20bce64f020ff87c5
Keigo Oka <oka@google.com>
2026-05-26 17:47:44 -0700

    Site Search: Gate AttachSiteSearchActionToMatches on Android
    The AttachSiteSearchActionToMatches function was being called unconditionally on
    Android. This change wraps the call in a feature flag check for
    omnibox::kOmniboxSiteSearch so that the site search action only appears when
    the feature is explicitly enabled.
    
    Bug: 516585270
    Change-Id: Ifc95075db707326359a90fdf65eab6deab7e6987
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867235
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636607}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -1790,7 +1790,9 @@ void AutocompleteController::AttachActions() {
 #endif
 
 #if BUILDFLAG(IS_ANDROID)
-  internal_result_.AttachSiteSearchActionToMatches(template_url_service_);
+  if (base::FeatureList::IsEnabled(omnibox::kOmniboxSiteSearch)) {
+    internal_result_.AttachSiteSearchActionToMatches(template_url_service_);
+  }
 #endif  // BUILDFLAG(IS_ANDROID)
 }
 
