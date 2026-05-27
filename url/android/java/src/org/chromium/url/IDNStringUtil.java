0dda54862dc6cb6269417d5a46ca86c3468e8b7f
Etienne Dechamps <edechamps@google.com>
2026-05-27 00:38:10 -0700

    Reland "Use IDNA2008 on Android API 24+"
    This reverts commit 48b44d2d40de8b245129a42dda1ac37dc7be083e.
    
    Reason for revert: re-landing with fix for ObsoleteSdkInt warning on
    some build configs that have min SDK 24+ (e.g. android-cast-arm-rel,
    apparently)
    
    Original change's description:
    > Revert "Use IDNA2008 on Android API 24+"
    >
    > This reverts commit 2c4aacb85e2756af5d9b917669f6bf8387a22e83.
    >
    > Reason for revert:
    > LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8680744491180794513
    >
    > Sample failed build: https://ci.chromium.org/b/8680744491180794513
    >
    > If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7868313&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8680744491180794513&type=BUG
    >
    > Original change's description:
    > > Use IDNA2008 on Android API 24+
    > >
    > > Bug: 513446116
    > > Change-Id: Ib466aabeff577114b3c6cb77a11be35a63b75b55
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7868313
    > > Reviewed-by: Stefano Duo <stefanoduo@google.com>
    > > Commit-Queue: Etienne Dechamps <edechamps@google.com>
    > > Reviewed-by: Mike West <mkwst@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1636053}
    > >
    >
    > Bug: 513446116
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I30ad3ec5c98e6572042f46f70e7e8d2cb8eccf25
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874650
    > Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1636057}
    
    Bug: 513446116
    Cq-Include-Trybots: luci.chromium.try:android-cast-arm-rel
    Change-Id: Ic27540f5c30b9f52887ca889fc2a5063f0ed7684
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874801
    Reviewed-by: Mike West <mkwst@chromium.org>
    Auto-Submit: Etienne Dechamps <edechamps@google.com>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1636742}

--- url/android/java/src/org/chromium/url/IDNStringUtil.java
+++ url/android/java/src/org/chromium/url/IDNStringUtil.java
@@ -4,6 +4,9 @@
 
 package org.chromium.url;
 
+import android.icu.text.IDNA;
+import android.os.Build;
+
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 
@@ -11,17 +14,60 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 
 import java.net.IDN;
+import java.util.StringJoiner;
 
 /** This class is used to convert unicode IDN domain names to ASCII, when not building with ICU. */
 @JNINamespace("url::android")
 @NullMarked
 public class IDNStringUtil {
+    private static final @Nullable IDNA sIDNA = getIDNA();
+
+    // We suppress "Unnecessary; SDK_INT is never < 24 [ObsoleteSdkInt]" warnings that occur in
+    // some, but not all, build configs because these build configs set their min SDK version to
+    // 24+. See https://crrev.com/c/7874650.
+    @SuppressWarnings("ObsoleteSdkInt")
+    private static @Nullable IDNA getIDNA() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            return null;
+        }
+
+        var sIDNA =
+                IDNA.getUTS46Instance(
+                        IDNA.NONTRANSITIONAL_TO_ASCII | IDNA.NONTRANSITIONAL_TO_UNICODE);
+        if (sIDNA == null) {
+            throw new IllegalStateException("Failed to create IDNA instance");
+        }
+        return sIDNA;
+    }
+
     /**
-     * Attempts to convert a Unicode string to an ASCII string using IDN rules. As of May 2014, the
-     * underlying Java function IDNA2003.
+     * Attempts to convert a Unicode hostname to an ASCII hostname using IDN rules. Uses IDNA2008 on
+     * Android API 24+, IDNA2003 otherwise. See also https://crbug.com/513446116. Additionally, the
+     * resulting ASCII hostname is validated against RFC 1122 and RFC 1123 rules.
      */
-    public static String idnToASCII(String src) {
-        return IDN.toASCII(src, IDN.USE_STD3_ASCII_RULES);
+    public static String idnToASCII(String unicodeHostname) {
+        if (sIDNA == null) {
+            return IDN.toASCII(unicodeHostname, IDN.USE_STD3_ASCII_RULES);
+        }
+
+        var asciiHostnameBuilder = new StringBuilder();
+        var info = new IDNA.Info();
+        sIDNA.nameToASCII(unicodeHostname, asciiHostnameBuilder, info);
+        if (info.hasErrors()) {
+            var errors = new StringJoiner(", ");
+            for (var error : info.getErrors()) {
+                errors.add(error.toString());
+            }
+            throw new IllegalArgumentException("Failed to convert IDN to ASCII: " + errors);
+        }
+        var asciiHostname = asciiHostnameBuilder.toString();
+
+        // android.icu.text.IDNA does not check against RFC 1122 and RFC 1123, so we feed the
+        // resulting ASCII hostname to java.net.IDN, which does offer this functionality. Validation
+        // failures will throw exceptions; no need to use the result.
+        IDN.toASCII(asciiHostname, IDN.USE_STD3_ASCII_RULES);
+
+        return asciiHostname;
     }
 
     @CalledByNative
