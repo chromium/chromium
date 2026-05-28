54ef61604150cba64cd04209b3cffef6912a8a2d
Nick Harper <nharper@chromium.org>
2026-05-27 17:12:20 -0700

    Enable a test when version is IETF QUIC
    QuicChromiumClientSessionTest.ConnectionCloseBeforeHandshakeConfirmed
    was disabled when the version is IETF QUIC. The test passes when IETF
    QUIC is in use, so don't skip it. (I don't know why the test used to
    fail when TLS was used instead of QUIC Crypto, but it works now.)
    
    Change-Id: I462d19a75042113eab294a3e1ea8c58ce9989ba9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878661
    Auto-Submit: Nick Harper <nharper@chromium.org>
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637342}

--- net/quic/quic_chromium_client_session_test.cc
+++ net/quic/quic_chromium_client_session_test.cc
@@ -1147,12 +1147,6 @@ TEST_P(QuicChromiumClientSessionTest, ConnectionCloseBeforeStreamRequest) {
 }
 
 TEST_P(QuicChromiumClientSessionTest, ConnectionCloseBeforeHandshakeConfirmed) {
-  if (version_.IsIetfQuic()) {
-    // TODO(nharper, b/112643533): Figure out why this test fails when TLS is
-    // enabled and fix it.
-    return;
-  }
-
   // Force the connection close packet to use long headers with connection ID.
   server_maker_.SetEncryptionLevel(quic::ENCRYPTION_INITIAL);
 
