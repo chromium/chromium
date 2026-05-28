a22d5e8b25b67723108bd9cccdcda152de5240f1
James Cook <jamescook@chromium.org>
2026-05-28 10:32:09 -0700

    extensions: Port CrossOriginXhrTest to desktop Android
    It passes without modifications. Rename the test so it meets the style
    guide rules for capitalization, and add the string "Test" for good
    measure.
    
    Bug: 469417243
    Change-Id: I408c59dee7bba5ca73a3dc72c083b1e948cc5f0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7879850
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637796}

--- chrome/browser/extensions/cross_origin_xhr_apitest.cc
+++ chrome/browser/extensions/cross_origin_xhr_apitest.cc
@@ -4,28 +4,33 @@
 
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
+#include "extensions/buildflags/buildflags.h"
 #include "net/dns/mock_host_resolver.h"
 
-class CrossOriginXHR : public extensions::ExtensionApiTest {
+static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
+
+namespace extensions {
+
+class CrossOriginXhrTest : public ExtensionApiTest {
  public:
   void SetUpOnMainThread() override {
-    extensions::ExtensionApiTest::SetUpOnMainThread();
+    ExtensionApiTest::SetUpOnMainThread();
     host_resolver()->AddRule("*.com", "127.0.0.1");
     ASSERT_TRUE(StartEmbeddedTestServer());
   }
 };
 
-IN_PROC_BROWSER_TEST_F(CrossOriginXHR, BackgroundPage) {
+IN_PROC_BROWSER_TEST_F(CrossOriginXhrTest, BackgroundPage) {
   ASSERT_TRUE(RunExtensionTest("cross_origin_xhr/background_page")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(CrossOriginXHR, ContentScript) {
+IN_PROC_BROWSER_TEST_F(CrossOriginXhrTest, ContentScript) {
   ASSERT_TRUE(RunExtensionTest("cross_origin_xhr/content_script")) << message_;
 }
 
 // Tests that an extension frame can xhr a file url if it has file access and
 // "<all_urls>" host permissions.
-IN_PROC_BROWSER_TEST_F(CrossOriginXHR, FileAccessAllURLs) {
+IN_PROC_BROWSER_TEST_F(CrossOriginXhrTest, FileAccessAllURLs) {
   ASSERT_TRUE(RunExtensionTest("cross_origin_xhr/file_access_all_urls", {},
                                {.allow_file_access = true}))
       << message_;
@@ -33,14 +38,14 @@ IN_PROC_BROWSER_TEST_F(CrossOriginXHR, FileAccessAllURLs) {
 
 // Tests that an extension frame can't xhr a file url if it has no file access
 // even with the "<all_urls>" host permissions.
-IN_PROC_BROWSER_TEST_F(CrossOriginXHR, NoFileAccessAllURLs) {
+IN_PROC_BROWSER_TEST_F(CrossOriginXhrTest, NoFileAccessAllURLs) {
   ASSERT_TRUE(RunExtensionTest("cross_origin_xhr/no_file_access_all_urls"))
       << message_;
 }
 
 // Ensures that an extension tab having no corresponding background page can xhr
 // a file URL. Regression test for crbug.com/40749878.
-IN_PROC_BROWSER_TEST_F(CrossOriginXHR, FileAccessNoBackgroundPage) {
+IN_PROC_BROWSER_TEST_F(CrossOriginXhrTest, FileAccessNoBackgroundPage) {
   ASSERT_TRUE(RunExtensionTest(
       "cross_origin_xhr/file_access_no_background_page",
       {.extension_url = "test.html"}, {.allow_file_access = true}))
@@ -49,8 +54,10 @@ IN_PROC_BROWSER_TEST_F(CrossOriginXHR, FileAccessNoBackgroundPage) {
 
 // Tests that an extension frame can't xhr a file url if it does not have host
 // permissions to the file scheme even though it has file access.
-IN_PROC_BROWSER_TEST_F(CrossOriginXHR, FileAccessNoHosts) {
+IN_PROC_BROWSER_TEST_F(CrossOriginXhrTest, FileAccessNoHosts) {
   ASSERT_TRUE(RunExtensionTest("cross_origin_xhr/file_access_no_hosts", {},
                                {.allow_file_access = true}))
       << message_;
 }
+
+}  // namespace extensions
