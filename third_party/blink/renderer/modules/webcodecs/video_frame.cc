9cad4244e460e6c8a5ff5daf4a033d97b345932d
Eugene Zemtsov <eugene@chromium.org>
2026-05-28 14:27:17 -0700

    media: Use cross-thread handles for WebCodecs callbacks
    Replace WrapPersistent/WrapWeakPersistent with cross-thread handles in
    VideoEncoder, VideoFrame, and BackgroundReadback to enable safe
    cross-thread destruction.
    
    Bug: 517129549
    Change-Id: Ibaf32966cf9000d03265ffc3325c20ee063af4f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7881219
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1637957}

--- third_party/blink/renderer/modules/webcodecs/video_frame.cc
+++ third_party/blink/renderer/modules/webcodecs/video_frame.cc
@@ -1358,8 +1358,9 @@ VideoFrame::CopyToPromise VideoFrame::CopyToAsync(
           resolver->Reject();
         }
       };
-  auto done_cb = BindOnce(readback_done_handler, std::move(contents),
-                          WrapPersistent(resolver), dest_layout);
+  auto done_cb =
+      BindOnce(readback_done_handler, std::move(contents),
+               MakeUnwrappingCrossThreadHandle(resolver), dest_layout);
 
   auto buffer = AsSpan<uint8_t>(destination);
   background_readback->ReadbackTextureBackedFrameToBuffer(
