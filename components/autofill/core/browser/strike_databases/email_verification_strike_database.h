348d067c70d66fc793fc3adf77dd845c16c59f3e
Christian Biesinger <cbiesinger@chromium.org>
2026-05-29 08:39:43 -0700

    [EVP] Store email strikes using a short hash
    As requested by privacy in b:510030655, store rejected emails in the
    strike database using a short hash because we do not display them to the
    user in settings.
    
    Fixed: 517327188
    Change-Id: Iac28aeb480341b7143a7c3ee2352f2aaed3896b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7881828
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Auto-Submit: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Sam Goto <goto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638501}

--- components/autofill/core/browser/strike_databases/email_verification_strike_database.h
+++ components/autofill/core/browser/strike_databases/email_verification_strike_database.h
@@ -5,12 +5,16 @@
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_STRIKE_DATABASES_EMAIL_VERIFICATION_STRIKE_DATABASE_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_STRIKE_DATABASES_EMAIL_VERIFICATION_STRIKE_DATABASE_H_
 
+#include <array>
 #include <optional>
 #include <string>
 #include <string_view>
 
+#include "base/containers/span.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 #include "components/strike_database/history_clearable_strike_database.h"
+#include "crypto/hash.h"
 
 namespace autofill {
 
@@ -23,14 +27,6 @@ struct EmailVerificationStrikeDatabaseTraits {
   static constexpr std::optional<base::TimeDelta> kExpiryTimeDelta =
       base::Days(180);
   static constexpr bool kUniqueIdRequired = true;
-
-  static std::string HostFromId(const std::string& id) {
-    size_t at_pos = id.find_last_of("@");
-    if (at_pos == std::string::npos) {
-      return "";
-    }
-    return id.substr(at_pos + 1);
-  }
 };
 
 class EmailVerificationStrikeDatabase
@@ -41,7 +37,13 @@ class EmailVerificationStrikeDatabase
       strike_database::StrikeDatabaseBase* strike_db)
       : HistoryClearableStrikeDatabase(strike_db) {}
 
-  static std::string GetId(const std::string& email) { return email; }
+  static std::string GetId(const std::string& email) {
+    // Use just the first two bytes of the hash for privacy reasons, so that the
+    // original email can't be determined from the hash.
+    const std::array<uint8_t, crypto::hash::kSha256Size> hash =
+        crypto::hash::Sha256(email);
+    return base::HexEncode(base::span(hash).first<2>());
+  }
 };
 
 }  // namespace autofill
---
+++ components/autofill/core/browser/strike_databases/email_verification_strike_database_unittest.cc
@@ -0,0 +1,31 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/autofill/core/browser/strike_databases/email_verification_strike_database.h"
+
+#include <string>
+
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/strings/ascii.h"
+
+namespace autofill {
+namespace {
+
+TEST(EmailVerificationStrikeDatabaseTest, GetIdReturnsFourCharHex) {
+  std::string id = EmailVerificationStrikeDatabase::GetId("test@example.com");
+  EXPECT_EQ(id.length(), 4u);
+  // Verify it's hex.
+  EXPECT_TRUE(absl::ascii_isxdigit(static_cast<unsigned char>(id[0])));
+  EXPECT_TRUE(absl::ascii_isxdigit(static_cast<unsigned char>(id[1])));
+  EXPECT_TRUE(absl::ascii_isxdigit(static_cast<unsigned char>(id[2])));
+  EXPECT_TRUE(absl::ascii_isxdigit(static_cast<unsigned char>(id[3])));
+}
+
+TEST(EmailVerificationStrikeDatabaseTest, GetIdIsConsistent) {
+  EXPECT_EQ(EmailVerificationStrikeDatabase::GetId("test@example.com"),
+            EmailVerificationStrikeDatabase::GetId("test@example.com"));
+}
+
+}  // namespace
+}  // namespace autofill
