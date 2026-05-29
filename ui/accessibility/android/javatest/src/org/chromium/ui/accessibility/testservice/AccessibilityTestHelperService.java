ac893ac4078adddbe1a6add88baed1c6a83a6927
Kavya Mehta <kavyamehta@google.com>
2026-05-29 11:19:01 -0700

    [a11y] Make logging output of ContentChangeType bitmasks human readable
    This change allowed for the ContentChangeType bitmask to be output in a
    human readable format where the enabled bitmasks are logged instead of
    an integer.
    
    Also added 2 helper functions which allow for converting a bitmask to
    string format.
    
    Testing Method: Using an android emulator with API level >= 34, run the
    WebContentsAccessibilityE2ETest#fireGeneratedEvent_ariaInvalidTrue_firesContentInvalid
    test. Observe the output generated using adb logcat.
    
    Fixed: 505789379
    Change-Id: I5d08e0fbe33d4b95d8b53e7c66f880838ad26647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7881180
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Commit-Queue: Kavya Mehta <kavyamehta@google.com>
    Reviewed-by: Kyra Seevers <kyraseevers@chromium.org>
    Reviewed-by: Aaron Moss <aaronmoss@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638603}

--- ui/accessibility/android/javatest/src/org/chromium/ui/accessibility/testservice/AccessibilityTestHelperService.java
+++ ui/accessibility/android/javatest/src/org/chromium/ui/accessibility/testservice/AccessibilityTestHelperService.java
@@ -7,8 +7,11 @@ package org.chromium.ui.accessibility.testservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.view.accessibility.AccessibilityEvent;
 
 import org.chromium.base.Log;
+
+import java.util.function.IntFunction;
 
 /**
  * Helper service to provide a bridge between instrumentation tests and the
@@ -21,6 +24,9 @@ public class AccessibilityTestHelperService extends Service {
             new IAccessibilityTestHelperService.Stub() {
                 @Override
                 public boolean waitForEvent(WaitForEventParams params) {
+                    String contentChangeTypesString =
+                            contentChangeTypesToString(params.contentChangeTypes);
+
                     Log.i(
                             TAG,
                             "waitForEvent called with type: "
@@ -28,7 +34,7 @@ public class AccessibilityTestHelperService extends Service {
                                     + ", class: "
                                     + params.className
                                     + ", ContentChangeTypes: "
-                                    + params.contentChangeTypes
+                                    + contentChangeTypesString
                                     + ", text: "
                                     + params.text);
                     return AccessibilityTestService.tryWaitForEvent(params);
@@ -69,5 +75,65 @@ public class AccessibilityTestHelperService extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy");
+    }
+
+    private String contentChangeTypesToString(int types) {
+        return flagsToString(
+                types, AccessibilityTestHelperService::singleContentChangeTypeToString);
+    }
+
+    private static String flagsToString(int flags, IntFunction<String> getFlagName) {
+        if (flags == 0) {
+            return "UNDEFINED";
+        }
+
+        // Parsing out the bits from flags bitmask, querying the corresponding
+        // value using getFlagName function parameter and appending to the
+        // return value.
+        StringBuilder builder = new StringBuilder();
+        int count = 0;
+        while (flags != 0) {
+            final int flag = 1 << Integer.numberOfTrailingZeros(flags);
+            flags &= ~flag;
+            if (count > 0) builder.append(", ");
+            builder.append(getFlagName.apply(flag));
+            count++;
+        }
+        return builder.toString();
+    }
+
+    private static String singleContentChangeTypeToString(int type) {
+        switch (type) {
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_UNDEFINED:
+                return "UNDEFINED";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_SUBTREE:
+                return "SUBTREE";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_TEXT:
+                return "TEXT";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_CONTENT_DESCRIPTION:
+                return "CONTENT_DESCRIPTION";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_STATE_DESCRIPTION:
+                return "STATE_DESCRIPTION";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_PANE_TITLE:
+                return "PANE_TITLE";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_PANE_APPEARED:
+                return "PANE_APPEARED";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_PANE_DISAPPEARED:
+                return "PANE_DISAPPEARED";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_DRAG_STARTED:
+                return "DRAG_STARTED";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_DRAG_CANCELLED:
+                return "DRAG_CANCELLED";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_DRAG_DROPPED:
+                return "DRAG_DROPPED";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_CONTENT_INVALID:
+                return "CONTENT_INVALID";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_ERROR:
+                return "ERROR";
+            case AccessibilityEvent.CONTENT_CHANGE_TYPE_ENABLED:
+                return "ENABLED";
+            default:
+                return "UNKNOWN: " + Integer.toString(type);
+        }
     }
 }
