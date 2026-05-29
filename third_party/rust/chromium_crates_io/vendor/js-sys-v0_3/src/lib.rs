28ba3f99f3d8ec52a19f1d4ee2f350e931d7786d
Martin Verde <thesalsa@google.com>
2026-05-28 18:59:25 -0700

    Roll js-sys: 0.3.98 => 0.3.99 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * js-sys: 0.3.98 => 0.3.99; https://docs.rs/crate/js-sys/0.3.99
    * wasm-bindgen-macro-support: 0.2.121 => 0.2.122;
      https://docs.rs/crate/wasm-bindgen-macro-support/0.2.122
    * wasm-bindgen-macro: 0.2.121 => 0.2.122;
      https://docs.rs/crate/wasm-bindgen-macro/0.2.122
    * wasm-bindgen-shared: 0.2.121 => 0.2.122;
      https://docs.rs/crate/wasm-bindgen-shared/0.2.122
    * wasm-bindgen: 0.2.121 => 0.2.122;
      https://docs.rs/crate/wasm-bindgen/0.2.122
    
    Bug: None
    Change-Id: Icf5edbd5774ee9584aa79c56843ee725c06a1f85
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7875704
    Commit-Queue: Martin Pan-Verde <thesalsa@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638167}

--- third_party/rust/chromium_crates_io/vendor/js-sys-v0_3/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/js-sys-v0_3/src/lib.rs
@@ -6,6 +6,6 @@
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `js-sys-0.3.98` crate.
+// `js-sys-0.3.99` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
