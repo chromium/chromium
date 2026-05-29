7fa2f09e444b5753fee2165b9c95bdaa4f062ed2
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-05-29 09:56:09 -0700

    Revert "disable CrComponentsComposeboxTest.ContextualActionMenu on Linux only"
    This reverts commit fdf6a1f15d210155ba7c79ce475a6cef0f4d5b9e.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5719829437743104
    
    Sample build with failed test: https://ci.chromium.org/b/8680497018151682609
    Affected test(s):
    [://chrome/test\:browser_tests!gtest::CrComponentsComposeboxTest#ContextualActionMenu](https://ci.chromium.org/ui/test/chromium/:%2F%2Fchrome%2Ftest%5C:browser_tests%21gtest::CrComponentsComposeboxTest%23ContextualActionMenu?q=VHash%3A964093c92ad1c142)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5719829437743104&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F7882078&type=BUG
    
    Original change's description:
    > disable CrComponentsComposeboxTest.ContextualActionMenu on Linux only
    >
    > This is a follow-up of I0b5fa2a3736cdbeff4b80c0f9aa6d5d28d0fdd05, as
    > this test is failing on linux only.
    >
    > Bug: 512920161
    > Change-Id: I0348be8e8a45b1d20ed0729d4fa4c7b23608b1a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7882078
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1638183}
    >
    
    Bug: 512920161
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iabc7dafffa128fb4b8080be5913cede5e66b9b5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7885845
    Reviewed-by: David A <awogbemila@chromium.org>
    Commit-Queue: Paula Gómez <paugomez@google.com>
    Reviewed-by: Paula Gómez <paugomez@google.com>
    Owners-Override: David A <awogbemila@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638550}

--- chrome/test/data/webui/cr_components/composebox/contextual_action_menu_test.ts
+++ chrome/test/data/webui/cr_components/composebox/contextual_action_menu_test.ts
@@ -890,9 +890,8 @@ suite('ContextualActionMenu', () => {
     assertEquals(trigger, actionMenu.shadowRoot.activeElement);
   });
 
-  // TODO(crbug.com/512920161): Reenable this test on Linux and Mac
-  // <if expr="not is_linux and not is_macosx">
-  test(
+  // TODO(crbug.com/512920161): Reenable this test.
+  test.skip(
       'navigates up and down between Share Tabs and other menu items',
       async () => {
         loadTimeData.overrideValues({
@@ -952,7 +951,6 @@ suite('ContextualActionMenu', () => {
         // directly on Image Upload.
         assertEquals(imageUpload, actionMenu.shadowRoot.activeElement);
       });
-  // </if>
 
   test('Share tabs flyout dynamic repositioning', async () => {
     loadTimeData.overrideValues({
