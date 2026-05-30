46cd1a73dd11068b4cf29447a533aee7578368e9
James Cook <jamescook@chromium.org>
2026-05-29 22:05:35 -0700

    extensions: Port IsolatedWorldCspBrowserTest to desktop Android
    The manifest V3 tests pass without modification.
    
    The manifest V2 tests are skipped on Android, but kept for other
    platforms, as they are explicitly testing MV2-only behavior.
    
    Bug: 469417243
    Change-Id: I44d7c561fa0829325e8a5ab0e5efde69c724cbc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7886610
    Reviewed-by: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Auto-Submit: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1638947}

--- chrome/browser/extensions/isolated_world_csp_browsertest.cc
+++ chrome/browser/extensions/isolated_world_csp_browsertest.cc
@@ -4,18 +4,24 @@
 
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/extensions/extension_apitest.h"
-#include "chrome/browser/ui/browser.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
+#include "url/gurl.h"
 
 namespace extensions {
+namespace {
 
-class IsolatedWorldCspBrowserTest : public ExtensionApiTest {
+enum class ManifestVersion { kMV2, kMV3 };
+
+class IsolatedWorldCspBrowserTest
+    : public ExtensionApiTest,
+      public testing::WithParamInterface<ManifestVersion> {
  public:
   // ExtensionApiTest override.
   void SetUpOnMainThread() override {
-    extensions::ExtensionApiTest::SetUpOnMainThread();
+    ExtensionApiTest::SetUpOnMainThread();
 
     // Override the path used for loading the extension.
     test_data_dir_ = test_data_dir_.AppendASCII("isolated_world_csp");
@@ -26,92 +32,83 @@ class IsolatedWorldCspBrowserTest : public ExtensionApiTest {
     // Map all hosts to localhost.
     host_resolver()->AddRule("*", "127.0.0.1");
   }
+
+  const char* GetExtensionName() const {
+    return GetParam() == ManifestVersion::kMV2 ? "mv2" : "mv3";
+  }
 };
 
-// Test that a Manifest V2 content script can use eval by bypassing the main
-// world CSP.
-IN_PROC_BROWSER_TEST_F(IsolatedWorldCspBrowserTest, Eval_ManifestV2) {
-  GURL url = embedded_test_server()->GetURL("eval.com",
-                                            "/page_with_script_src_csp.html");
-  ASSERT_TRUE(RunExtensionTest("mv2", {.page_url = url.spec().c_str()}))
-      << message_;
-}
+#if BUILDFLAG(IS_ANDROID)
+// Android only supports manifest V3.
+INSTANTIATE_TEST_SUITE_P(ManifestVersion,
+                         IsolatedWorldCspBrowserTest,
+                         testing::Values(ManifestVersion::kMV3));
+#else
+INSTANTIATE_TEST_SUITE_P(ManifestVersion,
+                         IsolatedWorldCspBrowserTest,
+                         testing::Values(ManifestVersion::kMV2,
+                                         ManifestVersion::kMV3));
+#endif
 
-// Test that a Manifest V3 content script can't use eval.
-IN_PROC_BROWSER_TEST_F(IsolatedWorldCspBrowserTest, Eval_ManifestV3) {
+// Test that a Manifest V2 content script can use eval by bypassing the main
+// world CSP, but a Manifest V3 script cannot.
+IN_PROC_BROWSER_TEST_P(IsolatedWorldCspBrowserTest, Eval) {
   GURL url = embedded_test_server()->GetURL("eval.com",
                                             "/page_with_script_src_csp.html");
-  ASSERT_TRUE(RunExtensionTest("mv3", {.page_url = url.spec().c_str()}))
+  ASSERT_TRUE(
+      RunExtensionTest(GetExtensionName(), {.page_url = url.spec().c_str()}))
       << message_;
 }
 
 // Test that a Manifest V2 content script can navigate to a javascript url by
-// bypassing the main world CSP.
-IN_PROC_BROWSER_TEST_F(IsolatedWorldCspBrowserTest, JavascriptUrl_ManifestV2) {
-  GURL url = embedded_test_server()->GetURL("js-url.com",
-                                            "/page_with_script_src_csp.html");
-  ASSERT_TRUE(RunExtensionTest("mv2", {.page_url = url.spec().c_str()}))
-      << message_;
-}
+// bypassing the main world CSP but a Manifest V3 script cannot.
+IN_PROC_BROWSER_TEST_P(IsolatedWorldCspBrowserTest, JavascriptUrl) {
+  if (GetParam() == ManifestVersion::kMV2) {
+    GURL url = embedded_test_server()->GetURL("js-url.com",
+                                              "/page_with_script_src_csp.html");
+    ASSERT_TRUE(RunExtensionTest("mv2", {.page_url = url.spec().c_str()}))
+        << message_;
+  } else {
+    // We wait on a console message which will be raised on an unsuccessful
+    // navigation to a javascript url since there isn't any other clean way to
+    // assert that the navigation didn't succeed.
+    content::WebContents* web_contents = GetActiveWebContents();
+    content::WebContentsConsoleObserver console_observer(web_contents);
+    console_observer.SetPattern(
+        "Running the JavaScript URL violates the following Content Security "
+        "Policy directive *");
 
-// Test that a Manifest V3 content script can't navigate to a javascript url
-// while in its isolated world.
-IN_PROC_BROWSER_TEST_F(IsolatedWorldCspBrowserTest, JavascriptUrl_ManifestV3) {
-  // We wait on a console message which will be raised on an unsuccessful
-  // navigation to a javascript url since there isn't any other clean way to
-  // assert that the navigation didn't succeed.
-  content::WebContents* web_contents = GetActiveWebContents();
-  content::WebContentsConsoleObserver console_observer(web_contents);
-  console_observer.SetPattern(
-      "Running the JavaScript URL violates the following Content Security "
-      "Policy directive *");
+    GURL url = embedded_test_server()->GetURL("js-url.com",
+                                              "/page_with_script_src_csp.html");
+    ASSERT_TRUE(RunExtensionTest("mv3", {.page_url = url.spec().c_str()}))
+        << message_;
+    ASSERT_TRUE(console_observer.Wait());
 
-  GURL url = embedded_test_server()->GetURL("js-url.com",
-                                            "/page_with_script_src_csp.html");
-  ASSERT_TRUE(RunExtensionTest("mv3", {.page_url = url.spec().c_str()}))
-      << message_;
-  ASSERT_TRUE(console_observer.Wait());
-
-  // Also ensure the page title wasn't changed.
-  EXPECT_EQ(u"Page With CSP", web_contents->GetTitle());
+    // Also ensure the page title wasn't changed.
+    EXPECT_EQ(u"Page With CSP", web_contents->GetTitle());
+  }
 }
 
 // Test that a Manifest V2 content script can execute a remote script even if
-// it is disallowed by the main world CSP.
-IN_PROC_BROWSER_TEST_F(IsolatedWorldCspBrowserTest,
-                       RemoteScriptSrc_ManifestV2) {
+// it is disallowed by the main world CSP, but a Manifest V3 script cannot.
+IN_PROC_BROWSER_TEST_P(IsolatedWorldCspBrowserTest, RemoteScriptSrc) {
   GURL url = embedded_test_server()->GetURL("remote-script.com",
                                             "/page_with_script_src_csp.html");
-  ASSERT_TRUE(RunExtensionTest("mv2", {.page_url = url.spec().c_str()}))
-      << message_;
-}
-
-// Test that a Manifest V3 content script can't execute a remote script even if
-// is allowed by the main world CSP.
-IN_PROC_BROWSER_TEST_F(IsolatedWorldCspBrowserTest,
-                       RemoteScriptSrc_ManifestV3) {
-  GURL url = embedded_test_server()->GetURL("remote-script.com",
-                                            "/page_with_script_src_csp.html");
-  ASSERT_TRUE(RunExtensionTest("mv3", {.page_url = url.spec().c_str()}))
+  ASSERT_TRUE(
+      RunExtensionTest(GetExtensionName(), {.page_url = url.spec().c_str()}))
       << message_;
 }
 
 // Test that a Manifest V2 content script can execute a remote script even if
-// it is disallowed by the main world Integrity Policy.
-IN_PROC_BROWSER_TEST_F(IsolatedWorldCspBrowserTest, IntegrityPolicyManifestV2) {
+// it is disallowed by the main world Integrity Policy but a Manifest V3
+// extension can.
+IN_PROC_BROWSER_TEST_P(IsolatedWorldCspBrowserTest, IntegrityPolicy) {
   GURL url = embedded_test_server()->GetURL("remote-script.com",
                                             "/page_with_integrity_policy.html");
-  ASSERT_TRUE(RunExtensionTest("mv2", {.page_url = url.spec().c_str()}))
+  ASSERT_TRUE(
+      RunExtensionTest(GetExtensionName(), {.page_url = url.spec().c_str()}))
       << message_;
 }
 
-// Test that a Manifest V3 content script can execute a remote script even if
-// it is disallowed by the main world Integrity Policy.
-IN_PROC_BROWSER_TEST_F(IsolatedWorldCspBrowserTest, IntegrityPolicyManifestV3) {
-  GURL url = embedded_test_server()->GetURL("remote-script.com",
-                                            "/page_with_integrity_policy.html");
-  ASSERT_TRUE(RunExtensionTest("mv3", {.page_url = url.spec().c_str()}))
-      << message_;
-}
-
+}  // namespace
 }  // namespace extensions
