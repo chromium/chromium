778fbd966aff4a2b5f5d70e082ed0ab13b898348
Di Wu <diwux@google.com>
2026-05-29 22:54:24 -0700

    Ash: Migrate browser navigator test off ProfileHelper
    This changelist is part of an ongoing series to decouple ChromeOS
    code from the main Chrome browser codebase. By migrating away from
    ash::ProfileHelper (//chrome/browser/ash) to lower-level Ash APIs such
    as ash::BrowserContextHelper (//chromeos/ash/components), it removes
    inappropriate upward dependencies from lower-level Ash components to
    the higher-level browser application layer. This modernization also
    moves code away from heavy, UI-bound Profile objects and toward the
    cleaner content::BrowserContext abstraction. Where ProfileHelper wraps
    user checks, the migration uses components/user_manager directly.
    
    BrowserNavigatorTestChromeOS now gets the signin browser context
    through BrowserContextHelper and converts it back to Profile for the
    existing creation-status check. The patch intentionally preserves
    ProfileHelper::GetSigninProfile() creation/loading semantics.
    
    Bug: b:511961755
    Test: CQ
    Change-Id: Ieddd81e39e7e900ce8bdfbb790fe9e6d54e5064b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878005
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1638954}

--- chrome/browser/ui/navigator/browser_navigator_browsertest_chromeos.cc
+++ chrome/browser/ui/navigator/browser_navigator_browsertest_chromeos.cc
@@ -14,7 +14,6 @@
 #include "chrome/browser/ash/login/chrome_restart_request.h"
 #include "chrome/browser/ash/login/test/device_state_mixin.h"
 #include "chrome/browser/ash/login/test/login_manager_mixin.h"
-#include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/multi_user/multi_user_util.h"
 #include "chrome/browser/ui/ash/session/session_controller_client_impl.h"
@@ -55,8 +54,9 @@ IN_PROC_BROWSER_TEST_F(BrowserNavigatorTestChromeOS, RestrictSigninProfile) {
   EXPECT_EQ(GlobalBrowserCollection::GetInstance()->GetSize(), 1u);
 
   EXPECT_EQ(Browser::CreationStatus::kErrorProfileUnsuitable,
-            Browser::GetCreationStatusForProfile(
-                ash::ProfileHelper::GetSigninProfile()));
+            Browser::GetCreationStatusForProfile(Profile::FromBrowserContext(
+                ash::BrowserContextHelper::Get()
+                    ->DeprecatedGetOrCreateSigninBrowserContext())));
 }
 
 // Verify that page navigation is blocked in locked fullscreen mode.
