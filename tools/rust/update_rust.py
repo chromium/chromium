3c03681c4c1bb8a1d556e56553dcc8170d8827e9
Lukasz Anforowicz <lukasza@chromium.org>
2026-06-01 13:24:17 -0700

    [crubit] Roll Crubit to ToT (as of 2026-05-29).
    Crubit has been rolled to ToT (as of 2026-05-29).  This includes support
    for `PartialEq` <=> `operator==` bindings which are needed to replace
    `cxx::bridge`-based FFI in Chromium's `components/qr_code`.
    
    Most toolchain rolls use an extended list of CQ trybots, but this CL has
    a much lower risk than typical rolls, because it only updates Crubit.
    (Crubit is disabled by the default value of `enable_cpp_api_from_rust`
    defined in `build/config/rust.gni`).
    
    Bug: 470466915
    Change-Id: Iab30b87cb0a04dc2cb106ccd50fad97d06e42b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7888777
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1639626}

--- tools/rust/update_rust.py
+++ tools/rust/update_rust.py
@@ -37,13 +37,13 @@ sys.path.append(
 # should not be changed manually.
 # They are also read by build/config/compiler/BUILD.gn.
 RUST_REVISION = '4c4205163abcbd08948b3efab796c543ba1ea687'
-RUST_SUB_REVISION = 4
+RUST_SUB_REVISION = 5
 
 # The revision of Crubit to use from https://github.com/google/crubit
 #
 # If changing the CRUBIT_REVISION but not the RUST_REVISION, bump the
 # RUST_SUB_REVISION to generate a unique package name.
-CRUBIT_REVISION = 'b2537a78e786ef5e71351b454064eda704a3fa60'
+CRUBIT_REVISION = '4ccd520aefa4310599b2b518507afb7706e57c63'
 
 # Hash of src/stage0.json, which itself contains the stage0 toolchain hashes.
 # We trust the Rust build system checks, but to ensure it is not tampered with
