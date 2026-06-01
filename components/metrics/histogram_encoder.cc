751779e936c43bf86e40e4f131783bd8bd346e9c
Luc Nguyen <lucnguyen@google.com>
2026-06-01 15:46:06 -0700

    Add a feature to denylist histograms from UMA logs.
    This change introduces a new feature, `kHistogramDenylist`, which allows
    specifying a comma-separated list of histogram names via a feature
    parameter. Histograms on this denylist will be recorded locally but will
    be skipped during the serialization process for UMA logs, preventing
    them from being uploaded.
    
    Motivation: We are seeing the Variations.FeatureAccess histogram greatly increasing UMA logs size, causing them to be trimmed. As a result, as default value, add Variations.FeatureAccess (and its Early variant) to address this. Rather than just commenting out the histogram, this feature could be reused in the future if this issue happens again.
    
    Bug: 518852743,b:512137734
    Change-Id: I3da5988741227f43af275b16b70cc6e8e222c10f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7891086
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1639739}

--- components/metrics/histogram_encoder.cc
+++ components/metrics/histogram_encoder.cc
@@ -4,26 +4,68 @@
 
 #include "components/metrics/histogram_encoder.h"
 
+#include <algorithm>
 #include <memory>
 #include <string_view>
 
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/histogram_samples.h"
 #include "base/metrics/metrics_hashes.h"
+#include "base/no_destructor.h"
+#include "base/strings/string_split.h"
 
 using base::SampleCountIterator;
 
 namespace metrics {
+namespace {
 
-void EncodeHistogramDelta(std::string_view histogram_name,
-                          const base::HistogramSamples& snapshot,
-                          HistogramEventProto* histogram_proto) {
+// A comma-separated list of histogram names to be skipped/excluded during
+// serialization of metrics logs. When the HistogramDenylist feature is
+// disabled, the default value specified here is used.
+const base::FeatureParam<std::string> kHistogramDenylistParam{
+    &kHistogramDenylist, "histogram_denylist",
+    // TODO: crbug.com/518852743 - These histograms are currently problematic
+    // as they bloat UMA logs, causing them to be trimmed. Remove once fixed.
+    "Variations.FeatureAccess,Variations.FeatureAccessEarly"};
+
+const std::vector<uint64_t>& GetHistogramDenylist() {
+  static const base::NoDestructor<std::vector<uint64_t>> histogram_denylist([] {
+    std::vector<uint64_t> hashes;
+    for (std::string_view name : base::SplitStringPiece(
+             kHistogramDenylistParam.Get(), ",", base::TRIM_WHITESPACE,
+             base::SPLIT_WANT_NONEMPTY)) {
+      hashes.push_back(base::HashMetricName(name));
+    }
+    return hashes;
+  }());
+  return *histogram_denylist;
+}
+
+}  // namespace
+
+BASE_FEATURE(kHistogramDenylist, base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool EncodeHistogramDelta(
+    std::string_view histogram_name,
+    const base::HistogramSamples& snapshot,
+    base::FunctionRef<HistogramEventProto*()> add_histogram_event) {
   DCHECK_NE(0, snapshot.TotalCount());
+
+  uint64_t name_hash = base::HashMetricName(histogram_name);
+  // Note: When the feature is disabled, `kHistogramDenylistParam`'s default
+  // value is used.
+  if (std::ranges::contains(GetHistogramDenylist(), name_hash)) {
+    return false;
+  }
+
+  HistogramEventProto* histogram_proto = add_histogram_event();
   DCHECK(histogram_proto);
 
   // We will ignore the MAX_INT/infinite value in the last element of range[].
 
-  histogram_proto->set_name_hash(base::HashMetricName(histogram_name));
+  histogram_proto->set_name_hash(name_hash);
   if (snapshot.sum() != 0) {
     histogram_proto->set_sum(snapshot.sum());
   }
@@ -54,6 +96,7 @@ void EncodeHistogramDelta(std::string_view histogram_name,
       bucket->clear_min();
     }
   }
+  return true;
 }
 
 }  // namespace metrics
--- components/metrics/histogram_encoder.h
+++ components/metrics/histogram_encoder.h
@@ -10,6 +10,8 @@
 
 #include <string_view>
 
+#include "base/feature.h"
+#include "base/functional/function_ref.h"
 #include "third_party/metrics_proto/chrome_user_metrics_extension.pb.h"
 
 namespace base {
@@ -18,11 +20,19 @@ class HistogramSamples;
 
 namespace metrics {
 
+// Allows specifying a list of histogram names (separated with a comma) to be
+// skipped/excluded during serialization of metrics logs (UMA and PUMA). Note
+// that the histograms will still be recorded locally -- they just won't be put
+// into logs (and therefore won't be uploaded).
+BASE_DECLARE_FEATURE(kHistogramDenylist);
+
 // Record any changes (histogram deltas of counts from |snapshot|) into
-// |histogram_proto| for the given histogram (|histogram_name|).
-void EncodeHistogramDelta(std::string_view histogram_name,
-                          const base::HistogramSamples& snapshot,
-                          HistogramEventProto* histogram_proto);
+// |histogram_proto| for the given histogram (|histogram_name|). Returns false
+// if the histogram changes are not recorded (i.e. due to histogram denylist).
+bool EncodeHistogramDelta(
+    std::string_view histogram_name,
+    const base::HistogramSamples& snapshot,
+    base::FunctionRef<HistogramEventProto*()> add_histogram_event);
 
 }  // namespace metrics
 
