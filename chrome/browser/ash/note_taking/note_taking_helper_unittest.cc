7b16e53987a088b6cce70c6093aaf990256cdcff
Tiago Vignatti <tvignatti@igalia.com>
2026-06-02 12:50:00 -0700

    [ash] Remove dead chrome/browser/ui includes from note_taking tests
    note_taking_helper_unittest.cc never uses Browser or TabStripModel
    (it inherits from BrowserWithTestWindowTest from chrome/test/base/);
    note_taking_helper_browsertest.cc never references NavigateParams.
    Drop the three dead includes.
    
    Bug: 353332589
    Change-Id: I6266259164f9307ce593c97d1fc8d2f0bc3c32e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874977
    Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640397}

--- chrome/browser/ash/note_taking/note_taking_helper_unittest.cc
+++ chrome/browser/ash/note_taking/note_taking_helper_unittest.cc
@@ -35,8 +35,6 @@
 #include "chrome/browser/media/router/media_router_feature.h"
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/web_applications/test/fake_web_app_provider.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
