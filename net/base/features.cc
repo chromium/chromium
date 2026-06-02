43b0e8d1f20baea23e60e0b7d043b40108902190
Ari Chivukula <arichiv@chromium.org>
2026-06-02 15:26:28 -0700

    [TCP] Enable TcpSocketPoolLimitRandomizationForProxy by default
    This was trialed in M150, so can be enabled by default in M151.
    
    Bug: 497837927
    Change-Id: I4b52a1438983f88fbe0f3d48bebd24b00fdcfd7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7818382
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640503}

--- net/base/features.cc
+++ net/base/features.cc
@@ -652,7 +652,7 @@ BASE_FEATURE_PARAM(double,
                    0.2);
 
 BASE_FEATURE(kTcpSocketPoolLimitRandomizationForProxy,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNetTaskScheduler, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE_PARAM(bool,
