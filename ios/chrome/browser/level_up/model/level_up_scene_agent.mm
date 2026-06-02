dfbdb11f021570bc85a136407cf33f6a6d1eb097
Robbie Gibson <rkgibson@google.com>
2026-06-02 09:23:40 -0700

    [iOS] Connect LevelUpSceneAgent to LevelUpService
    This CL connects LevelUpSceneAgent to LevelUpService to listen for user
    actions and mark tasks as completed. For each user action fired, if it
    matches the action for a task, the agent will alert the service that the
    task is completed.
    
    This CL also adds unittests for this behavior.
    
    Bug: b:513246860
    Change-Id: I9caa1f123a5ef8414405ad91a805da6126a52785
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7886191
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640230}

--- ios/chrome/browser/level_up/model/level_up_scene_agent.mm
+++ ios/chrome/browser/level_up/model/level_up_scene_agent.mm
@@ -4,14 +4,25 @@
 
 #import "ios/chrome/browser/level_up/model/level_up_scene_agent.h"
 
+#import <map>
+
 #import "base/functional/bind.h"
 #import "base/logging.h"
+#import "base/memory/raw_ptr.h"
 #import "base/metrics/user_metrics.h"
 #import "base/time/time.h"
+#import "ios/chrome/app/profile/profile_state.h"
+#import "ios/chrome/browser/level_up/model/level_up_service.h"
+#import "ios/chrome/browser/level_up/model/level_up_service_factory.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 
 @implementation LevelUpSceneAgent {
   // The callback registered with `base::AddActionCallback`.
   base::ActionCallback _actionCallback;
+  // The level up service.
+  raw_ptr<LevelUpService> _levelUpService;
+  // Map from user action to task type for fast lookup.
+  std::map<std::string, TaskType> _actionToTaskMap;
 }
 
 - (void)setSceneState:(SceneState*)sceneState {
@@ -34,6 +45,18 @@
   if (_actionCallback) {
     return;
   }
+
+  ProfileIOS* profile = self.sceneState.profileState.profile;
+  if (!profile) {
+    return;
+  }
+  _levelUpService = LevelUpServiceFactory::GetForProfile(profile);
+
+  _actionToTaskMap.clear();
+  for (const auto& [type, info] : _levelUpService->GetTasks()) {
+    _actionToTaskMap[info.trigger_action] = type;
+  }
+
   __weak LevelUpSceneAgent* weakSelf = self;
   _actionCallback = base::BindRepeating(
       ^(const std::string& action, base::TimeTicks action_time) {
@@ -43,8 +66,13 @@
 }
 
 - (void)stopListening {
+  if (!_actionCallback) {
+    return;
+  }
   base::RemoveActionCallback(_actionCallback);
   _actionCallback.Reset();
+  _levelUpService = nullptr;
+  _actionToTaskMap.clear();
 }
 
 - (void)dealloc {
@@ -52,6 +80,14 @@
 }
 
 - (void)onUserAction:(const std::string&)action {
+  if (!_levelUpService) {
+    return;
+  }
+
+  auto it = _actionToTaskMap.find(action);
+  if (it != _actionToTaskMap.end()) {
+    _levelUpService->MarkTaskCompleted(it->second);
+  }
 }
 
 @end
---
+++ ios/chrome/browser/level_up/model/level_up_scene_agent_unittest.mm
@@ -0,0 +1,81 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/level_up/model/level_up_scene_agent.h"
+
+#import "base/memory/raw_ptr.h"
+#import "base/metrics/user_metrics.h"
+#import "base/test/scoped_feature_list.h"
+#import "ios/chrome/app/application_delegate/app_state.h"
+#import "ios/chrome/app/application_delegate/fake_startup_information.h"
+#import "ios/chrome/app/profile/profile_state.h"
+#import "ios/chrome/browser/level_up/model/level_up_service.h"
+#import "ios/chrome/browser/level_up/model/level_up_service_factory.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
+#import "ios/web/public/test/web_task_environment.h"
+#import "testing/gtest/include/gtest/gtest.h"
+#import "testing/platform_test.h"
+
+namespace {
+
+class LevelUpSceneAgentTest : public PlatformTest {
+ protected:
+  void SetUp() override {
+    scoped_feature_list_.InitAndEnableFeature(kIOSLevelUp);
+    PlatformTest::SetUp();
+
+    TestProfileIOS::Builder builder;
+    profile_ = std::move(builder).Build();
+
+    fake_startup_information_ = [[FakeStartupInformation alloc] init];
+    app_state_ =
+        [[AppState alloc] initWithStartupInformation:fake_startup_information_];
+
+    profile_state_ = [[ProfileState alloc] initWithAppState:app_state_];
+    profile_state_.profile = profile_.get();
+
+    scene_state_ = [[SceneState alloc] initWithAppState:app_state_];
+    scene_state_.profileState = profile_state_;
+
+    agent_ = [[LevelUpSceneAgent alloc] init];
+    [scene_state_ addAgent:agent_];
+
+    service_ = LevelUpServiceFactory::GetForProfile(profile_.get());
+  }
+
+  void TearDown() override {
+    [agent_ stopListening];
+    agent_ = nil;
+    scene_state_ = nil;
+    PlatformTest::TearDown();
+  }
+
+  web::WebTaskEnvironment task_environment_;
+  base::test::ScopedFeatureList scoped_feature_list_;
+  std::unique_ptr<TestProfileIOS> profile_;
+  FakeStartupInformation* fake_startup_information_;
+  AppState* app_state_;
+  ProfileState* profile_state_;
+  SceneState* scene_state_;
+  LevelUpSceneAgent* agent_;
+  raw_ptr<LevelUpService> service_;
+};
+
+TEST_F(LevelUpSceneAgentTest, TestActionTriggersCompletion) {
+  // Ensure task is not completed initially.
+  EXPECT_FALSE(service_->IsTaskCompleted(TaskType::kTabGroups));
+
+  // Simulate the scene becoming active to start listening.
+  scene_state_.activationLevel = SceneActivationLevelForegroundActive;
+
+  // Record the action that should trigger completion.
+  base::RecordAction(base::UserMetricsAction("TabGroupCreated"));
+
+  // Verify that the task is now completed.
+  EXPECT_TRUE(service_->IsTaskCompleted(TaskType::kTabGroups));
+}
+
+}  // namespace
