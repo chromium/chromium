f9a719f8d8e7909d64e2dc08c8a92d1501f6ac6b
Norge Vizcay <vizcay@google.com>
2026-06-02 11:23:15 -0700

    personal_context: Swap pref names for Autofill notice and toggle
    Renames preference strings for the PersonalContext notice and settings
    toggle by swapping the 'personal_context' and 'autofill' segments.
    This is a pre-step to moving the prefs to the autofill component.
    
    Bug: 518740027
    Change-Id: I419fe1e163b2f96ea71d2ce2ee12a56cc78f265b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895080
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640330}

--- components/personal_context/core/personal_context_prefs.h
+++ components/personal_context/core/personal_context_prefs.h
@@ -10,17 +10,17 @@ class PrefRegistrySimple;
 namespace personal_context::prefs {
 
 inline constexpr char kPersonalContextInAutofillNoticeShouldBeShown[] =
-    "personal_context.autofill.notice_should_be_shown";
+    "autofill.personal_context.notice_should_be_shown";
 
 inline constexpr char kPersonalContextInAutofillNoticeHasBeenShown[] =
-    "personal_context.autofill.notice_has_been_shown";
+    "autofill.personal_context.notice_has_been_shown";
 
 // Represents the user-visible toggle in Autofill settings. Note that this only
 // represents the settings toggle, which is only one of multiple conditions for
 // PersonalContext to be enabled. Features that want to consume Context must
 // instead check via EnablementService.
 inline constexpr char kPersonalContextInAutofillSettingsToggleStatus[] =
-    "personal_context.autofill.settings_toggle_status";
+    "autofill.personal_context.settings_toggle_status";
 
 void RegisterProfilePrefs(PrefRegistrySimple* registry);
 
