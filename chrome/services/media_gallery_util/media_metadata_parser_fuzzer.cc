1f47d8a8a79eb8865c98a68ad01e09de0edcff80
Edgar Aguilar <eledgar@google.com>
2026-06-02 02:07:25 -0700

    [Fuzzing] Fix media_metadata_parser on Android
    The media_metadata_parser_fuzzer uses content::BrowserTaskEnvironment, which defaults to base::MessagePumpType::UI on Android. This requires initializing the test message loop to avoid a crash in MessagePumpAndroid when RunLoop::Run() is called without a full Java environment.
    
    Fix this by calling base::InitAndroidTestMessageLoop() via a scoped helper struct before the environment is created. This overrides MessagePumpAndroid with a test stub that supports Run(). Also add the necessary //base/test:test_support dependency in BUILD.gn.
    
    Bug: 515821900
    
    R=titouan@chromium.org
    
    Test: Built and executed successfully on an Android emulator.
    Change-Id: Iab0fd2301d8b401c81e4c030ee6b8d3dc3a8db43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7876136
    Reviewed-by: Titouan Rigoudy <titouan@chromium.org>
    Reviewed-by: Nigel Tao <nigeltao@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640017}

--- chrome/services/media_gallery_util/media_metadata_parser_fuzzer.cc
+++ chrome/services/media_gallery_util/media_metadata_parser_fuzzer.cc
@@ -13,17 +13,30 @@
 #include "base/at_exit.h"
 #include "base/containers/span.h"
 #include "base/functional/callback_helpers.h"
+#include "build/build_config.h"
 #include "content/public/test/browser_task_environment.h"
 #include "media/filters/memory_data_source.h"
 #include "testing/libfuzzer/libfuzzer_base_wrappers.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/test/test_support_android.h"
+#endif
+
 class Environment {
  public:
-  Environment() = default;
+  Environment() {
+#if BUILDFLAG(IS_ANDROID)
+    // On Android, BrowserTaskEnvironment creates a UI message pump that does
+    // not support RunLoop::Run(). This installs a stub pump to allow it in
+    // tests.
+    base::InitAndroidTestMessageLoop();
+#endif
+    task_environment_ = std::make_unique<content::BrowserTaskEnvironment>();
+  }
 
  private:
   base::AtExitManager manager_;
-  content::BrowserTaskEnvironment task_environment_;
+  std::unique_ptr<content::BrowserTaskEnvironment> task_environment_;
 };
 
 DEFINE_LLVM_FUZZER_TEST_ONE_INPUT_SPAN(const base::span<const uint8_t> data) {
