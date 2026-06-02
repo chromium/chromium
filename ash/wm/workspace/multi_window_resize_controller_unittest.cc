16fcc6bf11a461ff12514d92390866eb79ad3c88
Mitsuru Oshima <oshima@chromium.org>
2026-06-02 15:49:48 -0700

    Revert "gardener: Disable ModifyWindowDuringResize on Linux"
    This reverts commit d7e8e30d40e52bcec4b98e11013abf832b61de37.
    
    Reason for revert: The test is CrOS only so this had no effect. 
    
    Original change's description:
    > gardener: Disable ModifyWindowDuringResize on Linux
    >
    > Bug: 517366959
    > Change-Id: I42f8c46762fa5d1ed3e4fd8920c0922c5108d2e3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7878548
    > Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    > Owners-Override: Huanpo Lin <robertlin@chromium.org>
    > Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1637500}
    
    Bug: 517366959
    Change-Id: Icfee58f053672cfb0e68ed0de0a93454ee3c8b17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7895615
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1640526}

--- ash/wm/workspace/multi_window_resize_controller_unittest.cc
+++ ash/wm/workspace/multi_window_resize_controller_unittest.cc
@@ -355,13 +355,7 @@ TEST_F(MultiWindowResizeControllerTest, Three) {
 // This test ensures that multi window resizing will not cause a crash/UAF if an
 // attribute of the window involved in the multi resizing (visibility, property
 // or capture) has changed while resizing.
-// TODO(crbug.com/517366959): Re-enable this test on Linux.
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_ModifyWindowDuringResize DISABLED_ModifyWindowDuringResize
-#else
-#define MAYBE_ModifyWindowDuringResize ModifyWindowDuringResize
-#endif
-TEST_F(MultiWindowResizeControllerTest, MAYBE_ModifyWindowDuringResize) {
+TEST_F(MultiWindowResizeControllerTest, ModifyWindowDuringResize) {
   static auto release_capture = [](aura::Window* window) {
     auto* capture_client =
         aura::client::GetCaptureClient(window->GetRootWindow());
