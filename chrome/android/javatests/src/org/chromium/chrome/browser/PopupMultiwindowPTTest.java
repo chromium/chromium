d348c7bb07b0adae374ff1cc7dd8ae2cc749bed7
Henrique Nakashima <hnakashima@chromium.org>
2026-06-03 14:25:20 -0700

    [Android] Re-enable PopupMultiwindowPTTest in Desktop Freeform
    Tested locally and seems to be passing.
    
    Bug: 511288012
    Change-Id: Ib86877ff2c685965d7fb7abc349a206451f5b173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7887903
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641212}

--- chrome/android/javatests/src/org/chromium/chrome/browser/PopupMultiwindowPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/PopupMultiwindowPTTest.java
@@ -16,7 +16,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.transit.TransitAsserts;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.app.tab_activity_glue.PopupCreatorImpl;
@@ -51,7 +50,6 @@ public class PopupMultiwindowPTTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM) // crbug.com/511288012
     public void testBasic() {
         PopupOnClickPageStation page =
                 PopupOnClickPageStation.loadInCurrentTab(
@@ -72,7 +70,6 @@ public class PopupMultiwindowPTTest {
 
     @Test
     @MediumTest
-    @Restriction(DeviceFormFactor.ONLY_TABLET)
     public void testBasicIncognito() {
         final IncognitoNewTabPageStation incognitoEntryPoint =
                 mEntryPage.openNewIncognitoTabOrWindowFast();
