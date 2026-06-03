7eed78634a9a2e4eee1f7c2d28d5f4555db8cde3
Devon Loehr <dloehr@google.com>
2026-06-03 07:32:32 -0700

    [rustmojo] Define `ResponseSender` type
    This CL defines the `ResponseSender` type for multiplex routers,
    which is passed into the handlers that it invokes. The sender
    has two purposes: providing the ability to send responses (as the
    name implies), and providing enough information to register new
    associated endpoints during serialization/deserialization.
    
    The type ends up looking identical to `MultiplexRouterHandle`,
    but it has a very different use and, importantly, does not
    implement `Drop`. In a later CL, it will implement a different
    trait as part of its (de)serialization duties, but that trait
    isn't defined yet, and isn't part of the multiplex router code,
    so we omit it for now.
    
    Bug: 493274453
    Change-Id: Idd420018518072c2c27f250af3171e435af0180c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7884238
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Julia Hansbrough <flowerhack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1640921}

---
+++ mojo/public/rust/bindings/multiplex_router/response_sender.rs
@@ -0,0 +1,34 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// The `ResponseSender` type needs to be public so we can name it in other parts
+// of the code, but we don't actually need to expose any more information about
+// it than the name.
+#![allow(private_interfaces)]
+
+use crate::message::MojomMessage;
+
+use super::endpoint_registry::InterfaceId;
+use super::router::MultiplexRouter;
+
+/// This type is identical to a `MultiplexRouterHandle`, but passed into
+/// handlers so they can send responses and register new associated endpoints.
+/// Unlike `MultiplexRouterHandle`, it does not implement `Drop` because it is
+/// frequently cloned, and short-lived.
+#[derive(Clone)]
+pub struct ResponseSender {
+    pub(super) router: MultiplexRouter,
+    pub(super) interface_id: InterfaceId,
+}
+
+impl ResponseSender {
+    /// Send a response message through the router with the same interface ID
+    /// as the incoming message.
+    ///
+    /// This function returns `false` if the message wasn't sent, because one
+    /// end of the pipe was closed.
+    pub fn send_message(&self, msg: MojomMessage) {
+        self.router.send_message(msg, self.interface_id)
+    }
+}
