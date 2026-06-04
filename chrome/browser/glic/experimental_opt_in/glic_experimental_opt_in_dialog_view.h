6645d7dd191d40496bcec0be9351c51dcb21f1a5
Theodore Olsauskas-Warren <sauski@google.com>
2026-06-03 17:37:57 -0700

    [M150] [Glic] Round experimental opt-in dialog corners
    Original change's description:
    > [Glic] Round experimental opt-in dialog corners
    >
    > Tweak the widget properties so that the corners of the dialog are
    > correctly rounded. Introducing the ViewObserver to do this mirrors the
    > logic in ChromeWebUIDialog & PaymentHandlerWebFlowViewController.
    >
    > Bug: b:518897336
    > Change-Id: I2b4f0ad5dc623eadcb11bda3e37d755d7096b6b5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7890843
    > Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    > Reviewed-by: Dan H <harringtond@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1640272}
    
    (cherry picked from commit 34850aaa5fb5df0185ceb2e8e8fc66ff79be4973)
    
    Bug: 519307530,b:518897336
    Change-Id: I2b4f0ad5dc623eadcb11bda3e37d755d7096b6b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900374
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#113}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_dialog_view.h
+++ chrome/browser/glic/experimental_opt_in/glic_experimental_opt_in_dialog_view.h
@@ -5,7 +5,10 @@
 #ifndef CHROME_BROWSER_GLIC_EXPERIMENTAL_OPT_IN_GLIC_EXPERIMENTAL_OPT_IN_DIALOG_VIEW_H_
 #define CHROME_BROWSER_GLIC_EXPERIMENTAL_OPT_IN_GLIC_EXPERIMENTAL_OPT_IN_DIALOG_VIEW_H_
 
+#include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "ui/base/interaction/element_identifier.h"
+#include "ui/views/view_observer.h"
 #include "ui/views/window/dialog_delegate.h"
 
 class Profile;
@@ -20,7 +23,8 @@ class TabInterface;
 
 namespace glic {
 
-class GlicExperimentalOptInDialogView : public views::DialogDelegate {
+class GlicExperimentalOptInDialogView : public views::DialogDelegate,
+                                        public views::ViewObserver {
  public:
   DECLARE_CLASS_ELEMENT_IDENTIFIER_VALUE(kDialogElementId);
 
@@ -34,6 +38,15 @@ class GlicExperimentalOptInDialogView : public views::DialogDelegate {
   ~GlicExperimentalOptInDialogView() override;
 
   views::WebView* GetWebViewForTesting();
+
+  // views::ViewObserver:
+  void OnViewAddedToWidget(views::View* observed_view) override;
+  void OnViewIsDeleting(views::View* observed_view) override;
+
+ private:
+  raw_ptr<views::WebView> web_view_ = nullptr;
+  base::ScopedObservation<views::View, views::ViewObserver> view_observation_{
+      this};
 };
 
 }  // namespace glic
