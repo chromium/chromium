65dee54aa13dd5ec12125ca5864026d1e6d1b7a0
Evan Liu <evliu@google.com>
2026-06-04 12:12:54 -0700

    Fix build: Update AudioBus::ToInterleaved in speech tests
    Migrates speech_recognition_service_browsertest.cc to the new span-based
    ToInterleaved API and fixes the audio_data vector allocation size.
    
    Change-Id: I5b193634b268c7deef7a03d42869d26605dacf74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7901736
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1641849}

--- chrome/browser/speech/speech_recognition_service_browsertest.cc
+++ chrome/browser/speech/speech_recognition_service_browsertest.cc
@@ -377,9 +377,8 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionServiceTest, RecognizePhrase) {
   size_t bytes_written = 0u;
   ASSERT_TRUE(handler->CopyTo(bus.get(), &bytes_written));
 
-  std::vector<int16_t> audio_data(bus->frames());
-  bus->ToInterleaved<media::SignedInt16SampleTypeTraits>(bus->frames(),
-                                                         audio_data.data());
+  std::vector<int16_t> audio_data(bus->frames() * bus->channels());
+  bus->ToInterleaved<media::SignedInt16SampleTypeTraits>(audio_data);
 
   constexpr size_t kMaxChunkSize = 1024;
   constexpr int kReplayAudioCount = 2;
@@ -433,9 +432,8 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionServiceTest,
   size_t bytes_written = 0u;
   ASSERT_TRUE(handler->CopyTo(bus.get(), &bytes_written));
 
-  std::vector<int16_t> audio_data(bus->frames());
-  bus->ToInterleaved<media::SignedInt16SampleTypeTraits>(bus->frames(),
-                                                         audio_data.data());
+  std::vector<int16_t> audio_data(bus->frames() * bus->channels());
+  bus->ToInterleaved<media::SignedInt16SampleTypeTraits>(audio_data);
   constexpr size_t kMaxChunkSize = 1024;
 
   // Send an audio chunk to the service. It will output "Hey Google". When the
