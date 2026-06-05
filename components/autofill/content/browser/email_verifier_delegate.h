10e1ddc1a719dde4fe241e10bd372e768d9a447d
Yi Gu <yigu@chromium.org>
2026-06-05 05:52:16 -0700

    [EVP] Add Autofill side metrics
    This CL adds metrics from the autofill side.
    
    Fixed: 518553011
    Change-Id: I4017d3791c8ba8421fd3fd5a69274b9451eab4a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7884254
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642282}

--- components/autofill/content/browser/email_verifier_delegate.h
+++ components/autofill/content/browser/email_verifier_delegate.h
@@ -11,6 +11,8 @@
 #include "base/functional/callback.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
+#include "base/observer_list.h"
+#include "base/observer_list_types.h"
 #include "components/autofill/core/browser/foundations/autofill_client.h"
 #include "components/autofill/core/browser/foundations/autofill_manager.h"
 #include "components/autofill/core/browser/foundations/scoped_autofill_managers_observation.h"
@@ -24,6 +26,25 @@ namespace autofill {
 
 class AutofillClient;
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+// LINT.IfChange(EvpAutofillFlowResult)
+enum class EvpAutofillFlowResult {
+  kSuccess = 0,
+  kTokenFieldHasNoNonce = 1,
+  kUserPrefDisabled = 2,
+  kStrikeDatabaseBlock = 3,
+  kVerifierUnavailable = 4,
+  kNotVerifiable = 5,
+  kUserDeclinedPermissionPrompt = 6,
+  kUserIgnoredPermissionPrompt = 7,
+  kVerificationFailed = 8,
+  kManagerDestroyed = 9,
+  kTokenSentToRenderer = 10,
+  kMaxValue = kTokenSentToRenderer,
+};
+// LINT.ThenChange(//tools/metrics/histograms/metadata/blink/enums.xml:EvpAutofillFlowResult)
+
 // The EmailVerifierDelegate is owned by the ChromeAutofillClient (and hence is
 // one per WebContents) and observes all AutofillManagers.
 // It listens to filling events and triggers the email verification flow if an
@@ -34,11 +55,20 @@ class AutofillClient;
 class EmailVerifierDelegate : public AutofillManager::Observer,
                               public content::WebContentsObserver {
  public:
+  class Observer : public base::CheckedObserver {
+   public:
+    ~Observer() override = default;
+    virtual void OnFlowCompleted(EvpAutofillFlowResult result) = 0;
+  };
+
   explicit EmailVerifierDelegate(AutofillClient* client);
   EmailVerifierDelegate(const EmailVerifierDelegate&) = delete;
   EmailVerifierDelegate& operator=(const EmailVerifierDelegate&) = delete;
 
   ~EmailVerifierDelegate() override;
+
+  void AddObserver(Observer* observer);
+  void RemoveObserver(Observer* observer);
 
   // AutofillManager::Observer:
   void OnFillOrPreviewForm(
@@ -73,7 +103,7 @@ class EmailVerifierDelegate : public AutofillManager::Observer,
   void OnIsVerifiable(
       base::WeakPtr<AutofillManager> manager,
       FieldGlobalId email_field_id,
-      FieldGlobalId nonce_field_id,
+      FieldGlobalId token_field_id,
       gfx::RectF email_field_bounds,
       std::u16string email,
       std::string nonce,
@@ -87,6 +117,13 @@ class EmailVerifierDelegate : public AutofillManager::Observer,
               const std::string& nonce,
               const content::webid::EmailVerifier::Result& result);
 
+  void OnVerificationResponseReceived(base::WeakPtr<AutofillManager> manager,
+                                      FieldGlobalId email_field_id,
+                                      std::string email,
+                                      FieldGlobalId token_field_id,
+                                      net::SchemefulSite issuer_site,
+                                      std::optional<std::string> token);
+
   void OnEmailVerificationDecision(
       base::WeakPtr<AutofillManager> manager,
       FieldGlobalId email_field_id,
@@ -95,6 +132,18 @@ class EmailVerifierDelegate : public AutofillManager::Observer,
       std::string nonce,
       content::webid::EmailVerifier::Result result,
       AutofillClient::EmailVerificationPermissionUiResult ui_result);
+
+  void NotifyFlowCompleted(EvpAutofillFlowResult result);
+
+  class MetricsObserver : public Observer {
+   public:
+    MetricsObserver();
+    ~MetricsObserver() override;
+    void OnFlowCompleted(EvpAutofillFlowResult result) override;
+  };
+
+  MetricsObserver metrics_observer_;
+  base::ObserverList<Observer> observers_;
 
   ScopedAutofillManagersObservation observation_{this};
   std::map<FieldGlobalId, GURL> issuers_;
