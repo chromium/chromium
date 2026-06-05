d81d1854c72e28c2f44dace5535002fc815aaeaf
Andres Ricardo Perez <andresrperez@chromium.org>
2026-06-05 13:55:23 -0700

    [Memory Coordinator] Add traits to MemoryReclaimer
    Updates MemoryReclaimerPressureListener to explicitly define its memory
    coordination semantics using MemoryConsumerTraits. This provides the
    global Memory Coordinator with accurate information regarding the
    scale, eviction cost, and statefulness of the cache to optimize
    system-wide memory reclamation decisions.
    
    Bug: 489671163
    Change-Id: If07bd2f0ec9b1bf57816037c438d4536b1950a5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7845551
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Andres Ricardo Perez Rojas <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1642580}

--- content/renderer/memory_reclaimer_pressure_listener.cc
+++ content/renderer/memory_reclaimer_pressure_listener.cc
@@ -4,15 +4,34 @@
 
 #include "content/renderer/memory_reclaimer_pressure_listener.h"
 
+#include "base/memory_coordinator/traits.h"
 #include "base/memory_coordinator/utils.h"
 #include "partition_alloc/memory_reclaimer.h"
 
 namespace content {
 
+namespace {
+
+constexpr base::MemoryConsumerTraits kMemoryReclaimerTraits{
+    // Reclaims thread caches (megabytes).
+    base::MemoryConsumerTraits::EstimatedMemoryUsage::kMedium,
+    // Loops through active thread allocation buckets.
+    base::MemoryConsumerTraits::ReleaseMemoryCost::kRequiresTraversal,
+    // Frees unused pages back to OS.
+    base::MemoryConsumerTraits::InformationRetention::kLossless,
+    // Tasks are posted to the main thread runner.
+    base::MemoryConsumerTraits::ExecutionType::kAsynchronous,
+    // Reacts to a binary threshold. No specified limit.
+    base::MemoryConsumerTraits::SupportsMemoryLimit::kNo,
+    // One-shot reclamation.
+    base::MemoryConsumerTraits::IsStateful::kNo};
+
+}  // namespace
+
 MemoryReclaimerPressureListener::MemoryReclaimerPressureListener()
     : memory_consumer_registration_(
           "MemoryReclaimerPressureListener",
-          /*traits=*/std::nullopt,  // TODO(crbug.com/489671163): Fill traits.
+          kMemoryReclaimerTraits,
           this,
           base::AsyncMemoryConsumerRegistration::CheckUnregister::kDisabled,
           base::AsyncMemoryConsumerRegistration::CheckRegistryExists::
