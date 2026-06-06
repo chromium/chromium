f6eddafb7f2d36d6a30ee279c14e20b1493966f1
James Leung <jamesleung@google.com>
2026-06-06 10:30:07 -0700

    [M150] [voice_search] Add waiting placeholder when permission prompt
    Original change's description:
    > [voice_search] Add waiting placeholder when permission prompt
    >
    > * Changed 'Listening' placeholder to 'Waiting' when permission prompt is open to avoid misunderstandings now that voice search
    > is staying open but not listening when the permission prompt is open.
    > This is more important now than before since there needs to be
    > compatibility with features like colored tic tac voice animation and
    > PEPC being off. Without those features, the `Listening` placeholder
    > exists.
    > * Turn off clang format in composebox voice search html file and make sure HTML abides by html styling guidelines
    >
    > First half of screencast:
    > screencast/cast/NjQ4MTA1OTMzNTExMDY1NnxiZDdlOThlYS1lMQ
    >
    > Bug: b:518848753
    > Change-Id: I6cf5028fe289c1ced66e0f9763cd1c8bdfc27b3f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900777
    > Commit-Queue: James Leung <jamesleung@google.com>
    > Reviewed-by: Josiah Kiehl <kiehl@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1641551}
    
    (cherry picked from commit b7b05aa42ac7e45551d48cdad7a105c27728c146)
    
    Bug: 520514304,b:518848753
    Change-Id: I6cf5028fe289c1ced66e0f9763cd1c8bdfc27b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902855
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#405}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- ui/webui/resources/cr_components/composebox/composebox_voice_search.html.ts
+++ ui/webui/resources/cr_components/composebox/composebox_voice_search.html.ts
@@ -8,63 +8,61 @@ import type {ComposeboxVoiceSearchElement} from './composebox_voice_search.js';
 import {VoiceSearchError} from './composebox_voice_search.js';
 
 export function getHtml(this: ComposeboxVoiceSearchElement) {
+  // clang-format off
   return html`
     <div id="container">
       <div id="error-container" ?hidden="${!this.shouldShowErrorScrim_()}">
         <span id="error-message">${this.errorMessage_}</span>
-        ${
-      this.detailedError_ === VoiceSearchError.NO_MATCH ?
-          html`<a id="tryAgainLink" href="#" @click="${this.onTryAgainClick_}">
-            ${this.i18n('tryAgain')}
-          </a>
-        ` :
-          ''}
-        <a id="details" part="voice-details-link" target="_blank" href="${
-      this.detailsUrl_}"
-            @click="${this.onLinkClick_}">
-          ${this.i18n('voiceDetails')}
+        ${this.detailedError_ === VoiceSearchError.NO_MATCH ?
+            html`<a id="tryAgainLink" href="#"
+                @click="${this.onTryAgainClick_}"
+              >${this.i18n('tryAgain')}
+            </a>`
+        : ''}
+        <a id="details" part="voice-details-link" target="_blank"
+            href="${this.detailsUrl_}"
+            @click="${this.onLinkClick_}"
+          >${this.i18n('voiceDetails')}
         </a>
       </div>
-      ${
-      this.liveTranscriptEnabled ? html`<textarea id="input"
+      ${this.isPermissionPromptOpen_ ? html`
+          <textarea id="input"
+              class="${this.shouldShowErrorScrim_() ? 'hidden' : ''}"
+              placeholder="${this.i18n('voiceWaiting')}" disabled
+          ></textarea>`
+      : ''}
+      ${this.liveTranscriptEnabled && !this.isPermissionPromptOpen_ ?
+          html`<textarea id="input"
               .value="${this.transcript_}"
               placeholder="${this.listeningPlaceholder_}"
-              class="${this.shouldShowErrorScrim_() ? 'hidden' : ''}" disabled>
-          </textarea>
-          ` :
-                                   ''}
-      ${
-  !this.submitStopButtonsEnabled || this.shouldShowErrorScrim_() ?
-      html`<cr-icon-button id="closeButton" class="icon-clear"
+              class="${this.shouldShowErrorScrim_() ? 'hidden' : ''}" disabled
+          ></textarea>`
+      : ''}
+      ${!this.submitStopButtonsEnabled || this.shouldShowErrorScrim_() ?
+          html`<cr-icon-button id="closeButton" class="icon-clear"
               part="voice-close-button"
               title="${this.i18n('voiceClose')}" @click="${this.onCloseClick_}"
-              ></cr-icon-button>
-          ` :
-      ''}
-      ${
-      this.submitStopButtonsEnabled ?
-      html`<div id="bottomActions"
-              class="${
-          this.shouldShowErrorScrim_() ?
-          'hidden' :
-          ''}">
+              ></cr-icon-button>`
+      : ''}
+      ${this.submitStopButtonsEnabled ?
+          html`<div id="bottomActions"
+              class="${this.shouldShowErrorScrim_() ? 'hidden' : ''}">
             <cr-icon-button id="stopButton" part="voice-stop-button"
                 iron-icon="composebox:stop"
                  title="${this.i18n('voiceStop')}"
                 @click="${this.onStopClick_}"
-                >
-            </cr-icon-button>
+            ></cr-icon-button>
             <cr-composebox-submit id="submitButton"
                 exportparts="submit"
                 part="voice-submit-button"
                 .iconType="${this.submitButtonIconType}"
                 .submitButtonTitle="${this.i18n('composeboxSubmitButtonTitle')}"
                 @submit-click="${this.onSubmitClick_}"
-                ?disabled="${!(this.finalResult_ || this.interimResult_)}">
-            </cr-composebox-submit>
-          </div>
-        ` :
-      ''}
+                ?disabled="${!(this.finalResult_ || this.interimResult_)}"
+            ></cr-composebox-submit>
+          </div>`
+      : ''}
     </div>
   `;
+  // clang-format on
 }
