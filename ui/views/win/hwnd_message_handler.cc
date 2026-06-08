05424b3b4e68b7f3815903f115661f7af9891c61
David Bienvenu <davidbienvenu@chromium.org>
2026-06-07 17:13:13 -0700

    [M148] win: Handle window closing during move
    Original change's description:
    > win: Handle window closing during move
    >
    > Use weak refs to handle window closing during monitor change.
    >
    > Bug: 520210566
    > Change-Id: I7f9f218f88ef8e22e3dcffc9afa9d290a6af3fb9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7905269
    > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    > Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1642376}
    
    (cherry picked from commit 4dd1788994966b0c0f8a72ea5f150680f4b447bf)
    
    Bug: 520652702,520210566
    Change-Id: I7f9f218f88ef8e22e3dcffc9afa9d290a6af3fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7902767
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7778@{#4262}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- ui/views/win/hwnd_message_handler.cc
+++ ui/views/win/hwnd_message_handler.cc
@@ -2393,7 +2393,11 @@ LRESULT HWNDMessageHandler::OnInputEvent(UINT message,
 }
 
 void HWNDMessageHandler::OnMove(const gfx::Point& point) {
+  auto ref = msg_handler_weak_factory_.GetWeakPtr();
   delegate_->HandleMove();
+  if (!ref) {
+    return;
+  }
   SetMsgHandled(FALSE);
 }
 
