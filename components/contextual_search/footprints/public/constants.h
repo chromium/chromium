581b256cf3fee2ce9a8a4631a0330afc1de79d08
Adam Yao <adamyao@google.com>
2026-06-08 16:32:28 -0700

    [aim-drive] - add new pcontext flow FACs setting for drive integration
    Bug: b:517991968
    Change-Id: I4834e797374d983c6e6506287365f50fcbdc4635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7909409
    Commit-Queue: Adam Yao <adamyao@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Cr-Commit-Position: refs/heads/main@{#1643525}

--- components/contextual_search/footprints/public/constants.h
+++ components/contextual_search/footprints/public/constants.h
@@ -12,6 +12,11 @@ namespace contextual_search {
 // Setting ID for CONTEXTUAL_SEARCH_DRIVE_DISCLAIMER_ACCEPTED.
 inline constexpr int32_t kContextualSearchDriveDisclaimerAccepted = 274;
 
+// Must stay in sync with PERSONAL_CONTEXT_SEARCH_USING_WORKSPACE in
+// google3/personalization/footprints/boq/activity_controls/proto/
+// activity_controls_setting_enum.proto
+inline constexpr int32_t kPersonalContextSearchUsingWorkspace = 183;
+
 }  // namespace contextual_search
 
 #endif  // COMPONENTS_CONTEXTUAL_SEARCH_FOOTPRINTS_PUBLIC_CONSTANTS_H_
