5f0f1e2698a4908ff3477d4d2b12a04b7e349426
Colin Blundell <blundell@chromium.org>
2026-06-08 03:30:44 -0700

    [//ui] Generate dummy ANGLE dlls on Win when linking ANGLE statically
    This CL is a followup to [1]. On the bot side, Windows requires not just
    that the pdb files be present but that they contain valid symbols.
    
    [1] https://chromium-review.googlesource.com/7870935
    
    Bug: 514229803
    Change-Id: I58d202910935eb164d8dac18c703c949d2de00ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7899735
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643079}

---
+++ ui/gl/dummy_stub.cc
@@ -0,0 +1,10 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <windows.h>
+
+// Dummy entry point to make it a valid DLL.
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+  return TRUE;
+}
