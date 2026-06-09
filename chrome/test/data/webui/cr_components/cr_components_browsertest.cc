5fa53076de16692299bb06757ef577f278b539d9
Ari Chivukula <arichiv@chromium.org>
2026-06-09 07:11:51 -0700

    [gardener] Disable ComposeboxVoiceSearch test on ChromeOS.
    The test is currently failing on ChromeOS, so it is being disabled.
    
    Bug: 508867284
    Change-Id: Ife1cc8ad9bf5206d5b20a44f73cc87599890ca5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7910832
    Owners-Override: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1643934}

--- chrome/test/data/webui/cr_components/cr_components_browsertest.cc
+++ chrome/test/data/webui/cr_components/cr_components_browsertest.cc
@@ -324,7 +324,14 @@ IN_PROC_BROWSER_TEST_F(CrComponentsComposeboxTest, RecentTabChip) {
 }
 #endif
 
-IN_PROC_BROWSER_TEST_F(CrComponentsComposeboxTest, ComposeboxVoiceSearch) {
+// TODO(crbug.com/508867284): Re-enable when fixed on ChromeOS.
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_ComposeboxVoiceSearch DISABLED_ComposeboxVoiceSearch
+#else
+#define MAYBE_ComposeboxVoiceSearch ComposeboxVoiceSearch
+#endif
+IN_PROC_BROWSER_TEST_F(CrComponentsComposeboxTest,
+                       MAYBE_ComposeboxVoiceSearch) {
   RunTest("cr_components/composebox/composebox_voice_search_test.js",
           "mocha.run()");
 }
