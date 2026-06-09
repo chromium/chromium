0f7e9295fb5ad7788756a1f5ede1d046dd5b394c
Gyuyoung Kim <qkim@google.com>
2026-06-08 19:52:17 -0700

    [M144-LTS] Fix CreateNewWindow() to correctly handle a reused tab
    Under normal circumstances, a non-null return value from
    `ShowCreatedWindow()` will return the WebContents that was created by
    the earlier call to `CreateNewWindow()`. However, under certain
    circumstances `ShowCreatedWindow()` will return a different pre-existing
    WebContents and allow the just-created WebContents to expire, along with
    its frame tree node, widget host, and frame host.
    
    This CL ensures that the code doesn't rely on any of the newly-created
    objects after the call to `ShowCreatedWindow()`, and instead uses the
    normal accessors on WebContents to retrieve them.
    
    (cherry picked from commit 3c20517b961d59dea34098ef7e9fd4ca955eb081)
    
    Bug: 487338366
    Change-Id: I84a5c5cf7f395d708baf71c6a43b39e4099f61ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7610581
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1590960}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7822040
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Gyuyoung Kim (xWF) <qkim@google.com>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Owners-Override: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/branch-heads/7559@{#4968}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -9954,8 +9954,8 @@ void RenderFrameHostImpl::CreateNewWindow(
 
   DCHECK(IsRenderFrameLive());
 
-  // The non-owning pointer |new_frame_tree| is valid in this stack frame since
-  // nothing can delete it until this thread is freed up again.
+  // The non-owning pointer |new_frame_tree| is valid in this stack frame at
+  // least until the call to ShowCreatedWindow() below.
   FrameTree* new_frame_tree =
       delegate_->CreateNewWindow(this, *params, is_new_browsing_instance,
                                  was_consumed, cloned_namespace.get());
@@ -10013,11 +10013,11 @@ void RenderFrameHostImpl::CreateNewWindow(
   bool wait_for_debugger =
       devtools_instrumentation::ShouldWaitForDebuggerInWindowOpen();
 
-  // NOTE: if the call to ShowCreatedWindow() below returns nullptr, then
+  // NOTE: after the call to ShowCreatedWindow() below it's possible that
   // new_frame_tree, new_main_rfh, and new_main_rwh will all have been destroyed
-  // and point to freed memory! To preserve legacy behavior, we still need to
-  // send a fully-populated reply along with kSuccess, so we construct the reply
-  // here prior to ShowCreatedWindow().
+  // and point to freed memory! We still need to send a fully-populated reply
+  // along with kSuccess, so we construct the reply here prior to
+  // ShowCreatedWindow().
 
   blink::VisualProperties visual_properties;
   // If we can't get an accurate set of VisualProperties after ShowCreatedWindow
@@ -10038,28 +10038,37 @@ void RenderFrameHostImpl::CreateNewWindow(
   new_main_rfh->render_view_host()->RenderViewCreated(new_main_rfh);
 
   if (base::FeatureList::IsEnabled(blink::features::kCombineNewWindowIPCs)) {
-    // ShowCreatedWindow will return nullptr if the new WebContents has been
-    // destroyed, as described above (see NOTE).
+    int routing_id = new_rwh->GetRoutingID();
+
+    // These can point to freed memory after the call to ShowCreatedWindow(),
+    // even if that method returns non-null. Null them out here to prevent
+    // inadvertent UAF in the future.
+    new_frame_tree = nullptr;
+    new_main_rfh = nullptr;
+    new_rwh = nullptr;
+
     WebContents* shown_contents = delegate()->ShowCreatedWindow(
-        this, new_rwh->GetRoutingID(), params->disposition, *params->features,
+        this, routing_id, params->disposition, *params->features,
         params->consumes_user_activation);
 
-    if (!shown_contents) {
-      // These point to freed memory, so null them out to prevent inadvertent
-      // UAF in the future (see NOTE above).
-      new_frame_tree = nullptr;
-      new_main_rfh = nullptr;
-      new_rwh = nullptr;
-    } else if (new_main_rfh->GetView()) {
-      // Cannot populate window geometry until after ShowCreatedWindow().
-      reply->widget_screen_rect.emplace(
-          new_main_rfh->GetView()->GetViewBounds());
-      reply->window_screen_rect.emplace(
-          new_main_rfh->GetView()->GetBoundsInRootWindow());
-      reply->visual_properties = new_rwh->GetVisualProperties();
+    // Cannot populate window geometry until after ShowCreatedWindow().
+    if (shown_contents) {
+      if (auto* shown_rfh = shown_contents->GetPrimaryMainFrame()) {
+        if (auto* shown_rwh = static_cast<RenderFrameHostImpl*>(shown_rfh)
+                                  ->GetLocalRenderWidgetHost()) {
+          if (auto* shown_rwhv = shown_rwh->GetView()) {
+            reply->widget_screen_rect.emplace(shown_rwhv->GetViewBounds());
+            reply->window_screen_rect.emplace(
+                static_cast<RenderWidgetHostViewBase*>(shown_rwhv)
+                    ->GetBoundsInRootWindow());
+            reply->visual_properties =
+                static_cast<RenderWidgetHostImpl*>(shown_rwh)
+                    ->GetVisualProperties();
+          }
+        }
+      }
     }
   }
-
   std::move(callback).Run(mojom::CreateNewWindowStatus::kSuccess,
                           std::move(reply));
 }
