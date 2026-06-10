6c8954eff3820784a9d595da4377010063c3f1fb
Menghan YANG <myuu@google.com>
2026-06-10 05:15:03 -0700

    [M150] [Mobile][Signin] Add default url to cross device flags
    Original change's description:
    > [Mobile][Signin] Add default url to cross device flags
    >
    > This CL updates the `cross-device-signin` feature flag configuration in
    > about_flags.cc (Android) and about_flags.mm (iOS) to include the default
    > destination URL ("https://www.google.com/chrome/go-mobile") as a feature
    > variation.
    >
    > This is required for manually testing the feature without command line
    > access (e.g. test approval for the launch), and will be cherry-picked
    > for M150
    >
    > Bug: 521307776
    > Change-Id: Idce73d7ab5bc8f64884d9ecd535a18b973e3df34
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900086
    > Auto-Submit: Menghan Yang <myuu@google.com>
    > Reviewed-by: Lucia Giorgi <quacksort@google.com>
    > Commit-Queue: Samar Chehade <samarchehade@google.com>
    > Reviewed-by: Samar Chehade <samarchehade@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1643796}
    
    (cherry picked from commit aaf3a4345a7ae4e89b44db2937348360d742399e)
    
    Bug: 521703094,521307776
    Change-Id: Idce73d7ab5bc8f64884d9ecd535a18b973e3df34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7916488
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#767}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -4309,6 +4309,15 @@ const FeatureEntry::FeatureVariation kSeamlessSigninVariations[] = {
      kSeamlessSigninTwoButtonsSignin, nullptr},
     {"with compact layout and \"Sign in as\" button (group 4)",
      kSeamlessSigninCompactSignin, nullptr}};
+
+const FeatureEntry::FeatureParam kCrossDeviceSigninDefaultUrl[] = {
+    {"url", "https://www.google.com/chrome/go-mobile"},
+};
+
+const FeatureEntry::FeatureVariation kCrossDeviceSigninVariations[] = {
+    {"Default URL (https://www.google.com/chrome/go-mobile)",
+     kCrossDeviceSigninDefaultUrl, nullptr},
+};
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_ANDROID)
@@ -11526,7 +11535,9 @@ const FeatureEntry kFeatureEntries[] = {
 
     {"cross-device-signin", flag_descriptions::kCrossDeviceSigninName,
      flag_descriptions::kCrossDeviceSigninDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(switches::kCrossDeviceSignin)},
+     FEATURE_WITH_PARAMS_VALUE_TYPE(switches::kCrossDeviceSignin,
+                                    kCrossDeviceSigninVariations,
+                                    "CrossDeviceSignin")},
 
 #endif  // BUILDFLAG(IS_ANDROID)
 
