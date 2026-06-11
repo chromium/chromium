5d7c924b8b7bbb58cc8f603b9cd2268cb8ba2a37
Hira Mahmood <hiramahmood@google.com>
2026-06-11 09:52:56 -0700

    [M150] [iOS] Fix floaty vertical position when keyboard collapses
    Original change's description:
    > [iOS] Fix floaty vertical position when keyboard collapses
    >
    > With Fullscreen Refactoring enabled, obscured inset updates are no
    > longer fired when the keyboard appears or disappears. Because of this,
    > the expanded floaty overlay gets stranded in an elevated position after
    > the keyboard collapses.
    >
    > This CL fixes the issue by explicitly updating the overlay offset in
    > `OnKeyboardStateChanged` when the keyboard is dismissed.
    >
    > Before: https://screencast.googleplex.com/cast/NTAwMzcwNjQxNTE4NTkyMHwwNzIwZGUxNS1mYw
    > After: https://screencast.googleplex.com/cast/NDkzMTUxNjYwNDg3NDc1MnwxMjk1YzRkOC0yMA
    >
    > Fixed: 521801194
    > Change-Id: I8582b58fe7af369d219a9e9873c03f31467bb04e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7915424
    > Reviewed-by: Adam Arcaro <adamta@google.com>
    > Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1644613}
    
    (cherry picked from commit f2b595105d11bae96db7cd727cc8aa164427e18f)
    
    Bug: 522749071,521801194
    Change-Id: I8582b58fe7af369d219a9e9873c03f31467bb04e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7922301
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#938}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -514,6 +514,15 @@ void GeminiBrowserAgent::OnKeyboardStateChanged(bool is_visible) {
     ShowFloatyIfInvoked(/*animated=*/false,
                         gemini::FloatyUpdateSource::Keyboard);
     is_hidden_by_keyboard_ = false;
+  } else {
+    bool is_visible_and_expanded =
+        is_floaty_invoked_ && !is_floaty_temporarily_hidden_ &&
+        last_shown_view_state_ == ios::provider::GeminiViewState::kExpanded;
+
+    if (IsFullscreenRefactoringEnabled() && is_visible_and_expanded) {
+      ios::provider::UpdateOverlayOffsetWithOpacity(GetFloatyOffset(),
+                                                    GetFloatyProgress());
+    }
   }
 }
 
