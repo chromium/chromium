a454a1a2d90904aa3ee0790feab80e182d68d760
Scott Yoder <scottyoder@google.com>
2026-06-11 08:35:41 -0700

    [M150] [ios] Fix landscape top toolbar safe area inset on iOS 26.0
    Original change's description:
    > [ios] Fix landscape top toolbar safe area inset on iOS 26.0
    >
    > On iOS 26.0, UIKit's safe area layout system fails to drop the top safe
    > area inset to 0 in landscape mode when the status bar is hidden. This
    > mismatch causes an unexpected 20pt gap at the top of the toolbar on
    > iPhone.
    >
    > This CL fixes this by overriding safeAreaInsets in SceneView (the root
    > view of the window) to return 0 for the top inset when the device is in
    > compact height (iPhone landscape) specifically on iOS 26.0.
    >
    > Fixed: 519521695
    > Change-Id: Ibc42ae0a87936bd62ec0921e8a784f3445dad885
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7910587
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Scott Yoder <scottyoder@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1644575}
    
    (cherry picked from commit 48feaee531afc2f72c21b888494cb19f8148dff3)
    
    Bug: 522226409,519521695
    Change-Id: Ibc42ae0a87936bd62ec0921e8a784f3445dad885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7921993
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#924}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- ios/chrome/browser/scene/ui/scene_view.mm
+++ ios/chrome/browser/scene/ui/scene_view.mm
@@ -13,4 +13,22 @@
   [self.delegate sceneViewDidMoveToWindow:self];
 }
 
+- (UIEdgeInsets)safeAreaInsets {
+  UIEdgeInsets insets = [super safeAreaInsets];
+  // If the device is in landscape on an iPhone (compact height), there are no
+  // physical cutouts or status bars at the top of the screen. On iOS 26.0
+  // specifically, the window's safeAreaInsets.top is incorrectly left at 20pt;
+  // we manually override it to 0. This is skipped on iOS 26.1+ and iPad where
+  // the layout engine correctly handles the inset.
+  if (@available(iOS 26.0, *)) {
+    if (!@available(iOS 26.1, *)) {
+      if (insets.top > 0 && self.traitCollection.verticalSizeClass ==
+                                UIUserInterfaceSizeClassCompact) {
+        insets.top = 0;
+      }
+    }
+  }
+  return insets;
+}
+
 @end
