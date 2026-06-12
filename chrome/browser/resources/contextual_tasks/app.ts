61f40075f8bc9c088126f92bc0341d06201037b0
Duncan Mercer <mercerd@google.com>
2026-06-12 16:01:29 -0700

    [149] [Contextual Tasks] Optimize OAuth fetch and backoff.
    Original change's description:
    > [Contextual Tasks] Optimize OAuth fetch and backoff.
    >
    > - Trigger OAuth token pre-fetch early in
    > OnNavigationToAiPageIntercepted, parallelizing it with the WebUI
    > creation.
    > - Make the exponential backoff policy less aggressive by reducing
    > initial delay to 150ms, factor to 1.5, and max delay to 2s.
    > - Adds many new metrics to help understand OAuth caused latency.
    >
    > Bug: b:521515911, b:521516269
    > Change-Id: Ifdccd618a2635e8a6538e508d0385cc685d95cab
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7909290
    > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Commit-Queue: Duncan Mercer <mercerd@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1644928}
    
    Bug: 522949463
    Change-Id: Ifdccd618a2635e8a6538e508d0385cc685d95cab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7933713
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7827@{#3061}
    Cr-Branched-From: 9f3e9aaccba63bd2ec30334e45e0bfd07ebcc8f1-refs/heads/main@{#1625079}

--- chrome/browser/resources/contextual_tasks/app.ts
+++ chrome/browser/resources/contextual_tasks/app.ts
@@ -353,6 +353,14 @@ export class ContextualTasksAppElement extends CrLitElement {
   // double animations.
   private isInitialFrameLoad_: boolean = true;
 
+  private constructorStartTime_: number;
+  private isFirstLoadCommit_: boolean = true;
+
+  constructor() {
+    super();
+    this.constructorStartTime_ = performance.now();
+  }
+
   private updateThemeFromUrl(url: URL) {
     const csParam = url.searchParams.get('cs');
     if (csParam === '0') {
@@ -759,6 +767,13 @@ export class ContextualTasksAppElement extends CrLitElement {
     // If is from inner iframe and not from main webview URL:
     if (!ev.isTopLevel) {
       return;
+    }
+    if (this.isFirstLoadCommit_) {
+      this.isFirstLoadCommit_ = false;
+      const latencyMs =
+          Math.round(performance.now() - this.constructorStartTime_);
+      chrome.metricsPrivate.recordMediumTime(
+          'ContextualTasks.OAuth.StartToCommitLatency', latencyMs);
     }
     this.updateBasicModeAfterNavigation();
     this.maybeOnThreadFrameTopLevelNavigation(ev.url);
