9ec1bafffd52c3d4b82249e18bd1e4b7b3536725
Paul Jensen <pauljensen@chromium.org>
2026-06-16 15:07:07 -0700

    [M150] Fix screen reader synthetic mouse click handling
    Original change's description:
    > Fix screen reader synthetic mouse click handling
    >
    > Synthetic mouse clicks coming from a screen reader (e.g. Mac VoiceOver)
    > aren't able to hold pointer capture, presumably because there is no
    > pointer involved when the synthetic click comes from the keyboard.
    >
    > Fixed: 520327168
    > Change-Id: I42a89ebe310610cc2a05f5c02f5ccaafd4dd8e15
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7904758
    > Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    > Reviewed-by: Youseff Bourouphel <ybourouphael@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1642403}
    
    (cherry picked from commit 0b946804857e5e56c9f0f69c8f8b134707cdbff3)
    
    Bug: 522687341,520327168
    Change-Id: I42a89ebe310610cc2a05f5c02f5ccaafd4dd8e15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7926266
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#1386}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- chrome/browser/resources/webui_toolbar/app.ts
+++ chrome/browser/resources/webui_toolbar/app.ts
@@ -51,7 +51,7 @@ import {PointerProxyImpl} from './pointer_proxy.js';
 import type {PointerProxy} from './pointer_proxy.js';
 import {PermissionChipElement} from './permission_chip.js';
 import {ReadonlyOmniboxElement} from './readonly_omnibox.js';
-import {getClickSourceType, getContextMenuSourceType} from './toolbar_button.js';
+import {getClickSourceType, getContextMenuSourceType, PressHandler} from './toolbar_button.js';
 
 export {
   BrowserProxyImpl,
@@ -61,6 +61,7 @@ export {
   EventDispositionFlag,
   getClickSourceType,
   getContextMenuSourceType,
+  PressHandler,
   IconTable,
   IconType,
   LhsChipIdentifier,
