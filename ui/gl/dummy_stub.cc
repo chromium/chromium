2c58af443ac6a2d4c9da5e7215fe05a056b38e1a
Colin Blundell <blundell@chromium.org>
2026-06-17 05:58:56 -0700

    //ui: Generate dummy libs on all platforms when linking ANGLE statically
    This CL is a followup to [1]. It turns out that on Linux (and perhaps
    ChromeOS/Mac), it is also necessary that the symbol files contain valid
    symbols for symbol uploading. This CL switches to using the the approach
    of generating dummy ANGLE dylibs on Linux/ChromeOS/Mac.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/7899735
    
    (cherry picked from commit 7fb1b2e0dd982932174d2d15ba7d883d0db9587c)
    
    Bug: 514229803
    Change-Id: Ida9dd01f25d9bcdb9f699b4e5c4c3d2bb032518a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7947003
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1648108}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7957317
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7893@{#4}
    Cr-Branched-From: 744630f612b7b708c9a70d6dae9fc47270c18db4-refs/heads/main@{#1646714}

--- ui/gl/dummy_stub.cc
+++ ui/gl/dummy_stub.cc
@@ -2,9 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/build_config.h"
+
+#if BUILDFLAG(IS_WIN)
 #include <windows.h>
 
 // Dummy entry point to make it a valid DLL.
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
   return TRUE;
 }
+#else
+extern "C" void DummyAngleStub() {}
+#endif
