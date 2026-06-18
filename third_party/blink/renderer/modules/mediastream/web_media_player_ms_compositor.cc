919cdf9902b7c6415d75f1afddc56c2d0502f849
Johannes Kron <kron@chromium.org>
2026-06-18 04:09:53 -0700

    [M148] Protect rendering_frame_buffer_ with current_frame_lock_
    Original change's description:
    > Protect rendering_frame_buffer_ with current_frame_lock_
    >
    > Add thread-safety annotations to `rendering_frame_buffer_` and related
    > historical rendering variables to ensure safe concurrent access.
    >
    > Lock `current_frame_lock_` when determining the preferred render
    > interval. Introduce an internal helper method to allow execution when
    > the lock is already acquired by the caller. Additionally, update the
    > callback used to create the video renderer algorithm wrapper to use
    > `CrossThreadBindRepeating` to guarantee thread safety during
    > cross-thread execution.
    >
    > Change-Id: I5573960e28ce36530146815b664f61b5cf2a0585
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7921532
    > Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    > Commit-Queue: Johannes Kron <kron@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1645865}
    
    (cherry picked from commit 624c1fe1c7167c571305b01686588b5203269368)
    
    Bug: 525280419
    Change-Id: I5573960e28ce36530146815b664f61b5cf2a0585
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7961839
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7778@{#4383}
    Cr-Branched-From: 77f495ee216d4c3cc784d33658bad4778c0680ee-refs/heads/main@{#1610480}

--- third_party/blink/renderer/modules/mediastream/web_media_player_ms_compositor.cc
+++ third_party/blink/renderer/modules/mediastream/web_media_player_ms_compositor.cc
@@ -645,6 +645,14 @@ void WebMediaPlayerMSCompositor::PutCurrentFrame() {
 
 base::TimeDelta WebMediaPlayerMSCompositor::GetPreferredRenderInterval() {
   DCHECK(video_frame_compositor_task_runner_->BelongsToCurrentThread());
+  base::AutoLock auto_lock(current_frame_lock_);
+  return GetPreferredRenderIntervalInternal();
+}
+
+base::TimeDelta
+WebMediaPlayerMSCompositor::GetPreferredRenderIntervalInternal() {
+  DCHECK(video_frame_compositor_task_runner_->BelongsToCurrentThread());
+  current_frame_lock_.AssertAcquired();
   if (!rendering_frame_buffer_) {
     DCHECK_GE(last_render_length_, base::TimeDelta());
     return last_render_length_;
@@ -1045,7 +1053,7 @@ void WebMediaPlayerMSCompositor::SetCurrentFrame(
       (expected_display_time.has_value() && !expected_display_time->is_null())
           ? *expected_display_time
           : now;
-  last_preferred_render_interval_ = GetPreferredRenderInterval();
+  last_preferred_render_interval_ = GetPreferredRenderIntervalInternal();
   ++presented_frames_;
 
   TRACE_EVENT_INSTANT2("media", "SetCurrentFrame Timestamps",
@@ -1164,6 +1172,7 @@ void WebMediaPlayerMSCompositor::ReplaceCurrentFrameWithACopy() {
 
 void WebMediaPlayerMSCompositor::SetAlgorithmEnabledForTesting(
     bool algorithm_enabled) {
+  base::AutoLock auto_lock(current_frame_lock_);
   if (!algorithm_enabled) {
     rendering_frame_buffer_.reset();
     return;
@@ -1171,9 +1180,9 @@ void WebMediaPlayerMSCompositor::SetAlgorithmEnabledForTesting(
 
   if (!rendering_frame_buffer_) {
     rendering_frame_buffer_ = std::make_unique<VideoRendererAlgorithmWrapper>(
-        blink::BindRepeating(
+        ConvertToBaseRepeatingCallback(CrossThreadBindRepeating(
             &WebMediaPlayerMSCompositor::MapTimestampsToRenderTimeTicks,
-            Unretained(this)),
+            CrossThreadUnretained(this))),
         &media_log_);
   }
 }
