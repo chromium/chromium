64db911497fc9645ec56a10c98734b596390c117
Mickey Burks <mickeyburks@chromium.org>
2026-06-19 06:04:45 -0700

    [M150] [GlowUp] Define settings colors for custom themes
    Original change's description:
    > [GlowUp] Define settings colors for custom themes
    >
    > These colors were defined after an early return that prevented them from
    > being defined for custom themes when the WebuiRefresh2026 feature is
    > enabled. Moving the definitions above the return ensures the red
    > placeholder is not used for custom themes.
    >
    > Fixed: 523305742
    > Change-Id: I37d95d41735a2a5919d87cd7b300ea4e6d20e38e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7946338
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Auto-Submit: Mickey Burks <mickeyburks@chromium.org>
    > Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1647126}
    
    (cherry picked from commit 89868dc5d0eb6f76b9d48750ff2a374ce1f152be)
    
    Bug: 524645809,523305742
    Change-Id: I37d95d41735a2a5919d87cd7b300ea4e6d20e38e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7963868
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#1678}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- chrome/browser/ui/color/material_chrome_color_mixer.cc
+++ chrome/browser/ui/color/material_chrome_color_mixer.cc
@@ -142,6 +142,13 @@ void AddMaterialChromeColorMixer(ui::ColorProvider* provider,
   // Extension manager WebUI colors.
   mixer[kColorExtensionManagerHighlightText] = {ui::kColorSysPrimary};
 
+  // Settings columned header colors.
+  mixer[kColorSettingsColumnedSectionDescriptionHeader] = {
+      ui::kColorSysPrimary};
+
+  // Settings info card colors.
+  mixer[kColorSettingsInfoCardBackground] = {ui::kColorSysBaseContainer};
+
   // Lens overlay colors.
   mixer[kColorLensOverlayToastBackground] = {ui::kColorSysInverseSurface};
   mixer[kColorLensOverlayToastButtonBorder] = {ui::kColorSysInverseOnSurface};
@@ -435,13 +442,6 @@ void AddMaterialChromeColorMixer(ui::ColorProvider* provider,
       ui::kColorSysBaseContainerElevated};
   mixer[kColorOmniboxChipOnSystemBlockedActivityIndicatorForeground] = {
       ui::kColorSysError};
-
-  // Settings columned header colors.
-  mixer[kColorSettingsColumnedSectionDescriptionHeader] = {
-      ui::kColorSysPrimary};
-
-  // Settings info card colors.
-  mixer[kColorSettingsInfoCardBackground] = {ui::kColorSysBaseContainer};
 
   // Tabstrip tab alert colors.
   mixer[kColorTabAlertAudioPlayingActiveFrameActive] = {
