aa83cf9cc9ba223998de6e5d73a158e8be6ed3c2
Steinar H. Gunderson <sesse@chromium.org>
2026-06-23 17:02:48 -0700

    [M150] Run the full StyleAdjuster after animations.
    Original change's description:
    > Run the full StyleAdjuster after animations.
    >
    > When moving StyleAdjusted styles into the MPC, we split the
    > StyleAdjuster into two parts, one cacheable and one uncacheable.
    > However, on animated styles, we rerun the cascade, which may
    > override what the StyleAdjuster did; and we did not remember
    > to rerun the uncacheable part in this case. Fix that, and also
    > for MISU, which probably had the same problem (although getting
    > it to actually be a problem in practice would probably be hard
    > with our current list of MISU-allowed properties).
    >
    > Fixed: 525309813
    > Change-Id: I72f720cc5e1fd178cc34c6d0ab7cdaa9f720a7c0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7967920
    > Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    > Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    > Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    > Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1650176}
    
    (cherry picked from commit 1b420eff4355c6c0b4aa0d785efecb04168e72b5)
    
    Bug: 526489958,525309813
    Change-Id: I72f720cc5e1fd178cc34c6d0ab7cdaa9f720a7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7965670
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Daniel Yip <danielyip@google.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#2044}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -1393,6 +1393,11 @@ const ComputedStyle* StyleResolver::ResolveStyle(
                                   styles_animated, 1);
     StyleAdjuster::AdjustComputedStyle(
         state, IsForPseudoElement(*element, style_request) ? nullptr : element);
+    StyleAdjuster::RunUncacheableStyleAdjustment(
+        state.StyleBuilder(), *element,
+        IsForPseudoElement(*element, style_request) ? state.GetPseudoElement()
+                                                    : element,
+        state.GetStyledElement());
   }
 
   ApplyAnchorData(state);
@@ -2017,6 +2022,11 @@ void StyleResolver::ApplyBaseStyle(
 
     StyleAdjuster::AdjustComputedStyle(
         state, IsForPseudoElement(*element, style_request) ? nullptr : element);
+    StyleAdjuster::RunUncacheableStyleAdjustment(
+        state.StyleBuilder(), *element,
+        IsForPseudoElement(*element, style_request) ? state.GetPseudoElement()
+                                                    : element,
+        state.GetStyledElement());
 
     // Normally done by StyleResolver::MaybeAddToMatchedPropertiesCache(),
     // when applying the cascade. Note that this is probably redundant
