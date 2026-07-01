33e5ea6428a513e42d6f98ec4be70752e1bddd53
Nathan Memmott <memmott@chromium.org>
2026-07-01 08:16:30 -0700

    Reland "LiteRT LM: Enable LiteRT LM by default"
    This reverts commit 83dfe7359012046c2a2f987d7a1c2dd72fd84912.
    
    Reason for revert: The bug that caused the roll has been fixed.
    
    Original change's description:
    > Revert "LiteRT LM: Enable LiteRT LM by default"
    >
    > This reverts commit b8ddd2894371e4c20d340991d7805a650c3c96e9.
    >
    > Reason for revert: litert-lm constrained decoding is broken
    >
    > Failure Link: b/522950022
    >
    > Original change's description:
    > > LiteRT LM: Enable LiteRT LM by default
    > >
    > > Bug: 454420498
    > > Change-Id: I7263173e06f94b901e0750ed2ee8601facadde72
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7920010
    > > Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    > > Auto-Submit: Nathan Memmott <memmott@chromium.org>
    > > Commit-Queue: Nathan Memmott <memmott@chromium.org>
    > > Commit-Queue: Robbie McElrath <rmcelrath@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1645029}
    >
    > Bug: 454420498
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: I6cb3c7df0e108aa2dbec9bb086515d8f64d7df00
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7926660
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Robbie McElrath <rmcelrath@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1645716}
    
    Bug: 454420498
    Change-Id: Idc935d51b8ca0d70d8fec8dca9ebe6396dd3f871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7959885
    Auto-Submit: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655517}

--- services/on_device_model/public/cpp/features.cc
+++ services/on_device_model/public/cpp/features.cc
@@ -12,7 +12,7 @@ BASE_FEATURE(kOnDeviceModelForceCpuBackend, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOnDeviceModelCpuBackend, base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kOnDeviceModelLitertLmBackend, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kOnDeviceModelLitertLmBackend, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kOnDeviceModelGpuCache, base::FEATURE_DISABLED_BY_DEFAULT);
 
