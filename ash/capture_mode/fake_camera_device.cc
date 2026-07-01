fd912ff128d68e87a7afa1739eb066d2e15735ca
Kalvin Lee <kdlee@chromium.org>
2026-07-01 16:42:50 -0700

    IWYU: `base::span`
    This change was generated (and formatted) mostly automatically.
    For specific asks, leave a comment to elicit a fleshy response.
    
    This particular CL is scoped to
    ash/capture_mode
    
    This CL was uploaded by git cl split.
    
    R=xdai@chromium.org
    
    Bug: 529948677
    Change-Id: I5edc465dd4476777631dd29cb8d06fcf857e5e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8030220
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1655820}

--- ash/capture_mode/fake_camera_device.cc
+++ ash/capture_mode/fake_camera_device.cc
@@ -8,6 +8,7 @@
 #include <memory>
 
 #include "base/compiler_specific.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
