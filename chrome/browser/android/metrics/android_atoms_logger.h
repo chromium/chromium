4b976f3e707dbfc4626a8d92d2279756d2dc0402
Chirag Arora <heychirag@google.com>
2026-07-02 15:50:05 -0700

    Revert "Use MetricsReportingChoiceService for consent"
    Reverted CL: crrev.com/c/7890546
    
    This change is being reverted because we have changed the backend
    implementation approach.
    
    Bug: b:483043192
    Change-Id: I191be3b2b43198c0d577b57db196ab1159c5a0f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032904
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Chirag Arora <heychirag@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656296}

--- chrome/browser/android/metrics/android_atoms_logger.h
+++ chrome/browser/android/metrics/android_atoms_logger.h
@@ -8,7 +8,6 @@
 #include <memory>
 #include <vector>
 
-#include "base/callback_list.h"
 #include "base/containers/span.h"
 #include "base/functional/callback.h"
 #include "base/functional/function_ref.h"
@@ -67,7 +66,6 @@ class AndroidAtomsLogger {
 
   bool metrics_reporting_enabled_ = true;
   PrefChangeRegistrar pref_change_registrar_;
-  base::CallbackListSubscription consent_change_subscription_;
   std::vector<
       std::unique_ptr<base::StatisticsRecorder::ScopedHistogramSampleObserver>>
       observers_;
