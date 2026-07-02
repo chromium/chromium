520ffda7fe20b7b1ca45781ceb0f53787c0d553a
Mikel Astiz <mastiz@chromium.org>
2026-07-02 11:00:40 -0700

    [M151] Reland "[sync] Enable Android build prefix uploading by default"
    Original change's description:
    > Reland "[sync] Enable Android build prefix uploading by default"
    >
    > This is a reland of commit 559c3019f5d39236402292e8f4a8fb9bcf8281b3
    >
    > Reason to reland: there's now a decision to move forward with this
    > feature.
    >
    > Original change's description:
    > > [sync] Enable Android build prefix uploading by default
    > >
    > > This functionality can improve the user experience when dealing with
    > > device lists, as it contributes to more user-friendly device names.
    > >
    > > Bug: 510816366
    > > Change-Id: Ib7f4fc2e78d39f6dffd01e9e82cce65955523205
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7983944
    > > Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    > > Commit-Queue: Michael Tatarski <mtatarski@google.com>
    > > Reviewed-by: Michael Tatarski <mtatarski@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1650901}
    >
    > Bug: 510816366
    > Change-Id: I3bb6fcaba068cabdc3072db662d3a7e227942a7e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7985484
    > Auto-Submit: Mikel Astiz <mastiz@chromium.org>
    > Reviewed-by: Ankush Singh <ankushkush@google.com>
    > Commit-Queue: Ankush Singh <ankushkush@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1655437}
    
    (cherry picked from commit 6ce0f2d3d7af17417adf9322d9e2beb0c87be4af)
    
    Bug: 530583685,510816366
    Change-Id: I3bb6fcaba068cabdc3072db662d3a7e227942a7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032516
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7922@{#132}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -211,7 +211,7 @@ BASE_FEATURE(kSyncSearchEnginesAndroidLFF, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kAlwaysRegisterSessionsInvalidationsAndroid,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kSyncUploadAndroidBuildFingerprintPrefix,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(IS_CHROMEOS)
