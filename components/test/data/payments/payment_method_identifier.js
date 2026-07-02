7c42446286f0693dd3f6e09265ac8d381ab9f370
Stephen McGruer <smcgruer@chromium.org>
2026-07-02 14:24:57 -0700

    [Payments] Clean up unused code from PMI test data
    Basic card is no longer supported in Chromium. This CL cleans up all
    unused code from the payment method identifier test data files:
    
    - Removes basic-card and other unused constants (alicePayMethod,
      bobPayMethod) from payment_method_identifier.js.
    - Removes unused functions (run, checkCanMakePayment,
      checkHasEnrolledInstrument, buy) from payment_method_identifier.js.
    - Removes all buttons from
      payment_request_payment_method_identifier_test.html as they are
      no longer used by any tests.
    
    The only test using these files (Url_Valid) calls the remaining
    buyHelper() function directly with custom methods, and continues to
    pass.
    
    Change-Id: Ia26dc2a56ea5c5fabbd29b9a42d1e8cadf415d9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8036303
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Auto-Submit: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656268}

--- components/test/data/payments/payment_method_identifier.js
+++ components/test/data/payments/payment_method_identifier.js
@@ -4,39 +4,6 @@
  * found in the LICENSE file.
  */
 
-const basicCardMethod = {
-  supportedMethods: 'basic-card',
-};
-
-const basicMastercardVisaMethod = {
-  supportedMethods: 'basic-card',
-  data: {
-    supportedNetworks: ['mastercard', 'visa'],
-  },
-};
-
-const basicVisaMethod = {
-  supportedMethods: 'basic-card',
-  data: {
-    supportedNetworks: ['visa'],
-  },
-};
-
-const basicMastercardMethod = {
-  supportedMethods: 'basic-card',
-  data: {
-    supportedNetworks: ['mastercard'],
-  },
-};
-
-const alicePayMethod = {
-  supportedMethods: 'https://alicepay.test/webpay',
-};
-
-const bobPayMethod = {
-  supportedMethods: 'https://bobpay.test/webpay',
-};
-
 const defaultDetails = {
   total: {
     label: 'Total',
@@ -46,43 +13,6 @@ const defaultDetails = {
     },
   },
 };
-
-/**
- * Runs |testFunction| and prints any result or error.
- *
- * @param {function} testFunction A function with no argument and returns a
- * Promise.
- */
-function run(testFunction) {
-  try {
-    testFunction().then(print).catch(print);
-  } catch (error) {
-    print(error);
-  }
-}
-
-/**
- * Calls PaymentRequest.canMakePayment() and prints out the result.
- * @param {sequence<PaymentMethodData>} methodData The supported methods.
- * @private
- */
-function checkCanMakePayment(methodData) {
-  run(() => {
-    const request = new PaymentRequest(methodData, defaultDetails);
-    return request.canMakePayment();
-  });
-}
-
-/**
- * Calls PaymentRequest.hasEnrolledInstrument() and prints out the result.
- * @param {sequence<PaymentMethodData>} methodData The supported methods.
- */
-function checkHasEnrolledInstrument(methodData) {
-  run(() => {
-    const request = new PaymentRequest(methodData, defaultDetails);
-    return request.hasEnrolledInstrument();
-  });
-}
 
 /**
  * Calls PaymentRequest.show() and prints out the result.
@@ -110,10 +40,3 @@ function buyHelper(methodData) {
   }
 }
 
-/**
- * Merchant requests payment via either "mastercard" or "basic-card" with "visa"
- * as the supported network.
- */
-function buy() {
-  buyHelper([basicMastercardVisaMethod]);
-}
