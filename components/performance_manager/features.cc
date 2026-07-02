a5d14ad0193bafd2edfea12ec25232d7ada85733
Aldo Culquicondor <acondor@chromium.org>
2026-07-02 10:55:24 -0700

    Enable GlicActuationPriorityVoter in all platforms
    Bug: 513209932
    Change-Id: I5efaa2e93c21fe90ca5ac6c3fb014b7c645e35e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025671
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656175}

--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -312,7 +312,7 @@ BASE_FEATURE(kDisableTabDiscarding, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kUseLoadingStateToDetectBackgroundTitleOrFaviconUpdate,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kGlicActuationPriorityVoter, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kGlicActuationPriorityVoter, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kIgnoreMediaQueryFaviconUpdates, base::FEATURE_ENABLED_BY_DEFAULT);
 
