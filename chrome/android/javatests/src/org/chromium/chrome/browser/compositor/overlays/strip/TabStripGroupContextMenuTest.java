a5fccb1ea9406e35cfec5f06809d890463df25ac
Henrique Nakashima <hnakashima@chromium.org>
2026-07-03 13:56:04 -0700

    Revert "re-enable TabStripGroupContextMenuTest in desktop freeform"
    This reverts commit d0caec85f3a830da0f8a1237920bacd8fd918598.
    
    Reason for revert: Still flaky in Desktop Freeform
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/android-desktop-17-x64-rel-emu-tests/959/overview
    
    Original change's description:
    > re-enable TabStripGroupContextMenuTest in desktop freeform
    >
    > Bug: 511288697
    > Change-Id: I012fc4885e731c1626865a5104c14d14d37938b7
    > Include-Ci-Only-Tests: chromium.android.desktop:android-desktop-x64-rel-15-tests|chrome_public_test_apk, android-desktop-16-x64-rel-emu-tests
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7900489
    > Reviewed-by: Neil Coronado <nemco@google.com>
    > Commit-Queue: Zhe Li <zheliooo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1653452}
    
    Bug: 511288697
    Change-Id: I6cfecdf5a925502858a76ea6b826df5b19c1e50d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8041201
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Reviewed-by: Aldo Culquicondor <acondor@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656742}

--- chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripGroupContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/compositor/overlays/strip/TabStripGroupContextMenuTest.java
@@ -73,7 +73,6 @@ import org.chromium.components.tab_groups.TabGroupColorId;
 import org.chromium.components.tab_groups.TabGroupsFeatureMap;
 import org.chromium.ui.KeyboardVisibilityDelegate;
 import org.chromium.ui.base.DeviceFormFactor;
-import org.chromium.ui.base.DeviceInput;
 import org.chromium.ui.hierarchicalmenu.HierarchicalMenuController;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -90,6 +89,7 @@ import org.chromium.ui.modaldialog.ModalDialogManager;
 })
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP)
+@DisableIf.Device(DeviceFormFactor.DESKTOP) // crbug.com/511288697
 public class TabStripGroupContextMenuTest {
     @Rule
     public AutoResetCtaTransitTestRule mActivityTestRule =
@@ -412,7 +412,6 @@ public class TabStripGroupContextMenuTest {
     @Test
     @SmallTest
     @Feature("KeyboardA11y")
-    @DisableIf.Device(DeviceFormFactor.DESKTOP)
     public void testKeyboardFocusAndActivation() {
         // Prepare standard state and show menu.
         prepareStandardState();
@@ -686,14 +685,12 @@ public class TabStripGroupContextMenuTest {
         onView(withId(R.id.tab_group_title)).perform(click());
 
         // Verify keyboard is displayed.
-        if (!DeviceInput.supportsKeyboard(mPage.getActivity())) {
-            CriteriaHelper.pollUiThread(
-                    () ->
-                            delegate.isKeyboardShowing(
-                                    mActivityTestRule
-                                            .getActivity()
-                                            .getCompositorViewHolderForTesting()));
-        }
+        CriteriaHelper.pollUiThread(
+                () ->
+                        delegate.isKeyboardShowing(
+                                mActivityTestRule
+                                        .getActivity()
+                                        .getCompositorViewHolderForTesting()));
 
         // Enter new title in text box and press "enter" to dismiss keyboard to update group title.
         onView(withId(R.id.tab_group_title))
