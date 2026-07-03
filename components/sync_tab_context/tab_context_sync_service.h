946ee8842f8fb77af31dd1835c031925e94dc77f
Mikel Astiz <mastiz@chromium.org>
2026-07-03 10:10:17 -0700

    [sync] Introduce initial API for Tab Context service
    The design is not finalized but this API gives a rough sense of the
    responsibilities of the service.
    
    Change-Id: Iec0b6bff4f094c5ffd20654513ee652ac5e41b30
    Bug: 527991726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8034247
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656641}

--- components/sync_tab_context/tab_context_sync_service.h
+++ components/sync_tab_context/tab_context_sync_service.h
@@ -5,10 +5,19 @@
 #ifndef COMPONENTS_SYNC_TAB_CONTEXT_TAB_CONTEXT_SYNC_SERVICE_H_
 #define COMPONENTS_SYNC_TAB_CONTEXT_TAB_CONTEXT_SYNC_SERVICE_H_
 
+#include <optional>
+#include <string>
+
+#include "base/functional/callback_forward.h"
 #include "components/keyed_service/core/keyed_service.h"
 
 namespace sync_tab_context {
 
+// Service that allows sync-ing page context to the server in encrypted form.
+// The design is based on two primitives:
+// 1. Containers: a set of entries that are logically grouped and share
+//    encryption keys.
+// 2. Entries: individual data units that can be created, mutated or deleted.
 class TabContextSyncService : public KeyedService {
  public:
   TabContextSyncService();
@@ -16,6 +25,23 @@ class TabContextSyncService : public KeyedService {
 
   TabContextSyncService(const TabContextSyncService&) = delete;
   TabContextSyncService& operator=(const TabContextSyncService&) = delete;
+
+  // Creates a new container that can later be used to upload entries into.
+  // Returns nullopt in case of failure.
+  virtual std::optional<ContainerId> CreateContainer() = 0;
+
+  // Inserts or updates an entry into a container identified via
+  // `container_id`. `entry_id` acts as uniqueness key within the container.
+  virtual bool UploadPageContext(const ContainerId& container_id,
+                                 const std::string& entry_id,
+                                 std::string page_context) = 0;
+
+  // Asynchronously returns an access token that can be used to access the
+  // contents of a container. The precise means the token may be consumed is
+  // unspecified and implementation-specific.
+  virtual void GetContainerAccessToken(
+      const ContainerId& container_id,
+      base::OnceCallback<void(std::optional<std::string>)> cb) = 0;
 };
 
 }  // namespace sync_tab_context
