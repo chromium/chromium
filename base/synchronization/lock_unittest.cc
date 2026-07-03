1f7d564f7ed8bb0d3ce679ba45a81cbfedc47c1c
Fiona Verzivolli <fionav@google.com>
2026-07-03 09:12:50 -0700

    Turn on histogram pre-inflation in LockMetricsRecorder
    This change removes the intermediate sample buffer in
    LockMetricsRecorder. Lock contention samples are now recorded directly
    to UMA histograms.
    
    To prevent allocations during sample recording, the BaseLock and
    PartitionAllocLock histograms are now pre-inflated using the
    kDisableSingleSampleOptimizationFlag. This eliminates the need to report
    buffered samples when the thread goes idle, simplifying the recording
    process.
    
    Bug: 518786382
    Change-Id: I1d77b2c925ecb7a0e345bfb6694489ad9a41f4a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8016223
    Reviewed-by: Anand Ravi <anandrv@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Fiona Verzivolli <fionav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656614}

--- base/synchronization/lock_unittest.cc
+++ base/synchronization/lock_unittest.cc
@@ -34,6 +34,7 @@
 #include "base/system/sys_info.h"
 #include "base/test/bind.h"
 #include "base/test/gtest_util.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
@@ -634,24 +635,11 @@ class LockTrySpinTest : public testing::Test {
     PlatformThreadHandle handle_;
   };
 
-  bool DidRecordLockMetricsSample() {
-    bool sample_recorded = false;
-    auto* recorder = LockMetricsRecorder::GetForCurrentThread();
-    if (recorder) {
-      recorder->ForEachSample(
-          LockMetricsRecorder::LockType::kBaseLock,
-          [&sample_recorded](const TimeDelta&) { sample_recorded = true; });
-    }
-    return sample_recorded;
-  }
-
-  void ClearLockMetricsSamples() {
-    // Clear any samples that may have been recorded.
-    auto* recorder = LockMetricsRecorder::GetForCurrentThread();
-    if (recorder) {
-      recorder->ForEachSample(LockMetricsRecorder::LockType::kBaseLock,
-                              [](const TimeDelta&) {});
-    }
+  int GetBaseLockSampleCount(const base::HistogramTester& tester) const {
+    return tester
+        .GetHistogramSamplesSinceCreation(
+            "Scheduling.ContendedLockAcquisitionTime.BaseLock.LockTrySpinTest")
+        ->TotalCount();
   }
 
  private:
@@ -687,22 +675,31 @@ TEST_F(LockTrySpinTest, MAYBE_TrySpinAvoidsSyscall) {
       });
   base::Lock::InitializeFeatures();
 
-  ClearLockMetricsSamples();
   {
+    base::HistogramTester histogram_tester;
+
     TestThread thread;
     thread.CreateLockContention();
+
+    // We expect zero samples because the spin succeeded locally without
+    // blocking syscall.
+    EXPECT_EQ(GetBaseLockSampleCount(histogram_tester), 0);
   }
-  EXPECT_FALSE(DidRecordLockMetricsSample());
 
   // Set the try-spin count to zero to ensure that the thread always waits for
   // the lock in the kernel.
   internal::LockImpl::SetTrySpinCount(0);
-  ClearLockMetricsSamples();
+
   {
+    base::HistogramTester histogram_tester;
+
     TestThread thread;
     thread.CreateLockContention();
+
+    // We expect at least 1 sample because the mutex was contended blocked in
+    // kernel.
+    EXPECT_GE(GetBaseLockSampleCount(histogram_tester), 1);
   }
-  EXPECT_TRUE(DidRecordLockMetricsSample());
 }
 #endif  // BUILDFLAG(IS_POSIX)
 
