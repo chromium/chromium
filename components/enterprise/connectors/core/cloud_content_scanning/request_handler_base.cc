98cf5a2b4a814947d051b2b3ce2bebee3691a5e6
Elias Homsi <eliashomsi@google.com>
2026-07-03 08:44:16 -0700

    Map DeepScanAccessPoint::COPY to the clipboard copy event trigger.
    The GetEventTrigger function now returns
    kClipboardCopyDataTransferEventTrigger for DeepScanAccessPoint::COPY.
    
    BUG: 325455508
    Change-Id: If9ea6df6f3f732509d39cfad37d498097fd87ee0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025288
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Elias Homsi <eliashomsi@google.com>
    Commit-Queue: Elias Homsi <eliashomsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656602}

--- components/enterprise/connectors/core/cloud_content_scanning/request_handler_base.cc
+++ components/enterprise/connectors/core/cloud_content_scanning/request_handler_base.cc
@@ -68,6 +68,7 @@ std::string RequestHandlerBase::access_point_string() const {
     case DeepScanAccessPoint::DOWNLOAD:
       return kFileDownloadDataTransferEventTrigger;
     case DeepScanAccessPoint::COPY:
+      return kClipboardCopyDataTransferEventTrigger;
     case DeepScanAccessPoint::PRINT:
       return kPagePrintDataTransferEventTrigger;
   }
