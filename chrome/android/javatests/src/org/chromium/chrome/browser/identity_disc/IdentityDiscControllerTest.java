a49a87e19a3b30e18500c12d8b6b0c15ae5b3547
Sophie Crowley <crowleso@google.com>
2026-07-03 08:23:33 -0700

    [Signin][Android] Re-enable testIdentityDiscWithSwitchToIncognito
    This test was disabled due to flakiness on the Brya build which seems to have since resolved itself (see: https://crbug.com/496912352). The corresponding test for SigninLevelUpButton has also been re-enabled (https://crrev.com/c/8019945).
    
    Change-Id: Ieba510c1dbdc63b97035d553919793231f45afb7
    Bug: 489053128
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035042
    Commit-Queue: Samar Chehade <samarchehade@google.com>
    Reviewed-by: Samar Chehade <samarchehade@google.com>
    Reviewed-by: Łukasz Barczyński <lbarczynski@google.com>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Auto-Submit: Sophie Crowley <crowleso@google.com>
    Cr-Commit-Position: refs/heads/main@{#1656591}

--- chrome/android/javatests/src/org/chromium/chrome/browser/identity_disc/IdentityDiscControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/identity_disc/IdentityDiscControllerTest.java
@@ -59,7 +59,6 @@ import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableLeakChecks;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
@@ -528,7 +527,6 @@ public class IdentityDiscControllerTest {
     @Test
     @MediumTest
     @SuppressWarnings("CheckReturnValue")
-    @DisabledTest(message = "crbug.com/489053128")
     public void testIdentityDiscWithSwitchToIncognito() {
         mSigninTestRule.addAccountThenSignin(TestAccounts.ACCOUNT1);
         ViewUtils.waitForVisibleView(withId(R.id.optional_toolbar_button));
