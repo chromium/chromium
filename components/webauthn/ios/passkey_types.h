e11fa81301448ed9e9fe3631f2b084050710dff3
Rafał Godlewski <rgod@google.com>
2026-07-03 09:43:29 -0700

    [iOS][Passkeys] Move TODO to a dedicated cleanup bug
    Bug: 460485614
    Change-Id: I232daab485004fb436fa45229f18d71b74470d60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032494
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1656631}

--- components/webauthn/ios/passkey_types.h
+++ components/webauthn/ios/passkey_types.h
@@ -16,7 +16,7 @@ namespace webauthn {
 
 // Represents the code of an error returned when the user dismisses the GPM Pin
 // flow by clicking the "Cancel" button.
-// TODO(crbug.com/460485614): Define an enum and parse it in keychain provider.
+// TODO(crbug.com/530911220): Define an enum and parse it in keychain provider.
 static const NSInteger kErrorUserDismissedGPMPinFlow = -105;
 
 // Block type used for the completion of the primary action button tap in
