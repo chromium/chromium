cbacc3951d999f8a720c1688f5206d16bda30d28
Hao Dong <spdonghao@google.com>
2026-07-06 11:20:25 -0700

    Enable USE_P_LINK_IN_HELP by default.
    Bug: 365755043
    Change-Id: I516513f4838d6b85b398de8702296be98e51c38b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025851
    Commit-Queue: Hao Dong <spdonghao@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Auto-Submit: Hao Dong <spdonghao@google.com>
    Commit-Queue: Theresa Wellington <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657327}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -860,7 +860,7 @@ BASE_FEATURE(kUseInitialNetworkStateAtStartup, base::FEATURE_ENABLED_BY_DEFAULT)
 // Use the LibunwindstackNativeUnwinderAndroid for only browser main thread, and
 // only on Android.
 BASE_FEATURE(kUseLibunwindstackNativeUnwinderAndroid, base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kUsePLinkInHelp, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kUsePLinkInHelp, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kUseWebUiNtpAndroid, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kUserFeedbackAllowedPolicy, base::FEATURE_ENABLED_BY_DEFAULT);
 // Verify sign-in state on startup.
