02f5740d426ca03babca877ce27c8915acd639ed
Alex Rudenko <alexrudenko@chromium.org>
2026-07-06 01:10:52 -0700

    Revert "[tracing] Deduplicate Blink.Responsiveness.UserInteraction trace events"
    This reverts commit 9d9d846411d03f3c409cdd6fba1c6564f21200b7.
    
    Reason for revert: https://crbug.com/530174267
    
    Failure Link: https://ci.chromium.org/ui/p/devtools-frontend/builders/try/dtf_linux_rel/27792/test-results?view=legacy&q=ID%3Ae2e%2Flighthouse%2Ftimespan.test.ts%3A+Timespan%2Fsuccessfully+returns+a+Lighthouse+report+for+user+interactions+VHash%3Ae3b0c44298fc1c14
    
    Original change's description:
    > [tracing] Deduplicate Blink.Responsiveness.UserInteraction trace events
    >
    > To match how the UMA is emitted.
    > Drive-by: remove redundant/unused "Responsiveness.Renderer.UserInteraction"
    >
    > Change-Id: I04c20e45e68481f29e6ea97f7bd78b154904c70a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7997937
    > Reviewed-by: Michal Mocny <mmocny@chromium.org>
    > Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1652771}
    
    Bug: 530174267
    Change-Id: Ib672618321eaeda0e22cb8b5561349ecb2566a80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8037805
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Owners-Override: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657018}

--- third_party/blink/renderer/core/timing/responsiveness_metrics.cc
+++ third_party/blink/renderer/core/timing/responsiveness_metrics.cc
@@ -53,6 +53,16 @@ const char kHistogramTapOrClick[] = ".TapOrClick";
 
 constexpr char kUserInteractionTraceEventCategory[] = "latency";
 
+std::unique_ptr<TracedValue> UserInteractionTraceData(base::TimeDelta duration,
+                                                      bool is_pointer) {
+  auto traced_value = std::make_unique<TracedValue>();
+  traced_value->SetInteger("maxDuration",
+                           static_cast<int>(duration.InMilliseconds()));
+  traced_value->SetString("interactionType",
+                          is_pointer ? "tapOrClick" : "keyboard");
+  return traced_value;
+}
+
 void LogResponsivenessHistogram(base::TimeDelta duration, const char* suffix) {
   base::UmaHistogramCustomTimes(
       base::StrCat({kHistogramMaxEventDuration, suffix}), duration,
@@ -539,14 +549,15 @@ void ResponsivenessMetrics::ReportToMetrics(PerformanceEventTiming* entry) {
   }
 
   UserInteractionType interaction_type = entry->InteractionType();
+  uint64_t event_id = base::trace_event::GetNextGlobalTraceId();
   RecordUserInteractionUKM(window, interaction_type, *entry);
+  RecordUserInteractionTracing(window, interaction_type, *entry, event_id);
 
-  // For Histogram and Tracing convenience, we only report "unique" interaction
-  // durations. I.e. when keydown and keypress, or pointerup and click, report
-  // in the same animation frame, we don't duplicate reports. The first event
-  // for each interaction id should always be the longest. If they have the
-  // same end time, they perfectly overlap in time and don't need to be
-  // repeated.
+  // For Histogram convenience, we only report "unique" interaction durations.
+  // I.e. when keydown and keypress, or pointerup and click, report in the
+  // same animation frame, we don't duplicate reports.  The first event for each
+  // interaction id should always be the longest.  If they have the same end
+  // time, they perfectly overlap in time and don't need to be repeated.
   uint64_t frame_index = entry->GetEventTimingReportingInfo()->frame_index;
   if (!last_recorded_frame_index_.has_value() ||
       frame_index != *last_recorded_frame_index_) {
@@ -559,8 +570,6 @@ void ResponsivenessMetrics::ReportToMetrics(PerformanceEventTiming* entry) {
 
   if (!reported_interactions_in_frame_.Contains(key)) {
     reported_interactions_in_frame_.emplace_back(key);
-    uint64_t event_id = base::trace_event::GetNextGlobalTraceId();
-    RecordUserInteractionTracing(window, interaction_type, *entry, event_id);
     RecordUserInteractionHistograms(interaction_type, *entry, event_id);
   }
 }
@@ -621,7 +630,14 @@ void ResponsivenessMetrics::RecordUserInteractionTracing(
     UserInteractionType interaction_type,
     const PerformanceEventTiming& entry,
     uint64_t event_id) {
+  base::TimeDelta duration = entry.GetExactDuration();
+
   bool is_pointer_event = interaction_type == UserInteractionType::kTapOrClick;
+
+  TRACE_EVENT("devtools.timeline", "Responsiveness.Renderer.UserInteraction",
+              "data", UserInteractionTraceData(duration, is_pointer_event),
+              "frame", GetFrameIdForTracing(window->GetFrame()));
+
   const auto track = perfetto::NamedTrack::Global("Metrics: INP", event_id);
   TRACE_EVENT_BEGIN(
       kUserInteractionTraceEventCategory, kHistogramMaxEventDuration, track,
