59aaca4be552f78a2c011a48ec37b553e9be0749
kenoss <kenoss@chromium.org>
2026-07-06 01:46:46 -0700

    Prefetch: Remove PrefetchReferringPageMetrics/PrefetchServingPageMetrics (2/3)
    This CL removes `PrefetchPageLoadMetricsObserver` and UKM definitions.
    
    These UKMs can be safely removed. See
    https://groups.google.com/a/google.com/g/chrome-prerendering/c/gEZ3I-nfITA/m/Wxe59PedBQAJ.
    
    Bug: 360094997
    Change-Id: I4ea46ff5fba8231f14401047ddedd97d835c4e10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8000831
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Max Curran <curranmax@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657035}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -35,7 +35,6 @@
 #include "chrome/browser/page_load_metrics/observers/omnibox_suggestion_used_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/optimization_guide_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/page_anchors_metrics_observer.h"
-#include "chrome/browser/page_load_metrics/observers/prefetch_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/protocol_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/scheme_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/security_state_page_load_metrics_observer.h"
@@ -218,7 +217,6 @@ void PageLoadMetricsEmbedder::RegisterObservers(
     tracker->AddObserver(std::make_unique<ForegroundDurationUKMObserver>());
     tracker->AddObserver(
         std::make_unique<DocumentWritePageLoadMetricsObserver>());
-    tracker->AddObserver(std::make_unique<PrefetchPageLoadMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<MultiTabLoadingPageLoadMetricsObserver>());
     tracker->AddObserver(
