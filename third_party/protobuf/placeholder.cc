e9602b404ebb88b172bcececfb251a4f5045220d
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-07-06 09:39:50 -0700

    lpm: Add force_component to proto_library
    The `proto_library` template generates helper targets which other
    `proto_library` targets depend on. Some templates generate `component`
    wrappers around some protos, e.g. fuzzable_proto_library, so they had to
    manually generate the helpers targets that other proto_libraries
    expected, essentially depending on implementation details of
    `proto_library`.
    
    This CL introduces `force_component` that these targets can use instead
    of depending on implementation details of `proto_library`.
    
    Bug: 505034799
    Change-Id: Ia5e4fa981e0c389b3f2660d5516666b04762e83d
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel,linux-libfuzzer-high-end-asan-rel,chromeos-libfuzzer-asan-rel,mac-libfuzzer-asan-rel,win-libfuzzer-asan-rel,linux-centipede-asan-rel,win_chromium_compile_dbg_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007502
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657253}

--- third_party/protobuf/placeholder.cc
+++
@@ -1,5 +0,0 @@
-// Copyright 2026 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Placeholder file used to ensure that wrapper libraries get built on all platforms.
