d9bf5e2632fdcca99171020ad477cd361e0f8954
Bofeng Chen <bofengchen@google.com>
2026-07-07 10:50:40 -0700

    [151] [composebox] Deduplicate composebox file_carousel unit test suite
    Original change's description:
    > [composebox] Deduplicate composebox file_carousel unit test suite
    >
    > Deduplicates standalone unit test coverage for
    > <cr-composebox-file-carousel>(ComposeboxFileCarouselElement) by removing
    > redundant New Tab Page suites.
    >
    > file_carousel_test.ts currently exists under both
    > new_tab_page/composebox/ and shared cr_components/composebox/. The
    > 41-line NTP version tests only basic thumbnail rendering (`display
    > files`), which is 100% duplicated by `renders files` in the 279-line
    > shared cr_components suite (along with horizontal scrolling, overflow
    > indicators, and file deletion verification).
    >
    > Removes NTP file registrations from composebox.gni and
    > new_tab_page_browsertest.cc and deletes the NTP test file.
    >
    > Bug: b:528365874
    > Bypass-Check-License: File moved from /new_tab_page
    > Change-Id: I61a07590bd3396ee24b2e0609889d61c470ec156
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8006356
    > Reviewed-by: Sho Mizoe <shomizoe@google.com>
    > Reviewed-by: Devi Varaprasad Jonnala <jonnalad@google.com>
    > Commit-Queue: Bofeng Chen <bofengchen@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1654568}
    
    Bug: b:528365874,532099650
    Bypass-Check-License: File moved from /new_tab_page
    Change-Id: I61a07590bd3396ee24b2e0609889d61c470ec156
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8055245
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7922@{#510}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.cc
+++ chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.cc
@@ -301,9 +301,6 @@ IN_PROC_BROWSER_TEST_F(NewTabPageTest, ComposeboxDragAndDrop) {
           "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(NewTabPageTest, ComposeboxFileCarousel) {
-  RunTest("new_tab_page/composebox/file_carousel_test.js", "mocha.run()");
-}
 
 IN_PROC_BROWSER_TEST_F(NewTabPageTest, ComposeboxFileThumbnail) {
   RunTest("new_tab_page/composebox/file_thumbnail_test.js", "mocha.run()");
