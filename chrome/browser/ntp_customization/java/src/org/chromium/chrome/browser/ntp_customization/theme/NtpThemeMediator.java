5d1312c4ee616facada1de2f7f35b69d191b1e98
Xi Han <hanxi@google.com>
2026-07-07 08:58:35 -0700

    [M150] [Theme] Fix crash at calling ActivityResultLauncher#launch().
    Original change's description:
    > [Theme] Fix crash at calling ActivityResultLauncher#launch().
    >
    > The crash is caused by ActivityResultLauncher isn't registered when
    > calling #launch(). A hypothesis is due to race condition that
    > #destroy() unregistered the launcher's callback before calling the
    > #launch(). In this CL, we move the creation of the launcher to the
    > click handler, and it is called right before #launch() is called.
    >
    > Bug: 527549727
    > Change-Id: I238e938dc7e6e9fcea0d02bcf6d4677b34851d57
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8008366
    > Commit-Queue: Xi Han <hanxi@chromium.org>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1653448}
    
    (cherry picked from commit 643df6ad347361a4d979018f0b7139f3fa863655)
    
    Bug: 529456765,527549727
    Change-Id: I238e938dc7e6e9fcea0d02bcf6d4677b34851d57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8046360
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#2952}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme/NtpThemeMediator.java
+++ chrome/browser/ntp_customization/java/src/org/chromium/chrome/browser/ntp_customization/theme/NtpThemeMediator.java
@@ -123,14 +123,6 @@ public class NtpThemeMediator {
     /** Sets the on click listener for each theme bottom sheet section. */
     @VisibleForTesting
     void setOnClickListenerForAllSection() {
-        if (mActivityResultRegistry != null) {
-            mActivityResultLauncher =
-                    mActivityResultRegistry.register(
-                            UPLOAD_IMAGE_KEY,
-                            new ActivityResultContracts.GetContent(),
-                            this::onUploadImageResult);
-        }
-
         mThemePropertyModel.set(
                 SECTION_ON_CLICK_LISTENER,
                 new Pair<>(DEFAULT, this::handleChromeDefaultSectionClick));
@@ -214,6 +206,14 @@ public class NtpThemeMediator {
 
     @VisibleForTesting
     void handleUploadAnImageSectionClick(View view) {
+        if (mActivityResultRegistry == null) return;
+
+        mActivityResultLauncher =
+                mActivityResultRegistry.register(
+                        UPLOAD_IMAGE_KEY,
+                        new ActivityResultContracts.GetContent(),
+                        this::onUploadImageResult);
+
         if (mActivityResultLauncher != null) {
             mActivityResultLauncher.launch("image/*");
         }
