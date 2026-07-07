c7f5736057a104635b2ea8b2126adb7ebf016eb5
Aldo Culquicondor <acondor@google.com>
2026-07-07 08:15:55 -0700

    Reset scoped write access before sending copy result
    End write access on the destination SharedImage prior to sending the
    copy output result.
    
    Previously, write access remained active until function exit. Calling
    `request->SendResult()` before ending write access created a race
    condition if the client immediately attempted readback.
    
    Bug: 529326479, 486949806
    
    TAG=agy
    CONV=fa56a761-9db7-4a80-8f61-2dec34e71b38
    
    Change-Id: I5617f016ae52725be7b72edcecdf32c94dd87f84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8041080
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Aldo Culquicondor <acondor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657965}

--- components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
+++ components/viz/service/display_embedder/skia_output_surface_impl_on_gpu.cc
@@ -1129,6 +1129,15 @@ void SkiaOutputSurfaceImplOnGpu::CopyOutputRGBAInTexture(
     return;
   }
 
+  // End write access before sending the result to avoid racing with the
+  // client's subsequent readback.
+  scoped_write.reset();
+
+  // We conditionally move from request (if `should_wait_for_gpu_work` is true),
+  // DCHECK that we don't accidentally enter this codepath after the request was
+  // moved from.
+  DCHECK(request);
+
   if (request->has_blit_request()) {
     request->SendResult(std::make_unique<CopyOutputSharedImageResult>(
         CopyOutputResult::Format::RGBA, geometry.result_selection,
@@ -1538,6 +1547,10 @@ void SkiaOutputSurfaceImplOnGpu::CopyOutputNV12(
     // called.
     return;
   }
+
+  // End write access before sending the result to avoid racing with the
+  // client's subsequent readback.
+  mailbox_access_data.scoped_write.reset();
 
   // We conditionally move from request (if `should_wait_for_gpu_work` is true),
   // DCHECK that we don't accidentally enter this codepath after the request was
