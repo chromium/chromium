6200b4ce22c5e03e28bbdec3b9b8434eb272af75
Morten Stenshorne <mstensho@chromium.org>
2026-07-07 13:31:45 -0700

    Remove useless TODO.
    Change-Id: I6a36c60d45644fbd8b2643c58d0c0ec8c3d22af8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032725
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658250}

--- third_party/blink/renderer/core/paint/box_fragment_painter.cc
+++ third_party/blink/renderer/core/paint/box_fragment_painter.cc
@@ -1370,7 +1370,6 @@ void BoxFragmentPainter::PaintBoxDecorationBackground(
     const PaintInfo& paint_info,
     const PhysicalOffset& paint_offset,
     bool suppress_box_decoration_background) {
-  // TODO(mstensho): Break dependency on LayoutObject functionality.
   const LayoutObject& layout_object = *box_fragment_.GetLayoutObject();
 
   if (IsA<LayoutView>(layout_object) ||
