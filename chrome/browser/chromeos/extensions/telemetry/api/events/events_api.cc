73c6f2275b25e092d3772554f6330c7dd0709b92
Hidehiko Abe <hidehiko@chromium.org>
2026-07-07 00:33:52 -0700

    Move feature check from event_manager to events_api.
    Along with crosapi removal, event_manager will directly accept
    cros_healthd IPC args. There's some gap between JS API enums and
    cros_healthd enums, so this CL moves the preliminary check about
    the availability of JS api into events_api.
    
    BUG=510951937
    TEST=Tryjob
    
    Change-Id: Id11fd3b6cf1e4a2cb5c5881364a9c123d084ad18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8051730
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1657734}

--- chrome/browser/chromeos/extensions/telemetry/api/events/events_api.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/events/events_api.cc
@@ -16,6 +16,7 @@
 #include "chrome/common/chromeos/extensions/api/events.h"
 #include "chromeos/crosapi/mojom/telemetry_extension_exception.mojom.h"
 #include "content/public/browser/browser_context.h"
+#include "extensions/common/features/feature_provider.h"
 #include "ui/base/page_transition_types.h"
 #include "url/gurl.h"
 
@@ -34,6 +35,44 @@ void OpenDiagnosticsKeyboardPage(content::BrowserContext* browser_context) {
                                  GURL(kKeyboardDiagnosticsUrl),
                                  ui::PAGE_TRANSITION_FIRST);
   Navigate(&navigate_params);
+}
+
+std::string GetFeatureName(cx_events::EventCategory category) {
+  switch (category) {
+    case cx_events::EventCategory::kNone:
+      return "";
+    case cx_events::EventCategory::kAudioJack:
+      return "os.events.onAudioJackEvent";
+    case cx_events::EventCategory::kLid:
+      return "os.events.onLidEvent";
+    case cx_events::EventCategory::kUsb:
+      return "os.events.onUsbEvent";
+    case cx_events::EventCategory::kExternalDisplay:
+      return "os.events.onExternalDisplayEvent";
+    case cx_events::EventCategory::kSdCard:
+      return "os.events.onSdCardEvent";
+    case cx_events::EventCategory::kPower:
+      return "os.events.onPowerEvent";
+    case cx_events::EventCategory::kKeyboardDiagnostic:
+      return "os.events.onKeyboardDiagnosticEvent";
+    case cx_events::EventCategory::kStylusGarage:
+      return "os.events.onStylusGarageEvent";
+    case cx_events::EventCategory::kTouchpadButton:
+      return "os.events.onTouchpadButtonEvent";
+    case cx_events::EventCategory::kTouchpadTouch:
+      return "os.events.onTouchpadTouchEvent";
+    case cx_events::EventCategory::kTouchpadConnected:
+      return "os.events.onTouchpadConnectedEvent";
+    case cx_events::EventCategory::kTouchscreenTouch:
+      return "os.events.onTouchscreenTouchEvent";
+    case cx_events::EventCategory::kTouchscreenConnected:
+      return "os.events.onTouchscreenConnectedEvent";
+    case cx_events::EventCategory::kStylusTouch:
+      return "os.events.onStylusTouchEvent";
+    case cx_events::EventCategory::kStylusConnected:
+      return "os.events.onStylusConnectedEvent";
+  }
+  NOTREACHED();
 }
 
 }  // namespace
@@ -60,6 +99,35 @@ std::optional<Params> EventsApiFunctionBase::GetParams() {
 void OsEventsIsEventSupportedFunction::RunIfAllowed() {
   const auto params = GetParams<cx_events::IsEventSupported::Params>();
   if (!params) {
+    return;
+  }
+
+  std::string feature_name = GetFeatureName(params->category);
+  const auto* feature =
+      extensions::FeatureProvider::GetAPIFeatures()->GetFeature(feature_name);
+  // Healthd team uses feature flag "TelemetryExtensionPendingApprovalApi" for
+  // pending APIs. "os.events" API has been released, and we keep adding API
+  // under it like "os.events.abc" and use the feature flag for pending
+  // approval. Once it's approved, we remove it from the "_api_features.json"
+  // file.
+  //
+  // Hence, for the API under "os.events", as long as we can find it in the
+  // "_api_features.json" file, it means it's behind a feature flag and then we
+  // should report it as "unsupported".
+  //
+  // Note 1: This check is based on the above assumption. That is, if we need to
+  // add the feature into _api_features.json due to other reasons, this check
+  // will report incorrect answer.
+  //
+  // TODO(b/296816372): Retrieve the feature flag name to see if it's really
+  // behind a flag.
+  //
+  // Note 2: Indeed this will not work if we control feature access using ways
+  // other than adding feature flag (such as through blocklist).
+  if (feature) {
+    cx_events::EventSupportStatusInfo result;
+    result.status = cx_events::EventSupportStatus::kUnsupported;
+    Respond(ArgumentList(cx_events::IsEventSupported::Results::Create(result)));
     return;
   }
 
