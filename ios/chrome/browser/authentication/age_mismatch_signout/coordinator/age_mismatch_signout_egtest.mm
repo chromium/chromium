61e3d79b3c2f0f361375400929f6922648aa9134
Duong Dac <ddac@chromium.org>
2026-07-08 08:38:24 -0700

    Reland "[iOS] Add AgeMismatchSignout egtests for account switching"
    This reverts commit c183b60d9ed499d05568d75448ecfadfeb4559b2.
    
    Reason for revert: removed the failing test for multi-profiles.
    Similar tests are disabled. b/507486331
    
    Original change's description:
    > Revert "[iOS] Add AgeMismatchSignout egtests for account switching"
    >
    > This reverts commit 5134f7bf455207d56d4f3a9919973de547bb5e16.
    >
    > Reason for revert: Fails when fieldtrial is not enabled.
    >
    > Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-fieldtrial-rel/63388/overview
    >
    > Original change's description:
    > > [iOS] Add AgeMismatchSignout egtests for account switching
    > >
    > > Bug: 481654408
    > > Change-Id: I50340ec51fdee278b7915f2bf9ef9bf75a37ca3d
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8007129
    > > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > > Commit-Queue: Duong Dac <ddac@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1654704}
    >
    > Bug: 481654408, 530151850
    > Change-Id: I50254cc068eedf8975b3af0db5b4036aae5c28b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8035865
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1656461}
    
    Bug: 481654408, 530151850
    Change-Id: Id5530eb123962e0e66d77614e1d4e09e1c219a23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8052068
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1658791}

--- ios/chrome/browser/authentication/age_mismatch_signout/coordinator/age_mismatch_signout_egtest.mm
+++ ios/chrome/browser/authentication/age_mismatch_signout/coordinator/age_mismatch_signout_egtest.mm
@@ -4,6 +4,7 @@
 
 #import "components/signin/internal/identity_manager/account_capabilities_constants.h"
 #import "components/signin/public/base/signin_switches.h"
+#import "ios/chrome/browser/authentication/account_menu/public/account_menu_constants.h"
 #import "ios/chrome/browser/authentication/age_mismatch_signout/ui/age_mismatch_signout_ui_constants.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey.h"
 #import "ios/chrome/browser/authentication/test/signin_earl_grey_ui_test_util.h"
@@ -18,6 +19,7 @@
 #import "ios/chrome/browser/signin/model/test_constants.h"
 #import "ios/chrome/common/ui/button_stack/button_stack_constants.h"
 #import "ios/chrome/common/ui/promo_style/constants.h"
+#import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
@@ -246,6 +248,66 @@ id<GREYMatcher> AgeMismatchSecondaryButton() {
   // Verify the prompt is dismissed.
   [[EarlGrey selectElementWithMatcher:AgeMismatchPrimaryButton()]
       assertWithMatcher:grey_notVisible()];
+}
+
+// Tests that switching from an account that can sign in to one that cannot
+// triggers the age mismatch signout prompt.
+- (void)testSwitchToAccountCannotSignInTriggersAgeMismatch {
+  // Add two fake identities.
+  FakeSystemIdentity* fakeIdentity1 = [FakeSystemIdentity fakeIdentity1];
+  FakeSystemIdentity* fakeIdentity2 = [FakeSystemIdentity fakeIdentity2];
+
+  // Add the identities with their capabilities.
+  [SigninEarlGrey addFakeIdentity:fakeIdentity1
+                 withCapabilities:@{
+                   @(kCanSignInToChromeCapabilityName) : @YES,
+                 }];
+  [SigninEarlGrey addFakeIdentity:fakeIdentity2
+                 withCapabilities:@{
+                   @(kCanSignInToChromeCapabilityName) : @NO,
+                 }];
+
+  // Sign in with the first identity (can sign in).
+  [SigninEarlGrey signinWithFakeIdentity:fakeIdentity1];
+  [SigninEarlGrey verifySignedInWithFakeIdentity:fakeIdentity1];
+
+  // Open Account Menu from NTP.
+  [ChromeEarlGrey openNewTab];
+  [ChromeEarlGrey waitForSufficientlyVisibleElementWithMatcher:
+                      grey_accessibilityID(kNTPFeedHeaderIdentityDisc)];
+  [[EarlGrey
+      selectElementWithMatcher:grey_accessibilityID(kNTPFeedHeaderIdentityDisc)]
+      performAction:grey_tap()];
+
+  // Wait for the account menu to appear and the secondary account button to be
+  // visible.
+  [ChromeEarlGrey
+      waitForSufficientlyVisibleElementWithMatcher:
+          grey_accessibilityID(kAccountMenuSecondaryAccountButtonId)];
+
+  // Tap on the secondary account in the menu to switch.
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityID(
+                                          kAccountMenuSecondaryAccountButtonId)]
+      performAction:grey_tap()];
+
+  // Verify Age Mismatch prompt is displayed.
+  [ChromeEarlGrey
+      waitForSufficientlyVisibleElementWithMatcher:AgeMismatchPrimaryButton()];
+
+  // Verify that the "Use without an account" button is NOT displayed.
+  [[EarlGrey selectElementWithMatcher:AgeMismatchSecondaryButton()]
+      assertWithMatcher:grey_notVisible()];
+
+  // Tap "Use another account" (Primary button).
+  [[EarlGrey selectElementWithMatcher:AgeMismatchPrimaryButton()]
+      performAction:grey_tap()];
+
+  // Verify the prompt is dismissed.
+  [[EarlGrey selectElementWithMatcher:AgeMismatchPrimaryButton()]
+      assertWithMatcher:grey_notVisible()];
+
+  // Verify the original user is still signed in (switch failed).
+  [SigninEarlGrey verifySignedInWithFakeIdentity:fakeIdentity1];
 }
 
 @end
