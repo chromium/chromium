818e2c4b174bbeb5e9489e85b471a62f4a315f58
Daniel Cheng <dcheng@chromium.org>
2026-07-07 23:30:42 -0700

    Enable the Rust Pix validator by default
    UMA metrics show that the C++ and Rust validator have identical behavior
    except in cases where the C++ validator failed, e.g. due to the utility
    process crashing.
    
    Bug: 454668204
    Change-Id: I7585ccb7a47b5f6252d7c8d815200d34e7db8392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032363
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1658522}

--- components/facilitated_payments/core/features/features.cc
+++ components/facilitated_payments/core/features/features.cc
@@ -15,7 +15,7 @@ BASE_FEATURE(kEnablePixPaymentsInLandscapeMode,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Use the Rust implementation of the Pix code validator.
-BASE_FEATURE(kUseRustPixCodeValidator, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kUseRustPixCodeValidator, base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
 // When enabled, the check for matching the main frame domain with the
