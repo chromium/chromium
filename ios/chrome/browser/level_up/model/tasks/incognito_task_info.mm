3280876db0f6c75f9d0de668e8783317c7166ab2
Robbie Gibson <rkgibson@google.com>
2026-07-09 12:42:34 -0700

    [iOS] Implement navigation action for Incognito Level Up task
    Wires up the Incognito task in Level Up to open the Tab Grid in
    regular mode (TabGridOpeningMode::kRegular). Also adds a TODO to
    trigger the swipe-right-to-incognito IPH once wired up.
    
    Bug: b:523325903
    Change-Id: Ie4642cbc7ff133932858089b4138a62c86ed9c8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8067137
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1659779}

--- ios/chrome/browser/level_up/model/tasks/incognito_task_info.mm
+++ ios/chrome/browser/level_up/model/tasks/incognito_task_info.mm
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "base/functional/bind.h"
 #import "base/functional/callback_helpers.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/level_up/model/task_info.h"
 #import "ios/chrome/browser/level_up/model/tasks/task_factories.h"
+#import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
+#import "ios/chrome/browser/shared/public/commands/scene_commands.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -35,7 +38,12 @@ class IncognitoTaskInfo : public TaskInfo {
     return l10n_util::GetStringUTF8(IDS_IOS_LEVEL_UP_TASK_COMPLETED_INCOGNITO);
   }
   TaskInfo::NavigationAction GetNavigationAction() const override {
-    return base::DoNothing();
+    return base::BindRepeating(^(CommandDispatcher* dispatcher) {
+      id<SceneCommands> handler = HandlerForProtocol(dispatcher, SceneCommands);
+      // TODO(crbug.com/532639936): Also trigger the IPH to swipe right to open
+      // incognito (kIPHiOSTabGridSwipeRightForIncognito) once wired up.
+      [handler displayTabGridInMode:TabGridOpeningMode::kRegular];
+    });
   }
 };
 
