a5eda440f222193c437c8c0fe5913bc50b328408
Jiacheng Guo <gjc@google.com>
2026-07-08 22:58:56 -0700

    Launch AsyncPredictorDbUpdate feature
    Stable experiment showed that the feature improves the latency from user
    interaction to navigation by 2.1ms on Android and 0.4ms on Desktop.
    
    Detailed experiment reports:
    https://docs.google.com/document/d/1uvua12nXQWhS_tG-LRHHknjz-11s4mWzsgQdwcmpwPI/edit?usp=sharing
    
    Bug: 501273981
    Change-Id: Ib95aab2ec90a2ab6c460f2a16be38d94b48b1546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8061516
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1659331}

--- chrome/browser/predictors/predictors_features.cc
+++ chrome/browser/predictors/predictors_features.cc
@@ -18,7 +18,7 @@ constexpr base::FeatureState kFeatureEnabledOnlyOnAndroid =
 }  // namespace
 
 // Whether to update the predictor database asynchronously.
-BASE_FEATURE(kAsyncPredictorDbUpdate, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAsyncPredictorDbUpdate, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Whether local predictions should be used to make preconnect predictions.
 BASE_FEATURE(kLoadingPredictorUseLocalPredictions,
