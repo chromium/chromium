f1699fd88171a1f99ff41b89861b310ef42e594a
Eriko Kurimoto <elkurin@chromium.org>
2026-07-10 09:57:55 -0700

    WaaP: Re-enable NewTabPage UI tests on MSAN with Initial WebUI
    The GTEST_SKIP for NewTabPageModulesInteractiveTabGroupsUiTest
    ResumeTabGroupInAnotherWindow on MSAN with InitialWebUI enabled is
    removed as the test is fixed by sync window show feature.
    
    Bug: 477426026
    Cq-Include-Trybots: luci.chromium.try:linux_chromium_msan_rel_ng
    Change-Id: I934bf304eee017eb5de4bc10ae5033f196f944a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8073560
    Reviewed-by: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660344}

--- chrome/browser/new_tab_page/modules/new_tab_page_modules_interactive_uitest.cc
+++ chrome/browser/new_tab_page/modules/new_tab_page_modules_interactive_uitest.cc
@@ -558,13 +558,6 @@ IN_PROC_BROWSER_TEST_F(NewTabPageModulesInteractiveTabGroupsUiTest,
 #endif  // BUILDFLAG(IS_MAC)
 IN_PROC_BROWSER_TEST_F(NewTabPageModulesInteractiveTabGroupsUiTest,
                        MAYBE_ResumeTabGroupInAnotherWindow) {
-#if defined(MEMORY_SANITIZER)
-  if (base::FeatureList::IsEnabled(features::kInitialWebUI)) {
-    GTEST_SKIP() << "Skipping test on MSAN with InitialWebUI enabled. "
-                    "See crbug.com/477426026.";
-  }
-#endif
-
   ASSERT_TRUE(
       AddTabAtIndex(0, GURL(url::kAboutBlankURL), ui::PAGE_TRANSITION_TYPED));
   const tab_groups::TabGroupId group_id =
