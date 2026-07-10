00f53d49a1b779ca8322cc31d79c995faa1688bb
Claire Chambers <clchambers@microsoft.com>
2026-07-09 19:25:39 -0700

    Code Cleanup: Remove unused RequiresPropertyNodeForAnimation
    This function was superceded by CompositingReasonFinder, which sets
    direct_compositing_reasons when an animation is present. As a result,
    we no longer directly poll style flags for this purpose.
    
    Change-Id: I541d4b5bdc918b67d37e2799e2898e124d61d3b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8051685
    Commit-Queue: Claire Chambers <clchambers@microsoft.com>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660017}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2081,12 +2081,6 @@ class ComputedStyle final : public ComputedStyleBase {
            IsRunningRotateAnimationOnCompositor() ||
            IsRunningTranslateAnimationOnCompositor();
   }
-  bool RequiresPropertyNodeForAnimation() const {
-    return IsRunningOpacityAnimationOnCompositor() ||
-           IsRunningTransformRelatedAnimationOnCompositor() ||
-           IsRunningFilterAnimationOnCompositor() ||
-           IsRunningBackdropFilterAnimationOnCompositor();
-  }
 
   // Opacity utility functions.
   bool HasOpacity() const { return Opacity() < 1.0f; }
