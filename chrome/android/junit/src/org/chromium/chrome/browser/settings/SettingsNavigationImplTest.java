c1d7b70a654151aa0ec1b5f7e85a662ade552c6a
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2026-07-10 13:24:14 -0700

    Revert "settings: Fix SettingsNavigation for SettingsInTab on Android, part 1"
    This reverts commit d0a5f2032dd62590373633711bdf5ec2175ad33c.
    
    Reason for revert: 
    The Suspect CL https://chromium-review.googlesource.com/q/I2924a69c7e250e72e9941c6715ed4657e5c56677 is highly likely the cause of the crash. Please see the crash report http://crash/5f8c4915e615abcf.
    Rebase the Revert CL on top of main branch with conflicts allowed option to prevent false positive merge conflicts.
    
    If you agree with the revert CL, please approve it and mark as Commit-Queue +2 to submit it.
    
    More details can be found in the go/crash-revert-cls-creation.
    
    Original change's description:
    > settings: Fix SettingsNavigation for SettingsInTab on Android, part 1
    > 
    > Many settings classes use SettingsNavigation to trigger transitions to
    > different sub-sections of settings (e.g. Autofill or Third Party
    > Cookies). This causes them to launch a new SettingsActivity, which
    > takes over the main window and obscures settings in a tab.
    > 
    > Update SettingsHostFragment and SettingsNavigation to support in-place
    > fragment transitions with SettingsInTab. This requires a small
    > refactor of MultiColumnSettings to extract a helper method.
    > 
    > Programmatically opening the settings tab when it is closed will be
    > addressed in a future CL. See TODOs in the code.
    > 
    > I have manually tested the affected settings (see bug), both with
    > SettingsInTab enabled and disabled.
    > 
    > Bug: 532161175, 521895796
    > Change-Id: I2924a69c7e250e72e9941c6715ed4657e5c56677
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8059945
    > Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    > Commit-Queue: James Cook <jamescook@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1658995}
    > 
    
    Bug: 533485674
    Change-Id: I5347cf64029452cec8099f3f15dd181c65c54cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8072460
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Chrome Crash (Prod) <crash@system.gserviceaccount.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1660496}

--- chrome/android/junit/src/org/chromium/chrome/browser/settings/SettingsNavigationImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/settings/SettingsNavigationImplTest.java
@@ -5,49 +5,25 @@
 package org.chromium.chrome.browser.settings;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-
-import androidx.fragment.app.Fragment;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.autofill.settings.FinancialAccountsManagementFragment;
 import org.chromium.chrome.browser.autofill.settings.NonCardPaymentMethodsManagementFragment;
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.components.browser_ui.settings.SettingsNavigation;
-import org.chromium.ui.base.TestActivity;
 
 /** Tests for SettingsNavigationImpl. */
 @RunWith(BaseRobolectricTestRunner.class)
 public class SettingsNavigationImplTest {
     private final Context mContext;
     private final SettingsNavigationImpl mSettingsNavigationImpl;
-
-    /** Fake settings fragment for testing. */
-    public static class FakeSettingsFragment extends Fragment {
-        public FakeSettingsFragment() {}
-    }
-
-    /** Another fake settings fragment for testing transitions. */
-    public static class SecondFakeSettingsFragment extends Fragment {
-        public SecondFakeSettingsFragment() {}
-    }
-
-    /** Subclass SettingsHostFragment to mock initial fragment instantiation. */
-    public static class TestSettingsHostFragment extends SettingsHostFragment {
-        @Override
-        protected Fragment createInitialFragment() {
-            return new FakeSettingsFragment();
-        }
-    }
 
     public SettingsNavigationImplTest() {
         mContext = Robolectric.buildActivity(Activity.class).get();
@@ -76,52 +52,5 @@ public class SettingsNavigationImplTest {
         assertEquals(
                 intent.getStringExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT),
                 NonCardPaymentMethodsManagementFragment.class.getName());
-    }
-
-    @Test
-    @EnableFeatures({ChromeFeatureList.SETTINGS_IN_TAB})
-    public void testStartSettings_SettingsInTab_ShowsInHostFragment() {
-        var scenario = Robolectric.buildActivity(TestActivity.class).setup();
-        TestActivity activity = scenario.get();
-        TestSettingsHostFragment hostFragment = new TestSettingsHostFragment();
-        activity.getSupportFragmentManager()
-                .beginTransaction()
-                .add(
-                        android.R.id.content,
-                        hostFragment,
-                        SettingsHostFragment.SETTINGS_NATIVE_PAGE_TAG)
-                .commitNow();
-
-        mSettingsNavigationImpl.startSettings(activity, SecondFakeSettingsFragment.class, null);
-        hostFragment.getChildFragmentManager().executePendingTransactions();
-
-        assertTrue(hostFragment.getActiveFragment() instanceof SecondFakeSettingsFragment);
-    }
-
-    @Test
-    @EnableFeatures({ChromeFeatureList.SETTINGS_IN_TAB})
-    public void testStartSettings_SettingsInTab_NullFragment_ShowsInitialFragment() {
-        var scenario = Robolectric.buildActivity(TestActivity.class).setup();
-        TestActivity activity = scenario.get();
-        TestSettingsHostFragment hostFragment = new TestSettingsHostFragment();
-        activity.getSupportFragmentManager()
-                .beginTransaction()
-                .add(
-                        android.R.id.content,
-                        hostFragment,
-                        SettingsHostFragment.SETTINGS_NATIVE_PAGE_TAG)
-                .commitNow();
-
-        // First show some other fragment.
-        mSettingsNavigationImpl.startSettings(activity, SecondFakeSettingsFragment.class, null);
-        hostFragment.getChildFragmentManager().executePendingTransactions();
-        assertTrue(hostFragment.getActiveFragment() instanceof SecondFakeSettingsFragment);
-
-        // Now startSettings with null fragment class.
-        mSettingsNavigationImpl.startSettings(activity, null, null);
-        hostFragment.getChildFragmentManager().executePendingTransactions();
-
-        // The initial fragment shown when Settings is started for the first time is shown.
-        assertTrue(hostFragment.getActiveFragment() instanceof FakeSettingsFragment);
     }
 }
