77f3fc5a2580085f41c275692547ea25b6dcb92b
Jenna Himawan <jhimawan@google.com>
2026-07-10 12:16:41 -0700

    CrossDeviceThemeTracker: Add Android theme translation methods
    Add TranslateDesktop() and TranslateIos() methods for Android to
    components/themes/cross_device/theme_translation.h and .cc under
    IS_ANDROID in preparation for Android support.
    
    TAG=agy
    CONV=bfdbedd4-63c8-4089-90ed-438f127ba505
    
    Bug: 517615321
    Change-Id: Ic4b2c91842274eacadf202171e49eb9b6a44fca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8072578
    Commit-Queue: Jenna Himawan <jhimawan@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1660445}

--- components/themes/cross_device/theme_translation.h
+++ components/themes/cross_device/theme_translation.h
@@ -5,6 +5,8 @@
 #ifndef COMPONENTS_THEMES_CROSS_DEVICE_THEME_TRANSLATION_H_
 #define COMPONENTS_THEMES_CROSS_DEVICE_THEME_TRANSLATION_H_
 
+#include "build/build_config.h"
+#include "build/buildflag.h"
 #include "components/sync/protocol/theme_android_specifics.pb.h"
 #include "components/sync/protocol/theme_ios_specifics.pb.h"
 #include "components/sync/protocol/theme_specifics.pb.h"
@@ -12,11 +14,19 @@
 
 namespace themes {
 
+#if BUILDFLAG(IS_ANDROID)
+DeviceThemeInfo<sync_pb::ThemeAndroidSpecifics> TranslateDesktop(
+    const sync_pb::ThemeSpecifics& desktop_specifics);
+
+DeviceThemeInfo<sync_pb::ThemeAndroidSpecifics> TranslateIos(
+    const sync_pb::ThemeIosSpecifics& ios_specifics);
+#else
 DeviceThemeInfo<sync_pb::ThemeSpecifics> TranslateAndroid(
     const sync_pb::ThemeAndroidSpecifics& android_specifics);
 
 DeviceThemeInfo<sync_pb::ThemeSpecifics> TranslateIos(
     const sync_pb::ThemeIosSpecifics& ios_specifics);
+#endif
 
 }  // namespace themes
 
