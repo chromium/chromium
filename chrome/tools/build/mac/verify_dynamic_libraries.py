edacee11ef4723448b5e63b851c4188811e5f1cd
Alex Moshchuk <alexmos@chromium.org>
2026-07-13 16:14:42 -0700

    Revert "[mac] Implement libaperitif.dylib and unit tests"
    This reverts commit a7e42f8cde417c9a4234049b91fabc3e36a2765a.
    
    Reason for revert: [Gardener] The new AperitifTest.InitializerOrder test is failing consistently on mac13-arm64-rel-tests and Mac13 Tests.
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/mac13-arm64-rel-tests/76347
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac13%20Tests/41946/overview
    
    Sample failure:
    [ RUN      ] AperitifTest.InitializerOrder
    ../../chrome/browser/mac/aperitif_unittest.mm:71: Failure
    Expected equality of these values:
      0
      rv
        Which is: -1
    Stack trace:
    0   unit_tests                          0x0000000104ff5e74 AperitifTest_InitializerOrder_Test::TestBody() + 2240
    
    ../../chrome/browser/mac/aperitif_unittest.mm:126: Failure
    Value of: found_libaperitif
      Actual: false
    Expected: true
    Stack trace:
    0   unit_tests                          0x0000000104ff64f8 AperitifTest_InitializerOrder_Test::TestBody() + 3908
    
    [  FAILED  ] AperitifTest.InitializerOrder (129 ms)
    
    Original change's description:
    > [mac] Implement libaperitif.dylib and unit tests
    >
    > This CL implements the aperitif'ed chrome_exe_main_mac.cc in C and puts
    > all the initialization logic into libaperitif.dylib, plus unit tests to
    > verify link-order rules and early initialization sequences. Most of the
    > code is copied from the original proof of concept cl in
    > https://crrev.com/c/7673901 with a caveat: the CL is not producing the
    > aperitif'ed helper binaries yet in chrome.app. Therefore, in order to
    > run aperitif related unit tests, a temporary test-only helper binary
    > ('aperitif_test_helper') is created.
    >
    > The temporary aperitif_test_helper will be removed in the next chain cl,
    > once we start producing aperitif'ed helper binaries in chrome.app, and
    > we can then test directly against the helpers.
    >
    > For a while, both `chrome_exe_main_mac.cc` and `chrome_exe_main_mac.c`
    > will be kept in parallel while we conduct A/B testing to gather more
    > metrics as we transition to use `chrome_exe_main_mac.c`. The end goal is
    > to completely replace the current `chrome_exe_main_mac.cc` with
    > `chrome_exe_main_mac.c` by the end of this project.
    >
    > Bug: 40794783
    > Change-Id: I85577c99a541efe83dddf17e4837596f953e098f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7951866
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Yue She <yueshe@google.com>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1661347}
    
    Bug: 40794783
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ia59128fb00ada14a14e786b5a4a5c16ea27a5557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8089139
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Owners-Override: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661456}

--- chrome/tools/build/mac/verify_dynamic_libraries.py
+++ chrome/tools/build/mac/verify_dynamic_libraries.py
@@ -19,60 +19,26 @@ import subprocess
 import sys
 
 
-def verify_image_libraries(image_path, allowed_libraries, exact_match,
-                           binary_path):
+def verify_image_libraries(image_path, allowed_libraries, binary_path):
     """Verifies that the Mach-O image residing at |image_path| is only linked
-    against dynamic libraries listed in |allowed_libraries|. If |exact_match| is
-    True, the linked libraries must exactly match the items and order of
-    |allowed_libraries|. If |exact_match| is False, the image does not have to
-    link against everything that is allowed, but it cannot link against
-    anything that is not allowed. Returns True if the image only links against
-    allowed libraries, and otherwise returns False with an error printed.
+    against dynamic libraries listed in |allowed_libraries|. The image does
+    not have to link against everything that is allowed, but it cannot link
+    against anything that is not allowed. Returns True if the image only
+    links against allowed libraries, and otherwise returns False with an error
+    printed.
     """
     # If |image_path| is a dynamic library, allow the LC_DYLIB_ID implicitly.
     output = subprocess.check_output(
         [binary_path + 'llvm-objdump', '--macho', '--dylib-id', image_path])
     dylib_id = output.decode('utf8').split('\n')[1]
     if dylib_id:
-        allowed_libraries.insert(0, dylib_id)
+        allowed_libraries.append(dylib_id)
 
-    disallowed_libraries = []
-    for i, linked_library in enumerate(
-            _iterate_dylibs_used(image_path, binary_path)):
-        if exact_match:
-            if i >= len(allowed_libraries) or \
-                    linked_library != allowed_libraries[i]:
-                disallowed_libraries.append(linked_library)
-        else:
-            if linked_library not in allowed_libraries:
-                disallowed_libraries.append(linked_library)
-
-    if not disallowed_libraries:
-        return True
-
-    if exact_match:
-        print(
-            '{} does not link against libraries in the correct order. Expected:'
-            .format(image_path),
-            file=sys.stderr)
-        for allowed_library in allowed_libraries:
-            print('    {}'.format(allowed_library), file=sys.stderr)
-        print('Actual:', file=sys.stderr)
-    else:
-        print('{} linked against the following disallowed libraries:'.format(
-            image_path),
-              file=sys.stderr)
-    for disallowed_library in disallowed_libraries:
-        print('    {}'.format(disallowed_library), file=sys.stderr)
-
-    return False
-
-
-def _iterate_dylibs_used(image_path, binary_path):
-    """Iterates through the dependent dynamic libraries of |image_path|."""
     output = subprocess.check_output(
         [binary_path + 'llvm-objdump', '--macho', '--dylibs-used', image_path])
     output = output.decode('utf8').strip()
+
+    disallowed_libraries = []
 
     # Skip the first line of output, which is image_path.
     linked_libraries = output.split('\n')[1:]
@@ -82,7 +48,20 @@ def _iterate_dylibs_used(image_path, binary_path):
         linked_library = linked_library.lstrip('\t')
         open_paren = linked_library.rindex('(')
         linked_library = linked_library[:open_paren - 1]
-        yield linked_library
+
+        if linked_library not in allowed_libraries:
+            disallowed_libraries.append(linked_library)
+
+    if not disallowed_libraries:
+        return True
+
+    print('{} linked against the following disallowed libraries:'.format(
+        image_path),
+          file=sys.stderr)
+    for disallowed_library in disallowed_libraries:
+        print('    {}'.format(disallowed_library), file=sys.stderr)
+
+    return False
 
 
 def main():
@@ -97,17 +76,10 @@ def main():
         action='append',
         default=[],
         help='Allow the specified dynamic library to be linked to the image.')
-    parser.add_argument(
-        '--exact-match',
-        action='store_true',
-        default=False,
-        help='If specified, the dynamic libraries linked into --image must '
-        'exactly match both the items and order of the --allow arguments.')
     parser.add_argument('-B', help='Path to look for the objdump binary.')
     args = parser.parse_args()
 
-    if not verify_image_libraries(args.image, args.allow, args.exact_match,
-                                  args.B):
+    if not verify_image_libraries(args.image, args.allow, args.B):
         return 1
 
     if args.stamp:
