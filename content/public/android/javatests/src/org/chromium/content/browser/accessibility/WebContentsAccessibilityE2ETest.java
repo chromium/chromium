d2938ffe8ab503b3bbe3a55f35135d676125e179
Ioana Treib <ioanap@chromium.org>
2026-07-13 05:44:11 -0700

    [Gardener] Disable flaky WebContentsAccessibilityE2ETest on auto
    Test being restricted to non-auto: fireGeneratedEvent_alertDisplayStyleChange_firesSubtreeChangeType
    
    Bug: 534257179
    Change-Id: I13e347264a7746a02515ea5d0fc15c80055700cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8085383
    Owners-Override: Ioana Treib <ioanap@chromium.org>
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661079}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityE2ETest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityE2ETest.java
@@ -783,6 +783,7 @@ WebView focusable actions:[FOCUS, AX_FOCUS] bundle:[chromeRole="rootWebArea"]
 
     @Test
     @SmallTest
+    @Restriction(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO) // flaky crbug.com/534257179
     @MinAndroidSdkLevel(Build.VERSION_CODES.KITKAT) // API Level 19
     public void fireGeneratedEvent_alertDisplayStyleChange_firesSubtreeChangeType()
             throws Throwable {
