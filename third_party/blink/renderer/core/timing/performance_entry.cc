01fcaf9fedcf725fc1eaac488af577d0bf84c5c3
Noam Rosenthal <nrosenthal@chromium.org>
2026-07-13 13:12:51 -0700

    Merge PaintTimingMixin flag
    Bug: 378827535
    Change-Id: I1286437de906e00b343b3cd7179a3b9237ca1af3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8086143
    Commit-Queue: Noam Rosenthal <nrosenthal@google.com>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661330}

--- third_party/blink/renderer/core/timing/performance_entry.cc
+++ third_party/blink/renderer/core/timing/performance_entry.cc
@@ -152,11 +152,9 @@ PerformanceEntry::EntryType PerformanceEntry::ToEntryTypeEnum(
 }
 
 DOMHighResTimeStamp PerformanceEntry::paintTime() const {
-  CHECK(RuntimeEnabledFeatures::PaintTimingMixinEnabled());
   return paint_timing_info_ ? paint_timing_info_->paint_time : 0;
 }
 std::optional<DOMHighResTimeStamp> PerformanceEntry::presentationTime() const {
-  CHECK(RuntimeEnabledFeatures::PaintTimingMixinEnabled());
   return paint_timing_info_ ? paint_timing_info_->presentation_time : 0;
 }
 
@@ -182,7 +180,7 @@ void PerformanceEntry::BuildJSONValue(V8ObjectBuilder& builder) const {
     builder.AddNumber("navigationId", navigationId());
   }
 
-  if (paint_timing_info_ && RuntimeEnabledFeatures::PaintTimingMixinEnabled()) {
+  if (paint_timing_info_) {
     builder.AddNumber("paintTime", paint_timing_info_->paint_time);
     builder.AddNumber("presentationTime",
                       paint_timing_info_->presentation_time);
