7fbf5967f184724d7a4dcac3cec15e51e2e86e74
Amya Singhal <amyasinghal@google.com>
2026-07-13 13:50:56 -0700

    [M151] [Voice Typing] Align FRE strings and onboarding dialog with UX specifications
    Original change's description:
    > [Voice Typing] Align FRE strings and onboarding dialog with UX specifications
    >
    > Video recording:
    > https://drive.google.com/file/d/1oiX_rPte1PUTXktOv59tZxh6HS_ogqzG/view?usp=sharing
    >
    > Screenshot: https://drive.google.com/file/d/1QtsIpzeJ753P8Tr8eZuqxEJ789puvB6N/view?usp=sharing
    > Bug: b:525857719, 529831909
    > Change-Id: Ic2ae679a66d28520688ac2db9aaf423a84c62eea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8032548
    > Reviewed-by: Dana Fried <dfried@chromium.org>
    > Commit-Queue: Amya Singhal <amyasinghal@google.com>
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1658325}
    
    (cherry picked from commit 99bf3ad0acacefabb5facc7bac9a708a65114189)
    
    Bug: 532256199,b:525857719
    Change-Id: Ic2ae679a66d28520688ac2db9aaf423a84c62eea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8071963
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7922@{#1073}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- chrome/browser/ui/views/dictation/onboarding_dialog_controller.cc
+++ chrome/browser/ui/views/dictation/onboarding_dialog_controller.cc
@@ -5,17 +5,30 @@
 #include "chrome/browser/ui/views/dictation/onboarding_dialog_controller.h"
 
 #include <utility>
+#include <vector>
 
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/tabs/public/tab_dialog_manager.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/grit/branded_strings.h"
+#include "chrome/grit/theme_resources.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/tabs/public/tab_interface.h"
+#include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/dialog_model.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/geometry/rounded_corners_f.h"
+#include "ui/views/background.h"
 #include "ui/views/bubble/bubble_dialog_model_host.h"
+#include "ui/views/bubble/bubble_frame_view.h"
+#include "ui/views/controls/image_view.h"
+#include "ui/views/controls/label.h"
+#include "ui/views/controls/separator.h"
+#include "ui/views/controls/styled_label.h"
+#include "ui/views/layout/box_layout_view.h"
 #include "ui/views/view_class_properties.h"
 
 namespace dictation {
@@ -26,7 +39,108 @@ DEFINE_ELEMENT_IDENTIFIER_VALUE(kDictationOnboardingCancelButtonElementId);
 
 namespace {
 inline constexpr char kOnboardingDialogName[] = "DictationOnboardingDialog";
+
+// TODO(crbug.com/530962875): Update typography font styles once PM & UX
+// reach alignment.
+std::unique_ptr<views::View> CreateOnboardingCardView() {
+  return views::Builder<views::BoxLayoutView>()
+      .SetOrientation(views::BoxLayout::Orientation::kVertical)
+      .SetBetweenChildSpacing(2)
+      .SetCrossAxisAlignment(views::BoxLayout::CrossAxisAlignment::kStretch)
+      .AddChildren(
+          // Row 1 (Microphone) - Top tile with 16px top corners & 4px bottom
+          // corners
+          views::Builder<views::BoxLayoutView>()
+              .SetOrientation(views::BoxLayout::Orientation::kHorizontal)
+              .SetBetweenChildSpacing(12)
+              .SetCrossAxisAlignment(
+                  views::BoxLayout::CrossAxisAlignment::kStart)
+              .SetBackground(views::CreateRoundedRectBackground(
+                  ui::kColorSysNeutralContainer,
+                  gfx::RoundedCornersF(16.0f, 16.0f, 0.0f, 0.0f)))
+              .SetInsideBorderInsets(gfx::Insets(16))
+              .AddChildren(
+                  views::Builder<views::ImageView>()
+                      .SetImage(ui::ImageModel::FromVectorIcon(
+                          vector_icons::kMicIcon, ui::kColorSysPrimary, 20))
+                      .SetProperty(views::kMarginsKey,
+                                   gfx::Insets::TLBR(2, 0, 0, 0)),
+                  views::Builder<views::Label>()
+                      .SetText(l10n_util::GetStringUTF16(
+                          IDS_DICTATION_ONBOARDING_BULLET_MICROPHONE))
+                      .SetTextStyle(views::style::STYLE_BODY_4)
+                      .SetLineHeight(18)
+                      .SetMultiLine(true)
+                      .SetHorizontalAlignment(gfx::ALIGN_LEFT)
+                      .SetEnabledColor(ui::kColorSysOnSurface)
+                      .SetSubpixelRenderingEnabled(false)),
+          // Row 2 (Sparkles) - Bottom tile with 0px top corners & 16px bottom
+          // corners
+          views::Builder<views::BoxLayoutView>()
+              .SetOrientation(views::BoxLayout::Orientation::kHorizontal)
+              .SetBetweenChildSpacing(12)
+              .SetCrossAxisAlignment(
+                  views::BoxLayout::CrossAxisAlignment::kStart)
+              .SetBackground(views::CreateRoundedRectBackground(
+                  ui::kColorSysNeutralContainer,
+                  gfx::RoundedCornersF(0.0f, 0.0f, 16.0f, 16.0f)))
+              .SetInsideBorderInsets(gfx::Insets(16))
+              .AddChildren(
+                  // TODO(crbug.com/530949784): Add speech-spark icon to
+                  // src-internal and replace vector_icons::kLightbulbIcon.
+                  views::Builder<views::ImageView>()
+                      .SetImage(ui::ImageModel::FromVectorIcon(
+                          vector_icons::kLightbulbIcon, ui::kColorSysPrimary,
+                          20))
+                      .SetProperty(views::kMarginsKey,
+                                   gfx::Insets::TLBR(2, 0, 0, 0)),
+                  views::Builder<views::Label>()
+                      .SetText(l10n_util::GetStringUTF16(
+                          IDS_DICTATION_ONBOARDING_BULLET_DATA_SHARING))
+                      .SetTextStyle(views::style::STYLE_BODY_4)
+                      .SetLineHeight(18)
+                      .SetMultiLine(true)
+                      .SetHorizontalAlignment(gfx::ALIGN_LEFT)
+                      .SetEnabledColor(ui::kColorSysOnSurface)
+                      .SetSubpixelRenderingEnabled(false)))
+      .Build();
 }
+
+std::unique_ptr<views::View> CreateBodyView(
+    base::RepeatingClosure learn_more_callback) {
+  auto container = views::Builder<views::BoxLayoutView>()
+                       .SetOrientation(views::BoxLayout::Orientation::kVertical)
+                       .SetBetweenChildSpacing(20)
+                       .Build();
+
+  container->AddChildView(CreateOnboardingCardView());
+
+  std::vector<std::u16string> replacements;
+  std::u16string link_text = l10n_util::GetStringUTF16(IDS_LEARN_MORE);
+  replacements.push_back(link_text);
+
+  std::vector<size_t> offsets;
+  std::u16string disclaimer_text = l10n_util::GetStringFUTF16(
+      IDS_DICTATION_ONBOARDING_BULLET_DISCLAIMER, replacements, &offsets);
+  size_t link_offset = offsets.empty() ? 0 : offsets[0];
+
+  auto disclaimer_label = std::make_unique<views::StyledLabel>();
+  disclaimer_label->SetText(disclaimer_text);
+  disclaimer_label->SetHorizontalAlignment(gfx::ALIGN_CENTER);
+  disclaimer_label->SetDefaultTextStyle(views::style::STYLE_BODY_5);
+  disclaimer_label->SetDefaultEnabledColorId(ui::kColorSysOnSurfaceSubtle);
+
+  views::StyledLabel::RangeStyleInfo link_style =
+      views::StyledLabel::RangeStyleInfo::CreateForLink(
+          std::move(learn_more_callback));
+  disclaimer_label->AddStyleRange(
+      gfx::Range(link_offset, link_offset + link_text.length()), link_style);
+
+  container->AddChildView(std::move(disclaimer_label));
+
+  return container;
+}
+}  // namespace
 
 OnboardingDialogController::OnboardingDialogController(tabs::TabInterface& tab)
     : tab_(tab) {}
@@ -50,11 +164,18 @@ void OnboardingDialogController::Show(base::OnceClosure complete_callback,
           CreateDialogModel(std::move(complete_callback)),
           ui::mojom::ModalType::kChild)
           .release();
+  model_host->set_fixed_width(512);
   model_host->SetOwnershipOfNewWidget(
       views::Widget::InitParams::CLIENT_OWNS_WIDGET);
 
   widget_ = tab_->GetTabFeatures()->tab_dialog_manager()->CreateAndShowDialog(
       model_host, std::make_unique<tabs::TabDialogManager::Params>());
+
+  if (auto* frame = model_host->GetBubbleFrameView()) {
+    if (auto* title_label = views::AsViewClass<views::Label>(frame->title())) {
+      title_label->SetTextStyle(views::style::STYLE_HEADLINE_5);
+    }
+  }
 
   widget_->MakeCloseSynchronous(base::BindOnce(
       &OnboardingDialogController::Close, base::Unretained(this)));
@@ -66,15 +187,19 @@ bool OnboardingDialogController::IsShowing() const {
 
 std::unique_ptr<ui::DialogModel> OnboardingDialogController::CreateDialogModel(
     base::OnceClosure complete_callback) {
-  // TODO(crbug.com/525857719): Temporary placeholder strings.
   return ui::DialogModel::Builder()
       .SetInternalName(kOnboardingDialogName)
       .SetTitle(l10n_util::GetStringUTF16(IDS_DICTATION_ONBOARDING_TITLE))
       .SetElementIdentifier(kDictationOnboardingDialogElementId)
-      .AddParagraph(ui::DialogModelLabel(l10n_util::GetStringUTF16(
-          IDS_DICTATION_ONBOARDING_BULLET_MICROPHONE)))
-      .AddParagraph(ui::DialogModelLabel(l10n_util::GetStringUTF16(
-          IDS_DICTATION_ONBOARDING_BULLET_DATA_SHARING)))
+      .SetBannerImage(
+          ui::ImageModel::FromResourceId(IDR_DICTATION_ONBOARDING_BANNER),
+          ui::ImageModel::FromResourceId(IDR_DICTATION_ONBOARDING_BANNER_DARK))
+      .AddCustomField(
+          std::make_unique<views::BubbleDialogModelHost::CustomView>(
+              CreateBodyView(base::BindRepeating(
+                  &OnboardingDialogController::OnLearnMoreLinkClicked,
+                  weak_ptr_factory_.GetWeakPtr())),
+              views::BubbleDialogModelHost::FieldType::kControl))
       .AddOkButton(base::BindOnce(&OnboardingDialogController::OnDialogAccepted,
                                   weak_ptr_factory_.GetWeakPtr(),
                                   std::move(complete_callback)),
@@ -89,6 +214,12 @@ std::unique_ptr<ui::DialogModel> OnboardingDialogController::CreateDialogModel(
                            .SetId(kDictationOnboardingCancelButtonElementId)
                            .SetStyle(ui::ButtonStyle::kTonal))
       .Build();
+}
+
+// TODO(crbug.com/530948293): add Learn More url here
+void OnboardingDialogController::OnLearnMoreLinkClicked() {
+  tab_->GetBrowserWindowInterface()->OpenGURL(
+      GURL("about:blank"), WindowOpenDisposition::NEW_FOREGROUND_TAB);
 }
 
 void OnboardingDialogController::OnDialogAccepted(
