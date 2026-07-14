1718214d6bb3f4cb78d8063fcaaa2c16e79f571f
Sophie Chang <sophiechang@chromium.org>
2026-07-14 09:55:16 -0700

    Turn on GlicContextualCueingV2AutoSubmit by default
    This does not have effect unless ContextualCueingV2 is on but this is flipped on in all exps and the current UI is by default complaint with autosubmit
    
    Bug: 508946258
    Change-Id: Icb80f97af893a0ca34da0579a4eb054966545147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8089727
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661947}

--- chrome/browser/glic/public/features.cc
+++ chrome/browser/glic/public/features.cc
@@ -162,7 +162,7 @@ const base::FeatureParam<int> kGlicReloadMaxLoadingTimeMs{
     &kGlicWebClientLoadTimes, "glic-reload-max-loading-time-ms", 30000};
 
 BASE_FEATURE(kGlicContextualCueingV2AutoSubmit,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kGlicWebDragAndDropFileUpload, base::FEATURE_ENABLED_BY_DEFAULT);
 
