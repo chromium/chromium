048556d189c7b1500e90e1a57fb83110ed8b59fb
Achuith Bhandarkar <achuith@chromium.org>
2026-07-14 04:05:58 -0700

    crosier: Refactor UserManager usage in ChromeOSIntegrationArcMixin
    Remove deprecated GetPrimaryUser() and GetActiveUser() calls from
    ChromeOSIntegrationArcMixin. Replace them with SessionManager's
    GetPrimarySession() and GetActiveSession() respectively, and use
    BrowserContextHelper::GetBrowserContextByAccountId() to map the session
    to BrowserContext.
    
    TAG=agy
    CONV=d0f7b601-ba2d-4df1-b974-a77abf96ed15
    
    Bug: 531278783
    Change-Id: Ibe8c3b66c1bc94ea341a5e26514951e900ff38a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8078486
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661742}

--- chrome/test/base/chromeos/crosier/chromeos_integration_arc_mixin.cc
+++ chrome/test/base/chromeos/crosier/chromeos_integration_arc_mixin.cc
@@ -26,7 +26,8 @@
 #include "chrome/test/base/chromeos/crosier/upstart.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "chromeos/ash/experiences/arc/metrics/arc_metrics_constants.h"
-#include "components/user_manager/user_manager.h"
+#include "components/session_manager/core/session.h"
+#include "components/session_manager/core/session_manager.h"
 #include "content/public/browser/browser_context.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/window.h"
@@ -42,12 +43,13 @@ class ArcBootWaiter : public arc::ArcBootPhaseMonitorBridge::Observer {
   ~ArcBootWaiter() override = default;
 
   void Wait() {
-    const user_manager::User* primary_user =
-        user_manager::UserManager::Get()->GetPrimaryUser();
-    CHECK(primary_user);
+    const session_manager::Session* primary_session =
+        session_manager::SessionManager::Get()->GetPrimarySession();
+    CHECK(primary_session);
 
     auto* browser_context =
-        ash::BrowserContextHelper::Get()->GetBrowserContextByUser(primary_user);
+        ash::BrowserContextHelper::Get()->GetBrowserContextByAccountId(
+            primary_session->account_id());
     arc::ArcBootPhaseMonitorBridge* boot_bridge =
         arc::ArcBootPhaseMonitorBridge::GetForBrowserContext(browser_context);
     CHECK(boot_bridge);
@@ -166,8 +168,11 @@ bool ShouldEnableArcVm() {
 
 // Gets the active user's browser context.
 content::BrowserContext* GetActiveUserBrowserContext() {
-  auto* user = user_manager::UserManager::Get()->GetActiveUser();
-  return ash::BrowserContextHelper::Get()->GetBrowserContextByUser(user);
+  const session_manager::Session* active_session =
+      session_manager::SessionManager::Get()->GetActiveSession();
+  CHECK(active_session);
+  return ash::BrowserContextHelper::Get()->GetBrowserContextByAccountId(
+      active_session->account_id());
 }
 
 void WaitForAppRegister(const std::string& app_id) {
