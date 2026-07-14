9577d58c6da2b5ad60c31777542348a6679b26bc
Ananya Seelam <ananyaseelam@google.com>
2026-07-14 11:22:35 -0700

    [context-input] Remove closed submitted tabs from flyout
    When tab deletion is enabled, remove closed submitted tabs from the
    flyout just like how closed unselected tabs are removed when the tab is
    closed.
    
    Before (tab mgmt flag + tab deletion flag):
    http://screencast/cast/NTE1Mjg1NTI5NTAwMDU3Nnw3YjMyOTVlYS0yYQ
    
    After (tab mgmt flag + tab deletion flag):
    http://screencast/cast/Njc1NTE5MzI5ODM1NDE3Nnw2MGQ0NzgwYS05YQ
    
    Bug: b:534406907
    Change-Id: I537d0d43fc7db1b11ff2fbad56739b8798edc048
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8087399
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Reviewed-by: Josiah Kiehl <kiehl@google.com>
    Cr-Commit-Position: refs/heads/main@{#1662015}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -168,6 +168,7 @@ export const ComposeboxEmbedderMixin =
             tabSuggestions: {type: Array},
             tabSuggestionsState: {type: Number},
             aimThreadRestoredTabs: {type: Array},
+            tabDeselectionEnabled: {type: Boolean},
             transcript: {type: String},
             uploadButtonDisabled: {
               type: Boolean,
@@ -212,6 +213,8 @@ export const ComposeboxEmbedderMixin =
             getLoadTimeBoolean('composeboxSmartTabSharingVisible', false);
         accessor contextManagementInComposeboxEnabled: boolean =
             getLoadTimeBoolean('contextManagementInComposeboxEnabled', false);
+        accessor tabDeselectionEnabled: boolean = getLoadTimeBoolean(
+            'composeboxContextMenuEnableTabDeselection', false);
         contextMenuDescriptionEnabled: boolean =
             getLoadTimeBoolean('composeboxShowContextMenuDescription', false);
         accessor showContextMenuDescription: boolean =
@@ -2382,6 +2385,16 @@ export const ComposeboxEmbedderMixin =
               this.deleteFile(uuid, /*fromUserAction=*/ false);
             });
 
+            if (this.tabDeselectionEnabled) {
+              const closedRestoredTabs = this.aimThreadRestoredTabs.filter(
+                  tab => !openTabIds.has(tab.tabId));
+              closedRestoredTabs.forEach(tab => {
+                this.getSearchboxHandler().deleteTabContext(tab.tabId);
+              });
+              this.aimThreadRestoredTabs = this.aimThreadRestoredTabs.filter(
+                  tab => openTabIds.has(tab.tabId));
+            }
+
             const restored = this.contextManagementInComposeboxEnabled ?
                 (this.aimThreadRestoredTabs || []) :
                 [];
@@ -2709,6 +2722,7 @@ export interface ComposeboxEmbedderMixinInterface extends
   submitButtonIconType: SubmitButtonIconType;
   tabSuggestions: TabInfo[];
   tabSuggestionsState: TabSuggestionsState;
+  tabDeselectionEnabled: boolean;
   transcript: string;
   uploadButtonDisabled: boolean;
   composeboxNoFlickerSuggestionsFix: boolean;
