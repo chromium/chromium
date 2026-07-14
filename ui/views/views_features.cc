d6c38c6334aadf6a436297c141148399e356f211
Rodney Ding <rodneyding@google.com>
2026-07-14 08:01:54 -0700

    Reland "Reenable original NVH impl on linux"
    This reverts commit f937c24e4a4c0700ac8ecb72b69d84b020c1195c.
    
    Reason for revert: reverting it caused regression with nested views, e.g. IndigoToolbar stops receiving event
    
    Original change's description:
    > Revert "Reenable original NVH impl on linux"
    >
    > This reverts commit 61632431b8b7389ea1c743fbcf84a63c790dd2f8.
    >
    > Reason for revert:
    > The root cause of 532038992 was fixed in crrev.com/c/8060981
    >
    > Original change's description:
    > > Reenable original NVH impl on linux
    > >
    > > Fixed: 532038992
    > > Change-Id: I1a5609dbc222a59be1e08186657df32620b409e0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8058002
    > > Reviewed-by: Robert Liao <robliao@chromium.org>
    > > Commit-Queue: Robert Liao <robliao@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1658315}
    >
    > Bug: 532038992
    > Change-Id: I7cc4c1a0ab8f43b0ab7745c3596f3a73c4cc39ad
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8070019
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Robert Liao <robliao@chromium.org>
    > Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1659639}
    
    Bug: 532038992
    Change-Id: I081aab397050299a6f3f518ba250ca6c7ff434b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8085527
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Rodney Ding <rodneyding@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661863}

--- ui/views/views_features.cc
+++ ui/views/views_features.cc
@@ -50,7 +50,7 @@ BASE_FEATURE(kKeyboardAccessibleTooltipInViews,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Uses NativeViewHostAuraWithClipWindow instead of NativeViewHostAura.
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_CHROMEOS)
 BASE_FEATURE(kUseNativeViewHostAuraWithClipWindow,
              base::FEATURE_DISABLED_BY_DEFAULT);
 #else
