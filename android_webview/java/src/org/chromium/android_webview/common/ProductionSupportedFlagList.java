241bdfc2c3346a1ff718f0f6298ef47f35c5dc7f
Xianzhu Wang <wangxianzhu@chromium.org>
2026-07-14 11:00:56 -0700

    Format ProductionSupportedFlagList.java
    to prevent errors in git cl upload for unrelated changes.
    
    Change-Id: Ic3e7f75978fadff68aada7f16fdad15b7914a687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8092892
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661996}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -999,7 +999,8 @@ public final class ProductionSupportedFlagList {
                 "Disallow script execution during blink lifecycle update."),
         Flag.baseFeature("ServiceWorkerAutoPreload"),
         Flag.baseFeature(GpuFeatures.WEB_GPU_USE_SPIRV14, "Use WebGPU's SPIR-V 1.4"),
-        Flag.baseFeature(GpuFeatures.WEB_GPU_USE_SPIRV_RECONVERGENCE_MODE,
+        Flag.baseFeature(
+                GpuFeatures.WEB_GPU_USE_SPIRV_RECONVERGENCE_MODE,
                 "Use WebGPU's SPIR-V reconvergence mode"),
         Flag.commandLine(
                 AwSwitches.STARTUP_NON_BLOCKING_WEBVIEW_CONSTRUCTOR,
