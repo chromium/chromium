dbd1f4182b91570dec95e1e83d713c26695ec831
Wenhu Cheng <wenhuc@google.com>
2026-07-14 14:53:05 -0700

    [151] [Voice Search]Re-enable NewTabPageAppTest.VoiceSearchCoherence
    Original change's description:
    > [Voice Search]Re-enable NewTabPageAppTest.VoiceSearchCoherence
    >
    > The test 'scrim is shown when voice search coherence dialog is open and
    > closes dialog on click' was flaky because clicking the scrim invokes
    > dialog.close(), which queues a task to dispatch the 'close' event on the
    > <dialog> element. The test previously awaited microtasksFinished()
    > (setTimeout(0)), which raced against the asynchronous 'close' event
    > dispatch that updates `showVoiceSearchOverlay_`.Now it would explicitly
    > await eventToPromise('close', dialog) before checking DOM state,
    > resolving the race condition.
    >
    > Fixed: b:527605085
    > Change-Id: Ic2fbfe26baa39fc5e36e3d98ba98091a4a8dd8b6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8075788
    > Commit-Queue: Wenhu Cheng <wenhuc@google.com>
    > Reviewed-by: James Leung <jamesleung@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1660546}
    
    Bug: 534397223
    Change-Id: Ic2fbfe26baa39fc5e36e3d98ba98091a4a8dd8b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8086049
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7922@{#1202}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.cc
+++ chrome/test/data/webui/new_tab_page/new_tab_page_browsertest.cc
@@ -504,7 +504,7 @@ IN_PROC_BROWSER_TEST_F(NewTabPageAppTest, AutoRemovalToast) {
           "runMochaSuite('NewTabPageAppTest AutoRemovalToast')");
 }
 
-IN_PROC_BROWSER_TEST_F(NewTabPageAppTest, DISABLED_VoiceSearchCoherence) {
+IN_PROC_BROWSER_TEST_F(NewTabPageAppTest, VoiceSearchCoherence) {
   RunTest("new_tab_page/app_test.js",
           "runMochaSuite('NewTabPageAppTest VoiceSearchCoherence')");
 }
