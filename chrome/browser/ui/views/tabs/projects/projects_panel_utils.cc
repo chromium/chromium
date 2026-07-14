73dc150a16cda54c63112b9ba50b5812e1449ccc
Kunal Daftari <kunaldaftari@google.com>
2026-07-14 09:22:21 -0700

    [Organizer] Remove obsolete projects panel views and tests
    Now that the controls (close button) is the only element visible in the
    projects panel, we can safely delete the other elements (threads, scroll
    handler, tab group items) and their corresponding tests.
    
    Bug: 532670693
    Change-Id: I839897fbe2745bb604b9175daaaca98cf534a9d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8087237
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Kunal Daftari <kunaldaftari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661917}

--- chrome/browser/ui/views/tabs/projects/projects_panel_utils.cc
+++ chrome/browser/ui/views/tabs/projects/projects_panel_utils.cc
@@ -4,75 +4,14 @@
 
 #include "chrome/browser/ui/views/tabs/projects/projects_panel_utils.h"
 
-#include "build/branding_buildflags.h"
-#include "chrome/browser/ui/browser_element_identifiers.h"
-#include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_utils.h"
-#include "chrome/browser/ui/views/tabs/projects/layout_constants.h"
 #include "components/saved_tab_groups/public/features.h"
-#include "components/vector_icons/vector_icons.h"
-#include "ui/base/ui_base_features.h"
-#include "ui/views/animation/ink_drop.h"
-#include "ui/views/animation/ink_drop_host.h"
-#include "ui/views/controls/button/button.h"
-#include "ui/views/controls/focus_ring.h"
-#include "ui/views/controls/highlight_path_generator.h"
-#include "ui/views/view_class_properties.h"
-
-namespace {
-
-std::unique_ptr<views::HighlightPathGenerator>
-GetListItemHighlightPathGenerator() {
-  return std::make_unique<views::RoundRectHighlightPathGenerator>(
-      /*insets=*/gfx::Insets(0),
-      /*corner_radius=*/projects_panel::kListItemCornerRadius);
-}
-
-}  // namespace
 
 namespace projects_panel {
 
 bool IsProjectsPanelVisibleForProfile(Profile* profile) {
   return tab_groups::IsProjectsPanelFeatureEnabled() &&
          tab_groups::SavedTabGroupUtils::IsEnabledForProfile(profile);
-}
-
-void ConfigureInkDropForButton(views::Button* view) {
-  auto* ink_drop = views::InkDrop::Get(view);
-  ink_drop->SetMode(views::InkDropHost::InkDropMode::ON);
-  ink_drop->SetLayerRegion(views::LayerRegion::kBelow);
-  ink_drop->SetBaseColor(kColorProjectsPanelButtonHoverBackground);
-  ink_drop->GetInkDrop()->SetHoverHighlightFadeDuration(
-      projects_panel::kListItemHoverFadeAnimationDuration);
-  ink_drop->SetHighlightOpacity(1.0f);
-  views::HighlightPathGenerator::Install(view,
-                                         GetListItemHighlightPathGenerator());
-  views::FocusRing::Install(view);
-  views::FocusRing::Get(view)->SetPathGenerator(
-      GetListItemHighlightPathGenerator());
-  views::FocusRing::Get(view)->SetHaloInset(
-      projects_panel::kListItemFocusRingHaloInset);
-}
-
-const gfx::VectorIcon& GetIconForThreadType(
-    contextual_tasks::ThreadType thread_type) {
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  switch (thread_type) {
-    case contextual_tasks::ThreadType::kAiMode:
-      return vector_icons::kGoogleGLogoMonochromeIcon;
-    case contextual_tasks::ThreadType::kGemini:
-      return vector_icons::kGoogleAgentspaceMonochromeLogo25Icon;
-    case contextual_tasks::ThreadType::kUnknown:
-      NOTREACHED();
-  }
-#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  return features::IsRoundedIconsEnabled() ? vector_icons::kChatSparkIcon
-                                           : vector_icons::kChatSparkOldIcon;
-}
-
-bool IsFirstFocusableViewInPanel(views::View* view) {
-  return view->GetProperty(views::kElementIdentifierKey) ==
-         kProjectsPanelButtonElementId;
 }
 
 }  // namespace projects_panel
--- chrome/browser/ui/views/tabs/projects/projects_panel_utils.h
+++ chrome/browser/ui/views/tabs/projects/projects_panel_utils.h
@@ -5,14 +5,6 @@
 #ifndef CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_PROJECTS_PANEL_UTILS_H_
 #define CHROME_BROWSER_UI_VIEWS_TABS_PROJECTS_PROJECTS_PANEL_UTILS_H_
 
-#include "components/contextual_tasks/public/contextual_task.h"
-#include "ui/gfx/vector_icon_types.h"
-
-namespace views {
-class Button;
-class View;
-}  // namespace views
-
 class Profile;
 
 namespace projects_panel {
@@ -20,17 +12,6 @@ namespace projects_panel {
 // Returns whether the Projects Panel and its entrypoints should be visible in
 // the UI for the profile.
 bool IsProjectsPanelVisibleForProfile(Profile* profile);
-
-void ConfigureInkDropForButton(views::Button* view);
-
-const gfx::VectorIcon& GetIconForThreadType(
-    contextual_tasks::ThreadType thread_type);
-
-// Returns whether this view is the first focusable view in the panel.
-// Currently, this is the panel close button. This is used for accessibility
-// reasons (e.g., to focus the 3-dot button in a tab group item view) when
-// determining whether the user performed a reverse focus traversal.
-bool IsFirstFocusableViewInPanel(views::View* view);
 
 }  // namespace projects_panel
 
