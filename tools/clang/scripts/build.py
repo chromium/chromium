191d5fbe5e85fa26aa2e0ad6566ba6adda1b2833
Hans Wennborg <hans@chromium.org>
2026-07-14 10:06:35 -0700

    [clang] Update for tip-of-tree version bump to 24
    Bug: 534655507
    Change-Id: I1fdf9238aed1fd094b82024504153c2b14e8ba50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8092315
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1661955}

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -825,6 +825,11 @@ def main():
 
   global CLANG_REVISION, PACKAGE_VERSION, LLVM_BUILD_DIR, STAMP_FILE, FORCE_HEAD_REVISION_FILE
 
+  # TODO(crbug.com/534655507): Remove in next Clang roll.
+  if args.llvm_force_head_revision:
+    global RELEASE_VERSION
+    RELEASE_VERSION = '24'
+
   if (args.pgo or args.thinlto) and not args.bootstrap:
     print('--pgo/--thinlto requires --bootstrap')
     return 1
