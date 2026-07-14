5731884cf7f9436c5054eb247dcb11a5210b427a
Georg Neis <neis@chromium.org>
2026-07-13 23:58:45 -0700

    ash: Move power api test out of crosapi/
    It does not involve crosapi anymore. Move it into the existing
    power_apitest.cc in the extensions directory (with buildflag guard).
    
    Bug: b:365902693
    Change-Id: Ibc85613d11ef833b485b8256b55ae0fd4c3ec83b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8083180
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Auto-Submit: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1661654}

--- chrome/browser/extensions/api/power/power_apitest.cc
+++ chrome/browser/extensions/api/power/power_apitest.cc
@@ -8,6 +8,16 @@
 #include "extensions/browser/api/power/power_api.h"
 #include "extensions/buildflags/buildflags.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "extensions/browser/api/idle/idle_manager.h"
+#include "extensions/browser/api/idle/idle_manager_factory.h"
+#include "extensions/browser/background_script_executor.h"
+#include "extensions/common/extension.h"
+#include "extensions/test/extension_test_message_listener.h"
+#include "extensions/test/result_catcher.h"
+#include "ui/base/idle/scoped_set_idle_state.h"
+#endif
+
 static_assert(BUILDFLAG(ENABLE_EXTENSIONS_CORE));
 
 namespace extensions {
@@ -21,6 +31,73 @@ IN_PROC_BROWSER_TEST_F(PowerApiTest, Basics) {
   // The test should leave no wake locks (no "level" for any extension).
   EXPECT_TRUE(PowerAPI::Get(profile())->extension_levels().empty());
 }
+
+#if BUILDFLAG(IS_CHROMEOS)
+
+constexpr char kExtensionRelativePath[] = "power/report_activity";
+constexpr char kExtensionId[] = "dibbenaepdnglcjpgjmnefmjccpinang";
+
+using ContextType = extensions::browser_test_util::ContextType;
+
+class PowerReportActivityApiTest
+    : public ExtensionApiTest,
+      public testing::WithParamInterface<ContextType> {
+ public:
+  PowerReportActivityApiTest() : ExtensionApiTest(GetParam()) {}
+};
+
+INSTANTIATE_TEST_SUITE_P(PersistentBackground,
+                         PowerReportActivityApiTest,
+                         ::testing::Values(ContextType::kPersistentBackground));
+
+INSTANTIATE_TEST_SUITE_P(ServiceWorker,
+                         PowerReportActivityApiTest,
+                         ::testing::Values(ContextType::kServiceWorker));
+
+// Verifies that chrome.power.reportActivity() correctly reports a user activity
+// by observing idle states.
+IN_PROC_BROWSER_TEST_P(PowerReportActivityApiTest,
+                       ReportActivityChangesIdleStateFromIdleToActive) {
+  ResultCatcher catcher;
+
+  ExtensionTestMessageListener ready_listener("ready");
+  ready_listener.set_extension_id(kExtensionId);
+
+  LoadExtension(test_data_dir_.AppendASCII(kExtensionRelativePath));
+
+  // Wait for idle state listener for be set up.
+  ASSERT_TRUE(ready_listener.WaitUntilSatisfied());
+
+  {
+    // Wait for the state to change to idle.
+    ExtensionTestMessageListener idle_listener("idle");
+    ui::ScopedSetIdleState idle(ui::IDLE_STATE_IDLE);
+    ASSERT_TRUE(idle_listener.WaitUntilSatisfied());
+
+    // Allow the idle state to go out of scope to reset it.
+    // Otherwise the QueryState() call is always going to return
+    // the test state, even though it actually changed.
+  }
+
+  auto* idle_manager = IdleManagerFactory::GetForBrowserContext(profile());
+  auto threshold = idle_manager->GetThresholdForTest(kExtensionId);
+  ASSERT_EQ(idle_manager->QueryState(threshold), ui::IDLE_STATE_IDLE);
+
+  // Report activity.
+  BackgroundScriptExecutor script_executor(profile());
+  constexpr char kReportActivityScript[] = R"(chrome.power.reportActivity();)";
+  script_executor.BackgroundScriptExecutor::ExecuteScriptAsync(
+      kExtensionId, kReportActivityScript,
+      BackgroundScriptExecutor::ResultCapture::kNone);
+
+  // The test succeeds if the state goes from idle to active.
+  ASSERT_TRUE(catcher.GetNextResult());
+
+  // Test that the actual state is active.
+  ASSERT_EQ(idle_manager->QueryState(threshold), ui::IDLE_STATE_ACTIVE);
+}
+
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace
 }  // namespace extensions
