cbca5f1352f654f312157975f27e4fa7e94b0bca
Ioana Pandele <ioanap@chromium.org>
2026-07-15 11:10:11 -0700

    Ensure authorization right exists and maps to the correct rule
    Bug: 533798896, 533256765
    Change-Id: I79058d62bf9b0dca4e396af7ac7fe1440f36bbf9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8098006
    Commit-Queue: Ioana Treib <ioanap@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662744}

--- chrome/browser/password_manager/password_manager_util_mac.mm
+++ chrome/browser/password_manager/password_manager_util_mac.mm
@@ -6,8 +6,9 @@
 
 #include <CoreFoundation/CoreFoundation.h>
 #import <Foundation/Foundation.h>
-#include <Security/Authorization.h>
+#include <Security/Security.h>
 
+#include "base/apple/bridging.h"
 #include "base/apple/bundle_locations.h"
 #include "base/apple/foundation_util.h"
 #include "base/apple/osstatus_logging.h"
@@ -27,14 +28,26 @@ NSString* UserAuthenticationRightName() {
 }
 
 bool EnsureAuthorizationRightExists() {
-  NSString* rightName = UserAuthenticationRightName();
-  // If the authorization right already exists there is nothing to do.
-  if (AuthorizationRightGet(rightName.UTF8String, nullptr) ==
+  NSString* right_name = UserAuthenticationRightName();
+  base::apple::ScopedCFTypeRef<CFDictionaryRef> right_definition;
+  if (AuthorizationRightGet(right_name.UTF8String,
+                            right_definition.InitializeInto()) ==
       errAuthorizationSuccess) {
-    return true;
+    id rule = [base::apple::CFToNSPtrCast(right_definition.get())
+        objectForKey:@(kAuthorizationRightRule)];
+    if (NSString* rule_string = base::apple::ObjCCast<NSString>(rule);
+        [rule_string
+            isEqualToString:@(kAuthorizationRuleAuthenticateAsSessionUser)]) {
+      return true;
+    }
+    if (NSArray* rule_array = base::apple::ObjCCast<NSArray>(rule); [rule_array
+            containsObject:@(kAuthorizationRuleAuthenticateAsSessionUser)]) {
+      return true;
+    }
   }
 
-  // The authorization right does not exist so create it.
+  // The authorization right does not exist or does not match the desired rule,
+  // so create or overwrite it.
   base::mac::ScopedAuthorizationRef authorization =
       base::mac::CreateAuthorization();
   if (!authorization) {
@@ -44,7 +57,7 @@ bool EnsureAuthorizationRightExists() {
   // Create a right which requires that the user authenticate as the session
   // owner. The prompt must be specified each time the right is requested.
   OSStatus status =
-      AuthorizationRightSet(authorization, rightName.UTF8String,
+      AuthorizationRightSet(authorization.get(), right_name.UTF8String,
                             CFSTR(kAuthorizationRuleAuthenticateAsSessionUser),
                             nullptr, nullptr, nullptr);
   if (status != errAuthorizationSuccess) {
