b40e9115761115a5b413fb8cb970060d2640943b
Ireneusz Szulc <ireneuszszulc@google.com>
2026-07-15 15:38:45 -0700

    AtMemory: Clean up obsolete memory data type utils
    This change cleans up unused utility functions in memory_data_type_util
    which is no longer needed after switching AtMemory to a new Personal
    Context architecture.
    
    Bug: b:524743933
    Change-Id: I47bb764f35f9e79fb051cfa5faf001da6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8036083
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662904}

--- components/accessibility_annotator/core/annotation_reducer/memory_data_type_util.h
+++ components/accessibility_annotator/core/annotation_reducer/memory_data_type_util.h
@@ -10,20 +10,9 @@
 #include "base/containers/span.h"
 #include "components/accessibility_annotator/core/annotation_reducer/memory_data_type.h"
 #include "components/accessibility_annotator/core/annotation_reducer/memory_search_result.h"
-#include "components/accessibility_annotator/core/data_models/entity.h"
-#include "components/accessibility_annotator/core/data_models/entity_types.h"
 #include "components/personal_context/proto/features/common_data.pb.h"
 
 namespace accessibility_annotator {
-
-// Maps an Entity and a MemoryDataType to a MemorySearchResult.
-// Returns an empty MemorySearchResult if the mapping is not supported.
-MemorySearchResult CreateResultFromEntity(MemoryDataType memory_data_type,
-                                          const Entity& entity);
-
-// Maps a MemoryDataType to a set of EntityTypes.
-EntityTypeEnumSet GetEntityTypesForMemoryDataType(
-    MemoryDataType memory_data_type);
 
 // Returns true if the given `type` is considered sensitive personal
 // information.
