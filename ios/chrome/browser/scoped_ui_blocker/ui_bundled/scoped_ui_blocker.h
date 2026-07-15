7d67e7455f6c32a407ef26679e63f0ce321cb5cd
Sylvain Defresne <sdefresne@chromium.org>
2026-07-15 06:53:18 -0700

    [ios] Add helper factory methods for ScopedUIBlocker
    In preparation for converting the client code, add two helper methods
    to create a ScopedUIBlocker with a given scoped (either to AppState
    or ProfileState).
    
    Those methods are not yet used.
    
    Bug: 519105565
    Change-Id: I68d1ee19f900cc21c0afe22a0f2b715a7d19bd63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8075053
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1662551}

--- ios/chrome/browser/scoped_ui_blocker/ui_bundled/scoped_ui_blocker.h
+++ ios/chrome/browser/scoped_ui_blocker/ui_bundled/scoped_ui_blocker.h
@@ -7,8 +7,13 @@
 
 #import <Foundation/Foundation.h>
 
+#import <memory>
+
+#import "base/types/pass_key.h"
 #import "ios/chrome/browser/scoped_ui_blocker/ui_bundled/ui_blocker_extent.h"
 
+@class SceneState;
+@class AppState;
 @protocol UIBlockerManager;
 @protocol UIBlockerTarget;
 
@@ -20,15 +25,32 @@ class ScopedUIBlocker {
   // block and not only profile related scenes.
   explicit ScopedUIBlocker(id<UIBlockerTarget> target,
                            UIBlockerExtent extent = UIBlockerExtent::kProfile);
+
+  // Used to make the constructor private.
+  using PassKey = base::PassKey<ScopedUIBlocker>;
+
+  // Block the UI with given `target` and `manager`.
+  ScopedUIBlocker(PassKey,
+                  id<UIBlockerTarget> target,
+                  id<UIBlockerManager> manager);
+
+  // ScopedUIBlocker is a non-copyable, non-moveable class.
+  ScopedUIBlocker(const ScopedUIBlocker&) = delete;
+  ScopedUIBlocker& operator=(const ScopedUIBlocker&) = delete;
+
   ~ScopedUIBlocker();
+
+  // Constructs a ProfileState scoped blocking UI.
+  static std::unique_ptr<ScopedUIBlocker> ProfileScoped(SceneState* scene);
+
+  // Constructs an AppState scoped blocking UI.
+  static std::unique_ptr<ScopedUIBlocker> AppScoped(SceneState* scene,
+                                                    AppState* app_state);
 
  private:
   // The target showing the blocking UI.
   __weak id<UIBlockerTarget> target_;
-  __weak id<UIBlockerManager> uiBlockerManager_;
-
-  ScopedUIBlocker(const ScopedUIBlocker&) = delete;
-  ScopedUIBlocker& operator=(const ScopedUIBlocker&) = delete;
+  __weak id<UIBlockerManager> manager_;
 };
 
 #endif  // IOS_CHROME_BROWSER_SCOPED_UI_BLOCKER_UI_BUNDLED_SCOPED_UI_BLOCKER_H_
--- ios/chrome/browser/scoped_ui_blocker/ui_bundled/scoped_ui_blocker.mm
+++ ios/chrome/browser/scoped_ui_blocker/ui_bundled/scoped_ui_blocker.mm
@@ -6,19 +6,42 @@
 
 #import "base/check.h"
 #import "base/logging.h"
+#import "ios/chrome/app/application_delegate/app_state.h"
+#import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/browser/scoped_ui_blocker/ui_bundled/ui_blocker_manager.h"
 #import "ios/chrome/browser/scoped_ui_blocker/ui_bundled/ui_blocker_target.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 
 ScopedUIBlocker::ScopedUIBlocker(id<UIBlockerTarget> target,
                                  UIBlockerExtent extent)
-    : target_(target) {
+    : ScopedUIBlocker(PassKey{},
+                      target,
+                      [target uiBlockerManagerForExtent:extent]) {}
+
+ScopedUIBlocker::ScopedUIBlocker(PassKey,
+                                 id<UIBlockerTarget> target,
+                                 id<UIBlockerManager> manager)
+    : target_(target), manager_(manager) {
   DCHECK(target_);
-  uiBlockerManager_ = [target uiBlockerManagerForExtent:extent];
-  DCHECK(uiBlockerManager_);
-  [uiBlockerManager_ incrementBlockingUICounterForTarget:target_];
+  DCHECK(manager_);
+  [manager_ incrementBlockingUICounterForTarget:target_];
 }
 
 ScopedUIBlocker::~ScopedUIBlocker() {
   DCHECK(target_) << "Cannot unlock the blocking UI if scene is deallocated.";
-  [uiBlockerManager_ decrementBlockingUICounterForTarget:target_];
+  [manager_ decrementBlockingUICounterForTarget:target_];
+}
+
+// static
+std::unique_ptr<ScopedUIBlocker> ScopedUIBlocker::ProfileScoped(
+    SceneState* scene) {
+  return std::make_unique<ScopedUIBlocker>(PassKey{}, scene,
+                                           scene.profileState);
+}
+
+// static
+std::unique_ptr<ScopedUIBlocker> ScopedUIBlocker::AppScoped(
+    SceneState* scene,
+    AppState* app_state) {
+  return std::make_unique<ScopedUIBlocker>(PassKey{}, scene, app_state);
 }
