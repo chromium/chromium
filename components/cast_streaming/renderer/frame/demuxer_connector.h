d8901193fa246d49d67eceda2151c7f016f57d32
Jordan Bayles <jophba@chromium.org>
2026-07-14 18:19:03 -0700

    [M151] [cast_streaming] Fix UaF in FrameInjectingDemuxer with intermediate obj
    Original change's description:
    > [cast_streaming] Fix UaF in FrameInjectingDemuxer with intermediate obj
    >
    > Decouple DemuxerConnector (main renderer thread) and
    > FrameInjectingDemuxer (media thread) to simplify the demuxer
    > initialization sequence and resolve potential lifetime issues.
    >
    > - Introduce DemuxerStreamConfigBuffer (base::RefCountedThreadSafe)
    >   to store stream initialization configurations received over Mojo IPC
    >   on the main thread.
    > - Remove raw demuxer pointer storage (demuxer_) and cross-thread
    >   registration/cleanup methods (SetDemuxer, OnDemuxerDestroyed) from
    >   DemuxerConnector.
    > - FrameInjectingDemuxer::Initialize reads stream configurations
    >   directly from DemuxerStreamConfigBuffer on the media thread without
    >   posting registration tasks across thread boundaries.
    > - Update ResourceProviderImpl to pass DemuxerConnector's config buffer
    >   to FrameInjectingDemuxer upon instantiation.
    >
    > Fixed: 522436154
    > Change-Id: I3fa1dbe88db3f7719b260fdd60a0c87cac68fcd5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7981482
    > Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    > Commit-Queue: Jordan Bayles <jophba@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1659974}
    
    (cherry picked from commit 35415ca0da1163d2bfbff35565627fb7470956a0)
    
    Bug: 533660445,522436154
    Change-Id: I3fa1dbe88db3f7719b260fdd60a0c87cac68fcd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8094082
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7922@{#1218}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- components/cast_streaming/renderer/frame/demuxer_connector.h
+++ components/cast_streaming/renderer/frame/demuxer_connector.h
@@ -6,23 +6,53 @@
 #define COMPONENTS_CAST_STREAMING_RENDERER_FRAME_DEMUXER_CONNECTOR_H_
 
 #include "base/functional/callback.h"
-#include "base/memory/raw_ptr.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
+#include "base/synchronization/lock.h"
+#include "base/task/sequenced_task_runner.h"
 #include "components/cast_streaming/common/public/mojom/demuxer_connector.mojom.h"
 #include "mojo/public/cpp/bindings/associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 
 namespace cast_streaming {
 
-class FrameInjectingDemuxer;
+// Thread-safe buffer used to store stream initialization configurations
+// received over Mojo IPC on the main thread and dispatch them to
+// FrameInjectingDemuxer on the media thread.
+class DemuxerStreamConfigBuffer
+    : public base::RefCountedThreadSafe<DemuxerStreamConfigBuffer> {
+ public:
+  using ConfigCallback =
+      base::OnceCallback<void(mojom::AudioStreamInitializationInfoPtr,
+                              mojom::VideoStreamInitializationInfoPtr)>;
+
+  DemuxerStreamConfigBuffer();
+
+  void SetConfigs(mojom::AudioStreamInitializationInfoPtr audio_stream_info,
+                  mojom::VideoStreamInitializationInfoPtr video_stream_info);
+
+  void ReadConfigs(scoped_refptr<base::SequencedTaskRunner> media_task_runner,
+                   ConfigCallback callback);
+
+ private:
+  friend class base::RefCountedThreadSafe<DemuxerStreamConfigBuffer>;
+  ~DemuxerStreamConfigBuffer();
+
+  base::Lock lock_;
+  bool has_configs_ GUARDED_BY(lock_) = false;
+  mojom::AudioStreamInitializationInfoPtr audio_stream_info_ GUARDED_BY(lock_);
+  mojom::VideoStreamInitializationInfoPtr video_stream_info_ GUARDED_BY(lock_);
+
+  scoped_refptr<base::SequencedTaskRunner> media_task_runner_ GUARDED_BY(lock_);
+  ConfigCallback pending_callback_ GUARDED_BY(lock_);
+};
 
 // Handles initiating the streaming session between the browser-process sender
 // and renderer-process receiver of the Cast Streaming Session. Specifically,
 // this class manages the DemuxerConnector's lifetime in the renderer
 // process. The lifetime of this object should match that of |render_frame| with
-// which it is associated, and is guaranteed to outlive the
-// FrameInjectingDemuxer that uses it, as the RenderFrame destruction will have
-// triggered its destruction first.
+// which it is associated.
 class DemuxerConnector final : public mojom::DemuxerConnector {
  public:
   DemuxerConnector();
@@ -33,8 +63,9 @@ class DemuxerConnector final : public mojom::DemuxerConnector {
   void BindReceiver(
       mojo::PendingAssociatedReceiver<mojom::DemuxerConnector> connector);
 
-  void SetDemuxer(FrameInjectingDemuxer* demuxer);
-  void OnDemuxerDestroyed();
+  scoped_refptr<DemuxerStreamConfigBuffer> config_buffer() const {
+    return config_buffer_;
+  }
 
   // Returns true if a Mojo connection is active.
   bool IsBound() const;
@@ -54,8 +85,8 @@ class DemuxerConnector final : public mojom::DemuxerConnector {
       this};
 
   EnableReceiverCallback enable_receiver_callback_;
-  raw_ptr<FrameInjectingDemuxer> demuxer_ = nullptr;
-  bool is_demuxer_initialized_ = false;
+  scoped_refptr<DemuxerStreamConfigBuffer> config_buffer_ =
+      base::MakeRefCounted<DemuxerStreamConfigBuffer>();
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
