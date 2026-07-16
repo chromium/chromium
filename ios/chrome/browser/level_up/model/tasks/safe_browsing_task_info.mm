854ce507140e9d859a8289625deb1c31abf85027
Robbie Gibson <rkgibson@google.com>
2026-07-15 21:11:29 -0700

    [Level Up] Implement navigation action for Enhanced Safe Browsing
    Wires up the Enhanced Safe Browsing task in Level Up to open the
    Enhanced Safe Browsing promo via BrowserCoordinatorCommands
    (showEnhancedSafeBrowsingPromo).
    
    Bug: b:513246178
    Change-Id: Ib294b7a945698cb8ffc834007cb433b1ba1880d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8086446
    Reviewed-by: Pier-Alexandre Bouchard <pabouchard@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663033}

--- ios/chrome/browser/level_up/model/tasks/safe_browsing_task_info.mm
+++ ios/chrome/browser/level_up/model/tasks/safe_browsing_task_info.mm
@@ -2,10 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "base/functional/bind.h"
 #import "base/functional/callback_helpers.h"
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/level_up/model/task_info.h"
 #import "ios/chrome/browser/level_up/model/tasks/task_factories.h"
+#import "ios/chrome/browser/shared/public/commands/browser_coordinator_commands.h"
+#import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -36,7 +39,11 @@ class SafeBrowsingTaskInfo : public TaskInfo {
         IDS_IOS_LEVEL_UP_TASK_COMPLETED_SAFE_BROWSING);
   }
   TaskInfo::NavigationAction GetNavigationAction() const override {
-    return base::DoNothing();
+    return base::BindRepeating(^(CommandDispatcher* dispatcher) {
+      id<BrowserCoordinatorCommands> handler =
+          HandlerForProtocol(dispatcher, BrowserCoordinatorCommands);
+      [handler showEnhancedSafeBrowsingPromo];
+    });
   }
 };
 
