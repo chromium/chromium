3ae7a648d7c1fb08ef20e2f8f703c78a60bd39b6
James Cook <jamescook@chromium.org>
2026-07-17 13:19:58 -0700

    Disable ServiceWorkerSuspensionOnExtensionUnload on Android
    It is flakily failing with crashes in a variety of locations. It may
    take some time to figure out, so disable it for now.
    
    Bug: 534230908
    Change-Id: Ib27680cf631578c1159ae8a36ad8e534867eeaea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116273
    Auto-Submit: James Cook <jamescook@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664139}

--- chrome/browser/extensions/service_worker_apitest.cc
+++ chrome/browser/extensions/service_worker_apitest.cc
@@ -1332,8 +1332,16 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerTest,
   EXPECT_EQ("true", ExecuteScriptInBackgroundPage(extension->id(), kScript));
 }
 
+// Flaky crashes during test setup. http://crbug.com/534230908
+#if BUILDFLAG(IS_ANDROID)
+#define MAYBE_ServiceWorkerSuspensionOnExtensionUnload \
+  DISABLED_ServiceWorkerSuspensionOnExtensionUnload
+#else
+#define MAYBE_ServiceWorkerSuspensionOnExtensionUnload \
+  ServiceWorkerSuspensionOnExtensionUnload
+#endif
 IN_PROC_BROWSER_TEST_F(ServiceWorkerTest,
-                       ServiceWorkerSuspensionOnExtensionUnload) {
+                       MAYBE_ServiceWorkerSuspensionOnExtensionUnload) {
   // For this test, only hold onto the extension's ID and URL + a function to
   // get a resource URL, because we're going to be disabling and uninstalling
   // it, which will invalidate the pointer.
