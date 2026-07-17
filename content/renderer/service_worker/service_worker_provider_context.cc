63137823a4827f267c3519ddc17a5b8e4352be38
Shunya Shishido <sisidovski@chromium.org>
2026-07-16 19:19:19 -0700

    [M144-LTS] Reset states if updated and the new controller does not have routers
    (cherry picked from commit 6b56d1e9e2ced8fa508bce07563a0c903eb50203)
    
    Change-Id: I8753a1143a452777eaef58c872dbf103c5e37ca9
    Bug: 516827905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874295
    Auto-Submit: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1637502}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8084084
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Gyuyoung Kim (xWF) <qkim@google.com>
    Owners-Override: Victor Gabriel Savu <vsavu@google.com>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/branch-heads/7559@{#5115}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- content/renderer/service_worker/service_worker_provider_context.cc
+++ content/renderer/service_worker/service_worker_provider_context.cc
@@ -442,6 +442,14 @@ void ServiceWorkerProviderContext::SetController(
         std::move(controller_info->router_data->running_status_receiver);
     remote_cache_storage_ =
         std::move(controller_info->router_data->remote_cache_storage);
+  } else {
+    // The new controller has no static router. Reset any router state inherited
+    // from a previous controller so that a subsequent subresource loader
+    // factory cannot pair stale router rules.
+    router_rules_.reset();
+    initial_running_status_.reset();
+    running_status_receiver_.reset();
+    remote_cache_storage_.reset();
   }
 
   // Propagate the controller to workers related to this provider.
