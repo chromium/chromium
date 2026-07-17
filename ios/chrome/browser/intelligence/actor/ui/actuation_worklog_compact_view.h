d2e2006cea30ce996a8bb37346df2d715cdf3edf
Francis Beauchamp <fbeauchamp@google.com>
2026-07-17 08:17:12 -0700

    [iOS] Introduce ActuationWorklogCompactView for agentic worklog.
    Introduce ActuationWorklogCompactView to display a single active step
    in the agentic timeline. The view supports vertical slide-up
    transitions between steps, automatic height updates on device
    orientation or bounds changes, and a private serialization queue to
    handle fast sequential step triggers safely without layout overlaps.
    
    How the transitions and layout looks: https://screenshot.googleplex.com/3mM2aEMPMjFhqui
    
    Bug: 532209191
    Change-Id: Ib9ee098cff4568677894900c5d8e902c6aa0825d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8084603
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Commit-Queue: Francis Beauchamp <fbeauchamp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663914}

---
+++ ios/chrome/browser/intelligence/actor/ui/actuation_worklog_compact_view.h
@@ -0,0 +1,39 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_INTELLIGENCE_ACTOR_UI_ACTUATION_WORKLOG_COMPACT_VIEW_H_
+#define IOS_CHROME_BROWSER_INTELLIGENCE_ACTOR_UI_ACTUATION_WORKLOG_COMPACT_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+@class ActuationWorklogCompactView;
+@class ActuationWorklogItem;
+
+// Delegate for handling height change updates during transitions.
+@protocol ActuationWorklogCompactViewDelegate <NSObject>
+// Called when the view's height changes due to a layout transition.
+- (void)worklogCompactView:(ActuationWorklogCompactView*)view
+           didChangeHeight:(CGFloat)targetHeight;
+@end
+
+// Compact view displaying a single active step in the timeline.
+// Smoothly translates between steps vertically. A safety mechanism is added
+// to queue transitions in case they happen too quickly.
+@interface ActuationWorklogCompactView : UIView
+
+// The delegate for height update notifications.
+@property(nonatomic, weak) id<ActuationWorklogCompactViewDelegate> delegate;
+
+// Designated initializer.
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
+- (instancetype)initWithCoder:(NSCoder*)coder NS_UNAVAILABLE;
+
+// Transition to display a new item.
+- (void)transitionToItem:(ActuationWorklogItem*)item animated:(BOOL)animated;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_INTELLIGENCE_ACTOR_UI_ACTUATION_WORKLOG_COMPACT_VIEW_H_
---
+++ ios/chrome/browser/intelligence/actor/ui/actuation_worklog_compact_view.mm
@@ -0,0 +1,198 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/intelligence/actor/ui/actuation_worklog_compact_view.h"
+
+#import "base/check.h"
+#import "ios/chrome/browser/intelligence/actor/ui/actuation_worklog_item.h"
+#import "ios/chrome/browser/intelligence/actor/ui/actuation_worklog_item_view.h"
+
+namespace {
+
+// Animation transition duration in seconds.
+const NSTimeInterval kAnimationDuration = 0.5;
+
+// Spring damping ratio (1.0 = critically damped, no overshoot).
+const CGFloat kSpringDamping = 1.0;
+
+}  // namespace
+
+// Private helper container representing a pending layout transition queue item.
+@interface ActuationWorklogPendingTransition : NSObject
+@property(nonatomic, strong) ActuationWorklogItem* item;
+@property(nonatomic, assign) BOOL animated;
+@end
+
+@implementation ActuationWorklogPendingTransition
+@end
+
+@implementation ActuationWorklogCompactView {
+  ActuationWorklogItemView* _currentView;
+  ActuationWorklogItemView* _nextView;
+
+  NSLayoutConstraint* _currentTopConstraint;
+
+  NSMutableArray<ActuationWorklogPendingTransition*>* _pendingTransitions;
+  BOOL _isTransitioning;
+  CGFloat _lastReportedHeight;
+}
+
+- (instancetype)init {
+  self = [super initWithFrame:CGRectZero];
+  if (self) {
+    self.clipsToBounds = YES;
+    _pendingTransitions = [NSMutableArray array];
+    _isTransitioning = NO;
+    _lastReportedHeight = 0.0;
+  }
+  return self;
+}
+
+- (void)transitionToItem:(ActuationWorklogItem*)item animated:(BOOL)animated {
+  CHECK(item);
+  if (!_currentView) {
+    [self loadFirstItem:item];
+    return;
+  }
+
+  ActuationWorklogPendingTransition* transition =
+      [[ActuationWorklogPendingTransition alloc] init];
+  transition.item = item;
+  transition.animated = animated;
+
+  [_pendingTransitions addObject:transition];
+  [self processNextPendingTransitionIfNeeded];
+}
+
+#pragma mark - UIView
+
+- (void)layoutSubviews {
+  [super layoutSubviews];
+
+  // Only update height when resting (not transitioning).
+  if (_currentView && !_isTransitioning) {
+    [self notifyHeight:_currentView.bounds.size.height];
+  }
+}
+
+#pragma mark - Private
+
+// Dequeues and executes the next available transition if we are not currently
+// transitioning.
+- (void)processNextPendingTransitionIfNeeded {
+  if (_isTransitioning || _pendingTransitions.count == 0) {
+    return;
+  }
+
+  _isTransitioning = YES;
+  ActuationWorklogPendingTransition* transition =
+      _pendingTransitions.firstObject;
+  [_pendingTransitions removeObjectAtIndex:0];
+
+  [self executeTransition:transition];
+}
+
+// Prepares and executes a queued transition.
+- (void)executeTransition:(ActuationWorklogPendingTransition*)transition {
+  [self prepareTransitionToItem:transition.item];
+
+  CGFloat targetHeight = _nextView.bounds.size.height;
+  _currentTopConstraint.constant = -_currentView.bounds.size.height;
+
+  [self transitionToHeight:targetHeight animated:transition.animated];
+}
+
+// Instantiates the incoming step view and anchors it directly below the active
+// view in preparation for the slide transition.
+- (void)prepareTransitionToItem:(ActuationWorklogItem*)item {
+  _nextView = [self addViewWithItem:item
+                          connector:ActuationWorklogConnectorVisibility::kBoth];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [_nextView.topAnchor constraintEqualToAnchor:_currentView.bottomAnchor],
+  ]];
+
+  // Force placement of `_nextView` directly below
+  [self layoutIfNeeded];
+}
+
+// Executes the height layout change transition.
+- (void)transitionToHeight:(CGFloat)targetHeight animated:(BOOL)animated {
+  if (!animated) {
+    [self notifyHeight:targetHeight];
+    [self transitionDidComplete];
+    return;
+  }
+
+  __weak __typeof(self) weakSelf = self;
+  [UIView animateWithDuration:kAnimationDuration
+      delay:0.0
+      usingSpringWithDamping:kSpringDamping
+      initialSpringVelocity:0.0
+      options:UIViewAnimationOptionCurveEaseInOut
+      animations:^{
+        [weakSelf notifyHeight:targetHeight];
+        [weakSelf layoutIfNeeded];
+      }
+      completion:^(BOOL finished) {
+        [weakSelf transitionDidComplete];
+      }];
+}
+
+// Finalizes the layout state and view promotion once a transition completes.
+- (void)transitionDidComplete {
+  if (_nextView) {
+    [_currentView removeFromSuperview];
+    _currentView = _nextView;
+    _currentTopConstraint =
+        [_currentView.topAnchor constraintEqualToAnchor:self.topAnchor];
+    _currentTopConstraint.active = YES;
+    _nextView = nil;
+  }
+
+  _isTransitioning = NO;
+  [self processNextPendingTransitionIfNeeded];
+}
+
+// Renders the first step immediately without transition animations.
+- (void)loadFirstItem:(ActuationWorklogItem*)item {
+  _currentView =
+      [self addViewWithItem:item
+                  connector:ActuationWorklogConnectorVisibility::kBottom];
+
+  _currentTopConstraint =
+      [_currentView.topAnchor constraintEqualToAnchor:self.topAnchor];
+  _currentTopConstraint.active = YES;
+  [self layoutIfNeeded];
+  [self notifyHeight:_currentView.bounds.size.height];
+}
+
+// Helper to instantiate, add, and horizontally anchor a new step view.
+- (ActuationWorklogItemView*)
+    addViewWithItem:(ActuationWorklogItem*)item
+          connector:(ActuationWorklogConnectorVisibility)connector {
+  ActuationWorklogItemView* view = [[ActuationWorklogItemView alloc] init];
+  view.translatesAutoresizingMaskIntoConstraints = NO;
+  [self addSubview:view];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [view.leadingAnchor constraintEqualToAnchor:self.leadingAnchor],
+    [view.trailingAnchor constraintEqualToAnchor:self.trailingAnchor],
+  ]];
+
+  view.connectorVisibility = connector;
+  [view configureWithItem:item];
+  return view;
+}
+
+// Notifies `delegate` of view height changes, guarding against redundant calls.
+- (void)notifyHeight:(CGFloat)height {
+  if (height == _lastReportedHeight) {
+    return;
+  }
+  _lastReportedHeight = height;
+  [self.delegate worklogCompactView:self didChangeHeight:height];
+}
+
+@end
