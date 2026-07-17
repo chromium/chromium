906762751bd3b7848038c93675a6f6883f82028c
Lauren Winston <lwinston@google.com>
2026-07-16 18:10:44 -0700

    Refactor getReadingModeTextNodes.
    Prepwork CL for fixing some line focus bugs.
    
    Breaking this off into its own CL to give
    it proper scrutinty before using it in the line
    focus code.
    
    Bug: 534434606
    Change-Id: Ide0f141b156105cbff0827ea06b59506ed3389ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8109817
    Reviewed-by: Gustavo Martin <gusmartin@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1663617}

--- chrome/browser/resources/side_panel/read_anything/shared/common.ts
+++ chrome/browser/resources/side_panel/read_anything/shared/common.ts
@@ -95,19 +95,28 @@ export function getReadingModeTextNodes(root: Node): Node[] {
   const walker = createVisibleTreeWalker(root);
   let currentNode;
 
+  while ((currentNode = getNextValidNode(walker))) {
+    textNodes.push(currentNode);
+  }
+  return textNodes;
+}
+
+// Gets the next valid node for reading mode using the given TreeWalker.
+// This includes valid marker elements and text nodes.
+function getNextValidNode(walker: TreeWalker): Node|null {
+  let currentNode;
   while (currentNode = walker.nextNode()) {
     if (currentNode.nodeType === Node.ELEMENT_NODE) {
       const marker = addNodeForListElement(currentNode as HTMLElement);
       if (marker) {
-        textNodes.push(marker);
+        return marker;
       }
-    } else if (currentNode.nodeType === Node.TEXT_NODE) {
-      if (currentNode.textContent) {
-        textNodes.push(currentNode);
-      }
+    } else if (
+        currentNode.nodeType === Node.TEXT_NODE && currentNode.textContent) {
+      return currentNode;
     }
   }
-  return textNodes;
+  return null;
 }
 
 // Creates a TreeWalker configured to skip hidden elements, consistent with
