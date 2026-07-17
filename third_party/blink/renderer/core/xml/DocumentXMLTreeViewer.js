5358ec361931657efe3b683146d756cbeb05d8d4
Kalvin Lee <kdlee@chromium.org>
2026-07-16 22:19:47 -0700

    Revert "xml: Preserve whitespace in CDATA sections in tree viewer"
    This reverts commit c56bc18b23e5825102c84d1044aea09c851cde0f.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5528320604110848
    
    Sample build with failed test: https://ci.chromium.org/b/8676104521016792897
    Affected test(s):
    [://\:headless_shell_wpt!webtest::external/wpt/xml/xslt#large-cdata.html](https://ci.chromium.org/ui/test/chromium/:%2F%2F%5C:headless_shell_wpt%21webtest::external%2Fwpt%2Fxml%2Fxslt%23large-cdata.html?q=VHash%3A755e12d2dffc4cb2)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5528320604110848&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8084801&type=BUG
    
    Original change's description:
    > xml: Preserve whitespace in CDATA sections in tree viewer
    >
    > The XML tree viewer's DocumentXMLTreeViewer.js rendered CDATA
    > sections with a generic createText() span that has no CSS class,
    > so whitespace was collapsed by the default white-space: normal.
    > Adjacent comments used createComment(), which is tagged with a
    > "comment" class that sets white-space: pre, so comments preserved
    > whitespace while CDATA did not.
    >
    > Add a dedicated createCDATA() that tags the span with a new
    > "cdata" class, and share the white-space: pre rule between
    > .comment and .cdata so CDATA rendering matches "View Source" and
    > is consistent with how comments are already displayed.
    >
    > Bug: 40757386
    > Change-Id: Ib5939e08f289759ed6b40be00a9fa85467a1419b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8084801
    > Commit-Queue: David Baron <dbaron@chromium.org>
    > Reviewed-by: David Baron <dbaron@chromium.org>
    > Reviewed-by: Joey Arhar <jarhar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1663149}
    >
    
    Bug: 40757386
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I01dec981ab7b7ba4307f831617ddf81aac5248cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8114566
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1663670}

--- third_party/blink/renderer/core/xml/DocumentXMLTreeViewer.js
+++ third_party/blink/renderer/core/xml/DocumentXMLTreeViewer.js
@@ -155,7 +155,7 @@ function processComment(parentElement, node)
 function processCDATA(parentElement, node)
 {
   var line = createLine();
-  line.appendChild(createCDATA('<![CDATA[ ' + node.nodeValue + ' ]]>'));
+  line.appendChild(createText('<![CDATA[ ' + node.nodeValue + ' ]]>'));
   parentElement.appendChild(line);
 }
 
@@ -228,14 +228,6 @@ function createComment(commentString)
   comment.classList.add('html-comment');
   comment.textContent = commentString;
   return comment;
-}
-
-function createCDATA(cdataString)
-{
-  var cdata = createHTMLElement('span');
-  cdata.classList.add('cdata');
-  cdata.textContent = cdataString;
-  return cdata;
 }
 
 function createText(value)
