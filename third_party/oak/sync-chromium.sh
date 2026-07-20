d6c01d542c6fcb0ce7ae6fc538b9384871e51872
Kamil Jarosz <kjarosz@google.com>
2026-07-20 08:33:59 -0700

    oak: Add sync-chromium.sh and crowbar.txt
    Introduce a `sync-chromium.sh` script to automate copying the required
    proto files from upstream `src/` into the `chromium/` directory used
    by Chromium build targets.
    
    Also add `crowbar.txt` documenting the dependency update steps in
    accordance with the Crowbar third-party management guidelines, and
    clean up `README.chromium` by removing the `Date` metadata field.
    
    Change-Id: If49bff63f94bf701644310e8eab75ce7375ab2de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116774
    Reviewed-by: Jay Zhou <zhouzj@google.com>
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664704}

---
+++ third_party/oak/sync-chromium.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2026 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+shopt -s extglob dotglob
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+FILES=(
+  "proto/attestation/endorsement.proto"
+  "proto/attestation/eventlog.proto"
+  "proto/attestation/evidence.proto"
+  "proto/attestation/reference_value.proto"
+  "proto/attestation/tcb_version.proto"
+  "proto/crypto/certificate.proto"
+  "proto/crypto/crypto.proto"
+  "proto/digest.proto"
+  "proto/session/messages.proto"
+  "proto/session/session.proto"
+  "proto/variant.proto"
+)
+
+for f in "${FILES[@]}"; do
+  cp --update=all "${SCRIPT_DIR}/src/${f}" "${SCRIPT_DIR}/chromium/${f}"
+  sed -i 's|google/protobuf/any.proto|proto/chromium_types/any.proto|g' "${SCRIPT_DIR}/chromium/${f}"
+  sed -i 's|google.protobuf.Any|Any|g' "${SCRIPT_DIR}/chromium/${f}"
+  sed -i 's|google/protobuf/timestamp.proto|proto/chromium_types/timestamp.proto|g' "${SCRIPT_DIR}/chromium/${f}"
+  sed -i 's|google.protobuf.Timestamp|Timestamp|g' "${SCRIPT_DIR}/chromium/${f}"
+done
