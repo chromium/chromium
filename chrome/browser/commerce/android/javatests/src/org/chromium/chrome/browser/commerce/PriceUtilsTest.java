35274702423d6ce48320ecb3432893bfcaaf625b
Henrique Nakashima <hnakashima@chromium.org>
2026-07-20 13:47:44 -0700

    [Android] Convert PriceUtilsTest to unit device test
    Moves PriceUtilsTest out of javatests in chrome_public_test_apk to
    a dedicated unit_device_javatests target in commerce/android/BUILD.gn.
    
    Bug: 536872967
    Change-Id: If773d67fc61469a3c14b693329909e1dbf2a53a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8121214
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664918}

--- chrome/browser/commerce/android/javatests/src/org/chromium/chrome/browser/commerce/PriceUtilsTest.java
+++ chrome/browser/commerce/android/javatests/src/org/chromium/chrome/browser/commerce/PriceUtilsTest.java
@@ -11,9 +11,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.BaseJUnit4ClassRunner;
+import org.chromium.base.test.util.Batch;
 
 /** Test for {@link PriceUtils}. */
 @RunWith(BaseJUnit4ClassRunner.class)
+@Batch(Batch.UNIT_TESTS)
 public class PriceUtilsTest {
     private static final int MICROS_TO_UNITS = 1000000;
 
