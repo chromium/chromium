69f41203e93789fa9c51dc9aa319373af0872b8f
Allen Bauer <kylixrd@chromium.org>
2026-07-20 08:41:52 -0700

    Fix views_examples crash on shutdown
    Call ui::Clipboard::DestroyClipboardForCurrentThread() on shutdown to
    clean up the thread-local clipboard and prevent leaking the HWND
    message window, which causes a class unregistration DCHECK failure.
    
    Change-Id: I0ee6eb2a251181eec1bcdbd04449f392761fec3a
    Bug: 535826279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8118647
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664708}

--- ui/views/examples/examples_main_proc.cc
+++ ui/views/examples/examples_main_proc.cc
@@ -30,6 +30,7 @@
 #include "components/viz/service/frame_sinks/frame_sink_manager_impl.h"
 #include "mojo/core/embedder/embedder.h"
 #include "ui/accessibility/platform/ax_platform_for_test.h"
+#include "ui/base/clipboard/clipboard.h"
 #include "ui/base/ime/init/input_method_initializer.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
@@ -237,6 +238,8 @@ ExamplesExitCode ExamplesMainProc(bool under_test, ExampleVector examples) {
 #if defined(USE_AURA)
   env.reset();
 #endif
+
+  ui::Clipboard::DestroyClipboardForCurrentThread();
 
   return compare_result;
 }
