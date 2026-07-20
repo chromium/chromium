dbf32ddc3199dcb804f2dc838e09272e58e33568
Frédéric Wang Nélar <fwang@igalia.com>
2026-07-20 13:35:39 -0700

    [compression-dictionary] Add WPT test to ensure integrity attribute is ignored.
    See https://html.spec.whatwg.org/#attr-link-integrity
    
    Bug: 522338661, 40255884
    Change-Id: I2454f5305b855b21ca40c2177fabb7ecbf2e9a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8117450
    Commit-Queue: Frédéric Wang Nélar <fwang@igalia.com>
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1664905}

--- third_party/blink/web_tests/external/wpt/fetch/compression-dictionary/resources/fetch-options-worker.js
+++ third_party/blink/web_tests/external/wpt/fetch/compression-dictionary/resources/fetch-options-worker.js
@@ -3,10 +3,12 @@ self.addEventListener('fetch', function(event) {
     const params = new URL(event.request.url).searchParams;
     const credentials = params.get("credentials");
     const mode = params.get("mode");
+    const integrity = params.get('integrity');
     const referrer = params.get('referrer');
     const referrerPolicy = params.get('referrerPolicy');
     if ((credentials === null || credentials == event.request.credentials) &&
         (mode === null || mode == event.request.mode) &&
+        (integrity === null || integrity == event.request.integrity) &&
         (referrer === null || referrer == event.request.referrer) &&
         (referrerPolicy === null ||
          referrerPolicy == event.request.referrerPolicy)) {
