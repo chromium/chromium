7cf36baf1d67e7880b18cba002b78fb55d91c525
Richard Nichols <rknichols@google.com>
2026-07-20 07:45:36 -0700

    Cast: Remove work around for bad json launch args.
    The system integrator has fixed their bad launch args/payloads, so this
    work around is no longer necessary.
    
    Bug: 359329800
    Change-Id: I5b949d0a6429fd8f62872162cf1a2bbecc8a8e17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8061903
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Reviewed-by: Simeon Anfinrud <sanfin@chromium.org>
    Commit-Queue: Richard Nichols <rknichols@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664680}

--- chromecast/cast_core/cast_starboard_runtime_main.cc
+++ chromecast/cast_core/cast_starboard_runtime_main.cc
@@ -62,25 +62,8 @@ class JSONArgsParser {
 
     // JSON must be the following format. All keys and values are strings.
     // {"parameters":{"argv":["arg1", ...]}}
-    std::string argv1 = std::string(argv[1]);
     std::optional<base::DictValue> root =
-        base::JSONReader::ReadDict(argv1, base::JSON_PARSE_CHROMIUM_EXTENSIONS);
-    if (!root) {
-      // Try to fix unquoted JSON
-      base::ReplaceSubstringsAfterOffset(&argv1, 0, "{", "{\"");
-      base::ReplaceSubstringsAfterOffset(&argv1, 0, "[", "[\"");
-      base::ReplaceSubstringsAfterOffset(&argv1, 0, "]", "\"]");
-      base::ReplaceSubstringsAfterOffset(&argv1, 0, ":", "\":\"");
-      base::ReplaceSubstringsAfterOffset(&argv1, 0, ",", "\",\"");
-      base::ReplaceSubstringsAfterOffset(&argv1, 0, ":\"[", ":[");
-      base::ReplaceSubstringsAfterOffset(&argv1, 0, ":\"{", ":{");
-
-      // Special case to handle unix:/tmp. This means that things like
-      // "valid_key_unix":"/valid_value" will fail to parse. Known issue.
-      base::ReplaceSubstringsAfterOffset(&argv1, 0, "unix\":\"/", "unix:/");
-      root = base::JSONReader::ReadDict(argv1,
-                                        base::JSON_PARSE_CHROMIUM_EXTENSIONS);
-    }
+        base::JSONReader::ReadDict(argv[1], base::JSON_PARSE_CHROMIUM_EXTENSIONS);
 
     if (!root) {
       return false;
