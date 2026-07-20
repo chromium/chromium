d75034e0fe3a24adf1f19b47212e19f0eec9dc14
Eric Ekey <ericekey@google.com>
2026-07-20 11:11:43 -0700

    [M151] [iOS][AppBar] Fix Crash Opening Overflow Menu in Incognito
    Original change's description:
    > [iOS][AppBar] Fix Crash Opening Overflow Menu in Incognito
    >
    > This change fixes a crash that occurs when opening the overflow menu in
    > incognito mode. This crash occurs because the ImageFetcherServiceFactory
    > does not produce an instance for off-the-record profiles. The crash is
    > triggered by trying to get the ImageFetcher by dereferencing the null
    > ImageFetcherService. This change fixes the crash by ensuring that the
    > services needed for IsOverflowMenuHomeCustomizationEntrypointEnabled()
    > are only initialized for non-incognito profiles.
    >
    > Fixed: 535891943
    > Change-Id: I671624229f28b6e2e12c5e7d70dac761fb169d6b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8116990
    > Commit-Queue: Eric Ekey <ericekey@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1663850}
    
    (cherry picked from commit 85a81cc1f318e1bf66605457b5a7733635c38b41)
    
    Bug: 536013885,535891943
    Change-Id: I671624229f28b6e2e12c5e7d70dac761fb169d6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8125116
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Eric Ekey <ericekey@google.com>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Cr-Commit-Position: refs/branch-heads/7922@{#1629}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- ios/chrome/browser/popup_menu/coordinator/popup_menu_coordinator.mm
+++ ios/chrome/browser/popup_menu/coordinator/popup_menu_coordinator.mm
@@ -314,10 +314,6 @@ using base::UserMetricsAction;
       HandlerForProtocol(dispatcher, QuickDeleteCommands);
   mediator.whatsNewHandler = HandlerForProtocol(dispatcher, WhatsNewCommands);
   mediator.levelUpHandler = HandlerForProtocol(dispatcher, LevelUpCommands);
-  if (IsOverflowMenuHomeCustomizationEntrypointEnabled()) {
-    mediator.NTPCommandHandler =
-        HandlerForProtocol(dispatcher, NewTabPageCommands);
-  }
   mediator.webStateList = browser->GetWebStateList();
   mediator.navigationAgent = WebNavigationBrowserAgent::FromBrowser(browser);
   mediator.baseViewController = self.baseViewController;
@@ -329,7 +325,9 @@ using base::UserMetricsAction;
   mediator.browserPolicyConnector =
       GetApplicationContext()->GetBrowserPolicyConnector();
   mediator.syncService = SyncServiceFactory::GetForProfile(profile);
-  if (IsOverflowMenuHomeCustomizationEntrypointEnabled()) {
+  if (IsOverflowMenuHomeCustomizationEntrypointEnabled() && !incognito) {
+    mediator.NTPCommandHandler =
+        HandlerForProtocol(dispatcher, NewTabPageCommands);
     mediator.backgroundCustomizationService =
         HomeBackgroundCustomizationServiceFactory::GetForProfile(profile);
     mediator.userUploadedImageManager =
