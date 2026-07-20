242df623555ac13872e7096ab777303f694de435
Becky Hu <hbecky@google.com>
2026-07-20 15:42:15 -0700

    [BAM] Clean up unused IDCs and IDSs
    The following IDCs are not used and can be removed from the codebase:
    IDC_CONTENT_CONTEXT_SHARING_SHARED_CLIPBOARD_SINGLE_DEVICE
    IDC_CONTENT_CONTEXT_SHARING_SHARED_CLIPBOARD_MULTIPLE_DEVICES
    
    IDS_CONTENT_CONTEXT_SHARING_SHARED_CLIPBOARD_MULTIPLE_DEVICES
    IDS_CONTENT_CONTEXT_SHARING_SHARED_CLIPBOARD_SINGLE_DEVICE
    
    Fixed: 534334943, 40831186
    Change-Id: Ie687427c1b8e84e15fbec575def835c0905b6627
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8101025
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Becky Hu <hbecky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1664988}

--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -352,10 +352,6 @@
   E(kActionBookmarkBarRemoveFromBookmarksBar, \
     IDC_BOOKMARK_BAR_REMOVE_FROM_BOOKMARKS_BAR) \
   /* Context menu items for Sharing */ \
-  E(kActionContentContextSharingSharedClipboardSingleDevice, \
-    IDC_CONTENT_CONTEXT_SHARING_SHARED_CLIPBOARD_SINGLE_DEVICE) \
-  E(kActionContentContextSharingSharedClipboardMultipleDevices, \
-    IDC_CONTENT_CONTEXT_SHARING_SHARED_CLIPBOARD_MULTIPLE_DEVICES) \
   E(kActionContentContextGenerateQrCode, IDC_CONTENT_CONTEXT_GENERATE_QR_CODE) \
   E(kActionContentContextSharingSubmenu, IDC_CONTENT_CONTEXT_SHARING_SUBMENU) \
   /* Context menu item to show the clipboard history menu */ \
