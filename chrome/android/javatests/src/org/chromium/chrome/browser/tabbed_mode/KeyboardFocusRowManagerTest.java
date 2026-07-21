c9bf00f6db52109e395d0f62177000ef0c919b52
Michael Wu <mklw@google.com>
2026-07-20 17:49:19 -0700

    [Gardener] Disable testSwitchKeyboardFocusRow_withBookmarkBarFocus
    Failing on freeform desktop bots.
    
    Bug: 537025880
    Change-Id: Iaec4faa438e7f3de512efc74aaea7a063ce48c22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127839
    Reviewed-by: Neil Coronado <nemco@google.com>
    Owners-Override: Michael Wu <mklw@google.com>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665082}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/KeyboardFocusRowManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tabbed_mode/KeyboardFocusRowManagerTest.java
@@ -30,6 +30,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -289,6 +290,8 @@ public class KeyboardFocusRowManagerTest {
     @SmallTest
     @Feature("KeyboardShortcuts")
     @Restriction(DeviceFormFactor.TABLET_OR_DESKTOP)
+    // TODO(crbug.com/537025880): Re-enable when fixed
+    @DisableIf.Device(DeviceFormFactor.DESKTOP_FREEFORM)
     public void testSwitchKeyboardFocusRow_withBookmarkBarFocus() {
         setUserPrefsShowBookmarksBar(true);
 
