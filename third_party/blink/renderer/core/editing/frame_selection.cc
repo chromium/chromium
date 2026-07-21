fd88062dd72a54264624cb8af9616dc002d54021
Koji Ishii <kojii@chromium.org>
2026-07-21 01:49:28 -0700

    [M151] Add validity checks after `CompositionRange()`
    Original change's description:
    > Add validity checks after `CompositionRange()`
    >
    > Following up crrev.com/c/7840643, this patch adds validity
    > checks after calls to `CompositionRange()`, which invokes
    > `Document::UpdateStyleAndLayout()`.
    >
    > Also change `FrameSelection::RootEditableElementOrDocumentElement()`
    > to return `nullptr` when the selection is no longer available
    > after the layout it forces, instead of dereferencing a cleared
    > `document_`.
    >
    > Bug: 523292588
    > Change-Id: I4a82a3b57fb00137dd45be946fd8e045ed46be2f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8106916
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Auto-Submit: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1663857}
    
    (cherry picked from commit 9fb841bf28c373fccf20a546545ed581e45031d7)
    
    Bug: 536237077,523292588
    Change-Id: I4a82a3b57fb00137dd45be946fd8e045ed46be2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8126664
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7922@{#1693}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- third_party/blink/renderer/core/editing/frame_selection.cc
+++ third_party/blink/renderer/core/editing/frame_selection.cc
@@ -152,9 +152,15 @@ const SelectionInDomTree& FrameSelection::GetSelectionInDomTree() const {
 }
 
 Element* FrameSelection::RootEditableElementOrDocumentElement() const {
+  if (!IsAvailable()) {
+    return nullptr;
+  }
   // TODO(editing-dev): The use of UpdateStyleAndLayout
   // needs to be audited.  See http://crbug.com/590369 for more details.
   GetDocument().UpdateStyleAndLayout(DocumentUpdateReason::kSelection);
+  if (!IsAvailable()) {
+    return nullptr;
+  }
 
   Element* selection_root =
       ComputeVisibleSelectionInDomTree().RootEditableElement();
