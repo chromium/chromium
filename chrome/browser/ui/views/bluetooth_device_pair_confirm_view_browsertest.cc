ea8435473e35fa920b5e842e4a02634a25c1c4dc
Alvin Ji <alvinji@chromium.org>
2026-07-21 14:41:49 -0700

    [M150] bluetooth: Protect BluetoothDevicePairConfirmView against keyjacking
    Original change's description:
    > bluetooth: Protect BluetoothDevicePairConfirmView against keyjacking
    >
    > Overrides ShouldAllowKeyEventsDuringInputProtection to false in
    > BluetoothDevicePairConfirmView. This enforces the
    > InputEventActivationProtector 500ms safety window for all initial
    > keyboard events to prevent accidental or social-engineered instant
    > dialog acceptance.
    >
    > BUG=517736936
    >
    > Change-Id: I65184125da21a835c664c0f7a437a630887dadb6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8108664
    > Reviewed-by: Darryl James <dljames@chromium.org>
    > Commit-Queue: Alvin Ji <alvinji@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1664924}
    
    (cherry picked from commit f132e7c0896d4353e2c2c1adcb019e6eb43a92c1)
    
    Bug: 537167274
    Change-Id: I65184125da21a835c664c0f7a437a630887dadb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8126722
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7871@{#3853}
    Cr-Branched-From: f542126b8c1b3e80104b26bb05ec830bd1206f29-refs/heads/main@{#1639810}

--- chrome/browser/ui/views/bluetooth_device_pair_confirm_view_browsertest.cc
+++ chrome/browser/ui/views/bluetooth_device_pair_confirm_view_browsertest.cc
@@ -2,17 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/views/bluetooth_device_pair_confirm_view.h"
+
 #include <string>
 
 #include "base/functional/callback_helpers.h"
-#include "base/strings/utf_string_conversions.h"
+#include "base/test/test_future.h"
 #include "chrome/browser/ui/bluetooth/bluetooth_dialogs.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
+#include "components/constrained_window/constrained_window_views.h"
 #include "content/public/browser/bluetooth_delegate.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
+#include "ui/events/base_event_utils.h"
+#include "ui/views/test/button_test_api.h"
 
 #if PAIR_BLUETOOTH_ON_DEMAND()
 
@@ -49,6 +54,53 @@ class BluetoothDevicePairConfirmViewBrowserTest
 IN_PROC_BROWSER_TEST_P(BluetoothDevicePairConfirmViewBrowserTest,
                        InvokeUi_default) {
   ShowAndVerifyUi();
+}
+
+IN_PROC_BROWSER_TEST_P(BluetoothDevicePairConfirmViewBrowserTest,
+                       KeyjackingProtectionSafetyWindow) {
+  base::test::TestFuture<content::BluetoothDelegate::PairPromptResult> future;
+
+  auto passkey =
+      DisplayPasskey() ? std::optional<std::u16string>(kPasskey) : std::nullopt;
+
+  // 1. Instantiate the view and show it using the constrained_window API
+  // directly. ShowWebModalDialogViews returns the Widget* pointer directly!
+  auto* view = new BluetoothDevicePairConfirmView(
+      kDeviceIdentifier, passkey,
+      future
+          .GetCallback<const content::BluetoothDelegate::PairPromptResult&>());
+  views::Widget* dialog_widget = constrained_window::ShowWebModalDialogViews(
+      view, browser()->tab_strip_model()->GetActiveWebContents());
+  ASSERT_NE(dialog_widget, nullptr);
+
+  views::MdTextButton* ok_button = view->GetOkButton();
+  ASSERT_NE(ok_button, nullptr);
+
+  // 2. PRESS ENTER IMMEDIATELY (within the 500ms safety window)
+  ui::KeyEvent press_enter_soon(ui::EventType::kKeyPressed, ui::VKEY_RETURN,
+                                ui::EF_NONE, ui::EventTimeForNow());
+  views::test::ButtonTestApi(ok_button).NotifyClick(press_enter_soon);
+
+  // VERIFY: The dialog should NOT have closed (input was ignored)
+  EXPECT_FALSE(dialog_widget->IsClosed());
+  EXPECT_FALSE(future.IsReady());
+
+  // 3. PRESS ENTER AFTER the safety window (Offset by 600ms)
+  ui::KeyEvent press_enter_later(
+      ui::EventType::kKeyPressed, ui::VKEY_RETURN, ui::EF_NONE,
+      ui::EventTimeForNow() + base::Milliseconds(600));
+  views::test::ButtonTestApi(ok_button).NotifyClick(press_enter_later);
+
+  // VERIFY: The dialog SHOULD be closed now
+  EXPECT_TRUE(dialog_widget->IsClosed());
+  ASSERT_TRUE(future.IsReady());
+  EXPECT_EQ(future.Get().result_code,
+            content::BluetoothDelegate::PairPromptStatus::kSuccess);
+
+  // If the test fails and the dialog is somehow not closed, clean it up
+  if (!dialog_widget->IsClosed()) {
+    dialog_widget->CloseNow();
+  }
 }
 
 INSTANTIATE_TEST_SUITE_P(All,
