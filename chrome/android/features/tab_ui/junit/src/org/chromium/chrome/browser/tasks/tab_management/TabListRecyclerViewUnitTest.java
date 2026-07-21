8f41d177f01a99bc0630c1eee628545e41387eca
Hao Dong <spdonghao@google.com>
2026-07-21 14:19:48 -0700

    [Vertical Tabs] Exclude pinned tab counts for scrollbar computation.
    Demo: https://screencast.googleplex.com/cast/NjQ3OTg1MDMwMTgxNjgzMnwzOWVkN2RhMS1lZA
    
    Bug: 537021883
    Change-Id: I7f62444ef9e3bee0e3c2e95b3b7c80ede979e60c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8129399
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Commit-Queue: Hao Dong <spdonghao@google.com>
    Commit-Queue: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665792}

---
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListRecyclerViewUnitTest.java
@@ -0,0 +1,167 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.browser.tasks.tab_management.vertical_tabs.VerticalTabRailLayout;
+
+/** Unit tests for {@link TabListRecyclerView}. */
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class TabListRecyclerViewUnitTest {
+    @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
+    private static final int DEFAULT_SCROLL_OFFSET = 12345;
+    private static final int DEFAULT_SCROLL_RANGE = 54321;
+    private static final int TOTAL_ITEM_COUNT = 10;
+
+    @Mock private LinearLayoutManager mLayoutManager;
+    @Mock private Adapter<?> mAdapter;
+    @Mock private View mFirstView;
+    @Mock private View mLastView;
+
+    private Context mContext;
+    private TabListRecyclerView mRecyclerView;
+
+    @Before
+    public void setUp() {
+        mContext = ApplicationProvider.getApplicationContext();
+        VerticalTabRailLayout parentRailLayout = new VerticalTabRailLayout(mContext, null);
+        mRecyclerView = spy(new TabListRecyclerView(mContext, null));
+        mRecyclerView.setId(R.id.tab_list_recycler_view);
+        parentRailLayout.addView(mRecyclerView);
+
+        when(mLayoutManager.canScrollVertically()).thenReturn(true);
+        when(mLayoutManager.computeVerticalScrollOffset(any())).thenReturn(DEFAULT_SCROLL_OFFSET);
+        when(mLayoutManager.computeVerticalScrollRange(any())).thenReturn(DEFAULT_SCROLL_RANGE);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        when(mAdapter.getItemCount()).thenReturn(TOTAL_ITEM_COUNT);
+        when(mAdapter.getItemViewType(0)).thenReturn(TabProperties.UiType.PINNED_TAB);
+        when(mAdapter.getItemViewType(1)).thenReturn(TabProperties.UiType.PINNED_TAB);
+        for (int i = 2; i < TOTAL_ITEM_COUNT; i++) {
+            when(mAdapter.getItemViewType(i)).thenReturn(TabProperties.UiType.TAB);
+        }
+        doReturn(mAdapter).when(mRecyclerView).getAdapter();
+    }
+
+    @Test
+    public void testComputeVerticalScroll_NotVerticalTabList_DelegatesToSuper() {
+        FrameLayout standardParent = new FrameLayout(mContext);
+        TabListRecyclerView standardRecyclerView = spy(new TabListRecyclerView(mContext, null));
+        standardParent.addView(standardRecyclerView);
+
+        standardRecyclerView.setLayoutManager(mLayoutManager);
+        doReturn(mAdapter).when(standardRecyclerView).getAdapter();
+
+        assertEquals(DEFAULT_SCROLL_OFFSET, standardRecyclerView.computeVerticalScrollOffset());
+        assertEquals(DEFAULT_SCROLL_RANGE, standardRecyclerView.computeVerticalScrollRange());
+        verify(mLayoutManager).computeVerticalScrollOffset(any());
+        verify(mLayoutManager).computeVerticalScrollRange(any());
+    }
+
+    @Test
+    public void testComputeVerticalScroll_NoPinnedTabs_DelegatesToSuper() {
+        for (int i = 0; i < TOTAL_ITEM_COUNT; i++) {
+            when(mAdapter.getItemViewType(i)).thenReturn(TabProperties.UiType.TAB);
+        }
+
+        assertEquals(DEFAULT_SCROLL_OFFSET, mRecyclerView.computeVerticalScrollOffset());
+        assertEquals(DEFAULT_SCROLL_RANGE, mRecyclerView.computeVerticalScrollRange());
+        verify(mLayoutManager).computeVerticalScrollOffset(any());
+        verify(mLayoutManager).computeVerticalScrollRange(any());
+    }
+
+    @Test
+    public void testComputeVerticalScrollOffset_WithPinnedPlaceholders() {
+        when(mLayoutManager.findFirstVisibleItemPosition()).thenReturn(2);
+        when(mLayoutManager.findLastVisibleItemPosition()).thenReturn(4);
+        when(mLayoutManager.findViewByPosition(2)).thenReturn(mFirstView);
+        when(mLayoutManager.findViewByPosition(4)).thenReturn(mLastView);
+
+        when(mFirstView.getTop()).thenReturn(0);
+        when(mFirstView.getBottom()).thenReturn(100);
+        when(mLastView.getTop()).thenReturn(200);
+        when(mLastView.getBottom()).thenReturn(300);
+
+        assertEquals(0, mRecyclerView.computeVerticalScrollOffset());
+        verify(mLayoutManager, never()).computeVerticalScrollOffset(any());
+    }
+
+    @Test
+    public void testComputeVerticalScrollOffset_ScrolledDown() {
+        // Scroll down 2 regular tabs
+        when(mLayoutManager.findFirstVisibleItemPosition()).thenReturn(4);
+        when(mLayoutManager.findLastVisibleItemPosition()).thenReturn(7);
+        when(mLayoutManager.findViewByPosition(4)).thenReturn(mFirstView);
+        when(mLayoutManager.findViewByPosition(7)).thenReturn(mLastView);
+
+        when(mFirstView.getTop()).thenReturn(0);
+        when(mFirstView.getBottom()).thenReturn(100);
+        when(mLastView.getTop()).thenReturn(300);
+        when(mLastView.getBottom()).thenReturn(400);
+
+        assertEquals(200, mRecyclerView.computeVerticalScrollOffset());
+        verify(mLayoutManager, never()).computeVerticalScrollOffset(any());
+    }
+
+    @Test
+    public void testComputeVerticalScrollRange_FitsCompletely_HidesScrollbar() {
+        // "Ignore" some items to fit the screen
+        when(mAdapter.getItemCount()).thenReturn(4);
+        when(mLayoutManager.findFirstVisibleItemPosition()).thenReturn(2);
+        when(mLayoutManager.findLastVisibleItemPosition()).thenReturn(3);
+        when(mLayoutManager.findViewByPosition(2)).thenReturn(mFirstView);
+        when(mLayoutManager.findViewByPosition(3)).thenReturn(mLastView);
+
+        doReturn(500).when(mRecyclerView).getHeight();
+        when(mFirstView.getTop()).thenReturn(0);
+        when(mLastView.getBottom()).thenReturn(200);
+
+        assertEquals(
+                mRecyclerView.computeVerticalScrollExtent(),
+                mRecyclerView.computeVerticalScrollRange());
+        verify(mLayoutManager, never()).computeVerticalScrollRange(any());
+    }
+
+    @Test
+    public void testComputeVerticalScrollRange_ContentOverflows_ShowsScrollbar() {
+        when(mLayoutManager.findFirstVisibleItemPosition()).thenReturn(2);
+        when(mLayoutManager.findLastVisibleItemPosition()).thenReturn(5);
+        when(mLayoutManager.findViewByPosition(2)).thenReturn(mFirstView);
+        when(mLayoutManager.findViewByPosition(5)).thenReturn(mLastView);
+
+        doReturn(300).when(mRecyclerView).getHeight();
+        when(mFirstView.getTop()).thenReturn(0);
+        when(mLastView.getBottom()).thenReturn(400);
+
+        assertEquals(800, mRecyclerView.computeVerticalScrollRange());
+        verify(mLayoutManager, never()).computeVerticalScrollRange(any());
+    }
+}
