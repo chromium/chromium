a3ca3e3d6376919495e84bf3330a0500dfb6dcbe
Quentin Pubert <qpubert@google.com>
2026-07-21 00:58:19 -0700

    Revert "[iOS]Enable CacheIdentityListInChrome by default"
    This reverts commit 83c2dc535bd0294e3245c516cf5530ffb275d2a0.
    
    Reason for revert: This change breaks some tests on the smoke builder
    
    Failure Link:
    https://ci.chromium.org/ui/p/chrome/builders/ci/smoke/189160
    
    Original change's description:
    > [iOS]Enable CacheIdentityListInChrome by default
    >
    > The feature has been approved for full-launch from the guardrail council
    > in
    > https://docs.google.com/document/d/1ugocTanMm8TPBER2jBk4AGBKo84b_RipfrO3J7nS3jE/edit?tab=t.0#bookmark=id.2o2v9193l2h.
    > We thus need to enable-it by default.
    >
    > Bug: 443276240
    > Change-Id: I7f09d65c68d3758deadf9919c739eeb7b0830c25
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8117948
    > Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    > Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    > Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1664634}
    
    Bug: 443276240
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6ada0334b622d52902c9f4a4bd0ee1b14e9b5e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8124715
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1665256}

--- components/signin/public/base/signin_switches.cc
+++ components/signin/public/base/signin_switches.cc
@@ -105,7 +105,7 @@ const base::FeatureParam<std::string>
 #endif
 
 #if BUILDFLAG(IS_IOS)
-BASE_FEATURE(kCacheIdentityListInChrome, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kCacheIdentityListInChrome, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableACPrefetch, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
