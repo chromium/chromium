3ae31b51824a9f5fd97ca571622b14549f63eab4
Jan Keitel <jkeitel@google.com>
2026-07-21 03:37:55 -0700

    Disable DragToSeparateWindowDuringDragEnd on Mac
    Disable test DragToSeparateWindowDuringDragEnd under
    DetachToBrowserTabDragControllerTest on all Mac configurations due to
    flakiness.
    
    TAG=agy
    CONV=777b2a67-d97e-4fc0-ad46-c51f3b0c8238
    
    Bug: 40748225
    Change-Id: I13b2cefaf6fa83c587ab261f564c395d33618c17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128656
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1665352}

--- chrome/browser/ui/views/tabs/dragging/tab_drag_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/dragging/tab_drag_controller_interactive_uitest.cc
@@ -2093,8 +2093,7 @@ IN_PROC_BROWSER_TEST_P(DetachToBrowserTabDragControllerTest,
 }
 
 // Test is based on DragToSeparateWindow. https://crbug.com/40748225
-#if (BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)) || \
-    BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 #define MAYBE_DragToSeparateWindowDuringDragEnd \
   DISABLED_DragToSeparateWindowDuringDragEnd
 #else
