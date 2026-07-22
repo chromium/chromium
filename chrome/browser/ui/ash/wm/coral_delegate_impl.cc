38a58f5863affe6a1a64b99621dd3926ef7f63a9
Becky Hu <hbecky@google.com>
2026-07-22 14:18:18 -0700

    Update FindTabOnDeskAtIndex to use TabInterface for tab iteration.
    Modify the tab search in FindTabOnDeskAtIndex to iterate over the TabStripModel using TabInterface instead of accessing WebContents directly from the Browser.
    
    Fixed: 534342977
    Change-Id: Icaabe0a6319be00a8b1af7d16153950156d3a702
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131080
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Becky Hu <hbecky@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666618}

--- chrome/browser/ui/ash/wm/coral_delegate_impl.cc
+++ chrome/browser/ui/ash/wm/coral_delegate_impl.cc
@@ -171,11 +171,14 @@ ash::BrowserDelegate* FindTabOnDeskAtIndex(const GURL& url,
           return ash::BrowserController::kContinueIteration;
         }
 
-        for (size_t idx = 0; idx < browser.GetWebContentsCount(); idx++) {
-          if (browser.GetWebContentsAt(idx)->GetVisibleURL() == url) {
+        int idx = 0;
+        for (tabs::TabInterface* tab : *browser.GetBrowser().GetTabStripModel()) {
+          if (tab->GetContents()->GetVisibleURL() == url) {
             out_tab_index = idx;
             found_browser = &browser;
+            break;
           }
+          idx++;
         }
         return found_browser ? ash::BrowserController::kBreakIteration
                              : ash::BrowserController::kContinueIteration;
