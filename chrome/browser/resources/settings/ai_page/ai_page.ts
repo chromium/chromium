65ba094402416a3e921ab0f840e15c8e6771ba17
Reilly Grant <reillyg@chromium.org>
2026-07-22 15:40:15 -0700

    Include ai_page_official_test.ts in ChromeOS builds
    Follow-up on https://crrev.com/c/8129439 to make sure the test is
    available to run on ChromeOS.
    
    To do this we also need to fix the tests because when moving this
    setting to chrome://settings/ai the feedback button was removed because
    of a WebUI framework limitation. This patch splits the on-device AI
    setting to its own section so the feedback button can be added back.
    
    Bug: 535298023
    Change-Id: I3be4abf284fb7aaee5bcbd898bc41c7f939a3958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8135729
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666666}

--- chrome/browser/resources/settings/ai_page/ai_page.ts
+++ chrome/browser/resources/settings/ai_page/ai_page.ts
@@ -260,6 +260,11 @@ export class SettingsAiPageElement extends SettingsAiPageElementBase {
         loadTimeData.getString('onDeviceAiLearnMoreUrl'));
   }
 
+  private onOnDeviceAiSendFeedback_(e: Event) {
+    e.stopPropagation();
+    this.onDeviceAiBrowserProxy_.openFeedbackDialog();
+  }
+
   private onOnDeviceAiSettingsBooleanControlChange_(e: Event) {
     const enabled = (e.target as SettingsToggleButtonElement).checked;
     this.onDeviceAiBrowserProxy_.setOnDeviceAiEnabled(enabled);
