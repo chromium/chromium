08a07b81c8a19b9a1b4b57237eb5796776620880
Leah Ryu <woodchip@google.com>
2026-07-22 15:53:50 -0700

    [Notebooks] Add feature flag parameters for Notebooks API URL / HTTP query params.
    BUG=531809229
    
    Change-Id: I1715ba9802000ee1ec8a35d9841df38d6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8103884
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Leah Ryu <woodchip@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666672}

--- components/notebooks/public/features.cc
+++ components/notebooks/public/features.cc
@@ -8,5 +8,20 @@ namespace notebooks::features {
 
 BASE_FEATURE(kNotebooks, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE_PARAM(std::string, kNotebookHomeURL, &kNotebooks, "about:blank");
+BASE_FEATURE_PARAM(std::string,
+                   kNotebooksApiBaseURL,
+                   &kNotebooks,
+                   "notebooks_api_base_url",
+                   "");
+BASE_FEATURE_PARAM(std::string,
+                   kNotebookSourceURLSuffix,
+                   &kNotebooks,
+                   "notebook_source_url_suffix",
+                   "");
+BASE_FEATURE_PARAM(std::string,
+                   kProvenanceOriginProductId,
+                   &kNotebooks,
+                   "provenance_origin_product_id",
+                   "");
 
 }  // namespace notebooks::features
--- components/notebooks/public/features.h
+++ components/notebooks/public/features.h
@@ -15,6 +15,16 @@ namespace notebooks::features {
 BASE_DECLARE_FEATURE(kNotebooks);
 BASE_DECLARE_FEATURE_PARAM(std::string, kNotebookHomeURL);
 
+// The base URL for the Notebooks API endpoints.
+BASE_DECLARE_FEATURE_PARAM(std::string, kNotebooksApiBaseURL);
+
+// URL path suffix for notebook source requests. Format is
+// {kNotebooksApiBaseURL}/{notebook_id}/{kNotebookSourceURLSuffix}/{source_id}
+BASE_DECLARE_FEATURE_PARAM(std::string, kNotebookSourceURLSuffix);
+
+// Chrome Notebooks product identifier for backend logs.
+BASE_DECLARE_FEATURE_PARAM(std::string, kProvenanceOriginProductId);
+
 }  // namespace notebooks::features
 
 #endif  // COMPONENTS_NOTEBOOKS_PUBLIC_FEATURES_H_
