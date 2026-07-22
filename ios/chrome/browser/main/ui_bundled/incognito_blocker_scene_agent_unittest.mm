b888d121204ced78061cd9097ec50fb3a7c6c054
Sylvain Defresne <sdefresne@chromium.org>
2026-07-22 06:05:22 -0700

    [ios] Stop mocking SceneState in IncognitoBlockerSceneAgentTest
    The -scene property is readwrite so there is no need to use a mock
    to set it. Additionally, the SceneState no longer requires access
    to an UISceneSession, so there is no need to mock one either.
    
    Bug: 519105565
    Change-Id: Iefb982a1ecf78cf772b072dde9857f934f6365da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128021
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666212}

--- ios/chrome/browser/main/ui_bundled/incognito_blocker_scene_agent_unittest.mm
+++ ios/chrome/browser/main/ui_bundled/incognito_blocker_scene_agent_unittest.mm
@@ -16,15 +16,9 @@ class IncognitoBlockerSceneAgentTest : public PlatformTest {
  public:
   IncognitoBlockerSceneAgentTest()
       : scene_state_([[SceneState alloc] init]),
-        scene_state_mock_(OCMPartialMock(scene_state_)),
         agent_([[IncognitoBlockerSceneAgent alloc] init]) {
-    scene_session_mock_ = OCMClassMock([UISceneSession class]);
-    OCMStub([scene_session_mock_ persistentIdentifier])
-        .andReturn([[NSUUID UUID] UUIDString]);
     scene_mock_ = OCMClassMock([UIWindowScene class]);
-    OCMStub([scene_mock_ session]).andReturn(scene_session_mock_);
     scene_state_.scene = scene_mock_;
-    OCMStub([scene_state_mock_ scene]).andReturn(scene_mock_);
     agent_.sceneState = scene_state_;
   }
 
@@ -37,10 +31,6 @@ class IncognitoBlockerSceneAgentTest : public PlatformTest {
   SceneState* scene_state_;
   // Mock for scene_state_'s underlying UIWindowScene.
   id scene_mock_;
-  // Mock for scene_mock_'s underlying UISceneSession.
-  id scene_session_mock_;
-  // Partial mock for stubbing scene_state_'s methods
-  id scene_state_mock_;
   // The tested agent
   IncognitoBlockerSceneAgent* agent_;
 };
