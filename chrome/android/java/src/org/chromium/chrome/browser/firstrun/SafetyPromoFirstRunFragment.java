0d03c2ec6a3380fbd7c222a1de1fb79e9dcf3214
Yanting Yu <yyanting@google.com>
2026-07-22 13:22:14 -0700

    [SafetyFRE] Add landscape mode dual-pane safety screen UI for arm 4
    Demo: http://shortn/_Rktp06Q4Ln
    
    We use the Chrome logo as a placeholder for the product icon. We will
    update it in the follow-up CL.
    
    Bug: 534388538, 537828809
    Change-Id: I7e4df691458422219745ce5c121a4954f04c08cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8101224
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Yanting Yu <yyanting@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666573}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/SafetyPromoFirstRunFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/SafetyPromoFirstRunFragment.java
@@ -6,17 +6,20 @@ package org.chromium.chrome.browser.firstrun;
 
 import static org.chromium.build.NullUtil.assumeNonNull;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.FrameLayout;
 
 import androidx.fragment.app.Fragment;
 
 import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ui.signin.SigninUtils;
 
 /** A {@link Fragment} for the Safety Promo during the First Run Experience (FRE). */
 @NullMarked
@@ -26,19 +29,36 @@ public class SafetyPromoFirstRunFragment extends Fragment implements FirstRunFra
             LayoutInflater inflater,
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        SafetyPromoFirstRunView view =
-                (SafetyPromoFirstRunView)
-                        inflater.inflate(
-                                R.layout.safety_promo_fre_illustration_portrait_view,
-                                container,
-                                false);
+        FrameLayout rootView = new FrameLayout(getActivity());
+        updateView(inflater, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        ViewGroup rootView = (ViewGroup) getView();
+        if (rootView != null) {
+            rootView.removeAllViews();
+            updateView(getLayoutInflater(), rootView);
+        }
+    }
+
+    private void updateView(LayoutInflater inflater, ViewGroup container) {
+        boolean useLandscape = SigninUtils.shouldShowDualPanesHorizontalLayout(getActivity());
+        int layoutId =
+                useLandscape
+                        ? R.layout.safety_promo_fre_illustration_landscape_view
+                        : R.layout.safety_promo_fre_illustration_portrait_view;
+
+        View inflatedView = inflater.inflate(layoutId, container, false);
+        SafetyPromoFirstRunView view = (SafetyPromoFirstRunView) inflatedView;
+        container.addView(view);
 
         // These are just placeholders for the arm 4 UI skeleton. Actual illustration and animation
         // logic will be added in follow-up patches.
         var pageDelegate = assumeNonNull(getPageDelegate());
         view.getContinueButtonView().setOnClickListener(v -> pageDelegate.advanceToNextPage());
-
-        return view;
     }
 
     @Override
