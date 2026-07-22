8ee9e28bd35492c843b06facb2fecdd8a1620a5c
Alex Kallam <aakallam@chromium.org>
2026-07-22 12:14:35 -0700

    [Fuzzing] Fix CHECK_IS_TEST() failures for blink fuzzers
    crrev.com/c/8006149 changed how Memory Coordinator handled test
    environments and has caused all blink fuzzers to unnecessarily crash on
    startup since.
    
    Bug: 537390927
    Change-Id: I298834d540870825a787d888cfdaeaf76a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8132019
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Alex Kallam <aakallam@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666530}

--- third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.cc
+++ third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.cc
@@ -6,6 +6,7 @@
 
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
+#include "base/test/allow_check_is_test_for_testing.h"
 #include "base/test/test_timeouts.h"
 #include "content/public/test/blink_test_environment.h"
 #include "content/public/test/setup_field_trials.h"
@@ -19,6 +20,8 @@ BlinkFuzzerTestSupport::BlinkFuzzerTestSupport()
     : BlinkFuzzerTestSupport(0, nullptr) {}
 
 BlinkFuzzerTestSupport::BlinkFuzzerTestSupport(int argc, char** argv) {
+  base::test::AllowCheckIsTestForTesting();
+
   // Note: we don't tear anything down here after an iteration of the fuzzer
   // is complete, this is for efficiency. We rerun the fuzzer with the same
   // environment as the previous iteration.
