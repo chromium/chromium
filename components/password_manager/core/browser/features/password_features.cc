fca417029014456c790f91ef68daf3829f31798a
Anna Tsvirchkova <atsvirchkova@google.com>
2026-07-23 00:48:04 -0700

    Prepare Grouped affiliations feature for launch
    Bug: 485806845
    Change-Id: I6ba1eb61baea14c6531f81196ac2687e11c8f848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131625
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666883}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -163,7 +163,7 @@ BASE_FEATURE(kPasswordFormClientsideClassifier,
 );
 
 BASE_FEATURE(kPasswordFormGroupedAffiliations,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kPasswordManagerLogToTerminal, base::FEATURE_DISABLED_BY_DEFAULT);
 
