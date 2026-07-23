945c2c7ec65358e3ae2a57fa25a65f2ad6132808
Karol Sygiet <sygiet@google.com>
2026-07-23 05:20:11 -0700

    [AtMemory] Fix the "sad tab" icon that is used
    The outline version should be used for AtMemory instead of the filled
    one. There are currently no other references to this icon in Autofill
    apart from AtMemory, so this change should be a noop from other products
    perspective.
    
    New implementation light mode: screen/3JjSnydad4dktXr
    New implementation dark mode: screen/9xVR8Mw4DG6P9oc
    
    Fixed: 538011698
    Change-Id: I62afab149d5a05a9cccde9512b165463b9214c27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8137979
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1666993}

--- chrome/browser/ui/views/autofill/popup/popup_cell_utils.cc
+++ chrome/browser/ui/views/autofill/popup/popup_cell_utils.cc
@@ -531,10 +531,9 @@ std::optional<ui::ImageModel> GetIconImageModelFromIcon(Suggestion::Icon icon) {
                                                 : vector_icons::kErrorOldIcon,
                                             ui::kColorSysError, kIconSize);
     case Suggestion::Icon::kSadTab:
-      return ImageModelFromVectorIcon(::features::IsRoundedIconsEnabled()
-                                          ? kSadTabFilledIcon
-                                          : kSadTabOldIcon,
-                                      kIconSize);
+      return ImageModelFromVectorIcon(
+          ::features::IsRoundedIconsEnabled() ? kSadTabIcon : kSadTabOldIcon,
+          kIconSize);
     case Suggestion::Icon::kFlight:
       return ImageModelFromVectorIcon(::features::IsRoundedIconsEnabled()
                                           ? vector_icons::kFlightIcon
