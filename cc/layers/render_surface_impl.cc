4ec412db0fe61de9db05de87cf68d590637b9e48
Anna Sato <annasato@chromium.org>
2026-07-22 22:35:43 -0700

    Revert "cc: Ignore epsilon error on enclosing rect bounds."
    This reverts commit fa59d36dff722b5a8a0d6956d26113cb455a31da.
    
    Reason for revert: The CL likely causes test failures:
    - compositing/reflections/nested-reflection-animated.html
    - transforms/transformed-focused-text-input.html
    - virtual/disable-raster-inducing-scroll/compositing/reflections/nested-reflection-animated.html
    
    Failure Link: 
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac15-x64-rel-tests/20277/overview
    
    Original change's description:
    > cc: Ignore epsilon error on enclosing rect bounds.
    >
    > Render surface rotates and layer counter rotate end up with
    > ~1^-15 residual value in transforms which can result in a whole
    > pixel shift during rendering (and shows up as a clip).
    >
    > This patch fixes that by ignoring epsilon errors during bounds
    > calculation.
    >
    > R=khushalsagar@chromium.org
    >
    > Change-Id: Ia14cdf097c171b56c581fe0b36dfbc7191ca9123
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128499
    > Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    > Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1666786}
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: If9e029a7940526ec532e0a9466af00f6af3daccd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8133659
    Owners-Override: Anna Sato <annasato@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1666841}

--- cc/layers/render_surface_impl.cc
+++ cc/layers/render_surface_impl.cc
@@ -7,7 +7,6 @@
 #include <stddef.h>
 
 #include <algorithm>
-#include <limits>
 
 #include "base/check_op.h"
 #include "base/logging.h"
@@ -285,9 +284,8 @@ gfx::Rect RenderSurfaceImpl::CalculateClippedAccumulatedContentRect() {
     return gfx::Rect();
 
   gfx::Rect clipped_accumulated_rect_in_local_space =
-      MathUtil::ProjectEnclosingClippedRectIgnoringError(
-          target_to_surface, clipped_accumulated_rect_in_target_space,
-          std::numeric_limits<float>::epsilon());
+      MathUtil::ProjectEnclosingClippedRect(
+          target_to_surface, clipped_accumulated_rect_in_target_space);
   // Bringing clipped accumulated rect back to local space may result
   // in inflation due to axis-alignment.
   clipped_accumulated_rect_in_local_space.Intersect(accumulated_content_rect());
@@ -418,9 +416,8 @@ void RenderSurfaceImpl::AccumulateContentRectFromContributingRenderSurface(
   // The content rect of contributing surface is in its own space. Instead, we
   // will use contributing surface's DrawableContentRect which is in target
   // space (local space for this render surface) as required.
-  accumulated_content_rect_.Union(gfx::ToEnclosingRectIgnoringError(
-      contributing_surface->DrawableContentRect(),
-      std::numeric_limits<float>::epsilon()));
+  accumulated_content_rect_.Union(
+      gfx::ToEnclosedRect(contributing_surface->DrawableContentRect()));
 
   // Now if contributing surface is a *matching* view transition element,
   // meaning that we're doing a capture, then above we ensure that we can use
