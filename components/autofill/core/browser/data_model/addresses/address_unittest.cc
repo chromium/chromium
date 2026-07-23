dd6dbc24cb9ceae140a6a29063b704633419917a
Christoph Schwering <schwering@google.com>
2026-07-23 08:28:05 -0700

    [Autofill] Un-yoda expectations in address_unittest.cc
    The common practice in Autofill and general recommendation (go/noyoda)
    is to put the actual value first.
    
    TAG=agy
    CONV=3af65b4d-5fd3-4bae-b4b1-321575b7e711
    
    Bug: 40100455
    Change-Id: I56876b125b9c44a01e043acbaefedb8b25fa86fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139241
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1667127}

--- components/autofill/core/browser/data_model/addresses/address_unittest.cc
+++ components/autofill/core/browser/data_model/addresses/address_unittest.cc
@@ -194,8 +194,8 @@ TEST_F(AddressTest, IsCountry) {
     SCOPED_TRACE(valid_match);
     FieldTypeSet matching_types;
     address.GetMatchingTypes(ASCIIToUTF16(valid_match), "US", &matching_types);
-    ASSERT_EQ(1U, matching_types.size());
-    EXPECT_EQ(ADDRESS_HOME_COUNTRY, *matching_types.begin());
+    ASSERT_EQ(matching_types.size(), 1U);
+    EXPECT_EQ(*matching_types.begin(), ADDRESS_HOME_COUNTRY);
   }
 
   const char* const kInvalidMatches[] = {"United", "Garbage"};
@@ -203,7 +203,7 @@ TEST_F(AddressTest, IsCountry) {
     FieldTypeSet matching_types;
     address.GetMatchingTypes(ASCIIToUTF16(invalid_match), "US",
                              &matching_types);
-    EXPECT_EQ(0U, matching_types.size());
+    EXPECT_EQ(matching_types.size(), 0U);
   }
 
   // Make sure that garbage values don't match when the country code is empty.
@@ -211,7 +211,7 @@ TEST_F(AddressTest, IsCountry) {
   EXPECT_EQ(address.GetRawInfo(ADDRESS_HOME_COUNTRY), u"");
   FieldTypeSet matching_types;
   address.GetMatchingTypes(u"Garbage", "US", &matching_types);
-  EXPECT_EQ(0U, matching_types.size());
+  EXPECT_EQ(matching_types.size(), 0U);
 }
 
 // Verifies that Address::GetInfo() correctly combines address lines.
