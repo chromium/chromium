36b54b023fee277a653b0c1a8cd50404be8fa8db
Ben Wiser <bewise@chromium.org>
2026-07-24 14:50:39 -0700

    android: Always use the current surface id for SynchronousCompositor
    In order to use WebView in Jetpack Compose, you need to use an interop
    AndroidView Composable. This currently fails on a DCHECK for eviction
    when hiding and showing a new WebView.
    
    The root cause seems to be that Compose attaches it, but doesn't draw
    straight away like regular Android Views do.
    
    So `DelegatedFrameHostAndroid`'s speculative `local_surface_id_` and the
    actual `SynchronousCompositor` state end up getting out of sync.
    
    Chrome still falls back to `DelegatedFrameHost`.
    
    Bug: b:531633054
    Test: I have been using this change for my WebContent testing
    Change-Id: I5ef1cd4fc52bd1a571175b662290c24bdb083937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8106600
    Auto-Submit: Rupert Wiser <bewise@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668182}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -131,6 +131,9 @@ namespace content {
 
 namespace {
 
+BASE_FEATURE(kEvictSurfaceViaDelegatedFrameHost,
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 static const base::TimeDelta kClickCountInterval = base::Seconds(0.5);
 static const float kClickCountRadiusSquaredDIP = 25;
 static const base::TimeDelta kThrottleTimeout = base::Milliseconds(200);
@@ -1479,6 +1482,10 @@ viz::FrameSinkId RenderWidgetHostViewAndroid::GetRootFrameSinkId() {
 }
 
 viz::SurfaceId RenderWidgetHostViewAndroid::GetCurrentSurfaceId() const {
+  if (base::FeatureList::IsEnabled(kEvictSurfaceViaDelegatedFrameHost)) {
+    return delegated_frame_host_ ? delegated_frame_host_->SurfaceId()
+                                 : viz::SurfaceId();
+  }
   if (sync_compositor_)
     return sync_compositor_->GetSurfaceId();
   return delegated_frame_host_ ? delegated_frame_host_->SurfaceId()
