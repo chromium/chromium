47f2ebb49391559cf428d49a938c1bbd247c5517
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-07-23 19:30:17 -0700

    Revert "[ios] Replace legacy FullscreenController in GeminiBrowserAgentTest"
    This reverts commit 43be00becb948b1d11483ecce23070900fde0e09.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5681875617054720
    
    Sample build with failed test: https://ci.chromium.org/b/8675447324079809409
    Affected test(s):
    [://ios/chrome/test\:ios_chrome_unittests!gtest::GeminiBrowserAgentTest#TestFloatyKeepsFullscreenDisabledUntilDismissed](https://ci.chromium.org/ui/test/chromium/:%2F%2Fios%2Fchrome%2Ftest%5C:ios_chrome_unittests%21gtest::GeminiBrowserAgentTest%23TestFloatyKeepsFullscreenDisabledUntilDismissed?q=VHash%3A6886b68bc8d094f7)
    [://ios/chrome/test\:ios_chrome_unittests!gtest::GeminiBrowserAgentTest#TestFloatyKeepsFullscreenDisabledUntilDismissed](https://ci.chromium.org/ui/test/chromium/:%2F%2Fios%2Fchrome%2Ftest%5C:ios_chrome_unittests%21gtest::GeminiBrowserAgentTest%23TestFloatyKeepsFullscreenDisabledUntilDismissed?q=VHash%3Ab5da490144873d5a)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5681875617054720&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8131183&type=BUG
    
    Original change's description:
    > [ios] Replace legacy FullscreenController in GeminiBrowserAgentTest
    >
    > Update GeminiBrowserAgentTest to use FullscreenCoordinator and
    > FullscreenBrowserAgent when the FullscreenRefactoring feature is
    > enabled.
    >
    > - Add helper InitFullscreenCoordinatorIfNeeded to create
    >   FullscreenBrowserAgent and start FullscreenCoordinator when
    >   FullscreenRefactoring is enabled.
    > - Add helper IsFullscreenEnabled to check FullscreenBrowserAgent when
    >   refactoring is enabled or FullscreenController when disabled.
    > - Update TestFloatyKeepsFullscreenDisabledUntilDismissed to assert
    >   via IsFullscreenEnabled.
    >
    > Fixed: 535867411
    > Change-Id: I40bc53261c8ecca7736aeaea04bbc2dc7218a105
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8131183
    > Reviewed-by: Yasaman Sedaghat <yasamans@google.com>
    > Commit-Queue: Scott Yoder <scottyoder@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1667425}
    >
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I45d30c2132f132311cebc06780744da0ce890cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8137713
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Owners-Override: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1667601}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent_unittest.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent_unittest.mm
@@ -22,7 +22,6 @@
 #import "components/signin/public/identity_manager/primary_account_change_event.h"
 #import "ios/chrome/browser/favicon/model/favicon_service_factory.h"
 #import "ios/chrome/browser/feature_engagement/model/tracker_factory.h"
-#import "ios/chrome/browser/fullscreen/coordinator/fullscreen_coordinator.h"
 #import "ios/chrome/browser/fullscreen/model/fullscreen_browser_agent.h"
 #import "ios/chrome/browser/fullscreen/ui_bundled/fullscreen_controller.h"
 #import "ios/chrome/browser/intelligence/bwg/metrics/gemini_metrics.h"
@@ -108,7 +107,7 @@ class GeminiBrowserAgentTest : public PlatformTest {
                    PageContextExtractorJavaScriptFeature::GetInstance()});
     SceneState* scene_state = [[SceneState alloc] init];
     browser_ = std::make_unique<TestBrowser>(profile_, scene_state);
-    InitFullscreenCoordinatorIfNeeded();
+    FullscreenBrowserAgent::CreateForBrowser(browser_.get());
     GeminiBrowserAgent::CreateForBrowser(browser_.get());
     gemini_browser_agent_ = GeminiBrowserAgent::FromBrowser(browser_.get());
 
@@ -123,6 +122,28 @@ class GeminiBrowserAgentTest : public PlatformTest {
     [browser_->GetCommandDispatcher()
         startDispatchingToTarget:mock_gemini_handler_
                      forProtocol:@protocol(GeminiCommands)];
+
+    // TODO(crbug.com/535867411): Replace legacy FullscreenController in
+    // GeminiBrowserAgentTest.
+    mock_fullscreen_handler_ = OCMProtocolMock(@protocol(FullscreenCommands));
+    OCMStub([mock_fullscreen_handler_ disableFullscreenAnimated:YES])
+        .andDo(^(NSInvocation*) {
+          FullscreenController::FromBrowser(browser_.get())
+              ->IncrementDisabledCounter();
+        });
+    OCMStub([mock_fullscreen_handler_ disableFullscreenAnimated:NO])
+        .andDo(^(NSInvocation*) {
+          FullscreenController::FromBrowser(browser_.get())
+              ->IncrementDisabledCounter();
+        });
+    OCMStub([mock_fullscreen_handler_ reenableFullscreen])
+        .andDo(^(NSInvocation*) {
+          FullscreenController::FromBrowser(browser_.get())
+              ->DecrementDisabledCounter();
+        });
+    [browser_->GetCommandDispatcher()
+        startDispatchingToTarget:mock_fullscreen_handler_
+                     forProtocol:@protocol(FullscreenCommands)];
 
     std::unique_ptr<web::FakeWebState> web_state =
         std::make_unique<web::FakeWebState>();
@@ -175,8 +196,6 @@ class GeminiBrowserAgentTest : public PlatformTest {
   }
 
   void TearDown() override {
-    [fullscreen_coordinator_ stop];
-    fullscreen_coordinator_ = nil;
     fake_main_frame_ = nullptr;
     web_state_ = nullptr;
     profile_ = nullptr;
@@ -185,6 +204,7 @@ class GeminiBrowserAgentTest : public PlatformTest {
     optimization_guide_service_ = nullptr;
     mock_settings_handler_ = nullptr;
     mock_gemini_handler_ = nullptr;
+    mock_fullscreen_handler_ = nullptr;
     fake_snapshot_delegate_ = nullptr;
     browser_.reset();
     profile_manager_.PrepareForDestruction();
@@ -300,35 +320,9 @@ class GeminiBrowserAgentTest : public PlatformTest {
   raw_ptr<web::FakeWebFrame> fake_main_frame_;
   id mock_settings_handler_;
   id mock_gemini_handler_;
+  id mock_fullscreen_handler_;
   FakeSnapshotGeneratorDelegate* fake_snapshot_delegate_;
   raw_ptr<feature_engagement::test::MockTracker> mock_tracker_;
-  FullscreenCoordinator* fullscreen_coordinator_;
-
-  // Instantiates FullscreenBrowserAgent and starts FullscreenCoordinator if
-  // FullscreenRefactoring feature is enabled and coordinator is not yet
-  // created.
-  void InitFullscreenCoordinatorIfNeeded() {
-    if (IsFullscreenRefactoringEnabled() && !fullscreen_coordinator_) {
-      FullscreenBrowserAgent::CreateForBrowser(browser_.get());
-      fullscreen_coordinator_ = [[FullscreenCoordinator alloc]
-          initWithBaseViewController:nil
-                             browser:browser_.get()];
-      [fullscreen_coordinator_ start];
-    }
-  }
-
-  // Returns whether fullscreen is enabled using FullscreenBrowserAgent if the
-  // refactoring is enabled, or FullscreenController otherwise.
-  bool IsFullscreenEnabled() {
-    if (IsFullscreenRefactoringEnabled()) {
-      FullscreenBrowserAgent* agent =
-          FullscreenBrowserAgent::FromBrowser(browser_.get());
-      return agent && agent->IsEnabled();
-    }
-    FullscreenController* controller =
-        FullscreenController::FromBrowser(browser_.get());
-    return controller && controller->IsEnabled();
-  }
 };
 
 // A test observer for GeminiBrowserAgent.
@@ -1125,34 +1119,35 @@ TEST_F(GeminiBrowserAgentTest,
   scoped_feature_list.InitWithFeatures(
       {kChromeNextIa, kAppBarHideInFullscreen, kComposeboxIpad}, {});
 
-  InitFullscreenCoordinatorIfNeeded();
-
-  EXPECT_TRUE(IsFullscreenEnabled());
+  FullscreenController* controller =
+      FullscreenController::FromBrowser(browser_.get());
+  ASSERT_NE(controller, nullptr);
+  EXPECT_TRUE(controller->IsEnabled());
 
   InvokeFloaty([[GeminiConfiguration alloc] init]);
-  EXPECT_FALSE(IsFullscreenEnabled());
+  EXPECT_FALSE(controller->IsEnabled());
 
   // Fullscreen should remain disabled even when UI appears or state collapses.
   gemini_browser_agent_->OnGeminiUIDidAppear();
-  EXPECT_FALSE(IsFullscreenEnabled());
+  EXPECT_FALSE(controller->IsEnabled());
 
   gemini_browser_agent_->OnViewStateChanged(
       ios::provider::GeminiViewState::kCollapsed);
-  EXPECT_FALSE(IsFullscreenEnabled());
+  EXPECT_FALSE(controller->IsEnabled());
 
   // Fullscreen should be re-enabled once floaty is dismissed.
   gemini_browser_agent_->DismissFloaty();
-  EXPECT_TRUE(IsFullscreenEnabled());
+  EXPECT_TRUE(controller->IsEnabled());
 
   // Fullscreen should be disabled once the state transitions to expanded again.
   gemini_browser_agent_->OnViewStateChanged(
       ios::provider::GeminiViewState::kExpanded);
-  EXPECT_FALSE(IsFullscreenEnabled());
+  EXPECT_FALSE(controller->IsEnabled());
 
   // Fullscreen should be re-enabled once the state transitions to hidden.
   gemini_browser_agent_->OnViewStateChanged(
       ios::provider::GeminiViewState::kHidden);
-  EXPECT_TRUE(IsFullscreenEnabled());
+  EXPECT_TRUE(controller->IsEnabled());
 }
 
 // Tests that when the floaty is un-minimized (expanded), we check if the
