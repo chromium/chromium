5f60a80b692ad666076e0855efe2b31666240008
Tomasz Wiszkowski <ender@google.com>
2026-07-24 13:28:39 -0700

    Allow tabbing outside of LocationBar outside active input.
    When Autocomplete is enabled and active, tabbing allows the user to
    explore suggestions and get back to the location bar.
    
    When Autocomplete is not enabled (standby or disabled), tabbing will
    no longer confine the user to the LocationBar and suggestions.
    
    Change-Id: Ibf60b60f5f03d08b6f01d5063dcff59017831ab7
    Fixed: 532032970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8141201
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668135}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarSelectionController.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarSelectionController.java
@@ -41,7 +41,7 @@ public class LocationBarSelectionController extends SelectionController {
      * @param selectableViews The list of views that this controller addresses/
      */
     public LocationBarSelectionController(List<SelectableView> selectableViews) {
-        super(Mode.WRAPPING);
+        super(Mode.SATURATING);
         mSelectableViews = selectableViews;
         reset();
     }
@@ -99,5 +99,10 @@ public class LocationBarSelectionController extends SelectionController {
         }
 
         return mVisibleViewsHolder;
+    }
+
+    /* package */ @Mode
+    int getSelectionModeForTesting() {
+        return mMode;
     }
 }
