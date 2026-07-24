2fb6926a96ccb65d4347f45dce20b16a33533f7e
Henrique Nakashima <hnakashima@chromium.org>
2026-07-24 16:07:58 -0700

    [Android] Batch SiteSettingsTest with Batch.PER_CLASS
    Bug: 344672098,535536257
    Change-Id: I8bf2ecea39b3eaa2d68241ff552d0c76abc58f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8125338
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668209}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/SiteSettingsTest.java
@@ -45,13 +45,13 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.ApplicationTestUtils;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
-import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -103,7 +103,7 @@ import java.util.concurrent.TimeoutException;
 
 /** Tests for global category switches, embargoed settings, and permissions in Site Settings. */
 @RunWith(ChromeJUnit4ClassRunner.class)
-@DoNotBatch(reason = "To be batched in follow-up CL")
+@Batch(Batch.PER_CLASS)
 @CommandLineFlags.Add({
     ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE,
     ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1",
