dd058e0bf9e5b4ce30fa2c4fd89a8e51c0153d3b
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2026-07-27 03:54:13 -0700

    IDB: SQLite - Conditionally limit large values in getAll() response
    POSIX (minus macOS) currently does not support >128 file descriptors in
    one mojo message, triggering a CHECK - see crbug.com/439305148. Since
    SQLite-backed IDB does not wrap large values, each value may end up
    using shared memory, which requires file descriptors on POSIX. This CL
    limit the number of such values sent in each response chunk of a
    getAll() request.
    
    Bug: 523912081
    Change-Id: I5797da537f57cd6de8e65fa8e33929a3f7a93ce2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8083924
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1668478}

--- content/browser/indexed_db/instance/database.cc
+++ content/browser/indexed_db/instance/database.cc
@@ -27,6 +27,7 @@
 #include "base/trace_event/trace_event.h"
 #include "base/types/expected_macros.h"
 #include "base/unguessable_token.h"
+#include "build/build_config.h"
 #include "components/services/storage/indexed_db/locks/partitioned_lock_id.h"
 #include "components/services/storage/indexed_db/locks/partitioned_lock_manager.h"
 #include "components/services/storage/privileged/mojom/indexed_db_client_state_checker.mojom.h"
@@ -46,6 +47,8 @@
 #include "content/browser/indexed_db/instance/transaction.h"
 #include "content/browser/indexed_db/status.h"
 #include "ipc/constants.mojom.h"
+#include "mojo/buildflags.h"
+#include "mojo/public/cpp/base/big_buffer.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 #include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "mojo/public/cpp/bindings/pending_associated_remote.h"
@@ -738,13 +741,38 @@ Status Database::GetAllOperation(
       "Value wrapping threshold is higher than BigBuffer inline size; "
       "BigBuffer may use shared memory with LevelDB backing store");
 
-  const size_t max_values_before_sending = blink::mojom::kIDBGetAllChunkSize;
+  const size_t kMaxValuesBeforeSending = blink::mojom::kIDBGetAllChunkSize;
+
+  // Mojo does not support sending >128 file descriptors in a single message on
+  // platforms where channel_posix.cc is used - see crbug.com/439305148. Since
+  // SQLite does not wrap large values in blobs, the `mojo_base::BigBuffer`
+  // holding the value may be backed by shared memory - which uses file
+  // descriptors on POSIX - and hence need limiting.
+  std::optional<uint32_t> max_shared_memory_values_in_chunk;
+  uint32_t shared_memory_values_in_chunk = 0;
+
+#if BUILDFLAG(IS_POSIX) && !BUILDFLAG(MOJO_USE_APPLE_CHANNEL)
+  if (bucket_context_->IsUsingSqlite()) {
+    // See `mojo::core::ipcz_driver::SharedBuffer::Serialize()`:
+#if BUILDFLAG(IS_ANDROID)
+    // 1 file descriptor is used per shared memory handle.
+    max_shared_memory_values_in_chunk = 128;
+#else
+    // 2 file descriptors are used per shared memory handle.
+    max_shared_memory_values_in_chunk = 64;
+#endif
+  }
+#endif
 
   for (uint32_t i = 0; i < max_count; ++i) {
     // Periodically stream records if we have too many.
-    if (found_records.size() >= max_values_before_sending) {
+    if (found_records.size() >= kMaxValuesBeforeSending ||
+        (max_shared_memory_values_in_chunk.has_value() &&
+         shared_memory_values_in_chunk >=
+             max_shared_memory_values_in_chunk.value())) {
       result_sink->SendResults(std::move(found_records), /*done=*/false);
       found_records.clear();
+      shared_memory_values_in_chunk = 0;
     }
 
     // Cursor creation performs the first seek, returning a nullptr cursor when
@@ -793,6 +821,12 @@ Status Database::GetAllOperation(
           std::move(index_key));
     } else {
       NOTREACHED();
+    }
+
+    if (return_record->return_value &&
+        return_record->return_value->value->bits.storage_type() ==
+            mojo_base::BigBuffer::StorageType::kSharedMemory) {
+      ++shared_memory_values_in_chunk;
     }
 
     found_records.emplace_back(std::move(return_record));
