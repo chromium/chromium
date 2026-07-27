1d9847259dfcbbd3f9775d71eb2682a6a124b334
Joel Hockey <joelhockey@chromium.org>
2026-07-27 03:45:32 -0700

    Revert "Reland "geolocation: Implement visibility-aware pause and resume for position requests""
    This reverts commit 72a01d54771adbd05b799cad0a5dc2c70c6f2866.
    
    Reason for revert: bot failures
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/android-12l-x64-dbg-tests/43229/blamelist
    
    Original change's description:
    > Reland "geolocation: Implement visibility-aware pause and resume for position requests"
    >
    > This is a reland of commit e62f5b7b6ebb275abd1354914e4fd7ec82d2a2b7
    >
    > Original change's description:
    > > geolocation: Implement visibility-aware pause and resume for position requests
    > >
    > > This CL implements pause and resume support for Blink Geolocation API
    > > requests, ensuring mid-flight timeout timers correctly pause while a tab
    > > is hidden and resume with their exact remaining time upon unhiding.
    > >
    > > Requests initiated while hidden are left in a dormant state and armed
    > > upon foregrounding, preventing spurious background dispatches.
    > >
    > > Bug: 534027930
    > > Change-Id: I9c220d781b4bd68b745762cf3ed201e2960cfa55
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8103623
    > > Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    > > Commit-Queue: Alvin Ji <alvinji@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1667572}
    >
    > Bug: 534027930
    > Change-Id: I7923b1df8514d070bd766f30583dfb70004d85d8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139753
    > Commit-Queue: Alvin Ji <alvinji@chromium.org>
    > Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1668225}
    
    Bug: 534027930, 537519282
    Change-Id: Icc90154f84297b4e75206eb36c1d60a35977eae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8152419
    Owners-Override: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Owners-Override: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Commit-Queue: Friedrich Hauser <friedrichh@chromium.org>
    Reviewed-by: ben chin <lungc@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1668475}

--- third_party/blink/renderer/core/geolocation/geo_notifier.cc
+++ third_party/blink/renderer/core/geolocation/geo_notifier.cc
@@ -20,9 +20,7 @@ GeoNotifier::GeoNotifier(Geolocation* geolocation,
           geolocation->DomWindow()->GetTaskRunner(TaskType::kMiscPlatformAPI),
           this,
           &GeoNotifier::TimerFired)),
-      use_cached_position_(false),
-      initial_callback_run_(false),
-      remaining_timeout_(std::nullopt) {
+      use_cached_position_(false) {
   DCHECK(geolocation_);
 }
 
@@ -52,7 +50,6 @@ void GeoNotifier::SetUseCachedPosition() {
 }
 
 void GeoNotifier::RunSuccessCallback(Geoposition* position) {
-  initial_callback_run_ = true;
   LocalDOMWindow* win = geolocation_->DomWindow();
   UseCounter::Count(win, WebFeature::kGeolocationSucceeded);
 
@@ -64,22 +61,15 @@ void GeoNotifier::RunSuccessCallback(Geoposition* position) {
 }
 
 void GeoNotifier::RunErrorCallback(GeolocationPositionError* error) {
-  initial_callback_run_ = true;
   RunCallback(nullptr, error);
 }
 
 void GeoNotifier::StartTimer() {
-  base::TimeDelta timeout =
-      remaining_timeout_.value_or(base::Milliseconds(options_->timeout()));
-  remaining_timeout_ = std::nullopt;
-  timer_->StartOneShot(timeout, FROM_HERE);
+  timer_->StartOneShot(base::Milliseconds(options_->timeout()), FROM_HERE);
 }
 
 void GeoNotifier::StopTimer() {
-  if (timer_->IsActive()) {
-    remaining_timeout_ = timer_->RemainingTimeout();
-    timer_->Stop();
-  }
+  timer_->Stop();
 }
 
 bool GeoNotifier::IsTimerActive() const {
@@ -100,11 +90,6 @@ void GeoNotifier::Timer::StartOneShot(base::TimeDelta interval,
 void GeoNotifier::Timer::Stop() {
   DCHECK(notifier_->geolocation_->DoesOwnNotifier(notifier_));
   timer_.Stop();
-}
-
-base::TimeDelta GeoNotifier::Timer::RemainingTimeout() const {
-  DCHECK(notifier_->geolocation_->DoesOwnNotifier(notifier_));
-  return timer_.NextFireInterval();
 }
 
 void GeoNotifier::TimerFired(TimerBase*) {
--- third_party/blink/renderer/core/geolocation/geo_notifier.h
+++ third_party/blink/renderer/core/geolocation/geo_notifier.h
@@ -5,8 +5,6 @@
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_GEOLOCATION_GEO_NOTIFIER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_GEOLOCATION_GEO_NOTIFIER_H_
 
-#include <optional>
-
 #include "base/task/single_thread_task_runner.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_position_callback.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_position_error_callback.h"
@@ -30,7 +28,6 @@ class GeoNotifier : public GarbageCollectedMixin, public NameClient {
   void Trace(Visitor*) const override;
   const char* GetHumanReadableName() const override { return "GeoNotifier"; }
   const PositionOptions* Options() const { return options_.Get(); }
-  bool InitialCallbackRun() const { return initial_callback_run_; }
 
   // Sets the given error as the fatal error if there isn't one yet.
   // Starts the timer with an interval of 0.
@@ -67,7 +64,6 @@ class GeoNotifier : public GarbageCollectedMixin, public NameClient {
     void StartOneShot(base::TimeDelta interval, const base::Location& caller);
     void Stop();
     bool IsActive() const { return timer_.IsActive(); }
-    base::TimeDelta RemainingTimeout() const;
 
    private:
     HeapTaskRunnerTimer<GeoNotifier> timer_;
@@ -88,8 +84,6 @@ class GeoNotifier : public GarbageCollectedMixin, public NameClient {
   Member<Timer> timer_;
   Member<GeolocationPositionError> fatal_error_;
   bool use_cached_position_;
-  bool initial_callback_run_;
-  std::optional<base::TimeDelta> remaining_timeout_;
 };
 
 }  // namespace blink
