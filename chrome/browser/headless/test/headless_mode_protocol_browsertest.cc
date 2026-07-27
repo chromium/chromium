7691928cdf0382552e7bc7b7489d49360be801f7
Ireneusz Szulc <ireneuszszulc@google.com>
2026-07-27 06:19:17 -0700

    Disable flaky StartFullscreenSwitch tests
    Re-disable StartFullscreenSwitch and StartFullscreenSwitchScaled tests.
    These were re-enabled in CL 8115757 but are still failing/flaking.
    On Windows ASan, the window fails to go fullscreen,
    causing the test synchronization to timeout.
    
    Bug: 424797525, 520311348, 520432613, 423951863
    Change-Id: Id78813e1aecb6cd83e925175e939c6b36a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8152783
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Commit-Queue: Ireneusz Szulc <ireneuszszulc@google.com>
    Owners-Override: Ireneusz Szulc <ireneuszszulc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668577}

--- chrome/browser/headless/test/headless_mode_protocol_browsertest.cc
+++ chrome/browser/headless/test/headless_mode_protocol_browsertest.cc
@@ -412,9 +412,20 @@ HEADLESS_MODE_PROTOCOL_TEST(WindowSizeSwitchLargerThanScreen,
 
 HEADLESS_MODE_PROTOCOL_TEST(WindowScreenAvail, "shared/window-screen-avail.js")
 
-HEADLESS_MODE_PROTOCOL_TEST(StartFullscreenSwitch,
+// TODO(crbug.com/424797525): Fails Mac 13.
+// TODO(crbug.com/520311348): Fails win-asan.
+// TODO(crbug.com/520432613): Fails on Linux.
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
+#define MAYBE_StartFullscreenSwitch DISABLED_StartFullscreenSwitch
+#else
+#define MAYBE_StartFullscreenSwitch StartFullscreenSwitch
+#endif
+
+HEADLESS_MODE_PROTOCOL_TEST(MAYBE_StartFullscreenSwitch,
                             "sanity/start-fullscreen-switch.js")
-HEADLESS_MODE_PROTOCOL_TEST(StartFullscreenSwitchScaled,
+
+// TODO(crbug.com/423951863): Fails on Mac 13, Linux, and win-asan at least.
+HEADLESS_MODE_PROTOCOL_TEST(DISABLED_StartFullscreenSwitchScaled,
                             "sanity/start-fullscreen-switch-scaled.js")
 
 // TODO(crbug.com/430156442): This fails on macOS where fullscreen uses display
