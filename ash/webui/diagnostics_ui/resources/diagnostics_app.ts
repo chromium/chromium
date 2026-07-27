72d1e8aaa6ddeedd0a666639b9a49ecf91416c99
Michael Checo <michaelcheco@google.com>
2026-07-27 10:15:58 -0700

    Remove unused type imports in diagnostics_app.ts
    Bug: 538686155
    Change-Id: I9e9774851081a9028f2873c5b41dfc1bc13397d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8139405
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Auto-Submit: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1668764}

--- ash/webui/diagnostics_ui/resources/diagnostics_app.ts
+++ ash/webui/diagnostics_ui/resources/diagnostics_app.ts
@@ -26,8 +26,7 @@ import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bu
 import {getTemplate} from './diagnostics_app.html.js';
 import {DiagnosticsBrowserProxyImpl} from './diagnostics_browser_proxy.js';
 import {getDiagnosticsIcon, getNavigationIcon} from './diagnostics_utils.js';
-import type {KeyboardInfo} from './input.mojom-webui.js';
-import type {ConnectedDevices, InputDataProviderInterface, TouchDeviceInfo} from './input_data_provider.mojom-webui.js';
+import type {InputDataProviderInterface} from './input_data_provider.mojom-webui.js';
 import {ConnectedDevicesObserverReceiver} from './input_data_provider.mojom-webui.js';
 import {getInputDataProvider} from './mojo_interface_provider.js';
 
