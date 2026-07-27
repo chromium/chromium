3cf8f950a3790af63c9f0b6dbad4d99c389dbadf
Mihai Sardarescu <msarda@chromium.org>
2026-07-27 15:00:22 -0700

    Enable SyncFixWebSigninSessionDurationForShortLivedSessions by default.
    This change enables SyncFixWebSigninSessionDurationForShortLivedSessions
    feature by default and removes its corresponding entry from the
    fieldtrial testing config.
    
    Fixed: 510666442
    Change-Id: I9debd9e604a3d72460e3a469dc663618cbd29cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8155199
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669020}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -209,7 +209,7 @@ BASE_FEATURE(kEstimateNewSignInUsersWithFinchAvailablePopulation,
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 BASE_FEATURE(kSyncFixWebSigninSessionDurationForShortLivedSessions,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSyncSimplifyDeviceNaming, base::FEATURE_DISABLED_BY_DEFAULT);
 
