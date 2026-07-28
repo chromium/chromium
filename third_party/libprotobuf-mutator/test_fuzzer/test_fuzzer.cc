6d1dc651b1876a129bb6a614b648ca1b7649d4db
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2026-07-28 13:38:19 -0700

    lpm: Migrate lpm_test_fuzzer to new fuzzable protos
    The new fuzzable protos can be used in non-fuzzing builds and unblock
    the migration to FUZZ_TEST.
    
    Bug: 505034799
    Change-Id: I31aab66aeaaf9eb76393e0aa1d8cf45fcb78f208
    Include-Ci-Only-Tests: true
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel,linux-libfuzzer-high-end-asan-rel,chromeos-libfuzzer-asan-rel,mac-libfuzzer-asan-rel,win-libfuzzer-asan-rel,linux-centipede-asan-rel,win_chromium_compile_dbg_ng
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162842
    Reviewed-by: Victor Liu <liuvic@google.com>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669724}

--- third_party/libprotobuf-mutator/test_fuzzer/test_fuzzer.cc
+++ third_party/libprotobuf-mutator/test_fuzzer/test_fuzzer.cc
@@ -6,12 +6,24 @@
 // is working. Building this fuzzer without using fuzzable_proto_library will
 // fail because of test_fuzzer_input.proto
 
+#include <cstdlib>
 #include <iostream>
 
 #include "third_party/libprotobuf-mutator/src/src/libfuzzer/libfuzzer_macro.h"
 
 #include "third_party/libprotobuf-mutator/test_fuzzer/test_fuzzer_input.pb.h"
+#include "third_party/libprotobuf-mutator/test_fuzzer/test_fuzzer_input_fuzzable.pb.h"
 
-DEFINE_PROTO_FUZZER(const lpm_test_fuzzer::TestFuzzerInput& input) {
+DEFINE_PROTO_FUZZER(
+    const fuzzable::lpm_test_fuzzer::TestFuzzerInput& fuzzable_input) {
+  std::string serialized;
+  if (!fuzzable_input.SerializeToString(&serialized)) {
+    std::abort();
+  }
+  lpm_test_fuzzer::TestFuzzerInput input;
+  if (!input.ParseFromString(serialized)) {
+    std::abort();
+  }
+
   std::cout << input.imported().imported_publicly().input() << std::endl;
 }
