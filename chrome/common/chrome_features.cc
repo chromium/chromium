3e70c80a84b829159c656debbfa6b6f6f88266ae
Chris Thompson <cthomp@chromium.org>
2026-07-28 10:56:21 -0700

    Enable HttpsFirstModeDefaultSettingPairsWithEsb by default
    Bug: 517216858
    Change-Id: Iecfd37f5de177675633b7dbd29f4b2c50ecb797c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8159120
    Auto-Submit: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Reviewed-by: Zack Han <zackhan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669607}

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -1282,7 +1282,7 @@ BASE_FEATURE(kHttpsFirstModeForAdvancedProtectionUsers,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kHttpsFirstModeDefaultSettingPairsWithEsb,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Enables HTTPS-First Mode for engaged sites. No-op if HttpsFirstModeV2 or
 // HTTPS-Upgrades is disabled.
