38c49d2a96defae98e7efcf2a2d06c8b17e5c77d
Dave Tapuska <dtapuska@chromium.org>
2026-07-28 07:41:07 -0700

    [M144] Prevent use-after-free in WidgetBase::UpdateSurfaceAndScreen.
    Original change's description:
    > Prevent use-after-free in WidgetBase::UpdateSurfaceAndScreen.
    >
    > Add a weak pointer check after calling client_->OrientationChanged() because this call can cause the WidgetBase object to be destroyed.
    >
    > Bug: 523308824, 523711130
    > Change-Id: If70c28a4a616b4909dade238d8c39b001956fd05
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7930273
    > Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    > Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1646821}
    
    (cherry picked from commit 6b6931e5c44fc5fe912a04d4c67503a770b07e3d)
    
    Bug: 537922694,523308824,523711130
    Change-Id: If70c28a4a616b4909dade238d8c39b001956fd05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8155822
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7559@{#5171}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- third_party/blink/renderer/platform/widget/widget_base.cc
+++ third_party/blink/renderer/platform/widget/widget_base.cc
@@ -1052,7 +1052,11 @@ void WidgetBase::UpdateVisualState() {
       ShouldRecordBeginMainFrameMetrics()
           ? DocumentUpdateReason::kBeginMainFrame
           : DocumentUpdateReason::kTest;
+  auto weak_this = weak_ptr_factory_.GetWeakPtr();
   client_->UpdateLifecycle(WebLifecycleUpdate::kAll, lifecycle_reason);
+  if (!weak_this) {
+    return;
+  }
   client_->SetSuppressFrameRequestsWorkaroundFor704763Only(false);
 }
 
@@ -1751,8 +1755,13 @@ void WidgetBase::UpdateSurfaceAndScreenInfo(
         screen_infos_.current().display_color_spaces);
   }
 
-  if (orientation_changed)
+  if (orientation_changed) {
+    auto weak_this = weak_ptr_factory_.GetWeakPtr();
     client_->OrientationChanged();
+    if (!weak_this) {
+      return;
+    }
+  }
 
   client_->DidUpdateSurfaceAndScreen(previous_original_screen_infos);
 }
