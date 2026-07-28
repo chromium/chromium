1c0159a9ae4e2c06ef4d5190e782893f97590489
Colin Blundell <blundell@chromium.org>
2026-07-28 04:07:52 -0700

    [Blink] Streamline computation in WebGpuSharedImageWrapper
    No functionality change [1].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/client/client_shared_image.h;l=216
    
    Bug: 352263194
    Change-Id: I48ac7171dcc2241b44b75b415f7b8949402fc813
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8153547
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669351}

--- third_party/blink/renderer/platform/graphics/gpu/webgpu_shared_image_wrapper.cc
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_shared_image_wrapper.cc
@@ -396,11 +396,9 @@ void WebGpuSharedImageWrapper::OnMemoryDump(
       base::StringPrintf("%s/CanvasResource_0x%" PRIXPTR, path.c_str(),
                          reinterpret_cast<uintptr_t>(this));
   auto* dump = pmd->CreateAllocatorDump(dump_name);
-  size_t memory_size =
-      shared_image_->format().EstimatedSizeInBytes(shared_image_->size());
   dump->AddScalar(base::trace_event::MemoryAllocatorDump::kNameSize,
                   base::trace_event::MemoryAllocatorDump::kUnitsBytes,
-                  memory_size);
+                  GetSize());
 
   shared_image_->OnMemoryDump(
       pmd, dump->guid(),
