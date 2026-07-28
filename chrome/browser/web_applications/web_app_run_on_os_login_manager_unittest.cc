9fab649b04cfeb5405312d5f2144960ebffe38f7
Di Wu <diwux@google.com>
2026-07-28 02:37:50 -0700

    Ash: Drop an unused dependency from a ChromeOS-only web app test
    This CL is part of an ongoing Ash Refactoring series to remove
    dependencies on generic //chrome code from Ash/ChromeOS code that
    still lives under interim locations inside //chrome/browser.
    
    The unit test for a ChromeOS-only web app feature still included the
    browser's web app database machinery. That include is a leftover;
    nothing in the test refers to it anymore.
    
    Deleting the include removes this last tie between the ChromeOS-only
    feature and that piece of generic code, with nothing needed in its
    place.
    
    Bug: b:332804822
    Test: CQ
    Change-Id: I8f84af9874694fc519e90947f769dc98e966b17a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8137323
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669312}

--- chrome/browser/web_applications/web_app_run_on_os_login_manager_unittest.cc
+++ chrome/browser/web_applications/web_app_run_on_os_login_manager_unittest.cc
@@ -25,7 +25,6 @@
 #include "chrome/browser/web_applications/test/web_app_test_utils.h"
 #include "chrome/browser/web_applications/web_app_command_manager.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
-#include "chrome/browser/web_applications/web_app_database_factory.h"
 #include "chrome/browser/web_applications/web_app_management_type.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registry_update.h"
