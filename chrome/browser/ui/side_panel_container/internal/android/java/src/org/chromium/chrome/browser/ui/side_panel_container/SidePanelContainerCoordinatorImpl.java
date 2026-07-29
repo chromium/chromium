bcaa4f46ba62eec2d2c1baebc168d1b19526a6be
Linyu He <linyuh@google.com>
2026-07-29 08:15:50 -0700

    SidePanel: Return HeightType.NOT_APPLICABLE when showable width is 0.
    Not doing so will create an invalid SideUiSize that can lead to crashes.
    
    For example, if the side panel is currently closed, its showable width
    is 0, and an "updateUi()" flow is started by SideUiCoordinator, not
    returning HeightType.NOT_APPLICABLE will cause
      a SideUiSize(0, HeightType.TOOLBAR) or
      a SideUiSize(0, HeightType.WEB_CONTENTS).
    
    Then SideUiCoordinator _will_ apply the invalid SideUiSize to the UI
    because it's different from the current one:
      SideUiSize(0, HeightType.NOT_APPLICABLE).
    
    This will result in a crash:
    * when SideUiSize.width is 0, we should _detach_ the side panel
      container;
    * it doesn't make sense to set the top margin/width for a detached View.
    
    Cq-Include-Trybots: luci.chromium.try:android-desktop-x64-rel
    Include-Ci-Only-Tests: true
    Bug: 540141916
    Change-Id: I52b96be067842704d2ebfdfa1dfe5d91f9f844ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8165561
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670266}

--- chrome/browser/ui/side_panel_container/internal/android/java/src/org/chromium/chrome/browser/ui/side_panel_container/SidePanelContainerCoordinatorImpl.java
+++ chrome/browser/ui/side_panel_container/internal/android/java/src/org/chromium/chrome/browser/ui/side_panel_container/SidePanelContainerCoordinatorImpl.java
@@ -327,9 +327,9 @@ final class SidePanelContainerCoordinatorImpl
                         availableWidthDp, windowWidthDp, minSidePanelContainerWidthDp);
         @HeightType
         int heightType =
-                VerticalTabUtils.isVerticalTabsEnabled(mParentActivity)
-                        ? HeightType.WEB_CONTENTS
-                        : HeightType.TOOLBAR;
+                determineHeightType(
+                        showableWidthDp, VerticalTabUtils.isVerticalTabsEnabled(mParentActivity));
+
         return new SideUiSize(ViewUtils.dpToPx(mParentActivity, showableWidthDp), heightType);
     }
 
@@ -472,6 +472,15 @@ final class SidePanelContainerCoordinatorImpl
 
         // 4. Return 0 if available space can't accommodate the minimum side panel width.
         return 0;
+    }
+
+    @VisibleForTesting
+    static @HeightType int determineHeightType(int showableWidthDp, boolean isVerticalTabsEnabled) {
+        @HeightType int heightType = HeightType.NOT_APPLICABLE;
+        if (showableWidthDp != 0) {
+            heightType = isVerticalTabsEnabled ? HeightType.WEB_CONTENTS : HeightType.TOOLBAR;
+        }
+        return heightType;
     }
 
     private @Nullable ThinWebView findThinWebView(View view) {
