1ae997cc63e3bf61289f1e0f3da0ebf1bdaacd66
Christoph Schwering <schwering@google.com>
2026-07-28 18:39:47 -0700

    [M144-LTS][Autofill] Fix bad std::string_view
    (cherry picked from commit 26b5dccbc0bf09dcaae19af16e4f9b4af63a6e45)
    
    Bug: 515463295
    Change-Id: Ic8a58565e885b18495a06aed68b79aa95b6c77cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7866055
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1635015}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8123994
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Owners-Override: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Gyuyoung Kim (xWF) <qkim@google.com>
    Cr-Commit-Position: refs/branch-heads/7559@{#5176}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- components/autofill/core/browser/payments/payments_request_details.h
+++ components/autofill/core/browser/payments/payments_request_details.h
@@ -510,13 +510,13 @@ struct GetBnplPaymentInstrumentForFetchingUrlRequestDetails {
   // The instrument ID is used by the server to identify a specific BNPL issuer.
   std::string_view instrument_id;
   // The fingerprint data for the user and the device.
-  std::string_view risk_data;
+  std::string risk_data;
   // The merchant domain (including the scheme).
   GURL merchant_domain;
   // The total purchase amount (in micros) from the merchant checkout page.
   int64_t total_amount = 0;
   // Currency of the amount represented by a three-letter currency code.
-  std::string_view currency;
+  std::string currency;
 };
 
 // Information retrieved from a BNPL FetchUrlRequest.
