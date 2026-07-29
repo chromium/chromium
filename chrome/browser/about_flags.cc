5ecc4cb182396da4730d02bcf359f8cde76d8224
Evan Luo <evanluo@google.com>
2026-07-29 10:42:30 -0700

    [finds] Remove zero model execution cooldown flag from chrome://flags
    Bug: 539988040
    Change-Id: I95c006b20f41bc19a88979d513109a822b8195f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161669
    Auto-Submit: Evan Luo <evanluo@google.com>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670392}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -829,15 +829,6 @@ const FeatureEntry::Choice kSafetyHubUnifiedPasswordsModuleChoices[] = {
      "SafetyHubLocalPasswordsModule, SafetyHubUnifiedPasswordsModule"},
 };
 
-const FeatureEntry::FeatureParam kChromeFindsTestConfiguration[] = {
-    {"always_show_opt_in_promo", "true"},
-    {"theme_cooldown_duration_in_days", "0"},
-    {"finds_notification_schedule_start_time_minutes", "0"},
-    {"finds_notification_schedule_window_time_minutes", "1"},
-    {"model_execution_cooldown_duration_in_days", "0"},
-    {"history_time_window_in_days", "7"},
-    {"finds_opt_in_promo_cooldown_in_days", "0"}};
-
 const FeatureEntry::FeatureParam kChromeFindsTestOptInConditions[] = {
     {"enable_srp_return_count_opt_in", "true"},
     {"enable_theme_url_visit_count_opt_in", "true"},
@@ -846,7 +837,6 @@ const FeatureEntry::FeatureParam kChromeFindsTestOptInConditions[] = {
     {"finds_opt_in_promo_max_interacted_count", "9999"}};
 
 const FeatureEntry::FeatureVariation kChromeFindsVariations[] = {
-    {"(notifications testing)", kChromeFindsTestConfiguration, nullptr},
     {"(opt-in testing)", kChromeFindsTestOptInConditions, nullptr}};
 
 const FeatureEntry::FeatureParam kPopularSitesRefreshUsParamArm1[] = {
