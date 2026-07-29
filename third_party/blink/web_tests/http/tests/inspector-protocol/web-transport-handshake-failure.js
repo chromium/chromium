1027a47c52ab4b6e90edea25112d373b57a01aa3
Lily Chen <chlily@chromium.org>
2026-07-29 10:11:19 -0700

    Fix WebTransport failure test to accommodate more errors
    This web test started failing when BoringSSL changed its default
    supported_groups recently, resulting in the default ClientHello
    message changing from 1 packet to 2 packets.
    
    The test attempts to start a WebTransport connection to
    https://localhost, which does not support WebTransport (which requires a
    QUIC connection), and asserts that it should fail. When receiving the
    ClientHello, the OS loopback interface should realize that port 443 is
    closed and set ECONNREFUSED. Previously, this caused the test connection
    to fail upon trying to *read* the response from localhost, resulting in
    net::ERR_CONNECTION_REFUSED. After the default supported_groups change,
    the connection started failing on sending the 2nd ClientHello QUIC
    packet, because sending the 2nd packet encounters ECONNREFUSED (having
    been set after the 1st packet) which results in a *write* failure.
    
    This changed the error string for the failed connection, which caused
    the test assertion to fail.
    
    This CL loosens the regex for acceptable error strings, allowing the
    test to pass for any net::Error failure.
    
    Fixed: 535947496
    Change-Id: I202161744528f32ddb736f612ed10a566a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8140080
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Reviewed-by: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670352}

--- third_party/blink/web_tests/http/tests/inspector-protocol/web-transport-handshake-failure.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/web-transport-handshake-failure.js
@@ -8,8 +8,8 @@
 
   dp.Log.onEntryAdded(event => {
     const entry = event.params.entry;
-    // Remove the error code, as it is platform-specific and can change.
-    const text = entry.text.replace(/net::ERR_[A-Z_]+/, '[net error]');
+    // Remove the error string, as it is platform-specific and can change.
+    const text = entry.text.replace(/net::ERR_[A-Z_]+.*$/, '[net error]');
     testRunner.log('Log.onEntryAdded');
     testRunner.log(`source: ${entry.source}`);
     testRunner.log(`level: ${entry.level}`);
