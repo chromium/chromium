9a36324fb2bdd7987788f3df12664839afb7c2cc
Darryl James <dljames@chromium.org>
2026-07-29 14:12:59 -0700

    Reland "[BAM] Support explicit ActionInvocationSource on ActionInvocationContext"
    This reverts commit 9100f6316092b26edb1e9972a8b6a0fa3069eada.
    
    Reason for revert: tabs::IsVerticalTabsFeatureEnabled returns false on
    ChromeOS preventing all vertical tabs actions from being registered. On
    other platforms it is enabled by default. To fix this, vertical tabs is
    explicitly enabled in the test for all platforms.
    
    Original change's description:
    > Revert "[BAM] Support explicit ActionInvocationSource on ActionInvocationContext"
    >
    > This reverts commit 53bf2bfcfafbe001b4c3f5281c22aaa77cfee5a1.
    >
    > Reason for revert:
    > ExecuteToggleVerticalTabsCollapsePreservesKeyboardShortcutSource
    > test added by the original CL is failing on the
    > linux-chromeos-chrome bot.
    >
    > Failure Link:
    > https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/59550/overview
    >
    > Original change's description:
    > > [BAM] Support explicit ActionInvocationSource on ActionInvocationContext
    > >
    > > Add ActionInvocationSource enum and kActionInvocationSourceKey to
    > > ActionInvocationContext. ActionViewInterface sets kViewButton on button
    > > clicks, and CommandActionUpdater sets kKeyboardShortcut on command
    > > execution. kActionToggleCollapseVertical evaluates
    > > kActionInvocationSourceKey to log distinct user metrics for vertical tab
    > > strip collapse.
    > >
    > > TAG=agy
    > > CONV=57aa08e7-e6de-4228-8302-9477a8fb9001
    > >
    > > Bug: 525118424
    > > Change-Id: Ieef04c79b9e100425f4c863feed179e59fca9fd6
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160322
    > > Commit-Queue: Darryl James <dljames@chromium.org>
    > > Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1669944}
    >
    > Bug: 525118424
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Change-Id: Ifb3a325df2347bfd6eae30e0f0c8ff8b44286909
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161404
    > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Hiroki Nakagawa <nhiroki@chromium.org>
    > Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1670006}
    
    Bug: 525118424
    Change-Id: I11b5e8570cac853c08207c061ae4e16b87a87c0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8157210
    Reviewed-by: Kunal Daftari <kunaldaftari@google.com>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670580}

--- chrome/browser/ui/tabs/vertical_tab_strip_state_controller.cc
+++ chrome/browser/ui/tabs/vertical_tab_strip_state_controller.cc
@@ -312,6 +312,7 @@ void VerticalTabStripStateController::OnModeChanged() {
   is_vertical_tabs_enabled_ = new_enabled;
 
   NotifyModeChanged();
+  UpdateCollapseActionItem();
 }
 
 void VerticalTabStripStateController::OnExpandOnHoverEnabledChanged() {
@@ -399,6 +400,7 @@ void VerticalTabStripStateController::UpdateCollapseActionItem() {
         chrome::GetCleanTitleAndTooltipText(l10n_util::GetStringUTF16(text)));
     collapse_action->SetTooltipText(
         chrome::GetCleanTitleAndTooltipText(l10n_util::GetStringUTF16(text)));
+    collapse_action->SetEnabled(ShouldDisplayVerticalTabs());
   }
 }
 
