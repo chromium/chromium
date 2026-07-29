6ae7eddfa2ecf2da8e5d0cab1efc40155c158fd6
Hitarth Kothari <hitarthkothari@google.com>
2026-07-29 13:20:11 -0700

    [M152] Disable browser autofill in Tab Bottom Sheet WebUI.
    Original change's description:
    > Disable browser autofill in Tab Bottom Sheet WebUI.
    >
    > Disables browser autofill for the Tab Bottom Sheet WebUI by setting
    > setEnableBrowserAutofill to false in the configuration builder. This
    > prevents autofill suggestions from appearing in the bottom sheet.
    >
    > Bug: 536878907
    > Change-Id: I03ab37049e5ca16ab060464975667df3223466d6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8163842
    > Reviewed-by: Fiaz Muhammad <mfiaz@google.com>
    > Commit-Queue: Hitarth Kothari <hitarthkothari@google.com>
    > Auto-Submit: Hitarth Kothari <hitarthkothari@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1669621}
    
    (cherry picked from commit 9975fb8c7a0c2589346f8bbd4f09388e75932cae)
    
    Bug: 540004973,536878907
    Change-Id: I03ab37049e5ca16ab060464975667df3223466d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161054
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7977@{#83}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetWebUi.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/TabBottomSheetWebUi.java
@@ -224,7 +224,7 @@ public class TabBottomSheetWebUi {
                             .setContextMenuPopulatorFactory(mContextMenuPopulatorFactory)
                             .setSelectionDropdownMenuDelegate(mSelectionDropdownMenuDelegate)
                             .setSupportTheming(true)
-                            .setEnableBrowserAutofill(true)
+                            .setEnableBrowserAutofill(false)
                             .build());
             if (mClientType == TabBottomSheetClientType.CONTEXTUAL_TASKS) {
                 // This disables ActionModeSelectionMenu from ever being shown on AIM.
