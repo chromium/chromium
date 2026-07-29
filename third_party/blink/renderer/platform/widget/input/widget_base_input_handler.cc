745127bab2e85bc500af7c3d2ec1e574f2858767
Vladimir Levin <vmpstr@chromium.org>
2026-07-28 19:40:19 -0700

    [M144-LTS] Extend weak self checks to more places in HandleInputEvent
    Various calls can delete the "this" object, so extend the checks
    to more spots.
    
    R=dtapuska@chromium.org
    
    (cherry picked from commit a621298c354b64bb95932e6d76c9133b5f69c988)
    
    Bug: 523238265
    Change-Id: Ifb5b22fbc09bdbcb3a33be65b935d6633c9d03fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7941475
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1647525}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8137145
    Commit-Queue: Gyuyoung Kim (xWF) <qkim@google.com>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Owners-Override: Achuith Bhandarkar <achuith@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7559@{#5181}
    Cr-Branched-From: 223dfbac1c7542a06b422390d954afe5b560b607-refs/heads/main@{#1552494}

--- third_party/blink/renderer/platform/widget/input/widget_base_input_handler.cc
+++ third_party/blink/renderer/platform/widget/input/widget_base_input_handler.cc
@@ -346,6 +346,20 @@ void WidgetBaseInputHandler::HandleInputEvent(
           std::move(done_callback));
 
   bool prevent_default = false;
+  WebInputEventResult processed = WebInputEventResult::kNotHandled;
+
+  auto check_was_destroyed = [&]() {
+    if (!weak_self) {
+      if (callback) {
+        std::move(callback).Run(GetAckResult(processed), swap_latency_info,
+                                std::move(handling_state.event_overscroll()),
+                                std::move(handling_state.touch_action()));
+      }
+      return true;
+    }
+    return false;
+  };
+
   bool show_virtual_keyboard_for_mouse = false;
   if (WebInputEvent::IsMouseEventType(input_event.GetType())) {
     const WebMouseEvent& mouse_event =
@@ -355,6 +369,9 @@ void WidgetBaseInputHandler::HandleInputEvent(
                  mouse_event.PositionInWidget().y());
 
     widget_->client()->WillHandleMouseEvent(mouse_event);
+    if (check_was_destroyed()) {
+      return;
+    }
 
     // Reset the last known cursor if mouse has left this widget. So next
     // time that the mouse enters we always set the cursor accordingly.
@@ -391,21 +408,28 @@ void WidgetBaseInputHandler::HandleInputEvent(
       // through to the web app would cause compatibility problems since
       // DPAD_CENTER is also used as a "confirm" button).
       prevent_default = true;
+      processed = WebInputEventResult::kHandledSuppressed;
     }
   }
 #endif
+  if (check_was_destroyed()) {
+    return;
+  }
 
   if (WebInputEvent::IsGestureEventType(input_event.GetType())) {
     const WebGestureEvent& gesture_event =
         static_cast<const WebGestureEvent&>(input_event);
     bool suppress = false;
     widget_->client()->WillHandleGestureEvent(gesture_event, &suppress);
-    prevent_default = prevent_default || suppress;
+    if (suppress) {
+      prevent_default = true;
+      processed = WebInputEventResult::kHandledSuppressed;
+    }
+    if (check_was_destroyed()) {
+      return;
+    }
   }
 
-  WebInputEventResult processed = prevent_default
-                                      ? WebInputEventResult::kHandledSuppressed
-                                      : WebInputEventResult::kNotHandled;
   if (input_event.GetType() != WebInputEvent::Type::kChar ||
       !suppress_next_char_events_) {
     suppress_next_char_events_ = false;
@@ -417,15 +441,7 @@ void WidgetBaseInputHandler::HandleInputEvent(
         processed = widget_->client()->HandleInputEvent(coalesced_event);
     }
 
-    // The associated WidgetBase (and this WidgetBaseInputHandler) could
-    // have been destroyed. If it was return early before accessing any more of
-    // this class.
-    if (!weak_self) {
-      if (callback) {
-        std::move(callback).Run(GetAckResult(processed), swap_latency_info,
-                                std::move(handling_state.event_overscroll()),
-                                std::move(handling_state.touch_action()));
-      }
+    if (check_was_destroyed()) {
       return;
     }
   }
@@ -454,6 +470,9 @@ void WidgetBaseInputHandler::HandleInputEvent(
     HandleInjectedScrollGestures(
         std::move(handling_state.injected_scroll_params()), input_event,
         coalesced_event.latency_info(), cloned_metrics.get());
+    if (check_was_destroyed()) {
+      return;
+    }
   }
 
   // Send gesture scroll events and their dispositions to the compositor thread,
@@ -479,6 +498,10 @@ void WidgetBaseInputHandler::HandleInputEvent(
     }
   }
 
+  if (check_was_destroyed()) {
+    return;
+  }
+
   if (callback) {
     std::move(callback).Run(GetAckResult(processed), swap_latency_info,
                             std::move(handling_state.event_overscroll()),
@@ -486,6 +509,10 @@ void WidgetBaseInputHandler::HandleInputEvent(
   } else {
     DCHECK(!handling_state.event_overscroll())
         << "Unexpected overscroll for un-acked event";
+  }
+
+  if (!weak_self) {
+    return;
   }
 
   // Show the virtual keyboard if enabled and a user gesture triggers a focus
