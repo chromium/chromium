74f6d096958fcb7ced638d8163bf694c3cc97d78
Adem Derinel <derinel@google.com>
2026-07-29 03:09:42 -0700

    [M151] Fix UAF in AuthenticatorRequestDialogController
    Original change's description:
    > Fix UAF in AuthenticatorRequestDialogController
    >
    > Calling SetCurrentStep in HideDialogAndDispatchToPlatformAuthenticator
    > can trigger observers that destroy the dialog controller. This CL
    > introduces a WeakPtr check after SetCurrentStep to ensure the controller
    > is still alive before accessing its member variables. A regression test
    > has also been added to verify this behavior.
    >
    > Fixed: 536584251
    > Change-Id: Idc80a34ec731967a6e4290f67c174cfaebfcddc3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8127931
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Commit-Queue: Adem Derinel <derinel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1666890}
    
    (cherry picked from commit 25e013dd59d3e6eb9cafb4d730f86c532fd2789b)
    
    Bug: 540247654,536584251
    Change-Id: Idc80a34ec731967a6e4290f67c174cfaebfcddc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8165114
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7922@{#2178}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- chrome/browser/webauthn/authenticator_request_dialog_controller.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_controller.cc
@@ -1000,7 +1000,12 @@ bool AuthenticatorRequestDialogController::StartGuidedFlowForHint(
 void AuthenticatorRequestDialogController::
     HideDialogAndDispatchToPlatformAuthenticator(
         std::optional<AuthenticatorType> type) {
+  base::WeakPtr<AuthenticatorRequestDialogController> weak_this =
+      weak_factory_.GetWeakPtr();
   SetCurrentStep(Step::kPlatformAuthenticator);
+  if (!weak_this) {
+    return;
+  }
 
   std::vector<AuthenticatorReference>& authenticators =
       ephemeral_state_.saved_authenticators_;
@@ -2341,7 +2346,12 @@ AuthenticatorRequestDialogController::GetRenderFrameHost() const {
 }
 
 void AuthenticatorRequestDialogController::StartPasskeyUpgradeRequest() {
+  base::WeakPtr<AuthenticatorRequestDialogController> weak_this =
+      weak_factory_.GetWeakPtr();
   SetCurrentStep(Step::kPasskeyUpgrade);
+  if (!weak_this) {
+    return;
+  }
 
   if (!enclave_request_callback_) {
     RecordPasskeyUpgradeResultHistogram(PasskeyUpgradeResult::kGpmDisabled);
