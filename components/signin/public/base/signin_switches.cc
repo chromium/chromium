1e41d4fa9f3fd5597541bd9d6d14c50c08d4257e
Menghan Yang <myuu@google.com>
2026-07-29 13:39:15 -0700

    [Signin][Mobile] Enable CrossDeviceSignin by default
    Enables the CrossDeviceSignin feature by default and add default URL
    values for the feature parameter.
    
    Also removes the corresponding field trial config.
    (See launch/4471116)
    
    Bug: 496454816, 493573514
    Change-Id: I1a76960ceff9a02dd9814994c5e223be9a749e5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8166302
    Auto-Submit: Menghan Yang <myuu@google.com>
    Reviewed-by: Ernest Nguyen Hung <ernn@google.com>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670551}

--- components/signin/public/base/signin_switches.cc
+++ components/signin/public/base/signin_switches.cc
@@ -274,9 +274,9 @@ BASE_FEATURE(kChromeOsUseConsentLevelSigninForNewUsers,
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
-BASE_FEATURE(kCrossDeviceSignin, base::FEATURE_DISABLED_BY_DEFAULT);
-const base::FeatureParam<std::string> kCrossDeviceSigninUrl{&kCrossDeviceSignin,
-                                                            "url", ""};
+BASE_FEATURE(kCrossDeviceSignin, base::FEATURE_ENABLED_BY_DEFAULT);
+const base::FeatureParam<std::string> kCrossDeviceSigninUrl{
+    &kCrossDeviceSignin, "url", "https://www.google.com/chrome/go-mobile"};
 #endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
