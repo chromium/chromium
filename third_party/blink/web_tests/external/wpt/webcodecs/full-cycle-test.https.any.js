35efd36d229d5b5688fdca09392f7f83d465a430
Dale Curtis <dalecurtis@chromium.org>
2026-07-28 17:25:46 -0700

    [WebCodecs] Enable HBD format checks for vp9.(2|3) encoding tests
    HBD formats are now standardized, so the test can check these.
    
    R=tguilbert
    
    Bug: 537818862
    Change-Id: Iee46055180ed013d95e6a9bbc510628d883a05ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8164168
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1669896}

--- third_party/blink/web_tests/external/wpt/webcodecs/full-cycle-test.https.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/full-cycle-test.https.any.js
@@ -41,8 +41,7 @@ promise_setup(async () => {
       codec: 'vp09.02.10.10',
       hasEmbeddedColorSpace: true,
       hardwareAcceleration: 'prefer-software',
-      // TODO(https://github.com/w3c/webcodecs/issues/384):
-      // outputPixelFormat should be 'I420P10'
+      outputPixelFormat: 'I420P10',
     },
     '?vp9_444_p1': {
       codec: 'vp09.01.10.08.03',
@@ -54,8 +53,7 @@ promise_setup(async () => {
       codec: 'vp09.03.10.10.03',
       hasEmbeddedColorSpace: true,
       hardwareAcceleration: 'prefer-software',
-      // TODO(https://github.com/w3c/webcodecs/issues/384):
-      // outputPixelFormat should be 'I444P10'
+      outputPixelFormat: 'I444P10',
     },
     '?h264_avc': {
       codec: 'avc1.42001E',
