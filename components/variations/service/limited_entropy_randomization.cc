20fad9bd82d3c538faeb2d4bcf063c65bf38e548
Caitlin Fischer <caitlinfischer@google.com>
2026-07-29 13:20:53 -0700

    Tidy up includes in limited_entropy_randomization.h and .cc.
    Change-Id: I5fdfd88b47ddb038a7fcfbee19c209c2b46bae25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8167301
    Auto-Submit: Caitlin Fischer <caitlinfischer@google.com>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Commit-Queue: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1670534}

--- components/variations/service/limited_entropy_randomization.cc
+++ components/variations/service/limited_entropy_randomization.cc
@@ -4,27 +4,23 @@
 
 #include "components/variations/service/limited_entropy_randomization.h"
 
-#include <math.h>
-
 #include <algorithm>
 #include <cstdint>
-#include <limits>
 
-#include "base/check_op.h"
 #include "base/debug/crash_logging.h"
 #include "base/debug/dump_without_crashing.h"
-#include "base/logging.h"
+#include "base/memory/raw_ptr.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/numerics/checked_math.h"
-#include "base/numerics/safe_conversions.h"
 #include "base/rand_util.h"
 #include "base/version_info/version_info.h"
 #include "build/build_config.h"
 #include "components/variations/client_filterable_state.h"
 #include "components/variations/limited_layer_entropy_cost_tracker.h"
+#include "components/variations/proto/layer.pb.h"
+#include "components/variations/proto/study.pb.h"
+#include "components/variations/proto/variations_seed.pb.h"
 #include "components/variations/study_filtering.h"
 #include "components/variations/variations_layers.h"
-#include "components/variations/variations_seed_processor.h"
 #include "third_party/abseil-cpp/absl/container/flat_hash_map.h"
 
 #define SR_CRASH_KEY "SeedRejection"
