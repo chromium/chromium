b7a921811f659c4ae6e265db77fd68902e20d2d4
Camillia Smith Barnes <cammie@chromium.org>
2026-07-29 15:47:17 -0700

    Shared Storage: Remove unneeded test files
    Unused shared storage test HTML, JavaScript, and image asset files
    are no longer referenced by active tests in chrome/test/data and
    content/test/data.
    
    This change deletes the obsolete test files and cleans up the
    corresponding entries in content_test_bundle_data.filelist.
    
    Bug: 462465887
    Change-Id: I8d9eed656c0ce1d412b81f66cf160073d7ba5f59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162935
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670653}

--- content/test/data/shared_storage/simple_module2.js
+++
@@ -1,5 +0,0 @@
-// Copyright 2021 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-console.log('Executing simple_module2.js')
