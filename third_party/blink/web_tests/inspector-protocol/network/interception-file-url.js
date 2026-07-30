43d049e53187af476226de22577a8df6e6841720
Andrey Kosyakov <caseq@chromium.org>
2026-07-30 10:46:21 -0700

    Port a network interception URL under filesystem tests to Fetch
    Change-Id: I34d511c3661d10285758a97a7980cde5502d9d68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8171187
    Reviewed-by: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671216}

--- third_party/blink/web_tests/inspector-protocol/network/interception-file-url.js
+++ third_party/blink/web_tests/inspector-protocol/network/interception-file-url.js
@@ -1,17 +1,13 @@
 (async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
-  var {page, session, dp} = await testRunner.startBlank(`Verify that interception works for file URLs`);
+  const {session, dp} = await testRunner.startBlank(
+      `Verify that interception works for file URLs`);
 
-  await dp.Network.enable();
-  await dp.Network.setRequestInterception({
-    patterns: [{ urlPattern: '*' }]
-  });
+  await dp.Fetch.enable();
   await dp.Runtime.enable();
   const navigationPromise = session.navigate('./resources/simple.html');
-  const mainResource = (await dp.Network.onceRequestIntercepted()).params;
+  const mainResource = (await dp.Fetch.onceRequestPaused()).params;
   testRunner.log(`Intercepted: ${testRunner.trimURL(mainResource.request.url)}`);
-  dp.Network.continueInterceptedRequest({
-    interceptionId: mainResource.interceptionId
-  });
+  dp.Fetch.continueRequest({requestId: mainResource.requestId});
   await navigationPromise;
   // If resource is interpreted as text/plain instead of text/html, the HTML will be escaped.
   testRunner.log(await session.evaluate(() => document.body.innerHTML));
@@ -20,11 +16,9 @@
     script.src = 'simple.js';
     document.body.appendChild(script);
   })()`);
-  const subresource = (await dp.Network.onceRequestIntercepted()).params;
+  const subresource = (await dp.Fetch.onceRequestPaused()).params;
   testRunner.log(`Intercepted: ${testRunner.trimURL(subresource.request.url)}`);
-  dp.Network.continueInterceptedRequest({
-    interceptionId: subresource.interceptionId
-  });
+  dp.Fetch.continueRequest({requestId: subresource.requestId});
 
   testRunner.completeTest();
 })
