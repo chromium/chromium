87c36e961d15fd94862fc74348fdc439f9ad09a4
Matt Reynolds <mattreynolds@google.com>
2026-07-30 13:54:00 -0700

    gamepad: Wait for data fetchers to be cleared before thread join
    The GamepadProvider destructor posts a task to clear `data_fetchers_`
    on the polling thread, and then calls Stop() to wait for pending
    tasks posted to the polling thread to complete and join the thread.
    
    Stop() will wait for the clear task to complete, but if clearing
    data fetchers causes other tasks to be posted then those tasks may be
    queued after the stop task is queued and may not run.
    
    This CL introduces a WaitableEvent to ensure `data_fetchers_` is
    cleared and any followup tasks are queued before calling Stop().
    
    Bug: 519739390
    Change-Id: I810f1674b7eb28ff454813d0994038f11c900dfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8169896
    Reviewed-by: Alvin Ji <alvinji@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671391}

--- device/gamepad/gamepad_provider.cc
+++ device/gamepad/gamepad_provider.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 #include "device/gamepad/gamepad_provider.h"
 
 #include <stddef.h>
@@ -23,6 +22,7 @@
 #include "base/location.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/synchronization/waitable_event.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread.h"
@@ -110,9 +110,18 @@ GamepadProvider::~GamepadProvider() {
   // some of them require their destructor to be called on the same sequence as
   // their other methods.
   simulated_gamepad_data_fetcher_ = nullptr;
+
+  base::WaitableEvent cleared(base::WaitableEvent::ResetPolicy::MANUAL,
+                              base::WaitableEvent::InitialState::NOT_SIGNALED);
   polling_thread_->task_runner()->PostTask(
-      FROM_HERE, base::BindOnce(&GamepadFetcherVector::clear,
-                                base::Unretained(&data_fetchers_)));
+      FROM_HERE,
+      base::BindOnce(
+          [](GamepadFetcherVector* fetchers, base::WaitableEvent* event) {
+            fetchers->clear();
+            event->Signal();
+          },
+          base::Unretained(&data_fetchers_), base::Unretained(&cleared)));
+  cleared.Wait();
 
   // Use Stop() to join the polling thread, as there may be pending callbacks
   // which dereference |polling_thread_|.
