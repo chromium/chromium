8b3c59daea285175a4f1a5f174b388f5ac263149
Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
2026-07-30 12:46:16 -0700

    Reland "Reland "IDB: SQLite - Conditionally limit large values in getAll() response""
    This is a reland of commit b2ddead35aac45fc0bfd90621607f9227f896833
    
    The new test was too slow on MSAN, hence disabled it there.
    
    Original change's description:
    > Reland "IDB: SQLite - Conditionally limit large values in getAll() response"
    >
    > This is a reland of commit dd058e0bf9e5b4ce30fa2c4fd89a8e51c0153d3b
    >
    > It turns out that Fuchsia also has a limit on the number of handles that
    > can be sent in one mojo message; this reland respects the limit on
    > Fuchsia too.
    >
    > Original change's description:
    > > IDB: SQLite - Conditionally limit large values in getAll() response
    > >
    > > POSIX (minus macOS) currently does not support >128 file descriptors in
    > > one mojo message, triggering a CHECK - see crbug.com/439305148. Since
    > > SQLite-backed IDB does not wrap large values, each value may end up
    > > using shared memory, which requires file descriptors on POSIX. This CL
    > > limit the number of such values sent in each response chunk of a
    > > getAll() request.
    > >
    > > Bug: 523912081
    > > Change-Id: I5797da537f57cd6de8e65fa8e33929a3f7a93ce2
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8083924
    > > Reviewed-by: Steve Becker <stevebe@microsoft.com>
    > > Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    > > Cr-Commit-Position: refs/heads/main@{#1668478}
    >
    > Bug: 523912081
    > Change-Id: I0b4fd0708d38b0fae8f751c44d7cb3e849fa8212
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8160922
    > Reviewed-by: Steve Becker <stevebe@microsoft.com>
    > Auto-Submit: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    > Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1669558}
    
    Bug: 523912081
    Change-Id: Ic409b9d211a2def992eaf08c78bb7cdb1d148ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8161063
    Reviewed-by: Steve Becker <stevebe@microsoft.com>
    Commit-Queue: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Auto-Submit: Abhishek Shanthkumar <abhishek.shanthkumar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1671338}

--- content/browser/indexed_db/indexed_db_browsertest.cc
+++ content/browser/indexed_db/indexed_db_browsertest.cc
@@ -1663,6 +1663,18 @@ IN_PROC_BROWSER_TEST_P(IndexedDBBrowserTest, GetAllChunking) {
                                   blink::mojom::kIDBGetAllChunkSize)));
 }
 
+// Too slow on MSAN.
+#if defined(MEMORY_SANITIZER)
+#define MAYBE_GetAllLargeValues DISABLED_GetAllLargeValues
+#else
+#define MAYBE_GetAllLargeValues GetAllLargeValues
+#endif
+// Verifies that getAll() succeeds over an object store containing many large
+// values, testing platform limits. Regression test for crbug.com/523912081.
+IN_PROC_BROWSER_TEST_P(IndexedDBBrowserTest, MAYBE_GetAllLargeValues) {
+  SimpleTest(GetTestUrl("indexeddb", "get_all_large_values.html"));
+}
+
 // Large values are NOT wrapped when using SQLite, but are wrapped when using
 // LevelDB.
 IN_PROC_BROWSER_TEST_P(IndexedDBBrowserTest, LargeValueIsWrapped) {
