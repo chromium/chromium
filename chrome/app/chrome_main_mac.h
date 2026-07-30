fdf48ac9f2193a4e016ecc0c2b719b66f9080c8c
Ran Yeheskel <ran.yeheskel@island.io>
2026-07-30 01:55:21 -0700

    Mac: Depend on chrome_framework via data_deps in chrome/app:test_support
    On Mac, browser_tests C++ object compilations are blocked until the
    linking of //chrome:chrome_framework finishes, which unnecessarily slows
    the build.
    
    The reason is because chrome_framework is a create_bundle target and
    also a GN dep of //chrome/app:test_support (which is an indirect dep of
    the browser_tests target). However, chrome_framework is only needed at
    runtime by the test_support target.
    
    Therefore, we move chrome_framework to be a data_dep (instead of a dep)
    of test_support.
    
    Also, remove dead code from chrome/app/chrome_main_mac.h.
    
    Bug: 537472294
    Change-Id: Ieebd4ae3fb295594df6b8b3cde3407af00c3e7e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8128344
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Michael Maltsev <michaelm@island.io>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670917}

--- chrome/app/chrome_main_mac.h
+++ chrome/app/chrome_main_mac.h
@@ -5,14 +5,6 @@
 #ifndef CHROME_APP_CHROME_MAIN_MAC_H_
 #define CHROME_APP_CHROME_MAIN_MAC_H_
 
-namespace base {
-class FilePath;
-}
-
-// Checks if the UserDataDir policy has been set and returns its value in the
-// |user_data_dir| parameter. If no policy is set the parameter is not changed.
-void CheckUserDataDirPolicy(base::FilePath* user_data_dir);
-
 // Sets the app bundle (base::apple::FrameworkBundle()) to the framework's
 // bundle, and sets the base bundle ID (base::apple::BaseBundleID()) to the
 // proper value based on the running application. The base bundle ID is the
