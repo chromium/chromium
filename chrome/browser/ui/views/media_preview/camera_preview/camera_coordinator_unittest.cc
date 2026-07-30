774da518dd5fae1e1dffe74e83e9666a9f9b1b68
Wenbin Zhang <wenbinzhang@google.com>
2026-07-30 10:52:21 -0700

    [bedrock] Remove Browser* from camera_coordinator_unittest.cc
    Removes TestWithBrowserView dependency from
    camera_coordinator_unittest.cc as part of Project Bedrock.
    
    WHAT CHANGED:
    - Replaced TestWithBrowserView test fixture with ChromeViewsTestBase.
    - Created an isolated TestingProfile via TestingProfileManager in SetUp().
    - Removed unused //chrome/browser/ui/views/frame:test_with_browser_view dependency from BUILD.gn.
    
    WHY: CameraCoordinator and CameraCoordinatorTest only require a Views
    test environment and video capture device preferences. They do not use
    Browser, BrowserView, WebContents, or window management features.
    Migrating to ChromeViewsTestBase eliminates heavy browser window
    scaffolding and aligns with Chromium browser design principles.
    
    Bug: none
    
    TAG=agy CONV=152a45f5-31ae-495f-9f6b-4fad59aa30cc
    
    Change-Id: Ie7b4421e93cdfb4d9a1751c8680beb3122786350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8169122
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671222}

--- chrome/browser/ui/views/media_preview/camera_preview/camera_coordinator_unittest.cc
+++ chrome/browser/ui/views/media_preview/camera_preview/camera_coordinator_unittest.cc
@@ -13,11 +13,15 @@
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/media/prefs/capture_device_ranking.h"
-#include "chrome/browser/ui/views/frame/test_with_browser_view.h"
 #include "chrome/browser/ui/views/media_preview/media_preview_metrics.h"
+#include "chrome/test/base/testing_browser_process.h"
+#include "chrome/test/base/testing_profile.h"
+#include "chrome/test/base/testing_profile_manager.h"
+#include "chrome/test/views/chrome_views_test_base.h"
 #include "components/media_effects/test/fake_audio_service.h"
 #include "components/media_effects/test/fake_video_capture_service.h"
 #include "components/media_effects/test/scoped_media_device_info.h"
+#include "content/public/test/test_renderer_host.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/views/view.h"
@@ -64,10 +68,15 @@ MATCHER(VideoCaptureDeviceInfoEq, "") {
 
 }  // namespace
 
-class CameraCoordinatorTest : public TestWithBrowserView {
+class CameraCoordinatorTest : public ChromeViewsTestBase {
  protected:
   void SetUp() override {
-    TestWithBrowserView::SetUp();
+    ChromeViewsTestBase::SetUp();
+    profile_manager_ = std::make_unique<TestingProfileManager>(
+        TestingBrowserProcess::GetGlobal());
+    ASSERT_TRUE(profile_manager_->SetUp());
+    profile_ = profile_manager_->CreateTestingProfile("test_profile");
+
     fake_video_capture_service_.SetOnGetVideoSourceCallback(
         on_get_video_source_future_.GetRepeatingCallback());
     histogram_tester_.emplace();
@@ -79,8 +88,12 @@ class CameraCoordinatorTest : public TestWithBrowserView {
   void TearDown() override {
     coordinator_.reset();
     parent_view_.reset();
-    TestWithBrowserView::TearDown();
+    profile_ = nullptr;
+    profile_manager_.reset();
+    ChromeViewsTestBase::TearDown();
   }
+
+  Profile* profile() { return profile_; }
 
   void InitializeCoordinator(std::vector<std::string> eligible_camera_ids) {
     CHECK(profile()->GetPrefs());
@@ -126,6 +139,10 @@ class CameraCoordinatorTest : public TestWithBrowserView {
       const std::string&,
       mojo::PendingReceiver<video_capture::mojom::VideoSource>>
       on_get_video_source_future_;
+
+  content::RenderViewHostTestEnabler render_view_host_test_enabler_;
+  std::unique_ptr<TestingProfileManager> profile_manager_;
+  raw_ptr<TestingProfile> profile_;
 };
 
 TEST_F(CameraCoordinatorTest, RelevantVideoCaptureDeviceInfoExtraction) {
