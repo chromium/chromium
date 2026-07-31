f02df04430149c56bd4398b79f08e32850fa1795
Abe Boujane <boujane@google.com>
2026-07-31 07:46:06 -0700

    [Ad Privacy Cleanup] Part 4: Delete Ad Measurement fragment, stubs, and cleanup build rules
    Completely delete Ad Measurement fragment, stubs, XML preferences,
    tests, and screenshot SHA files. Clean up remaining build rules in
    BUILD.gn and remove obsolete preference rows from XML.
    
    TAG=agy
    
    Change-Id: I6266cc3a10e95efe5440d6e08ddbf8b84f9c559d
    Bug: b:534866765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8144685
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1671839}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragment.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxSettingsFragment.java
@@ -14,18 +14,14 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.build.annotations.Nullable;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.search.ChromeBaseSearchIndexProvider;
-import org.chromium.components.browser_ui.settings.ChromeBasePreference;
 import org.chromium.components.browser_ui.settings.SettingsFragment;
-import org.chromium.components.browser_ui.settings.SettingsUtils;
 import org.chromium.components.browser_ui.settings.search.SettingsIndexData;
 
 /** Settings fragment for privacy sandbox settings. */
 @NullMarked
 public class PrivacySandboxSettingsFragment extends PrivacySandboxSettingsBaseFragment {
-    public static final String AD_MEASUREMENT_PREF = "ad_measurement";
     public static final String HELP_CENTER_URL = "https://support.google.com/chrome/?p=ad_privacy";
 
-    private ChromeBasePreference mAdMeasurementPref;
     private final SettableMonotonicObservableSupplier<String> mPageTitle =
             ObservableSuppliers.createMonotonic();
 
@@ -40,13 +36,6 @@ public class PrivacySandboxSettingsFragment extends PrivacySandboxSettingsBaseFr
 
         // Add all preferences and set the title
         mPageTitle.set(getString(R.string.ad_privacy_page_title));
-        if (showRestrictedView()) {
-            SettingsUtils.addPreferencesFromResource(
-                    this, R.xml.privacy_sandbox_preferences_restricted);
-        } else {
-            SettingsUtils.addPreferencesFromResource(this, R.xml.privacy_sandbox_preferences);
-        }
-        mAdMeasurementPref = findPreference(AD_MEASUREMENT_PREF);
 
         parseAndRecordReferrer();
     }
@@ -54,24 +43,6 @@ public class PrivacySandboxSettingsFragment extends PrivacySandboxSettingsBaseFr
     @Override
     public MonotonicObservableSupplier<String> getPageTitle() {
         return mPageTitle;
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        updatePrefDescription();
-    }
-
-    private boolean showRestrictedView() {
-        return getPrivacySandboxBridge().isPrivacySandboxRestricted();
-    }
-
-    private void updatePrefDescription() {
-        mAdMeasurementPref.setSummary(
-                AdMeasurementFragment.isAdMeasurementPrefEnabled(getProfile())
-                        ? R.string.ad_privacy_page_ad_measurement_link_row_sub_label_enabled
-                        : R.string.ad_privacy_page_ad_measurement_link_row_sub_label_disabled);
     }
 
     @Override
@@ -92,8 +63,6 @@ public class PrivacySandboxSettingsFragment extends PrivacySandboxSettingsBaseFr
 
                 @Override
                 public void updateDynamicPreferences(
-                        Context context, SettingsIndexData indexData, Profile profile) {
-                    indexData.removeEntry(getUniqueId(AD_MEASUREMENT_PREF));
-                }
+                        Context context, SettingsIndexData indexData, Profile profile) {}
             };
 }
