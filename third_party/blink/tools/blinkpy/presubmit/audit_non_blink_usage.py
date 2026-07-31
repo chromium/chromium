37c05ae667dba20ed0f504e0634df4028f622c9a
Jan Wilken Dörrie <jdoerrie@chromium.org>
2026-07-31 09:17:21 -0700

    [base] Remove base::zip alias and base/types/zip.h
    This patch removes the `base::zip` alias, which previously served as a
    temporary wrapper for `std::views::zip`. Now that all callers have been
    successfully migrated to use `std::views::zip` directly, the alias is no
    longer needed.
    
    Consequently, this change deletes `base/types/zip.h` along with its unit
    tests, and removes `base::zip` from the allowed non-Blink usage list in
    the Blink presubmit configuration.
    
    Bug: 493534962
    Change-Id: I98ae5e48e467bd7926431b25bf93ab176a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8158744
    Auto-Submit: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1671899}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -206,9 +206,6 @@ _CONFIG = [
             'base::ok',
             'base::unexpected',
 
-            # //base/types/zip.h
-            'base::zip',
-
             # //base/functional/bind.h
             'base::IgnoreResult',
 
