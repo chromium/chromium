142c3b879572c74a8f7e1ecd9ad124cef0c85c22
James Leung <jamesleung@google.com>
2026-07-30 17:40:14 -0700

    [M152] [contextual_tasks] Ensure tools are not repopulated from URL if removed
    Original change's description:
    > [contextual_tasks] Ensure tools are not repopulated from URL if removed
    >
    > * Before: After removing a tool, the tool shows up briefly after
    > submitting the query before correctly disappearing.
    > * Fix: Adds a data structure to track the last removed tool
    > so that if there is a last removed tool (and it differs from the current
    > tool, and the added tool from the URL matches that, the last removed
    > tool is not added again from the URL. This prevents the tool from
    > incorrectly showing up after submitting the query briefly.
    > * `GetActiveToolFromUrl` is not deleted since it is needed for thread
    > and aim link changes.
    > * Added tests.
    >
    > Before - screencast/cast/NTY3NTM2ODU2NzUzNzY2NHwzY2QyYzU0Yi1mZQ
    >
    > After - screencast/cast/NjU1NjgxNDc3MzU4Mzg3MnxhOGJhNzhmYS02NQ
    >
    > Bug: b:538294355
    > Change-Id: I543391bf42a2825d4dee1033731fc3d372f9e162
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8142184
    > Reviewed-by: Duncan Mercer <mercerd@google.com>
    > Commit-Queue: James Leung <jamesleung@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1669928}
    
    (cherry picked from commit 0268045aa967a67abddb73aa3be09c5550b85f17)
    
    Bug: 540949500,b:538294355
    Change-Id: I543391bf42a2825d4dee1033731fc3d372f9e162
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177405
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Juliet Lévesque Knighton <julietlevesque@google.com>
    Cr-Commit-Position: refs/branch-heads/7977@{#221}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- components/contextual_search/input_state_model.cc
+++ components/contextual_search/input_state_model.cc
@@ -435,6 +435,19 @@ void InputStateModel::notifySubscribers() {
 }
 
 void InputStateModel::setActiveTool(ToolMode tool) {
+  // Track tools that user just actually removed to avoid setting a tool
+  // from an outdated URL right after submitting a query. Clear the
+  // removed tool if a tool was just added.
+  // TODO(crbug.com/539684815): Remove this code/set once Google3 fixes
+  // the slow-to-update URL.
+  if (tool == omnibox::ToolMode::TOOL_MODE_UNSPECIFIED &&
+      state_.active_tool != omnibox::ToolMode::TOOL_MODE_UNSPECIFIED) {
+    // `active_tool` represents last tool set before this change.
+    user_removed_tools_.insert(state_.active_tool);
+    state_.is_canvas_query_submitted = false;
+  } else if (tool != omnibox::ToolMode::TOOL_MODE_UNSPECIFIED) {
+    user_removed_tools_.erase(tool);
+  }
   updateSelectedState(tool, state_.active_model);
 }
 
@@ -443,11 +456,21 @@ void InputStateModel::setActiveModel(ModelMode model) {
 }
 
 void InputStateModel::UpdateStateFromUrl(const GURL& url) {
+  // `GetActiveToolFromUrl` is still needed for thread changes and deep links.
   auto matched_tool =
       GetActiveToolFromUrl(url, state_.tool_configs, state_.allowed_tools);
 
   bool thread_changed = GetThreadId(url) != GetThreadId(current_url_);
   current_url_ = url;
+
+  // Ignore tools removed by user in last turn unless thread changes.
+  if (thread_changed) {
+    user_removed_tools_.clear();
+  }
+
+  if (matched_tool.has_value() && user_removed_tools_.contains(*matched_tool)) {
+    matched_tool = std::nullopt;
+  }
 
   ToolMode new_tool = matched_tool.value_or(
       thread_changed ? ToolMode::TOOL_MODE_UNSPECIFIED : state_.active_tool);
@@ -456,6 +479,10 @@ void InputStateModel::UpdateStateFromUrl(const GURL& url) {
       thread_changed ? false : state_.is_canvas_query_submitted;
   if (matched_tool.has_value()) {
     new_canvas_submitted = (*matched_tool == ToolMode::TOOL_MODE_CANVAS);
+  }
+
+  if (user_removed_tools_.contains(omnibox::ToolMode::TOOL_MODE_CANVAS)) {
+    new_canvas_submitted = false;
   }
 
   auto matched_model =
