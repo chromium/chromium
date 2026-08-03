307121be02dab92d3443c4e7659321539060ebc1
Jordan Bayles <jophba@chromium.org>
2026-08-03 11:59:23 -0700

    [Symphonia] Launch MP3 decoding on Android
    This patch default enables Symphonia audio decoding for MP3 on
    Android & Fuchsia, effectively enabling it on all platforms.
    
    Bug: 476202821
    Change-Id: I5f764159d70dc4f10ad43188847f1855b4f95066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8181155
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672811}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -646,6 +646,9 @@ BASE_FEATURE(kDirectOpusAudioDecoding, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kRevokeMediaSourceObjectURLOnAttach,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Toggles SymphoniaAudioDecoder support for FLAC decoding. If disabled, the
+// media pipeline will fall back to a reasonable default, typically the FFmpeg
+// audio decoder.
 #if BUILDFLAG(ENABLE_SYMPHONIA)
 // Android / Fuchsia are expected to launch in M150.
 BASE_FEATURE(kSymphoniaAudioDecoding,
@@ -657,17 +660,19 @@ BASE_FEATURE(kSymphoniaAudioDecoding,
 #endif
 );
 
-// Android / Fuchsia are expected to launch in M150.
-BASE_FEATURE(kSymphoniaMp3Decoding,
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_CHROMEOS)
-             base::FEATURE_ENABLED_BY_DEFAULT
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT
-#endif
-);
+// Toggles SymphoniaAudioDecoder support for MP3 decoding. If disabled, the
+// media pipeline will fall back to a reasonable default, typically the FFmpeg
+// audio decoder.
+BASE_FEATURE(kSymphoniaMp3Decoding, base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Toggles SymphoniaAudioDecoder support for PCM decoding. If disabled, the
+// media pipeline will fall back to a reasonable default, typically the FFmpeg
+// audio decoder.
 BASE_FEATURE(kSymphoniaPcmDecoding, base::FEATURE_DISABLED_BY_DEFAULT);
+
+// Toggles SymphoniaAudioDecoder support for Vorbis decoding. If disabled, the
+// media pipeline will fall back to a reasonable default, typically the FFmpeg
+// audio decoder.
 BASE_FEATURE(kSymphoniaVorbisDecoding, base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
