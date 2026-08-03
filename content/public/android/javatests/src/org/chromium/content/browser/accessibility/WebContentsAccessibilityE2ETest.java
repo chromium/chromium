84e89ac75d0d2e8814178207b5806ca5677390f8
Tomek Jurkiewicz <tju@google.com>
2026-08-03 04:59:35 -0700

    Disable fireGeneratedEvent_ariaLabelChange_firesTextChangeType on Automotive
    Add @Restriction(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO) to
    fireGeneratedEvent_ariaLabelChange_firesTextChangeType() in
    WebContentsAccessibilityE2ETest.java so that the test is skipped on
    Android Automotive OS form factors.
    
    Test is notoriously failing on
    https://ci.chromium.org/ui/p/chrome-m152/builders/ci/android-automotive-12l-x64-rel-tests,
    example failure:
    https://ci.chromium.org/ui/p/chrome-m152/builders/ci/android-automotive-12l-x64-rel-tests/134/overview
    
    TAG=agy
    CONV=e44cfe93-e786-429b-9ae3-8cd477e6543f
    
    Bug: 542055199
    Change-Id: I8821cdf159edc4cc6f4defac3498e56282bdeba2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8185822
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Owners-Override: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672610}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityE2ETest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityE2ETest.java
@@ -843,6 +843,7 @@ WebView focusable actions:[FOCUS, AX_FOCUS] bundle:[chromeRole="rootWebArea"]
     @Test
     @SmallTest
     @DisabledTest(message = "crbug.com/533174018")
+    @Restriction(DeviceRestriction.RESTRICTION_TYPE_NON_AUTO) // crbug.com/542055199
     @MinAndroidSdkLevel(Build.VERSION_CODES.KITKAT) // API Level 19
     public void fireGeneratedEvent_ariaLabelChange_firesTextChangeType() throws Throwable {
         // Create an HTML document where there is a div tag with aria-label attribute set.
