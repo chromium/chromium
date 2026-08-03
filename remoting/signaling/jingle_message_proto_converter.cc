6a5892706b0df744b873f785b99c89048c660319
Joe Downing <joedow@google.com>
2026-08-02 17:50:14 -0700

    Fix missing authentication and transport info in proto converter
    FTL signaling was failing with a DCHECK when using protobuf format because JingleMessageFromProto did not populate the description field (which contains authentication details) or the transport info.
    
    This CL implements the missing conversions for JingleAuthentication and JingleTransportInfo in the Jingle proto converter and adds unit tests.
    
    TEST=remoting_unittests --gtest_filter=JingleMessageProtoConverterTest.*
    
    Bug: 540950638
    Change-Id: Ia73387a1c9633773a01d483c691762674a871893
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8184108
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672452}

--- remoting/signaling/jingle_message_proto_converter.cc
+++ remoting/signaling/jingle_message_proto_converter.cc
@@ -9,12 +9,189 @@
 
 #include "base/logging.h"
 #include "base/notreached.h"
+#include "remoting/signaling/content_description.h"
 #include "remoting/signaling/jingle_data_structures.h"
 #include "remoting/signaling/signaling_address.h"
+#include "third_party/webrtc/api/jsep.h"
 
 namespace remoting {
 
 namespace {
+
+ftl::AuthenticationMethod AuthMethodToProto(AuthenticationMethod method) {
+  switch (method) {
+    case AuthenticationMethod::INVALID:
+      return ftl::AUTHENTICATION_METHOD_UNSPECIFIED;
+    case AuthenticationMethod::SHARED_SECRET_SPAKE2_CURVE25519:
+      return ftl::AUTHENTICATION_METHOD_SPAKE2_CURVE25519;
+    case AuthenticationMethod::PAIRED_SPAKE2_CURVE25519:
+      return ftl::AUTHENTICATION_METHOD_PAIRED_SPAKE2_CURVE25519;
+    case AuthenticationMethod::CLOUD_SESSION_AUTHZ_SPAKE2_CURVE25519:
+      return ftl::AUTHENTICATION_METHOD_CLOUD_SESSION_AUTHZ_SPAKE2_CURVE25519;
+    case AuthenticationMethod::CORP_SESSION_AUTHZ_SPAKE2_CURVE25519:
+      return ftl::AUTHENTICATION_METHOD_CORP_SESSION_AUTHZ_SPAKE2_CURVE25519;
+  }
+  NOTREACHED();
+}
+
+AuthenticationMethod ProtoToAuthMethod(ftl::AuthenticationMethod method) {
+  switch (method) {
+    case ftl::AUTHENTICATION_METHOD_UNSPECIFIED:
+      return AuthenticationMethod::INVALID;
+    case ftl::AUTHENTICATION_METHOD_SPAKE2_CURVE25519:
+      return AuthenticationMethod::SHARED_SECRET_SPAKE2_CURVE25519;
+    case ftl::AUTHENTICATION_METHOD_PAIRED_SPAKE2_CURVE25519:
+      return AuthenticationMethod::PAIRED_SPAKE2_CURVE25519;
+    case ftl::AUTHENTICATION_METHOD_CLOUD_SESSION_AUTHZ_SPAKE2_CURVE25519:
+      return AuthenticationMethod::CLOUD_SESSION_AUTHZ_SPAKE2_CURVE25519;
+    case ftl::AUTHENTICATION_METHOD_CORP_SESSION_AUTHZ_SPAKE2_CURVE25519:
+      return AuthenticationMethod::CORP_SESSION_AUTHZ_SPAKE2_CURVE25519;
+    default:
+      return AuthenticationMethod::INVALID;
+  }
+}
+
+void JingleAuthenticationToProto(const JingleAuthentication& auth,
+                                 ftl::Authentication* proto) {
+  for (auto method : auth.supported_methods) {
+    proto->add_supported_methods(AuthMethodToProto(method));
+  }
+  if (auth.method) {
+    proto->set_method(AuthMethodToProto(*auth.method));
+  }
+  if (!auth.spake_message.empty()) {
+    proto->set_spake_message(auth.spake_message.data(),
+                             auth.spake_message.size());
+  }
+  if (!auth.verification_hash.empty()) {
+    proto->set_verification_hash(auth.verification_hash.data(),
+                                 auth.verification_hash.size());
+  }
+  if (!auth.session_authz_host_token.empty()) {
+    proto->set_session_authz_host_token(auth.session_authz_host_token);
+  }
+  if (!auth.session_authz_session_token.empty()) {
+    proto->set_session_authz_session_token(auth.session_authz_session_token);
+  }
+}
+
+void JingleAuthenticationFromProto(const ftl::Authentication& proto,
+                                   JingleAuthentication* auth) {
+  for (int i = 0; i < proto.supported_methods_size(); ++i) {
+    auth->supported_methods.push_back(
+        ProtoToAuthMethod(proto.supported_methods(i)));
+  }
+  if (proto.has_method()) {
+    auth->method = ProtoToAuthMethod(proto.method());
+  }
+  if (proto.has_spake_message()) {
+    auth->spake_message.assign(proto.spake_message().begin(),
+                               proto.spake_message().end());
+  }
+  if (proto.has_verification_hash()) {
+    auth->verification_hash.assign(proto.verification_hash().begin(),
+                                   proto.verification_hash().end());
+  }
+  if (proto.has_session_authz_host_token()) {
+    auth->session_authz_host_token = proto.session_authz_host_token();
+  }
+  if (proto.has_session_authz_session_token()) {
+    auth->session_authz_session_token = proto.session_authz_session_token();
+  }
+}
+
+ftl::SessionDescription::SdpType SdpTypeToProto(SessionDescription::Type type) {
+  switch (type) {
+    case SessionDescription::Type::kUnspecified:
+      return ftl::SessionDescription::SDP_TYPE_UNSPECIFIED;
+    case SessionDescription::Type::kOffer:
+      return ftl::SessionDescription::OFFER;
+    case SessionDescription::Type::kAnswer:
+      return ftl::SessionDescription::ANSWER;
+  }
+  NOTREACHED();
+}
+
+SessionDescription::Type ProtoToSdpType(ftl::SessionDescription::SdpType type) {
+  switch (type) {
+    case ftl::SessionDescription::SDP_TYPE_UNSPECIFIED:
+      return SessionDescription::Type::kUnspecified;
+    case ftl::SessionDescription::OFFER:
+      return SessionDescription::Type::kOffer;
+    case ftl::SessionDescription::ANSWER:
+      return SessionDescription::Type::kAnswer;
+    default:
+      return SessionDescription::Type::kUnspecified;
+  }
+}
+
+void JingleTransportInfoToProto(const JingleTransportInfo& transport,
+                                ftl::TransportInfo* proto) {
+  if (transport.session_description) {
+    ftl::SessionDescription* proto_sdp = proto->mutable_session_description();
+    proto_sdp->set_type(SdpTypeToProto(transport.session_description->type));
+    proto_sdp->set_sdp(transport.session_description->sdp);
+    if (!transport.session_description->signature.empty()) {
+      proto_sdp->set_signature(transport.session_description->signature.data(),
+                               transport.session_description->signature.size());
+    }
+  }
+
+  for (const auto& candidate : transport.candidates) {
+    if (!candidate.sdp_m_line_index.has_value()) {
+      LOG(WARNING) << "Ignoring candidate without sdp_m_line_index";
+      continue;
+    }
+    ftl::IceCandidate* proto_candidate = proto->add_candidates();
+    webrtc::IceCandidate webrtc_candidate(
+        candidate.name, *candidate.sdp_m_line_index, candidate.candidate);
+    std::string candidate_str = webrtc_candidate.ToString();
+    proto_candidate->set_candidate(candidate_str);
+    proto_candidate->set_sdp_mid(candidate.name);
+    proto_candidate->set_sdp_m_line_index(*candidate.sdp_m_line_index);
+  }
+}
+
+bool JingleTransportInfoFromProto(const ftl::TransportInfo& proto,
+                                  JingleTransportInfo* transport) {
+  if (proto.has_session_description()) {
+    SessionDescription sdp;
+    sdp.type = ProtoToSdpType(proto.session_description().type());
+    sdp.sdp = proto.session_description().sdp();
+    if (proto.session_description().has_signature()) {
+      sdp.signature.assign(proto.session_description().signature().begin(),
+                           proto.session_description().signature().end());
+    }
+    transport->session_description = std::move(sdp);
+  }
+
+  for (int i = 0; i < proto.candidates_size(); ++i) {
+    const ftl::IceCandidate& proto_candidate = proto.candidates(i);
+    if (!proto_candidate.has_sdp_m_line_index() ||
+        !proto_candidate.has_sdp_mid() || !proto_candidate.has_candidate()) {
+      LOG(WARNING) << "Incomplete candidate in proto, skipping";
+      continue;
+    }
+    IceTransportInfo::NamedCandidate candidate;
+    candidate.name = proto_candidate.sdp_mid();
+    candidate.sdp_m_line_index = proto_candidate.sdp_m_line_index();
+
+    webrtc::SdpParseError error;
+    std::unique_ptr<webrtc::IceCandidate> webrtc_candidate =
+        webrtc::IceCandidate::Create(candidate.name,
+                                     *candidate.sdp_m_line_index,
+                                     proto_candidate.candidate(), &error);
+    if (!webrtc_candidate) {
+      LOG(WARNING) << "Failed to parse incoming candidate: "
+                   << error.description << " line: " << error.line
+                   << ", skipping";
+      continue;
+    }
+    candidate.candidate = webrtc_candidate->candidate();
+    transport->candidates.push_back(std::move(candidate));
+  }
+  return true;
+}
 
 void SignalingAddressToJabberId(const SignalingAddress& address,
                                 ftl::JabberId* jabber_id) {
@@ -107,10 +284,23 @@ ftl::IqStanza JingleMessageToProto(const JingleMessage& message) {
   ftl::JingleMessage* jingle = stanza.mutable_jingle();
   jingle->set_session_id(message.sid);
 
-  if (std::holds_alternative<SessionInitiate>(message.payload())) {
-    jingle->mutable_session_initiate();
-  } else if (std::holds_alternative<SessionAccept>(message.payload())) {
-    jingle->mutable_session_accept();
+  if (const auto* initiate = std::get_if<SessionInitiate>(&message.payload())) {
+    ftl::SessionInitiate* proto_initiate = jingle->mutable_session_initiate();
+    if (!message.initiator.empty()) {
+      SignalingAddressToJabberId(SignalingAddress(message.initiator),
+                                 proto_initiate->mutable_initiator());
+    }
+    if (initiate->authentication) {
+      JingleAuthenticationToProto(*initiate->authentication,
+                                  proto_initiate->mutable_authentication());
+    }
+  } else if (const auto* accept =
+                 std::get_if<SessionAccept>(&message.payload())) {
+    ftl::SessionAccept* proto_accept = jingle->mutable_session_accept();
+    if (accept->authentication) {
+      JingleAuthenticationToProto(*accept->authentication,
+                                  proto_accept->mutable_authentication());
+    }
   } else if (auto* terminate =
                  std::get_if<SessionTerminate>(&message.payload())) {
     ftl::SessionTerminate* proto_terminate =
@@ -123,10 +313,16 @@ ftl::IqStanza JingleMessageToProto(const JingleMessage& message) {
     if (!terminate->error_details.empty()) {
       proto_terminate->set_error_details(terminate->error_details);
     }
-  } else if (std::holds_alternative<SessionInfo>(message.payload())) {
-    jingle->mutable_session_info();
-  } else if (std::holds_alternative<JingleTransportInfo>(message.payload())) {
-    jingle->mutable_transport_info();
+  } else if (const auto* session_info =
+                 std::get_if<SessionInfo>(&message.payload())) {
+    ftl::SessionInfo* proto_session_info = jingle->mutable_session_info();
+    if (session_info->authentication) {
+      JingleAuthenticationToProto(*session_info->authentication,
+                                  proto_session_info->mutable_authentication());
+    }
+  } else if (const auto* transport =
+                 std::get_if<JingleTransportInfo>(&message.payload())) {
+    JingleTransportInfoToProto(*transport, jingle->mutable_transport_info());
   } else {
     NOTREACHED() << "Unknown message payload.";
   }
@@ -148,9 +344,30 @@ bool JingleMessageFromProto(const ftl::IqStanza& stanza,
   message->sid = jingle.session_id();
 
   if (jingle.has_session_initiate()) {
-    message->SetPayload(SessionInitiate());
+    SessionInitiate initiate;
+    JingleAuthentication auth;
+    if (jingle.session_initiate().has_authentication()) {
+      JingleAuthenticationFromProto(jingle.session_initiate().authentication(),
+                                    &auth);
+      initiate.authentication = auth;
+    }
+    message->description = std::make_unique<ContentDescription>(auth);
+    if (jingle.session_initiate().has_initiator()) {
+      message->initiator =
+          JabberIdToSignalingAddress(jingle.session_initiate().initiator())
+              .id();
+    }
+    message->SetPayload(std::move(initiate));
   } else if (jingle.has_session_accept()) {
-    message->SetPayload(SessionAccept());
+    SessionAccept accept;
+    JingleAuthentication auth;
+    if (jingle.session_accept().has_authentication()) {
+      JingleAuthenticationFromProto(jingle.session_accept().authentication(),
+                                    &auth);
+      accept.authentication = auth;
+    }
+    message->description = std::make_unique<ContentDescription>(auth);
+    message->SetPayload(std::move(accept));
   } else if (jingle.has_session_terminate()) {
     SessionTerminate terminate;
     terminate.reason =
@@ -159,9 +376,21 @@ bool JingleMessageFromProto(const ftl::IqStanza& stanza,
     terminate.error_details = jingle.session_terminate().error_details();
     message->SetPayload(std::move(terminate));
   } else if (jingle.has_session_info()) {
-    message->SetPayload(SessionInfo());
+    SessionInfo session_info;
+    if (jingle.session_info().has_authentication()) {
+      JingleAuthentication auth;
+      JingleAuthenticationFromProto(jingle.session_info().authentication(),
+                                    &auth);
+      session_info.authentication = auth;
+    }
+    message->SetPayload(std::move(session_info));
   } else if (jingle.has_transport_info()) {
-    message->SetPayload(JingleTransportInfo());
+    JingleTransportInfo transport;
+    if (!JingleTransportInfoFromProto(jingle.transport_info(), &transport)) {
+      *error = "Failed to parse TransportInfo from proto";
+      return false;
+    }
+    message->SetPayload(std::move(transport));
   } else {
     *error = "Unknown Jingle action";
     return false;
--- remoting/signaling/jingle_message_proto_converter_unittest.cc
+++ remoting/signaling/jingle_message_proto_converter_unittest.cc
@@ -6,9 +6,11 @@
 
 #include <string>
 
+#include "remoting/signaling/content_description.h"
 #include "remoting/signaling/jingle_data_structures.h"
 #include "remoting/signaling/signaling_address.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/webrtc/api/jsep.h"
 
 namespace remoting {
 
@@ -19,6 +21,30 @@ const char kToLocalId[] = "to_user@gmail.com";
 const char kToRegistrationId[] = "to_registration_id";
 const char kMessageId[] = "test_message_id";
 const char kSid[] = "test_sid";
+
+JingleAuthentication CreateTestAuthentication() {
+  JingleAuthentication auth;
+  auth.supported_methods = {
+      AuthenticationMethod::SHARED_SECRET_SPAKE2_CURVE25519,
+      AuthenticationMethod::PAIRED_SPAKE2_CURVE25519};
+  auth.method = AuthenticationMethod::SHARED_SECRET_SPAKE2_CURVE25519;
+  auth.spake_message = {1, 2, 3, 4};
+  auth.verification_hash = {5, 6, 7, 8};
+  auth.session_authz_host_token = "host_token";
+  auth.session_authz_session_token = "session_token";
+  return auth;
+}
+
+void VerifyAuthentication(const JingleAuthentication& actual,
+                          const JingleAuthentication& expected) {
+  EXPECT_EQ(actual.supported_methods, expected.supported_methods);
+  EXPECT_EQ(actual.method, expected.method);
+  EXPECT_EQ(actual.spake_message, expected.spake_message);
+  EXPECT_EQ(actual.verification_hash, expected.verification_hash);
+  EXPECT_EQ(actual.session_authz_host_token, expected.session_authz_host_token);
+  EXPECT_EQ(actual.session_authz_session_token,
+            expected.session_authz_session_token);
+}
 }  // namespace
 
 class JingleMessageProtoConverterTest : public testing::Test {
@@ -143,6 +169,218 @@ TEST_F(JingleMessageProtoConverterTest, ConvertReplyError) {
   EXPECT_EQ(converted_reply.reply_type, JingleMessageReply::REPLY_ERROR);
   EXPECT_EQ(converted_reply.error_type, JingleMessageReply::INVALID_SID);
   EXPECT_EQ(converted_reply.text, "Invalid session ID");
+}
+
+TEST_F(JingleMessageProtoConverterTest, ConvertSessionInitiateWithAuth) {
+  JingleMessage message;
+  message.from = from_address_;
+  message.to = to_address_;
+  message.message_id = kMessageId;
+  message.sid = kSid;
+  message.initiator = kFromLocalId;
+  SessionInitiate initiate;
+  JingleAuthentication auth = CreateTestAuthentication();
+  initiate.authentication = auth;
+  message.SetPayload(std::move(initiate));
+
+  ftl::IqStanza stanza = message.ToFtlIqStanza();
+
+  EXPECT_TRUE(stanza.jingle().has_session_initiate());
+  EXPECT_EQ(stanza.jingle().session_initiate().initiator().local_part(),
+            kFromLocalId);
+  EXPECT_TRUE(stanza.jingle().session_initiate().has_authentication());
+
+  JingleMessage converted_message;
+  std::string error;
+  ASSERT_TRUE(JingleMessageFromProto(stanza, &converted_message, &error))
+      << error;
+  EXPECT_EQ(converted_message.initiator, kFromLocalId);
+  auto* converted_initiate =
+      std::get_if<SessionInitiate>(&converted_message.payload());
+  ASSERT_TRUE(converted_initiate);
+  ASSERT_TRUE(converted_initiate->authentication.has_value());
+  VerifyAuthentication(*converted_initiate->authentication, auth);
+
+  ASSERT_TRUE(converted_message.description);
+  VerifyAuthentication(converted_message.description->authentication(), auth);
+}
+
+TEST_F(JingleMessageProtoConverterTest, ConvertSessionAcceptWithAuth) {
+  JingleMessage message;
+  message.from = from_address_;
+  message.to = to_address_;
+  message.message_id = kMessageId;
+  message.sid = kSid;
+  SessionAccept accept;
+  JingleAuthentication auth = CreateTestAuthentication();
+  accept.authentication = auth;
+  message.SetPayload(std::move(accept));
+
+  ftl::IqStanza stanza = message.ToFtlIqStanza();
+
+  EXPECT_TRUE(stanza.jingle().has_session_accept());
+  EXPECT_TRUE(stanza.jingle().session_accept().has_authentication());
+
+  JingleMessage converted_message;
+  std::string error;
+  ASSERT_TRUE(JingleMessageFromProto(stanza, &converted_message, &error))
+      << error;
+  auto* converted_accept =
+      std::get_if<SessionAccept>(&converted_message.payload());
+  ASSERT_TRUE(converted_accept);
+  ASSERT_TRUE(converted_accept->authentication.has_value());
+  VerifyAuthentication(*converted_accept->authentication, auth);
+
+  ASSERT_TRUE(converted_message.description);
+  VerifyAuthentication(converted_message.description->authentication(), auth);
+}
+
+TEST_F(JingleMessageProtoConverterTest, ConvertSessionInfoWithAuth) {
+  JingleMessage message;
+  message.from = from_address_;
+  message.to = to_address_;
+  message.message_id = kMessageId;
+  message.sid = kSid;
+  SessionInfo session_info;
+  JingleAuthentication auth = CreateTestAuthentication();
+  session_info.authentication = auth;
+  message.SetPayload(std::move(session_info));
+
+  ftl::IqStanza stanza = message.ToFtlIqStanza();
+
+  EXPECT_TRUE(stanza.jingle().has_session_info());
+  EXPECT_TRUE(stanza.jingle().session_info().has_authentication());
+
+  JingleMessage converted_message;
+  std::string error;
+  ASSERT_TRUE(JingleMessageFromProto(stanza, &converted_message, &error))
+      << error;
+  auto* converted_session_info =
+      std::get_if<SessionInfo>(&converted_message.payload());
+  ASSERT_TRUE(converted_session_info);
+  ASSERT_TRUE(converted_session_info->authentication.has_value());
+  VerifyAuthentication(*converted_session_info->authentication, auth);
+}
+
+TEST_F(JingleMessageProtoConverterTest, ConvertTransportInfo) {
+  JingleMessage message;
+  message.from = from_address_;
+  message.to = to_address_;
+  message.message_id = kMessageId;
+  message.sid = kSid;
+
+  JingleTransportInfo transport;
+  SessionDescription sdp;
+  sdp.type = SessionDescription::Type::kOffer;
+  sdp.sdp = "test_sdp";
+  sdp.signature = {9, 10, 11};
+  transport.session_description = sdp;
+
+  webrtc::SdpParseError error;
+  std::unique_ptr<webrtc::IceCandidate> webrtc_candidate =
+      webrtc::IceCandidate::Create(
+          "audio", 0,
+          "candidate:842163049 1 udp 16777215 127.0.0.1 12345 typ host",
+          &error);
+  ASSERT_TRUE(webrtc_candidate) << error.description;
+  IceTransportInfo::NamedCandidate candidate("audio",
+                                             webrtc_candidate->candidate(), 0);
+  transport.candidates.push_back(candidate);
+
+  message.SetPayload(std::move(transport));
+
+  ftl::IqStanza stanza = message.ToFtlIqStanza();
+
+  EXPECT_TRUE(stanza.jingle().has_transport_info());
+  EXPECT_TRUE(stanza.jingle().transport_info().has_session_description());
+  EXPECT_EQ(stanza.jingle().transport_info().session_description().sdp(),
+            "test_sdp");
+  EXPECT_EQ(stanza.jingle().transport_info().candidates_size(), 1);
+
+  JingleMessage converted_message;
+  std::string conversion_error;
+  ASSERT_TRUE(
+      JingleMessageFromProto(stanza, &converted_message, &conversion_error))
+      << conversion_error;
+  auto* converted_transport =
+      std::get_if<JingleTransportInfo>(&converted_message.payload());
+  ASSERT_TRUE(converted_transport);
+  ASSERT_TRUE(converted_transport->session_description.has_value());
+  EXPECT_EQ(converted_transport->session_description->sdp, "test_sdp");
+  EXPECT_EQ(converted_transport->session_description->type,
+            SessionDescription::Type::kOffer);
+  EXPECT_EQ(converted_transport->session_description->signature, sdp.signature);
+
+  ASSERT_EQ(converted_transport->candidates.size(), 1u);
+  EXPECT_EQ(converted_transport->candidates[0].name, "audio");
+  EXPECT_EQ(*converted_transport->candidates[0].sdp_m_line_index, 0);
+  EXPECT_EQ(converted_transport->candidates[0].candidate.protocol(), "udp");
+  EXPECT_EQ(converted_transport->candidates[0]
+                .candidate.address()
+                .ipaddr()
+                .ToString(),
+            "127.0.0.1");
+}
+
+TEST_F(JingleMessageProtoConverterTest,
+       ConvertTransportInfoWithMalformedCandidates) {
+  JingleMessage message;
+  message.from = from_address_;
+  message.to = to_address_;
+  message.message_id = kMessageId;
+  message.sid = kSid;
+
+  JingleTransportInfo transport;
+  SessionDescription sdp;
+  sdp.type = SessionDescription::Type::kOffer;
+  sdp.sdp = "test_sdp";
+  transport.session_description = sdp;
+
+  webrtc::SdpParseError error;
+  std::unique_ptr<webrtc::IceCandidate> webrtc_candidate =
+      webrtc::IceCandidate::Create(
+          "audio", 0,
+          "candidate:842163049 1 udp 16777215 127.0.0.1 12345 typ host",
+          &error);
+  ASSERT_TRUE(webrtc_candidate) << error.description;
+  IceTransportInfo::NamedCandidate candidate("audio",
+                                             webrtc_candidate->candidate(), 0);
+  transport.candidates.push_back(candidate);
+
+  message.SetPayload(std::move(transport));
+
+  ftl::IqStanza stanza = message.ToFtlIqStanza();
+
+  // Now manually add malformed candidates to the proto stanza.
+  auto* jingle_stanza = stanza.mutable_jingle();
+  auto* transport_info = jingle_stanza->mutable_transport_info();
+
+  // 1. Incomplete candidate (missing sdp_m_line_index)
+  auto* incomplete_candidate = transport_info->add_candidates();
+  incomplete_candidate->set_sdp_mid("video");
+  incomplete_candidate->set_candidate(
+      "candidate:842163049 1 udp 16777215 127.0.0.1 12346 typ host");
+
+  // 2. Malformed candidate (invalid candidate string)
+  auto* malformed_candidate = transport_info->add_candidates();
+  malformed_candidate->set_sdp_mid("video");
+  malformed_candidate->set_sdp_m_line_index(1);
+  malformed_candidate->set_candidate("invalid candidate string");
+
+  JingleMessage converted_message;
+  std::string conversion_error;
+  ASSERT_TRUE(
+      JingleMessageFromProto(stanza, &converted_message, &conversion_error))
+      << conversion_error;
+
+  auto* converted_transport =
+      std::get_if<JingleTransportInfo>(&converted_message.payload());
+  ASSERT_TRUE(converted_transport);
+
+  // Should only have the one valid candidate.
+  ASSERT_EQ(converted_transport->candidates.size(), 1u);
+  EXPECT_EQ(converted_transport->candidates[0].name, "audio");
+  EXPECT_EQ(*converted_transport->candidates[0].sdp_m_line_index, 0);
 }
 
 }  // namespace remoting
