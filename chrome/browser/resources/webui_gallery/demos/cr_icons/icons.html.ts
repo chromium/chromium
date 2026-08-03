27cea59b38b938cff225026801a8ea102d9d0329
Emily Shack <emshack@chromium.org>
2026-08-03 16:59:12 -0700

    [GlowUp] Round webui_gallery custom icon
    See go/chrome-icon-modernization-dd. This is part of a larger effort to
    standardize icon naming, enabling easier bulk updates in the future, as
    well as applying 100% roundedness to all existing icons.
    
    Bug: 511303545
    Change-Id: I2a254de3c960e109bbf44ea0c83824d2546d6d83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8178920
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672988}

--- chrome/browser/resources/webui_gallery/demos/cr_icons/icons.html.ts
+++ chrome/browser/resources/webui_gallery/demos/cr_icons/icons.html.ts
@@ -7,7 +7,17 @@ import '//resources/cr_elements/cr_icon/cr_iconset.js';
 import {getTrustedHTML} from '//resources/js/static_types.js';
 
 const div = document.createElement('div');
-div.innerHTML = getTrustedHTML`
+if (document.documentElement.hasAttribute('webui-rounded-icons')) {
+  div.innerHTML = getTrustedHTML`
+<cr-iconset name="desserts">
+  <svg>
+    <defs>
+      <g id="cake"><path d="M4.8 21.602c-.34 0-.624-.118-.855-.348a1.16 1.16 0 0 1-.343-.856v-4.796c0-.497.175-.922.527-1.274a1.735 1.735 0 0 1 1.27-.527H6V9.6c0-.496.176-.921.527-1.273a1.744 1.744 0 0 1 1.274-.527h3.3V6.449a2.783 2.783 0 0 1-.664-.625c-.16-.215-.238-.515-.238-.894 0-.235.043-.457.125-.668.086-.207.215-.403.399-.582l.953-.953.324-.153c.066 0 .176.043.324.125l.973.973c.183.183.316.383.39.59.075.21.114.433.114.668 0 .379-.078.68-.239.894-.16.219-.378.426-.664.625v1.352H16.2c.496 0 .918.176 1.274.527.351.352.527.777.527 1.274V13.8h.602c.492 0 .918.176 1.27.527.35.352.526.777.526 1.274v4.796c0 .34-.113.625-.343.856-.23.23-.516.348-.856.348Zm3-7.801h8.4V9.6H7.8Zm-2.402 6h13.204v-4.2H5.398Zm2.403-6h8.398Zm-2.403 6h13.204Zm13.204-6H5.398Zm0 0"></path></g>
+    </defs>
+  </svg>
+</cr-iconset>`;
+} else {
+  div.innerHTML = getTrustedHTML`
 <cr-iconset name="desserts">
   <svg>
     <defs>
@@ -15,6 +25,7 @@ div.innerHTML = getTrustedHTML`
     </defs>
   </svg>
 </cr-iconset>`;
+}
 
 const iconsets = div.querySelectorAll('cr-iconset');
 for (const iconset of iconsets) {
