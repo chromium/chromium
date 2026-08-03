a56f4b4226fd93cc1e4290f4436466331ca16bcf
Abe Boujane <boujane@google.com>
2026-08-03 14:29:12 -0700

    Delete Privacy Sandbox Settings fragments, resources, and expired histogram
    Remove PrivacySandboxSettingsFragment,
    PrivacySandboxSettingsBaseFragment, their build target entries, unused
    resources (privacy_sandbox_preferences.xml and
    IDS_AD_PRIVACY_PAGE_TITLE), dependency injection handling, and the
    expired Settings.PrivacySandbox.PrivacySandboxReferrer histogram and
    unused enum.
    
    OBSOLETE_HISTOGRAM[Settings.PrivacySandbox.PrivacySandboxReferrer]=Removed
    along with PrivacySandboxSettingsFragment cleanup.
    
    TAG=agy CONV=228ec019-243f-41fd-bc71-5278bba979b7
    
    Bug: b:534866765
    Change-Id: I4f0735fe9b64352b40a759b0b2457bb9c0b97f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8186639
    Auto-Submit: Abe Boujane <boujane@google.com>
    Reviewed-by: Moe Adel <adelm@google.com>
    Commit-Queue: Moe Adel <adelm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672899}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/FragmentDependencyProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/FragmentDependencyProvider.java
@@ -29,11 +29,9 @@ import org.chromium.chrome.browser.image_descriptions.ImageDescriptionsSettings;
 import org.chromium.chrome.browser.language.settings.LanguageSettings;
 import org.chromium.chrome.browser.lifetime.ApplicationLifetime;
 import org.chromium.chrome.browser.locale.LocaleManager;
-import org.chromium.chrome.browser.page_info.SiteSettingsHelper;
 import org.chromium.chrome.browser.password_manager.PasswordManagerHelper;
 import org.chromium.chrome.browser.password_manager.PasswordStoreBridge;
 import org.chromium.chrome.browser.privacy_guide.PrivacyGuideFragment;
-import org.chromium.chrome.browser.privacy_sandbox.PrivacySandboxSettingsBaseFragment;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.safety_check.SafetyCheckBridge;
 import org.chromium.chrome.browser.safety_check.SafetyCheckCoordinator;
@@ -57,7 +55,6 @@ import org.chromium.components.browser_ui.settings.FragmentSettingsNavigation;
 import org.chromium.components.browser_ui.settings.SearchViewProvider;
 import org.chromium.components.browser_ui.settings.SettingsCustomTabLauncher;
 import org.chromium.components.browser_ui.site_settings.BaseSiteSettingsFragment;
-import org.chromium.components.browser_ui.site_settings.SiteSettingsCategory;
 import org.chromium.ui.base.ActivityResultTracker;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -176,18 +173,6 @@ public class FragmentDependencyProvider extends FragmentManager.FragmentLifecycl
                         ImageDescriptionsController.getInstance().onlyOnWifiEnabled(mProfile));
             }
             imageFragment.setDelegate(ImageDescriptionsController.getInstance().getDelegate());
-        }
-        if (fragment instanceof PrivacySandboxSettingsBaseFragment) {
-            PrivacySandboxSettingsBaseFragment sandboxFragment =
-                    (PrivacySandboxSettingsBaseFragment) fragment;
-            sandboxFragment.setSnackbarManagerSupplier(mSnackbarManagerSupplier);
-            sandboxFragment.setCookieSettingsIntentHelper(
-                    (Context context) -> {
-                        SiteSettingsHelper.showCategorySettings(
-                                context,
-                                SiteSettingsCategory.Type.THIRD_PARTY_COOKIES,
-                                /* addToBackStack= */ true);
-                    });
         }
         if (fragment instanceof LanguageSettings) {
             ((LanguageSettings) fragment)
