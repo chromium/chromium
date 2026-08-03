1d3c9652ab51ca847eb6e0692e259cd8c8aa59b2
Stanislav Mikheyev <mikhe@google.com>
2026-08-03 05:08:14 -0700

    YourSavedInfo: Delete unreachable pre-HoT Autofill settings pages
    Now that the "Your Saved Info" (Home of Transactions M1) settings page is unconditional on desktop, the legacy pre-HoT top-level Autofill wrappers are unreachable dead code.
    
    Specifically:
    - Delete pre-HoT top-level page containers (autofill_page.html/ts and autofill_page_index.html/ts).
    - Remove deleted files from chrome/browser/resources/settings/BUILD.gn.
    - Clean up obsolete element exports in settings.ts.
    
    Bug: b:447113309
    Change-Id: Iee1b07f13b9ddfcf3442610f5b88d56ddbec7143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8145480
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Stanislav Mikheyev <mikhe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672615}

--- chrome/browser/resources/settings/settings.ts
+++ chrome/browser/resources/settings/settings.ts
@@ -72,8 +72,6 @@ export {SettingsAppearancePageElement, SystemTheme} from './appearance_page/appe
 export {SettingsAppearancePageIndexElement} from './appearance_page/appearance_page_index.js';
 export {HomeUrlInputElement} from './appearance_page/home_url_input.js';
 export {SettingsAutofillAiEntriesListElement} from './autofill_page/autofill_ai_entries_list.js';
-export {SettingsAutofillPageElement} from './autofill_page/autofill_page.js';
-export {SettingsAutofillPageIndexElement} from './autofill_page/autofill_page_index.js';
 export {PasswordCheckReferrer, PasswordManagerImpl, PasswordManagerPage} from './autofill_page/password_manager_proxy.js';
 export type {PasswordManagerProxy} from './autofill_page/password_manager_proxy.js';
 export {BaseMixin} from './base_mixin.js';
