6d31f10d73009925632bfaf03b7587d287b6e802
Alison Gale <agale@chromium.org>
2026-08-03 12:53:23 -0700

    [Vertical Tabs] Fully launch Vertical Tabs on ChromeOS
    Change-Id: Ic9c0d2dd1f048d10824e0edf155ecfd7791fc11e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8188154
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1672844}

--- chrome/browser/ui/tabs/features.cc
+++ chrome/browser/ui/tabs/features.cc
@@ -28,13 +28,7 @@ BASE_FEATURE(kTabSearchCjkWordBoundary, base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kVerticalTabs, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kVerticalTabsLaunch,
-#if BUILDFLAG(IS_CHROMEOS)
-             base::FEATURE_DISABLED_BY_DEFAULT
-#else
-             base::FEATURE_ENABLED_BY_DEFAULT
-#endif
-);
+BASE_FEATURE(kVerticalTabsLaunch, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE_PARAM(bool,
                    kVerticalTabsToggleInTabContextMenu,
                    &kVerticalTabsLaunch,
