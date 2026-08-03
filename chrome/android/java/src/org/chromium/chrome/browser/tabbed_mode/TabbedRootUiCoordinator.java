957961b216d9056bf506f860fc08d5ce03138b5d
Zhe Li <zheliooo@google.com>
2026-08-03 12:14:23 -0700

    [WindowManagerDeprecation] Highlight the History menu item for Recent Tabs IPH when submenus are enabled
    Demo: https://drive.google.com/file/d/1wKfmsDaYvJ5GwAHjA-pGdJgSRbYcE2NZ/view?usp=sharing
    Fixed: 539559239
    Change-Id: Ia52b730a472a33577734d3c2a134eee9889bb3b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8158939
    Auto-Submit: Zhe Li <zheliooo@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672821}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -1649,7 +1649,10 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
             } else if (MultiWindowUtils.shouldShowRecentTabsIph()) {
                 featureName = FeatureConstants.RECENT_TABS;
                 stringId = R.string.iph_recent_tabs_text;
-                menuId = R.id.recent_tabs_menu_id;
+                menuId =
+                        TabbedAppMenuPropertiesDelegate.isSubmenusEnabled(mActivity)
+                                ? R.id.history_parent_menu_id
+                                : R.id.recent_tabs_menu_id;
             }
 
             if (featureName != null) {
