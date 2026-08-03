6e79ddfd826ef7f131ed977e04a74d92988f2796
Guillaume Saulnier-Comte <gsaul@google.com>
2026-08-03 08:52:00 -0700

    [iOS][Actor] Enable NextIA in actor overlay tests
    Enable the kChromeNextIa feature flag for actor overlay configuration.
    
    Bug: 542111962
    Change-Id: I7fb7bfd7b68081485bc1e15dfce1e59d9fb37f9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8185858
    Auto-Submit: Guillaume Saulnier-Comte <gsaul@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Francis Beauchamp <fbeauchamp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1672696}

--- ios/chrome/browser/intelligence/actor/ui/test/actor_overlay_egtest.mm
+++ ios/chrome/browser/intelligence/actor/ui/test/actor_overlay_egtest.mm
@@ -5,6 +5,7 @@
 #import "base/functional/bind.h"
 #import "ios/chrome/browser/intelligence/actor/tools/test/actor_app_interface.h"
 #import "ios/chrome/browser/intelligence/features/features.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
@@ -58,6 +59,7 @@ std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
 - (AppLaunchConfiguration)appConfigurationForTestCase {
   AppLaunchConfiguration config;
   config.features_enabled.push_back(kActorTools);
+  config.features_enabled.push_back(kChromeNextIa);
   return config;
 }
 
