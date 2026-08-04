30d248d47b5f79c5f8d0fec749464bb0bce2b3ce
Mariam Ali <alimariam@google.com>
2026-08-04 06:51:23 -0700

    [pContext][Bling] Add help improve enhanced autofill page content
    This change adds the page content for help improve enhanced autofill.
    
    Screenshot: http://screen/3bp5jl712m870
    Bug: b:498577738
    Change-Id: I0c3d66f120f9451e6672b15f17854f6b8fe61969
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8158681
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Auto-Submit: Mariam Ali <alimariam@google.com>
    Commit-Queue: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673292}

--- ios/chrome/browser/settings/autofill/suggestions_from_gemini/ui/suggestions_from_gemini_help_improve_table_view_controller.mm
+++ ios/chrome/browser/settings/autofill/suggestions_from_gemini/ui/suggestions_from_gemini_help_improve_table_view_controller.mm
@@ -4,9 +4,58 @@
 
 #import "ios/chrome/browser/settings/autofill/suggestions_from_gemini/ui/suggestions_from_gemini_help_improve_table_view_controller.h"
 
+#import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_icon_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_header_footer_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
+#import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
+
+namespace {
+
+typedef NS_ENUM(NSInteger, SectionIdentifier) {
+  SectionIdentifierHelpingImprove = kSectionIdentifierEnumZero,
+  SectionIdentifierWhenUsed,
+  SectionIdentifierThingsToConsider,
+};
+
+typedef NS_ENUM(NSInteger, ItemType) {
+  ItemTypeHeader = kItemTypeEnumZero,
+  ItemTypeFooter,
+  ItemTypeLabel,
+  ItemTypeDetailIcon,
+};
+
+// Creates and returns a configured TableViewTextHeaderFooterItem.
+TableViewTextHeaderFooterItem* CreateHeaderItem(int messageId) {
+  TableViewTextHeaderFooterItem* header =
+      [[TableViewTextHeaderFooterItem alloc] initWithType:ItemTypeHeader];
+  header.text = l10n_util::GetNSString(messageId);
+  return header;
+}
+
+// Creates and returns a configured TableViewDetailIconItem for the help
+// improve subpage.
+TableViewDetailIconItem* HelpImproveDetailItem(NSInteger titleId,
+                                               Symbol symbol) {
+  TableViewDetailIconItem* detailItem =
+      [[TableViewDetailIconItem alloc] initWithType:ItemTypeDetailIcon];
+  detailItem.text = l10n_util::GetNSString(titleId);
+  detailItem.textNumberOfLines = 0;
+  detailItem.textFont =
+      [UIFont preferredFontForTextStyle:UIFontTextStyleFootnote];
+  detailItem.textColor = [UIColor colorNamed:kTextSecondaryColor];
+  detailItem.selectionStyle = UITableViewCellSelectionStyleNone;
+  detailItem.iconImage =
+      SymbolWithPointSize(symbol, kSettingsRootSymbolImagePointSize);
+  detailItem.iconTintColor = [UIColor colorNamed:kTextPrimaryColor];
+  return detailItem;
+}
+
+}  // namespace
 
 @implementation SuggestionsFromGeminiHelpImproveTableViewController {
   BOOL _settingsAreDismissed;
@@ -26,6 +75,66 @@
 
 - (void)loadModel {
   [super loadModel];
+  if (_settingsAreDismissed) {
+    return;
+  }
+
+  TableViewModel* model = self.tableViewModel;
+
+  // Helping improve section.
+  [model addSectionWithIdentifier:SectionIdentifierHelpingImprove];
+
+  TableViewTextItem* helpingImproveItem =
+      [[TableViewTextItem alloc] initWithType:ItemTypeLabel];
+  helpingImproveItem.text = l10n_util::GetNSString(
+      IDS_IOS_PERSONAL_CONTEXT_AUTOFILL_SETTINGS_HELPING_IMPROVE_TITLE);
+  helpingImproveItem.selectionStyle = UITableViewCellSelectionStyleNone;
+  [model addItem:helpingImproveItem
+      toSectionWithIdentifier:SectionIdentifierHelpingImprove];
+
+  TableViewTextHeaderFooterItem* helpingImproveFooter =
+      [[TableViewTextHeaderFooterItem alloc] initWithType:ItemTypeFooter];
+  helpingImproveFooter.text = l10n_util::GetNSString(
+      IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_QUALITY_LOGGING_SUBTITLE);
+  [model setFooter:helpingImproveFooter
+      forSectionWithIdentifier:SectionIdentifierHelpingImprove];
+
+  // When used section.
+  [model addSectionWithIdentifier:SectionIdentifierWhenUsed];
+  [model setHeader:
+             CreateHeaderItem(
+                 IDS_IOS_PERSONAL_CONTEXT_AUTOFILL_SETTINGS_WHEN_USED_TITLE)
+      forSectionWithIdentifier:SectionIdentifierWhenUsed];
+
+  [model addItem:HelpImproveDetailItem(
+                     IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_WHEN_USED_1,
+                     SymbolChartBarXAxis)
+      toSectionWithIdentifier:SectionIdentifierWhenUsed];
+
+  [model addItem:HelpImproveDetailItem(
+                     IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_WHEN_USED_2,
+                     SymbolMagnifyingglassSpark)
+      toSectionWithIdentifier:SectionIdentifierWhenUsed];
+
+  // Things to consider section.
+  [model addSectionWithIdentifier:SectionIdentifierThingsToConsider];
+  [model setHeader:
+             CreateHeaderItem(
+                 IDS_IOS_PERSONAL_CONTEXT_AUTOFILL_SETTINGS_THINGS_TO_CONSIDER_TITLE)
+      forSectionWithIdentifier:SectionIdentifierThingsToConsider];
+
+  [model addItem:HelpImproveDetailItem(
+                     IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_CONSIDER_1,
+                     SymbolLinkAction)
+      toSectionWithIdentifier:SectionIdentifierThingsToConsider];
+
+  [model addItem:HelpImproveDetailItem(
+                     IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_CONSIDER_2,
+                     SymbolPersonCropCircle)
+      toSectionWithIdentifier:SectionIdentifierThingsToConsider];
+
+  // TODO(crbug.com/541220712): Add the enterprise item based on the correct
+  // policy.
 }
 
 #pragma mark - SettingsControllerProtocol
--- ios/chrome/browser/settings/autofill/suggestions_from_gemini/ui/suggestions_from_gemini_help_improve_table_view_controller_unittest.mm
+++ ios/chrome/browser/settings/autofill/suggestions_from_gemini/ui/suggestions_from_gemini_help_improve_table_view_controller_unittest.mm
@@ -4,6 +4,11 @@
 
 #import "ios/chrome/browser/settings/autofill/suggestions_from_gemini/ui/suggestions_from_gemini_help_improve_table_view_controller.h"
 
+#import "base/apple/foundation_util.h"
+#import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_icon_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_header_footer_item.h"
+#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/legacy_chrome_table_view_controller_test.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "testing/gtest/include/gtest/gtest.h"
@@ -18,10 +23,17 @@ class SuggestionsFromGeminiHelpImproveTableViewControllerTest
   LegacyChromeTableViewController* InstantiateController() override {
     return [[SuggestionsFromGeminiHelpImproveTableViewController alloc] init];
   }
+
+  void CheckDetailIconItem(int section, int item, int expected_string_id) {
+    TableViewDetailIconItem* detail_item =
+        base::apple::ObjCCastStrict<TableViewDetailIconItem>(
+            GetTableViewItem(section, item));
+    EXPECT_NSEQ(l10n_util::GetNSString(expected_string_id), detail_item.text);
+  }
 };
 
 // Tests that the SuggestionsFromGeminiHelpImproveTableViewController is
-// correctly initialized.
+// correctly initialized and displays the details.
 TEST_F(SuggestionsFromGeminiHelpImproveTableViewControllerTest,
        TestInitialization) {
   CreateController();
@@ -32,7 +44,40 @@ TEST_F(SuggestionsFromGeminiHelpImproveTableViewControllerTest,
           IDS_IOS_PERSONAL_CONTEXT_AUTOFILL_SETTINGS_HELPING_IMPROVE_NOTICE_TITLE),
       controller().title);
 
-  EXPECT_EQ(0, NumberOfSections());
+  EXPECT_EQ(3, NumberOfSections());
+
+  // Section 0: Helping Improve
+  EXPECT_EQ(1, NumberOfItemsInSection(0));
+  TableViewModel* model = controller().tableViewModel;
+  EXPECT_EQ(nil, [model headerForSectionIndex:0]);
+
+  TableViewTextItem* textItem =
+      base::apple::ObjCCastStrict<TableViewTextItem>(GetTableViewItem(0, 0));
+  EXPECT_NSEQ(
+      l10n_util::GetNSString(
+          IDS_IOS_PERSONAL_CONTEXT_AUTOFILL_SETTINGS_HELPING_IMPROVE_TITLE),
+      textItem.text);
+  EXPECT_EQ(UITableViewCellSelectionStyleNone, textItem.selectionStyle);
+
+  TableViewTextHeaderFooterItem* footer0 =
+      base::apple::ObjCCastStrict<TableViewTextHeaderFooterItem>(
+          [model footerForSectionIndex:0]);
+  EXPECT_NSEQ(
+      l10n_util::GetNSString(
+          IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_QUALITY_LOGGING_SUBTITLE),
+      footer0.text);
+
+  // Section 1: When Used
+  EXPECT_EQ(2, NumberOfItemsInSection(1));
+  CheckDetailIconItem(1, 0, IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_WHEN_USED_1);
+  CheckDetailIconItem(1, 1, IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_WHEN_USED_2);
+
+  // Section 2: Things to Consider
+  EXPECT_EQ(2, NumberOfItemsInSection(2));
+  CheckDetailIconItem(2, 0, IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_CONSIDER_1);
+  CheckDetailIconItem(2, 1, IDS_SETTINGS_SUGGESTIONS_FROM_GEMINI_CONSIDER_2);
+  // TODO(crbug.com/541220712): Verify the enterprise item is added based on
+  // the correct policy.
 }
 
 }  // namespace
