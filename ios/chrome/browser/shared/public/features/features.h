25a71d53797719e262af5e2101066a5743007a65
Robbie Gibson <rkgibson@google.com>
2026-08-04 15:50:09 -0700

    Revert "[iOS] Remove cobrowse flag"
    This reverts commit 602b237e7fc167fc487ea18c3bfc6779138b9890.
    
    Reason for revert: Flag is not guarded by buildflag in tests
    
    Failure Link: https://luci-milo.appspot.com/ui/p/chrome/builders/ci/ios-rel-ready/72416/overview
    
    Original change's description:
    > [iOS] Remove cobrowse flag
    >
    > Bug: 540834497
    > Change-Id: Id10526b1885be34bf578ff5749265edf9094f666
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8180385
    > Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1673202}
    
    Bug: 540834497
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I1cb24778dec714d6f0da87240eb1916edb9a98b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8193279
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Harry Souders <harrysouders@google.com>
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Cr-Commit-Position: refs/heads/main@{#1673748}

--- ios/chrome/browser/shared/public/features/features.h
+++ ios/chrome/browser/shared/public/features/features.h
@@ -10,7 +10,6 @@
 #import "base/feature_list.h"
 #import "base/metrics/field_trial_params.h"
 #import "ios/chrome/browser/ntp/ui_bundled/feed_top_section/notifications_promo_view_constants.h"
-#import "ios/chrome/browser/shared/public/features/aim_cobrowse_buildflags.h"
 
 enum class FeedActivityBucket;
 
@@ -830,10 +829,8 @@ extern const base::FeatureParam<base::TimeDelta>
 // Whether the Soft Lock feature is enabled.
 bool IsIOSSoftLockEnabled();
 
-#if BUILDFLAG(IOS_ENABLE_AIM_COBROWSE)
 // Enables the AimCobrowse feature.
 BASE_DECLARE_FEATURE(kAimCobrowse);
-#endif
 
 // Returns true if the AimCobrowse feature is enabled.
 bool IsAimCobrowseEnabled();
