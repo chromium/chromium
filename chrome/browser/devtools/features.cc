e0d8e2d32bcef283948c0ba609e0613be78b1c2a
Alex N. Jose <alexnj@chromium.org>
2026-08-04 16:02:58 -0700

    [DevTools] Enable Durable Messages by default
    This CL enables the Durable Messages feature by default. Durable
    Messages allows the DevTools Network panel to preserve network
    response bodies across cross-document navigations.
    
    This feature has been tested in preparation for the 100% stable
    launch. This CL updates ToT with the pre-requisite of enabling
    the feature by default, before Finch can be ramped up to 100%.
    
    The backend feature (`network::features::kDurableMessages`)
    is already enabled by default. This CL flips the corresponding
    frontend experiment flag (`features::kDevToolsEnableDurableMessages`)
    to be on by default.
    
    Ref: cl/959104477
    Bug: 475545872, 332657101, 475530966, 40254754
    Change-Id: I70ac2e31869eb1f4170b107867dbc08a8b3778a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8197284
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673757}

--- chrome/browser/devtools/features.cc
+++ chrome/browser/devtools/features.cc
@@ -197,7 +197,7 @@ const base::FeatureParam<bool> kDevToolsGdpProfilesStarterBadgeEnabled{
     /*default_value=*/true};
 
 // Whether Network panel should use Durable Messages to preserve network bodies.
-BASE_FEATURE(kDevToolsEnableDurableMessages, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kDevToolsEnableDurableMessages, base::FEATURE_ENABLED_BY_DEFAULT);
 
 // If enabled, allows starting remote debugging in a running Chrome instance.
 BASE_FEATURE(kDevToolsAcceptDebuggingConnections,
