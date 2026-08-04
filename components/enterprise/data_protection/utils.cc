4525c3d6ed157a551a71418600b2977278cfab0e
Adam Kassi Lahlou <adamkl@google.com>
2026-08-04 09:12:23 -0700

    [M152] [Watermark Timestamps] Pass timestamp_timezone to Watermark String
    Pass time zone preference through DataProtectionPageUserData and update
    the functions in watermark/utils.cc to have it applied into the final
    watermark string.
    
    Modify watermark browser tests to cover preference to settings
    retrieval, and IANA timezone formatting.
    
    (cherry picked from commit 065ec126fa7df9dd65ffbc16bc6283ab5e095869)
    
    Fixed: 534486558, 534487117, 534487269
    Fixed: 541328995
    Change-Id: I07ea0205afa24bab2aa1948fcbcd70cbe43b0685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8105782
    Reviewed-by: Sebastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Commit-Queue: Adam Kassi Lahlou <adamkl@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1669468}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8191657
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7977@{#583}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- components/enterprise/data_protection/utils.cc
+++ components/enterprise/data_protection/utils.cc
@@ -9,7 +9,9 @@
 #include "base/no_destructor.h"
 #include "base/strings/strcat.h"
 #include "base/time/time.h"
+#include "components/enterprise/connectors/core/connectors_prefs.h"
 #include "components/enterprise/data_protection/features.h"
+#include "third_party/icu/source/i18n/unicode/timezone.h"
 
 namespace enterprise_data_protection {
 
@@ -40,7 +42,8 @@ const UrlSettings& UrlSettings::None() {
 
 UrlSettings GetUrlSettings(
     const std::string& identifier,
-    const safe_browsing::RTLookupResponse* rt_lookup_response) {
+    const safe_browsing::RTLookupResponse* rt_lookup_response,
+    const std::optional<std::string>& timestamp_timezone) {
   UrlSettings settings;
   if (!rt_lookup_response) {
     return settings;
@@ -53,7 +56,8 @@ UrlSettings GetUrlSettings(
 
     const auto& rule = threat_info.matched_url_navigation_rule();
     if (settings.watermark_text.empty()) {
-      settings.watermark_text = GetWatermarkString(identifier, rule);
+      settings.watermark_text =
+          GetWatermarkString(identifier, rule, timestamp_timezone);
     }
     if (settings.allow_screenshots) {
       settings.allow_screenshots = !rule.block_screenshot();
@@ -63,14 +67,25 @@ UrlSettings GetUrlSettings(
   return settings;
 }
 
-std::string FormatWatermarkTimestamp(const base::Time& time) {
-  return base::UnlocalizedTimeFormatWithPattern(time,
-                                                "yyyy-MM-dd'T'HH:mm:ssxxx");
+std::string FormatWatermarkTimestamp(
+    const base::Time& time,
+    const std::optional<std::string>& timestamp_timezone) {
+  std::unique_ptr<icu::TimeZone> icu_timezone;
+  if (timestamp_timezone.has_value() &&
+      *timestamp_timezone !=
+          enterprise_connectors::kWatermarkStyleTimestampTimezoneDefault) {
+    icu_timezone.reset(icu::TimeZone::createTimeZone(
+        icu::UnicodeString::fromUTF8(*timestamp_timezone)));
+  }
+
+  return base::UnlocalizedTimeFormatWithPattern(
+      time, "yyyy-MM-dd'T'HH:mm:ssxxx", icu_timezone.get());
 }
 
 std::string GetWatermarkString(
     const std::string& identifier,
-    const safe_browsing::MatchedUrlNavigationRule& rule) {
+    const safe_browsing::MatchedUrlNavigationRule& rule,
+    const std::optional<std::string>& timestamp_timezone) {
   if (!rule.has_watermark_message()) {
     return std::string();
   }
@@ -81,7 +96,8 @@ std::string GetWatermarkString(
   std::string watermark_text = base::StrCat(
       {identifier, "\n",
        base::FeatureList::IsEnabled(kEnableWatermarkTimestampTimezone)
-           ? FormatWatermarkTimestamp(TimestampToTime(watermark.timestamp()))
+           ? FormatWatermarkTimestamp(TimestampToTime(watermark.timestamp()),
+                                      timestamp_timezone)
            : base::TimeFormatAsIso8601(
                  TimestampToTime(watermark.timestamp()))});
 
