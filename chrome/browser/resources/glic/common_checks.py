4a3e3d2d4dd4d8473f988d5967e5800fa0c5826e
Dan Harrington <harringtond@chromium.org>
2026-08-04 15:14:35 -0700

    Add glic_enums.mojom to monitored_files in common_checks.py
    Add chrome/common/glic_enums.mojom to monitored_files in common_checks.py
    so that changes to glic_enums.mojom trigger presubmit checks for the generated API.
    
    Bug: b:540273604
    Change-Id: I0f9baf26552a69042ff6f76b5c6543ab6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8191096
    Commit-Queue: Nick Birnie <birnie@google.com>
    Reviewed-by: Nick Birnie <birnie@google.com>
    Auto-Submit: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673714}

--- chrome/browser/resources/glic/common_checks.py
+++ chrome/browser/resources/glic/common_checks.py
@@ -20,6 +20,7 @@ def GlicCommonChecks(input_api, output_api):
 def _CheckGlicGeneratedApi(input_api, output_api):
     monitored_files = (
         'chrome/common/actor_webui.mojom',
+        'chrome/common/glic_enums.mojom',
         'chrome/browser/glic/host/glic.mojom',
         'chrome/browser/resources/glic/glic_api_impl/generate.py',
         'chrome/browser/resources/glic/glic_api/glic_api.ts',
