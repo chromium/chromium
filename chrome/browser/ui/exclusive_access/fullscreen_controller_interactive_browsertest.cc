9febf222f554d27ae9fdaa53e2f0a56fcceab955
Camillia Smith Barnes <cammie@chromium.org>
2026-08-04 11:36:41 -0700

    Gardener: Disable BrowserFullscreenContentFullscreenSwapDisplay on Mac
    Disable
    MultiScreenFullscreenControllerInteractiveTest.BrowserFullscreenContentFullscreenSwapDisplay
    on Mac.
    
    Bug: 542615039
    Change-Id: Ied1b6e209f5617c7d5f8d14cf4a30bc8e946a3d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8196925
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Auto-Submit: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Owners-Override: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1673534}

--- chrome/browser/ui/exclusive_access/fullscreen_controller_interactive_browsertest.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_controller_interactive_browsertest.cc
@@ -1738,13 +1738,14 @@ IN_PROC_BROWSER_TEST_F(MAYBE_MultiScreenFullscreenControllerInteractiveTest,
 #endif
 }
 
+// TODO(crbug.com/542615039): Disabled on Mac.
 // TODO(crbug.com/40111905): Disabled on Windows, where views::FullscreenHandler
 // implements fullscreen by directly obtaining MONITORINFO, ignoring the mocked
 // display::Screen configuration used in this test. Disabled on Linux, where the
 // window server's async handling of the fullscreen window state may transition
 // the window into fullscreen on the actual (non-mocked) display bounds before
 // or after the window bounds checks, yielding flaky results.
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_CHROMEOS)
 #define MAYBE_BrowserFullscreenContentFullscreenSwapDisplay \
   BrowserFullscreenContentFullscreenSwapDisplay
 #else
