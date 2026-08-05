9edeae25431c3d474bf56a924a3cf01e71bd2d8f
Evan Luo <evanluo@google.com>
2026-08-05 12:45:01 -0700

    [bookmarks] Fix infinite back navigation loop in bookmarks manager
    Screen recording:
    http://screencast/cast/NjA5MzM0ODE0MTAwNjg0OHwyOTQ4MGMwMy05Yw
    
    Bug: 539355592
    Change-Id: Id3afc656a4efcd5e600f75ab51249fb1be29eeaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8205104
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Evan Luo <evanluo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674403}

--- chrome/browser/bookmarks/android/junit/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediatorTest.java
+++ chrome/browser/bookmarks/android/junit/src/org/chromium/chrome/browser/bookmarks/BookmarkManagerMediatorTest.java
@@ -2778,6 +2778,24 @@ public class BookmarkManagerMediatorTest {
         verify(mNativePage).onStateChange("chrome-native://bookmarks/folder/6", true);
     }
 
+    @Test
+    public void testUpdateForUrl_popsStateStack() {
+        finishLoading();
+        mMediator.openFolder(mMobileFolderId);
+        mMediator.openFolder(mFolderId1);
+        mMediator.openFolder(mFolderId2);
+        // Initially, the stack should contain root folder (MobileBookmarks), Folder1, and Folder2.
+        assertEquals(3, mMediator.getStateStackForTesting().size());
+
+        // Simulate tab back navigation to Folder1 by calling updateForUrl.
+        mMediator.updateForUrl("chrome-native://bookmarks/folder/" + mFolderId1.getId());
+
+        // The stack should now contain root folder and Folder1. Folder2 should be popped.
+        assertEquals(2, mMediator.getStateStackForTesting().size());
+        assertEquals(BookmarkUiMode.FOLDER, mMediator.getCurrentUiMode());
+        assertEquals(mFolderId1, mMediator.getStateStackForTesting().peekLast().mFolder);
+    }
+
     private void verifyMenuListItemTitles(ModelList modelList, int... expectedTitleIds) {
         assertEquals(expectedTitleIds.length, modelList.size());
         for (int i = 0; i < expectedTitleIds.length; ++i) {
