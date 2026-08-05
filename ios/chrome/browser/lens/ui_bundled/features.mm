bf05a67a179746df1addae9e92240b76663783fe
Charles Yang <cmyang@google.com>
2026-08-05 15:06:30 -0700

    [Lens] Disable LensFollowupsFullHeightEnabled by default
    Was accidentally enabled by default in https://chromium-review.git.corp.google.com/c/chromium/src/+/8141424.
    
    Bug: 521408983
    Change-Id: I47ff635cfffc33066f219f87a83261f98c919af4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8206844
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Charles Yang <cmyang@google.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674537}

--- ios/chrome/browser/lens/ui_bundled/features.mm
+++ ios/chrome/browser/lens/ui_bundled/features.mm
@@ -82,4 +82,5 @@ BASE_FEATURE(kLensStreamServiceWebChannelTransportEnabled,
 
 BASE_FEATURE(kLensFilterToggleEnabled, base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kLensFollowupsFullHeightEnabled, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kLensFollowupsFullHeightEnabled,
+             base::FEATURE_DISABLED_BY_DEFAULT);
