6784b15e6e96efaae0011a1b24fca58e74318651
Joe Peplowski <joepeplowski@google.com>
2026-08-05 07:28:17 -0700

    [iOS] Ignore saving the active tab if tab grid is visible
    If we opened the Floaty on the tab grid, the active tab was getting
    saved in `attached_tabs_` with a `kBlocked` computation state. If the
    user had more than one tab open and went to the tab grid to switch to
    another attached tab, this could result in the original tab being stuck
    in the `kBlocked` computation state, meaning it would never appear in
    the Floaty and would unselect itself after being selected in the Tab
    Picker.
    
    This CL fixes that issue by not storing the active tab in
    `attached_tabs` when we open the Floaty on the tab grid.
    
    Bug: 540048666
    Change-Id: I57fdc88375d0c7e0c1370162f58d5affd3af69af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8179422
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674129}

--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.h
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.h
@@ -318,6 +318,9 @@ class GeminiBrowserAgent : public BrowserUserData<GeminiBrowserAgent>,
   // Returns true if the omnibox is focused.
   bool IsOmniboxFocused() const;
 
+  // Returns true if tab grid is currently visible.
+  bool IsTabGridVisible() const;
+
   // Returns true if the keyboard update should be ignored.
   bool ShouldIgnoreKeyboardUpdate() const;
 
--- ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_browser_agent.mm
@@ -886,8 +886,7 @@ CGFloat GeminiBrowserAgent::GetFloatyOffset() {
   CGFloat max_bottom_inset = 0;
 
   SceneState* scene_state = browser_->GetSceneState();
-  if (IsChromeNextIaEnabled() && scene_state &&
-      scene_state.tabGridState.tabGridVisible) {
+  if (IsChromeNextIaEnabled() && IsTabGridVisible()) {
     if (scene_state.layoutState.appBarPosition == AppBarPosition::kBottom) {
       max_bottom_inset = AppBarHeightPortrait();
     } else {
@@ -1800,6 +1799,11 @@ bool GeminiBrowserAgent::IsOmniboxFocused() const {
   return omnibox_agent && omnibox_agent->IsOmniboxFocused();
 }
 
+bool GeminiBrowserAgent::IsTabGridVisible() const {
+  SceneState* scene_state = browser_->GetSceneState();
+  return scene_state && scene_state.tabGridState.tabGridVisible;
+}
+
 bool GeminiBrowserAgent::ShouldIgnoreKeyboardUpdate() const {
   bool is_expanded_not_thinking =
       last_shown_view_state_ == ios::provider::GeminiViewState::kExpanded &&
@@ -1976,7 +1980,8 @@ void GeminiBrowserAgent::PropagatePageContextToProvider(
   }
 
   // Save the new active web state to attached tabs.
-  if (active_web_state && IsGeminiMultiTabContextEnabled()) {
+  if (active_web_state && !IsTabGridVisible() &&
+      IsGeminiMultiTabContextEnabled()) {
     attached_tabs_[active_web_state->GetUniqueIdentifier()] =
         active_page_context;
   }
