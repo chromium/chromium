68623016d69019cf897d4f14a2e85ace321e898c
Kartar Singh <kartarsingh@google.com>
2026-08-05 03:57:29 -0700

    Revert "Block WebAuthn requests on pages with SSL errors"
    This reverts commit e84aaf8b1e016ff23ddc3e4d6e9e5d5e7ec6f35f.
    
    Reason for revert: The newly added test fails on CI builders:
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-14-arm64-rel
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-automotive-12l-x64-rel-tests
    
    First failure Links:
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-14-arm64-rel/18670/overview
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-automotive-12l-x64-rel-tests/22306/overview
    
    Original change's description:
    > Block WebAuthn requests on pages with SSL errors
    >
    > AwContentBrowserClient inherits
    > ContentBrowserClient::IsSecurityLevelAcceptableForWebAuthn which
    > unconditionally returns true, allowing WebAuthn requests to proceed
    > without considering TLS errors.
    >
    > This CL overrides IsSecurityLevelAcceptableForWebAuthn in
    > AwContentBrowserClient to check the security level similar to what
    > Chrome browser does. This adds an end-to-end test verifying WebAuthn is
    > blocked on sites with SSL errors.
    >
    > TAG=agy
    > CONV=c1aad737-b624-4e3e-8c12-a5dd34c64316
    >
    > Fixed: 507219126
    > Test: run_webview_instrumentation_test_apk -f WebAuthnTest.*
    > Change-Id: I977fe7ec62450f2ab6ae93458a2bd6d62213315e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8178688
    > Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    > Reviewed-by: Rupert Wiser <bewise@chromium.org>
    > Commit-Queue: Rupert Wiser <bewise@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1673924}
    
    Bug: 507219126
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I24a65ed3fc441cb8081e0874460b6c0cf4467db5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8202345
    Reviewed-by: Rupert Wiser <bewise@chromium.org>
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Owners-Override: Kartar Singh <kartarsingh@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1674022}

--- android_webview/browser/aw_content_browser_client.h
+++ android_webview/browser/aw_content_browser_client.h
@@ -108,9 +108,6 @@ class AwContentBrowserClient : public content::ContentBrowserClient {
       bool strict_enforcement,
       base::OnceCallback<void(content::CertificateRequestResultType)> callback)
       override;
-  bool IsSecurityLevelAcceptableForWebAuthn(
-      content::RenderFrameHost* rfh,
-      const url::Origin& caller_origin) override;
   base::OnceClosure SelectClientCertificate(
       content::BrowserContext* browser_context,
       int process_id,
--- android_webview/javatests/src/org/chromium/android_webview/test/WebAuthnTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/WebAuthnTest.java
@@ -4,16 +4,7 @@
 
 package org.chromium.android_webview.test;
 
-import android.app.PendingIntent;
-import android.net.Uri;
-import android.os.Build;
-import android.os.ResultReceiver;
-
-import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.SmallTest;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -31,14 +22,8 @@ import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
-import org.chromium.blink.mojom.PublicKeyCredentialRequestOptions;
-import org.chromium.components.webauthn.AuthenticationContextProvider;
-import org.chromium.components.webauthn.Fido2ApiCallHelper;
 import org.chromium.components.webauthn.WebauthnMode;
-import org.chromium.components.webauthn.cred_man.CredManSupportProvider;
 import org.chromium.content_public.common.ContentSwitches;
-import org.chromium.net.test.EmbeddedTestServer;
-import org.chromium.net.test.ServerCertificate;
 import org.chromium.net.test.util.TestWebServer;
 import org.chromium.url.GURL;
 
@@ -47,23 +32,6 @@ import org.chromium.url.GURL;
 @CommandLineFlags.Add({ContentSwitches.HOST_RESOLVER_RULES + "=MAP * 127.0.0.1"})
 @Batch(Batch.PER_CLASS)
 public class WebAuthnTest {
-    private static class TestFido2ApiCallHelper extends Fido2ApiCallHelper {
-        public boolean mGetAssertionCalled;
-
-        @Override
-        public void invokeFido2GetAssertion(
-                AuthenticationContextProvider authenticationContextProvider,
-                PublicKeyCredentialRequestOptions options,
-                Uri uri,
-                byte[] clientDataHash,
-                ResultReceiver resultReceiver,
-                OnSuccessListener<PendingIntent> successCallback,
-                OnFailureListener failureCallback) {
-            mGetAssertionCalled = true;
-            failureCallback.onFailure(new Exception("MOCK_FIDO2_API_INVOKED"));
-        }
-    }
-
     @Rule public AwActivityTestRule mActivityTestRule = new AwActivityTestRule();
 
     private TestAwContentsClient mContentsClient;
@@ -188,69 +156,6 @@ public class WebAuthnTest {
         mAwSettings.setWebauthnSupport(WebauthnMode.BROWSER);
 
         histogramWatcher.assertExpected();
-    }
-
-    @Test
-    @SmallTest
-    @Feature({"AndroidWebView"})
-    public void testWebAuthnBlockedOnSslError() throws Throwable {
-        CredManSupportProvider.setupForTesting(Build.VERSION_CODES.TIRAMISU, false);
-        TestFido2ApiCallHelper testHelper = new TestFido2ApiCallHelper();
-        Fido2ApiCallHelper.overrideInstanceForTesting(testHelper);
-
-        EmbeddedTestServer testServer =
-                EmbeddedTestServer.createAndStartHTTPSServer(
-                        InstrumentationRegistry.getInstrumentation().getContext(),
-                        ServerCertificate.CERT_MISMATCHED_NAME);
-        try {
-            mAwSettings.setJavaScriptEnabled(true);
-            mAwSettings.setWebauthnSupport(WebauthnMode.APP);
-            mContentsClient.setAllowSslError(true);
-
-            final String pageUrl =
-                    testServer.getURLWithHostName(
-                            "a.test", "/android_webview/test/data/hello_world.html");
-            mActivityTestRule.loadUrlSync(
-                    mAwContents, mContentsClient.getOnPageFinishedHelper(), pageUrl);
-
-            mActivityTestRule.executeJavaScriptAndWaitForResult(
-                    mAwContents,
-                    mContentsClient,
-                    """
-                    window.webauthnResult = 'PENDING';
-                    navigator.credentials.get({
-                      publicKey: {
-                        challenge: new Uint8Array([1, 2, 3, 4]),
-                        timeout: 500,
-                        rpId: 'a.test'
-                      }
-                    }).then(
-                      () => { window.webauthnResult = 'SUCCESS'; },
-                      (e) => { window.webauthnResult = e.name + ': ' + e.message; }
-                    );
-                    """);
-
-            AwActivityTestRule.pollInstrumentationThread(
-                    () -> {
-                        String val =
-                                mActivityTestRule.executeJavaScriptAndWaitForResult(
-                                        mAwContents, mContentsClient, "window.webauthnResult");
-                        return !"\"PENDING\"".equals(val) && !"null".equals(val);
-                    });
-
-            String result =
-                    mActivityTestRule.executeJavaScriptAndWaitForResult(
-                            mAwContents, mContentsClient, "window.webauthnResult");
-
-            Assert.assertFalse(
-                    "FIDO2 API should not be invoked for pages with SSL errors",
-                    testHelper.mGetAssertionCalled);
-            Assert.assertTrue(
-                    "WebAuthn should be blocked due to SSL error, but got: " + result,
-                    result.contains("NotAllowedError") && result.contains("certificate errors"));
-        } finally {
-            testServer.stopAndDestroyServer();
-        }
     }
 
     private static boolean isSecureDomain(GURL url) {
