b0708da79fb74685936f349adcbfd67500ee2149
Mark Schillaci <mschillaci@google.com>
2026-08-06 10:08:20 -0700

    [Gardener] Disable CanCreateAndCreateWithManifestGemma4 in AISummarizerManifestTest
    Bug: 543163435
    Change-Id: If88421b8ea2fed1fdf35a0f430f424558eae6492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8217267
    Commit-Queue: Jack Thiesen <jthiesen@chromium.org>
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Jack Thiesen <jthiesen@chromium.org>
    Owners-Override: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Gazal Agarwal <agazal@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675060}

--- chrome/browser/ai/ai_summarizer_unittest.cc
+++ chrome/browser/ai/ai_summarizer_unittest.cc
@@ -1306,7 +1306,9 @@ TEST_F(AISummarizerManifestTest, InputLimitExceededErrorSpeedPreference) {
             blink::mojom::kTinyModelMaxInputTokenSize);
 }
 
-TEST_F(AISummarizerManifestTest, CanCreateAndCreateWithManifestGemma4) {
+// TODO(crbug.com/543163435): Flaky.
+TEST_F(AISummarizerManifestTest,
+       DISABLED_CanCreateAndCreateWithManifestGemma4) {
   version_info::Channel channel = chrome::GetChannel();
   if (channel != version_info::Channel::CANARY &&
       channel != version_info::Channel::DEV &&
