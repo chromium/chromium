097a461df43b0ad0aefed851e1c8b4f0455a82ce
Charles Cai <charlesyc@google.com>
2026-08-06 11:58:15 -0700

    Updated delete and send button in Omnibox
    This CL updates the icon for delete button, and changed the background
    of the navigation button to follow the plus button's oval shape on
    desktop.
    
    This change is applied to all platforms as mentioned in the bug.
    
    Desktop (before & after):
    https://screenshot-v2.corp.google.com/image/C4MADv2RzAUgpJV.png
    https://screenshot-v2.corp.google.com/image/9sCL2UBe5TLzbh8.png
    
    Phone (before & after):
    https://screenshot-v2.corp.google.com/image/9B4UvnYrWnhDn4u.png
    https://screenshot-v2.corp.google.com/image/Bw3CuVdHux8Lxpz.png
    
    NO_IFTTT=Only updated delete button icon drawable.
    
    Bug: 534377053
    Change-Id: I19cc2cb51080760cca70bce944acc408a20804d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8206205
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Charles Cai <charlesyc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1675166}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchRenderTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchRenderTest.java
@@ -82,7 +82,7 @@ public class TabSwitcherSearchRenderTest {
     @Rule
     public ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(20) // Adjust suggestion min height.
+                    .setRevision(21) // Updated delete/close button icon.
                     .setBugComponent(Component.UI_BROWSER_MOBILE_TAB_SWITCHER)
                     .build();
 
