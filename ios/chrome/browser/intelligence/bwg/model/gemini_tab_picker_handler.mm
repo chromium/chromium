320309f8b5b8be91fcd4cf3c8f8a7ce29324c780
Joe Peplowski <joepeplowski@google.com>
2026-08-06 07:38:04 -0700

    [iOS] Show snackbar messages on top of Tab Picker
    This CL fixes a bug where tab picker messages were appearing invisibly
    on the Chrome window which was beneath the Gemini window. It does so by
    creating a SnackbarView on the tab picker's window itself instead of
    relying on the Chrome snackbar handler, which can only present on the
    Chrome window.
    
    Fixed: 522850674
    Change-Id: I1779f91708d7325dbc2ab7f34ddf9523077e1803
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8200089
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Commit-Queue: Joe Peplowski <joepeplowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674960}

--- ios/chrome/browser/intelligence/bwg/model/gemini_tab_picker_handler.mm
+++ ios/chrome/browser/intelligence/bwg/model/gemini_tab_picker_handler.mm
@@ -11,8 +11,11 @@
 #import "ios/chrome/browser/shared/public/commands/snackbar_commands.h"
 #import "ios/chrome/browser/shared/public/commands/tab_picker_commands.h"
 #import "ios/chrome/browser/shared/public/snackbar/snackbar_message.h"
+#import "ios/chrome/browser/snackbar/ui_bundled/ui/snackbar_view.h"
+#import "ios/chrome/browser/snackbar/ui_bundled/ui/snackbar_view_delegate.h"
 #import "ios/chrome/browser/tab_picker/public/tab_picker_logger.h"
 #import "ios/chrome/browser/tab_picker/public/tab_picker_snackbar_presenter.h"
+#import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util.h"
 
@@ -23,16 +26,28 @@ constexpr NSUInteger kMaxTabAttachmentCount = 10;
 
 }  // namespace
 
-@interface GeminiTabPickerHandler () <TabPickerSnackbarPresenter,
-                                      TabPickerLogger>
+@interface GeminiTabPickerHandler () <SnackbarViewDelegate,
+                                      TabPickerLogger,
+                                      TabPickerSnackbarPresenter>
 @end
 
-@implementation GeminiTabPickerHandler
+@implementation GeminiTabPickerHandler {
+  // The view controller that presented the Tab Picker modal.
+  __weak UIViewController* _presentingViewController;
+  // The active snackbar view presented on the tab picker window.
+  SnackbarView* _snackbarView;
+}
+
+- (void)dealloc {
+  [self dismissSnackbarAnimated:NO];
+}
 
 #pragma mark - GeminiTabPickerDelegate
 
 - (void)openTabPickerFromViewController:
     (UIViewController*)presentingViewController {
+  _presentingViewController = presentingViewController;
+
   TabPickerParams* params =
       [[TabPickerParams alloc] initWithSnackbarPresenter:self];
   params.baseViewController = presentingViewController;
@@ -86,13 +101,55 @@ constexpr NSUInteger kMaxTabAttachmentCount = 10;
                                   IDS_IOS_GEMINI_TAB_PICKER_CANT_BE_SHARED)];
 }
 
+#pragma mark - SnackbarViewDelegate
+
+- (void)snackbarViewDidTapActionButton:(SnackbarView*)snackbarView {
+  [self dismissSnackbarAnimated:YES];
+}
+
+- (void)snackbarViewDidRequestDismissal:(SnackbarView*)snackbarView {
+  [self dismissSnackbarAnimated:YES];
+}
+
 #pragma mark - Private
 
-// Displays a snackbar with the given `title`. `bottomOffset` is set to 0 to
-// override default toolbar padding, since the Tab Picker covers the toolbar.
+// Displays a snackbar with the given `title` on the window presenting the Tab
+// Picker.
+// TODO(crbug.com/509898861): Once bottom sheet migration is complete, we can
+// remove `_snackbarView` and use the Chrome Snackbar Handler instead.
 - (void)showSnackbarWithTitle:(NSString*)title {
+  [self dismissSnackbarAnimated:NO];
+
+  UIWindow* window = _presentingViewController.view.window;
+  if (!window) {
+    return;
+  }
+
   SnackbarMessage* message = [[SnackbarMessage alloc] initWithTitle:title];
-  [self.snackbarHandler showSnackbarMessage:message bottomOffset:0];
+  _snackbarView = [[SnackbarView alloc] initWithMessage:message];
+  _snackbarView.delegate = self;
+  _snackbarView.translatesAutoresizingMaskIntoConstraints = NO;
+
+  [window addSubview:_snackbarView];
+  AddSameConstraints(_snackbarView, window);
+  [_snackbarView presentAnimated:YES completion:nil];
+}
+
+// Dismisses the active snackbar view and removes it from the window hierarchy.
+// TODO(crbug.com/509898861): Once bottom sheet migration is complete, we can
+// remove `_snackbarView` and use the Chrome Snackbar Handler instead.
+- (void)dismissSnackbarAnimated:(BOOL)animated {
+  if (!_snackbarView) {
+    return;
+  }
+  SnackbarView* snackbarView = _snackbarView;
+  _snackbarView.delegate = nil;
+  _snackbarView = nil;
+
+  [snackbarView dismissAnimated:animated
+                     completion:^{
+                       [snackbarView removeFromSuperview];
+                     }];
 }
 
 @end
