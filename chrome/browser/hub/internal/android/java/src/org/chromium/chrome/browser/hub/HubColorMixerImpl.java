c5dbc5165c84edf770d94675d3603d0584201809
Fiaz Muhammad <mfiaz@google.com>
2026-08-06 07:03:36 -0700

    [Hub] Store OverviewModeAlphaObserver in HubColorMixerImpl
    Avoid creating new lambdas in getOverviewModeAlphaObserver().
    
    Bug: 543348811
    Change-Id: I1045791b958fb272d9148e24d46d8c6be0e518fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8208324
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1674942}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubColorMixerImpl.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubColorMixerImpl.java
@@ -44,6 +44,8 @@ public class HubColorMixerImpl implements HubColorMixer {
     private final Callback<Boolean> mOnHubVisibilityObserver = this::onHubVisibilityChange;
     private final Callback<Pane> mOnFocusedPaneObserver =
             (Callback<Pane>) this::onFocusedPaneChange;
+    private final OverviewModeAlphaObserver mOverviewModeAlphaObserver =
+            this::onOverviewModeAlphaChanged;
     private final Callback<@Nullable ColorBlendProgress> mOnSwipeAnimationProgressObserver =
             this::onSwipeAnimationProgressChanged;
     private final NonNullObservableSupplier<Boolean> mHubVisibilitySupplier;
@@ -161,11 +163,13 @@ public class HubColorMixerImpl implements HubColorMixer {
 
     @Override
     public OverviewModeAlphaObserver getOverviewModeAlphaObserver() {
-        return alpha -> {
-            mOverviewColorAlpha = (float) alpha;
-            @ColorInt int color = mOverviewColorSupplier.get();
-            processOverviewColor(color, mOverviewColorAlpha);
-        };
+        return mOverviewModeAlphaObserver;
+    }
+
+    private void onOverviewModeAlphaChanged(double alpha) {
+        mOverviewColorAlpha = (float) alpha;
+        @ColorInt int color = mOverviewColorSupplier.get();
+        processOverviewColor(color, mOverviewColorAlpha);
     }
 
     private void onSwipeAnimationProgressChanged(@Nullable ColorBlendProgress progress) {
