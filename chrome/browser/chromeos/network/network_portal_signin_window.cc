76a71291c2e9585ce64e5c2ef72a6a7a92e95684
Georg Neis <neis@chromium.org>
2026-08-05 20:25:19 -0700

    ash: Remove some stale browser includes/DEPS
    Bug: b:369688254
    Change-Id: I4342907c0832844cd8d72b91e11c8b16157a41ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8198568
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Di Wu <diwux@google.com>
    Commit-Queue: Di Wu <diwux@google.com>
    Auto-Submit: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1674702}

--- chrome/browser/chromeos/network/network_portal_signin_window.cc
+++ chrome/browser/chromeos/network/network_portal_signin_window.cc
@@ -6,7 +6,6 @@
 
 #include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/browser_window/public/global_browser_collection.h"
 #include "chrome/browser/ui/dialogs/browser_dialogs.h"
