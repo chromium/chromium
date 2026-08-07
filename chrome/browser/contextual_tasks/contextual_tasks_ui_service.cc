c9b952596b95f4408adb699bdc4f737a0a2a33f3
Diana OuYang <dianaou@google.com>
2026-08-06 18:18:37 -0700

    [M151] Preserve zero-state task association
    Original change's description:
    > Preserve zero-state task association
    >
    > Prevents active tabs from prematurely losing zero-state task
    > associations during navigation while ensuring proper tab disassociation
    > occurs when the side panel is closed.
    >
    > Bug: b:540873936
    > Demo: https://screencast.googleplex.com/cast/NDYwMzIyOTE5NDE1ODA4MHwxYzkxODU0Yi0xNg
    > Change-Id: Ide07fd2de6ab471af8fc5160f687830fbe11cc65
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8177968
    > Auto-Submit: Diana OuYang <dianaou@google.com>
    > Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    > Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    > Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1673485}
    
    (cherry picked from commit 64c752f388b07f619803a5b189d2769ae43fe7cd)
    
    Bug: 543129242,b:540873936
    Change-Id: Ide07fd2de6ab471af8fc5160f687830fbe11cc65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8208762
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/branch-heads/7922@{#2677}
    Cr-Branched-From: 059c884787b1bc15faa5afe5250f7d3a7e0ef64a-refs/heads/main@{#1654411}

--- chrome/browser/contextual_tasks/contextual_tasks_ui_service.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_ui_service.cc
@@ -2390,6 +2390,8 @@ void ContextualTasksUiService::OnTaskChanged(
       for (const auto& id : tab_ids) {
         contextual_tasks_service_->AssociateTabWithTask(final_task_id, id);
       }
+    } else {
+      contextual_tasks_service_->AssociateTabWithTask(final_task_id, active_id);
     }
 
     controller->OnTaskChanged(web_contents, final_task_id);
