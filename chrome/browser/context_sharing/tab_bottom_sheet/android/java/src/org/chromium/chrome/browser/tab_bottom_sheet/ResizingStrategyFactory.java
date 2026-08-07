de7db049b49650dd168490cc3fc74e4fb2ec989d
Fiaz Muhammad <mfiaz@google.com>
2026-08-07 06:29:47 -0700

    [M152] [TabBottomSheet] Wire DragDirectionResizingStrategy
    Original change's description:
    > [TabBottomSheet] Wire DragDirectionResizingStrategy
    >
    > - Add 'default' and 'drag_direction' variations to feature params.
    > - Wire strategies in factory.
    >
    > Bug: 540352621
    > Change-Id: I632bcc28017282d2272e9699fb7cc202275ef437
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8180084
    > Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    > Reviewed-by: Dan Polanco <polardz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1671866}
    
    (cherry picked from commit a1025c896d2007b2846d95415280ed43eecdb272)
    
    Bug: 542691244,540352621
    Change-Id: I632bcc28017282d2272e9699fb7cc202275ef437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8201086
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Cr-Commit-Position: refs/branch-heads/7977@{#891}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/ResizingStrategyFactory.java
+++ chrome/browser/context_sharing/tab_bottom_sheet/android/java/src/org/chromium/chrome/browser/tab_bottom_sheet/ResizingStrategyFactory.java
@@ -5,16 +5,26 @@
 package org.chromium.chrome.browser.tab_bottom_sheet;
 
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
 /** Factory for creating {@link ResizingStrategy} instances. */
 @NullMarked
 public class ResizingStrategyFactory {
+    public static final String RESIZING_STRATEGY_PARAM = "resizing_strategy";
+    public static final String STRATEGY_DRAG_DIRECTION = "drag_direction";
+
     /**
-     * Creates a default strategy for managing resizing mode on {@link WebViewResizingHelper}.
+     * Creates a strategy for managing resizing mode on {@link WebViewResizingHelper}.
      *
      * @param helper The {@link WebViewResizingHelper} to control resizing mode on.
      */
     public static ResizingStrategy create(WebViewResizingHelper helper) {
+        String strategyParam =
+                ChromeFeatureList.getFieldTrialParamByFeature(
+                        ChromeFeatureList.TAB_BOTTOM_SHEET_RESIZE_WEBVIEW, RESIZING_STRATEGY_PARAM);
+        if (STRATEGY_DRAG_DIRECTION.equals(strategyParam)) {
+            return new DragDirectionResizingStrategy(helper);
+        }
         return new DefaultResizingStrategy(helper);
     }
 }
