6b547542e4f3cdb42eef45eb37d47093da76123c
Devon Loehr <dloehr@google.com>
2026-08-08 06:27:30 -0700

    Alternative way to provide compiler-rt when linking bindgen on x86 mac
    LLVM recently added a reference to the __cpu_model symbol on x86 macs,
    but the apple SDK doesn't expose it, so we provide the definition by
    adding compiler-rt to the link step.
    
    We previously did it by providing the .a files directly, but
    hopefully doing it like this is cleaner.
    
    Bug: 537814755
    Change-Id: Iac3071fe20eb9730256884ec8ed06aa743e581cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8220329
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676155}

--- tools/rust/build_bindgen.py
+++ tools/rust/build_bindgen.py
@@ -165,10 +165,8 @@ def RunCargo(cargo_args):
         env['LDFLAGS'] += f' -isysroot {sdk_path}'
         env['RUSTFLAGS'] += f' -Clink-arg=-isysroot -Clink-arg={sdk_path}'
         if 'x86_64' in RustTargetTriple():
-            for rt in Path(llvm_dir).glob(
-                    'lib/clang/*/lib/darwin/libclang_rt.osx.a'):
-                env['LDFLAGS'] += f' {rt}'
-                env['RUSTFLAGS'] += f' -Clink-arg={rt}'
+            env['LDFLAGS'] += ' -fapple-link-rtlib'
+            env['RUSTFLAGS'] += ' -Clink-arg=-fapple-link-rtlib'
 
     # This will `fail_hard` and not return if `cargo` reports problems.
     RunCommand([cargo_bin] + cargo_args, setenv=True, env=env)
