be9d8b1cc8c793a9c2c3f99a069d5e139bac3e11
Kamil Jarosz <kjarosz@google.com>
2026-08-10 04:59:19 -0700

    metrics: Remove remaining references to DKM
    DKM code is not used, so removing remaining references to DKM.
    
    Bug: b:540830448
    Change-Id: I97a903b89b0fdaa8621336b1ee55f0272ba5ba27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8227505
    Commit-Queue: Kamil Jarosz <kjarosz@google.com>
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1676361}

--- components/metrics/dwa/dwa_service.cc
+++ components/metrics/dwa/dwa_service.cc
@@ -322,9 +322,8 @@ DwaService::BuildPrivateMetricEndpointPayloadFromEncryptedReport(
     case ::private_metrics::EncryptedPrivateMetricReport::DWA:
       report_type = ::private_metrics::PrivateMetricEndpointPayload::DWA;
       break;
+    // TODO(b/540830448): Remove
     case ::private_metrics::EncryptedPrivateMetricReport::DKM:
-      report_type = ::private_metrics::PrivateMetricEndpointPayload::DKM;
-      break;
     case ::private_metrics::EncryptedPrivateMetricReport::REPORT_TYPE_INVALID:
       return std::nullopt;
   }
