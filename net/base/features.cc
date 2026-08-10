ed9661c0877bab43bc83e4dba68d0eda35c837c5
Stefano Duo <stefanoduo@google.com>
2026-08-10 10:23:56 -0700

    Revert "Enable kQuicIgnoreRedundantOnNetworkMadeDefault by default"
    This reverts commit 2242b1441b7f6154f59cadf6dc69f2995c119bb9.
    
    Reason for revert: Reverting to follow WebView's best practices.
    
    Original change's description:
    > Enable kQuicIgnoreRedundantOnNetworkMadeDefault by default
    >
    > The experiment results confirmed our assumption of this being the right
    > behavior. We're now seeing less "Already migrated on the new network"
    > due to the race condition explained in https://crrev.com/c/7885277.
    >
    > Bug: 517895903
    > Change-Id: I21880a4f884032d1ca2f6f94f0208455149f838e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8187035
    > Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    > Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    > Auto-Submit: Stefano Duo <stefanoduo@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1673892}
    
    Bug: 517895903
    Change-Id: Ic8969b9867325f518ec6e664cf0eaaa0ff21d152
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8231899
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1676546}

--- net/base/features.cc
+++ net/base/features.cc
@@ -824,7 +824,7 @@ BASE_FEATURE_PARAM(base::TimeDelta,
                    base::Seconds(quic::kInitialIdleTimeoutSecs));
 
 BASE_FEATURE(kQuicIgnoreRedundantOnNetworkMadeDefault,
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kQuicLongerIdleConnectionTimeout,
              base::FEATURE_DISABLED_BY_DEFAULT);
