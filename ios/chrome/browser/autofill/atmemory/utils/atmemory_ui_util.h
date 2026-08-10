0cc3fac71677967ec8d5ff1e48396cb5358e961e
Leo Zhao <leozhao@google.com>
2026-08-10 09:09:49 -0700

    [iOS][AtMemory] Cleanup unused helper functions
    This CL removes helper functions that are copied over from manual fill.
    Now AtMemory is to use standard UINavigationBar. There is no need for
    keeping those functions. This CL deletes them to have a clean space for
    adding new helper functions for converting memory search results to iOS
    native objects.
    
    Bug: 522340351
    Change-Id: I26e04b2281c8d731af32f59bac60c39a51a87c08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8216610
    Commit-Queue: Leo Zhao <leozhao@google.com>
    Reviewed-by: Angela Novakovic <novakovica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676493}

--- ios/chrome/browser/autofill/atmemory/utils/atmemory_ui_util.h
+++ ios/chrome/browser/autofill/atmemory/utils/atmemory_ui_util.h
@@ -5,24 +5,4 @@
 #ifndef IOS_CHROME_BROWSER_AUTOFILL_ATMEMORY_UTILS_ATMEMORY_UI_UTIL_H_
 #define IOS_CHROME_BROWSER_AUTOFILL_ATMEMORY_UTILS_ATMEMORY_UI_UTIL_H_
 
-#import <UIKit/UIKit.h>
-
-@class ImageContentConfiguration;
-
-#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
-
-namespace autofill {
-
-// Returns the symbol configuration to use for the close button.
-UIImageSymbolConfiguration* GetCloseButtonSymbolConfiguration();
-
-// Returns the foreground color to use for the close button color palette.
-UIColor* GetCloseButtonForegroundColor();
-
-// Creates and returns an ImageContentConfiguration, following the design system
-// for AtMemory cell icons.
-ImageContentConfiguration* AtMemoryCellIconConfiguration(Symbol symbol);
-
-}  // namespace autofill
-
 #endif  // IOS_CHROME_BROWSER_AUTOFILL_ATMEMORY_UTILS_ATMEMORY_UI_UTIL_H_
