f7cd8bec54dc9ba8e5a3016d13fc449c0b7eb740
Nasser Al-shawwa <alshawwa@chromium.org>
2026-08-10 07:19:36 -0700

    Enable EnableDlpFileSystemApi by default
    Bug: 490304471
    Change-Id: I8ae662b9c87ef446dc2bdc1ce9693755173f3f12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8230774
    Commit-Queue: Alex Chen <alxchn@google.com>
    Reviewed-by: Lina Ismail <ismaill@google.com>
    Reviewed-by: Alex Chen <alxchn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1676414}

--- components/enterprise/connectors/core/features.cc
+++ components/enterprise/connectors/core/features.cc
@@ -50,6 +50,6 @@ BASE_FEATURE(kEnableAuditOnlyNetworkRequestConnector,
 
 BASE_FEATURE(kContentAnalysisClipboardCopy, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kEnableDlpFileSystemApi, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableDlpFileSystemApi, base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace enterprise_connectors
