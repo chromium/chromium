c96f02a2f40e37951eb629ab9e31ed2d5dfacd50
Valerie Young <spectranaut@igalia.com>
2026-08-11 10:57:39 -0700

    Reland "Always expose AXTitle alongside AXDescription"
    This is a reland of commit f9f25c73a75ff2d355c809ad4e91bac6980f89b3
    
    The previous commit missed some additional tests expectation
    that needed updates that only run under "Include-Ci-Only-Tests:
    true". This new CL now updates those tests.
    
    Original change's description:
    > Always expose AXTitle alongside AXDescription
    >
    > As of MacOS 27+, VoiceOver will always look for the accessible
    > name of an element in AXTitle. For backwards compatibility,
    > we need to continue to exposed AXDescription in some scenarios
    > where VoiceOver will only look for the accessible name there.
    >
    > As long as the accessible name exposed by AXTitle and AXDescription
    > are identical (and they are) this change should not result in double
    > speech.
    >
    > Bug: 406819290
    > Change-Id: Ia58b55484272fb9f5b2380b7539f7496a7f70921
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8188434
    > Commit-Queue: Valerie Young <spectranaut@igalia.com>
    > SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1675184}
    
    Bug: 406819290
    Include-Ci-Only-Tests: true
    Change-Id: Id38bf4bbc245a3924c72d223ed56a927412079de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8226505
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677375}

--- ui/accessibility/platform/ax_platform_node_cocoa.mm
+++ ui/accessibility/platform/ax_platform_node_cocoa.mm
@@ -2941,9 +2941,6 @@ const ui::CocoaActionList& GetCocoaActionListForTesting() {
     return @"";
   }
 
-  if ([self isNameFromLabel])
-    return @"";
-
   ax::mojom::NameFrom nameFrom = _node->GetNameFrom();
 
   // The accessible name, which is exposed via accessibilityTitle, should not
