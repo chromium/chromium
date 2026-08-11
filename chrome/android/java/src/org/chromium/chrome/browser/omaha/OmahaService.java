6b609f01d1711cb8e897ceeff27ff6acefa8d587
Yaron Friedman <yfriedman@google.com>
2026-08-11 07:14:45 -0700

    [M152] Revert "Fix OmahaService unmanaged background thread and queue starvation leak."
    Original change's description:
    > Revert "Fix OmahaService unmanaged background thread and queue starvation leak."
    >
    > This reverts commit 4f440da94ae8d8c1d485aad954ea52b63857d3ef.
    >
    > Reason for revert: suspected for unintended impact on metrics
    >
    > Original change's description:
    > > Fix OmahaService unmanaged background thread and queue starvation leak.
    > >
    > > This CL resolves a critical background task thread leak in OmahaService by:
    > > 1. Aligning with the JobScheduler asynchronous lifecycle API by returning 'true' from onStartTask() to ensure the OS properly tracks execution and triggers onStopTask() on timeout.
    > > 2. Invoking 'cancel(true)' on the background AsyncTask inside onStopTask() to send a cooperative JVM thread interrupt signal, closing open socket connections and terminating the thread cleanly.
    > > 3. Adding robust blackbox JUnit tests verifying that the background thread leak and its global impact on the SerialExecutor queue are completely resolved.
    > >
    > > Bug: 467446215
    > >
    > > TAG=agy
    > > CONV=b9b51289-773f-46a6-bd89-b604f9fcc3d3
    > >
    > > Change-Id: I1690b65c92d5529c5db2b0d9b4286f644219fed9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7735345
    > > Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    > > Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1626896}
    >
    > Bug: 467446215
    > Change-Id: I6d7f9407841e999ffee3ab42320f47f3051a5dda
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8212181
    > Auto-Submit: Yaron Friedman <yfriedman@chromium.org>
    > Commit-Queue: Sorin Jianu <sorin@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1675049}
    
    (cherry picked from commit 96d11c5e62d459a3acd172a1b2be61e3b46bc11f)
    
    Bug: 544764146,467446215
    Change-Id: I6d7f9407841e999ffee3ab42320f47f3051a5dda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8233500
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7977@{#1220}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- chrome/android/java/src/org/chromium/chrome/browser/omaha/OmahaService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omaha/OmahaService.java
@@ -62,13 +62,9 @@ public class OmahaService extends OmahaBase implements BackgroundTask {
         super(new OmahaClientDelegate());
     }
 
-    OmahaService(OmahaDelegate delegate) {
-        super(delegate);
-    }
-
     /**
-     * Trigger the {@link BackgroundTaskScheduler} immediately. Must only be called by {@link
-     * OmahaBase#onForegroundSessionStart}.
+     * Trigger the {@link BackgroundTaskScheduler} immediately.
+     * Must only be called by {@link OmahaBase#onForegroundSessionStart}.
      */
     static void startServiceImmediately() {
         if (sHasPendingJob) return;
@@ -95,7 +91,7 @@ public class OmahaService extends OmahaBase implements BackgroundTask {
                         callback.taskFinished(false);
                     }
                 }.executeOnExecutor(AsyncTask.SERIAL_EXECUTOR);
-        return true;
+        return false;
     }
 
     @Override
@@ -104,7 +100,7 @@ public class OmahaService extends OmahaBase implements BackgroundTask {
         // clear this flag to avoid getting stuck in state where we won't ever be scheduled again.
         sHasPendingJob = false;
         if (mJobServiceTask != null) {
-            mJobServiceTask.cancel(true);
+            mJobServiceTask.cancel(false);
             mJobServiceTask = null;
         }
         return false;
