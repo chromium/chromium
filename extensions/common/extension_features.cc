6a7dfa9d11e13fa4a56c1685d848b6551e07abb1
Kaleigh Rosenblat <kaleigh@google.com>
2026-08-11 09:40:19 -0700

    Enable extensions for corporate desktop Android by default
    Flips the kEnableExtensionsForCorpDesktopAndroid feature flag to enabled by default and removes its corresponding field trial testing configuration.
    
    Change-Id: I0117468f12f5c1d5d4684219a33dadc12cfac454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8233501
    Commit-Queue: Kaleigh Rosenblat <kaleigh@google.com>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1677291}

--- extensions/common/extension_features.cc
+++ extensions/common/extension_features.cc
@@ -112,10 +112,8 @@ BASE_FEATURE(kCWSReviewPromptingNativeUI,
 BASE_FEATURE(kEnableWebHidInWebView, base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(ENABLE_DESKTOP_ANDROID_EXTENSIONS)
-// Disabled by default because on first-run we don't have a Finch seed yet, so
-// we want to default to the safe behavior of no extensions.
 BASE_FEATURE(kEnableExtensionsForCorpDesktopAndroid,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kExtensionAutoRejectIncognitoConnectability,
