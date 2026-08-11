aa4a8869c6bbf61fd6af81ea38aa94ced6ba0e4b
Rushan Suleymanov <rushans@google.com>
2026-08-11 04:32:22 -0700

    [sync] Rewrite TwoClientAppSettingsSyncTest to not use verifier
    Rewrite TwoClientAppSettingsSyncTest to eliminate verifier usage,
    following the pattern in TwoClientExtensionSettingsSyncTest. The tests
    now verify the final state on Profile 0 and wait for all profiles to
    converge using AllExtensionSettingsSameChecker.
    
    TAG=agy
    CONV=fe6476ca-9638-4f43-acfb-30b992b91cf3
    
    Bug: 40724949
    Change-Id: I49fc3dac25bee18fdcbb290e47804e7c62755ae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8232158
    Reviewed-by: Jood Hajeer <jood@google.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1677108}

--- chrome/browser/sync/test/integration/two_client_app_settings_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_app_settings_sync_test.cc
@@ -8,11 +8,7 @@
 #include "chrome/browser/sync/test/integration/apps_helper.h"
 #include "chrome/browser/sync/test/integration/apps_sync_test_base.h"
 #include "chrome/browser/sync/test/integration/extension_settings_helper.h"
-#include "chrome/browser/sync/test/integration/extensions_helper.h"
-#include "chrome/browser/sync/test/integration/multi_client_status_change_checker.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
-#include "components/sync/base/features.h"
-#include "components/sync/service/sync_service_impl.h"
 #include "content/public/test/browser_test.h"
 
 #if BUILDFLAG(IS_CHROMEOS)
@@ -23,6 +19,7 @@ namespace {
 
 using apps_helper::InstallHostedAppForAllProfiles;
 using extension_settings_helper::AllExtensionSettingsSameChecker;
+using extension_settings_helper::GetExtensionSettings;
 using extension_settings_helper::SetExtensionSettings;
 
 class TwoClientAppSettingsSyncTest : public AppsSyncTestBase {
@@ -30,200 +27,154 @@ class TwoClientAppSettingsSyncTest : public AppsSyncTestBase {
   TwoClientAppSettingsSyncTest() : AppsSyncTestBase(TWO_CLIENT) {}
   ~TwoClientAppSettingsSyncTest() override = default;
 
-  bool UseVerifier() override {
-    // TODO(crbug.com/40724949): rewrite tests to not use verifier.
-    return true;
-  }
-
   // APP_SETTINGS is only supported with Sync-the-feature.
   SyncTest::SetupSyncMode GetSetupSyncMode() const override {
     return SetupSyncMode::kSyncTheFeature;
   }
 
+  struct ExpectedSettings {
+    base::DictValue extension0;
+    base::DictValue extension1;
+    base::DictValue extension2;
+  };
+
   // Generic mutations done after the initial setup of all tests. Note that
-  // unfortunately we can't test existing configurations of the sync server
-  // since the tests don't support that.
-  void MutateSomeSettings(
+  // existing configurations of the sync server cannot be tested since the
+  // test infrastructure does not support that.
+  ExpectedSettings MutateSettingsAcrossProfiles(
       int seed,  // used to modify the mutation values, not keys.
       const std::string& extension0,
       const std::string& extension1,
       const std::string& extension2) {
-    {
-      // Write to extension0 from profile 0 but not profile 1.
-      base::DictValue settings;
-      settings.Set("asdf", base::StringPrintf("asdfasdf-%d", seed));
-      SetExtensionSettings(verifier(), extension0, settings);
-      SetExtensionSettings(GetProfile(0), extension0, settings);
-    }
-    {
-      // Write the same data to extension1 from both profiles.
-      base::DictValue settings;
-      settings.Set("asdf", base::StringPrintf("asdfasdf-%d", seed));
-      settings.Set("qwer", base::StringPrintf("qwerqwer-%d", seed));
-      SetExtensionSettings(GetAllProfiles(), extension1, settings);
-    }
-    {
-      // Write different data to extension2 from each profile.
-      base::DictValue settings0;
-      settings0.Set("zxcv", base::StringPrintf("zxcvzxcv-%d", seed));
-      SetExtensionSettings(verifier(), extension2, settings0);
-      SetExtensionSettings(GetProfile(0), extension2, settings0);
+    ExpectedSettings expected;
 
-      base::DictValue settings1;
-      settings1.Set("1324", base::StringPrintf("12341234-%d", seed));
-      settings1.Set("5687", base::StringPrintf("56785678-%d", seed));
-      SetExtensionSettings(verifier(), extension2, settings1);
-      SetExtensionSettings(GetProfile(1), extension2, settings1);
-    }
-  }
+    // Write to extension0 from profile 0 but not profile 1.
+    expected.extension0.Set("asdf", base::StringPrintf("asdfasdf-%d", seed));
+    SetExtensionSettings(GetProfile(0), extension0, expected.extension0);
 
-  // For three independent extensions:
-  //
-  // Set up each extension with the same (but not necessarily empty) settings
-  // for all profiles, start syncing, add some new settings, sync, mutate those
-  // settings, sync.
-  testing::AssertionResult StartWithSameSettingsTest(
-      const std::string& extension0,
-      const std::string& extension1,
-      const std::string& extension2) {
-    {
-      // Leave extension0 empty.
-    }
-    {
-      base::DictValue settings;
-      settings.Set("foo", "bar");
-      SetExtensionSettings(GetAllProfiles(), extension1, settings);
-    }
-    {
-      base::DictValue settings;
-      settings.Set("foo", "bar");
-      settings.Set("baz", "qux");
-      SetExtensionSettings(GetAllProfiles(), extension2, settings);
-    }
+    // Write the same data to extension1 from both profiles.
+    expected.extension1.Set("asdf", base::StringPrintf("asdfasdf-%d", seed));
+    expected.extension1.Set("qwer", base::StringPrintf("qwerqwer-%d", seed));
+    SetExtensionSettings(GetAllProfiles(), extension1, expected.extension1);
 
-    if (!SetupSync()) {
-      return testing::AssertionFailure();
-    }
-    if (!AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
-             .Wait()) {
-      return testing::AssertionFailure();
-    }
+    // Write different data to extension2 from each profile.
+    base::DictValue settings0;
+    settings0.Set("zxcv", base::StringPrintf("zxcvzxcv-%d", seed));
+    SetExtensionSettings(GetProfile(0), extension2, settings0);
 
-    MutateSomeSettings(0, extension0, extension1, extension2);
-    if (!AwaitQuiescence()) {
-      return testing::AssertionFailure();
-    }
-    if (!AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
-             .Wait()) {
-      return testing::AssertionFailure();
-    }
+    base::DictValue settings1;
+    settings1.Set("1324", base::StringPrintf("12341234-%d", seed));
+    settings1.Set("5687", base::StringPrintf("56785678-%d", seed));
+    SetExtensionSettings(GetProfile(1), extension2, settings1);
 
-    MutateSomeSettings(1, extension0, extension1, extension2);
-    if (!AwaitQuiescence()) {
-      return testing::AssertionFailure();
-    }
-    if (!AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
-             .Wait()) {
-      return testing::AssertionFailure();
-    }
+    expected.extension2.Merge(settings0.Clone());
+    expected.extension2.Merge(settings1.Clone());
 
-    return testing::AssertionSuccess();
-  }
-
-  // For three independent extensions:
-  //
-  // Set up each extension with different settings for each profile, start
-  // syncing, add some settings, sync, mutate those settings, sync, have a no-op
-  // (non-)change to those settings, sync, mutate again, sync.
-  testing::AssertionResult StartWithDifferentSettingsTest(
-      const std::string& extension0,
-      const std::string& extension1,
-      const std::string& extension2) {
-    {
-      // Leave extension0 empty again for no particular reason other than it's
-      // the only remaining unique combination given the other 2 tests have
-      // (empty, nonempty) and (nonempty, nonempty) configurations. We can't
-      // test (nonempty, nonempty) because the merging will provide
-      // unpredictable results, so test (empty, empty).
-    }
-    {
-      base::DictValue settings;
-      settings.Set("foo", "bar");
-      SetExtensionSettings(verifier(), extension1, settings);
-      SetExtensionSettings(GetProfile(0), extension1, settings);
-    }
-    {
-      base::DictValue settings;
-      settings.Set("foo", "bar");
-      settings.Set("baz", "qux");
-      SetExtensionSettings(verifier(), extension2, settings);
-      SetExtensionSettings(GetProfile(1), extension2, settings);
-    }
-
-    if (!SetupSync()) {
-      return testing::AssertionFailure();
-    }
-    if (!AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
-             .Wait()) {
-      return testing::AssertionFailure();
-    }
-
-    MutateSomeSettings(2, extension0, extension1, extension2);
-    if (!AwaitQuiescence()) {
-      return testing::AssertionFailure();
-    }
-    if (!AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
-             .Wait()) {
-      return testing::AssertionFailure();
-    }
-
-    MutateSomeSettings(3, extension0, extension1, extension2);
-    if (!AwaitQuiescence()) {
-      return testing::AssertionFailure();
-    }
-    if (!AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
-             .Wait()) {
-      return testing::AssertionFailure();
-    }
-
-    // Test a round of no-ops once, for sanity. Ideally we'd want to assert that
-    // this causes no sync activity, but that sounds tricky.
-    MutateSomeSettings(3, extension0, extension1, extension2);
-    if (!AwaitQuiescence()) {
-      return testing::AssertionFailure();
-    }
-    if (!AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
-             .Wait()) {
-      return testing::AssertionFailure();
-    }
-
-    MutateSomeSettings(4, extension0, extension1, extension2);
-    if (!AwaitQuiescence()) {
-      return testing::AssertionFailure();
-    }
-    if (!AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
-             .Wait()) {
-      return testing::AssertionFailure();
-    }
-
-    return testing::AssertionSuccess();
+    return expected;
   }
 };
 
 IN_PROC_BROWSER_TEST_F(TwoClientAppSettingsSyncTest,
                        AppsStartWithSameSettings) {
   ASSERT_TRUE(SetupClients());
-  EXPECT_TRUE(StartWithSameSettingsTest(InstallHostedAppForAllProfiles(0),
-                                        InstallHostedAppForAllProfiles(1),
-                                        InstallHostedAppForAllProfiles(2)));
+  const std::string extension0 = InstallHostedAppForAllProfiles(0);
+  const std::string extension1 = InstallHostedAppForAllProfiles(1);
+  const std::string extension2 = InstallHostedAppForAllProfiles(2);
+
+  // For three independent extensions:
+  // Set up each extension with the same (but not necessarily empty) settings
+  // for all profiles, start syncing, add some new settings, sync, mutate those
+  // settings, sync.
+  // Leave extension0 empty.
+  base::DictValue base_settings1;
+  base_settings1.Set("foo", "bar");
+  SetExtensionSettings(GetAllProfiles(), extension1, base_settings1);
+  base::DictValue base_settings2;
+  base_settings2.Set("foo", "bar");
+  base_settings2.Set("baz", "qux");
+  SetExtensionSettings(GetAllProfiles(), extension2, base_settings2);
+
+  ASSERT_TRUE(SetupSync());
+  ASSERT_TRUE(
+      AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
+          .Wait());
+  EXPECT_EQ(base::DictValue(), GetExtensionSettings(GetProfile(0), extension0));
+  EXPECT_EQ(base_settings1, GetExtensionSettings(GetProfile(0), extension1));
+  EXPECT_EQ(base_settings2, GetExtensionSettings(GetProfile(0), extension2));
+
+  for (int seed : {0, 1}) {
+    ExpectedSettings expected =
+        MutateSettingsAcrossProfiles(seed, extension0, extension1, extension2);
+    expected.extension1.Merge(base_settings1.Clone());
+    expected.extension2.Merge(base_settings2.Clone());
+    ASSERT_TRUE(
+        AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
+            .Wait());
+    EXPECT_EQ(expected.extension0,
+              GetExtensionSettings(GetProfile(0), extension0))
+        << "For seed=" << seed;
+    EXPECT_EQ(expected.extension1,
+              GetExtensionSettings(GetProfile(0), extension1))
+        << "For seed=" << seed;
+    EXPECT_EQ(expected.extension2,
+              GetExtensionSettings(GetProfile(0), extension2))
+        << "For seed=" << seed;
+  }
 }
 
 IN_PROC_BROWSER_TEST_F(TwoClientAppSettingsSyncTest,
                        AppsStartWithDifferentSettings) {
   ASSERT_TRUE(SetupClients());
-  EXPECT_TRUE(StartWithDifferentSettingsTest(
-      InstallHostedAppForAllProfiles(0), InstallHostedAppForAllProfiles(1),
-      InstallHostedAppForAllProfiles(2)));
+  const std::string extension0 = InstallHostedAppForAllProfiles(0);
+  const std::string extension1 = InstallHostedAppForAllProfiles(1);
+  const std::string extension2 = InstallHostedAppForAllProfiles(2);
+
+  // For three independent extensions:
+  // Set up each extension with different settings for each profile, start
+  // syncing, add some settings, sync, mutate those settings, sync, have a no-op
+  // (non-)change to those settings, sync, mutate again, sync.
+  // Leave extension0 empty again for no particular reason other than it's
+  // the only remaining unique combination given the other 2 tests have
+  // (empty, nonempty) and (nonempty, nonempty) configurations. Testing
+  // (nonempty, nonempty) cannot be done because the merging will provide
+  // unpredictable results, so (empty, empty) is tested.
+  base::DictValue base_settings1;
+  base_settings1.Set("foo", "bar");
+  SetExtensionSettings(GetProfile(0), extension1, base_settings1);
+  base::DictValue base_settings2;
+  base_settings2.Set("foo", "bar");
+  base_settings2.Set("baz", "qux");
+  SetExtensionSettings(GetProfile(1), extension2, base_settings2);
+
+  ASSERT_TRUE(SetupSync());
+  ASSERT_TRUE(
+      AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
+          .Wait());
+  EXPECT_EQ(base::DictValue(), GetExtensionSettings(GetProfile(0), extension0));
+  EXPECT_EQ(base_settings1, GetExtensionSettings(GetProfile(0), extension1));
+  EXPECT_EQ(base_settings2, GetExtensionSettings(GetProfile(0), extension2));
+
+  // The second seed=3 iteration tests a round of no-ops once, for sanity.
+  // Ideally this would assert that this causes no sync activity, but that
+  // is difficult to verify in integration tests.
+  for (int seed : {2, 3, 3, 4}) {
+    ExpectedSettings expected =
+        MutateSettingsAcrossProfiles(seed, extension0, extension1, extension2);
+    expected.extension1.Merge(base_settings1.Clone());
+    expected.extension2.Merge(base_settings2.Clone());
+    ASSERT_TRUE(
+        AllExtensionSettingsSameChecker(GetSyncServices(), GetAllProfiles())
+            .Wait());
+    EXPECT_EQ(expected.extension0,
+              GetExtensionSettings(GetProfile(0), extension0))
+        << "For seed=" << seed;
+    EXPECT_EQ(expected.extension1,
+              GetExtensionSettings(GetProfile(0), extension1))
+        << "For seed=" << seed;
+    EXPECT_EQ(expected.extension2,
+              GetExtensionSettings(GetProfile(0), extension2))
+        << "For seed=" << seed;
+  }
 }
 
 }  // namespace
