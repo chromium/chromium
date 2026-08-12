a6394790657c2e2c30003f430eabe2bb32944a13
Kevin McNee <mcnee@chromium.org>
2026-08-12 02:21:36 -0700

    [M152] Dictation: Remove param for caret bubble
    Original change's description:
    > Dictation: Remove param for caret bubble
    >
    > While minor polish remains, the bubble is functional, and no longer
    > needs to be withheld beyond the guarding of the overall dictation
    > feature.
    >
    > Bug: b:525859277, b:539693982
    > Change-Id: Idadea532a8e5ad9cd5c55cddbb793b45d4cbc02e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8218367
    > Reviewed-by: David Bokan <bokan@chromium.org>
    > Commit-Queue: David Bokan <bokan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1675461}
    
    (cherry picked from commit c0c0c7fd71f331b8c1b82bbe21f83453c7bb9ced)
    
    Bug: 543604809,b:525859277
    Change-Id: Idadea532a8e5ad9cd5c55cddbb793b45d4cbc02e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8246499
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7977@{#1307}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- chrome/browser/dictation/features.cc
+++ chrome/browser/dictation/features.cc
@@ -25,7 +25,4 @@ const base::FeatureParam<bool> kShowPartials{&kDictation, "show_partials",
 const base::FeatureParam<bool> kWebSpeechApiBackend{
     &kDictation, "web_speech_api_backend", false};
 
-const base::FeatureParam<bool> kShowCaretBubble{&kDictation,
-                                                "show_caret_bubble", false};
-
 }  // namespace dictation
