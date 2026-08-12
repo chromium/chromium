6cdfdf4bc577781741aa2cfa88676e0e6305b4bd
Bryan Kim <bryankim@google.com>
2026-08-12 11:19:19 -0700

    Resolve circular dependency between nearby_sharing:impl and ash/nearby
    Move NearbyConnectionsMdnsManager (and its unit test) from
    chrome/browser/nearby_sharing/mdns/ to chrome/browser/ash/nearby/mdns/.
    
    This resolves the circular dependency because NearbyConnectionsMdnsManager
    is only used by ash/nearby (specifically NearbyDependenciesProvider) and
    not by nearby_sharing itself. Moving it allows us to remove the
    allow_circular_includes_from dependency of nearby_sharing:impl on
    ash/nearby.
    
    TAG=agy
    CONV=e78dfb19-1185-49e8-bcd0-6bafa21a7aab
    AGENT_GENERATED_CL
    
    Bug: 353332589
    Change-Id: I9af0a9ae6ff1f50badeb0c689425c6e36cc7c2cf
    Bypass-Check-License: Moved files
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8126928
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Bryan Kim <bryankim@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1678196}

--- chrome/browser/ash/nearby/mdns/nearby_connections_mdns_manager.cc
+++ chrome/browser/ash/nearby/mdns/nearby_connections_mdns_manager.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/nearby_sharing/mdns/nearby_connections_mdns_manager.h"
+#include "chrome/browser/ash/nearby/mdns/nearby_connections_mdns_manager.h"
 
 #include <string>
 
