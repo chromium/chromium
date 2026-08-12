6c5ae7aacc522b43ddf96b4a9c260eb699451a53
Peter Conn <peconn@chromium.org>
2026-08-12 11:43:23 -0700

    Drain prefetch queue on navigate.
    We do this in loadUrl, we should do so in navigate as well.
    
    Bug: 363939616
    Change-Id: I8b8ccbacd825ac6281069c26a07355de400184a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8245992
    Reviewed-by: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Commit-Queue: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Auto-Submit: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678208}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -2499,6 +2499,10 @@ public class AwContents implements SmartClipProvider {
             throw headerException;
         }
 
+        // Drain any pending prefetch requests from the queue.
+        // TODO (crbug.com/363939616) Clean this up once prefetch infra supports bg thread.
+        mBrowserContext.getPrefetchManager().executeQueuedPrefetchRequests();
+
         // TODO(crbug.com/408974593): Consider adding a fixupUrl option.
         // TODO(crbug.com/408974593): Allow developers to set the PageTransition type.
         LoadUrlParams loadUrlParams =
