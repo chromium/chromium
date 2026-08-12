23b1131220496c116a6082d1088e73c980fbec6c
Gary Klassen <gklassen@chromium.org>
2026-08-12 10:10:26 -0700

    [ttc] Add form control attributes to PageContentNode and PageContextMonitor
    1. Define NodeRole enum in ai_overlay_dialog.mojom and extend
    PageContentNode
       with role, value, placeholder, and is_checked fields to support form
       controls.
    2. In PageContextMonitor, convert AnnotatedPageContent form control data
       and interactive attributes into PageContentNode Mojo structs with
       sibling label lookahead for unassociated labels.
    
    R=bokan@chromium.org, kenrb@chromium.org
    
    Bug: b:490458384
    Change-Id: I2714019734f7236b2335b6f01e66aea2604ed28f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8223505
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678129}

--- chrome/browser/ui/webui/ai_overlay_dialog/page_context_monitor.cc
+++ chrome/browser/ui/webui/ai_overlay_dialog/page_context_monitor.cc
@@ -4,6 +4,7 @@
 
 #include "chrome/browser/ui/webui/ai_overlay_dialog/page_context_monitor.h"
 
+#include "base/files/file_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "chrome/browser/page_content_annotations/page_content_screenshot_service_factory.h"
@@ -22,33 +23,143 @@ using page_content_annotations::PageContentScreenshotServiceFactory;
 
 namespace {
 
-ai_overlay_dialog::mojom::PageContentNodePtr ConvertContentNodeToMojo(
-    const optimization_guide::proto::ContentNode& proto_node) {
-  auto mojo_node = ai_overlay_dialog::mojom::PageContentNode::New();
-  if (proto_node.has_content_attributes()) {
-    const auto& attrs = proto_node.content_attributes();
-    mojo_node->dom_node_id = attrs.common_ancestor_dom_node_id();
+std::string FindFirstTextInSubtree(
+    const optimization_guide::proto::ContentNode& node) {
+  if (node.has_content_attributes()) {
+    const auto& attrs = node.content_attributes();
+    if (attrs.has_text_data() && !attrs.text_data().text_content().empty()) {
+      return attrs.text_data().text_content();
+    }
+    if (!attrs.label().empty()) {
+      return attrs.label();
+    }
+  }
+  for (const auto& child : node.children_nodes()) {
+    std::string text = FindFirstTextInSubtree(child);
+    if (!text.empty()) {
+      return text;
+    }
+  }
+  return "";
+}
 
-    if (attrs.attribute_type() ==
-        optimization_guide::proto::ContentAttributeType::
-            CONTENT_ATTRIBUTE_IMAGE) {
-      mojo_node->tag_name = "img";
-      if (attrs.has_image_data()) {
-        mojo_node->text = attrs.image_data().image_caption();
+void ConvertContentNodesToMojo(
+    const google::protobuf::RepeatedPtrField<
+        optimization_guide::proto::ContentNode>& proto_nodes,
+    std::vector<ai_overlay_dialog::mojom::PageContentNodePtr>& mojo_nodes) {
+  for (int i = 0; i < proto_nodes.size(); ++i) {
+    const auto& proto_node = proto_nodes[i];
+    auto mojo_node = ai_overlay_dialog::mojom::PageContentNode::New();
+
+    if (proto_node.has_content_attributes()) {
+      const auto& attrs = proto_node.content_attributes();
+      mojo_node->dom_node_id = attrs.common_ancestor_dom_node_id();
+
+      if (attrs.attribute_type() ==
+          optimization_guide::proto::ContentAttributeType::
+              CONTENT_ATTRIBUTE_IMAGE) {
+        mojo_node->tag_name = "img";
+        if (attrs.has_image_data()) {
+          mojo_node->text = attrs.image_data().image_caption();
+        }
+      }
+
+      if (attrs.has_text_data()) {
+        mojo_node->text = attrs.text_data().text_content();
+      }
+      if (attrs.has_anchor_data()) {
+        mojo_node->url = GURL(attrs.anchor_data().url());
+        mojo_node->is_interactive = true;
+        mojo_node->role = ai_overlay_dialog::mojom::NodeRole::kLink;
+      }
+      if (attrs.has_form_control_data()) {
+        const auto& fc = attrs.form_control_data();
+        mojo_node->is_interactive = true;
+        mojo_node->is_checked = fc.is_checked();
+        mojo_node->value = fc.field_value();
+        mojo_node->placeholder = fc.placeholder();
+
+        std::string label = attrs.has_text_data()
+                                ? attrs.text_data().text_content()
+                                : attrs.label();
+        if (label.empty()) {
+          label = FindFirstTextInSubtree(proto_node);
+        }
+
+        // Lookahead to next sibling if label is still empty
+        bool consumed_next_sibling = false;
+        if (label.empty() && i + 1 < proto_nodes.size()) {
+          const auto& next_node = proto_nodes[i + 1];
+          if (next_node.has_content_attributes()) {
+            const auto& next_attrs = next_node.content_attributes();
+            if (!next_attrs.has_form_control_data() &&
+                !next_attrs.has_anchor_data()) {
+              std::string next_text = FindFirstTextInSubtree(next_node);
+              if (!next_text.empty()) {
+                label = next_text;
+                consumed_next_sibling = true;
+              }
+            }
+          }
+        }
+        mojo_node->text = label;
+
+        switch (fc.form_control_type()) {
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_CHECKBOX:
+            mojo_node->role = ai_overlay_dialog::mojom::NodeRole::kCheckbox;
+            break;
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_RADIO:
+            mojo_node->role = ai_overlay_dialog::mojom::NodeRole::kRadio;
+            break;
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_TEXT:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_EMAIL:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_NUMBER:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_PASSWORD:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_SEARCH:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_TELEPHONE:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_URL:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_TEXT_AREA:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_CONTENT_EDITABLE:
+            mojo_node->role = ai_overlay_dialog::mojom::NodeRole::kTextbox;
+            break;
+          case optimization_guide::proto::FORM_CONTROL_TYPE_BUTTON_BUTTON:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_BUTTON_SUBMIT:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_BUTTON_RESET:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_INPUT_BUTTON:
+            mojo_node->role = ai_overlay_dialog::mojom::NodeRole::kButton;
+            break;
+          case optimization_guide::proto::FORM_CONTROL_TYPE_SELECT_ONE:
+          case optimization_guide::proto::FORM_CONTROL_TYPE_SELECT_MULTIPLE:
+            mojo_node->role = ai_overlay_dialog::mojom::NodeRole::kCombobox;
+            break;
+          default:
+            mojo_node->role = ai_overlay_dialog::mojom::NodeRole::kInput;
+            break;
+        }
+
+        if (consumed_next_sibling) {
+          ++i;
+        }
+      } else if (attrs.attribute_type() ==
+                 optimization_guide::proto::CONTENT_ATTRIBUTE_ANCHOR) {
+        mojo_node->is_interactive = true;
+        mojo_node->role = ai_overlay_dialog::mojom::NodeRole::kLink;
+      }
+
+      if (attrs.has_interaction_info()) {
+        const auto& ii = attrs.interaction_info();
+        if (ii.clickability_reasons_size() > 0 || ii.is_clickable()) {
+          mojo_node->is_interactive = true;
+        }
       }
     }
 
-    if (attrs.has_text_data()) {
-      mojo_node->text = attrs.text_data().text_content();
+    if (proto_node.children_nodes_size() > 0) {
+      ConvertContentNodesToMojo(proto_node.children_nodes(),
+                                mojo_node->children);
     }
-    if (attrs.has_anchor_data()) {
-      mojo_node->url = GURL(attrs.anchor_data().url());
-    }
+    mojo_nodes.push_back(std::move(mojo_node));
   }
-  for (const auto& child : proto_node.children_nodes()) {
-    mojo_node->children.push_back(ConvertContentNodeToMojo(child));
-  }
-  return mojo_node;
 }
 
 std::string FindUrlForDomNodeId(
@@ -152,6 +263,7 @@ void PageContextMonitor::StartNewFetch() {
 void PageContextMonitor::OnFetchComplete(
     page_content_annotations::FetchPageContextResultCallbackArg result) {
   if (!result.has_value()) {
+    LOG(WARNING) << "FetchPageContextResult returned error";
     return;
   }
 
@@ -163,8 +275,17 @@ void PageContextMonitor::OnFetchComplete(
         fetch_result.annotated_page_content_result.value().proto;
     ai_overlay_dialog::mojom::PageContentNodePtr root_mojo_node;
     if (last_page_content_->has_root_node()) {
-      root_mojo_node =
-          ConvertContentNodeToMojo(last_page_content_->root_node());
+      const auto& root_proto = last_page_content_->root_node();
+      root_mojo_node = ai_overlay_dialog::mojom::PageContentNode::New();
+      if (root_proto.has_content_attributes()) {
+        const auto& attrs = root_proto.content_attributes();
+        root_mojo_node->dom_node_id = attrs.common_ancestor_dom_node_id();
+        if (attrs.has_text_data()) {
+          root_mojo_node->text = attrs.text_data().text_content();
+        }
+      }
+      ConvertContentNodesToMojo(root_proto.children_nodes(),
+                                root_mojo_node->children);
     }
 
     // If the page looks mostly empty, crudely wait a bit and retry in case the
