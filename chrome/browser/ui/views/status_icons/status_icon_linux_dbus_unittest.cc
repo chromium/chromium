b65c0243e99b94931b8c606af437c2223893d9d9
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2026-08-11 23:31:32 -0700

    Revert "Fix D-Bus unique name destination resolution for status icons"
    This reverts commit a814681acc9b0ac3e1ece9bb923d01fbe6656c35.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8673696426560082881
    
    Sample failed build: https://ci.chromium.org/b/8673696426560082881
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F8236711&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8673696426560082881&type=BUG
    
    Original change's description:
    > Fix D-Bus unique name destination resolution for status icons
    >
    > Tray hosts using GDBusProxy (e.g. Cinnamon, XFCE, GNOME extensions)
    > resolve the owner of the well-known status icon service name
    > (org.freedesktop.StatusNotifierItem-PID-ID) and then address subsequent
    > method calls and property queries directly to the unique connection name
    > (:1.x).
    >
    > Previously, StatusIconLinuxDbus::Multiplexer::GetIcon only looked up
    > destinations in its map of well-known service names. When addressed via
    > a unique connection name, the lookup failed and returned a D-Bus error,
    > leaving status icons unresponsive and without images.
    >
    > This CL fixes the issue by updating Multiplexer::GetIcon to fall back to
    > the first registered status icon when destination lookup fails and
    > icons_ is non-empty.
    >
    > Fixed: 543471702
    > Change-Id: Id2c828498b1b94ceacf9bc9e0699a4f78c494144
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8236711
    > Auto-Submit: Thomas Anderson <thomasanderson@chromium.org>
    > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1677795}
    >
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I10d82a78cb46745ac9bd60165ca4a23bb85985d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8245962
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1677797}

--- chrome/browser/ui/views/status_icons/status_icon_linux_dbus_unittest.cc
+++ chrome/browser/ui/views/status_icons/status_icon_linux_dbus_unittest.cc
@@ -12,91 +12,19 @@
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/task/single_thread_task_runner.h"
 #include "base/test/bind.h"
+#include "base/test/task_environment.h"
 #include "base/test/test_future.h"
-#include "content/public/test/browser_task_environment.h"
 #include "dbus/bus.h"
-#include "dbus/dbus-shared.h"
 #include "dbus/message.h"
 #include "dbus/mock_bus.h"
 #include "dbus/mock_exported_object.h"
-#include "dbus/mock_object_proxy.h"
 #include "dbus/object_path.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/gfx/image/image_skia.h"
-#include "ui/linux/status_icon_linux.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 
 using testing::_;
-
-namespace {
-
-class TestDelegate : public ui::StatusIconLinux::Delegate {
- public:
-  void OnClick() override {}
-  bool HasClickAction() override { return false; }
-  const gfx::ImageSkia& GetImage() const override { return image_; }
-  const gfx::VectorIcon* GetIcon() const override { return nullptr; }
-  const std::u16string& GetToolTip() const override { return tool_tip_; }
-  ui::MenuModel* GetMenuModel() const override { return nullptr; }
-  void OnImplInitializationFailed() override {}
-
- private:
-  gfx::ImageSkia image_;
-  std::u16string tool_tip_;
-};
-
-void OnBusProxyCall(dbus::MethodCall* method_call,
-                    int timeout_ms,
-                    dbus::ObjectProxy::ResponseOrErrorCallback callback) {
-  if (method_call->GetSerial() == 0) {
-    method_call->SetSerial(1);
-  }
-  auto response = dbus::Response::FromMethodCall(method_call);
-  dbus::MessageWriter writer(response.get());
-  if (method_call->GetMember() == "NameHasOwner") {
-    writer.AppendBool(true);
-  }
-  base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
-      FROM_HERE, base::BindOnce(
-                     [](dbus::ObjectProxy::ResponseOrErrorCallback callback,
-                        std::unique_ptr<dbus::Response> response) {
-                       std::move(callback).Run(response.get(), nullptr);
-                     },
-                     std::move(callback), std::move(response)));
-}
-
-void OnWatcherProxyCall(std::string* registered_service_out,
-                        dbus::MethodCall* method_call,
-                        int timeout_ms,
-                        dbus::ObjectProxy::ResponseOrErrorCallback callback) {
-  if (method_call->GetSerial() == 0) {
-    method_call->SetSerial(1);
-  }
-  auto response = dbus::Response::FromMethodCall(method_call);
-  dbus::MessageWriter writer(response.get());
-  if (method_call->GetMember() == "Get") {
-    dbus::MessageWriter variant_writer(nullptr);
-    writer.OpenVariant("b", &variant_writer);
-    variant_writer.AppendBool(true);
-    writer.CloseContainer(&variant_writer);
-  } else if (method_call->GetMember() == "RegisterStatusNotifierItem") {
-    if (registered_service_out) {
-      dbus::MessageReader reader(method_call);
-      reader.PopString(registered_service_out);
-    }
-  }
-  base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
-      FROM_HERE, base::BindOnce(
-                     [](dbus::ObjectProxy::ResponseOrErrorCallback callback,
-                        std::unique_ptr<dbus::Response> response) {
-                       std::move(callback).Run(response.get(), nullptr);
-                     },
-                     std::move(callback), std::move(response)));
-}
-
-}  // namespace
 
 class StatusIconLinuxDbusTest : public testing::Test {
  public:
@@ -105,75 +33,18 @@ class StatusIconLinuxDbusTest : public testing::Test {
 
   void SetUp() override {
     bus_ = base::MakeRefCounted<dbus::MockBus>(dbus::Bus::Options());
-    bus_proxy_ = base::MakeRefCounted<dbus::MockObjectProxy>(
-        bus_.get(), DBUS_SERVICE_DBUS, dbus::ObjectPath(DBUS_PATH_DBUS));
-    watcher_proxy_ = base::MakeRefCounted<dbus::MockObjectProxy>(
-        bus_.get(), "org.kde.StatusNotifierWatcher",
-        dbus::ObjectPath("/StatusNotifierWatcher"));
-    exported_item_object_ = base::MakeRefCounted<dbus::MockExportedObject>(
-        bus_.get(), dbus::ObjectPath("/StatusNotifierItem/1"));
-    exported_menu_object_ = base::MakeRefCounted<dbus::MockExportedObject>(
-        bus_.get(), dbus::ObjectPath("/org/chromium/DbusMenu/1"));
-
-    EXPECT_CALL(*bus_, GetDBusTaskRunner())
-        .WillRepeatedly(testing::Return(
-            base::SingleThreadTaskRunner::GetCurrentDefault().get()));
-
-    EXPECT_CALL(*bus_, GetObjectProxy(_, _))
-        .WillRepeatedly([this](std::string_view service_name,
-                               const dbus::ObjectPath& object_path) {
-          if (service_name == "org.kde.StatusNotifierWatcher") {
-            return watcher_proxy_.get();
-          }
-          return bus_proxy_.get();
-        });
-
-    EXPECT_CALL(*bus_, GetExportedObject(_))
-        .WillRepeatedly([this](const dbus::ObjectPath& object_path) {
-          if (object_path.value().starts_with("/org/chromium/DbusMenu")) {
-            return exported_menu_object_.get();
-          }
-          return exported_item_object_.get();
-        });
-
-    EXPECT_CALL(*exported_menu_object_, ExportMethod(_, _, _, _))
-        .WillRepeatedly(
-            [](const std::string& interface_name,
-               const std::string& method_name,
-               dbus::ExportedObject::MethodCallCallback callback,
-               dbus::ExportedObject::OnExportedCallback on_exported_callback) {
-              std::move(on_exported_callback)
-                  .Run(interface_name, method_name, true);
-            });
-
-    EXPECT_CALL(*bus_proxy_, CallMethodWithErrorResponse(_, _, _))
-        .WillRepeatedly(&OnBusProxyCall);
-
-    EXPECT_CALL(*watcher_proxy_, CallMethodWithErrorResponse(_, _, _))
-        .WillRepeatedly(testing::Invoke(
-            [](dbus::MethodCall* method_call, int timeout_ms,
-               dbus::ObjectProxy::ResponseOrErrorCallback callback) {
-              OnWatcherProxyCall(nullptr, method_call, timeout_ms,
-                                 std::move(callback));
-            }));
-
-    EXPECT_CALL(*bus_, RequestOwnership(_, _, _))
-        .WillRepeatedly([](const std::string& service_name,
-                           dbus::Bus::ServiceOwnershipOptions options,
-                           dbus::Bus::OnOwnershipCallback callback) {
-          base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
-              FROM_HERE,
-              base::BindOnce(std::move(callback), service_name, true));
-        });
+    exported_object_ = base::MakeRefCounted<dbus::MockExportedObject>(
+        bus_.get(), dbus::ObjectPath("/StatusNotifierItem"));
+    EXPECT_CALL(*bus_,
+                GetExportedObject(dbus::ObjectPath("/StatusNotifierItem")))
+        .WillRepeatedly(testing::Return(exported_object_.get()));
   }
 
  protected:
-  content::BrowserTaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_{
+      base::test::TaskEnvironment::MainThreadType::UI};
   scoped_refptr<dbus::MockBus> bus_;
-  scoped_refptr<dbus::MockObjectProxy> bus_proxy_;
-  scoped_refptr<dbus::MockObjectProxy> watcher_proxy_;
-  scoped_refptr<dbus::MockExportedObject> exported_item_object_;
-  scoped_refptr<dbus::MockExportedObject> exported_menu_object_;
+  scoped_refptr<dbus::MockExportedObject> exported_object_;
 };
 
 TEST_F(StatusIconLinuxDbusTest, InterfaceSupport) {
@@ -181,7 +52,7 @@ TEST_F(StatusIconLinuxDbusTest, InterfaceSupport) {
            dbus::ExportedObject::MethodCallCallback>
       callbacks;
 
-  EXPECT_CALL(*exported_item_object_, ExportMethod(_, _, _, _))
+  EXPECT_CALL(*exported_object_, ExportMethod(_, _, _, _))
       .WillRepeatedly(
           [&](const std::string& interface_name, const std::string& method_name,
               dbus::ExportedObject::MethodCallCallback callback,
@@ -191,11 +62,10 @@ TEST_F(StatusIconLinuxDbusTest, InterfaceSupport) {
                 .Run(interface_name, method_name, true);
           });
 
-  TestDelegate delegate;
-  auto status_icon = base::MakeRefCounted<StatusIconLinuxDbus>(bus_);
-  status_icon->SetDelegate(&delegate);
-
-  task_environment_.RunUntilIdle();
+  StatusIconLinuxDbus::ExportMultiplexerMethodsForTesting(bus_.get());
+  absl::Cleanup cleanup = [this] {
+    StatusIconLinuxDbus::UnexportMultiplexerMethodsForTesting(bus_.get());
+  };
 
   // Check that methods are exported for both org.kde and org.freedesktop.
   EXPECT_TRUE(callbacks.contains({"org.kde.StatusNotifierItem", "Activate"}));
@@ -217,12 +87,12 @@ TEST_F(StatusIconLinuxDbusTest, InterfaceSupport) {
       callbacks.contains({"org.freedesktop.DBus.Properties", "GetAll"}));
 }
 
-TEST_F(StatusIconLinuxDbusTest, PropertiesGetAll) {
+TEST_F(StatusIconLinuxDbusTest, PropertiesGetAllInterfaces) {
   std::map<std::pair<std::string, std::string>,
            dbus::ExportedObject::MethodCallCallback>
       callbacks;
 
-  EXPECT_CALL(*exported_item_object_, ExportMethod(_, _, _, _))
+  EXPECT_CALL(*exported_object_, ExportMethod(_, _, _, _))
       .WillRepeatedly(
           [&](const std::string& interface_name, const std::string& method_name,
               dbus::ExportedObject::MethodCallCallback callback,
@@ -232,11 +102,10 @@ TEST_F(StatusIconLinuxDbusTest, PropertiesGetAll) {
                 .Run(interface_name, method_name, true);
           });
 
-  TestDelegate delegate;
-  auto status_icon = base::MakeRefCounted<StatusIconLinuxDbus>(bus_);
-  status_icon->SetDelegate(&delegate);
-
-  task_environment_.RunUntilIdle();
+  StatusIconLinuxDbus::ExportMultiplexerMethodsForTesting(bus_.get());
+  absl::Cleanup cleanup = [this] {
+    StatusIconLinuxDbus::UnexportMultiplexerMethodsForTesting(bus_.get());
+  };
 
   auto get_all_callback =
       callbacks[{"org.freedesktop.DBus.Properties", "GetAll"}];
@@ -245,43 +114,36 @@ TEST_F(StatusIconLinuxDbusTest, PropertiesGetAll) {
   // Test GetAll with org.kde.StatusNotifierItem
   {
     dbus::MethodCall method_call("org.freedesktop.DBus.Properties", "GetAll");
+    method_call.SetDestination("org.freedesktop.StatusNotifierItem-123-1");
     method_call.SetSerial(1);
     dbus::MessageWriter writer(&method_call);
     writer.AppendString("org.kde.StatusNotifierItem");
 
     base::test::TestFuture<std::unique_ptr<dbus::Response>> future;
     get_all_callback.Run(&method_call, future.GetCallback());
-    EXPECT_NE(future.Take(), nullptr);
+    // Unregistered destination returns nullptr.
+    EXPECT_EQ(future.Take(), nullptr);
   }
 
   // Test GetAll with org.freedesktop.StatusNotifierItem
   {
     dbus::MethodCall method_call("org.freedesktop.DBus.Properties", "GetAll");
+    method_call.SetDestination("org.freedesktop.StatusNotifierItem-123-1");
     method_call.SetSerial(2);
     dbus::MessageWriter writer(&method_call);
     writer.AppendString("org.freedesktop.StatusNotifierItem");
 
     base::test::TestFuture<std::unique_ptr<dbus::Response>> future;
     get_all_callback.Run(&method_call, future.GetCallback());
-    EXPECT_NE(future.Take(), nullptr);
-  }
-
-  // Test GetAll with empty interface string
-  {
-    dbus::MethodCall method_call("org.freedesktop.DBus.Properties", "GetAll");
-    method_call.SetSerial(3);
-    dbus::MessageWriter writer(&method_call);
-    writer.AppendString("");
-
-    base::test::TestFuture<std::unique_ptr<dbus::Response>> future;
-    get_all_callback.Run(&method_call, future.GetCallback());
-    EXPECT_NE(future.Take(), nullptr);
+    // Unregistered destination returns nullptr.
+    EXPECT_EQ(future.Take(), nullptr);
   }
 
   // Test GetAll with invalid interface
   {
     dbus::MethodCall method_call("org.freedesktop.DBus.Properties", "GetAll");
-    method_call.SetSerial(4);
+    method_call.SetDestination("org.freedesktop.StatusNotifierItem-123-1");
+    method_call.SetSerial(3);
     dbus::MessageWriter writer(&method_call);
     writer.AppendString("org.invalid.Interface");
 
@@ -289,37 +151,4 @@ TEST_F(StatusIconLinuxDbusTest, PropertiesGetAll) {
     get_all_callback.Run(&method_call, future.GetCallback());
     EXPECT_EQ(future.Take(), nullptr);
   }
-}
-
-TEST_F(StatusIconLinuxDbusTest, RegisterStatusNotifierItem) {
-  std::string registered_service;
-
-  EXPECT_CALL(*exported_item_object_, ExportMethod(_, _, _, _))
-      .WillRepeatedly(
-          [](const std::string& interface_name, const std::string& method_name,
-             dbus::ExportedObject::MethodCallCallback callback,
-             dbus::ExportedObject::OnExportedCallback on_exported_callback) {
-            std::move(on_exported_callback)
-                .Run(interface_name, method_name, true);
-          });
-
-  EXPECT_CALL(*watcher_proxy_, CallMethodWithErrorResponse(_, _, _))
-      .WillRepeatedly(testing::Invoke(
-          [&](dbus::MethodCall* method_call, int timeout_ms,
-              dbus::ObjectProxy::ResponseOrErrorCallback callback) {
-            OnWatcherProxyCall(&registered_service, method_call, timeout_ms,
-                               std::move(callback));
-          }));
-
-  TestDelegate delegate;
-  auto status_icon = base::MakeRefCounted<StatusIconLinuxDbus>(bus_);
-  status_icon->SetDelegate(&delegate);
-
-  task_environment_.RunUntilIdle();
-
-  // The service string passed to RegisterStatusNotifierItem should combine
-  // the well-known service name and the unique object path.
-  EXPECT_THAT(registered_service,
-              testing::StartsWith("org.freedesktop.StatusNotifierItem-"));
-  EXPECT_THAT(registered_service, testing::HasSubstr("/StatusNotifierItem/"));
 }
