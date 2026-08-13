5385563fc4014932de00ccc16e894bc69bafdb00
Andrew Grieve <agrieve@chromium.org>
2026-08-13 11:59:30 -0700

    [M152] Android: Add DUMP_WITHOUT_CRASHING for icu failure in bookmarks
    Original change's description:
    > Android: Add DUMP_WITHOUT_CRASHING for icu failure in bookmarks
    >
    > Bug: 40064988
    > Change-Id: If5e1e954ea51a661d54d4a5afd4b04171f9002b6
    > Cq-Include-Trybots: luci.chrome.try:android-internal-dbg,android-internal-binary-size
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8202168
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1674230}
    
    (cherry picked from commit a33191bdeb07604efff5ace003edcb9189ea7460)
    
    Bug: 545624761,40064988
    Change-Id: If5e1e954ea51a661d54d4a5afd4b04171f9002b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257990
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7977@{#1432}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- components/bookmarks/browser/titled_url_index.cc
+++ components/bookmarks/browser/titled_url_index.cc
@@ -27,6 +27,21 @@
 #include "third_party/icu/source/common/unicode/normalizer2.h"
 #include "third_party/icu/source/common/unicode/utypes.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/apk_assets.h"
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/files/scoped_file.h"
+#include "base/path_service.h"
+
+namespace base::i18n {
+extern bool g_icu_initialized;
+}
+
+#endif  // BUILDFLAG(IS_ANDROID)
+
 namespace bookmarks {
 
 namespace {
@@ -35,6 +50,35 @@ namespace {
 bool IsPrefix(const std::u16string& prefix, const std::u16string& string) {
   return prefix.size() <= string.size() &&
          prefix.compare(0, prefix.size(), string, 0, prefix.size()) == 0;
+}
+
+void DumpNormalizationFailure(bool retry_worked) {
+#if BUILDFLAG(IS_ANDROID)
+  std::string fallback_asset_path;
+  base::FilePath asset_path;
+  // This is the fall-back path used for tests.
+  if (base::PathService::Get(base::DIR_ASSETS, &asset_path)) {
+    asset_path = asset_path.AppendASCII("icudtl.dat");
+    if (base::PathExists(asset_path)) {
+      fallback_asset_path = asset_path.value();
+    }
+  }
+
+  int64_t file_size = -1;
+  base::MemoryMappedFile::Region region;
+  base::ScopedFD fd(base::android::OpenApkAsset("assets/icudtl.dat", &region));
+  if (fd.is_valid()) {
+    file_size = static_cast<int64_t>(region.size);
+  }
+
+  SCOPED_CRASH_KEY_STRING256("bookmarks", "fallback_asset_path",
+                             fallback_asset_path);
+  SCOPED_CRASH_KEY_NUMBER("bookmarks", "file_size", file_size);
+  SCOPED_CRASH_KEY_BOOL("bookmarks", "icu_init", base::i18n::g_icu_initialized);
+  SCOPED_CRASH_KEY_BOOL("bookmarks", "retry_worked", retry_worked);
+
+  base::debug::DumpWithoutCrashing();
+#endif  // BUILDFLAG(IS_ANDROID)
 }
 
 }  // namespace
@@ -129,6 +173,10 @@ std::u16string TitledUrlIndex::Normalize(std::u16string_view text) {
       icu::Normalizer2::getInstance(nullptr, "nfkc", UNORM2_COMPOSE, status);
   if (U_FAILURE(status)) {
     LOG(ERROR) << "failed to create a normalizer: " << u_errorName(status);
+    normalizer2 =
+        icu::Normalizer2::getInstance(nullptr, "nfkc", UNORM2_COMPOSE, status);
+    bool retry_worked = !U_FAILURE(status);
+    DumpNormalizationFailure(retry_worked);
     return std::u16string(text);
   }
   icu::UnicodeString unicode_text(text.data(),
