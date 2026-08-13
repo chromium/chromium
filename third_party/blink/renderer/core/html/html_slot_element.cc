629c9e55b5291191b6c15bcfa20fd279908b6610
Joey Arhar <jarhar@chromium.org>
2026-08-13 07:13:47 -0700

    Remove SlotAssignNotifyDifferentShadowRoots flag
    This flag has been enabled by default long enough that it can be safely
    removed.
    
    Bug: 517322978
    Change-Id: I0db0b7ad12105ee73a79d80cb2a58646daa75e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8239279
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678805}

--- third_party/blink/renderer/core/html/html_slot_element.cc
+++ third_party/blink/renderer/core/html/html_slot_element.cc
@@ -261,30 +261,19 @@ void HTMLSlotElement::Assign(const HeapVector<Member<Node>>& nodes) {
   }
 
   if (!changed_slots.empty()) {
-    if (RuntimeEnabledFeatures::SlotAssignNotifyDifferentShadowRootsEnabled()) {
-      if (shadow_root) {
-        for (HTMLSlotElement& slot :
-             Traversal<HTMLSlotElement>::DescendantsOf(*shadow_root)) {
-          if (changed_slots.Take(&slot)) {
-            slot.DidSlotChange(SlotChangeType::kSignalSlotChangeEvent);
-          }
+    if (shadow_root) {
+      for (HTMLSlotElement& slot :
+           Traversal<HTMLSlotElement>::DescendantsOf(*shadow_root)) {
+        if (changed_slots.Take(&slot)) {
+          slot.DidSlotChange(SlotChangeType::kSignalSlotChangeEvent);
         }
       }
-      // A previous slot may belong to a different shadow tree than `this`, or
-      // `this` may not be in a shadow tree at all. Such slots are not reached
-      // by the traversal above; signal them here.
-      for (HTMLSlotElement* slot : changed_slots) {
-        slot->DidSlotChange(SlotChangeType::kSignalSlotChangeEvent);
-      }
-    } else {
-      if (shadow_root) {
-        for (HTMLSlotElement& slot :
-             Traversal<HTMLSlotElement>::DescendantsOf(*shadow_root)) {
-          if (changed_slots.Contains(&slot)) {
-            slot.DidSlotChange(SlotChangeType::kSignalSlotChangeEvent);
-          }
-        }
-      }
+    }
+    // A previous slot may belong to a different shadow tree than `this`, or
+    // `this` may not be in a shadow tree at all. Such slots are not reached
+    // by the traversal above; signal them here.
+    for (HTMLSlotElement* slot : changed_slots) {
+      slot->DidSlotChange(SlotChangeType::kSignalSlotChangeEvent);
     }
   }
 }
