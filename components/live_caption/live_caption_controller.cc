8e96a7f2d14821c95aaffe0be8c9d0354782fc28
Yiren Wang <yrw@chromium.org>
2026-08-12 18:23:55 -0700

    Use ScopedObservation for SodaInstaller in LiveCaptionController
    LiveCaptionController is a KeyedService whose lifetime is tied to a
    Profile, whereas speech::SodaInstaller is a singleton. When SODA is
    downloading in the background such as when triggered by Bluefin,
    destroying LiveCaptionController prior to download completion leaves a
    dangling observer registered in SodaInstaller. Subsequent SODA download
    progress events can trigger a CHECK failure in base::ObserverList due to
    an invalidated WeakPtr.
    
    This CL switches LiveCaptionController to use base::ScopedObservation
    for speech::SodaInstaller, ensuring that observation is automatically
    detached when LiveCaptionController is destroyed. It also resets the
    observation upon SODA installation error.
    
    Bug: 545492402
    Change-Id: I80d669326a3ca031adaaab1db6fbea890770a94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8252478
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678487}

--- components/live_caption/live_caption_controller.cc
+++ components/live_caption/live_caption_controller.cc
@@ -127,14 +127,16 @@ void LiveCaptionController::OnFirstListenerAdded() {
   // SODAInstaller calls OnSodaInstalled on its observers.
   if (!speech::SodaInstaller::GetInstance()->IsSodaInstalled(
           speech::GetLanguageCode(GetLanguageCode()))) {
-    speech::SodaInstaller::GetInstance()->AddObserver(this);
+    if (!soda_installer_observation_.IsObserving()) {
+      soda_installer_observation_.Observe(speech::SodaInstaller::GetInstance());
+    }
     speech::SodaInstaller::GetInstance()->Init(profile_prefs(), global_prefs_);
   }
 }
 
 void LiveCaptionController::OnLastListenerRemoved() {
   // We might not have installed a listener, but that's okay.
-  speech::SodaInstaller::GetInstance()->RemoveObserver(this);
+  soda_installer_observation_.Reset();
   speech::SodaInstaller::GetInstance()->SetUninstallTimer(global_prefs_,
                                                           GetLanguageCode());
 }
@@ -178,15 +180,22 @@ void LiveCaptionController::OnSodaInstalled(
       prefs::IsLanguageCodeForLiveCaption(language_code, profile_prefs());
 
   if (is_language_code_for_live_caption) {
-    speech::SodaInstaller::GetInstance()->RemoveObserver(this);
+    soda_installer_observation_.Reset();
   }
 }
 
-// SODA install errors are observed and handled in the Settings WebUI:
+// UI strings for SODA install errors are handled in the Settings WebUI:
 // chrome/browser/ui/webui/settings/captions_handler.cc
 void LiveCaptionController::OnSodaInstallError(
     speech::LanguageCode language_code,
-    speech::SodaInstaller::ErrorCode error_code) {}
+    speech::SodaInstaller::ErrorCode error_code) {
+  bool is_language_code_for_live_caption =
+      prefs::IsLanguageCodeForLiveCaption(language_code, profile_prefs());
+
+  if (is_language_code_for_live_caption) {
+    soda_installer_observation_.Reset();
+  }
+}
 
 const std::string LiveCaptionController::GetLanguageCode() const {
   return prefs::GetLiveCaptionLanguageCode(profile_prefs());
