36c67f043fd8a12418dd9d8cc6f2d6eca4b0d907
Lukasz Anforowicz <lukasza@chromium.org>
2026-08-13 09:48:28 -0700

    Roll icu_time_data: 2.2.0 => 2.2.1 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * icu_time_data: 2.2.0 => 2.2.1;
      https://docs.rs/crate/icu_time_data/2.2.1
    
    Bug: None
    Change-Id: Ie5efce458d5ccb538f408f3bbd88901d03562bc2
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8232358
    Reviewed-by: Liza Burakova <liza@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1678890}

--- third_party/rust/chromium_crates_io/vendor/icu_time_data-v2/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/icu_time_data-v2/src/lib.rs
@@ -6,6 +6,6 @@
 // by `tools/crates/run_gnrt.py vendor`. Do not edit!
 //
 // This is an empty placeholder that has replaced the
-// `icu_time_data-2.2.0` crate.
+// `icu_time_data-2.2.1` crate.
 //
 // See `//tools/crates/gnrt/removed_crate.md` to learn more.
