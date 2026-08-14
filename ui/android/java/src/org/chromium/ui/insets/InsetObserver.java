df3297037e832c11efb6e9abe43b79e17ca44f7c
Calder Kitagawa <ckitagawa@chromium.org>
2026-08-13 22:15:11 -0700

    Filter duplicate InsetObserver dispatch
    Bug: 546231218
    Change-Id: I3e92c93daf9b673c900f88f0432a3ae88989469c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8259613
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1679366}

--- ui/android/java/src/org/chromium/ui/insets/InsetObserver.java
+++ ui/android/java/src/org/chromium/ui/insets/InsetObserver.java
@@ -578,12 +578,14 @@ public class InsetObserver implements OnApplyWindowInsetsListener {
      * @param bottom The updated bottom inset.
      */
     private void onInsetChanged(int left, int top, int right, int bottom) {
-        if (mWindowInsets.left != left
-                || mWindowInsets.top != top
-                || mWindowInsets.right != right
-                || mWindowInsets.bottom != bottom) {
-            mWindowInsets.set(left, top, right, bottom);
+        if (mWindowInsets.left == left
+                && mWindowInsets.top == top
+                && mWindowInsets.right == right
+                && mWindowInsets.bottom == bottom) {
+            return;
         }
+
+        mWindowInsets.set(left, top, right, bottom);
 
         for (WindowInsetObserver observer : mObservers) {
             observer.onInsetChanged();
