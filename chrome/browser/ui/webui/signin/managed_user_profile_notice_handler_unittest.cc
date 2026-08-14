89e206abc792f6933ef8e1d122af8f95ea6e8ffa
Wenbin Zhang <wenbinzhang@google.com>
2026-08-13 21:55:36 -0700

    [bedrock] Remove Browser* from managed_user_profile_notice_handler_unittest
    Converts ManagedUserProfileNoticeHandlerTestBase from
    BrowserWithTestWindowTest to testing::Test, using TestingProfileManager,
    TestWebContentsFactory, and MockBrowserWindowInterface.
    
    TAG=agy
    CONV=a89fc9fb-6826-4862-9b08-55f05a3a432a
    
    Change-Id: Iee60fbae8711eea6db749390ab1d7a55afd7247c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8254942
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1679355}

--- chrome/browser/ui/webui/signin/managed_user_profile_notice_handler_unittest.cc
+++ chrome/browser/ui/webui/signin/managed_user_profile_notice_handler_unittest.cc
@@ -20,18 +20,20 @@
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_test_util.h"
-#include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/browser_window/test/mock_browser_window_interface.h"
 #include "chrome/browser/ui/webui/signin/signin_utils.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
-#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
+#include "chrome/test/base/testing_profile_manager.h"
 #include "components/policy/core/common/management/scoped_management_service_override_for_testing.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
 #include "components/user_manager/user_names.h"
 #include "content/public/test/browser_task_environment.h"
+#include "content/public/test/test_web_contents_factory.h"
 #include "content/public/test/test_web_ui.h"
 #include "content/public/test/web_contents_tester.h"
 #include "google_apis/gaia/gaia_id.h"
@@ -57,24 +59,24 @@ ProfileAttributesEntry* GetProfileEntry(Profile* profile) {
 
 }  // namespace
 
-class ManagedUserProfileNoticeHandlerTestBase
-    : public BrowserWithTestWindowTest {
+class ManagedUserProfileNoticeHandlerTestBase : public testing::Test {
  public:
-  ManagedUserProfileNoticeHandlerTestBase() = default;
+  ManagedUserProfileNoticeHandlerTestBase()
+      : profile_manager_(TestingBrowserProcess::GetGlobal()) {}
   ManagedUserProfileNoticeHandlerTestBase(
       const ManagedUserProfileNoticeHandlerTestBase&) = delete;
-  ManagedUserProfileNoticeHandler& operator=(
+  ManagedUserProfileNoticeHandlerTestBase& operator=(
       const ManagedUserProfileNoticeHandlerTestBase&) = delete;
   ~ManagedUserProfileNoticeHandlerTestBase() override = default;
 
-  // BrowserWithTestWindowTest:
   void SetUp() override {
-    BrowserWithTestWindowTest::SetUp();
-    chrome::NewTab(browser(), NewTabTypes::kNoUserAction);
+    testing::Test::SetUp();
+    ASSERT_TRUE(profile_manager_.SetUp());
+    profile_ = profile_manager_.CreateTestingProfile("Primary Profile");
 
     web_ui_ = std::make_unique<content::TestWebUI>();
     web_ui_->set_web_contents(
-        browser()->tab_strip_model()->GetActiveWebContents());
+        web_contents_factory_.CreateWebContents(profile_));
 
     account_info_.email = user_manager::kStubUserEmail;
     account_info_.gaia = GaiaId(user_manager::kStubUserId);
@@ -88,7 +90,7 @@ class ManagedUserProfileNoticeHandlerTestBase
     message_handler_.reset();
 
     message_handler_ = std::make_unique<ManagedUserProfileNoticeHandler>(
-        browser(), screen_type, std::move(dialog_params));
+        &mock_browser_window_interface_, screen_type, std::move(dialog_params));
     message_handler_->set_web_ui_for_test(web_ui());
     message_handler_->RegisterMessages();
   }
@@ -97,7 +99,8 @@ class ManagedUserProfileNoticeHandlerTestBase
 
   void TearDown() override {
     message_handler_.reset();
-    BrowserWithTestWindowTest::TearDown();
+    web_ui_.reset();
+    testing::Test::TearDown();
   }
 
   content::TestWebUI* web_ui() { return web_ui_.get(); }
@@ -105,6 +108,11 @@ class ManagedUserProfileNoticeHandlerTestBase
   ManagedUserProfileNoticeHandler* handler() { return message_handler_.get(); }
 
  private:
+  content::BrowserTaskEnvironment task_environment_;
+  TestingProfileManager profile_manager_;
+  raw_ptr<TestingProfile> profile_ = nullptr;
+  content::TestWebContentsFactory web_contents_factory_;
+  testing::NiceMock<MockBrowserWindowInterface> mock_browser_window_interface_;
   std::unique_ptr<content::TestWebUI> web_ui_;
   AccountInfo account_info_;
 
