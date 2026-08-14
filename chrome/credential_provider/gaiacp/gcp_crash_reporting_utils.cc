c0c393f190ef81c4f8154c44dff09d48c92011b7
Greg Thompson <grt@chromium.org>
2026-08-13 23:44:35 -0700

    [M152] [GCPW] Put the crashpad database in GCPW's data directory
    Original change's description:
    > [GCPW] Put the crashpad database in GCPW's data directory
    >
    > This subdirectory of C:\ProgramData is already DACLd properly so that
    > only SYSTEM and local admin may access it, so it's a suitable place for
    > the crashpad database.
    >
    > Since Crashpad cannot function without a directory in which to work,
    > this change gently refactors initialization:
    >
    > * The crash dump location is only computed once; and the leaf directory
    >   itself is no longer created by GCPW or its installer. Crashpad will
    >   create it as needed.
    > * If the crash dump location's parent directory (GCPW's "data directory"
    >   within C:\ProgramData) cannot be created, crash initialization is
    >   skipped.
    > * GCPW's CrashReporterClient no longer recomputes the crash dump
    >   location, but rather holds and returns whatever location it was giving
    >   at construction when asked.
    >
    > Fixed: 517612295
    > Change-Id: I502af034e238de8e74e4fe3c81672bf60845dbb6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8182731
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Commit-Queue: Greg Thompson <grt@chromium.org>
    > Auto-Submit: Greg Thompson <grt@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1673938}
    
    (cherry picked from commit 34c840263df6758e27bd5f15dc4954f322b7a15e)
    
    Bug: 546215995,517612295
    Change-Id: I502af034e238de8e74e4fe3c81672bf60845dbb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258914
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/7977@{#1479}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- chrome/credential_provider/gaiacp/gcp_crash_reporting_utils.cc
+++ chrome/credential_provider/gaiacp/gcp_crash_reporting_utils.cc
@@ -9,19 +9,17 @@
 #include <string>
 
 #include "base/files/file_path.h"
-#include "base/files/file_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/win/registry.h"
 #include "build/branding_buildflags.h"
 #include "chrome/credential_provider/common/gcp_strings.h"
 #include "chrome/credential_provider/gaiacp/gcp_crash_reporter_client.h"
+#include "chrome/credential_provider/gaiacp/gcp_utils.h"
 #include "chrome/credential_provider/gaiacp/logging.h"
 #include "components/crash/core/common/crash_key.h"
 #include "components/crash/core/common/crash_keys.h"
 
 namespace {
-
-constexpr wchar_t kCrashpadDumpsFolder[] = L"GCPW Crashpad";
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 void SetCurrentVersionCrashKey() {
@@ -40,48 +38,19 @@ void SetCurrentVersionCrashKey() {
 }
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
-// Returns the SYSTEM version of TEMP. We do this instead of GetTempPath so
-// that both elevated and SYSTEM runs share the same directory.
-base::FilePath GetSystemTempFolder() {
-  base::win::RegKey reg_key(
-      HKEY_LOCAL_MACHINE,
-      L"SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment",
-      KEY_QUERY_VALUE);
-
-  std::wstring temp_string;
-  if (reg_key.ReadValue(L"TEMP", &temp_string) != ERROR_SUCCESS)
-    return base::FilePath();
-
-  return base::FilePath(temp_string);
-}
-
 }  // namespace
 
 namespace credential_provider {
 
-void InitializeGcpwCrashReporting(GcpCrashReporterClient* crash_client) {
-  crash_reporter::SetCrashReporterClient(crash_client);
-
-  base::FilePath crash_dir = GetFolderForCrashDumps();
-
-  if (crash_dir.empty() ||
-      (!base::PathExists(crash_dir) && !base::CreateDirectory(crash_dir))) {
-    HRESULT hr = HRESULT_FROM_WIN32(::GetLastError());
-    if (hr != HRESULT_FROM_WIN32(ERROR_ALREADY_EXISTS)) {
-      LOGFN(ERROR) << "Failed to create directory for crash dumps: "
-                   << crash_dir << " hr=" << putHR(hr);
-    }
-  }
-}
-
 base::FilePath GetFolderForCrashDumps() {
-  base::FilePath system_temp_dir = GetSystemTempFolder();
-  if (system_temp_dir.empty())
-    // Failed to get a temp dir, something's gone wrong.
-    return system_temp_dir;
-  return system_temp_dir.Append(kCrashpadDumpsFolder);
-}
+  base::FilePath data_dir = GetDataDirectory();
+  if (data_dir.empty()) {
+    return data_dir;
+  }
 
+  // Crashpad will create the directory as-needed.
+  return data_dir.Append(FILE_PATH_LITERAL("Crashpad"));
+}
 
 void SetCommonCrashKeys(const base::CommandLine& command_line) {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
