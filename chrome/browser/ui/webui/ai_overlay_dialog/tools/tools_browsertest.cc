fcea0c4d70612952f6af1929fa08dd92934279e7
Stanislav Mikheyev <mikhe@google.com>
2026-08-17 06:59:09 -0700

    [Gardener] Disable flaky AiOverlayToolsBrowserTest translation tests on Mac
    Disable TranslatePageDefault and TranslatePageSpecificTarget on macOS.
    The tests time out in TranslateWaiter while waiting for page
    translation.
    
    Bug: 542894342
    Change-Id: I5b37e32ad9e87838ba342cb8c645fb5596a25139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264027
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Stanislav Mikheyev <mikhe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1680514}

--- chrome/browser/ui/webui/ai_overlay_dialog/tools/tools_browsertest.cc
+++ chrome/browser/ui/webui/ai_overlay_dialog/tools/tools_browsertest.cc
@@ -13,6 +13,7 @@
 #include "base/test/bind.h"
 #include "base/test/test_future.h"
 #include "base/types/expected.h"
+#include "build/build_config.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/translate/chrome_translate_client.h"
@@ -468,7 +469,17 @@ IN_PROC_BROWSER_TEST_F(AiOverlayToolsBrowserTest,
   EXPECT_EQ("No active media session", future.Get().error());
 }
 
-IN_PROC_BROWSER_TEST_F(AiOverlayToolsBrowserTest, TranslatePageDefault) {
+// TODO(crbug.com/542894342): Re-enable flaky translate tests on Mac.
+#if BUILDFLAG(IS_MAC)
+#define MAYBE_TranslatePageDefault DISABLED_TranslatePageDefault
+#define MAYBE_TranslatePageSpecificTarget \
+  DISABLED_TranslatePageSpecificTarget
+#else
+#define MAYBE_TranslatePageDefault TranslatePageDefault
+#define MAYBE_TranslatePageSpecificTarget TranslatePageSpecificTarget
+#endif
+
+IN_PROC_BROWSER_TEST_F(AiOverlayToolsBrowserTest, MAYBE_TranslatePageDefault) {
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(), embedded_test_server()->GetURL("/empty.html")));
 
@@ -501,7 +512,8 @@ IN_PROC_BROWSER_TEST_F(AiOverlayToolsBrowserTest, TranslatePageDefault) {
             translate_client->GetLanguageState().current_language());
 }
 
-IN_PROC_BROWSER_TEST_F(AiOverlayToolsBrowserTest, TranslatePageSpecificTarget) {
+IN_PROC_BROWSER_TEST_F(AiOverlayToolsBrowserTest,
+                       MAYBE_TranslatePageSpecificTarget) {
   ASSERT_TRUE(ui_test_utils::NavigateToURL(
       browser(), embedded_test_server()->GetURL("/empty.html")));
 
