56e8c89225cbccffd57cc0f27ab2fcfe21cdc580
Takashi Nakayama <tnak@chromium.org>
2026-08-17 21:59:59 -0700

    Revert "Drop Ganesh/GL fallback on Mac+Intel"
    This reverts commit f87e38addd297c977d077c6ae47153a9df22b985.
    
    Reason for revert: Causing plenty of test failures in Mac
    
    Failure Link: https://ci.chromium.org/ui/test-investigate/invocations/build-8673177929180194817/tests
    
    Original change's description:
    > Drop Ganesh/GL fallback on Mac+Intel
    >
    > All mac devices are using ANGLE/Metal as of M152 and can run Graphite.
    > Drop the support for falling back to Ganesh/GL entirely on Mac.
    >
    > Bug: 40063538
    > Change-Id: I2c5c6b190e68fea38713118ce05ea01dfa169dc7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8247259
    > Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1680825}
    
    Bug: 40063538
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I6239e0e597b39bac61ca67625b9bbaa52c80c477
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8256653
    Commit-Queue: Takashi Nakayama <tnak@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Takashi Nakayama <tnak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681105}

--- content/browser/gpu/gpu_data_manager_impl_private.h
+++ content/browser/gpu/gpu_data_manager_impl_private.h
@@ -40,7 +40,7 @@ namespace content {
 class CONTENT_EXPORT GpuDataManagerImplPrivate {
  public:
   static constexpr bool kSupportsGpuModeHardwareGL =
-#if BUILDFLAG(IS_MAC)
+#if BUILDFLAG(IS_MAC) && defined(ARCH_CPU_ARM64)
       false;
 #else
       true;
