c2ccc766564267c9bc7ef5d60f2f3fff41938e6e
Zaina Al-Mashni <zalmashni@google.com>
2026-08-18 08:25:04 -0700

    Add fieldtrial testing config for location based-feature eligibility
    Bug: 543818464
    Change-Id: Ief2fe8f18af4ff6ac7ba0745e9618f5dc9132221
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8260831
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1681400}

--- chrome/browser/profiles/profile_keyed_service_browsertest.cc
+++ chrome/browser/profiles/profile_keyed_service_browsertest.cc
@@ -603,10 +603,6 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceGuestBrowserTest,
     guest_otr_active_services.insert("AimEligibilityExtensionBridge");
     guest_otr_active_services.insert("ExtensionMojoBinderRegistry");
   }
-  if (base::FeatureList::IsEnabled(
-          universal_optout::features::kUniversalOptOut)) {
-    guest_otr_active_services.insert("UniversalOptOutService");
-  }
 
   // On ChromeOS, Guest session startup navigates to chrome://newtab (a WebUI)
   // by default, whereas Desktop CreateGuestBrowser() navigates to about:blank.
@@ -1026,6 +1022,10 @@ IN_PROC_BROWSER_TEST_F(ProfileKeyedServiceGuestBrowserTest,
     guest_active_services.insert("ExtensionInstallPolicyService");
   }
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS_CORE)
+  if (base::FeatureList::IsEnabled(
+          universal_optout::features::kUniversalOptOut)) {
+    guest_active_services.insert("UniversalOptOutService");
+  }
 #if BUILDFLAG(IS_CHROMEOS)
   EXPECT_TRUE(user_manager::UserManager::Get()->IsLoggedInAsGuest());
   // ChromeOS Guest mode starts with the guest otr profile.
