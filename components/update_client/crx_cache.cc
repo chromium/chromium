349ddf6b6b53e786568fe8c15bfa5d1c1483697f
Jayson Chen <jaysonchen@microsoft.com>
2026-08-18 18:18:34 -0700

    update_client: Run CrxCache I/O at USER_VISIBLE priority
    CrxCache performs blocking file I/O for both foreground and background component updates. Its sequenced task runner currently defaults to USER_BLOCKING, which can preempt work needed for immediate user interactions.
    
    Use USER_VISIBLE priority so foreground updates remain responsive while cache work no longer runs at the highest ThreadPool priority.
    
    Change-Id: I9468cbfa40be1ceb87e352d2fc6f068b5c644a1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8262855
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Jayson Chen <jaysonchen@microsoft.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1681907}

--- components/update_client/crx_cache.cc
+++ components/update_client/crx_cache.cc
@@ -22,6 +22,7 @@
 #include "base/sequence_checker.h"
 #include "base/strings/strcat.h"
 #include "base/task/sequenced_task_runner.h"
+#include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/sequence_bound.h"
 #include "base/types/expected.h"
@@ -262,10 +263,13 @@ class CrxCacheError : public CrxCacheSynchronous {
 CrxCache::CrxCache(std::optional<base::FilePath> path) {
   if (path) {
     delegate_ = base::SequenceBound<CrxCacheImpl>(
-        base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()}), *path);
+        base::ThreadPool::CreateSequencedTaskRunner(
+            {base::MayBlock(), base::TaskPriority::USER_VISIBLE}),
+        *path);
   } else {
     delegate_ = base::SequenceBound<CrxCacheError>(
-        base::ThreadPool::CreateSequencedTaskRunner({base::MayBlock()}));
+        base::ThreadPool::CreateSequencedTaskRunner(
+            {base::MayBlock(), base::TaskPriority::USER_VISIBLE}));
   }
 }
 
