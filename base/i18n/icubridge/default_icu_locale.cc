bb96aafe11b48dc4ecd43c5877359cd253c23592
Danilo Tedeschi <danft@google.com>
2026-08-19 12:02:32 -0700

    Add IOS crash keys to new ICU locale setter
    Change-Id: I1e3f5cc955293b8773e3fe68528f1c3c186c6c6e
    Bug: 542711615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263955
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Danilo Tedeschi <danft@google.com>
    Cr-Commit-Position: refs/heads/main@{#1682528}

--- base/i18n/icubridge/default_icu_locale.cc
+++ base/i18n/icubridge/default_icu_locale.cc
@@ -7,8 +7,15 @@
 #include "base/i18n/language_tag.h"
 #include "base/i18n/locale_holder.h"
 #include "base/i18n/tag_converters.h"
+#include "base/logging.h"
 #include "base/no_destructor.h"
+#include "build/build_config.h"
 #include "third_party/icu/source/common/unicode/locid.h"
+
+#if BUILDFLAG(IS_IOS)
+#include "base/debug/crash_logging.h"
+#include "base/ios/ios_util.h"
+#endif
 
 namespace base::i18n {
 
@@ -29,11 +36,21 @@ LanguageTag GetDefaultIcuLocale() {
 void SetDefaultIcuLocale(DefaultIcuLocaleSetterKey key,
                          const LanguageTag& language_tag) {
   GetLocaleHolder().SetLocale(language_tag);
-  {
-    UErrorCode error_code = U_ZERO_ERROR;
-    icu::Locale locale =
-        IcuLocaleConverter::GetInstance().FromLanguageTag(language_tag);
-    icu::Locale::setDefault(locale, error_code);
+#if BUILDFLAG(IS_IOS)
+  static base::debug::CrashKeyString* crash_key_locale =
+      base::debug::AllocateCrashKeyString("icu_locale_input",
+                                          base::debug::CrashKeySize::Size256);
+  base::debug::SetCrashKeyString(crash_key_locale, language_tag.tag_string());
+#endif
+  UErrorCode error_code = U_ZERO_ERROR;
+  icu::Locale locale =
+      IcuLocaleConverter::GetInstance().FromLanguageTag(language_tag);
+  icu::Locale::setDefault(locale, error_code);
+  if (U_FAILURE(error_code)) {
+    LOG(ERROR) << "Failed to set the ICU default locale to " << language_tag
+               << ". Falling back to en-US.";
+    icu::Locale::setDefault(icu::Locale::getUS(), error_code);
+    GetLocaleHolder().SetLocale(GetKnownLanguageTag("en-US"));
   }
 }
 
