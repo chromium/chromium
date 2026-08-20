4d21bc43b6e02b2ea98ee625d6560d40de2f1cc7
Tiago Vignatti <tvignatti@igalia.com>
2026-08-20 06:54:06 -0700

    [ash] wallpaper client: drop dead chrome/browser/signin include
    wallpaper_controller_client_impl.cc no longer uses
    IdentityManagerFactory; its identity_manager_factory.h include was
    dead. It is the only //chrome/browser/signin user in this target,
    so remove the include, the +chrome/browser/signin DEPS rule, and
    the //chrome/browser/signin build dep.
    
    Bug: 546860700
    Test: CQ
    Change-Id: I59890105c020f969c51f4d4b4067b96832f5b3e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8266166
    Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683102}

--- chrome/browser/ui/ash/wallpaper/wallpaper_controller_client_impl.cc
+++ chrome/browser/ui/ash/wallpaper/wallpaper_controller_client_impl.cc
@@ -45,7 +45,6 @@
 #include "chrome/browser/ash/wallpaper_handlers/wallpaper_fetcher_delegate.h"
 #include "chrome/browser/ash/wallpaper_handlers/wallpaper_handlers.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/webui/ash/settings/pref_names.h"
