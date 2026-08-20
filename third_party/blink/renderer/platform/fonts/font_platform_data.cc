e1046739e48d9c1b7a5a96d9d2cc4cd7ac8d1852
Mahmoud Rashad <mmrashad@google.com>
2026-08-20 13:12:54 -0700

    Fix unsafe buffer usage in FontPlatformData
    Replaced unsafe memcpy with base::bit_cast in FontPlatformData::GetHash
    to fix unsafe buffer error(s).
    
    Initial patchset generated by headless gemini-cli using:
    //agents/prompts/projects/spanification/run.py
    
    Fixed: 490864711
    Change-Id: I3947e66065f141516d0c48ce795e54dc640add33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7647088
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1683370}

--- third_party/blink/renderer/platform/fonts/font_platform_data.cc
+++ third_party/blink/renderer/platform/fonts/font_platform_data.cc
@@ -20,6 +20,7 @@
 
 #include "third_party/blink/renderer/platform/fonts/font_platform_data.h"
 
+#include "base/bit_cast.h"
 #include "base/compiler_specific.h"
 #include "base/feature_list.h"
 #include "build/build_config.h"
@@ -205,12 +206,7 @@ unsigned FontPlatformData::GetHash() const {
                      (static_cast<int>(synthetic_bold_) << 1) |
                      static_cast<int>(synthetic_italic_));
 
-  // This memcpy is to avoid a reinterpret_cast that breaks strict-aliasing
-  // rules. Memcpy is generally optimized enough so that performance doesn't
-  // matter here.
-  uint32_t text_size_bytes;
-  UNSAFE_TODO(memcpy(&text_size_bytes, &text_size_, sizeof(uint32_t)));
-  h ^= text_size_bytes;
+  h ^= base::bit_cast<uint32_t>(text_size_);
 
   return h;
 }
