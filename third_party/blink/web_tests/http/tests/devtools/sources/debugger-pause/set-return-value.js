88e6abcf3bdbc5670223e21fea8a46de997cd8cf
Sho Mizoe <shomizoe@google.com>
2026-08-20 10:49:39 -0700

    [Build-Sheriff] Fix flakiness in set-return-value.js
    Following the migration of ObjectPropertiesSection and
    ScopeChainSidebarPane to ObjectTreeWidget, property inspection is
    handled asynchronously via performUpdate() over CDP.
    
    The web test was flaking on Mac tryjobs
    (http://screenshot/023r4ffv8fs70) because awaiting requestAnimationFrame
    resumed before asynchronous CDP property fetches completed,
    intermittently leaving localScope.children() empty when expanding or
    dumping scope variables.
    
    This CL replaces requestAnimationFrame with
    UI.Widget.Widget.allUpdatesComplete to ensure all scheduled widget
    updates and CDP round-trips finish before inspecting or dumping the
    Scope pane.
    
    Test: third_party/blink/tools/run_web_tests.py -t Default http/tests/devtools/sources/debugger-pause/set-return-value.js --iterations=20 --no-retry-failures
    Change-Id: I577bf913fb5f4b0ef9cf8e7aff63b97c10d5be6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8269035
    Commit-Queue: Sho Mizoe <shomizoe@google.com>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683272}

--- third_party/blink/web_tests/http/tests/devtools/sources/debugger-pause/set-return-value.js
+++ third_party/blink/web_tests/http/tests/devtools/sources/debugger-pause/set-return-value.js
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {TestRunner} from 'test_runner';
 import {ConsoleTestRunner} from 'console_test_runner';
-import {SourcesTestRunner} from 'sources_test_runner';
-
 import * as SourcesModule from 'devtools/panels/sources/sources.js';
+import * as UI from 'devtools/ui/legacy/legacy.js';
+import {SourcesTestRunner} from 'sources_test_runner';
+import {TestRunner} from 'test_runner';
 
 (async function() {
   TestRunner.addResult('Check that return value can be changed.');
@@ -18,36 +18,41 @@ import * as SourcesModule from 'devtools/panels/sources/sources.js';
     //# sourceURL=test.js
   `);
   await SourcesTestRunner.startDebuggerTestPromise();
-  await TestRunner.DebuggerAgent.invoke_setBreakpointByUrl({lineNumber: 15, url: 'test.js', columnNumber: 37});
+  await TestRunner.DebuggerAgent.invoke_setBreakpointByUrl(
+      {lineNumber: 15, url: 'test.js', columnNumber: 37});
   let sidebarUpdated = TestRunner.addSnifferPromise(
         SourcesModule.ScopeChainSidebarPane.ScopeChainSidebarPane.prototype, 'sidebarPaneUpdatedForTest');
   await Promise.all([SourcesTestRunner.runTestFunctionAndWaitUntilPausedPromise(), sidebarUpdated]);
+  await UI.Widget.Widget.allUpdatesComplete;
   let localScope = SourcesTestRunner.scopeChainSections()[0];
 
   TestRunner.addResult('Dump current');
   await new Promise(resolve => SourcesTestRunner.expandProperties([localScope, ['Return value']], resolve));
-  await new Promise(requestAnimationFrame);
+  await UI.Widget.Widget.allUpdatesComplete;
   SourcesTestRunner.dumpScopeVariablesSidebarPane();
 
   TestRunner.addResult('Set return value to {a:1}');
   let returnValueElement = localScope.children().find(x => x.property.name === 'Return value');
   await returnValueElement.applyExpression('{a:1}');
+  await UI.Widget.Widget.allUpdatesComplete;
   await new Promise(resolve => SourcesTestRunner.expandProperties([localScope, ['Return value']], resolve));
-  await new Promise(requestAnimationFrame);
+  await UI.Widget.Widget.allUpdatesComplete;
   SourcesTestRunner.dumpScopeVariablesSidebarPane();
 
   TestRunner.addResult('Try to remove return value');
   returnValueElement = localScope.children().find(x => x.property.name === 'Return value');
   await returnValueElement.applyExpression('');
+  await UI.Widget.Widget.allUpdatesComplete;
   await new Promise(resolve => SourcesTestRunner.expandProperties([localScope, ['Return value']], resolve));
-  await new Promise(requestAnimationFrame);
+  await UI.Widget.Widget.allUpdatesComplete;
   SourcesTestRunner.dumpScopeVariablesSidebarPane();
 
   TestRunner.addResult('Set return value to 239');
   returnValueElement = localScope.children().find(x => x.property.name === 'Return value');
   await returnValueElement.applyExpression('239');
+  await UI.Widget.Widget.allUpdatesComplete;
   await new Promise(resolve => SourcesTestRunner.expandProperties([localScope, ['Return value']], resolve));
-  await new Promise(requestAnimationFrame);
+  await UI.Widget.Widget.allUpdatesComplete;
   SourcesTestRunner.dumpScopeVariablesSidebarPane();
 
   SourcesTestRunner.resumeExecution();
