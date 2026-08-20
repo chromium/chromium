1eb697ed792ca9f1e4e037a0cb656cf5e18eddf2
Etienne Pierre-doray <etiennep@chromium.org>
2026-08-20 03:19:03 -0700

    [tracing] Bind PerfettoService receiver immediately in TracingService::AddClient
    Currently, TracingService::AddClient creates a new PerfettoService
    receiver pair and passes the PendingRemote to the remote TracedProcess,
    but waits for the ConnectToTracingService ACK callback before binding
    the receiver.
    
    By binding the receiver immediately, PerfettoService can process
    ConnectToProducerHost immediately upon receiving it rather than waiting
    for the extra round trip.
    
    Change-Id: Id959389f6c20a7e85ff2afbe95598f38c7f4d914
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8271241
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683006}

--- services/tracing/tracing_service.cc
+++ services/tracing/tracing_service.cc
@@ -7,6 +7,7 @@
 #include <utility>
 
 #include "base/functional/bind.h"
+#include "base/functional/callback_helpers.h"
 #include "build/build_config.h"
 #include "services/tracing/perfetto/consumer_host.h"
 #include "services/tracing/perfetto/perfetto_service.h"
@@ -18,14 +19,6 @@ namespace {
 
 void OnProcessConnectFailed(PerfettoService* perfetto_service, uint32_t pid) {
   perfetto_service->RemoveActiveServicePidIfNoActiveConnections(pid);
-}
-
-void OnProcessConnected(
-    PerfettoService* perfetto_service,
-    mojo::Remote<mojom::TracedProcess> traced_process,
-    uint32_t pid,
-    mojo::PendingReceiver<mojom::PerfettoService> service_receiver) {
-  perfetto_service->BindReceiver(std::move(service_receiver), pid);
 }
 
 }  // namespace
@@ -55,22 +48,19 @@ void TracingService::AddClient(mojom::ClientInfoPtr client) {
 
   // If the remote traced process goes away before ConnectToTracingService
   // responds, the PID should be removed from the list of active service PID.
-  // Note that the perfetto service will start monitoring disconnects after the
-  // service receiver is bound to it in OnProcessConnected().
   process.set_disconnect_handler(
       base::BindOnce(&OnProcessConnectFailed,
                      base::Unretained(perfetto_service_), client->pid));
 
   auto new_connection_request = mojom::ConnectToTracingRequest::New();
-  auto service_receiver =
-      new_connection_request->perfetto_service.InitWithNewPipeAndPassReceiver();
+  perfetto_service_->BindReceiver(
+      new_connection_request->perfetto_service.InitWithNewPipeAndPassReceiver(),
+      client->pid);
 
   mojom::TracedProcess* raw_process = process.get();
   raw_process->ConnectToTracingService(
       std::move(new_connection_request),
-      base::BindOnce(&OnProcessConnected, base::Unretained(perfetto_service_),
-                     std::move(process), client->pid,
-                     std::move(service_receiver)));
+      base::DoNothingWithBoundArgs(std::move(process)));
 }
 
 #if BUILDFLAG(USE_BLINK)
