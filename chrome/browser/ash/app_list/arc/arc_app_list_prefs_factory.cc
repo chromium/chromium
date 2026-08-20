b70d90c1360c4fb73a59e0de6190cc98f80ad194
Di Wu <diwux@google.com>
2026-08-20 01:04:17 -0700

    Ash: Drop stale notification dependency in ARC app list prefs
    ArcAppListPrefsFactory declared a KeyedService dependency on
    NotificationDisplayServiceFactory, but the prefs service makes no use of
    the display service anywhere, so the shutdown ordering that the
    dependency guarantees protects nothing. It goes away as part of retiring
    NotificationDisplayService on ChromeOS.
    
    This was the module's last reference to chrome/browser/notifications, so
    the DEPS allowance and the build dependency go away with it. No behavior
    change.
    
    Bug: b:332804822
    Test: CQ
    Change-Id: Ib3608fe152b4d9ffd1f44e84534bfcde3fdad02c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264757
    Commit-Queue: Di Wu <diwux@google.com>
    Reviewed-by: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1682928}

--- chrome/browser/ash/app_list/arc/arc_app_list_prefs_factory.cc
+++ chrome/browser/ash/app_list/arc/arc_app_list_prefs_factory.cc
@@ -6,7 +6,6 @@
 
 #include "chrome/browser/ash/app_list/arc/arc_app_list_prefs.h"
 #include "chrome/browser/ash/arc/arc_util.h"
-#include "chrome/browser/notifications/notification_display_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/ash/experiences/arc/session/arc_bridge_service.h"
 #include "chromeos/ash/experiences/arc/session/arc_service_manager.h"
@@ -57,7 +56,6 @@ ArcAppListPrefsFactory::ArcAppListPrefsFactory()
               // Ash Internals.
               .WithAshInternals(ProfileSelection::kRedirectedToOriginal)
               .Build()) {
-  DependsOn(NotificationDisplayServiceFactory::GetInstance());
 }
 
 ArcAppListPrefsFactory::~ArcAppListPrefsFactory() = default;
