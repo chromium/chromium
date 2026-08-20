f52b650e15324cb4878d37b9af14b81d5ebbef19
Balazs Engedy <engedy@chromium.org>
2026-08-20 11:35:22 -0700

    Gracefully ignore "user-controlled managed preferences".
    Some users have legacy user-controlled values in the Preferences file
    for the paths also used by synthesized prefs corresponding to enterprise
    policies.
    
    This used to be DCHECK'ed, which was turned into a CHECK in
    crrev.com/c/8225403, which led to a crash. This CL now turns this into
    an actual conditional to simply ignore the pref.
    
    An additional small code change is that when no preference value is set,
    `UpdateManagedDefaultSetting` no longer resets the `value_map_`, whereas
    in the past it did. This results, however, in no functional change,
    because as of crrev.com/c/8225403 the function is always called right
    after the `value_map_` is reset anyway, and within the function there
    are never conflicting policies.
    
    Bug: 549657306, 549652705
    Change-Id: I13a0d0f16fccc590658cd4195ef9f7ef2e26851b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8267749
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/branch-heads/8013@{#4}
    Cr-Branched-From: 666c139f7a68cb2e0626a50d2ab550b16414b107-refs/heads/main@{#1682742}

--- components/content_settings/core/browser/content_settings_policy_provider.cc
+++ components/content_settings/core/browser/content_settings_policy_provider.cc
@@ -16,7 +16,6 @@
 #include "base/json/json_reader.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/not_fatal_until.h"
 #include "base/trace_event/trace_event.h"
 #include "base/values.h"
 #include "components/content_settings/core/browser/content_settings_info.h"
@@ -739,18 +738,18 @@ void PolicyProvider::UpdateManagedDefaultSetting(
   // not registered, don't update them.
   const ContentSettingsInfo* info =
       ContentSettingsRegistry::GetInstance()->Get(entry.content_type);
-  if (!info)
+  if (!info) {
     return;
+  }
 
-  // If a pref to manage a default-content-setting was not set (NOTICE:
-  // "HasPrefPath" returns false if no value was set for a registered pref) then
-  // the default value of the preference is used. The default value of a
-  // preference to manage a default-content-settings is CONTENT_SETTING_DEFAULT.
-  // This indicates that no managed value is set. If a pref was set, than it
-  // MUST be managed.
-  CHECK(!prefs_->HasPrefPath(entry.pref_name) ||
-            prefs_->IsManagedPreference(entry.pref_name),
-        base::NotFatalUntil::M154);
+  // TODO(crbug.com/549652705): The preference is synthesized from enterprise
+  // policy and should never be directly user controlled. However, we cannot
+  // CHECK that here because some users have legacy entries in their Preferences
+  // file. As a long-term solution, we should instead remove the legacy values
+  // instead of ignoring them.
+  if (!prefs_->IsManagedPreference(entry.pref_name)) {
+    return;
+  }
 
   int setting = prefs_->GetInteger(entry.pref_name);
   ContentSetting content_setting = IntToContentSetting(setting);
@@ -780,11 +779,9 @@ void PolicyProvider::UpdateManagedAudioVideoCaptureDefaultSetting(
     return;
   }
 
-  CHECK(!prefs_->HasPrefPath(entry.pref_name) ||
-            prefs_->IsManagedPreference(entry.pref_name),
-        base::NotFatalUntil::M154);
-
-  if (!prefs_->HasPrefPath(entry.pref_name)) {
+  // TODO(crbug.com/549652705): Update this once legacy user-controlled
+  // preferences have been cleaned up.
+  if (!prefs_->IsManagedPreference(entry.pref_name)) {
     return;
   }
 
