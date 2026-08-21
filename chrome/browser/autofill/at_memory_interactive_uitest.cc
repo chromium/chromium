75f57686ad4d466166362e8c9d8b752d903e9549
Bryan Oltman <bryanoltman@google.com>
2026-08-21 14:33:21 -0700

    [Autofill] Ensure paint holding ends and input flushes in AtMemory test
    Simulates the end of paint holding and flushes pending input processing
    before dispatching trigger key events in
    AtMemoryInteractiveUiTest.TriggerAndFill.
    
    During paint holding, input events targeted to a newly loaded page are
    suppressed from being processed by the renderer until the first frame
    paints. Invoking SimulateEndOfPaintHoldingOnPrimaryMainFrame() releases
    this input suppression, and RunUntilInputProcessed() ensures prior
    JavaScript-initiated focus and layout mutations are completed by the
    renderer before simulated keystrokes are sent.
    
    This resolves a test failure that will be introduced when
    https://crrev.com/c/8209919 is relanded.
    
    Include-Ci-Only-Tests: chromium.mac:mac15-x64-rel-tests|interactive_ui_tests
    Bug: None
    Change-Id: I8d84bbac0f9a851808496d6318c23835478f0d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281404
    Commit-Queue: Bryan Oltman <bryanoltman@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684232}

--- chrome/browser/autofill/at_memory_interactive_uitest.cc
+++ chrome/browser/autofill/at_memory_interactive_uitest.cc
@@ -30,6 +30,8 @@
 #include "components/autofill/core/browser/integrators/at_memory/mock_at_memory_query_service.h"
 #include "components/autofill/core/common/autofill_debug_features.h"
 #include "components/autofill/core/common/autofill_features.h"
+#include "content/public/browser/render_view_host.h"
+#include "content/public/browser/render_widget_host.h"
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "net/dns/mock_host_resolver.h"
@@ -237,6 +239,13 @@ IN_PROC_BROWSER_TEST_P(AtMemoryInteractiveUiTest, MAYBE_TriggerAndFill) {
            sel.removeAllRanges();
            sel.addRange(range);
          })"));
+  // Wait for the selection to be processed.
+  content::RunUntilInputProcessed(
+      GetWebContents()->GetRenderWidgetHostView()->GetRenderWidgetHost());
+
+  // The input events below will not be processed until the end of the current
+  // paint, so we need to wait for that to happen before sending the key events.
+  content::SimulateEndOfPaintHoldingOnPrimaryMainFrame(GetWebContents());
 
   // Type '@'.
   ASSERT_TRUE(SendKeyToPageAndWait(ui::DomKey::FromCharacter('@'),
