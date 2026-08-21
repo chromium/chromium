2ab8c8090931ee114af1bac6ac3f4804a3f0b6e4
Daniel Angulo <angdaniel@google.com>
2026-08-21 07:21:34 -0700

    remove UNSAFE_TODOs from partition_alloc/partition_root.cc
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli and at last manually refined
    
    Gemini-CLI auto-fixes for compilation
    
    Bug: 505042664
    Change-Id: I839fd0af91a0ba601ccf13d4a3b206f274afa0e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7833803
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Daniel Angulo <angdaniel@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1683884}

--- base/allocator/partition_allocator/src/partition_alloc/partition_root.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_root.cc
@@ -1567,13 +1567,12 @@ void PartitionRoot::ShrinkEmptySlotSpansRing(size_t limit) {
   int16_t index = global_empty_slot_span_ring_index_;
   int16_t starting_index = index;
   while (empty_slot_spans_dirty_bytes_ > limit) {
-    internal::SlotSpanMetadata* slot_span =
-        PA_UNSAFE_TODO(global_empty_slot_span_ring_[index]);
+    internal::SlotSpanMetadata* slot_span = global_empty_slot_span_ring_[index];
     // The ring is not always full, may be nullptr.
     if (slot_span) {
       slot_span->DecommitIfPossible(this);
       // DecommitIfPossible() should set the buffer to null.
-      PA_UNSAFE_TODO(PA_DCHECK(!global_empty_slot_span_ring_[index]));
+      PA_DCHECK(!global_empty_slot_span_ring_[index]);
     }
     index += 1;
     // Walk through the entirety of possible slots, even though the last ones
@@ -1658,23 +1657,17 @@ void PartitionRoot::DumpStats(const char* partition_name,
       // order to preserve a fast size->bucket map (see
       // PartitionRoot::Init() for details).
       if (!bucket->is_valid()) {
-        PA_UNSAFE_TODO(bucket_stats[i]).is_valid = false;
+        bucket_stats[i].is_valid = false;
       } else {
-        internal::PartitionDumpBucketStats(&PA_UNSAFE_TODO(bucket_stats[i]),
-                                           this, bucket,
+        internal::PartitionDumpBucketStats(&bucket_stats[i], this, bucket,
                                            populate_discardable_bytes);
       }
-      if (PA_UNSAFE_TODO(bucket_stats[i]).is_valid) {
-        stats.total_resident_bytes +=
-            PA_UNSAFE_TODO(bucket_stats[i]).resident_bytes;
-        stats.total_active_bytes +=
-            PA_UNSAFE_TODO(bucket_stats[i]).active_bytes;
-        stats.total_active_count +=
-            PA_UNSAFE_TODO(bucket_stats[i]).active_count;
-        stats.total_decommittable_bytes +=
-            PA_UNSAFE_TODO(bucket_stats[i]).decommittable_bytes;
-        stats.total_discardable_bytes +=
-            PA_UNSAFE_TODO(bucket_stats[i]).discardable_bytes;
+      if (bucket_stats[i].is_valid) {
+        stats.total_resident_bytes += bucket_stats[i].resident_bytes;
+        stats.total_active_bytes += bucket_stats[i].active_bytes;
+        stats.total_active_count += bucket_stats[i].active_count;
+        stats.total_decommittable_bytes += bucket_stats[i].decommittable_bytes;
+        stats.total_discardable_bytes += bucket_stats[i].discardable_bytes;
       }
     }
 
