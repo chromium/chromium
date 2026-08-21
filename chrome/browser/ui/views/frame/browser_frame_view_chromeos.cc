6f9d5f50ca56403d7a32fb39b4f86f6121d91cf7
Paulina Gacek <paulinagacek@google.com>
2026-08-21 02:55:47 -0700

    [M152] [ChromeOS] Fix top gap in fullscreen web app popup windows
    Original change's description:
    > [ChromeOS] Fix top gap in fullscreen web app popup windows
    >
    > When a web app popup window entered fullscreen mode via
    > `requestFullscreen()` a blue stripe/gap appeared above the web contents.
    > This regression was introduced by crrev.com/c/7884718, which added
    > `MaybeAddAppIconToLayoutParams()` to
    > `BrowserFrameViewChromeOS::GetBrowserLayoutParams()`.
    >
    > That method unconditionally injected the window icon's height into
    > `leading_exclusion` without checking whether the frame header and
    > caption buttons were actually visible. Consequently,
    > `BrowserViewPopupLayoutImpl` pushed the web contents down by the icon
    > height even in fullscreen mode.
    >
    > This CL moves the `MaybeAddAppIconToLayoutParams()` call inside
    > the `if (GetShowCaptionButtonsWhenNotInOverview())` check so that
    > icon exclusion is only added when frame captions are visible, ensuring
    > fullscreen popup windows start at y = 0.
    >
    > Bug: 537698870
    > Change-Id: I3ac4eafdbe9e71e9863a435a58c7fa0fab8d16b1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8252460
    > Reviewed-by: James Cook <jamescook@chromium.org>
    > Commit-Queue: Paulina Gacek <paulinagacek@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1681083}
    
    (cherry picked from commit b329be3058e42e4a1675b62e8c3d9d1d8d7070ed)
    
    Bug: 548725301,537698870
    Change-Id: I3ac4eafdbe9e71e9863a435a58c7fa0fab8d16b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8267689
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Paulina Gacek <paulinagacek@google.com>
    Cr-Commit-Position: refs/branch-heads/7977@{#1859}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- chrome/browser/ui/views/frame/browser_frame_view_chromeos.cc
+++ chrome/browser/ui/views/frame/browser_frame_view_chromeos.cc
@@ -258,8 +258,8 @@ BrowserLayoutParams BrowserFrameViewChromeOS::GetBrowserLayoutParams() const {
                            : caption_bounds.height();
     params.trailing_exclusion.content =
         gfx::SizeF(width() - caption_bounds.x(), height);
+    MaybeAddAppIconToLayoutParams(params);
   }
-  MaybeAddAppIconToLayoutParams(params);
   return params;
 }
 
