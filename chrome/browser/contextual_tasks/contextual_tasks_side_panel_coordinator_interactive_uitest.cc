d95e251b6a1942f82b0ce23a1fab20ecbbcbc57f
Lily Chen <chlily@chromium.org>
2026-08-24 10:04:35 -0700

    [gardener] Disable ComposeboxFocusOnBoundsUpdateWhenComposeboxHidden
    Unfortunately, this test seems to still be broken on Linux dbg builders.
    
    https://luci-milo.appspot.com/ui/tests/p/chromium/clusters/reason-v6/07dad58338d72c9f03ab7674a3fd1cd9
    
    Bug: 528971436
    Change-Id: Ib5bba9937eec8ea308a8937716064cfc6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8281412
    Auto-Submit: Lily Chen <chlily@chromium.org>
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Owners-Override: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1684830}

--- chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator_interactive_uitest.cc
+++ chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator_interactive_uitest.cc
@@ -1079,8 +1079,17 @@ DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kElementExistsEvent);
 DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kFrameLoadedEvent);
 DEFINE_LOCAL_CUSTOM_ELEMENT_EVENT_TYPE(kComposeboxFocusedEvent);
 
-IN_PROC_BROWSER_TEST_F(ContextualTasksSidePanelCoordinatorInteractiveUiTest,
-                       ComposeboxFocusOnBoundsUpdateWhenComposeboxHidden) {
+// TODO(crbug.com/528971436): Flaky on Linux dbg.
+#if (BUILDFLAG(IS_LINUX) && !defined(NDEBUG))
+#define MAYBE_ComposeboxFocusOnBoundsUpdateWhenComposeboxHidden \
+  DISABLED_ComposeboxFocusOnBoundsUpdateWhenComposeboxHidden
+#else
+#define MAYBE_ComposeboxFocusOnBoundsUpdateWhenComposeboxHidden \
+  ComposeboxFocusOnBoundsUpdateWhenComposeboxHidden
+#endif
+IN_PROC_BROWSER_TEST_F(
+    ContextualTasksSidePanelCoordinatorInteractiveUiTest,
+    MAYBE_ComposeboxFocusOnBoundsUpdateWhenComposeboxHidden) {
   SetUpTasks();
   ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
   ContextualTasksSidePanelCoordinator* coordinator = GetCoordinator();
