1920c3529f3991a81d8c58203cadeca317bcdd98
Francis Beauchamp <fbeauchamp@google.com>
2026-08-24 09:44:01 -0700

    [iOS] Add accessory item data model and factory helpers for worklog.
    Introduces ActuationWorklogAccessoryItem to represent metadata for
    nested cards in actuation worklog card layouts. Adds factory
    constructors to ActuationWorklogItem to streamline model instantiation
    across simple, labeled, and card styles.
    
    How it looks: https://screenshot-v2.corp.google.com/1e5airksoh178
    
    Bug: 532205614
    Change-Id: I2c1aa3b76961dd8407991c93466486377d14396d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8257530
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Francis Beauchamp <fbeauchamp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1684814}

--- ios/chrome/browser/intelligence/actor/ui/actuation_worklog_view_data.h
+++ ios/chrome/browser/intelligence/actor/ui/actuation_worklog_view_data.h
@@ -14,9 +14,39 @@ enum class ActuationWorklogItemStyle {
   // Labeled style with an icon in a circle, and a title/subtitle next to it.
   kLabeled,
   // Card style with an icon in a circle, and the title/subtitle inside a card
-  // container.
+  // container. Optionally contains an `ActuationWorklogAccessoryItem`.
   kCard,
 };
+
+// View data object describing a nested accessory card inside an actuation
+// worklog card item.
+@interface ActuationWorklogAccessoryItem : NSObject
+
+// Icon image.
+@property(nonatomic, strong, readonly) UIImage* icon;
+
+// Primary title text.
+@property(nonatomic, copy, readonly) NSString* title;
+
+// Optional subtitle text.
+@property(nonatomic, copy, readonly) NSString* subtitle;
+
+// Optional detail text (e.g. expiration date).
+@property(nonatomic, copy, readonly) NSString* detailText;
+
+// Whether to show a trailing chevron.
+@property(nonatomic, assign, readonly) BOOL hasChevron;
+
+// Designated initializer.
+- (instancetype)initWithIcon:(UIImage*)icon
+                       title:(NSString*)title
+                    subtitle:(NSString*)subtitle
+                  detailText:(NSString*)detailText
+                  hasChevron:(BOOL)hasChevron NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)init NS_UNAVAILABLE;
+
+@end
 
 // View data object describing a node in the actuation worklog.
 @interface ActuationWorklogItem : NSObject
@@ -36,14 +66,53 @@ enum class ActuationWorklogItemStyle {
 // The layout style of this node.
 @property(nonatomic, assign, readonly) ActuationWorklogItemStyle style;
 
+// Optional accessory item (only used when style ==
+// ActuationWorklogItemStyle::kCard).
+@property(nonatomic, strong, readonly)
+    ActuationWorklogAccessoryItem* accessoryItem;
+
 // Designated initializer.
 - (instancetype)initWithTitle:(NSString*)title
                      subtitle:(NSString*)subtitle
                          icon:(UIImage*)icon
                         style:(ActuationWorklogItemStyle)style
-                       active:(BOOL)active NS_DESIGNATED_INITIALIZER;
+                       active:(BOOL)active
+                accessoryItem:(ActuationWorklogAccessoryItem*)accessoryItem
+    NS_DESIGNATED_INITIALIZER;
+
+- (instancetype)initWithTitle:(NSString*)title
+                     subtitle:(NSString*)subtitle
+                         icon:(UIImage*)icon
+                        style:(ActuationWorklogItemStyle)style
+                       active:(BOOL)active;
 
 - (instancetype)init NS_UNAVAILABLE;
+
+#pragma mark - Factory Constructors
+
+// Creates a simple timeline step with a dot indicator.
++ (instancetype)simpleItemWithTitle:(NSString*)title active:(BOOL)active;
+
+// Creates a labeled timeline step.
++ (instancetype)labeledItemWithTitle:(NSString*)title
+                            subtitle:(NSString*)subtitle
+                                icon:(UIImage*)icon
+                              active:(BOOL)active;
+
+// Creates a card layout step without an accessory item.
++ (instancetype)cardItemWithTitle:(NSString*)title
+                         subtitle:(NSString*)subtitle
+                             icon:(UIImage*)icon
+                           active:(BOOL)active;
+
+// Creates a card layout step with a nested accessory item.
++ (instancetype)cardItemWithTitle:(NSString*)title
+                         subtitle:(NSString*)subtitle
+                             icon:(UIImage*)icon
+                           active:(BOOL)active
+                    accessoryItem:(ActuationWorklogAccessoryItem*)accessoryItem;
+
+#pragma mark - Helpers
 
 // Returns a copy of the item with the active state updated.
 - (instancetype)withActive:(BOOL)active;
--- ios/chrome/browser/intelligence/actor/ui/actuation_worklog_view_data.mm
+++ ios/chrome/browser/intelligence/actor/ui/actuation_worklog_view_data.mm
@@ -6,14 +6,38 @@
 
 #import "base/check.h"
 
+@implementation ActuationWorklogAccessoryItem
+
+- (instancetype)initWithIcon:(UIImage*)icon
+                       title:(NSString*)title
+                    subtitle:(NSString*)subtitle
+                  detailText:(NSString*)detailText
+                  hasChevron:(BOOL)hasChevron {
+  CHECK(title);
+
+  self = [super init];
+  if (self) {
+    _icon = icon;
+    _title = [title copy];
+    _subtitle = [subtitle copy];
+    _detailText = [detailText copy];
+    _hasChevron = hasChevron;
+  }
+  return self;
+}
+
+@end
+
 @implementation ActuationWorklogItem
 
 - (instancetype)initWithTitle:(NSString*)title
                      subtitle:(NSString*)subtitle
                          icon:(UIImage*)icon
                         style:(ActuationWorklogItemStyle)style
-                       active:(BOOL)active {
+                       active:(BOOL)active
+                accessoryItem:(ActuationWorklogAccessoryItem*)accessoryItem {
   CHECK(title);
+  CHECK(!accessoryItem || style == ActuationWorklogItemStyle::kCard);
 
   self = [super init];
   if (self) {
@@ -22,8 +46,73 @@
     _icon = icon;
     _style = style;
     _active = active;
+    _accessoryItem = accessoryItem;
   }
   return self;
+}
+
+- (instancetype)initWithTitle:(NSString*)title
+                     subtitle:(NSString*)subtitle
+                         icon:(UIImage*)icon
+                        style:(ActuationWorklogItemStyle)style
+                       active:(BOOL)active {
+  return [self initWithTitle:title
+                    subtitle:subtitle
+                        icon:icon
+                       style:style
+                      active:active
+               accessoryItem:nil];
+}
+
+#pragma mark - Factory Constructors
+
++ (instancetype)simpleItemWithTitle:(NSString*)title active:(BOOL)active {
+  return [[ActuationWorklogItem alloc]
+      initWithTitle:title
+           subtitle:nil
+               icon:nil
+              style:ActuationWorklogItemStyle::kSimple
+             active:active
+      accessoryItem:nil];
+}
+
++ (instancetype)labeledItemWithTitle:(NSString*)title
+                            subtitle:(NSString*)subtitle
+                                icon:(UIImage*)icon
+                              active:(BOOL)active {
+  return [[ActuationWorklogItem alloc]
+      initWithTitle:title
+           subtitle:subtitle
+               icon:icon
+              style:ActuationWorklogItemStyle::kLabeled
+             active:active
+      accessoryItem:nil];
+}
+
++ (instancetype)cardItemWithTitle:(NSString*)title
+                         subtitle:(NSString*)subtitle
+                             icon:(UIImage*)icon
+                           active:(BOOL)active {
+  return [self cardItemWithTitle:title
+                        subtitle:subtitle
+                            icon:icon
+                          active:active
+                   accessoryItem:nil];
+}
+
++ (instancetype)cardItemWithTitle:(NSString*)title
+                         subtitle:(NSString*)subtitle
+                             icon:(UIImage*)icon
+                           active:(BOOL)active
+                    accessoryItem:
+                        (ActuationWorklogAccessoryItem*)accessoryItem {
+  return [[ActuationWorklogItem alloc]
+      initWithTitle:title
+           subtitle:subtitle
+               icon:icon
+              style:ActuationWorklogItemStyle::kCard
+             active:active
+      accessoryItem:accessoryItem];
 }
 
 - (instancetype)withActive:(BOOL)active {
@@ -31,7 +120,8 @@
                                             subtitle:self.subtitle
                                                 icon:self.icon
                                                style:self.style
-                                              active:active];
+                                              active:active
+                                       accessoryItem:self.accessoryItem];
 }
 
 @end
