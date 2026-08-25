fd974000e34fdde0e5f29f67b349aea8be63f8ca
Joe Mason <joenotcharles@google.com>
2026-08-24 17:37:34 -0700

    Replace KiBU/MiBU/etc with KiB/MiB/etc for cc
    The base::KiBU() family of functions have been deprecated and replaced
    with base::KiB().
    
    Mechanically converted with:
    
    git grep -l base::[KMGTPE]iBU|xargs sed -i
    "s/base::\([KMGTPE]\)iBU/base::\1iB/g"
    
    This CL was uploaded by git cl split.
    
    R=blundell@chromium.org
    
    Bug: 448661443
    Change-Id: I3f40e0238a76c92b365212d2f3f63dff1834cf94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8263277
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685100}

--- cc/tiles/image_decode_cache_utils.cc
+++ cc/tiles/image_decode_cache_utils.cc
@@ -19,18 +19,18 @@ namespace cc {
 // static
 size_t ImageDecodeCacheUtils::GetWorkingSetBytesForImageDecode(
     bool for_renderer) {
-  base::ByteSize decoded_image_working_set_budget = base::MiBU(128);
+  base::ByteSize decoded_image_working_set_budget = base::MiB(128);
 #if !BUILDFLAG(IS_ANDROID)
   if (for_renderer) {
     const bool using_low_memory_policy = base::SysInfo::IsLowEndDevice();
     // If there's over 4GB of RAM, increase the working set size to 256MB for
     // both gpu and software.
-    constexpr base::ByteSize kImageDecodeMemoryThreshold = base::GiBU(4);
+    constexpr base::ByteSize kImageDecodeMemoryThreshold = base::GiB(4);
     if (using_low_memory_policy) {
-      decoded_image_working_set_budget = base::MiBU(32);
+      decoded_image_working_set_budget = base::MiB(32);
     } else if (base::SysInfo::AmountOfTotalPhysicalMemory() >=
                kImageDecodeMemoryThreshold) {
-      decoded_image_working_set_budget = base::MiBU(256);
+      decoded_image_working_set_budget = base::MiB(256);
     }
   }
 #endif  // !BUILDFLAG(IS_ANDROID)
