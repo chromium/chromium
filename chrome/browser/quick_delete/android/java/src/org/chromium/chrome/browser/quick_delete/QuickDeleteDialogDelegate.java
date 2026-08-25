d887b53bce354a7f9efc37c1b0f68a7ffe1b37fb
Sky Malice <skym@chromium.org>
2026-08-25 10:30:19 -0700

    [Clean] Remove unused time period option in QuickDeleteDialogDelegate
    Remove the unused mCurrentTimePeriodOption member field and its
    hardcoded default value in QuickDeleteDialogDelegate. The spinner item
    is already retrieved directly from the adapter in onItemSelected.
    
    TAG=agy
    CONV=021f14a4-c69b-4188-98fd-cbfe7ac6c3d3
    
    Bug: 547916752
    Change-Id: I6dbcc6fe50aac393d3fd150b7cc82a672cf3538b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8284963
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1685709}

--- chrome/browser/quick_delete/android/java/src/org/chromium/chrome/browser/quick_delete/QuickDeleteDialogDelegate.java
+++ chrome/browser/quick_delete/android/java/src/org/chromium/chrome/browser/quick_delete/QuickDeleteDialogDelegate.java
@@ -62,8 +62,6 @@ class QuickDeleteDialogDelegate {
      */
     private @Nullable PropertyModel mModalDialogPropertyModel;
 
-    private TimePeriodSpinnerOption mCurrentTimePeriodOption;
-
     /** The modal dialog controller to detect events on the dialog. */
     private final ModalDialogProperties.Controller mModalDialogController =
             new ModalDialogProperties.Controller() {
@@ -111,11 +109,6 @@ class QuickDeleteDialogDelegate {
         mOnDismissCallback = onDismissCallback;
         mTabModelSelector = tabModelSelector;
         mTimePeriodChangeObserver = timePeriodChangeObserver;
-
-        mCurrentTimePeriodOption =
-                new TimePeriodSpinnerOption(
-                        TimePeriod.LAST_15_MINUTES,
-                        mContext.getString(R.string.clear_browsing_data_tab_period_15_minutes));
     }
 
     /** A method to create the dialog attributes for the quick delete dialog. */
@@ -188,21 +181,13 @@ class QuickDeleteDialogDelegate {
                             AdapterView<?> adapterView, View view, int position, long id) {
                         TimePeriodSpinnerOption item =
                                 (TimePeriodSpinnerOption) adapterView.getItemAtPosition(position);
-                        mCurrentTimePeriodOption = item;
-                        @TimePeriod int timePeriod = mCurrentTimePeriodOption.getTimePeriod();
+                        @TimePeriod int timePeriod = item.getTimePeriod();
                         mTimePeriodChangeObserver.onTimePeriodChanged(timePeriod);
                         recordTimePeriodChange(timePeriod);
                     }
 
                     @Override
-                    public void onNothingSelected(AdapterView<?> adapterView) {
-                        // Revert back to default time.
-                        String message =
-                                mContext.getString(
-                                        R.string.clear_browsing_data_tab_period_15_minutes);
-                        mCurrentTimePeriodOption =
-                                new TimePeriodSpinnerOption(TimePeriod.LAST_15_MINUTES, message);
-                    }
+                    public void onNothingSelected(AdapterView<?> adapterView) {}
                 });
     }
 
