10599c09827543d5af155568e45db00e5d052fd5
Sinan Sahin <sinansahin@google.com>
2026-08-26 15:37:32 -0700

    [FRE] Enable updated layout by default on Desktop Android
    Bug: 537826242
    Change-Id: Iacc58ff5f18d7c83708d4e3fe2288cd6f4e09c62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8296723
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686743}

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -320,6 +320,11 @@ void ChromeBrowserFieldTrials::RegisterFeatureOverrides(
   feature_overrides.EnableFeature(
       chrome::android::kAuxiliarySearchHistoryDonation);
 
+  // Enable updated FRE layout on Android Desktop.
+  // TODO(crbug.com/534451983): Remove when rollout is complete to all form
+  // factors.
+  feature_overrides.EnableFeature(chrome::android::kAndroidFreLayoutUpdate);
+
 #endif  // BUILDFLAG(IS_DESKTOP_ANDROID)
   // Desktop-first features which are past incubation should either end up here,
   // or to a finch trial that enables it for all form factors.
