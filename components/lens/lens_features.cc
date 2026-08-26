5dc8f2f5012cb8e33d56928ab03434373b7d6475
Diana OuYang <dianaou@google.com>
2026-08-26 14:34:44 -0700

    Enable menu icon by default
    Paired with 100% launch CL: cl/955468619
    
    LE Analysis Doc: https://docs.google.com/document/d/1YhGo_O1pODeoHSQD_1wOcHW_0t0AhE96c6VPY7c2sN4/edit?resourcekey=0-1rls2mm9vixNRn-Meh9QdQ&tab=t.0
    
    Bug: b:500476472
    Change-Id: Ic73099908bf0c5e4c44e09d7d0a73b5573987f20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8297181
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Diana OuYang <dianaou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1686706}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -18,7 +18,7 @@ namespace lens::features {
 
 BASE_FEATURE(kLensStandalone, base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kShowContextualTasksMenuIcon, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kShowContextualTasksMenuIcon, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kLensOverlay,
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
