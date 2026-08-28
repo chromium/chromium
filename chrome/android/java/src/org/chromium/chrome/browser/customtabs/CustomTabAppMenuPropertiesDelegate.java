11e788052bba7c3d61bc16ece28ecdbf798c11ff
Fuhsin Liao <fuhsinliao@google.com>
2026-08-27 21:36:40 -0700

    Revert "[TWA] Hides 'site controls'  from showing in TWA"
    This reverts commit 7ad9e33aa0e4cd4bebe1947ce477f8695289d033.
    
    Reason for revert: This cl has caused 'org.chromium.chrome.browser.customtabs.CustomTabAppMenuRenderTest#testTwaAppMenu' test failure on 'android-desktop-17-x64-rel-emu-tests' trybot.
    
    Failure Link: https://ci.chromium.org/ui/p/chrome/builders/ci/android-desktop-17-x64-rel-emu-tests/3166/overview
    
    Original change's description:
    > [TWA] Hides 'site controls'  from showing in TWA
    >
    > Fixes such that site controls is overridden to not show on the app menu
    > for TWA, as expected.
    >
    > Adds unit tests and a render test.
    >
    > Bug: 549795470
    > Change-Id: Ic401273631d08b5101b66aeeac1043a9d59777da
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8300574
    > Reviewed-by: Sinan Sahin <sinansahin@google.com>
    > Commit-Queue: Joel Tan-Aristy <jtanaristy@google.com>
    > Auto-Submit: Joel Tan-Aristy <jtanaristy@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1687400}
    
    Bug: 549795470
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I16049f7f175d216b533bb2b6cf91adb961cd8d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8311888
    Owners-Override: Fuhsin Liao <fuhsinliao@google.com>
    Reviewed-by: Fuhsin Liao <fuhsinliao@google.com>
    Auto-Submit: Fuhsin Liao <fuhsinliao@google.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Fuhsin Liao <fuhsinliao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1687762}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabAppMenuPropertiesDelegate.java
@@ -487,14 +487,6 @@ public class CustomTabAppMenuPropertiesDelegate extends AppMenuPropertiesDelegat
         modelList.add(new MVCListAdapter.ListItem(AppMenuHandler.AppMenuItemType.STANDARD, model));
     }
 
-    @Override
-    protected boolean shouldShowPageInfoItem() {
-        if (mUiType == CustomTabsUiType.TRUSTED_WEB_ACTIVITY) {
-            return false;
-        }
-        return super.shouldShowPageInfoItem();
-    }
-
     private boolean shouldShowReaderModeItem() {
         if (!ChromeFeatureList.sCctAdaptiveButton.isEnabled()
                 || !ChromeFeatureList.getFieldTrialParamByFeatureAsBoolean(
