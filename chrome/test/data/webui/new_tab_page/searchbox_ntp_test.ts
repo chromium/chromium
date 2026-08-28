13c1798fa277f62cb8acb1a41e2e6f48d37f630a
Jennifer Serrano <jennserrano@google.com>
2026-08-28 14:52:17 -0700

    [153] Add Realbox tab picker support.
    Original change's description:
    > Add Realbox tab picker support.
    >
    > Checks that the searchboxOverride is set to kRealbox to allow the
    > chip that has the preselected input source of kInputSourceTabPicker to
    > open the Realbox's context menu with the tab flyout open.
    >
    > screencast/cast/NTgzMDQwNDk2NjcxMTI5NnxiY2ZlZDhhNC05Ng
    >
    > Bug: 549938492
    > Change-Id: I9bdaa350264785a58efb6c8fd1cd688bae7b6d37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280273
    > Reviewed-by: Riley Tatum <rtatum@google.com>
    > Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1685263}
    
    Bug: 553947386
    Change-Id: I9bdaa350264785a58efb6c8fd1cd688bae7b6d37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8315847
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8010@{#550}
    Cr-Branched-From: 86cee6df69e0463a839c0cc8435c9d4c259434d3-refs/heads/main@{#1681091}

--- chrome/test/data/webui/new_tab_page/searchbox_ntp_test.ts
+++ chrome/test/data/webui/new_tab_page/searchbox_ntp_test.ts
@@ -5,7 +5,7 @@
 import 'chrome://new-tab-page/new_tab_page.js';
 
 import type {NtpSearchboxElement, SearchboxIconElement, SearchboxMatchElement} from 'chrome://new-tab-page/new_tab_page.js';
-import {BrowserProxyImpl, MetricsReporterImpl, SearchboxBrowserProxy} from 'chrome://new-tab-page/new_tab_page.js';
+import {BrowserProxyImpl, InputSource, MetricsReporterImpl, SearchboxBrowserProxy, SearchboxOverride} from 'chrome://new-tab-page/new_tab_page.js';
 import type {ContextualEntrypointAndMenuElement} from 'chrome://resources/cr_components/composebox/contextual_entrypoint_and_menu.js';
 import {createAutocompleteResultForTesting, createSearchMatchForTesting} from 'chrome://resources/cr_components/searchbox/searchbox_browser_proxy.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
@@ -1331,4 +1331,31 @@ suite('SearchboxTest', () => {
     realbox.closeContextMenu();
     assertTrue(closeMenuCalled);
   });
+
+  test(
+      'handleFuseboxAction opens tab picker for kInputSourceTabPicker',
+      async () => {
+        const realbox = await createAndAppendRealbox({
+          ntpRealboxNextEnabled: true,
+        });
+        const context =
+            realbox.shadowRoot
+                .querySelector<ContextualEntrypointAndMenuElement>('#context');
+        assertTrue(!!context);
+
+        assertFalse(realbox.shareTabsFlyoutOpen);
+
+        await realbox.handleFuseboxAction({
+          preselectedTool: null,
+          preferredInventory: null,
+          preselectedModel: null,
+          queryActionOverride: null,
+          preselectedInputSource: InputSource.kInputSourceTabPicker,
+          searchboxOverride: SearchboxOverride.kRealbox,
+        });
+        await microtasksFinished();
+
+        assertTrue(realbox.shareTabsFlyoutOpen);
+      });
+
 });
