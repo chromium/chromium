65f70b25e40ce6f43abaea0fc2929a926202863f
Howard Chang <howardcha@google.com>
2026-08-27 19:06:56 -0700

    Default-enable SkillsServiceApi for permanent 1P Skills launch
    Flip the compiled feature defaults so the 1P Skills feature stays enabled
    after the Localized1PSkills Finch study expires:
    
    - kSkillsServiceApi: FEATURE_ENABLED_BY_DEFAULT
    - kSkills1PDisabledForNonEnLocales: FEATURE_DISABLED_BY_DEFAULT
    
    This makes the behavior currently served by the Localized1PSkills Finch
    study the default in the binary. Older milestones continue to receive the
    feature via the Finch config (max_version), while newer milestones have it
    enabled directly in code.
    
    Bug: 505413031
    Change-Id: Id045afd0d69314cce57ae10ef9c0d4862c32f3af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8305776
    Commit-Queue: Howard Chang <howardcha@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1687728}

--- components/skills/features.cc
+++ components/skills/features.cc
@@ -18,11 +18,11 @@ BASE_FEATURE(kSkillsMetricsProviderEnabled, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kSkillsRefinementEnabled, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kSkillsAutocomplete, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kSkills1PDisabledForNonEnLocales,
-             base::FEATURE_ENABLED_BY_DEFAULT);
+             base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kSkillsSubheadersEnabled, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kSkillsWebViewV2Enabled, base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kSkillsServiceApi, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kSkillsServiceApi, base::FEATURE_ENABLED_BY_DEFAULT);
 const base::FeatureParam<std::string> kSkillsServiceApiUrl{
     &kSkillsServiceApi, "endpoint",
     "https://chromeskills.pa.googleapis.com/v1/management/"
