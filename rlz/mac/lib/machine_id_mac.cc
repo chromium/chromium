89494147413e20186c400e2fd1eecb3c30de9b9f
Arthur Sonzogni <arthursonzogni@chromium.org>
2026-08-31 03:44:48 -0700

    [rlz] Return std::optional<MacAddress> from GetMacAddress on Mac
    Modernize GetMACAddressFromIterator and GetMacAddress in
    machine_id_mac.cc to return
    std::optional<std::array<uint8_t, kIOEthernetAddressSize>> instead of
    using a raw buffer out-parameter.
    
    Bug: 351564777, 501112808
    Change-Id: I47146ce23554868a2bfbaeb8708c23dbd98db745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8278775
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1688952}

--- rlz/mac/lib/machine_id_mac.cc
+++ rlz/mac/lib/machine_id_mac.cc
@@ -10,11 +10,12 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <array>
+#include <optional>
 #include <string>
 
 #include "base/apple/foundation_util.h"
 #include "base/apple/scoped_cftyperef.h"
-#include "base/compiler_specific.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_ioobject.h"
 #include "base/strings/stringprintf.h"
@@ -24,21 +25,25 @@ namespace rlz_lib {
 
 namespace {
 
+using MacAddress = std::array<uint8_t, kIOEthernetAddressSize>;
+
 // See http://developer.apple.com/library/mac/#technotes/tn1103/_index.html
 
 // The caller is responsible for freeing |matching_services|.
 bool FindEthernetInterfaces(io_iterator_t* matching_services) {
   base::apple::ScopedCFTypeRef<CFMutableDictionaryRef> matching_dict(
       IOServiceMatching(kIOEthernetInterfaceClass));
-  if (!matching_dict)
+  if (!matching_dict) {
     return false;
+  }
 
   base::apple::ScopedCFTypeRef<CFMutableDictionaryRef> primary_interface(
       CFDictionaryCreateMutable(kCFAllocatorDefault, 0,
                                 &kCFTypeDictionaryKeyCallBacks,
                                 &kCFTypeDictionaryValueCallBacks));
-  if (!primary_interface)
+  if (!primary_interface) {
     return false;
+  }
 
   CFDictionarySetValue(primary_interface.get(), CFSTR(kIOPrimaryInterface),
                        kCFBooleanTrue);
@@ -51,14 +56,10 @@ bool FindEthernetInterfaces(io_iterator_t* matching_services) {
   return kern_result == KERN_SUCCESS;
 }
 
-bool GetMACAddressFromIterator(io_iterator_t primary_interface_iterator,
-                               uint8_t* buffer, size_t buffer_size) {
-  if (buffer_size < kIOEthernetAddressSize)
-    return false;
+std::optional<MacAddress> GetMACAddressFromIterator(
+    io_iterator_t primary_interface_iterator) {
+  std::optional<MacAddress> mac_address;
 
-  bool success = false;
-
-  UNSAFE_TODO(bzero(buffer, buffer_size));
   base::mac::ScopedIOObject<io_object_t> primary_interface;
   while (primary_interface.reset(IOIteratorNext(primary_interface_iterator)),
          primary_interface) {
@@ -67,46 +68,45 @@ bool GetMACAddressFromIterator(io_iterator_t primary_interface_iterator,
         primary_interface.get(), kIOServicePlane, &primary_interface_parent);
     base::mac::ScopedIOObject<io_object_t> primary_interface_parent_deleter(
         primary_interface_parent);
-    success = kern_result == KERN_SUCCESS;
-
-    if (!success)
+    if (kern_result != KERN_SUCCESS) {
       continue;
+    }
 
     base::apple::ScopedCFTypeRef<CFTypeRef> mac_data(
         IORegistryEntryCreateCFProperty(primary_interface_parent,
                                         CFSTR(kIOMACAddress),
                                         kCFAllocatorDefault, 0));
     CFDataRef mac_data_data = base::apple::CFCast<CFDataRef>(mac_data.get());
-    if (mac_data_data) {
-      CFDataGetBytes(
-          mac_data_data, CFRangeMake(0, kIOEthernetAddressSize), buffer);
+    if (mac_data_data &&
+        CFDataGetLength(mac_data_data) >= kIOEthernetAddressSize) {
+      MacAddress mac{};
+      CFDataGetBytes(mac_data_data, CFRangeMake(0, kIOEthernetAddressSize),
+                     mac.data());
+      mac_address = mac;
     }
   }
 
-  return success;
+  return mac_address;
 }
 
-bool GetMacAddress(unsigned char* buffer, size_t size) {
+std::optional<MacAddress> GetMacAddress() {
   io_iterator_t primary_interface_iterator;
-  if (!FindEthernetInterfaces(&primary_interface_iterator))
-    return false;
-  bool result = GetMACAddressFromIterator(
-      primary_interface_iterator, buffer, size);
-  IOObjectRelease(primary_interface_iterator);
-  return result;
+  if (!FindEthernetInterfaces(&primary_interface_iterator)) {
+    return std::nullopt;
+  }
+  base::mac::ScopedIOObject<io_iterator_t> primary_interface_iterator_deleter(
+      primary_interface_iterator);
+  return GetMACAddressFromIterator(primary_interface_iterator);
 }
 
 }  // namespace
 
 bool GetRawMachineId(std::u16string* data, int* more_data) {
-  uint8_t mac_address[kIOEthernetAddressSize];
-
   data->clear();
-  if (GetMacAddress(mac_address, sizeof(mac_address))) {
-    *data += base::ASCIIToUTF16(
-        base::StringPrintf("mac:%02x%02x%02x%02x%02x%02x",
-                           mac_address[0], mac_address[1], mac_address[2],
-                           mac_address[3], mac_address[4], mac_address[5]));
+  if (std::optional<MacAddress> mac_address = GetMacAddress()) {
+    const auto& [m0, m1, m2, m3, m4, m5] = *mac_address;
+    *data += base::ASCIIToUTF16(base::StringPrintf(
+        "mac:%02x%02x%02x%02x%02x%02x", m0, m1, m2, m3, m4, m5));
   }
 
   // A MAC address is enough to uniquely identify a machine, but it's only 6
