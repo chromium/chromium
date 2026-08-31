215c778c4f92ccef9965d44ec432a1661ad03f91
Jakob Koblinsky <jkoblinsky@google.com>
2026-08-31 16:27:15 -0700

    Reland "[LocationBar] Add suggestions mode facility and test"
    This is a reland of commit 6f5bffb43ba6b00a01bc44b6bb4be24f388e7809. We
    fixed the underlying cause of the flakiness in crrev.com/c/8308416.
    
    Original change's description:
    > [LocationBar] Add suggestions mode facility and test
    >
    > Bug: 530079993
    > Change-Id: Ie6425bedbcc385af3c071052029284a2a2f45495
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8267794
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Commit-Queue: Jakob Koblinsky <jkoblinsky@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1684874}
    
    Bug: 530079993
    Change-Id: Ia8abcbc8b89cc0e49e62e0e5157c5d583046cc05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8328588
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Jakob Koblinsky <jkoblinsky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689391}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/omnibox/OmniboxEnteredTextFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/omnibox/OmniboxEnteredTextFacility.java
@@ -4,12 +4,24 @@
 
 package org.chromium.chrome.test.transit.omnibox;
 
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.allOf;
+
+import static org.chromium.base.test.transit.ViewSpec.viewSpec;
+
+import android.view.View;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.transit.Facility;
 import org.chromium.base.test.transit.Station;
+import org.chromium.base.test.transit.ViewSpec;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.omnibox.LocationBarLayout;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.omnibox.OmniboxCapabilities;
 
@@ -22,6 +34,16 @@ import org.chromium.components.omnibox.OmniboxCapabilities;
 public class OmniboxEnteredTextFacility extends Facility<Station<?>> {
     private final OmniboxFacility mOmniboxFacility;
     private final String mText;
+
+    public static final ViewSpec<LocationBarLayout> LOCATION_BAR_POPPED_OUT =
+            viewSpec(
+                    LocationBarLayout.class,
+                    allOf(
+                            withId(R.id.location_bar),
+                            isDescendantOfA(withId(R.id.omnibox_suggestions_container))));
+
+    public static final ViewSpec<View> SUGGESTIONS_DROPDOWN =
+            viewSpec(allOf(withId(R.id.omnibox_suggestions_dropdown), isDisplayed()));
 
     public OmniboxEnteredTextFacility(OmniboxFacility omniboxFacility, String text) {
         mOmniboxFacility = omniboxFacility;
@@ -61,7 +83,7 @@ public class OmniboxEnteredTextFacility extends Facility<Station<?>> {
     }
 
     /** Simulate autocomplete suggestion received from the server. */
-    public OmniboxEnteredTextFacility simulateAutocomplete(String autocompleted) {
+    public OmniboxSuggestionsFacility simulateAutocomplete(String autocompleted) {
         return runTo(
                         () -> {
                             Profile profile =
@@ -72,7 +94,7 @@ public class OmniboxEnteredTextFacility extends Facility<Station<?>> {
                         })
                 .exitFacilityAnd()
                 .enterFacility(
-                        new OmniboxEnteredTextFacility(mOmniboxFacility, mText + autocompleted));
+                        new OmniboxSuggestionsFacility(mOmniboxFacility, mText + autocompleted));
     }
 
     /** Clear text in the omnibox. */
