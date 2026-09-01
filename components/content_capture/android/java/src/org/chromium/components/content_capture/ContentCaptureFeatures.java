ed30b08ef3aef709b50a525401a0cd9d7064a5fe
Fiaz Muhammad <mfiaz@google.com>
2026-09-01 14:04:23 -0700

    [TriState] Migrate @Nullable Boolean in ContentCaptureFeatures
    Migrate the static sEnableDebugLogging cached field from @Nullable
    Boolean to @TriState int in ContentCaptureFeatures to eliminate heap
    allocations for boxed Boolean objects and improve memory efficiency.
    
    Bug: 545992990
    Change-Id: I3e5e9666daecbfe551367a7895f048a0d73f48ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8334429
    Auto-Submit: Fiaz Muhammad <mfiaz@google.com>
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Reviewed-by: Sayed Elabady <elabadysayed@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1690111}

--- components/content_capture/android/java/src/org/chromium/components/content_capture/ContentCaptureFeatures.java
+++ components/content_capture/android/java/src/org/chromium/components/content_capture/ContentCaptureFeatures.java
@@ -6,24 +6,25 @@ package org.chromium.components.content_capture;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.base.CommandLine;
+import org.chromium.base.TriState;
+import org.chromium.base.TriStateUtils;
 import org.chromium.build.annotations.NullMarked;
-import org.chromium.build.annotations.Nullable;
 
 /** The class to get if feature is enabled from native. */
 @NullMarked
 public class ContentCaptureFeatures {
     private static final String FLAG = "dump-captured-content-to-logcat-for-testing";
-    private static @Nullable Boolean sEnableDebugLogging;
+    private static @TriState int sEnableDebugLogging;
 
     public static boolean isEnabled() {
         return ContentCaptureFeaturesJni.get().isEnabled();
     }
 
     public static boolean isDumpForTestingEnabled() {
-        if (sEnableDebugLogging == null) {
-            sEnableDebugLogging = CommandLine.getInstance().hasSwitch(FLAG);
+        if (sEnableDebugLogging == TriState.NOT_SET) {
+            sEnableDebugLogging = TriStateUtils.from(CommandLine.getInstance().hasSwitch(FLAG));
         }
-        return sEnableDebugLogging;
+        return sEnableDebugLogging == TriState.TRUE;
     }
 
     @NativeMethods
