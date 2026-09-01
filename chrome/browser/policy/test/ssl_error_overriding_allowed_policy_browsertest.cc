beaf523865e8211a5d1c0cc5e45ec177a3b88c5f
Evan Luo <evanluo@google.com>
2026-09-01 16:20:59 -0700

    [sheriff] Disable flaky SafeBrowsingPolicyTest.SSLErrorOverridingAllowed
    Bug: 555854628
    Change-Id: I80104192c58621a783e2724fbfa9a3d97380b84f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8339787
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Yanting Yu <yyanting@google.com>
    Owners-Override: Evan Luo <evanluo@google.com>
    Commit-Queue: Yanting Yu <yyanting@google.com>
    Auto-Submit: Evan Luo <evanluo@google.com>
    Reviewed-by: Madhav Pruthi <madhavpruthi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690282}

--- chrome/browser/policy/test/ssl_error_overriding_allowed_policy_browsertest.cc
+++ chrome/browser/policy/test/ssl_error_overriding_allowed_policy_browsertest.cc
@@ -5,6 +5,7 @@
 #include <vector>
 
 #include "base/strings/string_number_conversions.h"
+#include "build/build_config.h"
 #include "chrome/browser/interstitials/security_interstitial_page_test_utils.h"
 #include "chrome/browser/policy/safe_browsing_policy_test.h"
 #include "chrome/browser/profiles/profile.h"
@@ -68,8 +69,15 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
 
 // Test that when SSL error overriding is allowed, the origin list is ignored
 // and the proceed link appears on SSL blocking pages.
+// TODO(crbug.com/555854628): Flaky on Android.
+#if BUILDFLAG(IS_ANDROID)
+#define MAYBE_SSLErrorOverridingAllowedEnabled \
+  DISABLED_SSLErrorOverridingAllowedEnabled
+#else
+#define MAYBE_SSLErrorOverridingAllowedEnabled SSLErrorOverridingAllowedEnabled
+#endif
 IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
-                       SSLErrorOverridingAllowedEnabled) {
+                       MAYBE_SSLErrorOverridingAllowedEnabled) {
   net::EmbeddedTestServer https_server_expired(
       net::EmbeddedTestServer::TYPE_HTTPS);
   https_server_expired.SetSSLConfig(net::EmbeddedTestServer::CERT_EXPIRED);
@@ -112,8 +120,16 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
 
 // Test that when SSL error overriding is disabled, the proceed link does not
 // appear appear on SSL blocking pages.
+// TODO(crbug.com/555854628): Flaky on Android.
+#if BUILDFLAG(IS_ANDROID)
+#define MAYBE_SSLErrorOverridingAllowedDisabled \
+  DISABLED_SSLErrorOverridingAllowedDisabled
+#else
+#define MAYBE_SSLErrorOverridingAllowedDisabled \
+  SSLErrorOverridingAllowedDisabled
+#endif
 IN_PROC_BROWSER_TEST_F(SafeBrowsingPolicyTest,
-                       SSLErrorOverridingAllowedDisabled) {
+                       MAYBE_SSLErrorOverridingAllowedDisabled) {
   net::EmbeddedTestServer https_server_expired(
       net::EmbeddedTestServer::TYPE_HTTPS);
   https_server_expired.SetSSLConfig(net::EmbeddedTestServer::CERT_EXPIRED);
