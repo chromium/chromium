982a4a86f53215ad88d27b5a5cb1d473283fa69e
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-09-01 14:33:02 -0700

    [realbox][nextbox] Enable ContextMenuAnimationLimiting by default.
    This change enables the ContextMenuAnimationLimiting feature by default
    in the codebase and removes its corresponding entry from the fieldtrial
    testing config.
    
    Bug: b:505433159
    Change-Id: Ic5d20d63911e852b94ababa1acbd3c7c7cd7be37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8334391
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Jennifer Serrano <jennserrano@google.com>
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Cr-Commit-Position: refs/heads/main@{#1690152}

--- components/omnibox/common/composebox_features.cc
+++ components/omnibox/common/composebox_features.cc
@@ -16,7 +16,7 @@ BASE_FEATURE(kContextManagementInComposebox, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kContextMenuToolTips, base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kTabFaviconChipsToCoins, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kContextManagementInOmnibox, base::FEATURE_DISABLED_BY_DEFAULT);
-BASE_FEATURE(kContextMenuAnimationLimiting, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kContextMenuAnimationLimiting, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kComposeboxSkillsContextualTasks,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kComposeboxSkillsNtp, base::FEATURE_DISABLED_BY_DEFAULT);
