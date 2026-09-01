d6c1a635179a71b2930590b1df9f0547eac57d23
Dhruv Kathpalia <dhruvkathpalia@google.com>
2026-08-31 19:02:50 -0700

    [realbox] Enable Desktop Realbox Contextual Search IPH by default.
    This change enables the IPH_DesktopRealboxContextualSearchFeature by
    default in the feature constants and removes the corresponding
    DesktopRealboxContextualSearchIPH field trial testing configuration as
    it is no longer needed.
    
    Bug: b:507869549
    Change-Id: I5622da78634b68268848a309f94da5566171e5ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8303902
    Commit-Queue: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Auto-Submit: Dhruv Kathpalia <dhruvkathpalia@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1689522}

--- components/feature_engagement/public/feature_constants.cc
+++ components/feature_engagement/public/feature_constants.cc
@@ -73,7 +73,7 @@ BASE_FEATURE(kIPHDesktopCustomizeChromeAutoOpenFeature,
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHDesktopRealboxContextualSearchFeature,
              "IPH_DesktopRealboxContextualSearchFeature",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kIPHDiscardRingFeature,
              "IPH_DiscardRing",
              base::FEATURE_ENABLED_BY_DEFAULT);
