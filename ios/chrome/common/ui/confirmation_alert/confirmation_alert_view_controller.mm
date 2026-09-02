ebcde6c48dc625754454a5830613da7f8c8f3849
Robbie Gibson <rkgibson@google.com>
2026-09-02 14:04:50 -0700

    [iOS] Stop double-counting in confirmation alert height calculation
    I think the customSpacingBeforeImage was already added in to [super
    preferredHeightForContent].
    
    In my testing, changing customSpacingBeforeImage changed the height by
    double the amount changed (see bug).
    
    Bug: b:555721806
    Change-Id: I96c42187b9c99134097288b6f5375c4dab6a6f3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8334531
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691086}

--- ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.mm
+++ ios/chrome/common/ui/confirmation_alert/confirmation_alert_view_controller.mm
@@ -226,12 +226,6 @@ const CGFloat kFaviconBadgeSideLength = 24;
   // Do nothing by default. Subclasses can override this.
 }
 
-- (CGFloat)preferredHeightForContent {
-  CGFloat height = [super preferredHeightForContent];
-  height += self.customSpacingBeforeImage;
-  return height;
-}
-
 #pragma mark - ButtonStackActionDelegate
 
 - (void)didTapPrimaryActionButton {
