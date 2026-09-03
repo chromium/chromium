d10e5609a2f62544ae5eb8c61436f645fcb9d4ff
Scott Haseley <shaseley@chromium.org>
2026-09-03 08:52:27 -0700

    PaintTiming: Remove unused callback type
    This was removed at some point while refactoring the paint timing
    detectors.
    
    Bug: None
    Change-Id: Iab5e8c632ed4bca69e9c358bf169ba0c47586da5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8348343
    Commit-Queue: Michal Mocny <mmocny@chromium.org>
    Auto-Submit: Scott Haseley <shaseley@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691641}

--- third_party/blink/renderer/core/paint/timing/paint_timing_callbacks.h
+++ third_party/blink/renderer/core/paint/timing/paint_timing_callbacks.h
@@ -24,18 +24,6 @@ using PaintTimingCallback =
     base::OnceCallback<void(const base::TimeTicks&, const DOMPaintTimingInfo&)>;
 using OptionalPaintTimingCallback = std::optional<PaintTimingCallback>;
 
-// Presentation time callback for the image and text paint timing detectors. The
-// detectors should update the presentation time for any pending
-// `PaintTimingRecord`s for the relevant frame and add them to the given vector.
-template <IsDerivedFromPaintTimingRecord T>
-using PaintTimingDetectorCallback =
-    base::OnceCallback<void(const base::TimeTicks&,
-                            const DOMPaintTimingInfo&,
-                            HeapVector<Member<T>>&)>;
-template <IsDerivedFromPaintTimingRecord T>
-using OptionalPaintTimingDetectorCallback =
-    std::optional<PaintTimingDetectorCallback<T>>;
-
 }  // namespace blink
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_TIMING_PAINT_TIMING_CALLBACKS_H_
