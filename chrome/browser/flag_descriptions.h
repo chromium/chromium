0f604753b43bd30a4eb038201eb00e73ad4c0ab5
rosiey <rosiey@google.com>
2026-09-02 17:14:38 -0700

    [Glic] Clean up expired glic-experimental-triggering flag
    In accordance with flag expiry guidelines, this flag has expired in M154.
    
    * Removes the flag entry from chrome://flags UI in about_flags.cc.
    
    * Removes the name and description string constants from flag_descriptions.h.
    
    * Removes the flag configuration metadata from flag-metadata.json.
    
    * Keeps base::Feature kGlicExperimentalTriggering in chrome_features for rollout and country targeting controls.
    
    Bug: b:548281956
    Change-Id: Ifc1467293d605070ae37d24b2d264d753ab4fb4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8342448
    Commit-Queue: Rosie Yang <rosiey@google.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691213}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1701,10 +1701,6 @@ inline constexpr char kGlicHandoffButtonHideWhenModalUIShownName[] =
     "Glic handoff button hide when modal UI shown";
 inline constexpr char kGlicHandoffButtonHideWhenModalUIShownDescription[] =
     "Hides the Glic handoff button when a tab-modal UI is active.";
-inline constexpr char kGlicExperimentalTriggeringName[] =
-    "Glic experimental triggering";
-inline constexpr char kGlicExperimentalTriggeringDescription[] =
-    "Enables GLIC experimental triggering";
 inline constexpr char kGlicExperimentalTriggeringScreenshotName[] =
     "Glic experimental triggering screenshot";
 inline constexpr char kGlicExperimentalTriggeringScreenshotDescription[] =
