1ac72cbb1dc584384fe3159f5b6a03f3485c9806
Caitlin Chen <caitc@google.com>
2026-09-03 15:24:33 -0700

    [M154] [Omnibox] Add kAskGShowChip to has_contextual_chips logic
    Original change's description:
    > [Omnibox] Add kAskGShowChip to has_contextual_chips logic
    >
    > In ccrev.com/8279584, I missed adding the LE logic to
    > has_contextual_chips. If a web page has no contextual search
    > suggestions, `result().empty()` and kAskGShowChip was enabled it would
    > show no context entry menu point and no chip.
    >
    > This fix makes it show the entry point and the chip when we have the
    > experiment enabled and there are no results.
    >
    > http://screenshot/3mg6v3r48i3jo (Before state in bug).
    >
    > Bug: b:555271805
    > Change-Id: I35cce445fd86e18d9701870098c0bc4fd70adab4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8331727
    > Commit-Queue: Caitlin Chen <caitc@google.com>
    > Reviewed-by: Marlon Facey <mfacey@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1689990}
    
    (cherry picked from commit 0c6338f1a19a9c44805ed1f053f45afa99905d28)
    
    Bug: 556747821,b:555271805
    Change-Id: I35cce445fd86e18d9701870098c0bc4fd70adab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8354644
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8037@{#164}
    Cr-Branched-From: e10b20e60f162e7ad88e4930833695a54f4e15fb-refs/heads/main@{#1689415}

--- components/omnibox/browser/mock_autocomplete_provider_client.h
+++ components/omnibox/browser/mock_autocomplete_provider_client.h
@@ -168,7 +168,9 @@ class MockAutocompleteProviderClient
   MOCK_CONST_METHOD0(IsPagePaywalled, std::optional<bool>());
   MOCK_METHOD(bool, ShouldSendContextualUrlSuggestParam, (), (const));
   MOCK_METHOD(bool, ShouldSendPageTitleSuggestParam, (), (const));
+  MOCK_METHOD(bool, IsOmniboxNextLensSearchChipEnabled, (), (const, override));
   MOCK_METHOD(bool, IsAskGShowChipEnabled, (), (const, override));
+  MOCK_METHOD(bool, IsOmniboxNextAimPopupEnabled, (), (const, override));
   MOCK_METHOD(bool, IsWebUiNtpEnabledForDesktopAndroid, (), (const, override));
   MOCK_CONST_METHOD1(GetLensSuggestInputsWhenReady,
                      base::CallbackListSubscription(
