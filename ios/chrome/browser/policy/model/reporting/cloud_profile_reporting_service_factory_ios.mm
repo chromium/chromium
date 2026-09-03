46c0201808030b60002b2393f493a1305f7f1b4e
Michał Kaczmarczyk <mickaczmarczyk@google.com>
2026-09-03 08:37:36 -0700

    [enterprise] Register AttestationServiceIOS provider in CloudProfileReportingServiceFactoryIOS
    Wires ios::provider::CreateAttestationServiceIOS into
    DeviceAttestationServiceFactory within
    CloudProfileReportingServiceFactoryIOS on iOS. This provides the iOS
    attestation implementation used by ChromeProfileRequestGenerator for
    profile reporting.
    
    Bug: 421927804
    Change-Id: I0f036679b53c69005a517d52a1ff333be76abc74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8351585
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Michał Kaczmarczyk <mickaczmarczyk@google.com>
    Cr-Commit-Position: refs/heads/main@{#1691623}

--- ios/chrome/browser/policy/model/reporting/cloud_profile_reporting_service_factory_ios.mm
+++ ios/chrome/browser/policy/model/reporting/cloud_profile_reporting_service_factory_ios.mm
@@ -8,11 +8,14 @@
 #import <utility>
 
 #import "base/feature_list.h"
+#import "base/functional/bind.h"
+#import "components/enterprise/device_attestation/device_attestation_service_factory.h"
 #import "ios/chrome/browser/enterprise/identifiers/profile_id_service_factory_ios.h"
 #import "ios/chrome/browser/enterprise/signals/model/ios_signals_aggregator_factory.h"
 #import "ios/chrome/browser/policy/model/reporting/cloud_profile_reporting_service_ios.h"
 #import "ios/chrome/browser/policy/model/reporting/features.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#import "ios/public/provider/chrome/browser/device_attestation/device_attestation_api.h"
 
 namespace enterprise_reporting {
 
@@ -46,6 +49,8 @@ CloudProfileReportingServiceFactoryIOS::CloudProfileReportingServiceFactoryIOS()
                                     TestingCreation::kNoServiceForTests) {
   DependsOn(enterprise::ProfileIdServiceFactoryIOS::GetInstance());
   DependsOn(IOSSignalsAggregatorFactory::GetInstance());
+  enterprise::DeviceAttestationServiceFactory::SetAttestationServiceIOSProvider(
+      base::BindRepeating(&ios::provider::CreateAttestationServiceIOS));
 }
 
 CloudProfileReportingServiceFactoryIOS::
