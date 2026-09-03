dbef44fa37037c8b9048f725dcb93494f6bf461d
Alison Gale <agale@chromium.org>
2026-09-03 10:13:37 -0700

    [Organizer] Start generalizing types in recent tabs delegate
    Once we add tab groups and split views, the types will need to be
    generalized. Want to pull this out to make the groups part easier to
    follow. Followup will look like this:
    https://chromium-review.git.corp.google.com/c/chromium/src/+/8339975
    
    Bug: 555791438
    Change-Id: I12186a54bc053a28f2a43a28f6719fe48adbf8f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8352105
    Reviewed-by: Chase Hartsell <chazzy@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691719}

--- chrome/browser/resources/organizer_panel/delegates/recent_tabs_delegate.ts
+++ chrome/browser/resources/organizer_panel/delegates/recent_tabs_delegate.ts
@@ -10,12 +10,17 @@ import type {OrganizerListSectionItem} from '../organizer_list_section_item.js';
 import type {BrowserProxy, ProfileData, RecentlyClosedTab, TabsRemovedInfo} from '../tab_search.mojom-webui.js';
 import {browserProxyFactory} from '../tab_search.mojom-webui.js';
 
+// Union type to represent all possible recently closed items (tabs, tab groups,
+// split views).
+export type RecentlyClosedItem = RecentlyClosedTab;
+
 export class RecentTabsDelegate implements
-    OrganizerListSectionDelegate<RecentlyClosedTab> {
+    OrganizerListSectionDelegate<RecentlyClosedItem> {
   private browserProxy_: BrowserProxy = browserProxyFactory.getInstance();
   private client_?: OrganizerListSectionClient;
   private listenerIds_: number[] = [];
   private tabs_: RecentlyClosedTab[] = [];
+  private items_: RecentlyClosedItem[] = [];
 
   init(sectionClient: OrganizerListSectionClient) {
     this.client_ = sectionClient;
@@ -35,29 +40,31 @@ export class RecentTabsDelegate implements
   }
 
   async getItems():
-      Promise<Array<OrganizerListSectionItem<RecentlyClosedTab>>> {
-    await this.updateTabs_();
-    return this.tabs_.map(tab => this.tabToSectionItem_(tab));
+      Promise<Array<OrganizerListSectionItem<RecentlyClosedItem>>> {
+    await this.updateItems_();
+    return this.items_.map(item => this.toSectionItem_(item));
   }
 
-  onItemClick(item: OrganizerListSectionItem<RecentlyClosedTab>) {
-    const tab = item.data;
-    assert(tab);
-    this.browserProxy_.handler.openRecentlyClosedEntry(tab.tabId);
+  onItemClick(item: OrganizerListSectionItem<RecentlyClosedItem>) {
+    const data = item.data;
+    assert(data);
+    this.browserProxy_.handler.openRecentlyClosedEntry(data.tabId);
   }
 
-  private async updateTabs_() {
+  private async updateItems_() {
     const {profileData} = await this.browserProxy_.handler.getProfileData();
-    this.tabs_ = this.extractAndSortTabs_(profileData);
+    this.tabs_ = profileData.recentlyClosedTabs;
+    this.items_ = this.extractAndSortItems_();
   }
 
   private notifyClient_() {
     this.client_?.onItemsChanged(
-        this.tabs_.map(tab => this.tabToSectionItem_(tab)));
+        this.items_.map(item => this.toSectionItem_(item)));
   }
 
   private onTabsChanged_(profileData: ProfileData) {
-    this.tabs_ = this.extractAndSortTabs_(profileData);
+    this.tabs_ = profileData.recentlyClosedTabs;
+    this.items_ = this.extractAndSortItems_();
     this.notifyClient_();
   }
 
@@ -68,27 +75,32 @@ export class RecentTabsDelegate implements
 
     const newTabIds =
         new Set(tabsRemovedInfo.recentlyClosedTabs.map(t => t.tabId));
-    const allTabs = [
+    this.tabs_ = [
       ...tabsRemovedInfo.recentlyClosedTabs,
       ...this.tabs_.filter(t => !newTabIds.has(t.tabId)),
     ];
 
-    this.tabs_ = this.sortTabs_(allTabs);
+    this.items_ = this.extractAndSortItems_();
     this.notifyClient_();
   }
 
-  private extractAndSortTabs_(profileData: ProfileData): RecentlyClosedTab[] {
-    return this.sortTabs_([...profileData.recentlyClosedTabs]);
+  private extractAndSortItems_(): RecentlyClosedItem[] {
+    return this.sortItems_(this.tabs_);
   }
 
-  private sortTabs_(tabs: RecentlyClosedTab[]): RecentlyClosedTab[] {
-    tabs.sort((a, b) => {
+  private sortItems_(items: RecentlyClosedItem[]): RecentlyClosedItem[] {
+    items.sort((a, b) => {
       const timeA = a.lastActiveTime?.internalValue ?? 0n;
       const timeB = b.lastActiveTime?.internalValue ?? 0n;
       return timeB > timeA ? 1 : (timeB < timeA ? -1 : 0);
     });
 
-    return tabs;
+    return items;
+  }
+
+  private toSectionItem_(item: RecentlyClosedItem):
+      OrganizerListSectionItem<RecentlyClosedItem> {
+    return this.tabToSectionItem_(item);
   }
 
   private tabToSectionItem_(tab: RecentlyClosedTab):
