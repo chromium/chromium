e2a3f4969cc15afafc209000391ed6ab8b0be4dc
Muhammad Salmaan <musalmaan@chromium.org>
2026-09-03 06:53:49 -0700

    Enable Session Restore InfoBar Opt-In arm by default
    Enables the Session Restore InfoBar opt-in experience by default on
    desktop platforms (Mac, Windows, Linux).
    
    - Set features::kSessionRestoreInfobar to base::FEATURE_ENABLED_BY_DEFAULT.
    - Retain features::kSetDefaultToContinueSession with default false,
      ensuring the startup preference remains DEFAULT (New Tab Page) and
      users receive the opt-in prompt to turn on session restore.
    - Remove the SessionRestoreInfobar study from fieldtrial_testing_config.json now that the feature will enabled
    to 90%.
    
    Bug: 423868678
    Change-Id: I907c61bf6ffeff3d656c19d8f80ef763741bec9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8339513
    Reviewed-by: Foromo Daniel Soromou <koretadaniel@chromium.org>
    Commit-Queue: Muhammad Salmaan <musalmaan@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691543}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -365,7 +365,7 @@ BASE_FEATURE(kNonMilestoneUpdateToast, base::FEATURE_ENABLED_BY_DEFAULT);
 
 
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX)
-BASE_FEATURE(kSessionRestoreInfobar, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kSessionRestoreInfobar, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE_PARAM(bool,
                    kSetDefaultToContinueSession,
