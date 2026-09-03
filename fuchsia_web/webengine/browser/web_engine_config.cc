e875a3012892a7ca7630e224143658db6d200d10
Wez <wez@chromium.org>
2026-09-03 11:14:57 -0700

    [fuchsia] Avoid caching protected service workers in static variable
    Do not cache protected service worker patterns from the command line in
    a static variable, preventing state from leaking across tests running in
    the same process when the command line is overridden.
    
    TAG=agy
    CONV=265de3fc-c92e-4c68-8287-98412e745139
    
    Bug: 556576979
    Change-Id: Ibfc298f8dbfcdb3e0893307bd6810b3528dee80d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8351409
    Reviewed-by: Zijie He <zijiehe@google.com>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Auto-Submit: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691777}

--- fuchsia_web/webengine/browser/web_engine_config.cc
+++ fuchsia_web/webengine/browser/web_engine_config.cc
@@ -243,8 +243,7 @@ bool UpdateCommandLineFromConfigFile(const base::DictValue& config,
 }
 
 bool IsProtectedServiceWorker(const GURL& scope) {
-  static const auto protected_service_workers = GetProtectedServiceWorkers();
-  for (const auto& pattern : protected_service_workers) {
+  for (const auto& pattern : GetProtectedServiceWorkers()) {
     if (pattern.Matches(scope)) {
       return true;
     }
