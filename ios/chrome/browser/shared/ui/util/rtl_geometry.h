0113c6d5796d0c3c1c72c31818843a3faf74a8e6
Gauthier Ambard <gambard@chromium.org>
2026-09-03 03:56:09 -0700

    [Clean up] Remove unused RTL geometry and layout helper functions
    This CL removes dead layout rect and UIKit geometry utility functions
    from rtl_geometry (such as LayoutRectEqualToRect,
    LayoutRectGetBoundsRect, LayoutRectGetPositionForAnchor,
    LayoutOffset functions, CGRect edge helpers, and
    ScrollToSemanticLeading) which have no callers across the codebase.
    
    Low-Coverage-Reason: TRIVIALLY_NOT_EVALUATED removing dead code
    Change-Id: I74bd33047e811a1f33688066e0006ff4c711ae24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8321709
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1691466}

--- ios/chrome/browser/shared/ui/util/rtl_geometry.h
+++ ios/chrome/browser/shared/ui/util/rtl_geometry.h
@@ -29,19 +29,8 @@ struct LayoutRectPosition {
   CGFloat originY;
 };
 
-// The null LayoutRectPosition, with `leading` and `originY` equal to 0.0.
-extern const LayoutRectPosition LayoutRectPositionZero;
-
 // Returns a new LayoutRectPosition with the passed-in values.
 LayoutRectPosition LayoutRectPositionMake(CGFloat leading, CGFloat originY);
-
-// Returns YES if `a`'s values are equal to those of `b`.
-BOOL LayoutRectPositionEqualToPosition(LayoutRectPosition a,
-                                       LayoutRectPosition b);
-
-// Returns a new LayoutRectPosition created by aligning `position`'s values to
-// the nearest pixel boundary.
-LayoutRectPosition AlignLayoutRectPositionToPixel(LayoutRectPosition position);
 
 // A LayoutRect contains the information needed to generate a CGRect that may or
 // may not be flipped if positioned in RTL or LTR contexts. `boundingWidth` is
@@ -66,9 +55,6 @@ LayoutRect LayoutRectMake(CGFloat leading,
                           CGFloat width,
                           CGFloat height);
 
-// Returns YES if `a`'s values are equal to those of `b`.
-BOOL LayoutRectEqualToRect(LayoutRect a, LayoutRect b);
-
 // Given `layout`, returns the rect for that layout in text direction
 // `direction`.
 CGRect LayoutRectGetRectUsingDirection(LayoutRect layout,
@@ -76,23 +62,6 @@ CGRect LayoutRectGetRectUsingDirection(LayoutRect layout,
 // As above, using `direction` == RIGHT_TO_LEFT if UseRTLLayout(), LEFT_TO_RIGHT
 // otherwise.
 CGRect LayoutRectGetRect(LayoutRect layout);
-
-// Utilities for getting CALayer positioning values from a layoutRect.
-// Given `layout`, return the bounds rectangle of the generated rect -- that is,
-// a rect with origin (0,0) and size equal to `layout`'s size.
-CGRect LayoutRectGetBoundsRect(LayoutRect layout);
-
-// Given `layout` and some anchor point `anchor` (defined in the way that
-// CALayer's anchorPoint property is), return the CGPoint that defines the
-// position of a rect in the context used by `layout`.
-CGPoint LayoutRectGetPositionForAnchorUsingDirection(
-    LayoutRect layout,
-    CGPoint anchor,
-    base::i18n::TextDirection direction);
-
-// As above, using `direction` == RIGHT_TO_LEFT if UseRTLLayout(), LEFT_TO_RIGHT
-// otherwise.
-CGPoint LayoutRectGetPositionForAnchor(LayoutRect layout, CGPoint anchor);
 
 // Given `rect`, a rect, and `boundingRect`, a rect whose bounds are the
 // context in which `rect`'s frame is interpreted, return the layout that
@@ -107,91 +76,9 @@ LayoutRect LayoutRectForRectInBoundingRectUsingDirection(
 // otherwise.
 LayoutRect LayoutRectForRectInBoundingRect(CGRect rect, CGRect boundingRect);
 
-// Given a layout `layout`, return the layout that defines the leading area up
-// to `layout`.
-LayoutRect LayoutRectGetLeadingLayout(LayoutRect layout);
-
-// Given a layout `layout`, return the layout that defines the trailing area
-// after `layout`.
-LayoutRect LayoutRectGetTrailingLayout(LayoutRect layout);
-
-// Return the trailing extent of `layout` (its leading plus its width).
-CGFloat LayoutRectGetTrailingEdge(LayoutRect layout);
-
-// A LayoutOffset is an x-offset specified in leading pixels.
-typedef CGFloat LayoutOffset;
-
-// Returns `point` with its x-value shifted `offset` pixels in the leading
-// direction according to `direction`
-CGPoint CGPointLayoutOffsetUsingDirection(CGPoint point,
-                                          LayoutOffset offset,
-                                          base::i18n::TextDirection direction);
-
-// As above, using `direction` == RIGHT_TO_LEFT if UseRTLLayout(), LEFT_TO_RIGHT
-// otherwise.
-CGPoint CGPointLayoutOffset(CGPoint point, LayoutOffset offset);
-
-// Returns `rect` with its x-origin shifted `offset` pixels in the leading
-// direction according to `direction`
-CGRect CGRectLayoutOffsetUsingDirection(CGRect rect,
-                                        LayoutOffset offset,
-                                        base::i18n::TextDirection direction);
-
-// As above, using `direction` == RIGHT_TO_LEFT if UseRTLLayout(), LEFT_TO_RIGHT
-// otherwise.
-CGRect CGRectLayoutOffset(CGRect rect, LayoutOffset offset);
-
-// Returns the leading offset of `rect` inside `boundingBox`, as a LayoutOffset.
-LayoutOffset CGRectGetLeadingLayoutOffsetInBoundingRect(CGRect rect,
-                                                        CGRect boundingRect);
-
-// Returns the trailing offset of `rect` inside `boundingBox`, as a
-// LayoutOffset. Note that this will be the distance from the trailing edge of
-// `rect` to the trailing edge of `boundingRect`.
-LayoutOffset CGRectGetTrailingLayoutOffsetInBoundingRect(CGRect rect,
-                                                         CGRect boundingRect);
-
-// Returns the leading content offset of `scrollView`.
-LayoutOffset LeadingContentOffsetForScrollView(UIScrollView* scrollView);
-
-// Utilities for mapping UIKit geometric structures to RTL-independent geometry.
-
-// Get leading and trailing edges of `rect`, assuming layout direction
-// `direction`.
-CGFloat CGRectGetLeadingEdgeUsingDirection(CGRect rect,
-                                           base::i18n::TextDirection direction);
-CGFloat CGRectGetTrailingEdgeUsingDirection(
-    CGRect rect,
-    base::i18n::TextDirection direction);
-
-// As above, with `direction` == LayoutDirection().
-CGFloat CGRectGetLeadingEdge(CGRect rect);
-CGFloat CGRectGetTrailingEdge(CGRect rect);
-
-// Leading/trailing autoresizing masks. 'Leading' is 'Left' under iOS <= 8 or
-// in an LTR language, 'Right' otherwise; 'Trailing' is the obverse.
-UIViewAutoresizing UIViewAutoresizingFlexibleLeadingMargin();
-UIViewAutoresizing UIViewAutoresizingFlexibleTrailingMargin();
-
-// Text-direction aware UIEdgeInsets constructor; just like UIEdgeInsetsMake(),
-// except `leading` and `trailing` map to left and right when `direction` is
-// LEFT_TO_RIGHT, and are swapped for RIGHT_TO_LEFT.
-UIEdgeInsets UIEdgeInsetsMakeUsingDirection(
-    CGFloat top,
-    CGFloat leading,
-    CGFloat bottom,
-    CGFloat trailing,
-    base::i18n::TextDirection direction);
-// As above, but uses LayoutDirection() for `direction`.
-UIEdgeInsets UIEdgeInsetsMakeDirected(CGFloat top,
-                                      CGFloat leading,
-                                      CGFloat bottom,
-                                      CGFloat trailing);
-
-// Inverses of the above functions: return the leading/trailing inset for
-// the current direction.
+// Inverses of UIEdgeInsetsMake: return the leading inset for the current
+// direction.
 CGFloat UIEdgeInsetsGetLeading(UIEdgeInsets insets);
-CGFloat UIEdgeInsetsGetTrailing(UIEdgeInsets insets);
 
 // Utilities for testing RTL-dependent relations.
 
@@ -202,8 +89,5 @@ BOOL EdgeLeadsEdge(CGFloat a, CGFloat b);
 
 // Determines the best alignment for the provided `text`.
 NSTextAlignment DetermineBestAlignmentForText(NSString* text);
-
-// Scrolls to the scroll view semantic leading.
-void ScrollToSemanticLeading(UIScrollView* scrollview, BOOL animated);
 
 #endif  // IOS_CHROME_BROWSER_SHARED_UI_UTIL_RTL_GEOMETRY_H_
--- ios/chrome/browser/shared/ui/util/rtl_geometry.mm
+++ ios/chrome/browser/shared/ui/util/rtl_geometry.mm
@@ -6,10 +6,7 @@
 
 #import <UIKit/UIKit.h>
 
-#import <limits>
-
 #import "base/check_op.h"
-#import "ios/chrome/common/ui/util/ui_util.h"
 
 bool UseRTLLayout() {
   return base::i18n::IsRTL();
@@ -21,22 +18,8 @@ base::i18n::TextDirection LayoutDirection() {
 
 #pragma mark - LayoutRects.
 
-constexpr const LayoutRectPosition LayoutRectPositionZero = {0.0, 0.0};
-
 LayoutRectPosition LayoutRectPositionMake(CGFloat leading, CGFloat originY) {
   return {leading, originY};
-}
-
-BOOL LayoutRectPositionEqualToPosition(LayoutRectPosition a,
-                                       LayoutRectPosition b) {
-  CGFloat epsilon = std::numeric_limits<CGFloat>::epsilon();
-  return fabs(a.leading - b.leading) <= epsilon &&
-         fabs(a.originY - b.originY) <= epsilon;
-}
-
-LayoutRectPosition AlignLayoutRectPositionToPixel(LayoutRectPosition position) {
-  return LayoutRectPositionMake(AlignValueToLowerPixel(position.leading),
-                                AlignValueToLowerPixel(position.originY));
 }
 
 const LayoutRect LayoutRectZero = {0.0, {0.0, 0.0}, {0.0, 0.0}};
@@ -48,13 +31,6 @@ LayoutRect LayoutRectMake(CGFloat leading,
                           CGFloat height) {
   return {boundingWidth, LayoutRectPositionMake(leading, originY),
           CGSizeMake(width, height)};
-}
-
-BOOL LayoutRectEqualToRect(LayoutRect a, LayoutRect b) {
-  CGFloat epsilon = std::numeric_limits<CGFloat>::epsilon();
-  return fabs(a.boundingWidth - b.boundingWidth) <= epsilon &&
-         LayoutRectPositionEqualToPosition(a.position, b.position) &&
-         CGSizeEqualToSize(a.size, b.size);
 }
 
 CGRect LayoutRectGetRectUsingDirection(LayoutRect layout,
@@ -75,24 +51,6 @@ CGRect LayoutRectGetRectUsingDirection(LayoutRect layout,
 
 CGRect LayoutRectGetRect(LayoutRect layout) {
   return LayoutRectGetRectUsingDirection(layout, LayoutDirection());
-}
-
-CGRect LayoutRectGetBoundsRect(LayoutRect layout) {
-  return CGRectMake(0, 0, layout.size.width, layout.size.height);
-}
-
-CGPoint LayoutRectGetPositionForAnchorUsingDirection(
-    LayoutRect layout,
-    CGPoint anchor,
-    base::i18n::TextDirection direction) {
-  CGRect rect = LayoutRectGetRectUsingDirection(layout, direction);
-  return CGPointMake(CGRectGetMinX(rect) + (rect.size.width * anchor.x),
-                     CGRectGetMinY(rect) + (rect.size.height * anchor.y));
-}
-
-CGPoint LayoutRectGetPositionForAnchor(LayoutRect layout, CGPoint anchor) {
-  return LayoutRectGetPositionForAnchorUsingDirection(layout, anchor,
-                                                      LayoutDirection());
 }
 
 LayoutRect LayoutRectForRectInBoundingRectUsingDirection(
@@ -116,159 +74,10 @@ LayoutRect LayoutRectForRectInBoundingRect(CGRect rect, CGRect contextRect) {
                                                        LayoutDirection());
 }
 
-LayoutRect LayoutRectGetLeadingLayout(LayoutRect layout) {
-  LayoutRect leadingLayout;
-  leadingLayout.position.leading = 0;
-  leadingLayout.boundingWidth = layout.boundingWidth;
-  leadingLayout.position.originY = layout.position.originY;
-  leadingLayout.size = CGSizeMake(layout.position.leading, layout.size.height);
-  return leadingLayout;
-}
-
-LayoutRect LayoutRectGetTrailingLayout(LayoutRect layout) {
-  LayoutRect leadingLayout;
-  CGFloat trailing = LayoutRectGetTrailingEdge(layout);
-  leadingLayout.position.leading = trailing;
-  leadingLayout.boundingWidth = layout.boundingWidth;
-  leadingLayout.position.originY = layout.position.originY;
-  leadingLayout.size =
-      CGSizeMake((layout.boundingWidth - trailing), layout.size.height);
-  return leadingLayout;
-}
-
-CGFloat LayoutRectGetTrailingEdge(LayoutRect layout) {
-  return layout.position.leading + layout.size.width;
-}
-
-CGPoint CGPointLayoutOffsetUsingDirection(CGPoint point,
-                                          LayoutOffset offset,
-                                          base::i18n::TextDirection direction) {
-  CGPoint newPoint = point;
-  if (direction == base::i18n::RIGHT_TO_LEFT) {
-    offset = -offset;
-  }
-  newPoint.x += offset;
-  return newPoint;
-}
-
-CGPoint CGPointLayoutOffset(CGPoint point, LayoutOffset offset) {
-  return CGPointLayoutOffsetUsingDirection(point, offset, LayoutDirection());
-}
-
-CGRect CGRectLayoutOffsetUsingDirection(CGRect rect,
-                                        LayoutOffset offset,
-                                        base::i18n::TextDirection direction) {
-  if (direction == base::i18n::RIGHT_TO_LEFT) {
-    offset = -offset;
-  }
-  return CGRectOffset(rect, offset, 0);
-}
-
-CGRect CGRectLayoutOffset(CGRect rect, LayoutOffset offset) {
-  return CGRectLayoutOffsetUsingDirection(rect, offset, LayoutDirection());
-}
-
-LayoutOffset CGRectGetLeadingLayoutOffsetInBoundingRect(CGRect rect,
-                                                        CGRect boundingRect) {
-  CGFloat rectLeadingEdge = CGRectGetLeadingEdge(rect);
-  CGFloat boundingRectLeadingEdge = CGRectGetLeadingEdge(boundingRect);
-  LayoutOffset offset = 0;
-  if (LayoutDirection() == base::i18n::LEFT_TO_RIGHT) {
-    // Leading edges have low x-values for LTR, so subtract the bounding rect's
-    // from `rect`'s.
-    offset = rectLeadingEdge - boundingRectLeadingEdge;
-  } else {
-    DCHECK_EQ(LayoutDirection(), base::i18n::RIGHT_TO_LEFT);
-    offset = boundingRectLeadingEdge - rectLeadingEdge;
-  }
-  return offset;
-}
-
-LayoutOffset CGRectGetTrailingLayoutOffsetInBoundingRect(CGRect rect,
-                                                         CGRect boundingRect) {
-  CGFloat rectTrailingEdge = CGRectGetTrailingEdge(rect);
-  CGFloat boundingRectTrailingEdge = CGRectGetTrailingEdge(boundingRect);
-  LayoutOffset offset = 0;
-  if (LayoutDirection() == base::i18n::RIGHT_TO_LEFT) {
-    // Trailing edges have low x-values for RTL, so subtract the bounding rect's
-    // from `rect`'s.
-    offset = rectTrailingEdge - boundingRectTrailingEdge;
-  } else {
-    DCHECK_EQ(LayoutDirection(), base::i18n::LEFT_TO_RIGHT);
-    offset = boundingRectTrailingEdge - rectTrailingEdge;
-  }
-  return offset;
-}
-
-LayoutOffset LeadingContentOffsetForScrollView(UIScrollView* scrollView) {
-  return UseRTLLayout()
-             ? scrollView.contentSize.width - scrollView.contentOffset.x -
-                   CGRectGetWidth(scrollView.bounds)
-             : scrollView.contentOffset.x;
-}
-
 #pragma mark - UIKit utilities
-
-CGFloat CGRectGetLeadingEdgeUsingDirection(
-    CGRect rect,
-    base::i18n::TextDirection direction) {
-  return direction == base::i18n::RIGHT_TO_LEFT ? CGRectGetMaxX(rect)
-                                                : CGRectGetMinX(rect);
-}
-
-CGFloat CGRectGetTrailingEdgeUsingDirection(
-    CGRect rect,
-    base::i18n::TextDirection direction) {
-  return direction == base::i18n::RIGHT_TO_LEFT ? CGRectGetMinX(rect)
-                                                : CGRectGetMaxX(rect);
-}
-
-CGFloat CGRectGetLeadingEdge(CGRect rect) {
-  return CGRectGetLeadingEdgeUsingDirection(rect, LayoutDirection());
-}
-
-CGFloat CGRectGetTrailingEdge(CGRect rect) {
-  return CGRectGetTrailingEdgeUsingDirection(rect, LayoutDirection());
-}
-
-UIViewAutoresizing UIViewAutoresizingFlexibleLeadingMargin() {
-  return base::i18n::IsRTL() ? UIViewAutoresizingFlexibleRightMargin
-                             : UIViewAutoresizingFlexibleLeftMargin;
-}
-
-UIViewAutoresizing UIViewAutoresizingFlexibleTrailingMargin() {
-  return base::i18n::IsRTL() ? UIViewAutoresizingFlexibleLeftMargin
-                             : UIViewAutoresizingFlexibleRightMargin;
-}
-
-UIEdgeInsets UIEdgeInsetsMakeUsingDirection(
-    CGFloat top,
-    CGFloat leading,
-    CGFloat bottom,
-    CGFloat trailing,
-    base::i18n::TextDirection direction) {
-  if (direction == base::i18n::RIGHT_TO_LEFT) {
-    using std::swap;
-    swap(leading, trailing);
-  }
-  // At this point, `leading` == left, `trailing` = right.
-  return UIEdgeInsetsMake(top, leading, bottom, trailing);
-}
-
-UIEdgeInsets UIEdgeInsetsMakeDirected(CGFloat top,
-                                      CGFloat leading,
-                                      CGFloat bottom,
-                                      CGFloat trailing) {
-  return UIEdgeInsetsMakeUsingDirection(top, leading, bottom, trailing,
-                                        LayoutDirection());
-}
 
 CGFloat UIEdgeInsetsGetLeading(UIEdgeInsets insets) {
   return UseRTLLayout() ? insets.right : insets.left;
-}
-
-CGFloat UIEdgeInsetsGetTrailing(UIEdgeInsets insets) {
-  return UseRTLLayout() ? insets.left : insets.right;
 }
 
 BOOL EdgeLeadsEdge(CGFloat a, CGFloat b, base::i18n::TextDirection direction) {
@@ -290,18 +99,4 @@ NSTextAlignment DetermineBestAlignmentForText(NSString* text) {
     }
   }
   return NSTextAlignmentLeft;
-}
-
-void ScrollToSemanticLeading(UIScrollView* scrollview, BOOL animated) {
-  BOOL isRTL = [UIView userInterfaceLayoutDirectionForSemanticContentAttribute:
-                           scrollview.semanticContentAttribute] ==
-               UIUserInterfaceLayoutDirectionRightToLeft;
-
-  CGFloat contentStartX = 0;
-
-  if (isRTL) {
-    contentStartX = MAX(scrollview.contentSize.width - 1, 0);
-  }
-
-  [scrollview setContentOffset:CGPointMake(contentStartX, 0) animated:animated];
 }
