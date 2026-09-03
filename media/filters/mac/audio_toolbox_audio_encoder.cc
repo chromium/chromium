8e70b59717caf5b1105f4d53da48c6998b8875c5
Shuhei Takahashi <nya@chromium.org>
2026-09-02 19:35:20 -0700

    Revert "Fix AudioToolboxAudioEncoder crash on multiple callback invocations"
    This reverts commit 93e2da9f1016ab0c2d30dbb5e477be3a0563f6aa.
    
    Reason for revert: Suspected to break AudioEncodersTest on Android builders (crbug.com/556445310)
    
    Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/android-15-x64-rel/b8671724402977105185/overview
    
    Original change's description:
    > Fix AudioToolboxAudioEncoder crash on multiple callback invocations
    >
    > AudioConverterFillComplexBuffer() can invoke ProvideInputCallback()
    > multiple times in a single conversion call (e.g. due to encoder priming
    > or lookahead buffering).
    >
    > Previously, ProvideInputCallback() had a CHECK(input_data->bus) assuming
    > it would only be called once per AudioConverterFillComplexBuffer() call.
    > When the callback was invoked a second time in the same cycle, `bus` was
    > null, triggering a crash.
    >
    > Returning 0 packets with `noErr` would be interpreted by CoreAudio as an
    > End-Of-Stream (EOS) signal, causing the converter to transition to an
    > EOF state prematurely.
    >
    > This CL fixes the issue by returning a custom `kNoMoreDataError`
    > OSStatus when `bus` is null (matching the pattern used in
    > AudioToolboxAudioDecoder), preserving converter state across calls.
    > DoEncode() is updated to treat `kNoMoreDataError` as non-fatal when no
    > packets are produced.
    >
    > Fixed: 549893457
    > Change-Id: Ic0b4217102b5aec80970f22d94b231f843b07f18
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8332748
    > Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1691169}
    
    Bug: 549893457
    Bug: b/556445310
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Iddec8f3476a56f404a574c2eb29c1cadce7cdd96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8349383
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Shuhei Takahashi <nya@chromium.org>
    Owners-Override: Shuhei Takahashi <nya@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1691283}

--- media/filters/mac/audio_toolbox_audio_encoder.cc
+++ media/filters/mac/audio_toolbox_audio_encoder.cc
@@ -27,9 +27,6 @@ struct InputData {
   bool flushing = false;
 };
 
-// Special error code we use to differentiate real errors from end of buffer.
-constexpr OSStatus kNoMoreDataError = -12345;
-
 constexpr int kAacFramesPerBuffer = 1024;
 
 // Callback used to provide input data to the AudioConverter.
@@ -44,11 +41,7 @@ OSStatus ProvideInputCallback(AudioConverterRef decoder,
     return noErr;
   }
 
-  if (!input_data->bus) {
-    *num_packets = 0;
-    return kNoMoreDataError;
-  }
-
+  CHECK(input_data->bus);
   DCHECK_EQ(input_data->bus->frames(), kAacFramesPerBuffer);
 
   const AudioBus* bus = input_data->bus;
@@ -76,10 +69,8 @@ OSStatus ProvideInputCallback(AudioConverterRef decoder,
   // nFramesPerPacket is 1 for the input stream.
   *num_packets = bus->frames();
 
-  // This callback might be called more than once per
-  // AudioConverterFillComplexBuffer call (e.g. for encoder priming/lookahead).
-  // Nulling out `bus` ensures that subsequent calls in the same conversion
-  // cycle return kNoMoreDataError.
+  // This callback should never be called more than once. Otherwise, we will
+  // run into the CHECK above.
   input_data->bus = nullptr;
   return noErr;
 }
@@ -409,15 +400,18 @@ void AudioToolboxAudioEncoder::DoEncode(const AudioBus* input_bus) {
         encoder_, ProvideInputCallback, &input_data, &num_packets,
         &output_buffer_list, &packet_description);
 
-    if (result != noErr && result != kNoMoreDataError) {
+    // We expect "1 in, 1 out" when feeding packets into the encoder, except
+    // when flushing.
+    if (result == noErr && !num_packets) {
+      DCHECK(is_flushing);
+      return;
+    }
+
+    if (result != noErr) {
       OSSTATUS_DLOG(ERROR, result)
           << "AudioConverterFillComplexBuffer() failed";
       std::move(current_done_cb_)
           .Run(EncoderStatus::Codes::kEncoderFailedEncode);
-      return;
-    }
-
-    if (!num_packets) {
       return;
     }
 
