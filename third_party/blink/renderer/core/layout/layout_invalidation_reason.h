d3790688e00f97baf26f0bb88806f4454be443bb
Ian Kilpatrick <ikilpatrick@chromium.org>
2026-09-04 16:27:07 -0700

    [cleanup] Remove unused layout_invalidation_reason(s).
    There should be on behaviour change.
    
    Change-Id: Ib38dad506f60f58c82462e8665a5d9e2e977346e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8358623
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: David Grogan <dgrogan@chromium.org>
    Commit-Queue: David Grogan <dgrogan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1692836}

--- third_party/blink/renderer/core/layout/layout_invalidation_reason.h
+++ third_party/blink/renderer/core/layout/layout_invalidation_reason.h
@@ -12,35 +12,26 @@ namespace blink {
 namespace layout_invalidation_reason {
 extern const char kUnknown[];
 extern const char kSizeChanged[];
-extern const char kAncestorMoved[];
 extern const char kStyleChange[];
 extern const char kDomChanged[];
 extern const char kTextChanged[];
 extern const char kPrintingChanged[];
 extern const char kPaintPreview[];
 extern const char kAttributeChanged[];
-extern const char kColumnsChanged[];
 extern const char kChildAnonymousBlockChanged[];
 extern const char kAnonymousBlockChange[];
 extern const char kFontsChanged[];
-extern const char kFullscreen[];
 extern const char kChildChanged[];
 extern const char kListValueChange[];
 extern const char kListStyleTypeChange[];
 extern const char kCounterStyleChange[];
-extern const char kImageChanged[];
 extern const char kSliderValueChanged[];
-extern const char kAncestorMarginCollapsing[];
-extern const char kFieldsetChanged[];
 extern const char kSvgResourceInvalidated[];
-extern const char kFloatDescendantChanged[];
-extern const char kCountersChanged[];
 extern const char kGridChanged[];
 extern const char kMenuOptionsChanged[];
 extern const char kRemovedFromLayout[];
 extern const char kAddedToLayout[];
 extern const char kTableChanged[];
-extern const char kPaddingChanged[];
 extern const char kTextControlChanged[];
 // FIXME: This is too generic, we should be able to split out transform and
 // size related invalidations.
