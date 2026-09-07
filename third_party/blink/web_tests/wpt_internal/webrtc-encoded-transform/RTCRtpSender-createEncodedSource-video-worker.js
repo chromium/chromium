c5202f87593e486d024432d7c5ef272f18865710
Leonardo Evi <evil@chromium.org>
2026-09-07 02:22:31 -0700

    Relax captureTime test tolerance to avoid flakyeness.
    Bug: 532190488
    Change-Id: I4da58517644581ecd7dbbcab2ffd424ab283c58a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8366404
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Leonardo Evi <evil@chromium.org>
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1693219}

--- third_party/blink/web_tests/wpt_internal/webrtc-encoded-transform/RTCRtpSender-createEncodedSource-video-worker.js
+++ third_party/blink/web_tests/wpt_internal/webrtc-encoded-transform/RTCRtpSender-createEncodedSource-video-worker.js
@@ -76,7 +76,7 @@ self.onrtcsenderencodedsource = async (event) => {
     }
   }
 
-  senderInterval = setInterval(sendFrame, 500);
+  senderInterval = setInterval(sendFrame, 100);
 };
 
 self.onrtctransform = async (event) => {
@@ -109,10 +109,10 @@ self.onrtctransform = async (event) => {
       receivedBytes: Array.from(receivedBytes),
       sentBytes: Array.from(vp8_FrameData),
       contributingSources: metadata.contributingSources,
-      // Use a 3ms tolerance to account for differences due to possible changes
+      // Use a 20ms tolerance to account for differences due to possible changes
       // during transmission
       correctCaptureTime: metadata.captureTime !== undefined &&
-          Math.abs(time - metadata.captureTime) <= 3,
+          Math.abs(time - metadata.captureTime) <= 20,
     });
   } catch (e) {
     self.postMessage({error: 'Receiver error: ' + e.message});
