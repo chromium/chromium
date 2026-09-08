281cfae37dcc49cad1377888af15856bf0dbc338
Chrome Crash (Prod) <crash@system.gserviceaccount.com>
2026-09-08 15:02:33 -0700

    [M154] Convert CHECK to DCHECK in CacheStorageCache::PutDidWriteHeaders
    Original change's description:
    > Convert CHECK to DCHECK in CacheStorageCache::PutDidWriteHeaders
    >
    > The CHECK macro in PutDidWriteHeaders was introduced during a bulk
    > conversion of DCHECKs to CHECKs and can be triggered in certain
    > conditions. Revert it back to a DCHECK with a CHECK-exclusion comment
    > until we are confident it will not be triggered.
    >
    > This fix was generated by the Crash Autofix pipeline for the crash
    > details.
    >
    > http: //crash/923921a270400000. Please see go/crash-autofix for more
    > Bug: 558086375
    > Change-Id: Ia4229fdc83cfe7510bf26c71f559038370da0218
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8357916
    > Reviewed-by: Keishi Hattori <keishi@chromium.org>
    > Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    > Owners-Override: Stephen Nusko <nuskos@chromium.org>
    > Commit-Queue: Keishi Hattori <keishi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1693170}
    
    (cherry picked from commit c9a48517bb812ceeb6e69b9b04d4b656835a605f)
    
    Bug: 558105099,558086375
    Change-Id: Ia4229fdc83cfe7510bf26c71f559038370da0218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8360156
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8037@{#583}
    Cr-Branched-From: e10b20e60f162e7ad88e4930833695a54f4e15fb-refs/heads/main@{#1689415}

--- content/browser/cache_storage/cache_storage_cache.cc
+++ content/browser/cache_storage/cache_storage_cache.cc
@@ -1988,9 +1988,10 @@ void CacheStorageCache::PutDidWriteHeaders(
     return;
   }
 
-  CHECK(!ShouldPadResourceSize(*put_context->response) ||
-            (padding + side_data_padding),
-        base::NotFatalUntil::M158);
+  // TODO(crbug.com/558086387): CHECK-exclusion: Convert to a CHECK once we are
+  // confident it won't be triggered.
+  DCHECK(!ShouldPadResourceSize(*put_context->response) ||
+         (padding + side_data_padding));
   cache_padding_ += padding + side_data_padding;
 
   PutWriteBlobToCache(std::move(put_context), INDEX_RESPONSE_BODY);
