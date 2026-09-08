1d4f136b87167d8c9c966461c5eaa7280997edae
Thomas Anderson <thomasanderson@chromium.org>
2026-09-08 13:57:50 -0700

    Revert "[M152] [ui/gtk] Pin gtk-modules to an empty string during initialization"
    This reverts commit 24ab38162901165ccea2b047ef53c72c325c900b.
    
    Reason for revert: crbug.com/551107198#comment10
    
    Original change's description:
    > [M152] [ui/gtk] Pin gtk-modules to an empty string during initialization
    >
    > Original change's description:
    > > [ui/gtk] Pin gtk-modules to an empty string during initialization
    > >
    > > Setting `gtk-modules` to `""` in GtkUi::Initialize() assigns
    > > GTK_SETTINGS_SOURCE_APPLICATION priority to the setting, preventing
    > > lower-priority XSETTINGS updates from overriding `gtk-modules` and
    > > executing untrusted GTK modules in the unsandboxed browser process via a
    > > retained X11 connection.
    > >
    > > Fixed: 545124048
    > > Change-Id: I91e1c3df94c0567c69eec676da26ee1bb46eca2f
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8243490
    > > Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    > > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1678319}
    >
    > (cherry picked from commit 9ec61d5e14fff874426e560d593f93094c4377b7)
    >
    > Bug: 546660694,545124048
    > Change-Id: I91e1c3df94c0567c69eec676da26ee1bb46eca2f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8258320
    > Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    > Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/branch-heads/7977@{#1752}
    > Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}
    
    Bug: 546660694,545124048,551107198
    Fixed: 553628981
    Change-Id: I61e554d2044cfde5bd5af6a5b16836b08042d3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8305154
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/branch-heads/7977@{#2617}
    Cr-Branched-From: b7fe14017379ddffae396d944fb8b59a5896c261-refs/heads/main@{#1669021}

--- ui/gtk/gtk_ui.cc
+++ ui/gtk/gtk_ui.cc
@@ -366,9 +366,6 @@ bool GtkUi::Initialize() {
   };
 
   GtkSettings* settings = gtk_settings_get_default();
-  // Pin `gtk-modules` to an empty string with APPLICATION source priority
-  // to prevent XSETTINGS updates from loading GTK modules.
-  g_object_set(settings, "gtk-modules", "", nullptr);
   SanitizeIconThemeName();
   SanitizeThemeName();
   SanitizeCursorThemeName();
