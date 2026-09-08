8d26e38735c01685fb59e3dcb75909b046370f92
Your Name <przybytniowska@google.com>
2026-09-08 01:13:28 -0700

    [Isolated Mode] Generalize OTR profile destruction flow
    Generalize BrowserManagerService to support Enterprise Isolated Mode by
    using IsPrimaryOTRProfileWithRegularParent() instead of
    IsIncognitoProfile(). This ensures Isolated Mode profiles are correctly
    destroyed when the last browser window is closed.
    
    BUG=b:556635895
    
    DD: go/chrome-enterprise-isolated-mode-client-dd
    Change-Id: Ia9a7417a2fb82158d94217673a5c05ea17265964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8348046
    Reviewed-by: Viacheslav Krakhotin <krakhotin@google.com>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Julia Przybytniowska <przybytniowska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1693616}

--- chrome/browser/ui/browser_manager_service.cc
+++ chrome/browser/ui/browser_manager_service.cc
@@ -142,18 +142,21 @@ void BrowserManagerService::DeleteBrowser(
   // TODO(crbug.com/40159237): Use ScopedProfileKeepAlive for Incognito too,
   // instead of separate logic for Incognito and regular profiles.
   target_browser_and_subscriptions->browser.reset();
-  if (browsers_and_subscriptions_.empty() && profile_->IsIncognitoProfile() &&
+  if (browsers_and_subscriptions_.empty() &&
+      profile_->IsPrimaryOTRProfileWithRegularParent() &&
       !profile_->IsSystemProfile()) {
 #if BUILDFLAG(ENABLE_PRINT_PREVIEW)
     // The Printing Background Manager holds onto preview dialog WebContents
     // whose corresponding print jobs have not yet fully spooled. Make sure
-    // these get destroyed before tearing down the incognito profile so that
-    // their RenderFrameHosts can exit in time - see crbug.com/41235373
+    // these get destroyed before tearing down the incognito or isolated mode
+    // profile so that their RenderFrameHosts can exit in time - see
+    // crbug.com/41235373
     g_browser_process->background_printing_manager()
         ->DeletePreviewContentsForBrowserContext(&profile_.get());
 #endif
-    // An incognito profile is no longer needed, this indirectly frees
-    // its cache and cookies once it gets destroyed at the appropriate time.
+    // The incognito or isolated mode profile is no longer needed, this
+    // indirectly frees its cache and cookies once it gets destroyed at the
+    // appropriate time.
     ProfileDestroyer::DestroyOTRProfileWhenAppropriate(&profile_.get());
   }
 
