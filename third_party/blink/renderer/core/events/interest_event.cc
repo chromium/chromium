b8cb6a887434c8eda8e7998bcf5b7802a7d61b07
Mason Freed <masonf@chromium.org>
2026-09-08 15:47:33 -0700

    Standardize InterestEvent::source() to use Event::Retarget
    CommandEvent and ToggleEvent use Event::Retarget(source_).
    InterestEvent previously used manual tree-scope lookup. This CL
    standardizes InterestEvent::source() to match.
    
    No behavior change.
    
    Change-Id: Ifd52b87e14392b43967d33b260fc96862748855f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8358471
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Dan Clark <daniec@microsoft.com>
    Commit-Queue: Dan Clark <daniec@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1694137}

--- third_party/blink/renderer/core/events/interest_event.cc
+++ third_party/blink/renderer/core/events/interest_event.cc
@@ -48,12 +48,7 @@ Element* InterestEvent::source() const {
                           : nullptr;
   }
 
-  if (auto* current = currentTarget()) {
-    CHECK(current->ToNode());
-    return &current->ToNode()->GetTreeScope().Retarget(*source_);
-  }
-  DCHECK_EQ(eventPhase(), Event::PhaseType::kNone);
-  return source_;
+  return DynamicTo<Element>(Retarget(source_));
 }
 
 DispatchEventResult InterestEvent::DispatchEvent(EventDispatcher& dispatcher) {
