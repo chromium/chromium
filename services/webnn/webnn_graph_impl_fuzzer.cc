ca4c4b7567fcd643c81f6bcf7e09e8491a3b5026
Phillis Tang <phillis@chromium.org>
2026-09-08 13:42:53 -0700

    webnn: change fuzzer backend log level
    LOG(INFO) is suppressed in the fuzzer
    build, bump to LOG(ERROR).
    
    Change-Id: I05ba19ee3f91384a9248caef65c6c77898d49379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8374161
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Lynne Jiang <lyjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1694028}

--- services/webnn/webnn_graph_impl_fuzzer.cc
+++ services/webnn/webnn_graph_impl_fuzzer.cc
@@ -3191,7 +3191,7 @@ void WebNNGraphImplFuzzerBase::SetUp() {
       for (const auto backend_name : GetGlobalFuzzEnvironment()
                                          .GetWebNNTestEnvironment()
                                          .GetContextBackendNames()) {
-        LOG(INFO) << "[WebNN Fuzzer] Created WebNN context with backend: "
+        LOG(ERROR) << "[WebNN Fuzzer] Created WebNN context with backend: "
                   << backend_name;
       }
     }
