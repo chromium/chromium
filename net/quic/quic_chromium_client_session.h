45cc880f47484128bbc3b46fd2ab385efa225d90
Stefano Duo <stefanoduo@google.com>
2026-09-08 06:15:46 -0700

    Reland "Introduce QuicMigrationAttemptContext"
    This reverts commit f5d390d2d0af94562d034bdcfe6aa533e52dd57b.
    
    Reason for revert: Fixed by passing a non-infinite time for yielding
    when constructing a QuicChromiumPacketReader in
    https://crrev.com/c/8370023.
    
    Original change's description:
    > Revert "Introduce QuicMigrationAttemptContext"
    >
    > This reverts commit 55696d0a887ce57e79e23c549da9367baa7aed54.
    >
    > Reason for revert: VersionIncludeStreamDependencySequence/QuicSessionPoolTest.TestCloseSessionOnErrorLaterThenConnectionMigrationMigrate/*
    >
    > Failure Link: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20UBSan%20Tests/17829/blamelist
    >
    > Original change's description:
    > > Introduce QuicMigrationAttemptContext
    > >
    > > This new class encapsulates all context necessary to perform a migration
    > > attempt. This is going to be used both for probing and non-probing
    > > migration. For the time being, probing migration will continue having an
    > > additional QuicChromiumPathValidationContext.
    > >
    > > This is the basis for the new QUIC connection migration UMAs. We attempt
    > > to make it harder to wrongly log migration attempts. This will be
    > > achieved by keeping QuicMigrationAttemptContext a const-like object and
    > > logging in its destructor. Follow-up changes will make it impossible to
    > > perform a connection migration without consuming a
    > > QuicMigrationAttemptContext.
    > >
    > > Bug: 557126867, b:450253948
    > > Change-Id: Ibbbb3ea143ab3f38a150c7a9ba3399497c335362
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8346229
    > > Reviewed-by: Mohannad Farrag <aymanm@google.com>
    > > Auto-Submit: Stefano Duo <stefanoduo@google.com>
    > > Commit-Queue: Stefano Duo <stefanoduo@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1693328}
    >
    > Bug: 557126867, b:450253948
    > Change-Id: Ifd2868a74883ca1dc67d6880b214e758b485a811
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8366247
    > Owners-Override: Fergal Daly <fergal@google.com>
    > Commit-Queue: Fergal Daly <fergal@chromium.org>
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1693611}
    
    Bug: 558456601, 557126867, b:450253948
    Change-Id: Iae1fe74bb2ecf88550ecf379db81fff18635307f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8366566
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1693733}

--- net/quic/quic_chromium_client_session.h
+++ net/quic/quic_chromium_client_session.h
@@ -46,6 +46,7 @@
 #include "net/quic/quic_connection_logger.h"
 #include "net/quic/quic_crypto_client_config_handle.h"
 #include "net/quic/quic_http3_logger.h"
+#include "net/quic/quic_migration_attempt_context.h"
 #include "net/quic/quic_session_alias_key.h"
 #include "net/quic/quic_session_key.h"
 #include "net/socket/socket_performance_watcher.h"
@@ -103,21 +104,6 @@ enum class ConnectionMigrationMode {
   FULL_MIGRATION_V1,
   NO_MIGRATION_ON_PATH_DEGRADING_V2,
   FULL_MIGRATION_V2
-};
-
-// Cause of a migration.
-enum MigrationCause {
-  UNKNOWN_CAUSE,
-  ON_NETWORK_CONNECTED,                       // No probing.
-  ON_NETWORK_DISCONNECTED,                    // No probing.
-  ON_WRITE_ERROR,                             // No probing.
-  ON_NETWORK_MADE_DEFAULT,                    // With probing.
-  ON_MIGRATE_BACK_TO_DEFAULT_NETWORK,         // With probing.
-  CHANGE_NETWORK_ON_PATH_DEGRADING,           // With probing.
-  CHANGE_PORT_ON_PATH_DEGRADING,              // With probing.
-  NEW_NETWORK_CONNECTED_POST_PATH_DEGRADING,  // With probing.
-  ON_SERVER_PREFERRED_ADDRESS_AVAILABLE,      // With probing.
-  MIGRATION_CAUSE_MAX
 };
 
 // Result of connection migration.
@@ -510,23 +496,21 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
    public:
     QuicChromiumPathValidationContext(
         const quic::QuicSocketAddress& self_address,
-        const quic::QuicSocketAddress& peer_address,
-        handles::NetworkHandle network,
-        std::unique_ptr<QuicChromiumPacketWriter> writer,
-        std::unique_ptr<QuicChromiumPacketReader> reader);
+        std::unique_ptr<QuicMigrationAttemptContext> migration_context);
     ~QuicChromiumPathValidationContext() override;
 
-    handles::NetworkHandle network();
+    handles::NetworkHandle network() const;
     quic::QuicPacketWriter* WriterToUse() override;
 
-    // Transfer the ownership from |this| to the caller.
-    std::unique_ptr<QuicChromiumPacketWriter> ReleaseWriter();
-    std::unique_ptr<QuicChromiumPacketReader> ReleaseReader();
+    // Consumes `context` and returns the underlying migration context.
+    static std::unique_ptr<QuicMigrationAttemptContext> ReleaseMigrationContext(
+        std::unique_ptr<quic::QuicPathValidationContext> context);
+    QuicMigrationAttemptContext* migration_context() const {
+      return migration_context_.get();
+    }
 
    private:
-    handles::NetworkHandle network_handle_;
-    std::unique_ptr<QuicChromiumPacketReader> reader_;
-    std::unique_ptr<QuicChromiumPacketWriter> writer_;
+    std::unique_ptr<QuicMigrationAttemptContext> migration_context_;
   };
 
   // This class implements Chrome logic for path validation events associated
@@ -749,25 +733,16 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
   void OnWriteUnblocked() override;
 
   void OnConnectionMigrationProbeSucceeded(
-      handles::NetworkHandle network,
-      const quic::QuicSocketAddress& peer_address,
       const quic::QuicSocketAddress& self_address,
-      std::unique_ptr<QuicChromiumPacketWriter> writer,
-      std::unique_ptr<QuicChromiumPacketReader> reader);
+      std::unique_ptr<QuicMigrationAttemptContext> migration_context);
 
   void OnPortMigrationProbeSucceeded(
-      handles::NetworkHandle network,
-      const quic::QuicSocketAddress& peer_address,
       const quic::QuicSocketAddress& self_address,
-      std::unique_ptr<QuicChromiumPacketWriter> writer,
-      std::unique_ptr<QuicChromiumPacketReader> reader);
+      std::unique_ptr<QuicMigrationAttemptContext> migration_context);
 
   void OnServerPreferredAddressProbeSucceeded(
-      handles::NetworkHandle network,
-      const quic::QuicSocketAddress& peer_address,
       const quic::QuicSocketAddress& self_address,
-      std::unique_ptr<QuicChromiumPacketWriter> writer,
-      std::unique_ptr<QuicChromiumPacketReader> reader);
+      std::unique_ptr<QuicMigrationAttemptContext> migration_context);
 
   void OnProbeFailed(handles::NetworkHandle network,
                      const quic::QuicSocketAddress& peer_address);
@@ -843,7 +818,7 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
   // ConnectAndConfigureSocket and uses it to create the multiport path context.
   void FinishCreateContextForMultiPortPath(
       std::unique_ptr<quic::MultiPortPathContextObserver> context_observer,
-      std::unique_ptr<DatagramClientSocket> probing_socket,
+      std::unique_ptr<QuicMigrationAttemptContext> context,
       int rv);
 
   // Performs a crypto handshake with the server.
@@ -1069,12 +1044,8 @@ class NET_EXPORT_PRIVATE QuicChromiumClientSession
 
   // Helper to finish network probe once socket has been opened. Always called
   // asynchronously.
-  // TODO(crbug.com/518753285): Stop accepting a `network` parameter. Instead,
-  // require `probing_socket` to have already been bound at creation time.
   void FinishStartProbing(ProbingCallback probing_callback,
-                          std::unique_ptr<DatagramClientSocket> probing_socket,
-                          handles::NetworkHandle network,
-                          const quic::QuicSocketAddress& peer_address,
+                          std::unique_ptr<QuicMigrationAttemptContext> context,
                           int rv);
 
   // Perform a few checks before StartProbing. If any of those checks fails,
