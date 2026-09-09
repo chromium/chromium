2ecaa52c89242ace5c23c94fb31930327619aeea
Janek Zarzycki <janzarzycki@google.com>
2026-09-09 08:37:21 -0700

    [Isolated mode] Support Isolated Mode in ChromePageInfoDelegate
    Change profile->IsIncognitoProfile() to
    profile->IsPrimaryOTRProfileWithRegularParent() in
    ChromePageInfoDelegate. This ensures Enterprise Isolated Mode is treated
    the same way as Incognito.
    
    Bug: b:559087020
    Change-Id: I7332fc4919db3483cd0cf790acd22825c464a253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8377146
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Janek Zarzycki <janzarzycki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1694617}

--- chrome/browser/ui/page_info/chrome_page_info_delegate.cc
+++ chrome/browser/ui/page_info/chrome_page_info_delegate.cc
@@ -314,7 +314,7 @@ ChromePageInfoDelegate::CreateCookieControlsController() {
           ? CookieSettingsFactory::GetForProfile(profile->GetOriginalProfile())
           : nullptr,
       HostContentSettingsMapFactory::GetForProfile(profile),
-      profile->IsIncognitoProfile());
+      profile->IsPrimaryOTRProfileWithRegularParent());
 }
 
 bool ChromePageInfoDelegate::IsIsolatedWebApp() {
@@ -638,7 +638,7 @@ bool ChromePageInfoDelegate::IsHttpsFirstModeEnabledForUrl(const GURL& url) {
 }
 
 bool ChromePageInfoDelegate::IsIncognitoProfile() {
-  return GetProfile()->IsIncognitoProfile();
+  return GetProfile()->IsPrimaryOTRProfileWithRegularParent();
 }
 
 void ChromePageInfoDelegate::SetSecurityStateForTests(
