4b55efcc1a6521b783aae882360e3a3dfbc34480
Wez <wez@chromium.org>
2026-09-09 09:29:34 -0700

    [fuchsia] Implement CPUModelName and NumberOfEfficientProcessors
    Provide a CPU model name under x86 based on unprivileged CPUID
    instructions, and return an empty string on other architectures.
    
    Remove NOTIMPLEMENTED() from NumberOfEfficientProcessorsImpl(). The
    Zircon kernel's scheduler manages scheduler-profile affinity to efficint
    cores, so the Chromium userspace need not take any special action.
    
    TAG=agy
    CONV=21cf8dbc-82a7-4121-bba2-959cec50f298
    
    Bug: 547827650
    Change-Id: I03fefc3b5a6a46d20e83a426727b2128dd560c76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8363312
    Auto-Submit: Wez <wez@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694651}

--- base/system/sys_info_fuchsia.cc
+++ base/system/sys_info_fuchsia.cc
@@ -13,6 +13,7 @@
 
 #include "base/byte_size.h"
 #include "base/containers/flat_map.h"
+#include "base/cpu.h"
 #include "base/files/file_util.h"
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/system_info.h"
@@ -267,9 +268,7 @@ std::string SysInfo::OperatingSystemArchitecture() {
 
 // static
 std::string SysInfo::CPUModelName() {
-  // TODO(crbug.com/40191727): Implement this when Fuchsia supports it.
-  NOTIMPLEMENTED_LOG_ONCE();
-  return std::string();
+  return CPU().cpu_brand();
 }
 
 // static
@@ -279,7 +278,9 @@ size_t SysInfo::VMAllocationGranularity() {
 
 // static
 int SysInfo::NumberOfEfficientProcessorsImpl() {
-  NOTIMPLEMENTED();
+  // Zircon's scheduling profiles take care of efficient-core affinity, so
+  // return 0 to allow the default thread pool behavior to function
+  // reasonably on both symmetric and asymmetric cores.
   return 0;
 }
 
