aecf05c69794ee8f935c296f8a900a87bb89c569
Felipe Erias <felipeerias@igalia.com>
2026-09-09 00:07:23 -0700

    [SVG] Treat stroke-dasharray as non-additive
    SVG 2 defines `stroke-dasharray` values as non-additive:
    https://w3c.github.io/svgwg/svg2-draft/painting.html#interpolationDashPattern
    
    Blink currently uses CSSLengthListInterpolationType's generic additive
    composition for `stroke-dasharray`. As a result, keyframes with
    `composite: add` or `composite: accumulate` are added to the underlying
    value instead of behaving like `composite: replace`.
    
    Make CSSLengthListInterpolationType treat both operations as
    `composite: replace` for `stroke-dasharray`. A neutral keyframe must
    still represent the underlying value, so convert it to a copy of that
    value and invalidate it whenever the underlying value may have changed.
    
    `iterationComposite: accumulate` (behind CSSAnimationIterationComposite)
    and SMIL additive animation are out of scope for this change.
    
    Tests:
     external/wpt/svg/painting/animations/stroke-dasharray-composition.html
     animations/responsive/interpolation/stroke-dasharray-responsive.html
    
    Bug: 507903801
    Change-Id: I4dec526e796087045f62ca1ac5e3f371a3be6b80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8314391
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Felipe Erias <felipeerias@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1694385}

--- third_party/blink/renderer/core/animation/css_length_list_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_length_list_interpolation_type.cc
@@ -23,6 +23,27 @@
 
 namespace blink {
 
+namespace {
+
+// SVG 2 defines stroke-dasharray values as non-additive.
+// https://w3c.github.io/svgwg/svg2-draft/painting.html#interpolationDashPattern
+bool IsNonAdditiveLengthListProperty(const CSSProperty& property) {
+  return property.IDEquals(CSSPropertyID::kStrokeDasharray);
+}
+
+// The neutral value of a non-additive property is a copy of the underlying
+// value, so it has to be converted again whenever that value may have changed.
+class AlwaysInvalidateChecker final
+    : public CSSInterpolationType::CSSConversionChecker {
+ public:
+  bool IsValid(const StyleResolverState&,
+               const InterpolationValue&) const final {
+    return false;
+  }
+};
+
+}  // namespace
+
 CSSLengthListInterpolationType::CSSLengthListInterpolationType(
     PropertyHandle property)
     : CSSInterpolationType(property),
@@ -31,6 +52,12 @@ CSSLengthListInterpolationType::CSSLengthListInterpolationType(
 InterpolationValue CSSLengthListInterpolationType::MaybeConvertNeutral(
     const InterpolationValue& underlying,
     ConversionCheckers& conversion_checkers) const {
+  if (IsNonAdditiveLengthListProperty(CssProperty())) {
+    conversion_checkers.push_back(
+        MakeGarbageCollected<AlwaysInvalidateChecker>());
+    return underlying.Clone();
+  }
+
   wtf_size_t underlying_length =
       UnderlyingLengthChecker::GetUnderlyingLength(underlying);
   conversion_checkers.push_back(
@@ -152,6 +179,11 @@ void CSSLengthListInterpolationType::Composite(
     double underlying_fraction,
     const InterpolationValue& value,
     double interpolation_fraction) const {
+  if (IsNonAdditiveLengthListProperty(CssProperty())) {
+    underlying_value_owner.Set(this, value);
+    return;
+  }
+
   ListInterpolationFunctions::Composite(
       underlying_value_owner, underlying_fraction, this, value,
       ListInterpolationFunctions::LengthMatchingStrategy::kLowestCommonMultiple,
