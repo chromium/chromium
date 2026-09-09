d6a68e05eee1e4728bf3a96ccb01d1d48316c15b
Alexis Hétu <sugoi@chromium.org>
2026-09-09 09:07:13 -0700

    [iOS] Disable ClipboardJsTest read tests on device
    Disable ClipboardJsTest.TestReadAndAllow and
    ClipboardJsTest.TestReadTextAndAllow on device
    due to failures on physical hardware.
    
    Bug: 559140540
    Change-Id: Icaf9ed3450d35483d5f1b33c22c54b66d49984e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8376752
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Ibrahim Kanouche <kanouche@google.com>
    Owners-Override: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Ibrahim Kanouche <kanouche@google.com>
    Auto-Submit: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1694629}

--- ios/web/js_features/clipboard/clipboard_js_unittest.mm
+++ ios/web/js_features/clipboard/clipboard_js_unittest.mm
@@ -202,7 +202,13 @@ TEST_F(ClipboardJsTest, TestWriteAndAllow) {
 }
 
 // Tests that a single readText request can be successfully fulfilled.
-TEST_F(ClipboardJsTest, TestReadTextAndAllow) {
+// TODO(crbug.com/559140540): Re-enable this test on device.
+#if TARGET_OS_SIMULATOR
+#define MAYBE_TestReadTextAndAllow TestReadTextAndAllow
+#else
+#define MAYBE_TestReadTextAndAllow DISABLED_TestReadTextAndAllow
+#endif
+TEST_F(ClipboardJsTest, MAYBE_TestReadTextAndAllow) {
   // First, write text to the clipboard to ensure there's something to read.
   WriteText("test");
   EXPECT_TRUE(WaitUntilConditionOrTimeout(kWaitForJSCompletionTimeout, ^{
@@ -243,7 +249,13 @@ TEST_F(ClipboardJsTest, TestReadTextAndAllow) {
 }
 
 // Tests that a single read request can be successfully fulfilled.
-TEST_F(ClipboardJsTest, TestReadAndAllow) {
+// TODO(crbug.com/559140540): Re-enable this test on device.
+#if TARGET_OS_SIMULATOR
+#define MAYBE_TestReadAndAllow TestReadAndAllow
+#else
+#define MAYBE_TestReadAndAllow DISABLED_TestReadAndAllow
+#endif
+TEST_F(ClipboardJsTest, MAYBE_TestReadAndAllow) {
   // First, write text to the clipboard to ensure there's something to read.
   WriteText("test");
   EXPECT_TRUE(WaitUntilConditionOrTimeout(kWaitForJSCompletionTimeout, ^{
