439587d1ab365f3b3f2f4664544c8593032183d5
Peter Pakkenberg <pbirk@chromium.org>
2026-09-09 07:28:34 -0700

    Revert "[Signin][Settings] Consolidate ManageSyncSettings search tests"
    This reverts commit 06c6bf6003e7c8d576ce49c4968257e43eb25a47.
    
    Reason for revert: Causes failure on android-desktop-x64-rel-15-tests 
    
    Failure Link: https://luci-milo.appspot.com/ui/p/chromium/builders/ci/android-desktop-x64-rel-15-tests/22437/overview
    
    Original change's description:
    > [Signin][Settings] Consolidate ManageSyncSettings search tests
    >
    > Replace individual search-to-row tests for ManageSyncSettings with
    > screen-to-index parity tests
    >
    > This tests all preferences across both EEA and non-EEA configurations,
    > removes more expensive and less scoped end-to-end search interactions.
    >
    > This test revealed http://crbug.com/558260627 and would prevent
    > http://crbug.com/553945086 and http://crbug.com/549161371 which were a
    > motivation for the previous tests.
    >
    > See crrev.com/c/8337007 for the parity test check details.
    >
    > Bug: 549021942
    > Change-Id: Icd3259888ee3a418282b308d58acdb33ed779f8b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8355505
    > Reviewed-by: Lucia Giorgi <quacksort@google.com>
    > Reviewed-by: Menghan Yang <myuu@google.com>
    > Commit-Queue: Josef Raska <josefraska@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1694449}
    
    Bug: 549021942
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I64a81914c89d0afc6da3de2508b5090ab4ff77e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8380499
    Owners-Override: Peter Pakkenberg <pbirk@chromium.org>
    Auto-Submit: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1694577}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettingsTest.java
@@ -29,7 +29,6 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import static org.chromium.chrome.browser.settings.SettingsSearchTestUtils.assertNoSearchResultsFound;
-import static org.chromium.chrome.browser.settings.SettingsSearchTestUtils.assertPreferenceScreenMatchesIndex;
 import static org.chromium.chrome.browser.settings.SettingsSearchTestUtils.clickSearchResult;
 import static org.chromium.chrome.browser.settings.SettingsSearchTestUtils.highlighted;
 import static org.chromium.chrome.browser.settings.SettingsSearchTestUtils.typeSearchQuery;
@@ -197,14 +196,6 @@ public class ManageSyncSettingsTest {
                     entry(
                             UserSelectableType.THEMES,
                             ManageSyncSettings.PREF_ACCOUNT_SECTION_THEMES_TOGGLE));
-
-    /** Preferences on ManageSyncSettings that are not expected to be searchable in the index. */
-    private static final Set<String> ALLOWLISTED_PREFERENCE_KEYS =
-            Set.of(
-                    ManageSyncSettings.PREF_IDENTITY_ERROR_CARD_PREFERENCE,
-                    ManageSyncSettings.PREF_BATCH_UPLOAD_CARD_PREFERENCE,
-                    // TODO(http://crbug.com/558260627): Remove once the bug is fixed.
-                    ManageSyncSettings.PREF_SETTINGS_SYNC_DISABLED_BY_ADMINISTRATOR);
 
     private SettingsActivityInterface mSettingsActivityInterface;
 
@@ -1716,23 +1707,30 @@ public class ManageSyncSettingsTest {
 
     @Test
     @SmallTest
-    public void testPreferenceScreenMatchesSearchIndex_signedIn_nonEea() {
+    public void testSearchPersonalizationAndLinkingTitle_signedIn_nonEea() {
         when(mRegionalCapabilities.isInEeaCountry()).thenReturn(false);
+        mSettingsSearchTestRule.startSettingsActivity();
         mSyncTestRule.setUpAccountAndSignInForTesting();
-        ManageSyncSettings fragment = startManageSyncPreferences();
 
-        assertPreferenceScreenMatchesIndex(fragment, ALLOWLISTED_PREFERENCE_KEYS);
+        typeSearchQuery("personalization");
+
+        onViewWaiting(withText(R.string.sign_in_personalize_google_services_title))
+                .check(matches(isDisplayed()));
     }
 
     @Test
     @SmallTest
-    public void testPreferenceScreenMatchesSearchIndex_signedIn_eea() {
+    public void testSearchPersonalizationAndLinkingTitle_signedIn_eea() {
         when(mRegionalCapabilities.isInEeaCountry()).thenReturn(true);
+        mSettingsSearchTestRule.startSettingsActivity();
         mSyncTestRule.setUpAccountAndSignInForTesting();
-        ManageSyncSettings fragment = startManageSyncPreferences();
 
-        assertPreferenceScreenMatchesIndex(fragment, ALLOWLISTED_PREFERENCE_KEYS);
+        typeSearchQuery("linking");
+
+        onViewWaiting(withText(R.string.sign_in_personalize_google_services_title_eea))
+                .check(matches(isDisplayed()));
     }
+
 
     private void assertOpensIncognitoSession(
             boolean openAsWindow, Matcher<Intent> expectedIntentMatcher) {
