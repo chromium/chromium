9a6c8ad29c7fce87262aa1598b3f7afdfbaeaa35
Bryant Chandler <bryantchandler@google.com>
2026-09-10 08:39:45 -0700

    [glic] Remove unused PrintCrossProcessSubframe from GlicSidePanelUi
    Glic has no cross-origin subframes, so PrintCrossProcessSubframe is never
    invoked. GlicSidePanelUi can inherit the default no-op from
    WebContentsDelegateAndroid.
    
    Bug: b:534807813
    Change-Id: Ied278427e46f62db42da75424c98bdbd6a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8380907
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695457}

--- chrome/browser/glic/widget/glic_side_panel_ui_android.cc
+++ chrome/browser/glic/widget/glic_side_panel_ui_android.cc
@@ -32,7 +32,6 @@
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/drop_data.h"
-#include "printing/buildflags/buildflags.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "third_party/blink/public/common/mediastream/media_stream_request.h"
 #include "third_party/blink/public/mojom/mediastream/media_stream.mojom.h"
@@ -41,10 +40,6 @@
 #include "ui/base/base_window.h"
 #include "ui/content_accelerators/accelerator_util.h"
 #include "ui/events/keycodes/keyboard_codes.h"
-
-#if BUILDFLAG(ENABLE_PRINTING)
-#include "components/printing/browser/print_composite_client.h"
-#endif
 
 namespace glic {
 
@@ -361,19 +356,6 @@ void GlicSidePanelUi::RunFileChooser(
     const blink::mojom::FileChooserParams& params) {
   FileSelectHelper::RunFileChooser(render_frame_host, std::move(listener),
                                    params);
-}
-
-void GlicSidePanelUi::PrintCrossProcessSubframe(
-    content::WebContents* web_contents,
-    const gfx::Rect& rect,
-    int document_cookie,
-    content::RenderFrameHost* subframe_host) const {
-#if BUILDFLAG(ENABLE_PRINTING)
-  auto* client = printing::PrintCompositeClient::FromWebContents(web_contents);
-  if (client) {
-    client->PrintCrossProcessSubframe(rect, document_cookie, subframe_host);
-  }
-#endif
 }
 
 void GlicSidePanelUi::FocusIfOpen() {
--- chrome/browser/glic/widget/glic_side_panel_ui_android.h
+++ chrome/browser/glic/widget/glic_side_panel_ui_android.h
@@ -119,11 +119,6 @@ class GlicSidePanelUi
   void RunFileChooser(content::RenderFrameHost* render_frame_host,
                       scoped_refptr<content::FileSelectListener> listener,
                       const blink::mojom::FileChooserParams& params) override;
-  void PrintCrossProcessSubframe(
-      content::WebContents* web_contents,
-      const gfx::Rect& rect,
-      int document_cookie,
-      content::RenderFrameHost* subframe_host) const override;
 
   // BrowserCollectionObserver
   void OnBrowserActivated(BrowserWindowInterface* browser) override;
