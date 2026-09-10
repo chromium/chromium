3456e637312130ebb8a12fcc3dbd45fd95db7a30
Agata Szpotek <szpotekagata@google.com>
2026-09-09 23:55:02 -0700

    [Isolated mode] Do not show frame overlay images in Enterprise Isolated Mode.
    This updates BrowserFrameView::GetFrameOverlayImage to return an empty
    image when the browser is in Enterprise Isolated Mode, matching the
    behavior for Incognito and non-normal browser windows.
    
    Change-Id: I0defe7b08f3a52ad2a45918f4da9b1b12a865617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8377601
    Commit-Queue: Agata Szpotek <szpotekagata@google.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1695216}

--- chrome/browser/ui/views/frame/browser_frame_view.cc
+++ chrome/browser/ui/views/frame/browser_frame_view.cc
@@ -342,6 +342,7 @@ gfx::ImageSkia BrowserFrameView::GetFrameImage(
 gfx::ImageSkia BrowserFrameView::GetFrameOverlayImage(
     BrowserFrameActiveState active_state) const {
   if (GetBrowserView()->GetIncognito() ||
+      GetBrowserView()->GetEnterpriseIsolatedMode() ||
       !GetBrowserView()->GetIsNormalType()) {
     return gfx::ImageSkia();
   }
