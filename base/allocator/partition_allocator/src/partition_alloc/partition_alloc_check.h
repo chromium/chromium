f099401af5041072a9f42e88b25174d6e7d5b83d
Daniel Angulo <angdaniel@google.com>
2026-09-10 13:01:45 -0700

    Spanification of partition_alloc/partition_alloc_check.h
    This is the result of running the automatic spanification on linux and
    updating code to use and pass spans where size is known.
    
    The original patch was fully automated using script:
    //tools/clang/spanify/rewrite-multiple-platforms.sh -platforms=linux
    Then refined with gemini-cli and at last manually refined
    
    Gemini-CLI auto-fixes for compilation
    
    Bug: 501143531
    Change-Id: I2621d6874d02a429aea43e09a52e31e6092cde5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7833832
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Daniel Angulo <angdaniel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695599}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_check.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_check.h
@@ -5,7 +5,9 @@
 #ifndef PARTITION_ALLOC_PARTITION_ALLOC_CHECK_H_
 #define PARTITION_ALLOC_PARTITION_ALLOC_CHECK_H_
 
+#include <array>
 #include <cstdint>
+#include <string_view>
 
 #include "partition_alloc/build_config.h"
 #include "partition_alloc/buildflags.h"
@@ -111,19 +113,19 @@ static constexpr size_t kDebugKeyMaxLength = 8ull;
 struct PA_DEBUGKV_ALIGN DebugKv {
   // 16 bytes object aligned on 16 bytes, to make it easier to see in crash
   // reports.
-  char k[kDebugKeyMaxLength] = {};  // Not necessarily 0-terminated.
+  std::array<char, kDebugKeyMaxLength> k = {};  // Not necessarily 0-terminated.
   uint64_t v = 0;
 
-  DebugKv(const char* key, uint64_t value) : v(value) {
+  DebugKv(std::string_view key, uint64_t value) : v(value) {
     // Fill with ' ', so that the stack dump is nicer to read.  Not using
     // memset() on purpose, this header is included from *many* places.
     for (char& c : k) {
       c = ' ';
     }
 
-    for (size_t index = 0; index < sizeof k; index++) {
-      PA_UNSAFE_TODO(k[index]) = PA_UNSAFE_TODO(key[index]);
-      if (PA_UNSAFE_TODO(key[index]) == '\0') {
+    for (size_t index = 0; index < k.size() && index < key.size(); index++) {
+      k[index] = key[index];
+      if (key[index] == '\0') {
         break;
       }
     }
