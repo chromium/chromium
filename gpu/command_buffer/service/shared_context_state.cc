ffc448da81d9f2b228e4374ad3f1e8f3243bccb9
kylechar <kylechar@chromium.org>
2026-09-10 14:14:29 -0700

    [M154] Check transfer cache entries for wrap around
    Original change's description:
    > Check transfer cache entries for wrap around
    >
    > The GPU service transfer cache uses the same namespace of ids for every
    > client in the same process, The ID generation had no protection against
    > wrap around so we could see duplicate IDs if enough are allocated
    > maliciously. To prevent ID collisions add a CHECK that ensures wrap
    > around doesn't happen. We don't expect wrap around to happen through
    > normal usage of Chrome.
    >
    > This CL moves the ID generation implementation to source file since it's
    > only used there. It also generates uint32_t instead of int values so the
    > full uint32_t ID range is used.
    >
    > Bug: 556260782
    > Change-Id: I876c5fdd55185a3a2d290abfdb59cb257f8e1196
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8349499
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1693922}
    
    (cherry picked from commit 863daaa953d823531569d571cd77da02be50e265)
    
    Bug: 559510679,556260782
    Change-Id: I876c5fdd55185a3a2d290abfdb59cb257f8e1196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8382067
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/branch-heads/8037@{#801}
    Cr-Branched-From: e10b20e60f162e7ad88e4930833695a54f4e15fb-refs/heads/main@{#1689415}

--- gpu/command_buffer/service/shared_context_state.cc
+++ gpu/command_buffer/service/shared_context_state.cc
@@ -4,6 +4,7 @@
 
 #include "gpu/command_buffer/service/shared_context_state.h"
 
+#include "base/atomic_sequence_num.h"
 #include "base/compiler_specific.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/immediate_crash.h"
