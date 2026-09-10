862afa9f99c1abf9508e3c07a6812c57309d64ea
Alfredo Higuera <alfredohiguera@google.com>
2026-09-10 11:58:45 -0700

    [M154] [Read Anything] Remove line focus options from text menu
    Original change's description:
    > [Read Anything] Remove line focus options from text menu
    >
    > In the new UI design, line focus settings will live under a dedicated
    > tools menu rather than within the text settings menu. As a pre-refactor
    > for this change, remove the line focus options, properties, and event
    > handlers from text-menu.
    >
    > Bug: 554027759
    > Change-Id: I7462b1a1666737147e3b73412e951bf9799fcea2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8342952
    > Reviewed-by: Kristi Saney <kristislee@google.com>
    > Commit-Queue: Alfredo Higuera <alfredohiguera@google.com>
    > Reviewed-by: Martín Gómez <martinglopez@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1692711}
    
    (cherry picked from commit 26ec395a1c1f1fe9e74a9792549427378518c983)
    
    Bug: 559688433,554027759
    Change-Id: I7462b1a1666737147e3b73412e951bf9799fcea2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8386218
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8037@{#777}
    Cr-Branched-From: e10b20e60f162e7ad88e4930833695a54f4e15fb-refs/heads/main@{#1689415}

--- chrome/browser/resources/side_panel/read_anything/menus/text_menu.ts
+++ chrome/browser/resources/side_panel/read_anything/menus/text_menu.ts
@@ -12,7 +12,7 @@ import type {PropertyValues} from '//resources/lit/v3_0/lit.rollup.js';
 
 import type {VisualBrowserProxy} from '../app/visual_browser_proxy.js';
 import {VisualBrowserProxyImpl} from '../app/visual_browser_proxy.js';
-import {DEFAULT_SETTINGS, LineFocusMovement, LineFocusStyle, ToolbarEvent} from '../content/read_anything_types.js';
+import {DEFAULT_SETTINGS, ToolbarEvent} from '../content/read_anything_types.js';
 import type {SettingsPrefs, ShowAtConfigPrefs} from '../content/read_anything_types.js';
 import {ReadAnythingSettingsChange} from '../shared/metrics_browser_proxy.js';
 import {ReadAnythingLogger} from '../shared/read_anything_logger.js';
@@ -46,9 +46,6 @@ export class TextMenuElement extends TextMenuElementBase implements
       areFontsLoaded: {type: Boolean},
       pageLanguage: {type: String},
       groups_: {type: Array},
-      lineFocusStyle: {type: Object},
-      lineFocusEnabled: {type: Boolean},
-      lineFocusMovement: {type: Number},
     };
   }
 
@@ -56,9 +53,6 @@ export class TextMenuElement extends TextMenuElementBase implements
   accessor nonModal: boolean = false;
   accessor areFontsLoaded: boolean = false;
   accessor pageLanguage: string = '';
-  accessor lineFocusStyle: LineFocusStyle|null = null;
-  accessor lineFocusEnabled: boolean = false;
-  accessor lineFocusMovement: LineFocusMovement|null = null;
 
   private visualBrowserProxy_: VisualBrowserProxy =
       VisualBrowserProxyImpl.getInstance();
@@ -106,98 +100,36 @@ export class TextMenuElement extends TextMenuElementBase implements
     },
   ];
 
-  private toggleOptions_: Array<MenuStateItem<boolean>> = [
+  protected accessor groups_: Array<MenuGroup<number|string>> = [
     {
-      title: loadTimeData.getString('lineFocusOffTitle'),
-      data: false,
+      header: {
+        title: loadTimeData.getString('fontNameTitle'),
+        separator: false,
+      },
+      items: this.fontOptions_,
+      eventName: ToolbarEvent.FONT,
     },
     {
-      title: loadTimeData.getString('lineFocusOnTitle'),
-      data: true,
+      header: {
+        title: loadTimeData.getString('lineSpacingTitle'),
+        separator: true,
+      },
+      items: this.lineSpacingOptions_,
+      eventName: ToolbarEvent.LINE_SPACING,
+    },
+    {
+      header: {
+        title: loadTimeData.getString('letterSpacingTitle'),
+        separator: true,
+      },
+      items: this.letterSpacingOptions_,
+      eventName: ToolbarEvent.LETTER_SPACING,
     },
   ];
-
-  private styleOptions_: Array<MenuStateItem<LineFocusStyle>> = [
-    {
-      title: loadTimeData.getString('lineFocusUnderlineTitle'),
-      data: LineFocusStyle.UNDERLINE,
-    },
-    {
-      title: loadTimeData.getString('lineFocusOneLineTitle'),
-      data: LineFocusStyle.SMALL_WINDOW,
-    },
-    {
-      title: loadTimeData.getString('lineFocusThreeLineTitle'),
-      data: LineFocusStyle.MEDIUM_WINDOW,
-    },
-    {
-      title: loadTimeData.getString('lineFocusFiveLineTitle'),
-      data: LineFocusStyle.LARGE_WINDOW,
-    },
-  ];
-
-  private movementOptions_: Array<MenuStateItem<LineFocusMovement>> = [
-    {
-      title: loadTimeData.getString('lineFocusStaticTitle'),
-      data: LineFocusMovement.STATIC,
-    },
-    {
-      title: loadTimeData.getString('lineFocusCursorLineTitle'),
-      data: LineFocusMovement.CURSOR,
-    },
-  ];
-
-  private get defaultGroups_():
-      Array<MenuGroup<number|string|boolean|LineFocusStyle|LineFocusMovement>> {
-    return [
-      {
-        header: {
-          title: loadTimeData.getString('fontNameTitle'),
-          separator: false,
-        },
-        items: this.fontOptions_,
-        eventName: ToolbarEvent.FONT,
-      },
-      {
-        header: {
-          title: loadTimeData.getString('lineSpacingTitle'),
-          separator: true,
-        },
-        items: this.lineSpacingOptions_,
-        eventName: ToolbarEvent.LINE_SPACING,
-      },
-      {
-        header: {
-          title: loadTimeData.getString('letterSpacingTitle'),
-          separator: true,
-        },
-        items: this.letterSpacingOptions_,
-        eventName: ToolbarEvent.LETTER_SPACING,
-      },
-    ];
-  }
-
-  protected accessor groups_:
-      Array<MenuGroup<number|string|boolean|LineFocusStyle|LineFocusMovement>> =
-          this.defaultGroups_;
   private logger_: ReadAnythingLogger = ReadAnythingLogger.getInstance();
 
   override willUpdate(changedProperties: PropertyValues<this>) {
     super.willUpdate(changedProperties);
-
-    if (this.visualBrowserProxy_.isLineFocusEnabled()) {
-      if (changedProperties.has('lineFocusEnabled')) {
-        this.updateOptionsForToggle_(this.lineFocusEnabled);
-      }
-      if (changedProperties.has('lineFocusStyle') &&
-          this.lineFocusStyle !== null) {
-        this.updateOptionsForStyle_(this.lineFocusStyle);
-      }
-      if (changedProperties.has('lineFocusMovement') &&
-          this.lineFocusMovement !== null) {
-        this.updateOptionsForMovement_(this.lineFocusMovement);
-      }
-    }
 
     if (changedProperties.has('pageLanguage') ||
         changedProperties.has('areFontsLoaded')) {
@@ -206,14 +138,10 @@ export class TextMenuElement extends TextMenuElementBase implements
 
     if (changedProperties.has('settingsPrefs') ||
         changedProperties.has('pageLanguage') ||
-        changedProperties.has('areFontsLoaded') ||
-        changedProperties.has('lineFocusEnabled') ||
-        changedProperties.has('lineFocusStyle') ||
-        changedProperties.has('lineFocusMovement')) {
+        changedProperties.has('areFontsLoaded')) {
       this.updateOptionsForFont_();
       this.updateOptionsForLineSpacing_();
       this.updateOptionsForLetterSpacing_();
-      this.computeGroups_();
       this.groups_ = [...this.groups_];
     }
   }
@@ -246,21 +174,6 @@ export class TextMenuElement extends TextMenuElementBase implements
         ReadAnythingSettingsChange.LETTER_SPACING_CHANGE);
   }
 
-  protected onLineFocusStyleChange_() {
-    this.logger_.logTextSettingsChange(
-        ReadAnythingSettingsChange.LINE_FOCUS_STYLE_CHANGE);
-  }
-
-  protected onLineFocusToggleChange_() {
-    this.logger_.logTextSettingsChange(
-        ReadAnythingSettingsChange.LINE_FOCUS_TOGGLE);
-  }
-
-  protected onLineFocusMovementChange_() {
-    this.logger_.logTextSettingsChange(
-        ReadAnythingSettingsChange.LINE_FOCUS_MOVEMENT_CHANGE);
-  }
-
   private updateOptionsForFont_() {
     const currentFont = this.visualBrowserProxy_.getFontName();
     this.fontOptions_.forEach(option => {
@@ -280,61 +193,6 @@ export class TextMenuElement extends TextMenuElementBase implements
     this.letterSpacingOptions_.forEach(option => {
       option.selected = option.data === currentSpacing;
     });
-  }
-
-  private updateOptionsForToggle_(isEnabled: boolean) {
-    this.toggleOptions_.forEach(option => {
-      option.selected = option.data === isEnabled;
-    });
-  }
-
-  private updateOptionsForStyle_(newStyle: LineFocusStyle) {
-    this.styleOptions_.forEach(option => {
-      option.selected = option.data === newStyle;
-    });
-  }
-
-  private updateOptionsForMovement_(newMovement: LineFocusMovement) {
-    this.movementOptions_.forEach(option => {
-      option.selected = option.data === newMovement;
-    });
-  }
-
-  private computeGroups_() {
-    const groups = this.defaultGroups_;
-
-    if (this.visualBrowserProxy_.isLineFocusEnabled()) {
-      groups.push({
-        header: {
-          title: loadTimeData.getString('lineFocusLabel'),
-          separator: true,
-        },
-        items: this.toggleOptions_,
-        eventName: ToolbarEvent.LINE_FOCUS_TOGGLE,
-      });
-
-      if (this.lineFocusEnabled) {
-        groups.push(
-            {
-              header: {
-                title: loadTimeData.getString('lineFocusStyleHeading'),
-                separator: false,
-              },
-              items: this.styleOptions_,
-              eventName: ToolbarEvent.LINE_FOCUS_STYLE,
-            },
-            {
-              header: {
-                title: loadTimeData.getString('lineFocusMovementHeading'),
-                separator: false,
-              },
-              items: this.movementOptions_,
-              eventName: ToolbarEvent.LINE_FOCUS_MOVEMENT,
-            });
-      }
-    }
-
-    this.groups_ = groups;
   }
 
   private computeFontOptions_() {
