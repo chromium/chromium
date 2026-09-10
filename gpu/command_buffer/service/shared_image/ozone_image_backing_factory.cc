332df931631f3f6dd93d635d30ad27d80f8540d7
Gyuyoung Kim <gyuyoung@igalia.com>
2026-09-10 01:11:12 -0700

    [//gpu] Fix ozone build with enable_vulkan=false
    OzoneImageBackingFactory::CreateGpuMemoryBufferHandle() dereferences
    VulkanContextProvider, but this file only includes its header under
    BUILDFLAG(ENABLE_VULKAN), so an ozone build with enable_vulkan=false
    fails with "member access into incomplete type". Guard the dereference
    the same way CreateSharedImageInternal() in this file already does.
    
    Bug: 404905709
    Change-Id: I1a93ac9c720a11af76f0995e7458c840de6cf21a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8376406
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1695247}

--- gpu/command_buffer/service/shared_image/ozone_image_backing_factory.cc
+++ gpu/command_buffer/service/shared_image/ozone_image_backing_factory.cc
@@ -91,14 +91,17 @@ OzoneImageBackingFactory::CreateGpuMemoryBufferHandle(
     const gfx::Size& size,
     viz::SharedImageFormat format,
     gfx::BufferUsage usage) {
+  VulkanDeviceQueue* device_queue = nullptr;
+#if BUILDFLAG(ENABLE_VULKAN)
+  if (vulkan_context_provider) {
+    device_queue = vulkan_context_provider->GetDeviceQueue();
+  }
+#endif  // BUILDFLAG(ENABLE_VULKAN)
   scoped_refptr<gfx::NativePixmap> pixmap =
       ui::OzonePlatform::GetInstance()
           ->GetSurfaceFactoryOzone()
-          ->CreateNativePixmap(gpu::kNullSurfaceHandle,
-                               vulkan_context_provider
-                                   ? vulkan_context_provider->GetDeviceQueue()
-                                   : nullptr,
-                               size, format, usage, size);
+          ->CreateNativePixmap(gpu::kNullSurfaceHandle, device_queue, size,
+                               format, usage, size);
 
   if (!pixmap.get()) {
     DLOG(ERROR) << "Failed to create pixmap " << size.ToString() << ",  "
