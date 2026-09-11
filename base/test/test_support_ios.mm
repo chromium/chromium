855fa54323dbc647d25ad79fa949ce14d421dcff
fayev-g <fayev@google.com>
2026-09-11 09:06:49 -0700

    [tvOS] Do not redirect test output to file on tvOS
    Disable output file redirection in shouldRedirectOutputToFile for both
    tvOS physical devices and tvOS simulator targets. See bug for detailed
    info.
    
    Bug: 559272911
    Change-Id: I7f431e7880fd0898326f7978a14ea202a35b3fa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8382998
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Faye Johnston <fayev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696319}

--- base/test/test_support_ios.mm
+++ base/test/test_support_ios.mm
@@ -142,16 +142,17 @@ void PopulateUIWindow(UIWindow* window) {
 // Returns true if the gtest output should be redirected to a file, then sent
 // to NSLog when complete. This redirection is used because gtest only writes
 // output to stdout, but results must be written to NSLog in order to show up in
-// the device log that is retrieved from the device by the host.
+// the device log that is retrieved from the device by the host. Output is not
+// redirected for simulators or tvOS.
 - (BOOL)shouldRedirectOutputToFile {
-#if !TARGET_OS_SIMULATOR
+#if TARGET_OS_SIMULATOR || BUILDFLAG(IS_IOS_TVOS)
+  return NO;
+#else
   // Tests in XCTest mode don't need to redirect output to a file because the
   // test result parser analyzes console output.
   return !base::ShouldRunIOSUnittestsWithXCTest() &&
          !base::debug::BeingDebugged();
-#else
-  return NO;
-#endif  // TARGET_OS_SIMULATOR
+#endif  // TARGET_OS_SIMULATOR || BUILDFLAG(IS_IOS_TVOS)
 }
 
 // Returns the path to the directory to store gtest output files.
