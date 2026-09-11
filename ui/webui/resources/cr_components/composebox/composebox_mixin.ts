2c57e3678e0beba50b3c34cc2dec8cb11c801012
James Leung <jamesleung@google.com>
2026-09-11 13:41:40 -0700

    [tab_management] Add plus button padding for tab management feature
    - Reflects the contextManagementInComposeboxEnabled property to the host element to allow CSS styling.
    - Adjusts the padding of the context entrypoint (plus button) to
    to be larger when context management is enabled and tabs are present.
    
    No regression - screen/69hm751hpa840
    
    Test: screen/7tm13h5rmkut0
    Bug: b:527949363
    Change-Id: I04a864018750f9fbc7baff1b682a1f26ad9c1fe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8346195
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696565}

--- ui/webui/resources/cr_components/composebox/composebox_mixin.ts
+++ ui/webui/resources/cr_components/composebox/composebox_mixin.ts
@@ -101,8 +101,11 @@ export const ComposeboxEmbedderMixin =
             showContextMenuDescription: {type: Boolean},
             smartTabSharingActive: {type: Boolean},
             smartTabSharingVisible: {type: Boolean},
-            contextManagementInComposeboxEnabled: {type: Boolean},
             composeboxContextMenuTooltipsEnabled: {type: Boolean},
+            contextManagementInComposeboxEnabled: {
+              reflect: true,
+              type: Boolean,
+            },
             shouldShowGhostFiles: {type: Boolean},
             showMenuOnClick: {type: Boolean},
             submitButtonIconType: {type: String},
