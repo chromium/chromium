83a423b9817966697d40cedbacf03cea42d7441d
Camillia Smith Barnes <cammie@chromium.org>
2026-09-11 08:05:23 -0700

    WebUILocationBar: Fix VerticalTabStripInteractiveUiTest
    Fix VerticalTabStripInteractiveUiTest.KeyboardShortcutTogglesCollapse
    when WebUILocationBar is enabled.
    
    When WebUILocationBar is enabled, the WebUI toolbar and location bar
    initialize asynchronously. Ensure the test waits for the initial WebUI
    toolbar in SetUpOnMainThread. Additionally, dispatch the collapse toggle
    accelerator using SendAccelerator with the BrowserView-registered
    accelerator rather than raw OS keypresses (SendKeyPress) to ensure
    reliable handling during UI collapse and expansion state changes.
    
    TAG=agy
    CONV=0ec0109a-5792-4c1b-94cb-088038d6c3b8
    
    Bug: 559679679
    Change-Id: I19b0da86838e348753afc1607bce81fecc4ddd74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8390206
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696253}

--- chrome/browser/ui/tabs/vertical_tab_strip_state_controller_interactive_uitest.cc
+++ chrome/browser/ui/tabs/vertical_tab_strip_state_controller_interactive_uitest.cc
@@ -22,6 +22,7 @@
 #include "chrome/browser/ui/views/frame/browser_widget.h"
 #include "chrome/browser/ui/views/frame/system_menu_model_builder.h"
 #include "chrome/browser/ui/views/test/tab_strip_interactive_test_mixin.h"
+#include "chrome/browser/ui/views/toolbar/webui_test_utils.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -114,6 +115,11 @@ class VerticalTabStripInteractiveUiTest : public InteractiveBrowserTest {
                     &VerticalTabStripInteractiveUiTest::CreateFakeController,
                     base::Unretained(this)));
     InteractiveBrowserTest::SetUp();
+  }
+
+  void SetUpOnMainThread() override {
+    InteractiveBrowserTest::SetUpOnMainThread();
+    WaitForInitialWebUIToolbar(browser());
   }
 
   bool SystemMenuContainsStringId(int message_id) {
@@ -522,6 +528,10 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripInteractiveUiTest,
 
 IN_PROC_BROWSER_TEST_F(VerticalTabStripInteractiveUiTest,
                        KeyboardShortcutTogglesCollapse) {
+  ui::Accelerator toggle_collapse_accelerator;
+  ASSERT_TRUE(BrowserView::GetBrowserViewForBrowser(browser())->GetAccelerator(
+      IDC_TOGGLE_VERTICAL_TABS_COLLAPSE, &toggle_collapse_accelerator));
+
   base::UserActionTester user_action_tester;
   tabs::VerticalTabStripStateController* const controller =
       tabs::VerticalTabStripStateController::From(browser());
@@ -540,8 +550,7 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripInteractiveUiTest,
       CheckResult([controller]() { return controller->IsCollapsed(); }, false),
 
       // Send the accelerator.
-      SendKeyPress(kBrowserViewElementId, ui::VKEY_L,
-                   ui::EF_SHIFT_DOWN | ui::EF_PLATFORM_ACCELERATOR),
+      SendAccelerator(kBrowserViewElementId, toggle_collapse_accelerator),
 
       // Wait for the collapsed event.
       WaitForEvent(kTabStripRegionElementId,
@@ -558,8 +567,7 @@ IN_PROC_BROWSER_TEST_F(VerticalTabStripInteractiveUiTest,
       }),
 
       // Send the accelerator again.
-      SendKeyPress(kBrowserViewElementId, ui::VKEY_L,
-                   ui::EF_SHIFT_DOWN | ui::EF_PLATFORM_ACCELERATOR),
+      SendAccelerator(kBrowserViewElementId, toggle_collapse_accelerator),
 
       // Wait for the expansion to complete.
       Do([controller]() {
