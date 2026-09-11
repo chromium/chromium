23564530d6571dcd58053d9823e16ac22ba17dfa
Mingjing Zhang <mjzhang@chromium.org>
2026-09-11 07:43:19 -0700

    [xr] Remove redundant SyncToken check
    This CL introduces the following changes in XRWebGLDrawingBuffer:
    
    1. Replace the if-check of back_color_buffer_ with a hard CHECK in
    ::SwapColorBuffers(). This is safe because previously, when the if-check
    failed, the null buffer would also cause a crash soon in
    TransferToSharedImageHolder() (SwapColorBuffers()'s caller). This
    replacement with CHECK merely lets the crash happen slightly earlier.
    
    2. Remove the if-check for produce_sync_token.HasData() in
    TransferToSharedImageHolder(). In the current codebase, this check is
    redundant because SyncToken comes from
    SharedImageTexture::ScopedAccess::EndAccess() and will always pass the
    check. This removal is necessary because when
    UseAutomaticSyncTokenManagement is enabled (in the future), this
    SyncToken will never pass the check (since all SyncTokens are managed
    inside ClientSharedImage).
    
    This change is a prerequisite before enabling
    UseAutomaticSyncTokenManagement.
    
    Bug: 40286368
    Change-Id: I91cd3fd8e52a95c1abe54501d1600097ed88d796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8392318
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696226}

--- third_party/blink/renderer/platform/graphics/gpu/xr_webgl_drawing_buffer.cc
+++ third_party/blink/renderer/platform/graphics/gpu/xr_webgl_drawing_buffer.cc
@@ -595,9 +595,8 @@ void XRWebGLDrawingBuffer::SwapColorBuffers() {
 
   BindAndResolveDestinationFramebuffer();
 
-  if (back_color_buffer_) {
-    back_color_buffer_->EndAccess();
-  }
+  CHECK(back_color_buffer_);
+  back_color_buffer_->EndAccess();
 
   // Swap buffers
   front_color_buffer_ = back_color_buffer_;
@@ -642,11 +641,7 @@ XRWebGLDrawingBuffer::TransferToSharedImageHolder() {
     SwapColorBuffers();
 
     buffer = front_color_buffer_;
-
-    // This should only fail if the context is lost during the buffer swap.
-    if (buffer->produce_sync_token.HasData()) {
-      success = true;
-    }
+    success = true;
   }
 
   // If we can't get a mailbox, simply crash here since the image returned
