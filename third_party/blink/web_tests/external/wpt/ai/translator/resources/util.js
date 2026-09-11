5c8a9a5038f6571d754122d75be3d512e2128813
Paula Gomez <paugomez@google.com>
2026-09-10 17:51:19 -0700

    Reland "Move Translator create user activation WPT to its own file"
    This is a reland of commit 5edb7e90d621f54db30c9777b7616d0da07c2ffb
    
    Original change's description:
    > Built-In AI: Move Translator create user activation WPT to its own file
    >
    > When running chrome_wpt_tests on bots with pre-installed or sideloaded
    > models, Translator availability starts as available. This caused the
    > downloadable precondition assertion in
    > translator.optional.https.window.js to fail, flakily failing the entire
    > test suite despite all translation functionality subtests passing.
    >
    > Bug: 510552929
    > Change-Id: I7987656d0a8eb8f24299277d927c2a8f1d9fb332
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8280374
    > Reviewed-by: Danilo Tedeschi <danft@google.com>
    > Commit-Queue: Danilo Tedeschi <danft@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1687658}
    
    Bug: 510552929
    Change-Id: I5ec38d6670c75667d33866f6ca8b82ebadd3c2e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8392297
    Reviewed-by: Danilo Tedeschi <danft@google.com>
    Commit-Queue: Paula Gómez <paugomez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1695854}

--- third_party/blink/web_tests/external/wpt/ai/translator/resources/util.js
+++ third_party/blink/web_tests/external/wpt/ai/translator/resources/util.js
@@ -2,7 +2,7 @@ async function createTranslator(options = {}) {
   if (!options.monitor) {
     const availability = await Translator.availability(options);
     assert_implements_optional(
-        availability !== 'unavailable',
+        availability === 'available',
         'Translator is not available for the given options');
   }
   await test_driver.bless();
