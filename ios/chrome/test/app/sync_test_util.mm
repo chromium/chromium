150e09e67006f3ce589d9863e3ae0b3bd33dca32
Michael Tatarski <mtatarski@google.com>
2026-09-11 07:35:27 -0700

    [iOS][Sync] Verify instance ID token in sync invalidation fields
    Update VerifySyncInvalidationFieldsPopulated in sync_test_util to check
    that instance_id_token is populated on the committed DeviceInfo entity
    in addition to interested_data_type_ids.
    
    With FakeServerSyncInvalidationSender and the test GCM profile service
    configured in EarlGrey test runs, the client registers for instance ID
    tokens, allowing this verification to succeed.
    
    Bug: 40754340
    Change-Id: I23787ea45146e6694eedcc56c89e6c0e954a6e2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8391741
    Auto-Submit: Michael Tatarski <mtatarski@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Michael Tatarski <mtatarski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1696220}

--- ios/chrome/test/app/sync_test_util.mm
+++ ios/chrome/test/app/sync_test_util.mm
@@ -326,9 +326,8 @@ bool VerifySyncInvalidationFieldsPopulated() {
     if (entity.specifics().device_info().cache_guid() == cache_guid) {
       const sync_pb::InvalidationSpecificFields& invalidation_fields =
           entity.specifics().device_info().invalidation_fields();
-      // TODO(crbug.com/40754340): check if `instance_id_token` is present once
-      // fixed.
-      return !invalidation_fields.interested_data_type_ids().empty();
+      return !invalidation_fields.instance_id_token().empty() &&
+             !invalidation_fields.interested_data_type_ids().empty();
     }
   }
   // The local DeviceInfo hasn't been committed yet.
