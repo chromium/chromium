chromium/chromium

commit 991db6ce7b0a240140913dc833a4d7691544844e

Use std::make_unique for test handlers and managers in chrome/browser
Modernize several C++ test files in the chrome/browser directory by
replacing legacy base::WrapUnique(new Foo) calls with standard
std::make_unique<Foo>().

This aligns with the modern C++ styleguide recommendation:
"base::WrapUnique(new Foo) and base::WrapUnique(new Foo()) mean something
different if Foo does not have a user-defined constructor. Don't make
future maintainers guess whether you left off the '()' on purpose. Use
std::make_unique<Foo>() instead."

The following files were modernized:
- startup_browser_creator_triggered_reset_browsertest_win.cc (startup)
- tether_notification_presenter.cc (tether notification)
- amp_page_load_metrics_observer_unittest.cc (page load metrics)

Bug: 395677330
Change-Id: Ie44bd96947ef11ddc07da12409304a338dcfb929
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8376849
Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
Reviewed-by: Darryl James <dljames@chromium.org>
Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1695648}

chrome/browser/ui/ash/network/tether_notification_presenter.cc
@@ -133,7 +133,7 @@ TetherNotificationPresenter::TetherNotificationPresenter(
     NetworkConnect* network_connect)
     : profile_(profile),
       network_connect_(network_connect),
-      settings_ui_delegate_(base::WrapUnique(new SettingsUiDelegateImpl())) {}
+      settings_ui_delegate_(std::make_unique<SettingsUiDelegateImpl>()) {}
 
 TetherNotificationPresenter::~TetherNotificationPresenter() = default;
 

chrome/browser/ui/startup/startup_browser_creator_triggered_reset_browsertest_win.cc
@@ -62,7 +62,7 @@ bool MockTriggeredProfileResetter::has_reset_trigger_ = false;
 
 std::unique_ptr<KeyedService> BuildMockTriggeredProfileResetter(
     content::BrowserContext* context) {
-  return base::WrapUnique(new MockTriggeredProfileResetter);
+  return std::make_unique<MockTriggeredProfileResetter>();
 }
 
 GURL GetTriggeredResetSettingsURL() {

chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer_unittest.cc
@@ -105,7 +105,7 @@ class AMPPageLoadMetricsObserverTest
   bool WithFencedFrames() { return GetParam(); }
 
   void RegisterObservers(page_load_metrics::PageLoadTracker* tracker) override {
-    tracker->AddObserver(base::WrapUnique(new AMPPageLoadMetricsObserver()));
+    tracker->AddObserver(std::make_unique<AMPPageLoadMetricsObserver>());
   }
 
   content::RenderFrameHost* AppendChildFrame(content::RenderFrameHost* parent,

commit 38b53c136ff05a73fea35cff547be00238089e3e

Fix IWYU issue
This CL correctly includes/removes header to satisfy IWYU
guidenline. It is a follow-up to address IWYU issues
introduced in 991db6ce7b0a24014

Bug: 395677330
Change-Id: Ib81a299fa25ad4817423bce9f3373f1f1c17d6b4
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8397886
Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
Reviewed-by: Darryl James <dljames@chromium.org>
Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1696472}

chrome/browser/ui/ash/network/tether_notification_presenter.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ui/ash/network/tether_notification_presenter.h"
 
 #include <algorithm>
+#include <memory>
 #include <string>
 
 #include "ash/constants/notifier_catalogs.h"
@@ -15,7 +16,6 @@
 #include "ash/webui/settings/public/constants/routes.mojom.h"
 #include "base/check_deref.h"
 #include "base/functional/bind.h"
-#include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"

chrome/browser/ui/startup/startup_browser_creator_triggered_reset_browsertest_win.cc
@@ -9,7 +9,6 @@
 #include "base/callback_list.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
-#include "base/memory/ptr_util.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/win/windows_version.h"
 #include "chrome/browser/browser_process.h"

chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer_unittest.cc
@@ -4,11 +4,11 @@
 
 #include "chrome/browser/page_load_metrics/observers/core/amp_page_load_metrics_observer.h"
 
+#include <memory>
 #include <optional>
 #include <string>
 #include <utility>
 
-#include "base/memory/ptr_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "chrome/browser/page_load_metrics/observers/page_load_metrics_observer_test_harness.h"