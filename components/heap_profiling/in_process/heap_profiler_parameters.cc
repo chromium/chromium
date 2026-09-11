795a6fd27e847a7def8abc7d8be0fabc81ccbe9e
Sean Maher <spvm@chromium.org>
2026-09-11 11:28:59 -0700

    make HeapProfilerChurnReporting DISABLED_BY_DEFAULT
    It was set to enabled by default by accident!
    
    Change-Id: Ia19057bbf45b3d3fbf48c92b1ce32c5c4e34aa3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8391598
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Sean Maher <spvm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696455}

--- components/heap_profiling/in_process/heap_profiler_parameters.cc
+++ components/heap_profiling/in_process/heap_profiler_parameters.cc
@@ -126,7 +126,7 @@ constexpr base::FeatureParam<double> kUtilityHashSetLoadFactor{
 }  // namespace
 
 BASE_FEATURE(kHeapProfilerReporting, base::FEATURE_ENABLED_BY_DEFAULT);
-BASE_FEATURE(kHeapProfilerChurnReporting, base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kHeapProfilerChurnReporting, base::FEATURE_DISABLED_BY_DEFAULT);
 
 const base::FeatureParam<double> kHeapChurnSubsamplingChance{
     &kHeapProfilerChurnReporting, "subsampling-chance", 0.005};
