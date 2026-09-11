b3fd28d06664a40d77ea5a6a82b4b88398227735
Camillia Smith Barnes <cammie@chromium.org>
2026-09-11 11:50:48 -0700

    WebUILocationBar: Ensure tab focus in FullscreenKeyboardBrowserTestBase
    When WebUILocationBar is enabled, asynchronous WebUI toolbar transitions
    or focus clearing during fullscreen state changes and tab loads can
    leave FocusManager without focus on the web page. This caused shortcuts
    (such as Ctrl+W) to be executed by BrowserCommandController instead of
    being delivered to the renderer to be consumed by JavaScript via
    preventDefault().
    
    Explicitly ensure focus on the active WebContents and
    VIEW_ID_TAB_CONTAINER in StartFullscreenLockPage,
    SendFullscreenShortcutAndWait, SendJsFullscreenShortcutAndWait, and
    SendEscapeAndWaitForExitingFullscreen.
    
    This should fix BrowserCommandControllerInteractiveTest.
    KeyEventsShouldBeConsumedByWebPageInBrowserFullscreen and
    BrowserCommandControllerInteractiveTest.
    KeyEventsShouldBeConsumedByWebPageInJsFullscreenExceptForEsc with
    WebUILocationBar enabled.
    
    TAG=agy
    CONV=d3ee3a2e-bded-4206-8848-b9a5c2a6668b
    
    Bug: 557268183
    Change-Id: Ic020fac61659c95cdb41fb84f3f9b2cd5b8698fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394177
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696483}

--- chrome/browser/ui/fullscreen_keyboard_browsertest_base.cc
+++ chrome/browser/ui/fullscreen_keyboard_browsertest_base.cc
@@ -19,6 +19,7 @@
 #include "chrome/browser/ui/exclusive_access/exclusive_access_context.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/toolbar/webui_test_utils.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
@@ -161,6 +162,9 @@ void FullscreenKeyboardBrowserTestBase::StartFullscreenLockPage() {
       GetEmbeddedTestServer()->GetURL(kFullscreenKeyboardLockHTML),
       WindowOpenDisposition::CURRENT_TAB,
       ui_test_utils::BROWSER_TEST_WAIT_FOR_LOAD_STOP);
+  ASSERT_NO_FATAL_FAILURE(FocusOnLastActiveBrowser());
+  GetActiveWebContents()->Focus();
+  ui_test_utils::FocusView(GetActiveBrowser(), VIEW_ID_TAB_CONTAINER);
 }
 
 void FullscreenKeyboardBrowserTestBase::SendShortcut(ui::KeyboardCode key,
@@ -226,6 +230,8 @@ void FullscreenKeyboardBrowserTestBase::SendFullscreenShortcutAndWait() {
 #if !BUILDFLAG(IS_MAC)
   waiter.Wait();
 #endif
+  GetActiveWebContents()->Focus();
+  ui_test_utils::FocusView(GetActiveBrowser(), VIEW_ID_TAB_CONTAINER);
 }
 
 void FullscreenKeyboardBrowserTestBase::SendJsFullscreenShortcutAndWait() {
@@ -236,6 +242,8 @@ void FullscreenKeyboardBrowserTestBase::SendJsFullscreenShortcutAndWait() {
   expected_result_ += "KeyS ctrl:false shift:false alt:false meta:false\n";
   waiter.Wait();
   ASSERT_TRUE(IsActiveTabFullscreen());
+  GetActiveWebContents()->Focus();
+  ui_test_utils::FocusView(GetActiveBrowser(), VIEW_ID_TAB_CONTAINER);
 }
 
 void FullscreenKeyboardBrowserTestBase::SendEscape() {
@@ -252,6 +260,8 @@ void FullscreenKeyboardBrowserTestBase::
       GetActiveBrowser(), ui::VKEY_ESCAPE, false, false, false, false));
   waiter.Wait();
   ASSERT_FALSE(IsActiveTabFullscreen());
+  GetActiveWebContents()->Focus();
+  ui_test_utils::FocusView(GetActiveBrowser(), VIEW_ID_TAB_CONTAINER);
 }
 
 void FullscreenKeyboardBrowserTestBase::SendShortcutsAndExpectPrevented() {
