addf8e456b3f428cdc0dcb3d364e843adbe95302
Satyaveer Singh <satyaveer.s@samsung.com>
2026-09-11 02:34:28 -0700

    Modernize tether_notification_presenter_unittest
    This CL modernizes TestSettingsUiDelegate creation to use
    std::make_unique() instead of a raw new paired with
    base::WrapUnique() to align with Chromium C++ guidelines.
    
    Bug: None
    Change-Id: I97c7d7eb5c81c5a921cf57a3fea49a528ad27c80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8389396
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696060}

--- chrome/browser/ui/ash/network/tether_notification_presenter_unittest.cc
+++ chrome/browser/ui/ash/network/tether_notification_presenter_unittest.cc
@@ -9,7 +9,6 @@
 #include "ash/constants/ash_features.h"
 #include "ash/public/cpp/notification_utils.h"
 #include "base/check_deref.h"
-#include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
 #include "base/test/metrics/histogram_tester.h"
@@ -108,9 +107,10 @@ class TetherNotificationPresenterTest : public BrowserWithTestWindowTest {
     notification_presenter_ = std::make_unique<TetherNotificationPresenter>(
         profile(), test_network_connect_.get());
 
-    test_settings_ui_delegate_ = new TestSettingsUiDelegate();
+    auto delegate = std::make_unique<TestSettingsUiDelegate>();
+    test_settings_ui_delegate_ = delegate.get();
     notification_presenter_->SetSettingsUiDelegateForTesting(
-        base::WrapUnique(test_settings_ui_delegate_.get()));
+        std::move(delegate));
     has_verified_metrics_ = false;
   }
 
