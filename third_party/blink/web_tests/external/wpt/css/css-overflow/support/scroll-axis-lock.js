a6380e9cf732fe57bff4df47c0216f42aa32e929
Frédéric Wang Nélar <fwang@igalia.com>
2026-09-11 23:33:25 -0700

    Extend scroll-axis-lock: none test to cover all possible scroll directions.
    Bug: 479472367
    Change-Id: Ic41756a8a4106493962c9b0688ab557c424a0d95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8352606
    Commit-Queue: Frédéric Wang Nélar <fwang@igalia.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1696823}

--- third_party/blink/web_tests/external/wpt/css/css-overflow/support/scroll-axis-lock.js
+++ third_party/blink/web_tests/external/wpt/css/css-overflow/support/scroll-axis-lock.js
@@ -7,6 +7,11 @@
  * @param {config}        Test configuration
  *   - scroller: Element whose scrolling is tested.
  *   - input_source: the input triggering the scroll ("touch" or "wheel").
+ *   - dominant_axis: the dominant axis for the scroll ("X" or "Y").
+ *   - delta_dominant_axis_sign: should be +1 if the scroll is performed from
+ *     done from top (respectively left) to bottom (respectively right) on the
+ *     dominant axis or -1 if it's done the other way around.
+ *   - delta_minor_axis_sign: same but for the minor axis.
  * @return {promise}      A promise resolved after the checks complete
  *                        successufully or is rejected in case of failure.
  *
@@ -15,32 +20,45 @@ async function check_scroll_axis_lock(test, config) {
   await waitForCompositorReady();
 
   const scroller = config.scroller;
-  await waitForScrollReset(test, scroller);
-  assert_equals(scroller.scrollLeft, 0);
-  assert_equals(scroller.scrollTop, 0);
 
   let received_wheel_event = null;
 
-  // Scroll mostly vertically, with a small horizontal component.
-  // Move left (scroll right) by 2, up (scroll down) by 150.
-  // This steep angle would normally trigger vertical railing
-  // (horizontal scroll = 0).
+  // Scroll mostly along the dominant axis, with a small component along the
+  // minor axis.
+  // This steep angle would normally trigger railing to the dominant axis.
   // With scroll-axis-lock: none, it should not rail.
   const bounds = scroller.getBoundingClientRect();
   const start_x = bounds.left + bounds.width / 2;
   const start_y = bounds.top + bounds.height / 2;
-  const delta_x = 2;
-  const delta_y = 150;
+  const delta_minor_axis = config.delta_minor_axis_sign * 2;
+  const delta_dominant_axis = config.delta_dominant_axis_sign * 150;
+  const delta_x =
+      config.dominant_axis === 'X' ? delta_dominant_axis : delta_minor_axis;
+  const delta_y =
+      config.dominant_axis === 'Y' ? delta_dominant_axis : delta_minor_axis;
+
+  const initial_scroll_left =
+      delta_x > 0 ? 0 : scroller.scrollWidth - scroller.clientWidth;
+  const initial_scroll_top =
+      delta_y > 0 ? 0 : scroller.scrollHeight - scroller.clientHeight;
+  await waitForScrollReset(test, scroller, initial_scroll_left,
+                           initial_scroll_top);
+  assert_equals(scroller.scrollLeft, initial_scroll_left,
+                'initial scroll left');
+  assert_equals(scroller.scrollTop, initial_scroll_top, 'initial scroll top');
 
   // We use tolerances because the final scroll offsets might not
   // match the input deltas exactly. For touch, the initial gesture movement is
   // affected by touch slop (which is platform-dependent), requiring a loose
-  // tolerance of 30 on the dominant Y axis. Wheel scrolls do not have slop,
-  // so we use a small tolerance of 1. For the minor X axis in the touch test,
-  // we also use a small tolerance so that locking (scrollLeft=0) is still
-  // disallowed.
-  const tolerance_x = 1;
-  const tolerance_y = (config.input_source === 'touch') ? 30 : 1;
+  // tolerance of 30 on the dominant axis. Wheel scrolls do not have slop,
+  // so we use a small tolerance of 1. For the minor axis in the touch test,
+  // we also use a small tolerance so that locking is still disallowed.
+  const tolerance_minor_axis = 1;
+  const tolerance_dominant_axis = (config.input_source === 'touch') ? 30 : 1;
+  const tolerance_x = config.dominant_axis === 'X' ? tolerance_dominant_axis :
+                                                     tolerance_minor_axis;
+  const tolerance_y = config.dominant_axis === 'Y' ? tolerance_dominant_axis :
+                                                     tolerance_minor_axis;
 
   const end_x = start_x - delta_x;
   const end_y = start_y - delta_y;
@@ -75,10 +93,10 @@ async function check_scroll_axis_lock(test, config) {
   // Wait for scroll to end.
   await scrollend_promise;
 
-  assert_approx_equals(scroller.scrollTop, delta_y, tolerance_y,
-                       'Should have scrolled vertically');
-  assert_approx_equals(scroller.scrollLeft, delta_x, tolerance_x,
-                       'Should have scrolled horizontally (not railed to Y)');
+  assert_approx_equals(scroller.scrollLeft - initial_scroll_left, delta_x,
+                       tolerance_x, 'horizontal scroll offset');
+  assert_approx_equals(scroller.scrollTop - initial_scroll_top, delta_y,
+                       tolerance_y, 'vertical scroll offset');
 
   if (config.input_source === 'wheel') {
     // The wheel delta will always be the unlocked value.
