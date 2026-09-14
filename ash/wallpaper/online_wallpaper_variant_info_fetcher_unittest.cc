b73674f4f0d240a7bc87f79c762b88a6b4f5688d
Achuith Bhandarkar <achuith@chromium.org>
2026-09-14 10:39:03 -0700

    ash: Clean up exit-time destructors in ash/wallpaper and test_support
    Fix exit-time destructors across ash/wallpaper code, unit tests,
    and test API. This enables removing no_exit_time_destructors from
    static_library("test_support") in ash/BUILD.gn.
    
    Modernize static constants to constexpr arrays and scope test
    AccountIds as test fixture member variables.
    
    TAG=agy
    CONV=e630c7ac-06eb-4a92-a894-f7de234b5aeb
    
    Bug: 40031409, 522379416
    Change-Id: I63206cdf266e1e0f40bbf815416163b900a447b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8405445
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697332}

--- ash/wallpaper/online_wallpaper_variant_info_fetcher_unittest.cc
+++ ash/wallpaper/online_wallpaper_variant_info_fetcher_unittest.cc
@@ -28,8 +28,6 @@ using ::testing::UnorderedElementsAre;
 using ::testing::UnorderedElementsAreArray;
 
 constexpr char kUser1[] = "user1@test.com";
-const AccountId kAccount1 =
-    AccountId::FromUserEmailGaiaId(kUser1, GaiaId::Literal("1111"));
 constexpr char kDummyCollectionId[] = "testCollectionId";
 
 // Returns a set of images with the given |type|.
@@ -97,6 +95,9 @@ class OnlineWallpaperVariantInfoFetcherTest : public testing::Test {
   void TearDown() override {}
 
  protected:
+  const AccountId kAccount1 =
+      AccountId::FromUserEmailGaiaId(kUser1, GaiaId::Literal("1111"));
+
   base::test::SingleThreadTaskEnvironment task_environment_;
   base::HistogramTester histogram_tester_;
 
--- ash/wallpaper/wallpaper_utils/sea_pen_metadata_utils_unittest.cc
+++ ash/wallpaper/wallpaper_utils/sea_pen_metadata_utils_unittest.cc
@@ -22,10 +22,10 @@
 namespace ash {
 namespace {
 
-std::string user_search_query = "search query";
-std::string escaped_user_search_query = "search%20query";
-std::string user_visible_query_text = "test template query text";
-std::string user_visible_query_template = "test template title";
+constexpr char kUserSearchQuery[] = "search query";
+constexpr char kEscapedUserSearchQuery[] = "search%20query";
+constexpr char kUserVisibleQueryText[] = "test template query text";
+constexpr char kUserVisibleQueryTemplate[] = "test template title";
 
 base::DictValue GetTestTemplateQueryDict(base::Time time = base::Time::Now()) {
   return base::DictValue()
@@ -46,14 +46,14 @@ base::DictValue GetTestTemplateQueryDict(base::Time time = base::Time::Now()) {
                                base::NumberToString(static_cast<int32_t>(
                                    ash::personalization_app::mojom::
                                        SeaPenTemplateOption::kFlowerTypeRose))))
-      .Set("user_visible_query_text", user_visible_query_text)
-      .Set("user_visible_query_template", user_visible_query_template);
+      .Set("user_visible_query_text", kUserVisibleQueryText)
+      .Set("user_visible_query_template", kUserVisibleQueryTemplate);
 }
 
 base::DictValue GetTestFreeformQueryDict(base::Time time = base::Time::Now()) {
   return base::DictValue()
       .Set("creation_time", base::TimeToValue(time))
-      .Set("freeform_query", escaped_user_search_query);
+      .Set("freeform_query", kEscapedUserSearchQuery);
 }
 
 base::DictValue GetTestInvalidTemplateQueryDict(
@@ -104,7 +104,7 @@ TEST_F(SeaPenMetadataUtilsTest, SeaPenTextQueryToDict) {
 
   ash::personalization_app::mojom::SeaPenQueryPtr search_query =
       ash::personalization_app::mojom::SeaPenQuery::NewTextQuery(
-          user_search_query);
+          kUserSearchQuery);
 
   base::DictValue result = SeaPenQueryToDict(search_query);
 
@@ -142,7 +142,7 @@ TEST_F(SeaPenMetadataUtilsTest, SeaPenTemplateQueryToDict) {
               ash::personalization_app::mojom::SeaPenTemplateId::kFlower,
               options,
               ash::personalization_app::mojom::SeaPenUserVisibleQuery::New(
-                  user_visible_query_text, user_visible_query_template)));
+                  kUserVisibleQueryText, kUserVisibleQueryTemplate)));
 
   base::DictValue result = SeaPenQueryToDict(search_query);
 
@@ -193,7 +193,7 @@ TEST_F(SeaPenMetadataUtilsTest,
               ash::personalization_app::mojom::SeaPenTemplateId::kFlower,
               options,
               ash::personalization_app::mojom::SeaPenUserVisibleQuery::New(
-                  user_visible_query_text, user_visible_query_template)));
+                  kUserVisibleQueryText, kUserVisibleQueryTemplate)));
 
   auto recent_image_info =
       SeaPenQueryDictToRecentImageInfo(GetTestTemplateQueryDict());
@@ -205,7 +205,7 @@ TEST_F(SeaPenMetadataUtilsTest,
        SeaPenQueryDictToRecentImageInfoValidFreeformData) {
   ash::personalization_app::mojom::SeaPenQueryPtr expected_freeform_query =
       ash::personalization_app::mojom::SeaPenQuery::NewTextQuery(
-          user_search_query);
+          kUserSearchQuery);
 
   auto recent_image_info =
       SeaPenQueryDictToRecentImageInfo(GetTestFreeformQueryDict());
@@ -333,7 +333,7 @@ TEST_F(SeaPenMetadataUtilsTest, GetIdFromInvalidFilePath) {
 TEST_F(SeaPenMetadataUtilsTest, GetQueryStringFromTextQuery) {
   auto recent_image_info =
       SeaPenQueryDictToRecentImageInfo(GetTestFreeformQueryDict());
-  EXPECT_EQ(user_search_query, GetQueryString(recent_image_info));
+  EXPECT_EQ(kUserSearchQuery, GetQueryString(recent_image_info));
 }
 
 TEST_F(SeaPenMetadataUtilsTest, GetUnescapedQueryStringFromEscapedTextQuery) {
@@ -351,7 +351,7 @@ TEST_F(SeaPenMetadataUtilsTest, GetUnescapedQueryStringFromEscapedTextQuery) {
 TEST_F(SeaPenMetadataUtilsTest, GetQueryStringFromTemplateQuery) {
   auto recent_image_info =
       SeaPenQueryDictToRecentImageInfo(GetTestTemplateQueryDict());
-  EXPECT_EQ(user_visible_query_text, GetQueryString(recent_image_info));
+  EXPECT_EQ(kUserVisibleQueryText, GetQueryString(recent_image_info));
 }
 
 TEST_F(SeaPenMetadataUtilsTest, GetQueryStringFromNullPtr) {
