1f28a76640ed1c19d2810e7ac754fa3aa668d1c2
Camillia Smith Barnes <cammie@chromium.org>
2026-09-14 10:39:22 -0700

    WebUILocationBar: Fix ProfileMenuInteractiveUiTest
    Fix ProfileMenuInteractiveUiTest.OtherProfileFocus when
    WebUILocationBar is enabled.
    
    When WebUILocationBar is enabled, the WebUI toolbar and location bar
    initialize asynchronously. Add WaitForInitialWebUIToolbar(browser())
    in ProfileMenuInteractiveUiTest::SetUpOnMainThread to ensure the
    toolbar is ready before the test sends accelerator keypresses.
    
    Additionally, on desktop platforms with Dice support, opening the
    profile menu via ProfileMenuCoordinator queries avatar button promo info
    asynchronously. Use base::test::RunUntil to wait for the profile menu
    view to be instantiated rather than asserting synchronously.
    
    TAG=agy
    CONV=1ba21281-5a09-44c1-b01b-14997a2a2108
    
    Bug: 560237315
    Change-Id: I1da1e1be1b2e098b28ed1adcfadd9fb802694f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8395784
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1697333}

--- chrome/browser/ui/views/profiles/profile_bubble_interactive_uitest.cc
+++ chrome/browser/ui/views/profiles/profile_bubble_interactive_uitest.cc
@@ -4,6 +4,7 @@
 
 #include "base/functional/callback_helpers.h"
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/run_until.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -19,6 +20,7 @@
 #include "chrome/browser/ui/views/profiles/profile_menu_view.h"
 #include "chrome/browser/ui/views/profiles/profile_menu_view_base.h"
 #include "chrome/browser/ui/views/toolbar/avatar_toolbar_button_interface.h"
+#include "chrome/browser/ui/views/toolbar/webui_test_utils.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
 #include "components/signin/public/identity_manager/account_info.h"
@@ -126,6 +128,11 @@ class ProfileMenuInteractiveUiTest : public ProfileBubbleInteractiveUiTest {
     ProfileBubbleInteractiveUiTest::SetUp();
   }
 
+  void SetUpOnMainThread() override {
+    ProfileBubbleInteractiveUiTest::SetUpOnMainThread();
+    WaitForInitialWebUIToolbar(browser());
+  }
+
   ProfileMenuViewBase* profile_menu_view() {
     auto* coordinator = ProfileMenuCoordinator::From(browser());
     return coordinator ? coordinator->GetProfileMenuViewBaseForTesting()
@@ -150,7 +157,8 @@ IN_PROC_BROWSER_TEST_F(ProfileMenuInteractiveUiTest, OtherProfileFocus) {
 #endif
   ASSERT_TRUE(ui_test_utils::SendKeyPressSync(
       browser(), ui::VKEY_M, control, /*shift=*/true, /*alt=*/false, command));
-  ASSERT_TRUE(profile_menu_view());
+  ASSERT_TRUE(
+      base::test::RunUntil([&]() { return profile_menu_view() != nullptr; }));
 
   // This test doesn't care about performing the actual menu actions, only
   // about the histogram recorded.
