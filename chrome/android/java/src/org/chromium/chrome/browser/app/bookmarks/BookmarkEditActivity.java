1e483be4a020ed44233589636417ee3f71035072
Evan Luo <evanluo@google.com>
2026-09-13 20:24:17 -0700

    [M154][bookmarks] Fix edit ghost dialog behind folder picker dialog
    Regression from recent dialog modifications, properly ensure that the
    background edit dialog doesn't show a ghost dialog behind the folder
    picker dialog by programmatically setting the background drawable as
    needed.
    
    Screen recording:
    http://screencast/cast/NjAyMjU0Mzk1MjU3NjUxMnw4MjM2YmM3OC0zMg
    
    (cherry picked from commit 7311cc152a7e0d2fc0e375361e0b75f2309098aa)
    
    Bug: 556223766, 559725070
    Change-Id: I8dae415449a48415da1f5ef9d76f88a82489875b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8393617
    Commit-Queue: Evan Luo <evanluo@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1695890}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8401367
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/branch-heads/8037@{#1047}
    Cr-Branched-From: e10b20e60f162e7ad88e4930833695a54f4e15fb-refs/heads/main@{#1689415}

--- chrome/android/java/src/org/chromium/chrome/browser/app/bookmarks/BookmarkEditActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/bookmarks/BookmarkEditActivity.java
@@ -8,6 +8,7 @@ import static org.chromium.build.NullUtil.assumeNonNull;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -89,6 +90,7 @@ public class BookmarkEditActivity extends SnackbarActivity {
     private @Nullable BookmarkId mInitialParentId;
     private boolean mIsFolder;
     private boolean mOutcomeRecorded;
+    private boolean mFolderPickerActive;
 
     private @Nullable EdgeToEdgePadAdjuster mEdgeToEdgePadAdjuster;
     private @Nullable BookmarkUiPrefs mBookmarkUiPrefs;
@@ -398,6 +400,7 @@ public class BookmarkEditActivity extends SnackbarActivity {
                 ImprovedBookmarkRowProperties.ROW_CLICK_LISTENER,
                 () -> {
                     BookmarkEditMetrics.recordFolderPickerOpened();
+                    mFolderPickerActive = true;
                     setDialogContentVisible(false);
                     mBookmarkManagerOpener.startFolderPickerActivity(
                             /* context= */ this, mProfile, mBookmarkId);
@@ -424,7 +427,9 @@ public class BookmarkEditActivity extends SnackbarActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        setDialogContentVisible(true);
+        if (!mFolderPickerActive) {
+            setDialogContentVisible(true);
+        }
     }
 
     /**
@@ -439,17 +444,26 @@ public class BookmarkEditActivity extends SnackbarActivity {
         if (BookmarkUtils.isDesktopBookmarksDialogEnabled()) {
             findViewById(android.R.id.content)
                     .setVisibility(visible ? View.VISIBLE : View.INVISIBLE);
+            if (visible) {
+                getWindow().setBackgroundDrawableResource(R.drawable.dialog_bg_no_shadow);
+            } else {
+                getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            }
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == FOLDER_PICKER_REQUEST_CODE
-                && resultCode == BookmarkFolderPickerActivity.RESULT_DISMISS_ALL) {
-            finish();
-            if (BookmarkUtils.isDesktopBookmarksDialogEnabled()) {
-                overridePendingTransition(0, 0);
+        if (requestCode == FOLDER_PICKER_REQUEST_CODE) {
+            mFolderPickerActive = false;
+            if (resultCode == BookmarkFolderPickerActivity.RESULT_DISMISS_ALL) {
+                finish();
+                if (BookmarkUtils.isDesktopBookmarksDialogEnabled()) {
+                    overridePendingTransition(0, 0);
+                }
+            } else {
+                setDialogContentVisible(true);
             }
         }
     }
