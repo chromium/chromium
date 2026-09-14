b44a3868a233f76383ded2282f04944677a550e1
Hiroshige Hayashizaki <hiroshige@chromium.org>
2026-09-14 15:11:43 -0700

    Set EmptyWebWorkerFetchContext for worker unit tests
    The fetch context is needed for https://crrev.com/c/7872921 to migrate
    tests to go through off-the-main-thread worker script fetch.
    
    Bug: 40572995, 400455021
    Change-Id: Ib86e78e252b992d96b73d4518b377f22c9eac729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7872633
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697587}

--- third_party/blink/renderer/core/workers/global_scope_creation_params.cc
+++ third_party/blink/renderer/core/workers/global_scope_creation_params.cc
@@ -13,6 +13,7 @@
 #include "third_party/blink/public/common/features.h"
 #include "third_party/blink/public/mojom/script/script_type.mojom-blink.h"
 #include "third_party/blink/renderer/core/frame/settings.h"
+#include "third_party/blink/renderer/core/loader/empty_clients.h"
 #include "third_party/blink/renderer/platform/network/content_security_policy_parsers.h"
 
 namespace blink {
@@ -140,7 +141,7 @@ GlobalScopeCreationParams::CreateForWorkerForTesting(
   return std::make_unique<GlobalScopeCreationParams>(
       script_url, mojom::blink::ScriptType::kClassic, "fake global scope name",
       "fake user agent", UserAgentMetadata(),
-      /*web_worker_fetch_context=*/nullptr,
+      base::MakeRefCounted<EmptyWebWorkerFetchContext>(),
       Vector<network::mojom::blink::ContentSecurityPolicyPtr>(),
       Vector<network::mojom::blink::ContentSecurityPolicyPtr>(),
       network::mojom::ReferrerPolicy::kDefault,
