662ca75a84796e44a8a056ab9d1a67ca85b64f17
Adem Derinel <derinel@google.com>
2026-09-14 14:02:03 -0700

    [M154] [WebAuthn][Android] Do not pass SMART_CARD transport to GmsCore
    Original change's description:
    > [WebAuthn][Android] Do not pass SMART_CARD transport to GmsCore
    >
    > This is a temporary workaround for crbug.com/555599813.
    >
    > GmsCore fails to unmarshal AuthenticatorTransport.SMART_CARD and hangs
    > when allowCredentials[].transports contains smart-card (or when
    > transports is omitted by the RP, causing Blink to populate all known
    > transports including smart-card).
    >
    > Filter SMART_CARD out in Fido2Api when passing transports to GmsCore,
    > gated behind kWebAuthnFilterSmartCardTransport.
    >
    > TAG=agy
    > CONV=cb281aa3-451b-4626-ae72-5c0bd19820f7
    >
    > Bug: 555599813
    > Change-Id: I8c63160e2a4a7ab438c9638174ede26da60f4d4c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8385662
    > Commit-Queue: Adem Derinel <derinel@google.com>
    > Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1695533}
    
    (cherry picked from commit 9e8041fc04842129237a3cb80e63de134c0a4a4c)
    
    Bug: 560033489,555599813
    Change-Id: I8c63160e2a4a7ab438c9638174ede26da60f4d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8407265
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8037@{#1104}
    Cr-Branched-From: e10b20e60f162e7ad88e4930833695a54f4e15fb-refs/heads/main@{#1689415}

--- components/webauthn/android/webauthn_feature_map.cc
+++ components/webauthn/android/webauthn_feature_map.cc
@@ -16,6 +16,7 @@ namespace {
 // Array of features exposed through the Java WebauthnFeatureMap API.
 const base::Feature* const kFeaturesExposedToJava[] = {
     &kWebAuthnAndroidCredManForDev,
+    &kWebAuthnFilterSmartCardTransport,
 };
 
 // static
