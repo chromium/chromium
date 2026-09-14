3d638fcec8912f15b5c29f534187992767519e42
Sophie Crowley <crowleso@google.com>
2026-09-14 02:52:42 -0700

    [Signin] Disable SigninButtonProfileMenu for signin button click tests
    These tests check for the opening of the bottom sheet or settings,
    whereas on desktop if SigninButtonProfileMenu is enabled an account menu
    should show up instead. The appearance of this menu is separately
    tested. With the flag enabled by default, these signin button click
    tests fail.
    
    Bug: 560160039, 551756560
    Change-Id: Iede86103c6bab8143d015a7064748195d1c8e30c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8398053
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Sophie Crowley <crowleso@google.com>
    Commit-Queue: Sophie Crowley <crowleso@google.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697143}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/signin_button/SigninButtonCoordinatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/signin_button/SigninButtonCoordinatorTest.java
@@ -407,6 +407,9 @@ public class SigninButtonCoordinatorTest {
     @Test
     @MediumTest
     @EnableFeatures(SigninFeatures.ENABLE_SEAMLESS_SIGNIN)
+    // TODO(crbug.com/551756560): Once SIGNIN_BUTTON_PROFILE_MENU is launched, restrict
+    // this test by form factor instead of disabling the flag.
+    @DisableFeatures(SigninFeatures.SIGNIN_BUTTON_PROFILE_MENU)
     public void testClickSigninButton_SignedOut() {
         startActivityOnNtp();
 
@@ -422,7 +425,12 @@ public class SigninButtonCoordinatorTest {
 
     @Test
     @MediumTest
-    @DisableFeatures(SigninFeatures.ENABLE_SEAMLESS_SIGNIN)
+    // TODO(crbug.com/551756560): Once SIGNIN_BUTTON_PROFILE_MENU is launched, restrict
+    // this test by form factor instead of disabling the flag.
+    @DisableFeatures({
+        SigninFeatures.ENABLE_SEAMLESS_SIGNIN,
+        SigninFeatures.SIGNIN_BUTTON_PROFILE_MENU
+    })
     public void testClickSigninButton_SignedOut_SeamlessSigninDisabled() {
         startActivityOnNtp();
 
@@ -441,6 +449,9 @@ public class SigninButtonCoordinatorTest {
 
     @Test
     @MediumTest
+    // TODO(crbug.com/551756560): Once SIGNIN_BUTTON_PROFILE_MENU is launched, restrict
+    // this test by form factor instead of disabling the flag.
+    @DisableFeatures(SigninFeatures.SIGNIN_BUTTON_PROFILE_MENU)
     public void testClickSigninButton_SignedOut_SigninDisabled() {
         startActivityOnNtp();
 
@@ -462,6 +473,9 @@ public class SigninButtonCoordinatorTest {
     // is the min version that supports split stores UPM backend, to avoid
     // UserActionableError.NEEDS_UPM_BACKEND_UPGRADE.
     @Restriction(GmsCoreVersionRestriction.RESTRICTION_TYPE_VERSION_GE_24W15)
+    // TODO(crbug.com/551756560): Once SIGNIN_BUTTON_PROFILE_MENU is launched, restrict
+    // this test by form factor instead of disabling the flag.
+    @DisableFeatures(SigninFeatures.SIGNIN_BUTTON_PROFILE_MENU)
     public void testClickSigninButton_SignedIn() {
         startActivityOnNtp();
 
