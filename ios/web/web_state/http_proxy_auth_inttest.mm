e28bdc9b0843bbf1222aae4d0f4c42eb5b7c5616
Cheick Cisse <cheickcisse@google.com>
2026-09-14 12:05:14 -0700

    [Gardener] Skip HTTP proxy auth inttests on physical devices
    These tests fail on every chrome/ci/iphone-device and chrome/ci/ipad-device
    build, while passing consistently on every simulator builder:
    
    * HttpProxyAuthTest.SuccessfulProxyAuth
    * HttpProxyAuthTest.ProxyAuthUserCancelled
    * HttpProxyAuthTest.ProxyAuthCustomCancellationError
    * HttpProxyAuthTest.ProxyAuthFallbackToOnAuthRequired
    * HttpProxyAuthDisabledFeatureTest.ProxyAuthChallengeNotTriggered
    
    Scope the fixture setup to simulators so the device bots stop failing while
    simulator coverage is preserved.
    
    Bug: 561568617
    Change-Id: If009fb229ab77d77dc08ef54b446c852cfdd8c57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8405232
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Owners-Override: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Xu Li <mxlg@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697407}

--- ios/web/web_state/http_proxy_auth_inttest.mm
+++ ios/web/web_state/http_proxy_auth_inttest.mm
@@ -129,6 +129,7 @@ class HttpProxyAuthTest : public WebTestWithWebState {
   }
 
   void SetUp() override {
+#if TARGET_OS_SIMULATOR
     if (!@available(iOS 18.1, *)) {
       GTEST_SKIP() << "Proxy auth challenges require iOS 18.1+.";
     }
@@ -140,6 +141,10 @@ class HttpProxyAuthTest : public WebTestWithWebState {
     WebTestWithWebState::SetUp();
     delegate_ = std::make_unique<FakeWebStateDelegate>();
     web_state()->SetDelegate(delegate_.get());
+#else
+    // TODO(crbug.com/561568617): Re-enable
+    GTEST_SKIP() << "Proxy auth challenges are only testable on simulators.";
+#endif
   }
 
   void TearDown() override {
