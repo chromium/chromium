43c2f236f9998b2266f2cdb7ff9d1816dda06973
Eriko Kurimoto <elkurin@chromium.org>
2026-09-13 23:20:09 -0700

    CacheStorage: Remove BACKEND_OPEN assertions in Put and Delete
    CacheStorageCache::Put and CacheStorageCache::Delete previously asserted
    that the backend state was BACKEND_OPEN (or initializing). However,
    these methods can be called asynchronously after a quota check in
    BatchDidGetBucketSpaceRemaining. If the cache is closed while waiting
    for the quota check to complete, these assertions fail.
    
    This CL removes the assertions. This is safe because the scheduled inner
    implementations (PutImpl and DeleteImpl) already explicitly check the
    backend state when they execute and will fail gracefully if it is
    closed.
    
    Bug: 557615001, 558089492
    Change-Id: I113c884cbe87e67c7dcaab9769a586b3b3aa214e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8395566
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697063}

--- content/browser/cache_storage/cache_storage_cache.cc
+++ content/browser/cache_storage/cache_storage_cache.cc
@@ -1755,9 +1755,6 @@ void CacheStorageCache::WriteSideDataComplete(
 void CacheStorageCache::Put(blink::mojom::BatchOperationPtr operation,
                             int64_t trace_id,
                             ErrorCallback callback) {
-  // TODO(crbug.com/558089492): CHECK-exclusion: Convert to a CHECK once we
-  // are confident it won't be triggered.
-  DCHECK(BACKEND_OPEN == backend_state_ || initializing_);
   CHECK_EQ(blink::mojom::OperationType::kPut, operation->operation_type,
            base::NotFatalUntil::M158);
   Put(std::move(operation->request), std::move(operation->response), trace_id,
@@ -1768,10 +1765,6 @@ void CacheStorageCache::Put(blink::mojom::FetchAPIRequestPtr request,
                             blink::mojom::FetchAPIResponsePtr response,
                             int64_t trace_id,
                             ErrorCallback callback) {
-  // TODO(crbug.com/557615001): CHECK-exclusion: Convert to a CHECK once we are
-  // confident it won't be triggered.
-  DCHECK(BACKEND_OPEN == backend_state_ || initializing_);
-
   auto put_context = cache_entry_handler_->CreatePutContext(
       std::move(request), std::move(response), trace_id);
   auto id = scheduler_->CreateId();
@@ -2307,8 +2300,6 @@ CacheStorageCache::InitState CacheStorageCache::GetInitState() const {
 
 void CacheStorageCache::Delete(blink::mojom::BatchOperationPtr operation,
                                ErrorCallback callback) {
-  CHECK(BACKEND_OPEN == backend_state_ || initializing_,
-        base::NotFatalUntil::M158);
   CHECK_EQ(blink::mojom::OperationType::kDelete, operation->operation_type,
            base::NotFatalUntil::M158);
 
