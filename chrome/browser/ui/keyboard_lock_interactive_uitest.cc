5500122e2df188287a75021532ffb58b5baf3657
Camillia Smith Barnes <cammie@chromium.org>
2026-09-14 10:32:02 -0700

    WebUILocationBar: Fix KeyboardLockInteractiveBrowserTest
    Fix KeyboardLockInteractiveBrowserTest with WebUILocationBar enabled.
    
    In SubsequentLockCallSupersedesPreviousCall, creating and closing a new
    tab did not restore focus to the original tab's web contents.
    Because RenderWidgetHostImpl::LockKeyboard() only activates when both
    the tab is in fullscreen and is_focused_ is true, clicking on
    VIEW_ID_TAB_CONTAINER gives it native focus, allowing the fullscreen
    shortcut and keyboard lock to engage.
    
    Also update WaitForKeyboardLock() to wait using base::test::RunUntil()
    until IsKeyboardLockActive() returns true, ensuring the view-level lock
    state is properly established.
    
    TAG=agy
    CONV=a7224b11-cf9d-4e73-808c-f8f150dd9d66
    
    Bug: 557268106
    Change-Id: Ie7430e3b2b036334820dfbc78783da9bbf080bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8378029
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697326}

--- chrome/browser/ui/keyboard_lock_interactive_uitest.cc
+++ chrome/browser/ui/keyboard_lock_interactive_uitest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "base/run_loop.h"
+#include "base/test/run_until.h"
 #include "base/test/scoped_feature_list.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -10,6 +11,7 @@
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/browser/ui/exclusive_access/exclusive_access_manager.h"
 #include "chrome/browser/ui/fullscreen_keyboard_browsertest_base.h"
+#include "chrome/browser/ui/view_ids.h"
 #include "chrome/test/base/chrome_test_path_utils.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
@@ -206,16 +208,16 @@ bool KeyboardLockInteractiveBrowserTest::RequestKeyboardLock(
 // JS so that waiting for the promise to resolve in RequestKeyboardLock() is
 // sufficient and WaitForKeyboardLock() can be deleted.
 void KeyboardLockInteractiveBrowserTest::WaitForKeyboardLock() {
-  if (GetExclusiveAccessManager()
-          ->keyboard_lock_controller()
-          ->IsKeyboardLockActive()) {
-    return;
+  if (!GetExclusiveAccessManager()
+           ->keyboard_lock_controller()
+           ->IsKeyboardLockActive()) {
+    base::RunLoop run_loop;
+    GetExclusiveAccessManager()
+        ->keyboard_lock_controller()
+        ->set_lock_state_callback_for_test(run_loop.QuitClosure());
+    run_loop.Run();
   }
-  base::RunLoop run_loop;
-  GetExclusiveAccessManager()
-      ->keyboard_lock_controller()
-      ->set_lock_state_callback_for_test(run_loop.QuitClosure());
-  run_loop.Run();
+  EXPECT_TRUE(base::test::RunUntil([&]() { return IsKeyboardLockActive(); }));
 }
 
 bool KeyboardLockInteractiveBrowserTest::RequestAndWaitForKeyboardLock(
@@ -390,6 +392,7 @@ IN_PROC_BROWSER_TEST_F(KeyboardLockInteractiveBrowserTest,
   // request fullscreen again.
   ASSERT_FALSE(IsActiveTabFullscreen());
   ASSERT_TRUE(IsKeyboardLockRequestRegistered());
+  ui_test_utils::ClickOnView(GetActiveBrowser(), VIEW_ID_TAB_CONTAINER);
   ASSERT_NO_FATAL_FAILURE(SendJsFullscreenShortcutAndWaitForKeyboardLock());
   ASSERT_TRUE(IsActiveTabFullscreen());
   ASSERT_TRUE(IsKeyboardLockActive());
