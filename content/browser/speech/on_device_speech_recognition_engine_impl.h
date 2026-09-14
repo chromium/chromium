aa3c0c205513ece6496a9f95c7569f78dcb12ff2
Lei Zhang <thestig@chromium.org>
2026-09-14 12:12:58 -0700

    Forward declare more in on_device_speech_recognition_engine_impl.h
    Since ModelBrokerClient is already being forward declared in this
    header, do not include model_broker_client.h. Then forward declare
    ModelClient to fix the build.
    
    Change-Id: I8be034f9fdee43a4bf6fd52da5e6ec54c3f6b8ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8394906
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697414}

--- content/browser/speech/on_device_speech_recognition_engine_impl.h
+++ content/browser/speech/on_device_speech_recognition_engine_impl.h
@@ -13,7 +13,6 @@
 #include "base/task/sequenced_task_runner.h"
 #include "base/threading/sequence_bound.h"
 #include "components/optimization_guide/core/delivery/optimization_guide_model_provider.h"
-#include "components/optimization_guide/core/model_execution/model_broker_client.h"
 #include "content/browser/speech/speech_recognition_engine.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/speech_recognition_audio_forwarder_config.h"
@@ -28,6 +27,7 @@
 
 namespace optimization_guide {
 class ModelBrokerClient;
+class ModelClient;
 }  // namespace optimization_guide
 
 namespace content {
