b333578024155b5d479e4ce993388d8c167807c9
Jiacheng Guo <gjc@google.com>
2026-09-13 21:48:27 -0700

    Remove DeferSpeculativeRFHCreation field trial
    Removes the DeferSpeculativeRFHCreation field trial configuration from
    fieldtrial_testing_config.json as well as its exclusion in
    content_unittests.
    
    Bug: 556520606
    Change-Id: Ifd9568f864443250a0d245c046bf98d3b8e43cb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8351104
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1697042}

--- content/test/content_test_suite.cc
+++ content/test/content_test_suite.cc
@@ -98,10 +98,6 @@ ContentTestSuite::ContentTestSuite(int argc, char** argv)
   // Reproducer:FileSystemAccessFileHandleImplRequestPermissionTest.RequestWrite_Denied
   disable_field_trial("TrackEmptyRendererProcessesForReuse");
 
-  // TODO(447305320) Enable testing for this field trial feature.
-  // Reproducer:NavigatorTest.NoContent
-  disable_field_trial("DeferSpeculativeRFHCreation");
-
   // TODO(447307242) Enable testing for this field trial feature.
   // Reproducer:PrerendererTest.RemoveRendererHostAfterCandidateRemoved
   disable_field_trial("LCPTimingPredictorPrerender2");
