15f2656ea64ea28ebe81f7ff5b49b8e0ca005dcc
Jordan Bayles <jophba@chromium.org>
2026-09-15 16:40:56 -0700

    [symphonia] Enable Symphonia audio decoding for PCM and Vorbis by default
    Default-enables kSymphoniaPcmDecoding and kSymphoniaVorbisDecoding
    across all platforms, launching Symphonia-based audio decoding for PCM
    and Vorbis.
    
    Also removes SymphoniaPcmDecoding and SymphoniaVorbisDecoding entries
    from fieldtrial_testing_config.json now that both features are enabled
    by default everywhere.
    
    TAG=agy
    CONV=236af6ef-07cf-4411-85b3-1ae0de08c0e4
    
    Bug: 476202822, 476202823
    Change-Id: I9494b8f20ddc88c148ac37f80f35687b22937a7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8380969
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698504}

--- third_party/blink/renderer/modules/webcodecs/audio_decoder_broker_test.cc
+++ third_party/blink/renderer/modules/webcodecs/audio_decoder_broker_test.cc
@@ -20,7 +20,11 @@
 #include "media/base/sample_format.h"
 #include "media/base/test_data_util.h"
 #include "media/base/test_helpers.h"
+#include "media/media_buildflags.h"
 #include "media/mojo/buildflags.h"
+#if BUILDFLAG(ENABLE_SYMPHONIA)
+#include "media/filters/symphonia_audio_decoder.h"
+#endif
 #include "media/mojo/mojom/audio_decoder.mojom.h"
 #include "media/mojo/mojom/interface_factory.mojom.h"
 #include "media/mojo/services/interface_factory_impl.h"
@@ -322,7 +326,14 @@ TEST_F(AudioDecoderBrokerTest, Decode_NoMojoDecoder) {
   EXPECT_EQ(GetDecoderType(), media::AudioDecoderType::kBroker);
 
   InitializeDecoder(MakeVorbisConfig());
+#if BUILDFLAG(ENABLE_SYMPHONIA)
+  EXPECT_EQ(GetDecoderType(),
+            media::SymphoniaAudioDecoder::IsCodecSupported(kCodec)
+                ? media::AudioDecoderType::kSymphonia
+                : media::AudioDecoderType::kFFmpeg);
+#else
   EXPECT_EQ(GetDecoderType(), media::AudioDecoderType::kFFmpeg);
+#endif
 
   DecodeBuffer(
       media::ReadTestDataFile("vorbis-packet-0", base::Milliseconds(0)));
