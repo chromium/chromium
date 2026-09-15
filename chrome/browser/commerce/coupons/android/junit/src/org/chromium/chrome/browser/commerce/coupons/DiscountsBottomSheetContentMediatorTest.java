b80889041ae26980b3d3db88450cb1ad05c0d048
utzcoz <utzcoz@gmail.com>
2026-09-15 15:40:03 -0700

    [Android] Fix and re-enable Discounts contentReady test
    The test's expiry time is 2024-09-20 01:12:32 UTC and is formatted in
    the default time zone, so it shows 09/19 in zones behind UTC and fails.
    Pin the time zone to UTC in the test.
    
    Bug: 393301380
    Change-Id: If5c8fb3fb52942b1dca63165768382072c8272fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8350010
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698438}

--- chrome/browser/commerce/coupons/android/junit/src/org/chromium/chrome/browser/commerce/coupons/DiscountsBottomSheetContentMediatorTest.java
+++ chrome/browser/commerce/coupons/android/junit/src/org/chromium/chrome/browser/commerce/coupons/DiscountsBottomSheetContentMediatorTest.java
@@ -34,7 +34,6 @@ import org.robolectric.Robolectric;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.chrome.browser.commerce.ShoppingServiceFactory;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -49,6 +48,7 @@ import org.chromium.url.JUnitTestGURLs;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 /** Unit Tests for {@link DiscountsBottomSheetContentMediator}. */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -95,8 +95,9 @@ public class DiscountsBottomSheetContentMediatorTest {
     }
 
     @Test
-    @DisabledTest(message = "https://crbug.com/393301380")
     public void testRequestShowContent_contentReady() {
+        // The expiry date is formatted in the default time zone.
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         List<DiscountInfo> infoList = createDiscountInfoList();
         setShoppingServiceGetDiscountInfoForUrl(infoList);
 
