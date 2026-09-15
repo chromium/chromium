1033a4df6e7f1f2449095fad077b118678fd20dd
Vasiliy Telezhnikov <vasilyt@chromium.org>
2026-09-15 13:27:55 -0700

    Use ExportedCanvasResource in PrepareTransferableResource
    When we early out, we need to tell CanvasResource that we're dropping
    ref. Instead of doing it directly, we should use ExportedCanvasResource
    that will do it in dtor. [1]
    
    Follow-up will stream-line the code.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/exported_canvas_resource.cc;drc=579382d9cb8a93cc0d78873e8c27425d74f185e5;l=25
    
    Change-Id: I67b363dca6f07530521adb7b70a1e4676a3b8256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8395392
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698342}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -289,7 +289,8 @@ bool HTMLCanvasElement::PrepareTransferableResource(
 
   if (!frame->PrepareTransferableResource(out_resource,
                                           /*needs_verified_synctoken=*/false)) {
-    CanvasResource::DropRefOnOwningThread(std::move(frame));
+    auto exported_resource =
+        base::MakeRefCounted<ExportedCanvasResource>(std::move(frame));
     return false;
   }
   // TODO(https://crbug.com/1475955): HDR metadata should be propagated to
@@ -303,7 +304,8 @@ bool HTMLCanvasElement::PrepareTransferableResource(
     // If the resource did not change, the release will be handled correctly
     // when the callback from the previous frame is dispatched. But we need to
     // drop ref to the current resource.
-    CanvasResource::DropRefOnOwningThread(std::move(frame));
+    auto exported_resource =
+        base::MakeRefCounted<ExportedCanvasResource>(std::move(frame));
     return false;
   }
   // Note: frame is kept alive via a reference kept in out_release_callback.
