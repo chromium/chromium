fc97e8b7b792b473fc9172b0c40b936866ece1f1
Miyoung Shin <myid.shin@igalia.com>
2026-09-14 23:06:15 -0700

    Drop extension_tab_util.h in processes_api.cc
    This prepares the processes API for migration to //extensions.
    
    This patch replaces ExtensionTabUtil::GetTabById with
    ExtensionsBrowserClient::IsValidTabId.
    It also removes the unused Profile include.
    
    Bug: 358567092
    Change-Id: I5d218548c087cf456de10d1de180226630c22963
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8396728
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Miyoung Shin <myid.shin@igalia.com>
    Reviewed-by: Julie Jeongeun Kim <jkim@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1697845}

--- chrome/browser/extensions/api/processes/processes_api.cc
+++ chrome/browser/extensions/api/processes/processes_api.cc
@@ -18,8 +18,6 @@
 #include "base/process/process.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/extensions/extension_tab_util.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/task_manager/task_manager_interface.h"
 #include "chrome/common/extensions/api/processes.h"
 #include "components/sessions/core/session_id.h"
@@ -33,6 +31,7 @@
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/result_codes.h"
 #include "extensions/browser/api/constants.h"
+#include "extensions/browser/extensions_browser_client.h"
 #include "extensions/common/error_utils.h"
 #include "third_party/blink/public/mojom/devtools/console_message.mojom.h"
 
@@ -462,9 +461,9 @@ ExtensionFunction::ResponseAction ProcessesGetProcessIdForTabFunction::Run() {
 
   const int tab_id = params->tab_id;
   content::WebContents* contents = nullptr;
-  if (!ExtensionTabUtil::GetTabById(tab_id, browser_context(),
-                                    include_incognito_information(),
-                                    &contents)) {
+  if (!ExtensionsBrowserClient::Get()->IsValidTabId(
+          browser_context(), tab_id, include_incognito_information(),
+          &contents)) {
     return RespondNow(Error(kTabNotFoundError, base::NumberToString(tab_id)));
   }
 
