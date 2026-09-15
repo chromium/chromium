2379c966825102b21932a75c4e6cbd4c9d1e038b
Radu Nitescu <radunitescu@google.com>
2026-09-15 13:25:37 -0700

    [iOS] Avoid unnecessarily reloading input views in omnibox text view
    This addresses a hang in composebox, by only reloading the input views
    when the state of the button changes.
    
    Fixed: 560393979
    Change-Id: I943f58939cfc2b9bcd156e3db3289aa868feffea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8395920
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698340}

--- ios/chrome/browser/omnibox/ui/omnibox_text_field_ios.mm
+++ ios/chrome/browser/omnibox/ui/omnibox_text_field_ios.mm
@@ -1123,6 +1123,10 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
 }
 
 - (void)forceDisableReturnKey:(BOOL)forceDisable {
+  if (_forceDisableReturnKey == forceDisable) {
+    return;
+  }
+
   _forceDisableReturnKey = forceDisable;
   [self reloadInputViews];
 }
