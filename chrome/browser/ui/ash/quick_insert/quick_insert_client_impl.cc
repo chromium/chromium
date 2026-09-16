b9e655af447a5c3d2f213491aae9c5c689ef75ac
Tiago Vignatti <tvignatti@igalia.com>
2026-09-16 07:58:23 -0700

    [ash] quick_insert: drop chrome/browser/history dependency
    This CL drops chrome/browser/history from DEPS and BUILD.gn as no
    production code uses it anymore.
    
    Bug: 558505627
    Change-Id: Ibaa210105ec05fdfaeee4522af05b9016fda8a76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8367006
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Tiago Vignatti <tvignatti@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1698935}

--- chrome/browser/ui/ash/quick_insert/quick_insert_client_impl.cc
+++ chrome/browser/ui/ash/quick_insert/quick_insert_client_impl.cc
@@ -44,7 +44,6 @@
 #include "chrome/browser/ash/file_manager/fileapi_util.h"
 #include "chrome/browser/ash/input_method/editor_mediator_factory.h"
 #include "chrome/browser/ash/lobster/lobster_service_provider.h"
-#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/quick_insert/quick_insert_file_suggester.h"
 #include "chrome/browser/ui/ash/quick_insert/quick_insert_thumbnail_loader.h"
@@ -55,6 +54,7 @@
 #include "chromeos/ash/components/editor_menu/public/cpp/editor_mode.h"
 #include "chromeos/ash/components/editor_menu/public/cpp/preset_text_query.h"
 #include "chromeos/ash/components/favicon/favicon_service_provider.h"
+#include "chromeos/ash/components/history/history_service_provider.h"
 #include "chromeos/ash/components/search_engines/template_url_service_provider.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/user_manager/user.h"
@@ -514,8 +514,8 @@ void QuickInsertClientImpl::ActiveUserChanged(user_manager::User* active_user) {
 }
 
 history::HistoryService* QuickInsertClientImpl::GetHistoryService() {
-  return HistoryServiceFactory::GetForProfile(
-      profile_, ServiceAccessType::EXPLICIT_ACCESS);
+  return ash::HistoryServiceProvider::Get().Find(CHECK_DEREF(
+      ash::AnnotatedAccountId::Get(profile_->GetOriginalProfile())));
 }
 
 favicon::FaviconService* QuickInsertClientImpl::GetFaviconService() {
