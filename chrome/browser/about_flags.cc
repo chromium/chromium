2fc562b0751559177cd4bda6186d9734754a3e91
Ananya Seelam <ananyaseelam@google.com>
2026-09-16 11:24:46 -0700

    [context-input] Remove non-deselection flag options for composebox context management
    This CL removes the chrome://flags variations for #context-management-in-composebox that do not include tab deselection, as they are no longer candidates for launch.
    
    Bug: b:562465269
    Change-Id: Ia15d5fec4558e6d48f4ec5f562c01816eb344899
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8414271
    Reviewed-by: James Leung <jamesleung@google.com>
    Commit-Queue: Ananya Seelam <ananyaseelam@google.com>
    Auto-Submit: Ananya Seelam <ananyaseelam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699106}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1429,11 +1429,6 @@ const FeatureEntry::FeatureVariation
          nullptr}};
 
 const FeatureEntry::FeatureParam
-    kContextManagementKeepMenuOpenOnTabSelectForRealboxParams[] = {
-        {"ContextManagementInComposebox", "true"},
-        {"KeepMenuOpenOnTabSelectForRealboxComposebox", "true"}};
-
-const FeatureEntry::FeatureParam
     kContextManagementEnableTabDeselectionParams[] = {
         {"enable_tab_deselection", "true"}};
 
@@ -1452,11 +1447,6 @@ const FeatureEntry::FeatureParam
 // and thus the context menu and 'realbox closes menu on tab select'.
 const FeatureEntry::FeatureVariation
     kContextManagementInComposeboxVariations[] = {
-        {"Context Management in composebox (realbox closes menu on tab select)",
-         {}},
-        {"Context management in composebox (realbox keeps menu open on tab "
-         "select)",
-         kContextManagementKeepMenuOpenOnTabSelectForRealboxParams, nullptr},
         {"Context Management in composebox (with tab deselection)",
          kContextManagementEnableTabDeselectionParams, nullptr},
         {"Context Management in composebox (keeps menu open, with tab "
