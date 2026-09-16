4d58716da182a8b4151d1aac36822ceaa8e4f1ff
Greg Thompson <grt@chromium.org>
2026-09-16 00:50:15 -0700

    [elevated tracing service] Stop using IFastRundown
    This was causing a shutdown crash due to a race between the process
    watcher and COM's fast rundown.
    
    Bug: 560237720
    Change-Id: Ieb693e2c3e4cd8b734c05b4f8f290ad9044b3a6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8399187
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698713}

--- chrome/windows_services/elevated_tracing_service/system_tracing_session.h
+++ chrome/windows_services/elevated_tracing_service/system_tracing_session.h
@@ -43,8 +43,7 @@ class SystemTracingSession
           ISystemTraceSessionChrome,
           ISystemTraceSessionChromeBeta,
           ISystemTraceSessionChromeDev,
-          ISystemTraceSessionChromeCanary,
-          IFastRundown> {
+          ISystemTraceSessionChromeCanary> {
  public:
   SystemTracingSession();
   SystemTracingSession(const SystemTracingSession&) = delete;
