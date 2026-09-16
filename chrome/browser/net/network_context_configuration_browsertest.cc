e1112dddaea53f2d6537e145a15de38cad6294a6
Bruno Braga <brunobraga@google.com>
2026-09-16 08:20:44 -0700

    [gardener] Disable flaky test
    Bug: 540592485
    Change-Id: I6040387272eb358e8330add093d297cd061b40d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8409410
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Owners-Override: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Talita Halboth <talitahalboth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698955}

--- chrome/browser/net/network_context_configuration_browsertest.cc
+++ chrome/browser/net/network_context_configuration_browsertest.cc
@@ -2080,8 +2080,9 @@ class NetworkContextConfigurationProxySettingsBrowserTest
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
+// TODO(crbug.com/540592485): Enabled test once flakiness is fixed.
 IN_PROC_BROWSER_TEST_P(NetworkContextConfigurationProxySettingsBrowserTest,
-                       MaxConnectionsPerProxy) {
+                       DISABLED_MaxConnectionsPerProxy) {
   RunMaxConnectionsPerProxyTest();
 }
 
