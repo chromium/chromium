43b33c32b1bec6733aa70aa76de0dc47fd66c58e
Sky Malice <skym@chromium.org>
2026-09-16 09:13:08 -0700

    [AIM] Disable testOpenNewTabFromIncognito on Desktop
    Disables TabSwitcherActionMenuPTTest#testOpenNewTabFromIncognito on
    Desktop form factors due to flakiness on
    android-internal-desktop-x64-rel.
    
    TAG=agy
    CONV=28caf254-681e-457b-a84a-f625224341d7
    
    Bug: 562154154
    Change-Id: Icca904e9f6e12abdf1f846a838a12322cdc5b600
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8413247
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1698990}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuPTTest.java
@@ -74,6 +74,7 @@ public class TabSwitcherActionMenuPTTest {
 
     @Test
     @LargeTest
+    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/562154154
     public void testOpenNewTabFromIncognito() {
         IncognitoNewTabPageStation incognitoNtp =
                 mCtaTestRule
