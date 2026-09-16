315d77743df4fd4c5a52e0cd33581f7146835e0b
Andrew Paseltiner <apaseltiner@chromium.org>
2026-09-16 02:25:42 -0700

    Replace uses of deprecated GetWithParamsIf* accessors with GetIf*
    GetWithParamsIfItem and GetWithParamsIfInnerList returned an optional
    pair of the item/inner list and its parameters. Now that the internal
    representation has been changed to std::variant, the simpler GetIfItem
    and GetIfInnerList APIs are used instead, returning a possibly null
    pointer to the ParameterizedItem or InnerList.
    
    This contains no intentional observable behavioral differences.
    
    TAG=agy
    CONV=5ace3885-9dc3-42fe-9ad1-981c41c88e8a
    
    Bug: 517204961
    Change-Id: I3da904ea3375247aff82370426881e1d6768ee7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8401859
    Reviewed-by: Amelie Schneider <amelies@google.com>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698777}

--- chrome/browser/signin/bound_session_credentials/bound_session_registration_fetcher_param.cc
+++ chrome/browser/signin/bound_session_credentials/bound_session_registration_fetcher_param.cc
@@ -8,7 +8,6 @@
 #include <string>
 #include <vector>
 
-#include "base/types/expected_macros.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_params_util.h"
 #include "components/signin/public/base/session_binding_utils.h"
 #include "net/http/structured_headers.h"
@@ -74,10 +73,13 @@ std::optional<BoundSessionRegistrationFetcherParam>
 BoundSessionRegistrationFetcherParam::ParseListItem(
     const GURL& request_url,
     net::structured_headers::ParameterizedMember item) {
-  ASSIGN_OR_RETURN((auto [items, params]), item.GetWithParamsIfInnerList());
+  net::structured_headers::InnerList* inner_list = item.GetIfInnerList();
+  if (!inner_list) {
+    return std::nullopt;
+  }
 
   std::vector<crypto::sign::SignatureKind> supported_algos;
-  for (const auto& algo_token : items) {
+  for (const auto& algo_token : inner_list->items) {
     const std::string* token = algo_token.item.GetIfToken();
     if (!token) {
       continue;
@@ -94,7 +96,7 @@ BoundSessionRegistrationFetcherParam::ParseListItem(
 
   GURL registration_endpoint;
   std::string challenge;
-  for (auto& [name, value] : params) {
+  for (auto& [name, value] : inner_list->params) {
     std::string* str = value.GetIfString();
     if (!str) {
       continue;
