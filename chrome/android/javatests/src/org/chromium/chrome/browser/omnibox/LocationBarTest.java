2fd57f8a06b9187eb5fa7d5b735c85aff1dc6995
Tomasz Wiszkowski <ender@google.com>
2026-09-16 14:00:52 -0700

    [Omnibox] Fix desktop mode override and re-enable LocationBarTest on Desktop
    testEditingText_withDesktopModeDisabled failed on desktop form factors
    because setHasDesktopExperienceForTesting(false) was overridden by
    DeviceInfo.isDesktop() returning true in isDesktopPlatform().
    
    Explicitly set setIsDesktopPlatformForTesting alongside
    setHasDesktopExperienceForTesting, matching the pattern in
    testFocusLogic_buttonVisibilityTablet, and re-enable the test on
    Desktop.
    
    Bug: 526811362
    Change-Id: I1cdc0338f2a17e8d2456950675b1b4019236c8fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8417050
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699252}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarTest.java
@@ -42,7 +42,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -201,8 +200,8 @@ public class LocationBarTest {
 
     @Test
     @MediumTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/526811362
     public void testEditingText_withDesktopModeDisabled() {
+        OmniboxCapabilities.setIsDesktopPlatformForTesting(false);
         OmniboxCapabilities.setHasDesktopExperienceForTesting(false);
         testEditingText(/* expectDesktopMode= */ false);
     }
