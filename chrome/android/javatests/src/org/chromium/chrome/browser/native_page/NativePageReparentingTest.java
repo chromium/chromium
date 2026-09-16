04894df1bd16ff980e6288b6d5d291e4d7665475
Sky Malice <skym@chromium.org>
2026-09-15 18:18:20 -0700

    [AIM] Restrict NativePageReparentingTest on desktop
    Restrict testBackgroundNtpFrozenOnReparenting and
    testNtpReparentingPreservesTabAndRecreatesNativePage away from desktop
    due to flakiness on android-internal-desktop-x64-rel.
    
    TAG=agy
    CONV=a59fd189-448c-4127-82dc-47a0e25aeb37
    
    Bug: 562155405
    Change-Id: Idd50c265c90f4f5dc56535b1eefbc0f83a53f614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8416267
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698558}

--- chrome/android/javatests/src/org/chromium/chrome/browser/native_page/NativePageReparentingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/native_page/NativePageReparentingTest.java
@@ -20,12 +20,13 @@ import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
-import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.chrome.browser.ntp.NewTabPage;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabSelectionType;
@@ -52,6 +53,7 @@ public class NativePageReparentingTest {
     @Test
     @MediumTest
     @Feature({"Reparenting", "NewTabPage"})
+    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/562155405
     public void testNtpReparentingPreservesTabAndRecreatesNativePage() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
         ChromeTabbedActivity initialActivity = mActivityTestRule.getActivity();
@@ -110,6 +112,7 @@ public class NativePageReparentingTest {
     @Test
     @MediumTest
     @Feature({"Reparenting", "BackgroundTabs"})
+    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/562155405
     public void testBackgroundNtpFrozenOnReparenting() throws Exception {
         mActivityTestRule.startMainActivityOnBlankPage();
         ChromeTabbedActivity initialActivity = mActivityTestRule.getActivity();
