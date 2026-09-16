e6886c6ae92435cbe64998045a42ea1ccd9e039c
Angela Novakovic <novakovica@google.com>
2026-09-16 07:32:55 -0700

    [iOS][AtMemory] Improve VoiceOver accessibility in granular fill view
    This CL disables container-level accessibility on the table view cell
    and content view so VoiceOver can traverse the attribute label and value
    chip individually. Updates the chip button's accessibility traits and
    sets its accessibility label to announce "Autofill <value>".
    
    Fixed: 555811376
    Change-Id: I7e1b7b226370dcd30b044b07ae903106bdc3abdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8400989
    Commit-Queue: Angela Novakovic <novakovica@google.com>
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1698923}

--- ios/chrome/browser/autofill/atmemory/ui/at_memory_granular_fill_cell_content_configuration.mm
+++ ios/chrome/browser/autofill/atmemory/ui/at_memory_granular_fill_cell_content_configuration.mm
@@ -5,11 +5,14 @@
 #import "ios/chrome/browser/autofill/atmemory/ui/at_memory_granular_fill_cell_content_configuration.h"
 
 #import "base/apple/foundation_util.h"
+#import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/autofill/atmemory/public/at_memory_constants.h"
 #import "ios/chrome/browser/autofill/atmemory/utils/atmemory_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/button_util.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/grit/ios_strings.h"
+#import "ui/base/l10n/l10n_util.h"
 
 namespace {
 
@@ -37,7 +40,11 @@ UILabel* CreateAttributeLabel() {
 // Updates the title and accessibility label of a chip button.
 void UpdateChipButton(UIButton* button, NSString* value) {
   SetConfigurationTitle(button, value);
-  button.accessibilityLabel = value;
+  button.accessibilityLabel =
+      value.length > 0 ? l10n_util::GetNSStringF(
+                             IDS_IOS_MANUAL_FALLBACK_CHIP_ACCESSIBILITY_LABEL,
+                             base::SysNSStringToUTF16(value))
+                       : nil;
 }
 
 // Creates and configures the selectable chip button.
@@ -103,6 +110,8 @@ UIButton* CreateChipButton() {
     [_chipButton addTarget:self
                     action:@selector(onButtonTapped:)
           forControlEvents:UIControlEventTouchUpInside];
+
+    self.accessibilityElements = @[ _attributeLabel, _chipButton ];
 
     _containerStackView = [[UIStackView alloc]
         initWithArrangedSubviews:@[ _attributeLabel, _chipButton ]];
--- ios/chrome/browser/autofill/atmemory/ui/at_memory_granular_fill_view_controller.mm
+++ ios/chrome/browser/autofill/atmemory/ui/at_memory_granular_fill_view_controller.mm
@@ -250,6 +250,7 @@ enum ItemIdentifier {
   cell.contentConfiguration = configuration;
   cell.accessibilityIdentifier =
       GetAtMemoryGranularFillCellAccessibilityIdentifier(item.attributeName);
+  cell.isAccessibilityElement = NO;
   cell.accessoryType = UITableViewCellAccessoryNone;
   cell.selectionStyle = UITableViewCellSelectionStyleNone;
   return cell;
