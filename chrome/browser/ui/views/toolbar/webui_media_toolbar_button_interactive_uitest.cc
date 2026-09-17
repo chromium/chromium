b09c8acc04d411373d661b6dc31f9986c329db08
Paul Jensen <pauljensen@chromium.org>
2026-09-16 22:47:17 -0700

    Deflake and re-enable WebUIMediaToolbarButtonInteractiveTest
    This change re-enables WebUIMediaToolbarButtonInteractiveTest.
    MediaButtonClickedAndRightClicked and deflakes it.  The test is
    modified to wait for the button's anchor to be available so that
    the media bubble can anchor to it.
    
    Include-Ci-Only-Tests: true
    Fixed: 561995604
    Change-Id: If222402973c9b1aa870a08c7c22a0849277ddd78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415000
    Auto-Submit: Paul Jensen <pauljensen@chromium.org>
    Reviewed-by: Cammie Smith Barnes <cammie@chromium.org>
    Commit-Queue: Paul Jensen <pauljensen@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699538}

--- chrome/browser/ui/views/toolbar/webui_media_toolbar_button_interactive_uitest.cc
+++ chrome/browser/ui/views/toolbar/webui_media_toolbar_button_interactive_uitest.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/ui/views/toolbar/webui_media_toolbar_button.h"
 
 #include "base/test/run_until.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/global_media_controls/media_toolbar_button_controller.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/global_media_controls/media_dialog_view.h"
@@ -27,9 +28,8 @@ class WebUIMediaToolbarButtonInteractiveTest
             {}) {}
 };
 
-// TODO(crbug.com/561995604): Re-enable this test once the flakiness is fixed.
 IN_PROC_BROWSER_TEST_F(WebUIMediaToolbarButtonInteractiveTest,
-                       DISABLED_MediaButtonClickedAndRightClicked) {
+                       MediaButtonClickedAndRightClicked) {
   WebUIToolbarWebView* webui_toolbar_view = GetWebUIToolbarWebView(browser());
   ASSERT_TRUE(webui_toolbar_view);
 
@@ -43,7 +43,8 @@ IN_PROC_BROWSER_TEST_F(WebUIMediaToolbarButtonInteractiveTest,
       webui_toolbar_view->GetWebViewForTesting()->GetWebContents();
   ASSERT_TRUE(web_contents);
 
-  // 1. Verify media button is visible and not highlighted.
+  // 1. Verify media button is visible, tracked, and not highlighted.
+  ASSERT_TRUE(WaitForTrackedElementVisible(kToolbarMediaButtonElementId));
   ASSERT_TRUE(base::test::RunUntil([&]() {
     return content::EvalJs(
                web_contents,
