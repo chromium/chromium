b9b96ee0fb4794ce419f451b6633c5acb91f512b
Alex Mitra <alexmitra@chromium.org>
2026-09-17 12:44:26 -0700

    Revert "[PA] Consolidate PartitionAlloc TLS into a single structure."
    This reverts commit 67b58121286da3189afe0e05570f4e0c70499cf8.
    
    Reason for revert: Suspected cause of memory regression
    
    Failure Link: https://pinpoint-dot-chromeperf.appspot.com/job/157bfe49690000
    
    Original change's description:
    > [PA] Consolidate PartitionAlloc TLS into a single structure.
    >
    > This CL renames partition_tls.h to tls.h and defines a single
    > `PartitionTls` structure that holds all thread-unique objects used
    > within PartitionAlloc. Specifically, it now holds the ThreadCache
    > pointers and the disallow_allocations flag.
    >
    > This consolidation reduces TLS slot usage and minimizes code
    > duplication. TLS creation remains non-allocating.
    >
    > To minimize overhead on allocation and deallocation hot paths:
    > - Placed scalar flags at byte offset 0 of struct PartitionTls for
    >   D-cache locality.
    > - Combined thread cache validity and bucket size checks into a single
    >   branch (bucket_index < thread_cache->active_bucket_count()) on hot
    >   paths, removing duplicate checks and redundant TLS lookups.
    >
    > TAG=agy
    > CONV=80980e77-c356-4c8b-a00b-3a2c47533fbb
    >
    > Bug: 329027914
    > Change-Id: If05f8f540281a135eba36aba872df424a4c208be
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8264055
    > Commit-Queue: Mikihito Matsuura <mikt@google.com>
    > Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    > Reviewed-by: Takashi Sakamoto <tasak@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1688851}
    
    (cherry picked from commit 438d489235fc95769492fdb88920a78acee41f65)
    
    Bug: 329027914
    Bug: 561995364
    Fixed: 562054919
    Change-Id: Ic0a1771902cd4159e4571c563989e13e15fa0235
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8412606
    Reviewed-by: Mateusz Zbikowski <zbikowski@google.com>
    Commit-Queue: Alex Mitra <alexmitra@chromium.org>
    Owners-Override: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1698073}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8420683
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Krishna Govind <govind@chromium.org>
    Commit-Queue: Krishna Govind <govind@chromium.org>
    Cr-Commit-Position: refs/branch-heads/8037@{#1230}
    Cr-Branched-From: e10b20e60f162e7ad88e4930833695a54f4e15fb-refs/heads/main@{#1689415}

--- base/allocator/partition_allocator/src/partition_alloc/allocation_guard.cc
+++ base/allocator/partition_allocator/src/partition_alloc/allocation_guard.cc
@@ -6,46 +6,36 @@
 
 #include "partition_alloc/partition_alloc_base/immediate_crash.h"
 #include "partition_alloc/partition_alloc_config.h"
-#include "partition_alloc/partition_tls.h"
 
 #if PA_CONFIG(HAS_ALLOCATION_GUARD)
 
 namespace partition_alloc {
 
+namespace {
+thread_local bool g_disallow_allocations;
+}  // namespace
+
 ScopedDisallowAllocations::ScopedDisallowAllocations() {
-  auto* tls = internal::GetTls();
-  if (tls) {
-    if (tls->disallow_allocations) {
-      PA_IMMEDIATE_CRASH();
-    }
-    tls->disallow_allocations = true;
+  if (g_disallow_allocations) {
+    PA_IMMEDIATE_CRASH();
   }
+
+  g_disallow_allocations = true;
 }
 
 ScopedDisallowAllocations::~ScopedDisallowAllocations() {
-  auto* tls = internal::GetTls();
-  if (tls) {
-    tls->disallow_allocations = false;
-  }
+  g_disallow_allocations = false;
 }
 
 ScopedAllowAllocations::ScopedAllowAllocations() {
-  auto* tls = internal::GetTls();
-  if (tls) {
-    // Save the previous value, as ScopedAllowAllocations is used in all
-    // partitions, not just the malloc() ones(s).
-    saved_value_ = tls->disallow_allocations;
-    tls->disallow_allocations = false;
-  } else {
-    saved_value_ = false;
-  }
+  // Save the previous value, as ScopedAllowAllocations is used in all
+  // partitions, not just the malloc() ones(s).
+  saved_value_ = g_disallow_allocations;
+  g_disallow_allocations = false;
 }
 
 ScopedAllowAllocations::~ScopedAllowAllocations() {
-  auto* tls = internal::GetTls();
-  if (tls) {
-    tls->disallow_allocations = saved_value_;
-  }
+  g_disallow_allocations = saved_value_;
 }
 
 }  // namespace partition_alloc
