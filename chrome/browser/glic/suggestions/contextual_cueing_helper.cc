76b2d98c111278d8019a1acb4485a102e557d389
Michael Wu <mklw@google.com>
2026-09-16 18:58:15 -0700

    Reland "[Tab Strip Glic Button][Android] Block nudges when side panel is open"
    This is a reland of commit a4f4e75e5bbc42711c7a2d1171a6ab8eb6b7500e The
    change was reverted because it introduced failures in
    android-14-tablet-landscape-arm64-rel, which now passes in CQ.
    
    Original change's description:
    > [Tab Strip Glic Button][Android] Block nudges when side panel is open
    >
    > Enable BrowserWindowInterface and GlicSidePanelCoordinator checks in
    > ContextualCueingHelper::IsBrowserBlockingNudges for Android. Previously,
    > these checks were disabled behind #if !BUILDFLAG(IS_ANDROID) because
    > BrowserWindowInterface was not yet hooked to TabAndroid.
    >
    > Bug: 544854157
    > Change-Id: I0e5067a90b0509fd756497562637afc1127d690a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8244110
    > Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    > Commit-Queue: Michael Wu <mklw@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1696551}
    
    Bug: 544854157
    Change-Id: I240ce2389c8ddd85258e3316285dd19e391bac05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8406851
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Commit-Queue: Michael Wu <mklw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699423}

--- chrome/browser/glic/suggestions/contextual_cueing_helper.cc
+++ chrome/browser/glic/suggestions/contextual_cueing_helper.cc
@@ -58,7 +58,6 @@
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 #else
 #include "chrome/browser/contextual_tasks/contextual_tasks_side_panel_coordinator.h"  // nogncheck crbug.com/40147906
-#include "chrome/browser/glic/public/glic_side_panel_coordinator.h"
 #include "chrome/browser/ui/user_education/browser_user_education_interface.h"
 #include "chrome/browser/ui/views/glic/glic_button_interface.h"  // nogncheck crbug.com/40147906
 #include "ui/views/controls/button/label_button.h"  // nogncheck crbug.com/40147906
@@ -324,17 +323,12 @@ bool ContextualCueingHelper::IsBrowserBlockingNudges(
   }
 
   BrowserWindowInterface* browser_window_interface =
-#if !BUILDFLAG(IS_ANDROID)
       tab_interface->GetBrowserWindowInterface();
-#else
-      // NEEDS_ANDROID_IMPL: GetBrowserWindowInterface will be available later
-      nullptr;
-#endif
+
+#if !BUILDFLAG(IS_ANDROID)  // NEEDS_ANDROID_IMPL
   if (!browser_window_interface) {
     return false;
   }
-
-#if !BUILDFLAG(IS_ANDROID)  // NEEDS_ANDROID_IMPL
   auto* user_education_interface =
       BrowserUserEducationInterface::From(browser_window_interface);
   if (!user_education_interface) {
@@ -348,8 +342,6 @@ bool ContextualCueingHelper::IsBrowserBlockingNudges(
   }
 #endif
 
-#if !BUILDFLAG(IS_ANDROID)
-  // NEEDS_ANDROID_IMPL
   Profile* profile =
       Profile::FromBrowserContext(web_contents()->GetBrowserContext());
 
@@ -360,16 +352,9 @@ bool ContextualCueingHelper::IsBrowserBlockingNudges(
   auto* glic_service =
       glic::GlicKeyedServiceFactory::GetGlicKeyedService(profile);
 
-  if (glic_service->IsPanelShowingForBrowser(*browser_window_interface)) {
+  if (browser_window_interface &&
+      glic_service->IsPanelShowingForBrowser(*browser_window_interface)) {
     recorder->set_nudge_decision(NudgeDecision::kNudgeNotShownWindowShowing);
-    return true;
-  }
-
-  auto* glic_side_panel_coordinator =
-      glic::GlicSidePanelCoordinator::GetForTab(tab_interface);
-  if (glic_side_panel_coordinator && glic_side_panel_coordinator->IsShowing()) {
-    recorder->set_nudge_decision(
-        NudgeDecision::kNudgeNotShownSidePanelForTabShowing);
     return true;
   }
 
@@ -379,8 +364,6 @@ bool ContextualCueingHelper::IsBrowserBlockingNudges(
     recorder->set_nudge_decision(NudgeDecision::kNudgeNotShownActorActiveOnTab);
     return true;
   }
-
-#endif  // !BUILDFLAG(IS_ANDROID)
 
 #if !BUILDFLAG(IS_ANDROID)
   auto* controller = contextual_tasks::ContextualTasksPanelController::From(
