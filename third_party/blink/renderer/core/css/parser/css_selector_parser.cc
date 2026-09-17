11d934f79a843852a9e3124613548aa6cff744aa
Rune Lillesveen <futhark@chromium.org>
2026-09-17 15:24:19 -0700

    Unsupported pseudo elements should fall back to empty styles
    ParsePseudoElement returns kPseudoIdNone or kPseudoIdInvalid depending
    on whether getComputedStyle() should fall back to the originating
    element's style or empty styles. Unsupported pseudo elements were
    incorrectly falling back to the originating element's styles.
    
    Bug: 562643747
    Change-Id: I4e46d4ec24c2264be9fa8c09fd7b46ef11aee5f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419174
    Auto-Submit: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Kevin Babbitt <kbabbitt@microsoft.com>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Kevin Babbitt <kbabbitt@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1700175}

--- third_party/blink/renderer/core/css/parser/css_selector_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_selector_parser.cc
@@ -1116,7 +1116,8 @@ PseudoId CSSSelectorParser::ParsePseudoElement(const String& selector_string,
   }
 
   PseudoId pseudo_id = result.GetPseudoId(result.GetPseudoType());
-  if (!PseudoElement::IsWebExposed(pseudo_id, parent)) {
+  if (pseudo_id == kPseudoIdNone ||
+      !PseudoElement::IsWebExposed(pseudo_id, parent)) {
     return kPseudoIdInvalid;
   }
 
