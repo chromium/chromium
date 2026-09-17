18d538d4d40381a9bb2ccd0b0b16ff1db884686d
Christoph Schwering <schwering@google.com>
2026-09-17 03:13:53 -0700

    [Autofill] Fix AutofillPopupControllerBrowserTest.DoNotShowIfNotEnoughSpace
    The test was turned red by crrev.com/c/8412450 but it was actually
    broken before that. This CL rewrites the test.
    
    The test says
    
    > Position the popup in the lower right corner so that there is not
    > enough space to display it.
    
    but the actual behavior is different in multiple ways:
    
    (1) The corner is the top left corner, not the bottom right.
    (2) The position is the *field's* position, not the popup's position.
        The field being in a corner alone does not prevent Autofill from
        displaying the popup because the popup deflects upwards if there's
        no space downwards.
    (3) The browser window bounds are not identical to the web contents
        bounds, and they're referring to different coordinate systems:
        screen coordinates vs web contents coordinates.
    
    The test happened to succeed because the browser window's top-left
    is less than 100 DIPs from the screen's top-left. As a result, the
    element was, at least mostly, outside of the viewport.
    
    The test then started to fail on Mac after crrev.com/c/8412450 because
    on Mac the browser window is positioned more towards the center, so
    that the `window_bounds` overlap with the content area's bounds.
    
    Bug: 553117374
    Change-Id: I4f8d7bac928cad3dfc73037cd10e1900abd31016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8412893
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699659}

--- chrome/browser/ui/autofill/autofill_popup_controller_interactive_uitest.cc
+++ chrome/browser/ui/autofill/autofill_popup_controller_interactive_uitest.cc
@@ -134,20 +134,16 @@ IN_PROC_BROWSER_TEST_F(AutofillPopupControllerBrowserTest,
   EXPECT_TRUE(autofill_external_delegate().popup_hidden());
 }
 
+// Tests that if there is no space for the Autofill popup, the popup is not
+// displayed.
 IN_PROC_BROWSER_TEST_F(AutofillPopupControllerBrowserTest,
-                       DISABLED_DoNotShowIfNotEnoughSpace) {
-  constexpr float kSize = 100.0f;
-  // Set to smallest possible size. The actual minimum size is larger and
-  // platform dependent.
-  browser()->GetWindow()->SetBounds(gfx::Rect(1, 1));
-  gfx::Rect window_bounds = browser()->GetWindow()->GetBounds();
-  // Position the popup in the lower right corner so that there is not enough
-  // space to display it.
-  EXPECT_TRUE(GenerateTestAutofillPopup(
-      autofill_driver(), profile(),
-      /*expect_popup_to_be_shown=*/false, /*element_bounds=*/
-      gfx::RectF(window_bounds.x() - kSize, window_bounds.y() - kSize, kSize,
-                 kSize)));
+                       DoNotShowIfNotEnoughSpace) {
+  // Let the field occupy the whole viewport so that there's no space left for
+  // the popup.
+  const gfx::RectF field_bounds(gfx::Rect({}, web_contents()->GetSize()));
+  EXPECT_TRUE(GenerateTestAutofillPopup(autofill_driver(), profile(),
+                                        /*expect_popup_to_be_shown=*/false,
+                                        /*element_bounds=*/field_bounds));
 }
 
 // Tests that entering fullscreen hides the popup and, in particular, does not
