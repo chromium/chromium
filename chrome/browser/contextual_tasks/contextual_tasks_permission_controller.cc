60cbead5ec8b9a69e45c80f6c9586d3328363441
James Leung <jamesleung@google.com>
2026-09-16 19:45:06 -0700

    [contextual_tasks] Add permission controller
    * Add permission controller specifically for contextual tasks.
    * Implement state updaters, getters. Added functionality from
      base class for when chips are clicked, etc.
    * Create new permission manager and permission controller
      whenever switch tabs.
    * Attach the controller to the side panel coordinator and
      the web contents.
    * Get specific chip based on if it is permissions request or
      indicator from dashboard.
    * Made location bar getter const to ensure that dashboard
      getState can be const
    
    Fixed: b:554175499
    Change-Id: I28bae105658a1be39b81358971bd00f142685613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8340556
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: James Leung <jamesleung@google.com>
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699473}

---
+++ chrome/browser/contextual_tasks/contextual_tasks_permission_controller.cc
@@ -0,0 +1,148 @@
+// Copyright 2026 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/contextual_tasks/contextual_tasks_permission_controller.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/notreached.h"
+#include "build/build_config.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks_ui.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "components/browser_apis/ui_controllers/toolbar/toolbar_ui_api_data_model.mojom.h"
+#include "components/permissions/permission_request_manager.h"
+#include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/web_contents.h"
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/contextual_tasks/contextual_tasks_location_bar.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks_permission_chip.h"
+#include "chrome/browser/contextual_tasks/contextual_tasks_permission_dashboard.h"
+#include "chrome/browser/ui/location_bar/location_bar_override_data.h"
+#endif
+
+namespace contextual_tasks {
+
+WEB_CONTENTS_USER_DATA_KEY_IMPL(ContextualTasksPermissionController);
+
+ContextualTasksPermissionController::ContextualTasksPermissionController(
+    content::WebContents* web_contents,
+    BrowserWindowInterface* browser_window)
+    : content::WebContentsUserData<ContextualTasksPermissionController>(
+          *web_contents) {
+#if !BUILDFLAG(IS_ANDROID)
+  if (browser_window) {
+    // Connect dashboard and location bar for contextual tasks side panel.
+    location_bar_ =
+        std::make_unique<ContextualTasksLocationBar>(browser_window);
+
+    // Register the location bar as the override for this WebContents.
+    location_bar::LocationBarOverrideData::CreateForWebContents(
+        &GetWebContents(), location_bar_.get());
+  }
+#endif
+
+  // Observe PermissionRequestManager on this tab for permission prompt
+  // visibility updates.
+  if (auto* prm = permissions::PermissionRequestManager::FromWebContents(
+          &GetWebContents())) {
+    prm_observation_.Observe(prm);
+  }
+}
+
+ContextualTasksPermissionController::~ContextualTasksPermissionController() =
+    default;
+
+toolbar_ui_api::mojom::PermissionDashboardStatePtr
+ContextualTasksPermissionController::GetState() const {
+#if !BUILDFLAG(IS_ANDROID)
+  if (location_bar_ && location_bar_->permission_dashboard()) {
+    return location_bar_->permission_dashboard()->GetState();
+  }
+#endif
+  return toolbar_ui_api::mojom::PermissionDashboardState::New();
+}
+
+void ContextualTasksPermissionController::PushStateToWebUI() {}
+
+// ============================================================================
+// permissions::PermissionRequestManager::Observer:
+// ============================================================================
+
+void ContextualTasksPermissionController::OnRequestsFinalized() {
+  PushStateToWebUI();
+}
+
+void ContextualTasksPermissionController::OnPromptRemoved() {
+  PushStateToWebUI();
+}
+
+void ContextualTasksPermissionController::OnNavigation(
+    content::NavigationHandle* navigation_handle) {
+  if (navigation_handle && navigation_handle->IsInPrimaryMainFrame()) {
+    PushStateToWebUI();
+  }
+}
+
+void ContextualTasksPermissionController::
+    OnPermissionRequestManagerDestructed() {
+  prm_observation_.Reset();
+}
+
+// ============================================================================
+// Chip Interactions (called from Mojo via ContextualTasksUI):
+// ============================================================================
+
+#if !BUILDFLAG(IS_ANDROID)
+ContextualTasksPermissionChip* ContextualTasksPermissionController::GetChip(
+    toolbar_ui_api::mojom::LhsChipIdentifier chip_identifier) {
+  ContextualTasksPermissionDashboard* permission_dashboard =
+      location_bar_ ? location_bar_->permission_dashboard() : nullptr;
+  if (!permission_dashboard) {
+    return nullptr;
+  }
+  switch (chip_identifier) {
+    case toolbar_ui_api::mojom::LhsChipIdentifier::kPermissionRequest:
+      return permission_dashboard->request_chip();
+    case toolbar_ui_api::mojom::LhsChipIdentifier::kPermissionIndicator:
+      return permission_dashboard->indicator_chip();
+    default:
+      NOTREACHED();
+  }
+}
+#endif
+
+void ContextualTasksPermissionController::OnChipClicked(
+    toolbar_ui_api::mojom::LhsChipIdentifier chip_identifier,
+    bool is_mouse_interaction) {
+#if !BUILDFLAG(IS_ANDROID)
+  if (ContextualTasksPermissionChip* chip = GetChip(chip_identifier)) {
+    chip->OnClicked(is_mouse_interaction);
+  }
+
+  // Ignore the event if the chip does not exist (if dashboard is torn down
+  // and mojo event arrives afterwards).
+#endif
+}
+
+void ContextualTasksPermissionController::OnChipExpandAnimationEnded(
+    toolbar_ui_api::mojom::LhsChipIdentifier chip_identifier) {
+#if !BUILDFLAG(IS_ANDROID)
+  if (ContextualTasksPermissionChip* chip = GetChip(chip_identifier)) {
+    chip->OnExpandAnimationEnded();
+  }
+#endif
+}
+
+void ContextualTasksPermissionController::OnChipCollapseAnimationEnded(
+    toolbar_ui_api::mojom::LhsChipIdentifier chip_identifier) {
+#if !BUILDFLAG(IS_ANDROID)
+  if (ContextualTasksPermissionChip* chip = GetChip(chip_identifier)) {
+    chip->OnCollapseAnimationEnded();
+  }
+#endif
+}
+
+}  // namespace contextual_tasks
--- chrome/browser/contextual_tasks/contextual_tasks_permission_controller.h
+++ chrome/browser/contextual_tasks/contextual_tasks_permission_controller.h
@@ -5,33 +5,90 @@
 #ifndef CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_PERMISSION_CONTROLLER_H_
 #define CHROME_BROWSER_CONTEXTUAL_TASKS_CONTEXTUAL_TASKS_PERMISSION_CONTROLLER_H_
 
+#include <memory>
+
+#include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
+#include "build/build_config.h"
 #include "components/browser_apis/ui_controllers/toolbar/toolbar_ui_api_data_model.mojom-forward.h"
+#include "components/permissions/permission_request_manager.h"
+#include "content/public/browser/web_contents_user_data.h"
+
+class BrowserWindowInterface;
 
 namespace content {
+class NavigationHandle;
 class WebContents;
-}
+}  // namespace content
 
 namespace contextual_tasks {
 
+class ContextualTasksLocationBar;
+class ContextualTasksPermissionChip;
+class ContextualTasksPermissionDashboard;
+
 // Implements the toolbar mojom interface to control
 // the dashboard and permission chip for contextual tasks.
-class ContextualTasksPermissionController {
+class ContextualTasksPermissionController
+    : public content::WebContentsUserData<ContextualTasksPermissionController>,
+      public permissions::PermissionRequestManager::Observer {
  public:
-  virtual ~ContextualTasksPermissionController() = default;
+  ContextualTasksPermissionController(
+      const ContextualTasksPermissionController&) = delete;
+  ~ContextualTasksPermissionController() override;
+  ContextualTasksPermissionController& operator=(
+      const ContextualTasksPermissionController&) = delete;
 
-  static ContextualTasksPermissionController* FromWebContents(
-      content::WebContents* web_contents) {
-    return nullptr;
-  }
+  // permissions::PermissionRequestManager::Observer:
+  void OnRequestsFinalized() override;
+  void OnPromptRemoved() override;
+  void OnNavigation(content::NavigationHandle* navigation_handle) override;
+  void OnPermissionRequestManagerDestructed() override;
 
-  virtual toolbar_ui_api::mojom::PermissionDashboardStatePtr GetState() = 0;
+  virtual toolbar_ui_api::mojom::PermissionDashboardStatePtr GetState() const;
+  void PushStateToWebUI();
+
+  // Chip Interactions (called from Mojo via ContextualTasksUI):
   virtual void OnChipClicked(
-      toolbar_ui_api::mojom::LhsChipIdentifier identifier,
-      bool is_mouse_interaction) = 0;
+      toolbar_ui_api::mojom::LhsChipIdentifier chip_identifier,
+      bool is_mouse_interaction);
   virtual void OnChipExpandAnimationEnded(
-      toolbar_ui_api::mojom::LhsChipIdentifier identifier) = 0;
+      toolbar_ui_api::mojom::LhsChipIdentifier chip_identifier);
   virtual void OnChipCollapseAnimationEnded(
-      toolbar_ui_api::mojom::LhsChipIdentifier identifier) = 0;
+      toolbar_ui_api::mojom::LhsChipIdentifier chip_identifier);
+
+#if !BUILDFLAG(IS_ANDROID)
+  ContextualTasksLocationBar* GetLocationBarForTesting() {
+    return location_bar_.get();
+  }
+#endif
+
+ protected:
+  // Use `CreateForWebContents()` (inherited from `WebContentsUserData`) to
+  // create instances. `browser_window` may be null, in which case no location
+  // bar is created. Protected rather than private so that tests can be a
+  // subclass to access.
+  ContextualTasksPermissionController(content::WebContents* web_contents,
+                                      BrowserWindowInterface* browser_window);
+
+ private:
+  friend class content::WebContentsUserData<
+      ContextualTasksPermissionController>;
+  WEB_CONTENTS_USER_DATA_KEY_DECL();
+
+#if !BUILDFLAG(IS_ANDROID)
+  // Returns the chip `chip_identifier` refers to, or null if there is no
+  // dashboard (i.e. no location bar).
+  ContextualTasksPermissionChip* GetChip(
+      toolbar_ui_api::mojom::LhsChipIdentifier chip_identifier);
+#endif
+#if !BUILDFLAG(IS_ANDROID)
+  std::unique_ptr<ContextualTasksLocationBar> location_bar_;
+#endif
+
+  base::ScopedObservation<permissions::PermissionRequestManager,
+                          permissions::PermissionRequestManager::Observer>
+      prm_observation_{this};
 };
 
 }  // namespace contextual_tasks
