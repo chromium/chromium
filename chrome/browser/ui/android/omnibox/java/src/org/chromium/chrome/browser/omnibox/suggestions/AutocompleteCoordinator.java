400a8e4e932fa0c65f5aa56309f574c7d6013eab
Brandon Fong <bjfong@google.com>
2026-09-17 14:54:09 -0700

    [search] Update profile binding to be sync if non null on AC
    Currently if the profile is non null during setup in the ACCoordinator,
    it posts the call which can cause some favicons to not be ready on
    first load for tab search (as one use case).
    
    Change the post async call to be synchronous if the profile is ready.
    
    Bug: 562193502
    Change-Id: Id60c73318b0f2ddd0eaaf428b454fa8d0a657229
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419841
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700147}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinator.java
@@ -202,7 +202,7 @@ public class AutocompleteCoordinator implements OmniboxSuggestionsVisualState {
 
         mProfileSupplier = profileObservableSupplier;
         mProfileChangeCallback = this::setAutocompleteProfile;
-        mProfileSupplier.addSyncObserverAndPostIfNonNull(mProfileChangeCallback);
+        mProfileSupplier.addSyncObserverAndCallIfNonNull(mProfileChangeCallback);
 
         // When AsyncViewInflation is disabled, OmniboxSuggestionsDropdown cannot create the
         // recycled view pool b/c it causes issues with the timing of prewarming views. Creation of
@@ -233,7 +233,7 @@ public class AutocompleteCoordinator implements OmniboxSuggestionsVisualState {
         mMediator = mediator;
         mProfileSupplier = profileObservableSupplier;
         mProfileChangeCallback = this::setAutocompleteProfile;
-        mProfileSupplier.addSyncObserverAndPostIfNonNull(mProfileChangeCallback);
+        mProfileSupplier.addSyncObserverAndCallIfNonNull(mProfileChangeCallback);
         mLocationBarEmbedder = locationBarEmbedder;
         mModalDialogManagerSupplier = modalDialogManagerSupplier;
         mViewProvider = new SuggestionListViewHolderProvider(new ModelList());
--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteCoordinatorUnitTest.java
@@ -30,6 +30,7 @@ import org.mockito.quality.Strictness;
 
 import org.chromium.base.supplier.MonotonicObservableSupplier;
 import org.chromium.base.supplier.ObservableSuppliers;
+import org.chromium.base.supplier.SettableMonotonicObservableSupplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.omnibox.LocationBarEmbedder;
 import org.chromium.chrome.browser.omnibox.R;
@@ -55,6 +56,7 @@ public class AutocompleteCoordinatorUnitTest {
     @Mock private OmniboxSuggestionsContainer mSuggestionsContainer;
     @Mock private ViewGroup mParentView;
     @Mock private OmniboxResourceProvider mResourceProvider;
+    @Mock private Profile mProfile;
 
     @Before
     public void setUp() {
@@ -126,6 +128,23 @@ public class AutocompleteCoordinatorUnitTest {
         doReturn(false).when(mSuggestionsContainer).onKeyDown(eq(KeyEvent.KEYCODE_ENTER), any());
         assertFalse(sendKeyDownEvent(KeyEvent.KEYCODE_ENTER, KeyEvent.META_SHIFT_ON));
         verify(mSuggestionsContainer).onKeyDown(eq(KeyEvent.KEYCODE_ENTER), any());
+    }
+
+    @Test
+    public void testProfileObserver_synchronousBindingWhenNonNull() {
+        SettableMonotonicObservableSupplier<Profile> profileSupplier =
+                ObservableSuppliers.createMonotonic();
+        profileSupplier.set(mProfile);
+
+        new AutocompleteCoordinator(
+                mParentView,
+                mAutocompleteMediator,
+                profileSupplier,
+                mLocationBarEmbedder,
+                mModalDialogManagerSupplier,
+                mResourceProvider);
+
+        verify(mAutocompleteMediator).setAutocompleteProfile(mProfile);
     }
 
     private boolean sendKeyDownEvent(int keyCode, int metaState) {
