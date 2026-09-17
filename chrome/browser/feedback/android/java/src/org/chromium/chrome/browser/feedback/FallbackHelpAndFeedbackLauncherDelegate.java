28462e264d9bf6f1921bcfe09954545b5ca8bbe0
Hao Dong <spdonghao@google.com>
2026-09-16 20:40:09 -0700

    [M153] Revert "[cleanup] Remove USE_P_LINK_IN_HELP flag"
    This reverts commit 15f65ba25967b5d42b0e3031cc61da0f089156c3.
    
    Reason for revert: crbug.com/562480690, revert the removal of this flag, and disable it by default to fix the bug
    
    Original change's description:
    > [cleanup] Remove USE_P_LINK_IN_HELP flag
    >
    > USE_P_LINK_IN_HELP is enabled by default on stable, this CL removes this
    > flag.
    > This CL also cleans up HelpUrlLauncher which is not used.
    >
    > Bug: 365755043
    > Change-Id: I2909834d19f2742cd9c4edcfbc0c8eaa483c15e5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8025955
    > Reviewed-by: Theresa Wellington <twellington@chromium.org>
    > Commit-Queue: Hao Dong <spdonghao@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1680079}
    
    (cherry picked from commit 8755df1a43495eeface77a45f2fa2ff77f1cee00)
    
    Bug: 365755043, 562480690, 562336915
    Change-Id: I0b934a970dd2eea073d6134ce95be1f2645b9df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415316
    Commit-Queue: Theresa Wellington <twellington@chromium.org>
    Commit-Queue: Hao Dong <spdonghao@google.com>
    Reviewed-by: Theresa Wellington <twellington@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1699284}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419345
    Commit-Queue: Harry Souders <harrysouders@google.com>
    Cr-Commit-Position: refs/branch-heads/8010@{#1438}
    Cr-Branched-From: 86cee6df69e0463a839c0cc8435c9d4c259434d3-refs/heads/main@{#1681091}

--- chrome/browser/feedback/android/java/src/org/chromium/chrome/browser/feedback/FallbackHelpAndFeedbackLauncherDelegate.java
+++ chrome/browser/feedback/android/java/src/org/chromium/chrome/browser/feedback/FallbackHelpAndFeedbackLauncherDelegate.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 
 import org.chromium.base.Log;
 import org.chromium.build.annotations.NullMarked;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 
 /**
  * Basic implementation for displaying help support for Chrome.
@@ -21,8 +22,12 @@ public class FallbackHelpAndFeedbackLauncherDelegate implements HelpAndFeedbackL
     @Override
     public void show(Activity activity, String helpContext, FeedbackCollector collector) {
         Log.d(TAG, "Feedback data: %s", collector.getBundle());
-        HelpAndFeedbackLauncherDelegate.launchFallbackSupportUri(
-                activity, HelpAndFeedbackLauncherDelegate.getPLinkHelpUrl(helpContext));
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.USE_P_LINK_IN_HELP)) {
+            HelpAndFeedbackLauncherDelegate.launchFallbackSupportUri(
+                    activity, HelpAndFeedbackLauncherDelegate.getPLinkHelpUrl(helpContext));
+        } else {
+            HelpAndFeedbackLauncherDelegate.launchFallbackSupportUri(activity, /* url= */ null);
+        }
     }
 
     @Override
