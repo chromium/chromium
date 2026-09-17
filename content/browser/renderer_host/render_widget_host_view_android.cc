0f2fe67fcc30f59829ef26471e6dd99953cf2f62
Aman Verma <amanvr@google.com>
2026-09-17 02:37:56 -0700

    [Input] Remove overscroll investigation DumpWithoutCrashing
    Remove the temporary crash keys and DumpWithoutCrashing in
    RenderWidgetHostViewAndroid::OnTouchEvent. The underlying reentrancy
    and UAF issues during ViewAndroid child iteration have been resolved.
    
    Bug: 407571917, 553995407
    Change-Id: I5d2eee9998036c5dd39880b16f73bc36574e9797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8420039
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Kartar Singh <kartarsingh@google.com>
    Auto-Submit: Aman Verma <amanvr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1699637}

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -16,8 +16,6 @@
 #include "base/auto_reset.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
-#include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
@@ -1670,13 +1668,6 @@ bool RenderWidgetHostViewAndroid::OnTouchEvent(
     return true;
   }
 
-  if (is_sequence_overscrolling_) {
-    // TODO(407571917): Remove crash keys after investigation.
-    SCOPED_CRASH_KEY_STRING1024(
-        "crbug407571917", "event_type",
-        base::NumberToString(static_cast<int>(event.GetAction())));
-    base::debug::DumpWithoutCrashing();
-  }
 
   // In case input transfer to Viz is supported, let `input_transfer_handler_`
   // request the transfer on touch down, we are not expecting to receive the
