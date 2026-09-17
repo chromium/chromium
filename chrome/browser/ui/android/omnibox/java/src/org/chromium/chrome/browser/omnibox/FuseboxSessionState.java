8f3ba653c61a406b2accf76c616ef1abe5ecf721
Sky Malice <skym@chromium.org>
2026-09-17 08:48:35 -0700

    Reland "[AIM] Annotate AutocompleteInput enum usages and observers with
    IntDefs."
    
    This is a reland of commit 3eb75b56a80374ed7940e97590d6637db2207816
    
    Original change's description:
    > [AIM] Annotate AutocompleteInput enum usages and observers with
    IntDefs.
    >
    > Annotate enum variables and callback observers across
    AutocompleteInput
    > test and production code with IntDefs (@AutocompleteState,
    > @DisplayState, @RefineActionUsage, @OmniboxFocusReason, and
    > @AutocompleteRequestType).
    >
    > Also add missing DisplayState coverage in AutocompleteInputUnitTest.
    >
    > TAG=agy
    > CONV=ad052f43-692c-40c3-a4a5-f26377145455
    >
    > Bug: 561985020
    > Change-Id: I9fcfcdd6d4e22b9b4171524ab07721de9ee91127
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415425
    > Commit-Queue: Sky Malice <skym@chromium.org>
    > Reviewed-by: Jakob Koblinsky <jkoblinsky@google.com>
    > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > Reviewed-by: Peter Kang <peterkang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1698998}
    
    TAG=agy
    CONV=a3f46016-77ba-4c28-a8f2-2411e67dfc78
    
    Bug: 561985020
    Change-Id: I4af9cfd76a1248449b36a3fa71823eb7d4080ca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8412959
    Reviewed-by: Peter Kang <peterkang@google.com>
    Reviewed-by: Jakob Koblinsky <jkoblinsky@google.com>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1699848}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/FuseboxSessionState.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/FuseboxSessionState.java
@@ -67,7 +67,8 @@ public class FuseboxSessionState implements UserData {
                     FuseboxSessionState.this.onAttachmentListChanged();
                 }
             };
-    private final Callback<Integer> mOnRequestTypeChanged = this::onRequestTypeChanged;
+    private final Callback<@AutocompleteRequestType Integer> mOnRequestTypeChanged =
+            this::onRequestTypeChanged;
 
     /**
      * Details about the user input in the Omnibox. Retained to allow session reconstruction, for
--- components/omnibox/browser/android/java/src/org/chromium/components/omnibox/AutocompleteInputUnitTest.java
+++ components/omnibox/browser/android/java/src/org/chromium/components/omnibox/AutocompleteInputUnitTest.java
@@ -26,6 +26,8 @@ import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.components.metrics.OmniboxEventProtosIntDef.PageClassification;
 import org.chromium.components.omnibox.AimModelsProtoIntDef.ModelMode;
 import org.chromium.components.omnibox.AutocompleteInput.AutocompleteState;
+import org.chromium.components.omnibox.AutocompleteInput.DisplayState;
+import org.chromium.components.omnibox.AutocompleteInput.RefineActionUsage;
 import org.chromium.components.omnibox.AutocompleteInput.SiteSearchData;
 import org.chromium.components.omnibox.ToolModeProtoIntDef.ToolMode;
 import org.chromium.url.GURL;
@@ -41,7 +43,8 @@ public class AutocompleteInputUnitTest {
     @Rule
     public final MockitoRule mMockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
 
-    @Mock private Callback<Integer> mCallback;
+    @Mock private Callback<@AutocompleteState Integer> mCallback;
+    @Mock private Callback<@DisplayState Integer> mDisplayStateCallback;
     @Mock private Callback<GURL> mGurlCallback;
     private final AutocompleteInput mInput = new AutocompleteInput();
 
@@ -394,7 +397,7 @@ public class AutocompleteInputUnitTest {
         boolean[] called = new boolean[1];
         mInput.getRequestTypeSupplier()
                 .addSyncObserver(
-                        requestType -> {
+                        (@AutocompleteRequestType Integer requestType) -> {
                             if (requestType == AutocompleteRequestType.IMAGE_GENERATION) {
                                 assertEquals(ToolMode.TOOL_MODE_IMAGE_GEN, mInput.getToolMode());
                                 called[0] = true;
@@ -520,6 +523,22 @@ public class AutocompleteInputUnitTest {
     }
 
     @Test
+    public void getDisplayStateSupplier_notifiesObservers() {
+        mInput.setDisplayState(DisplayState.WEBSITE);
+        mInput.getDisplayStateSupplier().addSyncObserver(mDisplayStateCallback);
+        mInput.setDisplayState(DisplayState.DRAFTING);
+        verify(mDisplayStateCallback).onResult(DisplayState.DRAFTING);
+    }
+
+    @Test
+    public void getDisplayStateSupplier_resetNotifiesObservers() {
+        mInput.setDisplayState(DisplayState.DRAFTING);
+        mInput.getDisplayStateSupplier().addSyncObserver(mDisplayStateCallback);
+        mInput.reset();
+        verify(mDisplayStateCallback).onResult(DisplayState.WEBSITE);
+    }
+
+    @Test
     public void testCopyFrom() {
         long urlFocusTime = 12345L;
         GURL pageUrl = GURL.emptyGURL();
@@ -529,13 +548,14 @@ public class AutocompleteInputUnitTest {
         String initialUserText = "initialUserText";
         GURL initialPreviewMatchUrl = JUnitTestGURLs.BLUE_1;
         boolean hasAttachments = true;
-        int autocompleteState = AutocompleteState.STANDBY;
+        @AutocompleteState int autocompleteState = AutocompleteState.STANDBY;
+        @DisplayState int displayState = DisplayState.DRAFTING;
         int selectionStart = 1;
         int selectionEnd = 2;
-        int refineActionUsage = AutocompleteInput.RefineActionUsage.SEARCH_WITH_PREFIX;
-        int focusReason = OmniboxFocusReason.OMNIBOX_TAP;
+        @RefineActionUsage int refineActionUsage = RefineActionUsage.SEARCH_WITH_PREFIX;
+        @OmniboxFocusReason int focusReason = OmniboxFocusReason.OMNIBOX_TAP;
         @ModelMode int modelMode = ModelMode.MODEL_MODE_GEMINI_REGULAR;
-        int requestType = AutocompleteRequestType.IMAGE_GENERATION;
+        @AutocompleteRequestType int requestType = AutocompleteRequestType.IMAGE_GENERATION;
         SiteSearchData siteSearchData = new SiteSearchData("keyword", "name");
 
         AutocompleteInput input1 = new AutocompleteInput();
@@ -547,6 +567,7 @@ public class AutocompleteInputUnitTest {
         input1.setInitialInput(initialUserText, initialPreviewMatchUrl);
         input1.setHasAttachments(hasAttachments);
         input1.setAutocompleteState(autocompleteState);
+        input1.setDisplayState(displayState);
         input1.setSelection(new TextSelection(selectionStart, selectionEnd));
         input1.setRefineActionUsage(refineActionUsage);
         input1.setSuggestionsListScrolled();
@@ -567,6 +588,7 @@ public class AutocompleteInputUnitTest {
         assertEquals(initialPreviewMatchUrl, input2.getInitialPreviewMatchUrl());
         assertEquals(input1.allowExactKeywordMatch(), input2.allowExactKeywordMatch());
         assertEquals(autocompleteState, input2.getAutocompleteState());
+        assertEquals(displayState, input2.getDisplayState());
         assertEquals(selectionStart, input2.getSelection().from);
         assertEquals(selectionEnd, input2.getSelection().to);
         assertEquals(refineActionUsage, input2.getRefineActionUsage());
@@ -622,7 +644,7 @@ public class AutocompleteInputUnitTest {
         mInput.setInitialUserText("initial");
         mInput.getAutocompleteStateSupplier()
                 .addSyncObserver(
-                        (state) -> {
+                        (@AutocompleteState Integer state) -> {
                             if (state == AutocompleteState.ENABLED) {
                                 assertEquals(2, mInput.getSelection().from);
                                 assertEquals(2, mInput.getSelection().to);
