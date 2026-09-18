bb4077eb5e5b58590f5d39b883b404d4fa4eee2b
Mei Liang <meiliang@chromium.org>
2026-09-17 23:08:33 -0700

    [histogram-cleanup] Remove expired histogram: CustomTabs.DeviceSpec
    Remove expired histogram CustomTabs.DeviceSpec which expired on
    2025-01-12. Also cleans up the associated enum PcctDeviceSpec,
    unused methods and constants in PartialCustomTabBaseStrategy,
    and the dedicated unit test PartialCustomTabBaseStrategyTest.
    
    This change was generated using the skill histogram-cleanup.
    
    Bug: 499059525, 381877159
    Change-Id: I8b4d951ec57b9fa3829d03b80c5d2735f30acb9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8422342
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700508}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabBaseStrategy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabBaseStrategy.java
@@ -14,9 +14,6 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
 import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
@@ -37,7 +34,6 @@ import androidx.browser.customtabs.CustomTabsCallback;
 
 import org.chromium.base.Callback;
 import org.chromium.base.CallbackUtils;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.build.annotations.EnsuresNonNull;
 import org.chromium.build.annotations.Initializer;
 import org.chromium.build.annotations.NullMarked;
@@ -61,7 +57,6 @@ import java.util.function.BooleanSupplier;
 @NullMarked
 public abstract class PartialCustomTabBaseStrategy extends CustomTabHeightStrategy
         implements FullscreenManager.Observer {
-    private static boolean sDeviceSpecLogged;
 
     protected final Activity mActivity;
     protected final OnResizedCallback mOnResizedCallback;
@@ -129,27 +124,6 @@ public abstract class PartialCustomTabBaseStrategy extends CustomTabHeightStrate
         int COUNT = 4;
     }
 
-    // These values are persisted to logs. Entries should not be renumbered and
-    // numeric values should never be reused.
-    // This should be kept in sync with the definition |PcctDeviceSpec|
-    // in tools/metrics/histograms/enums.xml.
-    @IntDef({
-        DeviceSpec.LOWEND_NOPIP,
-        DeviceSpec.LOWEND_PIP,
-        DeviceSpec.HIGHEND_NOPIP,
-        DeviceSpec.HIGHEND_PIP
-    })
-    @Retention(RetentionPolicy.SOURCE)
-    @interface DeviceSpec {
-        int LOWEND_NOPIP = 0;
-        int LOWEND_PIP = 1;
-        int HIGHEND_NOPIP = 2;
-        int HIGHEND_PIP = 3;
-
-        // Number of elements in the enum
-        int COUNT = 4;
-    }
-
     public PartialCustomTabBaseStrategy(
             Activity activity,
             BrowserServicesIntentDataProvider intentData,
@@ -183,29 +157,6 @@ public abstract class PartialCustomTabBaseStrategy extends CustomTabHeightStrate
         // down to the initial height/width.
         mHeight = MATCH_PARENT;
         mWidth = MATCH_PARENT;
-
-        if (!sDeviceSpecLogged) {
-            logDeviceSpecForPcct(activity);
-            sDeviceSpecLogged = true;
-        }
-    }
-
-    static void logDeviceSpecForPcct(Context context) {
-        var pm = context.getPackageManager();
-        var am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
-        boolean pip = pm.hasSystemFeature(PackageManager.FEATURE_PICTURE_IN_PICTURE);
-        boolean lowEnd = am.isLowRamDevice();
-        @DeviceSpec int spec;
-        if (lowEnd && !pip) {
-            spec = DeviceSpec.LOWEND_NOPIP;
-        } else if (lowEnd && pip) {
-            spec = DeviceSpec.LOWEND_PIP;
-        } else if (!lowEnd && !pip) {
-            spec = DeviceSpec.HIGHEND_NOPIP;
-        } else {
-            spec = DeviceSpec.HIGHEND_PIP;
-        }
-        RecordHistogram.recordEnumeratedHistogram("CustomTabs.DeviceSpec", spec, DeviceSpec.COUNT);
     }
 
     @Override
@@ -673,9 +624,5 @@ public abstract class PartialCustomTabBaseStrategy extends CustomTabHeightStrate
 
     int getShadowOffsetForTesting() {
         return mShadowOffset;
-    }
-
-    static void resetDeviceSpecLoggedForTesting() {
-        sDeviceSpecLogged = false;
     }
 }
