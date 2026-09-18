3491269b9ae824dae7e54020daeb7e0cd636e111
Aishwarya Rajesh <aishwaryarj@google.com>
2026-09-18 07:43:22 -0700

    [ROSP] Remove tasks on app termination in ChromeLifetimeController
    When Chrome terminates deterministically (e.g. via chrome://restart),
    finishing activities with finish() leaves their Android tasks behind
    in Android Recents. This causes stray tasks (such as incognito windows
    or unrecovered windows under NEW_TAB/URLS startup preferences) to
    persist across restarts.
    
    Call finishAndRemoveTask() instead of finish() on running activities in
    ChromeLifetimeController#onTerminate() so that tasks are removed from
    Android Recents upon termination.
    
    Bug: 556432630, 562394604
    Change-Id: I92053cce8180ebf14b9f74631c56e80e15425900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419421
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700762}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ChromeLifetimeController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ChromeLifetimeController.java
@@ -21,6 +21,7 @@ import org.chromium.build.annotations.NullMarked;
 import org.chromium.chrome.browser.BrowserRestartActivity;
 import org.chromium.chrome.browser.lifetime.ApplicationLifetime;
 import org.chromium.chrome.browser.multiwindow.MultiInstanceManager.SessionStartupPolicy;
+import org.chromium.chrome.browser.multiwindow.MultiWindowUtils;
 import org.chromium.chrome.browser.multiwindow.TabbedStartupWindowPolicyDelegate;
 
 /**
@@ -79,11 +80,21 @@ class ChromeLifetimeController
         TabbedStartupWindowPolicyDelegate.getInstance()
                 .maybeSaveSessionStateOnTermination(SessionStartupPolicy.RESTORE_ALL);
 
-        // Tell all Chrome Activities to finish themselves.
+        // Tell all Chrome Activities to finish themselves. When startup window policy is enabled,
+        // the feature supports restoring all windows/tasks on subsequent launch, making it safe to
+        // remove tasks from Android Recents so they do not linger as stray tasks or interfere with
+        // instance allocation on restart.
+        boolean removeTask =
+                MultiWindowUtils.isNewStartupWindowPolicyEnabled()
+                        || MultiWindowUtils.isRestoreOnStartupPrefSyncEnabled();
         for (Activity activity : ApplicationStatus.getRunningActivities()) {
             ApplicationStatus.registerStateListenerForActivity(this, activity);
             mRemainingActivitiesCount++;
-            activity.finish();
+            if (removeTask) {
+                activity.finishAndRemoveTask();
+            } else {
+                activity.finish();
+            }
         }
 
         if (BuildCompat.isAtLeastV()) {
@@ -92,8 +103,7 @@ class ChromeLifetimeController
             mHandler.post(mRestartRunnable);
         } else {
             // Kick off a timer to kill the process after a delay, which fires only if the
-            // Activities
-            // take too long to be finished.
+            // Activities take too long to be finished.
             mHandler.postDelayed(mRestartRunnable, WATCHDOG_DELAY_MS);
         }
     }
