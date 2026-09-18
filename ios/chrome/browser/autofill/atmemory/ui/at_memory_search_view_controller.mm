54b7d3460daf80fd5286fed08b7aa6b6b1cfdccd
Sourav <sinhasourav@google.com>
2026-09-18 11:01:53 -0700

    [iOS][AtMemory] Update 'Previously filled' section header font and color
    Update the 'Previously filled' section header in
    AtMemorySearchViewController to use TableViewLinkHeaderFooterView with
    kTextSecondaryColor, matching the UI mocks.
    
    Mock: https://docs.google.com/presentation/d/1oDDz8z9-3I9PN7ZFsx1tvwoWaB2Hk_j85YsHSI9kukU/edit?slide=id.g3d996af2d99_2_19&resourcekey=0-XsqPKF93d_4JLYZr4O4CFQ#slide=id.g3d996af2d99_2_19
    Screenshot: https://screenshot-v2.corp.google.com/33a2o14v6pjlg
    Bug: 563350122
    Change-Id: Ia01c0f191919f64fee12294d01425724f897ac07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8430223
    Commit-Queue: Sourav Uttam Sinha <sinhasourav@google.com>
    Reviewed-by: Leo Zhao <leozhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700958}

--- ios/chrome/browser/autofill/atmemory/ui/at_memory_search_view_controller.mm
+++ ios/chrome/browser/autofill/atmemory/ui/at_memory_search_view_controller.mm
@@ -24,7 +24,6 @@
 #import "ios/chrome/browser/shared/ui/buildflags.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_link_header_footer_item.h"
-#import "ios/chrome/browser/shared/ui/table_view/cells/table_view_text_header_footer_item.h"
 #import "ios/chrome/browser/shared/ui/table_view/content_configuration/activity_indicator_content_configuration.h"
 #import "ios/chrome/browser/shared/ui/table_view/content_configuration/colorful_symbol_content_configuration.h"
 #import "ios/chrome/browser/shared/ui/table_view/content_configuration/table_view_cell_content_configuration.h"
@@ -134,7 +133,6 @@ enum class ItemIdentifier {
   self.title = l10n_util::GetNSString(IDS_IOS_AUTOFILL_AI_FIND_AND_FILL_TITLE);
 
   RegisterTableViewHeaderFooter<TableViewLinkHeaderFooterView>(self.tableView);
-  RegisterTableViewHeaderFooter<TableViewTextHeaderFooterView>(self.tableView);
   // Remove extra spacing on top of sections.
   self.tableView.sectionHeaderTopPadding = 0;
   self.tableView.backgroundColor =
@@ -250,11 +248,12 @@ enum class ItemIdentifier {
 
   switch (sectionIdentifier) {
     case SectionIdentifier::kRecentFillsSection: {
-      TableViewTextHeaderFooterView* header =
-          DequeueTableViewHeaderFooter<TableViewTextHeaderFooterView>(
+      TableViewLinkHeaderFooterView* header =
+          DequeueTableViewHeaderFooter<TableViewLinkHeaderFooterView>(
               tableView);
-      [header setTitle:l10n_util::GetNSString(
-                           IDS_AUTOFILL_AT_MEMORY_PREVIOUSLY_FILLED)];
+      [header setText:l10n_util::GetNSString(
+                          IDS_AUTOFILL_AT_MEMORY_PREVIOUSLY_FILLED)
+            withColor:[UIColor colorNamed:kTextSecondaryColor]];
       return header;
     }
     case SectionIdentifier::kAutofillSearchResultsSection: {
