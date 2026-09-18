7f2c0b30afc3b6b310287fd5b2828890969e8b22
Sky Malice <skym@chromium.org>
2026-09-18 10:22:06 -0700

    [Test] Deflake and re-enable OmniboxPedalsRenderTest
    Move OmniboxCapabilities.setHasDesktopExperienceForTesting(false) from
    the test class constructor into @Before setUp() so ResettersForTesting
    does not clear the override after the first batched test method in each
    parameter set.
    
    TAG=agy
    CONV=7a358a60-4db9-4bdd-ac0c-6d1477ea3d7b
    
    Bug: 479724464
    Change-Id: I42a501d45a4a5c28f4abf98b62b1d0345e912b05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8429266
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700914}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsRenderTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsRenderTest.java
@@ -18,7 +18,6 @@ import org.chromium.base.test.params.ParameterSet;
 import org.chromium.base.test.params.ParameterizedRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.night_mode.ChromeNightModeTestUtils;
@@ -37,7 +36,6 @@ import org.chromium.components.omnibox.AutocompleteResult;
 import org.chromium.components.omnibox.OmniboxCapabilities;
 import org.chromium.components.omnibox.OmniboxSuggestionType;
 import org.chromium.components.omnibox.action.OmniboxPedalId;
-import org.chromium.ui.base.DeviceFormFactor;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -76,7 +74,6 @@ public class OmniboxPedalsRenderTest {
     private OmniboxTestUtils mOmniboxUtils;
 
     public OmniboxPedalsRenderTest(boolean nightMode) {
-        OmniboxCapabilities.setHasDesktopExperienceForTesting(false);
         ChromeNightModeTestUtils.setUpNightModeForChromeActivity(nightMode);
         mRenderTestRule.setNightModeEnabled(nightMode);
         mRenderTestRule.setVariantPrefix("RegularTab");
@@ -89,6 +86,7 @@ public class OmniboxPedalsRenderTest {
 
     @Before
     public void setUp() throws InterruptedException {
+        OmniboxCapabilities.setHasDesktopExperienceForTesting(false);
         mStartingPage = mActivityTestRule.startOnBlankPage();
         mOmniboxUtils = new OmniboxTestUtils(mStartingPage.getActivity());
         mOmniboxUtils.requestFocus();
@@ -117,7 +115,6 @@ public class OmniboxPedalsRenderTest {
     @Test
     @MediumTest
     @Feature("RenderTest")
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/479724464
     public void testRunChromeSafetyCheckPedal() throws IOException, InterruptedException {
         List<AutocompleteMatch> suggestionsList = new ArrayList<>();
         suggestionsList.add(
@@ -132,7 +129,6 @@ public class OmniboxPedalsRenderTest {
     @Test
     @MediumTest
     @Feature("RenderTest")
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // https://crbug.com/479724464
     public void testPlayChromeDinoGamePedal() throws IOException, InterruptedException {
         List<AutocompleteMatch> suggestionsList = new ArrayList<>();
         suggestionsList.add(
