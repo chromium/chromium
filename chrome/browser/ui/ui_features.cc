8217b67b1fe146c542153ac83eee74c599e2385b
William Wong <willzawong@google.com>
2026-09-18 15:33:55 -0700

    Enable EnterpriseReleaseNotes by default
    BUG= b:454939617
    
    Change-Id: I045e4ed520989db25b67a1287cdd2a86adcd9e30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8423071
    Reviewed-by: Owen Min <zmin@chromium.org>
    Commit-Queue: William Wong <willzawong@google.com>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1701150}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -320,7 +320,7 @@ BASE_FEATURE(kNTPFooterBadgingPolicies, base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEnterpriseManagementDisclaimerUsesCustomLabel,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kEnterpriseReleaseNotes, base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnterpriseReleaseNotes, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kManagedProfileRequiredInterstitial,
              base::FEATURE_ENABLED_BY_DEFAULT);
