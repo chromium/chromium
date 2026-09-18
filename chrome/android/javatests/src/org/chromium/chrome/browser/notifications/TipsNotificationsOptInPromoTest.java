0efab9c3f5ca119e9aa3d600f30c6ab9af30ec74
Brandon Fong <bjfong@google.com>
2026-09-17 17:58:19 -0700

    [tips] Remove feature disable since tips is disabled on desktop
    Tips was disabled on desktop in a recent change, so this feature guard
    should be safe to remove since it is already disabled.
    
    Bug: 561423545
    Change-Id: I104cfc4b14689fc11c0f43f48f60ea96602e4c2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8415129
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1700345}

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/TipsNotificationsOptInPromoTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/TipsNotificationsOptInPromoTest.java
@@ -35,7 +35,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.IntentHandler;
@@ -59,7 +58,6 @@ import java.io.IOException;
 /** Integration and render tests for the tips notifications opt in promo. */
 @RunWith(ChromeJUnit4ClassRunner.class)
 @EnableFeatures({ChromeFeatureList.ANDROID_TIPS_NOTIFICATIONS + ":always_show_opt_in_promo/true"})
-@DisableFeatures(ChromeFeatureList.BOTTOM_SHEET_ON_DESKTOP_WINDOWING) // crbug.com/561423545
 @Batch(Batch.PER_CLASS)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_STARTUP_PROMOS})
 @Restriction({DeviceRestriction.RESTRICTION_TYPE_NON_AUTO, DeviceFormFactor.PHONE_OR_TABLET})
