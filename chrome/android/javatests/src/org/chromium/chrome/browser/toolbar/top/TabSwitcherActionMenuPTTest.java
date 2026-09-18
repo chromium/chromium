47b62e39724a2265f9db595207593fb07f5c57b1
Sky Malice <skym@chromium.org>
2026-09-17 18:33:55 -0700

    [Test] Use fast opener in testOpenNewIncognitoTabFromIncognito
    Re-enables testOpenNewIncognitoTabFromIncognito on Desktop Android by
    switching precondition setup to openNewIncognitoTabOrWindowFast().
    
    This avoids Espresso AppNotIdleException timeouts during menu-driven
    window transitions during setup on desktop while maintaining full test
    coverage of the tab switcher action menu interaction under test.
    
    TAG=agy
    CONV=f3eae6ce-294d-47a0-bd94-5ea35094612a
    
    Bug: 536995089, 562123843
    Change-Id: Iea36d0c12c6a93804e047c37f19144e42c4d6035
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8425540
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1700376}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuPTTest.java
@@ -21,6 +21,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.DisableFeatures;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.incognito.IncognitoUtils;
@@ -93,19 +94,21 @@ public class TabSwitcherActionMenuPTTest {
 
     @Test
     @LargeTest
-    @DisableIf.Device(DeviceFormFactor.DESKTOP) // TODO(crbug.com/536995089): Re-enable once passing
     public void testOpenNewIncognitoTabFromIncognito() {
         IncognitoNewTabPageStation incognitoNtp =
-                mCtaTestRule
-                        .startOnBlankPage()
-                        .openTabSwitcherActionMenu()
-                        .selectNewIncognitoTabOrWindow();
+                mCtaTestRule.startOnBlankPage().openNewIncognitoTabOrWindowFast();
+        ChromeTabbedActivity initialIncognitoActivity = incognitoNtp.getActivity();
 
         IncognitoNewTabPageStation page =
                 incognitoNtp.openTabSwitcherActionMenu().selectNewIncognitoTab();
 
+        ChromeTabbedActivity newPageActivity = page.getActivity();
         assertTrue(page.getTabModelSelector().isIncognitoSelected());
-        assertEquals(2, getTabCountOnUiThread(page.getActivity().getCurrentTabModel()));
+        assertEquals(initialIncognitoActivity, newPageActivity);
+        if (IncognitoUtils.shouldOpenIncognitoAsWindow()) {
+            assertNotEquals(mCtaTestRule.getActivity(), newPageActivity);
+        }
+        assertEquals(2, getTabCountOnUiThread(newPageActivity.getCurrentTabModel()));
     }
 
     @Test
