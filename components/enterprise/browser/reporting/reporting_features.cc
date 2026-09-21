a69f586be99b150cd6f1682ad947a721112790aa
Qiutan Wu <qiutanw@google.com>
2026-09-21 11:53:42 -0700

    [CliSwitchesReporting] Enable BrowserLaunchMetadataReporting by default.
    This change flips the default state of the kBrowserLaunchMetadataReporting
    feature flag from disabled to enabled as the feature is being launched.
    The field trial testing config for BrowserLaunchMetadataReporting is also
    removed.
    
    Bug: b:492328505
    Change-Id: I26442f22d9a91c881fc5769b33958ec7a38fb3cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8432071
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Qiutan Wu <qiutanw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1701937}

--- components/enterprise/browser/reporting/reporting_features.cc
+++ components/enterprise/browser/reporting/reporting_features.cc
@@ -19,7 +19,7 @@ BASE_FEATURE(kSaasUsageReporting,
 BASE_FEATURE(kGeminiInChromeUsageReporting, base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kBrowserLaunchMetadataReporting,
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kCbcmAndroidPackageNameIdentifier,
