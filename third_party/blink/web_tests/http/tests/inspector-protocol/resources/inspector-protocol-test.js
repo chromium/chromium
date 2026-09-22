8979939cf3337e8a568a915f119d9913074933b6
Sergei Rudenkov <sergeirudenkov@google.com>
2026-09-22 03:19:39 -0700

    inspector-protocol: Resolve CDP types from in-tree generated files
    Previously, writing inspector-protocol tests required developers to
    globally install the `devtools-protocol` npm package to get IDE type
    definitions.
    
    This change updates the tsconfig files to resolve protocol types from
    the in-tree generated definitions in `out/Default/gen/...`, and updates
    documentation on how to generate them via autoninja.
    
    Bug: 551716676
    Test: Open web_tests/http/tests/inspector-protocol/resources/inspector-protocol-test.js and verify type resolution.
    Change-Id: I11b499901b15b99b2169fdb6809626359d29c98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8430559
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Sergei Rudenkov <sergeirudenkov@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702466}

--- third_party/blink/web_tests/http/tests/inspector-protocol/resources/inspector-protocol-test.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/resources/inspector-protocol-test.js
@@ -4,13 +4,18 @@
 
 /**
  * @fileoverview
- * To have the IDE support for types when writing inspector-protocol tests:
+ * To have the IDE support for types when writing inspector-protocol tests,
+ * generate the Chrome DevTools Protocol typings from the .pdl files in this
+ * checkout:
  *
- * - `npm i devtools-protocol -g`
- * - `cd $HOME && npm link devtools-protocol`
+ *   autoninja -C out/Default \
+ *     third_party/blink/public/devtools_protocol:protocol_typescript_definitions
  *
- * Note that `devtools-protocol` package won't include your local changes
- * to the protocol and might be slightly out-of-date. Update it from time to time.
+ * The typings are written to
+ * `out/Default/gen/third_party/blink/public/devtools_protocol/types/`, which is
+ * where the `tsconfig.json` next to the tests points to. Regenerate them after
+ * changing a .pdl file. If you use an output directory other than
+ * `out/Default`, adjust the `paths` entry in `tsconfig.json` locally.
  */
 
 /**
