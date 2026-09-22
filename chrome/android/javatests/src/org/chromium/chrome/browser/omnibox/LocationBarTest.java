2acf214984544e4596d28c43eea6d8f5a5737524
Linyu He <linyuh@google.com>
2026-09-22 15:16:52 -0700

    Gardening: Disable LocationBarTest#testFocusLogic_keyboardVisibility on R
    This test is flaky across Android 11 (R) Pixel 4 (flame) builders:
    * android-arm64-tests
    * android-arm-tests
    * android-arm64-official-tests
    * android-arm-official-tests
    
    This CL disables the test on R to preserve test coverage on other
    Android versions.
    
    Bug: 565015956
    Change-Id: Ib2ec327ee5e0c6349db03688f5f968dc615ef777
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8445721
    Reviewed-by: Peilin Wang <peilinwang@google.com>
    Owners-Override: Linyu He <linyuh@google.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Linyu He <linyuh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702991}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarTest.java
@@ -20,6 +20,7 @@ import static org.chromium.base.test.transit.ViewFinder.waitForNoView;
 import static org.chromium.base.test.util.Criteria.checkThat;
 import static org.chromium.chrome.browser.url_constants.UrlConstantResolver.getOriginalNativeNtpUrl;
 
+import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -42,6 +43,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -504,6 +506,7 @@ public class LocationBarTest {
 
     @Test
     @MediumTest
+    @DisableIf.Build(sdk_equals = Build.VERSION_CODES.R, message = "https://crbug.com/565015956")
     public void testFocusLogic_keyboardVisibility() {
         startActivityNormally();
         assertFalse(mKeyboardDelegate.isKeyboardShowing(mUrlBar));
