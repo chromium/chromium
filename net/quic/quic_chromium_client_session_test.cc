3a2c8254f04629a3092fa35781b2d92f828e1fa8
Stefano Duo <stefanoduo@google.com>
2026-09-22 08:34:35 -0700

    Fix stale checks for RedundantOutcome
    RedundantOutcome was renamed to SpuriousOutcome but apparently we forgot
    to update a few tests (which kept passing because we're checking for
    something not being logged).
    
    Bug: 557126867
    Change-Id: I73a542da832c2c2a1ffcdc60c80e4ab3c4513cbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8442496
    Reviewed-by: Mohannad Farrag <aymanm@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702650}

--- net/quic/quic_chromium_client_session_test.cc
+++ net/quic/quic_chromium_client_session_test.cc
@@ -2421,7 +2421,7 @@ TEST_P(QuicChromiumClientSessionTest, MaybeCancelProbing_InFlightAttemptFails) {
       "Net.Quic.Migration.Attempt.FailureReason.ByTrigger.OnNetworkMadeDefault",
       QuicMigrationAttemptFailureReason::kProbeFailed, 1);
   histogram_tester.ExpectTotalCount(
-      "Net.Quic.Migration.Attempt.RedundantOutcome", 0);
+      "Net.Quic.Migration.Attempt.SpuriousOutcome", 0);
   histogram_tester.ExpectTotalCount(
       "Net.Quic.Migration.Attempt.UnclassifiedOutcome", 0);
   histogram_tester.ExpectUniqueSample("Net.QuicSession.ConnectionMigration",
@@ -2489,7 +2489,7 @@ TEST_P(QuicChromiumClientSessionTest, MaybeCancelProbing_IneligibleReason) {
   histogram_tester.ExpectTotalCount("Net.Quic.Migration.Attempt.FailureReason",
                                     0);
   histogram_tester.ExpectTotalCount(
-      "Net.Quic.Migration.Attempt.RedundantOutcome", 0);
+      "Net.Quic.Migration.Attempt.SpuriousOutcome", 0);
   histogram_tester.ExpectTotalCount(
       "Net.Quic.Migration.Attempt.UnclassifiedOutcome", 0);
   histogram_tester.ExpectUniqueSample("Net.QuicSession.ConnectionMigration",
@@ -2555,7 +2555,7 @@ TEST_P(QuicChromiumClientSessionTest, MaybeCancelProbing_SupersededReason) {
   histogram_tester.ExpectTotalCount("Net.Quic.Migration.Attempt.FailureReason",
                                     0);
   histogram_tester.ExpectTotalCount(
-      "Net.Quic.Migration.Attempt.RedundantOutcome", 0);
+      "Net.Quic.Migration.Attempt.SpuriousOutcome", 0);
   histogram_tester.ExpectTotalCount(
       "Net.Quic.Migration.Attempt.UnclassifiedOutcome", 0);
   histogram_tester.ExpectUniqueSample(
@@ -2624,7 +2624,7 @@ TEST_P(QuicChromiumClientSessionTest, PathValidationFailure_RetryTimeout) {
       "Net.Quic.Migration.Attempt.FailureReason.ByTrigger.OnNetworkMadeDefault",
       QuicMigrationAttemptFailureReason::kProbeTimeout, 1);
   histogram_tester.ExpectTotalCount(
-      "Net.Quic.Migration.Attempt.RedundantOutcome", 0);
+      "Net.Quic.Migration.Attempt.SpuriousOutcome", 0);
   histogram_tester.ExpectTotalCount(
       "Net.Quic.Migration.Attempt.UnclassifiedOutcome", 0);
   histogram_tester.ExpectUniqueSample("Net.QuicSession.ConnectionMigration",
--- net/quic/quic_migration_attempt_context_unittest.cc
+++ net/quic/quic_migration_attempt_context_unittest.cc
@@ -275,7 +275,7 @@ TEST_F(QuicMigrationAttemptContextTest, SetSuccess) {
   histogram_tester.ExpectTotalCount("Net.Quic.Migration.Attempt.Ineligible", 0);
   histogram_tester.ExpectTotalCount("Net.Quic.Migration.Attempt.Superseded", 0);
   histogram_tester.ExpectTotalCount(
-      "Net.Quic.Migration.Attempt.RedundantOutcome", 0);
+      "Net.Quic.Migration.Attempt.SpuriousOutcome", 0);
   histogram_tester.ExpectTotalCount(
       "Net.Quic.Migration.Attempt.UnclassifiedOutcome", 0);
 }
@@ -333,7 +333,7 @@ TEST_F(QuicMigrationAttemptContextTest, SetFailure) {
   histogram_tester.ExpectTotalCount("Net.Quic.Migration.Attempt.Ineligible", 0);
   histogram_tester.ExpectTotalCount("Net.Quic.Migration.Attempt.Superseded", 0);
   histogram_tester.ExpectTotalCount(
-      "Net.Quic.Migration.Attempt.RedundantOutcome", 0);
+      "Net.Quic.Migration.Attempt.SpuriousOutcome", 0);
   histogram_tester.ExpectTotalCount(
       "Net.Quic.Migration.Attempt.UnclassifiedOutcome", 0);
 }
