c8301c69e39e58dc65f6b3e072fbb7cc382977fb
Yann Dago <ydago@chromium.org>
2026-09-22 09:44:57 -0700

    [policy] Enable EnableExtensionInstallPolicyFetching on ChromeOS
    Enable the kEnableExtensionInstallPolicyFetching feature flag by default
    on ChromeOS now that the Finch experiment has concluded.
    
    Also remove the EnableExtensionInstallPolicyFetchingChromeOS testing
    configuration from fieldtrial_testing_config.json.
    
    TAG=agy
    CONV=ed83dbb8-9a5b-4fc5-9dbf-32f0e9920ac3
    
    Bug: 452305191
    Change-Id: I4eb82fcfae69aa50a20301f2edd6b374a4b3913a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8441018
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702725}

--- components/policy/core/common/features.cc
+++ components/policy/core/common/features.cc
@@ -53,7 +53,7 @@ const base::FeatureParam<std::string> kDesktopAndroidPolicyBlocklist{
 
 // Used to enable extension install policy support.
 BASE_FEATURE(kEnableExtensionInstallPolicyFetching,
-#if BUILDFLAG(ENABLE_EXTENSIONS_CORE) && !BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(ENABLE_EXTENSIONS_CORE)
              base::FEATURE_ENABLED_BY_DEFAULT);
 #else
              base::FEATURE_DISABLED_BY_DEFAULT);
