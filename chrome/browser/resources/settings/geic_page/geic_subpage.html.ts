29afdfece50141f87a60b998ae3848935eeaf545
Salma Elmahallawy <esalma@google.com>
2026-09-22 07:58:57 -0700

    [GEIC] Add setting for automatically sharing tab context.
    This CL introduces the setting for sharing tab context by default for
    glic. The setting shares the same pref used by the glic setting, and
    this is WAI.
    
    Screenshot: https://screenshot-v2.corp.google.com/37v3aon69r1d0
    
    Bug: 556354272
    Change-Id: I0c31480bd35d841abf8e127ef8f8ee72c78940eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8400288
    Reviewed-by: Austin Zhang <austinzzr@google.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1702622}

--- chrome/browser/resources/settings/geic_page/geic_subpage.html.ts
+++ chrome/browser/resources/settings/geic_page/geic_subpage.html.ts
@@ -19,6 +19,86 @@ export function getHtml(this: SettingsGeicSubpageElement) {
         sub-label="$i18n{glicTabstripButtonToggleSublabel}">
     </settings-toggle-button>
   </div>
+  <div class="section">
+    <h2 class="cr-title-text">$i18n{glicDataSection}</h2>
+    <settings-toggle-button
+        id="defaultTabAccessToggle"
+        pref-key="glic.default_tab_context_enabled"
+        label="$i18n{glicDefaultTabAccessToggle}"
+        sub-label-with-link="${this.getDefaultTabAccessSubLabel_()}"
+        @sub-label-link-clicked="${
+            this.onDefaultTabAccessToggleSubLabelLinkClicked_}"
+        no-toggle-on-host-click
+        @click="${this.onDefaultTabAccessExpandClick_}">
+      <div id="defaultTabAccessToggleActions"
+          class="toggle-actions-container"
+          slot="more-actions">
+        <cr-expand-button id="defaultTabAccessExpandButton" no-hover
+            ?expanded="${this.defaultTabAccessToggleExpanded_}"
+            @expanded-changed="${
+                this.onDefaultTabAccessToggleExpandedChanged_}"
+            aria-label="$i18n{glicDefaultTabAccessToggle}">
+        </cr-expand-button>
+        <div class="separator"></div>
+      </div>
+    </settings-toggle-button>
+    <cr-collapse id="defaultTabAccessInfoCollapse"
+        ?opened="${this.defaultTabAccessToggleExpanded_}">
+      <div class="settings-columned-section">
+        <div class="column">
+          <h2 class="description-header">$i18n{columnHeadingWhenOn}</h2>
+          <ul class="icon-bulleted-list">
+            <li>
+<if expr="not _google_chrome">
+              <cr-icon aria-hidden="true" icon="settings20:edit-square">
+              </cr-icon>
+</if>
+<if expr="_google_chrome">
+              <cr-icon aria-hidden="true"
+                  icon="settings-internal:text-analysis">
+              </cr-icon>
+</if>
+              <div class="secondary">
+                $i18n{glicDefaultTabAccessWhenOn1}
+              </div>
+            </li>
+            <li>
+<if expr="not _google_chrome">
+              <cr-icon aria-hidden="true" icon="settings20:edit-square">
+              </cr-icon>
+</if>
+<if expr="_google_chrome">
+              <cr-icon aria-hidden="true"
+                  icon="settings-internal:auto-tab-group">
+              </cr-icon>
+</if>
+              <div class="secondary">
+                $i18n{glicDefaultTabAccessWhenOn2}
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="column">
+          <h2 class="description-header">$i18n{columnHeadingConsider}</h2>
+          <ul class="icon-bulleted-list">
+            <li>
+<if expr="not _google_chrome">
+              <cr-icon aria-hidden="true" icon="settings20:web"></cr-icon>
+</if>
+<if expr="_google_chrome">
+              <cr-icon aria-hidden="true"
+                  icon="settings-internal:screensaver-auto">
+              </cr-icon>
+</if>
+              <div class="secondary">
+                $i18n{glicDefaultTabAccessConsider1}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </cr-collapse>
+  </div>
 </settings-subpage>
 <!--_html_template_end_-->`;
 }
--- chrome/browser/resources/settings/geic_page/geic_subpage.ts
+++ chrome/browser/resources/settings/geic_page/geic_subpage.ts
@@ -2,9 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/cr_elements/cr_collapse/cr_collapse.js';
+import 'chrome://resources/cr_elements/cr_expand_button/cr_expand_button.js';
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import '../controls/settings_toggle_button.js';
+import '../icons.html.js';
 import '../settings_page/settings_subpage.js';
+// <if expr="_google_chrome">
+import '../internal/icons.html.js';
 
+// </if>
+
+import {I18nMixinLit} from 'chrome://resources/cr_elements/i18n_mixin_lit.js';
+import {OpenWindowProxyImpl} from 'chrome://resources/js/open_window_proxy.js';
 import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
 import {SettingsViewMixinLit} from '../settings_page/settings_view_mixin_lit.js';
@@ -12,7 +22,8 @@ import {SettingsViewMixinLit} from '../settings_page/settings_view_mixin_lit.js'
 import {getCss} from './geic_subpage.css.js';
 import {getHtml} from './geic_subpage.html.js';
 
-const SettingsGeicSubpageElementBase = SettingsViewMixinLit(CrLitElement);
+const SettingsGeicSubpageElementBase =
+    SettingsViewMixinLit(I18nMixinLit(CrLitElement));
 
 export class SettingsGeicSubpageElement extends SettingsGeicSubpageElementBase {
   static get is() {
@@ -25,6 +36,41 @@ export class SettingsGeicSubpageElement extends SettingsGeicSubpageElementBase {
 
   override render() {
     return getHtml.bind(this)();
+  }
+
+  static override get properties() {
+    return {
+      defaultTabAccessToggleExpanded_: {type: Boolean},
+    };
+  }
+
+  protected accessor defaultTabAccessToggleExpanded_: boolean = false;
+
+  protected onDefaultTabAccessExpandClick_() {
+    this.defaultTabAccessToggleExpanded_ =
+        !this.defaultTabAccessToggleExpanded_;
+  }
+
+  protected onDefaultTabAccessToggleExpandedChanged_(
+      e: CustomEvent<{value: boolean}>) {
+    this.defaultTabAccessToggleExpanded_ = e.detail.value;
+  }
+
+  // i18nAdvanced is needed to allow for translating strings containing HTML.
+  // The sublabel contains <ph> elements which are translated to <a> tags to
+  // provide a link in the label.
+  //
+  // GEiC is only available to enterprise accounts whose data is protected, so
+  // the data-protected variant of the glic string always applies.
+  protected getDefaultTabAccessSubLabel_(): string {
+    return this.i18nAdvanced('glicDefaultTabAccessToggleSublabelDataProtected')
+        .toString();
+  }
+
+  // TODO(crbug.com/564532074): Update the learn more link.
+  protected onDefaultTabAccessToggleSubLabelLinkClicked_() {
+    OpenWindowProxyImpl.getInstance().openUrl(
+        this.i18n('glicDefaultTabAccessToggleLearnMoreUrlDataProtected'));
   }
 
   // SettingsViewMixin implementation.
