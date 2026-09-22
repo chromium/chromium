7580cf8cb9d1d3026ce0a16d7f15a58b386f1064
Jordan Bayles <jophba@chromium.org>
2026-09-22 13:13:37 -0700

    [M155] Revert "Remove class WebAudioBus"
    Original change's description:
    > Revert "Remove class WebAudioBus"
    >
    > This reverts commit 768c89d2c0f1a484a2c4d874512c4f260925a9c6 and commit 9e6d178b3492c058a2c76fcf784d627cee6283a3.
    >
    > Reason for revert:
    > Causes a high-volume P0/S2 crash spike on 32-bit Android (b/562782946).
    > Switching from WebAudioBus to media::AudioBus changed memory allocation
    > in DecodeAudioFileData from Blink's fallible allocator
    > (Partitions::BufferTryAlignedZeroedMalloc) to media::AudioBus's
    > infallible base::AlignedAlloc(), leading PartitionAlloc to terminate the
    > renderer on OOM instead of safely returning nullptr / rejecting the
    > decode promise.
    >
    > Reverting commit 768c89d2c0f1a484a2c4d874512c4f260925a9c6 is also
    > included as it was a follow-up that removed ResizeSmaller/MaxAbsValue
    > needed by WebAudioBus.
    >
    > Original change's description:
    > > Remove class WebAudioBus
    > >
    > > This should cause no functional change.
    > >
    > > The functionality of this class can be replaced directly with AudioBus,
    > > so remove it to ease maintainability and reduce complexity.
    > >
    > > Change-Id: Ib2c8db1ed65adee4abb1d75da49d58f6ce03bfaa
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8308032
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Reviewed-by: Hongchan Choi <hongchan@chromium.org>
    > > Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    > > Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1696056}
    >
    > Bug: 562782946
    > Change-Id: Iedadeb6f95ef64cc5fc18d984a90df5655bda971
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8433454
    > Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    > Commit-Queue: Jordan Bayles <jophba@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1702385}
    
    (cherry picked from commit 86298bb90e31129a07178dc11be85675585e9ca2)
    
    Bug: 564722037,562782946
    Change-Id: Iedadeb6f95ef64cc5fc18d984a90df5655bda971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8446497
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8059@{#437}
    Cr-Branched-From: 199ac2cd7ab289e11c2236bd2cede848781213da-refs/heads/main@{#1697595}

--- third_party/blink/renderer/platform/audio/audio_bus.cc
+++ third_party/blink/renderer/platform/audio/audio_bus.cc
@@ -41,6 +41,7 @@
 #include "media/base/audio_bus.h"
 #include "media/base/sinc_resampler.h"
 #include "third_party/blink/public/platform/platform.h"
+#include "third_party/blink/public/platform/web_audio_bus.h"
 #include "third_party/blink/renderer/platform/audio/audio_utilities.h"
 #include "third_party/blink/renderer/platform/audio/denormal_disabler.h"
 #include "third_party/blink/renderer/platform/audio/vector_math.h"
@@ -85,24 +86,6 @@ scoped_refptr<AudioBus> AudioBus::TryCreate(unsigned number_of_channels,
   return bus;
 }
 
-scoped_refptr<AudioBus> AudioBus::CreateFromMediaAudioBus(
-    std::unique_ptr<media::AudioBus> media_bus,
-    float sample_rate) {
-  if (!media_bus || media_bus->is_bitstream_format()) {
-    return nullptr;
-  }
-  if (!base::IsValueInRangeForNumericType<uint32_t>(media_bus->frames()) ||
-      !base::IsValueInRangeForNumericType<unsigned>(media_bus->channels()) ||
-      media_bus->channels() > static_cast<int>(kMaxBusChannels)) {
-    return nullptr;
-  }
-
-  scoped_refptr<AudioBus> bus =
-      base::AdoptRef(new AudioBus(std::move(media_bus)));
-  bus->SetSampleRate(sample_rate);
-  return bus;
-}
-
 AudioBus::AudioBus(unsigned number_of_channels, uint32_t length, bool allocate)
     : length_(length) {
   channels_.ReserveInitialCapacity(number_of_channels);
@@ -114,21 +97,6 @@ AudioBus::AudioBus(unsigned number_of_channels, uint32_t length, bool allocate)
       channels_.emplace_back();
     }
   }
-}
-
-AudioBus::AudioBus(std::unique_ptr<media::AudioBus> media_bus)
-    : underlying_media_bus_(std::move(media_bus)),
-      length_(static_cast<uint32_t>(underlying_media_bus_->frames())) {
-  channels_.ReserveInitialCapacity(
-      static_cast<unsigned>(underlying_media_bus_->channels()));
-  for (base::span<float> channel : underlying_media_bus_->AllChannels()) {
-    channels_.emplace_back();
-    channels_.back().Set(channel);
-  }
-}
-
-AudioBus::~AudioBus() {
-  channels_.clear();
 }
 
 void AudioBus::SetChannelMemory(unsigned channel_index,
@@ -804,13 +772,12 @@ void AudioBus::ClearSilentFlag() {
 }
 
 scoped_refptr<AudioBus> DecodeAudioFileData(base::span<const char> data) {
-  std::unique_ptr<Platform::DecodedAudioFile> decoded =
+  std::unique_ptr<WebAudioBus> out =
       Platform::Current()->DecodeAudioFileData(data);
-  if (!decoded || !decoded->bus) {
-    return nullptr;
+  if (out) {
+    return out->Release();
   }
-  return AudioBus::CreateFromMediaAudioBus(
-      std::move(decoded->bus), static_cast<float>(decoded->sample_rate));
+  return nullptr;
 }
 
 scoped_refptr<AudioBus> AudioBus::GetDataResource(int resource_id,
