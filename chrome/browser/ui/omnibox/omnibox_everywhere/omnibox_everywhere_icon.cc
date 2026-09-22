06fd1ab6ddbe09db1bd30ee88908473532e529bb
Kaan Alsan <alsan@chromium.org>
2026-09-22 10:21:42 -0700

    [OmniboxEverywhere] Use 2x icon representation for Windows status tray icon
    On Windows, StatusIconWin::SetImage() and
    HWNDMessageHandler::SetWindowIcons() construct HICONs from
    *image.bitmap(), which unconditionally extracts the 1.0f scale
    representation (16x16 px). Wrap the 2.0f representation (32x32 px) as a
    1x bitmap on Windows so the system tray and window HICONs are
    high-resolution on High-DPI displays.
    
    Before: https://screenshot-v2.corp.google.com/2lglq6k2qkuj0
    After: https://screenshot-v2.corp.google.com/4uij5850n6e5o
    Bug: 532187976
    Change-Id: I83b94ad991c14919ad3433f48dc5d633a2c5baa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8443196
    Reviewed-by: Atharv Maan <atharvmaan@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1702753}

--- chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_icon.cc
+++ chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_icon.cc
@@ -5,8 +5,10 @@
 #include "chrome/browser/ui/omnibox/omnibox_everywhere/omnibox_everywhere_icon.h"
 
 #include "build/branding_buildflags.h"
+#include "build/build_config.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/image/image_skia_rep.h"
 #include "ui/gfx/paint_vector_icon.h"
 
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -16,12 +18,33 @@
 
 namespace omnibox_everywhere {
 
-gfx::ImageSkia GetOmniboxEverywhereIcon() {
+namespace {
+
+gfx::ImageSkia GetBaseIcon() {
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   return *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
       IDR_GOOGLE_G_GRADIENT_16_ALT);
 #else
-  return gfx::CreateVectorIcon(vector_icons::kSearchIcon, 16, SK_ColorBLACK);
+#if BUILDFLAG(IS_WIN)
+  constexpr int kIconDipSize = 32;
+#else
+  constexpr int kIconDipSize = 16;
+#endif
+  return gfx::CreateVectorIcon(vector_icons::kSearchIcon, kIconDipSize,
+                               SK_ColorBLACK);
+#endif
+}
+
+}  // namespace
+
+gfx::ImageSkia GetOmniboxEverywhereIcon() {
+  gfx::ImageSkia icon = GetBaseIcon();
+
+#if BUILDFLAG(IS_WIN) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  return gfx::ImageSkia::CreateFrom1xBitmap(
+      icon.GetRepresentation(2.0f).GetBitmap());
+#else
+  return icon;
 #endif
 }
 
