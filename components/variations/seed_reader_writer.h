d9c46576a1effd93f0e8a7e2756c3c6ac0f82b62
Ramon Cano Aparicio <rcanoaparicio@google.com>
2026-09-23 06:56:38 -0700

    Bump variations seed files experiment version to V13
    This bumps the version of the seed files experiment control group
    (Control_V12 -> Control_V13) and treatment group (SeedFiles_V12 ->
    SeedFiles_V13).
    
    Bug: 530049020
    Change-Id: Ib3bfe5dbb00c69f68b5611bd0d4c9ab1f51bffef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8444716
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703347}

--- components/variations/seed_reader_writer.h
+++ components/variations/seed_reader_writer.h
@@ -32,10 +32,10 @@ class EntropyProviders;
 // Trial and group names for the seed file experiment.
 const char kSeedFileTrial[] = "SeedFileTrial";
 const char kDefaultGroup[] = "Default";
-const char kControlGroup[] = "Control_V12";
+const char kControlGroup[] = "Control_V13";
 // TODO(crbug.com/530049020): Launch on Android and iOS
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
-const char kSeedFilesGroup[] = "SeedFiles_V12";
+const char kSeedFilesGroup[] = "SeedFiles_V13";
 #else
 const char kSeedFilesGroup[] = "SeedFilesLaunched";
 #endif
