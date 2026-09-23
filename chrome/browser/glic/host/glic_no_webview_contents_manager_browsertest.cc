8161927eb712349d468f914265fbfc30ce014a69
Dan Harrington <harringtond@chromium.org>
2026-09-23 10:15:10 -0700

    Fix flaky LoadingStyleMatchesPanelState
    Use GlicNoWebviewOverlayBrowserTest so the guest page doesn't bootstrap
    a client and prematurely clear the loading overlay state during detach.
    
    Bug: b:565211288
    Change-Id: I4dfe8aa5518151324b4d7a6298b2c7596a6a6964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8452062
    Commit-Queue: Dan H <harringtond@chromium.org>
    Auto-Submit: Dan H <harringtond@chromium.org>
    Commit-Queue: Bryant Chandler <bryantchandler@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1703552}

--- chrome/browser/glic/host/glic_no_webview_contents_manager_browsertest.cc
+++ chrome/browser/glic/host/glic_no_webview_contents_manager_browsertest.cc
@@ -474,9 +474,8 @@ IN_PROC_BROWSER_TEST_F(GlicNoWebviewContentsManagerBrowserTest,
   EXPECT_EQ(unattached_state->get_loading(), mojom::LoadingStyle::kSidePanel);
 }
 
-// TODO(crbug.com/565211288): This test is flaky.
-IN_PROC_BROWSER_TEST_F(GlicNoWebviewContentsManagerBrowserTest,
-                       DISABLED_LoadingStyleMatchesPanelState) {
+IN_PROC_BROWSER_TEST_F(GlicNoWebviewOverlayBrowserTest,
+                       LoadingStyleMatchesPanelState) {
   {
     // Unattached manager defaults to kSidePanel loading style.
     GlicNoWebviewContentsManager manager(GetProfile(), &service()->enabling(),
