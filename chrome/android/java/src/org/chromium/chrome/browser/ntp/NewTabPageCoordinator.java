deca250c9aaee5f7252795368873d4b5a3ed121a
Xi Han <hanxi@chromium.org>
2026-09-23 13:48:34 -0700

    Revert "[Dse] Update ComposeplateUtils#IsAimEntrypointEligible() check."
    This reverts commit 6625805764c6a1598a172488f10b7f757bfc0a9e.
    
    Reason for revert: This check returns stale results on Android.
    
    
    Original change's description:
    > [Dse] Update ComposeplateUtils#IsAimEntrypointEligible() check.
    >
    > In this CL, we update C++ implementation of #isComposeplateEnabled(),
    > and now it handles all of the criteria checks when  3p DSE AI Mode
    > button swap is enabled.
    >
    > Bug: 561995440
    > Change-Id: I6859ae28c79fbc3bf2b1e6385a17596394384191
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8427943
    > Commit-Queue: Xi Han <hanxi@chromium.org>
    > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1702282}
    
    Bug: 561995440
    Change-Id: I145bc05c6152e673cba359a1d8ca148e28d66386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8447955
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1703741}

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageCoordinator.java
@@ -855,9 +855,7 @@ public class NewTabPageCoordinator implements ModuleDelegateHost {
             initializeComposeplate();
         }
 
-        if (mCanShowComposeplateButton == TriState.TRUE) {
-            maybeUpdateAiModeButton();
-        }
+        maybeUpdateAiModeButton();
 
         if (previousCanShowComposeplateButton != mCanShowComposeplateButton) {
             // When the AI mode button's visibility is changed, the height of search box might be
--- chrome/browser/composeplate/android/composeplate_utils.cc
+++ chrome/browser/composeplate/android/composeplate_utils.cc
@@ -3,17 +3,12 @@
 // found in the LICENSE file.
 
 #include "base/android/jni_android.h"
-#include "base/feature_list.h"
 #include "chrome/browser/autocomplete/aim_eligibility_service_factory.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/search_engines/ai_mode_button_service_factory.h"
-#include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "components/contextual_search/contextual_search_service.h"
 #include "components/omnibox/browser/aim_eligibility_service.h"
-#include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_prefs.h"
-#include "components/omnibox/common/omnibox_features.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/browser/composeplate/android/jni_headers/ComposeplateUtils_jni.h"
@@ -24,13 +19,7 @@ static bool JNI_ComposeplateUtils_IsAimEntrypointEligible(JNIEnv* env,
   DCHECK(profile);
   AimEligibilityService* aim_service =
       AimEligibilityServiceFactory::GetForProfile(profile);
-  if (!base::FeatureList::IsEnabled(omnibox::kAim3pEntrypoint)) {
-    return aim_service != nullptr && aim_service->IsAimEligible();
-  }
-
-  return OmniboxFieldTrial::IsAimOmniboxEntrypointEnabled(
-      aim_service, AiModeButtonServiceFactory::GetForProfile(profile),
-      TemplateURLServiceFactory::GetForProfile(profile));
+  return aim_service != nullptr && aim_service->IsAimEligible();
 }
 
 // static
