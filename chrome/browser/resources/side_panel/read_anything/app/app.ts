96f707398b4a1ab42fe54a718cde1dc14e57c601
Martín Gómez <martinglopez@google.com>
2026-09-24 11:34:19 -0700

    [M155] [Reading Mode] Update settingsPrefs to prevent stale values
    Original change's description:
    > [Reading Mode] Update settingsPrefs to prevent stale values
    >
    > Add settingPrefs_ update on specific method changes to keep the state
    > synchronized. This CL applies the settingsPrefs_ update to the following
    > methods:
    >
    > - onLinksToggle_
    > - onImagesToggle_
    > - onSpeechRateChange_
    > - onHighlightChange_
    >
    > Bug: 562849546
    > Change-Id: Idb49f147d392c63be2d716a874f93841003e8a7b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8434689
    > Reviewed-by: Lauren Winston <lwinston@google.com>
    > Commit-Queue: Martín Gómez <martinglopez@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1702337}
    
    (cherry picked from commit 26f60a8230cc396f27a110877b8e8f9cee31c818)
    
    Bug: 565775315,562849546
    Change-Id: Idb49f147d392c63be2d716a874f93841003e8a7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8457285
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8059@{#586}
    Cr-Branched-From: 199ac2cd7ab289e11c2236bd2cede848781213da-refs/heads/main@{#1697595}

--- chrome/browser/resources/side_panel/read_anything/app/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app/app.ts
@@ -417,6 +417,10 @@ export class AppElement extends AppElementBase implements SpeechListener,
   }
 
   protected onLinksToggle_() {
+    this.settingsPrefs_ = {
+      ...this.settingsPrefs_,
+      linksEnabled: this.visualBrowserProxy_.isLinksEnabled(),
+    };
     this.updateLinks_();
   }
 
@@ -425,6 +429,10 @@ export class AppElement extends AppElementBase implements SpeechListener,
   }
 
   protected onImagesToggle_() {
+    this.settingsPrefs_ = {
+      ...this.settingsPrefs_,
+      imagesEnabled: this.visualBrowserProxy_.isImagesEnabled(),
+    };
     this.updateImages_();
   }
 
@@ -680,6 +688,12 @@ export class AppElement extends AppElementBase implements SpeechListener,
   }
 
   protected onSpeechRateChange_() {
+    // TODO(crbug.com/564638585): Replace manual settingsPrefs_ updates for each
+    // onChange_ method with automated updates.
+    this.settingsPrefs_ = {
+      ...this.settingsPrefs_,
+      speechRate: this.audioBrowserProxy_.getSpeechRate(),
+    };
     this.speechController_.onSpeechSettingsChange();
   }
 
@@ -754,6 +768,10 @@ export class AppElement extends AppElementBase implements SpeechListener,
   }
 
   protected onHighlightChange_(event: CustomEvent<{data: number}>) {
+    this.settingsPrefs_ = {
+      ...this.settingsPrefs_,
+      highlightGranularity: event.detail.data,
+    };
     this.speechController_.onHighlightGranularityChange(event.detail.data);
     // Apply highlighting changes to the DOM.
     this.styleUpdater_.setHighlight();
