fea6434f89cb13aebeae260cdc056a406853c937
Mikita Kuchyn <kuchyn@google.com>
2026-09-24 06:38:58 -0700

    [autofill] Reenable `kAutofillShowTypePredictions` for capt-sites tests
    crrev.com/c/8425692 disabled kAutofillShowTypePredictions instead of
    only removing its "as-title" parameter. Re-enable the feature without
    parameters so "autofill-prediction" DOM attribute is set, required by
    "validateField" actions.
    
    Bug: 410879924
    Change-Id: I52c64dc23f9f3b9ac2d04938119c04ca57e82781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8460603
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Mikita Kuchyn <kuchyn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704252}

--- chrome/browser/autofill/autofill_captured_sites_interactive_uitest.cc
+++ chrome/browser/autofill/autofill_captured_sites_interactive_uitest.cc
@@ -410,7 +410,8 @@ class AutofillCapturedSitesInteractiveTest
         /*enabled_features=*/
         {{features::kAutofillRequireFocusInFrameForSuggestions, {}},
          {features::debug::kAutofillCapturedSiteTestsUseAutofillFlow, {}},
-         {features::debug::kAutofillServerCommunication, {}}},
+         {features::debug::kAutofillServerCommunication, {}},
+         {features::debug::kAutofillShowTypePredictions, {}}},
         /*disabled_features=*/{});
     command_line->AppendSwitchASCII(
         variations::switches::kVariationsOverrideCountry, "us");
