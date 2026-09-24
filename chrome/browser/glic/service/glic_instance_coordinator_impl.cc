ba97d6a14477c403f1b62abcc27f21435ea77ce2
Sana Akbani <sanaakbani@google.com>
2026-09-24 11:55:09 -0700

    [glic] Set kTrustFirstInline when kGlicActionFirstFRE is enabled
    When kGlicActionFirstFRE is enabled and the user has not yet consented,
    set fre_override to TrustFirstInline to prepare for Action First FRE
    changes.
    
    Bug: b:553523776
    Change-Id: I740c688efcb0d7e46bee7caca9217c87956265ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8449123
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Cr-Commit-Position: refs/heads/main@{#1704478}

--- chrome/browser/glic/service/glic_instance_coordinator_impl.cc
+++ chrome/browser/glic/service/glic_instance_coordinator_impl.cc
@@ -455,7 +455,8 @@ bool GlicInstanceCoordinatorImpl::MaybeInvoke(BrowserWindowInterface* bwi,
       !GlicEnabling::HasConsentedForProfile(profile_);
 
   if (fre_override_compatible && panel_closed &&
-      base::FeatureList::IsEnabled(features::kGlicMessageFirstFre)) {
+      (base::FeatureList::IsEnabled(features::kGlicMessageFirstFre) ||
+       base::FeatureList::IsEnabled(features::kGlicActionFirstFRE))) {
     GlicInvokeOptions options(source);
     if (auto* active_tab = TabListInterface::From(target_bwi)->GetActiveTab()) {
       options.target = Target(*active_tab);
@@ -1258,7 +1259,8 @@ void GlicInstanceCoordinatorImpl::InvokeAndLogToggle(
   invoke_options.target.surface = std::move(surface);
   invoke_options.fre_completion_wait_mode = FreCompletionWaitMode::kNever;
   if (!GlicEnabling::HasConsentedForProfile(profile_) &&
-      base::FeatureList::IsEnabled(features::kGlicMessageFirstFre)) {
+      (base::FeatureList::IsEnabled(features::kGlicMessageFirstFre) ||
+       base::FeatureList::IsEnabled(features::kGlicActionFirstFRE))) {
     invoke_options.fre_override = mojom::FreOverride::kTrustFirstInline;
   }
   auto weak_instance = InvokeInternal(std::nullopt, std::move(invoke_options),
