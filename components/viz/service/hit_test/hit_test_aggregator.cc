425de4fbabe45c9494800fc96df634ab0741fac4
Seonggon Cho <jmsmg1@me.com>
2026-09-24 12:22:07 -0700

    [viz] Remove the kRejectInvalidChildRegions kill switch
    HitTestAggregator rejects child hit-test regions whose frame sink is
    not actually a child of the submitting frame sink, behind a kill
    switch to be removed once M150 hit stable. The check has been enabled
    by default since it landed and the tree is at M156, so make it
    unconditional and delete the flag along with the include it was the
    last user of.
    
    R=jonross@chromium.org
    
    Bug: 495852034
    Change-Id: I4ae6d28f6f8185ce10c1ce8a9c6eefc5c0e53b6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8449710
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704506}

--- components/viz/service/hit_test/hit_test_aggregator.cc
+++ components/viz/service/hit_test/hit_test_aggregator.cc
@@ -5,7 +5,6 @@
 #include "components/viz/service/hit_test/hit_test_aggregator.h"
 
 #include "base/feature.h"
-#include "base/feature_list.h"
 #include "base/trace_event/trace_event.h"
 #include "base/types/expected.h"
 #include "components/viz/common/hit_test/hit_test_region_list.h"
@@ -16,10 +15,6 @@
 #include "ui/gfx/geometry/rrect_f.h"
 
 namespace viz {
-namespace {
-// TODO (crbug.com/495852034): Remove once M150 hits Stable.
-BASE_FEATURE(kRejectInvalidChildRegions, base::FEATURE_ENABLED_BY_DEFAULT);
-}  // namespace
 
 HitTestAggregator::HitTestAggregator(
     const HitTestManager* hit_test_manager,
@@ -162,8 +157,7 @@ HitTestAggregator::AppendRegion(size_t region_index,
     }
 
     // Verify that the child is actually a child of the submitting frame sink.
-    if (base::FeatureList::IsEnabled(kRejectInvalidChildRegions) &&
-        !delegate_->IsChildOf(submitting_frame_sink_id, region.frame_sink_id)) {
+    if (!delegate_->IsChildOf(submitting_frame_sink_id, region.frame_sink_id)) {
       return base::unexpected(AggregationError::INVALID_CHILD_REGION);
     }
 
