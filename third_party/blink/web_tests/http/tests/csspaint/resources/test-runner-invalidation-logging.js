31ecc38832ff982ab8a2f18c31afbd89d92ee7fe
Helmut Januschka <helmut@januschka.com>
2026-09-23 23:14:15 -0700

    Make CSS paint invalidation tests assert results
    Replace ignored console output with awaited testharness assertions so
    invalidation failures reliably fail the tests.
    
    Bug: 41313847
    Change-Id: Ifcbb8fd5ad2bb8f7a53360144079a9556209a460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8419448
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704053}

--- third_party/blink/web_tests/http/tests/csspaint/resources/test-runner-invalidation-logging.js
+++ third_party/blink/web_tests/http/tests/csspaint/resources/test-runner-invalidation-logging.js
@@ -1,105 +1,105 @@
 // Test runner for the paint worklet to test invalidation behaviour.
 //
-// Registers an promise_test per test case, which:
+// Registers a promise_test per test case, which:
 //  - Creates an element.
 //  - Invalidates the style of that element.
-//  - [worklet] Worklet code logs if it got the invalidation.
 //  - Asserts that it got the correct paint invalidation.
 //
 // Usage:
-// testRunnerInvalidationLogging('background-image', [
+// testRunnerInvalidation('background-image', [
 //      { property: 'max-height', value: '100px' },
-//      { property: 'color', prevValue: '#00F', value: 'blue', noInvalidation: true },
-//      { property: 'margin-inline-start', invalidationProperty: 'margin-left', prevValue: 'calc(50px + 50px)', value: '100px', noInvalidation: true }
 // ]);
 
-function testRunnerInvalidationLogging(imageType, tests) {
-    const keys = tests.map(function(obj) { return obj.property; });
-    const workletCode = 'const properties = ' + JSON.stringify(keys) + ';\n' + `
+function testRunnerInvalidation(imageType, tests) {
+  const keys = tests.map(function(obj) {
+    return obj.property;
+  });
+  const workletCode = 'const properties = ' + JSON.stringify(keys) + ';\n' +
+      `
         for (let i = 0; i < properties.length; i++) {
             registerPaint('paint-' + i, class {
                 static get inputProperties() { return [properties[i]]; }
-                constructor() { this.hasPainted= false; }
+                constructor() { this.hasPainted = false; }
                 paint(ctx, geom) {
                     ctx.fillStyle = this.hasPainted ? 'green' : 'blue';
                     ctx.fillRect(0, 0, geom.width, geom.height);
-                    if (this.hasPainted) {
-                        console.log('Successful invalidation for: ' + properties[i]);
-                    }
                     this.hasPainted = true;
                 }
             });
         }
     `;
 
-    CSS.paintWorklet.addModule(URL.createObjectURL(new Blob([workletCode], {type: 'text/javascript'}))).then(function() {
+  CSS.paintWorklet
+      .addModule(URL.createObjectURL(
+          new Blob([workletCode], {type: 'text/javascript'})))
+      .then(function() {
         for (let i = 0; i < tests.length; i++) {
-            tests[i].paintName = 'paint-' + i;
-            registerTest(imageType, tests[i]);
+          tests[i].paintName = 'paint-' + i;
+          registerTest(imageType, tests[i]);
         }
-    });
+      });
 }
 
 function registerTest(imageType, test) {
-    const testName = test.property + ': ' + (test.prevValue || '[inline not set]') + ' => ' + (test.invalidationProperty || test.property) + ': ' + (test.value || '[inline not set]');
+  const testName = test.property + ': ' +
+      (test.prevValue || '[inline not set]') + ' => ' +
+      (test.invalidationProperty || test.property) + ': ' +
+      (test.value || '[inline not set]');
 
-    // We use a promise_test instead of a async_test as they run sequentially.
-    promise_test(function() {
-        return new Promise(function(resolve) {
+  // We use a promise_test instead of an async_test as they run sequentially.
+  promise_test(async function() {
+    assert_true(!!window.internals, 'This test requires internals.');
 
-            const msg = ['\n\nTest case:', testName];
-            if (test.noInvalidation) {
-                msg.push('The worklet console should log nothing');
-            } else {
-                msg.push('The worklet console should log: \'Successful invalidation for: ' + test.property + '\'');
-            }
-            console.log(msg.join('\n'));
+    const el = document.createElement('div');
+    if (test.prevValue)
+      el.style.setProperty(test.property, test.prevValue);
+    el.style[imageType] = 'paint(' + test.paintName + ')';
+    document.body.appendChild(el);
 
-            // Create the test div.
-            const el = document.createElement('div');
-            if (test.prevValue) el.style.setProperty(test.property, test.prevValue);
-            el.style[imageType] = 'paint(' + test.paintName + ')';
-            document.body.appendChild(el);
+    let trackingRepaints = false;
+    try {
+      await new Promise(resolve => runAfterLayoutAndPaint(resolve));
+      internals.startTrackingRepaints(document);
+      trackingRepaints = true;
 
-            runAfterLayoutAndPaint(function() {
-                if (window.internals)
-                    internals.startTrackingRepaints(document);
+      // Keep the BCR for the paint invalidation assertion, and invalidate
+      // paint.
+      const rect = el.getBoundingClientRect();
+      if (test.invalidationProperty) {
+        el.style.setProperty(test.invalidationProperty, test.value);
+      } else {
+        el.style.setProperty(test.property, test.value);
+      }
 
-                // Keep the BCR for the paint invalidation assertion, and invalidate paint.
-                const rect = el.getBoundingClientRect();
-                if (test.invalidationProperty) {
-                    el.style.setProperty(test.invalidationProperty, test.value);
-                } else {
-                    el.style.setProperty(test.property, test.value);
-                }
+      await new Promise(resolve => runAfterLayoutAndPaint(resolve));
 
-                runAfterLayoutAndPaint(function() {
-                    // Check that the we got the correct paint invalidation.
-                    if (window.internals) {
-                        const layers = JSON.parse(internals.layerTreeAsText(document, internals.LAYER_TREE_INCLUDES_INVALIDATIONS));
-                        // Collect paint invalidations from all layers.
-                        var invalidations = [];
-                        layers.layers.forEach(layer => {
-                          if (layer.invalidations)
-                            invalidations.push.apply(invalidations, layer.invalidations);
-                        });
-                        var hasNoInvalidations = invalidations.length === 0;
-                        assert_equals(hasNoInvalidations, !!test.noInvalidation);
-                        if (!hasNoInvalidations) {
-                            assert_equals(invalidations.length, 1, 'There should be only one invalidation.');
-                            assert_array_equals(invalidations[0], [rect.left, rect.top, rect.width, rect.height],
-                                                'The paint invalidation should cover the entire element.');
-                        }
-                        internals.stopTrackingRepaints(document);
-                    }
+      const layers = JSON.parse(internals.layerTreeAsText(
+          document, internals.LAYER_TREE_INCLUDES_INVALIDATIONS));
+      // Collect paint invalidations from all layers.
+      const invalidations = [];
+      layers.layers.forEach(layer => {
+        if (layer.invalidations)
+          invalidations.push.apply(invalidations, layer.invalidations);
+      });
+      const hasNoInvalidations = invalidations.length === 0;
+      internals.stopTrackingRepaints(document);
+      trackingRepaints = false;
 
-                    // Cleanup.
-                    document.body.removeChild(el);
-                    resolve();
-                });
-            });
-
-        });
-
-    }, testName);
+      assert_equals(hasNoInvalidations, !!test.noInvalidation);
+      if (!hasNoInvalidations) {
+        assert_equals(invalidations.length, 1,
+                      'There should be only one invalidation.');
+        const expectedInvalidation = [
+          rect.left, rect.top, rect.width, rect.height
+        ].map(value => value * devicePixelRatio);
+        assert_array_equals(
+            invalidations[0], expectedInvalidation,
+            'The paint invalidation should cover the entire element.');
+      }
+    } finally {
+      if (trackingRepaints)
+        internals.stopTrackingRepaints(document);
+      el.remove();
+    }
+  }, testName);
 }
