aa40f059f2aca7d49d841151e80cf0d0240ecce0
Allen Bauer <kylixrd@chromium.org>
2026-09-25 11:22:54 -0700

    [M155] Harden AnimationBuilder against layer deletions during scheduling.
    Original change's description:
    > Harden AnimationBuilder against layer deletions during scheduling.
    >
    > For certain cases where a started animation is immediately finished when
    > scheduled, the callbacks invoked might destroy the view on which the
    > layer is animating. Any subsequence animation sequences for that layer
    > are now invalid and can lead to a UAF as they're being scheduled. This
    > tracks the lifetime of the layers while the AnimationBuilder instance
    > is scheduling the animations.
    >
    > Change-Id: I759edbe4bebbb86c5f31b83b5930a8484d966d1b
    > Bug: 561997480
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8423046
    > Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    > Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1703780}
    
    (cherry picked from commit c3b0bcd0d3b2f49d5126f0298796c0cdcfdc2921)
    
    Bug: 566048080,561997480
    Change-Id: I759edbe4bebbb86c5f31b83b5930a8484d966d1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8467850
    Auto-Submit: chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com <chrome-cherry-picker@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: rubber-stamper@appspot.gserviceaccount.com <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/branch-heads/8059@{#627}
    Cr-Branched-From: 199ac2cd7ab289e11c2236bd2cede848781213da-refs/heads/main@{#1697595}

--- ui/views/animation/animation_builder.cc
+++ ui/views/animation/animation_builder.cc
@@ -5,6 +5,8 @@
 #include "ui/views/animation/animation_builder.h"
 
 #include <algorithm>
+#include <iterator>
+#include <memory>
 #include <tuple>
 #include <utility>
 #include <vector>
@@ -14,6 +16,7 @@
 #include "base/functional/callback.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
+#include "base/memory/weak_ptr.h"
 #include "base/no_destructor.h"
 #include "base/time/time.h"
 #include "ui/compositor/layer.h"
@@ -28,6 +31,15 @@
 #include "ui/views/animation/animation_sequence_block.h"
 
 namespace views {
+namespace {
+// Used during scheduling to track whether a layer had been destroyed due to
+// a previously scheduled animation sequence terminating early.
+struct LayerSequences {
+  base::WeakPtr<ui::Layer> layer;
+  std::vector<std::unique_ptr<ui::LayerAnimationSequence>> sequences;
+};
+
+}  // namespace
 
 AnimationBuilder::Observer::Observer() = default;
 
@@ -220,10 +232,30 @@ AnimationBuilder::~AnimationBuilder() {
     animation_observer_.release();
   }
 
+  std::vector<LayerSequences> layer_sequences;
   for (auto it = layer_animation_sequences_.begin();
        it != layer_animation_sequences_.end();) {
     auto* const target = it->first;
     auto end_it = layer_animation_sequences_.upper_bound(target);
+    LayerSequences this_layer_sequences;
+    this_layer_sequences.layer = target->AsWeakPtr();
+    std::transform(it, end_it,
+                   std::back_inserter(this_layer_sequences.sequences),
+                   [](auto& it) { return std::move(it.second); });
+    layer_sequences.emplace_back(std::move(this_layer_sequences));
+    it = end_it;
+  }
+
+  for (auto& layer_sequence : layer_sequences) {
+    if (!layer_sequence.layer) {
+      // If the layer had been destroyed from an earlier animation sequence
+      // terminating early, make sure all remaining sequences are also aborted.
+      for (auto& sequence : layer_sequence.sequences) {
+        sequence->Abort(nullptr);
+      }
+      continue;
+    }
+    auto* const target = layer_sequence.layer.get();
 
     if (abort_handle_) {
       abort_handle_->AddLayer(target);
@@ -233,11 +265,13 @@ AnimationBuilder::~AnimationBuilder() {
     if (preemption_strategy_) {
       settings.SetPreemptionStrategy(preemption_strategy_.value());
     }
+
     std::vector<ui::LayerAnimationSequence*> sequences;
-    std::transform(it, end_it, std::back_inserter(sequences),
-                   [](auto& it) { return it.second.release(); });
+    std::transform(layer_sequence.sequences.begin(),
+                   layer_sequence.sequences.end(),
+                   std::back_inserter(sequences),
+                   [](auto& sequence) { return sequence.release(); });
     target->GetAnimator()->StartTogether(std::move(sequences));
-    it = end_it;
   }
 }
 
