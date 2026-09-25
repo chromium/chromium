2deeb538ecd641d3b1a2afc17abd5ab349022e31
Noelle Scobie <nscobie@google.com>
2026-09-25 12:22:45 -0700

    [skia] Add staging flag for flat varyings interpolation in Graphite
    Bug: 565400970
    Change-Id: I4a0b3e6cbae879ae9b97fe6c8955c21a6c5f4811
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8457989
    Commit-Queue: Noelle Scobie <nscobie@google.com>
    Reviewed-by: Greg Daniel <egdaniel@google.com>
    Auto-Submit: Noelle Scobie <nscobie@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1705044}

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -149,6 +149,8 @@ SK_API void SkLog_FileLine(const char* file,
 
 #define SK_LEGACY_GRAPHITE_READ_PIXELS_BOTTOM_LEFT_BEHAVIOR
 
+#define SK_DISABLE_GRAPHITE_NONINTEGRAL_VARYINGS_FLAT_INTERPOLATION
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
