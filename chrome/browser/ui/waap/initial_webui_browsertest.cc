a5c050d5b7b02b68986aeb33f82bde4f713fe1a1
Ming-Ying Chung <mych@chromium.org>
2026-09-24 20:51:32 -0700

    [webium] Remove deprecated `InitialWebUI` metrics
    - Deprecated V1 NewWindow metrics, represented by the empty variant in
      NewWindowMetricVersion, superseded by V2 (*2) in M152.
    - Obsolete base InitialWebUI.NewWindow.AllSources.*.FromConstructor
      metrics without existing window breakdowns.
    - Obsolete InitialWebUI.Startup.WithoutExistingWindow.* metrics, which
      were replaced by InitialWebUI.Startup{Scenario}.* (as the
      WithoutExistingWindow distinction does not apply during startup).
    
    Also fixes a stale metric name assertion in initial_webui_browsertest.cc
    to check the canonical ShowRequestedToFirstPaint2.
    
    OBSOLETE_HISTOGRAM[InitialWebUI.NewWindow.AllSources.BrowserWindow.FirstPaint.FromConstructor]=Replaced by WithExistingWindow and WithoutExistingWindow variants in M148
    OBSOLETE_HISTOGRAM[InitialWebUI.NewWindow.AllSources.ReloadButton.FirstPaint.FromConstructor]=Replaced by WithExistingWindow and WithoutExistingWindow variants in M148
    OBSOLETE_HISTOGRAM[InitialWebUI.Startup.WithoutExistingWindow.BrowserWindow.FirstPaint]=Removed WithoutExistingWindow dimension in M150, replaced by InitialWebUI.Startup{Scenario}.BrowserWindow.FirstPaint
    OBSOLETE_HISTOGRAM[InitialWebUI.Startup.WithoutExistingWindow.BrowserWindowToReloadButton.FirstPaintGap]=Removed WithoutExistingWindow dimension in M150, replaced by InitialWebUI.Startup{Scenario}.BrowserWindowToReloadButton.FirstPaintGap
    OBSOLETE_HISTOGRAM[InitialWebUI.Startup.WithoutExistingWindow.ReloadButton.FirstContentfulPaint]=Removed WithoutExistingWindow dimension in M150, replaced by InitialWebUI.Startup{Scenario}.ReloadButton.FirstContentfulPaint
    OBSOLETE_HISTOGRAM[InitialWebUI.Startup.WithoutExistingWindow.ReloadButton.FirstPaint]=Removed WithoutExistingWindow dimension in M150, replaced by InitialWebUI.Startup{Scenario}.ReloadButton.FirstPaint
    OBSOLETE_HISTOGRAMS=Deprecated V1 NewWindow metrics superseded by V2 in M152
    
    Bug: b:448794588
    Change-Id: I790258000f415eb24e0a43ed0d7405ed8bf776a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8458582
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Anna Sato <annasato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1704614}

--- chrome/browser/ui/waap/initial_webui_browsertest.cc
+++ chrome/browser/ui/waap/initial_webui_browsertest.cc
@@ -928,7 +928,7 @@ IN_PROC_BROWSER_TEST_F(InitialWebUIMinimizedWindowBrowserTest,
   // Verify ShowRequestedToFirstPaint was not recorded.
   histogram_tester.ExpectTotalCount(
       "InitialWebUI.NewWindow.AllSources.WithoutExistingWindow.BrowserWindow."
-      "ShowRequestedToFirstPaint.FromConstructor2",
+      "ShowRequestedToFirstPaint2",
       0);
 
   // Verify FirstPaintGap was not recorded.
