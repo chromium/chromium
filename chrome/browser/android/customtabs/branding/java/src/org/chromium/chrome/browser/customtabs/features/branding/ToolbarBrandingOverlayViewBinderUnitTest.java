8b51d4fdf6807cbba0acd7889b85e1bc3fdac189
Fiaz Muhammad <mfiaz@google.com>
2026-09-25 18:38:44 -0700

    [Tests] Migrate ToolbarBrandingOverlayViewBinderUnitTest to Robolectric
    Migrate ToolbarBrandingOverlayViewBinderUnitTest from on-device
    instrumentation tests
    (chrome/browser/android/customtabs/branding:unit_device_javatests) to
    host-side Robolectric
    (chrome/browser/android/customtabs/branding:junit), deleting the emptied
    branding:unit_device_javatests target.
    
    ToolbarBrandingOverlayViewBinderUnitTest exercises pure Java
    PropertyModelChangeProcessor color and theme bindings across
    BrandedColorScheme variants on custom_tabs_toolbar_branding_layout. It
    has zero dependency on native C++ browser processes, WebContents, or
    hardware-accelerated rendering, and previously launched
    BlankUiTestActivity only to inflate the layout.
    
    Running this test under BaseRobolectricTestRunner removes unnecessary
    device instrumentation and APK installation overhead, executing in
    milliseconds on the host JVM and improving CQ reliability.
    
    Bypass-Check-License: Renamed existing test file to Robolectric
    Bug: 551864627
    Change-Id: I9e7c590af06e889ac36d9aa14f83ae6a298b585b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8434043
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1705398}

--- chrome/browser/android/customtabs/branding/java/src/org/chromium/chrome/browser/customtabs/features/branding/ToolbarBrandingOverlayViewBinderUnitTest.java
+++ chrome/browser/android/customtabs/branding/java/src/org/chromium/chrome/browser/customtabs/features/branding/ToolbarBrandingOverlayViewBinderUnitTest.java
@@ -4,7 +4,7 @@
 
 package org.chromium.chrome.browser.customtabs.features.branding;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import static org.chromium.chrome.browser.customtabs.features.branding.ToolbarBrandingOverlayProperties.COLOR_DATA;
 
@@ -15,133 +15,107 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.test.filters.SmallTest;
-
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 
-import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.BaseActivityTestRule;
-import org.chromium.base.test.BaseJUnit4ClassRunner;
-import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.omnibox.styles.OmniboxResourceProvider;
 import org.chromium.chrome.browser.theme.ThemeUtils;
 import org.chromium.chrome.browser.ui.theme.BrandedColorScheme;
+import org.chromium.ui.base.TestActivity;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.modelutil.PropertyModelChangeProcessor;
-import org.chromium.ui.test.util.BlankUiTestActivity;
 
-/** On-device unit tests for {@link ToolbarBrandingOverlayViewBinder}. */
-@RunWith(BaseJUnit4ClassRunner.class)
-@Batch(Batch.UNIT_TESTS)
+/** Unit tests for {@link ToolbarBrandingOverlayViewBinder}. */
+@RunWith(BaseRobolectricTestRunner.class)
 public class ToolbarBrandingOverlayViewBinderUnitTest {
-    @ClassRule
-    public static final BaseActivityTestRule<BlankUiTestActivity> sActivityTestRule =
-            new BaseActivityTestRule<>(BlankUiTestActivity.class);
-
-    private static Activity sActivity;
-
+    private Activity mActivity;
     private PropertyModel mModel;
     private View mView;
     private ImageView mIcon;
     private TextView mText;
 
-    @BeforeClass
-    public static void setupSuite() {
-        sActivity = sActivityTestRule.launchActivity(null);
-    }
-
     @Before
     public void setUp() {
-        ThreadUtils.runOnUiThreadBlocking(
-                () -> {
-                    sActivity.setContentView(R.layout.custom_tabs_toolbar_branding_layout);
-                    mView = sActivity.findViewById(android.R.id.content);
-                    mIcon = mView.findViewById(R.id.branding_icon);
-                    mText = mView.findViewById(R.id.branding_text);
+        mActivity = Robolectric.buildActivity(TestActivity.class).setup().get();
+        mActivity.setContentView(R.layout.custom_tabs_toolbar_branding_layout);
+        mView = mActivity.findViewById(android.R.id.content);
+        mIcon = mView.findViewById(R.id.branding_icon);
+        mText = mView.findViewById(R.id.branding_text);
 
-                    mModel =
-                            new PropertyModel.Builder(ToolbarBrandingOverlayProperties.ALL_KEYS)
-                                    .build();
-                    PropertyModelChangeProcessor.create(
-                            mModel, mView, ToolbarBrandingOverlayViewBinder::bind);
-                });
+        mModel = new PropertyModel.Builder(ToolbarBrandingOverlayProperties.ALL_KEYS).build();
+        PropertyModelChangeProcessor.create(mModel, mView, ToolbarBrandingOverlayViewBinder::bind);
     }
 
     @Test
-    @SmallTest
     public void testAppDefault() {
         var colorData =
                 new ToolbarBrandingOverlayProperties.ColorData(
                         Color.WHITE, BrandedColorScheme.APP_DEFAULT);
-        ThreadUtils.runOnUiThreadBlocking(() -> mModel.set(COLOR_DATA, colorData));
+        mModel.set(COLOR_DATA, colorData);
 
         assertEquals(Color.WHITE, ((ColorDrawable) mView.getBackground()).getColor());
         assertEquals(
                 OmniboxResourceProvider.getUrlBarPrimaryTextColor(
-                        sActivity, BrandedColorScheme.APP_DEFAULT),
+                        mActivity, BrandedColorScheme.APP_DEFAULT),
                 mText.getTextColors().getDefaultColor());
         assertEquals(
-                ThemeUtils.getThemedToolbarIconTint(sActivity, BrandedColorScheme.APP_DEFAULT),
+                ThemeUtils.getThemedToolbarIconTint(mActivity, BrandedColorScheme.APP_DEFAULT),
                 mIcon.getImageTintList());
     }
 
     @Test
-    @SmallTest
     public void testDarkBrandedTheme() {
         var colorData =
                 new ToolbarBrandingOverlayProperties.ColorData(
                         Color.BLACK, BrandedColorScheme.DARK_BRANDED_THEME);
-        ThreadUtils.runOnUiThreadBlocking(() -> mModel.set(COLOR_DATA, colorData));
+        mModel.set(COLOR_DATA, colorData);
 
         assertEquals(Color.BLACK, ((ColorDrawable) mView.getBackground()).getColor());
         assertEquals(
                 OmniboxResourceProvider.getUrlBarPrimaryTextColor(
-                        sActivity, BrandedColorScheme.DARK_BRANDED_THEME),
+                        mActivity, BrandedColorScheme.DARK_BRANDED_THEME),
                 mText.getTextColors().getDefaultColor());
         assertEquals(
                 ThemeUtils.getThemedToolbarIconTint(
-                        sActivity, BrandedColorScheme.DARK_BRANDED_THEME),
+                        mActivity, BrandedColorScheme.DARK_BRANDED_THEME),
                 mIcon.getImageTintList());
     }
 
     @Test
-    @SmallTest
     public void testLightBrandedTheme() {
         var colorData =
                 new ToolbarBrandingOverlayProperties.ColorData(
                         Color.WHITE, BrandedColorScheme.LIGHT_BRANDED_THEME);
-        ThreadUtils.runOnUiThreadBlocking(() -> mModel.set(COLOR_DATA, colorData));
+        mModel.set(COLOR_DATA, colorData);
 
         assertEquals(Color.WHITE, ((ColorDrawable) mView.getBackground()).getColor());
         assertEquals(
                 OmniboxResourceProvider.getUrlBarPrimaryTextColor(
-                        sActivity, BrandedColorScheme.LIGHT_BRANDED_THEME),
+                        mActivity, BrandedColorScheme.LIGHT_BRANDED_THEME),
                 mText.getTextColors().getDefaultColor());
         assertEquals(
                 ThemeUtils.getThemedToolbarIconTint(
-                        sActivity, BrandedColorScheme.LIGHT_BRANDED_THEME),
+                        mActivity, BrandedColorScheme.LIGHT_BRANDED_THEME),
                 mIcon.getImageTintList());
     }
 
     @Test
-    @SmallTest
     public void testIncognito() {
         var colorData =
                 new ToolbarBrandingOverlayProperties.ColorData(
                         Color.DKGRAY, BrandedColorScheme.INCOGNITO);
-        ThreadUtils.runOnUiThreadBlocking(() -> mModel.set(COLOR_DATA, colorData));
+        mModel.set(COLOR_DATA, colorData);
 
         assertEquals(Color.DKGRAY, ((ColorDrawable) mView.getBackground()).getColor());
         assertEquals(
                 OmniboxResourceProvider.getUrlBarPrimaryTextColor(
-                        sActivity, BrandedColorScheme.INCOGNITO),
+                        mActivity, BrandedColorScheme.INCOGNITO),
                 mText.getTextColors().getDefaultColor());
         assertEquals(
-                ThemeUtils.getThemedToolbarIconTint(sActivity, BrandedColorScheme.INCOGNITO),
+                ThemeUtils.getThemedToolbarIconTint(mActivity, BrandedColorScheme.INCOGNITO),
                 mIcon.getImageTintList());
     }
 }
